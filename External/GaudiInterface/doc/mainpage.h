d7a27a9d8c12505515a00b7aa189dc44d7112efc
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2007-10-31 16:53:25 +0100

    cvs2svn commit to create tag 'GaudiInterface-01-03-04'. (GaudiInterface-01-03-04)
    Former-commit-id: 87898ed6ec003162f13110315e60be05615aaa01

---
+++ External/GaudiInterface/doc/mainpage.h
@@ -0,0 +1,15 @@
+/**
+
+@mainpage GaudiInterface package
+	 @author David.Quarrie@cern.ch
+	
+@section GaudiInterfaceIntro Introduction
+
+The package is a glue package to the GAUDI GaudiKernel package
+*/
+
+/**
+@page RequirementsGaudiInterface Requirements
+@include requirements
+
+*/
