888061b30313c0e9e60b5cc317714775e54f838a
Jochen Meyer <Jochen.Meyer@cern.ch>
2009-08-11 10:17:05 +0200

    See ChangeLog File (MuonSimValUtils-00-00-07)

---
+++ MuonSpectrometer/MuonValidation/MuonSimValUtils/src/MuonChamberNameConverter.cxx
@@ -0,0 +1,216 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "MuonSimValUtils/MuonChamberNameConverter.h"
+#include <cstdlib>
+
+using namespace std;
+
+
+//Function converting from ATHENA identifier int StationName,int StationEta, int StationPhi, std::string ChamberType 
+//to the (hopefully) corresponding Hardware chamber name.Works only for Barrel MDTs (for the moment) 
+std::string convertChamberName(int ChamberName , int ChamberEta , int ChamberPhi , std::string ChamberType) {
+
+  //Input values
+  int m_Name_int = ChamberName;
+  int m_Eta_int = ChamberEta;
+  int m_Phi_int = ChamberPhi;
+  //add type
+  //"MDT","CSC","TGC" go in the same loop while
+  //"RPC" go to a different loop due to the same
+  //naming with "MDT"
+  std::string m_Type=ChamberType;
+
+
+  //Set string variables
+  std::string m_Name_string;
+  std::string m_Eta_string;
+  std::string m_Phi_string;
+
+  //Vector setting the chamber eta, phi range
+  //Elements:
+  // limit eta , forbiden eta , min phi , max phi , allowed phi1 , allowed phi2
+  std::vector<int> limits;
+
+
+  if (m_Type=="MDT" || m_Type=="CSC" || m_Type=="TGC"){
+    //Switch from ATHENA int name to Hardware string name 
+    switch (m_Name_int) 
+      {
+	// identifier :   harware name	  : limit eta	       : forbiden eta	    : min phi		 :  max phi	      :   allowed    phi1     :	allowed phi2      :  long/short
+      case 0  : m_Name_string = "BIL"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(5); limits.push_back(7); limits.push_back(-100); limits.push_back(1); break;
+      case 1  : m_Name_string = "BIS"; limits.push_back(8); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 2  : m_Name_string = "BML"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 3  : m_Name_string = "BMS"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(5); limits.push_back(8); limits.push_back(-100); limits.push_back(-1); break;
+      case 4  : m_Name_string = "BOL"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 5  : m_Name_string = "BOS"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(5); limits.push_back(8); limits.push_back(-100); limits.push_back(-1); break;
+      case 6  : m_Name_string = "BEE"; limits.push_back(2); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 7  : m_Name_string = "BIR"; limits.push_back(6); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(8); limits.push_back(1); break;
+      case 8  : m_Name_string = "BMF"; limits.push_back(3); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;
+      case 9  : m_Name_string = "BOF"; limits.push_back(4); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;
+      case 10 : m_Name_string = "BOG"; limits.push_back(4); limits.push_back(-100); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;
+      case 11 : m_Name_string = "BOH_DOES_NOT_EXIST!"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 12 : m_Name_string = "EIC_DOES_NOT_EXIST!"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 13 : m_Name_string = "EIL"; limits.push_back(5); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 14 : m_Name_string = "EEL"; limits.push_back(2); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 15 : m_Name_string = "EES"; limits.push_back(2); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 16 : m_Name_string = "EMC_DOES_NOT_EXIST!"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 17 : m_Name_string = "EML"; limits.push_back(5); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 18 : m_Name_string = "EMS"; limits.push_back(5); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 19 : m_Name_string = "EOC_DOES_NOT_EXIST!"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 20 : m_Name_string = "EOL"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 21 : m_Name_string = "EOS"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 22 : m_Name_string = "T1C"; break;
+      case 23 : m_Name_string = "T1L"; break;
+      case 24 : m_Name_string = "T1S"; break;
+      case 25 : m_Name_string = "T2C"; break;
+      case 26 : m_Name_string = "T2L"; break;
+      case 27 : m_Name_string = "T2S"; break;
+      case 28 : m_Name_string = "T3C"; break;
+      case 29 : m_Name_string = "T3L"; break;
+      case 30 : m_Name_string = "T3S"; break;
+      case 31 : m_Name_string = "CI1"; break;
+      case 32 : m_Name_string = "CI2"; break;
+      case 33 : m_Name_string = "CI3"; break;
+      case 34 : m_Name_string = "CI4"; break;
+      case 35 : m_Name_string = "FIL"; break;
+      case 36 : m_Name_string = "FIS"; break;
+      case 37 : m_Name_string = "FML"; break;
+      case 38 : m_Name_string = "FMS"; break;
+      case 39 : m_Name_string = "FOL"; break;
+      case 40 : m_Name_string = "FOS"; break;
+        // TGC part implemented by Masaya Ishino (Masaya.Ishino@cern.ch)         
+	// TGC start ;    harware name	  : limit eta	       : forbiden eta	    : min phi		 :  max phi	      :   allowed    phi1     :	allowed phi2      :  long/short
+      case 41 : m_Name_string = "T1F"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(24); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 42 : m_Name_string = "T1E"; limits.push_back(4); limits.push_back(0); limits.push_back(1); limits.push_back(48); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 43 : m_Name_string = "T2F"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(24); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 44 : m_Name_string = "T2E"; limits.push_back(5); limits.push_back(0); limits.push_back(1); limits.push_back(48); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 45 : m_Name_string = "T3F"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(24); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 46 : m_Name_string = "T3E"; limits.push_back(5); limits.push_back(0); limits.push_back(1); limits.push_back(48); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 47 : m_Name_string = "T4F"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(24); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 48 : m_Name_string = "T4E"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(21); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+	// TGC end   ;    harware name	  : limit eta	       : forbiden eta	    : min phi		 :  max phi	      :   allowed    phi1     :	allowed phi2      :  long/short
+
+      case 49 : m_Name_string = "EIS"; limits.push_back(2); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 50 : m_Name_string = "CSS"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(-1); break;
+      case 51 : m_Name_string = "CSL"; limits.push_back(1); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 52 : m_Name_string = "BIM"; limits.push_back(5); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(8); limits.push_back(1); break;
+      default : m_Name_string = "WrongName"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100);
+      }
+  } else if (m_Type=="RPC") {
+    switch (m_Name_int) 
+      { 
+      case 2  : m_Name_string = "BML"; limits.push_back(7); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 3  : m_Name_string = "BMS"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(5); limits.push_back(8); limits.push_back(-100); limits.push_back(-1); break; 
+      case 4  : m_Name_string = "BOL"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(8); limits.push_back(-100); limits.push_back(-100); limits.push_back(1); break;
+      case 5  : m_Name_string = "BOS"; limits.push_back(6); limits.push_back(0); limits.push_back(1); limits.push_back(5); limits.push_back(8); limits.push_back(-100); limits.push_back(-1); break;  
+      case 8  : m_Name_string = "BMF"; limits.push_back(3); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;
+      case 9  : m_Name_string = "BOF"; limits.push_back(4); limits.push_back(0); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;
+      case 10 : m_Name_string = "BOG"; limits.push_back(4); limits.push_back(-100); limits.push_back(100); limits.push_back(-100); limits.push_back(6); limits.push_back(7); limits.push_back(-1); break;	 	    
+      default : m_Name_string = "WrongName"; limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); limits.push_back(-100); 
+      }
+  } else {m_Name_string = "WrongName";}
+
+  //        cout << "limits[0] limits[1] limits[2] limits[3] limits[4] limits[5] " << limits.at(0) << " " << limits.at(1) << " " <<
+  //        limits.at(2) << " " << limits.at(3) << " " << limits.at(4) << " " << limits.at(5) << endl;
+
+  //Convert ATHENA int eta to correct side and eta Hardware index
+  if (m_Eta_int>=-limits[0] && m_Eta_int<=limits[0] && m_Eta_int!=limits[1]){
+    if(m_Eta_int>0){
+      if (m_Name_int==9){//exception of the BOG,BOF alternating eta
+	m_Eta_int=(2*m_Eta_int)-1;
+      }
+      if (m_Name_int==10){//exception of the BOG,BOF alternating eta
+	m_Eta_int=2*m_Eta_int;
+      }				
+      std::stringstream eta_inttostring;
+      std::string eta_str;
+      eta_inttostring << m_Eta_int;
+      eta_inttostring >> eta_str; 
+      m_Eta_string += eta_str;
+      m_Eta_string += "A"; 
+    }
+ 
+    if (m_Eta_int<0){
+      if (m_Name_int==9){//exception of the BOG,BOF alternating eta
+	m_Eta_int=(2*m_Eta_int)+1;
+      }
+      if (m_Name_int==10){//exception of the BOG,BOF alternating eta
+	m_Eta_int=2*m_Eta_int;
+      }		
+      std::stringstream eta_inttostring;
+      std::string eta_str;
+      eta_inttostring << abs(m_Eta_int);
+      eta_inttostring >> eta_str; 
+      m_Eta_string += eta_str;
+      m_Eta_string += "C";
+ 
+    }
+	
+    if (m_Eta_int==0) {
+      std::stringstream eta_inttostring;
+      std::string eta_str;
+      eta_inttostring << m_Eta_int;
+      eta_inttostring >> eta_str; 
+      m_Eta_string += eta_str;
+      m_Eta_string += "B";	
+    } 
+  } else {m_Eta_string="WrongEta";}
+
+  //Convert ATHENA int phi to correct phi Hardware index (taking into account Long/Short case) 
+  if ((m_Phi_int>=limits[2] && m_Phi_int<=limits[3]) || m_Phi_int==limits[4] || m_Phi_int==limits[5]) // correct phi_index
+    { 
+      if (m_Type=="TGC") 
+	{
+	  std::stringstream phi_inttostring;
+	  std::string phi_str;
+	  phi_inttostring << m_Phi_int;
+	  phi_inttostring >> phi_str;
+	  m_Phi_string = phi_str ;
+	} // for m_Type ==TGC
+      else 
+	{
+	  if (limits[6]==1){//Long chamber
+	    m_Phi_int = (2*m_Phi_int)-1;
+	    std::stringstream phi_inttostring;
+	    std::string phi_str;
+	    phi_inttostring << m_Phi_int;
+	    phi_inttostring >> phi_str; 		
+	    if (m_Phi_int<10){
+	      m_Phi_string = "0"+phi_str;		
+	    } else {m_Phi_string = phi_str;}
+	  }
+
+	  if (limits[6]==-1){//Short chamber
+	    m_Phi_int = 2*m_Phi_int;
+	    std::stringstream phi_inttostring;
+	    std::string phi_str;
+	    phi_inttostring << m_Phi_int;
+	    phi_inttostring >> phi_str; 		
+	    if (m_Phi_int<10){
+	      m_Phi_string = "0"+phi_str;		
+	    } else {m_Phi_string = phi_str;}						
+	  }	
+	} // end of else ; MDT / RPC
+ 
+    } else {m_Phi_string="WrongPhi";}
+
+  //Return full Hardware name 
+  m_Name_string += m_Eta_string;
+  m_Name_string += m_Phi_string;
+  limits.clear();
+  return m_Name_string;
+
+}
+
+
+
+// //Function overload:
+// //Converting from Hardware chamber name to the ATHENA identifier int StationName,int StationEta, int StationPhi 
+//  convertChamberName(std::string HarwareChamberName , std::string HarwareChamberType , std::string ChamberType) {
+// 
+// 
+// }
+
+ 
---
+++ MuonSpectrometer/MuonValidation/MuonSimValUtils/src/MuonStrToIntName.cxx
@@ -0,0 +1,104 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "MuonSimValUtils/MuonStrToIntName.h"
+using namespace std;
+
+
+//Function converting from ATHENA identifier std::str StationName, std::string ChamberType 
+//to the corresponding ATHENA identifier int StationName
+int convertStrToIntName(std::string ChamberName, std::string ChamberType) {
+
+  //Input values
+  std::string m_Name_str = ChamberName;
+
+  //add type
+  //"MDT","CSC","TGC" go in the same loop while
+  //"RPC" go to a different loop due to the same
+  //naming with "MDT"
+  std::string m_Type=ChamberType;
+
+
+  //Set string variables
+  int m_Name_int;
+
+
+
+  if (m_Type=="MDT" || m_Type=="CSC" || m_Type=="TGC"){
+    //Switch from ATHENA int name to Hardware string name 
+    if (m_Name_str=="BIL") m_Name_int = 0; 
+    else if (m_Name_str=="BIS") m_Name_int = 1; 
+    else if (m_Name_str=="BML") m_Name_int = 2; 
+    else if (m_Name_str=="BMS") m_Name_int = 3;  
+    else if (m_Name_str=="BOL") m_Name_int = 4; 
+    else if (m_Name_str=="BOS") m_Name_int = 5; 
+    else if (m_Name_str=="BEE") m_Name_int = 6; 
+    else if (m_Name_str=="BIR") m_Name_int = 7; 
+    else if (m_Name_str=="BMF") m_Name_int = 8; 
+    else if (m_Name_str=="BOF") m_Name_int = 9; 
+    else if (m_Name_str=="BOG") m_Name_int = 10;
+    else if (m_Name_str=="BOH") m_Name_int = 11;
+    else if (m_Name_str=="EIC") m_Name_int = 12;
+    else if (m_Name_str=="EIL") m_Name_int = 13;
+    else if (m_Name_str=="EEL") m_Name_int = 14;
+    else if (m_Name_str=="EES") m_Name_int = 15;
+    else if (m_Name_str=="EMC") m_Name_int = 16;
+    else if (m_Name_str=="EML") m_Name_int = 17;
+    else if (m_Name_str=="EMS") m_Name_int = 18;
+    else if (m_Name_str=="EOC") m_Name_int = 19;
+    else if (m_Name_str=="EOL") m_Name_int = 20;
+    else if (m_Name_str=="EOS") m_Name_int = 21;
+    else if (m_Name_str=="T1C") m_Name_int = 22;
+    else if (m_Name_str=="T1L") m_Name_int = 23;
+    else if (m_Name_str=="T1S") m_Name_int = 24;
+    else if (m_Name_str=="T2C") m_Name_int = 25;
+    else if (m_Name_str=="T2L") m_Name_int = 26;
+    else if (m_Name_str=="T2S") m_Name_int = 27;
+    else if (m_Name_str=="T3C") m_Name_int = 28;
+    else if (m_Name_str=="T3L") m_Name_int = 29;
+    else if (m_Name_str=="T3S") m_Name_int = 30;
+    else if (m_Name_str=="CI1") m_Name_int = 31;
+    else if (m_Name_str=="CI2") m_Name_int = 32;
+    else if (m_Name_str=="CI3") m_Name_int = 33;
+    else if (m_Name_str=="CI4") m_Name_int = 34;
+    else if (m_Name_str=="FIL") m_Name_int = 35;
+    else if (m_Name_str=="FIS") m_Name_int = 36;
+    else if (m_Name_str=="FML") m_Name_int = 37;
+    else if (m_Name_str=="FMS") m_Name_int = 38;
+    else if (m_Name_str=="FOL") m_Name_int = 39;
+    else if (m_Name_str=="FOS") m_Name_int = 40;
+    else if (m_Name_str=="T1F") m_Name_int = 41;
+    else if (m_Name_str=="T1E") m_Name_int = 42;
+    else if (m_Name_str=="T2F") m_Name_int = 43;
+    else if (m_Name_str=="T2E") m_Name_int = 44;
+    else if (m_Name_str=="T3F") m_Name_int = 45;
+    else if (m_Name_str=="T3E") m_Name_int = 46;
+    else if (m_Name_str=="T4F") m_Name_int = 47;
+    else if (m_Name_str=="T4E") m_Name_int = 48;
+    else if (m_Name_str=="EIS") m_Name_int = 49;
+    else if (m_Name_str=="CSS") m_Name_int = 50;
+    else if (m_Name_str=="CSL") m_Name_int = 51;
+    else if (m_Name_str=="BIM") m_Name_int = 52;
+    else m_Name_int = -1;
+ 
+  } else if (m_Type=="RPC") {
+    if (m_Name_str=="BML") m_Name_int = 2; 
+    else if (m_Name_str=="BMS") m_Name_int = 3;  
+    else if (m_Name_str=="BOL") m_Name_int = 4; 
+    else if (m_Name_str=="BOS") m_Name_int = 5; 		  
+    else if (m_Name_str=="BMF") m_Name_int = 8; 
+    else if (m_Name_str=="BOF") m_Name_int = 9; 
+    else if (m_Name_str=="BOG") m_Name_int = 10;		  
+    else m_Name_int = -1;
+  } else {m_Name_int = -1;}
+
+
+
+
+
+  //Return int StationName
+
+  return m_Name_int;
+
+}
