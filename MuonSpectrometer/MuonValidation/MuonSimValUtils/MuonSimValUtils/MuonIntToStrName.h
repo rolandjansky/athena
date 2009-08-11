a1f447e8b02a697cd69de9a84b6ac79a90bd79a9
Jochen Meyer <Jochen.Meyer@cern.ch>
2009-08-11 10:17:05 +0200

    See ChangeLog File (MuonSimValUtils-00-00-07)

---
+++ MuonSpectrometer/MuonValidation/MuonSimValUtils/MuonSimValUtils/MuonIntToStrName.h
@@ -0,0 +1,16 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUONSTRTOINTNAME_H
+#define MUONSTRTOINTNAME_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+std::string convertIntToStrName(int ChamberNameInt, std::string ChamberType); 
+
+
+#endif
