d25c1bc2b449bdcabd3e3a9888bc841121418b19
Andy Buckley <a.g.buckley@gmail.com>
2010-04-07 00:34:54 +0200

    Fix HepMC -> AtlasHepMC (GenzModuleEvent-00-00-10)

---
+++ Generators/GenzModuleEvent/GenzModuleEvent/KineHepMcmap.h
@@ -0,0 +1,30 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef KINEHEPMCMAP_H
+#define KINEHEPMCMAP_H
+
+#include "HepMC/GenEvent.h"
+#include "HepMC/GenParticle.h"
+
+class KineHepMcmap
+{
+public:
+    KineHepMcmap(const HepMC::GenEvent* evt);
+	
+    int  			giveParticle_getkine	( const HepMC::GenParticle* p ) const;
+    HepMC::GenParticle*  	givekine_getParticle	( const int ikine ) const;
+private:
+    const HepMC::GenEvent*	m_evt;
+    const int	m_vertex_offset;
+    const int 	m_kine_offset;
+    const int 	m_mbias_int_kine_offset;
+    const int 	m_mbias_noint_kine_offset;
+};
+
+
+#endif  // KINEHEPMCMAP_H
+
+
+
---
+++ Generators/GenzModuleEvent/src/KineHepMcmap.cxx
@@ -0,0 +1,77 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "GenzModuleEvent/KineHepMcmap.h"
+
+KineHepMcmap::KineHepMcmap ( const HepMC::GenEvent* evt )
+    : m_evt			(evt),
+      m_vertex_offset		(-10000),
+      m_kine_offset		(100000),
+      m_mbias_int_kine_offset  	(200000),
+      m_mbias_noint_kine_offset	(300000)
+{}
+
+int
+KineHepMcmap::giveParticle_getkine	( const HepMC::GenParticle* p ) const
+{
+    int barcode	= p->barcode();
+    if (barcode < m_kine_offset)
+    {
+	if (p->status() > 1000) return p->status() / 1000;
+	return 0;
+    }
+    else
+    {
+	return barcode%m_kine_offset;
+    }
+}
+
+HepMC::GenParticle*
+KineHepMcmap::givekine_getParticle	( const int kine ) const
+{
+    if (kine <= 0) return 0;
+    HepMC::GenParticle*	part =	0;
+    bool not_found = true;
+    HepMC::GenEvent::particle_const_iterator p = m_evt->particles_begin();
+    do
+    {
+	int pkine = giveParticle_getkine(*p);
+	if (pkine == kine)
+	{
+	    not_found = false;
+	    part = *p;
+	}
+	++p;
+    } while (p != m_evt->particles_end() && not_found);
+
+    return part;
+}
+
+
+
+/////////////////////////////////////////////////////////////
+//  How to use this ?                                      //
+/////////////////////////////////////////////////////////////
+
+
+//  Providing you have GenEvent* evt
+
+//         int kine = 120;
+	 
+//	 HepMC::KineHepMcmap  kinm(evt )   ;
+//	 HepMC::GenParticle* myparticle = kinm.givekine_getParticle( kine);
+//	 int mykine = kinm.giveParticle_getkine(  myparticle);
+//	 cout << "KineHepMcmap geant particle 120 =  " << mykine << endl; 
+//	 if (myparticle) myparticle->print();
+
+
+	 
+
+	      
+
+		
+	  
+
+
+
