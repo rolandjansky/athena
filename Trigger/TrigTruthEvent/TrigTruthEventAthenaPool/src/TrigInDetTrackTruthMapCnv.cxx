77114088b056ac2739fe62aea4f119f38811fe23
Dmitry Emeliyanov <dmitry.emeliyanov@stfc.ac.uk>
2010-08-10 17:17:23 +0200

    Added TrigInDetTrackTruthMapCnv_tlp2 (TrigTruthEventAthenaPool-00-00-08)

---
+++ Trigger/TrigTruthEvent/TrigTruthEventAthenaPool/src/TrigInDetTrackTruthMapCnv.cxx
@@ -0,0 +1,81 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "TrigInDetTrackTruthMapCnv.h"
+#include "RootConversions/TConverterRegistry.h"
+#include "TrigTruthEventTPCnv/TrigInDetTrackTruthMap_old_cnv.h"
+#include "TROOT.h"
+#include <iostream>
+
+//constructor
+TrigInDetTrackTruthMapCnv::TrigInDetTrackTruthMapCnv( ISvcLocator* svcloc ) :
+  TrigInDetTrackTruthMapCnvBase( svcloc ) {
+}
+
+//destructor
+TrigInDetTrackTruthMapCnv::~TrigInDetTrackTruthMapCnv() {}
+
+
+//create persistent
+TrigInDetTrackTruthMap_PERS* TrigInDetTrackTruthMapCnv::createPersistent(TrigInDetTrackTruthMap* transObj) {
+    MsgStream mlog(messageService(), "TrigInDetTrackTruthMapConverter" );
+
+    mlog << MSG::DEBUG << "TrigInDetTrackTruthMapCnv::createPersistent" << endreq;
+
+    TrigInDetTrackTruthMap_PERS* persObj = m_trigInDetTrackTruthMapCnv_tlp2.createPersistent( transObj, mlog );
+
+    return persObj;
+}
+
+//create transient
+TrigInDetTrackTruthMap* TrigInDetTrackTruthMapCnv::createTransient() {
+  
+  MsgStream mlog(messageService(), "TrigInDetTrackTruthMapConverter" );
+  mlog << MSG::DEBUG << "TrigInDetTrackTruthMapCnv::createTransient " << endreq;
+  
+  static pool::Guid tlp1_guid("08892FEB-5706-4938-9226-F45C0AA662E7");
+  static pool::Guid tlp2_guid("02074F47-F290-4A48-B503-4DCAB4181B3D");
+  static pool::Guid p0_guid("41581666-F06D-44AE-93B9-D7E912A27AA1");
+  
+  TrigInDetTrackTruthMap  *transObj = 0;
+  
+  
+  if( compareClassGuid(tlp2_guid) ) {
+    
+    mlog << MSG::DEBUG << "TrigInDetTrackTruthMapCnv::reading tlp2 persistent object" << endreq;
+    poolReadObject< TrigInDetTrackTruthMap_PERS >( m_trigInDetTrackTruthMapCnv_tlp2 );
+    transObj = m_trigInDetTrackTruthMapCnv_tlp2.createTransient( mlog );
+    
+  }
+  else if( compareClassGuid(tlp1_guid) ) {    
+    mlog << MSG::DEBUG << "TrigInDetTrackTruthMapCnv::reading tlp1 persistent object" << endreq;
+    TrigInDetTrackTruthMapCnv_tlp1  m_tlp1_Converter;
+    poolReadObject< TrigInDetTrackTruthMap_tlp1 >(m_tlp1_Converter);
+    transObj = m_tlp1_Converter.createTransient( mlog );
+  }
+  else if( compareClassGuid(p0_guid) ) {
+    
+    mlog << MSG::DEBUG << "TrigInDetTrackTruthMapCnv::reading p0 persistent object" << endreq;
+    // old version from before TP separation, just return it
+    transObj = this->poolReadObject<TrigInDetTrackTruthMap>();
+  }  
+  else {
+    throw std::runtime_error("Unsupported persistent version of TrigInDetTrackTruthMap");
+  }
+  
+  return transObj;
+}
+
+
+StatusCode TrigInDetTrackTruthMapCnv::initialize()
+{
+  static bool did_rootcnv = false;
+  if (!did_rootcnv) {
+    did_rootcnv = true;
+    gROOT->GetClass("TrigInDetTrackTruthMap_old");
+    static TrigInDetTrackTruthMap_old_cnv cnv;
+    TConverterRegistry::Instance()->AddConverter (&cnv);
+  }
+  return TrigInDetTrackTruthMapCnvBase::initialize();
+}
---
+++ Trigger/TrigTruthEvent/TrigTruthEventAthenaPool/src/TrigInDetTrackTruthMapCnv.h
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**********************************************************************************
+ * @Project: Trigger
+ * @Package: TrigEventAthenaPool
+ * @Class  : TrigInDetTrackTruthMapCnv
+ *
+ * @brief transient persistent converter for TrigInDetTrackTruthMapCnv
+ *
+ * @author Andrew Hamilton  <Andrew.Hamilton@cern.ch>  - U. Geneva
+ * @author Francesca Bucci  <f.bucci@cern.ch>          - U. Geneva
+ *
+ * File and Version Information:
+ * $Id: TrigInDetTrackTruthMapCnv.h,v 1.2 2008-08-19 04:27:37 ssnyder Exp $
+ **********************************************************************************/
+#ifndef TRIGTRUTHEVENTATHENAPOOL_TRIGINDETTRACKTRUTHMAPCNV_H
+#define TRIGTRUTHEVENTATHENAPOOL_TRIGINDETTRACKTRUTHMAPCNV_H
+
+#include "TrigInDetTruthEvent/TrigInDetTrackTruthMap.h"
+#include "TrigTruthEventTPCnv/TrigInDetTrackTruthMap_tlp1.h"
+#include "TrigTruthEventTPCnv/TrigInDetTrackTruthMapCnv_tlp1.h"
+#include "TrigTruthEventTPCnv/TrigInDetTrackTruthMap_tlp2.h"
+#include "TrigTruthEventTPCnv/TrigInDetTrackTruthMapCnv_tlp2.h"
+
+#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+
+//typedef to the latest top level persistent class
+typedef TrigInDetTrackTruthMap_tlp2  TrigInDetTrackTruthMap_PERS;
+
+typedef T_AthenaPoolCustomCnv<TrigInDetTrackTruthMap, TrigInDetTrackTruthMap_PERS > TrigInDetTrackTruthMapCnvBase; 
+
+class MsgStream;
+
+class TrigInDetTrackTruthMapCnv : public TrigInDetTrackTruthMapCnvBase
+{
+
+  friend class CnvFactory<TrigInDetTrackTruthMapCnv>;
+
+  virtual StatusCode initialize();
+
+ protected:
+
+  TrigInDetTrackTruthMapCnv(ISvcLocator* svcloc);
+  ~TrigInDetTrackTruthMapCnv();
+
+  virtual TrigInDetTrackTruthMap_PERS*  createPersistent(TrigInDetTrackTruthMap* transObj);
+  virtual TrigInDetTrackTruthMap*       createTransient ();
+
+ private:
+
+  TrigInDetTrackTruthMapCnv_tlp2 m_trigInDetTrackTruthMapCnv_tlp2;
+
+
+};
+
+
+#endif
