25bb95f9d03afd3b325fb9e537c58c2807450dcc
George Herbert Lewis <george.herbert.lewis@cern.ch>
2010-08-23 12:31:35 +0200

    Updated Python Configuration (TrigPileUpHypo-00-00-07)
    Former-commit-id: de7eb9ba21357f7875e74bd36314f35e2e39a561

---
+++ Trigger/TrigHypothesis/TrigPileUpHypo/src/components/TrigPileUpHypo_entries.cxx
@@ -0,0 +1,9 @@
+#include "TrigPileUpHypo/TrigPileUpHypo.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( TrigPileUpHypo )
+
+DECLARE_FACTORY_ENTRIES( TrigPileUpHypo ) {
+  DECLARE_ALGORITHM( TrigPileUpHypo )
+}
+
