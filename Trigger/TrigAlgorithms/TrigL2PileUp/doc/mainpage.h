d700688d129ce5cd87ccd434820811ebd08bf053
George Herbert Lewis <george.herbert.lewis@cern.ch>
2011-03-18 14:56:35 +0100

    Fixed Warnings in hltExecute (TrigL2PileUp-00-00-09)
    Former-commit-id: 816ba66ae130071005fa2e873f079cb0847c460f

---
+++ Trigger/TrigAlgorithms/TrigL2PileUp/doc/mainpage.h
@@ -0,0 +1,24 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+
+
+@mainpage TrigGenericAlgs Package
+
+@section TrigGenericAlgsIntro Introduction
+
+This package contains some algorithms which are detector/slice
+independent or are plceholders to overcome steering contraints/limitations.
+
+
+Pcakage contains: 
+ - DummyFEX : used as a plceholder for sequences having no FEX
+ - DummyUnseededAllTEAlgo : used sometimes to make seeding fro nomale fexes
+ - OverlapRemoval : for removing RoIs overlaps
+
+@htmlinclude used_packages.html 
+
+
+*/
---
+++ Trigger/TrigAlgorithms/TrigL2PileUp/src/components/TrigL2PileUp_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "TrigL2PileUp/TrigL2PileUp.h"
+
+
+DECLARE_ALGORITHM_FACTORY( TrigL2PileUp )
+
+DECLARE_FACTORY_ENTRIES( TrigL2PileUp ) {
+    DECLARE_ALGORITHM( TrigL2PileUp )
+}
