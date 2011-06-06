8ee7c440f1e9d9405bde6294af506a48262691ad
Andrea Abba <andrea.abba@cern.ch>
2011-06-06 20:44:09 +0200

    Add a missing #include. (OverlayAlgBase-00-01-05)

---
+++ Event/EventOverlay/OverlayAlgBase/OverlayAlgBase/OverlayAlgBase.h
@@ -0,0 +1,63 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Dear emacs, this is -*-c++-*-
+
+/**
+ * @file
+ *
+ * Code to set up access to two StoreGate-s for overlaying algorithms.
+ * Factored out from InDetOverlay.
+ *
+ * @author Andrei Gaponenko, 2006-2009
+ */
+
+#ifndef OVERLAYALGBASE_H
+#define OVERLAYALGBASE_H
+
+#include <string>
+
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "AthenaBaseComps/AthMessaging.h"
+
+class StoreGateSvc;
+
+/** 
+ * This class sets up acces to the two event stores and makes sure
+ * they are syncronized.  It also handles McEventCollection.
+ * 
+ * Algorithms inheriting from OverlayAlgBase should not redefine
+ * initialize(), execute() and finalize() but provide
+ * overlayInitialize() etc. instead.
+ */
+class OverlayAlgBase : public Algorithm,
+		       public AthMessaging
+{
+public:
+  
+  OverlayAlgBase(const std::string &name,ISvcLocator *pSvcLocator);
+  
+  virtual StatusCode initialize();
+  virtual StatusCode execute();
+  virtual StatusCode finalize();
+
+  //----------------
+  virtual StatusCode overlayInitialize() = 0;
+  virtual StatusCode overlayExecute() = 0;
+  virtual StatusCode overlayFinalize() = 0;
+
+protected:
+  // ----------------------------------------------------------------
+  ServiceHandle<StoreGateSvc> m_storeGateData;
+  ServiceHandle<StoreGateSvc> m_storeGateMC;
+  ServiceHandle<StoreGateSvc> m_storeGateOutput;
+  
+  template<class TypeToBeRemoved> void removeAllObjectsOfType(StoreGateSvc *sg);
+  template<class TypeToBeCopied> void copyAllObjectsOfType(StoreGateSvc *to, StoreGateSvc *from);
+};
+
+#include "OverlayAlgBase/OverlayAlgBase.icc"
+
+#endif/*OVERLAYALGBASE_H*/
---
+++ Event/EventOverlay/OverlayAlgBase/src/OverlayAlgBase.cxx
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Andrei Gaponenko, 2006-2009
+
+#include "OverlayAlgBase/OverlayAlgBase.h"
+#include "AthenaBaseComps/AthMsgStreamMacros.h"
+#include "StoreGate/StoreGateSvc.h"
+
+//================================================================
+OverlayAlgBase::OverlayAlgBase(const std::string &name, ISvcLocator *pSvcLocator) :
+  Algorithm(name, pSvcLocator),
+  AthMessaging( msgSvc(), name),
+  m_storeGateData("StoreGateSvc/OriginalEvent_SG", name),
+  m_storeGateMC("StoreGateSvc/BkgEvent_0_SG", name),
+  m_storeGateOutput("StoreGateSvc/StoreGateSvc", name)
+{
+  declareProperty("DataStore", m_storeGateData, "help");
+  declareProperty("MCStore", m_storeGateMC, "help");
+  declareProperty("OutputStore", m_storeGateOutput, "help");
+}
+
+//================================================================
+StatusCode OverlayAlgBase::initialize()
+{
+  ATH_MSG_DEBUG("OverlayAlgBase::initialize()");
+
+  if (m_storeGateData.retrieve().isFailure()) {
+    ATH_MSG_FATAL("OverlayAlgBase::initialize): StoreGate[data] service not found !");
+    return StatusCode::FAILURE;
+  } 
+  
+  if (m_storeGateMC.retrieve().isFailure()) {
+    ATH_MSG_FATAL("OverlayAlgBase::initialize): StoreGate[MC] service not found !");
+    return StatusCode::FAILURE;
+  }
+
+  if (m_storeGateOutput.retrieve().isFailure()) {
+    ATH_MSG_FATAL("OverlayAlgBase::initialize): StoreGate[output] service not found !");
+    return StatusCode::FAILURE;
+  } 
+
+  // Call initialization() of a derived class.
+  return overlayInitialize();
+}
+
+//================================================================
+StatusCode OverlayAlgBase::execute() {
+  ATH_MSG_DEBUG("OverlayAlgBase::execute() begin. Calling derived class execute()");
+  StatusCode ret = overlayExecute();
+  ATH_MSG_DEBUG("OverlayAlgBase::execute() end");
+  return ret;
+}
+
+//================================================================
+StatusCode OverlayAlgBase::finalize() {
+  return overlayFinalize();
+}
+
+//================================================================
+//EOF
