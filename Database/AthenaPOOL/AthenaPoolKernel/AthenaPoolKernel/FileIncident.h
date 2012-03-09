eef6ccd5ece7e0370e918dde3d5b238ef40b6011
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2012-03-09 17:41:24 +0100

    Trying to persuade last clients to change their includes (AthenaPoolKernel-00-02-00)

---
+++ Database/AthenaPOOL/AthenaPoolKernel/AthenaPoolKernel/FileIncident.h
@@ -0,0 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//#include "GaudiKernel/FileIncident.h"
+
+#pragma Use_FileIncident_from_GaudiKernel
---
+++ Database/AthenaPOOL/AthenaPoolKernel/AthenaPoolKernel/IMetaDataTool.h
@@ -0,0 +1,32 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef ATHENAPOOLKERNEL_IMETADATATOOL_H
+#define ATHENAPOOLKERNEL_IMETADATATOOL_H
+
+/** @file IMetaDataTool.h
+ *  @brief This file contains the class definition for the IMetaDataTool class.
+ *  @author Peter van Gemmeren <gemmeren@anl.gov>
+ *  $Id: IMetaDataTool.h,v 1.2 2007-07-30 19:06:50 gemmeren Exp $
+ **/
+
+#include "GaudiKernel/IAlgTool.h"
+
+/** @class IMetaDataTool
+ *  @brief This class provides the interface for MetaDataTools.
+ **/
+class IMetaDataTool : virtual public IAlgTool {
+
+public: // Non-static members
+   /// Gaudi boilerplate
+   static const InterfaceID& interfaceID();
+private: // Data
+};
+
+inline const InterfaceID& IMetaDataTool::interfaceID() {
+   static const InterfaceID _IID("IMetaDataTool", 1, 0);
+   return(_IID);
+}
+
+#endif
