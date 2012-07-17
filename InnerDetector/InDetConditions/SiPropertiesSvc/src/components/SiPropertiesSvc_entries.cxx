2a4b29b94922b172b057d234e3e575114a31864c
Fares Djama <djama@cppm.in2p3.fr>
2012-07-17 16:24:19 +0200

    CLHEP updates (SiPropertiesSvc-00-00-08)

---
+++ InnerDetector/InDetConditions/SiPropertiesSvc/src/components/SiPropertiesSvc_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "SiPropertiesSvc/SiPropertiesSvc.h"
+
+DECLARE_SERVICE_FACTORY(SiPropertiesSvc)
+
+DECLARE_FACTORY_ENTRIES(SiPropertiesSvc) {
+  DECLARE_SERVICE( SiPropertiesSvc)
+}
+
