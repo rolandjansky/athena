f23d78e3387c674dea5a2b12a82b7cd9964d36f5
Andreas Schaelicke <aschaeli@cern.ch>
2012-11-16 07:35:17 +0100

    updates for new CLHEP and G4 version (LArG4FCAL-00-00-43)

---
+++ LArCalorimeter/LArG4/LArG4FCAL/src/LArFCALCalculatorBase.cc
@@ -0,0 +1,168 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//-----------------------------------------------------------------------------
+//
+// Base class describing common behaviour in FCal G4 calculator.
+//
+//----------------------------------------------------------------------------
+
+#include "LArG4FCAL/LArFCALCalculatorBase.h"
+
+#include "LArG4Code/LArG4Identifier.h"
+#include "LArG4Code/LArG4BirksLaw.h"
+#include "StoreGate/StoreGate.h"
+#include "LArG4RunControl/LArG4FCALOptions.h"
+// Geant4 includes
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4ThreeVector.hh"
+#include "G4StepPoint.hh"
+#include "G4Step.hh"
+#include "globals.hh"
+
+#include "LArReadoutGeometry/LArDetectorManager.h"
+#include "LArReadoutGeometry/FCALDetectorManager.h"
+#include "LArReadoutGeometry/FCALModule.h"
+#include "LArReadoutGeometry/FCALTile.h"
+#include "LArReadoutGeometry/FCALTube.h"
+
+//
+// constructor
+//
+LArFCALCalculatorBase::LArFCALCalculatorBase()
+  : m_OOTcut(2.5*CLHEP::ns),m_posModule(NULL),m_negModule(NULL), m_FCalSampling(0), birksLaw(NULL)
+{
+  m_identifier = LArG4Identifier();
+
+  m_time = 0.;
+  m_energy = 0.;
+  m_isInTime = false;
+
+
+  LArG4FCALOptions *fcalOptions=NULL;
+  StoreGateSvc *detStore = StoreGate::pointer("DetectorStore");
+  if (detStore->retrieve(m_ChannelMap)==StatusCode::FAILURE) {
+  }
+
+  if (detStore->retrieve(fcalOptions, "LArG4FCALOptions")==StatusCode::FAILURE) {
+  }
+
+  if (fcalOptions) {
+    if (fcalOptions->FCALBirksLaw()) {
+      const double Birks_LAr_density = 1.396;
+      const double Birks_k = fcalOptions->FCALBirksk();
+      birksLaw = new LArG4BirksLaw(Birks_LAr_density,Birks_k);
+    }
+    
+    if (fcalOptions->FCALHVEnable()) {
+      const LArDetectorManager *manager=NULL;
+      if (detStore->retrieve(manager)!=StatusCode::SUCCESS) {
+	throw std::runtime_error("Cannot locate FCAL Manager");
+      }
+      else {
+	const FCALDetectorManager* fcalManager=manager->getFcalManager();
+	m_posModule = fcalManager->getFCAL(FCALModule::Module(m_FCalSampling),FCALModule::POS);
+	m_negModule = fcalManager->getFCAL(FCALModule::Module(m_FCalSampling),FCALModule::NEG);
+      }
+    }
+  }
+}
+
+//
+// destructor
+//
+LArFCALCalculatorBase::~LArFCALCalculatorBase()
+{ 
+  delete birksLaw;
+}
+
+
+
+G4bool LArFCALCalculatorBase::Process(const G4Step* a_step)
+{
+  // Given a G4Step, determine the cell identifier.
+
+  // 29-Mar-2002 WGS: this method now returns a boolean.  If it's
+  // true, the hit is valid; if it's false, there was some problem
+  // with the hit and it should be ignored.
+
+  // First, get the energy.
+  m_energy = a_step->GetTotalEnergyDeposit();
+
+  G4double stepLengthCm = a_step->GetStepLength() / CLHEP::cm;
+  if(birksLaw)  m_energy = (*birksLaw)(m_energy, stepLengthCm, 10);
+
+  // Find out how long it took the energy to get here.
+  G4StepPoint* pre_step_point = a_step->GetPreStepPoint();
+  G4StepPoint* post_step_point = a_step->GetPostStepPoint();
+  G4double timeOfFlight = (pre_step_point->GetGlobalTime() +
+                           post_step_point->GetGlobalTime()) * 0.5;
+  G4ThreeVector startPoint = pre_step_point->GetPosition();
+  G4ThreeVector endPoint   = post_step_point->GetPosition();
+  G4ThreeVector p = (startPoint + endPoint) * 0.5;
+
+
+  // Determine if the hit was in-time.
+  m_time = timeOfFlight/CLHEP::ns - p.mag()/CLHEP::c_light/CLHEP::ns;
+  if (m_time > m_OOTcut)
+    m_isInTime = false;
+  else
+    m_isInTime = true;
+
+  // zSide is negative if z<0.
+  G4int zSide = 2;
+  if (p.z() < 0.) zSide = -zSide;
+
+ 
+  // Get the physical volume associated with this G4Step.
+  G4VPhysicalVolume* thisPV = a_step->GetPreStepPoint()->GetPhysicalVolume();   //a_step->GetTrack()->GetVolume();
+  G4int sampling = m_FCalSampling;
+  FCAL_ChannelMap::tubemap_const_iterator t= m_ChannelMap->getTubeByCopyNumber(sampling, thisPV->GetCopyNo());
+  FCAL_ChannelMap::tileName_t tileName = (*t).second.get_tileName();
+
+  unsigned int phiIndex =  tileName & 0x0000FFFF;
+  unsigned int etaIndex = (tileName & 0xFFFF0000) >> 16;
+
+  // Apply high voltage here:
+  {
+    const FCALModule *fcalMod = zSide<0 ? m_negModule : m_posModule;
+    if (fcalMod) { // Then apply high voltage:
+
+      bool tubeFound(false);
+
+      const FCALTile *tile = fcalMod->getTile(phiIndex, etaIndex);
+      for (unsigned int i=0;i<tile->getNumTubes();i++) {
+	FCALTubeConstLink tube=tile->getTube(i);
+	if (tube->getXLocal() == (*t).second.x() && tube->getYLocal()==(*t).second.y()) {
+	  FCALHVLineConstLink line =tube->getHVLine();
+	  if (line) {
+	    double voltage = line->voltage();
+	    //double current = line->current();
+	    bool   hvOn    = line->hvOn();
+	    if (!hvOn) m_energy=0.0;
+	    m_energy *= pow((voltage)/2000.0,0.6);
+	    tubeFound=true;
+	    break;
+	  }
+	}
+      }
+      if (!tubeFound) 	throw std::runtime_error("FCAL Tube not found (for HV calculation)");
+
+    }
+    
+  }
+  m_identifier.clear();
+
+  // Append the values to the empty identifier.
+  m_identifier << 4          // LArCalorimeter
+	       << 3          // LArFCAL
+	       << zSide      // EndCap	      
+	       << sampling   // FCal Module #
+	       << etaIndex
+	       << phiIndex;
+
+  return true;
+}
+
---
+++ LArCalorimeter/LArG4/LArG4FCAL/src/LArFCALCalibCalculatorBase.cc
@@ -0,0 +1,302 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+//-----------------------------------------------------------------------------
+//
+// Base class describing common behaviour in FCal G4 calculator.
+//
+//----------------------------------------------------------------------------
+
+// ATLAS LAr includes
+#include "LArG4FCAL/LArFCALCalibCalculatorBase.h"
+#include "LArG4FCAL/LArFCALCalculatorBase.h"
+
+#include "LArG4Code/LArG4Identifier.h"
+//#include "LArG4Code/SimulationEnergies.h"
+#include "CaloG4Sim/SimulationEnergies.h"
+
+// For reading the data files in Athena.
+#ifndef LARG4_STAND_ALONE
+#include "PathResolver/PathResolver.h"
+#endif
+
+// Geant4 includes
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4ThreeVector.hh"
+#include "G4StepPoint.hh"
+#include "G4Step.hh"
+#include "globals.hh"
+#include "G4TransportationManager.hh"
+
+#include <cstdlib>
+#include <fstream>
+
+#include <algorithm>
+
+#include "GeoModelInterfaces/IGeoModelSvc.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/Bootstrap.h"
+#include "RDBAccessSvc/IRDBAccessSvc.h"
+#include "RDBAccessSvc/IRDBRecordset.h"
+#include "RDBAccessSvc/IRDBRecord.h"
+
+// 03-Jan-2002 WGS: For 'copysign'.
+#include <cmath>
+#include <limits.h>
+#include "StoreGate/StoreGate.h"
+#undef DEBUG_FCAL_IO
+// Forward declaractions:
+class G4Step;
+
+// Forward declaration for namespace CaloG4.
+namespace CaloG4 {
+  class SimulationEnergies;
+}
+
+// constructor
+//
+namespace LArG4 {
+
+  namespace FCAL {
+
+
+    LArFCALCalibCalculatorBase::LArFCALCalibCalculatorBase()
+      : m_deltaX(0), m_deltaY(0), m_FCalSampling(0), m_identifier(LArG4Identifier()), m_OOTcut(0), m_zShift(0.),
+	m_time(0), m_isInTime(0)
+    { 	
+      // Initialize the energy calculators.
+      //      m_electrode.GetElectrodes();
+      ////     m_ElectrodeFile = "FCal1-electrodes.sorted.dat";
+      ////m_deltaX=7.5;
+      ////m_deltaY=7.5*sin(60.0);
+
+      StoreGateSvc *detStore = StoreGate::pointer("DetectorStore");
+      if (detStore->retrieve(m_ChannelMap)==StatusCode::FAILURE) 
+	throw std::runtime_error ("LArFCALCalibCalculatorBase ERROR: Cannot retrieve FCAL Channel Map!");
+
+      // get GeoModelSvc & RDBAccessSvc
+      ISvcLocator *svcLocator = Gaudi::svcLocator();
+      IGeoModelSvc *geoModel(0);
+      StatusCode status = svcLocator->service ("GeoModelSvc",geoModel);
+      if (status != StatusCode::SUCCESS) 
+	throw std::runtime_error ("LArFCALCalibCalculatorBase ERROR: Cannot locate GeoModelSvc!");
+   
+      // Access the geometry database:
+      IRDBAccessSvc *pAccessSvc(0);
+      status=svcLocator->service("RDBAccessSvc",pAccessSvc);
+      if (status != StatusCode::SUCCESS) 
+	throw std::runtime_error ("LArFCALCalibCalculatorBase ERROR: Cannot locate RDBAccessSvc!");
+
+      // Obtain the geometry version information:
+      
+      std::string AtlasVersion = geoModel->atlasVersion();
+      std::string LArVersion = geoModel->LAr_VersionOverride();
+
+      std::string detectorKey  = LArVersion.empty() ? AtlasVersion : LArVersion;
+      std::string detectorNode = LArVersion.empty() ? "ATLAS" : "LAr";
+
+      pAccessSvc->connect();
+      IRDBRecordset_ptr emecGeoPtr = pAccessSvc->getRecordsetPtr("EmecGeometry",detectorKey,detectorNode);
+      if (emecGeoPtr->size()==0) 
+	throw std::runtime_error ("LArFCALCalibCalculatorBase ERROR: Cannot find the EmecGeometry Table");
+ 
+      m_zShift = (*emecGeoPtr)[0]->getDouble("ZSHIFT")*CLHEP::cm;
+      pAccessSvc->disconnect();
+    }
+
+//
+// destructor
+//
+    LArFCALCalibCalculatorBase::~LArFCALCalibCalculatorBase()       
+    {
+    }
+    
+    
+    G4bool LArFCALCalibCalculatorBase::Process(const G4Step* a_step, 
+      				       const eCalculatorProcessing a_process) 
+    {
+      // First, get the energy.
+
+      m_energies.clear();
+      if ( a_process == kEnergyAndID  ||  a_process == kOnlyEnergy )
+        {
+          m_energyCalculator.Energies( a_step, m_energies );
+        }
+      else
+        for (unsigned int i=0; i != 4; i++) m_energies.push_back( 0. );
+
+      // find the space point for this deposit
+      G4StepPoint* pre_step_point = a_step->GetPreStepPoint();
+      G4StepPoint* post_step_point = a_step->GetPostStepPoint();
+      G4ThreeVector startPoint = pre_step_point->GetPosition();
+      G4ThreeVector endPoint   = post_step_point->GetPosition();
+      G4ThreeVector p = (startPoint + endPoint) * 0.5;
+
+      // S.M.: this does not work - there are some volumes inside the
+      // FCAL which have their own local coordinate system and
+      // just result in (~0,~0) local coordinates. I think they should
+      // be treated as absorbers ... so I take the original coordinates
+      // instead ...
+
+      // S.M.      // get LOCAL (u,v) coordinates!
+      // S.M.      G4Navigator* theNavigator = 
+      // S.M.	   G4TransportationManager::GetTransportationManager()->
+      // S.M.	   GetNavigatorForTracking();
+      // S.M.      G4ThreeVector theLocalPoint = theNavigator->
+      // S.M.	     GetGlobalToLocalTransform().
+      // S.M.	   TransformPoint(p);
+      
+
+      G4ThreeVector theLocalPoint(p);
+
+      // we've shifted the endcap, we have to project the geometry to a
+      // different point).
+      G4double pointZ = p.z();
+  
+      G4double pointZshift = pointZ - copysign(m_zShift,pointZ);
+  
+      G4ThreeVector pointShift(p.x(),
+                               p.y(),
+                               pointZshift);
+
+      int etaIndex,phiIndex;
+      float x = theLocalPoint.x(),y=theLocalPoint.y();
+      int sampling = m_FCalSampling;
+      bool ok = m_ChannelMap->getTileID(sampling,
+					x,
+					y,
+					etaIndex,
+					phiIndex);
+
+      
+      // zSide is negative if z<0.
+      G4int zSide = 2;
+      if (pointZshift < 0.) zSide = -zSide;
+  
+      m_identifier.clear();
+      if (ok) { 
+	// we have a active or non-active hit inside the FCal
+	G4int sampling = m_FCalSampling;
+	
+
+	// Append the values to the empty identifier.
+	m_identifier << 4          // LArCalorimeter
+		     << 3          // LArFCAL
+		     << zSide      // EndCap
+		     << sampling   // FCal Module # 
+		     << etaIndex
+		     << phiIndex;  
+
+//	std::cout << "LArG4FCAL/LArFCALCalibCalculatorBase: 4/3/"
+//		  <<zSide<<"/"
+//		  <<sampling<<"/"
+//		  <<etaIndex<<"/"
+//		  <<phiIndex<<": "
+//		  << m_energies[0] << ", " 
+//		  << m_energies[1] << ", " 
+//		  << m_energies[2] << ", " 
+//		  << m_energies[3] 
+//		  << ", i=" << i   
+//		  << ", j=" << j   
+//		  << ", l.x=" << theLocalPoint.x()
+//		  << ", l.y=" << theLocalPoint.y()
+//		  << ", p.x=" << p.x()
+//		  << ", p.y=" << p.y()
+//		  << ", deltaX=" << m_deltaX
+//		  << ", deltaY=" << m_deltaY
+//		  << std::endl;
+      }
+      else {
+	// S.M.: we have a hit which fails the electrode identifier
+	// calculation.  It's likely just a the border of the FCAL
+	// (outer or inner) and not really dead material ... Lacking a
+	// better method of assigning it to a correct cell I make a
+	// dead material identifier for it ...
+	G4int sampling(3); // FCAL leakage
+	G4int type(1); // FCAL leakage
+	G4double eta = fabs( pointShift.pseudoRapidity() );
+	G4double phi = pointShift.phi();
+        G4double rho = pointShift.perp();
+        // since the outer edge of the FCal has a irregular shape we
+        // take the innermost position of the outermost tubes as min
+        // radius - this is about 3 cm from the outermost tube
+        // position ...
+        const G4double fcal_edge_approx = 415.0*CLHEP::mm;
+	G4int subdet; // LAr
+	if ( pointZshift < 0. ) 
+	  subdet = -4;
+	else
+	  subdet = 4;
+	if (phi < 0) phi += 2*M_PI;
+	G4int etaIndex;
+	G4int phiIndex;
+	G4int region; 
+	if ( eta < 5 ) {
+        if(rho > fcal_edge_approx) {
+             type = 2;
+	     sampling = m_FCalSampling;
+             region = 4;
+	     etaIndex = 0;
+	     phiIndex = G4int(32*phi/M_PI);
+        } else {
+	     region = 0;
+	     etaIndex = G4int((eta-1.7)/0.1);
+	     phiIndex = G4int(32*phi/M_PI);        
+        }
+	}
+	else if ( eta < 8 ) {
+	  region = 1;
+	  etaIndex = G4int((eta-5)/0.2);
+	  phiIndex = G4int(32*phi/M_PI);
+	}
+	else {
+	  region = 2;
+	  etaIndex = 0;
+	  phiIndex = 0;
+	}
+	
+	// Append the values to the empty identifier.
+	m_identifier << 10          // Calorimeter Dead Material
+		     << subdet      // LAr
+		     << type
+		     << sampling  
+		     << region
+		     << etaIndex
+		     << phiIndex;  
+
+//	std::cout << "LArG4FCAL/LArFCALCalibCalculatorBase: 10/4/"
+//		  <<type<<"/"
+//		  <<sampling<<"/"
+//		  <<region<<"/"
+//		  <<etaIndex<<"/"
+//		  <<phiIndex<<": "
+//		  << m_energies[0] << ", " 
+//		  << m_energies[1] << ", " 
+//		  << m_energies[2] << ", " 
+//		  << m_energies[3] 
+//		  << ", i=" << i   
+//		  << ", j=" << j   
+//		  << ", l.x=" << theLocalPoint.x()
+//		  << ", l.y=" << theLocalPoint.y()
+//		  << ", p.x=" << p.x()
+//		  << ", p.y=" << p.y()
+//		  << ", deltaX=" << m_deltaX
+//		  << ", deltaY=" << m_deltaY
+//		  << std::endl;
+      }
+      
+      return true;
+    }
+
+  } // namespace FCAL
+  
+} // namespace LArG4
+
+
+
+
+
+
