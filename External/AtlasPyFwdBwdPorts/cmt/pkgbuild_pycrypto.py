ec6dfb02c8aeeb61e8d087c48819064f20db3273
Scott Raymond Mandry <scott.raymond.mandry@cern.ch>
2013-05-07 17:57:21 +0200

    add --without-libyaml to pkgbuild_pyyaml.py to fix compilation warning (AtlasPyFwdBwdPorts-00-02-25)
    Former-commit-id: cdb88e9ffd62fef3bfc264c356e10a06373aacbe

--- External/AtlasPyFwdBwdPorts/cmt/pkgbuild_pycrypto.py
+++ External/AtlasPyFwdBwdPorts/cmt/pkgbuild_pycrypto.py
@@ -6,13 +6,11 @@ import os
 import sys
 
 pkg_name= "pycrypto"
-pkg_ver = "2.6.1"
+pkg_ver = "2.1.0"
 
 dist_configure = configure
 def configure(self):
     env = self.env
     # there are C++-style comments in various C files...
     env['CFLAGS'] = env['CFLAGS'].replace('-ansi', '-std=c99')
-    if env['CC'].find ('clang')>=0 or env['CC'].find ('icc')>=0:
-        env['CFLAGS'] = env['CFLAGS'] + ' -DLTC_NO_ASM '
     return dist_configure(self)
