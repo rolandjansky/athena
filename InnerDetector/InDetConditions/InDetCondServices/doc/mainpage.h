2563bac4f3f51dfa4923a140453a56ae9e3543ed
Nicholas Styles <nicholas.styles@desy.de>
2013-07-04 11:29:13 +0200

    bug fix (InDetCondServices-01-00-01)

---
+++ InnerDetector/InDetConditions/InDetCondServices/doc/mainpage.h
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage InDetCondServices package
+
+@section InDetCondServicesIntroduction Introduction
+
+This package provides interfaces to conditions services.
+
+*/
