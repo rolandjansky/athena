35cf6ec529dd71c295c7beeee40e9ebcc51492e9
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2013-07-18 15:47:02 +0200

    LUCID_Digitization.h, LUCID_PileUpTool.h, LUCID_DigitizationToolBox.h - move to src directory.
    * src/LUCID_Digitization.h/.cxx - convert to skeleton algorthim -
    all digitization now done in LUCID_PileUpTool via
    processAllSubEvents method.
    
    * src/LUCID_PileUpTool.h - add processAllSubEvents method and
    PileUpMergeSvc service handle.
    
    * src/LUCID_PileUpTool.cxx - add processAllSubEvents method based
    on former LUCID_DigiTop::execute method. Further code sharing may
    be possible.
    
    * src/components/LUCID_Digitization_entries.cxx - update location
    of header files.
    
    * python/LUCID_DigitizationConfig.py,
    python/LUCID_DigitizationConfigDb.py - remove separate confGetter
    method for LUCID_DigiTop and update confGetter method for
    LUCID_PileUpTool.
    
    * cmt/requirements - use statements from public to
    private as necessary.
    
    * tagged as LUCID_Digitization-00-09-00 (LUCID_Digitization-00-09-00)
    
    
    Former-commit-id: 11d1e4a70dbe540d8ee645d7b4fe134a8f9e400e

--- ForwardDetectors/LUCID/LUCID_Digitization/src/LUCID_PileUpTool.h
+++ ForwardDetectors/LUCID/LUCID_Digitization/src/LUCID_PileUpTool.h
@@ -6,17 +6,12 @@
 #define LUCID_DIGITIZATION_TOOL_H
 
 #include "PileUpTools/PileUpToolBase.h"
-
 #include "LUCID_DigitizationToolBox.h"
-
-#include "LUCID_SimEvent/LUCID_SimHitCollection.h"
-#include "LUCID_RawEvent/LUCID_DigitContainer.h"
-
-#include "xAODEventInfo/EventInfo.h"   // SubEventIterator
-#include "xAODEventInfo/EventAuxInfo.h"// SubEventIterator
-
+#include "EventInfo/PileUpEventInfo.h"  /*SubEvent*/
 #include "GaudiKernel/Property.h"
 #include "GaudiKernel/ServiceHandle.h"
+#include "LUCID_SimEvent/LUCID_SimHitCollection.h"
+#include "LUCID_RawEvent/LUCID_DigitContainer.h"
 
 #include <vector>
 #include <utility> /* pair */
@@ -28,34 +23,34 @@ class IHistSvc;
 
 class LUCID_PileUpTool: public PileUpToolBase, public LUCID_DigitizationToolBox {
 
-public:
-
+ public:
+  
   LUCID_PileUpTool(const std::string& type,
-                   const std::string& name,
-                   const IInterface* parent);
-
-  virtual StatusCode initialize() override final;
-  virtual StatusCode finalize() override final;
+		   const std::string& name,
+		   const IInterface* parent);
+  
+  virtual StatusCode initialize();
+  virtual StatusCode finalize();
 
   /// called before the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode prepareEvent(const unsigned int nInputEvents) override final;
-
+  virtual StatusCode prepareEvent(const unsigned int nInputEvents);
+  
   /// called for each active bunch-crossing to process current SubEvents bunchXing is in ns
-  virtual StatusCode processBunchXing(int bunchXing,
-                                      SubEventIterator bSubEvents,
-                                      SubEventIterator eSubEvents) override final;
+  virtual StatusCode processBunchXing(int bunchXing, 
+				      PileUpEventInfo::SubEvent::const_iterator bSubEvents,
+				      PileUpEventInfo::SubEvent::const_iterator eSubEvents);
   /// return false if not interested in  certain xing times (in ns)
   /// implemented by default in PileUpToolBase as FirstXing<=bunchXing<=LastXing
-  //  virtual bool toProcess(int bunchXing) const override final;
+  //  virtual bool toProcess(int bunchXing) const;
 
   /// called at the end of the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode mergeEvent() override final;
+  virtual StatusCode mergeEvent();
 
   /// perform the digitization - used by LUCID_DigiTop::execute
-  virtual StatusCode processAllSubEvents() override final;
+  virtual StatusCode processAllSubEvents();
 
-private:
-
+ private:
+  
   ServiceHandle<IAtRndmGenSvc>  m_atRndmGenSvc;
   ServiceHandle<PileUpMergeSvc> m_mergeSvc;
 
@@ -67,12 +62,12 @@ private:
   LUCID_DigitizationToolBox *m_digitToolBox;
 
   CLHEP::HepRandomEngine      *m_rndEngine;
-  //LUCID_DigitContainer *m_digitContainer;
+  LUCID_DigitContainer *m_digitContainer;
   ITHistSvc  *m_digitHistSvc;
 
   LUCID_SimHitCollection* m_mergedhitList;
 
-  int    m_numTubes;
+  int    m_numTubes;  
   double m_qdcChannelsPerPE;
   double m_qdcPedestal;
   double m_qdcFedNoiseFactor;
@@ -90,8 +85,8 @@ private:
   std::vector<double> m_pmtScaling;
   std::vector<double> m_gasScaling;
   std::vector<double> m_npeThreshold;
-
+  
   bool   m_fillRootTree;
 };
-
+ 
 #endif
