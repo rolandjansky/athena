1ec9bb0e2949a34d0f08c68097585c5c887f9ce8
David Rousseau <rousseau@lal.in2p3.fr>
2013-07-29 14:06:01 +0200

    ' include for trigger migration ' (UserAnalysis-00-15-13)

---
+++ PhysicsAnalysis/AnalysisCommon/UserAnalysis/src/components/UserAnalysis_entries.cxx
@@ -0,0 +1,10 @@
+#include "UserAnalysis/AnalysisSkeleton.h"
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( AnalysisSkeleton )
+
+DECLARE_FACTORY_ENTRIES( UserAnalysis ) {
+  DECLARE_ALGORITHM( AnalysisSkeleton )
+}
+
