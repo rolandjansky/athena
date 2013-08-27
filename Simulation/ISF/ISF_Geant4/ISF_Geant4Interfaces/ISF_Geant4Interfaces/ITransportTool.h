b2140715de65ede8ba33e3be2e1fc0bd6ab4a971
Robert Harrington <robert.duane.harrington.jr@cern.ch>
2013-08-27 22:50:36 +0200

    added processVector method to ITransportTool (ISF_Geant4Interfaces-00-00-08)

---
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Interfaces/ISF_Geant4Interfaces/ITransportTool.h
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// ITransportTool.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef ISF_GEANT4INTERFACES_ITRANSPORTTOOL_H
+#define ISF_GEANT4INTERFACES_ITRANSPORTTOOL_H
+
+// Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+namespace Trk{
+  class Track;
+}
+  
+namespace ISF {
+  class ISFParticle;    
+}
+
+namespace iGeant4 {
+  
+  static const InterfaceID IID_ITransportTool("ITransportTool", 1, 0);
+    
+  /** 
+   @class ITransportTool
+
+   universal transport tool 
+
+   - return of new ISFParticle on the next detector boundary
+   - return 0 indicates that the particle did not reach the associated detector boundary
+       
+   @author Robert Harrington
+   @author Andreas Schaelicke
+   
+   */
+      
+  class ITransportTool : virtual public IAlgTool {
+     public:
+     
+       /** Virtual destructor */
+       virtual ~ITransportTool(){}
+
+       /** AlgTool interface methods */
+       static const InterfaceID& interfaceID() { return IID_ITransportTool; }
+
+       /** Processes ISF particle */
+       virtual StatusCode process(const ISF::ISFParticle& isp) const = 0;
+
+       /** Processes vector of ISF particles */
+       virtual StatusCode processVector(const std::vector<const ISF::ISFParticle*>& particles) const = 0;
+  };
+
+} // end of namespace
+
+#endif 
+
