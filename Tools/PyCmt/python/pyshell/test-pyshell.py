c349d7b28098ed41fd7243818a0d9c23b926f41e
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2013-10-10 13:01:57 +0200

    ignore --ccache-skip if it is in CFLAGS (PyCmt-00-00-36)

---
+++ Tools/PyCmt/python/pyshell/test-pyshell.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+import logging,user
+logging.RootLogger.root.setLevel (logging.DEBUG)
+
+import localshell as ls
+sh = ls.LocalShell()
