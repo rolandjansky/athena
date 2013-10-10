ce721aa6bae76cc0a3ed34ac4c2cf0528f970f69
Nathan Rogers Bernard <nathan.rogers.bernard@cern.ch>
2013-10-10 12:52:14 +0200

    Migrated to Eigen (TrkRefitAlg-01-00-00)

---
+++ Tracking/TrkFitter/TrkRefitAlg/TrkRefitAlg/ReFitTrack.h
@@ -0,0 +1,95 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// ReFit.h, (c) ATLAS Detector Softwareop 
+///////////////////////////////////////////////////////////////////
+
+#ifndef TRKREFITALG_REFITTRACK_H
+#define TRKREFITALG_REFITTRACK_H
+
+// Base class
+#include "AthenaBaseComps/AthAlgorithm.h"
+#include "GaudiKernel/ToolHandle.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "GaudiKernel/MsgStream.h"
+#include "TrkTrack/Track.h"
+#include "TrkFitterUtils/FitterTypes.h"
+#include "TrkTrack/TrackCollection.h"
+#include "TrkEventPrimitives/ParticleHypothesis.h"
+#include "TrkEventUtils/TrkParametersComparisonFunction.h"
+
+class IBeamCondSvc;
+
+namespace Trk{
+
+class ITrackFitter;
+class IExtrapolator;
+class ITrackSummaryTool;
+class ITrackSelectorTool;
+class IPRD_AssociationTool;
+
+ /** @brief Algorithm using an instance of a ITrackFitter
+     to refit the tracks of a given track collection.
+     
+     Constraint fits are available:
+     - Option 1 : fit the track with the vertex (first vertex is taken)
+     - Option 2 : fit the track with the beamspot
+
+     @author Maria.Jose.Costa@cern.ch, Andreas.Salzburger@cern.ch
+     */
+ 
+class ReFitTrack : public AthAlgorithm  {
+
+public:
+
+  typedef Trk::Track Track;
+
+  //! standard Algorithm constructor
+  ReFitTrack(const std::string &name,ISvcLocator *pSvcLocator);
+
+  virtual StatusCode initialize();
+  virtual StatusCode execute();
+  virtual StatusCode finalize();
+
+private:
+  ReFitTrack();
+  ReFitTrack(const ReFitTrack&);
+  ReFitTrack &operator=(const ReFitTrack&);
+  
+
+  // --- job options
+  std::string m_TrackName;                                 //!< Name of the input Trackcollection
+  std::string m_NewTrackName;                              //!< Name of the output Trackcollection
+  
+  // --- fitter steering
+  Trk::RunOutlierRemoval          m_runOutlier;             //!< switch whether to run outlier logics or not
+  int                             m_matEffects;             //!< type of material interaction in extrapolation
+  Trk::ParticleHypothesis         m_ParticleHypothesis;     //!< nomen est omen 
+  bool                            m_fitRIO_OnTrack;         //!< switch to fit directly from MeasurementBase / RIO_OnTrack
+
+  
+  // -- algorithm members
+  const TrackCollection              *m_tracks;              //!< job option for main track collection
+  ToolHandle<Trk::ITrackFitter>       m_ITrackFitter;        //!< the refit tool
+  ToolHandle<Trk::ITrackFitter>       m_ITrackFitterTRT;     //!< the TRT refit tool
+  ToolHandle<Trk::ITrackSummaryTool>  m_trkSummaryTool;      //!< the track summary tool
+  ToolHandle<Trk::IPRD_AssociationTool>  m_assoTool;         //!< association tool for PRDs
+  ToolHandle<Trk::ITrackSelectorTool> m_trkSelectorTool;     //!< the track selector tool
+                                                            
+  TrackCollection                 *m_newtracks;              //!< output of refitted tracks
+                                                            
+  unsigned int                     m_constrainFitMode;       //!< 0 - not constraint, 1 - vertex, 2 - beamspot
+  std::string                      m_vxContainerName;        //!< constrain the fit with
+  ServiceHandle<IBeamCondSvc>      m_iBeamCondSvc;           //!< the beam conditions service
+  ToolHandle<Trk::IExtrapolator>   m_extrapolator;           //!< the extrapoaltor for the consistent measurement frame
+  
+  bool m_usetrackhypo;                                       //!< Fit using particle hypothesis from input track    
+  
+};
+
+} 
+
+#endif //TRKREFITALG_REFITTRACK_H
+
---
+++ Tracking/TrkFitter/TrkRefitAlg/doc/mainpage.h
@@ -0,0 +1,56 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage TrkRefitAlg Package
+@author T. Cornelissen, M.J. Costa, W. Liebig
+
+@section TrkRefitAlgIntro Introduction
+
+This package provides a configurable Algorithm, Trk::ReFitTrack,
+to refit tracks.
+In can be used to refit tracks with a different fitter and/or
+track model, to re-create track parameters at each measurement
+previously lost through data conversion or compression, and to
+merge tracks from two single-track collections.
+The output is written to a new track collection.
+
+Note that this package is not meant to provide a full reconstruction
+strategy. For example combined track fits or improved fit strategies
+after particle identification should be implemented inside the
+offline Reconstruction CVS realm.
+
+@section TrkRefitAlgSteering Steering the Trk::ReFitTrack
+Trk::ReFitTrack is set up using standard Gaudi/Athena configurables
+@htmlonly
+<table cellspacing=10px><tr>
+<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
+<td style="background-color:#e8e0e8; color:#000000;
+           padding:10px;" >
+<pre>
+from TrkRefitAlg.TrkRefitAlgConf import Trk__ReFitTrack
+MyReFitTrack = Trk__ReFitTrack (name           = "MyRefitTrack",
+                                FitterTool     = ... )
+topSequence += MyReFitTrack
+</pre></td>
+</tr></table>
+@endhtmlonly
+ 
+@section ExtrasTrkRefitAlg Extra Pages
+
+ - @ref UsedTrkRefitAlg
+ - @ref RequirementsTrkRefitAlg
+*/
+
+/**
+@page UsedTrkRefitAlg Used Packages
+@htmlinclude used_packages.html
+*/
+
+/**
+@page RequirementsTrkRefitAlg Requirements
+@include requirements
+*/
+
+
