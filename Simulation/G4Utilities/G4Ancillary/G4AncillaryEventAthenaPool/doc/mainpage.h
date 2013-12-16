a1a18be7ed2a96d3ab3176d74f3a4d021f06e64f
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2013-12-16 18:17:18 +0100

    cmt/requirements - checkreq fixes. Tagged as G4AncillaryEventAthenaPool-00-00-07 (G4AncillaryEventAthenaPool-00-00-07)

---
+++ Simulation/G4Utilities/G4Ancillary/G4AncillaryEventAthenaPool/doc/mainpage.h
@@ -0,0 +1,32 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage  G4AncillaryEventAthenaPool
+@author Thomas Kittelman (tkittel@mail.cern.ch)
+
+@section G4AncillaryEventAthenaPool Introduction
+
+This package creates a Pool converter for scintillator hit collections
+
+@section G4AncillaryEventAthenaPoolOverview Class Overview
+
+Pool converters are included for ScintillatorHitCollections and SimpleScintillatorHitCollections.
+
+@ref used_G4AncillaryEventAthenaPool
+
+@ref requirements_G4AncillaryEventAthenaPool
+
+*/
+
+/**
+@page used_G4AncillaryEventAthenaPool Used Packages
+@htmlinclude used_packages.html
+*/
+
+/**
+@page requirements_G4AncillaryEventAthenaPool Requirements
+@include requirements
+*/
+
