a5694ef5bff3497d18f3686660a6b436a1bbef13
Scott Snyder <scott.snyder@cern.ch>
2013-12-31 04:09:54 +0100

    Eigen compatibility. (TrigBJetD3PDMaker-00-01-57)

---
+++ PhysicsAnalysis/D3PDMaker/TrigBJetD3PDMaker/python/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+
+import TrigBJetD3PDMakerConf
+for k, v in TrigBJetD3PDMakerConf.__dict__.items():
+    if k.startswith ('D3PD__'):
+        globals()[k[6:]] = v
+
