a38d2895e6b9bd3ccec76b9c0768554db59fd6ab
Andrea Coccaro <Andrea.Coccaro@cern.ch>
2014-01-02 15:31:26 +0100

    Eigen migration (TrigInDetVxInJetTool-00-00-13)

---
+++ Trigger/TrigTools/TrigInDetVxInJetTool/src/TimerHelper.h
@@ -0,0 +1,30 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef _TIMERHELPER_H_
+#define _TIMERHELPER_H_
+#include "TrigTimeAlgs/TrigTimer.h"
+
+
+class TimerHelper
+{
+public:
+
+  TimerHelper( TrigTimer* timer )
+    : m_timer( timer )
+  {
+    if ( m_timer ) m_timer->start();
+  }
+
+  ~TimerHelper()
+  {
+    if ( m_timer ) m_timer->stop();
+  }
+
+private:
+
+  TrigTimer* m_timer;
+};
+
+#endif /* _TIMERHELPER_H_ */
