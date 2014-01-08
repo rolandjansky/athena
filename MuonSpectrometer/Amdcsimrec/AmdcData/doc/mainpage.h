60d4333b28181b8690d75ea4195716b65f5011ff
Jochen Meyer <Jochen.Meyer@cern.ch>
2014-01-08 14:09:51 +0100

    Updating AmdcData-01-37-89 (AmdcData-01-37-89)

---
+++ MuonSpectrometer/Amdcsimrec/AmdcData/doc/mainpage.h
@@ -0,0 +1,32 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+
+@mainpage AmdcData Package
+
+@author samusog@cern.ch
+
+@section IntroductionAmdcData Introduction
+
+AmdcData is a package of the Amdcsimrec package.
+It contains (share sub dir)
+the source Ascii files for the Muon Spectrometer geometry description
+
+@section ExtrasAmdcData Extra Pages
+
+  - @ref used_AmdcData
+  - @ref requirements_AmdcData
+
+*/
+
+/**
+@page used_AmdcData Used Packages
+@htmlinclude used_packages.html
+*/
+
+/**
+@page requirements_AmdcData Requirements
+@include requirements
+*/
