b17e6f12c6169b971a0ae7e52dfe37cc60b7df96
Attila Krasznahorkay <krasznaa@cern.ch>
2014-01-10 08:18:34 +0100

    Making the package compile after the Eigen migration (TrigMinBiasNtuple-00-00-61)

---
+++ Trigger/TrigAnalysis/TrigMinBiasAnalysis/TrigMinBiasNtuple/src/components/TrigMinBiasNtuple_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h" 
+#include "TrigMinBiasNtuple/TrigMinBiasNtupleAlg.h"
+ 
+DECLARE_ALGORITHM_FACTORY(TrigMinBiasNtupleAlg)
+ 
+DECLARE_FACTORY_ENTRIES(TrigMinBiasNtuple) {
+   DECLARE_ALGORITHM(TrigMinBiasNtupleAlg)
+}
