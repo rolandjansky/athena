e2e285dc7711cdde5cd86c224965929483b705ca
Scott Snyder <scott.snyder@cern.ch>
2014-03-27 03:50:11 +0100

    Re-enable dictionary generation. (TrkTrackLink-01-00-01)

---
+++ Tracking/TrkEvent/TrkTrackLink/TrkTrackLink/ITrackLink.h
@@ -0,0 +1,54 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRKTRACKLINK_ITRACKLINK_H
+#define TRKTRACKLINK_ITRACKLINK_H
+
+/**
+ * @class Trk::ITrackLink
+ * An abstract class which is meant
+ * to represent an element link to
+ * the Trk::Track or Trk::TrackParticleBase
+ *
+ *  The use of abstract class is required to
+ *  break the TrackParticleBase<-->VxVertex
+ *  circular dependence.
+ *
+ *  Kirill Prokofiev, June 2007
+ */
+
+#include "TrkParameters/TrackParameters.h"    // no forward declare for now because needed in cast
+
+namespace Trk
+{
+  class ITrackLink
+  {
+
+  public:
+
+    /** default constructor */
+    ITrackLink() {}
+
+    /** default destructor */
+    virtual ~ITrackLink() {}
+
+    /** copy constructor */
+    ITrackLink ( const ITrackLink& ) {}
+
+    /** clone methods for copy constructors */
+    virtual ITrackLink * clone()  const = 0;
+
+    /** return the track parameters of the track or TrackParticleBase) */
+    virtual const TrackParameters* parameters() const = 0;
+
+    /**return number of parameters currently created*/
+    ////DRQ: Commented out because not implemented
+    ////static unsigned int numberOfInstantiations();
+
+  }
+    ; //end of class definitions
+
+}//end of namespace definitions
+
+#endif //TRKTRACKLINK_ITRACKLINK_H
---
+++ Tracking/TrkEvent/TrkTrackLink/doc/mainpage.h
@@ -0,0 +1,26 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage  TrkTrackLink
+
+@author Kirill.Prokofiev@cern.ch
+@author Andreas.Wildauer@cern.ch
+
+@section PackageDescription Package Description
+
+This package was created to resolve the circular dependency between the VxVertex and the TrkParticleBase package. The circular dependency
+happened because the Trk::VxTrackAtVertex object needs to have an ElementLink to a Trk::TrackParticleBase object and the latter has an
+ElementLink to a Trk::VxCandidate object. The Trk::ITrackLink class in this package replaces the ElementLink to the Track(ParticleBase)
+in the Trk::VxTrackAtVertex class and resolves this circular depenency therefore.
+
+The actual links which are used now are the classes Trk::LinkToTrack and Trk::LinkToTrackParticle which both inherit from Trk::ITrackLink
+and ElementLink< TrackCollection > or ElementLink< TrackParticleBaseContainer >, respectively.
+
+@section UsedPackages Used Packages
+@htmlinclude used_packages.html
+@section MyPackageReq Requirements
+@include requirements
+
+*/
