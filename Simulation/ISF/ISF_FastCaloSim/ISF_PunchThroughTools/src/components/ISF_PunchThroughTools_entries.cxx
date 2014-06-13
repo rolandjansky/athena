8904d8b0ec78f80a1c39fa42a29b4ff7de1d8410
Anna Usanova <ausanova@cern.ch>
2014-06-13 13:55:53 +0200

    changed the calculation of detector boundaries (ISF_PunchThroughTools-00-01-01)

---
+++ Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "ISF_PunchThroughTools/PunchThroughTool.h"
+
+DECLARE_NAMESPACE_TOOL_FACTORY( ISF,PunchThroughTool)
+DECLARE_FACTORY_ENTRIES( ISF_PunchThroughTools ) {
+  DECLARE_NAMESPACE_TOOL( ISF,PunchThroughTool)
+}
+
