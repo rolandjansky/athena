cf536c82b82d8fcc28b29f9a009a73ed0cb9aee3
Robert Johannes Langenberg <robert.langenberg@cern.ch>
2014-07-16 11:37:56 +0200

    merging Scott's dictionary addition and Anthony's index patch (GeoPrimitives-00-00-33-02)

---
+++ DetectorDescription/GeoPrimitives/GeoPrimitives/AmgStringHelpers.h
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/*
+ * eigen_migration
+ * AmgStringHelpers.h
+ *
+ *  Created on: Feb 25, 2014
+ *      Author: rbianchi <Riccardo.Maria.Bianchi@cern.ch>
+ *
+ */
+
+#ifndef AMGSTRINGHELPERS_H_
+#define AMGSTRINGHELPERS_H_
+
+#include "GeoPrimitives/GeoPrimitives.h"
+
+namespace Amg{
+
+
+/**
+ * write an Amg Eigen object to std::string
+ */
+template<class T>
+std::string AsString(const T& m) {
+	std::stringstream tmp;
+	tmp << m; // we just use eigen classes capability to write to std::ostream
+	return tmp.str();
+}
+
+
+
+} // end of namespace Amg
+
+#endif /* AMGSTRINGHELPERS_H_ */
