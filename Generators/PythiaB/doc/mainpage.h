b0d36ac5418cdb0eeb12d79568c923418540d940
Andy Buckley <a.g.buckley@gmail.com>
2014-08-01 11:10:18 +0200

    'Use GeneratorName now from new TruthUtils package. Tagging: PythiaB-00-01-00 (new series for R19+)' (PythiaB-00-01-00)

---
+++ Generators/PythiaB/doc/mainpage.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+
+ <i>
+Interface to Pythia6 dedicated to simulation of beauty events.
+
+PythiaB can be used for a generation of beauty events 
+written into a persistent root file, which can serve as an input into 
+a detector simulation
+(atlsim) or into ATFAST. PythiaB can also work in the same job with ATLFAST 
+  in this case the HepMC tree is passed from PythiaB to ATLFAST in
+  a transient form.
+
+    The description of pysics related to the PythiaB, as well as instructions how to run PythiaB can be found here:
+http://atlas-sw.cern.ch/cgi-bin/viewcvs-atlas.cgi/offline/Generators/PythiaB/doc/PythiaB.pdf
+
+
+</i>
+
+@author Maria Smizanska (maria.smizanska@cern.ch),
+
+    doxygen doc - Ewelina Lobodzinska (Jan. 2008)
+
+@mainpage
+
+@htmlinclude used_packages.html
+
+@include requirements
+
+*/
---
+++ Generators/PythiaB/src/components/PythiaB_entries.cxx
@@ -0,0 +1,19 @@
+//====================================================================
+//  GeneratorBModules_entries.cxx
+//--------------------------------------------------------------------
+//
+//  Package    : Generators/GeneratorModules
+//
+//  Description: Declaration of the components (factory entries)
+//               specified by this component library.
+//
+//====================================================================
+
+#include "PythiaB/PythiaB.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( PythiaB )
+
+DECLARE_FACTORY_ENTRIES( PythiaB ) {
+    DECLARE_ALGORITHM( PythiaB )
+}
