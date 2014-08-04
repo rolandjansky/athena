8a2d30921285d342a7e41a3c3c319fd8eb5167b8
Andrea Coccaro <Andrea.Coccaro@cern.ch>
2014-08-04 09:53:58 +0200

    removing the AtlasAnalysisRunTime dependency (MSVertexToolInterfaces-00-01-03)

---
+++ MuonSpectrometer/MSVertexReconstruction/MSVertexToolInterfaces/MSVertexToolInterfaces/IMSVertexRecoTool.h
@@ -0,0 +1,37 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MSVERTEXTOOLINTERFACES_MUON_IMSVERTEXRECOTOOL_H
+#define MSVERTEXTOOLINTERFACES_MUON_IMSVERTEXRECOTOOL_H
+ 
+#include "GaudiKernel/IAlgTool.h"
+#include "MSVertexUtils/Tracklet.h"
+#include "MSVertexUtils/MSVertex.h"
+#include <vector>
+//                           
+static const InterfaceID IID_IMSVertexRecoTool("Muon::IMSVertexRecoTool",1,0);
+
+namespace Muon {
+
+  /** @brief The IMSVertexRecoTool is a pure virtual interface */  
+  class IMSVertexRecoTool : virtual public IAlgTool 
+  {      
+    public:
+
+    /** access to tool interface */
+    static const InterfaceID& interfaceID();
+    
+    virtual StatusCode findMSvertices(std::vector<Tracklet>& tracklets) = 0;    
+
+  };
+  
+  inline const InterfaceID& IMSVertexRecoTool::interfaceID()
+  {
+    return IID_IMSVertexRecoTool;
+  }
+
+} // end of name space
+
+#endif 
+
---
+++ MuonSpectrometer/MSVertexReconstruction/MSVertexToolInterfaces/MSVertexToolInterfaces/IMSVertexTrackletTool.h
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MSVERTEXTOOLINTERFACES_MUON_IMSVERTEXTRACKLETTOOL_H
+#define MSVERTEXTOOLINTERFACES_MUON_IMSVERTEXTRACKLETTOOL_H
+ 
+#include "GaudiKernel/IAlgTool.h"
+#include "MSVertexUtils/Tracklet.h"
+#include <vector>
+//                           
+static const InterfaceID IID_IMSVertexTrackletTool("Muon::IMSVertexTrackletTool",1,0);
+
+namespace Muon {
+
+  /** @brief The IMSVertexTrackletTool is a pure virtual interface */  
+  class IMSVertexTrackletTool : virtual public IAlgTool 
+  {      
+    public:
+
+    /** access to tool interface */
+    static const InterfaceID& interfaceID();
+    
+    virtual StatusCode findTracklets(std::vector<Tracklet>& traklets) = 0;
+
+  };
+  
+  inline const InterfaceID& IMSVertexTrackletTool::interfaceID()
+  {
+    return IID_IMSVertexTrackletTool;
+  }
+
+} // end of name space
+
+#endif 
+
