5f1f4b5de9db6a1b819ac1241f954264ef0183bf
Zachary Louis Marshall <zach.marshall@cern.ch>
2014-08-15 17:14:24 +0200

    Updating requirements file again (G4StepLimitation-00-01-05)
    Former-commit-id: c39e034259734e26e7b9fc8b5e2aae5bf5af63c4

--- Simulation/G4Utilities/G4StepLimitation/python/G4StepLimitationConfig.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from AthenaCommon import CfgMgr
-
-def getG4StepLimitationTool(name="G4StepLimitationTool", **kwargs):
-    #Add any future configuration here
-    return CfgMgr.G4StepLimitationTool(name, **kwargs)
