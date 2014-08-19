729ed2d14760fce091ea7ae5ea6d3317fd2ff562
Mark Sutton <mark.sutton@cern.ch>
2014-08-19 10:18:07 +0200

    remove extra headers and fix cxx file (VP1TriggerDecisionSystems-00-00-12-01)
    Former-commit-id: 850dd6769799e1a062f47a2909af1a48906ecec9

--- graphics/VP1/VP1Systems/VP1TriggerDecisionSystems/src/VP1TriggerDecisionSystem.cxx
+++ graphics/VP1/VP1Systems/VP1TriggerDecisionSystems/src/VP1TriggerDecisionSystem.cxx
@@ -12,7 +12,6 @@
 
 #include "TrigDecisionTool/TrigDecisionTool.h"
 #include "TrigSteeringEvent/TrigRoiDescriptor.h"
-#include "TrigSteeringEvent/Lvl1Result.h"
 
 class VP1TriggerDecisionSystem::Clockwork
 {
@@ -28,7 +27,7 @@ public:
 };
 
 VP1TriggerDecisionSystem::Clockwork::Clockwork()
-  : m_trigDec("TrigDec::TrigDecisionTool",0), theclass{}
+  : m_trigDec("TrigDec::TrigDecisionTool",0)
 {
 }
 
@@ -151,10 +150,10 @@ void VP1TriggerDecisionSystem::Clockwork::getInfo(const std::string& key,const s
   if(key.find("L1",0)!= std::string::npos) {
     os << "	LV1: " << key << std::endl;
     const LVL1CTP::Lvl1Item*  l1iterm = m_trigDec->ExperimentalAndExpertMethods()->getItemDetails(key);
-    if(l1iterm) {
+    if(!l1iterm) {
       os << "	LV1 " << l1iterm->name() << " isPassed: " 	<< l1iterm->isPassed() << std::endl;
       os << "	LV1 " << l1iterm->name() << " isPrescaled: " 	<< l1iterm->isPrescaled()
-	        << " : "  <<  l1iterm->prescaleFactor()   << std::endl;
+	 << " : "  <<  l1iterm->prescaleFactor()   << std::endl;
       os << "	LV1 " << l1iterm->name() << " isVeto: " 		<< l1iterm->isVeto() << std::endl;
     }
   }
