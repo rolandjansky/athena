17c075e6812e9188b5c31a384258cf8322272962
Andreas Salzburger <salzburg@cern.ch>
2014-08-21 16:00:09 +0200

    recomitting (TrkDetDescrTools-01-02-16-01)
    	* allow layer index to be ignored
    	* tagged TrkDetDescrTools-01-02-16-01
    
    2014-08-21 Andreas Salzburger < andreas.salzburger -at- cern.ch >
    	* allow layer index to be ignored
    	* tagged TrkDetDescrTools-01-02-16-00

--- Tracking/TrkDetDescr/TrkDetDescrTools/TrkDetDescrTools/LayerMaterialProvider.h
+++ Tracking/TrkDetDescr/TrkDetDescrTools/TrkDetDescrTools/LayerMaterialProvider.h
@@ -60,6 +60,8 @@ namespace Trk {
         //!< boolean switch for assignLayerMaterial
         mutable const LayerMaterialMap*             m_layerMaterialMap;
         std::string                                 m_layerMaterialMapName;
+        
+        int                                         m_ignoreLayer; //!< ajustment for R2 19.1.1 geometry
                         
         
     };
