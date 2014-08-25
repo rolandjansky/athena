4df8f07295f68a3f255e29dc9509ca1be1c83367
Scott Snyder <scott.snyder@cern.ch>
2014-08-25 20:12:24 +0200

    Fix uninitialized members. (egammaRecEvent-00-00-08)

---
+++ Reconstruction/egamma/egammaRecEvent/egammaRecEvent/egammaRecEventDict.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef EGAMMARECEVENT_EGAMMARECEVENTDICT_H
+# define EGAMMARECEVENT_EGAMMARECEVENTDICT_H
+/**
+ * @file egammaInterfacesDict.h
+ *
+ * @brief Header file for Reflex dictionary generation
+ *
+ * @author Bruno Lenzi <Bruno.Lenzi@cern.ch>
+ */
+
+#include "egammaRecEvent/egammaRec.h"
+#include "egammaRecEvent/egammaRecContainer.h"
+
+#endif // EGAMMARECEVENT_EGAMMARECEVENTDICT_H
