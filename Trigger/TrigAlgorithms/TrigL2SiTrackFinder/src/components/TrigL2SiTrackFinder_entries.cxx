7bb9995298f7a0bb384d090677bbd6e3f2e8e0fa
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2014-08-27 14:23:57 +0200

    'Remove commented-out EventInfo calls' (TrigL2SiTrackFinder-00-00-69)

---
+++ Trigger/TrigAlgorithms/TrigL2SiTrackFinder/src/components/TrigL2SiTrackFinder_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+#include "TrigL2SiTrackFinder/TrigL2SiTrackFinder.h"
+
+DECLARE_ALGORITHM_FACTORY( TrigL2SiTrackFinder)
+DECLARE_FACTORY_ENTRIES( TrigL2SiTrackFinder )
+{
+  DECLARE_ALGORITHM( TrigL2SiTrackFinder )
+}
