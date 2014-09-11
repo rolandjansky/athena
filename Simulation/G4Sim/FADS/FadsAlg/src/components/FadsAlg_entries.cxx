ec8a292999c51504e40be9fdee8211c8d55d050f
Zachary Louis Marshall <zach.marshall@cern.ch>
2014-09-11 07:32:02 +0200

    Protecting against failed cast in FadsAlg (FadsAlg-00-00-06)

---
+++ Simulation/G4Sim/FADS/FadsAlg/src/components/FadsAlg_entries.cxx
@@ -0,0 +1,8 @@
+#include "FadsAlg/FadsAlg.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( FadsAlg )
+
+DECLARE_FACTORY_ENTRIES(FadsAlg) {
+DECLARE_ALGORITHM( FadsAlg );
+}
