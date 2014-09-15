1c40199f2dfdbd36cd29d202e97530c376170f21
Jochen Meyer <Jochen.Meyer@cern.ch>
2014-09-15 23:10:01 +0200

    fixing compiler warning (MuonTGMeasAssocAlg-01-00-01)

---
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/MuonTGMeasAssocAlg/MuonTGMeasAssocAlg/MuonTGMeasAssocAlg.h
@@ -0,0 +1,162 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// MuonTGMeasAssocAlg.h, (c) ATLAS Detector Softwareop 
+///////////////////////////////////////////////////////////////////
+
+#ifndef MUONTGMEASASSOCALG_MUONTGMEASASSOCALG_H
+#define MUONTGMEASASSOCALG_MUONTGMEASASSOCALG_H
+
+// Base class
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/ToolHandle.h"
+#include "StoreGate/DataHandle.h"
+#include "StoreGate/StoreGateSvc.h"
+#include "TrkTrack/Track.h"
+#include "TrkExInterfaces/IExtrapolator.h"
+#include "TrkGeometry/TrackingGeometry.h"
+#include "TrkSurfaces/CylinderSurface.h"
+#include "TrkPrepRawData/PrepRawData.h"
+#include "TrkSegment/SegmentCollection.h"
+#include <fstream>
+
+#include "MuonIdHelpers/MdtIdHelper.h"
+#include "MuonIdHelpers/RpcIdHelper.h"
+#include "MuonReadoutGeometry/MuonDetectorManager.h"
+#include "MuonPrepRawData/MdtPrepData.h"
+#include "MuonPrepRawData/RpcPrepData.h"
+#include "MuonPrepRawData/CscPrepData.h"
+#include "MuonPrepRawData/TgcPrepData.h"
+#include "MuonTGRecTools/IMuonTGMeasTool.h"
+#include "MuonTGRecTools/MuonTGHits.h"
+#include "MuonTGRecTools/MuonTGSegments.h"
+
+// Amg
+#include "GeoPrimitives/GeoPrimitives.h"
+
+class StoreGateSvc;
+
+namespace Muon{
+
+ /** @class MuonTGMeasAssocAlg
+
+     associates hits/segments with active layers from the (muon) tracking geometry
+          
+     @author Sarka.Todorova@cern.ch
+     */
+ 
+class MuonTGMeasAssocAlg : public Algorithm  {
+
+public:
+
+  typedef Trk::Track Track;
+  typedef std::pair<const Trk::Layer*,std::vector<Identifier>*> PairOfLayerID;
+  typedef std::pair<const Trk::Layer*,std::vector<const Trk::PrepRawData*>*> PairOfLayerPrd;
+  typedef std::pair<const Trk::Layer*,std::vector<const Muon::MdtPrepData*>*> PairOfLayerMdtPrd;
+  typedef std::pair<const Trk::Layer*,std::vector<const Muon::RpcPrepData*>*> PairOfLayerRpcPrd;
+  typedef std::pair<const Trk::Layer*,std::vector<const Muon::CscPrepData*>*> PairOfLayerCscPrd;
+  typedef std::pair<const Trk::Layer*,std::vector<const Muon::TgcPrepData*>*> PairOfLayerTgcPrd;
+
+  /** Constructor with parameters */
+  MuonTGMeasAssocAlg(const std::string &name,ISvcLocator *pSvcLocator);
+
+  /** Destructor */
+  ~MuonTGMeasAssocAlg();
+
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+
+  StatusCode retrieveMeasurements();
+  StatusCode storeMeasurements();
+  StatusCode storeSegments();
+
+private:
+  std::vector<PairOfLayerPrd*>* createMdtHitCollectionLayers() const;
+  std::vector<PairOfLayerPrd*>* createRpcHitCollectionLayers() const;
+  std::vector<PairOfLayerPrd*>* createCscHitCollectionLayers() const;
+  std::vector<PairOfLayerPrd*>* createTgcHitCollectionLayers() const;
+  StatusCode createStationHitCollection() const;
+  StatusCode createStationSegmentCollection() const;
+  void createStationMap(const Trk::TrackingVolume* vol) const;
+  void misAlignStations() const;
+  void reAlignStations() const;
+  const Trk::Layer* associatedLayer(int techn, Identifier id) const;
+  const Trk::Layer* associatedLayer(const Trk::TrackingVolume* trVol, Identifier id) const;
+  Identifier getStationId( Identifier id ) const;
+  
+  // --- job options
+  bool  m_mdtIn;
+  bool  m_rpcIn;
+  bool  m_cscIn;
+  bool  m_tgcIn;
+  bool  m_segmentsIn;
+  bool  m_writeTgHits;
+  bool  m_writeTgSegments;
+  bool  m_reAlign;
+  bool  m_misAlign;
+  bool  m_allowGeomAssoc;
+
+  mutable const Trk::TrackingGeometry* m_trackingGeometry;
+  std::string  m_trackingGeometryName;
+  
+  ToolHandle<Muon::IMuonTGMeasTool> m_muonTgTool;
+  
+  const MdtIdHelper* m_mdtIdHelper;
+  const RpcIdHelper* m_rpcIdHelper;
+  const CscIdHelper* m_cscIdHelper;
+  const TgcIdHelper* m_tgcIdHelper;
+  const MuonGM::MuonDetectorManager* m_muonMgr;
+
+  std::string  m_inputSegmentCollectionMoore;  
+  std::string  m_inputSegmentCollectionMoMu;  
+  std::string  m_inputSegmentCollectionMBoy;  
+  std::string  m_inputMdtPrdCollection;  
+  std::string  m_inputRpcPrdCollection;  
+  std::string  m_inputTgcPrdCollection;  
+  std::string  m_inputCscPrdCollection;  
+  
+  // -- algorithm members
+  StoreGateSvc                    *m_StoreGate;         //!< transient event store providing the tracks
+  ActiveStoreSvc*                  m_activeStore;
+  mutable std::vector<PairOfLayerPrd*>*       m_mdtHits;
+  mutable std::vector<PairOfLayerPrd*>*       m_rpcHits;
+  mutable std::vector<PairOfLayerPrd*>*       m_cscHits;
+  mutable std::vector<PairOfLayerPrd*>*       m_tgcHits;
+  mutable MuonTGHits                      *m_allHits;
+  mutable MuonTGSegments                  *m_allSegments;
+  ToolHandle<Trk::IExtrapolator>        m_extrapolator;
+  mutable std::map<Identifier,std::pair<const MuonGM::MuonStation*,const Trk::DetachedTrackingVolume*> >  m_stationMap;
+  mutable std::vector<std::pair<Identifier, Amg::Transform3D> > m_misAlignDiff;     // vector holding required misalignment
+};
+
+ inline StatusCode MuonTGMeasAssocAlg::retrieveMeasurements() {
+   // Get the messaging service, print where you are
+  MsgStream log(msgSvc(), name());
+  log << MSG::DEBUG << "MuonTGMeasAssocAlg::retrieveMeasurements()" << endreq;
+  //
+  //if (m_allHits) delete m_allHits;
+  if (m_mdtHits) delete m_mdtHits;
+  if (m_rpcHits) delete m_rpcHits;
+  if (m_cscHits) delete m_cscHits;
+  if (m_tgcHits) delete m_tgcHits;
+
+  if (m_mdtIn) m_mdtHits = createMdtHitCollectionLayers();  
+  if (m_rpcIn) m_rpcHits = createRpcHitCollectionLayers();  
+  if (m_cscIn) m_cscHits = createCscHitCollectionLayers();  
+  if (m_tgcIn) m_tgcHits = createTgcHitCollectionLayers();  
+  StatusCode sc = createStationHitCollection();
+
+  log << MSG::DEBUG << "MuonTGMeasAssocAlg::collected hits from " << m_allHits->size() << " stations"  << endreq;
+  
+  if (m_allHits->size()) return StatusCode::SUCCESS;
+
+  return StatusCode::FAILURE;  
+ }
+ 
+} 
+
+#endif //MUONTGMEASASSOCALG_MUONTGMEASASSOCALG_H
+
---
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/MuonTGMeasAssocAlg/share/MuonTGMeasAssocAlg_jobOptions.py
@@ -0,0 +1,66 @@
+# muon active/passive geometry setup
+from MuonTrackingGeometry.ConfigMuonTrackingGeometry import MuonTrackingGeometryBuilder
+ToolSvc += MuonTrackingGeometryBuilder
+
+# atlas tracking geometry builder
+from TrkDetDescrTools.TrkDetDescrToolsConf import Trk__GeometryBuilder
+MuonStandAloneGeometryBuilder = Trk__GeometryBuilder( name = "MuonStandAloneGeometryBuilder",
+                                        MuonTrackingGeometryBuilder = MuonTrackingGeometryBuilder )
+ToolSvc += MuonStandAloneGeometryBuilder
+ToolSvc.MuonStandAloneGeometryBuilder.InDetTrackingGeometry = False
+ToolSvc.MuonStandAloneGeometryBuilder.CaloTrackingGeometry = False
+ToolSvc.MuonStandAloneGeometryBuilder.MuonTrackingGeometry = True 
+print MuonStandAloneGeometryBuilder
+
+# combined tracking geometry options
+from TrkDetDescrSvc.TrkDetDescrSvcConf import TrackingGeometrySvc
+MuonTrackingGeometrySvc = TrackingGeometrySvc( name ="MuonTrackingGeometrySvc",
+                                                    TrackingGeometryName = "MuonStandaloneTrackingGeometry",
+                                                    GeometryBuilder = MuonStandAloneGeometryBuilder,
+                                                    BuildGeometryFromTagInfo = False,
+                                                    AssignMaterialFromCOOL = False )
+theApp.CreateSvc += [ "TrackingGeometrySvc/MuonTrackingGeometrySvc" ]
+
+# navigator 
+from TrkExTools.TrkExToolsConf import Trk__Navigator
+MuonNavigator = Trk__Navigator(name = "MuonNavigator" )
+ToolSvc += MuonNavigator
+ToolSvc.MuonNavigator.TrackingGeometrySvc = "MuonTrackingGeometrySvc" 
+ToolSvc.MuonNavigator.OutputLevel = 5
+print MuonNavigator
+
+# material effects updator
+from TrkExTools.TrkExToolsConf import Trk__MaterialEffectsUpdator
+MuonMatUpdator = Trk__MaterialEffectsUpdator( name = "MuonMatUpdator" )
+ToolSvc += MuonMatUpdator
+print MuonMatUpdator
+
+# propagator 
+from TrkExSTEP_Propagator.TrkExSTEP_PropagatorConf import Trk__STEP_Propagator
+MuonPropagator = Trk__STEP_Propagator(name = 'MuonPropagator')
+ToolSvc += MuonPropagator
+ToolSvc.MuonPropagator.MaterialEffects = True
+print MuonPropagator
+
+# extrapolator
+from TrkExTools.TrkExToolsConf import Trk__Extrapolator
+MuonExtrapolator = Trk__Extrapolator(  name = 'MuonExtrapolator', 
+                                   Propagators = [ MuonPropagator ] ,
+                                   Navigator = MuonNavigator,
+                                   MaterialEffectsUpdators = [ MuonMatUpdator ]
+                                       )
+ToolSvc += MuonExtrapolator
+#ToolSvc.MuonExtrapolator.ConsiderMuonStationOverlaps = False
+ToolSvc.MuonExtrapolator.OutputLevel = 5
+print MuonExtrapolator
+
+#
+from MuonTGMeasAssocAlg.MuonTGMeasAssocAlgConf import Muon__MuonTGMeasAssocAlg
+MuonTGMeasAssocAlg = Muon__MuonTGMeasAssocAlg(  name = 'MuonTGMeasAssocAlg',
+                                                Extrapolator = MuonExtrapolator, 
+                                                TrackingGeometry = 'MuonStandaloneTrackingGeometry',
+                                                ProcessSegments = True )
+topSequence += MuonTGMeasAssocAlg
+print MuonTGMeasAssocAlg
+
+
