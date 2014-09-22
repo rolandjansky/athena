966354738b27e0a8d192baa9d34bed35f3235599
Jochen Meyer <Jochen.Meyer@cern.ch>
2014-09-22 14:39:53 +0200

    removing Muonboy dependency (AmdcMGM-02-00-00)
    Former-commit-id: 396bef59ca0bf4beff38e27512ec9f960ae95734

--- MuonSpectrometer/Amdcsimrec/AmdcMGM/AmdcMGM/AmdcMGMMisc.h
+++ MuonSpectrometer/Amdcsimrec/AmdcMGM/AmdcMGM/AmdcMGMMisc.h
@@ -6,12 +6,25 @@
 #ifndef AmdcMGMMISC_H
 #define AmdcMGMMISC_H
 
+#ifdef HAVE_NEW_IOSTREAMS
+//gcc3.2
 #include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <cstdio>
-#include <cmath>
+#else
+//gcc 295
+#include <iostream.h>
+#include <iomanip.h>
+#include <fstream.h>
+#endif
+#if HAVE_CXX_STDC_HS
+# include <cstdio>
+# include <cmath>
+#else
+# include <stdio.h>
+# include <math.h>
+#endif
 #include <algorithm>
 #include <iterator>
 #include <list>
