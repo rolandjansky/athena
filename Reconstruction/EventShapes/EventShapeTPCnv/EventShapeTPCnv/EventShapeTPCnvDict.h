fe6c45419c934c5b2cdfcb64abe547d855e6552c
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2014-10-01 18:34:00 +0200

    PvG Autotag: Remove dummy struct from Dict.h for ROOT6 (EventShapeTPCnv-00-00-03)
    2014-09-23  scott snyder  <snyder@bnl.gov>
    
    	* Tagging EventShapeTPCnv-00-00-02.
    	* cmt/requirements: Fix checkreq warnings.

--- Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeTPCnvDict.h
+++ Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeTPCnvDict.h
@@ -11,7 +11,7 @@
 #include "EventShapeTPCnv/EventShapeStoreCnv_p2.h"
 
 namespace {
-  struct dummy {
+  struct GCCXML_DUMMY_INSTANTIATION_EVENTSHAPETPCNV {
     T_TPCnv<EventShapeStore, EventShapeStore_p1> m_esscnv_p1;
     T_TPCnv<EventShapeStore, EventShapeStore_p2> m_esscnv_p2;
   };
