cdcb405063504849b72303c46104aed3fd4af401
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2014-10-03 14:01:38 +0200

    3 October 2014 Mark Hodgkinson Update to fill new topocluster moment histograms (PFODQA-00-00-08)

--- Reconstruction/PFlow/PFlowValidation/PFODQA/src/ClusterValidationPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFODQA/src/ClusterValidationPlots.cxx
@@ -4,12 +4,12 @@
 
 #include "ClusterValidationPlots.h"
 
-ClusterValidationPlots::ClusterValidationPlots(PlotBase* pParent, std::string sDir,  std::string sClusterContainerName) : PlotBase(pParent, sDir),
-													     m_ClusterPlots(this,"PFO_JetETMiss/",sClusterContainerName)
+ClusterValidationPlots::ClusterValidationPlots(PlotBase* pParent, std::string sDir,  std::string sClusterContainerName) : PlotBase(pParent, sDir),m_ClusterPlots(this,"PFO_JetETMiss/",sClusterContainerName),m_ClusterMomentPlots(this,"PFO_JetETMiss/",sClusterContainerName)
 {}
 
 void ClusterValidationPlots::fill(const xAOD::CaloCluster& theCluster){
   m_ClusterPlots.fill(theCluster);
+  m_ClusterMomentPlots.fill(theCluster);
 }
 
 
