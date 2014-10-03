386cef7e1c945652693cf6ccb52873929e4fd95f
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2014-10-03 14:00:25 +0200

    3 October 2014 Mark Hodgkinson Update to fill new topocluster moment histograms (PFOHistUtils-00-00-07)

---
+++ Reconstruction/PFlow/PFlowValidation/PFOHistUtils/PFOHistUtils/ClusterMomentPlots.h
@@ -0,0 +1,44 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef CLUSTERMOMENTPLOTS_H
+#define CLUSTERMOMENTPLOTS_H
+
+#include "TrkValHistUtils/PlotBase.h"
+#include "xAODCaloEvent/CaloCluster.h"
+
+namespace PFO {
+
+  class ClusterMomentPlots : public PlotBase {
+
+  public:
+
+     ClusterMomentPlots(PlotBase *pParent, std::string sDir, std::string sClusterContainerName);
+
+     void fill(const xAOD::CaloCluster& cluster);
+
+  private:
+
+     TH1* m_LATERAL;
+     TH1* m_LONGITUDINAL;
+     TH1* m_SECOND_R;
+     TH1* m_CENTER_LAMBDA;
+     TH1* m_FIRST_ENG_DENS;
+     TH1* m_ENG_FRAC_MAX;
+     TH1* m_ISOLATION;
+     TH1* m_ENG_BAD_CELLS;
+     TH1* m_N_BAD_CELLS;
+     TH1* m_BADLARQ_FRAC;
+     TH1* m_ENG_POS;
+     TH1* m_SIGNIFICANCE;
+     TH1* m_AVG_LAR_Q;
+     TH1* m_AVG_TILE_Q;
+
+     void initializePlots();
+     std::string m_sClusterContainerName;
+
+  };
+
+}
+#endif
