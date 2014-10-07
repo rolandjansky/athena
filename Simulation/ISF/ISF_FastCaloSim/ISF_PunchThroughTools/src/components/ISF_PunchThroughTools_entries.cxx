9de69b9e994503ac8de6391a00cda59fee266dab
Anna Usanova <ausanova@cern.ch>
2014-10-07 12:09:01 +0200

    using new ISF::GeoIDSvc (ISF_PunchThroughTools-00-01-02)
    Former-commit-id: 0854000598a37d3bda1e6785b89c6ccd7e10a628

--- Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
@@ -1,5 +1,8 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "../PunchThroughTool.h"
+#include "ISF_PunchThroughTools/PunchThroughTool.h"
 
-DECLARE_NAMESPACE_TOOL_FACTORY(ISF, PunchThroughTool)
+DECLARE_NAMESPACE_TOOL_FACTORY( ISF,PunchThroughTool)
+DECLARE_FACTORY_ENTRIES( ISF_PunchThroughTools ) {
+  DECLARE_NAMESPACE_TOOL( ISF,PunchThroughTool)
+}
 
