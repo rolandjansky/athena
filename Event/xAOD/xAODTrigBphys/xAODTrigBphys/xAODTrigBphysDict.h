30a016c578ddb04acc991415bd653a0f0434a0e8
James William Walder <james.william.walder@cern.ch>
2014-10-15 13:50:45 +0200

    Tagging as xAODTrigBphys-00-00-06; clone method (xAODTrigBphys-00-00-06)
    	* Implementation of clone method
    	* Tagging as xAODTrigBphys-00-00-06
    
    2014-06-03 James Walder <jwalder@cern.ch>
    	* Another fix to the ...Dict.h file for TrackParticle EL links
    
    2014-06-02 James Walder <jwalder@cern.ch>
    	* update to dict file with pre includes for eigen vectorisation
    	* define EIGEN_DONT_VECTORIZE stuff for standalone xoad

--- Event/xAOD/xAODTrigBphys/xAODTrigBphys/xAODTrigBphysDict.h
+++ Event/xAOD/xAODTrigBphys/xAODTrigBphys/xAODTrigBphysDict.h
@@ -4,9 +4,14 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODTrigBphysDict.h 600124 2014-06-03 15:35:20Z krasznaa $
+// $Id: xAODTrigBphysDict.h 604721 2014-07-03 09:55:39Z jwalder $
 #ifndef XAODTRIGBPHYS_AODTRIGBPHYSDICT_H
 #define XAODTRIGBPHYS_AODTRIGBPHYSDICT_H
+
+// Needed to successfully generate the dictionary in standalone mode:
+#if defined(__GCCXML__) and not defined(EIGEN_DONT_VECTORIZE)
+#   define EIGEN_DONT_VECTORIZE
+#endif // __GCCXML__
 
 // STL include(s):
 #include <vector>
@@ -29,6 +34,15 @@ namespace{
       std::vector< ElementLink< xAOD::TrigBphysContainer_v1 > > bphys_l4;
       std::vector< DataLink< xAOD::TrigBphysContainer_v1 > > bphys_l5;
       std::vector< std::vector< ElementLink< xAOD::TrigBphysContainer_v1 > > > bphys_l6;
+      std::vector< std::vector< ElementLink< xAOD::TrackParticleContainer > > > bphys_17;
+
+     // Instantiate the classes used by xAOD::xAODTrigBphys so that
+     // Reflex would see them with their "correct type". Note that the
+     // dictionary for these types comes from other places. This instantiation
+     // is just needed for "Reflex related technical reasons"...
+     ElementLink< xAOD::TrackParticleContainer > auxlink1;
+     std::vector< ElementLink< xAOD::TrackParticleContainer > > auxlink2;
+     std::vector< std::vector< ElementLink< xAOD::TrackParticleContainer > > > auxlink3;
 
   };
 }
