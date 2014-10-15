5d74c0bcba9053004d133df81ce4aff0c70c1eb0
Steven Randolph Schramm <Steven.Schramm@cern.ch>
2014-10-15 14:02:47 +0200

    'Creating initial structure for ICPJetUncertaintiesTool' (JetCPInterfaces-00-00-00)

---
+++ Reconstruction/Jet/JetCPInterfaces/JetCPInterfaces/ICPJetUncertaintiesTool.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef JETCPINTERFACES_ICPJETUNCERTAINTIESTOOL_H
+#define JETCPINTERFACES_ICPJETUNCERTAINTIESTOOL_H
+
+#include "JetInterface/IJetUncertaintiesTool.h"
+
+#include "PATInterfaces/ISystematicsTool.h"
+#include "PATInterfaces/CorrectionCode.h"
+
+class ICPJetUncertaintiesTool : virtual public IJetUncertaintiesTool,
+                                virtual public CP::ISystematicsTool
+{
+    // Interface declaration
+    ASG_TOOL_INTERFACE(ICPJetUncertaintiesTool)
+
+    public:
+        // Apply a systematic variation or get a new copy
+        virtual CP::CorrectionCode applyCorrection(xAOD::Jet& input) const = 0;
+        virtual CP::CorrectionCode applyCorrection(xAOD::Jet& input, const xAOD::EventInfo& eInfo) const = 0;
+        virtual CP::CorrectionCode correctedCopy(const xAOD::Jet& input, xAOD::Jet*& output) const = 0;
+        virtual CP::CorrectionCode correctedCopy(const xAOD::Jet& input, xAOD::Jet*& output, const xAOD::EventInfo& eInfo) const = 0;
+        virtual CP::CorrectionCode applyContainerCorrection(xAOD::JetContainer& inputs) const = 0;
+        virtual CP::CorrectionCode applyContainerCorrection(xAOD::JetContainer& inputs, const xAOD::EventInfo& eInfo) const = 0;
+
+};
+
+#endif
+
