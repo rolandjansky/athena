66c72d81e8809fce4cb653d75a5eaaa47dc299d4
Zachary Louis Marshall <zach.marshall@cern.ch>
2014-10-28 13:24:33 +0100

    First import of Track Record Generator, ported from Cosmic Generator (TrackRecordGenerator-00-00-00)

---
+++ Generators/TrackRecordGenerator/src/components/TrackRecordGenerator_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "TrackRecordGenerator/TrackRecordGenerator.h"
+
+DECLARE_ALGORITHM_FACTORY( TrackRecordGenerator )
+
+DECLARE_FACTORY_ENTRIES( TrackRecordGenerator ){
+  DECLARE_ALGORITHM( TrackRecordGenerator )
+}
