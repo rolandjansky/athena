39b446f326638da9793625d7c306482a9062925a
James Robinson <james.robinson@cern.ch>
2014-10-28 13:31:52 +0100

    Fixed flg_debug problem from 00-00-15. Checked successful completion of example jobs. (PowhegControl-00-00-16)
    	* Updated integration parameters and checked example jobOptions
    	* VBF_H+Pythia8 and ttbar+Pythia6 have some problems in showering; not related to OTF
    
    20-10-14 James Robinson <james.robinson@cern.ch>
    	* Added first versions of ggHZ, HWJ and HZJ
    	* Optimised some integration parameters
    	* Added branches for 00-00-XX, 00-01-XX and 00-02-XX
    
    11-10-14 James Robinson <james.robinson@cern.ch>
    	* Added VBF_H process
    	* Renamed ggH to ggH_quark_mass_effects and added additional options
    	* Updated some default parameters for optimisation
    	* Added separate blocks for common EW, diboson and CKM settings
    
    17-09-14 James Robinson <james.robinson@cern.ch>
    	* Added ggH process
    	* Added some missing example jobOptions
    	* Updated defaults to ensure optimised parameters
    	* Matched parameters to those used in production
    
    ...
    (Long ChangeLog diff - truncated)

---
+++ Generators/PowhegControl/share/PowhegControl_ggHZ_Common.py
@@ -0,0 +1,7 @@
+from PowhegControl.PowhegUtils import PowhegConfig_ggHZ
+
+# Use the PowhegConfig_ggHZ configuration
+if 'runArgs' in dir() :
+  PowhegConfig = PowhegConfig_ggHZ(runArgs)
+else :
+  PowhegConfig = PowhegConfig_ggHZ()
