5e406145cc95662d7418409ee950a84ba6ff5b4d
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2014-10-30 17:12:18 +0100

    src/ZDC_StripSD.cxx - protection against accessing arrays out of bounds. Should fix Coverity Defects 13246 and 13247. ATLASSIM-541. src/ZDC_PxielSD.cxx - protection against accessing arrays out of bounds. Should fix Coverity Defects 13243 and 13244. ATLASSIM-1514. Tagged as ZDC_SD-00-00-10 (ZDC_SD-00-00-10)
    	* src/ZDC_StripSD.cxx - protection against accessing arrays out of
    	bounds. Should fix Coverity Defects 13246 and 13247. ATLASSIM-541.
    	* src/ZDC_PxielSD.cxx - protection against accessing arrays out of
    	bounds. Should fix Coverity Defects 13243 and 13244. ATLASSIM-1514.
    	* tagged as ZDC_SD-00-00-10
    
    	* cmt/requirements - add use statement for AtlasCLHEP.
    	directly instead (should be transparent).
    	* Fix Coverity bugs in ZDC_PixelSD.cxx/.h and ZDC_StripSD.cxx/.h
    	* hits deleted after being inserted in the collection in EndOfEvent (memory leak)
    	* hit_collections are now created by EndOfEvent and removed from class members
    	* tagged as ZDC_SD-00-00-06
    	* Fix Coverity bugs in ZDC_PixelSD.cxx and ZDC_StripSD.cxx
    	* tagged as ZDC_SD-00-00-05
    	* Adding ChangeLog
    	* tagged as ZDC_SD-00-00-03
    	* Fix CheckReq warnings and forward declarations
    	* tagged as ZDC_SD-00-00-02
    	* First import
    	* tagged as ZDC_SD-00-00-01

--- ForwardDetectors/ZDC/ZDC_SD/src/ZDC_PixelSD.cxx
+++ ForwardDetectors/ZDC/ZDC_SD/src/ZDC_PixelSD.cxx
@@ -9,7 +9,7 @@
 #include "CLHEP/Units/PhysicalConstants.h"
 #include <cmath>
 #include "ZDC_SD/ZDC_PixelSD.h"
-#include "ZDC_SimEvent/ZDC_SimPixelHit_Collection.h" 
+#include "ZDC_SimEvent/ZDC_SimPixelHit_Collection.h"
 #include "AthenaBaseComps/AthMsgStreamMacros.h"
 #include "G4Poisson.hh"
 
@@ -19,88 +19,101 @@ void ZDC_PixelSD::Initialize(G4HCofThisEvent*)
 {
   ATH_MSG_DEBUG("IN INITIALIZE OF PIXELSD");
 
-  for (int I=0; I<=1; I++) for (int J=0; J<=1; J++) for (int K=0; K<80; K++) {
-    
-    Edep_Cherenkov_Pixel   [I][J][K] = 0;
-    NPhoton_Cherenkov_Pixel[I][J][K] = 0;
+  for (int I=0; I<=1; I++) {
+    for (int J=0; J<=1; J++) for (int K=0; K<80; K++) {
+        Edep_Cherenkov_Pixel   [I][J][K] = 0;
+        NPhoton_Cherenkov_Pixel[I][J][K] = 0;
+      }
   }
 }
 
 G4bool ZDC_PixelSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
 {
-  G4StepPoint*             pPreStepPoint  = aStep->GetPreStepPoint();
-  G4StepPoint*             pPostStepPoint = aStep->GetPostStepPoint();
+  const G4StepPoint*             pPreStepPoint  = aStep->GetPreStepPoint();
+  const G4StepPoint*             pPostStepPoint = aStep->GetPostStepPoint();
   const G4DynamicParticle* aParticle      = aStep->GetTrack()->GetDynamicParticle();
-  const G4double           charge         = aParticle->GetDefinition()->GetPDGCharge();                
+  const G4double           charge         = aParticle->GetDefinition()->GetPDGCharge();
   const G4double           beta           = (pPreStepPoint ->GetBeta() + pPostStepPoint->GetBeta())/2.;
 
   G4double MeanNumberOfPhotons = 291.93*(charge/CLHEP::eplus)*(charge/CLHEP::eplus)* (1.0 - 1.0/(beta*1.48*beta*1.48))/(CLHEP::cm);
 
-  if (MeanNumberOfPhotons <= 0.0) return 1;
-  
-  G4double step_length = aStep->GetStepLength();
+  if (MeanNumberOfPhotons <= 0.0) { return true; }
+
+  const G4double step_length = aStep->GetStepLength();
 
   MeanNumberOfPhotons = MeanNumberOfPhotons * step_length;
-  
-  G4int NumPhotons = (G4int)G4Poisson(MeanNumberOfPhotons);
-  
-  if (NumPhotons <= 0) return 1;
+
+  const G4int NumPhotons = (G4int)G4Poisson(MeanNumberOfPhotons);
+
+  if (NumPhotons <= 0) { return true; }
 
   //THIS PART LOCATES THE SIDE, MODULE AND PIXEL WHERE THE HIT OCCURED
 
-  int Pixel_No = -1;
-  int Module   = -1;
-  int Side     = -1;
+  int Pixel_No(-1);
+  int Module(-1);
+  int Side(-1);
 
-  G4int Location_From_Copy_No = pPreStepPoint->GetPhysicalVolume()->GetCopyNo();
-  
+  const G4int Location_From_Copy_No = pPreStepPoint->GetPhysicalVolume()->GetCopyNo();
+
   Pixel_No =    (Location_From_Copy_No%100);
   Module   = int(Location_From_Copy_No/999) %10 - 1;
   Side     = int(Location_From_Copy_No/9999)%10 - 1;
-  
+
   if (Pixel_No<0 || Module<0 || Side<0) {
-    
-    ATH_MSG_ERROR("COULDNOT LOCATE PIXEL(From Copy) Values are (Side,Module,Pixel_No) = "
-		  << Side     << "   "
-		  << Module   << "   "
-		  << Pixel_No << "   ");
-    return 0;
+
+    ATH_MSG_ERROR("COULD NOT LOCATE PIXEL(From Copy) Values are (Side,Module,Pixel_No) = "
+                  << Side     << "   "
+                  << Module   << "   "
+                  << Pixel_No << "   ");
+    G4ExceptionDescription description;
+    description << "ProcessHits: COULD NOT LOCATE PIXEL(From Copy)";
+    G4Exception("ZDC_PixelSD", "FailedToLocatePixel", FatalException, description);
   }
-  
+
+  if (Pixel_No>79 || Module>1 || Side>1) {
+    ATH_MSG_ERROR("Impossible values obtained. Values are (Side,Module,Pixel_No) = "
+                  << Side     << "   "
+                  << Module   << "   "
+                  << Pixel_No << "   ");
+    G4ExceptionDescription description;
+    description << "ProcessHits: IMPOSSIBLE PIXEL LOCATION (From Copy)";
+    G4Exception("ZDC_PixelSD", "ImpossiblePixelLocation", FatalException, description);
+  }
+
   //THIS PART CALCULATES THE CHERENKOV ENERGY DEPOSITED IN PIXELS (AFTER CHECKING THE CUTS)
-  
-  G4ThreeVector p0 = aStep->GetDeltaPosition().unit();
-  G4double Pmin   = 2.761*CLHEP::eV;  // Rindex->GetMinPhotonEnergy();
+
+  const G4ThreeVector p0 = aStep->GetDeltaPosition().unit();
+  const G4double Pmin   = 2.761*CLHEP::eV;  // Rindex->GetMinPhotonEnergy();
   //G4double Pmax = 3.550*CLHEP::eV;  // Rindex->GetMaxPhotonEnergy();
-  G4double dp     = 0.789*CLHEP::eV;  // Pmax - Pmin;
-  G4double BetaInverse = 1./beta;
-  
+  const G4double dp     = 0.789*CLHEP::eV;  // Pmax - Pmin;
+  const G4double BetaInverse = 1./beta;
+
   for (G4int I = 0; I < NumPhotons; I++) {
 
     G4double rand;
     G4double sampledEnergy,sampledRI;
     G4double cosTheta, sin2Theta;
-    
+
     // sample an energy for Photon
     rand = G4UniformRand();
     sampledEnergy = Pmin + rand * dp;
     sampledRI = 1.48;
     cosTheta  = BetaInverse / sampledRI;
     sin2Theta = (1.0 - cosTheta)*(1.0 + cosTheta);
-      
+
     // Generate random position of photon on cone surface defined by Theta
     rand = G4UniformRand();
-    G4double phi = 2.*M_PI*rand;
-    G4double sinPhi = sin(phi);
-    G4double cosPhi = cos(phi);
-    
-    // calculate x,y, and z components of photon momentum 
-    // (in coord system with primary particle direction aligned with the z axis) 
+    const G4double phi = 2.*M_PI*rand;
+    const G4double sinPhi = sin(phi);
+    const G4double cosPhi = cos(phi);
+
+    // calculate x,y, and z components of photon momentum
+    // (in coord system with primary particle direction aligned with the z axis)
     // and Rotate momentum direction back to global reference system
-    G4double sinTheta = sqrt(sin2Theta);
-    G4double px = sinTheta*cosPhi;
-    G4double py = sinTheta*sinPhi;
-    G4double pz = cosTheta;
+    const G4double sinTheta = sqrt(sin2Theta);
+    const G4double px = sinTheta*cosPhi;
+    const G4double py = sinTheta*sinPhi;
+    const G4double pz = cosTheta;
     G4ParticleMomentum photonMomentum(px, py, pz);
     photonMomentum.rotateUz(p0);
 
@@ -110,69 +123,69 @@ G4bool ZDC_PixelSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
 #define THETA_CRITICAL_QUARTZ 0.742
 
     G4double Transmission = 1.0;
-    G4double PT = sqrt(photonMomentum.getX()*photonMomentum.getX() + photonMomentum.getY()*photonMomentum.getY());
-    G4double PZ = photonMomentum.getZ();
+    const G4double PT = sqrt(photonMomentum.getX()*photonMomentum.getX() + photonMomentum.getY()*photonMomentum.getY());
+    const G4double PZ = photonMomentum.getZ();
 
     if      (PZ<0)  continue;         // If photon is travelling with -ve PZ, Its not going to reach the PMT
     else if (PT==0) Transmission=1.0; // if photon is along Z-axis it will be completely transmitted into the PMT
     else {
 
-      G4double Theta = M_PI/2.0-atan(PT/PZ);  
-      
+      const G4double Theta = M_PI/2.0-atan(PT/PZ);
+
       if (Theta < THETA_CRITICAL_QUARTZ) continue;
       else Transmission=1.0;
     }
-    
+
     Edep_Cherenkov_Pixel   [Side][Module][Pixel_No] += sampledEnergy*Transmission;
     NPhoton_Cherenkov_Pixel[Side][Module][Pixel_No] += 1;
   }
-  
-  return 1;
-}  
+
+  return true;
+}
 
 void ZDC_PixelSD::EndOfEvent(G4HCofThisEvent*)
 {
   ATH_MSG_DEBUG("Printing Final Energy(eV) deposited in Pixel");
-  
+
   ZDC_SimPixelHit_Collection* m_ZDC_SimPixelHit_Collection = new ZDC_SimPixelHit_Collection("ZDC_SimPixelHit_Collection");
-  
+
   for (int I=0; I<=1; I++) {
 
-    int Jmin = 0; if (I==1) Jmin = 1; 
+    int Jmin = 0; if (I==1) Jmin = 1;
 
     for (int J=Jmin; J<=1; J++) {
-      
-      int Kmax = 24; // CHANGE TO 80 IF YOU WANT EACH PIXEL IN HMXY TP BE READ OUT SEPARATELY (Also make corresponding change in ZDC_GeoM file)
-      
-      if (J==0) Kmax = 64;
-      
-      for (int K=0; K<Kmax; K++) {
-	
-	int    NPhoton = NPhoton_Cherenkov_Pixel[I][J][K];
-	double Edep_C  = Edep_Cherenkov_Pixel   [I][J][K]/CLHEP::eV;
-	
-	ATH_MSG_DEBUG(" Final Cherenkov Energy(eV) deposited in Pixel "
-		      << I << "   "
-		      << J << "   "
-		      << K << "  = "
-		      << Edep_C << " eV and Number of Photons deposited = " << NPhoton);
-	
-	ZDC_SimPixelHit* ahit = new ZDC_SimPixelHit(I, J, K, NPhoton, Edep_C);
-	
-	m_ZDC_SimPixelHit_Collection->Insert(*ahit);
 
-	delete ahit;
+      int Kmax = 24; // CHANGE TO 80 IF YOU WANT EACH PIXEL IN HMXY TP BE READ OUT SEPARATELY (Also make corresponding change in ZDC_GeoM file)
+
+      if (J==0) Kmax = 64;
+
+      for (int K=0; K<Kmax; K++) {
+
+        const int    NPhoton = NPhoton_Cherenkov_Pixel[I][J][K];
+        const double Edep_C  = Edep_Cherenkov_Pixel   [I][J][K]/CLHEP::eV;
+
+        ATH_MSG_DEBUG(" Final Cherenkov Energy(eV) deposited in Pixel "
+                      << I << "   "
+                      << J << "   "
+                      << K << "  = "
+                      << Edep_C << " eV and Number of Photons deposited = " << NPhoton);
+
+        ZDC_SimPixelHit* ahit = new ZDC_SimPixelHit(I, J, K, NPhoton, Edep_C);
+
+        m_ZDC_SimPixelHit_Collection->Insert(*ahit);
+
+        delete ahit;
       }
-    }	    
+    }
   }
-  
+
   //for (ZDC_SimPixelHit_ConstIterator it = m_ZDC_SimPixelHit_Collection->begin(); it != m_ZDC_SimPixelHit_Collection->end(); it++)
   // ATH_MSG_DEBUG(" BUG_TRACKER "
-  // 		<< " Side: "     << it->GetSide()
-  // 		<< " ModuleNo: " << it->GetMod()
-  // 		<< " PixNo: "    << it->GetPix()
-  // 		<< " NPhotons: " << it->GetNPhotons()
-  // 		<< " Edep: "     << it->GetEdep());
-  
+  //            << " Side: "     << it->GetSide()
+  //            << " ModuleNo: " << it->GetMod()
+  //            << " PixNo: "    << it->GetPix()
+  //            << " NPhotons: " << it->GetNPhotons()
+  //            << " Edep: "     << it->GetEdep());
+
   m_HitCollHelp.ExportCollection<ZDC_SimPixelHit_Collection>(m_ZDC_SimPixelHit_Collection);
 }
