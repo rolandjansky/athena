cdb8845eb85e40ad367a31b964fc4e544fa4b061
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2014-10-31 19:38:50 +0100

    add ISFParticle::ekin() return method for EntryLayer filtering in ATLASSIM-1584 (ISF_Event-00-06-02)
    	* ISF_Event/ISFParticle.cxx/icc:  add ekin() return method
    	for EntryLayer filtering in ATLASSIM-1584.
    	* src/ISFTruthIncident.cxx: use new ISFParticle::ekin() method
    	* tagging as ISF_Event-00-06-02
    
    2014-10-22  John Chapman  <John.Chapman@cern.ch>
    
    	* src/ISFTruthIncident.cxx - fix stpuid bug in
    	constructor. ATLASSIM-1603.
    	* tagging as ISF_Event-00-06-01
    
    2014-10-21  John Chapman  <John.Chapman@cern.ch>
    
    	* ISF_Event/ITruthIncident.h - add prototype for
    	setSecondaryPassed method, which will be used to record that a
    	particular secondary passed a check by a TruthStrategy. Add
    	prototype for writeOutSecondary method which will be used to check
    	whether a particular secondary be written out to the GenEvent. Add
    	setPassWholeVertices method which will be used to set whether this
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/ISF/ISF_Core/ISF_Event/ISF_Event/ISFTruthIncident.h
+++ Simulation/ISF/ISF_Core/ISF_Event/ISF_Event/ISFTruthIncident.h
@@ -35,85 +35,96 @@ namespace ISF {
     fPrimarySurvives = false,
     fKillsPrimary    = true
   };
-      
+
   /** @class ISFTruthIncident
-  
+
       @TODO: class description
-  
+
       @author Elmar.Ritsch@cern.ch
-   */
-   
+  */
+
   class ISFTruthIncident : public ITruthIncident {
-    public:      
-      ISFTruthIncident( ISF::ISFParticle &primary,
-                        const ISFParticleVector &secondary,
-                        Barcode::PhysicsProcessCode process,
-                        AtlasDetDescr::AtlasRegion geoID,
-                        ISF::KillPrimary killsPrimary = ISF::fPrimarySurvives,
-                        const HepMC::FourVector *position = 0);
-      ~ISFTruthIncident();
+  public:
+    ISFTruthIncident( ISF::ISFParticle &primary,
+                      const ISFParticleVector &secondary,
+                      Barcode::PhysicsProcessCode process,
+                      AtlasDetDescr::AtlasRegion geoID,
+                      ISF::KillPrimary killsPrimary = ISF::fPrimarySurvives,
+                      const HepMC::FourVector *position = 0);
+    ~ISFTruthIncident();
 
-      /** Return HepMC position of the truth vertex */
-      const HepMC::FourVector&  position() const;
-      /** Return Physics process code of the truth incident */
-      Barcode::PhysicsProcessCode physicsProcessCode() const;
+    /** Return HepMC position of the truth vertex */
+    const HepMC::FourVector&  position() const;
+    /** Return Physics process code of the truth incident */
+    Barcode::PhysicsProcessCode physicsProcessCode() const;
 
-      /** Return p^2 of the primary particle */
-      double                    primaryP2() const;
-      /** Return pT^2 of the primary particle */
-      double                    primaryPt2() const;
-      /** Return Ekin of the primary particle */
-      double                    primaryEkin() const;
-      /** Return the PDG Code of the primary particle */
-      int                       primaryPdgCode() const;
-      /** Return the primary particle as a HepMC particle type 
-          (usually only called for particles that will enter the HepMC truth event) */
-      HepMC::GenParticle*       primaryParticle(bool setPersistent) const;
-      /** Return the barcode of the primary particle */
-      Barcode::ParticleBarcode  primaryBarcode() const;
-      /** Return the extra barcode of the primary particle */
-      Barcode::ParticleBarcode  primaryExtraBarcode() const;
-      /** Return the primary particle after the TruthIncident vertex (and give 
-          it a new barcode) */
-      HepMC::GenParticle*       primaryParticleAfterIncident(Barcode::ParticleBarcode newBC,
-                                                             bool setPersistent);
+    /** Return p^2 of the primary particle */
+    double                    primaryP2() const;
+    /** Return pT^2 of the primary particle */
+    double                    primaryPt2() const;
+    /** Return Ekin of the primary particle */
+    double                    primaryEkin() const;
+    /** Return the PDG Code of the primary particle */
+    int                       primaryPdgCode() const;
+    /** Return the primary particle as a HepMC particle type
+        (usually only called for particles that will enter the HepMC truth event) */
+    HepMC::GenParticle*       primaryParticle(bool setPersistent) const;
+    /** Return the barcode of the primary particle */
+    Barcode::ParticleBarcode  primaryBarcode() const;
+    /** Return the extra barcode of the primary particle */
+    Barcode::ParticleBarcode  primaryExtraBarcode() const;
+    /** Return the primary particle after the TruthIncident vertex (and give
+        it a new barcode) */
+    HepMC::GenParticle*       primaryParticleAfterIncident(Barcode::ParticleBarcode newBC,
+                                                           bool setPersistent);
 
-      /** Return total number of secondary particles */
-      unsigned short            numberOfSecondaries() const;
-      /** Return p^2 of the i-th secondary particle */
-      double                    secondaryP2(unsigned short index) const;
-      /** Return pT^2 of the i-th secondary particle */
-      double                    secondaryPt2(unsigned short index) const;
-      /** Return Ekin of the i-th secondary particle */
-      double                    secondaryEkin(unsigned short index) const;
-      /** Return the PDG Code of the i-th secondary particle */
-      int                       secondaryPdgCode(unsigned short index) const;
-      /** Return the i-th secondary as a HepMC particle type and assign the given
-          Barcode to the simulator particle (usually only called for particles that
-          will enter the HepMC truth event) */
-      HepMC::GenParticle*       secondaryParticle(unsigned short index,
-                                                  Barcode::ParticleBarcode bc,
-                                                  bool setPersistent) const;
-      /** Set the the barcode of all secondary particles to the given bc */
-      void                      setAllSecondaryBarcodes(Barcode::ParticleBarcode bc);
-      /** Set the the extra barcode of all secondary particles to the given bc */
-      void                      setAllSecondaryExtraBarcodes(Barcode::ParticleBarcode bc);
-      /** Set the the extra barcode of a secondary particles to the given bc */
-      void                      setSecondaryExtraBarcode(unsigned short index, Barcode::ParticleBarcode bc);
-      
+    /** Return total number of secondary particles */
+    unsigned short            numberOfSecondaries() const;
+    /** Return p^2 of the i-th secondary particle */
+    double                    secondaryP2(unsigned short index) const;
+    /** Return pT^2 of the i-th secondary particle */
+    double                    secondaryPt2(unsigned short index) const;
+    /** Return Ekin of the i-th secondary particle */
+    double                    secondaryEkin(unsigned short index) const;
+    /** Return the PDG Code of the i-th secondary particle */
+    int                       secondaryPdgCode(unsigned short index) const;
+    /** Return the i-th secondary as a HepMC particle type and assign the given
+        Barcode to the simulator particle (usually only called for particles that
+        will enter the HepMC truth event) */
+    HepMC::GenParticle*       secondaryParticle(unsigned short index,
+                                                Barcode::ParticleBarcode bc,
+                                                bool setPersistent) const;
+    /** Set the the barcode of all secondary particles to the given bc */
+    void                      setAllSecondaryBarcodes(Barcode::ParticleBarcode bc);
+    /** Set the the extra barcode of all secondary particles to the given bc */
+    void                      setAllSecondaryExtraBarcodes(Barcode::ParticleBarcode bc);
+    /** Set the the extra barcode of a secondary particles to the given bc */
+    void                      setSecondaryExtraBarcode(unsigned short index, Barcode::ParticleBarcode bc);
+    /** Record that a particular secondary passed a check */
+    inline void                      setSecondaryPassed(unsigned short index) const;
+    /** Should a particular secondary be written out to the GenEvent */
+    inline bool                      writeOutSecondary(unsigned short index) const;
+  private:
+    ISFTruthIncident();
+    inline HepMC::GenParticle* convert( ISF::ISFParticle *particle,
+                                        bool setPersistent) const;
 
-    private:
-      ISFTruthIncident();
-      inline HepMC::GenParticle* convert( ISF::ISFParticle *particle,
-                                          bool setPersistent) const;
+    ISF::ISFParticle&                  m_primary;
+    const ISFParticleVector&           m_secondary;
+    const Barcode::PhysicsProcessCode  m_process;
+    const ISF::KillPrimary             m_killsPrimary;
+    mutable const HepMC::FourVector   *m_position;
+    mutable std::vector<bool>          m_passedFilters;
+  };
 
-      ISF::ISFParticle&                  m_primary;
-      const ISFParticleVector&           m_secondary;
-      const Barcode::PhysicsProcessCode  m_process;
-      const ISF::KillPrimary             m_killsPrimary;
-      mutable const HepMC::FourVector   *m_position;
+  void ISF::ISFTruthIncident::setSecondaryPassed(unsigned short index) const {
+    m_passedFilters[index] = m_passedFilters[index] || true;
+    return;
+  }
 
-   };
+  bool ISF::ISFTruthIncident::writeOutSecondary(unsigned short index) const {
+    return m_wholeVertexPassed || m_passedFilters[index];
+  }
 
 } // end of namespace
 
--- Simulation/ISF/ISF_Core/ISF_Event/ISF_Event/ITruthIncident.h
+++ Simulation/ISF/ISF_Core/ISF_Event/ISF_Event/ITruthIncident.h
@@ -24,79 +24,89 @@ namespace HepMC {
 namespace ISF {
 
   /** @class ITruthIncident
-  
+
       @TODO: class description
-  
+
       @author Elmar.Ritsch -at- cern.ch
-     */
-  class ITruthIncident { 
-    public: 
-      ITruthIncident(AtlasDetDescr::AtlasRegion geoID):m_geoID(geoID) { };
+  */
+  class ITruthIncident {
+  public:
+    ITruthIncident(AtlasDetDescr::AtlasRegion geoID):m_geoID(geoID),m_passWholeVertex(true),m_wholeVertexPassed(false) { };
 
-      /** Return the SimGeoID corresponding to the vertex of the truth incident */
-      AtlasDetDescr::AtlasRegion geoID() { return m_geoID; };
+    /** Return the SimGeoID corresponding to the vertex of the truth incident */
+    AtlasDetDescr::AtlasRegion geoID() { return m_geoID; };
 
-      /** Return HepMC position of the truth vertex */
-      virtual const HepMC::FourVector&  position() const = 0;
+    /** Return HepMC position of the truth vertex */
+    virtual const HepMC::FourVector&  position() const = 0;
 
-      /** Return Physics process code of the truth incident */
-      virtual Barcode::PhysicsProcessCode physicsProcessCode() const = 0;
+    /** Return Physics process code of the truth incident */
+    virtual Barcode::PhysicsProcessCode physicsProcessCode() const = 0;
 
-      /** Return p^2 of the primary particle */
-      virtual double                    primaryP2() const = 0;
-      /** Return pT^2 of the primary particle */
-      virtual double                    primaryPt2() const = 0;
-      /** Return Ekin of the primary particle */
-      virtual double                    primaryEkin() const = 0;
-      /** Return the PDG Code of the primary particle */
-      virtual int                       primaryPdgCode() const = 0;
-      /** Return the primary particle as a HepMC particle type 
-          (only called for particles that will enter the HepMC truth event) */
-      virtual HepMC::GenParticle*       primaryParticle(bool setPersistent=false) const = 0;
-      /** Return the barcode of the primary particle */
-      virtual Barcode::ParticleBarcode  primaryBarcode() const = 0;
-      /** Return the extra barcode of the primary particle */
-      virtual Barcode::ParticleBarcode  primaryExtraBarcode() const { return 0; }
-      /** Return the primary particle after the TruthIncident vertex (and assign 
-          a new barcode to it) */
-      virtual HepMC::GenParticle*       primaryParticleAfterIncident(Barcode::ParticleBarcode newBC,
-                                                                     bool setPersistent=false) = 0;
+    /** Return p^2 of the primary particle */
+    virtual double                    primaryP2() const = 0;
+    /** Return pT^2 of the primary particle */
+    virtual double                    primaryPt2() const = 0;
+    /** Return Ekin of the primary particle */
+    virtual double                    primaryEkin() const = 0;
+    /** Return the PDG Code of the primary particle */
+    virtual int                       primaryPdgCode() const = 0;
+    /** Return the primary particle as a HepMC particle type
+        (only called for particles that will enter the HepMC truth event) */
+    virtual HepMC::GenParticle*       primaryParticle(bool setPersistent=false) const = 0;
+    /** Return the barcode of the primary particle */
+    virtual Barcode::ParticleBarcode  primaryBarcode() const = 0;
+    /** Return the extra barcode of the primary particle */
+    virtual Barcode::ParticleBarcode  primaryExtraBarcode() const { return 0; }
+    /** Return the primary particle after the TruthIncident vertex (and assign
+        a new barcode to it) */
+    virtual HepMC::GenParticle*       primaryParticleAfterIncident(Barcode::ParticleBarcode newBC,
+                                                                   bool setPersistent=false) = 0;
 
-      /** Return total number of secondary particles */
-      virtual unsigned short            numberOfSecondaries() const = 0;
-      /** Return p^2 of the i-th secondary particle */
-      virtual double                    secondaryP2(unsigned short index) const = 0;
-      /** Return pT^2 of the i-th secondary particle */
-      virtual double                    secondaryPt2(unsigned short index) const = 0;
-      /** Return Ekin of the i-th secondary particle */
-      virtual double                    secondaryEkin(unsigned short index) const = 0;
-      /** Return the PDG Code of the i-th secondary particle */
-      virtual int                       secondaryPdgCode(unsigned short index) const = 0;
-      /** Return true if at least one secondary particle passes the given p^2 cut
-          (= at least one secondary with p^2 >= pt2cut) */
-      inline bool                       secondaryP2Pass(double p2cut) const;
-      /** Return true if at least one secondary particle passes the given pT^2 cut
-          (= at least one secondary with pT^2 >= pt2cut) */
-      inline bool                       secondaryPt2Pass(double pt2cut) const;
-      /** Return true if at least one secondary particle passes the given Ekin cut
-          (= at least one secondary with Ekin >= ekincut) */
-      inline bool                       secondaryEkinPass(double ekincut) const;
-      /** Return the i-th secondary as a HepMC particle type and assign the given
-          Barcode to the simulator particle (only called for particles that will
-          enter the HepMC truth event) */
-      virtual HepMC::GenParticle*       secondaryParticle(unsigned short index,
-                                                          Barcode::ParticleBarcode bc = Barcode::fUndefinedBarcode,
-                                                          bool setPersistent=false) const = 0;
-      /** Set the the barcode of all secondary particles to the given bc */
-      virtual void                      setAllSecondaryBarcodes(Barcode::ParticleBarcode bc) = 0;
-      /** Set the the extra barcode of all secondary particles to the given bc */
-      virtual void                      setAllSecondaryExtraBarcodes(Barcode::ParticleBarcode /*bc*/) {};
-      /** Set the the extra barcode of a secondary particles to the given bc */
-      virtual void                      setSecondaryExtraBarcode(unsigned short /*index*/, Barcode::ParticleBarcode /*bc*/) {};
+    /** Return total number of secondary particles */
+    virtual unsigned short            numberOfSecondaries() const = 0;
+    /** Return p^2 of the i-th secondary particle */
+    virtual double                    secondaryP2(unsigned short index) const = 0;
+    /** Return pT^2 of the i-th secondary particle */
+    virtual double                    secondaryPt2(unsigned short index) const = 0;
+    /** Return Ekin of the i-th secondary particle */
+    virtual double                    secondaryEkin(unsigned short index) const = 0;
+    /** Return the PDG Code of the i-th secondary particle */
+    virtual int                       secondaryPdgCode(unsigned short index) const = 0;
+    /** Return true if at least one secondary particle passes the given p^2 cut
+        (= at least one secondary with p^2 >= pt2cut) */
+    inline bool                       secondaryP2Pass(double p2cut) const;
+    /** Return true if at least one secondary particle passes the given pT^2 cut
+        (= at least one secondary with pT^2 >= pt2cut) */
+    inline bool                       secondaryPt2Pass(double pt2cut) const;
+    /** Return true if at least one secondary particle passes the given Ekin cut
+        (= at least one secondary with Ekin >= ekincut) */
+    inline bool                       secondaryEkinPass(double ekincut) const;
+    /** Return the i-th secondary as a HepMC particle type and assign the given
+        Barcode to the simulator particle (only called for particles that will
+        enter the HepMC truth event) */
+    virtual HepMC::GenParticle*       secondaryParticle(unsigned short index,
+                                                        Barcode::ParticleBarcode bc = Barcode::fUndefinedBarcode,
+                                                        bool setPersistent=false) const = 0;
+    /** Set the the barcode of all secondary particles to the given bc */
+    virtual void                      setAllSecondaryBarcodes(Barcode::ParticleBarcode bc) = 0;
+    /** Set the the extra barcode of all secondary particles to the given bc */
+    virtual void                      setAllSecondaryExtraBarcodes(Barcode::ParticleBarcode /*bc*/) {};
+    /** Set the the extra barcode of a secondary particles to the given bc */
+    virtual void                      setSecondaryExtraBarcode(unsigned short /*index*/, Barcode::ParticleBarcode /*bc*/) {};
+    /** Record that a particular secondary passed a check */
+    virtual void                      setSecondaryPassed(unsigned short index) const = 0;
+    /** Should a particular secondary be written out to the GenEvent */
+    virtual bool                      writeOutSecondary(unsigned short index) const = 0;
+    /** Set whether this TruthIncident should pass the vertex as a whole or individual secondaries */
+    inline void                       setPassWholeVertices(bool passWholeVertex);
 
-    private:
-      AtlasDetDescr::AtlasRegion                     m_geoID;
-
+  private:
+    AtlasDetDescr::AtlasRegion                     m_geoID;
+  protected:
+    bool m_passWholeVertex;
+    mutable bool m_wholeVertexPassed; //FIXME clean up logic so that
+                                      //this doesn't need to be
+                                      //mutable.
   };
 
   //
@@ -110,9 +120,12 @@ namespace ISF {
     unsigned short numSec = numberOfSecondaries();
     bool pass = false; // true if cut passed
     // as soon as at a particle passes the cut -> end loop and return true
-    for ( unsigned short i=0; (!pass) && (i<numSec); ++i) {
-      pass = (secondaryP2(i) >= p2cut);
+    for ( unsigned short i=0; (!pass && m_passWholeVertex) && (i<numSec); ++i) {
+      bool thispassed = (secondaryP2(i) >= p2cut);
+      if(thispassed) { setSecondaryPassed(i); }
+      pass |= thispassed;
     }
+    m_wholeVertexPassed=m_passWholeVertex && pass;
     return pass;
   }
 
@@ -121,9 +134,12 @@ namespace ISF {
     unsigned short numSec = numberOfSecondaries();
     bool pass = false; // true if cut passed
     // as soon as at a particle passes the cut -> end loop and return true
-    for ( unsigned short i=0; (!pass) && (i<numSec); ++i) {
-      pass = (secondaryPt2(i) >= pt2cut);
+    for ( unsigned short i=0; (!pass && m_passWholeVertex) && (i<numSec); ++i) {
+      bool thispassed = (secondaryPt2(i) >= pt2cut);
+      if(thispassed) { setSecondaryPassed(i); }
+      pass |= thispassed;
     }
+    m_wholeVertexPassed=m_passWholeVertex && pass;
     return pass;
   }
 
@@ -132,13 +148,19 @@ namespace ISF {
     unsigned short numSec = numberOfSecondaries();
     bool pass = false; // true if cut passed
     // as soon as at a particle passes the cut -> end loop and return true
-    for ( unsigned short i=0; (!pass) && (i<numSec); ++i) {
-      pass = (secondaryEkin(i) >= ekincut);
+    for ( unsigned short i=0; (!pass && m_passWholeVertex) && (i<numSec); ++i) {
+      bool thispassed = (secondaryEkin(i) >= ekincut);
+      if(thispassed) { setSecondaryPassed(i); }
+      pass |= thispassed;
     }
+    m_wholeVertexPassed=m_passWholeVertex && pass;
     return pass;
+  }
+
+  void ISF::ITruthIncident::setPassWholeVertices(bool passWholeVertex) {
+    m_passWholeVertex=passWholeVertex;
   }
 
 }
 
 #endif //> !ISF_EVENT_ITRUTHINCIDENT_H
-
