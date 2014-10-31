8844d681e36e7161b427dc7e0d9d46e290f9e799
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2014-10-31 19:34:55 +0100

    add EntryLayerFilter to ISF_Flags for MC15 truth strategies (ATLASSIM-1584) + move respective parts of ConfGetterDB from ISF_Config into ISF_Tools & ISF_Geant4CommonTools packages (ISF_Config-00-01-05)
    	* ISF_jobProperties.py: add EntryLayerFilter for MC15 truth strategies (ATLASSIM-1584)
    	* move respective parts of ConfGetterDB from ISF_Config into ISF_Tools &
    	ISF_Geant4CommonTools packages
    	* tagging ISF_Config-00-01-05
    
    2014-10-27  Elmar Ritsch  <Elmar.Ritsch@cern.ch>
    
    	* ISF_jobProperties.py: comment out unused ParticleBroker property
    	* add Geant4 configuration for long-lived particle simulation (ATLASSIM-526)
    	* tagging ISF_Config-00-01-04
    
    2014-10-23  Elmar Ritsch  <Elmar.Ritsch@cern.ch>
    
    	* ISF_jobProperties.py: add BarcodeService property
    	* moved all BarcodeService configurations from file ISF_ToolsConfig.py into
    	package Simulation/Barcode/BarcodeService
    	* tagging ISF_Config-00-01-03
    
    2014-10-23  John Chapman  <John.Chapman@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/ISF/ISF_Config/python/ISF_Geant4CommonConfig.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+"""
+Common Geant4 configurations for ISF
+Elmar Ritsch, 04/09/2014
+"""
+
+from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicTool,getPublicToolClone,\
+        getService,getServiceClone,getAlgorithm,getAlgorithmClone
+
+from AthenaCommon.Constants import *  # FATAL,ERROR etc.
+from AthenaCommon.SystemOfUnits import *
+from AthenaCommon.DetFlags import DetFlags
+
+def getAFIIGeant4SimSvc(name="ISF_AFIIGeant4SimSvc", **kwargs):
+    from ISF_Geant4Config.ISF_iGeant4Config import getGeant4SimSvc
+    kwargs.setdefault('ParticleService'       , getService('ISF_AFIIParticleBrokerSvc') )
+    kwargs.setdefault('GeoIDSvc'              , getService('ISF_AFIIGeoIDSvc')          )
+    return getGeant4SimSvc(name, **kwargs)
+
+def getLongLivedGeant4SimSvc(name="ISF_LongLivedGeant4SimSvc", **kwargs):
+    from ISF_Geant4Config.ISF_iGeant4Config import getFullGeant4SimSvc
+    kwargs.setdefault('ParticleService'       , getService('ISF_LongLivedParticleBrokerSvc') )
+    return getFullGeant4SimSvc(name, **kwargs)
