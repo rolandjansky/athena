5516f13cc1a0a957e6f26bbac041cb231cf949b6
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2014-11-19 14:49:38 +0100

    src/FastSimMenu.cxx - tidy up messaging and whitespace cleanup. G4FastSimulation/FastSimModelCatalog.h, src/FastSimModelCatalog.cxx - switch to using MsgStream so that error messages are picked up by transforms. ATLASSIM-1703. cmt/requirements - add use statements for AthenaKernel and AthenaBaseComps. Tagging: G4FastSimulation-00-00-13 (G4FastSimulation-00-00-13)
    	* src/FastSimMenu.cxx - tidy up messaging and whitespace
    	cleanup. ATLASSIM-1703
    	* G4FastSimulation/FastSimModelCatalog.h,
    	src/FastSimModelCatalog.cxx - switch to using MsgStream so that
    	error messages are picked up by transforms. ATLASSIM-1703.
    	* cmt/requirements - add use statements for AthenaKernel and
    	AthenaBaseComps.
    	* Tagging: G4FastSimulation-00-00-13
    
    2014-11-19  Zach Marshall <ZLMarshall@lbl.gov>
    	* Adding the SimpleFastKiller
    	* Tagging: G4FastSimulation-00-00-12

--- Simulation/G4Utilities/G4FastSimulation/G4FastSimulation/FastSimModelCatalog.h
+++ Simulation/G4Utilities/G4FastSimulation/G4FastSimulation/FastSimModelCatalog.h
@@ -6,28 +6,39 @@
 #define FastSimModelCatalog_H
 
 #include "G4FastSimulation/FastSimModel.h"
+#include "AthenaKernel/MsgStreamMember.h"
 #include <map>
 #include <string>
 
 typedef std::map<std::string, FastSimModel*, std::less<std::string> >  fsModelCatalog;
 
-class FastSimModelCatalog 
+class FastSimModelCatalog
 {
- public:
+public:
+  ///
   static FastSimModelCatalog* GetInstance();
-
+  ///
   void RegisterModel(FastSimModel* model);
+  ///
   FastSimModel* GetModel(std::string name);
-	
+  ///
   void PrintAll();
-
+  /// Log a message using the Athena controlled logging system
+  MsgStream& msg( MSG::Level lvl ) const { return m_msg << lvl; }
+  /// Check whether the logging system is active at the provided verbosity level
+  bool msgLvl( MSG::Level lvl ) const { return m_msg.get().level() <= lvl; }
  private:
-  //Constructor
-  inline FastSimModelCatalog();
-  
-  fsModelCatalog theCatalog;
-  static FastSimModelCatalog* thePointer;
 
+
+private:
+  /// Constructor
+  inline FastSimModelCatalog();
+  ///
+  fsModelCatalog theCatalog;
+  ///
+  static FastSimModelCatalog* thePointer;
+  /// Private message stream member
+  mutable Athena::MsgStreamMember m_msg;
 };
 
 #endif
--- Simulation/G4Utilities/G4FastSimulation/src/FastSimModelCatalog.cxx
+++ Simulation/G4Utilities/G4FastSimulation/src/FastSimModelCatalog.cxx
@@ -3,26 +3,30 @@
 */
 
 #include "G4FastSimulation/FastSimModelCatalog.h"
+#include "AthenaBaseComps/AthMsgStreamMacros.h"
 
 FastSimModelCatalog* FastSimModelCatalog::thePointer = 0;
 
-FastSimModelCatalog::FastSimModelCatalog()
+FastSimModelCatalog::FastSimModelCatalog():m_msg("FastSimModelCatalog")
 {
 }
 
 FastSimModelCatalog* FastSimModelCatalog::GetInstance()
 {
-  if (!thePointer) 
+  if (!thePointer) {
     thePointer = new FastSimModelCatalog;
+  }
   return thePointer;
 }
 
 void FastSimModelCatalog::RegisterModel(FastSimModel* model)
 {
   if (theCatalog.find(model->GetName())!=theCatalog.end())
-    std::cout << "Redefining Fast Simulation Model " << model->GetName() << "\n";
-
+    {
+      ATH_MSG_INFO( "Redefining Fast Simulation Model " << model->GetName() );
+    }
   theCatalog[model->GetName()]=model;
+  return;
 }
 
 FastSimModel* FastSimModelCatalog::GetModel(std::string name)
@@ -30,17 +34,18 @@ FastSimModel* FastSimModelCatalog::GetModel(std::string name)
   fsModelCatalog::iterator it = theCatalog.find(name);
   if (it==theCatalog.end())
   {
-    std::cout << "Fast Simulation Model " << name << " not found!\n";
+    ATH_MSG_ERROR( "Fast Simulation Model " << name << " not found!" );
     return 0;
   }
-  else 
-    return (*it).second;
+  return (*it).second;
 }
 
 void FastSimModelCatalog::PrintAll()
 {
-  std::cout<<"Printing the list of fast simulation models:\n";
-  std::cout<<" Number of models :: " << theCatalog.size() << "\n";
-  for (fsModelCatalog::const_iterator it=theCatalog.begin();it!=theCatalog.end();it++)
-    std::cout << " --- model    " << (*it).first << "\n";
+  ATH_MSG_INFO( "Printing the list of fast simulation models:" );
+  ATH_MSG_INFO( " Number of models :: " << theCatalog.size() );
+  for (fsModelCatalog::const_iterator it=theCatalog.begin();it!=theCatalog.end();it++) {
+    ATH_MSG_INFO( " --- model    " << (*it).first );
+  }
+  return;
 }
