e99dcf64f52ab123a3f11cfe67c53fe97e685aa7
Scott Snyder <scott.snyder@cern.ch>
2014-11-20 18:03:27 +0100

    Fix coverity warning. (CombinedScintillator-00-01-09)
    Former-commit-id: caad8b705bce52c8d7d1abea05dc577351299ef5

--- TileCalorimeter/TileG4/TileAncillary/CombinedScintillator/python/CombinedScintillatorConfigDb.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from AthenaCommon.CfgGetter import addTool
-addTool("CombinedScintillatorSD.CombinedScintillatorSDConfig.getCombinedScintillatorSD", "CombinedScintillatorSD" )
-addTool("CombinedScintillatorSD.CombinedScintillatorSDConfig.getCombinedScintillatorTool", "CombinedScintillator")
