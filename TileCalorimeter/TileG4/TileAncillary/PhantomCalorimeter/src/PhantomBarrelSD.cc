a87d6f013573a1a999e1645f769d879dc4456f1d
Scott Snyder <scott.snyder@cern.ch>
2014-11-20 18:12:13 +0100

    Fix coverity warning. (PhantomCalorimeter-00-01-09)
    Former-commit-id: 7a9814257dbd7c607da2f2d8644ddc8855c1043e

--- TileCalorimeter/TileG4/TileAncillary/PhantomCalorimeter/src/PhantomBarrelSD.cc
+++ TileCalorimeter/TileG4/TileAncillary/PhantomCalorimeter/src/PhantomBarrelSD.cc
@@ -12,157 +12,207 @@
 //
 //************************************************************
 
-#include "PhantomBarrelSD.hh"
+#include "PhantomCalorimeter/PhantomBarrelSD.hh"
+#include "PhantomCalorimeter/PhantomBarrelSDMessenger.hh"
+#include "PhantomCalorimeter/PhantomBarrelDescription.hh"
 
-#include "CaloIdentifier/TileTBID.h"
-#include "CxxUtils/make_unique.h"// For make unique
-#include "StoreGate/StoreGateSvc.h"
-#include "TileSimEvent/TileHitVector.h"
-
-#include "GaudiKernel/ISvcLocator.h"
-#include "GaudiKernel/Bootstrap.h"
+#include "TileG4DetDescr/DetectorDescriptionStore.h"
+#include "FadsSensitiveDetector/SensitiveDetectorEntryT.h"
+#include "FadsSensitiveDetector/SensitiveDetectorCatalog.h"
 
 #include "G4HCofThisEvent.hh"
 #include "G4VPhysicalVolume.hh"
 #include "G4Step.hh"
 #include "G4VTouchable.hh"
 #include "G4TouchableHistory.hh"
+#include "G4SDManager.hh"
+#include "G4TransportationManager.hh"
+#include "G4Navigator.hh"
+#include "G4Trd.hh"
 
-PhantomBarrelSD::PhantomBarrelSD(const std::string& name, const std::string& hitCollectionName)
-    : G4VSensitiveDetector(name),
-    m_hit(),
-    m_HitColl(hitCollectionName)
+#include "G4UImanager.hh"
+#include "G4ios.hh"
+
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/IMessageSvc.h"
+#include "StoreGate/StoreGateSvc.h"
+
+#include "CaloIdentifier/TileTBID.h"
+#include "TileSimEvent/TileHitVector.h"
+
+static SensitiveDetectorEntryT<PhantomBarrelSD> phantombarrelSD("PhantomBarrelSD");
+
+PhantomBarrelSD::PhantomBarrelSD(G4String name)   
+  : FadsSensitiveDetector(name),
+    detectorDescription(0),    
+    phantombarrelSDMessenger(0),
+    m_iamowner(false),
+    m_hit()
 {
+  phantombarrelSDMessenger = new PhantomBarrelSDMessenger(this);
   ISvcLocator* svcLocator = Gaudi::svcLocator();
-  StoreGateSvc* detStore(nullptr);
-  if (svcLocator->service("DetectorStore", detStore).isFailure()) {
-    G4ExceptionDescription description;
-    description << "Constructor: DetectorStoreSvc not found!";
-    G4Exception("CombinedScintillatorSD", "NoDetStore", FatalException, description);
-    abort();
-  } else if (verboseLevel > 5) {
-    G4cout << "DetectorStoreSvc initialized" << G4endl;
-  }
+  StatusCode status = svcLocator->service("MessageSvc", m_msgSvc);
+  MsgStream log(m_msgSvc, "PhantomBarrelSD");
 
-  if (detStore->retrieve(m_tileTBID).isFailure()) {
-    G4ExceptionDescription description;
-    description << "Constructor: No TileTBID helper!";
-    G4Exception("CombinedScintillatorSD", "NoTileTBIDHelper", FatalException, description);
-    abort();
-  } else if (verboseLevel > 5) {
-    G4cout << "TileTBID helper retrieved" << G4endl;
-  }
-  int type = TileTBID::ADC_TYPE, module = TileTBID::PHANTOM_CALO;
-  for (int channel = 0; channel < N_CELLS; ++channel) {
-    m_id[channel] = m_tileTBID->channel_id(type, module, channel);
+  if(status.isFailure())
+    log << MSG::FATAL << "MessageSvc not found!" << endreq;
+  else
+    log << MSG::DEBUG << "MessageSvc initialized" << endreq;
+
+  status = svcLocator->service("StoreGateSvc", m_sgSvc);
+  if(status.isFailure())
+    log << MSG::FATAL << "StoreGateSvc not found!" << endreq;
+  else
+    log << MSG::DEBUG << "StoreGateSvc initialized" << endreq;
+
+  status = svcLocator->service("DetectorStore", m_detStore);
+  if(status.isFailure())
+    log << MSG::FATAL << "DetectorStoreSvc not found!" << endreq;
+  else
+    log << MSG::DEBUG << "DetectorStoreSvc initialized" << endreq;
+
+  status = m_detStore->retrieve(m_tileTBID);
+  if(status.isFailure())
+    log << MSG::FATAL << "No TileTBID helper" << endreq;
+  else
+    log << MSG::DEBUG << "TileTBID helper retrieved" << endreq;
+
+  int type=TileTBID::ADC_TYPE, module=TileTBID::PHANTOM_CALO;
+  for (int channel=0; channel<nCell; ++channel) {
+    m_id[channel] = m_tileTBID->channel_id(type,module,channel);
   }
 }
 
-PhantomBarrelSD::~PhantomBarrelSD() {}
-
-void PhantomBarrelSD::StartOfAthenaEvent() {
-  if (verboseLevel > 5) {
-    G4cout << "Initializing SD" << G4endl;
-  }
-
-  memset(m_nhits, 0, sizeof(m_nhits));
-}
-void PhantomBarrelSD::Initialize(G4HCofThisEvent* /* HCE */) {
-  if (verboseLevel > 5) {
-    G4cout << "PhantomBarrelSD::Initialize()" << G4endl;
-  }
-
-#ifdef ATHENAHIVE
-  // Temporary fix for Hive until isValid is fixed
-  m_HitColl = CxxUtils::make_unique<TileHitVector>(m_HitColl.name());
-#else
-
-  if (!m_HitColl.isValid())
-    m_HitColl = CxxUtils::make_unique<TileHitVector>(m_HitColl.name());
-#endif
+PhantomBarrelSD::~PhantomBarrelSD()
+{
+  delete phantombarrelSDMessenger;
 }
 
-G4bool PhantomBarrelSD::ProcessHits(G4Step* aStep, G4TouchableHistory* /* ROhist */) {
-  if (verboseLevel > 10) {
-    G4cout << "PhantomBarrelSD:: ProcessHits" << G4endl;
-  }
+void PhantomBarrelSD::Description(std::string descr_name)
+{
+  MsgStream log(m_msgSvc, "PhantomBarrelSD"); 
+  log << MSG::DEBUG << "PhantomBarrelSD, Description" << endreq; 
+  DetectorDescriptionStore *st=DetectorDescriptionStore::GetDetectorDescriptionStore();
+  DetectorDescription *dd=st->GetDetectorDescription(descr_name);
+  PhantomBarrelDescription* i=dynamic_cast<PhantomBarrelDescription *>(dd);
+  if (i)
+    detectorDescription = i;
+  else
+    log << MSG::FATAL << "Description" << descr_name << "is not of right type PhantomBarrelDescription!" << endreq;
+}
 
-  const G4TouchableHistory* theTouchable = (G4TouchableHistory*)(aStep->GetPreStepPoint()->GetTouchable());
-  const G4VPhysicalVolume* physVol = theTouchable->GetVolume();
-  const G4LogicalVolume* logiVol = physVol->GetLogicalVolume();
-  const G4String nameLogiVol = logiVol->GetName();
-  const G4int nScinti = physVol->GetCopyNo();
+void PhantomBarrelSD::Initialize(G4HCofThisEvent* /* HCE */)
+{
+  MsgStream log(m_msgSvc, "PhantomBarrelSD"); 
+  log << MSG::DEBUG << "Initializing SD" << endreq;
 
-  const G4double edep = aStep->GetTotalEnergyDeposit();
+  memset(m_nhits,0,sizeof(m_nhits));
+}
+
+G4bool PhantomBarrelSD::ProcessHits(G4Step* aStep,G4TouchableHistory* /* ROhist */)
+{
+  MsgStream log(m_msgSvc, "PhantomBarrelSD:: ProcessHits");
+  MSG::Level logLevel = log.level();
+  if (logLevel <= MSG::VERBOSE)
+    log << MSG::VERBOSE << "i am in PhantomBarrel process hit" << endreq; 
+
+  G4TouchableHistory* theTouchable = (G4TouchableHistory*)(aStep->GetPreStepPoint()->GetTouchable());
+  G4VPhysicalVolume* physVol = theTouchable->GetVolume();
+  G4LogicalVolume* logiVol   = physVol->GetLogicalVolume();
+  G4String nameLogiVol       = logiVol->GetName();
+  G4int nScinti              = physVol->GetCopyNo();
+
+  G4double edep  = aStep->GetTotalEnergyDeposit();
   G4double stepl = 0.;
   G4ThreeVector pStep = aStep->GetDeltaPosition();
+  
+  if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.) stepl = aStep->GetStepLength();
+    
+  if ((edep==0.)&&(stepl==0.)) return false;
+  if (logLevel <= MSG::VERBOSE)
+    log << MSG::VERBOSE << "Where " << nameLogiVol      << endreq; 
 
-  if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.) { // FIXME not-equal check on double
-
-    stepl = aStep->GetStepLength();
-  }
-
-  if ((edep == 0.) && (stepl == 0.)) { //FIXME equality check on double
-
-    return false;
-  }
-
-  if (verboseLevel > 10) {
-    G4cout << "Where " << nameLogiVol << G4endl;
-  }
-
-  if (nameLogiVol.find("ScintillatorLayer") != G4String::npos) {
+  if(nameLogiVol.find("ScintillatorLayer") !=G4String::npos) 
+  {
     int ind;
-    G4double scinti = nScinti / 4.;
+    G4double m_Scinti = nScinti / 4.;
 
-    if (scinti <= 1)                 ind = 0;
-    if (scinti <= 2 && scinti > 1)   ind = 1;
-    if (scinti <= 4 && scinti > 2)   ind = 2;
-    if (scinti <= 6 && scinti > 4)   ind = 3;
-    if (scinti <= 8 && scinti > 6)   ind = 4;
-    if (scinti <= 10 && scinti > 8)  ind = 5;
-    if (scinti <= 12 && scinti > 10) ind = 6;
-    if (scinti > 12)                 ind = 7;
+    if(m_Scinti<=1)                 ind=0;
+    if(m_Scinti<=2 && m_Scinti>1)   ind=1;
+    if(m_Scinti<=4 && m_Scinti>2)   ind=2;
+    if(m_Scinti<=6 && m_Scinti>4)   ind=3;
+    if(m_Scinti<=8 && m_Scinti>6)   ind=4;
+    if(m_Scinti<=10 && m_Scinti>8)  ind=5;
+    if(m_Scinti<=12 && m_Scinti>10) ind=6;
+    if(m_Scinti>12)                 ind=7;
 
-    if ( m_nhits[ind] > 0 ) {
-      if (verboseLevel > 10) {
-        G4cout << "Additional hit in CombinedScintillator " << nScinti
-               << " ene=" << edep << G4endl;
-      }
-      m_hit[ind]->add(edep, 0.0, 0.0);
-    } else {
+    if ( m_nhits[ind] > 0 )
+    { 
+      if (logLevel <= MSG::VERBOSE)
+        log << MSG::VERBOSE << "Additional hit in cell " << ind
+            << " ene=" << edep << endreq;
+      m_hit[ind]->add(edep,0.0,0.0);
+    } else
+    {
       // First hit in a cell
-      if (verboseLevel > 10) {
-        G4cout << "First hit in CombinedScintillator " << nScinti
-               << " ene=" << edep << G4endl;
-      }
-      m_hit[ind] = new TileSimHit(m_id[ind], edep, 0.0, 0.0);
+      if (logLevel <= MSG::VERBOSE)
+        log << MSG::VERBOSE << "First hit in cell " << ind
+            << " ene=" << edep << endreq;
+      m_hit[ind] = new TileSimHit(m_id[ind],edep,0.0,0.0);  
     }
     ++m_nhits[ind];
-  }
+  }  
   return true;
 }
 
-void PhantomBarrelSD::EndOfAthenaEvent() {
-  for (int ind = 0; ind < N_CELLS; ++ind) {
-    int nhit = m_nhits[ind];
-    if (nhit > 0) {
-      if (verboseLevel > 5) {
-        G4cout << "Cell id=" << m_tileTBID->to_string(m_id[ind])
-               << " nhit=" << nhit
-               << " ene=" << m_hit[ind]->energy()
-               << G4endl;
-      }
-      m_HitColl->Insert(TileHit(m_hit[ind]));
+void PhantomBarrelSD::EndOfEvent(G4HCofThisEvent* /* HCE */)
+{
+  MsgStream log(m_msgSvc, "PhantomBarrelSD");
+
+  TileHitVector* ExtraHitsCollection;   
+
+  if (! m_iamowner) {
+      const TileHitVector* ExtraColl;
+      StatusCode status = m_sgSvc->retrieve(ExtraColl,"TileTBHits");
+      m_iamowner = (status.isFailure());
+      ExtraHitsCollection = (TileHitVector *)ExtraColl;
+  }
+
+  if (m_iamowner) {
+    ExtraHitsCollection = new TileHitVector("TileTBHits"); 
+    StatusCode status = m_sgSvc->record(ExtraHitsCollection,ExtraHitsCollection->Name(),true);
+    if (status.isFailure())
+      log << MSG::FATAL << "Can't initialize TileTBHits vector" << endreq; 
+    else
+      log << MSG::DEBUG << "I have initialized TileTBHits vector" << endreq; 
+  }
+  
+  for (int ind=0; ind<nCell; ++ind) {
+    int nhit= m_nhits[ind];
+    if (nhit>0) {
+      log << MSG::DEBUG
+          << "Cell id=" << m_tileTBID->to_string(m_id[ind])
+          << " nhit=" << nhit << " ene=" << m_hit[ind]->energy()
+          << endreq;
+      ExtraHitsCollection->Insert(TileHit(m_hit[ind]));
       delete m_hit[ind];
-    } else if (verboseLevel > 10) {
-      G4cout << "Cell id=" << m_tileTBID->to_string(m_id[ind])
-             << " nhit=0" << G4endl;
+    } else {
+      log << MSG::VERBOSE
+          << "Cell id=" << m_tileTBID->to_string(m_id[ind])
+          << " nhit=0"  << endreq;
     }
   }
 
-  if (verboseLevel > 5) {
-    G4cout << "Total number of hits is " << m_HitColl->size() << G4endl;
+  log << MSG::DEBUG << "Total number of hits is " << ExtraHitsCollection->size() << endreq;
+
+  if (m_iamowner) {
+    StatusCode status = m_sgSvc->setConst(ExtraHitsCollection);
+    if(status.isFailure())
+      log << MSG::ERROR << "Can't lock Collection 'TileTBHits'" << endreq;
+    else
+      log << MSG::DEBUG << "Collection 'TileTBHits' is locked " << endreq;
   }
-  return ;
 }
+
--- TileCalorimeter/TileG4/TileAncillary/PhantomCalorimeter/src/components/PhantomCalorimeter_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "../PhantomBarrelSDTool.hh"
-#include "../PhantomBarrelTool.hh"
-
-DECLARE_TOOL_FACTORY( PhantomBarrelSDTool )
-DECLARE_TOOL_FACTORY( PhantomBarrelTool )
