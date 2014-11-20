51f28894614fd75183671a1fa8184ed4e15a04b6
Scott Snyder <scott.snyder@cern.ch>
2014-11-20 16:50:21 +0100

    Update default SG key for TrigEMCluster. (TrigCaloD3PDMaker-00-02-01)
    	* Tagging TrigCaloD3PDMaker-00-02-01.
    	* python/TrigEMClusterD3PDObject.py: Update default SG key.
    
    2014-10-02  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrigCaloD3PDMaker-00-02-00.
    	* Starting to make work with xAOD.

--- PhysicsAnalysis/D3PDMaker/TrigCaloD3PDMaker/python/TrigEMClusterD3PDObject.py
+++ PhysicsAnalysis/D3PDMaker/TrigCaloD3PDMaker/python/TrigEMClusterD3PDObject.py
@@ -1,37 +1,45 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 from D3PDMakerCoreComps.D3PDObject import make_SGDataVector_D3PDObject
-from TrigCaloD3PDMaker.TrigCaloD3PDMakerConf \
-     import D3PD__TrigEMClusterFillerTool, D3PD__TrigCaloClusterFillerTool
+import D3PDMakerCoreComps
 
-TrigEMClusterContainer_sgkey = 'HLT_TrigT2CaloEgamma'
-TrigEMClusterD3PDObject = make_SGDataVector_D3PDObject('TrigEMClusterContainer',
+TrigEMClusterContainer_sgkey = 'HLT_TrigT2CaloEgamma,HLT_xAOD__TrigEMClusterContainer_TrigT2CaloEgamma'
+TrigEMClusterD3PDObject = make_SGDataVector_D3PDObject('DataVector<xAOD::TrigEMCluster_v1>',
                                                        TrigEMClusterContainer_sgkey,
                                                        'trig_L2_emcl_',
                                                        'TrigEMClusterD3PDObject')
 
 # Level 0
-TrigEMClusterD3PDObject.defineBlock(0, 'Basics',
-                                    D3PD__TrigCaloClusterFillerTool,
-                                    WriteBasics=True)
-TrigEMClusterD3PDObject.defineBlock(0, 'Basics1',
-                                    D3PD__TrigEMClusterFillerTool,
-                                    WriteBasics=True)
+TrigEMClusterD3PDObject.defineBlock (
+    0, 'Basics',
+    D3PDMakerCoreComps.AuxDataFillerTool,
+    Vars = ['quality = clusterQuality'])
+TrigEMClusterD3PDObject.defineBlock (
+    0, 'Basics1',
+    D3PDMakerCoreComps.AuxDataFillerTool,
+    Vars = ['E = energy', 'Et = et', 'eta', 'phi'])
 
 # Level 1
-TrigEMClusterD3PDObject.defineBlock(1, 'HypoVars', 
-                                    D3PD__TrigCaloClusterFillerTool,
-                                    WriteHypoVars=True)
-
-TrigEMClusterD3PDObject.defineBlock(1, 'HypoVars1', 
-                                    D3PD__TrigEMClusterFillerTool,
-                                    WriteHypoVars=True)
+TrigEMClusterD3PDObject.defineBlock (
+    1, 'HypoVars1',
+    D3PDMakerCoreComps.AuxDataFillerTool,
+    Vars = ['E237 = e237',
+            'E277 = e277',
+            'fracs1',
+            'weta2',
+            'Ehad1 = ehad1',
+            'eta1',
+            'emaxs1',
+            'e2tsts1',
+            'E233 = e233',
+            'wstot'])
 
 # Level 2
-TrigEMClusterD3PDObject.defineBlock(2, 'Details', 
-                                    D3PD__TrigCaloClusterFillerTool,
-                                    WriteDetails=True)
-
-TrigEMClusterD3PDObject.defineBlock(2, 'Details1', 
-                                    D3PD__TrigEMClusterFillerTool,
-                                    WriteDetails=True)
+TrigEMClusterD3PDObject.defineBlock (
+    2, 'Details',
+    D3PDMakerCoreComps.AuxDataFillerTool,
+    Vars = ['rawEnergy', 'rawEta', 'rawPhi', 'RoIWord = RoIword', 'nCells'])
+TrigEMClusterD3PDObject.defineBlock (
+    2, 'Details1',
+    D3PDMakerCoreComps.AuxDataFillerTool,
+    Vars = ['energyInSample = energySample'])
