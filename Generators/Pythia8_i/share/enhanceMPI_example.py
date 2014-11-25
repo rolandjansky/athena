1b1ef4e74b2d6fe76f12044b74e49e1c3ecea4f0
David Quarrie <drquarrie@lbl.gov>
2014-11-25 12:36:23 +0100

    2014-11-25 David Quarrie <David.Quarrie@cern.ch> 	* cmt/requirements 		Add cmake-specific code to specify the Pythia8 version (transparent to CMT). 	* Tagged as Pythia8_i-00-11-02 (Pythia8_i-00-11-02)
    2014-11-25 David Quarrie <David.Quarrie@cern.ch>
    	* cmt/requirements
    		Add cmake-specific code to specify the Pythia8 version (transparent to CMT).
    	* Tagged as Pythia8_i-00-11-02
    
    2014-06-11  James Monk  <jmonk@cern.ch>
      * Merging trunk with Pythia8_i-00-09-10-branch (for 8.180 series)
    
    2014-05-12  James Monk	<jmonk@cern.ch>
      * main31.cxx: use QRen instead of QFac for SCALUP.  Should have been fixed ~12 months ago!
    
    2014-04-29  James Monk  <jmonk@cern.ch>
      * main31.cxx: allow ptDefMode and pTHardMode user settable.  Defaults unchanged
    
    2014-01-30  James Monk  <jmonk@cern.ch>
      * UserSettings.hh for reading user defined settings
      * UserSettings used in Enhance and Suppress MPI UserHooks
      * Allow user settable number of outgoing partons (nFinal) in main31 hook
    
    2014-01-22  James Monk  <jmonk@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

--- Generators/Pythia8_i/share/enhanceMPI_example.py
+++ Generators/Pythia8_i/share/enhanceMPI_example.py
@@ -5,6 +5,8 @@ evgenConfig.description = "Example QCD jets with MPI enhanced with UserHook"
 evgenConfig.keywords = ["QCD", "MPI"]
 include("MC12JobOptions/Pythia8_AU2_CTEQ6L1_Common.py")
 
+topAlg.Pythia8.UserParams += ["EnhanceMPI:PTCut=20"]
+
 topAlg.Pythia8.Commands += ["HardQCD:all=on",
                             "PhaseSpace:pTHatMin=60"]
 topAlg.Pythia8.UserHook = "EnhanceMPI"
