f18505e7dd8edcc54670d0b2a977f6451c006cf7
Jochen Meyer <Jochen.Meyer@cern.ch>
2014-11-25 14:37:51 +0100

    fixing requirements file (CSCcabling-00-00-13)
            * CSCcabling-00-00-13
            * adding "end_private" in requirements file
    
    2014-11-24 Ignacio Aracena <ignacio.aracena@cern.ch>
    	* CSCcabling-00-00-12
    	* src/CSCcablingSvc.cxx use AtlasCommon node for automatic
    	* configuration of run period and setting of m_run1
    	* remove jobProperty Run1Cabling
    
    2014-11-21 Ignacio Aracena <ignacio.aracena@cern.ch>
    	* CSCcabling-00-00-11
    	* src/CSCcablingSvc.cxx and .h added new jobProperty
    	* Run1Cabling : if True uses cabling as used before
    	* M6 2014 (two chambers/ROB), otherwise new cabling
    	* with one chamber/ROB.
    	* Move from Service to AthService
    
    2014-10-13 Ignacio Aracena <ignacio.aracena@cern.ch>
    	* src/CSCcablingSvc.cxx : implement new CSC ROB ids

--- MuonSpectrometer/MuonCablings/CSCcabling/src/CSCcablingSvc.cxx
+++ MuonSpectrometer/MuonCablings/CSCcabling/src/CSCcablingSvc.cxx
@@ -6,16 +6,20 @@
 #include "GaudiKernel/MsgStream.h"
 
 #include "CSCcabling/CSCcablingSvc.h"
+#include "GeoModelInterfaces/IGeoModelSvc.h"
+
+#include "GeometryDBSvc/IGeometryDBSvc.h"
+#include "RDBAccessSvc/IRDBAccessSvc.h"
+#include "RDBAccessSvc/IRDBRecordset.h"
+#include "RDBAccessSvc/IRDBRecord.h"
 #include <cassert>
 
 // Author: Ketevi A. Assamagan - may 2007
 
 CSCcablingSvc::CSCcablingSvc(const std::string& name, ISvcLocator* sl)
-  : Service(name,sl) {
+  : ::AthService(name,sl), m_side(2), m_rod(16), m_max(32) {
 
-  m_side = 2;
-  m_rod  = 8;
-  m_max  = m_side * m_rod;
+  declareProperty("Run1Cabling", m_run1 = false);
 }
 
 CSCcablingSvc::~CSCcablingSvc() {}
@@ -25,7 +29,7 @@ StatusCode CSCcablingSvc::queryInterface(const InterfaceID& riid, void** ppvIF) 
   if( IID_ICSCcablingSvc.versionMatch(riid) ) { 
       *ppvIF = (CSCcablingSvc*)this; 
   } else { 
-    return Service::queryInterface(riid, ppvIF); 
+    return ::AthService::queryInterface(riid, ppvIF); 
   }
 
   return StatusCode::SUCCESS;
@@ -34,18 +38,54 @@ StatusCode CSCcablingSvc::queryInterface(const InterfaceID& riid, void** ppvIF) 
 StatusCode CSCcablingSvc::initialize() { 
 
   m_log = new MsgStream(msgSvc(), name());
-  *m_log << MSG::DEBUG << " in initialize()" << endreq;
+  *m_log << MSG::INFO << " in initialize() with name " << this->name() << endreq;
   m_debug = m_log->level() <= MSG::DEBUG;
   m_verbose = m_log->level() <= MSG::VERBOSE;
 
-  StatusCode sc;
-
-  sc = Service::initialize();
-
-  if ( sc.isFailure() ) {
-    *m_log << MSG::ERROR << "Service::initialise() failed" << endreq;
-    return sc;
+  if(!::AthService::initialize().isSuccess()){
+    *m_log << MSG::ERROR << "AthService::initialise() failed" << endreq;
+    return StatusCode::FAILURE;
   }
+
+  // Retrieve geometry config information from the database (RUN1, RUN2, etc...)
+  IRDBAccessSvc* rdbAccess = 0;
+  StatusCode result = service("RDBAccessSvc",rdbAccess);
+
+  if(result.isFailure()) {
+     ATH_MSG_ERROR("Unable to get RDBAccessSvc");
+     return result;
+  }
+
+  if(!rdbAccess->connect()) {
+     ATH_MSG_ERROR("Unable to connect to the Geometry DB");
+     return StatusCode::FAILURE;
+  }
+
+  const IGeoModelSvc* geoModel = 0;
+  StatusCode sc = service("GeoModelSvc", geoModel);
+  if (sc.isFailure()) {
+      ATH_MSG_ERROR( "Could not locate GeoModelSvc"  );
+  } else {
+      // check the DetDescr version
+     std::string atlasVersion = geoModel->atlasVersion();
+
+     IRDBRecordset_ptr atlasCommonRec = rdbAccess->getRecordsetPtr("AtlasCommon",atlasVersion,"ATLAS");
+     if(atlasCommonRec->size()==0) {
+        m_run1 = true;
+     } else {
+        std::string configVal = (*atlasCommonRec)[0]->getString("CONFIG");
+        if(configVal=="RUN1"){
+          m_run1 = true;
+        } else if(configVal=="RUN2") {
+          m_run1 = false;
+        } else {
+          ATH_MSG_FATAL("Unexpected value for geometry config read from the database: " << configVal);
+          return StatusCode::FAILURE;
+        }
+     }
+  }
+  m_rod  = (m_run1 ? 8 : 16);
+  m_max  = m_side * m_rod;
 
   return StatusCode::SUCCESS;
 }
@@ -99,6 +139,11 @@ bool CSCcablingSvc::onlineId(const uint16_t offlineID, uint16_t& rodId) const {
 	rodId = 0xFFFF;
    } 
 
+   if((offlineID & 0x1F) & 0x10){
+     check = true;
+     // uses Rod Ids in 0x10 - 0x1F 
+     rodId = 0x80 | (0xF & offlineID);
+   }
    return check;
 }
 
@@ -141,6 +186,15 @@ bool CSCcablingSvc::offlineId(const uint32_t onlineIdentifier, uint16_t& rodId )
 	check = false;
 	rodId = 0xFFFF;
   }
+  // new online Rod 0x80 - 0x8F range Oct. 2014
+  if(!check && (id & 0x70) == 0){
+     check = true;
+     if( id & 1 ) {
+        rodId = (((id & 0xf) >> 1) | 0x10);
+     } else {
+        rodId = (((id & 0xf) >> 1) | 0x18);
+     }
+  }
 
   return check;
 }
@@ -150,6 +204,10 @@ bool CSCcablingSvc::is_rodId (const uint16_t rodId ) const {
 
   bool check = rodId == 0x5  || rodId == 0x7  || rodId == 0x9  || rodId == 0x11 ||
                rodId == 0x15 || rodId == 0x17 || rodId == 0x19 || rodId == 0x21;
+
+  // new online Rod 0x80 - 0x8F range Oct. 2014
+  if((rodId & 0x70) == 0) check = true;
+
   return check;
 }
 
@@ -158,6 +216,9 @@ bool CSCcablingSvc::is_offlineRodId (const uint16_t rodId ) const {
 
   bool check = rodId == 0x0  || rodId == 0x1  || rodId == 0x2  || rodId == 0x3 ||
                rodId == 0x4  || rodId == 0x5  || rodId == 0x6  || rodId == 0x7;
+
+  // new offline Rod range 0x10 - 0x1F Oct. 2014
+  if((0x10 & rodId)==0x10) check = true;
   return check;
 }
 
@@ -170,9 +231,15 @@ uint16_t CSCcablingSvc::collectionId(const uint16_t subDetectorId, const uint16_
   uint16_t subId = (subDetectorId == 0x6A) ? 0 : 1;
   uint16_t onlineColId = subId*this->nROD()+rodId;
 
-  assert ( onlineColId < 16 );
+  // new offline Rod range 0x10 - 0x1F Oct. 2014
+  // double number of rods
+  if((rodId & 0x10) == 0x10){
+      onlineColId = subId*this->nROD() + rodId - 16;
+  } else {
+      onlineColId = subId*this->nROD() + rodId;
+  }
+
+  //assert ( onlineColId < 16 );
   
   return onlineColId;
 }
-
-
