98466070ced8e710eb17c4e4fce7e692d2b61fca
Malcolm Bruce Skinner <malcolm.bruce.skinner@cern.ch>
2014-12-09 10:30:27 +0100

    src/PileupValidation.cxx- updated to correct coverity defect. (PileupRTT-00-03-37)
    Former-commit-id: 412c7f34e08599f5d433e402d4d633b936508308

---
+++ Simulation/PileupRTT/share/PileupValidation_interactive.py
@@ -0,0 +1,4 @@
+from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
+athenaCommonFlags.FilesInput = ["PileupDigiRTTOutput.RDO.pool.root"]
+#athenaCommonFlags.FilesInput = ["tmpRDO.pool.root"]
+include( "PileupRTT/PileupValidation_topOptions.py" )
