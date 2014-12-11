ee7877d4877b1ea28b0123f80c700c3d81a4e0f4
Peter Kluit <s01@nikhef.nl>
2014-12-11 14:11:34 +0100

    'fixed coverity' (CscClusterization-03-00-11)
    Former-commit-id: e9e1358abe4e385380c5f190a2e0df0b2001157c

--- MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/src/SimpleCscClusterFitter.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/src/SimpleCscClusterFitter.cxx
@@ -77,7 +77,7 @@ namespace {
 
 SimpleCscClusterFitter::
 SimpleCscClusterFitter(string type, string aname, const IInterface* parent)
-  : AthAlgTool(type, aname, parent), m_detMgr(nullptr), m_cscIdHelper(nullptr)
+  : AthAlgTool(type, aname, parent)
   , m_alignmentTool("CscAlignmentTool/CscAlignmentTool")
 {
   declareInterface<ICscClusterFitter>(this);
