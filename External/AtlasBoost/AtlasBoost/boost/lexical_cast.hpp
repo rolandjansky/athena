d7e23d2b735c4f664e0c1d6ae628eea2635089cd
Scott Snyder <scott.snyder@cern.ch>
2014-12-15 21:58:10 +0100

    Add a wrapper around lexical_cast to suppress warnings about uninitialized variables. (AtlasBoost-00-00-08)
    	* Tagging AtlasBoost-00-00-08.
    	* Add a wrapper around lexical_cast to suppress warnings about
    	uninitialized variables.

---
+++ External/AtlasBoost/AtlasBoost/boost/lexical_cast.hpp
@@ -0,0 +1,12 @@
+#ifndef ATLAS_BOOST_LEXICAL_CAST_HPP
+#define ATLAS_BOOST_LEXICAL_CAST_HPP
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#pragma GCC system_header
+
+#include_next "boost/lexical_cast.hpp"
+
+#pragma GCC diagnostic pop
+
+#endif
