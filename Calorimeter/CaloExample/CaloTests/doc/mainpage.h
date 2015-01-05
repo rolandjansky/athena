1f13a5ba33629c7882025c871e3fc367bd3b59f9
Scott Snyder <scott.snyder@cern.ch>
2015-01-05 17:06:28 +0100

    Fix coverity and cmt warnings. (CaloTests-00-02-04)

---
+++ Calorimeter/CaloExample/CaloTests/doc/mainpage.h
@@ -0,0 +1,13 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+
+@mainpage CaloTests Package
+
+This package is used to test (RTT) the full chain in the calorimeter (simulation -> digitization -> reconstruction)
+
+@author Karim Bernardet bernardet@cppm.in2p3.fr
+
+*/
