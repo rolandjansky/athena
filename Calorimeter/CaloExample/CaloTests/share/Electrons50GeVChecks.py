826e002315ab2220a4e9ad0882853c284fc9d50c
Scott Snyder <scott.snyder@cern.ch>
2015-01-05 17:06:28 +0100

    Fix coverity and cmt warnings. (CaloTests-00-02-04)

---
+++ Calorimeter/CaloExample/CaloTests/share/Electrons50GeVChecks.py
@@ -0,0 +1,10 @@
+
+doHistoComparison=True
+doTruth=False
+doMemLeak=True
+doextractRec=False
+docheckAODESDcontent=False
+docheckAODESDcontentAOD=False
+docheckAODESDcontentESD=False
+doaddPlotElectronicNoise=False
+
