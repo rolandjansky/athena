10f06554f991ea94d062d8a8eac04389d546ff2e
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-01-09 14:49:18 +0100

    python/ISF_Output.py (ISF_HITSStream): hack to maintain compatibility with both 19.2.X.Y after switch to versionless xAOD containers in release 20.0.0. See ATLASSIM-1746. Tagging: ISF_Example-00-02-46 (ISF_Example-00-02-46)
    	* python/ISF_Output.py (ISF_HITSStream): hack to
    	maintain compatibility with both 19.2.X.Y after switch to
    	versionless xAOD containers in release 20.0.0. See ATLASSIM-1746.
    	* Tagging: ISF_Example-00-02-46

--- Simulation/ISF/ISF_Example/python/ISF_Output.py
+++ Simulation/ISF/ISF_Example/python/ISF_Output.py
@@ -40,9 +40,16 @@ class ISF_HITSStream:
           stream1.ForceRead = True
           stream1.ItemList = ["EventInfo#*",
                               "McEventCollection#TruthEvent",
-                              "JetCollection#*",
-                              "xAOD::JetContainer_v1#*",
-                              "xAOD::JetAuxContainer_v1#*"]
+                              "JetCollection#*"]
+          from PyJobTransforms.trfUtils import releaseIsOlderThan
+          if releaseIsOlderThan(20,0):
+            #Hack to maintain compatibility of G4AtlasApps trunk with
+            #19.2.X.Y after EDM changes in release 20.0.0.
+            stream1.ItemList += ["xAOD::JetContainer_v1#*",
+                                 "xAOD::JetAuxContainer_v1#*"]
+          else:
+            stream1.ItemList += ["xAOD::JetContainer#*",
+                                 "xAOD::JetAuxContainer#*"]
 
           ## Make stream aware of aborted events
           stream1.AcceptAlgs = ['ISF_Kernel_'+ISF_Flags.Simulator()]#,"G4AtlasAlg"]
