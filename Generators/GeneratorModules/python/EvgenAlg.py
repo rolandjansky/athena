15f6f6fa5f49ef30a288854fbf15fc932fd6b193
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-01-15 16:00:13 +0100

    python/EvgenAlg.py (EvgenAlg.execute): fixes for ROOT6 compatibility. ATEAM-82. Tagging: GeneratorModules-03-00-04 (GeneratorModules-03-00-04)
    	* Tagging: GeneratorModules-03-00-04
    
    	* python/EvgenAlg.py (EvgenAlg.execute): fixes for ROOT6
    	compatibility. ATEAM-82.

--- Generators/GeneratorModules/python/EvgenAlg.py
+++ Generators/GeneratorModules/python/EvgenAlg.py
@@ -35,18 +35,17 @@ class EvgenAlg(PyAthena.Alg):
     def execute(self):
         self.msg.debug("Executing [%s]", self.name())
 
-        if self.evtStore.contains(McEventCollection, self.McEventKey):
-            self.msg.debug(self.McEventKey + " found before alg execution!")
-
         ## Retrieve MC event collection or create a new one
         mcevts = None
-        try:
+        if self.evtStore.contains(McEventCollection, self.McEventKey):
+            self.msg.debug(self.McEventKey + " found before alg execution!")
             mcevts = self.evtStore[self.McEventKey]
-        except:
+        else:
+            self.msg.debug("Creating " + self.McEventKey + " before alg execution!")
             mcevts = McEventCollection()
             try:
                 self.evtStore.record(mcevts, self.McEventKey, True, False)
-            except:
+            except: #FIXME should specify the type of exceptions to catch
                 self.evtStore.record(mcevts, self.McEventKey)
         ROOT.SetOwnership(mcevts, False)
 
@@ -55,11 +54,11 @@ class EvgenAlg(PyAthena.Alg):
 
         ## Get the first event from the MCEC, or make a new one
         evt = None
-        try:
-            evt = mcevts[0]
-        except:
+        if 1 > mcevts.size():
             evt = HepMC.GenEvent()
             mcevts.push_back(evt)
+        else:
+            evt = mcevts[0]
         ROOT.SetOwnership(evt, False)
 
         ## Fill/modify the event
