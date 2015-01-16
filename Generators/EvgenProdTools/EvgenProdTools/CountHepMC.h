271bd2bfe794910c55faa20cdc467605ef0f652a
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-01-16 16:45:27 +0100

    share/G4particle_whitelist.txt - add lines for neutrinos. ATLASSIM-1766. Tagging: EvgenProdTools-00-02-02 (EvgenProdTools-00-02-02)
    	* Tagging: EvgenProdTools-00-02-02
    
    	* share/G4particle_whitelist.txt - add lines for
    	neutrinos. ATLASSIM-1766
    
    2014-11-21  David Hall  <david.hall@cern.ch>
    
    	* Tagging: EvgenProdTools-00-02-01
    
    	* LogicalExpressionFilter handles case when _no_ filters are applied (!)
    
    2014-11-17  David Hall  <david.hall@cern.ch>
    
    	* Tagging: EvgenProdTools-00-02-00
    
    	* Fixes AGENE-805
    
    	* Add LogicalExpressionFilter algorithm (written by Will Buttinger),
    	to which generator filters are attached. This stores the stream
    ...
    (Long ChangeLog diff - truncated)

--- Generators/EvgenProdTools/EvgenProdTools/CountHepMC.h
+++ Generators/EvgenProdTools/EvgenProdTools/CountHepMC.h
@@ -10,13 +10,9 @@
 
 /// @brief Count the number of events to pass all algorithms/filters
 ///
-/// CountHepMC counts the number of events / weights to pass algs
-/// in the evgen production chain. Its main use seems to be handling
-/// complex situations like "flattened" weighted jet slice production,
-/// where it is responsible for deciding when to escape the event loop.
-///
-/// @todo This is hideously complex. With the new MC14 evgen alg sequence
-///  structure, can it now be (greatly) simplifed?
+/// CountHepMC counts the number of events to pass algs in the evgen 
+/// production chain. It signals to the AthenaEventLoopMgr service
+/// when the requested number of events are produced.
 ///
 class CountHepMC : public GenBase {
 public:
@@ -33,53 +29,10 @@ private:
   int m_nPass;
   int m_nCount;
 
-  double m_wtpass;
-  double m_wthepmc;
-  double m_wtall;
-
   int m_firstEv;
 
   bool m_corHepMC;
   bool m_corEvtID;
-  bool m_useEventWeight;
-
-  /// Vector of names of streams
-  StringArrayProperty m_streamNames;
-
-  /// Vector of Algorithm stream
-  /// @todo Why *pointers* to vectors?
-  std::vector<Algorithm*>* m_streamAlgs;
-
-  /// Vector of Algorithms that this stream requires
-  /// @todo Why *pointers* to vectors?
-  std::vector<Algorithm*>* m_requireAlgs;
-
-  /// Vector of Algorithms that this stream requires
-  /// @todo Why *pointers* to vectors?
-  std::vector<Algorithm*>* m_vetoAlgs;
-
-  /// Vector of Algorithms that this stream requires
-  /// @todo Why *pointers* to vectors?
-  std::vector<Algorithm*>* m_acceptAlgs;
-
-  /// Decode specified list of Algorithms
-  StatusCode decodeAlgorithms(StringArrayProperty& theNames, std::vector<Algorithm*>* theAlgs);
-  StatusCode decodeStreamAlgs();
-  /// Handler for RequireAlgs Property
-  void streamAlgsHandler(Property&);
-
-  /// Handler for RequireAlgs
-  StatusCode decodeAlgRequires();
-  /// Handler for VetoAlgs
-  StatusCode decodeAlgVetos();
-  /// Handler for AcceptAlgs
-  StatusCode decodeAlgAccepts();
-
-  /// Decode list of Algorithms that this stream is vetoed by
-  bool isEventAccepted() const;
-  bool isTestHepMCPassed() const;
-  bool isStreamRechecked;
-
 };
 
 
