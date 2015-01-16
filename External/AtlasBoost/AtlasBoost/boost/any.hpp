46a7c88dfcf5d1fd1769208133a8b2c0058c3570
Scott Snyder <scott.snyder@cern.ch>
2015-01-16 16:20:47 +0100

    Add wrapper for boost::any to suppress warnings about uninitialized variables. (AtlasBoost-00-00-09)
    	* Tagging AtlasBoost-00-00-09.
    	* AtlasBoost/boost/any.hpp: Wrapper to suppress warnings about
    	uninitialized variables.

---
+++ External/AtlasBoost/AtlasBoost/boost/any.hpp
@@ -0,0 +1,12 @@
+#ifndef ATLAS_BOOST_ANY_HPP
+#define ATLAS_BOOST_ANY_HPP
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#pragma GCC system_header
+
+#include_next "boost/any.hpp"
+
+#pragma GCC diagnostic pop
+
+#endif
