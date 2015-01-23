be8f97b740773778ea221499b692ccabd9e34cf5
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2015-01-23 15:52:14 +0100

    'Add TrigVertexFitter interfaces' (TrigInDetToolInterfaces-00-03-02)
    	* Add TrigVertexFitter interfaces
    	* tagged as TrigInDetToolInterfaces-00-03-02

---
+++ Trigger/TrigTools/TrigInDetToolInterfaces/TrigInDetToolInterfaces/ITrigPrimaryVertexFitter.h
@@ -0,0 +1,27 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef __ITRIG_PRIM_VERTEX_FITTER__
+#define __ITRIG_PRIM_VERTEX_FITTER__
+
+#include<vector>
+#include "GaudiKernel/IAlgTool.h"
+
+class TrigVertex;
+class TrigInDetTrackCollection;
+
+static const InterfaceID IID_ITrigPrimaryVertexFitter("ITrigPrimaryVertexFitter",1,0); 
+
+class ITrigPrimaryVertexFitter: virtual public IAlgTool 
+{
+
+ public:
+  static const InterfaceID& interfaceID() {
+    return IID_ITrigPrimaryVertexFitter;
+  }
+  virtual TrigVertex* fit(const TrigInDetTrackCollection*, double z=0.0) = 0;
+  virtual TrigVertex* fit(const TrigInDetTrackCollection*, std::vector<double>&, double z=0.0) = 0;
+};
+
+#endif
