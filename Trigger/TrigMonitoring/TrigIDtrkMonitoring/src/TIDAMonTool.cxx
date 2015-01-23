6120a77b927fb389222c3652193f3ce383298dc0
Mark Sutton <mark.sutton@cern.ch>
2015-01-23 12:08:08 +0100

    fixes for M7 cosmics (TrigIDtrkMonitoring-00-01-47)
    	* tagged as TrigIDtrkMonitoring-00-01-47
    	* fixes for M7 cosmics
    
    2014-12-01  Mark Sutton <sutt@cern.ch>
    	* tagged as TrigIDtrkMonitoring-00-01-46
    	* modify tier0 monitoring
    
    2014-11-25  Mark Sutton <sutt@cern.ch>
    	* tagged as TrigIDtrkMonitoring-00-01-45
    	* changes for tier0 monitoring

--- Trigger/TrigMonitoring/TrigIDtrkMonitoring/src/TIDAMonTool.cxx
+++ Trigger/TrigMonitoring/TrigIDtrkMonitoring/src/TIDAMonTool.cxx
@@ -78,7 +78,7 @@ TIDAMonTool::TIDAMonTool(const std::string & type, const std::string & name, con
   
   ATH_MSG_DEBUG("TIDAMonTool::TIDAMonTool() " << gDirectory->GetName());
   ATH_MSG_DEBUG("TIDAMonTool::TIDAMonTool() returning: ");
-
+  
 }
 
 
@@ -92,6 +92,8 @@ TIDAMonTool::~TIDAMonTool() {
 
 
 StatusCode TIDAMonTool::init() {
+  
+  //  std::cout << "SUTT  ----- enter init() ----- " << std::endl;
 
   ATH_MSG_DEBUG(" ----- enter init() ----- ");
   ATH_MSG_DEBUG("TIDAMonTool::init() " << gDirectory->GetName());
@@ -109,47 +111,51 @@ StatusCode TIDAMonTool::init() {
 template<typename T>
 std::ostream& operator<<( std::ostream& s, const std::vector<T>& v) {
   for ( unsigned i=0 ; i<v.size() ; i++ ) { 
-    s << "\t" << i << "\t" << v[i] << std::endl;
+    s << "\t" << i << "\t" << v[i] << "\n";
   }
   return s;
 } 
 
 
 #ifdef ManagedMonitorToolBase_Uses_API_201401
-StatusCode TIDAMonTool::book() { 
+StatusCode TIDAMonTool::book() 
 #else
-StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun) { 
+StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun) 
 #endif
+{
+  //  std::cout << "SUTT  ----- enter book() ----- " << std::endl;
+
 
   ATH_MSG_VERBOSE(" ----- enter book() ----- ");
   
   ATH_MSG_DEBUG("TIDAMonTool::book() " << gDirectory->GetName());
     
-  ATH_MSG_DEBUG("TIDAMonTool::book() SUTT buildNtuple " << m_buildNtuple
-        << "\tNewEventBlock " << newEventsBlock 
-		    << "\tNewLumiBlock "  << newLumiBlock 
-		    << "\tNewRun "        << newRun);
+  ATH_MSG_DEBUG("TIDAMonTool::book() " 
+		<< "\tbuildNtuple "   << m_buildNtuple
+		<< "\tNewEventBlock " << newEventsBlock 
+		<< "\tNewLumiBlock "  << newLumiBlock 
+		<< "\tNewRun "        << newRun);
   
   static bool _first = true;
   
   
   if ( _first ) { 
-
+    
 
     /// will modify m_chainNames - should not modify m_ntupleChainNames, as that is a job option, 
     /// and should not be changed
     m_chainNames = m_ntupleChainNames;
-
+    
     std::vector<std::string> chains;
     chains.reserve( m_chainNames.size() );
 
     _first = false;
-
+    
     // track filters 
     // reference (offline) tracks...
     TrackFilter* filterRef = new Filter_Track( m_etaCutOffline, m_d0CutOffline, m_z0CutOffline, m_pTCutOffline,
 					       1, 6, -1, -1,  -2, -2 );
-  
+    
     // test (trigger) tracks...
     TrackFilter* filterTest = new Filter_AcceptAll();
     
@@ -165,7 +171,7 @@ StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun
     // track associators 
     TrackAssociator*  dR_matcher = new   Associator_BestDeltaRMatcher(  "EBdeltaR",    m_matchR ); // this needs to be set correctly
     TrackAssociator* phi_matcher = new Associator_BestDeltaPhiMatcher( "EBdeltaPhi", m_matchPhi ); // this needs to be set correctly
-  
+    
     
     // keep track of the filters so they can be cleaned up at the end
     m_associators.push_back(dR_matcher);
@@ -173,55 +179,55 @@ StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun
     
     /// handle wildcard chain selection - but only the first time                                                                                                                                                                
     std::vector<std::string>::iterator chainitr = m_chainNames.begin();
-   
-      
-    while ( chainitr!=m_chainNames.end() ) {
-	
-	/// get chain                                                                                                                                                                                                              
-	ChainString chainName = (*chainitr);
-	
-	/// check for wildcard ...                                                                                                                                                                                                 
-	if ( chainName.head().find("*")!=std::string::npos ) {
-	  
-	  //                  std::cout << "wildcard chains: " << chainName << std::endl;                                                                                                                                          
-	  
-	  /// delete from vector                                                                                                                                                                                                   
-	  //	  m_chainNames.erase(chainitr);
-	  
-	  /// get matching chains                                                                                                                                                                                                  
-	  std::vector<std::string> selectChains  = m_tdt->getListOfTriggers( chainName.head() );
-	  
-	  //                  std::cout << "selected chains " << selectChains.size() << std::endl;                                                                                                                                 
-	  
-	  if ( selectChains.size()==0 ) msg(MSG::WARNING) << "No chains matched for requested input " << chainName << endreq;
-	 
-	  for ( unsigned iselected=0 ; iselected<selectChains.size() ; iselected++ ) {
-	    
-	      if ( chainName.tail()!="" )    selectChains[iselected] += ":"+chainName.tail();
-	      if ( chainName.extra()!="" )   selectChains[iselected] += ":"+chainName.extra();
-	      if ( chainName.element()!="" ) selectChains[iselected] += ":"+chainName.element();
-	      if ( !chainName.passed() )     selectChains[iselected] += ";DTE";
-	      
-	    /// replace wildcard with actual matching chains ...                                                                                                                                                                   
-	    chains.push_back( selectChains[iselected] );
-	    
-	    ATH_MSG_INFO( "Matching chain " << selectChains[iselected] );
-	    
-	  }
-	}
-	else {
-	  chains.push_back( *chainitr );
-	}
-
-	chainitr++;
-    }
-     
-    m_chainNames = chains;
- 
-
-    for( unsigned i=0 ; i<m_chainNames.size() ; i++ ){
     
-  
+    
+    while ( chainitr!=m_chainNames.end() ) {
+      
+      /// get chain                                                                                                                                                                                                              
+      ChainString chainName = (*chainitr);
+      
+      /// check for wildcard ...                                                                                                                                                                                                 
+      //	if ( chainName.head().find("*")!=std::string::npos ) {
+      
+      //                  std::cout << "wildcard chains: " << chainName << std::endl;                                                                                                                                          
+      
+      /// delete from vector                                                                                                                                                                                                   
+      //	  m_chainNames.erase(chainitr);
+      
+      /// get matching chains                                                                                                                                                                                                  
+      std::vector<std::string> selectChains  = m_tdt->getListOfTriggers( chainName.head() );
+      
+      //                  std::cout << "selected chains " << selectChains.size() << std::endl;                                                                                                                                 
+      
+      //      if ( selectChains.size()==0 ) msg(MSG::WARNING) << "No chains matched for requested input " << chainName << endreq;
+      
+      for ( unsigned iselected=0 ; iselected<selectChains.size() ; iselected++ ) {
+	
+	if ( chainName.tail()!="" )    selectChains[iselected] += ":"+chainName.tail();
+	if ( chainName.extra()!="" )   selectChains[iselected] += ":"+chainName.extra();
+	if ( chainName.element()!="" ) selectChains[iselected] += ":"+chainName.element();
+	if ( !chainName.passed() )     selectChains[iselected] += ";DTE";
+	
+	/// replace wildcard with actual matching chains ...                                                                                                                                                                   
+	chains.push_back( selectChains[iselected] );
+	
+	ATH_MSG_INFO( "Matching chain " << selectChains[iselected] );
+	
+      }
+      //  }
+      //	else {
+      //	  chains.push_back( *chainitr );
+      //	}
+      
+      chainitr++;
+    }
+    
+    m_chainNames = chains;
+    
+    
+    for( unsigned i=0 ; i<m_chainNames.size() ; i++ ){
+      
+      
       m_sequences.push_back( new AnalysisConfig_Tier0(m_chainNames[i], 
 						      m_chainNames[i], "", "",
 						      m_chainNames[i], "", "",
@@ -230,9 +236,12 @@ StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun
 						      dR_matcher,
 						      new Analysis_Tier0(m_chainNames.at(i), 1000., 2.5, 1.5, 1.5 ) ) );
       
+ 
+
+      /// Why this extra for the electrons ???      
       if(m_chainNames.at(i).find("L2_e")!=std::string::npos ||
 	 m_chainNames.at(i).find("EF_e")!=std::string::npos ||
-	m_chainNames.at(i).find("HLT_e")!=std::string::npos)
+	 m_chainNames.at(i).find("HLT_e")!=std::string::npos) { 
 	
 	m_sequences.push_back( new AnalysisConfig_Tier0(m_chainNames[i],
 							m_chainNames[i], "mediumPP", "",
@@ -241,15 +250,18 @@ StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun
 							filterTest, filterRef,
 							dR_matcher,
 							new Analysis_Tier0(m_chainNames.at(i), 1000., 2.5, 1.5, 1.5 ) ) );
+	
+      }
+      
       
     }
     
-
   }
-  
+
   if ( newRun && m_first_time) { 
     for ( unsigned i=0 ; i<m_sequences.size() ; i++ ) { 
       ATH_MSG_INFO(" ----- booking for analysis " << m_sequences[i]->name() << " -----");
+      m_sequences[i]->keepAllEvents(true);
       m_sequences[i]->initialize(this, &m_tdt);
       m_sequences[i]->setGenericFlag(m_genericFlag);
       m_sequences[i]->book();      
@@ -261,8 +273,38 @@ StatusCode TIDAMonTool::book(bool newEventsBlock, bool newLumiBlock, bool newRun
   return StatusCode::SUCCESS; 
 }
 
+
+
+
 StatusCode TIDAMonTool::fill() { 
-  ATH_MSG_VERBOSE(" ----- enter fill() ----- ");
+  ATH_MSG_INFO(" ----- enter fill() ----- ");
+
+  //  std::cout << "SUTT  ----- enter fill() ----- " << std::endl;
+
+  /// print ou trigger decisions
+  std::vector<std::string> selectChains  = m_tdt->getListOfTriggers( "HLT_.*" );
+
+  //  std::cout << "SUTT Configured chains " << selectChains.size() << std::endl;
+  ATH_MSG_INFO( "SUTT Configured chains " << selectChains.size() );
+
+  int passed_count = 0;
+
+  for ( unsigned i=0 ; i<selectChains.size() ; i++ ) { 
+    if ( m_tdt->isPassed(selectChains[i]) ) {
+      passed_count++;
+      ATH_MSG_DEBUG( "chain " << selectChains[i] << "\tpass: " << m_tdt->isPassed(selectChains[i]) << "\tprescale: " << m_tdt->getPrescale(selectChains[i]) );
+    }
+  }
+
+  //  std::cout << "SUTT Chains passed: " << passed_count << std::endl;
+  ATH_MSG_VERBOSE( "SUTT Chains passed: " << passed_count );
+
+  //  for ( unsigned i=0 ; i<selectChains.size() ; i++ ) { 
+  //    if ( m_tdt->isPassed(selectChains[i]) ) {
+  //     std::cout << "SUTT chain " << selectChains[i] << "\tpass: " << m_tdt->isPassed(selectChains[i]) << "\tprescale: " << m_tdt->getPrescale(selectChains[i]) << std::endl;
+  //    }
+  //  }
+
   
   for( unsigned i=0 ; i<m_sequences.size() ; i++ ) m_sequences[i]->execute();
   
@@ -272,19 +314,19 @@ StatusCode TIDAMonTool::fill() {
 }
 
 #ifdef ManagedMonitorToolBase_Uses_API_201401
-StatusCode TIDAMonTool::proc() { 
+StatusCode TIDAMonTool::proc() 
 #else
-StatusCode TIDAMonTool::proc(bool /*endOfEventsBlock*/, bool /*endOfLumiBlock*/, bool endOfRun) {
+StatusCode TIDAMonTool::proc(bool /*endOfEventsBlock*/, bool /*endOfLumiBlock*/, bool endOfRun)
 #endif
-
-  ATH_MSG_VERBOSE(" ----- enter proc() ----- ");
+{
   
+  ATH_MSG_VERBOSE(" ----- enter proc() ----- ");  
   
   if(endOfRun) {
     for( unsigned i=0 ; i<m_sequences.size() ; i++ ) m_sequences[i]->finalize();
   }
   
-  ATH_MSG_VERBOSE(" ====== exit proc() ====== ");
+  ATH_MSG_VERBOSE(" ------ exit proc() ------ ");
 
   return StatusCode::SUCCESS; 
 }
