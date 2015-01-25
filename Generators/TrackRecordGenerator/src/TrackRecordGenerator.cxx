7cee71367b657cd71ab4e44f1ed5a2e7b37e9b38
Zachary Louis Marshall <zach.marshall@cern.ch>
2015-01-25 17:32:30 +0100

    Migration of TrackRecordCollection to AtlasHitsVector (TrackRecordGenerator-00-00-01)
    	* Migration of TrackRecordCollection to AtlasHitsVector
    	* Tagging: TrackRecordGenerator-00-00-01

--- Generators/TrackRecordGenerator/src/TrackRecordGenerator.cxx
+++ Generators/TrackRecordGenerator/src/TrackRecordGenerator.cxx
@@ -21,7 +21,6 @@
 #include <string>
 #include <fstream>
 
-
 //--------------------------------------------------------------------------
 TrackRecordGenerator::TrackRecordGenerator(const std::string& name, ISvcLocator* pSvcLocator)
    : GenModule(name,pSvcLocator)
@@ -56,27 +55,23 @@ StatusCode TrackRecordGenerator::callGenerator() {
 
   //const DataHandle <TimedTrackRecordCollection> coll;
   const DataHandle <TrackRecordCollection> coll;
-  StatusCode sc = evtStore()->retrieve(coll,m_recordName);
-  if (sc.isFailure()) {
-    ATH_MSG_ERROR(" Cannot retrieve TrackRecordCollection ");
-    return StatusCode::FAILURE;
-  }
+  CHECK( evtStore()->retrieve(coll,m_recordName) );
 
   ATH_MSG_INFO("retrieved "<<coll->size()<<" TTR hits; will smear position by "<< (m_smearTR>0?m_smearTR:0.) <<" mm and momentum by "<< (m_smearTRp>0?m_smearTRp:0.) <<" radians");
 
   //TimedTrackRecordCollection::const_iterator iterTTR;
   TrackRecordCollection::const_iterator iterTTR;
-  for (iterTTR=coll->begin();iterTTR!=coll->end();++iterTTR) {
+  for (auto iterTTR : *coll) {
 
-    const HepPDT::ParticleData* particle = particleData(abs((*iterTTR)->GetPDGCode()));
+    const HepPDT::ParticleData* particle = particleData(abs(iterTTR.GetPDGCode()));
     double mass = particle->mass().value();
-    double en = std::sqrt(mass*mass+(*iterTTR)->GetMomentum().mag2());
+    double en = std::sqrt(mass*mass+iterTTR.GetMomentum().mag2());
 
-    ATH_MSG_VERBOSE("Reading back TTR:\n pos is "<<(*iterTTR)->GetPosition()
-                  <<"\n mom is "<<(*iterTTR)->GetMomentum()
-                  <<"\n pdg is "<<(*iterTTR)->GetPDGCode() );
+    ATH_MSG_VERBOSE("Reading back TTR:\n pos is "<<iterTTR.GetPosition()
+                  <<"\n mom is "<<iterTTR.GetMomentum()
+                  <<"\n pdg is "<<iterTTR.GetPDGCode() );
 
-    CLHEP::HepLorentzVector particle4Position( (*iterTTR)->GetPosition(), (*iterTTR)->GetTime());
+    CLHEP::HepLorentzVector particle4Position( iterTTR.GetPosition(), iterTTR.GetTime());
 
     ATH_MSG_DEBUG( "Smearing position by up to " << m_smearTR << " mm and momentum by up to " << m_smearTRp << " radians" );
     if (m_smearTR>0){
@@ -97,7 +92,7 @@ StatusCode TrackRecordGenerator::callGenerator() {
       }
       ATH_MSG_DEBUG( "Shifted track record to " << particle4Position );
     }
-    CLHEP::HepLorentzVector particle4Momentum( (*iterTTR)->GetMomentum(), en );
+    CLHEP::HepLorentzVector particle4Momentum( iterTTR.GetMomentum(), en );
     if (m_smearTRp>0){
       ATH_MSG_DEBUG( "Track record momentum was " << particle4Momentum );
 
@@ -162,7 +157,7 @@ StatusCode TrackRecordGenerator::callGenerator() {
     m_fourPos.push_back( particle4Position );
     m_fourMom.push_back( particle4Momentum );
 
-    m_pdgCode.push_back((*iterTTR)->GetPDGCode());
+    m_pdgCode.push_back(iterTTR.GetPDGCode());
     HepMC::Polarization thePolarization;
     thePolarization.set_normal3d(HepGeom::Normal3D<double>(0,0,0));
     m_polarization.push_back(thePolarization);
