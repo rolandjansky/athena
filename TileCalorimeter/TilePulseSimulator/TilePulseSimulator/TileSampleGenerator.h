4adef8296467ad8a7d7f07b41b07a78b541fdf7f
Damian Alvarez Piqueras <damian.alvarez.piqueras@cern.ch>
2015-01-26 18:36:45 +0100

    Moving default values of methods to header (TilePulseSimulator-00-00-09)
    	* Moving default values of methods to header instead of cxx
    	* Tagged as TilePulseSimulator-00-00-09
    
    2015-01-26  Damian Alvarez  <damian.alvarez.piqueras@cern.ch>
    	* Removing all references to Gain, only sum of amplitudes
    	* Set m_DEBUG as a variable to be called in constructor for
    	      debugging the sum of pulses
    	* Tagged as TilePulseSimulator-00-00-08
    
    2014-12-18  Damian Alvarez  <damian.alvarez.piqueras@cern.ch>
    	* Added function 'fill7Samples':
    		Sums contributions from *several* Pile-up pulses in each readout sample
    	* Tagged as TilePulseSimulator-00-00-07
    
    2014-10-27  David Quarrie <David.Quarrie@cern.ch>
    	* cmt/requirements
    		Specify the required ROOT components for cmake (transparent to CMT)
    	* Tagged as TilePulseSimulator-00-00-06

--- TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TileSampleGenerator.h
+++ TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TileSampleGenerator.h
@@ -17,15 +17,17 @@ class TRandom3;
 class TileSampleGenerator : public TObject{
  public:
   TileSampleGenerator();
-  TileSampleGenerator(TilePulseShape* ps, TileSampleBuffer* buf);
+  TileSampleGenerator(TilePulseShape* ps, TileSampleBuffer* buf, bool dd_DEBUG=false);
   virtual ~TileSampleGenerator();
 
   inline void setPulseShape(TilePulseShape* ps){_ps=ps;}
-  void fillSamples(double t0, double pedestal, double amplitude1, double amplitude2, TF1* pdf, bool addNoise, double itOffset, double otOffset);
+  void fillSamples(double t0, double pedestal, double amplitude1, double amplitude2, TF1* pdf, bool addNoise, double itOffset = 0, double otOffset = 50);
+  void fill7Samples(double t0, double pedestal, double amp_it, std::vector<float> amp_pu, TF1* pdf, bool addNoise, double itOffset= 0);
 
  private:
   TilePulseShape* _ps;
   TileSampleBuffer* _buf;
+  bool m_DEBUG;
 
  public:
 
