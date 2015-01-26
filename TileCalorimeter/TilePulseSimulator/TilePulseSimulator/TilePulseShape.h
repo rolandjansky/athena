d22d293340abad017e18beac1cb5a87da23d44dd
Damian Alvarez Piqueras <damian.alvarez.piqueras@cern.ch>
2015-01-26 18:36:45 +0100

    Moving default values of methods to header (TilePulseSimulator-00-00-09)
    Former-commit-id: f75109535f2e9986d3c10cbb83333c97a6ba278f

--- TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TilePulseShape.h
+++ TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TilePulseShape.h
@@ -2,14 +2,15 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TILEPULSESIMULATOR_TILEPULSESHAPE_H
-#define TILEPULSESIMULATOR_TILEPULSESHAPE_H
+#ifndef __TILEPULSESHAPE__HPP
+#define __TILEPULSESHAPE__HPP
 
 #include "TObject.h"
 #include "TString.h"
+#include "TF1.h"
 #include <vector>
 
-class TF1;
+
 class TGraph;
 class TSpline;
 class MsgStream;
@@ -17,12 +18,12 @@ class IMessageSvc;
 
 class TilePulseShape : public TObject{
  public:
-  TilePulseShape(IMessageSvc* msgSvc, const std::string& name);
-  TilePulseShape(IMessageSvc* msgSvc, const std::string& name, const TString& fileName);
-  TilePulseShape(IMessageSvc* msgSvc, const std::string& name, const std::vector<double>& shapevec);
+  TilePulseShape(IMessageSvc* msgSvc, std::string name);
+  TilePulseShape(IMessageSvc* msgSvc, std::string name, TString fileName);
+  TilePulseShape(IMessageSvc* msgSvc, std::string name, std::vector<double> shapevec);
   virtual ~TilePulseShape();
-  void loadPulseShape(const TString& fileName);
-  void setPulseShape(const std::vector<double>& shapevec);
+  void loadPulseShape(TString fileName);
+  void setPulseShape(std::vector<double> shapevec);
   
   //=== access to the underlying graph
   TGraph* getGraph(double t0=0., double ped=0., double amp=1.);
@@ -33,11 +34,11 @@ class TilePulseShape : public TObject{
   int scalePulse(double leftSF=1., double rightSF=1.);
   
  private:
-  TGraph* m_pulseShape;
-  TGraph* m_deformedShape;
-  TSpline* m_deformedSpline;
+  TGraph* _pulseShape;
+  TGraph* _deformedShape;
+  TSpline* _deformedSpline;
   
   MsgStream* m_log;
 };
 
-#endif // TILEPULSESIMULATOR_TILEPULSESHAPE_H
+#endif
--- TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TileSampleGenerator.h
+++ TileCalorimeter/TilePulseSimulator/TilePulseSimulator/TileSampleGenerator.h
@@ -2,38 +2,36 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TILEPULSESIMULATOR_TILESAMPLEGENERATOR_H
-#define TILEPULSESIMULATOR_TILESAMPLEGENERATOR_H
+#ifndef TILESAMPLEGENERATOR__HPP__
+#define TILESAMPLEGENERATOR__HPP__
 
 #include "TObject.h"
-
+#include "TH1F.h"
+#include "TF1.h"
 
 class TilePulseShape;
 class TileSampleBuffer;
 class TRandom3;
-class TF1;
 
-class TileSampleGenerator: public TObject {
-public:
-	TileSampleGenerator();
-	TileSampleGenerator(TilePulseShape* ps, TileSampleBuffer* buf, bool dd_DEBUG = false);
-	virtual ~TileSampleGenerator();
 
-	inline void setPulseShape(TilePulseShape* ps) {
-		m_ps = ps;
-	}
-	void fillSamples(double t0, double pedestal, double amplitude1, double amplitude2, TF1* pdf, bool addNoise, double itOffset = 0, double otOffset = 50);
-	void fill7Samples(double t0, double pedestal, double amp_it, std::vector<float> amp_pu, TF1* pdf, bool addNoise, double itOffset = 0);
-	void fill7SamplesQIE(float amp_it, float *amp_pu); //The function calculates charges for 7 intervals with each interval of 25 ns
+class TileSampleGenerator : public TObject{
+ public:
+  TileSampleGenerator();
+  TileSampleGenerator(TilePulseShape* ps, TileSampleBuffer* buf, bool dd_DEBUG=false);
+  virtual ~TileSampleGenerator();
 
-private:
-	TilePulseShape* m_ps;
-	TileSampleBuffer* m_buf;
-	bool m_DEBUG;
-	TRandom3* m_rndm;
+  inline void setPulseShape(TilePulseShape* ps){_ps=ps;}
+  void fillSamples(double t0, double pedestal, double amplitude1, double amplitude2, TF1* pdf, bool addNoise, double itOffset = 0, double otOffset = 50);
+  void fill7Samples(double t0, double pedestal, double amp_it, std::vector<float> amp_pu, TF1* pdf, bool addNoise, double itOffset= 0);
 
-public:
+ private:
+  TilePulseShape* _ps;
+  TileSampleBuffer* _buf;
+  bool m_DEBUG;
+
+ public:
 
 };
 
-#endif // TILEPULSESIMULATOR_TILESAMPLEGENERATOR_H
+
+#endif
