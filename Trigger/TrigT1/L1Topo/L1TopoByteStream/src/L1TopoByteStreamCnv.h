54a17a24d20d6ad939452c7eaafd727649ac78ca
Simon George <s.george@rhul.ac.uk>
2015-01-26 12:14:13 +0100

    'Create package' (L1TopoByteStream-00-00-01)

---
+++ Trigger/TrigT1/L1Topo/L1TopoByteStream/src/L1TopoByteStreamCnv.h
@@ -0,0 +1,83 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef L1TOPOBYTESTREAM_L1TOPOBYTESTREAMCNV_H
+#define L1TOPOBYTESTREAM_L1TOPOBYTESTREAMCNV_H
+
+// Gaudi/Athena include(s):
+#include "GaudiKernel/Converter.h"
+#include "GaudiKernel/CnvFactory.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "GaudiKernel/ToolHandle.h"
+
+#include "ByteStreamCnvSvcBase/ROBDataProviderSvc.h"
+#include "ByteStreamCnvSvcBase/IByteStreamEventAccess.h"
+
+// Local include(s):
+#include "L1TopoByteStreamTool.h"
+
+// Forward declaration(s):
+class L1TopoSrcIdMap;
+
+// External(s):
+extern long ByteStream_StorageType;
+
+/**
+ *   @short ByteStream converter for the L1TopoRDOCollection
+ *
+ *          This is a quite standard BS converter for the output sent from
+ *          L1Topo to its ROS. It uses a separate tool
+ *          (L1TopoByteStreamTool) to do the actual work of the conversion,
+ *          this converter is just to communicate with the framework.
+ *          It was based on CTPByteStreamCnv by David Berge 
+ *          and the L1Calo converters by Alan Watson and Peter Faulkner.
+ *
+ *     @see L1TopoByteStreamTool
+ *
+ *  @author Simon George
+ *    @date $Date: 2014-11-12 00:00:00 $
+ */
+class L1TopoByteStreamCnv : public Converter {
+
+   friend class CnvFactory< L1TopoByteStreamCnv >;
+
+ protected:
+   /// Standard constructor
+   L1TopoByteStreamCnv( ISvcLocator* svcloc );
+
+ public:
+   /// Standard destructor
+   ~L1TopoByteStreamCnv();
+
+   /// Function connecting to all the needed services/tools
+   virtual StatusCode initialize();
+   /// Function creating the L1TopoRDOCollection object from a L1Topo ROB fragment
+   virtual StatusCode createObj( IOpaqueAddress* pAddr, DataObject*& pObj );
+   /// Function creating the L1Topo ROB fragment from a L1TopoRDOCollection object
+   virtual StatusCode createRep( DataObject* pObj, IOpaqueAddress*& pAddr );
+
+   /// Function needed by the framework
+   virtual long repSvcType() const { return ByteStream_StorageType; }
+   /// Function needed by the framework
+   static long storageType() { return ByteStream_StorageType; }
+   /// Function needed by the framework
+   static const CLID& classID();
+
+ private:
+   /// Tool doing the actual conversion
+   ToolHandle< L1TopoByteStreamTool > m_tool;
+
+   /// Object storing the various IDs of the CTP fragment
+   L1TopoSrcIdMap* m_srcIdMap;
+
+   /// Service used when reading the BS data
+   ServiceHandle< IROBDataProviderSvc >    m_robDataProvider;
+   /// Service used when writing the BS data
+   ServiceHandle< IByteStreamEventAccess > m_ByteStreamEventAccess;
+
+ }; // class L1TopoByteStreamCnv
+
+#endif // L1TOPOBYTESTREAM_L1TOPOBYTESTREAMCNV_H
---
+++ Trigger/TrigT1/L1Topo/L1TopoByteStream/src/L1TopoSrcIdMap.cxx
@@ -0,0 +1,46 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#include "eformat/SourceIdentifier.h"
+
+#include "L1TopoSrcIdMap.h"
+
+using eformat::helper::SourceIdentifier;
+
+
+L1TopoSrcIdMap::L1TopoSrcIdMap() {
+
+}
+
+uint32_t L1TopoSrcIdMap::getRodID(int slink, int module, int daqOrRoi)
+{
+  // module ID = rmmmssss
+  uint16_t moduleId = (daqOrRoi << 7) | (module << 4) | slink;
+  SourceIdentifier helpID( eformat::TDAQ_CALO_TOPO_PROC, moduleId);
+  return helpID.code();
+}
+
+uint32_t L1TopoSrcIdMap::getRodID() {
+  SourceIdentifier helpID( eformat::TDAQ_CALO_TOPO_PROC, 0 );
+  return helpID.code();
+}
+
+uint32_t L1TopoSrcIdMap::getRobID( uint32_t rod_id ) {
+  //SourceIdentifier helpID( eformat::TDAQ_CALO_TOPO_PROC, 0 );
+  //return helpID.code();
+  return rod_id;
+}
+
+uint32_t L1TopoSrcIdMap::getRosID( uint32_t rob_id ) {
+  SourceIdentifier id = SourceIdentifier( rob_id );
+  SourceIdentifier id2 = SourceIdentifier( id.subdetector_id(), 0 );
+  return id2.code();
+}
+
+uint32_t L1TopoSrcIdMap::getDetID( uint32_t ros_id ) {
+  SourceIdentifier id = SourceIdentifier( ros_id );
+  SourceIdentifier id2 = SourceIdentifier( id.subdetector_id(), 0 );
+  return id2.code();
+}
