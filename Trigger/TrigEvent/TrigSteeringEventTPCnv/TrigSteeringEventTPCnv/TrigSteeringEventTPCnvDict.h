5a81ea70287f91844d73e8dfc02f62af702a483c
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-01-29 22:37:32 +0100

    Clean up dictionaries (removing TP converter entries) to optimize ROOT6 header parsing and VMEM consumption (TrigSteeringEventTPCnv-00-01-30)
    	* tagged as TrigSteeringEventTPCnv-00-01-30
    	* Clean up dictionaries (removing TP converter entries) to
    	optimize ROOT6 header parsing and VMEM consumption

--- Trigger/TrigEvent/TrigSteeringEventTPCnv/TrigSteeringEventTPCnv/TrigSteeringEventTPCnvDict.h
+++ Trigger/TrigEvent/TrigSteeringEventTPCnv/TrigSteeringEventTPCnv/TrigSteeringEventTPCnvDict.h
@@ -2,53 +2,36 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TRIGSTEERINGEVENTTPCNV_TRIGSTEERINGEVENTTPCNVDICT1_H
-#define TRIGSTEERINGEVENTTPCNV_TRIGSTEERINGEVENTTPCNVDICT1_H
+#ifndef TRIGSTEERINGEVENTTPCNV_TRIGSTEERINGEVENTTPCNVDICT_H
+#define TRIGSTEERINGEVENTTPCNV_TRIGSTEERINGEVENTTPCNVDICT_H
 
 #include "TrigSteeringEventTPCnv/HLTResult_p1.h"
-#include "TrigSteeringEventTPCnv/HLTResultCnv_p1.h"
 #include "TrigSteeringEventTPCnv/Lvl1Result_p1.h"
 #include "TrigSteeringEventTPCnv/Lvl1Result_p2.h"
 
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollection_tlp1.h"
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollectionCnv_tlp1.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollection_p1.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptor_p1.h"
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCnv_p1.h"
 
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollectionCnv_p2.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollection_p2.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptor_p2.h"
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCnv_p2.h"
 
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollectionCnv_p3.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptorCollection_p3.h"
 #include "TrigSteeringEventTPCnv/TrigRoiDescriptor_p3.h"
-#include "TrigSteeringEventTPCnv/TrigRoiDescriptorCnv_p3.h"
 
 
 #include "TrigSteeringEventTPCnv/TrigOperationalInfoCollection_tlp1.h"
-#include "TrigSteeringEventTPCnv/TrigOperationalInfoCollectionCnv_tlp1.h"
 #include "TrigSteeringEventTPCnv/TrigOperationalInfoCollection_p1.h"
 #include "TrigSteeringEventTPCnv/TrigOperationalInfo_p1.h"
 
-#include "TrigSteeringEventTPCnv/TrigPassBitsCollectionCnv_p1.h"
 #include "TrigSteeringEventTPCnv/TrigPassBitsCollection_p1.h"
 #include "TrigSteeringEventTPCnv/TrigPassBits_p1.h"
-#include "TrigSteeringEventTPCnv/TrigPassBitsCnv_p1.h"
 
-#include "TrigSteeringEventTPCnv/TrigPassFlagsCollectionCnv_p1.h"
 #include "TrigSteeringEventTPCnv/TrigPassFlagsCollection_p1.h"
 #include "TrigSteeringEventTPCnv/TrigPassFlags_p1.h"
-#include "TrigSteeringEventTPCnv/TrigPassFlagsCnv_p1.h"
-
-//#include "TrigSteeringEventTPCnv/HLTAODConfigData_p1.h"
-//#include "TrigSteeringEventTPCnv/Lvl1AODConfigData_p1.h"
-//#include "TrigSteeringEventTPCnv/Lvl1AODPrescaleConfigData_p1.h"
 
 
-
-struct dummy_TrigSteeringEventTPCnvDict1
+struct dummy_TrigSteeringEventTPCnvDict
 {
 
   std::vector<TrigRoiDescriptor_p1> 	      m_dummyTrigRoiDescriptor_p1;
