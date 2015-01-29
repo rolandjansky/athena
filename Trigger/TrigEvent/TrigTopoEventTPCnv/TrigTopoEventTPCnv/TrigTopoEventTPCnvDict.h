fe4fe1293d578abf76165677ad65f566a7586ba5
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-01-29 22:25:58 +0100

    Clean up dictionaries (removing TP converter entries) to optimize ROOT6 header parsing and VMEM consumption (TrigTopoEventTPCnv-00-00-03)
    	* Clean up dictionaries (removing TP converter entries) to
    	optimize ROOT6 header parsing and VMEM consumption
    	* TrigTopoEventTPCnv-00-00-03

--- Trigger/TrigEvent/TrigTopoEventTPCnv/TrigTopoEventTPCnv/TrigTopoEventTPCnvDict.h
+++ Trigger/TrigEvent/TrigTopoEventTPCnv/TrigTopoEventTPCnv/TrigTopoEventTPCnvDict.h
@@ -2,17 +2,15 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TRIGTOPOEVENTTPCNV_TRIGTOPOEVENTTPCNVDICT1_H
-#define TRIGTOPOEVENTTPCNV_TRIGTOPOEVENTTPCNVDICT1_H
+#ifndef TRIGTOPOEVENTTPCNV_TRIGTOPOEVENTTPCNVDICT_H
+#define TRIGTOPOEVENTTPCNV_TRIGTOPOEVENTTPCNVDICT_H
 
 /* ElectronMuonTopoInfo T/P classes: persistent version 1*/
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfoContainer_p1.h"
-#include "TrigTopoEventTPCnv/ElectronMuonTopoInfoContainerCnv_p1.h"
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfo_p1.h"
-#include "TrigTopoEventTPCnv/ElectronMuonTopoInfoCnv_p1.h"
 
 //dummy instances of all objects using std::vector<TPObjRef> for the dictionary
-struct dummy_TrigTopoEventTPCnvDict1
+struct dummy_TrigTopoEventTPCnvDict
 {
   std::vector<ElectronMuonTopoInfo_p1>                     m_dummyElectronMuonTopoInfo_p1;
 };
