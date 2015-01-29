77c456b72125085fdec94db4362ad0766096fdd1
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-01-29 22:25:58 +0100

    Clean up dictionaries (removing TP converter entries) to optimize ROOT6 header parsing and VMEM consumption (TrigTopoEventTPCnv-00-00-03)
    Former-commit-id: 82d4cab0b9cabb0b5ec890dfd8e83a82b4baf6d2

--- Trigger/TrigEvent/TrigTopoEventTPCnv/src/ElectronMuonTopoInfoCnv_p1.cxx
+++ Trigger/TrigEvent/TrigTopoEventTPCnv/src/ElectronMuonTopoInfoCnv_p1.cxx
@@ -2,7 +2,13 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
+#define private public
+#define protected public
 #include "TrigTopoEvent/ElectronMuonTopoInfo.h"
+#undef private
+#undef protected
+
+
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfo_p1.h"
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfoCnv_p1.h"
 
@@ -11,15 +17,15 @@ void ElectronMuonTopoInfoCnv_p1::persToTrans(const ElectronMuonTopoInfo_p1* pers
 				       ElectronMuonTopoInfo* transObj, 
 				       MsgStream &log)
 {
-   log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::persToTrans called " << endmsg;
+   log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::persToTrans called " << endreq;
 
-   transObj->SetRoiWord        (persObj->m_roiWord)        ;
-   transObj->SetDeltaPhi       (persObj->m_DeltaPhi)       ;
-   transObj->SetDeltaR         (persObj->m_DeltaR)          ;
-   transObj->SetInvMass        (persObj->m_InvMass)         ;
-   transObj->SetElecValid      (persObj->m_electronValid)   ;
-   transObj->SetOppositeCharge (persObj->m_oppositeCharge)  ;
-   transObj->SetVertexState    (persObj->m_vertexState)     ;
+   transObj->m_roiWord        = persObj->m_roiWord        ;
+   transObj->m_DeltaPhi       = persObj->m_DeltaPhi       ;
+   transObj->m_DeltaR         = persObj->m_DeltaR          ;
+   transObj->m_InvMass        = persObj->m_InvMass         ;
+   transObj->m_electronValid  = persObj->m_electronValid   ;
+   transObj->m_oppositeCharge = persObj->m_oppositeCharge  ;
+   transObj->m_vertexState    = persObj->m_vertexState     ;
  
 }
 
@@ -28,14 +34,14 @@ void ElectronMuonTopoInfoCnv_p1::transToPers(const ElectronMuonTopoInfo* transOb
 				       ElectronMuonTopoInfo_p1* persObj, 
 				       MsgStream &log)
 {
-   log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::transToPers called " << endmsg;
+   log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::transToPers called " << endreq;
 
-   persObj->m_roiWord        = transObj->RoiWord()        ;
-   persObj->m_DeltaPhi       = transObj->DeltaPhi()       ;
-   persObj->m_DeltaR         = transObj->DeltaR()         ;
-   persObj->m_InvMass        = transObj->InvMass()        ;
-   persObj->m_electronValid  = transObj->ElecValid()  ;
-   persObj->m_oppositeCharge = transObj->OppositeCharge() ;
-   persObj->m_vertexState    = transObj->VertexState()    ;
+   persObj->m_roiWord        = transObj->m_roiWord        ;
+   persObj->m_DeltaPhi       = transObj->m_DeltaPhi       ;
+   persObj->m_DeltaR         = transObj->m_DeltaR         ;
+   persObj->m_InvMass        = transObj->m_InvMass        ;
+   persObj->m_electronValid  = transObj->m_electronValid  ;
+   persObj->m_oppositeCharge = transObj->m_oppositeCharge ;
+   persObj->m_vertexState    = transObj->m_vertexState    ;
 
 }
