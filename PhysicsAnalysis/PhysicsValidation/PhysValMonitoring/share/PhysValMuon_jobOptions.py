312d4c0a5fcbdd91bb41a09a8c2a50ef4ec3e904
Carl Bryan Gwilliam <carl.bryan.gwilliam@cern.ch>
2015-01-30 13:30:02 +0100

    20.1.0.2 compatible tag (PhysValMonitoring-00-00-29-01)

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMuon_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMuon_jobOptions.py
@@ -3,9 +3,14 @@ tool1 = MuonPhysValMonitoring__MuonPhysValMonitoringTool()
 tool1.DoMuonTrackValidation = True
 tool1.DoMuonSegmentValidation = True
 tool1.DoBinnedResolutionPlots = False
+# tool1.MuonTruthParticleContainerName = 'MuonTruthParticle' # uncomment for release 19
+tool1.DoTrigMuonValidation = False
+tool1.DoTrigMuonEFValidation = True
+tool1.DoTrigMuonL2Validation = False
+tool1.DoTrigMuonL1Validation = True
 
 #Select Muon Authors... (empty: all authors, 1: combined, 4: MuTagIMO, 5: Standalone, 6: MuGirl)
-tool1.SelectMuonAuthors = [ 1, 4, 5, 6 ]
+tool1.SelectMuonAuthors = [ 1, 4, 5, 6, 8, 9  ]
 
 #Select Muon Categories... (origin of muons, empty: all categories, 0: ALL, 1: PROMPT, 2: IN-FLIGHT, 3: NON-ISOLATED, 4: REST)
 tool1.SelectMuonCategories = [ 0, 1 ]
