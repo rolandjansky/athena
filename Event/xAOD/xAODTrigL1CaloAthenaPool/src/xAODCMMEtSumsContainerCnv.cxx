f880591f9a9a37136dc67de667177ad82a163364
John Morris <j.d.morris@qmul.ac.uk>
2015-01-30 13:57:55 +0100

    Add JEMEtSums_v2 (xAODTrigL1CaloAthenaPool-00-00-11)
    	* Adding JEMEtSums_v2
    	* Tag as xAODTrigL1CaloAthenaPool-00-00-11

--- Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODCMMEtSumsContainerCnv.cxx
+++ Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODCMMEtSumsContainerCnv.cxx
@@ -34,12 +34,17 @@ createPersistent( xAOD::CMMEtSumsContainer* trans ) {
 
 xAOD::CMMEtSumsContainer* xAODCMMEtSumsContainerCnv::createTransient() {
    // The known ID(s) for this container:
-   static pool::Guid v1_guid( "186A613D-0C91-4880-9B09-B41C28B17B53" );
+   static const pool::Guid v1_guid( "186A613D-0C91-4880-9B09-B41C28B17B53" );
+   static const pool::Guid v2_guid( "9EE079C0-5737-4710-A81D-278D97F01E50" );   
 
    // Check if we're reading the most up to date type:
-   if( compareClassGuid( v1_guid ) ) {
+   if( compareClassGuid( v2_guid ) ) {
       return poolReadObject< xAOD::CMMEtSumsContainer >();
    }
+   if( compareClassGuid( v1_guid ) ) {
+    throw std::runtime_error( "Version 1 of xAOD::CMMEtSumsContainer found - bail out for now" );
+    return 0;    
+   }   
 
    // If we didn't recognise the ID, let's complain:
    throw std::runtime_error( "Unsupported version of "
