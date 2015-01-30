21197e040de4902c6e019f0d9dedf9e993235f18
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-01-30 20:00:00 +0100

    Clean up dictionaries (removing TP converter entries) to optimize ROOT6 header parsing and VMEM consumption (TrigCombinedEventTPCnv-00-00-13)
    	* Clean up dictionaries (removing TP converter entries) to
    	optimize ROOT6 header parsing and VMEM consumption
    	* TrigCombinedEventTPCnv-00-00-13

--- Trigger/TrigEvent/TrigCombinedEventTPCnv/TrigCombinedEventTPCnv/TrigCombinedEventTPCnvDict.h
+++ Trigger/TrigEvent/TrigCombinedEventTPCnv/TrigCombinedEventTPCnv/TrigCombinedEventTPCnvDict.h
@@ -2,17 +2,15 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TRIGCOMBINEDEVENTTPCNV_TRIGCOMBINEDEVENTTPCNVDICT1_H
-#define TRIGCOMBINEDEVENTTPCNV_TRIGCOMBINEDEVENTTPCNVDICT1_H
+#ifndef TRIGCOMBINEDEVENTTPCNV_TRIGCOMBINEDEVENTTPCNVDICT_H
+#define TRIGCOMBINEDEVENTTPCNV_TRIGCOMBINEDEVENTTPCNVDICT_H
      
 #include "TrigCombinedEventTPCnv/TrigCompositeContainer_p1.h"
-#include "TrigCombinedEventTPCnv/TrigCompositeContainerCnv_p1.h"
 #include "TrigCombinedEventTPCnv/TrigComposite_p1.h"
-#include "TrigCombinedEventTPCnv/TrigCompositeCnv_p1.h"
 #include "TrigNavigation/TrigFeatureLink.h"
 
 //dummy instances of all objects using std::vector<TPObjRef> for the dictionary
-struct dummy_TrigCombinedEventTPCnvDict1
+struct dummy_TrigCombinedEventTPCnvDict
 {
   std::vector<TrigComposite_p1>               m_dummyComposite_p1;
   std::vector<TrigCompositeContainer_p1>               m_dummyCompositeContainer_p1;
