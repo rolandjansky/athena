c630b5d6488712a6b795ffd2931880a6a811fde2
Aaron Angerami <aaron.richard.angerami@cern.ch>
2015-02-01 04:04:30 +0100

    tagging xAODHIEventAthenaPool-00-00-02 (xAODHIEventAthenaPool-00-00-02)

---
+++ Event/xAOD/xAODHIEventAthenaPool/pool/xAODHIEventShapeAuxContainerCnv.cxx
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+ * @file HIEventShapeAuxContainerCnv.cxx
+ * @brief Generated implementation file which includes header files needed by HIEventShapeAuxContainerCnv
+ * @author RD Schaffer <R.D.Schaffer@cern.ch>
+ */
+
+#include "xAODHIEventShapeAuxContainerCnv.h"
+
