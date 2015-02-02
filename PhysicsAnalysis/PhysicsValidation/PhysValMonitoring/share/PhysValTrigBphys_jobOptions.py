6e38ac8cb43a591d6254108f3d5e63db96cf6aa4
Carl Bryan Gwilliam <carl.bryan.gwilliam@cern.ch>
2015-02-02 10:34:52 +0100

    Add first verion of trigger JOs (PhysValMonitoring-00-01-29-02)
    Fix TrigMET JO
    
    27.01.2015 Patrick Czodrowski <czodrows@cern.ch>
    Add Trig* JOs in share

---
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValTrigBphys_jobOptions.py
@@ -0,0 +1,19 @@
+from TrigBphysMonitoring.TrigBphysMonitoringConfig import TrigBphysMonitoringTool
+tool1 = getattr(ToolSvc, TrigBphysMonitoringTool()[0].split('/')[-1])
+
+tool1.EnableLumi = False
+tool1.OutputLevel = WARNING
+#tool1.DetailLevel = 10
+
+monMan = CfgMgr.AthenaMonManager("PhysValMonManager")
+monMan.AthenaMonTools += [ tool1 ]
+
+# for name in TrigTauMonitoringTool():
+#     tool = getattr(ToolSvc, name.split('/')[-1])
+# 
+#     tool.EnableLumi = False
+#     tool.OutputLevel = WARNING
+#     # tool.DetailLevel = 10
+# 
+#     monMan = CfgMgr.AthenaMonManager("PhysValMonManager")
+#     monMan.AthenaMonTools += [ tool ]
