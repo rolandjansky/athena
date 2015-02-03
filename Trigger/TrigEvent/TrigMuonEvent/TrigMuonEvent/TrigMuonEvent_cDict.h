804e2854d4aeb1f69fa2f1b7a5d2d765240b29ec
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-02-03 09:07:21 +0100

    Move MuonFeature and MuonFeatureDetails into seperate dictionary (TrigMuonEvent-00-01-19)
            * Move MuonFeature and MuonFeatureDetails into seperate dictionary, as
    	enums are shared with persistent class representations.
            * TrigMuonEvent-00-01-19
    
    2015-01-14 Stefano Giagu <Stefano.Giagu@cern.ch>
            * requirements updated for xAOD::L2IsoMuonContainer
            * TrigMuonEvent-00-01-18

---
+++ Trigger/TrigEvent/TrigMuonEvent/TrigMuonEvent/TrigMuonEvent_cDict.h
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#ifndef TRIGMUONEVENT_TRIGMUONEVENT_CDICT_H
+#define TRIGMUONEVENT_TRIGMUONEVENT_CDICT_H
+
+#include "TrigMuonEvent/MuonFeature.h"
+#include "TrigMuonEvent/MuonFeatureDetails.h"
+
+#endif // TRIGMUONEVENT_TRIGMUONEVENTDICT_H
