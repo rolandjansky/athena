9680bc16156a82fb1d1593125fb1c31e38665807
Ioannis Nomidis <Ioannis.Nomidis@cern.ch>
2015-02-04 00:01:59 +0100

    added truth track extrapol. plots (TrkValHistUtils-00-00-24)

--- Tracking/TrkValidation/TrkValHistUtils/Root/RecoInfoPlots.cxx
+++ Tracking/TrkValidation/TrkValHistUtils/Root/RecoInfoPlots.cxx
@@ -3,6 +3,7 @@
 */
 
 #include "TrkValHistUtils/RecoInfoPlots.h"
+#include "TMath.h"
 
 namespace Trk{
 
@@ -10,19 +11,23 @@ void RecoInfoPlots::init() {
     trackfitchi2=NULL;
     trackfitndof=NULL;
     trackcon=NULL;
+    trackchi2prob=NULL;
 }
 
 void RecoInfoPlots::initializePlots(){
   trackfitchi2 = Book1D("tchi2","Track fit chi2;Track Fit Chi2;Entries",100,0.,100.);
-  trackfitndof = Book1D("tndof","Track fit ndof;Number of degrees of freedom;Entries",51,-0.5,50.5);
+  trackfitndof = Book1D("tndof","Track fit ndof;Number of degrees of freedom;Entries",101,-0.5,100.5);
   trackcon     = Book1D("chi2ndof", "Track fit chi2/ndof;Track fit chi2/ndof;Entries", 100, 0, 5);
-
+  trackchi2prob= Book1D("chi2prob", "Track fit chi2 probability;Track fit p(#chi^2);Entries", 20, 0., 1.);
 }
 
 void RecoInfoPlots::fill(const xAOD::TrackParticle& trkprt) {
-  trackfitchi2->Fill(trkprt.chiSquared());
-  trackfitndof->Fill(trkprt.numberDoF());
-  trackcon->Fill(trkprt.chiSquared()/trkprt.numberDoF());
+  float chi2 = trkprt.chiSquared();
+  int ndf = trkprt.numberDoF();
+  trackfitchi2->Fill(chi2);
+  trackfitndof->Fill(ndf);
+  trackcon->Fill(chi2/ndf);
+  trackchi2prob->Fill(TMath::Prob(chi2,ndf));
 }
 
 }
