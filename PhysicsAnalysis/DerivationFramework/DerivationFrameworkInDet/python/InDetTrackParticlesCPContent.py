38d03ec1db1291865f6a6350495132fd817a2b52
Anthony Morley <anthony.morley@cern.ch>
2015-02-05 16:12:53 +0100

    Add holes to TSOS dressing (DerivationFrameworkInDet-00-00-22)

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/python/InDetTrackParticlesCPContent.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+InDetTrackParticlesCPContent = [
+"InDetTrackParticles",
+"InDetTrackParticlesAux.phi.theta.qOverP.chiSquared.numberDoF.numberOfBLayerHits.numberOfPixelHits.numberOfPixelHoles.numberOfPixelSharedHits.numberOfPixelDeadSensors.numberOfSCTHits.numberOfSCTHoles.numberOfSCTSharedHits.numberOfSCTDeadSensors"
+]
