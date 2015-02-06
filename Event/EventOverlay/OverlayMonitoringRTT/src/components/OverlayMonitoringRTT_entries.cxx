2b2cd0adf271a0cdc374c7b5cb10f7c84a504f18
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-02-06 08:10:33 +0100

    test/OverlayMonitoringRTT_TestConfiguration.xml - update TriggerBit=240 obo Andy Haas. OverlayMonitoringRTT-00-01-16 (OverlayMonitoringRTT-00-01-16)

---
+++ Event/EventOverlay/OverlayMonitoringRTT/src/components/OverlayMonitoringRTT_entries.cxx
@@ -0,0 +1,12 @@
+#include "OverlayMonitoringRTT/rttmon.h"
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( rttmon )
+
+DECLARE_FACTORY_ENTRIES( OverlayMonitoringRTT ) {
+  DECLARE_ALGORITHM( rttmon )
+
+}
+
+
