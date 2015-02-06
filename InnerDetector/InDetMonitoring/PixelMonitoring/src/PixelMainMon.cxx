6f09fd4a2fd75cb374c360ce69685fb5cccd0e44
Rachel Reisner Hinman <rachel.reisner.hinman@cern.ch>
2015-02-06 20:19:03 +0100

    add DBM occupancy maps (PixelMonitoring-00-04-48)
    	* Added DBM hit and cluster occupancy maps

--- InnerDetector/InDetMonitoring/PixelMonitoring/src/PixelMainMon.cxx
+++ InnerDetector/InDetMonitoring/PixelMonitoring/src/PixelMainMon.cxx
@@ -28,6 +28,7 @@
 #include "InDetConditionsSummaryService/IInDetConditionsSvc.h"
 #include "TH2S.h"
 #include "TProfile2D.h"
+#include "PixelMonitoring/DBMMon2DMaps.h"
 #include "PixelMonitoring/PixelMon2DMaps.h"
 #include "PixelMonitoring/PixelMonProfiles.h"
 #include "LWHists/TH1F_LW.h"
@@ -161,6 +162,7 @@ PixelMainMon::PixelMainMon(const std::string & type,
    m_diff_ROD_BCID = 0;         
    m_diff_ROD_vs_Module_BCID = 0;
    m_occupancy = 0;
+   m_occupancyDBM = 0;
    m_average_occupancy = 0;
    m_FE_chip_hit_summary = 0;
    m_ecA_occupancy_summary_low = 0;    
@@ -378,6 +380,7 @@ PixelMainMon::PixelMainMon(const std::string & type,
    m_clussize_vs_eta_ECC = 0;
    m_clussize_vs_eta_ECA = 0;
    m_cluster_occupancy = 0;
+   m_clusocc_DBM = 0;
    m_clusocc_sizenot1 = 0; 
    m_average_cluster_occupancy = 0;
    m_cluseff_mod = 0;
