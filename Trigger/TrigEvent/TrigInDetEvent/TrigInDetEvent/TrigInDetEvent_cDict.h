472b5e62dd1647daf744fb7ffb8ea19a698972ea
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2015-02-09 17:03:18 +0100

    'Add enum for FTF to TrigInDetTrack' (TrigInDetEvent-00-01-48)
    	* Add enum for FTF to TrigInDetTrack
    	* Tagging as TrigInDetEvent-00-01-48
    
    2015-02-03 Peter van Gemmeren <gemmeren@anl.gov>
    	* Move TrigInDetTrackFitPar into seperate dictionary, as enums are
    	shared with persistent class representations.
    	* Tagging as TrigInDetEvent-00-01-47

---
+++ Trigger/TrigEvent/TrigInDetEvent/TrigInDetEvent/TrigInDetEvent_cDict.h
@@ -0,0 +1,10 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRIG_INDET_C_DICT_H
+#define TRIG_INDET_C_DICT_H
+
+#include "TrigInDetEvent/TrigInDetTrackFitPar.h"
+
+#endif
