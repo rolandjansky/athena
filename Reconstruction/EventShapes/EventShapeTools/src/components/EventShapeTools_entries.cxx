6041bb25efe02e26203c828674d6fed25b239f47
Pierre-Antoine Delsart <delsart@in2p3.fr>
2015-02-10 17:21:39 +0100

    'fix statuscode check' (EventShapeTools-00-01-07)
    	* ATLASRECTS-1636
    	* python/EventDensityConfig.py : fix check return code. Thanks to
    	Scott Snyder.
    	* EventShapeTools-00-01-07
    2015-02-06    <gemmeren@anl.gov>
    
    	* Adding dictionary creation for EventDensityTool, probably needed by
    	ROOT6 for python bindings
    	* EventShapeTools-00-01-06
    
    2015-02-07  Christos
    
    	* Coverity 19601
    	* EventShapeTools-00-01-05
    
    2015-02-05    <delsart@localhost>
    
    	* python/EventDensityConfig.py (EventDensityAlg): returns a
    	Recoverable in case of error as per request of ATR-10064
    ...
    (Long ChangeLog diff - truncated)

--- Reconstruction/EventShapes/EventShapeTools/src/components/EventShapeTools_entries.cxx
+++ Reconstruction/EventShapes/EventShapeTools/src/components/EventShapeTools_entries.cxx
@@ -1,8 +1,12 @@
 #include "EventShapeTools/EventDensityTool.h"
+#include "EventShapeTools/EventShapeCopier.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_TOOL_FACTORY( EventDensityTool )
+DECLARE_TOOL_FACTORY( EventShapeCopier )
 
 DECLARE_FACTORY_ENTRIES( EventShapeTools ) {
   DECLARE_TOOL( EventDensityTool );
+  DECLARE_TOOL( EventShapeCopier );
+
 }
