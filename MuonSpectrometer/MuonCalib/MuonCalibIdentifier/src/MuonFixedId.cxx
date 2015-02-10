e506ac94c096ac99a3af39a0b0431609bf7b4c98
Edward Diehl <diehl@umich.edu>
2015-02-10 16:22:19 +0100

    Add BOE/BMG stations to MuonFixedIdUnpack.py (MuonCalibIdentifier-00-00-37)
            * tagging MuonCalibIdentifier-00-00-37
    
    16-01-2015 Edward Diehl Add BOE/BMG stations for MuonFixedId
            *   Note: BOE3=BOL7 in offline, so no change needed, it will be handled as BOL
    	*   Add BMG since it is defined in IdDictMuonSpectrometer_S.02.xml
            * tagging MuonCalibIdentifier-00-00-36

--- MuonSpectrometer/MuonCalib/MuonCalibIdentifier/src/MuonFixedId.cxx
+++ MuonSpectrometer/MuonCalib/MuonCalibIdentifier/src/MuonFixedId.cxx
@@ -6,8 +6,7 @@
 
 namespace MuonCalib {
 
-std::ostream& MuonFixedId::dump(std::ostream& os) const
-{
+std::ostream& MuonFixedId::dump(std::ostream& os) const {
   os << technology() << ": stnEta " << eta() << ", stnPhi " << phi() << " " 
      << technologyString() << ": " << stationNameString() ;
   if( is_mdt() ){
@@ -29,14 +28,13 @@ std::ostream& MuonFixedId::dump(std::ostream& os) const
   return os;
 }
 
-
 const char MuonFixedId::kStationNameStrings[MuonFixedId::kNumberOfStationNames][4] = { 
      // 1      2      3      4      5      6      7      8      9     10     11     12     13
      "BIL", "BIS", "BML", "BMS", "BOL", "BOS", "BEE", "BIR", "BMF", "BOF", "BOG", "BME", "BIM",
      //14     15      16    17     18     19     20     21     22     23     24
      "EIC", "EIL", "EEL", "EES", "EMC", "EML", "EMS", "EOC", "EOL", "EOS", "EIS",
-     //25     26     27     28     29     30     31     32     33     34
-     "T1F", "T1E", "T2F", "T2E", "T3F", "T3E", "T4F", "T4E", "CSS", "CSL" 
+     //25     26     27     28     29     30     31     32     33     34     35 
+     "T1F", "T1E", "T2F", "T2E", "T3F", "T3E", "T4F", "T4E", "CSS", "CSL", "BMG" 
 };
 
 const char MuonFixedId::kTechnologyStrings[MuonFixedId::kNumberOfTechnologies][4] = {
@@ -44,4 +42,3 @@ const char MuonFixedId::kTechnologyStrings[MuonFixedId::kNumberOfTechnologies][4
 };
 
 } // end of namespace MuonCalib
-
