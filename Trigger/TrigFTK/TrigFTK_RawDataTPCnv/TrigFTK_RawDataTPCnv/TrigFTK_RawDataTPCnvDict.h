4c9442a964d49244bd0a6de7abc61b20665d3ed4
Peter Van Gemmeren <peter.van.gemmeren@cern.ch>
2015-02-11 17:22:10 +0100

    Clean up dictionaries (removing TP converter entries) to optimize ROOT6 header parsing and VMEM consumption (TrigFTK_RawDataTPCnv-00-00-03)

--- Trigger/TrigFTK/TrigFTK_RawDataTPCnv/TrigFTK_RawDataTPCnv/TrigFTK_RawDataTPCnvDict.h
+++ Trigger/TrigFTK/TrigFTK_RawDataTPCnv/TrigFTK_RawDataTPCnv/TrigFTK_RawDataTPCnvDict.h
@@ -2,14 +2,15 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef TRIGFTK_RAWDATATPCNV_TRIGINDETEVENTTPCNVDICT1_H
-#define TRIGFTK_RAWDATATPCNV_TRIGINDETEVENTTPCNVDICT1_H
+#ifndef TRIGFTK_RAWDATATPCNV_TRIGINDETEVENTTPCNVDICT_H
+#define TRIGFTK_RAWDATATPCNV_TRIGINDETEVENTTPCNVDICT_H
 
+#include "TrigFTK_RawDataTPCnv/FTK_RawTrack_p1.h"
+#include "TrigFTK_RawDataTPCnv/FTK_RawTrackContainer_p1.h"
 #include "TrigFTK_RawDataTPCnv/FTK_RawTrackContainer_tlp1.h"
-#include "TrigFTK_RawDataTPCnv/FTK_RawTrackContainerCnv_tlp1.h"
 
 //dummy instances of all objects using std::vector<TPObjRef> for the dictionary
-struct dummy_TrigFTK_RawDataTPCnvDict1
+struct dummy_TrigFTK_RawDataTPCnvDict
 {
 
   std::vector<FTK_RawTrackContainer_p1>       m_dummyFTK_RawTrackContainer_p1;
