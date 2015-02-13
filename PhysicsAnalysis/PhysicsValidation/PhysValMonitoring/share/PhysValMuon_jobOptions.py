2d6409d11c967f4b49c4cdc46174d97a7f2a2b9b
Ioannis Nomidis <Ioannis.Nomidis@cern.ch>
2015-02-13 17:35:10 +0100

    muons: enabled binned resolution plots (PhysValMonitoring-00-01-31)
    	* enabled binned resolution plots
    	* tagging PhysValMonitoring-00-01-31

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMuon_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMuon_jobOptions.py
@@ -1,19 +1,20 @@
 from MuonPhysValMonitoring.MuonPhysValMonitoringConf import MuonPhysValMonitoring__MuonPhysValMonitoringTool
 tool1 = MuonPhysValMonitoring__MuonPhysValMonitoringTool()
+# tool1.IsData = True # uncomment to run on data
 tool1.DoMuonTrackValidation = True
 tool1.DoMuonSegmentValidation = True
-tool1.DoBinnedResolutionPlots = False
+tool1.DoBinnedResolutionPlots = True
 # tool1.MuonTruthParticleContainerName = 'MuonTruthParticle' # uncomment for release 19
-#tool1.DoTrigMuonValidation = False
-#tool1.DoTrigMuonEFValidation = True
-#tool1.DoTrigMuonL2Validation = False
-#tool1.DoTrigMuonL1Validation = True
+tool1.DoTrigMuonValidation = False
+tool1.DoTrigMuonEFValidation = True
+tool1.DoTrigMuonL2Validation = False
+tool1.DoTrigMuonL1Validation = True
 
 #Select Muon Authors... (empty: all authors, 1: combined, 4: MuTagIMO, 5: Standalone, 6: MuGirl)
-tool1.SelectMuonAuthors = [ 1, 4, 5, 6 ]
+tool1.SelectMuonAuthors = [ 1, 4 ,5, 6, 8, 9 ]
 
 #Select Muon Categories... (origin of muons, empty: all categories, 0: ALL, 1: PROMPT, 2: IN-FLIGHT, 3: NON-ISOLATED, 4: REST)
-tool1.SelectMuonCategories = [ 0, 1 ]
+tool1.SelectMuonCategories = [ 0, 1 ] # for data, must be 0 (ALL)
 
 tool1.EnableLumi = False
 tool1.OutputLevel = WARNING
