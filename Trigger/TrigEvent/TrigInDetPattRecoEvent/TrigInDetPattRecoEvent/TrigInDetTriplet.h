09714c32fb65a5d79d6e0809625e8a9f1418af72
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2015-02-15 21:04:23 +0100

    'Optimisation - slim down TrigInDetTriplet to just (sp1, sp2, sp3, Q)' (TrigInDetPattRecoEvent-00-00-33)
    	* Optimisation - slim down TrigInDetTriplet to just (sp1, sp2, sp3, Q)
    	* Remove everything except TrigInDetTriplet from TrigInDetTracklet.h
    	* Rename to TrigInDetTriplet.h
    	* Tagging as TrigInDetPattRecoEvent-00-00-33

---
+++ Trigger/TrigEvent/TrigInDetPattRecoEvent/TrigInDetPattRecoEvent/TrigInDetTriplet.h
@@ -0,0 +1,39 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef __TRIG_IN_DET_TRIPLET_H__
+#define __TRIG_IN_DET_TRIPLET_H__
+
+#include "TrigInDetEvent/TrigSiSpacePointBase.h"
+
+
+
+
+class TrigInDetTriplet {
+
+ public:
+   TrigInDetTriplet() = delete; //to prevent creation w/o initialization
+
+ TrigInDetTriplet(const TrigSiSpacePointBase& s1, const TrigSiSpacePointBase& s2, const TrigSiSpacePointBase& s3, double Q) : 
+    m_s1(s1), m_s2(s2), m_s3(s3), m_Q(Q) {};
+
+ TrigInDetTriplet(const TrigInDetTriplet& t) :
+    m_s1(t.m_s1), m_s2(t.m_s2), m_s3(t.m_s3), m_Q(t.m_Q) {};
+ TrigInDetTriplet(TrigInDetTriplet* t) :
+    m_s1(t->m_s1), m_s2(t->m_s2), m_s3(t->m_s3), m_Q(t->m_Q) {};
+
+  const TrigSiSpacePointBase& s1() const {return m_s1;}
+  const TrigSiSpacePointBase& s2() const {return m_s2;}
+  const TrigSiSpacePointBase& s3() const {return m_s3;}
+  double Q() const {return m_Q;}
+
+ protected:
+
+  const TrigSiSpacePointBase& m_s1;
+  const TrigSiSpacePointBase& m_s2;
+  const TrigSiSpacePointBase& m_s3;
+  double m_Q;//Quality
+};
+
+#endif // __TRIG_IN_DET_TRIPLET__
