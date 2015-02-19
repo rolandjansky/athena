86776f711dee9e8ae8dfffed45b1d0615220967f
Patrick Karl Czodrowski <patrick.czodrowski@cern.ch>
2015-02-19 19:30:21 +0100

    try hacking the Valgrind setup to patch Dso.py from Tools/PyUtils ... again as RTT doesnt pick up the checked out package this time by pre-pending the newly checked out package to /Applications/root/root_v5.34.21/lib:/Applications/root/root_v5.34.21/lib:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/bin/, reduce number of MC_pp_v5_tight_mc_prescale events from 1000 to 750 (TriggerTest-00-06-95)
    2015-02-19 Patrick Czodrowski <czodrows@cern.ch>
        * try hacking the Valgrind setup to patch Dso.py from Tools/PyUtils ... again as RTT doesnt pick up the checked out package
         this time by pre-pending the newly checked out package to $PYTHONPATH
        * reduce number of MC_pp_v5_tight_mc_prescale events from 1000 to 750
        * TriggerTest-00-06-95
    
    2015-02-16 Patrick Czodrowski <czodrows@cern.ch>
        * hack the Valgrind setup to patch Dso.py from Tools/PyUtils
        * fix RTT MC_pp_v5_loose_mc_prescale setup
        * TriggerTest-00-06-93 & TriggerTest-00-06-94
    
    2015-02-12 Patrick Czodrowski <czodrows@cern.ch>
        * fix duplicate in requirements file to omit nicos warning
        * TriggerTest-00-06-92
    
    2015-02-11 Francesca Pastore
    	* updated (and cleaned) AthenaTrigRDO_L2EFMerging, becoming AthenaTrigBS_L2EFMerging
    	* TriggerTest-00-06-91
    ...
    (Long ChangeLog diff - truncated)

--- Trigger/TrigValidation/TriggerTest/share/testAthenaL2EFMerging.py
+++ Trigger/TrigValidation/TriggerTest/share/testAthenaL2EFMerging.py
@@ -14,20 +14,21 @@ from AthenaCommon.AthenaCommonFlags import athenaCommonFlags as acf
 ####################################
 # Generic setup
 ####################################
-EvtMax=20
+#EvtMax=20
 ReadBS=True
 #testCurrentMenu=True
 #testPhysicsV5=True
-testLS1V1=True
+#testLS1V1=True
+rerunLVL1=1
+
 OutputLevel=DEBUG
 HLTOutputLevel=DEBUG
 SkipEvents=0
 setPrescale="None"
 doValidation=False
 chainOrderedDown=True
-#try adding: ATLAS-R1-2012-02-00-00
-#setDetDescr="ATLAS-R2-2015-01-01-00"
-BSRDOInput='root://eosatlas//eos/atlas/atlascerngroupdisk/trig-daq/validation/test_data/data12_8TeV.00212967.physics_EnhancedBias.merge_eb_zee_zmumu_cc.RAW._lb0291._SFO-5._0001.data'
+
+#BSRDOInput='root://eosatlas//eos/atlas/atlascerngroupdisk/trig-daq/validation/test_data/data12_8TeV.00212967.physics_EnhancedBias.merge_eb_zee_zmumu_cc.RAW._lb0291._SFO-5._0001.data'
 
 #####################################
 # configure the menu
