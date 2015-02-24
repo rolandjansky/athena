aa03525a1b49e3bd6454f8ec42916959595bb743
Russell Woods Smith <russell.smith@cern.ch>
2015-02-24 17:00:31 +0100

    inheritance for python bindings (METInterface-00-01-08)

--- Reconstruction/MET/METInterface/METInterface/IMETSystematicsTool.h
+++ Reconstruction/MET/METInterface/METInterface/IMETSystematicsTool.h
@@ -19,19 +19,25 @@
 #include "xAODEventInfo/EventInfo.h"
 #include "xAODMissingET/MissingET.h"
 #include "xAODMissingET/MissingETAssociationMap.h"
+#include "xAODJet/JetContainer.h"
 
-class IMETSystematicsTool : public virtual asg::IAsgTool,
-                            public virtual CP::ISystematicsTool {
-
-  /// Declare the interface that the class provides
+class IMETSystematicsTool : public virtual asg::IAsgTool
+{
+  // Declare the interface that the class provides
   ASG_TOOL_INTERFACE( IMETSystematicsTool )
 
   public:
   virtual ~IMETSystematicsTool() {}
 
   //we don't inherit from CorrectionTool directly, but we are something close to that
-  virtual CP::CorrectionCode applyCorrection(xAOD::MissingET& met, xAOD::MissingETAssociationMap * map) const = 0;
-  virtual CP::CorrectionCode correctedCopy(const xAOD::MissingET& met, xAOD::MissingET*& outputmet,xAOD::MissingETAssociationMap * map) const = 0;
+  virtual CP::CorrectionCode applyCorrection(xAOD::MissingET& met, 
+					     const xAOD::MissingETAssociationMap * map=nullptr,
+					     const xAOD::JetContainer * jetCont = nullptr
+					     ) const = 0;
+  virtual CP::CorrectionCode correctedCopy(const xAOD::MissingET& met, xAOD::MissingET*& outputmet,
+					   const xAOD::MissingETAssociationMap * map=nullptr,
+					   const xAOD::JetContainer * jetCont = nullptr
+					   ) const = 0;
   //We don't want these for MET
   //virtual CP::CorrectionCode applyContainerCorrection(xAOD::MissingETContainer& inputs) const;
   //virtual CP::CorrectionCode applyContainerCorrection(xAOD::MissingETContainer& inputs, const xAOD::EventInfo& eInfo) const;
