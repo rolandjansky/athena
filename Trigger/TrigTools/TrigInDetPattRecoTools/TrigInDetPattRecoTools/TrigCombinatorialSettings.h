ed348f23dc268f7ff15541597c7eb8605ca34e41
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2015-02-24 14:24:32 +0100

    'New parameter to filter by rz, always true unless running cosmics' (TrigInDetPattRecoTools-00-01-03)
    	* New parameter to filter by rz, always true unless running cosmics
    	* Tagging as TrigInDetPattRecoTools-00-01-03
    
    2015-02-16 Stewart Martin-Haugh <smh -at- cern -dot- ch>
    	* Not using doublet z0 max parameter since we take it from RoI
    	* Tagging as TrigInDetPattRecoTools-00-01-02
    
    2015-02-15 Stewart Martin-Haugh <smh -at- cern -dot- ch>
    	* Slim down triplet EDM
    	* Remove unused header file
    	* Tagging as TrigInDetPattRecoTools-00-01-01

--- Trigger/TrigTools/TrigInDetPattRecoTools/TrigInDetPattRecoTools/TrigCombinatorialSettings.h
+++ Trigger/TrigTools/TrigInDetPattRecoTools/TrigInDetPattRecoTools/TrigCombinatorialSettings.h
@@ -19,12 +19,12 @@ public:
     m_maxSiliconLayer = 19;
 
     m_doubletD0Max         = 5.0;
-    m_doubletZ0Max         = 230.0;
 
     m_tripletD0Max      = 4.0; 
     m_tripletD0_PPS_Max = 1.7; 
     m_tripletPtMin      = 2500.0;//was 1000.0
     m_tripletDoPSS      = false; // Allow Pixel SCT SCT seeds?
+    m_doubletFilterRZ   = true;
     
     m_magFieldZ = 2.0;//switch to configured value
     m_nMaxPhiSlice = 53;
@@ -34,12 +34,12 @@ public:
   int m_maxBarrelPix, m_minEndcapPix, m_maxEndcapPix, m_maxSiliconLayer;
 
   float m_doubletD0Max;
-  float m_doubletZ0Max;
   float m_magFieldZ;
   float m_tripletD0Max;
   float m_tripletD0_PPS_Max;
   float m_tripletPtMin;
   bool  m_tripletDoPSS;
+  bool  m_doubletFilterRZ;
   int m_nMaxPhiSlice;
   unsigned int m_maxTripletBufferLength;
   const IRoiDescriptor* roiDescriptor;
