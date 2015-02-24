a92ed0fcfad5f876206969a89e9b55024eca8c44
Will Buttinger <William.Buttinger@cern.ch>
2015-02-24 12:14:19 +0100

    'Memory leak fix, CALIBPATH adjustment in rootcore, and cleanup' (PathResolver-00-02-14)
       * Root/PathResolver.cxx: Got rid of special coutLogging for python - seems like it wasn't needed after all (was rogue package in local testarea that caused the ApplicationMgr warnings I think)
             Also fixed a memory leak reported here: https://its.cern.ch/jira/browse/ATLASG-14
       * cmt/setup_external.RootCore.sh: added download dir to CALIBPATH . takes priority over data folder and is probably first place where downloads will go
       * Tagging as PathResolver-00-02-14

--- Tools/PathResolver/cmt/setup_external.RootCore.sh
+++ Tools/PathResolver/cmt/setup_external.RootCore.sh
@@ -3,4 +3,4 @@ then
    export CALIBPATH
 fi
 
-export CALIBPATH="$HOME/AtlasCalib:$ROOTCOREBIN/data:/cvmfs/atlas.cern.ch/repo/sw/database/GroupData:/afs/cern.ch/atlas/www/GROUPS/DATABASE/GroupData:http//atlas.web.cern.ch/Atlas/GROUPS/DATABASE/GroupData:$CALIBPATH"
+export CALIBPATH="$HOME/AtlasCalib:$ROOTCOREBIN/download:$ROOTCOREBIN/data:/cvmfs/atlas.cern.ch/repo/sw/database/GroupData:/afs/cern.ch/atlas/www/GROUPS/DATABASE/GroupData:http//atlas.web.cern.ch/Atlas/GROUPS/DATABASE/GroupData:$CALIBPATH"
