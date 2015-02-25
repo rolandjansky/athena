5c8fefab7ed96a1a4d316663decd138f052b2b5f
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-02-25 10:56:55 +0100

    python/ISF_iGeant4Config.py (getGeant4SimSvc): Switch to use base version of G4TransportTool, as this is currently required for more complicated ISF configurations. Requires ISF_Geant4Tools-00-02-36. ATLASSIM-1842. Tag ISF_Geant4Config-00-01-22 (ISF_Geant4Config-00-01-22)
    	* python/ISF_iGeant4Config.py (getGeant4SimSvc): Switch to use
    	base version of G4TransportTool, as this is currently required for
    	more complicated ISF configurations. Requires
    	ISF_Geant4Tools-00-02-36. ATLASSIM-1842
    	* tag ISF_Geant4Config-00-01-22
    
    2015-02-20  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ISF_iGeant4Config.py, python/iGeant4.py - migrate
    	configuration of Geant4SimSvc to be more like standard CfgGetter
    	configurations. Use CfgGetter methods to retrieve appropriate
    	G4TransportTool instances. Requires
    	ISF_Geant4Tools-00-02-35. ATLASSIM-1842
    	* tag ISF_Geant4Config-00-01-21
    
    2015-02-19  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ISF_iGeant4Config.py - whitespace-cleanup
    	* python/iGeant4.py - use basic CfgGetter methods to set up
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/ISF_iGeant4Config.py
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/ISF_iGeant4Config.py
@@ -20,52 +20,39 @@ cache = {}
 def usingGeant4():
     global cache
     return bool(cache)
-    
+
 def getIGeant4(**kwargs):
     global cache
-
-    FullGeant4 = kwargs['FullGeant4']
-
+    FullGeant4 = kwargs.pop('FullGeant4',False)
     if 'iGeant4' in cache:
         i = cache['iGeant4']
-
-
         # if FullGeant4 specified, make sure returning cached version with FullGeant4 already in use
         if FullGeant4:
             if (i.FullGeant4 == False) :
                 print "asking for FullGeant4 but already configured Geant4 without specifying Full Geant4! Must specify FullGeant4 everywhere (or nowhere)!!!"
                 sys.exit()
-
     else:
-        
-            
         from ISF_Config.ISF_jobProperties import ISF_Flags
         from iGeant4 import iGeant4
-        i = iGeant4(ParticleService        = kwargs['ParticleService'],
-                    TruthService           = kwargs['TruthService'],
-                    SimHitService          = kwargs['SimHitService'],
-                    GeoIDSvc               = kwargs["GeoIDSvc"],
-                    UseNewConfiguration    = ISF_Flags.UseNewG4Config(),
-                    PrintTimingInfo        = ISF_Flags.DoTimeMonitoring(),
-                    FullGeant4             = FullGeant4)
+        i = iGeant4(**kwargs)
         cache['iGeant4'] = i
     return i
 
 def getGeant4SimSvc(name="ISF_Geant4SimSvc", **kwargs):
-    kwargs.setdefault('FullGeant4'            , False                               )
-    kwargs.setdefault('ParticleService'       , getService('ISF_ParticleBrokerSvc') )
-    kwargs.setdefault('TruthService'          , ISF_Flags.TruthService.get_Value()  )
-    kwargs.setdefault('SimHitService'         , getService('ISF_SimHitService')     )
-    kwargs.setdefault('GeoIDSvc'              , getService('ISF_GeoIDSvc')          )
+    from ISF_Config.ISF_jobProperties import ISF_Flags
+    kwargs.setdefault('FullGeant4'            , False                        )
+    kwargs.setdefault('SimulationTool'        , 'ISFG4TransportTool'         )
+    kwargs.setdefault('Identifier'            , "Geant4"                     )
+    kwargs.setdefault('PrintTimingInfo'       , ISF_Flags.DoTimeMonitoring() )
+    kwargs.setdefault('UseNewConfiguration'   , ISF_Flags.UseNewG4Config()   )
     return getIGeant4(**kwargs).getSimSvc()
 
 def getFullGeant4SimSvc(name="ISF_Geant4SimSvc", **kwargs):
-    kwargs.setdefault('FullGeant4'            , True                                          )
-    kwargs.setdefault('ParticleService'       , getService('ISF_ParticleBrokerSvcNoOrdering') )
+    kwargs.setdefault('FullGeant4'            , True                         )
+    kwargs.setdefault('SimulationTool'        , 'FullG4TransportTool'        )
     return getGeant4SimSvc(name, **kwargs)
 
 def getPassBackGeant4SimSvc(name="ISF_PassBackGeant4SimSvc", **kwargs):
-    kwargs.setdefault('FullGeant4'            , False                                          )
-    kwargs.setdefault('ParticleService'       , getService('ISF_ParticleBrokerSvcNoOrdering') )
+    kwargs.setdefault('FullGeant4'            , False                        )
+    kwargs.setdefault('SimulationTool'        , 'PassBackG4TransportTool'    )
     return getGeant4SimSvc(name, **kwargs)
-
--- Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/iGeant4.py
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/iGeant4.py
@@ -11,13 +11,13 @@ from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicT
 G4Eng = None
 
 class iGeant4:
-  def __init__(self, ParticleService, TruthService, SimHitService, GeoIDSvc, UseNewConfiguration, PrintTimingInfo, FullGeant4 ):
+  def __init__(self, **kwargs):
     from G4AtlasApps.SimFlags import simFlags
     from AthenaCommon.AppMgr import ToolSvc
     from AthenaCommon.AppMgr import ServiceMgr as svcMgr
 
     global UseNewG4Configuration
-    UseNewG4Configuration = UseNewConfiguration
+    UseNewG4Configuration =  kwargs.pop('UseNewConfiguration', False)
 
     ####################################
     ##
@@ -25,6 +25,9 @@ class iGeant4:
     ##
     ####################################
 
+    ##FIXME: This should be configured using a CfgGetter method
+    ##defined in the SubDetectorEnvelopes package.
+    ##G4EnvelopeDefSvc is only used in G4DetectorEnvelopes/src/DefSvcEnvelope.cxx
     from SubDetectorEnvelopes.SubDetectorEnvelopesConf import DetDescrDBEnvelopeSvc
     G4_DDDBEnvelopeDefSvc = DetDescrDBEnvelopeSvc( "G4EnvelopeDefSvc" )
     G4_DDDBEnvelopeDefSvc.DBBeamPipeNode = 'BPipeEnvelopeG4'
@@ -34,67 +37,19 @@ class iGeant4:
     G4_DDDBEnvelopeDefSvc.DBCavernNode   = 'CavernEnvelopeG4'
     svcMgr += G4_DDDBEnvelopeDefSvc
 
-    # explicit initialisation of iGeant4::PhysicsValidationUserAction
-    from ISF_Geant4Tools.ISF_Geant4ToolsConf import iGeant4__PhysicsValidationUserAction
-    G4PhysicsValidationUserAction                = iGeant4__PhysicsValidationUserAction('ISFG4PhysicsValidationUserAction')
-    G4PhysicsValidationUserAction.ParticleBroker    = ParticleService
-    ToolSvc += G4PhysicsValidationUserAction
-
-    # explicit initialisation of iGeant4::TrackProcessorUserAction
-    from ISF_Geant4Tools.ISF_Geant4ToolsConf import iGeant4__TrackProcessorUserAction,iGeant4__SDActivateUserAction
-    G4TrackProcessorUserAction                   = iGeant4__TrackProcessorUserAction('ISFG4TrackProcessorUserAction')
-    G4TrackProcessorUserAction.ParticleBroker    = ParticleService
-    G4TrackProcessorUserAction.GeoIDSvc          = GeoIDSvc
-
-    G4TrackProcessorUserAction.Geant4OnlyMode    = FullGeant4
-
-    if jobproperties.Beam.beamType() == 'cosmics' or \
-       (simFlags.CavernBG.statusOn and not 'Signal' in simFlags.CavernBG.get_Value() ):
-      G4TrackProcessorUserAction.TruthVolumeLevel = 2
-
-    ToolSvc += G4TrackProcessorUserAction
-
-
-    G4SDActivateUserAction = iGeant4__SDActivateUserAction('ISFSDActivateUserAction')
-    ToolSvc += G4SDActivateUserAction
-
-    # explicit initialisation of iGeant4::MCTruthUserAction
-    from ISF_Geant4Tools.ISF_Geant4ToolsConf import iGeant4__MCTruthUserAction
-    ISFMCTruthUserAction = iGeant4__MCTruthUserAction('ISFMCTruthUserAction')
-    ISFMCTruthUserAction.TruthRecordSvc = TruthService
-    ToolSvc += ISFMCTruthUserAction
-
-    # explicit initialisation of iGeant4::G4TransportTool
-    from ISF_Geant4Tools.ISF_Geant4ToolsConf import iGeant4__G4TransportTool
-    Geant4TransportTool = iGeant4__G4TransportTool(
-                               'ISFG4TransportTool',
-                               RandomGenerator = 'athena',
-                               RandomNumberService = simFlags.RandomSvc(),
-                               PhysicsValidationUserAction = G4PhysicsValidationUserAction,
-                               TrackProcessorUserAction = G4TrackProcessorUserAction,
-                               MCTruthUserAction = ISFMCTruthUserAction,
-                               SDActivateUserAction = G4SDActivateUserAction )
-    from ISF_Config.ISF_jobProperties import ISF_Flags
-    if 'longLived' in ISF_Flags.Simulator(): #FIXME this should be configured in a nicer way. ATLASSIM-526
-      Geant4TransportTool.QuasiStableParticlesIncluded = True
-
-    ToolSvc += Geant4TransportTool
-
     # configure Geant4SimSvc
     from ISF_Geant4Services.ISF_Geant4ServicesConf import iGeant4__Geant4SimSvc
-    Geant4SimService = iGeant4__Geant4SimSvc('ISFG4SimSvc')
-    Geant4SimService.Identifier      = "Geant4"
-    Geant4SimService.PrintTimingInfo = PrintTimingInfo
-    if UseNewConfiguration:
-      Geant4SimService.G4ConfigTool    = G4ConfigTool
+    Geant4SimService = iGeant4__Geant4SimSvc('ISFG4SimSvc', **kwargs)
 
     # DEBUG: dump Geant4SimService
     print Geant4SimService
 
-    Geant4SimService.SimulationTool       = Geant4TransportTool
     svcMgr += Geant4SimService
     self.__simSvc__ = Geant4SimService
 
+    ##FIXME: ISF_ParticleFinalStateFilter is configured in
+    ##ISF_HepMC_ToolsConfig.py and is only used by ISF_StackFiller
+    ##which is also configured in ISF_HepMC_ToolsConfig.py.
     finalStateFilter = getPublicTool("ISF_ParticleFinalStateFilter")
     finalStateFilter.CheckGenSimStable = False
     finalStateFilter.CheckGenInteracting = False
@@ -108,7 +63,7 @@ class iGeant4:
     ##
     ## choose between new ISF G4 Python Configuration...
     ##   (work in progress)
-    if UseNewConfiguration:
+    if UseNewG4Configuration:
       ## Set up the appropriate sim skeleton in the G4 engine
       #from ISF_Geant4Config.ISF_AtlasSimSkeleton import ISF_AtlasSimSkeleton
       #dummy = ISF_AtlasSimSkeleton( self.G4Eng )
