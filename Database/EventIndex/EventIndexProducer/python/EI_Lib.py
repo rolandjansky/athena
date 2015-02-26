7455db6911adef5a0cbe2f87a91b2048f782d676
Javier Sanchez <javier.sanchez@cern.ch>
2015-02-26 16:19:27 +0100

    Tagging the 00.02.07 release (EventIndexProducer-00-02-07)

--- Database/EventIndex/EventIndexProducer/python/EI_Lib.py
+++ Database/EventIndex/EventIndexProducer/python/EI_Lib.py
@@ -13,33 +13,81 @@ class IOV(object):
     def __init__(self):
         pass
 
+    def cmpBN(self,a,b):
+
+        # compare a and b and return -1,0,1 if a is <,=,> than b
+        # a (and b) are represented as a pair of numbers (most significant part, least significant part)
+
+        try:
+            (r,e) = a
+            (r1,e1) = b
+        except:
+            raise TypeError("element must be a tuple with two components")
+
+        c = cmp(r,r1)       # compare most significant part
+        if c == 0:
+            c = cmp(e,e1)   # if equal, compare least significant part
+
+        return c
+
+
+    def isInInterval(self,point,interval):
+
+        try:
+            if len(interval) != 4:
+                raise TypeError("interval must contain 4 elements")
+        except:
+            raise TypeError("interval must be a tuple or list")
+
+        # broke down the two parts of the interval
+
+        i1 = interval[:2]
+        i2 = interval[2:]
+
+        # look if point belongs to [i1,i2[
+        if self.cmpBN(point,i1) >= 0 and self.cmpBN(point,i2) < 0:
+            return True
+        else:
+            return False
+
     def add(self,key,value,interval):
         if key not in self._iovs:
             self._iovs[key]=[]
-        self._iovs[key].append((interval,value))
+
+        try:
+            if len(interval) != 4:
+                raise TypeError("interval must contain 4 elements")
+        except:
+            raise TypeError("interval must be a tuple or list")
+
+        try:
+            # convert inverval into longs to prevent lexicographic comparison
+            interval2=[]
+            for number in interval:
+                interval2.append(long(number))
+        except:
+            raise TypeError("interval ranges must be a integers or longs")
+        
+        self._iovs[key].append((interval2,value))
 
     def dump(self):
         for k,v in self._iovs.iteritems():
-            print k,v
+            print k
+            for e in v:
+                print " ", e
 
     def get(self,key,point):
-        (r,e)=point
         if key not in self._iovs:
             return None
         if key in self._cached:
             i,v = self._cached[key]
-            (r0,e0,r1,e1) = i
-            if r >= r0 and ( e >= e0  or e0 == 0 ) and \
-                    r <= r1 and ( e < e1 or e1 == 0 ):
+            if self.isInInterval(point,i):
                 return v
         ivalues=self._iovs[key]
         for i,v in ivalues:
-            (r0,e0,r1,e1) = i
-            if r >= r0 and ( e >= e0 or e0 == 0 ) and \
-                    r <= r1 and ( e < e1 or e1 == 0 ):
+            if self.isInInterval(point,i):
                 self._cached[key]=(i,v)
                 return v
-            
         return None
 
 
