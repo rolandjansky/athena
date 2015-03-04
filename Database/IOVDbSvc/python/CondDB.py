b21ef1e47129e474fdd76d34e2e0c9c7984bc10c
Walter Lampl <Walter.Lampl@cern.ch>
2015-03-04 15:52:36 +0100

    change fallback database instance form COMP200 to CONDBR (eg run2 is now default) (IOVDbSvc-01-07-03)

--- Database/IOVDbSvc/python/CondDB.py
+++ Database/IOVDbSvc/python/CondDB.py
@@ -356,8 +356,8 @@ class CondDB:
         try:
             year=int(projectName[4:6]);
         except:
-            self.msg.error("Failed to extract year from project tag "+ projectName+". Guessing run1")
-            return "COMP200"
+            self.msg.error("Failed to extract year from project tag "+ projectName+". Guessing run2")
+            return "CONDBR2"
         
         if (year>13):
             return "CONDBR2"
