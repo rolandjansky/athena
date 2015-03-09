9c281ea35393fb9addc4ddb32913e88292d473e0
Susumu Oda <Susumu.Oda@cern.ch>
2015-03-09 15:16:01 +0100

    test/InDetSLHC_Example_TestConfiguration.xml - update to partly use Sim_tf.py instead of AtlasG4_tf.py and Ring geometry. (InDetSLHC_Example-00-01-16-03)

---
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/postInclude.SLHC_Setup.py
@@ -0,0 +1,6 @@
+#--------------------------------------------------------------
+# SLHC setup
+#--------------------------------------------------------------
+# set the path variables consistently
+from InDetSLHC_Example.SLHC_Setup import SLHC_Setup
+SLHC_Setup = SLHC_Setup()
