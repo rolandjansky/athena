7892dca1e7f30047e1d2ac4f7ea93f09519734f3
Simon George <s.george@rhul.ac.uk>
2015-03-10 09:34:46 +0100

    'correct L1Topo source ID numbering' (TrigOnlineMonitor-20-00-11)

---
+++ HLT/Trigger/TrigMonitoring/TrigOnlineMonitor/src/components/TrigOnlineMonitor_entries.cxx
@@ -0,0 +1,14 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "TrigOnlineMonitor/TrigROBMonitor.h"
+#include "TrigOnlineMonitor/TrigMuCTPiROBMonitor.h"
+#include "src/TrigL1TopoROBMonitor.h"
+
+DECLARE_ALGORITHM_FACTORY( TrigROBMonitor )
+DECLARE_ALGORITHM_FACTORY( TrigMuCTPiROBMonitor )
+DECLARE_ALGORITHM_FACTORY( TrigL1TopoROBMonitor )
+
+DECLARE_FACTORY_ENTRIES(TrigOnlineMonitor) {
+    DECLARE_ALGORITHM( TrigROBMonitor )
+    DECLARE_ALGORITHM( TrigMuCTPiROBMonitor )
+    DECLARE_ALGORITHM( TrigL1TopoROBMonitor )
+}
