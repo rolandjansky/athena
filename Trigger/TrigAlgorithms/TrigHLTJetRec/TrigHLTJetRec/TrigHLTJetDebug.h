18513ffe4a2f2bd89833ad3a494607a35273fd0c
Peter Sherwood <sherwood@cern.ch>
2015-03-11 09:24:20 +0100

    rndseed=1 (TrigHLTJetRec-00-00-39)
    	* tagging TrigHLTJetRec-00-00-39
    
    	* python/TrigHLTJetRecConfig.py (_getJetBuildTool):
    	set rndseed to 1 so that random number generator used
    	for pile-up subtraction is seeeded from the event number
    
    	* commented out obsolete classes TrigHLTJetRec_AntiKt04,
    	TrigHLTJetRec_AntiKt10
    
    2015-03-03   Caterina Doglioni <caterina.doglioni@cern.ch>
    
            * TrigHLTJetDSSelector: demoted INFO messages to DEBUG
    	* tagging TrigHLTJetRec-00-00-38
    
    2015-02-23   Peter Sherwood  <peter@localhost>
    
    	* tagging TrigHLTJetRec-00-00-37
    
    2015-02-20   Peter Sherwood  <peter@localhost>
    
    ...
    (Long ChangeLog diff - truncated)

---
+++ Trigger/TrigAlgorithms/TrigHLTJetRec/TrigHLTJetRec/TrigHLTJetDebug.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRIGHLTJETREC_TRIGHLTJETDEBUG_H
+#define TRIGHLTJETREC_TRIGHLTJETDEBUG_H
+
+#include "TrigInterfaces/HypoAlgo.h"
+#include "StoreGate/StoreGateSvc.h"
+
+class TrigHLTJetDebug: public HLT::HypoAlgo {
+	
+public:
+  TrigHLTJetDebug(const std::string& name, ISvcLocator* pSvcLocator);
+  ~TrigHLTJetDebug();
+
+  HLT::ErrorCode hltInitialize();
+  HLT::ErrorCode hltFinalize();
+  HLT::ErrorCode hltExecute(const HLT::TriggerElement* inputTE, bool& pass);
+
+
+private:
+  std::string m_chainName;
+  std::string m_eventShapeSGKey;
+
+  /** @brief Pointer to StoreGateSvc service.*/
+  StoreGateSvc* m_storeGate;
+
+}; 
+
+#endif
