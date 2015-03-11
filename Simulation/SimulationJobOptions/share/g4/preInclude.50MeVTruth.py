243f5f22c238ab0fc71188c549acd74181ac42c2
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-03-11 18:03:34 +0100

    add share/g4/postInclude.50MeV_MC15Truth.py: alternative approach to preInclude.50MeVTruth.py for ISF. Tagging: SimulationJobOptions-00-06-06 (SimulationJobOptions-00-06-06)
    	* Tagging: SimulationJobOptions-00-06-06
    	* add share/g4/postInclude.50MeV_MC15Truth.py: alternative
    	approach to preInclude.50MeVTruth.py for ISF.
    
    2015-03-11  Zach Marshall <ZLMarshall@lbl.gov>
    
    	* Tagging: SimulationJobOptions-00-06-05
    	* Fix to truth strategy naming obo Elmar
    	* Moving up init of truth strategy fragment in the case of ISF
    
    2015-03-10  Zach Marshall <ZLMarshall@lbl.gov>
    
    	* Tagging: SimulationJobOptions-00-06-04
    	* Ensuring that ISF is also correctly configured for 50 MeV truth
    
    2015-02-24  Zach Marshall <ZLMarshall@lbl.gov>
    
    	* Tagging: SimulationJobOptions-00-06-03
    	* add share/g4/preInclude.HyperspaceCatcher.py - new fragment for enabling
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/SimulationJobOptions/share/g4/preInclude.50MeVTruth.py
+++ Simulation/SimulationJobOptions/share/g4/preInclude.50MeVTruth.py
@@ -8,19 +8,35 @@
 #########################################################
 
 def reset_truthstrategies_50():
-    from G4AtlasApps import AtlasG4Eng
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("BremsPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("BremsSecondaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ComptonPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ComptonSecondaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ConversionPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ConversionSecondaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("DecayPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("DecaySecondaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("IonizationPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("IonizationSecondaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("HadronicPrimaryMinEnergy", 50)
-    AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("HadronicSecondaryMinEnergy", 50)
+    from G4AtlasApps.SimFlags import simFlags
+    if not simFlags.ISFRun:
+        from G4AtlasApps import AtlasG4Eng
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("BremsPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("BremsSecondaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ComptonPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ComptonSecondaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ConversionPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("ConversionSecondaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("DecayPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("DecaySecondaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("IonizationPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("IonizationSecondaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("HadronicPrimaryMinEnergy", 50)
+        AtlasG4Eng.G4Eng.menu_MCTruth.set_TruthStrategiesParameter("HadronicSecondaryMinEnergy", 50)
+    else:
+        from AthenaCommon.AppMgr import ToolSvc
+        from AthenaCommon.SystemOfUnits import MeV
+        ToolSvc.ISF_MCTruthStrategyGroupID_MC15.PrimaryMinPt = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupID_MC15.SecondaryMinPt = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupIDHadInt_MC15.PrimaryMinPt = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupIDHadInt_MC15.SecondaryMinPt = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupCaloMuBrem.PrimaryMinEkin = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupCaloMuBrem.SecondaryMinEkin = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupCaloDecay_MC15.PrimaryMinEkin = 50.*MeV
+        ToolSvc.ISF_MCTruthStrategyGroupCaloDecay_MC15.SecondaryMinEkin = 50.*MeV
+
+the_stage = 'preInitMCTruth'
+if simFlags.ISFRun: the_stage = 'preInit'
 
 from G4AtlasApps.SimFlags import simFlags
-simFlags.InitFunctions.add_function("preInitMCTruth", reset_truthstrategies_50)
+simFlags.InitFunctions.add_function(the_stage, reset_truthstrategies_50)
