6f67d618ee6f2e4bd08155084a6a98710bf361ac
Scott Snyder <scott.snyder@cern.ch>
2015-03-11 19:48:12 +0100

    Fix coverity warnings. (CoWTools-00-00-06)
    	* Tagging CoWTools-00-00-06.
    	* CoWTools/CoWRecordStats.h: Another couple coverity warnings.
    	* Tagging CoWTools-00-00-05.
    	* Fix coverity warnings.
    
    	* Tagging CoWTools-00-00-04.
    
    2015-02-27 sami kama<sami.kama@cern.ch>
    	* Adding detailed monitoring information

--- Control/CoWTools/CoWTools/CoWRecord.h
+++ Control/CoWTools/CoWTools/CoWRecord.h
@@ -11,17 +11,60 @@
 namespace CoWTools{
   class CoWRecord{
   public:
-    CoWRecord(bool summary=true):ms(summary)/*,_summary(summary)*/{}
+    CoWRecord(bool summary=true):
+      addrStart(0),
+      addrEnd(0),
+      perms(0),
+      offset(0),
+      inode(0),
+      m_ms(summary),
+      dmin(0),
+      dmaj(0),
+      m_summary(summary)
+    {}
     unsigned long addrStart,addrEnd;
     short perms;
     unsigned long long offset;
     unsigned long inode;
-    CoWRecordStats ms;
+    CoWRecordStats m_ms;
     unsigned int dmin,dmaj;
-    void parseRecord(std::istream& in);
+    void parseRecord(std::istream& in);    
   private:
-    //bool m_summary;
+    bool m_summary;
+  public:
+    friend CoWRecord operator-(CoWRecord lhs, const CoWRecord& rhs){
+      return lhs-=rhs;
+    }
+    CoWRecord operator-()const{
+      CoWRecord m(*this);
+      m.m_ms=-m_ms;
+      return m;
+    }
+    explicit operator bool() const {
+      auto vals=m_ms.getValueArray();
+      for(int i=0;i<11;i++){
+	if(vals[i])return true;
+      }
+      return false;
+    }
+
+    friend CoWRecord operator+(CoWRecord lhs, const CoWRecord& rhs){
+      return lhs+=rhs;
+    }
+
+    inline CoWRecord& operator+=(const CoWRecord& rhs){
+      perms|=rhs.perms;
+      m_ms+=rhs.m_ms;
+      return *this;
+    }
+
+    inline CoWRecord& operator-=(const CoWRecord& rhs){
+      perms^=rhs.perms;
+      m_ms-=rhs.m_ms;
+      return *this;
+    }
   };
+
 }//end namespace
 
 #endif
