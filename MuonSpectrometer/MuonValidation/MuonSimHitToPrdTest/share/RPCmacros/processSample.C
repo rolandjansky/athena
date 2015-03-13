dad0f51b25c98419f4ee2614d015ffe9bd56653c
Jochen Meyer <Jochen.Meyer@cern.ch>
2015-03-13 15:50:05 +0100

    commenting dumpOffToOnlineMapToFile (MuonSimHitToPrdTest-00-00-13)
    Former-commit-id: 903d435205d5aaa1c50ba34befda6a283cb80331

--- MuonSpectrometer/MuonValidation/MuonSimHitToPrdTest/share/RPCmacros/processSample.C
+++
@@ -1,8 +0,0 @@
-void processSample()
-{
-  gROOT->ProcessLine(".L validateDigitization.C++");
-  gROOT->ProcessLine("validateDigitization myValidator;");
-  gROOT->ProcessLine("myValidator.Loop()");
-
-  //  gROOT->ProcessLine(".x showResHistos.C")
-}
