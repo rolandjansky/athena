a8c66d36946d3add26ce59ac8bdd602382c778f0
James Frost <frost@hep.phy.cam.ac.uk>
2015-03-17 11:11:04 +0100

    re-enable MuonTrackMon by default (AthenaMonitoring-02-01-13-03)
    	* python/DQMonFlags.py re-enable MuonTrackMon by default
    	* tag as AthenaMonitoring-02-01-13-03

--- Control/AthenaMonitoring/python/DQMonFlags.py
+++ Control/AthenaMonitoring/python/DQMonFlags.py
@@ -178,7 +178,7 @@ class doMuonTrackMon(JobProperty):
     """ Switch for muon track monitoring """
     statusOn=True
     allowedTypes=['bool']
-    StoredValue=False
+    StoredValue=True
 list+=[doMuonTrackMon]
 
 class doMuonAlignMon(JobProperty):
