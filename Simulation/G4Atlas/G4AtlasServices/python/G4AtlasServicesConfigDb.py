5686e33bf54277fbe25e4877cc36add3ea121ac3
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-03-18 18:01:46 +0100

    Reverting changes tagged in G4AtlasServices-00-00-17, G4AtlasServices-00-00-14, G4AtlasServices-00-00-12, G4AtlasServices-00-00-07. Branch without geometry changes for 20.3.0. Tagging: G4AtlasServices-00-00-17-01 (G4AtlasServices-00-00-17-01)
    	* Tagging: G4AtlasServices-00-00-17-01
    	* Branch without geometry changes for 20.3.0
    	* Reverting changes tagged in G4AtlasServices-00-00-17,
    	G4AtlasServices-00-00-14, G4AtlasServices-00-00-12,
    	G4AtlasServices-00-00-07.
    
    2015-03-26  ADA <dellacqu@mail.cern.ch>
    	* Tagging: G4AtlasServices-00-00-17
    	* Modified DetectorGeometrySvc to append trailing detector scope to LV and PV names
    	* Added Logical and Physical volume notifiers
    
    2015-03-11  Zach Marshall <ZLMarshall@lbl.gov>
    	* Tagging: G4AtlasServices-00-00-16
    	* Moving fast simulation service to a master tool (out of this package)
    
    2015.03-10  Zach Marshall <ZLMarshall@lbl.gov>
    	* Tagging: G4AtlasServices-00-00-15
    	* Moving SD Service over to a master tool (out of this package)
    
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/G4Atlas/G4AtlasServices/python/G4AtlasServicesConfigDb.py
+++ Simulation/G4Atlas/G4AtlasServices/python/G4AtlasServicesConfigDb.py
@@ -1,5 +1,3 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.CfgGetter import addService
-addService("G4AtlasServices.G4AtlasServicesConfig.getSensitiveDetectorSvc","SensitiveDetectorSvc")
-addService("G4AtlasServices.G4AtlasServicesConfig.getFastSimulationSvc","FastSimulationSvc")
