156d586428480073149274828a62c177371dc515
Valentina Cairo <valentina.maria.cairo@cern.ch>
2015-03-18 18:33:49 +0100

    modified jobOptions (SCTExtension-00-00-02)

---
+++ InnerDetector/InDetPerformance/SCTExtension/src/components/SCTExtension_entries.cxx
@@ -0,0 +1,6 @@
+#include "SCTExtension/SCTExtensionAlg.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+DECLARE_ALGORITHM_FACTORY( SCTExtensionAlg )
+DECLARE_FACTORY_ENTRIES(SCTExtension) {
+DECLARE_ALGORITHM( SCTExtensionAlg )
+}
