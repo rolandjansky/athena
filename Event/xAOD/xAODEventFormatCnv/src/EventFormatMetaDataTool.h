afb7999f48a36c7aec166e922e5fc459186aaecd
Attila Krasznahorkay <krasznaa@cern.ch>
2015-03-23 12:28:48 +0100

    Updating EventFormatMetaDataTool according to the discussion on ATLASRECTS-1787 (xAODEventFormatCnv-00-00-08)

---
+++ Event/xAOD/xAODEventFormatCnv/src/EventFormatMetaDataTool.h
@@ -0,0 +1,77 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: EventFormatMetaDataTool.h 651874 2015-03-05 14:16:19Z krasznaa $
+#ifndef XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
+#define XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
+
+// System include(s):
+#include <string>
+#include <memory>
+
+// Gaudi/Athena include(s):
+#include "GaudiKernel/IIncidentListener.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "AthenaPoolKernel/IMetaDataTool.h"
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "StoreGate/StoreGateSvc.h"
+
+// EDM include(s):
+#include "xAODEventFormat/EventFormat.h"
+
+namespace xAODMaker {
+
+   /// Tool taking care of copying the event format object from file to file
+   ///
+   /// This tool does the heavy lifting when fast-merging DxAOD files to
+   /// make sure that the xAOD::EventFormat metadata object is propagated
+   /// correctly from the input files to the output.
+   ///
+   /// @author Jack Cranshaw <cranshaw@anl.gov>
+   /// @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
+   ///
+   /// $Revision: 651874 $
+   /// $Date: 2015-03-05 15:16:19 +0100 (Thu, 05 Mar 2015) $
+   ///
+   class EventFormatMetaDataTool : public virtual ::IMetaDataTool,
+                                   public virtual ::IIncidentListener,
+                                   public ::AthAlgTool {
+
+   public:
+      /// Regular AlgTool constructor
+      EventFormatMetaDataTool( const std::string& type, const std::string& name,
+                               const IInterface* parent );
+
+      /// Function initialising the tool
+      virtual StatusCode initialize();
+
+      /// Function called by the incident service
+      virtual void handle( const Incident& inc );
+
+   private:
+      /// Function collecting the event format metadata from the input file
+      StatusCode collectMetaData();
+      /// Function writing out the collected metadata
+      StatusCode writeMetaData();
+
+      /// Connection to the input metadata store
+      ServiceHandle< ::StoreGateSvc > m_inputMetaStore;
+      /// Connection to the output metadata store
+      ServiceHandle< ::StoreGateSvc > m_outputMetaStore;
+
+      /// The key of the trigger menu in the input file
+      std::string m_inputKey;
+      /// The key of the trigger menu for the output file
+      std::string m_outputKey;
+
+      /// The merged event format object
+      std::unique_ptr< xAOD::EventFormat > m_format;
+
+   }; // class EventFormatMetaDataTool
+
+} // namespace xAODMaker
+
+#endif // XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
--- Event/xAOD/xAODEventFormatCnv/src/components/xAODEventFormatCnv_entries.cxx
+++ Event/xAOD/xAODEventFormatCnv/src/components/xAODEventFormatCnv_entries.cxx
@@ -1,15 +1,18 @@
-// $Id: xAODEventFormatCnv_entries.cxx 578438 2014-01-15 09:34:19Z krasznaa $
+// $Id: xAODEventFormatCnv_entries.cxx 655221 2015-03-18 21:27:44Z cranshaw $
 
 // Gaudi/Athena include(s):
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Local include(s):
 #include "../EventFormatSvc.h"
+#include "../EventFormatMetaDataTool.h"
 
 DECLARE_NAMESPACE_SERVICE_FACTORY( xAODMaker, EventFormatSvc )
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, EventFormatMetaDataTool )
 
 DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
 
    DECLARE_NAMESPACE_SERVICE( xAODMaker, EventFormatSvc )
+   DECLARE_NAMESPACE_TOOL( xAODMaker, EventFormatMetaDataTool )
 
 }
