76cecb82778ab1eb28b5a67bd93894d67a4152d2
Ladislav Chytka <Ladislav.Chytka@cern.ch>
2015-03-31 14:17:01 +0200

    Coverity issue fixed (ForwardRegion_EventTPCnv-00-00-07)

--- ForwardDetectors/ForwardSimulation/ForwardRegion_EventCnv/ForwardRegion_EventTPCnv/ForwardRegion_EventTPCnv/SimulationHit_p1.h
+++ ForwardDetectors/ForwardSimulation/ForwardRegion_EventCnv/ForwardRegion_EventTPCnv/ForwardRegion_EventTPCnv/SimulationHit_p1.h
@@ -14,7 +14,34 @@ class SimulationHit_p1
 {
  public:
 	// needed by athenaRoot 
-	SimulationHit_p1(){};
+        SimulationHit_p1(){
+            m_pre_time= 0;
+            m_pre_position_x= 0;
+            m_pre_position_y= 0;
+            m_pre_position_z= 0;
+            m_pre_kinetic_energy= 0;
+            m_pre_direction_x= 0;
+            m_pre_direction_y= 0;
+            m_pre_direction_z= 0;
+            m_pre_momentum_x= 0;
+            m_pre_momentum_y= 0;
+            m_pre_momentum_z= 0;
+            m_post_time= 0;
+            m_post_position_x= 0;
+            m_post_position_y= 0;
+            m_post_position_z= 0;
+            m_post_kinetic_energy= 0;
+            m_post_direction_x= 0;
+            m_post_direction_y= 0;
+            m_post_direction_z= 0;
+            m_post_momentum_x= 0;
+            m_post_momentum_y= 0;
+            m_post_momentum_z= 0;
+            m_deposit_energy= 0;
+            m_pdgcode= 0;
+            m_track_id= 0;
+            m_parent_id= 0;
+        };
 	friend class SimulationHitCnv_p1;
 
 private:
@@ -40,7 +67,7 @@ private:
     double m_post_momentum_x;
     double m_post_momentum_y;
     double m_post_momentum_z;
-	double m_deposit_energy;
+    double m_deposit_energy;
     int m_pdgcode;
     int m_track_id;
     int m_parent_id;
