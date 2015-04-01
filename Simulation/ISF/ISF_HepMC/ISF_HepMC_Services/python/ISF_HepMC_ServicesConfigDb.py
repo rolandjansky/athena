1b2f885a9e645b26fc9eb3f66051a18a9f6b721b
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2015-04-01 08:36:13 +0200

    adding null-pointer check for secondary particles returned by TruthIncident (ISF_HepMC_Services-00-03-07)
    	* src/HepMC_TruthSvc.cxx - adding null-pointer check for secondary
    	particles returned by TruthIncident
    	* tag ISF_HepMC_Services-00-03-07
    
    2015-03-28  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ISF_HepMC_ServicesConfig.py,
    	python/ISF_HepMC_ServicesConfigDb.py - add getMC15aTruthService,
    	which has the same configuration as
    	getMC15TruthService. ATLASSIM-1909
    	* tag ISF_HepMC_Services-00-03-06

--- Simulation/ISF/ISF_HepMC/ISF_HepMC_Services/python/ISF_HepMC_ServicesConfigDb.py
+++ Simulation/ISF/ISF_HepMC/ISF_HepMC_Services/python/ISF_HepMC_ServicesConfigDb.py
@@ -3,5 +3,6 @@
 from AthenaCommon.CfgGetter import addTool, addService, addAlgorithm
 addService("ISF_HepMC_Services.ISF_HepMC_ServicesConfig.getTruthService"              , "ISF_TruthService")
 addService("ISF_HepMC_Services.ISF_HepMC_ServicesConfig.getMC12TruthService"          , "ISF_MC12TruthService")
+addService("ISF_HepMC_Services.ISF_HepMC_ServicesConfig.getMC15aTruthService"         , "ISF_MC15aTruthService")
 addService("ISF_HepMC_Services.ISF_HepMC_ServicesConfig.getMC15TruthService"          , "ISF_MC15TruthService")
 addService("ISF_HepMC_Services.ISF_HepMC_ServicesConfig.getValidationTruthService"    , "ISF_ValidationTruthService")
