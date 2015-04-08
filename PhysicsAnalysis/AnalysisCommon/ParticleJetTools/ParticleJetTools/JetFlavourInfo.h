0dd2d258dd5c7c7cf3b3e58be33b7293e9edc278
Pierre-Antoine Delsart <delsart@in2p3.fr>
2015-04-08 08:54:10 +0200

    'ATLJETMET-309. Recopy isNonInteracting locally' (ParticleJetTools-00-03-33-01)
    	* ATLJETMET-309
    	* Root/CopyTruthJetParticles.cxx: prevent inclusion of invisble
    	particles. Re-copying privately isNonInteracting() so the fix is cacheable.
    	* ParticleJetTools-00-03-33-01
    
    2015-04-04    <delsart@localhost>
    	* tagging latest change
    	* ParticleJetTools-00-03-33
    
    2015-03-30 Frank Filthaut
    	* add exclusive-cone labelling retrieval code
    
    2015-03-27    <delsart@localhost>
    	* ATLJETMET-306
    	* removed FindLeptonTruth which depends on JetTagInfo. This was
    	incompatible with AthAnalysisBase.
    	* tagging ParticleJetTools-00-03-32
    
    2015-03-25    <delsart@localhost>
    	* ATLJETMET-283
    ...
    (Long ChangeLog diff - truncated)

--- PhysicsAnalysis/AnalysisCommon/ParticleJetTools/ParticleJetTools/JetFlavourInfo.h
+++ PhysicsAnalysis/AnalysisCommon/ParticleJetTools/ParticleJetTools/JetFlavourInfo.h
@@ -21,11 +21,14 @@ namespace xAOD {
   int GAInitialHadronFlavourLabel (const xAOD::Jet* jet);
   int GAFinalPartonFlavourLabel (const xAOD::Jet* jet);
 
-  // The last method uses the "traditional" (Run-1) cone-based labelling
+  // The following methods use the "traditional" (Run-1) cone-based labelling and a "safer" successor
+  // (safer because it uses hadrons, and becauses it uses an exclusive labelling -- it will associate
+  // a HF hadron only with a single jet)
 
   int ConeFinalPartonFlavourLabel (const xAOD::Jet* jet);
+  int ExclusiveConeHadronFlavourLabel (const xAOD::Jet* jet);
 
-  enum JetFlavourLabelType { GAFinalHadron, GAInitialHadron, GAFinalParton, ConeFinalParton };
+  enum JetFlavourLabelType { GAFinalHadron, GAInitialHadron, GAFinalParton, ConeFinalParton, ExclConeHadron };
 
   // Provide a unified interface to all methods
   int jetFlavourLabel (const xAOD::Jet* jet, JetFlavourLabelType = GAFinalHadron);
--- PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/CopyTruthJetParticles.cxx
+++ PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/CopyTruthJetParticles.cxx
@@ -7,30 +7,62 @@
 #include "xAODTruth/TruthVertex.h"
 #include "TruthUtils/PIDUtils.h"
 
+#include "xAODTruth/TruthParticleContainer.h"
+#include "xAODTruth/TruthParticleAuxContainer.h"
+#include "xAODTruth/TruthEventContainer.h"
+#include "AthContainers/ConstDataVector.h"
+#include "AsgTools/Check.h"
+
+
+#ifndef XAOD_STANDALONE
+// Usage of metadata is for now only possible in Athena...
+//#include "CoralBase/AttributeListException.h"
+#include "AthAnalysisBaseComps/AthAnalysisHelper.h"
+#endif
+
 using namespace std;
 
+namespace MC {
+
+  /// TEMPORARY copy from TruthUtils/ in the ParticleJetTools-00-03-33-branch
+  /// @brief Identify if the particle with given PDG ID would not interact with the detector, i.e. not a neutrino or WIMP
+  /// @deprecated Use MCUtils functions instead, e.g. isStronglyInteracting || isEMInteracting
+  inline bool isNonInteracting(int pid) {
+    const int apid = abs(pid);
+    if (apid == 12 || apid == 14 || apid == 16) return true; //< neutrinos
+    if (apid == 1000022 || apid == 1000024 || apid == 5100022) return true; // SUSY & KK photon and Z partners
+    if (apid == 39 || apid == 1000039 || apid == 5000039) return true; //< gravitons: standard, SUSY and KK
+    if (apid == 9000001 || apid == 9000002 || apid == 9000003 || apid == 9000004 || apid == 9000005 || apid == 9000006) return true; //< exotic particles from monotop model
+    return false;
+  }
+
+}
 
 CopyTruthJetParticles::CopyTruthJetParticles(const std::string& name)
   : CopyTruthParticles(name) ,
     m_maxAbsEta(5)
+  , m_barcodeOffset(200000)
+  , m_barcodeFromMetadata(2)
 {
   declareProperty("IncludeNeutrinos",  m_includeNu=false, "Include neutrinos in the output collection");
   declareProperty("IncludeMuons",      m_includeMu=false, "Include muons in the output collection");
   declareProperty("IncludeWZLeptons",  m_includeWZ=true,  "Include leptons from W/Z decays in the output collection");
   declareProperty("IncludeTauLeptons", m_includeTau=true, "Include leptons from tau decays in the output collection");
   declareProperty("MaxAbsEta", m_maxAbsEta);
+  declareProperty("BarCodeOffset", m_barcodeOffset);
+  declareProperty("BarCodeFromMetadata", m_barcodeFromMetadata);
 }
 
-bool CopyTruthJetParticles::classify(const xAOD::TruthParticle* tp) const {
+bool CopyTruthJetParticles::classifyJetInput(const xAOD::TruthParticle* tp, int barcodeOffset) const {
 
   // Check if this thing is a candidate to be in a truth jet
   //  First block is largely copied from isGenStable, which works on HepMC only
-  if (tp->barcode()>=200000) return false; // Particle is from G4
+  if (tp->barcode()>=barcodeOffset) return false; // Particle is from G4
   if (tp->pdgId()==21 && tp->e()==0) return false; // Work around for an old generator bug
-  if ( tp->status()%1000!=1 ) return false; // Stable!
+  if ( tp->status()!=1 ) return false; // Stable!
   
   // Easy classifiers by PDG ID
-  if (!m_includeNu && MC::PID::isNeutrino(tp->pdgId())) return false;
+  if (!m_includeNu && MC::isNonInteracting(tp->pdgId())) return false;
   if (!m_includeMu && abs(tp->pdgId())==13) return false;
 
   // Cannot use the truth helper functions; they're written for HepMC
@@ -87,3 +119,81 @@ bool CopyTruthJetParticles::fromTau( const xAOD::TruthParticle* tp ) const
   }
   return false;
 }
+
+
+int CopyTruthJetParticles::execute() const {
+
+  //*******************************
+  // retrieve barcode Offset for this event from metadata.
+  // We'd need a cleaner solution where this offset is set only at 
+  // each new file, but this requires some tool interface which does 
+  // not exist in RootCore yet. 
+  // So we use the less disruptive solution in Athena for now...
+  int barcodeOffset = m_barcodeOffset;
+  if(m_barcodeFromMetadata>0){
+    bool found = false;
+    // retrieve the value for the current sample from metadata
+#ifndef XAOD_STANDALONE
+    // Usage of metadata is only possible in Athena (not supported by dual-use tools yet)...
+    int barcodeOffset_tmp(0);
+    ATH_MSG_DEBUG(" Look for barcode offset in  metadata ... ");
+    try {
+      StatusCode sc= AthAnalysisHelper::retrieveMetadata("/Simulation/Parameters","SimBarcodeOffset",barcodeOffset_tmp) ;
+      found = sc.isSuccess();
+    } catch(std::exception &e) {
+      ATH_MSG_DEBUG(" Could not retrieve barcode offset in metadata  : "<< e.what());
+    }
+
+    if(found){
+      barcodeOffset = barcodeOffset_tmp;
+      ATH_MSG_DEBUG(" Retrieved from metadata :  "<< barcodeOffset);
+    } else {
+      if(m_barcodeFromMetadata==1) {
+        ATH_MSG_ERROR( "Can not retrieve metadata info  /Simulation/Parameters SimBarcodeOffset ");
+        return 1;
+      }
+      // m_barcodeFromMetadata == 2
+      ATH_MSG_DEBUG(" NOT Retrieved from metadata, use default   "<< barcodeOffset << " _ "<< m_barcodeOffset);
+    }
+#else // standalone :
+    ATH_MSG_ERROR(" Can't retrieve automatically the truth barcode offset outside Athena. Please set the CopyTruthJetParticles::BarCodeOffset for this specific sample");
+    return 1;
+#endif
+  }
+
+
+
+  /// we recopy the CopyTruthParticles::execute() below, passing the barcodeOffset to the classify function.
+  //  we can not change m_barcodeOffset param since this is a const method.
+
+  // Retrieve the xAOD truth objects
+  const xAOD::TruthEventContainer* xTruthEventContainer = NULL;
+  ASG_CHECK( evtStore()->retrieve( xTruthEventContainer, "TruthEvents"));
+
+  // Make a new TruthParticleContainer and link it to StoreGate
+  if (evtStore()->contains<xAOD::TruthParticleContainer>(m_outputname))
+    ATH_MSG_ERROR("Tag input TruthParticleContainer " << m_outputname << " already exists");
+
+  ConstDataVector<xAOD::TruthParticleContainer> *ipc = new ConstDataVector<xAOD::TruthParticleContainer>(SG::VIEW_ELEMENTS);
+  if (evtStore()->record(ipc, m_outputname).isFailure())
+    ATH_MSG_ERROR("Failed to record a new TruthParticleContainer " << m_outputname);
+
+  // Classify particles for tagging and add to the TruthParticleContainer
+  const xAOD::TruthEvent* evt = *xTruthEventContainer->begin();
+  size_t numCopied = 0;
+  for (unsigned int ip = 0; ip < evt->nTruthParticles(); ++ip) {
+    const xAOD::TruthParticle* tp = evt->truthParticle(ip);
+    if (tp->pt() < m_ptmin)
+        continue;
+
+    if (classifyJetInput(tp, barcodeOffset)) { // Modification w.r.t CopyTruthParticles : pass the barcodeoffset argument
+      ipc->push_back(tp);
+      numCopied += 1;
+    }
+  }
+
+  ATH_MSG_DEBUG("Copied " << numCopied << " truth particles into " << m_outputname << " TruthParticle container");
+
+  return 0;
+}
+
