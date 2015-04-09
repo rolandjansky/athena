0863e1356d970e6f990b768c764c7aca31197b08
Edward Moyse <Edward.Moyse@cern.ch>
2015-04-09 14:45:31 +0200

    'Only try to write FitQuality if it exists (ATLASRECTS-1844). Removed debug output' (TrkEventTPCnv-01-09-18)
      * Only try to write FitQuality if it exists (ATLASRECTS-1844)
      * Removed debug output
      * Tagged as TrkEventTPCnv-01-09-18

--- Tracking/TrkEventCnv/TrkEventTPCnv/src/TrkTrack/TrackCnv_p4.cxx
+++ Tracking/TrkEventCnv/TrkEventTPCnv/src/TrkTrack/TrackCnv_p4.cxx
@@ -66,8 +66,11 @@ void TrackCnv_p4::transToPers( const Trk::Track    *transObj,
   }
   
   assert(transObj->fitQuality());
-  persObj->m_chiSquared = transObj->m_fitQuality->m_chiSquared;
-  persObj->m_numberDoF  = transObj->m_fitQuality->m_numberDoF;
-  
+  if (transObj->m_fitQuality){
+    persObj->m_chiSquared = transObj->m_fitQuality->m_chiSquared;
+    persObj->m_numberDoF  = transObj->m_fitQuality->m_numberDoF;
+  } else {
+    log<<MSG::WARNING<<"No FitQuality on track at ["<<transObj<<"]"<<" with info="<<transObj->info().dumpInfo()<<endreq;
+  }
   m_trackStateVectorCnv.transToPers( transObj->m_trackStateVector, &persObj->m_trackState, log );
 }
