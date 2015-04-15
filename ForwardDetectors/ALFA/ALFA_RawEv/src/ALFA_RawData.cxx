e0df2e9059f0c2dfab8a7bb6eda01ab6a3526959
Scott Snyder <scott.snyder@cern.ch>
2015-04-15 03:01:06 +0200

    Fix checkreq warnings. (ALFA_RawEv-00-00-18)
    Former-commit-id: fc21daa9ba3e4e5a0b77696c2106595baee6baec

--- ForwardDetectors/ALFA/ALFA_RawEv/src/ALFA_RawData.cxx
+++ ForwardDetectors/ALFA/ALFA_RawEv/src/ALFA_RawData.cxx
@@ -6,31 +6,33 @@
 #include "GaudiKernel/MsgStream.h"
 
 ALFA_RawData::ALFA_RawData():
-  m_WordId(0),
-  m_PMFId(0),  
-  m_MBId(0), 
-  m_EventCount(0),
-  m_MarocChan(),
+  WordId(0),
+  PMFId(0),  
+  MBId(0), 
+  EventCount(0),
+  MarocChan(),
   m_bit16(0),
   m_bit18(0),
   m_error_bit17(0),
   m_bit26_27(0),
   m_bit24_27(0)
 {
+  p_dataWords = new std::vector<uint32_t>();
 }
 
 ALFA_RawData::ALFA_RawData(uint16_t PMFId):
-  m_WordId(0),
-  m_PMFId(PMFId),  
-  m_MBId(0), 
-  m_EventCount(0),
-  m_MarocChan(),
+  WordId(0),
+  PMFId(PMFId),  
+  MBId(0), 
+  EventCount(0),
+  MarocChan(),
   m_bit16(0),
   m_bit18(0),
   m_error_bit17(0),
   m_bit26_27(0),
   m_bit24_27(0)
 {
+  p_dataWords = new std::vector<uint32_t>();
 }
 
 ////////////////////////
@@ -38,44 +40,45 @@ ALFA_RawData::ALFA_RawData(uint16_t PMFId):
 ////////////////////////
 ALFA_RawData::~ALFA_RawData()
 {
+  if (p_dataWords) delete p_dataWords;
 }
 
 
 void ALFA_RawData::SetWordId_PMF(uint16_t Word_number)
 {
-  m_WordId=Word_number;
+  WordId=Word_number;
 }
 
 void ALFA_RawData::SetPMFId_PMF(uint16_t PMF_number)
 {
-  m_PMFId=PMF_number;
+  PMFId=PMF_number;
 }
 
 void ALFA_RawData::SetMBId_PMF(uint16_t MB_number)
 {
-  m_MBId=MB_number;
+  MBId=MB_number;
 }
 
 void ALFA_RawData::SetEventCount_PMF(uint32_t Event_number)
 {
-  m_EventCount=Event_number;
+  EventCount=Event_number;
 }
 
 
-void ALFA_RawData::SetMarocChan_PMF(const std::vector<uint16_t>& HitChan)
+void ALFA_RawData::SetMarocChan_PMF(std::vector<uint16_t> HitChan)
 {
-  m_MarocChan=HitChan;
+  MarocChan=HitChan;
 
 
 }
 
 void ALFA_RawData::PushBack_PMF(uint16_t fiber_hit){
-  m_MarocChan.push_back(fiber_hit);
+  MarocChan.push_back(fiber_hit);
 }
 
 void ALFA_RawData::addData(uint32_t dataWord) 
 {
-  m_dataWords.push_back(dataWord);
+  p_dataWords->push_back(dataWord);
 }
 
 void ALFA_RawData::Set_error_bit17(bool bit)
@@ -86,17 +89,17 @@ void ALFA_RawData::Set_error_bit17(bool bit)
 
 void ALFA_RawData::SetZero_PMF() 
 {
-  m_WordId=0;
-  m_PMFId=0; 
-  m_MBId=0;
-  m_EventCount=0; 
+  WordId=0;
+  PMFId=0; 
+  MBId=0;
+  EventCount=0; 
   m_bit26_27=0;
   m_bit24_27=0;
   m_bit16=false;
   m_bit18=false;
   m_error_bit17=false;
-  m_MarocChan.clear();
-  m_dataWords.clear();
+  MarocChan.clear();
+  p_dataWords->clear();
 }
 
 
--- ForwardDetectors/ALFA/ALFA_RawEv/src/ALFA_RawDataCollection.cxx
+++ ForwardDetectors/ALFA/ALFA_RawEv/src/ALFA_RawDataCollection.cxx
@@ -8,110 +8,110 @@
 // Default constructor
 ALFA_RawDataCollection::ALFA_RawDataCollection() :
   DataVector<ALFA_RawData>(),
-  m_MBId_POT(0),
-  m_MrodId_POT(0),
-  m_EventCount_POT(0),
-  m_POT_DATA(),
-  m_patternId(),
-  m_scalerId(0),
-  m_ADC1Id(0),
-  m_ADC2Id(0),
-  m_TrigSyncErrId(0)
+  MBId_POT(0),
+  MrodId_POT(0),
+  EventCount_POT(0),
+  POT_DATA(),
+  patternId(),
+  scalerId(0),
+  ADC1Id(0),
+  ADC2Id(0),
+  TrigSyncErrId(0)
 { }
 
 // Full constructor
 ALFA_RawDataCollection::ALFA_RawDataCollection(uint16_t MB_number) :
   DataVector<ALFA_RawData>(),
-  m_MBId_POT(MB_number),
-  m_MrodId_POT(0),
-  m_EventCount_POT(0),
-  m_POT_DATA(),
-  m_patternId(),
-  m_scalerId(0),
-  m_ADC1Id(0),
-  m_ADC2Id(0),
-  m_TrigSyncErrId(0)
+  MBId_POT(MB_number),
+  MrodId_POT(0),
+  EventCount_POT(0),
+  POT_DATA(),
+  patternId(),
+  scalerId(0),
+  ADC1Id(0),
+  ADC2Id(0),
+  TrigSyncErrId(0)
 { }
 
 // Full constructor
 ALFA_RawDataCollection::ALFA_RawDataCollection( uint16_t Mrod_number, uint16_t MB_number) :
   DataVector<ALFA_RawData>(),
-  m_MBId_POT(MB_number),
-  m_MrodId_POT(Mrod_number),
-  m_EventCount_POT(0),
-  m_POT_DATA(),
-  m_patternId(),
-  m_scalerId(0),
-  m_ADC1Id(0),
-  m_ADC2Id(0),
-  m_TrigSyncErrId(0)
+  MBId_POT(MB_number),
+  MrodId_POT(Mrod_number),
+  EventCount_POT(0),
+  POT_DATA(),
+  patternId(),
+  scalerId(0),
+  ADC1Id(0),
+  ADC2Id(0),
+  TrigSyncErrId(0)
 { }
 
 
 
 void ALFA_RawDataCollection::SetMBId_POT(uint16_t MB_number)
 {
-  m_MBId_POT=MB_number;
+  MBId_POT=MB_number;
 }  
   
   
 void ALFA_RawDataCollection::SetMrodId_POT(uint16_t Mrod_number)
 {
-  m_MrodId_POT=Mrod_number;
+  MrodId_POT=Mrod_number;
 }  
 
 
 void ALFA_RawDataCollection::SetEventCount_POT(uint32_t Event_number)
 {
-  m_EventCount_POT=Event_number;
+  EventCount_POT=Event_number;
 }
 
-void ALFA_RawDataCollection::Set_pattern_POT(const std::vector<bool>& pattern_number)
+void ALFA_RawDataCollection::Set_pattern_POT(std::vector<bool> pattern_number)
 {
-  m_patternId=pattern_number;
+  patternId=pattern_number;
 }
 
 void ALFA_RawDataCollection::Set_scaler_POT(uint16_t scaler_number)
 {
-  m_scalerId=scaler_number;
+  scalerId=scaler_number;
 }
 
 void ALFA_RawDataCollection::Set_ADC1_POT(uint16_t ADC1_number)
 {
-  m_ADC1Id=ADC1_number;
+  ADC1Id=ADC1_number;
 }
 
 void ALFA_RawDataCollection::Set_ADC2_POT(uint16_t ADC2_number)
 {
-  m_ADC2Id=ADC2_number;
+  ADC2Id=ADC2_number;
 }
 
 void ALFA_RawDataCollection::SetTrigSyncErr(bool bit)
 {
-  m_TrigSyncErrId=bit;
+  TrigSyncErrId=bit;
 }
 
-void ALFA_RawDataCollection::PushBack_POT(const ALFA_RawData& PMF_HIT)
+void ALFA_RawDataCollection::PushBack_POT(ALFA_RawData PMF_HIT)
 {
-  m_POT_DATA.push_back(PMF_HIT);
+  POT_DATA.push_back(PMF_HIT);
 }  
 
 
 void ALFA_RawDataCollection::SetZero_POT()
 {
 
-  m_MBId_POT = 0;    // MB number
+  MBId_POT = 0;    // MB number
    
-  m_MrodId_POT = 0;    // Mrodnumber
+  MrodId_POT = 0;    // Mrodnumber
 
-  m_EventCount_POT=0; 
+  EventCount_POT=0; 
 
-  m_patternId.clear(); 
-  m_scalerId=0; 
-  m_ADC1Id=0; 
-  m_ADC2Id=0;
-  m_TrigSyncErrId=false;
+  patternId.clear(); 
+  scalerId=0; 
+  ADC1Id=0; 
+  ADC2Id=0;
+  TrigSyncErrId=false;
 
-  m_POT_DATA.clear();
+  POT_DATA.clear();
 }
 
