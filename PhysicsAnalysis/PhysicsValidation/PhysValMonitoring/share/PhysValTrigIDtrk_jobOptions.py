d669f7ba5e2323a108cbd0c4bfa5f8555c44818c
Patrick Karl Czodrowski <patrick.czodrowski@cern.ch>
2015-04-19 14:59:22 +0200

    Fix TrigID JO - thanks to Ben https://its.cern.ch/jira/browse/ATR-9213 (PhysValMonitoring-00-01-36)
        * Fix TrigID JO - thanks to Ben https://its.cern.ch/jira/browse/ATR-9213
     	* PhysValMonitoring-00-01-36
    
    08.04.2015 <Ioannis.Nomidis.cern.ch>
    	* importing tool needed for muons
    	* tagging PhysValMonitoring-00-01-35
    
    31.03.2015 <Ioannis.Nomidis.cern.ch>
    	* Change for Muon JOs, added plots for STACO author
    	* tagging PhysValMonitoring-00-01-34
    
    25.03.2015 <Ioannis.Nomidis.cern.ch>
    	* Change for Muon JOs, removing plots for disabled muonauthor
    	* tagging PhysValMonitoring-00-01-33

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValTrigIDtrk_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValTrigIDtrk_jobOptions.py
@@ -1,11 +1,11 @@
 from TrigIDtrkMonitoring.TrigIDPhysValMonitoringConfig import TrigIDPhysValMonitoringTool
-tool1 = getattr(ToolSvc, TrigIDPhysValMonitoringTool()[0].split('/')[-1])
+for toolname in TrigIDPhysValMonitoringTool():
+  tool1 = getattr(ToolSvc, toolname.split('/')[-1])
+  tool1.EnableLumi = False
+  tool1.OutputLevel = WARNING
+  #tool1.DetailLevel = 10
 
-tool1.EnableLumi = False
-tool1.OutputLevel = WARNING
-#tool1.DetailLevel = 10
-
-tool1.EnableLumi = False
-ToolSvc += tool1
-monMan.AthenaMonTools += [tool1]
+  tool1.EnableLumi = False
+  ToolSvc += tool1
+  monMan.AthenaMonTools += [tool1]
 
