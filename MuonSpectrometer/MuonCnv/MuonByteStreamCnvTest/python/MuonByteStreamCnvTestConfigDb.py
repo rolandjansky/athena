a7effac4cf39fb498c775fea7405e79e62493801
Jochen Meyer <Jochen.Meyer@cern.ch>
2015-04-23 20:38:53 +0200

    some cleaning of dependencies (MuonByteStreamCnvTest-04-01-07)
    Former-commit-id: 71e8f4e9414a16146c6244ea712079932ad7c167

--- MuonSpectrometer/MuonCnv/MuonByteStreamCnvTest/python/MuonByteStreamCnvTestConfigDb.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from AthenaCommon.CfgGetter import addTool
-
-addTool("MuonByteStreamCnvTest.MuonByteStreamCnvTestConfig.getMdtRdoToMdtDigit", "MdtRdoToMdtDigitOverlay")
-addTool("MuonByteStreamCnvTest.MuonByteStreamCnvTestConfig.getRpcRdoToRpcDigit", "RpcRdoToRpcDigitOverlay")
-addTool("MuonByteStreamCnvTest.MuonByteStreamCnvTestConfig.getTgcRdoToTgcDigit", "TgcRdoToTgcDigitOverlay")
-
