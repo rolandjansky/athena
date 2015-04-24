c9e934b651bcf1dd794ac2e15977b21c3a932393
John Baines <john.baines@stfc.ac.uk>
2015-04-24 17:01:31 +0200

    implement methods to get TrackParticles and vertices (FTK_DataProviderInterfaces-00-00-03)

--- Trigger/TrigFTK/FTK_DataProviderInterfaces/FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h
+++ Trigger/TrigFTK/FTK_DataProviderInterfaces/FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h
@@ -6,14 +6,22 @@
 #define IFTK_DATAPROVIDERSVC_H
 
 // Include Files
+#include <string>
 #include "GaudiKernel/IInterface.h"
 #include "AthenaKernel/IOVSvcDefs.h"
 #include "TrkTrack/Track.h"
 #include "TrkTrack/TrackCollection.h"
+//#include "VxVertex/VxContainer.h"
+//#include "VxVertex/VxCandidate.h"
 #include "TrigFTK_RawData/FTK_RawTrack.h"
 #include "IRegionSelector/IRoiDescriptor.h"
-#include <string>
-
+class VxContainer;
+#include "xAODTracking/VertexFwd.h"
+#include "xAODTracking/TrackParticleFwd.h"
+#include "xAODTracking/VertexContainerFwd.h"
+#include "xAODTracking/VertexAuxContainer.h"
+#include "xAODTracking/TrackParticleContainerFwd.h"
+#include "xAODTracking/TrackParticleAuxContainer.h"
 namespace InDet {
   class PixelCluster;
   class SCT_Cluster;
@@ -22,8 +30,8 @@ namespace InDet {
 namespace Trk {
   class Track;
 }
-
 class IRoiDescriptor;
+
 
 static const InterfaceID IID_IFTK_DataProviderSvc("IFTK_DataProviderSvc",1,0);
 
@@ -34,12 +42,15 @@ public:
   /// Retrieve interface ID
   static const InterfaceID& interfaceID() { return IID_IFTK_DataProviderSvc; }
   
-  virtual Trk::Track* ConvertTrack(const FTK_RawTrack &track, bool) = 0;
-  virtual StatusCode GetTracksFullScan(TrackCollection& track_collection, bool) = 0;
-  virtual StatusCode GetTracksInRoi(TrackCollection &track_collection, const IRoiDescriptor&, bool) = 0;
-  virtual StatusCode beginEvent() = 0;
-  virtual StatusCode endEvent() = 0;
+  virtual TrackCollection* getTracks(const bool withRefit) = 0;
+  virtual TrackCollection* getTracksInRoi(const IRoiDescriptor&, const bool withRefit) = 0;
 
+  virtual xAOD::TrackParticleContainer* getTrackParticles(const bool withRefit) = 0;
+  virtual xAOD::TrackParticleContainer* getTrackParticlesInRoi(const IRoiDescriptor&, const bool withRefit) = 0;
+
+  virtual VxContainer* getVxContainer(const bool withRefit)=0;
+  virtual xAOD::VertexContainer* getVertexContainer(const bool withRefit)=0;
+  //  virtual std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> getVertexContainer(const bool withRefit)=0;
 };
 
 #endif
