5bc1399bc69d6b4b7ed3d7c68c4ba52c88c471fb
Simone Pagan Griso <simone.pagan.griso@cern.ch>
2015-04-30 10:47:17 +0200

    'Fix python to determine if is simulation' (InDetPrepRawDataToxAOD-00-01-16)

---
+++ InnerDetector/InDetEventCnv/InDetPrepRawDataToxAOD/src/components/InDetPrepRawDataToxAOD_entries.cxx
@@ -0,0 +1,18 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "../TRT_PrepDataToxAOD.h"
+#include "../SCT_PrepDataToxAOD.h"
+#include "../PixelPrepDataToxAOD.h"
+
+
+//using namespace InDet;
+
+DECLARE_ALGORITHM_FACTORY( TRT_PrepDataToxAOD )
+DECLARE_ALGORITHM_FACTORY( SCT_PrepDataToxAOD )
+DECLARE_ALGORITHM_FACTORY( PixelPrepDataToxAOD )
+
+DECLARE_FACTORY_ENTRIES( InDetPrepRawDataToxAOD ) 
+{
+  DECLARE_ALGORITHM( TRT_PrepDataToxAOD )
+  DECLARE_ALGORITHM( SCT_PrepDataToxAOD )
+  DECLARE_ALGORITHM( PixelPrepDataToxAOD )
+}
