f278128dbfebd53a2d79d4e04b9e0738d0a81927
Mark Sutton <mark.sutton@cern.ch>
2015-05-04 19:54:06 +0200

    fix bjet chains (TrigIDtrkMonitoring-00-02-29)
    	* TrigIDtrkMonitoring-00-02-29
    	* fix bjet chains
    
    2015-05-03 sutt < sutt @ cern.ch >
    	* TrigIDtrkMonitoring-00-02-28
    	* modify tau instances
    
    2015-05-03 sutt < sutt @ cern.ch >
    	* TrigIDtrkMonitoring-00-02-27
    	* add roi specialisation to tau triggers
    
    2015-05-01 sutt < sutt @ cern.ch >
    	* TrigIDtrkMonitoring-00-02-26
    	* specify roi for tau chains
    
    2015-04-29 sutt < sutt @ cern.ch >
    	* TrigIDtrkMonitoring-00-02-25
    	* add efficiencies to the han config generator
    
    2015-04-29 sutt < sutt @ cern.ch >
    ...
    (Long ChangeLog diff - truncated)

--- Trigger/TrigMonitoring/TrigIDtrkMonitoring/utils/hanconfig.cxx
+++ Trigger/TrigMonitoring/TrigIDtrkMonitoring/utils/hanconfig.cxx
@@ -22,6 +22,7 @@
 #include "TKey.h"
 #include "TH1D.h"
 #include "TH2D.h"
+#include "TProfile.h"
 #include "TFile.h"
 #include "TClass.h"
 
@@ -40,15 +41,29 @@ bool dbg = false;
 
 bool dirflag = false;
 
+std::string base     = "HLT";
+std::string basepath;
+
+std::string spacer = "   ";
+
+bool header = true;
+
 /// glabal timer - how long have I taken so far?
 struct timeval global_timer;
 
 
 /// tag string for intendation
-std::string depth = ""; 
+std::string depth= ""; 
+
+
 
 /// counter of the current depth
 int ir=0;
+
+std::ostream& out = std::cout;
+
+
+std::vector<std::string> exclude; 
 
 
 std::string tail( const std::string& s ) { 
@@ -56,6 +71,38 @@ std::string tail( const std::string& s ) {
   std::string _s = s;
   size_t pos = _s.find("/");
   while ( pos!=std::string::npos ) { 
+    _s.erase( 0, pos+1 );
+    pos = _s.find("/");
+  }
+  return _s;
+}
+
+
+std::string head( const std::string& s ) { 
+  
+  std::string h = "";
+  std::string _s = s;
+  size_t pos = _s.find("/");
+  while ( pos!=std::string::npos ) { 
+    if ( h!="" ) h += "/";
+    h += _s.substr( 0, pos ); 
+    _s.erase( 0, pos+1 );
+    pos = _s.find("/");
+  }
+  return h;
+}
+
+
+
+std::string strip( const std::string& s, const std::string& dir ) { 
+  
+  if ( dir=="" ) return s;
+
+  std::string _s = s;
+  size_t pos = _s.find("/");
+  while ( pos!=std::string::npos ) { 
+    std::string h = _s.substr( 0, pos );
+    if ( h==dir ) return _s;
     _s.erase( 0, pos+1 );
     pos = _s.find("/");
   }
@@ -71,6 +118,24 @@ std::string chop( const std::string& s ) {
 }
 
 
+std::string first( const std::string& s ) {   
+  size_t pos = s.find("/");
+  if ( pos!=std::string::npos ) return s.substr( 0, pos );
+  return s;
+}
+
+
+bool hfilled( TH1* h ) { 
+  if ( h!=0 ) { 
+    for ( int i=1 ; i<=h->GetNbinsX() ; i++ ) { 
+      if ( h->GetBinContent(i)!=0 ) return true; 
+    }
+  }
+  return false;
+}
+
+
+
 std::string cwd = "";
 
 template<class T>
@@ -78,9 +143,9 @@ bool add( const std::string& hname, TKey* tobj, const std::string depth="" ) {
   
   T* h0 = (T*)tobj->ReadObj();
 
-  //  std::cout << "h0 " << h0;
-  //  if ( h0 ) std::cout << " : " << h0->GetName();
-  //  std::cout << std::endl;
+  //  out << "h0 " << h0;
+  //  if ( h0 ) out << " : " << h0->GetName();
+  //  out << std::endl;
   
   bool changed = false;
   
@@ -91,18 +156,28 @@ bool add( const std::string& hname, TKey* tobj, const std::string depth="" ) {
 
   if ( h0->GetSumw2N()==0 ) h0->Sumw2();
 
-  if ( dbg ) std::cout << depth << "found object " << hname << std::endl;
+  if ( dbg ) std::cerr << depth << "found object " << hname << std::endl;
 
   if ( dirflag ) { 
-    std::cout << depth << "hist " << tail(hname) << " {\n";
+    out << depth << spacer << "hist " << tail(hname) << " {\n";
     {
-      std::cout << depth << "\talgorithm = HLTidtrkHistogram_Not_Empty&GatherData\n";
-      std::cout << depth << "\tdescription = https://twiki.cern.ch/twiki/bin/view/Atlas/HltTrackingDataQualityMonitoring#Tier0\n";
-      std::cout << depth << "\toutput = " << chop(cwd) << "\n";
-      std::cout << depth << "\tweight = 0.0\n";
-      if ( hname.find("Eff_")==std::string::npos ) std::cout << depth << "\tdisplay = StatBox\n";    
+
+      out << depth << spacer <<  spacer << "algorithm = HLT_Histogram_Not_Empty&GatherData\n";
+      //     out << depth << spacer <<  spacer << "algorithm = HLTidtrkHistogram_Not_Empty&GatherData\n";
+      out << depth << spacer <<  spacer << "description = https://twiki.cern.ch/twiki/bin/view/Atlas/HltTrackingDataQualityMonitoring#Tier0\n";
+      //   out << depth << "\toutput = " << chop(cwd) << "\n";
+ 
+      if ( dbg ) out << depth << spacer <<  "\033[91;1m" << base << " - object :" << hname << ":\t:" << cwd << ":\t:" << strip(cwd, base) << ":\t:" <<  head(strip(cwd, base)) << ":\t:" << head(cwd) << ":\t:" << strip( head(cwd), base ) << ": \033[m\n";
+    
+      if ( basepath!="" ) out << depth << spacer <<  spacer << "output = " << head(basepath+"/"+hname) << "\n";
+      else                out << depth << spacer <<  spacer << "output = " << head(hname) << "\n";
+
+      std::cerr << depth << spacer <<  spacer << "output = " << head(hname) << "\t\t " << basepath << " " << h0->GetName() << "\tFilled: " << hfilled( h0 ) << "\n";
+
+      out << depth << spacer << spacer << "weight = 0.0\n";
+      if ( hname.find("Eff_")==std::string::npos ) out << depth << spacer << spacer << "display = StatBox\n";    
     }
-    std::cout << depth << "}\n";
+    out << depth << spacer << "}\n";
   }
 
   delete h0;
@@ -113,11 +188,11 @@ bool add( const std::string& hname, TKey* tobj, const std::string depth="" ) {
 
   for ( unsigned ifile=1 ; ifile<fptr.size() ; ifile++ ) { 
     
-    //    std::cout << "TFile* fptr " << fptr[ifile] << " " << files[ifile] << std::endl;
+    //    out << "TFile* fptr " << fptr[ifile] << " " << files[ifile] << std::endl;
 
     if ( fptr[ifile] ) {  
 
-      //      std::cout << "h (get) >" << hname << "<" << std::endl; 
+      //      out << "h (get) >" << hname << "<" << std::endl; 
 
       T* h  = (T*)fptr[ifile]->Get(hname.c_str());
 
@@ -132,7 +207,7 @@ bool add( const std::string& hname, TKey* tobj, const std::string depth="" ) {
 
       if ( h->GetSumw2N()==0 ) h->Sumw2();
 
-      //      std::cout << "h       " << h->GetName() << std::endl; 
+      //      out << "h       " << h->GetName() << std::endl; 
 
       changed = true;
       // h0->Add( h );
@@ -173,7 +248,7 @@ void search(TDirectory* td=0, const std::string& s="") {
 
   std::string savedepth = depth;
   
-  depth += "\t";
+  depth += spacer;
 
   TDirectory* here = gDirectory;
 
@@ -181,29 +256,43 @@ void search(TDirectory* td=0, const std::string& s="") {
 
   std::string ns = s;
 
+
   if ( ns!="" ) ns += "/"; 
 
+
+  bool print_trailer = false;
+
   if ( td )  { 
+
+    for ( unsigned i=0 ; i<exclude.size() ; i++ ) { 
+      if ( tail(td->GetName())==exclude[i] ) { 
+	depth = savedepth;
+	return;
+      }
+    }
+
     gDirectory->cd(td->GetName());
 
     ns +=  td->GetName();
 
-    if ( dbg ) std::cout << depth <<  "\033[91;1m" << "- Directory " << ns << "\033[m\n";
+    if ( dbg ) out << depth <<  "\033[91;1m" << "- Directory " << ns << "  (" << head(ns) << ")\033[m\n";
     
-    //    if ( !dirflag )  std::cout << depth <<  "output " << ns << "{\n";
-    if ( !dirflag )  std::cout << depth <<  "output " << tail(ns) << "{\n";
-    else             std::cout << depth <<  "dir " << tail(ns) << "{\n"; 
+    //    if ( !dirflag )  out << depth <<  "output " << ns << " {\n";
+    if ( !dirflag )  out << depth <<  "output " << tail(ns) << " {\n";
+    else             out << depth <<  "dir " << tail(ns) << " {\n"; 
+
+    print_trailer = true;
   }
 
   cwd = ns;
 
-  //  std::cout << ir << " " << ns << "TDirectory " << gDirectory->GetPath() << " " << gDirectory->GetName() << std::endl;
+  //  out << ir << " " << ns << "TDirectory " << gDirectory->GetPath() << " " << gDirectory->GetName() << std::endl;
 
   TList* tl  = gDirectory->GetListOfKeys();
 
   struct timeval tv = simpletimer_start();
 
-  //  std::cout << "\ttl " << tl << std::endl;
+  //  out << "\ttl " << tl << std::endl;
 
   for ( int i=tl->GetSize() ; i-- ; ) { 
 
@@ -211,12 +300,12 @@ void search(TDirectory* td=0, const std::string& s="") {
 
     if ( tobj==0 ) continue;
 
-    //    std::cout << "tobj " << tobj;
-    //    if ( tobj ) std::cout << " : \t" << tobj->GetName();
-    //    std::cout << std::endl; 
+    //    out << "tobj " << tobj;
+    //    if ( tobj ) out << " : \t" << tobj->GetName();
+    //    out << std::endl; 
     
     if ( std::string(tobj->GetClassName()).find("TDirectory")!=std::string::npos ) { 
-      //      std::cout << ns << "Directory " << tobj->GetName() << std::endl;
+      //      out << ns << "Directory " << tobj->GetName() << std::endl;
  
       TDirectory* tnd = (TDirectory*)tobj->ReadObj();
 
@@ -230,8 +319,9 @@ void search(TDirectory* td=0, const std::string& s="") {
       
       //      if ( std::string(tobj->GetClassName()).find("TH1")!=std::string::npos )  status = add<TH1>( objname.c_str(), tobj );
       //      if ( std::string(tobj->GetClassName()).find("TH2")!=std::string::npos )  status = add<TH2>( objname.c_str(), tobj );
-      if ( std::string(tobj->GetClassName()).find("TH1")!=std::string::npos )  add<TH1>( objname.c_str(), tobj, depth+"\t" );
-      if ( std::string(tobj->GetClassName()).find("TH2")!=std::string::npos )  add<TH2>( objname.c_str(), tobj, depth+"\t" );
+      if ( std::string(tobj->GetClassName()).find("TH1")!=std::string::npos )       add<TH1>( objname.c_str(), tobj, depth+"\t" );
+      if ( std::string(tobj->GetClassName()).find("TH2")!=std::string::npos )       add<TH2>( objname.c_str(), tobj, depth+"\t" );
+      if ( std::string(tobj->GetClassName()).find("TProfile")!=std::string::npos )  add<TProfile>( objname.c_str(), tobj, depth+"\t" );
       
       //      if ( !status ) std::cerr << "bad status" << std::endl;
     }
@@ -244,16 +334,18 @@ void search(TDirectory* td=0, const std::string& s="") {
 
   
   if ( dbg ) { 
-    std::cout << depth <<  "\033[91;1m" << "  Directory " << ns << "\033[m";
-    std::cout << "\033[91;1m\tprocessed directory in " << _t*0.001 << " s  from " << global_time*0.001 << " s\033[m\n";
+    out << depth <<  "\033[91;1m" << "  Directory " << ns << "\033[m";
+    out << "\033[91;1m\tprocessed directory in " << _t*0.001 << " s  from " << global_time*0.001 << " s\033[m\n";
   }
 
-  if ( !dirflag )  std::cout << depth <<  "}\n";
-  else             std::cout << depth <<  "}\n";
+
+  if ( print_trailer ) out << depth <<  "}\n";
 
   depth = savedepth;
 
   ir--;
+
+  //  out << depth <<  "## " << tail(ns) << "\n";
 
   here->cd();
 
@@ -279,7 +371,7 @@ bool file_exists(const std::string& s) {
 
 void cost( std::vector<std::string>& files, const std::string& directory="" ) { 
 
-  std::cout << "processing" << std::endl;
+  std::cerr << "processing" << std::endl;
 
   /// don't over write esisting files
   //  if ( file_exists(outputfile) ) { 
@@ -296,16 +388,39 @@ void cost( std::vector<std::string>& files, const std::string& directory="" ) {
   /// don't bother to open the first file - we've copied 
   /// it to the output and will update the contents
 
-  std::cout << "opening root files" << std::endl;
+  std::cerr << "opening root files" << std::endl;
 
   fptr.resize(files.size());
 
+  if ( !dirflag ) { 
+    out << "######################################################################\n";
+    out << "# $Id: cosmics_run.config 2015-03-13 14:31:39Z sutt $\n";
+    out << "######################################################################\n";
+    
+    out << "\n";
+    out << "#######################\n";
+    out << "# HLTidtrk\n";
+    out << "#######################\n";
 
-  std::cout << "opening " << files[0] << std::endl;
+    out << "\n\n";
+    out << "#######################\n";
+    out << "# Output\n";
+    out << "#######################\n";
+  }
+  else { 
+    out << "\n\n";
+    out << "#######################\n";
+    out << "# Histogram Assessments\n";
+    out << "#######################\n";
+  }
+
+  out << "\n";
+
+  std::cerr << "opening " << files[0] << std::endl;
 
   for ( unsigned i=0 ; i<files.size() ; i++ ) { 
 
-  /// open the output file
+    /// open the output file
     fptr[i] = new TFile( files[i].c_str() );
     
     if ( fptr[i]==0 ) { 
@@ -321,20 +436,79 @@ void cost( std::vector<std::string>& files, const std::string& directory="" ) {
     fptr[i]->cd();
   
     if ( directory!="" ) fptr[i]->cd(directory.c_str());
-
-    gDirectory->pwd();
+    
+    //    gDirectory->pwd();
 
     TDirectory* here = gDirectory;
-
+    
     global_timer = simpletimer_start();
+
+
+    int tcount = 0;
+
+    std::string savedepth = depth; 
+
+
+    if ( header ) { 
+    /// write out the rest of the config from the base directory
+
+    if ( !dirflag ) { 
+      out << "output top_level {\n";
+      tcount++;
+    }      
+
+    std::string bp = basepath;    
+    std::string _head = head( bp );
+    
+    std::vector<std::string> hdirs;
+    
+    std::string tdir = tail( bp );
+    
+    
+    while ( tdir!="" ) { 
+      
+      bp = head( bp );
+      
+      hdirs.push_back( tdir ); 
+      
+      tdir = tail( bp );
+      
+    }
+
+    for ( unsigned it=tcount ; it-- ; ) depth += spacer;
+
+      
+    for ( unsigned it=hdirs.size() ; it-- ; ) {
+      for ( int itt=0 ; itt<tcount ; itt++ ) out << spacer;
+      if ( !dirflag ) out << "output ";
+      else            out << "dir ";
+      out << hdirs[it] << " {\n";
+      tcount++;
+    }
+
+    }
+
 
     /// recursively look through the directory
     /// structure   
     search();
     
+    if ( header ) { 
+    /// write out the closing braces
+
+      for ( int it=tcount ; it-- ;  ) { 
+	for ( int itt=0 ; itt<it ; itt++ ) out << spacer;
+	out << "}\n";
+      }
+       
+    }
+
+    depth = savedepth;
+
+
     here->cd();
     
-    std::cout << "closing the files" << std::endl; 
+    std::cerr << "closing the files" << std::endl; 
     
     
     /// don't need to write the histogram again - writing them as we go along
@@ -352,7 +526,9 @@ void cost( std::vector<std::string>& files, const std::string& directory="" ) {
 int usage(std::ostream& s, int , char** argv) { 
   s << "Usage: " << argv[0] << " [OPTIONS] input1.root ... inputN.root\n\n";
   s << "     filename   \tname of output (filename required)\n";
-  s << "    -d,   --dir DIR  \tonly directories below DIR\n";
+  s << "    -d,   --dir  DIR  \tonly directories below DIR\n";
+  s << "    -x,          DIR  \texclude directory DIR\n";
+  s << "    -b,   --base DIR  \tuse directory DIR as the base for the han config\n";
   s << "    -dbg, --dbg      \tprint debugging information\n";
   s << "    -h,   --help     \tdisplay this help\n";
   s << std::endl;
@@ -369,9 +545,9 @@ int main(int argc, char** argv) {
 
 
   for ( int i=1 ; i<argc ; i++ ) { 
-    if ( std::string(argv[i])=="-h" || std::string(argv[i])=="--help" )  return usage( std::cout, argc, argv ); 
+    if ( std::string(argv[i])=="-h" || std::string(argv[i])=="--help" )  return usage( out, argc, argv ); 
     //    if ( std::string(argv[i])=="-v" || std::string(argv[i])=="--version" ) {
-    //      std::cout << argv[0] << " APPLgrid version " << PACKAGE_VERSION << std::endl; 
+    //      out << argv[0] << " APPLgrid version " << PACKAGE_VERSION << std::endl; 
     //  return 0;
   }
   
@@ -384,7 +560,17 @@ int main(int argc, char** argv) {
     else if ( std::string(argv[i])=="-dbg" || std::string(argv[i])=="--dbg" ) dbg = true;
     else if ( std::string(argv[i])=="-d"   || std::string(argv[i])=="--dir" ) {
       ++i;
-      if ( i<argc ) directory = argv[i];
+      if ( i<argc+1 ) directory = argv[i];
+      else  return usage( std::cerr, argc, argv );
+    } 
+    else if ( std::string(argv[i])=="-x" ) {
+      ++i;
+      if ( i<argc+1 ) exclude.push_back( argv[i] );
+      else  return usage( std::cerr, argc, argv );
+    } 
+    else if ( std::string(argv[i])=="-b" || std::string(argv[i])=="--base" ) {
+      ++i;
+      if ( i<argc+1 ) base = argv[i] ;
       else  return usage( std::cerr, argc, argv );
     } 
     //    else if ( std::string(argv[i])=="-o" ) { 
@@ -397,6 +583,7 @@ int main(int argc, char** argv) {
     }
   }
 
+  if ( base == "" ) base = directory;
 
   /// if output file is not defined
   //  if ( output_file == "" ) return usage( std::cerr, argc, argv );
@@ -408,11 +595,19 @@ int main(int argc, char** argv) {
 
 
   if ( files.size()<1 ) return usage( std::cerr, argc, argv );
-
   
+  basepath = strip( directory, base );
+
+  //  if ( basepath!="" ) basepath += "/";
+  
+
+  std::cerr << directory << "\t" << base << "\tbasepath " << basepath << std::endl; 
+
+  //  return 0;
+
   //  time the actual running of the cost() routine
   
-  if ( verbose ) std::cout << "timing" << std::endl;
+  if ( verbose ) std::cerr << "timing" << std::endl;
 
   struct timeval tv = simpletimer_start();
 
@@ -426,7 +621,7 @@ int main(int argc, char** argv) {
 
   double t = simpletimer_stop(tv);
 
-  std::cout << "\ntotal time " << t*0.001 << " s  (" << (t*0.001/60) << " min)" << std::endl;
+  std::cerr << "\ntotal time " << t*0.001 << " s  (" << (t*0.001/60) << " min)" << std::endl;
 
   return 0;
 }
