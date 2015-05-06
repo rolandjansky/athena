67a8a604dbc34d6ec2c119702a256d788ddb8cc2
Sasha Vanyashin <sacha.vaniachine@cern.ch>
2015-05-06 12:55:25 +0200

    initial import (IOVDbPTCnv-00-00-01)

---
+++ Database/IOVDbPTCnv/IOVDbPTCnv/IOVPayloadContainerPTCnv_p1.h
@@ -0,0 +1,52 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef IOVDBPTCNV_IOVPAYLOADCONTAINERPTCNV_P1_H
+#define IOVDBPTCNV_IOVPAYLOADCONTAINERPTCNV_P1_H
+/**
+ * @file IOVPayloadContainerPTCnv_p1.h
+ *
+ * @brief Header file for in-file metadata P/T converters (file peeking without Athena)
+ *
+ * @author  <vaniachine@anl.gov>
+ */
+
+//<<<<<< INCLUDES                                                       >>>>>>
+#include "CoralBase/AttributeList.h"
+#include "IOVDbTPCnv/IOVPayloadContainer_p1.h"
+#include "IOVDbDataModel/IOVPayloadContainer.h"
+
+class AttrListIndexes;
+
+class IOVPayloadContainerPTCnv_p1  {
+public:
+    IOVPayloadContainerPTCnv_p1();
+    virtual void   persToTrans(const IOVPayloadContainer_p1* persObj, IOVPayloadContainer* transObj);
+
+private:
+    void           fillPersAttrSpec(const IOVPayloadContainer* transObj, IOVPayloadContainer_p1* persObj);
+
+    void           fillAttributeTypeMap();
+
+    void           fillAttributeData(unsigned int attrName,
+				     unsigned int attrType,
+				     const coral::Attribute& attr,
+				     IOVPayloadContainer_p1* persObj);
+
+    void           fillAttributeData(const IOVPayloadContainer_p1* persObj,
+				     const AttrListIndexes& index,
+				     const std::string& name,
+				     coral::AttributeList& attrList);
+
+    
+    // AttributeList map from attribute name to type index
+    typedef std::map<std::string, unsigned int>::value_type    attrNameMapValue_t;
+    std::map<std::string, unsigned int>    m_attrNameMap;
+
+    // Attribute types
+    std::map<std::string, unsigned int>    m_attributeTypeMap;
+    std::vector<std::string>               m_attributeTypes;
+};
+
+#endif // IOVDBPTCNV_IOVPAYLOADCONTAINERPTCNV_P1_H
