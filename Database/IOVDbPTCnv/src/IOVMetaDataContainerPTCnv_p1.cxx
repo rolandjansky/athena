b4618c001cd046f85f8db79b36082d85388ee7cb
Sasha Vanyashin <sacha.vaniachine@cern.ch>
2015-05-06 12:55:25 +0200

    initial import (IOVDbPTCnv-00-00-01)

---
+++ Database/IOVDbPTCnv/src/IOVMetaDataContainerPTCnv_p1.cxx
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+ * @file IOVMetaDataContainerPTCnv_p1.cxx
+ *
+ * @author Alexandre Vaniachine <vaniachine@anl.gov>
+ *
+ */
+
+#define private public
+#include "IOVDbDataModel/IOVMetaDataContainer.h"
+#undef private
+
+
+#include "IOVDbDataModel/IOVPayloadContainer.h"
+#include "IOVDbPTCnv/IOVMetaDataContainerPTCnv_p1.h"
+#include "IOVDbTPCnv/IOVMetaDataContainer_p1.h"
+#include "CoralBase/Date.h"
+#include "CoralBase/TimeStamp.h"
+
+void
+IOVMetaDataContainerPTCnv_p1::persToTrans(const IOVMetaDataContainer_p1* persObj, 
+					  IOVMetaDataContainer* transObj)
+{
+
+    // copy foldername and description
+    transObj->m_folderName        = persObj->m_folderName;
+    transObj->m_folderDescription = persObj->m_folderDescription;
+
+    // convert payload
+    m_payloadCnv.persToTrans(&(persObj->m_payload), transObj->m_payload);
+}
