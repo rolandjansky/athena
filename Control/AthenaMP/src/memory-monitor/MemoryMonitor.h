db9562920a79d1209d64af856f0b38f8019dd2a0
Scott Snyder <scott.snyder@cern.ch>
2015-05-12 23:29:55 +0200

    Fix compilation and coverity warnings. (AthenaMP-01-02-29)

---
+++ Control/AthenaMP/src/memory-monitor/MemoryMonitor.h
@@ -0,0 +1,25 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <vector>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fstream>
+#include <condition_variable> 
+#include <thread> 
+#include <sstream>
+
+int ReadSmaps(pid_t mother_pid, unsigned long values[4], bool verbose=false);
+int MemoryMonitor(pid_t mpid, char* filename=NULL, char* jsonSummary=NULL, unsigned int interval=600);
+
+
