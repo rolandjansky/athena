19c39b504fee872feb8894078446bcbf6dbc8ef3
Thijs Cornelissen <Thijs.Cornelissen@cern.ch>
2015-05-13 13:33:52 +0200

    Update digitization test (Tier0ChainTests-00-02-87)
    	* Update digitization test
    	* Tagged as Tier0ChainTests-00-02-87
    
    2015-05-09 Christian Ohm <christian.ohm@cern.ch>
    	* Added q223 tests with 900 GeV data15_comm reco (MP + non-MP)
    
    2015-05-06 Thijs Cornelissen <thijs.cornelissen@cern.ch>
    	* Fix for 'overwrite' test
    	* Tagged as Tier0ChainTests-00-02-85
    
    2015-05-02 Christian Ohm <christian.ohm@cern.ch>
    	* Major update of tct_getstatus.py script which was
    	  very outdated. Not perfect yet, but job status reported
    	  is now correct
    
    2015-05-01 Christian Ohm <christian.ohm@cern.ch>
    	* Update of notification email
    	* Tagged as Tier0ChainTests-00-02-83
    
    2015-04-30 Thijs Cornelissen <thijs.cornelissen@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

--- Tools/Tier0ChainTests/scripts/tct_finishedMail.py
+++ Tools/Tier0ChainTests/scripts/tct_finishedMail.py
@@ -2,10 +2,11 @@
 
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 #
-# usage: tct_finishedMail.py
+# tct_finishedMail.py: utilities for sending notification emails
+# about the results from the tests in the Tier0ChainTests
+#
 
-import cgi, commands, pickle, sys, string, re, os, smtplib
-from datetime import datetime
+import sys, string, os, smtplib
 
 def sendSms(msg,toAdd='0041762725254@sms.switch.ch'):
   yourCernMailAdd = 'rtt@mail.cern.ch'
@@ -17,11 +18,9 @@ def sendSms(msg,toAdd='0041762725254@sms.switch.ch'):
   s.quit()
   return
 
-def sendMail(msg,toList):
-  yourCernMailAdd = 'rtt@mail.cern.ch'
-  cernSMTPsrv='cernmx.cern.ch'
-  s=smtplib.SMTP(cernSMTPsrv)
-  s.sendmail(yourCernMailAdd,toList,msg)
+def sendMail(recipient, message):
+  s = smtplib.SMTP('cernmx.cern.ch')
+  s.sendmail('rtt@mail.cern.ch', recipient, message)
   s.quit()
   return
 
@@ -41,69 +40,110 @@ if __name__ == "__main__":
   cmtconfig = os.environ['CMTCONFIG']
   project = releaseArr[7]
 
-  eList = sys.argv[1]
-  body = "https://atlas-rtt.cern.ch/index.php?q=%28release="+releaseArr[9]+";packagename=Tier0ChainTests;branch="+releaseArr[7]+";cmtconfig="+cmtconfig+";project=AtlasProduction;verbosity=vvv;%29"
-  body += "\nPlease look at the link at least 30 min after mail reception\n"
+  emailAddresses = sys.argv[1]
+
+  resultsurl = "https://atlas-rtt.cern.ch/index.php?q=%28release="+releaseArr[9]+";packagename=Tier0ChainTests;branch="+releaseArr[7]+";cmtconfig="+cmtconfig+";project=AtlasProduction;verbosity=vvv;%29"
+
+  comparisonlogurl = "https://atlas-rtt.cern.ch/prod/tct/%s/%s/build/x86_64-slc6-gcc48-opt/offline/Tier0ChainTests/ESDTAGCOMM_comparison/ESDTAGCOMM_comparison_log" % (releaseArr[9], releaseArr[7])
+
+  msgbody = """<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<body>
+<h1></h1>
+<p style=\"font-family: monospace;\">
+The results from the Tier0ChainTests are available <a
+href=\"%s\">here</a>
+<br><br>
+(Note that results may be published on the web with a slight delay)
+</p>
+""" % resultsurl
 
   if len(sys.argv) >= 3:
     filesNames = sys.argv[2]
     filesList = filesNames.split(',')
-    #body += "\nSummary of data quality results:\n"
-    #body += "================================\n"
     for fileName in filesList:
       if not os.path.exists(fileName): continue
       if 'ESDTAGcomp' in fileName:
           fSummary = open(fileName,'r')
-          body += "\nSummary of ESD/TAG comparison:\n"
-          body += "==============================\n\n"
-          body += "https://atlas-rtt.cern.ch/prod/tct/"+releaseArr[9]+"/"+releaseArr[7]+"/build/"+cmtconfig+"/offline/Tier0ChainTests/ESDTAGCOMM_comparison/ESDTAGCOMM_comparison_log \n"
-          for line in fSummary:
-              body += line
-              pass          
+          testSummaryString = """
+<p style=\"font-family: monospace;\">
+==========================================<br>
+= Summary of comparisons (ESD, AOD, TAG) =<br>
+==========================================<br>
+<br>
+(Full log available <a href=\"https://atlas-rtt.cern.ch/prod/tct/%s/%s/build/%s/offline/Tier0ChainTests/ESDTAGCOMM_comparison/ESDTAGCOMM_comparison_log\">here</a>)
+</p>
+<pre>""" % (releaseArr[9], releaseArr[7], cmtconfig)
+
+          lines = [line for line in fSummary]
+          lines.sort()
+          for line in lines:
+              testSummaryString += line
+              pass
+          testSummaryString += "</pre>"
           continue
+      # the rest of the files, if any
       fSummary = open(fileName,'r')
       lSummary = fSummary.readlines()
-      test = False
+      isSuccess = False
       for line in lSummary:
-        if 'TestCode=SUCCESS' in line: test = True
+        if 'TestCode=SUCCESS' in line: isSuccess = True
         pass
       fileNameList = fileName.split('/')
       shortFileName = fileNameList[len(fileNameList)-1]
-      if test:
-        body += ("%s = OK\n" % (shortFileName.split('.'))[0])
+      if isSuccess:
+        testSummaryString += ("%s = OK\n" % (shortFileName.split('.'))[0])
       else:
-        body += ("%s = NOT OK\n" % (shortFileName.split('.'))[0])
+        testSummaryString += ("%s = NOT OK\n" % (shortFileName.split('.'))[0])
         pass
       pass
     pass
 
+  msgbody += testSummaryString
+
+  overwriteCheckString = """
+<pre>
+
+===================================================================
+= Summary of comparisons of TAGs made with and without monitoring =
+===================================================================
+
+"""
   if len(sys.argv) >= 4:
-    # Summary of TAG comparisons for overwrite check
-    body += "\nSummary of TAG comparisons for overwrite check\n"
-    body += "==============================================\n"
     filesNames = sys.argv[3]
     filesList = filesNames.split(',')
     for fileName in filesList:
       if not os.path.exists(fileName): continue
       fOverwrite = open(fileName, 'r')
-      for line in fOverwrite: body += line
+      for line in fOverwrite: 
+        overwriteCheckString += line
 
+  overwriteCheckString += "</pre>\n"
+  msgbody += overwriteCheckString
+
+  tctStatusString = "<pre><br>"
   os.system('tct_getstatus.py --release=%s --day=%s --dq --dump' % (releaseArr[7],releaseArr[9]))
   fstatus = open('tctstatus.txt','r')
-  for line in fstatus: body += line
+  for line in fstatus: 
 
-  print body
+    tctStatusString += line
+
+  tctStatusString += "</pre>"
+  msgbody += tctStatusString
+  msgbody += "</html>"
 
   ## send emails
-  print "Sending email(s) to: " + eList
-  msg = "From: rtt@mail.cern.ch\nSubject: Finished TCT for nightly: %s\nTo: %s\n\n%s\n" % (release,eList,body)
+  print "Sending email(s) to: " + emailAddresses
+  msg = "From: rtt@mail.cern.ch\nSubject: Finished TCT for nightly: %s\nTo: %s\nContent-Type: text/html\n%s\n" % (release, emailAddresses, msgbody)
 
-  toList = eList.split(',')
-  for eaddress in toList:
-    sendMail(msg,eaddress)
+  recipients = emailAddresses.split(',')
+  for recipient in recipients:
+    print "Will now send notification email to %s with the following contents:" % recipient
+    print msg
+    sendMail(recipient, msg)
  
   ## send text messages
   #sendSms("From: rtt@mail.cern.ch\nSubject: test\n")
 
+  print "(the line below is to trick the RTT framework that this \"Athena\" job finished successfully)\n"
   print "ErrorCode=0 (OK)"
-
