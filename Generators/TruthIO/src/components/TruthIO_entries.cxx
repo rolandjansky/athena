87b36dd8c974bb7b36442fffccb3580b9d6dcfb0
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2015-05-21 12:44:25 +0200

    add HI printouts (TruthIO-00-00-13)
            * add HI printouts in PrintMC (A. Olszewski)
            * tag as TruthIO-00-00-13
    
    2015-05-05 E. M. Lobodzinska <ewelina@mail.desy.de>
            * inherit ReadHepEvtFromAscii from AthAlgorithm
            * tag as TruthIO-00-00-12
    
    2015-04-17 E. M. Lobodzinska <ewelina@mail.desy.de>
            * add ReadHepEvtFromAscii (Dirk Sammel)
            * tag as TruthIO-00-00-11

--- Generators/TruthIO/src/components/TruthIO_entries.cxx
+++ Generators/TruthIO/src/components/TruthIO_entries.cxx
@@ -4,12 +4,14 @@
 #include "TruthIO/WriteHepMC.h"
 #include "TruthIO/PrintMC.h"
 #include "TruthIO/PrintHijingPars.h"
+#include "TruthIO/ReadHepEvtFromAscii.h"
 
 DECLARE_ALGORITHM_FACTORY(DumpMC)
 DECLARE_ALGORITHM_FACTORY(HepMCReadFromFile)
 DECLARE_ALGORITHM_FACTORY(PrintMC)
 DECLARE_ALGORITHM_FACTORY(WriteHepMC)
 DECLARE_ALGORITHM_FACTORY(PrintHijingPars)
+DECLARE_ALGORITHM_FACTORY(ReadHepEvtFromAscii)
 
 DECLARE_FACTORY_ENTRIES(GeneratorModules) {
   DECLARE_ALGORITHM(DumpMC)
@@ -17,4 +19,5 @@ DECLARE_FACTORY_ENTRIES(GeneratorModules) {
   DECLARE_ALGORITHM(PrintMC)
   DECLARE_ALGORITHM(WriteHepMC)
   DECLARE_ALGORITHM(PrintHijingPars)
+  DECLARE_ALGORITHM(ReadHepEvtFromAscii)
 }
