547c72ced9935fd747ba75abc749628274944219
Nicholas Styles <nicholas.styles@desy.de>
2015-05-26 09:58:37 +0200

    added EventInfo unlocker (RecAlgs-00-00-19)
    	* Adding new alg to allow EventInfo to be updated during reconstruction from ESD
    	* Tagged as RecAlgs-00-00-19
    
    2015-03-16  scott snyder  <snyder@bnl.gov>
    
    	* Tagging RecAlgs-00-00-18.
    	* Fix clang warnings: remove unused class members.
    
    2015-03-12  scott snyder  <snyder@bnl.gov>
    
    	* Tagging RecAlgs-00-00-17.
    	* src/JobInfo.h: Fix coverity warning in root6 build.

--- Reconstruction/RecAlgs/src/components/RecAlgs_entries.cxx
+++ Reconstruction/RecAlgs/src/components/RecAlgs_entries.cxx
@@ -2,13 +2,16 @@
 #include "../TimingAlg.h"
 #include "../MemoryAlg.h"
 #include "../JobInfo.h"
+#include "../EventInfoUnlocker.h"
 
 DECLARE_ALGORITHM_FACTORY( TimingAlg )
 DECLARE_ALGORITHM_FACTORY( MemoryAlg )
 DECLARE_ALGORITHM_FACTORY( JobInfo )
+DECLARE_ALGORITHM_FACTORY( EventInfoUnlocker )
 
 DECLARE_FACTORY_ENTRIES( RecAlgs ) {
   DECLARE_ALGORITHM( TimingAlg );
   DECLARE_ALGORITHM( MemoryAlg );
   DECLARE_ALGORITHM( JobInfo );
+  DECLARE_ALGORITHM( EventInfoUnlocker );
 }   
