ee9e192ab8eac739c0ceedc55ec1507122ff272e
Attila Krasznahorkay <krasznaa@cern.ch>
2015-05-29 10:18:42 +0200

    Making the code capable of retrieving the mu values from LuminosityTool (xAODEventInfoCnv-00-00-17)
    	* Extended the code to be able to take the data mu values from
    	  the luminosity tool, instead of the already existing EventInfo
    	  object.
    	* This fixes the values put into xAOD::EventInfo while reconstructing
    	  data15 events.
    	* But more tests are needed for mc15-like setups, to make sure that
    	  the code handles it correctly when the info is not coming from the
    	  database.
    	* Tagging as xAODEventInfoCnv-00-00-17
    
    2015-04-29 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Added a new algorithm to the package, which just checks that
    	  all the xAOD::EventInfo objects that it encounters during a
    	  job, describe unique events.
    	* This is meant to be used in AODMerge_tf.py in the future, as
    	  a simple check that the production system didn't do anything
    	  "funny".
    	* Added a simple jobO to test the algorithm.
    	* Removed the executable flag from the EventInfoSelectorTool source
    	  files.
    ...
    (Long ChangeLog diff - truncated)

---
+++ Event/xAOD/xAODEventInfoCnv/src/EventDuplicateFinderAlg.cxx
@@ -0,0 +1,61 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: EventDuplicateFinderAlg.cxx 663741 2015-04-29 11:52:21Z krasznaa $
+
+// EDM include(s):
+#include "xAODEventInfo/EventInfo.h"
+
+// Local include(s):
+#include "EventDuplicateFinderAlg.h"
+
+namespace xAODReader {
+
+   EventDuplicateFinderAlg::EventDuplicateFinderAlg( const std::string& name,
+                                                     ISvcLocator* svcLoc )
+      : AthAlgorithm( name, svcLoc ),
+        m_seenEvents() {
+
+      declareProperty( "EventInfoKey", m_key = "EventInfo",
+                       "StoreGate key of the xAOD::EventInfo object to be "
+                       "used" );
+   }
+
+   StatusCode EventDuplicateFinderAlg::initialize() {
+
+      // Greet the user:
+      ATH_MSG_INFO( "Initialising - Package version: " << PACKAGE_VERSION );
+      ATH_MSG_DEBUG( "EventInfoKey = " << m_key );
+
+      // Reset the internal variable:
+      m_seenEvents.clear();
+
+      // Return gracefully:
+      return StatusCode::SUCCESS;
+   }
+
+   StatusCode EventDuplicateFinderAlg::execute() {
+
+      // Retrieve the EI object:
+      const xAOD::EventInfo* ei = 0;
+      ATH_CHECK( evtStore()->retrieve( ei, m_key ) );
+
+      // Check if we saw this event already or not:
+      if( ! m_seenEvents.insert(
+                         std::make_pair( ei->runNumber(),
+                                         ei->eventNumber() ) ).second ) {
+         ATH_MSG_FATAL( "Seeing run #" << ei->runNumber() << ", event #"
+                        << ei->eventNumber() << " for the second time" );
+         return StatusCode::FAILURE;
+      }
+
+      // No, we just saw it now for the first time:
+      ATH_MSG_VERBOSE( "Saw run #" << ei->runNumber() << ", event #"
+                       << ei->eventNumber() << " for the first time" );
+
+      // Return gracefully:
+      return StatusCode::SUCCESS;
+   }
+
+} // namespace xAODReader
