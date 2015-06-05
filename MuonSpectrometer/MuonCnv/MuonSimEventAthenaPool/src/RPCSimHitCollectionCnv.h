7914205382b9afe34a6cb53dfdf44a2764c9e7fe
Jochen Meyer <Jochen.Meyer@cern.ch>
2015-06-05 07:29:05 +0200

    factorizing out sim hit (MuonSimEventAthenaPool-00-00-00)

---
+++ MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/src/RPCSimHitCollectionCnv.h
@@ -0,0 +1,41 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUONSIMEVENTATHENAPOOL_RPCSIMHITCOLLECTIONCNV_H
+#define MUONSIMEVENTATHENAPOOL_RPCSIMHITCOLLECTIONCNV_H
+
+#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+#include "MuonSimEvent/RPCSimHitCollection.h"
+
+#include "MuonSimEventTPCnv/RPCSimHitCollectionCnv_p1.h"
+#include "MuonSimEventTPCnv/RPCSimHitCollectionCnv_p2.h"
+
+
+// the latest persistent representation type of DataCollection:
+typedef  Muon::RPCSimHitCollection_p2  RPCSimHitCollection_PERS;
+typedef  T_AthenaPoolCustomCnv<RPCSimHitCollection, RPCSimHitCollection_PERS >  RPCSimHitCollectionCnvBase;
+
+/**
+ ** Create derived converter to customize the saving of identifiable
+ ** container
+ **/
+class RPCSimHitCollectionCnv : 
+    public RPCSimHitCollectionCnvBase 
+{
+    
+    friend class CnvFactory<RPCSimHitCollectionCnv>;
+    
+public:
+    RPCSimHitCollectionCnv(ISvcLocator* svcloc);
+    virtual ~RPCSimHitCollectionCnv();
+    
+    virtual RPCSimHitCollection_PERS*   createPersistent (RPCSimHitCollection* transCont);
+    virtual RPCSimHitCollection*        createTransient ();
+        
+private:
+    RPCSimHitCollectionCnv_p1    m_TPConverter;
+    RPCSimHitCollectionCnv_p2    m_TPConverter_p2;
+};
+
+#endif
---
+++ MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/src/TGCSimHitCollectionCnv.h
@@ -0,0 +1,42 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUONSIMEVENTATHENAPOOL_TGCSIMHITCOLLECTIONCNV_H
+#define MUONSIMEVENTATHENAPOOL_TGCSIMHITCOLLECTIONCNV_H
+
+#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+#include "MuonSimEvent/TGCSimHitCollection.h"
+
+#include "MuonSimEventTPCnv/TGCSimHitCollectionCnv_p1.h"
+#include "MuonSimEventTPCnv/TGCSimHitCollectionCnv_p2.h"
+#include "MuonSimEventTPCnv/TGCSimHitCollectionCnv_p3.h"
+
+// the latest persistent representation type of DataCollection:
+typedef  Muon::TGCSimHitCollection_p3  TGCSimHitCollection_PERS;
+typedef  T_AthenaPoolCustomCnv<TGCSimHitCollection, TGCSimHitCollection_PERS >  TGCSimHitCollectionCnvBase;
+
+/**
+ ** Create derived converter to customize the saving of identifiable
+ ** container
+ **/
+class TGCSimHitCollectionCnv : 
+    public TGCSimHitCollectionCnvBase 
+{
+    
+    friend class CnvFactory<TGCSimHitCollectionCnv>;
+    
+public:
+    TGCSimHitCollectionCnv(ISvcLocator* svcloc);
+    virtual ~TGCSimHitCollectionCnv();
+    
+    virtual TGCSimHitCollection_PERS*   createPersistent (TGCSimHitCollection* transCont);
+    virtual TGCSimHitCollection*        createTransient ();
+        
+private:
+    TGCSimHitCollectionCnv_p1    m_TPConverter_p1;
+    TGCSimHitCollectionCnv_p2    m_TPConverter_p2;
+    TGCSimHitCollectionCnv_p3    m_TPConverter_p3;
+};
+
+#endif
