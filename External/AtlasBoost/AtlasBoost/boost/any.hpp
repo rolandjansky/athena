b095b7dab55b11c5bde67766b59844a0ff63c917
Scott Snyder <scott.snyder@cern.ch>
2015-06-06 22:55:45 +0200

    Avoid clang warnings. (AtlasBoost-00-00-11)
    	* Tagging AtlasBoost-00-00-11.
    	* AtlasBoost/boost/lexical_cast.hpp, AtlasBoost/boost/any.hpp:
    	Avoid more clang warnings.
    
    2015-02-22  scott snyder  <snyder@bnl.gov>
    
    	* Tagging AtlasBoost-00-00-10.
    	* Avoid clang warnings.

--- External/AtlasBoost/AtlasBoost/boost/any.hpp
+++ External/AtlasBoost/AtlasBoost/boost/any.hpp
@@ -1,12 +1,18 @@
 #ifndef ATLAS_BOOST_ANY_HPP
 #define ATLAS_BOOST_ANY_HPP
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#pragma GCC system_header
+#if defined(__GNUC__)
+# if !defined(__clang__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+# endif
+# pragma GCC system_header
+#endif
 
 #include_next "boost/any.hpp"
 
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic pop
+#endif
 
 #endif
