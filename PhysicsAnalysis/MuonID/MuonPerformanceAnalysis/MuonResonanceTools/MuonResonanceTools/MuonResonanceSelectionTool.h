afff679d734b7dd69f64fd18972c64b7359ef934
Ioannis Nomidis <Ioannis.Nomidis@cern.ch>
2015-06-11 23:54:54 +0200

    fixed mem leak (MuonResonanceTools-00-00-07)
    Former-commit-id: 4b262a3f96dedb7976933dbf4ef93f23c15dffe3

--- PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonResonanceTools/MuonResonanceTools/MuonResonanceSelectionTool.h
+++ PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonResonanceTools/MuonResonanceTools/MuonResonanceSelectionTool.h
@@ -41,7 +41,6 @@ class MuonResonanceSelectionTool
   bool IDTrk(const xAOD::Muon& mu) const;
   void applySF(const xAOD::Muon& mu, bool isMC) const;
   xAOD::Muon* copy(const xAOD::Muon& mu) const;
-  bool hasPassedGRL (void) const;
   bool isTriggered (void) const;
   void applyTriggerMatch(xAOD::Muon& mu) const; 
 
