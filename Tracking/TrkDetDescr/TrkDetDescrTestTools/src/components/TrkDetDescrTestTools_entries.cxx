fd889ba25fad660b20c3d618245ed0048fb90dca
Andreas Salzburger <salzburg@cern.ch>
2015-06-11 07:27:39 +0200

    Tagging as TrkDetDescrTestTools-00-01-09 (TrkDetDescrTestTools-00-01-09)
    2015-06-10 Andreas Salzburger < Andreas.Salzburger -at- cern.ch >
            * adding overlap descriptor to VertexMapper
            * tagged TrkDetDescrTestTools-00-01-09
    
    2015-06-10 Andreas Salzburger < Andreas.Salzburger -at- cern.ch >
            * new VertexMapper for secondary vertex association
            * tagged TrkDetDescrTestTools-00-01-08
    
    2015-06-04 Andreas Salzburger < Andreas.Salzburger -at- cern.ch >
    	* Enhanced MaterialMapper for Z/A*rho validation
            * tagged TrkDetDescrTestTools-00-01-07
    
    2015-05-08 Andreas Salzburger
    	* Corrected phi labelling
            * tagged TrkDetDescrTestTools-00-01-06
    
    2015-05-08 Andreas Salzburger
    	* added accumulated path length versus step & corect
            * tagged TrkDetDescrTestTools-00-01-05
    
    
    Former-commit-id: 422e2de40eab925be1b8775056c12dead44cc46c

--- Tracking/TrkDetDescr/TrkDetDescrTestTools/src/components/TrkDetDescrTestTools_entries.cxx
+++ Tracking/TrkDetDescr/TrkDetDescrTestTools/src/components/TrkDetDescrTestTools_entries.cxx
@@ -2,6 +2,7 @@
 #include "TrkDetDescrTestTools/GeometryAsciiDumper.h"
 #include "TrkDetDescrTestTools/GeometryTTreeDumper.h"
 #include "TrkDetDescrTestTools/MaterialMapper.h"
+#include "TrkDetDescrTestTools/VertexMapper.h"
 #include "TrkDetDescrTestTools/LayerMaterialInspector.h"
 #include "TrkDetDescrTestTools/LayerMaterialAnalyser.h"
 #include "TrkDetDescrTestTools/RandomSurfaceBuilder.h"
@@ -13,6 +14,7 @@ using namespace Trk;
 DECLARE_TOOL_FACTORY( GeometryAsciiDumper )
 DECLARE_TOOL_FACTORY( GeometryTTreeDumper )
 DECLARE_TOOL_FACTORY( MaterialMapper )
+DECLARE_TOOL_FACTORY( VertexMapper )
 DECLARE_TOOL_FACTORY( LayerMaterialInspector )
 DECLARE_TOOL_FACTORY( LayerMaterialAnalyser )
 DECLARE_TOOL_FACTORY( RandomSurfaceBuilder )
@@ -25,6 +27,7 @@ DECLARE_FACTORY_ENTRIES( TrkDetDescrTestTools )
     DECLARE_TOOL( GeometryAsciiDumper )
     DECLARE_TOOL( GeometryTTreeDumper )
     DECLARE_TOOL( MaterialMapper )
+    DECLARE_TOOL( VertexMapper )
     DECLARE_TOOL( LayerMaterialInspector )
     DECLARE_TOOL( LayerMaterialAnalyser )
     DECLARE_TOOL( RandomSurfaceBuilder )
