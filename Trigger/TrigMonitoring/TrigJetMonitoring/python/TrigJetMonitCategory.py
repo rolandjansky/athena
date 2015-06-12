1f9db532ab728efe5b7a41a596a5c3dc91d12f5d
Giulio Cornelio Grossi <giulio.cornelio.grossi@cern.ch>
2015-06-12 14:28:03 +0200

    Added new ES chains for Week3 (TrigJetMonitoring-00-05-08)
    	* Major Changes: Added new set of chains present in ES for Week3
    	*                Changed logic on how turn-on are filled: it now select                          the leading jet in a certain eta range
    
    2015-05-15 Giulio Cornelio Grossi <giulio.cornelio.grossi@cern.ch>
    	* Small Changes: moved Turn-on curves filling in proc() function
    	* Tag TrigJetMonitoring-00-05-07 created
    
    2015-05-15 Giulio Cornelio Grossi <giulio.cornelio.grossi@cern.ch>
    	* Major update: added Turn-on curves and average luminosity vs lumiblock number plots.
    	* Fixed coverity issue report.
    	* Changes in HLTJetMonitorinTool.cxx
    	* Tag TrigJetMonitoring-00-05-06 created
    
    	* Changes in HLTJetMonitorinTool.cxx :
    	* Tag TrigJetMonitoring-00-05-05 created

--- Trigger/TrigMonitoring/TrigJetMonitoring/python/TrigJetMonitCategory.py
+++ Trigger/TrigMonitoring/TrigJetMonitoring/python/TrigJetMonitCategory.py
@@ -2,8 +2,8 @@
 
 ## Monitoring triggers
 ## Modify to the default naming scheme for menu-aware montioring.
-monitoring_l1jet = ['L1_RD0_FILLED', 'L1_J15']
-monitoring_jet = ['j25', 'j60', 'j200_jes_PS'] 
+monitoring_l1jet = ['L1_J15','L1_RD0_FILLED']
+monitoring_jet = [ 'j25','j60', 'j60_L1RD0','j60_280eta320','j60_320eta490','j200_jes_PS','j360','j460_a10_sub_L1J100'] 
 
-primary_l1jet                 = [ 'L1_J15' ]
-primary_jet                = ['j25', 'j60' , 'j200_jes_PS'] 
+primary_l1jet                 = [ 'L1_J15','L1_RD0_FILLED']
+primary_jet                = ['j25','j60', 'j60_L1RD0','j60_280eta320','j60_320eta490','j200_jes_PS','j360','j460_a10_sub_L1J100'] 
