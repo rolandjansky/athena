7d4f5e03433e8e76db08143c1c2b15014675045a
Massimiliano Bellomo <massimiliano.bellomo@cern.ch>
2015-06-22 12:43:31 +0200

    'reverted back to previous tag to be cache compatible' (TrkValHistUtils-00-01-11)
    Former-commit-id: 7a0a8e8d873431b07c8d37981c6060847f347534

--- Tracking/TrkValidation/TrkValHistUtils/Root/EfficiencyPlots.cxx
+++ Tracking/TrkValidation/TrkValHistUtils/Root/EfficiencyPlots.cxx
@@ -6,34 +6,32 @@
 #include "TrkValHistUtils/ParamPlots.h"
 #include "TrkValHistUtils/EfficiencyPurityCalculator.h"
 
-namespace Trk {
-  EfficiencyPlots::EfficiencyPlots(PlotBase *pParent, std::string sDir, ParamPlots *pNumPlots,
-                                   ParamPlots *pDenomPlots) : PlotBase(pParent, sDir) {
-    m_pNumPlots = pNumPlots;
-    m_pDenomPlots = pDenomPlots;
+namespace Trk{
 
-    eff_eta = NULL;
-    eff_phi = NULL;
-    eff_pt = NULL;
-    eff_pti = NULL;
-  }
+EfficiencyPlots::EfficiencyPlots(PlotBase* pParent, std::string sDir, ParamPlots* pNumPlots, ParamPlots* pDenomPlots):PlotBase(pParent, sDir){
+  m_pNumPlots = pNumPlots;
+  m_pDenomPlots = pDenomPlots; 
 
-  void
-  EfficiencyPlots::initializePlots() {
-    eff_eta = Book1D("Eff_eta", m_pNumPlots->eta, "Eta Efficiency; eta; efficiency");
-    eff_phi = Book1D("Eff_phi", m_pNumPlots->phi, "Phi Efficiency;Azimuthal Angle; efficiency");
-    eff_pt = Book1D("Eff_pt", m_pNumPlots->pt, "Pt Efficiency; Transverse Momentum [GeV]; efficiency");
-    eff_pti = Book1D("Eff_pti", m_pNumPlots->pt,
-                     "Pt integrated Efficiency; Integrated Transverse Momentum [GeV]; efficiency");
-  }
-
-  void
-  EfficiencyPlots::finalizePlots() {
-    EfficiencyPurityCalculator calc;
-
-    calc.calculateEfficiency(m_pDenomPlots->eta, m_pNumPlots->eta, eff_eta);
-    calc.calculateEfficiency(m_pDenomPlots->phi, m_pNumPlots->phi, eff_phi);
-    calc.calculateEfficiency(m_pDenomPlots->pt, m_pNumPlots->pt, eff_pt);
-    calc.calculateIntegrated(m_pDenomPlots->pt, m_pNumPlots->pt, eff_pti, 1, EfficiencyPurityCalculator::kX);
-  }
+  eff_eta=NULL;
+  eff_phi=NULL;
+  eff_pt=NULL;
+  eff_pti=NULL;
 }
+
+void EfficiencyPlots::initializePlots() {
+  eff_eta  = Book1D("Eff_eta", m_pNumPlots->eta, "Eta Efficiency; eta; efficiency"); 
+  eff_phi  = Book1D("Eff_phi", m_pNumPlots->phi, "Phi Efficiency;Azimuthal Angle; efficiency");
+  eff_pt   = Book1D("Eff_pt" , m_pNumPlots->pt, "Pt Efficiency; Transverse Momentum [GeV]; efficiency");
+  eff_pti  = Book1D("Eff_pti" , m_pNumPlots->pt, "Pt integrated Efficiency; Integrated Transverse Momentum [GeV]; efficiency");
+}
+
+void EfficiencyPlots::finalizePlots() {
+  EfficiencyPurityCalculator calc;
+  calc.calculateEfficiency(m_pDenomPlots->eta, m_pNumPlots->eta, eff_eta);
+  calc.calculateEfficiency(m_pDenomPlots->phi, m_pNumPlots->phi, eff_phi);
+  calc.calculateEfficiency(m_pDenomPlots->pt, m_pNumPlots->pt, eff_pt);
+  calc.calculateIntegrated(m_pDenomPlots->pt, m_pNumPlots->pt, eff_pti, 1, EfficiencyPurityCalculator::kX);
+}
+
+}
+
--- Tracking/TrkValidation/TrkValHistUtils/TrkValHistUtils/PlotBase.h
+++ Tracking/TrkValidation/TrkValHistUtils/TrkValHistUtils/PlotBase.h
@@ -11,24 +11,15 @@
 #define TRKVALHISTUTILS_PLOTBASE_H
 
 #include <string>
-#include <utility> //for std::pair
-#include <vector>
-//should be possible to fwd-declare TH1F, TH2F, TH3F, TProfile, TProfile2D TTree .. why isn't this done?
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TH3F.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
 #include "TProfile.h"
 #include "TProfile2D.h"
 #include "TTree.h"
-#include "TEfficiency.h"
-
-
-//fwd-declares
-class TEfficiency;
 
 typedef std::pair<TH1*, std::string> HistData;
 typedef std::pair<TTree*, std::string> TreeData;
-typedef std::pair<TEfficiency*, std::string> EfficiencyData;
 
 class PlotBase {
 public:
@@ -43,41 +34,36 @@ public:
   std::vector<HistData> retrieveBookedHistograms();
   ///Retrieve all booked trees
   std::vector<TreeData> retrieveBookedTrees();
-  ///Retrieve all booked efficiency objects
-  std::vector<EfficiencyData> retrieveBookedEfficiencies();
+
   ///@name Methods to book monitoring histograms
-  ///Note: methods starting with capitals should be deprecated in favour of camel-cased methods
   ///@{
 
   /// Book a TH1D histogram
-  TH1F* Book1D(const std::string & name, const std::string & labels, int nBins, float start, float end, bool prependDir = true);
+  TH1* Book1D(const std::string & name, const std::string & labels, int nBins, float start, float end, bool prependDir = true);
   /// Book a TH1D histogram using refHist as reference for number of bins and axis range
-  TH1F* Book1D(const std::string & name, TH1* refHist, const std::string & labels, bool prependDir = true);
+  TH1* Book1D(const std::string & name, TH1* refHist, const std::string & labels, bool prependDir = true);
 
   /// Book a TH2D histogram
-  TH2F* Book2D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, bool prependDir = true);
+  TH2* Book2D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, bool prependDir = true);
   /// Book a TH2D histogram using refHist as reference for number of bins and axis range
-  TH2F* Book2D(const std::string & name, TH2* refHist, const std::string & labels, bool prependDir = true);
+  TH2* Book2D(const std::string & name, TH2* refHist, const std::string & labels, bool prependDir = true);
   /// Book a TH2D histogram with variable x axis binning
-  TH2F* Book2D(const std::string & name, const std::string & labels, int nBinsX, Double_t* binsX, int nBinsY, Double_t startY, Double_t endY, bool prependDir = true);
+  TH2* Book2D(const std::string & name, const std::string & labels, int nBinsX, Double_t* binsX, int nBinsY, Double_t startY, Double_t endY, bool prependDir = true);
 
   /// Book a TH3D histogram
-  TH3F* Book3D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, int nBinsZ, float startZ, float endZ, bool prependDir = true);
+  TH3* Book3D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, int nBinsZ, float startZ, float endZ, bool prependDir = true);
   /// Book a TH3D histogram using refHist as reference for number of bins and axis range
-  TH3F* Book3D(const std::string & name, TH3* refHist, const std::string & labels, bool prependDir = true);
+  TH3* Book3D(const std::string & name, TH3* refHist, const std::string & labels, bool prependDir = true);
 
   /// Book a TProfile histogram
-  TProfile* BookTProfile(const std::string &name, const std::string & labels, int nBinsX, float startX, float endX, float startY=-1, float endY=-1, bool prependDir = true, bool useRMS=false);
+  TProfile* BookTProfile(const std::string &name, const std::string & labels, int nBinsX, float startX, float endX, float startY=-1, float endY=-1, bool prependDir = true);
   /// Book a TProfile histogram with variable binning in x-axis
   TProfile* BookTProfile(const std::string &name, const std::string & labels, int nBinsX, float* binsX, bool prependDir = true);
   /// Book a TProfile histogram with variable binning in x-axis and limits in y-values
   TProfile* BookTProfileRangeY(const std::string &name, const std::string & labels, int nBinsX, double* binsX, double startY, double endY, bool prependDir = true); //cannot overload, conflicts with previous definitions
   /// Book a TProfile 2D histogram with variable binning in x-axis and limits in y-values
-  TProfile2D * BookTProfile2D(const std::string &name, const std::string & labels, const int nBinsX, const double xlo, const double xhi, const int nBinsY, const double ylo, const double yhi, bool prependDir=true, bool useRMS=false);
-  /// Book a TProfile 2D histogram with variable binning in x-axis and limits in y-values
-  TProfile2D * BookTProfile2D(const std::string &name, const std::string &labels, const int nBinsX, double* binsX, const int nBinsY, double* binsY, bool prependDir=true, bool useRMS=false);
-  /// Book a (1-D) TEfficiency histogram
-  TEfficiency * BookTEfficiency(const std::string &name, const std::string & labels, const int nBinsX, const float xlo, const float xhi, const bool prependDir = true);
+  TProfile2D * BookTProfile2D(const std::string &name, const std::string & labels, const int nBinsX, 
+    const double xlo, const double xhi, const int nBinsY, const double ylo, const double yhi, bool prependDir=true);
   ///@}
   /// Book a TTree
   TTree* BookTree(const std::string & name, bool prependDir = true);
@@ -93,7 +79,6 @@ protected:
   std::vector<PlotBase*> m_vSubNodes;
   std::vector<HistData>  m_vBookedHistograms;
   std::vector<TreeData>  m_vBookedTrees;
-  std::vector<EfficiencyData>  m_vBookedEfficiencies;
   std::string m_sDirectory;
   int m_iDetailLevel;
 };
