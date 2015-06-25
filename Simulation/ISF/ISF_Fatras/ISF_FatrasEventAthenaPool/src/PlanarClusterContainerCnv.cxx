52bcebf715e721d9b300ecd5c9482381664b8bce
Noemi Calace <noemi.calace@cern.ch>
2015-06-25 15:13:51 +0200

    tagged as ISF_FatrasEventAthenaPool-00-00-01 (ISF_FatrasEventAthenaPool-00-00-01)
    	* Updating the converter to use PlanarCluster*_p2
            * tag ISF_FatrasEventAthenaPool-00-00-01
    
            * initial import of Noemi's code
            * tag ISF_FatrasEventAthenaPool-00-00-00

--- Simulation/ISF/ISF_Fatras/ISF_FatrasEventAthenaPool/src/PlanarClusterContainerCnv.cxx
+++ Simulation/ISF/ISF_Fatras/ISF_FatrasEventAthenaPool/src/PlanarClusterContainerCnv.cxx
@@ -34,11 +34,17 @@ iFatras::PlanarClusterContainer* PlanarClusterContainerCnv::createTransient() {
   m_log.setLevel( m_msgSvc->outputLevel() );
   updateLog(); 
 
-  static pool::Guid   p1_guid("E0411C25-64DD-49D9-AC1A-8E32A75C8ABF"); // 
+  static pool::Guid   p1_guid("E0411C25-64DD-49D9-AC1A-8E32A75C8ABF"); // with PlanarCluster_p1
+  static pool::Guid   p2_guid("8697937F-1E3B-4C80-876E-0D6467C50688"); // with PlanarCluster_p2
+  ATH_MSG_DEBUG("createTransient(): main converter");
   
   iFatras::PlanarClusterContainer* p_collection(0);
-  if( compareClassGuid(p1_guid) ) {
-    
+  if( compareClassGuid(p2_guid) ) {
+    ATH_MSG_DEBUG("createTransient(): T/P version 2 detected");
+    std::auto_ptr< iFatras::PlanarClusterContainer_p2 >  p_coll( poolReadObject< iFatras::PlanarClusterContainer_p2 >() );
+    p_collection = m_converter_p2.createTransient( p_coll.get(), msg() );
+  } else if ( compareClassGuid(p1_guid) ) {
+    ATH_MSG_DEBUG("createTransient(): T/P version 1 detected");
     std::auto_ptr< iFatras::PlanarClusterContainer_p1 >  p_coll( poolReadObject< iFatras::PlanarClusterContainer_p1 >() );
     p_collection = m_converter_p1.createTransient( p_coll.get(), msg() );
   } else {
@@ -53,7 +59,7 @@ PlanarClusterContainer_PERS* PlanarClusterContainerCnv::createPersistent (iFatra
   m_log.setLevel( m_msgSvc->outputLevel() );
   updateLog(); 
   
-  PlanarClusterContainer_PERS *pldc_p= m_converter_p1.createPersistent( transCont, msg() );
+  PlanarClusterContainer_PERS *pldc_p= m_converter_p2.createPersistent( transCont, msg() );
   
   return pldc_p;
 }
