024c8706267bcbee3db3a09fb2dc813389ada00a
Joao Goncalves Pinto Firmino Da Costa <joao.costa@cern.ch>
2015-07-03 16:03:06 +0200

    Updated PhysValInDet_jobOptions.py for future  goodies for tracking plots. Code provided by Goetz Gaycken (PhysValMonitoring-00-01-40)
        * Updated PhysValInDet_jobOptions.py for future  goodies for tracking plots. Code provided by Goetz Gaycken
        * PhysValMonitoring-00-01-39
    
    31.05.2015 Patrick Czodrowski <czodrows@cern.ch>
        * Add TrigHLTMonitoring JO wrapper for all NEW doTrigHLTResult validation flag to retrieve TirggerCounts
        * PhysValMonitoring-00-01-38

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
@@ -1,43 +1,12 @@
+import InDetPhysValMonitoring.InDetPhysValDecoration
+decoration = InDetPhysValMonitoring.InDetPhysValDecoration.addDecoratorIfNeeded()
 
-from InDetPhysValMonitoring.InDetPhysValMonitoringConf import InDetPhysValMonitoringTool
-tool1 = InDetPhysValMonitoringTool()
-#Are we using a track selector...?
-tool1.useTrackSelection = False
-tool1.EnableLumi = False
-tool1.TruthParticleContainerName = "TruthParticles"
-ToolSvc += tool1
-monMan.AthenaMonTools += [tool1]
-'''
-#if the Track selector is used , selector must be initialised somewhere
-from InDetTrackSelectorTool.InDetTrackSelectorToolConf import InDet__InDetDetailedTrackSelectorTool
-InDetTrackSelectorTool = InDet__InDetDetailedTrackSelectorTool(name                                = "InDetDetailedTrackSelectorTool",
-                                                               pTMin                               = InDetPrimaryVertexingCuts.minPT(),
-                                                               IPd0Max                             = InDetPrimaryVertexingCuts.IPd0Max(),
-                                                               IPz0Max                             = InDetPrimaryVertexingCuts.IPz0Max(),
-                                                               z0Max                               = InDetPrimaryVertexingCuts.z0Max(),
-                                                               sigIPd0Max                          = InDetPrimaryVertexingCuts.sigIPd0Max(),
-                                                               sigIPz0Max                          = InDetPrimaryVertexingCuts.sigIPz0Max(),
-                                                               d0significanceMax                   = InDetPrimaryVertexingCuts.d0significanceMax(),
-                                                               z0significanceMax                   = InDetPrimaryVertexingCuts.z0significanceMax(),
-                                                               etaMax                              = InDetPrimaryVertexingCuts.etaMax(),
-                                                               useTrackSummaryInfo                 = InDetPrimaryVertexingCuts.useTrackSummaryInfo(),
-                                                               nHitBLayer                          = InDetPrimaryVertexingCuts.nHitBLayer(),
-                                                               nHitPix                             = InDetPrimaryVertexingCuts.nHitPix(),
-                                                               nHolesPixel                         = InDetPrimaryVertexingCuts.nHolesPix(),
-                                                               nHitBLayerPlusPix                   = InDetPrimaryVertexingCuts.nHitBLayerPlusPix(),
-                                                               nHitSct                             = InDetPrimaryVertexingCuts.nHitSct(),
-                                                               nHitSi                              = InDetPrimaryVertexingCuts.nHitSi(),
-                                                               nHitTrt                             = InDetPrimaryVertexingCuts.nHitTrt(),
-                                                               nHitTrtHighEFractionMax             = InDetPrimaryVertexingCuts.nHitTrtHighEFractionMax(),
-                                                               nHitTrtHighEFractionWithOutliersMax = InDetPrimaryVertexingCuts.nHitTrtHighEFractionWithOutliersMax(),
-                                                               useSharedHitInfo                    = InDetPrimaryVertexingCuts.useSharedHitInfo(),
-                                                               useTrackQualityInfo                 = InDetPrimaryVertexingCuts.useTrackQualityInfo(),
-                                                               fitChi2OnNdfMax                     = InDetPrimaryVertexingCuts.fitChi2OnNdfMax(),
-                                                               TrtMaxEtaAcceptance                 = InDetPrimaryVertexingCuts.TrtMaxEtaAcceptance(),
-                                                               # InDetTestBLayerTool                 = InDetRecTestBLayerTool,
-                                                               TrackSummaryTool                    = InDetTrackSummaryTool,
-                                                               Extrapolator                        = InDetExtrapolator)
+# add ID physics validation monitoring tool
 
-        
-ToolSvc += InDetTrackSelectorTool
-'''
+from InDetPhysValMonitoring.InDetPhysValMonitoringTool import InDetPhysValMonitoringTool
+indet_mon_tool = InDetPhysValMonitoringTool.InDetPhysValMonitoringTool()
+
+ToolSvc += indet_mon_tool
+monMan.AthenaMonTools += [indet_mon_tool]
+
+
