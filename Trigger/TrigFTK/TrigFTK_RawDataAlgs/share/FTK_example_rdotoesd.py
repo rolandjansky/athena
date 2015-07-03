0f5ad37839e49135ea33a83a2d558dfbe197de90
John Baines <john.baines@stfc.ac.uk>
2015-07-03 17:21:21 +0200

    coverity fixes and additional configurable switches (TrigFTK_RawDataAlgs-00-00-07)
    	* src/FTK_RDO_ReaderAlgo.cxx: Changes for covertity errors and define properties to switch on/off retrival of different objects

--- Trigger/TrigFTK/TrigFTK_RawDataAlgs/share/FTK_example_rdotoesd.py
+++ Trigger/TrigFTK/TrigFTK_RawDataAlgs/share/FTK_example_rdotoesd.py
@@ -1,5 +1,5 @@
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-athenaCommonFlags.FilesInput =["user.gvolpi.5174418.EXT2._000001.RDO_FTK.pool.root"]  # new Ztautau sample
+athenaCommonFlags.FilesInput =["group.trig-daq.5683272.EXT1._000005.RDO_FTK.pool.root"]
 if not 'rec.doWriteESD' in dir():
     rec.doWriteESD=True
 
@@ -11,22 +11,24 @@ rec.doWriteTAG=False
 
 # main jobOption
 include ("RecExCommon/RecExCommon_topOptions.py")
+
+from FTK_DataProviderSvc.FTK_DataProviderSvc_Config import ConfiguredFTK_DataProviderSvc
+FTK_DataProviderSvc = ConfiguredFTK_DataProviderSvc("ConfiguredFTK_DataProviderSvc")
+ServiceMgr += FTK_DataProviderSvc
+#ServiceMgr.ConfiguredFTK_DataProviderSvc.OutputLevel=VERBOSE
+
 from AthenaCommon.AlgSequence import AlgSequence
+
 alg = AlgSequence()
 
 from TrigFTK_RawDataAlgs.TrigFTK_RawDataAlgsConf import FTK_RDO_ReaderAlgo
 
 FTK_RDO_Reader = FTK_RDO_ReaderAlgo( "FTK_RDO_ReaderAlgo" , OutputLevel=VERBOSE)
-
+FTK_RDO_Reader.FTK_DataProvider=FTK_DataProviderSvc
 FTK_RDO_Reader.RDO_CollectionName="FTK_RDO_Tracks"
 FTK_RDO_Reader.fillHists=True
 
 alg += FTK_RDO_Reader  
 
-from AthenaCommon.AppMgr import ServiceMgr
-from FTK_DataProviderSvc.FTK_DataProviderSvc_Config import ConfiguredFTK_DataProviderSvc
 
-FTK_DataProviderSvc = ConfiguredFTK_DataProviderSvc("ConfiguredFTK_DataProviderSvc",TrackFitter="GX2", OutputLevel=VERBOSE);
-
-ServiceMgr += FTK_DataProviderSvc
 
