2ad9a7c4db66707d288ea2d48d6e39aa224bc3b9
Sasha Vanyashin <sacha.vaniachine@cern.ch>
2015-07-05 17:48:44 +0200

    tagging EventPTCnv-00-00-02 for ATEAM-186 (EventPTCnv-00-00-02)

---
+++ Event/EventPTCnv/src/EventTypePTCnv_p3.cxx
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+  * @file EventTypePTCnv_p3.cxx
+  *
+  * @author Alexandre Vaniachine <vaniachine@anl.gov>
+  *
+  */
+
+#include "EventPTCnv/EventTypePTCnv_p3.h"
