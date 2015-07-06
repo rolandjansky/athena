a2ba6805ff3da870850c5f5dfc79f9d423955d2c
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2015-07-06 09:56:06 +0200

    remove chargino from a list of noninteracting particles (TruthUtils-02-00-09-01)
            * tagging TruthUtils-02-00-09-01
            * remove chargino (10000024) from noninteracting particles

--- Generators/TruthUtils/TruthUtils/HepMCHelpers.h
+++ Generators/TruthUtils/TruthUtils/HepMCHelpers.h
@@ -72,7 +72,9 @@ namespace MC {
     if (apid == 12 || apid == 14 || apid == 16) return true;
     /// @todo Use MCUtils functions to make this more general, e.g. isStronglyInteracting || isEMInteracting (need to write these fns)
     if (p->status() % 1000 == 1) { //< Isn't this implied by isGenStable?
-      if (apid == 1000022 || apid == 1000024 || apid == 5100022) return true;
+      //      if (apid == 1000022 || apid == 1000024 || apid == 5100022) return true;
+      // remove chargino from list of nonIntercating for rel. 19, s not used in truth jet building
+      if (apid == 1000022 || apid == 5100022) return true;
       if (apid == 39 || apid == 1000039 || apid == 5000039) return true;
       if (apid == 3000015) return true;
     }
