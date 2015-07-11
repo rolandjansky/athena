9f5145f1ea3e3cc614d7cddbab3a175c88f8c346
Juergen Thomas <Juergen.Thomas@cern.ch>
2015-07-11 09:18:29 +0200

    Bugfix provided by MDT s/w expert: We used 'BAD_NAME' wrongly, no Muon geo written (GeometryJiveXML-00-01-12)
    Former-commit-id: 29575424ff5d4c45003951b2d38016124d179841

--- graphics/GeometryJiveXML/GeometryJiveXML/GeometryWriter.h
+++ graphics/GeometryJiveXML/GeometryJiveXML/GeometryWriter.h
@@ -153,15 +153,15 @@ namespace JiveXML{
     
     /** Pixel detector manager and ID helper */
     const InDetDD::SiDetectorManager* m_pixel_manager;
-    const PixelID * m_pixelIdHelper;   
+    const PixelID * pixelIdHelper;   
 
     /** Silicon detector manager and ID helper */
     const InDetDD::SiDetectorManager* m_silicon_manager;
-    const SCT_ID * m_sctIdHelper;
+    const SCT_ID * sctIdHelper;
     
     /** TRT detector manager and ID helper */
     const InDetDD::TRT_DetectorManager* m_trt_manager;
-    const TRT_ID * m_trtIdHelper;
+    const TRT_ID * trtIdHelper;
     
     /** Calorimeter detector manager. */
     const CaloDetDescrManager* m_calo_manager;
