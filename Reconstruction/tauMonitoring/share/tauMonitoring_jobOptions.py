846dbb8fa9eb639a73503205c82db8d9c8a8aae5
Lianyou Shan <lianyou.shan@cern.ch>
2015-07-18 12:03:43 +0200

    minor fix to pass qtest (tauMonitoring-00-03-06)

--- Reconstruction/tauMonitoring/share/tauMonitoring_jobOptions.py
+++ Reconstruction/tauMonitoring/share/tauMonitoring_jobOptions.py
@@ -3,7 +3,8 @@ monManTau = AthenaMonManager(
              FileKey             = DQMonFlags.monManFileKey(),
              Environment         = DQMonFlags.monManEnvironment(),
              ManualDataTypeSetup = DQMonFlags.monManManualDataTypeSetup(),
-             DataType            = DQMonFlags.monManDataType())
+             DataType            = DQMonFlags.monManDataType()
+          )
 topSequence += monManTau
 
 
@@ -13,7 +14,8 @@ monbadlb = GetLArBadLBFilterTool()
 
 tauMonTool           = tauMonTool (name = "tauMonTool")
 tauMonTool.doTrigger = DQMonFlags.useTrigger()
-
+if DQMonFlags.useTrigger() :
+    tauMonTool.TauEleJetMbtsTriggerItems = [ [ "L1_TAU1.*" , "L1_TAU2.*" , "L1_TAU60" , "L1_EM15VHI_2TAU.*" , "L1_EM15VHI_TAU40.*" , "L1_MU10_TAU.*" , "L1_J25_3J12_EM15-TAU.*" ] , [ "HLT_e2.*" , "L1_EM2.*" , "L1_2EM15" ] , [ "HLT_j55" , "HLT_j60" , "HLT_j85" , "HLT_j110" , "HLT_j175" , "HLT_4j85" , "HLT_4j10.*" ] , [ "HLT_mb_sp2000_trk70_hmt" ] ]
 
 tauMonTool.FilterTools = [ monAtlasReadyFilterTool ]
 tauMonTool.FilterTools.append(monbadlb)
