1086c7ad87e2182d2284c4d14bbfa567c914b7b6
Will Buttinger <William.Buttinger@cern.ch>
2015-07-20 10:07:19 +0200

    'Moved HepMCWeightSvc to its own package' (EvgenProdTools-00-02-24)
          * Moved HepMCWeightSvc to its own package (Generators/HepMCWeightSvc)
          * tagging as EvgenProdTools-00-02-24
    
    2015-07-13 Will Buttinger <will@cern.ch>
            * src/HepMCWeightSvc.cxx: if the weightname map is the default {'0':0}, then don't bother writing it!
            * tagging EvgenProdTools-00-02-23
    
    2015-07-10 Will Buttinger <will@cern.ch>
            * src/HepMCWeightSvc.cxx: Added missing initialization of flag ... thank you John Chapman for finding this!
            * tagging EvgenProdTools-00-02-22

--- Generators/EvgenProdTools/src/components/EvgenProdTools_entries.cxx
+++ Generators/EvgenProdTools/src/components/EvgenProdTools_entries.cxx
@@ -1,6 +1,5 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
-#ifndef XAOD_ANALYSIS
 #include "EvgenProdTools/CopyEventWeight.h"
 #include "EvgenProdTools/CountHepMC.h"
 #include "EvgenProdTools/FixHepMC.h"
@@ -12,19 +11,16 @@ DECLARE_ALGORITHM_FACTORY(CountHepMC)
 DECLARE_ALGORITHM_FACTORY(FixHepMC)
 DECLARE_ALGORITHM_FACTORY(TestHepMC)
 DECLARE_SERVICE_FACTORY(EvgenOTFTopUpSvc)
-#endif
 
-#include "../HepMCWeightSvc.h"
-DECLARE_SERVICE_FACTORY(HepMCWeightSvc)
+
+
 
 DECLARE_FACTORY_ENTRIES( EvgenProdTools ) {
-#ifndef XAOD_ANALYSIS
   DECLARE_ALGORITHM( ExpressionGenFilter )
   DECLARE_ALGORITHM(CopyEventWeight)
   DECLARE_ALGORITHM(CountHepMC)
   DECLARE_ALGORITHM(FixHepMC)
   DECLARE_ALGORITHM(TestHepMC)
   DECLARE_SERVICE(EvgenOTFTopUpSvc)
-#endif
   DECLARE_SERVICE(HepMCWeightSvc)
 }
