580b5e61f66757ac77810eb8c6530b4c72edd446
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-07-21 16:45:28 +0200

    python/MinBiasScintillatorConfig.py (getMinBiasScintillatorSD): add missing configuration for cosmics jobs. ATLASSIM-2070. Tagging MinBiasScintillator-00-03-02 (MinBiasScintillator-00-03-02)
    2015-07-21  John Chapman  <John.Chapman@cern.ch>
    
    	* python/MinBiasScintillatorConfig.py (getMinBiasScintillatorSD):
    	add missing configuration for cosmics jobs. ATLASSIM-2070.
    	* Tagging MinBiasScintillator-00-03-02
    
    2015-07-10  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging MinBiasScintillator-00-03-01
    	* src/MinBiasScintillatorSD.cc, src/MinBiasScintillatorSD.h: Fix
    	unitialized/uncleared variable issue. ATLASSIM-2070. Remove two
    	unused member variables. Clean up logging print outs.
    
    2015-07-08  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging MinBiasScintillator-00-03-00
    	* Migration to new base classes. ATLASSIM-2070

---
+++ TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/MinBiasScintillatorSDTool.h
@@ -0,0 +1,39 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//************************************************************
+//
+// Class MinBiasScintillatorSDTool
+// Tool for configuring the Sensitive detector for the Minimum Bias Scintillator
+//
+//************************************************************
+
+#ifndef MINBIASSCINTILLATOR_MINBIASSCINTILLATORSDTOOL_H
+#define MINBIASSCINTILLATOR_MINBIASSCINTILLATORSDTOOL_H
+
+// Base class
+#include "G4AtlasTools/SensitiveDetectorBase.h"
+
+#include "MinBiasScintSDOptions.h"
+
+class MinBiasScintillatorSDTool : public SensitiveDetectorBase
+{
+public:
+  MinBiasScintillatorSDTool(const std::string& type, const std::string& name, const IInterface *parent);
+
+  ~MinBiasScintillatorSDTool() { };
+
+  /** End of an athena event */
+  StatusCode Gather() override final; //FIXME would be good to be able to avoid this.
+
+protected:
+  // Make me an SD!
+  G4VSensitiveDetector* makeSD() override final;
+
+private:
+  // Options for the SD configuration
+  MinBiasScintSDOptions m_options;
+};
+
+#endif //MINBIASSCINTILLATOR_MINBIASSCINTILLATORSDTOOL_H
---
+++ TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/components/MinBiasScintillator_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "../MinBiasScintillatorSDTool.h"
+
+DECLARE_TOOL_FACTORY( MinBiasScintillatorSDTool )
+
+DECLARE_FACTORY_ENTRIES( MinBiasScintillator ) {
+  DECLARE_TOOL( MinBiasScintillatorSDTool )
+}
