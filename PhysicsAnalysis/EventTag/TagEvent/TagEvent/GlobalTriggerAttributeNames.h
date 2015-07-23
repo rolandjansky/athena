b1a9e685e5d1b58f76a01ed60fa49fa653cf1f2d
Till Eifert <till.eifert@cern.ch>
2015-07-23 14:11:57 +0200

    from L2+EF to merged HLT. (TagEvent-00-05-19)
    	* TagEvent/GlobalTriggerAttributeNames.h: changes from L2+EF to combined HLT.
    	* TagEvent/GlobalGroupAndUnitNames.h: same here
    	* tag as: TagEvent-00-05-19

--- PhysicsAnalysis/EventTag/TagEvent/TagEvent/GlobalTriggerAttributeNames.h
+++ PhysicsAnalysis/EventTag/TagEvent/TagEvent/GlobalTriggerAttributeNames.h
@@ -15,16 +15,15 @@
 
 /** list of ALL the Trigger status attributes */
 namespace Trg { 
-   static const int Ntrg = 5;      // This is the number of Trigger TAG attributes 
-   static const int NumTrig = 8;   // This is the max number of Trigger words in the TAG per some attributes
-   enum { LVL1=0, LVL2=1, EF=2, Stream=3, BunchGroup=4 }; 
+   static const int Ntrg = 4;      // This is the number of Trigger TAG attributes 
+   static const int NumTrig = 16;   // This is the max number of Trigger words in the TAG per some attributes
+   enum { LVL1=0, HLT=1, Stream=2, BunchGroup=3 }; 
 }
 
 /** This is the list of the Trigger TAG attributes */
 static const std::string TriggerAttributeNames[Trg::Ntrg] = {
                                                        "L1PassedTrigMask",
-                                                       "L2PassedTrigMask",
-                                                       "EFPassedTrigMask",
+                                                       "HLTPassedTrigMask",
                                                        "SFOStreams",
                                                        "BunchGroup"
                                                    };
@@ -34,15 +33,13 @@ static const std::string TriggerAttributeUnitNames[Trg::Ntrg] = {
                                                         UnitNames[Unt::Bitmask],
                                                         UnitNames[Unt::Bitmask],
                                                         UnitNames[Unt::Bitmask],
-                                                        UnitNames[Unt::Bitmask],
                                                         UnitNames[Unt::Number]
                                                    };
 
 /** This is the list of the Trigger TAG attributes group names*/
 static const std::string TriggerAttributeGroupNames[Trg::Ntrg] = {
                                                        GroupNames[Grp::TriggerL1],
-                                                       GroupNames[Grp::TriggerL2],
-                                                       GroupNames[Grp::TriggerEF],
+                                                       GroupNames[Grp::TriggerHLT],
                                                        GroupNames[Grp::TriggerStream],
                                                        GroupNames[Grp::TriggerStream]
                                                    };
