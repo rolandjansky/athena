acd759ae891148f65f9c955eba37ab13a8cc78b2
Niels Van Eldik <niels.van.eldik@cern.ch>
2015-07-24 06:02:45 +0200

    expose truth matching (MuonTruthTools-01-02-01)
    	* expose truth matching
    	* MuonTruthTools-01-02-01
    
    31-03-2015 Edward.Moyse@cern.ch
    	* Add sector (and commented out R/Z - need to work out how to do this, and if it's worth the effort)
    	* MuonTruthTools-01-02-00
    
    25-02-2015 Edward.Moyse@cern.ch
    	* Added the ability to write out a debug ntuple
    	* MuonTruthTools-01-01-01
    
    19-02-2015 Edward.Moyse@cern.ch
    	* Improved output and made it less NSW-centric.
    	* MuonTruthTools-01-01-00

--- MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonTruthTools/src/MuonTruthSummaryTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonTruthTools/src/MuonTruthSummaryTool.h
@@ -22,6 +22,8 @@ namespace Trk {
 }
 
 class IIncidentSvc;
+class ITHistSvc;
+class TTree;
 
 namespace Muon {
   class MuonSegment;
@@ -42,6 +44,9 @@ namespace Muon {
     /** init truth */
     void init();
 
+    /** get the associated barcode for the identifier, return -1 if the channel was not hit by a muon */
+    int getBarcode( const Identifier& id );
+
     /** add identifier */
     void add( const Identifier& id, int level );
 
@@ -50,9 +55,10 @@ namespace Muon {
 
     /** add track */
     void add( const Trk::Track& track, int level );
+    
 
     /** print summary */
-    std::string printSummary() const;
+    std::string printSummary() ;
 
     void handle(const Incident& inc);
 
@@ -61,7 +67,7 @@ namespace Muon {
     /** add measurements */
     void add( const std::vector<const Trk::MeasurementBase*>& measurements, int level );
     void getTruth(std::string name );
-    std::string printSummary( const std::set<Identifier>& truth, const std::set<Identifier>& found ) const;
+    std::string printSummary( const std::set<Identifier>& truth, const std::set<Identifier>& found );
 
     ToolHandle<MuonIdHelperTool>                m_idHelper;
     ToolHandle<MuonEDMHelperTool>               m_helper;
@@ -76,8 +82,32 @@ namespace Muon {
     std::string m_MM_TruthName;
     std::string m_STGC_TruthName;
 
-    std::set<Identifier> m_truthHits;
-    std::map<int,std::set<Identifier> > m_truthDataPerLevel;
+    std::map<Identifier,int>                    m_truthHits; // map containing truth hits associated with muons, stores barcode as second element
+    std::map<int,std::set<Identifier> >         m_truthDataPerLevel;
+    mutable std::map<int,unsigned int >         m_lossesPerLevel; // We can get rid of the mutable, once printSummary() isn't const.
+    mutable unsigned int                        m_truthHitsTotal;
+    
+    TTree *                                     m_tree;
+    ITHistSvc *                                 m_thistSvc;
+    bool                                        m_writeTree; // Set to true in order to write out ntuple
+    std::string                                 m_treeName;
+    std::string                                 m_histStream;
+    unsigned int                                m_level;
+    
+    std::vector< unsigned int           >       m_numChambers;
+    std::vector< std::vector<uint8_t>*  >       m_numMissedHits; 
+    std::vector< std::vector<uint8_t>*  >       m_missedHitTechnologyIndex; // [0..5] maps to "MDT", "CSC", "RPC", "TGC", "STGC", "MM"
+    std::vector< std::vector<int>    *  >       m_missedHitStationPhi;      // increases with eta (0 at eta=0)
+    std::vector< std::vector<int>    *  >       m_missedHitStationSector;      // increases with eta (0 at eta=0)
+    std::vector< std::vector<int>    *  >       m_missedHitStationEta;      // increases with phi
+    // std::vector< std::vector<float>    *  >     m_missedHitR; // Can't do these without re-writing code a bit to have link to Meas. Not sure it's worth it.
+    // std::vector< std::vector<float>    *  >     m_missedHitZ;   
+    // std::vector< std::vector<std::string>* >         m_missedHitStationName;     // "BIL", "EMS", "T1F", etc.
+    std::vector< std::vector<int>* >            m_missedHitStationNameIndex;   // BI=0 , BM=1, BO=2, BE=3,
+      
+    void fillChamberVariables(const Identifier& chamberId, const unsigned int numMissedHits);
+    void clearChamberVariables(const unsigned int level);
+    void initChamberVariables(const unsigned int levels);
   };
   
 } // end of name space
