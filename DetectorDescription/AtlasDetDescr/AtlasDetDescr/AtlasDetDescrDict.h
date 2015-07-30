cddf0b22a9e6b3f98074f4209251d026ba735902
Marcin Nowak <Marcin.Nowak@cern.ch>
2015-07-30 11:33:30 +0200

    added AtlasDetDescr namespace autoloading trigger for ROOT6 (AtlasDetDescr-00-08-07)
    	* AtlasDetDescr\AtlasDetDescrDict.h: added AtlasDetDescr namespace autoloading trigger for ROOT6
    	* tagging AtlasDetDescr-00-08-07
    
    2015-07-13  Elmar Ritsch  <elmar.ritsch@cern.ch>
    
    	* tagging AtlasDetDescr-00-08-06
    	* AtlasDetDescr/AtlasDetDescrDict.h, AtlasDetDescr/selection.xml, cmt/requirements:
    	adding Reflex dictionary for AtlasDetDescr::AtlasRegion enum

---
+++ DetectorDescription/AtlasDetDescr/AtlasDetDescr/AtlasDetDescrDict.h
@@ -0,0 +1,19 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// AtlasDetDescrDict.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef ATLASDETDESCR_ATLASDETDESCRDICT_H
+#define ATLASDETDESCR_ATLASDETDESCRDICT_H 1
+
+#include "AtlasDetDescr/AtlasRegion.h"
+
+namespace AtlasDetDescr {
+   // Athena namespace autoloading trigger for ROOT6
+   struct ROOT6_NamespaceAutoloadHook{};
+}
+
+#endif // ATLASDETDESCR_ATLASDETDESCRDICT_H
