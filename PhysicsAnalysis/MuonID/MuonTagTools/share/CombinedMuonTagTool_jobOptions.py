4a4d03c3f059ca07d43e4599e85da357a9836925
Markus Elsing <markuse@cern.ch>
2015-07-31 09:41:40 +0200

    Tagging as MuonTagTools-00-08-25 (MuonTagTools-00-08-25)

--- PhysicsAnalysis/MuonID/MuonTagTools/share/CombinedMuonTagTool_jobOptions.py
+++
@@ -1,9 +0,0 @@
-#include.block ("MuonTagTools/CombinedMuonTagTool_jobOptions.py")
-
-########### Combined Muon - MOORE/Muonboy analysis tag options ################
-
-#from MuonTagTools.MuonTagToolsConf import CombinedMuonTagTool as ConfiguredCombinedMuonTagTool
-#CombinedMuonTagTool=ConfiguredCombinedMuonTagTool(
-#    MuonContainerNames=[ "Muons", "CaloMuonCollection" ],
-#    MuonPtCut=2.0*GeV, AuthorBased=False)
-#ToolSvc += CombinedMuonTagTool
--- PhysicsAnalysis/MuonID/MuonTagTools/src/components/MuonTagTools_entries.cxx
+++ PhysicsAnalysis/MuonID/MuonTagTools/src/components/MuonTagTools_entries.cxx
@@ -1,13 +1,10 @@
-#include "MuonTagTools/CombinedMuonTagTool.h"
 #include "MuonTagTools/MuonTagTool.h"
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
  
-//DECLARE_TOOL_FACTORY( CombinedMuonTagTool )
 DECLARE_TOOL_FACTORY( MuonTagTool )
  
 DECLARE_FACTORY_ENTRIES( MuonTagTools ) {
-  // DECLARE_TOOL( CombinedMuonTagTool )
   DECLARE_TOOL( MuonTagTool )
 }
 
