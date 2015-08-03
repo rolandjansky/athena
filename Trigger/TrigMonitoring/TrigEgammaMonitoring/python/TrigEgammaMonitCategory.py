834eecab2b3141d576a91445c1ce6aca55cd7b9c
Avishek Chatterjee <avishek@cern.ch>
2015-08-03 15:10:03 +0200

    Add monitoring triggers for 25 ns. (TrigEgammaMonitoring-00-01-51)

--- Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitCategory.py
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitCategory.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-primary_single_ele = ['e17_lhloose_L1EM15','e24_lhmedium_L1EM18VH','e24_lhtight_L1EM20VH']
-primary_single_ele_cutbased = ['e17_loose_L1EM15','e24_medium_L1EM18VH','e24_tight_L1EM20VH'] 
+primary_single_ele = ['e17_lhloose_L1EM15','e24_lhmedium_L1EM18VH','e24_lhmedium_L1EM20VHI','e24_lhtight_L1EM20VH']
+primary_single_ele_cutbased = ['e17_loose_L1EM15','e24_medium_L1EM18VH','e24_medium_L1EM20VHI','e24_tight_L1EM20VH'] 
 primary_double_ele = ['2e12_lhloose_L12EM10VH','2e15_lhloose_L12EM13VH','2e17_lhloose']
 primary_double_ele_cutbased = ['2e12_loose_L12EM10VH','2e15_loose_L12EM13VH','2e17_loose']
 monitoring_ele_idperf = ['e5_lhloose_idperf','e5_lhtight_idperf','e10_lhtight_idperf','e24_lhmedium_idperf_L1EM20VH']
--- Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
@@ -13,7 +13,7 @@ def HLTEgammaMonitoringTool():
   HLTEgammaNavMon = HLTEgammaNavMonTool(name = 'HLTEgammaNavMon', histoPathBase = "/Trigger/HLT")
   HLTEgammaNavMon.signatures = hltmonList.monitoring_egamma
   HLTEgammaNavMon.categories = ['primary_single_ele' , 'primary_single_ele_cutbased' , 'primary_double_ele' , 'primary_double_ele_cutbased' , 'monitoring_ele_idperf' , 'monitoring_ele_idperf_cutbased' , 'monitoring_Zee' , 'monitoring_Jpsiee' , 'primary_single_pho' , 'primary_double_pho', 'primary_single_ele_iso', 'primary_single_ele_cutbased_iso']
-  HLTEgammaNavMon.sigsPerCategory = [3, 3, 3, 3, 4, 4, 2, 2, 4, 3, 5, 5]
+  HLTEgammaNavMon.sigsPerCategory = [4, 4, 3, 3, 4, 4, 2, 2, 4, 3, 5, 5]
   #HLTEgammaNavMon.signatures = ['e5_NoCut','e5_medium1','e10_loose','e20_loose_IdScan']
   #HLTEgammaNavMon.signatures +=['e5_NoCut_cosmic','e10_loose_cosmic','e10_medium_cosmic']
   #HLTEgammaNavMon.doExtrapol=False
@@ -27,7 +27,7 @@ def HLTEgammaMonitoringTool():
   #HLTEgammaNavSigTEMon.signatures = ['e28_tight_iloose']
   HLTEgammaNavSigTEMon.signatures = hltmonList.monitoring_egamma
   HLTEgammaNavSigTEMon.categories = ['primary_single_ele' , 'primary_single_ele_cutbased' , 'primary_double_ele' , 'primary_double_ele_cutbased' , 'monitoring_ele_idperf' , 'monitoring_ele_idperf_cutbased' , 'monitoring_Zee' , 'monitoring_Jpsiee' , 'primary_single_pho' , 'primary_double_pho', 'primary_single_ele_iso', 'primary_single_ele_cutbased_iso']
-  HLTEgammaNavSigTEMon.sigsPerCategory = [3, 3, 3, 3, 4, 4, 2, 2, 4, 3, 5, 5]
+  HLTEgammaNavSigTEMon.sigsPerCategory = [4, 4, 3, 3, 4, 4, 2, 2, 4, 3, 5, 5]
   #HLTEgammaNavSigTEMon.signatures +=['e5_loose1','e24_medium_iloose','e28_tight_iloose','e28_tight_iloose_L2StarA']
   #HLTEgammaNavSigTEMon.signatures += ['e24vh_medium1']
   #HLTEgammaNavSigTEMon.signatures +=['e24vhi_medium1']
@@ -76,36 +76,48 @@ def HLTEgammaMonitoringDumpTool():
 def TrigEgammaMonitoringTool():
   from AthenaCommon.AppMgr import ToolSvc
   from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaEmulationTool
-  from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaNavAnalysisTool,TrigEgammaNavTPAnalysisTool
+  from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaNavAnalysisTool,TrigEgammaNavTPAnalysisTool,TrigEgammaNavTPJpsieeAnalysisTool
   from TrigHLTMonitoring.HLTMonTriggerList import hltmonList
-  import copy
 
-  probelist = copy.copy(hltmonList.monitoring_egamma)
-  probelist += ['e24_tight_iloose_HLTCalo_L1EM20VH', 'e24_lhtight_iloose_L2EFCalo_L1EM20VH', 'e24_lhmedium_cutd0dphideta_iloose_L1EM20VH', 'e24_lhmedium_nod0_iloose_L1EM20VH', 'e24_lhmedium_nodeta_iloose_L1EM20VH', 'e24_lhmedium_nodphires_iloose_L1EM20VH']
-  categoryList = ['primary_single_ele' , 'primary_single_ele_cutbased' , 'primary_double_ele' , 'primary_double_ele_cutbased' , 'monitoring_ele_idperf' , 'monitoring_ele_idperf_cutbased' , 'monitoring_Zee' , 'monitoring_Jpsiee' , 'primary_single_pho' , 'primary_double_pho', 'primary_single_ele_iso', 'primary_single_ele_cutbased_iso']
-  sigsPerCategory = [3, 3, 3, 3, 4, 4, 2, 2, 4, 3, 5, 5]
+  probelist1 = hltmonList.primary_single_pho + hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased + hltmonList.monitoring_ele_idperf + hltmonList.monitoring_ele_idperf_cutbased
+  probelist2 = hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased + hltmonList.monitoring_ele_idperf + hltmonList.monitoring_ele_idperf_cutbased + hltmonList.monitoring_Zee + hltmonList.monitoring_Jpsiee
+  probelist2 += ['e24_tight_iloose_HLTCalo_L1EM20VH', 'e24_lhtight_iloose_L2EFCalo_L1EM20VH', 'e24_lhmedium_cutd0dphideta_iloose_L1EM20VH', 'e24_lhmedium_nod0_iloose_L1EM20VH', 'e24_lhmedium_nodeta_iloose_L1EM20VH', 'e24_lhmedium_nodphires_iloose_L1EM20VH']
+  probelistJPsi = ['e5_loose','e5_lhloose','e5_vloose','e5_lhvloose']
 
   basePath = '/HLT/Egamma/'
-  tagItems = hltmonList.monitoring_egamma[0:sigsPerCategory[0]+sigsPerCategory[1]]
+  tagItems = hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased
+  tagItemsJPsi = ['e5_tight_e4_etcut','e9_tight_e4_etcut','e14_tight_e4_etcut','e9_etcut_e5_tight','e14_etcut_e5_tight','e5_tight_e4_etcut_Jpsiee','e9_tight_e4_etcut_Jpsiee','e14_tight_e4_etcut_Jpsiee','e9_etcut_e5_tight_Jpsiee','e14_etcut_e5_tight_Jpsiee']
 
   Analysis = TrigEgammaNavAnalysisTool(name='NavAnalysis',
                                        DirectoryPath=basePath+'Analysis',
-                                       TriggerList=probelist,
+                                       TriggerList=probelist1,
                                        File="",
                                        OutputLevel=0)
   TPAnalysis = TrigEgammaNavTPAnalysisTool(name='NavTPAnalysis',
                                            DirectoryPath=basePath+'TPAnalysis',
-                                           TriggerList=probelist,
+                                           TriggerList=probelist2,
                                            File="",
                                            TagTriggerList=tagItems,
                                            OutputLevel=0)
-  Emulation = TrigEgammaEmulationTool("Emulation",TriggerList=probelist)
+  JpsiTPAnalysis = TrigEgammaNavTPJpsieeAnalysisTool(name='NavTPJpsieeAnalysis',
+                                                     DirectoryPath=basePath+'TPJpsieeAnalysis',
+                                                     TriggerList=probelistJPsi,
+                                                     File="",
+                                                     TagTriggerList=tagItemsJPsi,
+                                                     RemoveCrack=True,
+                                                     ZeeLowerMass=2,
+                                                     ZeeUpperMass=5,
+                                                     OfflineTagMinEt=5,
+                                                     OfflineProbeMinEt=5,
+                                                     OutputLevel=0,
+                                                     DetailedHistograms=False,
+                                                     doJpsiee=True)
   from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConf import TrigEgammaMonTool
   TrigEgammaMon = TrigEgammaMonTool( name = "TrigEgammaMon", 
                                      histoPathBase=basePath,
                                      Tools=["TrigEgammaNavAnalysisTool/NavAnalysis",
                                             "TrigEgammaNavTPAnalysisTool/NavTPAnalysis",
-                                            "TrigEgammaEmulationTool/Emulation"])
+                                            "TrigEgammaNavTPAnalysisTool/NavTPJpsieeAnalysis"])
   ToolSvc += TrigEgammaMon;
   list = ['TrigEgammaMonTool/TrigEgammaMon'];
   return list
