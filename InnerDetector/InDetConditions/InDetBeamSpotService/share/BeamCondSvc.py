c6089fb2ab62ec285589437fe1c5727c8326b2c0
Simone Pagan Griso <simone.pagan.griso@cern.ch>
2015-08-06 21:31:36 +0200

    'Using online beamspot position in case RecExConfig.doExpressProcessing is set' (InDetBeamSpotService-01-00-03)
    Former-commit-id: e70f40318ba36ac756d69d886ce7e634f0e34256

--- InnerDetector/InDetConditions/InDetBeamSpotService/share/BeamCondSvc.py
+++ InnerDetector/InDetConditions/InDetBeamSpotService/share/BeamCondSvc.py
@@ -2,14 +2,9 @@
 
 include.block("InDetBeamSpotService/BeamCondSvc.py")
 from IOVDbSvc.CondDB import conddb
-try:
-    from RecExConfig.RecFlags import rec
-    # If express processing, point beam spot to online folder results
-    if(rec.doExpressProcessing()):
-        conddb.addFolder('INDET_ONL','/Indet/Onl/Beampos <key>/Indet/Beampos</key>')
-    else:
-        conddb.addFolderSplitOnline("INDET","/Indet/Onl/Beampos","/Indet/Beampos")
-except ImportError:
-    # Protection for AthSimulationBase release which does not contain RecExConfig
+from RecExConfig.RecFlags import rec
+# If express processing, point beam spot to online folder results
+if(rec.doExpressProcessing()):
+    conddb.addFolder('INDET_ONL','/Indet/Onl/Beampos <key>/Indet/Beampos</key>')
+else:
     conddb.addFolderSplitOnline("INDET","/Indet/Onl/Beampos","/Indet/Beampos")
-
