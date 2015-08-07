69482a36b35b4b96510a9d10bcaa1d60ec2cb8b8
Craig Wiglesworth <Craig.Wiglesworth@cern.ch>
2015-08-07 17:45:11 +0200

    fixed medium priority issues from coverity scan (TrigMultiVarHypo-06-00-20)
    Former-commit-id: 3295231af5e3931f7b0b842a56835e726a14ea49

--- Trigger/TrigHypothesis/TrigMultiVarHypo/python/__init__.py
+++ Trigger/TrigHypothesis/TrigMultiVarHypo/python/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 # File: TrigMultiVarHypo/__init__.py
-# Author: Joao Victor da Fonseca Pinto
+# Author: Rodrigo Coura Torres
 
 __version__ = '1.0.0'
-__author__  = 'jodafons@cern.ch'
-#__all__ = [ 'TrigL2CaloRingerHypoConfig', 'TrigL2CaloRingerHypoMonitoring' ]
+__author__  = 'Rodrigo.Torres@cern.ch'
+__all__ = [ 'TrigMultiVarHypoConfig' ]
--- Trigger/TrigHypothesis/TrigMultiVarHypo/src/components/TrigMultiVarHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigMultiVarHypo/src/components/TrigMultiVarHypo_entries.cxx
@@ -1,19 +1,28 @@
+/**
+ * @file TrigMultiVarHypo_entries.cxx
+ * @author <a href="mailto:Andre.dos.Anjos@cern.ch">Andre ANJOS</a> 
+ * $Author: dquarrie $
+ * $Revision: 1.2 $
+ * $Date: 2007-10-03 07:15:32 $
+ *
+ * Declares my gaudi factory entries
+ */
 
+#include "TrigMultiVarHypo/TrigRingerNeuralHypo.h"
+#include "TrigMultiVarHypo/TrigRingerNeuralFex.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TrigMultiVarHypo/TrigL2CaloRingerFex.h"
-#include "TrigMultiVarHypo/TrigL2CaloRingerHypo.h"
 
 /**
  * Declares the current algorithms available
  */
-DECLARE_ALGORITHM_FACTORY( TrigL2CaloRingerFex  )
-DECLARE_ALGORITHM_FACTORY( TrigL2CaloRingerHypo )
+DECLARE_ALGORITHM_FACTORY( TrigRingerNeuralFex )
+DECLARE_ALGORITHM_FACTORY( TrigRingerNeuralHypo )
 
 /**
  * Declares the entries that are present in the factory at libTrigMultiVarHypo
  */
 DECLARE_FACTORY_ENTRIES( TrigMultiVarHypo ) {
-  DECLARE_ALGORITHM( TrigL2CaloRingerFex  );
-  DECLARE_ALGORITHM( TrigL2CaloRingerHypo );
+  DECLARE_ALGORITHM( TrigRingerNeuralFex );
+  DECLARE_ALGORITHM( TrigRingerNeuralHypo );
 }
 
