714262b713a6bad50793812728ec6a5436c4dd48
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2015-08-07 13:48:14 +0200

    improving robustness in PhotonTagTool_jobOptions.py for determining whether the dataset is fullsim or not (ATLASSIM-2148) (ElectronPhotonTagTools-00-03-45)
    Former-commit-id: 81a90b55121b6041ff2bf64f62a46aac06fe28af

--- PhysicsAnalysis/ElectronPhotonID/ElectronPhotonTagTools/share/PhotonTagTool_jobOptions.py
+++ PhysicsAnalysis/ElectronPhotonID/ElectronPhotonTagTools/share/PhotonTagTool_jobOptions.py
@@ -9,10 +9,8 @@ af = AthFile.fopen(svcMgr.EventSelector.InputCollections[0]) # opens the first f
 af.fileinfos # this is a dict of dicts, take a look at what's available! Below are some examples:
 
 isMC = 'IS_SIMULATION' in af.fileinfos['evt_type']
-
 beam_energy    = af.fileinfos['beam_energy']
 conditions_tag = af.fileinfos['conditions_tag'] #useful for figuring out which mc production this is
-
 isFullsim = False
 if isMC: 
     simulationFlavour = af.fileinfos['metadata']['/Simulation/Parameters']['SimulationFlavour']
@@ -33,30 +31,16 @@ if isFullsim:
     print DF_ElectronPhotonShowerShapeFudgeTool
 
 ############ Photon selecton options #################
-
-IsolationCorrectionTool          = CfgMgr.CP__IsolationCorrectionTool( "ElectronIsolationCorrectionTool" )
-IsolationCorrectionTool.IsMC     = isMC
-ToolSvc += IsolationCorrectionTool
-
+from IsolationSelection.IsolationSelectionConf import CP__IsolationSelectionTool
 cone20IsoTool = CfgMgr.CP__IsolationSelectionTool( "PhotonCone20IsolationSelectionTool" )
-cone20IsoTool.PhotonWP         = "Cone20"
-ToolSvc                       += cone20IsoTool
-cone40IsoTool                  = CfgMgr.CP__IsolationSelectionTool( "PhotonCone40IsolationSelectionTool" )
-cone40IsoTool.PhotonWP         = "Cone40"
-ToolSvc                       += cone40IsoTool
-cone40CaloOnlyIsoTool          = CfgMgr.CP__IsolationSelectionTool( "PhotonCone40CaloOnlyIsolationSelectionTool" )
+cone20IsoTool.PhotonWP = "Cone20"
+ToolSvc += cone20IsoTool
+cone40IsoTool = CfgMgr.CP__IsolationSelectionTool( "PhotonCone40IsolationSelectionTool" )
+cone40IsoTool.PhotonWP = "Cone40"
+ToolSvc += cone40IsoTool
+cone40CaloOnlyIsoTool = CfgMgr.CP__IsolationSelectionTool( "PhotonCone40CaloOnlyIsolationSelectionTool" )
 cone40CaloOnlyIsoTool.PhotonWP = "Cone40CaloOnly"
-ToolSvc                       += cone40CaloOnlyIsoTool
-
-FixedCutTightCaloOnlyIsoTool          = CfgMgr.CP__IsolationSelectionTool( "PhotonFixedCutTightCaloOnlyIsolationSelectionTool" )
-FixedCutTightCaloOnlyIsoTool.PhotonWP = "FixedCutTightCaloOnly"
-ToolSvc                              += FixedCutTightCaloOnlyIsoTool
-FixedCutLooseIsoTool                  = CfgMgr.CP__IsolationSelectionTool( "PhotonFixedCutLooseIsolationSelectionTool" )
-FixedCutLooseIsoTool.PhotonWP         = "FixedCutLoose"
-ToolSvc                              += FixedCutLooseIsoTool
-FixedCutTightIsoTool                  = CfgMgr.CP__IsolationSelectionTool( "PhotonFixedCutTightIsolationSelectionTool" )
-FixedCutTightIsoTool.PhotonWP         = "FixedCutTight"
-ToolSvc                              += FixedCutTightIsoTool
+ToolSvc += cone40CaloOnlyIsoTool
 
 ########### Photon cut based selection options ################
 from ROOT import egammaPID
@@ -70,33 +54,19 @@ ToolSvc += PhotonIsEMSelectorLoose
 PhotonIsEMSelectorTight = ConfiguredAsgPhotonIsEMSelector("PhotonIsEMSelectorTight", egammaPID.PhotonIDTight)
 ToolSvc += PhotonIsEMSelectorTight
 
-# EGammaAmbiTool
-from ElectronPhotonSelectorTools.ElectronPhotonSelectorToolsConf import EGammaAmbiguityTool
-EGammaAmbiTool = EGammaAmbiguityTool("EGammaAmbiguityTool")
-ToolSvc += EGammaAmbiTool
-
 
 from ElectronPhotonTagTools.ElectronPhotonTagToolsConf import PhotonTagTool as ConfiguredPhotonTagTool
 PhotonTagTool = ConfiguredPhotonTagTool(
-    Container                    = "Photons",
-    PhotonMETContainerName       = "Photons_TAG_skim", 
-    EtCut                        = 10.0*GeV,
-    EtconeIsoCutValues           = [ 0.15, 0.30, 3.0*GeV, 5.0*GeV ],
-    PtconeIsoCutValues           = [ 0.15, 0.30, 3.0*GeV, 5.0*GeV ],
-    isFullsim                    = isFullsim,
-    PhotonShowerFudgeTool        = DF_ElectronPhotonShowerShapeFudgeTool,
-    LooseSelector                = PhotonIsEMSelectorLoose,
-    TightSelector                = PhotonIsEMSelectorTight,
-    EGammaAmbiTool               = EGammaAmbiTool,
-    IsolationCorrectionTool      = IsolationCorrectionTool,
-    cone20IsoTool                = cone20IsoTool,
-    cone40IsoTool                = cone40IsoTool,
-    cone40CaloOnlyIsoTool        = cone40CaloOnlyIsoTool,
-    FixedCutTightCaloOnlyIsoTool = FixedCutTightCaloOnlyIsoTool,
-    FixedCutLooseIsoTool         = FixedCutLooseIsoTool,
-    FixedCutTightIsoTool         = FixedCutTightIsoTool
+    Container            = "Photons",
+    EtCut                = 10.0*GeV,
+    EtconeIsoCutValues   = [ 0.15, 0.30, 3.0*GeV, 5.0*GeV ],
+    PtconeIsoCutValues   = [ 0.15, 0.30, 3.0*GeV, 5.0*GeV ],
+    isFullsim            = isFullsim,
+    PhotonShowerFudgeTool= DF_ElectronPhotonShowerShapeFudgeTool,
+    LooseSelector        = PhotonIsEMSelectorLoose,
+    TightSelector        = PhotonIsEMSelectorTight,
+    cone20IsoTool        = cone20IsoTool,
+    cone40IsoTool        = cone40IsoTool,
+    cone40CaloOnlyIsoTool= cone40CaloOnlyIsoTool
     )
-
-PhotonTagTool.EgammaCalibrationAndSmearingTool.ESModel =  "es2015PRE"
-
 ToolSvc += PhotonTagTool
