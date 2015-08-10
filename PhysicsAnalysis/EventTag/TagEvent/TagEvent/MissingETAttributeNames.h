0185fd3c409dc45afbe87dd021ffada9b0d58e60
Gabriele Sabato <gabriele.sabato@cern.ch>
2015-08-10 15:45:54 +0200

    Introducing new MET term (TagEvent-00-05-20)
    	* Introducing new MET term
    	* tag as TagEvent-00-05-20

--- PhysicsAnalysis/EventTag/TagEvent/TagEvent/MissingETAttributeNames.h
+++ PhysicsAnalysis/EventTag/TagEvent/TagEvent/MissingETAttributeNames.h
@@ -5,18 +5,15 @@
 #ifndef MISSINGETATTRIBUTENAMES_H
 #define MISSINGETATTRIBUTENAMES_H
 
-/****************************************
- Author - Ketevi A. Assamagan
- Feb 2006
-****************************************/
 #include "GlobalGroupAndUnitNames.h"
 #include <string>
 
 /** list of ALL MissingEt attributes and their types */
 namespace EtMiss { 
-  static const int NetAttr=9;              // This number of MissingEt TAG Attributes
+  static const int NetAttr=19;              // This number of MissingEt TAG Attributes
   static const int NumMissingEt=1;         // This the max number of MissingEt objects in the TAG 
-  enum { MET_RefFinalX=0, MET_RefFinalY=1, SumET=2, MET_CellOutX=3, MET_CellOutY=4, MET_SoftJetsX=5, MET_SoftJetsY=6, MET_RefTauX=7, MET_RefTauY=8}; 
+  enum { MET_RefFinalX=0, MET_RefFinalY=1, SumET=2, MET_SoftClusX=3, MET_SoftClusY=4, MET_RefJetX=5, MET_RefJetY=6, MET_RefTauX=7, MET_RefTauY=8, MET_MuonsX=9, MET_MuonsY=10, MET_RefEleX=11, MET_RefEleY=12, MET_RefGammaX=13, MET_RefGammaY=14, MET_PVSoftTrkX=15, MET_PVSoftTrkY=16, MET_FinalTrkX=17,  MET_FinalTrkY=18
+}; 
 }
 
 /** This is the list of the Missing ET TAG attributes */
@@ -24,12 +21,22 @@ static const std::string MissingEtAttributeNames[EtMiss::NetAttr] = {
                                                        "MET_RefFinalX",
                                                        "MET_RefFinalY",
                                                        "SumET",
-                                                       "MET_CellOutX",
-                                                       "MET_CellOutY",
-						       "MET_SoftJetsX",
-						       "MET_SoftJetsY",
+                                                       "MET_SoftClusX",
+                                                       "MET_SoftClusY",
+						       "MET_RefJetX",
+						       "MET_RefJetY",
 						       "MET_RefTauX",
 						       "MET_RefTauY",
+						       "MET_MuonsX",
+						       "MET_MuonsY",
+						       "MET_RefEleX",
+						       "MET_RefEleY",
+						       "MET_RefGammaX",
+						       "MET_RefGammaY",
+						       "MET_PVSoftTrkX",
+						       "MET_PVSoftTrkY",
+						       "MET_FinalTrkX",
+						       "MET_FinalTrkY"
                                                      };
 
 /** This is the list of the Missing ET TAG attributes unit names*/
@@ -40,6 +47,16 @@ static const std::string MissingEtAttributeUnitNames[EtMiss::NetAttr] = {
                                                        UnitNames[Unt::Mev],
                                                        UnitNames[Unt::Mev],
                                                        UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+						       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+                                                       UnitNames[Unt::Mev],
+						       UnitNames[Unt::Mev],
                                                        UnitNames[Unt::Mev],
                                                        UnitNames[Unt::Mev],
                                                        UnitNames[Unt::Mev]
@@ -54,6 +71,16 @@ static const std::string MissingEtAttributeGroupNames[EtMiss::NetAttr] = {
                                                       GroupNames[Grp::PhysicsJetmet],
                                                       GroupNames[Grp::PhysicsJetmet],
                                                       GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+						      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+                                                      GroupNames[Grp::PhysicsJetmet],
+						      GroupNames[Grp::PhysicsJetmet],
                                                       GroupNames[Grp::PhysicsJetmet],
                                                       GroupNames[Grp::PhysicsJetmet],
                                                       GroupNames[Grp::PhysicsJetmet]
