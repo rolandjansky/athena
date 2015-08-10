a2523ce8fdcb1eefb461dc7a53fc12b98c232ebc
Rolf Seuster <Rolf.Seuster@cern.ch>
2015-08-10 09:03:28 +0200

    reverting changes from -04 (AtlasReflex-00-03-06)
    Former-commit-id: c71757b6cff5164b40f658456916ec5c1ea38e42

---
+++ External/AtlasReflex/cmt/fragments/DataLinkDict.h
@@ -0,0 +1,17 @@
+
+#ifndef DATALINKDICT_${namespace}
+#define DATALINKDICT_${namespace}
+
+#include "AthLinks/DataLink.h"
+
+namespace DictTools {
+
+struct DataLinkDict${namespace} {
+
+  DataLink< ${cont} > m_dlinkBase;
+
+};
+
+}// end DictTools namespace
+
+#endif // DATALINKDICT_${namespace}
