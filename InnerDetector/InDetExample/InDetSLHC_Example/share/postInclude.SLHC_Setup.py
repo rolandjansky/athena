666c4bdb162fd0d38deff124c334c2ff7c91328d
Susumu Oda <Susumu.Oda@cern.ch>
2015-08-12 15:38:09 +0200

    ServiceMgr.PixelOfflineCalibSvc.HDCFromCOOL = False if RAWtoESD. (InDetSLHC_Example-00-01-25-03)
    	* share/postInclude.SLHC_Setup.py - fix
    	  https://its.cern.ch/jira/browse/ATLASRECTS-2321 by enabling
    	  ServiceMgr.PixelOfflineCalibSvc.HDCFromCOOL = False
    	  only for RAWtoESD step.
    	* Tagged as InDetSLHC_Example-00-01-25-03
    
    2015-08-10 Susumu Oda <Susumu.Oda@cern.ch>
    	* share/postInclude.SLHC_Setup.py - fix
    	  https://its.cern.ch/jira/browse/ATLASRECTS-2321 by adding
    	  ServiceMgr.PixelOfflineCalibSvc.HDCFromCOOL = False
    	* Tagged as InDetSLHC_Example-00-01-25-02

--- InnerDetector/InDetExample/InDetSLHC_Example/share/postInclude.SLHC_Setup.py
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/postInclude.SLHC_Setup.py
@@ -4,3 +4,7 @@
 # set the path variables consistently
 from InDetSLHC_Example.SLHC_Setup import SLHC_Setup
 SLHC_Setup = SLHC_Setup()
+
+# https://its.cern.ch/jira/browse/ATLASRECTS-2321
+if rec.OutputFileNameForRecoStep() == 'RAWtoESD':
+    ServiceMgr.PixelOfflineCalibSvc.HDCFromCOOL = False 
