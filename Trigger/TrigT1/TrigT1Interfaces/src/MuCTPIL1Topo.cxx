e8e05eec1dbd596019687c1ba11e58bf017ecd28
Tomoe Kishimoto <Tomoe.Kishimoto@cern.ch>
2015-08-25 11:28:44 +0200

    Fixed a compilation error (ATR-12113) (TrigT1Interfaces-01-05-24)
    2015-08-25  Tomoe Kishimoto  <Tomoe.Kishimoto@cern.ch>
    
    	* Fixed a compilation error (ATR-12113)
    	* Tag as TrigT1Interfaces-01-05-24
    
           * Tag as TrigT1Interfaces-01-05-23
    
    2015-07-15  Tomoe Kishimoto  <Tomoe.Kishimoto@cern.ch>
    
    	* Fixed a bug (ATR-11804)
    	* Tag as TrigT1Interfaces-01-05-22
    
    2015-07-08  Thorsten Wengler  <thorsten.wengler@cern.ch>
    
    	* Changes to run with multiple BCID slices for L1Topo in Muctpi
    	* Tag as TrigT1Interfaces-01-05-21
    
    2015-06-26  Thorsten Wengler  <thorsten.wengler@cern.ch>
    
    	* Modified interface classes MuCTPI->L1Topo
    ...
    (Long ChangeLog diff - truncated)

---
+++ Trigger/TrigT1/TrigT1Interfaces/src/MuCTPIL1Topo.cxx
@@ -0,0 +1,67 @@
+
+#include "TrigT1Interfaces/MuCTPIL1Topo.h"
+
+namespace LVL1 {
+
+  MuCTPIL1Topo::MuCTPIL1Topo( ):m_bcidOffset(0){
+
+  }
+  MuCTPIL1Topo::MuCTPIL1Topo(std::vector<MuCTPIL1TopoCandidate> candList):m_bcidOffset(0){
+    m_muonTopoCandidates = candList;
+  }
+
+  MuCTPIL1Topo::~MuCTPIL1Topo() {
+
+  }
+
+  MuCTPIL1Topo& MuCTPIL1Topo::operator=( const MuCTPIL1Topo& a ) {
+    
+    clearCandidates();
+    m_muonTopoCandidates = a.getCandidates();
+
+    return *this;      
+  }
+  
+  MuCTPIL1Topo& MuCTPIL1Topo::operator+=( const MuCTPIL1Topo& a) {
+
+    std::vector<MuCTPIL1TopoCandidate>::iterator it;
+    std::vector<MuCTPIL1TopoCandidate> a_candvect = a.getCandidates();
+    for(it = a_candvect.begin(); it != a_candvect.end(); ++it) {
+      addCandidate( *it );
+    } 
+    return *this;    
+  }
+
+  MuCTPIL1Topo MuCTPIL1Topo::operator+( const MuCTPIL1Topo& a ) {
+    
+    MuCTPIL1Topo result = *this;
+      result += a;
+      return result;
+  }
+  
+  
+  std::vector<MuCTPIL1TopoCandidate> MuCTPIL1Topo::getCandidates() const {
+     return m_muonTopoCandidates;
+   }
+
+  void MuCTPIL1Topo::setCandidates(std::vector<MuCTPIL1TopoCandidate> candList)  {
+     m_muonTopoCandidates = candList;
+   }
+  
+  void MuCTPIL1Topo::addCandidate(MuCTPIL1TopoCandidate candidate){
+    m_muonTopoCandidates.push_back(candidate);
+  }
+
+  void  MuCTPIL1Topo::clearCandidates(){
+    m_muonTopoCandidates.clear();
+  }
+
+  void MuCTPIL1Topo::print() const {
+    for (std::vector<MuCTPIL1TopoCandidate>::const_iterator it =  m_muonTopoCandidates.begin(); 
+	 it != m_muonTopoCandidates.end(); ++it) {
+      it->print();
+    }
+
+  }
+
+} // namespace LVL1
