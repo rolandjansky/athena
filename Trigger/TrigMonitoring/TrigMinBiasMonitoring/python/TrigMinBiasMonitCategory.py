330c8ed406ee9a0edee00f259e5706ef780f3864
Qipeng Hu <qipeng.hu@cern.ch>
2015-08-28 11:57:21 +0200

    'cleaning up of logically dead code reported by Coverity' (TrigMinBiasMonitoring-00-01-29)
    Former-commit-id: 097a6f8e8d64159bdc6bcc16ebb05d46121592b2

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitCategory.py
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitCategory.py
@@ -1,76 +1,58 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 #----NORMAL USER ACCESS-------
-
-triggers_pp = ["mb_sptrk",
-               #"mb_sptrk_noisesup",
-               #"mb_sptrk_costr",
-               "noalg_L1RD0_FILLED",
-               #"noalg_mb_L1RD0_EMPTY",
-               "noalg_mb_L1MBTS_2",
-               "mb_perf_L1MBTS_2",	
-               "mb_sp1800_hmtperf_L1TE5",
-               ]
-
-# PbPb triggers 2015
-triggers_hi = [ "mb_sp",
-                "mb_sptrk_ion_L1ZDC_A_C_VTE50",
-                "mb_perf_L1MBTS_1_1",	
-                "noalg_mb_L1TE50",
-                ]
+triggers = ["mb_sptrk",
+            "mb_sptrk_noisesup",
+            "mb_sptrk_costr",
+            "noalg_mb_L1MBTS_1",
+            "noalg_mb_L1MBTS_2",
+            "noalg_mb_L1MBTS_1_1",
+            "noalg_mb_L1TE10",
+            "noalg_mb_L1TE10.0ETA25",
+            #"noalg_mb_L1TE20",
+            #"noalg_mb_L1TE20.0ETA25",
+            "mb_perf_L1MBTS_2",	
+            "mb_mbts_L1MBTS_1",
+            "mb_mbts_L1MBTS_2",
+            "mb_mbts_L1MBTS_1_1",
+            #leading-track triggers
+            #"mb_sptrk_pt4_L1MBTS_1",
+            "mb_sptrk_pt4_L1MBTS_2",
+            #"mb_sptrk_pt4_L1MBTS_1_1",
+            #"mb_sptrk_pt6_L1MBTS_1",
+            "mb_sptrk_pt6_L1MBTS_2",
+            #"mb_sptrk_pt6_L1MBTS_1_1",
+            #"mb_sptrk_pt8_L1MBTS_1",
+            "mb_sptrk_pt8_L1MBTS_2",
+            #"mb_sptrk_pt8_L1MBTS_1_1",
+            #HMT triggers
+            "mb_sp600_trk45_hmt_L1MBTS_1_1",	
+            "mb_sp700_trk55_hmt_L1MBTS_1_1",
+            "mb_sp900_trk60_hmt_L1MBTS_1_1",
+            "mb_sp300_trk10_sumet50_hmt_L1MBTS_1_1",
+            "mb_sp600_trk10_sumet50_hmt_L1MBTS_1_1",
+            "mb_sp300_trk10_sumet60_hmt_L1MBTS_1_1",
+            "mb_sp600_trk10_sumet60_hmt_L1MBTS_1_1",
+            "mb_sp300_trk10_sumet70_hmt_L1MBTS_1_1",
+            "mb_sp600_trk10_sumet70_hmt_L1MBTS_1_1",
+            "mb_sp1400_hmtperf_L1TE10",	
+            "mb_sp1400_hmtperf_L1TE10.0ETA25",	
+            "mb_sp1400_hmtperf_L1TE20",	
+            "mb_sp1400_hmtperf_L1TE20.0ETA25",
+	    #ATR-11216
+	    "mb_sptrk_vetombts2in_L1ALFA_CEP",
+	    "mb_sptrk_vetombts2in_peb_L1ALFA_ANY",
+            ]
 		
-# pPb triggers 2016
-triggers_pPb = ["mb_sptrk",
-               #"mb_sptrk_noisesup",
-               #"mb_sptrk_costr", 
-               "noalg_L1RD0_FILLED",
-               #"noalg_mb_L1RD0_EMPTY",
-               "noalg_mb_L1MBTS_2",
-               "mb_perf_L1MBTS_2",
-               "mb_mbts_L1MBTS_2",
-               "noalg_mb_L1ZDC_A",
-               "noalg_mb_L1ZDC_C",
-               "mb_sp1800_hmtperf_L1TE5",
-               "noalg_mb_L1TE5",
-               "hi_upc_FgapAC_mb_sptrk_L1MBTS_2",
-               "hi_upc_FgapAC_mb_sptrk_exclusiveloose",
-               "mb_sptrk_exclusiveloose_L1ZDC",
-               ]
-
 #!----NORMAL USER ACCESS-------
 		
 #----ADVANCED USER ACCESS-----
 #   PLEASE DO NOT MODIFY EXISTING CODE UNLESS YOU ARE PERFECTLY SURE WHAT YOU ARE DOING 
 
 #forces uniqueness of items on the list
-triggersFiltered_pp = []
-for x in triggers_pp:
-    if x not in triggersFiltered_pp:
-        triggersFiltered_pp.append(x)
+triggersFiltered = []
+for x in triggers:
+    if x not in triggersFiltered:
+        triggersFiltered.append(x)
 
-triggersFiltered_hi = []
-for x in triggers_hi:
-    if x not in triggersFiltered_hi:
-        triggersFiltered_hi.append(x)
-
-triggersFiltered_pPb = []
-for x in triggers_pPb:
-    if x not in triggersFiltered_pPb:
-        triggersFiltered_pPb.append(x)
-
-from RecExConfig.RecFlags import rec
-from AthenaCommon.BeamFlags import jobproperties
-from AthenaCommon.Logging        import logging
-
-log = logging.getLogger( 'TrigMinBiasMonitoring' )
-
-if rec.doHeavyIon == True:
-    log.info('HI mode of min-bias trigger monitoring')
-    monitoring_minbias = triggersFiltered_hi     
-elif rec.doHIP == True:
-    log.info('doHI mode of min-bias trigger monitoring')
-    monitoring_minbias = triggersFiltered_pPb
-else:
-    log.info('pp mode of min-bias trigger monitoring')
-    monitoring_minbias = triggersFiltered_pp
-
+monitoring_minbias = triggersFiltered
