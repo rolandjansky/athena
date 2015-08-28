8134f974817f81b9e7e3bee336f9ab9107da3f8b
Patrick Karl Czodrowski <patrick.czodrowski@cern.ch>
2015-08-28 12:40:15 +0200

    share/PhysValTrigEgamma_jobOptions.py to use the new tool called TrigEgammaMonitoringTool (PhysValMonitoring-00-01-41)
    Former-commit-id: f2ace48a3f94df64ccc9aa9d1288575ab723a598

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValTau_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValTau_jobOptions.py
@@ -3,9 +3,6 @@ tool1 = PhysValTau()
 tool1.EnableLumi = False
 tool1.OutputLevel = WARNING
 tool1.DetailLevel = 10
-from RecExConfig.RecFlags import rec as recFlags
-if recFlags.doTruth():
-    tool1.isMC = True
 
 tool1.TauContainerName = "TauJets"
 tool1.TruthParticleContainerName = "TruthParticles"
@@ -15,4 +12,3 @@ ToolSvc += tool1
 
 monMan = CfgMgr.AthenaMonManager("PhysValMonManager")
 monMan.AthenaMonTools += [ tool1 ]
-
