9b49de76b3c259635490e43a82b7622936f2daab
Andrea Dell'Acqua <Andrea.Dellacqua@cern.ch>
2015-09-07 14:48:09 +0200

    see ChangeLog (MuonAGDDDescription-00-00-01)

---
+++ MuonSpectrometer/MuonDetDescr/MuonAGDDDescription/MuonAGDDDescription/sTGCDetectorHelper.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef sTGCDetectorHelper_H
+#define sTGCDetectorHelper_H
+
+#include <map>
+
+class sTGCDetectorDescription;
+class AGDDDetectorPositioner;
+
+typedef std::map<std::string,sTGCDetectorDescription*> sTGCMap;
+typedef std::map<std::string,sTGCDetectorDescription*>::const_iterator sTGCIterator;
+
+typedef std::pair<sTGCDetectorDescription*,AGDDDetectorPositioner*> AGDDPositionedDetector;
+
+class sTGCDetectorHelper {
+public:
+	sTGCDetectorHelper();
+	sTGCIterator sTGC_begin() {return sTGCList.begin();}
+	sTGCIterator sTGC_end()   {return sTGCList.end();}
+	
+	sTGCDetectorDescription* Get_sTGCDetector(char type,int ieta,int iphi,int layer=1,char side='A');
+	sTGCDetectorDescription* Get_sTGCDetectorType(std::string type);
+	AGDDPositionedDetector Get_sTGCPositionedDetector(char type,int ieta,int iphi,int layer=1,char side='A');
+	
+private:
+	sTGCMap sTGCList;
+
+};
+
+#endif
