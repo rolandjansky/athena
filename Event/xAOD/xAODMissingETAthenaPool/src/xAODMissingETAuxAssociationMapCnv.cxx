761b27bf2503ca48ba3b13e2f663c27cf2b577e0
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2015-09-08 22:07:19 +0200

    'Converter validated' (xAODMissingETAthenaPool-00-02-01)
    	* Revamped conversion -- bugs squashed
    	* Tagging xAODMissingETAthenaPool-00-02-01
    
    2015-09-03 TJ Khoo <khoo@cern.ch>
    	* Fixes for v1/v2 conversion
    	* Tagging xAODMissingETAthenaPool-00-02-00
    
    2015-08-29 TJ Khoo <khoo@cern.ch>
    	* Adding pool converter for MissingETAssociationMap, which was never actually in SVN...
    	* Schema evolution for MissingETAuxAssociationMap

---
+++ Event/xAOD/xAODMissingETAthenaPool/src/xAODMissingETAuxAssociationMapCnv.cxx
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: xAODMissingETAuxAssociationMapCnv.cxx 692925 2015-09-03 12:11:56Z khoo $
+
+// System include(s):
+#include <exception>
+
+// Local include(s):
+#include "xAODMissingETAuxAssociationMapCnv.h"
+#include "xAODMissingETAuxAssociationMapCnv_v1.h"
+
+xAODMissingETAuxAssociationMapCnv::xAODMissingETAuxAssociationMapCnv( ISvcLocator* svcLoc )
+   : xAODMissingETAuxAssociationMapCnvBase( svcLoc ) {
+
+}
+
+xAOD::MissingETAuxAssociationMap*
+xAODMissingETAuxAssociationMapCnv::
+createPersistent( xAOD::MissingETAuxAssociationMap* trans ) {
+
+   // Create a copy of the container:
+   xAOD::MissingETAuxAssociationMap* result =
+      new xAOD::MissingETAuxAssociationMap( *trans );
+
+   return result;
+}
+
+xAOD::MissingETAuxAssociationMap* xAODMissingETAuxAssociationMapCnv::createTransient() {
+
+   // The known ID(s) for this container:
+   static const pool::Guid v1_guid( "48EEF1CD-F937-445C-A09C-B978D152868E" );
+   static const pool::Guid v2_guid( "05184C74-A3CE-4C89-959E-D7E9F0A98D13" );
+
+   // Check which version of the container we're reading:
+   if( compareClassGuid( v2_guid ) ) {
+      // It's the latest version, read it directly:
+      return poolReadObject< xAOD::MissingETAuxAssociationMap >();
+   } else if( compareClassGuid( v1_guid ) ) {
+
+     // The v1 converter:
+     static xAODMissingETAuxAssociationMapCnv_v1 converter;
+
+     // Read in the v1 version:
+     std::unique_ptr< xAOD::MissingETAuxAssociationMap_v1 >
+       old( poolReadObject< xAOD::MissingETAuxAssociationMap_v1 >() );
+
+     // Return the converted object:
+     return converter.createTransient( old.get(), msg() );
+   }
+
+   // If we didn't recognise the ID:
+   throw std::runtime_error( "Unsupported version of "
+                             "xAOD::MissingETAuxAssociationMap found" );
+   return 0;
+}
