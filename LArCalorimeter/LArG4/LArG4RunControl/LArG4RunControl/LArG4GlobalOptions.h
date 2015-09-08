077b4bb15eaaac523f59d14d999a2d13321717c4
Zachary Louis Marshall <zach.marshall@cern.ch>
2015-09-08 19:05:23 +0200

    Removing obsolete options that are ported into the new SD framework (LArG4RunControl-00-01-00)
    	* Tagging: LArG4RunControl-00-01-00
    	* Removing obsolete options that are ported into the new SD framework

--- LArCalorimeter/LArG4/LArG4RunControl/LArG4RunControl/LArG4GlobalOptions.h
+++ LArCalorimeter/LArG4/LArG4RunControl/LArG4RunControl/LArG4GlobalOptions.h
@@ -25,58 +25,21 @@ class LArG4GlobalOptions {
   // of defaults:
 
   inline LArG4GlobalOptions() : 
-    m_OutOfTimeCut   (300*CLHEP::ns),
-    m_TimeBinType   ("Default"),
-    m_TimeBinWidth   (2.5*CLHEP::ns),
-    m_doCalibHitParticleID(false),
-    m_allowHitModification(false) {}
+    m_OutOfTimeCut   (300*CLHEP::ns) {}
 
   void saveMe();
   void printMe();
 
   // Set methods
   inline void OutOfTimeCut(double value)     { m_OutOfTimeCut = value; }
-  inline void TimeBinType(std::string value) { m_TimeBinType = value; }
-  inline void TimeBinWidth(double value)     { m_TimeBinWidth = value; }
-  inline void SetDoCalibHitParticleID(bool doParticleID) { m_doCalibHitParticleID=doParticleID; }
-  inline void AllowHitModification(bool allowHitModification) { m_allowHitModification=allowHitModification; }
 
   // Get methods
   inline double OutOfTimeCut(void) const     { return m_OutOfTimeCut; }
-  inline std::string TimeBinType(void) const { return m_TimeBinType; }
-  inline double TimeBinWidth(void) const     { return m_TimeBinWidth; }
-  bool   GetDoCalibHitParticleID() const { return m_doCalibHitParticleID;}
-  inline bool AllowHitModification(void) const { return m_allowHitModification; }
 
  private:
   // Value of the substep length in the current simulation
   double m_OutOfTimeCut;
 
-
-  // -- removing  double m_TimeBinWidth;
-  // Two types of LAr hit time binning
-  // 1. 'Default'
-  //
-  //     All negative times to the bin 0
-  //     0 <= time < 10 by 2.5ns
-  //     10 <= time < 50 by 10ns
-  //     50 <= time < 100 by 25ns
-  //     All others to the bin 14
-  //
-  //
-  // 2. 'Uniform'
-  // 
-  // Old style time binning by 2.5ns
-  std::string m_TimeBinType;
-
-  // Width of the time bins in the uniform binning
-  double m_TimeBinWidth;
-
-  // Flag to produce calibration hits signed with primary particle ID
-  bool   m_doCalibHitParticleID;
-
-  // Flag to write hit collections to StoreGate as non-const to allow modifications by various simulation algorithms
-  bool   m_allowHitModification;
 };
 
 #ifndef GAUDI_NEUTRAL
