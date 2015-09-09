02d75c45e9147a8cf5d74b15561bd6c9ccccae9d
Attila Krasznahorkay <krasznaa@cern.ch>
2015-09-09 10:30:47 +0200

    Cleaning the package a bit before production with it would begin (xAODForward-00-00-04)
    Former-commit-id: 0e0f9247cd7c564cc12da73d61f5063876779264

--- Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
+++ Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
@@ -2,25 +2,13 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: ContainerProxies.cxx 766963 2016-08-08 20:07:09Z ggach $
+// $Id: ContainerProxies.cxx 693858 2015-09-09 10:30:15Z krasznaa $
 
 // EDM include(s):
 #include "xAODCore/AddDVProxy.h"
 
 // Local include(s):
 #include "xAODForward/versions/ALFADataContainer_v1.h"
-#include "xAODForward/versions/AFPDataContainer_v1.h"
-#include "xAODForward/versions/AFPSiHitContainer_v1.h"
-#include "xAODForward/versions/AFPTrackContainer_v1.h"
-#include "xAODForward/versions/ZdcModuleContainer_v1.h"
-#include "xAODForward/versions/MBTSModuleContainer_v1.h"
-#include "xAODForward/versions/ForwardEventInfoContainer_v1.h"
 
 // Set up the collection proxies:
 ADD_NS_DV_PROXY( xAOD, ALFADataContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, AFPDataContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, AFPSiHitContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, AFPTrackContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, ZdcModuleContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, MBTSModuleContainer_v1 );
-ADD_NS_DV_PROXY( xAOD, ForwardEventInfoContainer_v1 );
