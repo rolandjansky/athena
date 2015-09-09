fd10512c1372cb9f455ca4e50eaa44f28429f425
Giulio Cornelio Grossi <giulio.cornelio.grossi@cern.ch>
2015-09-09 13:42:50 +0200

    Added efficiencies calculated with TProfile and some lowest unprescaled Chains (TrigJetMonitoring-00-05-14)
    Former-commit-id: be986713b7594ca6a84da051e8a317d4bf9708c8

--- Trigger/TrigMonitoring/TrigJetMonitoring/python/TrigJetMonitCategory.py
+++ Trigger/TrigMonitoring/TrigJetMonitoring/python/TrigJetMonitCategory.py
@@ -2,55 +2,40 @@
 
 ## Monitoring triggers
 ## Modify to the default naming scheme for menu-aware montioring.
-
-monitoring_l1jet = [ 'L1_J10',
-                     'L1_TE50',
-                     'L1_J15',
+monitoring_l1jet = [ 'L1_J15',
                      'L1_J75',
                      'L1_J100',
                      'L1_J100.31ETA49',
                      'L1_J400']
+                   #  'L1_4J15',
+                   #  'L1_4J20']
 
+monitoring_jet = [ 'j25',
+                   'j25_320eta490',
+                   'j60', 
+                   'j60_L1RD0_FILLED',
+                   'j60_280eta320',
+                   'j60_320eta490',
+                   'j200_jes_PS',
+                   'j260',
+                #   'j260_320eta490',
+                   'j360',
+                   'j460_a10_sub_L1J100',
+                   'j80_xe80',
+                   '4j45',
+                   '6j45',
+                   '5j60',
+                   'j0_perf_ds1_L1J75',
+                   'j0_perf_ds1_L1J100']
+                #   'ht700_L1J75'] 
 
-monitoring_jet = ['j25',
-                  'j25_320eta490',
-                  'j60', 
-                  'j60_L1RD0_FILLED',
-                  'j60_280eta320',
-                  'j60_320eta490',
-                  'j200_jes_PS',
-                  'j260',
-                  'j360',
-                  'j460',
-                  'j460_a10_lcw_sub_L1J100',
-                  'j80_xe80',
-                  '4j45',
-                  '6j60',
-                  '5j60',
-                  '10j40_L14J20',
-                  'j0_perf_ds1_L1J75',
-                  'j0_perf_ds1_L1J100',
-                  'ht850_L1J100',
-                  'j60_TT',
-                  'j85_cleanLLP',
-                  'j85_cleanL',
-                  'j85_cleanT',
-                  'j0_1i2c300m500TLA',
-                  'j0_0i1c500m900TLA'] 
-
-
-
-
-
-
-primary_l1jet                 = ['L1_J10',
-                                 'L1_TE50',
-                                 'L1_J15',
-                                 'L1_J75',
-                                 'L1_J100',
-                                 'L1_J100.31ETA49',
-                                 'L1_J400']
-
+primary_l1jet                 = [ 'L1_J15',
+                                  'L1_J75',
+                                  'L1_J100',
+                                  'L1_J100.31ETA49',
+                                  'L1_J400']
+                                #  'L1_4J15',
+                                #  'L1_4J20']
 
 primary_jet                = ['j25',
                               'j25_320eta490',
@@ -60,288 +45,13 @@ primary_jet                = ['j25',
                               'j60_320eta490',
                               'j200_jes_PS',
                               'j260',
+                            #  'j260_320eta490',
                               'j360',
-                              'j460',
-                              'j460_a10_lcw_sub_L1J100',
+                              'j460_a10_sub_L1J100',
                               'j80_xe80',
                               '4j45',
-                              '6j60',
+                              '6j45',
                               '5j60',
-                              '10j40_L14J20',
                               'j0_perf_ds1_L1J75',
-                              'j0_perf_ds1_L1J100',
-                              'ht850_L1J100',
-                              'j60_TT',
-                              'j85_cleanLLP',
-                              'j85_cleanL',
-                              'j85_cleanT',
-                              'j0_1i2c300m500TLA',
-                              'j0_0i1c500m900TLA'] 
-
-
-
-########################## pp Config ###############################################
-
-
-monitoring_l1jet_pp = [ 'L1_J10',
-                        'L1_TE50',
-                        'L1_J15',
-                        'L1_J75',
-                        'L1_J100',
-                        'L1_J100.31ETA49',
-                        'L1_J400']
-
-
-monitoring_jet_pp = ['j25',
-                     'j25_320eta490',
-                     'j60', 
-                     'j60_L1RD0_FILLED',
-                     'j60_280eta320',
-                     'j60_320eta490',
-                     'j200_jes_PS',
-                     'j260',
-                     'j360',
-                     'j460',
-                     'j460_a10_lcw_sub_L1J100',
-                     'j80_xe80',
-                     '4j45',
-                     '6j60',
-                     '5j60',
-                     '10j40_L14J20',
-                     'j0_perf_ds1_L1J75',
-                     'j0_perf_ds1_L1J100',
-                     'ht850_L1J100',
-                     'j60_TT',
-                     'j85_cleanLLP',
-                     'j85_cleanL',
-                     'j85_cleanT',
-                     'j0_1i2c300m500TLA',
-                     'j0_0i1c500m900TLA'] 
-
-
-
-
-
-
-primary_l1jet_pp                 = ['L1_J10',
-                                    'L1_TE50',
-                                    'L1_J15',
-                                    'L1_J75',
-                                    'L1_J100',
-                                    'L1_J100.31ETA49',
-                                    'L1_J400']
-
-
-primary_jet_pp                = ['j25',
-                                 'j25_320eta490',
-                                 'j60', 
-                                 'j60_L1RD0_FILLED',
-                                 'j60_280eta320',
-                                 'j60_320eta490',
-                                 'j200_jes_PS',
-                                 'j260',
-                                 'j360',
-                                 'j460',
-                                 'j460_a10_lcw_sub_L1J100',
-                                 'j80_xe80',
-                                 '4j45',
-                                 '6j60',
-                                 '5j60',
-                                 '10j40_L14J20',
-                                 'j0_perf_ds1_L1J75',
-                                 'j0_perf_ds1_L1J100',
-                                 'ht850_L1J100',
-                                 'j60_TT',
-                                 'j85_cleanLLP',
-                                 'j85_cleanL',
-                                 'j85_cleanT',
-                                 'j0_1i2c300m500TLA',
-                                 'j0_0i1c500m900TLA'] 
-
-
-########################## validation Config ###############################################
-
-
-monitoring_l1jet_validation = [ 'L1_J10',
-                                'L1_TE50',
-                                'L1_J15',
-                                'L1_J75',
-                                'L1_J100',
-                                'L1_J100.31ETA49',
-                                'L1_J400']
-
-
-monitoring_jet_validation = ['j25',
-                             'j25_320eta490',
-                             'j60', 
-                             'j60_L1RD0_FILLED',
-                             'j60_280eta320',
-                             'j60_320eta490',
-                             'j200_jes_PS',
-                             'j260',
-                             'j360',
-                             'j460',
-                             'j460_a10_lcw_sub_L1J100',
-                             'j80_xe80',
-                             '4j45',
-                             '6j60',
-                             '5j60',
-                             '10j40_L14J20',
-                             'j0_perf_ds1_L1J75',
-                             'j0_perf_ds1_L1J100',
-                             'ht850_L1J100',
-                             'j60_TT',
-                             'j85_cleanLLP',
-                             'j85_cleanL',
-                             'j85_cleanT',
-                             'j0_1i2c300m500TLA',
-                             'j0_0i1c500m900TLA'] 
-
-
-
-
-
-
-primary_l1jet_validation         = ['L1_J10',
-                                    'L1_TE50',
-                                    'L1_J15',
-                                    'L1_J75',
-                                    'L1_J100',
-                                    'L1_J100.31ETA49',
-                                    'L1_J400']
-
-
-primary_jet_validation        = ['j25',
-                                 'j25_320eta490',
-                                 'j60', 
-                                 'j60_L1RD0_FILLED',
-                                 'j60_280eta320',
-                                 'j60_320eta490',
-                                 'j200_jes_PS',
-                                 'j260',
-                                 'j360',
-                                 'j460',
-                                 'j460_a10_lcw_sub_L1J100',
-                                 'j80_xe80',
-                                 '4j45',
-                                 '6j60',
-                                 '5j60',
-                                 '10j40_L14J20',
-                                 'j0_perf_ds1_L1J75',
-                                 'j0_perf_ds1_L1J100',
-                                 'ht850_L1J100',
-                                 'j60_TT',
-                                 'j85_cleanLLP',
-                                 'j85_cleanL',
-                                 'j85_cleanT',
-                                 'j0_1i2c300m500TLA',
-                                 'j0_0i1c500m900TLA'] 
-
-
-
-######################## Cosmics Config ############################################
-
-monitoring_l1jet_cosmic = ['L1_J10',
-                           'L1_J15']
-
-monitoring_jet_cosmic = ['j25']
-
-primary_l1jet_cosmic = ['L1_J10',
-                        'L1_J15']
-
-primary_jet_cosmic = ['j25']
-
-
-
-###################################### HI Config #######################################
-
-
-
-
-monitoring_l1jet_hi = ['L1_J10',
-                       'L1_TE50',
-                       'L1_J15',
-                       'L1_J75',
-                       'L1_J100',
-                       'L1_J100.31ETA49',
-                       'L1_J20',
-                       'L1_J400'] 
-
-                     
-
-monitoring_jet_hi = ['j25',
-                     'j25_320eta490',
-                     'j60', 
-                     'j60_L1RD0_FILLED',
-                     'j60_280eta320',
-                     'j60_320eta490',
-                     'j200_jes_PS',
-                     'j260',
-                     'j360',
-                     'j460_a10_lcw_sub_L1J100',
-                     'j80_xe80',
-                     '4j45',
-                     '6j60',
-                     '5j60',
-                     'j0_perf_ds1_L1J75',
-                     'j0_perf_ds1_L1J100',
-                     'ht850_L1J100',
-                     'j30_ion_L1TE20', #start HI chains
-                     'j30_L1TE20',
-                     'j75_ion_L1J20',
-                     'j85_ion_L1J20',
-                     'j100_ion_L1J20',
-                     'j150_ion_L1J30',
-                     'j75_L1J20',
-                     'j85_L1J20',
-                     'j100_L1J20',
-                     'j150_L1J30',
-                     'j45_320eta490_ion',
-                     'j45_320eta490',
-                     'j50_ion_2j30_ion_0eta490_L1J10',
-                     'j50_2j30_0eta490_L1J10'] 
-
-primary_l1jet_hi = ['L1_J10',
-                    'L1_TE50',
-                    'L1_J15',
-                    'L1_J75',
-                    'L1_J100',
-                    'L1_J100.31ETA49',
-                    'L1_J20',
-                    'L1_J400'] 
-                     
-                     
-
-primary_jet_hi = ['j25',
-                  'j25_320eta490',
-                  'j60', 
-                  'j60_L1RD0_FILLED',
-                  'j60_280eta320',
-                  'j60_320eta490',
-                  'j200_jes_PS',
-                  'j260',
-                  'j360',
-                  'j460_a10_lcw_sub_L1J100',
-                  'j80_xe80',
-                  '4j45',
-                  '6j60',
-                  '5j60',
-                  'j0_perf_ds1_L1J75',
-                  'j0_perf_ds1_L1J100',
-                  'ht850_L1J100',
-                  'j30_ion_L1TE20', #start HI chains
-                  'j30_L1TE20',
-                  'j75_ion_L1J20',
-                  'j85_ion_L1J20',
-                  'j100_ion_L1J20',
-                  'j150_ion_L1J30',
-                  'j75_L1J20',
-                  'j85_L1J20',
-                  'j100_L1J20',
-                  'j150_L1J30',
-                  'j45_320eta490_ion',
-                  'j45_320eta490',
-                  'j50_ion_2j30_ion_0eta490_L1J10',
-                  'j50_2j30_0eta490_L1J10'] 
-
-
+                              'j0_perf_ds1_L1J100']
+                         #     'ht700_L1J75'] 
