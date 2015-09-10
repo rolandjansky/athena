c206656bbcf2fe1ee52e2240fa1cd03f2f2cbb8e
Christos Leonidopoulos <C.Leonidopoulos@cern.ch>
2015-09-10 13:13:43 +0200

    fix for ATR-12037 (TrigHLTMonitoring-00-07-03)
    	* added new menu-aware monitoring code

--- Trigger/TrigMonitoring/TrigHLTMonitoring/database/MaDQM.py
+++ Trigger/TrigMonitoring/TrigHLTMonitoring/database/MaDQM.py
@@ -2,6 +2,21 @@
 
 print "hello from MaDQM.py"
 
+def getAndSetConfig(dbconnection, MCK, runtype, slice_name, reco_step, *vargs, **kwargs):
+  # here we check which entry there is in the DB for this slice
+  config_thisFunction = readSliceConfigFromDB(dbconnection, MCK, runtype, slice_name, reco_step) 
+  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
+  if config_thisFunction != None:
+    for (toolname, toolconfig) in config_thisFunction['Config']:
+      # Ben: if the tool is not loaded into ToolSvc then we best load it now!
+      if hasattr(ToolSvc,toolname) == False:
+        exec "from %s import %s" % (config_thisFunction["PkgName"],config_thisFunction["CreatorName"])
+        exec "%s()" % (config_thisFunction["CreatorName"])
+      # modify defaults according to config dictionary
+      tool = getattr(ToolSvc,toolname)
+      for confattr,confvalue in toolconfig.items():
+        tool.__setattr__(confattr, confvalue)
+
 # read monitoring configureation from the database
 # calling readSliceConfigFromDB(dbconnection, MCK, runtype, slicename, recostep) returns monitoring configuration
 # dbconnection is 'oracle'
@@ -15,136 +30,58 @@ print "hello from MaDQM.py"
 
 include("TrigHLTMonitoring/readConfigFromDB.py")
 
-if HLTMonFlags.doCalo and DQMonFlags.monManEnvironment == 'tier0Raw':
-  # here we check which entry there is in the DB for this slice
-  config_caloraw = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'CALORAW' , 'RAW') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_caloraw != None:
-    for (toolname, toolconfig) in config_caloraw['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+# if HLTMonFlags.doCalo and DQMonFlags.monManEnvironment == 'tier0Raw':
+#   # here we check which entry there is in the DB for this slice
+#   config_caloraw = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'CALORAW' , 'RAW') 
+#   # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
+#   if config_caloraw != None:
+#     for (toolname, toolconfig) in config_caloraw['Config']:
+#       #print "BENtest1:"
+#       #print "ToolSvc._Configurable__children",ToolSvc._Configurable__children
+#       #print "config_caloraw[\"PkgName\"]",config_caloraw["PkgName"]
+#       #print "config_caloraw[\"CreatorName\"]",config_caloraw["CreatorName"]
+#       exec "from %s import %s" % (config_caloraw["PkgName"],config_caloraw["CreatorName"])
+#       exec "%s()" % (config_caloraw["CreatorName"])
+#       #print "BENtest2:"
+#       #print "ToolSvc._Configurable__children",ToolSvc._Configurable__children
+#       #print "hasattr(ToolSvc,\"HLTCalo\")",hasattr(ToolSvc,"HLTCalo")
+#       # modify defaults according to config dictionary
+#       tool = getattr(ToolSvc,toolname)
+#       for confattr,confvalue in toolconfig.items():
+#         tool.__setattr__(confattr, confvalue)
 
+if HLTMonFlags.doCalo and DQMonFlags.monManEnvironment == 'tier0Raw':
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'CALORAW' , 'RAW')
 
 if HLTMonFlags.doCalo and DQMonFlags.monManEnvironment == 'tier0ESD':
-  # here we check which entry there is in the DB for this slice
-  config_caloesd = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'CALOESD' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_caloesd != None:
-    for (toolname, toolconfig) in config_caloesd['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'CALOESD' , 'ESD')
 
 if HLTMonFlags.doTau:
-  # here we check which entry there is in the DB for this slice
-  config_tau = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'TAU' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_tau != None:
-    for (toolname, toolconfig) in config_tau['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'TAU' , 'ESD')
 
 if HLTMonFlags.doBjet:
-  # here we check which entry there is in the DB for this slice
-  config_bjet = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'BJET' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_bjet != None:
-    for (toolname, toolconfig) in config_bjet['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'BJET' , 'ESD')
 
 if HLTMonFlags.doBphys:
-  # here we check which entry there is in the DB for this slice
-  config_bphys = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'BPHYS' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_bphys != None:
-    for (toolname, toolconfig) in config_bphys['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'BPHYS' , 'ESD')
 
 if HLTMonFlags.doMET:
-  # here we check which entry there is in the DB for this slice
-  config_met = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'MET' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_met != None:
-    for (toolname, toolconfig) in config_met['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'MET' , 'ESD')
 
 if HLTMonFlags.doJet:
-  # here we check which entry there is in the DB for this slice
-  config_jet = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'JET' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_jet != None:
-    for (toolname, toolconfig) in config_jet['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'JET' , 'ESD')
 
 if HLTMonFlags.doEgamma:
-  # here we check which entry there is in the DB for this slice
-  config_egamma = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'EGAMMA' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_egamma != None:
-    for (toolname, toolconfig) in config_egamma['Config']:
-        # modify defaults according to config dictionary
-       tool = getattr(ToolSvc,toolname)
-       for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'EGAMMA' , 'ESD')
 
 if HLTMonFlags.doMuon:
-  # here we check which entry there is in the DB for this slice
-  config_muon = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'MUON' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_muon != None:
-    for (toolname, toolconfig) in config_muon['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'MUON' , 'ESD')
 
 if HLTMonFlags.doIDtrk:
-  # here we check which entry there is in the DB for this slice
-  config_idtrk = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'IDTRK' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_idtrk != None:
-    for (toolname, toolconfig) in config_idtrk['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'IDTRK' , 'ESD')
 
 if HLTMonFlags.doMinBias:
-  # here we check which entry there is in the DB for this slice
-  config_minbias = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'MINBIAS' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_minbias != None:
-    for (toolname, toolconfig) in config_minbias['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'MINBIAS' , 'ESD')
 
 if HLTMonFlags.doIDJpsiMon:
-  # here we check which entry there is in the DB for this slice
-  config_idjpsi = readSliceConfigFromDB('oracle','ACTIVE_KEY','Physics', 'IDJPSI' , 'ESD') 
-  # if entry is not None then we take monitoring config from DB, else it is taken from the release in a standard way 
-  if config_idjpsi != None:
-    for (toolname, toolconfig) in config_idjpsi['Config']:
-        # modify defaults according to config dictionary
-        tool = getattr(ToolSvc,toolname)
-        for confattr,confvalue in toolconfig.items():
-            tool.__setattr__(confattr, confvalue)
-
+  getAndSetConfig('oracle','ACTIVE_KEY','Physics', 'IDJPSI' , 'ESD')
