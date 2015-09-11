6da0234541580c62bb5bf7c38e08020bd9fedd1a
Zachary Louis Marshall <zach.marshall@cern.ch>
2015-09-11 23:10:06 +0200

    New infrastructure and extensive changes (LArG4H8SD-00-02-00)
    	* Changes to the base classes - new infrastructure
    	* tag LArG4H8SD-00-02-00
    
    2015-06-02 Andrea Di Simone
    	* removed explicit instance CalibrationDefaultProcessing, since
    	the new UserActions don't need (nor allow) it
    	*  tag LArG4H8SD-00-01-02

---
+++ LArCalorimeter/LArG4/LArG4H8SD/python/LArG4H8SDConfigDb.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addTool
+addTool("LArG4H8SD.LArG4H8SDConfig.getLArH8CalibSensitiveDetector","LArH8CalibSensitiveDetector")
