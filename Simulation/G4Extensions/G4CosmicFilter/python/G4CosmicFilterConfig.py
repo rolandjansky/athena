ee79f50c13823acdf104e53282a7affe30323338
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-09-15 08:30:25 +0200

    src/G4CosmicFilter.cxx, src/G4CosmicAndFilter.cxx, src/G4CosmicOrFilter.cxx - drop include of MCTruth/TrackRecorderSD.h as not used. Tagging: G4CosmicFilter-00-01-02 (G4CosmicFilter-00-01-02)
    Former-commit-id: e2c6cdab47ab389e991604d8ef8b7fb675fc94d4

--- Simulation/G4Extensions/G4CosmicFilter/python/G4CosmicFilterConfig.py
+++ Simulation/G4Extensions/G4CosmicFilter/python/G4CosmicFilterConfig.py
@@ -8,57 +8,57 @@ def configCosmicFilterVolumeNames():
 
     # returns a list with volume names
     # depending on how many names are returned different filters must be called
-
+    
     # easier config, if StoppedParticleFile is on, use plain filter
     if simFlags.StoppedParticleFile.statusOn:
         return ['StoppingPositions']
 
     # more complex configs, in case real cosmic simulation is done
-    collectionName="CaloEntryLayer"
+    volumeName="CaloEntryLayer"
     if simFlags.CosmicFilterVolumeName=="Muon":
-        collectionName="MuonExitLayer"
+        volumeName="MuonExitLayer"
     if simFlags.CosmicFilterVolumeName=="Calo":
-       collectionName="MuonEntryLayer"
+       volumeName="MuonEntryLayer"
     if simFlags.CosmicFilterVolumeName=="TRT_Barrel":
-        collectionName="TRTBarrelEntryLayer"
+        volumeName="TRTBarrelEntryLayer"
     if simFlags.CosmicFilterVolumeName=="TRT_EC":
-        collectionName="TRTECAEntryLayer"
+        volumeName="TRTECAEntryLayer"
     if simFlags.CosmicFilterVolumeName=="SCT_Barrel":
-        collectionName="SCTBarrelEntryLayer"
+        volumeName="SCTBarrelEntryLayer"
     if simFlags.CosmicFilterVolumeName=="Pixel":
-        collectionName="PixelEntryLayer"
+        volumeName="PixelEntryLayer"
 
-    collectionName2=''
+    volumeName2=''
     # sometimes users turn CosmicFilterVolumeName2 on and set it to NONE
     if simFlags.CosmicFilterVolumeName2.statusOn and simFlags.CosmicFilterVolumeName2 != 'NONE':
         if simFlags.CosmicFilterVolumeName2=="Muon":
-            collectionName2="MuonExitLayer"
+            volumeName2="MuonExitLayer"
         if simFlags.CosmicFilterVolumeName=="Calo":
-            collectionName="MuonEntryLayer"
+            volumeName="MuonEntryLayer"
         if simFlags.CosmicFilterVolumeName2=="InnerDetector":
-            collectionName2="CaloEntryLayer"
+            volumeName2="CaloEntryLayer"
         if simFlags.CosmicFilterVolumeName2=="TRT_Barrel":
-            collectionName2="TRTBarrelEntryLayer"
+            volumeName2="TRTBarrelEntryLayer"
         if simFlags.CosmicFilterVolumeName2=="TRT_EC":
-            collectionName2="TRTECAEntryLayer"
+            volumeName2="TRTECAEntryLayer"
         if simFlags.CosmicFilterVolumeName2=="SCT_Barrel":
-            collectionName2="SCTBarrelEntryLayer"
+            volumeName2="SCTBarrelEntryLayer"
         if simFlags.CosmicFilterVolumeName2=="Pixel":
-            collectionName2="PixelEntryLayer"
+            volumeName2="PixelEntryLayer"
 
 
     if simFlags.CosmicFilterVolumeName=="TRT_EC":
         # sometimes users turn CosmicFilterVolumeName2 on and set it to NONE
         if (not simFlags.CosmicFilterVolumeName2.statusOn) or simFlags.CosmicFilterVolumeName2 == 'NONE' :
-            return [collectionName,"TRTECBEntryLayer"]
+            return [volumeName,"TRTECBEntryLayer"]
     # sometimes users turn CosmicFilterVolumeName2 on and set it to NONE
     if simFlags.CosmicFilterVolumeName2.statusOn and  simFlags.CosmicFilterVolumeName2 != 'NONE':
         if simFlags.CosmicFilterVolumeName2=="TRT_EC":
-            return [collectionName,collectionName2,"TRTECBEntryLayer"]
+            return [volumeName,volumeName2,"TRTECBEntryLayer"]
         else:
-            return [collectionName,collectionName2]
+            return [volumeName,volumeName2]
 
-    return [collectionName]
+    return [volumeName]
 
 def getCosmicFilter(name="G4CosmicFilter", **kwargs):
 
@@ -73,7 +73,7 @@ def getCosmicFilter(name="G4CosmicFilter", **kwargs):
             kwargs.setdefault("PtMin", simFlags.CosmicFilterPTmin.get_Value())
         if simFlags.CosmicFilterPTmax.statusOn:
             kwargs.setdefault("PtMax", simFlags.CosmicFilterPTmax.get_Value())
-        kwargs.setdefault("CollectionName",volumes[0])
+        kwargs.setdefault("VolumeName",volumes[0])
 
         print 'G4CosmicFilter: Filter volume is %s' % volumes[0]
 
@@ -81,45 +81,13 @@ def getCosmicFilter(name="G4CosmicFilter", **kwargs):
 
     elif len(volumes)==2:
         # need a cosmic AND filter
-        kwargs.setdefault("CollectionName",volumes[0])
-        kwargs.setdefault("CollectionName2",volumes[1])
+        kwargs.setdefault("VolumeName",volumes[0])
+        kwargs.setdefault("VolumeName2",volumes[1])
         return  CfgMgr.G4CosmicAndFilter(name, **kwargs)
-
+        
     else:
-        # need a cosmic OR filter
-        kwargs.setdefault("CollectionName",volumes[0])
-        kwargs.setdefault("CollectionName2",volumes[1])
-        kwargs.setdefault("CollectionName3",volumes[2])
+        # need a cosmif OR filter
+        kwargs.setdefault("VolumeName",volumes[0])
+        kwargs.setdefault("VolumeName2",volumes[1])
+        kwargs.setdefault("VolumeName2",volumes[2])
         return  CfgMgr.G4CosmicOrFilter(name, **kwargs)
-
-def getCosmicFilterTool(name="G4UA::G4CosmicFilterTool", **kwargs):
-
-    volumes=configCosmicFilterVolumeNames()
-
-    # use simple  cosmic filter
-    if len(volumes)==1:
-
-        if simFlags.CosmicFilterID.statusOn:
-            kwargs.setdefault("PDGId", simFlags.CosmicFilterID.get_Value())
-        if simFlags.CosmicFilterPTmin.statusOn:
-            kwargs.setdefault("PtMin", simFlags.CosmicFilterPTmin.get_Value())
-        if simFlags.CosmicFilterPTmax.statusOn:
-            kwargs.setdefault("PtMax", simFlags.CosmicFilterPTmax.get_Value())
-        kwargs.setdefault("CollectionName",volumes[0])
-
-        print 'G4CosmicFilter: Filter volume is %s' % volumes[0]
-
-        return  CfgMgr.G4UA__G4CosmicFilterTool(name, **kwargs)
-
-    elif len(volumes)==2:
-        # need a cosmic AND filter
-        kwargs.setdefault("CollectionName",volumes[0])
-        kwargs.setdefault("CollectionName2",volumes[1])
-        return  CfgMgr.G4UA__G4CosmicAndFilterTool(name, **kwargs)
-
-    else:
-        # need a cosmic OR filter
-        kwargs.setdefault("CollectionName",volumes[0])
-        kwargs.setdefault("CollectionName2",volumes[1])
-        kwargs.setdefault("CollectionName3",volumes[2])
-        return  CfgMgr.G4UA__G4CosmicOrFilterTool(name, **kwargs)
