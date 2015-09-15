4cdf6ac4023839882188a36e982b857ecde2fd45
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-09-15 15:57:35 +0200

    cmt/requirements - add use statement for 	MCTruthBase. ATLASSIM-2144. 	* G4TruthStrategies/HadronicInteractionStrategy.h , 	G4TruthStrategies/LLPStrategy.h , 	G4TruthStrategies/CaloCatchAllStrategy.h , 	G4TruthStrategies/MuonCatchAllStrategy.h , 	G4TruthStrategies/ComptonStrategy.h , 	G4TruthStrategies/BremsstrahlungStrategy.h , 	G4TruthStrategies/IonizationStrategy.h , 	G4TruthStrategies/ConversionStrategy.h , 	G4TruthStrategies/DecayStrategy.h - include TruthStrategy.h header 	from MCTruthBase rather than MCTruth. ATLASSIM-2144 	* Tagging: G4TruthStrategies-00-01-00 (G4TruthStrategies-00-01-00)
    Former-commit-id: b6f5c605c2ab43cab506b79619227a24cb0c645e

--- Simulation/G4Utilities/G4TruthStrategies/G4TruthStrategies/IonizationStrategy.h
+++ Simulation/G4Utilities/G4TruthStrategies/G4TruthStrategies/IonizationStrategy.h
@@ -2,14 +2,14 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef G4TruthStrategies_IonizationStrategy_H
-#define G4TruthStrategies_IonizationStrategy_H
+#ifndef IonizationStrategy_H
+#define IonizationStrategy_H
 
 #include "MCTruthBase/TruthStrategy.h"
 class IonizationStrategy: public TruthStrategy {
 public:
-  IonizationStrategy(const std::string&);
-  bool AnalyzeVertex(const G4Step*);
+	IonizationStrategy(const std::string);
+	bool AnalyzeVertex(const G4Step*);
 };
 
 #endif
--- Simulation/G4Utilities/G4TruthStrategies/src/LLPStrategy.cxx
+++ Simulation/G4Utilities/G4TruthStrategies/src/LLPStrategy.cxx
@@ -3,82 +3,59 @@
 */
 
 #include "G4TruthStrategies/LLPStrategy.h"
-
-#include "MCTruthBase/TruthStrategyUtils.h"
-//#include "SimHelpers/StepHelper.h"
-//#include "MCTruth/EventInformation.h"
-
+#include "SimHelpers/StepHelper.h"
+#include "MCTruth/EventInformation.h"
 #include "G4Step.hh"
 #include "G4ProcessType.hh"
 #include "G4VProcess.hh"
-
 #include <iostream>
 
 static LLPStrategy Strategy("LLP");
 
-LLPStrategy::LLPStrategy(const std::string& n)
+LLPStrategy::LLPStrategy(const std::string n)
   : TruthStrategy(n)
 {;}
 
 bool LLPStrategy::AnalyzeVertex(const G4Step* aStep)
 {
-  // Thread-safety issue
-  //static StepHelper sHelper;
-  //sHelper.SetStep(aStep);
-  //G4int pSubType = sHelper.GetProcessSubType();
+  static StepHelper sHelper;
 
-  G4int pSubType =
-    aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessSubType();
-
-  // All kinds of decay processes are included here...
-  if(pSubType > 200 && pSubType < 299) {
+  sHelper.SetStep(aStep);
+  G4int pSubType = sHelper.GetProcessSubType();
+  if( (pSubType>200 && pSubType<299) ){ // All kinds of decay processes are included here...
 
     // Check if this is a sparticle - if not, return
     int pdg = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
     if ( !isSUSYParticle( abs(pdg) ) ) return false;
 
-    // Get the list of secondaries in the current step
-    const auto& tracks = *aStep->GetSecondaryInCurrentStep();
-
-    // Save the vertex using TruthStrategyUtils
-    TruthStrategyUtils::saveSecondaryVertex(nullptr, aStep->GetPostStepPoint(), tracks);
-
-    // TODO: Remove these uncontrolled printouts!
-    std::cout << "ACHLLP: saved a truth vertex for pdg " << pdg << std::endl;
+    // we register the vertex using TruthManager's appropriate method
+    std::vector<G4Track*> tracks =theTruthManager->GetSecondaries();
+    theTruthManager->SaveSecondaryVertex(0,aStep->GetPostStepPoint(),tracks);
+    std::cout<<"ACHLLP: saved a truth vertex for pdg "<<pdg<<std::endl;
     return true;
   }
 
-  // This next bit could easily be a separate truth strategy
+  G4ProcessType pType=sHelper.GetProcess()->GetProcessType();
+  if(fUserDefined==pType){//save all interactions for user-defined processes, like rhadron interactions
+    G4TrackStatus tStatus=aStep->GetTrack()->GetTrackStatus();
+    std::vector<G4Track*> tracks =theTruthManager->GetSecondaries();
 
-  G4ProcessType pType =
-    aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessType();
+    if (tStatus==fAlive) theTruthManager->SaveSecondaryVertex(aStep->GetTrack(), aStep->GetPostStepPoint(), tracks);
+    else theTruthManager->SaveSecondaryVertex(0, aStep->GetPostStepPoint(), tracks);
 
-  // Save all interactions for user-defined processes, like rhadron interactions
-  if(fUserDefined == pType) {
-
-    // Get the list of secondaries in the current step
-    const auto& tracks = *aStep->GetSecondaryInCurrentStep();
-
-    // Save the vertex using TruthStrategyUtils
-    G4TrackStatus tStatus = aStep->GetTrack()->GetTrackStatus();
-    G4Track* primTrack = (tStatus==fAlive) ? aStep->GetTrack() : nullptr;
-    TruthStrategyUtils::saveSecondaryVertex(primTrack, aStep->GetPostStepPoint(), tracks);
-
-    // TODO: Remove these uncontrolled printouts!
     int pdg = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
-    std::cout << "ACHLLP: saved a truth interaction fUserDefined for pdg " << pdg << std::endl;
-
+    std::cout<<"ACHLLP: saved a truth interaction fUserDefined for pdg "<<pdg<<std::endl;
     return true;
   }
-
+  
   return false;	
 }
 
 bool LLPStrategy::isSUSYParticle(const int id) const
 {
-  return ( (id>1000000 && id<1000007) || (id>1000010 && id<1000017) ||
+  return ( (id>1000000 && id<1000007) || (id>1000010 && id<1000017) || 
            (id>2000000 && id<2000007) || (id>2000010 && id<2000017) || // squarks and sleptons
-           (id>1000020 && id<1000026) || (id>1000034 && id<1000040) || // higgses etc
+           (id>1000020 && id<1000026) || (id>1000034 && id<1000040) || // higgses etc 
            id==1000512 || id==1000522 || id==1000991 || id==1000993 || // R-hadrons
            id==1000612 || id==1000622 || id==1000632 || id==1000642 || id==1000652 || id==1005211 ||
            id==1006113 || id==1006211 || id==1006213 || id==1006223 || id==1006311 ||
