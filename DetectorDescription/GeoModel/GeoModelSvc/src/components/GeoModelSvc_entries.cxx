4b0c78ad891ca97c4b9db83ca34f61be9aa511d6
Scott Snyder <scott.snyder@cern.ch>
2015-09-15 16:12:40 +0200

    Comply with ATLAS naming conventions. (GeoModelSvc-02-00-11)
    Former-commit-id: 50d5c8d29e771dd02dd22ae78b2d0b5a969141df

--- DetectorDescription/GeoModel/GeoModelSvc/src/components/GeoModelSvc_entries.cxx
+++ DetectorDescription/GeoModel/GeoModelSvc/src/components/GeoModelSvc_entries.cxx
@@ -1,12 +1,10 @@
 #include "../GeoModelSvc.h"
-#include "../GeoDbTagSvc.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_SERVICE_FACTORY(GeoModelSvc)
-DECLARE_SERVICE_FACTORY(GeoDbTagSvc)
 
 DECLARE_FACTORY_ENTRIES(GeoModelSvc) {
     DECLARE_SERVICE   ( GeoModelSvc )
-    DECLARE_SERVICE   ( GeoDbTagSvc )
+    //    DECLARE_CONVERTER ( GeomodelElemCnv )
 }
 
