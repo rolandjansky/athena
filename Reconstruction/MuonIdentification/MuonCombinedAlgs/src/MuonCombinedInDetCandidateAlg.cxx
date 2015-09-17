0fc9ec1fa4c6ee556555b4e3d986f9bd160dff90
Graham John Cree <graham.cree@cern.ch>
2015-09-17 14:36:47 +0200

    added DoSiliconAssocForwardMuons option (MuonCombinedAlgs-00-00-13)
    	* src/MuonCombinedInDetCandidateAlg.h,.cxx
    	* Added option DoSiliconAssocForwardMuons (default false)
    	* Added second InDetCandidateTool, id track collection
    		* Tagging MuonCombinedAlgs-00-00-13

--- Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonCombinedInDetCandidateAlg.cxx
+++ Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonCombinedInDetCandidateAlg.cxx
@@ -14,8 +14,11 @@ MuonCombinedInDetCandidateAlg::MuonCombinedInDetCandidateAlg(const std::string& 
   m_indetCandidateTool("MuonCombined::InDetCandidateTool/InDetCandidateTool")
 {  
   declareProperty("InDetCandidateTool",m_indetCandidateTool);
+  declareProperty("InDetForwardCandidateTool",m_indetForwardCandidateTool);
   declareProperty("TrackParticleLocation",m_indetTrackParticleLocation = "InDetTrackParticles");
+  declareProperty("ForwardParticleLocation",m_indetForwardTrackParticleLocation = "InDetForwardTrackParticles");
   declareProperty("InDetCandidateLocation",m_candidateCollectionName = "InDetCandidates" );
+  declareProperty("DoSiliconAssocForwardMuons",m_doSiliconForwardMuons=false);
 }
 
 MuonCombinedInDetCandidateAlg::~MuonCombinedInDetCandidateAlg()
@@ -26,29 +29,56 @@ MuonCombinedInDetCandidateAlg::~MuonCombinedInDetCandidateAlg()
 StatusCode MuonCombinedInDetCandidateAlg::initialize()
 {
   ATH_CHECK(m_indetCandidateTool.retrieve());
-
+  if (m_doSiliconForwardMuons) {
+    ATH_CHECK(m_indetForwardCandidateTool.retrieve());
+  }
   return StatusCode::SUCCESS; 
 }
 
 StatusCode MuonCombinedInDetCandidateAlg::execute()
 {
+
   // retrieve MuonSpectrometer tracks
-  const xAOD::TrackParticleContainer* indetTrackParticles = 0;
-  if(evtStore()->contains<xAOD::TrackParticleContainer>(m_indetTrackParticleLocation)) {
-    if(evtStore()->retrieve(indetTrackParticles,m_indetTrackParticleLocation).isFailure()) {
-      ATH_MSG_FATAL( "Unable to retrieve " << m_indetTrackParticleLocation );
-      return StatusCode::FAILURE;
-    }
-  }
-  
-  if( !indetTrackParticles ){ 
-    ATH_MSG_WARNING("InDetTrackParticles tracks not found in StoreGate");
-    return StatusCode::SUCCESS;
+  std::vector<ToolHandle<MuonCombined::IInDetCandidateTool> > canTool;
+  std::vector<std::string> trackParticleLoc;
+
+  canTool.push_back(m_indetCandidateTool);
+  trackParticleLoc.push_back(m_indetTrackParticleLocation);
+
+  if (m_doSiliconForwardMuons){
+    canTool.push_back(m_indetForwardCandidateTool);
+    trackParticleLoc.push_back(m_indetForwardTrackParticleLocation);
   }
 
-  InDetCandidateCollection* indetCandidateCollection = new InDetCandidateCollection();
-  m_indetCandidateTool->create(*indetTrackParticles,*indetCandidateCollection);
-  
+
+  //output collection
+  InDetCandidateCollection* indetCandidateCollection = NULL ; 
+
+  for (unsigned int i(0);i<canTool.size();++i){
+
+    const xAOD::TrackParticleContainer* indetTrackParticles = 0;
+    if(evtStore()->contains<xAOD::TrackParticleContainer>(trackParticleLoc[i])){
+      if(evtStore()->retrieve(indetTrackParticles,trackParticleLoc[i]).isFailure()) {
+        ATH_MSG_FATAL( "Unable to retrieve " << trackParticleLoc[i] );
+        return StatusCode::FAILURE;
+      }
+    }
+
+    if( !indetTrackParticles ){ 
+      ATH_MSG_WARNING("InDetTrackParticles tracks not found in StoreGate");
+      return StatusCode::SUCCESS;
+    }
+
+    InDetCandidateCollection* tempCandidates = new InDetCandidateCollection(SG::VIEW_ELEMENTS);
+    canTool[i]->create(*indetTrackParticles,*tempCandidates);
+
+    //Copy indetCandidateCollection into output collection
+    if (!indetCandidateCollection) indetCandidateCollection = new InDetCandidateCollection(SG::OWN_ELEMENTS);
+    indetCandidateCollection->insert(indetCandidateCollection->end(), tempCandidates->begin(), tempCandidates->end());
+
+    delete tempCandidates;
+  }
+
   if(evtStore()->record(indetCandidateCollection,m_candidateCollectionName).isFailure()) {
     ATH_MSG_FATAL( "Could not record " << m_candidateCollectionName );
     return StatusCode::FAILURE;
