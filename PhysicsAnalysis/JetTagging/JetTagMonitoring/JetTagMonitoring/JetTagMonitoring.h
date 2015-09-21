b71693974768e30666538492f7129f5da39f096a
Kristian Anders Gregersen <kristian.anders.gregersen@cern.ch>
2015-09-21 19:56:35 +0200

    'see ChangeLog' (JetTagMonitoring-00-04-16)
    	* fixes for coverity reports 29967 (uninitialized member field), 29924 and 29923 (unchecked dynamic casts)
    	* tag: JetTagMonitoring-00-04-16
    
    2015-08-20 Kristian Gregersen <grkeren@cern.ch>
    	* modified procHistograms() for online monitoring
    	* tag: JetTagMonitoring-00-04-15

--- PhysicsAnalysis/JetTagging/JetTagMonitoring/JetTagMonitoring/JetTagMonitoring.h
+++ PhysicsAnalysis/JetTagging/JetTagMonitoring/JetTagMonitoring/JetTagMonitoring.h
@@ -103,7 +103,7 @@ private:
     bool m_histogramsCreated;
 
     /* const Trk::VxCandidate* m_priVtx; */
-    const xAOD::Vertex* m_priVtx;
+    const xAOD::Vertex* m_priVtx = nullptr;
 
     /** @brief String to retrieve JetContainer from StoreGate. */
     std::string m_jetName;
@@ -135,138 +135,138 @@ private:
 
 
     /** @brief To monitor number of tracks used to evaluate IP2D weight. */
-    TH1F_LW* m_tag_ip2d_n;
+    TH1F_LW* m_tag_ip2d_n = nullptr;
     /** @brief To monitor jet likelihood value if coming from b decay for IP2D tagger. */
-    TH1F_LW* m_tag_ip2d_b;
+    TH1F_LW* m_tag_ip2d_b = nullptr;
     /** @brief To monitor jet likelihood value if coming from u decay for IP2D tagger. */
-    TH1F_LW* m_tag_ip2d_u;
+    TH1F_LW* m_tag_ip2d_u = nullptr;
     /** @brief To monitor jet likelihood value if coming from c decay for IP2D tagger. */
-    TH1F_LW* m_tag_ip2d_c;
+    TH1F_LW* m_tag_ip2d_c = nullptr;
      /** @brief To monitor jet log likelihood ratio for IP2D tagger. */
-    TH1F_LW* m_tag_ip2d_llr;   
+    TH1F_LW* m_tag_ip2d_llr = nullptr;   
     /** @brief To monitor number of tracks used to evaluate IP3D weight. */   
-    TH1F_LW* m_tag_ip3d_n;
+    TH1F_LW* m_tag_ip3d_n = nullptr;
     /** @brief To monitor jet likelihood value if coming from b decay for IP3D tagger. */
-    TH1F_LW* m_tag_ip3d_b;
+    TH1F_LW* m_tag_ip3d_b = nullptr;
     /** @brief To monitor jet likelihood value if coming from u decay for IP3D tagger. */
-    TH1F_LW* m_tag_ip3d_u;
+    TH1F_LW* m_tag_ip3d_u = nullptr;
     /** @brief To monitor jet likelihood value if coming from c decay for IP3D tagger. */
-    TH1F_LW* m_tag_ip3d_c;   
+    TH1F_LW* m_tag_ip3d_c = nullptr;   
      /** @brief To monitor jet log likelihood ratio for IP3D tagger. */
-    TH1F_LW* m_tag_ip3d_llr;     
+    TH1F_LW* m_tag_ip3d_llr = nullptr;     
     /** @brief To monitor jet 3D significance of SV0 tagger. */
-    TH1F_LW* m_tag_sv0_sig3d;
+    TH1F_LW* m_tag_sv0_sig3d = nullptr;
     /** @brief To monitor jet likelihood value if coming from b decay for SV1 tagger. */
-    TH1F_LW* m_tag_sv1_b;
+    TH1F_LW* m_tag_sv1_b = nullptr;
     /** @brief To monitor jet likelihood value if coming from u decay for SV1 tagger. */
-    TH1F_LW* m_tag_sv1_u;
+    TH1F_LW* m_tag_sv1_u = nullptr;
      /** @brief To monitor jet likelihood value if coming from c decay for SV1 tagger. */
-    TH1F_LW* m_tag_sv1_c;    
+    TH1F_LW* m_tag_sv1_c = nullptr;    
       /** @brief To monitor jet log likelihood ratio for SV1 tagger. */
-    TH1F_LW* m_tag_sv1_llr;    
+    TH1F_LW* m_tag_sv1_llr = nullptr;    
     /** @brief To monitor log likelihood ratio for JetFitter. */
-    TH1F_LW* m_tag_jetfitter_llr;    
+    TH1F_LW* m_tag_jetfitter_llr = nullptr;    
     /** @brief To monitor log likelihood ratio for combined tagger JetFitterCOMBNN. */
-    TH1F_LW* m_tag_jfcnn_llr;
+    TH1F_LW* m_tag_jfcnn_llr = nullptr;
     
     /** @brief To monitor likelihood weight based on combined tagger (IP3D+SV1). */
-    TH1F_LW* m_tag_sv1ip3d_w;
-    TH1F_LW* m_tag_sv1ip3d_w_sj;
+    TH1F_LW* m_tag_sv1ip3d_w = nullptr;
+    TH1F_LW* m_tag_sv1ip3d_w_sj = nullptr;
    
     /** @brief To monitor likelihood weight based on MV2c20 tagger. */
-    TH1F_LW* m_tag_mv2c20_w;
-    TH1F_LW* m_tag_mv2c20_w_sj;
+    TH1F_LW* m_tag_mv2c20_w = nullptr;
+    TH1F_LW* m_tag_mv2c20_w_sj = nullptr;
     /** @brief To monitor number of Jets. */
-    TH1F_LW* m_jet_n;
+    TH1F_LW* m_jet_n = nullptr;
     /** @brief To monitor number of TrackParticles. */
-    TH1F_LW* m_trackParticle_n;
+    TH1F_LW* m_trackParticle_n = nullptr;
 
     /** @brief To monitor number of primary vertices. */
-    TH1F_LW* m_global_nPrimVtx;
+    TH1F_LW* m_global_nPrimVtx = nullptr;
     /** @brief To monitor x of primary vertex. */
-    TH1F_LW* m_global_xPrimVtx;
+    TH1F_LW* m_global_xPrimVtx = nullptr;
     /** @brief To monitor y of primary vertex. */
-    TH1F_LW* m_global_yPrimVtx;
+    TH1F_LW* m_global_yPrimVtx = nullptr;
     /** @brief To monitor z of primary vertex. */
-    TH1F_LW* m_global_zPrimVtx;
+    TH1F_LW* m_global_zPrimVtx = nullptr;
 
     /** @brief To monitor number of b layer hits in TrackParticle. */
-    TH1F_LW* m_global_BLayerHits;
+    TH1F_LW* m_global_BLayerHits = nullptr;
     /** @brief To monitor number of pixel hits in TrackParticle. */
-    TH1F_LW* m_global_PixelHits;
+    TH1F_LW* m_global_PixelHits = nullptr;
     /** @brief To monitor number of SCT hits in TrackParticle. */
-    TH1F_LW* m_global_SCTHits;
+    TH1F_LW* m_global_SCTHits = nullptr;
     /** @brief To monitor number of pixel+SCT hits in TrackParticle. */
-    TH1F_LW* m_global_SiHits;
+    TH1F_LW* m_global_SiHits = nullptr;
     /** @brief To monitor number of TRT hits in TrackParticle. */
-    TH1F_LW* m_global_TRTHits;
+    TH1F_LW* m_global_TRTHits = nullptr;
 
     /** @brief To monitor eta of the reconstructed jet. */
-    TH1F_LW* m_jet_eta;
+    TH1F_LW* m_jet_eta = nullptr;
     /** @brief To monitor phi of the reconstructed jet. */
-    TH1F_LW* m_jet_phi;
+    TH1F_LW* m_jet_phi = nullptr;
     /** @brief To monitor Et of the reconstructed jet. */
-    TH1F_LW* m_jet_et;
+    TH1F_LW* m_jet_et = nullptr;
 
     /** @brief To monitor number of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_n;
+    TH1F_LW* m_jet_tracks_n = nullptr;
     /** @brief To monitor Pt of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_pt;
+    TH1F_LW* m_jet_tracks_pt = nullptr;
     /** @brief To monitor eta of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_eta;
+    TH1F_LW* m_jet_tracks_eta = nullptr;
     /** @brief To monitor phi of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_phi;
+    TH1F_LW* m_jet_tracks_phi = nullptr;
     /** @brief To monitor d0 of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_d0;
+    TH1F_LW* m_jet_tracks_d0 = nullptr;
     /** @brief To monitor z0 of tracks in a jet. */
-    TH1F_LW* m_jet_tracks_z0;
+    TH1F_LW* m_jet_tracks_z0 = nullptr;
     /** @brief To monitor number of b layer hits in a jet. */
-    TH1F_LW* m_jet_tracks_BLayerHits;
+    TH1F_LW* m_jet_tracks_BLayerHits = nullptr;
     /** @brief To monitor number of pixel hits in a jet. */
-    TH1F_LW* m_jet_tracks_PixelHits;
+    TH1F_LW* m_jet_tracks_PixelHits = nullptr;
     /** @brief To monitor number of SCT hits in a jet. */
-    TH1F_LW* m_jet_tracks_SCTHits;
+    TH1F_LW* m_jet_tracks_SCTHits = nullptr;
     /** @brief To monitor number of muons in a jet. */
-    TH1F_LW* m_jet_muons_n;
+    TH1F_LW* m_jet_muons_n = nullptr;
     /** @brief To monitor Pt of muons in a jet. */
-    TH1F_LW* m_jet_muons_pt;
+    TH1F_LW* m_jet_muons_pt = nullptr;
 
     //* electron variables per jet *//
     /** @brief To monitor number of electrons in a jet. */
-    TH1F_LW* m_jet_electrons_n;
+    TH1F_LW* m_jet_electrons_n = nullptr;
     /** @brief To monitor Pt of electrons in a jet. */
-    TH1F_LW* m_jet_electrons_pt;
+    TH1F_LW* m_jet_electrons_pt = nullptr;
 
-    TH1F_LW* m_trigPassed;
-    TH1F_LW* m_cutflow;
+    TH1F_LW* m_trigPassed = nullptr;
+    TH1F_LW* m_cutflow = nullptr;
 
-    TH1F_LW* m_cutflow_jet;
+    TH1F_LW* m_cutflow_jet = nullptr;
 
-    TH1F_LW* m_priVtx_trks;
+    TH1F_LW* m_priVtx_trks = nullptr;
 
     /** @brief 2D map of track selector efficiency. */
-    TH2F_LW* m_track_selector_eff;
-    TH2F_LW* m_track_selector_eff_LS;
-    TH2F_LW* m_track_selector_suspect;
-    TH2F_LW* m_track_selector_all;
-    TH2F_LW* m_track_selector_all_LS;
+    TH2F_LW* m_track_selector_eff = nullptr;
+    TH2F_LW* m_track_selector_eff_LS = nullptr;
+    TH2F_LW* m_track_selector_suspect = nullptr;
+    TH2F_LW* m_track_selector_all = nullptr;
+    TH2F_LW* m_track_selector_all_LS = nullptr;
 
     /** @brief 2D map of jets at various cuts. */
-    TH2F_LW* m_jet_2D_all;
-    TH2F_LW* m_jet_2D_good;
-    TH2F_LW* m_jet_2D_kinematic;
-    TH2F_LW* m_jet_2D_kinematic_LS;
-    TH2F_LW* m_jet_2D_quality;
-    TH2F_LW* m_jet_2D_suspect;
+    TH2F_LW* m_jet_2D_all = nullptr;
+    TH2F_LW* m_jet_2D_good = nullptr;
+    TH2F_LW* m_jet_2D_kinematic = nullptr;
+    TH2F_LW* m_jet_2D_kinematic_LS = nullptr;
+    TH2F_LW* m_jet_2D_quality = nullptr;
+    TH2F_LW* m_jet_2D_suspect = nullptr;
 
     /** @brief 2D map of tag rates. */
-    TH2F_LW* m_sv1ip3d_tag_pos_rate_2D;
-    TH2F_LW* m_sv1ip3d_tag_neg_rate_2D;
+    TH2F_LW* m_sv1ip3d_tag_pos_rate_2D = nullptr;
+    TH2F_LW* m_sv1ip3d_tag_neg_rate_2D = nullptr;
 
-    TH2F_LW* m_mv2c20_tag_50_2D;
-    TH2F_LW* m_mv2c20_tag_70_2D;
-    TH2F_LW* m_mv2c20_tag_80_2D;   
-    TH2F_LW* m_mv2c20_tag_80_2D_LS;    
+    TH2F_LW* m_mv2c20_tag_50_2D = nullptr;
+    TH2F_LW* m_mv2c20_tag_70_2D = nullptr;
+    TH2F_LW* m_mv2c20_tag_80_2D = nullptr;   
+    TH2F_LW* m_mv2c20_tag_80_2D_LS = nullptr;    
 
     enum Cuts_t { pTMin, d0Max, z0Max, sigd0Max, sigz0Max, etaMax,
                   nHitBLayer, deadBLayer, nHitPix, nHitSct, nHitSi, nHitTrt, nHitTrtHighE,
@@ -275,57 +275,57 @@ private:
 
 
     /** @brief 2D map of TrackSelectorTool cuts */
-    TH2F_LW* m_tracks_all_2D;
-    TH2F_LW* m_tracks_pTMin_2D;
-    TH2F_LW* m_tracks_d0Max_2D;
-    TH2F_LW* m_tracks_z0Max_2D;
-    TH2F_LW* m_tracks_sigd0Max_2D;
-    TH2F_LW* m_tracks_sigz0Max_2D;
-    TH2F_LW* m_tracks_etaMax_2D;
-    TH2F_LW* m_tracks_nHitBLayer_2D;
-    TH2F_LW* m_tracks_deadBLayer_2D;
-    TH2F_LW* m_tracks_nHitPix_2D;
-    TH2F_LW* m_tracks_nHitSct_2D;
-    TH2F_LW* m_tracks_nHitSi_2D;
-    TH2F_LW* m_tracks_nHitTrt_2D;
-    TH2F_LW* m_tracks_nHitTrtHighE_2D;
-    TH2F_LW* m_tracks_fitChi2_2D;
-    TH2F_LW* m_tracks_fitProb_2D;
-    TH2F_LW* m_tracks_fitChi2OnNdfMax_2D;
+    TH2F_LW* m_tracks_all_2D = nullptr;
+    TH2F_LW* m_tracks_pTMin_2D = nullptr;
+    TH2F_LW* m_tracks_d0Max_2D = nullptr;
+    TH2F_LW* m_tracks_z0Max_2D = nullptr;
+    TH2F_LW* m_tracks_sigd0Max_2D = nullptr;
+    TH2F_LW* m_tracks_sigz0Max_2D = nullptr;
+    TH2F_LW* m_tracks_etaMax_2D = nullptr;
+    TH2F_LW* m_tracks_nHitBLayer_2D = nullptr;
+    TH2F_LW* m_tracks_deadBLayer_2D = nullptr;
+    TH2F_LW* m_tracks_nHitPix_2D = nullptr;
+    TH2F_LW* m_tracks_nHitSct_2D = nullptr;
+    TH2F_LW* m_tracks_nHitSi_2D = nullptr;
+    TH2F_LW* m_tracks_nHitTrt_2D = nullptr;
+    TH2F_LW* m_tracks_nHitTrtHighE_2D = nullptr;
+    TH2F_LW* m_tracks_fitChi2_2D = nullptr;
+    TH2F_LW* m_tracks_fitProb_2D = nullptr;
+    TH2F_LW* m_tracks_fitChi2OnNdfMax_2D = nullptr;
 
-    TH2F_LW* m_tracks_all_2D_LS;
-    TH2F_LW* m_tracks_pTMin_2D_LS;
-    TH2F_LW* m_tracks_d0Max_2D_LS;
-    TH2F_LW* m_tracks_z0Max_2D_LS;
-    TH2F_LW* m_tracks_sigd0Max_2D_LS;
-    TH2F_LW* m_tracks_sigz0Max_2D_LS;
-    TH2F_LW* m_tracks_etaMax_2D_LS;
-    TH2F_LW* m_tracks_nHitBLayer_2D_LS;
-    TH2F_LW* m_tracks_deadBLayer_2D_LS;
-    TH2F_LW* m_tracks_nHitPix_2D_LS;
-    TH2F_LW* m_tracks_nHitSct_2D_LS;
-    TH2F_LW* m_tracks_nHitSi_2D_LS;
-    TH2F_LW* m_tracks_nHitTrt_2D_LS;
-    TH2F_LW* m_tracks_nHitTrtHighE_2D_LS;
-    TH2F_LW* m_tracks_fitChi2_2D_LS;
-    TH2F_LW* m_tracks_fitProb_2D_LS;
-    TH2F_LW* m_tracks_fitChi2OnNdfMax_2D_LS;
+    TH2F_LW* m_tracks_all_2D_LS = nullptr;
+    TH2F_LW* m_tracks_pTMin_2D_LS = nullptr;
+    TH2F_LW* m_tracks_d0Max_2D_LS = nullptr;
+    TH2F_LW* m_tracks_z0Max_2D_LS = nullptr;
+    TH2F_LW* m_tracks_sigd0Max_2D_LS = nullptr;
+    TH2F_LW* m_tracks_sigz0Max_2D_LS = nullptr;
+    TH2F_LW* m_tracks_etaMax_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitBLayer_2D_LS = nullptr;
+    TH2F_LW* m_tracks_deadBLayer_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitPix_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitSct_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitSi_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitTrt_2D_LS = nullptr;
+    TH2F_LW* m_tracks_nHitTrtHighE_2D_LS = nullptr;
+    TH2F_LW* m_tracks_fitChi2_2D_LS = nullptr;
+    TH2F_LW* m_tracks_fitProb_2D_LS = nullptr;
+    TH2F_LW* m_tracks_fitChi2OnNdfMax_2D_LS = nullptr;
 
-    TH1F_LW* m_efficiency;
+    TH1F_LW* m_efficiency = nullptr;
 
-    unsigned int m_lumiBlockNum;
+    unsigned int m_lumiBlockNum = 0;
 
-    MonGroup* m_monGr_shift;
-    MonGroup* m_monGr_LowStat;
+    MonGroup* m_monGr_shift = nullptr;
+    MonGroup* m_monGr_LowStat = nullptr;
 
     //--------------Pixel stuff--------------------------------------
     ServiceHandle <IInDetConditionsSvc> m_pixelCondSummarySvc;
 
-    bool m_doModules;
-    bool m_doOffline;
+  //bool m_doModules;
+  //bool m_doOffline;
     bool m_isNewLumiBlock;
     bool m_doLumiBlock;
-    bool m_majorityDisabled;
+  //bool m_majorityDisabled;
     bool m_doStatus;
 
 };
