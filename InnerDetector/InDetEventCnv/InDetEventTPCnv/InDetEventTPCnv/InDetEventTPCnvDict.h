3bfb9595b43d23786f56327047f4d483f1ea192a
Nick Barlow <nick.barlow@cern.ch>
2015-09-24 17:10:47 +0200

    add timing info to SCT clusters (InDetEventTPCnv-01-04-01)
    	* Add timing info to SCT clusters (new _p3 persistent class for SCT_Cluster and SCT_ClusterContainer, and corresponding TP converters).
    	* Tag as InDetEventTPCnv-01-04-01

--- InnerDetector/InDetEventCnv/InDetEventTPCnv/InDetEventTPCnv/InDetEventTPCnvDict.h
+++ InnerDetector/InDetEventCnv/InDetEventTPCnv/InDetEventTPCnv/InDetEventTPCnvDict.h
@@ -27,6 +27,7 @@
 #include "InDetEventTPCnv/PixelClusterContainer_p2.h"
 #include "InDetEventTPCnv/PixelClusterContainer_p3.h"
 #include "InDetEventTPCnv/SCT_ClusterContainer_p2.h"
+#include "InDetEventTPCnv/SCT_ClusterContainer_p3.h"
 #include "InDetEventTPCnv/PixelClusterContainer_tlp1.h"
 #include "InDetEventTPCnv/SCT_ClusterContainer_tlp1.h"
 #include "InDetEventTPCnv/TRT_DriftCircleContainer_tlp1.h"
@@ -47,6 +48,7 @@ namespace InDetEventTPCnvDict {
 {
     std::vector< InDet::PixelCluster_p1>   		m_v1;
     std::vector< InDet::SCT_Cluster_p1>   		m_v2;
+    std::vector< InDet::SCT_Cluster_p3>   		m_v2a;
     std::vector< InDet::SiCluster_p1>   		m_v3;
     std::vector< InDet::TRT_DriftCircle_p1 >   		m_v6;
     std::vector< InDet::TRT_DriftCircle_p2 >   		m_v6a;
---
+++ InnerDetector/InDetEventCnv/InDetEventTPCnv/InDetEventTPCnv/SCT_ClusterContainerCnv_p3.h
@@ -0,0 +1,45 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef SCT_CLUSTERCONTAINERCNV_P3_H
+#define SCT_CLUSTERCONTAINERCNV_P3_H
+
+// SCT_DriftCircleContainerCnv_p3, T/P separation of SCT PRD
+// author D.Costanzo <davide.costanzo@cern.ch>
+
+#include "InDetPrepRawData/SCT_ClusterContainer.h"
+#include "AthenaPoolCnvSvc/T_AthenaPoolTPConverter.h"
+#include "InDetEventTPCnv/SCT_ClusterContainer_p3.h"
+
+class SCT_ID;
+namespace InDetDD{ class SCT_DetectorManager;}
+
+class SCT_ClusterContainerCnv_p3 : public T_AthenaPoolTPCnvBase<InDet::SCT_ClusterContainer, InDet::SCT_ClusterContainer_p3>
+{
+ public:
+  SCT_ClusterContainerCnv_p3() : m_isInitialized(0)  {};
+  
+  virtual void	persToTrans(const InDet::SCT_ClusterContainer_p3* persCont,
+			    InDet::SCT_ClusterContainer* transCont,
+			    MsgStream &log) ;
+  virtual void	transToPers(const InDet::SCT_ClusterContainer* transCont,
+			    InDet::SCT_ClusterContainer_p3* persCont,
+			    MsgStream &log) ;
+
+  virtual InDet::SCT_ClusterContainer* createTransient(const InDet::SCT_ClusterContainer_p3* persObj, MsgStream& log);
+  
+
+ private:
+   const SCT_ID *m_sctId;
+   StoreGateSvc *m_storeGate;
+   const InDetDD::SCT_DetectorManager* m_sctMgr;
+   bool m_isInitialized;
+   StatusCode initialize(MsgStream &log);
+
+
+};
+
+#endif
+
+
