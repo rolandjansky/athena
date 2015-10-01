36764ff74091c493fb645c0b2b60a04115d71425
Andrzej Olszewski <andrzej.olszewski@cern.ch>
2015-10-01 07:08:24 +0200

    pre-Run2 cleanup (HIRecExample-01-00-05)
    	* removed DQMonFlags.doJetTagMon=False from HIEaryIncludes.py
    	* removed explicit cutLevel(2) (is set by ID)
    	* tag HIRecExample-01-00-05
    2015-09-24  Andrzej Olszewski <Andrzej.Olszewski@ifj.edu.pl>
    	* cleaned/removed processing of HijingEventPars
    	* moved ESD flags settings from HIEaryIncludes.py to heavyion_flagsESD.py
    	* in trunk, not tagged yet

--- Reconstruction/HeavyIonRec/HIRecExample/share/HIEarlyIncludes.py
+++ Reconstruction/HeavyIonRec/HIRecExample/share/HIEarlyIncludes.py
@@ -1,38 +1,5 @@
-from RecExConfig.RecFlags import rec
-rec.doTrigger=False
-
-#disabling reco algs not needed in HI mode
-rec.doTau = False
-rec.doJetMissingETTag = False
-from JetRec.JetRecFlags import jetFlags
-jetFlags.Enabled = False
-
-from RecExConfig.RecAlgsFlags import recAlgs
-recAlgs.doEFlow = False
-recAlgs.doMissingET = False
-recAlgs.doMissingETSig = False
-
-#need this eventually, but for now it breaks egamma isolation 
-#from CaloRec.CaloRecFlags import jobproperties
-#jobproperties.CaloRecFlags.doCaloTopoCluster=False
-#jobproperties.CaloRecFlags.doCaloEMTopoCluster=False
-
-from HIRecExample.HIRecExampleFlags import jobproperties
-if rec.doESD() :
-    if jobproperties.HIRecExampleFlags.doHIJetRec() and jobproperties.HIRecExampleFlags.doHIegamma() :
-        jobproperties.CaloRecFlags.doCaloCluster=False
-        jobproperties.CaloRecFlags.doEmCluster=False
-        rec.doEgamma=False
-
-from MuonCombinedRecExample.MuonCombinedRecFlags import muonCombinedRecFlags
-muonCombinedRecFlags.doMuGirlLowBeta = False
-
 #do not save AOD cells
 rec.doAODCaloCells =False
-
-from HIRecExample.HIRecExampleFlags import jobproperties
-jobproperties.HIRecExampleFlags.ppMode = True
-
 
 # turn off pp pilup noise suppression
 from CaloTools.CaloNoiseFlags import jobproperties
@@ -43,8 +10,6 @@ if rec.doMonitoring() :
     from AthenaMonitoring.DQMonFlags import DQMonFlags
     DQMonFlags.doMissingEtMon.set_Value_and_Lock(False)
     DQMonFlags.doTauMon.set_Value_and_Lock(False)
-    #change this?
-    DQMonFlags.doJetTagMon.set_Value_and_Lock(False)
 
 #turn off some EventTags
 from RecExConfig.RecFlags import rec
@@ -53,5 +18,3 @@ if rec.doWriteTAG() :
     EventTagFlags.set_AnalysisOff()
     EventTagFlags.set_TauJetOff()
     EventTagFlags.set_MissingETOff()
-
-
