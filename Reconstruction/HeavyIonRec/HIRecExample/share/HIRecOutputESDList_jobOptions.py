1219b38aee33bab4d39ffb3f56392752baa367f3
Andrzej Olszewski <andrzej.olszewski@cern.ch>
2015-10-01 07:08:24 +0200

    pre-Run2 cleanup (HIRecExample-01-00-05)
    	* removed DQMonFlags.doJetTagMon=False from HIEaryIncludes.py
    	* removed explicit cutLevel(2) (is set by ID)
    	* tag HIRecExample-01-00-05
    2015-09-24  Andrzej Olszewski <Andrzej.Olszewski@ifj.edu.pl>
    	* cleaned/removed processing of HijingEventPars
    	* moved ESD flags settings from HIEaryIncludes.py to heavyion_flagsESD.py
    	* in trunk, not tagged yet

--- Reconstruction/HeavyIonRec/HIRecExample/share/HIRecOutputESDList_jobOptions.py
+++ Reconstruction/HeavyIonRec/HIRecExample/share/HIRecOutputESDList_jobOptions.py
@@ -2,13 +2,9 @@ HIESDItemList = []
 
 from HIRecExample.HIRecExampleFlags import jobproperties
 
-if jobproperties.HIRecExampleFlags.withHijingEventPars:
-    include ("HIRecExample/HijingOutputESDList_jobOptions.py")
-
 if jobproperties.HIRecExampleFlags.doHIGlobal:
     from HIGlobal.HIGlobalUtils import AppendOutputList
     AppendOutputList(HIESDItemList)
-
 
 if jobproperties.HIRecExampleFlags.doHIJetRec:
     from HIJetRec.HIJetRecUtils import AppendOutputList
