bd8e38630cc349cc1ca5b7f57aa6fb36b00001a6
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2015-10-05 13:14:34 +0200

    2015-10-05 Mark Hodgkinson         * Root/PFlowPseudoJetGetter.cxx: Bug fix so that tracks can be associated to the primary vertex         * Tag as JetRecTools-00-03-30 (JetRecTools-00-03-30)
    Former-commit-id: b0d36f13b678a6db9104a48a1c16a4a3bafc6dfa

--- Reconstruction/Jet/JetRecTools/src/components/JetRecTools_entries.cxx
+++ Reconstruction/Jet/JetRecTools/src/components/JetRecTools_entries.cxx
@@ -6,30 +6,12 @@
 #include "JetRecTools/TrackVertexAssociationTool.h"
 #include "JetRecTools/TrackPseudoJetGetter.h"
 #include "JetRecTools/PFlowPseudoJetGetter.h"
-#include "JetRecTools/JetConstituentModSequence.h"
-#include "JetRecTools/JetConstituentModifierBase.h"
-#include "JetRecTools/CaloClusterConstituentsWeight.h"
-#include "JetRecTools/CaloClusterConstituentsOrigin.h"
-#include "JetRecTools/SoftKillerWeightTool.h"
-#include "JetRecTools/VoronoiWeightTool.h"
-#include "JetRecTools/ClusterAtEMScaleTool.h"
-#include "JetRecTools/ConstituentSubtractorTool.h"
-#include "JetRecTools/JetInputElRemovalTool.h"
 
 DECLARE_TOOL_FACTORY(JetTrackSelectionTool)
 DECLARE_TOOL_FACTORY(SimpleJetTrackSelectionTool)
 DECLARE_TOOL_FACTORY(TrackVertexAssociationTool)
 DECLARE_TOOL_FACTORY(TrackPseudoJetGetter)
 DECLARE_TOOL_FACTORY(PFlowPseudoJetGetter)
-DECLARE_TOOL_FACTORY(JetConstituentModSequence)
-DECLARE_TOOL_FACTORY(JetConstituentModifierBase)
-DECLARE_TOOL_FACTORY(CaloClusterConstituentsWeight)
-DECLARE_TOOL_FACTORY(CaloClusterConstituentsOrigin)
-DECLARE_TOOL_FACTORY(SoftKillerWeightTool)
-DECLARE_TOOL_FACTORY( VoronoiWeightTool )
-DECLARE_TOOL_FACTORY( ClusterAtEMScaleTool )
-DECLARE_TOOL_FACTORY( ConstituentSubtractorTool )
-DECLARE_TOOL_FACTORY( JetInputElRemovalTool )
 
 DECLARE_FACTORY_ENTRIES(JetRecTools) {
   DECLARE_TOOL(JetTrackSelectionTool)
@@ -37,14 +19,4 @@ DECLARE_FACTORY_ENTRIES(JetRecTools) {
   DECLARE_TOOL(TrackVertexAssociationTool)
   DECLARE_TOOL(TrackPseudoJetGetter)
   DECLARE_TOOL(PFlowPseudoJetGetter)
-  DECLARE_TOOL(JetConstituentModSequence)
-  DECLARE_TOOL(JetConstituentModifierBase)
-  DECLARE_TOOL(CaloClusterConstituentsWeight)
-  DECLARE_TOOL(CaloClusterConstituentsOrigin)
-  DECLARE_TOOL(SoftKillerWeightTool)
-  DECLARE_TOOL( ConstituentSubtractorTool )
-  DECLARE_TOOL( VoronoiWeightTool )
-  DECLARE_TOOL( ClusterAtEMScaleTool )
-  DECLARE_TOOL( JetInputElRemovalTool )
-  
 }
