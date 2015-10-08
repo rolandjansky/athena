af95923206e91e33019627d2c7f9964825b4753c
Jochen Meyer <Jochen.Meyer@cern.ch>
2015-10-08 21:11:57 +0200

    as-built updates (MuonAlignmentData-03-01-03)
    	importing as-built updates OBO Manuel Schreyer
    	tagging 03-01-03

--- MuonSpectrometer/MuonAlignment/MuonAlignmentData/MuonAlignmentData/MdtAsBuiltParams.h
+++ MuonSpectrometer/MuonAlignment/MuonAlignmentData/MuonAlignmentData/MdtAsBuiltParams.h
@@ -19,6 +19,7 @@ class MdtAsBuiltParams : public MuonAlignmentPar {
   // Full constructor
 
     // list of parameters from Florian & al. 
+    // For definitions see: http://atlas-muon-align.web.cern.ch/atlas-muon-align/endplug/asbuilt.pdf
     // XTOMOCHBERNAME	XTOMOSITE	XTOMOSITEID	XTOMOTIME	XTOMOPASSED	XTOMOSIDE	XTOMONBERTUBE1	XTOMONBERTUBE2	XTOMONBERML	XTOMONBERLAYER	XTOMOML1NYTUB	XTOMOML1NZTUB	XTOMOML1NDELY	XTOMOML1NDELZ	XTOMOML1NDELA	XTOMOML1NYPIT	XTOMOML1NZPIT	XTOMOML1PYTUB	XTOMOML1PZTUB	XTOMOML1PDELY	XTOMOML1PDELZ	XTOMOML1PDELA	XTOMOML1PYPIT	XTOMOML1PZPIT	XTOMOML2NYTUB	XTOMOML2NZTUB	XTOMOML2NDELY	XTOMOML2NDELZ	XTOMOML2NDELA	XTOMOML2NYPIT	XTOMOML2NZPIT	XTOMOML2PYTUB	XTOMOML2PZTUB	XTOMOML2PDELY	XTOMOML2PDELZ	XTOMOML2PDELA	XTOMOML2PYPIT	XTOMOML2PZPIT 
     void setParameters(std::string XTOMOCHBERNAME,  	
 		       std::string XTOMOSITE, 	    
@@ -57,8 +58,9 @@ class MdtAsBuiltParams : public MuonAlignmentPar {
 		       float       XTOMOML2PDELZ,   
 		       float       XTOMOML2PDELA,   
 		       float       XTOMOML2PYPIT,   
-		       float       XTOMOML2PZPITbz);
+		       float       XTOMOML2PZPIT);
 
+    // depreciated, use get functions below to retrieve right parameters 
     void getParameters(std::string& XTOMOCHBERNAME,	
 		       std::string& XTOMOSITE, 	
 		       int& XTOMOSITEID, 	
@@ -96,8 +98,29 @@ class MdtAsBuiltParams : public MuonAlignmentPar {
 		       float& XTOMOML2PDELZ,
 		       float& XTOMOML2PDELA,
 		       float& XTOMOML2PYPIT,
-		       float& XTOMOML2PZPITbz);
+		       float& XTOMOML2PZPIT);
+    
+    // get AsBuilt parameters for read out side
+    float getDeltaZ_RO(int multilayer, std::string stationName);
+    float getDeltaY_RO(int multilayer, std::string stationName);
+    float getZPitch_RO(int multilayer, std::string stationName);
+    float getYPitch_RO(int multilayer, std::string stationName);
+    float getDeltaAlpha_RO(int multilayer, std::string stationName);
 
+    // get AsBuilt parameters for high voltage side
+    float getDeltaZ_HV(int multilayer, std::string stationName);
+    float getDeltaY_HV(int multilayer, std::string stationName);
+    float getZPitch_HV(int multilayer, std::string stationName);
+    float getYPitch_HV(int multilayer, std::string stationName);
+    float getDeltaAlpha_HV(int multilayer, std::string stationName);    
+
+    std::string getChamberName();
+    
+    // check if name of MuonStation which accesses AsBuilt parameters is identical to information from detector description database
+    bool checkChamberID(std::string stationTyp, int eta, int phi);
+    // check if X-tomograph measurement was performed for this chamber
+    bool wasTomographed();
+    
 private:
 
     std::string m_XTOMOCHBERNAME;  
@@ -137,7 +160,10 @@ private:
     float       m_XTOMOML2PDELZ;   
     float       m_XTOMOML2PDELA;   
     float       m_XTOMOML2PYPIT;   
-    float       m_XTOMOML2PZPITbz;
+    float       m_XTOMOML2PZPIT;
+    
+    // the multilayer on which the alignment sensors are mounted should remain fixed in space; the other multilayer should be moved relative to this one
+    int whichMLtoMove(std::string stationName);
     
 };
 
