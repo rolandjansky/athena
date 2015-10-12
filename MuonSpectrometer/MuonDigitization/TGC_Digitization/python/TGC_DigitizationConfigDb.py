eb1cc931eaa34f8518e673b299c59ae403e9d408
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-10-12 16:07:58 +0200

    python/TGC_DigitizationConfig.py, python/TGC_DigitizationConfigDb.py - add Tgc_OverlayDigitizationTool CfgGetter methods for Overlay jobs. ATLASSIM-1922. Tagging TGC_Digitization-01-12-07 (TGC_Digitization-01-12-07)
    	* python/TGC_DigitizationConfig.py,
    	python/TGC_DigitizationConfigDb.py - add
    	Tgc_OverlayDigitizationTool CfgGetter methods for Overlay
    	jobs. ATLASSIM-1922.
    	* Tagging TGC_Digitization-01-12-07

--- MuonSpectrometer/MuonDigitization/TGC_Digitization/python/TGC_DigitizationConfigDb.py
+++ MuonSpectrometer/MuonDigitization/TGC_Digitization/python/TGC_DigitizationConfigDb.py
@@ -4,3 +4,4 @@ from AthenaCommon.CfgGetter import addTool
 
 addTool("TGC_Digitization.TGC_DigitizationConfig.TgcDigitizationTool" , "TgcDigitizationTool") 
 addTool("TGC_Digitization.TGC_DigitizationConfig.getTgcRange"         , "TgcRange")
+addTool("TGC_Digitization.TGC_DigitizationConfig.Tgc_OverlayDigitizationTool" , "Tgc_OverlayDigitizationTool") 
