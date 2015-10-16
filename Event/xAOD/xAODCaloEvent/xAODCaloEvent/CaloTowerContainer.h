6bc3aea121571d685734844b72110cf4047efaab
Walter Lampl <Walter.Lampl@cern.ch>
2015-10-16 08:13:15 +0200

    fix warning in debug build (xAODCaloEvent-00-01-15)
    	* fix warning in debug build
    	* tag xAODCaloEvent-00-01-15
    
    2015-10-15 Walter Lampl <walter.lampl@ cern.ch>
    	* Bugfix in CaloTower_v1.cxx
    	* tag xAODCaloEvent-00-01-14
    
    2015-10-15 Walter Lampl <walter.lampl@ cern.ch>
    	* initial version of xAOD::CaloTower(Container)
    	* tag xAODCaloEvent-00-01-13

---
+++ Event/xAOD/xAODCaloEvent/xAODCaloEvent/CaloTowerContainer.h
@@ -0,0 +1,20 @@
+// -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef XAODCALOEVENT_CALOTOWERCONTAINER_H
+#define XAODCALOEVENT_CALOTOWERCONTAINER_H
+
+#include "xAODCaloEvent/versions/CaloTowerContainer_v1.h"
+
+#include "xAODCaloEvent/CaloTower.h"
+
+namespace xAOD {
+  typedef CaloTowerContainer_v1 CaloTowerContainer;  
+}
+
+#include "xAODCore/CLASS_DEF.h"
+CLASS_DEF( xAOD::CaloTowerContainer, 122830719, 1 ) 
+#endif
