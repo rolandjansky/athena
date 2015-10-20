f5be5a0c184a84b047c9a28304cd624332798f3b
Camila Jose Rangel Smith <Camila.Rangel.Smith@cern.ch>
2015-10-20 14:44:25 +0200

    New tag to run in data (TauDQA-00-02-01)

--- PhysicsAnalysis/TauID/TauDQA/src/TauValidationPlots.cxx
+++ PhysicsAnalysis/TauID/TauDQA/src/TauValidationPlots.cxx
@@ -5,22 +5,26 @@
 #include "../share/TauValidationPlots.h"
 
 TauValidationPlots::TauValidationPlots(PlotBase* pParent, std::string sDir, std::string sTauJetContainerName):PlotBase(pParent, sDir),
-													      m_oGeneralTauAllProngsPlots(this, "Matched/", sTauJetContainerName),
+                                                          m_oGeneralTauAllProngsPlots(this, "Matched/", sTauJetContainerName),
+													      m_oNewCoreMatchedPlots(this,"Matched/All/", sTauJetContainerName),             // all tau reco, newCore variables
 													      m_oHad1ProngPlots(this, "Matched/Tau1P/", sTauJetContainerName),             // tau1P plots : variables for tau ID
-													      m_oHad1ProngEVetoPlots(this, "Matched/Tau1P/", sTauJetContainerName),        // tau1P plots : variables for EVeto
 													      m_oHad3ProngPlots(this, "Matched/Tau3P/", sTauJetContainerName),             // tau3P plots : variables for tau ID
+													      m_oMatchedTauAllProngsPlots(this,"Matched/PFOs/", sTauJetContainerName),
+													      m_oMigrationPlots(this,"Matched/Migration/", sTauJetContainerName),             // Migration Matrix
+// all tau reco, no match to truth
 													      m_oElMatchedParamPlots(this, "Electron/", sTauJetContainerName),     // electron veto variables for electrons matching tau candidates	  
 													      m_oElMatchedEVetoPlots(this, "Electron/", sTauJetContainerName),     // electron veto variables for electrons matching tau candidates
 													      m_oFakeGeneralTauAllProngsPlots(this,"Fake/", sTauJetContainerName),  // general tau all fake prongs plots
+													      m_oNewCoreFakePlots(this,"Fake/All/", sTauJetContainerName),             // all tau reco, newCore variables
 													      m_oFakeHad1ProngPlots(this,"Fake/Jet1P/", sTauJetContainerName), 		     // tau1P fake plots : variables for tau ID
 													      m_oFakeHad3ProngPlots(this,"Fake/Jet3P/", sTauJetContainerName),  	       // tau3P fake plots : variables for tau ID
+													      m_oFakeTauAllProngsPlots(this,"Fake/PFOs/", sTauJetContainerName),             // all tau reco, no match to truth
+
 													      m_oRecoGeneralTauAllProngsPlots(this,"RecTau/All/",sTauJetContainerName),// "recTau_General"),  // general tau all fake prongs plots
 													      m_oRecoHad1ProngPlots(this,"RecTau/1P/", sTauJetContainerName),//"recTau_1P"), 		     // tau1P fake plots : variables for tau ID
 													      m_oRecoHad3ProngPlots(this,"RecTau/3P/", sTauJetContainerName),// "recTau_3P"),  	       // tau3P fake plots : variables for tau ID
 													      m_oRecoTauAllProngsPlots(this,"RecTau/PFOs/", sTauJetContainerName),             // all tau reco, no match to truth
-													      m_oNewCorePlots(this,"RecTau/All/", sTauJetContainerName),             // all tau reco, newCore variables
-													      m_oBDTinputPlots(this,"RecTau/BDT/", sTauJetContainerName),             // all tau reco, newCore variables
-													      m_oMigrationPlots(this,"Matched/Migration/", sTauJetContainerName)             // Migration Matrix
+													      m_oNewCorePlots(this,"RecTau/All/", sTauJetContainerName)             // all tau reco, newCore variables
 													      
 													      
 {}	
