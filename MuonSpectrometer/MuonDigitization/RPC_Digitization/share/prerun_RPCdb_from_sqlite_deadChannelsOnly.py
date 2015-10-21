89f21bf5f6ed163b172207853c4f533c2d38f300
Stefania Spagnolo <stefspa@cern.ch>
2015-10-21 17:22:48 +0200

    Fixed a small bug in the strip assignement on side C if CS >1; test production job-options added (RPC_Digitization-01-05-16)
    	* tagging RPC_Digitization-01-05-16
    	* src/RpcDigitizationTool.cxx (PhysicalClusterSize): fix a small bug affecting digit to hit position in side C when cluster size >1
    	* added share/preIncludeAverageEffScan_step1-5.py for test productions
    	1/Scale=1,00860215053763 (step1)
    	Eff(phi&eta) =0.930,  Eff(phionly)=0.0218   and Eff(etaonly)=0.0218
    	1/Scale=1,0195652173913 (step2)
    	Eff(phi&eta) =0.920,  Eff(phionly)=0.0216   and Eff(etaonly)=0.0216
    	1/Scale=1,03076923076923 (step3)
    	Eff(phi&eta) =0.910,  Eff(phionly)=0.0213   and Eff(etaonly)=0.0213
    	1/Scale=1,04222222222222 (step4)
    	Eff(phi&eta) =0.900,  Eff(phionly)=0.0211   and Eff(etaonly)=0.0211
    	1/Scale=1,05393258426966 (step5)
    	Eff(phi&eta) =0.890,  Eff(phionly)=0.0209   and Eff(etaonly)=0.0209
    	Final panel/gap eff (step5) are
    	Eff panel=0.9109 / Eff Gap=0.9318 (while MC15a had ... )
    
    	* tagging RPC_Digitization-01-05-15
    ...
    (Long ChangeLog diff - truncated)

---
+++ MuonSpectrometer/MuonDigitization/RPC_Digitization/share/prerun_RPCdb_from_sqlite_deadChannelsOnly.py
@@ -0,0 +1,49 @@
+useLocalSql = True
+if useLocalSql:
+    from IOVDbSvc.IOVDbSvcConf import IOVDbSvc
+    from IOVDbSvc.CondDB import conddb
+    include("RegistrationServices/IOVRegistrationSvc_jobOptions.py")
+
+    from MuonCondSvc.MuonCondSvcConf import RPC_STATUSConditionsSvc
+    ServiceMgr +=RPC_STATUSConditionsSvc()
+
+    from MuonCondTool.MuonCondToolConf import RpcDetectorStatusDbTool
+    RpcDetectorStatusDbTool = RpcDetectorStatusDbTool("RpcDetectorStatusDbTool")
+    RpcDetectorStatusDbTool.OutputLevel = INFO 
+    RpcDetectorStatusDbTool.RpcDqFolder = "/OFFLINE/FINAL"
+    ToolSvc += RpcDetectorStatusDbTool
+
+    dbConn_Rpc="sqlite://;schema=RPCConditionDB_271595_atLeast1TrackOnPanel.db;dbname=RPC_DQA"
+    folder_Rpc="/OFFLINE/FINAL"
+    
+    ServiceMgr.IOVDbSvc.dbConnection=dbConn_Rpc
+    ServiceMgr.IOVDbSvc.Folders+=[folder_Rpc+"<tag>RecoCondDB</tag> <dbConnection>"+dbConn_Rpc+"</dbConnection>"]
+
+
+
+from AthenaCommon import CfgGetter
+from AthenaCommon.CfgGetter import getPublicTool, getPrivateTool, getService, getAlgorithm
+RPC_DigitizationTool=getPublicTool("RpcDigitizationTool")
+#RPC_DigitizationTool.ValidationSetup = 1   
+RPC_DigitizationTool.IgnoreRunDependentConfig = True
+RPC_DigitizationTool.Force_BOG_BOF_DoubletR2_OFF    = False
+RPC_DigitizationTool.Efficiency_fromCOOL      = True  # dead strips from COOL          
+RPC_DigitizationTool.ClusterSize_fromCOOL     = True  
+RPC_DigitizationTool.RPCInfoFromDb            = True
+RPC_DigitizationTool.CutProjectedTracks = 999999999; #always apply python eff. and cs values; dead strips from COOL
+#RPC_DigitizationTool.CutProjectedTracks = 1;         #always apply COOL   eff. and cs values; dead strips from COOL
+#RPC_DigitizationTool.OutputLevel=DEBUG
+RPC_DigitizationTool.OutputLevel=INFO
+RPC_DigitizationTool.DumpFromDbFirst=True
+
+
+svcMgr.MessageSvc.debugLimit    = 999999999
+
+RpcDetectorStatusDbTool=getPublicTool("RpcDetectorStatusDbTool")
+RpcDetectorStatusDbTool.OutputLevel=INFO
+
+
+from AthenaCommon.AppMgr import ServiceMgr as svcMgr
+svcMgr.MessageSvc.Format = "% F%50W%S%7W%R%T %0W%M"
+svcMgr.MessageSvc.defaultLimit = 99999999
+
