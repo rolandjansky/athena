ba805501816344da0a2a5268c92a26dfe468fb11
Walter Lampl <Walter.Lampl@cern.ch>
2015-10-22 14:59:54 +0200

    change output level (LArCondUtils-00-03-30)
    	* Change output level to reduce log-length
    	* Tag  LArCondUtils-00-03-30
    
    2015-09-24  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArCondUtils-00-03-29.
    	* src/LArFEBTempTool.cxx: Fix clang warning.
    	* LArCondUtils/LArFEBTempTool.h,
    	LArCondUtils/LArFecLvTempDcsTool.h: Don't have using namespace in
    	headers.
    
    2015-06-22  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArCondUtils-00-03-28.
    	* Avoid redundant divisions.
    
    2015-02-17  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArCondUtils-00-03-27.
    	* src/LArHVToolMC.cxx (InitHV): Coverity warning fix: check return
    ...
    (Long ChangeLog diff - truncated)

--- LArCalorimeter/LArCondUtils/src/LArFEBTempTool.cxx
+++ LArCalorimeter/LArCondUtils/src/LArFEBTempTool.cxx
@@ -50,13 +50,13 @@ StatusCode LArFEBTempTool::initialize()
 
 FEBTemp LArFEBTempTool::getFebTemp( const HWIdentifier& id ) 
 {
-   FEBTemp m_temp;
-   m_temp.clear();
+   FEBTemp temp;
+   temp.clear();
 
    if(!m_isinit) {
      if ( detStore()->retrieve(m_atrlistcol,m_foldername).isFailure() ) {
        ATH_MSG_ERROR ( "Unable to retrieve AttrListCollection "<<m_foldername);
-       return m_temp;
+       return temp;
      }
      ATH_MSG_DEBUG ( "Successfully retrieved AttrListCollection" );
      m_isinit = true;
@@ -72,11 +72,11 @@ FEBTemp LArFEBTempTool::getFebTemp( const HWIdentifier& id )
              if(! ((*citr).second)["temp1"].isNull()) temp1 = (((*citr).second)["temp1"]).data<float>();
 	     float temp2 =-1;
              if(! ((*citr).second)["temp2"].isNull()) temp2 = (((*citr).second)["temp2"]).data<float>();
-	     m_temp.push_back(std::make_pair(temp1,temp2));
+	     temp.push_back(std::make_pair(temp1,temp2));
 	  }
      }
 
-   return m_temp;
+   return temp;
 }
 
 
