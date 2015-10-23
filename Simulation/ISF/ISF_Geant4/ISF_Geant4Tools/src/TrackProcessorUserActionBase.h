a841a869e59ec890986806a984741613c9a3a48e
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2015-10-23 14:23:30 +0200

    merging changes from ISF_Geant4Tools-00-02-37-14->ISF_Geant4Tools-00-02-37-18 into this branch (ATLASSIM-2304) (ISF_Geant4Tools-00-03-04-07)
    	* merging changes from ISF_Geant4Tools-00-02-37-14->ISF_Geant4Tools-00-02-37-18 into this branch (ATLASSIM-2304), details:
    
    	* removing std::map to associate G4Tracks with ISFParticles
    	  (ATLASSIM-2263, ATLASSIM-1868)
    	** the map was replaced by code in the TrackProcessorUserActionBase
    	   which calls ISF::ISFG4Helpers::linkG4TrackToISFParticle(..) for all
    	   new G4Tracks to propergate the ISFParticle pointer of the parent
    	   G4Track to all its G4 secondaries
    	* python/ISF_Geant4ToolsConfig(Db).py - removing obsolete
    	  'QuasiStableG4TrackProcessorUserAction' configuration (switched to
    	  'FullG4TrackProcessorUserAction' instead)
    	* src/ISFG4Helpers.h/cxx - adding currently unused
    	  iGeant4::ISFG4Helpers::linkG4TrackToISFParticle(..) method (to be used in
    	  next iteration of ATLASSIM-2263)
    	* src/TrackProcessorUserAction*.h/cxx, python/ISF_Geant4ToolsConfig.py -
    	  major code refactoring (ATLASSIM-2263)
    	  - splitting TrackProcessorUserAction into separate FullG4 and
    	    PassBack implementations
    	  - using one common TrackProcessorUserActionBase implementation for
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/TrackProcessorUserActionBase.h
@@ -0,0 +1,48 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef ISF_GEANT4TOOLS_TRACKPROCESSORUSERACTIONBASE_H
+#define ISF_GEANT4TOOLS_TRACKPROCESSORUSERACTIONBASE_H
+
+#include "ISF_Geant4Interfaces/ITrackProcessorUserAction.h"
+
+#include <string>
+
+// forward declarations
+namespace ISF {
+  class ISFParticle;
+}
+
+namespace iGeant4 {
+
+  class TrackProcessorUserActionBase: virtual public ITrackProcessorUserAction {
+
+  public:
+    TrackProcessorUserActionBase(const std::string& name);
+    virtual ~TrackProcessorUserActionBase() {}
+
+    void BeginOfEventAction(const G4Event*);
+    void EndOfEventAction(const G4Event*);
+    void BeginOfRunAction(const G4Run*);
+    void EndOfRunAction(const G4Run*);
+
+    void SteppingAction(const G4Step*);
+
+    void PreUserTrackingAction(const G4Track* aTrack);
+    void PostUserTrackingAction(const G4Track* aTrack);
+
+    /** This method is called by TrackProcessorUserActionBase after the
+     *  G4Track->ISFParticle association has been established for the current G4Track */
+    virtual void ISFSteppingAction(const G4Step*, ISF::ISFParticle *curISP) = 0;
+
+  private:
+    /** for keeping track of the currently processed G4Track and its corresponding ISFParticle */
+    int                                 m_curTrackID;  //!< the TrackID of the currently processed G4Track
+    ISF::ISFParticle                   *m_curISP;      //!< the corresponding ISFParticle to this G4Track
+
+  };
+
+}
+
+#endif // ISF_GEANT4TOOLS_TRACKPROCESSORUSERACTIONBASE_H
