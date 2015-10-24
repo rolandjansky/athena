0dcc5a0682248b717e1d56cb313a136cae047da4
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-10-24 16:26:51 +0200

    Add G4AtlasInterfaces/IG4GeometryNotifierSvc.h - new interface for service which handles G4Notifier creation and passes parent volume names to the notifiers when they are called. G4AtlasInterfaces/IDetectorGeometrySvc.h - remove methods which are now provided by IG4GeometryNotifierSvc. ATLASSIM-2138 Tagging G4AtlasInterfaces-00-00-28-02 (G4AtlasInterfaces-00-00-28-02)
    	* add G4AtlasInterfaces/IG4GeometryNotifierSvc.h - new interface
    	for service which handles G4Notifier creation and passes parent
    	volume names to the notifiers when they are called. ATLASSIM-2138
    	* G4AtlasInterfaces/IDetectorGeometrySvc.h - remove methods which
    	are now provided by IG4GeometryNotifierSvc. ATLASSIM-2138
    	* tagging G4AtlasInterfaces-00-00-28-02
    
    2015-10-06  John Chapman  <John.Chapman@cern.ch>
    
    	* add G4AtlasInterfaces/IUserLimitsTool.h - interface for new
    	UserLimitsTool. ATLASSIM-2254
    	* tagging G4AtlasInterfaces-00-00-28-01
    
    	* G4AtlasInterfaces/IAddPhysicsDecayTool.h,
    	G4AtlasInterfaces/IPhysicsListTool.h,
    	G4AtlasInterfaces/IPhysicsOptionTool.h - whitespace tidying.
    	* tagging G4AtlasInterfaces-00-00-28
    
    2015-07-07 Andrea Di Simone <andrea.di.simone@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/G4Atlas/G4AtlasInterfaces/G4AtlasInterfaces/IG4GeometryNotifierSvc.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef G4ATLASINTERFACES_IG4GEOMETRYNOTIFIERSVC_H
+#define G4ATLASINTERFACES_IG4GEOMETRYNOTIFIERSVC_H
+
+#include "GaudiKernel/IService.h"
+
+#include <vector>
+#include <string>
+
+/** @class IG4GeometryNotifierSvc IG4GeometryNotifierSvc.h "G4AtlasInterfaces/IG4GeometryNotifierSvc.h"
+ *
+ *  Abstract interface to service for Geant4 geometry notifier classes
+ */
+
+class IG4GeometryNotifierSvc : virtual public IService {
+ public:
+  IG4GeometryNotifierSvc() {}
+  virtual ~IG4GeometryNotifierSvc() {}
+  static const InterfaceID& interfaceID() {
+    static const InterfaceID IID_IG4GeometryNotifierSvc( "IG4GeometryNotifierSvc" , 1 , 0 ) ;
+    return IID_IG4GeometryNotifierSvc ;
+  }
+  virtual void SetCurrentDetectorName(std::string) = 0;
+
+  virtual const std::string GetCurrentDetectorName() const = 0;
+};
+
+#endif
