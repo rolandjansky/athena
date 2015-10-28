49ce09354f7651feebc83f55bdfd5afcc72a8e7e
Andrew Haas <drandyhaas@gmail.com>
2015-10-28 15:56:36 +0100

    'send digit to drift function tool, for overlay, to tell if it's a mc digit or not' (TRT_DriftCircleTool-04-00-00-01)

--- InnerDetector/InDetRecTools/TRT_DriftCircleTool/src/TRT_DriftCircleTool.cxx
+++ InnerDetector/InDetRecTools/TRT_DriftCircleTool/src/TRT_DriftCircleTool.cxx
@@ -288,7 +288,7 @@ InDet::TRT_DriftCircleCollection* InDet::TRT_DriftCircleTool::convert(int Mode,c
       if( newtdcvalue==0 || newtdcvalue==24 ) {
         isOK=false;
       } else {
-        radius    = m_driftFunctionTool->driftRadius(rawTime,id,t0,isOK);
+        radius    = m_driftFunctionTool->driftRadius(rawTime,id,t0,isOK,word);
         driftTime = rawTime-t0;
       }
 
@@ -311,7 +311,7 @@ InDet::TRT_DriftCircleCollection* InDet::TRT_DriftCircleTool::convert(int Mode,c
       std::vector<Identifier>    dvi                                   ;
       double error=0;
 
-      if(Mode<2) error = m_driftFunctionTool->errorOfDriftRadius(driftTime,id);
+      if(Mode<2) error = m_driftFunctionTool->errorOfDriftRadius(driftTime,id,word);
 
       if( !isOK || (error==0.&&Mode<2) ) //Drifttime out of range. Make wirehit
 	{
