a776f604d50122fd86f2e8433cfb6658e8d73526
Gabriele Chiodini <Gabriele.Chiodini@cern.ch>
2015-10-30 14:35:36 +0100

    fix unchecked code for eventInfo (RpcRawDataMonitoring-01-02-27)
            *   Tagging RpcRawDataMonitoring-01-02-27 fix unchecked code for eventInfo
    
    2015-25-10  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-26 fix y label lumiblock plot
    
    2015-25-10  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-25 Added Eff Trigger, Matched Muons, DQ Coin plots, mergepointdistance plot, fix shiftetapanel.
    
    2015-24-10  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-24 Added Eff Trigger, Matched Muons, DQ Coin plots, mergepointdistance plot.
    
    2015-24-10  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-23 Added Eff Trigger, Matched Muons, DQ Coin plots.
    
    2015-26-09  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-22 comment out few debugging lines.
    
    2015-25-09  Gabriele Chiodini gabriele.chiodini@le.infn.it
            *   Tagging RpcRawDataMonitoring-01-02-21 fixed coverity errors
    
    ...
    (Long ChangeLog diff - truncated)

--- MuonSpectrometer/MuonValidation/MuonDQA/MuonRawDataMonitoring/RpcRawDataMonitoring/share/RpcRawESD_MonitoringOptions.py
+++ MuonSpectrometer/MuonValidation/MuonDQA/MuonRawDataMonitoring/RpcRawDataMonitoring/share/RpcRawESD_MonitoringOptions.py
@@ -95,6 +95,20 @@ rpcLv1RawMonMan.AthenaMonTools += [ rpcLv1RawDataEfficiency ]
 topSequence += rpcLv1RawMonMan
 
 
+############# RpcLv1RawDataSectorLogic #############
+rpcLv1SLRawMonMan = AthenaMonManager(name="RpcLv1SLRawMonManager",
+                                     FileKey             = DQMonFlags.monManFileKey(),
+                                     Environment         = DQMonFlags.monManEnvironment(),
+                                     OutputLevel         = muonOutputLevel)
+############# RPCLV1RawDataValAlg #############
+from RpcRawDataMonitoring.RpcRawDataMonitoringConf import RpcLv1RawDataSectorLogic
+rpcLV1RawDataSectorLogic = RpcLv1RawDataSectorLogic(name='rpcLV1RawDataSectorLogic',OutputLevel = OutputMessageLevel)
+
+ToolSvc += rpcLV1RawDataSectorLogic
+rpcLv1SLRawMonMan.AthenaMonTools += [ rpcLV1RawDataSectorLogic ]
+topSequence += rpcLv1SLRawMonMan
+print rpcLv1SLRawMonMan
+
 ###################################################################
 #
 ##--------------------------------------------------------------
