5f36148294e9bac861c7548e8ca1e74636732cd9
Peter Alan Steinberg <peter.steinberg@bnl.gov>
2015-11-02 06:33:26 +0100

    'small change to adapt to xAOD changes (zdcModule)' (ZdcRec-00-02-08)

---
+++ ForwardDetectors/ZDC/ZdcRec/python/ZdcRecUtils.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+def AppendOutputList(HIAODItemList) :
+    """Adds HIGlobalAODOutputList to the list passed in as an argument"""
+    HIAODItemList+= ["xAOD::TriggerTowerContainer#ZdcTriggerTowers",
+                     "xAOD::TriggerTowerAuxContainer#ZdcTriggerTowersAux.",
+                     "xAOD::ZdcModuleContainer#ZdcModules",
+                     "xAOD::ZdcModuleAuxContainer#ZdcModulesAux."] 
