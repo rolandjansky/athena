07f1838e32d29c455ae7dc92c79be41c3ddba994
Lidija Zivkovic <Lidija.Zivkovic@cern.ch>
2015-11-04 16:30:10 +0100

    Fix for dz cut and monitoring update (TrigBjetHypo-02-00-61)
            * Fiz for dz cut for mu-jet and monitoring
            * Tagging as TrigBjetHypo-02-00-61

--- Trigger/TrigHypothesis/TrigBjetHypo/TrigBjetHypo/TrigLeptonJetMatchAllTE.h
+++ Trigger/TrigHypothesis/TrigBjetHypo/TrigBjetHypo/TrigLeptonJetMatchAllTE.h
@@ -55,6 +55,12 @@ class TrigLeptonJetMatchAllTE: public HLT::AllTEAlgo {
   float m_deltaPhiPass;
   float m_muonEFEta;
   float m_muonEFPhi;
+  float m_deltaRPass;
+  float m_deltaRAll;
+  float m_deltaZPass;
+  float m_deltaZAll;
+ 
+
 
   HLT::ErrorCode getPrmVtxCollection(const xAOD::VertexContainer*& pointerToEFPrmVtxCollections, const HLT::TriggerElement* outputTE, std::string priVtxKey);
   HLT::ErrorCode getTrackCollection(const xAOD::TrackParticleContainer*& pointerToEFTrackCollections, const HLT::TriggerElement* outputTE);
