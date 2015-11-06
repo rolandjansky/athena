a53579b08eb543a5de993d643a9c02533fa92125
Goetz Gaycken <Goetz.Gaycken@cern.ch>
2015-11-06 19:21:31 +0100

    Renamed TRTLocalOccupancy to TRTTrackOccupancy. (TrkParticleCreator-01-01-30)

---
+++ Tracking/TrkTools/TrkParticleCreator/doc/mainpage.h
@@ -0,0 +1,20 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage TrkParticleCreator
+
+@author Andreas.Wildauer@cern.ch
+
+@section Introduction
+This package contains the Trk::ParticleCreatorTool which is used (mainly) by the InDet::ParticleCreator of the
+package InDetParticleCreation to construct Rec::TrackParticle objects.
+
+@section UsedPackages
+@htmlinclude used_packages.html
+
+@section Requirements
+@include requirements
+
+*/
