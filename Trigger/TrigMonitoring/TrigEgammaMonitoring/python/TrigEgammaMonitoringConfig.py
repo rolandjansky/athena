555c2bc3c1fe490672ebcbd60f57339f0733b899
Avishek Chatterjee <avishek@cern.ch>
2015-11-11 16:03:07 +0100

    Add non-ion electron triggers. (TrigEgammaMonitoring-00-01-59)

--- Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
@@ -5,10 +5,13 @@ def TrigEgammaMonitoringTool():
   from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaNavAnalysisTool,TrigEgammaNavTPAnalysisTool
 
   probelist = ['e15_loose_ion','e15_etcut_ion','e15_medium_ion']
+  probelist += ['e15_loose','e15_etcut','e15_medium','e15_lhloose','e15_lhmedium']
   probelist += ['g20_loose_ion','g20_etcut_ion','g20_loose','g20_etcut']
   probelist += ['L1_EM10','L1_EM12','L1_EM15']
   probelistTP = ['e15_loose_ion','e15_etcut_ion','e15_medium_ion']
+  probelistTP += ['e15_loose','e15_etcut','e15_medium','e15_lhloose','e15_lhmedium']
   tagItems = ['e15_loose_ion','e15_medium_ion']
+  tagItems += ['e15_loose','e15_medium','e15_lhloose','e15_lhmedium']
   basePath = '/HLT/Egamma/'
 
   Analysis = TrigEgammaNavAnalysisTool(name='NavAnalysis',
