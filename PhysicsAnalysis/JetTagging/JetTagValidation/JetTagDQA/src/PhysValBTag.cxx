44d6aee3b0402cd6a60d97e34cb83d8a1c4cee6b
Andrea Sciandra <andrea.sciandra@cern.ch>
2015-11-12 13:29:34 +0100

    Added isData python flag (JetTagDQA-00-00-14)

--- PhysicsAnalysis/JetTagging/JetTagValidation/JetTagDQA/src/PhysValBTag.cxx
+++ PhysicsAnalysis/JetTagging/JetTagValidation/JetTagDQA/src/PhysValBTag.cxx
@@ -7,7 +7,7 @@
 // PhysValBTag.cxx
 // Implementation file for class PhysValBTag
 // Author: E.Schopf<elisabeth.schopf@cern.ch>
-/////////////////////////////////////////////////////////////////// 
+///////////////////////////////////////////////////////////////////
 // JetTagDQA includes
 #include "PhysValBTag.h"
 
@@ -17,9 +17,9 @@
 // FrameWork includes
 #include "GaudiKernel/IToolSvc.h"
 #include "xAODJet/JetContainer.h"
-#include "xAODTracking/TrackParticle.h" 
-#include "xAODTracking/Vertex.h" 
-#include "xAODBTagging/BTagging.h" 
+#include "xAODTracking/TrackParticle.h"
+#include "xAODTracking/Vertex.h"
+#include "xAODBTagging/BTagging.h"
 //#include "../../Event/xAOD/xAODBTagging/xAODBTagging/BTagging.h"
 
 #include "AthenaBaseComps/AthCheckMacros.h"
@@ -27,179 +27,192 @@
 
 namespace JetTagDQA {
 
-/////////////////////////////////////////////////////////////////// 
-// Public methods: 
-/////////////////////////////////////////////////////////////////// 
+  ///////////////////////////////////////////////////////////////////
+  // Public methods:
+  ///////////////////////////////////////////////////////////////////
 
-// Constructors
-////////////////
+  // Constructors
+  ////////////////
 
-  PhysValBTag::PhysValBTag( const std::string& type, 
-				  const std::string& name, 
-				  const IInterface* parent ) : 
+  PhysValBTag::PhysValBTag( const std::string& type,
+                            const std::string& name,
+                            const IInterface* parent ) :
     ManagedMonitorToolBase( type, name, parent ),
-//   m_jetPlots(0, "Summary/Jet/", "Jet"),
-//    m_trkvtxPlots(0, "Summary/TrackAndVertex/"),
+    //   m_jetPlots(0, "Summary/Jet/", "Jet"),
+    //    m_trkvtxPlots(0, "Summary/TrackAndVertex/"),
+    m_isData(false),
     m_antiKt4EMTopoPlots(0, "BTag/AntiKt4EMTopo/", "antiKt4EMTopo"),
     m_antiKt4LCTopoPlots(0, "BTag/AntiKt4LCTopo/", "antiKt4LCTopo"),
-//    m_antiKt4TruthPlots(0, "BTag/AntiKt4Truth/", "antiKt4Truth"),
+    //    m_antiKt4TruthPlots(0, "BTag/AntiKt4Truth/", "antiKt4Truth"),
     m_antiKt4TruthWZPlots(0, "BTag/AntiKt4TruthWZ/", "antiKt4TruthWZ"),
     m_antiKt10LCTopoPlots(0, "BTag/AntiKt10LCTopo/", "antiKt10LCTopo"),
-//    m_antiKt10TruthPlots(0, "BTag/AntiKt10Truth/", "antiKt10Truth"),
+    //    m_antiKt10TruthPlots(0, "BTag/AntiKt10Truth/", "antiKt10Truth"),
     m_antiKt10TruthWZPlots(0, "BTag/AntiKt10TruthWZ/", "antiKt10TruthWZ"),
+    m_antiKt2PV0TrackJetPlots(0, "BTag/AntiKt2PV0TrackJets/", "antiKt2PV0TrackJets"),
     m_antiKt3PV0TrackJetPlots(0, "BTag/AntiKt3PV0TrackJets/", "antiKt3PV0TrackJets"),
     m_antiKt4PV0TrackJetPlots(0, "BTag/AntiKt4PV0TrackJets/", "antiKt4PV0TrackJets")
   {
-    
+
+    declareProperty( "isData", m_isData );
+
     declareProperty( "JetContainerName1", m_jetName1 = "AntiKt4EMTopoJets" );
-    declareProperty( "JetContainerName2", m_jetName2 = "AntiKt4LCTopoJets"); 
-//    declareProperty( "JetContainerName3", m_jetName3 = "AntiKt4TruthJets"); 
-    declareProperty( "JetContainerName4", m_jetName4 = "AntiKt4TruthWZJets"); 
-    declareProperty( "JetContainerName5", m_jetName5 = "AntiKt10LCTopoJets"); 
-//    declareProperty( "JetContainerName6", m_jetName6 = "AntiKt10TruthJets"); 
-    declareProperty( "JetContainerName7", m_jetName7 = "AntiKt10TruthWZJets"); 
-    declareProperty( "JetContainerName8", m_jetName8 = "AntiKt3PV0TrackJets"); 
-    declareProperty( "JetContainerName9", m_jetName9 = "AntiKt4PV0TrackJets"); 
-    declareProperty( "TrackContainerName", m_trackName = "InDetTrackParticles" );  
-    declareProperty( "VertexContainerName", m_vertexName = "PrimaryVertices" );  
+    declareProperty( "JetContainerName2", m_jetName2 = "AntiKt4LCTopoJets");
+    //    declareProperty( "JetContainerName3", m_jetName3 = "AntiKt4TruthJets");
+    declareProperty( "JetContainerName4", m_jetName4 = "AntiKt4TruthWZJets");
+    declareProperty( "JetContainerName5", m_jetName5 = "AntiKt10LCTopoJets");
+    //    declareProperty( "JetContainerName6", m_jetName6 = "AntiKt10TruthJets");
+    declareProperty( "JetContainerName7", m_jetName7 = "AntiKt10TruthWZJets");
+    declareProperty( "JetContainerName8", m_jetName6 = "AntiKt2PV0TrackJets");
+    declareProperty( "JetContainerName8", m_jetName8 = "AntiKt3PV0TrackJets");
+    declareProperty( "JetContainerName9", m_jetName9 = "AntiKt4PV0TrackJets");
+    declareProperty( "TrackContainerName", m_trackName = "InDetTrackParticles" );
+    declareProperty( "VertexContainerName", m_vertexName = "PrimaryVertices" );
   }
-  
+
   // Destructor
   ///////////////
   PhysValBTag::~PhysValBTag()
   {}
-  
+
   // Athena algtool's Hooks
   ////////////////////////////
   StatusCode PhysValBTag::initialize()
   {
-	ATH_MSG_INFO ("Initializing " << name() << "...");    
+	ATH_MSG_INFO ("Initializing " << name() << "...");
 	ATH_CHECK(ManagedMonitorToolBase::initialize());
 
 	m_btagplots.insert(std::make_pair(m_jetName1, m_antiKt4EMTopoPlots));
 	m_btagplots.insert(std::make_pair(m_jetName2, m_antiKt4LCTopoPlots));
-//	m_btagplots.insert(std::make_pair(m_jetName3, m_antiKt4TruthPlots));
-	m_btagplots.insert(std::make_pair(m_jetName4, m_antiKt4TruthWZPlots));
+	// if(!m_isData) m_btagplots.insert(std::make_pair(m_jetName3, m_antiKt4TruthPlots));
+	if(!m_isData) m_btagplots.insert(std::make_pair(m_jetName4, m_antiKt4TruthWZPlots));
 	m_btagplots.insert(std::make_pair(m_jetName5, m_antiKt10LCTopoPlots));
-//	m_btagplots.insert(std::make_pair(m_jetName6, m_antiKt10TruthPlots));
-	m_btagplots.insert(std::make_pair(m_jetName7, m_antiKt10TruthWZPlots));
+	// if(!m_isData) m_btagplots.insert(std::make_pair(m_jetName6, m_antiKt10TruthPlots));
+	if(!m_isData) m_btagplots.insert(std::make_pair(m_jetName7, m_antiKt10TruthWZPlots));
+	m_btagplots.insert(std::make_pair(m_jetName6, m_antiKt2PV0TrackJetPlots));
 	m_btagplots.insert(std::make_pair(m_jetName8, m_antiKt3PV0TrackJetPlots));
 	m_btagplots.insert(std::make_pair(m_jetName9, m_antiKt4PV0TrackJetPlots));
-//	m_nevents = 0;		
+    //	m_nevents = 0;
     return StatusCode::SUCCESS;
   }
-  
+
   StatusCode PhysValBTag::book(PlotBase& plots)
   {
     plots.initialize();
     std::vector<HistData> hists = plots.retrieveBookedHistograms();
-    
+
     for (auto& hist : hists){
       ATH_MSG_INFO ("Initializing " << hist.first << " " << hist.first->GetName() << " " << hist.second << "...");
       ATH_CHECK(regHist(hist.first,hist.second,all));
     }
-    return StatusCode::SUCCESS;      
+    return StatusCode::SUCCESS;
   }
 
   StatusCode PhysValBTag::bookHistograms()
- {
-      ATH_MSG_INFO ("Booking hists " << name() << "...");      
+  {
+    ATH_MSG_INFO ("Booking hists " << name() << "...");
 
-      if (m_detailLevel >= 10) {
+    if (m_detailLevel >= 10) {
 
-	for(std::map<std::string,JetTagDQA::BTaggingValidationPlots>::iterator plot_i = m_btagplots.begin(); plot_i != m_btagplots.end(); ++plot_i){
-//		ATH_CHECK(book(m_jetPlots));		
+      for(std::map<std::string,JetTagDQA::BTaggingValidationPlots>::iterator plot_i = m_btagplots.begin(); plot_i != m_btagplots.end(); ++plot_i){
+        //		ATH_CHECK(book(m_jetPlots));
 		ATH_CHECK(book(plot_i->second));
-// 		ATH_CHECK(book(m_trkvtxPlots));
-	}
+        // 		ATH_CHECK(book(m_trkvtxPlots));
       }
+    }
 
-      return StatusCode::SUCCESS;      
+    return StatusCode::SUCCESS;
   }
 
   StatusCode PhysValBTag::fillHistograms()
   {
 	ATH_MSG_INFO ("Filling hists " << name() << "...");
-   
+
 	if (m_detailLevel < 10) return StatusCode::SUCCESS;
 
 	++m_nevents;
 	//int njets = 0;
-	//std::cout << "Number of proccessed events = " << m_nevents << std::endl; 
+	//std::cout << "Number of proccessed events = " << m_nevents << std::endl;
 
 	// Jets
 	for(std::map<std::string,JetTagDQA::BTaggingValidationPlots>::iterator plot_i = m_btagplots.begin(); plot_i != m_btagplots.end(); ++plot_i){
-		const xAOD::JetContainer* jets(0);
-    		ATH_CHECK(evtStore()->retrieve(jets, plot_i->first));
+      const xAOD::JetContainer* jets(0);
+      // ATH_CHECK(evtStore()->retrieve(jets, plot_i->first));
+      StatusCode jetException = evtStore()->retrieve(jets, plot_i->first);
+    // If StatusCode is not SUCCESS, no jet collection with this name, continue loop
+    if (jetException != StatusCode::SUCCESS)
+      continue;
 
-    		for (auto jet : *jets) {
 
-//			++njets;
-//     			m_jetPlots.fill(jet);
-			//std::cout << "number of processed jet: " << njets << std::endl; 
+    for (auto jet : *jets) {
 
-			//btagging
-			const xAOD::BTagging* btag =  jet->btagging();
+        //			++njets;
+        //     			m_jetPlots.fill(jet);
+        //std::cout << "number of processed jet: " << njets << std::endl;
 
-     			int label(1000);
-			//double dR(1000);
- 		 		
-			if(jet->pt() > 20000 && abs(jet->eta()) < 2.5){ 	   
-				(plot_i->second).fill(jet);  
+        //btagging
+        const xAOD::BTagging* btag =  jet->btagging();
 
-			        if(jet->isAvailable<int>("ConeTruthLabelID")) label = jetFlavourLabel(jet, xAOD::JetFlavourLabelType::GAFinalHadron);
-		                else jet->getAttribute("TruthLabelID",label);	
+        int label(1000);
+        //double dR(1000);
 
-				(plot_i->second).fill(label);
-     				if (btag){
-		 			(plot_i->second).fill(btag);
-					(plot_i->second).fill(jet, btag);
-				}
-			}
-     		}
+        if(jet->pt() > 20000 && abs(jet->eta()) < 2.5){
+          (plot_i->second).fill(jet);
+
+	  if(!m_isData) {
+	    if(jet->isAvailable<int>("ConeTruthLabelID")) label = jetFlavourLabel(jet, xAOD::JetFlavourLabelType::GAFinalHadron);
+	    else jet->getAttribute("TruthLabelID",label);
+	  }
+
+          (plot_i->second).fill(label);
+          if (btag){
+            (plot_i->second).fill(btag);
+            (plot_i->second).fill(jet, btag);
+          }
+        }
+      }
 	}
 
     // Tracks/Vertices
-//    const xAOD::TrackParticleContainer* trks(0);
-//    ATH_CHECK(evtStore()->retrieve(trks, m_trackName));
-//    const xAOD::VertexContainer* vtxs(0);
-//    ATH_CHECK(evtStore()->retrieve(vtxs, m_vertexName));
-//    for (auto vtx : *vtxs) m_trkvtxPlots.fill(vtx);
-//    m_trkvtxPlots.fill(trks->size(), vtxs->size() /*, event->averageInteractionsPerCrossing() */);
+    //    const xAOD::TrackParticleContainer* trks(0);
+    //    ATH_CHECK(evtStore()->retrieve(trks, m_trackName));
+    //    const xAOD::VertexContainer* vtxs(0);
+    //    ATH_CHECK(evtStore()->retrieve(vtxs, m_vertexName));
+    //    for (auto vtx : *vtxs) m_trkvtxPlots.fill(vtx);
+    //    m_trkvtxPlots.fill(trks->size(), vtxs->size() /*, event->averageInteractionsPerCrossing() */);
 
     return StatusCode::SUCCESS;
   }
-  
+
   StatusCode PhysValBTag::procHistograms()
   {
     ATH_MSG_INFO ("Finalising hists " << name() << "...");
-    for(std::map<std::string,JetTagDQA::BTaggingValidationPlots>::iterator plot_i = m_btagplots.begin(); plot_i != m_btagplots.end(); ++plot_i){	
-	(plot_i->second).finalize();
+    for(std::map<std::string,JetTagDQA::BTaggingValidationPlots>::iterator plot_i = m_btagplots.begin(); plot_i != m_btagplots.end(); ++plot_i){
+      (plot_i->second).finalize();
     }
     return StatusCode::SUCCESS;
   }
-  
-/////////////////////////////////////////////////////////////////// 
-// Const methods: 
-///////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////// 
-// Non-const methods: 
-/////////////////////////////////////////////////////////////////// 
+  ///////////////////////////////////////////////////////////////////
+  // Const methods:
+  ///////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////// 
-// Protected methods: 
-/////////////////////////////////////////////////////////////////// 
+  ///////////////////////////////////////////////////////////////////
+  // Non-const methods:
+  ///////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////// 
-// Const methods: 
-///////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  // Protected methods:
+  ///////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////// 
-// Non-const methods: 
-/////////////////////////////////////////////////////////////////// 
+  ///////////////////////////////////////////////////////////////////
+  // Const methods:
+  ///////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////
+  // Non-const methods:
+  ///////////////////////////////////////////////////////////////////
 
 
 }
 
-//  LocalWords:  str 
+//  LocalWords:  str
--- PhysicsAnalysis/JetTagging/JetTagValidation/JetTagDQA/src/PhysValBTag.h
+++ PhysicsAnalysis/JetTagging/JetTagValidation/JetTagDQA/src/PhysValBTag.h
@@ -4,10 +4,10 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// PhysValBTag.h 
+// PhysValBTag.h
 // Header file for class PhysValBTag
 // Author: E.Schopf<elisabeth.schopf@cern.ch>
-/////////////////////////////////////////////////////////////////// 
+///////////////////////////////////////////////////////////////////
 #ifndef JETTAGDQA_PHYSVALBTag_H
 #define JETTAGDQA_PHYSVALBTag_H 1
 
@@ -29,90 +29,94 @@
 
 namespace JetTagDQA {
 
-class PhysValBTag
-  : public ManagedMonitorToolBase
-{ 
-  /////////////////////////////////////////////////////////////////// 
-  // Public methods: 
-  /////////////////////////////////////////////////////////////////// 
- public: 
+  class PhysValBTag
+    : public ManagedMonitorToolBase
+  {
+    ///////////////////////////////////////////////////////////////////
+    // Public methods:
+    ///////////////////////////////////////////////////////////////////
+  public:
 
-  // Copy constructor: 
+    // Copy constructor:
 
-  /// Constructor with parameters: 
-  PhysValBTag( const std::string& type,
-		  const std::string& name, 
-		  const IInterface* parent );
+    /// Constructor with parameters:
+    PhysValBTag( const std::string& type,
+                 const std::string& name,
+                 const IInterface* parent );
 
-  /// Destructor: 
-  virtual ~PhysValBTag(); 
+    /// Destructor:
+    virtual ~PhysValBTag();
 
-  // Athena algtool's Hooks
-  virtual StatusCode initialize();
-  virtual StatusCode bookHistograms();
-  virtual StatusCode fillHistograms();
-  virtual StatusCode procHistograms();
+    // Athena algtool's Hooks
+    virtual StatusCode initialize();
+    virtual StatusCode bookHistograms();
+    virtual StatusCode fillHistograms();
+    virtual StatusCode procHistograms();
 
 
-  /////////////////////////////////////////////////////////////////// 
-  // Const methods: 
+    ///////////////////////////////////////////////////////////////////
+    // Const methods:
+    ///////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////
+    // Non-const methods:
+    ///////////////////////////////////////////////////////////////////
+
+
+    ///////////////////////////////////////////////////////////////////
+    // Private data:
+    ///////////////////////////////////////////////////////////////////
+  private:
+
+    /// Default constructor:
+    PhysValBTag();
+
+    // isData flag
+    bool m_isData;
+
+    // Containers
+    std::string m_jetName1;
+    std::string m_jetName2;
+    //  std::string m_jetName3;
+    std::string m_jetName4;
+    std::string m_jetName5;
+    std::string m_jetName7;
+    std::string m_jetName6;
+    std::string m_jetName8;
+    std::string m_jetName9;
+
+    std::string m_trackName;
+    std::string m_vertexName;
+
+    // Hists
+    //  JetTagDQA::BTaggingValidationPlots m_jetPlots;
+    //  PhysVal::TrkAndVtxPlots m_trkvtxPlots;
+
+    std::map<std::string, JetTagDQA::BTaggingValidationPlots> m_btagplots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt4EMTopoPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt4LCTopoPlots;
+    //  JetTagDQA::BTaggingValidationPlots m_antiKt4TruthPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt4TruthWZPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt10LCTopoPlots;
+    //  JetTagDQA::BTaggingValidationPlots m_antiKt10TruthPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt10TruthWZPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt2PV0TrackJetPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt3PV0TrackJetPlots;
+    JetTagDQA::BTaggingValidationPlots m_antiKt4PV0TrackJetPlots;
+
+    int m_nevents;
+    int m_nTruthB;
+    int m_nTruthNonB;
+
+    StatusCode book(PlotBase& plots);
+  };
+
+  // I/O operators
+  //////////////////////
+
   ///////////////////////////////////////////////////////////////////
-
-  /////////////////////////////////////////////////////////////////// 
-  // Non-const methods: 
-  /////////////////////////////////////////////////////////////////// 
-
-
-  /////////////////////////////////////////////////////////////////// 
-  // Private data: 
-  /////////////////////////////////////////////////////////////////// 
- private: 
-
-  /// Default constructor: 
-  PhysValBTag();
-
-  // Containers
-  std::string m_jetName1;
-  std::string m_jetName2;
-//  std::string m_jetName3;
-  std::string m_jetName4;
-  std::string m_jetName5;
-//  std::string m_jetName6;
-  std::string m_jetName7;
-  std::string m_jetName8;
-  std::string m_jetName9;
-
-  std::string m_trackName; 
-  std::string m_vertexName; 
-
-  // Hists
-//  JetTagDQA::BTaggingValidationPlots m_jetPlots;
-//  PhysVal::TrkAndVtxPlots m_trkvtxPlots;
-
-  std::map<std::string, JetTagDQA::BTaggingValidationPlots> m_btagplots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt4EMTopoPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt4LCTopoPlots;
-//  JetTagDQA::BTaggingValidationPlots m_antiKt4TruthPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt4TruthWZPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt10LCTopoPlots;
-//  JetTagDQA::BTaggingValidationPlots m_antiKt10TruthPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt10TruthWZPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt3PV0TrackJetPlots;
-  JetTagDQA::BTaggingValidationPlots m_antiKt4PV0TrackJetPlots;
-
-  int m_nevents;
-  int m_nTruthB;
-  int m_nTruthNonB;
-
-  StatusCode book(PlotBase& plots);
-}; 
-
-// I/O operators
-//////////////////////
-
-/////////////////////////////////////////////////////////////////// 
-// Inline methods: 
-/////////////////////////////////////////////////////////////////// 
+  // Inline methods:
+  ///////////////////////////////////////////////////////////////////
 }
 
 #endif //> !JETTAGDQA_PHYSVALBTag_H
