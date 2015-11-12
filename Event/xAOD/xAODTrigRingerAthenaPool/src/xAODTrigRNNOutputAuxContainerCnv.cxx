908cfc86300d1bd81cd1ab013b359237f67e4448
Attila Krasznahorkay <krasznaa@cern.ch>
2015-11-12 19:56:01 +0100

    Fixing yet another issue with reading BS files (xAODTrigRingerAthenaPool-00-00-08)

--- Event/xAOD/xAODTrigRingerAthenaPool/src/xAODTrigRNNOutputAuxContainerCnv.cxx
+++ Event/xAOD/xAODTrigRingerAthenaPool/src/xAODTrigRNNOutputAuxContainerCnv.cxx
@@ -2,24 +2,32 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODRingerRNNOutputAuxContainerCnv.cxx
+// $Id: xAODTrigRNNOutputAuxContainerCnv.cxx 707590 2015-11-12 19:09:03Z krasznaa $
 
 // System include(s):
 #include <exception>
+#include <memory>
 
-// Local includ
+// Gaudi/Athena include(s):
+#include "AthenaKernel/IThinningSvc.h"
+#include "AthContainers/tools/copyThinned.h"
+
+// Local include(s):
 #include "xAODTrigRNNOutputAuxContainerCnv.h"
 #include "xAODTrigRNNOutputAuxContainerCnv_v1.h"
 
-xAODTrigRNNOutputAuxContainerCnv::xAODTrigRNNOutputAuxContainerCnv( ISvcLocator* svcLoc ): xAODTrigRNNOutputAuxContainerCnvBase( svcLoc ) {
+xAODTrigRNNOutputAuxContainerCnv::
+xAODTrigRNNOutputAuxContainerCnv( ISvcLocator* svcLoc )
+   : xAODTrigRNNOutputAuxContainerCnvBase( svcLoc ) {
 
 }
 
-xAOD::TrigRNNOutputAuxContainer* xAODTrigRNNOutputAuxContainerCnv::createPersistent( xAOD::TrigRNNOutputAuxContainer* trans ) {
+xAOD::TrigRNNOutputAuxContainer*
+xAODTrigRNNOutputAuxContainerCnv::
+createPersistent( xAOD::TrigRNNOutputAuxContainer* trans ) {
 
-   // Create a copy of the container:
-   xAOD::TrigRNNOutputAuxContainer* result = new xAOD::TrigRNNOutputAuxContainer( *trans );
-   return result;
+   // Create a (possibly thinned) copy of the container:
+   return SG::copyThinned( *trans, IThinningSvc::instance() );
 }
 
 xAOD::TrigRNNOutputAuxContainer* xAODTrigRNNOutputAuxContainerCnv::createTransient() {
@@ -30,20 +38,22 @@ xAOD::TrigRNNOutputAuxContainer* xAODTrigRNNOutputAuxContainerCnv::createTransie
 
    // Check which version of the container we're reading:
    if( compareClassGuid( v2_guid ) ) {
+
       // It's the latest version, read it directly:
       return poolReadObject< xAOD::TrigRNNOutputAuxContainer >();
-   }
 
-   if( compareClassGuid( v1_guid ) ) {
-        // The v1 converter:
-        static xAODTrigRNNOutputAuxContainerCnv_v1 converter;
-  
-        // Read in the v1 object:
-        std::unique_ptr< xAOD::TrigRNNOutputAuxContainer_v1 > old( poolReadObject< xAOD::TrigRNNOutputAuxContainer_v1 >() );
-  
-        // Return the converted object:
-        return converter.createTransient( old.get(), msg() );   
-     }
+   } else if( compareClassGuid( v1_guid ) ) {
+
+      // The v1 converter:
+      static xAODTrigRNNOutputAuxContainerCnv_v1 converter;
+
+      // Read in the v1 object:
+      std::unique_ptr< xAOD::TrigRNNOutputAuxContainer_v1 >
+         old( poolReadObject< xAOD::TrigRNNOutputAuxContainer_v1 >() );
+
+      // Return the converted object:
+      return converter.createTransient( old.get(), msg() );   
+   }
 
    // If we didn't recognise the ID:
    throw std::runtime_error( "Unsupported version of "
--- Event/xAOD/xAODTrigRingerAthenaPool/src/xAODTrigRNNOutputAuxContainerCnv.h
+++ Event/xAOD/xAODTrigRingerAthenaPool/src/xAODTrigRNNOutputAuxContainerCnv.h
@@ -4,24 +4,28 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODRingerRNNOutputAuxContainerCnv.h
+// $Id: xAODTrigRNNOutputAuxContainerCnv.h 707590 2015-11-12 19:09:03Z krasznaa $
 #ifndef XAODTRIGRINGERATHENAPOOL_XAODRNNOUTPUTAUXCONTAINERCNV_H
 #define XAODTRIGRINGERATHENAPOOL_XAODRNNOUTPUTAUXCONTAINERCNV_H
 
 // Gaudi/Athena include(s):
 #include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+
 // EDM include(s):
 #include "xAODTrigRinger/TrigRNNOutputAuxContainer.h"
 
 /// Base class for the converter
-typedef T_AthenaPoolCustomCnv< xAOD::TrigRNNOutputAuxContainer, xAOD::TrigRNNOutputAuxContainer > xAODTrigRNNOutputAuxContainerCnvBase;
+typedef T_AthenaPoolCustomCnv< xAOD::TrigRNNOutputAuxContainer,
+                               xAOD::TrigRNNOutputAuxContainer >
+   xAODTrigRNNOutputAuxContainerCnvBase;
+
 /**
  *  @short POOL converter for the xAOD::TrigRNNOutputAuxContainer class
  *
  * @author joao victor da fonseca pinto <joao.victor.da.fonseca.pinto@cern.ch>
  *
- * $Revision:  $
- * $Date: $
+ * $Revision: 707590 $
+ * $Date: 2015-11-12 20:09:03 +0100 (Thu, 12 Nov 2015) $
  */
 class xAODTrigRNNOutputAuxContainerCnv :
    public xAODTrigRNNOutputAuxContainerCnvBase {
@@ -34,10 +38,11 @@ protected:
   xAODTrigRNNOutputAuxContainerCnv( ISvcLocator* svcLoc );
 
    /// Function preparing the container to be written out
-   virtual xAOD::TrigRNNOutputAuxContainer* createPersistent( xAOD::TrigRNNOutputAuxContainer* trans );
+   virtual xAOD::TrigRNNOutputAuxContainer*
+   createPersistent( xAOD::TrigRNNOutputAuxContainer* trans );
    /// Function reading in the object from the input file
    virtual xAOD::TrigRNNOutputAuxContainer* createTransient();
 
 }; // class xAODRingerRNNOutputAuxContainerCnv
 
-#endif // XAOD
+#endif // XAODTRIGRINGERATHENAPOOL_XAODRNNOUTPUTAUXCONTAINERCNV_H
