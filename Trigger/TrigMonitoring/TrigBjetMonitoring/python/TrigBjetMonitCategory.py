dab760b719c48258180ad9c28f0bda028bd14ba7
Elemer Nagy <elemer.nagy@cern.ch>
2015-11-13 11:47:21 +0100

    Bugfix in the muon-jet part of the code (TrigBjetMonitoring-00-03-24)

--- Trigger/TrigMonitoring/TrigBjetMonitoring/python/TrigBjetMonitCategory.py
+++ Trigger/TrigMonitoring/TrigBjetMonitoring/python/TrigBjetMonitCategory.py
@@ -17,6 +17,18 @@ monitoring_bjet      = ["HLT_j15_bperf", "HLT_j55_bperf", "HLT_j110_bperf",
 #       monitoring_bjet      = ["HLT_j0_perf_bperf_L1J12_EMPTY"]
 #	monitoring_bjet      = ["HLT_j0_perf_bperf_L1MU10"]
 
+#monitoring_mujet          = ["HLT_mu4_j55_match_bperf",
+#                             "HLT_mu4_j15_bperf_split_dr05_dz02", "HLT_mu4_j55_bperf_split_dr05_dz02", "HLT_mu6_j110_bperf_split_dr05_dz02"]
 
-monitoring_mujet          = ["HLT_mu4_j55_match_bperf"]
+monitoring_mujet          = ["HLT_mu4_j15_bperf_split_dr05_dz02", "HLT_mu4_j55_bperf_split_dr05_dz02", "HLT_mu6_j110_bperf_split_dr05_dz02",
+                             "HLT_mu4_j25_bperf_split_dr05_dz02", "HLT_mu4_j35_bperf_split_dr05_dz02", "HLT_mu6_j85_bperf_split_dr05_dz02",
+                             "HLT_mu6_j150_bperf_split_dr05_dz02", "HLT_mu6_j175_bperf_split_dr05_dz02", "HLT_mu6_j260_bperf_split_dr05_dz02",
+                             "HLT_mu6_j320_bperf_split_dr05_dz02", "HLT_mu6_j400_bperf_split_dr05_dz02",
+# adding HI triggers
+                             "HLT_mu4_j20_ion_dr05", "HLT_mu4_j60_ion_dr05", "HLT_mu6_j30_ion_dr05"]
 
+
+# Muon-jet parameters
+
+EtThreshold               = 10.
+PriVtxKey                 = "EFHistoPrmVtx"
