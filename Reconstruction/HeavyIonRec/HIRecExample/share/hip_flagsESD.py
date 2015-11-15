63d591a15e0fc1c32085ca7529de6b800996dd05
Aaron Angerami <aaron.richard.angerami@cern.ch>
2015-11-15 17:47:05 +0100

    Tagging HIRecExample-01-00-10 (HIRecExample-01-00-10)

--- Reconstruction/HeavyIonRec/HIRecExample/share/hip_flagsESD.py
+++ Reconstruction/HeavyIonRec/HIRecExample/share/hip_flagsESD.py
@@ -5,15 +5,21 @@ if rec.doESD :
      print "Applying HIP ESD flags mods"
 
      from RecExConfig.RecFlags import rec
-     rec.doAlfa.set_Value_and_Lock(False)
 
      from InDetRecExample.InDetJobProperties import InDetFlags
-     InDetFlags.doLowMuRunSetup.set_Value_and_Lock(True)
+     InDetFlags.doMinBias.set_Value_and_Lock(True)
      InDetFlags.doSplitVertexFindingForMonitoring.set_Value_and_Lock(False)
      InDetFlags.writeRDOs.set_Value_and_Lock(True)
+
+
+     InDetFlags.doMinBias.set_Value_and_Lock(True)
 
      from CaloTools.CaloNoiseFlags import jobproperties
      jobproperties.CaloNoiseFlags.FixedLuminosity.set_Value_and_Lock(0)
 
      from CaloRec.CaloCellFlags import jobproperties
      jobproperties.CaloCellFlags.doPileupOffsetBCIDCorr.set_Value_and_Lock(False)
+
+     #these jet tools clash w/ HIJetRec
+     from JetRec.JetRecFlags import jetFlags
+     jetFlags.skipTools+=["jetisol","run1jetisol"]
