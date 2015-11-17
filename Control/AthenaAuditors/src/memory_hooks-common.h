de446c297541b65c859dab840ac663e36f4e6980
Rolf Seuster <Rolf.Seuster@cern.ch>
2015-11-17 13:07:37 +0100

    adjust to new LCG_80 - namely new Boost (AthenaAuditors-00-00-13)
    	* Tagging AthenaAuditors-00-00-13.
    	* adjust to new LCG_80 - namely new Boost
    
    2015-11-12  Rolf Seuster
    
    	* Tagging AthenaAuditors-00-00-12.
    	* stdcmalloc for AthMemoryAuditor currently depreciated, added support for faster tcmalloc
    
    2015-09-15  scott snyder  <snyder@bnl.gov>
    
    	* Tagging AthenaAuditors-00-00-11.
    	* Comply with ATLAS naming conventions.

---
+++ Control/AthenaAuditors/src/memory_hooks-common.h
@@ -0,0 +1,105 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// common code for memory_hooks for tcmalloc and stdcmalloc which are used for the AthMemoryAuditor
+// Rolf Seuster, Oct 2015
+
+#ifndef ATHENAAUDITORS_MEMORYHOOKSCOMMON_H
+#define ATHENAAUDITORS_MEMORYHOOKSCOMMON_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+// needed for implementing hooks into tcmalloc
+#include "gperftools/malloc_hook.h"
+
+#include <cstdint>
+#include <string.h>
+#include <stdlib.h> // for getenv
+
+#include <boost/intrusive/splay_set.hpp>
+
+#define UNW_LOCAL_ONLY
+#include "libunwind.h"
+
+struct aiStruct
+{
+  aiStruct() : index(0), stacktrace(0) {};
+  aiStruct(uint16_t i, uint16_t s) : index(i), stacktrace(s) {};
+  uint16_t index;
+  uint16_t stacktrace;
+};
+
+static std::map<std::string, aiStruct> arrayAlgIndex;
+static std::map<uint32_t, std::string> algorithms;
+
+static uint32_t curMaxIndex(0);
+static uint32_t curIndex(0);
+
+static bool initialized(false);
+static bool finished(false);
+
+static uintptr_t context(0);
+static uintptr_t contextFirst(0);
+static uint32_t  current_stage(0);
+static std::string defaultString = "N/A";
+
+static size_t stacktraceDepth(25);
+
+static bool collectStacktraces(true);
+
+using namespace boost::intrusive;
+
+class myBlocks_tc : public bs_set_base_hook<optimize_size<false> >
+  {
+ public:
+  uintptr_t          allocated;
+  uintptr_t          allocsize;
+  uint32_t           algindex;
+  uint32_t           curstage;
+  std::vector<void*> allocatedFrom;
+  
+ public:
+  bs_set_member_hook<> member_hook_;
+  
+  myBlocks_tc()
+    : allocated(0)
+    , allocsize(0)
+    , algindex(0)
+    , curstage(0)
+    { };
+  
+ myBlocks_tc(uintptr_t mem, uintptr_t s, uint32_t algi, uint32_t cstage, size_t stDepth)
+   : allocated(mem)
+    , allocsize(s)
+    , algindex(algi)
+    , curstage(cstage)
+    , allocatedFrom(stDepth,nullptr)  { };
+  
+  friend bool operator<  ( const myBlocks_tc &a, const myBlocks_tc &b )
+  {
+    return a.allocated < b.allocated;
+  };
+  
+  friend bool operator>  ( const myBlocks_tc &a, const myBlocks_tc &b )
+  {
+    return a.allocated > b.allocated;
+  };
+  
+  friend bool operator== ( const myBlocks_tc &a, const myBlocks_tc &b)
+  {
+    return a.allocated == b.allocated;
+  };
+};
+
+myBlocks_tc *bg_tc;
+
+typedef splay_set< myBlocks_tc, base_hook<bs_set_base_hook<> > > allocSet_tc;
+
+static allocSet_tc::iterator allocset_last;
+static allocSet_tc allocset_tc;
+static allocSet_tc free_set_tc;
+
+#endif
