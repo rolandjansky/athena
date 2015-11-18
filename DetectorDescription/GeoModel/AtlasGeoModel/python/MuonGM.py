1a265ce60b55e35f8a382c693e80b1586ab2072c
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2015-11-18 11:59:10 +0100

    python/MuonGM.py: ensure that TheMuonAlignmentTool is set for overlay jobs. ATLASSIM-1922. AtlasGeoModel-00-03-40 (AtlasGeoModel-00-03-40)
    	* python/MuonGM.py: ensure that TheMuonAlignmentTool is set for
    	overlay jobs. ATLASSIM-1922.
    	* AtlasGeoModel-00-03-40
    
    2015-11-16 Noemi Calace <Noemi.Calace@cern.ch>
    	* Added try/except block in the GeoModelInit.py
    	* AtlasGeoModel-00-03-39
    
    2015-09-28 Noemi Calace <Noemi.Calace@cern.ch>
    	* Re-added flag to not re-create the DetDescrCnvSvc
    	* TODO: Make a new flag somewhere else to avoid tracking dependences in GeoModel
    	* AtlasGeoModel-00-03-38

--- DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGM.py
+++ DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGM.py
@@ -5,15 +5,15 @@ from AthenaCommon.DetFlags      import DetFlags
 
 if ( jobproperties.Global.DetGeo() == "ctbh8" or jobproperties.Global.DetGeo() == "ctbh6" ):
     print " CTB layout for Muon Spectrometer is not supported anymore"
- 
+
 elif ( DetFlags.detdescr.Muon_on() ):
     from GeoModelSvc.GeoModelSvcConf import GeoModelSvc
     GeoModelSvc = GeoModelSvc()
-   
+
     from MuonGeoModel.MuonGeoModelConf import MuonDetectorTool
     GeoModelSvc.DetectorTools += [ MuonDetectorTool() ]
     GeoModelSvc.DetectorTools[ "MuonDetectorTool" ].BuildFromNova = 0
-    if ( not DetFlags.simulate.any_on() ):
+    if ( not DetFlags.simulate.any_on() or DetFlags.overlay.any_on() ):
       GeoModelSvc.DetectorTools[ "MuonDetectorTool" ].TheMuonAlignmentTool = "MuonAlignmentDbTool/MGM_AlignmentDbTool"
     else:
       GeoModelSvc.DetectorTools[ "MuonDetectorTool" ].TheMuonAlignmentTool = ""
