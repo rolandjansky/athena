d0196050214e6b2951af2376457a0c75e207cc2e
Walter Lampl <Walter.Lampl@cern.ch>
2015-11-24 10:25:33 +0100

    adapt to change of LArNoisyROSummary (add members to track Mini Noise Bursts) (LArTPCnv-00-06-01)

--- LArCalorimeter/LArCnv/LArTPCnv/LArTPCnv/LArNoisyROSummary_p3.h
+++ LArCalorimeter/LArCnv/LArTPCnv/LArTPCnv/LArNoisyROSummary_p3.h
@@ -22,7 +22,9 @@ class LArNoisyROSummary_p3
     m_BadFEBFlaggedPartitions(0),
     m_BadFEB_WFlaggedPartitions(0),
     m_SatMediumFlaggedPartitions(0),
-    m_SatTightFlaggedPartitions(0)
+    m_SatTightFlaggedPartitions(0),
+    m_MNBLooseFlaggedPartitions(0),
+    m_MNBTightFlaggedPartitions(0)
   {};
 
  private:
@@ -32,6 +34,8 @@ class LArNoisyROSummary_p3
   uint8_t m_BadFEB_WFlaggedPartitions;
   uint8_t m_SatMediumFlaggedPartitions;
   uint8_t m_SatTightFlaggedPartitions;
+  uint8_t m_MNBLooseFlaggedPartitions;
+  uint8_t m_MNBTightFlaggedPartitions;
   
 };
 
