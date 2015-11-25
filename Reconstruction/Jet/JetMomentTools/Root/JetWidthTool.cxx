a9f4cafeb00fddd3e20eaab30576355f596bd598
James Frost <frost@hep.phy.cam.ac.uk>
2015-11-25 16:47:23 +0100

    backport WidthPhi moment (JetMomentTools-00-03-11-05)
    	* JetWidthTool: update to return PhiWidth additionally
    	* ATLJETMET-439
    	* JetMomentTools-00-03-11-05
    
    	* JetMomentTools-00-03-11-04

--- Reconstruction/Jet/JetMomentTools/Root/JetWidthTool.cxx
+++ Reconstruction/Jet/JetMomentTools/Root/JetWidthTool.cxx
@@ -17,13 +17,45 @@ JetWidthTool::JetWidthTool(std::string myname)
 //**********************************************************************
 
 int JetWidthTool::modifyJet(xAOD::Jet& jet) const {
-  jet.setAttribute("Width", width(jet));
+
+  double widthEta = 0, widthPhi = 0;
+  //jet.setAttribute("Width", width(jet));
+  jet.setAttribute("Width", width(jet,widthEta,widthPhi));
+  //jet.setAttribute("WidthEta",widthEta);
+  jet.setAttribute("WidthPhi",widthPhi);
   return 0;
 }
 
 //**********************************************************************
 
-double JetWidthTool::width(const xAOD::Jet& jet) const {
+//double JetWidthTool::width(const xAOD::Jet& jet) const {
+//
+//  // Get the constituents of the jet
+//  const xAOD::JetConstituentVector constituents = jet.getConstituents();
+//  xAOD::JetConstituentVector::iterator iter = constituents.begin();
+//  xAOD::JetConstituentVector::iterator iEnd = constituents.end();
+//
+//  // TODO: Switch to using helper function once JetUtils has been updated
+//  // Set the width
+//  // jetWidth = JetKinematics::ptWeightedWidth(iter,iEnd,&jet);
+//    
+//  // Calculate the pt weighted width
+//  const double jetEta = jet.eta();
+//  const double jetPhi = jet.phi();
+//  double weightedWidth = 0;
+//  double ptSum = 0;
+//
+//  for ( ; iter != iEnd; ++iter) {
+//    const double dR = jet::JetDistances::deltaR(jetEta, jetPhi, iter->eta(),  iter->phi() );
+//    const double pt = iter->pt();
+//    weightedWidth += dR * pt;
+//    ptSum += pt;
+//  }
+//
+//  return ptSum > 0 ? weightedWidth/ptSum : -1;
+//}
+
+double JetWidthTool::width(const xAOD::Jet& jet, double& widthEta, double& widthPhi) const {
 
   // Get the constituents of the jet
   const xAOD::JetConstituentVector constituents = jet.getConstituents();
@@ -38,14 +70,25 @@ double JetWidthTool::width(const xAOD::Jet& jet) const {
   const double jetEta = jet.eta();
   const double jetPhi = jet.phi();
   double weightedWidth = 0;
+  double weightedWidthEta = 0;
+  double weightedWidthPhi = 0;
   double ptSum = 0;
 
   for ( ; iter != iEnd; ++iter) {
-    const double dR = jet::JetDistances::deltaR(jetEta, jetPhi, iter->eta(),  iter->phi() );
-    const double pt = iter->pt();
+    const double dR   = jet::JetDistances::deltaR(jetEta, jetPhi, iter->eta(),  iter->phi() );
+    const double dEta = fabs(jet::JetDistances::deltaEta(jetEta, iter->eta() ));
+    const double dPhi = fabs(jet::JetDistances::deltaPhi(jetPhi, iter->phi() ));
+    const double pt   = iter->pt();
+
     weightedWidth += dR * pt;
+    weightedWidthEta += dEta * pt;
+    weightedWidthPhi += dPhi * pt;
+    
     ptSum += pt;
   }
+
+  widthEta = ptSum > 0 ? weightedWidthEta/ptSum : -1;
+  widthPhi = ptSum > 0 ? weightedWidthPhi/ptSum : -1;
 
   return ptSum > 0 ? weightedWidth/ptSum : -1;
 }
