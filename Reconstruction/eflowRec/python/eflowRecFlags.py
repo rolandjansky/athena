7e0e5c50e1ab60a138df3a6a8a2c95ca80f7c89c
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2015-11-26 12:56:17 +0100

    2015-11-26 Mark Hodgkinson         * python/eflowRecFlags.py         * Change 2015 lepton ID flag to true by default         * Tag as eflowRec-00-05-36-09 (eflowRec-00-05-36-09)
    	* python/eflowRecFlags.py
    	* Change 2015 lepton ID flag to true by default
    	* Tag as eflowRec-00-05-36-09

--- Reconstruction/eflowRec/python/eflowRecFlags.py
+++ Reconstruction/eflowRec/python/eflowRecFlags.py
@@ -84,7 +84,7 @@ class useUpdated2015LeptonID(JobProperty):
     """
     statusOn = True
     allowedTypes = ['bool']
-    StoredValue = False
+    StoredValue = True
 
 class storeLeptonCells(JobProperty):
     """ Flag to toggle storage of lepton cells on/off - if on will put lepton cells in storegate in eflowCaloObjectBuilder
