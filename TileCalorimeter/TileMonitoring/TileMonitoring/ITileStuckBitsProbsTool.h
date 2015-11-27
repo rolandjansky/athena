815e0103f67e7aa1e0308b3183d2ffe8387abc75
Tomas Davidek <Tomas.Davidek@cern.ch>
2015-11-27 21:52:29 +0100

    Updates in TileJetMonTool DQ histograms (TileMonitoring-00-07-67)
    	* TileJetMonTool: all improvements and bug-fixes from 00-07-64-07
    	ported to this version
    	* Tagging TileMonitoring-00-07-66
    
    2015-11-18  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* src/TileDigitsMonTool.cxx: update of stuck bits probabilities, use
    	  uint8_t, 0-100 for probability of stuck @ 0, 101-200 for stuck @ 1.
    	* share/TileMon_jobOptions.py: changed jet container for heavy ions to AntiKt4HIJets
    	* Tagging TileMonitoring-00-07-66
    
    2015-11-16 Tibor Zenis <tibor.zenis@cern.ch>
    
    	* src/TileDigitsMonTool: update of stuck bits probabilities, use
    	  uint8_t, 0-100 for probability of stuck @ 1, 101-200 for stuck @ 0.
    
    2015-11-11  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* TileMonitoring/ITileStuckBitsProbsTool.h: added new interface for tool which returns stuck bits probabilities
    ...
    (Long ChangeLog diff - truncated)

---
+++ TileCalorimeter/TileMonitoring/TileMonitoring/ITileStuckBitsProbsTool.h
@@ -0,0 +1,25 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TILEMONITORING_ITILESTUCKBITSPROBSTOOL_H
+#define TILEMONITORING_ITILESTUCKBITSPROBSTOOL_H
+
+class TTree;
+
+class ITileStuckBitsProbsTool: virtual public IAlgTool {
+ public:
+ 
+  virtual ~ITileStuckBitsProbsTool() {}
+ 
+  static const InterfaceID& interfaceID() {
+    static const InterfaceID IID_ITileStuckBitsProbsTool("ITileStuckBitsProbsTool", 1, 0);
+    return IID_ITileStuckBitsProbsTool;
+  }
+
+  virtual const uint8_t* stuckBitProb (int ros, int module, int channel, int gain) const = 0;
+  virtual void saveStuckBitsProbabilities(TTree* tree) = 0;
+};
+
+
+#endif
