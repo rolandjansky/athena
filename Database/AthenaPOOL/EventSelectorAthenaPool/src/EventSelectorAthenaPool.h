d4e0edd74019b155c092451fe050af784885460f
Will Buttinger <William.Buttinger@cern.ch>
2015-11-28 11:11:49 +0100

    'Overhaul order of EndInputFile and LastInputFile incident firing, see changelog' (EventSelectorAthenaPool-00-18-36-06)
           * src/EventSelectorAthenaPool.cxx: Changed firing of EndInputFile and LastInputFile to occur when switching to new file or end of eventselector respectively
             These events no longer fire on the ::stop, so should always fire before MetaDataStop.
             This means if the user interrupts the processing, the EndInputFile and LastInputFile are not fired, only the MetaDataStop (via the service stops)
             Happens in both ::next and ::seek, so that behaviour consistent in both athenaMP and not
    	* tag EventSelectorAthenaPool-00-18-36-06

--- Database/AthenaPOOL/EventSelectorAthenaPool/src/EventSelectorAthenaPool.h
+++ Database/AthenaPOOL/EventSelectorAthenaPool/src/EventSelectorAthenaPool.h
@@ -140,6 +140,9 @@ private: // internal member functions
    /// Search for event number evtNum.
    int findEvent(int evtNum);
 
+   /// Fires the EndInputFile incident (if there is an open file), EndTagFile incident, and LastInputFile incidents at end of selector
+   void fireEndFileIncidents(bool isLastFile) const;
+
 private: // data
    EventContextAthenaPool*      m_beginIter;
    EventContextAthenaPool*      m_endIter;
@@ -222,7 +225,7 @@ private: // properties
    mutable std::vector<long> m_skipEventSequence;
 
    mutable int m_evtCount; // internal count of events
-   bool m_firedIncident;
+   mutable bool m_firedIncident;
 };
 
 #endif
