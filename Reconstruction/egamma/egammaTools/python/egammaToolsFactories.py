a8b90d6e142ead75f0eee5bfd18c77cd7d136464
Ludovica Aperio Bella <ludovica.aperio.bella@cern.ch>
2015-11-30 10:39:02 +0100

    update latest crack calibration (egammaTools-01-01-35-05)
    	* Optimised ph conversion TRT hit fraction cut & latest crack-calibration
    	* Tagging as  egammaTools-01-01-35-05

--- Reconstruction/egamma/egammaTools/python/egammaToolsFactories.py
+++ Reconstruction/egamma/egammaTools/python/egammaToolsFactories.py
@@ -36,7 +36,7 @@ egammaSwTool = ToolFactory(egammaToolsConf.egammaSwTool,
 
 from egammaMVACalib import egammaMVACalibConf 
 egammaMVATool =  ToolFactory(egammaMVACalibConf.egammaMVATool,
-                              folder="egammaMVACalib/offline/v3_E4crack")
+                              folder="egammaMVACalib/offline/v3_E4crack_bis")
 
 EMClusterTool = ToolFactory(egammaToolsConf.EMClusterTool,
                             OutputClusterContainerName = egammaKeys.outputClusterKey(),
