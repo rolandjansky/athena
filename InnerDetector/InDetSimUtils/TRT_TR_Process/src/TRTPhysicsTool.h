a2ee9db0a7b68e3546ece1d0e92ae4d6610007bd
Steven Andrew Farrell <steven.farrell@cern.ch>
2015-12-11 22:32:00 +0100

    G4AutoDelete the TR process (TRT_TR_Process-00-01-06)
    	* Using G4AutoDelete to garbage-collect the TRTTransitionRadiation
    	  object created in TRTPhysicsTool. This is just a lazy way to cleanup
    	  the allocated memory, particularly for multi-threading.
    	* Tagging: TRT_TR_Process-00-01-06
    
    2015-11-25 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Removed the explicit link against Geo2G4, as CMT is able to link
    	  to that dual-use library without any special settings. And this
    	  setting screws up the CMT->CMake conversion.
    	* Tagging as TRT_TR_Process-00-01-05

--- InnerDetector/InDetSimUtils/TRT_TR_Process/src/TRTPhysicsTool.h
+++ InnerDetector/InDetSimUtils/TRT_TR_Process/src/TRTPhysicsTool.h
@@ -8,13 +8,13 @@
 // Include files
 #include "AthenaBaseComps/AthAlgTool.h"
 
-
 #include "G4AtlasInterfaces/IPhysicsOptionTool.h"
 
 //to handle
 #include "GaudiKernel/ToolHandle.h"
 
 #include "G4VPhysicsConstructor.hh"
+
 
 /** @class TRTPhysicsTool TRTPhysicsTool.h "TRT:TR_Process/TRTPhysicsTool.h"
  *
@@ -23,31 +23,28 @@
  *  @author Edoardo Farina
  *  @date  18-05-2015
  */
-class TRTPhysicsTool :  public G4VPhysicsConstructor, public AthAlgTool, virtual public IPhysicsOptionTool  {
+class TRTPhysicsTool : public G4VPhysicsConstructor, public AthAlgTool,
+                       virtual public IPhysicsOptionTool
+{
 public:
   /// Standard constructor
   TRTPhysicsTool( const std::string& type , const std::string& name,
                   const IInterface* parent ) ;
 
-  virtual ~TRTPhysicsTool( ); ///< Destructor
+  virtual ~TRTPhysicsTool(){}; ///< Destructor
 
   /// Initialize method
   virtual StatusCode initialize() override final;
   virtual void ConstructParticle() override final;
   virtual void ConstructProcess() override final;
 
-  /** Implements
-   */
-
+  /// IPhysicsOptionTool method; simply returns self.
   virtual G4VPhysicsConstructor* GetPhysicsOption() override final;
-
 
 protected:
 
-  G4VProcess* m_pXTR;
   std::string m_xmlFile;
+
 };
-
-
 
 #endif
