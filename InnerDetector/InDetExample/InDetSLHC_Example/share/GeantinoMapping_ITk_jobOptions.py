885dd2574920525c2d056649d429b17897e6bbd7
Susumu Oda <Susumu.Oda@cern.ch>
2015-12-13 20:30:12 +0100

    Fixed wrong format (InDetSLHC_Example-00-01-50)
    	* test/InDetSLHC_Example_TestConfiguration.xml
    	  Fixed wrong format
    	* Tagged as InDetSLHC_Example-00-01-50
    
    2015-12-09  Eric Feng  <Eric.Feng@cern.ch>
    	* New version 23-25-dev0 which updates LOI to current Strip layout
    	  data/slhcsct_local_database_SLHC-23-25-dev0.txt
      	  data/IdDictInnerDetector_SLHC-23-25-dev0.xml
    	* Fix parsing issue with Barrel & stub comments for geantino mapping
    	* New version 23-25-dev1 with interstrip spacing of 8mm in forward modules, in addition to updates from 23-25-dev0
    	  data/slhcsct_local_database_SLHC-23-25-dev1.txt
      	  data/IdDictInnerDetector_SLHC-23-25-dev1.xml
    2015-12-08 Susumu Oda <Susumu.Oda@cern.ch>
    	* test/InDetSLHC_Example_TestConfiguration.xml
    	  disabled the TrackingAxAODLOIVF test. Derivation production requires trigger
    	  information but Upgrade simulation does not have trigger information.
    	* share/postInclude.SLHC_Setup.py
    	  block the /Indet/IBLDist conditions folder to avoid crash.
    	* Tagged as InDetSLHC_Example-00-01-49
    
    ...
    (Long ChangeLog diff - truncated)

--- InnerDetector/InDetExample/InDetSLHC_Example/share/GeantinoMapping_ITk_jobOptions.py
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/GeantinoMapping_ITk_jobOptions.py
@@ -5,94 +5,103 @@
 #		of the ATLAS detector and the GeantinoMapping.
 #		It can be run using athena.py
 #
-__version__="$Revision: 706440 $"
+# This job options requires Tracking/TrkG4Components/TrkG4UserActions-01-00-03.
+# Modified version of Tracking/TrkG4Components/TrkG4UserActions/share/GeantinoMapping_jobOptions.py
+# See https://twiki.cern.ch/twiki/bin/view/Atlas/UpgradeSimulationInnerTrackerMigrationRel20p3p3#Submission_of_jobs
+#
+__version__="$Revision: 621824 $"
 #==============================================================
 
 #--- Algorithm sequence ---------------------------------------
 from AthenaCommon.AlgSequence import AlgSequence
 topSeq = AlgSequence()
 
-#- Output threshold (DEBUG, INFO, WARNING, ERROR, FATAL) ------
+#--- Output threshold (DEBUG, INFO, WARNING, ERROR, FATAL) ----
 #from AthenaCommon.AppMgr import ServiceMgr
 ServiceMgr.MessageSvc.OutputLevel  = INFO
-ServiceMgr.MessageSvc.defaultLimit = 10000
+ServiceMgr.MessageSvc.defaultLimit = 20000
 
 #--- Detector flags -------------------------------------------
 from AthenaCommon.DetFlags import DetFlags
 from AthenaCommon.GlobalFlags import globalflags
 
-#--- Include JobSpecs.py --------------------------------------
-#include ('JobSpecs.py')
+import random
+import time
 
 ### pass arguments with athena -c "..." ...jobOptions.py:
 if 'myMomentum' not in dir() :
     myMomentum = 10000
 
-if 'myRandomSeed' not in dir() :
-    myRandomSeed = 12398190
+if 'myRandomOffset' not in dir() :
+    myRandomOffset = int(random.uniform(0,time.time())*0.001 )
+
+if 'myRandomSeed1' not in dir() :
+    myRandomSeed1 = int(random.uniform(0,time.time()))
+
+if 'myRandomSeed2' not in dir() :
+    myRandomSeed2 = int(random.uniform(0,time.time()))
 
 if 'myMaxEvent' not in dir() :
     myMaxEvent = 10
-    
+
 if 'myPt' not in dir() :
     myPt = 'pt'  # values are 'p' or 'pt'
 
 if 'myGeo' not in dir() :
-    myGeo = ''
+    myGeo = 'ATLAS-P2-ITK-01-00-00_VALIDATION'
 
-JobSpecs_DetLayout = myGeo
-JobSpecs_Events    = myMaxEvent
-JobSpecs_Seed1     = myRandomSeed
+print 'Random seeds and offset as calcluated by jobOptions ', myRandomSeed1, ' ', myRandomSeed2, ' offset - ', myRandomOffset
 
 
-#include("InDetSLHC_Example/preInclude.OverrideBFieldTag.py")
-
-from InDetSLHC_Example.SLHC_JobProperties import SLHC_Flags
-SLHC_Flags.SLHC_Version = JobSpecs_DetLayout
-
+# Set everything to ATLAS
+DetFlags.ID_setOn()
+DetFlags.Calo_setOff()
+DetFlags.Muon_setOff()
+include("InDetSLHC_Example/preInclude.SLHC.py")
 include("InDetSLHC_Example/preInclude.SiliconOnly.py")
-    
-globalflags.ConditionsTag   = 'OFLCOND-ATLAS-HL-LHC-00'
+# the global flags
+globalflags.ConditionsTag = 'OFLCOND-MC12-ITK-26-80-25'
+print globalflags.ConditionsTag
 
-DetFlags.simulate.Truth_setOn()
-
-# specify your "/tmp/<username>/" directory here (output files may be large!)
-outPath = "" 
 
 #--- AthenaCommon flags ---------------------------------------
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-athenaCommonFlags.PoolHitsOutput = outPath + 'Hits.pool.root'
-athenaCommonFlags.EvtMax = JobSpecs_Events
+athenaCommonFlags.PoolEvgenInput.set_Off()   ### is this necessary?
+athenaCommonFlags.PoolHitsOutput = 'Hits.pool.root'
+athenaCommonFlags.EvtMax = myMaxEvent
 
 #--- Simulation flags -----------------------------------------
-from G4AtlasApps.SimFlags import simFlags
-simFlags.load_atlas_flags()
-simFlags.SimLayout.set_On() 
-# simFlags.SimLayout = 'ATLAS-SLHC-01-01-00_VALIDATION'
-simFlags.SimLayout='ATLAS-SLHC-01-03-00_VALIDATION'
-simFlags.RandomSeed=JobSpecs_Seed1
-#  sets the EtaPhi, VertexSpread and VertexRange checks on
-simFlags.EventFilter.set_Off()
+from G4AtlasApps.SimFlags import SimFlags
+SimFlags.load_atlas_flags() # Going to use an ATLAS layout
+SimFlags.SimLayout = myGeo
+SimFlags.EventFilter.set_Off()
 
-from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-athenaCommonFlags.PoolEvgenInput.set_Off()
-import AthenaCommon.AtlasUnixGeneratorJob
+myMinEta = -6.0
+myMaxEta =  6.0
+
+myPDG    = 999   # 999 = Geantinos, 13 = Muons
+
+include("GeneratorUtils/StdEvgenSetup.py")
+theApp.EvtMax = myMaxEvent
+
 import ParticleGun as PG
-pg = PG.ParticleGun(randomSvcName=simFlags.RandomSvc.get_Value(), randomStream="SINGLE")
-pg.sampler.pid = 999 #Geantino
-if 'pt' == str(myPt):
-    pg.sampler.mom = PG.PtEtaMPhiSampler(pt=myMomentum, eta=[-5,5])
-else:
-    print 'not sure what you want to do here'
-#pg.sampler.mom = PG.EEtaMPhiSampler(energy=50000, eta=[-3,3])
+pg = PG.ParticleGun(randomSvcName=SimFlags.RandomSvc.get_Value(), randomStream="SINGLE")
+pg.sampler.pid = myPDG
+pg.sampler.mom = PG.EEtaMPhiSampler(energy=10000, eta=[myMinEta,myMaxEta])
 topSeq += pg
-print topSeq.ParticleGun
 
-try:
-    include("G4AtlasApps/fragment.SimCopyWeights.py")
-except:
-    from GeneratorModules.GeneratorModulesConf import CopyEventWeight
-    topSeq += CopyEventWeight(TruthCollKey="GEN_EVENT")
+SimFlags.RandomSeedOffset = myRandomOffset
+
+### new rel17 (check Simulation/G4Atlas/G4AtlasApps/python/SimFlags.py for details)
+SimFlags.RandomSeedList.addSeed( "SINGLE", myRandomSeed1, myRandomSeed2 )
+
+from RngComps.RngCompsConf import AtRndmGenSvc 
+myAtRndmGenSvc = AtRndmGenSvc()
+myAtRndmGenSvc.Seeds = ["SINGLE "+str(myRandomSeed1)+" "+str(myRandomSeed2) ] 
+myAtRndmGenSvc.OutputLevel 	= VERBOSE
+myAtRndmGenSvc.EventReseeding   = False
+ServiceMgr += myAtRndmGenSvc
+
 
 ## Add an action
 try:
@@ -111,11 +120,7 @@ except:
                                         "recordMSc"    : "1" })
         AtlasG4Eng.G4Eng.menu_UserActions.add_UserAction(GeantinoAction)
 
-    simFlags.InitFunctions.add_function('preInitG4', geantino_action)
-
-# suppress the enormous amount of MC output
-from TruthExamples.TruthExamplesConf import DumpMC
-DumpMC.VerboseOutput = False
+    SimFlags.InitFunctions.add_function('preInitG4', geantino_action)
 
 ############### The Material hit collection ##################
 
@@ -126,8 +131,8 @@ ServiceMgr.AthenaSealSvc.CheckDictionary   = True
 ServiceMgr.AthenaPoolCnvSvc.OutputLevel = DEBUG
 ServiceMgr.AthenaPoolCnvSvc.CommitInterval = 10
 MaterialStream              = AthenaPoolOutputStream ( 'MaterialStream' )
-MaterialStream.OutputFile   = outPath + "MaterStepFile.root" 
-MaterialStream.ItemList    += [ 'MaterialStepVector#*']
+MaterialStream.OutputFile   =   "MaterialStepCollection.root"
+MaterialStream.ItemList    += [ 'Trk::MaterialStepCollection#*']
 
 ##############################################################
 
@@ -135,8 +140,6 @@ MaterialStream.ItemList    += [ 'MaterialStepVector#*']
 from G4AtlasApps.PyG4Atlas import PyG4AtlasAlg
 topSeq += PyG4AtlasAlg()
 
-from InDetIBL_Example.SLHC_Setup import SLHC_Setup
-SLHC_Setup = SLHC_Setup()
-
+include("InDetSLHC_Example/postInclude.SLHC_Setup.py")
 #--- End jobOptions.GeantinoMapping.py file  ------------------------------
 
