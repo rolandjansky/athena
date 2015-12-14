228b27ab39b4a9397c9bfc35b154b7db8498cdd9
Scott Snyder <scott.snyder@cern.ch>
2015-12-14 19:52:46 +0100

    Merge from StoreGate-03-06-16: Make sure old proxy is not freed until after replaceProxy() call. (StoreGate-02-45-01-13)

--- Control/StoreGate/src/StoreGateSvc.cxx
+++ Control/StoreGate/src/StoreGateSvc.cxx
@@ -875,6 +875,7 @@ StoreGateSvc::typeless_overwrite( const CLID& clid, DataObject* obj, const std::
   StatusCode sc(StatusCode::SUCCESS);
   SG::DataProxy* toRemove(proxy(clid, key, false));
   if (0 != toRemove) {  
+    toRemove->addRef();
     const bool FORCEREMOVE(true); 	 
     sc =removeProxy(toRemove, (void*)0, FORCEREMOVE);
   }
@@ -889,6 +890,8 @@ StoreGateSvc::typeless_overwrite( const CLID& clid, DataObject* obj, const std::
   if (toRemove && sc.isSuccess() && store()->storeID() == StoreID::DETECTOR_STORE) {
     sc = getIIOVSvc()->replaceProxy(toRemove, proxy(clid, key));
   }
+  if (toRemove)
+    toRemove->release();
   return sc;
 }
 
