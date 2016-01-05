19457f45c71830fbc45f7e7d8e50aead0b5c7c2f
John Baines <john.baines@stfc.ac.uk>
2016-01-05 17:57:45 +0100

    PriVtxKey = "HLT_PrimVertexFTK" for FTK chains (TrigBjetHypo-02-00-67)

--- Trigger/TrigHypothesis/TrigBjetHypo/src/TrigBjetFex.cxx
+++ Trigger/TrigHypothesis/TrigBjetHypo/src/TrigBjetFex.cxx
@@ -1013,10 +1013,12 @@ HLT::ErrorCode TrigBjetFex::hltExecute(const HLT::TriggerElement* /*inputTE*/, H
   // -----------------------------------
   HLT::ErrorCode status = getFeature(outputTE, pointerToEFTrackCollections, m_trackKey);
 
+
   if (status != HLT::OK) {
     msg() << MSG::DEBUG << "No HLT track collection retrieved" << endreq;
-  } 
-  else if (msgLvl() <= MSG::DEBUG) {
+  } else if (pointerToEFTrackCollections == nullptr) {
+    msg() << MSG::DEBUG << "Null pointer to HLT track collection" << endreq;
+  } else if (msgLvl() <= MSG::DEBUG) {
     msg() << MSG::DEBUG << "HLT track collection retrieved with size = " << pointerToEFTrackCollections->size() << endreq;
   }
 
