cb1e5105d3c274bdc984e7a41e3854be02c0a0b8
Steven Andrew Farrell <steven.farrell@cern.ch>
2016-01-06 20:28:44 +0100

    non-static trk mgr in helper (SimHelpers-00-02-00)
    	* Made track mgr and getter/setter methods non-static in
    	  SecondaryTracksHelper. This is an incremental update for thread
    	  safety in the truth infrastructure.
    	* Tagging: SimHelpers-00-02-00
    
    2015-12-25 Steve Farrell <Steven.Farrell@cern.ch>
    	* Some minor cleanup
    	* Added some class documentation and notes about a couple of design
    	  issues.
    	* Minor update. Not tagging.
    
    2015-11-30  John Chapman  <John.Chapman@cern.ch>
    	* Remove obsolete DetectorGeometryHelper class. ATLASSIM-2286
    	* Tagging: SimHelpers-00-01-07
    
    
    Former-commit-id: b4bc6a8836e43a2c6a289d5572fc0d4c4fa8b076

--- Simulation/G4Sim/SimHelpers/src/SecondaryTracksHelper.cxx
+++ Simulation/G4Sim/SimHelpers/src/SecondaryTracksHelper.cxx
@@ -2,48 +2,52 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #include "SimHelpers/SecondaryTracksHelper.h"
+#include "G4EventManager.hh"
 
 
-
-const G4TrackingManager* SecondaryTracksHelper::theTrackingManager=0;
-
+// Default constructor delegates to 2nd constructor and retrieves the
+// tracking manager automatically from the global event manager
 SecondaryTracksHelper::SecondaryTracksHelper()
+  : SecondaryTracksHelper( G4EventManager::GetEventManager()->GetTrackingManager() )
 {
-	currentNrOfSecondaries=0;
 }
+
 SecondaryTracksHelper::SecondaryTracksHelper(const G4TrackingManager *tm)
 {
-	theTrackingManager=tm;
-	currentNrOfSecondaries=0;
+  theTrackingManager=tm;
+  currentNrOfSecondaries=0;
 }
+
 void SecondaryTracksHelper::SetTrackingManager(const G4TrackingManager *tm)
 {
-	theTrackingManager=tm;
+  theTrackingManager=tm;
 }
+
 const G4TrackingManager *SecondaryTracksHelper::GetTrackingManager()
 {
-	return theTrackingManager;
+  return theTrackingManager;
 }
+
 int SecondaryTracksHelper::NrOfNewSecondaries()
 {
-	int value=0;
-//	std::cout<<" NrOfNewSecondaries "<<currentNrOfSecondaries << 
-//	         " "<<CurrentNrOfSecondaries()<<std::endl;
-	int curr=CurrentNrOfSecondaries();
-	if (curr>currentNrOfSecondaries) 
-	{
-		value=curr-currentNrOfSecondaries;
-		currentNrOfSecondaries=curr;
-	}
-	return value;
+  int value=0;
+  //std::cout<<" NrOfNewSecondaries "<<currentNrOfSecondaries <<
+  //           " "<<CurrentNrOfSecondaries()<<std::endl;
+  int curr=CurrentNrOfSecondaries();
+  if (curr>currentNrOfSecondaries)
+  {
+    value=curr-currentNrOfSecondaries;
+    currentNrOfSecondaries=curr;
+  }
+  return value;
 }
+
 std::vector<G4Track*> SecondaryTracksHelper::GetSecondaries(int ntracks) const
 {
-	std::vector<G4Track*> vTrack;
-	int iSize=theTrackingManager->GimmeSecondaries()->size();
-	for (int i=iSize-1;i>iSize-ntracks-1;i--) 
-		vTrack.push_back((*(theTrackingManager->GimmeSecondaries()))[i]);
-	return vTrack;
+  std::vector<G4Track*> vTrack;
+  int iSize=theTrackingManager->GimmeSecondaries()->size();
+  for (int i=iSize-1;i>iSize-ntracks-1;i--)
+    vTrack.push_back((*(theTrackingManager->GimmeSecondaries()))[i]);
+  return vTrack;
 }
