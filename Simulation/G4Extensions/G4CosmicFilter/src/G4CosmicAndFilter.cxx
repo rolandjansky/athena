74428d0027c3bbeef0550960fdfaadb7dd757795
Andrea Di Simone <Andrea.Di.Simone@cern.ch>
2016-01-11 14:43:28 +0100

    fixed action reports (G4CosmicFilter-00-01-05)
    	* fixed but in action reports
    	* tagging G4CosmicFilter-00-01-05
    
    2016-01-08 Andrea Di Simone
    	* implementing MT actions
    	* simplifying property handling: removed step volumeName ->
    	collectionName. The actgions require now directly the collectionNames
    	* tagging G4CosmicFilter-00-01-04
    
    
    Former-commit-id: db026c81fcd5b5691b29ab8c6ec2ad9452d9eb92

--- Simulation/G4Extensions/G4CosmicFilter/src/G4CosmicAndFilter.cxx
+++ Simulation/G4Extensions/G4CosmicFilter/src/G4CosmicAndFilter.cxx
@@ -9,17 +9,18 @@
 #include "G4Event.hh"
 
 
+#include "StoreGate/ReadHandle.h"
 
 G4CosmicAndFilter::G4CosmicAndFilter(const std::string& type, const std::string& name, const IInterface* parent)
   : UserActionBase(type,name,parent)
   , ntracks(0)
   , m_ntot(0)
   , m_npass(0)
-  , m_volumeName("TRTBarrelEntryLayer")
-  , m_volumeName2("CaloEntryLayer")
+  , m_collectionName("TRTBarrelEntryLayer")
+  , m_collectionName2("CaloEntryLayer")
 {
-  declareProperty("VolumeName",m_volumeName);
-  declareProperty("VolumeName2",m_volumeName2);
+  declareProperty("CollectionName",m_collectionName);
+  declareProperty("CollectionName2",m_collectionName2);
 }
 
 void G4CosmicAndFilter::EndOfEvent(const G4Event*)
@@ -27,8 +28,8 @@ void G4CosmicAndFilter::EndOfEvent(const G4Event*)
 
   m_ntot++;
   int counter(0);
-  const DataHandle <TrackRecordCollection> coll;
-  if (evtStore()->retrieve(coll,m_collectionName).isFailure())
+  SG::ReadHandle <TrackRecordCollection> coll(m_collectionName);
+  if (! coll.isValid())
     {
       ATH_MSG_WARNING( "Cannot retrieve TrackRecordCollection " << m_collectionName);
     }
@@ -44,8 +45,8 @@ void G4CosmicAndFilter::EndOfEvent(const G4Event*)
       return;
     }
 
-  const DataHandle <TrackRecordCollection> coll2;
-  if (evtStore()->retrieve(coll2,m_collectionName2).isFailure())
+  SG::ReadHandle <TrackRecordCollection> coll2(m_collectionName2);
+  if (! coll2.isValid())
     {
       ATH_MSG_INFO( "Cannot retrieve TrackRecordCollection " << m_collectionName2 );
     }
@@ -67,8 +68,6 @@ void G4CosmicAndFilter::EndOfEvent(const G4Event*)
 
 StatusCode G4CosmicAndFilter::initialize()
 {
-  m_collectionName=m_volumeName;
-  m_collectionName2=m_volumeName2;
 
   ATH_MSG_INFO( "using collectionName "<<m_collectionName << " and " <<m_collectionName2 );
 
@@ -95,3 +94,69 @@ StatusCode G4CosmicAndFilter::queryInterface(const InterfaceID& riid, void** ppv
     }
   return StatusCode::SUCCESS;
 }
+
+
+
+
+#include "G4CosmicFilter/G4CosmicAndFilter.h"
+
+
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/IMessageSvc.h"
+
+namespace G4UA{
+
+
+  G4CosmicAndFilter::G4CosmicAndFilter(const Config& config):
+    AthMessaging(Gaudi::svcLocator()->service< IMessageSvc >( "MessageSvc" ),"G4CosmicAndFilter"),
+    m_config(config),m_report(),
+    m_evtStore("StoreGateSvc/StoreGateSvc","G4CosmicAndFilter"),
+    m_detStore("StoreGateSvc/DetectorStore","G4CosmicAndFilter"){;
+  }
+
+  void G4CosmicAndFilter::endOfEvent(const G4Event*){;
+
+    m_report.ntot++;
+    int counter(0);
+    SG::ReadHandle <TrackRecordCollection> coll(m_config.collectionName);
+    if (! coll.isValid())
+      {
+	ATH_MSG_WARNING( "Cannot retrieve TrackRecordCollection " << m_config.collectionName);
+      }
+    else
+      {
+	counter = coll->size();
+      }
+    
+    if (counter==0)
+      {
+	ATH_MSG_INFO("aborting event due to failing AND filter");
+	G4RunManager::GetRunManager()->AbortEvent();
+	return;
+      }
+    
+    SG::ReadHandle <TrackRecordCollection> coll2(m_config.collectionName2);
+    if (! coll2.isValid())
+    {
+      ATH_MSG_INFO( "Cannot retrieve TrackRecordCollection " << m_config.collectionName2 );
+    }
+    else
+      {
+	counter = coll2->size();
+      }
+
+    if (counter==0)
+      {
+	ATH_MSG_INFO("aborting event due to failing AND filter");
+	G4RunManager::GetRunManager()->AbortEvent();
+	return;
+      }
+    
+    m_report.npass++;
+    return;
+
+  }
+
+
+} // namespace G4UA 
