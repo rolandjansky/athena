3434b2a2a8fe37e597dfc645878672ce513410c6
Matthias Danninger <matthias.danninger@cern.ch>
2016-01-14 22:55:39 +0100

    bugfixes in response to Jira ATLASRECTS-2707 (InDetReadoutGeometry-02-00-17-06)
    2015-12-07 Shaun Roe
    	* clang compilation corrections OBO Scott Snyder
    	* tag InDetReadoutGeometry-02-00-17-05

--- InnerDetector/InDetDetDescr/InDetReadoutGeometry/src/SiCellId.cxx
+++ InnerDetector/InDetDetDescr/InDetReadoutGeometry/src/SiCellId.cxx
@@ -4,12 +4,13 @@
 
 #include "InDetReadoutGeometry/SiCellId.h"
 
-std::ostream & operator << (std::ostream & os, const InDetDD::SiCellId & cellId)
-{
-  if (cellId.isValid()){
-    return os << "[" << cellId.phiIndex() << "." << cellId.etaIndex() << "]";
-  } else {
-    return os << "[INVALID]";
-  }
-}
-
+namespace InDetDD{
+	std::ostream & operator << (std::ostream & os, const SiCellId & cellId)
+	{
+		if (cellId.isValid()){
+			return os << "[" << cellId.phiIndex() << "." << cellId.etaIndex() << "]";
+		} else {
+			return os << "[INVALID]";
+		}
+	}
+}//namespace InDetDD
