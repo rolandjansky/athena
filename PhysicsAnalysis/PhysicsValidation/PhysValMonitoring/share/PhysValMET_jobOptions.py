84546ada75526edaddb5eefb81fb8749508299b4
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2016-01-21 17:17:34 +0100

    21.01.2016 Mark Hodgkinson     * share/PhysValMET_jobOptions.py: Add Philip Moggs updates for new MET histograms.     * Tag as PhysValMonitoring-00-01-43 (PhysValMonitoring-00-01-43)
        * share/PhysValMET_jobOptions.py: Add Philip Moggs updates for new MET histograms.
        * Tag as PhysValMonitoring-00-01-43

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMET_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValMET_jobOptions.py
@@ -9,6 +9,37 @@ if recFlags.doTruth():
     tool1.DoTruth = True
 
 from AthenaCommon.AppMgr import ToolSvc
+from AthenaCommon import CfgMgr
+jvtTool = CfgMgr.JetVertexTaggerTool('JVTTool')
+ToolSvc += jvtTool
+tool1.JVTTool = jvtTool
+
+mettool = CfgMgr.met__METMaker('METMaker')
+ToolSvc += mettool
+tool1.METMaker = mettool
+
+museltool = CfgMgr.CP__MuonSelectionTool("MuonSelectionTool",
+                                       MuQuality=1, # Medium
+                                       MaxEta=2.4)
+ToolSvc += museltool
+tool1.MuonSelectionTool = museltool
+
+eseltool = CfgMgr.AsgElectronLikelihoodTool("ElectronLHSelectionTool",
+                                            ConfigFile="ElectronPhotonSelectorTools/offline/mc15_20150429/ElectronLikelihoodMediumOfflineConfig2015.conf")
+ToolSvc += eseltool
+tool1.ElectronLHSelectionTool = eseltool
+
+from ROOT import egammaPID
+phoseltool = CfgMgr.AsgPhotonIsEMSelector("PhotonIsEMSelectionTool",
+                                             isEMMask=egammaPID.PhotonTight,
+                                             ConfigFile="ElectronPhotonSelectorTools/offline/mc15_20150429/PhotonIsEMTightSelectorCutDefs.conf")
+ToolSvc += phoseltool
+tool1.PhotonIsEMSelectionTool = phoseltool
+
+tauseltool = CfgMgr.TauAnalysisTools__TauSelectionTool("TauSelectionTool")
+ToolSvc += tauseltool
+tool1.TauSelectionTool = tauseltool
+
 ToolSvc += tool1
 
 monMan = CfgMgr.AthenaMonManager("PhysValMonManager")
