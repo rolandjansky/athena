a15ea1c43599fab24c4679817427e473c6e0fc29
Ioannis Nomidis <Ioannis.Nomidis@cern.ch>
2016-01-23 19:19:17 +0100

    Added charge-dependant plots / minor fixes to constructor (MuonHistUtils-00-03-26)
    	* Added charge-dependant plots / minor fixes to constructor
    	* Tagging MuonHistUtils-00-03-26
    
    2016-01-15 <Ioannis.Nomidis@Cern.ch>
    	* Added charge-dependant plots
    	* Tagging MuonHistUtils-00-03-25

---
+++ MuonSpectrometer/MuonValidation/MuonHistogramming/MuonHistUtils/MuonHistUtils/ChargeDepParamPlots.h
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUONHISTUTILS_CHARGEDEPPARAMPLOTS_H
+#define MUONHISTUTILS_CHARGEDEPPARAMPLOTS_H
+
+#include "TrkValHistUtils/PlotBase.h"
+#include "xAODTracking/TrackParticle.h"
+#include "xAODMuon/Muon.h"
+
+namespace Muon{
+
+class ChargeDepParamPlots:public PlotBase {
+  public:      
+    ChargeDepParamPlots(PlotBase *pParent, std::string sDir, std::string sType);
+    //void fill(const xAOD::TrackParticle& trkprt);
+    void fill(const xAOD::Muon& mu);
+
+    TH1* q_times_eta;
+    TH1* q_over_p;
+    TH1* q_over_pt;
+    TH1* qMS_times_qCB;
+    TH1* qID_times_qCB;
+
+  private:
+    void initializePlots();
+    std::string m_sType;
+};
+
+}
+
+#endif // MUONHISTUTILS_CHARGEDEPPARAMPLOTS_H
+
