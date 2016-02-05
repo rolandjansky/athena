6a3f4553297462fcf04417e65457903f3d1e3736
Federica Fabbri <federica.fabbri@cern.ch>
2016-02-05 11:58:01 +0100

    Tagging version FastSiDigitization-00-01-11 (FastSiDigitization-00-01-11)
           * Added share/preInclude.FastPixelDigi_ITK.py to run fast pixel digi
    	LoI geometry
           * Added share/preInclude.RecoPixelFastDigi.py to run reconstruction on
    	FastPixelDigi output

---
+++ InnerDetector/InDetDigitization/FastSiDigitization/share/preInclude.RecoPixelFastDigi.py
@@ -0,0 +1,7 @@
+from InDetRecExample.InDetJobProperties import InDetFlags
+InDetFlags.pixelClusterSplittingType = 'AnalogClus'
+InDetFlags.doTIDE_Ambi.set_Value_and_Lock(False)
+InDetFlags.doSCT_PRDFormation.set_Value_and_Lock(True)
+InDetFlags.doTRT_PRDFormation.set_Value_and_Lock(True)
+InDetFlags.doPixelPRDFormation.set_Value_and_Lock(False)
+#InDetFlags.doStandardPlots  = True
