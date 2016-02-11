bd301e9d2b51564bcead319d495ba9dd6cc910b8
Ryan Mackenzie White <ryan.white@cern.ch>
2016-02-11 17:48:47 +0100

    revert (TrigEgammaMonitoring-00-01-64)

--- Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/python/TrigEgammaMonitoringConfig.py
@@ -1,74 +1,94 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-def HLTEgammaMonitoringDumpTool():
-  from AthenaCommon.AppMgr import ToolSvc
-  from TrigEgammaMonitoring.TrigEgammaMonitoringConf import HLTEgammaDumpTool
-  HLTEgammaDump = HLTEgammaDumpTool(name = 'HLTEgammaDump', histoPathBase = "/Trigger/HLT")
-  ToolSvc += HLTEgammaDump;
-  list = ['HLTEgammaDumpTool/HLTEgammaDump'];
-  return list
+
+# Following method taken from TrigEgammaMonToolConfig found in TrigEgammaAnalysisTools/TrigEgammaMonToolConfig.py
+# Configuration should be updated and kept in this package for monitoring
+# Provide all triggers to be monitored
+# Provide list of categories and shifter plots
 
 def TrigEgammaMonitoringTool():
-  from AthenaCommon.AppMgr import ToolSvc
-  from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaNavAnalysisTool,TrigEgammaNavTPAnalysisTool,TrigEgammaNavTPJpsieeAnalysisTool
-  from TrigHLTMonitoring.HLTMonTriggerList import hltmonList
+    from AthenaCommon.AppMgr import ToolSvc
+    from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaNavAnalysisTool,TrigEgammaNavTPAnalysisTool,TrigEgammaNavTPJpsieeAnalysisTool
+    from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConfig import TrigEgammaPlotTool
 
-  probelist1 = hltmonList.primary_single_pho + hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased + hltmonList.primary_single_ele_iso + hltmonList.primary_single_ele_cutbased_iso
-  probelist1 += ['L1_EM18VH', 'L1_EM20VH', 'L1_EM20VHI']
-  probelist2 = hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased + hltmonList.primary_single_ele_iso + hltmonList.primary_single_ele_cutbased_iso + hltmonList.monitoring_Zee + hltmonList.monitoring_Jpsiee
-  probelist2 += ['e24_tight_iloose_HLTCalo_L1EM20VH', 'e24_lhtight_iloose_L2EFCalo_L1EM20VH', 'e24_lhmedium_cutd0dphideta_iloose_L1EM20VH', 'e24_lhmedium_nod0_iloose_L1EM20VH', 'e24_lhmedium_nodeta_iloose_L1EM20VH', 'e24_lhmedium_nodphires_iloose_L1EM20VH', 'L1_EM18VH', 'L1_EM20VH', 'L1_EM20VHI']
-  probelistJPsi = ['e5_loose','e5_lhloose','e5_vloose','e5_lhvloose']
-  probelistZee = hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased
+    # Move the ProbeList into MonitCategory to keep all configuration in TrigEgammaMonitoring 
+    from TrigEgammaAnalysisTools.TrigEgammaProbelist import monitoring_mam, monitoring_electron, monitoring_photon 
+    from TrigEgammaAnalysisTools.TrigEgammaProbelist import monitoringTP_electron, monitoringTP_electronZee, monitoringTP_electronJpsiee 
+   
+    #Define the base path for all histograms
+    basePath = '/HLT/Egamma'
+    
+    #Configure the TrigEgammaPlotTool
+    #MaM maps a category name (creates a directory) to a trigger (copies histos from trigger dir to category dir)
+    #Efficiency,Distribution,Resolution -- list of plots to monitor for shifter MaM categories
+    PlotTool = TrigEgammaPlotTool.copy(name="TrigEgammaPlotTool",
+            DirectoryPath=basePath,
+            MaM=monitoring_mam,
+            Efficiency=["eff_et","eff_eta","eff_mu"],
+            Distribution=["et","eta","d0","d0sig"],
+            Resolution=["res_et","res_eta","res_Rhad","res_Rphi","res_Reta"])
 
-  basePath = '/HLT/Egamma/'
-  tagItems = hltmonList.primary_single_ele + hltmonList.primary_single_ele_cutbased
-  tagItemsJPsi = ['e5_tight_e4_etcut','e9_tight_e4_etcut','e14_tight_e4_etcut','e9_etcut_e5_tight','e14_etcut_e5_tight','e5_tight_e4_etcut_Jpsiee','e9_tight_e4_etcut_Jpsiee','e14_tight_e4_etcut_Jpsiee','e9_etcut_e5_tight_Jpsiee','e14_etcut_e5_tight_Jpsiee']
-  tagItemsZee = hltmonList.monitoring_Zee
+    tagItems = ['HLT_e24_lhmedium_iloose_L1EM18VH',
+        'HLT_e24_lhmedium_iloose_L1EM20VH',
+        'HLT_e24_lhtight_iloose',
+        'HLT_e26_lhtight_iloose',
+        # Primary cut-based electron triggers
+        'HLT_e24_medium_iloose_L1EM18VH',
+        'HLT_e24_medium_iloose_L1EM20VH',
+        'HLT_e24_tight_iloose',
+        'HLT_e26_tight_iloose']
 
-  Analysis = TrigEgammaNavAnalysisTool(name='NavAnalysis',
-                                       DirectoryPath=basePath+'Analysis',
-                                       TriggerList=probelist1,
-                                       File="",
-                                       OutputLevel=0)
-  TPAnalysis = TrigEgammaNavTPAnalysisTool(name='NavTPAnalysis',
-                                           DirectoryPath=basePath+'TPAnalysis',
-                                           TriggerList=probelist2,
-                                           File="",
-                                           TagTriggerList=tagItems,
-                                           OutputLevel=0)
-  JpsiTPAnalysis = TrigEgammaNavTPJpsieeAnalysisTool(name='NavTPJpsieeAnalysis',
-                                                     DirectoryPath=basePath+'TPJpsieeAnalysis',
-                                                     TriggerList=probelistJPsi,
-                                                     File="",
-                                                     TagTriggerList=tagItemsJPsi,
-                                                     RemoveCrack=True,
-                                                     ZeeLowerMass=2,
-                                                     ZeeUpperMass=5,
-                                                     OfflineTagMinEt=5,
-                                                     OfflineProbeMinEt=5,
-                                                     OutputLevel=0,
-                                                     DetailedHistograms=False,
-                                                     doJpsiee=True)
-  ZeeTPAnalysis = TrigEgammaNavTPJpsieeAnalysisTool(name='NavTPZeeAnalysis',
-                                                    DirectoryPath=basePath+'TPZeeAnalysis',
-                                                    TriggerList=probelistZee,
-                                                    File="",
-                                                    TagTriggerList=tagItemsZee,
-                                                    RemoveCrack=True,
-                                                    ZeeLowerMass=80,
-                                                    ZeeUpperMass=100,
-                                                    OfflineTagMinEt=25,
-                                                    OfflineProbeMinEt=24,
-                                                    OutputLevel=0,
-                                                    DetailedHistograms=False,
-                                                    doJpsiee=False)
-  from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConf import TrigEgammaMonTool
-  TrigEgammaMon = TrigEgammaMonTool( name = "TrigEgammaMon", 
-                                     histoPathBase=basePath,
-                                     Tools=["TrigEgammaNavAnalysisTool/NavAnalysis",
-                                            "TrigEgammaNavTPAnalysisTool/NavTPAnalysis",
-                                            "TrigEgammaNavTPAnalysisTool/NavTPJpsieeAnalysis",
-                                            "TrigEgammaNavTPAnalysisTool/NavTPZeeAnalysis"])
-  ToolSvc += TrigEgammaMon;
-  list = ['TrigEgammaMonTool/TrigEgammaMon'];
-  return list
+    JpsitagItems = ['HLT_e5_tight_e4_etcut',
+                    'HLT_e9_tight_e4_etcut',
+                    'HLT_e14_tight_e4_etcut',
+                    'HLT_e9_etcut_e5_tight',
+                    'HLT_e14_etcut_e5_tight',
+                    # Primary cut-based electron triggers
+                    'HLT_e5_tight_e4_etcut_Jpsiee',
+                    'HLT_e9_tight_e4_etcut_Jpsiee',
+                    'HLT_e14_tight_e4_etcut_Jpsiee',
+                    'HLT_e9_etcut_e5_tight_Jpsiee',
+                    'HLT_e14_etcut_e5_tight_Jpsiee']
+
+    # For MaM, most important is the list of triggers to monitor
+    # Currently these are found in TRigEgammProbelist
+    # We should use the TrigEgammaMonitCategory to define lists 
+    # Use the MaM categories from those lists
+    ElectronAnalysis = TrigEgammaNavAnalysisTool(name='NavElectronAnalysis',
+            Analysis='Electrons',
+            PlotTool=PlotTool,
+            TriggerList=monitoring_electron,
+            File="",
+            OutputLevel=0,DetailedHistograms=False)
+    PhotonAnalysis = TrigEgammaNavAnalysisTool(name='NavPhotonAnalysis',
+            Analysis='Photons',
+            PlotTool=PlotTool,
+            TriggerList=monitoring_photon,
+            File="",
+            OutputLevel=0,DetailedHistograms=False)
+    TPAnalysis = TrigEgammaNavTPAnalysisTool(name='NavTPAnalysis',
+            Analysis='Zee',
+            PlotTool=PlotTool,
+            TriggerList=monitoringTP_electron,
+            File="",
+            TagTriggerList=tagItems,
+            RemoveCrack=False,
+            OutputLevel=0,DetailedHistograms=False)
+
+    JpsiTPAnalysis = TrigEgammaNavTPJpsieeAnalysisTool(name='NavTPJpsieeAnalysis',
+                                                        Analysis='Jpsiee',
+                                                        PlotTool=PlotTool,
+                                                        TriggerList=monitoringTP_electronJpsiee,
+                                                        File="",
+                                                        TagTriggerList= JpsitagItems)
+
+    from TrigEgammaAnalysisTools.TrigEgammaAnalysisToolsConf import TrigEgammaMonTool
+    TrigEgammaMonTool = TrigEgammaMonTool( name = "HLTEgammaMon", 
+            histoPathBase=basePath,
+            Tools=["TrigEgammaNavAnalysisTool/NavPhotonAnalysis",
+                    "TrigEgammaNavAnalysisTool/NavElectronAnalysis",
+                    "TrigEgammaNavTPAnalysisTool/NavTPAnalysis",
+                    "TrigEgammaNavTPAnalysisTool/NavTPJpsieeAnalysis"])
+    ToolSvc += TrigEgammaMonTool
+    list = ['TrigEgammaMonTool/HLTEgammaMon'];
+    return list
