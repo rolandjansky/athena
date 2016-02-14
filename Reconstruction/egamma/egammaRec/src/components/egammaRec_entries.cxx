fbc13cc1116d8afc40569ed3e590534e77b00744
Christos Anastopoulos <Christos.Anastopoulos@cern.ch>
2016-02-14 20:40:46 +0100

    Revert Factories to make sure I can check just the scheduling (egammaRec-07-01-24-02)
    	* Backport removal of egammaLocker
    	* Revert Factories to make sure I can check just the scheduling
    	* it seems we affect the calculartion of Isolation corrections.
    	* Tagging as egammaRec-07-01-24-02
    
    2016-02-12 Christos Anastopoulos  <Christos.Anastopoulos@cern.ch>
    
    	* Backport removal of egammaLocker
    	* Tagging as egammaRec-07-01-24-01

--- Reconstruction/egamma/egammaRec/src/components/egammaRec_entries.cxx
+++ Reconstruction/egamma/egammaRec/src/components/egammaRec_entries.cxx
@@ -5,21 +5,23 @@
 #include "egammaRec/egammaForwardBuilder.h"
 #include "egammaRec/egammaTruthAssociationAlg.h"
 #include "egammaRec/egammaTrackSlimmer.h"
+#include "egammaRec/egammaLockCollections.h"
 
 
-DECLARE_ALGORITHM_FACTORY( egammaBuilder             )
-DECLARE_ALGORITHM_FACTORY( topoEgammaBuilder          )
-DECLARE_ALGORITHM_FACTORY( egammaForwardBuilder      )
-DECLARE_ALGORITHM_FACTORY( egammaTruthAssociationAlg )
+DECLARE_ALGORITHM_FACTORY( egammaBuilder            )
+DECLARE_ALGORITHM_FACTORY( topoEgammaBuilder        )
+DECLARE_ALGORITHM_FACTORY( egammaForwardBuilder     )
+DECLARE_ALGORITHM_FACTORY( egammaTruthAssociationAlg)
 DECLARE_ALGORITHM_FACTORY( egammaTrackSlimmer       )
-
+DECLARE_ALGORITHM_FACTORY( egammaLockCollections        )
 
 
 DECLARE_FACTORY_ENTRIES(egammaRec) {
-     DECLARE_ALGORITHM( egammaBuilder            )
+     DECLARE_ALGORITHM( egammaBuilder             )
      DECLARE_ALGORITHM( topoEgammaBuilder         )
-     DECLARE_ALGORITHM( egammaForwardBuilder     )
+     DECLARE_ALGORITHM( egammaForwardBuilder      )
      DECLARE_ALGORITHM( egammaTruthAssociationAlg )
-     DECLARE_ALGORITHM( egammaTrackSlimmer       )
+     DECLARE_ALGORITHM( egammaTrackSlimmer        )
+     DECLARE_ALGORITHM( egammaLockCollections     )
 
 }
