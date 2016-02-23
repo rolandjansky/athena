39802b6f0cb3d615bb2cb0b3647c57be31d8fc9b
Dave Casper <dcasper@uci.edu>
2016-02-23 06:12:21 +0100

    Add VertexStoringTool to support persistence of vertex seeds (TrkVertexTools-01-01-03)
    	* Add VertexStoringTool to support persistence of seeds
    	* Tag as TrkVertexTools-01-01-03
    
    
    Former-commit-id: 8ea9ffe7ef08d7d6aa4b650ded599b0d0e17b044

--- Tracking/TrkVertexFitter/TrkVertexTools/src/components/TrkVertexTools_entries.cxx
+++ Tracking/TrkVertexFitter/TrkVertexTools/src/components/TrkVertexTools_entries.cxx
@@ -5,6 +5,7 @@
 #include "TrkVertexTools/VertexMergingTool.h"
 #include "TrkVertexTools/DummyVertexSelectionTool.h" 
 #include "TrkVertexTools/TruthVertexSelectionTool.h"  
+#include "TrkVertexTools/VertexStoringTool.h"
  
 using namespace Trk;
  
@@ -12,7 +13,7 @@ DECLARE_TOOL_FACTORY(VertexCollectionSortingTool)
 DECLARE_TOOL_FACTORY(VertexMergingTool)
 DECLARE_TOOL_FACTORY(DummyVertexSelectionTool)
 DECLARE_TOOL_FACTORY(TruthVertexSelectionTool)
-
+DECLARE_TOOL_FACTORY(VertexStoringTool)
     
 DECLARE_FACTORY_ENTRIES(TrkVertexTools)
 {
@@ -20,7 +21,7 @@ DECLARE_FACTORY_ENTRIES(TrkVertexTools)
   DECLARE_TOOL(VertexMergingTool)
   DECLARE_TOOL(DummyVertexSelectionTool)
   DECLARE_TOOL(TruthVertexSelectionTool)
-      
+  DECLARE_TOOL(VertexStoringTool)
 }
  
 
