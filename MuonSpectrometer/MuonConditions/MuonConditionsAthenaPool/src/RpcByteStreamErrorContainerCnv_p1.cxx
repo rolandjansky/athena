94dfdf2b7a0710e172d5fb3518b45458a00ebc3b
Attila Krasznahorkay <krasznaa@cern.ch>
2016-02-26 14:58:00 +0100

    Adding a hand-written CMakeLists.txt file to the package (MuonConditionsAthenaPool-00-00-10)
    	* Added a hand-written CMakeLists.txt file to the package to
    	  build its unit test correctly.
    	* Removed the executable flag from the source files.
    	* Tagging as MuonConditionsAthenaPool-00-00-10
    
    2015-10-16  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MuonConditionsAthenaPool-00-00-09.
    	* test/MuonConditionsAthenaPool.xml: Add test xml file.
    
    2015-10-15  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MuonConditionsAthenaPool-00-00-08.
    	* src/RpcByteStreamErrorContainerCnv_p1.cxx,
    	src/RpcByteStreamErrorContainerCnv.cxx: Remove #define private
    	public. (gcc5 port)
    	* Tagging MuonConditionsAthenaPool-00-00-07.
    	* Add unit test.
    
    
    Former-commit-id: 6283430e5d39b641be746b11265b859027474669

--- MuonSpectrometer/MuonConditions/MuonConditionsAthenaPool/src/RpcByteStreamErrorContainerCnv_p1.cxx
+++ MuonSpectrometer/MuonConditions/MuonConditionsAthenaPool/src/RpcByteStreamErrorContainerCnv_p1.cxx
@@ -6,12 +6,7 @@
 // file:   copied from ChamberT0sCnv_p1.cxx
 //-----------------------------------------------------------------------------
 
-#define private public
-#define protected public
 #include "MuonByteStreamErrors/RpcByteStreamErrorContainer.h"
-#undef private
-#undef protected
-
 #include "MuonConditionsAthenaPool/MuonByteStreamErrors/RpcByteStreamErrorContainerCnv_p1.h"
 #include <utility>
 
@@ -19,12 +14,10 @@ void RpcByteStreamErrorContainerCnv_p1::
 persToTrans( const Muon::RpcByteStreamErrorContainer_p1 *persObj, Muon::RpcByteStreamErrorContainer *transObj,MsgStream & log ) 
 {
     if (log.level() <= MSG::DEBUG) log << MSG::DEBUG << "RpcByteStreamErrorContainerCnv_p1::persToTrans" << endreq;
-    transObj->m_errorVector.clear();
-    transObj->m_errorVector.reserve( persObj->m_errorVector.size() );
+    transObj->clear();
     DataVector<std::pair<IdentifierHash, int> >::const_iterator it=persObj->m_errorVector.begin(), itEnd=persObj->m_errorVector.end();
     for (;it!=itEnd;++it) {
-      std::pair<IdentifierHash, int >* myPointer = new std::pair<IdentifierHash, int>(std::make_pair( (*it)->first, (*it)->second));
-      transObj->m_errorVector.push_back( myPointer );
+      transObj->addError ((*it)->first, (*it)->second);
     }
 }
 
@@ -32,11 +25,11 @@ void RpcByteStreamErrorContainerCnv_p1::
 transToPers( const Muon::RpcByteStreamErrorContainer *transObj, Muon::RpcByteStreamErrorContainer_p1 *persObj, MsgStream & log )
 {
     if (log.level() <= MSG::DEBUG) log << MSG::DEBUG << "RpcByteStreamErrorContainerCnv_p1::transToPers" << endreq;
-    using namespace std;
-    persObj->m_errorVector.reserve( transObj->m_errorVector.size() );
-    DataVector<std::pair<IdentifierHash, int> >::const_iterator it=transObj->m_errorVector.begin(), itEnd=transObj->m_errorVector.end();
-    for (;it!=itEnd;++it) {
-      persObj->m_errorVector.push_back( new pair<IdentifierHash, int>(make_pair( (*it)->first, (*it)->second)) );
+    const DataVector<std::pair<IdentifierHash, int> >& errs =
+      transObj->getAllErrors();
+    persObj->m_errorVector.reserve( errs.size() );
+    for (const std::pair<IdentifierHash, int>* p : errs) {
+      persObj->m_errorVector.push_back( new std::pair<IdentifierHash, int>( p->first, p->second) );
     }
 }
 
---
+++ MuonSpectrometer/MuonConditions/MuonConditionsAthenaPool/test/RpcByteStreamErrorContainerCnv_p1_test.cxx
@@ -0,0 +1,58 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id$
+/**
+ * @file MuonConditionsAthenaPool/test/RpcByteStreamErrorContainerCnv_p1_test.cxx
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Oct, 2015
+ * @brief Unit test
+ */
+
+#undef NDEBUG
+#include "MuonConditionsAthenaPool/MuonByteStreamErrors/RpcByteStreamErrorContainerCnv_p1.h"
+#include "GaudiKernel/MsgStream.h"
+#include <iostream>
+#include <cassert>
+
+
+void compare (const Muon::RpcByteStreamErrorContainer& c1,
+              const Muon::RpcByteStreamErrorContainer& c2)
+{
+  const DataVector<std::pair<IdentifierHash, int> >& err1 = c1.getAllErrors();
+  const DataVector<std::pair<IdentifierHash, int> >& err2 = c2.getAllErrors();
+  assert (err1.size() == err2.size());
+  for (size_t i = 0; i < err1.size(); i++) {
+    assert (*err1[i] == *err2[i]);
+  }
+}
+
+void testit (const Muon::RpcByteStreamErrorContainer& trans1)
+{
+  RpcByteStreamErrorContainerCnv_p1 cnv;
+  Muon::RpcByteStreamErrorContainer_p1 pers;
+  MsgStream log (0, "test");
+  cnv.transToPers (&trans1, &pers, log);
+  Muon::RpcByteStreamErrorContainer trans2;
+  cnv.persToTrans (&pers, &trans2, log);
+  compare (trans1, trans2);
+}
+
+
+void test1()
+{
+  std::cout << "test1\n";
+  Muon::RpcByteStreamErrorContainer c;
+  c.addError (1, 2);
+  c.addError (3, 4);
+  c.addError (5, 6);
+  testit (c);
+}
+
+
+int main()
+{
+  test1();
+  return 0;
+}
