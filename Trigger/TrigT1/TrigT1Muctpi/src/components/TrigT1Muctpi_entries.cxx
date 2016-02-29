2836069f329e04c6fef53dc7792ec8a0ec416b9d
Thorsten Wengler <wengler@cern.ch>
2016-02-29 12:23:51 +0100

    bug fix of unchecked status code (TrigT1Muctpi-00-06-31)

--- Trigger/TrigT1/TrigT1Muctpi/src/components/TrigT1Muctpi_entries.cxx
+++ Trigger/TrigT1/TrigT1Muctpi/src/components/TrigT1Muctpi_entries.cxx
@@ -1,4 +1,4 @@
-// $Id: TrigT1Muctpi_entries.cxx 500504 2012-05-14 11:39:28Z krasznaa $
+// $Id: TrigT1Muctpi_entries.cxx 726107 2016-02-25 11:04:42Z wengler $
 
 // Gaudi include(s):
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -7,13 +7,19 @@
 #include "../Algorithms/L1Muctpi.h"
 #include "../Algorithms/L1MuctpiPatGen.h"
 #include "../Algorithms/L1MuctpiTestReadResults.h"
+#include "../TrigT1Muctpi/L1MuctpiTool.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( LVL1MUCTPI, L1Muctpi )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( LVL1MUCTPI, L1MuctpiPatGen )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( LVL1MUCTPI, L1MuctpiTestReadResults )
 
+DECLARE_NAMESPACE_TOOL_FACTORY( LVL1MUCTPI, L1MuctpiTool )
+
+
 DECLARE_FACTORY_ENTRIES( TrigT1Muctpi ) {
    DECLARE_NAMESPACE_ALGORITHM( LVL1MUCTPI, L1Muctpi )
    DECLARE_NAMESPACE_ALGORITHM( LVL1MUCTPI, L1MuctpiPatGen )
    DECLARE_NAMESPACE_ALGORITHM( LVL1MUCTPI, L1MuctpiTestReadResults )
+
+   DECLARE_NAMESPACE_TOOL( LVL1MUCTPI, L1MuctpiTool )
 }
