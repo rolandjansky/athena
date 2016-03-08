6eea40dd3912208a7928e459e681c54c6912d6db
Scott Snyder <scott.snyder@cern.ch>
2016-03-08 17:41:53 +0100

    Add missing HepMC:: qualifiers. (Epos_i-00-01-06)
    	* Tagging Epos_i-00-01-06.
    	* Add missing HepMC:: qualifiers.
    
    2016-03-04 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Extending the requirements file a bit, to make the package's
    	  build successful with CMake.
    	* Tagging as Epos_i-00-01-05
    
    2015-12-17 E. M. Lobodzinska <ewelina@mail.desy.de>
            * add comment and printout explaining the signal_process_id
            * tagging as Epos_i-00-01-04

--- Generators/Epos_i/src/Epos.cxx
+++ Generators/Epos_i/src/Epos.cxx
@@ -194,6 +194,7 @@ Epos::~Epos()
 StatusCode Epos::genInitialize() 
 {
   ATH_MSG_INFO( " CRMC INITIALISING.\n" );
+  ATH_MSG_INFO( "signal_rocess_id 101-ND, 105-DD, 102-CD, 103 AB->XB, 104 AB->AX \n");
 
   static const bool CREATEIFNOTTHERE = true;
   
@@ -212,7 +213,7 @@ StatusCode Epos::genInitialize()
 
   // set up initial values
 
-    std::cout << "parameters " << m_nEvents << " " << iSeed << " " << m_beamMomentum << " " << m_targetMomentum << " " << m_primaryParticle << " " << m_targetParticle << " " << m_model << " " << m_itab << " " << m_ilheout << " " <<  m_lheout.c_str()<< " " <<  m_paramFile.c_str() << std::endl;
+  //   std::cout << "parameters " << m_nEvents << " " << iSeed << " " << m_beamMomentum << " " << m_targetMomentum << " " << m_primaryParticle << " " << m_targetParticle << " " << m_model << " " << m_itab << " " << m_ilheout << " " <<  m_lheout.c_str()<< " " <<  m_paramFile.c_str() << std::endl;
 
     crmc_set_f_(m_nEvents, iSeed, m_beamMomentum, m_targetMomentum, m_primaryParticle, m_targetParticle, m_model, m_itab, m_ilheout, m_paramFile.c_str() ); 
 
@@ -305,7 +306,7 @@ StatusCode Epos::genFinalize()
 }
 
 // ---------------------------------------------------------------------- 
-StatusCode Epos::fillEvt( GenEvent* evt ) 
+StatusCode Epos::fillEvt( HepMC::GenEvent* evt ) 
 {
   //  ATH_MSG_INFO( " EPOS Filling.\n" );
 
@@ -365,6 +366,7 @@ StatusCode Epos::fillEvt( GenEvent* evt )
 		      evt->set_heavy_ion(ion);  
 
   //an integer ID uniquely specifying the signal process (i.e. MSUB in Pythia)
+  // Epos 1-ND, 2-DD, 3-CD, 4 AB->XB, -4 AB->AX translated into signal_proces_id 101-ND, 105 - DD, 102 - CD, 103 -AB->XB, 104 AB->AX
    int sig_id = -1;
    switch (int(c2evt_.typevt))
     {
@@ -381,9 +383,9 @@ StatusCode Epos::fillEvt( GenEvent* evt )
 
   evt->set_signal_process_id(sig_id);
 
-  /*  ATH_MSG_INFO( "Event Information : \n");
-  std::cout << " signal_process_id "<<evt->signal_process_id() << std::endl;
-  std::cout << " heavy ion " << evt->heavy_ion() << std::endl;
+  //  ATH_MSG_INFO( "Event Information : \n");
+  // std::cout << " signal_process_id "<<evt->signal_process_id() << std::endl;
+  /*std::cout << " heavy ion " << evt->heavy_ion() << std::endl;
   evt->print();
   std::cout << " print-out done " << std::endl;*/
 
