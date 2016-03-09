83d410a003de483694afcdd40fa9b14f79bab186
Steven Andrew Farrell <steven.farrell@cern.ch>
2016-03-09 04:51:17 +0100

    get secondaries from G4Step; move to TruthStrategyUtils (G4TruthStrategies-00-01-04)
    	* Migrated the truth strategies to get secondaries from the G4Step
    	  method GetSecondaryInCurrentStep and to call saveSecondarVertex in
    	  TruthStrategyUtils rather than the TruthStrategyManager. These
    	  updates allow to run the truth-filling code in AthenaMT.
    	* Tagging: G4TruthStrategies-00-01-04
    
    2016-01-05 Steve Farrell <Steven.Farrell@cern.ch>
    	* Removed thread-unsafe usages of StepHelper in the strategies. The
    	  helper wasn't actually helping anyway; the code is shorter without
    	  it.
    	* Tagging: G4TruthStrategies-00-01-03
    
    2015-12-15 Steve Farrell <Steven.Farrell@cern.ch>
    	* Code cleanup.
    	* Passing const strings by reference to constructors.
    	* Tagging: G4TruthStrategies-00-01-02
    
    
    Former-commit-id: 178ce1019b18e31683c52cc27ca2903c19d39c17

--- Simulation/G4Utilities/G4TruthStrategies/src/CaloCatchAllStrategy.cxx
+++ Simulation/G4Utilities/G4TruthStrategies/src/CaloCatchAllStrategy.cxx
@@ -3,69 +3,91 @@
 */
 
 #include "G4TruthStrategies/CaloCatchAllStrategy.h"
-#include "SimHelpers/StepHelper.h"
+
+#include "MCTruthBase/TruthStrategyUtils.h"
+
+#include "G4EmProcessSubType.hh"
 #include "G4Step.hh"
+#include "G4VProcess.hh"
+
 #include <iostream>
+#include <cassert>
 
 #include "CLHEP/Units/PhysicalConstants.h"
 
 static CaloCatchAllStrategy caloStrategy("CALOCatchAll");
 
-CaloCatchAllStrategy::CaloCatchAllStrategy(const std::string n):
-			TruthStrategy(n)
+CaloCatchAllStrategy::CaloCatchAllStrategy(const std::string& n)
+  : TruthStrategy(n)
 {
-
-	if(theTruthManager->GetTruthParameter("CaloMuBremsPrimaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"CaloMuBremsPrimaryMinEnergy: setting default value of 1GeV"<<std::endl;
-	  theTruthManager->SetTruthParameter("CaloMuBremsPrimaryMinEnergy", 1000.);
-	}
-	if(theTruthManager->GetTruthParameter("CaloMuBremsSecondaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"CaloMuBremsPrimaryMinEnergy: setting default value of 500MeV"<<std::endl;
-	  theTruthManager->SetTruthParameter("CaloMuBremsSecondaryMinEnergy", 500.);
-	}
-	if(theTruthManager->GetTruthParameter("CaloDecayPrimaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"CaloDecayPrimaryMinEnergy: setting default value of 1GeV"<<std::endl;
-	  theTruthManager->SetTruthParameter("CaloDecayPrimaryMinEnergy",1000.);
-	}
+  if(theTruthManager->GetTruthParameter("CaloMuBremsPrimaryMinEnergy")==0){
+    log() << MSG::INFO << "CaloMuBremsPrimaryMinEnergy: "
+          << "setting default value of 1GeV" << endreq;
+    theTruthManager->SetTruthParameter("CaloMuBremsPrimaryMinEnergy", 1000.);
+  }
+  if(theTruthManager->GetTruthParameter("CaloMuBremsSecondaryMinEnergy")==0){
+    log() << MSG::INFO << "CaloMuBremsSecondaryMinEnergy: "
+          << "setting default value of 500MeV" << endreq;
+    theTruthManager->SetTruthParameter("CaloMuBremsSecondaryMinEnergy", 500.);
+  }
+  if(theTruthManager->GetTruthParameter("CaloDecayPrimaryMinEnergy")==0){
+    log() << MSG::INFO << "CaloDecayPrimaryMinEnergy: "
+          << "setting default value of 1GeV" << endreq;
+    theTruthManager->SetTruthParameter("CaloDecayPrimaryMinEnergy", 1000.);
+  }
 }
 
 bool CaloCatchAllStrategy::AnalyzeVertex(const G4Step* aStep)
 {
-	static StepHelper sHelper;
-	sHelper.SetStep(aStep);
-	
-	G4int pSubType = sHelper.GetProcessSubType();
-	G4int PDGcode = sHelper.ParticlePDGCode();
-	if((PDGcode==13||PDGcode==-13) && pSubType==fBremsstrahlung)
-	{
-		
-		static double eMuBremsMinPrimary=theTruthManager->GetTruthParameter("CaloMuBremsPrimaryMinEnergy");
-		static double eMuBremsMinSecondary=theTruthManager->GetTruthParameter("CaloMuBremsSecondaryMinEnergy");
-		
-		//primary tracks
-	        double trackKEner=aStep->GetPreStepPoint()->GetKineticEnergy()/CLHEP::MeV;
-		if (trackKEner<eMuBremsMinPrimary){
-	                return true;
-	        }
-		//secondary tracks
-		std::vector<G4Track*> tracks =theTruthManager->GetSecondaries();
-		if (tracks.size() != 1)
-		{
-			int trackToUse=tracks.size()-1;
-			G4Track* temp=tracks[trackToUse];
-			tracks.clear();
-			tracks.push_back(temp);
-		}
-		if (tracks.size()!=1) log()<<MSG::WARNING<<" CaloCatchAllStrategy: something is VERY wrong then!"<<std::endl;
-		trackKEner=tracks[0]->GetKineticEnergy();
-		if (trackKEner<eMuBremsMinSecondary) return true;
+  // Thread-safety issue
+  //static StepHelper sHelper;
+  //sHelper.SetStep(aStep);
+  //G4int pSubType = sHelper.GetProcessSubType();
+  //G4int pdgCode = sHelper.ParticlePDGCode();
 
-		// we register the vertex using TruthManager's appropriate method
-		theTruthManager->SaveSecondaryVertex(aStep->GetTrack(),
-			aStep->GetPostStepPoint(),tracks);
-		return true;
-	}//MuBrems
-	else return false;
-	
-	return true;
+  G4int pSubType =
+    aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessSubType();
+  G4int pdgCode =
+    aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
+
+  if((pdgCode==13||pdgCode==-13) && pSubType==fBremsstrahlung)
+  {
+
+    static double eMuBremsMinPrimary =
+      theTruthManager->GetTruthParameter("CaloMuBremsPrimaryMinEnergy");
+    static double eMuBremsMinSecondary =
+      theTruthManager->GetTruthParameter("CaloMuBremsSecondaryMinEnergy");
+
+    // Parent track threshold.
+    // NOTE: Why do other strategies use the magnitude of GetMomentum()?
+    double trackKEner = aStep->GetPreStepPoint()->GetKineticEnergy()/CLHEP::MeV;
+    if (trackKEner < eMuBremsMinPrimary){
+      return true;
+    }
+
+    // Get the list of secondaries in the current step
+    const auto& tracks = *aStep->GetSecondaryInCurrentStep();
+
+    // In the current implementation, we save only the first secondary on the
+    // list. It's not clear if there are ever more secondaries in this strategy,
+    // but to follow suit with BremsstrahlungStrategy we probably want to move
+    // to saving all secondaries that are above secondary threshold.
+    std::vector<const G4Track*> saveTracks;
+
+    // Assume vector ordering
+    assert( tracks.size() ); // optimized away in opt build
+    saveTracks.push_back( tracks[0] );
+
+    // Apply secondary threshold
+    trackKEner = saveTracks[0]->GetKineticEnergy();
+    if (trackKEner < eMuBremsMinSecondary) return true;
+
+    // Save the vertex using TruthStrategyUtils
+    TruthStrategyUtils::saveSecondaryVertex(aStep->GetTrack(),
+                                            aStep->GetPostStepPoint(),
+                                            saveTracks);
+    return true;
+  }//MuBrems
+
+  return false;
 }
--- Simulation/G4Utilities/G4TruthStrategies/src/ConversionStrategy.cxx
+++ Simulation/G4Utilities/G4TruthStrategies/src/ConversionStrategy.cxx
@@ -3,62 +3,74 @@
 */
 
 #include "G4TruthStrategies/ConversionStrategy.h"
-#include "SimHelpers/StepHelper.h"
+//#include "SimHelpers/StepHelper.h"
 #include "G4Step.hh"
+#include "G4VProcess.hh"
+#include "G4EmProcessSubType.hh"
 #include <iostream>
 
 #include "CLHEP/Units/PhysicalConstants.h"
 
+#include "MCTruthBase/TruthStrategyUtils.h"
+
 static ConversionStrategy Strategy("Conversion");
 
-ConversionStrategy::ConversionStrategy(const std::string n):
-			TruthStrategy(n)
+ConversionStrategy::ConversionStrategy(const std::string& n)
+  : TruthStrategy(n)
 {
-	double prim_thr = 100*CLHEP::MeV;
-        double seco_thr = 100*CLHEP::MeV;
-	if(theTruthManager->GetTruthParameter("ConversionPrimaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"ConversionPrimaryMinEnergy: setting default value of 100 MeV"<<std::endl;  
-	  theTruthManager->SetTruthParameter("ConversionPrimaryMinEnergy",prim_thr);
-	}
-	if(theTruthManager->GetTruthParameter("ConversionSecondaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"ConversionSecondaryMinEnergy: setting default value of 100 MeV"<<std::endl;  
-	  theTruthManager->SetTruthParameter("ConversionSecondaryMinEnergy",seco_thr);
-	}	
+  double prim_thr = 100*CLHEP::MeV;
+  double seco_thr = 100*CLHEP::MeV;
+  if(theTruthManager->GetTruthParameter("ConversionPrimaryMinEnergy")==0){
+    log() << MSG::INFO << "ConversionPrimaryMinEnergy: "
+          << "setting default value of 100 MeV" << endreq;
+    theTruthManager->SetTruthParameter("ConversionPrimaryMinEnergy", prim_thr);
+  }
+  if(theTruthManager->GetTruthParameter("ConversionSecondaryMinEnergy")==0){
+    log() << MSG::INFO << "ConversionSecondaryMinEnergy: "
+          << "setting default value of 100 MeV" << endreq;
+    theTruthManager->SetTruthParameter("ConversionSecondaryMinEnergy", seco_thr);
+  }
 }
 
 bool ConversionStrategy::AnalyzeVertex(const G4Step* aStep)
 {
-	static StepHelper sHelper;
-	static double eMinPrimary=theTruthManager->GetTruthParameter("ConversionPrimaryMinEnergy");
-	static double eMinSecondary=theTruthManager->GetTruthParameter("ConversionSecondaryMinEnergy");
-	
-	sHelper.SetStep(aStep);
-	G4int pSubType = sHelper.GetProcessSubType();
-	if(pSubType==fGammaConversion ||
-           pSubType==fGammaConversionToMuMu ||
-           pSubType==fPairProdByCharged)
-	{
-                double trackKEner= aStep->GetPreStepPoint()->GetMomentum().perp();
-		if (trackKEner<eMinPrimary) return true;
+  static double eMinPrimary =
+    theTruthManager->GetTruthParameter("ConversionPrimaryMinEnergy");
+  static double eMinSecondary =
+    theTruthManager->GetTruthParameter("ConversionSecondaryMinEnergy");
 
-		std::vector<G4Track*> tracks =theTruthManager->GetSecondaries();
-		double secondaryMom = 0.;
-		for (unsigned int i=0;i<tracks.size();i++){
-	                double thisTrackKEner= tracks[i]->GetMomentum().perp();
-			if (thisTrackKEner>secondaryMom) secondaryMom = thisTrackKEner;
-		}
-		if (secondaryMom<eMinSecondary){
-		  return true;
-		}
-	
-		// we register the vertex using TruthManager's appropriate method
-		theTruthManager->SaveSecondaryVertex(0,
-					     aStep->GetPostStepPoint(),
-					     tracks);
-	
-		return true;
-	
-	}
-	else return false;
-	
+  // Thread-safety issue
+  //static StepHelper sHelper;
+  //sHelper.SetStep(aStep);
+  //G4int pSubType = sHelper.GetProcessSubType();
+
+  G4int pSubType =
+    aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessSubType();
+
+  if(pSubType==fGammaConversion ||
+     pSubType==fGammaConversionToMuMu ||
+     pSubType==fPairProdByCharged)
+  {
+    double trackKEner = aStep->GetPreStepPoint()->GetMomentum().perp();
+    if (trackKEner<eMinPrimary) return true;
+
+    // Get the list of new secondaries directly from the step
+    const auto& tracks = *aStep->GetSecondaryInCurrentStep();
+
+    double secondaryMom = 0.;
+    for (unsigned int i=0;i<tracks.size();i++){
+      double thisTrackKEner = tracks[i]->GetMomentum().perp();
+      if (thisTrackKEner>secondaryMom) secondaryMom = thisTrackKEner;
+    }
+    if (secondaryMom<eMinSecondary){
+      return true;
+    }
+
+    // Save this vertex and secondaries
+    TruthStrategyUtils::saveSecondaryVertex(nullptr, aStep->GetPostStepPoint(), tracks);
+
+    return true;
+  }
+
+  else return false;
 }
