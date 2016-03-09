5f6f613d1b47e851997caa30ddd3ca52d070fbc0
Steven Andrew Farrell <steven.farrell@cern.ch>
2016-03-09 04:51:17 +0100

    get secondaries from G4Step; move to TruthStrategyUtils (G4TruthStrategies-00-01-04)
    	* Migrated the truth strategies to get secondaries from the G4Step
    	  method GetSecondaryInCurrentStep and to call saveSecondarVertex in
    	  TruthStrategyUtils rather than the TruthStrategyManager. These
    	  updates allow to run the truth-filling code in AthenaMT.
    	* Tagging: G4TruthStrategies-00-01-04
    
    2016-01-05 Steve Farrell <Steven.Farrell@cern.ch>
    	* Removed thread-unsafe usages of StepHelper in the strategies. The
    	  helper wasn't actually helping anyway; the code is shorter without
    	  it.
    	* Tagging: G4TruthStrategies-00-01-03
    
    2015-12-15 Steve Farrell <Steven.Farrell@cern.ch>
    	* Code cleanup.
    	* Passing const strings by reference to constructors.
    	* Tagging: G4TruthStrategies-00-01-02

--- Simulation/G4Utilities/G4TruthStrategies/src/BremsstrahlungStrategy.cxx
+++ Simulation/G4Utilities/G4TruthStrategies/src/BremsstrahlungStrategy.cxx
@@ -3,65 +3,93 @@
 */
 
 #include "G4TruthStrategies/BremsstrahlungStrategy.h"
-#include "SimHelpers/StepHelper.h"
+
+#include "MCTruthBase/TruthStrategyUtils.h"
+
 #include "G4Step.hh"
 #include "G4Track.hh"
+#include "G4VProcess.hh"
+#include "G4EmProcessSubType.hh"
+
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 #include "CLHEP/Units/PhysicalConstants.h"
 
 static BremsstrahlungStrategy Strategy("Bremsstrahlung");
 
-BremsstrahlungStrategy::BremsstrahlungStrategy(const std::string n):
-			TruthStrategy(n)
+BremsstrahlungStrategy::BremsstrahlungStrategy(const std::string& n)
+  : TruthStrategy(n)
 {
-	double prim_thr = 100*CLHEP::MeV;
-	double seco_thr = 100*CLHEP::MeV;
+  double prim_thr = 100*CLHEP::MeV;
+  double seco_thr = 100*CLHEP::MeV;
 
-	// don't overwrite the values already in the strategy manager
-	if(theTruthManager->GetTruthParameter("BremsPrimaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"BremsPrimaryMinEnergy: setting default value of 100 MeV"<<std::endl;  
-	  theTruthManager->SetTruthParameter("BremsPrimaryMinEnergy",prim_thr);
-	}
-	if(theTruthManager->GetTruthParameter("BremsSecondaryMinEnergy")==0){
-	  log()<<MSG::INFO<<"BremsSecondaryMinEnergy: setting default value of 100 MeV"<<std::endl;  
-	  theTruthManager->SetTruthParameter("BremsSecondaryMinEnergy",seco_thr);
-	}
+  // don't overwrite the values already in the strategy manager
+  if(theTruthManager->GetTruthParameter("BremsPrimaryMinEnergy")==0){
+    log() << MSG::INFO << "BremsPrimaryMinEnergy: setting default value of 100 MeV"
+          << endreq;
+    theTruthManager->SetTruthParameter("BremsPrimaryMinEnergy", prim_thr);
+  }
+  if(theTruthManager->GetTruthParameter("BremsSecondaryMinEnergy")==0){
+    log() << MSG::INFO << "BremsSecondaryMinEnergy: setting default value of 100 MeV"
+          << endreq;
+    theTruthManager->SetTruthParameter("BremsSecondaryMinEnergy", seco_thr);
+  }
 }
 
 bool BremsstrahlungStrategy::AnalyzeVertex(const G4Step* aStep)
 {
+  static double eMinPrimary =
+    theTruthManager->GetTruthParameter("BremsPrimaryMinEnergy");
+  static double eMinSecondary =
+    theTruthManager->GetTruthParameter("BremsSecondaryMinEnergy");
 
-	
-	static StepHelper sHelper;
-	static double eMinPrimary=theTruthManager->GetTruthParameter("BremsPrimaryMinEnergy");
-	static double eMinSecondary=theTruthManager->GetTruthParameter("BremsSecondaryMinEnergy");
-	sHelper.SetStep(aStep);
-        G4int pSubType = sHelper.GetProcessSubType();
+  // Thread-safety issue
+  //static StepHelper sHelper;
+  //sHelper.SetStep(aStep);
+  //G4int pSubType = sHelper.GetProcessSubType();
 
-        if(pSubType==fBremsstrahlung)
-	{
-		double trackKEner= aStep->GetPreStepPoint()->GetMomentum().perp();
-		if (trackKEner<eMinPrimary) return true;
+  G4int pSubType =
+    aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessSubType();
 
- 		std::vector<G4Track*> tracks =theTruthManager->GetSecondaries();
-		if (tracks.size() != 1) 
-		{	
-			int trackToUse=tracks.size()-1;
-			G4Track* temp=tracks[trackToUse];
-			tracks.clear();
-			tracks.push_back(temp);
-		}
-		if (tracks.size()!=1) log()<<MSG::WARNING<<" BremsstrahlungStrategy: something is VERY wrong then!"<<std::endl;
-		trackKEner= tracks[0]->GetMomentum().perp();
-		if (trackKEner<eMinSecondary) return true;
-		
-		// we register the vertex using TruthManager's appropriate method
-		theTruthManager->SaveSecondaryVertex(aStep->GetTrack(),
-					             aStep->GetPostStepPoint(),
-						     tracks);
-		return true;
-	}
-	else return false;
+  if(pSubType==fBremsstrahlung)
+  {
+    // Check parent particle threshold
+    double trackKEner = aStep->GetPreStepPoint()->GetMomentum().perp();
+    if (trackKEner < eMinPrimary) return true;
+
+    // Get the list of secondaries in the current step
+    const auto& tracks = *aStep->GetSecondaryInCurrentStep();
+
+    // In the current implementation (subject to change; see ATLASSIM-2409),
+    // we save only the photon produced by Brems, dropping any additional
+    // TRT transition radiation (XTR) photons. We should, however, move to
+    // saving all secondaries that are above secondary threshold.
+    std::vector<const G4Track*> saveTracks;
+
+    // Assume vector ordering gives the brem first on the list
+    assert( tracks.size() ); // optimized away in opt build
+    saveTracks.push_back( tracks[0] );
+
+    // Grab the brems photon by checking its creator process.
+    //for (auto trk : tracks) {
+    //  auto proc = trk->GetCreatorProcess();
+    //  if (proc->GetProcessSubType() == fBremsstrahlung) {
+    //    saveTracks.push_back(trk);
+    //    break;
+    //  }
+    //}
+
+    // Apply secondary threshold
+    trackKEner = saveTracks[0]->GetMomentum().perp();
+    if (trackKEner < eMinSecondary) return true;
+
+    // Save the vertex using TruthStrategyUtils
+    TruthStrategyUtils::saveSecondaryVertex(aStep->GetTrack(),
+                                            aStep->GetPostStepPoint(),
+                                            saveTracks);
+    return true;
+  }
+  else return false;
 }
