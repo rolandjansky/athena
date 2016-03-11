b3bb3200bd5ceca7cc74019fb9f3e00b11adcfe5
Scott Snyder <scott.snyder@cern.ch>
2016-03-11 03:37:09 +0100

    Add missing HepMC:: qualifiers. (Starlight_i-00-01-01)
            * Tagging Starlight_i-00-01-01.
    	* Add missing HepMC:: qualifiers.
    
    
    Former-commit-id: 5ba615f8083154f6f1d813748adaec1afae82eb7

--- Generators/Starlight_i/Starlight_i/Starlight_i.h
+++ Generators/Starlight_i/Starlight_i/Starlight_i.h
@@ -41,7 +41,7 @@ public:
     virtual StatusCode	genInitialize	();
     virtual StatusCode 	callGenerator	();
     virtual StatusCode 	genFinalize	();
-    virtual StatusCode 	fillEvt		(GenEvent* evt);
+    virtual StatusCode 	fillEvt		(HepMC::GenEvent* evt);
 
 protected:
 
