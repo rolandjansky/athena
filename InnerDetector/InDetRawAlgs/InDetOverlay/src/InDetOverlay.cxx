6525c80de2c507f933e8872d4f61ef999e420848
Andrew Haas <drandyhaas@gmail.com>
2016-03-14 16:31:13 +0100

    'Coverity and mem leak' (InDetOverlay-00-06-03)
    	* src/InDetOverlay.cxx - fix Coverity 106227, and no more
    	retrievePrivateCopy for data RDO containers - fixes serious mem leak
    	* tagging InDetOverlay-00-06-03
    
    2016-03-11  Tanya Kharlamova <Tatyana.Kharlamova@cern.ch>
    	* src/InDetOverlay.cxx - getStrip() method removal. ATLASSIM-2635
    	* tagging InDetOverlay-00-06-02
    
    2015-03-23  John Chapman  <John.Chapman@cern.ch>
    
    	* src/InDetOverlay.cxx - fix for Coverity defects 12379 and
    	12586. Whitespace clean-up.
    	* tagging InDetOverlay-00-06-01

--- InnerDetector/InDetRawAlgs/InDetOverlay/src/InDetOverlay.cxx
+++ InnerDetector/InDetRawAlgs/InDetOverlay/src/InDetOverlay.cxx
@@ -4,7 +4,7 @@
 
 // Andrei Gaponenko <agaponenko@lbl.gov>, 2006, 2007
 
-// FIXME: add merge() methods to  InDetRawData  classes and get rid of the hack.  
+// FIXME: add merge() methods to  InDetRawData  classes and get rid of the hack.
 #define private public
 #define protected public
 #include "InDetRawData/InDetRawData.h"
@@ -41,24 +41,24 @@ namespace Overlay {
 
   // Specialize mergeChannelData() for the TRT
   template<> void mergeChannelData(TRT_RDORawData& r1, const TRT_RDORawData& r2, IDC_OverlayBase* parent) {
-  
+
     // ----------------------------------------------------------------
     // demo
     static bool first_time = true;
     if(first_time) {
       first_time = false;
       parent->msg(MSG::INFO)<<"Overlay::mergeChannelData(): "
-			    <<"TRT specific code is called for "
-			    <<typeid(TRT_RDORawData).name()
-			    <<endreq;
+                            <<"TRT specific code is called for "
+                            <<typeid(TRT_RDORawData).name()
+                            <<endreq;
     }
-    
+
     // ----------------------------------------------------------------
     // FIXME: That should really be a call to r1.merge(r2);
-    
+
     TRT_LoLumRawData *pr1 = dynamic_cast<TRT_LoLumRawData*>(&r1);
     const TRT_LoLumRawData *pr2 = dynamic_cast<const TRT_LoLumRawData*>(&r2);
-    
+
     if(pr1 && pr2) {
       // the actual merging
       pr1->m_word |= pr2->m_word;
@@ -66,82 +66,83 @@ namespace Overlay {
     else {
       static bool first_time = true;
       if(first_time) {
-	first_time = false;
-	parent->msg(MSG::INFO)<<"Overlay::mergeChannelData(): "
-			      <<"TRT specific code for "
-			      <<typeid(TRT_RDORawData).name()
-			      <<" is not implemented"
-			      <<endreq;
+        first_time = false;
+        parent->msg(MSG::INFO)<<"Overlay::mergeChannelData(): "
+                              <<"TRT specific code for "
+                              <<typeid(TRT_RDORawData).name()
+                              <<" is not implemented"
+                              <<endreq;
       }
     } // else - dyncasts
   } // mergeChannelData()
 
 
   //================================================================
-  namespace {   // helper functions for SCT merging 
+  namespace {   // helper functions for SCT merging
     typedef SCT3_RawData  SCT_RDO_TYPE;
 
     typedef std::multimap<int, SCT_RDO_TYPE*> StripMap;
-    
-    void fillStripMap(StripMap *sm, const InDetRawDataCollection<SCT_RDORawData> &rdo_coll, const std::string& collectionName) {
+
+    void fillStripMap(StripMap *sm, const InDetRawDataCollection<SCT_RDORawData> &rdo_coll, const std::string& collectionName, InDetOverlay *parent) {
       for(InDetRawDataCollection<SCT_RDORawData>::const_iterator i=rdo_coll.begin(); i!=rdo_coll.end(); ++i) {
-	SCT_RDO_TYPE *rdo = dynamic_cast<SCT_RDO_TYPE*>(&**i);
-	if(!rdo) {
-	  std::ostringstream os;
-	  os<<"mergeCollection<SCT_RDORawData>(): wrong datum format for the '"<<collectionName<<"' collection. Only SCT3_RawData are produced by SCT_RodDecoder and supported by overlay.   For the supplied datum  typeid(datum).name() = "<<typeid(**i).name();
-	  throw std::runtime_error(os.str());
-	}
-	
-	int stripBegin = rdo->getStrip();
-	int stripEnd = stripBegin + rdo->getGroupSize();
-	for(int strip = stripBegin; strip < stripEnd; ++strip) {
-	  sm->insert(std::make_pair(strip, rdo));
-	}
+        SCT_RDO_TYPE *rdo = dynamic_cast<SCT_RDO_TYPE*>(&**i);
+        if(!rdo) {
+          std::ostringstream os;
+          os<<"mergeCollection<SCT_RDORawData>(): wrong datum format for the '"<<collectionName<<"' collection. Only SCT3_RawData are produced by SCT_RodDecoder and supported by overlay.   For the supplied datum  typeid(datum).name() = "<<typeid(**i).name();
+          throw std::runtime_error(os.str());
+        }
+
+        int stripBegin = parent->get_sct_id()->strip(rdo->identify());
+        int stripEnd = stripBegin + rdo->getGroupSize();
+        for(int strip = stripBegin; strip < stripEnd; ++strip) {
+          sm->insert(std::make_pair(strip, rdo));
+        }
       }
     }
   } // namespace - helper functions for SCT merging
-  
+
 
   //================
 
   template<> void mergeCollectionsNew(InDetRawDataCollection<SCT_RDORawData> *data_coll,
-				   InDetRawDataCollection<SCT_RDORawData> *mc_coll,
-				   IDC_OverlayBase *tmp)
+                                      InDetRawDataCollection<SCT_RDORawData> *mc_coll,
+                                      IDC_OverlayBase *tmp)
   {
     // We want to use the SCT_ID helper provided by InDetOverlay, thus the constraint
     InDetOverlay *parent = dynamic_cast<InDetOverlay*>(tmp);
     if(!parent) {
       std::ostringstream os;
       os<<"mergeCollectionsNew<SCT_RDORawData>() called by a wrong parent algorithm?  Must be InDetOverlay.";
+      throw std::runtime_error(os.str());
     }
-    
+
     // ----------------------------------------------------------------
     // debug
     static bool first_time = true;
     if(first_time) {
       first_time = false;
       parent->msg(MSG::INFO)<<"InDetOverlay::mergeCollectionsNew(): "
-			    <<" SCT specific code is called for "
-			    <<typeid(*data_coll).name() 
-			    <<endreq;
+                            <<" SCT specific code is called for "
+                            <<typeid(*data_coll).name()
+                            <<endreq;
     }
 
     // ----------------------------------------------------------------
     // Useful reading:
-    // 
+    //
     // Real data are handled like this:
-    // 
+    //
     // https://svnweb.cern.ch/trac/atlasoff/browser/InnerDetector/InDetEventCnv/SCT_RawDataByteStreamCnv/trunk/src/SCT_RodDecoder.cxx?rev=332542
-    // 
+    //
     // Note that SCT3 is hardcoded (no chance to get SCT1 from real data).
     // The SCT3 format is also the default in digitization:
     //
     // https://svnweb.cern.ch/trac/atlasoff/browser/InnerDetector/InDetDigitization/SCT_Digitization/trunk/src/SCT_Digitization.cxx?rev=355953
 
     // So we'll just go with SCT3.
-    // 
+    //
     // The client code that consumes SCT RDOs:
-    // 
+    //
     // http://alxr.usatlas.bnl.gov/lxr/source/atlas/InnerDetector/InDetRecTools/SiClusterizationTool/src/SCT_ClusteringTool.cxx
 
 
@@ -170,8 +171,8 @@ namespace Overlay {
     // Expand encoded RDOs into individual hit strips.
     // Each strip number is linked to the original RDO.
     StripMap sm;
-    fillStripMap(&sm, data, "data");
-    fillStripMap(&sm, mc, "MC");
+    fillStripMap(&sm, data, "data", parent);
+    fillStripMap(&sm, mc, "MC", parent);
 
     // collect all the hits
     StripMap::const_iterator p=sm.begin();
@@ -179,28 +180,28 @@ namespace Overlay {
 
       // We have a strip.  compute the group, collect all contributing RDOs.
       const int firstStrip = p->first;
-      
+
       // Get all strips for the current RDO
       std::set<SCT_RDO_TYPE*> origRDOs;
       origRDOs.insert(p->second);
       int currentStrip = firstStrip;
       while(++p != sm.end()) {
 
-	// The following condition forces the "condensed" mode, with
-	// one RDO representing a group of adjacent hit strips.  It
-	// looks like SCT_RodDecoder switches between "expanded" and
-	// "condensed" modes based on the input byte stream; but here
-	// we don't have information to do this.  So follow the
-	// digitization code and hardcode the "condensed" mode. 
-	// NB: change currentStrip+2 to currentStrip+1 if want to
-	// switch to the expanded mode.
-	if(p->first < currentStrip + 2) {
-	  origRDOs.insert(p->second);
-	  currentStrip = p->first;
-	}
-	else {
-	  break;
-	}
+        // The following condition forces the "condensed" mode, with
+        // one RDO representing a group of adjacent hit strips.  It
+        // looks like SCT_RodDecoder switches between "expanded" and
+        // "condensed" modes based on the input byte stream; but here
+        // we don't have information to do this.  So follow the
+        // digitization code and hardcode the "condensed" mode.
+        // NB: change currentStrip+2 to currentStrip+1 if want to
+        // switch to the expanded mode.
+        if(p->first < currentStrip + 2) {
+          origRDOs.insert(p->second);
+          currentStrip = p->first;
+        }
+        else {
+          break;
+        }
       }
 
       // We've got info on all strips for the current RDO here.
@@ -211,18 +212,18 @@ namespace Overlay {
       int ERRORS = 0;
       std::vector<int> errvec;
 
-      for(std::set<SCT_RDO_TYPE*>::const_iterator r = origRDOs.begin(); r!=origRDOs.end(); ++r) {
-	tbin |= (*r)->getTimeBin();
+      for(std::set<SCT_RDO_TYPE*>::const_iterator origRdoIter = origRDOs.begin(); origRdoIter!=origRDOs.end(); ++origRdoIter) {
+        tbin |= (*origRdoIter)->getTimeBin();
 
-	if((*r)->FirstHitError()) {
-	  ERRORS |= 0x10;
-	}
-	if((*r)->SecondHitError()) {
-	  ERRORS |= 0x20;
-	}
+        if((*origRdoIter)->FirstHitError()) {
+          ERRORS |= 0x10;
+        }
+        if((*origRdoIter)->SecondHitError()) {
+          ERRORS |= 0x20;
+        }
 
-	std::vector<int> tmp((*r)->getErrorCondensedHit());
-	errvec.insert(errvec.end(), tmp.begin(), tmp.end());
+        std::vector<int> origRdoErrorVec((*origRdoIter)->getErrorCondensedHit());
+        errvec.insert(errvec.end(), origRdoErrorVec.begin(), origRdoErrorVec.end());
       }
 
       unsigned int SCT_Word = (groupSize | (firstStrip << 11) | (tbin <<22) | (ERRORS << 25)) ;
@@ -243,7 +244,7 @@ InDetOverlay::InDetOverlay(const std::string &name, ISvcLocator *pSvcLocator) :
   m_detStore("StoreGateSvc/DetectorStore", name),
   m_sct_id(0)
 {
-  
+
   declareProperty("do_TRT", m_do_TRT=true);
   declareProperty("mainInputTRT_Name", m_mainInputTRT_Name="TRT_RDOs");
   declareProperty("overlayInputTRT_Name", m_overlayInputTRT_Name="TRT_RDOs");
@@ -261,7 +262,7 @@ InDetOverlay::InDetOverlay(const std::string &name, ISvcLocator *pSvcLocator) :
 StatusCode InDetOverlay::overlayInitialize()
 {
   ATH_MSG_INFO("InDetOverlay initialize()");
-  
+
   if(!m_detStore->retrieve(m_sct_id,"SCT_ID").isSuccess() || !m_sct_id ) {
     msg(MSG::FATAL) << "Cannot retrieve SCT ID helper"  << endreq;
     return StatusCode::FAILURE;
@@ -270,7 +271,7 @@ StatusCode InDetOverlay::overlayInitialize()
 }
 
 //================================================================
-StatusCode InDetOverlay::overlayFinalize() 
+StatusCode InDetOverlay::overlayFinalize()
 {
   ATH_MSG_INFO("InDetOverlay finalized");
   return StatusCode::SUCCESS;
@@ -283,7 +284,7 @@ StatusCode InDetOverlay::overlayExecute() {
   //----------------------------------------------------------------
   if(m_do_TRT) {
     ATH_MSG_VERBOSE("Retrieving data input TRT container");
-    std::auto_ptr<TRT_RDO_Container> cdata(m_storeGateData->retrievePrivateCopy<TRT_RDO_Container>(m_mainInputTRT_Name));
+    std::auto_ptr<TRT_RDO_Container> cdata(m_storeGateData->retrieve<TRT_RDO_Container>(m_mainInputTRT_Name));
     if(!cdata.get()) {
       ATH_MSG_WARNING("Could not get data TRT container \""<<m_mainInputTRT_Name<<"\"");
     }
@@ -296,28 +297,28 @@ StatusCode InDetOverlay::overlayExecute() {
       ATH_MSG_WARNING("Could not get MC TRT container \""<<m_overlayInputTRT_Name<<"\"");
     }
     ATH_MSG_DEBUG("TRT MC     = "<<shortPrint(cmc));
-    
+
     if(cdata.get() && cmc.get()) {
       overlayContainerNew(cdata, cmc);
       ATH_MSG_DEBUG("TRT Result = "<<shortPrint(cdata));
 
       if( !m_storeGateOutput->record(cdata, m_mainInputTRT_Name).isSuccess() ) {
-	ATH_MSG_WARNING("Failed to record TRT overlay container to output store ");
+        ATH_MSG_WARNING("Failed to record TRT overlay container to output store ");
       }
     }
-    
+
     //----------------
     // This kludge is a work around for problems created by another kludge:
     // Digitization algs keep a pointer to their output Identifiable Container and reuse
     // the same object over and other again.   So unlike any "normal" per-event object
     // this IDC is not a disposable one, and we should not delete it.
-    cmc.release();
+    //cmc.release();
   }
 
   //----------------------------------------------------------------
   if(m_do_SCT) {
     ATH_MSG_VERBOSE("Retrieving data input SCT container");
-    std::auto_ptr<SCT_RDO_Container> cdata(m_storeGateData->retrievePrivateCopy<SCT_RDO_Container>(m_mainInputSCT_Name));
+    std::auto_ptr<SCT_RDO_Container> cdata(m_storeGateData->retrieve<SCT_RDO_Container>(m_mainInputSCT_Name));
     if(!cdata.get()) {
       ATH_MSG_WARNING("Could not get data SCT container \""<<m_mainInputSCT_Name<<"\"");
     }
@@ -329,13 +330,13 @@ StatusCode InDetOverlay::overlayExecute() {
       ATH_MSG_WARNING("Could not get MC SCT container \""<<m_overlayInputSCT_Name<<"\"");
     }
     ATH_MSG_DEBUG("SCT MC     = "<<shortPrint(cmc, 50));
-    
+
     if(cdata.get() && cmc.get()) {
       overlayContainerNew(cdata, cmc);
       ATH_MSG_DEBUG("SCT Result = "<<shortPrint(cdata, 50));
-      
+
       if( !m_storeGateOutput->record(cdata, m_mainInputSCT_Name).isSuccess() ) {
-	ATH_MSG_WARNING("Failed to record SCT overlay container to output store ");
+        ATH_MSG_WARNING("Failed to record SCT overlay container to output store ");
       }
     }
 
@@ -344,13 +345,13 @@ StatusCode InDetOverlay::overlayExecute() {
     // Digitization algs keep a pointer to their output Identifiable Container and reuse
     // the same object over and other again.   So unlike any "normal" per-event object
     // this IDC is not a disposable one, and we should not delete it.
-    cmc.release();
+    //cmc.release();
   }
 
   //----------------------------------------------------------------
   if(m_do_Pixel) {
     ATH_MSG_VERBOSE("Retrieving data input Pixel container");
-    std::auto_ptr<PixelRDO_Container> cdata(m_storeGateData->retrievePrivateCopy<PixelRDO_Container>(m_mainInputPixel_Name));
+    std::auto_ptr<PixelRDO_Container> cdata(m_storeGateData->retrieve<PixelRDO_Container>(m_mainInputPixel_Name));
     if(!cdata.get()) {
       ATH_MSG_WARNING("Could not get data Pixel container \""<<m_mainInputPixel_Name<<"\"");
     }
@@ -366,9 +367,9 @@ StatusCode InDetOverlay::overlayExecute() {
     if(cdata.get() && cmc.get()) {
       overlayContainerNew(cdata, cmc);
       ATH_MSG_DEBUG("Pixel Result = "<<shortPrint(cdata));
-      
+
       if( !m_storeGateOutput->record(cdata, m_mainInputPixel_Name).isSuccess() ) {
-	ATH_MSG_WARNING("Failed to record Pixel overlay container to output store ");
+        ATH_MSG_WARNING("Failed to record Pixel overlay container to output store ");
       }
     }
 
@@ -377,14 +378,14 @@ StatusCode InDetOverlay::overlayExecute() {
     // Digitization algs keep a pointer to their output Identifiable Container and reuse
     // the same object over and other again.   So unlike any "normal" per-event object
     // this IDC is not a disposable one, and we should not delete it.
-    cmc.release();
+    //cmc.release();
   }
 
   //----------------------------------------------------------------
   ATH_MSG_DEBUG("Processing MC truth data");
 
-  // Main stream is normally real data without any MC info.  
-  // In tests we may use a MC generated file instead of real data. 
+  // Main stream is normally real data without any MC info.
+  // In tests we may use a MC generated file instead of real data.
   // Remove truth info from the main input stream, if any.
   //
   // Here we handle just InDet-specific truth classes.
