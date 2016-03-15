0511cb1544d488c8cbc4c6cbfdb0fff8c3191d2b
Jacob Martin Pasner <jake.pasner@cern.ch>
2016-03-15 18:44:24 +0100

    Fixed typo in cmt/requirements and replaced RTT LOI test with sFCal test (UpgradeTests-00-00-02)

---
+++ Tools/UpgradeTests/share/preInclude.NoTRT.py
@@ -0,0 +1,8 @@
+from AthenaCommon.DetFlags import DetFlags
+DetFlags.ID_setOn()
+DetFlags.TRT_setOff()
+DetFlags.detdescr.TRT_setOff()
+DetFlags.makeRIO.TRT_setOff()
+DetFlags.Calo_setOn()
+DetFlags.Muon_setOn()
+DetFlags.Truth_setOn()
