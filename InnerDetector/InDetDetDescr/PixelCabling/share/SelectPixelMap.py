6a14ec47fa3b618659260d87cf5d1674b001cb6c
Magda Anna Chelstowska <magda.anna.chelstowska@cern.ch>
2016-03-23 15:57:35 +0100

    pixel cabling for run1 (PixelCabling-00-01-03-01)
    	* Fixed Pixel Cabling for Run1 digi jobs (obo John Chapman)
    	* Tagged as PixelCabling-00-01-03-01

--- InnerDetector/InDetDetDescr/PixelCabling/share/SelectPixelMap.py
+++ InnerDetector/InDetDetDescr/PixelCabling/share/SelectPixelMap.py
@@ -17,103 +17,17 @@ logger = logging.getLogger("PixelCablingSvc")
 logger.debug("In SelectPixelMap.py")
 #geoFlags.dump()
 
-
-
 if not ('conddb' in dir()):
     IOVDbSvc = Service("IOVDbSvc")
     from IOVDbSvc.CondDB import conddb
 
-# Add the HitDiscCnfg folder if it has not been requested yet
-if (conddb.dbdata == "CONDBR2" or (conddb.dbmc == "OFLP200" and geoFlags.isIBL() == True)) and not conddb.folderRequested("/PIXEL/HitDiscCnfg"):
-    conddb.addFolderSplitMC("PIXEL","/PIXEL/HitDiscCnfg","/PIXEL/HitDiscCnfg")
-    logger.debug("Requested HitDiscCnfg folder")
+from AthenaCommon.CfgGetter import getService
+PixelCablingSvc = getService("PixelCablingSvc")
+ServiceMgr += PixelCablingSvc
+ 
+#See python/PixelCablingConfig.py for the main configuration.
 
-
-
-# Online mode
-if (athenaCommonFlags.isOnline == True):
-    logger.debug("Running in online mode")
-
-    # Request the CablingMap folder
-    # (COOL is the default mapping type)
-    if (conddb.dbdata == "CONDBR2"):
-        if not conddb.folderRequested("/PIXEL/CablingMap"):
-            conddb.addFolderSplitOnline("PIXEL", "/PIXEL/Onl/CablingMap","/PIXEL/CablingMap")
-            logger.debug("Requested CablingMap folder")
-
-
-
-# Offline mode
-else:
-
-    logger.debug("Running in offline mode")
-
-    # SIMULATION
-    if (globalflags.DataSource() == 'geant4'):
-        logger.debug("Running on simulation")
-
-        # Set cabling svc to get map from file
-        ServiceMgr.PixelCablingSvc.MappingType = "Final"
-
-        # No IBL
-        if (geoFlags.isIBL() == False):
-            ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping.dat"
-
-        else:
-            # Planar IBL
-            if (geoFlags.IBLLayout() == "planar"):
-
-                # DBM or not
-                if (geoFlags.isDBM() == True):
-                    ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_inclIBL_DBM.dat"
-                else:
-                    ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_inclIBL.dat"
-
-            # Hybrid IBL plus DBM
-            elif (geoFlags.IBLLayout() == "3D"):
-                #ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_inclIBL3D_DBM.dat"
-                ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_Run2.dat"
-
-            elif (geoFlags.IBLLayout() == "UNDEFINED"):
-                logger.warning("Got GeometryFlags.isIBL = True, but IBLLayout is UNDEFINED")
-                logger.warning("Pixel cabling map cannot be set at this point")
-
-
-    # DATA
-    elif (globalflags.DataSource == 'data'):
-
-        runNum = GetRunNumber()
-        logger.debug("Running on data, run number %d" % runNum)
-
-        # For data older than run number 222222, use the appropriate text file
-        if (runNum < 222222):
-            ServiceMgr.PixelCablingSvc.MappingType = "Final"
-            ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_May08.dat"
-
-        # For Run-2 onwards, get cabling map from database
-        else:
-            ServiceMgr.PixelCablingSvc.MappingType = "COOL"
-            ServiceMgr.PixelCablingSvc.KeyCabling = "/PIXEL/CablingMap"
-
-            # Request the CablingMap folder
-            if not conddb.folderRequested("/PIXEL/CablingMap"):
-                conddb.addFolderSplitOnline("PIXEL", "/PIXEL/Onl/CablingMap","/PIXEL/CablingMap")
-                logger.debug("Requested CablingMap folder")
-
-            # Even though we are reading from COOL, set the correct fallback map.
-            if (runNum >= 289350): # 2016
-                ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_2016.dat"
-            elif (runNum >= 222222 and runNum < 289350): # 2015
-                ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_Run2.dat"
-            else:
-                ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_May08.dat"
-
-
-    # Unknown input
-    else:
-        logger.warning("Unknown input source. Pixel cabling map cannot be set at this point")
-
-
+if (athenaCommonFlags.isOnline == False):
     # For debugging, print a summary of the chosen settings
     logger.debug("Summary from SelectPixelMap.py:")
     logger.debug("Mapping type: %s" % ServiceMgr.PixelCablingSvc.MappingType)
