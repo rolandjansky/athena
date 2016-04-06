049eccd291d93640961ff0c487de8214d1967d77
James Catmore <james.catmore@cern.ch>
2016-04-06 10:32:23 +0200

    Adding EXOT17 (DerivationFrameworkCore-00-03-19)
    	* Adding EXOt17
    	* Tagged as DerivationFrameworkCore-00-03-19
    
    2016-22-03 James Catmore <james.catmore@cern.ch>
    	* Adding BPHY7 and BPHY8
    	* Tagged as DerivationFrameworkCore-00-03-18
    
    2016-14-03 Jacob Searcy <jsearcy@umich.edu>
    	* Fixing typos in Antikt10 Smart Slimming
    	* Tagged as DerivationFrameworkCore-00-03-17
    
    2016-03-03 Jacob Searcy <jsearcy@umich.edu>
    	* Adding covariance matrix for forward track particles
    	* Tagged as DerivationFrameworkCore-00-03-16
    
    2016-03-03 Jacob Searcy <jsearcy@umich.edu>
    	* Adding Z0 and D0 for forward track particles
    	* Tagged as DerivationFrameworkCore-00-03-15
    
    2016-03-02 Jacob Searcy <jsearcy@umich.edu>
    ...
    (Long ChangeLog diff - truncated)

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/JetTriggerFixContent.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+JetTriggerFixContent = [
+'xAOD::JetEtRoI#*',
+'xAOD::JetEtRoIAuxInfo#*'
+]
---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/MinBiasTrigger_nonxAOD_Content.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+MinBiasTrigger_nonxAOD_Content = [
+'TrigT2MbtsBitsContainer#HLT_TrigT2MbtsBitsContainer_T2Mbts',
+'TrigSpacePointCountsCollection#HLT_TrigSpacePointCountsCollection_spacepoints',
+'TrigTrackCountsCollection#HLT_TrigTrackCountsCollection_trackcounts',
+'TrigVertexCountsCollection#HLT_TrigVertexCountsCollection_vertexcounts'
+]
