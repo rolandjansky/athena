b956b755e18401a013ca07684a7fc831cfca2e4f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:30:59 +0200

    'CMakeLists.txt' (MuonCombinedToolInterfaces-01-02-07)
        * add MuonCombinedToolInterfaces/IMuonCandidateTrackBuilderTool.h
        * add MuonCombinedToolInterfaces/IMuonLayerAmbiguitySolverTool.h
        * add MuonCombinedToolInterfaces/IMuonLayerSegmentMatchingTool.h
    	* MuonCombinedToolInterfaces-01-02-07

---
+++ Reconstruction/MuonIdentification/MuonCombinedToolInterfaces/MuonCombinedToolInterfaces/IMuonLayerAmbiguitySolverTool.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUON_IMUONLAYERAMBIGUITYSOLVERTOOL_H
+#define MUON_IMUONLAYERAMBIGUITYSOLVERTOOL_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include <vector>
+
+#include "MuonLayerEvent/MuonLayerRecoData.h"
+#include "MuonLayerEvent/MuonCandidate.h"
+
+static const InterfaceID IID_IMuonLayerAmbiguitySolverTool("Muon::IMuonLayerAmbiguitySolverTool",1,0);
+
+
+namespace Muon {
+
+  class MuonSegment;
+
+  /** Interface for tools calculating hit count summaries for track */
+  class IMuonLayerAmbiguitySolverTool : virtual public IAlgTool {     
+  public:
+    /** IAlgTool interface */
+    static const InterfaceID& interfaceID();
+
+    /** @brief resolve ambiguities and return a set of unique combinations */
+    virtual void resolveOverlaps( const std::vector<Muon::MuonLayerRecoData>& allLayers, std::vector< MuonCandidate >& resolvedCandidates ) const = 0;
+
+  };
+ 
+  inline const InterfaceID& IMuonLayerAmbiguitySolverTool::interfaceID()
+  {
+    return IID_IMuonLayerAmbiguitySolverTool;
+  }
+
+
+} // end of name space
+
+#endif // IMuonLayerAmbiguitySolverTool_H
---
+++ Reconstruction/MuonIdentification/MuonCombinedToolInterfaces/MuonCombinedToolInterfaces/IMuonLayerSegmentMatchingTool.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUON_IMUONLAYERSEGMENTMATCHINGTOOL_H
+#define MUON_IMUONLAYERSEGMENTMATCHINGTOOL_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include <vector>
+
+#include "MuonLayerEvent/MuonSystemExtension.h"
+
+static const InterfaceID IID_IMuonLayerSegmentMatchingTool("Muon::IMuonLayerSegmentMatchingTool",1,0);
+
+
+namespace Muon {
+
+  class MuonSegment;
+
+  /** Interface for tools calculating hit count summaries for track */
+  class IMuonLayerSegmentMatchingTool : virtual public IAlgTool {     
+  public:
+    /** IAlgTool interface */
+    static const InterfaceID& interfaceID();
+
+    /** @brief Match segments for to a given MuonSystemExtension::Intersection, return selected segments */
+    virtual void select( const MuonSystemExtension::Intersection& intersection, const std::vector< std::shared_ptr<const Muon::MuonSegment> >& segments, 
+                         std::vector< std::shared_ptr<const Muon::MuonSegment> >& selectedSegments ) const = 0;
+
+  };
+ 
+  inline const InterfaceID& IMuonLayerSegmentMatchingTool::interfaceID()
+  {
+    return IID_IMuonLayerSegmentMatchingTool;
+  }
+
+
+} // end of name space
+
+#endif // IMuonLayerSegmentMatchingTool_H
