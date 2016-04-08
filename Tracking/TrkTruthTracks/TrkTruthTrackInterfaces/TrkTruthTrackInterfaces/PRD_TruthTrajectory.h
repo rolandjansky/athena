750b5862c2ef36c5008376e991c35d508c1cee64
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:09:15 +0200

    'CMakeLists.txt' (TrkTruthTrackInterfaces-00-00-04)

---
+++ Tracking/TrkTruthTracks/TrkTruthTrackInterfaces/TrkTruthTrackInterfaces/PRD_TruthTrajectory.h
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// PRD_TruthTrajectory.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef TRK_TRKTRUTHTRACKS_PRD_TRUTHTRAJECTORY_H
+#define TRK_TRKTRUTHTRACKS_PRD_TRUTHTRAJECTORY_H 1
+
+
+namespace HepMC {
+  class GenParticle;
+}
+
+namespace Trk {
+    
+  class PrepRawData;
+  
+  /**
+    @class PRD_TruthTrajectory
+    
+    simple definitiion of a PRD_TruhtTrajectory
+    
+    @author Andreas.Salzburger -at- cern.ch, Thijs.Cornelissen -at- cern.ch, Roland.Wolfgang.Jansky -at- cern.ch 
+    
+  */
+  struct PRD_TruthTrajectory {
+      
+     /**  public members */ 
+     std::vector<const Trk::PrepRawData* > prds;
+     const HepMC::GenParticle*             genParticle;
+     size_t                                nDoF;
+
+     /** defualt constructor */
+      PRD_TruthTrajectory()  :
+       genParticle(0),
+       nDoF(0)
+     {}
+     
+     /** fast constructor */
+     PRD_TruthTrajectory( const std::vector<const Trk::PrepRawData* >& prdVec,
+                          const HepMC::GenParticle* gP    = 0,
+                          size_t numberOfDegreesOfFreedom = 0) :
+      prds(prdVec),
+      genParticle(gP),
+      nDoF(numberOfDegreesOfFreedom)
+    {}
+    
+  };
+
+}
+
+#endif
