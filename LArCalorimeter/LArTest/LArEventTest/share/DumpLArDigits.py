0018916646a82e4c942024de9a5c8cea180de26b
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:33:34 +0200

    'CMakeLists.txt' (LArEventTest-00-00-36)
    	* Tagging LArEventTest-00-00-36.
    	* Comply with ATLAS naming conventions.
    
    2015-08-20 Walter Lampl <walter.lampl@ cern.ch>
    	* Get DumpLArDigits going again
    	* tag LArEventTest-00-00-35
    
    
    Former-commit-id: d1d90edfeb25b2862dd7651b0e9b787cfda1acec

--- LArCalorimeter/LArTest/LArEventTest/share/DumpLArDigits.py
+++ LArCalorimeter/LArTest/LArEventTest/share/DumpLArDigits.py
@@ -1,48 +1,26 @@
-# Location & number of raw data file:
-runnumber=3004171
-#RawDataDir="/castor/cern.ch/atlas/testbeam/combined/2004/"
-RawDataDir="/castor/cern.ch/atlas/testbeam/lar/2004/"
-#RawDataDir="/afs/cern.ch/user/l/lafaye/scratch0/data"
-RawDataFilePrefix="daq_ROS-41_LargCalib"
-#Gain, set HIGH,MEDIUM,LOW or FREE
-GainKey="FREE" 
-# Number of events to be processed (default is 10)
-theApp.EvtMax = 2
+#tested setup: asetup 20.1.5.8,here
 
 
-# ***************************************************************
-include( "ByteStreamCnvSvc/TBEventSelector_jobOptions.py" )
-ByteStreamAddressProviderSvc = Service( "ByteStreamAddressProviderSvc" )
-ByteStreamAddressProviderSvc.TypeNames += ["LArRawChannelContainer/LArRawChannels"]
-ByteStreamAddressProviderSvc.TypeNames += ["LArDigitContainer/FREE"] 
-include( "LArDetMgrDetDescrCnv/LArDetMgrDetDescrCnv_H8_joboptions.py" )
+from AthenaCommon.AppMgr import (theApp, ServiceMgr as svcMgr,ToolSvc)
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence = AlgSequence()
 
-if GainKey=="FREE":
-  ToolSvc.LArRodDecoder.FirstSample=2
+include("LArConditionsCommon/LArMinimalSetup.py")
 
+svcMgr.IOVDbSvc.GlobalTag="CONDBR2-ES1PA-2015-05"
 
-# Directory & Prefix & Runnumber 
-ByteStreamInputSvc=Service("ByteStreamInputSvc")
-ByteStreamInputSvc.InputDirectory += [RawDataDir]
-ByteStreamInputSvc.FilePrefix += [RawDataFilePrefix]
-ByteStreamInputSvc.RunNumber = [runnumber]
+#Specify the input file(s)
+svcMgr.ByteStreamInputSvc.FullFileName=["/scratch/wlampl/data15_13TeV/data15_13TeV.00267073.express_express.merge.RAW._lb0706._SFO-ALL._0001.1",]
 
+# Specify the object you want to read from ByteStream
+theByteStreamAddressProviderSvc = svcMgr.ByteStreamAddressProviderSvc
+theByteStreamAddressProviderSvc.TypeNames += ["LArDigitContainer/FREE"]
+theByteStreamAddressProviderSvc.TypeNames += ["LArFebHeaderContainer/LArFebHeader"]
 
-ToolSvc = Service( "ToolSvc" )
-ToolSvc.LArRoI_Map.Print=FALSE
-# Set output level threshold (2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL )
-MessageSvc = Service( "MessageSvc" )
-MessageSvc.OutputLevel =3
-AthenaEventLoopMgr = Service ("AthenaEventLoopMgr")
-AthenaEventLoopMgr.OutputLevel=4
+from LArEventTest.LArEventTestConf import DumpLArDigits
+topSequence+=DumpLArDigits(LArDigitContainerName="FREE")
 
-#Necessary DLL's 
-theApp.Dlls += [ "LArRawUtils","LArTools"]
-theApp.Dlls += [ "LArByteStream"]
-#theApp.Dlls += [ "CaloDetMgrDetDescrCnv" ]
+theApp.EvtMax=5
 
-theApp.Dlls += [ "LArEventTest"]
-theApp.topAlg+=["DumpLArDigits"]
-DumpLArDigits=Algorithm("DumpLArDigits")
-DumpLArDigits.LArDigitContainerName = "FREE" 
-DumpLArDigits.OutputFileName="LArDigits.txt"
+#svcMgr.MessageSvc.OutputLevel=DEBUG
+svcMgr.StoreGateSvc.Dump=True
