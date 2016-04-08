eecf207c4494c1121526ff695442d55f4c5698cb
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:31:53 +0200

    'CMakeLists.txt' (METRecoInterface-00-01-02)
    	* Correct required header file for MissingETContainer
    	* Tagging METRecoInterface-00-01-02
    
    2014-10-10 TJ Khoo <khoo@cern.ch>
    	* Add METContainer arg to IMETAssocToolBase
    	* Tagging METRecoInterface-00-01-01
    
    2014-10-10 TJ Khoo <khoo@cern.ch>
    	* Tagging METRecoInterface-00-01-00
    
    2014-10-09 AS Mete <amete@cern.ch>
    	* DUMMYIMETToolBase.h => IMETAssocToolBase.h
    
    2014-10-07 AS Mete <amete@cern.ch>
    	* Prototype for migrations to new EDM

---
+++ Reconstruction/MET/METRecoInterface/METRecoInterface/IMETAssocToolBase.h
@@ -0,0 +1,34 @@
+///////////////////////// -*- C++ -*- /////////////////////////////
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// IMETAssocToolBase.h 
+// Header file for interface IMETToolBase
+//
+// This is the base class for all tools that calculate MET Terms
+//
+//  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//
+// Author: P Loch, S Resconi, TJ Khoo, AS Mete
+/////////////////////////////////////////////////////////////////// 
+	
+#ifndef IMETAssocToolBase_H
+#define IMETAssocToolBase_H
+
+#include "AsgTools/IAsgTool.h"
+
+#include "xAODMissingET/MissingETContainer.h"
+#include "xAODMissingET/MissingETAssociationMap.h"
+	
+class IMETAssocToolBase : virtual public asg::IAsgTool {
+  ASG_TOOL_INTERFACE(IMETAssocToolBase)
+	
+  public:
+
+  virtual StatusCode execute(xAOD::MissingETContainer*,xAOD::MissingETAssociationMap*) = 0;
+	
+};
+	
+#endif
