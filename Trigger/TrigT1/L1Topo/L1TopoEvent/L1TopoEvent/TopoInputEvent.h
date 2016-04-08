66c9ba1f7a26805a3af9f684ee1b87cb532156a8
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:21:28 +0200

    'CMakeLists.txt' (L1TopoEvent-00-00-22)
    	* added LateMuon and MuonNextBC for BC+/-1
    	* L1TopoEvent-00-00-22

--- Trigger/TrigT1/L1Topo/L1TopoEvent/L1TopoEvent/TopoInputEvent.h
+++ Trigger/TrigT1/L1Topo/L1TopoEvent/L1TopoEvent/TopoInputEvent.h
@@ -11,6 +11,8 @@
 
 #include "L1TopoEvent/ClusterTOBArray.h"
 #include "L1TopoEvent/JetTOBArray.h"
+#include "L1TopoEvent/LateMuonTOBArray.h"
+#include "L1TopoEvent/MuonNextBCTOBArray.h"
 #include "L1TopoEvent/MuonTOBArray.h"
 #include "L1TopoEvent/MetTOBArray.h"
 #include "L1TopoCommon/StatusCode.h"
@@ -33,18 +35,24 @@ namespace TCS {
                      const std::string & tauName = "InputTaus",
                      const std::string & jetName = "InputJets",
                      const std::string & muonName = "InputMuons",
+                     const std::string & lateMuonName = "InputLateMuons",
+                     const std::string & muonNextBCName = "InputMuonsNextBC",
                      const std::string & metName = "InputMet");
       ~TopoInputEvent();
       
       const ClusterTOBArray & clusters() const { return m_clusters; }
       const ClusterTOBArray & taus() const { return m_taus; }
       const MuonTOBArray & muons() const { return m_muons; }
+      const LateMuonTOBArray & lateMuons() const { return m_lateMuons; }
+      const MuonNextBCTOBArray & muonsNextBC() const { return m_muonsNextBC; }
       const JetTOBArray & jets() const { return m_jets; }
       const MetTOB & met() const { return m_met[0]; }
       
       StatusCode addCluster(const ClusterTOB & cluster);
       StatusCode addTau(const ClusterTOB & tau);
       StatusCode addMuon(const MuonTOB & muon);
+      StatusCode addLateMuon(const LateMuonTOB & muon);
+      StatusCode addMuonNextBC(const MuonNextBCTOB & muon);
       StatusCode addJet(const JetTOB & jet);
       StatusCode setMET(const MetTOB & met);
 
@@ -72,6 +80,8 @@ namespace TCS {
       ClusterTOBArray   m_taus;
       JetTOBArray       m_jets;
       MuonTOBArray      m_muons;
+      LateMuonTOBArray  m_lateMuons;
+      MuonNextBCTOBArray  m_muonsNextBC;
       MetTOBArray       m_met; // will have size 1
 
       bool              m_dumpEnabled { false };
