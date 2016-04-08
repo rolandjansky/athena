05834b8997fc035eefa327c512e266c14a491ca0
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:59:05 +0200

    'CMakeLists.txt' (AFP_Digitization-00-01-01)
    	* Clean up clang warnings ATLASSIM-2661
    	* src/AFP_PileUpTool.cxx (remove abs with unsigned int argument
    	which is positive by definition).
    	* Tagging AFP_Digitization-00-01-01
    
    2016-01-27  John Chapman  <John.Chapman@cern.ch>
    
    	* cmt/requirements - add use statement for xAODEventInfo
    	* AFP_Digitization/AFP_PileUpTool.h,
    	src/AFP_PileUpTool.cxx (processBunchXing): update interface to use
    	xAODEventInfo rather than PileUpEventInfo. ATLASSIM-2242. Requires
    	PileUpTools-01-18-00 or later. (release 21 onwards) ATLASSIM-2122
    	* Tagging AFP_Digitization-00-01-00
    
    
    Former-commit-id: 74819aab2660af1ce955d91f63edcdb165e95447

--- ForwardDetectors/AFP/AFP_Digitization/AFP_Digitization/AFP_PileUpTool.h
+++ ForwardDetectors/AFP/AFP_Digitization/AFP_Digitization/AFP_PileUpTool.h
@@ -7,6 +7,10 @@
 
 #include "EventInfo/PileUpEventInfo.h"  /*SubEvent*/
 #include "PileUpTools/PileUpToolBase.h"
+#include "xAODEventInfo/EventInfo.h"             // NEW EDM
+#include "xAODEventInfo/EventAuxInfo.h"          // NEW EDM
+
+
 #include "GaudiKernel/Property.h"
 #include "GaudiKernel/ServiceHandle.h"
 #include "GaudiKernel/ITHistSvc.h"
@@ -41,24 +45,26 @@ class AFP_PileUpTool: public PileUpToolBase {
 		 const std::string& name,
 		 const IInterface* parent);
   
-  virtual StatusCode initialize();
-  virtual StatusCode finalize();
+  virtual StatusCode initialize() override final;
+  virtual StatusCode finalize() override final;
 
   /// called before the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode prepareEvent(const unsigned int nInputEvents);
+  virtual StatusCode prepareEvent(const unsigned int nInputEvents) override final;
   
   /// called for each active bunch-crossing to process current SubEvents bunchXing is in ns
-  virtual StatusCode processBunchXing(int bunchXing, 
-				      PileUpEventInfo::SubEvent::const_iterator bSubEvents,
-				      PileUpEventInfo::SubEvent::const_iterator eSubEvents);
+  virtual StatusCode processBunchXing(
+                                        int bunchXing,
+                                        SubEventIterator bSubEvents,
+                                        SubEventIterator eSubEvents
+                                        ) override final;
   /// return false if not interested in  certain xing times (in ns)
   /// implemented by default in PileUpToolBase as FirstXing<=bunchXing<=LastXing
   //  virtual bool toProcess(int bunchXing) const;
 
   /// called at the end of the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode mergeEvent();
+  virtual StatusCode mergeEvent() override final;
 
-  StatusCode processAllSubEvents();
+  virtual StatusCode processAllSubEvents() override final;
 
  private:
   
