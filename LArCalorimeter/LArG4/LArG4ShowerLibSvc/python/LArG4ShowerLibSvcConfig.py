eff943f5a5b8415d52da8d6abfe677a34649b8bf
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:56:31 +0200

    'CMakeLists.txt' (LArG4ShowerLibSvc-01-00-08)
    	* add python modules for CfgGetter configuration.
    	* Tag LArG4ShowerLibSvc-01-00-08
    
    2014-02-03 George Sedov <george.sedov@desy.de>
    
            * Support for ATHENA RNG service
            * Tag LArG4ShowerLibSvc-01-00-07

---
+++ LArCalorimeter/LArG4/LArG4ShowerLibSvc/python/LArG4ShowerLibSvcConfig.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon import CfgMgr
+
+def getLArG4ShowerLibSvc(name="LArG4ShowerLibSvc", **kwargs):
+    fileNameList = []
+    #TODO make this configurable based on simFlags?
+    # FCAL1 frozen shower libraries
+    fileNameList += [ "LArG4ShowerLib.FCAL1.11.root",
+                      "LArG4ShowerLib.FCAL1.22.root",
+                      "LArG4ShowerLib.FCAL1.2112.root",
+                      "LArG4ShowerLib.FCAL1.211.root" ]
+    # FCAL2 frozen shower libraries
+    fileNameList += [ "LArG4ShowerLib.FCAL2.11.root",
+                      "LArG4ShowerLib.FCAL2.22.root",
+                      "LArG4ShowerLib.FCAL2.2112.root",
+                      "LArG4ShowerLib.FCAL2.211.root" ]
+    kwargs.setdefault("FileNameList", fileNameList)
+    return CfgMgr.LArG4ShowerLibSvc(name, **kwargs)
