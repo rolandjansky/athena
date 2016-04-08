8be298f16d87c124d82d3a150f25cc6148ea7ad5
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:53:30 +0200

    'CMakeLists.txt' (MuonSimEventAthenaPool-00-02-01)
    	* Tagging MuonSimEventAthenaPool-00-02-01.
    	* Add regression test reading old data files.
    
    	* Tagging MuonSimEventAthenaPool-00-02-00.
    	* Revert to MuonSimEventAthenaPool-00-00-00 to allow further
    	development on the trunk.  (ATLASSIM-2669)
    
    2016-01-21 Olivier Arnaez <Olivier.Arnaez@cern.ch>
    	* Adding support for CSCSimHitCollectionCnv_p3,GenericMuonSimHitCollectionCnv_p2,TGCSimHitCollectionCnv_p4,MDTSimHitCollectionCnv_p3,RPCSimHitCollectionCnv_p3
    	* Modifying CSCSimHitCollectionCnv.h,TGCSimHitCollectionCnv.cxx,GenericMuonSimHitCollectionCnv.h,TGCSimHitCollectionCnv.h,MDTSimHitCollectionCnv.cxx,RPCSimHitCollectionCnv.cxx,CSCSimHitCollectionCnv.cxx,MDTSimHitCollectionCnv.h,RPCSimHitCollectionCnv.h,GenericMuonSimHitCollectionCnv.cxx
    	* tagging MuonSimEventAthenaPool-00-01-00

---
+++ MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/share/MuonSimEventTPCnv_HITS_test.py
@@ -0,0 +1,26 @@
+infile = 'rtt:valid1.110401.PowhegPythia_P2012_ttbar_nonallhad.simul.HITS.e3099_s2578_tid04919495_00/HITS.04919495._000416.pool.root.1'
+keys = [
+    #Muon::CSCSimHitCollection_p2
+    'CSC_Hits',
+    
+    #Muon::MDTSimHitCollection_p2
+    'MDT_Hits',
+
+    #Muon::RPCSimHitCollection_p2
+    'RPC_Hits',
+
+    #Muon::TGCSimHitCollection_p3
+    'TGC_Hits',
+         ]
+
+#from AthenaCommon.JobProperties import jobproperties
+#jobproperties.Global.DetDescrVersion = 'ATLAS-GEO-10-00-00'
+
+theApp.EvtMax = 10
+
+from AthenaCommon.DetFlags      import DetFlags
+DetFlags.detdescr.Calo_setOn()
+noMuon = True
+noID = True
+include ('AthenaPoolUtilities/TPCnvTest.py')
+
