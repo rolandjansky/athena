f15781d7432935a781bcc9f6de4fc7a7f4682fe1
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:46:14 +0200

    'CMakeLists.txt' (CaloSimEx-00-00-60)

---
+++ Calorimeter/CaloExample/CaloSimEx/doc/mainpage.h
@@ -0,0 +1,13 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+
+@mainpage CaloSimEx Package
+
+This package is used to test (ATN, RTT) simulation in the calorimeter.
+
+@author Karim Bernardet bernardet@cppm.in2p3.fr
+
+*/
