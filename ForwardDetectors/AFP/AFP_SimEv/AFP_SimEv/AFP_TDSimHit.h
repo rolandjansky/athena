aabfb44dbb627d6d9830e47a3d036ed22c0f7763
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:38:27 +0200

    'CMakeLists.txt' (AFP_SimEv-00-00-03)
    2015-05-22  John Chapman
    	* AFP_SimEv/AFP_TDSimHit.h, src/AFP_TDSimHit.cxx,
    	AFP_SimEv/AFP_SIDSimHit.h, src/AFP_SIDSimHit.cxx - add constructor
    	with arguments. Remove obsolete GetTrackID methods. Part of
    	tidying for MC16 - see ATLASSIM-1983
    	* Tagging AFP_SimEv-00-00-03
    
    2013-08-13 Tom Sykora
    	* modification of Si hits
    	* Tagging AFP_SimEv-00-00-02
    
    2013-07-25 Libor Nozka
    	* Fix checkreq warnings
    	* Tagging AFP_SimEv-00-00-01
    
    2013-07-08 Mateusz Dyndal
    	* Initial version
    	* Tagging AFP_SimEv-00-00-00

--- ForwardDetectors/AFP/AFP_SimEv/AFP_SimEv/AFP_TDSimHit.h
+++ ForwardDetectors/AFP/AFP_SimEv/AFP_SimEv/AFP_TDSimHit.h
@@ -8,36 +8,35 @@
 class AFP_TDSimHit
 {
  public:
-	AFP_TDSimHit();
-	virtual ~AFP_TDSimHit();
-	bool operator < (const AFP_TDSimHit& obj) const {return m_nHitID < obj.m_nHitID;}
-	int GetTrackID() const { return 22; }
+        AFP_TDSimHit(const int& nHitID, const int& nTrackID, const int& nParticleEncoding, const float& fKineticEnergy, const float& fEnergyDeposit, const float& fWaveLength, const float& fPreStepX, const float& fPreStepY, const float& fPreStepZ, const float& fPostStepX, const float& fPostStepY, const float& fPostStepZ, const float& fGlobalTime, const int& nStationID, const int& nDetectorID, const int& nSensitiveElementID);
+        AFP_TDSimHit();
+        virtual ~AFP_TDSimHit();
+        bool operator < (const AFP_TDSimHit& obj) const {return m_nHitID < obj.m_nHitID;}
 
-public:
-	int m_nHitID; // To identify the hit
-	int m_nTrackID;
-	int m_nParticleEncoding;         // PDG id 
-	float m_fKineticEnergy;          // kin energy of the particle
-	float m_fEnergyDeposit;          // energy deposit by the hit
-	float m_fWaveLength;          	 // wavelength of the photon
-	float m_fPreStepX;
-	float m_fPreStepY;
-	float m_fPreStepZ;
-	float m_fPostStepX;
-	float m_fPostStepY;
-	float m_fPostStepZ;
-	float m_fGlobalTime;
+public://FIXME eventually these should be made private and should be accessed via get/set methods.
+        int m_nHitID; // To identify the hit
+        int m_nTrackID;
+        int m_nParticleEncoding;         // PDG id
+        float m_fKineticEnergy;          // kin energy of the particle
+        float m_fEnergyDeposit;          // energy deposit by the hit
+        float m_fWaveLength;             // wavelength of the photon
+        float m_fPreStepX;
+        float m_fPreStepY;
+        float m_fPreStepZ;
+        float m_fPostStepX;
+        float m_fPostStepY;
+        float m_fPostStepZ;
+        float m_fGlobalTime;
 
-	int m_nStationID;
-	int m_nDetectorID;
-	int m_nSensitiveElementID;
+        int m_nStationID;
+        int m_nDetectorID;
+        int m_nSensitiveElementID;
 };
 
 
 inline float hitTime(const AFP_TDSimHit& hit)
 {
-	return hit.m_fGlobalTime;
+        return hit.m_fGlobalTime;
 }
 
 #endif //AFP_TDSimHit_h
-
--- ForwardDetectors/AFP/AFP_SimEv/src/AFP_SIDSimHit.cxx
+++ ForwardDetectors/AFP/AFP_SimEv/src/AFP_SIDSimHit.cxx
@@ -2,35 +2,59 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #include "AFP_SimEv/AFP_SIDSimHit.h"
+
+AFP_SIDSimHit::AFP_SIDSimHit(const int& nHitID, const int& nTrackID, const int& nParticleEncoding, const float& fKineticEnergy, const float& fEnergyDeposit, const float& fPreStepX, const float& fPreStepY, const float& fPreStepZ, const float& fPostStepX, const float& fPostStepY, const float& fPostStepZ, const float& fGlobalTime, const int& nStationID, const int& nDetectorID, const bool& bIsAuxVSID, const int& nPixelRow, const int& nPixelCol)
+{
+  m_nHitID=          nHitID;
+  m_nTrackID=        nTrackID;
+  m_nParticleEncoding=nParticleEncoding;
+  m_fKineticEnergy= fKineticEnergy;
+  m_fEnergyDeposit= fEnergyDeposit;
+  m_fPreStepX=      fPreStepX;
+  m_fPreStepY=      fPreStepY;
+  m_fPreStepZ=      fPreStepZ;
+  m_fPostStepX=     fPostStepX;
+  m_fPostStepY=     fPostStepY;
+  m_fPostStepZ=     fPostStepZ;
+  m_fGlobalTime=    fGlobalTime;
+
+  m_nStationID=      nStationID;
+  m_nDetectorID=     nDetectorID;
+
+  m_nPixelRow=       nPixelRow;
+  m_nPixelCol=       nPixelCol;
+
+  m_bIsAuxVSID=bIsAuxVSID;
+}
+
+
 
 AFP_SIDSimHit::AFP_SIDSimHit( )
 {
-	m_nHitID=-1;
-	m_nTrackID=-1;
-	m_nParticleEncoding=-1;
-	m_fKineticEnergy=0.0;
-	m_fEnergyDeposit=0.0;
-	m_fPreStepX=0.0;
-	m_fPreStepY=0.0;
-	m_fPreStepZ=0.0;
-	m_fPostStepX=0.0;
-	m_fPostStepY=0.0;
-	m_fPostStepZ=0.0;
-	m_fGlobalTime=0.0;
+        m_nHitID=-1;
+        m_nTrackID=-1;
+        m_nParticleEncoding=-1;
+        m_fKineticEnergy=0.0;
+        m_fEnergyDeposit=0.0;
+        m_fPreStepX=0.0;
+        m_fPreStepY=0.0;
+        m_fPreStepZ=0.0;
+        m_fPostStepX=0.0;
+        m_fPostStepY=0.0;
+        m_fPostStepZ=0.0;
+        m_fGlobalTime=0.0;
 
-	m_nStationID=-1;
-	m_nDetectorID=-1;
-	
-	m_nPixelRow=-1;
-	m_nPixelCol=-1;
-	
-	m_bIsAuxVSID=false;
+        m_nStationID=-1;
+        m_nDetectorID=-1;
+
+        m_nPixelRow=-1;
+        m_nPixelCol=-1;
+
+        m_bIsAuxVSID=false;
 }
 
 AFP_SIDSimHit::~AFP_SIDSimHit()
 {
 
 }
-
