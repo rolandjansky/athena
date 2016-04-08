42be4216e365c2f349f33a495f888abee62592d2
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 15:44:37 +0200

    'CMakeLists.txt' (VP13DCocktailPlugin-00-09-01)

--- graphics/VP1/VP1Plugins/VP13DCocktailPlugin/src/VP13DCocktailChannel.cxx
+++ graphics/VP1/VP1Plugins/VP13DCocktailPlugin/src/VP13DCocktailChannel.cxx
@@ -14,7 +14,7 @@
 
 #include "VP13DCocktailPlugin/VP13DCocktailChannel.h"
 #include "VP1GeometrySystems/VP1GeometrySystem.h"
-#include "VP1MissingEtSystems/VP1MissingEtSystem.h"
+// #include "VP1MissingEtSystems/VP1MissingEtSystem.h"
 #include "VP1SimHitSystems/VP1SimHitSystem.h"
 #include "VP1CaloSystems/VP1CaloCellSystem.h"
 #include "VP1TrackingGeometrySystems/VP1TrackingGeometrySystem.h"
@@ -29,21 +29,22 @@
 #include "VP1TrackSystems/VP1TrackSystem.h"
 #include "VP1GuideLineSystems/VP1GuideLineSystem.h"
 //#include "VP1JetSystems/VP1JetSystem.h"
-#include "VP1VertexSystems/VP1VertexSystem.h"
+// #include "VP1VertexSystems/VP1VertexSystem.h"
 #include "VP1PRDSystems/VP1PrepRawDataSystem.h"
 #include "VP1RawDataSystems/VP1RawDataSystem.h"
 #include "VP1UtilitySystems/VP1ExtraInputSystem.h"
 #include "VP1UtilitySystems/VP1PartSpectSystem.h"
 //#include "VP1FatrasSystems/VP1FatrasSingleTrackSimSystem.h" // TODO: to be updated when the new Fatras will be available
-#include "VP1CaloClusterSystems/VP1CaloClusterSystem.h"
+// #include "VP1CaloClusterSystems/VP1CaloClusterSystem.h"
 #include "VP1PlugUtils/VP1SysConf.h"
 #include "VP1CaloReadoutSystems/VP1CaloReadoutSystem.h"
 #include "VP1BPhysSystems/VP1BPhysSystem.h"
+#include "VP1AODSystems/VP1AODSystem.h"
 
 
 VP13DCocktailChannel::VP13DCocktailChannel()
   : IVP13DStandardChannelWidget(VP1CHANNELNAMEINPLUGIN(VP13DCocktailChannel,"3DCocktail"),
-				"This channel shows essentially all 3D systems.",
+				"This channel shows essentially all functional 3D systems.",
 				"Thomas.Kittelmann@cern.ch")
 {
 }
@@ -68,9 +69,9 @@ void VP13DCocktailChannel::init()
   addSystem(prdsys, IVP13DStandardChannelWidget::StartDisabled);
 
   addSystem(new VP1CaloCellSystem,IVP13DStandardChannelWidget::StartDisabled);
-  addSystem(new VP1CaloClusterSystem,IVP13DStandardChannelWidget::StartDisabled);
+  // addSystem(new VP1CaloClusterSystem,IVP13DStandardChannelWidget::StartDisabled);
   //addSystem(new VP1JetSystem,IVP13DStandardChannelWidget::StartDisabled); // TODO: has to be updated with the new xAOD Jet class
-  addSystem(new VP1MissingEtSystem,IVP13DStandardChannelWidget::StartDisabled);
+  // addSystem(new VP1MissingEtSystem,IVP13DStandardChannelWidget::StartDisabled);
   addSystem(new VP1RawDataSystem, IVP13DStandardChannelWidget::StartDisabled);
   addSystem(new VP1SimHitSystem,IVP13DStandardChannelWidget::StartDisabled);
   addSystem(new VP1TrackingGeometrySystem, IVP13DStandardChannelWidget::StartDisabled);
@@ -80,8 +81,12 @@ void VP13DCocktailChannel::init()
   addSystem(new VP1TrigROISystem,IVP13DStandardChannelWidget::StartDisabled);
 #endif
 */
-  VP1VertexSystem* vertexsys = new VP1VertexSystem;
-  addSystem(vertexsys,IVP13DStandardChannelWidget::StartDisabled);
+  // VP1VertexSystem* vertexsys = new VP1VertexSystem;
+  // addSystem(vertexsys,IVP13DStandardChannelWidget::StartDisabled);
+
+  // VP1VertexSystem* vertexsys = new VP1VertexSystem;
+  addSystem(new VP1AODSystem,IVP13DStandardChannelWidget::StartDisabled);
+
 
   addSystem(new VP1ExtraInputSystem(),IVP13DStandardChannelWidget::StartDisabled);
   addSystem(new VP1CaloReadoutSystem,IVP13DStandardChannelWidget::StartDisabled);
@@ -97,5 +102,5 @@ void VP13DCocktailChannel::init()
   foreach(QString n,VP1SysConf::extraTrackSystems())
     addSystem(new VP1TrackSystem(n),IVP13DStandardChannelWidget::StartDisabled);
 
-  VP1SysConf::setupStandardConnectionsAndOptions( guidelinesystem,geosys,tracksys,prdsys,vertexsys,VP1SysConf::EVENTSTUDIES );
+  VP1SysConf::setupStandardConnectionsAndOptions( guidelinesystem,geosys,tracksys,prdsys,0,VP1SysConf::EVENTSTUDIES );
 }
