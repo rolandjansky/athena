e0ba537c660ecd1b9189e038a879236039fb2adc
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:19:51 +0200

    'CMakeLists.txt' (LArCabling-00-00-01)

---
+++ LArCalorimeter/LArCabling/LArCabling/LArCablingDict.h
@@ -0,0 +1,21 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+ * @file LArToolsDict.h
+ *
+ * @brief This file includes the class for dictionary definitions
+ *
+ * @author RD Schaffer <R.D.Schaffer@cern.ch>
+ *
+ * $Id: LArToolsDict.h,v 1.6 2008-08-12 13:41:59 wlampl Exp $
+ */
+#ifndef LARCABLING_LARCABLINGDICT_H
+# define LARCABLING_LARCABLINGDICT_H
+
+#include "LArCabling/LArCablingService.h"
+#include "LArCabling/LArSuperCellCablingTool.h"
+#include "LArCabling/LArHVCablingTool.h"
+
+#endif // LARCABLING_LARCABLINGDICT_H
