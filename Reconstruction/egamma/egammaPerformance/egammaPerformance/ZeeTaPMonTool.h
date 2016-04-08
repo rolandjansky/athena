03e74e81991564d5524e39475502e8ec7d46fa2a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 15:59:24 +0200

    'CMakeLists.txt' (egammaPerformance-00-03-30)

--- Reconstruction/egamma/egammaPerformance/egammaPerformance/ZeeTaPMonTool.h
+++ Reconstruction/egamma/egammaPerformance/egammaPerformance/ZeeTaPMonTool.h
@@ -58,10 +58,17 @@ class ZeeTaPMonTool : public egammaMonToolBase
   TH1 * m_hIDEt;
   TH1 * m_hIDEta;
   TH1 * m_hIDPhi;
+  TProfile * m_effhIDEt;
+  TProfile * m_effhIDEta;
+  TProfile * m_effhIDPhi;
+
   // Efficiencies of topoEtCone40 isolation criteria vs pT, eta and phi
   TH1 * m_hISOEt;
   TH1 * m_hISOEta;
   TH1 * m_hISOPhi;
+  TProfile * m_effhISOEt;
+  TProfile * m_effhISOEta;
+  TProfile * m_effhISOPhi;
 
   // Loose electrons histograms
   TH1 * m_hN;       // Histogram for number of electrons
