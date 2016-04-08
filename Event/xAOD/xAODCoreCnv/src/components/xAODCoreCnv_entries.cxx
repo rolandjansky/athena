1749f48857bc69af23f48db9870d307c15550ded
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:01:22 +0200

    'CMakeLists.txt' (xAODCoreCnv-00-00-02)
    Former-commit-id: d4347801c6a4d88daff4013884cb9a9e4f99f70c

--- Event/xAOD/xAODCoreCnv/src/components/xAODCoreCnv_entries.cxx
+++ Event/xAOD/xAODCoreCnv/src/components/xAODCoreCnv_entries.cxx
@@ -1,18 +1,15 @@
-// $Id: xAODCoreCnv_entries.cxx 751107 2016-05-31 11:23:23Z krasznaa $
+// $Id: xAODCoreCnv_entries.cxx 583869 2014-02-18 11:31:09Z krasznaa $
 
 // Gaudi/Athena include(s):
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Local include(s):
 #include "../AuxStoreWrapper.h"
-#include "../ElementLinkResetAlg.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, AuxStoreWrapper )
-DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, ElementLinkResetAlg )
 
-DECLARE_FACTORY_ENTRIES( xAODCoreCnv ) {
+DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
 
    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, AuxStoreWrapper )
-   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, ElementLinkResetAlg )
 
 }
