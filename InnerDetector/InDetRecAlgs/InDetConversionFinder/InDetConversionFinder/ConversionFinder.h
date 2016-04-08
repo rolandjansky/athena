6bef08cac171c0bb7932afd1c3d6f9906c8e989a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:18:39 +0200

    'CMakeLists.txt' (InDetConversionFinder-01-00-02)
            * Correct vertex momentum calc
            * tag InDetConversionFinder-01-00-02
    
    2014-07-16 Anthony Morley
            * Remove ability  to make conversions from Trk::Track and Rec::TrackParticles
            * Add ability to make consversions xAOD::TrackParticles
            * tag InDetConversionFinder-01-00-01

--- InnerDetector/InDetRecAlgs/InDetConversionFinder/InDetConversionFinder/ConversionFinder.h
+++ InnerDetector/InDetRecAlgs/InDetConversionFinder/InDetConversionFinder/ConversionFinder.h
@@ -15,40 +15,46 @@
 
 #include "AthenaBaseComps/AthAlgorithm.h"
 #include "GaudiKernel/ToolHandle.h"
-#include "VxVertex/VxContainer.h"
+#include "xAODTracking/VertexContainerFwd.h"
+
+class IEMExtrapolationTools;
+
 
 namespace InDet{
 
-	class IVertexFinder;
-	class ConversionFinder : public AthAlgorithm {
-	
- 	public:  
-  		ConversionFinder(const std::string &name, ISvcLocator *pSvcLocator);
-  		~ConversionFinder();
-	        StatusCode initialize();
-	        StatusCode finalize();
-  		StatusCode execute();
-		void resetStatistics();  //<! Initialize the statistics vectors
-                void analyzeResults(VxContainer*); //<! Analyze the conversion candidates
-
- 	protected:
+  class IVertexFinder;
+  class ConversionFinder : public AthAlgorithm {
+    
+  public:  
+    ConversionFinder(const std::string &name, ISvcLocator *pSvcLocator);
+    ~ConversionFinder();
+    StatusCode initialize();
+    StatusCode finalize();
+    StatusCode execute();
+    void resetStatistics();  //<! Initialize the statistics vectors
+    void analyzeResults(xAOD::VertexContainer*);
+    
+  protected:
 	    
-	    std::string m_tracksName; //!< Name of track container in StoreGate
-	    std::string m_InDetConversionOutputName; //!< Name of output container to store results
+    std::string m_tracksName; //!< Name of track container in StoreGate
+    std::string m_InDetConversionOutputName; //!< Name of output container to store results
+    
+    ToolHandle< IVertexFinder > m_VertexFinderTool;  //<! Vertex finder tool
+    ToolHandle< IEMExtrapolationTools >  m_EMExtrapolationTool;
 
-            ToolHandle< IVertexFinder > m_VertexFinderTool;  //<! Vertex finder tool
+    bool m_doExtrapolation;
+    
+      /** Statistics  */
+    long m_events_processed;    //!< Number of events processed
+    long m_Gamma_stored;        //!< Number of conversion vertices stored
+    long m_Double_Conversions;  //!< Number of two-track conversions
+    long m_Single_Conversions;  //!< Number of single-track conversions
+    long m_SiSi_Conversions;    //!< Number of Si-Si track conversions
+    long m_SiTrt_Conversions;   //!< Number of Si-TRT track conversions
+    long m_TrtTrt_Conversions;  //!< Number of TRT-TRT track conversions
+    long m_Si_Conversions;      //!< Number of Si single-track conversions
+    long m_Trt_Conversions;     //!< Number of TRT single-track conversions
 
-            /** Statistics  */
-            long m_events_processed;    //!< Number of events processed
-            long m_Gamma_stored;        //!< Number of conversion vertices stored
-            long m_Double_Conversions;  //!< Number of two-track conversions
-	    long m_Single_Conversions;  //!< Number of single-track conversions
-	    long m_SiSi_Conversions;    //!< Number of Si-Si track conversions
-	    long m_SiTrt_Conversions;   //!< Number of Si-TRT track conversions
-	    long m_TrtTrt_Conversions;  //!< Number of TRT-TRT track conversions
-	    long m_Si_Conversions;      //!< Number of Si single-track conversions
-	    long m_Trt_Conversions;     //!< Number of TRT single-track conversions
-
-	};
+  };
 }
 #endif // INDETCONVERSIONFINDER_CONVERSIONFINDER_H
--- InnerDetector/InDetRecAlgs/InDetConversionFinder/src/ConversionFinder.cxx
+++ InnerDetector/InDetRecAlgs/InDetConversionFinder/src/ConversionFinder.cxx
@@ -14,193 +14,203 @@
 
 #include "TrkTrack/TrackCollection.h"
 #include "InDetRecToolInterfaces/IVertexFinder.h"
-#include "VxVertex/VxTrackAtVertex.h"
 #include "TrkTrackSummary/TrackSummary.h"
 #include "GaudiKernel/MsgStream.h"
 
-#include "TrkParticleBase/LinkToTrackParticleBase.h"
-#include "TrkParticleBase/TrackParticleBase.h"
+
+#include "xAODTracking/TrackParticleContainer.h"
+#include "xAODTracking/TrackParticle.h"
+#include "xAODTracking/VertexContainer.h"
+#include "xAODTracking/Vertex.h"
+#include "xAODTracking/VertexAuxContainer.h"
+
+#include "egammaInterfaces/IEMExtrapolationTools.h"
+
 
 namespace InDet
 {
-    ConversionFinder::ConversionFinder(const std::string& name, ISvcLocator* pSvcLocator) 
-	: AthAlgorithm(name, pSvcLocator),
-        m_tracksName("Tracks"),
-        m_InDetConversionOutputName("InDetConversion"),
-        m_VertexFinderTool("InDet::InDetConversionFinderTools")
-    {
-	/* Retrieve StoreGate container and tool names from job options */
-	declareProperty("TracksName",m_tracksName);
-	declareProperty("InDetConversionOutputName", m_InDetConversionOutputName);
-	declareProperty("VertexFinderTool",m_VertexFinderTool);
-
-    }
+  ConversionFinder::ConversionFinder(const std::string& name, ISvcLocator* pSvcLocator) 
+  : AthAlgorithm(name, pSvcLocator),
+  m_tracksName("InDetTrackParticles"),
+  m_InDetConversionOutputName("InDetConversion"),
+  m_VertexFinderTool("InDet::InDetConversionFinderTools"),
+  m_EMExtrapolationTool("EMExtrapolationTools"),
+  m_doExtrapolation(false)
+  {
+    /* Retrieve StoreGate container and tool names from job options */
+    declareProperty("TracksName",m_tracksName);
+    declareProperty("InDetConversionOutputName", m_InDetConversionOutputName);
+    declareProperty("VertexFinderTool",m_VertexFinderTool);
+    declareProperty("ExtrapolationTool", m_EMExtrapolationTool, "Handle of the extrapolation tool");
+    declareProperty("doExtrapolation", m_doExtrapolation );
+	}
     
-    ConversionFinder::~ConversionFinder(){}
+  ConversionFinder::~ConversionFinder(){}
 
-   StatusCode ConversionFinder::initialize()
-   {
-        /* Get the VertexFinderTool */
-        if ( m_VertexFinderTool.retrieve().isFailure() ) {
-           msg(MSG::FATAL) << "Failed to retrieve tool " << m_VertexFinderTool << endreq;
-           return StatusCode::FAILURE;
-        } else {
-	  msg(MSG::INFO) << "Retrieved tool " << m_VertexFinderTool << endreq;
+  StatusCode ConversionFinder::initialize()
+  {
+    /* Get the VertexFinderTool */
+    if ( m_VertexFinderTool.retrieve().isFailure() ) {
+       msg(MSG::FATAL) << "Failed to retrieve tool " << m_VertexFinderTool << endreq;
+       return StatusCode::FAILURE;
+    } else {
+       msg(MSG::INFO) << "Retrieved tool " << m_VertexFinderTool << endreq;
+    }
+
+
+    if ( m_doExtrapolation && m_EMExtrapolationTool.retrieve().isFailure() ) {
+       msg(MSG::INFO) << "Failed to retrieve tool " << m_EMExtrapolationTool << endreq;
+       m_doExtrapolation = false;
+    } else {
+       msg(MSG::INFO) << "Retrieved tool " << m_EMExtrapolationTool << endreq;
+    }
+
+
+    resetStatistics();
+
+    msg(MSG::INFO) << "Initialization successful" << endreq;
+
+    return StatusCode::SUCCESS;
+  }
+
+  StatusCode ConversionFinder::finalize()
+  {
+    if ( msg().level() == MSG::INFO ){
+      msg(MSG::INFO) <<"Summary:  "<<endreq;
+      std::cout<< "------------------------------------------------------------"<<std::endl;
+      std::cout<< "Processed: " << m_events_processed<< " events " <<std::endl;
+      std::cout<< "Stored   : " << m_Gamma_stored<<" Conversions " <<std::endl;
+      std::cout<< "------------------------------------------------------------"<<std::endl;
+      std::cout<< "| Double Conversions       Si-Si      Si-TRT      TRT-TRT  |"<<std::endl;
+      std::cout<<"| "<<
+      std::setw(10)<<m_Double_Conversions<<
+      std::setw(18)<<m_SiSi_Conversions<<
+      std::setw(11)<<m_SiTrt_Conversions<<
+      std::setw(13)<<m_TrtTrt_Conversions<<"     |"<<
+      std::endl;
+      std::cout<< "------------------------------------------------------------" <<std::endl;
+      std::cout<< "| Single Conversions               Si                TRT   |"<<std::endl;
+      std::cout<<"| "<<
+      std::setw(10)<<m_Single_Conversions<<
+      std::setw(25)<<m_Si_Conversions<<
+      std::setw(19)<<m_Trt_Conversions<<"    |"<<
+      std::endl;
+      std::cout<< "------------------------------------------------------------" <<std::endl;
+    }
+    return StatusCode::SUCCESS;
+  }
+
+  void ConversionFinder::resetStatistics() {
+    m_events_processed   = 0;
+    m_Gamma_stored       = 0;
+    m_Double_Conversions = 0;
+    m_Single_Conversions = 0;
+    m_SiSi_Conversions   = 0;
+    m_SiTrt_Conversions  = 0;
+    m_TrtTrt_Conversions = 0;
+    m_Si_Conversions     = 0;
+    m_Trt_Conversions    = 0;
+  }
+
+  StatusCode ConversionFinder::execute()
+  {
+  
+    m_events_processed++;
+
+    const xAOD::TrackParticleContainer* trackParticleCollection(0);
+    if ( evtStore()->retrieve ( trackParticleCollection, m_tracksName ).isFailure() )
+    {
+      ATH_MSG_WARNING( "Could not find xAOD::TrackParticleContainer " << m_tracksName << " in StoreGate.");
+      return StatusCode::SUCCESS;
+    }
+
+    std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> conversions = m_VertexFinderTool->findVertex ( trackParticleCollection );
+
+    if (!conversions.first || !conversions.second)
+    {
+      ATH_MSG_WARNING("Null pointer to conversion container");
+      return StatusCode::SUCCESS;
+    }
+    ATH_MSG_DEBUG("New conversion container size: " << conversions.first->size());
+
+    m_Gamma_stored += conversions.first->size();
+  
+  
+    // Decorate the vertices with the momentum at the conversion point and 
+    // etaAtCalo, phiAtCalo (extrapolate each vertex)
+    if(m_doExtrapolation){
+      float etaAtCalo = -9999., phiAtCalo = -9999.;
+      for (auto vertex : *(conversions.first))
+      {
+
+        Amg::Vector3D momentum(0., 0., 0.);
+        for (unsigned int i = 0; i < vertex->nTrackParticles(); ++i){
+          momentum += m_EMExtrapolationTool->getMomentumAtVertex(*vertex, i);
         }
 
-	resetStatistics();
-
-	msg(MSG::INFO) << "Initialization successful" << endreq;
-
-	return StatusCode::SUCCESS;
-   }
-
-   StatusCode ConversionFinder::finalize()
-   {
-      if ( msg().level() == MSG::INFO ){
-          msg(MSG::INFO) <<"Summary:  "<<endreq;
-          std::cout<< "------------------------------------------------------------"<<std::endl;
-          std::cout<< "Processed: " << m_events_processed<< " events " <<std::endl;
-          std::cout<< "Stored   : " << m_Gamma_stored<<" Conversions " <<std::endl;
-          std::cout<< "------------------------------------------------------------"<<std::endl;
-	  std::cout<< "| Double Conversions       Si-Si      Si-TRT      TRT-TRT  |"
-	  <<std::endl;
-	  std::cout<<"| "<<
-	  std::setw(10)<<m_Double_Conversions<<
-          std::setw(18)<<m_SiSi_Conversions<<
-	  std::setw(11)<<m_SiTrt_Conversions<<
-	  std::setw(13)<<m_TrtTrt_Conversions<<"     |"<<
-	  std::endl;
-          std::cout<< "------------------------------------------------------------" <<std::endl;
-	  std::cout<< "| Single Conversions               Si                TRT   |"
-	  <<std::endl;
-	  std::cout<<"| "<<
-	  std::setw(10)<<m_Single_Conversions<<
-          std::setw(25)<<m_Si_Conversions<<
-	  std::setw(19)<<m_Trt_Conversions<<"    |"<<
-	  std::endl;
-          std::cout<< "------------------------------------------------------------" <<std::endl;
+        vertex->auxdata<float>("px") = momentum.x();
+        vertex->auxdata<float>("py") = momentum.y();
+        vertex->auxdata<float>("pz") = momentum.z();
+    
+        if (!m_EMExtrapolationTool->getEtaPhiAtCalo(vertex, &etaAtCalo, &phiAtCalo))
+        { 
+          ATH_MSG_DEBUG("getEtaPhiAtCalo failed!");
+        }
+    
+        // Decorate vertex with etaAtCalo, phiAtCalo
+        vertex->auxdata<float>("etaAtCalo") = etaAtCalo;
+        vertex->auxdata<float>("phiAtCalo") = phiAtCalo;
       }
-      return StatusCode::SUCCESS;
-   }
-
-   void ConversionFinder::resetStatistics() {
-      m_events_processed   = 0;
-      m_Gamma_stored       = 0;
-      m_Double_Conversions = 0;
-      m_Single_Conversions = 0;
-      m_SiSi_Conversions   = 0;
-      m_SiTrt_Conversions  = 0;
-      m_TrtTrt_Conversions = 0;
-      m_Si_Conversions     = 0;
-      m_Trt_Conversions    = 0;
-   }
-
-    StatusCode ConversionFinder::execute()
-    {
-	
-	m_events_processed++;
-        VxContainer* InDetConversionContainer ( 0 );
-
-	//---- First try if m_tracksName is a TrackCollection -----------------//
-	if ( evtStore()->contains<TrackCollection> ( m_tracksName ) )
-	  {
-	    const DataVector<Trk::Track> * trk_coll(0);
-	    
-	    if( evtStore()->retrieve( trk_coll, m_tracksName ).isFailure() ){
-	      msg(MSG::DEBUG) << "Could not find Trk::TrackCollection " <<  m_tracksName << " in StoreGate." << endreq;
-	      return StatusCode::SUCCESS;
-	    } 
-	
-	    // Find conversions
-	    InDetConversionContainer = m_VertexFinderTool->findVertex ( trk_coll );
-	  }
-	//---- Second try if m_tracksName is a TrackParticleBaseCollection ----//
-	else if ( evtStore()->contains<Trk::TrackParticleBaseCollection> ( m_tracksName ) )
-	  {
-	    const Trk::TrackParticleBaseCollection *trackParticleBaseCollection(0);
-	    if ( evtStore()->retrieve ( trackParticleBaseCollection, m_tracksName ).isFailure() )
-	      {
-		msg(MSG::DEBUG) << "Could not find Trk::TrackParticleBaseCollection " << m_tracksName << " in StoreGate." << endreq;
-		return StatusCode::SUCCESS;
-	      }
-
-	    InDetConversionContainer = m_VertexFinderTool->findVertex ( trackParticleBaseCollection );
-	  }
-	else
-	  {
-	    msg(MSG::DEBUG) << "Neither a TrackCollection nor a TrackParticleBaseCollection with key " << m_tracksName << " exists in StoreGate." << endreq;
-	    return StatusCode::SUCCESS;
-	  }
-
-        //Analyze the results, update counters
-        if (InDetConversionContainer) {
-	  m_Gamma_stored += InDetConversionContainer->size();
-	  analyzeResults(InDetConversionContainer);
-	}
-
-	// Write to StoreGate
-	msg(MSG::DEBUG) << " Recording conversion collection with key: " << m_InDetConversionOutputName << endreq;
-	if( evtStore()->record(InDetConversionContainer, m_InDetConversionOutputName, false).isFailure() ){
-	  msg(MSG::ERROR) << "Could not register conversion." << endreq;
-	  return StatusCode::FAILURE;
-	}
-     
-	return StatusCode::SUCCESS;
     }
+    
+    analyzeResults(conversions.first);
+  
+    //put the new conversion vertex container and its aux container into StoreGate
+    ATH_MSG_DEBUG("Writing container " << m_InDetConversionOutputName);
+    CHECK( evtStore()->record(conversions.first, m_InDetConversionOutputName) );
+    CHECK( evtStore()->record(conversions.second, m_InDetConversionOutputName + "Aux.") );
 
-    void ConversionFinder::analyzeResults(VxContainer* convContainer) {
+    return StatusCode::SUCCESS;
+  }
 
-      VxContainer::const_iterator fz,fze = convContainer->end();
-      for ( fz = convContainer->begin(); fz!=fze; ++fz){
+  void ConversionFinder::analyzeResults(xAOD::VertexContainer* convContainer) {
 
-	if ((*fz)->vxTrackAtVertex()) {
-	  int numTracksPerVertex = (*fz)->vxTrackAtVertex()->size();
-          if  (numTracksPerVertex == 2) m_Double_Conversions++;
-	  else                          m_Single_Conversions++;
+    for ( auto fz : *convContainer){
 
-          bool isTrt1 = false; bool isSi1 = false; bool isTrt2 = false; bool isSi2 = false; 
-	  std::vector<Trk::VxTrackAtVertex*> * trkAtVtx = (*fz)->vxTrackAtVertex();
-	  for (unsigned int i = 0; i < trkAtVtx->size() ; ++i) {
+      int numTracksPerVertex = fz->nTrackParticles();
+      if  (numTracksPerVertex == 2) m_Double_Conversions++;
+      else                          m_Single_Conversions++;
 
-	    const Trk::ITrackLink * trLink =(*(trkAtVtx))[i]->trackOrParticleLink();
-	    const Trk::TrackParticleBase* tempTrk(0);
-	    if(0!= trLink){
-	      const Trk::LinkToTrackParticleBase * linkToTrackPB = dynamic_cast<const Trk::LinkToTrackParticleBase *>(trLink); 
-	      if(0!= linkToTrackPB){
-		if(linkToTrackPB->isValid()) tempTrk = linkToTrackPB->cachedElement();
-		if(!tempTrk) continue;
-		const Trk::TrackSummary* summary = tempTrk->trackSummary();
-		if(!summary) continue;
-	        int ncl  = summary->get(Trk::numberOfPixelHits) + summary->get(Trk::numberOfSCTHits);
-	        int ntrt = summary->get(Trk::numberOfTRTHits);
-		if(i==0) {
-		  if(ncl>0) isSi1 = true; if(ncl==0 && ntrt>0) isTrt1 = true;
-		}
-		if(i==1) {
-		  if(ncl>0) isSi2 = true; if(ncl==0 && ntrt>0) isTrt2 = true;
-		}
-	      }//end of dynamic_cast check
-	    }//end of ITrackLink existance check
+      bool isTrt1 = false; bool isSi1 = false; bool isTrt2 = false; bool isSi2 = false; 
+      for (unsigned int i = 0; i < fz->nTrackParticles() ; ++i) {
+        auto trackParticle = fz->trackParticle( i );
+        if(!trackParticle) continue;
+        uint8_t temp(0);
+        uint8_t ncl(0);
+        uint8_t ntrt(0); 
+                 
+        if( trackParticle->summaryValue( temp , xAOD::numberOfPixelHits) ) ncl += temp; 
+        if( trackParticle->summaryValue( temp , xAOD::numberOfSCTHits)   ) ncl += temp; 
+        if( trackParticle->summaryValue( temp , xAOD::numberOfTRTHits)   ) ntrt += temp; 
+        if(i==0) {
+          if(ncl>0) isSi1 = true; if(ncl==0 && ntrt>0) isTrt1 = true;
+        }
+        if(i==1) {
+          if(ncl>0) isSi2 = true; if(ncl==0 && ntrt>0) isTrt2 = true;
+        }
 
-	  }
-
-	  //Decide on the type of track combination in vertex
-	  if(numTracksPerVertex==2){
-	    if      (isSi1 && isSi2)  m_SiSi_Conversions++;
-	    else if(isTrt1 && isTrt2) m_TrtTrt_Conversions++;
-	    else                      m_SiTrt_Conversions++;
-	  }
-	  if(numTracksPerVertex==1){
-	    if(isSi1)  m_Si_Conversions++;
-	    if(isTrt1) m_Trt_Conversions++;
-	  }
-	
-	}
-
+        //Decide on the type of track combination in vertex
+        if(numTracksPerVertex==2){
+          if      (isSi1 && isSi2)  m_SiSi_Conversions++;
+          else if(isTrt1 && isTrt2) m_TrtTrt_Conversions++;
+          else                      m_SiTrt_Conversions++;
+        }
+        if(numTracksPerVertex==1){
+          if(isSi1)  m_Si_Conversions++;
+          if(isTrt1) m_Trt_Conversions++;
+        }
       }
-
-   }
-
+    }
+  }
 }
 
