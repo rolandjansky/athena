f03e783826a7a05c9d2d0efa489eab92dabf3e5b
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:33:20 +0200

    'CMakeLists.txt' (TileMuId-00-03-05)
    	* src/TileLookForMuAlg.cxx fixing bug in energy cut condition
    	* A-cell threshold was appled to E-cell
    	* Tagging TileMuId-00-03-05
    
    2016-01-28  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* TileLookForMuAlg: fixed some class member names to follow ATLAS conventions
    	* TileMuToNtuple: fixed some class member names to follow ATLAS conventions
    	* Tagging TileMuId-00-03-04
    
    
    Former-commit-id: 233727e46005c9eaf2a00e284cc3aee71b44ee85

--- TileCalorimeter/TileMuId/TileMuId/TileMuToNtuple.h
+++ TileCalorimeter/TileMuId/TileMuId/TileMuToNtuple.h
@@ -43,7 +43,7 @@ class TileMuToNtuple: public AthAlgorithm {
 
     NTuple::Tuple* m_ntuplePtr;
     int m_ntupleID;
-    int max_ntag;
+    int m_maxNtag;
     int m_close;
     std::string m_ntupleLoc;
 
@@ -53,7 +53,7 @@ class TileMuToNtuple: public AthAlgorithm {
     NTuple::Matrix<float> m_energy;
     NTuple::Array<float> m_quality;
 
-    std::string m_TileMuContainer;
+    std::string m_tileMuContainer;
 
 };
 
