612ffae5d8b798ef0cb644bb180d970338a03308
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:19:08 +0200

    'CMakeLists.txt' (TrigInDetToolInterfaces-00-03-08)
    	* extended ITrigL2LayerNumberTool interface to collect / export geometry information
    	* tagged as TrigInDetToolInterfaces-00-03-08
    
    2016-01-28 : Dmitry Emeliyanov <D-dot-Emeliyanov-at-rl-dot-ac-dot-uk>
    	* extended ITrigL2LayerNumberTool interface to support the new layer numbering scheme
    	* tagged as TrigInDetToolInterfaces-00-03-07
    
    2015-10-22 Stewart Martin-Haugh <smh -at- cern.ch>
    	* Updates for working with Trk::Tracks (ATR-12542)
    	* tagged as TrigInDetToolInterfaces-00-03-06
    
    
    Former-commit-id: 5e5a51297774dd64789087448813af25583f9390

--- Trigger/TrigTools/TrigInDetToolInterfaces/TrigInDetToolInterfaces/ITrigPrimaryVertexFitter.h
+++ Trigger/TrigTools/TrigInDetToolInterfaces/TrigInDetToolInterfaces/ITrigPrimaryVertexFitter.h
@@ -7,6 +7,7 @@
 
 #include<vector>
 #include "GaudiKernel/IAlgTool.h"
+#include "TrkTrack/TrackCollection.h"
 
 class TrigVertex;
 class TrigInDetTrackCollection;
@@ -22,6 +23,8 @@ class ITrigPrimaryVertexFitter: virtual public IAlgTool
   }
   virtual TrigVertex* fit(const TrigInDetTrackCollection*, double z=0.0) = 0;
   virtual TrigVertex* fit(const TrigInDetTrackCollection*, std::vector<double>&, double z=0.0) = 0;
+  virtual TrigVertex* fit(const TrackCollection*, TrackCollection&, double z=0.0) = 0;
+  virtual TrigVertex* fit(const TrackCollection*, std::vector<double>&, double z=0.0) = 0;
 };
 
 #endif
