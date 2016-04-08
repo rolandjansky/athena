316eef2ed8ecaf0966fb8d286a542641e5855314
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:27:49 +0200

    'CMakeLists.txt' (TrigTopoEventTPCnv-00-00-05)
    	* Tagging TrigTopoEventTPCnv-00-00-05.
    	* Remove #define private.
    	* Tagging TrigTopoEventTPCnv-00-00-04.
    	* Add unit test.

--- Trigger/TrigEvent/TrigTopoEventTPCnv/src/ElectronMuonTopoInfoCnv_p1.cxx
+++ Trigger/TrigEvent/TrigTopoEventTPCnv/src/ElectronMuonTopoInfoCnv_p1.cxx
@@ -2,13 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#define private public
-#define protected public
 #include "TrigTopoEvent/ElectronMuonTopoInfo.h"
-#undef private
-#undef protected
-
-
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfo_p1.h"
 #include "TrigTopoEventTPCnv/ElectronMuonTopoInfoCnv_p1.h"
 
@@ -19,13 +13,13 @@ void ElectronMuonTopoInfoCnv_p1::persToTrans(const ElectronMuonTopoInfo_p1* pers
 {
    log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::persToTrans called " << endreq;
 
-   transObj->m_roiWord        = persObj->m_roiWord        ;
-   transObj->m_DeltaPhi       = persObj->m_DeltaPhi       ;
-   transObj->m_DeltaR         = persObj->m_DeltaR          ;
-   transObj->m_InvMass        = persObj->m_InvMass         ;
-   transObj->m_electronValid  = persObj->m_electronValid   ;
-   transObj->m_oppositeCharge = persObj->m_oppositeCharge  ;
-   transObj->m_vertexState    = persObj->m_vertexState     ;
+   transObj->SetRoiWord        (persObj->m_roiWord)        ;
+   transObj->SetDeltaPhi       (persObj->m_DeltaPhi)       ;
+   transObj->SetDeltaR         (persObj->m_DeltaR)          ;
+   transObj->SetInvMass        (persObj->m_InvMass)         ;
+   transObj->SetElecValid      (persObj->m_electronValid)   ;
+   transObj->SetOppositeCharge (persObj->m_oppositeCharge)  ;
+   transObj->SetVertexState    (persObj->m_vertexState)     ;
  
 }
 
@@ -36,12 +30,12 @@ void ElectronMuonTopoInfoCnv_p1::transToPers(const ElectronMuonTopoInfo* transOb
 {
    log << MSG::DEBUG << "ElectronMuonTopoInfoCnv_p1::transToPers called " << endreq;
 
-   persObj->m_roiWord        = transObj->m_roiWord        ;
-   persObj->m_DeltaPhi       = transObj->m_DeltaPhi       ;
-   persObj->m_DeltaR         = transObj->m_DeltaR         ;
-   persObj->m_InvMass        = transObj->m_InvMass        ;
-   persObj->m_electronValid  = transObj->m_electronValid  ;
-   persObj->m_oppositeCharge = transObj->m_oppositeCharge ;
-   persObj->m_vertexState    = transObj->m_vertexState    ;
+   persObj->m_roiWord        = transObj->RoiWord()        ;
+   persObj->m_DeltaPhi       = transObj->DeltaPhi()       ;
+   persObj->m_DeltaR         = transObj->DeltaR()         ;
+   persObj->m_InvMass        = transObj->InvMass()        ;
+   persObj->m_electronValid  = transObj->ElecValid()  ;
+   persObj->m_oppositeCharge = transObj->OppositeCharge() ;
+   persObj->m_vertexState    = transObj->VertexState()    ;
 
 }
