728a380e51e5375cba28d67c4a235d71d9a4ee56
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:03:26 +0200

    'CMakeLists.txt' (xAODTrigCaloAthenaPool-00-00-07)
    	* Tagging xAODTrigCaloAthenaPool-00-00-07.
    	* src/xAODTrigCaloAthenaPoolTPCnv.cxx: Use
    	DECLARE_TRIGTPCNV_FACTORY.

--- Event/xAOD/xAODTrigCaloAthenaPool/src/xAODTrigCaloAthenaPoolTPCnv.cxx
+++ Event/xAOD/xAODTrigCaloAthenaPool/src/xAODTrigCaloAthenaPoolTPCnv.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODTrigCaloAthenaPoolTPCnv.cxx 705793 2015-11-04 13:13:26Z krasznaa $
+// $Id: xAODTrigCaloAthenaPoolTPCnv.cxx 713751 2015-12-09 20:24:00Z ssnyder $
 
 // Gaudi/Athena include(s):
 #include "AthenaKernel/TPCnvFactory.h"
@@ -20,14 +20,14 @@
 #include "xAODTrigEMClusterAuxContainerCnv_v1.h"
 
 // Declare the T/P converter(s):
-DECLARE_TPCNV_FACTORY( xAODCaloClusterTrigAuxContainerOfflineCnv_v1,
-                       xAOD::CaloClusterTrigAuxContainer,
-                       xAOD::CaloClusterAuxContainer_v1,
-                       Athena::TPCnvVers::Old )
-DECLARE_TPCNV_FACTORY( xAODCaloClusterTrigAuxContainerOfflineCnv_v2,
-                       xAOD::CaloClusterTrigAuxContainer,
-                       xAOD::CaloClusterAuxContainer_v2,
-                       Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODCaloClusterTrigAuxContainerOfflineCnv_v1,
+                           xAOD::CaloClusterTrigAuxContainer,
+                           xAOD::CaloClusterAuxContainer_v1,
+                           Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODCaloClusterTrigAuxContainerOfflineCnv_v2,
+                           xAOD::CaloClusterTrigAuxContainer,
+                           xAOD::CaloClusterAuxContainer_v2,
+                           Athena::TPCnvVers::Old )
 DECLARE_TPCNV_FACTORY( xAODTrigEMClusterAuxContainerCnv_v1,
                        xAOD::TrigEMClusterAuxContainer,
                        xAOD::TrigEMClusterAuxContainer_v1,
