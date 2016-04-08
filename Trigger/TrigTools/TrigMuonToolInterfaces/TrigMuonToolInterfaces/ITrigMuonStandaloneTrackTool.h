2a497621c0324893ccdcb83024d15c8efcaaa476
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:19:51 +0200

    'CMakeLists.txt' (TrigMuonToolInterfaces-00-00-20)
    	* ITrigMuonStandaloneTrackTool: Add function needed for internal caching
    	* TrigMuonToolInterfaces-00-00-20

--- Trigger/TrigTools/TrigMuonToolInterfaces/TrigMuonToolInterfaces/ITrigMuonStandaloneTrackTool.h
+++ Trigger/TrigTools/TrigMuonToolInterfaces/TrigMuonToolInterfaces/ITrigMuonStandaloneTrackTool.h
@@ -116,6 +116,9 @@ class ITrigMuonStandaloneTrackTool : virtual public IAlgTool {
   virtual void recordPatterns() = 0;
   virtual void recordSpectrometerTracks() = 0;
 
+  /** Get list of hashIDs (used for internal caching) **/
+  virtual std::vector<std::vector<IdentifierHash> > getHashList(const IRoiDescriptor* muonRoI)=0;
+
   /** Declares all segment monitoring histograms. returned monVars should be passed to getSegments() */
   virtual void declareSegmentMonitoringVariables(HLT::Algo* fexAlgo, 
 						 TrigMuonEFMonVars& monVars) = 0;
