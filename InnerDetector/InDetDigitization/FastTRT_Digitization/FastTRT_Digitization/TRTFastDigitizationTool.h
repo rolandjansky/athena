d55a5e1b7da1302c3c55c98cd4be2eef20166fe0
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:38:23 +0200

    'CMakeLists.txt' (FastTRT_Digitization-00-02-08)
    	* Migrate FastTRT_Digitization package to use xAOD::EventInfo, ATLASSIM-2534
    	* Tagged FastTRT_Digitization-00-02-08
    
    2016-02-05 Vladimir Lyubushkin <Vladimir.Lyubushkin@cern.ch>
    	* python/FastTRT_DigitizationConfig.py - request additional folders from CondDB,
    	needed by InDetTRT_ElectronPidTool, rename variables, used for output containers
    	* cmt/requirements - add use statements for CxxUtils and StoreGate
    	* src/TRTFastDigitizationTool.cxx, FastTRT_Digitization/TRTFastDigitizationTool.h -
    	switch to using SG::WriteHandles separate out the creation of output containers and
    	checking for neighbouring clusters into separate methods;
    	use CHECK macro wherever possible
    	* Tested under 20.7.X-VAL,rel_1
    	* Tagged FastTRT_Digitization-00-02-07
    
    2015-11-21 Vladimir Lyubushkin <Vladimir.Lyubushkin@cern.ch>
    	* fix Coverity defects 32713, ATLASSIM-2413
    	* Tagged FastTRT_Digitization-00-02-06
    
    2015-11-18 Vladimir Lyubushkin <Vladimir.Lyubushkin@cern.ch>
    	* tune correction for high threshold probability separately for electron
    ...
    (Long ChangeLog diff - truncated)

--- InnerDetector/InDetDigitization/FastTRT_Digitization/FastTRT_Digitization/TRTFastDigitizationTool.h
+++ InnerDetector/InDetDigitization/FastTRT_Digitization/FastTRT_Digitization/TRTFastDigitizationTool.h
@@ -18,6 +18,9 @@
 #include "GaudiKernel/ToolHandle.h"
 
 #include "InDetPrepRawData/TRT_DriftCircleContainer.h"
+#include "TRT_DriftFunctionTool/ITRT_DriftFunctionTool.h"
+#include "TRT_ConditionsServices/ITRT_StrawStatusSummarySvc.h"
+#include "TrkToolInterfaces/ITRT_ElectronPidTool.h"
 
 #include "GaudiKernel/RndmGenerators.h"
 
@@ -31,16 +34,17 @@
 
 #include "TrkTruthData/PRD_MultiTruthCollection.h"
 
-//#include "GaudiKernel/IPartPropSvc.h"
-//#include "HepPDT/ParticleDataTable.hh"
+#include "xAODEventInfo/EventInfo.h"
+#include "xAODEventInfo/EventAuxInfo.h"
 
 #include "HitManagement/TimedHitCollection.h"
 #include "InDetSimEvent/TRTUncompressedHitCollection.h"
+#include "StoreGate/WriteHandle.h"
 
 #include <utility>
 #include <vector>
 #include <map>
-
+#include <cmath>
 
 class PileUpMergeSvc;
 class IAtRndmGenSvc;
@@ -67,8 +71,8 @@ public:
   ///called for each active bunch-crossing to process current SubEvents
   /// bunchXing is in ns
   StatusCode processBunchXing( int bunchXing,
-                               PileUpEventInfo::SubEvent::const_iterator bSubEvents,
-                               PileUpEventInfo::SubEvent::const_iterator eSubEvents );
+                               SubEventIterator bSubEvents,
+                               SubEventIterator eSubEvents );
 
   /// return false if not interested in  certain xing times (in ns)
   /// implemented by default in PileUpToolBase as FirstXing<=bunchXing<=LastXing
@@ -89,6 +93,7 @@ public:
 private:
 
   StatusCode produceDriftCircles();
+  StatusCode createOutputContainers();
 
   Identifier getIdentifier( int hitID, IdentifierHash &hash, Identifier &layer_id, bool &status ) const;
 
@@ -96,56 +101,46 @@ private:
 
   static double getDriftRadiusFromXYZ( const TimedHitPtr< TRTUncompressedHit > &hit );
   HepGeom::Point3D< double > getGlobalPosition( const TimedHitPtr< TRTUncompressedHit > &hit );
+  bool isArgonStraw( const Identifier &straw_id ) const;
+  int gasType( const Identifier &straw_id ) const;
+  double getProbHT( int particleEncoding, float kineticEnergy, const Identifier &straw_id, double driftRadiusLoc, double hitGlobalPosition ) const;
   static double HTProbabilityElectron_high_pt( double eta );
   static double HTProbabilityElectron_low_pt( double eta );
   static double HTProbabilityMuon_5_20( double eta );
   static double HTProbabilityMuon_60( double eta );
-  static double strawEfficiency( double driftRadius );                  // defined, but not used
+  static double strawEfficiency( double driftRadius, int BEC = 0 );
+  static double correctionHT( double momentum, Trk::ParticleHypothesis hypothesis );
   double particleMass( int i ) const;
 
   // Tools and Services
   ToolHandle< ITRT_DriftFunctionTool > m_trtDriftFunctionTool;
-  ServiceHandle< PileUpMergeSvc > m_mergeSvc;                           // PileUp Merge service
-  ServiceHandle < IAtRndmGenSvc > m_atRndmGenSvc;                       // Random number service
+  bool m_useTrtElectronPidTool;                                           // false: use Tina's parametrization
+  ToolHandle< Trk::ITRT_ElectronPidTool > m_trtElectronPidTool;
+  ServiceHandle< ITRT_StrawStatusSummarySvc > m_trtStrawStatusSummarySvc; // Argon / Xenon
+  ServiceHandle< PileUpMergeSvc > m_mergeSvc;                             // PileUp Merge service
+  ServiceHandle< IAtRndmGenSvc > m_atRndmGenSvc;                          // Random number service
   CLHEP::HepRandomEngine *m_randomEngine;
-  std::string m_randomEngineName;                                       // Name of the random number stream
-  //ServiceHandle< IPartPropSvc > m_partPropSvc;
-  //HepPDT::ParticleDataTable *m_particleDataTable;
+  std::string m_randomEngineName;                                         // Name of the random number stream
 
-  // Containers
-  std::string m_trtHitCollectionKey;                                    // INPUT:  TRT_HitCollection
-  std::list<TRTUncompressedHitCollection*> m_trtHitCollList;
-  TimedHitCollection<TRTUncompressedHit> *m_thpctrt;
-  std::string m_trtDriftCircleKey;                                      // OUTPUT: InDet::TRT_DriftCircleContainer
-  InDet::TRT_DriftCircleContainer *m_trtDriftCircleContainer;
-  std::string m_prdMultiTruthKey;                                       // OUTPUT: Trk::PRD_MultiTruthCollection
-  PRD_MultiTruthCollection *m_prdMultiTruthCollection;
+  // INPUT
+  std::string m_trtHitCollectionKey;
+  std::list< TRTUncompressedHitCollection * > m_trtHitCollList;
 
+  // OUTPUT 
+  SG::WriteHandle< InDet::TRT_DriftCircleContainer > m_trtDriftCircleContainer;
+  SG::WriteHandle< PRD_MultiTruthCollection > m_trtPrdTruth;
+
+  TimedHitCollection< TRTUncompressedHit > *m_thpctrt;
   std::multimap< Identifier, InDet::TRT_DriftCircle * > m_driftCircleMap;
 
   // Helpers
   const InDetDD::TRT_DetectorManager *m_trt_manager;
-  const TRT_ID *m_trt_id;                                               // TRT Id Helper
-
-  // Constants
-  bool m_trtTRsimulation;
-  double m_trtTailFraction;              // part of the fraction in Tails
-  double m_trtSigmaDriftRadius;          // sigma of one TRT straw in R
-  double m_trtSigmaDriftRadiusTail;      // sigma of one TRT straw in R (Tail)
-  double m_trtFitAmplitude;              // Fit parameter for TRT efficiency calculation
-  double m_trtFitMu;                     // Fit parameter for TRT efficiency calculation
-  double m_trtFitR;                      // Fit parameter for TRT efficiency calculation
-  double m_trtFitSigma;                  // Fit parameter for TRT efficiency calculation
-  double m_trtFitConstant;
-  double m_trtTRprobParC1;               // defined, but not used
-  double m_trtTRprobParC2;
-  double m_trtTRprobParC3;
-  double m_trtTRprobParC4;
+  const TRT_ID *m_trt_id;                                                 // TRT Id Helper
 
   // Split configuration
-  int    m_HardScatterSplittingMode; /**< Process all TRT_Hits or just those from signal or background events */
-  bool   m_HardScatterSplittingSkipper;
-  IntegerProperty  m_vetoThisBarcode;
+  int m_HardScatterSplittingMode;                                         // Process all TRT_Hits or just those from signal or background events
+  bool m_HardScatterSplittingSkipper;
+  IntegerProperty m_vetoThisBarcode;
 };
 
-#endif //FASTTRT_DIGITIZATION_FASTTRT_DIGITIZATIONTOOL_H
+#endif // FASTTRT_DIGITIZATION_FASTTRT_DIGITIZATIONTOOL_H
