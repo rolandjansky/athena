e6bcf5ff9a58980570fcc6f2b980ab9895e7e832
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:44:43 +0200

    'CMakeLists.txt' (G4StepLimitation-01-00-01)
    	* src/G4StepLimitationTool.cxx (initialize, ConstructProcess):
    	change message levels to reduce veborsity in standard log files.
    	* Tagging: G4StepLimitation-01-00-01
    
    2015-10-21  Edoardo Farina <edoardo.maria.farina@cern.ch>
    	* Migration to new infrastructure. ATLASSIM-1964
    	* Tagging: G4StepLimitation-01-00-00

---
+++ Simulation/G4Utilities/G4StepLimitation/python/G4StepLimitationConfigDb.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addTool
+addTool("G4StepLimitation.G4StepLimitationConfig.getG4StepLimitationTool","G4StepLimitationTool")
---
+++ Simulation/G4Utilities/G4StepLimitation/src/G4StepLimitationTool.cxx
@@ -0,0 +1,80 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// class header
+#include "G4StepLimitationTool.h"
+
+// G4 headers
+#include "G4StepLimiter.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ProcessManager.hh"
+#include "G4Version.hh"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : G4StepLimitationTool
+//
+// 03-08-2015 : Edoardo Farina
+//-----------------------------------------------------------------------------
+
+#if G4VERSION_NUMBER > 1009
+#define PARTICLEITERATOR aParticleIterator
+#else
+#define PARTICLEITERATOR theParticleIterator
+#endif
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+G4StepLimitationTool::G4StepLimitationTool( const std::string& type,
+                                            const std::string& nam,const IInterface* parent )
+  : AthAlgTool ( type, nam , parent )
+{
+  ATH_MSG_DEBUG("G4StepLimitationTool "<<type<<" "<<nam);
+  declareInterface< IPhysicsOptionTool >( this ) ;
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+
+G4StepLimitationTool::~G4StepLimitationTool()
+{
+}
+
+//=============================================================================
+// Initialize
+//=============================================================================
+StatusCode G4StepLimitationTool::initialize( )
+{
+  ATH_MSG_VERBOSE("G4StepLimitationTool initialize(  )");
+
+  this->SetPhysicsName(name());
+  return StatusCode::SUCCESS;
+}
+
+
+G4VPhysicsConstructor* G4StepLimitationTool::GetPhysicsOption()
+{
+  return this;
+}
+
+void G4StepLimitationTool::ConstructParticle()
+{
+
+
+
+}
+
+void G4StepLimitationTool::ConstructProcess()
+{
+
+  ATH_MSG_DEBUG(" ConstructProcess for G4StepLimiter being run");
+  PARTICLEITERATOR->reset();
+  while( (*PARTICLEITERATOR)() )
+    {
+      G4ParticleDefinition* particle = PARTICLEITERATOR->value();
+      G4ProcessManager* pmanager = particle->GetProcessManager();
+      pmanager->AddProcess(new G4StepLimiter,       -1,-1,3);
+    }
+}
