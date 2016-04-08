78747b2c3f0006c3ce91f3876a33b78bdfc0090f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:22:55 +0200

    'CMakeLists.txt' (iPatTrackFitter-02-03-00)

---
+++ Reconstruction/iPat/iPatTrackFitter/src/components/iPatTrackFitter_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "iPatTrackFitter/TrackFitter.h"
+
+DECLARE_TOOL_FACTORY( TrackFitter )
+DECLARE_FACTORY_ENTRIES( iPatTrackFitter )
+{
+    DECLARE_TOOL( TrackFitter )	
+}
