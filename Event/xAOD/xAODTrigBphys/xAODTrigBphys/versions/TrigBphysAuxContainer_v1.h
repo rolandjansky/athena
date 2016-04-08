81d99fabc17906791096aaec23997d94095e08ac
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:04:09 +0200

    'CMakeLists.txt' (xAODTrigBphys-00-00-16)
    	* ATR-12263, addtional variables added, lxy, errors etc.
    	* xAODTrigBphys-00-00-16
    
    2015-09-21 James Walder <James.Walder@cern.ch>
    	* Addition of pT and further particleType enums
    	* ATR-12263
    	* Some problems in local tests, so submitting into devval
    	* Tagging as xAODTrigBphys-00-00-15
    
    2015-02-11 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Disabled the generation of some more dictionaries when
    	  compiling the package with ROOT 6. To get rid of some
    	  runtime warnings in AnalysisBase-2.1.X.
    	* Tagging as xAODTrigBphys-00-00-14

--- Event/xAOD/xAODTrigBphys/xAODTrigBphys/versions/TrigBphysAuxContainer_v1.h
+++ Event/xAOD/xAODTrigBphys/xAODTrigBphys/versions/TrigBphysAuxContainer_v1.h
@@ -4,7 +4,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: TrigBphysAuxContainer_v1.h 631623 2014-11-27 17:13:40Z gwatts $
+// $Id: TrigBphysAuxContainer_v1.h 696633 2015-09-24 14:59:50Z jwalder $
 #ifndef XAODTRIGBPHYS_VERSIONS_TRIGBPHYSAUXCONTAINER_V1_H
 #define XAODTRIGBPHYS_VERSIONS_TRIGBPHYSAUXCONTAINER_V1_H
 
@@ -30,8 +30,8 @@ namespace xAOD {
     ///
     /// @author James Walder <jwalder@cern.ch>
     ///
-    /// $Revision: 631623 $
-    /// $Date: 2014-11-27 18:13:40 +0100 (Thu, 27 Nov 2014) $
+    /// $Revision: 696633 $
+    /// $Date: 2015-09-24 16:59:50 +0200 (Thu, 24 Sep 2015) $
 
     class TrigBphysAuxContainer_v1 : public AuxContainerBase {
         
@@ -46,6 +46,7 @@ namespace xAOD {
 
         std::vector<float> eta;
         std::vector<float> phi;
+        std::vector<float> pt;
         std::vector<float> mass;
         std::vector<float> fitmass;
         std::vector<float> fitchi2;
@@ -54,6 +55,13 @@ namespace xAOD {
         std::vector<float> fity;
         std::vector<float> fitz;
 
+        std::vector<float> lxy;
+        std::vector<float> lxyError;
+        std::vector<float> tau;
+        std::vector<float> tauError;
+        std::vector<float> fitmassError;
+
+        
         std::vector<ElementLink< xAOD::TrigBphysContainer_v1 > > secondaryDecayLink;
         std::vector<std::vector<ElementLink<xAOD::TrackParticleContainer> > > trackParticleLinks;
 
--- Event/xAOD/xAODTrigBphys/xAODTrigBphys/versions/TrigBphys_v1.h
+++ Event/xAOD/xAODTrigBphys/xAODTrigBphys/versions/TrigBphys_v1.h
@@ -4,7 +4,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: TrigBphys_v1.h 630850 2014-11-25 11:11:05Z jwalder $
+// $Id: TrigBphys_v1.h 696633 2015-09-24 14:59:50Z jwalder $
 #ifndef XAODTRIGBPHYS_VERSIONS_TRIGBPHYS_V1_H
 #define XAODTRIGBPHYS_VERSIONS_TRIGBPHYS_V1_H
 
@@ -38,8 +38,8 @@ namespace xAOD {
     /// @author James Walder <jwalder@cern.ch>
     /// Based on the previous TrigL2Bphys and TrigEFBphys classes
     ///
-    /// $Revision: 630850 $
-    /// $Date: 2014-11-25 12:11:05 +0100 (Tue, 25 Nov 2014) $
+    /// $Revision: 696633 $
+    /// $Date: 2015-09-24 16:59:50 +0200 (Thu, 24 Sep 2015) $
     ///
     class TrigBphys_v1 : public SG::AuxElement {
 
@@ -58,6 +58,17 @@ namespace xAOD {
            BSPHIMUMU = 9,
            LBLMUMU   = 10,
            BCDSMUMU  = 11,
+            // added JW: 21/09/15 for 2016 edm running
+            LAMBDAPIP = 12, // Lambda^0 -> pi- p
+            BCDPMUMU  = 13, // B_c -> D+ mu mu
+            BCDSTMUMU = 14, // B_c -> D*+ mu mu
+            OMEGALAMK = 15, // Omega- -> Lambda^0 K-
+            XILAMPI   = 16, // Xi- -> Lambda^0 pi-
+            DSTDZPI   = 17, // D*+ -> D0 pi+
+            DZMUKX    = 18, // D0 -> mu+ K- X
+            DZKPI     = 19, // D0 -> K- pi+
+            DPKPIPI   = 20, // D+ -> K- pi+ pi+
+
            UNKNOWNPTYPE = 9999
         };
 
@@ -80,10 +91,17 @@ namespace xAOD {
         void initialise();
 
         //** initialise with given parameters */
+        void initialise(uint32_t roi, float eta, float phi, float pt,
+                        pType particleType,
+                        float mass, levelType level);
         void initialise(uint32_t roi, float eta, float phi, pType particleType,
                         float mass, levelType level);
         /** constructor replacement for particle of type "particleType" with mass "mass" 
          and a secondary decay (given as ElementLink to another TrigEFBphys) */
+        void initialise(uint32_t roi, float eta, float phi, float pt,
+                        pType particleType,
+                        float mass, const ElementLink<xAOD::TrigBphysContainer_v1>& sDecay,
+                        levelType level);
         void initialise(uint32_t roi, float eta, float phi, pType particleType,
                         float mass, const ElementLink<xAOD::TrigBphysContainer_v1>& sDecay,
                         levelType level);
@@ -101,6 +119,8 @@ namespace xAOD {
         float eta()      const;
         /** accessor method: phi */
         float phi()      const;
+        /** accessor method: pt */
+        float pt()      const;
         /** accessor method: mass */
         float mass()      const;
         /** accessor method: mass from vertex fit */
@@ -116,6 +136,17 @@ namespace xAOD {
         /** accessor method: z position of vertex */
         float fitz()  const;
 
+        /** accessor method: lxy  */
+        float lxy     () const;
+        /** accessor method: lxy uncertainty */
+        float lxyError() const;
+        /** accessor method: tau  */
+        float tau     () const;
+        /** accessor method: tau uncertainty */
+        float tauError() const;
+        /** accessor method: fitmass uncertainty */
+        float fitmassError() const;
+        
         /** accessor method: secondary decay particle */
         const TrigBphys_v1* secondaryDecay() const;
         /** accessor method: secondary decay particle */
@@ -138,6 +169,8 @@ namespace xAOD {
         void setPhi (float);
         /** Set the mass of the object */
         void setMass(float);
+        /** Set the pT of the object */
+        void setPt(float);
 
         /** set method: roiId **/
         void setRoiId(uint32_t id);
@@ -161,6 +194,17 @@ namespace xAOD {
         void addTrackParticleLink(const ElementLink<xAOD::TrackParticleContainer>& track);
         /** set method: link to secondary decay particle  **/
         void setSecondaryDecayLink(const ElementLink<xAOD::TrigBphysContainer_v1> & link);
+
+        /** set method: lxy  **/
+        void setLxy     (float v);
+        /** set method: lxyError  **/
+        void setLxyError(float v);
+        /** set method: tau  **/
+        void setTau     (float v);
+        /** set method: tauError  **/
+        void setTauError(float v);
+        /** set method: fitmassError  **/
+        void setFitmassError(float v);
 
         
         /** accessor method:vector of tracks used to make particle */
