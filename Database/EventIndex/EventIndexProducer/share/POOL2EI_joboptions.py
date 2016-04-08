e8116b22e49d217b56d5e3bcb67a8cef03461dc0
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:39:11 +0200

    'CMakeLists.txt' (EventIndexProducer-00-02-11)
    	* python/POOL2EI_Lib.py: correct extraction of GUID for current file
    	                         get TriggerDecision from xAOD::TrigDecision when available
    	                         add EndEvtLoop incident handler to correctly save number of events for
    	                             current file in EI when maxEvents limit is reached
    	* scripts/sendEI.py: add JMSDeliveryMode to messages to make them PERSISTENT
    	                     properly close message groups using JMSXGroupSeq=-1
    	                     allow sending specific GUIDs from EventIndex file
    	                     compute number of unique events in terms of run-evt key per GUID
    	                     modify EISTATS messages to include:
    	                            number of unique events, producerID and seqID
    	                            EI creation original timestamps
    	                            GUID sending order and total number of GUIDs sent
    	* skeleton.POOLtoEI_tf.py: Change default value for SendToBroker to False
    	* POOL2EI_joboptions.py: Change default value for SendToBroker to False
    	                         get TriggerDecision from xAOD::TrigDecision when available
    	* Tag EventIndexProducer-00-02-11

--- Database/EventIndex/EventIndexProducer/share/POOL2EI_joboptions.py
+++ Database/EventIndex/EventIndexProducer/share/POOL2EI_joboptions.py
@@ -33,87 +33,88 @@ theApp.CreateSvc += [pool2eisvc.getFullJobOptName()]
 # (1=VERBOSE, 2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL )
 #--------------------------------------------------------------
 try:
-  svcMgr.MessageSvc.OutputLevel = Level
+    svcMgr.MessageSvc.OutputLevel = Level
 except:
-  svcMgr.MessageSvc.OutputLevel = INFO
+    svcMgr.MessageSvc.OutputLevel = INFO
 
 
 #--------------------------------------------------------------
 # Number of events to write
 #--------------------------------------------------------------
 try:
-  theApp.EvtMax = EvtMax
+    theApp.EvtMax = EvtMax
 except:
-  theApp.EvtMax = -1
+    theApp.EvtMax = -1
 
 
 #--------------------------------------------------------------
 # Input collection name
 #--------------------------------------------------------------
 try:
-  svcMgr.EventSelector.InputCollections  = In 
+    svcMgr.EventSelector.InputCollections  = In 
 except:
-  svcMgr.EventSelector.InputCollections  = [ "test.root" ]
+    svcMgr.EventSelector.InputCollections  = [ "test.root" ]
+
 
 #--------------------------------------------------------------
 # Output Event Index file name
 #--------------------------------------------------------------
 try:
-  job.pool2ei.Out = Out
+    job.pool2ei.Out = Out
 except:
-  import os
-  job.pool2ei.Out = 'pool2ei.{:08i}.pkl'.format(os.getpid())
+    import os
+    job.pool2ei.Out = 'pool2ei.{:08i}.pkl'.format(os.getpid())
 
 
 #--------------------------------------------------------------
 # Output Provenance references flag
 #--------------------------------------------------------------
 try: 
-  job.pool2ei.DoProvenanceRef = DoProvenanceRef
+    job.pool2ei.DoProvenanceRef = DoProvenanceRef
 except:
-  job.pool2ei.DoProvenanceRef = False
+    job.pool2ei.DoProvenanceRef = False
 
 #--------------------------------------------------------------
 # Output Trigger Info flag
 #--------------------------------------------------------------
 try: 
-  job.pool2ei.DoTriggerInfo = DoTriggerInfo
+    job.pool2ei.DoTriggerInfo = DoTriggerInfo
 except:
-  job.pool2ei.DoTriggerInfo = True
+    job.pool2ei.DoTriggerInfo = True
 
 #--------------------------------------------------------------
 # Send to Broker flag
 #--------------------------------------------------------------
 try: 
-  job.pool2ei.SendToBroker = SendToBroker
+    job.pool2ei.SendToBroker = SendToBroker
 except:
-  job.pool2ei.SendToBroker = True
+    job.pool2ei.SendToBroker = False
 
 #--------------------------------------------------------------
 # Input dataset name. Overrrides value read for job options
 #--------------------------------------------------------------
 try: 
-  job.pool2ei.EiDsName = EiDsName
+    job.pool2ei.EiDsName = EiDsName
 except:
-  job.pool2ei.EiDsName = None
+    job.pool2ei.EiDsName = None
 
 #--------------------------------------------------------------
 # Tier0 job parameters
 #--------------------------------------------------------------
 try:
-  job.pool2ei.TaskID = TaskID
+    job.pool2ei.TaskID = TaskID
 except:
-  job.pool2ei.TaskID = None
+    job.pool2ei.TaskID = None
   
 try:
-  job.pool2ei.JobID = JobID
+    job.pool2ei.JobID = JobID
 except:
-  job.pool2ei.JobID = None
+    job.pool2ei.JobID = None
 
 try:
-  job.pool2ei.AttemptNumber = AttemptNumber
+    job.pool2ei.AttemptNumber = AttemptNumber
 except:
-  job.pool2ei.AttemptNumber = None
+    job.pool2ei.AttemptNumber = None
 
 
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
@@ -124,6 +125,7 @@ athenaCommonFlags.PoolAODInput = In
 rec.readRDO=False
 rec.readESD=False
 rec.readAOD=True
+rec.readTAG=False
 rec.doWriteAOD=False
 rec.doWriteESD=False
 rec.doWriteTAG=False
@@ -136,30 +138,57 @@ globalflags.DataSource = 'data' if inputFileSummary['evt_type'][0] == "IS_DATA" 
 
 # set projectName from inputFileSummary
 try:
-  # in first place try to get it from tag_info
-  rec.projectName = inputFileSummary['tag_info']['project_name']
+    # in first place try to get it from tag_info
+    rec.projectName = inputFileSummary['tag_info']['project_name']
 except:
-  try: 
-    # in last place from metadata
-    rec.projectName = inputFileSummary['metadata']['/TagInfo']['project_name']
-  except:
-    pass
+    try: 
+        # in last place from metadata
+        rec.projectName = inputFileSummary['metadata']['/TagInfo']['project_name']
+    except:
+        pass
 
 if inputFileSummary.has_key('metadata') and inputFileSummary['metadata'].has_key('/TRIGGER/HLT/HltConfigKeys'):
-  from AthenaCommon.AppMgr import ToolSvc
-  from TrigDecisionTool.TrigDecisionToolConf import Trig__TrigDecisionTool
-  tdt = Trig__TrigDecisionTool("TrigDecisionTool")
-  ToolSvc += tdt
 
-  # To read files with trigger config stored as in-file meta-data,
-  from TriggerJobOpts.TriggerFlags import TriggerFlags
-  TriggerFlags.configurationSourceList = ['ds']
-  TriggerFlags.configForStartup = 'HLToffline'
-
-  from TriggerJobOpts.TriggerConfigGetter import TriggerConfigGetter
-  cfg = TriggerConfigGetter("ReadPool")
+    # we should search for 'xAOD::TrigDecision' in eventdata_items but 'zero events' files do not 
+    # contain eventdata_items, so we look for 'xAOD::TriggerMenuContainer' in metadata_items instead
+    if inputFileSummary.has_key('metadata_items') and 'xAOD::TriggerMenuContainer' \
+            in [ x[0] for x in inputFileSummary['metadata_items'] ]:
+        job.pool2ei.HaveXHlt = True
+    else:
+        job.pool2ei.HaveXHlt = False
+        
+    from TriggerJobOpts.TriggerConfigGetter import TriggerConfigGetter
+    cfg = TriggerConfigGetter("ReadPool")
   
-  job.pool2ei.HaveHlt = True
+    from AthenaCommon.AppMgr import ToolSvc
+    from TrigDecisionTool.TrigDecisionToolConf import Trig__TrigDecisionTool
+    tdt = Trig__TrigDecisionTool("TrigDecisionTool")
+    if job.pool2ei.HaveXHlt:
+        tdt.TrigDecisionKey = 'xTrigDecision'
+        tdt.UseAODDecision = False
+    else:
+        # use old decision
+        tdt.TrigDecisionKey = 'TrigDecision'
+        tdt.UseAODDecision = True
+    ToolSvc += tdt
+
+    if job.pool2ei.HaveXHlt:
+        from TrigEDMConfig.TriggerEDM import EDMLibraries
+        ToolSvc.TrigDecisionTool.Navigation.Dlls = [e for e in EDMLibraries if 'TPCnv' not in e]
+    
+        from TriggerJobOpts.Lvl1ResultBuilderGetter import Lvl1ResultBuilderGetter
+        hltouput = Lvl1ResultBuilderGetter()
+    
+        from TriggerJobOpts.HLTTriggerResultGetter import HLTTriggerResultGetter
+        hltouput = HLTTriggerResultGetter()
+    
+    # To read files with trigger config stored as in-file meta-data,
+    from TriggerJobOpts.TriggerFlags import TriggerFlags
+    TriggerFlags.configurationSourceList = ['ds']
+    TriggerFlags.configForStartup = 'HLToffline'
+
+    job.pool2ei.HaveHlt = True
 else:
-  job.pool2ei.HaveHlt = False
+    job.pool2ei.HaveHlt = False
+    job.pool2ei.HaveXHlt = False
 
