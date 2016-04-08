02526986243e48693b1f485c6780fce85c645f08
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:36:21 +0200

    'CMakeLists.txt' (ForwardRegion_SimEv-00-00-07)

--- ForwardDetectors/ForwardSimulation/ForwardRegion_SimEv/src/SimulationHit.cxx
+++ ForwardDetectors/ForwardSimulation/ForwardRegion_SimEv/src/SimulationHit.cxx
@@ -4,10 +4,10 @@
 
 #include "ForwardRegion_SimEv/SimulationHit.h"
 
-SimulationHit::StepPoint::StepPoint() { }        
+SimulationHit::StepPoint::StepPoint() : time(0), kinetic_energy(0) { }
 SimulationHit::StepPoint::StepPoint(double time,  const CLHEP::Hep3Vector& position, const double kinetic_energy,  const CLHEP::Hep3Vector& direction,  const CLHEP::Hep3Vector& momentum) : time(time), position(position), kinetic_energy(kinetic_energy), direction(direction), momentum(momentum) { }
 
-SimulationHit::SimulationHit( ) { }
+SimulationHit::SimulationHit( ) :  m_deposit_energy(0), m_pdgcode(0), m_track_id(0), m_parent_id(0) { }
 SimulationHit::SimulationHit(double pre_time, const CLHEP::Hep3Vector& pre_position, const double pre_kinetic_energy, const CLHEP::Hep3Vector& pre_direction, const CLHEP::Hep3Vector& pre_momentum,
                   double post_time, const CLHEP::Hep3Vector& post_position, const double post_kinetic_energy, const CLHEP::Hep3Vector& post_direction, const CLHEP::Hep3Vector& post_momentum,
                    const double deposit_energy, const int pdgcode, const int track_id, const int parent_id) 
