17c7e83f0c820027d8719809102c59969aac6d7a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 15:51:40 +0200

    'CMakeLists.txt' (QcdD3PDMaker-00-08-59)
    	* Tagging QcdD3PDMaker-00-08-59.
    	* Remove EventEtDensityFillerTool.  The underlying tool on which
    	it depends has gone away.
    
    
    Former-commit-id: 98730a82cd1ed915b327fbdb715fd64a4f1f1aea

--- PhysicsAnalysis/D3PDMaker/QcdD3PDMaker/src/components/QcdD3PDMaker_entries.cxx
+++ PhysicsAnalysis/D3PDMaker/QcdD3PDMaker/src/components/QcdD3PDMaker_entries.cxx
@@ -1,14 +1,11 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "QcdD3PDMaker/preprocessing/QcdD3PDPreProcessing.h"
 #include "QcdD3PDMaker/fillers/MuonSpShowerFillerTool.h"
-#include "QcdD3PDMaker/fillers/EventEtDensityFillerTool.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY (QcdD3PD, QcdD3PDPreProcessing)
 DECLARE_NAMESPACE_TOOL_FACTORY      (QcdD3PD, MuonSpShowerFillerTool)
-DECLARE_NAMESPACE_TOOL_FACTORY      (QcdD3PD, EventEtDensityFillerTool)
 
 DECLARE_FACTORY_ENTRIES(QcdD3PDMaker) {
   DECLARE_NAMESPACE_ALGORITHM (QcdD3PD, QcdD3PDPreProcessing)
   DECLARE_NAMESPACE_TOOL(QcdD3PD, MuonSpShowerFillerTool) 
-  DECLARE_NAMESPACE_TOOL(QcdD3PD, EventEtDensityFillerTool) 
 }
