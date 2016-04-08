d9230519b44d782c952c2c56d7a4213515ac8190
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:16:56 +0200

    'CMakeLists.txt' (InDetBeamSpotService-01-00-04)
    	* share/BeamCondSvc.py: protection against importing RecExConfig
    	in AthSimulationBase.
    	* InDetBeamSpotService-01-00-04

--- InnerDetector/InDetConditions/InDetBeamSpotService/share/BeamCondSvc.py
+++ InnerDetector/InDetConditions/InDetBeamSpotService/share/BeamCondSvc.py
@@ -2,9 +2,14 @@
 
 include.block("InDetBeamSpotService/BeamCondSvc.py")
 from IOVDbSvc.CondDB import conddb
-from RecExConfig.RecFlags import rec
-# If express processing, point beam spot to online folder results
-if(rec.doExpressProcessing()):
-    conddb.addFolder('INDET_ONL','/Indet/Onl/Beampos <key>/Indet/Beampos</key>')
-else:
+try:
+    from RecExConfig.RecFlags import rec
+    # If express processing, point beam spot to online folder results
+    if(rec.doExpressProcessing()):
+        conddb.addFolder('INDET_ONL','/Indet/Onl/Beampos <key>/Indet/Beampos</key>')
+    else:
+        conddb.addFolderSplitOnline("INDET","/Indet/Onl/Beampos","/Indet/Beampos")
+except ImportError:
+    # Protection for AthSimulationBase release which does not contain RecExConfig
     conddb.addFolderSplitOnline("INDET","/Indet/Onl/Beampos","/Indet/Beampos")
+
