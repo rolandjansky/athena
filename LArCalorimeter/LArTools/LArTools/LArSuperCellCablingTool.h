29a1b83b02ce2429f61f56c9292608a9d741dd25
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:22:23 +0200

    'CMakeLists.txt' (LArTools-00-08-18)
    	* Tagging LArTools-00-08-18.
    	* Moved cabling tools to new package LArCabling, to clean up
    	dependencies for the simulation release.
    
    20-10-2015 Pavol Strizenec <pavol @ mail.cern.ch>
    
    	* changing the defaults for HVmap from datafile
    	* not tagging

--- LArCalorimeter/LArTools/LArTools/LArSuperCellCablingTool.h
+++ LArCalorimeter/LArTools/LArTools/LArSuperCellCablingTool.h
@@ -7,36 +7,6 @@
 #ifndef LARSUPERCELLCABLINGTOOL
 #define LARSUPERCELLCABLINGTOOL
 
-#include "LArTools/LArCablingService.h"
-
-
-/** 
-   @class LArSuperCellCablingTool 
-   @brief Tool for mapping online and offline identifiers, <br>
-   @author Walter Lampl
-*/
-
-
-static const InterfaceID IID_LArSuperCellCablingTool("LArSuperCellCablingTool", 1 , 0); 
-
-
-class LArSuperCellCablingTool : public LArCablingBase  {
-
-public:
-   /** constructor */
-  LArSuperCellCablingTool( const std::string& type, const std::string& name, const IInterface* parent ) ;
-
-  ~LArSuperCellCablingTool();
-  
-  StatusCode initialize( ); 
-
-  StatusCode iovCallBack(IOVSVC_CALLBACK_ARGS_K(keys));
-
-  // Retrieve interface ID
-  static const InterfaceID& interfaceID() { return IID_LArSuperCellCablingTool; }
-
-
-  //All other methods inherited from regular LArCablingBase
-};
+#include "LArCabling/LArSuperCellCablingTool.h"
 
 #endif
