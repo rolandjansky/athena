dbc2415149e6e8706604fa56624ca22912633581
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:56:27 +0200

    'CMakeLists.txt' (RDO_OverlayBase-00-04-01)

---
+++ Event/EventOverlay/RDO_OverlayBase/RDO_OverlayBase/RDO_OverlayBase.h
@@ -0,0 +1,5 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#error "Error: obsolete include file.  The RDO_OverlayBase package has been renamed into IDC_OverlayBase."
