ae7a12ac6ffed1d61f8489ff47ea999c82a003ca
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:32:51 +0200

    'CMakeLists.txt' (TruthUtils-03-02-01)
    2015-10-01  John Chapman  <John.Chapman@cern.ch>
    	* Tagging: TruthUtils-03-02-01
    
    	* TruthUtils/HepMCHelpers.h - remove definition of
    	SIM_BARCODE_THRESHOLD, as now defined in
    	TruthUtils/TruthParticleHelpers.h. ATLASSIM-2154
    
    2015-09-30  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging: TruthUtils-03-02-00
    
    	* add TruthUtils/TruthParticleHelpers.h - for extra ATLAS-specific
    	particle classifier functions, starting with isGenStable with
    	status and barcode integers as arguments. obo Teng Jian
    	Khoo. ATLASSIM-2154
    
    	* TruthUtils/HepMCHelpers.h - change isGenStable function to
    	return the version from TruthParticleHelpers.h. obo Teng Jian
    	Khoo. ATLASSIM-2154
    
    ...
    (Long ChangeLog diff - truncated)

---
+++ Generators/TruthUtils/TruthUtils/PIDHelpers.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#pragma once
+
+/// @file
+///
+/// PID-only functions with no HepMC dependence, both from MCUtils and ATLAS-specific.
+
+#include "MCUtils/PIDUtils.h"
+
+namespace MC {
+
+  // Use the MCUtils and HEPUtils functions as if they were defined in the ATLAS MC and MC::PID namespaces
+  using namespace MCUtils;
+  using namespace HEPUtils;
+
+
+  /// @brief Identify if the particle with given PDG ID would not interact with the detector, i.e. not a neutrino or WIMP
+  inline bool isNonInteracting(int pid) {
+    return !(PID::isStrongInteracting(pid) || PID::isEMInteracting(pid));
+  }
+
+
+  /// @brief Identify if the particle with given PDG ID would produce ID tracks but not shower in the detector if stable
+  inline bool isChargedNonShowering(int pid) {
+    if (PID::isMuon(pid)) return true;
+    if (PID::isSUSY(pid)) return true; //(meta)stable charginos, R-hadrons etc
+     return false;
+   }
+
+}
---
+++ Generators/TruthUtils/TruthUtils/TruthParticleHelpers.h
@@ -0,0 +1,43 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#pragma once
+
+/// @file
+///
+/// Provides the HepMC tools from the external MCUtils header package,
+/// ATLAS-specific HepMC functions not suitable for MCUtils.
+
+#include "TruthUtils/PIDHelpers.h"
+
+namespace MC {
+
+  // Use the MCUtils and HEPUtils functions as if they were defined in the ATLAS MC and MC::PID namespaces
+  using namespace MCUtils;
+  using namespace HEPUtils;
+
+
+  /// @brief Constant defining the barcode threshold distinguishing generator record entries from detector sim ones
+  /// @todo The sim barcodes start at 1M in MC15, so we should update the 200k threshold,
+  ///   but >= 200k is still a valid test for b = 1M so let's keep it this way until MC12 is long-dead.
+  const int SIM_BARCODE_THRESHOLD = 200000;
+
+
+  /// @name Extra ATLAS-specific particle classifier functions
+  //@{
+
+  /// @brief Determine if the particle is stable at the generator (not det-sim) level,
+  ///
+  /// The receipe for this is barcode < 200k and status = 1. Gen-stable particles decayed by
+  /// G4 are not set to have status = 2 in ATLAS, but simply have more status = 1 children,
+  /// with barcodes > 200k.
+  inline bool isGenStable(int status, int barcode) {
+    if (status != 1) return false;
+    return barcode < SIM_BARCODE_THRESHOLD;
+  }
+
+  //@}
+
+
+}
