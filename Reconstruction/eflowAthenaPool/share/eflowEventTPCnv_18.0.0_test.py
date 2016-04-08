662fce53867775a9f14497c640eeca617f5d614a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:24:07 +0200

    'CMakeLists.txt' (eflowAthenaPool-00-00-11)
    	* Tagging eflowAthenaPool-00-00-11.
    	* Add regression test reading old data files.
    
    25 February 2016 Peter van Gemmeren
    Cleanup #include statements
    Tag as eflowAthenaPool-00-00-10

---
+++ Reconstruction/eflowAthenaPool/share/eflowEventTPCnv_18.0.0_test.py
@@ -0,0 +1,10 @@
+infile = 'aod/AOD-18.0.0/AOD-18.0.0-full.pool.root'
+keys = [
+    #eflowObjectContainer_p5
+    'eflowObjects_tauMode',
+         ]
+
+noMuon = True
+noID = True
+include ('AthenaPoolUtilities/TPCnvTest.py')
+
