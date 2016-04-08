eac82d84350b11c660167a500e8947ed56e09f6d
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:28:16 +0200

    'CMakeLists.txt' (TrigCombinedEventTPCnv-00-00-13-03)
    	* Tagging TrigCombinedEventTPCnv-00-00-13-03.
    	* Fix checkreq warnings.
    	* Tagging TrigCombinedEventTPCnv-00-00-13-02.
    	* Remove #define private.
    
    	* Tagging TrigCombinedEventTPCnv-00-00-13-01.
    	* Add unit test.

--- Trigger/TrigEvent/TrigCombinedEventTPCnv/src/TrigCompositeCnv_p1.cxx
+++ Trigger/TrigEvent/TrigCombinedEventTPCnv/src/TrigCompositeCnv_p1.cxx
@@ -2,10 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#define private public
 #include "TrigCombinedEvent/TrigComposite.h"
-#undef private
-
 #include "TrigCombinedEventTPCnv/TrigComposite_p1.h"
 #include "TrigCombinedEventTPCnv/TrigCompositeCnv_p1.h"
 //#include "AthenaKernel/errorcheck.h"
@@ -19,75 +16,67 @@ void TrigCompositeCnv_p1::transToPers(const TrigComposite* trans,
   
   if ( (!trans) || (!pers) ) {
     log << MSG::ERROR << "TrigCompositeCnv_p1::transToPers with trans or pers == 0" << endreq;
-  }else{
+  }
+  else{
 
-  // everywhere blow the vector::reserve() can be used since we know the sizes in advance, 
-  // in fact, however, we expect this arrays to be of small size so it should not matter  
-  std::map< std::string, TrigFeatureLink >::const_iterator it_link;
-  for(it_link=trans->m_links.begin(); it_link!=trans->m_links.end(); it_link++){
+    // everywhere below the vector::reserve() can be used since we know the sizes in advance, 
+    // in fact, however, we expect this arrays to be of small size so it should not matter  
+    for (const std::pair<const std::string, TrigFeatureLink>& p :
+           trans->allDetails<TrigFeatureLink>())
+    {
+  	pers->m_labels.push_back( p.first );
+  	pers->m_clids.push_back( (uint32_t)p.second.clid() );
+  	pers->m_stypes.push_back( p.second.subTypeIndex() );
+  	pers->m_indexes.push_back( p.second.index() );
+    }
   
-  	pers->m_labels.push_back( it_link->first );
-  	pers->m_clids.push_back( (uint32_t)it_link->second.clid() );
-  	pers->m_stypes.push_back( it_link->second.subTypeIndex() );
-  	pers->m_indexes.push_back( it_link->second.index() );
-  }
-  
-  //  std::map<std::string, bool>::const_iterator it_set;
+    for (const std::pair<const std::string, float>& p :
+           trans->allDetails<float>())
+    {
+      pers->m_floats.push_back(p.second);
+      pers->m_fnames.push_back(p.first);
+    }
 
-  std::map<std::string, float>::const_iterator i;
-  for ( i=trans->m_floats.begin() ; i != trans->m_floats.end(); i++ ){
-    pers->m_floats.push_back(i->second);
-    pers->m_fnames.push_back(i->first);
-    //    it_set=(trans->m_set).find(i->first);
-    //std::cout<<"Camille: bool value: "<<i->first<<"  "<< (*it_set).second<<std::endl;
-    //pers->m_v_set.push_back(it_set->second);
-  }
+    for (const std::pair<const std::string, int>& p :
+           trans->allDetails<int>())
+    {
+      pers->m_ints.push_back(p.second);
+      pers->m_inames.push_back(p.first);
+    }
 
-  std::map<std::string, int>::const_iterator j;
-  for ( j=trans->m_ints.begin() ; j != trans->m_ints.end(); j++ ){
-    pers->m_ints.push_back(j->second);
-    pers->m_inames.push_back(j->first);
-    //    it_set=(trans->m_set).find(j->first);
-    //pers->m_v_set.push_back(it_set->second);
-    //std::cout<<": bool value: "<<j->first<<"  "<< (*it_set).second<<std::endl;
-  }
+    for (const std::pair<const std::string, std::string>& p :
+           trans->allDetails<std::string>())
+    {
+      pers->m_strings.push_back(p.second);
+      pers->m_snames.push_back(p.first);
+    }
 
-  std::map<std::string, std::string>::const_iterator k;
-  for ( k=trans->m_strings.begin() ; k != trans->m_strings.end(); k++ ){
-    pers->m_strings.push_back(k->second);
-    pers->m_snames.push_back(k->first);
-    //    it_set=(trans->m_set).find(k->first);
-    //pers->m_v_set.push_back((*it_set).second);
-  }
-  std::map<std::string, std::vector<float> >::const_iterator l;
-  for ( l=trans->m_v_floats.begin() ; l != trans->m_v_floats.end(); l++ ){
-    pers->m_v_floats.push_back(l->second);
-    pers->m_v_fnames.push_back(l->first);
-    //    it_set=(trans->m_set).find(l->first);
-    //    pers->m_v_set.push_back((*it_set).second);
-  }
+    for (const std::pair<const std::string, std::vector<float> >& p :
+           trans->allDetails<std::vector<float> >())
+    {
+      pers->m_v_floats.push_back(p.second);
+      pers->m_v_fnames.push_back(p.first);
+    }
 
-  std::map<std::string, std::vector<int> >::const_iterator m;
-  for ( m=trans->m_v_ints.begin() ; m != trans->m_v_ints.end(); m++ ){
-    pers->m_v_ints.push_back(m->second);
-    pers->m_v_inames.push_back(m->first);
-    //    it_set=(trans->m_set).find(m->first);
-    //    pers->m_v_set.push_back((*it_set).second);
-  }
+    for (const std::pair<const std::string, std::vector<int> >& p :
+           trans->allDetails<std::vector<int> >())
+    {
+      pers->m_v_ints.push_back(p.second);
+      pers->m_v_inames.push_back(p.first);
+    }
 
-  std::map<std::string, std::vector<std::string> >::const_iterator n;
-  for ( n=trans->m_v_strings.begin() ; n != trans->m_v_strings.end(); n++ ){
-    pers->m_v_strings.push_back(n->second);
-    pers->m_v_snames.push_back(n->first);
-    //    it_set=(trans->m_set).find(n->first);
-    //    pers->m_v_set.push_back((*it_set).second);
-  }
+    for (const std::pair<const std::string, std::vector<std::string> >& p :
+           trans->allDetails<std::vector<std::string> >())
+    {
+      pers->m_v_strings.push_back(p.second);
+      pers->m_v_snames.push_back(p.first);
+    }
 
-  pers->m_instance=trans->m_name;
-  //  pers->m_f_locked=trans->m_locked;
-  //  pers->m_mustset=trans->m_mustset;
+    pers->m_instance=trans->name();
   }
 }
+
+
 template<class T>
 void copyDetails2Trans(const std::vector<std::string>& keys, const std::vector<T>& values, TrigComposite* trans, MsgStream &log) {
   if (values.size() != keys.size() ) {
@@ -110,6 +99,8 @@ void TrigCompositeCnv_p1::persToTrans(const TrigComposite_p1* pers,
   if ( (!trans) || (!pers) ) {
     log << MSG::ERROR << "TrigCompositeCnv_p1::persToTrans with trans or pers == 0" << endreq;
   }else{
+    *trans = TrigComposite (pers->m_instance);
+
     //  std::vector<bool>::const_iterator it_set;
     //  it_set=(pers->m_v_set).begin();
     
@@ -135,7 +126,6 @@ void TrigCompositeCnv_p1::persToTrans(const TrigComposite_p1* pers,
       log << MSG::ERROR << "TrigCompositeCnv_p1::persToTrans: The number of TrigFeatureLink labels, CLIDs, types and indexes does not match, problem with the stored data" << endreq;  
     }	
     
-    trans->m_name=pers->m_instance;
     //  trans->m_locked=pers->m_f_locked;
     //  traans->m_mustset=pers->m_mustset;
     //trans->m_log.m_label=pers->m_instance;
