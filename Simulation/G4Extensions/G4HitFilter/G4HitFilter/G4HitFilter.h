e7f6f3da8e4065a2507d50da1fa6f79190d8d896
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:45:12 +0200

    'CMakeLists.txt' (G4HitFilter-00-00-05)
    	* tagging G4HitFilter-00-00-05
    	* added missing queryinterface
    
    2015-04-30 Andrea Di Simone <andrea.di.simone@cern.ch>
    	* tagging G4HitFilter-00-00-04
    	* moving the filter to the new UA infrastructure
    
    2015-01-07  Zach Marshall <ZLMarshall@lbl.gov>
    	* Cleaning up requirements file
    	* Tagged as G4HitFilter-00-00-03

--- Simulation/G4Extensions/G4HitFilter/G4HitFilter/G4HitFilter.h
+++ Simulation/G4Extensions/G4HitFilter/G4HitFilter/G4HitFilter.h
@@ -5,24 +5,25 @@
 #ifndef G4HITFILTER_H
 #define G4HITFILTER_H
 
-#include "FadsActions/UserAction.h"
-#include "StoreGate/StoreGateSvc.h"
+
+#include "G4AtlasTools/UserActionBase.h"
 
 #include <string>
 #include <vector>
 #include <map>
 
-class G4HitFilter: public FADS::UserAction {
+class G4HitFilter final: public UserActionBase {
+
  public:
-  G4HitFilter(std::string s);
-  void BeginOfEventAction(const G4Event*);
-  void EndOfEventAction(const G4Event*);
-  void BeginOfRunAction(const G4Run*);
-  void EndOfRunAction(const G4Run*);
-  void SteppingAction(const G4Step*);
+  G4HitFilter(const std::string& type, const std::string& name, const IInterface* parent);
+
+  virtual void EndOfEvent(const G4Event*) override;
+  virtual StatusCode initialize() override;
+  virtual StatusCode finalize() override;
+  
+  virtual StatusCode queryInterface(const InterfaceID&, void**) override;
 
 private:
-  void ParseProperties();
 
   enum hitCntainerTypes {
     CALOCALIB,
@@ -36,10 +37,11 @@ private:
     TILE,
     TRT };
 
-  StoreGateSvc* m_storeGate;
-  std::string m_collectionName;
+  // property on python side
+  std::vector<std::string> m_volumenames;
+
+  // internals
   int m_ntot,m_npass;
-  bool m_init;
   std::vector<std::pair<int,std::string> > m_hitContainers;
 };
 
---
+++ Simulation/G4Extensions/G4HitFilter/src/components/G4HitFilter_entries.cxx
@@ -0,0 +1,11 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+#include "G4HitFilter/G4HitFilter.h"
+
+DECLARE_TOOL_FACTORY( G4HitFilter )
+
+DECLARE_FACTORY_ENTRIES( G4HitFilter ) {
+
+  DECLARE_TOOL( G4HitFilter )
+
+}
