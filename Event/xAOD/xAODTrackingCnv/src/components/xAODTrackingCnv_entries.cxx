847a00926327ff0a4246d10f16757b77a0fddd2e
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:03:51 +0200

    'CMakeLists.txt' (xAODTrackingCnv-00-01-13)
    	* move conversion logic from TrackParticleCnvAlg to
    	* TrackCollectionCnvTool and RecTrackParticleContainerCnvTool
    	* https://its.cern.ch/jira/browse/ATR-9340
    	* Tagged as xAODTrackingCnv-00-01-13
    
    2015-04-14 Edward Moyse
            * Coverity: 14587
            * Tagged as xAODTrackingCnv-00-01-12
    
    2015-02-27 Nick Styles
    	* Fix copy-pase error...
    	* Tagged as xAODTrackingCnv-00-01-11
    
    2015-02-27 Nick Styles
    	* Changes for allowing re-running tracking on ESD
    	* Tagged as xAODTrackingCnv-00-01-10

--- Event/xAOD/xAODTrackingCnv/src/components/xAODTrackingCnv_entries.cxx
+++ Event/xAOD/xAODTrackingCnv/src/components/xAODTrackingCnv_entries.cxx
@@ -4,13 +4,19 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Local include(s):
+#include "../TrackCollectionCnvTool.h"
+#include "../RecTrackParticleContainerCnvTool.h"
 #include "../TrackParticleCnvAlg.h"
 #include "../VertexCnvAlg.h"
 
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, RecTrackParticleContainerCnvTool )
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TrackCollectionCnvTool )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, TrackParticleCnvAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, VertexCnvAlg )
 
 DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
-  DECLARE_NAMESPACE_ALGORITHM( xAODMaker, TrackParticleCnvAlg )
+  DECLARE_NAMESPACE_TOOL( xAODMaker, RecTrackParticleContainerCnvTool )
+    DECLARE_NAMESPACE_TOOL( xAODMaker, TrackCollectionCnvTool )
+    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, TrackParticleCnvAlg )
     DECLARE_NAMESPACE_ALGORITHM( xAODMaker, VertexCnvAlg )
-}
+    }
---
+++ Event/xAOD/xAODTrackingCnv/xAODTrackingCnv/IRecTrackParticleContainerCnvTool.h
@@ -0,0 +1,54 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id$
+#ifndef XAODTRACKINGCNV_IRECTRACKPARTICLECONTAINERCNVTOOL_H
+#define XAODTRACKINGCNV_IRECTRACKPARTICLECONTAINERCNVTOOL_H
+
+// Gaudi/Athena include(s):
+#include "GaudiKernel/IAlgTool.h"
+
+// EDM include(s):
+#include "xAODTracking/TrackParticleContainer.h"
+//#include "TrkTrack/TrackCollection.h"
+
+// Forward declaration(s):
+namespace Rec {
+class TrackParticleContainer;
+}
+
+namespace Trk {
+  class ITrackParticleCreatorTool;
+}
+
+
+namespace xAODMaker {
+
+  /// The interface provided by IRecTrackParticleContainerCnvTool
+  static const InterfaceID
+  IID_IRecTrackParticleContainerCnvTool( "xAODMaker::IRecTrackParticleContainerCnvTool", 1, 0 );
+  
+
+  class IRecTrackParticleContainerCnvTool : public virtual IAlgTool {
+    
+  public:
+    /// Function that fills an existing xAOD::TrackParticleContainer
+    virtual StatusCode convert( const Rec::TrackParticleContainer* aod,
+				xAOD::TrackParticleContainer* xaod ) = 0;
+
+    virtual StatusCode setParticleCreatorTool(ToolHandle<Trk::ITrackParticleCreatorTool> *tool) = 0;
+    
+    /// Gaudi interface definition
+    static const InterfaceID& interfaceID() {
+      return IID_IRecTrackParticleContainerCnvTool;
+    }
+
+  };//class definition
+    
+} // xAODMaker namespace
+
+
+#endif // XAODTRACKINGCNV_IRECTRACKPARTICLECONTAINERCNVTOOL_H
