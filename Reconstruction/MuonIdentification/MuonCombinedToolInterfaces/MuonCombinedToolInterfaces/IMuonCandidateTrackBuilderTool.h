48d73194dd26268e36967b56f1c0727e3e89ab32
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:30:59 +0200

    'CMakeLists.txt' (MuonCombinedToolInterfaces-01-02-07)
        * add MuonCombinedToolInterfaces/IMuonCandidateTrackBuilderTool.h
        * add MuonCombinedToolInterfaces/IMuonLayerAmbiguitySolverTool.h
        * add MuonCombinedToolInterfaces/IMuonLayerSegmentMatchingTool.h
    	* MuonCombinedToolInterfaces-01-02-07

---
+++ Reconstruction/MuonIdentification/MuonCombinedToolInterfaces/MuonCombinedToolInterfaces/IMuonCandidateTrackBuilderTool.h
@@ -0,0 +1,41 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUON_IMUONCANDIDATETRACKBUILDERTOOL
+#define MUON_IMUONCANDIDATETRACKBUILDERTOOL
+
+#include "GaudiKernel/IAlgTool.h"
+#include <vector>
+
+#include "MuonLayerEvent/MuonLayerRecoData.h"
+#include "MuonLayerEvent/MuonCandidate.h"
+
+static const InterfaceID IID_IMuonCandidateTrackBuilderTool("Muon::IMuonCandidateTrackBuilderTool",1,0);
+
+namespace Trk{
+  class Track;
+}
+
+namespace Muon {
+
+  /** Interface for a tool to build tracks out of a MuonCandidate  */
+  class IMuonCandidateTrackBuilderTool : virtual public IAlgTool {     
+  public:
+    /** IAlgTool interface */
+    static const InterfaceID& interfaceID();
+
+    /** @brief build a track out of a MuonCandidate */
+    virtual const Trk::Track* buildCombinedTrack( const Trk::Track& idTrack, const MuonCandidate& candidate ) const = 0;
+
+  };
+ 
+  inline const InterfaceID& IMuonCandidateTrackBuilderTool::interfaceID()
+  {
+    return IID_IMuonCandidateTrackBuilderTool;
+  }
+
+
+} // end of name space
+
+#endif // IMuonCandidateTrackBuilderTool_H
