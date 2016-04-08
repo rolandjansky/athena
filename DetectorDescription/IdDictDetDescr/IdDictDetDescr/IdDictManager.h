96fa4dd7241decb5ade77d98fae73ddf689d28f1
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:48:46 +0200

    'CMakeLists.txt' (IdDictDetDescr-00-00-07)

---
+++ DetectorDescription/IdDictDetDescr/IdDictDetDescr/IdDictManager.h
@@ -0,0 +1,61 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/***************************************************************************
+ IdDictDetDescr package
+ -----------------------------------------
+ Copyright (C) 2002 by ATLAS Collaboration
+ ***************************************************************************/
+
+//<doc><file>	$Id: IdDictManager.h,v 1.4 2003-09-22 07:01:22 schaffer Exp $
+//<version>	$Name: not supported by cvs2svn $
+
+#ifndef IDDICTDETDESCR_IDDICTMANAGER_H
+# define IDDICTDETDESCR_IDDICTMANAGER_H
+
+//<<<<<< INCLUDES                                                       >>>>>>
+
+#include "IdDict/IdDictDefs.h"
+#include "CLIDSvc/CLASS_DEF.h"
+
+//<<<<<< PUBLIC TYPES                                                   >>>>>>
+
+class IdHelper;
+class IdDictDictionary;
+class IdDictMgr;
+
+//<<<<<< CLASS DECLARATIONS                                             >>>>>>
+
+/**
+ *  IdDictManager is the interface to identifier dictionaries. This
+ *  class inherits most of its functionality from IdDictMgr and adds
+ *  the capabilities to be an object in the detector store.
+ */
+
+class IdDictManager
+{
+public:
+    IdDictManager();
+    IdDictManager(const IdDictMgr& mgr);
+    ~IdDictManager();
+
+    //IdDictDictionary* 	find_dictionary (const std::string& name) const;  
+
+    const IdDictMgr*	manager			(void) const;
+
+    /// Return value: 0 OK, >0 error
+    int                 initializeHelper        (IdHelper& helper) const;
+
+private:
+    const IdDictMgr* 	m_mgr;
+};
+
+//<<<<<< INLINE PUBLIC FUNCTIONS                                        >>>>>>
+//<<<<<< INLINE MEMBER FUNCTIONS                                        >>>>>>
+
+//using the macros below we can assign an identifier (and a version)
+//This is required and checked at compile time when you try to record/retrieve
+CLASS_DEF(IdDictManager, 2411, 1)
+
+#endif // IDDICTDETDESCR_IDDICTMANAGER_H
---
+++ DetectorDescription/IdDictDetDescr/src/IdDictManager.cxx
@@ -0,0 +1,51 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/***************************************************************************
+ IdDictDetDescr package
+ -----------------------------------------
+ Copyright (C) 2002 by ATLAS Collaboration
+ ***************************************************************************/
+
+//<doc><file>	$Id: IdDictManager.cxx,v 1.3 2003-09-22 07:01:22 schaffer Exp $
+//<version>	$Name: not supported by cvs2svn $
+
+//<<<<<< INCLUDES                                                       >>>>>>
+
+#include "IdDictDetDescr/IdDictManager.h"
+#include "Identifier/IdHelper.h"
+#include "IdDict/IdDictDefs.h"
+
+
+//<<<<<< MEMBER FUNCTION DEFINITIONS                                    >>>>>>
+
+IdDictManager::IdDictManager()
+    :
+    m_mgr(0)
+{}
+
+IdDictManager::IdDictManager(const IdDictMgr& mgr)
+    :
+    m_mgr(&mgr)
+{}
+
+
+IdDictManager::~IdDictManager()
+{}
+
+const IdDictMgr*
+IdDictManager::manager			(void) const
+{
+    return (m_mgr);
+}
+
+
+int
+IdDictManager::initializeHelper	(IdHelper& helper) const
+{
+    if (m_mgr) return (helper.initialize_from_dictionary(*m_mgr));
+    return (1); // otherwise error
+}
+
+
