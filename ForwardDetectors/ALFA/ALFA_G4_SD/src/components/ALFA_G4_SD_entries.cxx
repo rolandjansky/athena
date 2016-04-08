53023146853ee040b3582eca29bf653578e9ac81
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:58:33 +0200

    'CMakeLists.txt' (ALFA_G4_SD-00-03-02)
    2015-06-15  John Chapman  <John.Chapman@cern.ch>
    
    	* src/ALFA_SensitiveDetector.cxx (ProcessHits): replace double==0
    	checks with double<std::numeric_limits<double>::epislon, to
    	hopefully improve reproducibility. ATLASSIM-1963
    	* src/ALFA_SensitiveDetector.cxx/.h - move most member variables
    	to be defined in the ProcessHits method instead. Add m_ prefix to
    	the remaining member variables.
    	* Tagged ALFA_G4_SD-00-03-02
    
    2015-06-12  John Chapman  <John.Chapman@cern.ch>
    
    	* src/ALFA_SensitiveDetector.cxx (Initialize): set collection
    	names in the Initialize method. ATLASSIM-1963.
    	* Tagged ALFA_G4_SD-00-03-01
    
    2015-06-08  John Chapman  <John.Chapman@cern.ch>
    
    	* src/ALFA_SensitiveDetectorTool.cxx/.h - change method return
    	types to match base classes. Move default configuration to
    ...
    (Long ChangeLog diff - truncated)

--- ForwardDetectors/ALFA/ALFA_G4_SD/src/components/ALFA_G4_SD_entries.cxx
+++ ForwardDetectors/ALFA/ALFA_G4_SD/src/components/ALFA_G4_SD_entries.cxx
@@ -1,9 +1,9 @@
-#include "ALFA_G4_SD/ALFA_SensitiveDetector.h"
+#include "../ALFA_SensitiveDetectorTool.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
-DECLARE_ALGORITHM_FACTORY(ALFA_SensitiveDetector)
+DECLARE_TOOL_FACTORY(ALFA_SensitiveDetectorTool)
 
 DECLARE_FACTORY_ENTRIES(ALFA_G4_SD) {
-DECLARE_ALGORITHM(ALFA_SensitiveDetector);
+  DECLARE_TOOL(ALFA_SensitiveDetectorTool);
 }
- 
+
