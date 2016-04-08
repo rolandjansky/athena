e7e7d150a6a5cf6d1eb2242ffc7a6570022a935d
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:41:58 +0200

    'CMakeLists.txt' (EventDisplaysOnline-00-02-19)

---
+++ graphics/EventDisplaysOnline/share/ByteStreamInput_jobOptions.py
@@ -0,0 +1,159 @@
+#########################################
+#
+# Example setup for ByteStreamEmonSvc
+# 
+#########################################
+
+# Common part, copy from here
+
+from AthenaCommon.AppMgr import ServiceMgr as svcMgr
+from AthenaCommon.Constants import *
+
+include("ByteStreamEmonSvc/ByteStreamEmonInputSvc_jobOptions.py")
+
+# #####################################################
+# If using the ERSBootstrap.py file, enable the output
+# via ERS
+# ####################################################
+#MessageSvc.useErs = True
+
+# Define the input
+ByteStreamEmonInputSvc = svcMgr.ByteStreamInputSvc
+
+# ############################################################
+# The name of the partition you want to connect to is taken
+# from 'TDAQ_PARTITION' if it exists, otherwise from here.
+# ############################################################
+if not 'Partition' in dir():
+  Partition = 'ATLAS'
+print "ByteStreamEmonInputSvc: Setting partition to : %s" % (Partition)
+ByteStreamEmonInputSvc.Partition = Partition
+
+# #########################################
+# The source of events, SFI for full events
+# #########################################
+if not 'SamplingLevel' in dir():
+  SamplingLevel="SFI"
+ByteStreamEmonInputSvc.Key = SamplingLevel
+
+# ############################################################
+# A list of of key values, e.g. a list of SFIs to contact.
+# If not defined, one event provider of this type (i.e. any SFI)
+# ############################################################
+#ByteStreamEmonInputSvc.KeyValue  = ["SFI-1", "SFI-2", "SFI-3" ]
+
+# #######################################
+# Alternative: N providers of type 'SFI'
+# KeyValue is ignored if this is set.
+# ######################################
+if not 'Nodes' in dir():
+  Nodes=10
+ByteStreamEmonInputSvc.KeyCount = Nodes
+
+# #######################################
+# Set this to the IS server where you want
+# to publish histograms, too. If unset, no
+# histograms are published.
+# #######################################
+#ByteStreamEmonInputSvc.ISServer = 'Histogramming'
+
+# ########################################
+# The provider name under which your histograms
+#  appear in OH.
+# ########################################
+#ByteStreamEmonInputSvc.PublishName = 'TRT_PT'
+
+# ###################################################
+# Should histograms be cleared at new run ? default: yes
+# ###################################################
+#ByteStreamEmonInputSvc.ClearHistograms = True
+
+# ####################################################
+# A regular expression to restrict which histograms are published.
+# ####################################################
+# ByteStreamEmonInputSvc.Include = '.*'
+# ByteStreamEmonInputSvc.Exclude = ''
+
+# ###############################################
+# Frequency of updates (in number of events, not secs...)
+# ###############################################
+# ByteStreamEmonInputSvc.Frequency = 5
+
+# #################################
+# Set dispersion flag to false (so other can sample the same event
+# #################################
+ByteStreamEmonInputSvc.Dispersion=False
+
+# #################################
+# Set timeout (in ms?)
+# #################################
+ByteStreamEmonInputSvc.Timeout=600000
+
+# ##############################
+# one of 'Ignore', 'Or', 'And'
+# ##############################
+ByteStreamEmonInputSvc.LVL1Logic = "Ignore"
+
+# #########################################
+# One of 'TAP', 'TBP' or 'TAV' (default)
+# #########################################
+#ByteStreamEmonInputSvc.LVL1Origin = "TAV"
+
+# ###########################################################
+# A list of L1 bit names to select on. This requires the
+# L1CT.TrigConfL1Items to be published in IS.
+# 
+# This can be used instead of or in addition to LVL1Bits.
+# ###########################################################
+#ByteStreamEmonInputSvc.LVL1Names = ['L1_MU2', 'L1_MU3', 'L1_EM10' ]
+
+# A list of numerical trigger bits instead of names. This is
+# Or'ed with LVL1Names
+#ByteStreamEmonInputSvc.LVL1Items = [ 10, 20, 72, 245 ]
+
+# ###########################################
+# Selection by stream tag:
+# One of 'Or', 'And', 'Ignore' (default)
+# ###########################################
+## Set stream logic ignore for 'Random' stream
+if ( not 'Stream' in dir()) or (Stream == 'Default') or (Stream.startswith("Test")):
+  ByteStreamEmonInputSvc.StreamLogic = "Ignore"
+else:
+  ByteStreamEmonInputSvc.StreamLogic = "And"
+
+# ###########################################
+# One of 'physics' or 'calibration'
+# ############################################
+#Make sure it is defined
+if not 'StreamType' in dir():
+  StreamType="physics"
+ByteStreamEmonInputSvc.StreamType = StreamType 
+
+# ############################################
+# A list of stream tag names
+# ############################################
+ByteStreamEmonInputSvc.StreamNames = [ Stream ]
+
+# #################################################
+# The event buffer size
+# #################################################
+ByteStreamEmonInputSvc.BufferSize = 30
+
+# #################################################
+# Shall athena exit if the partition is shutdown ?
+# For offline athena tasks mainly.
+# #################################################
+#ByteStreamEmonInputSvc.ExitOnPartitionShutdown = False
+
+ByteStreamCnvSvc = Service( "ByteStreamCnvSvc" )
+theApp.ExtSvc += [ "ByteStreamCnvSvc"]
+
+###
+# Switch output level if requested
+###
+if not 'BSEmonMsgLvl' in dir():
+   BSEmonMsgLvl=WARNING
+ByteStreamEmonInputSvc.OutputLevel=BSEmonMsgLvl
+
+if BSEmonMsgLvl <= DEBUG:
+  print "\n\n\t ByteStreamEmonInput setup:\n",ByteStreamEmonInputSvc,"\n\n"
