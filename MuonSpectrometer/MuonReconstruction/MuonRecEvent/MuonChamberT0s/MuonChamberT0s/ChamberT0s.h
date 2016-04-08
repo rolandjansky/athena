3294be7681458541de3d3c87a885ed427b9221b3
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:54:09 +0200

    'CMakeLists.txt' (MuonChamberT0s-00-02-04)
            * Tagging MuonChamberT0s-00-02-04.
    	* MuonChamberT0s/ChamberT0s.h, src/ChamberT0s.cxx: Vector should
    	be passed by const reference.  Add ctor taking vector by move.

--- MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonChamberT0s/MuonChamberT0s/ChamberT0s.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonChamberT0s/MuonChamberT0s/ChamberT0s.h
@@ -28,7 +28,8 @@ namespace Muon {
       
       ChamberT0s( ) ;
       
-      ChamberT0s( std::vector< std::pair < Identifier, float > > t0s ) ;
+      ChamberT0s( const std::vector< std::pair < Identifier, float > >& t0s ) ;
+      ChamberT0s(       std::vector< std::pair < Identifier, float > >&& t0s ) ;
       
       ~ChamberT0s();
       
--- MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonChamberT0s/src/ChamberT0s.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonChamberT0s/src/ChamberT0s.cxx
@@ -11,9 +11,14 @@ float ChamberT0s::ChamberUnknown = -99999.9;
 ChamberT0s::ChamberT0s( ) 
 {}
 
-ChamberT0s::ChamberT0s( std::vector< std::pair < Identifier, float > > t0s ) 
+ChamberT0s::ChamberT0s( const std::vector< std::pair < Identifier, float > >& t0s ) 
     :
     m_t0s (t0s) 
+{}
+
+ChamberT0s::ChamberT0s( std::vector< std::pair < Identifier, float > >&& t0s ) 
+    :
+    m_t0s (std::move(t0s)) 
 {}
 
 ChamberT0s::~ChamberT0s( )
