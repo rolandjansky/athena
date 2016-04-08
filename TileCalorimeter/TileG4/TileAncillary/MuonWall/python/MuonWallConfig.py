567216af3f11ee677184387557d4e8a6234c5659
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:47:28 +0200

    'CMakeLists.txt' (MuonWall-00-03-00)
    2015-12-16  John Chapman  <John.Chapman@cern.ch>
    
    	* Migrating MuonWall DetFacility class to the new
    	infrastructure. ATLASSIM-2423
    	* Use .h/.cxx rather than .hh/.cc for SD file names.
    	* Tagging MuonWall-00-03-00
    
    2015-09-18  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging MuonWall-00-02-02
    	* python/MuonWallConfigDb.py - fix typo. ATLASSIM-2140
    	* cmt/requirements - add missing declare python modules
    	statement. ATLASSIM-2140
    
    2015-08-28  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging MuonWall-00-02-01
    	* minor tidying
    
    2015-07-29  John Chapman  <John.Chapman@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

---
+++ TileCalorimeter/TileG4/TileAncillary/MuonWall/python/MuonWallConfig.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon import CfgMgr
+def getMuonWallSD(name="MuonWallSD", **kwargs):
+    kwargs.setdefault("LogicalVolumeNames", ['MuonWall::MuScintillatorLayer'])
+    kwargs.setdefault("OutputCollectionNames", ["TileTBHits"])
+    return CfgMgr.MuonWallSDTool(name, **kwargs)
+
+def getMuonWallTool(name="MuonWall", **kwargs):
+    kwargs.setdefault("DetectorName", "MuonWall")
+    kwargs.setdefault("ZLength", 200.)
+    kwargs.setdefault("YLength", 400.)
+    kwargs.setdefault("XLength", 20.)
+    eta=0
+    theta=3.1415926
+    delta=0.0
+    zmax=2380.22441662
+    r=5115.0
+    z=0.0
+    import math
+    if (math.fabs(theta)<1.57):
+        z=r*math.tan(-theta)-delta
+    elif (eta!=0):
+        z=r/math.tan(2*math.atan(math.exp(-eta)))
+    if (z>zmax):
+        z=zmax
+    elif (z<-zmax):
+        z=-zmax
+    kwargs.setdefault("OffsetX", r)
+    kwargs.setdefault("OffsetY", 0.0)
+    kwargs.setdefault("OffsetZ", z)
+    return CfgMgr.MuonWallTool(name, **kwargs)
+
+def getMuonWallTileTB(name="MuonWallTileTB", **kwargs):
+    from G4AtlasApps.SimFlags import simFlags
+    # Check the consistency of the flags
+    if simFlags.Eta.statusOn and (simFlags.Theta.statusOn or simFlags.Z.statusOn):
+        raise ValueError,('THE ETA PARAMETER CAN NOT BE SET TOGETHER WITH THETA AND Z')
+    elif not(simFlags.Theta.statusOn and simFlags.Z.statusOn) and not simFlags.Eta.statusOn:
+        raise ValueError,('THETA AND Z ARE NOT SET')
+    import math
+    from AthenaCommon import PhysicalConstants
+    if simFlags.Eta.statusOn:
+        # Mode 1 -> User enters only eta
+        eta=simFlags.Eta.get_Value()
+        ThetaY=-(PhysicalConstants.pi*0.5)+2*math.atan(math.exp(-eta))
+        DeltaF=0.0
+    elif(simFlags.Theta.statusOn and simFlags.Z.statusOn):
+        theta=simFlags.Theta.get_Value()
+        z=simFlags.Z.get_Value()
+        eta=0.0
+        if abs(theta) < 60.01:
+            # Mode 2 -> User enters theta!=+/-90 and Z
+            # Z is the distance from center of the module to the desired
+            # entrace point calculated at R=2290 (start of tilecal
+            # module)
+            ThetaY=math.radians(theta)
+            DeltaF=(2290.0*math.tan(-ThetaY)-z)
+        elif abs(abs(theta)-90.0) < 0.01:
+            # Mode 3 -> User enters theta=(+/-)90 and Z
+            # Z is the distance from ATLAS center to corresponding
+            # tilerow
+            # e.g center of first tile row is at 2300 + 100/2 = 2350
+            sign=int(theta>0)*2-1
+            ThetaY=sign*math.radians(90.0)
+            DeltaF=0.0
+    else:
+        print 'Tile table rotation: ERROR unknown rotation mode'
+        raise ValueError,('UNKNOWN MODE - NEITHER ETA NOR THETA AND Z ARE SET')
+    zmax=2380.22441662
+    r=5115.0
+    z=0.0
+    if (math.fabs(ThetaY)<1.57):
+        z=r*math.tan(-ThetaY)-DeltaF
+    elif (eta!=0):
+        z=r/math.tan(2*math.atan(math.exp(-eta)))
+    if (z>zmax):
+        z=zmax
+    elif (z<-zmax):
+        z=-zmax
+    kwargs.setdefault("OffsetX", r)
+    kwargs.setdefault("OffsetY", 0.0)
+    kwargs.setdefault("OffsetZ", z)
+    return getMuonWallTool(name, **kwargs)
+
---
+++ TileCalorimeter/TileG4/TileAncillary/MuonWall/src/MuonWallSDTool.cxx
@@ -0,0 +1,84 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//************************************************************
+//
+// Class MuonWallSD.
+// Sensitive detector for the muon wall
+//
+// Author: franck Martin <Franck.Martin@cern.ch>
+// january 12, 2004
+//
+//************************************************************
+
+#include "MuonWallSDTool.h"
+#include "MuonWallSD.h"
+
+MuonWallSDTool::MuonWallSDTool(const std::string& type, const std::string& name, const IInterface* parent)
+  : SensitiveDetectorBase(type,name,parent)
+{
+  declareInterface<ISensitiveDetector>(this);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+MuonWallSDTool::~MuonWallSDTool()
+{
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+StatusCode MuonWallSDTool::SetupEvent()
+{
+  ATH_MSG_VERBOSE( "MuonWallSDTool::SetupEvent()" );
+  if(!getSD())
+    {
+      ATH_MSG_ERROR ("SetupEvent: MuonWallSD never created!");
+      return StatusCode::FAILURE;
+    }
+  else
+    {
+      MuonWallSD *localSD = dynamic_cast<MuonWallSD*>(getSD());
+      if(!localSD)
+        {
+          ATH_MSG_ERROR ("SetupEvent: Failed to cast m_SD into MuonWallSD.");
+          return StatusCode::FAILURE;
+        }
+      localSD->StartOfAthenaEvent();
+    }
+  return StatusCode::SUCCESS;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+StatusCode MuonWallSDTool::Gather()
+{
+  ATH_MSG_VERBOSE( "MuonWallSDTool::Gather()" );
+  if(!getSD())
+    {
+      ATH_MSG_ERROR ("Gather: MuonWallSD never created!");
+      return StatusCode::FAILURE;
+    }
+  else
+    {
+      MuonWallSD *localSD = dynamic_cast<MuonWallSD*>(getSD());
+      if(!localSD)
+        {
+          ATH_MSG_ERROR ("Gather: Failed to cast m_SD into MuonWallSD.");
+          return StatusCode::FAILURE;
+        }
+      localSD->EndOfAthenaEvent();
+    }
+  return StatusCode::SUCCESS;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VSensitiveDetector* MuonWallSDTool::makeSD()
+{
+  ATH_MSG_DEBUG( "Initializing SD" );
+  // Create a fresh SD
+  return new MuonWallSD(name(), m_outputCollectionNames[0]);
+}
