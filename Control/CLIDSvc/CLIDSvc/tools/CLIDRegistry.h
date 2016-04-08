a88f01d376e0aae81823e2cef4dedd4ba15836c1
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:41:07 +0200

    'CMakeLists.txt' (CLIDSvc-00-05-02)

---
+++ Control/CLIDSvc/CLIDSvc/tools/CLIDRegistry.h
@@ -0,0 +1,11 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TOOLS_CLIDREGISTRY_H
+# define TOOLS_CLIDREGISTRY_H
+
+// forwarding to 'real' CLIDRegistry
+#include "SGTools/CLIDRegistry.h"
+
+#endif // TOOLS_CLIDREGISTRY_H
---
+++ Control/CLIDSvc/CLIDSvc/tools/ClassID_traits.h
@@ -0,0 +1,16 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TOOLS_CLASSID_TRAITS_H
+#define TOOLS_CLASSID_TRAITS_H
+
+// forwarding to 'real' ClassID_traits
+#include "SGTools/ClassID_traits.h"
+
+#endif
+
+
+
+
+
