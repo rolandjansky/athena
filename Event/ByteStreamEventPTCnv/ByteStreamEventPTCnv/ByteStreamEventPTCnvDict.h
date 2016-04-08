92adb4608412cf87a38220cb4f923d00fada894c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:56:11 +0200

    'CMakeLists.txt' (ByteStreamEventPTCnv-00-00-01)

---
+++ Event/ByteStreamEventPTCnv/ByteStreamEventPTCnv/ByteStreamEventPTCnvDict.h
@@ -0,0 +1,17 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef BYTESTREAMEVENTPTCNV_BYTESTREAMEVENTPTCNVDICT_H
+#define BYTESTREAMEVENTPTCNV_BYTESTREAMEVENTPTCNVDICT_H
+/**
+ * @file ByteStreamEventPTCnvDict.h
+ *
+ * @brief Header file for dictionary generation
+ *
+ * @author  <vaniachine@anl.gov>
+ */
+
+#include "ByteStreamEventPTCnv/ByteStreamMetadataPTCnv_p1.h"
+
+#endif // BYTESTREAMEVENTPTCNV_BYTESTREAMEVENTPTCNVDICT_H
