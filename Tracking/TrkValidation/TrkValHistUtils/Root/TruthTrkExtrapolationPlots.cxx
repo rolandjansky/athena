f4604ce40959ce66a2de1ecb3d6ab3ef7a86c50a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:04:02 +0200

    'CMakeLists.txt' (TrkValHistUtils-00-01-12)
    	* Changed TH?D to TH?F to reduce memory footprint
    	* Added class for muon truth extrapolation studies (disabled to maintain compatibility)
    	* Tagging TrkValHistUtils-00-01-12

--- Tracking/TrkValidation/TrkValHistUtils/Root/TruthTrkExtrapolationPlots.cxx
+++ Tracking/TrkValidation/TrkValHistUtils/Root/TruthTrkExtrapolationPlots.cxx
@@ -6,71 +6,211 @@
 
 namespace Trk{
 
-void TruthTrkExtrapolationPlots::init(){
-    CaloEntry_p=NULL;
-    MuonEntry_p=NULL;
-    MuonExit_p=NULL;
-    CaloEntry_pExtr=NULL;
-    MuonEntry_pExtr=NULL;
-    MuonExit_pExtr=NULL;
-    CaloEntry_dp=NULL;
-    MuonEntry_dp=NULL;
-    MuonExit_dp=NULL;
-}
-
-void TruthTrkExtrapolationPlots::initializePlots(){
-  CaloEntry_p = Book1D("CaloEntry_p","CaloEntry_p;p_{nom} @CaloEntry;Entries",100,0,400);
-  MuonEntry_p = Book1D("MuonEntry_p","MuonEntry_p;p_{nom} @MuonEntry;Entries",100,0,400);
-  MuonExit_p  = Book1D("MuonExit_p","MuonExit_p;p_{nom} @MuonExit;Entries",100,0,400);
-  CaloEntry_pExtr = Book1D("CaloEntry_pExtr","CaloEntry_p;p_{ext} @CaloEntry;Entries",100,0,400);
-  MuonEntry_pExtr = Book1D("MuonEntry_pExtr","MuonEntry_p;p_{ext} @MuonEntry;Entries",100,0,400);
-  MuonExit_pExtr  = Book1D("MuonExit_pExtr","MuonExit_p;p_p_{ext} @MuonExit;Entries",100,0,400);;
-  CaloEntry_dp = Book1D("CaloEntry_dp","CaloEntry_dp;p_{nom}- p_{ext} @CaloEntry;Entries",100,-10,10);
-  MuonEntry_dp = Book1D("MuonEntry_dp","MuonEntry_dp;p_{nom}- p_{ext} @MuonEntry;Entries",100,-10,10);
-  MuonExit_dp  = Book1D("MuonExit_dp","MuonExit_dp;p_{nom}- p_{ext} @MuonExit;Entries",100,-10,10);
-
-}
+  TruthTrkExtrapolationPlots::TruthTrkExtrapolationPlots(PlotBase* pParent, std::string sDir):PlotBase(pParent, sDir),
+											      m_CaloEntry(this,"","CaloEntry"),	
+											      m_MuonEntry(this,"","MuonEntry"),
+											      m_MuonExit(this,"","MuonExit")//,
+											      // m_Calo(this,"Scattering/CaloOnly","CaloEntryLayer", "MuonEntryLayer"),
+											      // m_MS(this,"/Scattering/MSOnly","MuonEntryLayer", "MuonExitLayer"),
+											      // m_IP(this,"/Scattering/IPOnly","IP","CaloEntryLayer"),
+											      // m_IP_Calo(this, "/Scattering/IP_Calo","IP","MuonEntryLayer"),
+											      // m_Calo_MS(this, "/Scattering/Calo_MS","CaloEntryLayer","MuonExitLayer"),
+											      // m_IP_MS(this, "/Scattering/IP_Calo_Ms","IP","MuonExitLayer")
+  {}
 
 void TruthTrkExtrapolationPlots::fill(const xAOD::TruthParticle& truthprt) {
-  FillPlots(CaloEntry_dp, truthprt, "CaloEntryLayer");
-  FillPlots(MuonEntry_dp, truthprt, "MuonEntryLayer");
-  FillPlots(MuonExit_dp,  truthprt, "MuonExitLayer");  
-}
+  m_CaloEntry.fill(truthprt, "CaloEntryLayer");
+  m_MuonEntry.fill(truthprt, "MuonEntryLayer");
+  m_MuonExit.fill(truthprt, "MuonExitLayer");
 
-
-void TruthTrkExtrapolationPlots::FillPlots(TH1* hist, const xAOD::TruthParticle& truthprt,const std::string& sNom ) {
-  if (!truthprt.isAvailable<float>(sNom + "_px") || 
-      !truthprt.isAvailable<float>(sNom + "_py") || 
-      !truthprt.isAvailable<float>(sNom + "_pz") || 
-      !truthprt.isAvailable<float>(sNom + "_px_extr" ) ||
-      !truthprt.isAvailable<float>(sNom + "_py_extr" ) ||
-      !truthprt.isAvailable<float>(sNom + "_pz_extr" )) return;
-
-  TVector3 nomVec(truthprt.auxdata<float>(sNom + "_px"), 
-                  truthprt.auxdata<float>(sNom + "_py"), 
-                  truthprt.auxdata<float>(sNom + "_pz"));
-
-  TVector3 exVec(truthprt.auxdata<float>(sNom + "_px_extr"), 
-                 truthprt.auxdata<float>(sNom + "_py_extr"), 
-                 truthprt.auxdata<float>(sNom + "_pz_extr"));
-
-  float pNom = nomVec.Mag();
-  float pExt = exVec.Mag();
-  hist->Fill((pNom-pExt)*0.001);
-
-  if (sNom=="CaloEntryLayer") {
-    CaloEntry_p->Fill(pNom*0.001);
-    CaloEntry_pExtr->Fill(pExt*0.001);
-  }
-  else if (sNom=="MuonEntryLayer") {
-    MuonEntry_p->Fill(pNom*0.001);
-    MuonEntry_pExtr->Fill(pExt*0.001);
-  }
-  else if (sNom=="MuonExitLayer") {
-    MuonExit_p->Fill(pNom*0.001);
-    MuonExit_pExtr->Fill(pExt*0.001);
-  }
+  // m_Calo.fill(truthprt,"CaloEntryLayer_","MuonEntryLayer_");
+  // m_MS.fill(truthprt, "MuonEntryLayer_","MuonExitLayer_");
+  // m_IP.fill(truthprt, "","CaloEntryLayer_");
+  // m_IP_Calo.fill(truthprt,"","MuonEntryLayer_");
+  // m_Calo_MS.fill(truthprt,"CaloEntryLayer_","MuonExitLayer_");
+  // m_IP_MS.fill(truthprt, "","MuonExitLayer_");
 
 }
 
+ExtrLayerPlots::ExtrLayerPlots(PlotBase *pParent, std::string sDir, std::string sLayerName):PlotBase(pParent,sDir),
+									 m_sLayerName(sLayerName)
+  {;}
+void  ExtrLayerPlots::initializePlots(){
+    p  = Book1D(m_sLayerName+"_p", m_sLayerName+ " truth_p;Momentum [GeV];Entries",100,0.,400);
+    p_extr  = Book1D(m_sLayerName+"_pExtr", m_sLayerName+ " truth_p_extr;Momentum [GeV];Entries",100,0.,400);
+    dp_truth  = Book1D(m_sLayerName+"_dp", m_sLayerName+ " truth_dp;Momentum difference p-p_{extr} [GeV];Entries",100,-10,10);
+    
+    // px  = Book1D(m_sLayerName+"_truth_px", m_sLayerName+ " truth_px;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+    // py  = Book1D(m_sLayerName+"_truth_py", m_sLayerName+ " truth_py;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+    // pz  = Book1D(m_sLayerName+"_truth_pz", m_sLayerName+ " truth_pz;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+
+    // px_extr  = Book1D(m_sLayerName+"_truth_px_extr", m_sLayerName+ " truth_px_extr;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+    // py_extr  = Book1D(m_sLayerName+"_truth_py_extr", m_sLayerName+ " truth_py_extr;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+    // pz_extr  = Book1D(m_sLayerName+"_truth_pz_extr", m_sLayerName+ " truth_pz_extr;Momentum [GeV];Entries / 2.5 GeV",200,-250.,250);
+
+    // dpx_truth  = Book1D(m_sLayerName+"_truth_dpx", m_sLayerName+ " truth_dpx;Momentum difference px-px_extr[GeV];Entries / 2.5 GeV",250,-25,25);
+    // dpy_truth  = Book1D(m_sLayerName+"_truth_dpy", m_sLayerName+ " truth_dpy;Momentum difference py-py_extr[GeV];Entries / 2.5 GeV",250,-25,25);
+    // dpz_truth  = Book1D(m_sLayerName+"_truth_dpz", m_sLayerName+ " truth_dpz;Momentum difference pz-pz_extr[GeV];Entries / 2.5 GeV",250,-25,25);
+    // pt = Book1D(m_sLayerName+"_truth_pt", m_sLayerName+ " truth_pt;Transverse Momentum [GeV];Entries / 2.5 GeV",100,0.,250);
+    // eta = Book1D(m_sLayerName+"_truth_eta",m_sLayerName+ " truth_eta;Pseudo-Rapidity;Entries / 0.1",64,-3.2,3.2);
+    // phi = Book1D(m_sLayerName+"_truth_phi",m_sLayerName+ " truth_phi;Azimuthal Angle;Entries / 0.1",64,-3.2,3.2);
+    // theta = Book1D(m_sLayerName+"_truth_theta",m_sLayerName+ " truth_theta;polar Angle;Entries / 0.1",32,0,3.2);
+  }
+void ExtrLayerPlots::fill(const xAOD::TruthParticle& truthprt, std::string sNom){
+  if (!truthprt.isAvailable<float>(sNom + "_px") ||
+      !truthprt.isAvailable<float>(sNom + "_py") ||
+      !truthprt.isAvailable<float>(sNom + "_pz")) return;
+	   
+  TVector3 vec(truthprt.auxdata<float>(sNom + "_px"),
+	       truthprt.auxdata<float>(sNom + "_py"),
+	       truthprt.auxdata<float>(sNom + "_pz"));
+
+  TVector3 vec_extr;
+  if(!truthprt.isAvailable<float>(sNom + "_px_extr") ||
+     !truthprt.isAvailable<float>(sNom + "_py_extr") ||
+     !truthprt.isAvailable<float>(sNom + "_pz_extr")) return;
+      
+  vec_extr.SetXYZ(truthprt.auxdata<float>(sNom + "_px_extr"),
+		  truthprt.auxdata<float>(sNom + "_py_extr"),
+		  truthprt.auxdata<float>(sNom + "_pz_extr"));
+   
+  p->Fill(vec.Mag()*0.001);
+  // px->Fill(vec.Px()*0.001);
+  // py->Fill(vec.Py()*0.001);
+  // pz->Fill(vec.Pz()*0.001);
+  p_extr->Fill(vec_extr.Mag()*0.001);
+  // px_extr->Fill(vec_extr.Px()*0.001);
+  // py_extr->Fill(vec_extr.Py()*0.001);
+  // pz_extr->Fill(vec_extr.Pz()*0.001);
+  dp_truth->Fill((vec.Mag()-vec_extr.Mag())*0.001);
+  // dpx_truth->Fill((vec.Px()-vec_extr.Px())*0.001);
+  // dpy_truth->Fill((vec.Py()-vec_extr.Py())*0.001);
+  // dpz_truth->Fill((vec.Pz()-vec_extr.Pz())*0.001);
+  // pt->Fill(vec.Perp()*0.001);
+  // eta->Fill(vec.PseudoRapidity());
+  // phi->Fill(vec.Phi());
+  // theta->Fill(vec.Theta());
+  }
+
+  ExtrRegionPlots::ExtrRegionPlots(PlotBase *pParent, std::string sDir, std::string sRegionBegin, std::string sRegionEnd):PlotBase(pParent,sDir),
+											       m_sRegionBegin(sRegionBegin),
+											       m_sRegionEnd(sRegionEnd)
+  {;}
+void  ExtrRegionPlots::initializePlots(){
+  //histogram definitions for scattering plots
+ //variables within subdetector(s)
+    //energy loss related quantities
+    dp  = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth p: p_{begin} vs p_{end}; #Delta p=p_{begin}-p_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpt = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpt",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth pt;#Delta pt=pt_{begin}-pt_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpx = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpx",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth px;#Delta px=px_{begin}-px_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpy = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpy",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth py;#Delta py=py_{begin}-py_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpz = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpz",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth pz;#Delta pz=pz_{begin}-pz_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+
+    //extrapolated quantities: CaloEntryLayer_p_extr=extrapolated value from IP to CaloEntryLayer using e-loss parametrization
+    //if comparison to IP, truth_p_extr=truth_p
+    dp_extr   = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_extr",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth p, extrapolated from IP to current layer; #Delta p=p_extr_{begin}-p_extr_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpx_extr  = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpx_extr",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth px, extrapolated from IP to current layer; #Delta px=px_extr_{begin}-px_extr_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpy_extr  = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpy_extr",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth py, extrapolated from IP to current layer; #Delta py=py_extr_{begin}-py_extr_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+    dpz_extr  = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dpz_extr",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in truth pz, extrapolated from IP to current layer; #Delta pz=pz_extr_{begin}-pz_extr_{end} [GeV];Entries / 0.5 GeV",80,-20.,20.);
+   
+    //2D plots 
+       dp_vs_p = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_vs_p",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in p (#Delta p=p_{begin}-p_{end}) vs p_{begin};p_{begin} [GeV];#Delta p=p_{begin}-p_{end} [GeV]", 250,0.,250.,80,0.,20);
+    dp_vs_phi = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_vs_phi",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in p(#Delta p=p_{begin}-p_{end}) vs phi_{begin};phi_{begin};#Delta p=p_{begin}-p_{end} [GeV]",128,-3.2,3.2,80,0.,20);
+    dp_vs_eta = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_vs_eta",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in p(#Delta p=p_{begin}-p_{end}) vs eta_{begin};#eta_{begin};#Delta p=p_{begin}-p_{end} [GeV]",128,-3.2,3.2,80,0.,20);
+    p_vs_p = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_p_vs_p",m_sRegionBegin+" vs "+m_sRegionEnd+" p_{begin} vs p_{end};p_{begin} [GeV]; p_{end} [GeV]",250,0.,250,250,0.,250);
+    p_extr_vs_p_extr = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_p_extr_vs_p_extr",m_sRegionBegin+" vs "+m_sRegionEnd+" p_{begin extr} vs p_{end extr};p_{begin extr} [GeV]; p_{end extr} [GeV]",250,0.,250,250,0.,250);
+    dp_extr_vs_eta = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_extr_vs_eta",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in p_{extr}(#Delta p_{extr}=p_{begin extr}-p_{end extr}) vs eta_{begin};#eta__{begin};#Delta p_{extr}=p_{begin extr}-p_{end extr} [GeV]",128,-3.2,3.2,80,0.,20);
+  
+    //3D
+     dp_vs_eta_phi = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dp_vs_eta_phi",m_sRegionBegin+" vs "+m_sRegionEnd+" sum of bin entries: difference in p(#Delta p=p_{begin}-p_{end}) vs eta_{begin} and phi_{begin};#eta__{begin};#phi__{begin};#Delta p=p_{begin}-p_{end} [GeV]",128,-3.2,3.2,128,-3.2,-3.2);
+   
+    //measure for multiple scattering
+    dR  = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR",m_sRegionBegin+" vs "+m_sRegionEnd+" dR between the two positions in the detector;pt_{begin}->dR(pt_{end});Entries / 0.05",100,0.,0.5);
+    //2D
+    dR_vs_p  = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR_vs_p",m_sRegionBegin+" vs "+m_sRegionEnd+" dR vs p_{begin};p_{begin} [GeV];_{begin}->dR(p_{end})",250,0.,250,100,0.,0.5);
+    dR_vs_dp = Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR_vs_dp",m_sRegionBegin+" vs "+m_sRegionEnd+" dR vs difference in p;#Delta p=p_{begin}-p_{end} [GeV];p_{begin}->dR(p_{end})",80,0.,20, 100,0.,0.5);
+    dR_vs_eta= Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR_vs_eta",m_sRegionBegin+" vs "+m_sRegionEnd+" dR vs eta;#eta_{begin};p_{begin}->dR(p_{end})",128,-3.2,3.2, 100,0.,0.5);
+    dR_vs_phi= Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR_vs_phi",m_sRegionBegin+" vs "+m_sRegionEnd+" dR vs phi;#phi_{begin};p_{begin}->dR(p_{end})",128,-3.2,3.2, 100,0.,0.5);
+    dR_vs_eta_phi= Book2D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dR_vs_eta_phi",m_sRegionBegin+" vs "+m_sRegionEnd+" sum of bin entries: dR vs eta and phi;#eta_{begin};#phi_{begin};#DeltaR", 128,-3.2,3.2,128,-3.2,3.2); 
+
+    dphi= Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dphi",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in phi;#Delta phi;Entries / 0.05",100,-0.25,0.25);
+    dtheta= Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dtheta",m_sRegionBegin+" vs "+m_sRegionEnd+" difference in theta;#Delta theta;Entries / 0.002",100,-0.25,0.25);
+    dAngle = Book1D(m_sRegionBegin+"_vs_"+m_sRegionEnd+"_dAngle",m_sRegionBegin+" vs "+m_sRegionEnd+" total angle difference;#Delta angle;Entries / 0.002",100,-0.25,0.25);
+ 
 }
+
+  void ExtrRegionPlots::fill(const xAOD::TruthParticle& truthprt, std::string sDetBegin, std::string sDetEnd){
+    if (!truthprt.isAvailable<float>(sDetBegin + "px") ||
+	!truthprt.isAvailable<float>(sDetBegin + "py") ||
+	!truthprt.isAvailable<float>(sDetBegin + "pz") ||
+	!truthprt.isAvailable<float>(sDetEnd + "px") ||
+	!truthprt.isAvailable<float>(sDetEnd + "py") ||
+	!truthprt.isAvailable<float>(sDetEnd + "pz")) return;
+	
+    TVector3 vecDetBegin(truthprt.auxdata<float>(sDetBegin + "px"),
+		     truthprt.auxdata<float>(sDetBegin + "py"),
+		     truthprt.auxdata<float>(sDetBegin + "pz"));
+
+   TVector3 vecDetEnd(truthprt.auxdata<float>(sDetEnd + "px"),
+		     truthprt.auxdata<float>(sDetEnd + "py"),
+		     truthprt.auxdata<float>(sDetEnd + "pz"));
+
+    //check if IP, since extr is not available there (sDetBegin="")
+    TVector3 vecDetBegin_extr;
+    if (sDetBegin!="") {
+      if (!truthprt.isAvailable<float>(sDetBegin + "px_extr") ||
+	  !truthprt.isAvailable<float>(sDetBegin + "py_extr") ||
+	  !truthprt.isAvailable<float>(sDetBegin + "pz_extr")) return;
+      
+      vecDetBegin_extr.SetXYZ(truthprt.auxdata<float>(sDetBegin + "px_extr"),
+			  truthprt.auxdata<float>(sDetBegin + "py_extr"),
+			  truthprt.auxdata<float>(sDetBegin + "pz_extr"));
+    }
+    else vecDetBegin_extr=vecDetBegin;
+
+    TVector3 vecDetEnd_extr;
+    if (sDetEnd!="") {
+      if (!truthprt.isAvailable<float>(sDetEnd + "px_extr") ||
+	  !truthprt.isAvailable<float>(sDetEnd + "py_extr") ||
+	  !truthprt.isAvailable<float>(sDetEnd + "pz_extr")) return;
+
+      vecDetEnd_extr.SetXYZ(truthprt.auxdata<float>(sDetEnd + "px_extr"),
+			    truthprt.auxdata<float>(sDetEnd + "py_extr"),
+			    truthprt.auxdata<float>(sDetEnd + "pz_extr"));
+    }
+    else vecDetEnd_extr=vecDetEnd;
+
+    dp ->Fill((vecDetBegin.Mag() - vecDetEnd.Mag())*0.001);
+    dpt->Fill((vecDetBegin.Perp() - vecDetEnd.Perp())*0.001); 
+    dpx->Fill((vecDetBegin.Px() - vecDetEnd.Px())*0.001); 
+    dpy->Fill((vecDetBegin.Py() - vecDetEnd.Py())*0.001); 
+    dpz->Fill((vecDetBegin.Pz() - vecDetEnd.Pz())*0.001); 
+    dp_extr ->Fill((vecDetBegin_extr.Mag() - vecDetEnd_extr.Mag())*0.001);
+    dpx_extr ->Fill((vecDetBegin_extr.Px() - vecDetEnd_extr.Px())*0.001);
+    dpy_extr ->Fill((vecDetBegin_extr.Py() - vecDetEnd_extr.Py())*0.001);
+    dpz_extr ->Fill((vecDetBegin_extr.Pz() - vecDetEnd_extr.Pz())*0.001);
+ //2D
+    dp_vs_p  ->Fill( vecDetBegin.Mag()*0.001, (vecDetBegin.Mag() - vecDetEnd.Mag())*0.001);
+    dp_vs_phi->Fill( vecDetBegin.Phi(), (vecDetBegin.Mag() - vecDetEnd.Mag())*0.001);
+    dp_vs_eta->Fill( vecDetBegin.Eta(), (vecDetBegin.Mag() - vecDetEnd.Mag())*0.001);
+    dp_vs_eta_phi->Fill( vecDetBegin.Eta(), vecDetBegin.Phi(), (vecDetBegin.Mag() - vecDetEnd.Mag())*0.001);
+    p_vs_p->Fill( vecDetBegin.Mag()*0.001,  vecDetEnd.Mag()*0.001); 
+    p_extr_vs_p_extr->Fill( vecDetBegin_extr.Mag()*0.001,  vecDetEnd_extr.Mag()*0.001); 
+    dp_extr_vs_eta->Fill( vecDetBegin_extr.Eta(), (vecDetBegin_extr.Mag() - vecDetEnd_extr.Mag())*0.001);
+   
+    //multiple scattering
+    dR->Fill(vecDetBegin.DeltaR(vecDetEnd)); 
+    //2D
+    dR_vs_p  ->Fill( vecDetBegin.Mag()*0.001, vecDetBegin.DeltaR(vecDetEnd));
+    dR_vs_dp ->Fill((vecDetBegin.Mag() - vecDetEnd.Mag())*0.001,  vecDetBegin.DeltaR(vecDetEnd));
+    dR_vs_eta->Fill(vecDetBegin.Eta(),vecDetBegin.DeltaR(vecDetEnd));
+    dR_vs_phi->Fill(vecDetBegin.Phi(),vecDetBegin.DeltaR(vecDetEnd));
+    dR_vs_eta_phi->Fill(vecDetBegin.Eta(),vecDetBegin.Phi(),vecDetBegin.DeltaR(vecDetEnd));
+
+    dphi->Fill(vecDetBegin.DeltaPhi(vecDetEnd)); 
+    dtheta->Fill(vecDetBegin.Theta()-vecDetEnd.Theta());
+   
+    dAngle->Fill(vecDetBegin.Angle(vecDetEnd));
+}                         
+  
+}//namespace trk
--- Tracking/TrkValidation/TrkValHistUtils/TrkValHistUtils/PlotBase.h
+++ Tracking/TrkValidation/TrkValHistUtils/TrkValHistUtils/PlotBase.h
@@ -11,9 +11,9 @@
 #define TRKVALHISTUTILS_PLOTBASE_H
 
 #include <string>
-#include "TH1.h"
-#include "TH2.h"
-#include "TH3.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
 #include "TProfile.h"
 #include "TProfile2D.h"
 #include "TTree.h"
@@ -39,21 +39,21 @@ public:
   ///@{
 
   /// Book a TH1D histogram
-  TH1* Book1D(const std::string & name, const std::string & labels, int nBins, float start, float end, bool prependDir = true);
+  TH1F* Book1D(const std::string & name, const std::string & labels, int nBins, float start, float end, bool prependDir = true);
   /// Book a TH1D histogram using refHist as reference for number of bins and axis range
-  TH1* Book1D(const std::string & name, TH1* refHist, const std::string & labels, bool prependDir = true);
+  TH1F* Book1D(const std::string & name, TH1* refHist, const std::string & labels, bool prependDir = true);
 
   /// Book a TH2D histogram
-  TH2* Book2D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, bool prependDir = true);
+  TH2F* Book2D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, bool prependDir = true);
   /// Book a TH2D histogram using refHist as reference for number of bins and axis range
-  TH2* Book2D(const std::string & name, TH2* refHist, const std::string & labels, bool prependDir = true);
+  TH2F* Book2D(const std::string & name, TH2* refHist, const std::string & labels, bool prependDir = true);
   /// Book a TH2D histogram with variable x axis binning
-  TH2* Book2D(const std::string & name, const std::string & labels, int nBinsX, Double_t* binsX, int nBinsY, Double_t startY, Double_t endY, bool prependDir = true);
+  TH2F* Book2D(const std::string & name, const std::string & labels, int nBinsX, Double_t* binsX, int nBinsY, Double_t startY, Double_t endY, bool prependDir = true);
 
   /// Book a TH3D histogram
-  TH3* Book3D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, int nBinsZ, float startZ, float endZ, bool prependDir = true);
+  TH3F* Book3D(const std::string & name, const std::string & labels, int nBinsX, float startX, float endX, int nBinsY, float startY, float endY, int nBinsZ, float startZ, float endZ, bool prependDir = true);
   /// Book a TH3D histogram using refHist as reference for number of bins and axis range
-  TH3* Book3D(const std::string & name, TH3* refHist, const std::string & labels, bool prependDir = true);
+  TH3F* Book3D(const std::string & name, TH3* refHist, const std::string & labels, bool prependDir = true);
 
   /// Book a TProfile histogram
   TProfile* BookTProfile(const std::string &name, const std::string & labels, int nBinsX, float startX, float endX, float startY=-1, float endY=-1, bool prependDir = true);
