4aed54e014f2597686fe3d33498923a5076978e0
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:20:02 +0200

    'CMakeLists.txt' (G4SimTPCnv-00-02-01)
    	* Tagging G4SimTPCnv-00-02-01.
    	* Remove unused ARA converter dictionaries.

--- Simulation/G4SimCnv/G4SimTPCnv/G4SimTPCnv/G4SimTPCnvDict.h
+++ Simulation/G4SimCnv/G4SimTPCnv/G4SimTPCnv/G4SimTPCnvDict.h
@@ -5,7 +5,6 @@
 #ifndef G4SIMCNVDICT_H
 #define G4SIMCNVDICT_H
 
-#include "G4SimTPCnv/TrackRecordCollectionCnv_p2.h"
 #include "G4SimTPCnv/TrackRecord_p1.h"
 #include "G4SimTPCnv/TrackRecordCollection_p1.h"
 #include "G4SimTPCnv/TrackRecordCollection_p2.h"
@@ -14,10 +13,6 @@
 // for ROOT streamer
 #include "G4SimTPCnv/TrackRecord_p0.h"
 #include "G4SimTPCnv/TrackRecordStreamer_p0.h"
-
-struct GCCXML_DUMMY_INSTANTIATION_G4SIMTPCNV {
-  T_TPCnv<TrackRecordCollection,TrackRecordCollection_p2> m_cnv;
-};
 
 #endif
 
