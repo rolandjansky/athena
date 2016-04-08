db607b16237a6f444edfdcd2b6d5f0f874e441a3
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:05:40 +0200

    'CMakeLists.txt' (xAODTrigRingerCnv-00-00-02)

--- Event/xAOD/xAODTrigRingerCnv/cmt/setup.sh
+++ Event/xAOD/xAODTrigRingerCnv/cmt/setup.sh
@@ -1,14 +1,14 @@
-# echo "setup xAODTrigRingerCnv xAODTrigRingerCnv-00-00-01 in /tmp/jodafons/testRinger/test2/Event/xAOD"
+# echo "setup xAODTrigRingerCnv xAODTrigRingerCnv-r655703 in /afs/cern.ch/user/j/jodafons/workspace_ringer_bundle/Event/xAOD"
 
 if test "${CMTROOT}" = ""; then
-  CMTROOT=/cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc47-opt/19.1.3/CMT/v1r25p20140131; export CMTROOT
+  CMTROOT=/cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc48-opt/20.1.0/CMT/v1r25p20140131; export CMTROOT
 fi
 . ${CMTROOT}/mgr/setup.sh
 cmtxAODTrigRingerCnvtempfile=`${CMTROOT}/${CMTBIN}/cmt.exe -quiet build temporary_name`
 if test ! $? = 0 ; then cmtxAODTrigRingerCnvtempfile=/tmp/cmt.$$; fi
-${CMTROOT}/${CMTBIN}/cmt.exe setup -sh -pack=xAODTrigRingerCnv -version=xAODTrigRingerCnv-00-00-01 -path=/tmp/jodafons/testRinger/test2/Event/xAOD  -no_cleanup $* >${cmtxAODTrigRingerCnvtempfile}
+${CMTROOT}/${CMTBIN}/cmt.exe setup -sh -pack=xAODTrigRingerCnv -version=xAODTrigRingerCnv-r655703 -path=/afs/cern.ch/user/j/jodafons/workspace_ringer_bundle/Event/xAOD  -quiet -without_version_directory -no_cleanup $* >${cmtxAODTrigRingerCnvtempfile}
 if test $? != 0 ; then
-  echo >&2 "${CMTROOT}/${CMTBIN}/cmt.exe setup -sh -pack=xAODTrigRingerCnv -version=xAODTrigRingerCnv-00-00-01 -path=/tmp/jodafons/testRinger/test2/Event/xAOD  -no_cleanup $* >${cmtxAODTrigRingerCnvtempfile}"
+  echo >&2 "${CMTROOT}/${CMTBIN}/cmt.exe setup -sh -pack=xAODTrigRingerCnv -version=xAODTrigRingerCnv-r655703 -path=/afs/cern.ch/user/j/jodafons/workspace_ringer_bundle/Event/xAOD  -quiet -without_version_directory -no_cleanup $* >${cmtxAODTrigRingerCnvtempfile}"
   cmtsetupstatus=2
   /bin/rm -f ${cmtxAODTrigRingerCnvtempfile}
   unset cmtxAODTrigRingerCnvtempfile
