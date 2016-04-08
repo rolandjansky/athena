755f35d05ec4b15766315bfa60b6e9146ea8a3bc
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:38:52 +0200

    'CMakeLists.txt' (FastChainPileup-00-00-25-01)
    	* minor update of RTT tests
    	* Tagging as FastChainPileup-00-00-25
    2016-03-12 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fix reco RTT commands
    	* Tagging as FastChainPileup-00-00-24
    2016-03-11 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part2
    	* added new tests for reco jobs
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-23
    2016-03-09 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part1
    	* added new jobs for sim, digi and reco
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-22
    2016-02-29 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fixed sim_reg_test_fastchain.py:
    	* to work properly when release set to be 'current'
    	* to take the latest succesful result instead of requiring it to be at least 1 day old ('yesterday' or older)
    	* Tagging as FastChainPileup-00-00-21
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: f69e66a983e043ffb2f5a2d36ebaaf6418fa6e26

---
+++ Simulation/FastSimulation/FastChainPileup/scripts/sim_reg_test_fastchain.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+import os,sys
+import time,datetime
+
+__author__ = 'FFF'
+__doc__    = 'FFF'
+
+
+dryrun=False
+
+
+def checkStatus(path,days=1):
+    if not os.path.exists(path+"/jobExitCode.txt"):
+        print path+"/jobExitCode.txt","missing - skipping this nightly"
+        return False,999
+    f = open(path+"/jobExitCode.txt")
+    print "Checking job status..."
+    print "Found "+path+"/jobExitCode.txt"
+    file_status=False
+    for line in f:
+        if line.strip()=='0':
+            file_status=True
+            #print line,
+        if "<jobSuccess>True</jobSuccess>" in line:
+            file_status=True
+            #print line,
+        elif "<jobSuccess>False</jobSuccess>" in line:
+            file_status=False
+            #print line,
+    if file_status: print "Job succeeded in this nightly."
+    else:
+        print "Job failed in this nightly."
+        return False,999
+    #check date
+    #<timeStamp>1352062929</timeStamp>
+    if os.path.exists(path+"/rttjobinfo.xml"):
+        import re
+        f = open(path+"/rttjobinfo.xml")
+        for l in f:
+            #<timeStamp>1352062929</timeStamp>#
+            m=re.search('<timeStamp>([0-9]*)</timeStamp>',l)
+            if m is not None:
+                #print m.groups()
+                ts=int(m.groups()[0])
+                import datetime as dt
+                yes=dt.date.fromtimestamp(ts)
+                now=dt.date.today()
+                days=(now-yes).days
+                if days>6:
+                    # to old, ignore
+                    print 'WARNING results found in '+path+' are more than a week old'
+#                    file_status=False
+    return file_status,days
+
+###get the recent tag###
+def get_recent(path,pathsplit):
+    print "---------------------------------------------------------------------------------------"
+    pathlist=path.split('/')
+    pos_index=0
+    release_list=[]
+    for i,a in enumerate(pathsplit):
+        if a.find("rel_")>=0:
+            pos_index=i
+            release_list=a.split('_')
+    todays_release=pathlist[pos_index]
+    recent_days=999;
+    recent_index=0;
+    for i in xrange(7):
+        release_tag=release_list[0]+"_"+str(i)
+#        print release_tag
+        pathlist[pos_index]=release_tag
+        path_refill="/".join(pathlist)
+#        print path_refill
+        print "Checking for results for "+release_tag+"..."
+        status_tmp,days_tmp=checkStatus(path_refill)
+        if status_tmp:
+            print "==> Nightly: "+release_tag+"  Age: "+str(days_tmp)+" days"
+            if days_tmp>=0 and days_tmp<recent_days:
+                recent_days=days_tmp
+                recent_index=i
+        else:
+            print "==> Nightly: "+release_tag+"  Age: N/A"
+        print "---------------------------------------------------------------------------------------"
+    if recent_days>998:
+        print "=====> Could not find another release to compare to. Returning today's nightly ("+todays_release+") as the reference."
+        return todays_release
+    good_rel_refease="rel_"+str(recent_index)
+    print "=====> "+good_rel_refease+" is the most recent nightly to use as reference."
+    return good_rel_refease
+
+###Add EOS prefix###
+def eoscp(filename,jobname,tag,store_tag,release):
+    ##rel_list=["0","1","2","3","4","5","6"]
+    current=os.getcwd()
+    if os.environ.has_key('LS_SUBCWD'):
+        current=os.environ['LS_SUBCWD']
+    #current='/afs/cern.ch/atlas/project/RTT/prod/Results/rtt/rel_2/19.2.X.Y-VAL/build/x86_64-slc6-gcc47-opt/offline/ISF_Validation/ttbar_ISF_G4_sim_Reg'
+    argv=current.split('/')
+    argv.pop()
+    ##for arg in argv:
+    ##    if arg.find("rel_")==0:
+    ##        arg_list=arg.split("_")
+    ##        #print arg_list
+    ##        if tag=="today":
+    ##            release=arg
+    ##            print release
+    ##        elif tag=="yesterday":
+    ##            release=arg_list[0]+"_"+rel_list[int(arg_list[1])-1]
+    ##            print release
+    postfix='/'.join(argv[-3:])
+    #middle='/'.join(argv[-6:-5])
+    middle=argv[-5]
+    pathlist=["prod","batch"]#This 2nd option should make the script work on the RTT test instance.
+#    status=0
+    for path in pathlist:
+        prefix="root://eosatlas//eos/atlas/atlascerngroupdisk/proj-sit/rtt/"+path+"/rtt"
+        ##print "postfix: "+postfix
+        ##print "middle: "+middle
+        ##print "prefix: "+prefix
+        ##print "jobname: "+jobname
+        ##print "filename: "+filename
+        path=prefix+'/'+release+'/'+middle+'/'+postfix+'/'+jobname+'/'+filename
+        print "Path to check on eos: "+path
+        if store_tag!='':
+            command_cp="xrdcp %s %s.%s.root"%(path,tag,store_tag)
+        else:
+            command_cp="xrdcp %s %s.%s"%(path,tag,filename)
+        print command_cp
+        status=os.system(command_cp)
+        if status==0: return status
+    return status
+
+## main
+if __name__ == "__main__":
+    print '------------------usage-----------------'
+    print 'sim_reg_test_fastchain.py [ref package] [ref rel] [ref job name] [ref filename] [mon package] [mon rel] [mon job name] [mon filename]'
+    print 'packages supported are ISF_Validation and FastChainPileup'
+    print 'to use current release, put "current" as a release argument'
+    print 'if job fails for today`s ref/mon, the latest succeeded job will be taken'
+    print '----------------------------------------'
+    
+    print 'Got input arguments:'
+    print 'reference:'
+    print 'package: ', sys.argv[1]
+    print 'release: ', sys.argv[2]
+    print 'job: ', sys.argv[3]
+    print 'filename: ', sys.argv[4]
+    print 'monitored:'
+    print 'package: ', sys.argv[5]
+    print 'release: ', sys.argv[6]
+    print 'job: ', sys.argv[7]
+    print 'filename: ', sys.argv[8]
+    if sys.argv[1]!='ISF_Validation' and sys.argv[1]!='FastChainPileup' :
+      print 'ERROR: unsupported reference package.'
+    if sys.argv[5]!='ISF_Validation' and sys.argv[5]!='FastChainPileup' :
+      print 'ERROR: unsupported monitored package.'
+    #establish path to monitored release, rel num is just dummy at this stage
+    pwdmon=''
+    prepath='/afs/cern.ch/atlas/project/RTT/prod/Results/rtt/rel_0/'
+    #TODO: make postpath configurable
+    postpath='/build/x86_64-slc6-gcc49-opt/offline/'
+    currel='NOREL'
+    if sys.argv[6] == 'current' or sys.argv[2] == 'current' :
+      curdir=os.getcwd()
+      if os.environ.has_key('LS_SUBCWD'): 
+	curdir= os.environ['LS_SUBCWD']
+      #!TEST
+      #curdir='/afs/cern.ch/atlas/project/RTT/prod/Results/rtt/rel_2/mig13/build/x86_64-slc6-gcc49-opt/offline/FastChainPileup/'
+      #!END TEST
+      folders=curdir.split('/')
+      print folders
+      for idx, folder in enumerate(folders):
+	if 'rel_' in folder :
+	  try:
+	    currel = folders[idx+1]
+	  except IndexError:
+	    print 'ERROR: Could not find current release in the path after /rel_*/'
+      if currel == 'NOREL' :
+	print 'ERROR: Could not find current release in the path. Setting release to "NOREL" and going to crash.'
+	    
+    relmon=sys.argv[6]
+    if sys.argv[6] == 'current' : 
+      relmon = currel  
+    pwdmon=prepath + relmon + postpath
+    #establish path to reference release, rel num is just dummy at this stage  
+    pwdref=''
+    prepath='/afs/cern.ch/atlas/project/RTT/prod/Results/rtt/rel_0/'
+    postpath='/build/x86_64-slc6-gcc49-opt/offline/'
+    relref=sys.argv[2]
+    if sys.argv[2] == 'current' : 
+      relref=currel
+    pwdref=prepath + relref + postpath
+   
+    argvref=pwdref.split('/')
+    argvref.pop()
+    argvref.append(sys.argv[1])
+    argvref.append(sys.argv[3])
+    print 'argvref ',argvref 
+    corr_path_ref='/'.join(argvref)
+    print 'reference path:', corr_path_ref
+    ## Find a release to use as a reference
+    print '###### Get the most recent successful reference job ######'
+    good_rel_ref=get_recent(corr_path_ref,argvref)
+    
+    print ''
+    print ''
+    print ''
+    print ''
+    
+    argvmon=pwdmon.split('/')
+    argvmon.pop()
+    argvmon.append(sys.argv[5])
+    argvmon.append(sys.argv[7])
+    print 'argvmon ',argvmon 
+    corr_path_mon='/'.join(argvmon)
+    print 'monitored path:', corr_path_mon
+    print '###### Get the most recent successful monitored job ######'
+    good_rel_mon=get_recent(corr_path_mon,argvmon)  
+
+    posref=9
+    for i,a in enumerate(argvref):
+        if a.find("rel_")>=0 :
+            posref = i
+    
+    posmon=9
+    for i,a in enumerate(argvmon):
+        if a.find("rel_")>=0 :
+            posmon = i
+
+    argvmon[posmon]=good_rel_mon
+    monPath='/'.join(argvmon)
+    
+    argvref[posref]=good_rel_ref
+    refPath='/'.join(argvref)
+
+    print "Releases to compare: "+good_rel_ref+", "+good_rel_mon
+    print refPath
+    print monPath
+
+    # look for suffix
+    file_name='.root'
+    store_tag=''
+
+    # make sure ISFBroker.root is not confused with truth*.root
+    compfilesref=['ISFBroker.root']
+    compfilesmon=['ISFBroker.root']
+    #if len(sys.argv)>3:
+        ## got extended argument list create file list
+        #for f in sys.argv[3:]:
+            #if f.endswith(file_name):
+                #print "Add file %s in searching list."%(f)
+                #compfiles.append(f)
+            #else:
+                #print "Find tag set, use common tag: %s."%(f)
+                #store_tag=f
+                
+    compfilesref.append(sys.argv[4])
+    compfilesmon.append(sys.argv[8])
+    print 'compfilesref:',compfilesref
+    print 'compfilesref:',compfilesmon
+    file_list_ref=compfilesref[:]
+    print file_list_ref
+    file_list_val=compfilesmon[:]
+    try:
+        print "Confirming monitored job status..."
+        file_status_val,days=checkStatus(monPath)
+
+        if file_status_val==True:
+            #print "Success"
+            file_root=False
+            for f in  os.listdir(monPath):
+                if f.endswith(file_name) and f != 'yesterday.root' and f!= 'today.root':
+                    print f
+                    name,suffix=f.split('.',1)
+                    if f in compfilesmon:
+                        suffix=f
+                        print 'f1'
+                    elif len(compfilesmon)>=2:
+                        continue
+                    if store_tag!='':
+                        cmd = 'cp'+' '+monPath+'/'+f+' '+'today.'+store_tag+".root"
+                    else:
+                        cmd = 'cp'+' '+monPath+'/'+f+' '+'today.'+suffix
+                    print cmd
+                    if not dryrun:
+                        os.system(cmd)
+                    file_root=True
+                    if f in file_list_val:
+                        file_list_val.remove(f)
+                        print file_list_val
+            if 'ISFBroker.root' in file_list_val:
+                file_list_val.remove('ISFBroker.root')
+            if len(file_list_val)!=0:
+                print "The following files were not found in the output directory of today's job: "+str(file_list_val)+", will try to copy them from EOS instead."
+                for val_f in file_list_val:
+                    if eoscp(val_f,sys.argv[8],"today",store_tag,good_rel_mon)!=0 :
+                        print "File does not exist in EOS."
+                    else:
+                        file_list_val.remove(val_f)
+
+
+            file_status_ref=False
+            file_list_ref_orig = file_list_ref
+            for i in range(1,5):
+                print "Confirming reference job status..."
+                file_status_ref,days_ref=checkStatus(refPath)
+                if file_status_ref==True:
+                    #print("Looking for .root files to compare...")
+                    file_root=False
+                    for f in os.listdir(refPath):
+                        if f.endswith(file_name) and f != 'yesterday.root' and f!= 'today.root':
+                            print f
+                            print compfilesref
+                            name,suffix=f.split('.',1)
+                            #if name in compfiles:
+                            if f in compfilesref:
+                                suffix=f
+                                print 'f2'
+                            elif len(compfilesref)>=2:
+                                continue
+                            if store_tag!='':
+                                cmd = 'cp'+' '+refPath+'/'+f+' '+'yesterday.'+store_tag+'.root'
+                            else:
+                                cmd = 'cp'+' '+refPath+'/'+f+' '+'yesterday.'+suffix
+                            print cmd
+                            if not dryrun:
+                                os.system(cmd)
+                            file_root=True
+                            if f in file_list_ref:
+                                file_list_ref.remove(f)
+                                print file_list_ref
+                    if 'ISFBroker.root' in file_list_ref:
+                        file_list_ref.remove('ISFBroker.root')
+                    if len(file_list_ref)!=0:
+                        print "The following files were not found in the output directory of "+good_rel_ref+"'s job: "+str(file_list_ref)+", will try to copy them from EOS instead."
+                        for ref_f in file_list_ref :
+                            if eoscp(ref_f,sys.argv[4],"yesterday",store_tag,good_rel_ref)!=0:
+                                print "File does not exist in EOS."
+                            else:
+                                file_list_ref.remove(ref_f)
+
+                if len(file_list_ref)==0:
+                    break
+                else:
+                    ##clean up files
+                    print "Could not find a full set of files for "+good_rel_ref+", so removing any we did find."
+                    if(len(file_list_ref)+1) < len(file_list_ref_orig): os.system('rm yesterday.*')
+                    file_list_ref = file_list_ref_orig
+                    file_status_ref=False
+                    previous_rel = good_rel_ref
+                    if good_rel_ref == 'rel_0':
+                        good_rel_ref = 'rel_6'
+                    else:
+                        good_rel_ref = 'rel_%d' % (int(good_rel_ref.split('_')[1])-1)
+
+                    argvref[pos]=good_rel_ref
+                    refPath='/'.join(argvref)
+                    print "As we failed to find all required files in "+previous_rel+", trying again with "+good_rel_ref
+                    print monPath
+                    print refPath
+
+            if not file_status_ref==True:
+                print("Failed to find a usable reference nightly!")
+            else:
+                # make fileGrepper happy
+                print "leaving with code 0"
+
+        elif file_status_val==False:
+            print "Failed to find expected files in the path for today's nightly!"
+
+    except (OSError,IOError):
+        print "No running in this release!"
---
+++ Simulation/FastSimulation/FastChainPileup/src/MultiParticleGunPileup.cxx
@@ -0,0 +1,185 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// MultiPY8Pileup.cxx - extension of GenModule_i to generate multiple pileup
+// events in one go for fast simulation chain
+// Richard Hawkings, started 20/2/15, + contributions from Vladimir Lyubushkin
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#include "CLHEP/Random/RandPoisson.h"
+#include "FastChainPileup/MultiParticleGunPileup.h"
+
+MultiParticleGunPileup::MultiParticleGunPileup(const std::string& name, ISvcLocator* pSvcLocator) :
+  GenModule( name, pSvcLocator ),
+  m_ngen(0),
+  m_nbad(0),
+  // m_evnumber(1),
+  m_randomEngine(NULL),
+  m_file(NULL),
+  m_htgPileupProfile(NULL),
+  m_htgPileupMu(NULL),
+  m_htgPileupEvents(NULL)
+{
+  declareProperty("NCollPerEvent",m_ncollevent=20,"Collisons per event (-1 to use profile)");
+  declareProperty("PileupProfile",m_pileupProfile,"Pileup profile array");
+  declareProperty("MultBCID",m_multbcid,"Pileup multiplier per BCID array");
+  declareProperty("HistFile",m_filename="","Filename for histograms");
+}
+
+MultiParticleGunPileup::~MultiParticleGunPileup() {
+
+}
+
+StatusCode MultiParticleGunPileup::genInitialize() {
+  ATH_MSG_INFO( "In MultiParticleGunPileup::genInitialize" );
+
+  // should be initialized after Pythia8_i::genInitialize() ???
+  // m_randomEngine = atRndmGenSvc().GetEngine( Pythia8_i::pythia_stream );
+
+  // if filename set, initialise histograms for mu/nevt profiles
+  if (m_filename!="") {
+    m_file = new TFile( m_filename.c_str(), "recreate" );
+    m_file->cd();
+    m_htgPileupProfile = new TH1D( "pileupProfile", "input pileupProfile", 100, 0., 100. );
+    m_htgPileupMu = new TH1D( "generatedMu", "generated pileup mu", 100, 0., 100. );
+    m_htgPileupEvents = new TH1D( "generatedEvents", "generated pileup events", 100, 0., 100. );
+  }
+
+  m_pileupProfileIntegral.clear();
+  m_pileupProfileIntegral.reserve( m_pileupProfile.size() );
+  for ( std::vector< int >::iterator itr = m_pileupProfile.begin(); itr != m_pileupProfile.end(); ++itr ) {
+    m_pileupProfileIntegral.push_back( std::accumulate( m_pileupProfile.begin(), itr + 1, 0. ) );
+    if (m_file)
+      m_htgPileupProfile->SetBinContent( std::distance( m_pileupProfile.begin(), itr ) + 1, *itr );
+  }
+
+  if ( m_ncollevent > 0 ) {
+    ATH_MSG_INFO("Generate fixed number of " << m_ncollevent << " minbias collisions per bunch crossing");
+  }
+  else {
+    ATH_MSG_INFO("Generate varying minbias collisions per event according to given pileup profile");
+  }
+  m_evts.reserve( 50 );
+
+  ATH_MSG_INFO("Simulate " << m_multbcid.size() << " bunch crossings per event");
+  int ibc=1;
+  for (std::vector<float>::const_iterator itr=m_multbcid.begin();
+       itr!=m_multbcid.end();++itr,++ibc) {
+    ATH_MSG_INFO("Bunch crossing BCID " << ibc << " mu multiplier " << *itr);
+  }
+
+  //Initialize the ParticleGun generator
+  partSampler = new ParticleSampler(new CyclicSeqSampler("-11,11,"), new PtEtaMPhiSampler(10000.,10000.,-3,3),1);
+
+  // exit via the base class initialisation
+  return StatusCode::SUCCESS;
+}
+
+StatusCode MultiParticleGunPileup::callGenerator() {
+  // initialise random generators the first time - delayed after Pythia init
+  if ( !m_randomEngine)
+    m_randomEngine = atRndmGenSvc().GetEngine( "ParticleGun" );
+
+  // decide how many events to generate
+
+  int muval = ( m_ncollevent > 0 ? m_ncollevent : nPileupEvents() );
+  if (m_file) m_htgPileupMu->Fill(muval+0.1);
+  m_evts.clear();
+
+  // loop over all BCID - starting from 1 for in-time pileup
+  int bcid=1;
+  for (std::vector<float>::const_iterator bcitr=m_multbcid.begin();
+       bcitr!=m_multbcid.end();++bcitr,++bcid) {
+    // actual number of minbias in this BCID
+    // Poisson around instantaneous_mu * multiplier value
+    const int nevtraw=CLHEP::RandPoisson::shoot(muval);
+    if (m_file) m_htgPileupEvents->Fill(nevtraw+0.1);
+    const int nevt=(float)nevtraw*(*bcitr);
+
+    for (int ievt=0;ievt<nevt;++ievt) {
+      ATH_MSG_DEBUG("Request generation of event " << ievt << " of " << nevt
+		    << " for BCID " << bcid);
+
+//--------------------------- CALL GUN HERE
+      HepMC::GenEvent* evt=new HepMC::GenEvent();
+      evt->weights().push_back(1.0);
+      //evt->set_event_number(ievt); //Maybe dangerous to do this since the first event gets stacked last
+      // Make and fill particles
+      std::vector<SampledParticle> particles = partSampler->shoot();
+      for (auto p : particles){
+	// Debug printout of particle properties
+	std::cout << ievt << " DEBUG0," <<  p.m_pid << ", " << p.m_mom.E()<< ", " << p.m_mom.Pt()<< ", " << p.m_mom.M() << std::endl;
+	std::cout << ievt << " DEBUG1 (px,py,pz,E) = (" << p.m_mom.Px()<< ", " << p.m_mom.Py()<< ", " << p.m_mom.Pz()<< ", " << p.m_mom.E() <<")" << std::endl;
+	std::cout << ievt << " DEBUG2 (eta,phi,pt,m) = ()" << p.m_mom.Eta()<< ", " << p.m_mom.Phi()<< ", " << p.m_mom.Pt()<< ", " << p.m_mom.M() <<")" << std::endl;
+	std::cout << ievt << " DEBUG3 (x,y,z,t) = ()" << p.m_pos.X()<< ", " << p.m_pos.Y()<< ", " << p.m_pos.Z()<< ", " << p.m_pos.T() << ")" << std::endl;
+	// Make particle-creation vertex
+	// TODO: do something cleverer than one vertex per particle?
+	HepMC::FourVector pos(p.m_pos.X(), p.m_pos.Y(), p.m_pos.Z(), p.m_pos.T());
+	HepMC::GenVertex * gv = new HepMC::GenVertex(pos);
+	evt->add_vertex(gv);
+	// Make particle with status == 1
+	HepMC::FourVector mom(p.m_mom.Px(), p.m_mom.Py(), p.m_mom.Pz(), p.m_mom.E());
+	HepMC::GenParticle * gp = new HepMC::GenParticle;
+	gp->set_status(1);
+	gp->set_pdg_id(p.m_pid);
+	gp->set_momentum(mom);
+	if (p.m_mass)
+	  gp->set_generated_mass(p.m_mass);
+	gv->add_particle_out(gp);
+      }
+//-----------------------------------------
+
+      // change the process ID to incorporate the BCID * 10000
+      int pid=evt->signal_process_id();
+      evt->set_signal_process_id(pid+10000*bcid);
+      ATH_MSG_DEBUG("Signal process ID " << pid << " set to " <<
+		    evt->signal_process_id() << " for BCID " << bcid);
+      m_evts.push_back(evt);
+      ++m_ngen;
+    }
+  }
+  ATH_MSG_DEBUG("callGenerator finished with " << m_evts.size() <<
+		" pileup events in buffer");
+  return StatusCode::SUCCESS;
+}
+
+StatusCode MultiParticleGunPileup::fillEvt(HepMC::GenEvent* evt) {
+  int nbuf=m_evts.size();
+  if (nbuf>1) {
+    // send extra events into McEventCollection via backdoor
+    for (int i=1;i<nbuf;++i) {
+      events()->push_back(m_evts[i]);
+    }
+  }
+  // send first event back via usual method
+  // have to copy it to the passed in GenEvent
+  if (nbuf>0) {
+    *evt=*m_evts[0];
+    delete m_evts[0];
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode MultiParticleGunPileup::genFinalize() {
+  ATH_MSG_INFO("MultiParticleGunPileup finished with " << m_ngen << " good events and " << m_nbad <<  " failures" );
+
+  if (m_file) {
+    m_file->cd();
+    m_file->Write();
+    m_file->Close();
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+
+int MultiParticleGunPileup::nPileupEvents() {
+  // return the instantaneous mu value according to the profile
+  double threshold = m_randomEngine->flat() * m_pileupProfileIntegral.back();
+  std::vector< double >::iterator itr = std::lower_bound( m_pileupProfileIntegral.begin(), m_pileupProfileIntegral.end(), threshold );
+  return std::distance( m_pileupProfileIntegral.begin(), itr );
+}
