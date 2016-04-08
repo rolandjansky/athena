dfd4738ff919f88f4a3f811646628dfab52c5c6b
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:38:52 +0200

    'CMakeLists.txt' (FastChainPileup-00-00-25-01)
    	* minor update of RTT tests
    	* Tagging as FastChainPileup-00-00-25
    2016-03-12 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fix reco RTT commands
    	* Tagging as FastChainPileup-00-00-24
    2016-03-11 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part2
    	* added new tests for reco jobs
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-23
    2016-03-09 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part1
    	* added new jobs for sim, digi and reco
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-22
    2016-02-29 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fixed sim_reg_test_fastchain.py:
    	* to work properly when release set to be 'current'
    	* to take the latest succesful result instead of requiring it to be at least 1 day old ('yesterday' or older)
    	* Tagging as FastChainPileup-00-00-21
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/FastSimulation/FastChainPileup/FastChainPileup/MultiParticleGunPileup.h
@@ -0,0 +1,61 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef FASTCHAINPILEUP_MULTIPARTICLEGUN8_PILEUP_H
+#define FASTCHAINPILEUP_MULTIPARTICLEGUN8_PILEUP_H
+
+// MultiParticleGunPileup.h - generates multiple
+// pileup events in one go for fast simulaton chain
+
+// Olivier Arnaez, started 17/12/15
+
+#include <string>
+#include <TROOT.h>
+#include <TFile.h>
+#include <TH1D.h>
+
+#include "GeneratorModules/GenModule.h"
+#include "FastChainPileup/Samplers.h"
+
+namespace HepMC {
+  class GenEvent;
+}
+
+class MultiParticleGunPileup : public GenModule {
+
+  public:
+    MultiParticleGunPileup( const std::string &name, ISvcLocator *pSvcLocator );
+    ~MultiParticleGunPileup();
+
+    virtual StatusCode genInitialize();
+    virtual StatusCode genFinalize();
+
+    virtual StatusCode callGenerator();
+    virtual StatusCode fillEvt( HepMC::GenEvent *event );
+
+  private:
+    int nPileupEvents();                // decide how much pileup events will be generated
+    // parameters
+    int m_ncollevent;                  // fixed number of collisions per event
+    std::vector< int > m_pileupProfile;  // pileup mu-profile
+    std::vector< float > m_multbcid;     // mu multipliers per BCID
+    std::string m_filename;            // filename for histogram output
+
+    int m_ngen;                         // total number of events generated
+    int m_nbad;                         // total number of failed events
+    // int m_evnumber;                     // event sequence number
+
+    std::vector< HepMC::GenEvent * > m_evts;   // vector of generated events
+
+    CLHEP::HepRandomEngine *m_randomEngine;
+
+    std::vector< double > m_pileupProfileIntegral;
+
+    TFile *m_file;
+    TH1D *m_htgPileupProfile, *m_htgPileupMu, *m_htgPileupEvents;
+
+    ParticleSampler * partSampler;
+};
+
+#endif
