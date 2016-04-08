ea21d3aa42d5e814886e7e4685ee24d7e261622a
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:17:37 +0200

    'CMakeLists.txt' (TrigT1CaloCondSvc-00-02-10)
    	* more flexibility for passing arguments to the AbstractL1CaloPersistentCondition
    	* Tag as TrigT1CaloCondSvc-00-02-10
    
            * Tag (after testing) as TrigT1CaloCondSvc-00-02-02

--- Trigger/TrigT1/TrigT1CaloCondSvc/TrigT1CaloCondSvc/L1CaloCondSvc.h
+++ Trigger/TrigT1/TrigT1CaloCondSvc/TrigT1CaloCondSvc/L1CaloCondSvc.h
@@ -1,3 +1,5 @@
+// -*- C++ -*-
+
 /*
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
@@ -32,92 +34,90 @@
 class L1CaloCondSvc :  public AthService
 {
 public:
-    L1CaloCondSvc(const std::string& name, ISvcLocator* svc);
-    virtual ~L1CaloCondSvc();
+  L1CaloCondSvc(const std::string& name, ISvcLocator* svc);
+  virtual ~L1CaloCondSvc();
 
-    virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface);
+  virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface);
 
-    static const InterfaceID& interfaceID();
+  static const InterfaceID& interfaceID();
 
-    virtual StatusCode initialize();
-    virtual StatusCode finalize();
+  virtual StatusCode initialize();
+  virtual StatusCode finalize();
 
 
-    /**
-        To store a condition object into the COOL DB
+  /**
+     To store a condition object into the COOL DB
 
-        @warning Only objects that implement the IL1CaloPersistenceCapable interface
-        can be stored using this method
-    */
-    template <typename T>
-    StatusCode store(const T*& pobj, const std::string& key, const std::string& folder, const std::string& tag) const;
+     @warning Only objects that implement the IL1CaloPersistenceCapable interface
+     can be stored using this method
+  */
+  template <typename T>
+  StatusCode store(const T*& pobj, const std::string& key, const std::string& folder, const std::string& tag) const;
 
-    /**
-        To retrieve a condition object from the COOL DB
+  /**
+     To retrieve a condition object from the COOL DB
 
-        @warning Only objects that implement the IL1CaloPersistenceCapable interface
-        can be retrieved using this method
-    */
-    template <typename T>
-    StatusCode retrieve(T*& pobj, const std::string& key = "");
+     @warning Only objects that implement the IL1CaloPersistenceCapable interface
+     can be retrieved using this method
+  */
+  template <typename T>
+  StatusCode retrieve(T*& pobj, const std::string& key = "");
 
-    /**
-       To retrieve a condition object from the COOL DB with custom folder mapping.
-       The second argument is a map of TrigT1CaloCalibConditions/L1CaloXXXContainer::eCoolFolders to
-       a string given the correct folder path.
+  /**
+     To retrieve a condition object from the COOL DB with custom folder mapping.
 
-        @warning Only objects that implement the IL1CaloPersistenceCapable interface
-        can be retrieved using this method
-    */
-    template <typename T, typename FolderType>
-    StatusCode retrieve(T*& pobj, const std::map<FolderType, std::string>& folderKeyMap, const std::string& key = "");
+     @warning Only objects that implement the IL1CaloPersistenceCapable interface
+     can be retrieved using this method
+  */
+  template <typename T, typename FolderMapping>
+  StatusCode retrieve(T*& pobj, const FolderMapping& folderKeyMap, const std::string& key = "");
 
-	virtual StatusCode updateConditions(IOVSVC_CALLBACK_ARGS);
+  virtual StatusCode updateConditions(IOVSVC_CALLBACK_ARGS);
 
 
 private:
-    ServiceHandle<StoreGateSvc> m_detStore;
-    ServiceHandle<IIOVRegistrationSvc> m_regSvc;
+  ServiceHandle<StoreGateSvc> m_detStore;
+  ServiceHandle<IIOVRegistrationSvc> m_regSvc;
 
-    // properties
-    std::string m_streamName;
+  // properties
+  std::string m_streamName;
 
-    bool m_bWriteNewChannels;
+  bool m_bWriteNewChannels;
 
-    // IOV range
-    unsigned int m_iovBeginRun;
-    unsigned int m_iovEndRun;
-    unsigned int m_iovBeginLB;
-    unsigned int m_iovEndLB;
-    uint64_t m_iovBeginTime;
-    uint64_t m_iovEndTime;
-    bool m_bUseTimeStamp;
+  // IOV range
+  unsigned int m_iovBeginRun;
+  unsigned int m_iovEndRun;
+  unsigned int m_iovBeginLB;
+  unsigned int m_iovEndLB;
+  uint64_t m_iovBeginTime;
+  uint64_t m_iovEndTime;
+  bool m_bUseTimeStamp;
 
-    // function for the non-differing parts of the two retrieve functions above
-    template <typename T>
-    StatusCode retrieveImpl(T*& pobj, const std::string& key);
+  // function for the non-differing parts of the two retrieve functions above
+  template <typename T>
+  StatusCode retrieveImpl(T*& pobj, const std::string& key);
 
-    //
-    //StatusCode registerCondition(std::string key, IL1CaloPersistenceCapable* pObj);
-//    template <typename T>
-//    StatusCode registerCondition(std::string key, T*& pobj); // to be replaced
-    template <typename T>
-    StatusCode registerCondition(T*& pobj);
+  //
+  //StatusCode registerCondition(std::string key, IL1CaloPersistenceCapable* pObj);
+  //    template <typename T>
+  //    StatusCode registerCondition(std::string key, T*& pobj); // to be replaced
+  template <typename T>
+  StatusCode registerCondition(T*& pobj);
 
-    std::map<std::string, const DataHandle<AthenaAttributeList>* > m_mDataHandleAttrList;
-    std::map<std::string, const DataHandle<CondAttrListCollection>* > m_mDataHandleAttrListColl;
-    //std::map<std::string, IL1CaloPersistenceCapable*> m_mConditions;
-    std::map<std::string, std::vector<IL1CaloPersistenceCapable*> > m_mConditions;
+  std::map<std::string, const DataHandle<AthenaAttributeList>* > m_mDataHandleAttrList;
+  std::map<std::string, const DataHandle<CondAttrListCollection>* > m_mDataHandleAttrListColl;
+  //std::map<std::string, IL1CaloPersistenceCapable*> m_mConditions;
+  std::map<std::string, std::vector<IL1CaloPersistenceCapable*> > m_mConditions;
 
 };
 
 inline const InterfaceID& L1CaloCondSvc::interfaceID() {
-    static const InterfaceID IID_L1CaloCondSvc("L1CaloCondSvc",1,0);
-    return IID_L1CaloCondSvc;
+  static const InterfaceID IID_L1CaloCondSvc("L1CaloCondSvc",1,0);
+  return IID_L1CaloCondSvc;
 }
 
 #ifndef TRIGT1CALOCONDSVC_L1CALOCONDSVC_ICC
- #include "TrigT1CaloCondSvc/L1CaloCondSvc.icc"
+#include "TrigT1CaloCondSvc/L1CaloCondSvc.icc"
 #endif
 
 #endif
