3a17d661f754977a349fa2bc18b499eb04f1491e
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:10:20 +0200

    'CMakeLists.txt' (TrkDistortedSurfaces-01-00-03)
    	* Tagging TrkDistortedSurfaces-01-00-03.
    	* Generate dictionaries.
    
    
    Former-commit-id: 80314e018e76b5648e4a6f439fc90590d869030d

---
+++ Tracking/TrkDetDescr/TrkDistortedSurfaces/TrkDistortedSurfaces/TrkDistortedSurfacesDict.h
@@ -0,0 +1,14 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRKDISTORTEDSURFACES_TRKDISTORTEDSURFACESDICT_h
+#define TRKDISTORTEDSURFACES_TRKDISTORTEDSURFACESDICT_h
+
+#include "TrkDistortedSurfaces/DistortionDescriptor.h"
+#include "TrkDistortedSurfaces/LineSaggingDescriptor.h"
+#include "TrkDistortedSurfaces/DistortedSurface.h"
+#include "TrkDistortedSurfaces/SaggedLineSurface.h"
+
+#endif // not TRKDISTORTEDSURFACES_TRKDISTORTEDSURFACESDICT_h
+
