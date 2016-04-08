2d36d3cd68627bf012eead24c1308dbb5b88a98c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:51:54 +0200

    'CMakeLists.txt' (MuonLinearSegmentMakerUtilities-01-00-05)
    	* Comply with ATLAS naming conventions
    	* tagging MuonLinearSegmentMakerUtilities-01-00-05
    
    2015-11-23 Nathan Bernard
    	* compatibility update for MuonIdHelpers
    	* tagging MuonLinearSegmentMakerUtilities-01-00-04
    
    2015-07-15 Nathan Bernard
    	* update for compatibility with MuonClusterSegmentMakerTools-00-00-01
    	* tagging MuonLinearSegmentMakerUtilities-01-00-03
    
    
    Former-commit-id: 473401f1bef49c76f8e1acd6e1514e78fee24e2b

--- MuonSpectrometer/MuonReconstruction/MuonRecUtils/MuonLinearSegmentMakerUtilities/src/ClusterAnalysis.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonRecUtils/MuonLinearSegmentMakerUtilities/src/ClusterAnalysis.cxx
@@ -25,7 +25,8 @@
 
 namespace ClusterSeg {
 
-  bool sortfunction (Cluster* i,Cluster* j) { return (i->z() < j->z()); }
+  bool sortfunctionTGC (Cluster* i,Cluster* j) { return (i->z() < j->z()); }
+  bool sortfunctionRPC (Cluster* i,Cluster* j) { return (i->rCyl() < j->rCyl()); }
 
   bool writeOut = false;
   double ang_cut = 0.5;
@@ -103,7 +104,7 @@ namespace ClusterSeg {
     h_corr = new TH2F("phi_theta_corr","",100,0.,TMath::PiOver2(),100,0,TMath::PiOver2());
     h_corr->GetXaxis()->SetTitle("|#Delta #phi| [rad]");
     h_corr->GetYaxis()->SetTitle("|#Delta #theta| [rad]");
-    h_rz = new TH2F("rz_spacePoints","",1000,12000,16000,3000,1000,13000);
+    h_rz = new TH2F("rz_spacePoints","",1000,0,12000,3000,1000,13000);
     h_rz->GetXaxis()->SetTitle("Z [mm]");
     h_rz->GetYaxis()->SetTitle("R [mm]");
     h_xy = new TH2F("xy_spacePoints","",3000,-13000,13000,3000,-13000,13000);
@@ -249,43 +250,49 @@ namespace ClusterSeg {
     for( Long64_t evt=0;evt<nentries;++evt ){
       m_tree->LoadTree(evt);
       m_tree->GetEntry(evt);
-      
+    
+
+      if (evt % 100 == 0) std::cout << "event " << evt << "/" << nentries << std::endl;
+  
       std::vector<Cluster*> clust;
       m_ntuple.read(clust);
-  
-      std::sort (clust.begin(),clust.end(), sortfunction); 
-     
-      thePoints = createSpacePoints(clust);
+ 
+
+      if (clust.at(0)->techIndex() == Muon::MuonStationIndex::TechnologyIndex::TGC){
+        std::sort (clust.begin(),clust.end(), sortfunctionTGC);      
+        thePoints = createSpacePoints(clust);
     
-      if(thePoints.empty()) continue;
-      std::vector<std::vector<SpacePoint>> seeds = createSeeds(thePoints);
-      h_numseeds->Fill(seeds.size());
+        if(thePoints.empty()) continue;
+        std::vector<std::vector<SpacePoint>> seeds = createTGCSeeds(thePoints);
+        h_numseeds->Fill(seeds.size());
+      } else {
+        std::sort (clust.begin(),clust.end(), sortfunctionRPC);
+        thePoints = createSpacePoints(clust);
+
+        if(thePoints.empty()) continue;
+        std::vector<std::vector<SpacePoint>> seeds = createRPCSeeds(thePoints);
+        h_numseeds->Fill(seeds.size());
+      }
 
       for(auto &it: thePoints){
         h_rz->Fill(fabs(it.z()),it.rCyl());
         h_xy->Fill(it.x(),it.y());
       }
       ++m_ncalls;
-      
       m_ntuple.clean(clust);
-
     }
-
-
   }
 
   std::vector<std::vector<SpacePoint>> ClusterAnalysis::analyse(std::vector<Cluster*> clust){
-
     std::vector<std::vector<SpacePoint>> sPoints;
-
     std::vector<SpacePoint> thePoints = createSpacePoints(clust);
     if(thePoints.empty()) return sPoints;
-    std::vector<std::vector<SpacePoint>> seeds = createSeeds(thePoints);
-  
+    std::vector<std::vector<SpacePoint>> seeds = (thePoints.at(0).techIndex() == Muon::MuonStationIndex::TechnologyIndex::TGC) ? 
+                                                 createTGCSeeds(thePoints) : createRPCSeeds(thePoints);
     return seeds;  
   }
 
-  std::vector<std::vector<SpacePoint>> ClusterAnalysis::createSeeds( std::vector<SpacePoint>& points ){
+  std::vector<std::vector<SpacePoint>> ClusterAnalysis::createTGCSeeds( std::vector<SpacePoint> points){
 
     std::vector<SpacePoint> layer1Points;
     std::vector<SpacePoint> layer2Points;
@@ -293,10 +300,11 @@ namespace ClusterSeg {
     std::vector<std::vector<SpacePoint>> seeds;
 
     for(auto &it: points){
-      if (it.isTGC(1)) layer1Points.push_back(it);
-      else if(it.isTGC(2)) layer2Points.push_back(it);
-      else layer3Points.push_back(it);
+      if (it.phiIndex() == Muon::MuonStationIndex::PhiIndex::T1) layer1Points.push_back(it);
+      else if(it.phiIndex() == Muon::MuonStationIndex::PhiIndex::T2) layer2Points.push_back(it);
+      else if(it.phiIndex() == Muon::MuonStationIndex::PhiIndex::T3) layer3Points.push_back(it);
     }
+
     if(writeOut){
       int L1size = layer1Points.size();
       int L2size = layer2Points.size();
@@ -308,7 +316,7 @@ namespace ClusterSeg {
       h_sizeL1L3->Fill(L1size*L3size);
       h_sizeL1L2L3->Fill(L1size*L2size*L3size);
     }
-    //look at events where one layer is missing spacepoints
+    
     if(writeOut && (layer1Points.empty() || layer2Points.empty() || layer3Points.empty())){
       if(!layer1Points.empty()){
         for(auto &it1: layer1Points){if(it1.isMatch()) h_miss_RZ->Fill(fabs(it1.z()),it1.rCyl()); h_miss_XY->Fill(it1.x(),it1.y());} 
@@ -321,13 +329,60 @@ namespace ClusterSeg {
       }
     }
  
-    //First make the seeds from the two outer stations
+    createSeedsAllLayers(layer1Points,layer2Points,layer3Points,seeds);
+    if(seeds.size() < 25){
+      if(!layer1Points.empty() && !layer2Points.empty() && layer3Points.empty()) createSeedsTwoLayers(layer1Points,layer2Points,seeds);
+      if(!layer3Points.empty() && !layer1Points.empty() && layer2Points.empty()) createSeedsTwoLayers(layer1Points,layer3Points,seeds);
+      if(!layer2Points.empty() && !layer3Points.empty() && layer1Points.empty()) createSeedsTwoLayers(layer2Points,layer3Points,seeds);
+    }
+    return seeds;
+  }
+
+  std::vector<std::vector<SpacePoint>> ClusterAnalysis::createRPCSeeds( std::vector<SpacePoint> points){
+
+    std::vector<SpacePoint> layer1Points;
+    std::vector<SpacePoint> layer2Points;
+    std::vector<std::vector<SpacePoint>> seeds;
+
+    for(auto &it: points){
+      if (it.phiIndex() == Muon::MuonStationIndex::PhiIndex::BM1) layer1Points.push_back(it);
+      else if (it.phiIndex() == Muon::MuonStationIndex::PhiIndex::BM2) layer2Points.push_back(it);
+    }
+    if(writeOut){
+      int L1size = layer1Points.size();
+      int L2size = layer2Points.size();
+
+      h_sizeL1->Fill(L1size);
+      h_sizeL2->Fill(L2size);
+    }
+  
+   
+    if(writeOut && (layer1Points.empty() || layer2Points.empty())){
+      if(!layer1Points.empty()){
+        for(auto &it1: layer1Points){if(it1.isMatch()) h_miss_RZ->Fill(fabs(it1.z()),it1.rCyl()); h_miss_XY->Fill(it1.x(),it1.y());}
+      }
+      if(!layer2Points.empty()){
+        for(auto &it1: layer2Points){if(it1.isMatch()) h_miss_RZ->Fill(fabs(it1.z()),it1.rCyl()); h_miss_XY->Fill(it1.x(),it1.y());}
+      }
+    }
+
+    if(seeds.size() < 25 && !layer1Points.empty() && !layer2Points.empty()) createSeedsTwoLayers(layer1Points,layer2Points,seeds);
+    
+    return seeds;
+  }
+
+
+  void ClusterAnalysis::createSeedsAllLayers(std::vector<SpacePoint> layer1Points,
+                                             std::vector<SpacePoint> layer2Points,
+                                             std::vector<SpacePoint> layer3Points,
+                                             std::vector<std::vector<SpacePoint>>& seeds){
+
     if(!layer1Points.empty() && !layer3Points.empty()){
       for(auto &it1: layer1Points){
       for(auto &it3: layer3Points){
       if (TMath::Sign(1.,it1.z()) != TMath::Sign(1.,it3.z())) continue;
         std::vector<SpacePoint> theSeed;
-        Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,false,0);
+        Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,0,0,false,0);
         double angle = acos((point.x()*it1.x()+point.y()*it1.y()+point.z()*it1.z())/(point.rSph()*it1.rSph()));
         if(angle > TMath::Pi()) angle = TMath::TwoPi() - angle;
         double dphi = TMath::Abs(point.phi()-it1.phi());
@@ -347,8 +402,7 @@ namespace ClusterSeg {
           h_angle_t->Fill(angle);
         }
 
-        if(angle < ang_cut) {
-          //Now try to add the third point
+    if(angle < ang_cut) {
           if(!layer2Points.empty()){
             for(auto &it2: layer2Points){
               std::vector<SpacePoint> theSeed;
@@ -362,36 +416,35 @@ namespace ClusterSeg {
               if (writeOut) {
                 h_R->Fill(TMath::Abs(spR-seedR));
                 h_XY->Fill(XY);
-	      }          
+              }
 
               if(writeOut && it1.isMatch() && it3.isMatch() && it2.isMatch() && it1.barcode() == it3.barcode() && it1.barcode() == it2.barcode() && it1.barcode() != 0) {
                 h_R_t->Fill(TMath::Abs(spR-seedR));
                 h_XY_t->Fill(XY);
-                
+
               }
 
               if( XY < dist_cut){
                  theSeed.push_back(it1);
                  theSeed.push_back(it2);
-	         theSeed.push_back(it3);
+                 theSeed.push_back(it3);
                  seeds.push_back(theSeed);
-              } 
+              }
             } //end loop of layer2points
           }
         } // check that seed passes IP pointing constraint   
       } //end loop over layer3points
       } //end loop over layer1points
-    }
+    }//end if statement
+  }
 
-
-    //now add cases for which it is missing one layer
-    if(seeds.size() < 25){
-      if(!layer1Points.empty() && !layer2Points.empty()){
-        for(auto &it1: layer1Points){
+  void ClusterAnalysis::createSeedsTwoLayers(std::vector<SpacePoint> layer1Points,std::vector<SpacePoint> layer2Points,
+                                             std::vector<std::vector<SpacePoint>>& seeds){
+    for(auto &it1: layer1Points){
           for(auto &it3: layer2Points){
             if (TMath::Sign(1.,it1.z()) != TMath::Sign(1.,it3.z())) continue;
             std::vector<SpacePoint> theSeed;
-            Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,false,0);
+            Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,0,0,false,0);
             double angle = acos((point.x()*it1.x()+point.y()*it1.y()+point.z()*it1.z())/(point.rSph()*it1.rSph()));
             if(angle > TMath::Pi()) angle = TMath::TwoPi() - angle;
             double dphi = TMath::Abs(point.phi()-it1.phi());
@@ -415,101 +468,31 @@ namespace ClusterSeg {
             }
           }//layer2Points
         } //layer1Points
-      } else if(!layer1Points.empty() && !layer3Points.empty()){
-        for(auto &it1: layer1Points){
-          for(auto &it3: layer3Points){
-            if (TMath::Sign(1.,it1.z()) != TMath::Sign(1.,it3.z())) continue;
-            std::vector<SpacePoint> theSeed;
-            Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,false,0);
-            double angle = acos((point.x()*it1.x()+point.y()*it1.y()+point.z()*it1.z())/(point.rSph()*it1.rSph()));
-            if(angle > TMath::Pi()) angle = TMath::TwoPi() - angle;
-            double dphi = TMath::Abs(point.phi()-it1.phi());
-            if(dphi > TMath::PiOver2() && dphi < 3*TMath::PiOver2()) dphi -= TMath::Pi();
-            if(dphi > 3*TMath::PiOver2()) dphi -= TMath::TwoPi();
-            double dtheta = TMath::Abs(point.theta()-it1.theta());
-            if (writeOut) {
-              h_phi13->Fill(TMath::Abs(dphi));
-              h_theta13->Fill(dtheta);
-              h_angle13->Fill(angle);
-            }
-            if(writeOut && it3.isMatch() && it1.isMatch() && it3.barcode() == it1.barcode() && it1.barcode() != 0 ) {
-              h_phi_t13->Fill(TMath::Abs(dphi));
-              h_theta_t13->Fill(dtheta);
-              h_angle_t13->Fill(angle);
-            }
-            if (angle < ang2_cut){
-              theSeed.push_back(it1);
-              theSeed.push_back(it3);
-              seeds.push_back(theSeed); 
-            }
-          }//layer3Points
-        } //layer1Points
-      } else if(!layer2Points.empty() && !layer3Points.empty()){
-        for(auto &it1: layer2Points){
-          for(auto &it3: layer3Points){
-            if (TMath::Sign(1.,it1.z()) != TMath::Sign(1.,it3.z())) continue;
-            std::vector<SpacePoint> theSeed;
-            Cluster point = Cluster(it3.x()-it1.x(),it3.y()-it1.y(),it3.z()-it1.z(),false,false,0);
-            double angle = acos((point.x()*it1.x()+point.y()*it1.y()+point.z()*it1.z())/(point.rSph()*it1.rSph()));
-            if(angle > TMath::Pi()) angle = TMath::TwoPi() - angle;
-            double dphi = TMath::Abs(point.phi()-it1.phi());
-            if(dphi > TMath::PiOver2() && dphi < 3*TMath::PiOver2()) dphi -= TMath::Pi();
-            if(dphi > 3*TMath::PiOver2()) dphi -= TMath::TwoPi();
-            double dtheta = TMath::Abs(point.theta()-it1.theta());
-            if (writeOut) {
-              h_phi23->Fill(TMath::Abs(dphi));
-              h_theta23->Fill(dtheta);
-              h_angle23->Fill(angle);
-            }
-            if(writeOut && it3.isMatch() && it1.isMatch() && it3.barcode() == it1.barcode() && it1.barcode() != 0 ) {
-              h_phi_t23->Fill(TMath::Abs(dphi));
-              h_theta_t23->Fill(dtheta);
-              h_angle_t23->Fill(angle);
-            }
-            if (angle < ang2_cut){
-              theSeed.push_back(it1);
-              theSeed.push_back(it3);
-              seeds.push_back(theSeed);
-            }
-          }//layer3Points
-        } //layer2Points
-      }
-    }
-    return seeds;
   }
 
-  std::vector<SpacePoint> ClusterAnalysis::createSpacePoints( std::vector<Cluster*>& clust) {
+  std::vector<SpacePoint> ClusterAnalysis::createSpacePoints( std::vector<Cluster*> clust) {
 
     std::vector<std::pair<Cluster*,int>> phiClusters;
     std::vector<std::pair<Cluster*,int>> etaClusters;
     std::vector<SpacePoint> spacePoints;
 
     int citer(0);
-    for(auto &it: clust){
+    for(auto it: clust){
       if(it->isPhi()) phiClusters.push_back(std::make_pair(it,citer));
       else etaClusters.push_back(std::make_pair(it,citer));
       citer++;
     }  
-
-
-
     for(auto &pit: phiClusters){
     for(auto &eit: etaClusters){
       bool tbool = false;
-      if (eit.first->isMatch() && pit.first->isMatch() && eit.first->barcode() == pit.first->barcode()) tbool = true; 
-      if(TMath::Sign(1.,pit.first->z()) == TMath::Sign(1.,eit.first->z()) && !pit.first->isTGC(4) && pit.first->chamberId() != "" && pit.first->chamberId() == eit.first->chamberId()) {
-        SpacePoint thePoint = SpacePoint(eit.first->eta(),pit.first->phi(),pit.first->z(),tbool,eit.first->barcode(),eit.second,pit.second);
-        spacePoints.push_back(thePoint);
-      }
-      if(writeOut && !pit.first->isTGC(4) && fabs(pit.first->z() - eit.first->z()) < 25.) {
-        SpacePoint thePoint = SpacePoint(eit.first->eta(),pit.first->phi(),pit.first->z(),tbool,eit.first->barcode(),eit.second,pit.second);
-        spacePoints.push_back(thePoint);
-      }    
+      if (eit.first->isMatch() && pit.first->isMatch() && eit.first->barcode() == pit.first->barcode()) tbool = true;
+        if(pit.first->phiIndex() == eit.first->phiIndex() && eit.first->isSideA() == pit.first->isSideA()) {
+          SpacePoint thePoint = SpacePoint(eit.first->eta(),pit.first->phi(),eit.first->z(),eit.first->techIndex(),eit.first->phiIndex(),tbool,eit.first->barcode(),eit.second,pit.second);
+          spacePoints.push_back(thePoint);
+        }
     }
     }
     return spacePoints;
   }
-
-
 }
 
