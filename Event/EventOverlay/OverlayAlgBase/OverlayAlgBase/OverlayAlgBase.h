fe0acdb5be3f5d67ffe7e2dadb7aa68787e5fb00
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:56:18 +0200

    'CMakeLists.txt' (OverlayAlgBase-00-02-00)
    	* OverlayAlgBase/OverlayAlgBase.h,
    	OverlayAlgBase/OverlayAlgBase.icc, src/OverlayAlgBase.cxx -
    	migrate to using AthAlgorithm rather than Algorithm as the
    	baseclass. Still need to sort out StoreGates though.
    
    	* tagging OverlayAlgBase-00-02-00

--- Event/EventOverlay/OverlayAlgBase/OverlayAlgBase/OverlayAlgBase.h
+++ Event/EventOverlay/OverlayAlgBase/OverlayAlgBase/OverlayAlgBase.h
@@ -18,27 +18,25 @@
 
 #include <string>
 
-#include "GaudiKernel/Algorithm.h"
+#include "AthenaBaseComps/AthAlgorithm.h"
 #include "GaudiKernel/ServiceHandle.h"
-#include "AthenaBaseComps/AthMessaging.h"
 
 class StoreGateSvc;
 
-/** 
+/**
  * This class sets up acces to the two event stores and makes sure
  * they are syncronized.  It also handles McEventCollection.
- * 
+ *
  * Algorithms inheriting from OverlayAlgBase should not redefine
  * initialize(), execute() and finalize() but provide
  * overlayInitialize() etc. instead.
  */
-class OverlayAlgBase : public Algorithm,
-		       public AthMessaging
+class OverlayAlgBase : public AthAlgorithm
 {
 public:
-  
+
   OverlayAlgBase(const std::string &name,ISvcLocator *pSvcLocator);
-  
+
   virtual StatusCode initialize();
   virtual StatusCode execute();
   virtual StatusCode finalize();
@@ -52,8 +50,8 @@ protected:
   // ----------------------------------------------------------------
   ServiceHandle<StoreGateSvc> m_storeGateData;
   ServiceHandle<StoreGateSvc> m_storeGateMC;
-  ServiceHandle<StoreGateSvc> m_storeGateOutput;
-  
+  ServiceHandle<StoreGateSvc> m_storeGateOutput; //FIXME this should be dropped in favour of the evtStore method of AthAlgorithm
+
   template<class TypeToBeRemoved> void removeAllObjectsOfType(StoreGateSvc *sg);
   template<class TypeToBeCopied> void copyAllObjectsOfType(StoreGateSvc *to, StoreGateSvc *from);
 };
