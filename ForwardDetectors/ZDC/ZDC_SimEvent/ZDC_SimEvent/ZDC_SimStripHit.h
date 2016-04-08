3c36a083039d02795c9556a5369c64f6c00a48f9
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:35:39 +0200

    'CMakeLists.txt' (ZDC_SimEvent-00-00-04)
            * Tagging ZDC_SimEvent-00-00-04.
    	* Fix coverity warnings: Remove badly-written (and useless)
    	assignment operators.
    
    2016-03-17  scott snyder  <snyder@bnl.gov>
    
            * Tagging ZDC_SimEvent-00-00-03.
    	* Comply with ATLAS naming conventions.
    
    
    Former-commit-id: 4d2b7607d30b56bf7b93665cd0f572b2134c7c93

--- ForwardDetectors/ZDC/ZDC_SimEvent/ZDC_SimEvent/ZDC_SimStripHit.h
+++ ForwardDetectors/ZDC/ZDC_SimEvent/ZDC_SimEvent/ZDC_SimStripHit.h
@@ -12,29 +12,27 @@ class ZDC_SimStripHit
   
   ZDC_SimStripHit() { // must have a constructor with no argument as otherwise there is some problem from the ".xml" file
     
-    Side = -1; ModNo = -1; Edep =-1; Nphotons = -1;
+    m_Side = -1; m_ModNo = -1; m_Edep =-1; m_Nphotons = -1;
   }
   
   ZDC_SimStripHit(int side, int modno, int nphot, double edep) {
     
-    Side = side; ModNo = modno; Edep = edep; Nphotons = nphot;
+    m_Side = side; m_ModNo = modno; m_Edep = edep; m_Nphotons = nphot;
   }
   
-  int    GetSide()     const { return Side; }
-  int    GetMod()      const { return ModNo; }
-  double GetEdep()     const { return Edep; }
-  int    GetNPhotons() const { return Nphotons; }
-
-  const ZDC_SimStripHit& operator = (const ZDC_SimStripHit&);
+  int    GetSide()     const { return m_Side; }
+  int    GetMod()      const { return m_ModNo; }
+  double GetEdep()     const { return m_Edep; }
+  int    GetNPhotons() const { return m_Nphotons; }
 
   int   operator == (const ZDC_SimStripHit&) const;
   bool  operator  < (const ZDC_SimStripHit&) const;
 
   private:
   
-  int    Side, ModNo; // The side and Module_No to which this entry corresponds to
-  int    Nphotons;    // The number of detected cherenkov photons in this TimeBin
-  double Edep;        // The total energy of all the cherenkov photons in this TimeBin
+  int    m_Side, m_ModNo; // The side and Module_No to which this entry corresponds to
+  int    m_Nphotons;    // The number of detected cherenkov photons in this TimeBin
+  double m_Edep;        // The total energy of all the cherenkov photons in this TimeBin
 };
 
 #endif
--- ForwardDetectors/ZDC/ZDC_SimEvent/src/ZDC_SimPixelHit.cxx
+++ ForwardDetectors/ZDC/ZDC_SimEvent/src/ZDC_SimPixelHit.cxx
@@ -4,24 +4,13 @@
 
 #include "ZDC_SimEvent/ZDC_SimPixelHit.h"
 
-const ZDC_SimPixelHit& ZDC_SimPixelHit::operator = (const ZDC_SimPixelHit& simhit) {
-  
-  Side           = simhit.Side;
-  ModNo          = simhit.ModNo;
-  PixNo          = simhit.PixNo;
-  Nphotons       = simhit.Nphotons;
-  Edep           = simhit.Edep;
-  
-  return *this;
-}
-
 int ZDC_SimPixelHit::operator == (const ZDC_SimPixelHit& simhit) const { return (this==&simhit) ? 1 : 0; }
 
 bool ZDC_SimPixelHit::operator < (const ZDC_SimPixelHit&    rhs) const { 
   
-  if (Side  < rhs.Side)  return true;
-  if (ModNo < rhs.ModNo) return true;
-  if (PixNo < rhs.PixNo) return true;
+  if (m_Side  < rhs.m_Side)  return true;
+  if (m_ModNo < rhs.m_ModNo) return true;
+  if (m_PixNo < rhs.m_PixNo) return true;
 
   return false;
 }
