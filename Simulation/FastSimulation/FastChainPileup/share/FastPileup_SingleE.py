611c14c08416ffddb55c117aa5204beffaad8153
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:38:52 +0200

    'CMakeLists.txt' (FastChainPileup-00-00-25-01)
    	* minor update of RTT tests
    	* Tagging as FastChainPileup-00-00-25
    2016-03-12 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fix reco RTT commands
    	* Tagging as FastChainPileup-00-00-24
    2016-03-11 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part2
    	* added new tests for reco jobs
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-23
    2016-03-09 Artem Basalaev <artem.basalaev@cern.ch>
    	* Major update of RTT config: part1
    	* added new jobs for sim, digi and reco
    	* this is transient version, more changes to come soon
    	* Tagging as FastChainPileup-00-00-22
    2016-02-29 Artem Basalaev <artem.basalaev@cern.ch>
    	* Fixed sim_reg_test_fastchain.py:
    	* to work properly when release set to be 'current'
    	* to take the latest succesful result instead of requiring it to be at least 1 day old ('yesterday' or older)
    	* Tagging as FastChainPileup-00-00-21
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/FastSimulation/FastChainPileup/share/FastPileup_SingleE.py
@@ -0,0 +1,50 @@
+# FastPileup.py - set up fast pileup emulation using in-time generator
+# Olivier Arnaez, 17/12/15
+
+if 'pileUpProfile' not in dir():
+    fast_chain_log.info("Configuring default pileup profile for MC12")
+    include("FastChainPileup/FastPileup_mc12_v2.py")
+else:
+    fast_chain_log.info("Using previously configured pileup profile")
+
+fast_chain_log.info("Configuring fast chain pileup")
+
+# setup event generation sequence
+gen_pu='GEN_EVENT_PU'
+from AthenaCommon.AlgSequence import AlgSequence
+topSeq = AlgSequence()
+import AthenaCommon.AppMgr as acam
+acam.athMasterSeq+=AlgSequence('EvgenGenSeq')
+genSeq=acam.athMasterSeq.EvgenGenSeq
+
+from EvgenJobTransforms.EvgenConfig import evgenConfig
+
+evgenConfig.description = "Single electron generator"
+evgenConfig.keywords = ["SM"]
+
+from FastChainPileup.FastChainPileupConf import MultiParticleGunPileup
+genSeq += MultiParticleGunPileup("PartGun")
+evgenConfig.generators += ["ParticleGun"]
+
+from EvgenProdTools.EvgenProdToolsConf import FixHepMC
+genSeq+=FixHepMC(McEventKey=gen_pu,McEventsR=gen_pu,McEventsRW=gen_pu,OutputLevel=INFO)
+
+genSeq.PartGun.McEventKey=gen_pu
+genSeq.PartGun.McEventsR=gen_pu
+genSeq.PartGun.McEventsRW=gen_pu
+# genSeq.PartGun.OutputLevel=DEBUG
+
+genSeq.PartGun.NCollPerEvent=-1
+genSeq.PartGun.PileupProfile=pileUpProfile
+genSeq.PartGun.MultBCID=[1.]
+# genSeq.PartGun.HistFile='profile.root'
+
+randomSeed=0
+if hasattr(runArgs, "randomSeed"):
+    randomSeed=runArgs.randomSeed
+
+from AthenaServices.AthenaServicesConf import AtRndmGenSvc
+ServiceMgr += AtRndmGenSvc("AtRndmGenSvc")
+ServiceMgr.AtRndmGenSvc.Seeds = [ "PYTHIA8 OFFSET " + str(randomSeed) + " 14789899 989240512", "ParticleGun 820021 2347532" ]
+
+topSeq+=genSeq
