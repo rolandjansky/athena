32c09e3df35857dc1365c4066f65826cd61ab6a7
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:09:03 +0200

    'CMakeLists.txt' (TrkResidualPullCalculator-01-00-01)

---
+++ Tracking/TrkTools/TrkResidualPullCalculator/doc/mainpage.h
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage The TrkResidualPullCalculator Package
+
+
+@author Sebastian.Fleischmann -at- cern.ch, Wolfgang.Liebig -at- cern.ch
+
+@section TrkResidualPullCalculatorIntro Introduction
+
+  Tool for the calculation of residuals and pulls.
+  Detector-specific specialities like the SCT endcap residuals are taken into
+  account by calling tools in the sub-detector realm.
+  See details with Trk::IResidualPullCalculator interface description
+
+*/
+@section TrkResidualPullCalculatorExtras Extra Pages
+
+ - @ref TrkResidualPullCalculatorUsed
+ - @ref TrkResidualPullCalculatorReqs
+ 
+*/
+
+/**
+@page TrkResidualPullCalculatorUsed Used Packages
+@htmlinclude used_packages.html
+*/
+
+/**
+@page TrkResidualPullCalculatorReqs Requirements
+@include requirements
+*/
