a98119b5215dd0d787199ffca39672e26691854c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:30:07 +0200

    'CMakeLists.txt' (TrigSerializeTP-00-02-11)
    	* Did a major cleanup in the TrigSerTPTool source code. Removing
    	  all functions and variables that I deemed unnecessary.
    	* Explicitly including the types in the header now that we declare
    	  ServiceHandles for. Since Gaudi has the tendency of introducing
    	  this requirement these days. (Currently battling such an issue
    	  with ToolHandles.)
    	* Taught the tool how to make use of trigger specific T/P converters,
    	  which we introduced to support using trigger specific auxiliary
    	  containers with some offline types at Point1.
    	* Tagging as TrigSerializeTP-00-02-11
    
    2015-04-09  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrigSerializeTP-00-02-10.
    	* AthenaBaseComps migration.

--- Trigger/TrigDataAccess/TrigSerializeTP/src/TrigSerTPTool.cxx
+++ Trigger/TrigDataAccess/TrigSerializeTP/src/TrigSerTPTool.cxx
@@ -2,284 +2,209 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "TrigSerializeTP/TrigSerTPTool.h"
+// $Id$
 
-//TP
-#include "AthenaPoolCnvSvc/ITPConverter.h"
-#include "AthenaPoolCnvSvc/AthenaPoolTopLevelTPCnvBase.h"
+// ROOT include(s):
+#include <TClass.h>
 
-#include "AthenaKernel/getMessageSvc.h"
-#include "AthenaKernel/IDictLoaderSvc.h"
-#include "AthenaKernel/ITPCnvSvc.h"
-#include "AthenaKernel/ITPCnvBase.h"
-
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/IMessageSvc.h"
+// Gaudi/Athena include(s):
 #include "GaudiKernel/System.h"
+#include "AthenaKernel/IDictLoaderSvc.h"
+#include "AthenaKernel/ITPCnvBase.h"
+#include "AthenaKernel/errorcheck.h"
+#include "CxxUtils/make_unique.h"
 
-#include "DataModel/Arena.h"
-
-//ROOT
-#include "TROOT.h"
-#include "TClass.h"
-#include "TBaseClass.h"
-
-#include <iostream>
-#include <algorithm>
-
-using std::map;
-using std::string;
+// Local include(s):
+#include "TrigSerializeTP/TrigSerTPTool.h"
 
 TrigSerTPTool::TrigSerTPTool( const std::string& type,
                               const std::string& name,
                               const IInterface* parent) :
-  AlgTool(type,name,parent),
-  m_log(0),
-  m_logTP(0),
-  m_tpCnvBaseClass(0),
-  m_msgsvcTP("",name),
-  m_dictSvc("AthDictLoaderSvc",name),
-  m_useAthDictLoader(true),
-  m_online(false),
-  m_tpcnvsvc("AthTPCnvSvc", name)
-{
+   AthAlgTool( type, name, parent ),
+   m_msgsvcTP( "", name ),
+   m_dictSvc( "AthDictLoaderSvc", name ),
+   m_useAthDictLoader( true ),
+   m_online( false ),
+   m_tpcnvsvc( "AthTPCnvSvc", name ) {
 
-  declareInterface<TrigSerTPTool>(this);
-  declareProperty("TPMap", m_TPmap, "map of T->P classes" );
-  declareProperty("ActiveClasses", m_activeClasses, "allows to skip classes not relevant in the current trigger level");
-  declareProperty("debuggingMsgSvc", m_msgsvcTP, "an instance of the messageSvc for debugging purposes");
-  declareProperty("onlineMode", m_online, "simpler setup for running online");
-  declareProperty("useAthDictLoader", m_useAthDictLoader, "use AthDictLoaderSvc instead of plain ROOT");
-  m_log = new MsgStream(msgSvc(), name);
+   declareInterface< TrigSerTPTool >( this );
 
-}
-
-TrigSerTPTool::~TrigSerTPTool(){
-  if (m_logTP!=m_log)
-    delete m_logTP;
-  delete m_log;
-}
-
-
-TClass *TrigSerTPTool::getClass(const std::string &cname) const {
-
-  TClass *cl(0);
-  if (m_useAthDictLoader && m_dictSvc){
-    m_dictSvc->load_type(cname);
-  }
-  cl = gROOT->GetClass(cname.c_str());
-  return cl;
+   declareProperty( "TPMap", m_TPmap, "map of T->P classes" );
+   declareProperty( "ActiveClasses", m_activeClasses,
+                    "allows to skip classes not relevant in the current "
+                    "trigger level" );
+   declareProperty( "debuggingMsgSvc", m_msgsvcTP,
+                    "an instance of the messageSvc for debugging purposes" );
+   declareProperty( "onlineMode", m_online,
+                    "simpler setup for running online" );
+   declareProperty( "useAthDictLoader", m_useAthDictLoader,
+                    "use AthDictLoaderSvc instead of plain ROOT" );
 }
 
 StatusCode TrigSerTPTool::initialize(){
 
-  //  std::cout << "TrigSerTPTool::initialize: " << name() << ":" << msgSvc()->outputLevel(name()) << std::endl;
-  m_log->setLevel(msgSvc()->outputLevel(name()));
-  *m_log << MSG::INFO << "in initialize " << name() << endreq;
+   // Greet the user:
+   ATH_MSG_INFO( "Initializing - Package version: " << PACKAGE_VERSION );
 
-  //a special instance of the MessageSvc for the T/P
-  m_logTP = m_log;
+   // Retrieve a custom message service:
+   if( ! m_msgsvcTP.empty() ) {
+      if( m_msgsvcTP.retrieve().isFailure() ) {
+         ATH_MSG_INFO( "Could not retrieve a separate MsgSvc for "
+                       "the T/P debugging" );
+      } else {
+         ATH_MSG_DEBUG( "Using " << m_msgsvcTP << " for debugging" );
+         IMessageSvc* msvc = m_msgsvcTP.operator->();
+         m_logTP = CxxUtils::make_unique< MsgStream >( msvc,
+                                                       "TrigSerTPTool-T/P" );
+      }
+   }
 
-  if(!m_msgsvcTP.empty()){
-    if (m_msgsvcTP.retrieve().isFailure()){
-      *m_log << MSG::INFO << "Could not retrieve a separate MsgSvc for the T/P debugging" << endreq;
-    } else {
-      *m_log << MSG::DEBUG << "Using " << m_msgsvcTP << " for debugging " << endreq;
-      IMessageSvc* msvc = m_msgsvcTP.operator->();
-      m_logTP = new MsgStream(msvc, "TrigSerTPTool-T/P");
-    }
-  }
+   // Retrieve the used services:
+   ATH_CHECK( m_dictSvc.retrieve() );
+   ATH_CHECK( m_tpcnvsvc.retrieve() );
 
-  if (!m_dictSvc.retrieve().isSuccess()) {
-    *m_log << MSG::ERROR << "could not retrieve athena dict. loader service !" << endreq;
-    return StatusCode::FAILURE;
-  }
-
-  if (!m_tpcnvsvc.retrieve().isSuccess()) {
-    *m_log << MSG::ERROR << "could not retrieve T/P cnv svc" << endmsg;
-    return StatusCode::FAILURE;
-  }
-
-  m_tpCnvBaseClass = getClass ("ITPCnvBase");
-
-  if (!m_tpCnvBaseClass ) {
-    *m_log << MSG::DEBUG << "Cannot find class ITPCnvBase" << endreq;
-  } else {
-    *m_log << MSG::DEBUG << "Found class ITPCnvBase" << endreq;
-  }
-
-  return StatusCode::SUCCESS;
+   // Return gracefully:
+   return StatusCode::SUCCESS;
 }
 
-StatusCode TrigSerTPTool::finalize(){
-  
-  map <void*,TClass*>::const_iterator mit(m_convObj.begin());
-  map <void*,TClass*>::const_iterator mend(m_convObj.end());
+void* TrigSerTPTool::convertTP( const std::string &clname, void *ptr,
+                                std::string &persName ) {
 
-  while (mit!=mend){
-    void *p = (*mit).first;
-    TClass *cl = (*mit).second;
-    if (cl && p){
-      //      *m_log << MSG::DEBUG << "cl=" << cl << " p=" << p << endreq;
-      cl->Destructor(p);
-    }
-    mit++;
-  }
-  m_convObj.clear();
+   ATH_MSG_DEBUG( "TrigSerTPTool::convertTP" );
 
-  return StatusCode::SUCCESS;
-}
+   //pers and persName set only after successful conversion
+   persName = "";
+   void *pers( 0 );
 
-StatusCode TrigSerTPTool::forceDictLoading(const std::string &clname) const {
-  TClass *cl = getClass(clname);
-  if (!cl){
-    *m_log << MSG::DEBUG << "Dict loading of " << clname << " failed" << endreq;
-    return StatusCode::FAILURE;
-  } else {
-    void *p = cl->New();
-    cl->Destructor(p);
-  }
-  return StatusCode::SUCCESS;
-}
-
-StatusCode TrigSerTPTool::testTPCnv(const std::string &clname) {
-  TClass *cl = getClass(clname);
-  if (!cl){
-    *m_log << MSG::DEBUG << "Dict loading of " << clname << " failed" << endreq;
-    return StatusCode::FAILURE;
-  } else {
-    void *p = cl->New();
-    if (p){
-      std::string persname;
-      void *pers = convertTP(clname, p, persname);
-      //cleanup
-      TClass *pcl = getClass(persname);
-      if (pcl)
-	pcl->Destructor(pers);
-    }
-    cl->Destructor(p);
-  }
-  return StatusCode::SUCCESS;
-}
-
-void*  TrigSerTPTool::convertTP(const std::string &clname, void *ptr, std::string &persName){
-  const int loglvl = m_log->level();
-
-  if (loglvl<=MSG::DEBUG)
-    *m_log << MSG::DEBUG << "TrigSerTPTool::convertTP " << endreq;
-
-  //pers and persName set only after successful conversion
-  persName = "";
-  void *pers(0);
-
-  // Start by trying to find the correct persistent class from the TP map
-  // Otherwise, use the internal logic of the TPCnvSvc
-  ITPCnvBase* cnvtr;
-  if(m_TPmap.find(clname) == m_TPmap.end())
-    {
-      *m_log << MSG::ERROR << "Transient class " << clname 
-	     << " is not in the T/P Converter map" << endreq;
+   // Start by trying to find the correct persistent class from the TP map
+   // Otherwise, use the internal logic of the TPCnvSvc
+   const auto tpItr = m_TPmap.find( clname );
+   if( tpItr == m_TPmap.end() ) {
+      REPORT_MESSAGE( MSG::ERROR )
+         << "Transient class " << clname 
+         << " is not in the T/P Converter map";
       return 0;
-    }
-  
-  cnvtr = m_tpcnvsvc->p2t_cnv(m_TPmap[clname]);
-  
-  if (!cnvtr) {
-    *m_log << MSG::ERROR << "T/P Converter for transient class "
-           << clname << " could not be retrieved" << endreq;
-    return 0;
-  }
+   }
 
-  const std::string _persname = System::typeinfoName(cnvtr->persistentTInfo());
+   // First look for a trigger specific converter:
+   ITPCnvBase* cnvtr =
+      m_tpcnvsvc->p2t_cnv( tpItr->second, Athena::TPCnvType::Trigger );
+   if( ! cnvtr ) {
+      // If there is no such thing, try a generic one:
+      cnvtr = m_tpcnvsvc->p2t_cnv( tpItr->second );
+   }
+   if( ! cnvtr ) {
+      REPORT_MESSAGE( MSG::ERROR )
+         << "T/P Converter for transient class "
+         << clname << " could not be retrieved";
+      return 0;
+   }
 
-  TClass *persObjCl = getClass(_persname);
-  void *persptr(0);
-  if (persObjCl){
-    persptr = persObjCl->New();
-    if (loglvl<=MSG::DEBUG){
-      *m_log << MSG::DEBUG << "created object of " << persptr << " at " << persptr << endreq;
-    }
-  } else {
-    return 0;
-  }
+   // Create a persistent object:
+   const std::string persname =
+      System::typeinfoName( cnvtr->persistentTInfo() );
+   TClass *persObjCl = getClass( persname );
+   void *persptr( 0 );
+   if( persObjCl ) {
+      persptr = persObjCl->New();
+      ATH_MSG_DEBUG( "created object of " << persptr << " at " << persptr  );
+   } else {
+      REPORT_MESSAGE( MSG::ERROR )
+         << "Couldn't find dictionary for type " << persname;
+      return 0;
+   }
 
-  if (loglvl<=MSG::DEBUG){
-    *m_log << MSG::DEBUG << "invoking TP for " << clname  << " at " << ptr << " - ";
-  }
+   // Do the conversion:
+   ATH_MSG_DEBUG( "invoking TP for " << clname  << " at " << ptr );
+   try {
+      cnvtr->transToPersUntyped( ptr, persptr, m_logTP ? *m_logTP : msg() );
+      persName = persname;
+      pers = persptr;
+      ATH_MSG_DEBUG( "succeeded at " << persptr  );
+   }
+   catch( const std::runtime_error& e ){
 
-  try {
-    cnvtr->transToPersUntyped(ptr, persptr, *m_logTP);
-    persName = _persname;
-    pers = persptr;
-    if (loglvl<=MSG::DEBUG){
-      *m_log << MSG::DEBUG <<  "succeeded at " << persptr << endreq;
-    }
-  }
-  catch (const std::runtime_error& e){
+      //delete persObjCl->New();
+      const std::string issue = e.what();
+      if( issue.find( "is deprecated" ) != std::string::npos ) {
+         ATH_MSG_INFO( "An exception " << e.what() );
+      } else {
+         pers = 0;
+         REPORT_MESSAGE( MSG::ERROR ) << "An exception occurred: " << e.what();
+      }
+   }
 
-    //delete persObjCl->New();
-    const std::string issue = e.what();
-    if (issue.find("is deprecated")!=std::string::npos){
-      *m_log << MSG::INFO << "An exception " << e.what() << endreq;
-    } else {
-      pers = 0;
-      *m_log << MSG::ERROR << "An exception " << e.what() << endreq;
-    }
-  }
-
-  return pers;
+   // Return the converted object:
+   return pers;
 }
 
-void* TrigSerTPTool::convertPT(const std::string &persName, void *pers, std::string &transName){
-  const int loglvl = m_log->level();
+void* TrigSerTPTool::convertPT( const std::string &persName, void *pers,
+                                std::string& transName ) {
 
-  // The p->t converter logic ensures the correct transient class is always loaded
-  ITPCnvBase* cnvtr = m_tpcnvsvc->p2t_cnv(persName);
-  if (!cnvtr) {
-    *m_log << MSG::ERROR << "T/P Converter for persistent class "
-           << persName << " could not be retrieved" << endreq;
-    return 0;
-  }
-  transName = System::typeinfoName(cnvtr->transientTInfo());
-  
-  TClass *transCl = getClass(transName);
-  void *trans(0);
-  if (transCl){
-    trans = transCl->New();
-    if (loglvl<=MSG::DEBUG){
-      *m_log << MSG::DEBUG << "trans  " << trans << endreq;
-    }
-  }
-    
-  if (loglvl<=MSG::DEBUG){
-    *m_log << MSG::DEBUG << "invoking PT for " << transName << " - ";
-  }
-  try {
-    cnvtr->persToTransUntyped(pers, trans, *m_logTP);
-    if (loglvl<=MSG::DEBUG){
-      *m_log << MSG::DEBUG << " succeeded at " << trans << endreq;
-    }
-  }
-  catch (const std::runtime_error& e){
-    *m_log << MSG::ERROR << "Exception " << e.what() << endreq;
-    //it should destruct trans here (is it in a good state?)
-    trans = 0;
-  }
-  
-  return trans;
-  
+   // First look for a trigger specific converter:
+   ITPCnvBase* cnvtr =
+      m_tpcnvsvc->p2t_cnv( persName, Athena::TPCnvType::Trigger );
+   if( ! cnvtr ) {
+      // If there is no such thing, try a generic one:
+      cnvtr = m_tpcnvsvc->p2t_cnv( persName );
+   }
+   if( ! cnvtr ) {
+      REPORT_MESSAGE( MSG::ERROR )
+         << "T/P Converter for persistent class "
+         << persName << " could not be retrieved";
+      return 0;
+   }
+
+   // Get the name of the transient class:
+   transName = System::typeinfoName( cnvtr->transientTInfo() );
+
+   // Create the transient object:
+   TClass *transCl = getClass( transName );
+   void *trans( 0 );
+   if( transCl ){
+      trans = transCl->New();
+      ATH_MSG_DEBUG( "trans  " << trans );
+   }
+
+   // Do the conversion:
+   ATH_MSG_DEBUG( "invoking PT for " << transName );
+   try {
+      cnvtr->persToTransUntyped( pers, trans, m_logTP ? *m_logTP : msg() );
+      ATH_MSG_DEBUG( " succeeded at " << trans );
+   }
+   catch (const std::runtime_error& e){
+      REPORT_MESSAGE( MSG::ERROR ) << "An exception occurred: " << e.what();
+      //it should destruct trans here (is it in a good state?)
+      trans = 0;
+   }
+
+   // Return the converted object:
+   return trans;
 }
 
-std::string TrigSerTPTool::persClassName(const std::string &transClassName){
-  
-  // Try to extract from the TP map
-  if(m_TPmap.find(transClassName) == m_TPmap.end())
-    {
-      *m_log << MSG::ERROR << "Transient class " << transClassName
-             << " is not in the T/P Converter map" << endreq;
-      return "";
-    }
+const std::string&
+TrigSerTPTool::persClassName( const std::string& transClassName ) const {
 
-  return m_TPmap[transClassName];  
-  
+   // Try to extract from the TP map
+   const auto tpItr = m_TPmap.find( transClassName );
+   if( tpItr == m_TPmap.end() ) {
+      REPORT_MESSAGE( MSG::ERROR )
+         << "Transient class " << transClassName
+         << " is not in the T/P Converter map";
+      static const std::string dummy( "" );
+      return dummy;
+   }
+
+   // Return the persistent class name:
+   return tpItr->second;
+}
+
+TClass *TrigSerTPTool::getClass( const std::string &cname ) const {
+
+   TClass *cl( 0 );
+   if( m_useAthDictLoader && m_dictSvc ){
+      m_dictSvc->load_type( cname );
+   }
+   cl = TClass::GetClass( cname.c_str() );
+   return cl;
 }
