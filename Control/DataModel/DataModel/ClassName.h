4a5a8c18926f73ff9a923fa1be81ba645d9570e5
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:40:41 +0200

    'CMakeLists.txt' (DataModel-00-23-71)
    Former-commit-id: 49df99bb5c9cd4e5fda8a53152878d41071a31c0

---
+++ Control/DataModel/DataModel/ClassName.h
@@ -0,0 +1,23 @@
+// This file's extension implies that it's C, but it's really -*- C++ -*-.
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: ClassName.h 559821 2013-08-29 20:51:26Z ssnyder $
+
+/**
+ * @file  DataModel/ClassName.h
+ * @author scott snyder
+ * @date Jul 2005
+ * @brief An interface for getting the name of a class as a string.
+ */
+
+
+#ifndef DATAMODEL_CLASSNAME_H
+#define DATAMODEL_CLASSNAME_H
+
+// moved to SGTools for dependency issues
+#include "SGTools/ClassName.h"
+
+#endif // not DATAMODEL_CLASSNAME_H
---
+++ Control/DataModel/DataModel/OwnershipPolicy.h
@@ -0,0 +1,10 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef DATAMODEL_OWNERSHIPPOLICY_H
+# define DATAMODEL_OWNERSHIPPOLICY_H
+
+#include "AthContainers/OwnershipPolicy.h"
+
+#endif // DATAMODEL_OWNERSHIPPOLICY_H
