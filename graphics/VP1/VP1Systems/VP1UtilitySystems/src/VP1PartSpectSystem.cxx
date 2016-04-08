6912dfe06625a61a0d58b62fea8e097f84651853
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 15:43:22 +0200

    'CMakeLists.txt' (VP1UtilitySystems-00-01-07)
    Former-commit-id: f07cac25a4e30ef5bbfa963e6a7cd60edf01c78d

--- graphics/VP1/VP1Systems/VP1UtilitySystems/src/VP1PartSpectSystem.cxx
+++ graphics/VP1/VP1Systems/VP1UtilitySystems/src/VP1PartSpectSystem.cxx
@@ -260,7 +260,7 @@ void VP1PartSpectSystem::plotSpectrum(QStack<QString>& path, int copyNumber)
   PlotHist1D::Properties* prop = new PlotHist1D::Properties();
   prop->pen.setColor(QColor("darkRed"));
   prop->pen.setWidth(3);
-  pHist->setProperties(*prop); //const reference, but does not take possession of pointer
+  pHist->setProperties(*prop);
     
   // Make a plot
   QRectF rect = pHist->rectHint();
@@ -312,8 +312,6 @@ void VP1PartSpectSystem::plotSpectrum(QStack<QString>& path, int copyNumber)
   // Show this view
 
   d->PlotMainWindow->show();
-  delete prop;
-  prop=nullptr;
   // ___________ Plotting _______________
 }
 
