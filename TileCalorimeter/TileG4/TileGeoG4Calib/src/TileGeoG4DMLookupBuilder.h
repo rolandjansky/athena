23dc8c3cc32a2bb97a58535fb1789f7b9b79656c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:47:41 +0200

    'CMakeLists.txt' (TileGeoG4Calib-00-05-05)
    	* Tagging TileGeoG4Calib-00-05-05.
    	* Fix compilation with gcc5: G4cout -> G4endl.
    
    	* Tagging TileGeoG4Calib-00-05-04.
    	* Fix a couple more names.
    
    	* Tagging TileGeoG4Calib-00-05-03.
    	* Fix clang warning: unused member.
    
    	* Tagging TileGeoG4Calib-00-05-02.
    
    2016-02-21  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* TileGeoG4DMLookupBuilder: fixed some class member names to follow ATLAS conventions
    	* TileGeoG4CalibSD: fixed some class member names to follow ATLAS conventions
    	* TileGeoG4CalibSD2: fixed some class member names to follow ATLAS conventions
    	* TileCalibDddbManager: fixed some class member names to follow ATLAS conventions
    
    2015-09-25  John Chapman  <John.Chapman@cern.ch>
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: fe81253b56d238215e74de711f67897c4d039a62

--- TileCalorimeter/TileG4/TileGeoG4Calib/src/TileGeoG4DMLookupBuilder.h
+++ TileCalorimeter/TileG4/TileGeoG4Calib/src/TileGeoG4DMLookupBuilder.h
@@ -12,8 +12,8 @@
 //
 //************************************************************
 
-#ifndef TileGeoG4DMLookupBuilder_H
-#define TileGeoG4DMLookupBuilder_H
+#ifndef TILEGEOG4CALIB_TILEGEOG4DMLOOKUPBUILDER_H
+#define TILEGEOG4CALIB_TILEGEOG4DMLOOKUPBUILDER_H
 
 #include "TileDetDescr/TileDddbManager.h"
 #include "TileDetDescr/TileDetDescrManager.h"
@@ -30,53 +30,50 @@ class IRDBAccessSvc;
 class IGeoModelSvc;
 class StoreGateSvc;
 
-class TileGeoG4DMLookupBuilder
-{
-public:
-  TileGeoG4DMLookupBuilder(TileGeoG4LookupBuilder* tile_lookup_builder, 
-                           ServiceHandle<IRDBAccessSvc> &access, 
-                           ServiceHandle<IGeoModelSvc> &geo_svc,
-                           ServiceHandle<StoreGateSvc> &pDetStore,
-                           const int verboseLevel);
-  ~TileGeoG4DMLookupBuilder();
-    
-  void BuildLookup(bool test_beam = false);
-  void ResetCells();
-  TileGeoG4CalibSection* GetSection(TileCalibDddbManager::TileCalibSections key) const;
-  bool GetPlateToCell();
-  
-  //Geometry constans fo DH calculator
-  double rBMin,rBMax;
-  double zBarrMaxPos,zBarrMaxNeg;
-  double dzBarrMod,dzExtBarrMod;
-  double zLegngthITC; 
-  double dzBarrPeriod,dzExtBarrPeriod;  
-  double rGirdMin;
-  double dRFront;
-  double dZEnd,dZEndSh;
-  
-  double rP1Min,rP2Min,rGapMax,rGapMin,rCrMax,rCrMin;
+class TileGeoG4DMLookupBuilder {
+  public:
+    TileGeoG4DMLookupBuilder(TileGeoG4LookupBuilder* tile_lookup_builder, ServiceHandle<IRDBAccessSvc> &access,
+                             ServiceHandle<IGeoModelSvc> &geo_svc, ServiceHandle<StoreGateSvc> &pDetStore,
+                             const int verboseLevel);
+    ~TileGeoG4DMLookupBuilder();
 
-private:
+    void BuildLookup(bool test_beam = false);
+    void ResetCells();
+    TileGeoG4CalibSection* GetSection(TileCalibDddbManager::TileCalibSections key) const;
+    bool GetPlateToCell();
 
-  TileGeoG4DMLookupBuilder (const TileGeoG4DMLookupBuilder&);  
-  TileGeoG4DMLookupBuilder& operator= (const TileGeoG4DMLookupBuilder&); 
+    //Geometry constans fo DH calculator
+    double rBMin, rBMax;
+    double zBarrMaxPos, zBarrMaxNeg;
+    double dzBarrMod, dzExtBarrMod;
+    double zLegngthITC;
+    double dzBarrPeriod, dzExtBarrPeriod;
+    double rGirdMin;
+    double dRFront;
+    double dZEnd, dZEndSh;
 
-  typedef std::map< TileCalibDddbManager::TileCalibSections, TileGeoG4CalibSection*, 
-                    std::less<TileCalibDddbManager::TileCalibSections> >  TileGeoG4CalibSectionMap;
+    double rP1Min, rP2Min, rGapMax, rGapMin, rCrMax, rCrMin;
 
-  void CreateGeoG4CalibSections(bool is_ctb);
+  private:
 
-  TileCalibDddbManager* m_dbManager;
-  TileGeoG4LookupBuilder* m_lookup_builder;
-  TileGeoG4CalibSectionMap* m_sectionMap;
+    TileGeoG4DMLookupBuilder(const TileGeoG4DMLookupBuilder&);
+    TileGeoG4DMLookupBuilder& operator=(const TileGeoG4DMLookupBuilder&);
 
-  const TileDetDescrManager* m_theManager;
-  TileDddbManager* m_tdbManager;
+    typedef std::map<TileCalibDddbManager::TileCalibSections, TileGeoG4CalibSection*,
+        std::less<TileCalibDddbManager::TileCalibSections> > TileGeoG4CalibSectionMap;
 
-  int m_verboseLevel;
+    void CreateGeoG4CalibSections(bool is_ctb);
 
-  bool plateToCell;
+    TileCalibDddbManager* m_dbManager;
+    TileGeoG4LookupBuilder* m_lookup_builder;
+    TileGeoG4CalibSectionMap* m_sectionMap;
+
+    const TileDetDescrManager* m_theManager;
+    TileDddbManager* m_tdbManager;
+
+    int m_verboseLevel;
+
+    bool m_plateToCell;
 };
 
-#endif
+#endif // TILEGEOG4CALIB_TILEGEOG4DMLOOKUPBUILDER_H
