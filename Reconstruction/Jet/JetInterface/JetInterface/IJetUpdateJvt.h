d5487c5dc7cd19581b2ede2b4d763b53d29e3f1f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:23:55 +0200

    'CMakeLists.txt' (JetInterface-00-00-43)
    	* JetInterface-00-00-43
    	* ATLJETMET-376
    	* JetInterface/IJetUpdateJvt.h: Add this interface for updating JVT.
    
    2015-05-08 S. Schramm
    	* JetInterface-00-00-42
    	* IJetUncertaintiesTool: fixed pileup reference retrieval (doesn't affect uncertainty)

---
+++ Reconstruction/Jet/JetInterface/JetInterface/IJetUpdateJvt.h
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// IJetUpdateJvt.h
+
+#ifndef IJetUpdateJvt_H
+#define IJetUpdateJvt_H
+
+/// \class IJetUpdateJvt
+/// \author David Adams
+/// \date June 2015
+///
+/// Class interface for a toool that updates JVT.
+
+#include "AsgTools/IAsgTool.h"
+#include "xAODJet/JetContainer.h"
+
+class IJetUpdateJvt : virtual public asg::IAsgTool {
+ASG_TOOL_INTERFACE(IJetUpdateJvt)
+
+public:
+
+  /// Calculate the updated JVT.
+  ///   jet - jet for which JVT is updated
+  ///   sjvt - name of the existing JVT moment (and prefix for RpT and JVFcorr).
+  ///   scale - name of the jet scale holding the original pT
+  /// The new value for JVT is returned.
+  virtual float updateJvt(const xAOD::Jet& jet, std::string sjvt = "Jvt",
+                          std::string scale ="JetPileupScaleMomentum") const =0;
+
+};
+
+#endif
