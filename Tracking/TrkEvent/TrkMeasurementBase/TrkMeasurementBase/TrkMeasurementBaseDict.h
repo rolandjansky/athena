adf99e228912396960e18637af372d824db47e95
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:15:26 +0200

    'CMakeLists.txt' (TrkMeasurementBase-02-00-02)
    	* Adding a statement to the requirements file to handle this
    	  package correctly in the CMake build.
    	* Tagging as TrkMeasurementBase-02-00-02
    
    2015-11-26  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrkMeasurementBase-02-00-01.
    	* Restore dictionary generation.  DataModel -> AthContainers.

--- Tracking/TrkEvent/TrkMeasurementBase/TrkMeasurementBase/TrkMeasurementBaseDict.h
+++ Tracking/TrkEvent/TrkMeasurementBase/TrkMeasurementBase/TrkMeasurementBaseDict.h
@@ -6,7 +6,7 @@
 #define TRKMEASUREMENTBASEDICT_H
 
 #include "TrkMeasurementBase/MeasurementBase.h"
-#include "DataModel/DataVector.h"
+#include "AthContainers/DataVector.h"
 
 // We need to instantiate some templates here to be able to create the
 // dictionary for them
