fc1459e57ce410927ae5df0086b56a8bf32874c6
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 15:41:24 +0200

    'CMakeLists.txt' (MuonDQAUtils-00-04-02)
    	* src\MuonDQAFitFunc.cxx: THist rebinning fix for ROOT6
    	* tagging MuonDQAUtils-00-04-02
    
    2015-06-15 Ioannis Nomidis
    	* Fixed checkreq warnings
    	* tagging MuonDQAUtils-00-04-01
    
    2015-04-07 Ioannis Nomidis
    	* Removed classes no longer used
    	* tagging MuonDQAUtils-00-04-00

--- MuonSpectrometer/MuonValidation/MuonDQA/MuonDQAUtils/src/components/MuonDQAUtils_entries.cxx
+++ MuonSpectrometer/MuonValidation/MuonDQA/MuonDQAUtils/src/components/MuonDQAUtils_entries.cxx
@@ -1,7 +1,4 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "MuonDQAUtils/MuonDQATrackHistTool.h"
-#include "MuonDQAUtils/MuonDQAHistTool.h"
-#include "MuonDQAUtils/MuonDQAHitHistTool.h"
 #include "MuonDQAUtils/MuonDQAFitFunc.h"
 #include "MuonDQAUtils/InsituTrackTools.h"
 #include "MuonDQAUtils/MuonSpectrometerProbeCollectorTool.h"
@@ -9,9 +6,6 @@
 
 using namespace Muon;
 
-  DECLARE_TOOL_FACTORY(MuonDQATrackHistTool)
-  DECLARE_TOOL_FACTORY(MuonDQAHitHistTool)
-  DECLARE_TOOL_FACTORY(MuonDQAHistTool)
   DECLARE_TOOL_FACTORY(MuonDQAFitFunc)
   DECLARE_TOOL_FACTORY(MuonSpectrometerProbeCollectorTool)
   DECLARE_TOOL_FACTORY(InnerDetProbeCollectorTool)
@@ -19,9 +13,6 @@ using namespace Muon;
 
   DECLARE_FACTORY_ENTRIES(MuonDQAUtils)
 {
-  DECLARE_ALGTOOL(MuonDQAHistTool);
-  DECLARE_ALGTOOL(MuonDQATrackHistTool);
-  DECLARE_ALGTOOL(MuonDQAHistHistTool);
   DECLARE_ALGTOOL(MuonDQAFitFunc);  
   DECLARE_TOOL(MuonSpectrometerProbeCollectorTool)
   DECLARE_TOOL(InnerDetProbeCollectorTool)
