34fec1009b123253046204b185fca430585f7141
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:39:46 +0200

    'CMakeLists.txt' (TrigPython-20-00-01)

---
+++ HLT/Trigger/TrigControl/TrigPython/TrigPython/TrigPythonDict.h
@@ -0,0 +1,10 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRIGCONTROL_TRIGPYTHON_H
+#define TRIGCONTROL_TRIGPYTHON_H
+
+#include "TrigKernel/ITrigMessageSvc.h"
+
+#endif
