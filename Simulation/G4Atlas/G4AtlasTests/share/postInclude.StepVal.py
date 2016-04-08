3e4ae69bbb767229b2ba8ff556049cb25451cc1b
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:42:05 +0200

    'CMakeLists.txt' (G4AtlasTests-01-00-17)
    2015-11-15  John Chapman  <John.Chapman@cern.ch>
    
    	* src/TileHitsTestTool.cxx (processEvent): Add protection against
    	nullptr ddElement values.
    	* tagging  G4AtlasTests-01-00-17
    
    2015-07-17 Andrea Di Simone <andrea.di.simone@cern.ch>
    	* migrating SteppingValidation to new user actions
    	* separating the histogramming functionality in a separate class SimTestHisto
    	* tagging  G4AtlasTests-01-00-16

--- Simulation/G4Atlas/G4AtlasTests/share/postInclude.StepVal.py
+++ Simulation/G4Atlas/G4AtlasTests/share/postInclude.StepVal.py
@@ -4,11 +4,15 @@ if not hasattr(ServiceMgr, 'THistSvc'):
     ServiceMgr += THistSvc("THistSvc")
     ServiceMgr.THistSvc.Output  = ["truth DATAFILE='stepping.root' OPT='NEW'"];
 
-from AthenaCommon.AlgSequence import AlgSequence
-job = AlgSequence()
-if not hasattr(job, 'G4TestAlg'):
-    from G4AtlasTests.G4AtlasTestsConf import G4TestAlg
-    job += G4TestAlg()
-from AthenaCommon import CfgGetter
-job.G4TestAlg.SimTestTools += [CfgGetter.getPrivateTool("SteppingValidation", checkType=True)]
+#from AthenaCommon.AlgSequence import AlgSequence
+#job = AlgSequence()
+#if not hasattr(job, 'G4TestAlg'):
+#    from G4AtlasTests.G4AtlasTestsConf import G4TestAlg
+#    job += G4TestAlg()
+#from AthenaCommon import CfgGetter
+#job.G4TestAlg.SimTestTools += [CfgGetter.getPrivateTool("SteppingValidation", checkType=True)]
 
+# no need to add it to G4TestAlg. adding to UAStore is enough to schedule execution
+from G4AtlasServices.G4AtlasUserActionConfig import UAStore
+# add default configurable
+UAStore.addAction('SteppingValidation',['Step','BeginOfEvent','EndOfEvent'])
