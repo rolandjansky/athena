ac6d2b01dbdc08a0c1d6b5158bdefeca417ca04f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:03:51 +0200

    'CMakeLists.txt' (xAODTrackingCnv-00-01-13)
    	* move conversion logic from TrackParticleCnvAlg to
    	* TrackCollectionCnvTool and RecTrackParticleContainerCnvTool
    	* https://its.cern.ch/jira/browse/ATR-9340
    	* Tagged as xAODTrackingCnv-00-01-13
    
    2015-04-14 Edward Moyse
            * Coverity: 14587
            * Tagged as xAODTrackingCnv-00-01-12
    
    2015-02-27 Nick Styles
    	* Fix copy-pase error...
    	* Tagged as xAODTrackingCnv-00-01-11
    
    2015-02-27 Nick Styles
    	* Changes for allowing re-running tracking on ESD
    	* Tagged as xAODTrackingCnv-00-01-10

---
+++ Event/xAOD/xAODTrackingCnv/src/TrackCollectionCnvTool.cxx
@@ -0,0 +1,81 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// EDM include(s):
+// $Id$
+#include "xAODTracking/TrackParticle.h"
+#include "xAODTracking/TrackParticleContainer.h"
+#include "TrkTrack/TrackCollection.h"
+
+// Local include(s):
+#include "TrackCollectionCnvTool.h"
+
+namespace xAODMaker {
+
+  TrackCollectionCnvTool::TrackCollectionCnvTool( const std::string& type, 
+					      const std::string& name,
+					      const IInterface* parent )
+    : AthAlgTool( type, name, parent ),
+      m_particleCreator("Trk::TrackParticleCreatorTool/TrackParticleCreatorTool")
+  {
+    
+    // Declare the interface(s) provided by the tool:
+    declareInterface< ITrackCollectionCnvTool >( this );
+    declareProperty( "TrackParticleCreator", m_particleCreator );
+  }
+  
+  StatusCode TrackCollectionCnvTool::initialize() {
+    // Greet the user:
+    ATH_MSG_INFO( "Initializing - Package version: " << PACKAGE_VERSION );
+    ATH_CHECK( m_particleCreator.retrieve() );
+    
+    return StatusCode::SUCCESS;
+  }
+
+  StatusCode TrackCollectionCnvTool::convert( const TrackCollection* aod,
+					      xAOD::TrackParticleContainer* xaod ) {
+    
+    ATH_MSG_DEBUG( "Sizes of containers before conversion: aod, xaod: " << aod->size() << ", " << xaod->size() );
+    
+    TrackCollection::const_iterator itr = aod->begin();
+    TrackCollection::const_iterator end = aod->end();
+
+    for( ;itr!=end;++itr ) {
+      // Create the xAOD object:
+      if (!(*itr)) {
+        ATH_MSG_WARNING("WTaF? Empty element in container!");
+        continue;
+      }
+      xAOD::TrackParticle* particle = createParticle(*xaod, *aod, **itr);
+
+      if(!particle){
+	ATH_MSG_WARNING("Failed to create a TrackParticle");
+	continue;
+      }      
+    }
+
+    ATH_MSG_DEBUG( "Sizes of containers after conversion: aod, xaod: " << aod->size() << ", " << xaod->size() );
+
+    return StatusCode::SUCCESS;    
+  }
+
+  xAOD::TrackParticle* TrackCollectionCnvTool::createParticle(xAOD::TrackParticleContainer& xaod,
+							      const TrackCollection& container,
+							      const Trk::Track& tp) {
+    // create the xAOD::TrackParticle, the pointer is added to the container in the function
+    ElementLink<TrackCollection> trackLink( &tp, container );
+    return m_particleCreator->createParticle( trackLink, &xaod );
+    //no!    return m_particleCreator->createParticle( tp, &xaod );
+  }
+
+  StatusCode TrackCollectionCnvTool::setParticleCreatorTool(ToolHandle<Trk::ITrackParticleCreatorTool> *tool)
+  {
+    ATH_MSG_DEBUG( "In setParticleCreatorTool" );
+    m_particleCreator = *tool;
+
+    return StatusCode::SUCCESS;
+  }
+
+} // namespace xAODMaker
+
---
+++ Event/xAOD/xAODTrackingCnv/src/TrackCollectionCnvTool.h
@@ -0,0 +1,51 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id$
+#ifndef XAODTRACKINGCNV_TRACKCOLLECTIONCNVTOOL_H
+#define XAODTRACKINGCNV_TRACKCOLLECTIONCNVTOOL_H
+
+// Gaudi/Athena include(s):
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "GaudiKernel/ToolHandle.h"
+
+// converting tool that will be invoked
+#include "TrkToolInterfaces/ITrackParticleCreatorTool.h"
+
+// Local include(s):
+#include "xAODTrackingCnv/ITrackCollectionCnvTool.h"
+
+namespace xAODMaker {
+
+  class TrackCollectionCnvTool : public AthAlgTool,
+			       public virtual ITrackCollectionCnvTool {
+
+  public:
+    /// Regular AlgTool constructor
+    TrackCollectionCnvTool( const std::string& type, const std::string& name,
+			  const IInterface* parent );
+    
+    /// Function initialising the tool
+    virtual StatusCode initialize();
+    
+    /// Function that fills an existing xAOD::TrackParticleContainer
+    virtual StatusCode convert( const TrackCollection* aod,
+			       xAOD::TrackParticleContainer* xaod );
+    
+    /// allow other algorithms to pass the tool in order to preserve initialisation
+    virtual StatusCode setParticleCreatorTool(ToolHandle<Trk::ITrackParticleCreatorTool> *tool);
+
+  private:
+    ToolHandle<Trk::ITrackParticleCreatorTool> m_particleCreator;
+    
+    inline xAOD::TrackParticle* createParticle(xAOD::TrackParticleContainer& xaod, const TrackCollection& container, const Trk::Track& tp) ;
+
+
+  }; // class TrackCollectionCnvTool definition
+
+} // xAODMaker namespace
+
+#endif // XAODTRACKINGCNV_TRACKCOLLECTIONCNVTOOL_H
