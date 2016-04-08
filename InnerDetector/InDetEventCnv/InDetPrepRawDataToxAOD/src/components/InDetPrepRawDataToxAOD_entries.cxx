ad028d1510de9b2a08e111fd0359135b28cf0ab8
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:42:51 +0200

    'CMakeLists.txt' (InDetPrepRawDataToxAOD-00-01-34)
    	* Testing changes related to ATLASRECTS-2860 from yesterday
    	* Tagging as InDetPrepRawDataToxAOD-00-01-34
    
    2016-03-16 Simon Viel
            * Re-commit Kilian Rosbach's changes on top of the latest
            * NOT tagged
    
    2016-03-16 Simon Viel
            * On behalf of Weiming Yao and Peilian Liu
            * Add pseudo-tracking (aka truth-tracking) in share/InDetDxAOD.py
            * NOT including Kilian Rosbach's latest commit
            * Tagged InDetPrepRawDataToxAOD-00-01-33
    
    2016-03-16 Kilian Rosbach <Kilian.Rosbach@cern.ch>
    	* Adding SCT_RawDataToxAOD algorithm (ATLASRECTS-2860)
    
    2016-03-10 Susumu Oda <Susumu.Oda@cern.ch>
    	* Use m_SCTHelper->strip(sctRdo->identify()) instead of sctRdo->getStrip()
    	  in src/SCT_PrepDataToxAOD.cxx.
    	  https://its.cern.ch/jira/browse/ATLASSIM-1907
    ...
    (Long ChangeLog diff - truncated)

--- InnerDetector/InDetEventCnv/InDetPrepRawDataToxAOD/src/components/InDetPrepRawDataToxAOD_entries.cxx
+++ InnerDetector/InDetEventCnv/InDetPrepRawDataToxAOD/src/components/InDetPrepRawDataToxAOD_entries.cxx
@@ -1,6 +1,7 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "../TRT_PrepDataToxAOD.h"
 #include "../SCT_PrepDataToxAOD.h"
+#include "../SCT_RawDataToxAOD.h"
 #include "../PixelPrepDataToxAOD.h"
 
 
@@ -8,11 +9,13 @@
 
 DECLARE_ALGORITHM_FACTORY( TRT_PrepDataToxAOD )
 DECLARE_ALGORITHM_FACTORY( SCT_PrepDataToxAOD )
+DECLARE_ALGORITHM_FACTORY( SCT_RawDataToxAOD )
 DECLARE_ALGORITHM_FACTORY( PixelPrepDataToxAOD )
 
 DECLARE_FACTORY_ENTRIES( InDetPrepRawDataToxAOD ) 
 {
   DECLARE_ALGORITHM( TRT_PrepDataToxAOD )
   DECLARE_ALGORITHM( SCT_PrepDataToxAOD )
+  DECLARE_ALGORITHM( SCT_RawDataToxAOD )
   DECLARE_ALGORITHM( PixelPrepDataToxAOD )
 }
