a4563e3f35b0a758a0826301f1e8f85677bd8ee5
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:09:27 +0200

    'CMakeLists.txt' (L1TopoByteStream-00-00-10)
    	* src/L1TopoByteStream{xAOD,Aux}Cnv: Converterts to xAOD objects
    	  (Event/xAOD/xAODTrigL1Calo/xAODTrigL1Calo/versions/L1TopoRawData_v1.h)
    	* share/ReadLVL1CaloBSRun2_jobOptions.py: register converters (included from Trigger/TrigT1/TrigT1CaloByteStream/share/ReadLVL1CaloBSRun2_jobOptions.py)
    	* L1TopoByteStream-00-00-10

--- Trigger/TrigT1/L1Topo/L1TopoByteStream/src/components/L1TopoByteStream_entries.cxx
+++ Trigger/TrigT1/L1Topo/L1TopoByteStream/src/components/L1TopoByteStream_entries.cxx
@@ -1,13 +1,18 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "../L1TopoByteStreamCnv.h"
+#include "../L1TopoByteStreamxAODCnv.h"
+#include "../L1TopoByteStreamAuxCnv.h"
 #include "../L1TopoByteStreamTool.h"
 
-DECLARE_CONVERTER_FACTORY( L1TopoByteStreamCnv )
-DECLARE_TOOL_FACTORY( L1TopoByteStreamTool )
+DECLARE_CONVERTER_FACTORY(L1TopoByteStreamCnv)
+DECLARE_NAMESPACE_CONVERTER_FACTORY(LVL1BS, L1TopoByteStreamxAODCnv)
+DECLARE_NAMESPACE_CONVERTER_FACTORY(LVL1BS, L1TopoByteStreamAuxCnv)
+DECLARE_TOOL_FACTORY(L1TopoByteStreamTool)
 
-DECLARE_FACTORY_ENTRIES( L1TopoByteStream)
-{
-  DECLARE_CONVERTER( L1TopoByteStreamCnv )
-  DECLARE_TOOL( L1TopoByteStreamTool )
+DECLARE_FACTORY_ENTRIES(L1TopoByteStream) {
+  DECLARE_CONVERTER(L1TopoByteStreamCnv)
+  DECLARE_TOOL(L1TopoByteStreamTool)
 
+  DECLARE_NAMESPACE_CONVERTER(LVL1BS, L1TopoByteStreamxAODCnv)
+  DECLARE_NAMESPACE_CONVERTER(LVL1BS, L1TopoByteStreamAuxCnv)
 }
