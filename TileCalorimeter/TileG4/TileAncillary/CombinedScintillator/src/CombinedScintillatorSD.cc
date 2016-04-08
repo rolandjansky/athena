6fcb192efa3a16a3d10d3387cc5634320c39ea9f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:47:10 +0200

    'CMakeLists.txt' (CombinedScintillator-00-03-00)
    	* Tagging CombinedScintillator-00-03-00
    	* Migrating CombinedScintillator DetFacility class to the new
    	infrastructure. ATLASSIM-2423
    
    2015-07-29  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging CombinedScintillator-00-02-00
    	* Migrate CombinedScintillatorSD to new infrastructure.

--- TileCalorimeter/TileG4/TileAncillary/CombinedScintillator/src/CombinedScintillatorSD.cc
+++ TileCalorimeter/TileG4/TileAncillary/CombinedScintillator/src/CombinedScintillatorSD.cc
@@ -7,201 +7,168 @@
 // Class CombinedScintillatorSD.
 // Sensitive detector for the Scintillator between LAr/Tile
 //
-// Author: franck Martin <Franck.Martin@cern.ch>
-// january 12, 2004
-//
 //************************************************************
 
-#include "CombinedScintillator/CombinedScintillatorSD.hh"
-#include "CombinedScintillator/CombinedScintillatorSDMessenger.hh"
-#include "CombinedScintillator/CombinedScintillatorDescription.hh"
+#include "CombinedScintillatorSD.hh"
 
-#include "TileG4DetDescr/DetectorDescriptionStore.h"
-#include "FadsSensitiveDetector/SensitiveDetectorEntryT.h"
-#include "FadsSensitiveDetector/SensitiveDetectorCatalog.h"
+#include "CaloIdentifier/TileTBID.h"
+#include "CxxUtils/make_unique.h"// For make unique
+#include "StoreGate/StoreGateSvc.h"
+#include "TileSimEvent/TileHitVector.h"
+
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/Bootstrap.h"
 
 #include "G4HCofThisEvent.hh"
 #include "G4VPhysicalVolume.hh"
 #include "G4Step.hh"
 #include "G4VTouchable.hh"
 #include "G4TouchableHistory.hh"
-#include "G4SDManager.hh"
-#include "G4TransportationManager.hh"
-#include "G4Navigator.hh"
-#include "G4Trd.hh"
 
-#include "G4UImanager.hh"
-#include "G4ios.hh"
-
-#include "GaudiKernel/ISvcLocator.h"
-#include "GaudiKernel/Bootstrap.h"
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/IMessageSvc.h"
-#include "StoreGate/StoreGateSvc.h"
-
-#include "CaloIdentifier/TileTBID.h"
-#include "TileSimEvent/TileHitVector.h"
-
-static SensitiveDetectorEntryT<CombinedScintillatorSD> combinedscintillatorSD("CombinedScintillatorSD");
-
-CombinedScintillatorSD::CombinedScintillatorSD(G4String name)
-  : FadsSensitiveDetector(name),
-    detectorDescription(0),    
-    combinedscintillatorSDMessenger(0),
-    m_iamowner(false),
-    m_hit()
-{  
-  combinedscintillatorSDMessenger = new CombinedScintillatorSDMessenger(this);
+CombinedScintillatorSD::CombinedScintillatorSD(const std::string& name, const std::string& hitCollectionName)
+  : G4VSensitiveDetector(name),
+    m_hit(),
+    m_HitColl(hitCollectionName)
+{
   ISvcLocator* svcLocator = Gaudi::svcLocator();
-  StatusCode status = svcLocator->service("MessageSvc", m_msgSvc);
-  MsgStream log(m_msgSvc, "CombinedScintillatorSD");
+  StoreGateSvc* detStore(nullptr);
+  if(svcLocator->service("DetectorStore", detStore).isFailure())
+    {
+      G4ExceptionDescription description;
+      description << "Constructor: DetectorStoreSvc not found!";
+      G4Exception("CombinedScintillatorSD", "NoDetStore", FatalException, description);
+      abort();
+    }
+  else if (verboseLevel>5)
+    {
+      G4cout << "DetectorStoreSvc initialized" << G4endl;
+    }
 
-  if(status.isFailure())
-    log << MSG::FATAL << "MessageSvc not found!" << endreq;
-  else
-    log << MSG::DEBUG << "MessageSvc initialized" << endreq;
-
-  status = svcLocator->service("StoreGateSvc", m_sgSvc);
-  if(status.isFailure())
-    log << MSG::FATAL << "StoreGateSvc not found!" << endreq;
-  else
-    log << MSG::DEBUG << "StoreGateSvc initialized" << endreq;
-
-  status = svcLocator->service("DetectorStore", m_detStore);
-  if(status.isFailure())
-    log << MSG::FATAL << "DetectorStoreSvc not found!" << endreq;
-  else
-    log << MSG::DEBUG << "DetectorStoreSvc initialized" << endreq;
-
-  status = m_detStore->retrieve(m_tileTBID);
-  if(status.isFailure())
-    log << MSG::FATAL << "No TileTBID helper" << endreq;
-  else
-    log << MSG::DEBUG << "TileTBID helper retrieved" << endreq;
-
+  if(detStore->retrieve(m_tileTBID).isFailure())
+    {
+      G4ExceptionDescription description;
+      description << "Constructor: No TileTBID helper!";
+      G4Exception("CombinedScintillatorSD", "NoTileTBIDHelper", FatalException, description);
+      abort();
+    }
+  else if (verboseLevel>5)
+    {
+      G4cout << "TileTBID helper retrieved" << G4endl;
+    }
   int type=TileTBID::ADC_TYPE, module=TileTBID::CRACK_WALL;
-  for (int channel=0; channel<nCell; ++channel) {
-    m_id[channel] = m_tileTBID->channel_id(type,module,channel);
-  }
+  for (int channel=0; channel<nCell; ++channel)
+    {
+      m_id[channel] = m_tileTBID->channel_id(type,module,channel);
+    }
 }
 
 CombinedScintillatorSD::~CombinedScintillatorSD()
 {
-  delete combinedscintillatorSDMessenger;
+
 }
 
-void CombinedScintillatorSD::Description(std::string descr_name)
+void CombinedScintillatorSD::StartOfAthenaEvent()
 {
-  MsgStream log(m_msgSvc, "CombinedScintillatorSD"); 
-  log << MSG::DEBUG << "CombinedScintillatorSD, Description" << endreq; 
-  DetectorDescriptionStore *st=DetectorDescriptionStore::GetDetectorDescriptionStore();
-  DetectorDescription *dd=st->GetDetectorDescription(descr_name);
-  CombinedScintillatorDescription* i=dynamic_cast<CombinedScintillatorDescription *>(dd);
-  if (i)
-    detectorDescription = i;
-  else
-    log << MSG::FATAL << "Description" << descr_name << "is not of right type combinedScintillatorDescription!" << endreq;
-}
-
-void CombinedScintillatorSD::Initialize(G4HCofThisEvent* /* HCE */)
-{
-  MsgStream log(m_msgSvc, "CombinedScintillatorSD"); 
-  log << MSG::DEBUG << "Initializing SD" << endreq;
+  if (verboseLevel>5) { G4cout << "Initializing SD" << G4endl; }
 
   memset(m_nhits,0,sizeof(m_nhits));
 }
 
+void CombinedScintillatorSD::Initialize(G4HCofThisEvent* /* HCE */)
+{
+  if (verboseLevel>5)
+    {
+      G4cout << "CombinedScintillatorSD::Initialize()" << G4endl;
+    }
+
+#ifdef ATHENAHIVE
+  // Temporary fix for Hive until isValid is fixed
+  m_HitColl = CxxUtils::make_unique<TileHitVector>(m_HitColl.name());
+#else
+  if (!m_HitColl.isValid()) m_HitColl = CxxUtils::make_unique<TileHitVector>(m_HitColl.name());
+#endif
+}
+
 G4bool CombinedScintillatorSD::ProcessHits(G4Step* aStep,G4TouchableHistory* /* ROhist */)
 {
-  MsgStream log(m_msgSvc, "CombinedScintillatorSD : Process hit");
-  MSG::Level logLevel = log.level();
-  if (logLevel <= MSG::VERBOSE)
-    log << MSG::VERBOSE << "i am in comb. Scinti process hit" << endreq; 
+  if (verboseLevel>10)
+    {
+      G4cout << "CombinedScintillatorSD::ProcessHits" << G4endl;
+    }
 
-  G4TouchableHistory* theTouchable = (G4TouchableHistory*)(aStep->GetPreStepPoint()->GetTouchable());
-  G4VPhysicalVolume* physVol = theTouchable->GetVolume();
-  G4LogicalVolume* logiVol   = physVol->GetLogicalVolume();
-  G4String nameLogiVol       = logiVol->GetName();
-  G4int nScinti              = physVol->GetCopyNo(); 
+  const G4TouchableHistory* theTouchable = (G4TouchableHistory*)(aStep->GetPreStepPoint()->GetTouchable());
+  const G4VPhysicalVolume* physVol = theTouchable->GetVolume();
+  const G4LogicalVolume* logiVol   = physVol->GetLogicalVolume();
+  const G4String nameLogiVol       = logiVol->GetName();
+  const G4int nScinti              = physVol->GetCopyNo();
 
-  G4double edep  = aStep->GetTotalEnergyDeposit();
+  const G4double edep  = aStep->GetTotalEnergyDeposit();
   G4double stepl = 0.;
   G4ThreeVector pStep = aStep->GetDeltaPosition();
-  
-  if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.) stepl = aStep->GetStepLength();
-    
-  if ((edep==0.)&&(stepl==0.)) return false;
 
-  if(nameLogiVol.find("CScintillatorLayer") !=G4String::npos) 
-  {
-    int ind = nScinti; // Only one scintillator at the test beam
-                       // No copy nScinti == 0 !!
-
-    if ( m_nhits[ind] > 0 )
-    { 
-      if (logLevel <= MSG::VERBOSE)
-        log << MSG::VERBOSE << "Additional hit in Combined scint " << nScinti
-            << " ene=" << edep << endreq;
-      m_hit[ind]->add(edep,0.0,0.0);
-    } else
+  if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.) // FIXME not-equal check on double
     {
-      // First hit in a cell
-      if (logLevel <= MSG::VERBOSE)
-        log << MSG::VERBOSE << "First hit in Combined scint " << nScinti
-            << " ene=" << edep << endreq;
-      m_hit[ind] = new TileSimHit(m_id[ind],edep,0.0,0.0);  
+      stepl = aStep->GetStepLength();
     }
-    ++m_nhits[ind];
-  }  
+
+  if ((edep==0.)&&(stepl==0.)) //FIXME equality check on double
+    {
+      return false;
+    }
+
+  if(nameLogiVol.find("CScintillatorLayer") !=G4String::npos)
+    {
+      int ind = nScinti; // Only one scintillator at the test beam
+      // No copy nScinti == 0 !!
+
+      if ( m_nhits[ind] > 0 )
+        {
+          if (verboseLevel>10)
+            {
+              G4cout << "Additional hit in CombinedScintillator " << nScinti
+                     << " ene=" << edep << G4endl;
+            }
+          m_hit[ind]->add(edep,0.0,0.0);
+        }
+      else
+        {
+          // First hit in a cell
+          if (verboseLevel>10)
+            {
+              G4cout << "First hit in CombinedScintillator " << nScinti
+                     << " ene=" << edep << G4endl;
+            }
+          m_hit[ind] = new TileSimHit(m_id[ind],edep,0.0,0.0);
+        }
+      ++m_nhits[ind];
+    }
   return true;
 }
 
-void CombinedScintillatorSD::EndOfEvent(G4HCofThisEvent* /* HCE */)
+void CombinedScintillatorSD::EndOfAthenaEvent()
 {
-  MsgStream log(m_msgSvc, "CombinedScintillatorSD"); 
-
-  TileHitVector* ExtraHitsCollection;   
-
-  if (! m_iamowner) {
-    const TileHitVector* ExtraColl;
-    StatusCode status = m_sgSvc->retrieve(ExtraColl,"TileTBHits");
-    m_iamowner = (status.isFailure());
-    ExtraHitsCollection = (TileHitVector *)ExtraColl;
-  }
-
-  if (m_iamowner) {
-    ExtraHitsCollection = new TileHitVector("TileTBHits"); 
-    StatusCode status = m_sgSvc->record(ExtraHitsCollection,ExtraHitsCollection->Name(),true);
-    if (status.isFailure())
-      log << MSG::FATAL << "Can't initialize TileTBHits vector" << endreq; 
-    else
-      log << MSG::DEBUG << "I have initialized TileTBHits vector" << endreq; 
-  }
-  
-  for (int ind=0; ind<nCell; ++ind) {
-    int nhit= m_nhits[ind];
-    if (nhit>0) {
-      log << MSG::DEBUG
-          << "Cell id=" << m_tileTBID->to_string(m_id[ind])
-          << " nhit=" << nhit << " ene=" << m_hit[ind]->energy()
-          << endreq;
-      ExtraHitsCollection->Insert(TileHit(m_hit[ind]));
-      delete m_hit[ind];
-    } else {
-      log << MSG::VERBOSE
-          << "Cell id=" << m_tileTBID->to_string(m_id[ind])
-          << " nhit=0"  << endreq;
+  for (int ind=0; ind<nCell; ++ind)
+    {
+      int nhit= m_nhits[ind];
+      if (nhit>0)
+        {
+          if (verboseLevel>5)
+            { G4cout
+                << "Cell id=" << m_tileTBID->to_string(m_id[ind])
+                << " nhit=" << nhit << " ene=" << m_hit[ind]->energy()
+                << G4endl;
+            }
+          m_HitColl->Insert(TileHit(m_hit[ind]));
+          delete m_hit[ind];
+        }
+      else if (verboseLevel>10)
+        {
+          G4cout
+            << "Cell id=" << m_tileTBID->to_string(m_id[ind])
+            << " nhit=0"  << G4endl;
+        }
     }
-  }
 
-  log << MSG::DEBUG << "Total number of hits is " << ExtraHitsCollection->size() << endreq;
-
-  if (m_iamowner) {
-    StatusCode status = m_sgSvc->setConst(ExtraHitsCollection);
-    if(status.isFailure())
-      log << MSG::ERROR << "Can't lock Collection 'TileTBHits'" << endreq;
-    else
-      log << MSG::DEBUG << "Collection 'TileTBHits' is locked " << endreq;
-  }
+  if (verboseLevel>5) { G4cout << "Total number of hits is " << m_HitColl->size() << G4endl; }
+  return;
 }
-
