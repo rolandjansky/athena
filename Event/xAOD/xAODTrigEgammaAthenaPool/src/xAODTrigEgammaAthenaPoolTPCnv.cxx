48fdb0db88154e43b71df97eb256ec6f06a0ed2c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:01:51 +0200

    'CMakeLists.txt' (xAODTrigEgammaAthenaPool-00-00-07)
    	* Tagging xAODTrigEgammaAthenaPool-00-00-07.
    	* src/xAODTrigEgammaAthenaPoolTPCnv.cxx: Use
    	DECLARE_TRIGTPCNV_FACTORY for trigger-only conversions.  Avoids
    	warnings about duplicate converters.

--- Event/xAOD/xAODTrigEgammaAthenaPool/src/xAODTrigEgammaAthenaPoolTPCnv.cxx
+++ Event/xAOD/xAODTrigEgammaAthenaPool/src/xAODTrigEgammaAthenaPoolTPCnv.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODTrigEgammaAthenaPoolTPCnv.cxx 705790 2015-11-04 13:09:23Z krasznaa $
+// $Id: xAODTrigEgammaAthenaPoolTPCnv.cxx 714614 2015-12-14 21:26:28Z ssnyder $
 
 // Gaudi/Athena include(s):
 #include "AthenaKernel/TPCnvFactory.h"
@@ -26,28 +26,28 @@
 #include "xAODPhotonTrigAuxContainerOfflineCnv_v3.h"
 
 // Declare the T/P converter(s):
-DECLARE_TPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v1,
-                       xAOD::ElectronTrigAuxContainer,
-                       xAOD::ElectronAuxContainer_v1,
-                       Athena::TPCnvVers::Old )
-DECLARE_TPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v2,
-                       xAOD::ElectronTrigAuxContainer,
-                       xAOD::ElectronAuxContainer_v2,
-                       Athena::TPCnvVers::Old )
-DECLARE_TPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v3,
-                       xAOD::ElectronTrigAuxContainer,
-                       xAOD::ElectronAuxContainer_v3,
-                       Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v1,
+                           xAOD::ElectronTrigAuxContainer,
+                           xAOD::ElectronAuxContainer_v1,
+                           Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v2,
+                           xAOD::ElectronTrigAuxContainer,
+                           xAOD::ElectronAuxContainer_v2,
+                           Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODElectronTrigAuxContainerOfflineCnv_v3,
+                           xAOD::ElectronTrigAuxContainer,
+                           xAOD::ElectronAuxContainer_v3,
+                           Athena::TPCnvVers::Old )
 
-DECLARE_TPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v1,
-                       xAOD::PhotonTrigAuxContainer,
-                       xAOD::PhotonAuxContainer_v1,
-                       Athena::TPCnvVers::Old )
-DECLARE_TPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v2,
-                       xAOD::PhotonTrigAuxContainer,
-                       xAOD::PhotonAuxContainer_v2,
-                       Athena::TPCnvVers::Old )
-DECLARE_TPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v3,
-                       xAOD::PhotonTrigAuxContainer,
-                       xAOD::PhotonAuxContainer_v3,
-                       Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v1,
+                           xAOD::PhotonTrigAuxContainer,
+                           xAOD::PhotonAuxContainer_v1,
+                           Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v2,
+                           xAOD::PhotonTrigAuxContainer,
+                           xAOD::PhotonAuxContainer_v2,
+                           Athena::TPCnvVers::Old )
+DECLARE_TRIGTPCNV_FACTORY( xAODPhotonTrigAuxContainerOfflineCnv_v3,
+                           xAOD::PhotonTrigAuxContainer,
+                           xAOD::PhotonAuxContainer_v3,
+                           Athena::TPCnvVers::Old )
