f5a78767f1d5a95b1b2cf109e250f4734361fd96
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:55:52 +0200

    'CMakeLists.txt' (AthenaPoolTestAthenaPool-00-00-27)
    	* Tagging AthenaPoolTestAthenaPool-00-00-27.
    	* Remove #define private.

--- AtlasTest/DatabaseTest/AthenaPoolTestAthenaPool/src/PixelRDOElemLinkVecCnv_p1.cxx
+++ AtlasTest/DatabaseTest/AthenaPoolTestAthenaPool/src/PixelRDOElemLinkVecCnv_p1.cxx
@@ -2,12 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#define private public
-#define protected public
 #include "AthenaPoolTestData/PixelRDOElemLinkVec.h"
-#undef private
-#undef protected 
-
 #include "PixelRDOElemLinkVecCnv_p1.h"
 
 
@@ -18,13 +13,13 @@ void PixelRDOElemLinkVecCnv_p1::persToTrans(const PixelRDOElemLinkVec_p1* pers,
     // reset element link converters, and provide container name lookup table
     m_elementLinkCnv.resetForCnv(pers->m_linkNames);
 
-    trans->m_pixelRDOs.resize(pers->m_pixelRDOs.size());
+    trans->pixelRDOs().resize(pers->m_pixelRDOs.size());
     for (unsigned int i = 0; i < pers->m_pixelRDOs.size(); ++i) {
 //         log << MSG::ERROR
 //             << "persToTrans: index " <<   pers->m_pixelRDOs[i].m_contIndex << " " 
 //             << pers->m_pixelRDOs[i].m_elementIndex 
 //             << endreq;
-        m_elementLinkCnv.persToTrans(&pers->m_pixelRDOs[i], &trans->m_pixelRDOs[i], log);
+      m_elementLinkCnv.persToTrans(&pers->m_pixelRDOs[i], &trans->pixelRDOs()[i], log);
     }
 }
 
@@ -36,13 +31,13 @@ void PixelRDOElemLinkVecCnv_p1::transToPers(const PixelRDOElemLinkVec* trans,
     // reset element link converters, and provide container name lookup table
     m_elementLinkCnv.resetForCnv(pers->m_linkNames);
 
-    pers->m_pixelRDOs.resize(trans->m_pixelRDOs.size());
-    for (unsigned int i = 0; i < trans->m_pixelRDOs.size(); ++i) {
+    pers->m_pixelRDOs.resize(trans->pixelRDOs().size());
+    for (unsigned int i = 0; i < trans->pixelRDOs().size(); ++i) {
 //         log << MSG::ERROR
 //             << "transToPers: index " <<   MSG::hex 
 //             << trans->m_pixelRDOs[i].index() << " " << MSG::dec
 //             << endreq;
-        m_elementLinkCnv.transToPers( &trans->m_pixelRDOs[i], &pers->m_pixelRDOs[i], log);
+      m_elementLinkCnv.transToPers( &trans->pixelRDOs()[i], &pers->m_pixelRDOs[i], log);
     }
 }
 
