6d96d439c2f592c776b188d84d20c81523b7e0d3
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:22:21 +0200

    'CMakeLists.txt' (EventShapeTPCnv-00-00-04)
    	* Tagging EventShapeTPCnv-00-00-04.
    	* Remove some unneccessary converter dictionaries.

--- Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeStore_p1.h
+++ Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeStore_p1.h
@@ -5,12 +5,6 @@
 #ifndef EVENTSHAPETPCNV_EVENTSHAPESTORE_P1_H
 #define EVENTSHAPETPCNV_EVENTSHAPESTORE_P1_H
 
-#include "CLIDSvc/CLASS_DEF.h"
-
-// DataModelAthenaPool includes
-#include "DataModelAthenaPool/ElementLink_p1.h"
-#include "DataModelAthenaPool/ElementLinkVector_p1.h"
-
 #include <vector>
 
 class EventShapeStore_p1
@@ -23,7 +17,7 @@ public:
   EventShapeStore_p1() { };
   virtual ~EventShapeStore_p1() { };
   
- protected:
+protected:
   
   std::vector<unsigned int>  m_mtypV;
   std::vector<float>         m_mvalV;
--- Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeTPCnvDict.h
+++ Reconstruction/EventShapes/EventShapeTPCnv/EventShapeTPCnv/EventShapeTPCnvDict.h
@@ -7,14 +7,5 @@
 
 #include "EventShapeTPCnv/EventShapeStore_p1.h"
 #include "EventShapeTPCnv/EventShapeStore_p2.h"
-#include "EventShapeTPCnv/EventShapeStoreCnv_p1.h"
-#include "EventShapeTPCnv/EventShapeStoreCnv_p2.h"
-
-namespace {
-  struct GCCXML_DUMMY_INSTANTIATION_EVENTSHAPETPCNV {
-    T_TPCnv<EventShapeStore, EventShapeStore_p1> m_esscnv_p1;
-    T_TPCnv<EventShapeStore, EventShapeStore_p2> m_esscnv_p2;
-  };
-}
 
 #endif
