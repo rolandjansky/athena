88162941267b784d0a2b8622e4e7f36843749904
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:06:57 +0200

    'CMakeLists.txt' (TrkExRungeKuttaIntersector-00-10-06)
    Former-commit-id: 80264b34c749a163624f90ac775e8d66ae2f1a9e

--- Tracking/TrkExtrapolation/TrkExRungeKuttaIntersector/src/IntersectorWrapper.cxx
+++ Tracking/TrkExtrapolation/TrkExRungeKuttaIntersector/src/IntersectorWrapper.cxx
@@ -35,12 +35,8 @@ IntersectorWrapper::IntersectorWrapper	(const std::string& type,
     :	AthAlgTool		(type, name, parent),
 	m_intersector		("Trk::RungeKuttaIntersector/RungeKuttaIntersector"),
 	m_linePropagator	(""),
-	m_charge{},
-	m_intersection		(nullptr),
-	m_momentum{},
-	m_parameters		(nullptr),
-	m_position{},
-	m_qOverP{}
+	m_intersection		(0),
+	m_parameters		(0)
 {
     declareInterface<Trk::IPropagator>(this);
     declareProperty("Intersector",	m_intersector);
@@ -53,11 +49,24 @@ IntersectorWrapper::~IntersectorWrapper	(void)
 //<<<<<< PUBLIC MEMBER FUNCTION DEFINITIONS                             >>>>>>
 
 StatusCode
-IntersectorWrapper::initialize(){
+IntersectorWrapper::initialize()
+{
+
     // get the Tools
-    ATH_CHECK(m_intersector.retrieve());
-    ATH_MSG_VERBOSE( "Retrieved tool " << m_intersector );
-    if (!m_linePropagator.empty()) ATH_CHECK( m_linePropagator.retrieve()); 
+    if (m_intersector.retrieve().isFailure())
+    {
+        ATH_MSG_FATAL( "Failed to retrieve tool " << m_intersector );
+        return StatusCode::FAILURE;
+    }
+    else
+    {
+        ATH_MSG_INFO( "Retrieved tool " << m_intersector );
+    }
+    if (!m_linePropagator.empty() && m_linePropagator.retrieve().isFailure())
+    {
+        ATH_MSG_FATAL( "Failed to retrieve tool " << m_linePropagator );
+        return StatusCode::FAILURE;
+    }      
     return StatusCode::SUCCESS;
 }
  
