acd7664e53e3d7841f64a63882a8413cc23b8090
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:08:44 +0200

    'CMakeLists.txt' (TrkSegmentConverter-01-00-00)

---
+++ Tracking/TrkTools/TrkSegmentConverter/src/components/TrkSegmentConverter_entries.cxx
@@ -0,0 +1,11 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "TrkSegmentConverter/TrkSegmentConverter.h"
+
+using namespace Trk;
+DECLARE_TOOL_FACTORY( TrkSegmentConverter )
+
+DECLARE_FACTORY_ENTRIES( TrkSegmentConverter )
+{
+	DECLARE_TOOL( TrkSegmentConverter )
+}
+
