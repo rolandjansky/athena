dc574477c1d791e3b743f9b9b091c486c033a112
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:33:00 +0200

    'CMakeLists.txt' (HforTool-02-00-00)

---
+++ Generators/HforTool/src/components/HforTool_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "HforTool/HforTool.h"
+
+DECLARE_TOOL_FACTORY( HforTool )
+
+DECLARE_FACTORY_ENTRIES( HforTool ) {
+  DECLARE_TOOL( HforTool )
+}
