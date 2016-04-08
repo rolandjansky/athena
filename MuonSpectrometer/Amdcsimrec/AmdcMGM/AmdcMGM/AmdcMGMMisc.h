3d2734db6fe84dd6a13e6e5ec23d731043808fcc
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:04:41 +0200

    'CMakeLists.txt' (AmdcMGM-02-00-01)
            * Tagging AmdcMGM-02-00-01.
    	* AmdcMGM/AmdcMGMMisc.h: Remove obsolete #inclues (fix clang
    	compilation).

--- MuonSpectrometer/Amdcsimrec/AmdcMGM/AmdcMGM/AmdcMGMMisc.h
+++ MuonSpectrometer/Amdcsimrec/AmdcMGM/AmdcMGM/AmdcMGMMisc.h
@@ -6,25 +6,12 @@
 #ifndef AmdcMGMMISC_H
 #define AmdcMGMMISC_H
 
-#ifdef HAVE_NEW_IOSTREAMS
-//gcc3.2
 #include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#else
-//gcc 295
-#include <iostream.h>
-#include <iomanip.h>
-#include <fstream.h>
-#endif
-#if HAVE_CXX_STDC_HS
-# include <cstdio>
-# include <cmath>
-#else
-# include <stdio.h>
-# include <math.h>
-#endif
+#include <cstdio>
+#include <cmath>
 #include <algorithm>
 #include <iterator>
 #include <list>
