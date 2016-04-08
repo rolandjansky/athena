4ddda5974243cba2e324dc435df718a1cc08b41f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:52:53 +0200

    'CMakeLists.txt' (MadCUP_i-00-01-00)

---
+++ Generators/MadCUP_i/share/jobOptions.MadCupPythia.py
@@ -0,0 +1,54 @@
+###############################################################
+#
+# Job options file
+#
+#==============================================================
+#--------------------------------------------------------------
+# General Application Configuration options
+#--------------------------------------------------------------
+import AthenaCommon.AtlasUnixGeneratorJob
+
+from AthenaCommon.AppMgr import theApp
+from AthenaCommon.AppMgr import ServiceMgr
+
+# make sure we are loading the ParticleProperty service
+from PartPropSvc.PartPropSvcConf import PartPropSvc
+ServiceMgr += PartPropSvc()
+
+#--------------------------------------------------------------
+# Private Application Configuration options
+#--------------------------------------------------------------
+# Set output level threshold (2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL )
+ServiceMgr.MessageSvc.OutputLevel = INFO
+
+#--------------------------------------------------------------
+# Event related parameters
+#--------------------------------------------------------------
+# Number of events to be processed (default is 10)
+theApp.EvtMax = 5
+#--------------------------------------------------------------
+# Algorithms Private Options
+#--------------------------------------------------------------
+from AthenaServices.AthenaServicesConf import AtRndmGenSvc
+ServiceMgr += AtRndmGenSvc()
+ServiceMgr.AtRndmGenSvc.Seeds = ["PYTHIA 4789899 989240512", "PYTHIA_INIT 820021 2347532"]
+# AtRndmGenSvc.ReadFromFile = true;
+
+
+from AthenaCommon.AlgSequence import AlgSequence
+job=AlgSequence()
+from Pythia_i.Pythia_iConf import Pythia
+job += Pythia()
+job.Pythia.PythiaCommand = ["pyinit user madcup"]
+
+from TruthExamples.TruthExamplesConf import DumpMC
+job += DumpMC()
+
+#---------------------------------------------------------------
+# Ntuple service output
+#---------------------------------------------------------------
+#==============================================================
+#
+# End of job options file
+#
+###############################################################
