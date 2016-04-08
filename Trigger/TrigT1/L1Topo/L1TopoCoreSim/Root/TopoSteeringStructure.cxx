f437527cc33ee60347a501dabb909972afe59dc6
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:21:16 +0200

    'CMakeLists.txt' (L1TopoCoreSim-00-00-23)
    	* added lateMuon for BC+1
    	* L1TopoCoreSim-00-00-23
    
    2015-11-06  Simon George  <s.george@rhul.ac.uk>
    	* Root/TopoSteering.cxx (executeDecisionAlgorithm): Fix coverity
    	#17441 unchecked dynamic_cast
    	* Root/DecisionConnector.cxx (DecisionConnector): fix coverity
    	#17451 uninitialized pointer m_decisionAlgorithm.
    	* Root/TopoCoreSimResult.cxx (output): fix coverity #17454
    	structurally dead code
    	* Root/TopoSteering.cxx (executeConnector): fix coverity #10537
    	unchecked dynamic_cast
    	* Root/TopoSteeringStructure.cxx (setupFromMenu): fix coverity
    	#19071 resource leak
    	* Coverity fixes tracked in ATR-11107
    	* L1TopoCoreSim-00-00-22

--- Trigger/TrigT1/L1Topo/L1TopoCoreSim/Root/TopoSteeringStructure.cxx
+++ Trigger/TrigT1/L1Topo/L1TopoCoreSim/Root/TopoSteeringStructure.cxx
@@ -209,6 +209,7 @@ TCS::TopoSteeringStructure::setupFromMenu(const TXC::L1TopoMenu& menu, bool debu
    // set algorithm parameters
    if(debug)
       cout << "... setting algorithm parameters" << endl;
+   
    for( const TXC::L1TopoConfigAlg & configalgo: menu.getL1TopoConfigAlgs() ) {
 
       ConfigurableAlg * alg = AlgFactory::instance().algorithm(configalgo.name());
@@ -267,6 +268,10 @@ TCS::TopoSteeringStructure::setupFromMenu(const TXC::L1TopoMenu& menu, bool debu
             TCS_EXCEPTION("Sorting algorithm " << alg->name() << " has algoId " << alg->algoId() << " which is already used");
          }
          m_parameters[alg->algoId() + LayoutConstraints::maxComponents()] = ps;
+      } else {
+	// newed parameters unused so delete to avoid memory leak
+	delete ps;
+	ps=0;
       }
    }
 
