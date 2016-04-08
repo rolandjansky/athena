b209d13d170f97d2d70defa0861b3ae060b13b97
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:02:03 +0200

    'CMakeLists.txt' (xAODTrigCalo-00-00-14)
    	* added variables to CaloClusterTrigAuxContainer_v1
    	* tagged as xAODTrigCalo-00-00-14
    
    2015-08-30 craig wiglesworth <craig.wiglesworth@cern.ch>
    	* added CaloClusterTrigAuxContainer (for dyn aux var issue)
    	* tagged as xAODTrigCalo-00-00-13
    
    2015-01-13 craig wiglesworth <craig.wiglesworth@cern.ch>
    	* fixed aux class inheritance issues and added xAOD::TrigEMClusterAuxContainer_v2 (ATR-9716)
    	* tagged as xAODTrigCalo-00-00-12

--- Event/xAOD/xAODTrigCalo/xAODTrigCalo/TrigEMClusterAuxContainer.h
+++ Event/xAOD/xAODTrigCalo/xAODTrigCalo/TrigEMClusterAuxContainer.h
@@ -4,16 +4,16 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: TrigEMClusterAuxContainer.h 631625 2014-11-27 17:24:55Z gwatts $
+// $Id: TrigEMClusterAuxContainer.h 639154 2015-01-13 14:35:02Z wigleswt $
 #ifndef XAODTRIGCALO_TRIGEMCLUSTERAUXCONTAINER_H
 #define XAODTRIGCALO_TRIGEMCLUSTERAUXCONTAINER_H
 
 // Local include(s):
-#include "xAODTrigCalo/versions/TrigEMClusterAuxContainer_v1.h"
+#include "xAODTrigCalo/versions/TrigEMClusterAuxContainer_v2.h"
 
 namespace xAOD {
   /// Define the latest version of the trigger EM cluster auxiliary container
-  typedef TrigEMClusterAuxContainer_v1 TrigEMClusterAuxContainer;
+  typedef TrigEMClusterAuxContainer_v2 TrigEMClusterAuxContainer;
 }
 
 #include "xAODCore/CLASS_DEF.h"
