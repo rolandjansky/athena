94da8d31e39b0b01a5e01cd38adbf8f68911e90f
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:40:50 +0200

    'CMakeLists.txt' (ISF_Algorithms-00-02-00)
    	* move SimKernel.h into src directory. ATLASSIM-1654.
    	* tag as ISF_Algorithms-00-02-00

--- Simulation/ISF/ISF_Core/ISF_Algorithms/src/components/ISF_Algorithms_entries.cxx
+++ Simulation/ISF/ISF_Core/ISF_Algorithms/src/components/ISF_Algorithms_entries.cxx
@@ -1,8 +1,8 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "ISF_Algorithms/SimKernel.h"
+#include "../SimKernel.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( ISF , SimKernel )
 
 DECLARE_FACTORY_ENTRIES( ISF_Algorithms ) {
   DECLARE_NAMESPACE_ALGORITHM( ISF ,  SimKernel )
-}   
+}
