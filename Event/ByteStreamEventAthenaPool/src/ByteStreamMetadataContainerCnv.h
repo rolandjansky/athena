dc2ea24d62b48473d0f7e02a203aaa75e9bc9e2e
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:33:13 +0200

    'CMakeLists.txt' (ByteStreamEventAthenaPool-00-00-01)

---
+++ Event/ByteStreamEventAthenaPool/src/ByteStreamMetadataContainerCnv.h
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef BYTESTREAMMETADATACONTAINERCNV_H
+#define BYTESTREAMMETADATACONTAINERCNV_H
+
+/** @file ByteStreamMetadataContainerCnv.h
+ *  @brief This file contains the class definition for the ByteStreamMetadataContainerCnv class.
+ *  @author Peter van Gemmeren <gemmeren@anl.gov>
+ *  $Id: ByteStreamMetadataContainerCnv.h,v 1.3 2009-03-18 17:48:04 gemmeren Exp $
+ **/
+
+#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+#include "ByteStreamData/ByteStreamMetadataContainer.h"
+#include "ByteStreamEventTPCnv/ByteStreamMetadata_p1.h"
+
+/// the latest persistent representation type of ByteStreamMetadataContainer
+typedef ByteStreamMetadataContainer_p1 ByteStreamMetadataContainer_PERS;
+typedef T_AthenaPoolCustomCnv<ByteStreamMetadataContainer, ByteStreamMetadataContainer_PERS> ByteStreamMetadataContainerCnvBase;
+
+/** @class ByteStreamMetadataContainerCnv
+ *  @brief This class provides a converter for the ByteStreamMetadataContainer class.
+ **/
+class ByteStreamMetadataContainerCnv : public ByteStreamMetadataContainerCnvBase {
+
+friend class CnvFactory<ByteStreamMetadataContainerCnv>;
+
+protected:
+   ByteStreamMetadataContainerCnv(ISvcLocator* svcloc) : ByteStreamMetadataContainerCnvBase(svcloc) {}
+
+   virtual ByteStreamMetadataContainer_PERS* createPersistent(ByteStreamMetadataContainer* transObj);
+   virtual ByteStreamMetadataContainer* createTransient();
+};
+
+#endif
