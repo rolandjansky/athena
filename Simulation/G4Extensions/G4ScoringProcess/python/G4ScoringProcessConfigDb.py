ce8dc9c09cf61a5fa70350370855077352416183
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:44:48 +0200

    'CMakeLists.txt' (G4ScoringProcess-00-01-00)
    	* Updating package to the new G4 Atlas
    	infrastructure. ATLASSIM-2327
    	* Tagging: G4ScoringProcess-00-01-00

---
+++ Simulation/G4Extensions/G4ScoringProcess/python/G4ScoringProcessConfigDb.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addTool
+addTool("G4ScoringProcess.G4ScoringProcessConfig.getG4ScoringProcessTool","G4ScoringProcessTool")
