78ab90a4a7da10a22749cea4b75ee48a964e52db
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 16:15:18 +0200

    'CMakeLists.txt' (InDetGeometryValidation-00-00-01-04)
    	* When running in the CMake build, don't execute the package
    	  build operation as part of the ATN test.
    	* Tagging as InDetGeometryValidation-00-00-01-04

--- InnerDetector/InDetValidation/InDetGeometryValidation/test/InDetGeometryValidation_Test.sh
+++ InnerDetector/InDetValidation/InDetGeometryValidation/test/InDetGeometryValidation_Test.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# In the CMake build just skip this whole thing:
+if [ -z "${CMTPATH}" ]; then
+    exit
+fi
+
 echo "## Running in $PWD"
 cd ../../../InnerDetector/InDetValidation/InDetGeometryValidation/cmt/
 gmake
