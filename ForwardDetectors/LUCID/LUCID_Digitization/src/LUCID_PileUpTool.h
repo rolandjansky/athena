f577c50e78ddd21df75890ca93c47285f64e5fa6
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:58:55 +0200

    'CMakeLists.txt' (LUCID_Digitization-00-10-01)
    	* Clean up clang warnings ATLASSIM-2661.
    	* src/LUCID_PileUpTool.h  remove unused variable m_digitContainer
    	* tagged as LUCID_Digitization-00-10-01
    
    2016-01-29  John Chapman  <John.Chapman@cern.ch>
    
    	* cmt/requirements - add use statement for xAODEventInfo.
    	* src/LUCID_PileUpTool.cxx,
    	src/LUCID_PileUpTool.h (processBunchXing): update interface to use
    	xAODEventInfo rather than PileUpEventInfo. ATLASSIM-2241. Requires
    	PileUpTools-01-18-00 or later. (release 21 onwards) ATLASSIM-2122
    	* tagged as LUCID_Digitization-00-10-00

--- ForwardDetectors/LUCID/LUCID_Digitization/src/LUCID_PileUpTool.h
+++ ForwardDetectors/LUCID/LUCID_Digitization/src/LUCID_PileUpTool.h
@@ -6,12 +6,17 @@
 #define LUCID_DIGITIZATION_TOOL_H
 
 #include "PileUpTools/PileUpToolBase.h"
+
 #include "LUCID_DigitizationToolBox.h"
-#include "EventInfo/PileUpEventInfo.h"  /*SubEvent*/
-#include "GaudiKernel/Property.h"
-#include "GaudiKernel/ServiceHandle.h"
+
 #include "LUCID_SimEvent/LUCID_SimHitCollection.h"
 #include "LUCID_RawEvent/LUCID_DigitContainer.h"
+
+#include "xAODEventInfo/EventInfo.h"   // SubEventIterator
+#include "xAODEventInfo/EventAuxInfo.h"// SubEventIterator
+
+#include "GaudiKernel/Property.h"
+#include "GaudiKernel/ServiceHandle.h"
 
 #include <vector>
 #include <utility> /* pair */
@@ -23,34 +28,34 @@ class IHistSvc;
 
 class LUCID_PileUpTool: public PileUpToolBase, public LUCID_DigitizationToolBox {
 
- public:
-  
+public:
+
   LUCID_PileUpTool(const std::string& type,
-		   const std::string& name,
-		   const IInterface* parent);
-  
-  virtual StatusCode initialize();
-  virtual StatusCode finalize();
+                   const std::string& name,
+                   const IInterface* parent);
+
+  virtual StatusCode initialize() override final;
+  virtual StatusCode finalize() override final;
 
   /// called before the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode prepareEvent(const unsigned int nInputEvents);
-  
+  virtual StatusCode prepareEvent(const unsigned int nInputEvents) override final;
+
   /// called for each active bunch-crossing to process current SubEvents bunchXing is in ns
-  virtual StatusCode processBunchXing(int bunchXing, 
-				      PileUpEventInfo::SubEvent::const_iterator bSubEvents,
-				      PileUpEventInfo::SubEvent::const_iterator eSubEvents);
+  virtual StatusCode processBunchXing(int bunchXing,
+                                      SubEventIterator bSubEvents,
+                                      SubEventIterator eSubEvents) override final;
   /// return false if not interested in  certain xing times (in ns)
   /// implemented by default in PileUpToolBase as FirstXing<=bunchXing<=LastXing
-  //  virtual bool toProcess(int bunchXing) const;
+  //  virtual bool toProcess(int bunchXing) const override final;
 
   /// called at the end of the subevts loop. Not (necessarily) able to access SubEvents
-  virtual StatusCode mergeEvent();
+  virtual StatusCode mergeEvent() override final;
 
   /// perform the digitization - used by LUCID_DigiTop::execute
-  virtual StatusCode processAllSubEvents();
+  virtual StatusCode processAllSubEvents() override final;
 
- private:
-  
+private:
+
   ServiceHandle<IAtRndmGenSvc>  m_atRndmGenSvc;
   ServiceHandle<PileUpMergeSvc> m_mergeSvc;
 
@@ -62,12 +67,12 @@ class LUCID_PileUpTool: public PileUpToolBase, public LUCID_DigitizationToolBox 
   LUCID_DigitizationToolBox *m_digitToolBox;
 
   CLHEP::HepRandomEngine      *m_rndEngine;
-  LUCID_DigitContainer *m_digitContainer;
+  //LUCID_DigitContainer *m_digitContainer;
   ITHistSvc  *m_digitHistSvc;
 
   LUCID_SimHitCollection* m_mergedhitList;
 
-  int    m_numTubes;  
+  int    m_numTubes;
   double m_qdcChannelsPerPE;
   double m_qdcPedestal;
   double m_qdcFedNoiseFactor;
@@ -85,8 +90,8 @@ class LUCID_PileUpTool: public PileUpToolBase, public LUCID_DigitizationToolBox 
   std::vector<double> m_pmtScaling;
   std::vector<double> m_gasScaling;
   std::vector<double> m_npeThreshold;
-  
+
   bool   m_fillRootTree;
 };
- 
+
 #endif
