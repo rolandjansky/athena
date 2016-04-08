2845b0ff2e160d42fc37d72d9102dc115c975eac
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:31:45 +0200

    'CMakeLists.txt' (TileSimEventAthenaPool-00-00-03)
    	* Tagging TileSimEventAthenaPool-00-00-03.
    	* test/TileSimEventAthenaPool.xml: Add test xml file.
    
    	* Tagging TileSimEventAthenaPool-00-00-02.
    	* Add regression test for reading old data files.

---
+++ TileCalorimeter/TileSvc/TileSimEventAthenaPool/share/TileSimEventTPCnv_HITS_test.py
@@ -0,0 +1,18 @@
+infile = 'rtt:valid1.110401.PowhegPythia_P2012_ttbar_nonallhad.simul.HITS.e3099_s2578_tid04919495_00/HITS.04919495._000416.pool.root.1'
+keys = [
+    #TileHitVector_p1
+    'MBTSHits@100',
+    'TileHitVec@100',
+         ]
+
+#from AthenaCommon.JobProperties import jobproperties
+#jobproperties.Global.DetDescrVersion = 'ATLAS-GEO-10-00-00'
+
+theApp.EvtMax = 10
+
+from AthenaCommon.DetFlags      import DetFlags
+DetFlags.detdescr.Calo_setOn()
+noMuon = True
+noID = True
+include ('AthenaPoolUtilities/TPCnvTest.py')
+
