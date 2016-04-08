cd06b72da97144b1b517b393590fd9b68b16f8dd
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:11:21 +0200

    'CMakeLists.txt' (InDetRecToolInterfaces-02-01-08)
    Former-commit-id: bc0292343d4fac1b3a68351e20325ba38cf71384

--- InnerDetector/InDetRecTools/InDetRecToolInterfaces/InDetRecToolInterfaces/IVertexFinder.h
+++ InnerDetector/InDetRecTools/InDetRecToolInterfaces/InDetRecToolInterfaces/IVertexFinder.h
@@ -10,14 +10,7 @@
             : Giacinto Piacquadio (Freiburg University), Kirill Prokofiev (Sheffield University)
     email   : andreas.wildauer@cern.ch
               giacinto.piacquadio@physik.uni-freiburg.de
-    changes : David Shope <david.richard.shope@cern.ch> (2016-04-26)
-
-                EDM Migration to xAOD - from Trk::VxCandidate to xAOD::Vertex
-
-                findVertex will now always return an xAOD::VertexContainer,
-                even when using a TrackCollection or a TrackParticleBaseCollection
-                as input.
-
+    changes :
  ***************************************************************************/
 
 
@@ -38,6 +31,8 @@
 
 */
 
+/* Forward declarations */
+class VxContainer;
 //xAOD includes
 #include "xAODTracking/VertexFwd.h"
 #include "xAODTracking/TrackParticleFwd.h"
@@ -58,9 +53,9 @@ public:
         return IID_IVertexFinder;
     }
     
-    virtual std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> findVertex(const TrackCollection* trackTES)=0;
+    virtual VxContainer* findVertex(const TrackCollection* trackTES)=0;
 
-    virtual std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> findVertex(const Trk::TrackParticleBaseCollection* trackTES)=0;
+    virtual VxContainer* findVertex(const Trk::TrackParticleBaseCollection* trackTES)=0;
 
     /** Find vertex from xAOD::TrackParticleContainer.
      * @param trackParticles input track container
