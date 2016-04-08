1696d754b973b40c21e75263ca099817fb0039b8
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:08:44 +0200

    'CMakeLists.txt' (TrkSegmentConverter-01-00-00)

---
+++ Tracking/TrkTools/TrkSegmentConverter/doc/mainpage.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+@mainpage TrkSegmentConverter Package
+
+>> Please enter an overview of the created package here: What does it contain?
+This package implements the SegmentConverter interface. It uses a TrackFitter to convert the Segments into full Tracks.
+
+@author Christian Schmitt <Christian.Schmitt@cern.ch>
+
+
+@section TrkSegmentConverterReq Requirements
+
+@include requirements
+
+*/
