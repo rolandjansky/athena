c32788e77cca37670a3179d266366eb50c30ecdb
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 17:05:27 +0200

    'CMakeLists.txt' (xAODTrigEgamma-00-00-23)
    	* Add ptvarcone
    	* xAODTrigEgamma-00-00-23
    
    2015-11-04 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Changed the GUID of xAOD::ElectronTrigAuxContainer_v1 and
    	  xAOD::PhotonTrigAuxContainer_v1 to be in all-caps. As this is
    	  required by the I/O system.
    	* Tagging as xAODTrigEgamma-00-00-22
    
    2015-08-28 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Added explicit CLID generation from the package's main library,
    	  since the new aux containers are not used in any component
    	  library at the moment...
    	* Tagging as xAODTrigEgamma-00-00-21
    
    2015-08-28 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Making some small tweaks to have the files in a format that
    	  I liked...
    	* Removed the unnecessary entries from the requirements file.
    	* Added xAODEgamma as a dependence of this package for the
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 58bb55aa1615f5e2a36fe83ebc338ba9d40db459

---
+++ Event/xAOD/xAODTrigEgamma/Root/PhotonTrigAuxContainer_v1.cxx
@@ -0,0 +1,52 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: PhotonTrigAuxContainer_v1.cxx 707253 2015-11-11 19:28:36Z rwhite $
+
+// Local include(s):
+#include "xAODTrigEgamma/versions/PhotonTrigAuxContainer_v1.h"
+
+namespace xAOD {
+
+   PhotonTrigAuxContainer_v1::PhotonTrigAuxContainer_v1()
+      : AuxContainerBase() {
+
+/// Include the egamma variables:
+#define EGAMMA_AUX_VARIABLE( TYPE, NAME ) AUX_VARIABLE( NAME )
+#include "xAODEgamma/versions/EgammaAuxVariables_v3.def"
+#undef EGAMMA_AUX_VARIABLE
+
+      /// Links to track particles
+      AUX_VARIABLE(vertexLinks);
+
+      /// @name Vertex Match details
+      /// @{
+
+      AUX_VARIABLE(convMatchDeltaEta1);
+      AUX_VARIABLE(convMatchDeltaEta2);
+      AUX_VARIABLE(convMatchDeltaPhi1);
+      AUX_VARIABLE(convMatchDeltaPhi2);
+      AUX_VARIABLE(ptcone20);
+      AUX_VARIABLE(ptcone30);
+      AUX_VARIABLE(ptcone40);
+      AUX_VARIABLE(ptvarcone20);
+      AUX_VARIABLE(ptvarcone30);
+      AUX_VARIABLE(ptvarcone40);
+      AUX_VARIABLE(etcone20);
+      AUX_VARIABLE(etcone30);
+      AUX_VARIABLE(etcone40);
+      AUX_VARIABLE(topoetcone20);
+      AUX_VARIABLE(topoetcone30);
+      AUX_VARIABLE(topoetcone40);
+      AUX_VARIABLE(Loose);
+      AUX_VARIABLE(Medium);
+      AUX_VARIABLE(Tight);
+      AUX_VARIABLE(isEMLoose);
+      AUX_VARIABLE(isEMMedium);
+      AUX_VARIABLE(isEMTight);
+
+      /// @}
+   }
+
+} // namespace xAOD
---
+++ Event/xAOD/xAODTrigEgamma/xAODTrigEgamma/versions/PhotonTrigAuxContainer_v1.h
@@ -0,0 +1,120 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// $Id: PhotonTrigAuxContainer_v1.h 707253 2015-11-11 19:28:36Z rwhite $
+#ifndef XAODTRIGEGAMMA_VERSIONS_PHOTONTRIGAUXCONTAINER_V1_H
+#define XAODTRIGEGAMMA_VERSIONS_PHOTONTRIGAUXCONTAINER_V1_H
+
+// System include(s):
+#include <vector>
+
+// EDM include(s):
+#include "AthLinks/ElementLink.h"
+#include "xAODCore/AuxContainerBase.h"
+
+// xAOD include(s):
+#include "xAODTracking/VertexContainer.h"
+#include "xAODTracking/TrackParticleContainer.h"
+#include "xAODCaloEvent/CaloClusterContainer.h"
+
+namespace xAOD {
+
+   /// Auxiliary store for offline photons
+   ///
+   /// This class is used as the auxiliary store for offline reconstructed
+   /// photons.
+   /// This is the trigger version to inherit from ByteStreamAuxContainer to persist all dynamic varibles
+   /// @author Christos Anastopoulos
+   /// @author Anthony Morley
+   /// @author Ryan White
+   ///
+   class PhotonTrigAuxContainer_v1 : public AuxContainerBase {
+
+   public:
+      /// @name xAOD::PhotonAuxContainer constructors
+      PhotonTrigAuxContainer_v1();
+
+   private:
+      /// Include the egamma variables:
+#define EGAMMA_AUX_VARIABLE( TYPE, NAME ) TYPE NAME
+#include "xAODEgamma/versions/EgammaAuxVariables_v3.def"
+#undef EGAMMA_AUX_VARIABLE
+
+      ///@name vector of links to vertexes
+      /// @{
+
+      std::vector< std::vector< ElementLink< VertexContainer > > > vertexLinks;
+
+      /// @}
+
+      /// @name Track Match details
+      /// @{
+
+      /// Difference between the cluster eta and the eta of the first track of
+      /// the vertex extrapolated to the second sampling.
+      std::vector< float > convMatchDeltaEta1;
+
+      /// Difference between the cluster eta and the eta of the second track of
+      /// the vertex extrapolated to the second sampling.
+      std::vector< float > convMatchDeltaEta2;
+
+      /// Difference between the cluster phi and the phi of the first track of
+      /// the vertex extrapolated to the second sampling.
+      std::vector< float > convMatchDeltaPhi1;
+
+      /// Difference between the cluster phi and the phi of the second track of
+      /// the vertex extrapolated to the second sampling.
+      std::vector< float > convMatchDeltaPhi2;
+
+      /// @brief ET in a cone with half-opening angle 0.2, with exclusion of a window of size 7x5 in electromagnetic calorimeter and exclude CaloSampling::TileGap3
+      std::vector< float >  etcone20; 
+      /// @brief ET in a cone with half-opening angle 0.3, with exclusion of a window of size 7x5 in electromagnetic calorimeter and exclude CaloSampling::TileGap3
+      std::vector< float >  etcone30; 
+      /// @brief ET in a cone with half-opening angle 0.4, with exclusion of a window of size 7x5 in electromagnetic calorimeter and exclude CaloSampling::TileGap3
+      std::vector< float >  etcone40; 
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.2 (no zvx cut photons, 1mm electrons)
+      std::vector< float > ptcone20;
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.3
+      std::vector< float > ptcone30;
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.4
+      std::vector< float > ptcone40;
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.2 (no zvx cut photons, 1mm electrons)
+      std::vector< float > ptvarcone20;
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.3
+      std::vector< float > ptvarcone30;
+      /// @brief summed pt of tracks in a cone with half-opening angle 0.4
+      std::vector< float > ptvarcone40;
+      //et cone for topo cluster
+      /// @brief ET in a cone with half-opening angle 0.20
+      std::vector< float >  topoetcone20;
+      /// @brief ET in a cone with half-opening angle 0.30
+      std::vector< float >  topoetcone30;
+      /// @brief ET in a cone with half-opening angle 0.40
+      std::vector< float >  topoetcone40;
+      /// @brief Pid decision
+      std::vector< char > Loose;
+      /// @brief Pid decision
+      std::vector< char > Medium;
+      /// @brief Pid decision
+      std::vector< char > Tight;
+      /// @brief Pid decision isEM word
+      std::vector< unsigned int > isEMLoose;
+      /// @brief Pid decision isEM word
+      std::vector< unsigned int > isEMMedium;
+      /// @brief Pid decision isEM word
+      std::vector< unsigned int > isEMTight;
+
+      ///@}
+
+   }; // class PhotonTrigAuxContainer_v1
+
+} // namespace xAOD
+
+// Set up the StoreGate inheritance for the class:
+#include "xAODCore/BaseInfo.h"
+SG_BASE( xAOD::PhotonTrigAuxContainer_v1, xAOD::AuxContainerBase );
+
+#endif // XAODTRIGEGAMMA_VERSIONS_PHOTONAUXCONTAINER_V3_H
