a7b13f6c4338a089776e422e59538e8743790519
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:47:22 +0200

    'CMakeLists.txt' (MinBiasScintillator-00-03-05)
    	* Tagging MinBiasScintillator-00-03-05.
    	* src/MinBiasScintillatorSD.cc (MinBiasScintillatorSD): Fix
    	compilation with gcc5: G4cout -> G4endl.
    
    	* Tagging MinBiasScintillator-00-03-04.
    
    2016-02-21  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* MinBiasScintillatorSD: fixed some class member names to follow ATLAS conventions
    	* MinBiasScintSDOptions.h: fixed constructor initialization warning

--- TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/MinBiasScintSDOptions.h
+++ TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/MinBiasScintSDOptions.h
@@ -6,47 +6,50 @@
 #define MINBIASSCINTILLATOR_MINBIASSCINTSDOPTIONS_h
 
 /* This is a very simple struct for holding sensitive detector
-configuration options; formerly this was in
-TileCalorimeter/TileSimUtils/TileSimUtils/TileG4SimOptions.h
-But now that there are real tools, that setup is not necessary
-*/
+ configuration options; formerly this was in
+ TileCalorimeter/TileSimUtils/TileSimUtils/TileG4SimOptions.h
+ But now that there are real tools, that setup is not necessary
+ */
 
 #include <string>
 #include <vector>
 
 struct MinBiasScintSDOptions {
-  /** Setup defaults */
-  MinBiasScintSDOptions()
-   : timeCut ( 350.5 )
-   , tileTB ( false )
-   , doBirk ( true )
-   , doTOFCorrection ( true )
-   , rDBAccessSvcName ( "RDBAccessSvc" )
-   , geoModelSvcName ( "GeoModelSvc" )
-  { deltaTHit = { 0.5 , -75.25 , 75.25 , 5. }; }
+    /** Setup defaults */
+    MinBiasScintSDOptions()
+        : timeCut(350.5),
+          tileTB(false),
+          doBirk(true),
+          doTOFCorrection(true),
+          verboseLevel(0),
+          rDBAccessSvcName("RDBAccessSvc"),
+          geoModelSvcName("GeoModelSvc")
+    {
+      deltaTHit = {0.5 , -75.25 , 75.25 , 5.};
+    }
 
-  /** A time granularity for G4 hits in TileHit
-   it can be just one number, e.g. 5.0 which means that the same
-   granularity is used everywhere or it can be a vector which contains
-   3*N+1 elements, e.g. 0.1,-5,5, 0.5,-75,75, 5.0 which means that
-   for [-5,5] ns interval granularity 0.1ns will be used
-   for [-75,75] ns interval granularity 0.5ns will be used
-   and 5ns granularity will be used for all other hits  */
-  std::vector<double> deltaTHit;
-  /** Time cut for hits, all hits go to one single time bin if time is above this cut */
-  double timeCut;
-  /** Flag to indicate TileCal TestBeam configuration */
-  bool tileTB;
-  /** Flag to enable Birk's law */
-  bool doBirk;
-  /** Flag to apply TOF correction (subtract Time Of Flight from ATLAS center) */
-  bool   doTOFCorrection;
-  /** Helper to keep the same verbosity everywhere */
-  int verboseLevel;
-  //** Name of the RDBAccessSvc instance to use */
-  std::string rDBAccessSvcName;
-  //** Name of the GeoModelSvc instance to use */
-  std::string geoModelSvcName;
-};
+    /** A time granularity for G4 hits in TileHit
+     it can be just one number, e.g. 5.0 which means that the same
+     granularity is used everywhere or it can be a vector which contains
+     3*N+1 elements, e.g. 0.1,-5,5, 0.5,-75,75, 5.0 which means that
+     for [-5,5] ns interval granularity 0.1ns will be used
+     for [-75,75] ns interval granularity 0.5ns will be used
+     and 5ns granularity will be used for all other hits  */
+    std::vector<double> deltaTHit;
+    /** Time cut for hits, all hits go to one single time bin if time is above this cut */
+    double timeCut;
+    /** Flag to indicate TileCal TestBeam configuration */
+    bool tileTB;
+    /** Flag to enable Birk's law */
+    bool doBirk;
+    /** Flag to apply TOF correction (subtract Time Of Flight from ATLAS center) */
+    bool doTOFCorrection;
+    /** Helper to keep the same verbosity everywhere */
+    int verboseLevel;
+    //** Name of the RDBAccessSvc instance to use */
+    std::string rDBAccessSvcName;
+    //** Name of the GeoModelSvc instance to use */
+    std::string geoModelSvcName;
+  };
 
 #endif
--- TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/MinBiasScintillatorSD.h
+++ TileCalorimeter/TileG4/TileAncillary/MinBiasScintillator/src/MinBiasScintillatorSD.h
@@ -33,81 +33,85 @@ class G4TouchableHistory;
 
 class TileTBID;
 
-class MinBiasScintillatorSD : public G4VSensitiveDetector
-{
-public:
-  MinBiasScintillatorSD(const G4String& name, const std::string& hitCollectionName, const MinBiasScintSDOptions& opts);
-  ~MinBiasScintillatorSD();
+class MinBiasScintillatorSD: public G4VSensitiveDetector {
+  public:
+    MinBiasScintillatorSD(const G4String& name, const std::string& hitCollectionName,
+        const MinBiasScintSDOptions& opts);
+    ~MinBiasScintillatorSD();
 
-  virtual void Initialize(G4HCofThisEvent*) override final;
+    virtual void Initialize(G4HCofThisEvent*) override final;
 
-  virtual G4bool ProcessHits(G4Step*, G4TouchableHistory*) override final;
+    virtual G4bool ProcessHits(G4Step*, G4TouchableHistory*) override final;
 
-  void EndOfAthenaEvent();
+    void EndOfAthenaEvent();
 
-private:
-  // Options for the SD configuration
-  const MinBiasScintSDOptions m_options;
+  private:
+    // Options for the SD configuration
+    const MinBiasScintSDOptions m_options;
 
-  // The hits collection
-  SG::WriteHandle<TileHitVector> m_HitColl;
+    // The hits collection
+    SG::WriteHandle<TileHitVector> m_HitColl;
 
-  const TileTBID* m_tileTBID;
+    const TileTBID* m_tileTBID;
 
-  static const int nSide = 2;
-  static const int nPhi  = 8;
-  static const int nEta  = 2;
-  static const int nCell = nSide*nPhi*nEta;
-  static const int nDist = nSide*nEta;
+    static const int N_SIDE = 2;
+    static const int N_PHI = 8;
+    static const int N_ETA = 2;
+    static const int N_CELLS = N_SIDE * N_PHI * N_ETA;
+    static const int N_DIST = N_SIDE * N_ETA;
 
-  inline int cell_index(int side, int phi, int eta) const { return (side*nPhi+phi)*nEta+eta; }
-  inline int dist_index(int side, int eta) const { return side*nEta+eta; }
-
-  /** @brief number of contributions to the energy in each cell
-      (size=nCell).
-   */
-  std::vector<int> m_numberOfHitsInCell;
-
-  /** @brief local temporary TileSimHit for each cell (size=nCell).
-   */
-  std::vector<TileSimHit*> m_tempSimHit;
-
-  /** @brief array to cache look-ups of Identifiers for each cell
-      (size=nCell).
-   */
-  Identifier m_channelID[nCell];
-
-  /** @brief granularity in time for hits
-   */
-  double m_deltaT;
-  std::vector<double> m_deltaTvec;
-
-  /** @brief max allowed time for hits
-   */
-  double m_timeCut;
-
-  /** @brief time for hits which are above m_timeCut threshold
-      it is equal to m_tilesize_deltaT - m_deltaT
-   */
-  double m_lateHitTime;
-
-  double m_deltaT_cell[nDist];
-
-  G4double BirkLaw(const G4Step* aStep) const;
-
-  /** @brief function to provide correct deltaT bin
-      width for given time
-  */
-
-  inline double deltaT(double time) const {
-    unsigned int i=0;
-    double delta = m_deltaTvec[i++];
-    while (i<m_deltaTvec.size()) {
-      if (time>m_deltaTvec[i++] && time<m_deltaTvec[i++]) break;
-      delta = m_deltaTvec[i++];
+    inline int cell_index(int side, int phi, int eta) const {
+      return (side * N_PHI + phi) * N_ETA + eta;
     }
-    return delta;
-  }
+    inline int dist_index(int side, int eta) const {
+      return side * N_ETA + eta;
+    }
+
+    /** @brief number of contributions to the energy in each cell
+     (size=nCell).
+     */
+    std::vector<int> m_numberOfHitsInCell;
+
+    /** @brief local temporary TileSimHit for each cell (size=nCell).
+     */
+    std::vector<TileSimHit*> m_tempSimHit;
+
+    /** @brief array to cache look-ups of Identifiers for each cell
+     (size=nCell).
+     */
+    Identifier m_channelID[N_CELLS];
+
+    /** @brief granularity in time for hits
+     */
+    double m_deltaT;
+    std::vector<double> m_deltaTvec;
+
+    /** @brief max allowed time for hits
+     */
+    double m_timeCut;
+
+    /** @brief time for hits which are above m_timeCut threshold
+     it is equal to m_tilesize_deltaT - m_deltaT
+     */
+    double m_lateHitTime;
+
+    double m_deltaT_cell[N_DIST];
+
+    G4double BirkLaw(const G4Step* aStep) const;
+
+    /** @brief function to provide correct deltaT bin
+     width for given time
+     */
+
+    inline double deltaT(double time) const {
+      unsigned int i = 0;
+      double delta = m_deltaTvec[i++];
+      while (i < m_deltaTvec.size()) {
+        if (time > m_deltaTvec[i++] && time < m_deltaTvec[i++]) break;
+        delta = m_deltaTvec[i++];
+      }
+      return delta;
+    }
 };
 
 #endif //MINBIASSCINTILLATOR_MINBIASSCINTILLATORSD_H
