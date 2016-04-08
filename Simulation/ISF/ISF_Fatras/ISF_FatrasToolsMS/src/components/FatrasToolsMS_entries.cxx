baf9a196ca8acd4ad4ec9199096ef2c724101751
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 18:20:22 +0200

    'CMakeLists.txt' (ISF_FatrasToolsMS-00-02-03)

---
+++ Simulation/ISF/ISF_Fatras/ISF_FatrasToolsMS/src/components/FatrasToolsMS_entries.cxx
@@ -0,0 +1,10 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "ISF_FatrasToolsMS/SimHitCreatorMS.h"
+
+DECLARE_NAMESPACE_TOOL_FACTORY( iFatras, SimHitCreatorMS )
+
+DECLARE_FACTORY_ENTRIES( ISF_FatrasToolsMS )
+{
+  DECLARE_NAMESPACE_TOOL( iFatras, SimHitCreatorMS )
+}
+
