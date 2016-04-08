c262c56ba06645d3955c31a0282ffe3c8190a28c
Atlas-Software Librarian <Atlas-Software.Librarian@cern.ch>
2016-04-08 19:21:33 +0200

    'CMakeLists.txt' (L1CommonCore-00-00-07)
    	* include CTPdataformatVersion.h from CTPfragment and print message
    	* L1CommonCore-00-00-07

---
+++ Trigger/TrigT1/L1CommonCore/L1CommonCore/CTPdataformatVersion.h
@@ -0,0 +1,6 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#pragma message( "The package L1CommonCore is obsolete and will be removed" )
+#include "CTPfragment/CTPdataformatVersion.h"
