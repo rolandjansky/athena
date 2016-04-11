f5bb6bd8d682772d24636ec1c247dd4b74aa3b00
Pavol Strizenec <Pavol.Strizenec@cern.ch>
2016-04-11 13:54:55 +0200

    Correcting wrong commit (LArRecEvent-01-03-15-02)
    	* Modified LArNoisyROSummary.cxx and LArNoisyROSummary.h
    	* Adding additional MNB information ... we now keep lists of febs which are deemed "mini-noisy" in the same
    		way that we previously kept this information for regular noisy FEBs.
            * firs wrong commit by Pavol corrected in:
    	* tag tag LArRecEvent-01-03-15-02

--- LArCalorimeter/LArRecEvent/LArRecEvent/LArNoisyROSummary.h
+++ LArCalorimeter/LArRecEvent/LArRecEvent/LArNoisyROSummary.h
@@ -39,20 +39,29 @@ class LArNoisyROSummary
   //** clear internal data (useful for ARA that does not unpack to a new object every time) */
   void clear();
 
-
   //** Set the list of noisy FEBs via the FEB HWIdentifiers */
   void set_noisy_febs(const std::vector<HWIdentifier>& );
 
   //** add a FEB to the bad FEB list */
   void add_noisy_feb(HWIdentifier febid);
+  
+  //** Set the list of MNB Tight FEBs via the FEB HWIdentifiers */
+  void set_MNBTight_febs(const std::vector<HWIdentifier>& );
+  
+  //** add an MNB Tight FEB to the bad FEB list */
+  void add_MNBTight_feb(HWIdentifier febid);
 
+  //** Set the list of MNB Tight FEBs via the FEB HWIdentifiers */
+  void set_MNBLoose_febs(const std::vector<HWIdentifier>& );
+
+  //** add an MNB Tight FEB to the bad FEB list */
+  void add_MNBLoose_feb(HWIdentifier febid);
 
   //** Set the list of noisy preamps: for each FEB identifier via HWIdentifier keep the noisy preamps as the first channel number for that preamp */
   void set_noisy_preamps(const std::vector< std::pair<HWIdentifier, std::vector<int> > >&);
 
   //** add noisy preamp as FEB id + first channel for preamp */
   void add_noisy_preamp(HWIdentifier febid, int channel);
-
 
   //** set Partition bit pattern for bad FEB flagging **//
   void SetBadFEBFlaggedPartitions(uint8_t bitpattern) { m_BadFEBFlaggedPartitions = bitpattern;}
@@ -72,9 +81,14 @@ class LArNoisyROSummary
   //** Set Partition bit pattern for mini-noise-burst flagging **/
   void SetMNBLooseFlaggedPartitions(uint8_t bitpattern) { m_MNBLooseFlaggedPartitions=bitpattern; }
 
-
   //** retrieve noisy FEBs by id */
   const std::vector<HWIdentifier>& get_noisy_febs() const;
+
+  //** retrieve MNB Tight FEBs by id */
+  const std::vector<HWIdentifier>& get_MNBTight_febs() const;
+
+  //** retrieve MNB Loose FEBs by id */
+  const std::vector<HWIdentifier>& get_MNBLoose_febs() const;
 
   //** retrieve noisy preamplifiers */
   const std::vector<std::pair<HWIdentifier, std::vector<int> > >& get_noisy_preamps() const;
@@ -92,17 +106,22 @@ class LArNoisyROSummary
   uint8_t SatTightFlaggedPartitions() const {return m_SatTightFlaggedPartitions;}
 
   //** Partition bit map for mini-noise-burst flagging **//
-  uint8_t MNBLooseFlaggedPartititions() const {return m_MNBLooseFlaggedPartitions;}
+  uint8_t MNBLooseFlaggedPartitions() const {return m_MNBLooseFlaggedPartitions;}
 
   //** Partition bit map for mini-noise-burst flagging (tight) **//
-  uint8_t MNBTightFlaggedPartititions() const {return m_MNBTightFlaggedPartitions;}
+  uint8_t MNBTightFlaggedPartitions() const {return m_MNBTightFlaggedPartitions;}
 
 
  private:
 
   //** List of noisy FEBs */
   std::vector<HWIdentifier> m_noisy_febs;
+  
+  //** List of MNB Tight FEBs */
+  std::vector<HWIdentifier> m_MNBTight_febs;
 
+  //** List of MNB Loose FEBs */
+  std::vector<HWIdentifier> m_MNBLoose_febs;
 
   //** List of noisy preamps, for each FEB identifier via HWIdentifier keep the noisy preamps as the first channel number for that preamp */
   std::vector< std::pair<HWIdentifier, std::vector<int> > > m_noisy_preamps;
