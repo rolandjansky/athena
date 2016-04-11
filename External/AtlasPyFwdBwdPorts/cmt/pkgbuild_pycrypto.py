26461f63d1b35501372198e08bd9ea76e98a61f2
Scott Snyder <scott.snyder@cern.ch>
2016-04-11 23:15:46 +0200

    'Fix compilation warnings in cmake build.' (AtlasPyFwdBwdPorts-00-03-08)
    	* Tagging AtlasPyFwdBwdPorts-00-03-08.
    	* Fix compilation warnings in cmake build.
    
    2016-03-16  Frank Winklmeier  <fwinkl@cern>
    	* bump pyflakes to 1.1.0
    	* Tagging as AtlasPyFwdBwdPorts-00-03-07
    
    2016-03-03  scott snyder  <snyder@bnl.gov>
    	* Tagging AtlasPyFwdBwdPorts-00-03-06.
    	* cmt/requirements: Fix for clang build.
    
    2016-01-29 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Taught the code how to install the python code correctly when using
    	  the "DESTDIR mechanism".
    	* Tagging as AtlasPyFwdBwdPorts-00-03-05
    
    2016-01-28 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Only noticed now that the installed python modules were getting
    	  in the wrong directly all along... :-(
    ...
    (Long ChangeLog diff - truncated)

--- External/AtlasPyFwdBwdPorts/cmt/pkgbuild_pycrypto.py
+++ External/AtlasPyFwdBwdPorts/cmt/pkgbuild_pycrypto.py
@@ -13,4 +13,6 @@ def configure(self):
     env = self.env
     # there are C++-style comments in various C files...
     env['CFLAGS'] = env['CFLAGS'].replace('-ansi', '-std=c99')
+    if env['CC'].find ('clang')>=0 or env['CC'].find ('icc')>=0:
+        env['CFLAGS'] = env['CFLAGS'] + ' -DLTC_NO_ASM '
     return dist_configure(self)
