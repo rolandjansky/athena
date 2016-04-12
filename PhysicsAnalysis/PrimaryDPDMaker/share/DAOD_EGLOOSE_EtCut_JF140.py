121da7c320bd9e708b979da1c0fda6809a7705cd
Paul James Laycock <paul.james.laycock@cern.ch>
2016-04-12 16:28:04 +0200

    Adding DESDM_EXOTHIP and DAOD_IDLUMI (PrimaryDPDMaker-00-06-01)
    	* Added back DESDM_EXOTHIP (was DESD_HIPs)
    	* Added DAOD_IDLUMI
    	* Tagging as PrimaryDPDMaker-00-06-01
    
    2016-04-08 Paul Laycock <paul.james.laycock@cern.ch>
    	* Removed ancient formats from share and ancient filters from python
    	* Reduced steering in share/PrimaryDPDMaker.py and python/PrimaryDPDMaker.py to minimum
    	* Tagging as PrimaryDPDMaker-00-06-00
    
    2016-03-29 Graeme Stewart <graeme.andrew.stewart@cern.ch>
    	* share/PrimaryDPDMaker.py
    	- Cosmetic code changes and tidy up useless messages
    
    2016-04-05 Paul Laycock <paul.james.laycock@cern.ch>
    	* Adding DAOD_IDNCB format for non-colliding backgrounds group
    	* Tagging as PrimaryDPDMaker-00-05-42
    
    2016-03-29 Graeme Stewart <graeme.andrew.stewart@cern.ch>
    	* Remove import of RAWPerfDPD_Zmumu.py from PrimaryDPDMaker.py (old
    	  Run1 DRAW, now superceded by DRAW_ZMUMU.py)
    ...
    (Long ChangeLog diff - truncated)

--- PhysicsAnalysis/PrimaryDPDMaker/share/DAOD_EGLOOSE_EtCut_JF140.py
+++
@@ -1,2 +0,0 @@
-DAOD_EGLOOSE_photonFilter.cutEtMin   = 145. * Units.GeV
-DAOD_EGLOOSE_electronFilter.cutEtMin = 145. * Units.GeV
--- PhysicsAnalysis/PrimaryDPDMaker/share/DAOD_EGLOOSE_EtCut_JF70.py
+++
@@ -1,2 +0,0 @@
-DAOD_EGLOOSE_photonFilter.cutEtMin   = 75. * Units.GeV
-DAOD_EGLOOSE_electronFilter.cutEtMin = 75. * Units.GeV
