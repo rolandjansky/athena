7a61785e66578c93b8d214ad2da3047b3892bd82
Paul James Laycock <paul.james.laycock@cern.ch>
2016-04-12 16:28:04 +0200

    Adding DESDM_EXOTHIP and DAOD_IDLUMI (PrimaryDPDMaker-00-06-01)
    	* Added back DESDM_EXOTHIP (was DESD_HIPs)
    	* Added DAOD_IDLUMI
    	* Tagging as PrimaryDPDMaker-00-06-01
    
    2016-04-08 Paul Laycock <paul.james.laycock@cern.ch>
    	* Removed ancient formats from share and ancient filters from python
    	* Reduced steering in share/PrimaryDPDMaker.py and python/PrimaryDPDMaker.py to minimum
    	* Tagging as PrimaryDPDMaker-00-06-00
    
    2016-03-29 Graeme Stewart <graeme.andrew.stewart@cern.ch>
    	* share/PrimaryDPDMaker.py
    	- Cosmetic code changes and tidy up useless messages
    
    2016-04-05 Paul Laycock <paul.james.laycock@cern.ch>
    	* Adding DAOD_IDNCB format for non-colliding backgrounds group
    	* Tagging as PrimaryDPDMaker-00-05-42
    
    2016-03-29 Graeme Stewart <graeme.andrew.stewart@cern.ch>
    	* Remove import of RAWPerfDPD_Zmumu.py from PrimaryDPDMaker.py (old
    	  Run1 DRAW, now superceded by DRAW_ZMUMU.py)
    ...
    (Long ChangeLog diff - truncated)

--- PhysicsAnalysis/PrimaryDPDMaker/share/DAOD_EGLOOSE_EtCut_JF17.py
+++
@@ -1,2 +0,0 @@
-DAOD_EGLOOSE_photonFilter.cutEtMin   = 20. * Units.GeV
-DAOD_EGLOOSE_electronFilter.cutEtMin = 20. * Units.GeV
---
+++ PhysicsAnalysis/PrimaryDPDMaker/share/PerfDESDM_TILEMU.py
@@ -0,0 +1,158 @@
+##----------------------------------------------------------------------
+## Name: PerfDESDM_TILEMU.py
+##
+## Author: Tomoe Kishimoto
+## Email:  Tomoe.Kishimoto@cern.ch
+##
+## Description: This defines the content of the commissioning stream for 
+##              Tile-Muon trigger
+##
+##----------------------------------------------------------------------
+
+# If this file is already included, don't include it again
+include.block("PrimaryDPDMaker/PerfDESDM_TILEMU.py")
+
+## for messaging
+from AthenaCommon.Logging import logging
+muonDPDStream_msg = logging.getLogger( 'PerfDESDM_TILEMUStream' )
+
+## Import the module that allows to use named units, e.g. GeV
+import AthenaCommon.SystemOfUnits as Units
+
+## Include the job property flags for this package
+from PrimaryDPDMaker.PrimaryDPDFlags import primDPD
+from PrimaryDPDMaker.PrimaryDPDHelpers import buildFileName
+from RecExConfig.RecFlags import rec
+
+## Include the job property flags for this package
+from PrimaryDPDMaker.PrimaryDESDMFlags_TileMu import primDESDMTileMu
+
+## This handels multiple output streams
+from OutputStreamAthenaPool.MultipleStreamManager import MSMgr
+
+##======================================================================
+## Write the used options to the log file
+##======================================================================
+if rec.OutputLevel() <= DEBUG:
+    muonDPDStream_msg.info('Values of all PerfDESDM_TileMu flags:')
+    print primDESDMTileMu
+    pass
+
+# ======================================================================
+# Create a sub-sequence where all needed algorithms will run in
+# ======================================================================
+import AthenaCommon.CfgMgr as CfgMgr
+desdTileMuonSequence = CfgMgr.AthSequencer('DESDM_TILEMU_Sequence',
+                                          StopOverride = True )
+topSequence += desdTileMuonSequence
+
+##======================================================================
+## Define the skimming for the DESDM_TILEMU output stream
+##======================================================================
+from PrimaryDPDMaker.LogicalFilterCombiner import LogicalFilterCombiner
+from PrimaryDPDMaker.PrimaryDPDMakerConf   import PrimaryDPDPrescaler
+        
+from RecExConfig.ObjKeyStore import objKeyStore
+muonDPDStream_msg.debug("Items in objKeyStore[transient]: %s" %  
+    objKeyStore['transient'].list() )
+
+# ----------------------------------------------------------------------
+# String based reduction
+# ----------------------------------------------------------------------
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__FilterCombinationOR
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__FilterCombinationAND
+from DerivationFrameworkCore.DerivationFrameworkCoreConf import DerivationFramework__DerivationKernel
+
+# ----------------------------------------------------------------------
+# Muon HLT triggers:
+# ----------------------------------------------------------------------
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__TriggerSkimmingTool
+TileMuTriggerSkimmingTool = DerivationFramework__TriggerSkimmingTool(name = "TileMuTriggerSkimmingTool",
+    TriggerListOR =  ["HLT_mu.*"] )
+
+ToolSvc += TileMuTriggerSkimmingTool
+print TileMuTriggerSkimmingTool
+
+# ----------------------------------------------------------------------
+# Muon selection
+# ----------------------------------------------------------------------
+muon_type = '(Muons.muonType == 0)'
+muon_pt   = '(Muons.pt > 13*GeV)'
+muon_eta  = '((abs(Muons.eta) < 1.5) && (abs(Muons.eta) > 0.8))'
+
+muon_expression = '(count('+muon_type+' && '+muon_pt+' && '+muon_eta+') > 0)'
+
+TileMuEventSkimmingTool = DerivationFramework__xAODStringSkimmingTool(name = "TileMuEventSkimmingTool",
+    expression = muon_expression)
+
+ToolSvc += TileMuEventSkimmingTool
+print TileMuEventSkimmingTool
+
+# ----------------------------------------------------------------------
+# Final logical selection
+# ----------------------------------------------------------------------
+TileMuEventFilterTool = DerivationFramework__FilterCombinationAND(name="TileMuEventFilterTool",
+    FilterList=[TileMuTriggerSkimmingTool, TileMuEventSkimmingTool])
+
+ToolSvc += TileMuEventFilterTool
+print TileMuEventFilterTool
+
+desdTileMuonSequence += CfgMgr.DerivationFramework__DerivationKernel("TileMuKernel", 
+    SkimmingTools = [TileMuEventFilterTool])
+
+##======================================================================
+## Define this Muon DPD output stream
+##======================================================================
+streamName = primDPD.WriteDESDM_TILEMUStream.StreamName
+fileName   = buildFileName( primDPD.WriteDESDM_TILEMUStream )
+algsToBookkeep = []
+
+if primDPD.WriteDESDM_TILEMUStream.isVirtual or primDPD.isVirtual() :
+    TileMuStream = MSMgr.NewVirtualStream( streamName, fileName )
+    pass
+else:
+    TileMuStream = MSMgr.NewPoolStream( streamName, fileName )
+    # Add the per-event bookkeeping 
+    TileMuStream.AddOtherAlgsToBookkeep( algsToBookkeep )
+    pass
+
+if primDESDMTileMu.ApplySkimming():
+    TileMuStream.AcceptAlgs(["TileMuKernel"])
+    pass
+
+#-----------------------------------------------------------------------
+# Add the containers to the output stream
+#-----------------------------------------------------------------------
+from PrimaryDPDMaker import PrimaryDPD_OutputDefinitions as dpdOutput
+ExcludeList=[]
+
+#dpdOutput.addAllItemsFromInputExceptExcludeList(streamName,ExcludeList)
+
+
+TileMuStream.AddItem(["xAOD::EventInfo#*"])
+TileMuStream.AddItem(["xAOD::EventAuxInfo#*"])
+
+TileMuStream.AddItem(["xAOD::MuonContainer#Muons"])
+TileMuStream.AddItem(["xAOD::MuonAuxContainer#MuonsAux."])
+
+TileMuStream.AddItem(["xAOD::TrigNavigation#TrigNavigation"])
+TileMuStream.AddItem(["xAOD::TrigNavigationAuxInfo#TrigNavigationAux."])
+TileMuStream.AddItem(["xAOD::TrigDecision#xTrigDecision"])
+TileMuStream.AddItem(["xAOD::TrigDecisionAuxInfo#xTrigDecisionAux."])
+
+TileMuStream.AddItem(["xAOD::MuonRoIContainer#LVL1MuonRoIs"])
+TileMuStream.AddItem(["xAOD::MuonRoIAuxContainer#LVL1MuonRoIsAux."])
+TileMuStream.AddItem(["MuCTPI_RDO#MUCTPI_RDO"])
+
+TileMuStream.AddItem(["Muon::TgcCoinDataContainer#TrigT1CoinDataCollectionPriorBC"])
+TileMuStream.AddItem(["Muon::TgcCoinDataContainer#TrigT1CoinDataCollectionNextBC"])
+TileMuStream.AddItem(["Muon::TgcCoinDataContainer#TrigT1CoinDataCollection"])
+
+TileMuStream.AddItem(["TileDigitsContainer#MuRcvDigitsCnt"])
+TileMuStream.AddItem(["TileRawChannelContainer#MuRcvRawChCnt"])
+TileMuStream.AddItem(["TileMuonReceiverContainer#TileMuRcvCnt"])
+TileMuStream.AddItem(["CaloCellContainer#AllCalo"])
+
+TileMuStream.AddItem(["xAOD::MuonContainer#HLT_xAOD__MuonContainer_MuonEFInfo"])
+TileMuStream.AddItem(["xAOD::MuonAuxContainer#HLT_xAOD__MuonContainer_MuonEFInfoAux."])
