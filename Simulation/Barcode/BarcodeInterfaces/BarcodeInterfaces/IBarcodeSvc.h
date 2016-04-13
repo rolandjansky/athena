ae513cf075f875b4ee132783da0133722ef4e90d
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-04-13 13:19:51 +0200

    Add CMakeLists.txt file to newest package tag. ATLASSIM-2739 (BarcodeInterfaces-00-00-09)
    	* Add CMakeLists.txt file to newest package tag. ATLASSIM-2739
    	* tag as BarcodeInterfaces-00-00-09
    
    2016-02-23  John Chapman  <John.Chapman@cern.ch>
    	* BarcodeInterfaces/IBarcodeSvc.h - adding
    	particleGenerationIncrement() getter method. ATLASSIM-2603
    	* tag as BarcodeInterfaces-00-00-08
    
    2015-10-14 Elmar Ritsch <elmar.ritsch@cern.ch>
    	* BarcodeInterfaces/IBarcodeSvc.h
    		** adding secondaryParticleBcOffset() and secondaryVertexBcOffset()
    		getter methods to barcode service interface class
    	* tag as BarcodeInterfaces-00-00-07
    
    
    Former-commit-id: cd27f2bd78ad35f64bc6ca5b46149753ca5ec697

--- Simulation/Barcode/BarcodeInterfaces/BarcodeInterfaces/IBarcodeSvc.h
+++ Simulation/Barcode/BarcodeInterfaces/BarcodeInterfaces/IBarcodeSvc.h
@@ -29,47 +29,54 @@ namespace Barcode {
   /** @class IBarcodeSvc
 
       @author Andreas.Salzburger -at- cern.ch , Elmar.Ritsch -at- cern.ch
-     */
-  class IBarcodeSvc : virtual public IInterface { 
+  */
+  class IBarcodeSvc : virtual public IInterface {
 
-      /////////////////////////////////////////////////////////////////// 
-      // Public methods: 
-      /////////////////////////////////////////////////////////////////// 
-    public: 
+    ///////////////////////////////////////////////////////////////////
+    // Public methods:
+    ///////////////////////////////////////////////////////////////////
+  public:
 
-      /** Retrieve interface ID */
-      static const InterfaceID& interfaceID() { return IID_IBarcodeSvc; }
-      
-      /** Generate a new unique vertex barcode, based on the parent particle barcode and
-          the physics process code causing the truth vertex*/
-      virtual VertexBarcode newVertex( ParticleBarcode parentBC=Barcode::fUndefinedBarcode,
-                                       PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
+    /** Retrieve interface ID */
+    static const InterfaceID& interfaceID() { return IID_IBarcodeSvc; }
 
-      /** Generate a new unique barcode for a secondary particle, based on the parent
-          particle barcode and the process code of the physics process that created
-          the secondary  */
-      virtual ParticleBarcode newSecondary( ParticleBarcode parentBC=Barcode::fUndefinedBarcode,
-                                            PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
+    /** Generate a new unique vertex barcode, based on the parent particle barcode and
+        the physics process code causing the truth vertex*/
+    virtual VertexBarcode newVertex( ParticleBarcode parentBC=Barcode::fUndefinedBarcode,
+                                     PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
 
-      /** Generate a common barcode which will be shared by all children
-          of the given parent barcode (used for child particles which are 
-          not stored in the mc truth event) */
-      virtual ParticleBarcode sharedChildBarcode( ParticleBarcode parentBC,
-                                                  PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
+    /** Generate a new unique barcode for a secondary particle, based on the parent
+        particle barcode and the process code of the physics process that created
+        the secondary  */
+    virtual ParticleBarcode newSecondary( ParticleBarcode parentBC=Barcode::fUndefinedBarcode,
+                                          PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
 
-      /** Update the given barcode (e.g. after an interaction) */
-      virtual ParticleBarcode incrementBarcode( ParticleBarcode oldBC,
+    /** Generate a common barcode which will be shared by all children
+        of the given parent barcode (used for child particles which are
+        not stored in the mc truth event) */
+    virtual ParticleBarcode sharedChildBarcode( ParticleBarcode parentBC,
                                                 PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
-      
-      /** Inform the BarcodeSvc about the largest particle and vertex Barcodes
-          in the event input */
-      virtual void registerLargestGenEvtParticleBC( ParticleBarcode bc) = 0;
-      virtual void registerLargestGenEvtVtxBC( VertexBarcode bc) = 0;
 
-      /** return bit calculator for encoding extra barcode info */
-      virtual BitCalculator* getBitCalculator() const { return 0; }
-      virtual bool hasBitCalculator() const { return false; }
-  }; 
+    /** Update the given barcode (e.g. after an interaction) */
+    virtual ParticleBarcode incrementBarcode( ParticleBarcode oldBC,
+                                              PhysicsProcessCode process=Barcode::fUndefinedProcessCode ) = 0;
+
+    /** Inform the BarcodeSvc about the largest particle and vertex Barcodes
+        in the event input */
+    virtual void registerLargestGenEvtParticleBC( ParticleBarcode bc) = 0;
+    virtual void registerLargestGenEvtVtxBC( VertexBarcode bc) = 0;
+
+    /** Return the secondary particle and vertex offsets */
+    virtual Barcode::ParticleBarcode secondaryParticleBcOffset() const = 0;
+    virtual Barcode::VertexBarcode   secondaryVertexBcOffset()  const = 0;
+
+    /** Return the barcode increment for each generation of updated particles */
+    virtual Barcode::ParticleBarcode particleGenerationIncrement() const = 0;
+
+    /** return bit calculator for encoding extra barcode info */
+    virtual BitCalculator* getBitCalculator() const { return 0; }
+    virtual bool hasBitCalculator() const { return false; }
+  };
 
 }
 
