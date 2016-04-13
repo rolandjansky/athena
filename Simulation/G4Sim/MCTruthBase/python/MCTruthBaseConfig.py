817a2f3b888bd3475b8199b26e9421bbb77fff25
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-04-13 12:22:05 +0200

    src/TruthStrategyUtils.cxx (saveSecondaryVertex): update code to use new naming of ISFParticle accessors in TrackInformation. ATLASSIM-2736. Tagging MCTruthBase-00-02-07 (MCTruthBase-00-02-07)
    2016-04-13  John Chapman  <John.Chapman@cern.ch>
    	* src/TruthStrategyUtils.cxx (saveSecondaryVertex): update code to
    	use new naming of ISFParticle accessors in
    	TrackInformation. ATLASSIM-2736
    	* Tagging MCTruthBase-00-02-07
    2016-04-13  John Chapman  <John.Chapman@cern.ch>
    	* src/TruthStrategyManager.cxx (SaveSecondaryVertex): update code
    	to use new naming of ISFParticle accessors in
    	TrackInformation. ATLASSIM-2736
    	* Tagging MCTruthBase-00-02-06
    2016-03-09 Steve Farrell <Steven.Farrell@cern.ch>
    	* Truth-filling updates for multi-threading:
    		* Simplifed AtlasTrajectory to keep track of number of
    		  secondaries rather than use unsafe SecondaryTracksHelper.
    		* Added new implementation of vertex-saving code in
    		  TruthStrategyUtils. The standalone functions here are
    		  stateless, thread safe, and allow to use G4Step method
    		  GetSecondaryInCurrentStep(). The truth strategies are being
    		  updated accordingly.
    	* Tagging: MCTruthBase-00-02-05
    ...
    (Long ChangeLog diff - truncated)

---
+++ Simulation/G4Sim/MCTruthBase/python/MCTruthBaseConfig.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon import CfgMgr
+
+def getEnvelopeMap():
+    from G4AtlasApps.SimFlags import simFlags
+    from AthenaCommon.DetFlags import DetFlags
+    from AthenaCommon.BeamFlags import jobproperties as beamProps
+
+    # Map of volume name to output collection name
+    envelopeMap = dict()
+    if ( hasattr(simFlags, "LArFarUpstreamMaterial") and
+         simFlags.LArFarUpstreamMaterial.statusOn and
+         simFlags.LArFarUpstreamMaterial.get_Value() ):
+        envelopeMap.update({'LARFARUPSTREAMMATERIAL::LARFARUPSTREAMMATERIAL':
+                            'LArFarUpstreamMaterialExitLayer'})
+    if beamProps.Beam.beamType() == 'cosmics':
+        ## First filter volume
+        if simFlags.CosmicFilterVolumeName == "TRT_Barrel":
+            envelopeMap['TRT::BarrelOuterSupport'] = 'TRTBarrelEntryLayer'
+        elif simFlags.CosmicFilterVolumeName == "TRT_EC":
+            envelopeMap['TRT::WheelA'] = 'TRTECAEntryLayer'
+            envelopeMap['TRT::WheelB'] = 'TRTECBEntryLayer'
+        elif simFlags.CosmicFilterVolumeName == "SCT_Barrel":
+            envelopeMap['SCT::ThShieldOuterCly'] = 'SCTBarrelEntryLayer'# could be ThShieldInnerCly or Cyl..
+        elif simFlags.CosmicFilterVolumeName == "Pixel":
+            envelopeMap['Pixel::Pixel'] = 'PixelEntryLayer'
+        ## If second volume requested
+        if simFlags.CosmicFilterVolumeName2.statusOn:
+            if simFlags.CosmicFilterVolumeName2 == "TRT_Barrel":
+                envelopeMap['TRT::BarrelOuterSupport'] = 'TRTBarrelEntryLayer'
+            elif simFlags.CosmicFilterVolumeName2 == "TRT_EC":
+                envelopeMap['TRT::WheelA'] = 'TRTECAEntryLayer'
+                envelopeMap['TRT::WheelB'] = 'TRTECBEntryLayer'
+            elif simFlags.CosmicFilterVolumeName2 == "SCT_Barrel":
+                envelopeMap['SCT::ThShieldOuterCly'] = 'SCTBarrelEntryLayer'# could be ThShieldInnerCly or Cyl..
+            elif simFlags.CosmicFilterVolumeName2 == "Pixel":
+                envelopeMap['Pixel::Pixel'] = 'PixelEntryLayer'
+    if not simFlags.ISFRun:
+        if DetFlags.ID_on():
+            envelopeMap['IDET::IDET'] = 'CaloEntryLayer'
+        if DetFlags.Calo_on():
+            envelopeMap['CALO::CALO'] = 'MuonEntryLayer'
+        if DetFlags.geometry.Muon_on():
+            envelopeMap['MUONQ02::MUONQ02'] = 'MuonExitLayer'
+    return envelopeMap
+
+def getMCTruthSteppingAction(name="MCTruthSteppingAction", **kwargs):
+    kwargs.setdefault("VolumeCollectionMap", getEnvelopeMap())
+    return CfgMgr.MCTruthSteppingAction(name, **kwargs)
+
+# V2 MCTruthSteppingActionTool
+def getMCTruthSteppingActionTool(name='G4UA::MCTruthSteppingActionTool', **kwargs):
+    """Retrieve the MCTruthSteppingActionTool"""
+    kwargs.setdefault("VolumeCollectionMap", getEnvelopeMap())
+    return CfgMgr.G4UA__MCTruthSteppingActionTool(name, **kwargs)
