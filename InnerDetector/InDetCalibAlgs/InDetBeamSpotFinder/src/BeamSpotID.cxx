8eba6e548a02168d034d0a549ad0a5ec5107058e
Anthony Morley <anthony.morley@cern.ch>
2016-04-14 07:31:19 +0200

    Allow BCID jobs to be grouped by LB and run number (InDetBeamSpotFinder-01-01-07)

--- InnerDetector/InDetCalibAlgs/InDetBeamSpotFinder/src/BeamSpotID.cxx
+++ InnerDetector/InDetCalibAlgs/InDetBeamSpotFinder/src/BeamSpotID.cxx
@@ -5,15 +5,15 @@
 #include "BeamSpotID.h"
 
 bool BeamSpot::ID::operator<( const ID & rhs) const { 
-  if ( m_runNumber < rhs.m_runNumber ) return true;
-  if ( m_runNumber > rhs.m_runNumber ) return false;
-  if ( m_lumiBlock < rhs.m_lumiBlock ) return true;
-  if ( m_lumiBlock > rhs.m_lumiBlock ) return false;
   if ( m_pileup < rhs.m_pileup ) return true;
   if ( m_pileup > rhs.m_pileup ) return false;
   if ( m_bcid < rhs.m_bcid ) return true;
   if ( m_bcid > rhs.m_bcid ) return false;
-  return false;
+  if ( m_lumiBlock < rhs.m_lumiBlock ) return true;
+  if ( m_lumiBlock > rhs.m_lumiBlock ) return false;
+  if ( m_runNumber < rhs.m_runNumber ) return true;
+  if ( m_runNumber > rhs.m_runNumber ) return false;
+ return false;
 }
 
 
