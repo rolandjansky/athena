cc7cad499ab40b31205fc5d8b4fb8c30ee9e7de8
Denis Oliveira Damazio <Denis.Oliveira.Damazio@cern.ch>
2016-04-14 11:42:57 +0200

    update to have full detector ROBList for pre-fetching requests. JIRA 11561 (TrigCaloRec-00-04-54)

--- Trigger/TrigAlgorithms/TrigCaloRec/TrigCaloRec/TrigCaloCellMaker.h
+++ Trigger/TrigAlgorithms/TrigCaloRec/TrigCaloRec/TrigCaloCellMaker.h
@@ -23,6 +23,7 @@
 class IAlgToolEFCalo;
 class ICaloCellMakerTool; 
 class TrigCaloQuality;
+class ITrigDataAccess;
 
 class TrigCaloCellMaker : public HLT::FexAlgo {
 
@@ -46,6 +47,11 @@ class TrigCaloCellMaker : public HLT::FexAlgo {
    TrigCaloCellMaker execution. **/
   HLT::ErrorCode hltExecute(const HLT::TriggerElement* inputTE, 
 			    HLT::TriggerElement* outputTE);
+
+  /** code to pre-fetch ROBs now that we do this in
+ *  in this algorithm */
+  using HLT::FexAlgo::prepareRobRequests;
+  HLT::ErrorCode prepareRobRequests(const HLT::TriggerElement* inputTE );
 
  private:
 
@@ -76,6 +82,9 @@ class TrigCaloCellMaker : public HLT::FexAlgo {
   /** Pointers to timers */
   std::vector<TrigTimer*> m_timer;
 
+  /** Need access to prefetch */
+  ToolHandle<ITrigDataAccess>  m_data;
+
   /** Produced Calorimeter Cell Container. */
   CaloCellContainer* pCaloCellContainer;
 
@@ -97,6 +106,8 @@ class TrigCaloCellMaker : public HLT::FexAlgo {
 
   double m_etaWidthForID;
   double m_phiWidthForID;
+
+  std::vector<uint32_t> m_vec_robs;
 
   
 //public:
