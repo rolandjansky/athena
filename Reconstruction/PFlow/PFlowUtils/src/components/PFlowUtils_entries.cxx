3f36e50738a58aa71054b009202e369aa1a37b03
Attila Krasznahorkay <krasznaa@cern.ch>
2016-04-16 07:39:11 +0200

    Tagging the package with a CMakeLists.txt file included (PFlowUtils-00-00-06)
    Former-commit-id: 95b7fee00608fdb6c4d6eb21c4f4675a8a08aaf5

--- Reconstruction/PFlow/PFlowUtils/src/components/PFlowUtils_entries.cxx
+++ Reconstruction/PFlow/PFlowUtils/src/components/PFlowUtils_entries.cxx
@@ -1,9 +1,11 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "PFlowUtils/RetrievePFOTool.h"
-
+#include "PFlowUtils/ParticleFlowEventFilter_r207.h"
 
 DECLARE_NAMESPACE_TOOL_FACTORY( CP, RetrievePFOTool)
+DECLARE_ALGORITHM_FACTORY(ParticleFlowEventFilter_r207)
 
 DECLARE_FACTORY_ENTRIES(PFlowUtils){
   DECLARE_NAMESPACE_TOOL (CP, RetrievePFOTool)
+  DECLARE_ALGORITHM(ParticleFlowEventFilter_r207)  
 }
