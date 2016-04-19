aa1e3a5c2f05eff307340d24f0b186ee2ed23141
Ryan Mackenzie White <ryan.white@cern.ch>
2016-04-19 18:27:21 +0200

    remove egammaTools (xAODEgammaCnv-00-01-27)
    	* remove egammaTools
    	* xAODEgammaCnv-00-01-27
    
    2016-04-14 Oleg Kuprash <oleg.kuprash@cern.ch>
    	* initialize  m_PIDBuilder and m_EMClusterTool
    	* xAODEgammaCnv-00-01-26
    
    2016-03-23 Ryan White <ryan.white@cern.ch>
    	* Use egammaContainer rather DataVector<egamma>
    	* Compatible with TrigBStoxAODTool
    	* xAODEgammaCnv-00-01-25
    
    2016-03-15 Ryan White <ryan.white@cern.ch>
    	* Add convert for electron trigger use case in TrigBStoxAODTool
    	* Takes (*aod,*xaod) and calls convert(*aod,*xaod,*xaod)
    	* xAODEgammaCnv-00-01-24
    
    2015-12-08  scott snyder  <snyder@bnl.gov>
    
    	* Tagging xAODEgammaCnv-00-01-23
    ...
    (Long ChangeLog diff - truncated)

--- Event/xAOD/xAODEgammaCnv/src/PhotonCnvAlg.cxx
+++ Event/xAOD/xAODEgammaCnv/src/PhotonCnvAlg.cxx
@@ -9,7 +9,6 @@
 #include "egammaEvent/PhotonContainer.h"
 #include "xAODEgamma/PhotonContainer.h"
 #include "xAODEgamma/PhotonAuxContainer.h"
-#include "egammaInterfaces/IegammaBaseTool.h"
 
 // Local include(s):
 #include "PhotonCnvAlg.h"
@@ -42,9 +41,9 @@ namespace xAODMaker {
   StatusCode PhotonCnvAlg::execute() {
 
     // Retrieve the AOD particles:
-    const PhotonContainer* aod = evtStore()->tryConstRetrieve<PhotonContainer>(m_aodContainerName); 
+    const egammaContainer* aod = evtStore()->tryConstRetrieve<egammaContainer>(m_aodContainerName); 
     if (!aod) { 
-      ATH_MSG_WARNING("No PhotonCollection with key " << m_aodContainerName << " found. Do nothing."); 
+      ATH_MSG_WARNING("No egammaContainer with key " << m_aodContainerName << " found. Do nothing."); 
       return StatusCode::SUCCESS; 
     }
     ATH_MSG_DEBUG( "Retrieved particles with key: " << m_aodContainerName );
