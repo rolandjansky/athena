03fbc3c0b39176a7685a80b66d19d19ddb29cd2c
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-04-20 09:44:08 +0200

    python/InDetAlignFolders.py - second pass at fix for ATLASSIM-2746. Tagging as InDetCondFolders-00-00-14 (InDetCondFolders-00-00-14)
    	* python/InDetAlignFolders.py - second pass at fix for
    	ATLASSIM-2746
    	* Tagging as InDetCondFolders-00-00-14
    
    2016-04-19 Matthias Danninger
    	* Fix due to JIRA ATLASSIM-2746
    	* Tagging as InDetCondFolders-00-00-13
    
    2016-04-11 Matthias Danninger
    	* Adding first conditional support for new dynamic alignment folder scheme
    	* Potential update needed for MC DB later on
    	Tagging as InDetCondFolders-00-00-12

--- InnerDetector/InDetConditions/InDetCondFolders/python/InDetAlignFolders.py
+++ InnerDetector/InDetConditions/InDetCondFolders/python/InDetAlignFolders.py
@@ -3,8 +3,8 @@
 from IOVDbSvc.CondDB import conddb
 
 # Inner Detector alignment
-conddb.addFolderSplitOnline("INDET","/Indet/Onl/Align","/Indet/Align")
-conddb.addFolderSplitOnline("TRT","/TRT/Onl/Align","/TRT/Align")
+#conddb.addFolderSplitOnline("INDET","/Indet/Onl/Align","/Indet/Align")
+#conddb.addFolderSplitOnline("TRT","/TRT/Onl/Align","/TRT/Align")
 conddb.addFolderSplitOnline("TRT","/TRT/Onl/Calib/DX","/TRT/Calib/DX")
 conddb.addFolderSplitOnline("TRT","/TRT/Onl/Cond/StatusHT","/TRT/Cond/StatusHT")
 conddb.addFolderSplitOnline("TRT","/TRT/Onl/Cond/Status","/TRT/Cond/Status")
@@ -13,3 +13,25 @@ conddb.addFolderSplitOnline("TRT","/TRT/Onl/Cond/StatusPermanent","/TRT/Cond/Sta
 conddb.addFolderSplitOnline("INDET","/Indet/Onl/PixelDist","/Indet/PixelDist")
 # IBL stave distortions 
 conddb.addFolderSplitOnline("INDET","/Indet/Onl/IBLDist","/Indet/IBLDist")
+
+# Adding protection against new dynamic folder scheme;
+# In future we might want to add also to MC DB
+# Solution below is not pretty but in response to JIRA ATLASSIM-2746
+useDynamicAlignFolders = False
+try:
+    from InDetRecExample.InDetJobProperties import InDetFlags
+    if InDetFlags.useDynamicAlignFolders and conddb.dbdata == "CONDBR2":
+        useDynamicAlignFolders = True
+except ImportError:
+    pass
+if useDynamicAlignFolders:
+    conddb.addFolderSplitOnline("INDET","/Indet/Onl/AlignL1/ID","/Indet/AlignL1/ID")
+    conddb.addFolderSplitOnline("INDET","/Indet/Onl/AlignL2/PIX","/Indet/AlignL2/PIX")
+    conddb.addFolderSplitOnline("INDET","/Indet/Onl/AlignL2/SCT","/Indet/AlignL2/SCT")
+    conddb.addFolderSplitOnline("INDET","/Indet/Onl/AlignL3","/Indet/AlignL3")
+    conddb.addFolderSplitOnline("TRT","/TRT/Onl/AlignL1/TRT","/TRT/AlignL1/TRT")
+    conddb.addFolderSplitOnline("TRT","/TRT/Onl/AlignL2","/TRT/AlignL2")
+else:
+    conddb.addFolderSplitOnline("INDET","/Indet/Onl/Align","/Indet/Align")
+    conddb.addFolderSplitOnline("TRT","/TRT/Onl/Align","/TRT/Align")
+del useDynamicAlignFolders #tidy up
