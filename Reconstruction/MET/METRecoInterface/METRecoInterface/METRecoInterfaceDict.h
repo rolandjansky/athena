48d4f8f22968c7460156a86f01aa0c6f43e48e7f
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2016-04-20 10:35:30 +0200

    'virtual interface dictionaries' (METRecoInterface-00-01-03)
    	* Adding virtual interface dictionaries
    	* Tagging as METRecoInterface-00-01-03

---
+++ Reconstruction/MET/METRecoInterface/METRecoInterface/METRecoInterfaceDict.h
@@ -0,0 +1,16 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef METRECOINTERFACEDICT_H
+#define METRECOINTERFACEDICT_H
+
+#if defined(__GCCXML__) and not defined(EIGEN_DONT_VECTORIZE)
+#define EIGEN_DONT_VECTORIZE
+#endif // __GCCXML__
+
+#include "METRecoInterface/IMETRecoTool.h"
+#include "METRecoInterface/IMETToolBase.h"
+#include "METRecoInterface/IMETAssocToolBase.h"
+
+#endif //METRECOINTERFACEDICT_H
