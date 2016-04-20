6048ce146c6b1d409b81bf56d96aeac6c112cfeb
Iain Bertram <iain.bertram@cern.ch>
2016-04-20 13:03:07 +0200

    Add  diffPoolFiles.py to standard tests to ensure all differences are tagged  [ATLASSIM-2717] Tagging DigitizationTests-01-01-65 (DigitizationTests-01-01-65)
    	* Add  diffPoolFiles.py to standard tests to ensure all differences are tagged
    	* Tagging DigitizationTests-01-01-65
    
    2016-01-24 Iain Bertram <iain.bertram@cern.ch>
    	* scripts/DigitizationCheckReferenceLocation.sh - updates for 20.20 compare with 20.7
    	* Tagging DigitizationTests-01-01-64
    
    2016-03-22 Iain Bertram <iain.bertram@cern.ch>
    	* clang code cleanup ATLASSIM-2661
    	* remove unused paramater m_numberOfEventsSelected from
    	McEventCollectionTestTool
    	* Tagging DigitizationTests-01-01-63
    
    2016-03-11  John Chapman  <John.Chapman@cern.ch>
    	* src/SCT_RDOsTestTool.cxx (processEvent): drop call to
    	SCT_RDORawData::getStrip() method as this is now
    	deprecated. ATLASSIM-2634
    	* Tagging DigitizationTests-01-01-62
    
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
+++ Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
@@ -3,6 +3,12 @@
 export DigitizationTestsVersion="dev"
 if [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then 
     DigitizationTestsVersion="20.7.X"
+elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then 
+    DigitizationTestsVersion="20.7.X"
+elif [[ $ATLAS_RELEASE_BASE == *"mig7"* ]]; then
+    DigitizationTestsVersion="20.3"
+elif [[ $ATLAS_RELEASE_BASE == *"20.3"* ]]; then
+    DigitizationTestsVersion="20.3"
 elif [[ $ATLAS_RELEASE_BASE == *"20.1"* ]]; then
     DigitizationTestsVersion="20.1.X"
 fi
