f2f7eb99dda1cda094f7822c8cbe2f0281ee70c5
Antonio Limosani <Antonio.Limosani@cern.ch>
2016-04-22 10:18:16 +0200

    Remove check for proxy grid certificate (PROCTools-00-01-14)
    	* Tagging PROCTools-00-01-14
    	* Remove check for proxy grid certificate
    
    2016-04-22 Antonio Limosani <antonio.limosani@cern.ch>
    	* Tagging PROCTools-00-01-13
    	* Make clean directory name unique
    
    2016-04-20 Christian Ohm <christian.ohm@cern.ch>
    	* ExtractEvents.py: updated fixed-version path to eos to sym link as recommended in
    	https://cern.service-now.com/service-portal/view-incident.do?n=INC1004372
    
    2016-04-18  scott snyder  <snyder@bnl.gov>
    	* Tagging PROCTools-00-01-12.
    	* Fix typo in requirements file.
    
    2016-04-13 Antonio Limosani <antonio.limosani@cern.ch>
    	* remove builds argument in setup
    	* PROCTools-00-01-11
    
    2016-04-11 Antonio Limosani <antonio.limosani@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

--- Tools/PROCTools/python/ExtractEvents.py
+++ Tools/PROCTools/python/ExtractEvents.py
@@ -16,7 +16,9 @@ import sys, os, argparse, subprocess, fnmatch
 
 # This is a bit ugly, but seems to be the only way for now
 # See more info here: https://cern.service-now.com/service-portal/view-request.do?n=RQF0492611
-eospath = "/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
+#eospath = "/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
+# Update: https://cern.service-now.com/service-portal/view-incident.do?n=INC1004372
+eospath = "/afs/cern.ch/project/eos/installation/atlas/bin/eos.select"
 
 def validEventList(eventList):
     eventStrings = eventList.split(',')
@@ -86,7 +88,7 @@ def main():
     parser.add_argument('-e', '--events', type=str, required=True, help='Event number(s), separated by commas if several (e.g. 12345 or 123,456,789)')
     parser.add_argument('-p', '--projecttag', type=str, nargs='?', help='Project tag, defaults to "data15_13TeV"', default='data15_13TeV')
     parser.add_argument('-s', '--stream', type=str, nargs='?', help='Stream name, defaults to "physics_Main"', default='physics_Main')
-    parser.add_argument('-f', '--fileformat', type=validFileFormat, help='File format: (D)RAW(_XYZ), (D)ESD(_XYZ), (D)AOD(_XYZ)', default='DESDM_RPVLL')
+    parser.add_argument('-f', '--fileformat', type=validFileFormat, help='File format: (D)RAW(_XYZ), (D)ESD(_XYZ), (D)AOD(_XYZ)', default='AOD')
     parser.add_argument('-m', '--matchingstring', type=str, nargs='?', default='', help='String for matching the dataset to look in, useful when there are several processings available, or both merged and unmerged datasets, e.g. "*merge.AOD*f620*" will do what you think')
     parser.add_argument('-o', '--outputfile', type=str, nargs='?', help='Name for the output file (appropriate suffix based on input format will be appended automatically)')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Verbose mode, prints out eos paths and commands, file names, etc')
@@ -131,16 +133,12 @@ def main():
             datasetNames.append(line.rstrip())
 
     # now get rid of all datasets that don't match the matching pattern
-    for ds in datasetNames:
-        # get rid of log file archives too - that's not what we want!
-        if ".LOGARC" in ds:
-            datasetNames.remove(ds)
-        elif args.matchingstring != '':
-            if not fnmatch.fnmatch(ds, args.matchingstring):
-                datasetNames.remove(ds)
-                if args.verbose:
-                    print "Removing %s from list of datasets since it does not match %s" % (ds, args.matchingstring)
-
+    datasetNames = [ds for ds in datasetNames if not ".LOGARC" in ds]
+    if args.matchingstring != '':
+        if args.verbose:
+            print "Removing datasets that don't match %s" % (args.matchingstring)
+        datasetNames = [ds for ds in datasetNames if fnmatch.fnmatch(ds, "*"+args.matchingstring+"*")]
+    
     if len(datasetNames) > 1:
         print "More than one dataset matching the provided info"
         for ds in datasetNames:
@@ -162,6 +160,7 @@ def main():
     fileListing = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     fileNames = []
     for line in fileListing.stdout.readlines():
+        print line
         substrings = line.split('lb')
         lbs = []
         if line.count('lb') == 2:
@@ -180,7 +179,7 @@ def main():
 	print "No files available in %s - will exit" % (filePath+datasetNames[0]+'/')
 	sys.exit(1)
 
-    # Actually extract the events!
+    # actually extract the events
     extractEvents(args.run, eventList, fileNames, args.fileformat)
 
 if __name__ == '__main__':
