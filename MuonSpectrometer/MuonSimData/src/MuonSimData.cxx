442e8419ba5cd6330e5eb1a9db93da85aabb948f
Jochen Meyer <Jochen.Meyer@cern.ch>
2016-04-24 17:57:41 +0200

    adding time to SDO (MuonSimData-00-03-12)
    	* adding time to SDO (ATLASRECTS-3022)
    	* tagging MuonSimData-00-03-12
    
    2016-01-05  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MuonSimData-00-03-11.
    	* MuonSimData/MuonSimData.h: Argument of MuonSimData::setPosition
    	should be const.
    
    2015-12-12  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MuonSimData-00-03-10.
    	* Comply with ATLAS naming conventions.
    
    2015-12-09  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MuonSimData-00-03-09.
    	* cmt/requirements: Don't need reflex_dict_options_cppflags
    	setting any more.
    	* Tagging MuonSimData-00-03-08.
    ...
    (Long ChangeLog diff - truncated)

--- MuonSpectrometer/MuonSimData/src/MuonSimData.cxx
+++ MuonSpectrometer/MuonSimData/src/MuonSimData.cxx
@@ -10,39 +10,34 @@
 
 // $Id: MuonSimData.cxx,v 1.1 2004-05-29 15:17:53 ketevi Exp $
 
-//<<<<<< INCLUDES                                                       >>>>>>
-
 #include "MuonSimData/MuonSimData.h"
 #include <iostream>
-
-//<<<<<< PRIVATE DEFINES                                                >>>>>>
-//<<<<<< PRIVATE CONSTANTS                                              >>>>>>
-//<<<<<< PRIVATE TYPES                                                  >>>>>>
-//<<<<<< PRIVATE VARIABLE DEFINITIONS                                   >>>>>>
-//<<<<<< PUBLIC VARIABLE DEFINITIONS                                    >>>>>>
-//<<<<<< CLASS STRUCTURE INITIALIZATION                                 >>>>>>
-//<<<<<< PRIVATE FUNCTION DEFINITIONS                                   >>>>>>
-//<<<<<< PUBLIC FUNCTION DEFINITIONS                                    >>>>>>
-//<<<<<< MEMBER FUNCTION DEFINITIONS                                    >>>>>>
 
 
 MuonSimData::MuonSimData() :
   m_word(0),
-  x(0.0),y(0.0),z(0.0)
+  m_x(0.0),m_y(0.0),m_z(0.0),m_t(0.0)
 {
   
 }
 
 MuonSimData::MuonSimData (const std::vector<Deposit>& deposits, 
 			    int simDataWord)
-    : m_word((deposits.size()<<29 & 0xe0000000) | (simDataWord & 0x1fffffff)), x(0.0),y(0.0),z(0.0)
+    : m_word((deposits.size()<<29 & 0xe0000000) | (simDataWord & 0x1fffffff)), m_x(0.0),m_y(0.0),m_z(0.0),m_t(0.0)
 {
     m_deposits = deposits;
 }
 
+MuonSimData::MuonSimData (std::vector<Deposit>&& deposits, 
+                          int simDataWord)
+    : m_word((deposits.size()<<29 & 0xe0000000) | (simDataWord & 0x1fffffff)), m_x(0.0),m_y(0.0),m_z(0.0),m_t(0.0)
+{
+  m_deposits = std::move(deposits);
+}
+
 MuonSimData::MuonSimData (const MuonSimData& other)
     :m_word(other.m_word),
-     m_deposits(other.m_deposits), x(other.x),y(other.y),z(other.z)
+     m_deposits(other.m_deposits), m_x(other.m_x),m_y(other.m_y),m_z(other.m_z),m_t(other.m_t)
 {}
 
 
