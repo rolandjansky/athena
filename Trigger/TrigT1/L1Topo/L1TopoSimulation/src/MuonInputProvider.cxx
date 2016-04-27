7bc099921fe5128bd3a745524e7d13f2acc2cade
Olga Igonkina <Olga.Igonkina@cern.ch>
2016-04-27 19:42:02 +0200

    Using ieta/iphi values from MuCTPIL1TopoCandidate (L1TopoSimulation-00-00-41)
    	* Using ieta/iphi values from MuCTPIL1TopoCandidate
            * to match L1Topo hardware values
    	* needs TrigT1Muctpi-00-06-37 and TrigT1Interfaces-01-05-34
    	* tag as L1TopoSimulation-00-00-41
    
    2016-04-17 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Updated the CMake configuration to make use of the
    	  atlas_disable_as_needed() function. To make the code a bit
    	  more robust.
    	* Tagging as L1TopoSimulation-00-00-40
    
    2016-04-08 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Discovered a mistake that I made in CMakeLists.txt in the last
    	  update. Fixed now.
    	* This tag should be used instead of 00-00-38 when collecting the
    	  package into devval.
    	* Tagging as L1TopoSimulation-00-00-39
    
    2016-04-01  Thorsten Wengler  <thorsten.wengler@cern.ch>
    
    ...
    (Long ChangeLog diff - truncated)

--- Trigger/TrigT1/L1Topo/L1TopoSimulation/src/MuonInputProvider.cxx
+++ Trigger/TrigT1/L1Topo/L1TopoSimulation/src/MuonInputProvider.cxx
@@ -18,6 +18,7 @@
 #include "TrigT1Interfaces/RecMuonRoiSvc.h"
 #include "TrigT1Interfaces/MuCTPIL1Topo.h"
 #include "TrigT1Interfaces/MuCTPIL1TopoCandidate.h"
+#include "TrigT1Interfaces/IMuctpiSimTool.h"
 
 #include "TrigT1Result/MuCTPIRoI.h"
 #include "TrigT1Result/RoIBResult.h"
@@ -39,6 +40,7 @@ MuonInputProvider::MuonInputProvider( const std::string& type, const std::string
    m_configSvc( "TrigConf::TrigConfigSvc/TrigConfigSvc", name ),
    m_recRPCRoiSvc( LVL1::ID_RecRpcRoiSvc, name ),
    m_recTGCRoiSvc( LVL1::ID_RecTgcRoiSvc, name ),
+   m_MuctpiSimTool("LVL1MUCTPI::L1MuctpiTool/LVL1MUCTPI__L1MuctpiTool"),
    m_muonROILocation( LVL1MUCTPI::DEFAULT_MuonRoIBLocation ),
    m_MuonEncoding(0),
    m_MuCTPItoL1TopoLocation ("/Run/L1MuCTPItoL1TopoLocation")
@@ -52,6 +54,7 @@ MuonInputProvider::MuonInputProvider( const std::string& type, const std::string
 
    declareProperty( "MuonEncoding", m_MuonEncoding = 0, "0=full granularity Mu ROIs, 1=MuCTPiToTopo granularity");
    declareProperty( "locationMuCTPItoL1Topo", m_MuCTPItoL1TopoLocation = "/Run/L1MuCTPItoL1TopoLocation", "Storegate key for MuCTPItoL1Topo ");
+   declareProperty( "MuctpiSimTool", m_MuctpiSimTool,"Tool for MUCTPIsimulation");
 }
 
 MuonInputProvider::~MuonInputProvider()
@@ -60,7 +63,6 @@ MuonInputProvider::~MuonInputProvider()
 
 StatusCode
 MuonInputProvider::initialize() {
-
    ATH_MSG_DEBUG("Retrieving LVL1ConfigSvc " << m_configSvc);
    CHECK( m_configSvc.retrieve() );
 
@@ -79,6 +81,10 @@ MuonInputProvider::initialize() {
    incidentSvc->addListener(this,"BeginRun", 100);
    incidentSvc.release().ignore();
   
+   // get MuctpiTool handle
+   ATH_MSG_DEBUG("Retrieving MuctpiToolHandle " << m_MuctpiSimTool);
+   CHECK( m_MuctpiSimTool.retrieve() );
+
    return StatusCode::SUCCESS;
 }
 
@@ -123,23 +129,20 @@ TCS::MuonTOB
 MuonInputProvider::createMuonTOB(const MuCTPIL1TopoCandidate & roi) const {
 
 
-   ATH_MSG_DEBUG("Muon ROI (MuCTPiToTopo): thr ID = " << roi.getptThresholdID() << " eta = " << roi.geteta() << " phi = " << roi.getphi() << ", w   = " << MSG::hex << std::setw( 8 ) << roi.getRoiID() << MSG::dec);
+  ATH_MSG_DEBUG("Muon ROI (MuCTPiToTopo): thr ID = " << roi.getptThresholdID() << " eta = " << roi.geteta() << " phi = " << roi.getphi()  
+                << ", w   = " << MSG::hex << std::setw( 8 ) << roi.getRoiID() << MSG::dec );
+  ATH_MSG_DEBUG("                            Oct = " << roi.getMioctID() << " etacode=" <<  roi.getetacode() << " phicode= " <<  
+                 roi.getphicode()<< ", Sector="<< roi.getSectorName() );
 
+   // The L1 topo hardware works with phi in [0,2pi]. The MuCTPi give muons in [0,2pi].
+   // However, L1 topo simulation works with [-pi, pi] and otherwise it crashes. Thus we have to convert here
+    int etaTopo = roi.getieta();
+    int phiTopo = roi.getiphi();
+    if( phiTopo > 31 ) phiTopo -= 64;
 
-   // Here it is unclear. The L1 topo hardware works with phi in [0,2pi]. The MuCTPi give muons in [0,2pi].
-   // However, L1 topo simulation works with [-pi, pi] and otherwise it crashes. Thus we have to put check here
-   float phi = roi.getphi();
-   if(phi<-M_PI) phi+=2.0*M_PI;
-   if(phi> M_PI) phi-=2.0*M_PI;
-
-   TCS::MuonTOB muon( roi.getptValue(), 0, int(10*roi.geteta()), int(10*phi), roi.getRoiID() );
-
-   //OI this does not work cout << " Trying getphi "<<roi.getphi()<<" \n";
-   // phi has to be in [-pi,pi] range, although hardware works with [0,2pi]
-   //TCS::MuonTOB muon( roi.getptValue(), 0, int(10*roi.geteta()), int(10*roi.getphi()), 0 );
-
-   muon.setEtaDouble( roi.geteta() );
-   muon.setPhiDouble( phi );
+    TCS::MuonTOB muon( roi.getptValue(), 0, etaTopo, phiTopo, roi.getRoiID() );
+    muon.setEtaDouble( etaTopo );
+    muon.setPhiDouble( phiTopo );
 
    m_hPt->Fill(muon.Et());
    m_hEtaPhi->Fill(muon.eta(),muon.phi());
@@ -173,22 +176,22 @@ MuonInputProvider::fillTopoInputEvent(TCS::TopoInputEvent& inputEvent) const {
 
   if( m_MuonEncoding == 0 ) {
 
-   ATH_MSG_DEBUG("Filling the muon input from MuCTPIToRoIBSLink produced by L1Muctpi.cxx.");
+    ATH_MSG_DEBUG("Filling the muon input from MuCTPIToRoIBSLink produced by L1Muctpi.cxx.");
 
-   const ROIB::RoIBResult* roibResult {nullptr};
+    const ROIB::RoIBResult* roibResult {nullptr};
 
-   const L1MUINT::MuCTPIToRoIBSLink* muctpi_slink {nullptr};
+    const L1MUINT::MuCTPIToRoIBSLink* muctpi_slink {nullptr};
 
-   if( evtStore()->contains<L1MUINT::MuCTPIToRoIBSLink>("/Run/L1MuCTPItoRoIBLocation") ) {
+    if( evtStore()->contains<L1MUINT::MuCTPIToRoIBSLink>("/Run/L1MuCTPItoRoIBLocation") ) {
       CHECK( evtStore()->retrieve( muctpi_slink, "/Run/L1MuCTPItoRoIBLocation" ) );
-   } else if( evtStore()->contains<ROIB::RoIBResult>(m_roibLocation) ) {
+    } else if( evtStore()->contains<ROIB::RoIBResult>(m_roibLocation) ) {
       CHECK( evtStore()->retrieve(roibResult, m_roibLocation) );
-   } else {
+    } else {
       ATH_MSG_WARNING("Neither a MuCTPIToRoIBSLink with SG key '/Run/L1MuCTPItoRoIBLocation' nor a an RoIBResult were found in the event. No muon input for the L1Topo simulation.");
       return StatusCode::RECOVERABLE;
-   }
+    }
 
-   if( roibResult ) {
+    if( roibResult ) {
 
       const std::vector< ROIB::MuCTPIRoI >& rois = roibResult->muCTPIResult().roIVec();
 
@@ -196,62 +199,85 @@ MuonInputProvider::fillTopoInputEvent(TCS::TopoInputEvent& inputEvent) const {
 
       for( const ROIB::MuCTPIRoI & muonRoI : rois ) {
 
-         if( !( muonRoI.roIWord() & LVL1::CandidateVetoMask  )  )
-           inputEvent.addMuon( MuonInputProvider::createMuonTOB( muonRoI.roIWord() ) );
-         else
-           ATH_MSG_DEBUG(" Ignore Vetoed L1 Mu RoI " <<  muonRoI.roIWord() );
+	if( !( muonRoI.roIWord() & LVL1::CandidateVetoMask  )  )
+	  inputEvent.addMuon( MuonInputProvider::createMuonTOB( muonRoI.roIWord() ) );
+	else
+	  ATH_MSG_DEBUG(" Ignore Vetoed L1 Mu RoI " <<  muonRoI.roIWord() );
       }
 
-   } else if( muctpi_slink ) {
+    } else if( muctpi_slink ) {
 
       ATH_MSG_DEBUG("Filling the input event. Number of Muon ROIs: " << muctpi_slink->getMuCTPIToRoIBWords().size() - ROIB::Header::wordsPerHeader - ROIB::Trailer::wordsPerTrailer - 1);
-
+     
       unsigned int icnt = 0;
       for ( unsigned int roiword : muctpi_slink->getMuCTPIToRoIBWords() ) {
-
-         ++icnt;
-         // skip header
-         if ( icnt <= ROIB::Header::wordsPerHeader + 1 )
-            continue;
-
-         // skip trailer
-         if ( icnt > ( muctpi_slink->getMuCTPIToRoIBWords().size() - ROIB::Trailer::wordsPerTrailer ) )
-            continue;
-
-         if( !( roiword & LVL1::CandidateVetoMask  )  )
-           inputEvent.addMuon( MuonInputProvider::createMuonTOB( roiword ) );
-         else
-            ATH_MSG_DEBUG(" Ignore Vetoed L1 Mu RoI " << roiword );
-
-      }
-
-   }
-   } else {  // This option for trying   MuCTPiToTopo
-     ATH_MSG_DEBUG("Use MuCTPiToTopo granularity Muon ROIs.");
-
-     LVL1::MuCTPIL1Topo* l1topo  {nullptr};
-     CHECK( evtStore()->retrieve( l1topo, m_MuCTPItoL1TopoLocation ) );
-     std::vector<MuCTPIL1TopoCandidate> candList = l1topo->getCandidates();
-     for(  std::vector<MuCTPIL1TopoCandidate>::const_iterator iMuCand = candList.begin(); iMuCand != candList.end(); iMuCand++)
-       {
-         //MuonInputProvider::createMuonTOB( *iMuCand );
-         inputEvent.addMuon( MuonInputProvider::createMuonTOB( *iMuCand ) );
-       }
-
-     //BC+1
-     if( evtStore()->contains<LVL1::MuCTPIL1Topo>(m_MuCTPItoL1TopoLocationBC1) ) {
-       LVL1::MuCTPIL1Topo* l1topoBC1  {nullptr};
-       CHECK( evtStore()->retrieve( l1topoBC1,m_MuCTPItoL1TopoLocationBC1));
-       ATH_MSG_DEBUG( "Contains L1Topo LateMuons L1Muctpi object from StoreGate!" );
-       l1topoBC1->print();
        
-       std::vector<MuCTPIL1TopoCandidate> candList = l1topoBC1->getCandidates();
-       for(  std::vector<MuCTPIL1TopoCandidate>::const_iterator iMuCand = candList.begin(); iMuCand != candList.end(); iMuCand++)
-	 {
-	   //or would it be better to create muon and dynamic_cast?
-	   ATH_MSG_DEBUG("MuonInputProvider addLateMuon ");
-	   inputEvent.addLateMuon( MuonInputProvider::createLateMuonTOB( *iMuCand ) );	   
-	 }
+	++icnt;
+	// skip header
+	if ( icnt <= ROIB::Header::wordsPerHeader + 1 )
+	  continue;
+       
+	// skip trailer
+	if ( icnt > ( muctpi_slink->getMuCTPIToRoIBWords().size() - ROIB::Trailer::wordsPerTrailer ) )
+	  continue;
+       
+	if( !( roiword & LVL1::CandidateVetoMask  )  )
+	  inputEvent.addMuon( MuonInputProvider::createMuonTOB( roiword ) );
+	else
+	  ATH_MSG_DEBUG(" Ignore Vetoed L1 Mu RoI " << roiword );
+       
+      }
+     
+    }
+  } else {  // reduced granularity encoding
+    ATH_MSG_DEBUG("Use MuCTPiToTopo granularity Muon ROIs.");
+
+    // first see if L1Muctpi simulation already ran and object is in storegate, if not
+    // call tool version of the L1MuctpiSimulation and create it on the fly
+    if( evtStore()->contains<LVL1::MuCTPIL1Topo>(m_MuCTPItoL1TopoLocation) ) {
+      LVL1::MuCTPIL1Topo* l1topo  {nullptr};
+      CHECK( evtStore()->retrieve( l1topo, m_MuCTPItoL1TopoLocation ) );      
+      ATH_MSG_DEBUG("Use MuCTPiToTopo granularity Muon ROIs: retrieve from SG");
+
+      // std::cout << "TW: MuonInputProvider print l1topo candidates from SG" << std::endl; 
+      // l1topo->print();
+
+      std::vector<MuCTPIL1TopoCandidate> candList = l1topo->getCandidates();
+      for(  std::vector<MuCTPIL1TopoCandidate>::const_iterator iMuCand = candList.begin(); iMuCand != candList.end(); iMuCand++)
+	{
+	  //MuonInputProvider::createMuonTOB( *iMuCand );
+	  inputEvent.addMuon( MuonInputProvider::createMuonTOB( *iMuCand ) );
+	}
+    } else {
+
+      ATH_MSG_DEBUG("Use MuCTPiToTopo granularity Muon ROIs: calculate from ROIs sent to RoIB");
+      LVL1::MuCTPIL1Topo l1topo;
+      m_MuctpiSimTool->fillMuCTPIL1Topo(l1topo);
+
+      // std::cout << "TW: MuonInputProvider print l1topo candidates from Tool" << std::endl; 
+      // l1topo.print();
+      
+            
+      for( const MuCTPIL1TopoCandidate & cand : l1topo.getCandidates() ) {
+	inputEvent.addMuon( MuonInputProvider::createMuonTOB( cand ) );
+      }
+    }
+    
+    //BC+1 ... this can only come from simulation, in data taking this is collected by the L1Topo at its input
+    // so no need to do anything else here
+    if( evtStore()->contains<LVL1::MuCTPIL1Topo>(m_MuCTPItoL1TopoLocation.toString()+std::to_string(1)) ) {
+      LVL1::MuCTPIL1Topo* l1topoBC1  {nullptr};
+      CHECK( evtStore()->retrieve( l1topoBC1,m_MuCTPItoL1TopoLocation.toString()+std::to_string(1)));
+      ATH_MSG_DEBUG( "Contains L1Topo LateMuons L1Muctpi object from StoreGate!" );
+      //      l1topoBC1->print();
+      
+      std::vector<MuCTPIL1TopoCandidate> candList = l1topoBC1->getCandidates();
+      for(  std::vector<MuCTPIL1TopoCandidate>::const_iterator iMuCand = candList.begin(); iMuCand != candList.end(); iMuCand++)
+	{
+	  //or would it be better to create muon and dynamic_cast?
+	  ATH_MSG_DEBUG("MuonInputProvider addLateMuon ");
+	  inputEvent.addLateMuon( MuonInputProvider::createLateMuonTOB( *iMuCand ) );	   
+	}
      }  
   }
 
