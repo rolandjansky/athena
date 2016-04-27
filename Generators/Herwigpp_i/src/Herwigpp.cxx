2d11d4918c062221e8a0af2252ee355a525ca61d
Attila Krasznahorkay <krasznaa@cern.ch>
2016-04-27 07:51:33 +0200

    Fixing an error with the CMake export generated for the package's library (Herwigpp_i-00-06-26)
    	* As it turns out, public definitions on a library involving
    	  double quotes don't work all that well. The CMake configuration
    	  generated for the library ends up being corrupt.
    	* Since the definition is strictly only needed privately, now
    	  declaring it as such.
    	* Tagging as Herwigpp_i-00-06-26
    
    2016-04-26 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Updated the CMake configuration of the package to use the
    	  newly created Herwig3 module correctly during the build.
    	  (LCG now calls the latest Herwig++ version Herwig3.)
    	* Tagging as Herwigpp_i-00-06-25
    
    2016-03-07 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Teaching the Herwigpp algorithm how to correctly set up the
    	  Herwig++ generator in the CMake build. Without relying on the
    	  CMTPATH and CMTCONFIG environment variables.
    	* Tagging as Herwigpp_i-00-06-24
    
    2016-02-07  Orel Gueta  <Orel.Gueta@cern.ch>
    ...
    (Long ChangeLog diff - truncated)

--- Generators/Herwigpp_i/src/Herwigpp.cxx
+++ Generators/Herwigpp_i/src/Herwigpp.cxx
@@ -101,7 +101,21 @@ StatusCode Herwigpp::genInitialize() {
   char* env2 = getenv("CMTCONFIG");
   string reposearchpaths;
   if (env1 == 0 || env2 == 0) {
-    ATH_MSG_WARNING("$CMTPATH or $CMTCONFIG variable not set: finding the ThePEG/H++ module directories will be difficult...");
+     // Use everything from $DATAPATH and $LD_LIBRARY_PATH:
+     const char* datapath = getenv( "DATAPATH" );
+     reposearchpaths = datapath;
+     std::vector< std::string > datapaths;
+     boost::split( datapaths, datapath,
+                   boost::is_any_of( std::string( ":" ) ) );
+     for( const std::string& p : datapaths ) {
+        ThePEG::Repository::appendReadDir( p );
+     }
+     const char* ldpath = getenv( "LD_LIBRARY_PATH" );
+     std::vector< std::string > ldpaths;
+     boost::split( ldpaths, ldpath, boost::is_any_of( std::string( ":" ) ) );
+     for( const std::string& p : ldpaths ) {
+        ThePEG::DynamicLoader::appendPath( p );
+     }
   } else {
     vector<string> cmtpaths;
     boost::split(cmtpaths, env1, boost::is_any_of(string(":")));
@@ -217,7 +231,7 @@ StatusCode Herwigpp::genInitialize() {
   // MPI parameters
   const string repo_intrkt = ThePEG::Repository::exec("get /Herwig/Shower/Evolver:IntrinsicPtGaussian", std::cout);
   ATH_MSG_INFO("Intrinsic kT: " + repo_intrkt + " GeV/c");
-  const string repo_ptmatch = ThePEG::Repository::exec("get /Herwig/UnderlyingEvent/KtCut:MinKT", std::cout);
+  const string repo_ptmatch = ThePEG::Repository::exec("get /Herwig/UnderlyingEvent/MPIHandler:pTmin0", std::cout);
   ATH_MSG_INFO("MPI matching pT0: " + repo_ptmatch + " GeV/c");
   const string repo_invrad = ThePEG::Repository::exec("get /Herwig/UnderlyingEvent/MPIHandler:InvRadius", std::cout);
   ATH_MSG_INFO("Inverse hadron radius: " + repo_invrad + " GeV**2");
