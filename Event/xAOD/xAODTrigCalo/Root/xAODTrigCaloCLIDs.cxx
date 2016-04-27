09d8c3d066efdf13b2af3f61a79e199af2f2784c
Will Buttinger <William.Buttinger@cern.ch>
2016-04-27 08:32:38 +0200

    'CLID generating at compile time' (xAODTrigCalo-00-00-15)
             * Root/xAODTrigCaloCLIDs.cxx : added for CLIDs generating at compile time
            * tagging as xAODTrigCalo-00-00-15
    
    
    Former-commit-id: 2def2273db817bb18fcf60988492f6babf61b9c9

---
+++ Event/xAOD/xAODTrigCalo/Root/xAODTrigCaloCLIDs.cxx
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+//simple includes to force the CLASS_DEF etc to be encountered during compile
+
+#include "xAODTrigCalo/CaloClusterTrigAuxContainer.h"
+#include "xAODTrigCalo/TrigCaloClusterAuxContainer.h"
+#include "xAODTrigCalo/TrigCaloClusterContainer.h"
+#include "xAODTrigCalo/TrigEMClusterContainer.h"
+#include "xAODTrigCalo/TrigEMClusterAuxContainer.h"
