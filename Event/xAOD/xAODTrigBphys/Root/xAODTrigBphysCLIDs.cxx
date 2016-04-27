e78845aff8510e506a315f8ca99f807ff84698ce
Will Buttinger <William.Buttinger@cern.ch>
2016-04-27 08:37:46 +0200

    'CLIDs at compile time' (xAODTrigBphys-00-00-17)
             * Root/xAODTrigBphysCLIDs.cxx : added to get CLIDs at compile time
             * Tagging xAODTrigBphys-00-00-17
    
    
    Former-commit-id: 9ce72e9d58733f8e7beda4cd1acc6fd069648aa8

---
+++ Event/xAOD/xAODTrigBphys/Root/xAODTrigBphysCLIDs.cxx
@@ -0,0 +1,10 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+//simple includes to force the CLASS_DEF etc to be encountered during compile
+
+#include "xAODTrigBphys/TrigBphysContainer.h"
+#include "xAODTrigBphys/TrigBphysAuxContainer.h"
+
