3a5e1f1eccd2ae1e39a77532f1f9ca681d22b463
Will Buttinger <William.Buttinger@cern.ch>
2016-04-27 08:42:35 +0200

    'CLIDs at compile time' (xAODTrigMissingET-00-00-16)
          * Root/xAODTrigMissingETCLIDs.cxx : added to get CLIDs at compile time
          * Tagged as xAODTrigMissingET-00-00-16
    
    	* Tagged as xAODTrigMissingET-00-00-15
    
    
    Former-commit-id: 85289ebb8998e3d271b09ecf41eb166112ba4cc6

---
+++ Event/xAOD/xAODTrigMissingET/Root/xAODTrigMissingETCLIDs.cxx
@@ -0,0 +1,9 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+//simple includes to force the CLASS_DEF etc to be encountered during compile
+
+#include "xAODTrigMissingET/TrigMissingETContainer.h"
+#include "xAODTrigMissingET/TrigMissingETAuxContainer.h"
