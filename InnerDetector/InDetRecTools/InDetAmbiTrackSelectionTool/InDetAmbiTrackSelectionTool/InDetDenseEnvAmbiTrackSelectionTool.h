8496f7b1440e8138fe7ef08f4a60f79941bde607
Anthony Morley <anthony.morley@cern.ch>
2016-04-27 10:07:50 +0200

    Add logic for sharing SCT hits (InDetAmbiTrackSelectionTool-00-01-40)
    	* Add SCT splitting for nearby tracks
      * Tagged as InDetAmbiTrackSelectionTool-00-01-40
    
    2016-04-18 Christoph Rauchegger
      * Adding holes, split/shared hits to TrkObserverTool
      * Tagged as InDetAmbiTrackSelectionTool-00-01-39
    
    2016-04-08 Roland Jansky
        * Adding in configuration for TrkObserverTool, off by default
    	* Tagging InDetAmbiTrackSelectionTool-00-01-38
    
    
    Former-commit-id: a737c64962eb50d3dc54886c7e7ddef60924eb15

--- InnerDetector/InDetRecTools/InDetAmbiTrackSelectionTool/InDetAmbiTrackSelectionTool/InDetDenseEnvAmbiTrackSelectionTool.h
+++ InnerDetector/InDetRecTools/InDetAmbiTrackSelectionTool/InDetAmbiTrackSelectionTool/InDetDenseEnvAmbiTrackSelectionTool.h
@@ -19,7 +19,8 @@
 #include <map>
 #include <vector>
 #include "TrkRIO_OnTrack/RIO_OnTrack.h"
-
+#include "TrkValInterfaces/ITrkObserverTool.h"
+#include "TrkParameters/TrackParameters.h"
 
 class SiliconID;
 class Identifier;
@@ -40,7 +41,6 @@ namespace InDet
       
   */  
   class ITrtDriftCircleCutTool;
-
   class InDetDenseEnvAmbiTrackSelectionTool : virtual public Trk::IAmbiTrackSelectionTool,
                                               virtual public IIncidentListener, 
                                               public AthAlgTool
@@ -86,7 +86,8 @@ namespace InDet
      
       struct TrackHitDetails
       {  
-        bool  isPatternTrack;
+        bool  isPatternTrack        ;
+        bool  inROI                 ;
         bool  thishasblayer         ; //Track has a hit in the b-layer
         bool  hassharedblayer       ; //Track has a shared it in the b-layer
         bool  hassharedpixel        ; //Track has a shared pixel hit
@@ -118,6 +119,7 @@ namespace InDet
         TrackHitDetails()
         {          
           isPatternTrack    = true;
+          inROI             = false;
           thishasblayer     = false;   
           hassharedblayer   = false;
           hassharedpixel    = false;
@@ -148,7 +150,7 @@ namespace InDet
         
         int totalSiHits()
         { 
-          return numUnused + numPixelDeadSensor + numSCTDeadSensor + numSplitSharedPix + numSplitSharedSCT/2;
+          return numUnused + numPixelDeadSensor + numSCTDeadSensor + numSplitSharedPix + numSplitSharedSCT + (inROI ? numShared/2 : 0) ;
         };
         
         void dumpInfo()
@@ -213,8 +215,8 @@ namespace InDet
         
         int findIndexOfPreviousMeasurement( int currentIndex )
         {
-	        int indexPreviousMeasurement = currentIndex-1;
-			    while(indexPreviousMeasurement >= 0){
+          int indexPreviousMeasurement = currentIndex-1;
+          while(indexPreviousMeasurement >= 0){
             if( type[indexPreviousMeasurement] != OtherTsos ){
                break;
             } else {
@@ -241,8 +243,11 @@ namespace InDet
       
       /** Check if the cluster is compatible with a hadronic cluster*/
       bool isHadCaloCompatible(const Trk::TrackParameters& Tp) const;      
+
+      /** Check if the cluster is compatible with a EM cluster*/
+      bool isEmCaloCompatible(const Trk::TrackParameters& Tp) const;      
       
-      /** Fill hadronic cluster map*/
+      /** Fill hadronic & EM cluster map*/
       void newEvent();
       
       /** Returns the number of track that use that hit already
@@ -252,7 +257,17 @@ namespace InDet
           
         */
       int checkOtherTracksValidity(const Trk::RIO_OnTrack*,bool isSplitable, int& maxiShared, int& maxothernpixel, bool& maxotherhasblayer, bool& failMinHits) const;
-      
+
+
+      /** Returns the Trackparameters of the two tracks on the n'th TrackStateOnSurface of the first track*/
+      std::pair< const Trk::TrackParameters* , const Trk::TrackParameters* > 
+         getOverlapTrackParameters(int n, const Trk::Track* track1, const Trk::Track* track2, int numSplitSharedPix ) const;
+
+      /** Check if two sets of track paremeters are compatible with being from a the same low mass particle decay. 
+          It is assumed that the track parmeters are on the same surface.*/
+      bool isNearbyTrackCandidate(const Trk::TrackParameters* paraA, const Trk::TrackParameters* paraB) const;
+
+
       /**Association tool - used to work out which (if any) PRDs are shared between 
        tracks*/
       ToolHandle<Trk::IPRD_AssociationTool> m_assoTool;
@@ -265,6 +280,9 @@ namespace InDet
       
       /**atlas id helper*/
       const SiliconID* m_detID;
+      
+      /**Observer tool      This tool is used to observe the tracks and their 'score' */
+      ToolHandle<Trk::ITrkObserverTool> m_observerTool;
       
       /** some cut values */
       int m_minHits;                // Min Number of hits on track            
@@ -300,10 +318,30 @@ namespace InDet
       std::vector<double>   m_hadE;
       std::vector<double>   m_hadR;
       std::vector<double>   m_hadZ;
+
+
+      bool  m_useEmClusSeed;
+      float m_minPtEm;
+      float m_phiWidthEm;
+      float m_etaWidthEm;
+
+      std::string m_inputEmClusterContainerName;
+      std::vector<double>   m_emF;
+      std::vector<double>   m_emE;
+      std::vector<double>   m_emR;
+      std::vector<double>   m_emZ;
+
       bool                  m_mapFilled;
-       
-      // Counters  
-      //mutable int 
+            
+      //Track Pair Selection
+      bool  m_doPairSelection;
+      float m_minPairTrackPt;
+      float m_pairDeltaX;
+      float m_pairDeltaY;
+      float m_pairDeltaPhi;
+      float m_pairDeltaEta;
+
+      bool m_monitorTracks; // to track observeration/monitoring (default is false)
       
     }; 
 } // end of namespace
