9fb8a0169e01720becd1a4fdca9282e286b03cd8
Graeme Stewart <graemes.cern@gmail.com>
2016-04-29 13:29:49 +0200

    Support steering for RAWtoALL workflow (RecJobTransforms-02-00-03)
    	* python/recTransformUtils.py
    	- New utility do detection of RAWtoALL before Reco_tf is setup
    	* scripts/Reco_tf.py
    	- Use new utility
    
    2016-04-27 Graeme Stewart <graeme.andrew.stewart@cern.ch>
    	** Manual merge of changes from RAWtoALL devbranch
    	* scripts/Reco_tf.py
    	- Pre-parse command line to determine if doRAWtoALL is requested (bit hacky)
    	* python/recTransformUtils.py
    	- Add RAWtoALL substep, disabled by default
    	* share/rawtodpd.py, share/skeleton.RAWtoALL_tf.py, python/DPDUtils.py
    	- New job options files for RAWtoALL
    
    2016-01-28 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* python/BsConfig.py, python/MixStreamConfig.py, python/RecConfig.py,
    	  python/streaming_arg.py
    	  - Removed the executable flag from these normal python modules.
    	* python/__init__.py
    	  - Added a dummy file to make the modules of this package usable in
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: a777ac2ae23ef64c196f3f45de9c57d5e7c68598

---
+++ Reconstruction/RecJobTransforms/share/skeleton.RAWtoALL_tf.py
@@ -0,0 +1,145 @@
+###############################################################
+#
+# Skeleton top job options for RAW->ALL
+#
+# TODO: Review of options supported here...
+#
+#==============================================================
+
+#Common job options disable most RecExCommon by default. Re-enable below on demand.
+include("RecJobTransforms/CommonRecoSkeletonJobOptions.py")
+
+#from AthenaCommon.Logging import logging
+import logging
+recoLog = logging.getLogger('raw_to_all')
+recoLog.info( '****************** STARTING RAW->ALL MAKING *****************' )
+
+## Input
+# BS
+DRAWInputs = [ prop for prop in dir(runArgs) if prop.startswith('inputDRAW') and prop.endswith('File')]
+if hasattr(runArgs,"inputBSFile"):
+    if len(DRAWInputs) > 0:
+        raise RuntimeError('Impossible to run RAWtoESD with input BS and DRAW files (one input type only!)')
+    rec.readRDO.set_Value_and_Lock( True )
+    globalflags.InputFormat.set_Value_and_Lock('bytestream')
+    athenaCommonFlags.BSRDOInput.set_Value_and_Lock( runArgs.inputBSFile )
+if len(DRAWInputs) == 1:
+    rec.readRDO.set_Value_and_Lock( True )
+    globalflags.InputFormat.set_Value_and_Lock('bytestream')
+    athenaCommonFlags.BSRDOInput.set_Value_and_Lock( getattr(runArgs, DRAWInputs[0]) )
+elif len(DRAWInputs) > 1:
+    raise RuntimeError('Impossible to run RAWtoESD with multiple input DRAW files (viz.: {0})'.format(DRAWInputs))
+        
+# RDO
+if hasattr(runArgs,"inputRDOFile"):
+    rec.readRDO.set_Value_and_Lock( True )
+    globalflags.InputFormat.set_Value_and_Lock('pool')
+    athenaCommonFlags.PoolRDOInput.set_Value_and_Lock( runArgs.inputRDOFile )
+    
+# EVNT (?)
+if hasattr(runArgs,"inputEVNTFile"):
+    #specific settings for AtlfastIIF
+    rec.readRDO.set_Value_and_Lock( True )
+    athenaCommonFlags.FilesInput.set_Value_and_Lock( runArgs.inputEVNTFile )
+
+
+## Output
+if hasattr(runArgs,"trigFilterList"):
+    rec.doTriggerFilter.set_Value_and_Lock(True)
+    rec.triggerFilterList = "|".join(runArgs.trigFilterList)
+    
+if hasattr(runArgs,"outputESDFile"):
+    rec.doESD.set_Value_and_Lock( True )
+    rec.doWriteESD.set_Value_and_Lock( True )
+    athenaCommonFlags.PoolESDOutput.set_Value_and_Lock( runArgs.outputESDFile )
+    
+if hasattr(runArgs,"outputAODFile"):
+    rec.doAOD.set_Value_and_Lock( True )
+    rec.doWriteAOD.set_Value_and_Lock( True ) 
+    athenaCommonFlags.PoolAODOutput.set_Value_and_Lock( runArgs.outputAODFile )
+
+if hasattr(runArgs,"outputHIST_R2AFile"):
+    rec.doMonitoring.set_Value_and_Lock(True)
+    from AthenaMonitoring.DQMonFlags import DQMonFlags
+    DQMonFlags.histogramFile.set_Value_and_Lock( runArgs.outputHIST_R2AFile )
+    DQMonFlags.monType='BSall' 
+
+if hasattr(runArgs,"outputTAG_COMMFile"):
+    rec.doWriteTAGCOM.set_Value_and_Lock( True )
+    rec.PoolTAGCOMOutput.set_Value_and_Lock( runArgs.outputTAG_COMMFile )
+
+if hasattr(runArgs,"outputHISTFile"):
+    rec.doMonitoring.set_Value_and_Lock(True)
+    from AthenaMonitoring.DQMonFlags import DQMonFlags
+    DQMonFlags.histogramFile.set_Value_and_Lock( runArgs.outputHISTFile )
+    DQMonFlags.monType='BSall' 
+
+if hasattr(runArgs, "outputTXT_FTKIPFile"):
+    rec.doCalo.set_Value_and_Lock(False)
+    rec.doInDet.set_Value_and_Lock(True)
+    rec.doMuon.set_Value_and_Lock(False)
+    rec.doJetMissingETTag.set_Value_and_Lock(False)
+    rec.doEgamma.set_Value_and_Lock(False)
+    rec.doMuonCombined.set_Value_and_Lock(False)
+    rec.doTau.set_Value_and_Lock(False)
+    rec.doTrigger.set_Value_and_Lock(False)
+    rec.UserAlgs=["FastTrackSimWrap/FastTrackSimWrap_jobOptions.py"]
+    
+# Event display tarballs    
+if hasattr(runArgs, 'outputTXT_JIVEXMLTGZFile'):
+    jp.Rec.doJiveXML.set_Value_and_Lock(True)
+
+# DESD and DRAW
+listOfFlags=[]
+try:
+    from PrimaryDPDMaker.PrimaryDPDFlags import primDPD
+    listOfFlags.append(primDPD)
+except ImportError:
+    print "WARNING PrimaryDPDFlags not available. Only OK if you're using job transforms without the AtlasAnalysis project."
+
+from RecJobTransforms.DPDUtils import SetupOutputDPDs
+rec.DPDMakerScripts.append(SetupOutputDPDs(runArgs,listOfFlags))
+
+DRAWOutputs = [ prop for prop in dir(runArgs) if prop.startswith('outputDRAW') and prop.endswith('File')]
+
+
+# Need to handle this properly in RecExCommon top options
+rec.OutputFileNameForRecoStep="RAWtoALL"
+
+## Pre-exec
+if hasattr(runArgs,"preExec"):
+    recoLog.info("transform pre-exec")
+    for cmd in runArgs.preExec:
+        recoLog.info(cmd)
+        exec(cmd)
+
+## Pre-include
+if hasattr(runArgs,"preInclude"): 
+    for fragment in runArgs.preInclude:
+        include(fragment)
+
+#========================================================
+# Central topOptions (this is one is a string not a list)
+#========================================================
+#if len(rec.DPDMakerScripts()) > 0 and len(DRAWOutputs) == 0:
+if len(rec.DPDMakerScripts()) > 0:
+    rec.doDPD = True
+    rec.DPDMakerScripts.append("PrimaryDPDMaker/PrimaryDPDMaker.py")
+include( "RecExCommon/RecExCommon_topOptions.py" )
+
+if hasattr(runArgs,"inputRDO_TRIGFile") and rec.doFileMetaData():
+    ToolSvc += CfgMgr.xAODMaker__TriggerMenuMetaDataTool( "TriggerMenuMetaDataTool",
+                              OutputLevel = 3 )
+    svcMgr.MetaDataSvc.MetaDataTools += [ ToolSvc.TriggerMenuMetaDataTool ]
+
+## Post-include
+if hasattr(runArgs,"postInclude"): 
+    for fragment in runArgs.postInclude:
+        include(fragment)
+
+## Post-exec
+if hasattr(runArgs,"postExec"):
+    recoLog.info("transform post-exec")
+    for cmd in runArgs.postExec:
+        recoLog.info(cmd)
+        exec(cmd)
