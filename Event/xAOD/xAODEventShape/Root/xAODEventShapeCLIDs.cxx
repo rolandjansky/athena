1bbebdebd1dfbb5c153bcd24158fae385fca7f81
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-02 15:37:34 +0200

    Creating a new tag with a slightly tweaked CMakeLists.txt file (xAODEventShape-00-00-09)
    	* Creating a new tag with a slightly tweaked CMakeLists.txt
    	  file.
    	* Tagging as xAODEventShape-00-00-09
    
    2015-06-02 Will Buttinger <will@cern.ch>
    	* Root/xAODEventShapeCLIDs.cxx: added to get CLID into db (included change to cmt/requirements)
    	* Tagging as xAODEventShape-00-00-08
    
    
    Former-commit-id: 8cdb7f76d4874054a7a94f11bfffad08a31cb410

---
+++ Event/xAOD/xAODEventShape/Root/xAODEventShapeCLIDs.cxx
@@ -0,0 +1,9 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+//simple includes to force the CLASS_DEF etc to be encountered during compile
+
+#include "xAODEventShape/EventShape.h"
+#include "xAODEventShape/EventShapeAuxInfo.h"
