4a681d4feb5497e48a496e18b1dce667a45a8bdd
Javier Montejo Berlingen <javier.montejo.berlingen@cern.ch>
2016-05-04 14:58:18 +0200

    Fix for jet window size in JJ15.23ETA49: ATR-13984 (TriggerMenuXML-04-01-91)
    	* Fix for jet window size in JJ15.23ETA49: ATR-13984
    	* Belongs to TriggerMenu-00-17-89
    	* TriggerMenuXML-04-01-91
    
    2016-05-04 Emma Torro
            * adding L1_J15.31ETA49_UNPAIRED_ISO (ATR-13982)
            * Belongs to TriggerMenu-00-17-84
            * TriggerMenuXML-04-01-90
    2016-04-29 Nils Ruthmann
            * Changing AFP L1 items (ATR-13506)
            * TriggerMenuXML-04-01-89
    2016-04-27 Nils Ruthmann
            * Adding AFP L1 items (ATR-13506)
            * TriggerMenuXML-04-01-88
    2016-04-27 Nils Ruthmann
            * Adjusting EM and Tau isol thresholds (ATR-13892)
            * TriggerMenuXML-04-01-87
    2016-04-26 Sophio Pataraia
            * add L1 item L1_TGC_BURST_EMPTY in LVL1_MC_pp_v6 (ATR-13924)
            * TriggerMenuXML-04-01-86
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 842d1083e7925cba6e80388cef91d576b9ec33c4

--- Trigger/TriggerCommon/TriggerMenuXML/scripts/XMLDumperFromAthena.sh
+++ Trigger/TriggerCommon/TriggerMenuXML/scripts/XMLDumperFromAthena.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env zsh
-# Script to generate generate the trigger menu XML files
+# Script to generate the trigger menu XML files
 #
 
-if [ $# -ne 1 ]; then
-    echo "Syntax: XMLDumperFromAthena.sh MENU"
+if [ $# -lt 1 ]; then
+    echo "Syntax: XMLDumperFromAthena.sh MENU [DEST]"
     exit 1
 fi
 
 menu=$1
+dest=$2
+if [ -z "$dest" ]; then
+    dest=$PWD
+fi
+
+# Get absolute path name
+dest=`cd $dest; pwd`
+
 jo=TriggerMenuXML/runHLT_forXMLgeneration.py
 sqliteFile=TriggerMenuSQLiteFile.sqlite
 uploadLog=TriggerMenuUpload.log
 doDBUpload="false"
 
 if [[ "$NICOS_ATLAS_RELEASE" != "" ]]; then
-    release=$NICOS_ATLAS_RELEASE
+    if [[ "$NICOS_ATLAS_ALT_RELEASE" != "None" ]]; then   # MIG nightlies
+        release=$NICOS_ATLAS_ALT_RELEASE
+    else
+        release=$NICOS_ATLAS_RELEASE
+    fi
     if [[ "$AtlasProject" != "AtlasP1HLT" && "$AtlasProject" != "AtlasCAFHLT" && "$AtlasProject" != "AtlasTestHLT" ]]; then
         #doDBUpload="true"     # Temporarily disabled due to sqlite slowness (Savannah #102038)
         # we upload the default menu
-	menuForUpload=`python -c "from TriggerJobOpts import TriggerFlags;print TriggerFlags.triggerMenuSetup().get_Value()" | tail -1`
+	    menuForUpload=`python -c "from TriggerJobOpts import TriggerFlags;print TriggerFlags.triggerMenuSetup().get_Value()" | tail -1`
     fi
 else
     release=`python -c "from TriggerJobOpts import TriggerFlags; print(TriggerFlags.menuVersion().get_Value())" | tail -1`
@@ -49,28 +61,38 @@ TRAPTERM() {
 
 
 ## menu generation starts here
-pkgdir=`readlink -f $PWD/../`
-echo "XMLDumperFromAthena: *** Building menu: $menu for $release ***"
+if [ -n "${AtlasTrigger_PLATFORM}" ]; then   # CMAKE
+    platform=${AtlasTrigger_PLATFORM}
+else  # CMT
+    platform=${CMTCONFIG}
+fi
+echo "XMLDumperFromAthena: *** Building menu: ${menu} for ${release} ***"
 logfiletopo=topo${menu}.log
 logfile=${menu}.log
 cd $rundir
 
 # L1Topo config file
 generateL1TopoMenu.py $menu >&! $logfiletopo
-cp L1Topoconfig_*.xml ${pkgdir}/data
-
+cp L1Topoconfig_*.xml ${dest}
 
 # L1 + HLT config file
-athena.py -c "TriggerMenuSetup='$menu'" $jo >&! $logfile
-athena_exit=$?
-cp $logfile $logfiletopo ${pkgdir}/${CMTCONFIG}
-if [ $athena_exit -eq 0 ]; then
-    echo "XMLDumperFromAthena: *** $menu DONE | Exit code: $athena_exit | Log: $pkgdir/$CMTCONFIG/$logfile ***"
+if [ -z "$TMXML_BUILD_TEST" ]; then
+    athena.py -c "TriggerMenuSetup='$menu'" $jo >&! $logfile
+    athena_exit=$?
 else
-    echo "XMLDumperFromAthena: *** $menu FAILED | Exit code: $athena_exit | Log: $pkgdir/$CMTCONFIG/$logfile ***"
+    # Set the above env var to fake the output files (for fast build system test)
+    echo $menu > outputLVL1config.xml
+    echo $menu > outputHLTconfig.xml
+    echo $menu > $logfile
+    athena_exit=0
 fi
 
-
+cp $logfile $logfiletopo ${dest}
+if [ $athena_exit -eq 0 ]; then
+    echo "XMLDumperFromAthena: *** $menu DONE | Exit code: $athena_exit | Log: $dest/$logfile ***"
+else
+    echo "XMLDumperFromAthena: *** $menu FAILED | Exit code: $athena_exit | Log: $dest/$logfile ***"
+fi
 
 
 #upload default menu into to the SQLite file
@@ -81,29 +103,28 @@ if [[ ( ${doDBUpload} = "true" ) && ( ${menu} = ${menuForUpload} ) && ( -s outpu
     cmd=(java -jar TriggerTool.jar -up -release $release -l1_menu outputLVL1config.xml -hlt_menu outputHLTconfig.xml -name $menu -offlineconfig -dbConn "sqlite_file:$sqliteFile" --outputLevel FINER --output $uploadLog)
     echo "XMLDumperFromAthena: Upload command: java $cmd"
     ${cmd}
-    cp $uploadLog $pkgdir/$CMTCONFIG/TriggerMenuUpload.log
-    echo "Copying sqlite file $sqliteFile to $pkgdir/data/TriggerMenuSQLiteFile_${release}.sqlite"
+    cp $uploadLog $dest/TriggerMenuUpload.log
+    echo "Copying sqlite file $sqliteFile to $dest/TriggerMenuSQLiteFile_${release}.sqlite"
     chmod 777 $sqliteFile
-    cp $sqliteFile ${pkgdir}/data/TriggerMenuSQLiteFile_${release}.sqlite
+    cp $sqliteFile ${dest}/TriggerMenuSQLiteFile_${release}.sqlite
     
     if [[ -e MenusKeys.txt ]]; then
-        echo "Copying MenuKeys.txt to ${pkgdir}/data/MenusKeys_${release}.txt"
-        cp MenusKeys.txt ${pkgdir}/data/MenusKeys_${release}.txt
+        echo "Copying MenuKeys.txt to ${dest}/MenusKeys_${release}.txt"
+        cp MenusKeys.txt ${dest}/MenusKeys_${release}.txt
     else
         echo "Error : MenuKeys.txt does not exist, likely a DB upload problem "
     fi
 fi
 
 if [[ -e outputLVL1config.xml ]]; then
-    cp outputLVL1config.xml ${pkgdir}/data/LVL1config_${menu}_${release}.xml
-    cp outputHLTconfig.xml ${pkgdir}/data/HLTconfig_${menu}_${release}.xml
+    cp outputLVL1config.xml ${dest}/LVL1config_${menu}_${release}.xml
+    cp outputHLTconfig.xml ${dest}/HLTconfig_${menu}_${release}.xml
 fi
 
 #this gives some more sensitivity to hidden problems
-grep --colour ERROR $pkgdir/$CMTCONFIG/$logfile
-grep --colour -A 100 "Shortened traceback" $pkgdir/$CMTCONFIG/$logfile
+grep --colour ERROR ${dest}/$logfile
+grep --colour -A 100 "Shortened traceback" ${dest}/$logfile
 
-cd $pkgdir/cmt
 rm -rf $rundir
 
 # Do not return real athena exit code as we want to pretend everything was fine
