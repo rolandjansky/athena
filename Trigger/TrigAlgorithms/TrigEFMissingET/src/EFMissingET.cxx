e516f3c0de4d7b1904f40a763327733651074b71
Kenji Hamano <kenji.hamano@cern.ch>
2016-05-05 21:43:51 +0200

    fix for ATR-11794 (TrigEFMissingET-00-04-27)
    Former-commit-id: c2f91792c324d9872e08ee715360039b2b0396d6

--- Trigger/TrigAlgorithms/TrigEFMissingET/src/EFMissingET.cxx
+++ Trigger/TrigAlgorithms/TrigEFMissingET/src/EFMissingET.cxx
@@ -48,7 +48,7 @@ EFMissingET::EFMissingET(const std::string & name, ISvcLocator* pSvcLocator):
   declareProperty("MissingETOutputKey",m_metOutputKey = "TrigEFMissingET", "label for the MET feature in the HLT navigation");
   declareProperty("Tools", m_tools, "list of missEt tools");
   declareProperty("doTimers", m_doTimers = true, "switch on/off internal timers");
-  declareProperty("DecodeDetMask", m_decodeDetMask = true, "switch on/off DetMask decoding");
+  declareProperty("DecodeDetMask", m_decodeDetMask = false, "switch on/off DetMask decoding");
   declareProperty("doTopoClusters", m_doTopoClusters = false, "run with or without topo. clusters");
   declareProperty("doJets", m_doJets = false, "run with or without jets");
   declareProperty("doPUC", m_doPUC = false, "run with or without pile-up correction fit");
@@ -456,17 +456,16 @@ HLT::ErrorCode EFMissingET::hltExecute(std::vector<std::vector<HLT::TriggerEleme
         m_TileExtBarCside=true;
 
         if (m_decodeDetMask) {
-          uint64_t mask0 = pEvent->detectorMask0();
-          uint64_t mask1 = pEvent->detectorMask1();
+	  uint64_t mask64 = pEvent->detectorMask();
           if(msgLvl() <= MSG::DEBUG){
             char buff[512];
-            snprintf(buff,512,"REGTEST: DetMask_1 = 0x%08lu, DetMask_0 = 0x%08lu",mask1,mask0);
+            snprintf(buff,512,"REGTEST: DetMask_1 = 0x%08lu",mask64);
             msg() << MSG::DEBUG << buff << endreq;
           }
 
-          if (!(mask0==0 && mask1==0)) {  // 0 means present
+          if (!(mask64==0)) {  // 0 means present
 
-            eformat::helper::DetectorMask dm(mask1, mask0);
+            eformat::helper::DetectorMask dm(mask64);
             m_LArEMbarrelAside  = dm.is_set(eformat::LAR_EM_BARREL_A_SIDE);
             m_LArEMbarrelCside  = dm.is_set(eformat::LAR_EM_BARREL_C_SIDE);
             m_LArEMendCapAside  = dm.is_set(eformat::LAR_EM_ENDCAP_A_SIDE);
