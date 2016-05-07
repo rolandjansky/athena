3e4985b319a4eb1bd9948bd045920b1eb5e17a1f
William Axel Leight <william.axel.leight@cern.ch>
2016-05-07 19:53:54 +0200

    Fixing mistake in previous commit (xAODMuonAthenaPool-00-06-01)
    	* Fixing mistake in previous commit
    	* Tagging as xAODMuonAthenaPool-00-06-01
    
    2015-05-05 Will Leight
    	* Migrated to v4 of MuonAuxContainer adding MS-only extrapolated track particle
    	* Tagging as xAODMuonAthenaPool-00-06-00
    
    2015-11-28  scott snyder  <snyder@bnl.gov>
    
    	* Tagging xAODMuonAthenaPool-00-05-02.
    	* src/xAODMuonAthenaPoolTPCnv.cxx: Declare _v2 converter.
    
    2015-11-25 Edward.Moyse@cern.ch
    	* Migrated to v3 of MuonAuxContainer, which adds varcone* to the Aux store
    	* Tagging xAODMuonAthenaPool-00-05-01
    
    
    Former-commit-id: 0f29d882b1cdbff942d88d7242333e55ec5caa68

--- Event/xAOD/xAODMuonAthenaPool/src/xAODMuonAuxContainerCnv.cxx
+++ Event/xAOD/xAODMuonAthenaPool/src/xAODMuonAuxContainerCnv.cxx
@@ -10,6 +10,8 @@
 #include "AthContainers/tools/copyThinned.h"
 #include "AthenaKernel/IThinningSvc.h"
 #include "xAODMuonAuxContainerCnv_v1.h"
+#include "xAODMuonAuxContainerCnv_v2.h"
+#include "xAODMuonAuxContainerCnv_v3.h"
 
 xAODMuonAuxContainerCnv::
 xAODMuonAuxContainerCnv( ISvcLocator* svcLoc )
@@ -30,11 +32,29 @@ xAODMuonAuxContainerCnv::createTransient() {
    // The known ID(s) for this container:
    static const pool::Guid v1_guid( "EC9B677A-B3BA-4C75-87D3-373FC478291E" );
    static const pool::Guid v2_guid( "AB53C81C-B10C-11E4-AC59-6C3BE51AB9F1" );
+   static const pool::Guid v3_guid( "ECC65005-302B-4662-ACBA-D2CE5A0218B8" );
+   static const pool::Guid v4_guid( "8251F481-EA4C-4852-AE72-BED87E6FD2FB" );
 
    // Check which version of the container we're reading:
-   if( compareClassGuid( v2_guid ) ) {
+   if( compareClassGuid( v4_guid ) ) {
      // It's the latest version, read it directly:
      return poolReadObject< xAOD::MuonAuxContainer >();
+   }else if( compareClassGuid( v3_guid ) ) {
+     // Handle dynamic to aux conversion.
+     // The v3 converter:
+     static xAODMuonAuxContainerCnv_v3 converter;
+     std::unique_ptr< xAOD::MuonAuxContainer_v3 >
+       old( poolReadObject< xAOD::MuonAuxContainer_v3 >() );
+
+     return converter.createTransient( old.get(), msg() );
+   } else if( compareClassGuid( v2_guid ) ) {
+     // Handle dynamic to aux conversion.
+     // The v2 converter:
+     static xAODMuonAuxContainerCnv_v2 converter;
+     std::unique_ptr< xAOD::MuonAuxContainer_v2 >
+             old( poolReadObject< xAOD::MuonAuxContainer_v2 >() );
+     
+     return converter.createTransient( old.get(), msg() );
    } else if( compareClassGuid( v1_guid ) ) {
      // Fix for 'charge' bug ATLASRECTS-1577
   	 static xAODMuonAuxContainerCnv_v1 converter;
