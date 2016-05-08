0864bfa05c44e1fc005d865341ba797cd2ff2b61
Iwona Grabowska-Bold <iwona.grabowskabold@gmail.com>
2016-05-08 07:21:33 +0200

    fix of the main moniotirng trigger name (noalg_mb_L1RD0_FILLED->noalg_L1RD0_FILLED) (TrigMinBiasMonitoring-00-01-34)

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitoringConfig.py
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitoringConfig.py
@@ -27,7 +27,7 @@ def TrigMinBiasMonitoringTool():
 					  InDetTrackParticleContainerName = "InDetTrackParticles",
 					  
 					  monitoring_minbias = hltmonList.monitoring_minbias,
-					  MinBiasRefTrigItem = "noalg_mb_L1RD0_FILLED", # "mb_sptrk" should be used
+					  MinBiasRefTrigItem = "noalg_L1RD0_FILLED", # "mb_sptrk" should be used
                                           MinBiasAvailableAlgorithms = ['sp', 'sptrk', 'mbts', 'noalg', 'hmtperf',  'idperf',   'perf',   'hmt'], #the order here matters: more specific names should go before general eg. 'ideperf' before 'perf'... 
                                           MinBiasHistoTargets =     [IDMINBIAS,  IDMINBIAS, MBTS,  0,  HMT,   0,  MBTS + IDMINBIAS,   HMT],
 					  IsPassedCondtitions =     [PHYSICS,  PHYSICS, PHYSICS, L1_ISPASSEDBEFOREPRESCALE,  PHYSICS,  	PHYSICS,   PHYSICS,   PHYSICS],
