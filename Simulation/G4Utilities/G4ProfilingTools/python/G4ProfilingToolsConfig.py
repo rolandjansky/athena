95cc79026aced0e7a353b0c7db3d9c6171caeb07
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-11 14:45:03 +0200

    share/postInclude.G4ProfilingTools.py - update syntax to work for both V1 and V2 UserActions. ATLASSIM-2226. Tagging G4ProfilingTools-00-00-04 (G4ProfilingTools-00-00-04)
    	* share/postInclude.G4ProfilingTools.py - update syntax to work
    	for both V1 and V2 UserActions. ATLASSIM-2226
    	* tagging G4ProfilingTools-00-00-04
    
    2016-05-05 Andrea Di Simone
    	* implemeting V2 (MT) user actions
    	* tagging G4ProfilingTools-00-00-03
    
    
    Former-commit-id: 058413b368874a005f77121b92e325b126b07ae1

---
+++ Simulation/G4Utilities/G4ProfilingTools/python/G4ProfilingToolsConfig.py
@@ -0,0 +1,57 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon import CfgMgr,Logging
+from G4AtlasServices import G4AtlasServicesConfig
+
+
+def getTestActionTimerTool(name="G4UA::TestActionTimerTool", **kwargs):
+    from AthenaCommon.ConcurrencyFlags import jobproperties as concurrencyProps
+    if concurrencyProps.ConcurrencyFlags.NumThreads() >1:
+        log=Logging.logging.getLogger(name)
+        log.fatal('Attempt to run '+name+' with more than one thread, which is not supported')
+        #from AthenaCommon.AppMgr import theApp
+        #theApp.exit(1)
+        return False
+
+    from G4AtlasApps.SimFlags import simFlags
+    # example custom configuration
+    if name in simFlags.UserActionConfig.get_Value().keys():
+        for prop,value in simFlags.UserActionConfig.get_Value()[name].iteritems():
+            kwargs.setdefault(prop,value)
+    from G4ProfilingTools.G4ProfilingToolsConf import G4UA__TestActionTimerTool
+    return G4UA__TestActionTimerTool(name, **kwargs)
+        
+def getTestActionVPTimerTool(name="G4UA::TestActionVPTimerTool", **kwargs):
+    from AthenaCommon.ConcurrencyFlags import jobproperties as concurrencyProps
+    # ADS: this tool should be safe to run in MT
+    #if concurrencyProps.ConcurrencyFlags.NumThreads() >1:
+    #    log=Logging.logging.getLogger(name)
+    #    log.fatal('Attempt to run '+name+' with more than one thread, which is not supported')
+    #    return False
+    
+    from G4AtlasApps.SimFlags import simFlags
+    # example custom configuration
+    if name in simFlags.UserActionConfig.get_Value().keys():
+        for prop,value in simFlags.UserActionConfig.get_Value()[name].iteritems():
+            kwargs.setdefault(prop,value)
+            
+    from G4ProfilingTools.G4ProfilingToolsConf import G4UA__TestActionVPTimerTool
+    return G4UA__TestActionVPTimerTool(name, **kwargs)
+
+def getTestActionEHistTool(name="G4UA::TestActionEHistTool", **kwargs):
+    from AthenaCommon.ConcurrencyFlags import jobproperties as concurrencyProps
+    if concurrencyProps.ConcurrencyFlags.NumThreads() >1:
+        log=Logging.logging.getLogger(name)
+        log.fatal('Attempt to run '+name+' with more than one thread, which is not supported')
+        #from AthenaCommon.AppMgr import theApp
+        #theApp.exit(1)
+        return False
+
+    from G4AtlasApps.SimFlags import simFlags
+    # example custom configuration
+    if name in simFlags.UserActionConfig.get_Value().keys():
+        for prop,value in simFlags.UserActionConfig.get_Value()[name].iteritems():
+            kwargs.setdefault(prop,value)
+            
+    from G4ProfilingTools.G4ProfilingToolsConf import G4UA__TestActionEHistTool
+    return G4UA__TestActionEHistTool(name, **kwargs)
--- Simulation/G4Utilities/G4ProfilingTools/src/TestActionEHist.h
+++ Simulation/G4Utilities/G4ProfilingTools/src/TestActionEHist.h
@@ -81,4 +81,67 @@ class TestActionEHist final: public UserActionBase
 };
 
 
+#include "G4AtlasInterfaces/IPreTrackingAction.h"
+#include "G4AtlasInterfaces/IPostTrackingAction.h"
+#include "G4AtlasInterfaces/IBeginRunAction.h"
+#include "G4AtlasInterfaces/IEndRunAction.h"
+#include "G4AtlasInterfaces/ISteppingAction.h"
+namespace G4UA{
+
+  class TestActionEHist:
+  public IPreTrackingAction,  public IPostTrackingAction,  public IBeginRunAction,  public IEndRunAction,  public ISteppingAction
+  {
+    
+  public:
+    
+    struct Config
+    {      
+      int dCALO=2;
+      int dBeam=2;
+      int dIDET=2;
+      int dMUON=2;       //!< Used for depth limits applied in jobOptions
+      int maxhists=1000;
+      int maxdirs=1000;            //!< Used to set maximum number of histograms/directories from jobOptions
+      std::string dDetail="";  //!< Path used to set detailed depth cuts
+      std::string name="default.root";	//!< Name of root output file
+    };
+    
+    TestActionEHist(const Config& config);
+    virtual void preTracking(const G4Track*) override;
+    virtual void postTracking(const G4Track*) override;
+    virtual void beginOfRun(const G4Run*) override;
+    virtual void endOfRun(const G4Run*) override;
+    virtual void processStep(const G4Step*) override;
+  private:
+    Config m_config;	//!< holds the python configuration
+    
+    TFile* m_world;				//!< File in which to store neutron & electron info
+    
+    bool m_firstStep;			//!< Flag indicating whether step is first in current volume
+    
+    std::string m_p_tag;			//!< Used to specify current particle in tracking
+
+    std::vector<std::string> m_trajectory;	//!< Used to store volume names which the current track has entered
+    
+    void BuildHists (		//!< Function to generate TH1-type histograms
+		     std::string vol_tag,		//!< Tag to identify object by volume
+		     std::string part_tag,		//!< Tag to identify object by particle
+		     int& hLeft,			//!< Amount of histogram space remaining, set by maxhists
+		     double xfill = -1,		//!< Parameter to fill histogram along x
+		     double yfill = -1,		//!< Paramteter to fill histogram along y
+		     const int nbins = 3000,	//!< Number of bins in histogram
+		     const int binsize = 1);	//!< Size of bins in histogram, in MeV
+    
+    bool BuildDirs (		//!< Function to generate TDirectory objects
+		    std::string vol_tag,		//!< Tag to identify object by volume
+		    std::string dirTitle,		//!< Directory title
+		    int& dLeft);			//!< Remaining directories to create
+    
+    
+    
+  }; // class TestActionEHist
+  
+  
+} // namespace G4UA 
+
 #endif // #define TestActionEHist_H
