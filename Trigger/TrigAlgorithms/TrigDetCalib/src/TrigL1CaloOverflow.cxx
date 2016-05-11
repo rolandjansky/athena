61075a6392b582d13c3a04ceb138ca6296f1778d
Frank Winklmeier <Frank.Winklmeier@cern.ch>
2016-05-11 13:26:57 +0200

    Ignore events in the abort gap and also check RoIB overflows (ATR-12285) (TrigDetCalib-01-02-16)
    	* src/TrigL1CaloOverflow.cxx: Ignore events in the abort gap and also check RoIB overflows (ATR-12285)
    	* TrigDetCalib-01-02-16
    
    
    Former-commit-id: 885d004ae1461b4ce05a9c71758316e70357187d

--- Trigger/TrigAlgorithms/TrigDetCalib/src/TrigL1CaloOverflow.cxx
+++ Trigger/TrigAlgorithms/TrigDetCalib/src/TrigL1CaloOverflow.cxx
@@ -3,14 +3,23 @@
 */
 
 #include "TrigL1CaloOverflow.h"
+
 #include "TrigT1Result/RoIBResult.h"
+#include "xAODEventInfo/EventInfo.h"
 
 #include <bitset>
+
+// BCIDs of the abort gap
+const int ABORT_GAP_START = 3446;
+const int ABORT_GAP_END   = 3563;
 
 TrigL1CaloOverflow::TrigL1CaloOverflow(const std::string& name, ISvcLocator* pSvcLocator):
   HLT::AllTEAlgo(name, pSvcLocator),
   m_lvl1Tool("HLT::Lvl1ResultAccessTool/Lvl1ResultAccessTool",this)
 {
+  declareProperty("ignoreAbortGap", m_ignoreAbortGap = true, "Ignore overflows in abort gap");
+  declareProperty("acceptCMXOverflows", m_acceptCMXOverflows = true, "Accept CMX overflows");
+  declareProperty("acceptCaloRoIBOverflows", m_acceptCaloRoIBOverflows = true, "Accept Calo RoIB overflows");
 }
 
 HLT::ErrorCode TrigL1CaloOverflow::hltInitialize()
@@ -26,14 +35,33 @@ HLT::ErrorCode TrigL1CaloOverflow::hltExecute(std::vector<std::vector<HLT::Trigg
 {
   beforeExecMonitors().ignore();
 
+  // Do nothing for events in the abort gap (tile laser causes overflows)
+  if (m_ignoreAbortGap) {
+    const xAOD::EventInfo* evt(0);
+    if (evtStore()->retrieve(evt).isSuccess() &&
+        evt->bcid() >= ABORT_GAP_START && evt->bcid() <= ABORT_GAP_END) {
+      return HLT::OK;
+    }
+  }
+
   const ROIB::RoIBResult* result;
   if (evtStore()->retrieve(result).isFailure()) return HLT::OK;
 
-  std::bitset<3> overflow = m_lvl1Tool->lvl1EMTauJetOverflow(*result);
-  if (overflow.any()) {
-    HLT::TriggerElement* te = addRoI(output);
-    te->setActiveState(true);
-    ATH_MSG_DEBUG("Event was force accepted by L1Calo due to TOB overflows in CMX transmission");
+  // Overflows in the TOB transmission to CMX
+  if (m_acceptCMXOverflows) {
+    std::bitset<3> overflow = m_lvl1Tool->lvl1EMTauJetOverflow(*result);
+    if (overflow.any()) {
+      addRoI(output)->setActiveState(true);
+      ATH_MSG_DEBUG("Event was force accepted by L1Calo due to TOB overflows in CMX transmission");
+    }
+  }
+
+  // Overflows on the RoI link to the RoIB
+  if (m_acceptCaloRoIBOverflows) {
+    if (result->CheckEMOverflow() || result->CheckJOverflow()) {
+      addRoI(output)->setActiveState(true);
+      ATH_MSG_DEBUG("Event has overflows in the EM or JET RoI transmission to the RoIB");
+    }
   }
   
   afterExecMonitors().ignore();
