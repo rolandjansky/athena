a2a5179cbfc51a06fcf8b7da78dd94789dfc6379
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-11 14:45:03 +0200

    share/postInclude.G4ProfilingTools.py - update syntax to work for both V1 and V2 UserActions. ATLASSIM-2226. Tagging G4ProfilingTools-00-00-04 (G4ProfilingTools-00-00-04)
    	* share/postInclude.G4ProfilingTools.py - update syntax to work
    	for both V1 and V2 UserActions. ATLASSIM-2226
    	* tagging G4ProfilingTools-00-00-04
    
    2016-05-05 Andrea Di Simone
    	* implemeting V2 (MT) user actions
    	* tagging G4ProfilingTools-00-00-03
    
    
    Former-commit-id: 823b62909b6514204be94425de6a3f66626ab6ff

--- Simulation/G4Utilities/G4ProfilingTools/src/TestActionTimer.h
+++ Simulation/G4Utilities/G4ProfilingTools/src/TestActionTimer.h
@@ -73,4 +73,85 @@ private:
   int ClassifyVolume( G4String& ) const; //!< Method to sort out which volume we are in
 };
 
+#include "G4AtlasInterfaces/IBeginEventAction.h"
+#include "G4AtlasInterfaces/IEndEventAction.h"
+#include "G4AtlasInterfaces/IBeginRunAction.h"
+#include "G4AtlasInterfaces/IEndRunAction.h"
+#include "G4AtlasInterfaces/ISteppingAction.h"
+namespace G4UA{
+  
+  /// @class TestActionTimer
+  /// @brief User action to measure time spent in subdetectors
+  ///
+  /// 
+  ///         @author Zachary Marshall, Caltech, USA                       
+  ///         @author Wolfgang Ehrenfeld, University of Hamburg, Germany
+
+  class TestActionTimer:
+  public IBeginEventAction,  public IEndEventAction,  public IBeginRunAction,  public IEndRunAction,  public ISteppingAction
+  {
+    
+  public:
+    /// constructor
+    TestActionTimer();
+    
+    /// this holds all the data from individual threads that needs to be merged at EoR
+    struct Report
+    {
+      int nev=0;
+      std::vector<double> time; //!< Vector of timers for each of the enum
+      std::vector<std::string> timeName;  //!< Vector of names for each of the timers
+      double runTime=0;
+      void merge(const Report& rep){
+	nev+=rep.nev;
+	runTime+=rep.runTime;
+	// copy first report
+	if(time.empty()){
+	  time=rep.time;
+	  timeName=rep.timeName;
+	  return;
+	}
+	// sum the following ones
+	for(unsigned int i=0;i<time.size();++i)
+	  time[i]+=rep.time[i];
+      }
+    };
+    
+    const Report& getReport() const
+    { return m_report; }
+    
+    virtual void beginOfEvent(const G4Event*) override;
+    virtual void endOfEvent(const G4Event*) override;
+    virtual void beginOfRun(const G4Run*) override;
+    virtual void endOfRun(const G4Run*) override;
+    virtual void processStep(const G4Step*) override;
+
+    /* Enumeration for timers to be used
+       First timers are by subdetector, second few are by particle
+       These are not straightforward for the non-expert to interpret*/
+    enum { eEMB, eEMEC, eFC1, eFC23, eFCO, eHEC, eCry, eLAr, eHCB, 
+	   ePre, eMu, ePx, eSct, eSev, eTrt, eOther, 
+	   eElec, ePos, eGam, eNeut, eMax };
+
+  private:
+    Report m_report;
+
+    
+    G4Timer* m_runTimer;                     //!< Timer for the entire run
+    G4Timer* m_eventTimer;                   //!< Timer for this event
+    double  m_eventTime;           //!< Double for storing this event
+    
+    std::vector<G4Timer*> m_timer;           //!< Vector of timers for each of the enum
+    
+    double TimerSum(G4Timer* timer) const;   //!< Gets the appropriate time from the timer for adding to the sum
+    
+    void PPanic();                           //!< Method to shut down all particle timers
+    void VPanic();                           //!< Method to shut down all volume timers
+    int ClassifyVolume( G4String& ) const; //!< Method to sort out which volume we are in
+    
+  }; // class TestActionTimer
+  
+  
+} // namespace G4UA 
+
 #endif // #define TestActionTimer_H
--- Simulation/G4Utilities/G4ProfilingTools/src/TestActionVPTimer.cxx
+++ Simulation/G4Utilities/G4ProfilingTools/src/TestActionVPTimer.cxx
@@ -113,7 +113,10 @@ void TestActionVPTimer::EndOfRun(const G4Run* /*aRun*/)
 	 << "#########################################" << G4endl;
 #endif
   
+  // this stops the timer
   m_runTime += TimerSum(m_runTimer);
+
+  std::cout<<"TestActionVPTimer::EndOfRun "<< m_runTime<<std::endl;
 
   VolTree topPV;
   topPV.push_back(v_time_index.begin()->first.front());
@@ -242,3 +245,98 @@ StatusCode TestActionVPTimer::queryInterface(const InterfaceID& riid, void** ppv
   }
   return StatusCode::SUCCESS;
 }
+
+
+namespace G4UA{
+
+
+  TestActionVPTimer::TestActionVPTimer(const Config& config):m_config(config),m_report(),
+							     m_runTimer(0), m_eventTimer(0),
+							     m_eventTime(0.){
+
+    // create event & run timers
+    m_runTimer = new G4Timer();
+    m_runTimer->Start();
+    m_runTimer->Stop();
+    m_eventTimer = new G4Timer();
+    m_eventTimer->Start();
+    m_eventTimer->Stop();
+    
+    // create step timer
+    v_timer = new G4Timer();
+    v_timer->Start();
+    v_timer->Stop();
+
+  }
+
+  void TestActionVPTimer::beginOfEvent(const G4Event*){
+    m_report.nev++;
+    m_eventTimer->Start();
+  }
+  
+  void TestActionVPTimer::endOfEvent(const G4Event*){
+    // this function also stops the timer. it will be restarted at BoE
+    m_eventTime += TimerSum(m_eventTimer);
+
+  }
+  
+  void TestActionVPTimer::beginOfRun(const G4Run*){;
+    m_runTimer->Start();
+  }
+  
+  void TestActionVPTimer::endOfRun(const G4Run*){
+    // this also stops the timer
+
+    m_report.runTime += TimerSum(m_runTimer);
+
+  }
+  
+  void TestActionVPTimer::processStep(const G4Step* aStep){
+
+    // HERE IS WHERE WE BEGIN OUR CLOCKING -- ONLY IF 
+    // TIMERS ARE NOT VALID
+    
+    if (!v_timer->IsValid()) {
+      
+      // Collect the total time before processing anything else
+      // this stops the timer
+      double vtime = TimerSum(v_timer);
+      
+      // CHECKIN' OUT THA NAVIGATA
+      VolumeTreeNavigator currentTree( aStep );
+      
+      // Set depth cuts here
+      currentTree.SetDepthCutSimple(m_config.dCALO, m_config.dBeam, m_config.dIDET, m_config.dMUON);
+      if ( !m_config.dDetail.empty() ) {
+        currentTree.SetDepthCutDetail( m_config.dDetail.c_str() );
+      }
+      
+      // Store time generated in current volume, remove deepest entry in v_history, then
+      // repeat for the resulting VolTree (one level higher)
+      G4ParticleDefinition* PDef = currentTree.GetTrack()->GetDefinition();
+      while ( true ) {
+        VolTree VHistory = currentTree.Extract();
+        m_report.time_index[VHistory].tTotal += vtime;
+	//std::cout<<"filling "<<m_report.time_index[VHistory].tTotal<<std::endl;
+        if (PDef->GetParticleName() == "neutron")	{ m_report.time_index[VHistory].tNeutron += vtime; }
+        else if (PDef->GetParticleSubType() == "e")	{ m_report.time_index[VHistory].tElectron += vtime; }
+        else if (PDef->GetParticleSubType() == "pi")	{ m_report.time_index[VHistory].tPion += vtime; }
+        else if (PDef->GetParticleType() == "gamma")	{ m_report.time_index[VHistory].tPhoton += vtime; }
+        else if (PDef->GetParticleType() == "baryon")	{ m_report.time_index[VHistory].tBaryon += vtime; }
+        else if (PDef->GetParticleType() == "lepton")	{ m_report.time_index[VHistory].tLepton += vtime; }
+        else if (PDef->GetParticleType() == "meson")	{ m_report.time_index[VHistory].tMeson += vtime; }
+        else						{ m_report.time_index[VHistory].tOther += vtime; }
+        //ATH_MSG_DEBUG("Time stored in "<<VHistory.back().first->GetName());
+        if ( !currentTree.Ascend() )  break;
+      }
+    }
+    
+    // Restart timer
+    v_timer->Start();
+    
+    return;
+
+  }
+
+  
+} // namespace G4UA 
