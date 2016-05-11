90899303457af434dd55fed5d3e6f8414a470a35
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-11 14:51:18 +0200

    share/G4HitFilter.py: update syntax to work for both V1 and V2 UserActions. ATLASSIM-2226. Tagging G4HitFilter-00-00-09 (G4HitFilter-00-00-09)
    	* share/G4HitFilter.py: update syntax to work for both V1 and V2
    	UserActions. ATLASSIM-2226
    	* tagging G4HitFilter-00-00-09
    
    2016-02-11  John Chapman  <John.Chapman@cern.ch>
    	* move headers to src directory.
    	* tagging G4HitFilter-00-00-08
    
    2016-01-11 Andrea Di Simone <andrea.di.simone@cern.ch>
    	* reduced verbosity
    	* tagging G4HitFilter-00-00-07
    
    2016-01-11 Andrea Di Simone <andrea.di.simone@cern.ch>
    	* implemented MT action
    	* tagging G4HitFilter-00-00-06

---
+++ Simulation/G4Extensions/G4HitFilter/python/G4HitFilterConfig.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon import CfgMgr
+from G4AtlasServices import G4AtlasServicesConfig
+
+def getG4HitFilterTool(name="G4UA::G4HitFilterTool", **kwargs):
+    from G4HitFilter.G4HitFilterConf import G4UA__G4HitFilterTool
+    return G4UA__G4HitFilterTool(name, **kwargs)
+
+def addG4HitFilterTool(name="G4UA::G4HitFilterTool",system=False):
+    G4AtlasServicesConfig.addAction(name,['BeginOfRun','EndOfEvent'],system)
--- Simulation/G4Extensions/G4HitFilter/src/G4HitFilter.cxx
+++ Simulation/G4Extensions/G4HitFilter/src/G4HitFilter.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "G4HitFilter/G4HitFilter.h"
+#include "G4HitFilter.h"
 
 #include "G4RunManager.hh"
 #include "G4Event.hh"
@@ -18,16 +18,20 @@
 #include "TileSimEvent/TileHitVector.h"
 #include "LUCID_SimEvent/LUCID_SimHitCollection.h"
 
+#include "StoreGate/ReadHandle.h"
+
 #include <iostream>
 
-G4HitFilter::G4HitFilter(const std::string& type, const std::string& name, const IInterface* parent):UserActionBase(type,name,parent),
+G4HitFilter::G4HitFilter(const std::string& type, const std::string& name, const IInterface* parent):
+  UserActionBase(type,name,parent),
   m_volumenames(),
   m_ntot(0),
   m_npass(0),
-  m_hitContainers(){
+  m_hitContainers()
+{
 
   declareProperty("VolumeNames",m_volumenames);
-  
+
 }
 
 StatusCode G4HitFilter::initialize(){
@@ -69,7 +73,7 @@ StatusCode G4HitFilter::initialize(){
       return StatusCode::FAILURE;
 
     }
-    
+
     m_hitContainers.push_back(std::make_pair(hitType,vol));
 
 
@@ -82,60 +86,60 @@ StatusCode G4HitFilter::initialize(){
 
 void G4HitFilter::EndOfEvent(const G4Event*){
   unsigned int counter = 0;
-  
+
   m_ntot++;
 
   std::vector<std::pair<int, std::string> >::iterator itr = m_hitContainers.begin();
   std::vector<std::pair<int, std::string> >::iterator itr_end = m_hitContainers.end();
   for(;itr!=itr_end;++itr) {
     if((*itr).first == CALOCALIB) {
-      const CaloCalibrationHitContainer *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle<CaloCalibrationHitContainer> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == CSC) {
-      const CSCSimHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< CSCSimHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == LAR) {
-      const LArHitContainer *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< LArHitContainer> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == LUCID) {
-      const LUCID_SimHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< LUCID_SimHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == MDT) {
-      const MDTSimHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< MDTSimHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == RPC) {
-      const RPCSimHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< RPCSimHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == SI) {
-      const SiHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< SiHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == TGC) {
-      const TGCSimHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< TGCSimHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == TILE) {
-      const TileHitVector *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< TileHitVector> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else if((*itr).first == TRT) {
-      const TRTUncompressedHitCollection *cont = 0;
-      if(evtStore()->retrieve(cont, (*itr).second).isFailure() || !cont) continue;
+      SG::ReadHandle< TRTUncompressedHitCollection> cont((*itr).second);
+      if(! cont.isValid()) continue;
       counter += cont->size();
     }
     else {
@@ -149,7 +153,7 @@ void G4HitFilter::EndOfEvent(const G4Event*){
   if (counter==0){
     ATH_MSG_INFO("G4HitFilter: failing the event");
     G4RunManager::GetRunManager()->AbortEvent();
-  } 
+  }
   else {
     ATH_MSG_INFO("G4HitFilter: passing the event");
     m_npass++;
@@ -174,3 +178,149 @@ StatusCode G4HitFilter::queryInterface(const InterfaceID& riid, void** ppvInterf
   }
   return StatusCode::SUCCESS;
 }
+
+
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/IMessageSvc.h"
+
+namespace G4UA{
+
+
+  G4HitFilter::G4HitFilter(const Config& config):
+    AthMessaging(Gaudi::svcLocator()->service< IMessageSvc >( "MessageSvc" ),"G4HitFilter"),
+    m_config(config),m_report(),
+    m_evtStore("StoreGateSvc/StoreGateSvc","G4HitFilter"),
+    m_detStore("StoreGateSvc/DetectorStore","G4HitFilter"){;
+  }
+
+
+  void G4HitFilter::beginOfRun(const G4Run*){;
+
+    if(m_config.volumenames.size()==0){
+
+      ATH_MSG_ERROR("No hit containers declared");
+
+    }
+
+    int hitType = 999;
+
+    for(auto vol: m_config.volumenames){
+      if(vol == "BCMHits") hitType = SI;
+      else if(vol == "BLMHits") hitType = SI;
+      else if(vol == "CSC_Hits") hitType = CSC;
+      else if(vol == "LArCalibrationHitActive") hitType = CALOCALIB;
+      else if(vol == "LArCalibrationHitDeadMaterial") hitType = CALOCALIB;
+      else if(vol == "LArCalibrationHitInactive") hitType = CALOCALIB;
+      else if(vol == "LArHitEMB") hitType = LAR;
+      else if(vol == "LArHitEMEC") hitType = LAR;
+      else if(vol == "LArHitFCAL") hitType = LAR;
+      else if(vol == "LArHitHEC") hitType = LAR;
+      else if(vol == "LucidSimHitsVector") hitType = LUCID;
+      else if(vol == "MBTSHits") hitType = TILE;
+      else if(vol == "MDT_Hits") hitType = MDT;
+      else if(vol == "PixelHits") hitType = SI;
+      else if(vol == "RPC_Hits") hitType = RPC;
+      else if(vol == "SCT_Hits") hitType = SI;
+      else if(vol == "TGC_Hits") hitType = TGC;
+      else if(vol == "TRTUncompressedHits") hitType = TRT;
+      else if(vol == "TileHitVec") hitType = TILE;
+
+
+      if(hitType==999){
+
+        ATH_MSG_ERROR("unknown hit tipe"<<vol);
+
+      }
+
+      m_hitContainers.push_back(std::make_pair(hitType,vol));
+
+    }
+
+
+
+  }
+
+
+  void G4HitFilter::endOfEvent(const G4Event*){;
+    unsigned int counter = 0;
+
+    m_report.ntot++;
+
+    std::vector<std::pair<int, std::string> >::iterator itr = m_hitContainers.begin();
+    std::vector<std::pair<int, std::string> >::iterator itr_end = m_hitContainers.end();
+
+    for(;itr!=itr_end;++itr) {
+
+      if((*itr).first == CALOCALIB) {
+        SG::ReadHandle<CaloCalibrationHitContainer> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == CSC) {
+        SG::ReadHandle<CSCSimHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == LAR) {
+        SG::ReadHandle<LArHitContainer> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == LUCID) {
+        SG::ReadHandle<LUCID_SimHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == MDT) {
+        SG::ReadHandle<MDTSimHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == RPC) {
+        SG::ReadHandle<RPCSimHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == SI) {
+        SG::ReadHandle<SiHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == TGC) {
+        SG::ReadHandle<TGCSimHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == TILE) {
+        SG::ReadHandle<TileHitVector> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else if((*itr).first == TRT) {
+        SG::ReadHandle<TRTUncompressedHitCollection> cont((*itr).second);
+        if(! cont.isValid()) continue;
+        counter += cont->size();
+      }
+      else {
+        continue;
+      }
+
+      // Require at least one hit from one selected detector volume.
+      if(counter>0) break;
+    }
+
+    if (counter==0){
+      ATH_MSG_INFO("G4HitFilter: failing the event");
+      G4RunManager::GetRunManager()->AbortEvent();
+    }
+    else {
+      ATH_MSG_INFO("G4HitFilter: passing the event");
+
+      m_report.npass++;
+
+    }
+
+  }
+
+} // namespace G4UA
