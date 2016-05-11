19a20920a9dcc046752c234b961e1fadde8357fb
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-11 16:18:01 +0200

    Remove G4AtlasControl/FieldMenu.h, src/FieldMenu.cxx - obsolete code. G4AtlasControl/SimControl.h, G4AtlasControl/selection.xml, G4AtlasControl/G4AtlasControlDict.h, doc/mainpage.h, src/SimControl.cxx - remove all references to FieldMenu. cmt/requirements, CMakeLists.txt - drop dependencies on FadsField and G4Field packages. ATLASSIM-2373. Tagging: G4AtlasControl-00-06-00 (G4AtlasControl-00-06-00)
    2016-05-11  John Chapman  <John.Chapman@cern.ch>
    
    	* Remove G4AtlasControl/FieldMenu.h, src/FieldMenu.cxx - obsolete
    	code. ATLASSIM-2373
    	* G4AtlasControl/SimControl.h, G4AtlasControl/selection.xml,
    	G4AtlasControl/G4AtlasControlDict.h, doc/mainpage.h,
    	src/SimControl.cxx - remove all references to
    	FieldMenu. ATLASSIM-2373
    	* cmt/requirements, CMakeLists.txt - drop dependencies on
    	FadsField and G4Field packages. ATLASSIM-2373
    	* Tagging: G4AtlasControl-00-06-00
    
    2016-05-05  John Chapman  <John.Chapman@cern.ch>
    
    	* Remove G4AtlasControl/FieldIntegrationMenu.h,
    	src/FieldIntegrationMenu.cxx - obsolete code. ATLASSIM-2782
    	* G4AtlasControl/SimControl.h, G4AtlasControl/selection.xml,
    	G4AtlasControl/G4AtlasControlDict.h, src/SimControl.cxx - remove
    	all references to FieldIntegrationMenu. ATLASSIM-2782
    	* cmt/requirements, CMakeLists.txt - move dependency on FadsField
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 3afe85d12be8724e9f2c64353fc35f0a598ff0cf

--- Simulation/G4Atlas/G4AtlasControl/G4AtlasControl/G4AtlasControlDict.h
+++ Simulation/G4Atlas/G4AtlasControl/G4AtlasControl/G4AtlasControlDict.h
@@ -3,9 +3,6 @@
 */
 
 #include "G4AtlasControl/SimControl.h"
-#include "G4AtlasControl/FieldMenu.h"
-#include "G4AtlasControl/FieldIntegrationMenu.h"
-#include "G4AtlasControl/PhysicsMenu.h"
 #include "G4AtlasControl/MCTruthMenu.h"
 #include "G4AtlasControl/G4CommandInterface.h"
 #include "G4AtlasControl/ParticleDataModifier.h"
--- Simulation/G4Atlas/G4AtlasControl/doc/mainpage.h
+++ Simulation/G4Atlas/G4AtlasControl/doc/mainpage.h
@@ -8,26 +8,15 @@
 
 @section G4AtlasControl Introduction
 
-This package includes all the control interfaces from the python layer into the C++ layer of the simulation.  All the menus available to the user trhough the AtlasG4Engine are defined here (and are applied in G4AtlasApps).  
+This package includes all the control interfaces from the python layer into the C++ layer of the simulation.  All the menus available to the user trhough the AtlasG4Engine are defined here (and are applied in G4AtlasApps).
 
 @section G4AtlasControl Class Overview
 
 The classes in this package are:
 
- - ActionMenu : Controls the addition of user actions (user code that is applied before a run or event or after a run, event, or step).
- - ColorFacade : An interface for retrieving r/g/b colors
- - DataCardSvc : Best not to ask what this does
- - ElementFacade : An interface for dealing with G4Elements
- - FieldMenu : An interface to the fields applied during simulation
  - G4CommandInterface : An interface to the G4 command line (available from Python)
- - GenericDetectorFacility : An interface to the FadsDetectorFacility available from python
- - GeometryMenu : An interface to basic geometry functions in Geant4
- - MaterialFacade : An interface for dealing with G4Materials
  - MCTruthMenu : An interface for adding or applying truth strategies during the simulation
  - ParticleDataModifier : An interface for modifying particle properties during the simulation (charge, mass, etc)
- - PhysicsMenu : An interface for modifying the physics list to be used during G4 simulation
- - PhysicsRegionProxy : An interface for dealing with physics regions in G4
- - RandomNrMenu : An interface for dealing with random numbers during the simulation
  - SimControl : One interface to rule them all
 
 @ref used_G4AtlasControl
