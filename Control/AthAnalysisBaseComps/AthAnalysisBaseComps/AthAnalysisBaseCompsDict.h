1df8d2f73f3a808cd84360c367baee8b5dcf0cb1
Will Buttinger <William.Buttinger@cern.ch>
2016-05-11 22:07:57 +0200

    'addPropertyToCatalogue method' (AthAnalysisBaseComps-00-00-18)
    	* AthAnalysisHelper: methods for creating and setting properties on algorithms
    	* Tagging as AthAnalysisBaseComps-00-00-16
    	* made ToolSvc the default parent for created tools
    	* Tagging as AthAnalysisBaseComps-00-00-17
    	* One more helper method for adding properties to catalogue
    	* Tagging as AthAnalysisBaseComps-00-00-18
    
    2016-05-08 Will Buttinger <Will@cern.ch>
          * AthAnalysisHelper: printAuxElement method for printing content of an aux element
          * Tagging as AthAnalysisBaseComps-00-00-15
    
    2016-05-03 Will Buttinger <will@cern.ch>
          * AthAnalysisHelper: Use const references instead of r-value references.
                               Added specialization of setProperty for toolHandles + arrays
          * Tagging as AthAnalysisBaseComps-00-00-14
    
    2016-04-07 Will Buttinger <will@cern.ch>
            * AthAnalysisHelper: createTool method for simplifying call to gaudi factory,
                                 corresponding setProperty method for bare tools
                                 setProperty method for services too
    ...
    (Long ChangeLog diff - truncated)

---
+++ Control/AthAnalysisBaseComps/AthAnalysisBaseComps/AthAnalysisBaseCompsDict.h
@@ -0,0 +1,11 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef ATHANALYSISBASECOMPSDICT_H
+#define ATHANALYSISBASECOMPSDICT_H 
+
+#include "AthAnalysisBaseComps/AthAnalysisHelper.h"
+
+
+#endif 
