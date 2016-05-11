8fe0288fda014459481c73a9ccd30114bbd4eb9c
Benjamin William Allen <benjamin.william.allen@cern.ch>
2016-05-11 12:25:05 +0200

    Fix L1CALO3 (DerivationFrameworkL1Calo-00-00-22)
    Former-commit-id: 9a3dd884ad0b786b3fdb024c1c74925a5d3b3f3b

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkL1Calo/DerivationFrameworkL1Calo/SaturatedTriggerTower.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// SaturatedTriggerTower.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef DERIVATIONFRAMEWORK_SATURATEDTRIGGERTOWER_H
+#define DERIVATIONFRAMEWORK_SATURATEDTRIGGERTOWER_H
+
+#include <string>
+
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "DerivationFrameworkInterfaces/ISkimmingTool.h"
+
+namespace DerivationFramework {
+
+  class SaturatedTriggerTower : public AthAlgTool, public ISkimmingTool {
+    public: 
+      SaturatedTriggerTower(const std::string& t, const std::string& n, const IInterface* p);
+
+      StatusCode initialize();
+      StatusCode finalize();
+      virtual bool eventPassesFilter() const;
+
+    private:
+      int m_adcThreshold;
+      std::string m_collName;
+  }; 
+}
+
+#endif // DERIVATIONFRAMEWORK_SATURATEDTRIGGERTOWER_H
---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkL1Calo/share/disableInDetMuons.py
@@ -0,0 +1,19 @@
+
+jp.Rec.doMuon.set_Value_and_Lock(False)
+jp.Rec.doInDet.set_Value_and_Lock(False)
+jp.Rec.doLucid.set_Value_and_Lock(False)
+jp.Rec.doZdc.set_Value_and_Lock(False)
+jp.Rec.doTagRawSummary.set_Value_and_Lock(False)
+
+import JetRec.JetRecFlags
+import tauRec.tauRecFlags
+import CaloRec.CaloRecFlags
+import RecExConfig.RecAlgsFlags
+jp.Rec.doEgamma.set_Value_and_Lock(False)
+jp.JetRecFlags.Enabled.set_Value_and_Lock(False)
+jp.tauRecFlags.Enabled.set_Value_and_Lock(False)
+jp.CaloRecFlags.doCaloCluster.set_Value_and_Lock(False)
+jp.Rec.RecAlgs.doEFlow.set_Value_and_Lock(False)
+jp.Rec.RecAlgs.doEFlowJet.set_Value_and_Lock(False)
+jp.Rec.RecAlgs.doMissingET.set_Value_and_Lock(False)
+
