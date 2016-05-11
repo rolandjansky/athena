9ee312a5c66cb2378aeeb6cc2848b8821bfebce1
Will Buttinger <William.Buttinger@cern.ch>
2016-05-11 20:50:59 +0200

    'added dictionary for interface classes' (DerivationFrameworkInterfaces-00-00-03)
    	* Added dictionary for interface classes - so can use on root prompt/pyroot
    	* Tagged as DerivationFrameworkInterfaces-00-00-03
    
    
    Former-commit-id: b294445a8757ae59b786c24ba13aaeb7cba4bbb8

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkInterfaces/DerivationFrameworkInterfaces/DerivationFrameworkInterfacesDict.h
@@ -0,0 +1,13 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef DFINTERFACES_DICT
+#define DFINTERFACES_DICT
+
+#include "DerivationFrameworkInterfaces/IAugmentationTool.h"
+#include "DerivationFrameworkInterfaces/IThinningTool.h"
+#include "DerivationFrameworkInterfaces/ISkimmingTool.h"
+
+
+#endif
