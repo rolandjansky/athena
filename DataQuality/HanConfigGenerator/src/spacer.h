77f99b061f1ece48bb219f376387fed3efce1980
Mark Sutton <mark.sutton@cern.ch>
2016-05-14 10:40:04 +0200

    STUPID CMAKE SVN COMMIT BLOCK (HanConfigGenerator-00-00-18)
    Former-commit-id: e4abbb5e39f7ae9ece652291deb1b1ecadf7ebb8

---
+++ DataQuality/HanConfigGenerator/src/spacer.h
@@ -0,0 +1,37 @@
+/**************************************************************************
+ **
+ **   @file         spacer.h  
+ **
+ **   Description:    
+ **                   
+ **                   
+ ** 
+ **   Author:       M.Sutton  
+ **
+ **   Created:      Thu 21 Apr 2016 21:34:47 CEST
+ **   Modified:     
+ **                   
+ **                   
+ **
+ **************************************************************************/ 
+
+
+#ifndef   SPACER_H
+#define   SPACER_H
+
+#include "string.h"
+
+const std::string spacer = "\t";
+
+
+#endif  /* SPACER_H */
+
+
+
+
+
+
+
+
+
+
