8eb875be61d34bdb214b20f2b9bf07ed66e40b87
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-18 10:52:37 +0200

    Add G4AtlasInterfaces/IEquationOfMotionTool.h - provides the makeEquationOfMotion method. ATLASSIM-2794. Tagging: G4AtlasInterfaces-00-00-38 (G4AtlasInterfaces-00-00-38)
    	* add G4AtlasInterfaces/IEquationOfMotionTool.h - provides the
    	makeEquationOfMotion method. ATLASSIM-2794
    	* Tagging: G4AtlasInterfaces-00-00-38
    
    2016-05-14 Steve Farrell <Steven.Farrell@cern.ch>
    	* Merging G4AtlasInterfaces-00-00-36-branch
    	* Added initializeField method to IFieldManagerTool.
    	* Added initializeFields method to IDetectorGeometrySvc.
    	* Cleanup of field interfaces, documentation, method naming
    	  conventions.
    	* Tagging: G4AtlasInterfaces-00-00-37
    
    2016-04-28  ADA <dellacqu@mail.cern.ch>
    	* add G4AtlasInterfaces/IG4FieldSvc.h - new field
    	interface. ATLASSIM-2373
    	* remove G4AtlasInterfaces/IFieldTool.h - no longer
    	used. ATLASSIM-2373
    	* G4AtlasInterfaces/IFieldManagerTool.h - no longer inherits from
    	G4FieldManager. ATLASSIM-2373
    	* Tagging as G4AtlasInterfaces-00-00-36
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 4259d0307a7cd94835d5fcce78416c830e324f20

---
+++ Simulation/G4Atlas/G4AtlasInterfaces/G4AtlasInterfaces/IG4FieldSvc.h
@@ -0,0 +1,38 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef G4AtlasInterfaces_IG4FieldSvc_H
+#define G4AtlasInterfaces_IG4FieldSvc_H
+
+// Include files
+// from Gaudi
+#include "GaudiKernel/IService.h"
+
+class G4MagneticField;
+
+/** @class IG4FieldSvc IG4FieldSvc.h "G4AtlasInterfaces/IG4FieldSvc.h"
+ *
+ *  Abstract interface to a field service.
+ *
+ *  @author ADA
+ *  @date   2015-11-17
+ */
+
+class IG4FieldSvc : virtual public IService
+{
+ public:
+
+  IG4FieldSvc() {;}
+  virtual ~IG4FieldSvc() {;}
+
+  static const InterfaceID& interfaceID() { 
+    static const InterfaceID IID_IG4FieldSvc( "IG4FieldSvc", 1, 0 );
+    return IID_IG4FieldSvc;
+  }
+
+  virtual G4MagneticField* getField( ) = 0;
+
+};
+
+#endif
--- Simulation/G4Atlas/G4AtlasInterfaces/G4AtlasInterfaces/IUserActionSvc.h
+++ Simulation/G4Atlas/G4AtlasInterfaces/G4AtlasInterfaces/IUserActionSvc.h
@@ -48,3 +48,38 @@ class IUserActionSvc : virtual public IService {
 };
 
 #endif
+
+#ifndef G4ATLASINTERFACES_G4UA__IUSERACTIONSVC_H
+#define G4ATLASINTERFACES_G4UA__IUSERACTIONSVC_H
+
+// Framework includes
+#include "GaudiKernel/IService.h"
+
+namespace G4UA
+{
+
+  /// @class IUserActionSvc
+  /// @brief Abstract interface for the UserActionSvc
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  class IUserActionSvc : virtual public IService
+  {
+
+  public:
+
+    /// @brief Initialize all the user actions for the current thread.
+    virtual StatusCode initializeActions() = 0;
+
+    /// Interface declaration
+    static const InterfaceID& interfaceID()
+    {
+      static const InterfaceID iid_IUASvc("G4UA::IUserActionSvc", 1, 0);
+      return iid_IUASvc;
+    }
+
+  }; // class IUserActionSvc
+
+} // namespace G4UA
+
+#endif
