9fb58c81411069a98439ce91d90c272c4d802026
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-20 10:58:56 +0200

    Adding xAODMaker::ElementLinkResetAlg (xAODCoreCnv-00-00-07-01)
    	* Created a new algorithm (xAODMaker::ElementLinkResetAlg) that
    	  can reset the caches of all ElementLink objects stored as auxiliary
    	  variables. To make sure for instance that after AODFix all the
    	  links would be functional.
    	* Had to add a dependency on AthLinks to achieve this.
    	* Had to add a CMakeLists.txt file to the branch, otherwise SVN
    	  wouldn't let me modify the requirements file.
    	* Tagging as xAODCoreCnv-00-00-07-01

--- Event/xAOD/xAODCoreCnv/src/components/xAODCoreCnv_entries.cxx
+++ Event/xAOD/xAODCoreCnv/src/components/xAODCoreCnv_entries.cxx
@@ -1,15 +1,18 @@
-// $Id: xAODCoreCnv_entries.cxx 583869 2014-02-18 11:31:09Z krasznaa $
+// $Id: xAODCoreCnv_entries.cxx 748595 2016-05-20 10:57:20Z krasznaa $
 
 // Gaudi/Athena include(s):
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Local include(s):
 #include "../AuxStoreWrapper.h"
+#include "../ElementLinkResetAlg.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, AuxStoreWrapper )
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, ElementLinkResetAlg )
 
-DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
+DECLARE_FACTORY_ENTRIES( xAODCoreCnv ) {
 
    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, AuxStoreWrapper )
+   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, ElementLinkResetAlg )
 
 }
