397d8a7cf8a909244cddcbb3f955f4f4189f05bb
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-05-26 13:28:12 +0200

    src/CharginosPhysicsTool.h, src/CharginosPhysicsTool.cxx - initialize pointer member variables in the constructor. ATLASSIM-2846. Rename member variables to conform to ATLAS naming conventions. Tagging: Charginos-00-01-01 (Charginos-00-01-01)
    	* src/CharginosPhysicsTool.h, src/CharginosPhysicsTool.cxx -
    	intialize pointer member variables in the
    	constructor. ATLASSIM-2846. Rename member variables to conform to
    	ATLAS naming conventions.
    	* Tagging: Charginos-00-01-01
    
    
    Former-commit-id: 225bed39b384e2a6cd69d7ce2ba959d9880c9eda

--- Simulation/G4Extensions/Charginos/src/CharginosPhysicsTool.cxx
+++ Simulation/G4Extensions/Charginos/src/CharginosPhysicsTool.cxx
@@ -36,35 +36,38 @@
 // Standard constructor, initializes variables
 //=============================================================================
 CharginosPhysicsTool::CharginosPhysicsTool( const std::string& type,
-                                            const std::string& nam,const IInterface* parent )
-  : AthAlgTool ( type, nam , parent )
+                                            const std::string& name, const IInterface* parent )
+  : AthAlgTool ( type, name , parent )
+  , m_theCharginoMinus(nullptr)
+  , m_theCharginoPlus(nullptr)
+  , m_theNeutralino(nullptr)
 {
-  ATH_MSG_DEBUG("CharginosPhysicsTool "<<type<<" "<<nam);
+  ATH_MSG_DEBUG("CharginosPhysicsTool "<<type<<" "<<name);
   declareInterface< IPhysicsOptionTool >( this ) ;
 
-  declareProperty("CharginoPlusMass",CharginoPlusMass= 101.0*CLHEP::GeV,"CharginoPlus Mass");
-  declareProperty("CharginoPlusWidth",CharginoPlusWidth=0.0*CLHEP::MeV,"CharginoPlus Width");
-  declareProperty("CharginoPlusCharge",CharginoPlusCharge=+1.*CLHEP::eplus,"CharginoPlus charge");
-  declareProperty("CharginoPlusPDGCode",CharginoPlusPDGCode=1000024,"CharginoPlus PDG CODE");
-  declareProperty("CharginoPlusStable",CharginoPlusStable=true,"CharginoPlus Stable");
-  declareProperty("CharginoPlusLifetime",CharginoPlusLifetime=-1,"CharginoPlus Lifetime");
-  declareProperty("CharginoPlusShortlived",CharginoPlusShortlived=false,"CharginoPlus Shortlived");
+  declareProperty("CharginoPlusMass",m_CharginoPlusMass= 101.0*CLHEP::GeV,"CharginoPlus Mass");
+  declareProperty("CharginoPlusWidth",m_CharginoPlusWidth=0.0*CLHEP::MeV,"CharginoPlus Width");
+  declareProperty("CharginoPlusCharge",m_CharginoPlusCharge=+1.*CLHEP::eplus,"CharginoPlus charge");
+  declareProperty("CharginoPlusPDGCode",m_CharginoPlusPDGCode=1000024,"CharginoPlus PDG CODE");
+  declareProperty("CharginoPlusStable",m_CharginoPlusStable=true,"CharginoPlus Stable");
+  declareProperty("CharginoPlusLifetime",m_CharginoPlusLifetime=-1,"CharginoPlus Lifetime");
+  declareProperty("CharginoPlusShortlived",m_CharginoPlusShortlived=false,"CharginoPlus Shortlived");
 
-  declareProperty("CharginoMinusMass",CharginoMinusMass= 101.0*CLHEP::GeV,"CharginoMinus Mass");
-  declareProperty("CharginoMinusWidth",CharginoMinusWidth=0.0*CLHEP::MeV,"CharginoMinus Width");
-  declareProperty("CharginoMinusCharge",CharginoMinusCharge=-1.*CLHEP::eplus,"CharginoMinus charge");
-  declareProperty("CharginoMinusPDGCode",CharginoMinusPDGCode=-1000024,"CharginoMinus PDG CODE");
-  declareProperty("CharginoMinusStable",CharginoMinusStable=true,"CharginoMinus Stable");
-  declareProperty("CharginoMinusLifetime",CharginoMinusLifetime=-1,"CharginoMinus Lifetime");
-  declareProperty("CharginoMinusShortlived",CharginoMinusShortlived=false,"CharginoMinus Shortlived");
+  declareProperty("CharginoMinusMass",m_CharginoMinusMass= 101.0*CLHEP::GeV,"CharginoMinus Mass");
+  declareProperty("CharginoMinusWidth",m_CharginoMinusWidth=0.0*CLHEP::MeV,"CharginoMinus Width");
+  declareProperty("CharginoMinusCharge",m_CharginoMinusCharge=-1.*CLHEP::eplus,"CharginoMinus charge");
+  declareProperty("CharginoMinusPDGCode",m_CharginoMinusPDGCode=-1000024,"CharginoMinus PDG CODE");
+  declareProperty("CharginoMinusStable",m_CharginoMinusStable=true,"CharginoMinus Stable");
+  declareProperty("CharginoMinusLifetime",m_CharginoMinusLifetime=-1,"CharginoMinus Lifetime");
+  declareProperty("CharginoMinusShortlived",m_CharginoMinusShortlived=false,"CharginoMinus Shortlived");
 
-  declareProperty("NeutralinoMass",NeutralinoMass= 100.0*CLHEP::GeV,"Neutralino Mass");
-  declareProperty("NeutralinoWidth",NeutralinoWidth=0.0*CLHEP::MeV,"Neutralino Width");
-  declareProperty("NeutralinoCharge",NeutralinoCharge=0.*CLHEP::eplus,"Neutralino charge");
-  declareProperty("NeutralinoPDGCode",NeutralinoPDGCode=1000022,"Neutralino PDG CODE");
-  declareProperty("NeutralinoStable",NeutralinoStable=true,"Neutralino Stable");
-  declareProperty("NeutralinoLifetime",NeutralinoLifetime=-1,"Neutralino Lifetime");
-  declareProperty("NeutralinoShortlived",NeutralinoShortlived=false,"Neutralino Shortlived");
+  declareProperty("NeutralinoMass",m_NeutralinoMass= 100.0*CLHEP::GeV,"Neutralino Mass");
+  declareProperty("NeutralinoWidth",m_NeutralinoWidth=0.0*CLHEP::MeV,"Neutralino Width");
+  declareProperty("NeutralinoCharge",m_NeutralinoCharge=0.*CLHEP::eplus,"Neutralino charge");
+  declareProperty("NeutralinoPDGCode",m_NeutralinoPDGCode=1000022,"Neutralino PDG CODE");
+  declareProperty("NeutralinoStable",m_NeutralinoStable=true,"Neutralino Stable");
+  declareProperty("NeutralinoLifetime",m_NeutralinoLifetime=-1,"Neutralino Lifetime");
+  declareProperty("NeutralinoShortlived",m_NeutralinoShortlived=false,"Neutralino Shortlived");
 
 }
 
@@ -83,7 +86,7 @@ CharginosPhysicsTool::~CharginosPhysicsTool()
 StatusCode CharginosPhysicsTool::initialize( )
 {
   ATH_MSG_DEBUG("CharginosPhysicsTool::initialize( )");
-  this->SetPhysicsName(name());
+  this->SetPhysicsName(this->name());
   return StatusCode::SUCCESS;
 }
 
@@ -96,11 +99,11 @@ void CharginosPhysicsTool::ConstructParticle()
 {
   ATH_MSG_DEBUG(" ConstructParticle for the Charginos being run");
 
-  theCharginoPlus = AMSBCharginoPlus::Definition(CharginoPlusMass, CharginoPlusWidth, CharginoPlusCharge, CharginoPlusPDGCode, CharginoPlusStable, CharginoPlusLifetime, CharginoPlusShortlived );
+  m_theCharginoPlus = AMSBCharginoPlus::Definition(m_CharginoPlusMass, m_CharginoPlusWidth, m_CharginoPlusCharge, m_CharginoPlusPDGCode, m_CharginoPlusStable, m_CharginoPlusLifetime, m_CharginoPlusShortlived );
 
-  theCharginoMinus = AMSBCharginoMinus::Definition(CharginoMinusMass, CharginoMinusWidth, CharginoMinusCharge, CharginoMinusPDGCode, CharginoMinusStable, CharginoMinusLifetime, CharginoMinusShortlived );
+  m_theCharginoMinus = AMSBCharginoMinus::Definition(m_CharginoMinusMass, m_CharginoMinusWidth, m_CharginoMinusCharge, m_CharginoMinusPDGCode, m_CharginoMinusStable, m_CharginoMinusLifetime, m_CharginoMinusShortlived );
 
-  theNeutralino = AMSBNeutralino::Definition(NeutralinoMass, NeutralinoWidth, NeutralinoCharge, NeutralinoPDGCode, NeutralinoStable, NeutralinoLifetime, NeutralinoShortlived );
+  m_theNeutralino = AMSBNeutralino::Definition(m_NeutralinoMass, m_NeutralinoWidth, m_NeutralinoCharge, m_NeutralinoPDGCode, m_NeutralinoStable, m_NeutralinoLifetime, m_NeutralinoShortlived );
 
 }
 
@@ -111,8 +114,8 @@ void CharginosPhysicsTool::ConstructProcess()
   G4hhIonisation *theIonisation = new G4hhIonisation;
   G4Transportation *theTransportation=new G4Transportation;
 
-  G4ProcessManager *charginoPlus= theCharginoPlus->GetProcessManager();
-  G4ProcessManager *charginoMinus=theCharginoMinus->GetProcessManager();
+  G4ProcessManager *charginoPlus = m_theCharginoPlus->GetProcessManager();
+  G4ProcessManager *charginoMinus = m_theCharginoMinus->GetProcessManager();
 
   charginoPlus->AddProcess(theTransportation);
   charginoMinus->AddProcess(theTransportation);
