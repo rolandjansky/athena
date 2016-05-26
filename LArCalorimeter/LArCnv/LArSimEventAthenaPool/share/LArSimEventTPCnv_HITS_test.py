592934b56b967036b0dfd81d78d49e063dc1a991
Scott Snyder <scott.snyder@cern.ch>
2016-05-26 18:56:01 +0200

    'Do not fail if we cannot find run_tpcnv_test.' (LArSimEventAthenaPool-00-00-03)
    	* Tagging LArSimEventAthenaPool-00-00-03.
    	* CMakeLists.txt: Do not fail if we cannot find run_tpcnv_test.
    	* Tagging LArSimEventAthenaPool-00-00-02.
    	* Get tests running in cmake builds.
    
    2016-03-07  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArSimEventAthenaPool-00-00-01.
    	* Add regression test for reading old data file.
    
    	* tagging LArSimEventAthenaPool-00-00-00
    	(requested by John Chapman)
    
    
    Former-commit-id: 3dd2836125c1a5bd259d170d333d8e90dbfcff27

---
+++ LArCalorimeter/LArCnv/LArSimEventAthenaPool/share/LArSimEventTPCnv_HITS_test.py
@@ -0,0 +1,20 @@
+infile = 'rtt:valid1.110401.PowhegPythia_P2012_ttbar_nonallhad.simul.HITS.e3099_s2578_tid04919495_00/HITS.04919495._000416.pool.root.1'
+keys = [
+    #LArHitContainer_p2
+    'LArHitHEC@100',
+    'LArHitFCAL@100',
+    'LArHitEMB@100',
+    'LArHitEMEC@100',
+         ]
+
+#from AthenaCommon.JobProperties import jobproperties
+#jobproperties.Global.DetDescrVersion = 'ATLAS-GEO-10-00-00'
+
+theApp.EvtMax = 10
+
+from AthenaCommon.DetFlags      import DetFlags
+DetFlags.detdescr.Calo_setOn()
+noMuon = True
+noID = True
+include ('AthenaPoolUtilities/TPCnvTest.py')
+
