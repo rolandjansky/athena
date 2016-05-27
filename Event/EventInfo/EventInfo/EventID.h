123c2746ab52e56f94fa7213a626e99059e78e92
Scott Snyder <scott.snyder@cern.ch>
2016-05-27 17:58:47 +0200

    'Update test to work with both old and new Gaudi versions (as we are still building against both).' (EventInfo-00-11-02-10)
    	* Tagging EventInfo-00-11-02-10.
    	* test/MergedEventInfo_test.cxx: Update to work with both old and
    	new Gaudi versions (as we are still building against both).
    
    2016-05-05  scott snyder  <snyder@bnl.gov>
    
    	* Tagging EventInfo-00-11-02-09.
    	* test/MergedEventInfo_test.cxx: Fix default run number comparison
    	for Gaudi v27.
    
    2016-03-03  Charles Leggett
    	* EventID: remove unnecessary comparison op declarations
    	* Tag EventInfo-00-11-02-08
    
    M       EventInfo/EventID.h
    
    2016-03-03  Charles Leggett
    	* EventID: base on GaudiKernel/EventIDBase
    	* Tag EventInfo-00-11-02-07
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 79849f36fc725e8ad7c81ce7b62328dbd7b1d0d1

--- Event/EventInfo/EventInfo/EventID.h
+++ Event/EventInfo/EventInfo/EventID.h
@@ -22,6 +22,7 @@
 #include <iostream>
 #include <stdint.h>
 
+#include "GaudiKernel/EventIDBase.h"
 
 /**
  * @class EventID
@@ -31,266 +32,137 @@
  *
  */
 
-class EventID {
+class EventID : public EventIDBase {
 public:
 
-    typedef unsigned int       number_type;
+  typedef EventIDBase::number_type       number_type;
   
-    /// \name structors
-    //@{
-    EventID();
-    EventID(number_type run_number, 
-            uint64_t event_number);
-    EventID(number_type run_number, 
-            uint64_t event_number,
-            number_type time_stamp);
-    EventID(number_type run_number, 
-            uint64_t event_number,
-            number_type time_stamp,
-            number_type time_stamp_ns_offset,
-            number_type lumi_block,
-            number_type bunch_crossing_id);
-    EventID(number_type run_number, 
-            uint64_t event_number,
-            number_type time_stamp,
-            number_type time_stamp_ns_offset,
-            number_type lumi_block,
-            number_type bunch_crossing_id,
-            number_type detector_mask0,
-            number_type detector_mask1,
-            number_type detector_mask2 = 0,
-            number_type detector_mask3 = 0);
-    // Use default copy constructor.
-    virtual ~EventID();
-    //@}
+  /// \name structors
+  //@{
+  EventID();
+  EventID(number_type run_number, 
+          uint64_t event_number);
+  EventID(number_type run_number, 
+          uint64_t event_number,
+          number_type time_stamp);
+  EventID(number_type run_number, 
+          uint64_t event_number,
+          number_type time_stamp,
+          number_type time_stamp_ns_offset,
+          number_type lumi_block,
+          number_type bunch_crossing_id);
+
+  EventID(number_type run_number, 
+          uint64_t event_number,
+          number_type time_stamp,
+          number_type time_stamp_ns_offset,
+          number_type lumi_block,
+          number_type bunch_crossing_id,
+          number_type detector_mask0,
+          number_type detector_mask1,
+          number_type detector_mask2 = 0,
+          number_type detector_mask3 = 0);
+  // Use default copy constructor.
+  virtual ~EventID();
+  //@}
   
-    /// run number - 32 bit unsigned
-    number_type   run_number           (void) const;
+  /// detector mask0 - bit field indicating which TTC zones have been built into the event, one
+  /// bit per zone, 32 bit unsigned
+  number_type   detector_mask0       (void) const;
 
-    /// event number - 64 bit unsigned
-    uint64_t      event_number         (void) const;
+  /// detector mask1 - bit field indicating which TTC zones have been built into the event, one
+  /// bit per zone, 32 bit unsigned
+  number_type   detector_mask1       (void) const;
 
-    /// time stamp - posix time in seconds from 1970, 32 bit unsigned
-    number_type   time_stamp           (void) const; 
+  /// detector mask0 - bit field indicating which TTC zones have been built into the event, one
+  /// bit per zone, 32 bit unsigned
+  number_type   detector_mask2       (void) const;
 
-    /// time stamp ns - ns time offset for time_stamp, 32 bit unsigned
-    number_type   time_stamp_ns_offset (void) const; 
+  /// detector mask1 - bit field indicating which TTC zones have been built into the event, one
+  /// bit per zone, 32 bit unsigned
+  number_type   detector_mask3       (void) const;
 
-    /// luminosity block identifier, 32 bit unsigned
-    number_type   lumi_block           (void) const;
+  /// detector mask - as a 64 bit number - only provides access to the combined 64 bits det mask,
+  /// the newer 64 bits, adding up to the 128 bits, must be accessed directly by mask2,3
+  uint64_t      detector_mask        (void) const;
 
-    /// bunch crossing ID,  32 bit unsigned
-    number_type   bunch_crossing_id    (void) const;
 
-    /// detector mask0 - bit field indicating which TTC zones have been built into the event, one
-    /// bit per zone, 32 bit unsigned
-    number_type   detector_mask0       (void) const;
+  /// set detector mask
+  void   set_detector_mask           (number_type detectorMask0, 
+                                      number_type detectorMask1,
+                                      number_type detectorMask2 = 0,
+                                      number_type detectorMask3 = 0);
 
-    /// detector mask1 - bit field indicating which TTC zones have been built into the event, one
-    /// bit per zone, 32 bit unsigned
-    number_type   detector_mask1       (void) const;
-
-    /// detector mask0 - bit field indicating which TTC zones have been built into the event, one
-    /// bit per zone, 32 bit unsigned
-    number_type   detector_mask2       (void) const;
-
-    /// detector mask1 - bit field indicating which TTC zones have been built into the event, one
-    /// bit per zone, 32 bit unsigned
-    number_type   detector_mask3       (void) const;
-
-    /// detector mask - as a 64 bit number - only provides access to the combined 64 bits det mask,
-    /// the newer 64 bits, adding up to the 128 bits, must be accessed directly by mask2,3
-    uint64_t      detector_mask        (void) const;
-
-    /// set run number
-    void   set_run_number              (number_type runNumber);
-
-    /// set event number
-    void   set_event_number            (uint64_t eventNumber);
-
-    /// set time stamp
-    void   set_time_stamp              (number_type timeStamp); 
-
-    /// set time stamp in ns
-    void   set_time_stamp_ns_offset    (number_type timeStampNs); 
-
-    /// set luminosity block identifier
-    void   set_lumi_block              (number_type lumiBlock);
-
-    /// set bunch crossing ID
-    void   set_bunch_crossing_id       (number_type bcid);
-
-    /// set detector mask
-    void   set_detector_mask           (number_type detectorMask0, 
-                                        number_type detectorMask1,
-                                        number_type detectorMask2 = 0,
-                                        number_type detectorMask3 = 0);
-
-    /// Comparison operators
-    friend bool operator<(const EventID& lhs, const EventID& rhs);
-    friend bool operator>(const EventID& lhs, const EventID& rhs);
-    friend bool operator==(const EventID& lhs, const EventID& rhs);
-    friend bool operator!=(const EventID& lhs, const EventID& rhs);
-    friend bool operator<=(const EventID& lhs, const EventID& rhs);
-    friend bool operator>=(const EventID& lhs, const EventID& rhs);
-
-    /// Extraction operators
-    friend std::ostream& operator<<(std::ostream& os, const EventID& rhs);
+  /// Extraction operators
+  friend std::ostream& operator<<(std::ostream& os, const EventID& rhs);
 
 private:
 
-    /// run number
-    number_type   m_run_number;
+  /// detector mask0 - bit field indicating which TTC zones have been
+  /// built into the event, one bit per zone, 32 bit unsigned
+  number_type   m_detector_mask0 {0};
 
-    /// event number
-    uint64_t      m_event_number;
+  /// detector mask1 - bit field indicating which TTC zones have been
+  /// built into the event, one bit per zone, 32 bit unsigned
+  number_type   m_detector_mask1 {0};
 
-    /// posix time in seconds since 1970/01/01
-    number_type   m_time_stamp;
+  /// detector mask2 - additional trigger bits
+  number_type   m_detector_mask2 {0};
 
-    /// time stamp ns - ns time offset for time_stamp, 32 bit unsigned
-    number_type   m_time_stamp_ns_offset; 
-
-    /// luminosity block number: 
-    /// the number which uniquely tags a luminosity block within a run
-    number_type   m_lumiBlock;
-
-    /// bunch crossing ID,  32 bit unsigned
-    number_type   m_bunch_crossing_id;
-
-    /// detector mask0 - bit field indicating which TTC zones have been
-    /// built into the event, one bit per zone, 32 bit unsigned
-    number_type   m_detector_mask0;
-
-    /// detector mask1 - bit field indicating which TTC zones have been
-    /// built into the event, one bit per zone, 32 bit unsigned
-    number_type   m_detector_mask1;
-
-    /// detector mask2 - additional trigger bits
-    number_type   m_detector_mask2;
-
-    /// detector mask3 - additional trigger bits
-    number_type   m_detector_mask3;
+  /// detector mask3 - additional trigger bits
+  number_type   m_detector_mask3 {0};
 };
 
 
 //<<<<<< INLINE PUBLIC FUNCTIONS                                        >>>>>>
 
-inline 
-EventID::number_type 	
-EventID::run_number	(void) const
-{
-    return m_run_number;
-}
-
-inline 
-uint64_t
-EventID::event_number	(void) const
-{
-    return m_event_number;
-}
-
-inline 
-EventID::number_type 	
-EventID::time_stamp	(void) const
-{
-    return m_time_stamp;
-}
-
-inline 
-EventID::number_type 	
-EventID::time_stamp_ns_offset (void) const
-{
-    return m_time_stamp_ns_offset;
-}
-
-inline 
-EventID::number_type 	
-EventID::lumi_block	(void) const
-{
-    return m_lumiBlock;
-}
-
-inline 
-EventID::number_type
-EventID::bunch_crossing_id    (void) const
-{
-    return m_bunch_crossing_id;
-}
 
 inline 
 EventID::number_type
 EventID::detector_mask0       (void) const
 {
-    return m_detector_mask0;
+  return m_detector_mask0;
 }
 
 inline 
 EventID::number_type
 EventID::detector_mask1       (void) const
 {
-    return m_detector_mask1;
+  return m_detector_mask1;
 }
 
 inline 
 EventID::number_type
 EventID::detector_mask2       (void) const
 {
-    return m_detector_mask2;
+  return m_detector_mask2;
 }
 
 inline 
 EventID::number_type
 EventID::detector_mask3       (void) const
 {
-    return m_detector_mask3;
+  return m_detector_mask3;
 }
 
 inline
 uint64_t
 EventID::detector_mask        (void) const
 {
-    uint64_t result = m_detector_mask1;
-    result = result << 32;
-    result |= m_detector_mask0;
-    return (result);
-}
-
-inline bool operator<(const EventID& lhs, const EventID& rhs) {
-  // We are assuming that ALL events will have run and event numbers,
-  // and never just a time stamp.
-  // FIXME: any use for also ordering by lumi-block ?
-  return lhs.m_run_number<rhs.m_run_number ||
-         ( lhs.m_run_number==rhs.m_run_number && 
-           lhs.m_event_number<rhs.m_event_number) ;
-}
-
-inline bool operator==(const EventID& lhs, const EventID& rhs) {
-    // We assume that equality via run/event numbers is sufficient
-  return lhs.m_run_number   == rhs.m_run_number && 
-         lhs.m_event_number == rhs.m_event_number;
-}
-inline bool operator>(const EventID& lhs, const EventID& rhs) {
-  return !( (lhs < rhs) || (lhs == rhs));
-}
-inline bool operator!=(const EventID& lhs, const EventID& rhs) {
-  return !(lhs == rhs);
-}
-inline bool operator<=(const EventID& lhs, const EventID& rhs) {
-  return !(lhs > rhs);
-}
-inline bool operator>=(const EventID& lhs, const EventID& rhs) {
-  return !(lhs < rhs);
+  uint64_t result = m_detector_mask1;
+  result = result << 32;
+  result |= m_detector_mask0;
+  return (result);
 }
 
 inline std::ostream& operator<<(std::ostream& os, const EventID& rhs) {
   os << "[Run,Evt,Lumi,Time,BunchCross,DetMask] = [" 
-     << rhs.m_run_number 
-     << "," << rhs.m_event_number 
-     << "," << rhs.m_lumiBlock
-     << "," << rhs.m_time_stamp 
-     << ":" << rhs.m_time_stamp_ns_offset
-     << "," << rhs.m_bunch_crossing_id
+     << rhs.run_number() 
+     << "," << rhs.event_number()
+     << "," << rhs.lumi_block()
+     << "," << rhs.time_stamp()
+     << ":" << rhs.time_stamp_ns_offset()
+     << "," << rhs.bunch_crossing_id()
      << ",0x" << std::hex << rhs.detector_mask() 
      << ",0x" << std::hex << rhs.detector_mask2()
      << ",0x" << std::hex << rhs.detector_mask3() << std::dec
--- Event/EventInfo/src/EventID.cxx
+++ Event/EventInfo/src/EventID.cxx
@@ -25,71 +25,30 @@
 //<<<<<< PUBLIC FUNCTION DEFINITIONS                                    >>>>>>
 //<<<<<< MEMBER FUNCTION DEFINITIONS                                    >>>>>>
 
+
 EventID::EventID() :
-    m_run_number          (0),
-    m_event_number        (0),
-    m_time_stamp          (0),
-    m_time_stamp_ns_offset(0),
-    m_lumiBlock           (0),
-    m_bunch_crossing_id   (0),
-    m_detector_mask0      (0),
-    m_detector_mask1      (0),
-    m_detector_mask2      (0),
-    m_detector_mask3      (0)
-
+  EventIDBase() 
 {}
 
 EventID::EventID(number_type run_number, 
-		 uint64_t event_number)
-    :
-    m_run_number          (run_number),
-    m_event_number        (event_number),
-    m_time_stamp          (0),
-    m_time_stamp_ns_offset(0),
-    m_lumiBlock           (0),
-    m_bunch_crossing_id   (0),
-    m_detector_mask0      (0),
-    m_detector_mask1      (0),
-    m_detector_mask2      (0),
-    m_detector_mask3      (0)
-
+                 uint64_t event_number) :
+  EventIDBase(run_number, event_number)
 {}
 
 EventID::EventID(number_type run_number, 
-		 uint64_t event_number,
-		 number_type time_stamp)
-    :
-    m_run_number          (run_number),
-    m_event_number        (event_number),
-    m_time_stamp          (time_stamp),
-    m_time_stamp_ns_offset(0),
-    m_lumiBlock           (0),
-    m_bunch_crossing_id   (0),
-    m_detector_mask0      (0),
-    m_detector_mask1      (0),
-    m_detector_mask2      (0),
-    m_detector_mask3      (0)
-
+                 uint64_t event_number,
+                 number_type time_stamp) :
+  EventIDBase(run_number, event_number, time_stamp)
 {}
 
 EventID::EventID(number_type run_number, 
-		 uint64_t event_number,
-		 number_type time_stamp,
+                 uint64_t event_number,
+                 number_type time_stamp,
                  number_type time_stamp_ns_offset,
                  number_type lumi_block,
-                 number_type bunch_crossing_id)
-    :
-    m_run_number           (run_number),
-    m_event_number         (event_number),
-    m_time_stamp           (time_stamp),
-    m_time_stamp_ns_offset (time_stamp_ns_offset),
-    m_lumiBlock            (lumi_block),
-    m_bunch_crossing_id    (bunch_crossing_id),
-    m_detector_mask0       (0),
-    m_detector_mask1       (0),
-    m_detector_mask2       (0),
-    m_detector_mask3       (0)
-
+                 number_type bunch_crossing_id) :
+  EventIDBase(run_number, event_number, time_stamp, time_stamp_ns_offset,
+              lumi_block, bunch_crossing_id)
 {}
 
 
@@ -103,17 +62,13 @@ EventID::EventID(number_type run_number,
                  number_type detector_mask1,
                  number_type detector_mask2,
                  number_type detector_mask3) 
-    :
-    m_run_number           (run_number),
-    m_event_number         (event_number),
-    m_time_stamp           (time_stamp),
-    m_time_stamp_ns_offset (time_stamp_ns_offset),
-    m_lumiBlock            (lumi_block),
-    m_bunch_crossing_id    (bunch_crossing_id),
-    m_detector_mask0       (detector_mask0),
-    m_detector_mask1       (detector_mask1),
-    m_detector_mask2       (detector_mask2),
-    m_detector_mask3       (detector_mask3)
+  :
+  EventIDBase(run_number, event_number, time_stamp, time_stamp_ns_offset,
+              lumi_block, bunch_crossing_id),
+  m_detector_mask0       (detector_mask0),
+  m_detector_mask1       (detector_mask1),
+  m_detector_mask2       (detector_mask2),
+  m_detector_mask3       (detector_mask3)
 {}
 
 
@@ -121,52 +76,16 @@ EventID::~EventID()
 {}
 
 
-void   
-EventID::set_run_number              (number_type runNumber)
-{
-    m_run_number = runNumber;
-}
-
-void
-EventID::set_event_number            (uint64_t eventNumber)
-{
-    m_event_number = eventNumber;
-}
-
-void
-EventID::set_time_stamp              (number_type timeStamp)
-{
-    m_time_stamp = timeStamp;
-}
-
-void
-EventID::set_time_stamp_ns_offset    (number_type timeStampNs)
-{
-    m_time_stamp_ns_offset = timeStampNs;
-}
-
-void
-EventID::set_lumi_block              (number_type lumiBlock)
-{
-    m_lumiBlock = lumiBlock;
-}
-
-
-void
-EventID::set_bunch_crossing_id       (number_type bcid)
-{
-    m_bunch_crossing_id = bcid;
-}
 
 void   
-EventID::set_detector_mask           (number_type detectorMask0, 
-                                      number_type detectorMask1,
-                                      number_type detectorMask2,
-                                      number_type detectorMask3)
+EventID::set_detector_mask (number_type detectorMask0, 
+                            number_type detectorMask1,
+                            number_type detectorMask2,
+                            number_type detectorMask3)
 {
-    m_detector_mask0 = detectorMask0;
-    m_detector_mask1 = detectorMask1;
-    m_detector_mask2 = detectorMask2;
-    m_detector_mask3 = detectorMask3;
+  m_detector_mask0 = detectorMask0;
+  m_detector_mask1 = detectorMask1;
+  m_detector_mask2 = detectorMask2;
+  m_detector_mask3 = detectorMask3;
 }
 
