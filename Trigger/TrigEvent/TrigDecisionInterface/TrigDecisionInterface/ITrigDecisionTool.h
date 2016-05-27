a8f4cf7a42ab30e35854677bbf3d4ae97c39aff2
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-27 13:04:14 +0200

    Declaring an interface library for the CMake build (TrigDecisionInterface-00-01-01)
    	* Creating a new tag that would contain a CMakeLists.txt file.
    	* Now declaring an interface library in the package.
    	* Tagging as TrigDecisionInterface-00-01-01
    
    2014-10-25 Lukas Heinrich <lukas.heinrich@cern.ch>
    	* removed old interface finally since we're moving to xAOD
    	* new AsgTools compatible interface
    
    
    Former-commit-id: 9cef9075e2b45c11b1f834b376a62d642eb00190

--- Trigger/TrigEvent/TrigDecisionInterface/TrigDecisionInterface/ITrigDecisionTool.h
+++ Trigger/TrigEvent/TrigDecisionInterface/TrigDecisionInterface/ITrigDecisionTool.h
@@ -4,7 +4,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: ITrigDecisionTool.h 582206 2014-02-07 16:47:44Z krasznaa $
+// $Id: ITrigDecisionTool.h 624182 2014-10-25 21:44:42Z lheinric $
 #ifndef TRIGDECISIONINTERFACE_ITRIGDECISIONTOOL_H
 #define TRIGDECISIONINTERFACE_ITRIGDECISIONTOOL_H
 
@@ -12,53 +12,8 @@
 #include <string>
 #include <vector>
 
-// Gaudi/Athena include(s):
-#include "GaudiKernel/IAlgTool.h"
-
-/// ID of the ITrigDecisionTool interface
-static const InterfaceID IID_ITrigDecisionTool( "ITrigDecisionTool", 1, 0 );
-
-/**
- * \brief Abstract interface for obsolete version of the TrigDecisionTool.
- * \warning This is obsolete interface! Use the one below (in the Trig namesapce).
- *
- * Unfortunately some monitoring code still seems to use this interface...
- */
-class ITrigDecisionTool : virtual public IAlgTool {
-
-public:
-   /// Function declaring the interface defined by this class
-   static const InterfaceID& interfaceID() { return IID_ITrigDecisionTool; }
-
-   /// @name Deprecated functions
-   /// @{
-
-   virtual bool isPassed( const std::string& chain_name ) const
-      __attribute__ ((deprecated)) = 0;
-   virtual bool isPassedThrough( const std::string& chain ) const
-      __attribute__ ((deprecated)) = 0; 
-   virtual bool isConfigured( const std::string& chain_name ) const
-      __attribute__ ((deprecated)) = 0; 
-   virtual bool isGroupPassed( const std::string& group_name ) const
-      __attribute__ ((deprecated)) = 0; 
-   virtual bool isStreamPassed( const std::string& stream_name ) const
-      __attribute__ ((deprecated)) = 0;
-   virtual int getFullChainPrescaleFactor( const std::string& chain_name ) const
-      __attribute__ ((deprecated)) = 0;  
-   virtual std::vector< std::string > getConfiguredChainNames() const
-      __attribute__ ((deprecated)) = 0;      
-   virtual std::map< std::string, std::vector< std::string > >
-   getConfiguredGroups() const __attribute__ ((deprecated)) = 0;  
-   virtual std::map< std::string, std::vector< std::string > >
-   getConfiguredStreams() const __attribute__ ((deprecated)) = 0;
-
-   /// @}
-
-}; // class ITrigDecisionTool
-
-/// ID of the Trig::ITrigDecisionTool interface
-static const InterfaceID
-IID_Trig_ITrigDecisionTool( "Trig::ITrigDecisionTool", 1, 0 );
+// Framework include(s):
+#include "AsgTools/IAsgTool.h"
 
 namespace Trig {
 
@@ -67,17 +22,13 @@ namespace Trig {
     *
     * It is used by some packages that are not in AtlasTrigger or higher.
     *
-    * $Revision: 582206 $
-    * $Date: 2014-02-07 17:47:44 +0100 (Fri, 07 Feb 2014) $
+    * $Revision: 624182 $
+    * $Date: 2014-10-25 23:44:42 +0200 (Sat, 25 Oct 2014) $
     **/
-   class ITrigDecisionTool : virtual public IAlgTool {
+   class ITrigDecisionTool : virtual public asg::IAsgTool {
+     ASG_TOOL_INTERFACE(ITrigDecisionTool)
 
    public:
-      /// Function declaring the interface defined by this class
-      static const InterfaceID& interfaceID() {
-         return IID_Trig_ITrigDecisionTool;
-      }
-
       /// Get the decision for a trigger chain
       /**
        * @see Trig::TrigDecisionTool::isPassed
