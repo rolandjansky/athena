e495802b3b60338ce835bf276cb13c62c40c105b
Pavol Strizenec <Pavol.Strizenec@cern.ch>
2016-05-29 17:13:31 +0200

    adding LArNoisyROSummary_p4 (LArTPCnv-00-06-07)
            * Creating LArNoisyROSummaryCnv_p4.cxx  with additional MNB information.
            * Creating LArNoisyROSummary_p4.h
            * Creating LArNoisyROSummaryCnv_p4.h
            * Modifying LArTPCnv.cxx to include the new TP convertor.
            * Modifying selection.xml to include the new class and corresponding ID.
            * tag LArTPCnv-00-06-07
    
    
    Former-commit-id: a7826c871df2f313d68e9e2ad50462c2f35d34d2

--- LArCalorimeter/LArCnv/LArTPCnv/LArTPCnv/LArTPCnvDict.h
+++ LArCalorimeter/LArCnv/LArTPCnv/LArTPCnv/LArTPCnvDict.h
@@ -21,6 +21,7 @@
 #include "LArTPCnv/LArNoisyROSummary_p1.h" 
 #include "LArTPCnv/LArNoisyROSummary_p2.h" 
 #include "LArTPCnv/LArNoisyROSummary_p3.h" 
+#include "LArTPCnv/LArNoisyROSummary_p4.h" 
 
 #include "Identifier/HWIdentifier.h"
 
