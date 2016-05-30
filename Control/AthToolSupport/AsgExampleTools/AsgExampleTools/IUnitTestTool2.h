df3cf7034e6070b8167bc95f94fe26cf3c8d32a3
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-30 16:42:19 +0200

    Making the unit test setup private in the CMT configuration (AsgExampleTools-00-00-11)
    	* Fixed the build issue in packages using this one, as pointed
    	  out by Grigori.
    	* Modified the CMakeLists.txt file, just because the requirements
    	  file was modified.
    	* Tagging as AsgExampleTools-00-00-11
    
    2016-05-11 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* added a test for the AnaToolHandle::swap bug reported by Steve
    	  based on the reproducer provided by Will
    	* tagging as AsgExampleTools-00-00-10
    
    2016-05-10 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* fixed the athena unit tests for AnaToolHandle
    	* tagging as AsgExampleTools-00-00-09
    
    2016-05-09 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* added some name checks to the Athena Job-Options unit tests (and
    	  they fail for sub-tools)
    	* going into AsgExampleTools-00-00-09
    
    ...
    (Long ChangeLog diff - truncated)

---
+++ Control/AthToolSupport/AsgExampleTools/AsgExampleTools/IUnitTestTool2.h
@@ -0,0 +1,44 @@
+#ifndef ASG_TOOLS__I_UNIT_TEST_TOOL2_H
+#define ASG_TOOLS__I_UNIT_TEST_TOOL2_H
+
+//        Copyright Iowa State University 2016.
+//                  Author: Nils Krumnack
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Please feel free to contact me (nils.erik.krumnack@cern.ch) for bug
+// reports, feature suggestions, praise and complaints.
+
+
+#include <AsgTools/IAsgTool.h>
+
+namespace asg
+{
+  class IUnitTestTool1;
+
+  /// \brief the interface for \ref UnitTestTool2
+
+  class IUnitTestTool2 : virtual public IAsgTool
+  {
+    // Declare the interface that this class provides
+    ASG_TOOL_INTERFACE( CP::IUnitTestTool2 )
+
+    /// \brief get the tool from the regular ToolHandle
+  public:
+    virtual IUnitTestTool1 *
+    getToolHandle (const std::string& handleName) const = 0;
+
+    /// \brief call retrieve on the regular ToolHandle
+  public:
+    virtual StatusCode
+    retrieveToolHandle (const std::string& handleName) const = 0;
+
+    /// \brief whether the AnaToolHandle was user configured
+  public:
+    virtual bool
+    wasUserConfigured (const std::string& handleName) const = 0;
+  };
+}
+
+#endif
---
+++ Control/AthToolSupport/AsgExampleTools/Root/UnitTestTool1A.cxx
@@ -0,0 +1,73 @@
+//        Copyright Iowa State University 2016.
+//                  Author: Nils Krumnack
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Please feel free to contact me (nils.erik.krumnack@cern.ch) for bug
+// reports, feature suggestions, praise and complaints.
+
+
+//
+// includes
+//
+
+#include <AsgExampleTools/UnitTestTool1A.h>
+
+//
+// method implementations
+//
+
+namespace asg
+{
+  UnitTestTool1A ::
+  UnitTestTool1A (const std::string& val_name)
+    : AsgTool (val_name)
+  {
+    declareProperty ("propertyInt", m_propertyInt, "the integer property");
+    declareProperty ("initializeFail", m_initializeFail, "whether initialize should fail");
+  }
+
+
+
+  StatusCode UnitTestTool1A ::
+  initialize ()
+  {
+    if (m_initializeFail)
+    {
+      ATH_MSG_ERROR ("tool configured to fail initialize");
+      return StatusCode::FAILURE;
+    }
+    if (m_isInitialized)
+    {
+      ATH_MSG_ERROR ("initialize called twice");
+      return StatusCode::FAILURE;
+    }
+    m_isInitialized = true;
+    return StatusCode::SUCCESS;
+  }
+
+
+
+  int UnitTestTool1A ::
+  getPropertyInt () const
+  {
+    return m_propertyInt;
+  }
+
+
+
+  void UnitTestTool1A ::
+  setPropertyInt (int val_property)
+  {
+    m_propertyInt = val_property;
+  }
+
+
+
+  bool UnitTestTool1A ::
+  isInitialized () const
+  {
+    return m_isInitialized;
+  }
+}
