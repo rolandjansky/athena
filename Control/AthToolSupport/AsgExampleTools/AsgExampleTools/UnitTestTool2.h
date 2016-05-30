ea111b583811d12b84c665ed612f6a302160c3e0
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-30 16:42:19 +0200

    Making the unit test setup private in the CMT configuration (AsgExampleTools-00-00-11)
    	* Fixed the build issue in packages using this one, as pointed
    	  out by Grigori.
    	* Modified the CMakeLists.txt file, just because the requirements
    	  file was modified.
    	* Tagging as AsgExampleTools-00-00-11
    
    2016-05-11 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* added a test for the AnaToolHandle::swap bug reported by Steve
    	  based on the reproducer provided by Will
    	* tagging as AsgExampleTools-00-00-10
    
    2016-05-10 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* fixed the athena unit tests for AnaToolHandle
    	* tagging as AsgExampleTools-00-00-09
    
    2016-05-09 Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
    	* added some name checks to the Athena Job-Options unit tests (and
    	  they fail for sub-tools)
    	* going into AsgExampleTools-00-00-09
    
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 2ada041c11d097fa26f84d952adc1229ebebc5c6

---
+++ Control/AthToolSupport/AsgExampleTools/AsgExampleTools/UnitTestTool2.h
@@ -0,0 +1,69 @@
+#ifndef ASG_TOOLS__UNIT_TEST_TOOL2_H
+#define ASG_TOOLS__UNIT_TEST_TOOL2_H
+
+//        Copyright Iowa State University 2016.
+//                  Author: Nils Krumnack
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Please feel free to contact me (nils.erik.krumnack@cern.ch) for bug
+// reports, feature suggestions, praise and complaints.
+
+
+#include <AsgTools/AnaToolHandle.h>
+#include <AsgTools/AsgTool.h>
+#include <AsgExampleTools/IUnitTestTool2.h>
+
+namespace asg
+{
+  /// \brief a tool used to unit test AnaToolHandle
+  ///
+  /// This allows to unit test anything that happens in conjunction
+  /// with a tool holding a ToolHandle/AnaToolHandle.
+
+  struct UnitTestTool2 : virtual public IUnitTestTool2,
+			 public AsgTool
+  {
+    ASG_TOOL_CLASS (UnitTestTool2, IUnitTestTool2)
+
+    /// \brief standard constructor
+  public:
+    UnitTestTool2 (const std::string& val_name);
+
+  public:
+    StatusCode initialize () override;
+
+  public:
+    virtual IUnitTestTool1 *
+    getToolHandle (const std::string& handleName) const override;
+
+  public:
+    virtual StatusCode
+    retrieveToolHandle (const std::string& handleName) const override;
+
+  public:
+    virtual bool
+    wasUserConfigured (const std::string& handleName) const override;
+
+  public:
+    ToolHandle<IUnitTestTool1> m_regPublicHandle;
+
+  public:
+    ToolHandle<IUnitTestTool1> m_regPrivateHandle;
+
+  public:
+    AnaToolHandle<IUnitTestTool1> m_anaPublicHandle;
+
+  public:
+    AnaToolHandle<IUnitTestTool1> m_anaPrivateHandle;
+
+  private:
+    bool m_wasUserConfiguredPublic = false;
+
+  private:
+    bool m_wasUserConfiguredPrivate = false;
+  };
+}
+
+#endif
