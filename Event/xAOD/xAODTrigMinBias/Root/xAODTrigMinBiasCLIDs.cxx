883955fa151e5087df61fa238fa8f7ef5ae6c5b7
Attila Krasznahorkay <krasznaa@cern.ch>
2016-05-31 12:56:10 +0200

    Creating a new tag with the CMakeLists.txt file included (xAODTrigMinBias-00-00-22)
    	* Creating a new tag that now has a cleaned up CMakeLists.txt
    	  file. Which can compile the package in both the full nightly,
    	  and in a standalone build.
    	* Tagging as xAODTrigMinBias-00-00-22
    
    2015-11-25  Will Buttinger <will@cern.ch>
    	*woops ... forgot to add the actual CLIDs file
    	* tagging as xAODTrigMinBias-00-00-21
    
    2015-11-24 Will Buttinger <will@cern.ch>
    	* Added missing CLIDs at compile time (Root/xAODTrigMinBiasCLIDs.cxx)
    	* tagging as xAODTrigMinBias-00-00-20
    
    
    Former-commit-id: 8ddf0f7dfd78105719c6ad4e0d3622a60cb250fb

---
+++ Event/xAOD/xAODTrigMinBias/Root/xAODTrigMinBiasCLIDs.cxx
@@ -0,0 +1,21 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//This file contains list of headers that include CLASS_DEFs 
+//so that the CLIDs get loaded automatically into the CLIDdb at compile time
+//Please ensure it remains up to date
+
+#include "xAODTrigMinBias/TrigHisto2DContainer.h"
+#include "xAODTrigMinBias/TrigHisto2DAuxContainer.h"
+#include "xAODTrigMinBias/TrigSpacePointCountsContainer.h"
+#include "xAODTrigMinBias/TrigSpacePointCountsAuxContainer.h"
+#include "xAODTrigMinBias/TrigT2MbtsBitsContainer.h"
+#include "xAODTrigMinBias/TrigT2MbtsBitsAuxContainer.h"
+#include "xAODTrigMinBias/TrigT2ZdcSignalsContainer.h"
+#include "xAODTrigMinBias/TrigT2ZdcSignalsAuxContainer.h"
+#include "xAODTrigMinBias/TrigTrackCountsContainer.h"
+#include "xAODTrigMinBias/TrigTrackCountsAuxContainer.h"
+#include "xAODTrigMinBias/TrigVertexCountsContainer.h"
+#include "xAODTrigMinBias/TrigVertexCountsAuxContainer.h"
+
