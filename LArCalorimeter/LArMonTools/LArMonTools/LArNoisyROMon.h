fbca939d8e61a1b7b2e245a295c54aaf58124f45
Pavol Strizenec <Pavol.Strizenec@cern.ch>
2016-05-31 17:13:06 +0200

    add histograms for MNB monitoring (LArMonTools-02-00-40)
            * add many histograms for mini-noise burst (MNB) monitoring in LArNoisyROMon.cxx / LArNoisyROMon.h
            * increase range on plots from 2000 to 2500 luminosity blocks, following long run (in LArNoisyROMon.cxx)
       	* tag LArMonTools-02-00-40
    
    
    Former-commit-id: cd6c425140c3c603350ab4673893cb76a636f47c

--- LArCalorimeter/LArMonTools/LArMonTools/LArNoisyROMon.h
+++ LArCalorimeter/LArMonTools/LArMonTools/LArNoisyROMon.h
@@ -53,27 +53,74 @@ private:
   NoiseTime m_NoiseTime;
   TTree* m_NoiseTimeTree;
   
+  struct CandidateMNB
+  {
+    unsigned int candidate_MNB_time;
+    unsigned int candidate_MNB_time_ns;
+    int n_candidate_MNBTight_FEB;
+    int n_candidate_MNBLoose_FEB;
+    std::vector<int>* v_candidate_MNBTightFEB;
+    std::vector<int>* v_candidate_MNBLooseFEB;
+  };
+  CandidateMNB m_CandidateMNB;
+  TTree* m_CandidateMNBTree;
   
   struct partitionHistos
   {
     partitionHistos() {
       h_NoisyFEBFracPerEvt = NULL;
+      h_MNBTightFEBFracPerEvt = NULL;
+      h_MNBLooseFEBFracPerEvt = NULL;
+      h_CandidateMNBTightFEBFracPerEvt = NULL;
+      h_CandidateMNBLooseFEBFracPerEvt = NULL;
       h_NoisyEventFrac = NULL;
       h_NoisyEventTimeVetoFrac = NULL;
+      h_MNBTightEventFrac = NULL;
+      h_MNBTightEventTimeVetoFrac = NULL;
+      h_MNBLooseEventFrac = NULL;
+      h_MNBLooseEventTimeVetoFrac = NULL;
+      h_CandidateMNBTightEventFrac = NULL;
+      h_CandidateMNBTightEventTimeVetoFrac = NULL;
+      h_CandidateMNBLooseEventFrac = NULL;
+      h_CandidateMNBLooseEventTimeVetoFrac = NULL;
       h_NoisyWEventFrac = NULL;
       h_NoisyWEventTimeVetoFrac = NULL;
       h_NoisyEventTrigger = NULL;
       h_NoisyFEBPerEvt = NULL;
+      h_MNBTightFEBPerEvt = NULL;
+      h_MNBLooseFEBPerEvt = NULL;
+      h_CandidateMNBTightFEBPerEvt = NULL;
+      h_CandidateMNBLooseFEBPerEvt = NULL;
       h_NoisyEvent = NULL;
       h_NoisyEventTimeVeto = NULL;
+      h_MNBTightEvent = NULL;
+      h_MNBLooseEvent = NULL;
+      h_MNBTightEventTimeVeto = NULL;
+      h_MNBLooseEventTimeVeto = NULL;
+      h_CandidateMNBTightEvent = NULL;
+      h_CandidateMNBLooseEvent = NULL;
+      h_CandidateMNBTightEventTimeVeto = NULL;
+      h_CandidateMNBLooseEventTimeVeto = NULL;
       h_NoisyWEvent = NULL;
       h_NoisyWEventTimeVeto = NULL;
       h_NoisyEventTriggerL1 = NULL;
     }      
     TH2F_LW* h_NoisyFEBFracPerEvt;
+    TH2F_LW* h_MNBTightFEBFracPerEvt;
+    TH2F_LW* h_MNBLooseFEBFracPerEvt;
+    TH2F_LW* h_CandidateMNBTightFEBFracPerEvt;
+    TH2F_LW* h_CandidateMNBLooseFEBFracPerEvt;
     TH1F_LW* h_NoisyEventFrac;
+    TH1F_LW* h_MNBTightEventFrac;
+    TH1F_LW* h_MNBLooseEventFrac;
+    TH1F_LW* h_CandidateMNBTightEventFrac;
+    TH1F_LW* h_CandidateMNBLooseEventFrac;
     TH1F_LW* h_NoisyWEventFrac;
     TH1F_LW* h_NoisyEventTimeVetoFrac;
+    TH1F_LW* h_MNBTightEventTimeVetoFrac;
+    TH1F_LW* h_MNBLooseEventTimeVetoFrac;
+    TH1F_LW* h_CandidateMNBTightEventTimeVetoFrac;
+    TH1F_LW* h_CandidateMNBLooseEventTimeVetoFrac;
     TH1F_LW* h_NoisyWEventTimeVetoFrac;
     TH1I_LW* h_NoisyEventTrigger;
     TH1I_LW* h_NoisyEventTriggerL1;
@@ -82,17 +129,49 @@ private:
     // in 'online' mode they are used to produce regularly the 'Frac'
     // histograms
     TH2F_LW* h_NoisyFEBPerEvt;
+    TH2F_LW* h_MNBTightFEBPerEvt;
+    TH2F_LW* h_MNBLooseFEBPerEvt;
+    TH2F_LW* h_CandidateMNBTightFEBPerEvt;
+    TH2F_LW* h_CandidateMNBLooseFEBPerEvt;
     TH1I_LW* h_NoisyEvent;
+    TH1I_LW* h_MNBTightEvent;
+    TH1I_LW* h_MNBLooseEvent;
+    TH1I_LW* h_CandidateMNBTightEvent;
+    TH1I_LW* h_CandidateMNBLooseEvent;
     TH1I_LW* h_NoisyWEvent;
     TH1I_LW* h_NoisyEventTimeVeto;
+    TH1I_LW* h_MNBTightEventTimeVeto;
+    TH1I_LW* h_MNBLooseEventTimeVeto;
+    TH1I_LW* h_CandidateMNBTightEventTimeVeto;
+    TH1I_LW* h_CandidateMNBLooseEventTimeVeto;
     TH1I_LW* h_NoisyWEventTimeVeto;
   };
   TH1I_LW* h_NoisyFEB;
   TH1F_LW* h_NoisyEventFrac;
   TH1F_LW* h_NoisyEventTimeVetoFrac;
+  TH1I_LW* h_MNBTightFEB;
+  TH1F_LW* h_MNBTightEventFrac;
+  TH1F_LW* h_MNBTightEventTimeVetoFrac;
+  TH1I_LW* h_MNBLooseFEB;
+  TH1F_LW* h_MNBLooseEventFrac;
+  TH1F_LW* h_MNBLooseEventTimeVetoFrac;
+  TH1I_LW* h_CandidateMNBTightFEB;
+  TH1F_LW* h_CandidateMNBTightEventFrac;
+  TH1F_LW* h_CandidateMNBTightEventTimeVetoFrac;
+  TH1I_LW* h_CandidateMNBLooseFEB;
+  TH1F_LW* h_CandidateMNBLooseEventFrac;
+  TH1F_LW* h_CandidateMNBLooseEventTimeVetoFrac;
   TH1F_LW* h_SaturatedTightFrac;
   TH1I_LW* h_NoisyEvent;
   TH1I_LW* h_NoisyEventTimeVeto;
+  TH1I_LW* h_MNBTightEvent;
+  TH1I_LW* h_MNBTightEventTimeVeto;
+  TH1I_LW* h_MNBLooseEvent;
+  TH1I_LW* h_MNBLooseEventTimeVeto;
+  TH1I_LW* h_CandidateMNBTightEvent;
+  TH1I_LW* h_CandidateMNBTightEventTimeVeto;
+  TH1I_LW* h_CandidateMNBLooseEvent;
+  TH1I_LW* h_CandidateMNBLooseEventTimeVeto;
   TH1I_LW* h_SaturatedTight;
   TH1I_LW* h_SaturatedTightTimeVeto;
   TH1F_LW* h_SaturatedTightTimeVetoFrac;
