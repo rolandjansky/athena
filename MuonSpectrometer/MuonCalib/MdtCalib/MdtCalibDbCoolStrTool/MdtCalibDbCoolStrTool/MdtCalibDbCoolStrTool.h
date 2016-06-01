823301075eefd7ea79e44f4d7bb51cb4bb334cd3
Edward Diehl <diehl@umich.edu>
2016-06-01 22:49:32 +0200

    Updated ML RT storage method - compatible with MdtCalibSvc-03-00-07 (MdtCalibDbCoolStrTool-00-04-08)
    	*  Updated ML RT storage method - compatible with MdtCalibSvc-03-00-07
    	*  RTs at OnePerChamber is default - need to add job option to change
    	* Tagged MdtCalibDbCoolStrTool-00-04-08
    
    2016-05-27 Edward Diehl
    	*  Implement reading multilayer RT functions from COOL
    	*  RTs at OnePerChamber is default - need to add job option to change
            *  Cleaned and commented code
    	* Tagged MdtCalibDbCoolStrTool-00-04-07
    
    2015-06-14 Jochen Meyer
    	* fixing more bugs introduced in previous tags
    	   (wrong format assumed for string/blob)
    	* Tagged as MdtCalibDbCoolStrTool-00-04-06
    
    2015-06-10 Jochen Meyer
    	* fixing bug introduced in previous tag
    	   (wrong format assumed for string/blob)
    	* Tagged as MdtCalibDbCoolStrTool-00-04-05
    
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: a16c6e3cd42049699205e868b1701e7230903f70

--- MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibDbCoolStrTool/MdtCalibDbCoolStrTool/MdtCalibDbCoolStrTool.h
+++ MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibDbCoolStrTool/MdtCalibDbCoolStrTool/MdtCalibDbCoolStrTool.h
@@ -24,42 +24,32 @@ class IIOVDbSvc;
 class MsgStream;
 class IAtRndmGenSvc;
 namespace MuonGM{
-class MuonDetectorManager;
+  class MuonDetectorManager;
 }
 
-namespace coral
-	{
-	class Blob;
-	}
+namespace coral	{
+  class Blob;
+}
 
+namespace MuonCalib {
 
-
-namespace MuonCalib
-{
 class RtResolutionLookUp;
 class SamplePoint;
 
+class MdtCalibDbCoolStrTool: public AthAlgTool, virtual public IMdtCalibDBTool {
 
-class MdtCalibDbCoolStrTool: public AthAlgTool,
-	             virtual public IMdtCalibDBTool
-
-{
-
-
-public:    
-
+ public:    
   /** constructor */
-  MdtCalibDbCoolStrTool(const std::string& type, 
-		const std::string& name, 
-		const IInterface* parent); 
+  MdtCalibDbCoolStrTool(const std::string &type, 
+			const std::string &name, 
+			const IInterface *parent); 
 		
   virtual ~MdtCalibDbCoolStrTool();
 
   /** required by the IAddressProvider interface */
-  virtual StatusCode updateAddress(StoreID::type storeID, SG::TransientAddress* tad);
+  virtual StatusCode updateAddress(StoreID::type storeID, SG::TransientAddress *tad);
 
 private: 
-
   /** intialization: callbacks to loadTube and loadRt are registered 
    * in this method. Warning: apparently if something fails in the callback 
    * registration (for example due to a failure of the connection to the DB 
@@ -73,59 +63,63 @@ private:
   StatusCode defaultRt();
   /** Build a MuonCalib::MdtTubeCalibContainer for a given Identifier
    * @return the MuonCalib::MdtTubeCalibContainer
-   * @param[in] const reference to the Identifier
-   */
-  MdtTubeCalibContainer * buildMdtTubeCalibContainer(const Identifier& id);
+   * @param[in] const reference to the Identifier  */
+  MdtTubeCalibContainer* buildMdtTubeCalibContainer(const Identifier &id);
 
-  /** retrieve from database, calls loadRt or loadTube accordinlgy to 
-   *  the IOVSVC_CALLBACK_ARGS */
+  /** retrieve from database, calls loadRt or loadTube according to the IOVSVC_CALLBACK_ARGS */
   virtual StatusCode LoadCalibration(IOVSVC_CALLBACK_ARGS);
   /** loads the RTs from the DB */
   virtual StatusCode loadRt(IOVSVC_CALLBACK_ARGS);
   /** loads the tube constants from the DB */
   virtual StatusCode loadTube(IOVSVC_CALLBACK_ARGS);
 
+  const MdtIdHelper *m_mdtIdHelper;
+  const MuonGM::MuonDetectorManager *m_detMgr;
   ToolHandle<IIdToFixedIdTool> m_idToFixedIdTool;
-  ServiceHandle<IIOVDbSvc> m_IOVDbSvc;
-  const MdtIdHelper* m_mdtIdHelper;
-  const MuonGM::MuonDetectorManager* m_detMgr;
+  ServiceHandle<IIOVDbSvc>     m_IOVDbSvc;
   ServiceHandle<MdtCalibrationRegionSvc> m_regionSvc;
-  mutable MdtTubeCalibContainerCollection * m_tubeData;
-  mutable MdtRtRelationCollection * m_rtData;
+  mutable MdtTubeCalibContainerCollection *m_tubeData;
+  mutable MdtRtRelationCollection *m_rtData;
 
   std::string      m_rtFolder;
   std::string      m_tubeFolder;
-  std::string      m_rtDataLocation;
-  std::string      m_tubeDataLocation;
-//if set to true, it is assumed, that the time slewing correction is applied. If false not. If this flag does not match the bit in the creation parameters, the rt-relation and t0 will be corrected.
-//NOTE: This is a preliminary solution for 17.2. In Principle each MdtDriftCircleOnTrackCreator could decide individually if it want to have TS-correction. In the default reco-jobs however, this is configured by one muonRecFlag, that will be used to set this job-option.
-  bool             m_TimeSlewingCorrection;
-  std::vector<float> m_MeanCorrectionVsR;
-  float m_TsCorrectionT0;
-  
-  double           m_defaultT0;
+  std::string      m_rtDataLocation;     //string used to access m_rtData in StoreGate
+  std::string      m_tubeDataLocation;   //string used to access m_tubeData in StoreGate
 
+//if m_TimeSlewingCorrection is set to true, it is assumed, that the
+//time slewing correction is applied. If false not. If this flag does 
+//not match the bit in the creation parameters, the rt-relation and t0 
+//will be corrected.  
+//NOTE: This is a preliminary solution for 17.2. In principle each
+//MdtDriftCircleOnTrackCreator could decide individually if it wants to
+//have TS-correction. In the default reco-jobs however, this is
+//configured by one muonRecFlag, that will be used to set this job-option.
+  bool   m_TimeSlewingCorrection;
+  std::vector<float> m_MeanCorrectionVsR;
+  float  m_TsCorrectionT0;
+  double m_defaultT0;
   double m_t0Shift;
   double m_t0Spread;
   double m_rtShift;
   double m_rtScale;
   double m_prop_beta;
   ServiceHandle<IAtRndmGenSvc> m_AtRndmGenSvc;
-  CLHEP::HepRandomEngine* p_engine;
+  CLHEP::HepRandomEngine *p_engine;
 
   StringArrayProperty m_RTfileNames; //temporary!!!
  
 //decompression buffer and length of buffer
    uLongf m_buffer_length;
-   Bytef * m_decompression_buffer;
+   Bytef *m_decompression_buffer;
 
 //wrapper function for the zlib uncompress, that automatically creates or increases the buffer if needed.    
-   inline bool uncompressInMyBuffer(const coral::Blob & blob);
-    inline RtResolutionLookUp * getRtResolutionInterpolation(const std::vector<SamplePoint> & sample_points);
+   inline bool uncompressInMyBuffer(const coral::Blob &blob);
+   inline RtResolutionLookUp* getRtResolutionInterpolation(const std::vector<SamplePoint> &sample_points);
+   inline StatusCode extractString(std::string& input, std::string& output, std::string separator);
   
 };
 
-}
+}  //namespace MuonCalib
 
 #endif
 
