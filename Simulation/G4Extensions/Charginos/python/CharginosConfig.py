f6a58008ed03197e3afe50302d39dbd1763486e3
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-02 10:28:18 +0200

    python/CharginosConfig.py (getCharginosPhysicsTool): pulling in the configuration of the particle masses and lifetimes from SimulationJobOptions/share/specialConfig/preInclude.AMSB.py. Tagging: Charginos-00-02-00 (Charginos-00-02-00)
    	* python/CharginosConfig.py (getCharginosPhysicsTool): pulling in
    	the configuration of the particle masses and lifetimes from
    	SimulationJobOptions/share/specialConfig/preInclude.AMSB.py
    	* Tagging: Charginos-00-02-00
    
    
    Former-commit-id: 60fb1295586129d87c451e89a87fdd4116de03b3

--- Simulation/G4Extensions/Charginos/python/CharginosConfig.py
+++ Simulation/G4Extensions/Charginos/python/CharginosConfig.py
@@ -2,30 +2,43 @@
 
 from AthenaCommon import CfgMgr
 def getCharginosPhysicsTool(name="CharginosPhysicsTool", **kwargs):
-    ##from AthenaCommon.SystemOfUnits import GeV,MeV,eplus
+    from G4AtlasApps import AtlasG4Eng
+    from AthenaCommon.SystemOfUnits import GeV,MeV,eplus,ns
+    # FIXME AtlasG4Eng will soon be obsolete need to do this in a better way.
+    C1Mass = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["AMSBC1Mass"])
+    N1Mass = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["AMSBN1Mass"])
+    C1Lifetime = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["AMSBC1Lifetime"])
 
-    ##kwargs.setdefault"CharginoPlusMass",        101.0*GeV);
-    ##kwargs.setdefault"CharginoPlusWidth",       0.0*MeV);
-    ##kwargs.setdefault"CharginoPlusCharge",      1.*eplus);
-    ##kwargs.setdefault"CharginoPlusPDGCode",     1000024);
-    ##kwargs.setdefault"CharginoPlusStable",      True);
-    ##kwargs.setdefault"CharginoPlusLifetime",    -1);
-    ##kwargs.setdefault"CharginoPlusShortlived",  False);
+    kwargs.setdefault("CharginoPlusMass", C1Mass*MeV) # TODO check units
+    kwargs.setdefault("CharginoPlusStable",      (C1Lifetime < 0))
+    if not (C1Lifetime < 0):
+        kwargs.setdefault("CharginoPlusLifetime",    C1Lifetime*ns)
+    ##kwargs.setdefault("CharginoPlusMass",        101.0*GeV)
+    ##kwargs.setdefault("CharginoPlusWidth",       0.0*MeV)
+    ##kwargs.setdefault("CharginoPlusCharge",      1.*eplus)
+    ##kwargs.setdefault("CharginoPlusPDGCode",     1000024)
+    ##kwargs.setdefault("CharginoPlusStable",      True)
+    ##kwargs.setdefault("CharginoPlusLifetime",    -1)
+    ##kwargs.setdefault("CharginoPlusShortlived",  False)
 
-    ##kwargs.setdefault"CharginoMinusMass",       101.0*GeV);
-    ##kwargs.setdefault"CharginoMinusWidth",      0.0*MeV);
-    ##kwargs.setdefault"CharginoMinusCharge",     -1.*eplus);
-    ##kwargs.setdefault"CharginoMinusPDGCode",    -1000024);
-    ##kwargs.setdefault"CharginoMinusStable",     True);
-    ##kwargs.setdefault"CharginoMinusLifetime",   -1);
-    ##kwargs.setdefault"CharginoMinusShortlived", False);
+    kwargs.setdefault("CharginoMinusMass", C1Mass*MeV) # TODO check units
+    kwargs.setdefault("CharginoMinusStable",      (C1Lifetime < 0))
+    if not (C1Lifetime < 0):
+        kwargs.setdefault("CharginoMinusLifetime",    C1Lifetime*ns)
+    ##kwargs.setdefault("CharginoMinusMass",        101.0*GeV)
+    ##kwargs.setdefault("CharginoMinusWidth",       0.0*MeV)
+    ##kwargs.setdefault("CharginoMinusCharge",      1.*eplus)
+    ##kwargs.setdefault("CharginoMinusPDGCode",     1000024)
+    ##kwargs.setdefault("CharginoMinusStable",      True)
+    ##kwargs.setdefault("CharginoMinusLifetime",    -1)
+    ##kwargs.setdefault("CharginoMinusShortlived",  False)
 
-    ##kwargs.setdefault"NeutralinoMass",          100.0*GeV);
-    ##kwargs.setdefault"NeutralinoWidth",         0.0*MeV);
-    ##kwargs.setdefault"NeutralinoCharge",        0.*eplus);
-    ##kwargs.setdefault"NeutralinoPDGCode",       1000022);
-    ##kwargs.setdefault"NeutralinoStable",        True);
-    ##kwargs.setdefault"NeutralinoLifetime",      -1);
-    ##kwargs.setdefault"NeutralinoShortlived",    False);
+    kwargs.setdefault("NeutralinoMass",          N1Mass*MeV) # TODO check units
+    ##kwargs.setdefault("NeutralinoWidth",         0.0*MeV)
+    ##kwargs.setdefault("NeutralinoCharge",        0.*eplus)
+    ##kwargs.setdefault("NeutralinoPDGCode",       1000022)
+    ##kwargs.setdefault("NeutralinoStable",        True)
+    ##kwargs.setdefault("NeutralinoLifetime",      -1)
+    ##kwargs.setdefault("NeutralinoShortlived",    False)
 
     return CfgMgr.CharginosPhysicsToolTool(name, **kwargs)
