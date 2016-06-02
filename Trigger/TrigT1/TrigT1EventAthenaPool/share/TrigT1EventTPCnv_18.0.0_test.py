d755fb5750b49c5c37396fa4f2e051ad37e3cb48
Scott Snyder <scott.snyder@cern.ch>
2016-06-02 15:46:51 +0200

    'Get tests working with cmake builds.' (TrigT1EventAthenaPool-00-02-04)
    	* Tagging TrigT1EventAthenaPool-00-02-04.
    	* Get tests working with cmake builds.
    
    
    Former-commit-id: 626a31fc94338d06a0a25440de6b6e53ed4e0e37

--- Trigger/TrigT1/TrigT1EventAthenaPool/share/TrigT1EventTPCnv_18.0.0_test.py
+++ Trigger/TrigT1/TrigT1EventAthenaPool/share/TrigT1EventTPCnv_18.0.0_test.py
@@ -46,9 +46,6 @@ keys = [
     'TriggerTowers',
          ]
 
-from AthenaCommon.JobProperties import jobproperties
-jobproperties.Global.DetDescrVersion = 'ATLAS-GEO-10-00-00'
-
 noMuon = True
 noID = True
 include ('AthenaPoolUtilities/TPCnvTest.py')
