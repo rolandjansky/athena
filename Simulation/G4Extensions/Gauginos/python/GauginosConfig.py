045c23d4bcbd039abe4d9ca9acdd1e99924de56e
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-02 10:47:41 +0200

    python/GauginosConfig.py (getGauginosPhysicsTool): pulling in the configuration of the particle masses and lifetimes from SimulationJobOptions/share/specialConfig/preInclude.GMSB.py. Tagging: Gauginos-00-02-00 (Gauginos-00-02-00)
    	* python/GauginosConfig.py (getGauginosPhysicsTool): pulling in the
    	configuration of the particle masses and lifetimes from
    	SimulationJobOptions/share/specialConfig/preInclude.GMSB.py.
    	* Tagging: Gauginos-00-02-00
    
    
    Former-commit-id: 3d93a68b0f8b0b7d62921c45ceea4a04f14bda2a

--- Simulation/G4Extensions/Gauginos/python/GauginosConfig.py
+++ Simulation/G4Extensions/Gauginos/python/GauginosConfig.py
@@ -2,21 +2,25 @@
 
 from AthenaCommon import CfgMgr
 def getGauginosPhysicsTool(name="GauginosPhysicsTool", **kwargs):
-    ##from AthenaCommon.SystemOfUnits import GeV,MeV,eplus
+    from AthenaCommon.SystemOfUnits import GeV,MeV,eplus,ns
+    from G4AtlasApps import AtlasG4Eng
+    GMSBNeutralino = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["GMSBNeutralino"])
+    GMSBTime = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["GMSBLifeTime"])
+    kwargs.setdefault("GravitinoMass",        GMSBNeutralino)
+    ##kwargs.setdefault("GravitinoWidth",       0.0*GeV);
+    ##kwargs.setdefault("GravitinoCharge",      0);
+    ##kwargs.setdefault("GravitinoPDGCode",     1000039);
+    kwargs.setdefault("GravitinoStable",      False);
+    kwargs.setdefault("GravitinoLifetime",    GMSBTime);
+    ##kwargs.setdefault("GravitinoShortlived",  False);
 
-    ##kwargs.setdefault"GravitinoMass",        0.108e-04*GeV);
-    ##kwargs.setdefault"GravitinoWidth",       0.0*GeV);
-    ##kwargs.setdefault"GravitinoCharge",      0);
-    ##kwargs.setdefault"GravitinoPDGCode",     1000039);
-    ##kwargs.setdefault"GravitinoStable",      True);
-    ##kwargs.setdefault"GravitinoLifetime",    -1);
-    ##kwargs.setdefault"GravitinoShortlived",  False);
-
-    ##kwargs.setdefault"NeutralinoMass",        118.848*GeV);
-    ##kwargs.setdefault"NeutralinoWidth",       0.0*GeV);
-    ##kwargs.setdefault"NeutralinoCharge",      0);
-    ##kwargs.setdefault"NeutralinoPDGCode",     1000022);
-    ##kwargs.setdefault"NeutralinoStable",      True);
-    ##kwargs.setdefault"NeutralinoLifetime",    -1);
-    ##kwargs.setdefault"NeutralinoShortlived",  False);
+    if AtlasG4Eng.G4Eng.Dict_SpecialConfiguration.has_key("GMSBGravitino"):
+        GMSBGravitino = eval(AtlasG4Eng.G4Eng.Dict_SpecialConfiguration["GMSBGravitino"])
+        kwargs.setdefault("NeutralinoMass",       GMSBGravitino);
+        ##kwargs.setdefault("NeutralinoWidth",       0.0*GeV);
+        ##kwargs.setdefault("NeutralinoCharge",      0);
+        ##kwargs.setdefault("NeutralinoPDGCode",     1000022);
+        ##kwargs.setdefault("NeutralinoStable",      True);
+        ##kwargs.setdefault("NeutralinoLifetime",    -1);
+        ##kwargs.setdefault("NeutralinoShortlived",  False);
     return CfgMgr.GauginosPhysicsToolTool(name, **kwargs)
