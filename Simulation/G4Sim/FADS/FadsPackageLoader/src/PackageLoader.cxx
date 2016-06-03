45338b1fad4cfb22628ca7c96f55b854d5b8f369
Zachary Louis Marshall <zach.marshall@cern.ch>
2016-06-03 09:54:34 +0200

    Boost instead of FadsUtilities (FadsPackageLoader-00-00-56)
    	* Removing dependence on FadsUtilities
    	* Tagged as FadsPackageLoader-00-00-56
    
    2014-10-27  David Quarrie  <David.Quarrie@cern.ch>
    	* cmt/requirements
    		Add required system libraries for cmake (transparent to CMT)
    	* Tagged as FadsPackageLoader-00-00-55
    
    
    Former-commit-id: 068743a5e4baff282338254bde4f9b3a5b894965

--- Simulation/G4Sim/FADS/FadsPackageLoader/src/PackageLoader.cxx
+++ Simulation/G4Sim/FADS/FadsPackageLoader/src/PackageLoader.cxx
@@ -2,15 +2,18 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include<iostream>
-#include <string>
-#include <cstdio> 
-#include <dlfcn.h>
-
-#include "FadsUtilities/Tokenizer.h"
-
+// This package header
 #include "FadsPackageLoader/PackageLoader.h"
 
+// For the string handling
+#include <boost/foreach.hpp>
+#include <boost/tokenizer.hpp>
+
+// standard library
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <dlfcn.h>
 #include <cstdlib>
 
 namespace FADS {
@@ -78,36 +81,35 @@ bool PackageLoader::operator()(const std::string & packlist, const std::string& 
   if (packlist.empty()) return true;
   Dict & localLoaded = loaded();
   bool ok = true;
-  std::string semicolon(":");
-  FADS::Tokenizer packs(semicolon,packlist);
-   // load
-  typedef std::vector<std::string>::const_iterator Itr;
-  for (Itr p=packs.begin(); p!=packs.end(); p++) {
 
-    CDI ol = localLoaded.find(*p);
+   // load
+  boost::char_separator<char> sep(":");
+  boost::tokenizer<boost::char_separator<char>> packs(packlist,sep);
+  BOOST_FOREACH(std::string p, packs){
+    CDI ol = localLoaded.find(p);
     if (ol==localLoaded.end()) {
       // new package
-      std::cout << "loading package " << *p 
+      std::cout << "loading package " << p 
 		<< " version " << version << std::endl;
       std::string fLibPath("lib");
-      fLibPath+=*p; fLibPath+=".so";
+      fLibPath+=p; fLibPath+=".so";
       void * handle=dlopen(fLibPath.c_str(),RTLD_LAZY|RTLD_GLOBAL);
       if (handle==0) {
-	std::cerr << "error in loading shared library: " << dlerror() << std::endl;
-	ok = false;
+        std::cerr << "error in loading shared library: " << dlerror() << std::endl;
+        ok = false;
       }
-      CDI nl = localLoaded.find(*p);
-      if (nl!=localLoaded.end()) std::cout << "package " << *p << ": loading confirmed" << std::endl;
-      localLoaded[*p] = Elem(version,handle);
+      CDI nl = localLoaded.find(p);
+      if (nl!=localLoaded.end()) std::cout << "package " << p << ": loading confirmed" << std::endl;
+      localLoaded[p] = Elem(version,handle);
     }
     else {
       // package already loaded 
-      std::cerr << "asked to load package " << *p << " version \"" << version << "\"" << std::endl;
+      std::cerr << "asked to load package " << p << " version \"" << version << "\"" << std::endl;
       if ((*ol).second.first==version) std::cerr << "Warning: package already loaded" << std::endl;
       else if ((*ol).second.first=="Unknown") std::cerr << "Warning: package already loaded with unknown version" << std::endl;
       else { 
-	std::cerr << "ERROR: package already loaded with version" << (*ol).second.first << std::endl;
-	ok = false;
+        std::cerr << "ERROR: package already loaded with version" << (*ol).second.first << std::endl;
+        ok = false;
       }
     }
 
@@ -120,24 +122,23 @@ bool PackageLoader::unload(const std::string & packlist) {
   if (packlist.empty()) return true;
   Dict & localLoaded = loaded();
   bool ok = true;
-  std::string semicolon(":");
-  FADS::Tokenizer packs(semicolon,packlist);
    // load
-  typedef std::vector<std::string>::const_iterator Itr;
-  for (Itr p=packs.begin(); p!=packs.end(); p++) {
-    DI ol = localLoaded.find(*p);
+  boost::char_separator<char> sep(":");
+  boost::tokenizer<boost::char_separator<char>> packs(packlist,sep);
+  BOOST_FOREACH(std::string p, packs){
+    DI ol = localLoaded.find(p);
     if (ol==localLoaded.end()) {
-      std::cerr<< "cannot unload not registered package " << *p <<std::endl;
+      std::cerr<< "cannot unload not registered package " << p <<std::endl;
       ok=false;
     }
     else if((*ol).second.second==0) {
-      std::cerr << "cannot unload package " << *p 
+      std::cerr << "cannot unload package " << p 
 		<< " handle is 0" << std::endl;
       ok=false;
     }
     else {
       dlclose((*ol).second.second);
-      std::cout << "unloading package " << *p 
+      std::cout << "unloading package " << p 
 		<< " version " << (*ol).second.first << std::endl;
       localLoaded.erase(ol);
     }
