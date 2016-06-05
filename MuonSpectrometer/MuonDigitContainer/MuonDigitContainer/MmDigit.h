0832d1fa163db053bfa4a410462ee3679f09efcb
Jochen Meyer <Jochen.Meyer@cern.ch>
2016-06-05 15:48:40 +0200

    re-shuffling for nicer looks (MuonDigitContainer-01-02-23)
    2016-06-05  Jochen Meyer  <jomeyer@mail.cern.ch>
    	* re-shuffling for nicer looks
    	* Tagging MuonDigitContainer-01-02-23
    
    2016-05-26  Tomoyuki Saito  <tomoyuki.saito@cern.ch>
    	* Fixing bug on MmDigit in MuonDigitContainer-01-02-21
    	* Tagging MuonDigitContainer-01-02-22
    2016-05-05  Tomoyuki Saito  <tomoyuki.saito@cern.ch>
    	* Adding trigger output in MmDigitContainer
    	* Tagging MuonDigitContainer-01-02-21
    
    2016-01-07  scott snyder  <snyder@bnl.gov>
    
    
    Former-commit-id: a850f4cc3c0bd8757e0af462947b455b344ca91d

--- MuonSpectrometer/MuonDigitContainer/MuonDigitContainer/MmDigit.h
+++ MuonSpectrometer/MuonDigitContainer/MuonDigitContainer/MmDigit.h
@@ -29,8 +29,12 @@ class MmDigit : public MuonDigit {
   std::vector<float> m_chipResponseCharge;
   std::vector<int> m_chipResponsePosition;
   
-  int m_stripForTrigger;
-  float m_stripTimeForTrigger;
+  std::vector<float> m_stripTimeForTrigger;
+  std::vector<int> m_stripPositionForTrigger;
+  std::vector<float> m_stripChargeForTrigger;
+    
+  std::vector<int> m_MMFE_VMM_idForTrigger;
+  std::vector<int> m_VMM_idForTrigger;
    
  public:  // functions
 
@@ -45,27 +49,38 @@ class MmDigit : public MuonDigit {
   MmDigit(const Identifier& id,
           const std::vector<float>& stripResponseTime,
           const std::vector<int>& stripResponsePosition,
-          const std::vector<float>& stripResponseCharge, 
+          const std::vector<float>& stripResponseCharge,
           const std::vector<float>& chipResponseTime,
           const std::vector<int>& chipResponsePosition,
           const std::vector<float>& chipResponseCharge,
-          int stripForTrigger,
-          float stripTimeForTrigger);
+          const std::vector<float>& stripTimeForTrigger,
+          const std::vector<int>& stripPositionForTrigger,
+          const std::vector<float>& stripChargeForTrigger,
+          const std::vector<int>& MMFE_VMM_idForTrigger,
+          const std::vector<int>& VMM_idForTrigger);
 
   // Is this a valid digit?
   bool is_valid(const MmIdHelper * mmHelper) const;
   
    /** strip response info */
-  /** return the time */
+  /** Return the time */
   const std::vector<float>& stripResponseTime() const { return m_stripResponseTime; }
   /** Return the strip position */
   const std::vector<int>& stripResponsePosition() const { return m_stripResponsePosition; }
   /** Return the charge */
   const std::vector<float>& stripResponseCharge() const { return m_stripResponseCharge; }
-  /** Return the stripForTrigger */
-  int stripForTrigger() const { return m_stripForTrigger; }
-  /** Return the charge */
-  float stripTimeForTrigger() const { return m_stripTimeForTrigger; }
+    
+  /** Return the time for trigger*/
+  const std::vector<float>& stripTimeForTrigger() const { return m_stripTimeForTrigger; }
+  /** Return the strip position for trigger*/
+  const std::vector<int>& stripPositionForTrigger() const { return m_stripPositionForTrigger; }
+  /** Return the charge for trigger*/
+  const std::vector<float>& stripChargeForTrigger() const { return m_stripChargeForTrigger; }
+  
+  /** Retrum the VMM id*/
+  const std::vector<int>& MMFE_VMM_idForTrigger() const { return m_MMFE_VMM_idForTrigger; }
+  const std::vector<int>& VMM_idForTrigger() const { return m_VMM_idForTrigger; }
+
 
    /** chip response info */
   /** return the time */
