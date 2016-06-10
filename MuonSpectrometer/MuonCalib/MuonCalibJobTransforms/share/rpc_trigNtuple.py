2d665812ba8e8428a673979bf9616e43038bfc7c
Edward Diehl <diehl@umich.edu>
2016-06-10 10:06:33 +0200

    add rawTgc transform option to put rawTgc rawTgcCoin in calib ntuple (MuonCalibJobTransforms-00-04-15)
    Former-commit-id: 3b1e8abc3df52b95d2a8005e95604a3d78c8b619

---
+++ MuonSpectrometer/MuonCalib/MuonCalibJobTransforms/share/rpc_trigNtuple.py
@@ -0,0 +1,8 @@
+from MuonCalibAlgs.MuonCalib import MuonCalibAlg
+
+MuonCalibAlg.doMDTs = False
+MuonCalibAlg.doCSCs = False
+MuonCalibAlg.doRPCs = True
+MuonCalibAlg.doTGCs = False
+
+MuonCalibAlg.doTGCCoinData = False
