93d390dedfa1aa1d866a5587f3845c667242e06f
Andrea Coccaro <Andrea.Coccaro@cern.ch>
2016-06-11 22:30:01 +0200

    new instance for selecting collimated photons (TrigLongLivedParticlesHypo-00-02-31)
    	* new instance for selecting collimated photons
            * tagged as TrigLongLivedParticlesHypo-00-02-31
    
    
    Former-commit-id: b0d7f221bbc22292c4e3fb95dac877537c5f44f9

--- Trigger/TrigHypothesis/TrigLongLivedParticlesHypo/src/TrigCaloRatioHypo.cxx
+++ Trigger/TrigHypothesis/TrigLongLivedParticlesHypo/src/TrigCaloRatioHypo.cxx
@@ -36,6 +36,7 @@ TrigCaloRatioHypo::TrigCaloRatioHypo(const std::string& name, ISvcLocator* pSvcL
   declareProperty("PtMinID",     m_ptCut = 2000.0, "minimum track Pt in MeV");
   declareProperty("DeltaR",      m_deltaR=0.2, "radius for the track isolation requirement");
   declareProperty("EtaCut",      m_etaCut = 2.5, "cut value for Eta of the Jet");
+  declareProperty("Reversed",    m_reversedCut = false, "reversed cut for collimated photons");
   declareProperty("AcceptAll",   m_acceptAll=false);
 
   declareMonitoredStdContainer("JetEt",      m_jetEt);
@@ -143,62 +144,82 @@ HLT::ErrorCode TrigCaloRatioHypo::hltExecute(const HLT::TriggerElement* outputTE
   if(msgLvl() <= MSG::DEBUG)
     msg() << MSG::DEBUG << " jet with et=" << jetEt << ", eta=" << jetEta << ", phi=" << jetPhi 
 	  << ", log-ratio=" << jetRatio << endreq;
-
-  if (jetEt > m_etCut && std::fabs(jetEta) <= m_etaCut && jetRatio >= m_logRatioCut) {
+  
+  if(!m_reversedCut) {
     
-    m_jetEt.push_back(jetEt/CLHEP::GeV);
-    m_jetEta.push_back(jetEta); 
-    m_jetPhi.push_back(jetPhi);
-    m_logRatio = jetRatio;
+    if (jetEt > m_etCut && std::fabs(jetEta) <= m_etaCut && jetRatio >= m_logRatioCut) {
+      
+      m_jetEt.push_back(jetEt/CLHEP::GeV);
+      m_jetEta.push_back(jetEta); 
+      m_jetPhi.push_back(jetPhi);
+      m_logRatio = jetRatio;
+      
+      passCutJet = true;
+    }
     
-    passCutJet = true;
+  } else {
+      
+    if (jetEt > m_etCut && std::fabs(jetEta) <= m_etaCut && jetRatio <= m_logRatioCut) {
+	
+      m_jetEt.push_back(jetEt/CLHEP::GeV);
+      m_jetEta.push_back(jetEta); 
+      m_jetPhi.push_back(jetPhi);
+      m_logRatio = jetRatio;
+      
+      passCutJet = true;
+    }
+    
   }
 
-  const xAOD::TrackParticleContainer* vectorOfTracks;
-  status = getFeature(outputTE, vectorOfTracks, "InDetTrigTrackingxAODCnv_Tau_IDTrig");
+  if(!m_reversedCut) {
 
-  if(status != HLT::OK) {
-    msg() << MSG::ERROR << "Failed to get the xAOD track container" << endreq;
-    m_errors++;
-    return HLT::ERROR;
-  } else if (msgLvl() <= MSG::DEBUG)
-    msg() << MSG::DEBUG << "Got the xAOD track container" << endreq;
-
-  if(vectorOfTracks->size() == 0) {
-    if (msgLvl() <= MSG::DEBUG)
-      msg() << MSG::DEBUG << "The xAOD track container is empty" << endreq;
-     passCutTrk = true; //default is true, but just for clarity
-  } else {
-    if (msgLvl() <= MSG::DEBUG)
-      msg() << MSG::DEBUG << vectorOfTracks->size() << " tracks are found" << endreq;
-  
-  xAOD::TrackParticleContainer::const_iterator
-    track     = vectorOfTracks->begin(),
-    lasttrack = vectorOfTracks->end();
-
-  for(; track !=lasttrack; track++ ) {
-
-    float theta   = (*track)->theta();
-    float qOverPt = (*track)->qOverP()/TMath::Sin(theta);
-    float pT      = (1/qOverPt);
-
-    if (fabs(pT) <= m_ptCut) continue;
-
-    double phi  = (*track)->phi0();
-    double eta  = (*track)->eta();
+    const xAOD::TrackParticleContainer* vectorOfTracks;
+    status = getFeature(outputTE, vectorOfTracks, "InDetTrigTrackingxAODCnv_Tau_IDTrig");
     
-    if(msgLvl() <= MSG::DEBUG)
-      msg() << MSG::DEBUG << " track with " << "pt=" << pT << ", eta=" << eta << ", phi=" << phi  << endreq;
-
-    double deta = fabs(eta-jetEta);	
-    double dphi = fabs(HLT::wrapPhi(phi-jetPhi));
-
-    double dR = sqrt((deta*deta)+(dphi*dphi));
-    if (dR<m_deltaR) {
-      passCutTrk = false;
+    if(status != HLT::OK) {
+      msg() << MSG::ERROR << "Failed to get the xAOD track container" << endreq;
+      m_errors++;
+      return HLT::ERROR;
+    } else if (msgLvl() <= MSG::DEBUG)
+      msg() << MSG::DEBUG << "Got the xAOD track container" << endreq;
+    
+    if(vectorOfTracks->size() == 0) {
+      if (msgLvl() <= MSG::DEBUG)
+	msg() << MSG::DEBUG << "The xAOD track container is empty" << endreq;
+      passCutTrk = true; //default is true, but just for clarity
+    } else {
+      if (msgLvl() <= MSG::DEBUG)
+	msg() << MSG::DEBUG << vectorOfTracks->size() << " tracks are found" << endreq;
+      
+      xAOD::TrackParticleContainer::const_iterator
+	track     = vectorOfTracks->begin(),
+	lasttrack = vectorOfTracks->end();
+      
+      for(; track !=lasttrack; track++ ) {
+	
+	float theta   = (*track)->theta();
+	float qOverPt = (*track)->qOverP()/TMath::Sin(theta);
+	float pT      = (1/qOverPt);
+	
+	if (fabs(pT) <= m_ptCut) continue;
+	
+	double phi  = (*track)->phi0();
+	double eta  = (*track)->eta();
+	
+	if(msgLvl() <= MSG::DEBUG)
+	  msg() << MSG::DEBUG << " track with " << "pt=" << pT << ", eta=" << eta << ", phi=" << phi  << endreq;
+	
+	double deta = fabs(eta-jetEta);	
+	double dphi = fabs(HLT::wrapPhi(phi-jetPhi));
+	
+	double dR = sqrt((deta*deta)+(dphi*dphi));
+	if (dR<m_deltaR) {
+	  passCutTrk = false;
+	}
+      }
     }
   }
-  }
+
   if((passCutJet&&passCutTrk) || m_acceptAll) {
 
     pass = true;
