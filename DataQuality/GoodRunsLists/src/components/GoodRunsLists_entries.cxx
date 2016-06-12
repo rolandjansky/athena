7ae1cd1503329b929ef12f299a8d7fb5e7988eb7
Will Buttinger <William.Buttinger@cern.ch>
2016-06-12 10:28:45 +0200

    'remove getLumi.exe program, lives in LumiBlockComps now' (GoodRunsLists-00-01-32)

--- DataQuality/GoodRunsLists/src/components/GoodRunsLists_entries.cxx
+++ DataQuality/GoodRunsLists/src/components/GoodRunsLists_entries.cxx
@@ -7,7 +7,12 @@ DECLARE_TOOL_FACTORY( GoodRunsListSelectorTool )
 DECLARE_TOOL_FACTORY( GoodRunsListSelectionTool )
 DECLARE_TOOL_FACTORY( TriggerRegistryTool )
 
+
+#include "../GRLSelectorAlg.h"
+DECLARE_ALGORITHM_FACTORY( GRLSelectorAlg )
+
 DECLARE_FACTORY_ENTRIES( GoodRunsLists ) {
+  DECLARE_ALGORITHM( GRLSelectorAlg );
     //DECLARE_ALGTOOL( GoodRunsListSelectorTool )
     DECLARE_TOOL( GoodRunsListSelectorTool )
     DECLARE_TOOL( GoodRunsListSelectionTool )
