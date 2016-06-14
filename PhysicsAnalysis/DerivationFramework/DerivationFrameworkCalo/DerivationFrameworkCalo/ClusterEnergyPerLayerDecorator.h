1d86f8ad1bfafb7c7df4b15422d15bc5e5328597
Christos Anastopoulos <Christos.Anastopoulos@cern.ch>
2016-06-14 20:08:56 +0200

    Fix coverity warnings (DerivationFrameworkCalo-00-00-15)
    	* Fix coverity warnings
    	* Tagging DerivationFrameworkCalo-00-00-15
    
    2016-05-27  Bruno Lenzi
      * Adding ClusterEnergyPerLayerDecorator
      * Tagging DerivationFrameworkCalo-00-00-14
    
    2016-05-13  Bruno Lenzi
      * Small changes in requirements and CMakeLists.txt (Justin Griffith)
      * Tagging DerivationFrameworkCalo-00-00-13
    
    2016-05-13  Bruno Lenzi
      * Change in CaloClusterThinning class to save clusters associated to taus obo Justin Griffith
      * Tagging DerivationFrameworkCalo-00-00-12
    
    
    Former-commit-id: 629cc263f39d8f99c7c42111033a28db57d2c67b

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCalo/DerivationFrameworkCalo/ClusterEnergyPerLayerDecorator.h
@@ -0,0 +1,53 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// ClusterEnergyPerLayerDecorator.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef DERIVATIONFRAMEWORK_ClusterEnergyPerLayerDecorator_H
+#define DERIVATIONFRAMEWORK_ClusterEnergyPerLayerDecorator_H
+
+#include <string>
+
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "DerivationFrameworkInterfaces/IThinningTool.h"
+#include "DerivationFrameworkInterfaces/IAugmentationTool.h"
+
+#include "xAODEgamma/EgammaFwd.h"
+
+#include "GaudiKernel/ToolHandle.h"
+
+class CaloFillRectangularCluster;
+class CaloClusterCollectionProcessor;
+
+namespace DerivationFramework {
+  
+  /** Decorate egamma objects with the energy per layer for a rectangular cluster
+    * of size neta X nphi built on the fly
+    **/
+  class ClusterEnergyPerLayerDecorator : public AthAlgTool, public IAugmentationTool {
+    public:
+      ClusterEnergyPerLayerDecorator(const std::string& t, const std::string& n, const IInterface* p);
+      ~ClusterEnergyPerLayerDecorator();
+      StatusCode initialize();
+      StatusCode finalize();
+      virtual StatusCode addBranches() const;
+
+    private:
+      std::string m_SGKey_photons;
+      std::string m_SGKey_electrons;
+      int m_eta_size;
+      int m_phi_size;
+      std::vector<unsigned int> m_layers;
+
+      ToolHandle<CaloClusterCollectionProcessor> m_caloFillRectangularTool;
+      CaloFillRectangularCluster *m_tool;
+      std::string m_CellCollectionName;
+      
+      void decorateObject(const xAOD::Egamma*& egamma) const;
+  };
+}
+
+#endif // DERIVATIONFRAMEWORK_ClusterEnergyPerLayerDecorator_H
