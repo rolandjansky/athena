c18253121fa8721b7dd12d17e631bf85923ee105
Iain Bertram <iain.bertram@cern.ch>
2016-06-16 14:15:33 +0200

    Rationalised the tests, removing many redundant sections to reduce resource usage. Kept reference of tests in test/DigitizationTests_TestConfiguration_old.xml. [ATLASSIM-2901]  Tagging DigitizationTests-01-01-70. (DigitizationTests-01-01-70)
        * Rationalised the tests, removing many redundant sections to reduce resource usage.
        * Kept reference of tests in test/DigitizationTests_TestConfiguration_old.xml
        * [ATLASSIM-2901]
        * Tagging DigitizationTests-01-01-70
    
    2016-06-06 Iain Bertram <iain.bertram@cern.ch>
        * Added new set of reference files for 20.20 release
        * Tagging DigitizationTests-01-01-69
    
    
    Former-commit-id: 8eb4144b4f8f54254a80ff3586a989f2c3701681

--- Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
+++ Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
@@ -4,7 +4,7 @@ export DigitizationTestsVersion="dev"
 if [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then 
     DigitizationTestsVersion="20.7.X"
 elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then 
-    DigitizationTestsVersion="20.7.X"
+    DigitizationTestsVersion="20.20.X"
 elif [[ $ATLAS_RELEASE_BASE == *"mig7"* ]]; then
     DigitizationTestsVersion="20.3"
 elif [[ $ATLAS_RELEASE_BASE == *"20.3"* ]]; then
