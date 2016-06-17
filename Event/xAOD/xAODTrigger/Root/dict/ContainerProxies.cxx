9460638ba262f51f7e7d59024613e0fac5374bb7
Attila Krasznahorkay <krasznaa@cern.ch>
2016-06-17 14:27:39 +0200

    Adding a collection proxy for xAOD::TrigPassBitsContainer (xAODTrigger-00-00-47-01)

--- Event/xAOD/xAODTrigger/Root/dict/ContainerProxies.cxx
+++ Event/xAOD/xAODTrigger/Root/dict/ContainerProxies.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: ContainerProxies.cxx 635056 2014-12-10 15:35:39Z watsona $
+// $Id: ContainerProxies.cxx 755798 2016-06-17 14:27:12Z krasznaa $
 
 // EDM include(s):
 #include "xAODCore/AddDVProxy.h"
@@ -17,8 +17,8 @@
 #include "xAODTrigger/versions/MuonRoIContainer_v1.h"
 
 #include "xAODTrigger/versions/TrigCompositeContainer_v1.h"
-
 #include "xAODTrigger/versions/BunchConfContainer_v1.h"
+#include "xAODTrigger/versions/TrigPassBitsContainer_v1.h"
 
 // Set up the collection proxies:
 ADD_NS_DV_PROXY( xAOD, TriggerMenuContainer_v1 );
@@ -30,5 +30,5 @@ ADD_NS_DV_PROXY( xAOD, JetRoIContainer_v2 );
 ADD_NS_DV_PROXY( xAOD, MuonRoIContainer_v1 );
 
 ADD_NS_DV_PROXY( xAOD, TrigCompositeContainer_v1 );
-
 ADD_NS_DV_PROXY( xAOD, BunchConfContainer_v1 );
+ADD_NS_DV_PROXY( xAOD, TrigPassBitsContainer_v1 );
