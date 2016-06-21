eba38078d6dff4bfda3acdc42cc5e138702b9914
Alexander Naip Tuna <alexander.tuna@cern.ch>
2016-06-21 13:39:17 +0200

    see changelog (MuonTrackMonitoring-04-02-22)
    	* make plots vs LB easier to configure, extend to 2500 LBs

--- MuonSpectrometer/MuonValidation/MuonDQA/MuonTrackMonitoring/MuonTrackMonitoring/RecoLumiPlots.h
+++ MuonSpectrometer/MuonValidation/MuonDQA/MuonTrackMonitoring/MuonTrackMonitoring/RecoLumiPlots.h
@@ -24,53 +24,17 @@ class RecoLumiPlots:public PlotBase {
             void fill_Other(const xAOD::MuonContainer* Muons, int m_current_lb, float m_inst_lumi_bcid, float m_inst_lumi_lb);
             void fill(std::vector<std::pair<const xAOD::Muon*, const xAOD::Muon*> > resonances, int m_current_lb, float m_inst_lumi_bcid, float m_inst_lumi_lb);
 
-            //set the useful varaibles
-
-
-            // Set 1D hists for webdisplay
-
             TH1* m_hNSegment_LB_1D;
             TH1* m_hNMuonTrack_LB_1D;
             TH1* m_hNMuon_LB_1D;
             TH1* m_hNResonance_LB_1D;
-
-            // TH1* m_hNSegment_Inst_1D;
-            // TH1* m_hNMuonTrack_Inst_1D;
-            // TH1* m_hNMuon_Inst_1D;
-            // TH1* m_hNResonance_Inst_1D;
-
-            // TH1* m_hNSegment_IntLumi_1D;
-            // TH1* m_hNMuonTrack_IntLumi_1D;
-            // TH1* m_hNMuon_IntLumi_1D;
-            // TH1* m_hNResonance_IntLumi_1D;
-
-            // TH1* m_hNSegment_LB_BA_1D;
-            // TH1* m_hNSegment_LB_BC_1D;
-            // TH1* m_hNSegment_LB_EA_1D;
-            // TH1* m_hNSegment_LB_EC_1D;
-
-            // Set 2D hists for development
 
             TH2* m_hNSegment_LB;
             TH2* m_hNMuonTrack_LB;
             TH2* m_hNMuon_LB;
             TH2* m_hNResonance_LB;
 
-            // TH2* m_hNSegment_Inst;
-            // TH2* m_hNMuonTrack_Inst;
-            // TH2* m_hNMuon_Inst;
-            // TH2* m_hNResonance_Inst;
-
-            // TH2* m_hNSegment_IntLumi;
-            // TH2* m_hNMuonTrack_IntLumi;
-            // TH2* m_hNMuon_IntLumi;
-            // TH2* m_hNResonance_IntLumi;
-
-            // TH2* m_hNSegment_LB_BA;
-            // TH2* m_hNSegment_LB_BC;
-            // TH2* m_hNSegment_LB_EA;
-            // TH2* m_hNSegment_LB_EC;
-
+            int n_lbs;
 
             //for locating Z and Jpsi
             std::string type;
