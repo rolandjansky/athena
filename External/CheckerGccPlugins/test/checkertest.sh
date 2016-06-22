485476061eaf8870c47859c166e181149883ecc0
Attila Krasznahorkay <krasznaa@cern.ch>
2016-06-22 14:03:50 +0200

    Disabled the use of -Wl,--no-undefined for this package (CheckerGccPlugins-00-02-03)
    	* Explicitly disabled -Wl,--no-undefined for the linking of the
    	  checker_gccplugins library. As it can't be linked against all
    	  of the symbols that it uses.
    	* Tagging as CheckerGccPlugins-00-02-03
    
    2016-06-09  scott snyder  <snyder@bnl.gov>
    
    	* Tagging CheckerGccPlugins-00-02-02.
    	* Add a little extra output to the log files to try to figure out
    	why tests are failing in the cmt builds.
    
    
    Former-commit-id: fd00e47c102ccc3f456a31edcefacc0b68178d8f

--- External/CheckerGccPlugins/test/checkertest.sh
+++ External/CheckerGccPlugins/test/checkertest.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+echo '-->' $cc $cppflags $cppdebugflags -frtti -c $CheckerGccPlugins_args  -fplugin-arg-libchecker_gccplugins-checkers=all  $1
 $cc $cppflags $cppdebugflags -frtti -c $CheckerGccPlugins_args  -fplugin-arg-libchecker_gccplugins-checkers=all  $1
 
