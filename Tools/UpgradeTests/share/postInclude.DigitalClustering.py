beaa356eaea84d26f862da83172bb9816ca6d388
Jacob Martin Pasner <jake.pasner@cern.ch>
2016-06-22 06:59:04 +0200

    Made changes Simon requested to fix errors he noticed (run numbers, condition tag, preExec formatting) (UpgradeTests-00-00-08)
    	* Made changes Simon requested to fix errors he noticed (run numbers,
    	condition tag, preExec formatting)
    
    2016-06-21 Jacob Pasner <jake.pasner@cern.ch>
    	* Updated geometries for new tests to correspond to the new tags
    	ATLAS-P2-SFCAL-01-06-01 and ATLAS-P2-SFCAL-01-08-01 (forgot to
    	change -00 to -01 before)
    
    2016-06-20 Jacob Pasner <jake.pasner@cern.ch>
    	* Changed maxEvent=1 to 6 for new ExtBrl4 and InclBrl4 tests since we have 3
    	athenaMP nodes.
    
    2016-06-20 Jacob Pasner <jake.pasner@cern.ch>
    	* Added two test chains for global integration of sFCal with 1)GMXExtBrl4
    	2)GMXInclBrl4
    	* Removed 'dump-athfile.py SLHCtest.sFCal.HITS.pool.root' test from
    	DigisFCal job
    
    2016-05-26 Jacob Pasner <jake.pasner@cern.ch>
    	* Added 'dump-athfile.py SLHCtest.sFCal.HITS.pool.root' to the DigisFCal job
    ...
    (Long ChangeLog diff - truncated)

--- Tools/UpgradeTests/share/postInclude.DigitalClustering.py
+++
@@ -1,4 +0,0 @@
-ToolSvc.InDetPixelClusterOnTrackTool.PositionStrategy=0
-ToolSvc.InDetPixelClusterOnTrackTool.ErrorStrategy=1
-ToolSvc.InDetSCT_ClusterOnTrackTool.CorrectionStrategy=0
-ToolSvc.InDetSCT_ClusterOnTrackTool.ErrorStrategy=0
