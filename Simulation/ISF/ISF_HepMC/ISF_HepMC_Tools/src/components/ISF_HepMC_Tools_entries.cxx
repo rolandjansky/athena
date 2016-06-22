3d720206ac6e2eab51fc1894038cc11f0b4a0546
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-22 12:18:09 +0200

    python/ISF_HepMC_ToolsConfig.py (getStackFiller): fix syntax problem. Tag as ISF_HepMC_Tools-01-00-02 (ISF_HepMC_Tools-01-00-02)
    	* python/ISF_HepMC_ToolsConfig.py (getStackFiller): fix another
    	syntax problem...
    	* Tag as ISF_HepMC_Tools-01-00-02
    
    2016-06-22  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ISF_HepMC_ToolsConfig.py (getStackFiller): fix syntax
    	problem.
    	* Tag as ISF_HepMC_Tools-01-00-01
    
    2016-06-16  John Chapman  <John.Chapman@cern.ch>
    	* remove src/GenEventValidityChecker.h,
    	src/GenEventValidityChecker.cxx ,
    	src/CrabKissingVertexPositioner.h,
    	src/CrabKissingVertexPositioner.cxx,
    	src/LongBeamspotVertexPositioner.h,
    	src/LongBeamspotVertexPositioner.cxx ,
    	src/VertexBeamCondPositioner.h, src/VertexBeamCondPositioner.cxx ,
    	src/GenEventVertexPositioner.h, src/GenEventVertexPositioner.cxx ,
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: f909ad27b8cfa0fb56ca764cd9b3093d9af7eacd

--- Simulation/ISF/ISF_HepMC/ISF_HepMC_Tools/src/components/ISF_HepMC_Tools_entries.cxx
+++ Simulation/ISF/ISF_HepMC/ISF_HepMC_Tools/src/components/ISF_HepMC_Tools_entries.cxx
@@ -1,11 +1,4 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "../GenEventValidityChecker.h"
-#include "../GenEventBeamEffectBooster.h"
-#include "../GenEventVertexPositioner.h"
-#include "../VertexBeamCondPositioner.h"
-#include "../LongBeamspotVertexPositioner.h"
-#include "../CrabKissingVertexPositioner.h"
-#include "../VertexPositionFromFile.h"
 #include "../GenEventStackFiller.h"
 #include "../GenParticleGenericFilter.h"
 #include "../GenParticleFinalStateFilter.h"
@@ -17,13 +10,6 @@
 #include "../GenParticleSimWhiteList.h"
 #include "../GenParticleInteractingFilter.h"
 
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenEventValidityChecker      )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenEventBeamEffectBooster    )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenEventVertexPositioner     )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , VertexBeamCondPositioner     )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , LongBeamspotVertexPositioner )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , CrabKissingVertexPositioner )
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF , VertexPositionFromFile       )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenEventStackFiller          )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenParticleGenericFilter     )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenParticleFinalStateFilter  )
@@ -34,23 +20,3 @@ DECLARE_NAMESPACE_TOOL_FACTORY( ISF , ValidationTruthStrategy      )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , LLPTruthStrategy             )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenParticleSimWhiteList      )
 DECLARE_NAMESPACE_TOOL_FACTORY( ISF , GenParticleInteractingFilter )
-
-DECLARE_FACTORY_ENTRIES( ISF_HepMC_Tools ) {
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenEventValidityChecker      )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenEventBeamEffectBooster    )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenEventVertexPositioner     )
-  DECLARE_NAMESPACE_TOOL( ISF ,  VertexBeamCondPositioner     )
-  DECLARE_NAMESPACE_TOOL( ISF ,  LongBeamspotVertexPositioner )
-  DECLARE_NAMESPACE_TOOL( ISF ,  CrabKissingVertexPositioner )
-  DECLARE_NAMESPACE_TOOL( ISF ,  VertexPositionFromFile       )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenEventStackFiller          )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenParticleGenericFilter     )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenParticleFinalStateFilter  )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenParticlePositionFilter    )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenericTruthStrategy         )
-  DECLARE_NAMESPACE_TOOL( ISF ,  CylinderVolumeTruthStrategy  )
-  DECLARE_NAMESPACE_TOOL( ISF ,  ValidationTruthStrategy      )
-  DECLARE_NAMESPACE_TOOL( ISF ,  LLPTruthStrategy             )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenParticleSimWhiteList      )
-  DECLARE_NAMESPACE_TOOL( ISF ,  GenParticleInteractingFilter )
-}
