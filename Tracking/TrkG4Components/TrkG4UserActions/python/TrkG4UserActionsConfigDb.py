38d44cac348565173e41c26012bb2bf4df2b6c73
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-23 16:37:01 +0200

    Added getter for MaterialStepRecorder, and updated GeantinoMapping_jobOptions.py (matches TrkG4UserActions-01-00-00-05 updates). Tagging TrkG4UserActions-02-00-02 (TrkG4UserActions-02-00-02)
    	* added getter for MaterialStepRecorder, and updated
    	GeantinoMapping_jobOptions.py (matches
    	TrkG4UserActions-01-00-00-05 updates)
    	* tagging TrkG4UserActions-02-00-02

--- Tracking/TrkG4Components/TrkG4UserActions/python/TrkG4UserActionsConfigDb.py
+++ Tracking/TrkG4Components/TrkG4UserActions/python/TrkG4UserActionsConfigDb.py
@@ -1,6 +1,9 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.CfgGetter import addTool
+addTool("TrkG4UserActions.TrkG4UserActionsConfig.getMaterialStepRecorderTool", "G4UA::MaterialStepRecorderTool")
+
+from AthenaCommon.CfgGetter import addTool
 addTool("TrkG4UserActions.TrkG4UserActionsConfig.getMaterialStepRecorder",  "MaterialStepRecorder"  )
 addTool("TrkG4UserActions.TrkG4UserActionsConfig.getPositionMomentumWriter","PosMomWriter"          )
 addTool("TrkG4UserActions.TrkG4UserActionsConfig.getEnergyLossRecorder"  ,  "EnergyLossRecorder"    )
