5250bbcecaa9f44f66ec708186895b27a4c02e59
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-24 16:08:24 +0200

    CMakeLists.txt - fix typo in link library names. ATLASSIM-2870. Tagging ForwardRegionMgField-01-00-03 (ForwardRegionMgField-01-00-03)
    	* CMakeLists.txt - fix typo in link library names. ATLASSIM-2870
    	* Tagging ForwardRegionMgField-01-00-03
    
    2016-05-23  John Chapman  <John.Chapman@cern.ch>
    
    	* src/ForwardRegionFieldSvc.cxx (ForwardRegionFieldSvc): fix
    	initial setting of m_magnet member variable was set to CLHEP::m by
    	mistake. ATLASSIM-2795
    	* Tagging ForwardRegionMgField-01-00-02
    
    2016-05-17  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ForwardRegionMgFieldConfig.py - fix configuration for Q1,
    	Q2 and Q3 fields. Output now matches old code! ATLASSIM-2798
    	* Tagging ForwardRegionMgField-01-00-01
    
    2016-05-17  John Chapman  <John.Chapman@cern.ch>
    
    	* Migrating away from FadsField. ATLASSIM-2798
    ...
    (Long ChangeLog diff - truncated)

---
+++ ForwardDetectors/ForwardSimulation/ForwardRegionMgField/python/ForwardRegionMgFieldConfigDb.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addService
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ1FieldSvc", "Q1")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ2FieldSvc", "Q2")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ3FieldSvc", "Q3")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getD1FieldSvc", "D1")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getD2FieldSvc", "D2")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ4FieldSvc", "Q4")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ5FieldSvc", "Q5")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ6FieldSvc", "Q6")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ7FieldSvc", "Q7")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ1hkickFieldSvc", "Q1HKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ1vkickFieldSvc", "Q1VKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ2hkickFieldSvc", "Q2HKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ2vkickFieldSvc", "Q2VKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ3hkickFieldSvc", "Q3HKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ3vkickFieldSvc", "Q3VKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ4vkickAFieldSvc", "Q4VKickA")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ4hkickFieldSvc", "Q4HKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ4vkickBFieldSvc", "Q4VKickB")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ5hkickFieldSvc", "Q5HKick")
+addService("ForwardRegionMgField.ForwardRegionMgFieldConfig.getQ6vkickFieldSvc", "Q6VKick")
