f5e9628c19e7b57133571280c432cbe22e5084a7
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-24 12:18:22 +0200

    CMakeLists.txt: Fix syntax problems. ATLASSIM-2870. Tagging BeamEffects-00-00-08 (BeamEffects-00-00-08)
    Former-commit-id: 2c2a559c1e1d49cd19812f23841b1bc12c79a114

---
+++ Simulation/BeamEffects/src/GenEventVertexPositioner.h
@@ -0,0 +1,54 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// GenEventVertexPositioner.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+#ifndef BEAMEFFECTS_GENEVENTVERTEXPOSITIONER_H
+#define BEAMEFFECTS_GENEVENTVERTEXPOSITIONER_H 1
+
+// FrameWork includes
+#include "GaudiKernel/ToolHandle.h"
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "HepMC_Interfaces/IGenEventManipulator.h"
+
+
+namespace Simulation {
+
+  // forward declarations
+  class ILorentzVectorGenerator;
+
+  /** @class GenEventVertexPositioner
+
+      This tool takes a HepMC::GenEvent and applies geometrical modifications,
+      such as random vertex smearing, beam tilt, etc.
+
+      based on:
+      https://svnweb.cern.ch/trac/atlasoff/browser/Simulation/G4Atlas/G4AtlasUtilities/trunk/src/VertexPositioner.cxx
+
+      @author Andreas.Salzburger -at- cern.ch , Elmar.Ritsch -at- cern.ch
+     */
+  class GenEventVertexPositioner : public AthAlgTool,
+                                   virtual public IGenEventManipulator {
+
+    public:
+      /** Constructor with parameters */
+      GenEventVertexPositioner( const std::string& t, const std::string& n, const IInterface* p );
+
+      /** Athena algtool's Hooks */
+      StatusCode  initialize() override final;
+      StatusCode  finalize() override final;
+
+      /** modifies (displaces) the given GenEvent */
+      StatusCode  manipulate(HepMC::GenEvent& ge) const override final;
+
+    private:
+      /** Vertex Shifters applied in the given order */
+      ToolHandleArray<ILorentzVectorGenerator> m_vertexShifters;
+  };
+
+}
+
+#endif //> !BEAMEFFECTS_GENEVENTVERTEXPOSITIONER_H
