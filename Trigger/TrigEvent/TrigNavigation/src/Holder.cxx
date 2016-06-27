cf910f0ccd2e65535e35e1f8b31bd9852c4f10f0
Frank Winklmeier <Frank.Winklmeier@cern.ch>
2016-06-27 12:15:39 +0200

    Use memoize pattern for formatSGKey (ATR-14439), use macros for messaging (TrigNavigation-00-06-11-02)
    	* src/Holder.cxx: Use memoize pattern for formatSGKey (ATR-14439), use macros for messaging
    	* TrigNavigation-00-06-11-02

--- Trigger/TrigEvent/TrigNavigation/src/Holder.cxx
+++ Trigger/TrigEvent/TrigNavigation/src/Holder.cxx
@@ -15,6 +15,8 @@
 
 using namespace HLTNavDetails;
 
+#define MLOG(x)   if (m_log->level()<=MSG::x) *m_log << MSG::x 
+
 /*****************************************************************************
  *
  * INNER CLASS (fatures handling)
@@ -42,8 +44,8 @@ void IHolder::prepare(MsgStream* log, HLT::AccessProxy* sg, IConversionSvc* objS
 }
 
 bool IHolder::serialize(std::vector<uint32_t>& output)  const {
-  if ( m_log->level()<=MSG::DEBUG )
-    *m_log << MSG::DEBUG << "Holder<T> serialize, " << *this  << endreq;
+
+  MLOG(DEBUG) << "Holder<T> serialize, " << *this  << endreq;
   // if feature to be forgotten then indication of it is simply 0 size of serialized vector
 
   output.push_back( typeClid() );
@@ -61,13 +63,13 @@ bool IHolder::serialize(std::vector<uint32_t>& output)  const {
 bool IHolder::serializeWithPayload(std::vector<uint32_t>& output, size_t& payloadsize) {
   bool header_ser = serialize(output);
   if(!header_ser){
-    *m_log << MSG::ERROR << " header serialization failed " << endreq;
+    MLOG(ERROR) << " header serialization failed " << endreq;
     return false;
   }
   std::vector<uint32_t> payload;
   bool payload_ser = serializePayload(payload);
   if(!payload_ser){
-    *m_log << MSG::ERROR << " payload serialization failed " << endreq;
+    MLOG(ERROR) << " payload serialization failed " << endreq;
     return false;
   }
   payloadsize = payload.size();
@@ -79,8 +81,7 @@ std::string IHolder::generateAliasKey(CLID c, uint16_t sti, const std::string& l
   std::ostringstream ss;
   ss <<  "HLTAutoKey_" << label << "_" << c << "_" << sti << "_" << (size == 0xffffffff ? "back" : "to")  << "_" << m_uniqueCounter;
   std::string st = ss.str();
-  if ( m_log->level()<=MSG::DEBUG )
-    *m_log << MSG::DEBUG << "IHolder::generateAliasKey generated key: " << st << endreq;
+  MLOG(DEBUG) << "IHolder::generateAliasKey generated key: " << st << endreq;
   m_uniqueCounter++;
   return st;
 }
@@ -95,20 +96,34 @@ MsgStream& HLTNavDetails::operator<< ( MsgStream& m, const HLTNavDetails::IHolde
   return m;
 }
 
-std::string HLTNavDetails::formatSGkey(const std::string& prefix, const std::string& containername, const std::string& label){
-  std::string cleaned = containername;
+// only construct the regex once
+namespace HLTNavDetails {  
   boost::regex rx1("_v[0-9]+$");
-  cleaned = boost::regex_replace(cleaned,rx1,std::string(""));
-  boost::regex rx2("::");
-  cleaned = boost::regex_replace(cleaned,rx2,std::string("__"));
-  std::string ret = prefix+"_"+cleaned;
-  if (label == "Aux.")
-    return ret + label;
-  else if (!label.empty())
-    return ret + "_" + label;
-  return ret;
 }
 
+std::string HLTNavDetails::formatSGkey(const std::string& prefix, const std::string& containername, const std::string& label){
+  // Memoize already used keys
+  static std::map<std::string,std::string> memo;
+  std::string key = prefix+containername+label;
+  if (memo.count(key)) return memo[key];
+
+  // Remove version
+  std::string ret = boost::regex_replace(containername,rx1,std::string(""));
+  
+  // Replace :: with __
+  size_t pos = ret.find("::");
+  if (pos!=std::string::npos) ret.replace(pos,2,"__");
+
+  // Add prefix and label
+  ret = prefix + "_" + ret;
+  if (label == "Aux.")
+    ret += label;
+  else if (!label.empty())
+    ret += ("_" + label);
+
+  memo[key] = ret;
+  return ret;
+}
 
 bool HLTNavDetails::IHolder::deserializePayload(const std::vector<uint32_t>& dataBlob, int version){
   const std::string sgkey = key();
@@ -117,16 +132,16 @@ bool HLTNavDetails::IHolder::deserializePayload(const std::vector<uint32_t>& dat
   const std::string container_typename = collectionName();
 
   if ( m_storeGate->transientContains(container_clid, sgkey) ) {
-    *m_log << MSG::VERBOSE << "deserialize: while working on: " << container_typename << " and key: " << sgkey
-	   << " from serialized form found it in SG already, sync with it" << endreq;
+    MLOG(VERBOSE) << "deserialize: while working on: " << container_typename << " and key: " << sgkey
+                  << " from serialized form found it in SG already, sync with it" << endreq;
     syncWithSG();
   }
 
-  *m_log << MSG::VERBOSE << "deserializing a data blob of size " << dataBlob.size() << " navi version is " << version;
-  *m_log << MSG::DEBUG << "holder type clid " << feature_clid<< " collection clid: " << container_clid << endreq;
+  MLOG(VERBOSE) << "deserializing a data blob of size " << dataBlob.size() << " navi version is " << version;
+  MLOG(DEBUG) << "holder type clid " << feature_clid<< " collection clid: " << container_clid << endreq;
 
   if (container_clid<1){
-    *m_log << MSG::WARNING << "holder type clid " << feature_clid << " collection clid: " << container_clid << endreq;
+    MLOG(WARNING) << "holder type clid " << feature_clid << " collection clid: " << container_clid << endreq;
   }  
   
   typedef std::vector<uint32_t>::const_iterator it_type;
@@ -138,18 +153,18 @@ bool HLTNavDetails::IHolder::deserializePayload(const std::vector<uint32_t>& dat
 
   if(version == 4){
     auto firstsize = *(it++);
-    *m_log << MSG::VERBOSE << "first part has size: " << firstsize;
+    MLOG(VERBOSE) << "first part has size: " << firstsize;
   
     first = std::vector<uint32_t>(it,it+firstsize);
   
     std::advance(it,firstsize);  
   
     if(!(it!=dataBlob.end())){
-      *m_log << MSG::VERBOSE << "this datablob only has a first part (non xAOD case)";
+      MLOG(VERBOSE) << "this datablob only has a first part (non xAOD case)";
     }
     else{
       auto secondsize = *(it++);
-      *m_log << MSG::VERBOSE << "second part has size: " << secondsize;
+      MLOG(VERBOSE) << "second part has size: " << secondsize;
       second = std::vector<uint32_t>(it,it+secondsize);
     }
   }
@@ -162,7 +177,7 @@ bool HLTNavDetails::IHolder::deserializePayload(const std::vector<uint32_t>& dat
   DataObject* dobj(0);
 
   if (m_objectserializerSvc->createObj((IOpaqueAddress*)&addr, dobj).isFailure() ){
-    *m_log << MSG::WARNING << "deserialize main: failed" << std::endl;
+    MLOG(WARNING) << "deserialize main: failed" << std::endl;
     return false;
   }
 
@@ -172,12 +187,12 @@ bool HLTNavDetails::IHolder::deserializePayload(const std::vector<uint32_t>& dat
   DataObject* dobjaux(0);
   if(!second.empty()){
     std::string sgkeyaux = sgkey+"Aux.";
-    *m_log << MSG::VERBOSE << "aux clid was deduced to be: " << auxClidOrZero();
+    MLOG(VERBOSE) << "aux clid was deduced to be: " << auxClidOrZero();
     TrigStreamAddress auxaddr(auxClidOrZero(), sgkeyaux, "", 0, 0);
     auxaddr.add(second);
 
     if (m_objectserializerSvc->createObj((IOpaqueAddress*)&auxaddr, dobjaux).isFailure() ){
-      *m_log << MSG::WARNING << "Aux Store deserialization failed";
+      MLOG(WARNING) << "Aux Store deserialization failed";
       return false;
     }
     bool setaux = setAuxDataObject(dobjaux);
@@ -190,56 +205,56 @@ bool HLTNavDetails::IHolder::serializePayload(std::vector<uint32_t>& dataBlob) {
   DataObject* dobj = getDataObject();
   
   if(!dobj){
-    *m_log << MSG::WARNING << "data object points to null. can't serialize" << endreq;
+    MLOG(WARNING) << "data object points to null. can't serialize" << endreq;
   }
 
-  *m_log << MSG::VERBOSE << "creating serialized representation for " << dobj << endreq;
+  MLOG(VERBOSE) << "creating serialized representation for " << dobj << endreq;
 
 
   TrigStreamAddress* addr(0);
   if ( m_objectserializerSvc->createRep(dobj, *pp_cast<IOpaqueAddress>(&addr) ).isSuccess() ) {
-    *m_log << MSG::VERBOSE << "serialization of feature (object) successful, blob size: "  << addr->get().size() << endreq;
+    MLOG(VERBOSE) << "serialization of feature (object) successful, blob size: "  << addr->get().size() << endreq;
     dataBlob.push_back(addr->get().size());
     dataBlob.insert(dataBlob.end(), addr->get().begin(), addr->get().end());
     // above should be optimized (need to know if in case of failed serialization addr->get() contains empty data vector)
   }
   else{
-    *m_log << MSG::ERROR << "Container serialization Failure" << endreq;
+    MLOG(ERROR) << "Container serialization Failure" << endreq;
     return false;
   }
   DataBucketBase* dobjBase = static_cast<DataBucketBase*>(dobj);
   if ( dobjBase ) {
     dobjBase->relinquish();
-    *m_log << MSG::VERBOSE << "serialization giving up the ownership" << endreq;;
+    MLOG(VERBOSE) << "serialization giving up the ownership" << endreq;;
   }
   delete dobj;
   delete addr;
   
   DataObject* dobjaux = getAuxDataObject();
 
-  *m_log << MSG::VERBOSE << "aux object: " << dobjaux << endreq;;
+  MLOG(VERBOSE) << "aux object: " << dobjaux << endreq;;
 
   TrigStreamAddress* auxaddr(0);
   if(dobjaux){
-    *m_log << MSG::VERBOSE << "AuxStore serialization" << endreq;
+    MLOG(VERBOSE) << "AuxStore serialization" << endreq;
     if(m_objectserializerSvc->createRep(dobjaux,*pp_cast<IOpaqueAddress>(&auxaddr) ).isSuccess() ){
-      *m_log << MSG::VERBOSE << "aux conversion success! aux blob has size: " << auxaddr->get().size() << endreq;
+      MLOG(VERBOSE) << "aux conversion success! aux blob has size: " << auxaddr->get().size() << endreq;
       dataBlob.push_back(auxaddr->get().size());
       dataBlob.insert(dataBlob.end(), auxaddr->get().begin(), auxaddr->get().end());
     }
     else{
-      *m_log << MSG::ERROR << "AuxStore serialization Failure" << endreq;
+      MLOG(ERROR) << "AuxStore serialization Failure" << endreq;
       return false;
     }
     DataBucketBase* dobjBaseAux = static_cast<DataBucketBase*>(dobjaux);
     if ( dobjBaseAux ) {
       dobjBaseAux->relinquish();
-      *m_log << MSG::VERBOSE << "serialization giving up the ownership of Aux" << endreq;
+      MLOG(VERBOSE) << "serialization giving up the ownership of Aux" << endreq;
     }
     delete dobjaux;
     delete auxaddr;
   }
-  *m_log << MSG::VERBOSE << "finished serializing payload. size: " << dataBlob.size() << endreq;
+  MLOG(VERBOSE) << "finished serializing payload. size: " << dataBlob.size() << endreq;
 
   return true;
 }
