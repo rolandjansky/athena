02482e35c58f4128179604c9bf883a831ae6ac49
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-06-28 16:04:21 +0200

    share/skeleton.EVGENtoHIT.py: - set simFlags.IsEventOverlayInputSim (if available) if runargs.inputTXT_EVENTIDFile is present. ATLASSIM-2913. Tagging: SimuJobTransforms-00-31-08 (SimuJobTransforms-00-31-08)
    	* share/skeleton.EVGENtoHIT.py: - set
    	simFlags.IsEventOverlayInputSim (if available) if
    	runargs.inputTXT_EVENTIDFile is present. ATLASSIM-2913
    	* Tagging: SimuJobTransforms-00-31-08
    
    2016-06-22  John Chapman  <John.Chapman@cern.ch>
    
    	* share/skeleton.TestBeam.py: update so that the output matches
    	the most recent TileSimEx/jobOptions_TileTB_Sim.py
    	job. ATLASSIM-2912
    	* Tagging as SimuJobTransforms-00-31-07
    
    2016-06-21  John Chapman  <John.Chapman@cern.ch>
    
    	* add share/skeleton.RDOtoHIST_DIGI.py, scripts/DigiValid_tf.py -
    	for RDO file validation. ATLASSIM-1976
    	* python/simTrfArgs.py - add arguments for RDOtoHIST_DIGI
    	step. ATLASSIM-1976
    	* python/SimTransformUtils.py - add configuration for
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 943efda13a3800ad2a66b0a8035b55acc6732f7b

--- Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT.py
+++ Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT.py
@@ -8,3 +8,8 @@ if hasattr(runArgs, "jobNumber"):
 if hasattr(runArgs, "inputTXT_EVENTIDFile"):
     from OverlayCommonAlgs.OverlayFlags import overlayFlags
     overlayFlags.EventIDTextFile = runArgs.inputTXT_EVENTIDFile[0]
+    from G4AtlasApps.SimFlags import simFlags
+    simFlags.load_atlas_flags()
+    if hasattr(simFlags, 'IsEventOverlayInputSim'):
+        simFlags.IsEventOverlayInputSim = True
+
