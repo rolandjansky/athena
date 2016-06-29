6e42a85390b122bb86f4c554caf7082a181d122a
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2016-06-29 12:46:39 +0200

    29 June 2016 Mark Hodgkinson Update to use PFOAlgPropertyPlots Tag as PFODQA-00-00-11 (PFODQA-00-00-11)
    Former-commit-id: ed9a9db7836ddca9f7f8482cfb04d87bfa7489d7

--- Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFOValidationPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFOValidationPlots.cxx
@@ -7,7 +7,8 @@
 PFOValidationPlots::PFOValidationPlots(PlotBase* pParent, std::string sDir,  std::string sPFOContainerName) : PlotBase(pParent, sDir),
 													     m_PFOPlots(this,"PFO_JetETMiss/",sPFOContainerName),
 													     m_PFOClusterMomentPlots(this,"PFO_JetETMiss/",sPFOContainerName),
-													     m_PFOAttributePlots(this,"PFO_JetETMiss/",sPFOContainerName), 
+													     m_PFOAttributePlots(this,"PFO_JetETMiss/",sPFOContainerName),
+													     m_PFOAlgPropertyPlots(this,"PFO_JetETMiss/",sPFOContainerName),
 													     m_PFOEMPlots(this,"PFO_JetETMiss/",sPFOContainerName)
 {}
 
@@ -15,6 +16,7 @@ void PFOValidationPlots::fill(const xAOD::PFO& thePFO, bool fillEMHistograms){
   m_PFOPlots.fill(thePFO);
   if (0.0 == thePFO.charge()) m_PFOClusterMomentPlots.fill(thePFO);
   if (0.0 == thePFO.charge()) m_PFOAttributePlots.fill(thePFO);
+  else  m_PFOAlgPropertyPlots.fill(thePFO);
   if (true == fillEMHistograms) m_PFOEMPlots.fill(thePFO);
 }
 
