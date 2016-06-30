0073efc4bf98613012e8b1172fe0506cd4f5c6e0
Iain Bertram <iain.bertram@cern.ch>
2016-06-30 09:06:29 +0200

    Added 20.7.X.Y-VAL set of reference files as these differ from 20.7.X Tagging DigitizationTests-01-01-72 (DigitizationTests-01-01-72)
        * Added 20.7.X.Y-VAL set of reference files as these differ from 20.7.X
        * Tagging DigitizationTests-01-01-72
    
    2016-06-27 Iain Bertram <iain.bertram@cern.ch>
        * Removed LVL1 tests.
        * [ATLASSIM-2901]
        * Tagging DigitizationTests-01-01-71
    
    
    Former-commit-id: f0a264de6c1a3896cc9fe7f763039ec8b4ac56fb

--- Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
+++ Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 
 export DigitizationTestsVersion="dev"
-if [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then 
+if [[ $ATLAS_RELEASE_BASE == *"20.7.X.Y-VAL"* ]]; then
+    DigitizationTestsVersion="20.7.X.Y-VAL"
+elif [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then
     DigitizationTestsVersion="20.7.X"
-elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then 
+elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then
     DigitizationTestsVersion="20.20.X"
 elif [[ $ATLAS_RELEASE_BASE == *"mig7"* ]]; then
     DigitizationTestsVersion="20.3"
