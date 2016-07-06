751feac1ad221d147c23ebf0c112b6c0508d1e88
Jahred Adelman <jahreda@gmail.com>
2016-07-06 21:10:29 +0200

    update tower data as per jordan (TrigFTK_RawData-01-00-23)
    	* Tag as TrigFTK_RawData-01-00-23
    	* As per Jordan W, update FTK_RawTrack to save tower data
    
    2016-05-27 Jahred Adelman <jahreda@gmail.com>
    	* FTK_RawPixelCluster.cxx: Widths saved starting at 0
    	* FTK_RawSCT_Cluster.cxx: Widths saved starting at 0
    	* Tag as TrigFTK_RawData-01-00-22
    
    2016-05-18 Jahred Adelman <jahreda@gmail.com>
    	* FTK_RawPixelCluster.cxx: No scaling or multipliers
    	* FTK_RawSCT_Cluster.cxx: No scaling or multipliers
    	* Tag as TrigFTK_RawData-01-00-20
    
    2016-05-10  John Baines  <john.baines@stfc.ac.uk>
    
    	* TrigFTK_RawData/FTK_RawSCT_Cluster.h: remove redundant layer member
    	* TrigFTK_RawData/FTK_RawPixel_Cluster.h: remove redundant layer member
    	* TrigFTK_RawData/FTK_RawTrack.h: add methos isMissingLayer, isMissingPixelLayer, isMissingSCTLayer
    	* Tag as TrigFTK_RawData-01-00-18
    
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 1e9aa94c4111498999319f596f642fa1ddd2180f

--- Trigger/TrigFTK/TrigFTK_RawData/src/FTK_RawSCT_Cluster.cxx
+++ Trigger/TrigFTK/TrigFTK_RawData/src/FTK_RawSCT_Cluster.cxx
@@ -10,50 +10,54 @@
 using namespace std;
 
 FTK_RawSCT_Cluster::FTK_RawSCT_Cluster() :
-  m_word(0),
-  m_layer(FTK_RawSCT_Cluster::missingLayer),
+  m_word(0), 
   m_barcode(0)
 {
   // nothing to do
 }
 
-FTK_RawSCT_Cluster::FTK_RawSCT_Cluster(int i) :
-  m_word(0),
-  m_barcode(0)
-{
-  m_layer = i;
-}
 
-FTK_RawSCT_Cluster::FTK_RawSCT_Cluster(uint32_t word, signed long barcode, int layer)
+FTK_RawSCT_Cluster::FTK_RawSCT_Cluster(uint32_t word)
 {
   m_word        = word;
-  m_layer       = layer;
+  m_barcode     = 0;
+}
+
+FTK_RawSCT_Cluster::FTK_RawSCT_Cluster(uint32_t word, signed long barcode)
+{
+  m_word        = word;
   m_barcode     = barcode;
 }
+
 
 FTK_RawSCT_Cluster::~FTK_RawSCT_Cluster()
 {
   // nothing to do
 }
 
+  void FTK_RawSCT_Cluster::setWord(uint32_t word){ 
+    m_word = word;
+  }
+
 float FTK_RawSCT_Cluster::getHitCoord() const{
 
-  // TopBit 23 BottomBit 13  (11 bits) Max 2048 LeftShift 8 (=31-TopBit) RightShift 21 (=LeftShift+BottomBit)
+  // Hit coordinate is 11 bits, starting at bit 16
 
-  uint32_t hit = (m_word <<  8) >> 21; 
+  uint32_t hit = (m_word  & (0x7ff << 16)) >> 16;
 
-  return 0.5 * (float)hit;
-
+  //  return 0.5 * (float)hit;
+  return (float)hit;
 }
 
 void FTK_RawSCT_Cluster::setHitCoord( float hit_coord ){
 
   // TopBit 23 BottomBit 13  (11 bits) Max 2048 
 
-  uint32_t hit = uint32_t (hit_coord*2.);
+  uint32_t hit = uint32_t (hit_coord);
+  //  uint32_t hit = uint32_t (hit_coord*2.);
 
   if ( hit < 2048 ){
-    hit = hit << 13 ;
+    hit = hit << 16 ;
     m_word = (hit | m_word);
   }
   return;
@@ -61,23 +65,25 @@ void FTK_RawSCT_Cluster::setHitCoord( float hit_coord ){
 
 unsigned int FTK_RawSCT_Cluster::getHitWidth() const{
 
-  // TopBit 26 BottomBit 24  (3 bits) Max 8 LeftShift 5 (=31-TopBit) RightShift 29 (=LeftShift+BottomBit)
+  // width is 3 bits, starting at bit 28
+  unsigned int width = (m_word & 0x70000000)>> 28;
 
-  unsigned int width = (m_word << 5) >> 29;
-  width = width + 1;
+  // Store this as nominal (starting from 0)
+//  return (width+1);
+
   return width;
-
 }
 
 void FTK_RawSCT_Cluster::setHitWidth( unsigned int hit_width ){
 
   // TopBit 26 BottomBit 24  (3 bits) Max 8
 
+  // Store this as nominal (starting from 0)
+  // if (width>0) width-=1; // transform from width=1 to 8 to packed width 0-7
   uint32_t width = hit_width;
 
-  if(width > 8) width = 8;
-  width -=1;
-  width = width << 24;
+  if(width > 7) width = 7;
+  width = width << 28;
 
   m_word = (width | m_word);
   
@@ -85,154 +91,25 @@ void FTK_RawSCT_Cluster::setHitWidth( unsigned int hit_width ){
 
 }
 
+// JAA modified this to use 13 bits
 unsigned int FTK_RawSCT_Cluster::getModuleID() const{
   
-  // TopBit 12 BottomBit 0  (13 bits) Max 8192 LeftShift 19 (=31-TopBit) RightShift 19 (=LeftShift+BottomBit)
-
-  uint32_t module_id = m_word << 19;
-  module_id = module_id >> 19;
+  // First 13 bits
+  uint32_t module_id = (m_word & 0x1fff);
   return module_id;
 
 }
-
-
+// JAA modified this to use 13 bits
 void FTK_RawSCT_Cluster::setModuleID( unsigned int module_id ){
-
-  // TopBit 11 BottomBit 0  (13 bits) 
 
   uint32_t id = module_id;
 
   if (id <  8192) m_word = (id | m_word);
-
   
   return;
 }
 
 
-/*void FTK_RawSCT_Cluster::setHitStereo(float hit_stereo){
-
-  float multiplier = 1.;
-  float range_top  = 767.;
-  float range_bot  = 0.;
-  int   offset     = 0;
-  int   position   = 0;
-
-  uint32_t hit = 0;
-  if(hit_stereo > range_bot && hit_stereo < range_top)
-    hit = NINT( multiplier*hit_stereo + offset);
- 
-  hit = hit << position;
-  m_word = (hit | m_word);
-
-  return;
-  }*/
-
-/*void FTK_RawSCT_Cluster::setWidthStereo(float width_stereo){
-
-  float multiplier = 1.;
-  float range_top  = 9.;
-  float range_bot  = 0.;
-  int   offset     = 1;
-  int   position   = 12;
-
-  uint32_t width = 0;
-  if(width_stereo > range_bot && width_stereo < range_top)
-    width = NINT( multiplier*width_stereo + offset);
-
-  width = width << position;
-  m_word = (width | m_word);
-
-  return;
-}*/
-
-/*float FTK_RawSCT_Cluster::getHitStereo(){
-
-  float multiplier = 1.;
-  int   offset     = 0;
-  int   position   = 0;
-
-  uint16_t hit = 0;
-  hit = hit | (m_word >> position);
-  hit = hit << 5;
-  hit = hit >> 5; // Could probably do this in one line with a NOR or something.
-  //  std::cout <<"JWH DEBUG HIT = " << hit << std::endl;
-  float hit_stereo = (((float)(hit - offset))/multiplier);
-  return hit_stereo;
-  }*/
-
-/*float FTK_RawSCT_Cluster::getHitAxial(){
-
-  float multiplier = 1.;
-  int   offset     = 0;
-  int   position   = 16;
-
-  uint16_t hit = 0;
-  hit = hit | (m_word >> position);
-  hit = hit << 5;
-  hit = hit >> 5; // Could probably do this in one line with a NOR or something.
-
-  float hit_axial = (((float)(hit - offset))/multiplier);
-  return hit_axial;
-  }*/
-
-/*int FTK_RawSCT_Cluster::getWidthStereo(){
-
-  float multiplier = 1.;
-  int   offset     = 1;
-  int   position   = 12;
-
-  uint16_t width = 0;
-  width = width | (m_word >> position);
-  width = width << 13;
-  width = width >> 13; // Could probably do this in one line with a NOR or something.
-
-  int width_stereo = NINT(((float)(width - offset))/multiplier);
-  return width_stereo;
-  }*/
-
-/*int FTK_RawSCT_Cluster::getWidthAxial(){
-
-  float multiplier = 1.;
-  int   offset     = 1;
-  int   position   = 28;
-
-  uint16_t width = 0;
-  width = width | (m_word >> position);
-  width = width << 1;
-  width = width >> 1; //Dont actually need this since last bit will always be set to 0
-
-  int width_stereo = NINT(((float)(width - offset))/multiplier);
-  return width_stereo;
-  }*/
-
-/*unsigned int FTK_RawSCT_Cluster::getHashID(){
-
-  float multiplier = 1.;
-  int   offset     = 0;
-  int   position   = 0;
-
-  uint16_t hash = 0;
-  hash = hash | (m_word >> position);
-  hash = hash << 4;
-  hash = hash >> 4;
-
-  unsigned int hash_id = (unsigned int)hash;
-  return hash_id;
-  }*/
-
-/*void FTK_RawSCT_Cluster::setHashID(unsigned int hash_id){
-
-  float multiplier = 1.;
-  int   offset     = 0;
-  int   position   = 0;
-
-  uint32_t hash = 0;
-  hash = NINT( multiplier*hash_id + offset);
-
-  hash = hash << position;
-  m_word_header = (hash | m_word_header);
-  return;
-  }*/
 
 #if defined(__MAKECINT__)
 #pragma link C++ class FTK_RawSCT_Cluster;
