8f0ae89cfa2e3de6b273738c9640ec4dce4376ef
Jahred Adelman <jahreda@gmail.com>
2016-07-07 12:23:43 +0200

    tag move to AOD Vx (FTK_RecToolInterfaces-00-00-02)
    Former-commit-id: d6190366a70be7aaab2ab8267089411b8b900575

--- Trigger/TrigFTK/FTK_RecToolInterfaces/FTK_RecToolInterfaces/IFTK_VertexFinderTool.h
+++ Trigger/TrigFTK/FTK_RecToolInterfaces/FTK_RecToolInterfaces/IFTK_VertexFinderTool.h
@@ -7,15 +7,18 @@
 #ifndef __IFTK_VERTEXFINDER_TOOL_H__
 #define __IFTK_VERTEXFINDER_TOOL_H__
 
-#include "GaudiKernel/IAlgTool.h"
+#include "GaudiKernel/IAlgTool.h"	
+//#include "AthenaKernel/IOVSvcDefs.h"
 class VxContainer;
 #include "TrigFTK_RawData/FTK_RawTrack.h"
 #include "TrigFTK_RawData/FTK_RawTrackContainer.h"
-namespace Trk {
-  class Track;
-}
-
-
+#include "xAODTracking/VertexFwd.h"
+//#include "xAODTracking/TrackParticleFwd.h"
+#include "xAODTracking/VertexContainerFwd.h"
+//#include "xAODTracking/TrackParticleContainerFwd.h"
+//namespace Trk {
+////  class Track;
+//}
 static const InterfaceID IID_IFTK_VertexFinderTool("IFTK_VertexFinderTool",1,0);
 
 class IFTK_VertexFinderTool : virtual public IAlgTool { 
@@ -25,8 +28,9 @@ class IFTK_VertexFinderTool : virtual public IAlgTool {
   static const InterfaceID& interfaceID ()   //!< the Tool's interface
   {  return IID_IFTK_VertexFinderTool; }  	
 
-  virtual VxContainer* findVertex(const FTK_RawTrackContainer* trks) = 0;
-  
+ // virtual VxContainer* findVertex(const FTK_RawTrackContainer* trks) = 0;
+  virtual xAOD::VertexContainer* findVertex(const FTK_RawTrackContainer* trks) = 0;
+ private:
 };
 
 #endif
