40e677226bc9dca5d33790d3af80d941ce5824a3
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-12 12:42:03 +0200

    src/G4PolyconeGeoIDSvc.cxx - syntax tidying. python/ISF_Geant4CommonServicesConfig.py - syntax tidying. Aadd python/ISF_Geant4CommonServicesConfigDb.py - add CfgGetter method registrations previously in ISF_Config. ATLASSIM-2352. Tagging ISF_Geant4CommonServices-00-02-06 (ISF_Geant4CommonServices-00-02-06)
    	* src/G4PolyconeGeoIDSvc.cxx - syntax tidying.
    	* python/ISF_Geant4CommonServicesConfig.py - syntax tidying.
    	* add python/ISF_Geant4CommonServicesConfigDb.py - add CfgGetter
    	method registrations previously in ISF_Config. ATLASSIM-2352
    	* tagging ISF_Geant4CommonServices-00-02-06

--- Simulation/ISF/ISF_Geant4/ISF_Geant4CommonServices/python/ISF_Geant4CommonServicesConfig.py
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4CommonServices/python/ISF_Geant4CommonServicesConfig.py
@@ -5,32 +5,18 @@ Tools configurations for ISF
 KG Tan, 17/06/2012
 """
 
-from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicTool,getPublicToolClone,\
-        getService,getServiceClone,getAlgorithm,getAlgorithmClone
-
-from AthenaCommon.Constants import *  # FATAL,ERROR etc.
-from AthenaCommon.SystemOfUnits import *
-from AthenaCommon.DetFlags import DetFlags
-
-from ISF_Config.ISF_jobProperties import ISF_Flags # IMPORTANT: Flags must be set before tools are retrieved
-
-
-def getAFIIG4PolyconeGeoIDSvc(name="ISF_AFIIG4PolyconeGeoIDSvc", **kwargs):
-    kwargs.setdefault("EnvelopeDefSvc"          , getService("ISF_AFIIEnvelopeDefSvc"))
-    return getG4PolyconeGeoIDSvc(name, **kwargs)
-
+from AthenaCommon import CfgMgr
 
 def getG4PolyconeGeoIDSvc(name="ISF_G4PolyconeGeoIDSvc", **kwargs):
     # with ISF volume definitions
-    kwargs.setdefault("EnvelopeDefSvc"          , getService("ISF_ISFEnvelopeDefSvc"))
+    kwargs.setdefault("EnvelopeDefSvc"          , "ISF_ISFEnvelopeDefSvc")
+    return CfgMgr.ISF__G4PolyconeGeoIDSvc(name, **kwargs)
 
-    from ISF_Geant4CommonServices.ISF_Geant4CommonServicesConf import ISF__G4PolyconeGeoIDSvc 
-    return ISF__G4PolyconeGeoIDSvc(name, **kwargs)
+def getAFIIG4PolyconeGeoIDSvc(name="ISF_AFIIG4PolyconeGeoIDSvc", **kwargs):
+    kwargs.setdefault("EnvelopeDefSvc"          , "ISF_AFIIEnvelopeDefSvc")
+    return getG4PolyconeGeoIDSvc(name, **kwargs)
 
-
-def getG4PolyconeGeoIDSvc_G4(name="ISF_G4PolyconeGeoIDSvc_G4", **kwargs):
-    # with G4 volume definitions
-    kwargs.setdefault("GeomDBNodeSuffix"        , 'EnvelopeG4')
-
-    from ISF_Geant4CommonServices.ISF_Geant4CommonServicesConf import ISF__G4PolyconeGeoIDSvc 
-    return ISF__G4PolyconeGeoIDSvc(name, **kwargs)
+## def getG4PolyconeGeoIDSvc_G4(name="ISF_G4PolyconeGeoIDSvc_G4", **kwargs):
+##     # with G4 volume definitions
+##     kwargs.setdefault("GeomDBNodeSuffix"        , 'EnvelopeG4')
+##     return CfgMgr.ISF__G4PolyconeGeoIDSvc(name, **kwargs)
