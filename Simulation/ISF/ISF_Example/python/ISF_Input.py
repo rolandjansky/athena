0f44e07e9d7a780e23d25dd095e84693cdc3fc3c
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-12 12:31:42 +0200

    Remove python/iParticleKiller.py, python/ISF_iParticleKillerConfig.py - configuration simplified and moved to ISF_Services. ATLASSIM-2995. Tagging: ISF_Example-00-04-00 (ISF_Example-00-04-00)
    Former-commit-id: 6742444a6331c2a20121db03405eb85385391c5c

---
+++ Simulation/ISF/ISF_Example/python/ISF_Input.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+
+## Get the logger
+from AthenaCommon.Logging import *
+isfiplog = logging.getLogger('ISF_Input')
+
+# almost all the contents of this file is stolen
+# from G4AtlasApps/PyG4Atlas.py
+
+class ISF_Input:
+    """
+     Place to handle the persistency.
+    """
+
+    isfiplog.info("in ISF_Input")
+
+    """ Place to read evgen events.
+    """
+    from G4AtlasApps.SimFlags import simFlags
+    from AthenaCommon.AppMgr import ServiceMgr as svcMgr
+    #G4AtlasEngine.log.verbose('SimSkeleton._do_readevgen :: starting')
+
+    from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
+
+    if athenaCommonFlags.PoolEvgenInput.statusOn:
+        ## Tell the event selector about the evgen input files and event skipping
+        if not hasattr(svcMgr, 'EventSelector'):
+            import AthenaPoolCnvSvc.ReadAthenaPool
+        svcMgr.EventSelector.InputCollections = athenaCommonFlags.PoolEvgenInput()
+        if athenaCommonFlags.SkipEvents.statusOn:
+            svcMgr.EventSelector.SkipEvents = athenaCommonFlags.SkipEvents()
+        from ISF_Example.ISF_Metadata import inputFileValidityCheck
+        inputFileValidityCheck()
+    else:
+        ## No input file so assume that we are running a Generator in the same job
+        if not hasattr(svcMgr, 'EventSelector'):
+            import AthenaCommon.AtlasUnixGeneratorJob
+        # TODO: Check that there is at least one algorithm already in the AlgSequence?
+        ## Warn if attempting to skip events in a generator job
+        if athenaCommonFlags.SkipEvents.statusOn:
+            msg = "athenaCommonFlags.SkipEvents set in a job without an active "
+            msg += "athenaCommonFlags.PoolEvgenInput flag: ignoring event skip request"
+            isfiplog.warning(msg)
+
+    isfiplog.verbose('ISF_Input done')
---
+++ Simulation/ISF/ISF_Example/python/ISF_Output.py
@@ -0,0 +1,164 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+
+## Get the logger
+from AthenaCommon.Logging import *
+isfoplog = logging.getLogger('ISF_Output')
+
+def getHITSStreamItemList():
+    hitsItemList=[]
+    ## EventInfo & TruthEvent always written by default
+    hitsItemList = ["EventInfo#*",
+                    "McEventCollection#TruthEvent",
+                    "JetCollection#*"]
+    from G4AtlasApps.SimFlags import simFlags
+    if simFlags.SimulationFlavour() is not None and 'ongLived' in simFlags.SimulationFlavour(): # to catch LongLived and longLived
+        hitsItemList += ["McEventCollection#GEN_EVENT"]
+    from PyJobTransforms.trfUtils import releaseIsOlderThan
+    if releaseIsOlderThan(20,0):
+        #Hack to maintain compatibility of G4AtlasApps trunk with
+        #19.2.X.Y after EDM changes in release 20.0.0.
+        hitsItemList += ["xAOD::JetContainer_v1#*",
+                         "xAOD::JetAuxContainer_v1#*"]
+    else:
+        hitsItemList += ["xAOD::JetContainer#*",
+                           "xAOD::JetAuxContainer#*"]
+    ## Detectors
+    from AthenaCommon.DetFlags import DetFlags
+    ## Inner Detector
+    if DetFlags.ID_on():
+        hitsItemList += ["SiHitCollection#*",
+                         "TRTUncompressedHitCollection#*",
+                         "TrackRecordCollection#CaloEntryLayer"]
+    ## Calo
+    if DetFlags.Calo_on():
+        hitsItemList += ["CaloCalibrationHitContainer#*",
+                         "LArHitContainer#*",
+                         "TileHitVector#*",
+                         #"SimpleScintillatorHitCollection#*",
+                         "TrackRecordCollection#MuonEntryLayer"]
+    ## Muon
+    if DetFlags.Muon_on():
+        hitsItemList += ["RPCSimHitCollection#*",
+                         "TGCSimHitCollection#*",
+                         "CSCSimHitCollection#*",
+                         "MDTSimHitCollection#*",
+                         "TrackRecordCollection#MuonExitLayer"]
+    ## FwdRegion
+    if DetFlags.FwdRegion_on():
+        hitsItemList += ["SimulationHitCollection#*"]
+
+    ## Lucid
+    if DetFlags.Lucid_on():
+        hitsItemList += ["LUCID_SimHitCollection#*"]
+
+    ## ZDC
+    if DetFlags.ZDC_on():
+        hitsItemList += ["ZDC_SimPixelHit_Collection#*",
+                             "ZDC_SimStripHit_Collection#*"]
+    ## ALFA
+    if DetFlags.ALFA_on():
+        hitsItemList += ["ALFA_HitCollection#*","ALFA_ODHitCollection#*"]
+
+    ## AFP
+    if DetFlags.AFP_on():
+        hitsItemList += ["AFP_TDSimHitCollection#*","AFP_SIDSimHitCollection#*"]
+
+    ## Ancillary scintillators
+    #hitsItemList += ["ScintillatorHitCollection#*"]
+
+    ## TimingAlg
+    hitsItemList +=["RecoTimingObj#EVNTtoHITS_timings"]
+
+    ## add xAOD::TrackParticles output collection Parametric Simulation
+    if simFlags.SimulationFlavour == "ParametricSimulation":
+        hitsItemList +=["xAOD::TrackParticleContainer#*",
+                            "xAOD::TrackParticleAuxContainer#*"]
+    ## Add cosmics and test beam configuration hit persistency if required cf. geom tag
+    layout = simFlags.SimLayout.get_Value()
+    if "tb" not in layout:
+        from AthenaCommon.BeamFlags import jobproperties
+        if jobproperties.Beam.beamType() == 'cosmics' or \
+                (hasattr(simFlags, "WriteTR") and simFlags.WriteTR.statusOn) or \
+                (hasattr(simFlags, "ReadTR") and simFlags.ReadTR.statusOn):
+            hitsItemList += ["TrackRecordCollection#CosmicRecord", "TrackRecordCollection#CosmicPerigee"]
+    else:
+        ## CTB-specific
+        if layout.startswith("ctb"):
+            if simFlags.LArFarUpstreamMaterial.statusOn and simFlags.LArFarUpstreamMaterial.get_Value():
+                hitsItemList.append("TrackRecordCollection#LArFarUpstreamMaterialExitLayer")
+        ## Persistency of test-beam layout
+        if layout.startswith('ctb') or layout.startswith('tb_Tile2000_'):
+            hitsItemList += ["TBElementContainer#*"]
+    return hitsItemList
+
+def getEVNTStreamItemList():
+    evntItemList=[]
+    from G4AtlasApps.SimFlags import simFlags
+    if simFlags.CavernBG.statusOn and 'Write' in simFlags.CavernBG.get_Value():
+        evntItemList += ["TrackRecordCollection#NeutronBG"]
+    else:
+        evntItemList += ["TrackRecordCollection#CosmicRecord"]
+    return evntItemList
+
+class ISF_HITSStream:
+    """
+     Place to handle the persistency.
+    """
+
+    isfoplog.info("in ISF_HITSStream")
+
+    from G4AtlasApps.SimFlags import simFlags
+    from ISF_Config.ISF_jobProperties import ISF_Flags
+    from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
+    if athenaCommonFlags.PoolHitsOutput.statusOn or (hasattr(simFlags, "WriteTR") and simFlags.WriteTR.statusOn):
+        ## Write hits in POOL
+        isfoplog.info("ISF_HITSStream starting")
+
+        ## The following used to be in G4AtlasApps/HitAthenaPoolWriteOptions
+        from AthenaCommon.Configurable import Configurable
+        from AthenaPoolCnvSvc.WriteAthenaPool import AthenaPoolOutputStream
+
+        ## Default setting for one output stream
+        from AthenaCommon.AppMgr import ServiceMgr
+        ServiceMgr.AthenaPoolCnvSvc.PoolAttributes += ["TREE_BRANCH_OFFSETTAB_LEN = '100'"]
+        ServiceMgr.AthenaPoolCnvSvc.PoolAttributes += ["DEFAULT_BUFFERSIZE = '2048'"]
+
+        ## Write geometry tag info
+        import EventInfoMgt.EventInfoMgtInit
+
+        ## Instantiate StreamHITS
+        ## NB. Two-arg constructor is needed, since otherwise metadata writing fails!
+        stream1 = None
+        if athenaCommonFlags.PoolHitsOutput.statusOn:
+            stream1 = AthenaPoolOutputStream("StreamHITS", athenaCommonFlags.PoolHitsOutput())
+            stream1.ForceRead = True
+            stream1.ItemList = getHITSStreamItemList()
+            ## Make stream aware of aborted events
+            stream1.AcceptAlgs = [ISF_Flags.Simulator.KernelName()]
+
+        ## StreamEVGEN: needed for cosmic simulations and cavern BG
+        ## Separate stream of track record (TR) info -- it does not apply to the CTB simulations.
+        # TODO: Can this be merged into the cosmics sec above, or do the AthenaPool includes *need* to be in-between?
+        layout = simFlags.SimLayout.get_Value()
+        if "tb" not in layout:
+            if hasattr(simFlags, "WriteTR") and simFlags.WriteTR.statusOn:
+                stream2 = AthenaPoolOutputStream("StreamEVGEN", simFlags.WriteTR.get_Value())
+                stream2.ItemList = getEVNTStreamItemList()
+                ## Make stream aware of aborted events
+                stream2.AcceptAlgs = [ISF_Flags.Simulator.KernelName()]
+
+
+        #
+        #  Setup and add metadata to the HITS file.
+        #  ( heavily based on G4AtlasApps/python/SimAtlasKernel.py )
+        #
+        #  causes problems with cosmics currently:
+        #    "IOVDbSvc ERROR Duplicate request for folder /Simulation/Parameters associated to already"
+        from AthenaServices.AthenaServicesConf import AthenaOutputStream
+        stream1_SimMetaData = AthenaOutputStream("StreamHITS_SimMetaData")
+        stream1_SimMetaData.ItemList += [ "IOVMetaDataContainer#*" ]
+
+        isfoplog.info("ISF_HITSStream done")
+    else:
+        isfoplog.info("ISF_HITSStream nothing done")
