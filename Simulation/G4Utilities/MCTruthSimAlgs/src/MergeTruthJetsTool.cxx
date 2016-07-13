b79947bc9effec2bc9169705ffd83d8460875ccc
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-13 15:39:53 +0200

    src/MergeMcEventCollTool.h, src/MergeMcEventCollTool.cxx - revert changes from MCTruthSimAlgs-01-02-04-02. src/MergeHijingParsTool.h, src/MergeHijingParsTool.cxx - revert changes from MCTruthSimAlgs-01-02-04-01. src/MergeTruthJetsTool.h, src/MergeTruthJetsTool.cxx - fix filtering based on truth-jets. Only filter based on in-time truth jets. Made code more robust for the case where we want to filter and write any signal truth jets to the output collection. ATLASSIM-1621. python/MCTruthSimAlgsConfig.py, python/MCTruthSimAlgsConfigDb.py - add new CfgGetter method for MergeTruthJetTool with filtering switched on. ATLASSIM-1621. Tagging: MCTruthSimAlgs-01-02-04-03 (MCTruthSimAlgs-01-02-04-03)
    	* src/MergeMcEventCollTool.h, src/MergeMcEventCollTool.cxx -
    	revert changes from MCTruthSimAlgs-01-02-04-02.
    	* src/MergeHijingParsTool.h, src/MergeHijingParsTool.cxx,
    	CMakeLists.txt, cmt/requirements - revert changes from
    	MCTruthSimAlgs-01-02-04-01.
    	* src/MergeTruthJetsTool.h, src/MergeTruthJetsTool.cxx - fix
    	filtering based on truth-jets. Only filter based on in-time truth
    	jets. Made code more robust for the case where we want to filter
    	and write any signal truth jets to the output
    	collection. ATLASSIM-1621.
    	* python/MCTruthSimAlgsConfig.py, python/MCTruthSimAlgsConfigDb.py
    	- add new CfgGetter method for MergeTruthJetTool with filtering
    	switched on. ATLASSIM-1621
    	* Tagging: MCTruthSimAlgs-01-02-04-03
    
    2016-04-04  John Chapman  <John.Chapman@cern.ch>
    
    	* src/MergeMcEventCollTool.h, src/MergeMcEventCollTool.cxx - add
    	saveHeavyIonInfo method to try to ensure that GenEvent::heavy_ion
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/G4Utilities/MCTruthSimAlgs/src/MergeTruthJetsTool.cxx
+++ Simulation/G4Utilities/MCTruthSimAlgs/src/MergeTruthJetsTool.cxx
@@ -12,24 +12,29 @@
 
 MergeTruthJetsTool::MergeTruthJetsTool(const std::string& type,
                                        const std::string& name,
-                                       const IInterface* parent) :
-  PileUpToolBase(type, name, parent),m_intool(""),
-  m_pMergeSvc("PileUpMergeSvc", name),
-  m_inTimeOutputJetContainer(NULL),
-  m_outOfTimeOutputJetContainer(NULL),
-  m_first_event(true),
-  m_signal_max_pT(-1.),
-  m_pileup_max_pT(-1.)
+                                       const IInterface* parent)
+  : PileUpToolBase(type, name, parent)
+  , m_pMergeSvc("PileUpMergeSvc", name)
+  , m_inTimeOutputJetContainer(nullptr)
+  , m_outOfTimeOutputJetContainer(nullptr)
+  , m_inputJetCollKey("AntiKt4TruthJets")
+  , m_inTimeOutputJetCollKey("InTimeAntiKt4TruthJets")
+  , m_outOfTimeOutputJetCollKey("OutOfTimeAntiKt4TruthJets")
+  , m_inTimePtCut(10.0*Gaudi::Units::GeV)
+  , m_outOfTimePtCut(15.0*Gaudi::Units::GeV)
+  , m_activateFilter(false)
+  , m_includeSignalJets(false)
+  , m_signal_max_pT(-1.)
+  , m_pileup_max_pT(-1.)
 {
   declareInterface<IPileUpTool>(this);
-  declareProperty("InputTool", m_intool);
-  declareProperty("InputTruthJetCollKey", m_inputJetCollKey="AntiKt4TruthJets");
-  declareProperty("InTimeOutputTruthJetCollKey", m_inTimeOutputJetCollKey="InTimeAntiKt4TruthJets");
-  declareProperty("OutOfTimeTruthJetCollKey", m_outOfTimeOutputJetCollKey="OutOfTimeAntiKt4TruthJets");
-  declareProperty("InTimePtCut", m_inTimePtCut=10.0*Gaudi::Units::GeV);
-  declareProperty("OutOfTimePtCut", m_outOfTimePtCut=15.0*Gaudi::Units::GeV);
-  declareProperty("VetoOnInTime", m_vetoOnInTime=false);
-  declareProperty("IncludeSignalJets", m_includeSignalJets=false, "Include signal jets in the pileup truth jet collections, if they exist");
+  declareProperty("InputTruthJetCollKey", m_inputJetCollKey);
+  declareProperty("InTimeOutputTruthJetCollKey", m_inTimeOutputJetCollKey);
+  declareProperty("OutOfTimeTruthJetCollKey", m_outOfTimeOutputJetCollKey);
+  declareProperty("InTimePtCut", m_inTimePtCut);
+  declareProperty("OutOfTimePtCut", m_outOfTimePtCut);
+  declareProperty("ActivateFilter", m_activateFilter);
+  declareProperty("IncludeSignalJets", m_includeSignalJets, "Include signal jets in the pileup truth jet collections, if they exist");
 }
 StatusCode MergeTruthJetsTool::initialize() {
   ATH_MSG_DEBUG ( "Initializing " << name() << " - package version " << PACKAGE_VERSION );
@@ -46,60 +51,89 @@ StatusCode MergeTruthJetsTool::prepareEvent(unsigned int nInputEvents) {
 }
 StatusCode MergeTruthJetsTool::processBunchXing(int bunchXing,
                                                 PileUpEventInfo::SubEvent::const_iterator bSubEvents,
-                                                PileUpEventInfo::SubEvent::const_iterator eSubEvents) {
+                                                PileUpEventInfo::SubEvent::const_iterator eSubEvents)
+{
   ATH_MSG_VERBOSE ( "processBunchXing()" );
   PileUpEventInfo::SubEvent::const_iterator iEvt(bSubEvents);
-  while (iEvt != eSubEvents) {
-    StoreGateSvc& seStore(*iEvt->pSubEvtSG);
-    const xAOD::JetContainer* inputJetContainer = 0;
-    if (seStore.contains<xAOD::JetContainer>(m_inputJetCollKey)) {
-      ATH_MSG_VERBOSE("Found an xAOD::JetContainer in storeGate.");
-      inputJetContainer = seStore.retrieve<const xAOD::JetContainer>(m_inputJetCollKey);
-      //Back-compatibility with inputs which contain old JetCollections
-      if ( inputJetContainer==0 ) {
-        if (!m_intool.empty() ) {
-          ATH_MSG_VERBOSE("Excuting input tool.");
-          if ( m_intool->execute() ) { //NEED TO BE ABLE TO SET THE STOREGATE FOR m_intool AT THIS POINT.
-            ATH_MSG_WARNING("Input tool execution failed.");
-          }
+  while (iEvt != eSubEvents)
+    {
+      StoreGateSvc& seStore(*iEvt->pSubEvtSG);
+      const xAOD::JetContainer* inputJetContainer = 0;
+      if (seStore.contains<xAOD::JetContainer>(m_inputJetCollKey))
+        {
+          ATH_MSG_VERBOSE("Found an xAOD::JetContainer in storeGate.");
           inputJetContainer = seStore.retrieve<const xAOD::JetContainer>(m_inputJetCollKey);
+          //Back-compatibility with inputs which contain old JetCollections
+          if ( inputJetContainer==0 )
+            {
+              if (!m_intool.empty() )
+                {
+                  ATH_MSG_VERBOSE("Excuting input tool.");
+                  if ( m_intool->execute() )
+                    { //NEED TO BE ABLE TO SET THE STOREGATE FOR m_intool AT THIS POINT.
+                      ATH_MSG_WARNING("Input tool execution failed.");
+                    }
+                  inputJetContainer = seStore.retrieve<const xAOD::JetContainer>(m_inputJetCollKey);
+                }
+              if(inputJetContainer == 0)
+                {
+                  ATH_MSG_ERROR("Unable to retrieve input jet container: " << m_inputJetCollKey);
+                  return StatusCode::FAILURE;
+                }
+            }
+          ATH_MSG_DEBUG ( "processBunchXing: bunch Crossing = " << bunchXing << " JetContainer size = " << inputJetContainer->size());
+          if (bunchXing==0)
+            {
+              if (m_first_event)
+                {//FIXME this may not be robust in the case that there is no TruthJet container from the signal event.
+                  m_signal_max_pT = processJetContainer(&(*inputJetContainer), 0, 0.0, 0.0);
+                  ATH_MSG_DEBUG ( "Setting m_signal_max_pT = " << m_signal_max_pT);
+                  if (m_includeSignalJets)
+                    {
+                      (void)this->processJetContainer(&(*inputJetContainer), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
+                    }
+                  else
+                    {
+                      ATH_MSG_VERBOSE ( "Don't include signal events in output Truth Jet Containers.");
+                    }
+                  ++iEvt;
+                  continue;
+                }
+              const double pileup_this_pT=processJetContainer(&(*inputJetContainer), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
+              ATH_MSG_VERBOSE ( "highest jet pT in the current background event = " << pileup_this_pT);
+              if (pileup_this_pT>m_pileup_max_pT) m_pileup_max_pT=pileup_this_pT;
+              ATH_MSG_DEBUG ( "highest in-time background jet pT so far = " << m_pileup_max_pT);
+            }
+          else
+            {
+              const float timeOfBCID(static_cast<float>(iEvt->time()));
+              (void)processJetContainer(&(*inputJetContainer), m_outOfTimeOutputJetContainer, m_outOfTimePtCut, timeOfBCID);
+            }
         }
-        if(inputJetContainer == 0) {
-          ATH_MSG_ERROR("Unable to retrieve input jet container: " << m_inputJetCollKey);
-          return StatusCode::FAILURE;
+      else
+        {
+          ATH_MSG_DEBUG ( "processBunchXing: No JetContainers found." );
         }
-      }
-      ATH_MSG_DEBUG ( "processBunchXing: bunch Crossing = " << bunchXing << " JetContainer size = " << inputJetContainer->size());
-      double pileup_this_pT=-1.;
-      if (bunchXing==0) {
-        if (m_first_event) {//FIXME this may not be robust in the case that there is no TruthJet container from the signal event.
-          m_signal_max_pT = processJetContainer(&(*inputJetContainer), 0, 0.0, 0.0);
-          ATH_MSG_DEBUG ( "Setting m_signal_max_pT = " << m_signal_max_pT);
-          m_first_event=false;
-          if (!m_includeSignalJets) {
-            ATH_MSG_VERBOSE ( "Don't include signal events in output Truth Jet Containers.");
-            ++iEvt;
-            continue;
-          }
-        }
-        pileup_this_pT=processJetContainer(&(*inputJetContainer), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
-      }
-      else {
-        const float timeOfBCID(static_cast<float>(iEvt->time()));
-        pileup_this_pT=processJetContainer(&(*inputJetContainer), m_outOfTimeOutputJetContainer, m_outOfTimePtCut, timeOfBCID);
-      }
-      if (pileup_this_pT>m_pileup_max_pT) m_pileup_max_pT=pileup_this_pT;
-    } else {
-      ATH_MSG_DEBUG ( "processBunchXing: No JetContainers found." );
+      ++iEvt;
     }
-    ++iEvt;
-  }
-  if(m_first_event) {m_first_event=false;}//signal is always the first event, so even if we didn't see anything should set this to false here.
+  //signal is always the first event, so even if we didn't see
+  //anything should set this to false here.
+  if(m_first_event) {m_first_event=false;}
   return StatusCode::SUCCESS;
 }
 
 StatusCode MergeTruthJetsTool::mergeEvent() {
   ATH_MSG_VERBOSE ( "mergeEvent" );
+
+  // Veto event when m_pileup_max_pT>m_signal_max_pT
+  if (m_activateFilter && m_pileup_max_pT>m_signal_max_pT)
+    {
+      ATH_MSG_INFO ( "Highest pT Jet in Hard-scatter event = " << m_signal_max_pT
+                     << ", highest pT jet in a background event = " << m_pileup_max_pT
+                     << ". Therefore filtering this hard-scatter event." );
+      m_filterPassed = false;
+    }
+
   if(this->record(m_inTimeOutputJetContainer, m_inTimeOutputJetCollKey).isFailure()) { //This call also records the xAOD::JetAuxContainer.
     ATH_MSG_ERROR("mergeEvent: Failed to record InTimeOutputJetContainer");
     return StatusCode::FAILURE;
@@ -157,44 +191,69 @@ StatusCode MergeTruthJetsTool::processAllSubEvents()
 
   typedef PileUpMergeSvc::TimedList<xAOD::JetContainer>::type TruthJetList;
   TruthJetList truthList;
-  double pileup_this_pT=-1.;
-  if ( (m_pMergeSvc->retrieveSubEvtsData(m_inputJetCollKey, truthList)).isSuccess() ) {
-    if (!truthList.empty()) {
+  if ( (m_pMergeSvc->retrieveSubEvtsData(m_inputJetCollKey, truthList)).isSuccess() )
+    {
+      if (!truthList.empty())
+        {
 
-      //now merge all collections into one
-      TruthJetList::const_iterator jetColl_iter(truthList.begin());
-      const TruthJetList::const_iterator endOfJetColls(truthList.end());
-      while (jetColl_iter!=endOfJetColls) {
-        //FIXME we are forced to do a deep copy
-        if (static_cast<int>((jetColl_iter)->first.time())==0) {
-          if (m_first_event) {//FIXME this may not be robust in the case that there is no TruthJet container from the signal event.
-            m_signal_max_pT = this->processJetContainer(&(*((jetColl_iter)->second)), 0, 0.0, 0.0);
-            m_first_event=false;
-            if (!m_includeSignalJets) {
-              ATH_MSG_VERBOSE ( "Don't include signal events in output Truth Jet Containers.");
+          //now merge all collections into one
+          TruthJetList::const_iterator jetColl_iter(truthList.begin());
+          const TruthJetList::const_iterator endOfJetColls(truthList.end());
+          while (jetColl_iter!=endOfJetColls)
+            {
+              //FIXME we are forced to do a deep copy
+              if (static_cast<int>((jetColl_iter)->first.time())==0)
+                {
+                  if (m_first_event)
+                    {//FIXME this may not be robust in the case that there is no TruthJet container from the signal event.
+                      m_signal_max_pT = this->processJetContainer(&(*((jetColl_iter)->second)), 0, 0.0, 0.0);
+                      if (m_includeSignalJets)
+                        {
+                          (void)this->processJetContainer(&(*((jetColl_iter)->second)), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
+                        }
+                      else
+                        {
+                          ATH_MSG_VERBOSE ( "Don't include signal events in output Truth Jet Containers.");
+                        }
+                      m_first_event=false;
+                      ++jetColl_iter;
+                      continue;
+                    }
+                  const double pileup_this_pT=this->processJetContainer(&(*((jetColl_iter)->second)), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
+                  ATH_MSG_VERBOSE ( "highest jet pT in the current background event = " << pileup_this_pT);
+                  if (pileup_this_pT>m_pileup_max_pT) m_pileup_max_pT=pileup_this_pT;
+                  ATH_MSG_DEBUG ( "highest in-time background jet pT so far = " << m_pileup_max_pT);
+                }
+              else
+                {
+                  const float timeOfBCID(static_cast<float>((jetColl_iter)->first.time()));
+                  (void)this->processJetContainer(&(*((jetColl_iter)->second)), m_outOfTimeOutputJetContainer, m_outOfTimePtCut, timeOfBCID);
+                }
+              //signal is always the first event, so if the first event
+              //wasn't in-time, then the signal collection was missing and
+              //we should skip further checks.
+              if(m_first_event) {m_first_event=false;}
               ++jetColl_iter;
-              continue;
             }
-          }
-          pileup_this_pT=this->processJetContainer(&(*((jetColl_iter)->second)), m_inTimeOutputJetContainer, m_inTimePtCut, 0.0);
         }
-        else {
-          const float timeOfBCID(static_cast<float>((jetColl_iter)->first.time()));
-          pileup_this_pT=this->processJetContainer(&(*((jetColl_iter)->second)), m_outOfTimeOutputJetContainer, m_outOfTimePtCut, timeOfBCID);
+      else
+        {
+          ATH_MSG_DEBUG ( "processAllSubEvents: TruthJetList is empty" );
         }
-        if (pileup_this_pT>m_pileup_max_pT) m_pileup_max_pT=pileup_this_pT;
-        //signal is always the first event, so if the first event
-        //wasn't in-time, then the signal collection was missing and
-        //we should skip further checks.
-        if(m_first_event) {m_first_event=false;}
-        ++jetColl_iter;
-      }
-
-    } else ATH_MSG_DEBUG ( "processAllSubEvents: TruthJetList is empty" );
-  } else ATH_MSG_ERROR ( "processAllSubEvents: Can not find TruthJetList" );
+    }
+  else
+    {
+      ATH_MSG_ERROR ( "processAllSubEvents: Can not find TruthJetList" );
+    }
 
   // Veto event when m_pileup_max_pT>m_signal_max_pT
-  if (m_pileup_max_pT>m_signal_max_pT) m_filterPassed = false;
+  if (m_activateFilter && m_pileup_max_pT>m_signal_max_pT)
+    {
+      ATH_MSG_INFO ( "Highest pT Jet in Hard-scatter event = " << m_signal_max_pT
+                     << ", highest pT jet in a background event = " << m_pileup_max_pT
+                     << ". Therefore filtering this hard-scatter event." );
+      m_filterPassed = false;
+    }
 
   if(this->record(m_inTimeOutputJetContainer, m_inTimeOutputJetCollKey).isFailure()) { //This call also records the JetMomentMap.
     ATH_MSG_ERROR ( "processAllSubEvents: Failed to record InTimeOutputJetContainer" );
