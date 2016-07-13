45ebdb0cb66d29464e462dbc52a2e16632da1895
Benjamin Taylor Carlson <benjamin.taylor.carlson@cern.ch>
2016-07-13 12:11:30 +0200

    Fixing access method to offline cell energy in FCAL (TrigT1CaloCalibTools-00-05-49)
    	* src/L1CaloTriggerTowerDecoratorAlg.cxx:
    	  Fix decoration usage - could not have decorators with empty names and the same type
    	* Tag TrigT1CaloCalibTools-00-05-48
    
    2016-02-25 Veit Scharf <veit.scharf@cern.ch>
    	* implement additional decorations and make all decorations steerable via jobOptions
    	* Tag TrigT1CaloCalibTools-00-05-47
    
    
    Former-commit-id: c3fcfa7e7afb0c98121fef813b5ac5a84c5bd7f5

--- Trigger/TrigT1/TrigT1CaloCalibTools/src/L1CaloTriggerTowerDecoratorAlg.cxx
+++ Trigger/TrigT1/TrigT1CaloCalibTools/src/L1CaloTriggerTowerDecoratorAlg.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: L1CaloTriggerTowerDecoratorAlg.cxx 684325 2015-07-20 13:47:57Z amazurov $
+// $Id: L1CaloTriggerTowerDecoratorAlg.cxx 728363 2016-03-08 12:45:29Z amazurov $
 
 // TrigT1 common definitions
 #include "TrigT1Interfaces/TrigT1CaloDefs.h"
@@ -13,62 +13,155 @@
 // EDM include(s):
 #include "xAODTrigL1Calo/TriggerTowerContainer.h"
 
-namespace LVL1 
+namespace {
+xAOD::TriggerTower::Decorator<float>* caloCellEnergyDecorator;
+xAOD::TriggerTower::Decorator<float>* caloCellETDecorator;
+xAOD::TriggerTower::Decorator<float>* caloCellsQualityDecorator;
+xAOD::TriggerTower::Decorator<std::vector<float>>*
+    caloCellEnergyByLayerDecorator;
+xAOD::TriggerTower::Decorator<std::vector<float>>* caloCellETByLayerDecorator;
+xAOD::TriggerTower::Decorator<std::vector<std::vector<float>>>*
+    caloCellEnergyByLayerByReceiverDecorator;
+xAOD::TriggerTower::Decorator<std::vector<std::vector<float>>>*
+    caloCellETByLayerByReceiverDecorator;
+}
+
+namespace LVL1 {
+
+L1CaloTriggerTowerDecoratorAlg::L1CaloTriggerTowerDecoratorAlg(
+    const std::string& name, ISvcLocator* svcLoc)
+    : AthAlgorithm(name, svcLoc),
+      m_sgKey_TriggerTowers(LVL1::TrigT1CaloDefs::xAODTriggerTowerLocation),
+      m_caloCellEnergy(""),  // disabled by default
+      m_caloCellET(""),      // disabled by default
+      m_caloCellEnergyByLayer("CaloCellEnergyByLayer"),
+      m_caloCellETByLayer("CaloCellETByLayer"),
+      m_caloCellsQuality("CaloCellQuality"),
+      m_caloCellEnergyByLayerByReceiver(""),  // disabled by default
+      m_caloCellETByLayerByReceiver("")       // disabled by default
 {
+  declareProperty("sgKey_TriggerTowers", m_sgKey_TriggerTowers);
 
-  L1CaloTriggerTowerDecoratorAlg::L1CaloTriggerTowerDecoratorAlg( const std::string& name, ISvcLocator* svcLoc ) :
-    AthAlgorithm( name, svcLoc ),
-    m_sgKey_TriggerTowers(LVL1::TrigT1CaloDefs::xAODTriggerTowerLocation),
-    m_caloCellEnergy("CaloCellEnergy"),
-    m_caloCellET("CaloCellET"),
-    m_caloCellEnergyByLayer("CaloCellEnergyByLayer"),
-    m_caloCellETByLayer ("CaloCellETByLayer"),
-    m_caloCellsQuality ("CaloCellQuality")
-  {  
-    declareProperty( "sgKey_TriggerTowers" , m_sgKey_TriggerTowers = LVL1::TrigT1CaloDefs::xAODTriggerTowerLocation );
- 
-    declareProperty( "DecorName_caloCellEnergy" , m_caloCellEnergy = "CaloCellEnergy" );
-    declareProperty( "DecorName_caloCellET" , m_caloCellET = "CaloCellET" );
-    declareProperty( "DecorName_caloCellEnergyByLayer" , m_caloCellEnergyByLayer = "CaloCellEnergyByLayer" );
-    declareProperty( "DecorName_caloCellETByLayer" , m_caloCellETByLayer = "CaloCellETByLayer" );
-    declareProperty( "DecorName_caloCellQuality" , m_caloCellsQuality = "CaloCellQuality" );    
-    
-    declareProperty( "TriggerTowerTools", m_ttTools );    
+  declareProperty("DecorName_caloCellEnergy",
+                  m_caloCellEnergy,  // disabled by default
+                  "Decoration name - leave empty to disable");
+  declareProperty("DecorName_caloCellET", m_caloCellET,  // disabled by default
+                  "Decoration name - leave empty to disable");
+  declareProperty("DecorName_caloCellEnergyByLayer", m_caloCellEnergyByLayer,
+                  "Decoration name - leave empty to disable");
+  declareProperty("DecorName_caloCellETByLayer", m_caloCellETByLayer,
+                  "Decoration name - leave empty to disable");
+  declareProperty("DecorName_caloCellQuality", m_caloCellsQuality,
+                  "Decoration name - leave empty to disable");
+  declareProperty(
+      "DecorName_caloCellEnergyByLayerByReceiver",
+      m_caloCellEnergyByLayerByReceiver,
+      "Decoration name - leave empty to disable");  // disabled by default
+  declareProperty(
+      "DecorName_caloCellETByLayerByReceiver", m_caloCellETByLayerByReceiver,
+      "Decoration name - leave empty to disable");  // disabled by default
+
+  declareProperty("TriggerTowerTools", m_ttTools);
+}
+
+StatusCode L1CaloTriggerTowerDecoratorAlg::initialize() {
+  ATH_MSG_INFO(
+      "TrigT1CaloCalibTools/L1CaloTriggerTowerDecoratorAlg::initialize()");
+  CHECK(m_ttTools.retrieve());
+
+  if (!m_caloCellEnergy.empty()) {
+    caloCellEnergyDecorator =
+        new xAOD::TriggerTower::Decorator<float>(m_caloCellEnergy);
   }
 
-  StatusCode L1CaloTriggerTowerDecoratorAlg::initialize()
-  {
-    ATH_MSG_INFO("TrigT1CaloCalibTools/L1CaloTriggerTowerDecoratorAlg::initialize()" );
-    CHECK( m_ttTools.retrieve() );
-    
-    // Return gracefully:
-    return StatusCode::SUCCESS;  
+  if (!m_caloCellET.empty()) {
+    caloCellETDecorator =
+        new xAOD::TriggerTower::Decorator<float>(m_caloCellET);
   }
 
-  StatusCode L1CaloTriggerTowerDecoratorAlg::execute()
-  {
- 
-    // Shall I proceed?
-    if (evtStore()->contains<xAOD::TriggerTowerContainer>( m_sgKey_TriggerTowers )) {
-       
-      CHECK( m_ttTools->initCaloCells() );
-        
-      const xAOD::TriggerTowerContainer* tts(nullptr);
-      CHECK( evtStore()->retrieve( tts , m_sgKey_TriggerTowers ) );
-      for (const auto x : *tts) {      
-    
-        // Don't need CellEnergy or CellET, figure them out from the byLayer sums. Waste of disk space.
-        // x->auxdecor< float >( m_caloCellEnergy ) = m_ttTools->caloCellsEnergy( *x );
-        // x->auxdecor< float >( m_caloCellET ) = m_ttTools->caloCellsET( *x );
-        x->auxdecor< std::vector<float> >( m_caloCellEnergyByLayer ) = m_ttTools->caloCellsEnergyByLayer( *x );
-        x->auxdecor< std::vector<float> >( m_caloCellETByLayer ) = m_ttTools->caloCellsETByLayer( *x );   
-        x->auxdecor< float >( m_caloCellsQuality ) = m_ttTools->caloCellsQuality( *x );
-        
+  if (!m_caloCellsQuality.empty()) {
+    caloCellsQualityDecorator =
+        new xAOD::TriggerTower::Decorator<float>(m_caloCellsQuality);
+  }
 
+  if (!m_caloCellEnergyByLayer.empty()) {
+    caloCellEnergyByLayerDecorator =
+        new xAOD::TriggerTower::Decorator<std::vector<float>>(
+            m_caloCellEnergyByLayer);
+  }
+
+  if (!m_caloCellETByLayer.empty()) {
+    caloCellETByLayerDecorator =
+        new xAOD::TriggerTower::Decorator<std::vector<float>>(
+            m_caloCellETByLayer);
+  }
+
+  if (!m_caloCellEnergyByLayerByReceiver.empty()) {
+    caloCellEnergyByLayerByReceiverDecorator =
+        new xAOD::TriggerTower::Decorator<std::vector<std::vector<float>>>(
+            m_caloCellEnergyByLayerByReceiver);
+  }
+
+  if (!m_caloCellETByLayerByReceiver.empty()) {
+    caloCellETByLayerByReceiverDecorator =
+        new xAOD::TriggerTower::Decorator<std::vector<std::vector<float>>>(
+            m_caloCellETByLayerByReceiver);
+  }
+
+  // Return gracefully:
+  return StatusCode::SUCCESS;
+}
+
+StatusCode L1CaloTriggerTowerDecoratorAlg::finalize() {
+  ATH_MSG_INFO(
+      "TrigT1CaloCalibTools/L1CaloTriggerTowerDecoratorAlg::finalize()");
+
+  delete caloCellEnergyDecorator;
+  delete caloCellETDecorator;
+  delete caloCellsQualityDecorator;
+  delete caloCellEnergyByLayerDecorator;
+  delete caloCellETByLayerDecorator;
+  delete caloCellEnergyByLayerByReceiverDecorator;
+  delete caloCellETByLayerByReceiverDecorator;
+
+  // Return gracefully:
+  return StatusCode::SUCCESS;
+}
+
+StatusCode L1CaloTriggerTowerDecoratorAlg::execute() {
+  // use decorators to avoid the costly name -> auxid lookup
+
+  // Shall I proceed?
+  if (evtStore()->contains<xAOD::TriggerTowerContainer>(
+          m_sgKey_TriggerTowers)) {
+    CHECK(m_ttTools->initCaloCells());
+
+    const xAOD::TriggerTowerContainer* tts(nullptr);
+    CHECK(evtStore()->retrieve(tts, m_sgKey_TriggerTowers));
+    for (const auto x : *tts) {
+      if (caloCellEnergyDecorator)
+        (*caloCellEnergyDecorator)(*x) = m_ttTools->caloCellsEnergy(*x);
+      if (caloCellETDecorator)
+        (*caloCellETDecorator)(*x) = m_ttTools->caloCellsET(*x);
+      if (caloCellEnergyByLayerDecorator)
+        (*caloCellEnergyByLayerDecorator)(*x) =
+            m_ttTools->caloCellsEnergyByLayer(*x);
+      if (caloCellETByLayerDecorator)
+        (*caloCellETByLayerDecorator)(*x) = m_ttTools->caloCellsETByLayer(*x);
+      if (caloCellsQualityDecorator)
+        (*caloCellsQualityDecorator)(*x) = m_ttTools->caloCellsQuality(*x);
+      if (caloCellEnergyByLayerByReceiverDecorator) {
+        (*caloCellEnergyByLayerByReceiverDecorator)(*x) =
+            m_ttTools->caloCellsEnergyByLayerByReceiver(*x);
       }
-    } // Trigger towers present in Storegate
-        
-    // Return gracefully:
-    return StatusCode::SUCCESS;  
-  }
+      if (caloCellETByLayerByReceiverDecorator) {
+        (*caloCellETByLayerByReceiverDecorator)(*x) =
+            m_ttTools->caloCellsETByLayerByReceiver(*x);
+      }
+    }
+  }  // Trigger towers present in Storegate
+
+  // Return gracefully:
+  return StatusCode::SUCCESS;
+}
 }
