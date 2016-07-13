bbd509273b0bd17a7193a9efaf3ba04463f267ba
Benjamin Taylor Carlson <benjamin.taylor.carlson@cern.ch>
2016-07-13 12:11:30 +0200

    Fixing access method to offline cell energy in FCAL (TrigT1CaloCalibTools-00-05-49)

--- Trigger/TrigT1/TrigT1CaloCalibTools/TrigT1CaloCalibTools/L1CaloTriggerTowerDecoratorAlg.h
+++ Trigger/TrigT1/TrigT1CaloCalibTools/TrigT1CaloCalibTools/L1CaloTriggerTowerDecoratorAlg.h
@@ -4,7 +4,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: L1CaloTriggerTowerDecoratorAlg.h 652501 2015-03-08 15:06:35Z morrisj $
+// $Id: L1CaloTriggerTowerDecoratorAlg.h 728363 2016-03-08 12:45:29Z amazurov $
 #ifndef TRIGGER_TRIGT1_TRIGT1CALOXAODCALIBTOOLS_DECORATETRIGGERTOWERSALG_H
 #define TRIGGER_TRIGT1_TRIGT1CALOXAODCALIBTOOLS_DECORATETRIGGERTOWERSALG_H
 
@@ -15,29 +15,31 @@
 // Local include(s):
 #include "TrigT1CaloCalibToolInterfaces/IL1CaloxAODOfflineTriggerTowerTools.h"
 
-namespace LVL1
-{
-  class L1CaloTriggerTowerDecoratorAlg : public AthAlgorithm{
-    public:
-      L1CaloTriggerTowerDecoratorAlg( const std::string& name, ISvcLocator* svcLoc );
-      
-      /// Function initialising the algorithm
-      virtual StatusCode initialize();
-      /// Function executing the algorithm
-      virtual StatusCode execute();   
-      
-    private:
-      std::string m_sgKey_TriggerTowers;   
-      
-      /// Decoration strings
-      std::string m_caloCellEnergy;
-      std::string m_caloCellET;
-      std::string m_caloCellEnergyByLayer;
-      std::string m_caloCellETByLayer;   
-      std::string m_caloCellsQuality;
-      
-      
-      ToolHandle< LVL1::IL1CaloxAODOfflineTriggerTowerTools > m_ttTools;
-  };
+namespace LVL1 {
+class L1CaloTriggerTowerDecoratorAlg : public AthAlgorithm {
+ public:
+  L1CaloTriggerTowerDecoratorAlg(const std::string& name, ISvcLocator* svcLoc);
+
+  /// Function initialising the algorithm
+  virtual StatusCode initialize();
+  /// Function finalize the algorithm
+  virtual StatusCode finalize();
+  /// Function executing the algorithm
+  virtual StatusCode execute();
+
+ private:
+  std::string m_sgKey_TriggerTowers;
+
+  /// Decoration strings (leave empty to disable the decoration)
+  std::string m_caloCellEnergy;
+  std::string m_caloCellET;
+  std::string m_caloCellEnergyByLayer;
+  std::string m_caloCellETByLayer;
+  std::string m_caloCellsQuality;
+  std::string m_caloCellEnergyByLayerByReceiver;
+  std::string m_caloCellETByLayerByReceiver;
+
+  ToolHandle<LVL1::IL1CaloxAODOfflineTriggerTowerTools> m_ttTools;
+};
 }
 #endif
