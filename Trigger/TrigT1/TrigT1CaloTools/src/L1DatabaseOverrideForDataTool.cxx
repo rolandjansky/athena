b9fdc79f72379a50c37b6dfe2bc08a1ae65385a5
Sasha Mazurov <Alexander.Mazurov@cern.ch>
2016-07-19 08:37:38 +0200

    L1CPCMXTool.cxx, L1JetCMXTools.cxx: fix getHits method for tobs with overflow (TrigT1CaloTools-03-06-30)
        * L1CPCMXTool.cxx, L1JetCMXTools.cxx: fix getHits method for tobs with overflow
        * Tag as TrigT1CaloTools-03-06-30
    
    2016-05-02 Hanno Meyer zu Theenhausen <hanno.meyer.zu.theenhausen@cern.ch>
        * L1TriggerTowerTools: Implement non linear lut jep simulation for strategy 3
        * Tag as TrigT1CaloTools-03-06-29
    
    2016-04-28 Alexander Mazurov <alexander.mazurov@cern.ch>
        * L1JetCMXTools: fix getHits method.
        * Tag as TrigT1CaloTools-03-06-28
    
    2016-04-20  Hanno Meyer zu Theenhausen <hanno.meyer.zu.theenhausen@cern.ch>
    	* L1TriggerTowerTools: For LutJep retrieve the scale parameter
    	from the menu now, rather than the coolDb
    	* Tag as TrigT1CaloTools-03-06-27
    
    2016-02-19  Veit Scharf <veit.scharf@CERN.CH>
    	* L1DatabaseOverrideForDataTool: new AthAlgTool that makes the
    	online database folders compatible with the Ppm simulation in
    	Run2TriggerTowerMaker
    ...
    (Long ChangeLog diff - truncated)

---
+++ Trigger/TrigT1/TrigT1CaloTools/src/L1DatabaseOverrideForDataTool.cxx
@@ -0,0 +1,53 @@
+#include "TrigT1CaloTools/L1DatabaseOverrideForDataTool.h"
+
+#include "TrigT1CaloCalibConditions/L1CaloPprChanDefaultsContainer.h"
+#include "TrigT1CaloCalibConditions/L1CaloPprChanCalibContainer.h"
+#include "TrigT1CaloCalibConditions/L1CaloDisabledTowersContainer.h"
+#include "TrigT1CaloCalibConditions/L1CaloPpmDeadChannelsContainer.h"
+
+namespace LVL1 {
+
+L1DatabaseOverrideForDataTool::L1DatabaseOverrideForDataTool(
+                          const std::string& t,
+			  const std::string& n,
+			  const IInterface*  p )
+  :  AthAlgTool(t,n,p)
+{
+  declareInterface<IL1DatabaseOverrideTool>(this);
+}
+
+L1DatabaseOverrideForDataTool::~L1DatabaseOverrideForDataTool()
+{
+}
+
+// this tool only modifies ChanDefaults, the rest is a straight-copy of the input
+StatusCode L1DatabaseOverrideForDataTool::modifyChanCalib(L1CaloPprChanCalibContainer&)
+{
+  return StatusCode::SUCCESS;
+}
+StatusCode L1DatabaseOverrideForDataTool::modifyDisabledTowers(L1CaloDisabledTowersContainer&)
+{
+  return StatusCode::SUCCESS;
+}
+StatusCode L1DatabaseOverrideForDataTool::modifyDeadChannels(L1CaloPpmDeadChannelsContainer&)
+{
+  return StatusCode::SUCCESS;
+}
+
+StatusCode L1DatabaseOverrideForDataTool::modifyChanDefaults(L1CaloPprChanDefaultsContainer& c)
+{
+  for(auto& chan : c) {
+    // swap bcidDecision1 and bcidDecision3
+    auto bcidDecision1 = chan.bcidDecision1(), bcidDecision3 = chan.bcidDecision3();
+    chan.setBcidDecision1(bcidDecision3);
+    chan.setBcidDecision3(bcidDecision1);
+
+    // swap satOverride1 and satOverride3
+    auto satOverride1 = chan.satOverride1(), satOverride3 = chan.satOverride3();
+    chan.setSatOverride1(satOverride3);
+    chan.setSatOverride3(satOverride1);
+  }
+  return StatusCode::SUCCESS;
+}
+
+} // end of namespace LVL1
