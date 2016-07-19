d8e1e93083681baa74a227fc320deaec28f5efcf
Sasha Mazurov <Alexander.Mazurov@cern.ch>
2016-07-19 08:37:38 +0200

    L1CPCMXTool.cxx, L1JetCMXTools.cxx: fix getHits method for tobs with overflow (TrigT1CaloTools-03-06-30)
        * L1CPCMXTool.cxx, L1JetCMXTools.cxx: fix getHits method for tobs with overflow
        * Tag as TrigT1CaloTools-03-06-30
    
    2016-05-02 Hanno Meyer zu Theenhausen <hanno.meyer.zu.theenhausen@cern.ch>
        * L1TriggerTowerTools: Implement non linear lut jep simulation for strategy 3
        * Tag as TrigT1CaloTools-03-06-29
    
    2016-04-28 Alexander Mazurov <alexander.mazurov@cern.ch>
        * L1JetCMXTools: fix getHits method.
        * Tag as TrigT1CaloTools-03-06-28
    
    2016-04-20  Hanno Meyer zu Theenhausen <hanno.meyer.zu.theenhausen@cern.ch>
    	* L1TriggerTowerTools: For LutJep retrieve the scale parameter
    	from the menu now, rather than the coolDb
    	* Tag as TrigT1CaloTools-03-06-27
    
    2016-02-19  Veit Scharf <veit.scharf@CERN.CH>
    	* L1DatabaseOverrideForDataTool: new AthAlgTool that makes the
    	online database folders compatible with the Ppm simulation in
    	Run2TriggerTowerMaker
    ...
    (Long ChangeLog diff - truncated)

--- Trigger/TrigT1/TrigT1CaloTools/TrigT1CaloTools/L1JetCMXTools.h
+++ Trigger/TrigT1/TrigT1CaloTools/TrigT1CaloTools/L1JetCMXTools.h
@@ -1,12 +1,12 @@
 
- /***************************************************************************
-  *                                                                         *
-  *   This program is free software; you can redistribute it and/or modify  *
-  *   it under the terms of the GNU General Public License as published by  *
-  *   the Free Software Foundation; either version 2 of the License, or     *
-  *   (at your option) any later version.                                   *
-  *                                                                         *
-  ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef LVL1L1JETCMXTOOLS_H
 #define LVL1L1JETCMXTOOLS_H
@@ -21,112 +21,105 @@
 #include "TrigConfInterfaces/ITrigConfigSvc.h"
 #include "TrigT1CaloToolInterfaces/IL1JetCMXTools.h"
 
+namespace LVL1 {
 
-namespace LVL1 
-{
+/** @class L1JetCMXTools
 
-  /** @class L1JetCMXTools
+    This is a tool to reconstruct the L1 JEM and CMX-Jet hits
+    from RoIs.
+    Used for offline monitoring and trigger reconstruction.
 
-      This is a tool to reconstruct the L1 JEM and CMX-Jet hits
-      from RoIs.
-      Used for offline monitoring and trigger reconstruction.
+    Useage: L1JetCMXTools->formJEMTobRoI()  // convert to JEMTobRoI from
+                                            // other RoI classes
+            L1JetCMXTools->formCMXJetTob()  // form CMX-Jet TOBs from
+                                            // JEMTobRoIs
+            L1JetCMXTools->formCMXJetHits() // form CMX-Jet hits from
+                                            // CMXJetTobs
 
-      Useage: L1JetCMXTools->formJEMTobRoI()  // convert to JEMTobRoI from
-                                              // other RoI classes
-              L1JetCMXTools->formCMXJetTob()  // form CMX-Jet TOBs from
-	                                      // JEMTobRoIs
-              L1JetCMXTools->formCMXJetHits() // form CMX-Jet hits from
-	                                      // CMXJetTobs
-      
-      @author  Peter Faulkner
-  */  
+    @author  Peter Faulkner
+*/
 
-class L1JetCMXTools : virtual public IL1JetCMXTools, public AthAlgTool
-{
-  public:
-     
-    L1JetCMXTools(const std::string& type, const std::string& name,
-                                           const IInterface* parent);
+class L1JetCMXTools : virtual public IL1JetCMXTools, public AthAlgTool {
+ public:
+  L1JetCMXTools(const std::string& type, const std::string& name,
+                const IInterface* parent);
 
-     /** default destructor */
-    virtual ~L1JetCMXTools ();
-      
-     /** standard Athena-Algorithm method */
-    virtual StatusCode initialize();
-     /** standard Athena-Algorithm method */
-    virtual StatusCode finalize  ();
-      
-    
-    /** form CMX-Jet TOBs from RoIs - single slice */
-    virtual void formCMXJetTob(const xAOD::JEMTobRoIContainer* jemRoiVec,
-                                     xAOD::CMXJetTobContainer* cmxTobVec) const;
+  /** default destructor */
+  virtual ~L1JetCMXTools();
 
-     /** form CMX-Jet TOBs from RoIs - multiple slices */
-    virtual void formCMXJetTob(
-                 const std::vector<const xAOD::JEMTobRoIContainer*>& jemRoiColls,
-                 xAOD::CMXJetTobContainer* cmxTobVec, int peak) const;
+  /** standard Athena-Algorithm method */
+  virtual StatusCode initialize();
+  /** standard Athena-Algorithm method */
+  virtual StatusCode finalize();
 
-    /** form complete CMX-Jet hits from CMX-Jet TOBs */
-    virtual void formCMXJetHits(const xAOD::CMXJetTobContainer*  cmxTobVec,
-                                      xAOD::CMXJetHitsContainer* cmxHitsVec) const;
+  /** form CMX-Jet TOBs from RoIs - single slice */
+  virtual void formCMXJetTob(const xAOD::JEMTobRoIContainer* jemRoiVec,
+                             xAOD::CMXJetTobContainer* cmxTobVec) const;
 
-    /** form partial CMX-Jet hits (crate) from CMX-Jet TOBs */
-    virtual void formCMXJetHitsCrate(const xAOD::CMXJetTobContainer* cmxTobVec,
-                                    xAOD::CMXJetHitsContainer* cmxHitsCrate) const;
+  /** form CMX-Jet TOBs from RoIs - multiple slices */
+  virtual void formCMXJetTob(
+      const std::vector<const xAOD::JEMTobRoIContainer*>& jemRoiColls,
+      xAOD::CMXJetTobContainer* cmxTobVec, int peak) const;
 
-    /** form partial CMX-Jet hits (system) from crate CMX-Jet hits */
-    virtual void formCMXJetHitsSystem(
-                                     const xAOD::CMXJetHitsContainer* cmxHitsCrate,
-                                     xAOD::CMXJetHitsContainer* cmxHitsSys) const;
-     /** form partial CMX-Jet hits (topo) from system CMX-Jet TOBs */
-    virtual void formCMXJetHitsTopo(const xAOD::CMXJetTobContainer* cmxTobVec,
-                                    xAOD::CMXJetHitsContainer* cmxHitsTopo) const;
-      
-  private:
+  /** form complete CMX-Jet hits from CMX-Jet TOBs */
+  virtual void formCMXJetHits(const xAOD::CMXJetTobContainer* cmxTobVec,
+                              xAOD::CMXJetHitsContainer* cmxHitsVec) const;
 
-    enum    HitsType { MAIN_HITS, FORWARD_HITS };
-      
-    typedef std::vector<uint32_t>   HitsVector;
-    typedef std::vector<uint32_t>   ErrorVector;
+  /** form partial CMX-Jet hits (crate) from CMX-Jet TOBs */
+  virtual void formCMXJetHitsCrate(
+      const xAOD::CMXJetTobContainer* cmxTobVec,
+      xAOD::CMXJetHitsContainer* cmxHitsCrate) const;
 
+  /** form partial CMX-Jet hits (system) from crate CMX-Jet hits */
+  virtual void formCMXJetHitsSystem(
+      const xAOD::CMXJetHitsContainer* cmxHitsCrate,
+      xAOD::CMXJetHitsContainer* cmxHitsSys) const;
+  /** form partial CMX-Jet hits (topo) from system CMX-Jet TOBs */
+  virtual void formCMXJetHitsTopo(const xAOD::CMXJetTobContainer* cmxTobVec,
+                                  xAOD::CMXJetHitsContainer* cmxHitsTopo) const;
 
-    /** Get hit map */
-    void getHits(const xAOD::CMXJetTob* tob, HitsVector& hit0,
-                                       HitsVector& hit1) const;
+ private:
+  enum HitsType { MAIN_HITS, FORWARD_HITS };
 
-    /** Add overflow bit */
-    void addOverflow(ErrorVector& hitErr, const ErrorVector& tobErr) const;
-    /** Add hits from second vector to first */
-    void addCMXJetHits(HitsVector& vec1, const HitsVector& vec2,
-                                               HitsType type) const;
-    /** Increment JEM/CMX hit word */
-    unsigned int addHits(unsigned int hitMult, unsigned int hitVec,
-                         int multBits, int vecBits, int nthresh) const;
+  typedef std::vector<uint32_t> HitsVector;
+  typedef std::vector<uint32_t> ErrorVector;
 
-    /** Merge CMX-Jet hits vectors */
-    void mergeCMXJetHits(xAOD::CMXJetHitsContainer* cmxHitsVec1,
-                         xAOD::CMXJetHitsContainer* cmxHitsVec2) const;
+  /** Get hit map */
+  void getHits(const xAOD::CMXJetTob* tob, HitsVector& hit10, HitsVector& hit11,
+               HitsVector& hit20, HitsVector& hit21) const;
 
-    void saveCMXJetHits(xAOD::CMXJetHitsContainer* cmxHitsVec,
-                        const HitsVector& hits0, const HitsVector& hits1,
-      const ErrorVector& err0, const ErrorVector& err1,
-            int crate, int source, int peak) const;
+  /** Add overflow bit */
+  void addOverflow(ErrorVector& hitErr, const ErrorVector& tobErr) const;
+  /** Add hits from second vector to first */
+  void addCMXJetHits(HitsVector& vec1, const HitsVector& vec2,
+                     HitsType type) const;
+  /** Increment JEM/CMX hit word */
+  unsigned int addHits(unsigned int hitMult, unsigned int hitVec, int multBits,
+                       int vecBits, int nthresh) const;
 
-    /** Trigger configuration service */
-    ServiceHandle<TrigConf::ITrigConfigSvc> m_configSvc;
-    /** Number of crates */
-    int m_crates;
-    /** Number of JEM modules per crate */
-    int m_modules;
-    /** Maximum number of TOBs per module to CMXs */
-    int m_maxTobs;
-    /** System crate number */
-    int m_sysCrate;
-    /** Debug flag */
-    bool m_debug;
-     
+  /** Merge CMX-Jet hits vectors */
+  void mergeCMXJetHits(xAOD::CMXJetHitsContainer* cmxHitsVec1,
+                       xAOD::CMXJetHitsContainer* cmxHitsVec2) const;
+
+  void saveCMXJetHits(xAOD::CMXJetHitsContainer* cmxHitsVec,
+                      const HitsVector& hits0, const HitsVector& hits1,
+                      const ErrorVector& err0, const ErrorVector& err1,
+                      int crate, int source, int peak) const;
+
+  /** Trigger configuration service */
+  ServiceHandle<TrigConf::ITrigConfigSvc> m_configSvc;
+  /** Number of crates */
+  int m_crates;
+  /** Number of JEM modules per crate */
+  int m_modules;
+  /** Maximum number of TOBs per module to CMXs */
+  int m_maxTobs;
+  /** System crate number */
+  int m_sysCrate;
+  /** Debug flag */
+  bool m_debug;
 };
 
-} // end of namespace
+}  // end of namespace
 
-#endif 
+#endif
