55886231de7ae606c8403cedff6f10ff11b1b694
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-22 10:41:28 +0200

    CMakeLists.txt - fixes so that all required symbols are available to client packages. ATLASSIM-3004. Tagging as ISF_FastCaloSimEvent-00-02-01 (ISF_FastCaloSimEvent-00-02-01)
    	* CMakeLists.txt - fixes so that all required symbols are
    	available to client packages. ATLASSIM-3004
    	* cmt/requirements - whitespace clean-up.
    	* Tagging as ISF_FastCaloSimEvent-00-02-01
    
    2015-06-01  Elmar Ritsch  <Elmar.Ritsch@cern.ch>
    
    	* Move following classes from ISF_FastCaloSimParametrization package into
    	this package and make them compile-able with cmt (outside of ROOT). ATLASSIM-2882:
    		FastCaloSim_CaloCell_ID
    		IntArray
    		TFCS1DFunction
    		TFCSEnergyParametrization
    		TFCSExtrapolationState
    		TFCSParametrization
    		TFCSParametrizationBase
    		TFCSPCAEnergyParametrization
    		TFCSSimulationState
    		TFCSTruthState
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 0217f1af3f42617bd91e18e067552df3ed982d97

---
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/IntArray.cxx
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "ISF_FastCaloSimEvent/IntArray.h"
+
+#include "TArrayI.h"
+
+//==========================
+//======= IntArray =========
+//==========================
+
+using namespace std;
+
+IntArray::IntArray(int n)
+{
+  m_array=new TArrayI(n);
+}
+
+IntArray::IntArray()
+{
+  m_array=new TArrayI();
+}
+
+
+//=============================================
+//========== ROOT persistency stuff ===========
+//=============================================
+
+ClassImp(IntArray)
+
