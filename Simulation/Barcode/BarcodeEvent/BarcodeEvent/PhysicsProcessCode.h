e97af9ef424a51e9aae9299b4b1613e0ab292776
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-07-27 16:01:39 +0200

    Initial package setup: move Barcode.h and PhysicsProcessCode.h from BarcodeInterfaces package into BarcodeEvent package. This is follow ATLAS conventions for putting event-data types into dedicated *Event packages. ATLASSIM-2281 (BarcodeEvent-00-00-01)

---
+++ Simulation/Barcode/BarcodeEvent/BarcodeEvent/PhysicsProcessCode.h
@@ -0,0 +1,21 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// PhysicsProcessCode.h, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+#ifndef BARCODEINTERFACES_PHYSICSPROCESSCODE_H
+#define BARCODEINTERFACES_PHYSICSPROCESSCODE_H 1
+
+
+namespace Barcode {
+
+  /** Identifier type for physics processes */
+  typedef int PhysicsProcessCode;
+
+  /** Use this to make the code more readable */
+  const PhysicsProcessCode fUndefinedProcessCode = 0;
+}
+
+#endif  //< BARCODEINTERFACES_PHYSICSPROCESSCODE_H
