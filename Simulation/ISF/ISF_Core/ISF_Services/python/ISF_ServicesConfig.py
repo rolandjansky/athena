257ee947e6735548bd0a3ccff6966cd83daeb68a
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-28 10:04:54 +0200

    python/ISF_ServicesConfig.py (getSimHitService): add missing import of ISF_Flags. ATLASSIM-3004, ATLASSIM-2679. Tagging ISF_Services-01-00-04 (ISF_Services-01-00-04)
    	* python/ISF_ServicesConfig.py (getSimHitService): add missing
    	import of ISF_Flags. ATLASSIM-3004, ATLASSIM-2679
    	* ISF_Services-01-00-04
    
    2016-07-12  John Chapman  <John.Chapman@cern.ch>
    
    	* python/ISF_ServicesConfigDb.py - remove unneeded imports.
    	Register CfgGetter method for ISF_ParticleKillerSvc. ATLASSIM-2352
    	* python/ISF_ServicesConfig.py - add getParticleKillerSvc
    	CfgGetter method. ATLASSIM-2352. General tidying of syntax.
    	* ISF_Services-01-00-03
    
            * src/SimHitSvc.cxx - check empty hit collections
    
    
    Former-commit-id: ef7ffd4d0e64367f31764942ea8cdf3823c1bfa9

--- Simulation/ISF/ISF_Core/ISF_Services/python/ISF_ServicesConfig.py
+++ Simulation/ISF/ISF_Core/ISF_Services/python/ISF_ServicesConfig.py
@@ -5,28 +5,22 @@ Tools configurations for ISF
 KG Tan, 17/06/2012
 """
 
-from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicTool,getPublicToolClone,\
-        getService,getServiceClone,getAlgorithm,getAlgorithmClone
+from AthenaCommon import CfgMgr
 
 from AthenaCommon.Constants import *  # FATAL,ERROR etc.
 from AthenaCommon.SystemOfUnits import *
-from AthenaCommon.DetFlags import DetFlags
-
-from ISF_Config.ISF_jobProperties import ISF_Flags # IMPORTANT: Flags must be set before tools are retrieved
-
 
 def getParticleBrokerSvcNoOrdering(name="ISF_ParticleBrokerSvcNoOrdering", **kwargs):
     kwargs.setdefault('StackFiller'                 , 'ISF_StackFiller'                     )
     kwargs.setdefault('EntryLayerTool'              , 'ISF_EntryLayerTool'                  )
     kwargs.setdefault('GeoIDSvc'                    , 'ISF_GeoIDSvc'                        )
     kwargs.setdefault('AlwaysUseGeoIDSvc'           , False                                 )
+    from ISF_Config.ISF_jobProperties import ISF_Flags
     kwargs.setdefault('ValidateGeoIDs'              , ISF_Flags.ValidationMode()            )
     kwargs.setdefault('ValidationOutput'            , ISF_Flags.ValidationMode()            )
     kwargs.setdefault('ValidationStreamName'        , "ParticleBroker"                      )
     kwargs.setdefault('BarcodeService'              , ISF_Flags.BarcodeService()            )
-
-    from ISF_Services.ISF_ServicesConf import ISF__ParticleBrokerDynamicOnReadIn
-    return ISF__ParticleBrokerDynamicOnReadIn(name, **kwargs)
+    return CfgMgr.ISF__ParticleBrokerDynamicOnReadIn(name, **kwargs)
 
 def getParticleBrokerSvc(name="ISF_ParticleBrokerSvc", **kwargs):
     #kwargs.setdefault('ParticleOrderingTool'       , 'ISF_InToOutSubDetOrderingTool'       )
@@ -42,10 +36,9 @@ def getAFIIParticleBrokerSvc(name="ISF_AFIIParticleBrokerSvc", **kwargs):
     return getParticleBrokerSvc(name, **kwargs)
 
 def getSimHitService(name="ISF_SimHitService", **kwargs):
+    from ISF_Config.ISF_jobProperties import ISF_Flags
     kwargs.setdefault('ValidationOutput'    , ISF_Flags.ValidationMode()                    )
-
-    from ISF_Services.ISF_ServicesConf import ISF__SimHitSvc
-    return ISF__SimHitSvc(name, **kwargs)
+    return CfgMgr.ISF__SimHitSvc(name, **kwargs)
 
 def getNoG4SimHitService(name="ISF_NoG4SimHitService", **kwargs):
     kwargs.setdefault("SensitiveDetectorMasterTool","EmptySensitiveDetectorMasterTool")
@@ -59,28 +52,26 @@ def getPileupSimHitService(name="ISF_PileupSimHitService", **kwargs):
 def getISFEnvelopeDefSvc(name="ISF_ISFEnvelopeDefSvc", **kwargs):
     # ATLAS common envlope definitions
     kwargs.setdefault("ATLASEnvelopeDefSvc"    , "AtlasGeometry_EnvelopeDefSvc")
-
-    from ISF_Services.ISF_ServicesConf import ISF__ISFEnvelopeDefSvc
-    return ISF__ISFEnvelopeDefSvc(name, **kwargs)
+    return CfgMgr.ISF__ISFEnvelopeDefSvc(name, **kwargs)
 
 def getAFIIEnvelopeDefSvc(name="ISF_AFIIEnvelopeDefSvc", **kwargs):
     from AthenaCommon.SystemOfUnits import mm
     # ATLAS common envlope definitions
     kwargs.setdefault("ISFEnvelopeDefSvc"    , "ISF_ISFEnvelopeDefSvc"         )
     kwargs.setdefault("InDetMaxExtentZ"      , 3549.5*mm                       )
-
-    from ISF_Services.ISF_ServicesConf import ISF__AFIIEnvelopeDefSvc
-    return ISF__AFIIEnvelopeDefSvc(name, **kwargs)
+    return CfgMgr.ISF__AFIIEnvelopeDefSvc(name, **kwargs)
 
 
 def getGeoIDSvc(name="ISF_GeoIDSvc", **kwargs):
     # with ISF volume definitions
     kwargs.setdefault("EnvelopeDefSvc"          , "ISF_ISFEnvelopeDefSvc"      )
-
-    from ISF_Services.ISF_ServicesConf import ISF__GeoIDSvc
-    return ISF__GeoIDSvc(name, **kwargs)
+    return CfgMgr.ISF__GeoIDSvc(name, **kwargs)
 
 
 def getAFIIGeoIDSvc(name="ISF_AFIIGeoIDSvc", **kwargs):
     kwargs.setdefault("EnvelopeDefSvc"          , "ISF_AFIIEnvelopeDefSvc"     )
     return getGeoIDSvc(name, **kwargs)
+
+def getParticleKillerSvc(name="ISF_ParticleKillerSvc", **kwargs):
+    kwargs.setdefault('Identifier',           "ParticleKiller")
+    return CfgMgr.ISF__ParticleKillerSimSvc(name, **kwargs)
