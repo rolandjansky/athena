fbdb3457819b9f0116b20eb582a7afa84d95c355
Will Buttinger <William.Buttinger@cern.ch>
2016-07-28 12:28:26 +0200

    'fix cmt compilation' (JetInterface-00-00-47)
    	* cmt/requirements: fix cmt compilation
    	* JetInterface-00-00-47
    
    2016-06-23    <delsart@lpsc1120x.in2p3.fr>
    
    	* JetInterface/JetInterfaceDict.h : added for dictionnaries in athena
    	* Root/LinkDef.h: added for dictionnaries in RootCore
    	* JetInterface-00-00-46
    
    
    Former-commit-id: a3d64da2c8b473e25337df4a1f2177978575cac9

---
+++ Reconstruction/Jet/JetInterface/JetInterface/JetInterfaceDict.h
@@ -0,0 +1,26 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef JETINTERFACE_JETINTERFACEDICT_H
+#define JETINTERFACE_JETINTERFACEDICT_H
+
+#include "JetInterface/IJetBuildTool.h"
+#include "JetInterface/IJetConstituentModifier.h"
+#include "JetInterface/IJetConstituentsRetriever.h"
+#include "JetInterface/IJetConsumer.h"
+#include "JetInterface/IJetExecuteTool.h"
+#include "JetInterface/IJetFinder.h"
+#include "JetInterface/IJetFromPseudojet.h"
+#include "JetInterface/IJetGroomer.h"
+#include "JetInterface/IJetModifier.h"
+#include "JetInterface/IJetPseudojetRetriever.h"
+#include "JetInterface/IJetSelector.h"
+#include "JetInterface/IJetTrackSelector.h"
+#include "JetInterface/IJetUncertaintiesTool.h"
+#include "JetInterface/IJetUpdateJvt.h"
+#include "JetInterface/IPseudoJetGetter.h"
+#include "JetInterface/ISingleJetModifier.h"
+
+
+#endif
---
+++ Reconstruction/Jet/JetInterface/Root/LinkDef.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include  "JetInterface/IFastJetInterfaceTool.h"
+#include  "JetInterface/IJetBuildTool.h"
+#include  "JetInterface/IJetConstituentsRetriever.h"
+#include  "JetInterface/IJetConsumer.h"
+#include  "JetInterface/IJetExecuteTool.h"
+#include  "JetInterface/IJetFinder.h"
+#include  "JetInterface/IJetFromPseudojet.h"
+#include  "JetInterface/IJetGroomer.h"
+#include  "JetInterface/IJetModifier.h"
+#include  "JetInterface/IJetPseudojetRetriever.h"
+#include  "JetInterface/IPseudoJetGetter.h"
+#include  "JetInterface/ISingleJetModifier.h"
+
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+
+#pragma link C++ class IFastJetInterfaceTool;
+#pragma link C++ class IJetBuildTool;
+#pragma link C++ class IJetConstituentsRetriever;
+#pragma link C++ class IJetConsumer;
+#pragma link C++ class IJetExecuteTool;
+#pragma link C++ class IJetFinder;
+#pragma link C++ class IJetFromPseudojet;
+#pragma link C++ class IJetGroomer;
+#pragma link C++ class IJetModifier;
+#pragma link C++ class IJetPseudojetRetriever;
+#pragma link C++ class IPseudoJetGetter;
+#pragma link C++ class ISingleJetModifier;
+
+#endif
