2308188afb351666fc980ca00d0ddf3af216a419
Scott Snyder <scott.snyder@cern.ch>
2016-07-28 02:50:29 +0200

    'endreq -> endmsg.' (CoWTools-00-00-10)
    	* Tagging CoWTools-00-00-10.
    	* endreq -> endmsg.
    
    
    Former-commit-id: 941617366095a2234e19fd3cbe80a4a3ff01d759

--- Control/CoWTools/src/MStrStream.cxx
+++ Control/CoWTools/src/MStrStream.cxx
@@ -10,5 +10,5 @@ void CoWTools::MStrStream::putResults(const CoWTools::CoWRecordStats &res){
   const long long * vals=res.getValueArray();
   snprintf(buff,2048,"VMem= %8lld, RSS= %8lld, PSS= %8lld, Shared= %8lld, Private= %8lld, Swap=%8lld, Anon= %8lld",
 	   vals[0],vals[1],vals[2],vals[3]+vals[4],vals[5]+vals[6],vals[10],vals[8]);
-  m_out<<buff<<endreq;
+  m_out<<buff<<endmsg;
 }
