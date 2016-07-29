063aa60f031726e96d7fa0c38c5251ed672893e0
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-07-29 08:51:36 +0200

    CMakeLists.txt: add missing atlas_install_python_modules call. ATLASSIM-2679, ATLASSIM-3004. Tagging ISF_Geant4Services-00-02-01 (ISF_Geant4Services-00-02-01)
    	* CMakeLists.txt: add missing atlas_install_python_modules
    	call. ATLASSIM-2679, ATLASSIM-3004
    	* Tagging ISF_Geant4Services-00-02-01
    
    2016-07-11  John Chapman  <John.Chapman@cern.ch>
    
    	* add python/iGeant4.py, python/ISF_Geant4ServicesConfig.py,
    	python/ISF_Geant4ServicesConfigDb.py - with configuration
    	previously in ISF_Geant4Config and ISF_Config
    	packages. ATLASSIM-2352. Further tidying still required.
    	* Tagging ISF_Geant4Services-00-02-00
    
    
    Former-commit-id: 38252965dcef6894352db09e1e3dd6065b8823e1

---
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Services/python/ISF_Geant4ServicesConfig.py
@@ -0,0 +1,65 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+"""
+Tools configurations for ISF
+KG Tan, 17/06/2012
+"""
+
+from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicTool,getPublicToolClone,\
+        getService,getServiceClone,getAlgorithm,getAlgorithmClone
+
+from AthenaCommon.Constants import *  # FATAL,ERROR etc.
+from AthenaCommon.SystemOfUnits import *
+from AthenaCommon.DetFlags import DetFlags
+
+from ISF_Config.ISF_jobProperties import ISF_Flags # IMPORTANT: Flags must be set before tools are retrieved
+
+
+cache = {}
+
+def usingGeant4():
+    global cache
+    return bool(cache)
+
+def getIGeant4(**kwargs):
+    global cache
+    FullGeant4 = kwargs.pop('FullGeant4',False)
+    if 'iGeant4' in cache:
+        i = cache['iGeant4']
+        # if FullGeant4 specified, make sure returning cached version with FullGeant4 already in use
+        if FullGeant4:
+            if (i.FullGeant4 == False) :
+                print "asking for FullGeant4 but already configured Geant4 without specifying Full Geant4! Must specify FullGeant4 everywhere (or nowhere)!!!"
+                sys.exit()
+    else:
+        from ISF_Config.ISF_jobProperties import ISF_Flags
+        from iGeant4 import iGeant4
+        i = iGeant4(**kwargs)
+        cache['iGeant4'] = i
+    return i
+
+def getGeant4SimSvc(name="ISF_Geant4SimSvc", **kwargs):
+    from ISF_Config.ISF_jobProperties import ISF_Flags
+    kwargs.setdefault('FullGeant4'            , False                        )
+    kwargs.setdefault('SimulationTool'        , 'ISFG4TransportTool'         )
+    kwargs.setdefault('Identifier'            , "Geant4"                     )
+    kwargs.setdefault('PrintTimingInfo'       , ISF_Flags.DoTimeMonitoring() )
+    return getIGeant4(**kwargs).getSimSvc()
+
+def getFullGeant4SimSvc(name="ISF_Geant4SimSvc", **kwargs):
+    kwargs.setdefault('FullGeant4'            , True                         )
+    kwargs.setdefault('SimulationTool'        , 'FullG4TransportTool'        )
+    return getGeant4SimSvc(name, **kwargs)
+
+def getLongLivedGeant4SimSvc(name="ISF_LongLivedGeant4SimSvc", **kwargs):
+    kwargs.setdefault('SimulationTool'       , 'QuasiStableG4TransportTool')
+    return getFullGeant4SimSvc(name, **kwargs)
+
+def getPassBackGeant4SimSvc(name="ISF_PassBackGeant4SimSvc", **kwargs):
+    kwargs.setdefault('FullGeant4'            , False                        )
+    kwargs.setdefault('SimulationTool'        , 'PassBackG4TransportTool'    )
+    return getGeant4SimSvc(name, **kwargs)
+
+def getAFIIGeant4SimSvc(name="ISF_AFIIGeant4SimSvc", **kwargs):
+    kwargs.setdefault('SimulationTool'       , 'AFII_G4TransportTool')
+    return getPassBackGeant4SimSvc(name, **kwargs)
