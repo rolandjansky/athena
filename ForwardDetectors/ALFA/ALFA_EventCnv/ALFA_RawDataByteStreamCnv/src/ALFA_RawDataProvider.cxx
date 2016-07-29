97731687526bb56641ab9d23fd0f7e2d837eb922
Scott Snyder <scott.snyder@cern.ch>
2016-07-29 15:36:25 +0200

    'endreq -> endmsg.' (ALFA_RawDataByteStreamCnv-00-00-23)
    	* Tagging ALFA_RawDataByteStreamCnv-00-00-23.
    	* endreq -> endmsg.
    
    2016-06-02 Christian Heinz <christian.heinz@cern.ch>
    
    	* Introduction of the EventFlagBit and changed Failure Code to Success for corrupted events

--- ForwardDetectors/ALFA/ALFA_EventCnv/ALFA_RawDataByteStreamCnv/src/ALFA_RawDataProvider.cxx
+++ ForwardDetectors/ALFA/ALFA_EventCnv/ALFA_RawDataByteStreamCnv/src/ALFA_RawDataProvider.cxx
@@ -38,23 +38,23 @@ ALFA_RawDataProvider::~ALFA_RawDataProvider(){}
 StatusCode ALFA_RawDataProvider::initialize()
 {
 
-  msg(MSG::DEBUG) << "ALFA_RawDataProvider::initialize" << endreq;
+  msg(MSG::DEBUG) << "ALFA_RawDataProvider::initialize" << endmsg;
 
   // Get ROBDataProviderSvc
   if (m_robDataProvider.retrieve().isFailure())
     {
-      msg(MSG::FATAL) << "Failed to retrieve service " << m_robDataProvider << endreq;
+      msg(MSG::FATAL) << "Failed to retrieve service " << m_robDataProvider << endmsg;
       return StatusCode::FAILURE;
     } else
-    msg(MSG::DEBUG) << "Retrieved service " << m_robDataProvider << endreq;
+    msg(MSG::DEBUG) << "Retrieved service " << m_robDataProvider << endmsg;
 
   // Get ALFARawDataProviderTool
   if (m_rawDataTool.retrieve().isFailure())
     {
-      msg(MSG::FATAL) << "Failed to retrieve service " << m_rawDataTool << endreq;
+      msg(MSG::FATAL) << "Failed to retrieve service " << m_rawDataTool << endmsg;
       return StatusCode::FAILURE;
     } else
-    msg(MSG::DEBUG) << "Retrieved service " << m_rawDataTool << endreq;
+    msg(MSG::DEBUG) << "Retrieved service " << m_rawDataTool << endmsg;
 
   return StatusCode::SUCCESS;
 }
@@ -69,11 +69,11 @@ StatusCode ALFA_RawDataProvider::initialize()
 StatusCode ALFA_RawDataProvider::execute()
 {
 
-  msg(MSG::DEBUG) << "ALFA_RawDataProvider::EXECUTE" << endreq;
+  msg(MSG::DEBUG) << "ALFA_RawDataProvider::EXECUTE" << endmsg;
 
 
   ALFA_RawDataContainer *container = new ALFA_RawDataContainer();
-  msg(MSG::DEBUG) << " Created ALFA RDO Container" << endreq;
+  msg(MSG::DEBUG) << " Created ALFA RDO Container" << endmsg;
 
   StatusCode sc;
 
@@ -81,10 +81,10 @@ StatusCode ALFA_RawDataProvider::execute()
 
   if (sc.isFailure())
     {
-      msg(MSG::FATAL) << "Unable to record ALFA RDO Container" << endreq;
+      msg(MSG::FATAL) << "Unable to record ALFA RDO Container" << endmsg;
       return StatusCode::FAILURE;
     } else if (sc.isSuccess()){
-    msg(MSG::DEBUG) << "ALFA RDO Container recorded" << endreq;
+    msg(MSG::DEBUG) << "ALFA RDO Container recorded" << endmsg;
   }
 
 
@@ -97,18 +97,18 @@ StatusCode ALFA_RawDataProvider::execute()
   m_robDataProvider->getROBData(ROBIDs, listOfRobf);
 
 
-  msg(MSG::DEBUG) << "  ROB ID " << std::hex <<ROBIDs <<MSG::dec<< endreq;
+  msg(MSG::DEBUG) << "  ROB ID " << std::hex <<ROBIDs <<MSG::dec<< endmsg;
 
-  msg(MSG::DEBUG) << " Number of ROB fragments is " << listOfRobf.size() << endreq;
+  msg(MSG::DEBUG) << " Number of ROB fragments is " << listOfRobf.size() << endmsg;
 
   // ask ALFA_RawDataProviderTool to decode it and to fill the container
 
   if (m_rawDataTool->convert(listOfRobf,container).isFailure())
     {
-      msg(MSG::ERROR) << "BS conversion into RDOs failed" << endreq;
+      msg(MSG::ERROR) << "BS conversion into RDOs failed" << endmsg;
       return StatusCode::RECOVERABLE;
     } else
-    msg(MSG::DEBUG) << " Number of collections in container is " << container->size() << endreq;
+    msg(MSG::DEBUG) << " Number of collections in container is " << container->size() << endmsg;
 
 
   // check retrieve from StoreGate ///////////////////////////////////////////////////////////
@@ -116,54 +116,7 @@ StatusCode ALFA_RawDataProvider::execute()
   sc = evtStore()->retrieve(container, m_ALFA_RawDataCollectionKey);
   if (sc.isSuccess())
     {
-      msg(MSG::DEBUG) << "ALFA RDO Container retrieved" << endreq;
-
-/*
-      ALFA_RawDataContainer::const_iterator RawData_Collection_Beg = container->begin();
-      ALFA_RawDataContainer::const_iterator RawData_Collection_End = container->end();
-
-      //int MBId_prova;
-
-      //loop over collection (container) with hits (i.e. over 1 event)
-      for(;RawData_Collection_Beg!=RawData_Collection_End;++RawData_Collection_Beg)
-	{
-
-	  //MBId_prova =(*RawData_Collection_Beg)->GetMBId_POT();
-
-	  //msg(MSG::DEBUG)  << " MBId = " << MBId_prova <<  endreq;
-
-
-	  ALFA_RawDataCollection::const_iterator p_RawData_Beg=(*RawData_Collection_Beg)->begin();//pointer to RawData data vector
-	  ALFA_RawDataCollection::const_iterator p_RawData_End=(*RawData_Collection_Beg)->end();
-
-	  //int PMFId_prova;
-	  //int FiberNum_prova;
-
-
-	  // Loop over RawData   -> apparently this is not used anymore
-
-	  for(;p_RawData_Beg!=p_RawData_End; ++p_RawData_Beg)
-	    {
-
-	      if (((*p_RawData_Beg)->GetWordId_PMF())==3)
-		{
-		  //PMFId_prova = ((*p_RawData_Beg)->GetPMFId_PMF());
-		  //msg(MSG::DEBUG)  << " PMFId = " << ((*p_RawData_Beg)->GetPMFId_PMF()) <<  endreq;
-
-		} else {PMFId_prova = 100;}
-
-	      for (unsigned int i=0;i<16;i++)
-		{
-		  //FiberNum_prova = ((*p_RawData_Beg)->GetWordId_PMF()*16+i);
-
-		  //if((*p_RawData_Beg)->HitChan()[i]!=100) msg(MSG::DEBUG)  <<"    hit = " << (*p_RawData_Beg)-> HitChan()[i]<< endreq;
-		}
-
-
-	    } // loop raw data
-
-	}// loop collection
-	*/
+      msg(MSG::DEBUG) << "ALFA RDO Container retrieved" << endmsg;
     } // end check
 
   return StatusCode::SUCCESS;
