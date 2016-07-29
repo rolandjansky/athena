13c76a0a35faeb48f08a50e1b6540f2f82c99311
Scott Snyder <scott.snyder@cern.ch>
2016-07-29 18:19:45 +0200

    'endreq -> endmsg.' (TrigMuonEvent-00-01-26)
    	* Tagging TrigMuonEvent-00-01-26.
    	* endreq -> endmsg.
    
    
    Former-commit-id: 1fec0fd2b2d36cb9598c1638ed157c7e04e1aae0

--- Trigger/TrigEvent/TrigMuonEvent/test/Operators_test.cxx
+++ Trigger/TrigEvent/TrigMuonEvent/test/Operators_test.cxx
@@ -11,7 +11,7 @@
 //////////////////////////////////////////////////////////////////
 int testEqual_IsoMuonFeature() {
   MsgStream log(Athena::getMessageSvc(), "testEqual_IsoMuonFeature");  
-  log << MSG::INFO << "testing identical IsoMuonFeatures" << endreq;
+  log << MSG::INFO << "testing identical IsoMuonFeatures" << endmsg;
 
   IsoMuonFeature a, b;
   a.setRoiMu(1,10000.,1.,3.14,1.,1);
@@ -22,12 +22,12 @@ int testEqual_IsoMuonFeature() {
   b.setTrackInfos(1000.,4000.,5000.,7000.,10000.,2000.);
 
   if ( !(a == b) ) {
-    log << MSG::ERROR << "Identical IsoMuonFeatures found distinct! " << endreq;
+    log << MSG::ERROR << "Identical IsoMuonFeatures found distinct! " << endmsg;
     return -1;
   }
 
 
-  log << MSG::INFO << "testing distinct IsoMuonFeatures" << endreq;
+  log << MSG::INFO << "testing distinct IsoMuonFeatures" << endmsg;
 
   IsoMuonFeature c;
   c.setRoiMu(2,14000.,-1.,3.14,1.,1);
@@ -35,9 +35,9 @@ int testEqual_IsoMuonFeature() {
   c.setTrackInfos(1000.,1500.,1900.,3000.,14000.,0.0);
 
   if ( a == c ) {
-    log << MSG::ERROR << "Different IsoMuonFeatures found same! " << endreq;
-    log << MSG::ERROR << a << endreq;
-    log << MSG::ERROR << c << endreq;
+    log << MSG::ERROR << "Different IsoMuonFeatures found same! " << endmsg;
+    log << MSG::ERROR << a << endmsg;
+    log << MSG::ERROR << c << endmsg;
     return -1;
   }
 
@@ -52,8 +52,8 @@ int testPrint_IsoMuonFeature() {
   a.setTrackInfos(1000.,1500.,1900.,3000.,14000.,0.0);
   
   MsgStream log(Athena::getMessageSvc(), "testPrint_IsoMuonFeature");
-  log << MSG::INFO << "MSG a " << a << endreq;
-  log << MSG::INFO << "MSG b " << b << endreq;
+  log << MSG::INFO << "MSG a " << a << endmsg;
+  log << MSG::INFO << "MSG b " << b << endmsg;
   return 0;
 }
 
@@ -62,16 +62,16 @@ int main() {
 
   // IsoMuonFeature
   if ( testEqual_IsoMuonFeature() != 0 ) {
-    log << MSG::ERROR << "IsoMuonFeature == operator is wrong" << endreq;
+    log << MSG::ERROR << "IsoMuonFeature == operator is wrong" << endmsg;
     return -1;
   }
-  log << MSG::INFO << "IsoMuonFeature == operator is correct " << endreq;    
+  log << MSG::INFO << "IsoMuonFeature == operator is correct " << endmsg;    
   if ( testPrint_IsoMuonFeature() != 0 ) {
-    log << MSG::ERROR << "IsoMuonFeature << operator is wrong" << endreq;
+    log << MSG::ERROR << "IsoMuonFeature << operator is wrong" << endmsg;
     return -1;
     
   }
-  log << MSG::INFO << "IsoMuonFeature << operator is correct " << endreq;    
+  log << MSG::INFO << "IsoMuonFeature << operator is correct " << endmsg;    
 
   return 0;
 }
