8eda40bae00901dcbe580aca207072ad424ad00e
John Baines <john.baines@stfc.ac.uk>
2016-08-02 16:55:25 +0200

    first import of package (xAODViews-00-00-01)
    Former-commit-id: b1f6cde6f0432e2b5c599f9a419a3c789bfcd3aa

---
+++ Event/xAOD/xAODViews/xAODViews/xAODViewsDict.h
@@ -0,0 +1,14 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef XAODVIEWS_XAODVIEWSDICT_H
+#define  XAODVIEWS_XAODVIEWSDICT_H
+
+#include "xAODViews/TrackParticleViewVector.h"
+#include "xAODViews/VertexViewVector.h"
+namespace xAODViewsDict {
+  xAOD::TrackParticleViewVector::Pers_t dumTP;
+  xAOD::VertexViewVector::Pers_t dumV; 
+} 
+#endif //  XAODVIEWS_XAODVIEWSDICT_H
