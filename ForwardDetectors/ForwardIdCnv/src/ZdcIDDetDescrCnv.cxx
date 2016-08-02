205b58b70954eb913474be46b115361de0ad7899
Scott Snyder <scott.snyder@cern.ch>
2016-08-02 03:10:41 +0200

    'endreq -> endmsg.' (ForwardIdCnv-00-00-06)
    	* Tagging ForwardIdCnv-00-00-06.
    	* endreq -> endmsg.
    
    2016-07-12  scott snyder  <snyder@bnl.gov>
    
    	* Tagging ForwardIdCnv-00-00-05.
    	* src/ZdcIDDetDescrCnv.cxx: Add missing braces (fix gcc6
    	compilation warning).
    
    
    Former-commit-id: 67ec34c89f9ceab171ec2a5f893e60852795526d

--- ForwardDetectors/ForwardIdCnv/src/ZdcIDDetDescrCnv.cxx
+++ ForwardDetectors/ForwardIdCnv/src/ZdcIDDetDescrCnv.cxx
@@ -49,11 +49,11 @@ ZdcIDDetDescrCnv::initialize()
 {
     // First call parent init
     StatusCode sc = DetDescrConverter::initialize();
-    MsgStream log(messageService(), "ZdcIDDetDescrCnv");
-    log << MSG::DEBUG << "in initialize" << endreq;
+    MsgStream log(msgSvc(), "ZdcIDDetDescrCnv");
+    log << MSG::DEBUG << "in initialize" << endmsg;
 
     if (sc.isFailure()) {
-        log << MSG::ERROR << "DetDescrConverter::initialize failed" << endreq;
+        log << MSG::ERROR << "DetDescrConverter::initialize failed" << endmsg;
 	return sc;
     }
 
@@ -68,7 +68,7 @@ ZdcIDDetDescrCnv::initialize()
 //      // - this is ONLY needed for the manager of each system
 //      sc = addToDetStore(classID(), "PidelID");
 //      if (sc.isFailure()) {
-//  	log << MSG::FATAL << "Unable to add proxy for ZdcID to the Detector Store!" << endreq;
+//  	log << MSG::FATAL << "Unable to add proxy for ZdcID to the Detector Store!" << endmsg;
 //  	return StatusCode::FAILURE;
 //      } else {}
 
@@ -80,8 +80,8 @@ ZdcIDDetDescrCnv::initialize()
 StatusCode 
 ZdcIDDetDescrCnv::finalize()
 {
-    MsgStream log(messageService(), "ZdcIDDetDescrCnv");
-    log << MSG::DEBUG << "in finalize" << endreq;
+    MsgStream log(msgSvc(), "ZdcIDDetDescrCnv");
+    log << MSG::DEBUG << "in finalize" << endmsg;
 
     return StatusCode::SUCCESS; 
 }
@@ -92,32 +92,32 @@ StatusCode
 ZdcIDDetDescrCnv::createObj(IOpaqueAddress* pAddr, DataObject*& pObj) 
 {
     //StatusCode sc = StatusCode::SUCCESS;
-    MsgStream log(messageService(), "ZdcIDDetDescrCnv");
-    log << MSG::INFO << "in createObj: creating a ZdcID helper object in the detector store" << endreq;
+    MsgStream log(msgSvc(), "ZdcIDDetDescrCnv");
+    log << MSG::INFO << "in createObj: creating a ZdcID helper object in the detector store" << endmsg;
 
     // Create a new ZdcID
 
     DetDescrAddress* ddAddr;
     ddAddr = dynamic_cast<DetDescrAddress*> (pAddr);
     if(!ddAddr) {
-	log << MSG::FATAL << "Could not cast to DetDescrAddress." << endreq;
+	log << MSG::FATAL << "Could not cast to DetDescrAddress." << endmsg;
 	return StatusCode::FAILURE;
     }
 
     // Get the StoreGate key of this container.
     std::string helperKey  = *( ddAddr->par() );
     if ("" == helperKey) {
-	log << MSG::DEBUG << "No Helper key " << endreq;
+	log << MSG::DEBUG << "No Helper key " << endmsg;
     }
     else {
-	log << MSG::DEBUG << "Helper key is " << helperKey << endreq;
+	log << MSG::DEBUG << "Helper key is " << helperKey << endmsg;
     }
     
     // get DetectorStore service
     StoreGateSvc * detStore;
     StatusCode status = serviceLocator()->service("DetectorStore", detStore);
     if (status.isFailure()) {
-	log << MSG::FATAL << "DetectorStore service not found !" << endreq;
+	log << MSG::FATAL << "DetectorStore service not found !" << endmsg;
 	return StatusCode::FAILURE;
     } else {}
  
@@ -125,11 +125,11 @@ ZdcIDDetDescrCnv::createObj(IOpaqueAddress* pAddr, DataObject*& pObj)
     const DataHandle<IdDictManager> idDictMgr;
     status = detStore->retrieve(idDictMgr, "IdDict");
     if (status.isFailure()) {
-	log << MSG::FATAL << "Could not get IdDictManager !" << endreq;
+	log << MSG::FATAL << "Could not get IdDictManager !" << endmsg;
 	return StatusCode::FAILURE;
     } 
     else {
-	log << MSG::DEBUG << " Found the IdDictManager. " << endreq;
+	log << MSG::DEBUG << " Found the IdDictManager. " << endmsg;
     }
 
 
@@ -149,7 +149,7 @@ ZdcIDDetDescrCnv::createObj(IOpaqueAddress* pAddr, DataObject*& pObj)
     if (!dict) {
 	log << MSG::ERROR 
 	    << "unable to find idDict for InnerDetector" 
-	    << endreq;
+	    << endmsg;
 	return StatusCode::FAILURE;
     }
 
@@ -168,39 +168,40 @@ ZdcIDDetDescrCnv::createObj(IOpaqueAddress* pAddr, DataObject*& pObj)
 	    // Internal InDet id tag
 	    initHelper = true;
 	    log << MSG::DEBUG << " Changed internal InDet id tag: " 
-		<< inDetIDTag << endreq;
+		<< inDetIDTag << endmsg;
 	}
 	if (inDetIDFileName != m_inDetIDFileName) {
 	    // File to be read for InDet ids
 	    initHelper = true;
 	    log << MSG::DEBUG << " Changed InDetFileName:" 
-		<< inDetIDFileName << endreq;
+		<< inDetIDFileName << endmsg;
 	}
 	if (inDetIdDictTag != m_inDetIdDictTag) {
 	    // Tag of RDB record for InDet ids
 	    initHelper = true;
 	    log << MSG::DEBUG << " Changed InDetIdDictTag: "
 		<< inDetIdDictTag 
-		<< endreq;
+		<< endmsg;
 	}
-	if (doChecks != m_doChecks)
+	if (doChecks != m_doChecks) {
 	    // DoChecks flag
 	    initHelper = true;
 	    log << MSG::DEBUG << " Changed doChecks flag: "
 		<< doChecks
-		<< endreq;
+		<< endmsg;
+        }
     }
     else {
 	// create the helper
 	m_zdcId = new ZdcID;
 	initHelper = true;
         // add in message service for printout
-        m_zdcId->setMessageSvc(messageService());
+        m_zdcId->setMessageSvc(msgSvc());
     }
     
     if (initHelper) {
 	if (idDictMgr->initializeHelper(*m_zdcId)) {
-	    log << MSG::ERROR << "Unable to initialize ZdcID" << endreq;
+	    log << MSG::ERROR << "Unable to initialize ZdcID" << endmsg;
 	    return StatusCode::FAILURE;
 	} 
 	// Save state:
