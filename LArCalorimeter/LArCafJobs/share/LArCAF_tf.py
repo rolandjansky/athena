dd1fdf25829b33afbb596b69a2e60b04914e759b
Pavol Strizenec <Pavol.Strizenec@cern.ch>
2016-08-02 15:00:17 +0200

    getting tf from RAW working correctly (LArCafJobs-01-02-02)

---
+++ LArCalorimeter/LArCafJobs/share/LArCAF_tf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+__doc__ = """JobTransform to run LAr CAF jobs"""
+
+
+import sys
+from PyJobTransforms.transform import transform
+from PyJobTransforms.trfExe import athenaExecutor
+from PyJobTransforms.trfArgs import addAthenaArguments, addDetectorArguments
+import PyJobTransforms.trfArgClasses as trfArgClasses
+
+if __name__ == '__main__':
+
+    executorSet = set()
+    executorSet.add(athenaExecutor(name = 'LArCAFcalib', skeletonFile = 'LArCafJobs/skeleton.LArCAF.py',
+                                   substep = 'r2e', inData = ['BS',], outData = ['NTUP_SAMPLESMON','NTUP_HECNOISE']))
+    
+    trf = transform(executor = executorSet)  
+    addAthenaArguments(trf.parser)
+    addDetectorArguments(trf.parser)
+    trf.parser.add_argument('--inputBSFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argBSFile, io='input'),
+                            help='Input bytestream file', group='Reco Files')
+    
+    trf.parser.add_argument('--outputNTUP_SAMPLESMONFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argNTUPFile, io='output', treeNames="events"),
+                            help='Output LAr Samples Mon file', group='Ntuple Files')
+
+    trf.parser.add_argument('--outputNTUP_HECNOISEFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argNTUPFile, io='output'),
+                            help='Output HECNoise file', group='Ntuple Files')
+
+    trf.parseCmdLineArgs(sys.argv[1:])
+    trf.execute()
+    trf.generateReport()
+
+    
--- LArCalorimeter/LArCafJobs/share/UploadAMITag.larcaf.py
+++ LArCalorimeter/LArCafJobs/share/UploadAMITag.larcaf.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python
+
+#Exampel command: UploadAMITag.larcaf.py wlampl xxx create_c909 AtlasProduction_20.1.5.6 noisebursts
+ 
+
 import sys,pickle
 
-patcharea="/afs/cern.ch/user/l/larcomm/w0/digitMon/LArCAF"
-setupScript="/afs/cern.ch/atlas/tzero/software/setup/setuptrf.sh"
-specialT0Setup="/afs/cern.ch/atlas/tzero/software/setup/specialsetup_tier0.sh"
-
+patcharea='/afs/cern.ch/user/l/larcomm/w0/digitMon/LArCAF'
+setupScript='/afs/cern.ch/atlas/tzero/software/setup/setuptrf.sh'
+specialT0Setup='/afs/cern.ch/atlas/tzero/software/setup/specialsetup_tier0.sh'
+trfsetuppath='/afs/cern.ch/atlas/tzero/software/setup/'
 processConfigs = {}
+
+processConfigs['noisebursts'] = {
+    'inputs': { 'inputESDFile': {} },
+    'outputs': { 'outputNTUP_LARNOISEFile': {'dstype': 'NTUP_LARNOISE'}, }, 
+    'tasktransinfo': { 'trfpath': 'LArNoiseBursts_tf.py',
+                       'trfsetupcmd': setupScript+ ' '+patcharea+' RELEASE ' + specialT0Setup},
+    'phconfig': {}, 
+    'description': 'Produced LAr noise burst ntuple from ESD files. Uses RELEASE' 
+    }
+
+processConfigs['noiseburstsmerge'] = {
+    'inputs': { 'inputNTUP_LARNOISEFile': {} },
+    'outputs': { 'outputNTUP_LARNOISE_MRGFile': {'dstype': 'NTUP_LARNOISE_MRG'}, }, 
+    'tasktransinfo': { 'trfpath': 'NTUPMerge_tf.py',
+                       'trfsetupcmd': setupScript+ ' '+patcharea+' RELEASE ' + specialT0Setup},
+    'phconfig': {}, 
+    'description': 'Merge LAr noise burst ntuple files. Uses RELEASE' 
+    }
+
 
 processConfigs['reco'] = {
     'inputs': { 'inputBSFile': {} },
     'outputs': { 'outputNTUP_SAMPLESMONFile': {'dstype': 'NTUP_SAMPLESMON'}, }, 
-    'tasktransinfo': { 'trfpath': 'LArCAF_trf.py',
+    'tasktransinfo': { 'trfpath': 'LArCAF_tf.py',
                        'trfsetupcmd': setupScript+ ' '+patcharea+' RELEASE ' + specialT0Setup},
     'phconfig': {}, 
     'description': 'Runs LAr Samples monitoring job on LArCells or LArCellsEmpty calibration streams. Uses RELEASE' 
@@ -21,9 +44,9 @@ processConfigs['merge'] = {
     'inputs': { 'inputLArFiles': {} },
     'outputs': { 'outputLArFile': {'dstype': 'NTUP_SAMPLESMON'}, }, 
     'tasktransinfo': { 'trfpath': 'LArMerge_trf.py',
-                       'trfsetupcmd': '/afs/cern.ch/atlas/tzero/prod1/inst/projects/data11/setup/setuptrf.sh '+patcharea+' RELEASE /afs/cern.ch/atlas/tzero/prod1/inst/projects/data11/setup/specialsetup_tier0.sh' },
+                       'trfsetupcmd': trfsetuppath+'setuptrf.sh '+patcharea+' RELEASE '+trfsetuppath+'specialsetup_tier0.sh' },
     'phconfig': {}, 
-    'description': 'Merges LAr Samples monitoring ntuples.. Uses RELEASE' 
+    'description': 'Merges LAr Samples monitoring ntuples. Uses RELEASE' 
     }
 
 
@@ -31,9 +54,9 @@ processConfigs['histmerge'] = {
     'inputs': { 'inputHistFiles': {'metatype':'inputLFNlistDA'} },
     'outputs': { 'outputLArHistFile': {'dstype': 'LARHIST'}, }, 
     'tasktransinfo': { 'trfpath': 'LArHistMerge_trf.py',
-                       'trfsetupcmd': '/afs/cern.ch/atlas/tzero/prod1/inst/projects/data11/setup/setuptrf.sh '+patcharea+' RELEASE /afs/cern.ch/atlas/tzero/prod1/inst/projects/data11/setup/specialsetup_tier0.sh' },
+                       'trfsetupcmd': trfsetuppath+'setuptrf.sh '+patcharea+' RELEASE '+trfsetuppath+'specialsetup_tier0.sh' },
     'phconfig': {}, 
-    'description': 'Merges LAr Samples monitoring ntuples.. Uses RELEASE' 
+    'description': 'Merges LAr Monitoring Histograms. Uses RELEASE' 
     }
 
 
@@ -41,7 +64,7 @@ processConfigs['histmerge'] = {
 
 
 
-description_var = '. Used in production from November 2011 onwards.'
+description_var = '. Used in production from Feb 2015 onwards.'
 
 # example release: AtlasTier0-15.5.3.7
 
@@ -104,7 +127,8 @@ if __name__ == '__main__':
     #------------------------
     #Build final AMI tag info
     s={} #stable values
-    s['configTag']=amiTag
+    s['amiTag']=amiTag
+    s['tagType']=amiTag[0]
     s['AMIUser']=login
     s['AMIPass']=password
 
@@ -112,36 +136,40 @@ if __name__ == '__main__':
     c['inputs']=str(processConfigs[process]['inputs'].__str__())
     c['outputs']=str(processConfigs[process]['outputs'].__str__())
     c['SWReleaseCache']=str(release)
-    if process=='reco':
-        c['Geometry']='ATLAS-GEO-18-01-01'
-        c['ConditionsTag']='COMCOND-ES1PA-006-01'
+    if process=='reco' or process=='noisebursts':
+        c['Geometry']='ATLAS-R2-2015-02-00-00'
+        c['ConditionsTag']='CURRENT'
     else: #merging case
         c['Geometry']='n/a'
         c['ConditionsTag']='n/a'
     c['phconfig']=str(processConfigs[process]['phconfig'].__str__())
     c['transformation']=str(moreInfoDic['tasktransinfo']['trfpath'])
-    c['trfsetupcmd']="\""+str(moreInfoDic['tasktransinfo']['trfsetupcmd'])+"\""
+    c['trfsetupcmd']=str(moreInfoDic['tasktransinfo']['trfsetupcmd'])
     c['moreInfo']=str(moreInfoDic.__str__())
     c['description']=processConfigs[process]['description'].replace('RELEASE', str(release)) + description_var 
 
 
     #Upload info to AMI
     if doWhat=="create":
-        l=['AddConfigurationTag']
+        l=['AddAMITag']
         for k in s.keys():
-            l.append(k+'='+s[k])
+            l.append(k+"=\""+s[k]+"\"")
         for k in c.keys():
-            l.append(k+'='+c[k])
+            l.append(k+"=\""+c[k]+"\"")
             
         print "l=",l
         result=amiclient.execute(l)
+        
+        print " ================================================ "
+
+        print result
         print "\n\n###############################################"
         print "#  Successfully created new tag %s !!   :-)  #"%amiTag
         print "###############################################\n\n"        
 
 
     elif doWhat=="update":
-        l=['UpdateConfigurationTag']
+        l=['UpdateAMITag']
         for k in s.keys():
             l.append(k+'='+s[k])
 
@@ -167,6 +195,9 @@ if __name__ == '__main__':
         
         #print "l=",l
         result=amiclient.execute(l)
+        print result
+
+
         print "\n\n#####################################################"
         print "#  Succesfully updated existing tag %s !!!   :-)  #"%amiTag
         print "#####################################################\n\n"        
