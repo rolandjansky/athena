2bf7cc67fd2696565ed63b11eac469691bc5e0f9
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-08-03 09:08:31 +0200

    Adopt package to new location of Barcode.h and PhysicsProcessCode.h files (moved from BarcodeInterfaces package into new BarcodeEvent package). This is to follow ATLAS conventions about providing event-data types in dedicated *Event packages. ATLASSIM-2281 (ISF_PunchThroughTools-00-02-00)
    	* Adopt package to new location of Barcode.h and PhysicsProcessCode.h files
    	(moved from BarcodeInterfaces package into new BarcodeEvent package). This is
    	to follow ATLAS conventions about providing event-data types in dedicated
    	*Event packages. ATLASSIM-2281
    	* Tagging as ISF_PunchThroughTools-00-02-00
    
    2016-07-19  John Chapman  <John.Chapman@cern.ch>
    	* Move headers to src directory. ATLASSIM-2390
    	* src/*.* - white-space clean-up.
    	* CMakeLists.txt, cmt/requirements - move public dependencies to
    	be private.
    	* Tagging as ISF_PunchThroughTools-00-01-04
    
    
    Former-commit-id: 3435ff27448b36ed4ceaee3b3b147e55f8c9fc79

--- Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/PunchThroughParticle.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/PunchThroughParticle.cxx
@@ -5,13 +5,13 @@
 ///////////////////////////////////////////////////////////////////
 // PunchThroughParticle.cxx, (c) ATLAS Detector software
 ///////////////////////////////////////////////////////////////////
-	
+
 // class header
-#include "ISF_PunchThroughTools/PunchThroughParticle.h"
-	
+#include "PunchThroughParticle.h"
+
 // ROOT
 #include "TH2F.h"
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -31,24 +31,24 @@ ISF::PunchThroughParticle::PunchThroughParticle(int pdg, bool doAnti):
         m_histCorrLowE(0),
         m_histCorrHighE(0),
         m_corrHistDomains(0),
-        m_pdfNumParticles(0),	//does this number ever change?
+        m_pdfNumParticles(0),//does this number ever change?
         m_pdfExitEnergy(0),
         m_pdfExitDeltaTheta(0),
         m_pdfExitDeltaPhi(0),
         m_pdfMomDeltaTheta(0),
         m_pdfMomDeltaPhi(0)
 { }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
  *=======================================================================*/
 
 void ISF::PunchThroughParticle::setMinEnergy(double minEnergy)
-{ 
+{
   m_minEnergy = minEnergy;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -58,7 +58,7 @@ void ISF::PunchThroughParticle::setMaxNumParticles(int maxNum)
 {
   m_maxNum = maxNum;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -68,7 +68,7 @@ void ISF::PunchThroughParticle::setNumParticlesFactor(double numFactor)
 {
   m_numParticlesFactor = numFactor;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -78,7 +78,7 @@ void ISF::PunchThroughParticle::setEnergyFactor(double energyFactor)
 {
   m_energyFactor = energyFactor;
 }
-	
+
 
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
@@ -118,7 +118,7 @@ void ISF::PunchThroughParticle::setCorrelation(int corrPdg,
     double minCorrelationEnergy,
     double fullCorrelationEnergy,
     double lowE,
-    double midE, 
+    double midE,
     double upperE)
 {
   m_corrPdg = corrPdg;
@@ -132,7 +132,7 @@ void ISF::PunchThroughParticle::setCorrelation(int corrPdg,
   m_corrHistDomains[1] = midE;
   m_corrHistDomains[2] = upperE;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -142,7 +142,7 @@ void ISF::PunchThroughParticle::setExitEnergyPDF(PDFcreator *pdf)
 {
   m_pdfExitEnergy = pdf;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
@@ -152,7 +152,7 @@ void ISF::PunchThroughParticle::setExitDeltaThetaPDF(PDFcreator *pdf)
 {
   m_pdfExitDeltaTheta = pdf;
 }
-	
+
 /*=========================================================================
  *  DESCRIPTION OF FUNCTION:
  *  ==> see headerfile
