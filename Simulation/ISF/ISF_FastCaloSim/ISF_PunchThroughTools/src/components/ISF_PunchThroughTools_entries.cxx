80ca07d3d5d8adca63c415a70ce8fc504d3e0815
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-08-03 09:08:31 +0200

    Adopt package to new location of Barcode.h and PhysicsProcessCode.h files (moved from BarcodeInterfaces package into new BarcodeEvent package). This is to follow ATLAS conventions about providing event-data types in dedicated *Event packages. ATLASSIM-2281 (ISF_PunchThroughTools-00-02-00)
    	* Adopt package to new location of Barcode.h and PhysicsProcessCode.h files
    	(moved from BarcodeInterfaces package into new BarcodeEvent package). This is
    	to follow ATLAS conventions about providing event-data types in dedicated
    	*Event packages. ATLASSIM-2281
    	* Tagging as ISF_PunchThroughTools-00-02-00
    
    2016-07-19  John Chapman  <John.Chapman@cern.ch>
    	* Move headers to src directory. ATLASSIM-2390
    	* src/*.* - white-space clean-up.
    	* CMakeLists.txt, cmt/requirements - move public dependencies to
    	be private.
    	* Tagging as ISF_PunchThroughTools-00-01-04
    
    2016-03-14 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Tweaked the requirements file so that cmt2cmake.py would
    	  understand all of the statements in it.
    	* Tagging as ISF_PunchThroughTools-00-01-03

--- Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
@@ -1,8 +1,5 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-#include "ISF_PunchThroughTools/PunchThroughTool.h"
+#include "../PunchThroughTool.h"
 
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF,PunchThroughTool)
-DECLARE_FACTORY_ENTRIES( ISF_PunchThroughTools ) {
-  DECLARE_NAMESPACE_TOOL( ISF,PunchThroughTool)
-}
+DECLARE_NAMESPACE_TOOL_FACTORY(ISF, PunchThroughTool)
 
