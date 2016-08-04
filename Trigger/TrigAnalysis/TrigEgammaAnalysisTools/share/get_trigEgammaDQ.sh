bf39907f1dd78b29b20b9562feea966eab0e45a9
Ryan Mackenzie White <ryan.white@cern.ch>
2016-08-04 15:55:16 +0200

    update macro for new TP triggers (TrigEgammaAnalysisTools-00-01-23)
    	* Requires TrigEgammaMonitoring-00-01-75
    	* Update macro to new list of TP triggers
    	* TrigEgammaAnalysisTools-00-01-23
    
    2016-07-22 Moritz Backes <Moritz.Backes@cern.ch>
    	* Fix to previous tag
    	* TrigEgammaAnalysisTools-00-01-22
    
    2016-07-22 Moritz Backes <Moritz.Backes@cern.ch>
    	* Reference run support for DQ scripts
    	* TrigEgammaAnalysisTools-00-01-21
    
    2016-07-19 Ryan White <ryan.white@cern.ch>
    	* Revert to old TrigPassBits
    	* TrigEgammaAnalysisTools-00-01-20
    
    
    Former-commit-id: ea6e8a43cc2d4cc8a46fc73681269e41e8cdb7a7

--- Trigger/TrigAnalysis/TrigEgammaAnalysisTools/share/get_trigEgammaDQ.sh
+++ Trigger/TrigAnalysis/TrigEgammaAnalysisTools/share/get_trigEgammaDQ.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 display_usage() { 
-    echo -e "\nUsage:\n$0 [runNumber] [dataCampaign] [stream] \n" 
+    echo -e "\nUsage:\n$0 [runNumber] [dataCampaign] [stream] [refNumber] \n" 
 } 
 if [  $# -le 0 ] 
 then 
@@ -22,42 +22,75 @@ then
 else
     data=${2}
 fi
-if [ -z "$2" ]
+
+if [ -z "$3" ]
 then
     echo "No stream, using physics_Main"
 else
     stream=${3}
 fi
 
-echo $run $data $stream
+if [ -z "$4" ]
+then
+    echo "No reference run number supplied, no reference is used"
+else
+    refrun=${4}
+fi
+
+
+echo $run $data $stream $refrun
 source /afs/cern.ch/project/eos/installation/atlas/etc/setup.sh
 
-# BASE=/eos/atlas/atlastier0/rucio/data15_cos/express_express/00259416
-# DIR1=`eos ls ${BASE} | grep HIST.x`
-# FILETOCOPY=`eos ls ${BASE}/${DIR1} | grep HIST`
-# echo "TEST ${BASE}/${DIR1}/${FILETOCOPY}"
-# eos cp ${BASE}/${DIR1}/${FILETOCOPY} .
-
-# for file in `echo ${LIST}`
-# do
 BASE=/eos/atlas/atlastier0/rucio/${data}/${stream}/00${run}
-echo $BASE    
+echo $BASE 
+echo eos ls ${BASE} | grep HIST.x
+echo eos ls ${BASE} | grep HIST.f
 DIR1=`eos ls ${BASE} | grep HIST.x`
 DIR2=`eos ls ${BASE} | grep HIST.f`
 echo $DIR1 $DIR2
+
 if [ -n "$DIR1" ]; then
-    FILETOCOPY=`eos ls ${BASE}/${DIR1} | grep HIST`
-    echo "TEST ${BASE}/${DIR1}/${FILETOCOPY}"
-    eos cp ${BASE}/${DIR1}/${FILETOCOPY} .
+    FILETOCOPY1=`eos ls ${BASE}/${DIR1} | grep HIST`
+    echo "TEST ${BASE}/${DIR1}/${FILETOCOPY1}"
+    eos cp ${BASE}/${DIR1}/${FILETOCOPY1} .
 else
-    FILETOCOPY=`eos ls ${BASE}/${DIR2} | grep HIST`
-    echo "TEST ${BASE}/${DIR2}/${FILETOCOPY}"
-    eos cp ${BASE}/${DIR2}/${FILETOCOPY} .
+    FILETOCOPY1=`eos ls ${BASE}/${DIR2} | grep HIST`
+    echo "TEST ${BASE}/${DIR2}/${FILETOCOPY1}"
+    eos cp ${BASE}/${DIR2}/${FILETOCOPY1} .
 fi    
-inputfile=`ls | grep HIST`
-hcg.exe ${inputfile} -d HLT/Egamma -r Egamma TREG -ds "https://twiki.cern.ch/twiki/bin/view/Atlas/TrigEgammaDataQualityAndMonitoring" -a "HLT_Histogram_Not_Empty_with_Ref&GatherData" --deleteref > ${data}.${stream}.$run.config
-trigEgammaDQ.py --file ${inputfile} --run ${run} --t0 True
-rm *.bak
+
+if [ -n "$refrun" ]; then
+    BASEREF=/eos/atlas/atlastier0/rucio/${data}/${stream}/00${refrun}
+    echo $BASEREF 
+    echo eos ls ${BASEREF} | grep HIST.x
+    echo eos ls ${BASEREF} | grep HIST.f
+    DIR3=`eos ls ${BASEREF} | grep HIST.x`
+    DIR4=`eos ls ${BASEREF} | grep HIST.f`
+    echo $DIR3 $DIR4
+
+    if [ -n "$DIR3" ]; then
+	FILETOCOPY2=`eos ls ${BASEREF}/${DIR3} | grep HIST`
+	echo "TEST ${BASEREF}/${DIR3}/${FILETOCOPY2}"
+	eos cp ${BASEREF}/${DIR3}/${FILETOCOPY2} .
+    else
+	FILETOCOPY2=`eos ls ${BASEREF}/${DIR4} | grep HIST`
+	echo "TEST ${BASEREF}/${DIR4}/${FILETOCOPY2}"
+	eos cp ${BASEREF}/${DIR4}/${FILETOCOPY2} .
+    fi    
+fi
+
+
+
+
+#hcg.exe ${inputfile} -d HLT/Egamma -r Egamma TREG -ds "https://twiki.cern.ch/twiki/bin/view/Atlas/TrigEgammaDataQualityAndMonitoring" -a "HLT_Histogram_Not_Empty_with_Ref&GatherData" --deleteref > ${data}.${stream}.$run.config
+
+if [ -n "$refrun" ]; then
+    trigEgammaDQ.py --file ${FILETOCOPY1} --run ${run} --t0 True --ref ${FILETOCOPY2} --refrun ${refrun}
+else 
+    trigEgammaDQ.py --file ${FILETOCOPY1} --run ${run} --t0 True
+fi
+
+#rm *.bak
 #tar -czf TrigEgammaDQArchive_Run_${run}.tar.gz ${data}.* *.C *.eps --remove-files
 
 
