dff2f6dbafd914cecfb23b0ecba837780e128d14
Jahred Adelman <jahreda@gmail.com>
2016-08-05 13:26:00 +0200

    add SSB data flow (TrigFTKPool-00-00-08)

--- Trigger/TrigFTK/TrigFTKPool/src/FTKTrackFitterStats_p1.cxx
+++ Trigger/TrigFTK/TrigFTKPool/src/FTKTrackFitterStats_p1.cxx
@@ -5,6 +5,6 @@
 #include "TrigFTKPool/FTKTrackFitterStats_p1.h"
 
 FTKTrackFitterStats_p1::FTKTrackFitterStats_p1() :
-  m_ncombs(0), m_nfits(0), m_nfits_maj(0), m_nfits_rec(0), m_nfits_addrec(0),
+  m_ncombs(0), m_nfits(0), m_nfits_maj(0), m_nfits_maj_SCT(0), m_nfits_maj_pix(0), m_nfits_rec(0), m_nfits_addrec(0),
   m_nfits_bad(0), m_nfits_rej(0), m_nfits_badmaj(0), m_nfits_rejmaj(0)
 {}
