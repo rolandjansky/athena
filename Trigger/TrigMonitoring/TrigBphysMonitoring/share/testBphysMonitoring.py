3e380d8a1a9b0408da5411ab4105bf00c6471e53
Scott Snyder <scott.snyder@cern.ch>
2016-08-10 02:32:16 +0200

    'endreq -> endmsg.' (TrigBphysMonitoring-00-00-64)
    	* Tagging TrigBphysMonitoring-00-00-64.
    	* endreq -> endmsg.
    
    2016-07-02 Semen Turchikhin <Semen.Turchikhin@cern.ch>
    	* added share/testBphysMonitoring.py JO for quick
    	* local tests
    
    
    Former-commit-id: cb78e1c36f30b41e41ee5998deb6a288b5e1ac38

---
+++ Trigger/TrigMonitoring/TrigBphysMonitoring/share/testBphysMonitoring.py
@@ -0,0 +1,79 @@
+# JO for quick testing Bphys monitoring.
+# When running on AOD produced by RTT TriggerTest, 
+# JpsiFinder histograms will be empty as there are no
+# offline muon containers.
+
+from RecExConfig.RecFlags import rec
+from AthenaCommon.AthenaCommonFlags import athenaCommonFlags as acf
+
+if not ('fileList' in dir()):
+  fileList=['root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/proj-sit/rtt/prod/rtt/rel_2/20.7.X.Y-VAL/x86_64-slc6-gcc49-opt/offline/TriggerTest/Bphysics_toAOD/AOD.pool.root']
+
+# allow this to work in RTT
+if not fileList == []:
+    acf.FilesInput=fileList
+
+
+if not 'RootNtupleOutput' in dir():
+    rec.RootNtupleOutput="ntuple_fromAOD.root"
+
+rec.doCBNT=False
+rec.readESD=True
+rec.doWriteESD=False 
+rec.doWriteAOD=False 
+rec.doAOD=False
+rec.doDPD=False 
+rec.doWriteTAG=False 
+
+#doTrigger=True
+#SkipEvents=840
+
+if not acf.EvtMax.is_locked():
+    acf.EvtMax=-1
+if not ('OutputLevel' in dir()):
+    #OutputLevel=DEBUG
+    rec.OutputLevel=INFO
+
+#-----------------------------------------------------------
+include("RecExCond/RecExCommon_flags.py")
+#-----------------------------------------------------------
+from TriggerJobOpts.TriggerFlags import TriggerFlags
+TriggerFlags.configurationSourceList.set_Value_and_Lock( [ "ds" ] )
+
+from TriggerJobOpts.TriggerConfigGetter import TriggerConfigGetter
+TriggerConfigGetter()
+
+
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence = AlgSequence()
+
+include("TrigBphysMonitoring/RunJpsiFinder.py")
+
+from AthenaMonitoring.AthenaMonitoringConf import AthenaMonManager
+topSequence += AthenaMonManager( "HLTMonManager")
+HLTMonManager = topSequence.HLTMonManager
+
+################ Mon Tools #################
+
+#Global HLTMonTool
+
+from TrigHLTMonitoring.TrigHLTMonitoringConf import HLTMonTool
+HLTMon = HLTMonTool(name               = 'HLTMon',
+                   histoPathBase      = "/Trigger/HLT");
+
+#HLTMon.TrigDecisionTool = monTrigDecTool
+
+ToolSvc += HLTMon;
+
+HLTMonManager.AthenaMonTools += [ "HLTMonTool/HLTMon" ];
+
+from TrigBphysMonitoring.TrigBphysMonitoringConfig import TrigBphysMonitoringTool
+HLTMonManager.AthenaMonTools += TrigBphysMonitoringTool()
+
+HLTMonManager.FileKey = "GLOBAL"
+
+print HLTMonManager;
+
+# main jobOption
+include ("RecExCommon/RecExCommon_topOptions.py")
+
