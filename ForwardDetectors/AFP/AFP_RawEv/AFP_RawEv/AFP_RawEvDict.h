4fc44f8783c7efb14c24616c8ef755e842acaca6
Nikolai Fomin <nikolai.fomin@cern.ch>
2016-08-11 17:44:17 +0200

    cmake should work (AFP_RawEv-00-00-02)
    Former-commit-id: 24560a48e17bf548b511002cf26c9bb46f962552

---
+++ ForwardDetectors/AFP/AFP_RawEv/AFP_RawEv/AFP_RawEvDict.h
@@ -0,0 +1,16 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef AFP_RawEvDict_h
+#define AFP_RawEvDict_h
+
+//#include "AFP_RawEv/AFP_DigitCollection.h"
+//#include "AFP_RawEv/AFP_ODDigitCollection.h"
+
+#include "AFP_RawEv/AFP_RawDataCollection.h"
+#include "AFP_RawEv/AFP_RawDataContainer.h"
+
+#endif
+
+
