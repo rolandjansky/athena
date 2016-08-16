4a47445896bb942f1388d860e9c76270f667144d
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-08-16 12:03:40 +0200

    Merge ISF_Services-00-08-00-branch into trunk (ATLASSIM-3029): Adopt package to new location of Barcode.h and PhysicsProcessCode.h files. ATLASSIM-2281 + Register CfgGetter method for ISF_ParticleKillerSvc. ATLASSIM-2995 + Add getParticleKillerSvc CfgGetter method. ATLASSIM-2995 + Add support for pileup input collection to new ISF input processing implementation. ATLASSIM-2928 + Remove no longer needed ISF_LongLivedParticleBrokerSvc configuration as it was made obsolete by ISF_LongLivedInputConverter. ATLASSIM-2928 + Update long-lived particle simulation to new ISF input processing. ATLASSIM-2928 + Refactor ISF Input Processing for AthenaMT. ATLASSIM-2928 + Add first tests for new InputConverter AthService (ISF_Services-01-01-00)
    	* Merge ISF_Services-00-08-00-branch into trunk (ATLASSIM-3029):
    		* Adopt package to new location of Barcode.h and PhysicsProcessCode.h files
    		(moved from BarcodeInterfaces package into new BarcodeEvent package). This is
    		to follow ATLAS conventions about providing event-data types in dedicated
    		*Event packages. ATLASSIM-2281
    		* python/ISF_ServicesConfigDb.py - remove unneeded imports.
    		Register CfgGetter method for ISF_ParticleKillerSvc. ATLASSIM-2995
    		* python/ISF_ServicesConfig.py - add getParticleKillerSvc
    		CfgGetter method. ATLASSIM-2995. General tidying of syntax.
    		* Add support for pileup input collection to new
    		ISF input processing implementation. Crucial for
    		FastChain with pilup simulation. ATLASSIM-2928
    		* python/ISF_ServicesConfigDb.py - remove no longer needed
    		ISF_LongLivedParticleBrokerSvc configuration as it was made obsolete by
    		ISF_LongLivedInputConverter. ATLASSIM-2928
    		* Update long-lived particle simulation to new ISF input processing.
    		ATLASSIM-2928
    		* Refactor ISF Input Processing for AthenaMT. ATLASSIM-2928
    		* Add first tests for new InputConverter AthService
    	* ISF_Services-01-01-00
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: fde4cb65338ebebc1457ee57864b01b317fdcbd7

--- Simulation/ISF/ISF_Core/ISF_Services/python/ISF_ServicesConfig.py
+++ Simulation/ISF/ISF_Core/ISF_Services/python/ISF_ServicesConfig.py
@@ -11,67 +11,159 @@ from AthenaCommon.Constants import *  # FATAL,ERROR etc.
 from AthenaCommon.SystemOfUnits import *
 
 def getParticleBrokerSvcNoOrdering(name="ISF_ParticleBrokerSvcNoOrdering", **kwargs):
-    kwargs.setdefault('StackFiller'                 , 'ISF_StackFiller'                     )
-    kwargs.setdefault('EntryLayerTool'              , 'ISF_EntryLayerTool'                  )
-    kwargs.setdefault('GeoIDSvc'                    , 'ISF_GeoIDSvc'                        )
-    kwargs.setdefault('AlwaysUseGeoIDSvc'           , False                                 )
+    kwargs.setdefault('EntryLayerTool', 'ISF_EntryLayerTool')
+    kwargs.setdefault('GeoIDSvc', 'ISF_GeoIDSvc')
+    kwargs.setdefault('AlwaysUseGeoIDSvc', False)
     from ISF_Config.ISF_jobProperties import ISF_Flags
-    kwargs.setdefault('ValidateGeoIDs'              , ISF_Flags.ValidationMode()            )
-    kwargs.setdefault('ValidationOutput'            , ISF_Flags.ValidationMode()            )
-    kwargs.setdefault('ValidationStreamName'        , "ParticleBroker"                      )
-    kwargs.setdefault('BarcodeService'              , ISF_Flags.BarcodeService()            )
+    kwargs.setdefault('ValidateGeoIDs', ISF_Flags.ValidationMode())
+    kwargs.setdefault('ValidationOutput', ISF_Flags.ValidationMode())
+    kwargs.setdefault('ValidationStreamName', "ParticleBroker")
+    kwargs.setdefault('BarcodeService', ISF_Flags.BarcodeService())
     return CfgMgr.ISF__ParticleBrokerDynamicOnReadIn(name, **kwargs)
 
 def getParticleBrokerSvc(name="ISF_ParticleBrokerSvc", **kwargs):
-    #kwargs.setdefault('ParticleOrderingTool'       , 'ISF_InToOutSubDetOrderingTool'       )
-    kwargs.setdefault('ParticleOrderingTool'        , 'ISF_ParticleOrderingTool'            )
-    return getParticleBrokerSvcNoOrdering(name, **kwargs)
-
-def getLongLivedParticleBrokerSvc(name="ISF_LongLivedParticleBrokerSvc", **kwargs):
-    kwargs.setdefault('StackFiller'                 , 'ISF_LongLivedStackFiller'            )
+    #kwargs.setdefault('ParticleOrderingTool', 'ISF_InToOutSubDetOrderingTool')
+    kwargs.setdefault('ParticleOrderingTool', 'ISF_ParticleOrderingTool')
     return getParticleBrokerSvcNoOrdering(name, **kwargs)
 
 def getAFIIParticleBrokerSvc(name="ISF_AFIIParticleBrokerSvc", **kwargs):
-    kwargs.setdefault('EntryLayerTool'              , 'ISF_AFIIEntryLayerTool'              )
+    kwargs.setdefault('EntryLayerTool', 'ISF_AFIIEntryLayerTool')
     return getParticleBrokerSvc(name, **kwargs)
 
 def getSimHitService(name="ISF_SimHitService", **kwargs):
     from ISF_Config.ISF_jobProperties import ISF_Flags
-    kwargs.setdefault('ValidationOutput'    , ISF_Flags.ValidationMode()                    )
+    kwargs.setdefault('ValidationOutput', ISF_Flags.ValidationMode())
     return CfgMgr.ISF__SimHitSvc(name, **kwargs)
 
 def getNoG4SimHitService(name="ISF_NoG4SimHitService", **kwargs):
-    kwargs.setdefault("SensitiveDetectorMasterTool","EmptySensitiveDetectorMasterTool")
-    kwargs.setdefault("FastSimulationMasterTool"   ,"EmptyFastSimulationMasterTool"   )
+    kwargs.setdefault("SensitiveDetectorMasterTool", "EmptySensitiveDetectorMasterTool")
+    kwargs.setdefault("FastSimulationMasterTool", "EmptyFastSimulationMasterTool")
     return getSimHitService(name, **kwargs)
 
 def getPileupSimHitService(name="ISF_PileupSimHitService", **kwargs):
-    kwargs.setdefault('SeparateInDetPileupHits'    , True )
+    kwargs.setdefault('SeparateInDetPileupHits', True)
     return getNoG4SimHitService(name, **kwargs)
 
 def getISFEnvelopeDefSvc(name="ISF_ISFEnvelopeDefSvc", **kwargs):
     # ATLAS common envlope definitions
-    kwargs.setdefault("ATLASEnvelopeDefSvc"    , "AtlasGeometry_EnvelopeDefSvc")
+    kwargs.setdefault("ATLASEnvelopeDefSvc", "AtlasGeometry_EnvelopeDefSvc")
+    return CfgMgr.ISF__ISFEnvelopeDefSvc(name, **kwargs)
     return CfgMgr.ISF__ISFEnvelopeDefSvc(name, **kwargs)
 
 def getAFIIEnvelopeDefSvc(name="ISF_AFIIEnvelopeDefSvc", **kwargs):
     from AthenaCommon.SystemOfUnits import mm
     # ATLAS common envlope definitions
-    kwargs.setdefault("ISFEnvelopeDefSvc"    , "ISF_ISFEnvelopeDefSvc"         )
-    kwargs.setdefault("InDetMaxExtentZ"      , 3549.5*mm                       )
+    kwargs.setdefault("ISFEnvelopeDefSvc", "ISF_ISFEnvelopeDefSvc")
+    kwargs.setdefault("InDetMaxExtentZ", 3549.5*mm)
+    return CfgMgr.ISF__AFIIEnvelopeDefSvc(name, **kwargs)
     return CfgMgr.ISF__AFIIEnvelopeDefSvc(name, **kwargs)
 
 
 def getGeoIDSvc(name="ISF_GeoIDSvc", **kwargs):
     # with ISF volume definitions
-    kwargs.setdefault("EnvelopeDefSvc"          , "ISF_ISFEnvelopeDefSvc"      )
+    kwargs.setdefault("EnvelopeDefSvc", "ISF_ISFEnvelopeDefSvc")
+    return CfgMgr.ISF__GeoIDSvc(name, **kwargs)
     return CfgMgr.ISF__GeoIDSvc(name, **kwargs)
 
 
 def getAFIIGeoIDSvc(name="ISF_AFIIGeoIDSvc", **kwargs):
-    kwargs.setdefault("EnvelopeDefSvc"          , "ISF_AFIIEnvelopeDefSvc"     )
+    kwargs.setdefault("EnvelopeDefSvc", "ISF_AFIIEnvelopeDefSvc")
     return getGeoIDSvc(name, **kwargs)
 
 def getParticleKillerSvc(name="ISF_ParticleKillerSvc", **kwargs):
     kwargs.setdefault('Identifier',           "ParticleKiller")
     return CfgMgr.ISF__ParticleKillerSimSvc(name, **kwargs)
+
+def getInputConverter(name="ISF_InputConverter", **kwargs):
+    kwargs.setdefault("UseGeneratedParticleMass", False)
+    genParticleFilters = ['ISF_ParticleFinalStateFilter']
+    from AthenaCommon.BeamFlags import jobproperties
+    if jobproperties.Beam.beamType() != "cosmics":
+        genParticleFilters += ['ISF_ParticlePositionFilterDynamic',
+                                'ISF_EtaPhiFilter']
+    genParticleFilters += ['ISF_GenParticleInteractingFilter']
+    kwargs.setdefault("GenParticleFilters", genParticleFilters)
+    return CfgMgr.ISF__InputConverter(name, **kwargs)
+
+def getLongLivedInputConverter(name="ISF_LongLivedInputConverter", **kwargs):
+    kwargs.setdefault("GenParticleFilters"      , [ 'ISF_ParticleSimWhiteList',
+                                                    'ISF_ParticlePositionFilterDynamic',
+                                                    'ISF_EtaPhiFilter',
+                                                    'ISF_GenParticleInteractingFilter', ] )
+    return getInputConverter(name, **kwargs)
+
+def getGenericTruthService(name="ISF_TruthService", **kwargs):
+    from ISF_Config.ISF_jobProperties import ISF_Flags
+    kwargs.setdefault('BarcodeSvc', ISF_Flags.BarcodeService())
+    kwargs.setdefault('SkipIfNoChildren', True)
+    kwargs.setdefault('SkipIfNoParentBarcode', True)
+    kwargs.setdefault('ForceEndVtxInRegions', [])
+    if 'longLived' in ISF_Flags.Simulator(): #FIXME this should be configured in a nicer way. ATLASSIM-526
+        kwargs.setdefault('QuasiStableParticlesIncluded', True)
+    return CfgMgr.ISF__TruthSvc(name, **kwargs)
+
+def getMC15TruthService(name="ISF_MC15TruthService", **kwargs):
+    # importing Reflex dictionary to access AtlasDetDescr::AtlasRegion enum
+    import ROOT, cppyy
+    cppyy.loadDictionary('AtlasDetDescrDict')
+    AtlasRegion = ROOT.AtlasDetDescr
+    kwargs.setdefault('BeamPipeTruthStrategies', ['ISF_MCTruthStrategyGroupID_MC15']) # this is used for beam pipe but not BeamPipeCentral which uses same as ID
+    kwargs.setdefault('IDTruthStrategies', ['ISF_MCTruthStrategyGroupID_MC15', 'ISF_MCTruthStrategyGroupIDHadInt_MC15'])
+    kwargs.setdefault('CaloTruthStrategies', ['ISF_MCTruthStrategyGroupCaloMuBrem', 'ISF_MCTruthStrategyGroupCaloDecay_MC15'])
+    kwargs.setdefault('MSTruthStrategies', [])
+    kwargs.setdefault('IgnoreUndefinedBarcodes', False)
+    kwargs.setdefault('PassWholeVertices', False) # new for MC15 - can write out partial vertices.
+    kwargs.setdefault('ForceEndVtxInRegions', [AtlasRegion.fAtlasID])
+    return getGenericTruthService(name, **kwargs)
+
+def getMC15aTruthService(name="ISF_MC15aTruthService", **kwargs):
+    kwargs.setdefault('ForceEndVtxInRegions', [])
+    return getMC15TruthService(name, **kwargs)
+
+def getMC15aPlusTruthService(name="ISF_MC15aPlusTruthService", **kwargs):
+    # importing Reflex dictionary to access AtlasDetDescr::AtlasRegion enum
+    import ROOT, cppyy
+    cppyy.loadDictionary('AtlasDetDescrDict')
+    AtlasRegion = ROOT.AtlasDetDescr
+    kwargs.setdefault('ForceEndVtxInRegions', [AtlasRegion.fAtlasID])
+    return getMC15TruthService(name, **kwargs)
+
+def getMC12TruthService(name="ISF_MC12TruthService", **kwargs):
+    kwargs.setdefault('BeamPipeTruthStrategies', ['ISF_MCTruthStrategyGroupID']) # this is used for beam pipe but not BeamPipeCentral which uses same as ID
+    kwargs.setdefault('IDTruthStrategies', ['ISF_MCTruthStrategyGroupID', 'ISF_MCTruthStrategyGroupIDHadInt'])
+    kwargs.setdefault('CaloTruthStrategies', ['ISF_MCTruthStrategyGroupCaloMuBrem'])
+    kwargs.setdefault('MSTruthStrategies', [])
+    kwargs.setdefault('IgnoreUndefinedBarcodes', False)
+    kwargs.setdefault('PassWholeVertices', True)
+    return getGenericTruthService(name, **kwargs)
+
+def getMC12LLPTruthService(name="ISF_MC12TruthLLPService", **kwargs):
+    kwargs.setdefault('BeamPipeTruthStrategies', ['ISF_MCTruthStrategyGroupID', 'ISF_LLPTruthStrategy']) # this is used for beam pipe but not BeamPipeCentral which uses same as ID
+    kwargs.setdefault('IDTruthStrategies', ['ISF_MCTruthStrategyGroupID', 'ISF_MCTruthStrategyGroupIDHadInt', 'ISF_LLPTruthStrategy'])
+    kwargs.setdefault('CaloTruthStrategies', ['ISF_MCTruthStrategyGroupCaloMuBrem', 'ISF_LLPTruthStrategy'])
+    kwargs.setdefault('MSTruthStrategies', ['ISF_LLPTruthStrategy'])
+    return getMC12TruthService(name, **kwargs)
+
+def getMC12PlusTruthService(name="ISF_MC12PlusTruthService", **kwargs):
+    # importing Reflex dictionary to access AtlasDetDescr::AtlasRegion enum
+    import ROOT, cppyy
+    cppyy.loadDictionary('AtlasDetDescrDict')
+    AtlasRegion = ROOT.AtlasDetDescr
+    kwargs.setdefault('ForceEndVtxInRegions', [AtlasRegion.fAtlasID] )
+    return getMC12TruthService(name, **kwargs)
+
+def getValidationTruthService(name="ISF_ValidationTruthService", **kwargs):
+    kwargs.setdefault('BeamPipeTruthStrategies', [])
+    kwargs.setdefault('IDTruthStrategies', ['ISF_ValidationTruthStrategy'] )
+    kwargs.setdefault('CaloTruthStrategies', ['ISF_ValidationTruthStrategy'] )
+    kwargs.setdefault('MSTruthStrategies', [])
+    kwargs.setdefault('IgnoreUndefinedBarcodes', True)
+    kwargs.setdefault('PassWholeVertices', True)
+    return getGenericTruthService(name, **kwargs)
+
+def getTruthService(name="ISF_TruthService", **kwargs):
+    from ISF_Config.ISF_jobProperties import ISF_Flags
+    if ISF_Flags.ValidationMode() :
+      return getValidationTruthService(name, **kwargs)
+    else :
+      return getMC12TruthService(name, **kwargs)
---
+++ Simulation/ISF/ISF_Core/ISF_Services/src/InputConverter.cxx
@@ -0,0 +1,296 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+///////////////////////////////////////////////////////////////////
+// InputConverter.cxx, (c) ATLAS Detector software
+///////////////////////////////////////////////////////////////////
+
+// class header include
+#include "InputConverter.h"
+
+// framework
+#include "GaudiKernel/IPartPropSvc.h"
+#include "GaudiKernel/PhysicalConstants.h"
+
+// ISF_HepMC include
+#include "ISF_Event/TruthBinding.h"
+#include "ISF_HepMC_Interfaces/IGenParticleFilter.h"
+// barcode event and interfaces for bc creation
+#include "BarcodeEvent/Barcode.h"
+#include "BarcodeInterfaces/IBarcodeSvc.h"
+// ISF_Event include
+#include "ISF_Event/ISFParticle.h"
+#include "ISF_Event/ISFParticleContainer.h"
+#include "ISF_Event/ParticleUserInformation.h"
+// HepMC includes
+#include "HepMC/GenParticle.h"
+#include "HepMC/GenEvent.h"
+// McEventCollection
+#include "GeneratorObjects/McEventCollection.h"
+// CLHEP includes
+#include "CLHEP/Geometry/Point3D.h"
+#include "CLHEP/Geometry/Vector3D.h"
+// HepPDT
+#include "HepPDT/ParticleID.hh"
+#include "HepPDT/DecayData.hh"
+#include "HepPDT/ParticleDataTable.hh"
+
+
+/** Constructor **/
+ISF::InputConverter::InputConverter(const std::string& name, ISvcLocator* svc)
+    : AthService(name,svc),
+      m_particlePropSvc("PartPropSvc",name),
+      m_particleDataTable(nullptr),
+      m_useGeneratedParticleMass(false),
+      m_genParticleFilters()
+{
+  // particle mass from particle data table?
+  declareProperty("UseGeneratedParticleMass",
+                  m_useGeneratedParticleMass,
+                  "Use particle mass assigned to GenParticle.");
+  // particle filters
+  declareProperty("GenParticleFilters",
+                  m_genParticleFilters,
+                  "Tools for filtering out GenParticles.");
+  // the particle property service
+  declareProperty("ParticlePropertyService",
+                  m_particlePropSvc,
+                  "ParticlePropertyService to retrieve the PDT.");
+}
+
+
+/** Destructor **/
+ISF::InputConverter::~InputConverter()
+{
+}
+
+
+// Athena algtool's Hooks
+StatusCode
+ISF::InputConverter::initialize()
+{
+  ATH_MSG_VERBOSE("initialize() begin");
+
+  // setup PDT if requested (to get particle masses later on)
+  if (!m_useGeneratedParticleMass) {
+    ATH_CHECK( m_particlePropSvc.retrieve() );
+    m_particleDataTable = m_particlePropSvc->PDT();
+    if (!m_particleDataTable) {
+      ATH_MSG_FATAL( "Could not get ParticleDataTable from " << m_particlePropSvc << ". Abort" );
+      return StatusCode::FAILURE;
+    }
+  }
+
+  if (!m_genParticleFilters.empty()){
+    ATH_CHECK(m_genParticleFilters.retrieve());
+  }
+
+  ATH_MSG_VERBOSE("initialize() successful");
+  return StatusCode::SUCCESS;
+}
+
+
+/** Athena algtool Hook */
+StatusCode
+ISF::InputConverter::finalize()
+{
+  ATH_MSG_DEBUG("Finalizing ...");
+  return StatusCode::SUCCESS;
+}
+
+
+/** Convert selected particles from the given McEventCollection into ISFParticles
+    and push them into the given ISFParticleContainer */
+StatusCode
+ISF::InputConverter::convert(const McEventCollection& inputGenEvents,
+                             ISF::ISFParticleContainer& simParticles,
+                             bool isPileup) const
+{
+  for ( const auto& eventPtr : inputGenEvents ) {
+    // skip empty events
+    if (eventPtr == nullptr) continue;
+
+    // @FIXME: set the bunch-crossing identifier for pileup dynamically
+    // rather than a constant '1' (e.g. could use GenEvent index for that?)
+    int bcid = isPileup ? 1 : 0;
+
+    ATH_MSG_DEBUG("Starting conversion of GenEvent with"
+                  " signal_process_id=" << eventPtr->signal_process_id() <<
+                  " and event_number=" << eventPtr->event_number() );
+
+    // new collection containing all gen particles that passed filters
+    bool legacyOrdering = true; // FIXME: this is only to keep the same order of particles
+                                //        as the prior 'StackFiller' implementation
+                                // TODO:  remove this functionality from the
+                                //        getSelectedParticles function once
+                                //        happy with the results
+    const auto passedGenParticles = getSelectedParticles(*eventPtr, legacyOrdering);
+
+    for ( auto& genPartPtr : passedGenParticles ) {
+      ATH_MSG_VERBOSE("Picking up following GenParticle for conversion to ISFParticle: " <<  *genPartPtr);
+      auto simParticlePtr = convertParticle(genPartPtr, bcid);
+      if (!simParticlePtr) {
+        ATH_MSG_ERROR("Error while trying to convert input generator particles. Aborting.");
+        return StatusCode::FAILURE;
+      }
+      // add to collection that will be returned
+      simParticles.push_back(simParticlePtr);
+
+    } // loop over passed gen particles
+
+  } // loop over gen events
+
+  ATH_MSG_DEBUG( "Created initial simulation particle collection with size " << simParticles.size() );
+
+  return StatusCode::SUCCESS;
+}
+
+
+/** get all generator particles which pass filters */
+std::vector<HepMC::GenParticle*>
+ISF::InputConverter::getSelectedParticles(const HepMC::GenEvent& evnt, bool legacyOrdering) const {
+  auto allGenPartBegin = evnt.particles_begin();
+  auto allGenPartEnd = evnt.particles_end();
+
+  // reserve destination container with maximum size, i.e. number of particles in input event
+  std::vector<HepMC::GenParticle*> passedGenParticles{};
+  size_t maxParticles = std::distance(allGenPartBegin, allGenPartEnd);
+  passedGenParticles.reserve(maxParticles);
+
+  if (legacyOrdering) {
+    // FIXME: remove this block and the 'legacyOrdering' flag
+    //        once we don't need the legacy order any longer
+    auto vtxIt = evnt.vertices_begin();
+    auto vtxItEnd = evnt.vertices_end();
+    for ( ; vtxIt != vtxItEnd; ++vtxIt ) {
+      const auto vtxPtr = *vtxIt;
+      std::copy_if(vtxPtr->particles_begin(HepMC::children),
+                   vtxPtr->particles_end(HepMC::children),
+                   std::back_inserter(passedGenParticles),
+                   [this](HepMC::GenParticle* p){return this->passesFilters(*p);});
+    }
+
+  } else {
+    std::copy_if(allGenPartBegin,
+                 allGenPartEnd,
+                 std::back_inserter(passedGenParticles),
+                 [this](HepMC::GenParticle* p){return this->passesFilters(*p);});
+  }
+
+  passedGenParticles.shrink_to_fit();
+
+  return passedGenParticles;
+}
+
+
+/** get all generator particles which pass filters */
+ISF::ISFParticle*
+ISF::InputConverter::convertParticle(HepMC::GenParticle* genPartPtr, int bcid) const {
+
+  if (!genPartPtr)
+      return nullptr;
+
+  auto& genPart = *genPartPtr;
+
+  // -> particle origin (TODO: add proper GeoID, collision/cosmics)
+  DetRegionSvcIDPair origin(AtlasDetDescr::fUndefinedAtlasRegion, ISF::fEventGeneratorSimID);
+
+  // -> truth binding
+  ISF::TruthBinding* tBinding = new ISF::TruthBinding(genPartPtr);
+
+  // 4-vectors: position, momentum
+  HepMC::GenVertex* pVertex = genPart.production_vertex();
+  if (!pVertex) {
+    ATH_MSG_ERROR("Unable to convert following generator particle due to missing "
+                  << "production vertex: " << genPart);
+    return nullptr;
+  }
+  const auto& pVertexPos(pVertex->point3d());
+  const Amg::Vector3D pos(pVertexPos.x(), pVertexPos.y(), pVertexPos.z());
+  const auto& pMomentum(genPart.momentum());
+  const Amg::Vector3D mom(pMomentum.px(), pMomentum.py(), pMomentum.pz());
+
+  // get the pdg_id, mass & time, barcode
+  int pPdgId = genPart.pdg_id();
+  double pMass = getParticleMass(genPart);
+  double pTime = pVertex->position().t() / Gaudi::Units::c_light;
+  double charge = HepPDT::ParticleID(pPdgId).charge();
+  auto pBarcode = genPart.barcode();
+
+  auto sParticle = new ISF::ISFParticle( std::move(pos),
+                                         std::move(mom),
+                                         pMass,
+                                         charge,
+                                         pPdgId,
+                                         pTime,
+                                         origin,
+                                         bcid,
+                                         pBarcode,
+                                         tBinding );
+  return sParticle;
+}
+
+
+/** get right GenParticle mass */
+double
+ISF::InputConverter::getParticleMass(const HepMC::GenParticle &part) const {
+  // default value: generated particle mass
+  double mass = part.generated_mass();
+  ATH_MSG_VERBOSE("part.generated_mass, mass="<<mass);
+
+  // 1. use PDT mass?
+  if ( !m_useGeneratedParticleMass) {
+    int absPDG = abs(part.pdg_id());
+    HepPDT::ParticleData const *pData = (m_particleDataTable)
+      ? m_particleDataTable->particle(absPDG)
+      : nullptr ;
+    if (pData) {
+      mass = pData->mass();
+      ATH_MSG_VERBOSE("using pData mass, mass="<<mass);
+    }
+    else
+      ATH_MSG_WARNING( "Unable to find mass of particle with PDG ID '" << absPDG << "' in ParticleDataTable. Will set mass to generated_mass: " << mass);
+  }
+  return mass;
+}
+
+
+/** check if the given particle passes all filters */
+bool
+ISF::InputConverter::passesFilters(const HepMC::GenParticle& part) const {
+  // TODO: implement this as a std::find_if with a lambda function
+  for ( const auto& filter : m_genParticleFilters ) {
+    // determine if the particle passes current filter
+    bool passFilter = filter->pass(part);
+    ATH_MSG_VERBOSE("GenParticleFilter '" << filter.typeAndName() << "' returned: "
+                    << (passFilter ? "true, will keep particle."
+                        : "false, will remove particle."));
+    const auto& momentum = part.momentum();
+    ATH_MSG_VERBOSE("Particle: ("
+                    <<momentum.px()<<", "
+                    <<momentum.py()<<", "
+                    <<momentum.pz()<<"), pdgCode: "
+                    <<part.pdg_id() );
+
+    if (!passFilter) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+/** Query the interfaces. */
+StatusCode ISF::InputConverter::queryInterface(const InterfaceID& riid, void** ppvInterface) {
+
+  if ( IID_IInputConverter == riid )
+    *ppvInterface = (IInputConverter*)this;
+  else  {
+    // Interface is not directly available: try out a base class
+    return Service::queryInterface(riid, ppvInterface);
+  }
+  addRef();
+  return StatusCode::SUCCESS;
+}
