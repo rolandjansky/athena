f455238c4951528ceb012dcc3c4790f1ed1c3eb2
Attila Krasznahorkay <krasznaa@cern.ch>
2016-08-17 10:50:17 +0200

    Restored the original IDs of xAOD::ALFADataContainer_v1 and xAOD::ALFADataAuxContainer_v1 (xAODForward-00-01-12)
    	* Restored the original IDs for xAOD::ALFADataContainer_v1 and
    	  xAOD::ALFADataAuxContainer_v1 in the selection.xml file. So that
    	  old files could still be read with the new package version.
    	* Tagging as xAODForward-00-01-12
    
    2016-08-08 Grzegorz Gach <grzegorz.gach@cern.ch>
    	* Adding AFPSiHit and AFPTrack classes and containers
    	* Tagging as xAODForward-00-01-11
    
    2016-05-02 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Creating a new tag, with a slightly tweaked CMakeLists.txt
    	  file.
    	* Tagging as xAODForward-00-01-10
    
    
    Former-commit-id: b42095e5791b210c10511b20069e7eede1d9cda9

--- Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
+++ Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: ContainerProxies.cxx 712044 2015-12-02 07:14:06Z nozka $
+// $Id: ContainerProxies.cxx 766963 2016-08-08 20:07:09Z ggach $
 
 // EDM include(s):
 #include "xAODCore/AddDVProxy.h"
@@ -10,6 +10,8 @@
 // Local include(s):
 #include "xAODForward/versions/ALFADataContainer_v1.h"
 #include "xAODForward/versions/AFPDataContainer_v1.h"
+#include "xAODForward/versions/AFPSiHitContainer_v1.h"
+#include "xAODForward/versions/AFPTrackContainer_v1.h"
 #include "xAODForward/versions/ZdcModuleContainer_v1.h"
 #include "xAODForward/versions/MBTSModuleContainer_v1.h"
 #include "xAODForward/versions/ForwardEventInfoContainer_v1.h"
@@ -17,6 +19,8 @@
 // Set up the collection proxies:
 ADD_NS_DV_PROXY( xAOD, ALFADataContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, AFPDataContainer_v1 );
+ADD_NS_DV_PROXY( xAOD, AFPSiHitContainer_v1 );
+ADD_NS_DV_PROXY( xAOD, AFPTrackContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, ZdcModuleContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, MBTSModuleContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, ForwardEventInfoContainer_v1 );
---
+++ Event/xAOD/xAODForward/xAODForward/AFPSiHitContainer.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef XAODFORWARD_AFPSIHITCONTAINER_H
+#define XAODFORWARD_AFPSIHITCONTAINER_H
+
+#include"xAODForward/versions/AFPSiHitContainer_v1.h"
+
+namespace xAOD
+{
+  typedef AFPSiHitContainer_v1 AFPSiHitContainer;
+}
+
+#include "xAODCore/CLASS_DEF.h"
+CLASS_DEF( xAOD::AFPSiHitContainer, 1306814364, 1 )
+
+#endif	// XAODFORWARD_AFPSIHITCONTAINER_H
