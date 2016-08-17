5dc23cfcf27a8ad0e08aa263a5051b01bd4cf41f
Attila Krasznahorkay <krasznaa@cern.ch>
2016-08-17 10:50:17 +0200

    Restored the original IDs of xAOD::ALFADataContainer_v1 and xAOD::ALFADataAuxContainer_v1 (xAODForward-00-01-12)
    	* Restored the original IDs for xAOD::ALFADataContainer_v1 and
    	  xAOD::ALFADataAuxContainer_v1 in the selection.xml file. So that
    	  old files could still be read with the new package version.
    	* Tagging as xAODForward-00-01-12
    
    2016-08-08 Grzegorz Gach <grzegorz.gach@cern.ch>
    	* Adding AFPSiHit and AFPTrack classes and containers
    	* Tagging as xAODForward-00-01-11
    
    2016-05-02 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Creating a new tag, with a slightly tweaked CMakeLists.txt
    	  file.
    	* Tagging as xAODForward-00-01-10
    
    2016-03-03 Will Buttinger <will@cern.ch>
    	* Added Root/xAODForwardCLIDs.cxx - NOTE TO DEVELOPERS: You *must* remember to add new containers to the list in this file, otherwise their CLID wont be properly created at compile time
    	* Tagging as xAODForward-00-01-09
    
    2016-01-13 Libor Nozka <libor.nozka@cern.ch>
    		* add Afp* classes
    ...
    (Long ChangeLog diff - truncated)

---
+++ Event/xAOD/xAODForward/Root/AFPTrack_v1.cxx
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+ * @file   AFPTrack_v1.cxx
+ * @author Grzegorz Gach <grzegorz.gach@cern.ch>
+ * @date   2016-07-14
+ * 
+ * @brief  Implementation file for class xAOD::AFPTrack_v1
+ * 
+ */
+
+
+// xAOD include(s):
+#include "xAODCore/AuxStoreAccessorMacros.h"
+
+// Local include(s):
+#include "xAODForward/AFPSiHitContainer.h"
+#include "xAODForward/versions/AFPTrack_v1.h"
+
+namespace xAOD
+{
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, int, stationID, setStationID)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, xLocal, setXLocal)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, yLocal, setYLocal)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, zLocal, setZLocal)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, xSlope, setXSlope)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, ySlope, setYSlope)
+  // AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1,  float, zSlope, setZSlope)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, int, nHoles, setNHoles)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, int, nHits, setNHits)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, float, chi2, setChi2)
+  AUXSTORE_PRIMITIVE_SETTER_AND_GETTER (AFPTrack_v1, int, algID, setAlgID)
+
+  AUXSTORE_OBJECT_SETTER_AND_GETTER (AFPTrack_v1, std::vector< AFPTrack_v1::AFPHitLink_t >, hits, setHits)
+  static SG::AuxElement::Accessor< std::vector<AFPTrack_v1::AFPHitLink_t> > hitsAcc( "hits" );
+
+  void AFPTrack_v1::addHit( const AFPHitLink_t& link )
+  {
+    hitsAcc( *this ).push_back( link );
+  }
+
+  void AFPTrack_v1::toPersistent() {
+    // Prepare the hits links for persistification:
+    if( hitsAcc.isAvailableWritable( *this ) ) {
+      std::vector<AFPHitLink_t>::iterator end = hitsAcc( *this ).end();
+      for(std::vector<AFPHitLink_t>::iterator itr = hitsAcc( *this ).begin(); itr != end; ++itr )
+	itr->toPersistent();
+
+    }
+    
+      return;
+  }
+
+  
+}
