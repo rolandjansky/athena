a68c602f793024ef0c868208879165def99dfd7c
Jochen Meyer <Jochen.Meyer@cern.ch>
2016-08-22 17:08:48 +0200

    msg svc updates (MuonSimEventAthenaPool-00-02-05)
    	* Tagging MuonSimEventAthenaPool-00-02-05
    	* endreq -> endmsg; messageService() -> msgSvc()
    
    
    Former-commit-id: 2f1f065d47b73713d292873a8144f85e63bb778c

--- MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/src/GenericMuonSimHitCollectionCnv.cxx
+++ MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/src/GenericMuonSimHitCollectionCnv.cxx
@@ -26,14 +26,14 @@ GenericMuonSimHitCollectionCnv::~GenericMuonSimHitCollectionCnv() {
 }
 
 GenericMuonSimHitCollection_PERS*    GenericMuonSimHitCollectionCnv::createPersistent (GenericMuonSimHitCollection* transCont) {
-    MsgStream log(messageService(), "GenericMuonSimHitCollectionCnv" );
+    MsgStream log(msgSvc(), "GenericMuonSimHitCollectionCnv" );
     ATH_MSG_DEBUG("createPersistent(): main converter");
     GenericMuonSimHitCollection_PERS *pixdc_p= m_TPConverter_p1.createPersistent( transCont, log );
     return pixdc_p;
 }
 
 GenericMuonSimHitCollection* GenericMuonSimHitCollectionCnv::createTransient() {
-    MsgStream log(messageService(), "GenericMuonSimHitCollectionCnv" );
+    MsgStream log(msgSvc(), "GenericMuonSimHitCollectionCnv" );
     static pool::Guid   p1_guid("C2D5D558-2671-11E1-AAFA-001E4F3E5646"); 
     ATH_MSG_DEBUG("createTransient(): main converter");
     GenericMuonSimHitCollection* p_collection(0);
