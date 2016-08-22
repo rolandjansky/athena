f8a4c04def09b2930f22ca395070470a5f68f22f
Walter Lampl <Walter.Lampl@cern.ch>
2016-08-22 08:14:18 +0200

    cmake: Add missing dependency on LArBadChannelTool (LArCafJobs-01-02-03)
    	* cmake: Add missing dependency on LArBadChannelTool
    	* tag LArCafJobs-01-02-03
    
    2-08-2016   Pavol Strizenec <pavol @ mail.cern.ch>
    	* getting tf from RAW working correctly
    	* tag LArCafJobs-01-02-02
    
    31-07-2016 Pavol Strizenec <pavol @ mail.cern.ch>
    	* fixing and debugging all issues
    	* working with 20.7.7.4 and higher
    	* tag LArCafJobs-01-02-01
    
    13-06-2016 Pavol Strizenec <pavol @ mail.cern.ch>
    	* adding LArNoiseBurst_fromraw
    	* no tag yet
    
    
    Former-commit-id: e533bcebc0f1e670b81e59e3a6851b647b828971

--- LArCalorimeter/LArCafJobs/LArCafJobs/LArNoiseBursts.h
+++ LArCalorimeter/LArCafJobs/LArCafJobs/LArNoiseBursts.h
@@ -20,7 +20,8 @@
 #include "Identifier/IdentifierHash.h"
 //#include "Identifier/HWIdentifier.h"
 //#include "LArElecCalib/ILArPedestal.h"
-#include "LArRecConditions/ILArBadChannelMasker.h"
+//#include "LArRecConditions/ILArBadChannelMasker.h"
+#include "LArBadChannelTool/LArBadChanTool.h"
 #include "CaloInterface/ICaloNoiseTool.h"
 #include "CaloInterface/ICalorimeterNoiseTool.h"
 #include "TrigAnalysisInterfaces/IBunchCrossingTool.h"
@@ -91,6 +92,7 @@ class LArNoiseBursts : public AthAlgorithm  {
    int GetPartitionLayerIndex(const Identifier& id);
    std::vector<int>* GetHVLines(const Identifier& id);
      
+   StatusCode fillCell(HWIdentifier onlID, float eCalo, float qfactor, CaloGain::CaloGain gain);
 
  private:
 
@@ -103,6 +105,7 @@ class LArNoiseBursts : public AthAlgorithm  {
    ToolHandle<LArHVCablingTool> m_LArHVCablingTool;
    ToolHandle<ICaloNoiseTool> m_calo_noise_tool;
    ToolHandle<Trig::IBunchCrossingTool> m_bc_tool;
+   ToolHandle<ILArBadChanTool> m_badchan_tool;
 
    ToolHandle< Trig::TrigDecisionTool > m_trigDec;
 
@@ -134,7 +137,7 @@ class LArNoiseBursts : public AthAlgorithm  {
    int  m_nt_larcellsize;
    int  m_nt_cellsize;
    int  m_nt_run ;
-   int  m_nt_evtId;
+   unsigned long  m_nt_evtId;
    int  m_nt_evtTime;
    int  m_nt_evtTime_ns;
    int  m_nt_lb;
@@ -217,7 +220,7 @@ class LArNoiseBursts : public AthAlgorithm  {
    std::vector<float> m_nt_phicell;
    std::vector<float> m_nt_etacell;
    std::vector<float> m_nt_signifcell;
-   float m_nt_noisycellpercent;
+   //float m_nt_noisycellpercent;
    std::vector<short> m_nt_ft_noisy;
    std::vector<short> m_nt_slot_noisy;
    std::vector<short> m_nt_channel_noisy;
--- LArCalorimeter/LArCafJobs/share/LArNoiseBursts_fromraw_tf.py
+++ LArCalorimeter/LArCafJobs/share/LArNoiseBursts_fromraw_tf.py
@@ -11,17 +11,12 @@ import PyJobTransforms.trfArgClasses as trfArgClasses
 if __name__ == '__main__':
 
     executorSet = set()
-    executorSet.add(athenaExecutor(name = 'LArNoiseBursts_from_raw', skeletonFile = 'LArCafJobs/skeleton.LArNoise.py',
-                                   substep = 'r2e', inData = ['BS',], outData = ['NTUP_LARNOISE','NTUP_HECNOISE']))
-    executorSet.add(athenaExecutor(name = 'LArNoiseBursts', skeletonFile = 'LArCafJobs/skeleton.LArNoise.py',
-                                   substep = 'e2a', inData = ['ESD',], outData = ['NTUP_LARNOISE','NTUP_HECNOISE']))
+    executorSet.add(athenaExecutor(name = 'LArNoiseBursts_from_raw', skeletonFile = 'LArCafJobs/skeleton.LArNoise_fromraw.py',
+                                   substep = 'r2e', inData = ['BS',], outData = ['NTUP_LARNOISE','NTUP_HECNOISE','HIST_LARNOISE']))
    
     trf = transform(executor = executorSet) 
     addAthenaArguments(trf.parser)
     addDetectorArguments(trf.parser)
-    trf.parser.add_argument('--inputESDFile', nargs='+',
-                            type=trfArgClasses.argFactory(trfArgClasses.argPOOLFile, io='input'),
-                            help='Input pool file', group='Reco Files')
    
     trf.parser.add_argument('--inputBSFile', nargs='+',
                             type=trfArgClasses.argFactory(trfArgClasses.argPOOLFile, io='input'),
@@ -34,6 +29,10 @@ if __name__ == '__main__':
     trf.parser.add_argument('--outputNTUP_HECNOISEFile', nargs='+',
                             type=trfArgClasses.argFactory(trfArgClasses.argNTUPFile, io='output'),
                             help='Output HECNoise file', group='Ntuple Files')
+
+    trf.parser.add_argument('--outputHIST_LARNOISEFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argHISTFile, io='output'),
+                            help='Output Noise hist file', group='Hist Files')
 
     trf.parseCmdLineArgs(sys.argv[1:])
 
