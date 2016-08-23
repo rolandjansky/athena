e573b7dff351a0e7ec188207113ea71fc88d7a22
Attila Krasznahorkay <krasznaa@cern.ch>
2016-08-23 15:51:43 +0200

    Creating a new tag with the CMakeLists.txt file included (xAODCoreAthenaPool-00-00-06)
    	* Created a new tag that contains the CMakeLists.txt file for the
    	  package.
    	* Tagging as xAODCoreAthenaPool-00-00-06
    
    2015-10-09 Will Buttinger <will@cern.ch>
            * Merging modifications in xAODCoreAthenaPool-shallowThinning devBranch into trunk, which provides thinning of shallow aux containers
            * Tagging as xAODCoreAthenaPool-00-00-05
    
    2015-04-16 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* As it turns out, the source of ATLASRECTS-1990 was a problem
    	  in this package.
    	* The xAOD::AuxContainerBaseCnv POOL converter now makes sure that
    	  the thinned container retains the auxiliary variable selection
    	  of the parent class.
    	* Tagging as xAODCoreAthenaPool-00-00-04
    
    
    Former-commit-id: 43bf900c6afb8031558c26bd12a175dbb40ad152

--- Event/xAOD/xAODCoreAthenaPool/src/xAODAuxContainerBaseCnv.h
+++ Event/xAOD/xAODCoreAthenaPool/src/xAODAuxContainerBaseCnv.h
@@ -4,7 +4,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: xAODAuxContainerBaseCnv.h 635273 2014-12-11 13:58:24Z krasznaa $
+// $Id: xAODAuxContainerBaseCnv.h 660871 2015-04-16 08:20:17Z krasznaa $
 #ifndef XAODCOREATHENAPOOL_XAODAUXCONTAINERBASECNV_H
 #define XAODCOREATHENAPOOL_XAODAUXCONTAINERBASECNV_H
 
@@ -12,7 +12,9 @@
 #include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
 
 // EDM include(s):
-#include "xAODCore/AuxContainerBase.h"
+#define private public
+#   include "xAODCore/AuxContainerBase.h"
+#undef private
 
 /// Base class for the converter
 typedef T_AthenaPoolCustomCnv< xAOD::AuxContainerBase,
@@ -29,8 +31,8 @@ typedef T_AthenaPoolCustomCnv< xAOD::AuxContainerBase,
  *
  * @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
  *
- * $Revision: 635273 $
- * $Date: 2014-12-11 14:58:24 +0100 (Thu, 11 Dec 2014) $
+ * $Revision: 660871 $
+ * $Date: 2015-04-16 10:20:17 +0200 (Thu, 16 Apr 2015) $
  */
 class xAODAuxContainerBaseCnv : public xAODAuxContainerBaseCnvBase {
 
