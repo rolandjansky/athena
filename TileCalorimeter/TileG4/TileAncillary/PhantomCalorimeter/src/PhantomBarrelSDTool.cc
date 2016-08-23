f02ecb2fb26b2fc5cbf147c40be411d077fcf8c3
Scott Snyder <scott.snyder@cern.ch>
2016-08-23 15:43:44 +0200

    Comply with ATLAS naming conventions. (PhantomCalorimeter-00-03-01)
    	* Tagging PhantomCalorimeter-00-03-01.
    	* Tag previous changes.
    
    2016-02-21  Siarhei Harkusha  <Siarhei.Harkusha@cern.ch>
    
    	* PhantomBarrelSD: fixed some class member names to follow ATLAS conventions
    
    
    Former-commit-id: 52b2d61ea640d295d4af54e62175def5af8cbff9

--- TileCalorimeter/TileG4/TileAncillary/PhantomCalorimeter/src/PhantomBarrelSDTool.cc
+++ TileCalorimeter/TileG4/TileAncillary/PhantomCalorimeter/src/PhantomBarrelSDTool.cc
@@ -13,69 +13,56 @@
 #include "PhantomBarrelSD.hh"
 
 PhantomBarrelSDTool::PhantomBarrelSDTool(const std::string& type, const std::string& name, const IInterface* parent)
-  : SensitiveDetectorBase(type,name,parent)
-{
+    : SensitiveDetectorBase(type, name, parent) {
   declareInterface<ISensitiveDetector>(this);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-PhantomBarrelSDTool::~PhantomBarrelSDTool()
-{
+PhantomBarrelSDTool::~PhantomBarrelSDTool() {
 
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-StatusCode PhantomBarrelSDTool::SetupEvent()
-{
-  ATH_MSG_VERBOSE( "PhantomBarrelSDTool::SetupEvent()" );
-  if(!getSD())
-    {
-      ATH_MSG_ERROR ("SetupEvent: PhantomBarrelSD never created!");
+StatusCode PhantomBarrelSDTool::SetupEvent() {
+  ATH_MSG_VERBOSE("PhantomBarrelSDTool::SetupEvent()");
+  if (!getSD()) {
+    ATH_MSG_ERROR("SetupEvent: PhantomBarrelSD never created!");
+    return StatusCode::FAILURE;
+  } else {
+    PhantomBarrelSD *localSD = dynamic_cast<PhantomBarrelSD*>(getSD());
+    if (!localSD) {
+      ATH_MSG_ERROR("SetupEvent: Failed to cast m_SD into PhantomBarrelSD.");
       return StatusCode::FAILURE;
     }
-  else
-    {
-      PhantomBarrelSD *localSD = dynamic_cast<PhantomBarrelSD*>(getSD());
-      if(!localSD)
-        {
-          ATH_MSG_ERROR ("SetupEvent: Failed to cast m_SD into PhantomBarrelSD.");
-          return StatusCode::FAILURE;
-        }
-      localSD->StartOfAthenaEvent();
-    }
+    localSD->StartOfAthenaEvent();
+  }
   return StatusCode::SUCCESS;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-StatusCode PhantomBarrelSDTool::Gather()
-{
-  ATH_MSG_VERBOSE( "PhantomBarrelSDTool::Gather()" );
-  if(!getSD())
-    {
-      ATH_MSG_ERROR ("Gather: PhantomBarrelSD never created!");
+StatusCode PhantomBarrelSDTool::Gather() {
+  ATH_MSG_VERBOSE("PhantomBarrelSDTool::Gather()");
+  if (!getSD()) {
+    ATH_MSG_ERROR("Gather: PhantomBarrelSD never created!");
+    return StatusCode::FAILURE;
+  } else {
+    PhantomBarrelSD *localSD = dynamic_cast<PhantomBarrelSD*>(getSD());
+    if (!localSD) {
+      ATH_MSG_ERROR("Gather: Failed to cast m_SD into PhantomBarrelSD.");
       return StatusCode::FAILURE;
     }
-  else
-    {
-      PhantomBarrelSD *localSD = dynamic_cast<PhantomBarrelSD*>(getSD());
-      if(!localSD)
-        {
-          ATH_MSG_ERROR ("Gather: Failed to cast m_SD into PhantomBarrelSD.");
-          return StatusCode::FAILURE;
-        }
-      localSD->EndOfAthenaEvent();
-    }
+    localSD->EndOfAthenaEvent();
+  }
   return StatusCode::SUCCESS;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4VSensitiveDetector* PhantomBarrelSDTool::makeSD()
-{
-  ATH_MSG_DEBUG( "Initializing SD" );
+G4VSensitiveDetector* PhantomBarrelSDTool::makeSD() {
+  ATH_MSG_DEBUG("Initializing SD");
   // Create a fresh SD
   return new PhantomBarrelSD(name(), m_outputCollectionNames[0]);
 }
