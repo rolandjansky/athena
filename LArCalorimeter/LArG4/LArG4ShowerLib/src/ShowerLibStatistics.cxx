4dd3dca886e2bc3a78260c9d2da9cbc3c9f8ccc3
Scott Snyder <scott.snyder@cern.ch>
2016-08-23 13:48:39 +0200

    'Comply with ATLAS naming conventions.  Pass large objects by const reference, not by value.' (LArG4ShowerLib-01-00-10)
    	* Tagging LArG4ShowerLib-01-00-10.
    	* Comply with ATLAS naming conventions.  Pass large objects by
    	const reference, not by value.
    
    
    Former-commit-id: 1b904c4a92cc812fae2a07cf1e28062a915c0496

--- LArCalorimeter/LArG4/LArG4ShowerLib/src/ShowerLibStatistics.cxx
+++ LArCalorimeter/LArG4/LArG4ShowerLib/src/ShowerLibStatistics.cxx
@@ -15,34 +15,35 @@
 
 namespace ShowerLib {
 
-ShowerLibStatistics::ShowerLibStatistics(const std::map<int,std::string> bincaptions, const std::map<int,int> binsizes) : counter(0),
-                                                                                                                          binstrings(bincaptions),
-                                                                                                                          binsizes(binsizes)
+ShowerLibStatistics::ShowerLibStatistics(const std::map<int,std::string>& bincaptions,
+                                         const std::map<int,int>& binsizes) : m_counter(0),
+                                                                                                                          m_binstrings(bincaptions),
+                                                                                                                          m_binsizes(binsizes)
 {
-	for(std::map<int,int>::const_iterator it = binsizes.begin(); it != binsizes.end(); it++)
-		  calls[(*it).first] = 0;
+	for(std::map<int,int>::const_iterator it = m_binsizes.begin(); it != m_binsizes.end(); it++)
+		  m_calls[(*it).first] = 0;
 }
 
 void ShowerLibStatistics::recordShowerLibUse(int binkey)
 {
-	  counter++;
-	  calls[binkey] += 1;
+	  m_counter++;
+	  m_calls[binkey] += 1;
 }
 
 const std::string ShowerLibStatistics::statistics()
 {
 	std::stringstream out;
 	out << std::fixed << std::setprecision(2);
-    out << "Number of bins: " << binsizes.size() << std::endl;
+    out << "Number of bins: " << m_binsizes.size() << std::endl;
     out << "Bin Statistics: " << std::endl;
     float maxuse = 0;
     float minuse = -1;
     int totnum = 0;
-    for(std::map<int,int>::const_iterator it = binsizes.begin(); it != binsizes.end();++it) {
+    for(std::map<int,int>::const_iterator it = m_binsizes.begin(); it != m_binsizes.end();++it) {
   	  totnum += (*it).second;
-  	  int curcalls = calls.at((*it).first);
+  	  int curcalls = m_calls.at((*it).first);
   	  int curlen = (*it).second;
-	  std::string name = binstrings.at((*it).first);
+	  std::string name = m_binstrings.at((*it).first);
   	  int use = 0;
   	  if (curlen >0) {
   		  use = 100 * curcalls/curlen;
@@ -58,7 +59,7 @@ const std::string ShowerLibStatistics::statistics()
     	return out.str();
     }
 
-    out << "Total:               size " << std::setw(6) << totnum << " / calls " << std::setw(9) << counter << " (min " << minuse << "% av " << (100 * counter/totnum) << "% max " << maxuse << "%)" << std::endl;
+    out << "Total:               size " << std::setw(6) << totnum << " / calls " << std::setw(9) << m_counter << " (min " << minuse << "% av " << (100 * m_counter/totnum) << "% max " << maxuse << "%)" << std::endl;
 
     return out.str();
 }
