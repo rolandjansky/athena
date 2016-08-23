ea7b435ad89ae849fe40962e7c6dcac7e54ed8bf
Jochen Meyer <Jochen.Meyer@cern.ch>
2016-08-23 12:56:55 +0200

    msg svc updates (MooSegmentCombinationFinder-01-00-17)
    	* endreq -> endmsg
    	* tagging MooSegmentCombinationFinder-01-00-17
    
    
    Former-commit-id: 81c82d184151bf98e17a8518573aa86badbe09c0

--- MuonSpectrometer/MuonReconstruction/MuonSegmentCombiners/MuonSegmentCombinerTools/MooSegmentCombinationFinder/src/MooSegmentCombinationFinder.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonSegmentCombiners/MuonSegmentCombinerTools/MooSegmentCombinationFinder/src/MooSegmentCombinationFinder.cxx
@@ -343,7 +343,7 @@ Muon::MooSegmentCombinationFinder::findSegments( const std::vector<const MdtPrep
       finalSegmentCombinations = cleanedSegmentCombinations;
     }
 
-    if( m_doSummary || msgLvl(MSG::DEBUG) ) msg() << endreq;
+    if( m_doSummary || msgLvl(MSG::DEBUG) ) msg() << endmsg;
 
     // increase counters, assume that the tools is only called once per event
     ++m_nevents;
@@ -519,7 +519,7 @@ Muon::MooSegmentCombinationFinder::extractSegmentCollection( const MuonSegmentCo
 	  if( !m_segmentSelector->select( **sit, ignoreHoles, quality ) ){
 	    if( msgLvl(MSG::VERBOSE) ) {
 	      int q = m_segmentSelector->quality(**sit,ignoreHoles);
-	      msg(MSG::VERBOSE) << " bad segment " << m_edmPrinter->print(**sit) << " quality " << q << endreq;
+	      msg(MSG::VERBOSE) << " bad segment " << m_edmPrinter->print(**sit) << " quality " << q << endmsg;
 	    }
 	    ++nremovedBadSegments;
 	    continue;
@@ -538,7 +538,7 @@ Muon::MooSegmentCombinationFinder::extractSegmentCollection( const MuonSegmentCo
 	  if( !m_segmentSelector->select( **sit, ignoreHoles, quality ) ){
 	    if( msgLvl(MSG::VERBOSE) ) {
 	      int q = m_segmentSelector->quality(**sit,ignoreHoles);
-	      msg(MSG::VERBOSE) << " bad segment " << m_edmPrinter->print(**sit) << " quality " << q << endreq;
+	      msg(MSG::VERBOSE) << " bad segment " << m_edmPrinter->print(**sit) << " quality " << q << endmsg;
 	    }
 	    ++nremovedBadSegments;
 	    continue;
