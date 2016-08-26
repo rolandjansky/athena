9f5b7c358f3c870dc35018161dc4f344a62f08b0
Scott Snyder <scott.snyder@cern.ch>
2016-08-26 18:18:30 +0200

    'Coverity 111509-111510.' (POOLCore-00-04-05)
    	* Tagging POOLCore-00-04-05.
    	* Coverity 111509-111510.
    
    2016-08-10  Marcin Nowak  <Marcin.Nowak@cern.ch>
    	* test/CommandOptions_test.cxx: add destructor to delete resources
    	* Tagging POOLCore-00-04-04
    
    
    Former-commit-id: 683766dce9bdffe4e3bb9b01b9a139b2ab319a7d

--- Database/APR/POOLCore/test/CommandOptions_test.cxx
+++ Database/APR/POOLCore/test/CommandOptions_test.cxx
@@ -39,7 +39,13 @@ namespace pooltest{
         m_argv[i] = m_args[i].c_str(); 
       }
     } 
+    Command (const Command&) = delete;
+    Command& operator= (const Command&) = delete;
 
+    ~Command() {
+       delete [] m_argv;
+    }
+    
     int argc() const {
       return m_args.size();
     }
