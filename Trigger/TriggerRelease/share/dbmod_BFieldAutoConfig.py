1a68a0c473028ba6f5e24d1cf5f7757556d33ff2
Frank Winklmeier <Frank.Winklmeier@cern.ch>
2016-08-29 08:49:37 +0200

    Fix for run-1/2 detection in dbmod_BFieldAutoConfig.py (ATR-14899) (TriggerRelease-00-12-12)
    2016-08-29  Frank Winklmeier  <fwinkl@cern>
    	* share/dbmod_BFieldAutoConfig.py: Fix for run-1/2 detection (ATR-14899)
    	* TriggerRelease-00-12-12
    
    2016-08-11  Patrick Czodrowski <czodrows@cern.ch>
    	* share/runHLT_standalone.py: add heavy ion v4 menus HIV4 and MCHIV4
    	* TriggerRelease-00-12-11
    
    
    Former-commit-id: 3454fd944b8c90351090cd70fbdd224f6ce98c1e

--- Trigger/TriggerRelease/share/dbmod_BFieldAutoConfig.py
+++ Trigger/TriggerRelease/share/dbmod_BFieldAutoConfig.py
@@ -4,12 +4,14 @@
 # Use DCS currents to configure magnetic field
 #
 from GaudiPython.Bindings import iProperty
+from AthenaCommon.Logging import logging
+log = logging.getLogger('dbmod_BFieldAutoConfig.py')
 
 db = 'CONDBR2'
-if _run_number in dir() and _run_number<222222: db = 'COMP200'
+if '_run_number' in dir() and _run_number<222222: db = 'COMP200'
 
 # Add the DCS folder (note the double-use of quotes)
-iProperty('IOVDbSvc').Folders.push_back("'<db>COOLOFL_DCS/%s</db> /EXT/DCS/MAGNETS/SENSORDATA'" % db)
+f = "'<db>COOLOFL_DCS/%s</db> /EXT/DCS/MAGNETS/SENSORDATA'" % db
+log.info("Adding folder %s to IOVDbSvc" % f)
+iProperty('IOVDbSvc').Folders.push_back(f)
 iProperty('AtlasFieldSvc').UseDCS = True
-
-
