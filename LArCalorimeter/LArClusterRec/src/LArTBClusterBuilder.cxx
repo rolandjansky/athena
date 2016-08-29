58722e6a70564d13c95c32b90813aed92b436ea0
Scott Snyder <scott.snyder@cern.ch>
2016-08-29 20:05:26 +0200

    'Use ATH_ macros instead of endreq.' (LArClusterRec-02-06-12)
    	* Tagging LArClusterRec-02-06-12.
    	* Use ATH_ macros instead of endreq.
    
    2016-06-25  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArClusterRec-02-06-11.
    	* LArCablingService moved to LArCabling.
    	* Tagging LArClusterRec-02-06-10.
    	* Avoid hidden method warning.
    
    2016-06-24  scott snyder  <snyder@bnl.gov>
    
    	* Tagging LArClusterRec-02-06-09.
    	* LArClusterRec/LArTBClusterBuilder.h,
    	src/LArTBClusterBuilder.cxx: Update for change in processor
    	interface: execute() becomes const.
    
    
    Former-commit-id: e11a0f1d1de36e9071bb1e55ebc70e654487c827

--- LArCalorimeter/LArClusterRec/src/LArTBClusterBuilder.cxx
+++ LArCalorimeter/LArClusterRec/src/LArTBClusterBuilder.cxx
@@ -27,23 +27,16 @@
 #include "CaloEvent/CaloCellContainer.h"
 #include "CaloUtils/CaloClusterStoreHelper.h"
 #include "StoreGate/StoreGateSvc.h"
-#include "GaudiKernel/Property.h"
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/ToolFactory.h"
 
 LArTBClusterBuilder::LArTBClusterBuilder(const std::string& type, 
 					 const std::string& name,
 					 const IInterface* parent):
   AthAlgTool(type, name, parent),
-  m_storeGateSvc(0),
   m_eta_size(3.),
   m_deta(0),
   m_phi_size(3.),
   m_dphi(0),
   m_nbcluster(1),
-  m_layere(),
-  m_layereta(),
-  m_layerphi(),
   m_caloRegion("BARREL")
 {
   declareProperty("CellContainers",m_cellContainerName);
@@ -59,21 +52,8 @@ LArTBClusterBuilder::~LArTBClusterBuilder()
 
 StatusCode LArTBClusterBuilder::initialize()
 {
-  StatusCode sc;
-  
-  // Use the message service
-  MsgStream log(msgSvc(), name());
-  log << MSG::INFO << ">>> Initialize" << endreq;
+  ATH_MSG_INFO( ">>> Initialize"  );
     
-  // StoreGate service
-  sc = service("StoreGateSvc", m_storeGateSvc);
-  if (sc.isFailure()) 
-    {
-      log << MSG::FATAL << " StoreGate service not found " << std::endl;
-      sc = StatusCode::FAILURE; 
-      return sc;
-    }
-
   //eta & phi size
   m_deta = (m_eta_size*0.025)/2.;
   m_dphi = (m_phi_size*0.0245436926)/2.;
@@ -82,64 +62,52 @@ StatusCode LArTBClusterBuilder::initialize()
   return StatusCode::SUCCESS;
 }
 
-StatusCode LArTBClusterBuilder::execute(xAOD::CaloClusterContainer* clusColl)
+StatusCode
+LArTBClusterBuilder::execute(const EventContext& /*ctx*/,
+                             xAOD::CaloClusterContainer* clusColl) const
 {
-  MsgStream log(msgSvc(), name());
-
-  StatusCode sc;
-
   xAOD::CaloClusterContainer& larClusterContainer = *clusColl;
   
-  const CaloCellContainer *caloCellContainer;
-
-  sc = m_storeGateSvc->retrieve(caloCellContainer, m_cellContainerName) ; 
-  if(sc!=StatusCode::SUCCESS)
-    {
-      log<< MSG::ERROR
-	 << "could not get caloCellContainer with name "
-	 << m_cellContainerName
-	 << endreq;
-      return StatusCode::RECOVERABLE;
-    }
+  const CaloCellContainer *caloCellContainer = nullptr;
+  ATH_CHECK( evtStore()->retrieve(caloCellContainer, m_cellContainerName) );
 
   CaloCellContainer::const_iterator itrCell=caloCellContainer->beginConstCalo( CaloCell_ID::LAREM);
 
   if (caloCellContainer->size()==0){
-    log << MSG::ERROR << " Empty CaloCellContainer " 
-	<< endreq ;
+    ATH_MSG_ERROR( " Empty CaloCellContainer "  );
   }
-  
+
+  CellList cell_list;
+  CellList middle_cell_list;
   for( ; itrCell!=caloCellContainer->endConstCalo( CaloCell_ID::LAREM); ++itrCell)
     {
       const CaloCell* theCell = (*itrCell);
       CaloSampling::CaloSample sample=(CaloSampling::CaloSample)theCell->caloDDE()->getSampling();
-      m_cell_list.push_back(theCell);
-      if(sample==CaloSampling::EMB2 || sample==CaloSampling::EME2) m_middle_cell_list.push_back(theCell);
+      cell_list.push_back(theCell);
+      if(sample==CaloSampling::EMB2 || sample==CaloSampling::EME2) middle_cell_list.push_back(theCell);
     }
   
 
    //
   ene_sup tri;
-  m_cell_list.sort(tri);
-  m_middle_cell_list.sort(tri);
+  cell_list.sort(tri);
+  middle_cell_list.sort(tri);
   
 
   for(int j=0;j<m_nbcluster;j++)
     {      
-      CellList::iterator it_cell = m_cell_list.begin();
-      CellList::iterator it_middle_cell = m_middle_cell_list.begin();
+      CellList::iterator it_cell = cell_list.begin();
+      CellList::iterator it_middle_cell = middle_cell_list.begin();
             
+      double layere[8] = {0};
+      double layereta[8]= {0};
+      double layerphi[8]= {0};  
+
       double eta_clus = 0.;
       double phi_clus = 0.;
       bool seed = true;
       xAOD::CaloCluster* cluster = 0;
-      for(unsigned int k=0;k<8;k++)
-	{
-	  m_layere[k]=0.;
-	  m_layereta[k]=0.;
-	  m_layerphi[k]=0.;
-	}
-      for(;it_cell!= m_cell_list.end(); )
+      for(;it_cell!= cell_list.end(); )
 	{ 
 	  if(seed)
 	    {//seed cell position
@@ -167,12 +135,12 @@ StatusCode LArTBClusterBuilder::execute(xAOD::CaloClusterContainer* clusColl)
 		      if (cell_idx>0) {
 			cluster->addCell(cell_idx,1.0);
 			// This could produce an error anyhow if the ENDCAP is included... the arrays are redimensioned to 8...
-			m_layere[isampling] += ene;
-			m_layereta[isampling] += ene*eta;
-			m_layerphi[isampling] += ene*phi;
-			it_cell=m_cell_list.erase(it_cell); // Removes the cells which are used for the clustering... keeping the others in the container for further clusters...
+			layere[isampling] += ene;
+			layereta[isampling] += ene*eta;
+			layerphi[isampling] += ene*phi;
+			it_cell=cell_list.erase(it_cell); // Removes the cells which are used for the clustering... keeping the others in the container for further clusters...
 			if(isampling==CaloSampling::EMB2 || isampling==CaloSampling::EME2)
-			  it_middle_cell=m_middle_cell_list.erase(it_middle_cell);
+			  it_middle_cell=middle_cell_list.erase(it_middle_cell);
 		      }
 		    }
 		  else{
@@ -203,23 +171,21 @@ StatusCode LArTBClusterBuilder::execute(xAOD::CaloClusterContainer* clusColl)
 	{
 	  CaloSampling::CaloSample sampling=static_cast<xAOD::CaloCluster::CaloSample>(iSample);
 	  
-	  if(m_layere[sampling]>0.){
-	    m_layereta[sampling]= m_layereta[sampling]/m_layere[sampling];
-	    m_layerphi[sampling]= m_layerphi[sampling]/m_layere[sampling];
+	  if(layere[sampling]>0.){
+	    layereta[sampling]= layereta[sampling]/layere[sampling];
+	    layerphi[sampling]= layerphi[sampling]/layere[sampling];
 	  }
 	  else
 	    {
-	      m_layereta[sampling]=-999.;
-	      m_layerphi[sampling]=-999.;
+	      layereta[sampling]=-999.;
+	      layerphi[sampling]=-999.;
 	    }
 	  
- 	  cluster->setEnergy(sampling, m_layere[sampling]);
-	  cluster->setEta(sampling, m_layereta[sampling]);
-	  cluster->setPhi(sampling, m_layerphi[sampling]);
+ 	  cluster->setEnergy(sampling, layere[sampling]);
+	  cluster->setEta(sampling, layereta[sampling]);
+	  cluster->setPhi(sampling, layerphi[sampling]);
 	}
     }
-  m_cell_list.clear();
-  m_middle_cell_list.clear();
 
   return StatusCode::SUCCESS;
 }
