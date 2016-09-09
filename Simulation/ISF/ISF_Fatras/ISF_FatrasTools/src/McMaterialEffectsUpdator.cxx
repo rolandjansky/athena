28180498ad7432925c8ecdf0d251ff4d08076f13
Sarka Todorova <todorova@cern.ch>
2016-09-09 13:56:05 +0200

    memory leak fixes (ISF_FatrasTools-00-03-44)
    	* memory leak fixes
    	* tagged as ISF_FatrasTools-00-03-44
    
    2016-07-05 Sharka Todorova <sarka.todorova@cern.ch>
    	* bufixes in material update
    	* tagged as ISF_FatrasTools-00-03-43

--- Simulation/ISF/ISF_Fatras/ISF_FatrasTools/src/McMaterialEffectsUpdator.cxx
+++ Simulation/ISF/ISF_Fatras/ISF_FatrasTools/src/McMaterialEffectsUpdator.cxx
@@ -478,7 +478,7 @@ const Trk::TrackParameters* iFatras::McMaterialEffectsUpdator::updateInLay(const
   int iStatus = 0;
   double dX0 = (1.-matFraction)*pathCorrection*m_matProp->thicknessInX0();
 
-  if (!m_matProp || particle==Trk::geantino || particle==Trk::nonInteracting ) {   
+  if (!m_matProp || particle==Trk::geantino || particle==Trk::nonInteracting || dX0==0.) {   
     // non-interacting - pass them back
     pathLim.updateMat(dX0,m_matProp->averageZ(),dInL0);
 
@@ -538,7 +538,7 @@ const Trk::TrackParameters* iFatras::McMaterialEffectsUpdator::updateInLay(const
   AmgVector(5) updatedParameters(parm->parameters());  
   const Trk::TrackParameters* updated = 0;
 
-  if ( m_eLoss && particle==Trk::electron ) {
+  if ( m_eLoss && particle==Trk::electron && dX0>0.) {
 
     double matTot =  (1-matFraction)*pathCorrection*m_matProp->thicknessInX0();
     Amg::Vector3D edir = parm->momentum().unit(); 
@@ -552,10 +552,10 @@ const Trk::TrackParameters* iFatras::McMaterialEffectsUpdator::updateInLay(const
     matFraction += dX0/pathCorrection/m_matProp->thicknessInX0();  
   }
 
-  if ( isp->charge()!=0 ) {
+  if ( isp->charge()!=0 && dX0>0.) {
     if ( m_eLoss ) ionize(*parm, updatedParameters, dX0, dir, particle );
 
-    if ( m_ms ) {
+    if ( m_ms  && m_matProp->thicknessInX0()>0.) {
 
       double sigmaMSproj = (m_use_msUpdator && m_msUpdator ) ?
 	sqrt(m_msUpdator->sigmaSquare(*m_matProp, p, dX0/m_matProp->thicknessInX0(), particle)) : msSigma(dX0,p,particle);
@@ -964,7 +964,7 @@ const Trk::TrackParameters* iFatras::McMaterialEffectsUpdator::update(double tim
     m_deltaP = deltaP;
   }
   
-  if (m_ms){
+  if (m_ms && matprop.x0()>0 ){
     // get the projected scattering angle
     double sigmaMSproj   = (m_use_msUpdator && m_msUpdator) ? sqrt(m_msUpdator->sigmaSquare(matprop, p, pathCorrection, particle)) : 
                                                               msSigma(pathCorrection*matprop.thicknessInX0(),p,particle); 
