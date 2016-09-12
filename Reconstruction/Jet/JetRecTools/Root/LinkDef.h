bce73e2f92d9b90611a87ae7ae3ed011e15d417d
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2016-09-12 15:48:17 +0200

    update of WeightPFOTool interface (JetRecTools-00-03-50)
    	* Root/PFlowPseudoJetGetter: Update to WeightPFOTool interface
    	* Tag as JetRecTools-00-03-50
    
    2016-09-10    <khoo@cern.ch>
    	* Root/PFlowPseudoJetGetter: initial change to retain CPFOs with weight 0 as jet constituents
    	* Tag as JetRecTools-00-03-49
    
    2016-09-10    <delsart@lpsc1120x.in2p3.fr>
    
    	* Root/LinkDef.h: added
    	* Tag as JetRecTools-00-03-48
    
    2016-08-31  scott snyder  <snyder@bnl.gov>
    
    	* Tagging JetRecTools-00-03-47.
    	* Comply with ATLAS naming conventions.
    
    2016-08-23  scott snyder  <snyder@bnl.gov>
    
    	* Tagging JetRecTools-00-03-46.
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 28ecbd31c8856c292778b9d689895c100957665c

---
+++ Reconstruction/Jet/JetRecTools/Root/LinkDef.h
@@ -0,0 +1,20 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include <JetRecTools/JetTrackSelectionTool.h>
+#include <JetRecTools/TrackVertexAssociationTool.h>
+#include <JetRecTools/TrackPseudoJetGetter.h>
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+#pragma link C++ class JetTrackSelectionTool+;
+#pragma link C++ class TrackVertexAssociationTool+;
+#pragma link C++ class TrackPseudoJetGetter+;
+
+#endif
