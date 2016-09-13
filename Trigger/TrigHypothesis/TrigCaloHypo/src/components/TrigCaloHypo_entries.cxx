1892272bca1342ccee1d42ebcbbe31849db8efbe
Denis Oliveira Damazio <Denis.Oliveira.Damazio@cern.ch>
2016-09-13 09:06:04 +0200

    including missing part (TrigCaloHypo-00-00-01)
    Former-commit-id: 7b960be3fe1e2a28de674d8c1dc066314251a4d6

---
+++ Trigger/TrigHypothesis/TrigCaloHypo/src/components/TrigCaloHypo_entries.cxx
@@ -0,0 +1,13 @@
+
+#include "TrigCaloHypo/TrigEFCaloHypoNoise.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_ALGORITHM_FACTORY( TrigEFCaloHypoNoise )
+
+DECLARE_FACTORY_ENTRIES( TrigCaloHypo ) {
+
+   DECLARE_ALGORITHM( TrigEFCaloHypoNoise )
+
+}
+
+
