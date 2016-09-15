175274e9361f85aefd896c354f63334fe5b48b57
Attila Krasznahorkay <krasznaa@cern.ch>
2016-09-15 13:17:55 +0200

    Adapting the code to egammaEvent-04-00-15 (UserAnalysisUtils-02-02-15)
    2016-09-15 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Adapted the code to the update in egammaEvent-04-00-15.
    	  (Obsoleting the egammaPID definition in that package.)
    	* Cleaned up the CMakeLists.txt file of the package a bit.
    	* Cleaned up the UserAnalysisSelectionTool implementation, as
    	  it pained me to see how poorely some stuff was implemented.
    	* Removed the executable flag of the source files.
    	* Tagging as UserAnalysisUtils-02-02-15
    
    
    Former-commit-id: bd3a46eb34bb6c79d18821d2fee1a7f008a2c2b8

--- PhysicsAnalysis/AnalysisCommon/UserAnalysisUtils/src/UserAnalysisSelectionTool.cxx
+++ PhysicsAnalysis/AnalysisCommon/UserAnalysisUtils/src/UserAnalysisSelectionTool.cxx
@@ -10,252 +10,269 @@ Created : November 2007
 Purpose : User Analysis Selections - see UserAnalysisSelectionTool.h for details
 *****************************************************************************/
 
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/GaudiException.h"
-#include "GaudiKernel/Property.h"
-
+// EDM include(s):
+#include "egammaEvent/Electron.h"
+#include "egammaEvent/Photon.h"
 #include "egammaEvent/EMShower.h"
+#include "muonEvent/Muon.h"
+#include "tauEvent/TauJet.h"
+#include "JetEvent/Jet.h"
+#include "MissingETEvent/MissingET.h"
+#include "Particle/TrackParticleContainer.h"
+#include "CaloEvent/CaloClusterContainer.h"
 
-// Accessing data:
-#include "CLHEP/Units/PhysicalConstants.h"
-
-// User Tools
+// Local include(s):
 #include "UserAnalysisUtils/UserAnalysisSelectionTool.h"
 
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-
-using namespace Analysis;
-using namespace Rec;
-using namespace std;
-using CLHEP::GeV;
+/// Convenience GeV definition
+static const double GeV = 1000.0;
 
 //------------------------------------------------------------------------------
 UserAnalysisSelectionTool::UserAnalysisSelectionTool( const std::string& type,
-                                                      const std::string& name, 
+                                                      const std::string& name,
                                                       const IInterface* parent )
-  : AthAlgTool( type, name, parent ) {
-  declareInterface<UserAnalysisSelectionTool>( this );
+   : AthAlgTool( type, name, parent ) {
+   declareInterface<UserAnalysisSelectionTool>( this );
 
-  declareProperty("IsAtlfastData",          m_isAtlfast=false);
+   declareProperty("IsAtlfastData",          m_isAtlfast=false);
 
-  /** caloCluster selection */
-  declareProperty("CaloClusterE", m_caloClusterE=1.0*GeV);
+   /** caloCluster selection */
+   declareProperty("CaloClusterE", m_caloClusterE=1.0*GeV);
 
-  /** TrackParticle Pt */
-  declareProperty("TrackParticlePt", m_trackParticlePt=1.0*GeV);
+   /** TrackParticle Pt */
+   declareProperty("TrackParticlePt", m_trackParticlePt=1.0*GeV);
 
-  /** Electron selection */
-  declareProperty("ElectronPt",       m_electronPt=10*GeV);
-  declareProperty("ElectronEta",      m_electronEta=2.5);
-  declareProperty("ElectronIsEMFlag", m_electronIsEMFlag="Loose");
-  declareProperty("ElectronIsEM",     m_electronIsEM=0);
-  declareProperty("AuthorEgammaOnly", m_authorEgammaOnly=false);
-  declareProperty("ElectronEtaWindowCut", m_electronEtaWindCut = false);
-  declareProperty("ElectronEtaWindow", m_electronEtaWind = 0.075);
-  declareProperty("ElectronEtaWindowCenter", m_electronEtaWindCent=1.445);
-  declareProperty("DoElectronIsolation", m_doElectronIsolation=true);
-  declareProperty("ElectronIsolationConeIndex", m_electronIsolationConeIndex=1);
-  declareProperty("ElectronIsolationEt", m_electronIsolationEt=10*GeV);
-  declareProperty("NormalizedElectronIsolationEt",m_normElectronIsolEt=0.2);
+   /** Electron selection */
+   declareProperty("ElectronPt",       m_electronPt=10*GeV);
+   declareProperty("ElectronEta",      m_electronEta=2.5);
+   declareProperty("ElectronIsEMFlag", m_electronIsEMFlag="Loose");
+   declareProperty("ElectronIsEM",     m_electronIsEM=0);
+   declareProperty("AuthorEgammaOnly", m_authorEgammaOnly=false);
+   declareProperty("ElectronEtaWindowCut", m_electronEtaWindCut = false);
+   declareProperty("ElectronEtaWindow", m_electronEtaWind = 0.075);
+   declareProperty("ElectronEtaWindowCenter", m_electronEtaWindCent=1.445);
+   declareProperty("DoElectronIsolation", m_doElectronIsolation=true);
+   declareProperty("ElectronIsolationConeIndex",
+                   m_electronIsolationConeIndex=1);
+   declareProperty("ElectronIsolationEt", m_electronIsolationEt=10*GeV);
+   declareProperty("NormalizedElectronIsolationEt",m_normElectronIsolEt=0.2);
 
-  /** Photon selection */
-  declareProperty("PhotonPt",   m_photonPt=10*GeV);
-  declareProperty("PhotonEta",  m_photonEta=3.2);
-  declareProperty("PhotonIsEM", m_photonIsEM=0);
+   /** Photon selection */
+   declareProperty("PhotonPt",   m_photonPt=10*GeV);
+   declareProperty("PhotonEta",  m_photonEta=3.2);
+   declareProperty("PhotonIsEM", m_photonIsEM=0);
 
-  /** Muon selection */
-  declareProperty("MuonPt",                 m_muonPt=3.0*GeV);
-  declareProperty("MuonEta",                m_muonEta=2.7);
-  declareProperty("DoMuonIsolation",        m_doMuonIsolation=true);
-  declareProperty("MuonIsolationConeIndex", m_muonIsolationConeIndex=1);
-  declareProperty("MuonIsolationEt",        m_muonIsolationEt=10*GeV);
-  declareProperty("UseMatchChi2",           m_useMatchChi2=false);
-  declareProperty("MuonMatchChi2",          m_muonMatchChi2=100);
-  declareProperty("NormalizedMuonIsolationEt",m_normMuonIsolEt=0.2);
-  declareProperty("egDetailContainerName", m_egDetailContainerName="egDetailAOD");
+   /** Muon selection */
+   declareProperty("MuonPt",                 m_muonPt=3.0*GeV);
+   declareProperty("MuonEta",                m_muonEta=2.7);
+   declareProperty("DoMuonIsolation",        m_doMuonIsolation=true);
+   declareProperty("MuonIsolationConeIndex", m_muonIsolationConeIndex=1);
+   declareProperty("MuonIsolationEt",        m_muonIsolationEt=10*GeV);
+   declareProperty("UseMatchChi2",           m_useMatchChi2=false);
+   declareProperty("MuonMatchChi2",          m_muonMatchChi2=100);
+   declareProperty("NormalizedMuonIsolationEt",m_normMuonIsolEt=0.2);
+   declareProperty("egDetailContainerName",
+                   m_egDetailContainerName="egDetailAOD");
 
-  /** TauJet selection */
-  declareProperty("TauJetPt",           m_tauJetPt=20*GeV);
-  declareProperty("TauJetEta",          m_tauJetEta=2.5);
-  declareProperty("TauJetLikelihood",   m_tauJetLikelihood=-6.0);
-  declareProperty("TauEleBDTCut", m_tauEleBDTCut=0.5); // not yet set - No 23 1007
+   /** TauJet selection */
+   declareProperty("TauJetPt",           m_tauJetPt=20*GeV);
+   declareProperty("TauJetEta",          m_tauJetEta=2.5);
+   declareProperty("TauJetLikelihood",   m_tauJetLikelihood=-6.0);
+   declareProperty("TauEleBDTCut", m_tauEleBDTCut=0.5); // not yet set - No 23 1007
 
-  /** Jet selection */
-  declareProperty("JetPt",          m_jetPt=20*GeV);
-  declareProperty("JetEta",         m_jetEta=5.0);
-  declareProperty("BJetLikelihood", m_bJetLikelihood=6.0);
+   /** Jet selection */
+   declareProperty("JetPt",          m_jetPt=20*GeV);
+   declareProperty("JetEta",         m_jetEta=5.0);
+   declareProperty("BJetLikelihood", m_bJetLikelihood=6.0);
 
 }
 
-//------------------------------------------------------------------------------
 StatusCode UserAnalysisSelectionTool::initialize() {
 
-  ATH_MSG_DEBUG(MSG::DEBUG << "in initialize()");
+   ATH_MSG_DEBUG( "in initialize()" );
+   ATH_MSG_DEBUG( " electron Flag/isEM val " << m_electronIsEMFlag << ","
+                  << m_electronIsEM );
 
-
-  ATH_MSG_DEBUG(" electron Flag/isEM val "<< m_electronIsEMFlag <<","<<m_electronIsEM);
-  return StatusCode::SUCCESS;
+   return StatusCode::SUCCESS;
 }
 
-//------------------------------------------------------------------------------
-StatusCode UserAnalysisSelectionTool::finalize() {
+bool UserAnalysisSelectionTool::
+isSelected( const Analysis::Electron* electron ) const {
 
-  ATH_MSG_DEBUG("in finalize()");
- 
-  return StatusCode::SUCCESS;
+   if( ! electron ) {
+      return false;
+   }
+
+   ATH_MSG_VERBOSE( "in electron isSelected()" );
+
+   bool select = ( ( electron->pt() > m_electronPt ) &&
+                   ( std::abs( electron->eta() ) < m_electronEta ) );
+
+   if( m_isAtlfast ) {
+      return select;
+   }
+
+   if( m_electronIsEMFlag == "Loose" ) {
+      select = select && ( ! electron->isem( egammaPIDObs::ElectronLoose ) );
+   } else if( m_electronIsEMFlag == "Medium" ) {
+      select = select && ( ! electron->isem( egammaPIDObs::ElectronMedium ) );
+   } else if( m_electronIsEMFlag == "Tight" ) {
+      select = select && ( ! electron->isem( egammaPIDObs::ElectronTight ) );
+   } else {
+      select = select && ( ( electron->isem() & m_electronIsEM ) ==0 );
+   }
+
+   ATH_MSG_VERBOSE( "in electron isSelected() -1 " );
+
+   if( m_authorEgammaOnly ) {
+      select = select && electron->author( egammaParameters::AuthorElectron );
+   }
+
+   ATH_MSG_VERBOSE( "in electron isSelected() - 2 " );
+   if( m_electronEtaWindCut ) {
+      const double diff = std::abs( electron->eta() ) - m_electronEtaWindCent;
+      select = select && ( std::abs( diff ) > m_electronEtaWind );
+   }
+
+   ATH_MSG_VERBOSE( "in electron isSelected() " << m_egDetailContainerName );
+
+   if( m_doElectronIsolation ) {
+      const EMShower* egdetail =
+            electron->detail< EMShower >( m_egDetailContainerName );
+      ATH_MSG_VERBOSE( "in electron isSelected() " << egdetail );
+      double etIsol = 0.;
+      if( egdetail ) {
+         const auto helper1 = m_electronIsolationConeIndex;
+         const egammaParameters::ParamDef helper2 =
+               static_cast< egammaParameters::ParamDef >( helper1 );
+         etIsol = egdetail->parameter( helper2 );
+      }
+      select = select && ( etIsol < m_electronIsolationEt );
+      if( electron->pt() ) {
+         select = select && ( ( etIsol / electron->pt() ) <
+                              m_normElectronIsolEt );
+      }
+   }
+   ATH_MSG_VERBOSE("in electron isSelected() - 4 ");
+
+   return select;
 }
 
-//------------------------------------------------------------------------------
-UserAnalysisSelectionTool::~UserAnalysisSelectionTool()
-{}
+bool UserAnalysisSelectionTool::
+isSelected( const Analysis::Photon* photon ) const  {
 
-bool UserAnalysisSelectionTool::isSelected( const Analysis::Electron * electron ) const
-{
-  ATH_MSG_DEBUG("in electron isSelected()");
+   if( ! photon ) {
+      return false;
+   }
 
-  bool select = false;
-  if ( !electron ) return select;
+   bool select = ( ( photon->pt() > m_photonPt ) &&
+                   ( std::abs( photon->eta() ) < m_photonEta ) );
 
-  if ( m_isAtlfast ) {
-    select = electron->pt()>m_electronPt && fabs(electron->eta())<m_electronEta;
-    return select;
-  }
+   if( m_isAtlfast ) {
+      return select;
+   }
 
-  if ( m_electronIsEMFlag == "Loose" )                 select = electron->isem(egammaPID::ElectronLoose)==0;
-  else if ( m_electronIsEMFlag == "Medium" )           select = electron->isem(egammaPID::ElectronMedium)==0;
-  //else if ( m_electronIsEMFlag == "TightNoIsolation" ) select = electron->isem(egammaPID::ElectronTightNoIsolation)==0;
-  else if ( m_electronIsEMFlag == "Tight" )            select = electron->isem(egammaPID::ElectronTight)==0;
-  else select = ((electron->isem()&m_electronIsEM)==0); 
-
-  ATH_MSG_DEBUG("in electron isSelected() -1 ");
-
-  if ( m_authorEgammaOnly ) select = select && electron->author(egammaParameters::AuthorElectron);
-  select = select && electron->pt()>m_electronPt && fabs(electron->eta())<m_electronEta;
-  // check if electron is in bad eta region
-
-  ATH_MSG_DEBUG("in electron isSelected() - 2 ");
-  if(m_electronEtaWindCut) {
-
-    double diff = fabs( electron->eta()) - m_electronEtaWindCent;
-    select = select &&( fabs(diff)  > m_electronEtaWind );
-  }
-
-  ATH_MSG_DEBUG("in electron isSelected() " << m_egDetailContainerName);
-
-  if ( m_doElectronIsolation ) {
-    const EMShower* egdetail = electron->detail<EMShower>(m_egDetailContainerName);
-    ATH_MSG_VERBOSE("in electron isSelected() " << egdetail);
-    double etIsol = 0.;
-    if(egdetail) {
-      etIsol = egdetail->parameter( static_cast<egammaParameters::ParamDef>(m_electronIsolationConeIndex) );
-    }
-    select = select && ( etIsol < m_electronIsolationEt );
-    if ( electron->pt() ) select = select && ( (etIsol/electron->pt()) < m_normElectronIsolEt );
-  }
-  ATH_MSG_DEBUG("in electron isSelected() - 4 ");
-
-  return select;
+   select = select && ( photon->isem() == m_photonIsEM );
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const Analysis::Photon * photon ) const 
-{
-  bool select = false;
-  if ( !photon ) return select;
+bool UserAnalysisSelectionTool::
+isSelected( const Analysis::Muon* muon ) const {
 
-  if ( m_isAtlfast ) {
-    select = photon->pt()>m_photonPt && fabs(photon->eta())<m_photonEta;
-    return select;
-  }
- 
-  select = photon->pt()>m_photonPt && fabs(photon->eta())<m_photonEta && photon->isem()==m_photonIsEM;
-  return select;
+   if( ! muon ) {
+      return false;
+   }
+
+   bool select = ( ( muon->pt() > m_muonPt ) &&
+                   ( std::abs( muon->eta() ) < m_muonEta ) );
+
+   if( m_isAtlfast ) {
+      return select;
+   }
+   if( m_useMatchChi2 && muon->isCombinedMuon() ) {
+      select = select && ( muon->matchChi2() < m_muonMatchChi2 );
+   }
+   if( m_doMuonIsolation ) {
+      const MuonParameters::ParamDef helper =
+            static_cast< MuonParameters::ParamDef >( m_muonIsolationConeIndex );
+      const double etIsol = muon->parameter( helper );
+      select = select && ( etIsol < m_muonIsolationEt );
+      if ( muon->pt() ) {
+         select = select && ( ( etIsol / muon->pt() ) < m_normMuonIsolEt );
+      }
+   }
+
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const Analysis::Muon * muon ) const
-{
-  bool select = false;
-  if ( !muon ) return select;
+bool UserAnalysisSelectionTool::isSelected( const Jet* jet ) const {
 
-  if ( m_isAtlfast ) {
-    select = muon->pt()>m_muonPt && fabs(muon->eta())<m_muonEta;
-    return select;
-  }
+   if( ! jet ) {
+      return false;
+   }
 
-  select = muon->pt()>m_muonPt && fabs(muon->eta())<m_muonEta;
-  if ( m_useMatchChi2 && muon->isCombinedMuon() ) select = select && muon->matchChi2() < m_muonMatchChi2;
-  if ( m_doMuonIsolation ) {
-    double etIsol = muon->parameter( static_cast<MuonParameters::ParamDef>(m_muonIsolationConeIndex) );
-    select = select && ( etIsol < m_muonIsolationEt );
-    if ( muon->pt() ) select = select && ( (etIsol/muon->pt()) < m_normMuonIsolEt );
- 
-  }
+   bool select = ( ( jet->pt() > m_jetPt ) &&
+                   ( std::abs( jet->eta() ) < m_jetEta ) );
 
-  return select;
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const Jet* jet ) const
-{
-  bool select = false;
-  if ( !jet ) return select;
-  select = jet->pt() > m_jetPt && fabs(jet->eta()) < m_jetEta;
+bool UserAnalysisSelectionTool::
+isSelected( const Rec::TrackParticle* trackParticle ) const {
 
-  if ( m_isAtlfast ) return select;
+   if( ! trackParticle ) {
+      return false;
+   }
 
-  return select;
+   bool select = trackParticle->pt() > m_trackParticlePt;
+
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const Rec::TrackParticle * trackParticle ) const
-{
-  bool select = false;
-  if ( !trackParticle ) return select;
-  select = trackParticle->pt() > m_trackParticlePt; 
+bool UserAnalysisSelectionTool::
+isSelected( const CaloCluster* caloCluster ) const {
 
-  if ( m_isAtlfast ) return select;
+   if( ! caloCluster ) {
+      return false;
+   }
 
-  return select;
+   bool select = caloCluster->e() > m_caloClusterE;
+
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const CaloCluster* caloCluster ) const
-{
-  bool select = false;
-  if ( !caloCluster ) return select;
-  select = caloCluster->e() > m_caloClusterE;
+bool UserAnalysisSelectionTool::
+isSelected( const Analysis::TauJet* tauJet ) const {
 
-  if ( m_isAtlfast ) return select;
+   if( ! tauJet ) {
+      return false;
+   }
 
-  return select;
+   const int numTrack = tauJet->numTrack();
+   bool select = ( ( tauJet->pt() > m_tauJetPt ) &&
+                   ( std::abs( tauJet->eta() ) < m_tauJetEta ) &&
+                   ( ( std::abs( tauJet->charge() ) - 1.0 ) < 0.001 ) &&
+                   ( ( numTrack == 1 ) || ( numTrack == 3 ) ) );
+
+   const Analysis::TauPID* tauId = tauJet->tauID();
+   if ( tauId ) {
+      const auto likelihood =
+            tauId->discriminant( TauJetParameters::Likelihood );
+      const auto bdtelescore =
+            tauId->discriminant( TauJetParameters::BDTEleScore );
+      select = select && ( ( likelihood > m_tauJetLikelihood ) &&
+                           ( bdtelescore > m_tauEleBDTCut ) );
+   }
+
+   return select;
 }
 
-bool UserAnalysisSelectionTool::isSelected( const Analysis::TauJet * tauJet ) const {
+bool UserAnalysisSelectionTool::isBJet( const Jet* jet ) const {
 
-  bool select = false;
-  if ( !tauJet ) return select;
+   if( ! jet ) {
+      return false;
+   }
 
-  int numTrack = tauJet->numTrack();
-  select = tauJet->pt()>m_tauJetPt &&
-           fabs(tauJet->eta())<m_tauJetEta &&
-           fabs(tauJet->charge())==1.0 &&
-           (numTrack==1 || numTrack==3);
-
-  const Analysis::TauPID* tauId = tauJet->tauID();
-  if ( tauId ) {
-    select = select &&
-             tauId->discriminant( TauJetParameters::Likelihood ) > m_tauJetLikelihood &&
-             tauId->discriminant( TauJetParameters::BDTEleScore )> m_tauEleBDTCut;
-  }
-
-  return select;
-
+   bool select = this->isSelected( jet );
+   return ( select && ( jet->getFlavourTagWeight() > m_bJetLikelihood ) );
 }
-
-bool UserAnalysisSelectionTool::isBJet( const Jet * jet ) const {
-
-  /** first check that it is a selected jet */
-  if ( !jet ) return false;
-
-  bool is_bjet = this->isSelected( jet );
-  return ( is_bjet && jet->getFlavourTagWeight()>m_bJetLikelihood );
-}
-
