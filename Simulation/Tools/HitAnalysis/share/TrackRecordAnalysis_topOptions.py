e8b4a37fd8acb749d4bea7d207603cc33707f4c5
Ana Rosario Cueto Gomez <ana.rosario.cueto.gomez@cern.ch>
2016-09-15 08:51:48 +0200

    Outputs histograms and ntuples (HitAnalysis-00-02-00)
    	* Moving headers into src directory
    	* Adding first draft of PixelHitTool obo Ana
    	* Tagging: HitAnalysis-00-00-11
    
    2015.01.19  Zach Marshall <ZLMarshall@lbl.gov>
    	* Simplifying code
    	* Tagging: HitAnalysis-00-00-10
    
    
    Former-commit-id: a336e0b7a7e2182a6dd60010cdb6786c012f0969

--- Simulation/Tools/HitAnalysis/share/TrackRecordAnalysis_topOptions.py
+++ Simulation/Tools/HitAnalysis/share/TrackRecordAnalysis_topOptions.py
@@ -5,7 +5,7 @@ import AthenaPoolCnvSvc.ReadAthenaPool
 from PartPropSvc.PartPropSvcConf import PartPropSvc
 
 include( "ParticleBuilderOptions/McAOD_PoolCnv_jobOptions.py")
-#include( "EventAthenaPool/EventAthenaPool_joboptions.py" )
+include( "EventAthenaPool/EventAthenaPool_joboptions.py" )
 
 import os
 from glob import glob
@@ -16,20 +16,22 @@ ServiceMgr.EventSelector.InputCollections = athenaCommonFlags.FilesInput() # Thi
 from AthenaCommon.AlgSequence import AlgSequence
 topSequence = AlgSequence()
 
-from HitAnalysis.HitAnalysisConf import CaloHitAnalysis
-CaloHitAnalysis = CaloHitAnalysis()
-topSequence += CaloHitAnalysis
-CaloHitAnalysis.HistPath = '/CaloHitAnalysis/histos/'
-CaloHitAnalysis.NtupleFileName = '/CaloHitAnalysis/ntuple/'
-#ExpertMode adds more histograms to the output. Default mode is off
-CaloHitAnalysis.ExpertMode = "off"
-#CalibHits adds Calibrated hits histograms to the output. Default mode is off
-CaloHitAnalysis.CalibHits = "off"
+from HitAnalysis.HitAnalysisConf import TrackRecordAnalysis
+topSequence += TrackRecordAnalysis('TrackRecordCaloEntry') 
+topSequence.TrackRecordCaloEntry.CollectionName='CaloEntryLayer'
+topSequence.TrackRecordCaloEntry.HistPath='/TrackRecordAnalysis/'
+topSequence += TrackRecordAnalysis('TrackRecordMuonEntry') 
+topSequence.TrackRecordMuonEntry.CollectionName='MuonEntryLayer'
+topSequence.TrackRecordMuonEntry.HistPath='/TrackRecordAnalysis/'
+topSequence += TrackRecordAnalysis('TrackRecordMuonExit') 
+topSequence.TrackRecordMuonExit.CollectionName='MuonExitLayer'
+topSequence.TrackRecordMuonExit.HistPath='/TrackRecordAnalysis/'
+
+
 
 from GaudiSvc.GaudiSvcConf import THistSvc
 ServiceMgr += THistSvc()
-ServiceMgr.THistSvc.Output += ["CaloHitAnalysis DATAFILE='CaloHitAnalysis.root' OPT='RECREATE'" ]
-
+ServiceMgr.THistSvc.Output += [ "TrackRecordAnalysis DATAFILE='TrackRecordAnalysis.root' OPT='RECREATE'" ]
 
 ServiceMgr.MessageSvc.OutputLevel = INFO
 ServiceMgr.MessageSvc.defaultLimit = 9999999
@@ -39,11 +41,11 @@ theApp.EvtMax = -1
 ServiceMgr.AuditorSvc.Auditors  += [ "ChronoAuditor"]
 
 AthenaPoolCnvSvc = Service("AthenaPoolCnvSvc")
-AthenaPoolCnvSvc.UseDetailChronoStat = TRUE
+AthenaPoolCnvSvc.UseDetailChronoStat = FALSE
 
 # To set up a geometry
 from RecExConfig.AutoConfiguration import *
 ConfigureFieldAndGeo() # Configure the settings for the geometry
 include("RecExCond/AllDet_detDescr.py") # Actually load the geometry
-#include( "TrkDetDescrSvc/AtlasTrackingGeometrySvc.py" ) # Tracking geometry, handy for ID work
+#include("TrkDetDescrSvc/AtlasTrackingGeometrySvc.py") # Tracking geometry, handy for ID work
 
