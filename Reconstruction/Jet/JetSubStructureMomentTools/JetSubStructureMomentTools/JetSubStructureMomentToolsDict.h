a04530632e73d0ed125ac0225e3cd00e7a45c7b6
Miles Wu <miles.wu@cern.ch>
2016-09-29 19:28:27 +0200

    Tag (JetSubStructureMomentTools-00-01-33)
    	* BoostedXbbTagTool: Update to version used for ICHEP 2016 result
    	* Tagging as 00-01-33
    
    2016-09-06  scott snyder  <snyder@bnl.gov>
    
            * Tagging JetSubStructureMomentTools-00-01-32.
    	* Do not put using directives in the global namespace in headers.
    
    2016-08-16  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
            * Tagging JetSubStructureMomentTools-00-01-31
    
    2016-08-16  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
    	* Remove Validator entry from dictionaries
    	* Tagging JetSubStructureMomentTools-00-01-29
    
    2016-08-12  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
    	* Root/LinkDef.h: Remove Validator entry
    	* Tagging JetSubStructureMomentTools-00-01-28
    
    2016-08-11  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 51589b70512e7671b16a0345fc3b81e5658c9e7d

--- Reconstruction/Jet/JetSubStructureMomentTools/JetSubStructureMomentTools/JetSubStructureMomentToolsDict.h
+++ Reconstruction/Jet/JetSubStructureMomentTools/JetSubStructureMomentTools/JetSubStructureMomentToolsDict.h
@@ -2,6 +2,9 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
+#ifndef JETSUBSTRUCTUREMOMENTTOOLS_JETSUBSTRUCTUREMOMENTTOOLSDICT_H
+#define JETSUBSTRUCTUREMOMENTTOOLS_JETSUBSTRUCTUREMOMENTTOOLSDICT_H
+
 #include "JetSubStructureMomentTools/AngularityTool.h"
 #include "JetSubStructureMomentTools/DipolarityTool.h"
 #include "JetSubStructureMomentTools/JetChargeTool.h"
@@ -25,36 +28,4 @@
 #include "JetSubStructureMomentTools/KtMassDropTool.h"
 #include "JetSubStructureMomentTools/QwTool.h"
 #include "JetSubStructureMomentTools/SubjetRecorderTool.h"
-
-#ifdef __CINT__
-
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ nestedclass;
-
-#pragma link C++ class AngularityTool+;
-#pragma link C++ class BoostedXbbTagTool+;
-#pragma link C++ class BosonTagTool+;
-#pragma link C++ class CenterOfMassShapesTool+;
-#pragma link C++ class DipolarityTool+;
-#pragma link C++ class EnergyCorrelatorRatiosTool+;
-#pragma link C++ class EnergyCorrelatorTool+;
-#pragma link C++ class ISubjetRecorderTool+;
-#pragma link C++ class JetChargeTool+;
-#pragma link C++ class JetPullTool+;
-#pragma link C++ class JetSubStructureMomentToolsBase+;
-#pragma link C++ class KTSplittingScaleTool+;
-#pragma link C++ class KtDeltaRTool+;
-#pragma link C++ class KtMassDropTool+;
-#pragma link C++ class NSubjettinessRatiosTool+;
-#pragma link C++ class NSubjettinessTool+;
-#pragma link C++ class PlanarFlowTool+;
-#pragma link C++ class QwTool+;
-#pragma link C++ class ShowerDeconstructionTool+;
-#pragma link C++ class SubjetFinderTool+;
-#pragma link C++ class SubjetMakerTool+;
-#pragma link C++ class SubjetRecorderTool+;
-#pragma link C++ class VolatilityTool+;
-
 #endif
