7e394dfbf0b8113dcd3398e909cd30615388b063
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-09-30 13:54:21 +0200

    Make sure SD collections are properly initialized in every Athena event (ATLASSIM-3079) (ISF_Geant4Services-00-02-02)
    	* Geant4SimSvc.cxx: make sure SD collections are properly initialized in
    	every Athena event (ATLASSIM-3079)
    	* Tagging ISF_Geant4Services-00-02-02
    
    2016-07-29  John Chapman  <John.Chapman@cern.ch>
    
    	* CMakeLists.txt: add missing atlas_install_python_modules
    	call. ATLASSIM-2679, ATLASSIM-3004
    	* Tagging ISF_Geant4Services-00-02-01
    
    2016-07-11  John Chapman  <John.Chapman@cern.ch>
    
    	* add python/iGeant4.py, python/ISF_Geant4ServicesConfig.py,
    	python/ISF_Geant4ServicesConfigDb.py - with configuration
    	previously in ISF_Geant4Config and ISF_Config
    	packages. ATLASSIM-2352. Further tidying still required.
    	* Tagging ISF_Geant4Services-00-02-00
    
    
    Former-commit-id: 3089b7e760b42921ec4616cc61fc6615cd99688f

---
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Services/python/ISF_Geant4ServicesConfigDb.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addService
+addService("ISF_Geant4Services.ISF_Geant4ServicesConfig.getGeant4SimSvc",          "ISF_Geant4SimSvc")
+addService("ISF_Geant4Services.ISF_Geant4ServicesConfig.getFullGeant4SimSvc",      "ISF_FullGeant4SimSvc")
+addService("ISF_Geant4Services.ISF_Geant4ServicesConfig.getPassBackGeant4SimSvc",  "ISF_PassBackGeant4SimSvc")
+addService("ISF_Geant4Services.ISF_Geant4ServicesConfig.getAFIIGeant4SimSvc",        "ISF_AFIIGeant4SimSvc")
+addService("ISF_Geant4Services.ISF_Geant4ServicesConfig.getLongLivedGeant4SimSvc",   "ISF_LongLivedGeant4SimSvc")
