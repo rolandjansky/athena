9c9bad51b5a430d1a9dd1d51297e72d98edbffb8
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2016-10-03 16:37:20 +0200

    'allow toggle for E/P weight' (PFlowUtils-00-00-12)
    Former-commit-id: 0b74c46335492047862f65ba954fceb7c31855ff

--- Reconstruction/PFlow/PFlowUtils/Root/WeightPFOTool.cxx
+++ Reconstruction/PFlow/PFlowUtils/Root/WeightPFOTool.cxx
@@ -6,7 +6,10 @@
 
 namespace CP {
 
-  WeightPFOTool::WeightPFOTool(const std::string& name) : asg::AsgTool( name ) { }
+  WeightPFOTool::WeightPFOTool(const std::string& name) : asg::AsgTool( name )
+  {
+    declareProperty("DoEoverPWeight", m_doEoverPweight=true);
+  }
 
   // The intended result:
   // 
@@ -39,24 +42,31 @@ namespace CP {
       // Start by computing the correction as though we subtracted the calo energy
       // This interpolates between the full track P and the expected calo E
       float EoverP = expectedEnergy/cpfo.e(); // divide once only
-      if(cpfo.pt()<30e3) {        // take full track
-	weight = 1.;
-      } else if(cpfo.pt()<60e3) { // linearly interpolate between 1 and E/P
-	float interpolf = (1.0 - (cpfo.pt()-30000)/30000);
-	weight = EoverP + interpolf * (1-EoverP);
-      } else {                    // take the expected energy
-	weight = EoverP;
+      if(m_doEoverPweight) {
+	if(cpfo.pt()<30e3) {        // take full track
+	  weight = 1.;
+	} else if(cpfo.pt()<60e3) { // linearly interpolate between 1 and E/P
+	  float interpolf = (1.0 - (cpfo.pt()-30000)/30000);
+	  weight = EoverP + interpolf * (1-EoverP);
+	} else {                    // take the expected energy
+	  weight = EoverP;
+	}
       }
+
+      ATH_MSG_VERBOSE("cpfo in dense environment? " << isInDenseEnvironment);
+      ATH_MSG_VERBOSE("cpfo pt: " << cpfo.pt() << ", E/P: " << EoverP << ", weight: " << weight);
 
       if(isInDenseEnvironment) {
 	// In this case we further remove the expected deposited energy from the track
 	weight -= EoverP;
       }
-      // If the weight went to 0, set it to the ghost scale, so that the cPFOs
-      // are always added to the track.
-      if (weight<1e-9) {weight = 1e-40;}
     }
 	
+    ATH_MSG_VERBOSE("Weight before zero check: " << weight);
+    // If the weight went to 0, set it to the ghost scale, so that the cPFOs
+    // are always added to the track.
+    if (weight<1e-9) {weight = 1e-20;}
+    ATH_MSG_VERBOSE("Final weight: " << weight);
     return StatusCode::SUCCESS;
   }
 
