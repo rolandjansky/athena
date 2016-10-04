94c9a3618fc1a78690caf099ec63470ce8fec55b
Shaun Roe <Shaun.Roe@cern.ch>
2016-10-04 14:55:21 +0200

    fix coverity 17136 (VP1Gui-00-08-14)
    	* fix coverity 17136
    17136 09/07/2014 (Medium) Uninitialized pointer field :/graphics/VP1/VP1Gui/src/VP1Gui.cxx in function "Imp"
    	* tag as VP1Gui-00-08-14
    2016-09-09 Shaun Roe
      * Fix Coverity:
      16179 09/07/2014 (Medium) Function address comparison :/graphics/VP1/VP1Gui/src/VP1EventDisplayEditor.cxx in function "getEventDetailsLabel"
    16915 09/07/2014 (Medium) Uninitialized pointer field :/graphics/VP1/VP1Gui/src/VP1EventDisplaySceneView.cxx in function "VP1EventDisplaySceneView"
    17114 09/07/2014 (Medium) Uninitialized pointer field :/graphics/VP1/VP1Gui/src/VP1IncomingMessageDialog.cxx in function "Imp"
    17003 09/07/2014 (Medium) Uninitialized scalar field :/graphics/VP1/VP1Gui/src/VP1TabManager.cxx in function "Imp"
    16381 09/07/2014 (Medium) Unchecked dynamic_cast :/graphics/VP1/VP1Gui/src/VP1TabManager.cxx in function "launchStereoEditorCurrentTab"
      *tag as VP1Gui-00-08-13
    
    
    Former-commit-id: 8de3bb4bdeece785a49281a16d007cb4295b55a5

--- graphics/VP1/VP1Gui/src/VP1EventDisplayEditor.cxx
+++ graphics/VP1/VP1Gui/src/VP1EventDisplayEditor.cxx
@@ -371,7 +371,7 @@ QString VP1EventDisplayEditor::getEventDetailsLabel()
 
 	QString evtstr = "Run: "+QString::number(_runNumber)+"\n"
 			+ "Event: "+QString::number(_eventNumber)+"\n"
-			+ QString(time>0 ? QDateTime::fromTime_t(_eventTimestamp).toString(Qt::ISODate).replace('T',' ')+ " CEST" : "");
+			+ QString(QDateTime::fromTime_t(_eventTimestamp).toString(Qt::ISODate).replace('T',' ')+ " CEST" );
 
 	//VP1Msg::messageVerbose(evtstr);
 
