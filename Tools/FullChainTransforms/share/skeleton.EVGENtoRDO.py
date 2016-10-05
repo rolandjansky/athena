0501a0baf6ea04ef920928664a91830970b22cfa
Elmar Ritsch <Elmar.Ritsch@cern.ch>
2016-10-05 13:40:30 +0200

    Fix for FastSim full chain jobs for releases with refactored ISF input processing. ATLASSIM-2952 (FullChainTransforms-00-01-09-02)
    	* share/skeleton.EVGENtoRDO.py: Fix for FastSim full chain jobs
    	for releases with refactored ISF input processing. ATLASSIM-2952
    	* Tag FullChainTransforms-00-01-09-02

--- Tools/FullChainTransforms/share/skeleton.EVGENtoRDO.py
+++ Tools/FullChainTransforms/share/skeleton.EVGENtoRDO.py
@@ -243,12 +243,6 @@ else:
 
 include("ISF_Config/ISF_ConfigJobInclude.py")
 
-## check to see if  pileup emulation is being used, if so do post-ISF-config
-## actions to enable simulation of pileup collection
-if 'AthSequencer/EvgenGenSeq' in topSeq.getSequence():
-    fast_chain_log.info("Pileup emulation enabled - setup GenEventStackFiller")
-    include("FastChainPileup/FastPileupSimConfig.py")
-
 ## Add AMITag MetaData to TagInfoMgr
 if hasattr(runArgs, 'AMITag'):
     if runArgs.AMITag != "NONE":
