3641a2ce39b924e1865716247ffb19832a288fb9
Sasha Mazurov <Alexander.Mazurov@cern.ch>
2016-10-07 12:09:56 +0200

    L1EnergyCMXTools.{h,cmx}: fix resticted and full ranges calculations (TrigT1CaloTools-03-06-31)

--- Trigger/TrigT1/TrigT1CaloTools/TrigT1CaloTools/L1EnergyCMXTools.h
+++ Trigger/TrigT1/TrigT1CaloTools/TrigT1CaloTools/L1EnergyCMXTools.h
@@ -1,5 +1,5 @@
 
- /***************************************************************************
+/***************************************************************************
   *                                                                         *
   *   This program is free software; you can redistribute it and/or modify  *
   *   it under the terms of the GNU General Public License as published by  *
@@ -26,7 +26,6 @@
 
 #include "xAODTrigL1Calo/JEMEtSumsContainer.h"
 #include "xAODTrigL1Calo/CMXEtSumsContainer.h"
-  
 
 namespace LVL1
 {
@@ -34,7 +33,7 @@ class ModuleEnergy;
 class CrateEnergy;
 class SystemEnergy;
 
-  /** @class L1EnergyCMXTools
+/** @class L1EnergyCMXTools
 
       This is a tool to reconstruct the L1 JEM and CMX-Energy Et sums
       from Jet Elements.
@@ -46,74 +45,77 @@ class SystemEnergy;
 	                                        //   from JEMEtSums
       
       @author  Peter Faulkner
-  */  
+  */
 
 class L1EnergyCMXTools : virtual public IL1EnergyCMXTools, public AthAlgTool
 {
   public:
-     
-    L1EnergyCMXTools(const std::string& type, const std::string& name,
-                                              const IInterface* parent);
+    L1EnergyCMXTools(const std::string &type, const std::string &name,
+                     const IInterface *parent);
 
-     /** default destructor */
-    virtual ~L1EnergyCMXTools ();
-      
-     /** standard Athena-Algorithm method */
+    /** default destructor */
+    virtual ~L1EnergyCMXTools();
+
+    /** standard Athena-Algorithm method */
     virtual StatusCode initialize();
-     /** standard Athena-Algorithm method */
-    virtual StatusCode finalize  ();
-      
-     /** form JEMEtSums from JetElements */
-    virtual void formJEMEtSums(const xAOD::JetElementContainer* jetElementVec,
-                               xAOD::JEMEtSumsContainer* jemEtSumsVec) const;
-     /** form complete CMXEtSums from JEMEtSums */
-    virtual void formCMXEtSums(const xAOD::JEMEtSumsContainer* jemEtSumsVec,
-                               xAOD::CMXEtSumsContainer* cmxEtSumsVec) const;
-     /** form partial CMXEtSums (module) from JEMEtSums */
-    virtual void formCMXEtSumsModule(const xAOD::JEMEtSumsContainer* jemEtSumsVec,
-                               xAOD::CMXEtSumsContainer* cmxEtSumsMod) const;
-     /** form partial CMXEtSums (crate) from module CMXEtSums */
-    virtual void formCMXEtSumsCrate(const xAOD::CMXEtSumsContainer* cmxEtSumsMod,
-                               xAOD::CMXEtSumsContainer* cmxEtSumsCrate) const;
-     /** form partial CMXEtSums (system) from crate CMXEtSums */
-    virtual void formCMXEtSumsSystem(
-                               const xAOD::CMXEtSumsContainer* cmxEtSumsCrate,
-                               xAOD::CMXEtSumsContainer* cmxEtSumsSys) const;
-     /** form partial CMXEtSums (sumEt/missingEt maps) from system CMXEtSums */
-    virtual void formCMXEtSumsEtMaps(const xAOD::CMXEtSumsContainer* cmxEtSumsSys,
-                               xAOD::CMXEtSumsContainer* cmxEtSumsMap) const;
-    
-        
-  private:
+    /** standard Athena-Algorithm method */
+    virtual StatusCode finalize();
 
-    typedef std::vector<uint16_t>              EnergyVector;
-    typedef std::vector<uint32_t>                       ErrorVector;
-    typedef std::vector<DataVector<ModuleEnergy>*> MultiSliceModuleEnergy;
-    typedef std::vector<DataVector<CrateEnergy>*>  MultiSliceCrateEnergy;
-    typedef std::vector<SystemEnergy*>             MultiSliceSystemEnergy;
+    /** form JEMEtSums from JetElements */
+    virtual void formJEMEtSums(const xAOD::JetElementContainer *jetElementVec,
+                               xAOD::JEMEtSumsContainer *jemEtSumsVec) const;
+    /** form complete CMXEtSums from JEMEtSums */
+    virtual void formCMXEtSums(const xAOD::JEMEtSumsContainer *jemEtSumsVec,
+                               xAOD::CMXEtSumsContainer *cmxEtSumsVec) const;
+    /** form partial CMXEtSums (module) from JEMEtSums */
+    virtual void formCMXEtSumsModule(const xAOD::JEMEtSumsContainer *jemEtSumsVec,
+                                     xAOD::CMXEtSumsContainer *cmxEtSumsMod) const;
+    /** form partial CMXEtSums (crate) from module CMXEtSums */
+    virtual void formCMXEtSumsCrate(const xAOD::CMXEtSumsContainer *cmxEtSumsMod,
+                                    xAOD::CMXEtSumsContainer *cmxEtSumsCrate) const;
+
+    /** form partial CMXEtSums (system) from crate CMXEtSums */
+    virtual void formCMXEtSumsSystem(
+        const xAOD::CMXEtSumsContainer *cmxEtSumsCrate,
+        xAOD::CMXEtSumsContainer *cmxEtSumsSys) const;
+    /** form partial CMXEtSums (sumEt/missingEt maps) from system CMXEtSums */
+    virtual void formCMXEtSumsEtMaps(const xAOD::CMXEtSumsContainer *cmxEtSumsSys,
+                                     xAOD::CMXEtSumsContainer *cmxEtSumsMap) const;
+
+  private:
+    typedef std::vector<uint16_t> EnergyVector;
+    typedef std::vector<uint32_t> ErrorVector;
+    typedef std::vector<DataVector<ModuleEnergy> *> MultiSliceModuleEnergy;
+    typedef std::vector<DataVector<CrateEnergy> *> MultiSliceCrateEnergy;
+    typedef std::vector<SystemEnergy *> MultiSliceSystemEnergy;
 
     /** Convert CMXEtSums container to internal ModuleEnergy containers */
-    void etSumsToModuleEnergy(const xAOD::CMXEtSumsContainer* etSums,
-                       MultiSliceModuleEnergy& modulesVec, int& peak) const;
+    void etSumsToModuleEnergy(const xAOD::CMXEtSumsContainer *etSums,
+                              MultiSliceModuleEnergy &modulesVec, int &peak) const;
     /** Convert CMXEtSums container to internal CrateEnergy containers */
-    void etSumsToCrateEnergy(const xAOD::CMXEtSumsContainer* etSums,
-                       MultiSliceCrateEnergy& crateVec, int& peak) const;
+    void etSumsToCrateEnergy(const xAOD::CMXEtSumsContainer *etSums,
+                             MultiSliceCrateEnergy &crateVecFull, MultiSliceCrateEnergy &crateVecRestricted,
+                             int &peak) const;
     /** Convert CMXEtSums container to internal SystemEnergy objects */
-    void etSumsToSystemEnergy(const xAOD::CMXEtSumsContainer* etSums,
-                       MultiSliceSystemEnergy& systemVec, int& peak) const;
+    void etSumsToSystemEnergy(const xAOD::CMXEtSumsContainer *etSums,
+                              MultiSliceSystemEnergy &systemVec, int &peak) const;
     /** Convert internal ModuleEnergy containers to JEMEtSums container */
-    void moduleEnergyToEtSums(const MultiSliceModuleEnergy& modulesVec,
-                       xAOD::JEMEtSumsContainer* jemEtSumsVec, int peak) const;
+    void moduleEnergyToEtSums(const MultiSliceModuleEnergy &modulesVec,
+                              xAOD::JEMEtSumsContainer *jemEtSumsVec, int peak) const;
     /** Convert internal CrateEnergy containers to CMXEtSums container */
-    void crateEnergyToEtSums(const MultiSliceCrateEnergy& cratesVec,
-                       xAOD::CMXEtSumsContainer* cmxEtSumsVec, int peak) const;
-    /** Convert internal SystemEnergy objects to CMXEtSums object */
-    void systemEnergyToEtSums(const MultiSliceSystemEnergy& systemVec,
-                       xAOD::CMXEtSumsContainer* cmxEtSumsVec, int peak) const;
-    /** Convert maps from internal SystemEnergy objects to CMXEtSums objects */
-    void etMapsToEtSums(const MultiSliceSystemEnergy& systemVec,
-                       xAOD::CMXEtSumsContainer* cmxEtSumsVec, int peak) const;
+    void crateEnergyToEtSums(const MultiSliceCrateEnergy &cratesVec,
+                             xAOD::CMXEtSumsContainer *cmxEtSumsVec, int peak) const;
+    void crateEnergyToEtSums(
+        const MultiSliceCrateEnergy &cratesVecFull, const MultiSliceCrateEnergy &cratesVecRestricted,
+        xAOD::CMXEtSumsContainer *cmxEtSumsVec, int peak) const;
 
+    /** Convert internal SystemEnergy objects to CMXEtSums object */
+    void systemEnergyToEtSums(const MultiSliceSystemEnergy &systemVec,
+                              xAOD::CMXEtSumsContainer *cmxEtSumsVec, int peak, xAOD::CMXEtSums_v1::Sources source = xAOD::CMXEtSums::TOTAL_STANDARD) const;
+    /** Convert maps from internal SystemEnergy objects to CMXEtSums objects */
+    void etMapsToEtSums(const MultiSliceSystemEnergy &systemVec,
+                        xAOD::CMXEtSumsContainer *cmxEtSumsVec, int peak) const;
+    void findRestrictedEta(float &etaTruncXE, float &etaTruncTE) const;
     /** trigger configuration service */
     ServiceHandle<TrigConf::ITrigConfigSvc> m_configSvc;
     /** Tool for JetElement map */
@@ -122,9 +124,13 @@ class L1EnergyCMXTools : virtual public IL1EnergyCMXTools, public AthAlgTool
     ToolHandle<LVL1::IL1EtTools> m_etTool;
     /** Debug flag */
     bool m_debug;
-     
+    /** Find restructed eta range.
+     *  This will use the min/max values for any threshold in the range 9-16 to define the ranges
+     */
+    float m_etaTruncXE;
+    float m_etaTruncTE;
 };
 
 } // end of namespace
 
-#endif 
+#endif
