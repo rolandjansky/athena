b420f1e4fadac151ac7dcf18f21f023cfb333f5a
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-10-11 09:19:42 +0200

    Remove postInclude.CrabKissingVertexPositioner_noSmear.py, postInclude.CrabKissingVertexPositioner_flat0mrad.py, postInclude.CrabKissingVertexPositioner_flat50mrad.py, postInclude.CrabKissingVertexPositioner_gauss0mrad.py, postInclude.CrabKissingVertexPositioner_gauss50mrad.py. Add share/postInclude.CrabKissingVertexPositioner_Nominal.py, share/postInclude.CrabKissingVertexPositioner_Stage1.py, share/postInclude.CrabKissingVertexPositioner_200MHz.py, share/postInclude.CrabKissingVertexPositioner_CrabKissing50mrad.py - these reproduce HL-LHC scenarios provided by the machine experts: Nominal, Stage1 and 200MHz. The fourth one is an aggressive crab kissing scenario used in HGTD simulations. ATLASSIM-3097 (Originally discussed in ATLASSIM-2395.) Tagging BeamEffects-00-00-18 (BeamEffects-00-00-18)
    	* share/postInclude.CrabKissingVertexPositioner_Nominal.py,
    	share/postInclude.CrabKissingVertexPositioner_Stage1.py,
    	share/postInclude.CrabKissingVertexPositioner_200MHz.py,
    	share/postInclude.CrabKissingVertexPositioner_CrabKissing50mrad.py
    	- update syntax. ATLASSIM-3097
    	* Tagging BeamEffects-00-00-18
    
    2016-10-10  John Chapman  <John.Chapman@cern.ch>
    
    	* remove postInclude.CrabKissingVertexPositioner_noSmear.py,
    	postInclude.CrabKissingVertexPositioner_flat0mrad.py,
    	postInclude.CrabKissingVertexPositioner_flat50mrad.py,
    	postInclude.CrabKissingVertexPositioner_gauss0mrad.py,
    	postInclude.CrabKissingVertexPositioner_gauss50mrad.py
    	* add share/postInclude.CrabKissingVertexPositioner_Nominal.py,
    	share/postInclude.CrabKissingVertexPositioner_Stage1.py,
    	share/postInclude.CrabKissingVertexPositioner_200MHz.py,
    	share/postInclude.CrabKissingVertexPositioner_CrabKissing50mrad.py
    	- these reproduce HL-LHC scenarios provided by the machine
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 363848b7f223a038a6664cec2feb67fbda1df027

--- Simulation/BeamEffects/share/postInclude.CrabKissingVertexPositioner_Nominal.py
+++ Simulation/BeamEffects/share/postInclude.CrabKissingVertexPositioner_Nominal.py
@@ -1,16 +1,13 @@
 from AthenaCommon.CfgGetter import getPublicTool
 ckvptool = getPublicTool("CrabKissingVertexPositioner")
 # all lengths are in mm
-#ckvptool.SimpleTimeSmearing = True
 ckvptool.BunchShape = "GAUSS" # GAUSS or FLAT
-ckvptool.BunchLength = 75.
-#ckvptool.TimeType = "GAUSS"
-#ckvptool.TimeWidth = .2 # 0.2ns
+ckvptool.BunchLength = 81.
 ckvptool.Epsilon = 2.5e-3 # normalized emittance
 ckvptool.BetaStar = 150. #
-ckvptool.AlfaParallel = 0e-3 #40mrad, kissing angle
-ckvptool.AlfaX = 295e-6 #rad, crabbing angle, we assume crossing angle is fully compensated
-ckvptool.ThetaX = 295e-6 #rad, half crossing angle
+ckvptool.AlfaParallel = 3.3e-3 # kissing angle
+ckvptool.AlfaX = 6e-3 #rad, crabbing angle
+ckvptool.ThetaX = 0. #rad, half crossing angle
 from AthenaCommon.AlgSequence import AlgSequence
 job = AlgSequence()
 job.BeamEffectsAlg.GenEventManipulators["GenEventVertexPositioner"].VertexShifters=[ckvptool]
