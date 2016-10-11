cdfb556faa04861c593f7c60f1013252c496cb25
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-10-11 14:29:15 +0200

    share/heavyIons/postInclude.HijingPars.py: allow the possibility of writing out multiple streams in a single job. ATLASSIM-2712. Tagging: SimulationJobOptions-00-07-16 (SimulationJobOptions-00-07-16)
    	* Tagging: SimulationJobOptions-00-07-16
    	* share/heavyIons/postInclude.HijingPars.py: allow the possibility
    	of writing out multiple streams in a single job. ATLASSIM-2712
    
    2016-10-11  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging: SimulationJobOptions-00-07-15
    	* share/heavyIons/postInclude.HijingPars.py: updates to also copy
    	HijingEventParams into the ESD and AOD. ATLASSIM-2712
    
    2016-09-09  John Chapman  <John.Chapman@cern.ch>
    
    	* Tagging: SimulationJobOptions-00-07-14
    	* share/specialConfig/preInclude.Monopole.py,
    	share/specialConfig/preInclude.Qball.py - drop use of
    	simFlags.UseV2UserActions. ATLASSIM-3023
    
    2016-06-20  John Chapman  <John.Chapman@cern.ch>
    
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/SimulationJobOptions/share/g4/preInclude.PhotonNuclOff.py
+++ Simulation/SimulationJobOptions/share/g4/preInclude.PhotonNuclOff.py
@@ -5,9 +5,5 @@
 #########################################################
 
 atlasG4log.info("Turning off photon inelastic interactions")
-def disable_photonuclear():
-    from G4AtlasApps import AtlasG4Eng
-    AtlasG4Eng.G4Eng.gbl.G4Commands().process.inactivate('PhotonInelastic', 'gamma')
-
 from G4AtlasApps.SimFlags import simFlags
-simFlags.InitFunctions.add_function("postInit", disable_photonuclear)
+simFlags.G4Commands += ['/process/inactivate PhotonInelastic gamma']
--- Simulation/SimulationJobOptions/share/g4/preInclude.VerboseProcess.py
+++ Simulation/SimulationJobOptions/share/g4/preInclude.VerboseProcess.py
@@ -1,9 +1,5 @@
 # Use verbose output for all G4 process.
 # Only works in dbg builds.
 
-def use_verbose_process():
-    from G4AtlasApps import AtlasG4Eng
-    AtlasG4Eng.G4Eng.gbl.G4Commands().process.verbose(10)
-
 from G4AtlasApps.SimFlags import simFlags
-simFlags.InitFunctions.add_function("preInitPhysics", use_verbose_process)
+simFlags.G4Commands += ['/process/verbose 10']
