6ded4a7938d09ce9b29dbd6c37853e1d97c1498f
Tomoe Kishimoto <Tomoe.Kishimoto@cern.ch>
2016-10-13 18:57:32 +0200

    Added TGC trigger DB interface (TGC_CondCabling-00-00-13)
    2016-10-13 Tomoe Kishimoto <Tomoe.Kishimoto@cern.ch>
            * Tagged as TGC_CondCabling-00-00-13
    	* Added TGC trigger DB interface
    
    
    Former-commit-id: 09459560859509d5dbe09813b9fc1ae15166a093

--- MuonSpectrometer/MuonConditions/MuonCondCabling/TGC_CondCabling/src/components/TGC_CondCabling_entries.cxx
+++ MuonSpectrometer/MuonConditions/MuonCondCabling/TGC_CondCabling/src/components/TGC_CondCabling_entries.cxx
@@ -1,9 +1,12 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "TGC_CondCabling/TGCCablingDbTool.h"
+#include "TGC_CondCabling/TGCTriggerDbTool.h"
 
 DECLARE_TOOL_FACTORY( TGCCablingDbTool )
+DECLARE_TOOL_FACTORY( TGCTriggerDbTool )
 
 DECLARE_FACTORY_ENTRIES( TGC_CondCabling ){
    DECLARE_TOOL( TGCCablingDbTool )
+   DECLARE_TOOL( TGCTriggerDbTool )
 }
 
