404ba3b5858bbe3f3b5f779a9c1f7838a69b3489
Jochen Meyer <Jochen.Meyer@cern.ch>
2016-10-17 22:04:49 +0200

    adjusting timing of SDO (RPC_Digitization-01-06-04)
    	* tagging RPC_Digitization-01-06-04
    	* adjusting timing of SDO (ATLASSIM-3112)
    
    2016-06-12  Jochen Meyer  <jomeyer@mail.cern.ch>
    	* tagging RPC_Digitization-01-06-03
    	* addressing coverity defect 108571
    
    
    Former-commit-id: 7eebac0200a34db51455dc75e70055393d30c9b6

--- MuonSpectrometer/MuonDigitization/RPC_Digitization/src/RpcDigitizationTool.cxx
+++ MuonSpectrometer/MuonDigitization/RPC_Digitization/src/RpcDigitizationTool.cxx
@@ -68,11 +68,15 @@ RpcDigitizationTool::RpcDigitizationTool(const std::string& type,
   : PileUpToolBase(type, name, pIID)
   , m_digitContainer(0)
   , m_sdoContainer(0)
+  , m_GMmgr(0)
   , m_idHelper(0)
   , muonHelper(0)
   , m_thpcRPC(0)
   , m_rSummarySvc("RPCCondSummarySvc", name)
+  , m_cs3Para(0)
   , m_validationSetup(false)
+  , SetPhiOn(false)
+  , SetEtaOn(false)
   , m_mergeSvc(0)
   , m_inputHitCollectionName("RPC_Hits")
   , m_outputDigitCollectionName("RPC_DIGITS")
@@ -80,6 +84,7 @@ RpcDigitizationTool::RpcDigitizationTool(const std::string& type,
   , m_rndmSvc("AtRndmGenSvc", name )
   , m_rndmEngine(0)
   , m_rndmEngineName("RPC_Digitization")
+  , m_tagInfoMgr(0)
 {
 
   declareInterface<IMuonDigitizationTool>(this);
@@ -811,7 +816,7 @@ StatusCode RpcDigitizationTool::doDigitization() {
             content.channelId = atlasId;
             content.deposits.push_back(deposit);
             content.gpos = intersection.position;
-            content.simTime = hit.globalTime();
+            content.simTime = hitTime(phit);
             ATH_MSG_VERBOSE("adding SDO entry: r " << content.gpos.perp() << " z " << content.gpos.z() );
           }
         }
