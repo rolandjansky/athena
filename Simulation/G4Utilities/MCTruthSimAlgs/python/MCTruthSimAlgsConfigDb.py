17c694776f42b19611d3774726e38a53cee15a10
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2016-10-17 09:50:29 +0200

    src/MergeHijingParsTool.cxx: updates to allow copying of the HijingEventParams object from background as well as signal StoreGates. src/MergeMcEventCollTool.h, src/MergeMcEventCollTool.cxx - add saveHeavyIonInfo method to try to ensure that GenEvent::heavy_ion is filled for the first event in the McEventCollection in heavy ion jobs. ATLASSIM-2712. Tagging: MCTruthSimAlgs-01-06-00 (MCTruthSimAlgs-01-06-00)
    	* src/MergeHijingParsTool.cxx: updates to allow copying of the
    	HijingEventParams object from background as well as signal
    	StoreGates. ATLASSIM-2712.
    	* src/MergeMcEventCollTool.h, src/MergeMcEventCollTool.cxx - add
    	saveHeavyIonInfo method to try to ensure that GenEvent::heavy_ion
    	is filled for the first event in the McEventCollection in heavy
    	ion jobs. ATLASSIM-2712
    	* Tagging: MCTruthSimAlgs-01-06-00
    
    2016-08-18  scott snyder  <snyder@bnl.gov>
    
    	* Tagging MCTruthSimAlgs-01-05-04.
    	* endreq -> endmsg.
    	* Tagging MCTruthSimAlgs-01-05-03.
    	* const fixes.
    
    2016-07-06  John Chapman  <John.Chapman@cern.ch>
    
    	* src/MergeTruthJetsTool.cxx - made code more robust for the case
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 4462ac274e2a7d2b5c5dbceb522982d2eab934dc

--- Simulation/G4Utilities/MCTruthSimAlgs/python/MCTruthSimAlgsConfigDb.py
+++ Simulation/G4Utilities/MCTruthSimAlgs/python/MCTruthSimAlgsConfigDb.py
@@ -10,7 +10,8 @@ addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.getCalibRange"                  , "
 addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.MergeCalibHitsTool"             , "MergeCalibHitsTool")
 
 addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.getTruthJetRange"               , "TruthJetRange")
-addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.MergeTruthJetsTool"             , "MergeTruthJetsTool")
+addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.getMergeTruthJetsTool"          , "MergeTruthJetsTool")
+addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.getMergeTruthJetsFilterTool"    , "MergeTruthJetsFilterTool")
 
 addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.getTimingObjRange"              , "TimingObjRange")
 addTool("MCTruthSimAlgs.MCTruthSimAlgsConfig.MergeRecoTimingObjTool"         , "MergeRecoTimingObjTool")
