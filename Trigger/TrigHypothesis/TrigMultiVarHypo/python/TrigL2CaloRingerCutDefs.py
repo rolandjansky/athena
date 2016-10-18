6ecd34841f369f1735974f9ed51e80824e925703
Joao Victor Da Fonseca Pinto <joao.victor.da.fonseca.pinto@cern.ch>
2016-10-18 15:18:00 +0200

    fix first et bin neural network configuration (TrigMultiVarHypo-07-00-21)
    	* Set threshold to 3 GeV below HLT threshold in the neural
    	* network configuration. Fix he first et bin [15,20] to [0,20]
    	* TrigMultiVarHypo-07-00-21
    
    2016-10-18 Ryan White <ryan.white@cern.ch>
    	* Use trigType (string) passed from menu dict
    	* Set threshold to 3 GeV below HLT threshold
    	* TrigMultiVarHypo-07-00-19 (tag from Joao) new tune
    	* TrigMultiVarHypo-07-00-20
    
    2016-09-27 Joao Victor Pinto <jodafons@cern.ch>
    	* New ringer tuning with label 201609
    	* Attend all chains for electron menu with
    	* high energy (Et > 15GeV). Only tight and medium
    	* configuration available on this tuning.
    	* Tagging TrigMultiVarHypo-07-00-18
    
    
    Former-commit-id: ac7d18257316e4f4efefcdcce953e6c21af16bcd

--- Trigger/TrigHypothesis/TrigMultiVarHypo/python/TrigL2CaloRingerCutDefs.py
+++ Trigger/TrigHypothesis/TrigMultiVarHypo/python/TrigL2CaloRingerCutDefs.py
@@ -4,16 +4,14 @@
 class TrigL2CaloRingerCutDefs():
 
   _signatureDict = {
-      #'benchmark' : ['pd','sp','pf'],
-      'chainName' : '',
-      #L2+EF (Calo) combined. ringers is set like T2Calo benchmark
-      #EFCalo: using only ringer calo like EFCalo benchmark step
-      'caloInfo'  : ['L2EFCalo','EFCalo'],
       'signature' : ['e','g'],
-      'threshold' : 0,
   }
+  _pidMap = {
+      'tight': 'Tight', 'medium':'Medium', 'loose':'Loose', 'vloose':'VeryLoose',
+      'lhtight': 'Tight', 'lhmedium': 'Medium', 'lhloose':'Loose', 'lhvloose': 'VeryLoose'
+      }
 
-  def __init__(self, threshold, IDinfo, chainName):
+  def __init__(self, threshold, IDinfo, trigType):
     self._chainPart           = dict()
     self.Nodes                = list()
     self.Weights              = list()
@@ -28,62 +26,49 @@ class TrigL2CaloRingerCutDefs():
     from AthenaCommon.Logging import logging
     logging.getLogger().info("Importing %s",__name__)
     logger = logging.getLogger("TrigMultiVarHypo.TrigL2CaloRingerCutDefs")
-    logger.info(('CutDefs for chain %s')%(chainName))
+    logger.info(('CutDefs for chain %s')%(trigType))
    
     #local variable
     discrs = None
 
-    self._chainPart['chainName'] = chainName
-    self._chainPart['IDinfo']    = IDinfo
-    #self._chainPart['benchmark'] = 'pd'
-    self._chainPart['caloInfo']  = 'EFCalo'
-
-    from TrigMultiVarHypo.TrigL2CaloRingerConstants import SignaturesMap
-    self._signatures = SignaturesMap()
-
-    if chainName[0] in self._signatureDict['signature']:
-      self._chainPart['signature']=chainName[0]
-    else:
+    if not (trigType[0] in self._signatureDict['signature']):
       raise RuntimeError('Bad signature')
 
-    for partName in chainName.split('_'):
+    # Select the correct threshold.
+    if (float(threshold) < 15): 
+      if self._signatureDict['signature'][0] in trigType:
+        # ElectronLowEnergyTightConf
+        # ElectronLowEnergyMediumConf
+        # ElectronLowEnergyLooseConf
+        # ElectronLowEnergyVeryLooseConf
+        key = ('ElectronLowEnergy%sConf')%(IDinfo)
+      else:
+        # PhotonLowEnergyTightConf
+        # PhotonLowEnergyMediumConf
+        # PhotonLowEnergyLooseConf
+        # PhotonLowEnergyVeryLooseConf
+        key = ('PhotonLowEnergy%sConf')%(IDinfo)
+    else: #(float(threshold) >= 20):
+      if self._signatureDict['signature'][0] in trigType:
+        # ElectronHighEnergyTightConf
+        # ElectronHighEnergyMediumConf
+        # ElectronHighEnergyLooseConf
+        # ElectronHighEnergyVeryLooseConf
+        key = ('ElectronHighEnergy%sConf')%(self._pidMap[IDinfo])
+      else:
+        # PhotonHighEnergyTightConf
+        # PhotonHighEnergyMediumConf
+        # PhotonHighEnergyLooseConf
+        # PhotonHighEnergyVeryLooseConf
+        key = ('PhotonHighEnergy%sConf')%(self._pidMap[IDinfo])
 
-      #default is Probabillity detection (PD) reference
-      #if partName in self._signatureDict['benchmark']:
-      #  self._chainPart['benchmark']=partName
-      
-      #default is EFCalo benchmark level    
-      if partName in self._signatureDict['caloInfo']:
-        self._chainPart['caloInfo']=partName
 
-    #Select the correct threshold.
-    if (float(threshold) < 10): 
-      self._chainPart['threshold'] = 0
-    elif(float(threshold) >= 10. and float(threshold) < 17):
-      self._chainPart['threshold'] = 10
-    elif(float(threshold) >= 17. and float(threshold) < 24):
-      self._chainPart['threshold'] = 17
-    elif(float(threshold) >= 24. and float(threshold) < 30):
-      self._chainPart['threshold'] = 24
-    elif(float(threshold) >= 30 and float(threshold) < 60):
-      self._chainPart['threshold'] = 30
-    elif(float(threshold) >= 60):
-      self._chainPart['threshold'] = 60
-    
-    logger.info(('set discriminator for  %dGeV because the threshold is %d.')%(self._chainPart['threshold'],
-                                                                               float(threshold)))
- 
-    key = ('%s%d_%s_%s')%(  self._chainPart['signature'],
-                               self._chainPart['threshold'],
-                               self._chainPart['IDinfo'],
-                               self._chainPart['caloInfo'],
-                               #self._chainPart['benchmark'],
-                               )
 
     try:  
       #inport all the discriminators
-      logger.info('discriminator: %s -> key: %s',self._chainPart['chainName'],key)
-      discrs =  self._signatures[key] 
+      from TrigMultiVarHypo.TrigL2CaloRingerConstants import SignaturesMap
+      logger.info('discriminator: %s -> key: %s', trigType, key)
+      discrs =  SignaturesMap()[key]
     except:
       logger.info(('INCORRECT key info: [%s] not configured.')%(key) )
     
@@ -92,13 +77,10 @@ class TrigL2CaloRingerCutDefs():
       #This is a dep region hypothesis: row is Et bin and col is eta bin
       from TrigMultiVarHypo.TrigRingerPreprocessorDefs import Norm1
 
-      ppList =     [  Norm1(),Norm1(), Norm1(), Norm1(),
-                      Norm1(),Norm1(), Norm1(), Norm1(),
-                      Norm1(),Norm1(), Norm1(), Norm1() ]
-      
-      discrList  = [ discrs['eta0_et0'], discrs['eta1_et0'], discrs['eta2_et0'], discrs['eta3_et0'],
-                     discrs['eta0_et1'], discrs['eta1_et1'], discrs['eta2_et1'], discrs['eta3_et1'],
-                     discrs['eta0_et2'], discrs['eta1_et2'], discrs['eta2_et2'], discrs['eta3_et2'],]
+      ppList=list();  discrList=list()
+      for key_, o in discrs.iteritems():
+        ppList.append( Norm1() )
+        discrList.append( o )
       
       logger.info('parser discrimiator with key: [%s]',key)
       #prepare the parser to athena phase
@@ -108,13 +90,12 @@ class TrigL2CaloRingerCutDefs():
   def __parser(self, discrList, ppList):
 
     for discr in discrList:
-      self.Nodes += discr['nodes'] 
-      self.Thresholds.append(   discr['threshold']   )
-      #list of lists to std::vector<std::vector<double>>
-      self.Weights.append(     discr['weights']      )
-      self.Bias.append(        discr['bias']         )
-      self.EtBins.append(      discr['etBin']        )
-      self.EtaBins.append(     discr['etaBin']       )
+      self.Nodes += discr['discriminator']['nodes'] 
+      self.Thresholds.append(  discr['discriminator']['threshold']   )
+      self.Weights.append(     discr['discriminator']['weights']      )
+      self.Bias.append(        discr['discriminator']['bias']         )
+      self.EtBins.append(      discr['configuration']['etBin']        )
+      self.EtaBins.append(     discr['configuration']['etaBin']       )
 
     for pp in ppList:
       #Prepoc configuration
