1fc1d3ca20d3c71a1dd0060fc545b22213ab50bc
John Baines <john.baines@stfc.ac.uk>
2016-10-20 09:37:48 +0200

    add getFastVertexCacheName method (FTK_DataProviderInterfaces-00-00-10)
    	* FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h: add getFastVertexCacheName method
    		* Tag as FTK_DataProviderInterfaces-00-00-10
    
    2016-09-21  John Baines  <john.baines@stfc.ac.uk>
    
    	* FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h:  Add getFastVertices
    	* Tag as FTK_DataProviderInterfaces-00-00-09
    2016-08-03  John Baines  <john.baines@stfc.ac.uk>
    
    	* FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h: add methods to get cache names
    	* Tag as FTK_DataProviderInterfaces-00-00-08
    
    2016-05-11 Jahred Adelman <jahreda@gmail.com>
    
    	* FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h: Remove VxContainer references
    	* Tag as FTK_DataProviderInterfaces-00-00-07
    
    
    Former-commit-id: 09334f2f7ec29f6a49f56ae2b5d78adc91a0eef6

--- Trigger/TrigFTK/FTK_DataProviderInterfaces/FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h
+++ Trigger/TrigFTK/FTK_DataProviderInterfaces/FTK_DataProviderInterfaces/IFTK_DataProviderSvc.h
@@ -11,11 +11,8 @@
 #include "AthenaKernel/IOVSvcDefs.h"
 #include "TrkTrack/Track.h"
 #include "TrkTrack/TrackCollection.h"
-//#include "VxVertex/VxContainer.h"
-//#include "VxVertex/VxCandidate.h"
 #include "TrigFTK_RawData/FTK_RawTrack.h"
 #include "IRegionSelector/IRoiDescriptor.h"
-class VxContainer;
 #include "xAODTracking/VertexFwd.h"
 #include "xAODTracking/TrackParticleFwd.h"
 #include "xAODTracking/VertexContainerFwd.h"
@@ -33,7 +30,7 @@ namespace Trk {
 class IRoiDescriptor;
 
 namespace ftk{
-    enum FTK_TrackType{RawTrackType, ConvertedTrackType, RefittedTrackType};
+    enum FTK_TrackType{RawTrack, ConvertedTrack, RefittedTrack};
 }
 
 static const InterfaceID IID_IFTK_DataProviderSvc("IFTK_DataProviderSvc",1,0);
@@ -51,13 +48,22 @@ public:
   virtual xAOD::TrackParticleContainer* getTrackParticles(const bool withRefit) = 0;
   virtual xAOD::TrackParticleContainer* getTrackParticlesInRoi(const IRoiDescriptor&, const bool withRefit) = 0;
 
-  virtual VxContainer* getVxContainer(const ftk::FTK_TrackType)=0;
+  virtual xAOD::VertexContainer* getFastVertices(const ftk::FTK_TrackType trackType=ftk::RawTrack) =0;
 
   // methods to create and return a view container. DataProviderSvc retains ownership 
   virtual xAOD::VertexContainer* getVertexContainer(const bool withRefit)=0;
 
   // method to fill container. Ownership passes to caller 
   virtual StatusCode getVertexContainer(xAOD::VertexContainer* vertex, const bool withRefit)=0;
+
+  virtual std::string getTrackParticleCacheName(const bool withRefit)=0;
+
+  virtual std::string getTrackCacheName(const bool withRefit)=0;
+
+  virtual std::string getVertexCacheName(const bool withRefit)=0;
+
+  virtual std::string getFastVertexCacheName(const bool withRefit)=0;
+
 };
 
 #endif
