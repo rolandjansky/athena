2e384b35dc59c420039da63bee20cc858df483f8
Steven Randolph Schramm <Steven.Schramm@cern.ch>
2016-10-21 18:55:33 +0200

    Merge trimming branch back intro trunk, add support for LC subjesIS calibration (TrigHLTJetRec-00-00-54)
    	* TrigHLTJetRec-00-00-54
    	* Merging TrigHLTJetRec-00-00-52-branch back into trunk
    	* See log below for trimming changes
    	* python/TrigHLTJetRecConfig.py: Finalized subjesIS calibrations (now
    	LC is supported in addition to EM)
    
    2016-10-16 Steven Schramm
    	* TrigHLTJetRec-00-00-52-02
    	* TrigHLTJetRec-00-00-52-branch
    	* See ATR-14738 for details
    	* python/TrigHLTJetRecConfig.py: Fixed trimming usage (major change)
    	* src/TrigJetFromPseudojet.cxx: Wraps offline class for avoiding SG access
    	* src/components/TrigHLTJetRec_entries.cxx: Added new classes
    	* src/TrigHLTJetRecFromCluster.cxx: Removed old trimming approach
    	* src/TrigHLTJetRecGroomer.cxx: New location for trimming fex
    	* TrigHLTJetRec/TrigHLTJetRecFromCluster.h: Removed old trimming approach
    	* TrigHLTJetRec/TrigHLTJetRecFromJet.h: Added missing protected classifier
    	* TrigHLTJetRec/TrigHLTJetRecBase.h: Made relevant methods virtual
    	* TrigHLTJetRec/TrigHLTJetRecGroomer.h: New location for trimming fex
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 44dee4686353cf794889f0735f1d9729fa17ebea

--- Trigger/TrigAlgorithms/TrigHLTJetRec/TrigHLTJetRec/TrigHLTJetRecFromJet.h
+++ Trigger/TrigAlgorithms/TrigHLTJetRec/TrigHLTJetRec/TrigHLTJetRecFromJet.h
@@ -14,7 +14,7 @@ class TrigHLTJetRecFromJet: public TrigHLTJetRecBase<xAOD::JetContainer>{
   TrigHLTJetRecFromJet(const std::string & name, ISvcLocator* pSvcLocator);
   ~TrigHLTJetRecFromJet();
 
-
+ protected:
   const xAOD::JetContainer* build() const override;
 
 }; 
