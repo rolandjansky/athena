ca8a28773a41fe29dd3cc976676f7e7a2ee579b9
Stewart Martin-Haugh <Stewart.Martin-Haugh@cern.ch>
2016-10-25 08:06:20 +0200

    'Fix CMakeLists.txt, remove unnecessary tdaq-common dependency' (TrigTimeMonitor-00-01-01)
    2016-10-25  Stewart Martin-Haugh  <smh@cern.ch>
    	* Fix CMakeLists.txt
    	* remove unnecessary tdaq-common dependency
    	* TrigTimeMonitor-00-01-01
    2016-10-07  Frank Winklmeier  <fwinkl@cern>
    	* Code cleanup and modernization
    	* Convert to component library
    	* TrigTimeHistTool: add option for per-LB (TProfile) timing histograms (ATR-15179)
    	* TrigTimeMonitor-00-01-00
            * fixed mem leak
    
    
    Former-commit-id: ca7b066ff4f76690a403350e3dc818f34a285496

--- Trigger/TrigMonitoring/TrigTimeMonitor/src/TrigTimeTreeTool.cxx
+++ Trigger/TrigMonitoring/TrigTimeMonitor/src/TrigTimeTreeTool.cxx
@@ -2,7 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/Algorithm.h"
 #include "GaudiKernel/ThreadGaudi.h"
 
@@ -13,7 +12,6 @@
 #include "TrigTimeMonitor/TrigTimeTreeTool.h"
 
 #include <algorithm>
-#include <vector>
 #include <TTree.h>
 
 TrigTimeTreeTool::TrigTimeTreeTool(const std::string & type, const std::string & name,
@@ -40,110 +38,95 @@ StatusCode TrigTimeTreeTool::initialize() {
 }
 
 StatusCode TrigTimeTreeTool::bookHists() {
-    MsgStream log(msgSvc(), name());
 
-    // Cleanup before re-booking
-    m_entries.clear();
+  // Cleanup before re-booking
+  m_entries.clear();
   
-    ITrigTimerSvc* m_timerSvc = 0;
-    if (!service("TrigTimerSvc", m_timerSvc).isSuccess()) {
-	log << MSG::ERROR << "Unable to locate TrigTimerSvc" << endreq;
-	return StatusCode::FAILURE;
-    }
+  ServiceHandle<ITrigTimerSvc> timerSvc("TrigTimerSvc",name());  
+  ATH_CHECK(timerSvc.retrieve());
+  ServiceHandle<ITHistSvc> rootHistSvc("THistSvc",name());  
+  ATH_CHECK(rootHistSvc.retrieve());
 
-    ITHistSvc *rootHistSvc;
-    if (!service("THistSvc", rootHistSvc).isSuccess()) {
-	log << MSG::ERROR << "Unable to locate THistSvc" << endreq;
-	return StatusCode::FAILURE;
-    }
-
-
-    // find out for whom we are running (i.e. Algo)    
-    const Algorithm *parentAlg = dynamic_cast<const Algorithm*>(parent());
-    if ( parentAlg ) 
+  // find out for whom we are running (i.e. Algo)    
+  const Algorithm *parentAlg = dynamic_cast<const Algorithm*>(parent());
+  if ( parentAlg ) 
 	m_parentName = getGaudiThreadGenericName(parentAlg->name());
 
-
-
-    std::string treeName = m_path + m_parentName;
-    // try to get global tree  make my own tree if can not get global
-    if ( rootHistSvc->getTree(m_path, m_tree).isFailure() ) {
+  std::string treeName = m_path + m_parentName;
+  // try to get global tree  make my own tree if can not get global
+  if ( rootHistSvc->getTree(m_path, m_tree).isFailure() ) {
 	
 	m_tree = new TTree ( m_parentName.c_str(), "timing ntuple");    
 	m_localTree = true;
-
-	if ( rootHistSvc->regTree(treeName,
-				  m_tree).isFailure() ) {
-	    log << MSG::WARNING << "Unable to register TTree" << endreq;
+    
+	if ( rootHistSvc->regTree(treeName, m_tree).isFailure() ) {
+      ATH_MSG_WARNING("Unable to register TTree");
 	}
 
-    } else {
-	log << MSG::DEBUG << "Attached info to global tree" << endreq;	
+  } else {     
+    ATH_MSG_DEBUG("Attached info to global tree");
 	m_localTree = false;
-    }
-
-
-    // loop over all the timers and make a mediating 
-    // object beetween them and ntuple
-    // namely TTreeEntry
-    std::vector < TrigTimer* >::iterator tIter;
-    for ( tIter = m_timerSvc->begin(); tIter != m_timerSvc->end(); tIter++ ){
+  }
+  
+  
+  // loop over all the timers and make a mediating 
+  // object beetween them and ntuple
+  // namely TTreeEntry
+  std::vector < TrigTimer* >::iterator tIter;
+  for ( tIter = timerSvc->begin(); tIter != timerSvc->end(); ++tIter ){
 	if ( selected((*tIter)->name()) ) {
-	    
-	    TrigTimeTreeEntry *etemp = new TrigTimeTreeEntry(*tIter, m_doStamps);
-	    m_entries.push_back(etemp);
-
-	    // book it in n-tuple
-	    if ( etemp->addToTree(m_tree, m_branches).isFailure() ) {
-		log << MSG::WARNING << "Unable to add the timomg info branch " 
-		    << (*tIter)->name()
-		    << "to the tree" << endreq;
-	    }
+      
+      TrigTimeTreeEntry *etemp = new TrigTimeTreeEntry(*tIter, m_doStamps);
+      m_entries.push_back(etemp);
+      
+      // book it in n-tuple
+      if ( etemp->addToTree(m_tree, m_branches).isFailure() ) {
+		ATH_MSG_WARNING("Unable to add the timomg info branch " 
+                        << (*tIter)->name() << "to the tree");
+      }
 	}
-    }
-    return  StatusCode::SUCCESS;
+  }
+  return  StatusCode::SUCCESS;
 }
 
 StatusCode TrigTimeTreeTool::fillHists() {
-    MsgStream log(msgSvc(), name());
-    log << MSG::DEBUG << " execHLTAlgorithm" << endreq;
 
-    std::vector<TrigTimeTreeEntry *>::iterator teIter;
-    for (  teIter = m_entries.begin(); teIter != m_entries.end(); ++teIter ) {
+  ATH_MSG_DEBUG(" execHLTAlgorithm");
+  
+  std::vector<TrigTimeTreeEntry *>::iterator teIter;
+  for (  teIter = m_entries.begin(); teIter != m_entries.end(); ++teIter ) {
 	(*teIter)->fill();
-    }
-
-    if ( m_localTree )
+  }
+  
+  if ( m_localTree )
 	m_tree->Fill();
     
-
-    return  StatusCode::SUCCESS;
+  return  StatusCode::SUCCESS;
 }
 
 StatusCode TrigTimeTreeTool::finalHists() {
-    return  StatusCode::SUCCESS;
+  return  StatusCode::SUCCESS;
 }
 
 
-
 bool TrigTimeTreeTool::selected(const std::string& name) {
-    if (m_key.empty()) {
+  if (m_key.empty()) {
 	// no specific Keys are selected ... record
 	// then all the timers which parent algo registered
 	if ( ! m_parentName.empty() ) {
-	    if ( name.find(m_parentName) == 0 ) // name starts from algo name
+      if ( name.find(m_parentName) == 0 ) // name starts from algo name
 		return true;
 	}
 	return false;
-    }
-
-    // exact match (maybe consider regex here)
-    std::vector<std::string>::const_iterator key 
+  }
+  
+  // exact match (maybe consider regex here)
+  std::vector<std::string>::const_iterator key 
 	= find(m_key.begin(), m_key.end(), name);
-    
-    if ( key != m_key.end() )
+  
+  if ( key != m_key.end() )
 	return true;
-    return false;
+  return false;
 }
 
 
