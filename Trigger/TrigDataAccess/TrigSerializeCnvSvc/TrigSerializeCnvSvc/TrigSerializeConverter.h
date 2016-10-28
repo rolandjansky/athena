8d345eb0d63a3ce504b58dd0d434411f2fb1ba91
Scott Snyder <scott.snyder@cern.ch>
2016-10-28 20:49:50 +0200

    Update for compatibility with AthContainers-00-03-08. (TrigSerializeCnvSvc-00-00-46)
    	* Tagging TrigSerializeCnvSvc-00-00-46.
    	* TrigSerializeCnvSvc/TrigSerializeConverter.h: Update for
    	compatibility with AthContainers-00-03-08.
    
    2016-10-12  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrigSerializeCnvSvc-00-00-45.
    	* TrigSerializeCnvSvc/TrigSerializeConverter.h,
    	src/TrigSerializeConvHelper.cxx: Additions to handle serializing
    	ViewVector objects to bytestream.
    
    2016-07-29  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrigSerializeCnvSvc-00-00-44.
    	* endreq -> endmsg.
    
    
    Former-commit-id: 5d664989e2950e22c972e28657f3a37d603f45d0

--- Trigger/TrigDataAccess/TrigSerializeCnvSvc/TrigSerializeCnvSvc/TrigSerializeConverter.h
+++ Trigger/TrigDataAccess/TrigSerializeCnvSvc/TrigSerializeCnvSvc/TrigSerializeConverter.h
@@ -21,6 +21,8 @@
 #include "TrigSerializeCnvSvc/TrigSerializeCnvSvc.h"
 
 #include "AthContainers/normalizedTypeinfoName.h"
+#include "AthContainers/ViewVector.h"
+#include "CxxUtils/make_unique.h"
 
 #include <memory>
 
@@ -55,6 +57,84 @@ template<typename T>
   static const bool value = isxAODVector<T>(0) || isAuxVector<T>(0);
 };
 
+
+namespace TrigSerialization {
+
+
+/**
+ * @brief Called before serializing an object to BS.
+ * @param d The object being serialized.
+ * @param holder A @c unique_ptr to hold a newly-created object.
+ *
+ * This generic version simply returns the input object unaltered.
+ */
+template <class DATA>
+DATA* prepareForWrite (DATA* d, std::unique_ptr<DATA>& /*holder*/)
+{
+  return d;
+}
+
+
+/**
+ * @brief Called before serializing an object to BS.
+ * @param d The object being serialized.
+ * @param holder A @c unique_ptr to hold a newly-created object.
+ *
+ * This version is specialized for @c ViewVector.
+ * A copy of the object is made, and @c setClearOnPersistent called on the copy.
+ * The copy is returned, as well as being assigned to the @c unique_ptr
+ * so that it will get deleted after serialization.
+ */
+template <class DV>
+ViewVector<DV>* prepareForWrite (ViewVector<DV>* d,
+                                 std::unique_ptr<ViewVector<DV> >& holder)
+{
+  holder = CxxUtils::make_unique<ViewVector<DV> > (*d);
+  holder->setClearOnPersistent();
+  return holder.get();
+}
+
+
+/**
+ * @brief Called after an object has been read from BS.
+ * @param d The object read.
+ *
+ * This generic version simply returns the input object unaltered.
+ */
+template <class DATA>
+DATA* finishRead (DATA* d)
+{
+  return d;
+}
+
+
+/**
+ * @brief Called after an object has been read from BS.
+ * @param d The object read.
+ *
+ * This version is specialized for @c ViewVector.
+ * It handles schema evolution and clears the persistent data.
+ */
+template <class DV>
+ViewVector<DV>* finishRead (ViewVector<DV>* d)
+{
+  // In the case of schema evolution, we'll actually get a
+  // @c ViewVector for a different _v type.
+  if (typeid(*d) != typeid(ViewVector<DV>)) {
+    auto d2 = std::make_unique<ViewVector<DV> > (*d);
+    delete d;
+    d = d2.release();
+    // root read rule doesn't do anything in this case.
+    d->toTransient();
+  }
+  d->clearPersistent();
+  return d;
+}
+
+
+} // namespace TrigSerialization
+
+
 template< typename DATA >
 class TrigSerializeConverter : public Converter {
 
@@ -71,21 +151,22 @@ public:
 
       void *ptr( 0 );
       if( m_log->level() <= MSG::DEBUG ) {
-	*m_log << MSG::DEBUG << "In createObj for : " << clname << " normalized to " << normalized << endreq;
+	*m_log << MSG::DEBUG << "In createObj for : " << clname << " normalized to " << normalized << endmsg;
       }
 
       StatusCode sc = m_convHelper->createObj( normalized, iAddr, ptr, typeIsxAOD);
       if( sc.isFailure() ){
          *m_log << MSG::WARNING << "m_convHelper->createObj failed for "
-                << clname << endreq;
+                << clname << endmsg;
       }
 
       DATA *nObj = ( DATA* ) ptr;
+      nObj = TrigSerialization::finishRead (nObj);
       pO = SG::asStorable( nObj );
 
       if( m_log->level() <= MSG::DEBUG ) {
          *m_log << MSG::DEBUG << "IOpaq: " << iAddr 
-                << " created nObj: " << nObj << endreq;
+                << " created nObj: " << nObj << endmsg;
       }
 
       
@@ -102,16 +183,16 @@ public:
             }
             if( sc.isFailure() ) {
                *m_log << MSG::ERROR << "SG::record failed for " << addr->sgkey()
-                      <<  endreq;
+                      <<  endmsg;
             } else if( m_log->level() <= MSG::DEBUG ) {
                *m_log << MSG::DEBUG << "SG::record key: "
-                      << addr->sgkey() << " class: " << clname << endreq;
+                      << addr->sgkey() << " class: " << clname << endmsg;
             }
          } else {
-            *m_log << MSG::WARNING << "createObj cast failed" << endreq;
+            *m_log << MSG::WARNING << "createObj cast failed" << endmsg;
          }
       } else if( m_log->level() <= MSG::DEBUG ) {
-         *m_log << MSG::DEBUG << "did not put an object into SG" << endreq;
+         *m_log << MSG::DEBUG << "did not put an object into SG" << endmsg;
       }
 
       return sc;
@@ -130,15 +211,17 @@ public:
       SG::fromStorable( pObj, d );
       if( m_log->level() <= MSG::DEBUG ) {
          *m_log << MSG::DEBUG << "My createRep for " << classID() << " "
-                << classname << endreq;
+                << classname << endmsg;
       }
-      void *serptr = d;
+
+      std::unique_ptr<DATA> holder;
+      void *serptr = TrigSerialization::prepareForWrite (d, holder);
       //
       std::vector< uint32_t > ser;
 
       StatusCode sc = m_convHelper->createRep( classname, serptr, ser, typeIsxAOD);
       if( m_log->level() <= MSG::DEBUG ) {
-         *m_log << MSG::DEBUG << "convHelper " << ser.size() << endreq;
+         *m_log << MSG::DEBUG << "convHelper " << ser.size() << endmsg;
       }
 
       TrigStreamAddress *addr = new TrigStreamAddress( classID(), classname, "" );
@@ -160,24 +243,24 @@ public:
       StatusCode sc = m_convHelper.retrieve();
       if( ! sc.isSuccess() ) {
          if( m_log->level() <= MSG::DEBUG ) {
-            *m_log << MSG::DEBUG << m_convHelper << "not retrieved" << endreq;
+            *m_log << MSG::DEBUG << m_convHelper << "not retrieved" << endmsg;
          }
       } else {
          if( m_log->level() <= MSG::DEBUG ) {
             *m_log << MSG::DEBUG << m_convHelper << "m_serializer retrieved"
-                   << endreq;
+                   << endmsg;
          }
          if( ! m_convHelper->initialize().isSuccess() ) {
             if( m_log->level() <= MSG::DEBUG ) {
                *m_log << MSG::DEBUG << m_convHelper << " not initialized"
-                      << endreq;
+                      << endmsg;
             }
          }
       }
 
       StatusCode scsg = m_sgHandle.retrieve();
       if (scsg.isFailure())
-         *m_log << MSG::ERROR << "cannot access SG" << endreq;
+         *m_log << MSG::ERROR << "cannot access SG" << endmsg;
       else {
          m_sgsvc = m_sgHandle.operator->();
       }
