9d8e8c26d5693a8b945f242828fd309cd00a0618
John Baines <john.baines@stfc.ac.uk>
2016-11-02 15:23:36 +0100

    put back TrkTrack in requirements and CMakeLists (FTK_RecToolInterfaces-00-00-06)
    	* cmt/requirements: put back use TrkTrack
    	* tag FTK_RecToolInterfaces-00-00-06
    
    2016-09-01  Jahred Adelman <jahreda@gmail.com>
    
    	* IFTK_VertexFinderTool.h: add missing include
    
    2016-08-29  Jahred Adelman <jahreda@gmail.com>
    
    	* IFTK_VertexFinderTool.h: update for multiple types of tracks
    
    
    Former-commit-id: 2c01c739a850804282babcc1ff46a435ed2347f6

--- Trigger/TrigFTK/FTK_RecToolInterfaces/FTK_RecToolInterfaces/IFTK_VertexFinderTool.h
+++ Trigger/TrigFTK/FTK_RecToolInterfaces/FTK_RecToolInterfaces/IFTK_VertexFinderTool.h
@@ -7,15 +7,20 @@
 #ifndef __IFTK_VERTEXFINDER_TOOL_H__
 #define __IFTK_VERTEXFINDER_TOOL_H__
 
-#include "GaudiKernel/IAlgTool.h"
+#include "GaudiKernel/IAlgTool.h"	
+//#include "AthenaKernel/IOVSvcDefs.h"
 class VxContainer;
 #include "TrigFTK_RawData/FTK_RawTrack.h"
 #include "TrigFTK_RawData/FTK_RawTrackContainer.h"
-namespace Trk {
-  class Track;
-}
-
-
+#include "xAODTracking/VertexFwd.h"
+//#include "xAODTracking/TrackParticleFwd.h"
+#include "xAODTracking/VertexContainerFwd.h"
+#include "xAODTracking/VertexAuxContainer.h"
+#include "TrkTrack/TrackCollection.h"
+//#include "xAODTracking/TrackParticleContainerFwd.h"
+//namespace Trk {
+////  class Track;
+//}
 static const InterfaceID IID_IFTK_VertexFinderTool("IFTK_VertexFinderTool",1,0);
 
 class IFTK_VertexFinderTool : virtual public IAlgTool { 
@@ -25,8 +30,10 @@ class IFTK_VertexFinderTool : virtual public IAlgTool {
   static const InterfaceID& interfaceID ()   //!< the Tool's interface
   {  return IID_IFTK_VertexFinderTool; }  	
 
-  virtual VxContainer* findVertex(const FTK_RawTrackContainer* trks) = 0;
-  
+ // virtual VxContainer* findVertex(const FTK_RawTrackContainer* trks) = 0;
+  virtual std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> findVertex(const FTK_RawTrackContainer* trks) = 0;
+  virtual std::pair<xAOD::VertexContainer*, xAOD::VertexAuxContainer*> findVertex(const TrackCollection* trks) = 0;
+ private:
 };
 
 #endif
