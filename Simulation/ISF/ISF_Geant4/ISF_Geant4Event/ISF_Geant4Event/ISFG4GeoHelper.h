fdf42271d3d7d743d40fa3c5988c23fd87c08178
Andreas George Petridis <Andreas.Petridis@cern.ch>
2016-11-03 13:21:26 +0100

    Make the ISF truth service wraper work w/ and w/o Cavern volume, ATLASSIM-3000 (ISF_Geant4Event-00-00-05)
    Former-commit-id: 6df63a85716b6ff6a130e4ef7dc9098b350ee1b3

---
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Event/ISF_Geant4Event/ISFG4GeoHelper.h
@@ -0,0 +1,28 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef ISFG4GEOHELPER_H
+#define ISFG4GEOHELPER_H
+
+
+#include "AtlasDetDescr/AtlasRegion.h" 
+#include "G4Step.hh"
+#include "ISF_Interfaces/IGeoIDSvc.h"
+
+
+namespace iGeant4 {
+
+class ISFG4GeoHelper {
+
+ public:
+  ISFG4GeoHelper() = delete;
+
+  static AtlasDetDescr::AtlasRegion nextGeoId(const G4Step* aStep, int truthVolLevel,ISF::IGeoIDSvc *geoIDSvc);
+  static bool checkVolumeDepth(G4LogicalVolume* logicalVol, int volLevel, int depth=0);
+
+ private:
+};
+
+}
+#endif
