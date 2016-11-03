41036dc69e72632985d46be69e41f6f45dad1a5c
Krzysztof Wieslaw Wozniak <krzysztof.wozniak@ifj.edu.pl>
2016-11-03 13:59:49 +0100

    additional MinBias/UPC trigger chains to be monitored in p+Pb run (TrigMinBiasMonitoring-00-01-38)
            * additional MinBias/UPC trigger chains to be monitored in p+Pb run
    	* TrigMinBiasMonitoring-00-01-38
    
    2016-11-03 Iwona Grabowska-Bold
            * reference trigger switched to mb_sptrk for pPb run
            * TrigMinBiasMonitoring-00-01-37
    
    2016-10-07 Iwona Grabowska-Bold
            * updated for pPb run (ATR-15190)
            * TrigMinBiasMonitoring-00-01-36
    
    2016-08-26  scott snyder  <snyder@bnl.gov>
    
    	* Tagging TrigMinBiasMonitoring-00-01-35.
    	* endreq -> endmsg.

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitCategory.py
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigMinBiasMonitCategory.py
@@ -12,13 +12,31 @@ triggers_pp = ["mb_sptrk",
                "mb_sp1800_hmtperf_L1TE5",
                ]
 
-
+# PbPb triggers 2015
 triggers_hi = [ "mb_sp",
                 "mb_sptrk_ion_L1ZDC_A_C_VTE50",
                 "mb_perf_L1MBTS_1_1",	
                 "noalg_mb_L1TE50",
                 ]
 		
+# pPb triggers 2016
+triggers_pPb = ["mb_sptrk",
+               #"mb_sptrk_noisesup",
+               #"mb_sptrk_costr", 
+               "noalg_L1RD0_FILLED",
+               #"noalg_mb_L1RD0_EMPTY",
+               "noalg_mb_L1MBTS_2",
+               "mb_perf_L1MBTS_2",
+               "mb_mbts_L1MBTS_2",
+               "noalg_mb_L1ZDC_A",
+               "noalg_mb_L1ZDC_C",
+               "mb_sp1800_hmtperf_L1TE5",
+               "noalg_mb_L1TE5",
+               "hi_upc_FgapAC_mb_sptrk_L1MBTS_2",
+               "hi_upc_FgapAC_mb_sptrk_exclusiveloose",
+               "mb_sptrk_exclusiveloose_L1ZDC",
+               ]
+
 #!----NORMAL USER ACCESS-------
 		
 #----ADVANCED USER ACCESS-----
@@ -35,15 +53,23 @@ for x in triggers_hi:
     if x not in triggersFiltered_hi:
         triggersFiltered_hi.append(x)
 
+triggersFiltered_pPb = []
+for x in triggers_pPb:
+    if x not in triggersFiltered_pPb:
+        triggersFiltered_pPb.append(x)
+
 from RecExConfig.RecFlags import rec
 from AthenaCommon.BeamFlags import jobproperties
 from AthenaCommon.Logging        import logging
 
 log = logging.getLogger( 'TrigMinBiasMonitoring' )
 
-if rec.doHeavyIon == True or rec.doHIP == True:
+if rec.doHeavyIon == True:
     log.info('HI mode of min-bias trigger monitoring')
     monitoring_minbias = triggersFiltered_hi     
+elif rec.doHIP == True:
+    log.info('doHI mode of min-bias trigger monitoring')
+    monitoring_minbias = triggersFiltered_pPb
 else:
     log.info('pp mode of min-bias trigger monitoring')
     monitoring_minbias = triggersFiltered_pp
