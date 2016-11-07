3068a9b4a9a335e6aebd65e81c2fca090691753b
Attila Krasznahorkay <krasznaa@cern.ch>
2016-11-07 12:21:36 +0100

    Fixed the package's build in AthAnalysisBase with CMake (CPAnalysisExamples-00-00-70)
    Former-commit-id: e08ec7632bbb3fcec84560ff247e5c82f28e971b

---
+++ PhysicsAnalysis/AnalysisCommon/CPAnalysisExamples/CPAnalysisExamples/CPAnalysisExamplesDict.h
@@ -0,0 +1,11 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#ifndef CPANALYSISEXAMPLES_CPANALYSISEXAMPLESDICT_H
+#define CPANALYSISEXAMPLES_CPANALYSISEXAMPLESDICT_H
+
+#include "CPAnalysisExamples/MetadataToolExample.h"
+
+#endif
