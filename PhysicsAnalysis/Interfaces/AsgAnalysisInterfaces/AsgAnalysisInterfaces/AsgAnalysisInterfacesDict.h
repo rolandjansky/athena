d527052bc5304ce7722ef640cd7bd9247a011592
Will Buttinger <William.Buttinger@cern.ch>
2016-11-08 14:28:13 +0100

    'adding IObservableTool' (AsgAnalysisInterfaces-00-00-04)
    Former-commit-id: 4555271cbb1e1a11ea834b9389f02f4fe7a62241

---
+++ PhysicsAnalysis/Interfaces/AsgAnalysisInterfaces/AsgAnalysisInterfaces/AsgAnalysisInterfacesDict.h
@@ -0,0 +1,13 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef ASGANALYSISINTERFACESDICT_H
+#define ASGANALYSISINTERFACESDICT_H
+
+#include "AsgAnalysisInterfaces/IPileupReweightingTool.h"
+#include "AsgAnalysisInterfaces/IGoodRunsListSelectionTool.h"
+#include "AsgAnalysisInterfaces/IObservableTool.h"
+
+
+#endif
