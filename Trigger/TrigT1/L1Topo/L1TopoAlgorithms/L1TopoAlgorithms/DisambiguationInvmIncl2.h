4fb73c39ffa63f46868abe51472c1b5fb0513a26
Davide Gerbaudo <Davide.Gerbaudo@cern.ch>
2016-11-11 16:27:23 +0100

    tag 00-01-02 for ATR-15105 from L1TopoAlgorithms-00-00-69-branch @ 783757 (L1TopoAlgorithms-00-01-02)
    	* fix MuonSort_1BC to use LateMuonTOBArray -> MuonTOBArray (ATR-15105)
    	* tag L1TopoAlgorithms-00-01-02
    
    2016-11-07 Davide Gerbaudo <gerbaudo@cern.ch>
    	* rename DisambiguationInvariantMass2 --> DisambiguationInvmIncl2,
    	as requested in ATR-15105. Tag L1TopoAlgorithms-00-01-01 (on
    	L1TopoAlgorithms-00-00-69-branch).
    
    
    Former-commit-id: 5ae95e4905009d2bf1608b77ceee793535b7b00f

--- Trigger/TrigT1/L1Topo/L1TopoAlgorithms/L1TopoAlgorithms/DisambiguationInvmIncl2.h
+++ Trigger/TrigT1/L1Topo/L1TopoAlgorithms/L1TopoAlgorithms/DisambiguationInvmIncl2.h
@@ -1,20 +1,20 @@
-//  DisambiguationInvariantMass2.h
+//  DisambiguationInvmIncl2.h
 //  TopoCore
 //  Created by Davide Gerbaudo on 2016-10-10.
 //  Copyright (c) 2016 Davide Gerbaudo. All rights reserved.
 
-#ifndef __TopoCore__DisambiguationInvariantMass2__
-#define __TopoCore__DisambiguationInvariantMass2__
+#ifndef __TopoCore__DisambiguationInvmIncl2__
+#define __TopoCore__DisambiguationInvmIncl2__
 
 #include <iostream>
 #include "L1TopoInterfaces/DecisionAlg.h"
 
 namespace TCS {
 
-  class DisambiguationInvariantMass2 : public DecisionAlg {
+  class DisambiguationInvmIncl2 : public DecisionAlg {
   public:
-    DisambiguationInvariantMass2(const std::string & name);
-    virtual ~DisambiguationInvariantMass2();
+    DisambiguationInvmIncl2(const std::string & name);
+    virtual ~DisambiguationInvmIncl2();
     virtual StatusCode initialize();
     virtual StatusCode processBitCorrect(const std::vector<TCS::TOBArray const *> & input,
                                          const std::vector<TCS::TOBArray *> & output,
