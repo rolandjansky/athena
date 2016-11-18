34836ffb907f6fd39a07a5334e7288c116704a8d
Scott Snyder <scott.snyder@cern.ch>
2016-11-18 05:53:57 +0100

    Const fixes. (AnalysisTriggerAlgs-00-01-47)
    	* Tagging AnalysisTriggerAlgs-00-01-47.
    	* Const fixes.
    
    
    Former-commit-id: 39afa4d3edf496778fa39495610c7d0840b32acf

--- PhysicsAnalysis/AnalysisTrigger/AnalysisTriggerAlgs/src/RoIBResultToxAOD.cxx
+++ PhysicsAnalysis/AnalysisTrigger/AnalysisTriggerAlgs/src/RoIBResultToxAOD.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: RoIBResultToxAOD.cxx 743100 2016-04-26 21:57:39Z watsona $
+// $Id: RoIBResultToxAOD.cxx 784984 2016-11-18 05:53:57Z ssnyder $
 
 // STL include(s):
 #include <algorithm>
@@ -417,7 +417,7 @@ StatusCode RoIBResultToxAOD::addEmTauRoI( const ROIB::RoIBResult* result ) {
    // Tool to reconstruct EM/tau cluster & isolation sums
    //   - need to form tower map for RoI reconstruction
    const DataVector< xAOD::CPMTower >* storedTTs;
-   std::map< int, xAOD::CPMTower* > cpmtowers;
+   xAOD::CPMTowerMap_t cpmtowers;
    if( m_retrievedEmTauTool ) {
       if( evtStore()->contains< xAOD::CPMTowerContainer >( m_CPMTowerLocation ) ) {
          StatusCode sc = evtStore()->retrieve( storedTTs, m_CPMTowerLocation );
