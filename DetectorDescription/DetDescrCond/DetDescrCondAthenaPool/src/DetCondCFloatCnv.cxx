14f3cbb71dbd721b93ac0c03a109df4809816d18
Vakhtang Tsulaia <Vakhtang.Tsulaia@cern.ch>
2016-11-18 23:06:54 +0100

    Switching from endreq to endmsg (DetDescrCondAthenaPool-00-01-13)
    	* Switching from endreq to endmsg
    
    
    Former-commit-id: 413278ef0e2c8e849f6e00727308b417200d4a02

--- DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/DetCondCFloatCnv.cxx
+++ DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/DetCondCFloatCnv.cxx
@@ -18,28 +18,28 @@ DetCondCFloatCnv::DetCondCFloatCnv(ISvcLocator* svcloc)
 
 
 DetCondCFloat_PERS* DetCondCFloatCnv::createPersistent(DetCondCFloat* transObj) {
-    MsgStream log(messageService(), "DetCondCFloatCnv" ); 
+    MsgStream log(msgSvc(), "DetCondCFloatCnv" ); 
     DetCondCFloatCnv_p1 cnv;
     DetCondCFloat_PERS* persObj = cnv.createPersistent( transObj, log );
-    log << MSG::DEBUG << "DetCondCFloat write Success" << endreq;
-    if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv write p1" << endreq;
+    log << MSG::DEBUG << "DetCondCFloat write Success" << endmsg;
+    if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv write p1" << endmsg;
     return persObj; 
 }
 
 DetCondCFloat* DetCondCFloatCnv::createTransient() {
-    MsgStream log(messageService(), "DetCondCFloatCnv" );
+    MsgStream log(msgSvc(), "DetCondCFloatCnv" );
     static pool::Guid   p1_guid("7E19F4EE-540A-4FB0-AEED-0DC65C1DEFC6");
     static pool::Guid   p0_guid("DCE158E2-27A7-4A78-9778-D3E4D4F740CB");
     if( compareClassGuid(p1_guid) ) {
         // using auto_ptr ensures deletion of the persistent object
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv read p1" << endreq;
+        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv read p1" << endmsg;
         std::auto_ptr< DetCondCFloat_p1 > col_vect( poolReadObject< DetCondCFloat_p1 >() );
         DetCondCFloatCnv_p1 cnv;
         return cnv.createTransient( col_vect.get(), log );
     }
     else if( compareClassGuid(p0_guid) ) {
         // regular object from before TP separation, just return it
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv read p0" << endreq;
+        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetCondCFloatCnv read p0" << endmsg;
         return this->poolReadObject<DetCondCFloat>();
     } 
     throw std::runtime_error("Unsupported persistent version of DetCondCFloat");
--- DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/DetStatusMapCnv.cxx
+++ DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/DetStatusMapCnv.cxx
@@ -18,28 +18,28 @@ DetStatusMapCnv::DetStatusMapCnv(ISvcLocator* svcloc)
 
 
 DetStatusMap_PERS* DetStatusMapCnv::createPersistent(DetStatusMap* transObj) {
-    MsgStream log(messageService(), "DetStatusMapCnv" ); 
+    MsgStream log(msgSvc(), "DetStatusMapCnv" ); 
     DetStatusMapCnv_p1 cnv;
     DetStatusMap_PERS* persObj = cnv.createPersistent( transObj, log );
-    log << MSG::DEBUG << "DetStatusMap write Success" << endreq;
-    if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv write p1" << endreq;
+    log << MSG::DEBUG << "DetStatusMap write Success" << endmsg;
+    if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv write p1" << endmsg;
     return persObj; 
 }
 
 DetStatusMap* DetStatusMapCnv::createTransient() {
-    MsgStream log(messageService(), "DetStatusMapCnv" );
+    MsgStream log(msgSvc(), "DetStatusMapCnv" );
     static pool::Guid   p1_guid("AD99EE0F-DFD9-4734-8930-F5FC702C0BE8");
     static pool::Guid   p0_guid("CB1BFF00-64B1-4693-8035-19BB18867AF1");
     if( compareClassGuid(p1_guid) ) {
         // using auto_ptr ensures deletion of the persistent object
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv read p1" << endreq;
+        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv read p1" << endmsg;
         std::auto_ptr< DetStatusMap_p1 > col_vect( poolReadObject< DetStatusMap_p1 >() );
         DetStatusMapCnv_p1 cnv;
         return cnv.createTransient( col_vect.get(), log );
     }
     else if( compareClassGuid(p0_guid) ) {
         // regular object from before TP separation, just return it
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv read p0" << endreq;
+        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "DetStatusMapCnv read p0" << endmsg;
         return this->poolReadObject<DetStatusMap>();
     } 
     throw std::runtime_error("Unsupported persistent version of DetStatusMap");
