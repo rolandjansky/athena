14aeb764c755c229028e6b805330ce2ff83a2d10
Vakhtang Tsulaia <Vakhtang.Tsulaia@cern.ch>
2016-11-18 22:14:48 +0100

    Introducing IGeoDbTagSvc (GeoModelInterfaces-00-01-00)
    	* Introducing IGeoDbTagSvc
    
    
    Former-commit-id: 29dd24cc7c1a150f51221c3671567c978575c61a

---
+++ DetectorDescription/GeoModel/GeoModelInterfaces/GeoModelInterfaces/IGeoDbTagSvc.h
@@ -0,0 +1,61 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef GEOMODELINTERFACES_IGEODBTAGSVC_H
+#define GEOMODELINTERFACES_IGEODBTAGSVC_H
+
+// Include Files
+#include "GaudiKernel/IInterface.h"
+#include <string>
+
+namespace GeoModel {
+  enum GeoConfig {
+    GEO_RUN1,
+    GEO_RUN2,
+    GEO_RUN3,
+    GEO_RUN4,
+    GEO_ITk,
+    GEO_TESTBEAM
+  };
+}  
+
+static const InterfaceID IID_IGeoDbTagSvc("IGeoDbTagSvc", 1, 0);
+
+class IGeoDbTagSvc : virtual public IInterface {
+ public:
+  // Retrieve interface ID
+  static const InterfaceID& interfaceID() {return IID_IGeoDbTagSvc;}
+
+  virtual const std::string & atlasVersion()                    const =0;
+  virtual const std::string & inDetVersion()                    const =0;
+  virtual const std::string & pixelVersion()                    const =0;
+  virtual const std::string & SCT_Version()                     const =0;
+  virtual const std::string & TRT_Version()                     const =0;
+  virtual const std::string & LAr_Version()                     const =0;
+  virtual const std::string & tileVersion()                     const =0;
+  virtual const std::string & muonVersion()                     const =0;
+  virtual const std::string & caloVersion()                     const =0;
+  virtual const std::string & magFieldVersion()                 const =0;
+  virtual const std::string & cavernInfraVersion()              const =0;
+  virtual const std::string & forwardDetectorsVersion()         const =0;
+  
+  virtual const std::string & inDetVersionOverride()            const =0;
+  virtual const std::string & pixelVersionOverride()            const =0;
+  virtual const std::string & SCT_VersionOverride()             const =0;
+  virtual const std::string & TRT_VersionOverride()             const =0;
+  virtual const std::string & LAr_VersionOverride()             const =0;
+  virtual const std::string & tileVersionOverride()             const =0;
+  virtual const std::string & muonVersionOverride()             const =0;
+  virtual const std::string & caloVersionOverride()             const =0;
+  virtual const std::string & magFieldVersionOverride()         const =0;
+  virtual const std::string & cavernInfraVersionOverride()      const =0;
+  virtual const std::string & forwardDetectorsVersionOverride() const =0;
+  
+  virtual GeoModel::GeoConfig geoConfig() const = 0;
+};
+
+#endif // GEOMODELINTERFACES_IGEODBTAGSVC_H
+
+
+
