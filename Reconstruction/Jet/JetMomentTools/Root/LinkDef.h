da72f4104fab662fb3fef745dc087a185f63baf0
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2016-11-21 15:19:46 +0100

    'Expanded constit fourmom tool for detector position calculations' (JetMomentTools-00-03-54)
    	* Rewrote constit fourmom tool to calculate detector eta
    	* Tagging JetMomentTools-00-03-54
    
    2016-11-17  Matt Klein  <matthew.henry.klein@cern.ch>
    	* Root/JetVertexTaggerTool
    	  Downgraded WARNING to VERBOSE if no PV is found
    	* Tagging JetMomentTools-00-03-53
    
    2016-11-13  Matt Klein  <matthew.henry.klein@cern.ch>
    	* util/
    
    2016-11-07  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
    	* cmt/requirements fix typo in PFlowUtils dependency
    	* Tagging JetMomentTools-00-03-52
    
    2016-11-04 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Added an explicit include of "PFlowUtils/IWeightPFOTool.h" to
    	  JetWidthTool.h. Otherwise the component library was failing to
    	  build.
    	* After making PFlowUtils a dual-use library (PFlowUtils-00-00-15),
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 6dfad0f366f0ca505f2b148f849bafc36fd7b290

--- Reconstruction/Jet/JetMomentTools/Root/LinkDef.h
+++ Reconstruction/Jet/JetMomentTools/Root/LinkDef.h
@@ -2,6 +2,7 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
+#include "PFlowUtils/IWeightPFOTool.h"
 #include "JetMomentTools/JetClusterMomentsTool.h"
 #include "JetMomentTools/JetConstitFourMomTool.h"
 #include "JetMomentTools/JetECPSFractionTool.h"
