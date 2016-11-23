6353a6e4f20c1a65640a89fbbbe285ba23041188
Scott Snyder <scott.snyder@cern.ch>
2016-11-23 20:46:23 +0100

    clang warning: redundant struct keyword. (TrigTimeMonitor-00-01-02)
    2016-11-23  scott snyder  <snyder@bnl.gov>
    	* Tagging TrigTimeMonitor-00-01-02.
    	* src/TrigTimeHistTool.cxx: clang warning: redundant struct
    	keyword.
    
    2016-10-25  Stewart Martin-Haugh  <smh@cern.ch>
    	* Fix CMakeLists.txt
    	* remove unnecessary tdaq-common dependency
    	* TrigTimeMonitor-00-01-01
    
    2016-10-07  Frank Winklmeier  <fwinkl@cern>
    	* Code cleanup and modernization
    	* Convert to component library
    	* TrigTimeHistTool: add option for per-LB (TProfile) timing histograms (ATR-15179)
    	* TrigTimeMonitor-00-01-00
            * fixed mem leak
    
    
    Former-commit-id: 0b62db46e9d61b6477d9eb4467b4926e1c87068f

--- Trigger/TrigMonitoring/TrigTimeMonitor/src/TrigTimeHistTool.cxx
+++ Trigger/TrigMonitoring/TrigTimeMonitor/src/TrigTimeHistTool.cxx
@@ -2,34 +2,30 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "boost/foreach.hpp"
-#include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/Algorithm.h"
 #include "GaudiKernel/ThreadGaudi.h"
 
 #include "TrigTimeAlgs/ITrigTimerSvc.h"
 #include "TrigTimeAlgs/TrigTimer.h"
-
-//#include "AthenaMonitoring/IMonitorToolBase.h"
 #include "TrigTimeMonitor/TrigTimeHistTool.h"
 
+#include "boost/algorithm/string/erase.hpp"
 
 #include <vector>
 #include <algorithm>
 #include <utility>
 #include <TH1F.h>
 #include <TH2F.h>
-#include "boost/regex.hpp"
-
 
 TrigTimeHistTool::TrigTimeHistTool(const std::string & type, const std::string & name,
 				   const IInterface* parent) 
   :  TrigMonitorToolBase(type, name, parent), 
      m_path("TIMERS"),
      m_bins(50),
-     m_doPerObjHist(false), 
+     m_doPerObjHist(false),
+     m_doPerLBGroups(false),
      m_includeName(".+"),     // this matches all, by default all timers are histogrammed
-     m_excludeName("()")      // this matches only empty string so shoudl not block any timer
+     m_excludeName("()")      // this matches only empty string so should not block any timer
  {
   m_timerHistLimits.push_back(0.);
   m_timerHistLimits.push_back(200.);
@@ -37,52 +33,38 @@ TrigTimeHistTool::TrigTimeHistTool(const std::string & type, const std::string &
   m_timerPerObjHistLimits.push_back(200.);
 
   declareInterface<IMonitorToolBase>(this);
-  declareProperty("Key", m_key, "Exact names of timer to be monitored");
-  declareProperty("BookingDir", m_path="TIMERS", "Prefered booking path for histogram, if blank the algorithm name used" );
+  declareProperty("Key", m_key, "Regex macthing timer names, if not set only consider timers of parent");
+  declareProperty("BookingDir", m_path, "Prefered booking path for histogram, if blank the algorithm name used" );
   declareProperty("NumberOfHistBins", m_bins, "Number of bins");
   declareProperty("TimerHistLimits", m_timerHistLimits, "Limits of the histogram");
-  declareProperty("DoPerObjHist", m_doPerObjHist=false, "Do histogram time/(propery value) if value != 0");
+  declareProperty("DoPerObjHist", m_doPerObjHist, "Do histogram time/(propery value) if value != 0");
   declareProperty("TimerPerObjHistLimits", m_timerPerObjHistLimits, "Limits of histogram with time/property value");
-  declareProperty("GroupedTimers", m_regexGroups, "Regex matching timers to be joined in single histogram");
+  declareProperty("GroupedTimers", m_regexGroups, "Regex matching timers to be joined in single histogram (regex captures will be removed from bin label)");
+  declareProperty("DoPerLBGroups", m_doPerLBGroups, "Publish grouped histograms per LB as TProfile");
   declareProperty("IncludeName", m_includeName, "Regex matching timer names, if do not match then this timer is histogrammed (default accepts all).");
   declareProperty("ExcludeName", m_excludeName, "Regex matching timer names, if matches then this timer is not histogrammed (default matches nothing).");
   
 }
 
-
-TrigTimeHistTool::~TrigTimeHistTool(){
+TrigTimeHistTool::~TrigTimeHistTool()
+{
 }
 
-StatusCode TrigTimeHistTool::initialize(){
-  if ( TrigMonitorToolBase::initialize().isFailure() )
-    return StatusCode::FAILURE;
+StatusCode TrigTimeHistTool::initialize()
+{
+  ATH_CHECK(TrigMonitorToolBase::initialize());
   return StatusCode::SUCCESS;
 }
 
-StatusCode TrigTimeHistTool::bookHists(){
-  MsgStream log(msgSvc(), name());
-
+StatusCode TrigTimeHistTool::bookHists()
+{
   // Cleanup before re-booking
-  BOOST_FOREACH(struct TimerHist *t, m_entries){
-    delete t;
-  }
-
-
-
+  for (auto t : m_entries) delete t;
+  for (const auto& g :m_groupHists) g.second->m_timers.clear();
   m_entries.clear();
 
-  // More cleanup before re-booking (added 28.09.10 by MM)
-  for (std::map <std::string, struct TimerGroupHist *>::const_iterator g = m_groupHists.begin(); 
-       g != m_groupHists.end(); g++) {
-    (g->second)->m_timers.clear();
-  }
-
-  ITrigTimerSvc *m_timerSvc = 0;
-  if (!service("TrigTimerSvc", m_timerSvc).isSuccess()) {
-    log << MSG::ERROR << "Unable to locate TrigTimerSvc" << endreq;
-    return StatusCode::FAILURE;
-  }
-
+  ServiceHandle<ITrigTimerSvc> timerSvc("TrigTimerSvc",name());
+  ATH_CHECK(timerSvc.retrieve());
 
   // find out for whom we are running (i.e. Algo)    
   const Algorithm *parentAlg = dynamic_cast<const Algorithm*>(parent());
@@ -90,27 +72,23 @@ StatusCode TrigTimeHistTool::bookHists(){
     m_parentName = getGaudiThreadGenericName(parentAlg->name());
   
 
-
   std::string path = (m_path == "") ? m_parentName : m_path;
   TrigMonGroup expertHistograms( this, path, expert );
 
   // compile regexes for grouping
   std::vector<std::pair<std::string,boost::regex> > matchingList;
-  std::map<std::string, std::string>::const_iterator regIter;
-  for (regIter = m_regexGroups.begin(); regIter != m_regexGroups.end(); regIter++) {
-    matchingList.push_back(make_pair((*regIter).first,boost::regex((*regIter).second)));
+  for (const auto& reg : m_regexGroups) {
+    matchingList.push_back(make_pair(reg.first, boost::regex(reg.second)));
   }
 
   // compile regexes for exclusion / inclusion
   boost::regex includeName(m_includeName);
   boost::regex excludeName(m_excludeName);
-
+  boost::regex key(m_key);
 
   // loop over all the timers and extract 
-  std::vector < TrigTimer * >::iterator tIter;
-  std::vector<std::pair<std::string,boost::regex> >::const_iterator matchIter;
-  boost::cmatch what;
-  for (tIter = m_timerSvc->begin(); tIter != m_timerSvc->end(); tIter++) {
+  std::vector < TrigTimer * >::const_iterator tIter;
+  for (tIter = timerSvc->begin(); tIter != timerSvc->end(); ++tIter) {
 
     std::string name = (*tIter)->name();
 
@@ -118,148 +96,167 @@ StatusCode TrigTimeHistTool::bookHists(){
     // timer names should now be: "algoA:mytimer"
     // I will assure it in TrigMonTHistSvc regex that name does not have "." 
 
-    if ( !selected(name) ) {
+    if ( !selected(name, key) ) {
       continue;
     }
-    if ( ! boost::regex_match(name.c_str(), what, includeName) ) {
-      log << MSG::DEBUG << "Skipping "<<name<<" because it does not match includeName regex " << m_includeName << endreq;
+    if ( ! boost::regex_match(name, includeName) ) {
+      ATH_MSG_DEBUG("Skipping " << name << " because it does not match includeName regex " << m_includeName);
+      continue;
+    }
+    if ( boost::regex_match(name, excludeName) ) {
+      ATH_MSG_DEBUG("Skipping " << name << " because it matches match excludeName regex " << m_excludeName);
       continue;
     }
 
-    if ( boost::regex_match(name.c_str(), what, excludeName) ) {
-      log << MSG::DEBUG << "Skipping "<<name<<" because it matches match excludeName regex " << m_excludeName << endreq;
-      continue;
-    }
-    log << MSG::VERBOSE << "Considering " << name <<endreq;
-
-
-      
-    replace(name.begin(), name.end(), '.', '_');
-    replace(name.begin(), name.end(), ':', '_');
-   
-    bool isGrouped=false;
-    for (matchIter=matchingList.begin(); matchIter!=matchingList.end(); matchIter++) {
-      if ( boost::regex_match(name.c_str(), what, (*matchIter).second) ) {
-	log << MSG::DEBUG << "Adding timer "<<name<<" to "<<(*matchIter).first<<" histogram"<<endreq;
-	isGrouped=true;
-	if (m_groupHists.find((*matchIter).first)==m_groupHists.end())
-	  m_groupHists[(*matchIter).first]=new struct TimerGroupHist;
-	m_groupHists[(*matchIter).first]->m_timers.push_back(*tIter);
+    ATH_MSG_VERBOSE("Considering " << name);
+         
+    bool isGrouped = false;
+    for (const auto& m : matchingList) {
+      if ( boost::regex_match(name, m.second) ) {
+        ATH_MSG_DEBUG("Adding timer " << name << " to " << m.first << " histogram");
+        isGrouped=true;
+        if (m_groupHists.find(m.first)==m_groupHists.end())
+          m_groupHists[m.first] = new struct TimerGroupHist;
+        m_groupHists[m.first]->m_timers.push_back(*tIter);
       }
     }
 
     if (isGrouped) continue;
 
+    // Cleanup characters for histogram names
     std::string prop = (*tIter)->propName();
+    replace(name.begin(), name.end(), '.', '_');
+    replace(name.begin(), name.end(), ':', '_');
     replace(prop.begin(), prop.end(), '.', '_');
     replace(prop.begin(), prop.end(), ':', '_');
 
-      
     struct TimerHist *th = new struct TimerHist;
     th->m_timer = *tIter;
     // create histograms
     th->m_timerHist = new TH1F(name.c_str(),
-			       (name + ";time[ms];entries").c_str(),
-			       m_bins,
-			       m_timerHistLimits[0],
-			       m_timerHistLimits[1]);
+                               (name + ";time[ms];entries").c_str(),
+                               m_bins,
+                               m_timerHistLimits[0],
+                               m_timerHistLimits[1]);
 
     if (expertHistograms.regHist(th->m_timerHist).isFailure())
-      log << MSG::WARNING << "Unable to book timer histogram " 
-	  << m_path + name << endreq;
+      ATH_MSG_WARNING("Unable to book timer histogram " << m_path << name);
 
     // create histogram with timing per object
     if (m_doPerObjHist && prop != "") {
       th->m_timerPerObjHist = new TH1F((name + prop ).c_str(),
-				       (name + prop + ";time[ms];entries").c_str(),
-				       m_bins,
-				       m_timerPerObjHistLimits[0],
-				       m_timerPerObjHistLimits[1]);
+                                       (name + prop + ";time[ms];entries").c_str(),
+                                       m_bins,
+                                       m_timerPerObjHistLimits[0],
+                                       m_timerPerObjHistLimits[1]);
 
       if (expertHistograms.regHist(th->m_timerPerObjHist).isFailure())
-	log << MSG::WARNING << "Unable to book timer histogram " 
-	    << m_path + name + prop << endreq;
+        ATH_MSG_WARNING("Unable to book timer histogram " << m_path << name << prop);
     }
     else
-      th->m_timerPerObjHist = (TH1F *) 0;
+      th->m_timerPerObjHist = 0;
 	    
     m_entries.push_back(th);
     
   }
 
   // book the grouped histograms
-  std::map <std::string, struct TimerGroupHist *>::const_iterator g;
-  for (g = m_groupHists.begin(); g != m_groupHists.end(); g++) {
-    std::vector<TrigTimer *>::const_iterator t;
-    std::string histName=(*g).first;
+  int ig(-1);
+  for (const auto& g : m_groupHists) {
+    ig++;
+    std::string histName = g.first;
     replace(histName.begin(), histName.end(), '.', '_');
     replace(histName.begin(), histName.end(), ':', '_');
-    TH2F *timerHist = new TH2F(histName.c_str(),
-			       (histName + ";;time[ms]").c_str(),
-			       (*g).second->m_timers.size(),0,(*g).second->m_timers.size(),
-			       m_bins,
-			       m_timerHistLimits[0],
-			       m_timerHistLimits[1]);
-    int binNum=1;
-    TAxis *xAxis=timerHist->GetXaxis();
-    for(std::vector<TrigTimer *>::iterator iter=(*g).second->m_timers.begin();iter!=(*g).second->m_timers.end();iter++) {
-      xAxis->SetBinLabel(binNum++,(*iter)->name().c_str());
+
+    // Regular TH2F or TProfile for LB-histograms
+    TAxis *xAxis;
+    if (m_doPerLBGroups) {
+      g.second->m_timerProf = new
+        TrigLBNHist<TProfile>(TProfile(histName.c_str(),
+                                       (histName + ";;time[ms]").c_str(),
+                                       g.second->m_timers.size(), 0, g.second->m_timers.size()));
+      xAxis = g.second->m_timerProf->GetXaxis();
     }
-    if (expertHistograms.regHist(timerHist).isFailure())
-      log << MSG::WARNING << "Unable to book timer histogram "
-	  << m_path + histName << endreq;
-    (*g).second->m_timerHist=timerHist;
+    else {
+      g.second->m_timerHist = new TH2F(histName.c_str(),
+                                       (histName + ";;time[ms]").c_str(),
+                                       g.second->m_timers.size(), 0, g.second->m_timers.size(),
+                                       m_bins,
+                                       m_timerHistLimits[0],
+                                       m_timerHistLimits[1]);
+      xAxis = g.second->m_timerHist->GetXaxis();
+    }
+
+    // Assign bin labels
+    int binNum = 1;
+    boost::smatch what;
+    for (TrigTimer* timer : g.second->m_timers) {
+      std::string label = timer->name();
+      // If there is a capture in the regex, remove these matches from the bin label
+      if (boost::regex_match(timer->name(), what, matchingList[ig].second) && what.size()>1) {
+        for (size_t i=1; i<what.size(); ++i) {
+          boost::erase_all(label, std::string(what[i]));
+        }
+      }
+      xAxis->SetBinLabel(binNum++, label.c_str());
+    }
+    // Register histogram
+    StatusCode sc = m_doPerLBGroups ?
+      expertHistograms.regHist((ITrigLBNHist*)g.second->m_timerProf) :
+      expertHistograms.regHist(g.second->m_timerHist);
+    
+    if (sc.isFailure())
+        ATH_MSG_WARNING("Unable to book timer histogram " << m_path << histName);
   }
   return StatusCode::SUCCESS;  
 }
 
-StatusCode TrigTimeHistTool::fillHists(){
-  std::vector < struct TimerHist *>::const_iterator t;
-  for (t = m_entries.begin(); t != m_entries.end(); t++) {
-    if ( !(*t)->m_timer->running() && (*t)->m_timer->wasRun() ) {
-      (*t)->m_timerHist->Fill((*t)->m_timer->elapsed());
-      if ((*t)->m_timerPerObjHist != (TH1F *) 0
-	  && (*t)->m_timer->propVal() != 0) {
-	(*t)->m_timerPerObjHist->Fill((*t)->m_timer->elapsed()
-				      / (*t)->m_timer->propVal());
+StatusCode TrigTimeHistTool::fillHists()
+{
+  for (const TimerHist* t : m_entries) {
+    if ( !t->m_timer->running() && t->m_timer->wasRun() ) {
+      t->m_timerHist->Fill(t->m_timer->elapsed());
+      if (t->m_timerPerObjHist != 0 && t->m_timer->propVal() != 0) {
+        t->m_timerPerObjHist->Fill(t->m_timer->elapsed() / t->m_timer->propVal());
       }
     }
   }
-  std::map <std::string, struct TimerGroupHist *>::const_iterator g;
-  for (g = m_groupHists.begin(); g != m_groupHists.end(); g++) {
-    std::vector<TrigTimer *>::const_iterator t;
-    for (t= (*g).second->m_timers.begin(); t != (*g).second->m_timers.end();t++) {
-      if ( !(*t)->running() && (*t)->wasRun() ) {
-        (*g).second->m_timerHist->Fill((*t)->name().c_str(),(*t)->elapsed(),1.);
+  for (const auto& g : m_groupHists) {
+    int bin(0);
+    for (const TrigTimer* t : g.second->m_timers) {
+      if ( !t->running() && t->wasRun() ) {
+        // For performance reasons, the filling is done via the x-value (first bin: x=0)
+        if (m_doPerLBGroups) {
+          g.second->m_timerProf->Fill((Double_t)bin, t->elapsed(), 1.);
+        }
+        else {
+          g.second->m_timerHist->Fill((Float_t)bin, t->elapsed(), 1.);
+        }
       }
+      bin++;
     }
   }
   return StatusCode::SUCCESS;
 }
 
-StatusCode TrigTimeHistTool::finalHists(){
-  // should I print out some sophisticated statistical params of the hists?
+StatusCode TrigTimeHistTool::finalHists()
+{
   return StatusCode::SUCCESS;
 }
 
 
-
-bool TrigTimeHistTool::selected(const std::string & name){
-  if (m_key.empty()) {
+bool TrigTimeHistTool::selected(const std::string& name, const boost::regex& regex){
+  if (regex.size()==0) {
     // no specific Keys are selected ... record
     // then all the timers which parent algo registered
     if ( ! m_parentName.empty() ) {
       if ( name.find(m_parentName+":") == 0 ) // name starts from algo name
-	return true;
+        return true;
     }
-    return false;
   }
-  // exact match (maybe consider regex here)
-  std::vector < std::string >::const_iterator key =
-    find(m_key.begin(), m_key.end(), name);
+  else {
+    return boost::regex_match(name, regex);
+  }
 
-  if (key != m_key.end())
-    return true;
-
-  return false;
+  return false;  
 }
