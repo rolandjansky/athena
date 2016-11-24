754ed13b89b7dbb9b8dffe9225a496fd57f60dcf
John Baines <john.baines@stfc.ac.uk>
2016-11-24 12:56:54 +0100

    Disable NN correction for FTK clusters (FTK_RecTools-00-00-17)
    	* src/FTK_PixelClusterOnTrackTool.cxx: Disable NN correction for FTK clusters
    	* Tag FTK_RecTools-00-00-17
    
    2016-11-23  John Baines  <john.baines@stfc.ac.uk>
    
    	* src/FTK_SCTClusterOnTrackTool.cxx: protect against missing trackPar.associatedSurface().bounds()
    	* Tag FTK_RecTools-00-00-16
    
            * Tag FTK_RecTools-00-00-14
    
    
    Former-commit-id: 4d3f041ce8e928b3c776056563244dc38f417654

--- Trigger/TrigFTK/FTK_RecTools/src/components/FTK_RecTools_entries.cxx
+++ Trigger/TrigFTK/FTK_RecTools/src/components/FTK_RecTools_entries.cxx
@@ -1,9 +1,15 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "FTK_RecTools/FTK_VertexFinderTool.h"
+#include "FTK_RecTools/FTK_PixelClusterOnTrackTool.h"
+#include "FTK_RecTools/FTK_SCTClusterOnTrackTool.h"
 
 DECLARE_TOOL_FACTORY(FTK_VertexFinderTool)
+DECLARE_TOOL_FACTORY(FTK_PixelClusterOnTrackTool)
+DECLARE_TOOL_FACTORY(FTK_SCTClusterOnTrackTool)
 
 DECLARE_FACTORY_ENTRIES(FTK_RecTools) {
   DECLARE_TOOL(FTK_VertexFinderTool)
+  DECLARE_TOOL(FTK_PixelClusterOnTrackTool)
+  DECLARE_TOOL(FTK_SCTClusterOnTrackTool)
 }
