5b572c3dc729f4ae6afd10f98a57e84086584808
Christos Anastopoulos <Christos.Anastopoulos@cern.ch>
2016-11-24 17:19:20 +0100

    obo Zach (MCTruthClassifier-00-01-53-01)
    	* Tagging MCTruthClassifier-00-01-53-01
    	* Obo Zach
    	* Attempting to add some catches for Pythia8 prompt photon samples that
    	* previously resulted in unclassified photons
    
    2016-10-19 Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
    	* Taught the package how to build in standalone mode with CMake.
    	* Removed the executable flag from the source files of the package.
    	* Made testClassifier work correctly, and without warnings on new
    	  MC files. (That may have a split level of 0 for their auxiliary
    	  stores.)
    	* Tagging as MCTruthClassifier-00-01-53
    
    2016-10-10 Christos Anastopoulos  <Christos.Anastopoulos@cern.ch>
    
    	* Tagging MCTruthClassifier-00-01-52.
    	* More coverity warnings
    
    
    Former-commit-id: 381341cdf19f533b8a2723b86e720029c15f9ccc

--- PhysicsAnalysis/MCTruthClassifier/util/testClassifier.cxx
+++ PhysicsAnalysis/MCTruthClassifier/util/testClassifier.cxx
@@ -42,10 +42,10 @@ int main( int argc, char* argv[] ) {
      Error( APP_NAME, "  Usage: %s [xAOD file name]", APP_NAME );
       return 1;
    }
-   
+
    // Initialise the application:
    CHECK( xAOD::Init( APP_NAME ) );
-   
+
    // Open the input file:
    const TString fileName = argv[ 1 ];
    Info( APP_NAME, "Opening file: %s", fileName.Data() );
@@ -53,7 +53,7 @@ int main( int argc, char* argv[] ) {
    CHECK( ifile.get() );
 
    // Create a TEvent object:
-   xAOD::TEvent event( xAOD::TEvent::kBranchAccess );
+   xAOD::TEvent event;
    CHECK( event.readFrom( ifile.get() ) );
    Info( APP_NAME, "Number of events in the file: %i",
          static_cast< int >( event.getEntries() ) );
@@ -68,52 +68,47 @@ int main( int argc, char* argv[] ) {
    }
 
    MCTruthClassifier myClassifier ("myClassifier");
-   myClassifier.initialize();
+   CHECK( myClassifier.initialize() );
 
    // Loop over the events:
    for( Long64_t entry = 0; entry < entries; ++entry ) {
-     
+
      // Tell the object which entry to look at:
      event.getEntry( entry );
-   
-     std::cout << "=================NEXT EVENT==========================" << std::endl;
-     
-     //Electrons
-     const xAOD::ElectronContainer* electrons = 0 ;  
-     CHECK( event.retrieve(electrons, "Electrons") );
-     
-     if ( !event.retrieve( electrons, "Electrons" ).isSuccess() ){ // retrieve arguments: container type, container key
-       Error("execute()", "Failed to retrieve El container. Exiting." );
-     }
 
-     xAOD::ElectronContainer::const_iterator el_it      = electrons->begin(); 
+     std::cout << "=================NEXT EVENT==========================" << std::endl;
+
+     //Electrons
+     const xAOD::ElectronContainer* electrons = 0 ;
+     CHECK( event.retrieve(electrons, "Electrons") );
+
+     xAOD::ElectronContainer::const_iterator el_it      = electrons->begin();
      xAOD::ElectronContainer::const_iterator el_it_last = electrons->end(); 
      unsigned int i = 0; 
-     
+
      for (; el_it != el_it_last; ++el_it, ++i) {
        const xAOD::Electron* el = (*el_it); 
-       std::cout << "Electron " << el << " Num " << i << std::endl; 
-       std::cout << "xAOD pt = " << (*el_it)->pt() << std::endl; 
+       std::cout << "Electron " << el << " Num " << i << std::endl;
+       std::cout << "xAOD pt = " << (*el_it)->pt() << std::endl;
        Info (APP_NAME,"Electron #%d", i); 
-       std::pair<MCTruthPartClassifier::ParticleType,MCTruthPartClassifier::ParticleOrigin> 
-	 classification = myClassifier.particleTruthClassifier(*el_it);
+       std::pair<MCTruthPartClassifier::ParticleType,MCTruthPartClassifier::ParticleOrigin>
+          classification = myClassifier.particleTruthClassifier(*el_it);
 
        static SG::AuxElement::Accessor<int> tT("truthType") ;
        if (tT.isAvailable(**el_it)){;
-	 Info (APP_NAME,"Electron Type from  Reco returns %d ", tT(**el_it) );
+          Info (APP_NAME,"Electron Type from  Reco returns %d ", tT(**el_it) );
        }
        Info (APP_NAME,"Electron Type from  Analysis Base returns %d ", classification.first );
        static SG::AuxElement::Accessor<int> tO("truthOrigin") ;
        if (tO.isAvailable(**el_it)){;
-	 Info (APP_NAME,"Electron Origin from  Reco returns %d ", tO(**el_it) );
+          Info (APP_NAME,"Electron Origin from  Reco returns %d ", tO(**el_it) );
        }
        Info (APP_NAME,"Electron Origin from  Analysis Base returns %d ", classification.second );
      }
-     Info( APP_NAME,
-	   "===>>>  done processing event #%lld ",entry);
+     Info( APP_NAME, "===>>>  done processing event #%lld ",entry);
    }
 
-   myClassifier.finalize();
+   CHECK( myClassifier.finalize() );
    // Return gracefully:
    return 0;
 }
