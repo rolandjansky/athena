3c77bacfa06b82d7fb0ea737d6dcfe5480bf7782
Ye Chen <cheny@stu.kobe-u.ac.jp>
2016-11-24 13:03:10 +0100

    HLT_mu10/14 not in 8TeV HI run, to use HLT_mu15_L1MU10 (TrigMuonMonitoring-00-02-43)
      * HI 5TeV->8TeV no HLT_mu10 and HLT_mu14
      * HLT_mu15_L1MU10(new monitoring chain)
      * Chain1: HLT_mu15_L1MU10 Chain2: HLT_mu14(in case keep this in rerun-mode)
      * Tagging TrigMuonMonitoring-00-02-43
    
    
    Former-commit-id: 140e6419c1656dcb96bdfd288cc53a756803e655

--- Trigger/TrigMonitoring/TrigMuonMonitoring/python/TrigMuonMonitCategory.py
+++ Trigger/TrigMonitoring/TrigMuonMonitoring/python/TrigMuonMonitCategory.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 monitoring_muonNonIso = ['HLT_mu50']
-monitoring_muonNonIso_HI = ['HLT_mu10','HLT_mu14']
+monitoring_muonNonIso_HI = ['HLT_mu15_L1MU10','HLT_mu14']#8TeV
+#monitoring_muonNonIso_HI = ['HLT_mu10','HLT_mu14']
 monitoring_muonNonIso_pp = ['HLT_mu50']
 
 monitoring_muonIso = ['HLT_mu26_ivarmedium','HLT_mu26_imedium']
