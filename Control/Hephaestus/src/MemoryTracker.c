287e5c65fd0caaf79eb98c665ac5b397cb621700
Scott Snyder <scott.snyder@cern.ch>
2016-11-30 17:27:13 +0100

    'Adding more symbols to ignore.' (Hephaestus-00-06-35)
    	* Tagging Hephaestus-00-06-35.
    	* Adding more symbols to ignore.
    
    
    Former-commit-id: f061d749252ec6255899091cc88b1cf890d30113

--- Control/Hephaestus/src/MemoryTracker.c
+++ Control/Hephaestus/src/MemoryTracker.c
@@ -55,9 +55,10 @@ static struct hhh_HashTable *gTraceInfo  = 0;
 static struct hhh_HashTable *gPushBacks = 0;
 static struct hhh_NameSet {
    char **names;
+   unsigned int *lens;
    unsigned int size;
    unsigned int capacity;
-} gIgnoreNames, gTraceNames;
+} gIgnoreNames, gIgnoreCallNames, gTraceNames;
 static struct hhh_HashTable *gOldTracers[ hhh_MAX_CHECKPOINTS + 1 ];
 
 /* for free statistics */
@@ -92,6 +93,7 @@ struct hhh_HashTable *hhh_getTraceInfo() {
 /* - helpers for dealing with sets of names -------------------------------- */
 static void hhh_NameSet_construct( struct hhh_NameSet *nameset, unsigned int size ) {
    nameset->names = (char**)malloc( sizeof(char*)*size );
+   nameset->lens = (unsigned int*)malloc( sizeof(unsigned int)*size );
    nameset->size = 0;
    nameset->capacity = size;
 }
@@ -102,7 +104,9 @@ static void hhh_NameSet_destroy( struct hhh_NameSet *nameset ) {
    for ( i = 0; i < nameset->size; i++ )
       free( nameset->names[i] );
    free( nameset->names );
+   free( nameset->lens );
    nameset->names = 0; nameset->capacity = 0; nameset->size = 0;
+   nameset->lens = 0;
 }
 
 static void hhh_NameSet_Add( struct hhh_NameSet *nameset, const char *name ) {
@@ -111,11 +115,15 @@ static void hhh_NameSet_Add( struct hhh_NameSet *nameset, const char *name ) {
    if ( nameset->size >= nameset->capacity ) {
       nameset->names =
          (char**)realloc( nameset->names, sizeof(char*)*nameset->capacity*2 );
+      nameset->lens =
+         (unsigned int*)realloc( nameset->lens, sizeof(unsigned int)*nameset->capacity*2 );
       nameset->capacity *= 2;
    }
 
-   name_cp = (char*)malloc( strlen( name ) + 1 );
+   unsigned int len = strlen( name );
+   name_cp = (char*)malloc( len + 1 );
    strcpy( name_cp, name );
+   nameset->lens[ nameset->size ] = len;
    nameset->names[ nameset->size++ ] = name_cp;
 }
 
@@ -237,6 +245,24 @@ static void hhh_report( void ) {
             }
          }
 
+         {
+           StackElement* elt;
+           for (elt = STACK_HANDLE_ELEMENT (summary->leak->handle);
+                !STACK_ELEMENT_IS_ROOT(elt);
+                elt = STACK_ELEMENT_PARENT (elt))
+           {
+             const char* s = hhh_getSymbol( *elt );
+             int iname;
+             for ( iname = 0; iname < gIgnoreCallNames.size; ++iname ) {
+               if ( strncmp (s, gIgnoreCallNames.names[iname], gIgnoreCallNames.lens[iname]) == 0 )
+               {
+                 ignore = 1;
+                 break;
+               }
+             }
+           }
+         }
+
          if ( pos )
             free( sub );
          sub = 0;
@@ -316,6 +342,11 @@ static void trace( const char *name ) {
 /* _________________________________________________________________________ */
 static void ignore( const char *name ) {
    hhh_NameSet_Add( &gIgnoreNames, name );
+}
+
+/* _________________________________________________________________________ */
+static void ignoreCall( const char *name ) {
+   hhh_NameSet_Add( &gIgnoreCallNames, name );
 }
 
 /* _________________________________________________________________________ */
@@ -529,6 +560,13 @@ static void captureTrace( void *result, long size, int isrealloc ) {
             continue;
 
          if ( gFlags & LEAK_CHECK ) {
+         /* force lookup of symbols, such that they are cached */
+            if ( ! ( gFlags & QUICK ) && i+1 < tcount ) {
+              for ( j = i+1; j < tcount; j++ ) {
+                hhh_getSymbol( addresses[j] );
+              }
+            }
+
          /* save full traceback info */
             b = hhh_MemoryTrace_new();
             if ( b == 0 ) {
@@ -542,11 +580,6 @@ static void captureTrace( void *result, long size, int isrealloc ) {
                                         tcount - responsible );
 
             hhh_HashTable_insert( gTraceInfo, result, b );
-
-         /* force lookup of symbols, such that they are cached */
-            if ( ! ( gFlags & QUICK ) && i+1 < tcount ) {
-               for ( j = i+1; j < tcount; j++ ) hhh_getSymbol( addresses[j] );
-            }
          }
 
          if ( gFlags & PROFILE ) {
@@ -815,6 +848,7 @@ static void setup() {
    gPushBacks   = hhh_HashTable_new( hhh_HASHTABLE_MINSIZE );
    hhh_NameSet_construct( &gTraceNames, 2 );
    hhh_NameSet_construct( &gIgnoreNames, 10 );
+   hhh_NameSet_construct( &gIgnoreCallNames, 10 );
 
    gOldTracers[ hhh_MAX_CHECKPOINTS ] = 0;
 
@@ -864,6 +898,7 @@ static void teardown() {
          pclose( gProfileStream );
 
       hhh_NameSet_destroy( &gIgnoreNames );
+      hhh_NameSet_destroy( &gIgnoreCallNames );
       hhh_NameSet_destroy( &gTraceNames );
 
       hhh_HashTable_delete( gPushBacks,  &free ); gPushBacks = 0;
@@ -919,6 +954,7 @@ static PyObject* hep_##fname( PyObject* unused, PyObject* args ) {            \
 
 CPPTOPYTHONWITHSTRING( trace )
 CPPTOPYTHONWITHSTRING( ignore )
+CPPTOPYTHONWITHSTRING( ignoreCall )
 
 /* _________________________________________________________________________ */
 #define CPPTOPYTHONWITHINT( fname )                                           \
@@ -1063,6 +1099,7 @@ static PyMethodDef gMemoryTrackerMethods[] = {
    { (char*)"atexit",    (PyCFunction)hep_atexit,    METH_VARARGS, (char*)"atexit handler" },
    { (char*)"trace",     (PyCFunction)hep_trace,     METH_VARARGS, (char*)"trace given filter" },
    { (char*)"ignore",    (PyCFunction)hep_ignore,    METH_VARARGS, (char*)"ignore in report" },
+   { (char*)"ignoreCall",(PyCFunction)hep_ignoreCall,METH_VARARGS, (char*)"ignore in report anywhere in trace" },
    { (char*)"configure", (PyCFunction)hep_configure, METH_VARARGS, (char*)"set configuration flags" },
    { (char*)"outstream", (PyCFunction)hep_outstream, METH_VARARGS, (char*)"set new outstream" },
    { (char*)"depth",     (PyCFunction)hep_depth,     METH_VARARGS, (char*)"set large trace depth" },
