3a35d8bf2acf010653f547a16b63b23ad9b9c45c
Denis Oliveira Damazio <Denis.Oliveira.Damazio@cern.ch>
2016-11-30 17:11:03 +0100

    algorithm migration (TrigCaloHypo-00-00-02)
    Former-commit-id: c7f26d30eb5358ca601c0ad70a1c3b71f5925218

--- Trigger/TrigHypothesis/TrigCaloHypo/python/TrigCaloHypoConfig.py
+++ Trigger/TrigHypothesis/TrigCaloHypo/python/TrigCaloHypoConfig.py
@@ -14,3 +14,36 @@ class EFCaloHypoNoiseConfig (TrigEFCaloHypoNoise):
         self.BadFEBCut=3
         theLArNoisyROTool=LArNoisyROTool(SaturatedCellTightCut=20,MNBLooseCut=5,MNBTightCut=17)
         self.NoiseTool = theLArNoisyROTool
+
+from TrigJetHypo.TrigJetHypoConf import TrigL2JetHypo
+
+class L2JetHypoBase (TrigL2JetHypo):
+    __slots__ = []
+    def __init__(self, name):
+        super( L2JetHypoBase, self ).__init__( name )
+        
+        #validation = TrigL2JetHypoValidationMonitoring()
+        #online = TrigL2JetHypoOnlineMonitoring()
+        #cosmic = TrigL2JetHypoCosmicMonitoring()
+        
+        from TrigTimeMonitor.TrigTimeHistToolConfig import TrigTimeHistToolConfig
+        time = TrigTimeHistToolConfig("L2JetHypo_Time")
+        
+        self.AthenaMonTools = [ time ]
+        
+        # switch on basic cleaning by default for all chains
+        self.n90CleaningThreshold = 2
+        self.presamplerCleaningThreshold = 0.9
+
+class L2JetHypo (L2JetHypoBase):
+    __slots__ = []
+    def __init__(self, name = "L2JetHypo",l2_thr=20*GeV):
+        super( L2JetHypo, self ).__init__( name )
+
+        self.Etcut_L2 = l2_thr
+        self.doHECfnLeadingCellsCleaning = False
+        self.doEMfJetQualityCleaning = False
+        self.doHECfJetQualityCleaning = False
+        self.doTimeQualityCleaning = False
+        self.doEMfCleaningHighEta = False
+        self.doEMfCleaningLowEta = False
