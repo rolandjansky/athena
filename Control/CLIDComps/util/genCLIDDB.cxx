4b5f3f52b2302b86c3596af1ed2f461f00ab1f2e
Attila Krasznahorkay <krasznaa@cern.ch>
2016-12-02 10:19:00 +0100

    Added the genCLIDDB executable to this package (from AthenaKernel) (CLIDComps-00-06-24)
    	* Added the genCLIDDB executable to this package. So that it would
    	  be possible to correctly declare its dependencies. So that the
    	  build would not try to run it before the component lists file
    	  of the CLIDComps library is available.
    	* Removed the wscript file, as hwaf is not coming back...
    	* Tagging as CLIDComps-00-06-24
    
    2016-09-24  scott snyder  <snyder@bnl.gov>
    
    	* Tagging CLIDComps-00-06-23.
    	* Change initializing message from INFO to VERBOSE.
    
    2016-08-26  scott snyder  <snyder@bnl.gov>
    
    	* Tagging CLIDComps-00-06-22.
    	* Coverity 111265.
    
    2016-07-28  scott snyder  <snyder@bnl.gov>
    
    	* Tagging CLIDComps-00-06-21.
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: 638618120c0b840c020dbd42784b0b7a16eb567c

---
+++ Control/CLIDComps/util/genCLIDDB.cxx
@@ -0,0 +1,132 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#include <cassert>
+#include <exception>
+#include <iostream>
+#include <string>
+#include "TestTools/initGaudi.h"
+#include "GaudiKernel/IClassManager.h"
+#include "GaudiKernel/IProperty.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/SmartIF.h"
+#include "AthenaKernel/IClassIDSvc.h"
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+using namespace std;
+using namespace Athena_test;
+
+int inputError(std::string errDescr, const po::options_description& optDescr ) {
+  cerr << errDescr << "\n" << optDescr << endl;
+  return 1;
+}
+int gaudiError(std::string errDescr) {
+  cerr << errDescr << endl;
+  return 2;
+}
+
+int main(int argc, char* argv[]) {
+  // Declare the supported options.
+  po::options_description desc("clidDB_gen allowed options");
+  desc.add_options()
+    ("help,h", "produce help message")
+    ("package,p", po::value<string>(), "package we want to load clids from")
+    ("input,i", po::value<string>(), "optional path to input clid db file")
+    ("output,o", po::value<string>(), "optional path to resulting clid db file")
+    ("jobopts,j", po::value<string>(), "name of optional job options txt file, located at ../share/jobopts")
+    ;
+  string packageName("ALL");
+  string inputCLIDDB;
+  string outFileName;
+
+  po::variables_map vm;
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);    
+  } catch (const exception& e) {
+    return inputError(e.what(), desc);
+  }
+
+  if (vm.count("help")) {
+    cout << desc << endl;
+    return 0;
+  }
+  
+  if (vm.count("package")) {
+    cout << "Generating clid.db for package " 
+	 << vm["package"].as<string>() << ".\n";
+    packageName = vm["package"].as<string>();
+  } else {
+    return inputError("Please specify a package using option --package.\n", 
+		      desc);
+  }
+
+  if (vm.count("output")) {
+    outFileName = vm["output"].as<string>();
+  } else {
+    outFileName = packageName + "_clid.db";
+  }
+  cout << "Resulting clid.db will be written to " 
+       << outFileName << ".\n";
+
+  ISvcLocator* pSvcLoc(0);
+  if (vm.count("jobopts")) { 
+    if (!initGaudi(vm["jobopts"].as<string>(), pSvcLoc)) {
+      return gaudiError("clidDB_gen can not run");
+    }  
+  } else {
+    if (!initGaudi(pSvcLoc)) {
+      return gaudiError("clidDB_gen can not run");
+    }  
+  }
+  if ( 0 == pSvcLoc ) {
+    return gaudiError( "NULL pointer to ISvcLocator" );
+  }
+  SmartIF<IClassManager> pICM(pSvcLoc);
+  if (!pICM.isValid()) {
+    gaudiError("can not get IClassManager");
+  }
+
+  IClassIDSvc* pClassIDSvc(0);
+  if (!(pSvcLoc->service("ClassIDSvc", pClassIDSvc, true).isSuccess())) {
+    cerr << "can not get ClassIDSvc, no clid.db will be generated" << endl;
+    return 0;
+  }
+  if ( 0 == pClassIDSvc ) {
+    return gaudiError("NULL pointer to IClassIDSvc");
+  }
+
+  IProperty *pCLIDSvcProp(dynamic_cast<IProperty*>(pClassIDSvc));
+  if ( 0 == pCLIDSvcProp ) {
+    return gaudiError("NULL pointer to IClassIDSvc's property");
+  }
+
+  if (vm.count("input")) {
+    cout << "Reading clid.db from " 
+	 << vm["input"].as<string>() << ".\n";
+    if (!(pCLIDSvcProp->setProperty( "CLIDDBFiles", 
+				     "{\"" + vm["input"].as<string>() + "\"}" )).isSuccess()) {
+      return inputError("Error setting ClassIDSvc.CLIDDBFiles to " 
+			+ vm["input"].as<string>(), 
+			desc);
+    }
+  }
+
+  pCLIDSvcProp->setProperty( "OutputFileName", outFileName );
+
+  if (!pICM->loadModule(packageName).isSuccess()) {
+    return gaudiError("can not load module " + packageName);
+  }
+
+  //fill clid db
+  if (!(pClassIDSvc->reinitialize()).isSuccess()) {
+    return gaudiError("can not reinitialize ClassIDSvc");
+  }
+
+  //write out merged clid db on service finalize
+  return (pClassIDSvc->finalize()).isSuccess() ? 0 : -1;
+}
