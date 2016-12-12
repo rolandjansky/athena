3268cf8c6b982832647afe695f7754826be04700
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2016-12-12 09:29:50 +0100

    set EtaJES step in the calibopt (JetRec-03-01-02)
    	* share/JetRec_jobOptions.py: set EtaJES step in the calibopt
    	* Tagging as JetRec-03-01-02
    
    2016-12-05  Johannes Elmsheuser  <Johannes.Elmsheuser@cern.ch>
    	* share/JetRec_jobOptions.py: Raise ptmin and/or ptminFilter cuts for AntiKt4EMTopoJets,
    	AntiKt4LCTopoJets, AntiKt10LCTopoJets, AntiKt4EMPFlowJet
    	* Tagging as JetRec-03-01-01
    
    
    Former-commit-id: 2a7413966af73f7df9d7676574423c38f9cdbc37

--- Reconstruction/Jet/JetRec/share/JetRec_jobOptions.py
+++ Reconstruction/Jet/JetRec/share/JetRec_jobOptions.py
@@ -38,17 +38,17 @@ if jetFlags.useTracks():
   jtm.addJetFinder("AntiKt2PV0TrackJets", "AntiKt", 0.2, "pv0track", ptmin= 2000)
 #  jtm.addJetFinder("AntiKt3PV0TrackJets", "AntiKt", 0.3, "pv0track", ptmin= 2000)
   jtm.addJetFinder("AntiKt4PV0TrackJets", "AntiKt", 0.4, "pv0track", ptmin= 2000)
-calibopt = "ar"
+calibopt = "arj"
 if not jetFlags.useVertices():
-  calibopt = "a"
+  calibopt = "aj"
   jetlog.info(myname + "No vertices -- switch calibopt to " + calibopt)
 if jetFlags.useTopo():
-  jtm.addJetFinder("AntiKt4EMTopoJets",   "AntiKt", 0.4,   "emtopo", "emtopo_ungroomed", ghostArea=0.01, ptmin= 2000, ptminFilter= 5000, calibOpt=calibopt)
-  jtm.addJetFinder("AntiKt4LCTopoJets",   "AntiKt", 0.4,   "lctopo", "lctopo_ungroomed", ghostArea=0.01, ptmin= 2000, ptminFilter= 7000, calibOpt=calibopt)
-  jtm.addJetFinder("AntiKt10LCTopoJets",  "AntiKt", 1.0,   "lctopo", "lctopo_ungroomed", ghostArea=0.01, ptmin= 2000, ptminFilter=50000, calibOpt="none")
+  jtm.addJetFinder("AntiKt4EMTopoJets",   "AntiKt", 0.4,   "emtopo", "emtopo_ungroomed", ghostArea=0.01, ptmin= 5000, ptminFilter= 15000, calibOpt=calibopt)
+  jtm.addJetFinder("AntiKt4LCTopoJets",   "AntiKt", 0.4,   "lctopo", "lctopo_ungroomed", ghostArea=0.01, ptmin= 5000, ptminFilter= 15000, calibOpt=calibopt)
+  jtm.addJetFinder("AntiKt10LCTopoJets",  "AntiKt", 1.0,   "lctopo", "lctopo_ungroomed", ghostArea=0.01, ptmin= 40000, ptminFilter=50000, calibOpt="none")
 #  jtm.addJetFinder("CamKt12LCTopoJets",    "CamKt", 1.2,   "lctopo", "calib", ghostArea=0.01, ptmin= 5000, ptminFilter=50000, calibOpt="none")
 if jetFlags.usePFlow():
-  jtm.addJetFinder("AntiKt4EMPFlowJets",  "AntiKt", 0.4,   "empflow", "pflow_ungroomed", ghostArea=0.01, ptmin= 2000, ptminFilter= 5000, calibOpt=calibopt+":pflow")
+  jtm.addJetFinder("AntiKt4EMPFlowJets",  "AntiKt", 0.4,   "empflow", "pflow_ungroomed", ghostArea=0.01, ptmin= 5000, ptminFilter= 10000, calibOpt=calibopt+":pflow")
 
 #--------------------------------------------------------------
 # Build output container list.
