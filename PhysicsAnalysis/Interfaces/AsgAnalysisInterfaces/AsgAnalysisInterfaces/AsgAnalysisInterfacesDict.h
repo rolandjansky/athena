60023ddebb6b887503532ff1dbc18bc0f9c09284
Attila Krasznahorkay <krasznaa@cern.ch>
2016-12-16 16:37:28 +0100

    Adding IEfficiencyScaleFactorTool.h (AsgAnalysisInterfaces-00-00-05)
    	* Added IEfficiencyScaleFactorTool.h to the package. It is meant to
    	  become a common interface for all analysis tools providing scale
    	  factors for particle type objects.
    	* Tagging as AsgAnalysisInterfaces-00-00-05
    
    
    Former-commit-id: cca4fc0720040f4c1e5377afd2bab65738773e8b

--- PhysicsAnalysis/Interfaces/AsgAnalysisInterfaces/AsgAnalysisInterfaces/AsgAnalysisInterfacesDict.h
+++ PhysicsAnalysis/Interfaces/AsgAnalysisInterfaces/AsgAnalysisInterfaces/AsgAnalysisInterfacesDict.h
@@ -1,13 +1,17 @@
+// Dear emacs, this is -*- c++ -*-
+
 /*
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef ASGANALYSISINTERFACESDICT_H
-#define ASGANALYSISINTERFACESDICT_H
+// $Id: AsgAnalysisInterfacesDict.h 790193 2016-12-16 16:36:15Z krasznaa $
+#ifndef ASGANALYSISINTERFACES_ASGANALYSISINTERFACESDICT_H
+#define ASGANALYSISINTERFACES_ASGANALYSISINTERFACESDICT_H
 
-#include "AsgAnalysisInterfaces/IPileupReweightingTool.h"
+// Local include(s):
+#include "AsgAnalysisInterfaces/IEfficiencyScaleFactorTool.h"
 #include "AsgAnalysisInterfaces/IGoodRunsListSelectionTool.h"
 #include "AsgAnalysisInterfaces/IObservableTool.h"
+#include "AsgAnalysisInterfaces/IPileupReweightingTool.h"
 
-
-#endif
+#endif // ASGANALYSISINTERFACES_ASGANALYSISINTERFACESDICT_H
