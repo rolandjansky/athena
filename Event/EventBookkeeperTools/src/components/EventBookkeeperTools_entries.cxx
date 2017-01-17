0d22cb04c0df1171dcd9f526b0f1f7aac5d16a36
Jack Cranshaw <cranshaw@anl.gov>
2017-01-17 22:07:23 +0100

    Fix some protections that gave warnings (EventBookkeeperTools-00-00-96)
      * Fix some protections that gave warnings
      * Tag EventBookkeeperTools-00-00-96
    
    2016-12-08 Peter van Gemmeren  <gemmeren@anl.gov>
      * Remove EventSelectorCounterTool
      * Tag EventBookkeeperTools-00-00-95
    
    
    Former-commit-id: b921578815eca6e4f5a0d65d309f7c8e24d8b9e5

--- Event/EventBookkeeperTools/src/components/EventBookkeeperTools_entries.cxx
+++ Event/EventBookkeeperTools/src/components/EventBookkeeperTools_entries.cxx
@@ -5,13 +5,11 @@
 #include "../FileCutFlowSvc.h"
 #include "../CutFlowSvc.h"
 #include "EventBookkeeperTools/BookkeeperTool.h"
-#include "../EventSelectorCounterTool.h"
 #include "../EventCounterAlg.h"
 
 DECLARE_ALGORITHM_FACTORY( myCppFilterTest )
 DECLARE_ALGORITHM_FACTORY( SkimDecisionMultiFilter )
 DECLARE_ALGORITHM_FACTORY( EventCounterAlg )
-DECLARE_TOOL_FACTORY( EventSelectorCounterTool )
 DECLARE_TOOL_FACTORY( BookkeeperTool )
 DECLARE_SERVICE_FACTORY( CutFlowSvc )
 DECLARE_SERVICE_FACTORY( FileCutFlowSvc )
@@ -20,7 +18,6 @@ DECLARE_FACTORY_ENTRIES(EventBookkeeperTools) {
   DECLARE_ALGORITHM( myCppFilterTest );
   DECLARE_ALGORITHM( SkimDecisionMultiFilter );
   DECLARE_ALGORITHM( EventCounterAlg );
-  DECLARE_ALGTOOL( EventSelectorCounterTool );
   DECLARE_ALGTOOL( BookkeeperTool );
   DECLARE_SERVICE( CutFlowSvc );
   DECLARE_SERVICE( FileCutFlowSvc );
