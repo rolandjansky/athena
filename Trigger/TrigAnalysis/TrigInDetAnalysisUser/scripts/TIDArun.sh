1188f48edb12afc7d24c849e48a06a0dd18bfa3d
Mark Sutton <mark.sutton@cern.ch>
2017-01-19 12:51:54 +0100

    test script changes (TrigInDetAnalysisUser-00-05-20)
            * TrigInDetAnalysisUser-00-05-20
            * test script changes
    
    2017-01-19 sutt < sutt @ cern.ch >
            * TrigInDetAnalysisUser-00-05-19
            * protect web page copies
    
    2017-01-19  Julie Hart Kirk  <hartj@lxplus086.cern.ch>
            * TrigInDetAnalysisUser-00-05-18
    	* add new Test_bin.dat for larged0 tests:
    	* share/Test_bin_larged0.dat:
    
    2017-01-19 sutt < sutt @ cern.ch >
            * TrigInDetAnalysisUser-00-05-18
            * protect web page copies
    
    2017-01-19 sutt < sutt @ cern.ch >
            * TrigInDetAnalysisUser-00-05-17
            * improve rtt test run script
    
    ...
    (Long ChangeLog diff - truncated)
    
    
    Former-commit-id: be9b82e78ff3597899ffc290dca0d1786b3d0da9

--- Trigger/TrigAnalysis/TrigInDetAnalysisUser/scripts/TIDArun.sh
+++ Trigger/TrigAnalysis/TrigInDetAnalysisUser/scripts/TIDArun.sh
@@ -1,4 +1,49 @@
 #!/usr/bin/env bash
 
-TIDAcomparitor.exe $*
+pwd
 
+echo "args: $*"
+
+BASEDIR=/afs/cern.ch/user/s/sutt/public
+
+for FILE in $BASEDIR/TIDAWeb/TIDAV/{style.css,index.php}; do
+  if [ ! -e $FILE ]; then 
+      cp  $FILE .
+  fi
+done
+
+
+BASEREFDIR=$BASEDIR/TrigInDetValidationReference
+
+if [ -e TrkNtuple-0000.root ]; then 
+
+    RELEASE=$(TIDAreader.exe -ro data*.root | grep release | awk '{print $2}' | head -1)
+
+    REFDIR=$BASEREFDIR/share
+
+    if [ -d $BASEREFDIR/$RELEASE ]; then 
+        REFDIR=$BASEREFDIR/$RELEASE
+    fi
+
+    echo "fetching reference files from $REFDIR"
+     
+    REFFILE=$REFDIR/$2
+
+    if [ -e  $REFFILE ]; then 
+      echo "copying $REFFILE" 
+      cp $REFFILE .
+
+      ls -l
+
+      echo
+      echo "running comparitor " `date`
+      echo
+
+      TIDAcomparitor.exe $*
+
+    else
+      echo "no reference file $REFFILE" 
+    fi
+
+
+fi
