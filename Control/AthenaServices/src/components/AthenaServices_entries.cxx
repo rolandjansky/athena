7051ee4740ea976395890bba289e007f3f883d64
Charles Leggett <charles.g.leggett@gmail.com>
2017-01-25 02:06:07 +0100

    remove ifdef ATHENAHIVE (AthenaServices-01-60-51)
    	* remove ifdef ATHENAHIVE in order to merge 20.8.X and 22
    	* tag AthenaServices-01-60-51
    
    M       src/AthenaHiveEventLoopMgr.cxx
    M       src/components/AthenaServices_entries.cxx
    
    
    Former-commit-id: 506d88e2d35f77f325a965d5ea210e58847b78c3

--- Control/AthenaServices/src/components/AthenaServices_entries.cxx
+++ Control/AthenaServices/src/components/AthenaServices_entries.cxx
@@ -31,18 +31,14 @@
 #include "../MetaDataSvc.h"
 #include "../OutputStreamSequencerSvc.h"
 #include "../AthenaStopperAlg.h"
-#ifdef ATHENAHIVE
 #include "../AthenaHiveEventLoopMgr.h"
-#endif
 
 DECLARE_ALGORITHM_FACTORY( AthenaOutputStream )
 DECLARE_ALGORITHM_FACTORY( AthenaConditionStream )
 DECLARE_ALGORITHM_FACTORY( TestRandomSeqAlg )
 DECLARE_SERVICE_FACTORY( MultipleEventLoopMgr )
 DECLARE_SERVICE_FACTORY( AthenaEventLoopMgr )
-#ifdef ATHENAHIVE
 DECLARE_SERVICE_FACTORY( AthenaHiveEventLoopMgr )
-#endif
 DECLARE_SERVICE_FACTORY( PyAthenaEventLoopMgr )
 DECLARE_SERVICE_FACTORY( SimplePOSIXTimeKeeperSvc )
 DECLARE_SERVICE_FACTORY( MixingEventSelector )
