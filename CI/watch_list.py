c0df62ae284500fe22a74016c63fdea473b70b9a
Christian Gumpert <christian.gumpert@cern.ch>
2017-01-31 17:17:51 +0100

    explain structure of domain map/watch list
    Former-commit-id: a5a37ae4ff4dedbbb876d63912c63c11511e7842

--- CI/watch_list.py
+++ CI/watch_list.py
@@ -1,4 +1,7 @@
-WATCH_LIST = \
-{ 'CI': set(['cgumpert']),
-  'Simulation': set(['ritsch'])
-}
+# dictionary defining a watch list for MR notifications
+# keys   ... must be valid regular expression
+# values ... must be sets containing Gitlab usernames as strings
+WATCH_LIST = {}
+
+WATCH_LIST['^CI/']       = set(['cgumpert'])
+WATCH_LIST['Simulation'] = set(['ritsch'])
