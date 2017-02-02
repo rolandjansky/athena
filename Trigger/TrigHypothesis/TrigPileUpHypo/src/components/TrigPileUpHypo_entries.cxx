59bfec1b7d15dd0f4a106c5ddd2f6df259b0b5fe
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:04:46 +0100

    Trigger/TrigHypothesis/TrigPileUpHypo deleted from master

--- Trigger/TrigHypothesis/TrigPileUpHypo/src/components/TrigPileUpHypo_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "TrigPileUpHypo/TrigPileUpHypo.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_ALGORITHM_FACTORY( TrigPileUpHypo )
-
-DECLARE_FACTORY_ENTRIES( TrigPileUpHypo ) {
-  DECLARE_ALGORITHM( TrigPileUpHypo )
-}
-
