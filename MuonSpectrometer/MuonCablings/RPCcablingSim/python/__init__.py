e937eaafd88f0cf124b7950d64f440b92deb3547
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:35 +0100

    MuonSpectrometer/MuonCablings/RPCcablingSim deleted from master

--- MuonSpectrometer/MuonCablings/RPCcablingSim/python/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-# File: RPCcablingSimConfig/__init__.py
-
-__version__ = '1.0.0'
-__author__  = 'alessandro.dimattia@roma1.infn.it'
-__all__ = [ 'RPCcablingSimConfig' ]
--- MuonSpectrometer/MuonCablings/RPCcablingSim/src/components/RPCcablingSim_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "RPCcablingSim/RPCcablingSimSvc.h"
-
-DECLARE_SERVICE_FACTORY( RPCcablingSimSvc )
-
-DECLARE_FACTORY_ENTRIES(RPCcablingSim) {
-    DECLARE_SERVICE( RPCcablingSimSvc );
-}
