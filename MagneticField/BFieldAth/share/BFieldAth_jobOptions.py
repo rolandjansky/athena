942900c17d27661024dfd7b03a20a2909adb9746
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:20:28 +0100

    MagneticField/BFieldAth deleted from master
    Former-commit-id: a618a10aae065387eea3e96aa05b5eafbaa6ec28

--- MagneticField/BFieldAth/share/BFieldAth_jobOptions.py
+++
@@ -1,6 +0,0 @@
-from AthenaCommon.Logging import logging
-logging.getLogger().error("BFieldAth/BFieldAth_jobOptions.py is OBSOLETE as the package is being DISCONTINUED")
-logging.getLogger().error("please use import MagFieldServices.SetupField instead")
-logging.getLogger().error("package BField will be removed from the release on June 13th, 2014")
-
-raise RuntimeError('protectedInclude(\'BFieldAth/BFieldAth_jobOptions.py\') is OBSOLETE, use instead: import MagFieldServices.SetupField')
