0acad7e493a18b80326a0fa312f3e23ad2fc6fcd
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:55 +0100

    Trigger/TrigT1/TrigT1RpcLogicSimTool deleted from master

--- Trigger/TrigT1/TrigT1RpcLogicSimTool/src/components/RpcTriggerHardSimulator_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TrigT1RpcLogicSimTool/RpcTriggerLogicSimTool.h"
-
-DECLARE_TOOL_FACTORY( RpcTriggerLogicSimTool )
-
-DECLARE_FACTORY_ENTRIES( TrigT1RpcLogicSimTool ) {
-  DECLARE_TOOL( RpcTriggerLogicSimTool )
-}
-
