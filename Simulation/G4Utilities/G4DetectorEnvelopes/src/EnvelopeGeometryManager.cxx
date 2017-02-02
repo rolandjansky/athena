01895e20dba687aab834b65850fdb1998a29f24f
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:11:25 +0100

    Simulation/G4Utilities/G4DetectorEnvelopes deleted from master

--- Simulation/G4Utilities/G4DetectorEnvelopes/src/EnvelopeGeometryManager.cxx
+++
@@ -1,8 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "G4DetectorEnvelopes/EnvelopeGeometryManager.h"
-
-EnvelopeGeometryManager* EnvelopeGeometryManager::thePointer=
-	EnvelopeGeometryManager::GetGeometryManager();
