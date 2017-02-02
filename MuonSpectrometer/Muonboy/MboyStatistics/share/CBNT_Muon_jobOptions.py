88b0e5b3d2fdf1b3c6cfea3c449afd5e02a10529
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:47 +0100

    MuonSpectrometer/Muonboy/MboyStatistics deleted from master

--- MuonSpectrometer/Muonboy/MboyStatistics/share/CBNT_Muon_jobOptions.py
+++
@@ -1,1 +0,0 @@
-print "Obsolete file CBNT_Muon_jobOptions.py"
--- MuonSpectrometer/Muonboy/MboyStatistics/src/components/MboyStatistics_entries.cxx
+++
@@ -1,4 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_FACTORY_ENTRIES( MboyStatistics ) {
-}                                                   
