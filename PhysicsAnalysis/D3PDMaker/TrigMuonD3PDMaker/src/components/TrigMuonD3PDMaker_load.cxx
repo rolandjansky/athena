9e3542a74ff3f721869598f15cb9c240584efbcd
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:32:17 +0100

    PhysicsAnalysis/D3PDMaker/TrigMuonD3PDMaker deleted from 20.8
    Former-commit-id: 6b3ef25e968eddc2874ce66c7480283088469b95

--- PhysicsAnalysis/D3PDMaker/TrigMuonD3PDMaker/src/components/TrigMuonD3PDMaker_load.cxx
+++
@@ -1,7 +0,0 @@
-// $Id: TrigMuonD3PDMaker_load.cxx 282498 2010-03-03 15:36:37Z krasznaa $
-
-// Gaudi/Athena include(s):
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-// Declare the library to the framework:
-LOAD_FACTORY_ENTRIES( TrigMuonD3PDMaker )
