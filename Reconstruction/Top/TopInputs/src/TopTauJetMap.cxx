70eab6893e9cdcd941b7a0a70e10c43386a00e74
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:45:16 +0100

    Reconstruction/Top/TopInputs deleted from master

--- Reconstruction/Top/TopInputs/src/TopTauJetMap.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "TopInputs/TopTauJetMap.h"
-
-template class TopRec::TopInputMap<Analysis::TauJetContainer>;
