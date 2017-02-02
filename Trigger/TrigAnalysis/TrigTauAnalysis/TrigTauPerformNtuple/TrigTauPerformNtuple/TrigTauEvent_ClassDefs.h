a872d9c9aafda118f39e6475e8dff0ab09760e1a
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:52 +0100

    Trigger/TrigAnalysis/TrigTauAnalysis/TrigTauPerformNtuple deleted from master

--- Trigger/TrigAnalysis/TrigTauAnalysis/TrigTauPerformNtuple/TrigTauPerformNtuple/TrigTauEvent_ClassDefs.h
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "SGTools/CLASS_DEF.h"
-#include "TrigTauPerformNtuple/TrigTauEvent.h"
-CLASS_DEF(TrigTauEvent, 9001, 0)
