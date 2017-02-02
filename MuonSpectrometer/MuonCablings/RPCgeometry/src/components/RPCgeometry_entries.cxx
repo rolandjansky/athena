d7f642db663a32203199a6ebd3c68b95bdbb8654
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:04:51 +0100

    MuonSpectrometer/MuonCablings/RPCgeometry deleted from master

--- MuonSpectrometer/MuonCablings/RPCgeometry/src/components/RPCgeometry_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "RPCgeometry/RPCgeometrySvc.h"
-
-DECLARE_SERVICE_FACTORY( RPCgeometrySvc )
-
-DECLARE_FACTORY_ENTRIES(RPCgeometry) {
-    DECLARE_SERVICE( RPCgeometrySvc )
-}
