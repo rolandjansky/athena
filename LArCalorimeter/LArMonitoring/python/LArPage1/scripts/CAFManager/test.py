d88877b607e33a59f9d276cb92a1f6bdade5e5be
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:21:23 +0100

    LArCalorimeter/LArMonitoring deleted from master
    Former-commit-id: 9d82ba4dddd0e2154d2a8627e78b1f2cc1250c3d

--- LArCalorimeter/LArMonitoring/python/LArPage1/scripts/CAFManager/test.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from CAFManager import CAFManager
-from JobInputSelectors import FullRunsWithLimit
-mgr = CAFManager()
-jSel = FullRunsWithLimit()
-jSel.configure(mgr.db(), mgr.db().getJobTypeByName('samples2009'), mgr.stream)
-l2 = jSel.getFreeInputs()
-for f in l2[0] : print f.name
