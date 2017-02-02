b73b48e3173ec7e531c47bd46207cd2683367ada
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:37 +0100

    MuonSpectrometer/MuonValidation/MuonInSituPerformance deleted from master

--- MuonSpectrometer/MuonValidation/MuonInSituPerformance/src/components/MuonInSituPerformance_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "MuonInSituPerformance/ZmumuValidator.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_TOOL_FACTORY( ZmumuValidator )
-
-DECLARE_FACTORY_ENTRIES(MuonInSituPerformance)
-{
-DECLARE_TOOL( ZmumuValidator )
-}
