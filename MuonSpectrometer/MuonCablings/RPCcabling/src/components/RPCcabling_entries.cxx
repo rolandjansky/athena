c3eb12d0fee9e69aff4a2de6d3a06c2c16b0a232
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:22 +0100

    MuonSpectrometer/MuonCablings/RPCcabling deleted from master

--- MuonSpectrometer/MuonCablings/RPCcabling/src/components/RPCcabling_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "RPCcabling/RPCcablingSvc.h"
-
-DECLARE_SERVICE_FACTORY( RPCcablingSvc )
-
-DECLARE_FACTORY_ENTRIES(RPCcabling) {
-    DECLARE_SERVICE( RPCcablingSvc )
-}
