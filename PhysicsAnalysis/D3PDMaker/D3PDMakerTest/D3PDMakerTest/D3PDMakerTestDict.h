b1bb3c71a0a02434eee3aeeede55ea0e75a85a77
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:00:31 +0100

    PhysicsAnalysis/D3PDMaker/D3PDMakerTest deleted from 19.2

--- PhysicsAnalysis/D3PDMaker/D3PDMakerTest/D3PDMakerTest/D3PDMakerTestDict.h
+++
@@ -1,8 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "D3PDMakerTest/Obj1.h"
-#include "D3PDMakerTest/Obj3.h"
-#include "D3PDMakerTest/Obj4.h"
-#include "D3PDMakerTest/MapDumper.h"
