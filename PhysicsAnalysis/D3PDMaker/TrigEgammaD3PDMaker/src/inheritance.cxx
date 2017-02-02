79852de43b55e202b4677d3ebad4a62644b00156
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:20:42 +0100

    PhysicsAnalysis/D3PDMaker/TrigEgammaD3PDMaker deleted from master
    Former-commit-id: 6666efd1ef645464ea2ab6cb56252516344074a8

--- PhysicsAnalysis/D3PDMaker/TrigEgammaD3PDMaker/src/inheritance.cxx
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "SGTools/BaseInfo.h"
-#include "TrigParticle/TrigElectron.h"
-#include "EventKernel/INavigable4Momentum.h"
-
-SG_ADD_BASE(TrigElectron, INavigable4Momentum);
