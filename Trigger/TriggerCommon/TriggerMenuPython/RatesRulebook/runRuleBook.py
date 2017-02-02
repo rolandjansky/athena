78d53b347360fc770b91937b23f2349d60e8c3d1
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:20:23 +0100

    Trigger/TriggerCommon/TriggerMenuPython deleted from master
    Former-commit-id: 0eb4f876377bd264929400dbea49a9ec7bdb4ef6

--- Trigger/TriggerCommon/TriggerMenuPython/RatesRulebook/runRuleBook.py
+++
@@ -1,3 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-print "runRuleBook has been moved from the TriggerMenuPython package to scripts directory of the TrigMenuRulebook package"
