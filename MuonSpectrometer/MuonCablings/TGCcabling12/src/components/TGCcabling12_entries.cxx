11271c612808b2ab1f3c0a923e5bbface5cea11b
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:51 +0100

    MuonSpectrometer/MuonCablings/TGCcabling12 deleted from master

--- MuonSpectrometer/MuonCablings/TGCcabling12/src/components/TGCcabling12_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TGCcabling12/TGCcabling12Svc.h"
-
-DECLARE_SERVICE_FACTORY( TGCcabling12Svc )
-
-DECLARE_FACTORY_ENTRIES(TGCcabling12) {
-    DECLARE_SERVICE( TGCcabling12Svc );
-}
