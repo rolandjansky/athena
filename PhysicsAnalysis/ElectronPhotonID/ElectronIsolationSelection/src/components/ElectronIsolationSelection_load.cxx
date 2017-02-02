4e185bd794042f88768ccdd9f71bbe905f8930ef
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:21:10 +0100

    PhysicsAnalysis/ElectronPhotonID/ElectronIsolationSelection deleted from master
    Former-commit-id: ad595c41e10de2599710f091ffb9b3e79b33ccb6

--- PhysicsAnalysis/ElectronPhotonID/ElectronIsolationSelection/src/components/ElectronIsolationSelection_load.cxx
+++
@@ -1,7 +0,0 @@
-// $Id: ElectronIsolationSelection_load.cxx 605132 2014-07-07 12:58:34Z morrisj $
-
-// Gaudi/Athena include(s):
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-// Declare the library to the infrastructure:
-LOAD_FACTORY_ENTRIES( ElectronIsolationSelection )
