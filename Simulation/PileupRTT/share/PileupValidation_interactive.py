0d0717c758ad1ed60aaa1d816ae2070c16b5b272
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:33:13 +0100

    Simulation/PileupRTT deleted from 20.8
    Former-commit-id: f985302839fe54661f926d4a17c3fc328fd20e79

--- Simulation/PileupRTT/share/PileupValidation_interactive.py
+++
@@ -1,4 +0,0 @@
-from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-athenaCommonFlags.FilesInput = ["PileupDigiRTTOutput.RDO.pool.root"]
-#athenaCommonFlags.FilesInput = ["tmpRDO.pool.root"]
-include( "PileupRTT/PileupValidation_topOptions.py" )
