804de45cd0371d69219e6bf33d9466a52b96f7cc
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:00:15 +0100

    Event/EventOverlay/EventOverlayJobTransforms deleted from 19.2

--- Event/EventOverlay/EventOverlayJobTransforms/share/custom_MC.py
+++
@@ -1,8 +0,0 @@
-from AthenaCommon.GlobalFlags import globalflags
-#globalflags.DataSource.set_Value_and_Lock('data') #force to use the data database
-globalflags.DataSource.set_Value_and_Lock('geant4') #force to use the mc database
-
-#from AthenaCommon.DetFlags import DetFlags
-#DetFlags.Calo_setOff()
-#DetFlags.Tile_setOn()
-
--- Event/EventOverlay/EventOverlayJobTransforms/share/postInclude.py
+++
@@ -1,6 +0,0 @@
-import MDTcabling.MDTcablingConfig
-ServiceMgr.MDTcablingSvc.RODfile="AtlasRODmap.data"
-ServiceMgr.MDTcablingSvc.MEZfile="AtlasMEZmap.data"
-ServiceMgr.MDTcablingSvc.ShiftLinks=False
-ServiceMgr.MDTcablingSvc.DisableTagInfoMatch = True
-
