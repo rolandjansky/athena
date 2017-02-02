27342d25604e714f67be08946a27004e96ae345a
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:11:39 +0100

    LArCalorimeter/LArG4/LArG4FastSimSvc deleted from master

--- LArCalorimeter/LArG4/LArG4FastSimSvc/python/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-# File: $Id: __init__.py 448398 2011-07-12 18:38:02Z gsedov $
-
-__author__  = 'Zach Marshall (zmarshal@caltech.edu), Wolfgang Ehrenfeld (wolfgang.ehrenfeld@desy.de)'
-__version__ = '$Revision: 448398 $'
-__all__ = [ 'LArG4FastSimSvcConfig', 'LArG4FastSimSvcInit' ]
-
