cec98c070e4dce4dbd95348f3eeae4a79c3ec4a3
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:32:05 +0100

    PhysicsAnalysis/StandardModelPhys/PhotonAnalysisUtils deleted from master

--- PhysicsAnalysis/StandardModelPhys/PhotonAnalysisUtils/share/photonLikelihoodJobOption.py
+++
@@ -1,6 +0,0 @@
-ToolSvc = Service( "ToolSvc" )
-ToolSvc.PAUphotonLogLikelihood.OutputLevel = INFO
-ToolSvc.PAUphotonLogLikelihood.etabins = [0.,0.8,1.52,2.0,2.5]
-ToolSvc.PAUphotonLogLikelihood.ptbins = [20000.,35000.,70000.,140000.]
-ToolSvc.PAUphotonLogLikelihood.LikelihoodPdfFile = "PAUphotonLikelihoodPdf.root"
-
