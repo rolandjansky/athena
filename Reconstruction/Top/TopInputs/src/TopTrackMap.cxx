4101385b3ad75bf433597b985c630bd81113a48a
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:16:10 +0100

    Reconstruction/Top/TopInputs deleted from 20.1

--- Reconstruction/Top/TopInputs/src/TopTrackMap.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "TopInputs/TopTrackMap.h"
-
-template class TopRec::TopInputMap<Rec::TrackParticleContainer>;
