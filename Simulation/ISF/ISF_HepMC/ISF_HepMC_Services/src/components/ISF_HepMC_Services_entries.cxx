3cd245ee2b8ffb99448846a67386d73c6facdbef
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:42:58 +0100

    Simulation/ISF/ISF_HepMC/ISF_HepMC_Services deleted from master
    Former-commit-id: 04c1bdeb02760ca06549a10a6cae53e5bdba7a47

--- Simulation/ISF/ISF_HepMC/ISF_HepMC_Services/src/components/ISF_HepMC_Services_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "../HepMC_TruthSvc.h"
-
-DECLARE_NAMESPACE_SERVICE_FACTORY( ISF , HepMC_TruthSvc )
-
-DECLARE_FACTORY_ENTRIES( ISF_HepMC_Services ) {
-  DECLARE_NAMESPACE_SERVICE( ISF ,  HepMC_TruthSvc )
-}   
-
