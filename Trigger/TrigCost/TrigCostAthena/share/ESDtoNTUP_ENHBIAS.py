2a0a2130932989f4ea3ec1fd1f52fa8468e31146
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:26 +0100

    Trigger/TrigCost/TrigCostAthena deleted from master

--- Trigger/TrigCost/TrigCostAthena/share/ESDtoNTUP_ENHBIAS.py
+++
@@ -1,8 +0,0 @@
-from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-
-import TrigCostAthena.TrigCostAthenaConfig as costAthena
-alg = costAthena.prepareCostAlg()
-from AthenaCommon.AlgSequence import AlgSequence
-topSeq = AlgSequence()
-topSeq += [alg]
-
