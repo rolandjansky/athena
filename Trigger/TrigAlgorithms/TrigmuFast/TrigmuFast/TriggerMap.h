467689231f3994d56d2b8e1355aea58a3ba991ac
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:41:49 +0100

    Trigger/TrigAlgorithms/TrigmuFast deleted from master

--- Trigger/TrigAlgorithms/TrigmuFast/TrigmuFast/TriggerMap.h
+++
@@ -1,8 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include <string>
-#include "TrigMuonEvent/MuonFeatureDetails.h"
-
-MuonFeatureDetails::AlgoId AlgoMap(const std::string& name);
