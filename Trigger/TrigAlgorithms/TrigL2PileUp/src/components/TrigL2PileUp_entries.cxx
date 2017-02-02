2d4d6210a9c11158e861b5fdc280715be81b495e
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:45:34 +0100

    Trigger/TrigAlgorithms/TrigL2PileUp deleted from 20.7

--- Trigger/TrigAlgorithms/TrigL2PileUp/src/components/TrigL2PileUp_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TrigL2PileUp/TrigL2PileUp.h"
-
-
-DECLARE_ALGORITHM_FACTORY( TrigL2PileUp )
-
-DECLARE_FACTORY_ENTRIES( TrigL2PileUp ) {
-    DECLARE_ALGORITHM( TrigL2PileUp )
-}
