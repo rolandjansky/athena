e431008e490755c592a3c343382b7059b96920c4
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:52:18 +0100

    HLT/Trigger/TrigControl/TrigExamples/TrigExMTHelloWorld deleted from 20.7

--- HLT/Trigger/TrigControl/TrigExamples/TrigExMTHelloWorld/src/components/MTHelloWorld_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "TrigExMTHelloWorld/MTHelloWorld.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_ALGORITHM_FACTORY( MTHelloWorld )
-
-DECLARE_FACTORY_ENTRIES(TrigExMTHelloWorld) {
-    DECLARE_ALGORITHM( MTHelloWorld );
-}
