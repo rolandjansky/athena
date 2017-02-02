34de5a81c5e881db4903940d3d1ba22617c4556f
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:54:09 +0100

    Offloading/OffloadSvc deleted from 21.0
    Former-commit-id: 2c2f00fc959bce48a1b6477efb7be245b121f1ef

--- Offloading/OffloadSvc/src/components/OffloadSvc_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "OffloadSvc/OffloadSvc.h"
-
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_SERVICE_FACTORY( OffloadSvc )
-
-DECLARE_FACTORY_ENTRIES( OffloadSvc ) {
-  DECLARE_SERVICE( OffloadSvc )
-}
