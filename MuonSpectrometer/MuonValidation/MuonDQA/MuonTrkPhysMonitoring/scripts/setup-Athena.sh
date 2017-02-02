c4daef94c00eb0af579e9d4351894b3774566daa
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:43:01 +0100

    MuonSpectrometer/MuonValidation/MuonDQA/MuonTrkPhysMonitoring deleted from master
    Former-commit-id: a359fba381f0550ca828cc33ccf17a26a9ec1208

--- MuonSpectrometer/MuonValidation/MuonDQA/MuonTrkPhysMonitoring/scripts/setup-Athena.sh
+++
@@ -1,3 +0,0 @@
-export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
-alias asetup='source $AtlasSetup/scripts/asetup.sh'
-asetup $1,here
--- MuonSpectrometer/MuonValidation/MuonDQA/MuonTrkPhysMonitoring/src/components/MuonTrkPhysMonitoring_entries.cxx
+++
@@ -1,9 +0,0 @@
-// #include "GaudiKernel/DeclareFactoryEntries.h"
-// #include "MuonTrkPhysMonitoring/MuonTrkPhysMonitoring.h"
- 
-// DECLARE_TOOL_FACTORY(MuonTrkPhysMonitoring)
- 
-// DECLARE_FACTORY_ENTRIES(MuonTrkPhysMonitoring)
-// {
-// 	   DECLARE_ALGTOOL(MuonTrkPhysMonitoring)
-// } 
