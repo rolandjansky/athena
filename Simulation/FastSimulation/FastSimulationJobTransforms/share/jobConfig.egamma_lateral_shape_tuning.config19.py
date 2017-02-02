cab8ec2eccf8d691b893b0ec88c1d840cc8429bf
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:49:28 +0100

    Simulation/FastSimulation/FastSimulationJobTransforms deleted from master

--- Simulation/FastSimulation/FastSimulationJobTransforms/share/jobConfig.egamma_lateral_shape_tuning.config19.py
+++
@@ -1,4 +0,0 @@
-from FastCaloSim.FastCaloSimConf import FastShowerCellBuilderTool
-theFastShowerCellBuilderTool=FastShowerCellBuilderTool()
-
-theFastShowerCellBuilderTool.AdditionalParticleParametrizationFileNames.insert(0,"L1_L2_egamma_corr.config19.root")
