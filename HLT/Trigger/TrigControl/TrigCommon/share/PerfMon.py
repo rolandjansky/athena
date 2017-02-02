e8de4eea2564dc271dc7ddb8487499b6b52b71fb
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:52:10 +0100

    HLT/Trigger/TrigControl/TrigCommon deleted from 21.0
    Former-commit-id: 73ca39df8cd9d1bf3d2dd4d39cce71ff8d9393b4

--- HLT/Trigger/TrigControl/TrigCommon/share/PerfMon.py
+++
@@ -1,5 +0,0 @@
-# Job option fragment to enable PerfMon
-from PerfMonComps.PerfMonFlags import jobproperties
-jobproperties.PerfMonFlags.doMonitoring = True
-jobproperties.PerfMonFlags.doPersistencyMonitoring = False
-
