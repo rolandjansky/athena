2c8d8721a522f06e2ed20d5f1716ede370f4d1f3
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:11:40 +0100

    Trigger/TrigValidation/TrigP1Test deleted from master

--- Trigger/TrigValidation/TrigP1Test/share/testPrescaleChange_postconfig.py
+++
@@ -1,7 +0,0 @@
-# Change the partition name in HLTConfigSvc to avoid collissions between
-# parallel running ATN tests
-from GaudiPython.Bindings import iProperty
-from TrigP1Test.PrescaleChange import partitionName
-
-prop = iProperty('HLTConfigSvc')
-prop.PartitionName = partitionName
