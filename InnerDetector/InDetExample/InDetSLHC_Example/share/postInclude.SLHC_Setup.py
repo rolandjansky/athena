f3f4747470eca65912ec5e54a21cd0a9fba843d1
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:04:56 +0100

    InnerDetector/InDetExample/InDetSLHC_Example deleted from master

--- InnerDetector/InDetExample/InDetSLHC_Example/share/postInclude.SLHC_Setup.py
+++
@@ -1,9 +0,0 @@
-#--------------------------------------------------------------
-# SLHC setup
-#--------------------------------------------------------------
-# set the path variables consistently
-from InDetSLHC_Example.SLHC_Setup import SLHC_Setup
-SLHC_Setup = SLHC_Setup()
-
-if rec.OutputFileNameForRecoStep() == 'RAWtoESD' or rec.OutputFileNameForRecoStep() == 'ESDtoDPD':
-    ServiceMgr.PixelOfflineCalibSvc.HDCFromCOOL = False  
