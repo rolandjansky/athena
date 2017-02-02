5481a6728302dd9bdfcf40f87ca5585bfec984a0
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:32:18 +0100

    Simulation/G4Sim/FADS/FadsAlg deleted from 20.8
    Former-commit-id: 1b8283cc4f7220dd29062e66e2b1b1cfbd0ef2d1

--- Simulation/G4Sim/FADS/FadsAlg/src/components/FadsAlg_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "FadsAlg/FadsAlg.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_ALGORITHM_FACTORY( FadsAlg )
-
-DECLARE_FACTORY_ENTRIES(FadsAlg) {
-DECLARE_ALGORITHM( FadsAlg );
-}
