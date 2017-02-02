6671a30dd02be8994f409cafa092772497a7dc51
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:21:18 +0100

    MuonSpectrometer/MuonCalib/MuonCalibJobTransforms deleted from 20.8
    Former-commit-id: 2b50ebd249021d84ff622d6d1e1cbd4117b5cc77

--- MuonSpectrometer/MuonCalib/MuonCalibJobTransforms/share/rpc_trigNtuple.py
+++
@@ -1,8 +0,0 @@
-from MuonCalibAlgs.MuonCalib import MuonCalibAlg
-
-MuonCalibAlg.doMDTs = False
-MuonCalibAlg.doCSCs = False
-MuonCalibAlg.doRPCs = True
-MuonCalibAlg.doTGCs = False
-
-MuonCalibAlg.doTGCCoinData = False
