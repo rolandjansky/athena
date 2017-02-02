5d1f613b30ce857d81d5ffb67bc34550c5d5ac8a
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:20:19 +0100

    PhysicsAnalysis/D3PDMaker/TrigTauD3PDMaker deleted from master
    Former-commit-id: cdd889fe848480c8500a8fd5e975f82d6c76c98c

--- PhysicsAnalysis/D3PDMaker/TrigTauD3PDMaker/python/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-import TrigTauD3PDMakerConf
-for k, v in TrigTauD3PDMakerConf.__dict__.items():
-    if k.startswith ('D3PD__'):
-        globals()[k[6:]] = v
