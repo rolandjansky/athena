c82969dcfe24610b92cf20e3f40cdd869eec434d
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:32:15 +0100

    Trigger/TriggerCommon/TriggerMenuPython deleted from 20.8
    Former-commit-id: 9aa004664d2115e9974df7b169243ad79d8e0a84

--- Trigger/TriggerCommon/TriggerMenuPython/RatesRulebook/processRules.py
+++
@@ -1,3 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-print "processRules has been moved from the TriggerMenuPython package to the python directory of the TrigMenuRulebook package"
