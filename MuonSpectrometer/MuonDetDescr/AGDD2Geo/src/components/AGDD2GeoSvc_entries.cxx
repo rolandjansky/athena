12eb70e991e0efee47855018f1fa88851f600a1e
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:00:24 +0100

    MuonSpectrometer/MuonDetDescr/AGDD2Geo deleted from 19.2

--- MuonSpectrometer/MuonDetDescr/AGDD2Geo/src/components/AGDD2GeoSvc_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "AGDD2Geo/AGDD2GeoSvc.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_SERVICE_FACTORY(AGDD2GeoSvc)
-
-DECLARE_FACTORY_ENTRIES(AGDD2Geo) {
-    DECLARE_SERVICE   ( AGDD2GeoSvc )
-}
-
