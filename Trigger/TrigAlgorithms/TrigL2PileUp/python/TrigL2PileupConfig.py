78c8ce8c49c8dc42dc1e786ee29d676995459a47
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:04:57 +0100

    Trigger/TrigAlgorithms/TrigL2PileUp deleted from master

--- Trigger/TrigAlgorithms/TrigL2PileUp/python/TrigL2PileupConfig.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from TrigL2PileUp.TrigL2PileUpConf import TrigL2PileUp
-
-class TrigL2PileUp_Fex ( TrigL2PileUp ) :
-    __slots__ = []
-    def __init__ (self, name="TrigL2PileUp_Fex"):
-        super(TrigL2PileUp_Fex, self).__init__(name)
