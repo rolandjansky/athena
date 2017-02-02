70b1a2812dc960b77779b229f207e506f0b11aff
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:21:14 +0100

    AtlasPolicy deleted from master
    Former-commit-id: 01fffd7b889a1d69d3b8014123d8a8f82f225d5e

--- AtlasPolicy/cmt/llp.sh
+++
@@ -1,9 +0,0 @@
-
-unset LLP
-for p in `echo ${CMTPATH} | sed -e 's#:# #g'`; do 
-  if test -d ${p}/InstallArea; then
-    export LLP=${LLP}:"${p}/InstallArea/${CMTCONFIG}/lib"
-  fi
-done
-echo ${LLP}
-
