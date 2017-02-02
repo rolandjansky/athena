a1b150ce1c1054653888fbe59f8969f30c1e05c2
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 23:45:33 +0100

    Trigger/TrigAlgorithms/TrigMuGirl deleted from 20.7

--- Trigger/TrigAlgorithms/TrigMuGirl/src/components/TrigMuGirl_load.cxx
+++
@@ -1,8 +0,0 @@
-/***************************************************************************
-    Authors: E. Reinherz-Aronis, A. Kreisel 
-             based on MuGirl by Shlomit Tarem
-***************************************************************************/
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES( TrigMuGirl )
-
