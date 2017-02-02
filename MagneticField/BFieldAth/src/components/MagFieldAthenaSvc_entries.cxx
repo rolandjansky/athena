b0d6f86205a10421fa6c3843d569d3381682c1b6
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 00:32:22 +0100

    MagneticField/BFieldAth deleted from 20.8
    Former-commit-id: c98f86c1100caccd2de7844c395a1605362972e4

--- MagneticField/BFieldAth/src/components/MagFieldAthenaSvc_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "../MagFieldAthenaSvc.h"
-
-DECLARE_SERVICE_FACTORY( MagFieldAthenaSvc )
- 
-DECLARE_FACTORY_ENTRIES(BFieldAth) {
-    DECLARE_SERVICE( MagFieldAthenaSvc )
-}
- 
