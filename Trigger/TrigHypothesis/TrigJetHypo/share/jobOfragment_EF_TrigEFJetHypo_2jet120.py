b30b6ccb0f3984a9a4bc50f475052bf11dcb1176
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 22:36:24 +0100

    Trigger/TrigHypothesis/TrigJetHypo deleted from master
    Former-commit-id: 63e3f3953bd14b0b5b8c0563f6fa09bffbd4d910

--- Trigger/TrigHypothesis/TrigJetHypo/share/jobOfragment_EF_TrigEFJetHypo_2jet120.py
+++
@@ -1,4 +0,0 @@
-theApp.Dlls += [ "TrigJetHypo" ]
-TrigEFJetHypo_2jet120_EF = Algorithm( "TrigEFJetHypo_2jet120_EF" )
-TrigEFJetHypo_2jet120_EF.Etcut = 120*GeV #cut for 2j120!
-
--- Trigger/TrigHypothesis/TrigJetHypo/share/jobOfragment_L2_TrigL2JetHypo_3jet65.py
+++
@@ -1,3 +0,0 @@
-theApp.Dlls += [ "TrigJetHypo" ]
-TrigL2JetHypo_3jet65_L2 = Algorithm( "TrigL2JetHypo_3jet65_L2" )
-TrigL2JetHypo_3jet65_L2.Etcut_L2 = 65*GeV #cut for 3jet !
