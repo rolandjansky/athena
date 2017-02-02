f781f54f6312059ae53f854d6f3bd4a8d7c5226e
Graeme Stewart <graemes.cern@gmail.com>
2017-02-02 21:45:13 +0100

    Reconstruction/Top/TopEvent deleted from master

--- Reconstruction/Top/TopEvent/src/TTbarJJTopContainer.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "TopEvent/TTbarJJTopContainer.h"
-
-TopRec::TTbarJJTopContainer::TTbarJJTopContainer():std::vector<TTbarJJTop>(){;}
--- Reconstruction/Top/TopEvent/src/TopJetHadronicContainer.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "TopEvent/TopJetHadronicContainer.h"
-
-TopRec::TopJetHadronicContainer::TopJetHadronicContainer():std::vector<TopJetHadronic>(){;}	
