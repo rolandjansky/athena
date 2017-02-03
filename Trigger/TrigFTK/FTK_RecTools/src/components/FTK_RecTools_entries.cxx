ca48b53a7b907e88d797ed7a6e91a058611b83d9
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:27:10 +0100

    Trigger/TrigFTK/FTK_RecTools deleted from 20.20

--- Trigger/TrigFTK/FTK_RecTools/src/components/FTK_RecTools_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-#include "FTK_RecTools/FTK_VertexFinderTool.h"
-
-DECLARE_TOOL_FACTORY(FTK_VertexFinderTool)
-
-DECLARE_FACTORY_ENTRIES(FTK_RecTools) {
-  DECLARE_TOOL(FTK_VertexFinderTool)
-}
