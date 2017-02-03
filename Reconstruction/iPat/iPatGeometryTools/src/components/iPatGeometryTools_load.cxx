4ee01573d4ec68c9d280e085b778eccad0cc0eff
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:26:38 +0100

    Reconstruction/iPat/iPatGeometryTools deleted from 20.20

--- Reconstruction/iPat/iPatGeometryTools/src/components/iPatGeometryTools_load.cxx
+++
@@ -1,8 +0,0 @@
-/***************************************************************************
- iPatGeometryTools
- ATLAS Collaboration
- ***************************************************************************/
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES( iPatGeometryTools )
