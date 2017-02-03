f9c5148784b5e064da25d05424872a947b21a7d6
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:12:17 +0100

    PhysicsAnalysis/D3PDMaker/TrigMissingETD3PDMaker deleted from 20.11

--- PhysicsAnalysis/D3PDMaker/TrigMissingETD3PDMaker/src/components/TrigMissingETD3PDMaker_load.cxx
+++
@@ -1,7 +0,0 @@
-// $Id: TrigMissingETD3PDMaker_load.cxx 278517 2010-02-10 16:46:44Z krasznaa $
-
-// Gaudi/Athena include(s):
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-// Declare the library to the framework:
-LOAD_FACTORY_ENTRIES( TrigMissingETD3PDMaker )
