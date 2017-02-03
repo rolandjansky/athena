42f80451dfc67c7357d9200d1e94e1efa053709c
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:27:02 +0100

    Reconstruction/egamma/egammaMVACalib deleted from 20.20

--- Reconstruction/egamma/egammaMVACalib/src/components/egammaMVACalib_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "egammaMVACalib/egammaMVATool.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_TOOL_FACTORY( egammaMVATool )
-
-DECLARE_FACTORY_ENTRIES( egammaMVACalib ){
-  DECLARE_TOOL( egammaMVATool );
-}
