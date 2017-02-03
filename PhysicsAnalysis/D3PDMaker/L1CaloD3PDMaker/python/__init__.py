2e4fbd199c03d2379c7fbba05718027df223d359
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:12:20 +0100

    PhysicsAnalysis/D3PDMaker/L1CaloD3PDMaker deleted from 20.11

--- PhysicsAnalysis/D3PDMaker/L1CaloD3PDMaker/python/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-# -*- coding: utf-8 -*-
-import L1CaloD3PDMakerConf
-for k, v in L1CaloD3PDMakerConf.__dict__.items():
-    if k.startswith ('D3PD__'):
-        globals()[k[6:]] = v
--- PhysicsAnalysis/D3PDMaker/L1CaloD3PDMaker/share/testTier1Production.sh
+++
@@ -1,7 +0,0 @@
-### Source this file to test Tier-1 production of L1Calo D3PD
-Reco_trf.py inputESDFile=esd.root autoConfiguration=everything maxEvents=10  outputNTUP_L1CALOFile=d3pd.root
-
-# Reco_trf.py inputESDFile=esd.root autoConfiguration=everything maxEvents=10  outputNTUP_L1CALOPROBFile=d3pd.root
-
-### For a full python trace, please append the above line with:
-### --athenaopts="-s"
