1825bf786d0d2c975a0044eee7eca4928fbcc47d
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:27:11 +0100

    Simulation/G4Utilities/MCTruthAlgs deleted from 20.20

--- Simulation/G4Utilities/MCTruthAlgs/src/components/MCTruthAlgs_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "MCTruthAlgs/TrackRecordFilter.h"
-
-DECLARE_ALGORITHM_FACTORY( TrackRecordFilter ) 
-
-DECLARE_FACTORY_ENTRIES(MCTruthAlgs) 
-{
-                DECLARE_ALGORITHM(TrackRecordFilter)
-}
