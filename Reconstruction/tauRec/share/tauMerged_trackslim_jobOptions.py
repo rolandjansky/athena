d42aca5b9a8078112904ce557796b99e9703c32f
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:26:55 +0100

    Reconstruction/tauRec deleted from 20.20

--- Reconstruction/tauRec/share/tauMerged_trackslim_jobOptions.py
+++
@@ -1,7 +0,0 @@
-from RecExConfig.RecFlags import rec 
-if rec.ScopingLevel()<=3:
-  from ParticleBuilderOptions.AODFlags import AODFlags
-  if AODFlags.TauTrackSlimmer:
-    from tauRec.tauTrackSlimmer import tauTrackSlimmer
-    tauTrackSlimmer()
-
