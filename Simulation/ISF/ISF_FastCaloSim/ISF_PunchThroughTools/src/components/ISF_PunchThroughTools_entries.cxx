7960e5ba150ef91f3b797deae7d2ead70ae4303a
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:27:28 +0100

    Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools deleted from 20.20

--- Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/components/ISF_PunchThroughTools_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "ISF_PunchThroughTools/PunchThroughTool.h"
-
-DECLARE_NAMESPACE_TOOL_FACTORY( ISF,PunchThroughTool)
-DECLARE_FACTORY_ENTRIES( ISF_PunchThroughTools ) {
-  DECLARE_NAMESPACE_TOOL( ISF,PunchThroughTool)
-}
-
