4a78a38a02824e3e369a0af3f5bab23a76451626
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:27:03 +0100

    Tracking/TrkTools/TrkRIO_OnTrackCreator deleted from 20.20

--- Tracking/TrkTools/TrkRIO_OnTrackCreator/share/RIO_OnTrackErrorScaling.py
+++
@@ -1,6 +0,0 @@
-print "***** WARNING ******************************************************************"
-print " The file TrkRIO_OnTrackCreator/RIO_OnTrackErrorScaling.py"
-print "          is currently obsolete."
-print " To use scaled errors in the track fit and reconstruction, submit the scaling"
-print " values as tag to COOL. Instructions are in the TrkRIO_OnTrackCreator package."
-print "********************************************************************************"
