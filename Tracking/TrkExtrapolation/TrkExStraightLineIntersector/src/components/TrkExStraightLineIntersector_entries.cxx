b43ef55837191290d9053baa03136babe77b4bb3
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:26:52 +0100

    Tracking/TrkExtrapolation/TrkExStraightLineIntersector deleted from 20.20

--- Tracking/TrkExtrapolation/TrkExStraightLineIntersector/src/components/TrkExStraightLineIntersector_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TrkExStraightLineIntersector/StraightLineIntersector.h"
-
-DECLARE_NAMESPACE_TOOL_FACTORY( Trk, StraightLineIntersector )
-DECLARE_FACTORY_ENTRIES( TrkExStraightLineIntersector )
-{
-    DECLARE_NAMESPACE_TOOL( Trk, StraightLineIntersector )
-}
