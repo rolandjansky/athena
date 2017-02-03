161073c4178e533e043e03fc6d08124f393ffee7
Graeme Stewart <graemes.cern@gmail.com>
2017-02-03 01:12:18 +0100

    Trigger/TrigAnalysis/TrigMinBiasAnalysis/TrigMinBiasNtuple deleted from 20.11

--- Trigger/TrigAnalysis/TrigMinBiasAnalysis/TrigMinBiasNtuple/src/components/TrigMinBiasNtuple_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h" 
-#include "TrigMinBiasNtuple/TrigMinBiasNtupleAlg.h"
- 
-DECLARE_ALGORITHM_FACTORY(TrigMinBiasNtupleAlg)
- 
-DECLARE_FACTORY_ENTRIES(TrigMinBiasNtuple) {
-   DECLARE_ALGORITHM(TrigMinBiasNtupleAlg)
-}
