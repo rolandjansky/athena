0a4b361a04d4c1eaf08677f66e57016bf009ed12
Simon Viel <simon.viel@cern.ch>
2017-02-06 22:01:58 +0100

    Add RTT for InclBrl4_OptRing, Remove failing AthMP Reg RTT tests (InDetSLHC_Example-00-02-57)

---
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/postInclude.SLHC_Setup_IExtBrl_4_OptRing.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------
+# SLHC setup
+#--------------------------------------------------------------
+
+# build GeoModel geometry
+from InDetSLHC_Example.SLHC_Setup_IExtBrl_4_OptRing import SLHC_Setup
+SLHC_Setup = SLHC_Setup()
+
+include("InDetSLHC_Example/postInclude.SLHC_Setup_Common.py")
