0531271f88580c3415e5264762fd650c8b985d7c
Zachary Louis Marshall <zach.marshall@cern.ch>
2017-02-07 04:52:21 +0100

    Missed a line for Sim_tf.py -- adding to the executor (SimuJobTransforms-00-33-05)
    	* Missed a line for Sim_tf.py -- adding to the executor
    	* Tagging: SimuJobTransforms-00-33-05
    
    2017-01-31  Zach Marshall <ZLMarshall@lbl.gov>
    
    	* Removing underscore from naming
    	* Removing duplicate group definition
    	* Tagging: SimuJobTransforms-00-33-04
    
    2017-01-31  Zach Marshall <ZLMarshall@lbl.gov>
    
    	* python/simTrfArgs.py : Only input TR and output TR, not separate cosmic
    	/ cavern TR files
    	* python/SimTransformUtils.py : Adding a sub-step to deal with jobs that
    	write out TRs and jobs that read in TRs separately
    	* share/skeleton.EVGENtoHIT_ISF.py : updating to configure only based on
    	TR file requirements and whether beam config is cosmic, rather than using
    	type of TR file as a hint
    	* share/skeleton.EVGENtoHIT_MC12.py : updating to configure only based on
    ...
    (Long ChangeLog diff - truncated)

--- Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_MC12.py
+++ Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_MC12.py
@@ -45,10 +45,8 @@ if hasattr(runArgs, "inputFile"):
 # We don't expect both inputFile and inputEVNT*File to be specified
 if hasattr(runArgs, "inputEVNTFile"):
     setInputEvgenFileJobProperties( runArgs.inputEVNTFile )
-elif hasattr(runArgs, "inputEVNT_COSMICSFile"):
-    setInputEvgenFileJobProperties( runArgs.inputEVNT_COSMICSFile )
-elif hasattr(runArgs, "inputEVNT_CAVERNFile"):
-    setInputEvgenFileJobProperties( runArgs.inputEVNT_CAVERNFile )
+elif hasattr(runArgs, "inputEVNT_TRFile"):
+    setInputEvgenFileJobProperties( runArgs.inputEVNT_TRFile )
 elif hasattr(runArgs, "inputEVNT_STOPPEDFile"):
     setInputEvgenFileJobProperties( runArgs.inputEVNT_STOPPEDFile )
 elif jobproperties.Beam.beamType.get_Value() == 'cosmics':
@@ -61,7 +59,7 @@ else:
 ## Handle cosmics configs
 if jobproperties.Beam.beamType.get_Value() == 'cosmics':
     simFlags.load_cosmics_flags()
-    if hasattr(runArgs, "inputEVNT_COSMICSFile"):
+    if hasattr(runArgs, "inputEVNT_TRFile"):
         if simFlags.CosmicFilterVolumeName.statusOn and simFlags.CosmicFilterVolumeName.get_Value() != "Muon":
             atlasG4log.warning("Filtering was already done. Using CosmicFilterVolumeName=Muon rather than "
                                "provided value (%s)" % str(runArgs.CosmicFilterVolumeName))
@@ -109,11 +107,12 @@ if hasattr(runArgs, "inputEVNT_STOPPEDFile"):
     include('SimulationJobOptions/preInclude.ReadStoppedParticles.py')
 
 # Avoid command line preInclude for cavern background
-if hasattr(runArgs, "inputEVNT_CAVERNFile"):
-    include('SimulationJobOptions/preInclude.G4ReadCavern.py')
-if hasattr(runArgs, "outputEVNT_CAVERNTRFile"):
-    include('SimulationJobOptions/preInclude.G4WriteCavern.py')
-    
+if jobproperties.Beam.beamType.get_Value() != 'cosmics':
+    if hasattr(runArgs, "inputEVNT_TRFile"):
+        include('SimulationJobOptions/preInclude.G4ReadCavern.py')
+    if hasattr(runArgs, "outputEVNT_TRFile"):
+        include('SimulationJobOptions/preInclude.G4WriteCavern.py')
+
 # Avoid command line preInclude for event service
 if hasattr(runArgs, "eventService") and runArgs.eventService:
     include('AthenaMP/AthenaMP_EventService.py')
@@ -203,11 +202,11 @@ elif hasattr(runArgs,'jobNumber'):
 ## Handle cosmics track record
 from AthenaCommon.BeamFlags import jobproperties
 if jobproperties.Beam.beamType.get_Value() == 'cosmics':
-    if hasattr(runArgs, "inputEVNT_COSMICSFile"):
+    if hasattr(runArgs, "inputEVNT_TRFile"):
         simFlags.ReadTR = athenaCommonFlags.PoolEvgenInput()[0]
     else:
-        if hasattr(runArgs, "outputEVNT_COSMICSTRFile"):
-            simFlags.WriteTR = runArgs.outputEVNT_COSMICSTRFile
+        if hasattr(runArgs, "outputEVNT_TRFile"):
+            simFlags.WriteTR = runArgs.outputEVNT_TRFile
         include( 'CosmicGenerator/jobOptions_ConfigCosmicProd.py' )
 
 
