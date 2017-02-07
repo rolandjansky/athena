a0227aa420b82df24e32ac20cc3bfe0bdb7cce67
Edward Moyse <edward.moyse@gmail.com>
2017-02-07 16:26:54 +0100

    Adding docs directory
    Former-commit-id: def5a316a0087d2f528ffafdbaf78029268cfa0b

---
+++ docs/mainpage.h
@@ -0,0 +1,10 @@
+/*! \mainpage ATLAS offline software
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the documentation for ATLAS offline software.
+ *
+ * Please see the https://twiki.cern.ch/twiki/bin/view/AtlasComputing/AtlasComputing twiki for more information, or look at the documentation:
+ * https://atlassoftwaredocs.web.cern.ch
+ *
+ */
