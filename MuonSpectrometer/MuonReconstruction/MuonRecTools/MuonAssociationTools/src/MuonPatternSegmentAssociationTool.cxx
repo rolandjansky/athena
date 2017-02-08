2e639f3fe4947869302139621ff38a356b5cd5fd
Scott Snyder <scott.snyder@cern.ch>
2017-02-08 03:30:37 +0100

    'Fix faulty logic leading to null pointer dereferences.' (MuonAssociationTools-01-01-02)
    	* Tagging MuonAssociationTools-01-01-02.
    	* src/MuonPatternSegmentAssociationTool.cxx: Fix faulty logic
    	leading to null pointer dereferences.
    
    2016-12-01 Edward.Moyse@cern.ch
    	* Some fixes for const methods & new interface using EDM from MuonEDM_AssociationTools.
    	* MuonAssociationTools-01-01-01
    
    2016-11-28 Edward.Moyse@cern.ch
    	* Shift map to SG in preparation for making tool MT compatible.
    	* MuonAssociationTools-01-01-00
    
    
    Former-commit-id: 9dfc627bd10a2f9de5020b44cdca42e4bd78e134

--- MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonAssociationTools/src/MuonPatternSegmentAssociationTool.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonAssociationTools/src/MuonPatternSegmentAssociationTool.cxx
@@ -9,6 +9,7 @@
 #include "MuonAssociationTools/MuonPatternSegmentAssociationTool.h"
 #include "MuonSegment/MuonSegmentCombination.h" // Not sure these are actually needed. EJWM
 #include "MuonPattern/MuonPatternCombination.h"
+#include <stdexcept>
 
 //================ Constructor =================================================
 
@@ -16,7 +17,8 @@ Muon::MuonPatternSegmentAssociationTool::MuonPatternSegmentAssociationTool(const
               const std::string& n,
               const IInterface*  p )
   :
-  AthAlgTool(t,n,p)
+  AthAlgTool(t,n,p),
+  m_key("MuonPatternSegmentAssociationMap")
 {
   declareInterface<IMuonPatternSegmentAssociationTool>(this);
 
@@ -36,6 +38,7 @@ StatusCode Muon::MuonPatternSegmentAssociationTool::initialize()
 {
   
   ATH_CHECK( AthAlgTool::initialize() );
+  declareProperty( "AssociationMapKey",     m_key);
 
   ATH_MSG_INFO("initialize() successful in " << name() );
   return StatusCode::SUCCESS;
@@ -51,3 +54,66 @@ StatusCode Muon::MuonPatternSegmentAssociationTool::finalize()
 
 //============================================================================================
 
+inline 
+const Muon::IMuonPatternSegmentAssociationTool::AssociationMap& 
+Muon::MuonPatternSegmentAssociationTool::map() const {
+   
+   IMuonPatternSegmentAssociationTool::AssociationMap* assocmap = evtStore()->tryRetrieve<IMuonPatternSegmentAssociationTool::AssociationMap>(m_key);
+   if (!assocmap) {
+   	 assocmap = new IMuonPatternSegmentAssociationTool::AssociationMap;
+	 if (evtStore()->record(assocmap, m_key).isFailure() ){
+		 ATH_MSG_ERROR("Could not record association map at "+m_key);
+		 throw std::runtime_error("Unable to record AssociationMap in SG.");
+	 }
+   }
+   return nonconst_map();
+}
+
+inline 
+Muon::IMuonPatternSegmentAssociationTool::AssociationMap& 
+Muon::MuonPatternSegmentAssociationTool::nonconst_map() const {
+   
+   IMuonPatternSegmentAssociationTool::AssociationMap* assocmap = evtStore()->tryRetrieve<IMuonPatternSegmentAssociationTool::AssociationMap>(m_key);
+   if (!assocmap) {
+   	 assocmap = new IMuonPatternSegmentAssociationTool::AssociationMap;
+	 if (evtStore()->record(assocmap, m_key).isFailure() ){
+		 ATH_MSG_ERROR("Could not record association map at "+m_key);
+		 throw std::runtime_error("Unable to record AssociationMap in SG.");
+	 }
+   }
+   return *assocmap;
+}
+
+inline 
+void 
+Muon::MuonPatternSegmentAssociationTool::insert( const Muon::MuonSegmentCombination* segmentCombi, 
+                                                 const Muon::MuonPatternCombination* associatedPatternCombi) const
+{
+    //std::pair<IMuonPatternSegmentAssociationTool::AssociationMap::iterator, bool> retVal;
+    nonconst_map().insert(std::make_pair(segmentCombi, associatedPatternCombi) );
+    return; // true if successfully inserted into map
+}
+
+
+
+inline 
+Muon::IMuonPatternSegmentAssociationTool::AssociationMapRange 
+Muon::MuonPatternSegmentAssociationTool::find(const Muon::MuonSegmentCombination* segmentCombi) const
+{
+    return map().equal_range(segmentCombi);
+}
+
+inline 
+unsigned int 
+Muon::MuonPatternSegmentAssociationTool::count(const MuonSegmentCombination* segmentCombi) const
+{
+    return map().count(segmentCombi);    
+}
+
+
+inline 
+void
+Muon::MuonPatternSegmentAssociationTool::reset() const {
+	// EJWM - do we need this now?
+   nonconst_map().clear();
+}
