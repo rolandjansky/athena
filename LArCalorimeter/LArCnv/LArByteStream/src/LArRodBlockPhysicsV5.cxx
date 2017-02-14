e8001481aae2c8f160f83459abc14311e17193a9
Scott Snyder <scott.snyder@cern.ch>
2017-02-14 19:41:09 +0100

    'Coverity 108500, 108501.' (LArByteStream-01-21-56)
    	* Tagging LArByteStream-01-21-56.
    	* Coverity 108500, 108501.
    
    
    Former-commit-id: b33aea4a667ac1aa061608965e0edbcc04793564

--- LArCalorimeter/LArCnv/LArByteStream/src/LArRodBlockPhysicsV5.cxx
+++ LArCalorimeter/LArCnv/LArByteStream/src/LArRodBlockPhysicsV5.cxx
@@ -36,7 +36,9 @@ union ShortLong {
 };
 }
 
-LArRodBlockPhysicsV5::LArRodBlockPhysicsV5() : LArRodBlockStructure()
+LArRodBlockPhysicsV5::LArRodBlockPhysicsV5()
+  : LArRodBlockStructure(),
+    m_onlineHelper(nullptr)
 {
   m_iHeadBlockSize=endtag/2; // The implicit cast rounds down to the right size 
   m_fixedGain=CaloGain::LARNGAIN;
