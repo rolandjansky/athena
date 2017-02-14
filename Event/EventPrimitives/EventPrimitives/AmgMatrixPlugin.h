60ae06a9127da16ed70bec33e36084ea98adf9ae
Scott Snyder <scott.snyder@cern.ch>
2017-02-14 17:22:56 +0100

    Fix compilation warning (unused variable). (EventPrimitives-00-00-50)
    	* Tagging EventPrimitives-00-00-50.
    	* EventPrimitives/AmgMatrixPlugin.h: Fix compilation warning
    	(unused variable).
    
    
    Former-commit-id: 8b541588501362336bf93adb106e8609348f3004

--- Event/EventPrimitives/EventPrimitives/AmgMatrixPlugin.h
+++ Event/EventPrimitives/EventPrimitives/AmgMatrixPlugin.h
@@ -30,7 +30,6 @@
     /**pseudorapidity method */
     inline Scalar eta() const {
         EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE(Matrix, 3)
-        const Scalar* data = this->data();
         double m = std::sqrt((*this).x() * (*this).x() + (*this).y() * (*this).y() + (*this).z() * (*this).z());;
         return 0.5*std::log((m+(*this).z())/(m-(*this).z()));
     }
