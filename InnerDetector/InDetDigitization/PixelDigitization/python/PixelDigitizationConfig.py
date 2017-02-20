6a25d5bdf9e50a3fb445bb195ea88ec92d67a327
Soshi Tsuno <soshi.tsuno@cern.ch>
2017-02-20 12:39:36 +0100

    Replace ISiChargedDiodesProcessorTool with PixelProcessorTool (PixelDigitization-02-03-31)
      * Replace ISiChargedDiodesProcessorTool with PixelProcessorTool.
    	* Tagged as PixelDigitization-02-03-31.
    
    
    Former-commit-id: c5aa163f50b92507b64997a7e539b21e47e4ec07

--- InnerDetector/InDetDigitization/PixelDigitization/python/PixelDigitizationConfig.py
+++ InnerDetector/InDetDigitization/PixelDigitization/python/PixelDigitizationConfig.py
@@ -113,14 +113,17 @@ def SubChargesTool(name="SubChargesTool", **kwargs):
     kwargs.setdefault("RndmEngine", "PixelDigitization")
     return CfgMgr.SubChargesTool(name, **kwargs)
 
+def PixelProcessorTool(name="PixelProcessorTool", **kwargs):
+    kwargs.setdefault("RndmSvc", digitizationFlags.rndmSvc())
+    kwargs.setdefault("RndmEngine", "PixelDigitization")
+    return CfgMgr.PixelProcessorTool(name, **kwargs)
+
 def PixelNoisyCellGenerator(name="PixelNoisyCellGenerator", **kwargs):
     from AthenaCommon.BeamFlags import jobproperties
     kwargs.setdefault("MergeCharge", True)
     NoiseShape = [0.00596, 0.03491, 0.07058, 0.11991, 0.17971, 0.24105, 0.29884, 0.35167, 0.39912, 0.44188, 0.48016, 0.51471, 0.54587, 0.57405, 0.59958, 0.62288, 0.64411, 0.66360, 0.68159, 0.69823, 0.71362, 0.72781, 0.74096, 0.75304, 0.76415, 0.77438, 0.78383, 0.79256, 0.80066, 0.80821, 0.81547, 0.82246, 0.82918, 0.83501, 0.84054, 0.84576, 0.85078, 0.85558, 0.86018, 0.86455, 0.86875, 0.87273, 0.87653, 0.88020, 0.88369, 0.88705, 0.89027, 0.89336, 0.89633, 0.89921, 0.90195, 0.90460, 0.90714, 0.90961, 0.91198, 0.91426, 0.91644, 0.91853, 0.92055, 0.92250, 0.92435, 0.92611, 0.92782, 0.92947, 0.93105, 0.93257, 0.93404, 0.93547, 0.93688, 0.93822, 0.93953, 0.94079, 0.94201, 0.94318, 0.94432, 0.94542, 0.94649, 0.94751, 0.94851, 0.94949, 0.95045, 0.95137, 0.95227, 0.95314, 0.95399, 0.95483, 0.95563, 0.95646, 0.95729, 0.95812, 0.95896, 0.95980, 0.96063, 0.96144, 0.96224, 0.96301, 0.96377, 0.96451, 0.96521, 0.96590, 0.96657, 0.96722, 0.96787, 0.96849, 0.96911, 0.96971, 0.97031, 0.97090, 0.97148, 0.97204, 0.97260, 0.97314, 0.97367, 0.97421, 0.97474, 0.97525, 0.97576, 0.97627, 0.97676, 0.97722, 0.97769, 0.97815, 0.97861, 0.97906, 0.97950, 0.97992, 0.98033, 0.98073, 0.98111, 0.98147, 0.98182, 0.98216, 0.98249, 0.98281, 0.98312, 0.98343, 0.98374, 0.98402, 0.98430, 0.98456, 0.98482, 0.98507, 0.98532, 0.98555, 0.98579, 0.98602, 0.98624, 0.98646, 0.98668, 0.98690, 0.98711, 0.98732, 0.98753, 0.98773, 0.98793, 0.98813, 0.98832, 0.98851, 0.98870, 0.98888, 0.98907, 0.98925, 0.98943, 0.98961, 0.98979, 0.98996, 0.99014, 0.99031, 0.99048, 0.99064, 0.99081, 0.99098, 0.99114, 0.99131, 0.99147, 0.99163, 0.99179, 0.99194, 0.99210, 0.99225, 0.99240, 0.99256, 0.99271, 0.99286, 0.99300, 0.99315, 0.99329, 0.99344, 0.99358, 0.99372, 0.99386, 0.99400, 0.99414, 0.99427, 0.99440, 0.99453, 0.99466, 0.99479, 0.99491, 0.99503, 0.99515, 0.99527, 0.99538, 0.99549, 0.99560, 0.99571, 0.99582, 0.99592, 0.99602, 0.99613, 0.99623, 0.99633, 0.99643, 0.99653, 0.99662, 0.99672, 0.99682, 0.99691, 0.99701, 0.99710, 0.99719, 0.99728, 0.99737, 0.99746, 0.99755, 0.99764, 0.99772, 0.99781, 0.99790, 0.99798, 0.99806, 0.99814, 0.99823, 0.99831, 0.99839, 0.99847, 0.99855, 0.99863, 0.99871, 0.99879, 0.99887, 0.99895, 0.99902, 0.99910, 0.99918, 0.99925, 0.99933, 0.99940, 0.99948, 0.99955, 0.99963, 0.99971, 0.99978, 0.99985, 0.99993, 1.00000]
     kwargs.setdefault("NoiseShape",NoiseShape)
     #print "Noise Shape = ",NoiseShape
-    kwargs.setdefault("RndmSvc", digitizationFlags.rndmSvc())
-    kwargs.setdefault("RndmEngine", "PixelDigitization")
     if jobproperties.Beam.beamType != "cosmics" :
          kwargs.setdefault("RndNoiseProb", 1.e-10)
     return CfgMgr.PixelNoisyCellGenerator(name, **kwargs)
@@ -129,8 +132,6 @@ def PixelGangedMerger(name="PixelGangedMerger", **kwargs):
     return CfgMgr.PixelGangedMerger(name, **kwargs)
 
 def PixelChargeSmearer(name="PixelChargeSmearer", **kwargs):
-    kwargs.setdefault("RndmSvc", digitizationFlags.rndmSvc())
-    kwargs.setdefault("RndmEngine", "PixelDigitization")
     kwargs.setdefault("ThermalNoise", 160.0)
     return CfgMgr.PixelChargeSmearer(name, **kwargs)
 
@@ -159,8 +160,6 @@ def TimeSvc(name="TimeSvc", **kwargs):
     return CfgMgr.TimeSvc(name, **kwargs)
 
 def PixelRandomDisabledCellGenerator(name="PixelRandomDisabledCellGenerator", **kwargs):
-    kwargs.setdefault("RndmSvc", digitizationFlags.rndmSvc())
-    kwargs.setdefault("RndmEngine", "PixelDigitization")
     return CfgMgr.PixelRandomDisabledCellGenerator(name, **kwargs)
 
 def FrontEndSimTool(name="FrontEndSimTool", **kwargs):
@@ -218,23 +217,19 @@ def BasicPixelDigitizationTool(name="PixelDigitizationTool", **kwargs):
       pixelSiPropertiesSvc = SiPropertiesSvc(name = "PixelSiPropertiesSvc",DetectorName="Pixel",SiConditionsServices = lorentzAngleSvc.pixelSiliconConditionsSvc)
       ServiceMgr += pixelSiPropertiesSvc
     kwargs.setdefault("InputObjectName", "PixelHits")
-    pixTools = []
+    procTools = []
     chargeTools = []
     feSimTools = []
     if GeometryFlags.isSLHC():
-      pixTools += ['PixelDiodeCrossTalkGenerator']
-      pixTools += ['PixelChargeSmearer']
-      pixTools += ['PixelNoisyCellGenerator']
-      pixTools += ['PixelGangedMerger']
-      pixTools += ['PixelRandomDisabledCellGenerator']
+      procTools += ['PixelDiodeCrossTalkGenerator']
       chargeTools += ['IblPlanarBichselChargeTool']
       feSimTools += ['FEI4SimTool']
     else:
-      pixTools += ['PixelDiodeCrossTalkGenerator']
-      pixTools += ['PixelChargeSmearer']
-      pixTools += ['PixelNoisyCellGenerator']
-      pixTools += ['PixelGangedMerger']
-      pixTools += ['PixelRandomDisabledCellGenerator']
+      procTools += ['PixelDiodeCrossTalkGenerator']
+      procTools += ['PixelChargeSmearer']
+      procTools += ['PixelNoisyCellGenerator']
+      procTools += ['PixelGangedMerger']
+      procTools += ['PixelRandomDisabledCellGenerator']
       chargeTools += ['DBMChargeTool']
       chargeTools += ['PixelECBichselChargeTool']
       chargeTools += ['PixelBarrelBichselChargeTool']
@@ -242,7 +237,7 @@ def BasicPixelDigitizationTool(name="PixelDigitizationTool", **kwargs):
       chargeTools += ['Ibl3DBichselChargeTool']
       feSimTools += ['FEI4SimTool']
       feSimTools += ['FEI3SimTool']
-    kwargs.setdefault("PixelTools", pixTools)
+    kwargs.setdefault("PixelProcessorTools", procTools)
     kwargs.setdefault("ChargeTools", chargeTools)
     kwargs.setdefault("FrontEndSimTools", feSimTools)
     if digitizationFlags.doXingByXingPileUp(): # PileUpTool approach
--- InnerDetector/InDetDigitization/PixelDigitization/src/PixelGangedMerger.h
+++ InnerDetector/InDetDigitization/PixelDigitization/src/PixelGangedMerger.h
@@ -8,67 +8,27 @@
 ///////////////////////////////////////////////////////////////////
 // (c) ATLAS Detector software
 ///////////////////////////////////////////////////////////////////
-// Class to add/merge ganged pixels
-///////////////////////////////////////////////////////////////////
-
-/** @class PixelGangedMerger
- * @brief merges ganged pixels
- * @author Fredrik Tegenfeldt
- *
- * \b Description
- *
- * This class merges ganged pixels.
- * The pixel corresponding to the readout electronics is retained and obtains the merged charge.
- * The other pixel is masked out.
- *
- */
-
 #ifndef PIXELDIGITIZATION_PIXELGANGEDMERGER_H
 #define PIXELDIGITIZATION_PIXELGANGEDMERGER_H
 
-#include "SiDigitization/ISiChargedDiodesProcessorTool.h"
-#include "AthenaBaseComps/AthAlgTool.h"
+#include "PixelProcessorTool.h"
+#include "InDetIdentifier/PixelID.h"
 
+class PixelGangedMerger:public PixelProcessorTool {
 
-#include "Identifier/Identifier.h"
+  public:
+    PixelGangedMerger( const std::string& type, const std::string& name,const IInterface* parent);
 
-namespace InDetDD {
-	class SiDetectorElement;
-}
+    virtual ~PixelGangedMerger();
+    virtual StatusCode initialize();
+    virtual StatusCode finalize();
+    virtual void process(SiChargedDiodeCollection &collection);
 
-class PixelID;
+    bool isGanged(const Identifier& rdoID, const InDetDD::SiDetectorElement* element, Identifier& gangedID) const; 
 
-class PixelGangedMerger : public AthAlgTool, virtual public ISiChargedDiodesProcessorTool {
+  private:
+    PixelGangedMerger();
+    const PixelID *m_pixelID;
 
-public:
-
-  // Constructor:
-  PixelGangedMerger( const std::string& type, const std::string& name,const IInterface* parent);
-
-  /** Destructor */
-  virtual ~PixelGangedMerger();
-
-  /** AlgTool InterfaceID */
-  static const InterfaceID& interfaceID() ;
-
-  /** AlgTool initialize */
-  virtual StatusCode initialize();
-
-  /** AlgTool finalize */
-  virtual StatusCode finalize();
-
-  /** process the collection of pre digits */
-  virtual void process(SiChargedDiodeCollection &collection) const;
-  bool isGanged(const Identifier& rdoID,
-                const InDetDD::SiDetectorElement* element,
-                Identifier& gangedID) const; 
-
-private:
-
-  /** empty constructor mad private */
-  PixelGangedMerger();
-
-private:
-  const PixelID            *m_pixelID;     /**< the ID helper */
 };
 #endif // PIXELDIGITIZATION_PIXELGANGEDMERGER_H
