66342dc8de392c751ede9f3dad0fb04a1632e21d
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2017-02-21 10:48:14 +0100

    Pulled in the latest version of TrigRoiConversion from SVN. To make it build correctly in AnalysisBase.
    Former-commit-id: ce53c16d205c938d27bc9d0de94cafb95792ac80

--- Trigger/TrigEvent/TrigRoiConversion/src/components/TrigRoiConversion_entries.cxx
+++ Trigger/TrigEvent/TrigRoiConversion/src/components/TrigRoiConversion_entries.cxx
@@ -1,10 +1,13 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "TrigRoiConversion/RoiWriter.h"
+// $Id: TrigRoiConversion_entries.cxx 781704 2016-11-01 17:50:14Z krasznaa $
 
+// Gaudi include(s):
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Local include(s):
+#include "../RoiWriter.h"
 
 DECLARE_ALGORITHM_FACTORY( RoiWriter )
 
-
-DECLARE_FACTORY_ENTRIES(TrigEoiConversion) {
+DECLARE_FACTORY_ENTRIES( TrigRoiConversion ) {
     DECLARE_ALGORITHM( RoiWriter )
 }
