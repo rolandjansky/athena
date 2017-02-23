fa4618f1b92338243662e2c6ac771d1956b9a93f
Charles Leggett <leggett@cern.ch>
2017-02-23 23:47:41 +0100

    add new package MuonSpectrometer/MuonValidation/MuonSimValUtils
    from ChangeLog:
    
    2009-08-11  Jochen Meyer       <Jochen.Meyer@cern.ch>
    	    Nektarios Benekos  <Nektarios.Benekos@cern.ch>
    	                       <nbenekos@uiuc.edu>
    
    	* tagging MuonSimValUtils-00-00-07
    	* removal of compiler warnings (e.g. commenting out unused variables)
    
    2009-05-29  Jochen Meyer       <Jochen.Meyer@cern.ch>
    	    Nektarios Benekos  <Nektarios.Benekos@cern.ch>
    	                       <nbenekos@uiuc.edu>
    
    	* tagging MuonSimValUtils-00-00-06
    	* CSS and CSL implemented in src/MuonChamberNameConverter.cxx
    	* new method convertIntToStrName
    
    2009-05-14  Jochen Meyer       <Jochen.Meyer@cern.ch>
    	    Nektarios Benekos  <Nektarios.Benekos@cern.ch>
    	                       <nbenekos@uiuc.edu>
    
    	* tagging MuonSimValUtils-00-00-05
    	* BEE, EES and EEL and all the TGC chambers correctly implemented in  src/MuonChamberNameConverter.cxx
    
    2009-01-27  David Quarrie <David.Quarrie@cern.ch>
    	* cmt/requirements: Comment out declare_xxxx patterns with no corresponding directories
    	* src/MuonChamberNameConverter.cxx: ) Add gcc 4.3 compatibility
    	* src/MuonChambersRange.cxx:        )
    	* tagging MuonSimValUtils-00-00-04
    
    2009-01-19  Nectarios Benekos  <Nectarios.Benekos@cern.ch>
    
    	* tagging MuonSimValUtils-00-00-03
    	* fixes in checkreq WARNING alarms
    
    2007-07-01  Nectarios Benekos  <Nectarios.Benekos@cern.ch>
    
    	* bugfixes for EIL chambers
    	* tagging MuonSimValUtils-00-00-02
    
    2007-05-21  Nectarios Benekos  <Nectarios.Benekos@cern.ch>
    
    	* tagging MuonSimValUtils-00-00-01
    	* functionnalities added for MuonSimulValidation purpose
    
    
    Former-commit-id: d584fddd562e8a28429275b7a8350584f42a2e89

---
+++ MuonSpectrometer/MuonValidation/MuonSimValUtils/MuonSimValUtils/MuonIntToStrName.h
@@ -0,0 +1,12 @@
+#ifndef MUONSTRTOINTNAME_H
+#define MUONSTRTOINTNAME_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+std::string convertIntToStrName(int ChamberNameInt, std::string ChamberType); 
+
+
+#endif
