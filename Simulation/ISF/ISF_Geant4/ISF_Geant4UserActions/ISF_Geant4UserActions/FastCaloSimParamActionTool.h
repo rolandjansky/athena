68c10b9f4dee9005b20373fdf0182300487ff625
Charles Leggett <leggett@cern.ch>
2017-02-23 23:57:31 +0100

    update ISF_Geant4UserActions-00-03-04 to ISF_Geant4UserActions-00-05-00 from ChangeLog:
    2016-11-08  John Chapman  <John.Chapman@cern.ch>
    
            * ISF_Geant4UserActions/FastCaloSimParamActionTool.h,
            src/FastCaloSimParamActionTool.cxx,
            ISF_Geant4UserActions/FastCaloSimParamAction.h,
            src/FastCaloSimParamAction.cxx - migrate to use new
            AthService-based LAr Calculators. ATLASSIM-2290
            * cmt/requirements, CMakeLists.txt - drop obsolete dependencies on
            LArG4Barrel, LArG4EC, LArG4HEC and LArG4FCAL. ATLASSIM-2290
            * tagging ISF_Geant4UserActions-00-05-00
    
    2016-09-11  John Chapman  <John.Chapman@cern.ch>
    
            * src/FastCaloSimParamAction.cxx - switch to use new const
            interface of LArVCalculator. ATLASSIM-2606
            * tagging ISF_Geant4UserActions-00-04-00
    
    2016-08-11 Andrea Di Simone  <andrea.di.simone@cern.ch>
            * removed V1 UAs
            * tagging ISF_Geant4UserActions-00-03-05
    
    
    Former-commit-id: 735468fe9914f7122c231064d0eeb53d7fc54592

--- Simulation/ISF/ISF_Geant4/ISF_Geant4UserActions/ISF_Geant4UserActions/FastCaloSimParamActionTool.h
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4UserActions/ISF_Geant4UserActions/FastCaloSimParamActionTool.h
@@ -2,17 +2,20 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef ISF_GEANT4USERACTIONS_G4UA__FASTCALOSIMPARAMACTIONTOOL_H 
-#define ISF_GEANT4USERACTIONS_G4UA__FASTCALOSIMPARAMACTIONTOOL_H 
+#ifndef ISF_GEANT4USERACTIONS_G4UA__FASTCALOSIMPARAMACTIONTOOL_H
+#define ISF_GEANT4USERACTIONS_G4UA__FASTCALOSIMPARAMACTIONTOOL_H
 #include "G4AtlasInterfaces/IBeginRunActionTool.h"
 #include "G4AtlasInterfaces/IEndRunActionTool.h"
 #include "G4AtlasInterfaces/IBeginEventActionTool.h"
 #include "G4AtlasInterfaces/IEndEventActionTool.h"
 #include "G4AtlasInterfaces/ISteppingActionTool.h"
+
 #include "G4AtlasTools/ActionToolBase.h"
+
 #include "ISF_Geant4UserActions/FastCaloSimParamAction.h"
 
-namespace G4UA{ 
+class ILArCalculatorSvc;
+namespace G4UA{
 
   /// @class FastCaloSimParamActionTool
   /// @brief Tool which manages the FastCaloSimParamAction
@@ -20,28 +23,30 @@ namespace G4UA{
   /// @author Andrea Di Simone
   ///
 
-  class FastCaloSimParamActionTool: 
+  class FastCaloSimParamActionTool:
   public ActionToolBase<FastCaloSimParamAction>,
     public IBeginRunActionTool,  public IEndRunActionTool,  public IBeginEventActionTool,  public IEndEventActionTool,  public ISteppingActionTool
   {
-    
+
   public:
     /// Standard constructor
     FastCaloSimParamActionTool(const std::string& type, const std::string& name,const IInterface* parent);
+    /// Intialize Athena components
+    StatusCode initialize() override final;
     /// Retrieve the BoR action
-    virtual IBeginRunAction* getBeginRunAction() override final 
+    virtual IBeginRunAction* getBeginRunAction() override final
     { return static_cast<IBeginRunAction*>( getAction() ); }
     /// Retrieve the EoR action
-    virtual IEndRunAction* getEndRunAction() override final 
+    virtual IEndRunAction* getEndRunAction() override final
     { return static_cast<IEndRunAction*>( getAction() ); }
     /// Retrieve the BoE action
-    virtual IBeginEventAction* getBeginEventAction() override final 
+    virtual IBeginEventAction* getBeginEventAction() override final
     { return static_cast<IBeginEventAction*>( getAction() ); }
     /// Retrieve the EoE action
-    virtual IEndEventAction* getEndEventAction() override final 
+    virtual IEndEventAction* getEndEventAction() override final
     { return static_cast<IEndEventAction*>( getAction() ); }
     /// Retrieve the stepping action
-    virtual ISteppingAction* getSteppingAction() override final 
+    virtual ISteppingAction* getSteppingAction() override final
     { return static_cast<ISteppingAction*>( getAction() ); }
     virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface) override;
   protected:
@@ -50,8 +55,22 @@ namespace G4UA{
   private:
     /// Configuration parameters
     FastCaloSimParamAction::Config m_config;
+
+    ServiceHandle<ILArCalculatorSvc> m_emepiwcalc;  //!< EMEC positive inner wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_emeniwcalc;  //!< EMEC negative inner wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_emepowcalc;  //!< EMEC positive outer wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_emenowcalc;  //!< EMEC negative outer wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_emeobarcalc; //!< EMEC Back Outer Barrette wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_embcalc;     //!< Barrel calculator
+    ServiceHandle<ILArCalculatorSvc> m_fcal1calc;   //!< FCAL1 calculator
+    ServiceHandle<ILArCalculatorSvc> m_fcal2calc;   //!< FCAL2 calculator
+    ServiceHandle<ILArCalculatorSvc> m_fcal3calc;   //!< FCAL3 calculator
+    ServiceHandle<ILArCalculatorSvc> m_heccalc;     //!< HEC wheel calculator
+    ServiceHandle<ILArCalculatorSvc> m_pscalc;      //!< Barrel presampler calculator
+    ServiceHandle<ILArCalculatorSvc> m_emepscalc;   //!< Endcap presampler calculator
+
   }; // class FastCaloSimParamActionTool
-  
-  
-} // namespace G4UA 
+
+
+} // namespace G4UA
 #endif
