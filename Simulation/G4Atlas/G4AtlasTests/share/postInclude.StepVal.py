ca37075fc4596f1dbd69cb323b21273f9fb826b1
Charles Leggett <leggett@cern.ch>
2017-02-23 22:44:42 +0100

    update G4AtlasTests-01-02-01 to G4AtlasTests-01-02-04
    from ChangeLog:
    
    2016-08-11 Andrea Di Simone <andrea.di.simone@cern.ch>
            * removed V1 user actions
            * tagging G4AtlasTests-01-02-04
    2016-08-09  Chilufya Mwewa  <Chilufya.Mwewa@cern.ch>
    
            * Moving validation histograms for MMs and STGCs from
            MuonHitTestToolBase to MMHitsTestTool and sTGCHitsTestTool
            classes respectively.
            * tagging G4AtlasTests-01-02-03
    
    2016-08-02  Chilufya Mwewa  <Chilufya.Mwewa@cern.ch>
    
            * Added validation histograms for MMs and STGCs.
            * tagging G4AtlasTests-01-02-02
    
    
    Former-commit-id: 35d6919edb185e7f18e757fbdcc7baf7a90c51ea

--- Simulation/G4Atlas/G4AtlasTests/share/postInclude.StepVal.py
+++ Simulation/G4Atlas/G4AtlasTests/share/postInclude.StepVal.py
@@ -6,15 +6,6 @@ if not hasattr(ServiceMgr, 'THistSvc'):
 ServiceMgr.THistSvc.Output  += ["truth DATAFILE='stepping.root' OPT='NEW'"];
 
 from G4AtlasApps.SimFlags import simFlags
-if hasattr(simFlags, 'UseV2UserActions') and simFlags.UseV2UserActions():
-    # hive-friendly version
-    simFlags.OptionalUserActionList.addAction('G4UA::SteppingValidationTool',['BeginOfRun','EndOfEvent','BeginOfEvent','Step'])
-else:
-    # V1 userActions
-    try:
-        from G4AtlasServices.G4AtlasUserActionConfig import UAStore
-    except ImportError:
-        from G4AtlasServices.UserActionStore import UAStore
-    # add default configurable
-    UAStore.addAction('SteppingValidation',['Step','BeginOfEvent','EndOfEvent'])
+simFlags.OptionalUserActionList.addAction('G4UA::SteppingValidationTool',['BeginOfRun','EndOfEvent','BeginOfEvent','Step'])
+
 
--- Simulation/G4Atlas/G4AtlasTests/src/MMHitsTestTool.h
+++ Simulation/G4Atlas/G4AtlasTests/src/MMHitsTestTool.h
@@ -22,7 +22,7 @@ public:
       //m_pMdtIdHelper(0)
   {
     declareProperty("DoMMTest", m_DoMMTest=true);
-    m_BarrelEtaCut=1.;
+    m_BarrelEtaCut=10.;
   }
 
   StatusCode initialize();
@@ -30,8 +30,26 @@ public:
   StatusCode processEvent();
 
  protected:
+ 	  
+ 	TH1 *m_MM_rPlot_S1_posZ, *m_MM_rPlot_S2_posZ, *m_MM_rPlot_L1_posZ, *m_MM_rPlot_L2_posZ;
+
+ 	TH2 *m_MMTransverseEta1SmallWedge1, *m_MMTransverseEta2SmallWedge1, *m_MMTransverseEta1LargeWedge1;
+  	TH2 *m_MMTransverseEta2LargeWedge1, *m_MMTransverseEta1SmallWedge2, *m_MMTransverseEta2SmallWedge2;
+  	TH2 *m_MMTransverseEta1LargeWedge2, *m_MMTransverseEta2LargeWedge2;
+  
+  	TH2 *m_MM_SmallWedge1_TransverseView_positiveZ, *m_MM_SmallWedge1_TransverseView_negativeZ;
+  	TH2 *m_MM_LargeWedge1_TransverseView_positiveZ, *m_MM_LargeWedge1_TransverseView_negativeZ;
+  	TH2 *m_MM_SmallWedge1_rZview_positiveZ, *m_MM_SmallWedge1_rZview_negativeZ;
+  	TH2 *m_MM_LargeWedge1_rZview_positiveZ, *m_MM_LargeWedge1_rZview_negativeZ;
+   
+  	TH2 *m_MM_SmallWedge2_TransverseView_positiveZ, *m_MM_SmallWedge2_TransverseView_negativeZ;
+  	TH2 *m_MM_LargeWedge2_TransverseView_positiveZ, *m_MM_LargeWedge2_TransverseView_negativeZ;
+  	TH2 *m_MM_SmallWedge2_rZview_positiveZ, *m_MM_SmallWedge2_rZview_negativeZ;
+  	TH2 *m_MM_LargeWedge2_rZview_positiveZ, *m_MM_LargeWedge2_rZview_negativeZ;
+ 	  
   // Identifier getIdentifier(HitID cschit);
 //   StatusCode checkIdentifier(Identifier offid);
+	
 private:
 
   bool m_DoMMTest;
