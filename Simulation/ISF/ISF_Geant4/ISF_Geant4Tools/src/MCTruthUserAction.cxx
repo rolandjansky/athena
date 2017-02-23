76623e4f7eca1e1a0350aecf116ea60aed78108d
Charles Leggett <leggett@cern.ch>
2017-02-23 23:56:15 +0100

    update ISF_Geant4Tools-00-11-00-05 to ISF_Geant4Tools-00-12-08
    from ChangeLog:
    
    2016-11-28  Zach Marshall <ZLMarshall@lbl.gov>
            * Re-adding missing parts of neutron flux scoring
            * Tagging ISF_Geant4Tools-00-12-08
    
    2016-11-08 Elmar Ritsch <elmar.ritsch@cern.ch>
            * Refactor TrackProcessorUserActionBase to improve readability
            * tagging ISF_Geant4Tools-00-12-07
    
    2016-11-03 Andreas Petridis <apetrid@cern.ch>, Elmar Ritsch
            <elmar.ritsch@cern.ch>
            * Make the ISF truth service wraper work w/ and w/o Cavern
            volume, ATLASSIM-3000
            * tagging ISF_Geant4Tools-00-12-06
    
    
    Former-commit-id: 7a313d1028f1eec5964c683d3cfe432dba48ce12

--- Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/MCTruthUserAction.cxx
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/MCTruthUserAction.cxx
@@ -10,14 +10,16 @@
 #include "MCTruthUserAction.h"
 
 // package includes
-#include "Geant4TruthIncident.h"
 #include "ISFTrajectory.h"
 
 // ISF includes
+#include "ISF_Event/ISFParticle.h"
+
 #include "ISF_Interfaces/ITruthSvc.h"
 #include "ISF_Interfaces/IParticleBroker.h"
 
-#include "ISF_Event/ISFParticle.h"
+// ISF Geant4 includes
+#include "ISF_Geant4Event/Geant4TruthIncident.h"
 
 // Athena includes
 #include "MCTruth/TrackInformation.h"
@@ -26,96 +28,9 @@
 
 #include "SimHelpers/StepHelper.h"
 
+#include "G4EventManager.hh"
 
 // Atlas G4 Helpers
-
-iGeant4::MCTruthUserAction::MCTruthUserAction(const std::string& type,
-                                              const std::string& name,
-                                              const IInterface* parent)
-  : UserActionBase(type,name,parent),
-    m_sHelper(),
-    m_truthRecordSvc("ISF_TruthRecordSvc", name),
-    m_truthRecordSvcQuick(nullptr)
-{
-
-  //declareInterface<IUserAction>(this);
-
-  declareProperty("TruthRecordSvc", m_truthRecordSvc, "ISF Particle Truth Svc");
-
-  declareProperty("SecondarySavingLevel", m_ilevel=2);
-
-}
-
-StatusCode iGeant4::MCTruthUserAction::initialize()
-{
-  ATH_MSG_INFO("Initializing iGeant4::MCTruthUserAction");
-
-  if (m_truthRecordSvc.retrieve().isFailure()){
-    ATH_MSG_FATAL( "Could not retrieve " << m_truthRecordSvc );
-    return StatusCode::FAILURE;
-  }
-
-  m_sHelper=SecondaryTracksHelper(G4EventManager::GetEventManager()->GetTrackingManager());
-
-  // store a pointer directly to the truth service class
-  // by doing so, the Gaudi overhead can be minimized
-  m_truthRecordSvcQuick = &(*m_truthRecordSvc);
-
-  return StatusCode::SUCCESS;
-}
-
-StatusCode iGeant4::MCTruthUserAction::finalize()
-{
-  return StatusCode::SUCCESS;
-}
-
-void iGeant4::MCTruthUserAction::PreTracking(const G4Track* inTrack)
-{
-
-  //std::cout<<"in MCTruthUserAction::PreUserTrackingAction, m_ilevel="<<m_ilevel<<std::endl;
-
-  //m_sHelper.ResetNrOfSecondaries();
-
-  G4Track* inT = const_cast<G4Track*> (inTrack);
-  TrackHelper trackHelper(inT);
-
-  if (trackHelper.IsPrimary() ||
-      (trackHelper.IsRegisteredSecondary()&&m_ilevel>1) ||
-      (trackHelper.IsSecondary()&&m_ilevel>2)) {
-
-    G4Trajectory *temp=new iGeant4::ISFTrajectory(inTrack, m_truthRecordSvcQuick);
-    m_fpTrackingManager->SetStoreTrajectory(true);
-    m_fpTrackingManager->SetTrajectory(temp);
-    // @TODO check that the G4TrackingManager deletes the 'temp'
-    //       object after G4TrackingManager::SetStoreTrajectory(false)
-    //       is executed.
-  }
-
-}
-
-void iGeant4::MCTruthUserAction::PostTracking(const G4Track* )
-{
-  // We are done tracking this particle, so reset the trajectory.
-  m_fpTrackingManager->SetStoreTrajectory(false);
-
-}
-
-iGeant4::MCTruthUserAction::~MCTruthUserAction()
-{}
-
-
-
-StatusCode iGeant4::MCTruthUserAction::queryInterface(const InterfaceID& riid, void** ppvInterface)
-{
-  if ( IUserAction::interfaceID().versionMatch(riid) ) {
-    *ppvInterface = dynamic_cast<IUserAction*>(this);
-    addRef();
-  } else {
-    // Interface is not directly available : try out a base class
-    return UserActionBase::queryInterface(riid, ppvInterface);
-  }
-  return StatusCode::SUCCESS;
-}
 
 
 namespace G4UA{
