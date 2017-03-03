4219f1985e0785daac4e96acc42620dc707e3d9a
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-03-03 15:12:34 +0100

    Had to update the 21.0 commit to use the newer versions of Rivet and YODA that were by now configured for the master branch.
    Made sure that the generator externals set up by the Athena project would
    be in sync with what some of the generator packages requested explicitly.
    
    Then I removed the explicit requests from the generator packages, to only
    have the version declarations in a single place.
    
    Also had to add a configuration file for Herwig3.
    
    
    Former-commit-id: 3022153185eb418201ed7de811a1757a0b14afae

--- Projects/Athena/externals/YODA.cmake
+++ Projects/Athena/externals/YODA.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of YODA to use.
 #
 
-set( YODA_VERSION 1.6.3 )
+set( YODA_VERSION 1.6.6 )
 set( YODA_ROOT
    ${LCG_RELEASE_DIR}/MCGenerators/yoda/${YODA_VERSION}/${LCG_PLATFORM} )
