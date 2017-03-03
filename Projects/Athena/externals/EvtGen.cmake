34bd7a8e24c9802920630820fae5aa977f98f367
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-03-03 14:56:26 +0100

    Added the external setup files that were previously in the AthenaExternals project into the Athena project's configuration. This is to allow us to fine-tune which externals we want to use in each branch.
    Updated the project configuration of Athena to make use of the new files.
    
    Updated the Athena project to build against the latest hash of the
    atlasexternals repository. While the update is being tested. This will
    have to be changed to an annoted tag once the build is working
    correctly.
    
    Updated the package filters file of the Athena project to work correctly
    with the latest version of the AtlasCMake code.
    
    
    Former-commit-id: 2038615812fb15d3115d642f17b8970dbc29496e

---
+++ Projects/Athena/externals/EvtGen.cmake
@@ -0,0 +1,7 @@
+#
+# File specifying the location of EvtGen to use.
+#
+
+set( EVTGEN_VERSION 1.4.0 )
+set( EVTGEN_ROOT
+   ${LCG_RELEASE_DIR}/MCGenerators/evtgen/${EVTGEN_VERSION}/${LCG_PLATFORM} )
