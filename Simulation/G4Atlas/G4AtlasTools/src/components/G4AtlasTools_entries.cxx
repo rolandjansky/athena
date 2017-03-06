d6a112904441be3b1ae4583cb0abefe428ae5802
John Chapman <jchapman@cern.ch>
2017-03-06 13:20:52 +0100

    Remove Base classes for V1 UserActions
    Removing obsolete base classes for V1 UserActions, now that V2
    UserActions have been validated for production.
    
    
    Former-commit-id: f3c228c9eeec67cac0c1bec1413c1458d5134b18

--- Simulation/G4Atlas/G4AtlasTools/src/components/G4AtlasTools_entries.cxx
+++ Simulation/G4Atlas/G4AtlasTools/src/components/G4AtlasTools_entries.cxx
@@ -1,13 +1,13 @@
-e#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "G4AtlasTools/PhysicsListToolBase.h"
 #include "G4AtlasTools/DetectorGeometryBase.h"
-#include "G4AtlasTools/UserActionBase.h"
 #include "G4AtlasTools/CylindricalEnvelope.h"
 #include "G4AtlasTools/BoxEnvelope.h"
 #include "G4AtlasTools/PolyconicalEnvelope.h"
 #include "G4AtlasTools/EnvelopeTool.h"
 #include "G4AtlasTools/G4AtlasDetectorConstructionTool.h"
+#include "G4AtlasTools/GeoDetectorTool.h"
 #include "G4AtlasTools/RegionCreator.h"
 #include "../UserLimitsTool.h"
 #include "G4AtlasTools/AddPhysicsDecayTool.h"
@@ -18,12 +18,12 @@ e#include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_TOOL_FACTORY( PhysicsListToolBase )
 DECLARE_TOOL_FACTORY( DetectorGeometryBase )
-DECLARE_TOOL_FACTORY( UserActionBase )
 DECLARE_TOOL_FACTORY( CylindricalEnvelope )
 DECLARE_TOOL_FACTORY( BoxEnvelope )
 DECLARE_TOOL_FACTORY( PolyconicalEnvelope )
 DECLARE_TOOL_FACTORY( EnvelopeTool )
 DECLARE_TOOL_FACTORY( G4AtlasDetectorConstructionTool )
+DECLARE_TOOL_FACTORY( GeoDetectorTool )
 DECLARE_TOOL_FACTORY( RegionCreator )
 DECLARE_TOOL_FACTORY( UserLimitsTool )
 DECLARE_TOOL_FACTORY( SensitiveDetectorMasterTool )
