94dfadf8ec53c619225cdd8b216963d10cdf6420
Charles Leggett <leggett@cern.ch>
2017-03-07 00:13:33 +0100

    update CondSvc methods
    remove unnecessary CondSvc methods, update to match ICondSvc signatures
    
    
    Former-commit-id: feb4c28f1cff34389b16b4acaf1a79987e0227ac

--- Control/IOVSvc/src/CondSvc.cxx
+++ Control/IOVSvc/src/CondSvc.cxx
@@ -1,7 +1,3 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
 #include "IOVSvc/CondSvc.h"
 #include "AthenaKernel/CondCont.h"
 #include "GaudiKernel/SvcFactory.h"
@@ -29,9 +25,6 @@ CondSvc::CondSvc( const std::string& name, ISvcLocator* svcLoc ):
   base_class(name,svcLoc),
   m_sgs("StoreGateSvc/ConditionStore", name)
 {
-
-  declareProperty("CondFile",m_file="");
-  
 }
 
 //---------------------------------------------------------------------------
@@ -59,80 +52,7 @@ CondSvc::initialize() {
     return StatusCode::FAILURE;
   }
 
-  if (m_file == "") {
-    ATH_MSG_DEBUG("db file not set");
-    return StatusCode::SUCCESS;
-  }
-
-  if (readDbFile(m_file).isFailure()) {
-    return StatusCode::FAILURE;
-  }
-
-  std::ostringstream ost;
-  ost << " Printing CondDB registry";
-  for (auto e : m_registry) {
-    ost << std::endl << "  - id: " << e.first << "  r:";
-    for (auto r : e.second) {
-      ost << "  " << r.range() << " :: " << *r.objPtr();
-    }
-  }
-
-  ATH_MSG_DEBUG( ost.str() );  
-
   return StatusCode::SUCCESS;
-
-}
-//---------------------------------------------------------------------------
-
-StatusCode
-CondSvc::readDbFile(const std::string& fname) {
-
-  StatusCode sc(StatusCode::SUCCESS);
-
-  ATH_MSG_DEBUG("reading cond db from \"" << fname << "\"");
-
-  std::ifstream ifs (fname);
-  std::string line;
-  if(ifs.is_open()) {
-
-    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-    boost::char_separator<char> sep(" ");
-
-    IOVEntryT<ICondSvc::dbData_t> ie;
-
-    while( getline (ifs, line) ) {
-      
-      // ignore anything after a "#" and blank lines
-      size_t fh = line.find("#");
-      if(fh != std::string::npos) 
-        line.erase(fh,line.length()-fh);
-      if (line.length() == 0) continue;
-      
-      tokenizer tokens(line, sep);
-      auto it = tokens.begin();
-      
-      std::string dbKey = *it;
-      
-      ++it;
-      
-      while (it != tokens.end()) {
-        if (parse(ie,*it)) {
-          m_registry[dbKey].push_back( ie );
-        } else {
-          error() << "while reading " << fname << " problem parsing " << *it 
-                  << " in line\n" << line << endmsg;
-          sc = StatusCode::FAILURE;
-        }
-        ++it;
-      }
-    }
-    ifs.close();
-  } else {
-    error() << "unable to open file " << m_file << endmsg;
-    sc = StatusCode::FAILURE;
-  }
-
-  return sc;
 
 }
 //---------------------------------------------------------------------------
@@ -201,14 +121,6 @@ CondSvc::finalize() {
     ATH_MSG_DEBUG( ost.str() );
   }
 
-  for ( auto e : m_registry ) {
-    for ( auto ie : e.second ) {
-      delete ie.objPtr();
-      ie.setPtr(0);
-    }
-  }
-
-
   return StatusCode::SUCCESS;
 
 }
@@ -247,6 +159,8 @@ CondSvc::regHandle(IAlgorithm* alg, const Gaudi::DataHandle& dh,
   m_keyMap[key] = dh.fullKey();
   m_condAlgs.insert(alg);
 
+  m_condIDs.emplace( dh.fullKey() );
+
   //  id_map_t::iterator itd2 = m_idMap.find(dh.fullKey());
   if (itd2 != m_idMap.end()) {
     itd2->second.insert( alg );
@@ -268,13 +182,13 @@ CondSvc::regHandle(IAlgorithm* alg, const Gaudi::DataHandle& dh,
 //---------------------------------------------------------------------------
 
 bool
-CondSvc::getInvalidIDs(const EventContext* ctx, DataObjIDColl& invalidIDs) {
+CondSvc::getInvalidIDs(const EventContext& ctx, DataObjIDColl& invalidIDs) {
   std::lock_guard<std::recursive_mutex> lock(m_lock);
 
-  EventIDBase now(ctx->eventID().run_number(), ctx->eventID().event_number());
+  EventIDBase now(ctx.eventID().run_number(), ctx.eventID().event_number());
 
   std::ostringstream ost;
-  ost << "getInvalidIDS " << ctx->eventID() 
+  ost << "getInvalidIDS " << ctx.eventID() 
       << ": retrieving all ConstIterator<CondContBase>";
   SG::ConstIterator<CondContBase> cib, cie;
   if (m_sgs->retrieve(cib,cie).isSuccess()) {
@@ -304,14 +218,14 @@ CondSvc::getInvalidIDs(const EventContext* ctx, DataObjIDColl& invalidIDs) {
 //---------------------------------------------------------------------------
 
 bool
-CondSvc::getIDValidity(const EventContext* ctx, DataObjIDColl& validIDs,
+CondSvc::getIDValidity(const EventContext& ctx, DataObjIDColl& validIDs,
                        DataObjIDColl& invalidIDs) {
   std::lock_guard<std::recursive_mutex> lock(m_lock);
 
-  EventIDBase now(ctx->eventID());
+  EventIDBase now(ctx.eventID());
 
   std::ostringstream ost;
-  ost << "getValidIDS " << ctx->eventID() 
+  ost << "getValidIDS " << ctx.eventID() 
       << ": retrieving all ConstIterator<CondContBase>";
   SG::ConstIterator<CondContBase> cib, cie;
   if (m_sgs->retrieve(cib,cie).isSuccess()) {
@@ -344,13 +258,13 @@ CondSvc::getIDValidity(const EventContext* ctx, DataObjIDColl& validIDs,
 //---------------------------------------------------------------------------
 
 bool
-CondSvc::getValidIDs(const EventContext* ctx, DataObjIDColl& validIDs) {
+CondSvc::getValidIDs(const EventContext& ctx, DataObjIDColl& validIDs) {
   std::lock_guard<std::recursive_mutex> lock(m_lock);
 
-  EventIDBase now(ctx->eventID());
+  EventIDBase now(ctx.eventID());
 
   std::ostringstream ost;
-  ost << "getValidIDS " << ctx->eventID() 
+  ost << "getValidIDS " << ctx.eventID() 
       << ": retrieving all ConstIterator<CondContBase>";
   SG::ConstIterator<CondContBase> cib, cie;
   if (m_sgs->retrieve(cib,cie).isSuccess()) {
@@ -379,118 +293,36 @@ CondSvc::getValidIDs(const EventContext* ctx, DataObjIDColl& validIDs) {
 
 //---------------------------------------------------------------------------
 
-std::set<std::string>
-CondSvc::getUnchangedAlgs(const DataObjIDColl& ids) {
+bool
+CondSvc::isValidID(const EventContext& ctx, const DataObjID& id) const {
   std::lock_guard<std::recursive_mutex> lock(m_lock);
 
-  std::ostringstream ost;
-  ost << "getUnchangedAlgs():";
+  EventIDBase now(ctx.eventID());
 
-  std::set<std::string> algs;
-  id_map_t::const_iterator itr;
-
-  for (auto id : ids) {
-    ost << "\n  id: " << id;
-    itr = m_idMap.find(id);
-    if (itr == m_idMap.end()) {
-      ost << " does not belong to any Alg!!! this shouldn't happen";
-    } else {
-      ost << " belongs to";
-      // loop over all Algs registered to this obj
-      for (auto alg : itr->second) {
-        ost << " " << alg->name();
-
-        // loop over all objs registered against this alg, make sure they
-        // are ALL unchanged
-        bool match = true;
-        alg_map_t::const_iterator itr2 = m_algMap.find(alg);
-        for (auto id2 : itr2->second) {
-          if ( ids.find(id2) == ids.end()) {
-            ost << " but has obj " << id2 << " that is not valid";
-            match = false;
-          }
-        }
-
-        if (match) {
-          algs.insert( alg->name() );
-        }
-      }
-    }
-    //    ost << std::endl;
+  CondContBase *cib;
+  if (m_sgs->retrieve(cib, id.key()).isSuccess()) {
+    return cib->valid(now);
   }
 
-  ATH_MSG_DEBUG( ost.str() );
-    
-  return algs;
-}
-
-//---------------------------------------------------------------------------
-
-bool 
-CondSvc::parse(EventIDRange& t, const std::string& s) {
-
-  boost::smatch m;
-  boost::regex_match(s,m,rr);
-
-  // for (auto res : m) {
-  //   cout << " - " << res << endl;
-  // }
-
-  if (m.size() != 5) { return false; }
-
-  t = EventIDRange( EventIDBase(std::stoi(m[1]),std::stoi(m[2])), 
-                    EventIDBase(std::stoi(m[3]), std::stoi(m[4])));
-
-  return true;
+  return false;
 
 }
 
 //---------------------------------------------------------------------------
 
 bool
-CondSvc::parse(IOVEntryT<ICondSvc::dbData_t>& ie, const std::string& s) {
+CondSvc::isRegistered(const DataObjID& id) const {
 
-  boost::smatch m;
-  boost::regex_match(s,m,ref);
-
-  if (m.size() != 6) { return false; }
-
-  ie.setRange( EventIDRange( EventIDBase(std::stoi(m[1]), std::stoi(m[2])), 
-                             EventIDBase(std::stoi(m[3]), std::stoi(m[4])) ) );
-
-  ICondSvc::dbData_t *v = new ICondSvc::dbData_t( std::stof(m[5]) );
-  ie.setPtr(v);
-
-  return true;
+  return (m_condIDs.find(id) != m_condIDs.end());
 
 }
 
 //---------------------------------------------------------------------------
 
-StatusCode
-CondSvc::getRange(const std::string& dbKey , const EventContext* ctx,
-                  EventIDRange& rng, ICondSvc::dbData_t& val) const {
+const DataObjIDColl&
+CondSvc::conditionIDs() const {
 
-  std::lock_guard<std::recursive_mutex> lock(m_lock);
+  return m_condIDs;
 
-  registry_t::const_iterator itr = m_registry.find(dbKey);
-
-  if (itr == m_registry.end()) {
-    error() << "getRange: no dbKey " << dbKey << " found in registry" 
-            << endmsg;
-    return StatusCode::FAILURE;
-  }
-
-  for (auto e : itr->second) {
-    if (e.range().isInRange(EventIDBase(ctx->eventID()))) {
-      rng = e.range();
-      val = *(e.objPtr());
-      return StatusCode::SUCCESS;
-    }
-  }
-
-  error() << "getRange: no range for Time " << ctx->eventID() 
-          << " found for dbKey "  << dbKey << endmsg;
-
-  return StatusCode::FAILURE;
 }
+
