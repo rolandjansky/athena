b57ed789c0903938ecc6b8d60065e97f4e51450c
John Chapman <jchapman@cern.ch>
2017-03-07 14:49:38 +0100

    Refactoring the TrackProcessorUserActionBase classe to improve readability.
    Former-commit-id: e4e5ee6cd35fba86ef4da81258ec60f6f9b954eb

--- Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/TrackProcessorUserActionBase.h
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/TrackProcessorUserActionBase.h
@@ -9,6 +9,15 @@
 
 #include <string>
 
+#include "G4AtlasInterfaces/IPreTrackingAction.h"
+#include "G4AtlasInterfaces/ISteppingAction.h"
+#include "G4AtlasInterfaces/IBeginEventAction.h"
+#include "G4AtlasInterfaces/IEndEventAction.h"
+#include "G4AtlasInterfaces/IPostTrackingAction.h"
+
+#include "MCTruth/VTrackInformation.h"
+
+
 // forward declarations
 class EventInformation;
 
@@ -21,48 +30,56 @@ namespace ISF {
 }
 
 
-#include "G4AtlasInterfaces/IPreTrackingAction.h"
-#include "G4AtlasInterfaces/ISteppingAction.h"
-#include "G4AtlasInterfaces/IBeginEventAction.h"
-#include "G4AtlasInterfaces/IEndEventAction.h"
-#include "G4AtlasInterfaces/IPostTrackingAction.h"
-
-
 namespace G4UA{
 
-  namespace iGeant4 {
+namespace iGeant4 {
 
-    class TrackProcessorUserActionBase:
-      public IPreTrackingAction,  public ISteppingAction,  public IBeginEventAction, public IEndEventAction, public IPostTrackingAction
-    {
+class TrackProcessorUserActionBase: public IPreTrackingAction, public ISteppingAction,
+                                    public IBeginEventAction, public IEndEventAction,
+                                    public IPostTrackingAction
+{
 
-    public:
+public:
+  TrackProcessorUserActionBase();
+  virtual void beginOfEvent(const G4Event*) override final;
+  virtual void endOfEvent(const G4Event*) override final;
 
-      TrackProcessorUserActionBase();
-      virtual void beginOfEvent(const G4Event*) override final;
-      virtual void endOfEvent(const G4Event*) override final;
+  virtual void preTracking(const G4Track*) override;
+  virtual void postTracking(const G4Track*) override final;
 
-      virtual void preTracking(const G4Track*) override;
-      virtual void postTracking(const G4Track*) override final;
+  virtual void processStep(const G4Step*) override final;
 
-      virtual void processStep(const G4Step*) override final;
+protected:
+  EventInformation* m_eventInfo;   //!< event-global G4 UserInformation
 
-  protected:
-      EventInformation                   *m_eventInfo;   //!< event-global G4 UserInformation
+  /** This method is called by TrackProcessorUserActionBase after the
+   *  G4Track->ISFParticle association has been established for the current G4Track */
+  virtual void ISFSteppingAction(const G4Step*, ISF::ISFParticle*) = 0;
 
-      /** This method is called by TrackProcessorUserActionBase after the
-       *  G4Track->ISFParticle association has been established for the current G4Track */
-      virtual void ISFSteppingAction(const G4Step*, ISF::ISFParticle*) = 0;
+private:
+  /// Setup the given G4Track as the current primary particle which we'll process
+  void setupPrimary(G4Track&);
 
-    private:
-      /** for keeping track of the currently processed G4Track and its corresponding ISFParticle */
-      ISF::ISFParticle                   *m_curBaseISP;      //!< the corresponding ISFParticle to this G4Track
-    }; // class TrackProcessorUserActionBase
+  /// Setup the given G4Track as the current secondary particle which we'll process
+  void setupSecondary(const G4Track&);
 
+  /// Set the following information as the currently traced particle
+  void setCurrentParticle(ISF::ISFParticle* baseISFParticle,
+                          HepMC::GenParticle* truthPrimary,
+                          HepMC::GenParticle* truthCurrentlyTraced);
 
+  /// Classify the particle represented by the given set of truth links
+  TrackClassification classify(const HepMC::GenParticle* primaryTruthParticle,
+                               const HepMC::GenParticle* generationZeroTruthParticle,
+                               const HepMC::GenParticle* currentlyTracedHepPart,
+                               int regenerationNumber) const;
 
+  /// The most recent ISFParticle ancestor that triggers the currently processed G4Track
+  ISF::ISFParticle* m_curBaseISP;
 
-  }// iGeant4
+}; // class TrackProcessorUserActionBase
+
+}// iGeant4
 
 }// G4UA
 
