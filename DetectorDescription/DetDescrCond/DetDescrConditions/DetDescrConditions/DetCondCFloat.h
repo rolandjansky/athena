9c466e6c39129d8f72890c571332f25adedd4141
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2017-03-08 19:58:17 +0100

    Changed two header files following reviewer's comments on the MR
    DetDescrConditions/DetCondCFloat.h: changed position of two include
    statements and moved multi-line comment into Doxygen format
    
    DetDescrConditions/DetCondFloat.h: moved multi-line comment into
    Doxygen format
    
    
    Former-commit-id: 693349ea3facdbfdcfb8c0c1aa307a940854bbcd

--- DetectorDescription/DetDescrCond/DetDescrConditions/DetDescrConditions/DetCondCFloat.h
+++ DetectorDescription/DetDescrCond/DetDescrConditions/DetDescrConditions/DetCondCFloat.h
@@ -4,31 +4,35 @@
 
 #ifndef DETDESCRCOND_DETCONDCFLOAT_H
 #define DETDESCRCOND_DETCONDCFLOAT_H
-// DetCondCFloat.h - class to hold sets of Identifiers and arrays of floats
-// for detector element specific information
-// Richard Hawkings, started 29/1/04
-// formerly DetCondFloat but renamed to DetCondCFloat to allow DetCondFloat
-// to be a parallel virtual class with no CLASS_DEF
-// Latter is inherited by specialised TRT conditions data classes
-//
-// Based on AlignableTransform but with vectors of floats of arbitrary size 
-// (at initialisation time). These objects are designed to reside in the
-// detector store and be kept in the conditionsDB.
-//
-// Methods are provided to create a new object with a float vector of a 
-// specified size, add an Identifier and associated array of floats,
-// and to associate a new Identifier with some already stored set of floats
-// This sharing mechanism can be used to tranparently associate one set of
-// constants to a large number of identifiers (i.e. detector elements)
-// and then increase the granularity later
-//
-// Access to the data is provided by the find method, which returns a pointer
-// to the floats associated with a particular identifier (or a null pointer
-// if the identifier is not known. The index within the internal storage
-// of a particular identifier's data can also be retrieved, for use in 
-// a subsequent call to the share method (this index is also returned by
-// the add method).
-// print and print2 methods print the data with varying amounts of detail
+/**
+ * @class DetCondCFloat
+ *
+ * @brief DetCondCFloat is a class to hold sets of Identifiers and 
+ * arrays of floats for detector element specific information
+ * Richard Hawkings, started 29/1/04
+ * formerly DetCondFloat but renamed to DetCondCFloat to allow DetCondFloat
+ * to be a parallel virtual class with no CLASS_DEF
+ * Latter is inherited by specialised TRT conditions data classes
+ *
+ * Based on AlignableTransform but with vectors of floats of arbitrary size 
+ * (at initialisation time). These objects are designed to reside in the
+ * detector store and be kept in the conditionsDB.
+ *
+ * Methods are provided to create a new object with a float vector of a 
+ * specified size, add an Identifier and associated array of floats,
+ * and to associate a new Identifier with some already stored set of floats
+ * This sharing mechanism can be used to tranparently associate one set of
+ * constants to a large number of identifiers (i.e. detector elements)
+ * and then increase the granularity later
+ *
+ * Access to the data is provided by the find method, which returns a pointer
+ * to the floats associated with a particular identifier (or a null pointer
+ * if the identifier is not known. The index within the internal storage
+ * of a particular identifier's data can also be retrieved, for use in 
+ * a subsequent call to the share method (this index is also returned by
+ * the add method).
+ * print and print2 methods print the data with varying amounts of detail
+ */
 
 #include <vector>
 #include <map>
@@ -38,6 +42,9 @@
 #ifndef CLIDSVC_CLASSDEF_H
 #include "CLIDSvc/CLASS_DEF.h"
 #endif
+
+#include "AthenaKernel/CondCont.h"
+#include "SGTools/BaseInfo.h"
 
 class DetCondCFloat {
  public:
@@ -67,9 +74,7 @@ class DetCondCFloat {
 };
 
 CLASS_DEF(DetCondCFloat,247459965,1)
-#include "AthenaKernel/CondCont.h"
 CLASS_DEF( CondCont<DetCondCFloat> , 85257013 , 1 )
-#include "SGTools/BaseInfo.h"
 SG_BASE( CondCont<DetCondCFloat>, CondContBase );
 
 // inline functions for DetCondCFloat
