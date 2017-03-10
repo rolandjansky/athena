9fa6e20396dfa5520c774311c5ffa57b1c490395
scott snyder <scott.snyder@cern.ch>
2017-03-11 00:02:36 +0100

    Do not use std::move on a return value.
    Former-commit-id: cc7f94de1a1e91aec856a3c02fbd87db3aa65351

--- Calorimeter/CaloG4Sim/src/CalibrationDefaultProcessingTool.cxx
+++ Calorimeter/CaloG4Sim/src/CalibrationDefaultProcessingTool.cxx
@@ -2,7 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "CxxUtils/make_unique.h"
 #include "CaloG4Sim/CalibrationDefaultProcessingTool.h"
 
 namespace G4UA{ 
@@ -19,8 +18,7 @@ namespace G4UA{
     
     std::unique_ptr<CalibrationDefaultProcessing>  CalibrationDefaultProcessingTool::makeAction(){
       ATH_MSG_DEBUG("makeAction");
-      auto action = CxxUtils::make_unique<CalibrationDefaultProcessing>(m_config);
-      return std::move(action);
+      return std::make_unique<CalibrationDefaultProcessing>(m_config);
     }
     
     StatusCode CalibrationDefaultProcessingTool::queryInterface(const InterfaceID& riid, void** ppvIf){
