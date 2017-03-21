920eb76f5273d8e75332c78c7f21c7939a35c815
Stewart Martin-Haugh <smh@cern.ch>
2017-03-21 10:07:55 +0100

    Make ATN test for menu example
    Former-commit-id: 5f30d9c29550cf49798f6e82f808321f77ab0b09

--- Trigger/TrigSteer/ViewAlgsTest/share/menuRun.py
+++ Trigger/TrigSteer/ViewAlgsTest/share/menuRun.py
@@ -45,7 +45,7 @@ else:
 
 from TrigConfigSvc.TrigConfigSvcConf import TrigConf__LVL1ConfigSvc
 l1svc = TrigConf__LVL1ConfigSvc("LVL1ConfigSvc")
-l1svc.XMLMenuFile = "LVL1config_Physics_pp_v5.xml"
+l1svc.XMLMenuFile = "LVL1config_Physics_pp_v7.xml"
 svcMgr += l1svc
 
 
@@ -93,7 +93,6 @@ if ( testViewAlgorithm ):
   from ViewAlgsTest.ViewAlgsTestConf import SchedulerProxyAlg
   viewAlgName = "algInView"
   viewAlg = SchedulerProxyAlg( viewAlgName )
-  viewAlg.RoIsContainer = "InViewRoI"
   viewAlg.RequireView = True
   topSequence += viewAlg
 
@@ -106,9 +105,7 @@ if ( testViewAlgorithm ):
   # The algorithm to launch the views
   from ViewAlgsTest.ViewAlgsTestConf import TestViewDriver
   runInViews = TestViewDriver( "runInViews" )
-  connectAlgorithmsIO ( producer=(caloFakeRoI, "OutputRoIs"), consumer=(runInViews, "RoIsContainer") )
-  runInViews.RoITypeInViews = 1 # collection of RoIs, each containing single RoI
-  runInViews.RoIKeyInViews = "InViewRoI" #
+  connectAlgorithmsIO ( producer=(caloFakeRoI, "OutputRoIs"), consumer=(runInViews, "RoIsContainer") ) 
   runInViews.OutputLevel = DEBUG
   runInViews.ViewAlgorithmNames = [ viewAlgName ]
   topSequence += runInViews
