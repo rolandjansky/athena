05c7da814fec464ed1494f817f99cddc7540e5bb
Stefan Kluth <stefan.kluth@cern.ch>
2017-03-22 10:29:23 +0100

    Beautification
    Former-commit-id: 8547ea31d231f109ba1924b5dc3f4cbc4e580503

--- AtlasTest/GoogleTestTools/test/gt_GoogleTestTools.cxx
+++ AtlasTest/GoogleTestTools/test/gt_GoogleTestTools.cxx
@@ -4,17 +4,22 @@
 
 namespace Athena_test {
 
+  // Fixture class derived from InitGaudiGoogleTest
+  // General pattern for fixture objects setting up Gaudi
   class InitGaudiGoogleTestTest : public InitGaudiGoogleTest {
   public:
 
     InitGaudiGoogleTestTest() 
-    : InitGaudiGoogleTest( MSG::INFO ) // get usual Gaudi message blurb
+    // Get usual Gaudi message blurb, comment out to silence Gaudi
+    : InitGaudiGoogleTest( MSG::INFO ) 
     {}
 
+    // Real Gaudi configuration should happen here
     //virtual void SetUp() override {}
 
   };
   
+  // Test shows how to access Gaudis pointers
   TEST_F( InitGaudiGoogleTestTest, basictest ) {
     EXPECT_TRUE( theApp != nullptr );
     EXPECT_TRUE( propMgr.isValid() );
