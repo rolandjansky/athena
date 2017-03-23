dc43372d9e28ed690c34ff5fa1e2fafbc81f451f
scott snyder <scott.snyder@cern.ch>
2017-03-24 00:15:38 +0100

    ReweightUtils: Comply with ATLAS naming conventions.
    Former-commit-id: 567b36d73a18770b91c14ab154af741363508edf

--- PhysicsAnalysis/AnalysisCommon/ReweightUtils/Root/APReweight.cxx
+++ PhysicsAnalysis/AnalysisCommon/ReweightUtils/Root/APReweight.cxx
@@ -14,20 +14,20 @@ using namespace std;
 
 APReweight::APReweight() :
   APReweightBase(),
-  _denominator_hist(0),
-  _numerator_hist(0),
-  _n_bins(0),
-  _axis(0)
+  m_denominator_hist(0),
+  m_numerator_hist(0),
+  m_n_bins(0),
+  m_axis(0)
 {
-  _isQuiet = false;
+  m_isQuiet = false;
 }
 
 APReweight::APReweight(TTree* denominator, string denominator_branch, TTree* numerator, string numerator_branch, unsigned int n_bins, double x_min, double x_max, bool isTrig) : APReweightBase() {
-  _empty_weight = new APWeightEntry(0, 0, 1.);
-  _denominator_hist = new TH1D("", "denominator_hist", n_bins, x_min, x_max);
-  _numerator_hist = new TH1D("", "numerator_hist", n_bins, x_min, x_max);
-  _axis = (TAxis*) _denominator_hist->GetXaxis()->Clone("");
-  _n_bins = _denominator_hist->GetNbinsX();
+  m_empty_weight = new APWeightEntry(0, 0, 1.);
+  m_denominator_hist = new TH1D("", "denominator_hist", n_bins, x_min, x_max);
+  m_numerator_hist = new TH1D("", "numerator_hist", n_bins, x_min, x_max);
+  m_axis = (TAxis*) m_denominator_hist->GetXaxis()->Clone("");
+  m_n_bins = m_denominator_hist->GetNbinsX();
 
   //Readout from Trees and fill histograms
   vector<double> *denominator_vec = 0, *numerator_vec = 0;
@@ -37,13 +37,13 @@ APReweight::APReweight(TTree* denominator, string denominator_branch, TTree* num
   unsigned long nentries_denominator = denominator->GetEntries(), nentries_numerator = numerator->GetEntries(), counter = 0;
   while (counter < nentries_denominator) {
     denominator->GetEntry(counter);
-    for (unsigned int i = 0, I = denominator_vec->size(); i < I; ++i) _denominator_hist->Fill((*denominator_vec)[i]);
+    for (unsigned int i = 0, I = denominator_vec->size(); i < I; ++i) m_denominator_hist->Fill((*denominator_vec)[i]);
     ++counter;
   }
   counter = 0;
   while (counter < nentries_numerator) {
     numerator->GetEntry(counter);
-    for (unsigned int i = 0, I = numerator_vec->size(); i < I; ++i) _numerator_hist->Fill((*numerator_vec)[i]);
+    for (unsigned int i = 0, I = numerator_vec->size(); i < I; ++i) m_numerator_hist->Fill((*numerator_vec)[i]);
     ++counter;
   }
 
@@ -57,60 +57,60 @@ APReweight::APReweight(TTree* denominator, string denominator_branch, TTree* num
   numerator_vec->clear();
   delete numerator_vec;
 
-  _scale = (double) nentries_denominator / (double) nentries_numerator;
-  _isTrig = isTrig;
-  _isQuiet = false;
-  for (unsigned int i = 0; i < _n_bins; ++i) {
-    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) _denominator_hist->GetBinContent(i + 1), (unsigned int) _numerator_hist->GetBinContent(i + 1), _scale, _isTrig);
-    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,_n_bins));
-    temp_entry->SetID(_ID);
-    _weights.push_back(temp_entry);
+  m_scale = (double) nentries_denominator / (double) nentries_numerator;
+  m_isTrig = isTrig;
+  m_isQuiet = false;
+  for (unsigned int i = 0; i < m_n_bins; ++i) {
+    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) m_denominator_hist->GetBinContent(i + 1), (unsigned int) m_numerator_hist->GetBinContent(i + 1), m_scale, m_isTrig);
+    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,m_n_bins));
+    temp_entry->SetID(m_ID);
+    m_weights.push_back(temp_entry);
   }
 }
 
 APReweight::APReweight(vector<double> denominator, vector<double> numerator, unsigned int n_bins, double x_min, double x_max, bool isTrig) : APReweightBase() {
-  _empty_weight = new APWeightEntry(0, 0, 1.);
-  _denominator_hist = new TH1D("", "denominator_hist", n_bins, x_min, x_max);
-  _numerator_hist = new TH1D("", "numerator_hist", n_bins, x_min, x_max);
-  _axis = (TAxis*) _denominator_hist->GetXaxis()->Clone("");
-  _n_bins = _denominator_hist->GetNbinsX();
+  m_empty_weight = new APWeightEntry(0, 0, 1.);
+  m_denominator_hist = new TH1D("", "denominator_hist", n_bins, x_min, x_max);
+  m_numerator_hist = new TH1D("", "numerator_hist", n_bins, x_min, x_max);
+  m_axis = (TAxis*) m_denominator_hist->GetXaxis()->Clone("");
+  m_n_bins = m_denominator_hist->GetNbinsX();
 
   //Readout from vectors and fill histograms
-  for (unsigned long i = 0, I = denominator.size(); i < I; ++i) _denominator_hist->Fill(denominator[i]);
-  for (unsigned long i = 0, I = numerator.size(); i < I; ++i) _numerator_hist->Fill(numerator[i]);
+  for (unsigned long i = 0, I = denominator.size(); i < I; ++i) m_denominator_hist->Fill(denominator[i]);
+  for (unsigned long i = 0, I = numerator.size(); i < I; ++i) m_numerator_hist->Fill(numerator[i]);
 
-  _scale = (double) denominator.size() / (double) numerator.size();
-  _isTrig = isTrig;
-  _isQuiet = false;
-  for (unsigned int i = 0; i < _n_bins; ++i) {
-    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) _denominator_hist->GetBinContent(i + 1), (unsigned int) _numerator_hist->GetBinContent(i + 1), _scale, _isTrig);
-    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,_n_bins));
-    temp_entry->SetID(_ID);
-    _weights.push_back(temp_entry);
+  m_scale = (double) denominator.size() / (double) numerator.size();
+  m_isTrig = isTrig;
+  m_isQuiet = false;
+  for (unsigned int i = 0; i < m_n_bins; ++i) {
+    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) m_denominator_hist->GetBinContent(i + 1), (unsigned int) m_numerator_hist->GetBinContent(i + 1), m_scale, m_isTrig);
+    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,m_n_bins));
+    temp_entry->SetID(m_ID);
+    m_weights.push_back(temp_entry);
   }
 }
 
 APReweight::APReweight(TH1* denominator_in, TH1* numerator_in, bool isTrig) : APReweightBase() {
-  _empty_weight = new APWeightEntry(0, 0, 1.);
-  _denominator_hist = (TH1D*) denominator_in->Clone("");
-  _numerator_hist = (TH1D*) numerator_in->Clone("");
-  _axis = (TAxis*) _denominator_hist->GetXaxis()->Clone("");
-  _n_bins = _denominator_hist->GetNbinsX();
-  _scale = (double) denominator_in->GetEntries() / (double) numerator_in->GetEntries();
-  _isTrig = isTrig;
-  _isQuiet = false;
-  for (unsigned int i = 0; i < _n_bins; ++i) {
-    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) _denominator_hist->GetBinContent(i + 1), (unsigned int) _numerator_hist->GetBinContent(i + 1), _scale, _isTrig);
-    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,_n_bins));
-    temp_entry->SetID(_ID);
-    _weights.push_back(temp_entry);
+  m_empty_weight = new APWeightEntry(0, 0, 1.);
+  m_denominator_hist = (TH1D*) denominator_in->Clone("");
+  m_numerator_hist = (TH1D*) numerator_in->Clone("");
+  m_axis = (TAxis*) m_denominator_hist->GetXaxis()->Clone("");
+  m_n_bins = m_denominator_hist->GetNbinsX();
+  m_scale = (double) denominator_in->GetEntries() / (double) numerator_in->GetEntries();
+  m_isTrig = isTrig;
+  m_isQuiet = false;
+  for (unsigned int i = 0; i < m_n_bins; ++i) {
+    APWeightEntry* temp_entry = new APWeightEntry((unsigned int) m_denominator_hist->GetBinContent(i + 1), (unsigned int) m_numerator_hist->GetBinContent(i + 1), m_scale, m_isTrig);
+    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,m_n_bins));
+    temp_entry->SetID(m_ID);
+    m_weights.push_back(temp_entry);
   }
   
-  if( _isTrig ) {
-    for( int i = 1; i < _numerator_hist->GetNbinsX(); ++i ) {
-      if( _numerator_hist->GetBinContent(i) > _denominator_hist->GetBinContent(i) ) {
-        std::cout << "WARNING in APReweight::~APReweight(TH1* denominator_in, TH1* numerator_in, bool isTrig) : Using histograms " << _numerator_hist->GetName() << " and " << _denominator_hist->GetName() << " the efficiency is larger than 1 for bin " << i << "! This is inconsistent and can lead to unwanted behaviour (weights > 1, variance < 0 )! Please check your input histograms. In order to avoid negative variances, the efficiency in this bin will be set to 0. " << std::endl;
-        _numerator_hist->SetBinContent(i,0);
+  if( m_isTrig ) {
+    for( int i = 1; i < m_numerator_hist->GetNbinsX(); ++i ) {
+      if( m_numerator_hist->GetBinContent(i) > m_denominator_hist->GetBinContent(i) ) {
+        std::cout << "WARNING in APReweight::~APReweight(TH1* denominator_in, TH1* numerator_in, bool isTrig) : Using histograms " << m_numerator_hist->GetName() << " and " << m_denominator_hist->GetName() << " the efficiency is larger than 1 for bin " << i << "! This is inconsistent and can lead to unwanted behaviour (weights > 1, variance < 0 )! Please check your input histograms. In order to avoid negative variances, the efficiency in this bin will be set to 0. " << std::endl;
+        m_numerator_hist->SetBinContent(i,0);
       }
     }
   }
@@ -119,36 +119,36 @@ APReweight::APReweight(TH1* denominator_in, TH1* numerator_in, bool isTrig) : AP
 
 void APReweight::ReadEfficiency(TH1* efficiency_in, TH1* err_low_in, TH1* err_high_in) {
   if (err_high_in == 0) err_high_in = err_low_in;
-  _empty_weight = new APWeightEntry(0, 0, 1.);
-  _denominator_hist = new TH1D("", "", 1, 0., 1.);
-  _numerator_hist = new TH1D("", "", 1, 0., 1.);
-  _axis = (TAxis*) efficiency_in->GetXaxis()->Clone("");
-  _n_bins = efficiency_in->GetNbinsX();
-  _scale = 1.0;
-  _isTrig = true;
-  for (unsigned int i = 0; i < _n_bins; ++i) {
+  m_empty_weight = new APWeightEntry(0, 0, 1.);
+  m_denominator_hist = new TH1D("", "", 1, 0., 1.);
+  m_numerator_hist = new TH1D("", "", 1, 0., 1.);
+  m_axis = (TAxis*) efficiency_in->GetXaxis()->Clone("");
+  m_n_bins = efficiency_in->GetNbinsX();
+  m_scale = 1.0;
+  m_isTrig = true;
+  for (unsigned int i = 0; i < m_n_bins; ++i) {
     APWeightEntry *temp_entry = new APWeightEntry();
     temp_entry->ReadEfficiency(efficiency_in->GetBinContent(i + 1), err_low_in->GetBinContent(i + 1), err_high_in->GetBinContent(i + 1));
-    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,_n_bins));
-    temp_entry->SetID(_ID);
-    _weights.push_back(temp_entry);
+    temp_entry->SetCoordinates(vector<int>(1,i+1),vector<int>(1,m_n_bins));
+    temp_entry->SetID(m_ID);
+    m_weights.push_back(temp_entry);
   }
 }
 
 APReweight::~APReweight() {
-  delete _denominator_hist;
-  delete _numerator_hist;
-  delete _empty_weight;
-  delete _axis;
-  for (vector<APWeightEntry*>::reverse_iterator it = _weights.rbegin(); it != _weights.rend(); ++it) {
+  delete m_denominator_hist;
+  delete m_numerator_hist;
+  delete m_empty_weight;
+  delete m_axis;
+  for (vector<APWeightEntry*>::reverse_iterator it = m_weights.rbegin(); it != m_weights.rend(); ++it) {
     delete *it;
   }
-  _weights.clear();
+  m_weights.clear();
 }
 
 APWeightEntry* APReweight::GetBinWeight(unsigned int bin) const {
-  if (bin == 0) return _empty_weight;
-  return _weights[bin - 1];
+  if (bin == 0) return m_empty_weight;
+  return m_weights[bin - 1];
 }
 
 APWeightEntry* APReweight::GetWeight(double value) const {
@@ -156,37 +156,37 @@ APWeightEntry* APReweight::GetWeight(double value) const {
 }
 
 TH1D* APReweight::GetDenominatorHist() const {
-  return _denominator_hist;
+  return m_denominator_hist;
 }
 
 TH1D* APReweight::GetNumeratorHist() const {
-  return _numerator_hist;
+  return m_numerator_hist;
 }
 
 double APReweight::GetSampleScale() const {
-  return _scale;
+  return m_scale;
 }
 
 unsigned int APReweight::NBins() const {
-  return _n_bins;
+  return m_n_bins;
 }
 
 unsigned int APReweight::GetBin(double value) const {
-  for (unsigned int i = 1; i <= _n_bins; ++i) {
-    if (value >= _axis->GetBinLowEdge(i) && value <= _axis->GetBinUpEdge(i)) {
+  for (unsigned int i = 1; i <= m_n_bins; ++i) {
+    if (value >= m_axis->GetBinLowEdge(i) && value <= m_axis->GetBinUpEdge(i)) {
       return i;
     }
   }
-  if (!_isQuiet) cout << "WARNING in APReweight::GetBin: Value out of range! Returning 0." << endl;
+  if (!m_isQuiet) cout << "WARNING in APReweight::GetBin: Value out of range! Returning 0." << endl;
   return 0;
 }
 
 void APReweight::SetSystUncert(double rel_uncert) {
-  for (unsigned int i = 1; i <= _n_bins; ++i) {
+  for (unsigned int i = 1; i <= m_n_bins; ++i) {
     GetBinWeight(i)->SetSystUncert(rel_uncert);
   }
 }
 
 void APReweight::SetQuietMode(bool isQuiet) {
-  _isQuiet = isQuiet;
+  m_isQuiet = isQuiet;
 }
--- PhysicsAnalysis/AnalysisCommon/ReweightUtils/Root/APWeightHist.cxx
+++ PhysicsAnalysis/AnalysisCommon/ReweightUtils/Root/APWeightHist.cxx
@@ -16,27 +16,27 @@ ClassImp(APWeightHist)
 using namespace std;
 
 APWeightHist::APWeightHist(const char *name, const char *title, const int n_bins, const float x_min, const float x_max)
-  : TH1D(name, title, n_bins, x_min, x_max),
-    _prec(0)
+  : TH1D(name, title, n_bins, x_min, x_max)
+    //m_prec(0)
 {
-  _graph_stat = new TGraphAsymmErrors();
-  _graph_stat->GetXaxis()->SetTitle(GetXaxis()->GetTitle());
-  _graph_stat->GetYaxis()->SetTitle(GetYaxis()->GetTitle());
-  _graph_syst = new TGraphErrors();
-  _graph_syst->GetXaxis()->SetTitle(GetXaxis()->GetTitle());
-  _graph_syst->GetYaxis()->SetTitle(GetYaxis()->GetTitle());
-  _binned_weights = vector< vector< APWeightEntry* > >(n_bins);
+  m_graph_stat = new TGraphAsymmErrors();
+  m_graph_stat->GetXaxis()->SetTitle(GetXaxis()->GetTitle());
+  m_graph_stat->GetYaxis()->SetTitle(GetYaxis()->GetTitle());
+  m_graph_syst = new TGraphErrors();
+  m_graph_syst->GetXaxis()->SetTitle(GetXaxis()->GetTitle());
+  m_graph_syst->GetYaxis()->SetTitle(GetYaxis()->GetTitle());
+  m_binned_weights = vector< vector< APWeightEntry* > >(n_bins);
   Sumw2();
-  fSumSys2 = vector< double >(n_bins, 0.);
-  _computed_entries = 0.;
+  m_SumSys2 = vector< double >(n_bins, 0.);
+  m_computed_entries = 0.;
 }
 
 APWeightHist::APWeightHist()
-  : _graph_stat(0),
-    _graph_syst(0),
-    _prec(0)
+  : m_graph_stat(0),
+    m_graph_syst(0)
+    //m_prec(0)
 {
-  _computed_entries = 0.;
+  m_computed_entries = 0.;
 }
 
 APWeightHist::~APWeightHist() {
@@ -58,29 +58,29 @@ int APWeightHist::Fill(const double value, APWeightEntry* weight) {
   fTsumw2 += z*z;
   fTsumwx += z*value;
   fTsumwx2 += z * value*value;
-  (_binned_weights[bin - 1]).push_back(weight);
+  (m_binned_weights[bin - 1]).push_back(weight);
   fSumw2.fArray[bin] += weight->GetVariance() + z*z;
-  fSumSys2[bin - 1] += weight->GetSysUncert2();
+  m_SumSys2[bin - 1] += weight->GetSysUncert2();
   return bin;
 }
 
 void APWeightHist::ComputeGraph(const int prec) {
-  for (int i = 0, I = _graph_stat->GetN(); i < I; ++i) _graph_stat->RemovePoint(i);
-  for (int i = 0, I = _graph_syst->GetN(); i < I; ++i) _graph_syst->RemovePoint(i);
+  for (int i = 0, I = m_graph_stat->GetN(); i < I; ++i) m_graph_stat->RemovePoint(i);
+  for (int i = 0, I = m_graph_syst->GetN(); i < I; ++i) m_graph_syst->RemovePoint(i);
   vector< TH1D* > original_dists = vector< TH1D* >(fXaxis.GetNbins());
-  _bin_dists = vector< TH1D* >(fXaxis.GetNbins());
+  m_bin_dists = vector< TH1D* >(fXaxis.GetNbins());
   int point_count = 0;
   for (int j = 0; j < fXaxis.GetNbins(); ++j) {
     //Create Poisson distributions for original histogram.
-    double unweighted_content = (double) (_binned_weights[j]).size();
+    double unweighted_content = (double) (m_binned_weights[j]).size();
     double sig_low_o = (max((double) 0.0, (unweighted_content - 5 * sqrt(unweighted_content))));
     double sig_high_o = (unweighted_content + 5 * sqrt(unweighted_content));
-    double step = (sig_high_o - sig_low_o) / 1000.;
+    double step = (sig_high_o - sig_low_o) / 1000;
 
     original_dists[j] = new TH1D("", "", 1000, sig_low_o, sig_high_o);
 
     int bin_no = 1;
-    double curr_fact = ln_factorialApp((_binned_weights[j]).size());
+    double curr_fact = ln_factorialApp((m_binned_weights[j]).size());
     for (double lambda = sig_low_o; lambda < sig_high_o; lambda += step) {
       double value = exp(-lambda + unweighted_content * log(lambda) - curr_fact);
       if (value != value) value = 0.;
@@ -96,7 +96,7 @@ void APWeightHist::ComputeGraph(const int prec) {
     double min_value = 1.e100;
 
     for (vector<double>::iterator it_hist = bin_sumw.begin(), it_histE = bin_sumw.end(); it_hist != it_histE; ++it_hist) {
-      for (vector<APWeightEntry*>::iterator it_func = (_binned_weights[j]).begin(), it_funcE = (_binned_weights[j]).end(); it_func != it_funcE; ++it_func) {
+      for (vector<APWeightEntry*>::iterator it_func = (m_binned_weights[j]).begin(), it_funcE = (m_binned_weights[j]).end(); it_func != it_funcE; ++it_func) {
         *it_hist += (*it_func)->GetRandom();
       }
       double cur_val = *it_hist;
@@ -124,7 +124,7 @@ void APWeightHist::ComputeGraph(const int prec) {
     scaled_hist_h->GetQuantiles(1, quant, prob);
     double sig_high = quant[0];
 
-    _bin_dists[j] = new TH1D("", "", 1000, sig_low, sig_high);
+    m_bin_dists[j] = new TH1D("", "", 1000, sig_low, sig_high);
 
     //fill final distribution for this bin with scaled/weighted Poisson distributions
     if (unweighted_content > 0.) {
@@ -134,47 +134,47 @@ void APWeightHist::ComputeGraph(const int prec) {
         double temp_scale = new_val / old_mode;
         scaled_hist->GetXaxis()->SetLimits(temp_scale * sig_low_o, temp_scale * sig_high_o);
         for (int l = 1; l < 1001; ++l) {
-          (_bin_dists[j])->AddBinContent(l, scaled_hist->GetBinContent(scaled_hist->FindBin((_bin_dists[j])->GetBinCenter(l))));
+          (m_bin_dists[j])->AddBinContent(l, scaled_hist->GetBinContent(scaled_hist->FindBin((m_bin_dists[j])->GetBinCenter(l))));
         }
         delete scaled_hist;
       }
-      double local_value = sig_low + (_bin_dists[j])->GetMaximumBin() * ((sig_high - sig_low) / 1000.);
-      _graph_stat->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), local_value);
-      _graph_syst->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), local_value);
+      double local_value = sig_low + (m_bin_dists[j])->GetMaximumBin() * ((sig_high - sig_low) / 1000);
+      m_graph_stat->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), local_value);
+      m_graph_syst->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), local_value);
       double quantile[ 2 ];
       double conf[ 2 ] = {0.1585, 0.8415};
-      (_bin_dists[j])->GetQuantiles(2, quantile, conf);
-      _graph_stat->SetPointError(point_count, GetBinWidth(j + 1) / 2, GetBinWidth(j + 1) / 2, local_value - quantile[0], quantile[1] - local_value);
-      _graph_syst->SetPointError(point_count, GetBinWidth(j + 1) / 2, local_value * (sqrt(fSumSys2[j]) / GetBinContent(j + 1)));
+      (m_bin_dists[j])->GetQuantiles(2, quantile, conf);
+      m_graph_stat->SetPointError(point_count, GetBinWidth(j + 1) / 2, GetBinWidth(j + 1) / 2, local_value - quantile[0], quantile[1] - local_value);
+      m_graph_syst->SetPointError(point_count, GetBinWidth(j + 1) / 2, local_value * (sqrt(m_SumSys2[j]) / GetBinContent(j + 1)));
       ++point_count;
     }
   }
-  _computed_entries = fEntries;
+  m_computed_entries = fEntries;
 }
 
 TGraphAsymmErrors* APWeightHist::GetGraphStatUncert(bool autocompute) {
-  if (autocompute && fEntries > _computed_entries) ComputeGraph();
-  return _graph_stat;
+  if (autocompute && fEntries > m_computed_entries) ComputeGraph();
+  return m_graph_stat;
 }
 
 TGraphErrors* APWeightHist::GetGraphSystUncert(bool simple, bool autocompute) {
   if (simple) {
-    for (int i = 0, I = _graph_syst->GetN(); i < I; ++i) _graph_syst->RemovePoint(i);
+    for (int i = 0, I = m_graph_syst->GetN(); i < I; ++i) m_graph_syst->RemovePoint(i);
     int point_count = 0;
     for (int j = 0; j < fXaxis.GetNbins(); ++j) {
-      if ((_binned_weights[j]).size() > 0) {
-        _graph_syst->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), GetBinContent(j + 1));
-        _graph_syst->SetPointError(point_count, GetBinWidth(j + 1) / 2, sqrt(fSumSys2[j]));
+      if ((m_binned_weights[j]).size() > 0) {
+        m_graph_syst->SetPoint(point_count, (GetXaxis()->GetXmin() + j * GetBinWidth(j + 1) + GetBinWidth(j + 1) / 2), GetBinContent(j + 1));
+        m_graph_syst->SetPointError(point_count, GetBinWidth(j + 1) / 2, sqrt(m_SumSys2[j]));
         ++point_count;
       }
     }
-    return _graph_syst;
+    return m_graph_syst;
   }
-  if (autocompute && fEntries > _computed_entries) ComputeGraph();
-  return _graph_syst;
+  if (autocompute && fEntries > m_computed_entries) ComputeGraph();
+  return m_graph_syst;
 }
 
 TH1D* APWeightHist::GetBinPDF(unsigned int bin, bool autocompute) {
-  if (autocompute && fEntries > _computed_entries) ComputeGraph();
-  return _bin_dists[bin - 1];
+  if (autocompute && fEntries > m_computed_entries) ComputeGraph();
+  return m_bin_dists[bin - 1];
 }
