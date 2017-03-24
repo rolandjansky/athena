af57e4045befd3fc8ae54c9ffde604e33f8f7fb8
scott snyder <scott.snyder@cern.ch>
2017-03-24 13:43:08 +0100

    SimHelpers: Comply with ATLAS naming conventions.
    Private class members should start with m_ (or s_ for statics).
    
    
    
    Former-commit-id: 772c7e19250068ebff10d64220c370fdb41c04af

--- Simulation/G4Sim/SimHelpers/src/SecondaryTracksHelper.cxx
+++ Simulation/G4Sim/SimHelpers/src/SecondaryTracksHelper.cxx
@@ -7,43 +7,43 @@
 
 
 
-const G4TrackingManager* SecondaryTracksHelper::theTrackingManager=0;
+const G4TrackingManager* SecondaryTracksHelper::s_theTrackingManager=0;
 
 SecondaryTracksHelper::SecondaryTracksHelper()
 {
-	currentNrOfSecondaries=0;
+	m_currentNrOfSecondaries=0;
 }
 SecondaryTracksHelper::SecondaryTracksHelper(const G4TrackingManager *tm)
 {
-	theTrackingManager=tm;
-	currentNrOfSecondaries=0;
+	s_theTrackingManager=tm;
+	m_currentNrOfSecondaries=0;
 }
 void SecondaryTracksHelper::SetTrackingManager(const G4TrackingManager *tm)
 {
-	theTrackingManager=tm;
+	s_theTrackingManager=tm;
 }
 const G4TrackingManager *SecondaryTracksHelper::GetTrackingManager()
 {
-	return theTrackingManager;
+	return s_theTrackingManager;
 }
 int SecondaryTracksHelper::NrOfNewSecondaries()
 {
 	int value=0;
-//	std::cout<<" NrOfNewSecondaries "<<currentNrOfSecondaries << 
+//	std::cout<<" NrOfNewSecondaries "<<m_currentNrOfSecondaries << 
 //	         " "<<CurrentNrOfSecondaries()<<std::endl;
 	int curr=CurrentNrOfSecondaries();
-	if (curr>currentNrOfSecondaries) 
+	if (curr>m_currentNrOfSecondaries) 
 	{
-		value=curr-currentNrOfSecondaries;
-		currentNrOfSecondaries=curr;
+		value=curr-m_currentNrOfSecondaries;
+		m_currentNrOfSecondaries=curr;
 	}
 	return value;
 }
 std::vector<G4Track*> SecondaryTracksHelper::GetSecondaries(int ntracks) const
 {
 	std::vector<G4Track*> vTrack;
-	int iSize=theTrackingManager->GimmeSecondaries()->size();
+	int iSize=s_theTrackingManager->GimmeSecondaries()->size();
 	for (int i=iSize-1;i>iSize-ntracks-1;i--) 
-		vTrack.push_back((*(theTrackingManager->GimmeSecondaries()))[i]);
+		vTrack.push_back((*(s_theTrackingManager->GimmeSecondaries()))[i]);
 	return vTrack;
 }
