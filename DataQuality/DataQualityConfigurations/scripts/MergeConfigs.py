697d8b7d977e34bf54a1e5ba0480aab4539cbcbe
Peter Onyisi <ponyisi@utexas.edu>
2017-03-27 12:52:33 -0500

    Write out Git info instead of SVN
    Former-commit-id: 9d6d1f37f71f98199848b16b1b73d64778f4a485

--- DataQuality/DataQualityConfigurations/scripts/MergeConfigs.py
+++ DataQuality/DataQualityConfigurations/scripts/MergeConfigs.py
@@ -102,9 +102,8 @@ def merge_han_configs(template, parent_dir, out, options):
     # if we got here, we should be ok on the unique names: pass 2
     outobj = open(out, 'w')
     outobj.write('# ****************************\n')
-    outobj.write('metadata SVNInfo {\n')
-    outobj.write('  Revision = $Revision: $\n')
-    outobj.write('  URL = $URL: $\n')
+    outobj.write('metadata GitInfo {\n')
+    outobj.write('  Hash = %s\n' % (os.environ.get('GIT_COMMIT_HASH', 'unknown')))
     outobj.write('}\n')
     outobj.write('# ****************************\n\n')
     for f in files:
