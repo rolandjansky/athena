2af23026f6a3770959d3ba890bc4cb132d44d288
morgens <marcus.matthias.morgenstern@cern.ch>
2017-03-28 18:06:36 +0200

    migrate MuonCombinedToolInterfaces-01-02-08-01
    Former-commit-id: 763bf7c9d3f5a61aee01f712d60db666b53176d5

--- Reconstruction/MuonIdentification/MuonCombinedToolInterfaces/MuonCombinedToolInterfaces/IMuonSegmentTagTool.h
+++ Reconstruction/MuonIdentification/MuonCombinedToolInterfaces/MuonCombinedToolInterfaces/IMuonSegmentTagTool.h
@@ -23,12 +23,15 @@ namespace MuonCombined {
    */
 
   class IMuonSegmentTagTool : virtual public IAlgTool {
+    using SegmentMap = std::map< const Muon::MuonSegment*, ElementLink<xAOD::MuonSegmentContainer> >;
+
   public:
     static const InterfaceID& interfaceID( ) ;
 
 
     /**IMuonSegmentTagTool interface: build muons from ID and MuonSegments */    
     virtual void tag( const InDetCandidateCollection& inDetCandidates, const xAOD::MuonSegmentContainer& segments ) const = 0;
+    virtual void tag( const InDetCandidateCollection& inDetCandidates, const std::vector<const Muon::MuonSegment*>& segments, SegmentMap* segmentToxAODSegmentMap ) const = 0;
 
   };
 
