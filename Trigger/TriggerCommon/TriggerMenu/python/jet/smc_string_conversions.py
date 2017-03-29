883769208b91ce3e99936110531c996577e3133c
Heather Russell <heather.russell@cern.ch>
2017-03-29 11:27:25 +0200

    Removing an unnecessary whitespace line
    Former-commit-id: 4399d50f4f59d8354ebe5fb2d798662bbb8c4629

--- Trigger/TriggerCommon/TriggerMenu/python/jet/smc_string_conversions.py
+++ Trigger/TriggerCommon/TriggerMenu/python/jet/smc_string_conversions.py
@@ -3,7 +3,6 @@
 """convert standard string such as nosmc, 0smc30, -INFsmc30,
 30smcINF, to smc_min, smc_max strings"""
 
-
 import re
 smc_range_re = re.compile(r'^(?P<smc_min>((-)?\d+|-?INF))smc(?P<smc_max>((-)?\d+|-?INF))$')
 
