a352939ef6af391b511af1496d055c30c9725862
Christian Gumpert <christian.gumpert@cern.ch>
2017-03-30 17:48:40 +0200

    fix syntax problem
    Former-commit-id: 70f4d4a9326e0c28fc10e6878b1eb17d155ee258

--- CI/sweep_MR.py
+++ CI/sweep_MR.py
@@ -199,7 +199,7 @@ def cherry_pick_mr(merge_commit,source_branch,target_branch_rules,project):
     if failed_branches:
         comment += "failed:  \n* " + "\n* ".join(sorted(failed_branches))
         # add label to original MR indicating cherry-pick problem
-        mr_handle.labels = list(set(mr_handle.labels) | "sweep:failed")
+        mr_handle.labels = list(set(mr_handle.labels) | {"sweep:failed"})
         mr_handle.save()
 
     # add sweep summary to MR in Gitlab
