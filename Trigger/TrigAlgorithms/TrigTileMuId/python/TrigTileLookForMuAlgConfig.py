5e1cf0c511ebde996c56d2241d2ccdd6b208c559
Sami Kama <sami@cern.ch>
2017-04-03 16:59:41 +0200

    Don't import * from AppMgr. Import only used components
    Former-commit-id: 41659196e5a6bbfdd12a64c541307c2c01762b43

--- Trigger/TrigAlgorithms/TrigTileMuId/python/TrigTileLookForMuAlgConfig.py
+++ Trigger/TrigAlgorithms/TrigTileMuId/python/TrigTileLookForMuAlgConfig.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-from AthenaCommon.AppMgr import *
+# from AthenaCommon.AppMgr import *
 
 try:
     from TrigTileMuId.TrigTileMuIdConf import TrigTileLookForMuAlg
