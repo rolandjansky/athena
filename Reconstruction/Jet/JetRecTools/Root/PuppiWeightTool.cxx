db30b2989136fc2cf554b27aa276350bf4d7b6f5
John Stupak <john.stupak@cern.ch>
2017-04-03 10:18:55 +0200

    overhaul of puppi tool
    Former-commit-id: a1c5878b9d4d500cc0978e1bda3a83e9ac46230a

--- Reconstruction/Jet/JetRecTools/Root/PuppiWeightTool.cxx
+++ Reconstruction/Jet/JetRecTools/Root/PuppiWeightTool.cxx
@@ -21,16 +21,20 @@ PuppiWeightTool::PuppiWeightTool(const std::string& name) : JetConstituentModifi
 
   declareProperty("R0", m_R0 = 0.3);
   declareProperty("Rmin", m_Rmin = 0.001);
-  declareProperty("Exponent", m_exponent = 1);
-  declareProperty("WeightCut",m_weightCut = 0);
-  declareProperty("ScaleFactorA", m_scaleFactorA = 0);
-  declareProperty("ScaleFactorB", m_scaleFactorB = 0);
-  declareProperty("ForwardScaleFactor",m_forwardSF = 1);
-  declareProperty("EtaForward",m_etaForward = 2.5);
+  declareProperty("Beta", m_beta = 1);
+  declareProperty("CentralPTCutOffset", m_centralPTCutOffset = 0);
+  declareProperty("CentralPTCutSlope", m_centralPTCutSlope = 0);
+  declareProperty("ForwardPTCutOffset", m_forwardPTCutOffset = 0);
+  declareProperty("ForwardPTCutSlope", m_forwardPTCutSlope = 0);
+  declareProperty("EtaBoundary",m_etaBoundary = 2.5);
+
   declareProperty("ApplyWeight",m_applyWeight=true);
+  declareProperty("PUPenalty", m_PUPenalty=false);
+  declareProperty("IncludeCentralNeutralsInAlpha",m_includeCentralNeutralsInAlpha=false);
+  declareProperty("IncludeLowPTTracks",m_includeLowPTTracks=false);
 
   // Where we no longer have charged and neutral information
-  //m_etaForward = 2.5;
+  //m_etaBoundary = 2.5;
 }
 
 //------------------------------------------------------------------------------
@@ -53,9 +57,12 @@ StatusCode PuppiWeightTool::process(xAOD::PFOContainer* cont) const{
   SG::AuxElement::Accessor<double> alphaAcc("PUPPI_alpha");
   SG::AuxElement::Accessor<double> weightAcc("PUPPI_weight");
 
+  SG::AuxElement::Accessor<float> z0SinThetaPUMinAcc("z0SinThetaPUMin");
+
   std::vector<fastjet::PseudoJet> chargedHSVector;
   std::vector<fastjet::PseudoJet> chargedPUVector;
-  std::vector<fastjet::PseudoJet> neutralAndForwardVector;
+  std::vector<fastjet::PseudoJet> neutralVector;
+  std::vector<fastjet::PseudoJet> forwardVector;
 
   // Fill input particle vectors for puppi
   for ( xAOD::PFO* ppfo : *cont ) {
@@ -66,32 +73,109 @@ StatusCode PuppiWeightTool::process(xAOD::PFOContainer* cont) const{
 
     if (ppfo->pt()<=0) continue;
 
+    fastjet::PseudoJet pj(ppfo->p4());
+
+    //pj.set_user_info(new PuppiUserInfo({someOtherChi2,yetAnotherChi2}));
+
     float charge = ppfo->charge();
 
-    if(charge == 0)  neutralAndForwardVector.push_back( fastjet::PseudoJet( ppfo->p4() ));
-		
+    if(fabs(ppfo->eta()) > m_etaBoundary) forwardVector.push_back(pj);
+    
     bool matchedToPrimaryVertex=PVMatchedAcc(*ppfo);
 
     if(charge != 0){
-      if(matchedToPrimaryVertex) chargedHSVector.push_back( fastjet::PseudoJet( ppfo->p4() ));
-      else chargedPUVector.push_back( fastjet::PseudoJet( ppfo->p4() ));
+      if(matchedToPrimaryVertex) chargedHSVector.push_back(pj);
+      else chargedPUVector.push_back(pj);
     }
-  }	
 
-  Puppi *m_Puppi = new Puppi(m_R0, m_Rmin, m_exponent, m_scaleFactorA, m_scaleFactorB, m_etaForward, m_forwardSF);
-  m_Puppi->SetParticles(chargedHSVector, chargedPUVector, neutralAndForwardVector, 1);
+    if(m_includeCentralNeutralsInAlpha){
+      std::cout<<"WARNING: you are doing something experimental"<<std::endl;
+      if(charge == 0 && fabs(ppfo->eta()) < m_etaBoundary) neutralVector.push_back(pj);
+    }
 
-  for ( xAOD::PFO* ppfo : *cont ) {
-    fastjet::PseudoJet pj(ppfo->p4());
+    //Test inclusion of low pT tracks
+    if (m_includeLowPTTracks){
+      std::cout<<"WARNING: you are doing something experimental"<<std::endl;
+      const xAOD::Vertex* vtx = nullptr; 
 
-    double weight = m_Puppi->getWeight(pj);
-    double alpha = m_Puppi->getAlpha(pj);
+      const xAOD::VertexContainer* pvtxs = 0;
+      ATH_CHECK(evtStore()->retrieve(pvtxs, "PrimaryVertices"));
+      if ( pvtxs == 0 || pvtxs->size()==0 ) {
+  ATH_MSG_WARNING(" This event has no primary vertices " );
+  return StatusCode::FAILURE;
+      }
 
-    if (ppfo->charge() == 0 && m_applyWeight) ppfo->setP4(weight*ppfo->p4());
-    alphaAcc(*ppfo) = alpha;
-    weightAcc(*ppfo) = weight;
+      //Usually the 0th vertex is the primary one, but this is not always the case. So we will choose the first vertex of type PriVtx
+    for (auto theVertex : *pvtxs) {
+  if (xAOD::VxType::PriVtx == theVertex->vertexType() ) {
+    vtx = theVertex;
+    break;
+  }//If we have a vertex of type primary vertex
+      }//iterate over the vertices and check their type
+
+      //If there is no primary vertex, then we cannot correct pflow inputs and hence we return (because this should not happen).
+      if (nullptr == vtx) {
+  ATH_MSG_VERBOSE("Could not find a primary vertex in this event " );
+  for (auto theVertex : *pvtxs) {
+    if (xAOD::VxType::NoVtx == theVertex->vertexType() ) {
+      vtx = theVertex;
+      break;
+    }
   }
+  if (nullptr == vtx) {
+    ATH_MSG_WARNING("Could not find a NoVtx in this event " );
+    return StatusCode::FAILURE;
+  }
+      }
 
-  ATH_MSG_INFO("Leaving execute");
-  return StatusCode::SUCCESS;
+      const xAOD::TrackParticleContainer *tracks=0;
+      evtStore()->retrieve(tracks, "InDetTrackParticles");
+      for (auto track: *tracks){
+
+  bool matchedToPrimaryVertex = false;
+
+  //vtz.z() provides z of that vertex w.r.t the center of the beamspot (z = 0). Thus we corrext the track z0 to be w.r.t z = 0
+  float z0 = track->z0() + track->vz() - vtx->z();
+  float theta = track->theta();
+  if ( fabs(z0*sin(theta)) < 2.0 ) {
+    matchedToPrimaryVertex = true;
+  }
+  if( track->pt()<800){  //Not sure what the threshold should be
+    if(matchedToPrimaryVertex) chargedHSVector.push_back( fastjet::PseudoJet( track->p4() ));
+    else chargedPUVector.push_back( fastjet::PseudoJet( track->p4() ));
+  }
+      }
+    }
 }
+
+    Puppi* puppi = new Puppi(m_R0, m_Rmin, m_beta, m_centralPTCutOffset, m_centralPTCutSlope, m_forwardPTCutOffset, m_forwardPTCutSlope,m_etaBoundary, m_PUPenalty);
+    puppi->setParticles(chargedHSVector, chargedPUVector, neutralVector, forwardVector, 0);
+
+    for ( xAOD::PFO* ppfo : *cont ) {
+      fastjet::PseudoJet pj(ppfo->p4());
+
+      double weight = puppi->getWeight(pj);
+      double alpha = puppi->getAlpha(pj);
+
+      if (ppfo->charge() == 0 && m_applyWeight) ppfo->setP4(weight*ppfo->p4());
+      alphaAcc(*ppfo) = alpha;
+      weightAcc(*ppfo) = weight;
+    }
+
+    /*
+    if(m_limitRemoval){
+      xAOD:PFOContainer neutralAndForward;
+      for ( xAOD::PFO* ppfo : *cont ) {
+    }
+    */
+
+    //if(evtStore()->record( puppi, "puppi" ).isFailure()) ATH_MSG_WARNING("Couldnt store puppi");
+    //evtStore()->record(puppi->getMedian(0),"PUPPI_alpha_median");
+    //evtStore()->record(puppi->getRMS(0),"PUPPI_alpha_RMS");
+
+    std::cout<<"median: "<<puppi->getMedian()<<std::endl;
+    std::cout<<"RMS: "<<puppi->getRMS()<<std::endl;
+
+    ATH_MSG_INFO("Leaving execute");
+    return StatusCode::SUCCESS;
+  }
