ef41939883ae8e7343438a9cb0dc2e8ade9f38e7
Piotr Sarna <piotr.sarna@cern.ch>
2017-04-04 02:47:24 +0200

    Update tests to GenericMonitoringTool
    Former-commit-id: b220e5e5daa4e11ddea5d824b6b3ee2ab36c6634

--- Control/AthenaMonitoring/AthenaMonitoring/GenericMonitoringToolTest.h
+++ Control/AthenaMonitoring/AthenaMonitoring/GenericMonitoringToolTest.h
@@ -18,6 +18,11 @@ public:
 
 private:
     ToolHandle<GenericMonitoringTool> m_monitoringTool;
+
+    void scalarTest();
+    void valuesCollectionTest();
+    void objectsCollectionTest();
+    void inLoopTest();
 };
 
 #endif
