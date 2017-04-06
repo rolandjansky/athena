f6cbd2a5873d6aaac9cc7dfe588c62c1f274fd1a
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2017-04-06 16:24:50 +0100

    PFOClusterMomentPlots: Update list of moments to be consistent with the new list mandated by the AOD reduction TF.
    Former-commit-id: c7c4df937bd3a5308b4b7b3f6121f3923d34f46d

--- Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/PFOClusterMomentPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/PFOClusterMomentPlots.cxx
@@ -7,55 +7,37 @@
 namespace PFO {
 
   PFOClusterMomentPlots::PFOClusterMomentPlots(PlotBase* pParent, std::string sDir, std::string sPFOContainerName) : PlotBase(pParent, sDir), m_sPFOContainerName(sPFOContainerName){
-    m_PFO_LATERAL = nullptr;
-    m_PFO_LONGITUDINAL = nullptr;
     m_PFO_SECOND_R = nullptr;
     m_PFO_CENTER_LAMBDA = nullptr;
-    m_PFO_FIRST_ENG_DENS = nullptr;
-    m_PFO_ENG_FRAC_MAX = nullptr;
     m_PFO_ISOLATION = nullptr;
     m_PFO_ENG_BAD_CELLS = nullptr;
     m_PFO_N_BAD_CELLS = nullptr;
     m_PFO_BADLARQ_FRAC = nullptr;
     m_PFO_ENG_POS = nullptr;
-    m_PFO_SIGNIFICANCE = nullptr;
     m_PFO_AVG_LAR_Q = nullptr;
     m_PFO_AVG_TILE_Q = nullptr;
+    m_PFO_EM_PROBABILTY = nullptr;
 }
 
   void PFOClusterMomentPlots::initializePlots(){
 
-    m_PFO_LATERAL = Book1D("PFO_LATERAL",m_sPFOContainerName + "_LATERAL",60,-1.0,2.0);
-    m_PFO_LONGITUDINAL = Book1D("PFO_LONGITUDINAL",m_sPFOContainerName + "_LONGITUDINAL",60,-1.0,2.0);
     m_PFO_SECOND_R = Book1D("PFO_SECOND_R",m_sPFOContainerName + "_SECOND_R",60,-1.0,50.0); 
     m_PFO_CENTER_LAMBDA = Book1D("PFO_CENTER_LAMBDA",m_sPFOContainerName + "_CENTER_LAMBDA",60,-1.0,3000.0);
-    m_PFO_FIRST_ENG_DENS = Book1D("PFO_FIRST_ENG_DENS",m_sPFOContainerName + "_FIRST_ENG_DENS",26,-1.0,0.3);
-    m_PFO_ENG_FRAC_MAX = Book1D("PFO_ENG_FRAC_MAX",m_sPFOContainerName + "_ENG_FRAC_MAX",60,-1.0,2.0);
     m_PFO_ISOLATION = Book1D("PFO_ISOLATION",m_sPFOContainerName + "_ISOLATION",60,-1.0,2.0);
     m_PFO_ENG_BAD_CELLS = Book1D("PFO_ENG_BAD_CELLS",m_sPFOContainerName + "_ENG_BAD_CELLS",20,-1.0,1);
     m_PFO_N_BAD_CELLS = Book1D("PFO_N_BAD_CELLS",m_sPFOContainerName + "_N_BAD_CELLS",30,-1.0,2.0);
     m_PFO_BADLARQ_FRAC = Book1D("PFO_BADLARQ_FRAC",m_sPFOContainerName + "_BADLARQ_FRAC",25,-1.0,1.5);
     m_PFO_ENG_POS = Book1D("PFO_ENG_POS",m_sPFOContainerName + "_ENG_POS",60,-100.0,10000.0);
-    m_PFO_SIGNIFICANCE = Book1D("PFO_SIGNIFICANCE",m_sPFOContainerName + "_SIGNIFICANCE",300,-20.0,20.0);
     m_PFO_AVG_LAR_Q = Book1D("PFO_AVG_LAR_Q",m_sPFOContainerName + "_AVG_LAR_Q",240,-1.0,200.0);
     m_PFO_AVG_TILE_Q = Book1D("PFO_AVG_TILE_Q",m_sPFOContainerName + "_AVG_TILE_Q",21,-1.0,20.0);
+    m_PFO_EM_PROBABILTY = Book1D("PFO_EM_PROBABILTY",m_sPFOContainerName + "_EM_PROBABILTY",30,0,3);
 
   }
 
   void PFOClusterMomentPlots::fill(const xAOD::PFO& PFO){
 
-    float moment_LATERAL = 0.0;
-    bool gotMoment = PFO.getClusterMoment(moment_LATERAL,xAOD::CaloCluster::LATERAL);
-    if (true == gotMoment) m_PFO_LATERAL->Fill(moment_LATERAL);
-    else m_PFO_LATERAL->Fill(-1.0);
-
-    float moment_LONGITUDINAL = 0.0;
-    gotMoment = PFO.getClusterMoment(moment_LONGITUDINAL,xAOD::CaloCluster::LONGITUDINAL);
-    if (true == gotMoment) m_PFO_LONGITUDINAL->Fill(moment_LONGITUDINAL);
-    else m_PFO_LONGITUDINAL->Fill(-1.0);
-    
     float moment_SECOND_R = 0.0;
-    gotMoment = PFO.getClusterMoment(moment_SECOND_R,xAOD::CaloCluster::SECOND_R);
+    bool gotMoment = PFO.getClusterMoment(moment_SECOND_R,xAOD::CaloCluster::SECOND_R);
     if (true == gotMoment) m_PFO_SECOND_R->Fill(moment_SECOND_R);
     else m_PFO_SECOND_R->Fill(-1.0);
 
@@ -64,16 +46,6 @@ namespace PFO {
     if (true == gotMoment) m_PFO_CENTER_LAMBDA->Fill(moment_CENTER_LAMBDA);
     else m_PFO_CENTER_LAMBDA->Fill(-1.0);
     
-    float moment_FIRST_ENG_DENS = 0.0;
-    gotMoment = PFO.getClusterMoment(moment_FIRST_ENG_DENS,xAOD::CaloCluster::FIRST_ENG_DENS);
-    if (true == gotMoment) m_PFO_FIRST_ENG_DENS->Fill(moment_FIRST_ENG_DENS);
-    else m_PFO_FIRST_ENG_DENS->Fill(-1.0);
-
-    float moment_ENG_FRAC_MAX = 0.0;
-    gotMoment = PFO.getClusterMoment(moment_ENG_FRAC_MAX,xAOD::CaloCluster::ENG_FRAC_MAX);
-    if (true == gotMoment) m_PFO_ENG_FRAC_MAX->Fill(moment_ENG_FRAC_MAX);
-    else m_PFO_ENG_FRAC_MAX->Fill(-1.0);
-
     float moment_ISOLATION = 0.0;
     gotMoment = PFO.getClusterMoment(moment_ISOLATION,xAOD::CaloCluster::ISOLATION);
     if (true == gotMoment) m_PFO_ISOLATION->Fill(moment_ISOLATION);
@@ -99,11 +71,6 @@ namespace PFO {
     if (true == gotMoment) m_PFO_ENG_POS->Fill(moment_ENG_POS);
     else m_PFO_ENG_POS->Fill(-1.0);
 
-    float moment_SIGNIFICANCE = 0.0;
-    gotMoment = PFO.getClusterMoment(moment_SIGNIFICANCE,xAOD::CaloCluster::SIGNIFICANCE);
-    if (true == gotMoment) m_PFO_SIGNIFICANCE->Fill(moment_SIGNIFICANCE);
-    else m_PFO_SIGNIFICANCE->Fill(-1.0);
-
     float moment_AVG_LAR_Q = 0.0;
     gotMoment = PFO.getClusterMoment(moment_AVG_LAR_Q,xAOD::CaloCluster::AVG_LAR_Q);
     if (true == gotMoment) m_PFO_AVG_LAR_Q->Fill(moment_AVG_LAR_Q);
@@ -113,6 +80,11 @@ namespace PFO {
     gotMoment = PFO.getClusterMoment(moment_AVG_TILE_Q,xAOD::CaloCluster::AVG_TILE_Q);
     if (true == gotMoment) m_PFO_AVG_TILE_Q->Fill(moment_AVG_TILE_Q);
     else m_PFO_AVG_TILE_Q->Fill(-1.0);
+
+    float moment_EM_PROBABILTY = 0.0;
+    gotMoment = PFO.getClusterMoment(moment_EM_PROBABILTY,xAOD::CaloCluster::EM_PROBABILITY);
+    if (true == gotMoment) m_PFO_EM_PROBABILTY->Fill(moment_EM_PROBABILTY);
+    else m_PFO_EM_PROBABILTY->Fill(-1.0);
 
   }
 
