e5133eba72b8438212b818c9cb43bf8d1477e365
Shaun Roe <shaun.roe@cern.ch>
2017-04-10 09:23:18 +0200

    Pull from SVN for ITk development
    Former-commit-id: aa9bcb03269ac1aa78a80086d941fbfb75d85a3c

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/src/GetMeanWidth.h
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/src/GetMeanWidth.h
@@ -26,39 +26,53 @@ using std::vector;
   useful eg for histograms with long tails etc.
 */
 namespace IDPVM {
+  
   class GetMeanWidth {
   public:
     GetMeanWidth();
-    ~GetMeanWidth(){};
+    ~GetMeanWidth() {
+      // nop
+    };
     
     // methods acc to which mean&RMS can be evaluated
-    enum m_methods { iterative_RMS_convergence, Gauss_fit, fusion_iterRMS_Gaussfit };
+    enum methods { iterRMS_convergence, Gauss_fit, fusion_iterRMS_Gaussfit };
     
-    // wrapper: 
+    // wrapper to set mean,rms,and fraction of events in tails
     // nb: some of the methods are allowed to modify input histogram
-    // p_result will be filled as
-    // <mean,mean_error,RMS,RMS_error>
-    // +<fOut,foutUnc>, where fOut is a fraction of events out of signal region
-    //                  and foutUnc its approximate uncertainty
-    void getResults(TH1* p_input_hist, vector<float>& p_result, m_methods p_method);
+    void setResults(TH1* p_input_hist, methods p_method);
 
+    // results getters
+    double getMean() { return m_mean; };
+    double getMeanError() { return m_meanError; };
+    double getRMS() { return m_RMS; };
+    double getRMSError() { return m_RMSError; };
+    double getFracOut() { return m_FracOut; };
+    double getFracOutUnc() { return m_FracOutUnc; };
     // return and clear accumulated warnings and errors
-    string flushWarning();
-    string flushError();
+    vector<string> getInfos() { return m_infos; };
+    vector<string> getWarnings() { return m_warnings; };
+    vector<string> getErrors() { return m_errors; };
     
   private:
     // use gaussian fit, return 0 in case of successful fit
-    int getGaussFit(TH1* p_input_hist, vector<float>& p_result);
+    int setGaussFit(TH1* p_input_hist);
     // iteratively change histogram range, until convergence
     // return # remaining iterations before hitting the max. allowed
-    int getIterativeConvergence(TH1* p_input_hist, vector<float>& p_result);
+    int setIterativeConvergence(TH1* p_input_hist);
     // evaluate the fraction of evens out of signal region and its uncertainty
-    void getFout(double p_nsig,double p_ntot,double& p_resFOut, double& p_resUncOut);
+    void setFout(double p_nsig,double p_ntot);
     // helper to fill-in starting values of the results vector
-    void setStartResult(TH1* p_input_hist, vector<float>& p_result);
-      
-    string m_warning;
-    string m_error;
+    bool initialize(TH1* p_input_hist);
+
+
+    // results/outputs:
+    double m_mean,m_meanError,m_RMS,m_RMSError,m_FracOut,m_FracOutUnc;  
+    vector<string> m_infos;
+    vector<string> m_warnings;
+    vector<string> m_errors;
+    
+    //helpers 
+    string m_inHistName;   
   };
 }
 #endif
