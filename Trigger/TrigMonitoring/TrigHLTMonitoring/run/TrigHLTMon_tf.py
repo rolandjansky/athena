164262f8bf4e56c5f2a19f827a7b4e91e13d91fc
Ryan Mackenzie White <ryan.white@cern.ch>
2017-04-13 14:13:03 +0200

    adding new files
    Former-commit-id: cd8574d5209b4c1f702d879195c294bcffbc88b0

---
+++ Trigger/TrigMonitoring/TrigHLTMonitoring/run/TrigHLTMon_tf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+__doc__ = """JobTransform to run TrigHLTMonitoring standalone jobs"""
+
+
+import sys
+from PyJobTransforms.transform import transform
+from PyJobTransforms.trfExe import athenaExecutor
+from PyJobTransforms.trfArgs import addAthenaArguments, addDetectorArguments
+import PyJobTransforms.trfArgClasses as trfArgClasses
+
+if __name__ == '__main__':
+
+    executorSet = set()
+    executorSet.add(athenaExecutor(name = 'HLTMon', skeletonFile = 'TrigHLTMonitoring/skeleton.HLTMon_tf.py',
+                                   substep = 'r2a', inData = ['BS','AOD'], outData = ['HIST']))
+    
+    trf = transform(executor = executorSet)  
+    addAthenaArguments(trf.parser)
+    trf.parser.defineArgGroup('TrigHLTMon', 'HLT Monitoring transform arguments')
+    trf.parser.add_argument('--inputBSFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argBSFile, io='input'),
+                            help='Input bytestream file', group='TrigHLTMon args')
+    
+    trf.parser.add_argument('--inputAODFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argPOOLFile, io='input'),
+                            help='Input pool file', group='TrigHLTMon args')
+
+    trf.parser.add_argument('--outputHISTFile', nargs='+',
+                            type=trfArgClasses.argFactory(trfArgClasses.argHISTFile, io='output'),
+                            help='Output HIST file', group='TrigHLTMon args')
+
+    trf.parser.add_argument('--useDB',type=trfArgClasses.argFactory(trfArgClasses.argBool),help='Use TrigConfigSvc',)
+
+    trf.parser.defineArgGroup('Monitoring Args', 'Monitoring options')
+    trf.parser.add_argument('--monFlags', nargs='+',type=trfArgClasses.argFactory(trfArgClasses.argList),
+            help='TrigHLTMon histogram switches', group='TrigHLTMon args')
+    trf.parseCmdLineArgs(sys.argv[1:])
+    trf.execute()
+    trf.generateReport()
+
+    
---
+++ Trigger/TrigMonitoring/TrigHLTMonitoring/share/TrigHLTMonCommon_jobOptions.py
@@ -0,0 +1,143 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+#-- set flags ------------------------------------------------------------------------------
+log = logging.getLogger( "TrigHLTMonCommon_jobOptions.py" )
+
+from AthenaCommon.GlobalFlags import globalflags
+from AthenaCommon.AppMgr import (theApp, ServiceMgr as svcMgr,ToolSvc)
+
+from RecExConfig.RecFlags import rec
+from RecExConfig.AutoConfiguration import GetProjectName,GetRunNumber,ConfigureTriggerStream,ConfigureSimulationOrRealData,ConfigureBeamType
+
+from AthenaCommon.BeamFlags import jobproperties
+runNb=GetRunNumber()
+projectName=GetProjectName()
+ConfigureTriggerStream()
+ConfigureSimulationOrRealData()
+ConfigureBeamType()
+
+data_type=globalflags.InputFormat.get_Value()
+beam_type=jobproperties.Beam.beamType.get_Value()
+#evt_type=inputFileSummary['evt_type']
+log.info("Run Number %s",runNb)
+log.info("Project name %s",projectName)
+log.info("File type %s",data_type)
+log.info("Beam type %s",beam_type)
+log.info("Trigger stream %s",rec.triggerStream())
+#log.info("Event type %s",evt_type)
+
+#set up DQMonFlags required for HLTMonitoring
+from AthenaMonitoring.DQMonFlags import DQMonFlags
+if globalflags.DataSource.get_Value() == 'geant4':
+    DQMonFlags.monManDataType = 'monteCarlo'
+elif (rec.doHeavyIon() or rec.doHIP()):
+    # requires something from InputFileSummary rather than rec flag
+    DQMonFlags.monManDataType = 'heavyioncollisions'
+    DQMonFlags.doHIMon = True
+elif jobproperties.Beam.beamType()   == 'cosmics':
+    DQMonFlags.monManDataType = 'cosmics'
+elif jobproperties.Beam.beamType() == 'collisions':
+    DQMonFlags.monManDataType = 'collisions'
+elif jobproperties.Beam.beamType() == 'singlebeam':
+    ## There is no singlebeam in AthenaMonitoring
+    DQMonFlags.monManDataType = 'collisions'
+else:
+    log.warning("invalid jobproperties.Beam.beamType(): %s, using default (%s)", 
+            jobproperties.Beam.beamType(), DQMonFlags.monManDataType)
+                             
+if data_type == 'bytestream':
+    DQMonFlags.monManEnvironment = 'tier0Raw'
+if data_type == 'pool':
+    DQMonFlags.monManEnvironment = 'tier0'
+
+DQMonFlags.monManStream=rec.triggerStream()
+log.info('DQMonFlags %s',DQMonFlags)
+
+log.info('RecFlags %s', rec)
+
+if not 'HLTMonFlags' in dir():
+    from TrigHLTMonitoring.HLTMonFlags import HLTMonFlags
+#-- set up job ------------------------------------------------------------------------------
+if data_type == 'bytestream':
+    log.info('BS to HIST Monitoring')
+    from AthenaCommon.AlgSequence import AlgSequence
+    topSequence = AlgSequence()
+    
+    # Only doGeneral monitoring from RAW
+    HLTMonFlags.set_All_Off()
+    HLTMonFlags.doGeneral.set_Value_and_Lock(True)
+    #HLTMonFlags.doCalo = False
+    #HLTMonFlags.doMonTier0=False
+    #-- set up BS reading ------------------------------------------------------------------------------
+
+    from ByteStreamCnvSvc import ReadByteStream
+    svcMgr.ByteStreamInputSvc.FullFileName = athenaCommonFlags.BSRDOInput()
+    if hasattr(runArgs,"skipEvents"):
+        svcMgr.EventSelector.SkipEvents=runArgs.skipEvents
+
+    if hasattr(runArgs,"maxEvents"):
+            theApp.EvtMax=runArgs.maxEvents
+    #-- get Trigger Configuration ------------------------------------------------------------------------------
+
+    from TriggerJobOpts.T0TriggerGetter import T0TriggerGetter
+    gettrigger = T0TriggerGetter()
+    
+    from TriggerJobOpts.HLTTriggerResultGetter import ByteStreamUnpackGetter
+    bsu=ByteStreamUnpackGetter()
+
+    theByteStreamAddressProviderSvc = svcMgr.ByteStreamAddressProviderSvc
+    theByteStreamAddressProviderSvc.TypeNames += ['HLT::HLTResult/HLTResult_HLT',
+        "ROIB::RoIBResult/RoIBResult",
+        "MuCTPI_RDO/MUCTPI_RDO",
+        "CTP_RDO/CTP_RDO",
+        ]
+
+if data_type == 'pool':
+    log.info('AOD to HIST Monitoring')
+    algseq = CfgMgr.AthSequencer("AthAlgSeq")
+    import AthenaPoolCnvSvc.ReadAthenaPool 
+    svcMgr.EventSelector.InputCollections=athenaCommonFlags.PoolAODInput()
+    
+    if hasattr(runArgs,"skipEvents"):
+        svcMgr.EventSelector.SkipEvents=runArgs.skipEvents
+
+    if hasattr(runArgs,"maxEvents"):
+            theApp.EvtMax=runArgs.maxEvents
+    
+    from TrigDecisionTool.TrigDecisionToolConf import Trig__TrigDecisionTool
+    from AthenaCommon.AppMgr import ToolSvc
+    ToolSvc += Trig__TrigDecisionTool( "TrigDecisionTool" )
+    from TrigEDMConfig.TriggerEDM import EDMLibraries
+    ToolSvc.TrigDecisionTool.Navigation.Dlls = [e for e in  EDMLibraries if 'TPCnv' not in e]
+    
+    if hasattr(runArgs,"useDB"):
+        if runArgs.useDB:
+            from TriggerJobOpts.TriggerConfigGetter import TriggerConfigGetter
+            cfg =  TriggerConfigGetter()
+
+            ToolSvc.TrigDecisionTool.TrigConfigSvc = "Trig::TrigConfigSvc/TrigConfigSvc"
+    
+    # enable basic flags to run monitoring 
+    # all other flags go through runArgs.monFlags
+    HLTMonFlags.doGeneral = True
+    HLTMonFlags.doMonTier0 = True
+    
+#-- set up output histogram file ------------------------------------------------------------------------------
+
+from GaudiSvc.GaudiSvcConf import THistSvc
+svcMgr += THistSvc()
+svcMgr.THistSvc.Output += ["GLOBAL DATAFILE='" + DQMonFlags.histogramFile() + "' OPT='RECREATE'"]
+
+include( "TrigHLTMonitoring/HLTMonitoring_topOptions.py" )
+
+if hasattr(runArgs,"useDB") or data_type == 'bytestream':
+    pass
+else:
+    log.info("Update tools to use xAODConfigTool")
+    for tool in ToolSvc.getAllChildren(): 
+        tool_prop = tool.getDefaultProperties()
+        for prop,value in tool_prop.iteritems():
+            if prop == "TrigConfigTool":
+                log.info("Set TrigConfigTool %s",tool.getName())
+                tool.TrigConfigTool="TrigConf::xAODConfigTool"
+        
