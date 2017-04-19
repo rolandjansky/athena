cf9f978c7c9b9ccd9e3643ee1cac8c4d7249cd6f
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-04-19 11:59:58 +0000

    Merge branch 'BMGTickSize' into '21.0'
    Fix BMG chamber TDC ticksize
    
    See merge request !994
    
    Former-commit-id: fa5a4c27d4e28604b7544e6b8115e5204bab9e1b

--- MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibSvc/MdtCalibSvc/MdtCalibrationSvc.h
+++ MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibSvc/MdtCalibSvc/MdtCalibrationSvc.h
@@ -14,6 +14,7 @@
 
 #include "AthenaBaseComps/AthService.h"
 #include "GaudiKernel/IInterface.h"
+#include "Identifier/Identifier.h"
 #include "MuonPrepRawData/MdtDriftCircleStatus.h"
 
 class MdtCalibHit;
@@ -93,12 +94,9 @@ public:
   */
   bool driftRadiusFromTime( MdtCalibHit &hit, const MdtCalibrationSvcInput &inputData, bool resolFromRtrack=false  );
 
-  /** TDC bin size. (25/32 ns for Atlas) */
-  double tdcBinSize();
+  /** TDC bin size. 25/32 ns for all MDT/sMDT, except BMG=0.2ns */
+  double tdcBinSize(const Identifier &id);
 
-  /** dummy function to be removed */
-  void triggerCounts(int) {}
- 
   /** Convert the raw MDT times of two twin hits into a Twin position (coordinate along tube)
       It returns whether the conversion was successful. */
   bool twinPositionFromTwinHits( MdtCalibHit &hit, MdtCalibHit &twinhit, double signedTrackLength, double twinSignedTrackLength, bool &twinDigitIsPrompt, double triggerTime = 0.0 );
