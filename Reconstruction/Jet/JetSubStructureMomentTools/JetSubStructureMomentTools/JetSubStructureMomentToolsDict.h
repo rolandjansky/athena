5aaca95b52cb62574b3e384f5d7a3a10a45d6735
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-04-19 11:03:01 +0200

    Updated the build configuration of JetSubStructureMomentTools to compile the package correctly as part of the AnalysisBase project.
    At the same time backported the Reflex dictionary of the package from the
    master branch, to make it possible to use the tool from PyROOT in an
    analysis.
    
    
    Former-commit-id: 3b45cb5e67eaaee978749d37c8356244925547c0

---
+++ Reconstruction/Jet/JetSubStructureMomentTools/JetSubStructureMomentTools/JetSubStructureMomentToolsDict.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef JETSUBSTRUCTUREMOMENTTOOLS_JETSUBSTRUCTUREMOMENTTOOLSDICT_H
+#define JETSUBSTRUCTUREMOMENTTOOLS_JETSUBSTRUCTUREMOMENTTOOLSDICT_H
+
+#include "JetSubStructureMomentTools/AngularityTool.h"
+#include "JetSubStructureMomentTools/DipolarityTool.h"
+#include "JetSubStructureMomentTools/JetChargeTool.h"
+#include "JetSubStructureMomentTools/NSubjettinessRatiosTool.h"
+#include "JetSubStructureMomentTools/ShowerDeconstructionTool.h"
+#include "JetSubStructureMomentTools/BoostedXbbTagTool.h"
+#include "JetSubStructureMomentTools/EnergyCorrelatorRatiosTool.h"
+#include "JetSubStructureMomentTools/JetPullTool.h"
+#include "JetSubStructureMomentTools/KTSplittingScaleTool.h"
+#include "JetSubStructureMomentTools/NSubjettinessTool.h"
+#include "JetSubStructureMomentTools/SubjetFinderTool.h"
+#include "JetSubStructureMomentTools/VolatilityTool.h"
+#include "JetSubStructureMomentTools/BosonTagTool.h"
+#include "JetSubStructureMomentTools/EnergyCorrelatorTool.h"
+#include "JetSubStructureMomentTools/JetSubStructureMomentToolsBase.h"
+#include "JetSubStructureMomentTools/KtDeltaRTool.h"
+#include "JetSubStructureMomentTools/PlanarFlowTool.h"
+#include "JetSubStructureMomentTools/SubjetMakerTool.h"
+#include "JetSubStructureMomentTools/CenterOfMassShapesTool.h"
+#include "JetSubStructureMomentTools/ISubjetRecorderTool.h"
+#include "JetSubStructureMomentTools/KtMassDropTool.h"
+#include "JetSubStructureMomentTools/QwTool.h"
+#include "JetSubStructureMomentTools/SubjetRecorderTool.h"
+#endif
