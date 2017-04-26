bba3d265210d148ca6fd84181aa2836e3b5b4f2b
Grzegorz Gach <grzegorz.gach@cern.ch>
2017-04-26 07:44:35 +0200

    Added xAOD::AFPSiHitsCluster and helper classes for station and layer ID.
    The new hitsCluster class represents clusters of htis and will be used
    as input to the track reconstruction algorithm.
    
    
    Former-commit-id: da89e0b15992881683ecd4922d4016c0ab9520ca

--- Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
+++ Event/xAOD/xAODForward/Root/dict/ContainerProxies.cxx
@@ -2,8 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id: ContainerProxies.cxx 766963 2016-08-08 20:07:09Z ggach $
-
 // EDM include(s):
 #include "xAODCore/AddDVProxy.h"
 
@@ -11,6 +9,7 @@
 #include "xAODForward/versions/ALFADataContainer_v1.h"
 #include "xAODForward/versions/AFPDataContainer_v1.h"
 #include "xAODForward/versions/AFPSiHitContainer_v1.h"
+#include "xAODForward/versions/AFPSiHitsClusterContainer_v1.h"
 #include "xAODForward/versions/AFPTrackContainer_v1.h"
 #include "xAODForward/versions/ZdcModuleContainer_v1.h"
 #include "xAODForward/versions/MBTSModuleContainer_v1.h"
@@ -20,6 +19,7 @@
 ADD_NS_DV_PROXY( xAOD, ALFADataContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, AFPDataContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, AFPSiHitContainer_v1 );
+ADD_NS_DV_PROXY( xAOD, AFPSiHitsClusterContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, AFPTrackContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, ZdcModuleContainer_v1 );
 ADD_NS_DV_PROXY( xAOD, MBTSModuleContainer_v1 );
