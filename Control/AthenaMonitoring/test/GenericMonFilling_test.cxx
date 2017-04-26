3943aa3b9b888f47f2df82f8b9d548505f8c4df6
Tomasz Bold <tomasz.bold@gmail.com>
2017-04-26 17:01:03 +0200

    Initial work on unit tests
    Former-commit-id: b960bc2c9f1c505f16bf360c490d95da1733d2b2

---
+++ Control/AthenaMonitoring/test/GenericMonFilling_test.cxx
@@ -0,0 +1,92 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+#include <iostream>
+
+#undef NDEBUG
+#include <cassert>
+
+
+#include "TestTools/initGaudi.h"
+#include "TestTools/expect.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/ITHistSvc.h"
+#include "AthenaKernel/getMessageSvc.h"
+#include "CxxUtils/ubsan_suppress.h"
+
+
+#include "AthenaMonitoring/GenericMonitoringTool.h"
+#include "AthenaMonitoring/MonitoredScope.h"
+#include "AthenaMonitoring/MonitoredScalar.h"
+
+void resetHist(ITHistSvc* histSvc, const std::string& histName) {
+  TH1* h(0);
+  histSvc->getHist(histName, h);
+  VALUE ( h ) NOT_EXPECTED ( nullptr );
+  h->Reset();
+}
+
+
+double contentInBin1DHist(ITHistSvc* histSvc, const std::string& histName, int bin) {
+  TH1* h(0);
+  histSvc->getHist(histName, h);
+  // this are in fact securing basic correctness of the tests
+  VALUE ( h )   NOT_EXPECTED ( nullptr );  
+  VALUE ( bin >= 1 ) EXPECTED ( true );
+  VALUE ( bin <= h->GetXaxis()->GetNbins()+1 ) EXPECTED ( true );
+  return h->GetBinContent(bin);
+}
+
+
+bool fillFromScalarWorked(ToolHandle<GenericMonitoringTool>& monTool, ITHistSvc* histSvc) {
+  resetHist(histSvc, "/EXPERT/TestGroup/Eta");
+  resetHist(histSvc, "/EXPERT/TestGroup/Phi");
+  using namespace Monitored;
+  {
+    auto roiPhi = MonitoredScalar::declare("Phi", -99.0); //deducted double      
+    auto roiEta = MonitoredScalar::declare<double>("Eta", -99); //explicit double
+    auto monitorIt = MonitoredScope::declare(monTool, roiPhi, roiEta);
+    roiPhi = 0.1;
+    roiEta = -0.2;    
+  }
+  VALUE ( contentInBin1DHist(histSvc, "/EXPERT/TestGroup/Phi", 1) ) EXPECTED ( 0 );
+  VALUE ( contentInBin1DHist(histSvc, "/EXPERT/TestGroup/Phi", 2) ) EXPECTED ( 1 );
+  
+  VALUE ( contentInBin1DHist(histSvc, "/EXPERT/TestGroup/Eta", 1) ) EXPECTED ( 1 );
+  VALUE ( contentInBin1DHist(histSvc, "/EXPERT/TestGroup/Eta", 2) ) EXPECTED ( 0 );
+
+  return true;
+}
+
+
+
+int main() {
+  //CxxUtils::ubsan_suppress ([]() { TInterpreter::Instance(); } );
+  ISvcLocator* pSvcLoc;
+  if (!Athena_test::initGaudi("GenericMon.txt",  pSvcLoc)) {
+    std::cerr << "ERROR This test can not be run" << std::endl;
+    return -1;
+  }
+  MsgStream log(Athena::getMessageSvc(), "GenericMonFilling_test");
+
+  // we need to test what happens to the monitoring when tool is not valid
+  //  ToolHandle<GenericMonitoringTool> m_emptyMon("");
+  //  VALUE ( m_emptyMon.isValid() ) EXPECTED ( true );
+  //  log << MSG::DEBUG << " mon tool validity " << m_emptyMon.isValid() << endmsg;
+
+  ToolHandle<GenericMonitoringTool> m_validMon("GenericMonitoringTool/MonTool");
+  if ( m_validMon.retrieve().isFailure() ) {
+    log << MSG::ERROR << "Failed to acquire the MonTool tools via the ToolHandle" << endmsg;
+    return -1;
+  }
+
+  ITHistSvc* histSvc;
+  if( pSvcLoc->service("THistSvc", histSvc, true).isFailure()  ) {
+    log << MSG::ERROR << "THistSvc not available " << endmsg;
+    return -1;
+  }
+  assert( fillFromScalarWorked(m_validMon, histSvc) );
+  
+  log << MSG::DEBUG << "All OK"  << endmsg;
+  return 0;
+}
