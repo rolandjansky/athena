b23c5e8bae0fd2f694f85d2ebe53ad45ffdac17e
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-05-02 16:43:37 +0000

    Merge branch '21.0-addXAOD-AFPSiCluster' into '21.0'
    Added xAOD::AFPSiHitsCluster and helper classes for station and layer ID.
    
    See merge request !1174
    
    Former-commit-id: dffd72cc62ffa20cacb769084bb358260e4f72c6

---
+++ Event/xAOD/xAODForward/xAODForward/AFPSiHitsClusterAuxContainer.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef XAODFORWARD_AFPSIHITSCLUSTERAUXCONTAINER_H
+#define XAODFORWARD_AFPSIHITSCLUSTERAUXCONTAINER_H
+
+#include"xAODForward/versions/AFPSiHitsClusterAuxContainer_v1.h"
+
+namespace xAOD
+{
+  typedef AFPSiHitsClusterAuxContainer_v1 AFPSiHitsClusterAuxContainer;
+}
+
+#include "xAODCore/CLASS_DEF.h"
+CLASS_DEF( xAOD::AFPSiHitsClusterAuxContainer, 1272181344, 1 )
+
+#endif
