5c1faba022dbbb3c40b8bd91716d26658d70789f
John Chapman <jchapman@cern.ch>
2017-05-02 15:17:46 +0200

    Replace obsolete V1 UserActions with V2 UserAction implementations
    The V1 UserAction implementations are obsolete, so should be replaced
    by V2 implementations wherever they appear. See ATLASSIM-3023.
    
    
    Former-commit-id: ba226b4080c3c725ef825fb44a0427af50a8b36d

--- Simulation/G4Extensions/Quirks/src/DebugSteppingAction.h
+++ Simulation/G4Extensions/Quirks/src/DebugSteppingAction.h
@@ -10,37 +10,57 @@
 #ifdef QUIRKS_STANDALONE
 #include "G4UserSteppingAction.hh"
 #else
-//#include "FadsActions/UserAction.h"
-#include "G4AtlasTools/UserActionBase.h"
+#include "G4AtlasInterfaces/ISteppingAction.h"
+#endif
+
+#ifndef QUIRKS_STANDALONE
+namespace G4UA{
 #endif
 
 class DebugSteppingAction :
 #ifdef QUIRKS_STANDALONE
     public G4UserSteppingAction
 #else
-    public UserActionBase
+    public ISteppingAction
 #endif
 {
 public:
 #ifdef QUIRKS_STANDALONE
     DebugSteppingAction(G4double step, G4int numSteps);
 #else
-    DebugSteppingAction(const std::string& type, const std::string& name, const IInterface* parent);
-    virtual StatusCode queryInterface(const InterfaceID&, void**) override;
+    struct Config
+    {
+      G4double step=0;
+      G4int numSteps=0;
+    };
+    
+    DebugSteppingAction(const Config& config);    
 #endif
 
     virtual ~DebugSteppingAction();
 
-    virtual void Step(const G4Step* step) override;
-    virtual void UserSteppingAction(const G4Step* step) {Step(step);}
+    virtual void processStep(const G4Step* step) override;
+    virtual void UserSteppingAction(const G4Step* step) {processStep(step);}
 
 private:
-    G4int m_iStep[2];
+#ifndef QUIRKS_STANDALONE
+    Config m_config;
+#else
     G4double m_step;
     G4int m_numSteps;
+#endif
+    
+    G4int m_iStep[2];
+ 
     G4LorentzVector m_x[2];
     G4LorentzVector m_p[2];
     G4LorentzVector m_xprev[2];
 };
+
+
+
+#ifndef QUIRKS_STANDALONE
+} // namespace G4UA
+#endif
 
 #endif
