c035eaa45f0fecf6ec2c8e19f828cadb782cca5b
Will Leight <wleight@cern.ch>
2017-05-09 14:18:12 +0200

    Change from a loop over two elements to two assignment statements
    Former-commit-id: 75b7addac08569caa81da50ac15deea3fdcb6c28

--- MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonSegment/src/MuonSegmentCombination.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonSegment/src/MuonSegmentCombination.cxx
@@ -34,7 +34,8 @@ namespace Muon {
 
   void MuonSegmentCombination::copy( const MuonSegmentCombination& segc )
   {
-    m_use2LayerSegs=segc.use2LayerSegments();
+    nGood[0]=segc.getNGoodCscLayers(0);
+    nGood[1]=segc.getNGoodCscLayers(1);
     unsigned int nst( segc.numberOfStations() );
     for( unsigned int i=0;i<nst;++i ){
       SegmentVec::const_iterator vit = segc.stationSegments( i )->begin();
