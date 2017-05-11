ab8f086c2d1dd9bbab188e102c548e27d55e319c
Frank Winklmeier <Frank.Winklmeier@cern.ch>
2017-05-11 19:06:47 +0200

    Use TrigConf IS classes from tdaq/TTCInfo (ADHI-4381)
    Former-commit-id: 480742fb57f781b9b8d56e8324dcd56fdca583ef

--- HLT/Trigger/TrigConfiguration/TrigConf2COOLOnline/src/OnlTrigC2CWriter.cxx
+++ HLT/Trigger/TrigConfiguration/TrigConf2COOLOnline/src/OnlTrigC2CWriter.cxx
@@ -17,10 +17,10 @@
 
 #include "TrigConf2COOLOnline/OnlTrigC2CWriter.h"
 
-#include "TrigConf2COOLOnline/TrigConfSmKeyNamed.h"
-#include "TrigConf2COOLOnline/TrigConfL1PsKeyNamed.h"
-#include "TrigConf2COOLOnline/TrigConfHltPsKeyNamed.h"
-#include "TrigConf2COOLOnline/TrigConfReleaseNamed.h"
+#include "TTCInfo/TrigConfSmKeyNamed.h"
+#include "TTCInfo/TrigConfL1PsKeyNamed.h"
+#include "TTCInfo/TrigConfHltPsKeyNamed.h"
+#include "TTCInfo/TrigConfReleaseNamed.h"
 
 #include "config/Configuration.h"
 
