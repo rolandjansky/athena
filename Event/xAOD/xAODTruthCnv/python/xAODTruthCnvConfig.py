0217e80d4fac0cc122abfde8ed6e18a9230bd58d
John Chapman <jchapman@cern.ch>
2017-05-12 14:56:20 +0200

    Updating the name of the digiSteeringConf to compare to.
    Former-commit-id: 477618e596f5a14e960a09535b5bb91f5787e3c6

--- Event/xAOD/xAODTruthCnv/python/xAODTruthCnvConfig.py
+++ Event/xAOD/xAODTruthCnv/python/xAODTruthCnvConfig.py
@@ -4,7 +4,7 @@ def getGEN_AOD2xAOD(name="GEN_AOD2xAOD", **kwargs):
     writeInTimePileUpTruth=False
     try:
         from RecExConfig.InputFilePeeker import inputFileSummary
-        if inputFileSummary['metadata']['/Digitization/Parameters']['digiSteeringConf'] == 'StandardInTimeOnlyGeantinoTruth':
+        if inputFileSummary['metadata']['/Digitization/Parameters']['digiSteeringConf'] == 'StandardInTimeOnlyGeantinoTruthPileUpToolsAlg':
             writeInTimePileUpTruth=True
     except:
         pass
