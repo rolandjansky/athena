fd0908ad70b8116c90de7ec43c89412f0721d930
Pierre-Antoine Delsart <delsart@in2p3.fr>
2017-05-15 17:19:57 +0200

    Replace msg( XXX) with ATH_MSG_XXX
    Former-commit-id: 8fd883434794d56d019c850105eb7c1c1c3ba1a3

--- Calorimeter/CaloRec/src/CaloTowerxAODAlgoBase.cxx
+++ Calorimeter/CaloRec/src/CaloTowerxAODAlgoBase.cxx
@@ -37,7 +37,7 @@ StatusCode CaloTowerxAODAlgoBase::initBase() {
   const IGeoModelSvc *geoModel=0;
   StatusCode sc = service("GeoModelSvc", geoModel);
   if(sc.isFailure()) {
-    msg(MSG::FATAL) << "Could not locate GeoModelSvc" << endreq;
+    ATH_MSG_FATAL("Could not locate GeoModelSvc" );
     return sc;
   }
 
@@ -106,7 +106,7 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
 
   //Some basic sanity checks:
   if (m_minEta>=m_maxEta) {
-    msg(MSG::ERROR) << "Configuration problem: minEta>=maxEta " << m_minEta << "," << m_maxEta << endreq;
+    ATH_MSG_ERROR( "Configuration problem: minEta>=maxEta " << m_minEta << "," << m_maxEta );
     return StatusCode::FAILURE;
   }
   
@@ -115,7 +115,7 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
   ATH_CHECK(detStore()->retrieve(theManager));
 
   if ( ! theManager->isInitialized() ){
-    msg(MSG::ERROR) << "CaloDetDescrManager is not yet initialized!" << endreq;
+    ATH_MSG_ERROR( "CaloDetDescrManager is not yet initialized!" );
     return StatusCode::FAILURE;
   }
 
@@ -127,7 +127,7 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
   const int nTowers=dummy->nTowers();
   
   if (nTowers<1) {
-    msg(MSG::ERROR) << "Something went wrong with tower grid config: Got container with " << nTowers << " Towers." << endreq;
+    ATH_MSG_ERROR( "Something went wrong with tower grid config: Got container with " << nTowers << " Towers." );
     return StatusCode::FAILURE;
   }
 
@@ -143,9 +143,9 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
 
   for (CaloDetDescrManager::calo_element_const_iterator ddeIt=theManager->element_begin();
        ddeIt!=theManager->element_end();++ddeIt) {
-
+    
     const CaloDetDescrElement* dde=*ddeIt;
-
+    
     if (dde->is_lar_fcal()) {
       //special handling of FCAL cells. Inspired by LArRecUtils/src/LArFCalTowerStore.cxx 
       double xCell  = dde->x();
@@ -162,29 +162,29 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
 
       // loop the cell fragments
       for (int iX=0;iX<ndxFCal[thisModule];++iX) {
-	double x=xCell-dxCell/2.0 + iX*theXBin;
-	for (int iY=0;iY<ndyFCal[thisModule];++iY) {
-	  double y=yCell-dyCell/2.0 + iY*theYBin;
-
-	  // eta,phi of fragment
-	  double r   = sqrt( x * x + y * y + zCell * zCell );
-	  double eta = -0.5 * log((r-zCell)/(r+zCell));
-	  double phi =  CaloPhiRange::fix(atan2(y,x));
-	  //int towerIdx=xAOD::CaloTowerGrid::index(dummy.get(),eta,phi);
-	  int towerIdx=dummy->index(eta,phi);
-	  if (towerIdx>=nTowers || towerIdx<0) {
-	    ATH_MSG_ERROR("Found invalid tower index for FCAL cell eta/phi " << eta << "/" << phi << ", x/y=" << x << "/" << y);
-	    return StatusCode::FAILURE;
-	  }
-	  //m_cellToTower[dde->calo_hash()].push_back(cellToTower_t(towerIdx,theWeight));
-	  m_cellToTower[dde->calo_hash()].emplace_back(towerIdx,theWeight);
-	  ATH_MSG_VERBOSE("cell hash " << dde->calo_hash() << ", goes into tower " << towerIdx << " with weight " << theWeight);
-	} //end y loop
+        double x=xCell-dxCell/2.0 + iX*theXBin;
+        for (int iY=0;iY<ndyFCal[thisModule];++iY) {
+          double y=yCell-dyCell/2.0 + iY*theYBin;
+          
+          // eta,phi of fragment
+          double r   = sqrt( x * x + y * y + zCell * zCell );
+          double eta = -0.5 * log((r-zCell)/(r+zCell));
+          double phi =  CaloPhiRange::fix(atan2(y,x));
+          //int towerIdx=xAOD::CaloTowerGrid::index(dummy.get(),eta,phi);
+          int towerIdx=dummy->index(eta,phi);
+          if (towerIdx>=nTowers || towerIdx<0) {
+            ATH_MSG_ERROR("Found invalid tower index for FCAL cell eta/phi " << eta << "/" << phi << ", x/y=" << x << "/" << y);
+            return StatusCode::FAILURE;
+          }
+          //m_cellToTower[dde->calo_hash()].push_back(cellToTower_t(towerIdx,theWeight));
+          m_cellToTower[dde->calo_hash()].emplace_back(towerIdx,theWeight);
+          ATH_MSG_VERBOSE("cell hash " << dde->calo_hash() << ", goes into tower " << towerIdx << " with weight " << theWeight);
+        } //end y loop
       } //end x loop
     }//end FCAL
 
     else { //Non-FCAL subcalos:
-
+      
       const double cellDeta = dde->deta();
       const double cellDphi = dde->dphi();
       const double etaRaw   = dde->eta_raw();
@@ -197,9 +197,8 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
       kp = (kp==0) ? 1 : kp;
       // signal weight
       if ( msgLvl(MSG::VERBOSE) && (ke>1 || kp>1)) {
-	msg(MSG::VERBOSE) << "Found cell [0x" << std::hex << dde->identify().get_compact() << std::dec 
-			  << "] spanning several towers. nEta=" << ke << "nPhi="<<kp
-			  << endreq;
+        ATH_MSG_VERBOSE( "Found cell [0x" << std::hex << dde->identify().get_compact() << std::dec 
+                         << "] spanning several towers. nEta=" << ke << "nPhi="<<kp );                         
       }
 
 
@@ -213,17 +212,17 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
       //Loop over cell fragments:
 
       for ( size_t ie=1; ie<=ke; ie++ ){                            // outer (eta) fragment loop
-	double cellEta  = etaMin + ((double)ie - 0.5) * cellDdeta;        // eta of fragment
-	for ( size_t ip=1; ip<=kp; ip++ ){                          // inner (phi) fragement loop
-	  double cellPhi = phiMin + ((double)ip - 0.5) * cellDdphi;       // phi of fragment
-	  int towerIdx=dummy->index(cellEta,cellPhi);
-	  if (towerIdx>=nTowers || towerIdx<0) {
-	    ATH_MSG_ERROR("Found invalid tower index " << towerIdx << " for cell eta/phi " << cellEta << "/" << cellPhi << " coming from " <<  dde->calo_hash() << "/" << ie << "/" << ip);
-	    return StatusCode::FAILURE;
-	  }
-	  m_cellToTower[dde->calo_hash()].push_back(cellToTower_t(towerIdx,theWeight));
-	  ATH_MSG_VERBOSE("cell hash " << dde->calo_hash() << ", goes into tower " << towerIdx << "with weight" << theWeight);
-	}//end loop over fragments (phi)
+        double cellEta  = etaMin + ((double)ie - 0.5) * cellDdeta;        // eta of fragment
+        for ( size_t ip=1; ip<=kp; ip++ ){                          // inner (phi) fragement loop
+          double cellPhi = phiMin + ((double)ip - 0.5) * cellDdphi;       // phi of fragment
+          int towerIdx=dummy->index(cellEta,cellPhi);
+          if (towerIdx>=nTowers || towerIdx<0) {
+            ATH_MSG_ERROR("Found invalid tower index " << towerIdx << " for cell eta/phi " << cellEta << "/" << cellPhi << " coming from " <<  dde->calo_hash() << "/" << ie << "/" << ip);
+            return StatusCode::FAILURE;
+          }
+          m_cellToTower[dde->calo_hash()].push_back(cellToTower_t(towerIdx,theWeight));
+          ATH_MSG_VERBOSE("cell hash " << dde->calo_hash() << ", goes into tower " << towerIdx << "with weight" << theWeight);
+        }//end loop over fragments (phi)
       }// end loop over fragments (eta)
     }//end else FCAL
   }//end loop over detDescrElements
@@ -248,9 +247,9 @@ StatusCode CaloTowerxAODAlgoBase::fillIndexCache() {
         sumWeight+=ct.m_weight;
       if (fabs(sumWeight-1)>0.001) {
         const Identifier id=caloCellId->cell_id(i);
-        msg(MSG::ERROR) << "Cell with index " << i << ", id=0x" 
-                        << std::hex << id.get_identifier32().get_compact() << std::dec 
-                        << ": Weights don't add up to 1.0, got " << sumWeight << endreq;
+        ATH_MSG_ERROR( "Cell with index " << i << ", id=0x" 
+                       << std::hex << id.get_identifier32().get_compact() << std::dec 
+                       << ": Weights don't add up to 1.0, got " << sumWeight );
       }
     }//end loop over cells
   }//end if doxCheck
