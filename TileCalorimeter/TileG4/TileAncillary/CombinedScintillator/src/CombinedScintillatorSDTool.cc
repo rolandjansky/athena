89544767d0e60ab9a12e3d39c21f37218c2957e6
Elmar Ritsch <elmar.ritsch@cern.ch>
2017-05-15 15:05:40 +0200

    Pull TileG4 syntax tidying from SVN into this repository
    Pulled SVN tags:
     * CombinedScintillator-00-04-00
     * MinBiasScintillator-00-03-05
     * MuonWall-00-04-00
     * PhantomCalorimeter-00-04-00
    
    Removed obsolete ATHENAHIVE preprocessor statements in addition to the updates from the above SVN tags.
    
    
    Former-commit-id: 4e91254f17b6f352aea998e1742c9b67f1b4b208

--- TileCalorimeter/TileG4/TileAncillary/CombinedScintillator/src/CombinedScintillatorSDTool.cc
+++ TileCalorimeter/TileG4/TileAncillary/CombinedScintillator/src/CombinedScintillatorSDTool.cc
@@ -12,70 +12,59 @@
 #include "CombinedScintillatorSDTool.hh"
 #include "CombinedScintillatorSD.hh"
 
-CombinedScintillatorSDTool::CombinedScintillatorSDTool(const std::string& type, const std::string& name, const IInterface* parent)
-  : SensitiveDetectorBase(type,name,parent)
+CombinedScintillatorSDTool::CombinedScintillatorSDTool(const std::string& type, const std::string& name,
+                                                       const IInterface* parent)
+    : SensitiveDetectorBase(type, name, parent)
 {
   declareInterface<ISensitiveDetector>(this);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-CombinedScintillatorSDTool::~CombinedScintillatorSDTool()
-{
+CombinedScintillatorSDTool::~CombinedScintillatorSDTool() {
 
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-StatusCode CombinedScintillatorSDTool::SetupEvent()
-{
-  ATH_MSG_VERBOSE( "CombinedScintillatorSDTool::SetupEvent()" );
-  if(!getSD())
-    {
-      ATH_MSG_ERROR ("SetupEvent: CombinedScintillatorSD never created!");
+StatusCode CombinedScintillatorSDTool::SetupEvent() {
+  ATH_MSG_VERBOSE("CombinedScintillatorSDTool::SetupEvent()");
+  if (!getSD()) {
+    ATH_MSG_ERROR("SetupEvent: CombinedScintillatorSD never created!");
+    return StatusCode::FAILURE;
+  } else {
+    CombinedScintillatorSD *localSD = dynamic_cast<CombinedScintillatorSD*>(getSD());
+    if (!localSD) {
+      ATH_MSG_ERROR("SetupEvent: Failed to cast m_SD into CombinedScintillatorSD.");
       return StatusCode::FAILURE;
     }
-  else
-    {
-      CombinedScintillatorSD *localSD = dynamic_cast<CombinedScintillatorSD*>(getSD());
-      if(!localSD)
-        {
-          ATH_MSG_ERROR ("SetupEvent: Failed to cast m_SD into CombinedScintillatorSD.");
-          return StatusCode::FAILURE;
-        }
-      localSD->StartOfAthenaEvent();
-    }
+    localSD->StartOfAthenaEvent();
+  }
   return StatusCode::SUCCESS;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-StatusCode CombinedScintillatorSDTool::Gather()
-{
-  ATH_MSG_VERBOSE( "CombinedScintillatorSDTool::Gather()" );
-  if(!getSD())
-    {
-      ATH_MSG_ERROR ("Gather: CombinedScintillatorSD never created!");
+StatusCode CombinedScintillatorSDTool::Gather() {
+  ATH_MSG_VERBOSE("CombinedScintillatorSDTool::Gather()");
+  if (!getSD()) {
+    ATH_MSG_ERROR("Gather: CombinedScintillatorSD never created!");
+    return StatusCode::FAILURE;
+  } else {
+    CombinedScintillatorSD *localSD = dynamic_cast<CombinedScintillatorSD*>(getSD());
+    if (!localSD) {
+      ATH_MSG_ERROR("Gather: Failed to cast m_SD into CombinedScintillatorSD.");
       return StatusCode::FAILURE;
     }
-  else
-    {
-      CombinedScintillatorSD *localSD = dynamic_cast<CombinedScintillatorSD*>(getSD());
-      if(!localSD)
-        {
-          ATH_MSG_ERROR ("Gather: Failed to cast m_SD into CombinedScintillatorSD.");
-          return StatusCode::FAILURE;
-        }
-      localSD->EndOfAthenaEvent();
-    }
+    localSD->EndOfAthenaEvent();
+  }
   return StatusCode::SUCCESS;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4VSensitiveDetector* CombinedScintillatorSDTool::makeSD()
-{
-  ATH_MSG_DEBUG( "Initializing SD" );
+G4VSensitiveDetector* CombinedScintillatorSDTool::makeSD() {
+  ATH_MSG_DEBUG("Initializing SD");
   // Create a fresh SD
   return new CombinedScintillatorSD(name(), m_outputCollectionNames[0]);
 }
