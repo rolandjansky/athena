e68370ca38a2b7be8ecc5378afe6dba8ec4d33e9
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-05-27 18:13:16 +0000

    Merge branch 'add_RDOAnalysis_package_21.0' into '21.0'
    Add RDOAnalysis package to 21.0
    
    See merge request !1680
    
    Former-commit-id: 3fde97146a359a3375f28cc09a6d43a484a86663

---
+++ Simulation/Tools/RDOAnalysis/share/TRT_FastRDOAnalysis_topOptions.py
@@ -0,0 +1,58 @@
+from AthenaCommon.Constants import *
+from AthenaCommon.AppMgr import theApp
+from AthenaCommon.AppMgr import ServiceMgr
+import AthenaPoolCnvSvc.ReadAthenaPool
+
+ServiceMgr.AuditorSvc.Auditors += ["ChronoAuditor"]
+
+AthenaPoolCnvSvc = Service("AthenaPoolCnvSvc")
+AthenaPoolCnvSvc.UseDetailChronoStat = TRUE
+
+from PartPropSvc.PartPropSvcConf import PartPropSvc
+
+include("ParticleBuilderOptions/McAOD_PoolCnv_jobOptions.py")
+include("EventAthenaPool/EventAthenaPool_joboptions.py")
+
+#----------------------------
+# Input Dataset
+#----------------------------
+import os
+from glob import glob
+from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
+athenaCommonFlags.FilesInput = glob("/tmp/" + os.environ['USER'] + "/" + "RDO*fastdigi*.root*")
+ServiceMgr.EventSelector.InputCollections = athenaCommonFlags.FilesInput()
+
+theApp.EvtMax = -1
+#theApp.EvtMax = 100
+
+#----------------------------
+# Message Service
+#----------------------------
+# set output level threshold (2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL)
+ServiceMgr.MessageSvc.OutputLevel = INFO
+ServiceMgr.MessageSvc.defaultLimit = 9999999
+
+#----------------------------
+# Algorithms
+#----------------------------
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence = AlgSequence()
+from RDOAnalysis.RDOAnalysisConf import TRT_FastRDOAnalysis
+topSequence += TRT_FastRDOAnalysis()
+TRT_FastRDOAnalysis = TRT_FastRDOAnalysis()
+TRT_FastRDOAnalysis.NtupleFileName = '/TRT_FastRDOAnalysis/'
+TRT_FastRDOAnalysis.HistPath = '/TRT_FastRDOAnalysis/'
+
+#----------------------------
+# Histogram and Tree Service
+#----------------------------
+from GaudiSvc.GaudiSvcConf import THistSvc
+ServiceMgr += THistSvc()
+ServiceMgr.THistSvc.Output += ["TRT_FastRDOAnalysis DATAFILE='TRT_FastRDOAnalysis.root' OPT='RECREATE'"]
+
+#----------------------------
+# Detector geometry
+#----------------------------
+from RecExConfig.AutoConfiguration import *
+ConfigureFieldAndGeo()
+include("RecExCond/AllDet_detDescr.py")
