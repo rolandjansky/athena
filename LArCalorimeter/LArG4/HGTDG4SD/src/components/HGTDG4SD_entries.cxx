370b027f84d21c9dd7a81069f2b9bc2083bd458d
John Chapman <jchapman@cern.ch>
2017-05-29 16:13:01 +0200

    Import HGTDG4SD package
    The new HGTDG4SD package for phase-II High Granularity Timing Detector.
    Based on HGTDG4SD-00-00-05. See ATLASSIM-2788
    
    
    Former-commit-id: c617bf523db14a9296aa69c63aac83141b9b5ee0

---
+++ LArCalorimeter/LArG4/HGTDG4SD/src/components/HGTDG4SD_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+#include "../HGTDSensorSDTool.h"
+
+DECLARE_TOOL_FACTORY( HGTDSensorSDTool )
+
+//DECLARE_FACTORY_ENTRIES( HGTDG4SD ) {
+//  DECLARE_TOOL( HGTDSensorSDTool )
+//}
