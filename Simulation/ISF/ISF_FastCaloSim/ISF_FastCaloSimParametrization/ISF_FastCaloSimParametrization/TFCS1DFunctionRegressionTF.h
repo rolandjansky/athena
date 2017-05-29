625a1ec7f084cf2800ec43e83ced31fa966d9a3e
John Chapman <jchapman@cern.ch>
2017-05-29 13:38:42 +0200

    Make the ClassDef of the TFCS1DFunctionRegressionTF class public
    Adding this change manually to master after the failed sweep of
    merge request !1379 from the 21.0 branch.
    
    
    Former-commit-id: 22ecc3ecbcc4721e2d24fe999dabe7f74cad3325

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/ISF_FastCaloSimParametrization/TFCS1DFunctionRegressionTF.h
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/ISF_FastCaloSimParametrization/TFCS1DFunctionRegressionTF.h
@@ -12,22 +12,23 @@
 class TFCS1DFunctionRegressionTF:public TFCS1DFunctionRegression
 {
   public:
-  	
+
     TFCS1DFunctionRegressionTF();
     void storeRegression(string, float, float);
 
     virtual double rnd_to_fct(double rnd);
     double retransform(double value);
-    
+
   private:
-    
+
     vector<vector<double> > m_fWeightMatrix0to1;
     vector<vector<double> > m_fWeightMatrix1to2;
     float m_rangeval;
     float m_startval;
-		
-  ClassDef(TFCS1DFunctionRegressionTF,1)
-  
+
+  public:
+    ClassDef(TFCS1DFunctionRegressionTF,1)
+
 };
 
 #if defined(__MAKECINT__)
