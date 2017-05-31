84d879c37f31d9031bbcd06ea890f19057d34145
Nils Krumnack <krumnack@iastate.edu>
2017-05-30 19:54:01 -0500

    svnpull HFORTools-04-00-14
    For consistency with AnalysisBase 2.6.
    
    
    Former-commit-id: 11d96f334e35b9a7d8898f8aabe267a80417456f

---
+++ PhysicsAnalysis/AnalysisCommon/PMGOverlapRemovalTools/HFORTools/HFORTools/HFORToolsDict.h
@@ -0,0 +1,11 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#ifndef HFORTOOLS_HFORTOOLSDICT_H
+#define HFORTOOLS_HFORTOOLSDICT_H
+
+#include "HFORTools/HFORSelectionTool.h"
+
+#endif
