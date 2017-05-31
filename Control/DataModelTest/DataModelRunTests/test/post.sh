c78b89afb4fc45ec5e587fe850826a0c60709691
scott snyder <snyder@bnl.gov>
2017-05-31 14:53:58 +0200

    DataModelRunTests: Fix ATN test.
    Formatting of ClassIDSvc message.
    
    
    
    Former-commit-id: 6b3350e8c3e97701f40f7aec125cae238d1887c4

--- Control/DataModelTest/DataModelRunTests/test/post.sh
+++ Control/DataModelTest/DataModelRunTests/test/post.sh
@@ -20,7 +20,7 @@ PP="$PP"'|\w+-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}'
 PP="$PP"'|^==> New TileCablingService created$'
 
 # CLIDsvc output depends on module cache, existing files?
-PP="$PP"'|^ClassIDSvc *INFO'
+PP="$PP"'|^ClassIDSvc[ 0]*INFO'
 
 # File catalog stuff.
 PP="$PP"'|^Domain.ROOT_All.:'
