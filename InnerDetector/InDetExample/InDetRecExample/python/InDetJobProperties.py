be2d07f592cce9e23c6327676aa47b526dd161b7
Simone Pagan Griso <simone.pagan.griso@cern.ch>
2017-06-01 22:22:25 +0200

    Reverted back to False by default for VtxLumi
    Former-commit-id: 3d8e94fe046cb1128c318232633540008c322d26

--- InnerDetector/InDetExample/InDetRecExample/python/InDetJobProperties.py
+++ InnerDetector/InDetExample/InDetRecExample/python/InDetJobProperties.py
@@ -1420,7 +1420,7 @@ class InDetJobProperties(JobPropertyContainer):
        self.checkThenSet(self.doBremRecovery         , False)
        self.checkThenSet(self.doCaloSeededBrem       , False)
        self.checkThenSet(self.doHadCaloSeededSSS     , False)
-       self.checkThenSet(self.checkDeadElementsOnTrack,True)
+       self.checkThenSet(self.checkDeadElementsOnTrack,False)
        # --- turn off TRT
        DetFlags.makeRIO.TRT_setOff()
        DetFlags.TRT_setOff()
