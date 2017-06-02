07cfa5f11d2e03ec15b020b9db7fe6ac6844881d
Will Buttinger <william.buttinger@cern.ch>
2017-06-02 14:38:12 +0000

    put back in copyright
    Former-commit-id: ebf3f113c3833d4316e1ede24934a33cd78c336a

--- Tools/PyUtils/python/scripts/cmt_newjobo.py
+++ Tools/PyUtils/python/scripts/cmt_newjobo.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
 # @file PyUtils.scripts.cmt_newalg
 # @purpose streamline and ease the creation of a skeleton joboption
 # @author Will Buttinger
