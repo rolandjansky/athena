cd019014f6889d8a1f1e984677c88c6f643fc4c5
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-06-07 10:11:09 +0000

    Merge branch '21.0-art-june6' into '21.0'
    Add new local tests and modify the grid tests
    
    See merge request !2510
    
    Former-commit-id: f32d6c06d0e2bca96fb03ef5fed2f44a7ed8ee93

--- Reconstruction/RecExample/RecJobTransformTests/test/test_mc_heavy_ion.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_mc_heavy_ion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-#art-description: heavy ion reconstruction test from Andrzej Olszewski and Iwona Grabowska-Bold
-#art-queue: long
-#
+# art-description: heavy ion reconstruction test from Andrzej Olszewski and Iwona Grabowska-Bold
+# art-type: grid
+
 export TRF_ECHO=True; Reco_tf.py --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/HITS.04974747._000006.pool.root.1 --outputRDOFile=RDO.pool.root --outputESDFile=ESD.pool.root --outputAODFile=AOD.pool.root --autoConfiguration=everything --conditionsTag=OFLCOND-RUN12-SDR-14 --geometryVersion=ATLAS-R2-2015-01-01-00 --DataRunNumber=222222 --maxEvents=25 --preExec 'rec.Commissioning.set_Value_and_Lock(True)' --preInclude 'RAWtoESD:RecJobTransforms/HeavyIonRecConfig.py' --triggerConfig MCRECO:Physics_HI_v3
 
--- Tools/Tier0ChainTests/test/test_q223_mp.sh
+++ Tools/Tier0ChainTests/test/test_q223_mp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-#art-description: RecoTrf
-#art-queue: long
-#
+# art-description: RecoTrf
+# art-type: grid
+
 Reco_tf.py --AMI=q223 --athenaopts='--nprocs=2' --maxEvents=100 --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data15_comm.00264034.physics_MinBias.daq.RAW._lb0644._SFO-6._0001.data
 
