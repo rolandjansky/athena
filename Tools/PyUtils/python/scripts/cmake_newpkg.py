d9685f5ca38a8a53ce8edbbb9b83dacefc8781e8
Will Buttinger <william.buttinger@cern.ch>
2017-06-07 08:59:08 +0000

    Added copyright
    Former-commit-id: 9393cc9bbebc9cda7e5ae0c38cce948191a35e1f

--- Tools/PyUtils/python/scripts/cmake_newpkg.py
+++ Tools/PyUtils/python/scripts/cmake_newpkg.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
 # @file PyUtils.scripts.cmake_newpkg
 # @purpose streamline and ease the creation of new cmake packages
 # @author Will Buttinger
