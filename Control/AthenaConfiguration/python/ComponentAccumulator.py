40f00a08b3aa9e2863b27b424049d9322847fc9c
Walter Lampl <Walter.Lampl@cern.ch>
2017-06-07 17:04:36 +0200

    Extend ComponentAccumulator prototye to handle service, cond-algos and (nested) sets of AlgTools
    Former-commit-id: e9904d3b514575d79a8e6b864acd5c56f011dbda

--- Control/AthenaConfiguration/python/ComponentAccumulator.py
+++ Control/AthenaConfiguration/python/ComponentAccumulator.py
@@ -1,5 +1,7 @@
 from AthenaCommon.Logging import logging
 from AthenaCommon.Configurable import ConfigurableService,ConfigurableAlgorithm
+import GaudiKernel.GaudiHandles as GaudiHandles
+import ast
 
 class ComponentAccumulator(object): 
     
@@ -84,6 +86,42 @@ class ComponentAccumulator(object):
                 
     def __iadd__(self,other):
         merge(other)
+
+
+    def appendTool(self,tool,jocat):
+        toolname=tool.getJobOptName() #FIXME: Don't overwrite duplicates! 
+        for k, v in tool.getValuedProperties().items():
+            if isinstance(v,GaudiHandles.GaudiHandle):
+                jocat[toolname][k]=v.getJobOptName()
+                self.appendTool(v,jocat)
+            elif isinstance(v,GaudiHandles.GaudiHandleArray):
+                jocat[toolname][k]=[]
+                for v1 in v:
+                    jocat[algname][k].append(v1.getJobOptName())
+                    self.appendTool(v1,jocat)
+            else:
+                jocat[toolname][k]=str(v)
+
+
+    def appendConfigurable(self,confElem):
+        name=confElem.getJobOptName() #FIXME: Don't overwrite duplicates! 
+        for k, v in confElem.getValuedProperties().items():
+            if isinstance(v,GaudiHandles.GaudiHandle):
+                self._jocat[name][k]=v.getJobOptName()
+                self.appendConfigurable(v)
+            elif isinstance(v,GaudiHandles.GaudiHandleArray):
+                #self._jocat[name][k]=[]#[ v1.getJobOptName() for v1 in v ]
+                children=[]
+                for v1 in v:
+                    children.append(v1.getFullName())
+                    print "Appending ",v1.getJobOptName(),"to", name, k
+                    self.appendConfigurable(v1)
+                self._jocat[name][k]=str(children)
+            else:
+                self._jocat[name][k]=str(v)
+            
+        
+        return
         
 
     def store(self,outfile):
@@ -93,26 +131,51 @@ class ComponentAccumulator(object):
         #first load basics from the bootstrap-pickle
         bsfilename=find_datafile("bootstrap.pkl")
         bsfile=open(bsfilename)
-        jocat=pickle.load(bsfile)
-        jocfg=pickle.load(bsfile)
-        pycomps=pickle.load(bsfile)
+        self._jocat=pickle.load(bsfile)
+        self._jocfg=pickle.load(bsfile)
+        self._pycomps=pickle.load(bsfile)
         bsfile.close()
 
 
-        #jocat=defaultdict(dict)
-        #jocfg=defaultdict(dict)
-        #pycomps=defaultdict(dict)
-        algseq=[]
+        #EventAlgorithms
+        evtalgseq=[]
         for alg in  self._eventAlgs:
-            algname=alg.getJobOptName()
-            #jocat["AthAlgSeq"]["Members"]="["+alg.getFullName()+"]"
-            #algseqstr+="'"+alg.getFullName()+"'"
-            algseq.append(alg.getFullName())
-            for k, v in alg.getValuedProperties().items():
-                jocat[algname][k]=str(v)
+            self.appendConfigurable(alg)
+            evtalgseq.append(alg.getFullName())
+        #print "EventAlgs:",evtalgseq
+        self._jocat["AthAlgSeq"]["Members"]=str(evtalgseq)
 
-        jocat["AthAlgSeq"]["Members"]=str(algseq)
 
-        pickle.dump( jocat, outfile ) 
-        pickle.dump( jocfg, outfile ) 
-        pickle.dump( pycomps, outfile )     
+        #Conditions Algorithms:
+        condalgseq=[]
+        for alg in self._conditionsAlgs:
+            self.appendConfigurable(alg)
+            condalgseq.append(alg.getFullName())
+        self._jocat["AthCondSeq"]["Members"]=str(condalgseq)
+
+        #Service:
+        svcList=ast.literal_eval(self._jocfg["ApplicationMgr"]["ExtSvc"])
+        for svc in self._services:
+            svcname=svc.getJobOptName()
+            svcList.append(svc.getFullname())
+            for k, v in svc.getValuedProperties().items():
+                self._jocat[svcname][k]=str(v)
+        self._jocfg["ApplicationMgr"]["ExtSvc"]=str(svcList)
+        pickle.dump( self._jocat, outfile ) 
+        pickle.dump( self._jocfg, outfile ) 
+        pickle.dump( self._pycomps, outfile )     
+
+
+    def run(self,nEvents):
+        from AthenaCommon.AppMgr import theApp
+        #theApp.setup()
+        
+        print theApp
+        topSeq=theApp.TopAlg
+
+        
+        for alg in self._eventAlgs:
+            theApp.TopAlg+=alg
+
+        theApp.run()
+            
