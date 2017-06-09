443afc624f6adcc0c3f1c90a42eb67c08993d4dc
lawrenceleejr <lawrenceleejr@gmail.com>
2017-06-09 17:08:48 +0200

    VrtSecInclusive: Renaming python file for clarity
    Former-commit-id: 57064c143dc44241d4c232635e85154727e92963

--- Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
+++
@@ -1,8 +0,0 @@
-StreamESD.ItemList+=["xAOD::TrackParticleContainer#VrtSecInclusive*"]
-StreamESD.ItemList+=["xAOD::VertexContainer#VrtSecInclusive*"]
-StreamESD.ItemList+=["xAOD::VertexAuxContainer#VrtSecInclusive*"]
-
-#JiveXML stuff
-# thexAODVertexRetriever.SecondaryVertexCollection="VrtSecInclusive_SecondaryVertices" #for rel21
-# thexAODVertexRetriever.TracksName="VrtSecInclusive_SelectedTrackParticles"
-
