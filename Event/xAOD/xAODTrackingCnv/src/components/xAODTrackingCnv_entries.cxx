bc62f9b50b93fb7bba527a96242a85a71977ca02
wbuttinger <mypost@gmail.com>
2017-06-12 09:40:20 +0100

    prepare this package for use in AthAnalysis (needed by the xAODTrackingAthenaPool package) - it needs to compile only the compressor tool when used in this environment
    Former-commit-id: ff25af57dccf8052f3949eb04a741f9161892fd7

--- Event/xAOD/xAODTrackingCnv/src/components/xAODTrackingCnv_entries.cxx
+++ Event/xAOD/xAODTrackingCnv/src/components/xAODTrackingCnv_entries.cxx
@@ -4,22 +4,31 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Local include(s):
+#ifndef XAOD_ANALYSIS
+
 #include "../TrackCollectionCnvTool.h"
 #include "../RecTrackParticleContainerCnvTool.h"
 #include "../TrackParticleCnvAlg.h"
 #include "../VertexCnvAlg.h"
-#include "../TrackParticleCompressorTool.h"
 
 DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, RecTrackParticleContainerCnvTool )
 DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TrackCollectionCnvTool )
-DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TrackParticleCompressorTool )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, TrackParticleCnvAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, VertexCnvAlg )
 
+#endif //XAOD_ANALYSIS
+
+#include "../TrackParticleCompressorTool.h"
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TrackParticleCompressorTool )
+
+
 DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
+   DECLARE_NAMESPACE_TOOL( xAODMaker, TrackParticleCompressorTool )
+#ifndef XAOD_ANALYSIS
    DECLARE_NAMESPACE_TOOL( xAODMaker, RecTrackParticleContainerCnvTool )
    DECLARE_NAMESPACE_TOOL( xAODMaker, TrackCollectionCnvTool )
-   DECLARE_NAMESPACE_TOOL( xAODMaker, TrackParticleCompressorTool )
    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, TrackParticleCnvAlg )
    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, VertexCnvAlg )
+#endif
+
 }
