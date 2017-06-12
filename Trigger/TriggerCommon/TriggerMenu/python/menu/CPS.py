dacde167ca8465d82eb47380808ccdfd9b29ff4f
Heather Russell <heather.russell@cern.ch>
2017-06-12 12:57:44 +0200

    ATR-16308, adding muX_bJpsi_TrkPEB/Trkloose chains to CPS
    Former-commit-id: 76314e909cf23b94c631ea49ac9a1bb143503c60

--- Trigger/TriggerCommon/TriggerMenu/python/menu/CPS.py
+++ Trigger/TriggerCommon/TriggerMenu/python/menu/CPS.py
@@ -324,6 +324,11 @@ def defineCPSGroups():
                                                    'mu6_j60_gsc110_bperf_split_dr05_dz02',
                                                    ],
 
+                'RATE:CPS:HLT_mu4_bJpsi_Trk' : ['mu4_bJpsi_TrkPEB','mu4_bJpsi_Trkloose'],
+                'RATE:CPS:HLT_mu6_bJpsi_Trk' : ['mu6_bJpsi_TrkPEB','mu6_bJpsi_Trkloose'],
+                'RATE:CPS:HLT_mu10_bJpsi_Trk' : ['mu10_bJpsi_TrkPEB','mu10_bJpsi_Trkloose'],
+                'RATE:CPS:HLT_mu14_bJpsi_Trk' : ['mu14_bJpsi_TrkPEB','mu14_bJpsi_Trkloose'],
+                'RATE:CPS:HLT_mu20_bJpsi_Trk' : ['mu20_bJpsi_TrkPEB','mu20_bJpsi_Trkloose'],
 
 #        'RATE:CPS:HLT_xe70_L1XE50' : ['xe70_L1XE50',
 #                                      'xe75_L1XE50',
