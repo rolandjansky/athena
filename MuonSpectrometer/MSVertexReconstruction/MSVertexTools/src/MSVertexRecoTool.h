b7f7281525cbbad70ce37908630a854488ff671b
abarton <adam.edward.barton@cern.ch>
2017-06-14 16:43:54 +0100

    Make VertexRecoAlg Reentrant, decoration handles
    Other optimizations
    
    
    Former-commit-id: 70c65726759d6459f8259d87f00ba727d35b2c8d

--- MuonSpectrometer/MSVertexReconstruction/MSVertexTools/src/MSVertexRecoTool.h
+++ MuonSpectrometer/MSVertexReconstruction/MSVertexTools/src/MSVertexRecoTool.h
@@ -19,7 +19,7 @@
 #include "MSVertexUtils/Tracklet.h"
 #include "MSVertexUtils/MSVertex.h"
 #include "AthenaKernel/IAtRndmGenSvc.h"
-
+#include "StoreGate/WriteDecorHandle.h"
 #include "MuonPrepRawData/MdtPrepDataContainer.h"
 #include "MuonPrepRawData/RpcPrepDataContainer.h"
 #include "MuonPrepRawData/TgcPrepDataContainer.h"
@@ -34,15 +34,15 @@ namespace Muon {
 
   class MSVertexRecoTool : virtual public IMSVertexRecoTool, public AthAlgTool
   {
-	
+  typedef  xAOD::VertexContainer decortype;
   public :
     /** default constructor */
     MSVertexRecoTool (const std::string& type, const std::string& name, const IInterface* parent);
     /** destructor */
     virtual ~MSVertexRecoTool();
 
-    virtual StatusCode initialize(void);  
-    virtual StatusCode finalize(void);  
+    virtual StatusCode initialize(void) override;  
+    virtual StatusCode finalize(void) override;  
 
     struct TrkCluster {
       float eta;
@@ -80,7 +80,6 @@ namespace Muon {
     unsigned int m_maxGlobalTracklets;
     unsigned int m_maxClusterTracklets;
     float m_MaxTollDist;
-    float m_PI;
     bool m_doSystematics;
 
     CLHEP::HepRandomEngine* m_rndmEngine;
@@ -90,29 +89,33 @@ namespace Muon {
 
   public:
 
-    StatusCode findMSvertices(std::vector<Tracklet>& traklets, std::vector<MSVertex*>& vertices);
+    StatusCode findMSvertices(std::vector<Tracklet>& traklets, std::vector<MSVertex*>& vertices, const EventContext &ctx) const override;
     
   private:
     //barrel vertex reco algorithm
-    void MSVxFinder(std::vector<Tracklet>& tracklets, MSVertex*& vtx);
+    void MSVxFinder(const std::vector<Tracklet>& tracklets, std::unique_ptr<MSVertex>& vtx, const EventContext &ctx) const;
     //endcap vertex reco algorithm
-    void MSStraightLineVx(std::vector<Tracklet> trks, MSVertex*& vtx);
-    void MSStraightLineVx_oldMethod(std::vector<Tracklet> trks, MSVertex*& vtx);
-    void MakeDummyVertex(MSVertex*&);
-    float vxPhiFinder(float theta,float phi);//vertex phi location reco algorithm
-    void HitCounter(MSVertex* MSRecoVx);//counts MDT, RPC & TGC around a reco'd vertex
-    std::vector<TrkCluster> findTrackClusters(std::vector<Tracklet>& tracklets);//group tracklets into clusters -- vertex reco runs on each cluster of tracklets
-    TrkCluster ClusterizeTracks(std::vector<Tracklet>& tracks);//core algorithm for creating the clusters
-    StatusCode FillOutputContainer(std::vector<MSVertex*>&, SG::WriteHandle<xAOD::VertexContainer> &xAODVxContainer);
-    Amg::Vector3D VxMinQuad(std::vector<Tracklet> tracks);//endcap vertex reco core
-    std::vector<Tracklet> RemoveBadTrk(std::vector<Tracklet> tracklets, Amg::Vector3D Vx);//endcap vertex algo track selector
-    bool EndcapHasBadTrack(std::vector<Tracklet> tracklets, Amg::Vector3D Vx);
-    std::vector<Tracklet> getTracklets(std::vector<Tracklet> trks, std::set<int> tracklet_subset);
-    float sq(float x) { return (x)*(x); }
+    void MSStraightLineVx(const std::vector<Tracklet> &trks, std::unique_ptr<MSVertex>& vtx, const EventContext &ctx) const;
+    void MSStraightLineVx_oldMethod(const std::vector<Tracklet> &trks, std::unique_ptr<MSVertex>& vtx, const EventContext &ctx )const;
+    void MakeDummyVertex(MSVertex*&) const;
+    float vxPhiFinder(float theta,float phi, const EventContext &ctx) const;//vertex phi location reco algorithm
+    void HitCounter(MSVertex* MSRecoVx, const EventContext &ctx) const;//counts MDT, RPC & TGC around a reco'd vertex
+    std::vector<TrkCluster> findTrackClusters(std::vector<Tracklet>& tracklets) const;//group tracklets into clusters -- vertex reco runs on each cluster of tracklets
+    TrkCluster ClusterizeTracks(std::vector<Tracklet>& tracks) const;//core algorithm for creating the clusters
+    StatusCode FillOutputContainer(std::vector<MSVertex*>&, SG::WriteHandle<xAOD::VertexContainer> &xAODVxContainer, SG::WriteDecorHandle<decortype, int> &, SG::WriteDecorHandle<decortype, int> &, SG::WriteDecorHandle<decortype, int> &) const;
+    Amg::Vector3D VxMinQuad(const std::vector<Tracklet> &tracks) const;//endcap vertex reco core
+    std::vector<Tracklet> RemoveBadTrk(const std::vector<Tracklet> &tracklets, const Amg::Vector3D &Vx) const;//endcap vertex algo track selector
+    bool EndcapHasBadTrack(const std::vector<Tracklet> &tracklets, const Amg::Vector3D &Vx) const;
+    std::vector<Tracklet> getTracklets(const std::vector<Tracklet> &trks, const std::set<int> &tracklet_subset) const;
+
     SG::WriteHandleKey<xAOD::VertexContainer> m_xAODContainerKey;
     SG::ReadHandleKey<Muon::RpcPrepDataContainer> m_rpcTESKey;//"RPC_Measurements"
     SG::ReadHandleKey<Muon::TgcPrepDataContainer> m_tgcTESKey;//"TGC_Measurements"
     SG::ReadHandleKey<Muon::MdtPrepDataContainer> m_mdtTESKey; //"MDT_DriftCircles" 
+
+    SG::WriteDecorHandleKey<decortype> m_decor_nMDT;
+    SG::WriteDecorHandleKey<decortype> m_decor_nRPC;
+    SG::WriteDecorHandleKey<decortype> m_decor_nTGC;
   };
   
   
