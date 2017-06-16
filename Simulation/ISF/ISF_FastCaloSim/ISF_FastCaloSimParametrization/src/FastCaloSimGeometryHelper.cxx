335d2f5e05d7d30fa11437a3bab267697147fc9a
Vincent Pascuzzi <Vincent.Pascuzzi@cern.ch>
2017-06-17 01:07:37 +0200

    Replace `cout` with `ATH_MSG_DEBUG`
    Saying "good-bye" to the beloved `cout`...
    
    
    Former-commit-id: c46f3cba680713f57b7f5c3ef54e05d1dad1e3fc

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/src/FastCaloSimGeometryHelper.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/src/FastCaloSimGeometryHelper.cxx
@@ -13,7 +13,7 @@ using namespace std;
 /** Constructor **/
 FastCaloSimGeometryHelper::FastCaloSimGeometryHelper(const std::string& t, const std::string& n, const IInterface* p) : AthAlgTool(t,n,p), CaloGeometry(), m_geoModel(0),m_caloMgr(0)
 {
-  std::cout<<"CHECK123 in the FastCaloSimGeometryHelper constructor "<<std::endl;
+  ATH_MSG_DEBUG( "CHECK123 in the FastCaloSimGeometryHelper constructor " );
 
   declareInterface<IFastCaloSimGeometryHelper>(this);
 
@@ -28,10 +28,10 @@ FastCaloSimGeometryHelper::~FastCaloSimGeometryHelper()
 StatusCode FastCaloSimGeometryHelper::initialize()
 {
   ATH_MSG_INFO("Initializing FastCaloSimGeometryHelper");
-  
+
   std::cout<<"CHECK123 in the FastCaloSimGeometryHelper initialize "<<std::endl;
 
-  
+
   if(service("GeoModelSvc", m_geoModel).isFailure()) {
     ATH_MSG_ERROR( "Could not locate GeoModelSvc" );
     return StatusCode::FAILURE;
@@ -40,13 +40,13 @@ StatusCode FastCaloSimGeometryHelper::initialize()
   if(detStore()->retrieve(m_caloMgr, "CaloMgr").isFailure()) {
     ATH_MSG_ERROR("Unable to retrieve CaloDetDescrManager from DetectorStore");
     return StatusCode::FAILURE;
-  }  
+  }
 
   if (m_geoModel->geoInitialized()) {
     // dummy parameters for the callback:
     int dummyInt=0;
     std::list<std::string> dummyList;
-    
+
     if(geoInit(dummyInt,dummyList).isFailure()) {
       ATH_MSG_ERROR( "Call to geoInit failed" );
       return StatusCode::FAILURE;
@@ -56,7 +56,7 @@ StatusCode FastCaloSimGeometryHelper::initialize()
       ATH_MSG_ERROR( "Could not register geoInit callback" );
       return StatusCode::FAILURE;
     }
-  }  
+  }
 
   return StatusCode::SUCCESS;
 }
@@ -66,7 +66,7 @@ StatusCode FastCaloSimGeometryHelper::geoInit(IOVSVC_CALLBACK_ARGS)
   ATH_MSG_INFO("geoInit for " << m_geoModel->atlasVersion() );
 
   LoadGeometryFromCaloDDM();
-  
+
   return StatusCode::SUCCESS;
 }
 
@@ -80,28 +80,27 @@ StatusCode FastCaloSimGeometryHelper::finalize()
 bool FastCaloSimGeometryHelper::LoadGeometryFromCaloDDM()
 {
   ATH_MSG_INFO("Start LoadGeometryFromCaloDDM()");
-  
+
   std::cout<<"CHECK123 in LoadGeometryFromCaloDDM "<<std::endl;
-  
+
   int jentry=0;
   for(CaloDetDescrManager::calo_element_const_iterator calo_iter=m_caloMgr->element_begin();calo_iter<m_caloMgr->element_end();++calo_iter)
   {
     const CaloDetDescrElement* pcell=*calo_iter;
     addcell(pcell);
-    
+
     if(jentry%10000==0)
     {
       ATH_MSG_DEBUG("Load calo cell "<<jentry<<" : "<<pcell->getSampling()<<", "<<pcell->identify());
     }
     ++jentry;
   }
-  
+
   bool ok=PostProcessGeometry();
-  
+
   std::cout<<"CHECK123 in LoadGeometryFromCaloDDM ok="<<ok<<std::endl;
-  
+
   ATH_MSG_INFO("Done LoadGeometryFromCaloDDM()");
 
   return ok;
 }
-
