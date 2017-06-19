41e84b7c30159a3c9485881a92817b3d04c0ed43
scott snyder <snyder@bnl.gov>
2017-06-19 17:47:22 +0200

    DQDefects: Fix ATN test.
    ATN test was broken by the insertion of the copyright header, which
    screwed up the encoding tag.  Fixed.
    
    
    Former-commit-id: 57f9e3b8c58b1ea8c8a97b3c763b65e8c1b08e19

--- DataQuality/DQDefects/python/tests/__init__.py
+++ DataQuality/DQDefects/python/tests/__init__.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-# -*- coding: utf-8 -*-
 from os import unlink, environ
 from os.path import exists
 from nose import with_setup
