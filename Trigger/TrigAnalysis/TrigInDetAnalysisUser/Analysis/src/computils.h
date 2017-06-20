2d4cf2044de41d81c713e866f4369a874873c9a1
sutt <sutt@cern.ch>
2017-06-20 14:32:25 +0200

    Improvements to the HanConfigGenerator and TrigInDetAnalysisUtilities
    Changes to extract the lumiblock range in the HanConfigGenerator
    - also brings 21.1-dev up to date with the HanConfigGenerator changes
    already in 21.0
    
    Additions to improve the plotting and automatic axis ranging in the
    TrigInDetAnalysisUtilities
    
    
    Former-commit-id: 14f4dff3d51fb03294cea0a55f625bfb98330c3e

--- Trigger/TrigAnalysis/TrigInDetAnalysisUser/Analysis/src/computils.h
+++ Trigger/TrigAnalysis/TrigInDetAnalysisUser/Analysis/src/computils.h
@@ -42,6 +42,8 @@ void myText( Double_t x, Double_t y, Color_t color, const std::string& text, Dou
 std::string stime();
 static std::string release;
 
+double integral( TH1* h );
+
 void Norm( TH1* h, double scale=1 );
 
 double Entries( TH1* h );
@@ -107,6 +109,7 @@ public:
     m_lo(0),
     m_hi(0),
     m_norm(false),
+    m_refnorm(false),
     m_binwidth(false)
   { 
     //    std::cout << "AxisInfo::info" << m_info << std::endl;
@@ -126,6 +129,7 @@ public:
       else if  ( keys[i]=="log" )   m_log       = true;
       else if  ( keys[i]=="sym" )   m_symmetric = true; 
       else if  ( keys[i]=="norm" )  m_norm      = true;
+      else if  ( keys[i]=="refn" )  m_refnorm   = true;
       else if  ( keys[i]=="width" ) m_binwidth  = true;
       else if  ( keys[i]=="auto" )  m_autoset   = true;
       else if  ( keys[i]=="auton" )  {
@@ -181,7 +185,9 @@ public:
 
   bool   autoset() const { return m_autoset; }
 
-  bool   normset() const { return m_norm; }
+  bool   normset()    const { return m_norm; }
+
+  bool   refnormset() const { return m_refnorm; }
   
   bool   symmetric() const { return m_symmetric; }
 
@@ -227,6 +233,7 @@ public:
   double m_hi;
 
   bool m_norm;
+  bool m_refnorm;
   
   bool m_binwidth;
   
@@ -355,12 +362,14 @@ public:
 	href()->SetLineColor(colours[i%6]);
 	href()->SetLineStyle(2);
 	href()->SetMarkerStyle(0);
+        href()->GetYaxis()->SetMoreLogLabels(true);
       }
       //      href()->SetMarkerColor(href()->GetLineColor());
       htest()->SetLineColor(colours[i%6]);
       htest()->SetLineStyle(1);
       htest()->SetMarkerColor(htest()->GetLineColor());
       htest()->SetMarkerStyle(markers[i%6]);
+      htest()->GetYaxis()->SetMoreLogLabels(true);
 
       //      std::cout << "Draw() href() " << href() << "\thtest() " << htest() << "\ttgtest() " << tgtest();
       if ( htest() ) std::cout << "\tentries " << plotable( htest() );
@@ -381,6 +390,7 @@ public:
 	  if ( tgtest() ) { 
 	    zeroErrors(htest());
 	    htest()->GetXaxis()->SetMoreLogLabels(true);
+	    htest()->GetYaxis()->SetMoreLogLabels(true);
 	    htest()->Draw("ep");
 	    setParameters( htest(), tgtest() );
 	    // tgtest()->Draw("p1same");
@@ -388,6 +398,7 @@ public:
 	  }
 	  else { 
 	    htest()->GetXaxis()->SetMoreLogLabels(true);
+	    htest()->GetYaxis()->SetMoreLogLabels(true);
 	    htest()->Draw("ep");
 	  }
 	}
