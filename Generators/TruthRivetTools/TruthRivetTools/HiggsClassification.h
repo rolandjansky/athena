ca01d837c4f04514ff5f86f649b2c1905ca21d45
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-06-22 19:14:53 +0200

    Did a *large* re-write in the TruthRivetTools package.
    Tried to move as much code under Root/ as possible. Out of the header
    files. Also, tried to make as many functions/variables private as
    reasonable. I may have overdone it a bit, but the developers can walk
    back a bit on these updates later on.
    
    
    Former-commit-id: f8ba817ad3207d3f2720de52c5ac57134d06865d

---
+++ Generators/TruthRivetTools/TruthRivetTools/HiggsClassification.h
@@ -0,0 +1,106 @@
+// Dear emacs, this is -*- c++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRUTHRIVETTOOLS_HIGGSCLASSIFICATION_H
+#define TRUTHRIVETTOOLS_HIGGSCLASSIFICATION_H
+
+// ROOT include(s):
+#include <TLorentzVector.h>
+
+// Rivet include(s):
+#include <Rivet/Particle.hh>
+#include <Rivet/Jet.hh>
+
+// Local include(s):
+#include "TruthRivetTools/Enums.h"
+
+/// Higgs Template Cross Section namespace
+namespace HTXS {
+
+   /// Structure holding information about the current event
+   ///
+   /// Four-momenta and event classification according to the
+   /// Higgs Template Cross Section.
+   ///
+   struct HiggsClassification {
+
+      /// Higgs production mode
+      HTXS::HiggsProdMode prodMode;
+
+      /// The Higgs boson
+      TLorentzVector higgs;
+      /// The Higgs boson decay products
+      TLorentzVector p4decay_higgs;
+      /// Associated vector bosons
+      TLorentzVector V;
+      /// The V-boson decay products
+      TLorentzVector p4decay_V;
+      /// Jets are built ignoring Higgs decay products and leptons from V decays
+      /// jets with pT > 25 GeV and 30 GeV
+      std::vector< TLorentzVector > jets25, jets30;
+
+      /// Stage-0 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage0::Category stage0_cat;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet25GeV;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet30GeV;
+
+      /// Error code: classification was succesful or some error occured
+      HTXS::ErrorCode errorCode;
+
+   }; // struct HiggsClassification
+
+} // namespace HTXS
+
+/// Rivet namespace
+namespace Rivet {
+
+   /// Structure holding information about the current event
+   ///
+   /// Four-momenta and event classification according to the
+   /// Higgs Template Cross Section
+   ///
+   struct HiggsClassification {
+
+      /// Higgs production mode
+      HTXS::HiggsProdMode prodMode;
+
+      /// The Higgs boson
+      Rivet::Particle higgs;
+      /// Vector boson produced in asscoiation with the Higgs
+      Rivet::Particle V;
+      /// The four momentum sum of all stable decay products orignating from
+      /// the Higgs boson
+      Rivet::FourMomentum p4decay_higgs;
+      /// The four momentum sum of all stable decay products orignating from
+      /// the vector boson in associated production
+      Rivet::FourMomentum p4decay_V;
+      /// Jets built ignoring Higgs decay products and leptons from V decays,
+      /// pT thresholds at 25 GeV and 30 GeV
+      Rivet::Jets jets25, jets30;
+
+      /// Stage-0 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage0::Category stage0_cat;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet25GeV;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet30GeV;
+
+      /// Error code: classification was succesful or some error occured
+      HTXS::ErrorCode errorCode;
+
+   }; // class HiggsClassification
+
+} // namespace Rivet
+
+#endif // TRUTHRIVETTOOLS_HIGGSCLASSIFICATION_H
--- Generators/TruthRivetTools/TruthRivetTools/IHiggsTruthCategoryTool.h
+++ Generators/TruthRivetTools/TruthRivetTools/IHiggsTruthCategoryTool.h
@@ -1,29 +1,39 @@
+// Dear emacs, this is -*- c++ -*-
+
 /*
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-/*
- * Dual-use tool interface for Rivet routine for classifying MC events according to the Higgs template cross section categories
- * Authors: Jim Lacey (Carleton University)
- * <james.lacey@cern.ch,jlacey@physics.carleton.ca>
- */
-
 #ifndef TRUTHRIVETTOOLS_IHIGGSTRUTHCATEGORYTOOL_H
 #define TRUTHRIVETTOOLS_IHIGGSTRUTHCATEGORYTOOL_H 1
 
+// ASG include(s):
 #include "AsgTools/IAsgTool.h"
-#include "TLorentzVector.h"
-#include "HiggsTemplateCrossSections.h"
-#include "HepMC/GenEvent.h"
 
+// Local include(s):
+#include "TruthRivetTools/HiggsClassification.h"
+
+// Forward declaration(s):
+namespace HepMC {
+   class GenEvent;
+}
+
+/**
+ * Dual-use tool interface for Rivet routine for classifying MC events according
+ * to the Higgs template cross section categories.
+ *
+ * @authors Jim Lacey (Carleton University) <james.lacey@cern.ch,jlacey@physics.carleton.ca>
+ */
 class IHiggsTruthCategoryTool : public virtual asg::IAsgTool {
- public:
-  ASG_TOOL_INTERFACE( IHiggsTruthCategoryTool ) //declares the interface to athena
-    virtual ~IHiggsTruthCategoryTool() {};
- public:
-  virtual StatusCode initialize() = 0;
-  virtual StatusCode finalize () = 0;  
-  virtual HTXS::HiggsClassification getHiggsTruthCategoryObject(const HepMC::GenEvent& HepMCEvent, const HTXS::HiggsProdMode prodMode)=0;
-};
 
-#endif //> !HIGGSTRUTHCLASSIFIER_IHIGGSTRUTHCATEGORYTOOL_H
+public:
+   /// Declare the interface to Athena
+   ASG_TOOL_INTERFACE( IHiggsTruthCategoryTool )
+
+   /// Get the classification for the specifed generated event
+   virtual HTXS::HiggsClassification
+   getHiggsTruthCategoryObject( const HepMC::GenEvent& HepMCEvent ) = 0;
+
+}; // class IHiggsTruthCategoryTool
+
+#endif // !HIGGSTRUTHCLASSIFIER_IHIGGSTRUTHCATEGORYTOOL_H
