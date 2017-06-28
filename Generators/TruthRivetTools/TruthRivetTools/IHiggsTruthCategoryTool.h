c53103ec32a8689e06a62a129a4f9bb6ddf96a80
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-06-28 12:36:21 +0200

    Moved the declaration of HTXS::HiggsClassification into the IHiggsTruthCategoryTool.h header. This is so that users could make use of the tool without being exposed to the Rivet headers. And that's important because certain versions of Rivet are not compatible with the version of Eigen that we use in our EDM. (Their headers can not be included at the same time.)
    Former-commit-id: bed6b3222cd3128b58fc8d7c6d1dcfd6ebb9b0ba

--- Generators/TruthRivetTools/TruthRivetTools/IHiggsTruthCategoryTool.h
+++ Generators/TruthRivetTools/TruthRivetTools/IHiggsTruthCategoryTool.h
@@ -7,16 +7,61 @@
 #ifndef TRUTHRIVETTOOLS_IHIGGSTRUTHCATEGORYTOOL_H
 #define TRUTHRIVETTOOLS_IHIGGSTRUTHCATEGORYTOOL_H 1
 
+// ROOT include(s):
+#include <TLorentzVector.h>
+
 // ASG include(s):
 #include "AsgTools/IAsgTool.h"
 
 // Local include(s):
-#include "TruthRivetTools/HiggsClassification.h"
+#include "TruthRivetTools/Enums.h"
 
 // Forward declaration(s):
 namespace HepMC {
    class GenEvent;
 }
+
+/// Higgs Template Cross Section namespace
+namespace HTXS {
+
+   /// Structure holding information about the current event
+   ///
+   /// Four-momenta and event classification according to the
+   /// Higgs Template Cross Section.
+   ///
+   struct HiggsClassification {
+
+      /// Higgs production mode
+      HTXS::HiggsProdMode prodMode;
+
+      /// The Higgs boson
+      TLorentzVector higgs;
+      /// The Higgs boson decay products
+      TLorentzVector p4decay_higgs;
+      /// Associated vector bosons
+      TLorentzVector V;
+      /// The V-boson decay products
+      TLorentzVector p4decay_V;
+      /// Jets are built ignoring Higgs decay products and leptons from V decays
+      /// jets with pT > 25 GeV and 30 GeV
+      std::vector< TLorentzVector > jets25, jets30;
+
+      /// Stage-0 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage0::Category stage0_cat;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet25GeV;
+      /// Stage-1 HTXS event classifcation
+      /// See: https://cds.cern.ch/record/2138079
+      HTXS::Stage1::Category stage1_cat_pTjet30GeV;
+
+      /// Error code: classification was succesful or some error occured
+      HTXS::ErrorCode errorCode;
+
+   }; // struct HiggsClassification
+
+} // namespace HTXS
 
 /**
  * Dual-use tool interface for Rivet routine for classifying MC events according
