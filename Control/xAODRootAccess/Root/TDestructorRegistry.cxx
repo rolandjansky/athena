ee36096c0964097aa1477d61f7cad03a0d48e5c9
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-06-28 15:27:46 +0200

    Made xAOD::TDestructorRegistry thread safe. Simply by introducing an appropriate mutex in it.
    Former-commit-id: ed8783b701a9ce6d2a4184e8581e4dcf01c04777

--- Control/xAODRootAccess/Root/TDestructorRegistry.cxx
+++ Control/xAODRootAccess/Root/TDestructorRegistry.cxx
@@ -25,6 +25,9 @@ namespace xAOD {
    const TVirtualDestructor*
    TDestructorRegistry::get( const std::type_info& ti ) const {
 
+      // Get a "read lock":
+      std::shared_lock< std::shared_timed_mutex > lock( m_mutex );
+
       // Look for this type:
       Map_t::const_iterator itr = m_types.find( &ti );
       if( itr != m_types.end() ) {
@@ -35,7 +38,12 @@ namespace xAOD {
       ::Error( "xAOD::TDestructorRegistry::get",
                XAOD_MESSAGE( "Destructor not known for type %s" ),
                SG::normalizedTypeinfoName( ti ).c_str() );
-      return 0;
+      return nullptr;
+   }
+
+   TDestructorRegistry::TDestructorRegistry()
+      : m_types(), m_mutex() {
+
    }
 
    TDestructorRegistry::TDestructorHolder::
