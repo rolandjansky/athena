42c82655ec3f27d57358237a33bb454555ab39b9
David Kirchmeier <david.kirchmeier@cern.ch>
2017-06-28 13:46:25 +0200

    svnpull of DerivationFrameworkTau-00-01-56
    changes since last svnpull:
    
    00-01-37: fix ATLASG-564
    00-01-38: TAUP4 introduced; TAUPExtraContent.py modified to accommodate TAUP4
    00-01-39: TAUPExtraContent.py added extra variables for electron BDT
    00-01-40: TAUP2.py updated; TAUP4.py updated; TAUPExtraContent.py updated
    00-01-41: Added InDetTrackParticles container to TAUP2 and TAUP4
    00-01-42: TAUP1 changed trigger thinning; TAUP3 enabled trigger thinning
    00-01-43: include more triggers in TAUP1/3; one HLT collection to TAUP4; update …
    00-01-44: moving the tau truth additional variables to smart slimming list; TJ …
    00-01-45: update trigger on TAUP4; update TauTruthCommon to calculate by default …
    00-01-46: rel21 specific; adding TauTracks in smart slimming; removing trigger …
    00-01-47: rel21 specific; change tauTrackLinks, compatibility with …
    00-01-48: update to include changes of variable names in Rel21, new variables …
    00-01-49: update for rel21 preparation: needs new TAT version to work; test tag
    00-01-50: bug correction for 01-50; missed update in TauCommon
    00-01-51: adding missing variables
    00-01-52: trigger thinning in taup1
    00-01-53: fixing bug with truthmatching not done due to missing antikt4truth …
    00-01-54: bug fix in taup2
    00-01-55: Modified algseq for addStandardJets from TAUPseq to DerivationFrameworkJob
    00-01-56: TJ requested change
    
    Former-commit-id: 99e6f0d57a0fbd79991f38d62ee16b1cfd3b4381

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkTau/share/TAUP2.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkTau/share/TAUP2.py
@@ -11,153 +11,151 @@ from DerivationFrameworkEGamma.EGammaCommon import *
 from DerivationFrameworkMuons.MuonsCommon import *
 if DerivationFrameworkIsMonteCarlo:
   from DerivationFrameworkMCTruth.MCTruthCommon import *
-  from DerivationFrameworkTau.TauTruthCommon import *
 from DerivationFrameworkTau.TauCommon import *
 
-# ==========================================================================================================================
+
+# =============================================
+# Private sequence here
+# =============================================
+TAUP2seq = CfgMgr.AthSequencer("TAUP2Sequence")
+DerivationFrameworkJob += TAUP2seq
+
+# =============================================
 # Set up stream
-# ==========================================================================================================================
-streamName                                       = derivationFlags.WriteDAOD_TAUP2Stream.StreamName
-fileName                                         = buildFileName( derivationFlags.WriteDAOD_TAUP2Stream )
-TAUP2Stream                                      = MSMgr.NewPoolRootStream( streamName, fileName )
+# =============================================
+streamName      = derivationFlags.WriteDAOD_TAUP2Stream.StreamName
+fileName        = buildFileName( derivationFlags.WriteDAOD_TAUP2Stream )
+TAUP2Stream     = MSMgr.NewPoolRootStream( streamName, fileName )
 TAUP2Stream.AcceptAlgs(["TAUP2Kernel"])
 
-# ==========================================================================================================================
+
+# =============================================
 # Thinning tool
-# ==========================================================================================================================
-# from DerivationFrameworkCore.ThinningHelper import ThinningHelper
-# TAUP2ThinningHelper                              = ThinningHelper( "TAUP2ThinningHelper" )
-# TAUP2ThinningHelper.TriggerChains                = "HLT_e24.*|HLT_e60.*"
-# TAUP2ThinningHelper.AppendToStream( TAUP2Stream )
-# 
-# # MET/Jet tracks
-# thinning_expression                              = "(InDetTrackParticles.pt > 0.5*GeV) && (InDetTrackParticles.numberOfPixelHits > 0) && (InDetTrackParticles.numberOfSCTHits > 5) && (abs(DFCommonInDetTrackZ0AtPV) < 1.5)"
-# from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__TrackParticleThinning
-# TAUP2MetTPThinningTool                           = DerivationFramework__TrackParticleThinning(
-#                                                      name                      = "TAUP2MetTPThinningTool",
-#                                                      ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
-#                                                      SelectionString           = thinning_expression,
-#                                                      InDetTrackParticlesKey    = "InDetTrackParticles",
-#                                                      ApplyAnd                  = True)
-# ToolSvc                                         += TAUP2MetTPThinningTool
-# 
-# from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__JetTrackParticleThinning
-# TAUP2JetTPThinningTool                           = DerivationFramework__JetTrackParticleThinning(
-#                                                      name                      = "TAUP2JetTPThinningTool",
-#                                                      ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
-#                                                      JetKey                    = "AntiKt4LCTopoJets",
-#                                                      InDetTrackParticlesKey    = "InDetTrackParticles",
-#                                                      ApplyAnd                  = True)
-# ToolSvc                                         += TAUP2JetTPThinningTool
-# 
-# # Tracks associated with electrons
-# from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__EgammaTrackParticleThinning
-# TAUP2ElectronTPThinningTool                      = DerivationFramework__EgammaTrackParticleThinning(
-#                                                      name                      = "TAUP2ElectronTPThinningTool",
-#                                                      ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
-#                                                      SGKey                     = "Electrons",
-#                                                      InDetTrackParticlesKey    = "InDetTrackParticles")
-# ToolSvc                                         += TAUP2ElectronTPThinningTool
-# 
-# # Tracks associated with muons
-# from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__MuonTrackParticleThinning
-# TAUP2MuonTPThinningTool                          = DerivationFramework__MuonTrackParticleThinning(
-#                                                      name                      = "TAUP2MuonTPThinningTool",
-#                                                      ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
-#                                                      MuonKey                   = "Muons",
-#                                                      InDetTrackParticlesKey    = "InDetTrackParticles")
-# ToolSvc                                         += TAUP2MuonTPThinningTool
-# 
-# # Tracks associated with taus
-# from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__TauTrackParticleThinning
-# TAUP2TauTPThinningTool                           = DerivationFramework__TauTrackParticleThinning(
-#                                                      name                      = "TAUP2TauTPThinningTool",
-#                                                      ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
-#                                                      TauKey                    = "TauJets",
-#                                                      InDetTrackParticlesKey    = "InDetTrackParticles",
-#                                                      ConeSize                  = 0.6)
-# ToolSvc                                         += TAUP2TauTPThinningTool
+# =============================================
+from DerivationFrameworkCore.ThinningHelper import ThinningHelper
+TAUP2ThinningHelper                              = ThinningHelper( "TAUP2ThinningHelper" )
+TAUP2ThinningHelper.TriggerChains                = "HLT_e24.*|HLT_e60.*"
+TAUP2ThinningHelper.AppendToStream( TAUP2Stream )
 
-# ==========================================================================================================================
+thinningTools = []
+
+# MET/Jet tracks
+thinning_expression     = "(InDetTrackParticles.pt > 0.5*GeV) && (InDetTrackParticles.numberOfPixelHits > 0) && (InDetTrackParticles.numberOfSCTHits > 5) && (abs(DFCommonInDetTrackZ0AtPV) < 1.5)"
+from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__TrackParticleThinning
+TAUP2MetTPThinningTool  = DerivationFramework__TrackParticleThinning(
+  name                    = "TAUP2MetTPThinningTool",
+  ThinningService         = TAUP2ThinningHelper.ThinningSvc(),
+  SelectionString         = thinning_expression,
+  InDetTrackParticlesKey  = "InDetTrackParticles",
+  ApplyAnd                = True)
+
+ToolSvc += TAUP2MetTPThinningTool
+thinningTools.append(TAUP2MetTPThinningTool)
+
+from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__JetTrackParticleThinning
+TAUP2JetTPThinningTool = DerivationFramework__JetTrackParticleThinning(
+  name                    = "TAUP2JetTPThinningTool",
+  ThinningService         = TAUP2ThinningHelper.ThinningSvc(),
+  JetKey                  = "AntiKt4LCTopoJets",
+  InDetTrackParticlesKey  = "InDetTrackParticles",
+  ApplyAnd                = True)
+
+ToolSvc += TAUP2JetTPThinningTool
+thinningTools.append(TAUP2JetTPThinningTool)
+
+# Tracks associated with electrons
+from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__EgammaTrackParticleThinning
+TAUP2ElectronTPThinningTool = DerivationFramework__EgammaTrackParticleThinning(
+  name                      = "TAUP2ElectronTPThinningTool",
+  ThinningService           = TAUP2ThinningHelper.ThinningSvc(),
+  SGKey                     = "Electrons",
+  InDetTrackParticlesKey    = "InDetTrackParticles")
+
+ToolSvc  += TAUP2ElectronTPThinningTool
+thinningTools.append(TAUP2ElectronTPThinningTool)
+
+# Tracks associated with muons
+from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__MuonTrackParticleThinning
+TAUP2MuonTPThinningTool = DerivationFramework__MuonTrackParticleThinning(
+  name                    = "TAUP2MuonTPThinningTool",
+  ThinningService         = TAUP2ThinningHelper.ThinningSvc(),
+  MuonKey                 = "Muons",
+  InDetTrackParticlesKey  = "InDetTrackParticles")
+
+ToolSvc  += TAUP2MuonTPThinningTool
+thinningTools.append(TAUP2MuonTPThinningTool)
+
+# Tracks associated with taus
+from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFramework__TauTrackParticleThinning
+TAUP2TauTPThinningTool = DerivationFramework__TauTrackParticleThinning(
+  name                    = "TAUP2TauTPThinningTool",
+  ThinningService         = TAUP2ThinningHelper.ThinningSvc(),
+  TauKey                  = "TauJets",
+  # SelectionString         = "TauJets.pt > 12*GeV",
+  InDetTrackParticlesKey  = "InDetTrackParticles",
+  ConeSize                = 0.6)
+
+ToolSvc  += TAUP2TauTPThinningTool
+thinningTools.append(TAUP2TauTPThinningTool)
+
+
+# =============================================
 # Skimming tool
-# ==========================================================================================================================
-# elRequirement                                    = "( count( (Electrons.DFCommonElectronsIsEMLoose || Electrons.DFCommonElectronsLHLoose) && (Electrons.pt > 20.0*GeV) && (abs(Electrons.eta) < 2.6) ) >= 1 )"
-# muRequirement                                    = "( count( (Muons.pt > 10.0*GeV) && (abs(Muons.eta) < 2.0) && Muons.DFCommonGoodMuon ) < 1 )"
-# tauRequirement                                   = "( count( (TauJets.pt > 12.0*GeV) && (abs(TauJets.eta) < 2.6) && (abs(TauJets.charge) == 1.0) ) >= 1 )"
-# 
-# expression                                       = elRequirement + " && " + muRequirement + " && " + tauRequirement
-# 
-# from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool
-# TAUP2SkimmingTool                                = DerivationFramework__xAODStringSkimmingTool(
-#                                                      name                      = "TAUP2SkimmingTool",
-#                                                      expression                = expression)
-# 
-# ToolSvc                                         += TAUP2SkimmingTool
+# =============================================
+tauRequirement = "( count( (TauJets.pt > 12.0*GeV) && (abs(TauJets.eta) < 2.6) ) >= 1 )"
+expression     = tauRequirement
 
-# ==========================================================================================================================
-# Kernel algorithm
-# ==========================================================================================================================
-augmentationTools                                = []
-# JRC: This is now done in TauCommon.py
-# if DerivationFrameworkIsMonteCarlo:
-#   augmentationTools                             += [DFCommonTauTruthMatchingWrapper]
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool
+TAUP2SkimmingTool   = DerivationFramework__xAODStringSkimmingTool(
+  name                = "TAUP2SkimmingTool",
+  expression          = expression)
+
+ToolSvc += TAUP2SkimmingTool
+
+# =============================================
+# Standard jets
+# =============================================
+if globalflags.DataSource() == "geant4":
+  print 'Adding AntiKt4TruthJets here'
+  #addStandardJets("AntiKt", 0.4, "Truth", 5000, mods="truth_ungroomed", algseq=TAUP2seq, outputGroup="TAUP2")
+  reducedJetList = ["AntiKt4TruthJets"]
+  replaceAODReducedJets(reducedJetList,TAUP2seq, "TAUP2")  
+  from DerivationFrameworkTau.TauTruthCommon import *
+
+# =============================================
+# Create derivation Kernel
+# =============================================
 
 from DerivationFrameworkCore.DerivationFrameworkCoreConf import DerivationFramework__DerivationKernel
-DerivationFrameworkJob                          += CfgMgr.DerivationFramework__DerivationKernel(
-                                                     "TAUP2Kernel",
-#                                                     SkimmingTools             = [TAUP2SkimmingTool],
-#                                                     ThinningTools             = [TAUP2MetTPThinningTool,
-#                                                                                  TAUP2JetTPThinningTool,
-#                                                                                  TAUP2ElectronTPThinningTool,
-#                                                                                  TAUP2MuonTPThinningTool,
-#                                                                                  TAUP2TauTPThinningTool],
-                                                     AugmentationTools         = augmentationTools)
 
-# ==========================================================================================================================
+TAUP2seq += CfgMgr.DerivationFramework__DerivationKernel(
+  "TAUP2Kernel",
+  SkimmingTools             = [TAUP2SkimmingTool],
+  ThinningTools             = thinningTools
+  )
+
+# ===============================================
 # Add the containers to the output stream (slimming done here)
-# ==========================================================================================================================
+# ===============================================
 from DerivationFrameworkCore.SlimmingHelper import SlimmingHelper
 from DerivationFrameworkTau.TAUPExtraContent import *
 
-TAUP2SlimmingHelper                              = SlimmingHelper("TAUP2SlimmingHelper")
-TAUP2SlimmingHelper.SmartCollections             = [
-#                                                    "Electrons",
-#                                                    "Photons",
-#                                                    "Muons",
-                                                    "TauJets"]
-#                                                    "MET_Reference_AntiKt4EMTopo",
-#                                                    "MET_Reference_AntiKt4LCTopo",
-#                                                    "AntiKt4EMTopoJets",
-#                                                    "AntiKt4LCTopoJets",
-#                                                    "BTagging_AntiKt4EMTopo",
-#                                                    "BTagging_AntiKt4LCTopo",
-#                                                    "InDetTrackParticles",
-#                                                    "PrimaryVertices"]
-
+TAUP2SlimmingHelper  = SlimmingHelper("TAUP2SlimmingHelper")
+TAUP2SlimmingHelper.SmartCollections  = ["Electrons",
+                                         # "Photons",
+                                         "Muons"]
 if DerivationFrameworkIsMonteCarlo:
-  TAUP2SlimmingHelper.StaticContent              = ["xAOD::TruthParticleContainer#TruthElectrons",
-                                                    "xAOD::TruthParticleAuxContainer#TruthElectronsAux.",
-                                                    "xAOD::TruthParticleContainer#TruthMuons",
-                                                    "xAOD::TruthParticleAuxContainer#TruthMuonsAux.",
-                                                    "xAOD::TruthParticleContainer#TruthTaus",
-                                                    "xAOD::TruthParticleAuxContainer#TruthTausAux."]
-#                                                    "xAOD::TruthParticleContainer#TruthPhotons",
-#                                                    "xAOD::TruthParticleAuxContainer#TruthPhotonsAux.",
-#                                                    "xAOD::TruthParticleContainer#TruthNeutrinos",
-#                                                    "xAOD::TruthParticleAuxContainer#TruthNeutrinosAux."]
+  TAUP2SlimmingHelper.StaticContent   = ["xAOD::TruthParticleContainer#TruthElectrons",
+                                         "xAOD::TruthParticleAuxContainer#TruthElectronsAux.",
+                                         "xAOD::TruthParticleContainer#TruthMuons",
+                                         "xAOD::TruthParticleAuxContainer#TruthMuonsAux."]
 
-# TAUP2SlimmingHelper.IncludeMuonTriggerContent    = True
-# TAUP2SlimmingHelper.IncludeTauTriggerContent     = True
-# TAUP2SlimmingHelper.IncludeEGammaTriggerContent  = True
-# TAUP2SlimmingHelper.IncludeEtMissTriggerContent  = True
-# TAUP2SlimmingHelper.IncludeJetTriggerContent     = True
-# TAUP2SlimmingHelper.IncludeBJetTriggerContent    = True
-
-TAUP2SlimmingHelper.ExtraVariables               = ExtraContentTAUP2
+#TAUP2SlimmingHelper.ExtraVariables               = ExtraContentTAUP2
 TAUP2SlimmingHelper.AllVariables                 = ExtraContainersTAUP2
 
 if globalflags.DataSource() == "geant4":
-  TAUP2SlimmingHelper.ExtraVariables            += ExtraContentTruthTAUP2
+  #TAUP2SlimmingHelper.ExtraVariables            += ExtraContentTruthTAUP2
+  TAUP2SlimmingHelper.ExtraVariables            = ExtraContentTruthTAUP2
   TAUP2SlimmingHelper.AllVariables              += ExtraContainersTruthTAUP2
 
 TAUP2SlimmingHelper.AppendContentToStream(TAUP2Stream)
@@ -165,6 +163,3 @@ TAUP2SlimmingHelper.AppendContentToStream(TAUP2Stream)
 TAUP2Stream.AddItem("xAOD::EventShape#*")
 TAUP2Stream.AddItem("xAOD::EventShapeAuxInfo#*")
 
-# Add MET_RefFinalFix
-# JRC TEMPORARILY COMMENTED
-# addMETOutputs(TAUP2Stream)
---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkTau/share/TAUP4.py
@@ -0,0 +1,95 @@
+# **************************************************************************************************************************
+# TAUP4.py 
+# reductionConf flag TAUP4 in Reco_tf.py   
+# **************************************************************************************************************************
+from DerivationFrameworkCore.DerivationFrameworkMaster import *
+
+# ==========================================================================================================================
+# Set up stream
+# ==========================================================================================================================
+streamName                                       = derivationFlags.WriteDAOD_TAUP4Stream.StreamName
+fileName                                         = buildFileName( derivationFlags.WriteDAOD_TAUP4Stream )
+TAUP4Stream                                      = MSMgr.NewPoolRootStream( streamName, fileName )
+TAUP4Stream.AcceptAlgs(["TAUP4Kernel"])
+
+from AthenaServices.Configurables import ThinningSvc, createThinningSvc
+augStream                                        = MSMgr.GetStream( streamName )
+evtStream                                        = augStream.GetEventStream()
+svcMgr                                          += createThinningSvc(
+                                                     svcName                   = "TAUP4ThinningSvc",
+                                                     outStreams                = [evtStream])
+
+
+# ==========================================================================================================================
+# Trigger skimming tool
+# ==========================================================================================================================
+trigger_list                                     = ['HLT_j10' , 'HLT_j15' , 'HLT_j20' ,
+                                                    'HLT_j25' , 'HLT_j30' , 'HLT_j35' ,
+                                                    'HLT_j40' , 'HLT_j45' , 'HLT_j55' ,
+                                                    'HLT_j60' , 'HLT_j60_L1RD0_FILLED', 'HLT_j85',
+                                                    'HLT_j100', 'HLT_j110', 'HLT_j150',
+                                                    'HLT_j175', 'HLT_j200', 'HLT_j260',
+                                                    'HLT_j300', 'HLT_j320', 'HLT_j360',
+                                                    'HLT_j380', 'HLT_j400', 'HLT_j420' , 'HLT_j440' ,'HLT_j460']
+
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__TriggerSkimmingTool
+TAUP4TriggerSkimmingTool                         = DerivationFramework__TriggerSkimmingTool(
+                                                     name                      = "TAUP4TriggerSkimmingTool",
+                                                     TriggerListOR             = trigger_list)
+
+ToolSvc                                         += TAUP4TriggerSkimmingTool
+
+# ==========================================================================================================================
+# String skimming tool
+# ==========================================================================================================================
+tau_requirement                                  = "( count( (TauJets.pt > 15.0*GeV) && (abs(TauJets.eta) < 2.6) ) >= 1 )"
+
+expression                                       = tau_requirement
+# print expression
+
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool
+TAUP4StringSkimmingTool                          = DerivationFramework__xAODStringSkimmingTool(
+                                                     name                      = "TAUP4StringSkimmingTool",
+                                                     expression                = expression)
+
+ToolSvc                                         += TAUP4StringSkimmingTool
+#print TAUP4StringSkimmingTool
+# truth thinning here:
+import DerivationFrameworkTau.TAUPThinningHelper 
+TAUP4TruthThinningTools = DerivationFrameworkTau.TAUPThinningHelper.setup("TAUP4",
+                                                                          "TAUP4ThinningSvc",
+                                                                          ToolSvc)
+
+# ==========================================================================================================================
+# Kernel algorithm
+# ==========================================================================================================================
+from DerivationFrameworkCore.DerivationFrameworkCoreConf import DerivationFramework__DerivationKernel
+DerivationFrameworkJob                          += CfgMgr.DerivationFramework__DerivationKernel(
+                                                     "TAUP4Kernel",
+                                                     SkimmingTools             = [TAUP4TriggerSkimmingTool,
+                                                                                  TAUP4StringSkimmingTool],
+                                                     ThinningTools             = TAUP4TruthThinningTools,
+                                                     )
+
+# ==========================================================================================================================
+# Add the containers to the output stream (slimming done here)
+# ==========================================================================================================================
+from DerivationFrameworkCore.SlimmingHelper import SlimmingHelper
+from DerivationFrameworkTau.TAUPExtraContent import *
+
+TAUP4SlimmingHelper                              = SlimmingHelper("TAUP4SlimmingHelper")
+TAUP4SlimmingHelper.SmartCollections             = ["Electrons",
+                                                    "Photons",
+                                                    "Muons",
+                                                    # "TauJets",
+                                                    "MET_Reference_AntiKt4EMTopo",
+                                                    "AntiKt4EMTopoJets",
+                                                    "BTagging_AntiKt4EMTopo",
+                                                    "InDetTrackParticles",
+                                                    "PrimaryVertices"]
+
+#TAUP4SlimmingHelper.StaticContent                = ["CaloCellContainer#AODCellContainer"]
+#TAUP4SlimmingHelper.ExtraVariables               = ["PhotonCollection.weta2.f1.phi.weta1.emaxs1"]
+TAUP4SlimmingHelper.AllVariables                 = ExtraContainersTAUP4
+TAUP4SlimmingHelper.IncludeTauTriggerContent     = True
+TAUP4SlimmingHelper.AppendContentToStream(TAUP4Stream)
