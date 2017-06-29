d31e9ac42cc772598b56a7ee0e39b0fcd0dbfda0
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2017-06-29 23:28:34 +0200

    Adapt CaloClusterConstituentsOrigin to new JetConstituentModifierBase structure   * Add some type-checking to the inputs   * Eliminate the EM-scale handling, as this will be done by the     ClusterAtEMScale tool as an earlier modification step
    Former-commit-id: 844ba8bb3f759e2a906cfbc46bbb2492f675ed8c

--- Reconstruction/Jet/JetRecTools/Root/CaloClusterConstituentsOrigin.cxx
+++ Reconstruction/Jet/JetRecTools/Root/CaloClusterConstituentsOrigin.cxx
@@ -7,25 +7,27 @@
 //
 //
 #include "JetRecTools/CaloClusterConstituentsOrigin.h"
-//#include "xAODBase/IParticleContainer.h"
-//#include "xAODCaloEvent/CaloClusterContainer.h"
-//#include "JetRecTools/JetConstituentModifierBase.h"
 #include "xAODCaloEvent/CaloVertexedTopoCluster.h"
-
-
 
 CaloClusterConstituentsOrigin::CaloClusterConstituentsOrigin(const std::string & name): JetConstituentModifierBase(name) {
 
 #ifdef ASG_TOOL_ATHENA
   declareInterface<IJetConstituentModifier>(this);
 #endif
-  declareProperty("VertexContainer",m_vertexContName="PrimaryVertices");
-  declareProperty("UseEMScale",m_useEMScale=false);
+  declareProperty("VertexContainer",m_vertexContName);
   
 }
 
-StatusCode CaloClusterConstituentsOrigin::process(xAOD::IParticleContainer* cont) const {
-   xAOD::CaloClusterContainer* clust = dynamic_cast<xAOD::CaloClusterContainer*> (cont); // Get CaloCluster container
+StatusCode CaloClusterConstituentsOrigin::initialize() {
+  if(m_inputType!=xAOD::Type::CaloCluster) {
+    ATH_MSG_ERROR("As the name suggests, CaloClusterConstituentsOrigin cannot operate on objects of type "
+		  << m_inputType);
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode CaloClusterConstituentsOrigin::process_impl(xAOD::IParticleContainer* cont) const {
+   xAOD::CaloClusterContainer* clust = static_cast<xAOD::CaloClusterContainer*> (cont); // Get CaloCluster container
 
    if(clust)
      {
@@ -33,49 +35,28 @@ StatusCode CaloClusterConstituentsOrigin::process(xAOD::IParticleContainer* cont
 
        //get vertexcontainer from eventstore
        ATH_CHECK( evtStore()->retrieve(vertexContainer, m_vertexContName) );
-
-       return process(clust, vertexContainer->at(0)); 
+       
+       for(const auto& pv : *vertexContainer) {
+	 // Apply the origin correction iff a PV is identified
+	 if(pv->vertexType()==xAOD::VxType::PriVtx) {return correctToOriginVtx(*clust, *pv);}
+       }
+       // Exit silently if we did not find a designated primary vertex
+       // This leaves the clusters uncorrected, which is fine
+       return StatusCode::SUCCESS;
      }
    return StatusCode::FAILURE;
 }
 
-// Apply PU origining and decorate the CaloCluster container appropriately:
-	
-StatusCode CaloClusterConstituentsOrigin::process(xAOD::CaloClusterContainer* cont, const xAOD::Vertex *vert) const {
-  if (!vert)
-    {
-      ATH_MSG_ERROR( " Unable to find vertex " << vert);
-      return StatusCode::FAILURE;
-    }
-  if(m_useEMScale) return processEM(cont,vert);
-  else return processLC(cont,vert);
-}
-
-StatusCode CaloClusterConstituentsOrigin::processLC(xAOD::CaloClusterContainer* cont, const xAOD::Vertex *vert) const {
-
-
-  for(xAOD::CaloCluster* cl : *cont) {
+// Apply PV origin correction and decorate the CaloCluster container appropriately.
+// We depend on the ClustersAtEMScaleTool to change the calibration state if needed
+// so we can just correct the position based on the default four-momentum
+StatusCode CaloClusterConstituentsOrigin::correctToOriginVtx(xAOD::CaloClusterContainer& cont, const xAOD::Vertex& vert) const {
+  for(xAOD::CaloCluster* cl : cont) {
     if(cl->calE()>1e-9) {
-      xAOD::CaloVertexedTopoCluster corrCL( *cl,vert->position());
+      xAOD::CaloVertexedTopoCluster corrCL( *cl,vert.position());
       cl->setEta(corrCL.eta());
       cl->setPhi(corrCL.phi());
     }
   }
   return StatusCode::SUCCESS;
 }
-
-StatusCode CaloClusterConstituentsOrigin::processEM(xAOD::CaloClusterContainer* cont, const xAOD::Vertex *vert) const {
-
-
-  for(xAOD::CaloCluster* cl : *cont) {
-    if(cl->rawE()>1e-9) {
-      xAOD::CaloVertexedTopoCluster corrCL( *cl,xAOD::CaloCluster::UNCALIBRATED, vert->position());
-      cl->setE(corrCL.e());
-      cl->setEta(corrCL.eta());
-      cl->setPhi(corrCL.phi());
-    }
-  }
-  return StatusCode::SUCCESS;
-}
-
-
