cd5e8019ee325552e8053cfe996726b944e9b812
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2017-06-29 02:07:32 +0200

    sync with ElectronEfficiencyCorrection-00-02-03 from svn. This commit adds a future to get the index of the uncorrelated systematic requested by the Standard Model group. Plus,  minor const correctness fixes and avoiding "global static" variables (for now just hide it in a function)
    Former-commit-id: 1c68bb8b00021c47b71343bfa49ce898f29f83f0

--- PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/util/Messaging.h
+++ PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/util/Messaging.h
@@ -6,19 +6,24 @@
 #define __ELECTRONEFFICIENCYCORRECTIONMESSAGING__
 #include "AsgTools/AsgMessaging.h"
 
-static  asg::AsgMessaging dummymsg("");
-
-#define DUMMY_MSG( lvl, ARG ) {dummymsg.msg(lvl)<<ARG<<endmsg ;}
-#define MSG_DEBUG( ARG ) {DUMMY_MSG(MSG::DEBUG, ARG);}
-#define MSG_INFO( ARG ) {DUMMY_MSG(MSG::INFO, ARG);}
-#define MSG_WARNING( ARG ) {DUMMY_MSG(MSG::WARNING, ARG);}
-#define MSG_ERROR( ARG ) {DUMMY_MSG(MSG::ERROR, ARG);}
-#define MSG_FATAL( ARG ) {DUMMY_MSG(MSG::FATAL, ARG);}
-#define MSG_ABORT( ARG ) {DUMMY_MSG(MSG::FATAL, ARG); std::abort();} 
-
 namespace MSGHELPERS{
   const asg::AsgMessaging& getMsgStream();
 }
-const asg::AsgMessaging& MSGHELPERS::getMsgStream(){return dummymsg;}
+const asg::AsgMessaging& MSGHELPERS::getMsgStream(){
+  static const asg::AsgMessaging dummymsg("");
+  return dummymsg;
+}
+
+#define DUMMY_MSG(lvl, ARG ) {						\
+    const asg::AsgMessaging& dummymsg=MSGHELPERS::getMsgStream();	\
+    dummymsg.msg(lvl)<<ARG<<endmsg ;					\
+}
+#define MSG_DEBUG( ARG ) {DUMMY_MSG(MSG::DEBUG, ARG);}
+#define MSG_INFO( ARG ) {DUMMY_MSG(MSG::INFO, ARG);}
+#define MSG_WARNING( ARG ) { DUMMY_MSG(MSG::WARNING, ARG);}
+#define MSG_ERROR( ARG ) { DUMMY_MSG(MSG::ERROR, ARG);}
+#define MSG_FATAL( ARG ) { DUMMY_MSG(MSG::FATAL, ARG);}
+#define MSG_ABORT( ARG ) { DUMMY_MSG(MSG::FATAL, ARG); std::abort();} 
+
 
 #endif
