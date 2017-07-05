7e8e79aed508a727da5393270da7563ffb43cf02
Efe Yigitbasi <efe.yigitbasi@cern.ch>
2017-07-05 14:51:34 +0000

    Update ISF_FastCaloSimParametrization_SimPostInclude_1mm.py
    Former-commit-id: 0db518a82426fb6f858aedb6bc028a1d40caf36f

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_SimPostInclude_1mm.py
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_SimPostInclude_1mm.py
@@ -1,12 +1,5 @@
-def add_my_user_action():
-    from G4AtlasApps import PyG4Atlas,AtlasG4Eng
-    MyAction = PyG4Atlas.UserAction( 'ISF_Geant4UserActions','FastCaloSimParamAction',['BeginOfEvent','EndOfEvent','BeginOfRun','EndOfRun','Step'])
-    ## Radii include:  MaxRadius MaxRadiusLAr MaxRadiusHEC MaxRadiusFCAL MaxRadiusTile
-    #MyAction.set_Properties({"shift_lar_subhit":"1","shorten_lar_step":"1","MaxRadius":"1","MaxRadiusLAr":"1"})
-    MyAction.set_Properties({"shift_lar_subhit":"1","shorten_lar_step":"1","MaxRadiusLAr":"1"})
-    AtlasG4Eng.G4Eng.menu_UserActions.add_UserAction(MyAction)
-simFlags.InitFunctions.add_function("postInit", add_my_user_action)
-                           
-#from ISF_FastCaloSimParametrization.ISF_FastCaloSimParametrizationConf import FastCaloSimParamAlg
-#topSeq += FastCaloSimParamAlg()
-ISF_HITSStream.stream1.ItemList += ["ISF_FCS_Parametrization::FCS_StepInfoCollection#ZHMergedEventSteps"]
+from G4AtlasApps.SimFlags import simFlags
+simFlags.OptionalUserActionList.addAction('G4UA::FastCaloSimParamActionTool',['BeginOfEvent','EndOfEvent','BeginOfRun','EndOfRun','Step'])
+simFlags.UserActionConfig.addConfig('G4UA::FastCaloSimParamActionTool',"shift_lar_subhit","1")
+simFlags.UserActionConfig.addConfig('G4UA::FastCaloSimParamActionTool',"shorten_lar_step","1")
+#simFlags.UserActionConfig.addConfig('G4UA::FastCaloSimParamActionTool',"MaxRadiusLAr","1")
