611e36743b327d2620d9b772192cc53fbc8136b9
lawrenceleejr <lawrenceleejr@gmail.com>
2017-07-07 11:55:19 +0200

    VrtSecInclusive: Fully working setup for r21 from RAW to DAOD_RPVLL without Errors
    Former-commit-id: 4a6a897a85eda4e8d450b4e05fb18d182681ec44

---
+++ Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
@@ -0,0 +1,4 @@
+StreamESD.ItemList+=["xAOD::TrackParticleContainer#VrtSecInclusive*"]
+StreamESD.ItemList+=["xAOD::VertexContainer#VrtSecInclusive*"]
+StreamESD.ItemList+=["xAOD::VertexAuxContainer#VrtSecInclusive*"]
+
