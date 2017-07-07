2baf06d3e7f78f3fd7327db5d671ccb8c52d4fd4
John Chapman <jchapman@cern.ch>
2017-07-07 15:21:34 +0200

    Remove all usage of MCTruthMenu and MCTruthStrg classes in python
    There are a number of job options which define callbacks to tweak the old-style
    MCTruth Strategy configuration. This code is all obsolete. For now I've left it
    commented out. The next stage will be to check for (and create if needed) the
    equivalent ISF truthStrategy configurations and mention the error messages
    that these should be used.
    
    
    Former-commit-id: 08062604bdf4c37b1ec223e28636913fe8a2c406

--- Simulation/SimulationJobOptions/share/g4/preInclude.CaloDecays.py
+++ Simulation/SimulationJobOptions/share/g4/preInclude.CaloDecays.py
@@ -7,10 +7,12 @@
 ###############################################################################
 
 def add_calo_decay_truth_strategies():
-    ## Modifying truth strategies as requested by e/gamma group
-    from G4AtlasApps import AtlasG4Eng
-    AtlasG4Eng.G4Eng._ctrl.mctruthMenu.listStrategies()
-    AtlasG4Eng.G4Eng._ctrl.mctruthMenu.activateStrategy('Decay', 'CALO::CALO', 1)
+    print "ERROR add_calo_decay_truth_strategies is obsolete"
+    ## ## Modifying truth strategies as requested by e/gamma group
+    ## from G4AtlasApps import AtlasG4Eng
+    ## AtlasG4Eng.G4Eng._ctrl.mctruthMenu.listStrategies()
+    ## AtlasG4Eng.G4Eng._ctrl.mctruthMenu.activateStrategy('Decay', 'CALO::CALO', 1)
+
 
 ## Register the callback
 simFlags.InitFunctions.add_function("postInit", add_calo_decay_truth_strategies)
