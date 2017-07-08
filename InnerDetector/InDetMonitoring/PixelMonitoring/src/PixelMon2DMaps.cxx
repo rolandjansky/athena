a52912619f0f83fb06cc99a39f5ea2bb4bb5279d
Knut Zoch <knut.zoch@cern.ch>
2017-07-08 03:26:12 +0200

    Adjusted formatting to minimise visual differences
    Former-commit-id: dc4894b07ca3d1566ffbcf651dedb1ef9ab354ff

--- InnerDetector/InDetMonitoring/PixelMonitoring/src/PixelMon2DMaps.cxx
+++ InnerDetector/InDetMonitoring/PixelMonitoring/src/PixelMon2DMaps.cxx
@@ -14,18 +14,25 @@
 
 PixelMon2DMaps::PixelMon2DMaps(std::string name, std::string title, bool doIBL) : m_doIBL(doIBL)
 {
+   std::string setatext = ";shifted eta index of module";
+   std::string etatext = ";eta index of module";
+   std::string phitext = ";phi index of module";
+   std::string disktext = ";disk number";
+
    if (m_doIBL) {
-      IBL3D = new TH2F((name+"_IBL3D").c_str(),(title + ", IBL 3D modules " + ";eta index of module;phi index of module").c_str(), 8, -.5, 7.5, 14, -0.5, 13.5);
-      IBL2D = new TH2F((name+"_IBL2D").c_str(),(title + ", IBL planar modules " + ";shifted eta index of module;phi index of module").c_str(), 12, -6.5, 5.5, 14, -0.5, 13.5);
-      IBL   = new TH2F((name+"_IBL").c_str(),  (title + ", IBL " + ";shifted eta index of module;phi index of module").c_str(), 32, -16.5, 15.5, 14, -0.5, 13.5);
+      IBL3D = new TH2F((name+"_IBL3D").c_str(), (title + ", IBL 3D modules " + etatext + phitext).c_str(),8,-.5,7.5,14,-0.5,13.5);
+      IBL2D = new TH2F((name+"_IBL2D").c_str(), (title + ", IBL planar modules " + setatext + phitext).c_str(),12,-6.5,5.5,14,-0.5,13.5);
+      IBL   = new TH2F((name+"_IBL").c_str(),   (title + ", IBL " + setatext + phitext).c_str(),32,-16.5,15.5,14,-0.5,13.5);
    }
-   B0 = new TH2F((name+"_B0").c_str(),      (title + ", B0 "   + ";eta index of module;phi index of module").c_str(), 13, -6.5, 6.5, 22, -0.5, 21.5);
-   B1 = new TH2F((name+"_B1").c_str(),      (title + ", B1 "   + ";eta index of module;phi index of module").c_str(), 13, -6.5, 6.5, 38, -0.5, 37.5);
-   B2 = new TH2F((name+"_B2").c_str(),      (title + ", B2 "   + ";eta index of module;phi index of module").c_str(), 13, -6.5, 6.5, 52, -0.5, 51.5);
-   A  = new TH2F((name+"_ECA" ).c_str(),    (title + ", ECA "  + ";disk number;phi index of module").c_str(),          3, -0.5, 2.5, 48, -0.5, 47.5);
-   C  = new TH2F((name+"_ECC" ).c_str(),    (title + ", ECC "  + ";disk number;phi index of module").c_str(),          3, -0.5, 2.5, 48, -0.5, 47.5);
-   DBMA = new TH2F((name+"_DBMA" ).c_str(), (title + ", DBMA " + ";layer number;phi index of telescope").c_str(),      3, -0.5, 2.5, 4, -0.5, 3.5);
-   DBMC = new TH2F((name+"_DBMC" ).c_str(), (title + ", DBMC " + ";layer number;phi index of telescope").c_str(),      3, -0.5, 2.5, 4, -0.5, 3.5);
+   B0 = new TH2F((name+"_B0").c_str(),       (title + ", B0 " + etatext + phitext).c_str(),13,-6.5,6.5,22,-0.5,21.5);
+   B1 = new TH2F((name+"_B1").c_str(),       (title + ", B1 " + etatext + phitext).c_str(),13,-6.5,6.5,38,-0.5,37.5);
+   B2 = new TH2F((name+"_B2").c_str(),       (title + ", B2 " + etatext + phitext).c_str(),13,-6.5,6.5,52,-0.5,51.5);
+   A  = new TH2F((name+"_ECA" ).c_str(),     (title + ", ECA " + disktext + phitext).c_str(),3,-0.5,2.5,48,-0.5,47.5);
+   C  = new TH2F((name+"_ECC" ).c_str(),     (title + ", ECC " + disktext + phitext).c_str(),3,-0.5,2.5,48,-0.5,47.5);
+   if (true) {
+      DBMA = new TH2F((name+"_DBMA" ).c_str(),  (title + ", DBMA " + disktext + phitext).c_str(),3,-0.5,2.5,4,-0.5,3.5);
+      DBMC = new TH2F((name+"_DBMC" ).c_str(),  (title + ", DBMC " + disktext + phitext).c_str(),3,-0.5,2.5,4,-0.5,3.5);
+   }
 
    formatHist();
 }
@@ -33,17 +40,19 @@ PixelMon2DMaps::PixelMon2DMaps(std::string name, std::string title, bool doIBL) 
 PixelMon2DMaps::~PixelMon2DMaps()
 {
    if (m_doIBL) {
-      delete IBL3D;
-      delete IBL2D;
       delete IBL;
+      delete IBL2D;
+      delete IBL3D;
    }
    delete B0;
    delete B1;
    delete B2;
    delete A;
    delete C;
-   delete DBMA;
-   delete DBMC;
+   if (true) {
+      delete DBMA;
+      delete DBMC;
+   }
 }
 
 void PixelMon2DMaps::Fill(Identifier &id, const PixelID* pixID)
@@ -52,34 +61,34 @@ void PixelMon2DMaps::Fill(Identifier &id, const PixelID* pixID)
    const int pm  = pixID->phi_module(id);
    int ld = pixID->layer_disk(id);
 
-   if (bec == 2) A->Fill(ld, pm);
+   if (bec == 2) A->Fill(ld, pm); 
    else if (bec == -2) C->Fill(ld, pm);
    else if (bec == 4) DBMA->Fill(ld, pm);
    else if (bec == -4) DBMC->Fill(ld, pm);
    else if (bec == 0) {
       if (m_doIBL) ld--;
-      const int em  = pixID->eta_module(id);
+      const int em = pixID->eta_module(id);
       if (ld == 0) {
-         B0->Fill(em, pm);
+	 B0->Fill(em, pm);
       } else if (ld == 1) {
-         B1->Fill(em, pm);
+	 B1->Fill(em,pm);
       } else if (ld == 2) {
-         B2->Fill(em, pm);
+	 B2->Fill(em,pm);
       } else if (ld == -1) {
-         int feid = 0;
-         int emf = 0;
-         if (em < 6 && em > -7) {
+	 int feid = 0;
+	 int emf = 0;
+	 if (em < 6 && em > -7) {
             if (pixID->eta_index(id) >= 80) feid = 1;
             emf = 2 * em + feid; 
             IBL2D->Fill(em, pm);
-         } else if (em < -6) {
+	 } else if (em < -6) {
             emf = em - 6;
-            IBL3D->Fill(em + 10, pm);
-         } else {
+            IBL3D->Fill(em + 10, pm); 
+	 } else {
             emf = em + 6;
-            IBL3D->Fill(em - 2, pm);
-         }
-         IBL->Fill(emf, pm);
+            IBL3D->Fill(em - 2,pm); 
+	 }
+	 IBL->Fill(emf, pm);
       }
    }
 }
@@ -94,10 +103,11 @@ void PixelMon2DMaps::WeightingFill(Identifier &id, const PixelID* pixID, float w
    else if (bec == -2) C->Fill(ld, pm, weight);
    else if (bec == 4) DBMA->Fill(ld, pm, weight);
    else if (bec == -4) DBMC->Fill(ld, pm, weight);
+
    else if (bec == 0) {
       if (m_doIBL) ld--;
-      int em  = pixID->eta_module(id);
-      if (ld == 0) {
+      const int em  = pixID->eta_module(id);
+      if (ld == 0) { 
          B0->Fill(em, pm, weight);
       } else if (ld == 1) { 
          B1->Fill(em, pm, weight);
@@ -201,8 +211,8 @@ void PixelMon2DMaps::Fill2DMon(PixelMon2DMaps* oldmap)
 void PixelMon2DMaps::formatHist()
 {
    const int ndisk = 3;
-   const int nphi_dbm  = 4;
    const int nphi  = 48;
+   const int nphi_dbm  = 4;
    const char *disk[ndisk] = { "Disk 1", "Disk 2", "Disk 3" };
    const char *phi_dbm[nphi_dbm] = { "M1","M2","M3","M4"};
    const int nmod = 13;
@@ -220,7 +230,6 @@ void PixelMon2DMaps::formatHist()
 				  "A3","","A4","",
 				  "A5","","A6","",
 				  "A7","","A8",""};
-
    const int nstaveb = 14;
    const char *staveb[nstaveb] = {
       "S01", "S02", "S03", "S04", "S05", "S06","S07",
@@ -249,8 +258,8 @@ void PixelMon2DMaps::formatHist()
       "B13_S2", "B14_S1", "B14_S2", "B15_S1", "B15_S2", "B16_S1",
       "B16_S2", "B17_S1", "B17_S2", "B18_S1", "B18_S2", "B19_S1",
       "B19_S2", "B20_S1", "B20_S2", "B21_S1", "B21_S2", "B22_S1",
-      "B22_S2", "B23_S1", "B23_S2", "B24_S1", "B24_S2",
-      "B25_S1", "B25_S2", "B26_S1", "B26_S2", "B01_S1" };
+      "B22_S2", "B23_S1", "B23_S2", "B24_S1", "B24_S2", "B25_S1",
+      "B25_S2", "B26_S1", "B26_S2", "B01_S1" };
    const char *nstaveA[nphi] = {
       "B01_S2_M1", "B01_S2_M6", "B01_S2_M2", "B01_S2_M5", "B01_S2_M3", "B01_S2_M4", 
       "B02_S1_M1", "B02_S1_M6", "B02_S1_M2", "B02_S1_M5", "B02_S1_M3", "B02_S1_M4", 
@@ -274,29 +283,44 @@ void PixelMon2DMaps::formatHist()
       A->GetYaxis()->SetBinLabel(i + 1, nstaveA[i]);
       C->GetYaxis()->SetBinLabel(i + 1, nstaveC[i]);
    }
-   for (int i = 0; i < nphi_dbm; i++) {
-      DBMA->GetYaxis()->SetBinLabel(i + 1, phi_dbm[i]);
-      DBMC->GetYaxis()->SetBinLabel(i + 1, phi_dbm[i]);
+   if (true) {
+      for (int i = 0; i < nphi_dbm; i++) {
+         DBMA->GetYaxis()->SetBinLabel(i + 1, phi_dbm[i]);
+         DBMC->GetYaxis()->SetBinLabel(i + 1, phi_dbm[i]);
+      }
+      for (int i = 0; i < ndisk; i++) {
+         DBMA->GetXaxis()->SetBinLabel(i + 1, disk[i]);
+         DBMC->GetXaxis()->SetBinLabel(i + 1, disk[i]);
+      }
    }
    for (int i = 0; i < ndisk; i++) {
       A->GetXaxis()->SetBinLabel(i + 1, disk[i]);
       C->GetXaxis()->SetBinLabel(i + 1, disk[i]);
-      DBMA->GetXaxis()->SetBinLabel(i + 1, disk[i]);
-      DBMC->GetXaxis()->SetBinLabel(i + 1, disk[i]);
    }
    for (int i = 0; i < nmod; i++) {
       B0->GetXaxis()->SetBinLabel(i + 1, mod[i]);  // bin 0 is underflow
       B1->GetXaxis()->SetBinLabel(i + 1, mod[i]);
       B2->GetXaxis()->SetBinLabel(i + 1, mod[i]);
    }
-
    if (m_doIBL) {
-      for (int i = 0; i < nmodIBL; i++) IBL->GetXaxis()->SetBinLabel(i + 1, modIBL[i]);
-      for (int i = 0; i < nstaveb; i++) IBL->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
-      for (int i = 0; i < nmodIBL2D; i++) IBL2D->GetXaxis()->SetBinLabel(i + 1, modIBL2D[i]);
-      for (int i = 0; i < nstaveb; i++) IBL2D->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
-      for (int i = 0; i < nmodIBL3D; i++) IBL3D->GetXaxis()->SetBinLabel(i + 1, modIBL3D[i]);
-      for (int i = 0; i < nstaveb; i++) IBL3D->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
+      for (int i = 0; i < nmodIBL; i++) {
+         IBL->GetXaxis()->SetBinLabel(i + 1, modIBL[i]);
+      }
+      for (int i = 0; i < nstaveb; i++) {
+         IBL->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
+      }
+      for (int i = 0; i < nmodIBL2D; i++){
+	 IBL2D->GetXaxis()->SetBinLabel(i + 1, modIBL2D[i]);
+      }
+      for (int i = 0; i < nstaveb; i++) {
+	 IBL2D->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
+      }
+      for (int i = 0; i < nmodIBL3D; i++) {
+	 IBL3D->GetXaxis()->SetBinLabel(i + 1, modIBL3D[i]);
+      }
+      for (int i = 0; i < nstaveb; i++) {
+	 IBL3D->GetYaxis()->SetBinLabel(i + 1, staveb[i]);
+      }
    }
 
    for (int i = 0; i < nstave0; i++) {
@@ -309,88 +333,84 @@ void PixelMon2DMaps::formatHist()
       B2->GetYaxis()->SetBinLabel(i + 1, stave2[i]); 
    }
 
-   //Make the text smaller
    if (m_doIBL) {
+      IBL->GetYaxis()->SetLabelSize(0.03);
       IBL2D->GetYaxis()->SetLabelSize(0.03);
       IBL3D->GetYaxis()->SetLabelSize(0.03);
-      IBL->GetYaxis()->SetLabelSize(0.03);
+      IBL->GetYaxis()->SetTitleOffset(1.35);
+      IBL2D->GetYaxis()->SetTitleOffset(1.35);
+      IBL3D->GetYaxis()->SetTitleOffset(1.35);
+      IBL->SetOption("colz");
+      IBL2D->SetOption("colz");
+      IBL3D->SetOption("colz");
+      IBL->SetMinimum(0.);
+      IBL2D->SetMinimum(0.);
+      IBL3D->SetMinimum(0.);
+      IBL->SetStats(0.);
+      IBL2D->SetStats(0.);
+      IBL3D->SetStats(0.);
    }
+   if (true) {
+      DBMA->GetYaxis()->SetLabelSize(0.02);
+      DBMC->GetYaxis()->SetLabelSize(0.02);
+      DBMA->GetYaxis()->SetTitleOffset(1.35);
+      DBMC->GetYaxis()->SetTitleOffset(1.35);
+      DBMA->SetOption("colz");
+      DBMC->SetOption("colz");
+      DBMA->SetMinimum(0.);
+      DBMC->SetMinimum(0.);
+      DBMA->SetStats(0.);
+      DBMC->SetStats(0.);
+   }
+
+   //Make the text smaller
    B0->GetYaxis()->SetLabelSize(0.03);
    B1->GetYaxis()->SetLabelSize(0.03);
    B2->GetYaxis()->SetLabelSize(0.03);
    A->GetYaxis()->SetLabelSize(0.02);
    C->GetYaxis()->SetLabelSize(0.02);
-   DBMA->GetYaxis()->SetLabelSize(0.02);
-   DBMC->GetYaxis()->SetLabelSize(0.02);
-   //Move the lable so you can read it
-   if (m_doIBL) {
-      IBL2D->GetYaxis()->SetTitleOffset(1.35);
-      IBL3D->GetYaxis()->SetTitleOffset(1.35);
-      IBL->GetYaxis()->SetTitleOffset(1.35);
-   }
-   B0->GetYaxis()->SetTitleOffset(1.35);
-   B1->GetYaxis()->SetTitleOffset(1.35);
-   B2->GetYaxis()->SetTitleOffset(1.35);
-   A->GetYaxis()->SetTitleOffset(1.35);
-   C->GetYaxis()->SetTitleOffset(1.35);
-   DBMA->GetYaxis()->SetTitleOffset(1.35);
-   DBMC->GetYaxis()->SetTitleOffset(1.35);
    //put histograms in the easier to read colz format
-   if (m_doIBL) {
-      IBL2D->SetOption("colz");
-      IBL3D->SetOption("colz");
-      IBL->SetOption("colz");
-   }
    B0->SetOption("colz");
    B1->SetOption("colz");
    B2->SetOption("colz");
    A->SetOption("colz");
    C->SetOption("colz");
-   DBMA->SetOption("colz");
-   DBMC->SetOption("colz");
    //force the minimum to be 0 so you can spot empty blocks easily
-   if (m_doIBL) {
-      IBL2D->SetMinimum(0.);
-      IBL3D->SetMinimum(0.);
-      IBL->SetMinimum(0.);
-   }
    B0->SetMinimum(0.);
    B1->SetMinimum(0.);
    B2->SetMinimum(0.);
    A->SetMinimum(0.);
    C->SetMinimum(0.);
-   DBMA->SetMinimum(0.);
-   DBMC->SetMinimum(0.);
-   //Remvoe the stats box because it's in the way
-   if (m_doIBL) {
-      IBL2D->SetStats(0.);
-      IBL3D->SetStats(0.);
-      IBL->SetStats(0.);
-   }
+   B0->GetYaxis()->SetTitleOffset(1.35);
+   B1->GetYaxis()->SetTitleOffset(1.35);
+   B2->GetYaxis()->SetTitleOffset(1.35);
+   A->GetYaxis()->SetTitleOffset(1.35);
+   C->GetYaxis()->SetTitleOffset(1.35);
    B0->SetStats(0.);
    B1->SetStats(0.);
    B2->SetStats(0.);
    A->SetStats(0.);
    C->SetStats(0.);
-   DBMA->SetStats(0.);
-   DBMC->SetStats(0.);
-
 }
 
 StatusCode PixelMon2DMaps::regHist(ManagedMonitorToolBase::MonGroup &group)
 {
-  StatusCode sc = StatusCode::SUCCESS;
-  if (m_doIBL) {
-     if (group.regHist(IBL2D).isFailure()) sc = StatusCode::FAILURE;
-     if (group.regHist(IBL3D).isFailure()) sc = StatusCode::FAILURE;
-     if (group.regHist(IBL).isFailure()) sc = StatusCode::FAILURE;
-  }
-  if (group.regHist(B0).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(B1).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(B2).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(A).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(C).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(DBMA).isFailure()) sc = StatusCode::FAILURE;
-  if (group.regHist(DBMC).isFailure()) sc = StatusCode::FAILURE;
-  return sc;
+   StatusCode sc = StatusCode::SUCCESS;
+
+   if(m_doIBL) {
+      if (group.regHist(IBL).isFailure()) sc = StatusCode::FAILURE;
+      if (group.regHist(IBL2D).isFailure()) sc = StatusCode::FAILURE;
+      if (group.regHist(IBL3D).isFailure()) sc = StatusCode::FAILURE;
+   }
+   if (group.regHist(B0).isFailure()) sc = StatusCode::FAILURE;
+   if (group.regHist(B1).isFailure()) sc = StatusCode::FAILURE;
+   if (group.regHist(B2).isFailure()) sc = StatusCode::FAILURE;
+   if (group.regHist(A).isFailure()) sc = StatusCode::FAILURE;
+   if (group.regHist(C).isFailure()) sc = StatusCode::FAILURE;
+   if (true) {
+      if (group.regHist(DBMA).isFailure()) sc = StatusCode::FAILURE;
+      if (group.regHist(DBMC).isFailure()) sc = StatusCode::FAILURE;
+   }
+
+   return sc;
 }
