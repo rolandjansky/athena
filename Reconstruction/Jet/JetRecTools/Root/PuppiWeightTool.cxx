65991998b640fcd3c75e0f25b34c39b1c51e2f2a
John Stupak <john.stupak@cern.ch>
2017-07-09 20:45:01 +0200

    calculate NPV and cut appropriately
    Former-commit-id: 28f308970e3bab7e7780667b689b94ed68a37269

--- Reconstruction/Jet/JetRecTools/Root/PuppiWeightTool.cxx
+++ Reconstruction/Jet/JetRecTools/Root/PuppiWeightTool.cxx
@@ -96,81 +96,97 @@ StatusCode PuppiWeightTool::process(xAOD::PFOContainer* cont) const{
       const xAOD::VertexContainer* pvtxs = 0;
       ATH_CHECK(evtStore()->retrieve(pvtxs, "PrimaryVertices"));
       if ( pvtxs == 0 || pvtxs->size()==0 ) {
-  ATH_MSG_WARNING(" This event has no primary vertices " );
-  return StatusCode::FAILURE;
+	ATH_MSG_WARNING(" This event has no primary vertices " );
+	return StatusCode::FAILURE;
       }
 
       //Usually the 0th vertex is the primary one, but this is not always the case. So we will choose the first vertex of type PriVtx
-    for (auto theVertex : *pvtxs) {
-  if (xAOD::VxType::PriVtx == theVertex->vertexType() ) {
-    vtx = theVertex;
-    break;
-  }//If we have a vertex of type primary vertex
+      for (auto theVertex : *pvtxs) {
+	if (xAOD::VxType::PriVtx == theVertex->vertexType() ) {
+	  vtx = theVertex;
+	  break;
+	}//If we have a vertex of type primary vertex
       }//iterate over the vertices and check their type
 
       //If there is no primary vertex, then we cannot correct pflow inputs and hence we return (because this should not happen).
       if (nullptr == vtx) {
-  ATH_MSG_VERBOSE("Could not find a primary vertex in this event " );
-  for (auto theVertex : *pvtxs) {
-    if (xAOD::VxType::NoVtx == theVertex->vertexType() ) {
-      vtx = theVertex;
-      break;
-    }
-  }
-  if (nullptr == vtx) {
-    ATH_MSG_WARNING("Could not find a NoVtx in this event " );
-    return StatusCode::FAILURE;
-  }
+	ATH_MSG_VERBOSE("Could not find a primary vertex in this event " );
+	for (auto theVertex : *pvtxs) {
+	  if (xAOD::VxType::NoVtx == theVertex->vertexType() ) {
+	    vtx = theVertex;
+	    break;
+	  }
+	}
+	if (nullptr == vtx) {
+	  ATH_MSG_WARNING("Could not find a NoVtx in this event " );
+	  return StatusCode::FAILURE;
+	}
       }
 
       const xAOD::TrackParticleContainer *tracks=0;
       evtStore()->retrieve(tracks, "InDetTrackParticles");
       for (auto track: *tracks){
 
-  bool matchedToPrimaryVertex = false;
+	bool matchedToPrimaryVertex = false;
 
-  //vtz.z() provides z of that vertex w.r.t the center of the beamspot (z = 0). Thus we corrext the track z0 to be w.r.t z = 0
-  float z0 = track->z0() + track->vz() - vtx->z();
-  float theta = track->theta();
-  if ( fabs(z0*sin(theta)) < 2.0 ) {
-    matchedToPrimaryVertex = true;
-  }
-  if( track->pt()<800){  //Not sure what the threshold should be
-    if(matchedToPrimaryVertex) chargedHSVector.push_back( fastjet::PseudoJet( track->p4() ));
-    else chargedPUVector.push_back( fastjet::PseudoJet( track->p4() ));
-  }
+	//vtz.z() provides z of that vertex w.r.t the center of the beamspot (z = 0). Thus we corrext the track z0 to be w.r.t z = 0
+	float z0 = track->z0() + track->vz() - vtx->z();
+	float theta = track->theta();
+	if ( fabs(z0*sin(theta)) < 2.0 ) {
+	  matchedToPrimaryVertex = true;
+	}
+	if( track->pt()<800){  //Not sure what the threshold should be
+	  if(matchedToPrimaryVertex) chargedHSVector.push_back( fastjet::PseudoJet( track->p4() ));
+	  else chargedPUVector.push_back( fastjet::PseudoJet( track->p4() ));
+	}
       }
     }
-}
-
-    Puppi* puppi = new Puppi(m_R0, m_Rmin, m_beta, m_centralPTCutOffset, m_centralPTCutSlope, m_forwardPTCutOffset, m_forwardPTCutSlope,m_etaBoundary, m_PUPenalty);
-    puppi->setParticles(chargedHSVector, chargedPUVector, neutralVector, forwardVector, 0);
-
-    for ( xAOD::PFO* ppfo : *cont ) {
-      fastjet::PseudoJet pj(ppfo->p4());
-
-      double weight = puppi->getWeight(pj);
-      double alpha = puppi->getAlpha(pj);
-
-      if (ppfo->charge() == 0 && m_applyWeight) ppfo->setP4(weight*ppfo->p4());
-      alphaAcc(*ppfo) = alpha;
-      weightAcc(*ppfo) = weight;
-    }
-
-    /*
-    if(m_limitRemoval){
-      xAOD:PFOContainer neutralAndForward;
-      for ( xAOD::PFO* ppfo : *cont ) {
-    }
-    */
-
-    //if(evtStore()->record( puppi, "puppi" ).isFailure()) ATH_MSG_WARNING("Couldnt store puppi");
-    //evtStore()->record(puppi->getMedian(0),"PUPPI_alpha_median");
-    //evtStore()->record(puppi->getRMS(0),"PUPPI_alpha_RMS");
-
-    //std::cout<<"median: "<<puppi->getMedian()<<std::endl;
-    //std::cout<<"RMS: "<<puppi->getRMS()<<std::endl;
-
-    //ATH_MSG_INFO("Leaving execute");
-    return StatusCode::SUCCESS;
   }
+
+  const xAOD::VertexContainer* pvtxs = 0;
+  ATH_CHECK(evtStore()->retrieve(pvtxs, "PrimaryVertices"));
+  if ( pvtxs == 0 || pvtxs->size()==0 ) {
+    ATH_MSG_WARNING(" This event has no primary vertices " );
+    return StatusCode::FAILURE;
+  }
+
+  int NPV=0;
+  for( auto vtx_itr : *pvtxs ){
+    if((int)vtx_itr->nTrackParticles() < 2 ) { continue; }
+    NPV++;
+  }
+
+  Puppi* puppi = new Puppi(m_R0, m_Rmin, m_beta, m_centralPTCutOffset, m_centralPTCutSlope, m_forwardPTCutOffset, m_forwardPTCutSlope,m_etaBoundary, m_PUPenalty);
+  puppi->setParticles(chargedHSVector, chargedPUVector, neutralVector, forwardVector, NPV);
+
+  for ( xAOD::PFO* ppfo : *cont ) {
+    fastjet::PseudoJet pj(ppfo->p4());
+
+    double weight = puppi->getWeight(pj);
+    double alpha = puppi->getAlpha(pj);
+
+    if (ppfo->charge() == 0 && m_applyWeight) ppfo->setP4(weight*ppfo->p4());
+    alphaAcc(*ppfo) = alpha;
+    weightAcc(*ppfo) = weight;
+  }
+
+  /*
+    if(m_limitRemoval){
+    xAOD:PFOContainer neutralAndForward;
+    for ( xAOD::PFO* ppfo : *cont ) {
+    }
+  */
+
+  //if(evtStore()->record( puppi, "puppi" ).isFailure()) ATH_MSG_WARNING("Couldnt store puppi");
+  //evtStore()->record(puppi->getMedian(0),"PUPPI_alpha_median");
+  //evtStore()->record(puppi->getRMS(0),"PUPPI_alpha_RMS");
+
+  //std::cout<<"median: "<<puppi->getMedian()<<std::endl;
+  //std::cout<<"RMS: "<<puppi->getRMS()<<std::endl;
+
+  //ATH_MSG_INFO("Leaving execute");
+
+  delete puppi;
+
+  return StatusCode::SUCCESS;
+}
