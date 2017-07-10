3305a106739c54d1e3cb9ccb3f68413bee7f19fd
John Chapman <jchapman@cern.ch>
2017-07-10 10:41:31 +0200

    Remove obsolete G4AtlasUtilities package
    The classes in the `G4AtlasUtilities` package are now all obsolete.
    The replacements are in the `ISF_HepMC_Tools` package.
    
    
    Former-commit-id: ebcd05a4207a276c92e9dcdbe4ca1e588be9ed18

--- Simulation/G4Atlas/G4AtlasUtilities/G4AtlasUtilities/G4AtlasUtilitiesDict.h
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "FadsKinematics/ManipulatorBase.h"
-#include "FadsKinematics/VertexManipulator.h"
-#include "FadsKinematics/ParticleManipulator.h"
-#include "G4AtlasUtilities/EtaPhiFilters.h"
-#include "G4AtlasUtilities/VertexRangeChecker.h"
