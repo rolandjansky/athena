8a2abea3fd19c2bb466a4359336edd293b4fb2a4
John Chapman <jchapman@cern.ch>
2017-07-10 11:03:53 +0200

    Remove all usage of the FadsPackageLoader package from G4AtlasControl
    Removed the obsolete `load` method of `SimControl`. Part of the changes
    for ATLASSIM-2768.
    
    
    Former-commit-id: dcc56234a26100a0a6f564f8144f82140727b562

--- Simulation/G4Atlas/G4AtlasControl/src/SimControl.cxx
+++ Simulation/G4Atlas/G4AtlasControl/src/SimControl.cxx
@@ -7,9 +7,6 @@
 // Geant4 includes
 #include "G4RunManager.hh"
 
-// FADS includes
-#include "FadsPackageLoader/PackageLoader.h"
-
 // STL includes
 #include <stdexcept>
 
@@ -19,11 +16,6 @@ SimControl::SimControl()
 
 SimControl::~SimControl()
 {
-}
-
-void SimControl::load(const std::string& lib) const
-{
-  FADS::PackageLoader a(lib.c_str());
 }
 
 void SimControl::initializeG4(bool isMT) const
