509448c8393202604aad921706ed226aaa4a6199
Heather Russell <heather.russell@cern.ch>
2017-07-11 17:41:32 +0200

    ATR-16673, Making IDprescaledL1 stream lb-aware
    Former-commit-id: ac12c972906e581c177580e9dc1ee1c228c88452

--- Trigger/TriggerCommon/TriggerMenu/python/menu/StreamInfo.py
+++ Trigger/TriggerCommon/TriggerMenu/python/menu/StreamInfo.py
@@ -96,7 +96,7 @@ def getStreamTag(streams):
         if stream in physicsStreams:
             streamTags += [(stream, 'physics', 'yes', '1')]
         elif stream in calibStreams:
-            if 'BeamSpot' in stream or 'Background' in stream or "IDTracks" in stream or 'VdM' in stream or 'PixelBeam' in stream or 'BphysPEB' in stream:
+            if 'BeamSpot' in stream or 'Background' in stream or "IDTracks" in stream or 'VdM' in stream or 'PixelBeam' in stream or 'BphysPEB' in stream or 'IDprescaledL1' in stream:
                streamTags += [(stream, 'calibration', 'yes', '1')]
             else: 
                streamTags += [(stream, 'calibration', 'no', '1')]
