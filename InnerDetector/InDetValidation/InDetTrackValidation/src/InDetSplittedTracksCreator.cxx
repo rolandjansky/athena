af51f2936d0875dd065f27c0d707c0b4619be515
Goetz Gaycken <goetz.gaycken@cern.ch>
2017-07-13 20:16:20 +0200

    Use DataHandles for all data access and made algorithm reentrant.
    Former-commit-id: 3de8143aea5aa759a4c65d06efc6fa573782fa0e

--- InnerDetector/InDetValidation/InDetTrackValidation/src/InDetSplittedTracksCreator.cxx
+++ InnerDetector/InDetValidation/InDetTrackValidation/src/InDetSplittedTracksCreator.cxx
@@ -13,7 +13,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 InDet::InDetSplittedTracksCreator::InDetSplittedTracksCreator(const std::string& name, ISvcLocator* pSvcLocator) :
-AthAlgorithm(name, pSvcLocator),
+AthReentrantAlgorithm(name, pSvcLocator),
 // m_sgSvc(0),
 m_TrackCol("Tracks"),
 m_trackSplitterTool()
@@ -42,30 +42,38 @@ StatusCode InDet::InDetSplittedTracksCreator::initialize(){
    } else{ 
       ATH_MSG_VERBOSE ("Retrieved tool InDetTrackSplitterTool");
    }
-
+   ATH_CHECK( m_TrackCol.initialize() );
+   ATH_CHECK( m_OutputTrackCol.initialize() );
    return StatusCode::SUCCESS;
 }   
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-StatusCode InDet::InDetSplittedTracksCreator::execute() {
+StatusCode InDet::InDetSplittedTracksCreator::execute_r(const EventContext& ctx) const {
    ATH_MSG_DEBUG("InDetSplittedTracksCreator execute()");
    ATH_MSG_DEBUG("Input Track Collection: " << m_TrackCol);
 
    //return if no input collection is found
-   const TrackCollection* trks = 0;
-   if ((not evtStore()->contains<TrackCollection>(m_TrackCol)) or
-        (evtStore()->retrieve(trks,m_TrackCol)).isFailure()){
-      ATH_MSG_ERROR(" Could not retrieve the track collection " <<m_TrackCol);
-      return StatusCode::SUCCESS;
+   SG::ReadHandle<TrackCollection> trks(m_TrackCol,ctx);
+   if (!trks.isValid()) {
+       ATH_MSG_ERROR(" Could not retrieve the track collection " <<m_TrackCol.key() );
+       return StatusCode::SUCCESS;
    }
 
-   TrackCollection* splittedTracks=0;
-   if ((not evtStore()->contains<TrackCollection>(m_OutputTrackCol)) or
-       (evtStore()->retrieve(splittedTracks,m_OutputTrackCol)).isFailure()) {
-     splittedTracks = new TrackCollection;
-   } else {
-     ATH_MSG_WARNING("Output split track collection already exists: " << m_OutputTrackCol << ". Quitting.");
-     return StatusCode::SUCCESS;
-   }
+   // @In athenaMT the algorithm should not be scheduled if the splitted tracks exist already or they are overwritten
+   // TrackCollection* splittedTracks=0;
+   // if ((not evtStore()->contains<TrackCollection>(m_OutputTrackCol)) or
+   //     (evtStore()->retrieve(splittedTracks,m_OutputTrackCol)).isFailure()) {
+   //   splittedTracks = new TrackCollection;
+   // } else {
+   //   ATH_MSG_WARNING("Output split track collection already exists: " << m_OutputTrackCol.key() << ". Quitting.");
+   //   return StatusCode::SUCCESS;
+   // }
+
+    SG::WriteHandle<TrackCollection> splittedTracks(m_OutputTrackCol,ctx);
+    if (splittedTracks.record( std::make_unique<TrackCollection>() ).isFailure()){
+      ATH_MSG_ERROR("Failed to record output collection " << m_OutputTrackCol.key());
+      return StatusCode::FAILURE;
+    }
+
 
    //loop over tracks and split them in upper and lower part
    TrackCollection::const_iterator it = trks->begin();
@@ -73,12 +81,7 @@ StatusCode InDet::InDetSplittedTracksCreator::execute() {
    for (; it != itE; ++it){
      ATH_MSG_VERBOSE("Splitting Track");
      std::pair<Trk::Track*, Trk::Track*> splitTracks =  std::pair<Trk::Track*, Trk::Track*> (0,0);
-     if (m_makeSiOnlyTracks){
-       splitTracks = m_trackSplitterTool->splitInUpperLowerTrack(**it,true); //use Si only hits
-     }
-     else {
-       splitTracks = m_trackSplitterTool->splitInUpperLowerTrack(**it);
-     }
+     splitTracks = m_trackSplitterTool->splitInUpperLowerTrack(**it,m_makeSiOnlyTracks);
      //only save them if both track fits succeeded       
      if(splitTracks.first && splitTracks.second){
        if (m_takeUpperSegment) {
@@ -94,51 +97,13 @@ StatusCode InDet::InDetSplittedTracksCreator::execute() {
      }
        
    }
-   ATH_MSG_DEBUG("** InDetSplittedTracksCreator ** SplittedTracks->size() " << splittedTracks->size() << "  stored in " << m_OutputTrackCol );
+   ATH_MSG_DEBUG("** InDetSplittedTracksCreator ** SplittedTracks->size() " << splittedTracks->size() << "  stored in " << m_OutputTrackCol.key() );
    
-   if(evtStore()->record(splittedTracks, m_OutputTrackCol,false).isFailure() ){
-     ATH_MSG_ERROR("Could not save the "<<m_OutputTrackCol);
-   }
 
    return StatusCode::SUCCESS;
 }
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 StatusCode InDet::InDetSplittedTracksCreator::finalize() {
-    ATH_MSG_INFO("InDetSplittedTracksCreator finalize()");
+    ATH_MSG_VERBOSE("InDetSplittedTracksCreator finalize()");
     return StatusCode::SUCCESS;
-}
-
-void InDet::InDetSplittedTracksCreator::copyTrackCollection(const TrackCollection* inputTrackCol,  TrackCollection* outputTrackCol) { 
-  if(inputTrackCol && inputTrackCol->size()) {
-    ATH_MSG_VERBOSE("Size of track collection " << inputTrackCol->size());
-    // loop over tracks
-    TrackCollection::const_iterator rf,rfe=inputTrackCol->end();
-    for(rf=inputTrackCol->begin();rf!=rfe; ++rf){
-      // add track into output
-      Trk::Track* newTrack = new Trk::Track(**rf);
-      outputTrackCol->push_back(newTrack);
-    }
-  }
-}
-
-void InDet::InDetSplittedTracksCreator::copyTrackCollection(const TrackCollection* inputTrackCol_upper,const TrackCollection* inputTrackCol_lower,   TrackCollection* outputTrackCol) { 
-  if(inputTrackCol_upper && inputTrackCol_upper->size() && inputTrackCol_lower && inputTrackCol_lower->size() &&  inputTrackCol_upper->size() == inputTrackCol_lower->size()) {
-    ATH_MSG_VERBOSE ("Size of track collection upper" << inputTrackCol_upper->size());
-    ATH_MSG_VERBOSE ("Size of track collection lower" << inputTrackCol_lower->size());
-    
-    // loop over tracks
-    TrackCollection::const_iterator rfU,rfeU=inputTrackCol_upper->end();
-    TrackCollection::const_iterator rfL;//,rfeL=inputTrackCol_lower->end();
-    
-    for(rfU=inputTrackCol_upper->begin(),rfL=inputTrackCol_lower->begin();rfU!=rfeU; ++rfU,++rfL){
-      
-//     for(rfL=inputTrackCol_lower->begin();rfL!=rfeL; ++rfL){
-      // add track into output
-      Trk::Track* newTrack_U = new Trk::Track(**rfU);
-      Trk::Track* newTrack_L = new Trk::Track(**rfL);
-      outputTrackCol->push_back(newTrack_U);
-      outputTrackCol->push_back(newTrack_L);
-      
-    }
-  }
 }
