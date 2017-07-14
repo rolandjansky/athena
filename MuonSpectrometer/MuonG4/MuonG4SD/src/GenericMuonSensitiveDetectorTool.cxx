faa222057ab6460e1d6ee506f412ee1a4c5df25b
John Chapman <jchapman@cern.ch>
2017-07-14 14:45:24 +0200

    Suppress INFO level messages in Muon SD Tools
    Former-commit-id: 7003768cb2752548869e5a11a80c5b3f08881007

--- MuonSpectrometer/MuonG4/MuonG4SD/src/GenericMuonSensitiveDetectorTool.cxx
+++ MuonSpectrometer/MuonG4/MuonG4SD/src/GenericMuonSensitiveDetectorTool.cxx
@@ -13,6 +13,6 @@ GenericMuonSensitiveDetectorTool::GenericMuonSensitiveDetectorTool(const std::st
 
 G4VSensitiveDetector* GenericMuonSensitiveDetectorTool::makeSD()
 {
-  ATH_MSG_INFO( "Initializing SD" );
+  ATH_MSG_DEBUG( "Initializing SD" );
   return new GenericMuonSensitiveDetector(name(), m_outputCollectionNames[0]);
 }
