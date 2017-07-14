a832257da6d1870e8b80282ef67d4831f2fd72a6
Walter Lampl <Walter.Lampl@cern.ch>
2017-07-14 16:05:11 +0200

    first version of conditions algo for LArBadChannels
    Former-commit-id: c4f522d25510140c7e4261fffa90daa24d5fae18

--- LArCalorimeter/LArBadChannelTool/src/components/LArBadChannelTool_entries.cxx
+++ LArCalorimeter/LArBadChannelTool/src/components/LArBadChannelTool_entries.cxx
@@ -8,6 +8,7 @@
 #include "LArBadChannelTool/LArBadChannelTimingAlg.h"
 #include "LArBadChannelTool/LArBadChannel2Ascii.h"
 #include "LArBadChannelTool/LArBadFeb2Ascii.h"
+#include "LArBadChannelTool/LArBadChannelCondAlg.h"
 
 DECLARE_TOOL_FACTORY( LArBadChannelMasker )
 DECLARE_TOOL_FACTORY( LArBadChanTool )
@@ -16,14 +17,6 @@ DECLARE_ALGORITHM_FACTORY( LArBadChannelDBAlg )
 DECLARE_ALGORITHM_FACTORY( LArBadChannelTimingAlg )
 DECLARE_ALGORITHM_FACTORY( LArBadChannel2Ascii )
 DECLARE_ALGORITHM_FACTORY( LArBadFeb2Ascii )
+DECLARE_ALGORITHM_FACTORY( LArBadChannelCondAlg )
 
-DECLARE_FACTORY_ENTRIES(LArBadChannelTool) {
-   DECLARE_TOOL( LArBadChannelMasker )
-   DECLARE_TOOL( LArBadChanTool )
-   DECLARE_ALGORITHM( LArBadChannelToolTest )
-   DECLARE_ALGORITHM( LArBadChannelDBAlg )
-   DECLARE_ALGORITHM( LArBadChannelTimingAlg )
-   DECLARE_ALGORITHM( LArBadChannel2Ascii )
-   DECLARE_ALGORITHM( LArBadFeb2Ascii )
-}
  
