73cc585629b23fb26e529132a93b0170c55e21f5
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-07-14 18:11:16 +0000

    Merge branch 'art-14may' into '21.0'
    move rucio setup from TCT scripts to art-task-grid.sh
    
    See merge request !3377
    
    Former-commit-id: 71c062a7195d0b120ec74caba00294fd33e97a58

--- Tools/Tier0ChainTests/test/test_mainstream_run276689_collisions_withouthist.sh
+++ Tools/Tier0ChainTests/test/test_mainstream_run276689_collisions_withouthist.sh
@@ -8,10 +8,6 @@ Reco_tf.py  --AMI=f628 --maxEvents=375 --outputESDFile=myESD_Main_2.pool.root --
 Reco_tf.py --autoConfiguration=everything  --inputESDFile=myESD_Main_2.pool.root --outputTAGFile=myTAG_Main_3.root
 
 
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-setupATLAS --quiet
-lsetup rucio
 SCRIPT_DIRECTORY=$1
 PACKAGE=$2
 TYPE=$3
