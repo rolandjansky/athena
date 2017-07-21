8704a89811c4154949cb63af0618baf5f5d67f84
John Chapman <jchapman@cern.ch>
2017-07-21 14:07:43 +0200

    The DetectorGeometrySvc configuration should be tweaked as a postInclude
    Now that `IRegionCreator` tools are private ToolHandles owned by
    `DetectorGeometrySvc` then extra tools should be added after the main
    configuration. For this reason I've renamed the related preInclude files
    to be postIncludes. `preInclude.Cavern.py` and `preInclude.ScoringVolumeKiller.py`
    had to be split into separate pre and post includes. Part of the changes
    for ATLASSIM-3379.
    It would be nice to find a way to decide whether or not to add
    `MuonPhysicsRegionTool` to `DetectorGeometrySvc.RegionCreators` within
    `G4AtlasServicesConfig.py`.
    
    
    Former-commit-id: 667ad53e9cd87b13c19026eecbbac6ad9a046132

--- Simulation/SimulationJobOptions/share/cavern/preInclude.Cavern.py
+++ Simulation/SimulationJobOptions/share/cavern/preInclude.Cavern.py
@@ -12,8 +12,3 @@ simFlags.CalibrationRun.set_Off()
 simFlags.CavernCuts = True
 simFlags.LArParameterization = 0
 
-from AthenaCommon.CfgGetter import getPrivateTool,getService
-getService('DetectorGeometrySvc').RegionCreators += [getPrivateTool('MuonPhysicsRegionTool')]
-#FIXME need to add this to the list of RegionCreators, but
-#currently initializing the DetectorGeometrySvc too early causes
-#problems with the GeoModelSvc configuration.
