4cc83f07c40d7cdcb9b8e992fcbe65205e0551fe
John Alison <johnda102@gmail.com>
2017-07-24 20:29:33 +0200

    First version with data structure
    Former-commit-id: 190b92cb169df6dcecb8948f8afc0d01ccab0663

--- Trigger/TrigHypothesis/TrigBjetHypo/TrigBjetHypo/TrigBjetHypoAllTE.h
+++ Trigger/TrigHypothesis/TrigBjetHypo/TrigBjetHypo/TrigBjetHypoAllTE.h
@@ -32,11 +32,33 @@ class TrigBjetHypoAllTE: public HLT::AllTEAlgo {
 
  private:
 
+  const xAOD::BTagging* getBTaggingPtr(HLT::TriggerElement* btagTE);
+  const xAOD::Jet* getJetPtr(const xAOD::BTagging* btagInfo);
+  bool  eventPassesTrigger();
+  void  clearCounters();
+
+ private:
+
+  struct triggerRequirement{
+    float          m_etThreshold;
+    float          m_btagCut;
+    unsigned int   m_multiplicity;
+
+    unsigned int   m_count;
+    triggerRequirement(float etThreshold, float btagCut, unsigned multiplicity)  : m_etThreshold(etThreshold), m_btagCut(btagCut), m_multiplicity(multiplicity),  m_count(0) 
+    { }
+    
+  };
+  std::vector<triggerRequirement> m_triggerReqs;
+
   /** @brief string corresponding to the trigger level in which the algorithm is running. */
   //  std::string m_instance;
 
   /** @brief Name of the input btagging objects. */
   std::string m_btaggingKey;
+
+  /** @brief Name of the btagging variable. */
+  std::string m_tagger;
 
   /** @brief to check the beam spot flag status. */
   bool m_useBeamSpotFlag;
