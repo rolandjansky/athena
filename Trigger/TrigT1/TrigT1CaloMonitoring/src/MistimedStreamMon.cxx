c4c8135d292d170521befeaf6d662d9acb55d8a6
Julia Djuvsland <jhofmann@cern.ch>
2017-07-25 14:55:39 +0200

    filling jep and cp histos
    Former-commit-id: 3648bd61401e6c8f67cc718e64870b8bce9a9e86

--- Trigger/TrigT1/TrigT1CaloMonitoring/src/MistimedStreamMon.cxx
+++ Trigger/TrigT1/TrigT1CaloMonitoring/src/MistimedStreamMon.cxx
@@ -32,6 +32,8 @@
 
 #include "xAODTrigL1Calo/TriggerTowerContainer.h"
 #include "xAODTrigL1Calo/TriggerTowerAuxContainer.h"
+#include "xAODTrigL1Calo/CPMTowerContainer.h"
+#include "xAODTrigL1Calo/JetElementContainer.h"
 
 #include "AthenaPoolUtilities/AthenaAttributeList.h"
 
@@ -177,29 +179,29 @@ void MistimedStreamMon::bookEventHistograms(std::string number)
 
   name = "em_2d_etaPhi_tt_lut_jep0_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 0 = BCID-1 in the EM layer of selected event number " + number;
-  TH2F_LW *emLUTjep0 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *emLUTjep0 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_em_2d_etaPhi_tt_lut_jep0_mistimedStreamAna.push_back(emLUTjep0);
   name = "had_2d_etaPhi_tt_lut_jep0_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 0 = BCID-1 in the HAD layer of selected event number " + number;
-  TH2F_LW *hadLUTjep0 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *hadLUTjep0 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_had_2d_etaPhi_tt_lut_jep0_mistimedStreamAna.push_back(hadLUTjep0);
   
   name = "em_2d_etaPhi_tt_lut_jep1_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 1 = BCID in the EM layer of selected event number " + number;
-  TH2F_LW *emLUTjep1 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *emLUTjep1 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_em_2d_etaPhi_tt_lut_jep1_mistimedStreamAna.push_back(emLUTjep1);
   name = "had_2d_etaPhi_tt_lut_jep1_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 1 = BCID in the HAD layer of selected event number " + number;
-  TH2F_LW *hadLUTjep1 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *hadLUTjep1 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_had_2d_etaPhi_tt_lut_jep1_mistimedStreamAna.push_back(hadLUTjep1);
 
   name = "em_2d_etaPhi_tt_lut_jep2_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 2 = BCID+1 in the EM layer of selected event number " + number;
-  TH2F_LW *emLUTjep2 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *emLUTjep2 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_em_2d_etaPhi_tt_lut_jep2_mistimedStreamAna.push_back(emLUTjep2);
   name = "had_2d_etaPhi_tt_lut_jep2_mistimedStreamAna_event_" + number;
   title = "#eta - #phi Map of TT LUT JEP in timeslice 2 = BCID+1 in the HAD layer of selected event number " + number;
-  TH2F_LW *hadLUTjep2 = m_histTool->bookPPMEmEtaVsPhi(name, title);
+  TH2F_LW *hadLUTjep2 = m_histTool->bookJEMEtaVsPhi(name, title);
   m_v_had_2d_etaPhi_tt_lut_jep2_mistimedStreamAna.push_back(hadLUTjep2);
   
   return;
@@ -309,6 +311,14 @@ StatusCode MistimedStreamMon::fillHistograms()
   //Object holding the decoration, to optimize access speed
   xAOD::TriggerTower::Decorator<float> myDecoration("pulseClassification");
   
+  //Get CPM Towers for LUT_CP
+  const xAOD::CPMTowerContainer* cpmTowCon = 0;
+  CHECK(evtStore()->retrieve(cpmTowCon,"CPMTowers")); 
+  
+  //Get Jet Elements for LUT-JEP
+  const xAOD::JetElementContainer* jetEleCon = 0;
+  CHECK(evtStore()->retrieve(jetEleCon,"JetElements")); 
+  
   // Retrieve EventInfo from SG and save lumi block number, global event number and run number
   uint32_t bunchCrossing = 0;
   const EventInfo *evInfo = 0;
@@ -357,18 +367,9 @@ StatusCode MistimedStreamMon::fillHistograms()
   for (; TriggerTowerIterator != TriggerTowerIteratorEnd;
        ++TriggerTowerIterator) {
     auto vecIndexTT = std::distance( TriggerTowerTES->begin(), TriggerTowerIterator ) ;
-//     const int layer = (*TriggerTowerIterator)->layer();
-//     const double eta = (*TriggerTowerIterator)->eta();
-//     const double phi = (*TriggerTowerIterator)->phi();
-//     const int cpET = (*TriggerTowerIterator)->cpET();
-//     int jepET = 0;
-//     const std::vector<uint_least8_t> jepETvec = (*TriggerTowerIterator)->lut_jep();
-//     if (jepETvec.size() > 0) {
-//       jepET = (*TriggerTowerIterator)->jepET();
-//     }
     float ttPulseCategory = 0;
     // retrieve max ADC value and position, this seems to be buggy in the DAOD
-    std::cout<<vecIndexTT<< " TT index; lumi block "<<lumiBlock<<std::endl;
+//     std::cout<<vecIndexTT<< " TT index; lumi block "<<lumiBlock<<std::endl;
 //     << " event Number " <<eventNumber<<" run number "<<runNumber 
     auto maxValIterator = std::max_element(TriggerTowerTES->at(vecIndexTT)->adc().begin(), TriggerTowerTES->at(vecIndexTT)->adc().end());
     int maxADCval = *maxValIterator;
@@ -443,48 +444,73 @@ StatusCode MistimedStreamMon::fillHistograms()
   
   m_h_1d_cutFlow_mistimedStreamAna->Fill(8.5);
   m_selectedEventCounter++;
-  std::cout<<" m_selectedEventCounter is "<<m_selectedEventCounter<<std::endl;
-    
-//   for (; TriggerTowerIterator != TriggerTowerIteratorEnd;
-//        ++TriggerTowerIterator) {
-// 
-//     const int layer = (*TriggerTowerIterator)->layer();
-//     const double eta = (*TriggerTowerIterator)->eta();
-//     const double phi = (*TriggerTowerIterator)->phi();
-//     const int cpET = (*TriggerTowerIterator)->cpET();
-//     int jepET = 0;
-//     const std::vector<uint_least8_t> jepETvec = (*TriggerTowerIterator)->lut_jep();
-//     if (jepETvec.size() > 0) {
-//       jepET = (*TriggerTowerIterator)->jepET();
-//     }
-//     // Check if TT is in EM or HAD layer:
-//     if (layer == 0) { //========== ELECTROMAGNETIC LAYER =========================
-//       //---------------------------- ADC HitMaps per timeslice -----------------
-//       unsigned int tslice = (*TriggerTowerIterator)->adcPeak();
-// 
-//       if (tslice < ((*TriggerTowerIterator)->adc()).size()) {
-// //         const int ADC = ((*TriggerTowerIterator)->adc())[tslice];
-// //           m_h_1d_selectedEvents_mistimedStreamAna->Fill(eta,1);
-// //           m_histTool->fillPPMEmEtaVsPhi(m_h_em_2d_etaPhi_tt_classification_mistimedStreamAna, 
-// //                                         eta, phi, 1);
-//       }
-//     }
-// //     if (layer == 1) { //========== HADRONIC LAYER ===============================
-// //       //---------------------------- ADC HitMaps per timeslice -----------------
-// //       unsigned int tslice = (*TriggerTowerIterator)->adcPeak();
-// // 
-// //       if (tslice < ((*TriggerTowerIterator)->adc()).size()) {
-// //         const int ADC = ((*TriggerTowerIterator)->adc())[tslice];
-// //         if (ADC > m_TT_ADC_HitMap_Thresh) {
-// //           m_histTool->fillPPMHadEtaVsPhi(m_h_ppm_had_2d_etaPhi_tt_adc_HitMap,
-// //                                          eta, phi, 1);
-// //         }
-// //       }
-// //     }
-//   }
-  
 
+  //loop over the decorated TTcollection, if less than 10 events are selected for this run
+  if(m_selectedEventCounter < 10){
+    for(auto decoratedIterator = ttContainer->begin() ; decoratedIterator != ttContainer->end() ; ++decoratedIterator ){
 
+        const int layer = (*decoratedIterator)->layer();
+        const double eta = (*decoratedIterator)->eta();
+        const double phi = (*decoratedIterator)->phi();
+        const float pulseCat = (*decoratedIterator)->auxdata<float>("pulseClassification");
+        uint8_t bcidWord = (*decoratedIterator)->bcidVec()[0]; // look at the status bit in the central time slice. Hard coded for now as we do the 5 slice check above
+        
+        // Check if TT is in EM or HAD layer:
+        if (layer == 0) { //========== ELECTROMAGNETIC LAYER =========================
+            m_histTool->fillPPMEmEtaVsPhi(m_v_em_2d_etaPhi_tt_classification_mistimedStreamAna[m_selectedEventCounter], eta, phi, pulseCat);
+            if(pulseCat > 0.5){
+               m_histTool->fillPPMEmEtaVsPhi(m_v_em_2d_etaPhi_tt_pseBits_mistimedStreamAna[m_selectedEventCounter], eta, phi, (unsigned int)bcidWord);
+                
+            }
+
+        }
+        else if(layer == 1) { //========== HADRONIC LAYER ===============================
+            m_histTool->fillPPMEmEtaVsPhi(m_v_had_2d_etaPhi_tt_classification_mistimedStreamAna[m_selectedEventCounter], eta, phi, pulseCat);
+            if(pulseCat > 0.5){
+               m_histTool->fillPPMEmEtaVsPhi(m_v_had_2d_etaPhi_tt_pseBits_mistimedStreamAna[m_selectedEventCounter], eta, phi, (unsigned int)bcidWord);
+            }            
+         }
+     }
+   }
+   
+   //loop over the cpm tower container to fill the lut_cp histos 
+   for(auto thisCT:*cpmTowCon){
+      double eta = thisCT->eta();
+      double phi = thisCT->phi();
+      std::vector<uint8_t> cpmEMenergy = thisCT->emEnergyVec();
+      std::vector<uint8_t> cpmHADenergy = thisCT->hadEnergyVec();
+
+      if(cpmEMenergy.size() > 2){ // expect 3 slicesto be read out
+          m_histTool->fillPPMEmEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_cp0_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmEMenergy.at(0));
+          m_histTool->fillPPMEmEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_cp1_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmEMenergy.at(1));
+          m_histTool->fillPPMEmEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_cp2_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmEMenergy.at(2));
+      }
+      if(cpmHADenergy.size() > 2){
+          m_histTool->fillPPMEmEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_cp0_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmHADenergy.at(0));
+          m_histTool->fillPPMEmEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_cp1_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmHADenergy.at(1));
+          m_histTool->fillPPMEmEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_cp2_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int) cpmHADenergy.at(2));
+      }
+   }
+   
+   // the jet element container
+   for(auto thisJE:*jetEleCon){
+      double eta = thisJE->eta();
+      double phi = thisJE->phi();
+      std::vector<uint16_t> jepEMenergy = thisJE->emJetElementETVec();
+      std::vector<uint16_t> jepHADenergy = thisJE->hadJetElementETVec();
+      if(jepEMenergy.size() > 2){
+          m_histTool->fillJEMEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_jep0_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepEMenergy.at(0));
+          m_histTool->fillJEMEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_jep1_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepEMenergy.at(1));
+          m_histTool->fillJEMEtaVsPhi(m_v_em_2d_etaPhi_tt_lut_jep2_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepEMenergy.at(2));
+
+      }
+      if(jepHADenergy.size()){
+          m_histTool->fillJEMEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_jep0_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepHADenergy.at(0));
+          m_histTool->fillJEMEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_jep1_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepHADenergy.at(1));
+          m_histTool->fillJEMEtaVsPhi(m_v_had_2d_etaPhi_tt_lut_jep2_mistimedStreamAna[m_selectedEventCounter], eta, phi, (int)jepHADenergy.at(2));
+
+      }
+  }
 //   //adding the histos dynamically does not seem to work... too bad!!!
 //   std::stringstream buffer;
 //   m_histTool->setMonGroup(&TT_MistimedMon);
@@ -501,8 +527,12 @@ StatusCode MistimedStreamMon::fillHistograms()
 // //         m_histTool->ppmCrateModule(hist, crate, crate + 1, 0, false);
 //         m_v_em_2d_etaPhi_tt_lut_cp1_mistimedStreamAna.push_back(hist);
 //       }
-//   
+  
   m_h_1d_selectedEvents_mistimedStreamAna->Fill(lumiBlock);
+  
+  //dont forget to delete the decorated TTcontainers
+  delete ttContainer;
+  delete ttAuxContainer;
   
   return StatusCode::SUCCESS;
 }
