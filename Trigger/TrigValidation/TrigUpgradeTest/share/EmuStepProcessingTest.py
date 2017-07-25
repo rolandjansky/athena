10fc1d20ece0d87d2a01212a5b2a455beee433d9
Francesca Pastore <francesca.pastore@cern.ch>
2017-07-25 18:05:04 +0200

    simplified running on 2 single chains
    Former-commit-id: 46cc54d18e00af50639d7070b434308f6203a333

--- Trigger/TrigValidation/TrigUpgradeTest/share/EmuStepProcessingTest.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/EmuStepProcessingTest.py
@@ -90,10 +90,10 @@ eChains   = [ 'HLT_e20' ]
 gChains   = [ 'HLT_g100', 'HLT_2g50' ]
 mueChains = [ 'HLT_mu8_e8' ]
 
-steps = [ parOR("step%i" % i) for i in range(5)]
+steps = [ parOR("step%i" % i) for i in range(2)]
 stepNo = 0
 steps[stepNo] += seqFilter( "Step1MU", Inputs=["L1MU"], Outputs=["step0MU"], Chains=muChains )
-steps[stepNo] += seqFilter( "Step1MU_E", Inputs=["L1MU", "L1EM"], Outputs=["step0MU","step0EM"], Chains=mueChains )
+#steps[stepNo] += seqFilter( "Step1MU_E", Inputs=["L1MU", "L1EM"], Outputs=["step0MU","step0EM"], Chains=mueChains )
 steps[stepNo] += seqFilter( "Step1EM", Inputs=["L1EM"], Outputs=["step0EM"], Chains=(eChains + gChains)  )
 
 emHypo = hypo("Step1ElGamHypo", Input="EMClusters", Output="EMDecisions")
@@ -114,65 +114,63 @@ msMuHypo += msMuHypoTools
 from TrigUpgradeTest.TrigUpgradeTestConf import HLTTest__TestComboHypoAlg
 
 stepNo += 1 
-em1 = stepSeq( "em1", useExisting("Step1EM"), [ reco("EMRoIs", Output="EMRoIs"),
-                                         reco("CaloClustering", FileName="emclusters.dat", Output="EMClusters"),
-                                         emHypo ] )
+em1 = stepSeq( "em1", useExisting("Step1EM"), [ reco("CaloClustering", FileName="emclusters.dat", Output="EMClusters", Input="L1EM"),
+                                                emHypo ] )
 steps[stepNo] += em1
-mu1 = stepSeq("mu1", useExisting("Step1MU"), [ reco("MURoIs", Output="MURoIs"),
-                                        reco("muMSRecAlg", FileName="msmu.dat", Output="MSMuons"),
-                                        msMuHypo ] )
+mu1 = stepSeq("mu1", useExisting("Step1MU"), [  reco("muMSRecAlg", FileName="msmu.dat", Output="MSMuons", Input="L1MU"),
+                                                msMuHypo ] )
 steps[stepNo] += mu1
 
-mue1 = stepSeq("mue1", useExisting("Step1MU_E"), [ useExisting("CaloClustering"), useExisting("muMSRecAlg"),
-                                            HLTTest__TestComboHypoAlg("mueHypo1", OutputLevel=DEBUG, Input1="EMClusters", Input2="MSMuons",
-                                                                      Output1="step1MUEDecisionsEM", Output2="step1MUEDecisionsMU",
-                                                                      Property1="et", Property2="pt", Threshold1=8000, Threshold2=8000,
-                                                                      DecisionLabel="HLT_mu8_e8" )  ])
-steps[stepNo] += mue1
+## mue1 = stepSeq("mue1", useExisting("Step1MU_E"), [ useExisting("CaloClustering"), useExisting("muMSRecAlg"),
+##                                             HLTTest__TestComboHypoAlg("mueHypo1", OutputLevel=DEBUG, Input1="EMClusters", Input2="MSMuons",
+##                                                                       Output1="step1MUEDecisionsEM", Output2="step1MUEDecisionsMU",
+##                                                                       Property1="et", Property2="pt", Threshold1=8000, Threshold2=8000,
+##                                                                       DecisionLabel="HLT_mu8_e8" )  ])
+## steps[stepNo] += mue1
+
+## # STEP2 
+## stepNo += 1
+## steps[stepNo] += seqFilter("Step2MU", Inputs=["step1MUDecisions"], Outputs=["step2MU"], Chains=muChains )
+## steps[stepNo] += seqFilter("Step2MU_E", Inputs=[ "step1MUEDecisionsEM", "step1MUEDecisionsMU"], Outputs=["step2MUE_EM", "step2MUE_MU"], Chains=mueChains  )
+## steps[stepNo] += seqFilter("Step2E", Inputs=["step1EDecisions"], Outputs=["step2E"], Chains=eChains  )
+## steps[stepNo] += seqFilter("Step2G", Inputs=["step1GDecisions"], Outputs=["step2G"], Chains=gChains )
 
 
-stepNo += 1
-steps[stepNo] += seqFilter("Step2MU", Inputs=["step1MUDecisions"], Outputs=["step2MU"], Chains=muChains )
-steps[stepNo] += seqFilter("Step2MU_E", Inputs=[ "step1MUEDecisionsEM", "step1MUEDecisionsMU"], Outputs=["step2MUE_EM", "step2MUE_MU"], Chains=mueChains  )
-steps[stepNo] += seqFilter("Step2E", Inputs=["step1EDecisions"], Outputs=["step2E"], Chains=eChains  )
-steps[stepNo] += seqFilter("Step2G", Inputs=["step1GDecisions"], Outputs=["step2G"], Chains=gChains )
+## muCombHypo = hypo("Step2MuHypo", Input="CombMuons", Output="CombMuonDecisions")
+## muCombHypoTools = [ msMuHTool("HLT_mu6"), msMuHTool("HLT_mu8"),  msMuHTool("HLT_mu10")  ] # this tools are the same as MS ones,
+## muCombHypo.HypoTools = muCombHypoTools
+## muCombHypo += muCombHypoTools
 
 
-muCombHypo = hypo("Step2MuHypo", Input="CombMuons", Output="CombMuonDecisions")
-muCombHypoTools = [ msMuHTool("HLT_mu6"), msMuHTool("HLT_mu8"),  msMuHTool("HLT_mu10")  ] # this tools are the same as MS ones,
-muCombHypo.HypoTools = muCombHypoTools
-muCombHypo += muCombHypoTools
+## eHypo = hypo("Step2ElHypo", Input="Electrons", Output="ElectronDecisions")
+## eHypoTools = [ emHTool("HLT_e2"), emHTool("HLT_e3"), emHTool("HLT_e5") ]
+## eHypo.HypoTools = emHypoTools 
+## eHypo += emHypoTools
 
+## gHypo = hypo("Step2GammHypo", Input="Photons", Output="PhotonDecisions")
+## gHypoTools = [ emHTool("HLT_e2"), emHTool("HLT_e3"), emHTool("HLT_e5") ]
+## gHypo.HypoTools = emHypoTools 
+## gHypo += emHypoTools
 
-eHypo = hypo("Step2ElHypo", Input="Electrons", Output="ElectronDecisions")
-eHypoTools = [ emHTool("HLT_e2"), emHTool("HLT_e3"), emHTool("HLT_e5") ]
-eHypo.HypoTools = emHypoTools 
-eHypo += emHypoTools
+## from TrigUpgradeTest.TrigUpgradeTestConf import HLTTest__TestMerger
+## def merger(name, Inputs, Output ):
+##     m = HLTTest__TestMerger( "M_"+name, Inputs=Inputs, Output=Output )
+##     allAlgs[name] = m
+##     return m
 
-gHypo = hypo("Step2GammHypo", Input="Photons", Output="PhotonDecisions")
-gHypoTools = [ emHTool("HLT_e2"), emHTool("HLT_e3"), emHTool("HLT_e5") ]
-gHypo.HypoTools = emHypoTools 
-gHypo += emHypoTools
+## stepNo += 1
+## mu2 = stepSeq("mu2", useExisting("Step2MU"), [ reco("TrigFastTrackFinder", FileName="tracks.dat", Output="Tracks"),  reco("MuonRecAlg", FileName="mucomb.dat", Output="CombMuons"), muCombHypo ] )
+## steps[stepNo] += mu2
 
-from TrigUpgradeTest.TrigUpgradeTestConf import HLTTest__TestMerger
-def merger(name, Inputs, Output ):
-    m = HLTTest__TestMerger( "M_"+name, Inputs=Inputs, Output=Output )
-    allAlgs[name] = m
-    return m
+## e2 = stepSeq( "e2" , useExisting("Step2E"), [ useExisting("TrigFastTrackFinder"), reco("ElectronRecAlg", FileName="electrons.dat", Output="Electrons"), eHypo ] )
+## steps[stepNo] += e2
 
-stepNo += 1
-mu2 = stepSeq("mu2", useExisting("Step2MU"), [ reco("TrigFastTrackFinder", FileName="tracks.dat", Output="Tracks"),  reco("MuonRecAlg", FileName="mucomb.dat", Output="CombMuons"), muCombHypo ] )
-steps[stepNo] += mu2
+## g2 = stepSeq("g2", useExisting("Step2G"), [ reco("GRoIs", "noreco.dat"), reco("PhotonRecAlg", FileName="photons.dat", Output="Photons"), gHypo ])
+## steps[stepNo] += g2
+## stepNo += 1
 
-e2 = stepSeq( "e2" , useExisting("Step2E"), [ useExisting("TrigFastTrackFinder"), reco("ElectronRecAlg", FileName="electrons.dat", Output="Electrons"), eHypo ] )
-steps[stepNo] += e2
-
-g2 = stepSeq("g2", useExisting("Step2G"), [ reco("GRoIs", "noreco.dat"), reco("PhotonRecAlg", FileName="photons.dat", Output="Photons"), gHypo ])
-steps[stepNo] += g2
-stepNo += 1
-
-#mue2 = stepSeq("mue2", useExisting("Step2MU_E"), [ useExisting("TrkRoIs", "noreco.dat"), useExisting("TrigFastTrackFinder"), reco("MuonRecAlg", "mucomb.dat"),  reco("ElectronRecAlg"), hypo("Step2MuEHypo") ])
-#steps[stepNo] += mue2
+## #mue2 = stepSeq("mue2", useExisting("Step2MU_E"), [ useExisting("TrkRoIs", "noreco.dat"), useExisting("TrigFastTrackFinder"), reco("MuonRecAlg", "mucomb.dat"),  reco("ElectronRecAlg"), hypo("Step2MuEHypo") ])
+## #steps[stepNo] += mue2
 
 
 theApp.EvtMax = 3
