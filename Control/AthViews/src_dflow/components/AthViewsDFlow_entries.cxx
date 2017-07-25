878c36d5577eb22197655a72b4f1b95d3a2f98bf
bwynne <bwynne@cern.ch>
2017-07-25 16:22:55 +0200

    Adding view behaviour to ID HLT demonstrator, generic helper algs added to views package
    Former-commit-id: 27d4ad79104fe05d083028cad0bc384561f96494

--- Control/AthViews/src_dflow/components/AthViewsDFlow_entries.cxx
+++ Control/AthViews/src_dflow/components/AthViewsDFlow_entries.cxx
@@ -1,23 +1,29 @@
 #include "../DFlowAlg1.h"
 #include "../DFlowAlg2.h"
 #include "../DFlowAlg3.h"
+#include "../ViewTestAlg.h"
 #include "../ViewMergeAlg.h"
 #include "../ViewSubgraphAlg.h"
+#include "../RoiCollectionToViews.h"
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, DFlowAlg1 )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, DFlowAlg2 )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, DFlowAlg3 )
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, ViewTestAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, ViewMergeAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, ViewSubgraphAlg )
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( AthViews, RoiCollectionToViews )
 
 DECLARE_FACTORY_ENTRIES( AthViewsDFlow )
 {
     DECLARE_NAMESPACE_ALGORITHM( AthViews, DFlowAlg1 )
     DECLARE_NAMESPACE_ALGORITHM( AthViews, DFlowAlg2 )
     DECLARE_NAMESPACE_ALGORITHM( AthViews, DFlowAlg3 )
+    DECLARE_NAMESPACE_ALGORITHM( AthViews, ViewTestAlg )
     DECLARE_NAMESPACE_ALGORITHM( AthViews, ViewMergeAlg )
     DECLARE_NAMESPACE_ALGORITHM( AthViews, ViewSubgraphAlg )
+    DECLARE_NAMESPACE_ALGORITHM( AthViews, RoiCollectionToViews )
 }
 
