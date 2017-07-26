1eb78d721668df23915a3484c3a4015545e9011f
John Chapman <jchapman@cern.ch>
2017-07-26 13:06:06 +0200

    Move truthStrategy configuration into common configuration.
    Reducing repeated code.
    
    
    Former-commit-id: 2e065c867bbb79c87f587863197b06358bf8a377

--- Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT.py
+++ Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT.py
@@ -15,3 +15,22 @@ if hasattr(runArgs, "inputTXT_EVENTIDFile"):
     if hasattr(runArgs, 'inputVertexPosFile'):
         simFlags.VertexOverrideFile= runArgs.inputVertexPosFile
 
+if hasattr(runArgs, 'truthStrategy'):
+    simFlags.TruthService     = 'ISF_'     + runArgs.truthStrategy + 'TruthService'
+    simFlags.TruthStrategy    = runArgs.truthStrategy
+    try:
+        from BarcodeServices.BarcodeServicesConfig import barcodeOffsetForTruthStrategy
+        simFlags.SimBarcodeOffset  = barcodeOffsetForTruthStrategy(runArgs.truthStrategy)
+    except RuntimeError:
+        Offset200kStrings = ['MC12', 'MC15a', 'MC16']
+        is200kOffset = any(x in runArgs.truthStrategy for x in Offset200kStrings)
+        if is200kOffset:
+            simFlags.SimBarcodeOffset  = 200000 #MC12, MC15a, MC16 setting
+        else:
+            simFlags.SimBarcodeOffset  = 1000000 #MC15 setting
+        atlasG4log.warning('Using unknown truth strategy '+str(runArgs.truthStrategy)+' guessing that barcode offset is '+str(simFlags.SimBarcodeOffset))
+else:
+    simFlags.TruthService     = 'ISF_TruthService'
+    simFlags.TruthStrategy    = 'MC12'
+    simFlags.SimBarcodeOffset  = 200000 #MC12 setting
+
