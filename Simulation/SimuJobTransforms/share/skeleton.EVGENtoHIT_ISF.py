06043cd98fefefbf75f7d1e2ef930b4f6e8f3539
John Chapman <jchapman@cern.ch>
2017-07-26 13:06:06 +0200

    Move truthStrategy configuration into common configuration.
    Reducing repeated code.
    
    
    Former-commit-id: dd6e016ee28068959c12fdfe60b2fde6b3638e91

--- Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_ISF.py
+++ Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_ISF.py
@@ -248,25 +248,6 @@ try:
 except:
     atlasG4log.warning('Could not add TimingAlg, no timing info will be written out.')
 
-if hasattr(runArgs, 'truthStrategy'):
-    simFlags.TruthService     = 'ISF_'     + runArgs.truthStrategy + 'TruthService'
-    simFlags.TruthStrategy    = runArgs.truthStrategy
-    try:
-        from BarcodeServices.BarcodeServicesConfig import barcodeOffsetForTruthStrategy
-        simFlags.SimBarcodeOffset  = barcodeOffsetForTruthStrategy(runArgs.truthStrategy)
-    except RuntimeError:
-        Offset200kStrings = ['MC12', 'MC15a', 'MC16']
-        is200kOffset = any(x in runArgs.truthStrategy for x in Offset200kStrings)
-        if is200kOffset:
-            simFlags.SimBarcodeOffset  = 200000 #MC12, MC15a, MC16 setting
-        else:
-            simFlags.SimBarcodeOffset  = 1000000 #MC15 setting
-        atlasG4log.warning('Using unknown truth strategy '+str(runArgs.truthStrategy)+' guessing that barcode offset is '+str(simFlags.SimBarcodeOffset))
-else:
-    simFlags.TruthService     = 'ISF_TruthService'
-    simFlags.TruthStrategy    = 'MC12'
-    simFlags.SimBarcodeOffset  = 200000 #MC12 setting
-
 ## Always enable the looper killer, unless it's been disabled
 if not hasattr(runArgs, "enableLooperKiller") or runArgs.enableLooperKiller:
     simFlags.OptionalUserActionList.addAction('G4UA::LooperKillerTool', ['Step'])
