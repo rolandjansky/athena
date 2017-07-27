6848a091491630a612d13390e71c4858c2bf26e3
James Richard Catmore <james.catmore@cern.ch>
2017-07-27 18:01:55 +0200

    Updating PHYSVAL job options with flavour tagging, and commenting sections which are not working yet
    Former-commit-id: 43c0551f00ff280b69fff01478d76e4c0999dbb7

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExamples/share/PHYSVAL.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExamples/share/PHYSVAL.py
@@ -7,10 +7,10 @@
 # It requires the reductionConf flag PHYSVAL in Reco_tf.py   
 #====================================================================
 
-# Set up common services and job object. 
 from DerivationFrameworkCore.DerivationFrameworkMaster import *
 from DerivationFrameworkJetEtMiss.JetCommon import *
 from DerivationFrameworkJetEtMiss.ExtendedJetCommon import *
+from DerivationFrameworkFlavourTag.FlavourTagCommon import FlavorTagInit
 
 #====================================================================
 # CREATE THE DERIVATION KERNEL ALGORITHM   
@@ -23,45 +23,77 @@ DerivationFrameworkJob += CfgMgr.DerivationFramework__DerivationKernel("PHYSVALK
 # JET/MET   
 #====================================================================
 
-OutputJets["PHYSVAL"] = []
-reducedJetList = ["AntiKt2PV0TrackJets",
-                  "AntiKt4PV0TrackJets",
-                  "AntiKt4TruthJets"]
+#OutputJets["PHYSVAL"] = ["AntiKtVR30Rmax4Rmin02TrackJets"]
+
+#from JetRecTools.JetRecToolsConfig import ctm
+#clustLCOrigSeq = ctm.buildConstitModifSequence( "JetConstitSeq_LCOrigin",
+#                                                OutputContainer='LCOriginTopoClusters',
+#                                                InputContainer= 'CaloCalTopoClusters',
+#                                                modList = [  'lc_origin' ] )
+#
+#clustEMOrigSeq = ctm.buildConstitModifSequence( "JetConstitSeq_EMOrigin",
+#                                                OutputContainer='EMOriginTopoClusters',
+#                                                InputContainer= 'CaloCalTopoClusters',
+#                                                modList = [  'em_origin' ] )
+#
+#from JetRec.JetRecConf import JetAlgorithm
+#clustSeqAlg = JetAlgorithm("ClusterModifiers", Tools = [clustLCOrigSeq,clustEMOrigSeq])
+#DerivationFrameworkJob += clustSeqAlg
+
+reducedJetList = [ "AntiKt10PV0TrackJets",
+                   "AntiKt4TruthWZJets",
+                   "AntiKt10TruthJets",
+                   "AntiKt10TruthWZJets",
+                   "AntiKt10LCTopoJets"
+                   "AntiKt2PV0TrackJets",
+                   "AntiKt4PV0TrackJets",
+                   "AntiKt4TruthJets"]
 replaceAODReducedJets(reducedJetList,DerivationFrameworkJob,"PHYSVAL")
+updateJVT_xAODColl("AntiKt4EMTopo")
+
+#====================================================================
+# FLAVOUR TAGGING   
+#====================================================================
+
+FlavorTagInit(isFTAG1 = True, JetCollections  = [ 'AntiKt4EMTopoJets',
+                                                  'AntiKtVR30Rmax4Rmin02TrackJets',
+                                                  'AntiKt4EMPFlowJets'], Sequencer = DerivationFrameworkJob)
 
 #====================================================================
 # SET UP STREAM   
 #====================================================================
 
-# SKIMMING
-# The base name (DAOD_PHYSVAL here) must match the string in 
-# DerivationFrameworkProdFlags (in DerivationFrameworkCore) 
 streamName = derivationFlags.WriteDAOD_PHYSVALStream.StreamName
 fileName   = buildFileName( derivationFlags.WriteDAOD_PHYSVALStream )
 PHYSVALStream = MSMgr.NewPoolRootStream( streamName, fileName )
-# Only events that pass the filters listed below are written out.
-# Name must match that of the kernel above
-# AcceptAlgs  = logical OR of filters
-# RequireAlgs = logical AND of filters
 PHYSVALStream.AcceptAlgs(["PHYSVALKernel"])
 
 #====================================================================
 # CONTENTS   
 #====================================================================
-#from PrimaryDPDMaker import PrimaryDPD_OutputDefinitions as dpdOutput
-#excludeList = []
-#dpdOutput.addAllItemsFromInputExceptExcludeList( streamName, excludeList )
 from DerivationFrameworkCore.SlimmingHelper import SlimmingHelper
 PHYSVALSlimmingHelper = SlimmingHelper("PHYSVALSlimmingHelper")
-PHYSVALSlimmingHelper.AllVariables = [ "Electrons", "Photons", "Muons", "PrimaryVertices",
-                                     "AntiKt4EMTopoJets","AntiKt4LCTopoJets","AntiKt4EMPFlowJets",
-                                     "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets",
-                                     "BTagging_AntiKt4EMTopo",
-                                     "BTagging_AntiKt2Track","MuonTruthParticles", "egammaTruthParticles",
-                                     "TruthParticles", "TruthEvents", "TruthVertices",
-                                     "MuonSegments",
-                                     "Kt4EMTopoOriginEventShape","Kt4LCTopoOriginEventShape","Kt4EMPFlowEventShape"
+PHYSVALSlimmingHelper.AllVariables = [ "Electrons", "Photons", "Muons", "PrimaryVertices", "InDetTrackParticles",
+                                       "AntiKt4EMTopoJets","AntiKt4LCTopoJets", "AntiKt4EMPFlowJets",
+                                       "AntiKt10PV0TrackJets", "AntiKt4TruthWZJets", "AntiKt10TruthJets",
+                                       "AntiKt10TruthWZJets", "AntiKt10LCTopoJets", "AntiKt2PV0TrackJets",
+                                       "AntiKt4PV0TrackJets", "AntiKt4TruthJets",
+                                       "Kt4EMPFlowEventShape","Kt4LCTopoOriginEventShape","Kt4EMTopoOriginEventShape",
+                                       "BTagging_AntiKt4EMTopoJFVtx","BTagging_AntiKt4EMTopo",
+                                       "BTagging_AntiKtVR30Rmax4Rmin02TrackJFVtx","BTagging_AntiKt4EMPFlow",
+                                       "BTagging_AntiKtVR30Rmax4Rmin02Track","BTagging_AntiKt4EMPFlowJFVtx",
+                                       #"BTagging_AntiKt4EMPFlowSecVtx","BTagging_AntiKt2TrackSecVtx","BTagging_AntiKt4EMTopoSecVtx",
+                                       #"BTagging_AntiKt4TrackSecVtx","BTagging_AntiKtVR30Rmax4Rmin02TrackSecVtx",
+                                       "LCOriginTopoClusters","EMOriginTopoClusters",
+                                       "TruthParticles", "TruthEvents", "TruthVertices"
                                      ]
+#PHYSVALSlimmingHelper.ExtraVariables = [ 
+                                         #"BTagging_AntiKt4EMTopoSecVtx.-vxTrackAtVertex",
+                                         #"BTagging_AntiKtVR30Rmax4Rmin02TrackSecVtx.-vxTrackAtVertex",
+                                         #"BTagging_AntiKt2TrackSecVtx.-vxTrackAtVertex",
+                                         #"BTagging_AntiKt4TrackSecVtx.-vxTrackAtVertex",
+                                         #"BTagging_AntiKt4EMPFlowSecVtx.-vxTrackAtVertex" ]
+
 
 # Trigger content
 PHYSVALSlimmingHelper.IncludeJetTriggerContent = True
@@ -75,12 +107,34 @@ PHYSVALSlimmingHelper.IncludeBJetTriggerContent = True
 PHYSVALSlimmingHelper.IncludeBPhysTriggerContent = True
 PHYSVALSlimmingHelper.IncludeMinBiasTriggerContent = True
 
-# Add the jet containers to the stream
-addJetOutputs(PHYSVALSlimmingHelper,["SmallR","JETM1"],[], # smart list
-              [
-               "AntiKt4TruthWZJets",
-               ]# veto list
-              )
+#----------------------------------------------------------------------
+# NamesAndTypes lookup table for on-the-fly containers
+PHYSVALSlimmingHelper.AppendToDictionary = {
+  "AntiKtVR30Rmax4Rmin02TrackJets"             :   "xAOD::JetContainer"        ,
+  "AntiKtVR30Rmax4Rmin02TrackJetsAux"          :   "xAOD::JetAuxContainer"     ,
+  "BTagging_AntiKtVR30Rmax4Rmin02Track"        :   "xAOD::BTaggingContainer"   ,
+  "BTagging_AntiKtVR30Rmax4Rmin02TrackAux"     :   "xAOD::BTaggingAuxContainer",
+  "BTagging_AntiKt2Track"                      :   "xAOD::BTaggingContainer"   ,
+  "BTagging_AntiKt2TrackAux"                   :   "xAOD::BTaggingAuxContainer",
+  "BTagging_AntiKt4Track"                      :   "xAOD::BTaggingContainer"   ,
+  "BTagging_AntiKt4TrackAux"                   :   "xAOD::BTaggingAuxContainer",
+  "BTagging_AntiKt2TrackJFVtx"                 :   "xAOD::BTagVertexContainer"   ,
+  "BTagging_AntiKt2TrackJFVtxAux"              :   "xAOD::BTagVertexAuxContainer",
+  "BTagging_AntiKt4TrackJFVtx"                 :   "xAOD::BTagVertexContainer"   ,
+  "BTagging_AntiKt4TrackJFVtxAux"              :   "xAOD::BTagVertexAuxContainer",
+  "BTagging_AntiKt2TrackSecVtx"                :   "xAOD::VertexContainer"   ,
+  "BTagging_AntiKt2TrackSecVtxAux"             :   "xAOD::VertexAuxContainer",
+  "BTagging_AntiKt4TrackSecVtx"                :   "xAOD::VertexContainer"   ,
+  "BTagging_AntiKt4TrackSecVtxAux"             :   "xAOD::VertexAuxContainer",
+  "BTagging_AntiKt4EMTopoSecVtx"               :   "xAOD::VertexContainer"   ,
+  "BTagging_AntiKt4EMTopoSecVtxAux"            :   "xAOD::VertexAuxContainer",
+  "BTagging_AntiKtVR30Rmax4Rmin02TrackSecVtx"  :   "xAOD::VertexContainer"   ,
+  "BTagging_AntiKtVR30Rmax4Rmin02TrackSecVtxAux"  :"xAOD::VertexAuxContainer",  
+  "BTagging_AntiKt4EMPFlowSecVtx"              :   "xAOD::VertexContainer"   ,
+  "BTagging_AntiKt4EMPFlowSecVtxAux"           :   "xAOD::VertexAuxContainer",
+  }
+#----------------------------------------------------------------------
+
 
 # Final construction of output stream
 PHYSVALSlimmingHelper.AppendContentToStream(PHYSVALStream)
