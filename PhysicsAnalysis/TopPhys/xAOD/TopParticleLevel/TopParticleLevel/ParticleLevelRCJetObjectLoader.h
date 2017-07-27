1eddcb2fecb7da6b5eab72264c62d735c44a2018
iconnell <ian.connelly@cern.ch>
2017-07-27 16:03:33 +0100

    Updating to TopParticleLevel-00-00-16
    Former-commit-id: 6640176447f3885436092be7df9ab69336b3a85b

---
+++ PhysicsAnalysis/TopPhys/xAOD/TopParticleLevel/TopParticleLevel/ParticleLevelRCJetObjectLoader.h
@@ -0,0 +1,88 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+//Load the reclustering tool for the collection of truth jets already selected.
+//Create the collection of reclustered jets and save them in the evtStore().
+//There is also the pass slection to apply the cut on eta and pt on the reclustered jets.
+
+#ifndef ParticleLevelRCJetObjectLoader_H_
+#define ParticleLevelRCJetObjectLoader_H_
+
+
+#include "TopParticleLevel/ObjectSelectorBase.h"
+#include "AsgTools/AsgTool.h"
+#include "xAODCore/ShallowCopy.h"
+
+#include "xAODRootAccess/TEvent.h"
+#include "xAODRootAccess/TStore.h"
+#include "xAODRootAccess/TActiveStore.h"
+
+#include "xAODJet/JetContainer.h"
+#include "xAODJet/JetAuxContainer.h"
+#include "JetReclustering/JetReclusteringTool.h"
+
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+#include <set>
+#include <list>
+#include <vector>
+
+
+namespace top{
+    class ParticleLevelEvent;
+    class TopConfig;
+}
+
+
+class ParticleLevelRCJetObjectLoader: public asg::AsgTool {
+public:
+    
+    
+    //Default - so root can load based on a name
+    ParticleLevelRCJetObjectLoader( const std::shared_ptr<top::TopConfig> & cfg ) ;
+   
+
+    //Default - so we can clean up
+    ~ParticleLevelRCJetObjectLoader();
+
+    //Run once at the start of the job
+    StatusCode initialize();
+
+    //Run for every event (in every systematic) that needs saving
+    StatusCode execute(const top::ParticleLevelEvent& plEvent);
+
+
+    //Clean-up remaining things
+    StatusCode finalize();
+
+    bool passSelection(const xAOD::Jet& jet) const;
+    
+    //Access all of the container names from the Event Saver
+     std::string inputContainerName(){return m_InputJetContainer; };
+     std::string rcjetContainerName(){return m_OutputJetContainer; };
+    
+private:
+    const std::shared_ptr<top::TopConfig> & m_config;
+
+    float m_ptcut;       // in GeV
+    float m_etamax;
+    float m_trim;
+    float m_radius;
+    
+    std::string m_treeName;
+
+    std::string m_InJetContainerBase;
+    std::string m_OutJetContainerBase;
+    std::string m_InputJetContainer;
+    std::string m_OutputJetContainer;
+   
+    
+
+    //re-clustered jets
+    //  -> need unordered map for systematics
+    JetReclusteringTool* m_jetReclusteringTool;
+};
+
+#endif
