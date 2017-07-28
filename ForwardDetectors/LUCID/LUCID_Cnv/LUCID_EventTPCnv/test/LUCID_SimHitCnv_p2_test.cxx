b710e859e754db10a3a9060a296ff5e1a6402e95
John Chapman <jchapman@cern.ch>
2017-07-28 14:21:28 +0200

    Migrating the LUCID_EventTPCnv TP Converters to use the new HepMcParticleLink
    This commit updates the TP Converters which use `HepMcParticleLink_p1` to
    properly initialize the new transient `HepMcParticleLink` class. It also
    adds code to write rather than just read the `LUCID_SimHit_p2` persistent
    version of `LUCID_SimHit` which uses `HepMcParticleLink_p2`. This pulls
    in the remaining changes from `LUCID_EventTPCnv-00-07-01`. This is part of
    the `HepMcParticleLink` migration. See ATLASSIM-2430.
    
    Additionally add a unit test for `LUCID_SimHitCnv_p2` and update the
    `LUCID_EventTPCnv` unit tests to use valid `HepMcParticleLinks`.

--- ForwardDetectors/LUCID/LUCID_Cnv/LUCID_EventTPCnv/test/LUCID_SimHitCnv_p2_test.cxx
+++ ForwardDetectors/LUCID/LUCID_Cnv/LUCID_EventTPCnv/test/LUCID_SimHitCnv_p2_test.cxx
@@ -1,19 +1,17 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 // $Id$
 /**
- * @file LUCID_EventTPCnv/test/LUCID_SimHitCnv_p1_test.cxx
- * @author scott snyder <snyder@bnl.gov>
- * @date Feb, 2016
- * @brief Tests for LUCID_SimHitCnv_p1.
+ * @file LUCID_EventTPCnv/test/LUCID_SimHitCnv_p2_test.cxx
+ * @date Feb, 2018
+ * @brief Tests for LUCID_SimHitCnv_p2.
  */
 
 
 #undef NDEBUG
-#include "LUCID_EventTPCnv/LUCID_SimHitCnv_p1.h"
-#include "TestTools/leakcheck.h"
+#include "LUCID_EventTPCnv/LUCID_SimHitCnv_p2.h"
 #include <cassert>
 #include <iostream>
 
@@ -58,8 +56,8 @@ void compare (const LUCID_SimHit& p1,
 void testit (const LUCID_SimHit& trans1)
 {
   MsgStream log (0, "test");
-  LUCID_SimHitCnv_p1 cnv;
-  LUCID_SimHit_p1 pers;
+  LUCID_SimHitCnv_p2 cnv;
+  LUCID_SimHit_p2 pers;
   cnv.transToPers (&trans1, &pers, log);
   LUCID_SimHit trans2;
   cnv.persToTrans (&pers, &trans2, log);
@@ -71,12 +69,6 @@ void testit (const LUCID_SimHit& trans1)
 void test1(std::vector<HepMC::GenParticle*>& genPartVector)
 {
   std::cout << "test1\n";
-  const HepMC::GenParticle *particle = genPartVector.at(0);
-  // Create HepMcParticleLink outside of leak check.
-  HepMcParticleLink dummyHMPL(particle->barcode(),0);
-  assert(dummyHMPL.cptr()==particle);
-  Athena_test::Leakcheck check;
-
   HepMcParticleLink trkLink(genPartVector.at(0)->barcode(),genPartVector.at(0)->parent_event()->event_number());
   LUCID_SimHit trans1 (1, genPartVector.at(0)->pdg_id(), trkLink, 4,
                        5.5, 6.5, 7.5,
