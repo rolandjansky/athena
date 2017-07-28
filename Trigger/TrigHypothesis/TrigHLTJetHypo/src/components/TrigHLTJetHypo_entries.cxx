95b034df0a9ff8ef58fb6b57b29cd4d82ea92ddf
sherwood <peter.sherwood@cern.ch>
2017-07-28 22:38:24 +0200

    Add the TrigHLTJetHypo_SMCTool and TrigHLTJetHypo_TLATool
    This completes the tools needed for the currently defined chains.
    
    
    Former-commit-id: 27953f0e6431357a8f6b25ae9cb35b544be1dd57

--- Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
@@ -7,7 +7,8 @@
 
 #include "../TrigHLTJetHypo_EtaEtTool.h"
 #include "../TrigHLTJetHypo_DijetMassDEtaTool.h"
-
+#include "../TrigHLTJetHypo_SMCTool.h"
+#include "../TrigHLTJetHypo_TLATool.h"
 
 DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo2)
 
@@ -22,8 +23,12 @@ DECLARE_FACTORY_ENTRIES(TrigHLTJetHypo) {
     DECLARE_ALGORITHM(TrigEFDPhiMetJetAllTE)
     DECLARE_TOOL(TrigHLTJetHypo_EtaEtTool)
     DECLARE_TOOL(TrigHLTJetHypo_DijetDEtaMassTool)
+    DECLARE_TOOL(TrigHLTJetHypo_SMCTool)
+    DECLARE_TOOL(TrigHLTJetHypo_TLATool)
 }
 
 DECLARE_TOOL_FACTORY(TrigHLTJetHypo_EtaEtTool)
 DECLARE_TOOL_FACTORY(TrigHLTJetHypo_DijetMassDEtaTool)
+DECLARE_TOOL_FACTORY(TrigHLTJetHypo_SMCTool)
+DECLARE_TOOL_FACTORY(TrigHLTJetHypo_TLATool)
 
