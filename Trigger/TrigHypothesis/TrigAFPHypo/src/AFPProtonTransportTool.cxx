f8910f65b91c557f2bd04a1d643095b5b26512b9
Tiago Dias Do Vale <tiago.vale@cern.ch>
2017-08-02 17:45:43 +0200

    Added some documentation to source file of Tool
    Former-commit-id: 2ada401051851c62acd20ed08840099189d30b56

--- Trigger/TrigHypothesis/TrigAFPHypo/src/AFPProtonTransportTool.cxx
+++ Trigger/TrigHypothesis/TrigAFPHypo/src/AFPProtonTransportTool.cxx
@@ -71,61 +71,65 @@ int AFPProtonTransportTool::load(std::string filename) {
     std::string name;
     file >> ch >> name;
 
-    if (name == "m_xPositionInitIP") {
-      double value;
-      file >> value;
-      m_xPositionInitIP = value;
-    }
-    if (name == "m_yPositionInitIP") {
-      double value;
-      file >> value;
-      m_yPositionInitIP = value;
-    }
-    if (name == "m_zPositionInitIP") {
-      double value;
-      file >> value;
-      m_zPositionInitIP = value;
-    }
-    if (name == "m_xSlopeInitIP") {
-      double value;
-      file >> value;
-      m_xSlopeInitIP = value;
-    }
-    if (name == "m_ySlopeInitIP") {
-      double value;
-      file >> value;
-      m_ySlopeInitIP = value;
-    }
-    if (name == "m_detectorPosition") {
-      double value;
-      file >> value;
-      m_parametrisationPosition = value;
-    }
-    if (name == "m_nominalEnergy") {
-      double value;
-      file >> value;
-      m_energy = value;
-    }
+	//Fetching variable values from the txt file
+	if (name == "m_xPositionInitIP") {
+		double value;
+		file >> value;
+		m_xPositionInitIP = value;
+	}
+	if (name == "m_yPositionInitIP") {
+		double value;
+		file >> value;
+		m_yPositionInitIP = value;
+	}
+	if (name == "m_zPositionInitIP") {
+		double value;
+		file >> value;
+		m_zPositionInitIP = value;
+	}
+	if (name == "m_xSlopeInitIP") {
+		double value;
+		file >> value;
+		m_xSlopeInitIP = value;
+	}
+	if (name == "m_ySlopeInitIP") {
+		double value;
+		file >> value;
+		m_ySlopeInitIP = value;
+	}
+	if (name == "m_detectorPosition") {
+		double value;
+		file >> value;
+		m_parametrisationPosition = value;
+	}
+	if (name == "m_nominalEnergy") {
+		double value;
+		file >> value;
+		m_energy = value;
+	}
 
-    file.ignore(1000, '\n');
+	file.ignore(1000, '\n');
   }
 
+  //Fetching the polynomial coeficients
   for (int iEqu = 0; iEqu < 4; iEqu++) {
-    file.ignore(1000, '\n');
-    for (int iPoly = 0; iPoly < 8; iPoly++) {
-      int N = 0;
-      file >> N;
-      double ch[10];
-      for (int i = 0; i < 10; i++) {
-        if (i <= N) file >> ch[i];
-        else ch[i] = 0;
-      }
-      file.ignore(1000, '\n');
+	  file.ignore(1000, '\n');
+	  for (int iPoly = 0; iPoly < 8; iPoly++) {
+		  int N = 0;
+		  file >> N;
+		  double ch[10];
+		  for (int i = 0; i < 10; i++) {
+			  if (i <= N) file >> ch[i];
+			  else ch[i] = 0;
+		  }
+		  file.ignore(1000, '\n');
 
-      pol[iEqu][iPoly] =
-        new AFPProtonTransportPolynomial(N, ch[0], ch[1], ch[2], ch[3], ch[4], ch[5], ch[6], ch[7], ch[8], ch[9]);
-    }
+		  //Create polynomial object with the coeficients loaded from the file
+		  pol[iEqu][iPoly] =
+			  new AFPProtonTransportPolynomial(N, ch[0], ch[1], ch[2], ch[3], ch[4], ch[5], ch[6], ch[7], ch[8], ch[9]);
+	  }
   }
+  //Creating the Parameterization objects with the polynomials created
   m_px = new AFPProtonTransportParam(m_energy, pol[0]);
   m_py = new AFPProtonTransportParam(m_energy, pol[1]);
   m_psx = new AFPProtonTransportParam(m_energy, pol[2]);
