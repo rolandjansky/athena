7b359ff50a8b1ca2e153616befb56dbf4c849ccf
Tiago Dias Do Vale <tiago.vale@cern.ch>
2017-08-02 17:55:26 +0200

    Fixed typos
    Former-commit-id: f298665ca629523b8d15eaefbcccf0a6908a098f

--- Trigger/TrigHypothesis/TrigAFPHypo/TrigAFPHypo/AFPProtonTransportPolynomial.h
+++ Trigger/TrigHypothesis/TrigAFPHypo/TrigAFPHypo/AFPProtonTransportPolynomial.h
@@ -7,14 +7,13 @@
 
 class AFPProtonTransportPolynomial {
 public:
-  /// Default constructor of kinRecoPolynomial. Takes number of coefficients and coefficients itself up to 10 as
-     arguments.
+  /// Default constructor of kinRecoPolynomial. Takes number of coefficients and coefficients itself up to 10 as arguments.
   AFPProtonTransportPolynomial(int n, double c0, double c1 = 0, double c2 = 0, double c3 = 0, double c4 = 0,
                                double c5 = 0, double c6 = 0, double c7 = 0, double c8 = 0, double c9 = 0);
 
   /// This function returns the number of coefficients. 
   int numCoefficients() const;
-  /// This function sets the coefficients of polynomial. 
+  /// This function sets the coefficients of the polynomial. 
   void setCoefficients(int n, double c0, double c1 = 0, double c2 = 0, double c3 = 0, double c4 = 0,
                        double c5 = 0, double c6 = 0, double c7 = 0, double c8 = 0, double c9 = 0);
 
