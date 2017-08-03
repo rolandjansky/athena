cab3e456ce1098cc12e816f72587106d65457740
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2017-08-03 19:18:44 +0200

    Account for the cell weights in the calculation to not double count, when looping over the cluster cells, avoid picking up inner endcap wheel cells from the CaloCellList
    Former-commit-id: bf44311df825c483745e40aa241e0509077902da

--- Reconstruction/egamma/egammaCaloTools/src/egammaStripsShape.h
+++ Reconstruction/egamma/egammaCaloTools/src/egammaStripsShape.h
@@ -184,7 +184,7 @@ class egammaStripsShape : public AthAlgTool, virtual public IegammaStripsShape {
   double m_dphi;
 
   enum { STRIP_ARRAY_SIZE = 40 };
-  enum { DOUBLE_STRIP_ARRAY_SIZE = 80 };
+  enum { BIG_STRIP_ARRAY_SIZE = STRIP_ARRAY_SIZE*3 };
   /** @brief array of cell energies*/
   double m_enecell[STRIP_ARRAY_SIZE]; 
   /** @brief array of cell eta*/
