c20f706929c40bdb485478b29b9b939559b8775e
John Chapman <jchapman@cern.ch>
2017-08-03 09:47:22 +0200

    Add a specific configuration for TileGeoG4SDCalc for Test Beam jobs
    Former-commit-id: abccb27861ab7ce3a4ffdd1f8e732158390f8ee4

--- TileCalorimeter/TileG4/TileGeoG4SD/python/TileGeoG4SDConfigDb.py
+++ TileCalorimeter/TileG4/TileGeoG4SD/python/TileGeoG4SDConfigDb.py
@@ -4,3 +4,4 @@ from AthenaCommon.CfgGetter import addTool, addService
 addTool("TileGeoG4SD.TileGeoG4SDConfig.getTileGeoG4SD"         , "TileGeoG4SD" )
 addTool("TileGeoG4SD.TileGeoG4SDConfig.getTileCTBGeoG4SD"      , "TileCTBGeoG4SD" )
 addService("TileGeoG4SD.TileGeoG4SDConfig.getTileGeoG4SDCalc"  , "TileGeoG4SDCalc")
+addService("TileGeoG4SD.TileGeoG4SDConfig.getTileCTBGeoG4SDCalc"  , "TileCTBGeoG4SDCalc")
