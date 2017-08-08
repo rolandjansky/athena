6dd78056184947ffcdc1f2dd9c3dc84e358b5a3c
Frank Winklmeier <Frank.Winklmeier@cern.ch>
2017-08-08 15:03:17 +0200

    Further MT improvements for CoreDumpSvc
    In MT-mode use the AlgExecStateSvc to find the currently executing
    algorithm(s) for each slot.
    
    Other changes:
    - General code modernization (e.g. Gaudi::Property, extends, ...)
    - Add setCoreDumpInfo interface taking event context as parameter
    
    
    Former-commit-id: f1e1a6684e130655834b5bcb24454e5c036d6116

--- Control/AthenaKernel/AthenaKernel/ICoreDumpSvc.h
+++ Control/AthenaKernel/AthenaKernel/ICoreDumpSvc.h
@@ -10,15 +10,11 @@
  * @brief  Interface of a core dump service
  * @author Frank Winklmeier
  *
- * $Id: ICoreDumpSvc.h,v 1.2 2008-05-28 10:55:04 fwinkl Exp $
  */
 
-#ifndef GAUDIKERNEL_IINTERFACE_H
- #include "GaudiKernel/IInterface.h"
-#endif
-#ifndef _CPP_STRING
- #include <string>
-#endif
+#include "GaudiKernel/IInterface.h"
+#include "GaudiKernel/EventContext.h"
+#include <string>
 
 /**
  * @class  ICoreDumpSvc
@@ -30,24 +26,19 @@
 
 class ICoreDumpSvc: virtual public IInterface {
 public:
-  /// Retrieve interface ID
-  static const InterfaceID& interfaceID();
+  DeclareInterfaceID(ICoreDumpSvc, 1, 0);
 
   /// Virtualize D'tor
   virtual ~ICoreDumpSvc() {}
 
   /// Set a name/value pair in the core dump record
   virtual void setCoreDumpInfo( const std::string& name, const std::string& value ) = 0;
+
+  /// Set a name/value pair in the core dump record for given EventContext
+  virtual void setCoreDumpInfo( const EventContext& ctx, const std::string& name, const std::string& value ) = 0;
   
   /// Print all core dump records
   virtual std::string dump() const = 0;
 };
-
-
-inline const InterfaceID& ICoreDumpSvc::interfaceID()
-{
-  static const InterfaceID IID_ICoreDumpSvc("ICoreDumpSvc", 1, 0);
-  return IID_ICoreDumpSvc;
-}
 
 #endif // ATHENAKERNEL_ICOREDUMPSVC_H
