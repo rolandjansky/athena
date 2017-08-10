57dba081a2ed98facb0d7227d88e8de1250981a4
lawrenceleejr <lawrenceleejr@gmail.com>
2017-08-10 02:56:25 +0200

    VrtSecInclusive: adding lepton-specific settings to DV_postInclude
    Former-commit-id: ce071b3d87aaa4502ebd75a3b2203461c7251d34

--- Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
+++ Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
@@ -2,3 +2,14 @@ StreamESD.ItemList+=["xAOD::TrackParticleContainer#VrtSecInclusive*"]
 StreamESD.ItemList+=["xAOD::VertexContainer#VrtSecInclusive*"]
 StreamESD.ItemList+=["xAOD::VertexAuxContainer#VrtSecInclusive*"]
 
+
+ToolSvc.EGammaAmbiguityTool.minNoSiHits = 2
+ToolSvc.EGammaAmbiguityTool.minNoPixHits = 0
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.IPd0Max = 9999.
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitPix = 0
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitSct = 2
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitSi = 2
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.IPd0Max = 9999.
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitPix = 0
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitSct = 2
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitSi = 2
