09e3d9d9311e4d00cec5ba9a8c2d6e0ab29aa4f0
Steve Farrell <sfarrell@cern.ch>
2017-08-10 23:20:08 +0200

    Removed duplication in RNGWrapper setSeed methods.
    Changed the EventContext-based setSeed method to call the other setSeed
    method instead of duplicating implementation.
    
    
    Former-commit-id: b3dbfe264988ddc50721f9c3d26fbc25da525feb

--- Control/AthenaKernel/src/RNGWrapper.cxx
+++ Control/AthenaKernel/src/RNGWrapper.cxx
@@ -22,18 +22,9 @@ bool ATHRNG::RNGWrapper::setSeed(const std::string& algName, const EventContext&
   if(!ctx.valid()){
     return false;
   }
-  if ((m_evNum!=ctx.eventID().event_number())||(m_runNum!=ctx.eventID().run_number())){
-    m_evNum=ctx.eventID().event_number();
-    m_runNum=ctx.eventID().run_number();
-    auto algHash=std::hash<std::string>{}(algName);
-    auto evHash=std::hash<decltype(m_evNum)>{}(m_evNum);
-    auto runHash=std::hash<decltype(m_runNum)>{}(m_evNum);
-    auto hsh=evHash^(runHash+(evHash<<6)+(evHash>>2));
-    hsh=hsh^(algHash+(hsh<<6)+(hsh>>2));
-    m_engines[ctx.slot()]->setSeed(hsh,0);
-    return true;
-  }
-  return false;
+  return setSeed( algName, ctx.slot(),
+                  ctx.eventID().event_number(),
+                  ctx.eventID().run_number() );
 }
 
 bool ATHRNG::RNGWrapper::setSeed(const std::string& algName, size_t slot,
