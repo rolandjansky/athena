8ff4a11972b447c3dbe920ed4748ff7cf38c6833
Hass AbouZeid <hass.abouzeid@cern.ch>
2017-08-11 09:14:08 +0000

    Merge branch 'triggerTestARTInitialTest' into '21.0'
    TriggerTest ART initial test
    
    See merge request !3952
    
    Former-commit-id: 54c584e01625674ec5568dc4dc6b6f33da38d841

---
+++ Trigger/TrigValidation/TriggerTest/test/test_slice_muon_grid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# art-description: Muon slice TriggerTest on MC
+# art-type: grid
+
+export SLICE="muon"
+
+
+source exec_athena_art_trigger_validation.sh
+source exec_art_triggertest_post.sh
+
+return $ATH_EXIT
