017da418f13bfa7f7b0d811824016ae70a32d2bb
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2017-08-12 21:52:16 +0200

    FixedCutHighPtTrack
    Former-commit-id: 6f9ff3dc3f360675ccf5940a308d6d54a8d1ce09

--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationSelectionTool.cxx
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationSelectionTool.cxx
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+ FixedCutHighPtTrackOnly Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
  */
 
 #include <xAODPrimitives/IsolationType.h>
@@ -194,6 +194,8 @@ namespace CP {
         } else if (muWPname == "FixedCutTight") {
             wp->addCut(new IsolationConditionFormula("ptvarcone30R0p06", xAOD::Iso::ptvarcone30, "0.06*x"));
             wp->addCut(new IsolationConditionFormula("topoetcone20R0p06", xAOD::Iso::topoetcone20, "0.06*x"));
+        } else if (muWPname == "FixedCutHighPtTrackOnly") {
+            wp->addCut(new IsolationConditionFormula("ptcone20_1p25", xAOD::Iso::ptcone20, "1.25E03"));  //units are MeV!
         } else {
             ATH_MSG_ERROR("Unknown muon isolation WP: " << muWPname);
             delete wp;
