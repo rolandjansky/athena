8676c04bc28a535eb024202945789c52aeed6d6c
abarton <adam.edward.barton@cern.ch>
2017-08-15 15:00:48 +0100

    Add Cache creator algorithm for external InDetPrepRawDataFormation caches
    Former-commit-id: a8d341ae65c7e1ae5a72534bc094c747624d06cd

--- InnerDetector/InDetRecAlgs/InDetPrepRawDataFormation/src/components/InDetPrepRawDataFormation_entries.cxx
+++ InnerDetector/InDetRecAlgs/InDetPrepRawDataFormation/src/components/InDetPrepRawDataFormation_entries.cxx
@@ -1,6 +1,7 @@
 #include "InDetPrepRawDataFormation/PixelClusterization.h"
 #include "InDetPrepRawDataFormation/SCT_Clusterization.h"
 #include "InDetPrepRawDataFormation/TRT_RIO_Maker.h"
+#include "InDetPrepRawDataFormation/CacheCreator.h"
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
@@ -9,12 +10,14 @@ using namespace InDet;
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( InDet, PixelClusterization)
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( InDet, SCT_Clusterization )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( InDet, TRT_RIO_Maker      )
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( InDet, CacheCreator      )
 
 DECLARE_FACTORY_ENTRIES( InDetPrepRawDataFormation ) 
 {
   DECLARE_NAMESPACE_ALGORITHM( InDet, PixelClusterization)
   DECLARE_NAMESPACE_ALGORITHM( InDet, SCT_Clusterization )
   DECLARE_NAMESPACE_ALGORITHM( InDet, TRT_RIO_Maker      )
+  DECLARE_NAMESPACE_ALGORITHM( InDet, CacheCreator       )
 }
 
 
