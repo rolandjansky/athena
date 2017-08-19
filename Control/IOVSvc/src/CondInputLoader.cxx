29710548580566364fb881ec91470b0544b5578b
scott snyder <scott.snyder@cern.ch>
2017-08-19 22:10:09 +0200

    IOVSvc: Do not expose `Input' dependency to the scheduler.
    Do not expose the dependency on the AthenaAttributeList `Input' to the scheduler.
    Otherwise, conditions access doesn't work if there's no input file.
    This will anyway be cleaned up in a better way soon.
    
    
    Former-commit-id: e8bcd129ae0b38827fd7c2e6eeb0832740ea24e2

--- Control/IOVSvc/src/CondInputLoader.cxx
+++ Control/IOVSvc/src/CondInputLoader.cxx
@@ -49,7 +49,6 @@ CondInputLoader::CondInputLoader( const std::string& name,
   declareProperty( "Load", m_load); 
   //->declareUpdateHandler(&CondInputLoader::loader, this);
   declareProperty( "ShowEventDump", m_dump=false);
-  declareProperty( "InputKey", m_inputKey="Input" );
 }
 
 // Destructor
@@ -90,8 +89,6 @@ CondInputLoader::initialize()
     ATH_MSG_FATAL("unable to retrieve IOVDbSvc");
     return StatusCode::FAILURE;
   }
-
-  ATH_CHECK( m_inputKey.initialize() );
 
   std::vector<std::string> keys = idb->getKeyList();
   std::string folderName, tg;
@@ -250,7 +247,9 @@ CondInputLoader::execute()
   // may be different from that of the event itself.  If we have
   // a ConditionsRun attribute defined, use that to override
   // the event number.
-  SG::ReadHandle<AthenaAttributeList> input (m_inputKey, getContext());
+  SG::ReadHandleKey<AthenaAttributeList> inputKey ("Input");
+  ATH_CHECK( inputKey.initialize() );
+  SG::ReadHandle<AthenaAttributeList> input (inputKey, getContext());
   if (input.isValid()) {
     if (input->exists ("ConditionsRun"))
       now.set_run_number ((*input)["ConditionsRun"].data<unsigned int>());
