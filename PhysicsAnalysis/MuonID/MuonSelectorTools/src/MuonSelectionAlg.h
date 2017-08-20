1631c33ddabd1d2b452b9b3486e9636c07c57f06
Stefano Zambito <stefano.zambito@cern.ch>
2017-08-20 22:19:15 +0200

    Including updated low- and high-pt selections. Fixing several warnings/infos. Adding few other minor improvements.
    Former-commit-id: 826de94f51258d0abf3617a1af49043789d8abe5

--- PhysicsAnalysis/MuonID/MuonSelectorTools/src/MuonSelectionAlg.h
+++ PhysicsAnalysis/MuonID/MuonSelectorTools/src/MuonSelectionAlg.h
@@ -2,7 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-
 //Simple alg to wrap the selectiontool
 //outputs a view container of the selected muons, if m_outputMuons different to m_inputMuons
 
