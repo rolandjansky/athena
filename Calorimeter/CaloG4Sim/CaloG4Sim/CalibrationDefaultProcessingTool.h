73bc2c17a5f62122ba3a44bcbd00d98cd37f01aa
Steve Farrell <sfarrell@cern.ch>
2017-08-21 23:17:16 +0200

    Migrated CaloG4Sim user actions to G4 base classes
    Moved CalibrationDefaultProcessing to G4 user action base classes and
    moved the tool to the corresponding tool interfaces.
    
    Cleaned up some whitespace as well.
    
    
    Former-commit-id: ee95ba48c972f52a540766c5582ee5729c8c0c02

--- Calorimeter/CaloG4Sim/CaloG4Sim/CalibrationDefaultProcessingTool.h
+++ Calorimeter/CaloG4Sim/CaloG4Sim/CalibrationDefaultProcessingTool.h
@@ -2,37 +2,37 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef CALOG4SIM_G4UA_CALOG4_CALIBRATIONDEFAULTPROCESSINGTOOL_H 
-#define CALOG4SIM_G4UA_CALOG4_CALIBRATIONDEFAULTPROCESSINGTOOL_H 
-#include "G4AtlasInterfaces/IBeginEventActionTool.h"
-#include "G4AtlasInterfaces/ISteppingActionTool.h"
+#ifndef CALOG4SIM_G4UA_CALOG4_CALIBRATIONDEFAULTPROCESSINGTOOL_H
+#define CALOG4SIM_G4UA_CALOG4_CALIBRATIONDEFAULTPROCESSINGTOOL_H
+#include "G4AtlasInterfaces/IG4EventActionTool.h"
+#include "G4AtlasInterfaces/IG4SteppingActionTool.h"
 #include "G4AtlasTools/ActionToolBase.h"
 #include "CaloG4Sim/CalibrationDefaultProcessing.h"
 
-namespace G4UA{ 
-  
+namespace G4UA{
+
   namespace CaloG4 {
-    
+
     /// @class CalibrationDefaultProcessingTool
     /// @brief Tool which manages  CalibrationDefaultProcessing
     ///
     /// @author Andrea Di Simone
     ///
 
-    class CalibrationDefaultProcessingTool: 
-    public ActionToolBase<CalibrationDefaultProcessing>,
-      public IBeginEventActionTool,  public ISteppingActionTool
+    class CalibrationDefaultProcessingTool:
+      public ActionToolBase<CalibrationDefaultProcessing>,
+      public IG4EventActionTool, public IG4SteppingActionTool
     {
-      
+
     public:
       /// Standard constructor
       CalibrationDefaultProcessingTool(const std::string& type, const std::string& name,const IInterface* parent);
       /// Retrieve the BoE action
-      virtual IBeginEventAction* getBeginEventAction() override final 
-      { return static_cast<IBeginEventAction*>( getAction() ); }
+      virtual G4UserEventAction* getEventAction() override final
+      { return static_cast<G4UserEventAction*>( getAction() ); }
       /// Retrieve the stepping action
-      virtual ISteppingAction* getSteppingAction() override final 
-      { return static_cast<ISteppingAction*>( getAction() ); }
+      virtual G4UserSteppingAction* getSteppingAction() override final
+      { return static_cast<G4UserSteppingAction*>( getAction() ); }
       /// Gaudi interface management
       virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface) override;
     protected:
@@ -42,8 +42,8 @@ namespace G4UA{
       /// python configuration
       CalibrationDefaultProcessing::Config m_config;
     }; // class CalibrationDefaultProcessingTool
-    
+
   }// namespace CaloG4
-  
-} // namespace G4UA 
+
+} // namespace G4UA
 #endif
