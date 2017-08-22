d9edca9cb3e9ba6e9a464952a0f85a14f684ccf7
Steve Farrell <sfarrell@cern.ch>
2017-08-22 20:41:11 +0200

    migrate G4DebuggingTools JOs to new user actions
    Former-commit-id: 6759a051dae84620f730de56ce754636a6fe4323

--- Simulation/G4Utilities/G4DebuggingTools/share/EnergyConservation_options.py
+++ Simulation/G4Utilities/G4DebuggingTools/share/EnergyConservation_options.py
@@ -1,4 +1,4 @@
 from G4AtlasApps.SimFlags import simFlags
 # new MT actions. Note that this will only work with one thread.
-simFlags.OptionalUserActionList.addAction('G4UA::EnergyConservationTestTool',['PreTracking','Step','PostTracking','EndOfEvent'])
+simFlags.OptionalUserActionList.addAction('G4UA::EnergyConservationTestTool',['Tracking','Step','Event'])
 
--- Simulation/G4Utilities/G4DebuggingTools/share/Geant4SetupChecker_options.py
+++ Simulation/G4Utilities/G4DebuggingTools/share/Geant4SetupChecker_options.py
@@ -1,6 +1,6 @@
 # Job options to configure the Geant4 setup checker tool
 from G4AtlasApps.SimFlags import simFlags
-simFlags.OptionalUserActionList.addAction('G4UA::Geant4SetupCheckerTool',['BeginOfRun'])
+simFlags.OptionalUserActionList.addAction('G4UA::Geant4SetupCheckerTool',['Run'])
 
 # This flag disables checking so that you can write your own reference file
 #simFlags.UserActionConfig.addConfig('G4UA::Geant4SetupCheckerTool','RunTest',False)
