ef55d4c38dcc3130b169ee81c8b35f05ca58dbe7
Steve Farrell <sfarrell@cern.ch>
2017-08-23 00:23:47 +0200

    update SimulationJobOptions for new user actions
    Former-commit-id: 20f4873062a2793f688dcf63af317030534c899d

--- Simulation/SimulationJobOptions/share/cavern/preInclude.G4ReadCavern75.py
+++ Simulation/SimulationJobOptions/share/cavern/preInclude.G4ReadCavern75.py
@@ -1,10 +1,6 @@
 # Modify the hit wrapping action to wrap on 75 ns
 
-try:
-    from G4AtlasServices.G4AtlasUserActionConfig import UAStore
-except ImportError:
-    from G4AtlasServices.UserActionStore import UAStore
-from AthenaCommon.CfgGetter import getPublicTool
-
-getPublicTool('HitWrapper',tryDefaultConfigurable=True).WrapTime=75
-UAStore.addAction('HitWrapper',['EndOfEvent']) 
+from G4AtlasApps.SimFlags import simFlags
+# new MT actions. Note that this will only work with one thread.
+simFlags.OptionalUserActionList.addAction('G4UA::HitWrapperTool',['Event'])
+simFlags.UserActionConfig.addConfig('G4UA::HitWrapperTool','WrapTime',75)
