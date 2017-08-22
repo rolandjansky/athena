d8e6b607e0e9fd4482ff459ef6fdd323b89306ee
Steve Farrell <sfarrell@cern.ch>
2017-08-23 00:18:25 +0200

    update ISF_FastCaloSimParametrization JOs for new user actions
    Former-commit-id: 5e28a6dee512b1ead561b46c2ae9e50d301ef15c

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_SimPreInclude.py
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_SimPreInclude.py
@@ -1,3 +1,3 @@
 from G4AtlasApps.SimFlags import simFlags
-simFlags.OptionalUserActionList.addAction('G4UA::FastCaloSimParamActionTool',['BeginOfEvent','EndOfEvent','BeginOfRun','EndOfRun','Step'])
+simFlags.OptionalUserActionList.addAction('G4UA::FastCaloSimParamActionTool',['Event','Run','Step'])
 simFlags.UserActionConfig.addConfig('G4UA::FastCaloSimParamActionTool','shift_lar_subhit','1')
