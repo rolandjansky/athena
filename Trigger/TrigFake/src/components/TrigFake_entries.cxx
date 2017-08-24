b924ca5fb64579bb0347fe89cd469d2c32293fae
Tomasz Bold <tomasz.bold@gmail.com>
2017-08-24 18:34:39 +0000

    Testing of Lvl1Converter as preparation for OR seeding fix
    Former-commit-id: fc0c707c71a923e9c426e31e7d2d2834b20242b4

--- Trigger/TrigFake/src/components/TrigFake_entries.cxx
+++ Trigger/TrigFake/src/components/TrigFake_entries.cxx
@@ -1,16 +1,17 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
 #include "TrigFake/FakeLvl1RoIatFixedEtaPhi.h"
 #include "TrigFake/FakeLvl1MultipleRoIsatFixedEtaPhi.h"
 //#include "TrigFake/FakeLvl1ConversionTestBeam.h"
 #include "TrigFake/FakeLvl1RoIfromKine.h"
 #include "TrigFake/ZVertexFromKine.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
+#include "../FakeRoIB.h"
 
 DECLARE_TOOL_FACTORY( FakeLvl1RoIfromKine )
   //DECLARE_ALGORITHM_FACTORY( FakeLvl1ConversionTestBeam )
 DECLARE_TOOL_FACTORY( FakeLvl1RoIatFixedEtaPhi )
 DECLARE_TOOL_FACTORY( FakeLvl1MultipleRoIsatFixedEtaPhi )
 DECLARE_ALGORITHM_FACTORY( ZVertexFromKine )
+DECLARE_ALGORITHM_FACTORY( FakeRoIB )
 
 DECLARE_FACTORY_ENTRIES(TrigFake) {
     DECLARE_TOOL( FakeLvl1RoIfromKine )
@@ -18,4 +19,5 @@ DECLARE_FACTORY_ENTRIES(TrigFake) {
     DECLARE_TOOL( FakeLvl1RoIatFixedEtaPhi )
     DECLARE_TOOL( FakeLvl1MultipleRoIsatFixedEtaPhi )
     DECLARE_ALGORITHM( ZVertexFromKine )
+    DECLARE_ALGORITHM( FakeRoIB )
 }
