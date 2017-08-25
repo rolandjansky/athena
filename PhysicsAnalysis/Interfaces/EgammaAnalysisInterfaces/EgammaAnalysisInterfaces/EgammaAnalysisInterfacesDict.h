cf05763af59c2a88f312a6619ef11a1e08a12e1d
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2017-08-25 18:56:42 +0200

    start working on moving to a common Interfaces package for analysis
    Former-commit-id: cc2be0135c9eb288abe9f07239671b41d4314153

---
+++ PhysicsAnalysis/Interfaces/EgammaAnalysisInterfaces/EgammaAnalysisInterfaces/EgammaAnalysisInterfacesDict.h
@@ -0,0 +1,16 @@
+#ifndef __EGAMMAANALYSISINTERRFACESDICT__
+#define __EGAMMAANALYSISINTERRFACESDICT__
+
+#include "EgammaAnalysisInterfaces/IAsgDeadHVCellRemovalTool.h"
+#include "EgammaAnalysisInterfaces/IAsgEGammaIsEMSelector.h"
+#include "EgammaAnalysisInterfaces/IAsgElectronEfficiencyCorrectionTool.h"
+#include "EgammaAnalysisInterfaces/IAsgElectronIsEMSelector.h"
+#include "EgammaAnalysisInterfaces/IAsgElectronLikelihoodTool.h"
+#include "EgammaAnalysisInterfaces/IAsgElectronMultiLeptonSelector.h"
+#include "EgammaAnalysisInterfaces/IAsgForwardElectronIsEMSelector.h"
+#include "EgammaAnalysisInterfaces/IAsgPhotonIsEMSelector.h"
+#include "EgammaAnalysisInterfaces/IEGammaAmbiguityTool.h"
+#include "EgammaAnalysisInterfaces/IEgammaCalibrationAndSmearingTool.h"
+#include "EgammaAnalysisInterfaces/IElectronPhotonShowerShapeFudgeTool.h"
+
+#endif
---
+++ PhysicsAnalysis/Interfaces/EgammaAnalysisInterfaces/EgammaAnalysisInterfaces/IAsgForwardElectronIsEMSelector.h
@@ -0,0 +1,87 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Dear emacs, this is -*-c++-*-
+
+#ifndef EGAMMAANALYSISINTERFACES__IASGFORWARDELECTRONISEMSELECTOR__
+#define EGAMMAANALYSISINTERFACES__IASGFORWARDELECTRONISEMSELECTOR__
+
+/**
+   @class IAsgForwardElectronIsEMSelector
+   @brief Interface to tool to select electrons
+
+   @author Jovan Mitrevski (UCSC) Karsten Koeneke (CERN)
+   @date   Dec 2011 - Fab 2012
+
+   11-MAR-2014, convert to ASGTool
+*/
+
+// Include the interfaces
+#include "PATCore/IAsgSelectionTool.h"
+#include "EgammaAnalysisInterfaces/IAsgEGammaIsEMSelector.h"
+// Include the return object and the underlying ROOT tool
+#include "EgammaAnalysisInterfaces/egammaPIDdefs.h"
+
+// Forward declarations
+namespace Root{
+  class TAccept;
+}
+#include "xAODEgamma/ElectronFwd.h"
+#include "xAODEgamma/PhotonFwd.h"
+#include "xAODEgamma/EgammaFwd.h"
+
+class IAsgForwardElectronIsEMSelector : virtual public IAsgEGammaIsEMSelector
+{
+
+  ASG_TOOL_INTERFACE(IAsgForwardElectronIsEMSelector)
+  
+  public:
+
+  /**Virtual Destructor*/
+  virtual ~IAsgForwardElectronIsEMSelector() {};
+
+  /** Accept with generic interface */
+  virtual const Root::TAccept& accept( const xAOD::IParticle* part ) const = 0;
+
+  /** Accept with generic interface */
+  virtual const Root::TAccept& accept( const xAOD::IParticle& part ) const = 0;
+  
+  /** Accept with Egamma objects */
+  virtual const Root::TAccept& accept( const xAOD::Egamma* part) const = 0;
+
+  /** Accept with Egamma objects */
+  virtual const Root::TAccept& accept( const xAOD::Egamma& part) const = 0;
+
+  /** Accept with Photon objects */
+  virtual const Root::TAccept& accept( const xAOD::Photon* part ) const = 0;
+
+  /** Accept with Photon objects */
+  virtual const Root::TAccept& accept( const xAOD::Photon& part ) const = 0;
+
+  /** Accept with Electron objects */
+  virtual const Root::TAccept& accept( const xAOD::Electron* part ) const = 0;
+
+  /** Accept with Electron objects */
+  virtual const Root::TAccept& accept( const xAOD::Electron& part ) const = 0;
+
+
+  ///////////////Expert methods //////////////////////////////////////
+
+  
+  //The main execute method
+  virtual StatusCode execute(const xAOD::Egamma* eg) const =0;
+
+  //////////////////////////////////////////////////////////////
+  virtual unsigned int IsemValue() const = 0;
+  
+  /** Method to get the operating point */
+  virtual std::string getOperatingPointName( ) const =0;
+
+
+
+}; // End: class definition
+
+
+#endif
+
