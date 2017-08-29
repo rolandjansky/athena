3777c093dcd18d59c63fe1661b388c52495e1cb1
Grzegorz Gach <grzegorz.gach@cern.ch>
2017-08-29 13:44:41 +0200

    Refactor AFP_Bytestream converter and add xAOD for AFP time-of-flight.
    AFP_Bytestream converter was refactored to deal with two kinds of data
    silicon and time-of-flight. Additionally, the code was cleaned and
    unused classes and variables deleted. ATLAS naming convetion was
    implemented. The following classes were replaced:
    - AFP_RawDataContainer -> AFP_RawContainer
    - AFP_RawDataCollection -> AFP_SiRawCollection, AFP_ToFRawCollection
    - AFP_RawData -> AFP_SiRawData, AFP_ToFRawData
    
    xAOD classes representing AFP time-of-flight (AFPToFHit) were added to xAODForward.
    
    
    Former-commit-id: b1dfe348cd8a06a5f00d2a0cc68009187c0047fc

--- ForwardDetectors/AFP/AFP_Monitoring/src/AFPTechnicalLayerMonitor.cxx
+++ ForwardDetectors/AFP/AFP_Monitoring/src/AFPTechnicalLayerMonitor.cxx
@@ -13,11 +13,8 @@
 AFPTechnicalLayerMonitor::AFPTechnicalLayerMonitor(const int pixelLayerID, const int stationID)
    : m_pixelLayerID (pixelLayerID)
    ,m_stationID (stationID)
-   ,m_lvl1(nullptr)
-   ,m_lvl1Seed(nullptr)
 {
   // make sure that in first event time-over threshold can be overwriten
-  m_seed.Set_ToT(0);
 }
 
 
@@ -32,46 +29,21 @@ void AFPTechnicalLayerMonitor::bookHistograms(ManagedMonitorToolBase* toolToStor
   // ********** Per lumi block **********
   ManagedMonitorToolBase::MonGroup managed_booking_lumiBlock( toolToStoreHistograms, histsDirName.data(), toolToStoreHistograms->lumiBlock);   // to re-booked every luminosity block
   
-  // ----- hit map -----
-  // create histogram name and title
-  std::string lvl1Name = makeHistName("h_lvl1");
-  std::string lvl1Title = makeHistName("lvl1");
-
-  // create and register histogram
-  m_lvl1  = TH1F_LW::create(lvl1Name.data(),
-			    lvl1Title.data(),
-			    16, -0.5, 15.5);
-
-  toolToStoreHistograms->regHist(m_lvl1, managed_booking_lumiBlock ).ignore();
-
-
-  std::string lvl1SeedName = makeHistName("h_lvl1Seed");
-  std::string lvl1SeedTitle = makeHistName("lvl1 for hit with largest time over threshold");
-  m_lvl1Seed  = TH1F_LW::create(lvl1SeedName.data(),
-			    lvl1SeedTitle.data(),
-			    16, -0.5, 15.5);
-
-  toolToStoreHistograms->regHist(m_lvl1Seed, managed_booking_lumiBlock ).ignore();
-
 }
 
 
-void AFPTechnicalLayerMonitor::fillHistograms(const AFP_RawData& hit)
+void AFPTechnicalLayerMonitor::fillHistograms(const AFP_SiRawData& /*hit*/)
 {
   // fill histograms
-  m_lvl1->Fill(hit.Get_lvl1());
 
-  // look for hit with largest time over threshold
-  if (hit.Get_ToT() >= m_seed.Get_ToT())
-    m_seed = hit;
 }
 
 void AFPTechnicalLayerMonitor::eventEnd()
 {
-  m_lvl1Seed->Fill(m_seed.Get_lvl1());
+
 }
 
-void AFPTechnicalLayerMonitor::endOfLumiBlock(ManagedMonitorToolBase* toolToStoreHistograms)
+void AFPTechnicalLayerMonitor::endOfLumiBlock(ManagedMonitorToolBase* /* toolToStoreHistograms */)
 {
 
 }
---
+++ ForwardDetectors/AFP/AFP_RawEv/AFP_RawEv/AFP_ToFRawData.h
@@ -0,0 +1,65 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef AFP_TOFRAWDATA_H
+#define AFP_TOFRAWDATA_H
+
+#include "AFP_RawDataCommonHead.h"
+
+#include <stdint.h>
+
+/// @brief Class representing data record for time-of-flight detectors.
+///
+/// Documentation of the format is available in the twiki page:
+/// https://twiki.cern.ch/twiki/bin/view/Atlas/AFPHPTDC#Data_Encoding
+class AFP_ToFRawData : public AFP_RawDataCommonHead
+{
+public:
+  /// @copydoc AFP_ToFRawData::m_header
+  uint16_t header() const {return m_header;}
+  void setHeader(const uint16_t header) {m_header = header;}
+
+  /// @copydoc AFP_ToFRawData::m_edge
+  uint16_t edge() const {return m_edge;}
+  void setEdge(const uint16_t edge) {m_edge = edge;}
+
+  /// @copydoc AFP_ToFRawData::m_channel
+  uint16_t channel() const {return m_channel;}
+  void setChannel(const uint16_t channel) {m_channel = channel;}
+  
+  /// @copydoc AFP_ToFRawData::m_time
+  uint32_t time() const {return m_time;}
+  void setTime(const uint32_t time) {m_time = time;}
+
+private:
+  /// @brief Value of 9-11 most significant bits
+  ///
+  /// In the following record: `xxxx xxxx hhhx xxxx xxxx xxxx xxxx xxxx`
+  /// it means bits marked with `h`. Contains information about time-of-flight data type or error:
+  /// - value 0 means TDC header
+  /// - value 2 means hit measurement
+  /// - value 3 is per event error 
+  uint16_t m_header;
+
+  /// @brief Value of 12 most significant bit
+  ///
+  /// In the following record: `xxxx xxxx xxxE xxxx xxxx xxxx xxxx xxxx`
+  /// it means bits marked with `E`. Contains information about signal
+  /// beginning or end.
+  uint16_t m_edge;
+
+  /// @brief Value of 13-17 most significant bits
+  ///
+  /// In the following record: `xxxx xxxx xxxx CCCC xxxx xxxx xxxx xxxx`
+  /// it means bits marked with `C`. Contains information about
+  /// channel in HPTDC of the time measurement.
+  uint16_t m_channel;
+  
+  /// @brief Value of 10 least significant bits
+  ///
+  /// In the following record: `xxxx xxxx xxxx xxxx xxxx xxtt tttt tttt`
+  /// it means bits marked with `t`. Contains information about time.
+  uint32_t m_time;
+};
+#endif
