b8a692fcdad785c84137e803d6588c006b17c69e
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2017-09-01 18:12:23 +0200

    Cya MuonTPTools
    Former-commit-id: 1635a959dd65fd751e334193b55c23d8dc9246ed

--- PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonPtCalibNtupleMaker/src/components/MuonPtCalibNtupleMaker_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-#include "MuonPtCalibNtupleMaker/CalibMuonsNtupleMaker.h"
-#include "MuonPtCalibNtupleMaker/CalibMuonsSelectorTool.h"
-
-
-DECLARE_ALGORITHM_FACTORY( CalibMuonsNtupleMaker )
-DECLARE_TOOL_FACTORY( CalibMuonsSelectorTool )
-
