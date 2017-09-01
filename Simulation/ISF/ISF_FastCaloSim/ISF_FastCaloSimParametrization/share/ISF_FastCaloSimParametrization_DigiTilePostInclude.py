4f71190432cae5f00649e7f63c0242ea6893084a
Petr Jacka <petr.jacka@cern.ch>
2017-09-01 14:47:42 +0000

    Reverted changes
    Former-commit-id: b4fb67d6857d5d11d39e77a831ce2eac2474d066

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_DigiTilePostInclude.py
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_DigiTilePostInclude.py
@@ -1,6 +1,5 @@
 from AthenaCommon.AppMgr import ServiceMgr
-ServiceMgr.MessageSvc.OutputLevel= VERBOSE
-#rel 19 is configuring this via a tool!!
+
 ToolSvc.TileHitVecToCntTool.HitTimeFlag=1 
 
 ServiceMgr.TileInfoLoader.NPhElec=0
@@ -9,13 +8,16 @@ ServiceMgr.TileInfoLoader.NPhElecC=0
 ServiceMgr.TileInfoLoader.NPhElecD=0
 ServiceMgr.TileInfoLoader.NPhElecE=0
 
+topSequence.TileDigitsMaker.IntegerDigits=True
+
 ###########
 print ServiceMgr.TileInfoLoader
 print topSequence.TileDigitsMaker
 print ToolSvc.TileHitVecToCntTool
-topSequence.TileDigitsMaker.OutputLevel=1
-topSequence.TileDigitsMaker.IntegerDigits=True
-TileHitVecToCntTool.OutputLevel=1
-ToolSvc.TileRawChannelBuilderOptATLAS.OutputLevel=1
+#topSequence.TileDigitsMaker.OutputLevel=1
+#topSequence.TileDigitsMaker.IntegerDigits=True
+#TileHitVecToCntTool.OutputLevel=1
+#ToolSvc.TileRawChannelBuilderOptATLAS.OutputLevel=1
 ########### 
+
 
