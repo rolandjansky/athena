d5e99a8e9f2e3383d6800fae5cd6d3a5f04ec691
James Richard Catmore <james.catmore@cern.ch>
2017-09-01 17:40:12 +0200

    Sweep of xAODBTaggingCnv-00-00-05 into AthDerivation 21.2
    The version of xAODBTaggingCnv currently in 21.2 is several versions behind the one in SVN (00-00-02 vs 00-00-05). This is a sweep of the latest version from SVN into 21.2.
    
    
    Former-commit-id: 09a71949f3a87d192a16a0b5803a9d4f3cccaaca

--- Event/xAOD/xAODBTaggingCnv/src/components/xAODBTaggingCnv_entries.cxx
+++ Event/xAOD/xAODBTaggingCnv/src/components/xAODBTaggingCnv_entries.cxx
@@ -6,6 +6,7 @@
 #include "../BTaggingReader.h"
 #include "../TrigBjetCnvAlg.h"
 #include "../TrigBjetCnvTool.h"
+#include "../BTaggingAuxFixerAlg.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, BTaggingCnvAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODReader, BTaggingReader )
@@ -13,10 +14,14 @@ DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODReader, BTaggingReader )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, TrigBjetCnvAlg )
 DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TrigBjetCnvTool )
 
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, BTaggingAuxFixerAlg )
+
 DECLARE_FACTORY_ENTRIES( xAODBTaggingCnv ) {
   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, BTaggingCnvAlg )
   DECLARE_NAMESPACE_ALGORITHM( xAODReader, BTaggingReader )
 
   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, TrigBJetCnvAlg )
   DECLARE_NAMESPACE_TOOL( xAODMaker, TrigBJetCnvTool )
+
+  DECLARE_NAMESPACE_ALGORITHM( xAODMaker, BTaggingAuxFixerAlg )
 }
