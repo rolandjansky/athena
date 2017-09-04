57ee899be49bad8ef19f759799543cdb4d28edba
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-09-04 11:17:27 +0200

    Removed the useless SVN $Id$ tag from the unit test source file.
    Former-commit-id: d0521d4cd64bd36c52dee7d6250adb099ad842aa

--- Event/xAOD/xAODTrigger/test/ut_xaodtrigger_bytestreamauxcontainer_v1_test.cxx
+++ Event/xAOD/xAODTrigger/test/ut_xaodtrigger_bytestreamauxcontainer_v1_test.cxx
@@ -2,7 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-// $Id$
 /**
  * @file xAODRootAccess/test/ut_xaodtrigger_bytestreamauxcontainer_v1_test.cxx
  * @author scott snyder <snyder@bnl.gov>
