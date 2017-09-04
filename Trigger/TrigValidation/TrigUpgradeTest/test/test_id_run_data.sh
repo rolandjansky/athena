af145fb1f30017ebe9d5d2b65c2ddece42db72a1
Stewart Martin-Haugh <smh@cern.ch>
2017-09-04 18:52:19 +0200

    Update tests to use common testHLT_MT, datasets on CVMFS
    Former-commit-id: bd5311d6e8fa6f2f3cd03b1530ccad20386395f5

--- Trigger/TrigValidation/TrigUpgradeTest/test/test_id_run_data.sh
+++ Trigger/TrigValidation/TrigUpgradeTest/test/test_id_run_data.sh
@@ -2,4 +2,4 @@
 # art-type: build
 # art-ci: master
 
-athena.py --threads=1 TrigUpgradeTest/ID_RawDataMT_Trigger.py
+athena.py --threads=1 -c 'doCalo=False;EvtMax=10;FilesInput="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"' TrigUpgradeTest/IDCalo.py
