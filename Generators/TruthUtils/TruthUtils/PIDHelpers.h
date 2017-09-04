6334b3f2e1d3ef48ae9add7b0188e6936feffb4c
Zach Marshall <ZLMarshall@lbl.gov>
2017-09-04 04:46:47 +0200

    Proper namespacing
    Former-commit-id: 38f8cc2ff10a7c68aa7dc09a54033249cad959ec

--- Generators/TruthUtils/TruthUtils/PIDHelpers.h
+++ Generators/TruthUtils/TruthUtils/PIDHelpers.h
@@ -31,93 +31,101 @@ namespace MC {
     return !(PID::isStrongInteracting(pid) || PID::isEMInteracting(pid));
   }
 
-  /// Is this a Vector-like Quark?
-  inline bool isVLQ(int pid){
-    // X, T, B, Y in that order
-    return in_closed_range(abs(pid),6000005,6000008);
-  }
-
-  /// Is this from a Type-III See-Saw model
-  inline bool isTypeIIISeeSaw(int pid){
-    // Even numbers between 8000012 and 8000020: v1, v2, v3, tr0, tr+
-    return _digit(n,pid) == 8 &&
-           in_closed_range(abs(pid),8000012,8000020) &&
-           pid%2 == 0;
-  }
-
-  /// Is this a vector triplet particle?
-  inline bool isVectorTriplet(int pid){
-    // Heavy Vector Triplet Z' and W'
-    return in_closed_range(abs(pid),9000001,9000002);
-  }
-
-  /// Is this a doubly-charged Higgs boson?
-  inline bool isDoublyChargedHiggs(int pid){
-    // H_L++ and H_R++ (negative for --)
-    return in_closed_range(abs(pid),9900041,9900042);
-  }
-
-  /// Is this from a dark sector model?
-  inline bool isDarkSector(int pid){
-    // Hidden scalar and hidden photon
-    if (in_closed_range(abs(pid),700021,700022)) return true;
-    if (!_digit(n,pid) == 3) return false;
-    int fundId = _fundamentalID(pid);
-    // Z_Dark, H_Dark, N_Dark1, N_Dark2
-    return fundId == 1 || fundId == 5 ||
-           fundId == 15 || fundId = 16;
-  }
-
-  /// Is this from a hiddel valley model?
-  inline bool isHiddenValley(int pid){
-    if (_digit(n,pid) != 4) return false;
-    if (_digit(nr,pid) != 9) return false;
-    if (_digit(nl,pid) != 0) return false;
-    return true;
-  }
-
-  /// Is this a heavy neutrino?
-  inline bool isHeavyNeutrino(int pid){
-    if (_digit(n,pid) != 9) return false;
-    if (_digit(nr,pid) != 9) return false;
-    return isNeutrino( _fundamentalID(pid) );
-  }
-
-  /// @brief Is this an exotic particle?
-  inline bool isExotic(int pid){
-    return in_closed_range(abs(pid),40,80);
-  }
-
-  /// @brief Is this a BSM boson?
-  inline bool isBSMBoson(int pid){
-    return in_closed_range(abs(pid),32,39);
-  }
-
-  /// @brief is this a fourth generation particle?
-  inline bool isFourthGen(int pid){
-    return abs(pid)==7 || abs(pid)==8 || // Quarks
-           abs(pid)==17 || abs(pid)==18; // Leptons
-
-  /// @brief Is this a BSM particle?
-  inline bool isBSM(int pid) {
-    return isVLQ(pid) || isTypeIIISeeSaw(pid) || isVectorTriplet(pid) ||
-           isDoublyChargedHiggs(pid) || isDarkSector(pid) || isHiddenValley(pid) ||
-           isHeavyNeutrino(pid) || isExotic(pid) || isBSMBoson(pid) ||
-           isFourthGen(pid) ||
-           abs(pid)==5100039 || // Special case: Gravitino in Pythia
-           abs(pid)==43 || in_closed_range(apid, 601,606) || // Extra lepto-quark UFOs
-           abs(pid)==1999 || // Coloron (rho)
-           abs(pid)==6666 || // Pseudo Nambu-Goldstone ED model Higgs particle
-           abs(pid)==9000006 || // New 'phi' particle from cosmo UFO model
-           abs(pid)==5000001 || // UFO Z'
-           isBSM_MCUtils(pid);
-  }
-
   /// @brief Identify if the particle with given PDG ID would produce ID tracks but not shower in the detector if stable
   inline bool isChargedNonShowering(int pid) {
     if (PID::isMuon(pid)) return true;
     if (PID::isSUSY(pid)) return true; //(meta)stable charginos, R-hadrons etc
-     return false;
-   }
+    return false;
+  }
 
-}
+  namespace PID {
+
+    using namespace MCUtils::PID;
+    using namespace HEPUtils;
+
+    /// Is this a Vector-like Quark?
+    inline bool isVLQ(int pid){
+      // X, T, B, Y in that order
+      return in_closed_range(abs(pid),6000005,6000008);
+    }
+
+    /// Is this from a Type-III See-Saw model
+    inline bool isTypeIIISeeSaw(int pid){
+      // Even numbers between 8000012 and 8000020: v1, v2, v3, tr0, tr+
+      return _digit(n,pid) == 8 &&
+             in_closed_range(abs(pid),8000012,8000020) &&
+             pid%2 == 0;
+    }
+
+    /// Is this a vector triplet particle?
+    inline bool isVectorTriplet(int pid){
+      // Heavy Vector Triplet Z' and W'
+      return in_closed_range(abs(pid),9000001,9000002);
+    }
+
+    /// Is this a doubly-charged Higgs boson?
+    inline bool isDoublyChargedHiggs(int pid){
+      // H_L++ and H_R++ (negative for --)
+      return in_closed_range(abs(pid),9900041,9900042);
+    }
+
+    /// Is this from a dark sector model?
+    inline bool isDarkSector(int pid){
+      // Hidden scalar and hidden photon
+      if (in_closed_range(abs(pid),700021,700022)) return true;
+      if (_digit(n,pid) != 3) return false;
+      int fundId = _fundamentalID(pid);
+      // Z_Dark, H_Dark, N_Dark1, N_Dark2
+      return fundId == 1 || fundId == 5 ||
+             fundId == 15 || fundId == 16;
+    }
+
+    /// Is this from a hiddel valley model?
+    inline bool isHiddenValley(int pid){
+      if (_digit(n,pid) != 4) return false;
+      if (_digit(nr,pid) != 9) return false;
+      if (_digit(nl,pid) != 0) return false;
+      return true;
+    }
+
+    /// Is this a heavy neutrino?
+    inline bool isHeavyNeutrino(int pid){
+      if (_digit(n,pid) != 9) return false;
+      if (_digit(nr,pid) != 9) return false;
+      return isNeutrino( _fundamentalID(pid) );
+    }
+
+    /// @brief Is this an exotic particle?
+    inline bool isExotic(int pid){
+      return in_closed_range(abs(pid),40,80);
+    }
+
+    /// @brief Is this a BSM boson?
+    inline bool isBSMBoson(int pid){
+      return in_closed_range(abs(pid),32,39);
+    }
+
+    /// @brief is this a fourth generation particle?
+    inline bool isFourthGen(int pid){
+      return abs(pid)==7 || abs(pid)==8 || // Quarks
+             abs(pid)==17 || abs(pid)==18; // Leptons
+    }
+
+    /// @brief Is this a BSM particle?
+    inline bool isBSM(int pid) {
+      return isVLQ(pid) || isTypeIIISeeSaw(pid) || isVectorTriplet(pid) ||
+             isDoublyChargedHiggs(pid) || isDarkSector(pid) || isHiddenValley(pid) ||
+             isHeavyNeutrino(pid) || isExotic(pid) || isBSMBoson(pid) ||
+             isFourthGen(pid) ||
+             abs(pid)==5100039 || // Special case: Gravitino in Pythia
+             abs(pid)==43 || in_closed_range(abs(pid), 601,606) || // Extra lepto-quark UFOs
+             abs(pid)==1999 || // Coloron (rho)
+             abs(pid)==6666 || // Pseudo Nambu-Goldstone ED model Higgs particle
+             abs(pid)==9000006 || // New 'phi' particle from cosmo UFO model
+             abs(pid)==5000001 || // UFO Z'
+             isBSM_MCUtils(pid);
+    }
+
+  } // End of PID namespace
+} // End of MC namespace
+
