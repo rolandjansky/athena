2c1def43b3f6964e5ea87cd136b4679ff2de082d
Alexander Solodkov <Sanya.Solodkov@cern.ch>
2017-09-07 21:13:33 +0200

    backporting from upstream/master
    Former-commit-id: 1ebe09d6348d7e9690228ab0037306f6f0a02a69

--- TileCalorimeter/TileCalib/TileCalibBlobPython/share/TileCalibBlobPython_writeDefaults.py
+++ TileCalorimeter/TileCalib/TileCalibBlobPython/share/TileCalibBlobPython_writeDefaults.py
@@ -1,4 +1,7 @@
 #!/bin/env python
+
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+#
 # TileCalibBlobPython_writeDefaults.py
 # Nils Gollub <nils.gollub@cern.ch>, 2007-11-26
 # change: Yuri Smirnov <iouri.smirnov@cern.ch>, 2014-12-24
