445a5b338ae104d0fa1f3d88c2c84e44d02cc522
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2017-09-12 16:51:45 +0200

    Since I moved all the clients, let remove the old
    Former-commit-id: 34ec863126f5e5ef336fd4213f80b37490459152

--- PhysicsAnalysis/ElectronPhotonID/ElectronPhotonSelectorTools/ElectronPhotonSelectorTools/PhotonSelectorHelpers.h
+++ PhysicsAnalysis/ElectronPhotonID/ElectronPhotonSelectorTools/ElectronPhotonSelectorTools/PhotonSelectorHelpers.h
@@ -5,24 +5,6 @@
 #ifndef __PHOTONSELECTORHELPERS__
 #define __PHOTONSELECTORHELPERS__
 
-#pragma message "This code  is deprecated, consider moving to EgammaAnalysisHelpers/PhotonHelpers.h instead"
-
-// Atlas includes
-#include "xAODEgamma/Photon.h"
-#include <cstddef>
-
-namespace PhotonSelectorHelpers{
-  bool passOQquality(const xAOD::Photon *ph){
-    static const asg::AsgMessaging msg("PhotonSelectorHelpers");
-    msg.msg(MSG::WARNING) << "This function is  deprecated, move to EgammaAnalysisHelpers/PhotonHelpers.h " << endmsg;
-    return 0;
-  }
-
-  bool passOQqualitydelayed(const xAOD::Photon *ph){
-    static const asg::AsgMessaging msg("PhotonSelectorHelpers");
-    msg.msg(MSG::WARNING) << "This function is  deprecated, moveto EgammaAnalysisHelpers/PhotonHelpers.h instead" << endmsg;
-    return 0; 
-  }
-}
+#pragma message "This functionality of this header has moved to  EgammaAnalysisHelpers/PhotonHelpers.h instead"
 
 #endif
