1d2ebf8ca423a270fdb5a76f7e7dfad0079f7d32
James Richard Catmore <james.catmore@cern.ch>
2017-09-13 22:25:42 +0200

    Adjustments to DerivationFrameworkRTT
    This MR adds extra metadata to the EGAM1 script to describe the output file name, and drops an incorrectly adding CI line
    
    
    Former-commit-id: 3bf9fd016836f46e140760f6a909b604a14ab7c4

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data17EGAM1.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data17EGAM1.sh
@@ -2,6 +2,6 @@
 
 # art-description: DAOD building EGAM1 data17_13TeV
 # art-type: grid
-# art-ci: 21.2
+# art-output: *.pool.root
 
 Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/data17_13TeV.00327342.physics_Main.merge.AOD.f838_m1824._lb0300._0001.1 --outputDAODFile art.pool.root --reductionConf EGAM1 --maxEvents 5000
