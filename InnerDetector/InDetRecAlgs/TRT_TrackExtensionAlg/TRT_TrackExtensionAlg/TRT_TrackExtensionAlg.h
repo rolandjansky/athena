17b7067b72e3f02db312aa9b6ada6409b60f0f6e
Nikita Smirnov <nikita.smirnov@cern.ch>
2017-09-14 17:42:52 +0300

    Moved to using VarHandleKeys in TRT_TrackExtensionAlg
    Former-commit-id: 3f23b7015bcb9e2e57015af98b8e453b1993c7e1

--- InnerDetector/InDetRecAlgs/TRT_TrackExtensionAlg/TRT_TrackExtensionAlg/TRT_TrackExtensionAlg.h
+++ InnerDetector/InDetRecAlgs/TRT_TrackExtensionAlg/TRT_TrackExtensionAlg/TRT_TrackExtensionAlg.h
@@ -20,60 +20,70 @@
 #include "GaudiKernel/ToolHandle.h"
 #include "InDetRecToolInterfaces/ITRT_TrackExtensionTool.h"
 
+// Store Gate handle keys
+#include "StoreGate/ReadHandleKey.h"
+#include "StoreGate/WriteHandleKey.h"
+
+#include "TrkTrack/TrackExtensionMap.h"
+#include "TrkTrack/TrackCollection.h"
+
 namespace InDet {
 
-  class TRT_TrackExtensionAlg : public AthAlgorithm 
-    {
-    
-      ///////////////////////////////////////////////////////////////////
-      // Public methods:
-      ///////////////////////////////////////////////////////////////////
-      
-    public:
-      
-      ///////////////////////////////////////////////////////////////////
-      // Standard Algotithm methods
-      ///////////////////////////////////////////////////////////////////
+	class TRT_TrackExtensionAlg : public AthAlgorithm {
 
-      TRT_TrackExtensionAlg(const std::string &name, ISvcLocator *pSvcLocator);
-      virtual ~TRT_TrackExtensionAlg() {}
-      StatusCode initialize();
-      StatusCode execute();
-      StatusCode finalize();
+		///////////////////////////////////////////////////////////////////
+		// Public methods:
+		///////////////////////////////////////////////////////////////////
+  
+	public:
 
-      ///////////////////////////////////////////////////////////////////
-      // Print internal algorithm parameters and status
-      ///////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////
+		// Standard Algotithm methods
+		///////////////////////////////////////////////////////////////////
 
-      MsgStream&    dump     (MsgStream&    out) const;
-      std::ostream& dump     (std::ostream& out) const;
+		TRT_TrackExtensionAlg(const std::string &name, ISvcLocator *pSvcLocator);
+		virtual ~TRT_TrackExtensionAlg() {}
+		StatusCode initialize();
+		StatusCode execute();
+		StatusCode finalize();
 
-    protected:
+		///////////////////////////////////////////////////////////////////
+		// Print internal algorithm parameters and status
+		///////////////////////////////////////////////////////////////////
 
-      ///////////////////////////////////////////////////////////////////
-      // Protected data 
-      ///////////////////////////////////////////////////////////////////
-     
-      int                                 m_outputlevel           ;  // Print level for debug
-      int                                 m_nprint                ;  // Kind of  print    
-      int                                 m_nTracks               ;  // Number input  tracks
-      int                                 m_nTracksExtended       ;  // Number output tracks
-      int                                 m_nTracksTotal          ;  // Number input  tracks
-      int                                 m_nTracksExtendedTotal  ;  // Number output tracks
-      std::string                         m_tracksLocation        ;  // Input  tracks location
-      std::string                         m_extendedTracksLocation;  // Output tracks location
-      ToolHandle<ITRT_TrackExtensionTool> m_trtExtension          ;  // TRT track extension tool
+		MsgStream& dump (MsgStream& out) const;
+		std::ostream& dump (std::ostream& out) const;
 
-      ///////////////////////////////////////////////////////////////////
-      // Protected methods
-      ///////////////////////////////////////////////////////////////////
+	protected:
 
-      MsgStream&    dumpConditions(MsgStream&    out) const;
-      MsgStream&    dumpEvent     (MsgStream&    out) const;
+		///////////////////////////////////////////////////////////////////
+		// Protected data 
+		///////////////////////////////////////////////////////////////////
 
-    };
+		int m_outputlevel;  // Print level for debug
+		int m_nprint;  // Kind of  print    
+		int m_nTracks;  // Number input  tracks
+		int m_nTracksExtended;  // Number output tracks
+		int m_nTracksTotal;  // Number input  tracks
+		int m_nTracksExtendedTotal;  // Number output tracks
+		//		std::string m_tracksLocation;  // Input  tracks location
+		//		std::string m_extendedTracksLocation;  // Output tracks location
+		ToolHandle<ITRT_TrackExtensionTool> m_trtExtension;  // TRT track extension tool
 
-  MsgStream&    operator << (MsgStream&   ,const TRT_TrackExtensionAlg&);
-  std::ostream& operator << (std::ostream&,const TRT_TrackExtensionAlg&); 
+		// Data handle keys
+		SG::ReadHandleKey<TrackCollection> m_inputTracksKey	{ this, "InputTracksLocation", "SiSPSeededTracks", "Input tracks container name" };
+		SG::WriteHandleKey<TrackExtensionMap> m_outputTracksKey { this, "ExtendedTracksLocation", "ExtendedTracksMapPhase", "Output tracks container name" };
+
+		///////////////////////////////////////////////////////////////////
+		// Protected methods
+		///////////////////////////////////////////////////////////////////
+
+		MsgStream& dumpConditions(MsgStream& out) const;
+		MsgStream& dumpEvent(MsgStream& out) const;
+
+	};
+
+	MsgStream& operator << (MsgStream&, const TRT_TrackExtensionAlg&);
+	std::ostream& operator << (std::ostream&, const TRT_TrackExtensionAlg&); 
 }
 #endif // TRT_TrackExtensionAlg_H
