bd32ca42af629b9d28eff4d4c4325d342ad68c92
James Richard Catmore <james.catmore@cern.ch>
2017-09-17 15:04:14 +0200

    Adding 1st merge test to DerivationFrameworkART and introducing an NTUP_PILEUP test
    Former-commit-id: d550b526ddd59e75d04258d536c0f044efb51e6d

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data17EGAM1.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data17EGAM1.sh
@@ -5,3 +5,5 @@
 # art-output: *.pool.root
 
 Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/data17_13TeV.00327342.physics_Main.merge.AOD.f838_m1824._lb0300._0001.1 --outputDAODFile art.pool.root --reductionConf EGAM1 --maxEvents 5000
+
+DAODMerge_tf.py --maxEvents 5 --inputDAOD_EGAM1File DAOD_EGAM1.art.pool.root --outputDAOD_EGAM1_MRGFile art_merged.pool.root
