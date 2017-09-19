863022905700273dd8781a12b418df562f70e50f
John Chapman <jchapman@cern.ch>
2017-09-19 16:00:47 +0200

    Extending TrackHelper::GetParticleLink for HepMcParticleLink migration
    Pulling in changes from `MCTruth-00-06-01`.
    Extend `TrackHelper::GetParticleLink` method to be able to retrieve/use
    parent `HepMcParticleLink` information from the `ISFParticle` (if available).
    See ATLASSIM-3009. Part of the changes for the `HepMcParticleLink` migration.
    ATLASSIM-2430.
    
    
    Former-commit-id: a17fe710ccc8c98d0bcea5d31178f5a1443fe544

--- Simulation/G4Sim/MCTruth/src/TrackHelper.cxx
+++ Simulation/G4Sim/MCTruth/src/TrackHelper.cxx
@@ -9,40 +9,43 @@ TrackHelper::TrackHelper(const G4Track* t)
 {
   m_trackInfo=static_cast<TrackInformation *>(t->GetUserInformation());
 }
+
 bool TrackHelper::IsPrimary() const
 {
-  if (m_trackInfo==0) return false;
+  if (m_trackInfo==nullptr) return false;
   return m_trackInfo->GetClassification()==Primary;
 }
+
 bool TrackHelper::IsRegeneratedPrimary() const
 {
-  if (m_trackInfo==0) return false;
+  if (m_trackInfo==nullptr) return false;
   return m_trackInfo->GetClassification()==RegeneratedPrimary;
 }
+
 bool TrackHelper::IsRegisteredSecondary() const
 {
-  if (m_trackInfo==0) return false;
+  if (m_trackInfo==nullptr) return false;
   return m_trackInfo->GetClassification()==RegisteredSecondary;
 }
+
 bool TrackHelper::IsSecondary() const
 {
-  if (m_trackInfo==0) return true;
+  if (m_trackInfo==nullptr) return true;
   return m_trackInfo->GetClassification()==Secondary;
 }
+
 int TrackHelper::GetBarcode() const
 {
-  if (m_trackInfo==0 || m_trackInfo->GetHepMCParticle()==0) return 0;
+  if (m_trackInfo==nullptr || m_trackInfo->GetHepMCParticle()==nullptr) return 0;
   return m_trackInfo->GetParticleBarcode();
 }
 
 HepMcParticleLink TrackHelper::GetParticleLink()
 {
-  int barcode = this->GetBarcode();
-  return HepMcParticleLink(barcode);
-  //HepMcParticleLink * originalParticleLink(nullptr);
-  //if (this->GetTrackInformation() && this->GetTrackInformation()->GetBaseISFParticle())
-  //  {
-  //    originalParticleLink = this->GetTrackInformation()->GetBaseISFParticle()->getParticleLink();
-  //  }
-  //return (originalParticleLink) ? HepMcParticleLink(barcode, originalParticleLink->eventIndex(), originalParticleLink->getEventCollection()) : HepMcParticleLink(barcode);
+  const HepMcParticleLink *originalParticleLink(nullptr);
+  if (this->GetTrackInformation() && this->GetTrackInformation()->GetBaseISFParticle())
+    {
+      originalParticleLink = this->GetTrackInformation()->GetBaseISFParticle()->getParticleLink();
+    }
+  return (originalParticleLink) ? HepMcParticleLink(this->GetBarcode(), originalParticleLink->eventIndex(), originalParticleLink->getEventCollection()) : HepMcParticleLink(this->GetBarcode());
 }
