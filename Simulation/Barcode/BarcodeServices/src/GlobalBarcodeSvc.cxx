af5e2c98e5989d61d77ffb5e65eb810002a853ee
John Chapman <jchapman@cern.ch>
2017-09-20 16:54:08 +0200

    Migrate AthServices in BarcodeServices to use the extends pattern
    Using `declareInterface<...>` is now deprecated, so switched to using
    `extends<...>`.
    
    
    Former-commit-id: 6da1e8416ce6d6c440fc8eeb604c4fca7b0bbc09

--- Simulation/Barcode/BarcodeServices/src/GlobalBarcodeSvc.cxx
+++ Simulation/Barcode/BarcodeServices/src/GlobalBarcodeSvc.cxx
@@ -13,7 +13,7 @@
 
 /** Constructor **/
 Barcode::GlobalBarcodeSvc::GlobalBarcodeSvc(const std::string& name,ISvcLocator* svc) :
-  AthService(name,svc),
+  base_class(name,svc),
   m_incidentSvc("IncidentSvc", name),
   m_bitcalculator(new Barcode::BitCalculator()),
   m_firstVertex(-200000),
@@ -177,23 +177,5 @@ StatusCode Barcode::GlobalBarcodeSvc::finalize()
 {
   ATH_MSG_VERBOSE ("finalize() ...");
   ATH_MSG_VERBOSE ("finalize() successful");
-  return StatusCode::SUCCESS;
-}
-
-
-/** Query the interfaces. */
-StatusCode Barcode::GlobalBarcodeSvc::queryInterface(const InterfaceID& riid, void** ppvInterface)
-{
-
-  if ( IID_IBarcodeSvc == riid )
-    {
-      *ppvInterface = (IBarcodeSvc*)this;
-    }
-  else
-    {
-      // Interface is not directly available: try out a base class
-      return Service::queryInterface(riid, ppvInterface);
-    }
-  addRef();
   return StatusCode::SUCCESS;
 }
