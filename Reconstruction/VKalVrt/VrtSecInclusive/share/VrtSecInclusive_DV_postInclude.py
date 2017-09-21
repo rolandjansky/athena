f2e40bee1b9e8089802dc3530e9e543a9fba17d7
Hideyuki Oide <Hideyuki.Oide@cern.ch>
2017-09-21 17:09:08 +0200

    VrtSecInclusive:  DV_Configuration jobOption update for the new features
    Former-commit-id: 2a0769f638bdb896f5817e732733955158503efe

--- Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
+++ Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_postInclude.py
@@ -1,89 +1,15 @@
-# File for feeding Reco_tf.py vertexing options for the searches 
-# looking for displaced vertices in the SUSY and Exotics groups.
-# The options here are needed both when running on RAW and ESD inputs.
-# Must run after the large-radius tracking to use large-d0 tracks.
-
-# instantiate the vertexing alg
-from VrtSecInclusive.VrtSecInclusive import VrtSecInclusive
-topSequence.insert(-1, VrtSecInclusive())
-
-# set options for vertexing
-topSequence.VrtSecInclusive.OutputLevel                  = INFO
-topSequence.VrtSecInclusive.do_PVvetoCut                 = True
-topSequence.VrtSecInclusive.do_d0Cut                     = False
-topSequence.VrtSecInclusive.do_z0Cut                     = False
-topSequence.VrtSecInclusive.do_d0errCut                  = False
-topSequence.VrtSecInclusive.do_z0errCut                  = False
-topSequence.VrtSecInclusive.do_d0signifCut               = False # this option is not commissioned for the moment
-topSequence.VrtSecInclusive.do_z0signifCut               = False # this option is not commissioned for the moment
-topSequence.VrtSecInclusive.doTRTPixCut                  = True
-topSequence.VrtSecInclusive.DoSAloneTRT                  = False
-topSequence.VrtSecInclusive.ImpactWrtBL                  = True
-topSequence.VrtSecInclusive.RemoveFake2TrkVrt            = True
-topSequence.VrtSecInclusive.CheckHitPatternStrategy      = 'Extrapolation' # Classical or Extrapolation
-topSequence.VrtSecInclusive.doReassembleVertices         = True
-topSequence.VrtSecInclusive.doMergeByShuffling           = True
-topSequence.VrtSecInclusive.doMergeFinalVerticesDistance = True
-topSequence.VrtSecInclusive.doAssociateNonSelectedTracks = True
-topSequence.VrtSecInclusive.doFinalImproveChi2           = False
-topSequence.VrtSecInclusive.DoTruth                      = (globalflags.DataSource == 'geant4' and globalflags.InputFormat == "pool")
-topSequence.VrtSecInclusive.FillHist                     = True
-topSequence.VrtSecInclusive.CutPixelHits                 = 0
-topSequence.VrtSecInclusive.CutSctHits                   = 2
-topSequence.VrtSecInclusive.TrkA0ErrCut                  = 200000
-topSequence.VrtSecInclusive.TrkZErrCut                   = 200000
-topSequence.VrtSecInclusive.a0TrkPVDstMinCut             = 2.0    # track d0 min
-topSequence.VrtSecInclusive.a0TrkPVDstMaxCut             = 300.0  # track d0 max: default is 1000.0
-topSequence.VrtSecInclusive.zTrkPVDstMinCut              = 0.0    # track z0 min: default is 0.0, just for clarification
-topSequence.VrtSecInclusive.zTrkPVDstMaxCut              = 1500.0 # track z0 max: default is 1000.0
-topSequence.VrtSecInclusive.TrkPtCut                     = 1000
-topSequence.VrtSecInclusive.SelVrtChi2Cut                = 5
-topSequence.VrtSecInclusive.CutSharedHits                = 2
-topSequence.VrtSecInclusive.TrkChi2Cut                   = 50
-topSequence.VrtSecInclusive.TruthTrkLen                  = 1
-topSequence.VrtSecInclusive.SelTrkMaxCutoff              = 300
-topSequence.VrtSecInclusive.mergeByShufflingAllowance    = 5.
-topSequence.VrtSecInclusive.MergeFinalVerticesDist       = 1.
-topSequence.VrtSecInclusive.MergeFinalVerticesScaling    = 0.
-topSequence.VrtSecInclusive.improveChi2ProbThreshold     = 0.0001
-
-# set options related to the vertex fitter
-from TrkVKalVrtFitter.TrkVKalVrtFitterConf import Trk__TrkVKalVrtFitter
-InclusiveVxFitterTool = Trk__TrkVKalVrtFitter(name                = "InclusiveVxFitter",
-                                              Extrapolator        = ToolSvc.AtlasExtrapolator,
-                                              IterationNumber     = 30,
-                                              AtlasMagFieldSvc    = "AtlasFieldSvc"
-                                             )
-ToolSvc +=  InclusiveVxFitterTool;
-InclusiveVxFitterTool.OutputLevel = INFO
-topSequence.VrtSecInclusive.VertexFitterTool=InclusiveVxFitterTool
-topSequence.VrtSecInclusive.Extrapolator = ToolSvc.AtlasExtrapolator
+StreamESD.ItemList+=["xAOD::TrackParticleContainer#VrtSecInclusive*"]
+StreamESD.ItemList+=["xAOD::VertexContainer#VrtSecInclusive*"]
+StreamESD.ItemList+=["xAOD::VertexAuxContainer#VrtSecInclusive*"]
 
 
-# tell VrtSecInclusive the interface name for Trk::IVertexMapper
-from TrkDetDescrTestTools.TrkDetDescrTestToolsConf import Trk__VertexMapper
-HadronicVertexMapper = Trk__VertexMapper("HadronicVertexMapper")
-ToolSvc += HadronicVertexMapper
-topSequence.VrtSecInclusive.VertexMapper = HadronicVertexMapper
-
-# now add the new vertex collection to the output DAOD_RPVLL file
-from OutputStreamAthenaPool.MultipleStreamManager import MSMgr
-MSMgr.GetStream("StreamDAOD_SUSY15").AddItem( [ 'xAOD::TrackParticleContainer#InDet*',
-                                                'xAOD::TrackParticleAuxContainer#InDet*',
-                                                'xAOD::TrackParticleContainer#VrtSecInclusive*',
-                                                'xAOD::TrackParticleAuxContainer#VrtSecInclusive*',
-                                                'xAOD::VertexContainer#VrtSecInclusive*',
-                                                'xAOD::VertexAuxContainer#VrtSecInclusive*'] )
-print "List of items for the DAOD_RPVLL output stream:"
-print MSMgr.GetStream("StreamDAOD_SUSY15").GetItems()
-
-
-ServiceMgr += THistSvc()
-ServiceMgr.THistSvc.Output = ["AANT DATAFILE='HIST.VrtSecInclusive.root' OPT='RECREATE'"]
-
-from AnalysisTools.AnalysisToolsConf import AANTupleStream
-topSequence += AANTupleStream()
-topSequence.AANTupleStream.ExtraRefNames = [ "StreamDAOD_SUSY15" ]
-topSequence.AANTupleStream.OutputName = 'HIST.VrtSecInclusive.root'
-topSequence.AANTupleStream.WriteInputDataHeader = True
-topSequence.AANTupleStream.OutputLevel = INFO
+ToolSvc.EGammaAmbiguityTool.minNoSiHits = 2
+ToolSvc.EGammaAmbiguityTool.minNoPixHits = 0
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.IPd0Max = 9999.
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitPix = 0
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitSct = 2
+ToolSvc.MuonCombinedInDetDetailedTrackSelectorTool.nHitSi = 2
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.IPd0Max = 9999.
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitPix = 0
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitSct = 2
+ToolSvc.CaloTrkMuIdAlgTrackSelectorTool.nHitSi = 2
