7e424fb420b8ae60306da5324bbeeaebce7c982b
Hideyuki Oide <Hideyuki.Oide@cern.ch>
2017-09-21 22:06:18 +0200

    DV_Configuration jobOption update for the new features
    Former-commit-id: 1fa588f299934abc84df53be6544b1fa703677ad

--- Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_Configuration.py
+++ Reconstruction/VKalVrt/VrtSecInclusive/share/VrtSecInclusive_DV_Configuration.py
@@ -12,29 +12,47 @@ from VrtSecInclusive.VrtSecInclusive import VrtSecInclusive
 topSequence.insert(-1, VrtSecInclusive())
 
 # set options for vertexing
-topSequence.VrtSecInclusive.ImpactWrtBL = True
-topSequence.VrtSecInclusive.CutPixelHits = 0
-topSequence.VrtSecInclusive.CutSctHits = 2
-topSequence.VrtSecInclusive.TrkA0ErrCut = 200000
-topSequence.VrtSecInclusive.TrkZErrCut = 200000
-topSequence.VrtSecInclusive.a0TrkPVDstMinCut=2.0  # track d0 min
-topSequence.VrtSecInclusive.a0TrkPVDstMaxCut=300.0 # track d0 max: default is 1000.0
-topSequence.VrtSecInclusive.zTrkPVDstMinCut=0.0    # track z0 min: default is 0.0, just for clarification
-topSequence.VrtSecInclusive.zTrkPVDstMaxCut=1500.0 # track z0 max: default is 1000.0
-topSequence.VrtSecInclusive.TrkPtCut = 1000
-topSequence.VrtSecInclusive.SelVrtChi2Cut=5
-topSequence.VrtSecInclusive.CutSharedHits=2
-topSequence.VrtSecInclusive.TrkChi2Cut=50
-topSequence.VrtSecInclusive.TruthTrkLen=1
-topSequence.VrtSecInclusive.SelTrkMaxCutoff=300
-topSequence.VrtSecInclusive.DoSAloneTRT=False
-topSequence.VrtSecInclusive.DoTruth = (globalflags.DataSource == 'geant4' and globalflags.InputFormat == "pool")
-topSequence.VrtSecInclusive.RemoveFake2TrkVrt = True
-topSequence.VrtSecInclusive.doTRTPixCut=True
-topSequence.VrtSecInclusive.doMergeFinalVerticesDistance=True
-topSequence.VrtSecInclusive.VertexMergeFinalDistCut=1.0
-topSequence.VrtSecInclusive.VertexMergeFinalDistScaling=0.00
-topSequence.VrtSecInclusive.SelTrkMaxCutoff = 300
+topSequence.VrtSecInclusive.do_PVvetoCut                           = True
+topSequence.VrtSecInclusive.do_d0Cut                               = False
+topSequence.VrtSecInclusive.do_z0Cut                               = False
+topSequence.VrtSecInclusive.do_d0errCut                            = False
+topSequence.VrtSecInclusive.do_z0errCut                            = False
+topSequence.VrtSecInclusive.do_d0signifCut                         = False
+topSequence.VrtSecInclusive.do_z0signifCut                         = False
+topSequence.VrtSecInclusive.doTRTPixCut                            = True
+topSequence.VrtSecInclusive.DoSAloneTRT                            = False
+topSequence.VrtSecInclusive.ImpactWrtBL                            = True
+topSequence.VrtSecInclusive.doPVcompatibilityCut                   = False
+topSequence.VrtSecInclusive.RemoveFake2TrkVrt                      = True
+topSequence.VrtSecInclusive.CheckHitPatternStrategy                = 'Extrapolation' # Classical or Extrapolation
+topSequence.VrtSecInclusive.doReassembleVertices                   = True
+topSequence.VrtSecInclusive.doMergeByShuffling                     = True
+topSequence.VrtSecInclusive.doMergeFinalVerticesDistance           = True
+topSequence.VrtSecInclusive.doAssociateNonSelectedTracks           = True
+topSequence.VrtSecInclusive.doFinalImproveChi2                     = True
+topSequence.VrtSecInclusive.DoTruth                                = (globalflags.DataSource == 'geant4' and globalflags.InputFormat == "pool")
+topSequence.VrtSecInclusive.FillHist                               = True
+topSequence.VrtSecInclusive.FillIntermediateVertices               = True
+topSequence.VrtSecInclusive.CutPixelHits                           = 0
+topSequence.VrtSecInclusive.CutSctHits                             = 2
+topSequence.VrtSecInclusive.TrkA0ErrCut                            = 200000
+topSequence.VrtSecInclusive.TrkZErrCut                             = 200000
+topSequence.VrtSecInclusive.a0TrkPVDstMinCut                       = 2.0    # track d0 min
+topSequence.VrtSecInclusive.a0TrkPVDstMaxCut                       = 300.0  # track d0 max: default is 1000.0
+topSequence.VrtSecInclusive.zTrkPVDstMinCut                        = 0.0    # track z0 min: default is 0.0, just for clarification
+topSequence.VrtSecInclusive.zTrkPVDstMaxCut                        = 1500.0 # track z0 max: default is 1000.0
+topSequence.VrtSecInclusive.TrkPtCut                               = 1000
+topSequence.VrtSecInclusive.SelVrtChi2Cut                          = 100
+topSequence.VrtSecInclusive.CutSharedHits                          = 2
+topSequence.VrtSecInclusive.TrkChi2Cut                             = 50
+topSequence.VrtSecInclusive.TruthTrkLen                            = 1
+topSequence.VrtSecInclusive.SelTrkMaxCutoff                        = 300
+topSequence.VrtSecInclusive.mergeByShufflingAllowance              = 5.
+topSequence.VrtSecInclusive.MergeFinalVerticesDist                 = 1.
+topSequence.VrtSecInclusive.MergeFinalVerticesScaling              = 0.
+topSequence.VrtSecInclusive.improveChi2ProbThreshold               = 0.00001
+topSequence.VrtSecInclusive.doAugmentDVimpactParametersToMuons     = True
+topSequence.VrtSecInclusive.doAugmentDVimpactParametersToElectrons = True
 
 # set options related to the vertex fitter
 from TrkVKalVrtFitter.TrkVKalVrtFitterConf import Trk__TrkVKalVrtFitter
