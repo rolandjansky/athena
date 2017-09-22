5323cc952930ff747242bea4194ffd011f363d02
Johannes Josef Junggeburth <jojungge@CERN.CH>
2017-09-22 13:46:30 +0200

    Removed obsolete Interface files
    Former-commit-id: c017b98f9508860949cde7f49a37d03188b48c0d

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/MuonEfficiencyCorrections/IMuonTriggerScaleFactors.h
+++
@@ -1,2 +0,0 @@
-#warning "The IMuonTriggerScaleFactors has moved to MuonAnalysisInterfaces/IMuonTriggerScaleFactors.h . Please update your code."
-#include <MuonAnalysisInterfaces/IMuonTriggerScaleFactors.h>
