0f32b093e414fbefbd72564d75917956ae4e3e41
Tim Martin <Tim.Martin@cern.ch>
2017-09-26 14:57:57 +0200

    Adding art-output: directives to all TriggerTest grid packages to save outputs on the grid
    Former-commit-id: 64f81622f839a7ce0b3d094724e36407757fdf3e

--- Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v7_nops_aod_ftk_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v7_nops_aod_ftk_grid.sh
@@ -2,6 +2,17 @@
 
 # art-description: MC v7 no prescales TriggerTest on FTK MC to AOD
 # art-type: grid
+# art-output: HLTChain.txt
+# art-output: HLTTE.txt
+# art-output: L1AV.txt
+# art-output: HLTconfig_*.xml
+# art-output: L1Topoconfig*.xml
+# art-output: LVL1config*.xml
+# art-output: *.log
+# art-output: costMonitoring_*
+# art-output: *.root
+# art-output: ntuple.pmon.gz
+# art-output: *perfmon*
 
 export NAME="mc_pp_v7_nops_aod_ftk_grid"
 export MENU="MC_pp_v7_no_prescale"
--- Trigger/TrigValidation/TriggerTest/test/test_slice_bphysics_aod_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_slice_bphysics_aod_grid.sh
@@ -2,6 +2,17 @@
 
 # art-description: Bphysics slice TriggerTest on MC using Jpsimu6mu6 to AOD
 # art-type: grid
+# art-output: HLTChain.txt
+# art-output: HLTTE.txt
+# art-output: L1AV.txt
+# art-output: HLTconfig_*.xml
+# art-output: L1Topoconfig*.xml
+# art-output: LVL1config*.xml
+# art-output: *.log
+# art-output: costMonitoring_*
+# art-output: *.root
+# art-output: ntuple.pmon.gz
+# art-output: *perfmon*
 
 export NAME="slice_bphysics_aod_grid"
 export SLICE="bphysics"
