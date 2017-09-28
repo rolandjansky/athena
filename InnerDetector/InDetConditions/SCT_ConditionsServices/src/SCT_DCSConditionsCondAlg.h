801d4e30bc9a517058372bcef55c919569f874f5
Susumu Oda <Susumu.Oda@cern.ch>
2017-09-28 14:58:17 +0200

    Going back to the original header file
    Former-commit-id: 88296e7c80ac4e6d0d71abdc3b11ff79b2a1b942

--- InnerDetector/InDetConditions/SCT_ConditionsServices/src/SCT_DCSConditionsCondAlg.h
+++ InnerDetector/InDetConditions/SCT_ConditionsServices/src/SCT_DCSConditionsCondAlg.h
@@ -6,11 +6,16 @@
 #define SCT_DCSCONDITIONSCONDALG
 
 #include "AthenaBaseComps/AthAlgorithm.h"
+
 #include "StoreGate/ReadCondHandleKey.h"
 #include "AthenaPoolUtilities/CondAttrListCollection.h"
+
 #include "StoreGate/WriteCondHandleKey.h"
 #include "SCT_ConditionsData/SCT_DCSStatCondData.h"
+
 #include "GaudiKernel/ICondSvc.h"
+
+#include "GaudiKernel/Property.h"
 
 class SCT_DCSConditionsCondAlg : public AthAlgorithm 
 {  
@@ -22,9 +27,23 @@ class SCT_DCSConditionsCondAlg : public AthAlgorithm
   StatusCode finalize();
 
  private:
+  //  SG::ReadCondHandleKey<CondAttrListCollection> m_readKeyHV;
+  //  SG::ReadCondHandleKey<CondAttrListCollection> m_readKeyState;
   SG::ReadCondHandleKey<CondAttrListCollection> m_readKey;
+  //  SG::WriteCondHandleKey<SCT_DCSStatCondData> m_writeKeyState;
   SG::WriteCondHandleKey<SCT_DCSStatCondData> m_writeKey;
-  ServiceHandle<ICondSvc> m_condSvc; 
+
+  ServiceHandle<ICondSvc> m_condSvc;
+
+  BooleanProperty m_readAllDBFolders;
+  BooleanProperty m_returnHVTemp;
+  std::string m_chanstatCut;
+  float m_hvLowLimit;
+  float m_hvUpLimit;
+  bool m_useHV;
+  float m_useHVLowLimit;
+  float  m_useHVUpLimit;
+  std::string m_useHVChanCut;
 };
 
 #endif // SCT_DCSCONDITIONSCONDALG
