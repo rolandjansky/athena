68f6d574fc71bdc4fc352b085016daeb2ec78fb5
Graeme Stewart <graemes.cern@gmail.com>
2017-09-29 12:00:56 +0200

    InnerDetector/InDetConditions/SiLorentzAngleSvc deleted from 20.20.10

--- InnerDetector/InDetConditions/SiLorentzAngleSvc/src/components/SiLorentzAngleSvc_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "SiLorentzAngleSvc/SiLorentzAngleSvc.h"
-
-DECLARE_SERVICE_FACTORY(SiLorentzAngleSvc)
-
-DECLARE_FACTORY_ENTRIES(SiLorentzAngleSvc) {
-  DECLARE_SERVICE( SiLorentzAngleSvc)
-}
-
