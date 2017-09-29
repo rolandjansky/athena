c7fdd085295b03cfe6310e85bb1498c2af4b1af2
Graeme Stewart <graemes.cern@gmail.com>
2017-09-29 12:01:19 +0200

    InnerDetector/InDetPerformance/SiHitAnalysis deleted from 20.20.10

--- InnerDetector/InDetPerformance/SiHitAnalysis/src/components/SiHitAnalysis_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "SiHitAnalysis/SiHitAnalysis.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-DECLARE_ALGORITHM_FACTORY(SiHitAnalysis)
-DECLARE_FACTORY_ENTRIES(SiHitAnalysis) {
-DECLARE_ALGORITHM(SiHitAnalysis)
-}
