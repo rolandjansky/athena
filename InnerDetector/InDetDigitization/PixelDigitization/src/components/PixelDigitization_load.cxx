069b82285f4f95f0c584b827cff78f403ba893c7
Graeme Stewart <graemes.cern@gmail.com>
2017-09-29 12:01:15 +0200

    InnerDetector/InDetDigitization/PixelDigitization deleted from 20.20.10

--- InnerDetector/InDetDigitization/PixelDigitization/src/components/PixelDigitization_load.cxx
+++
@@ -1,6 +0,0 @@
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES( PixelDigitization )
-LOAD_FACTORY_ENTRIES( ChargeCollProbSvc )
-LOAD_FACTORY_ENTRIES( TimeSvc )
-LOAD_FACTORY_ENTRIES( CalibSvc )
