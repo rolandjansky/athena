3e62870167ef39f93f26faedf0e0c606fc464443
Graeme Stewart <graemes.cern@gmail.com>
2017-09-29 12:01:07 +0200

    AtlasGeometryCommon/SubDetectorEnvelopes deleted from 20.20.10

--- AtlasGeometryCommon/SubDetectorEnvelopes/src/components/SubDetectorEnvelopes_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "src/DetDescrDBEnvelopeSvc.h"
-
-DECLARE_SERVICE_FACTORY( DetDescrDBEnvelopeSvc )
-
-DECLARE_FACTORY_ENTRIES( SubDetectorEnvelopes ) {
-  DECLARE_SERVICE( DetDescrDBEnvelopeSvc )
-}
-
