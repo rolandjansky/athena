7a75ab2111b6b4f3015b26dfed2819e7060418b9
Graeme Stewart <graemes.cern@gmail.com>
2017-09-29 12:00:57 +0200

    InnerDetector/InDetDetDescr/PixelCabling deleted from 20.20.10

--- InnerDetector/InDetDetDescr/PixelCabling/share/postInclude.IBLPixelSvcCablingSetup.py
+++
@@ -1,9 +0,0 @@
-#ServiceMgr.PixelCablingSvc.UseIBLParameterSvc = False
-#ServiceMgr.PixelCablingSvc.MappingFile = "Pixels_Atlas_IdMapping_inclIBL.dat"
-#ServiceMgr.PixelCablingSvc.LayerColumnsPerFE = [80]
-#ServiceMgr.PixelCablingSvc.LayerRowsPerFE = [336]
-#ServiceMgr.PixelCablingSvc.LayerFEsPerHalfModule = [2]
-
-# Print a warning if this jO is included
-from AthenaCommon.Logging import logging
-logging.getLogger().warning("Use of PixelCabling postIncludes is depreciated - postInclude.IBLPixelSvcCablingSetup.py has no effect")
