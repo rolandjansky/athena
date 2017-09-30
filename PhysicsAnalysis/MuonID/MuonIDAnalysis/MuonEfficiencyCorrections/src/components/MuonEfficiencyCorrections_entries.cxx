7da095ade1ecedfe62ef20e38e2ab72dc02dc655
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2017-09-30 09:58:45 +0200

    Terminated the EfficiencyCorrectionsProviderAlg
    Former-commit-id: fca90e141921b50dacc993b511615bfabf2ec088

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/src/components/MuonEfficiencyCorrections_entries.cxx
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/src/components/MuonEfficiencyCorrections_entries.cxx
@@ -6,31 +6,24 @@
 #include "MuonEfficiencyCorrections/MuonTriggerScaleFactors.h"
 
 #include "../MuonEfficiencyCorrections_TestAlg.h"
-#include "../MuonEfficiencyCorrectionsProvider.h"
 #include "../TestTrigSF.h"
 #include "../TestTrigSF.h"
 #include "../MuonTriggerSF_TestAlg.h"
 
 
 DECLARE_NAMESPACE_TOOL_FACTORY( CP, MuonEfficiencyScaleFactors )
-
 DECLARE_NAMESPACE_TOOL_FACTORY( CP, MuonTriggerScaleFactors )
-
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( CP, MuonEfficiencyCorrections_TestAlg )
 
-DECLARE_NAMESPACE_ALGORITHM_FACTORY( CP, MuonEfficiencyCorrectionsProvider )
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( Trig, TestTrigSF )
 
 DECLARE_FACTORY_ENTRIES( MuonEfficiencyCorrections ) {
 
    DECLARE_NAMESPACE_TOOL( CP, MuonEfficiencyScaleFactors )
-
    DECLARE_NAMESPACE_TOOL( CP, MuonTriggerScaleFactors )
 
    DECLARE_NAMESPACE_ALGORITHM( CP, MuonEfficiencyCorrections_TestAlg )
-
-   DECLARE_NAMESPACE_ALGORITHM( CP, MuonEfficiencyCorrectionsProvider )
 
    DECLARE_NAMESPACE_ALGORITHM( Trig, TestTrigSF )
 }
