a24b8b27476ac379fbd551b1f8c756b657f74473
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2017-10-01 15:02:56 +0200

    Fixing merge conflicts
    Former-commit-id: 416dd14f9a3602b260cafbe4042293c1de373cea

--- Reconstruction/Jet/JetRecTools/Root/PFlowPseudoJetGetter.cxx
+++ Reconstruction/Jet/JetRecTools/Root/PFlowPseudoJetGetter.cxx
@@ -26,11 +26,7 @@ int PFlowPseudoJetGetter::appendTo(PseudoJetVector& psjs, const LabelIndex* pli)
   for(const auto& pfo : *pfos) {
     // Technically this also skips 0 energy, which has no effect other than
     // on memory size, but is used in some workflows for pileup rejection
-<<<<<<< HEAD
     bool reject = (m_skipNegativeEnergy && pfo->e()<FLT_MIN);
-=======
-    bool reject = (m_skipNegativeEnergy && pfo->e()<-1*FLT_MIN);
->>>>>>> 35cc0ba5ea72e2d0857fedf027cb81eddf05a8a2
     // Although it may seem pointless, we need to keep charged PFOs with energy==0
     // because for MET TST with PFlow, there may be high pt charged PFOs that receive
     // a weight of 0 due to being in dense showers, but need to be present for
