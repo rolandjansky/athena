2f1781f84239c5a8d56d84180329547c363f81de
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2017-10-02 22:45:25 +0200

    move copyrights below the line defining python env.
    Former-commit-id: 961ed070d3c94102ddfe621b1697a244837d12e3

--- Generators/EvgenJobTransforms/scripts/Generate_tf.py
+++ Generators/EvgenJobTransforms/scripts/Generate_tf.py
@@ -1,8 +1,7 @@
-#  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
 #! /usr/bin/env python
 
 """
+#  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 Run event simulation and produce an EVNT file.
 """
 
