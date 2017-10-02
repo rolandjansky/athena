ee423c0f9ccdea0b1a067ecd27eca735a4ca3f09
Dwayne Patrick Spiteri <dwayne.patrick.spiteri@cern.ch>
2017-10-02 15:54:27 +0200

    Deleting repeated instances of objects and cleaning up of outdate code sections on sparse checkout
    Former-commit-id: 413207df131995b01cb8b86ef55e812e3d78fb27

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValDecoration.py
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValDecoration.py
@@ -376,19 +376,19 @@ def addExtraMonitoring() :
            topSequence = AlgSequence()
            mon_manager = topSequence.getChildren()[mon_index]
            from InDetPhysValMonitoring.InDetPhysValMonitoringTool import InDetPhysValMonitoringTool
+           
+           # from InDetRecExample.InDetKeys import InDetKeys
+           ##  for backward compatibility check whether DBM has been added already
+           # if InDetPhysValFlags.doValidateTightPrimaryTracks() :
+           #    mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolTightPrimary ) ]
 
-           from InDetRecExample.InDetKeys import InDetKeys
-           # for backward compatibility check whether DBM has been added already
-           if InDetPhysValFlags.doValidateTightPrimaryTracks() :
-              mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolTightPrimary ) ]
+           # if InDetPhysValFlags.doValidateGSFTracks() :
+             #  mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolGSF ) ]
 
-           if InDetPhysValFlags.doValidateGSFTracks() :
-              mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolGSF ) ]
-
-           from InDetRecExample.InDetKeys import InDetKeys
-           # for backward compatibility check whether DBM has been added already
-           if InDetPhysValFlags.doValidateDBMTracks() and hasattr(InDetKeys,'DBMTrackParticles') :
-              mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolDBM ) ]
+           # from InDetRecExample.InDetKeys import InDetKeys
+           ##  for backward compatibility check whether DBM has been added already
+           # if InDetPhysValFlags.doValidateDBMTracks() and hasattr(InDetKeys,'DBMTrackParticles') :
+            #   mon_manager.AthenaMonTools += [ toolFactory(InDetPhysValMonitoringTool.InDetPhysValMonitoringToolDBM ) ]
 
   except ImportError :
     import sys,traceback
@@ -455,9 +455,9 @@ def addDecoratorIfNeeded() :
    # the monitoring manager exists already.
    from  InDetPhysValMonitoring.InDetPhysValJobProperties import InDetPhysValFlags
    InDetPhysValFlags.init()
-   if InDetPhysValFlags.doValidateGSFTracks() or InDetPhysValFlags.doValidateDBMTracks() or InDetPhysValFlags.doValidateTightPrimaryTracks():
-       from RecExConfig.RecFlags import rec
-       rec.UserExecs += ['from InDetPhysValMonitoring.InDetPhysValDecoration import addExtraMonitoring;addExtraMonitoring();']
+   # if InDetPhysValFlags.doValidateGSFTracks() or InDetPhysValFlags.doValidateDBMTracks() or InDetPhysValFlags.doValidateTightPrimaryTracks():
+    #    from RecExConfig.RecFlags import rec
+    #    rec.UserExecs += ['from InDetPhysValMonitoring.InDetPhysValDecoration import addExtraMonitoring;addExtraMonitoring();']
 
    # from AthenaCommon.AppMgr import ServiceMgr as svcMgr
    # print 'DEBUG addDecoratorIfNeeded add meta data %s.' % svcMgr.TagInfoMgr.ExtraTagValuePairs
--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
@@ -4,9 +4,19 @@ decoration = InDetPhysValMonitoring.InDetPhysValDecoration.addDecoratorIfNeeded(
 # add ID physics validation monitoring tool
 
 from InDetPhysValMonitoring.InDetPhysValMonitoringTool import InDetPhysValMonitoringTool
-indet_mon_tool = InDetPhysValMonitoringTool.InDetPhysValMonitoringTool()
 
-ToolSvc += indet_mon_tool
-monMan.AthenaMonTools += [indet_mon_tool]
+indet_mon_tool = InDetPhysValMonitoringTool.InDetPhysValMonitoringTool()
+ToolSvc += [indet_mon_tool] 
+monMan.AthenaMonTools += [indet_mon_tool] 
+
+if InDetPhysValFlags.doValidateLooseTracks():
+    indet_mon_tool_Loose = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolLoose()
+    ToolSvc += [ indet_mon_tool_Loose ]
+    monMan.AthenaMonTools += [ indet_mon_tool_Loose ] 
+
+if InDetPhysValFlags.doValidateTightPrimaryTracks():
+    indet_mon_tool_TightPrimary = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolTightPrimary()
+    ToolSvc += [ indet_mon_tool_TightPrimary ]
+    monMan.AthenaMonTools += [ indet_mon_tool_TightPrimary ] 
 
 
