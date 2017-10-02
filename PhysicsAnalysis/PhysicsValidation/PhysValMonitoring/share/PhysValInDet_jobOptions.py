83ec2667d7bf29ca5a33ec56246b8ad49aac78d3
Dwayne Patrick Spiteri <dwayne.patrick.spiteri@cern.ch>
2017-10-02 19:02:19 +0200

    Deleting code experimentally commented out and fixing job option tool creation
    Former-commit-id: 23d4a764bb52b4feed680680434cb4b5b3036457

--- PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
+++ PhysicsAnalysis/PhysicsValidation/PhysValMonitoring/share/PhysValInDet_jobOptions.py
@@ -9,6 +9,16 @@ indet_mon_tool = InDetPhysValMonitoringTool.InDetPhysValMonitoringTool()
 ToolSvc += [indet_mon_tool] 
 monMan.AthenaMonTools += [indet_mon_tool] 
 
+if InDetPhysValFlags.doValidateDBMTracks():
+    indet_mon_tool_DBM = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolDBM()
+    ToolSvc += [ indet_mon_tool_DBM ]
+    monMan.AthenaMonTools += [ indet_mon_tool_DBM ] 
+
+if InDetPhysValFlags.doValidateGSFTracks():
+    indet_mon_tool_GSF = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolGSF()
+    ToolSvc += [ indet_mon_tool_GSF ]
+    monMan.AthenaMonTools += [ indet_mon_tool_GSF ]
+
 if InDetPhysValFlags.doValidateLooseTracks():
     indet_mon_tool_Loose = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolLoose()
     ToolSvc += [ indet_mon_tool_Loose ]
@@ -18,5 +28,3 @@ if InDetPhysValFlags.doValidateTightPrimaryTracks():
     indet_mon_tool_TightPrimary = InDetPhysValMonitoringTool.InDetPhysValMonitoringToolTightPrimary()
     ToolSvc += [ indet_mon_tool_TightPrimary ]
     monMan.AthenaMonTools += [ indet_mon_tool_TightPrimary ] 
-
-
