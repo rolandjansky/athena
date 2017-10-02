981440bd9189efd80b88e16d945b329f91890a0b
Tomasz Bold <tomasz.bold@gmail.com>
2017-10-02 10:42:27 +0000

    Completion of l2 calo hypo alg
    Former-commit-id: c9762b296d0e91bcbc91d49cd8e49d60fdf3f4dc

--- Trigger/TrigHypothesis/TrigEgammaHypo/src/TrigL2CaloHypoAlg.h
+++ Trigger/TrigHypothesis/TrigEgammaHypo/src/TrigL2CaloHypoAlg.h
@@ -12,36 +12,35 @@
 #include "xAODTrigCalo/TrigEMClusterContainer.h"
 #include "DecisionHandling/TrigCompositeUtils.h"
 
-#include "TrigL2CaloHypoTool.h"
+#include "ITrigL2CaloHypoTool.h"
 
 /**
  * @class Implements egamma calo selection for the new HLT framework
  * @brief 
  **/
 
-class TrigL2CaloHypoAlg
-  : public ::AthReentrantAlgorithm
-{ 
+class TrigL2CaloHypoAlg : public ::AthReentrantAlgorithm { 
  public: 
 
   TrigL2CaloHypoAlg( const std::string& name, ISvcLocator* pSvcLocator );
 
   virtual ~TrigL2CaloHypoAlg(); 
 
-  StatusCode  initialize() override;
-  StatusCode  execute_r( const EventContext& context ) const override;
-  StatusCode  finalize() override;
+  virtual StatusCode  initialize() override;
+  virtual StatusCode  execute_r( const EventContext& context ) const override;
+
  
  private: 
   TrigL2CaloHypoAlg();
-  ToolHandleArray< TrigL2CaloHypoTool > m_hypoTools { this, "HypoTools", {}, "Hypo tools" };
+  ToolHandleArray< ITrigL2CaloHypoTool > m_hypoTools { this, "HypoTools", {}, "Hypo tools" };
   
   SG::ReadHandleKey< std::vector< SG::View* > > m_viewsKey { this, "Views", "Unspecified", "Input Views" };
   SG::ReadHandleKey< xAOD::TrigEMClusterContainer > m_clustersKey { this, "CaloClusters", "CaloClusters", "CaloClusters in view" };
   SG::ReadHandleKey< TrigRoiDescriptorCollection > m_roisKey { this, "RoIs", "RoIs", "RoIs key in the view" };
+  SG::ReadHandleKey< TrigCompositeUtils::DecisionContainer > m_previousDecisionsKey { this, "L1Decisions", "Undefined", "Key for L1 decisions per RoI" };
   SG::WriteHandleKey< TrigCompositeUtils::DecisionContainer > m_decisionsKey { this, "Decisions", "Unspecified", "Decisions" };
  
 }; 
 
-
+DECLARE_ALGORITHM_FACTORY( TrigL2CaloHypoAlg )
 #endif //> !TRIGEGAMMAHYPO_TRIGL2CALOHYPOALG_H
