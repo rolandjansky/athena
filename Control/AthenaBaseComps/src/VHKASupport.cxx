37c8ec3bc90d4ac44950a66b6a7b7b3e2756abd0
Tomasz Bold <tomasz.bold@gmail.com>
2017-10-03 14:10:12 +0200

    Servers update handles as well now
    Former-commit-id: 370572a01ebe25fc977d88b4e46ff96009f37ef6

--- Control/AthenaBaseComps/src/VHKASupport.cxx
+++ Control/AthenaBaseComps/src/VHKASupport.cxx
@@ -13,10 +13,10 @@ namespace VHKASupport {
     void insert ( const std::vector<SG::VarHandleKeyArray*>& src,  std::vector<Gaudi::DataHandle*>& dest, Gaudi::DataHandle::Mode requiredMode ) {
       for ( const SG::VarHandleKeyArray* vhka : src ) {
 	if ( not vhka->renounced() ) {
-	  for ( auto key: vhka->keys() ) {
-	    if ( key->mode()  & requiredMode ) {
-	      dest.push_back( key );
-	    }
+	  for ( SG::VarHandleKey* key: vhka->keys() ) {
+	    if ( not ( key->mode()  & requiredMode ) )  break;
+	    dest.push_back( key );
+	    
 	  }
 	}
       }    
