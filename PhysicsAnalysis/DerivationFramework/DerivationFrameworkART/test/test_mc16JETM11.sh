5cf6502856c2a809300b94e1bc9008583f6029ad
James Richard Catmore <james.catmore@cern.ch>
2017-10-03 23:25:48 +0200

    Error code exit for DerivationFrameworkART jobs
    This adds the ```set -e``` to all of the ART jobs, such that if either of the tasks in an ART job fail (DAOD building or merging) then the job returns a failure code and is marked red by ART. Without this MR only the merge job is used to detect the success or failure. The script-making script is also updated to reflect this modification.
    
    
    Former-commit-id: db33bd38f72183a791dd13156dadab941f200f09

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16JETM11.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16JETM11.sh
@@ -4,6 +4,8 @@
 # art-type: grid
 # art-output: *.pool.root
 
+set -e
+
 Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.11866988._000378.pool.root.1 --outputDAODFile art.pool.root --reductionConf JETM11 --maxEvents 5000 --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-40" ' 
 
 DAODMerge_tf.py --maxEvents 5 --inputDAOD_JETM11File DAOD_JETM11.art.pool.root --outputDAOD_JETM11_MRGFile art_merged.pool.root
