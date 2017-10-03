d9b08079122e4875f96f37b516d43105149694a8
Tomasz Bold <tomasz.bold@gmail.com>
2017-10-03 20:18:24 +0200

    Typos fixing
    Former-commit-id: b15bd06b7d8f83cf96b4026c2b8d1a65b270dfe7

--- Control/AthenaBaseComps/src/VHKASupport.h
+++ Control/AthenaBaseComps/src/VHKASupport.h
@@ -7,16 +7,16 @@
 #include <vector>
 #include <StoreGate/VarHandleKeyArray.h>
 /**
- * @brief functions in this namespace are to reduce a bioler plate code from the Ath* base classes
+ * @brief functions in this namespace are to reduce a boiler plate code from the Ath* base classes
  **/
 namespace VHKASupport {
   /**
-   * Copies all VHKAs which are reasers from the src to dest
+   * Copies all VHKAs which are readers from the src to dest
    **/
   void insertInput ( const std::vector<SG::VarHandleKeyArray*>& src,  std::vector<Gaudi::DataHandle*>& dest ); 
 
   /**
-   * @see insertInput
+   * @see insertInput but for writers
    **/  
   void insertOutput ( const std::vector<SG::VarHandleKeyArray*>& src,  std::vector<Gaudi::DataHandle*>& dest );
 }
