ed4c0c17d90fa0b58c66e4a321ccfcd4a47e93e0
Samuel Ross Meehan <smeehan12@gmail.com>
2017-10-04 18:46:36 +0200

    made changes to allow for configuration of JetRecTools in python with AnalysisBase due to generation of reflex dictionaries
    Former-commit-id: 8b3647b8b05180444732f907026d0fa7cd71e942

---
+++ Reconstruction/Jet/JetRecTools/JetRecTools/JetRecToolsDict.h
@@ -0,0 +1,25 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef JETRECTOOLS_JETRECTOOLSDICT_H
+#define JETRECTOOLS_JETRECTOOLSDICT_H
+
+
+#include "JetRecTools/CaloClusterConstituentsOrigin.h"
+#include "JetRecTools/CaloClusterConstituentsWeight.h"
+#include "JetRecTools/ClusterAtEMScaleTool.h"
+#include "JetRecTools/ConstituentSubtractorTool.h"
+#include "JetRecTools/JetConstituentModifierBase.h"
+#include "JetRecTools/JetConstituentModSequence.h"
+#include "JetRecTools/JetInputElRemovalTool.h"
+#include "JetRecTools/JetTrackSelectionTool.h"
+#include "JetRecTools/PFlowPseudoJetGetter.h"
+#include "JetRecTools/SimpleJetTrackSelectionTool.h"
+#include "JetRecTools/SoftKillerWeightTool.h"
+#include "JetRecTools/TrackPseudoJetGetter.h"
+#include "JetRecTools/TrackVertexAssociationTool.h"
+#include "JetRecTools/VoronoiWeightTool.h"
+
+
+#endif
