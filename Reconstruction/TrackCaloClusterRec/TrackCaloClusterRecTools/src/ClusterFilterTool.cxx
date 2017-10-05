64bee11eb7a71a1226abf53735b26b096f6fb8b2
Noemi Calace <noemi.calace@cern.ch>
2017-10-05 13:18:03 +0000

    Adding copyright statement
    Former-commit-id: 83dc2f8ec2cff3aab0a4a5d6cdcf4175287efe47

--- Reconstruction/TrackCaloClusterRec/TrackCaloClusterRecTools/src/ClusterFilterTool.cxx
+++ Reconstruction/TrackCaloClusterRec/TrackCaloClusterRecTools/src/ClusterFilterTool.cxx
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 #include "TrackCaloClusterRecTools/ClusterFilterTool.h"
 #include "TrackCaloClusterRecTools/IParticleToCaloExtensionMap.h"
 
