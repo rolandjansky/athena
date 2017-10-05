9263894afc21440f7f3cc675ea8428013d8287d5
Pavol Strizenec <pavol@pckeatlas05.cern.ch>
2017-10-05 15:39:37 +0200

    Fixes for MR 5148
    Former-commit-id: a818263a6bc5cff1efa0130c4063193e5d9d165d

--- Calorimeter/CaloCondPhysAlgs/src/LArHVMapTool.cxx
+++ Calorimeter/CaloCondPhysAlgs/src/LArHVMapTool.cxx
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 #include "CaloCondPhysAlgs/LArHVMapTool.h"
 #include "CaloDetDescr/CaloDetDescrManager.h"
 #include "CaloDetDescr/CaloDetectorElements.h"
