4a892ecfbc9997c9bf682cc6597f19d017251f9e
Noemi Calace <noemi.calace@cern.ch>
2017-10-05 13:07:27 +0000

    Adding copyright statement
    Former-commit-id: e69944ad8bf6b2aaef4c010e22552a13db860f10

--- Event/xAOD/xAODPFlowAthenaPool/src/xAODTrackCaloClusterAuxContainerCnv.h
+++ Event/xAOD/xAODPFlowAthenaPool/src/xAODTrackCaloClusterAuxContainerCnv.h
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 // Dear emacs, this is -*- c++ -*-
 // $Id: xAODTrackCaloClusterAuxContainerCnv.h $
 #ifndef XAODPFLOWATHENAPOOL_XAODTRACKCALOCLUSTERAUXCONTAINERCNV_H
