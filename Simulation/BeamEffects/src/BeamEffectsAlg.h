fb7a4dc5e01bb4e5783b2bfcd9800ded83c53a32
Steve Farrell <sfarrell@cern.ch>
2017-10-05 23:11:41 +0200

    Various cleanups of BeamEffects package
    whitespace, indentation, long lines, etc.
    
    
    Former-commit-id: c6f2fab30077bafb8cb3183770daa7f976d2c95c

--- Simulation/BeamEffects/src/BeamEffectsAlg.h
+++ Simulation/BeamEffects/src/BeamEffectsAlg.h
@@ -4,9 +4,8 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #ifndef BEAMEFFECTS_BEAMEFFECTSALG_H
-#define BEAMEFFECTS_BEAMEFFECTSALG_H 1
+#define BEAMEFFECTS_BEAMEFFECTSALG_H
 
 // base class header
 #include "AthenaBaseComps/AthAlgorithm.h"
@@ -19,16 +18,18 @@
 #include "GaudiKernel/ToolHandle.h"
 #include "HepMC_Interfaces/IGenEventManipulator.h"
 
+// Forward declarations
 class McEventCollection;
 namespace HepMC {
   class GenEvent;
 }
-
 namespace SimTesting {
   class BeamEffectsAlg_test;
 }
 
-namespace Simulation {
+namespace Simulation
+{
+
   /** @class BeamEffectsAlg
 
       This AthAlgorithm reads in the McEventCollection produced by the
@@ -36,31 +37,44 @@ namespace Simulation {
       simulation.  The IGenEventManipulators are used to apply the
       effects of beam properties to the GenEvents.
      */
-  class BeamEffectsAlg : public AthAlgorithm {
+  class BeamEffectsAlg : public AthAlgorithm
+  {
+
     friend class SimTesting::BeamEffectsAlg_test;
+
   public:
+
     //** Constructor with parameters */
     BeamEffectsAlg( const std::string& name, ISvcLocator* pSvcLocator );
 
     /** Destructor */
     virtual ~BeamEffectsAlg();
 
-    /** can clone for AthenaMT **/
+    /** Athena algorithm's interface method initialize() */
+    StatusCode initialize() override final;
+    /** Athena algorithm's interface method execute() */
+    StatusCode execute() override final;
+    /** Athena algorithm's interface method finalize() */
+    StatusCode finalize() override final;
+
+    /** Can clone for AthenaMT **/
     bool isClonable() const override { return true; }
 
-    /** Athena algorithm's interface method initialize() */
-    StatusCode  initialize() override final; /** Athena algorithm's interface method execute() */
-    StatusCode  execute() override final;
-
-    /** Athena algorithm's interface method finalize() */
-    StatusCode  finalize() override final;
   private:
+
     /** Ensure that the GenEvent::signal_process_vertex has been set */
     StatusCode patchSignalProcessVertex(HepMC::GenEvent& ge) const;
     SG::ReadHandleKey<McEventCollection> m_inputMcEventCollection;
     SG::WriteHandleKey<McEventCollection> m_outputMcEventCollection;
-    ToolHandleArray<IGenEventManipulator> m_genEventManipulators;     //!< event manipulators (executed in given order)
-    bool m_ISFRun; /** Temporary property so that we don't change the output in the initial switch to this code. */
+
+    /// Event manipulator tools (executed in given order)
+    ToolHandleArray<IGenEventManipulator> m_genEventManipulators;
+
+    /// Temporary property so that we don't change the output in the initial switch to this code.
+    bool m_ISFRun;
+
   };
+
 }
-#endif //BEAMEFFECTS_BEAMEFFECTSALG_H
+
+#endif // BEAMEFFECTS_BEAMEFFECTSALG_H
