453fc6d934271deb45e6ad661a243fecc3630d64
Peter Alan Steinberg <peter.steinberg@bnl.gov>
2017-10-05 20:58:37 +0000

    Update ZdcDigitsCnv_p1_test.cxx
    Former-commit-id: 33781750490f1b0537ef1288184d971a6b0d0592

--- ForwardDetectors/ZDC/ZdcCnv/ZdcEventTPCnv/test/ZdcDigitsCnv_p1_test.cxx
+++ ForwardDetectors/ZDC/ZdcCnv/ZdcEventTPCnv/test/ZdcDigitsCnv_p1_test.cxx
@@ -13,7 +13,7 @@
 
 #undef NDEBUG
 #include "ZdcEventTPCnv/ZdcDigitsCnv_p1.h"
-#include "TestTools/leakcheck.h"
+//#include "TestTools/leakcheck.h"
 #include <cassert>
 #include <iostream>
 
@@ -45,7 +45,7 @@ void testit (const ZdcDigits& trans1)
 void test1()
 {
   std::cout << "test1\n";
-  Athena_test::Leakcheck check;
+  //Athena_test::Leakcheck check;
 
   ZdcDigits trans1 (Identifier (1234));
   trans1.set_digits_gain0_delay0 (std::vector<int> {1});
