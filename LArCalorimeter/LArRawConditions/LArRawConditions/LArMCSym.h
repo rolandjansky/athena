b90efa28cdeb60614d5a62dddfcb2d029ea12223
Walter Lampl <Walter.Lampl@cern.ch>
2017-10-06 14:06:54 +0200

    add cond data obj and cond alg for LArMCSym (first iteration)
    Former-commit-id: f67cb0d3fa1cc03c08db6ba9554eeaa09c2437c9

--- LArCalorimeter/LArRawConditions/LArRawConditions/LArMCSym.h
+++ LArCalorimeter/LArRawConditions/LArRawConditions/LArMCSym.h
@@ -7,39 +7,46 @@
 
 
 /** Helper class to handle z-phi symmetry of calibration constants in MC
- *
- * @author S. Laplace
- * @version  0-0-1 , 02/02/2004
  */
 
 #include "Identifier/HWIdentifier.h"
-
-class StoreGateSvc; 
-class LArCablingService ;
-class LArEM_ID;
-class LArHEC_ID;
-class LArFCAL_ID;
+#include "Identifier/IdentifierHash.h"
+#include "LArIdentifier/LArOnlineID.h"
 
 class LArMCSym {
 
  public:
-
-  LArMCSym();
-  virtual ~LArMCSym() ;
-  
+  LArMCSym() = delete;
+  LArMCSym(const LArOnlineID* onlId, 
+	   std::vector<HWIdentifier>&& oflHashtoSymOnl); 
+   
   // method to handle z-phi symmetry
-  HWIdentifier ZPhiSym(const HWIdentifier& NotSymOnId) const;
-  
-  Identifier ZPhiSym(const Identifier& NotSymOffId) const;
-  
- private:
+  HWIdentifier ZPhiSym(const HWIdentifier& notSymOffId) const {
+    const IdentifierHash h=m_onlineID->channel_Hash(notSymOffId);
+    return ZPhiSym(h);
+  }
 
-  const LArEM_ID*           m_lar_em_id; 
-  const LArHEC_ID*          m_lar_hec_id;   
-  const LArFCAL_ID*         m_lar_fcal_id; 
-  LArCablingService*        m_cablingService;
-  StoreGateSvc*             m_detStore;
+  HWIdentifier ZPhiSym(const IdentifierHash notSymOffHash) const {
+    assert(notSymOffHash < m_oflHashtoSymOnl.size());
+    return m_oflHashtoSymOnl[notSymOffHash];
+  }
+  
+  
+
+
+ private:
+  const LArOnlineID* m_onlineID;
+  const std::vector<HWIdentifier> m_oflHashtoSymOnl;
+  const std::vector<HWIdentifier> m_onlHashtoSymOnl;
 
 };
+
+#include "CLIDSvc/CLASS_DEF.h"
+CLASS_DEF( LArMCSym , 33771274 , 1 )
+//ConditionsContainer clid for athenaMT
+#include "AthenaKernel/CondCont.h"
+CLASS_DEF(  CondCont<LArMCSym> ,159334782 , 1 )
+#include "SGTools/BaseInfo.h"
+SG_BASE( CondCont<LArMCSym>, CondContBase );
 
 #endif 
