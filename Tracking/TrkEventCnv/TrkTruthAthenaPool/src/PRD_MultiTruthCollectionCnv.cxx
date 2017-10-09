98a51d2a4e184e634903ca1d14d05cbed521dda7
scott snyder <scott.snyder@cern.ch>
2017-10-09 17:12:19 +0200

    TrkTruthAthenaPool: Comply with ATLAS naming conventions.
    Static, private class members should start with s_.
    
    
    
    Former-commit-id: 66ce1a2ffe17adbc6f636654909c3e56536e5997

--- Tracking/TrkEventCnv/TrkTruthAthenaPool/src/PRD_MultiTruthCollectionCnv.cxx
+++ Tracking/TrkEventCnv/TrkTruthAthenaPool/src/PRD_MultiTruthCollectionCnv.cxx
@@ -9,9 +9,9 @@
 #include "TrkTruthTPCnv/PRD_MultiTruthCollectionCnv_p2.h"
 #include "TrkTruthData/PRD_MultiTruthCollection.h"
 
-pool::Guid PRD_MultiTruthCollectionCnv::p0_guid("30794FF9-F003-44A6-8553-ED61E2039882");
-pool::Guid PRD_MultiTruthCollectionCnv::p1_guid("6649A9D1-719F-4954-A385-D01BCA8E41EF");
-pool::Guid PRD_MultiTruthCollectionCnv::p2_guid("714F2E4A-419D-4BDB-8080-BEEB6CDBA0DA");
+pool::Guid PRD_MultiTruthCollectionCnv::s_p0_guid("30794FF9-F003-44A6-8553-ED61E2039882");
+pool::Guid PRD_MultiTruthCollectionCnv::s_p1_guid("6649A9D1-719F-4954-A385-D01BCA8E41EF");
+pool::Guid PRD_MultiTruthCollectionCnv::s_p2_guid("714F2E4A-419D-4BDB-8080-BEEB6CDBA0DA");
 
 //================================================================
 PRD_MultiTruthCollectionCnv::PRD_MultiTruthCollectionCnv(ISvcLocator* svcLoc) : 
@@ -32,19 +32,19 @@ PRD_MultiTruthCollection* PRD_MultiTruthCollectionCnv::createTransient() {
   MsgStream log(msgSvc(), "PRD_MultiTruthCollectionCnv" );
   std::auto_ptr<PRD_MultiTruthCollection> trans(new PRD_MultiTruthCollection());
   
-  if (compareClassGuid(p2_guid)) {
+  if (compareClassGuid(s_p2_guid)) {
     log<<MSG::DEBUG<<"Read PRD_MultiTruthCollection_p2. GUID="<<m_classID.toString()<<endmsg;
     Trk::PRD_MultiTruthCollection_p2* pers=poolReadObject<Trk::PRD_MultiTruthCollection_p2>();
     m_converter_p2.persToTrans(pers, trans.get(), log);
     delete pers;
   }
-  else if (compareClassGuid(p1_guid)) {
+  else if (compareClassGuid(s_p1_guid)) {
     log<<MSG::DEBUG<<"Read PRD_MultiTruthCollection_p1. GUID="<<m_classID.toString()<<endmsg;
     Trk::PRD_MultiTruthCollection_p1* pers=poolReadObject<Trk::PRD_MultiTruthCollection_p1>();
     m_converter_p1.persToTrans(pers, trans.get(), log);
     delete pers;
   }
-  else if (compareClassGuid(p0_guid)) {
+  else if (compareClassGuid(s_p0_guid)) {
     log<<MSG::DEBUG<<"Read version p0 of PRD_MultiTruthCollection. GUID="<<m_classID.toString()<<endmsg;
     trans.reset(poolReadObject<PRD_MultiTruthCollection>());
   }
