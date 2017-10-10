68df990bc29cf9579fa65350aa16c2a39bba83ec
Walter Lampl <Walter.Lampl@cern.ch>
2017-10-10 09:48:05 +0200

    add LArfSamplSym class
    Former-commit-id: b0b0f9f5c73f7a3790c0c4b6504e2fdb636a50df

---
+++ LArCalorimeter/LArRawConditions/LArRawConditions/LArfSamplSym.h
@@ -0,0 +1,52 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef LARRAWCONDITIONS_LARFSAMPLSYM_H
+#define LARRAWCONDITIONS_LARFSAMPLSYM_H
+
+#include "LArElecCalib/ILArfSampl.h"
+
+class LArMCSym;
+class LArfSamplComplete ;
+class LArfSamplMC;
+
+/** Implementation of the interface ILArfSampl for MC
+    using the z-phi symmetry
+ */
+
+class LArfSamplSym: virtual public ILArfSampl {
+  
+ public: 
+  
+  LArfSamplSym()=delete;
+
+  LArfSamplSym(const LArMCSym* mcSym, const LArfSamplMC* fSamplMC);
+  
+  virtual ~LArfSamplSym();
+  
+  // retrieving fSampl using online ID
+  
+  virtual const float& FSAMPL(const HWIdentifier&  CellID) const override;
+  
+  virtual const float& FSAMPL(const Identifier&  CellID) const override;
+
+  static float dummy;
+    
+ private: 
+  
+  const LArMCSym* m_mcSym;
+  const LArfSamplComplete* m_fSamplComplete;
+
+};
+
+#include "CLIDSvc/CLASS_DEF.h"
+CLASS_DEF( LArfSamplSym,169816659,1)
+//ConditionsContainer clid for athenaMT
+#include "AthenaKernel/CondCont.h"
+CLASS_DEF( CondCont<LArfSamplSym> ,75658929 , 1 )
+#include "SGTools/BaseInfo.h"
+SG_BASE( CondCont<LArfSamplSym>, CondContBase );
+SG_BASE( LArfSamplSym, SG_VIRTUAL( ILArfSampl ) );
+
+#endif 
