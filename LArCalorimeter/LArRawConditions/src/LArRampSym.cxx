b4495897cdc7065c9ded5147a20335c57d60a42e
Walter Lampl <Walter.Lampl@cern.ch>
2017-10-12 15:58:27 +0200

    add copyright statement
    Former-commit-id: a8d013bcccf9441f05712fff49c5fc2d7bca7f4e

--- LArCalorimeter/LArRawConditions/src/LArRampSym.cxx
+++ LArCalorimeter/LArRawConditions/src/LArRampSym.cxx
@@ -1,3 +1,8 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+
 #include "LArRawConditions/LArRampSym.h"
 #include "LArRawConditions/LArRampComplete.h"
 #include "LArRawConditions/LArRampMC.h"
