b68f464171184ad896adc654a4180f69113931e3
Walter Lampl <Walter.Lampl@cern.ch>
2017-10-12 16:27:51 +0200

    switch back to ERROCODE
    Former-commit-id: b141cbf1c28db68bad17ffdf49b254717dbdcf24

--- LArCalorimeter/LArRawConditions/src/LArPedestalMC.cxx
+++ LArCalorimeter/LArRawConditions/src/LArPedestalMC.cxx
@@ -27,25 +27,25 @@ void LArPedestalMC::set(const std::vector<float>& vPedestal,
 float LArPedestalMC::pedestal(const HWIdentifier& /*CellID*/, int /*gain*/) const 
 { 
   if (m_vPedestal.size()>0) return m_vPedestal[0];
-  else  return LArElecCalib::errorcode;
+  else  return LArElecCalib::ERRORCODE;
 }
 
 float LArPedestalMC::pedestalRMS(const HWIdentifier& /*CellID*/, int /*gain*/) const 
 { 
 
   if (m_vPedestalRMS.size()>0) return m_vPedestalRMS[0];
-  else  return LArElecCalib::errorcode;
+  else  return LArElecCalib::ERRORCODE;
 
 }
 
 float LArPedestalMC::pedestal(const Identifier&  /*CellID*/, int /*gain*/) const
 {
   if (m_vPedestal.size()>0) return m_vPedestal[0];
-  else  return LArElecCalib::errorcode;
+  else  return LArElecCalib::ERRORCODE;
 }
 
 float LArPedestalMC::pedestalRMS(const Identifier&  /*CellID*/, int /*gain*/) const
 {
   if (m_vPedestalRMS.size()>0) return m_vPedestalRMS[0];
-  else  return LArElecCalib::errorcode;
+  else  return LArElecCalib::ERRORCODE;
 }
