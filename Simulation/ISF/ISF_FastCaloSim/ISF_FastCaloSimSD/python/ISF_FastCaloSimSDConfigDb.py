89cbd614f54ca6f85eb44c592966bb3678b169cb
James Beacham <j.beacham@cern.ch>
2017-10-14 21:19:44 +0200

    Revert "Merge branch 'FCS_StepInfoSDs_ATLASSIM-3443_21.0' into '21.0'"
    This reverts commit ae10fb168d7791e0e193676c57cfc2e636e9b4ca [formerly c946c5c2d7fa22229e1fc820834b5009ddf819f3], reversing
    changes made to 1000a1cfbad79703c734143c20e1375279e92a3c [formerly d57d58cd9ec70419f96e1a5459267738957d1b30].
    
    
    Former-commit-id: 1acee8606e12a4a94717e5a701df3c1875435769

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimSD/python/ISF_FastCaloSimSDConfigDb.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-from AthenaCommon.CfgGetter import addTool
-
-addTool("ISF_FastCaloSimSD.ISF_FastCaloSimSDConfig.getFCS_StepInfoSensitiveDetector", "FCS_StepInfoSensitiveDetector")
--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimSD/src/components/ISF_FastCaloSimSD_entries.cxx
+++
@@ -1,5 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-#include "../FCS_StepInfoSDTool.h"
-
-DECLARE_TOOL_FACTORY( FCS_Param::FCS_StepInfoSDTool )
