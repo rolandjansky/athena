946f6d42001c50e29be4049870879297f9e4213d
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-10-16 16:23:21 +0200

    Added xAODMaker::TruthMetaDataTool to the package, copying the code that Zach wrote.
    Former-commit-id: a409c14d8979f97dbf03ffb13ebccd69c9bccfb7

--- Event/xAOD/xAODTruthCnv/src/components/xAODTruthCnv_entries.cxx
+++ Event/xAOD/xAODTruthCnv/src/components/xAODTruthCnv_entries.cxx
@@ -4,17 +4,17 @@
 #include "../xAODTruthCnvAlg.h"
 #include "../xAODTruthReader.h"
 #include "../HepMCTruthReader.h"
+#include "xAODTruthCnv/TruthMetaDataTool.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, xAODTruthCnvAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODReader, xAODTruthReader )
 DECLARE_ALGORITHM_FACTORY( HepMCTruthReader )
-
-DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
-   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, xAODTruthCnvAlg )
-   DECLARE_NAMESPACE_ALGORITHM( xAODReader, xAODTruthReader )
-}
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TruthMetaDataTool )
 
 DECLARE_FACTORY_ENTRIES( xAODTruthCnv ) {
+   DECLARE_NAMESPACE_ALGORITHM( xAODMaker, xAODTruthCnvAlg )
+   DECLARE_NAMESPACE_ALGORITHM( xAODReader, xAODTruthReader )
    DECLARE_ALGORITHM( HepMCTruthReader )
+   DECLARE_NAMESPACE_TOOL( xAODMaker, TruthMetaDataTool )
 }
 
