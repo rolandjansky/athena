7525b69247c1a89b82786a315a205c4742fde8c5
James Beacham <j.beacham@cern.ch>
2017-10-16 09:50:14 +0200

    Revert "Revert "Merge branch 'tauhistutils-update-atltau-1368' into '21.0'""
    This reverts commit 1c4cd8fa90e5acdcfb96f3f35d5c6849c61e9caa [formerly c0c84f17263bec571fbceb0911d89ac6cceaaff2].
    
    
    Former-commit-id: 14384b1ba54cc868a77938c81a7e034a855e1fb0

--- PhysicsAnalysis/TauID/TauHistUtils/TauHistUtils/ParamPlots.h
+++ PhysicsAnalysis/TauID/TauHistUtils/TauHistUtils/ParamPlots.h
@@ -11,20 +11,21 @@
 namespace Tau{
 
 class ParamPlots:public PlotBase {
-  public:      
-    ParamPlots(PlotBase *pParent, std::string sDir, std::string sParticleType);
-    void fill(const xAOD::IParticle& prt);
-
-    TH1* eta;
-    TH1* phi;
-    TH1* pt;
-
-    TH1* eta_phi;
-    TH1* eta_pt;
-
-  private:
-    void initializePlots();
-    std::string m_sParticleType;
+   public:      
+      ParamPlots(PlotBase *pParent, std::string sDir, std::string sParticleType);
+      ~ParamPlots();
+      void fill(const xAOD::IParticle& prt);
+      
+      TH1* eta;
+      TH1* phi;
+      TH1* pt;
+      
+      TH1* eta_phi;
+      TH1* eta_pt;
+      
+   private:
+      void initializePlots();
+      std::string m_sParticleType;
 };
 
 }
