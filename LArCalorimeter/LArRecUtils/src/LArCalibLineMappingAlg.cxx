3daafac311ab5dd7fcc38177b2a12bd6c9a41d26
Walter Lampl <Walter.Lampl@cern.ch>
2017-10-16 14:30:18 +0200

    adjust to changes of CondSvc::regHandle
    Former-commit-id: 3c1ff130547106870c1e63a94032a98a30f68487

--- LArCalorimeter/LArRecUtils/src/LArCalibLineMappingAlg.cxx
+++ LArCalorimeter/LArRecUtils/src/LArCalibLineMappingAlg.cxx
@@ -34,7 +34,7 @@ StatusCode LArCalibLineMappingAlg::initialize() {
   ATH_CHECK( m_readKey.initialize() );
   ATH_CHECK( m_writeKey.initialize() );
   // Register write handle
-  if (m_condSvc->regHandle(this, m_writeKey, m_writeKey.dbKey()).isFailure()) {
+  if (m_condSvc->regHandle(this, m_writeKey).isFailure()) {
     ATH_MSG_ERROR("unable to register WriteCondHandle " << m_writeKey.fullKey() << " with CondSvc");
     return StatusCode::FAILURE;
   }
