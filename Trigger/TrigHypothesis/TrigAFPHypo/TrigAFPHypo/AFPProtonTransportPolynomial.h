e3d3a9a0c177749319aa0c816093876775b9362b
Imma Riu <imma.riu@cern.ch>
2017-10-17 08:30:52 +0000

    Merge branch 'AFPDoxygen' into '21.1'
    Add documentation to AFP transport tools
    
    See merge request !5410
    
    Former-commit-id: 2a2d0871a686dbe670622ab439a47f4791a9ea92

--- Trigger/TrigHypothesis/TrigAFPHypo/TrigAFPHypo/AFPProtonTransportPolynomial.h
+++ Trigger/TrigHypothesis/TrigAFPHypo/TrigAFPHypo/AFPProtonTransportPolynomial.h
@@ -7,18 +7,20 @@
 
 class AFPProtonTransportPolynomial {
 public:
-  /*! Default constructor of kinRecoPolynomial. Takes number of coefficients and coefficients itself up to 10 as
-     arguments.*/
+  /// Default constructor of kinRecoPolynomial. Takes number of coefficients and coefficients itself up to 10 as arguments.
   AFPProtonTransportPolynomial(int n, double c0, double c1 = 0, double c2 = 0, double c3 = 0, double c4 = 0,
                                double c5 = 0, double c6 = 0, double c7 = 0, double c8 = 0, double c9 = 0);
 
-  /*! Gets number of coefficients. */
+  /// This function returns the number of coefficients. 
   int numCoefficients() const;
-  /*! Sets coefficients of polynomial. */
+  /// This function sets the coefficients of the polynomial. 
   void setCoefficients(int n, double c0, double c1 = 0, double c2 = 0, double c3 = 0, double c4 = 0,
                        double c5 = 0, double c6 = 0, double c7 = 0, double c8 = 0, double c9 = 0);
 
-  /*! Gets value of polynomial with given argument x. */
+  /** 
+   * This function of returns the sum of the polynomial 
+   * coeficients of a given argument x
+   */
   double evaluate(double x) const;
 private:
   int m_numCoefficients;
