591077037d8a94117f1b964bb727dd80456a91e4
iconnell <ian.connelly@cern.ch>
2017-10-18 13:36:45 +0100

    Renaming the test and also adding the path to test TOPQ1 file which will be distributed to cvmfs
    Former-commit-id: 3d198f2dec253650000f3beccd67d47fe2f9d344

--- PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/test/test_mc16atopq1.py
+++ PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/test/test_mc16atopq1.py
@@ -10,7 +10,7 @@ import subprocess, sys
 
 # -- Settings -- 
 cutfilename   = "validation-cuts.txt"
-inputpath     = "root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/phys-top/topreco/R21/MC/p3215/mc16_13TeV.410501.PowhegPythia8EvtGen_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_TOPQ1.e5458_s3126_r9364_r9315_p3215/DAOD_TOPQ1.11627611._000441.pool.root.1"
+inputpath     = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TopAnalysis/p3215/mc16_13TeV.410501.PowhegPythia8EvtGen_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_TOPQ1.e5458_s3126_r9364_r9315_p3215/DAOD_TOPQ1.11627611._000441.pool.root.1"
 
 # -- Get the validation file path from the most recent location --
 cutfilepath   = ROOT.PathResolver.find_file(cutfilename,
