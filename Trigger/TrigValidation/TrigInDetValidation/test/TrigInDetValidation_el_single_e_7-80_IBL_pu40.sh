a8be4f0e56046a5b9d1282ec8f301569e5634f40
Imma Riu <imma.riu@cern.ch>
2017-10-18 14:55:41 +0000

    Merge branch '21.1-dev-id-art' into '21.1-dev'
    Changes to get the TrigInDetValidation working in the ART framework
    
    See merge request !5477
    
    Former-commit-id: 6303ffb48b7b70564fba4e024b3fa7502d2a376a

---
+++ Trigger/TrigValidation/TrigInDetValidation/test/TrigInDetValidation_el_single_e_7-80_IBL_pu40.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# art-description: art job for el_single_e_7-80_IBL_pu40
+# art-type: grid
+# art-output: HLTEF-plots
+# art-output: HLTEF-plots-lowpt
+# art-output: HLTL2-plots
+# art-output: HLTL2-plots-lowpt
+# art-output: times
+# art-output: times-FTF
+# art-output: cost-perCall
+# art-output: cost-perEvent
+# art-output: cost-perCall-chain
+# art-output: cost-perEvent-chain
+# art-input:  mc15_13TeV.159010.ParticleGenerator_e_Et7to80.recon.RDO.e1948_s2726_r7772
+# art-output: *.dat 
+# art-output: *.root
+# art-output: *.log
+
+export RTTJOBNAME=TrigInDetValidation_el_single_e_7-80_IBL_pu40
+
+get_files -jo             TrigInDetValidation/TrigInDetValidation_RTT_topOptions_ElectronSlice.py
+athena.py  -c 'ARTConfig=["/eos/atlas/atlascerngroupdisk/proj-sit/trigindet/mc15_13TeV.159010.ParticleGenerator_e_Et7to80.recon.RDO.e1948_s2726_r7772/RDO.09929877._000001.pool.root.1"];	    EventMax=15000;runMergedChain=True'             TrigInDetValidation/TrigInDetValidation_RTT_topOptions_ElectronSlice.py
+get_files -data TIDAdata11-rtt.dat
+get_files -data TIDAdata_cuts.dat
+get_files -data TIDAdata_chains.dat
+get_files -data TIDAbeam.dat
+get_files -data Test_bin.dat
+TIDArdict.exe TIDAdata11-rtt.dat -f data-electron-IBL.root -p 11 -b Test_bin.dat
+
+TIDArun-art.sh data-electron-IBL.root data-el_single_e_7-80_IBL_pu40-reference.root HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_FTF  HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_IDTrig  -d HLTEF-plots
+
+TIDArun-art.sh data-electron-IBL.root data-el_single_e_7-80_IBL_pu40-reference.root  HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_FTF  HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_IDTrig  -d HLTEF-plots-lowpt
+
+TIDArun-art.sh data-electron-IBL.root data-el_single_e_7-80_IBL_pu40-reference.root HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_FTF -d HLTL2-plots
+
+TIDArun-art.sh data-electron-IBL.root data-el_single_e_7-80_IBL_pu40-reference.root HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_FTF -d HLTL2-plots-lowpt
+
+TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -o times
+
+TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -p FastTrack -o times-FTF
+
+RunTrigCostD3PD.exe -f trig_cost.root  --outputTagFromAthena --costMode --linkOutputDir
+
+TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall --auto -d "/Algorithm" -p "_Time_perCall"
+
+TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent --auto -d "/Algorithm" -p "_Time_perEvent"
+
+TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall-chain --auto -d "/Chain_Algorithm" -p "_Time_perCall"
+
+TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent-chain --auto -d "/Chain_Algorithm" -p "_Time_perEvent"
+
