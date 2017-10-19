0eea7db39c5b4e6ad81580e1cb24659172da4229
Tadej Novak <tadej.novak@cern.ch>
2017-10-19 13:47:59 +0200

    Add EXOT22 ART tests
    Former-commit-id: 76fe626bf4fdad5c1d62d22b5386b0fe8cd2a157

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16EXOT22.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# art-description: DAOD building EXOT22 mc16
+# art-type: grid
+# art-output: *.pool.root
+
+set -e
+
+Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.11866988._000378.pool.root.1 --outputDAODFile art.pool.root --reductionConf EXOT22 --maxEvents 10000 --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-40" ' 
+
+DAODMerge_tf.py --maxEvents 5 --inputDAOD_EXOT22File DAOD_EXOT22.art.pool.root --outputDAOD_EXOT22_MRGFile art_merged.pool.root
+checkFile.py DAOD_EXOT22.art.pool.root > checkFile.txt
