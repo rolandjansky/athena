aebd7f577c09728e2150a3e7c84a1bf7886123ba
Edward Moyse <edward.moyse@cern.ch>
2017-10-20 11:22:05 +0200

    Adding new digit to RDO convertor
    Former-commit-id: 1263ea842778168df43f4de651294390c313f13f

---
+++ MuonSpectrometer/MuonCnv/MuonByteStreamCnvTest/src/sTGC_DigitToRDO.cxx
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "StoreGate/StoreGateSvc.h"
+#include "MuonDigitContainer/TgcDigitContainer.h"
+#include "MuonDigitContainer/TgcDigitCollection.h"
+#include "MuonDigitContainer/TgcDigit.h"
+#include "sTGC_DigitToRDO.h"
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+sTGC_DigitToRDO::sTGC_DigitToRDO(const std::string& name, ISvcLocator* pSvcLocator)
+  : AthAlgorithm(name, pSvcLocator),
+    m_idHelper(0)
+{
+}
+
+StatusCode sTGC_DigitToRDO::initialize()
+{
+  ATH_MSG_DEBUG( " in initialize()"  );
+  ATH_CHECK( m_rdoContainer.initialize() );
+  ATH_CHECK( m_digitContainer.initialize() );
+  
+  if ( detStore()->retrieve(m_idHelper, "STGCIDHELPER").isFailure()) {
+    ATH_MSG_FATAL ( "Could not get sTGC IdHelper !" );
+    return StatusCode::FAILURE;
+  } 
+  
+  return StatusCode::SUCCESS;
+}
+
+StatusCode sTGC_DigitToRDO::execute()
+{  
+  ATH_MSG_DEBUG( "in execute()"  );
+  SG::WriteHandle<sTGC_RawDataContainer> rdos (m_rdoContainer);
+  SG::ReadHandle<sTgcDigitContainer> digits (m_digitContainer);
+  ATH_CHECK( rdos.record(std::unique_ptr<sTGC_RawDataContainer>(new sTGC_RawDataContainer(m_idHelper->module_hash_max())) ) );
+
+  for (auto digitColl : *digits ){
+    // Making some assumptions here that digit hash == RDO hash. 
+    IdentifierHash hash = digitColl->identifierHash();
+    sTGC_RawDataCollection* coll = new sTGC_RawDataCollection(hash);
+    if (rdos->addCollection(coll,hash).isFailure() ){
+      ATH_MSG_WARNING("Failed to add collection with hash " << (int)hash );
+      /// TODO cleanup
+    }
+    for (auto digit : *digitColl ){
+      Identifier id = digit->identify();
+      sTGC_RawData* rdo = new sTGC_RawData(id);
+      coll->push_back(rdo);
+    }
+  }
+  
+  ATH_MSG_DEBUG( "done execute()"  );
+  return StatusCode::SUCCESS;
+}
+
+StatusCode sTGC_DigitToRDO::finalize() {
+  return StatusCode::SUCCESS;
+}
---
+++ MuonSpectrometer/MuonCnv/MuonByteStreamCnvTest/src/sTGC_DigitToRDO.h
@@ -0,0 +1,38 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef sTGCDIGITTORDO_H
+#define sTGCDIGITTORDO_H
+
+#include "AthenaBaseComps/AthAlgorithm.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "StoreGate/DataHandle.h"
+
+#include "MuonRDO/sTGC_RawDataContainer.h"
+#include "MuonDigitContainer/sTgcDigitContainer.h"
+
+class sTgcIdHelper;
+
+/////////////////////////////////////////////////////////////////////////////
+
+class sTGC_DigitToRDO : public AthAlgorithm
+{
+
+public:
+
+  sTGC_DigitToRDO (const std::string& name, ISvcLocator* pSvcLocator);
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+
+private:
+
+protected:
+  
+  const sTgcIdHelper*  m_idHelper;
+  SG::WriteHandleKey<sTGC_RawDataContainer> m_rdoContainer{this,"RDOContainerName","sTGCRDO","sTGC_RawDataContainer handle key"};
+  SG::ReadHandleKey<sTgcDigitContainer> m_digitContainer{this,"DigitContainerName","sTGCSensitiveDetector","sTGC_RawDataContainer handle key"};
+};
+
+#endif
