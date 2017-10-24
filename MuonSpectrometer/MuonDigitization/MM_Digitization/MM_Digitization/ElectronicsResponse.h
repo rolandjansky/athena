355569b4bc7bc4aae3f761123c26e27455d789c3
Jochen Meyer <Jochen.Meyer@cern.ch>
2017-10-24 16:40:25 +0200

    adjust MM_Digitization code for NSW (ATLASSIM-3472)
    This merge request imports all micromegas digitization bug fixes
    implemented during the last months. Amongst others the gas input
    file got moved to the calibration area. There were also some
    adjustments regarding Atlas coding conventions. For more details
    see the Jira ticket. The compilation in 21.3 worked out fine.
    
    
    Former-commit-id: 6d683c62e82391872842d4f16ffd698806d853cf

--- MuonSpectrometer/MuonDigitization/MM_Digitization/MM_Digitization/ElectronicsResponse.h
+++ MuonSpectrometer/MuonDigitization/MM_Digitization/MM_Digitization/ElectronicsResponse.h
@@ -1,4 +1,7 @@
-/* -*- C++ -*- */
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 #ifndef ElectronicsResponse_h
 #define ElectronicsResponse_h
 /** @class ElectronicsResponse
