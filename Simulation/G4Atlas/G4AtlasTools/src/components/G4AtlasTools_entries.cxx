c63322833fa55ee11f3e6172d6510a4e7518ff67
John Chapman <jchapman@cern.ch>
2017-10-25 15:23:45 +0200

    Adding Geometry Configuration Tools
    These tools hang off the `DetectorGeometrySvc` and will allow geometry
    building from GDML to work properly. The first use-case uses them to
    define non-standard materials (replacing the code that was previously
    in `DetectorGeometrySvc::BuildExtraMaterials()`) and is in this merge
    request. They could also be used to set smartlessness or other tracking
    parameters in a neat way (possibly could be an alternate approach for
    ATLASSIM-1727). See ATLASSIM-3047.
    
    
    Former-commit-id: f1f0b249c9542a9c88fa43fe15fa2e5a42dc36d0

--- Simulation/G4Atlas/G4AtlasTools/src/components/G4AtlasTools_entries.cxx
+++ Simulation/G4Atlas/G4AtlasTools/src/components/G4AtlasTools_entries.cxx
@@ -13,6 +13,7 @@
 #include "G4AtlasTools/GlobalFieldManagerTool.h"
 #include "G4AtlasTools/DetectorFieldManagerTool.h"
 #include "../G4ThreadInitTool.h"
+#include "../MaterialDescriptionTool.h"
 
 DECLARE_COMPONENT( PhysicsListToolBase )
 DECLARE_COMPONENT( DetectorGeometryBase )
@@ -29,4 +30,4 @@ DECLARE_COMPONENT( AddPhysicsDecayTool )
 DECLARE_COMPONENT( GlobalFieldManagerTool )
 DECLARE_COMPONENT( DetectorFieldManagerTool )
 DECLARE_COMPONENT( G4ThreadInitTool )
-
+DECLARE_COMPONENT( MaterialDescriptionTool )
