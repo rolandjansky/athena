711c3ef581c46bb07f11cd64399c0c3f28128b64
John Chapman <jchapman@cern.ch>
2017-10-25 15:23:45 +0200

    Adding Geometry Configuration Tools
    These tools hang off the `DetectorGeometrySvc` and will allow geometry
    building from GDML to work properly. The first use-case uses them to
    define non-standard materials (replacing the code that was previously
    in `DetectorGeometrySvc::BuildExtraMaterials()`) and is in this merge
    request. They could also be used to set smartlessness or other tracking
    parameters in a neat way (possibly could be an alternate approach for
    ATLASSIM-1727). See ATLASSIM-3047.
    
    
    Former-commit-id: 4cb888e03b3f183717948e0fb10d0912e3b05dde

--- Simulation/G4Atlas/G4AtlasServices/src/DetectorGeometrySvc.cxx
+++ Simulation/G4Atlas/G4AtlasServices/src/DetectorGeometrySvc.cxx
@@ -29,6 +29,7 @@ DetectorGeometrySvc::DetectorGeometrySvc( const std::string& name, ISvcLocator* 
     m_detConstruction("",this),
     m_regionCreators(this),
     m_parallelWorlds(this),
+    m_configurationTools(this),
     m_fieldManagers(this),
     m_activateParallelWorlds(false)
 {
@@ -38,6 +39,7 @@ DetectorGeometrySvc::DetectorGeometrySvc( const std::string& name, ISvcLocator* 
   declareProperty( "ParallelWorlds" , m_parallelWorlds , "Tools to define G4 parallel worlds" );
   declareProperty( "ActivateParallelWorlds",m_activateParallelWorlds,"Toggle on/off the G4 parallel geometry system");
   declareProperty( "FieldManagers",m_fieldManagers,"field managers used");
+  declareProperty( "GeometryConfigurationTools",m_configurationTools,"Tools for geometry configuration");
   ATH_MSG_DEBUG( "DetectorGeometrySvc being created!" );
 }
 
@@ -46,13 +48,15 @@ DetectorGeometrySvc::~DetectorGeometrySvc()
 }
 
 StatusCode DetectorGeometrySvc::initialize(){
-  // First build additional volumes that might be required for geometry
-  //  Some of the tools will take action on initialize(), so the materials
-  //  should be available prior to their retrieval
-  BuildExtraMaterials();
 
   // go through all tools and retrieve them
   //  This fires initialize() for each of those tools
+  ATH_MSG_DEBUG( "Initializing Geometry configuration tools "  );
+  for (auto it: m_configurationTools)
+  {
+    ATH_CHECK( it.retrieve() );
+    ATH_CHECK( it->preGeometryConfigure() );
+  }
 
   ATH_MSG_DEBUG( "Setting up a DetectorConstruction " << m_detConstruction.name() );
   ATH_CHECK(m_detConstruction.retrieve());
@@ -83,6 +87,10 @@ StatusCode DetectorGeometrySvc::initialize(){
           m_detConstruction->RegisterParallelWorld(it->GetParallelWorld());
         }
     }
+
+  ATH_MSG_DEBUG( "Running geometry post-configuration tools" );
+  for (auto it: m_configurationTools)
+    ATH_CHECK( it->postGeometryConfigure() );
 
   ATH_MSG_DEBUG( "Setting up field managers" );
   ATH_CHECK( m_fieldManagers.retrieve() );
@@ -157,46 +165,3 @@ std::vector<std::string>& DetectorGeometrySvc::GetParallelWorldNames()
   return m_parallelWorldNames;
 }
 
-void DetectorGeometrySvc::BuildExtraMaterials()
-{
-  G4NistManager* man = G4NistManager::Instance();
-  G4Element* N   = man->FindOrBuildElement("N");
-  G4Element* O   = man->FindOrBuildElement("O");
-  G4Element* Ar  = man->FindOrBuildElement("Ar");
-  G4Element* H   = man->FindOrBuildElement("H");
-  G4Element* Si  = man->FindOrBuildElement("Si");
-  G4Element* Ca  = man->FindOrBuildElement("Ca");
-  G4Element* Al  = man->FindOrBuildElement("Al");
-  G4Element* Fe  = man->FindOrBuildElement("Fe");
-  G4Element* C   = man->FindOrBuildElement("C");
-
-  // Constructor of G4Material inserts it into a table for later use
-  G4Material *air = new G4Material("Air", 0.001214*CLHEP::g/CLHEP::cm3,4);
-  air->AddElement( N, 0.7494);
-  air->AddElement( O, 0.2369);
-  air->AddElement(Ar, 0.0129);
-  air->AddElement( H, 0.0008);
-
-  G4Material *vacuum = new G4Material("Vacuum", 0.000001*CLHEP::g/CLHEP::cm3,1);
-  vacuum->AddMaterial(air, 1.0);
-
-  G4Material *material_concrete = new G4Material("Concrete",2.3*CLHEP::g/CLHEP::cm3,6);
-  material_concrete->AddElement(Si,0.227915);
-  material_concrete->AddElement(O,0.605410);
-  material_concrete->AddElement(H,0.099720);
-  material_concrete->AddElement(Ca,0.049860);
-  material_concrete->AddElement(Al,0.014245);
-  material_concrete->AddElement(Fe,0.002850);
-
-  G4Material *material_scintillator = new G4Material("Scintillator",1.032*CLHEP::g/CLHEP::cm3,2);
-  material_scintillator->AddElement(C,0.5);
-  material_scintillator->AddElement(H,0.5);
-
-  G4Material *material_mylar = new G4Material("Mylar",1.39*CLHEP::g/CLHEP::cm3,3);
-  material_mylar->AddElement(H,4./11.);
-  material_mylar->AddElement(C,5./11.);
-  material_mylar->AddElement(O,2./11.);
-
-  G4Material *material_rock = new G4Material("Rock",2.33*CLHEP::g/CLHEP::cm3,1);
-  material_rock->AddElement(Si,1);
-}
