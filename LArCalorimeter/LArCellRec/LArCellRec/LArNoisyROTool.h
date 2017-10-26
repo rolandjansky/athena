fa9e719a0df8ff661f8d6f95e3833cb1aa2029df
Pavol Strizenec <pavol@pckeatlas05.cern.ch>
2017-10-26 15:07:32 +0200

    Fixing missing copyright
    Former-commit-id: d00fab5ada93b17dc1685086d8dfb84e273747d1

--- LArCalorimeter/LArCellRec/LArCellRec/LArNoisyROTool.h
+++ LArCalorimeter/LArCellRec/LArCellRec/LArNoisyROTool.h
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 ///////////////////////// -*- C++ -*- /////////////////////////////
 // LArNoisyROTool.h 
 // Header file for class LArNoisyROTool
--- LArCalorimeter/LArCellRec/python/LArNoisyROSummaryGetter.py
+++ LArCalorimeter/LArCellRec/python/LArNoisyROSummaryGetter.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
 from RecExConfig.Configured import Configured
 from LArCellRec.LArNoisyROFlags import larNoisyROFlags
 
