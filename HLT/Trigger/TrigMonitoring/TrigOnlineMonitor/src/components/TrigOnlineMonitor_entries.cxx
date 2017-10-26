381929dd32d9645d73284951d3854e702a617855
gerbaudo <davide.gerbaudo@gmail.com>
2017-10-26 15:33:38 +0200

    remove TrigL1TopoROBMonitor
    Details:
    see AcceptL1TopoMonitor under Trigger/TrigAlgorithms/TrigGenericAlgs
    
    Former-commit-id: 1467c568007cd390a56fa4c21029760051236aad

--- HLT/Trigger/TrigMonitoring/TrigOnlineMonitor/src/components/TrigOnlineMonitor_entries.cxx
+++ HLT/Trigger/TrigMonitoring/TrigOnlineMonitor/src/components/TrigOnlineMonitor_entries.cxx
@@ -2,19 +2,16 @@
 #include "TrigOnlineMonitor/TrigROBMonitor.h"
 #include "TrigOnlineMonitor/TrigMuCTPiROBMonitor.h"
 #include "src/TrigALFAROBMonitor.h"
-#include "src/TrigL1TopoROBMonitor.h"
 #include "src/TrigL1TopoWriteValData.h"
 
 DECLARE_ALGORITHM_FACTORY( TrigROBMonitor )
 DECLARE_ALGORITHM_FACTORY( TrigMuCTPiROBMonitor )
 DECLARE_ALGORITHM_FACTORY( TrigALFAROBMonitor )
-DECLARE_ALGORITHM_FACTORY( TrigL1TopoROBMonitor )
 DECLARE_ALGORITHM_FACTORY( TrigL1TopoWriteValData )
 
 DECLARE_FACTORY_ENTRIES(TrigOnlineMonitor) {
     DECLARE_ALGORITHM( TrigROBMonitor )
     DECLARE_ALGORITHM( TrigMuCTPiROBMonitor )
     DECLARE_ALGORITHM( TrigALFAROBMonitor )
-    DECLARE_ALGORITHM( TrigL1TopoROBMonitor )
     DECLARE_ALGORITHM( TrigL1TopoWriteValData )
 }
