0f1fe7a5a57ca3dcd08c0f8c1b8b45871205742d
Knut Zoch <knut.zoch@cern.ch>
2017-10-30 10:42:59 +0000

    Recover lost copyright header
    Former-commit-id: 2b38a618d9dd8d105bc46a6fa7d3f3661c5572d1

--- InnerDetector/InDetCalibAlgs/PixelCalibAlgs/src/NoiseMapBuilder.cxx
+++ InnerDetector/InDetCalibAlgs/PixelCalibAlgs/src/NoiseMapBuilder.cxx
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+ */
+
 // PixelCalibAlgs
 #include "PixelCalibAlgs/NoiseMapBuilder.h"
 #include "PixelCalibAlgs/PixelConvert.h"
