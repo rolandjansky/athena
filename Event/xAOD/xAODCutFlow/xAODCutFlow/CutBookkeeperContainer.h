b2ca4d2e764d4f404dc0b444b556c90473f04393
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2017-10-31 14:58:16 +0100

    Hid the metadata container declarations when in standalone mode.
    Former-commit-id: 22d19bffee09c7ce4aa13296a6603cd7d2a1f0ff

--- Event/xAOD/xAODCutFlow/xAODCutFlow/CutBookkeeperContainer.h
+++ Event/xAOD/xAODCutFlow/xAODCutFlow/CutBookkeeperContainer.h
@@ -22,9 +22,11 @@ namespace xAOD {
 // Set up a CLID for the container:
 #include "xAODCore/CLASS_DEF.h"
 CLASS_DEF( xAOD::CutBookkeeperContainer, 1234982351, 1 )
+#ifndef XAOD_STANDALONE
 #include "AthenaKernel/MetaCont.h"
 CLASS_DEF( MetaCont<xAOD::CutBookkeeperContainer> , 34480468 , 1 )
 #include "SGTools/BaseInfo.h"
 SG_BASE( MetaCont<xAOD::CutBookkeeperContainer>, MetaContBase );
+#endif // not XAOD_STANDALONE
 
 #endif // XAODCUTFLOW_CUTBOOKKEEPERCONTAINER_H
