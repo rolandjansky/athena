444597b9bc3b5f2754407472dda57be0b996c5e6
Frank Winklmeier <frank.winklmeier@cern.ch>
2017-11-01 17:52:41 +0100

    Remove deprecated DECLARE_FACTORY_ENTRIES
    Former-commit-id: 58f34028605d6813cb4b27f5cba1bd91a03b4352

--- AtlasGeometryCommon/SubDetectorEnvelopes/src/components/SubDetectorEnvelopes_entries.cxx
+++ AtlasGeometryCommon/SubDetectorEnvelopes/src/components/SubDetectorEnvelopes_entries.cxx
@@ -1,9 +1,4 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
 #include "src/DetDescrDBEnvelopeSvc.h"
 
 DECLARE_COMPONENT( DetDescrDBEnvelopeSvc )
-
-DECLARE_FACTORY_ENTRIES( SubDetectorEnvelopes ) {
-  DECLARE_SERVICE( DetDescrDBEnvelopeSvc )
-}
 
