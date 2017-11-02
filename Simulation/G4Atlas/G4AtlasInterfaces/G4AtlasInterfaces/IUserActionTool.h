b49727f0df454a53d01557d70a0179e945bce20c
Steve Farrell <sfarrell@cern.ch>
2017-11-02 22:21:27 +0100

    Adding new IUserActionTool interface.
    This is a consolidated user action tool interface which uses a struct of
    user action lists so tools can plug their actions into relevant places.
    
    
    Former-commit-id: 87d99a993d4202f679728d054f83571213b1c4f1

---
+++ Simulation/G4Atlas/G4AtlasInterfaces/G4AtlasInterfaces/IUserActionTool.h
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef G4ATLASINTERFACES_IUSERACTIONTOOL_H
+#define G4ATLASINTERFACES_IUSERACTIONTOOL_H
+
+// System includes
+#include <vector>
+
+// Framework includes
+#include "GaudiKernel/IAlgTool.h"
+
+// Geant4 forward declarations
+class G4UserRunAction;
+class G4UserEventAction;
+class G4UserTrackingAction;
+class G4UserSteppingAction;
+class G4UserStackingAction;
+
+namespace G4UA
+{
+
+  /// @class G4AtlasUserActions
+  /// @brief Struct for passing around user actions.
+  ///
+  /// Tools push their actions onto the appropriate lists.
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  struct G4AtlasUserActions
+  {
+    std::vector<G4UserRunAction*> runActions;
+    std::vector<G4UserEventAction*> eventActions;
+    std::vector<G4UserTrackingAction*> trackingActions;
+    std::vector<G4UserSteppingAction*> steppingActions;
+    std::vector<G4UserStackingAction*> stackingActions;
+  };
+
+  /// @class IUserActionTool
+  /// @brief Abstract interface for tools that construct G4 user actions.
+  ///
+  /// @todo add more details.
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  class IUserActionTool : virtual public IAlgTool
+  {
+
+    public:
+
+      /// Constructs a user action and populates the user action lists with it.
+      virtual StatusCode fillUserAction(G4AtlasUserActions* userActions) = 0;
+
+      /// Declare this interface to the framework
+      DeclareInterfaceID(IUserActionTool, 1, 0);
+
+  }; // class IUserActionTool
+
+} // namespace G4UA
+
+#endif // G4ATLASINTERFACES_IUSERACTIONTOOL_H
