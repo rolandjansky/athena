fda5867d6a6b91502c544916226118842353da0c
morgens <marcus.matthias.morgenstern@cern.ch>
2017-11-03 18:21:41 +0100

    revert wrong merge
    Former-commit-id: 14e8de79b054fd2cea21a3bfafacdff98e6b8bed

--- Reconstruction/MuonIdentification/MuonCombinedBaseTools/src/components/MuonCombinedBaseTools_entries.cxx
+++ Reconstruction/MuonIdentification/MuonCombinedBaseTools/src/components/MuonCombinedBaseTools_entries.cxx
@@ -1,4 +1,3 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
 #include "../MuonCandidateTool.h"
 #include "../MuonCombinedTool.h"
 #include "../MuonCombinedDebuggerTool.h"
@@ -13,30 +12,14 @@
 
 using namespace MuonCombined;
 using namespace Muon;
-DECLARE_TOOL_FACTORY( MuonCandidateTool )
-DECLARE_TOOL_FACTORY( MuonCombinedTool )
-DECLARE_TOOL_FACTORY( MuonCombinedDebuggerTool )
-DECLARE_TOOL_FACTORY( MuonCreatorTool )
-DECLARE_TOOL_FACTORY( MuonDressingTool )
-DECLARE_TOOL_FACTORY( MuonCombinedStacoTagTool )
-DECLARE_TOOL_FACTORY( MuonCombinedFitTagTool )
-DECLARE_TOOL_FACTORY( MuonSegmentTagTool )
-DECLARE_TOOL_FACTORY( MuGirlTagTool )
-DECLARE_TOOL_FACTORY( MuonCaloTagTool )
-DECLARE_TOOL_FACTORY( TrackSegmentAssociationTool )
-
-DECLARE_FACTORY_ENTRIES( MuonCombinedBaseTools )
-{
-  DECLARE_TOOL( MuonCandidateTool )
-  DECLARE_TOOL( MuonCombinedTool )
-  DECLARE_TOOL( MuonCombinedDebuggerTool )
-  DECLARE_TOOL( MuonCreatorTool )
-  DECLARE_TOOL( MuonDressingTool )
-  DECLARE_TOOL( MuonCombinedStacoTagTool )
-  DECLARE_TOOL( MuonCombinedFitTagTool )
-  DECLARE_TOOL( MuonSegmentTagTool )
-  DECLARE_TOOL( MuGirlTagTool )
-  DECLARE_TOOL( MuonCaloTagTool )
-  DECLARE_TOOL( TrackSegmentAssociationTool )
-}
-
+DECLARE_COMPONENT( MuonCandidateTool )
+DECLARE_COMPONENT( MuonCombinedTool )
+DECLARE_COMPONENT( MuonCombinedDebuggerTool )
+DECLARE_COMPONENT( MuonCreatorTool )
+DECLARE_COMPONENT( MuonDressingTool )
+DECLARE_COMPONENT( MuonCombinedStacoTagTool )
+DECLARE_COMPONENT( MuonCombinedFitTagTool )
+DECLARE_COMPONENT( MuonSegmentTagTool )
+DECLARE_COMPONENT( MuGirlTagTool )
+DECLARE_COMPONENT( MuonCaloTagTool )
+DECLARE_COMPONENT( TrackSegmentAssociationTool )
