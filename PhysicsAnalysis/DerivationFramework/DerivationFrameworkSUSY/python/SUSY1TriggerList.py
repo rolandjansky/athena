90fb9f3c3ca87ef6637c6f4ac516dd03f203867d
Walter Hopkins <whopkins@uoregon.edu>
2017-11-07 03:10:35 -0500

    Updated SUSY1 trigger list.
    Former-commit-id: ae8058b78361cf21c67c9ad96ee404d79cd07d8d

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkSUSY/python/SUSY1TriggerList.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkSUSY/python/SUSY1TriggerList.py
@@ -1,250 +1,157 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-SUSY1ElectronTriggers = [
-    'HLT_e26_lhtight_smooth_iloose',
-    'HLT_e26_lhtight_smooth_ivarloose',
-    'HLT_e26_lhtight_iloose',
-    'HLT_e26_lhtight_ivarloose',
-    'HLT_e26_lhtight_nod0_iloose',
-    'HLT_e26_lhtight_nod0_ivarloose',
-    'HLT_e26_lhtight_cutd0dphideta_iloose',
-    'HLT_e28_lhtight_smooth_iloose',
-    'HLT_e28_lhtight_smooth_ivarloose',
-    'HLT_e28_lhtight_iloose',
-    'HLT_e28_lhtight_ivarloose',
-    'HLT_e28_lhtight_nod0_iloose',
-    'HLT_e28_lhtight_nod0_ivarloose',
-    'HLT_e28_lhtight_nod0_ringer_iloose',
-    'HLT_e28_lhtight_nod0_ringer_ivarloose',
-    'HLT_e60_lhmedium',
-    'HLT_e60_lhmedium_nod0',
-    'HLT_e60_lhmedium_L1EM24VHI', 
-    'HLT_e60_lhmedium_nod0_L1EM24VHI', 
-    'HLT_e140_lhloose',
-    'HLT_e140_lhloose_nod0', 
-    'HLT_e140_lhloose_L1EM24VHI', 
-    'HLT_e140_lhloose_nod0_L1EM24VHI', 
-    'HLT_e300_etcut', 
-    'HLT_e60_lhmedium_cutd0dphideta',
-    'HLT_e26_lhtight_idperf',
-    'HLT_e24_lhtight_iloose',
-    'HLT_e24_lhtight_nod0_iloose',
-    'HLT_e24_lhtight_ivarloose',
-    'HLT_e24_lhtight_nod0_ivarloose',
-    'HLT_e24_lhtight_cutd0dphideta_iloose',
-    'HLT_e17_lhloose_nod0_2e9_lhloose_nod0',
-    'HLT_e24_lhmedium_iloose',
-    'HLT_e24_lhmedium_ivarloose',
-    'HLT_e24_lhmedium_nod0_iloose',
-    'HLT_e24_lhmedium_nod0_ivarloose', 
-    'HLT_e24_lhmedium_iloose_L1EM20VH',
-    'HLT_e24_lhmedium_nod0_iloose_L1EM20VH',
-    'HLT_e24_lhmedium_ivarloose_L1EM20VH',
-    'HLT_e24_lhmedium_nod0_ivarloose_L1EM20VH',
-    'HLT_e24_lhmedium_L1EM20VH',
-    'HLT_e24_lhmedium_nod0_L1EM20VH',
-    'HLT_e120_lhloose',
-    'HLT_e120_lhloose_nod0',
-    'HLT_e24_lhmedium_iloose_L1EM18VH',
-    'HLT_e24_lhmedium_nod0_iloose_L1EM18VH',
-    'HLT_e24_lhmedium_ivarloose_L1EM18VH',
-    'HLT_e24_lhmedium_nod0_ivarloose_L1EM18VH',
-    'HLT_e24_lhmedium_nod0_ringer_iloose',
-    'HLT_e24_lhmedium_nod0_ringer_ivarloose'
-    ]
+################################################################ 
+#
+# the trigger list below is created as of Oct23, 2017 based on Twiki pages:
+# https://twiki.cern.ch/twiki/bin/viewauth/Atlas/LowestUnprescaled
+# https://twiki.cern.ch/twiki/bin/view/Atlas/L34TriggerMenu2017
+#
+################################################################ 
 
-SUSY1MuonTriggers = [
-    'HLT_mu26_imedium',
-    'HLT_mu26_ivarmedium',
-    'HLT_mu50',
-    'HLT_mu60_0eta105_msonly',
-    'HLT_mu24_imedium',
-    'HLT_mu24_ivarmedium',
-    'HLT_mu40',
-    'HLT_mu24_ivarloose',
-    'HLT_mu24_iloose',
-    'HLT_mu24_iloose_L1MU15',
-    'HLT_mu24_ivarloose_L1MU15',
-    'HLT_mu20_iloose_L1MU15',
-    'HLT_mu20_ivarloose_L1MU15'
-    ]
 
-SUSY1DiLeptonTriggers = [
-    'HLT_2e12_lhloose_L12EM10VH',
-    'HLT_2e15_lhloose_L12EM13VH',
-    'HLT_2e17_lhloose',
-    'HLT_2e17_lhloose_cutd0dphideta',
-    'HLT_2e17_lhvloose',
-    'HLT_2e17_lhvloose_nod0',
-    'HLT_2e15_lhvloose_L12EM13VH',
-    'HLT_2e15_lhvloose_nod0_L12EM13VH',
-    'HLT_2e12_lhvloose_L12EM10VH',
-    'HLT_2e12_lhvloose_nod0_L12EM10VH',
-    'HLT_2mu10',
-    'HLT_2mu10_nomucomb',
-    'HLT_2mu14',
-    'HLT_2mu14_nomucomb',
-    'HLT_mu18_mu8noL1',
-    'HLT_mu20_mu8noL1',
-    'HLT_mu22_mu8noL1',
-    'HLT_mu24_mu8noL1',
-    'HLT_e7_lhmedium_mu24',
-    'HLT_e17_lhloose_mu14',
-    'HLT_e24_lhmedium_L1EM20VHI_mu8noL1',
-    'HLT_e26_lhmedium_L1EM22VHI_mu8noL1'
-    ]
-
-SUSY1TriLeptonTriggers = [
-    'HLT_e17_lhloose_2e9_lhloose',
-    'HLT_e17_lhmedium_2e9_lhmedium',
-    'HLT_e17_lhmedium_nod0_2e9_lhmedium_nod0',
-    'HLT_e17_lhloose_nod0_2e10_lhloose_nod0_L1EM15VH_3EM8VH',
-    'HLT_mu24_2mu4noL1',
-    'HLT_mu22_2mu4noL1',
-    'HLT_mu20_2mu4noL1',
-    'HLT_mu18_2mu4noL1',
-    'HLT_3mu4',
-    'HLT_3mu6',
-    'HLT_3mu6_msonly'
-    ]
-
-SUSY1LeptonJetTriggers = [
-    'HLT_mu6_2j40_0eta490_invm400_L1MU6_J30.0ETA49_2J20.0ETA49',
-    'HLT_mu6_2j40_0eta490_invm400',
-    'HLT_e15_lhtight_iloose_3j20_L1EM13VH_3J20',
-    'HLT_e15_lhtight_ivarloose_3j20_L1EM13VH_3J20',
-    'HLT_e15_lhtight_nod0_iloose_3j20_L1EM13VH_3J20',
-    'HLT_e15_lhtight_nod0_ivarloose_3j20_L1EM13VH_3J20',
-    'HLT_mu14_iloose_3j20_L1MU10_3J20',
-    'HLT_mu14_ivarloose_3j20_L1MU10_3J20',
-    ]
-
-SUSY1LeptonTriggers = SUSY1ElectronTriggers + SUSY1MuonTriggers + SUSY1DiLeptonTriggers + SUSY1TriLeptonTriggers + SUSY1LeptonJetTriggers
-
-SUSY1PhotonTriggers = [
-    'HLT_g20_loose',
-    'HLT_g20_loose_L1EM12',
-    'HLT_g20_loose_L1EM15',
-    'HLT_g25_loose',
-    'HLT_g25_loose_L1EM15',
-    'HLT_g30_loose',
-    'HLT_g30_loose_L1EM15',
-    'HLT_g35_loose',
-    'HLT_g35_loose_L1EM15',
-    'HLT_g40_loose_L1EM15',
-    'HLT_g45_loose_L1EM15',
-    'HLT_g50_loose',
-    'HLT_g50_loose_L1EM15',
-    'HLT_g60_loose',
-    'HLT_g60_loose_L1EM15VH',
-    'HLT_g70_loose',
-    'HLT_g80_loose',
-    'HLT_g100_loose',
-    'HLT_g120_loose',
-    'HLT_g140_loose',
-    'HLT_g140_loose_L1EM24VHI',
-    'HLT_g120_loose',
-    'HLT_g140_loose',
-    'HLT_g200_etcut',
-    'HLT_g300_etcut'
-    ]
-
-SUSY1XETriggers = [
-    'HLT_xe70_tc_lcw',
-    'HLT_xe70_mht',
-    'HLT_xe70',
-    'HLT_xe80_tc_lcw_L1XE50',
-    'HLT_xe90_tc_lcw_L1XE50',
-    'HLT_xe90_mht_L1XE50',
-    'HLT_xe100_tc_lcw_L1XE50',
-    'HLT_xe100_tc_lcw_L1XE60',
-    'HLT_xe100_mht_L1XE50',
-    'HLT_xe100_L1XE50',
-    'HLT_xe100_L1XE60',
-    'HLT_xe110_mht_L1XE50',
-    'HLT_xe100_mht_L1XE60',
-    'HLT_xe110_pueta_L1XE50', 
-    'HLT_xe110_pufit_L1XE50',    
-    'HLT_xe110_L1XE50', 
-    'HLT_xe110_tc_em_L1XE50',
-    'HLT_xe110_mht_L1XE50_AND_xe70_L1XE50',
-    'HLT_xe110_mht_L1XE50_AND_xe70_L1XE55'
-    ]
-
-SUSY1CombinedXETriggers = [
-    'HLT_g60_loose_xe60noL1',
-    'HLT_g70_loose_xe70noL1',
-    'HLT_j80_xe80',
-    'HLT_j80_boffperf_split_L12J50_XE40',
-    'HLT_j80_boffperf_split_xe70_L1J400ETA25_XE50',
-    'HLT_j100_boffperf_split_L1XE60',
-    'HLT_j80_bmv2c2060_split_L12J50_XE40',
-    'HLT_j80_bmv2c2060_split_xe60_L12J50_XE40',
-    'HLT_j80_bmv2c2077_split_xe70_L1J400ETA25_XE50',
-    'HLT_j100_bmv2c2070_split_L1XE60',
-    'HLT_j100_xe110_mht_1dphi10_L1J40_DPHI-J20s2XE50'
-    ]
-
-SUSY1RazorTriggers = [
-    'HLT_j30_xe60_razor100',
-    'HLT_j30_xe60_razor170',
-    'HLT_j30_xe60_razor185',
-    'HLT_j30_xe60_razor195',
-    'HLT_j30_xe60_razor220']
-
-SUSY1JetTriggers =[ 
-    'HLT_j15',
-    'HLT_j25',
-    'HLT_j35',
-    'HLT_j55',
-    'HLT_j60',
-    'HLT_j85',
-    'HLT_j110',
-    'HLT_j150',
-    'HLT_j175',
-    'HLT_j200',
-    'HLT_j260',
-    'HLT_j300',
-    'HLT_j320',
-    'HLT_j360',
-    'HLT_j380',
-    'HLT_j400',
-    'HLT_j420',
-    'HLT_j440',
-    'HLT_j460',
-    'HLT_noalg_L1J400'
+# 2017 menu (covering 1.5e34 to 2.0e34 menus)
+MET_2017 = [
+'HLT_xe90_pufit_L1XE50',
+'HLT_xe100_pufit_L1XE50',
+'HLT_xe100_pufit_L1XE55',
+'HLT_xe110_pufit_L1XE50',
+'HLT_xe110_pufit_L1XE55',
+'HLT_xe110_pufit_L1XE60',
+'HLT_xe120_pufit_L1XE50',
+'HLT_xe120_pufit_L1XE55',
+'HLT_xe120_pufit_L1XE60',
 ]
 
+SingleLepton_2017 = [
+'HLT_e26_lhtight_nod0_ivarloose',
+'HLT_e26_lhtight_nod0_ivarloose_L1EM22VHIM',
+'HLT_e28_lhtight_nod0_ivarloose',
+'HLT_e28_lhtight_nod0_ivarloose_L1EM24VHIM',
+'HLT_e60_lhmedium_nod0',
+'HLT_e60_lhmedium_nod0_L1EM24VHI',
+'HLT_e140_lhloose_nod0',
+'HLT_e140_lhloose_nod0_L1EM24VHI',
+'HLT_e300_etcut',
+'HLT_e300_etcut_L1EM24VHI',
+'HLT_mu26_ivarmedium',
+'HLT_mu50',
+'HLT_mu60',
+'HLT_mu60_0eta105_msonly',
+]
 
-#do we need multiphoton triggers?
-#    HLT_g35_loose_g25_loose
-#    HLT_g35_loose_L1EM15_g25_loose_L1EM15
-#    HLT_g20_loose_2g15_loose_L12EM13VH
+DiLepton_2017 = [
+'HLT_2e17_lhvloose_nod0_L12EM15VHI',
+'HLT_2e24_lhvloose_nod0',
+'HLT_e17_lhloose_nod0_mu14',
+'HLT_e26_lhmedium_nod0_mu8noL1',
+'HLT_e7_lhmedium_nod0_mu24',
+'HLT_2mu14',
+'HLT_mu22_mu8noL1',
+'HLT_mu22_mu8noL1_calotag_0eta010',
+'HLT_mu24_mu8noL1',
+'HLT_mu24_mu8noL1_calotag_0eta010',
+'HLT_mu26_mu10noL1',
+]
 
-SUSY1BjetTriggers = ['HLT_2j55_boffperf_split_ht300_L14J20',
-                     'HLT_2j45_boffperf_split_2j45',
-                     'HLT_j75_boffperf_split_3j75',
-                     'HLT_j225_bmv2c2085_split',
-                     'HLT_j80_boffperf_split_L12J50_XE40',
-                     'HLT_2j35_bmv2c2070_split_2j35_L14J15.0ETA25',
-                     'HLT_j150_bmv2c2077_split_j50_bmv2c2077_split',
-                     'HLT_j100_2j55_boffperf_split',
-                     'HLT_2j55_bmv2c2060_split_ht300_L14J15',
-                     'HLT_2j45_bmv2c2077_split_3j45_L14J15.0ETA25',
-                     'HLT_j75_bmv2c2070_split_3j75_L14J15.0ETA25',
-                     'HLT_j225_bmv2c2060_split',
-                     'HLT_2j55_bmv2c2050_split_ht300_L14J15',
-                     'HLT_2j45_bmv2c2070_split_3j45_L14J15.0ETA25',
-                     'HLT_j75_bmv2c2060_split_3j75_L14J15.0ETA25',
-                     'HLT_j225_bmv2c2050_split',
-                     'HLT_j80_bmv2c2060_split_xe60_L12J50_XE40',
-                     'HLT_2j35_bmv2c2060_split_2j35_L14J15.0ETA25',
-                     'HLT_j150_bmv2c2060_split_j50_bmv2c2060_split',
-                     'HLT_j100_2j55_bmv2c2060_split']
+SinglePhoton_2017 = [
+'HLT_g140_loose',
+'HLT_g140_tight',
+'HLT_g200_loose',
+'HLT_g300_etcut',
+'HLT_g300_etcut_L1EM24VHI',
+]
+
+# 2016 menu
+MET_2016 = [
+'HLT_xe80_tc_lcw_L1XE50',
+'HLT_xe90_mht_L1XE50',
+'HLT_xe90_tc_lcw_wEFMu_L1XE50',
+'HLT_xe90_mht_wEFMu_L1XE50',
+'HLT_xe100_L1XE50',
+'HLT_xe100_tc_em_L1XE50',
+'HLT_xe110_pueta_L1XE50',
+'HLT_xe110_mht_L1XE50',
+'HLT_xe120_tc_lcw_L1XE50',
+'HLT_xe120_pueta',
+'HLT_xe120_pufit',
+]
+
+SingleLepton_2016 = [
+'HLT_e24_lhtight_nod0_ivarloose',
+'HLT_e26_lhtight_nod0_ivarloose',
+'HLT_e60_lhmedium_nod0',
+'HLT_e60_medium',
+'HLT_e140_lhloose_nod0',
+'HLT_e300_etcut',
+'HLT_mu24_iloose',
+'HLT_mu24_iloose_L1MU15',
+'HLT_mu24_ivarloose',
+'HLT_mu24_ivarloose_L1MU15',
+'HLT_mu24_ivarmedium',
+'HLT_mu24_imedium',
+'HLT_mu26_ivarmedium',
+'HLT_mu26_imedium',
+'HLT_mu40',
+'HLT_mu50',
+]
+
+DiLepton_2016 = [
+'HLT_2e15_lhvloose_nod0_L12EM13VH',
+'HLT_2e17_lhvloose_nod0',
+'HLT_e17_lhloose_nod0_mu14',
+'HLT_e7_lhmedium_nod0_mu24',
+'HLT_e24_lhmedium_nod0_L1EM20VHI_mu8noL1',
+'HLT_e26_lhmedium_nod0_L1EM22VHI_mu8noL1',
+'HLT_2mu10',
+'HLT_2mu10_nomucomb',
+'HLT_2mu14',
+'HLT_2mu14_nomucomb',
+'HLT_mu20_mu8noL1',
+'HLT_mu20_nomucomb_mu6noL1_nscan03',
+'HLT_mu20_msonly_mu10noL1_msonly_nscan05_noComb',
+'HLT_mu22_mu8noL1',
+]
+
+SinglePhoton_2016 = [
+'HLT_g140_loose',
+'HLT_g300_etcut',
+]
+
+# 2015 menu
+MET_2015 = [
+'HLT_xe70_mht',
+]
+
+SingleLepton_2015 = [
+'HLT_e24_lhmedium_L1EM20VH',
+'HLT_e60_lhmedium',
+'HLT_e120_lhloose',
+'HLT_mu20_iloose_L1MU15',
+'HLT_mu40',
+'HLT_mu60_0eta105_msonly',
+]
+
+DiLepton_2015 = [
+'HLT_2e12_lhloose_L12EM10VH',
+'HLT_e17_lhloose_mu14',
+'HLT_e24_lhmedium_L1EM20VHI_mu8noL1',
+'HLT_e7_lhmedium_mu24',
+'HLT_2mu10',
+'HLT_mu18_mu8noL1',
+]
+
+SinglePhoton_2015 = [
+'HLT_g120_loose',
+'HLT_g200_etcut',
+]
+
+SUSY1LeptonTriggers = SingleLepton_2015+SingleLepton_2016+SingleLepton_2017+DiLepton_2015+DiLepton_2016+DiLepton_2017
+SUSY1PhotonTriggers = SinglePhoton_2015+SinglePhoton_2016+SinglePhoton_2017
+SUSY1XETriggers = MET_2015+MET_2016+MET_2017
 
 #all triggers
-SUSY1AllTriggers = SUSY1LeptonTriggers + SUSY1PhotonTriggers + SUSY1XETriggers + SUSY1CombinedXETriggers + SUSY1BjetTriggers + SUSY1RazorTriggers + SUSY1JetTriggers
+SUSY1AllTriggers = SUSY1LeptonTriggers + SUSY1PhotonTriggers + SUSY1XETriggers# + SUSY1CombinedXETriggers + SUSY1BjetTriggers + SUSY1RazorTriggers + SUSY1JetTriggers
 
 #just those we need the navigation thinning for
-SUSY1ThinTriggers = SUSY1ElectronTriggers + SUSY1MuonTriggers + SUSY1DiLeptonTriggers + SUSY1PhotonTriggers
+SUSY1ThinTriggers = SUSY1LeptonTriggers + SUSY1PhotonTriggers
 
