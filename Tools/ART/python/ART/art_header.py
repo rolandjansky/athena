86bf9e3ea04139db38d916e198ba59e4cca11cb5
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2017-11-08 19:25:55 +0000

    Merge branch 'art-v053' into '21.0'
    ART v0.5.4
    
    See merge request !6314
    
    Former-commit-id: 22b4f34bdff783bec9b3be28daf17d19864d8a74

--- Tools/ART/python/ART/art_header.py
+++ Tools/ART/python/ART/art_header.py
@@ -6,7 +6,7 @@ __author__ = "Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>"
 
 import re
 
-from copy import copy
+from types import IntType
 from types import ListType
 from types import StringType
 
@@ -16,59 +16,78 @@ class ArtHeader(object):
 
     def __init__(self, filename):
         """TBD."""
-        self.header = re.compile(r'#\s(art-[\w-]+):\s+(.+)$')
+        self.header_format = re.compile(r'#\s(art-[\w-]+):\s+(.+)$')
         self.header_format_error1 = re.compile(r'#(art-[\w-]+):\s*(.+)$')
         self.header_format_error2 = re.compile(r'#\s\s+(art-[\w-]+):\s*(.+)$')
         self.header_format_error3 = re.compile(r'#\s(art-[\w-]+):\S(.*)$')
 
         self.filename = filename
 
-        self.type = {}
-        self.defaultValue = {}
-        self.constraint = {}
-        self.value = {}
+        self.header = {}
 
+        # general
         self.add('art-description', StringType, '')
         self.add('art-type', StringType, None, ['build', 'grid'])
+
+        # "build" type only
         self.add('art-ci', ListType, [])
+
+        # "grid" type only
+        self.add('art-include', ListType, ['*'])
         self.add('art-output', ListType, [])
+        self.add('art-input', StringType, None)
+        self.add('art-input-file', ListType, [])
+        self.add('art-input-nfiles', IntType, 0)
+        self.add('art-input-nevents', IntType, 0)
+        self.add('art-input-split', IntType, 0)
 
         self.read(filename)
 
-    def add(self, key, type, defaultValue=None, constraint=None):
+    def add(self, key, value_type, default_value=None, constraint=None):
         """TBD."""
-        self.type[key] = type
-        self.defaultValue[key] = defaultValue
-        self.constraint[key] = constraint
-        self.value[key] = copy(defaultValue)
+        self.header[key] = {}
+        self.header[key]['type'] = value_type
+        self.header[key]['default'] = default_value
+        self.header[key]['constraint'] = constraint
+        self.header[key]['value'] = None    # never set
 
     def is_list(self, key):
         """TBD."""
-        return self.type[key] is ListType if key in self.type else False
+        return self.header[key]['type'] is ListType if key in self.header else False
 
     def read(self, filename):
         """Read all headers from file."""
         for line in open(filename, "r"):
-            line_match = self.header.match(line)
+            line_match = self.header_format.match(line)
             if line_match:
                 key = line_match.group(1)
                 value = line_match.group(2)
-                if self.type[key] == StringType:
+                if key in self.header and self.header[key]['type'] == StringType:
                     value = value.strip()
 
                 if self.is_list(key):
-                    self.value[key].append(value)
+                    if self.header[key]['value'] is None:
+                        self.header[key]['value'] = []
+                    self.header[key]['value'].append(value)
                 else:
-                    self.value[key] = value
+                    if key not in self.header:
+                        self.header[key] = {}
+                    self.header[key]['value'] = value
 
     def get(self, key):
         """TBD."""
-        return self.value[key] if key in self.value else None
+        if key not in self.header:
+            return None
+
+        if self.header[key]['value'] is None:
+            return self.header[key]['default']
+
+        return self.header[key]['value']
 
     def print_it(self):
         """TBD."""
-        for key in self.type:
-            print key, self.type[key], self.defaultValue[key], self.value[key], self.constraint[key]
+        for key in self.header:
+            print key, self.header[key]['type'], self.header[key]['default'], self.header[key]['value'], self.header[key]['constraint']
 
     def validate(self):
         """
@@ -78,7 +97,7 @@ class ArtHeader(object):
         - a header is spaced correctly (e.g. '#art-header: value')
         - a value in a header is not spaced correctly (e.g. '# art-header:value')
         - a key is found which is not defined
-        - a value is found of the wrong type
+        - a value is found of the wrong value_type
         - a value is found outside the constraint
         """
         for line in open(self.filename, "r"):
@@ -95,20 +114,20 @@ class ArtHeader(object):
                 print "ERROR: Header Validation - invalid header format, use at least one space between ': and value' in file", self.filename
                 print
 
-        for key in self.value:
-            if key not in self.type:
-                print "ERROR: Header Validation - Invalid key: '" + key + "' in file", self.filename
+        for key in self.header:
+            if 'type' not in self.header[key]:
+                print "ERROR: Header Validation - Invalid key:", key, "in file", self.filename
                 print
                 continue
-            if type(self.value[key]) != self.type[key]:
-                if not isinstance(self.value[key], type(None)):
-                    print "ERROR: Header Validation - type:", type(self.value[key]), "not valid for key:", key, "expected type:", self.type[key], "in file", self.filename
+            if type(self.header[key]['value']) != self.header[key]['type']:
+                if not isinstance(self.header[key]['value'], type(None)):
+                    print "ERROR: Header Validation - value_type:", type(self.header[key]['value']), "not valid for key:", key, "expected value_type:", self.header[key]['type'], "in file", self.filename
                     print
-            if self.constraint[key] is not None and self.value[key] not in self.constraint[key]:
-                if self.value[key] is None:
-                    print "ERROR: Header Validation - missing key: '" + key + "' in file", self.filename
+            if self.header[key]['constraint'] is not None and self.header[key]['value'] not in self.header[key]['constraint']:
+                if self.header[key]['value'] is None:
+                    print "ERROR: Header Validation - missing key:", key, "in file", self.filename
                 else:
-                    print "ERROR: Header Validation - value: '" + self.value[key] + "' for key: '" + key + "' not in constraints:", self.constraint[key], "in file", self.filename
+                    print "ERROR: Header Validation - value:", self.header[key]['value'], "for key:", key, "not in constraints:", self.header[key]['constraint'], "in file", self.filename
                 print
 
         return 0
