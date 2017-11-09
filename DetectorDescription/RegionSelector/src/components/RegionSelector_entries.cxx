eac00924dc9aa1a0cfeeb46f75aa069ae8f702ec
Tomasz Bold <tomasz.bold@gmail.com>
2017-11-09 09:55:15 +0100

    Undo
    Former-commit-id: 26b0ab4c6331128fc3080af22459ee91c8b232b8

--- DetectorDescription/RegionSelector/src/components/RegionSelector_entries.cxx
+++ DetectorDescription/RegionSelector/src/components/RegionSelector_entries.cxx
@@ -1,9 +1,8 @@
 // #include "RegionSelector/RegSelSvc.h"
 #include "../RegSelSvc.h"
-#include "../RegSelTestAlg.h"
 // #include "../NewRegSelSvc.h"
 
 DECLARE_COMPONENT( RegSelSvc )
-DECLARE_COMPONENT( RegSelTestAlg )
+
 // DECLARE_COMPONENT( NewRegSelSvc )
 
