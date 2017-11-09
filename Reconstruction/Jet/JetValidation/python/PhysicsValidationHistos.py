c82b3bda8eb1d662e5e45688c8c0eaaa70d98b00
Francesco Spano <spano@lxplus111.cern.ch>
2017-11-09 13:28:02 +0100

    modification in Reconstruction/Jet/JetValidation/python/PhysicsValidationHistos.py
    Former-commit-id: 20f540231d751e124966a7c5143f45da2a45e653

--- Reconstruction/Jet/JetValidation/python/PhysicsValidationHistos.py
+++ Reconstruction/Jet/JetValidation/python/PhysicsValidationHistos.py
@@ -173,6 +173,7 @@ akt4refContainer = "AntiKt4TruthJets" if rec.doTruth() else ""
 #globalSelection = "0.7<JVF[0]"
 globalSelection = ""
 
+# necesary updates for validation directories
 
 athenaMonTool = JetMonitoringTool(HistoTools = [
     commonPhysValTool( "AntiKt4LCTopoJets", akt4refContainer ,globalSelection = globalSelection),
