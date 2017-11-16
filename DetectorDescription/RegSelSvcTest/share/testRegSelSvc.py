37f94a32ed670010bf6f7c8e0232d6e44d0a492e
Tomasz Bold <tomasz.bold@gmail.com>
2017-11-16 10:34:58 +0100

    MT test almost in place
    Former-commit-id: 6675c915502551b4ef42b8512d7d65445a8b92aa

--- DetectorDescription/RegSelSvcTest/share/testRegSelSvc.py
+++ DetectorDescription/RegSelSvcTest/share/testRegSelSvc.py
@@ -30,8 +30,15 @@ theApp.EvtMax = 1
 
 from RegSelSvcTest.RegSelSvcTestConf import RegSelTestAlg
 athAlgSeq += RegSelTestAlg()
-athAlgSeq.RegSelTestAlg.Dump=True
 athAlgSeq.RegSelTestAlg.OutputLevel=DEBUG
+if "TestType" not in dir():
+    raise "Specify the test type, either \"dump\" or \"mt\""
+if TestType == "dump":
+    athAlgSeq.RegSelTestAlg.Dump=True
+
+if TestType == "mt":
+    athAlgSeq.RegSelTestAlg.Mt=True
+
 
 
 
