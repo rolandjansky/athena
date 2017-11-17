cba1ba91841bda3732da5e9e616160b2deb4fe0e
Vincent Pascuzzi <Vincent.Pascuzzi@cern.ch>
2017-11-17 20:35:28 +0100

    Add back TrkExUtils/doc/mainpage.h after svnpull
    Former-commit-id: 2b5a333df8c66f61b7e5ec6fcd94fe15e87e24c8

---
+++ Tracking/TrkExtrapolation/TrkExUtils/doc/mainpage.h
@@ -0,0 +1,26 @@
+/*
+     Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+     */
+
+/**
+
+  @mainpage The TrkExUtils package
+
+  This Package contains useful utilities in the TrkExtrapolation container package.
+
+  @section TrkExUtilsIntroduction Introduction
+
+  The TrkExUtils is a purely installed library that contains common utilities used within
+  the extrapolation/propagation process.
+
+  @author Andreas.Salzburger@cern.ch, Igor.Gavrilenko@cern.ch, Esben.Lund@cern.ch
+
+
+  @section requirements Requirements
+  @include requirements
+  */
+
+/**
+  \namespace Trk
+  A namespace to enclose the new Track class, and related tools.
+  */
