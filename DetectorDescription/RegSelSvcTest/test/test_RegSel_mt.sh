7e93ea6dbab81f37d2a3a03301d4918604ad7f91
scott snyder <snyder@bnl.gov>
2017-11-20 05:37:14 +0100

    RegSelSvcTest: Test fixes.
    Run tests in separate directories, so they don't interfere with each other.
    Increase test timeout so that it completes successfully in the debug build.
    
    
    
    Former-commit-id: b9d67fd29a5fb83183dc12cde219297ea3e9b1ad

--- DetectorDescription/RegSelSvcTest/test/test_RegSel_mt.sh
+++ DetectorDescription/RegSelSvcTest/test/test_RegSel_mt.sh
@@ -2,4 +2,7 @@
 # art-type: build
 # art-ci: master
 
+rm -rf test_RegSel_mt
+mkdir test_RegSel_mt
+cd test_RegSel_mt
 athena --threads=4 --concurrent-events=1 -c 'EvtMax=10;enableViews=True;FilesInput="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1";TestType="mt"' RegSelSvcTest/testRegSelSvc.py
