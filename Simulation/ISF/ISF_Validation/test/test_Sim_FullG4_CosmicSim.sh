50a8c1dc8912f52f2a6f6bafbfa46299a1e94eeb
John Chapman <jchapman@cern.ch>
2017-11-27 12:56:28 +0100

    Update of Cosmic Ray and Cavern Background Simulation Configuration
    In ATLASSIM-3214 the naming of TrackRecord input/output file arguments was discussed
    and it was agreed to use `inputEVNT_TRFile` and `outputEVNT_TRFile` for all
    simulation jobs which either read in or write out TrackRecord files. These
    new names were agreed with the AMI developers. (The old names having been too
    long.)
    This commit pulls in changes from `SimuJobTransforms-00-33-03`, `SimuJobTransforms-00-33-04`
    and `SimuJobTransforms-00-33-05`.
    It also pulls in corresponding updates to the syntax or RTT and ART jobs.
    
    +	* python/simTrfArgs.py : Only input TR and output TR, not separate cosmic
    +	/ cavern TR files
    +	* python/SimTransformUtils.py : Adding a sub-step to deal with jobs that
    +	write out TRs and jobs that read in TRs separately
    +	* share/skeleton.EVGENtoHIT_ISF.py : updating to configure only based on
    +	TR file requirements and whether beam config is cosmic, rather than using
    +	type of TR file as a hint
    +	* share/skeleton.EVGENtoHIT_MC12.py : updating to configure only based on
    +	TR file requirements and whether beam config is cosmic, rather than using
    +	type of TR file as a hint
    +	* scripts/AtlasG4_tf.py : updating list of input arguments
    +	* scripts/Sim_tf.py : updating list of input arguments
    +	* Tagging: SimuJobTransforms-00-33-03
    
    
    Former-commit-id: f015a48aa6ba345486dde39b1b1570459d236054

--- Simulation/ISF/ISF_Validation/test/test_Sim_FullG4_CosmicSim.sh
+++ Simulation/ISF/ISF_Validation/test/test_Sim_FullG4_CosmicSim.sh
@@ -3,7 +3,7 @@
 # art-description: Run cosmics simulation using ISF with the FullG4 simulator, generating events on-the-fly, using 2015 geometry and conditions
 # art-type: grid
 
-Sim_tf.py --conditionsTag 'OFLCOND-RUN12-SDR-19' --physicsList 'QGSP_BERT' --truthStrategy 'MC12' --simulator 'FullG4' --outputEVNT_COSMICSTRFile 'test.TR.pool.root' --outputHITSFile 'test.HITS.pool.root' --maxEvents '1500' --randomSeed '1234' --DataRunNumber '10' --CosmicFilterVolume 'Calo' --CosmicFilterVolume2 'NONE' --preInclude 'SimulationJobOptions/preInclude.Cosmics.py' --geometryVersion 'ATLAS-R2-2015-03-01-00_VALIDATION' --CosmicPtSlice 'NONE' --beamType 'cosmics'
+Sim_tf.py --conditionsTag 'OFLCOND-RUN12-SDR-19' --physicsList 'QGSP_BERT' --truthStrategy 'MC12' --simulator 'FullG4' --outputEVNT_TRFile 'test.TR.pool.root' --outputHITSFile 'test.HITS.pool.root' --maxEvents '1500' --randomSeed '1234' --DataRunNumber '10' --CosmicFilterVolume 'Calo' --CosmicFilterVolume2 'NONE' --preInclude 'SimulationJobOptions/preInclude.Cosmics.py' --geometryVersion 'ATLAS-R2-2015-03-01-00_VALIDATION' --CosmicPtSlice 'NONE' --beamType 'cosmics'
 
 SCRIPT_DIRECTORY=$1
 PACKAGE=$2
