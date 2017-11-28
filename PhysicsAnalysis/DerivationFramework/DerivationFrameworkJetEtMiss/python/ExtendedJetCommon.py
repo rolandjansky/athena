e021abf967ec503f9cca0d0692fa15c7016f1c88
Ishan Pokharel <ispokhar@lxplus047.cern.ch>
2017-11-28 15:49:52 +0100

    Updated the R21 calibration configurations in ExtendedJetCommon.py for small-R jets
    Former-commit-id: 01094799b41416c52621ab37ac53f248db556d15

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/ExtendedJetCommon.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/ExtendedJetCommon.py
@@ -207,11 +207,11 @@ def applyJetCalibration(jetalg,algname,sequence):
     else:
         isdata=False
 
-        configdict = {'AntiKt4EMTopo':('JES_MC16Recommendation_Aug2017.config',
+        configdict = {'AntiKt4EMTopo':('JES_MC16Recommendation_Nov2017.config',
                                        'JetArea_Residual_EtaJES_GSC'),
-                      'AntiKt4LCTopo':('JES_data2016_data2015_Recommendation_Dec2016_rel21.config',
+                      'AntiKt4LCTopo':('JES_MC16Recommendation_Nov2017.config',
                                        'JetArea_Residual_EtaJES_GSC'),
-                      'AntiKt4EMPFlow':('JES_MC15Prerecommendation_PFlow_Aug2016.config',
+                      'AntiKt4EMPFlow':('JES_MC16Recommendation_PFlow_Nov2017.config',
                                         'JetArea_Residual_EtaJES_GSC'),
                       'AntiKt10LCTopoTrimmedPtFrac5SmallR20':('JES_MC15recommendation_FatJet_Nov2016_QCDCombinationUncorrelatedWeights.config',
                                                               'EtaJES_JMS'),
