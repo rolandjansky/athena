32c64a5d4ce11bea46b48c698eee52a3e30072b4
Marcin Nowak <Marcin.Nowak@cern.ch>
2017-11-28 17:31:19 +0100

    Cleanup MuonMeasurementsCnv, remove MuonMeasurements_tlp3
    remove MuonMeasurements_tlp3 because it was never written out, start
    using MuonMeasurements_tlp2 consistently again
    remove old MuonMeasurementsCnv_tlp2, because it was never used, rename
    MuonMeasurementsCnv_tlp3 to MuonMeasurementsCnv_tlp2
    
    
    Former-commit-id: e6b92aab6524888862b66389bf2e1ab72d210db4

--- MuonSpectrometer/MuonCnv/MuonEventTPCnv/MuonEventTPCnv/MuonEventTPCnvDict.h
+++ MuonSpectrometer/MuonCnv/MuonEventTPCnv/MuonEventTPCnv/MuonEventTPCnvDict.h
@@ -11,7 +11,7 @@
 //
 //-----------------------------------------------------------------------------
 
-#include "MuonEventTPCnv/MuonMeasurements_tlp3.h"
+#include "MuonEventTPCnv/MuonMeasurements_tlp2.h"
 
 #include "MuonEventTPCnv/CscStripPrepDataContainer_tlp1.h"
 
@@ -37,44 +37,5 @@
 #include "MuonEventTPCnv/MuonDigitContainer/MuonSimDataCollection_p2.h"
 #include "MuonEventTPCnv/MuonDigitContainer/CscSimDataCollection_p1.h"
 #include "MuonEventTPCnv/MuonDigitContainer/CscSimDataCollection_p2.h"
-
-struct MuonEventTPCnvDict 
-{
-    std::vector< Muon::CscPrepData_p2>              m_v1;
-    std::vector< Muon::CscStripPrepData_p1>         m_v2;
-    std::vector< Muon::RpcPrepData_p3>              m_v3;
-    std::vector< Muon::TgcPrepData_p2 >             m_v4;
-    std::vector< Muon::TgcPrepData_p3 >             m_v4a;
-    std::vector< Muon::MdtPrepData_p2 >             m_v5;
-    std::vector< Muon::MuonPRD_Collection_p2 >      m_v6;
-    std::vector< Muon::MdtPrepDataContainer_p2 >    m_v7a;
-    std::vector< Muon::RpcPrepDataContainer_p3 >    m_v7b;
-    std::vector< Muon::CscPrepDataContainer_p2 >    m_v7c;
-    std::vector< Muon::TgcPrepDataContainer_p2 >    m_v7d;
-    std::vector< Muon::TgcPrepDataContainer_p3 >    m_v7e;
-    std::vector< Muon::MuonSegment_p1>              m_v8;    
-    std::vector< Muon::MuonSegmentQuality_p1>       m_v9;
-    std::vector< Muon::CompetingMuonClustersOnTrack_p1> m_v10;
-    std::vector< Muon::MuonCoinDataCollection_p1 >      m_v11;
-    std::vector< Muon::MuonCoinDataContainer_p1 >       m_v12;
-    std::vector< Muon::TgcCoinData_p2>              m_v13a;
-    std::vector< Muon::TgcCoinData_p3>              m_v13b;
-    Muon::MuonPRD_Container_p2<Muon::MdtPrepData_p2>      m_v14;
-    Muon::MuonPRD_Container_p2<Muon::RpcPrepData_p3>      m_v14a;
-    Muon::MuonPRD_Container_p2<Muon::CscPrepData_p2>      m_v14b;
-    Muon::MuonPRD_Container_p2<Muon::TgcPrepData_p2>      m_v14c;
-    Muon::MuonPRD_Container_p2<Muon::TgcPrepData_p3>      m_v14d;
-    std::vector<Muon::MuonPRD_Container_p2<Muon::MdtPrepData_p2> > m_v15;
-    std::vector< Muon::MdtTwinPrepData_p1 >             m_v16;
-    std::vector< Muon::RpcCoinData_p1 >             m_v17;
-    Muon::MuonPRD_Container_p2<Muon::RpcCoinData_p1>             m_v18;
-    std::pair< unsigned int, float> m_v19;
-    Muon::MuonPRD_Container_p2<Muon::sTgcPrepData_p1>      m_v20;
-    Muon::MuonPRD_Container_p2<Muon::MMPrepData_p1>      m_v21;
-    Muon::MuonDigitContainer_p1<Muon::MM_Digit_p1>      m_v22;
-    Muon::MuonDigitContainer_p1<Muon::STGC_Digit_p1>      m_v23;
-    Muon::MuonDigitContainer_p2<Muon::MM_Digit_p2>      m_v24;
-    
-};
 
 #endif // MUONEVENTTPCNV_MUONEVENTTPCNVDICT_H
