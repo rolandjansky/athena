d40e521ab5ef8443695a3ba79dbc3ff557f28072
scott snyder <snyder@bnl.gov>
2017-11-29 20:04:07 +0100

    TRT_RawDataByteStreamCnv: Fix compilation warning.
    Compilation warning: uninitialized variable.
    
    
    
    Former-commit-id: f820b2da3c8aa9b1f44716ff020d0433a6764330

--- InnerDetector/InDetEventCnv/TRT_RawDataByteStreamCnv/src/TRT_RodDecoder.cxx
+++ InnerDetector/InDetEventCnv/TRT_RawDataByteStreamCnv/src/TRT_RodDecoder.cxx
@@ -303,7 +303,7 @@ TRT_RodDecoder::handle(const Incident& inc) {
    **/
 
   if ( inc.type() == "BeginRun" ) {
-    const EventInfo* currentEvent;
+    const EventInfo* currentEvent = nullptr;
     StatusCode sc = evtStore()->retrieve(currentEvent);
     if ( sc.isFailure() ) 
        ATH_MSG_ERROR( "Could not get current event" );
