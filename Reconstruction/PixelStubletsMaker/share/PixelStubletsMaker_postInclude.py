02ce47227345b72ad6f113288a8094a271df2cfb
Masahiko Saito <masahiko.saito@cern.ch>
2017-11-29 17:41:09 +0100

    remove PixelStubletsMaker
    Former-commit-id: 6d00f9e9f92658036a159c05b17f5c7abb6b7e0d

--- Reconstruction/PixelStubletsMaker/share/PixelStubletsMaker_postInclude.py
+++
@@ -1,5 +0,0 @@
-excludedAuxData = "-caloExtension.-cellAssociation.-clusterAssociation.-trackParameterCovarianceMatrices.-parameterX.-parameterY.-parameterZ.-parameterPX.-parameterPY.-parameterPZ.-parameterPosition"
-StreamESD.ItemList+=['xAOD::TrackParticleContainer#InDetTwoPlusPVTrackParticles']
-StreamESD.ItemList+=['xAOD::TrackParticleAuxContainer#InDetTwoPlusPVTrackParticlesAux.' + excludedAuxData]
-print "TestTestTest"
-print StreamESD
--- Reconstruction/PixelStubletsMaker/src/components/PixelStubletsMaker_entries.cxx
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "PixelStubletsMaker/PixelStubletsMakerAlg.h"
- 
-DECLARE_ALGORITHM_FACTORY(PixelStubletsMakerAlg)
- 
-DECLARE_FACTORY_ENTRIES(PixelStubletsMaker) {
-  DECLARE_ALGORITHM(PixelStubletsMakerAlg)
-}
