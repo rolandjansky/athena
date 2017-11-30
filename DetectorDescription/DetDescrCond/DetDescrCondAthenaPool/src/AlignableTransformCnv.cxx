389f5c37eabe7c866448e0d4d5837daa08663b7f
scott snyder <scott.snyder@cern.ch>
2017-11-30 17:29:22 +0100

    DetDescrCondAthenaPool: Use templated pool converters.
    Use the templated pool converters rather then copy/pasted code.
    This will help with future migrations of the converter interfaces
    and standard functionality.
    
    
    
    Former-commit-id: 54d6bcd435c54f291175d9ae69d3f5958e4a2e07

--- DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/AlignableTransformCnv.cxx
+++ DetectorDescription/DetDescrCond/DetDescrCondAthenaPool/src/AlignableTransformCnv.cxx
@@ -2,50 +2,6 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-/**
- * @file AlignableTransformCnv.cxx
- * @brief Implementation file for AthenaPool converter for AlignableTransform
- * @author RD Schaffer <R.D.Schaffer@cern.ch>
- */
-
-#include "AlignableTransformCnv.h"
-#include "DetDescrCondTPCnv/AlignableTransformCnv_p1.h"
-
-AlignableTransformCnv::AlignableTransformCnv(ISvcLocator* svcloc) 
-:       
-        AlignableTransformCnvBase(svcloc) 
-{}
-
-
-AlignableTransform_PERS* AlignableTransformCnv::createPersistent(AlignableTransform* transObj) {
-    MsgStream log(msgSvc(), "AlignableTransformCnv" ); 
-    AlignableTransformCnv_p1 cnv;
-    AlignableTransform_PERS* persObj = cnv.createPersistent( transObj, log );
-    if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "AlignableTransform write Success" << endmsg;
-    return persObj; 
-}
-
-AlignableTransform* AlignableTransformCnv::createTransient() {
-    MsgStream log(msgSvc(), "AlignableTransformCnv" );
-    static pool::Guid   p1_guid("BA1A841C-8D92-45AE-9AD1-9AF7A1736844");
-    static pool::Guid   p0_guid("E779C6B5-3F2A-473E-B35E-6CCB345E0665");
-    if( compareClassGuid(p1_guid) ) {
-        // using auto_ptr ensures deletion of the persistent object
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "AlignableTransform read p1" << endmsg;
-        std::auto_ptr< AlignableTransform_p1 > col_vect( poolReadObject< AlignableTransform_p1 >() );
-        AlignableTransformCnv_p1 cnv;
-        return cnv.createTransient( col_vect.get(), log );
-    }
-    else if( compareClassGuid(p0_guid) ) {
-        // regular object from before TP separation, just return it
-        if (log.level()<=MSG::DEBUG) log << MSG::DEBUG << "AlignableTransform read p0" << endmsg;
-        return this->poolReadObject<AlignableTransform>();
-    } 
-    throw std::runtime_error("Unsupported persistent version of AlignableTransform");
-}
-
-
-StatusCode AlignableTransformCnv::initialize()
-{
-    return AlignableTransformCnvBase::initialize();
-}
+// Dummy file so that cmake will know this is a custom converter.
+// Make this file unique, so as not to confuse git's tiny little mind:
+//   AlignableTransform
