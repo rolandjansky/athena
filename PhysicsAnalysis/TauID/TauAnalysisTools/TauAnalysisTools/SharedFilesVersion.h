4795b2e7785dedda68bd755bf017457944a44477
David Kirchmeier <david.kirchmeier@cern.ch>
2017-12-01 15:22:34 +0100

    restructure SharedFilesVersion
    Former-commit-id: 9d51fed9f979c6452ae2adfdc3e7f94d1a2a3099

--- PhysicsAnalysis/TauID/TauAnalysisTools/TauAnalysisTools/SharedFilesVersion.h
+++ PhysicsAnalysis/TauID/TauAnalysisTools/TauAnalysisTools/SharedFilesVersion.h
@@ -2,4 +2,10 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#define sSharedFilesVersion "00-03-03"
+#ifndef TAUANALYSISTOOLS_SHAREDFILESVERSION_H
+#define TAUANALYSISTOOLS_SHAREDFILESVERSION_H
+
+/// Version of the calibration files
+static const char* sSharedFilesVersion = "00-03-03";
+
+#endif // TAUANALYSISTOOLS_SHAREDFILESVERSION_H
