7310f1842f579d7a2efa8cf8175a668e2239431e
scott snyder <snyder@bnl.gov>
2017-12-03 21:50:48 +0100

    tauRecTools: Fix compilation warning.
    Compilation warning: uninitialized variable.
    
    
    
    Former-commit-id: e7f0bd9ae8524cec6f54d1ff9491d4de286d5364

--- Reconstruction/tauRecTools/src/TauConversionFinder.cxx
+++ Reconstruction/tauRecTools/src/TauConversionFinder.cxx
@@ -70,7 +70,7 @@ StatusCode TauConversionFinder::eventFinalize() {
     // ------------------------------------------------------------------ 
     //               Retrieving VxCandidates (conversions)
     // ------------------------------------------------------------------   
-    const xAOD::VertexContainer* ConvContainer;
+    const xAOD::VertexContainer* ConvContainer =  nullptr;
 
     sc = evtStore()->retrieve(ConvContainer, m_ConversionCandidatesName);
     if (sc.isFailure() || !ConvContainer) {
