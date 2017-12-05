49464a1d599bf8d256b1690b688bfc2a53ea1363
David Kirchmeier <david.kirchmeier@cern.ch>
2017-12-05 15:38:56 +0100

    making sSharedFilesVersion const
    Former-commit-id: a5a627a8006c89ce1cdef79d0d1663fc4e6d870b

--- PhysicsAnalysis/TauID/TauAnalysisTools/TauAnalysisTools/SharedFilesVersion.h
+++ PhysicsAnalysis/TauID/TauAnalysisTools/TauAnalysisTools/SharedFilesVersion.h
@@ -6,6 +6,6 @@
 #define TAUANALYSISTOOLS_SHAREDFILESVERSION_H
 
 /// Version of the calibration files
-static const char* sSharedFilesVersion = "00-03-03";
+static const char *const sSharedFilesVersion = "00-03-03";
 
 #endif // TAUANALYSISTOOLS_SHAREDFILESVERSION_H
