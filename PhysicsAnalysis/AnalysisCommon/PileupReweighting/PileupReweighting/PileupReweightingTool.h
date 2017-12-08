99b0444eeefb2e3d39982aa99e61a725ae63b7bc
Will Buttinger <will@cern.ch>
2017-12-08 15:14:22 +0000

    make new run-dependent prescale weights an option, not the default. Since I realised how to work things properly with the old way of doing things
    Former-commit-id: 26a4ddfc28db6f4366d770bb1c916c43773855dc

--- PhysicsAnalysis/AnalysisCommon/PileupReweighting/PileupReweighting/PileupReweightingTool.h
+++ PhysicsAnalysis/AnalysisCommon/PileupReweighting/PileupReweighting/PileupReweightingTool.h
@@ -157,6 +157,7 @@ namespace CP {
       bool m_noWeightsMode;
       
       bool m_varyRunNumber;
+      bool m_useRunDependentPrescaleWeight;
 
       ///< Default weight tool in standalone mode
       std::unique_ptr< IWeightTool > m_defaultWeightTool;
