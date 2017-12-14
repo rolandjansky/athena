5c20fc7e4bf6170eed335dd531a94cdb9e76af92
Nils Krumnack <krumnack@iastate.edu>
2017-12-14 18:50:22 +0100

    change the location of the user code in the docker image
    It's now installed under /usr as Attila requested.  Though that does
    require passing in the project name as a parameter.  For the user this
    change should hopefully be invisible, i.e. it is taken care of by
    el_build_docker.
    
    
    Former-commit-id: 4bca73541c2ff4c4f3c742d248e36135f3e5e850

--- PhysicsAnalysis/D3PDTools/EventLoop/data/docker_analysis_setup.sh
+++ PhysicsAnalysis/D3PDTools/EventLoop/data/docker_analysis_setup.sh
@@ -6,8 +6,8 @@
 source /home/atlas/analysis_release_setup.sh
 
 # Set up the analysis base installation:
-source /home/atlas/install/${PROJECT_NAME}/*/InstallArea/*/setup.sh
-echo "Configured analysis setup from from: /home/atlas/install"
+source /usr/${PROJECT_NAME}/*/InstallArea/*/setup.sh
+echo "Configured analysis setup from from: /usr/${PROJECT_NAME}"
 
 # Set up the prompt:
 # export PS1='\[\033[01;35m\][bash]\[\033[01;31m\][\u GitLabCITests-$GitLabCITests_VERSION]\[\033[01;34m\]:\W >\[\033[00m\] ';
