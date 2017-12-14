c76aa1b3f85bc8418865a7a90c095902ce112155
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2017-12-14 14:51:25 +0000

    Revert "Merge branch 'revert-18b754a9' into '21.0'"
    This reverts merge request !7425
    
    Former-commit-id: cd496b950f40222cab443afb3973e1aa8cf1aab8

--- Event/xAOD/xAODTruthCnv/src/components/xAODTruthCnv_entries.cxx
+++ Event/xAOD/xAODTruthCnv/src/components/xAODTruthCnv_entries.cxx
@@ -2,12 +2,14 @@
 
 // Local include(s):
 #include "../xAODTruthCnvAlg.h"
+#include "xAODTruthCnv/TruthMetaDataTool.h"
 #include "../xAODTruthReader.h"
 #include "../HepMCTruthReader.h"
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODMaker, xAODTruthCnvAlg )
 DECLARE_NAMESPACE_ALGORITHM_FACTORY( xAODReader, xAODTruthReader )
 DECLARE_ALGORITHM_FACTORY( HepMCTruthReader )
+DECLARE_NAMESPACE_TOOL_FACTORY( xAODMaker, TruthMetaDataTool )
 
 DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
    DECLARE_NAMESPACE_ALGORITHM( xAODMaker, xAODTruthCnvAlg )
@@ -16,5 +18,5 @@ DECLARE_FACTORY_ENTRIES( xAODCreatorAlgs ) {
 
 DECLARE_FACTORY_ENTRIES( xAODTruthCnv ) {
    DECLARE_ALGORITHM( HepMCTruthReader )
+   DECLARE_NAMESPACE_TOOL( xAODMaker, TruthMetaDataTool )
 }
-
---
+++ Event/xAOD/xAODTruthCnv/xAODTruthCnv/xAODTruthCnvDict.h
@@ -0,0 +1,11 @@
+// Dear emacs, this is -*- c++ -*-
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+#ifndef XAODTRUTHCNV_XAODTRUTHCNVDICT_H
+#define XAODTRUTHCNV_XAODTRUTHCNVDICT_H
+
+// Local include(s):
+#include "xAODTruthCnv/TruthMetaDataTool.h"
+
+#endif // XAODTRUTHCNV_XAODTRUTHCNVDICT_H
