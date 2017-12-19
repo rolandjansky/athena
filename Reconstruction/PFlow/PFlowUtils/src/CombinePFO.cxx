50b242187fe163320c9e6440a1bedcb56d6f5d1d
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2017-12-19 16:56:20 +0000

    Add copyright statements.
    Former-commit-id: 503b7ccd8c65b977019d97659278c43e97c6282f

--- Reconstruction/PFlow/PFlowUtils/src/CombinePFO.cxx
+++ Reconstruction/PFlow/PFlowUtils/src/CombinePFO.cxx
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 #include "CombinePFO.h"
 #include "AthContainers/ConstDataVector.h"
 #include "xAODCore/ShallowCopy.h"
--- Reconstruction/PFlow/PFlowUtils/src/CombinePFO.h
+++ Reconstruction/PFlow/PFlowUtils/src/CombinePFO.h
@@ -1,3 +1,7 @@
+/*                                                                                                                                                                                                                                           
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration                                                                                                                                                                    
+*/
+
 #ifndef COMBINEPFO_H
 #define COMBINEPFO_H
 
