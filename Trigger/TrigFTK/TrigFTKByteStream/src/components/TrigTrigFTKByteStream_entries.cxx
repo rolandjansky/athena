fdf0201ec94d84fe638aa13b769d4dcebf5b88c9
Jiří Mašík <Jiri.Masik@manchester.ac.uk>
2017-12-26 21:16:01 +0100

    adjust to the new macros of _entries.cxx
    Former-commit-id: 2fb272f6fdfbff77b8d3faef41d5166693a209f6

--- Trigger/TrigFTK/TrigFTKByteStream/src/components/TrigTrigFTKByteStream_entries.cxx
+++ Trigger/TrigFTK/TrigFTKByteStream/src/components/TrigTrigFTKByteStream_entries.cxx
@@ -1,19 +1,10 @@
-
 #include "TrigFTKByteStream/TrigFTKByteStreamCnv.h"
 #include "TrigFTKByteStream/TrigFTKByteStreamTool.h"
 #include "../FTKByteStreamDecoderEncoder.h"
 #include "../FTKDump.h"
 
-DECLARE_NAMESPACE_CONVERTER_FACTORY( FTK, TrigFTKByteStreamCnv )
-DECLARE_NAMESPACE_TOOL_FACTORY( FTK, TrigFTKByteStreamTool )
-DECLARE_NAMESPACE_TOOL_FACTORY( FTK, FTKByteStreamDecoderEncoderTool )
-DECLARE_ALGORITHM_FACTORY(FTKDump)
-
-DECLARE_FACTORY_ENTRIES( TrigFTKByteStream ) {
-  DECLARE_NAMESPACE_CONVERTER(FTK, TrigFTKByteStreamCnv)
-  DECLARE_NAMESPACE_TOOL(FTK, TrigFTKByteStreamTool)
-  DECLARE_NAMESPACE_TOOL(FTK, FTKByteStreamDecoderEncoderTool)
-  DECLARE_ALGORITHM(FTKDump)
-}
-
+DECLARE_CONVERTER_FACTORY( FTK::TrigFTKByteStreamCnv )
+DECLARE_COMPONENT( FTK::TrigFTKByteStreamTool )
+DECLARE_COMPONENT( FTK::FTKByteStreamDecoderEncoderTool )
+DECLARE_COMPONENT( FTKDump )
 
