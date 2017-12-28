f954cb89a81278a47167d675d06d111ccb7a31fb
scott snyder <scott.snyder@cern.ch>
2017-12-28 23:26:03 +0100

    InDetAlignGenAlgs: Fix clang warnings.
    clang warnings: Unused private data members.
    Bad indexing in initialization loops.
    
    
    Former-commit-id: c2f35466f5aefbfcadb2b8ce2ca748cebe3f30cb

--- InnerDetector/InDetAlignAlgs/InDetAlignGenAlgs/src/SPLayer.cxx
+++ InnerDetector/InDetAlignAlgs/InDetAlignGenAlgs/src/SPLayer.cxx
@@ -9,10 +9,10 @@
 #include <iostream>
 #include "InDetAlignGenAlgs/SPLayer.h"
 
-SPLayer::SPLayer(const int h1, const int h2, const int det, const int bec,
+SPLayer::SPLayer(const int /*h1*/, const int /*h2*/, const int det, const int bec,
      const int layer, const int side, const std::string& name,
      const std::vector<TH1*>& histlist) :
-  m_hbase(h1), m_hbase2(h2), m_dettype(det), m_bec(bec), m_layer(layer),
+  m_dettype(det), m_bec(bec), m_layer(layer),
   m_side(side),m_nmod(0), m_nhit(0), m_name(name), m_histlist(histlist)
  { 
    std::cout << "In splayer constructer" << std::endl;
