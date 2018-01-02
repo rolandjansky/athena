f9527b8b31a26ac1bce53a218b2d60fbe87fefec
Tomasz Bold <tomasz.bold@gmail.com>
2018-01-02 16:36:39 +0000

    New trigger calo data access svc
    Former-commit-id: 71b5f023aa7b390e40dda4268a295979eb05bbd5

---
+++ Trigger/TrigDataAccess/TrigCaloDataAccessSvc/share/testDataAccessService.py
@@ -0,0 +1,31 @@
+#
+#  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+#
+
+include("TrigUpgradeTest/testHLT_MT.py")
+
+viewTest = opt.enableViews   # from testHLT_MT.py
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence = AlgSequence()
+
+
+from AthenaMonitoring.GenericMonitoringTool import GenericMonitoringTool, defineHistogram
+
+from TrigCaloDataAccessSvc.TrigCaloDataAccessSvcConf import TrigCaloDataAccessSvc, TestCaloDataAccess
+import math
+mon = GenericMonitoringTool("CaloDataAccessSvcMon")
+mon.Histograms += [defineHistogram( "TIME_locking_LAr_RoI", title="Time spent in unlocking the LAr collection", xbins=100, xmin=0, xmax=100 ),
+                   defineHistogram( "roiROBs_LAr", title="Number of ROBs unpacked in RoI requests", xbins=20, xmin=0, xmax=20 ),
+                   defineHistogram( "TIME_locking_LAr_FullDet", title="Time spent in unlocking the LAr collection", xbins=100, xmin=0, xmax=100 ),
+                   defineHistogram( "roiEta_LAr,roiPhi_LAr", type="TH2F", title="Geometric usage", xbins=50, xmin=-5, xmax=5, ybins=64, ymin=-math.pi, ymax=math.pi )]
+
+svcMgr += TrigCaloDataAccessSvc()
+svcMgr.TrigCaloDataAccessSvc.OutputLevel=DEBUG
+svcMgr.TrigCaloDataAccessSvc.MonTool = mon
+
+
+algo = TestCaloDataAccess()
+algo.OutputLevel=DEBUG
+
+
+topSequence += algo
