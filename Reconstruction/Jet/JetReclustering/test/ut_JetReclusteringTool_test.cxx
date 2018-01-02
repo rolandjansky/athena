63ee1eb12ced2c9650ea613c03ec85c9e85cb114
Giordon Stark <gstark@cern.ch>
2018-01-02 17:43:59 +0100

    Make tests work in both Analysis and AthAnalysis
    Former-commit-id: d637ea32f56a2ca7b3951488944fcb0dde3ea06d

--- Reconstruction/Jet/JetReclustering/test/ut_JetReclusteringTool_test.cxx
+++ Reconstruction/Jet/JetReclustering/test/ut_JetReclusteringTool_test.cxx
@@ -7,7 +7,9 @@
 #include "JetInterface/IJetExecuteTool.h"
 #include "xAODJet/JetContainer.h"
 
-#ifndef XAOD_STANDALONE
+#ifdef ROOTCORE
+#include "xAODRootAccess/TEvent.h"
+#else
 #include "POOLRootAccess/TEvent.h"
 #endif
 
@@ -20,44 +22,40 @@ int main() {
   ANA_CHECK (xAOD::Init ());
 #endif
 
-#ifndef XAOD_STANDALONE
+#ifdef ROOTCORE
+  xAOD::TEvent evt( xAOD::TEvent::kAthenaAccess );
+#else
+  POOL::TEvent evt( POOL::TEvent::kAthenaAccess );
+#endif
 
-  //here's an example for AthAnalysisBase
-
-  //start just by loading the first event of the test MC file
-  POOL::TEvent evt(POOL::TEvent::kAthenaAccess);
   evt.readFrom("$ASG_TEST_FILE_MC");
   evt.getEntry(0);
 
   //example of configuring the tool
-   asg::AnaToolHandle<IJetExecuteTool> myTool("JetReclusteringTool/testRCJets");
-   ANA_CHECK(myTool.setProperty("InputJetContainer",  "AntiKt4LCTopoJets"));
-   ANA_CHECK(myTool.setProperty("OutputJetContainer", "MyNewJets"));
-   ANA_CHECK(myTool.setProperty("ReclusterRadius",    1.0));
-   ANA_CHECK(myTool.setProperty("ReclusterAlgorithm", "AntiKt"));
-   ANA_CHECK(myTool.setProperty("VariableRMinRadius", -1.0));
-   ANA_CHECK(myTool.setProperty("VariableRMassScale", -1.0));
-   ANA_CHECK(myTool.setProperty("InputJetPtMin",      25.0)); //GeV
-   ANA_CHECK(myTool.setProperty("RCJetPtMin",         50.0)); //GeV
-   ANA_CHECK(myTool.setProperty("RCJetPtFrac",        0.05));
-   ANA_CHECK(myTool.setProperty("RCJetSubjetRadius",  0.4));
-   ANA_CHECK(myTool.setProperty("DoArea",             false));
-   ANA_CHECK(myTool.setProperty("AreaAttributes",     ""));
-   ANA_CHECK(myTool.initialize());
+  asg::AnaToolHandle<IJetExecuteTool> myTool("JetReclusteringTool/testRCJets");
+  ANA_CHECK(myTool.setProperty("InputJetContainer",  "AntiKt4LCTopoJets"));
+  ANA_CHECK(myTool.setProperty("OutputJetContainer", "MyNewJets"));
+  ANA_CHECK(myTool.setProperty("ReclusterRadius",    1.0));
+  ANA_CHECK(myTool.setProperty("ReclusterAlgorithm", "AntiKt"));
+  ANA_CHECK(myTool.setProperty("VariableRMinRadius", -1.0));
+  ANA_CHECK(myTool.setProperty("VariableRMassScale", -1.0));
+  ANA_CHECK(myTool.setProperty("InputJetPtMin",      25.0)); //GeV
+  ANA_CHECK(myTool.setProperty("RCJetPtMin",         50.0)); //GeV
+  ANA_CHECK(myTool.setProperty("RCJetPtFrac",        0.05));
+  ANA_CHECK(myTool.setProperty("RCJetSubjetRadius",  0.4));
+  ANA_CHECK(myTool.setProperty("DoArea",             false));
+  ANA_CHECK(myTool.setProperty("AreaAttributes",     ""));
+  ANA_CHECK(myTool.initialize());
 
-   //actually run the tool: call its execute method, and check that the return value is 0
-   if( myTool->execute()!=0 ) {
-     ANA_MSG_INFO("Failured to recluster");
-     return -1;
-   }
+  //actually run the tool: call its execute method, and check that the return value is 0
+  if( myTool->execute()!=0 ) {
+    ANA_MSG_INFO("Failured to recluster");
+    return -1;
+  }
 
-   //if it worked, the reclustered jets will be accessible from the storegate:
-   const xAOD::JetContainer* newJets = 0;
-   ANA_CHECK( evt.retrieve( newJets, "MyNewJets" ) );
+  //if it worked, the reclustered jets will be accessible from the storegate:
+  const xAOD::JetContainer* newJets = 0;
+  ANA_CHECK( evt.retrieve( newJets, "MyNewJets" ) );
 
-
-
-#endif
-
-   return 0; //zero = success
+  return 0; //zero = success
 }
