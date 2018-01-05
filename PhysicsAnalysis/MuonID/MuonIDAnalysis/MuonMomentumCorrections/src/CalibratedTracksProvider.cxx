37a9b65e3bb68a14eb6b5f0bfa19f620d90b1645
Nicolas Köhler <nicolas.koehler@cern.ch>
2018-01-05 12:48:38 +0100

    added src/CalibratedTracksProvider
    Former-commit-id: 2621eb0634a9b6c898261a30785a1f5d8bccfd26

---
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonMomentumCorrections/src/CalibratedTracksProvider.cxx
@@ -0,0 +1,68 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "xAODTracking/TrackParticleContainer.h"
+
+// Local include(s):
+#include "CalibratedTracksProvider.h"
+#include "xAODCore/ShallowCopy.h"
+#include "xAODBase/IParticleHelpers.h"
+
+namespace CP {
+
+static const SG::AuxElement::Accessor<ElementLink<xAOD::IParticleContainer> > origLinkAcc("originalObjectLink");
+
+CalibratedTracksProvider::CalibratedTracksProvider( const std::string& name, ISvcLocator* svcLoc )
+      : AthAlgorithm( name, svcLoc ),
+         m_tool( "CP::MuonCalibrationAndSmearingTool/MuonCalibrationAndSmearingTool"),
+         m_detType(2) // MS = 1, ID = 2
+{
+      declareProperty( "Input", m_inputKey = "InDetTrackParticles" ); // or ExtrapolatedMuonTrackParticles
+      declareProperty( "Output", m_outputKey = "CalibratedInDetTrackParticles");
+      declareProperty( "Tool", m_tool );
+      declareProperty( "DetType", m_detType );
+
+}
+
+StatusCode CalibratedTracksProvider::initialize() {
+      ATH_CHECK( m_tool.retrieve() );
+      return StatusCode::SUCCESS;
+}
+
+StatusCode CalibratedTracksProvider::execute() {
+
+      xAOD::TrackParticleContainer* nonconst_tracks = 0;
+      const xAOD::TrackParticleContainer* tracks = 0;
+      std::pair< xAOD::TrackParticleContainer*, xAOD::ShallowAuxContainer* > out;
+
+      if(m_inputKey==m_outputKey) { //attempt to modify in-situ, but must retrieve as non-const
+         CHECK( evtStore()->retrieve( nonconst_tracks, m_inputKey ) );
+              tracks = nonconst_tracks;
+               out.first = nonconst_tracks;
+      } else {
+         CHECK( evtStore()->retrieve( tracks, m_inputKey ) );
+         out = xAOD::shallowCopyContainer( *tracks );
+         //add original object link to met recalculations work
+         if(!setOriginalObjectLink( *tracks, *out.first )) { 
+            ATH_MSG_ERROR("Failed to add original object links to shallow copy of " << m_inputKey);
+            return StatusCode::FAILURE;
+         }  
+      }
+      
+      for(auto iParticle : *(out.first)) {
+         if(msg().level()==MSG::VERBOSE) std::cout << name() << " Old pt=" << iParticle->pt();
+         if(m_tool->applyCorrectionTrkOnly(*iParticle, m_detType).code()==CorrectionCode::Error) return StatusCode::FAILURE;
+         if(msg().level()==MSG::VERBOSE) std::cout << " New pt=" << iParticle->pt() << std::endl;
+      }
+
+      //record to storegate 
+      CHECK( evtStore()->record( out.first, m_outputKey ) );
+      CHECK( evtStore()->record( out.second, m_outputKey+"Aux.") );
+
+
+      // Return gracefully:
+      return StatusCode::SUCCESS;
+   }
+
+} // namespace CP
