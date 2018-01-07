f0ddf6eed32d1d6e100680fa144881fd30635a27
scott snyder <scott.snyder@cern.ch>
2018-01-07 16:59:43 +0100

    G4PhysicsLists: Fix clang warnings.
    clang warnings: unused private class data members.
    
    
    Former-commit-id: ecb091f09d263064ab4d3cd4276af880eff5c3ea

--- Simulation/G4Utilities/G4PhysicsLists/src/hadronicVariations/G4HadronPhysicsQGSP_FTFP_BERT_ATL.cc
+++ Simulation/G4Utilities/G4PhysicsLists/src/hadronicVariations/G4HadronPhysicsQGSP_FTFP_BERT_ATL.cc
@@ -85,7 +85,6 @@ G4HadronPhysicsQGSP_FTFP_BERT_ATL::G4HadronPhysicsQGSP_FTFP_BERT_ATL(G4int)
     , theHyperon(0)
     , theAntiBaryon(0)
     , theFTFPAntiBaryon(0) */
-    , QuasiElastic(false)
   /*    , ChipsKaonMinus(0)
     , ChipsKaonPlus(0)
     , ChipsKaonZero(0)
@@ -93,7 +92,7 @@ G4HadronPhysicsQGSP_FTFP_BERT_ATL::G4HadronPhysicsQGSP_FTFP_BERT_ATL(G4int)
     , xsNeutronCaptureXS(0)*/
 {}
 
-G4HadronPhysicsQGSP_FTFP_BERT_ATL::G4HadronPhysicsQGSP_FTFP_BERT_ATL(const G4String& name, G4bool quasiElastic)
+G4HadronPhysicsQGSP_FTFP_BERT_ATL::G4HadronPhysicsQGSP_FTFP_BERT_ATL(const G4String& name, G4bool /*quasiElastic*/)
     :  G4VPhysicsConstructor(name) 
 /*    , theNeutrons(0)
     , theBertiniNeutron(0)
@@ -110,7 +109,6 @@ G4HadronPhysicsQGSP_FTFP_BERT_ATL::G4HadronPhysicsQGSP_FTFP_BERT_ATL(const G4Str
     , theHyperon(0)
     , theAntiBaryon(0)
     , theFTFPAntiBaryon(0)*/
-    , QuasiElastic(quasiElastic)
   /*    , ChipsKaonMinus(0)
     , ChipsKaonPlus(0)
     , ChipsKaonZero(0)
