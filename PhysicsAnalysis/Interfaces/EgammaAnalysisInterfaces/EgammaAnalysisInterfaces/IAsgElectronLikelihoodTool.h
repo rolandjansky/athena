a76c94fefd20c1086c4e3c782aa2e5ff94ddd64c
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2018-01-08 15:44:23 +0100

    Mistakenly forgot some conflicts in this file during the previous large conflict resolution campaign.
    Former-commit-id: a77658d28de47152126cc8c8fed28e9aeac583ce

--- PhysicsAnalysis/Interfaces/EgammaAnalysisInterfaces/EgammaAnalysisInterfaces/IAsgElectronLikelihoodTool.h
+++ PhysicsAnalysis/Interfaces/EgammaAnalysisInterfaces/EgammaAnalysisInterfaces/IAsgElectronLikelihoodTool.h
@@ -14,12 +14,6 @@
 
 */
 
-<<<<<<< HEAD
-=======
-// CONSIDER MERGING THIS WITH IASGELECTRONMULTILEPTONSELECTOR, SINCE THE INTERFACE
-// IS EXACTLY THE SAME. LEAVING THEM SEPERATE FOR NOW IN CASE THEY DIVERGE
-
->>>>>>> upstream/21.0
 // Include the interfaces
 #include "PATCore/IAsgSelectionTool.h"
 // Forward declarations
@@ -35,7 +29,6 @@ class IAsgElectronLikelihoodTool : virtual public IAsgSelectionTool
 
   ASG_TOOL_INTERFACE(IAsgElectronLikelihoodTool)
 
-<<<<<<< HEAD
  public:
 
   /// @name IAsgElectronLikelihoodTool  methods in Addition to the IAsgSelectionTool ones
@@ -81,70 +74,7 @@ class IAsgElectronLikelihoodTool : virtual public IAsgSelectionTool
   ///@}
 
   /// Virtual Destructor
-  virtual ~IAsgElectronLikelihoodTool() {};
-=======
-  public:
-
-  /**Virtual Destructor*/
-  virtual ~IAsgElectronLikelihoodTool() {};
-
-
-  /** The main accept method: using the generic interface , with pointers*/
-  virtual const Root::TAccept& accept( const xAOD::IParticle* part ) const = 0;
-
-  /** The main accept method: the actual cuts are applied here */
-  virtual const Root::TAccept& accept( const xAOD::Electron* part ) const = 0;
-
-  /** The main accept method: the actual cuts are applied here */
-  virtual const Root::TAccept& accept( const xAOD::Egamma* part ) const = 0;
-
-  /** The main accept method: in case mu not in EventInfo online */
-  virtual const Root::TAccept& accept( const xAOD::Electron* part, double mu ) const = 0;
-
-  /** The main accept method: in case mu not in EventInfo online */
-  virtual const Root::TAccept& accept( const xAOD::Egamma* part, double mu ) const = 0;
-
-
-  /** The main accept method: using the generic interface, with reference */
-  virtual const Root::TAccept& accept( const xAOD::IParticle& part) const = 0;
-
-  /** The main accept method: the actual cuts are applied here */
-  virtual const Root::TAccept& accept( const xAOD::Electron& part ) const = 0;
-
-  /** The main accept method: the actual cuts are applied here */
-  virtual const Root::TAccept& accept( const xAOD::Egamma& part ) const = 0;
-
-  /** The main accept method: in case mu not in EventInfo online */
-  virtual const Root::TAccept& accept( const xAOD::Electron& part, double mu ) const = 0;
-
-  /** The main accept method: in case mu not in EventInfo online */
-  virtual const Root::TAccept& accept( const xAOD::Egamma& part, double mu ) const = 0;
-
-
-  /** The main result method: the actual likelihood is calculated here */
-  virtual const Root::TResult& calculate( const xAOD::IParticle* part ) const = 0;
-
-  /** The main result method: the actual likelihood is calculated here */
-  virtual const Root::TResult& calculate( const xAOD::Electron* eg ) const = 0;
-
-  /** The main result method: the actual likelihood is calculated here */
-  virtual const Root::TResult& calculate( const xAOD::Egamma* eg ) const = 0;
-
-  /** The main result method: in case mu not in EventInfo online */
-  virtual const Root::TResult& calculate( const xAOD::Electron* eg, double mu ) const = 0;
-
-  /** The main result method: in case mu not in EventInfo online */
-  virtual const Root::TResult& calculate( const xAOD::Egamma* eg, double mu ) const = 0;
-
-
-  /** Method to get the plain TResult */
-  virtual const Root::TResult& getTResult( ) const=0;
-
-  /** Method to get the operating point */
-  virtual std::string getOperatingPointName( ) const =0;
-
-  //virtual const Root::TAccept& getTAccept( ) const =0; // in base
->>>>>>> upstream/21.0
+  virtual ~IAsgElectronLikelihoodTool() {}
 
 }; // End: class definition
 
