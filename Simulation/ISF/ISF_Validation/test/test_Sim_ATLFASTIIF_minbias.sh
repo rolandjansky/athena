83760b79ef141c2c713e446d473378fc48e58e16
John Chapman <jchapman@cern.ch>
2018-01-10 11:19:51 +0100

    Update ISF Validation ART tests to all use MC15aPlus Truth Strategy
    Former-commit-id: d3c332bdcfe4f36a76b4f1a17a65bbe4a9a012a7

--- Simulation/ISF/ISF_Validation/test/test_Sim_ATLFASTIIF_minbias.sh
+++ Simulation/ISF/ISF_Validation/test/test_Sim_ATLFASTIIF_minbias.sh
@@ -3,7 +3,7 @@
 # art-description: Run simulation using ISF with the ATLFASTIIF simulator, reading minbias events, writing HITS, using 2015 geometry and conditions
 # art-type: grid
 
-Sim_tf.py --conditionsTag 'default:OFLCOND-RUN12-SDR-19' --physicsList 'FTFP_BERT' --truthStrategy 'MC12' --simulator 'ATLFASTIIF' --postInclude 'default:PyJobTransforms/UseFrontier.py' 'G4AtlasTests/postInclude.DCubeTest.py' --DataRunNumber '222525' --geometryVersion 'default:ATLAS-R2-2015-03-01-00_VALIDATION' --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ISF_Validation/mc12_valid.119994.Pythia8_A2MSTW2008LO_minbias_inelastic.evgen.EVNT.e3099.01517253._000001.pool.root.1" --outputHITSFile "test.HITS.pool.root" --maxEvents 2000
+Sim_tf.py --conditionsTag 'default:OFLCOND-RUN12-SDR-19' --physicsList 'FTFP_BERT' --truthStrategy 'MC15aPlus' --simulator 'ATLFASTIIF' --postInclude 'default:PyJobTransforms/UseFrontier.py' 'G4AtlasTests/postInclude.DCubeTest.py' --DataRunNumber '222525' --geometryVersion 'default:ATLAS-R2-2015-03-01-00' --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ISF_Validation/mc12_valid.119994.Pythia8_A2MSTW2008LO_minbias_inelastic.evgen.EVNT.e3099.01517253._000001.pool.root.1" --outputHITSFile "test.HITS.pool.root" --maxEvents 2000
 
 ArtPackage=$1
 ArtJobName=$2
