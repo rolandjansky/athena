09020a6356337eb411410010a11f63951f157371
Christian Wiel <christian.wiel@cern.ch>
2018-01-18 16:17:31 +0100

    add new LepHadBuilders and Interface
    add new tools that build ditau containers for the lep had decay channels
    with a common interface
    add dual use support in CMakeLists.txt
    
    
    Former-commit-id: d12b6143da23485cebd4b7572b4b6ec747337e9c

---
+++ Reconstruction/DiTauRec/DiTauRec/HadMuBuilder.h
@@ -0,0 +1,37 @@
+#ifndef DITAUREC_HADMUBUILDER_H
+#define DITAUREC_HADMUBUILDER_H
+
+#include "AsgTools/AsgTool.h"
+
+#include "DiTauRec/ILepHadBuilder.h"
+
+namespace DiTauRec
+{
+  
+class HadMuBuilder
+  : public DiTauRec::ILepHadBuilder
+  , public asg::AsgTool
+{
+
+  ASG_TOOL_CLASS( HadMuBuilder,
+		  DiTauRec::ILepHadBuilder )
+ 
+public:
+
+  HadMuBuilder( const std::string& name );
+
+  virtual ~HadMuBuilder() override;
+
+  virtual StatusCode initialize() override;
+
+  virtual StatusCode initializeEvent() override;
+
+  virtual StatusCode execute() override;
+private:
+  std::string m_sDiTauHadMuContainerName;
+  std::string m_sDiTauHadMuAuxContainerName;
+  std::string m_sMuonContainerName;
+  std::string m_sTauContainerName;
+};
+}
+#endif // DITAUREC_HADMUBUILDER_H
---
+++ Reconstruction/DiTauRec/DiTauRec/ILepHadBuilder.h
@@ -0,0 +1,27 @@
+#ifndef DITAUREC_ILEPHADBUILDER_H
+#define DITAUREC_ILEPHADBUILDER_H
+
+#include "AsgTools/IAsgTool.h"
+namespace DiTauRec
+{
+class ILepHadBuilder :
+    public virtual asg::IAsgTool
+{
+  
+  // Declare the interface that the class provides
+  ASG_TOOL_INTERFACE( DiTauRec::ILepHadBuilder )
+  
+  public:
+  // initialize the tool
+  virtual StatusCode initialize() = 0;
+
+  // set pointer to event
+  virtual StatusCode initializeEvent() = 0;
+
+  virtual StatusCode execute() = 0;
+  
+}; //class ILepHadBuilder
+} // namespace DiTauRec
+
+
+#endif // DITAUREC_ILEPHADBUILDER_H
