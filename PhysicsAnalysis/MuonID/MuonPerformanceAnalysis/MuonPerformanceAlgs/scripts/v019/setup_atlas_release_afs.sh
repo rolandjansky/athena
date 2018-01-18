d4d770bc671929d98259a8cd36fb4d9755432be2
Nicolas Köhler <nicolas.koehler@cern.ch>
2018-01-18 13:06:51 +0100

    removed obsolete MCP MuonPerformanceAnalysis packages
    Former-commit-id: 398a12a466252a526d4475f6a1249735c0721337

--- PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonPerformanceAlgs/scripts/v019/setup_atlas_release_afs.sh
+++
@@ -1,3 +0,0 @@
-source $AtlasSetup/scripts/asetup.sh AtlasDerivation,20.7.5.3,here,save,slc6 --testarea=`pwd`
-
-alias cdl='cd $TestArea/PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonPerformanceAlgs/'
--- PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonPerformanceAlgs/scripts/v023/setup_atlas_release_afs.sh
+++
@@ -1,3 +0,0 @@
-source $AtlasSetup/scripts/asetup.sh AtlasDerivation,20.7.6.4,here,save --testarea=`pwd`
-
-alias cdl='cd $TestArea/PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonPerformanceAlgs/'
