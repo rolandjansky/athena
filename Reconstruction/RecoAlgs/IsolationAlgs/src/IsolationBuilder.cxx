6af107308addb497735e8a475d41112ab1cd0c6e
Jovan Mitrevski <Jovan.Mitrevski@cern.ch>
2018-01-19 17:21:21 +0100

    Switch from using templated implementation to using IParticleContainer. Still doesn't compile because of vector<vector<int>> property; would like to keep it and have the ability to configure those structures added.
    Former-commit-id: 479e950db2490e2b55c010873f48264d1a0c9fdb

--- Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.cxx
+++ Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.cxx
@@ -28,28 +28,28 @@ StatusCode IsolationBuilder::initialize()
   std::set<xAOD::Iso::IsolationFlavour> runIsoType;
   
   ATH_MSG_DEBUG("Initializing central electrons");
-  ATH_CHECK(initializeIso<xAOD::ElectronContainer>(runIsoType, &m_elCaloIso, &m_elTrackIso, 
-						   m_ElectronContainerName,
-						   m_elisoInts, m_elcorInts,
-						   m_customConfigEl, false));
-
+  ATH_CHECK(initializeIso(runIsoType, &m_elCaloIso, &m_elTrackIso, 
+			  m_ElectronContainerName,
+			  m_elisoInts, m_elcorInts,
+			  m_customConfigEl, false));
+  
   ATH_MSG_DEBUG("Initializing central photons");
-  ATH_CHECK(initializeIso<xAOD::PhotonContainer>(runIsoType, &m_phCaloIso, &m_phTrackIso, 
-						 m_PhotonContainerName,
-						 m_phisoInts, m_phcorInts,
-						 m_customConfigPh, false));
-
+  ATH_CHECK(initializeIso(runIsoType, &m_phCaloIso, &m_phTrackIso, 
+			  m_PhotonContainerName,
+			  m_phisoInts, m_phcorInts,
+			  m_customConfigPh, false));
+  
   ATH_MSG_DEBUG("Initializing forward electrons");
-  ATH_CHECK(initializeIso<xAOD::ElectronContainer>(runIsoType, &m_feCaloIso, nullptr, 
-						   m_FwdElectronContainerName,
-						   m_feisoInts, m_fecorInts,
-						   m_customConfigFwd, false));
+  ATH_CHECK(initializeIso(runIsoType, &m_feCaloIso, nullptr, 
+			  m_FwdElectronContainerName,
+			  m_feisoInts, m_fecorInts,
+			  m_customConfigFwd, false));
 
   ATH_MSG_DEBUG("Initializing muons");
-  ATH_CHECK(initializeIso<xAOD::MuonContainer>(runIsoType, &m_muCaloIso, &m_muTrackIso, 
-					       m_MuonContainerName,
-					       m_muisoInts, m_mucorInts,
-					       m_customConfigMu, m_addCoreCorr));
+  ATH_CHECK(initializeIso(runIsoType, &m_muCaloIso, &m_muTrackIso, 
+			  m_MuonContainerName,
+			  m_muisoInts, m_mucorInts,
+			  m_customConfigMu, m_addCoreCorr));
 			  
 
   // Retrieve the tools (there three Calo ones are the same in fact)
@@ -172,6 +172,228 @@ StatusCode IsolationBuilder::execute()
   
   return StatusCode::SUCCESS;
 }
+
+IsolationBuilder::CaloIsoHelpHandles::CaloIsoHelpHandles(const IsolationBuilder::CaloIsoHelpKey& keys)
+{
+  for (const auto& key : keys.isoDeco) {
+    isoDeco.emplace_back(key);
+  }
+}
+
+IsolationBuilder::TrackIsoHelpHandles::TrackIsoHelpHandles(const IsolationBuilder::TrackIsoHelpKey& keys)
+{
+  for (const auto& key : keys.isoDeco) {
+    isoDeco.emplace_back(key);
+  }
+  for (const auto& key : keys.isoDecoV) {
+    isoDecoV.emplace_back(key);
+  }
+}
+
+StatusCode IsolationBuilder::initializeIso(std::set<xAOD::Iso::IsolationFlavour>& runIsoType, // out
+					   std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> >*  caloIsoMap, // out
+					   std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey> >* trackIsoMap, // out
+					   const std::string& containerName,
+					   const std::vector<std::vector<int> >& isoInts,
+					   const std::vector<std::vector<unsigned int> >& corInts,
+					   const std::string& customConfig,
+					   bool addCoreCorr)
+{
+  
+  std::string prefix = containerName + ".";
+  
+  for (size_t flavor = 0; flavor < isoInts.size(); flavor++) {
+    // iterate over the flavor (cell, topo, eflow, track
+    //   Note: it is a configuration error if different types
+    //         are included in one inner vector
+
+    CaloIsoHelpKey cisoH;
+    TrackIsoHelpKey tisoH;
+    
+    //std::vector<SG::AuxElement::Decorator<float>*> Deco;
+    xAOD::Iso::IsolationFlavour isoFlav =
+      xAOD::Iso::numIsolationFlavours;
+    xAOD::Iso::IsolationFlavour oldIsoFlav =
+      xAOD::Iso::numIsolationFlavours;
+    for (size_t type = 0; type < isoInts[flavor].size();type++) { // the cone size
+      xAOD::Iso::IsolationType isoType = static_cast<xAOD::Iso::IsolationType>(isoInts[flavor][type]);
+      isoFlav = xAOD::Iso::isolationFlavour(isoType);
+      if (oldIsoFlav != xAOD::Iso::numIsolationFlavours && oldIsoFlav != isoFlav) {
+	ATH_MSG_FATAL("Configuration error:  can only have one type of isolation in inner vector");
+	return StatusCode::FAILURE;
+      }
+      oldIsoFlav = isoFlav;
+      std::string isoName = prefix + std::string(xAOD::Iso::toString(isoType));
+      if (customConfig != "") {
+	isoName += "_" + customConfig;
+      }
+      if (isoFlav == xAOD::Iso::etcone || isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
+	cisoH.isoTypes.push_back(isoType);
+	cisoH.isoDeco.emplace_back(isoName);
+	ATH_CHECK(cisoH.isoDeco.back().initialize());
+      } else if (isoFlav == xAOD::Iso::ptcone) {
+	tisoH.isoTypes.push_back(isoType);
+	tisoH.isoDeco.emplace_back(isoName);
+	ATH_CHECK(tisoH.isoDeco.back().initialize());
+	std::string isoNameV = prefix + "ptvarcone" + std::to_string(100*xAOD::Iso::coneSize(isoType));
+	if (customConfig != "") {
+	  isoNameV += "_" + customConfig;
+	}
+	tisoH.isoDecoV.emplace_back(isoNameV);
+	ATH_CHECK(tisoH.isoDeco.back().initialize());
+      } else {
+	ATH_MSG_FATAL("Configuration error: Isolation flavor " << isoFlav << " not supported.");
+	return StatusCode::FAILURE;
+      }	
+    }
+    if (isoFlav == xAOD::Iso::etcone || isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
+      
+      for (size_t corrType = 0; corrType < corInts[flavor].size(); corrType++) {
+	const unsigned int cor = corInts[flavor][corrType];
+	cisoH.CorrList.calobitset.set(cor);
+	const xAOD::Iso::IsolationCaloCorrection isoCor = static_cast<xAOD::Iso::IsolationCaloCorrection>(cor);
+	if (addCoreCorr && (isoCor == xAOD::Iso::coreCone || isoCor == xAOD::Iso::coreMuon) ) {
+	  std::string isoCorName = prefix;
+	  if (isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
+	    isoCorName += std::string(xAOD::Iso::toString(isoFlav));
+	  }
+	  isoCorName += std::string(xAOD::Iso::toString(isoCor)) + std::string(xAOD::Iso::toString(xAOD::Iso::coreEnergy))
+	    + "Correction"; // hard coded since we never store the core area in fact
+	  if (customConfig != "") {
+	    isoCorName += "_" + customConfig;
+	  }
+	  cisoH.addCoreCorr = true;
+	  cisoH.coreCorisoDeco = isoCorName;
+	  ATH_CHECK(cisoH.coreCorisoDeco.initialize());
+	}
+      }
+      if (caloIsoMap) {
+	caloIsoMap->push_back(std::make_pair(isoFlav,cisoH));
+      } else {
+	ATH_MSG_FATAL("caloIsoMap was nullptr but the configuration attempted to use it");
+	return StatusCode::FAILURE;
+      }
+    } else if (isoFlav == xAOD::Iso::ptcone) {
+      tisoH.CorrList.trackbitset.set(static_cast<unsigned int>(xAOD::Iso::coreTrackPtr));
+      if (trackIsoMap) {
+	trackIsoMap->push_back(std::make_pair(isoFlav,tisoH));
+      } else {
+	ATH_MSG_FATAL("trackIsoMap was nullptr but the configuration attempted to use it");
+	return StatusCode::FAILURE;
+      }	
+    } else 
+      ATH_MSG_WARNING("Isolation flavour " << std::string(xAOD::Iso::toString(isoFlav)) << " does not exist ! Check your inputs");
+    runIsoType.insert(isoFlav);
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode IsolationBuilder::executeCaloIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> >& caloIsoMap)
+{
+  for (const auto& pr : caloIsoMap) {
+    const xAOD::Iso::IsolationFlavour flav = pr.first;
+    const auto& keys =  pr.second;
+    CaloIsoHelpHandles handles(keys);
+
+    if (handles.isoDeco.empty()) {
+      ATH_MSG_FATAL("Have a CaloIsoHelpHandles with no actual isolations; something wrong happened");
+      return StatusCode::FAILURE;
+    }
+    auto& readHandle = handles.isoDeco[0]; // can treat the writeDecorHandle as a read handle;
+    if (!readHandle.isValid()) {
+      ATH_MSG_FATAL("Could not retrieve read handle for " << keys.isoDeco[0].key());
+      return StatusCode::FAILURE;
+    }
+
+    for (const auto& part : *readHandle) {
+      xAOD::CaloIsolation CaloIsoResult;
+      bool successfulCalc = false;
+      if (flav == xAOD::Iso::IsolationFlavour::etcone && m_cellColl) {
+	successfulCalc = m_cellIsolationTool->caloCellIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList, m_cellColl);
+      } else if (flav == xAOD::Iso::IsolationFlavour::topoetcone) {
+	successfulCalc = m_topoIsolationTool->caloTopoClusterIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList);
+      } else if (flav == xAOD::Iso::IsolationFlavour::neflowisol) {
+	successfulCalc = m_pflowIsolationTool->neutralEflowIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList);
+      }
+
+      if (successfulCalc) {
+	for (unsigned int i = 0; i < keys.isoTypes.size(); i++) {
+	  float iso = CaloIsoResult.etcones[i];
+	  ATH_MSG_DEBUG("custom Iso " << std::string(xAOD::Iso::toString(keys.isoTypes[i])) << " = " << iso/1e3);
+	  (handles.isoDeco[i])(*part) = iso;
+	}
+	// not that nice. I expect a single core correction (e.g. for topoetcone, not coreMuon and coreCone together...)
+	if (keys.addCoreCorr) {
+	  SG::WriteDecorHandle<xAOD::IParticleContainer, float> coreCorisoDeco(keys.coreCorisoDeco);
+	  xAOD::Iso::IsolationCaloCorrection icc = xAOD::Iso::coreCone;
+	  bool ison = false;
+	  if (CaloIsoResult.corrlist.calobitset.test(static_cast<unsigned int>(xAOD::Iso::coreMuon))) {
+	    icc  = xAOD::Iso::coreMuon;
+	    ison = true;
+	  } else if (CaloIsoResult.corrlist.calobitset.test(static_cast<unsigned int>(xAOD::Iso::coreCone))) {
+	    ison = true;
+	  }
+	  if (ison) {
+	    if (CaloIsoResult.coreCorrections.find(icc) != CaloIsoResult.coreCorrections.end()) {
+	      if (CaloIsoResult.coreCorrections[icc].find(xAOD::Iso::coreEnergy) != CaloIsoResult.coreCorrections[icc].end()) {
+		coreCorisoDeco(*part) = CaloIsoResult.coreCorrections[icc][xAOD::Iso::coreEnergy];
+	      } else {
+		ATH_MSG_WARNING("Cannot find the core energy correction for custom flavour " << std::string(xAOD::Iso::toString(flav)));
+	      }
+	    } else {
+	      ATH_MSG_WARNING("Cannot find the core correction for custom flavour " << std::string(xAOD::Iso::toString(flav)));
+	    }
+	  }
+	}
+      } else {
+	ATH_MSG_ERROR("Call to CaloIsolationTool failed for custom flavour " << std::string(xAOD::Iso::toString(flav)));
+	return StatusCode::RECOVERABLE;
+      }
+    }
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode IsolationBuilder::executeTrackIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey > >& trackIsoMap)
+{
+  for (const auto& pr : trackIsoMap) {
+    const xAOD::Iso::IsolationFlavour flav = pr.first;
+    const auto& keys =  pr.second;
+    TrackIsoHelpHandles handles(keys);
+
+    if (handles.isoDeco.empty()) {
+      ATH_MSG_FATAL("Have a TrackIsoHelpHandles with no actual isolations; something wrong happened");
+      return StatusCode::FAILURE;
+    }
+    auto& readHandle = handles.isoDeco[0]; // can treat the writeDecorHandle as a read handle;
+    if (!readHandle.isValid()) {
+      ATH_MSG_FATAL("Could not retrieve read handle for " << keys.isoDeco[0].key());
+      return StatusCode::FAILURE;
+    }
+
+    for (const auto& part : *readHandle) {
+      xAOD::TrackIsolation TrackIsoResult;
+      bool successfulCalc = m_trackIsolationTool->trackIsolation(TrackIsoResult, *part, keys.isoTypes, keys.CorrList);
+
+      if (successfulCalc) {
+	for (unsigned int i = 0; i < keys.isoTypes.size(); i++) {
+	  float iso = TrackIsoResult.ptcones[i];
+	  float isoV = TrackIsoResult.ptvarcones_10GeVDivPt[i];
+	  ATH_MSG_DEBUG("custom Iso " << std::string(xAOD::Iso::toString(keys.isoTypes[i])) << " = " << iso/1e3 << ", var cone = " << isoV/1e3);
+	  (handles.isoDeco[i])(*part) = iso;
+	  (handles.isoDecoV[i])(*part) = isoV;
+	}
+      } else {
+	ATH_MSG_ERROR("Call to TrackIsolationTool failed for custom flavour " << std::string(xAOD::Iso::toString(flav)));
+	return StatusCode::RECOVERABLE;
+      }
+    }
+  }
+  return StatusCode::SUCCESS;
+}
+
+
+
  
 // JM:  of AODFix, so commenting out for now
 
--- Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.h
+++ Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.h
@@ -32,9 +32,7 @@
 #include "xAODPrimitives/IsolationConeSize.h"
 #include "xAODPrimitives/IsolationHelpers.h"
 #include "xAODPrimitives/IsolationFlavour.h"
-#include "xAODMuon/MuonContainer.h"
-#include "xAODEgamma/ElectronContainer.h"
-#include "xAODEgamma/PhotonContainer.h"
+#include "xAODBase/IParticleContainer.h"
 
 #include "RecoToolInterfaces/ICaloCellIsolationTool.h"
 #include "RecoToolInterfaces/ICaloTopoClusterIsolationTool.h"
@@ -157,45 +155,45 @@ class IsolationBuilder
   Gaudi::Property<bool> m_addCoreCorr{this, "AddCoreCorr", true, 
       "Add core correction for muon calo isolation"};
 
-  template<class T> struct CaloIsoHelpKey {
-    std::vector<SG::WriteDecorHandleKey<T> > isoDeco;
+  struct CaloIsoHelpKey {
+    std::vector<SG::WriteDecorHandleKey<xAOD::IParticleContainer> > isoDeco;
     bool addCoreCorr = false;
-    SG::WriteDecorHandleKey<T> coreCorisoDeco;
+    SG::WriteDecorHandleKey<xAOD::IParticleContainer> coreCorisoDeco;
     std::vector<xAOD::Iso::IsolationType> isoTypes;
     xAOD::CaloCorrection CorrList;
   };
 
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<xAOD::ElectronContainer> > > m_elCaloIso;
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<xAOD::PhotonContainer> > > m_phCaloIso;
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<xAOD::ElectronContainer> > > m_feCaloIso;
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<xAOD::MuonContainer> > > m_muCaloIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> > m_elCaloIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> > m_phCaloIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> > m_feCaloIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> > m_muCaloIso;
 
-  template<class T> struct TrackIsoHelpKey {
-    std::vector<SG::WriteDecorHandleKey<T> > isoDeco;
-    std::vector<SG::WriteDecorHandleKey<T> > isoDecoV;
+  struct TrackIsoHelpKey {
+    std::vector<SG::WriteDecorHandleKey<xAOD::IParticleContainer> > isoDeco;
+    std::vector<SG::WriteDecorHandleKey<xAOD::IParticleContainer> > isoDecoV;
     std::vector<xAOD::Iso::IsolationType> isoTypes;
     xAOD::TrackCorrection CorrList;
   };
 
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<xAOD::ElectronContainer> > > m_elTrackIso;
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<xAOD::PhotonContainer> > > m_phTrackIso;
-  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<xAOD::MuonContainer> > > m_muTrackIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey> > m_elTrackIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey> > m_phTrackIso;
+  std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey> > m_muTrackIso;
 
-  template<class T> struct CaloIsoHelpHandles {
+  struct CaloIsoHelpHandles {
 
-    CaloIsoHelpHandles(const CaloIsoHelpKey<T>& keys);
+    CaloIsoHelpHandles(const CaloIsoHelpKey& keys);
 
-    std::vector<SG::WriteDecorHandle<T, float> > isoDeco;
-    SG::WriteDecorHandle<T, float> coreCorisoDeco;
+    std::vector<SG::WriteDecorHandle<xAOD::IParticleContainer, float> > isoDeco;
+    // SG::WriteDecorHandle<xAOD::IParticleContainer, float> coreCorisoDeco; // try moving it
   };
 
 
-  template<class T> struct TrackIsoHelpHandles {
+  struct TrackIsoHelpHandles {
 
-    TrackIsoHelpHandles(const TrackIsoHelpKey<T>& keys);
+    TrackIsoHelpHandles(const TrackIsoHelpKey& keys);
 
-    std::vector<SG::WriteDecorHandle<T, float> > isoDeco;
-    std::vector<SG::WriteDecorHandle<T, float> > isoDecoV;
+    std::vector<SG::WriteDecorHandle<xAOD::IParticleContainer, float> > isoDeco;
+    std::vector<SG::WriteDecorHandle<xAOD::IParticleContainer, float> > isoDecoV;
   };
 
 
@@ -219,21 +217,18 @@ class IsolationBuilder
   Gaudi::Property<bool> m_allTrackRemoval {this, 
       "AllTrackRemoval", true};
 
-  template<class T>
   StatusCode initializeIso(std::set<xAOD::Iso::IsolationFlavour>& runIsoType, // out
-			   std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<T> > >* caloIsoMap, // out
-			   std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<T> > >* trackIsoMap, // out
+			   std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey > >* caloIsoMap, // out
+			   std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey > >* trackIsoMap, // out
 			   const std::string& containerName,
 			   const std::vector<std::vector<int> >& isoInts,
 			   const std::vector<std::vector<unsigned int> >& corInts,
 			   const std::string& customConfig,
 			   bool addCoreCorr);
 
-  template<class T>
-  StatusCode executeCaloIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<T> > >& caloIsoMap);
+  StatusCode executeCaloIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey> >& caloIsoMap);
 
-  template<class T>
-  StatusCode executeTrackIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<T> > >& trackIsoMap);
+  StatusCode executeTrackIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey> >& trackIsoMap);
 
   // For an AODFix
   // JM:  Not yet supported
@@ -249,233 +244,6 @@ class IsolationBuilder
   
   
 }; 
-
-template<class T>
-IsolationBuilder::CaloIsoHelpHandles<T>::CaloIsoHelpHandles(const IsolationBuilder::CaloIsoHelpKey<T>& keys)
-{
-  for (const SG::WriteDecorHandleKey<T>& key : keys.isoDeco) {
-    isoDeco.emplace_back(key);
-  }
-
-  if (keys.addCoreCorr) {
-    coreCorisoDeco = SG::WriteDecorHandle<T, float>(keys.coreCorisoDeco);
-  }
-}
-
-template<class T>
-IsolationBuilder::TrackIsoHelpHandles<T>::TrackIsoHelpHandles(const IsolationBuilder::TrackIsoHelpKey<T>& keys)
-{
-  for (const SG::WriteDecorHandleKey<T>& key : keys.isoDeco) {
-    isoDeco.emplace_back(key);
-  }
-  for (const SG::WriteDecorHandleKey<T>& key : keys.isoDecoV) {
-    isoDecoV.emplace_back(key);
-  }
-}
-
-template<class T>
-StatusCode IsolationBuilder::initializeIso(std::set<xAOD::Iso::IsolationFlavour>& runIsoType, // out
-					   std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<T> > >*  caloIsoMap, // out
-					   std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<T> > >* trackIsoMap, // out
-					   const std::string& containerName,
-					   const std::vector<std::vector<int> >& isoInts,
-					   const std::vector<std::vector<unsigned int> >& corInts,
-					   const std::string& customConfig,
-					   bool addCoreCorr)
-{
-  
-  std::string prefix = containerName + ".";
-  
-  for (size_t flavor = 0; flavor < isoInts.size(); flavor++) {
-    // iterate over the flavor (cell, topo, eflow, track
-    //   Note: it is a configuration error if different types
-    //         are included in one inner vector
-
-    CaloIsoHelpKey<T> cisoH;
-    TrackIsoHelpKey<T> tisoH;
-    
-    //std::vector<SG::AuxElement::Decorator<float>*> Deco;
-    xAOD::Iso::IsolationFlavour isoFlav =
-      xAOD::Iso::numIsolationFlavours;
-    xAOD::Iso::IsolationFlavour oldIsoFlav =
-      xAOD::Iso::numIsolationFlavours;
-    for (size_t type = 0; type < isoInts[flavor].size();type++) { // the cone size
-      xAOD::Iso::IsolationType isoType = static_cast<xAOD::Iso::IsolationType>(isoInts[flavor][type]);
-      isoFlav = xAOD::Iso::isolationFlavour(isoType);
-      if (oldIsoFlav != xAOD::Iso::numIsolationFlavours && oldIsoFlav != isoFlav) {
-	ATH_MSG_FATAL("Configuration error:  can only have one type of isolation in inner vector");
-	return StatusCode::FAILURE;
-      }
-      oldIsoFlav = isoFlav;
-      std::string isoName = prefix + std::string(xAOD::Iso::toString(isoType));
-      if (customConfig != "") {
-	isoName += "_" + customConfig;
-      }
-      if (isoFlav == xAOD::Iso::etcone || isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
-	cisoH.isoTypes.push_back(isoType);
-	cisoH.isoDeco.emplace_back(isoName);
-	ATH_CHECK(cisoH.isoDeco.back().initialize());
-      } else if (isoFlav == xAOD::Iso::ptcone) {
-	tisoH.isoTypes.push_back(isoType);
-	tisoH.isoDeco.emplace_back(isoName);
-	ATH_CHECK(tisoH.isoDeco.back().initialize());
-	std::string isoNameV = prefix + "ptvarcone" + std::to_string(100*xAOD::Iso::coneSize(isoType));
-	if (customConfig != "") {
-	  isoNameV += "_" + customConfig;
-	}
-	tisoH.isoDecoV.emplace_back(isoNameV);
-	ATH_CHECK(tisoH.isoDeco.back().initialize());
-      } else {
-	ATH_MSG_FATAL("Configuration error: Isolation flavor " << isoFlav << " not supported.");
-	return StatusCode::FAILURE;
-      }	
-    }
-    if (isoFlav == xAOD::Iso::etcone || isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
-      
-      for (size_t corrType = 0; corrType < corInts[flavor].size(); corrType++) {
-	const unsigned int cor = corInts[flavor][corrType];
-	cisoH.CorrList.calobitset.set(cor);
-	const xAOD::Iso::IsolationCaloCorrection isoCor = static_cast<xAOD::Iso::IsolationCaloCorrection>(cor);
-	if (addCoreCorr && (isoCor == xAOD::Iso::coreCone || isoCor == xAOD::Iso::coreMuon) ) {
-	  std::string isoCorName = prefix;
-	  if (isoFlav == xAOD::Iso::topoetcone || isoFlav == xAOD::Iso::neflowisol) {
-	    isoCorName += std::string(xAOD::Iso::toString(isoFlav));
-	  }
-	  isoCorName += std::string(xAOD::Iso::toString(isoCor)) + std::string(xAOD::Iso::toString(xAOD::Iso::coreEnergy))
-	    + "Correction"; // hard coded since we never store the core area in fact
-	  if (customConfig != "") {
-	    isoCorName += "_" + customConfig;
-	  }
-	  cisoH.addCoreCorr = true;
-	  cisoH.coreCorisoDeco = isoCorName;
-	  ATH_CHECK(cisoH.coreCorisoDeco.initialize());
-	}
-      }
-      if (caloIsoMap) {
-	caloIsoMap->push_back(std::make_pair(isoFlav,cisoH));
-      } else {
-	ATH_MSG_FATAL("caloIsoMap was nullptr but the configuration attempted to use it");
-	return StatusCode::FAILURE;
-      }
-    } else if (isoFlav == xAOD::Iso::ptcone) {
-      tisoH.CorrList.trackbitset.set(static_cast<unsigned int>(xAOD::Iso::coreTrackPtr));
-      if (trackIsoMap) {
-	trackIsoMap->push_back(std::make_pair(isoFlav,tisoH));
-      } else {
-	ATH_MSG_FATAL("trackIsoMap was nullptr but the configuration attempted to use it");
-	return StatusCode::FAILURE;
-      }	
-    } else 
-      ATH_MSG_WARNING("Isolation flavour " << std::string(xAOD::Iso::toString(isoFlav)) << " does not exist ! Check your inputs");
-    runIsoType.insert(isoFlav);
-  }
-  return StatusCode::SUCCESS;
-}
-
-template<class T>
-StatusCode IsolationBuilder::executeCaloIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,CaloIsoHelpKey<T> > >& caloIsoMap)
-{
-  for (const auto& pr : caloIsoMap) {
-    const xAOD::Iso::IsolationFlavour flav = pr.first;
-    const auto& keys =  pr.second;
-    CaloIsoHelpHandles<T> handles(keys);
-
-    if (handles.isoDeco.empty()) {
-      ATH_MSG_FATAL("Have a CaloIsoHelpHandles with no actual isolations; something wrong happened");
-      return StatusCode::FAILURE;
-    }
-    auto& readHandle = handles.isoDeco[0]; // can treat the writeDecorHandle as a read handle;
-    if (!readHandle.isValid()) {
-      ATH_MSG_FATAL("Could not retrieve read handle for " << keys.isoDeco[0].key());
-      return StatusCode::FAILURE;
-    }
-
-    for (const auto& part : *readHandle) {
-      xAOD::CaloIsolation CaloIsoResult;
-      bool successfulCalc = false;
-      if (flav == xAOD::Iso::IsolationFlavour::etcone && m_cellColl) {
-	successfulCalc = m_cellIsolationTool->caloCellIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList, m_cellColl);
-      } else if (flav == xAOD::Iso::IsolationFlavour::topoetcone) {
-	successfulCalc = m_topoIsolationTool->caloTopoClusterIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList);
-      } else if (flav == xAOD::Iso::IsolationFlavour::neflowisol) {
-	successfulCalc = m_pflowIsolationTool->neutralEflowIsolation(CaloIsoResult, *part, keys.isoTypes, keys.CorrList);
-      }
-
-      if (successfulCalc) {
-	for (unsigned int i = 0; i < keys.isoTypes.size(); i++) {
-	  float iso = CaloIsoResult.etcones[i];
-	  ATH_MSG_DEBUG("custom Iso " << std::string(xAOD::Iso::toString(keys.isoTypes[i])) << " = " << iso/1e3);
-	  (handles.isoDeco[i])(*part) = iso;
-	}
-	// not that nice. I expect a single core correction (e.g. for topoetcone, not coreMuon and coreCone together...)
-	if (keys.addCoreCorr) {
-	  xAOD::Iso::IsolationCaloCorrection icc = xAOD::Iso::coreCone;
-	  bool ison = false;
-	  if (CaloIsoResult.corrlist.calobitset.test(static_cast<unsigned int>(xAOD::Iso::coreMuon))) {
-	    icc  = xAOD::Iso::coreMuon;
-	    ison = true;
-	  } else if (CaloIsoResult.corrlist.calobitset.test(static_cast<unsigned int>(xAOD::Iso::coreCone))) {
-	    ison = true;
-	  }
-	  if (ison) {
-	    if (CaloIsoResult.coreCorrections.find(icc) != CaloIsoResult.coreCorrections.end()) {
-	      if (CaloIsoResult.coreCorrections[icc].find(xAOD::Iso::coreEnergy) != CaloIsoResult.coreCorrections[icc].end()) {
-		(handles.coreCorisoDeco)(*part) = CaloIsoResult.coreCorrections[icc][xAOD::Iso::coreEnergy];
-	      } else {
-		ATH_MSG_WARNING("Cannot find the core energy correction for custom flavour " << std::string(xAOD::Iso::toString(flav)));
-	      }
-	    } else {
-	      ATH_MSG_WARNING("Cannot find the core correction for custom flavour " << std::string(xAOD::Iso::toString(flav)));
-	    }
-	  }
-	}
-      } else {
-	ATH_MSG_ERROR("Call to CaloIsolationTool failed for custom flavour " << std::string(xAOD::Iso::toString(flav)));
-	return StatusCode::RECOVERABLE;
-      }
-    }
-  }
-  return StatusCode::SUCCESS;
-}
-
-template<class T>
-StatusCode IsolationBuilder::executeTrackIso(const std::vector<std::pair<xAOD::Iso::IsolationFlavour,TrackIsoHelpKey<T> > >& trackIsoMap)
-{
-  for (const auto& pr : trackIsoMap) {
-    const xAOD::Iso::IsolationFlavour flav = pr.first;
-    const auto& keys =  pr.second;
-    TrackIsoHelpHandles<T> handles(keys);
-
-    if (handles.isoDeco.empty()) {
-      ATH_MSG_FATAL("Have a TrackIsoHelpHandles with no actual isolations; something wrong happened");
-      return StatusCode::FAILURE;
-    }
-    auto& readHandle = handles.isoDeco[0]; // can treat the writeDecorHandle as a read handle;
-    if (!readHandle.isValid()) {
-      ATH_MSG_FATAL("Could not retrieve read handle for " << keys.isoDeco[0].key());
-      return StatusCode::FAILURE;
-    }
-
-    for (const auto& part : *readHandle) {
-      xAOD::TrackIsolation TrackIsoResult;
-      bool successfulCalc = m_trackIsolationTool->trackIsolation(TrackIsoResult, *part, keys.isoTypes, keys.CorrList);
-
-      if (successfulCalc) {
-	for (unsigned int i = 0; i < keys.isoTypes.size(); i++) {
-	  float iso = TrackIsoResult.ptcones[i];
-	  float isoV = TrackIsoResult.ptvarcones_10GeVDivPt[i];
-	  ATH_MSG_DEBUG("custom Iso " << std::string(xAOD::Iso::toString(keys.isoTypes[i])) << " = " << iso/1e3 << ", var cone = " << isoV/1e3);
-	  (handles.isoDeco[i])(*part) = iso;
-	  (handles.isoDecoV[i])(*part) = isoV;
-	}
-      } else {
-	ATH_MSG_ERROR("Call to TrackIsolationTool failed for custom flavour " << std::string(xAOD::Iso::toString(flav)));
-	return StatusCode::RECOVERABLE;
-      }
-    }
-  }
-  return StatusCode::SUCCESS;
-}
 
 
 #endif //> !ISOLATIONALGS_ISOLATIONBUILDER_H
