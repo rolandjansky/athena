36623154d2bd7661d50d474a66b587f3854357ff
sutt <sutt@cern.ch>
2018-01-20 11:22:31 +0100

    Reinstate jobs with newer datasets
    Replace the datasets for the taus jobs with non-empty comtainers
    
    
    Former-commit-id: e5798e147e089894b4363130d0b5085073e54d0f

--- Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_tau_ibl_pu46_offline.sh
+++ Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_tau_ibl_pu46_offline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# art-description: art job for tau_IBL_pu46
+# art-description: art job for tau_IBL_pu46_offline
 # art-type: grid
 # art-output: HLTEF-plots
 # art-output: HLTL2-plots
@@ -9,7 +9,7 @@
 # art-output: cost-perEvent
 # art-output: cost-perCall-chain
 # art-output: cost-perEvent-chain
-# art-input:  mc15_13TeV.361108.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Ztautau.recon.RDO.e3601_s2757_r7245
+# art-input:  mc15_13TeV:mc15_13TeV.361108.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Ztautau.recon.RDO.e3601_s2576_s2132_r8012
 # art-output: *.dat 
 # art-output: *.root
 # art-output: *.log
@@ -17,28 +17,28 @@
 
 
 
-export RTTJOBNAME=TrigInDetValidation_tau_IBL_pu46
+export RTTJOBNAME=TrigInDetValidation_tau_IBL_pu46_offline
 
 
 fileList="['${ArtInFile//,/', '}']"
 echo "List of files = $fileList"
 
 get_files -jo            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_TauSlice.py
-athena.py  -c "ARTConfig=$fileList;EventMax=8000"            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_TauSlice.py
+athena.py  -c "ARTConfig=$fileList;EventMax=4000;doIDNewTracking=True;PdgId=15"            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_TauSlice.py
 echo "art-result: $? athena_0"
 
-get_files -data TIDAdata11-rtt.dat
+get_files -data TIDAdata11-rtt-offline.dat
 get_files -data TIDAdata_cuts.dat
 get_files -data TIDAdata_chains.dat
 get_files -data TIDAbeam.dat
 get_files -data Test_bin.dat
-TIDArdict.exe TIDAdata11-rtt.dat -f data-tau-IBL.root -b Test_bin.dat
+TIDArdict.exe TIDAdata11-rtt-offline.dat -f data-tau-IBL.root -b Test_bin.dat
 echo "art-result: $? TIDArdict_1"
 
-TIDArun-art.sh data-tau-IBL.root data-tau_IBL_pu46-reference.root HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_FTF HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_IDTrig HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauIso_FTF_forID3 HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_Tau_IDTrig_forID3  -d HLTEF-plots
+TIDArun-art.sh data-tau-IBL.root data-tau_IBL_pu46_offline-reference.root HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_FTF HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_IDTrig   HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauIso_FTF_forID3 HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_Tau_IDTrig_forID3  -d HLTEF-plots
 echo "art-result: $? TIDArun_2"
 
-TIDArun-art.sh data-tau-IBL.root data-tau_IBL_pu46-reference.root  HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_FTF HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauCore_FTF_forID1 HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauIso_FTF_forID3 -d HLTL2-plots
+TIDArun-art.sh data-tau-IBL.root data-tau_IBL_pu46_offline-reference.root HLT_tau25_idperf_track_InDetTrigTrackingxAODCnv_Tau_FTF HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauCore_FTF_forID1 HLT_tau25_idperf_tracktwo_InDetTrigTrackingxAODCnv_TauIso_FTF_forID3 -d HLTL2-plots
 echo "art-result: $? TIDArun_3"
 
 TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -o times
@@ -61,5 +61,4 @@ echo "art-result: $? TIDAcpucost_9"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent-chain --auto -d "/Chain_Algorithm" -p "_Time_perEvent"
 echo "art-result: $? TIDAcpucost_10"
-
 
