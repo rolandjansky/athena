fe0b650ae6703d6d59c2c34b989b9bbc03c4e55f
James Beacham <j.beacham@cern.ch>
2018-01-23 20:29:34 +0000

    Merge branch '21.0-artid' into '21.0'
    Update the ART generator script and the ART scripts themselves
    
    See merge request atlas/athena!8161
    
    (cherry picked from commit ff87af60776480840ec6c947033a4cd14d0d7456 [formerly 531a5c9abba9b28464eb1ad3dfdecc3a9bfd2ad7])
    
    31834732 Update the ART generator script and the ART scripts themselves
    a8e178d7 Update the ART generator script and the ART scripts themselves
    9173aca5 Fix spacing in utils/mkart script
    d2c8d3d6 Further modifications to the ART scripts
    3505050c Deactivate additional script
    3719355c Deactivate additional scripts with empty container
    e5798e14 Reinstate jobs with newer datasets
    f21bb597 Fix permissions on ART scripts
    15bac209 Fix FTK ART job data sample and reinstate ART test
    f237e4b2 Add the new FTK data set to those available to the RTT
    55c5c039 Update RTT job for the FTK data sample
    
    Former-commit-id: 95a9ad6a817f674fb3f37bc34fcdaa0e103a0f03

--- Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_beamspot_ttbar_pu80_zfinder.sh
+++ Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_beamspot_ttbar_pu80_zfinder.sh
@@ -12,12 +12,19 @@
 # art-output: *.dat 
 # art-output: *.root
 # art-output: *.log
+# art-input-nfiles: 25
+
+
 
 export RTTJOBNAME=TrigInDetValidation_beamspot_ttbar_pu80_zfinder
 
+
+fileList="['${ArtInFile//,/', '}']"
+echo "List of files = $fileList"
+
 get_files -jo             TrigInDetValidation/TrigInDetValidation_RTT_topOptions_BeamspotSlice.py
-athena.py  -c 'ARTConfig=["/eos/atlas/atlascerngroupdisk/proj-sit/trigindet/mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.recon.RDO.e3698_s2608_s2183_r7195/RDO.06752780._000001.pool.root.1"];	    EventMax=1000;runMergedChain=True;fastZFinder=True;rec.doFloatingPointException.set_Value_and_Lock(False)'             TrigInDetValidation/TrigInDetValidation_RTT_topOptions_BeamspotSlice.py
-JOBSTATUS_0=$?
+athena.py  -c "ARTConfig=$fileList;EventMax=2000;fastZFinder=True;rec.doFloatingPointException.set_Value_and_Lock(False)"             TrigInDetValidation/TrigInDetValidation_RTT_topOptions_BeamspotSlice.py
+echo "art-result: $? athena_0"
 
 get_files -data TIDAdata11-rtt.dat
 get_files -data TIDAdata_cuts.dat
@@ -25,29 +32,29 @@ get_files -data TIDAdata_chains.dat
 get_files -data TIDAbeam.dat
 get_files -data Test_bin.dat
 TIDArdict.exe TIDAdata11-rtt.dat  -f data-all.root -b Test_bin.dat
-JOBSTATUS_1=$?
+echo "art-result: $? TIDArdict_1"
 
 TIDArun-art.sh data-all.root data-beamspot_ttbar_pu80_zfinder-reference.root HLT_beamspot_allTE_trkfast_InDetTrigTrackingxAODCnv_BeamSpot_FTF -d HLTL2-test-plots
-JOBSTATUS_2=$?
+echo "art-result: $? TIDArun_2"
 
 TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -o times
-JOBSTATUS_3=$?
+echo "art-result: $? TIDArun_3"
 
 TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -p FastTrack -o times-FTF
-JOBSTATUS_4=$?
+echo "art-result: $? TIDArun_4"
 
 RunTrigCostD3PD.exe -f trig_cost.root  --outputTagFromAthena --costMode --linkOutputDir
-JOBSTATUS_5=$?
+echo "art-result: $? RunTrigCostD3PD_5"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall --auto -d "/Algorithm" -p "_Time_perCall"
-JOBSTATUS_6=$?
+echo "art-result: $? TIDAcpucost_6"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent --auto -d "/Algorithm" -p "_Time_perEvent"
-JOBSTATUS_7=$?
+echo "art-result: $? TIDAcpucost_7"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall-chain --auto -d "/Chain_Algorithm" -p "_Time_perCall"
-JOBSTATUS_8=$?
+echo "art-result: $? TIDAcpucost_8"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent-chain --auto -d "/Chain_Algorithm" -p "_Time_perEvent"
-JOBSTATUS_9=$?
+echo "art-result: $? TIDAcpucost_9"
 
--- Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_el_zee_ibl_pu40_rzmatcher.sh
+++ Trigger/TrigValidation/TrigInDetValidation/test/test_trigindetvalidation_el_zee_ibl_pu40_rzmatcher.sh
@@ -15,12 +15,19 @@
 # art-output: *.dat 
 # art-output: *.root
 # art-output: *.log
+# art-input-nfiles: 25
+
+
 
 export RTTJOBNAME=TrigInDetValidation_el_Zee_IBL_pu40_rzMatcher
 
+
+fileList="['${ArtInFile//,/', '}']"
+echo "List of files = $fileList"
+
 get_files -jo            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_ElectronSlice.py
-athena.py  -c 'ARTConfig=["/eos/atlas/atlascerngroupdisk/proj-sit/trigindet/mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.recon.RDO.e3601_s2665_s2183_r7191/RDO.06753544._000001.pool.root.1"];           EventMax=5000;runMergedChain=True;PdgId=11'            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_ElectronSlice.py
-JOBSTATUS_0=$?
+athena.py  -c "ARTConfig=$fileList;EventMax=10000;PdgId=11"            TrigInDetValidation/TrigInDetValidation_RTT_topOptions_ElectronSlice.py
+echo "art-result: $? athena_0"
 
 get_files -data TIDAdata11-rtt-rzMatcher.dat
 get_files -data TIDAdata_cuts.dat
@@ -28,38 +35,38 @@ get_files -data TIDAdata_chains.dat
 get_files -data TIDAbeam.dat
 get_files -data Test_bin.dat
 TIDArdict.exe TIDAdata11-rtt-rzMatcher.dat -f data-electron-merge.root -b Test_bin.dat
-JOBSTATUS_1=$?
+echo "art-result: $? TIDArdict_1"
 
 TIDArun-art.sh data-electron-merge.root data-el_Zee_IBL_pu40_rzMatcher-reference.root HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_FTF  HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_IDTrig   -d HLTEF-plots
-JOBSTATUS_2=$?
+echo "art-result: $? TIDArun_2"
 
 TIDArun-art.sh data-electron-merge.root data-el_Zee_IBL_pu40_rzMatcher-reference.root HLT_e24_medium_idperf_InDetTrigTrackingxAODCnv_Electron_FTF -d HLTL2-plots
-JOBSTATUS_3=$?
+echo "art-result: $? TIDArun_3"
 
 TIDArun-art.sh data-electron-merge.root data-el_Zee_IBL_pu40_rzMatcher-reference.root  HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_FTF  HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_IDTrig  -d HLTEF-plots-lowpt
-JOBSTATUS_4=$?
+echo "art-result: $? TIDArun_4"
 
 TIDArun-art.sh data-electron-merge.root data-el_Zee_IBL_pu40_rzMatcher-reference.root HLT_e5_loose_idperf_InDetTrigTrackingxAODCnv_Electron_FTF  -d HLTL2-plots-lowpt
-JOBSTATUS_5=$?
+echo "art-result: $? TIDArun_5"
 
 TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -o times
-JOBSTATUS_6=$?
+echo "art-result: $? TIDArun_6"
 
 TIDArun-art.sh expert-monitoring.root  expert-monitoring*-ref.root --auto -p FastTrack -o times-FTF
-JOBSTATUS_7=$?
+echo "art-result: $? TIDArun_7"
 
 RunTrigCostD3PD.exe -f trig_cost.root  --outputTagFromAthena --costMode --linkOutputDir
-JOBSTATUS_8=$?
+echo "art-result: $? RunTrigCostD3PD_8"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall --auto -d "/Algorithm" -p "_Time_perCall"
-JOBSTATUS_9=$?
+echo "art-result: $? TIDAcpucost_9"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent --auto -d "/Algorithm" -p "_Time_perEvent"
-JOBSTATUS_10=$?
+echo "art-result: $? TIDAcpucost_10"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perCall-chain --auto -d "/Chain_Algorithm" -p "_Time_perCall"
-JOBSTATUS_11=$?
+echo "art-result: $? TIDAcpucost_11"
 
 TIDAcpucost.exe costMon/TrigCostRoot_Results.root costMon/TrigCostRoot_Results.root -o cost-perEvent-chain --auto -d "/Chain_Algorithm" -p "_Time_perEvent"
-JOBSTATUS_12=$?
+echo "art-result: $? TIDAcpucost_12"
 
