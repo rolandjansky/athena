b628e489b5ee865a01aa440c6f89aed75f9b57f0
sherwood <peter.sherwood@cern.ch>
2018-01-25 13:12:02 +0100

    Added new dijet hypo - looks for >= 1 dijets. Accpets cuts on jt Et, et, dijet mass, dEta, dPhi.
    Former-commit-id: c914d92daae7ba0d5dbc59d5241aa38637f7402c

---
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/TrigHLTJetHypo_Dijet.cxx
@@ -0,0 +1,128 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+// ********************************************************************
+//
+// NAME:     TrigHLTJet_Dijet.cxx
+// PACKAGE:  Trigger/TrigHypothesis/TrigHLTJetHypo
+//
+// AUTHOR: P Sherwood
+//
+// ********************************************************************
+
+#include "TrigHLTJetHypo/TrigHLTJetHypo_Dijet.h"
+
+#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/conditionsFactory2.h"
+#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/ConditionsSorter.h"
+
+#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/CombinationsGrouper.h"
+
+
+TrigHLTJetHypo_Dijet::TrigHLTJetHypo_Dijet(const std::string& name,
+			       ISvcLocator* pSvcLocator):
+TrigHLTJetHypoBase(name, pSvcLocator) {
+
+  //Cuts on the individual jets in a pair
+
+  declareProperty("etThresholds1",  m_EtThresholds1);
+  declareProperty("etThresholds2",  m_EtThresholds2);
+
+  declareProperty("eta_mins1", m_etaMins1);
+  declareProperty("eta_maxs1", m_etaMaxs1);
+
+  declareProperty("eta_mins2", m_etaMins2);
+  declareProperty("eta_maxs2", m_etaMaxs2);
+
+  // cuts on the dijet properties.
+
+  declareProperty("mass_mins", m_massMins);
+  declareProperty("mass_maxs", m_massMaxs);
+  
+  declareProperty("deta_mins", m_dEtaMins);
+  declareProperty("deta_maxs", m_dEtaMaxs);
+
+  declareProperty("dphi_mins", m_dPhiMins);
+  declareProperty("dphi_maxs", m_dPhiMaxs);
+
+}
+
+TrigHLTJetHypo_Dijet::~TrigHLTJetHypo_Dijet(){
+}
+
+
+ Conditions TrigHLTJetHypo_Dijet::getConditions() const {
+   
+   auto conditions = conditionsFactoryDijet(m_EtThresholds1,
+                                            m_EtThresholds2,
+                                            m_etaMins1,
+                                            m_etaMaxs1,
+                                            m_etaMins2,
+                                            m_etaMaxs2,
+                                            m_massMins,
+                                            m_massMaxs,
+                                            m_dEtaMins,
+                                            m_dEtaMaxs,
+                                            m_dPhiMins,
+                                            m_dPhiMaxs);
+   
+   std::sort(conditions.begin(), conditions.end(), ConditionsSorter());
+   
+   for(auto& c : conditions){ATH_MSG_DEBUG(c.toString());} 
+
+   return conditions;
+ }
+
+
+ 
+std::shared_ptr<IJetGrouper> TrigHLTJetHypo_Dijet::getJetGrouper()  const{
+
+  return std::make_shared<CombinationsGrouper>(2);
+}
+
+
+bool TrigHLTJetHypo_Dijet::checkVals() const {
+
+  std::size_t n_dijets = m_EtThresholds1.size();
+  if (n_dijets  != m_EtThresholds2.size() or
+      n_dijets  != m_etaMins1.size() or
+      n_dijets  != m_etaMaxs1.size() or
+      n_dijets  != m_etaMins2.size() or
+      n_dijets  != m_etaMaxs2.size() or
+      n_dijets  != m_massMins.size() or
+      n_dijets  != m_massMaxs.size() or
+      n_dijets  != m_dEtaMins.size() or
+      n_dijets  != m_dEtaMaxs.size() or
+      n_dijets  != m_dPhiMins.size() or
+      n_dijets  != m_dPhiMaxs.size()
+      ){
+    ATH_MSG_ERROR(name()
+                  << "dijets configuration inconsistent for " <<n_dijets
+                  << " dijets "
+                  << m_etaMins1.size() << " "
+                  << m_etaMaxs1.size() << " "
+                  << m_etaMins2.size() << " "
+                  << m_etaMaxs2.size() << " "
+                  << m_massMins.size() << " "
+                  << m_massMaxs.size() << " "
+                  << m_dEtaMins.size() << " "
+                  << m_dEtaMaxs.size() << " "
+                  << m_dPhiMins.size() << " "
+                  << m_dPhiMaxs.size()
+                  );
+    
+    return false;
+  }
+
+  bool multOK = n_dijets > 0;
+  
+  if (not multOK){ATH_MSG_ERROR(name() << "no dijet cuts specified ");}
+  
+  return multOK;
+}
+
+
+std::vector<std::shared_ptr<ICleaner>> TrigHLTJetHypo_Dijet::getCleaners () const {
+  std::vector<std::shared_ptr<ICleaner>> v;
+  return v;
+}
--- Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
@@ -3,6 +3,7 @@
 #include "TrigHLTJetHypo/TrigHLTJetHypo2.h"
 #include "TrigHLTJetHypo/TrigEFRazorAllTE.h"
 #include "TrigHLTJetHypo/TrigEFDPhiMetJetAllTE.h"
+#include "TrigHLTJetHypo/TrigHLTJetHypo_Dijet.h"
 #include "TrigHLTJetHypo/TrigHLTJetHypo_DijetMassDEta.h"
 #include "TrigHLTJetHypo/TrigHLTJetHypo_DijetMassDEtaDPhi.h"
 #include "TrigHLTJetHypo/TrigHLTJetHypo_EtaEt.h"
@@ -14,6 +15,7 @@ DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo2)
 
 DECLARE_ALGORITHM_FACTORY(TrigEFRazorAllTE)
 DECLARE_ALGORITHM_FACTORY(TrigEFDPhiMetJetAllTE)
+DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo_Dijet)
 DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo_DijetMassDEta)
 DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo_DijetMassDEtaDPhi)
 DECLARE_ALGORITHM_FACTORY(TrigHLTJetHypo_EtaEt)
@@ -26,6 +28,7 @@ DECLARE_FACTORY_ENTRIES(TrigHLTJetHypo) {
     DECLARE_ALGORITHM(TrigEFRazorAllTE)
     DECLARE_ALGORITHM(TrigEFDPhiMetJetAllTE)
 
+    DECLARE_ALGORITHM(TrigHLTJetHypo_Dijet)
     DECLARE_ALGORITHM(TrigHLTJetHypo_DijetMassDEta)
     DECLARE_ALGORITHM(TrigHLTJetHypo_DijetMassDEtaDPhi)
     DECLARE_ALGORITHM(TrigHLTJetHypo_EtaEt)
