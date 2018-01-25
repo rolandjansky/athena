2030a7082fd8f017bf1e7cc89a97a83a72c76f5c
Steve Farrell <sfarrell@cern.ch>
2018-01-25 22:41:49 +0100

    VerboseSelector: whitespace cleanup
    Former-commit-id: ccc9f84b6be46434cb9c362cac6c1d9e424bcff3

--- Simulation/G4Utilities/G4DebuggingTools/src/VerboseSelector.cxx
+++ Simulation/G4Utilities/G4DebuggingTools/src/VerboseSelector.cxx
@@ -54,61 +54,61 @@ namespace G4UA
 
       const G4ThreeVector& myPos = aStep->GetPostStepPoint()->GetPosition();
       if ( ( myPos.x() < m_config.Xmax && myPos.x() > m_config.Xmin &&
-	     myPos.y() < m_config.Ymax && myPos.y() > m_config.Ymin &&
-	     myPos.z() < m_config.Zmax && myPos.z() > m_config.Zmin ) ||
-	   m_config.verb==2){
-	
-	G4TransportationManager *tm = G4TransportationManager::GetTransportationManager();
-	tm->GetNavigatorForTracking()->SetVerboseLevel(m_config.verboseLevel);
-	tm->GetPropagatorInField()->SetVerboseLevel(m_config.verboseLevel);
-	
-	G4RunManagerKernel *rmk = G4RunManagerKernel::GetRunManagerKernel();
-	rmk->GetTrackingManager()->SetVerboseLevel(m_config.verboseLevel);
-	rmk->GetTrackingManager()->GetSteppingManager()->SetVerboseLevel(m_config.verboseLevel);
-	rmk->GetStackManager()->SetVerboseLevel(m_config.verboseLevel);
-	
-	G4Track *tr = aStep->GetTrack();
-       const G4ThreeVector& mom = tr->GetMomentumDirection();
-	
-	std::cout << "Moving " << tr->GetDefinition()->GetParticleName() << " at (" << myPos.x()
-		  << ", " << myPos.y() << ", " << myPos.z() << ") to (" << mom.x() << ", " << mom.y()
-		  << ", " << mom.z() << ") from ";
-	if (aStep->GetPreStepPoint()->GetPhysicalVolume()) std::cout << aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName();
-	else std::cout << "noVolume";
-	std::cout << " to ";
-	if (aStep->GetPostStepPoint()->GetPhysicalVolume()) std::cout << aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName();
-	else std::cout << "outOfWorld";
-	std::cout << " with KE=" << tr->GetKineticEnergy() << " pT=" << tr->GetMomentum().perp()
-		  << " eta=" << tr->GetMomentum().eta() << " length " << aStep->GetStepLength() << " energy "
-		  << aStep->GetTotalEnergyDeposit() << " with process ";
-	if (aStep->GetPostStepPoint()->GetProcessDefinedStep()) std::cout << aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessName();
-	else std::cout << "Unknown";
-	std::cout << " from a ";
-	if (tr->GetCreatorProcess()) std::cout << tr->GetCreatorProcess()->GetProcessName();
-	else if (tr->GetParentID()==0) std::cout << "Generator";
-	else std::cout << "pid="<<tr->GetParentID();
-	if (tr->GetLogicalVolumeAtVertex()) std::cout << " in " << tr->GetLogicalVolumeAtVertex()->GetName() << ", ";
-	else std::cout << " nowhere, ";
-	std::cout << tr->GetTrackLength() << " mm ago" << std::endl;
-	
-	if (m_config.verb==1) m_config.verb=2;
-	
+             myPos.y() < m_config.Ymax && myPos.y() > m_config.Ymin &&
+             myPos.z() < m_config.Zmax && myPos.z() > m_config.Zmin ) ||
+           m_config.verb==2){
+
+        G4TransportationManager *tm = G4TransportationManager::GetTransportationManager();
+        tm->GetNavigatorForTracking()->SetVerboseLevel(m_config.verboseLevel);
+        tm->GetPropagatorInField()->SetVerboseLevel(m_config.verboseLevel);
+
+        G4RunManagerKernel *rmk = G4RunManagerKernel::GetRunManagerKernel();
+        rmk->GetTrackingManager()->SetVerboseLevel(m_config.verboseLevel);
+        rmk->GetTrackingManager()->GetSteppingManager()->SetVerboseLevel(m_config.verboseLevel);
+        rmk->GetStackManager()->SetVerboseLevel(m_config.verboseLevel);
+
+        G4Track *tr = aStep->GetTrack();
+        const G4ThreeVector& mom = tr->GetMomentumDirection();
+
+        std::cout << "Moving " << tr->GetDefinition()->GetParticleName() << " at (" << myPos.x()
+                  << ", " << myPos.y() << ", " << myPos.z() << ") to (" << mom.x() << ", " << mom.y()
+                  << ", " << mom.z() << ") from ";
+        if (aStep->GetPreStepPoint()->GetPhysicalVolume()) std::cout << aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName();
+        else std::cout << "noVolume";
+        std::cout << " to ";
+        if (aStep->GetPostStepPoint()->GetPhysicalVolume()) std::cout << aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName();
+        else std::cout << "outOfWorld";
+        std::cout << " with KE=" << tr->GetKineticEnergy() << " pT=" << tr->GetMomentum().perp()
+                  << " eta=" << tr->GetMomentum().eta() << " length " << aStep->GetStepLength() << " energy "
+                  << aStep->GetTotalEnergyDeposit() << " with process ";
+        if (aStep->GetPostStepPoint()->GetProcessDefinedStep()) std::cout << aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessName();
+        else std::cout << "Unknown";
+        std::cout << " from a ";
+        if (tr->GetCreatorProcess()) std::cout << tr->GetCreatorProcess()->GetProcessName();
+        else if (tr->GetParentID()==0) std::cout << "Generator";
+        else std::cout << "pid="<<tr->GetParentID();
+        if (tr->GetLogicalVolumeAtVertex()) std::cout << " in " << tr->GetLogicalVolumeAtVertex()->GetName() << ", ";
+        else std::cout << " nowhere, ";
+        std::cout << tr->GetTrackLength() << " mm ago" << std::endl;
+
+        if (m_config.verb==1) m_config.verb=2;
+
       }
     }
-
   }
 
   void VerboseSelector::PreUserTrackingAction(const G4Track* aTrack)
   {
     if(m_evtCount==(uint64_t)m_config.targetEvent||m_config.targetEvent<0)
       {
+        int trackID = aTrack->GetTrackID();
+        // FIXME: can we remove this const_cast?
+        G4Track *itr = const_cast<G4Track*>(aTrack);
+        TrackHelper trackHelper(itr);
 
-	int trackID = aTrack->GetTrackID();
-	G4Track *itr=const_cast<G4Track*>(aTrack);
-	TrackHelper trackHelper(itr);
-
-	EventInformation* eventInfo=static_cast<EventInformation*>
-    (G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetUserInformation());
+        EventInformation* eventInfo = static_cast<EventInformation*>
+          (G4EventManager::GetEventManager()->GetConstCurrentEvent()->
+           GetUserInformation());
 
         int currentBarcode(0);
 
@@ -119,25 +119,25 @@ namespace G4UA
           currentBarcode = eventInfo->GetCurrentlyTraced()->barcode();
         }
 
-	bool p1=m_config.targetTrack<0 && m_config.targetBarcode<0;
-	bool p2=trackID==m_config.targetTrack ;
-	bool p3=currentBarcode==m_config.targetBarcode;
-	
+        bool p1 = m_config.targetTrack<0 && m_config.targetBarcode<0;
+        bool p2 = trackID==m_config.targetTrack;
+        bool p3 = currentBarcode==m_config.targetBarcode;
 
         if(p1 || p2 || p3) {
-          ATH_MSG_INFO(std::endl<<"---------> Dumping now track #"<<trackID<<" barcode "
-                       <<currentBarcode<<" in event "<<m_evtCount<<std::endl);
-          G4EventManager::GetEventManager()->GetTrackingManager()->SetVerboseLevel(m_config.verboseLevel);
+          ATH_MSG_INFO(std::endl << "---------> Dumping now track #"
+                       << trackID << " barcode " << currentBarcode
+                       << " in event " << m_evtCount);
+          G4EventManager::GetEventManager()->GetTrackingManager()->
+            SetVerboseLevel(m_config.verboseLevel);
         }
       }
-
   }
 
   void VerboseSelector::PostUserTrackingAction(const G4Track* aTrack)
   {
     if(m_evtCount==(uint64_t)m_config.targetEvent||m_config.targetEvent<0){
       if(aTrack->GetTrackID()==m_config.targetTrack||m_config.targetTrack<0)
-	G4EventManager::GetEventManager()->GetTrackingManager()->SetVerboseLevel(0);
+        G4EventManager::GetEventManager()->GetTrackingManager()->SetVerboseLevel(0);
     }
   }
 
