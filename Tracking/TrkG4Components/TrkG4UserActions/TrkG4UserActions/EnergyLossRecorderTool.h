6b25d8b5ead0cfa5832afd720371194b23e89ddf
Steve Farrell <sfarrell@cern.ch>
2018-01-28 00:30:25 +0100

    Migrate TrkG4UserActions to new tool base.
    Migrated EnergyLossRecorderTool, GeantFollowerTool, GeantFollowerMSTool,
    and MaterialStepRecorderTool to the new UserActionToolBase (V3 user
    actions).
    
    
    Former-commit-id: 5cf9cbf5bbcf8cb420812bbda32303b395eed498

--- Tracking/TrkG4Components/TrkG4UserActions/TrkG4UserActions/EnergyLossRecorderTool.h
+++ Tracking/TrkG4Components/TrkG4UserActions/TrkG4UserActions/EnergyLossRecorderTool.h
@@ -8,7 +8,7 @@
 #include "G4AtlasInterfaces/IG4RunActionTool.h"
 #include "G4AtlasInterfaces/IG4EventActionTool.h"
 #include "G4AtlasInterfaces/IG4SteppingActionTool.h"
-#include "G4AtlasTools/ActionToolBase.h"
+#include "G4AtlasTools/UserActionToolBase.h"
 #include "TrkG4UserActions/EnergyLossRecorder.h"
 
 namespace Trk
@@ -26,31 +26,19 @@ namespace G4UA
   ///
   /// @author Andrea Di Simone
 
-  class EnergyLossRecorderTool:
-
-  public ActionToolBase<EnergyLossRecorder>,
-    public IG4RunActionTool, public IG4EventActionTool,  public IG4SteppingActionTool
+  class EnergyLossRecorderTool : public UserActionToolBase<EnergyLossRecorder>
   {
 
   public:
     /// standard tool ctor
-    EnergyLossRecorderTool(const std::string& type, const std::string& name,const IInterface* parent);
+    EnergyLossRecorderTool(const std::string& type, const std::string& name,
+                           const IInterface* parent);
     /// Athena initialize method
     virtual StatusCode initialize() override final;
-    /// gets the run action
-    virtual G4UserRunAction* getRunAction() override final
-    { return static_cast<G4UserRunAction*>( getAction() ); }
-    /// gets the event action
-    virtual G4UserEventAction* getEventAction() override final
-    { return static_cast<G4UserEventAction*>( getAction() ); }
-    /// gets the stepping action
-    virtual G4UserSteppingAction* getSteppingAction() override final
-    { return static_cast<G4UserSteppingAction*>( getAction() ); }
-    /// Gaudi interface manipulation
-    virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface) override;
   protected:
     /// creates the instance for this thread
-    virtual std::unique_ptr<EnergyLossRecorder> makeAction() override final;
+    virtual std::unique_ptr<EnergyLossRecorder>
+    makeAndFillAction(G4AtlasUserActions&) override final;
   private:
     /// stores the config obtained from the python side
     EnergyLossRecorder::Config m_config;
