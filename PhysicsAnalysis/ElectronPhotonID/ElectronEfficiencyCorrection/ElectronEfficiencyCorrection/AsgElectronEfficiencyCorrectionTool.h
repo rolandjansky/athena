41f545f8a86f1f0cb071c83c9e77688328bb5e1a
christos@cern.ch <your.name@cern.ch>
2018-01-30 18:16:58 +0000

    Some minor aesthetic changes. This should be it for this MR except for comments
    Former-commit-id: b50b81a28eea8e388f5af9537e3fd96c74ce3e35

--- PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/ElectronEfficiencyCorrection/AsgElectronEfficiencyCorrectionTool.h
+++ PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/ElectronEfficiencyCorrection/AsgElectronEfficiencyCorrectionTool.h
@@ -1,214 +1,220 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-// Dear emacs, this is -*-c++-*-
+   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+ */
 
 #ifndef __ASGELECTRONEFFICIENCYCORRECTIONTOOL__
 #define __ASGELECTRONEFFICIENCYCORRECTIONTOOL__
 
 /**
-   @class AthElectronEfficiencyCorrectionTool
-   @brief Calculate the egamma scale factors in Athena
+  @class AthElectronEfficiencyCorrectionTool
+  @brief Calculate the egamma scale factors in Athena
 
-   @author Kristin Lohwasser , Rob Roy Fletcher, Karsten Koeneke
-   @date   May 2014
-*/
+  @author Kristin Lohwasser , Rob Roy Fletcher, Karsten Koeneke
+  @date   May 2014
+  */
+
+//Fwd Declarations
 class TH2F;
+namespace Root{
+    class TElectronEfficiencyCorrectionTool;
+}
+#include "xAODEgamma/ElectronFwd.h"
+//Interface
 #include "EgammaAnalysisInterfaces/IAsgElectronEfficiencyCorrectionTool.h"
 // STL includes
 #include <vector>
 #include <string>
-#include <iterator>
 #include <map>
 #include <unordered_map>
 //PAT includes
 #include "AsgTools/AsgMetadataTool.h"
-#include "PATInterfaces/ISystematicsTool.h"
 #include "PATInterfaces/SystematicRegistry.h"
 #include "PATInterfaces/CorrectionCode.h"
-// Include the underlying ROOT tool
-#include "ElectronEfficiencyCorrection/TElectronEfficiencyCorrectionTool.h"
-//xAOD includes
-#include "xAODEgamma/ElectronFwd.h"
+#include "PATCore/PATCoreEnums.h"
 
 class AsgElectronEfficiencyCorrectionTool
-  : virtual public IAsgElectronEfficiencyCorrectionTool, public asg::AsgMetadataTool
+: virtual public IAsgElectronEfficiencyCorrectionTool, public asg::AsgMetadataTool
 {
-  ASG_TOOL_CLASS(AsgElectronEfficiencyCorrectionTool, IAsgElectronEfficiencyCorrectionTool)
+    ASG_TOOL_CLASS(AsgElectronEfficiencyCorrectionTool, IAsgElectronEfficiencyCorrectionTool)
 
-  public:
-  /// Standard constructor
-  AsgElectronEfficiencyCorrectionTool ( const std::string myname );
+public:
+        /// Standard constructor
+        AsgElectronEfficiencyCorrectionTool ( const std::string myname );
 
-  /// Standard destructor
-  virtual ~AsgElectronEfficiencyCorrectionTool();
+        /// Standard destructor
+        virtual ~AsgElectronEfficiencyCorrectionTool();
 
 
 public:
-  /// Gaudi Service Interface method implementations
-  virtual StatusCode initialize();
+        /// Gaudi Service Interface method implementations
+        virtual StatusCode initialize();
 
-  /// Gaudi Service Interface method implementations
-  virtual StatusCode finalize();
+        /// Gaudi Service Interface method implementations
+        virtual StatusCode finalize();
 
-  // Introducing to check if  METADATA working
-  virtual StatusCode beginInputFile();    
-  virtual StatusCode beginEvent();
-  virtual StatusCode endInputFile();    
- 
-  // Main methods from IUserDataCalcTool
+        // Introducing to check if  METADATA working
+        virtual StatusCode beginInputFile();    
+        virtual StatusCode beginEvent();
+        virtual StatusCode endInputFile();    
+
+        // Main methods from IUserDataCalcTool
 public:
 
-  int getNumberOfToys( ){return m_number_of_toys;} ;
+        int getNumberOfToys( ){return m_number_of_toys;} ;
 
 
-  CP::CorrectionCode getEfficiencyScaleFactor(const xAOD::Electron& inputObject, double& efficiencyScaleFactor) const;
-  CP::CorrectionCode applyEfficiencyScaleFactor(const xAOD::Electron& inputObject) const;
+        CP::CorrectionCode getEfficiencyScaleFactor(const xAOD::Electron& inputObject, double& efficiencyScaleFactor) const;
+        CP::CorrectionCode applyEfficiencyScaleFactor(const xAOD::Electron& inputObject) const;
 
-  /// print available/implemented correlation models
-  void printCorrelationModels() {
-    ATH_MSG_INFO(" Available Correlation Models for the ElectronEfficiencyCorrectionTool");
-    ATH_MSG_INFO("FULL");
-    ATH_MSG_INFO("SIMPLIFIED");
-    ATH_MSG_INFO("MCTOYS");
-    ATH_MSG_INFO("SYST");
-    ATH_MSG_INFO("COMBMCTOYS");
-    ATH_MSG_INFO("TOTAL");
-  } ;
+        /// print available/implemented correlation models
+        void printCorrelationModels() {
+            ATH_MSG_INFO(" Available Correlation Models for the ElectronEfficiencyCorrectionTool");
+            ATH_MSG_INFO("FULL");
+            ATH_MSG_INFO("SIMPLIFIED");
+            ATH_MSG_INFO("MCTOYS");
+            ATH_MSG_INFO("SYST");
+            ATH_MSG_INFO("COMBMCTOYS");
+            ATH_MSG_INFO("TOTAL");
+        } ;
 
- /// returns: whether this tool is affected by the given systematis
-  virtual bool isAffectedBySystematic( const CP::SystematicVariation& systematic ) const ;
-  
-  /// returns: the list of all systematics this tool can be affected by
-  virtual CP::SystematicSet affectingSystematics() const ;
-  
-  /// returns: the list of all systematics this tool recommends to use
-  virtual CP::SystematicSet recommendedSystematics() const ;
+        /// returns: whether this tool is affected by the given systematis
+        virtual bool isAffectedBySystematic( const CP::SystematicVariation& systematic ) const ;
 
-  /// returns: the currently applied systematics
-  const CP::SystematicSet& appliedSystematics() const {
-    return *m_appliedSystematics;
-  }
+        /// returns: the list of all systematics this tool can be affected by
+        virtual CP::SystematicSet affectingSystematics() const ;
 
-  /// Configure this tool for the given systematics
-  virtual CP::SystematicCode applySystematicVariation ( const CP::SystematicSet& systConfig ) ;
+        /// returns: the list of all systematics this tool recommends to use
+        virtual CP::SystematicSet recommendedSystematics() const ;
 
-  CP::SystematicCode registerSystematics();
+        /// returns: the currently applied systematics
+        const CP::SystematicSet& appliedSystematics() const {
+            return *m_appliedSystematics;
+        }
 
-  int systUncorrVariationIndex( const xAOD::Electron &inputObject) const;
+        /// Configure this tool for the given systematics
+        virtual CP::SystematicCode applySystematicVariation ( const CP::SystematicSet& systConfig ) ;
 
-  // Private member variables
+        CP::SystematicCode registerSystematics();
+
+        int systUncorrVariationIndex( const xAOD::Electron &inputObject) const;
+
+        // Private member variables
 private:
-  // To check if the metadat can be retrieved 
-  bool m_metadata_retrieved = false;
+        // To check if the metadat can be retrieved 
+        bool m_metadata_retrieved = false;
 
-  // Get the simulation type from metadata
-  StatusCode get_simType_from_metadata(PATCore::ParticleDataType::DataType& result) const;
+        // Get the simulation type from metadata
+        StatusCode get_simType_from_metadata(PATCore::ParticleDataType::DataType& result) const;
 
-  int currentSimplifiedUncorrSystRegion(const double cluster_eta, const double et) const ;
-  int currentUncorrSystRegion(const double cluster_eta, const double et) const ;
+        int currentSimplifiedUncorrSystRegion(const double cluster_eta, const double et) const ;
+        int currentUncorrSystRegion(const double cluster_eta, const double et) const ;
 
 
-  /// The main calculate method: the actual correction factors are determined here
-  CP::SystematicCode InitSystematics();
+        /// The main calculate method: the actual correction factors are determined here
+        CP::SystematicCode InitSystematics();
 
-  // struct for toys
-  struct SystConf{
-    float m_total;
-    unsigned m_toy_index;
-    float m_toy_scale;
-  };
+        // struct for toys
+        struct SystConf{
+            float m_total;
+            unsigned m_toy_index;
+            float m_toy_scale;
+        };
 
-  // Gets the correction filename from map  
-  virtual StatusCode getFile(const std::string& recokey, const std::string& idkey, const std::string& isokey, const std::string& trigkey);
+        // Gets the correction filename from map  
+        virtual StatusCode getFile(const std::string& recokey, 
+                const std::string& idkey, 
+                const std::string& isokey, 
+                const std::string& trigkey);
 
-  // Convert reco, ID, iso and trigger key values into a 
-  // single key according to the map file key format
-  std::string convertToOneKey(const std::string& recokey, const std::string& idkey, const std::string& isokey, const std::string& trigkey) const;
+        // Convert reco, ID, iso and trigger key values into a 
+        // single key according to the map file key format
+        std::string convertToOneKey(const std::string& recokey, 
+                const std::string& idkey, 
+                const std::string& isokey, 
+                const std::string& trigkey) const;
 
-  // Retrieves the value from the provided map file as 
-  // associated with the provided key
-  std::string getValueByKey(const std::string& mapFile, const std::string& key);
+        // Retrieves the value from the provided map file as 
+        // associated with the provided key
+        std::string getValueByKey(const std::string& mapFile, const std::string& key);
 
-  // Reads the provided map file
-  // and construct the map
-  StatusCode read (const std::string& strFile);
-      
-  // Retrieves the value from the provided map file if 
-  // even the provided key is found. If the key has an 
-  // association then, the actual retrieved value would
-  // be assigned to the 2nd argument of this method 
-  std::string getValue(const std::string& strKey, std::string& strValue);  
+        // Reads the provided map file
+        // and construct the map
+        StatusCode read (const std::string& strFile);
 
-  /// Pointer to the underlying ROOT based tool
-  Root::TElectronEfficiencyCorrectionTool* m_rootTool;
+        // Retrieves the value from the provided map file if 
+        // even the provided key is found. If the key has an 
+        // association then, the actual retrieved value would
+        // be assigned to the 2nd argument of this method 
+        std::string getValue(const std::string& strKey, std::string& strValue);  
 
-  /// Systematics filter map
-  std::unordered_map<CP::SystematicSet, CP::SystematicSet> m_systFilter;
+        /// Pointer to the underlying ROOT based tool
+        Root::TElectronEfficiencyCorrectionTool* m_rootTool;
 
-  ///Affected systematic, should be done only once
-  CP::SystematicSet m_affectedSys;
+        /// Systematics filter map
+        std::unordered_map<CP::SystematicSet, CP::SystematicSet> m_systFilter;
 
-  /// Currently applied systematics
-  CP::SystematicSet* m_appliedSystematics;
+        ///Affected systematic, should be done only once
+        CP::SystematicSet m_affectedSys;
 
-  // Correlation Model
-  std::string m_correlation_model_name;
-  int  m_correlation_model;
+        /// Currently applied systematics
+        CP::SystematicSet* m_appliedSystematics;
 
-  // Map-key properties
-  std::map<std::string,std::string> m_map;
-  std::string  m_mapFile;
-  
-  //Four SF key Properties 
-  std::string  m_recoKey;
-  std::string  m_idKey;
-  std::string  m_isoKey;
-  std::string  m_trigKey;
-  
-  /// The list of file names
-  std::vector< std::string > m_corrFileNameList;
+        // Correlation Model
+        std::string m_correlation_model_name;
+        int  m_correlation_model;
 
-  /// The prefix string for the result
-  std::string m_resultPrefix;
+        // Map-key properties
+        std::map<std::string,std::string> m_map;
+        std::string  m_mapFile;
 
-  /// The string for the result
-  std::string m_resultName;
+        //Four SF key Properties 
+        std::string  m_recoKey;
+        std::string  m_idKey;
+        std::string  m_isoKey;
+        std::string  m_trigKey;
 
-  // The prefix for the systematic name
-  std::string m_sysSubstring;
+        /// The list of file names
+        std::vector< std::string > m_corrFileNameList;
 
-  /// The Event info collection name
-  std::string m_eventInfoCollectionName;
+        /// The prefix string for the result
+        std::string m_resultPrefix;
 
-  //use RandomRun Number
-  bool m_useRandomRunNumber;
-  int m_defaultRandomRunNumber;
+        /// The string for the result
+        std::string m_resultName;
 
-  //The data type
-  PATCore::ParticleDataType::DataType m_dataType;
+        // The prefix for the systematic name
+        std::string m_sysSubstring;
 
-  /// Force the data type to a given value
-  int m_dataTypeOverwrite;
+        /// The Event info collection name
+        std::string m_eventInfoCollectionName;
 
-  // Number of toys:
-  int m_number_of_toys;
+        //use RandomRun Number
+        bool m_useRandomRunNumber;
+        int m_defaultRandomRunNumber;
 
-  // seed for toys:
-  int m_seed_toys;
-    
-  // scale for toy uncertainty:
-  double m_scale_toys;
+        //The data type
+        PATCore::ParticleDataType::DataType m_dataType;
 
-  // number if systematics uncertainty    
-  int m_nCorrSyst;
-  int m_nUncorrSyst ;
-  std::map<float, std::vector<float> > m_pteta_bins;
-  
-  // simplified uncorrelation regions
-  TH2F * m_UncorrRegions;
-  int m_nSimpleUncorrSyst;
+        /// Force the data type to a given value
+        int m_dataTypeOverwrite;
+
+        // Number of toys:
+        int m_number_of_toys;
+
+        // seed for toys:
+        int m_seed_toys;
+
+        // scale for toy uncertainty:
+        double m_scale_toys;
+
+        // number if systematics uncertainty    
+        int m_nCorrSyst;
+        int m_nUncorrSyst ;
+        std::map<float, std::vector<float> > m_pteta_bins;
+
+        // simplified uncorrelation regions
+        TH2F * m_UncorrRegions;
+        int m_nSimpleUncorrSyst;
 
 }; // End: class definition
 
