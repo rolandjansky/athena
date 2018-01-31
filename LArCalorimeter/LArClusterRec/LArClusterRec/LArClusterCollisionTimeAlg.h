cfe64f57fd294e7d1cfa941b97dfd5f80d315bc5
Pavol Strizenec <pavol.strizenec@cern.ch>
2018-01-31 17:46:39 +0100

    virtual fix
    Former-commit-id: c914f3e4b03b65735bf17757a538fe28622e94d1

--- LArCalorimeter/LArClusterRec/LArClusterRec/LArClusterCollisionTimeAlg.h
+++ LArCalorimeter/LArClusterRec/LArClusterRec/LArClusterCollisionTimeAlg.h
@@ -19,7 +19,7 @@
 class LArClusterCollisionTimeAlg : public AthAlgorithm {
  public:
   LArClusterCollisionTimeAlg(const std::string& name, ISvcLocator* pSvcLocator);
-  ~LArClusterCollisionTimeAlg();
+  virtual ~LArClusterCollisionTimeAlg();
     
   /** standard Athena-Algorithm method */
   StatusCode          initialize() override final;
