51653f65a99b7b38b2515772110e74756a03229d
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2018-02-09 13:29:29 +0100

    Commit all changes
    Former-commit-id: f1b211b31b50bdcc382cb00b30e033420f4e1d94

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/share/CommonToolSetup.py
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/share/CommonToolSetup.py
@@ -83,7 +83,7 @@ def GetPRWTool(
         # this default is for MC16a -> data2016
         PRWLumiCalcFiles = [
             "/afs/cern.ch/atlas/project/muon/mcp/PRWFiles/ilumicalc_histograms_OflLumi-13TeV-009_data15_13TeV.periodAllYear_DetStatus-v89-pro21-02_Unknown_PHYS_StandardGRL_All_Good_25ns.root",
-            "/afs/cern.ch/atlas/project/muon/mcp/PRWFiles/ilumicalc_histograms_OflLumi-13TeV-009_data16_13TeV.periodAllYear_DetStatus-v89-pro21-01_DQDefects-00-02-04_PHYS_StandardGRL_All_Good_25ns.root",
+#            "/afs/cern.ch/atlas/project/muon/mcp/PRWFiles/ilumicalc_histograms_OflLumi-13TeV-009_data16_13TeV.periodAllYear_DetStatus-v89-pro21-01_DQDefects-00-02-04_PHYS_StandardGRL_All_Good_25ns.root",
        	],
         PRWMCConfigFiles = [
             "/afs/cern.ch/atlas/project/muon/mcp/PRWFiles/prwConfigFiles/mc16_FULLSIM_r9364_r9315_NTUP_PILEUP.root",
