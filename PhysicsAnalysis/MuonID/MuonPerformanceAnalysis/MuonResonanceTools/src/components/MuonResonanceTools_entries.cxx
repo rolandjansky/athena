04eba3ef3566731124cf98c6af439684526b1686
Nicolas Köhler <nicolas.koehler@cern.ch>
2018-02-09 21:53:25 +0100

    use MuonResonanceTools version from master
    Former-commit-id: d0254b4364ec6084f9c508f4b117ee91b011ce1a

--- PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonResonanceTools/src/components/MuonResonanceTools_entries.cxx
+++ PhysicsAnalysis/MuonID/MuonPerformanceAnalysis/MuonResonanceTools/src/components/MuonResonanceTools_entries.cxx
@@ -1,21 +1,12 @@
 // AsgExampleTools_entries.cxx
 
-#include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "MuonResonanceTools/MuonResonanceTool.h"
 #include "MuonResonanceTools/MuonResonanceSelectionTool.h"
 #include "MuonResonanceTools/MuonResonancePairingTool.h"
 
 
-DECLARE_TOOL_FACTORY(MuonResonanceTool)
-DECLARE_TOOL_FACTORY(MuonResonanceSelectionTool)
-DECLARE_TOOL_FACTORY(MuonResonancePairingTool)
-
-
-DECLARE_FACTORY_ENTRIES(MuonPerformanceTools) {
-
-  DECLARE_TOOL(MuonResonanceTool)
-  DECLARE_TOOL(MuonResonanceSelectionTool)
-  DECLARE_TOOL(MuonResonancePairingTool)
-}
+DECLARE_COMPONENT( MuonResonanceTool )
+DECLARE_COMPONENT( MuonResonanceSelectionTool )
+DECLARE_COMPONENT( MuonResonancePairingTool )
 
