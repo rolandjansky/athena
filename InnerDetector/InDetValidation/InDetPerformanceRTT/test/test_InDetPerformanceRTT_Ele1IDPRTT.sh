74ef0b50f22b05128195717713a9d379e1a5bcbe
Joshua Moss <jmoss@lxplus085.cern.ch>
2018-02-10 01:55:00 +0100

    Updated the ART test scripts to include the correct output.  Also added post-processing:  rootcomp.py to do comparison with the appropriate reference histograms.
    Former-commit-id: 54c6b61401796b79cbec1cb1b01cc1734b22da20

--- InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_Ele1IDPRTT.sh
+++ InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_Ele1IDPRTT.sh
@@ -3,6 +3,11 @@
 # art-type: grid
 # art-input: mc15_13TeV.422007.ParticleGun_single_ele_Pt1.recon.RDO.e4459_s2726_r7059_tid07275503_00
 # art-input-nfiles: 10
+# art-output: InDetStandardPlots.root
+# art-output: comparison.root
+# art-output: comparison.ps
+# art-output: *.root
+# art-output: *.xml
 
 fileList="['${ArtInFile//,/', '}']"
 echo "List of files = ", $fileList
@@ -10,3 +15,6 @@ echo "List of files = ", $fileList
 joboptions="InDetPerformanceRTT/InDetPerformanceRTT_jobOptions.py"
 get_files -jo $joboptions
 athena.py  -c "from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; athenaCommonFlags.FilesInput=$fileList; EventMax=20000;" $joboptions
+
+refhist="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/InDetPerformanceRTT/ReferenceHistograms/InDetStandardPlots-SingleEle1000.root"
+rootcomp.py -o comparison -c $refhist InDetStandardPlots.root
--- InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_ttbar.sh
+++ InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_ttbar.sh
@@ -3,6 +3,11 @@
 # art-type: grid
 # art-input:  mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.recon.RDO.e3698_s2608_s2183_r7728_tid08543088_00
 # art-input-nfiles: 1
+# art-output: InDetStandardPlots.root
+# art-output: comparison.root
+# art-output: comparison.ps
+# art-output: *.root
+# art-output: *.xml
 
 fileList="['${ArtInFile//,/', '}']"
 echo "List of files = ", $fileList
@@ -10,3 +15,6 @@ echo "List of files = ", $fileList
 
 get_files -jo InDetPerformanceRTT/InDetPerformanceRTT_jobOptions_For20.7Digi.py
 athena.py  -c "from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; athenaCommonFlags.FilesInput=$fileList; EventMax=500;" InDetPerformanceRTT/InDetPerformanceRTT_jobOptions_For20.7Digi.py
+
+refhist="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/InDetPerformanceRTT/ReferenceHistograms/InDetStandardPlots-SingleEle100000.root"
+rootcomp.py -o comparison -c $refhist InDetStandardPlots.root
