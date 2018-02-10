ccb92504270b54503b5fdcd5f3753d00051ef14b
Jiří Mašík <Jiri.Masik@manchester.ac.uk>
2018-02-10 12:33:38 +0100

    fix writing of the TrigGSFTrackParticles (missing Aux container)
    Former-commit-id: 701387d648141ed411e2fe0439e79d63ffc51e97

--- Trigger/TriggerCommon/TrigEDMConfig/python/TriggerEDM.py
+++ Trigger/TriggerCommon/TrigEDMConfig/python/TriggerEDM.py
@@ -220,7 +220,8 @@ TriggerHLTList = [
 
     ('xAOD::TrigCompositeContainer#HLT_TrigEFDielectronMassFex',                   'BS ESD AODFULL AODSLIM',  'Egamma'),
     ('xAOD::TrigCompositeAuxContainer#HLT_TrigEFDielectronMassFexAux.',            'BS ESD AODFULL AODSLIM',  'Egamma'),
-    ('xAOD::TrackParticleContainer#HLT_TrigGSFTrackParticles',             'BS ESD AODFULL',          'Egamma'),
+    ('xAOD::TrackParticleContainer#HLT_TrigGSFTrackParticles',                     'BS ESD AODFULL AODSLIM',  'Egamma'),
+    ('xAOD::TrackParticleAuxContainer#HLT_TrigGSFTrackParticlesAux.',              'BS ESD AODFULL AODSLIM',  'Egamma'),
     
     #bphys
     ('xAOD::TrigBphysContainer#HLT_EFBMuMuFex',                               'BS ESD AODFULL AODSLIM AODVERYSLIM AODBLSSLIM',  'Bphys'),
