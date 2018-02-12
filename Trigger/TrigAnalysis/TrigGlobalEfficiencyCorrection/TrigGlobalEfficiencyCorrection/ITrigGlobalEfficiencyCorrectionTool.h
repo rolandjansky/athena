7814adb6360b5f009e3f2d812dbe3872db6637c8
Julien Maurer <julien.maurer@cern.ch>
2018-02-12 23:40:01 +0200

    dropping no-longer-needed files
    Former-commit-id: fc23cf840cf298a4d0849e6f164445516952371e

--- Trigger/TrigAnalysis/TrigGlobalEfficiencyCorrection/TrigGlobalEfficiencyCorrection/ITrigGlobalEfficiencyCorrectionTool.h
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-// contact: jmaurer@cern.ch
-
-#warning Please use instead TriggerAnalysisInterfaces/ITrigGlobalEfficiencyCorrectionTool.h
-
-#include "TriggerAnalysisInterfaces/ITrigGlobalEfficiencyCorrectionTool.h"
