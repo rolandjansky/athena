7569142d45c768a9f0f2b916c4bad73887bdec63
David Shope <david.richard.shope@cern.ch>
2018-02-13 17:54:35 +0100

    Use dedicated IP smearing maps for high pT tracks (above 15 GeV)
    Former-commit-id: 9b6b0ff796ac84b52c2171cc86eed77a4116795c

--- PhysicsAnalysis/TrackingID/InDetTrackSystematicsTools/Root/InDetTrackSmearingTool.cxx
+++ PhysicsAnalysis/TrackingID/InDetTrackSystematicsTools/Root/InDetTrackSmearingTool.cxx
@@ -61,7 +61,8 @@ namespace InDet {
     declareProperty("calibFileD0Dead", m_calibFileD0Dead = "InDetTrackSystematicsTools/CalibData_21.2_2018-v15/res_diff_d0_vs_pt.hist.root");
     declareProperty("calibFileZ0Dead", m_calibFileZ0Dead = "InDetTrackSystematicsTools/CalibData_21.2_2018-v15/res_diff_z0_vs_pt.hist.root");
 
-    declareProperty("calibFileIP", m_calibFileIP = "InDetTrackSystematicsTools/CalibData_21.2_2018-v15/trackIPAlign_dec2017.root");
+    declareProperty("calibFileIP_lowpt", m_calibFileIP_lowpt = "InDetTrackSystematicsTools/CalibData_21.2_2018-v15/trackIPAlign_dec2017.root");
+    declareProperty("calibFileIP_highpt", m_calibFileIP_highpt = "InDetTrackSystematicsTools/CalibData_21.2_2018-v15/trackIPAlignTight.root");
   }
     
 
@@ -73,19 +74,27 @@ namespace InDet {
     ATH_CHECK( initObject<TH1>(m_smearD0Dead, m_calibFileD0Dead, "res_pt_d0_0") );
     ATH_CHECK( initObject<TH1>(m_smearZ0Dead, m_calibFileZ0Dead, "res_pt_z0_0") );
 
-    ATH_CHECK( initObject<TH2>(m_smearD0, m_calibFileIP, "d0quaddiff_comb_Pt_Eta" ) );
-    ATH_CHECK( initObject<TH2>(m_smearZ0, m_calibFileIP, "z0quaddiff_comb_Pt_Eta" ) );
-    ATH_CHECK( initObject<TH2>(m_smearD0_sys_up, m_calibFileIP, "d0quaddiff_comb_Pt_Eta_sys_up" ) );
-    ATH_CHECK( initObject<TH2>(m_smearZ0_sys_up, m_calibFileIP, "z0quaddiff_comb_Pt_Eta_sys_up" ) );
-    ATH_CHECK( initObject<TH2>(m_smearD0_sys_dw, m_calibFileIP, "d0quaddiff_comb_Pt_Eta_sys_dw" ) );
-    ATH_CHECK( initObject<TH2>(m_smearZ0_sys_dw, m_calibFileIP, "z0quaddiff_comb_Pt_Eta_sys_dw" ) );
+    ATH_CHECK( initObject<TH2>(m_smearD0_lowpt, m_calibFileIP_lowpt, "d0quaddiff_comb_Pt_Eta" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_lowpt, m_calibFileIP_lowpt, "z0quaddiff_comb_Pt_Eta" ) );
+    ATH_CHECK( initObject<TH2>(m_smearD0_lowpt_sys_up, m_calibFileIP_lowpt, "d0quaddiff_comb_Pt_Eta_sys_up" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_lowpt_sys_up, m_calibFileIP_lowpt, "z0quaddiff_comb_Pt_Eta_sys_up" ) );
+    ATH_CHECK( initObject<TH2>(m_smearD0_lowpt_sys_dw, m_calibFileIP_lowpt, "d0quaddiff_comb_Pt_Eta_sys_dw" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_lowpt_sys_dw, m_calibFileIP_lowpt, "z0quaddiff_comb_Pt_Eta_sys_dw" ) );
+
+    ATH_CHECK( initObject<TH2>(m_smearD0_highpt, m_calibFileIP_highpt, "quad_diff/d0quaddiff_comb_Pt_Eta" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_highpt, m_calibFileIP_highpt, "quad_diff/z0quaddiff_comb_Pt_Eta" ) );
+    ATH_CHECK( initObject<TH2>(m_smearD0_highpt_sys_up, m_calibFileIP_highpt, "quad_diff/d0quaddiff_comb_Pt_Eta_sys_up" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_highpt_sys_up, m_calibFileIP_highpt, "quad_diff/z0quaddiff_comb_Pt_Eta_sys_up" ) );
+    ATH_CHECK( initObject<TH2>(m_smearD0_highpt_sys_dw, m_calibFileIP_highpt, "quad_diff/d0quaddiff_comb_Pt_Eta_sys_dw" ) );
+    ATH_CHECK( initObject<TH2>(m_smearZ0_highpt_sys_dw, m_calibFileIP_highpt, "quad_diff/z0quaddiff_comb_Pt_Eta_sys_dw" ) );
 
     ATH_MSG_INFO( "Using seed of " << m_seed << " to initialize RNG" );
     m_rnd = make_unique<TRandom3>(m_seed);
 
     ATH_MSG_INFO( "Using for TRK_RES_D0_DEAD case the calibration file " << PathResolverFindCalibFile(m_calibFileD0Dead) );
     ATH_MSG_INFO( "Using for TRK_RES_Z0_DEAD case the calibration file " << PathResolverFindCalibFile(m_calibFileZ0Dead) );
-    ATH_MSG_INFO( "Using for all other cases the calibration file " << PathResolverFindCalibFile(m_calibFileIP) );
+    ATH_MSG_INFO( "Using for all other cases the low pT (< 15 GeV) calibration file " << PathResolverFindCalibFile(m_calibFileIP_lowpt) );
+    ATH_MSG_INFO( "Using for all other cases the high pT (> 15 GeV) calibration file " << PathResolverFindCalibFile(m_calibFileIP_highpt) );
 
     // do common initialization (at time of writing, register affecting systematics)
     ATH_CHECK( InDetTrackSystematicsTool::initialize() );
@@ -99,12 +108,20 @@ namespace InDet {
     delete m_smearD0Dead; m_smearD0Dead = nullptr;
     delete m_smearZ0Dead; m_smearZ0Dead = nullptr;
 
-    delete m_smearD0; m_smearD0 = nullptr;
-    delete m_smearZ0; m_smearZ0 = nullptr;    
-    delete m_smearD0_sys_up; m_smearD0_sys_up = nullptr;
-    delete m_smearZ0_sys_up; m_smearZ0_sys_up = nullptr;
-    delete m_smearD0_sys_dw; m_smearD0_sys_dw = nullptr;
-    delete m_smearZ0_sys_dw; m_smearZ0_sys_dw = nullptr;
+    delete m_smearD0_lowpt; m_smearD0_lowpt = nullptr;
+    delete m_smearZ0_lowpt; m_smearZ0_lowpt = nullptr;
+    delete m_smearD0_lowpt_sys_up; m_smearD0_lowpt_sys_up = nullptr;
+    delete m_smearZ0_lowpt_sys_up; m_smearZ0_lowpt_sys_up = nullptr;
+    delete m_smearD0_lowpt_sys_dw; m_smearD0_lowpt_sys_dw = nullptr;
+    delete m_smearZ0_lowpt_sys_dw; m_smearZ0_lowpt_sys_dw = nullptr;
+
+    delete m_smearD0_highpt; m_smearD0_highpt = nullptr;
+    delete m_smearZ0_highpt; m_smearZ0_highpt = nullptr;
+    delete m_smearD0_highpt_sys_up; m_smearD0_highpt_sys_up = nullptr;
+    delete m_smearZ0_highpt_sys_up; m_smearZ0_highpt_sys_up = nullptr;
+    delete m_smearD0_highpt_sys_dw; m_smearD0_highpt_sys_dw = nullptr;
+    delete m_smearZ0_highpt_sys_dw; m_smearZ0_highpt_sys_dw = nullptr;
+
     return StatusCode::SUCCESS;
   }
 
@@ -128,12 +145,27 @@ namespace InDet {
     }
 
     if ( isActiveD0Meas || isActiveD0MeasUp || isActiveD0MeasDown ) {
-      TH2* d0hist = m_smearD0;
-      if(isActiveD0MeasUp) d0hist = m_smearD0_sys_up;
-      if(isActiveD0MeasDown) d0hist = m_smearD0_sys_dw;
+      TH2* d0hist_lowpt = m_smearD0_lowpt;
+      TH2* d0hist_highpt = m_smearD0_highpt;
+      if(isActiveD0MeasUp) {
+        d0hist_lowpt = m_smearD0_lowpt_sys_up;
+        d0hist_highpt = m_smearD0_highpt_sys_up;
+      }
+      if(isActiveD0MeasDown) {
+        d0hist_lowpt = m_smearD0_lowpt_sys_dw;
+        d0hist_highpt = m_smearD0_highpt_sys_dw;
+      }
                                                
-      if (d0hist == nullptr) ATH_MSG_ERROR( "d0 histogram is null!" );
-      float d0Smear = readHistogram(d0hist, pt, eta);
+      if (d0hist_lowpt == nullptr) ATH_MSG_ERROR( "d0 lowpt histogram is null!" );
+      if (d0hist_highpt == nullptr) ATH_MSG_ERROR( "d0 highpt histogram is null!" );
+
+      float d0Smear = 0.f;
+      if (pt < 15) {
+        d0Smear = readHistogram(d0hist_lowpt, pt, eta);
+      } else {
+        d0Smear = readHistogram(d0hist_highpt, pt, eta);
+      }
+
       sigma_D0 += d0Smear*d0Smear;
     }
 
@@ -163,11 +195,26 @@ namespace InDet {
     }
 
     if ( isActiveZ0Meas || isActiveZ0MeasUp || isActiveZ0MeasDown ) {
-      TH2* z0hist = m_smearZ0;
-      if(isActiveZ0MeasUp) z0hist = m_smearZ0_sys_up;
-      if(isActiveZ0MeasDown) z0hist = m_smearZ0_sys_dw;
-      if (z0hist == nullptr) ATH_MSG_ERROR( "z0 histogram is null!" );
-      float z0Smear = readHistogram(z0hist, pt, eta);
+      TH2* z0hist_lowpt = m_smearZ0_lowpt;
+      TH2* z0hist_highpt = m_smearZ0_highpt;
+      if(isActiveZ0MeasUp) {
+        z0hist_lowpt = m_smearZ0_lowpt_sys_up;
+        z0hist_highpt = m_smearZ0_highpt_sys_up;
+      }
+      if(isActiveZ0MeasDown) {
+        z0hist_lowpt = m_smearZ0_lowpt_sys_dw;
+        z0hist_highpt = m_smearZ0_highpt_sys_dw;
+      }
+      if (z0hist_lowpt == nullptr) ATH_MSG_ERROR( "z0 lowpt histogram is null!" );
+      if (z0hist_highpt == nullptr) ATH_MSG_ERROR( "z0 highpt histogram is null!" );
+
+      float z0Smear = 0.f;
+      if (pt < 15) {
+        z0Smear = readHistogram(z0hist_lowpt, pt, eta);
+      } else {
+        z0Smear = readHistogram(z0hist_highpt, pt, eta);
+      }
+
       sigma_Z0 += z0Smear*z0Smear;
     }
 
@@ -225,6 +272,9 @@ namespace InDet {
     // safety measure:
     if( eta>2.499 )  eta= 2.499;
     if( eta<-2.499 ) eta=-2.499;
+    if ( pt < histogram->GetXaxis()->GetXmin() ) {
+      pt = histogram->GetXaxis()->GetXmin() + 0.01;
+    }
     if ( pt >= histogram->GetXaxis()->GetXmax() ) {
       pt = histogram->GetXaxis()->GetXmax() - 0.01;
     }
