7dcaa3d0768b519b0d8734627f7c69d4f6d7478c
Olivier Arnaez <olivier.arnaez@cern.ch>
2018-02-16 21:05:31 +0100

    Add new TrackParticleTruth persistent classes using HepMcParticleLink_p2
    Add new `TrackParticleTruthCollection_p2` and `TrackParticleTruthCollectionContainer_p2`
    classes which use `HepMcParticleLink_p2` and corresponding TP Converters.
    NB The peristent versions using `HepMcParticleLink_p1` are still written
    out by default.
    (cherry picked from commit aa3286c2a93ac47d342d30131581dc15970e1f27)
    
    Add unit tests the for `TrackParticleTruth` TP Converters
    (cherry picked from commit 4d3cc18027874d378522730b6e941ced8e8eba5a)
    
    Updated `TrackParticleTruthTPCnv` unit tests to use valid `HepMcParticleLinks`
    
    
    Former-commit-id: 59565e700c14576e7785fe61b61c7f62047fc76e

---
+++ Reconstruction/TrackParticleTruthTPCnv/src/TrackParticleTruthCollectionContainer_tlp2.cxx
@@ -0,0 +1,15 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "TrackParticleTruthTPCnv/TrackParticleTruthCollectionContainer_tlp2.h"
+
+TrackParticleTruthCollectionContainer_tlp2::TrackParticleTruthCollectionContainer_tlp2()
+{
+
+}
+
+TrackParticleTruthCollectionContainer_tlp2::~TrackParticleTruthCollectionContainer_tlp2()
+{
+
+}
