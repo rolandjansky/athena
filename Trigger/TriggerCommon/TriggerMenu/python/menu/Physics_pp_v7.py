3f593ca79d000ba0e57d152c01d6791efa457488
lixia <ligang.xia@cern.ch>
2018-02-18 23:00:55 +0100

    ATR-17600
    Former-commit-id: b03e4b3e1d875b1944c9afbfd74ac1cabaae342e

--- Trigger/TriggerCommon/TriggerMenu/python/menu/Physics_pp_v7.py
+++ Trigger/TriggerCommon/TriggerMenu/python/menu/Physics_pp_v7.py
@@ -223,6 +223,9 @@ def setupMenu():
         ['mu4_mu2noL1_L1MU4', 'L1_MU4',  ['L1_MU4',''], [PhysicsStream], ['RATE:MultiMuon','BW:Muon'], -1,['serial',-1,['mu4','mu2noL1']]],
         ['mu6_mu2noL1_L1MU6', 'L1_MU6',  ['L1_MU6',''], [PhysicsStream], ['RATE:MultiMuon','BW:Muon'], -1,['serial',-1,['mu6','mu2noL1']]],
 
+	  #ATR-17600
+	  ['mu6_mu2noL1_msonly_bJpsimumu_noid','L1_MU6', ['L1_MU6',''], [PhysicsStream], ['RATE:IDMonitoring',BW_BphysTag, 'BW:ID'], -1, ['serial',-1,['mu6','mu2noL1_msonly']]],
+
 			 ]
 
 
