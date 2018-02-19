0856774f49851a71166a6d258d079ca9fb72478f
John Chapman <jchapman@cern.ch>
2018-02-19 14:10:49 +0100

    HepMcParticleLink migration updates for TrigTruthEventTPCnv
    Switched to using `TrigInDetTrackTruthMapCnv_tlp3` (which uses
    `HepMcParticleLinkCnv_p2`) rather than `TrigInDetTrackTruthMapCnv_tlp2`
    (which uses `HepMcParticleLinkCnv_p1`).
    
    Update `TrigInDetTrackTruthCnv_p1_test` and `TrigInDetTrackTruthMapCnv_p1_test`
    unit tests.

--- Trigger/TrigTruthEvent/TrigTruthEventTPCnv/test/TrigInDetTrackTruthCnv_p1_test.cxx
+++ Trigger/TrigTruthEvent/TrigTruthEventTPCnv/test/TrigInDetTrackTruthCnv_p1_test.cxx
@@ -19,6 +19,9 @@
 #include <cassert>
 #include <iostream>
 
+#include "GeneratorObjectsTPCnv/initMcEventCollection.h"
+#include "HepMC/GenParticle.h"
+#include "HepMC/GenEvent.h"
 
 void compare (const TrigIDHitStats& p1,
               const TrigIDHitStats& p2)
@@ -92,9 +95,13 @@ void testit (const TrigInDetTrackTruth& trans1)
 }
 
 
-void test1()
+void test1(std::vector<HepMC::GenParticle*>& genPartVector)
 {
   std::cout << "test1\n";
+  const HepMC::GenParticle *particle = genPartVector.at(0);
+  // Create HepMcParticleLink outside of leak check.
+  HepMcParticleLink dummyHMPL(particle->barcode(),particle->parent_event()->event_number());
+  assert(dummyHMPL.cptr()==particle);
   Athena_test::Leakcheck check;
 
   TrigInDetTrackTruth trans1;
@@ -106,6 +113,13 @@ void test1()
 
 int main()
 {
-  test1();
+  ISvcLocator* pSvcLoc = nullptr;
+  std::vector<HepMC::GenParticle*> genPartVector;
+  if (!Athena_test::initMcEventCollection(pSvcLoc, genPartVector)) {
+    std::cerr << "This test can not be run" << std::endl;
+    return 0;
+  }
+
+  test1(genPartVector);
   return 0;
 }
