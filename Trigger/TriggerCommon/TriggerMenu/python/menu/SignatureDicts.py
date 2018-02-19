b069b87c79d0186778e388cf6dfbd15e4c7e9ee2
Javier Montejo <jmontejo@cern.ch>
2018-02-19 09:54:52 +0100

    Added VBF thresholds
    Former-commit-id: e1961b36e393c4360cc3fea2d56d53ea02cb2467

--- Trigger/TriggerCommon/TriggerMenu/python/menu/SignatureDicts.py
+++ Trigger/TriggerCommon/TriggerMenu/python/menu/SignatureDicts.py
@@ -84,12 +84,16 @@ AllowedTopos_jet = ['muvtx',
                     'dphi26',
                     'invm250',
                     'invm400',
+                    'invm400j40',
                     'invm500',
                     'invm600',
                     'invm700',
+                    'invm700j50',
                     'invm800',
                     'invm900',
-                    'invm1000']
+                    'invm1000',
+                    'invm1000j50',
+                    ]
 AllowedTopos_Tau = ['ditauL', 'ditauM', 'ditauT','tautsf','notautsf','50mVis10000','60mVis10000','03dR27','03dR30']
 AllowedTopos_comb = ['taumass', 'dr05', 'dz02','dz99']
 
