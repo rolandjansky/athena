7979df190c66f583470fe0b2bb6ffe90e9c1d5b5
Dan Guest <dguest@cern.ch>
2018-02-20 14:42:05 +0100

    Add ghost tracks to VR jet pseudoJetGetters
    Former-commit-id: b7f012e6d15a60c657feae4d5b846c8bb5501ce3

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkFlavourTag/share/FTAG5.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkFlavourTag/share/FTAG5.py
@@ -22,6 +22,7 @@ from DerivationFrameworkJetEtMiss.JetCommon import (
     OutputJets, addJetOutputs)
 from DerivationFrameworkJetEtMiss.ExtendedJetCommon import (
     addDefaultTrimmedJets, replaceAODReducedJets)
+from JetRec.JetRecStandard import jtm
 
 # tracking
 from TrkVertexFitterUtils.TrkVertexFitterUtilsConf import (
@@ -131,11 +132,13 @@ addDefaultTrimmedJets(FTAG5Seq,"FTAG5",dotruth=True)
 # Create variable-R trackjets and dress AntiKt10LCTopo with ghost VR-trkjet
 # Note that the ghost association to the 'AntiKt10LCTopo' jets is
 # hardcoded within this function "for now".
+pseudoJetGetters = jtm.gettersMap['pv0track'] + ["gtrackget"]
+
 addVRJets(FTAG5Seq,
           VRJetName="AntiKtVR30Rmax4Rmin02Track",
           VRGhostLabel="GhostVR30Rmax4Rmin02TrackJet",
           VRJetAlg="AntiKt", VRJetRadius=0.4,
-          VRJetInputs="pv0track", #or should this be lctopo?
+          VRJetInputs=pseudoJetGetters,
           ghostArea = 0 , ptmin = 2000, ptminFilter = 7000,
           variableRMinRadius = 0.02, variableRMassScale = 30000,
           calibOpt = "none")
