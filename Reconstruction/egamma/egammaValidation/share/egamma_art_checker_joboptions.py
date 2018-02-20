81dc4abf9e80942b1c1231420f50c841803efcb5
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2018-02-20 12:59:23 +0000

    Cherry-pick "Merge branch 'ART_Implementation' into '21.0'" into master
    First ART tests for egamma software
    
    See merge request atlas/athena!8606
    
    
    Former-commit-id: 97fe12789d8853f4ea8dc9106c3f694129b55416

---
+++ Reconstruction/egamma/egammaValidation/share/egamma_art_checker_joboptions.py
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration.
+#
+
+from AthenaCommon.AppMgr                    import theApp
+from AthenaCommon.AppMgr                    import ServiceMgr as svcMgr
+from AthenaCommon.AlgSequence               import AlgSequence
+from GaudiSvc.GaudiSvcConf                  import THistSvc
+import AthenaRootComps.ReadAthenaxAODHybrid
+
+# Ouput Message Level
+svcMgr.MessageSvc.OutputLevel = INFO
+
+# ART File
+testFile = 'ART_Rec_'+particleType+'.pool.root'
+svcMgr.EventSelector.InputCollections = [testFile]
+
+
+job = CfgMgr.AthSequencer("AthAlgSeq")
+job += CfgMgr.EgammaMonitoring('MonitoringAlg', sampleType = particleType)
+
+
+theApp.EvtMax = 2000
+
+
+outputFile = 'ART-monitoring_'+particleType+'.root'
+svcMgr += CfgMgr.THistSvc()
+svcMgr.THistSvc.Output = ["MONITORING DATAFILE='"+outputFile+"' OPT='RECREATE'"]
+
+
+print ( "\n\nALL OK\n\n" )
