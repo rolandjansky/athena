77c91b1124764b5c080b38b7c62838517e7a3ab8
christos@cern.ch <your.name@cern.ch>
2018-02-20 17:17:26 +0000

    bring Photon Eff up to date, although are not part of the Athena project
    Former-commit-id: eae4cd4fd4a779dd66c6022a9c7b6f2dd9a176c1

--- PhysicsAnalysis/ElectronPhotonID/PhotonEfficiencyCorrection/src/components/PhotonEfficiencyCorrection_entries.cxx
+++ PhysicsAnalysis/ElectronPhotonID/PhotonEfficiencyCorrection/src/components/PhotonEfficiencyCorrection_entries.cxx
@@ -1,8 +1,18 @@
+// Gaudi/Athena include(s):
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Local include(s):
 #include "PhotonEfficiencyCorrection/AsgPhotonEfficiencyCorrectionTool.h"
 
 #include "../testAthenaPhotonAlg.h"
 
-DECLARE_COMPONENT( AsgPhotonEfficiencyCorrectionTool )
+DECLARE_TOOL_FACTORY( AsgPhotonEfficiencyCorrectionTool )
 
-DECLARE_COMPONENT( testAthenaPhotonAlg )
+DECLARE_ALGORITHM_FACTORY( testAthenaPhotonAlg )
 
+DECLARE_FACTORY_ENTRIES( PhotonEfficiencyCorrection ) 
+{
+  DECLARE_TOOL( AsgPhotonEfficiencyCorrectionTool )
+  DECLARE_ALGORITHM( testAthenaPhotonAlg )  
+
+}
