9f9e1bb41b8c8171825218a1c206e77036bf80d3
Filomena Sopkova <filomena.sopkova@cern.ch>
2018-02-20 11:15:25 +0100

    add new histogram
    Former-commit-id: 52577d43c5329daa203c6b77cedb91e620c91b2b

--- Reconstruction/PFlow/PFlowValidation/PFOHistUtils/PFOHistUtils/ClusterPlots.h
+++ Reconstruction/PFlow/PFlowValidation/PFOHistUtils/PFOHistUtils/ClusterPlots.h
@@ -34,6 +34,10 @@ namespace PFO {
 
      TH1* m_Cluster_eta_lowpt;
      TH1* m_Cluster_pt_interval;
+     TH1* m_Cluster_pt_zoom;
+
+     TH1* m_Cluster_eSample_EM;
+     TH1* m_Cluster_eSample_HAD;
 
      void initializePlots();
      std::string m_sClusterContainerName;
