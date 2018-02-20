c2a4bb9b26aeefbd5b352f33fcc202a04bce39e4
Filomena Sopkova <filomena.sopkova@cern.ch>
2018-02-20 11:16:05 +0100

    add new histogram
    Former-commit-id: 7300dff0f372085195b0d56cccb61475e92db4c0

--- Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/ClusterPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/ClusterPlots.cxx
@@ -26,6 +26,10 @@ namespace PFO {
 
     m_Cluster_eta_lowpt = Book1D("Cluster_Eta_lowPt",m_sClusterContainerName + "_Eta_lowPt (Entries/0.1)",100,-5.0,5.0);
     m_Cluster_pt_interval = Book1D("Cluster_Pt_interval",m_sClusterContainerName + "_Pt_interval (Entries/1 GeV)", 980, 20.0, 1000.0);
+    m_Cluster_pt_zoom = Book1D("Cluster_Pt_zoom",m_sClusterContainerName + "_Pt_zomm (Entries/0.1 GeV)",3000,-100.0,200.0);
+
+    m_Cluster_eSample_EM = Book1D("Cluster_eSample_EM",m_sClusterContainerName +"_eSample_EM (Entries/1 GeV) ",300,-100.0,200.0);
+    m_Cluster_eSample_HAD = Book1D("Cluster_eSample_HAD",m_sClusterContainerName +"_eSample_HAD (Entries/1 GeV) ",300,-100.0,200.0);
 
   }
 
@@ -47,6 +51,17 @@ namespace PFO {
         m_Cluster_eta_lowpt->Fill(Cluster.eta());
     }
     m_Cluster_pt_interval->Fill(Cluster.pt()/1000.0);
+    m_Cluster_pt_zoom->Fill(Cluster.pt()/1000.0);
+
+    float eSample_EM;
+   eSample_EM = Cluster.eSample(CaloSampling::PreSamplerB) + Cluster.eSample(CaloSampling::EMB1) + Cluster.eSample(CaloSampling::EMB2) + Cluster.eSample(CaloSampling::EMB3) + Cluster.eSample(CaloSampling::PreSamplerE) + Cluster.eSample(CaloSampling::EME1) + Cluster.eSample(CaloSampling::EME2) + Cluster.eSample(CaloSampling::EME3) + Cluster.eSample(CaloSampling::FCAL0);
+   m_Cluster_eSample_EM->Fill((double)eSample_EM/1000.0);
+   
+   
+   
+   float eSample_HAD;
+   eSample_HAD = Cluster.eSample(CaloSampling::TileBar0) + Cluster.eSample(CaloSampling::TileBar1) + Cluster.eSample(CaloSampling::TileBar2) + Cluster.eSample(CaloSampling::HEC0) + Cluster.eSample(CaloSampling::HEC1) + Cluster.eSample(CaloSampling::HEC2) + Cluster.eSample(CaloSampling::HEC3) + Cluster.eSample(CaloSampling::FCAL1) + Cluster.eSample(CaloSampling::FCAL2);
+   m_Cluster_eSample_HAD->Fill((double)eSample_HAD/1000.0);
 
   }
 
