faeff75636a393399fc8741515801c5a28033ab3
Goetz Gaycken <goetz.gaycken@cern.ch>
2018-02-21 23:17:13 +0100

    Removed extra content definition now also provided by HIGG5Common.
    Former-commit-id: 7fa2f4cad1bdd1c7696b12c3d79860cdd34b8aea

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5D2ExtraContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5D2ExtraContent.py
@@ -5,7 +5,6 @@
 import HIGG5Common
 ExtraContent=HIGG5Common.getHIGG5Common()
 ExtraContent+=[
-   "AntiKt10TrackCaloClusterTrimmedPtFrac5SmallR20Jets.pt.eta.phi.m.constituentLinks.JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m.Angularity.Aplanarity.DetectorEta.ECF1.ECF2.ECF3.FoxWolfram0.FoxWolfram2.GhostMuonSegmentCount.GhostTrackCount.KtDR.Parent.PlanarFlow.Qw.Split12.Split23.Tau1_wta.Tau2_wta.Tau3_wta.ZCut12",
     "Photons.f3core"
    ]
 
