bb8913c1c168e7aa216ee9c3fae1da9b52491a6b
Danilo Enoque Ferreira de Lima <dferreir@cern.ch>
2018-02-23 10:57:17 +0100

    Updated EXOT4 to use new interface for e-in-jet OR.
    Former-commit-id: 8bdc5b5bda6a1a78a2265e5efa767b2ad0cefebf

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/share/EXOT4.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/share/EXOT4.py
@@ -133,9 +133,6 @@ if isMC:
 
 # in the next lines: actually make the AntiKt4Truth, Track and EMTopo jet using
 # the topo clusters lists that have the electron-matched clusters removed
-if isMC:
-   jfind_noel_truth = jtm.addJetFinder("AntiKt4TruthNoElJets", "AntiKt", 0.4, "truthNoEl","truthNoEl", ghostArea=0.0 , ptmin=10000, ptminFilter=70000,calibOpt="")
-
 
 # add algorithms in the sequence that calculate the b-tagging variables
 # for the el-subtracted jets
@@ -163,31 +160,20 @@ btag_jetnoel_track = ConfInst.setupJetBTaggerTool(ToolSvc, JetCollection="AntiKt
 jtm.modifiersMap["akt4tracknoel"] = jtm.modifiersMap["track"] + [btag_jetnoel_track]
 jtm.modifiersMap["akt4emtoponoel"] = jtm.modifiersMap["emtopo_ungroomed"] + [btag_jetnoel]
 
-jfind_smallnoel_trk = jtm.addJetFinder("AntiKt4TrackNoElJets", "AntiKt", 0.4, "TrkJetElRemovalgetter", "akt4tracknoel",
-                ghostArea=0.01 , ptmin=10000, ptminFilter=10000)
+jfind_smallnoel_trk = addStandardJet("AntiKt", 0.4, "TrackNoEl",
+                                     ptmin = 10000, ptminFilter = 10000,
+                                     mods = "akt4tracknoel", calibOpt="none", ghostArea=0.01,
+                                     customGetters = "TrkJetElRemovalgetter", pretools=[EXOT16ElRemovalTool])
 
-jfind_smallnoel_emtopo = jtm.addJetFinder("AntiKt4EMTopoNoElJets", "AntiKt", 0.4, "JetElRemovalgetter", "akt4emtoponoel",
-                ghostArea=0.01 , ptmin=10000, ptminFilter=10000,
-                calibOpt="none")
+jfind_smallnoel_emtopo = addStandardJet("AntiKt", 0.4, "EMTopoNoEl",
+                                     ptmin = 10000, ptminFilter = 10000,
+                                     mods = "akt4emtoponoel", calibOpt="none", ghostArea=0.01,
+                                     customGetters = "JetElRemovalgetter", pretools=[])
 
-# the jet finder tool has been created, but
-# the JetAlgorithm is what, added in the exot4Seq actually
-# makes jets (by just calling the finder)
-
-from JetRec.JetRecConf import JetAlgorithm
-
-jetalg_smallnoel_emtopo = JetAlgorithm("jetalgAntiKt4EMTopoNoElJets", Tools = [  jfind_smallnoel_emtopo] )
-jetalg_smallnoel_trk = JetAlgorithm("jetalgAntiKt4trkNoElJets", Tools = [EXOT16ElRemovalTool, jfind_smallnoel_trk] )
-if isMC:
-   jetalg_smallnoel_truth =  JetAlgorithm("jetalgAntiKt4truthNoElJets", Tools = [jtm.truthpartcopyNoEl, jfind_noel_truth] )
-
-# now we have a JetAlgorithm instance that calls the full chain
-# add it in the sequence:
-if isMC:
-   exot4Seq += jetalg_smallnoel_truth
-exot4Seq += jetalg_smallnoel_trk
-exot4Seq += jetalg_smallnoel_emtopo 
-
+jfind_smallnoel_truth = addStandardJet("AntiKt", 0.4, "TruthNoEl",
+                                     ptmin = 10000, ptminFilter = 70000,
+                                     mods = "truthNoEl", calibOpt="", ghostArea=0.0,
+                                     customGetters = "truthNoEl", pretools=[jtm.truthpartcopyNoEl])
 
 # at this point we have new on-the-fly jets
 # in the containers AntiKt4TruthNoElJets, AntiKt4TrackNoElJets, AntiKt4EMTopoNoElJets
