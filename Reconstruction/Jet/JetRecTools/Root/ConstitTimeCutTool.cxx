118b7d105a6d79f156213524c9b75e24bdb7356f
Marco Valente <marco.valente@cern.ch>
2018-02-24 20:58:12 +0100

    Cherry-pick a69d8ce07e95a3ed3d22aa3f47787d2f631df945 [formerly e12e3283911e0e39372a3eca07c7ef94990bfe1c]
    Former-commit-id: 97b4f86bc013946d5d84c99d4b36a24176d7f1c0

--- Reconstruction/Jet/JetRecTools/Root/ConstitTimeCutTool.cxx
+++ Reconstruction/Jet/JetRecTools/Root/ConstitTimeCutTool.cxx
@@ -1,57 +1,94 @@
 #include <vector>
-#include "JetRecTools/ClusterTimeCutTool.h"
+#include "JetRecTools/ConstitTimeCutTool.h"
+#include "xAODCaloEvent/CaloClusterContainer.h"
+#include "xAODPFlow/PFOContainer.h"
 
 using namespace std;
 
 
-ClusterTimeCutTool::ClusterTimeCutTool(const std::string& name) : JetConstituentModifierBase(name)
-                                                                , m_lambdaCalDivide(0)
-                                                                , m_qualityCut(0)
-                                                                , m_timeCut1(0)
-                                                                , m_timeCut2(0)
+ConstitTimeCutTool::ConstitTimeCutTool(const std::string& name) : JetConstituentModifierBase(name)
 {
-#ifdef ASG_TOOL_ATHENA
-  declareInterface<IJetConstituentModifier>(this);
-#endif
   declareProperty("LambdaCalDivide", m_lambdaCalDivide = 317);
   declareProperty("QualityCut", m_qualityCut = 0.02);
-  declareProperty("TimeCut1", m_timeCut1 = 5);
-  declareProperty("TimeCut2", m_timeCut2 = 15);
+  declareProperty("TimeCutLargeQ", m_timeCutLargeQ = 5);
+  declareProperty("TimeCutSmallQ", m_timeCutSmallQ = 15);
+  declareProperty("MaxEtaForCut", m_etaMax = 2.5);
 }
 
+StatusCode ConstitTimeCutTool::initialize() {
 
-StatusCode ClusterTimeCutTool::process(xAOD::CaloClusterContainer* cont) const {
-  xAOD::CaloClusterContainer::iterator cl = cont->begin();
-  xAOD::CaloClusterContainer::iterator cl_end = cont->end();
+  if(m_inputType==xAOD::Type::ParticleFlow) {
+    if(!m_applyToNeutralPFO) {
+      ATH_MSG_ERROR("Incompatible configuration: ApplyToNeutralPFO=False -- what kind of pileup do you wish to suppress?");
+      return StatusCode::FAILURE;
+    }
+  } else {
+    if(m_inputType!=xAOD::Type::CaloCluster) {
+      ATH_MSG_ERROR("Incompatible configuration: ConstitTimeCutTool is not specialised for inputs of type "
+		    << m_inputType);
+      return StatusCode::FAILURE;
+    }
+  }
+  return StatusCode::SUCCESS;
+}
 
-  for( ; cl != cl_end; ++cl ) {
-		if(abs( (*cl)->eta() ) < 2.5){
-			double quality =(*cl)->auxdata<float>("AVG_LAR_Q");
-                        quality /= 65535; //quality is on [0,2^16-1] scale
-			double lambda_center =(*cl)->auxdata<float>("CENTER_LAMBDA");
-			double time = (*cl)->time();
-			if(lambda_center > m_lambdaCalDivide) continue;
-			if( quality > m_qualityCut && abs(time) > m_timeCut1){
-				(*cl)->setE( 0 );
-			}
-			if( quality <= m_qualityCut && abs(time) > m_timeCut2){
-				(*cl)->setE( 0 );
-			}
-		}
-
+// Determine the appropriate input type and extract the timing, then
+// call the applyTimingCut method
+StatusCode ConstitTimeCutTool::process_impl(xAOD::IParticleContainer* cont) const {
+  switch(m_inputType) {
+  case xAOD::Type::CaloCluster:
+    {
+      xAOD::CaloClusterContainer* clusters = static_cast<xAOD::CaloClusterContainer*> (cont);
+      for(xAOD::CaloCluster* cl : *clusters) {
+	float time = cl->time();
+	ATH_CHECK( applyTimingCut(cl, time) );
+      }
+    }
+    break;
+  case xAOD::Type::ParticleFlow:
+    {
+      xAOD::PFOContainer* pfos = static_cast<xAOD::PFOContainer*> (cont);
+      for(xAOD::PFO* pfo : *pfos) {
+	if(fabs(pfo->charge())<FLT_MIN) { // only apply to neutrals
+	  float time(0);
+	  // Only apply cut if retrieval succeeded, else warn
+	  if(pfo->attribute<float>(xAOD::PFODetails::eflowRec_TIMING,time)) {
+	    ATH_CHECK( applyTimingCut(pfo, time) );
+	  } else {
+	    ATH_MSG_WARNING("Failed to retrieve PFO timing for PFO #" << pfo->index());
+	  }
+	}
+      }
+    }
+    break;
+  default:
+    // Should not get here, because type-checking should happen in process()
+    ATH_MSG_ERROR("No specialisation for object type " << m_inputType);
+    return StatusCode::FAILURE;
   }
 
   return StatusCode::SUCCESS;
 }
 
-StatusCode ClusterTimeCutTool::process(xAOD::IParticleContainer* cont) const {
-  xAOD::CaloClusterContainer* clust = dynamic_cast<xAOD::CaloClusterContainer*> (cont); // Get CaloCluster container
-  if(clust) return process(clust);
-  return StatusCode::FAILURE;
+
+StatusCode ConstitTimeCutTool::applyTimingCut(xAOD::IParticle* part, float time) const {
+  const static SG::AuxElement::ConstAccessor<float> acc_larq("AVG_LAR_Q");
+  const static SG::AuxElement::ConstAccessor<float> acc_clambda("CENTER_LAMBDA");
+
+  if(abs( part->eta() ) < m_etaMax){
+    //quality is on [0,2^16-1] scale
+    float quality = acc_larq(*part) / 65535;
+    float lambda_center = acc_clambda(*part);
+
+    // Only apply to ECal by default (with depth cut)
+    // Should switch to a test on the dominant sampling??
+    if(lambda_center < m_lambdaCalDivide) {
+      // Apply variable timing cut based on he cluster quality
+      // -- a larger spread is observed for clusters with small Q
+      float timeCut = quality > m_qualityCut ? m_timeCutLargeQ : m_timeCutSmallQ;
+      if( abs(time) > timeCut ) {setEnergyPt( part, 0., 0. );}
+    }
+  }
+
+  return StatusCode::SUCCESS;
 }
-
-
-ClusterTimeCutTool::~ClusterTimeCutTool(){
-}
-
-
