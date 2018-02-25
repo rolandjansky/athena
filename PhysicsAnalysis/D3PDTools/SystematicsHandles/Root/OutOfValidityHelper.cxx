dfd617742a433fa98e6615c2387ef07f4cbc40ad
Nils Krumnack <krumnack@iastate.edu>
2018-02-24 18:16:29 -0600

    add a helper class for OutOfValidity results
    It is undefined what to do in those cases, so this class makes it
    configurable.
    
    
    Former-commit-id: aeaafac8988e20ee6e3a4b618a481198ed752048

---
+++ PhysicsAnalysis/D3PDTools/SystematicsHandles/Root/OutOfValidityHelper.cxx
@@ -0,0 +1,103 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+/// @author Nils Krumnack
+
+
+
+//
+// includes
+//
+
+#include <SystematicsHandles/OutOfValidityHelper.h>
+
+#include <AsgTools/MessageCheck.h>
+#include <AsgTools/StatusCode.h>
+#include <PATInterfaces/CorrectionCode.h>
+#include <RootCoreUtils/Assert.h>
+#include <SystematicsHandles/SelectionHelpers.h>
+
+//
+// method implementations
+//
+
+namespace EL
+{
+  ::StatusCode OutOfValidityHelper ::
+  initialize ()
+  {
+    if (m_decoName.empty())
+    {
+      ANA_MSG_ERROR ("failed to set decoration name for OutOfValidityRange selection");
+      return StatusCode::FAILURE;
+    }
+    if (m_decoName == "SUCCESS")
+    {
+      m_action = Action::SUCCESS;
+    } else if (m_decoName == "FAILURE")
+    {
+      m_action = Action::FAILURE;
+    } else if (m_decoName.find ("WARNING") == 0)
+    {
+      // warnings can be combined with adding a decoration, or be used
+      // on their own
+      m_action = Action::WARNING;
+      if (m_decoName != "WARNING")
+      {
+        m_accessor = std::make_unique<const SG::AuxElement::Accessor<SelectionType> >
+          (m_decoName.substr (8));
+      }
+    } else
+    {
+      m_accessor = std::make_unique<const SG::AuxElement::Accessor<SelectionType> >
+        (m_decoName);
+    }
+    return StatusCode::SUCCESS;
+  }
+
+
+  ::StatusCode OutOfValidityHelper ::
+  check (xAOD::IParticle& particle,
+         const CP::CorrectionCode& code,
+         const char *context) const
+  {
+    assert (m_action != Action::UNDEFINED);
+
+    switch (code)
+    {
+    case CP::CorrectionCode::Ok:
+      if (m_accessor) (*m_accessor) (particle) = selectionAccept();
+      return StatusCode::SUCCESS;
+    case CP::CorrectionCode::Error:
+      return StatusCode::FAILURE;
+    case CP::CorrectionCode::OutOfValidityRange:
+      if (m_accessor) (*m_accessor) (particle) = selectionReject();
+      switch (m_action)
+      {
+      case Action::UNDEFINED:
+        ANA_MSG_ERROR ("failed to initialize OutOfValidityHelper: " << context);
+        return StatusCode::FAILURE;
+      case Action::FAILURE:
+        ANA_MSG_ERROR ("encountered OutOfValidity: " << context);
+        return StatusCode::FAILURE;
+      case Action::SUCCESS:
+        return StatusCode::SUCCESS;
+      case Action::WARNING:
+        ANA_MSG_WARNING ("encountered OutOfValidity: " << context);
+        return StatusCode::FAILURE;
+      }
+      assert (false);
+    }
+  }
+
+
+
+  MsgStream& OutOfValidityHelper ::
+  msg (MSG::Level lvl) const
+  {
+    assert (m_msg != nullptr);
+    *m_msg << lvl;
+    return *m_msg;
+  }
+}
---
+++ PhysicsAnalysis/D3PDTools/SystematicsHandles/SystematicsHandles/OutOfValidityHelper.h
@@ -0,0 +1,108 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+/// @author Nils Krumnack
+
+
+
+#ifndef SYSTEMATICS_HANDLES__OUT_OF_VALIDITY_HELPER_H
+#define SYSTEMATICS_HANDLES__OUT_OF_VALIDITY_HELPER_H
+
+#include <AsgTools/MsgStream.h>
+#include <AthContainers/AuxElement.h>
+#include <SystematicsHandles/SelectionHelpers.h>
+#include <xAODBase/IParticle.h>
+#include <memory>
+
+class StatusCode;
+
+namespace CP
+{
+  class CorrectionCode;
+}
+
+namespace EL
+{
+  /// \brief a helper to translate a \ref CP::CorrectionCode into a
+  /// \ref ::StatusCode
+  ///
+  /// The prolem is OutOfValidityRange which does not have an
+  /// equivalent in StatusCode and which does not have a unique,
+  /// correct handling in all situations.  This helper allows to
+  /// configure a variety of behaviors via properties.
+
+  class OutOfValidityHelper final
+  {
+    /// \brief standard constructor
+  public:
+    template<typename T>
+    OutOfValidityHelper (T *owner, const std::string& propertyName = "outOfValidity",
+                         const std::string& propertyDescription = "how to handle out of validity results",
+                         const std::string& defaultValue = "FAILURE");
+
+
+    /// \brief standard initialize
+  public:
+    ::StatusCode initialize ();
+
+    /// \brief check the correction code and do the proper thing
+  public:
+    ::StatusCode check (xAOD::IParticle& particle,
+                        const CP::CorrectionCode& code,
+                        const char *context) const;
+
+
+    /// \brief either the name of the decoration we use, or a special
+    /// keyword for the action we apply when encountering
+    /// OutOfValidity
+  private:
+    std::string m_decoName;
+
+    /// \brief the action to perform on encountering an
+    /// OutOfValidityRange
+  private:
+    enum class Action
+    {
+      UNDEFINED,
+      SUCCESS,
+      FAILURE,
+      WARNING
+    };
+
+    /// \brief the action to take
+  private:
+    Action m_action {Action::UNDEFINED};
+
+    /// \brief the message stream we use
+  private:
+    MsgStream *m_msg {nullptr};
+
+    /// \brief the accessor if we apply one
+  private:
+    std::unique_ptr<const SG::AuxElement::Accessor<SelectionType> > m_accessor;
+
+    /// \brief helper for message macros
+  private:
+    MsgStream& msg( const MSG::Level lvl ) const;
+  };
+
+
+
+  template<typename T> OutOfValidityHelper ::
+  OutOfValidityHelper (T *owner, const std::string& propertyName,
+                       const std::string& propertyDescription,
+                       const std::string& defaultValue)
+    : m_msg (&owner->msg())
+  {
+    owner->declareProperty (propertyName, m_decoName = defaultValue,
+                            propertyDescription);
+  }
+}
+
+/// \brief a helper check macro to work with \ref OutOfValidityHelper
+#define ANA_CHECK_CORRECTION(helper,object,expr)                \
+  if ((helper).check ((object), (expr), #expr).isFailure())     \
+    return StatusCode::FAILURE;
+
+#endif
