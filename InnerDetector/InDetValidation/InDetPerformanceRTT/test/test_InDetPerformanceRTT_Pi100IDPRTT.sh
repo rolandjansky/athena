2266d58f6d077931ed0c63bd7f21c98bec5f449e
Joshua Moss <jmoss@lxplus043.cern.ch>
2018-02-26 18:23:08 +0100

    Updated all InDetPerformanceRTT ART scripts to use DCube for Post processing instead of rootcomp.py.
    Nightly processing using ATHENA is unchanged.  DCube will run from cvmfs
    art-data location until art-scripts is setup.
    
    
    Former-commit-id: 6e71ef355925ea66b6b2b3147e92fd19d281ec62

--- InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_Pi100IDPRTT.sh
+++ InnerDetector/InDetValidation/InDetPerformanceRTT/test/test_InDetPerformanceRTT_Pi100IDPRTT.sh
@@ -3,6 +3,8 @@
 # art-type: grid
 # art-input: mc15_13TeV.422015.ParticleGun_single_pion_Pt100.recon.RDO.e4459_s2726_r7059_tid07275536_00
 # art-input-nfiles: 10
+# art-include: 21.0/Athena
+# art-include: master/Athena
 # art-output: InDetStandardPlots.root
 # art-output: comparison.root
 # art-output: comparison.ps
@@ -13,8 +15,15 @@ fileList="['${ArtInFile//,/', '}']"
 echo "List of files = ", $fileList
 
 joboptions="InDetPerformanceRTT/InDetPerformanceRTT_jobOptions.py"
-get_files -jo $joboptions
-athena.py  -c "from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; athenaCommonFlags.FilesInput=$fileList; EventMax=20000;" $joboptions
+set -x
 
-refhist="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/InDetPerformanceRTT/ReferenceHistograms/InDetStandardPlots-SinglePi100000.root"
-rootcomp.py -o comparison -c $refhist InDetStandardPlots.root
+get_files -jo $joboptions
+ls -lR
+
+athena.py  -c "from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; athenaCommonFlags.FilesInput=$fileList; EventMax=20000;" $joboptions
+echo  "art-result: $? reco"
+
+
+bash /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/InDetPerformanceRTT/dcube/bin/art-dcube "Pi100" "InDetStandardPlots-SinglePi100000.root"
+echo  "art-result: $? plot"
+
