2bb70fc1c0a4c184c5a4dacf169e59f05355bde7
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2018-02-27 21:45:56 +0100

    cleanup share/enhanceMPI_example.py by removal of commented lines
    Former-commit-id: b993abc15515a4f056ce5e996227f22eacecc9e6

--- Generators/Pythia8_i/share/enhanceMPI_example.py
+++ Generators/Pythia8_i/share/enhanceMPI_example.py
@@ -3,10 +3,8 @@
 
 evgenConfig.description = "Example QCD jets with MPI enhanced with UserHook"
 evgenConfig.keywords = ["QCD", "MPI"]
-#include("MC12JobOptions/Pythia8_AU2_CTEQ6L1_Common.py")
 include('MC15JobOptions/nonStandard/Pythia8_A14_NNPDF23LO_Common.py')
 
-#genSeq.Pythia8.UserParams += ["EnhanceMPI:PTCut=20"]
 
 genSeq.Pythia8.Commands += ["HardQCD:all=on",
                             "PhaseSpace:pTHatMin=60",
