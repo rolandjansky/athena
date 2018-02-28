1d1ebbd927e86c4d83d767ad117068eaa838b608
jonathanjamieson <jonathan.jamieson@cern.ch>
2018-02-28 02:33:28 +0100

    Remove superfluous comments and imports from python config files
    Former-commit-id: cc61ca3fe9e9af4cf1ad22f6fc3ce5b41565e249

--- Trigger/TrigAlgorithms/TrigMuSuperEF/python/TrigMuonEFTagandProbeConfig.py
+++ Trigger/TrigAlgorithms/TrigMuSuperEF/python/TrigMuonEFTagandProbeConfig.py
@@ -1,60 +1,36 @@
 # Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
-#from TriggerJobOpts.TriggerFlags import TriggerFlags
 from TrigTimeMonitor.TrigTimeHistToolConfig import TrigTimeHistToolConfig
 from TrigMuonEFTagandProbeMonitoring import TrigMuonEFTagandProbeMonitoring
-#from TrigMuonEF.TrigMuonEFMonitoring import TrigMuonEFStandaloneToolMonitoring,TrigMuonEFCombinerToolMonitoring,TrigMuonEFStandaloneToolValidationMonitoring,TrigMuonEFCombinerToolValidationMonitoring
-#from TrigMuonEF.TrigMuonEFConf import TrigMuonEFTrackIsolationTool
 
-#from TrigMuonHypo.TrigMuonHypoConfig import TrigMuonEFCombinerHypoConfig
 from TrigMuSuperEFConf import TrigMuonEFTagandProbe #Conf is based on algorithm package not individual algs
-#from AthenaCommon.BeamFlags import jobproperties
-
-#from AthenaCommon.CfgGetter import getPublicTool
-#from AthenaCommon import CfgMgr
-#from AthenaCommon.SystemOfUnits import mm
                                                                                                                                
-# Default config: RoI based, Combined, TrigMuonEF only
+# Default config: Online Z->mumu HLT based Tag and Probe efficiency monitoring
  
 class TrigMuonEFTagandProbeConfig(TrigMuonEFTagandProbe): #making python class from Conf import TaP object
     __slots__ = () #Saves space by not using memory slots rather than a  dictionary for storing class attributes
 
-# These are methods tied to the class we are building
-# self is a reference to the object we passed to the class
-# we need this as python is not object orientated so we can't assume this is assumed like it is in c++
-# __init__ is essentially pythons version of a constructor, this method gets called whenever our base class is used and it takes self as an implicit argument to know what to build as well as any other arguments that we might want to pass when calling our base class method
-# **kwargs is a 'magic' argument variable which stands in for arguments when we don't know how many are going to be passed, Kwargs over args is for arguments in python list format with associated key strings    
+# These are methods tied to class being built
+# self is a reference to the class object itself
+# __init__ is essentially pythons version of a constructor, gets called whenever base class is used and takes self as an implicit argument to know what to build as well as any other arguments passed when calling base class method
+# **kwargs is a 'magic' argument variable which stands in for unknown numbers of arguments when we don't know how many are going to be passed, used for arguments in python list format with associated key strings
 
-# Right now there are no kwargs to set up for TaP
+# Currently no kwargs to set up for TaP
     def __init__(self,name="TrigMuonEFTagandProbe",**kwargs): 
 
         #Set out actions associated with class construction
 
 
-
-        # turn on seeded data decoding by defualt       
-        #TriggerFlags.MuonSlice.doEFRoIDrivenAccess = True
-
-
-
         # make instance of TrigMuonEFTagandProbe class when constructor is called
-        # Super allows direct use of inherited methods and attributes from the parent class, TrigMuonEFTagandProbe in this case without having to directly reference this class, this is useful for multiple inheritance 
+        # Super allows direct use of inherited methods and attributes from the parent class, TrigMuonEFTagandProbe in this case, without having to directly reference this class, useful for multiple inheritance 
 
         super(TrigMuonEFTagandProbeConfig,self).__init__(name,**kwargs)
 
         # setup monitoring depending on configuration
         monTools = []
 
-        # top level histograms use Combined Muons
         monTools.append( TrigMuonEFTagandProbeMonitoring() )
 
-
-
-#        from AthenaCommon.CfgGetter import getPublicTool,getPublicToolClone
-#        self.StauCreatorTool = getPublicToolClone("TMEF_StauCreatorTool","TMEF_MuonCreatorTool",BuildStauContainer=True)
-
-
- 
         # always add timing monitoring
         timetool =  TrigTimeHistToolConfig("Time")
         timetool.NumberOfHistBins = 100
@@ -66,8 +42,8 @@ class TrigMuonEFTagandProbeConfig(TrigMuonEFTagandProbe): #making python class f
 
 
 #
-# Space for Several pre-configured RoI based variations
-# Only base class needed for now though
+# Space for Several pre-configured TaP variations
+# Only base class needed for now
 #
 def TrigMuonEFTagandProbe(name="TrigMuonEFTagandProbe",**kwargs):
     return TrigMuonEFTagandProbeConfig(name,**kwargs)
--- Trigger/TrigAlgorithms/TrigMuSuperEF/python/TrigMuonEFTagandProbeConfigDb.py
+++ Trigger/TrigAlgorithms/TrigMuSuperEF/python/TrigMuonEFTagandProbeConfigDb.py
@@ -1,11 +1,5 @@
 # Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
-from AthenaCommon.CfgGetter import addAlgorithm,addTool,addService
+from AthenaCommon.CfgGetter import addAlgorithm
 
-# RoI based (Currently only on flavour of TaP algorithm)
+# Online Z->mumu Tag and Probe efficiency monitoring (Currently only one flavour of TaP algorithm)
 addAlgorithm("TrigMuonEFTagandProbe.TrigMuonEFTagandProbeConfig.TrigMuonEFTagandProbe","TrigMuonEFTagandProbe")
-
-# tools for Calotag  
-                                                                  
-#addTool("TrigMuSuperEF.TrigMuSuperEFConfig.TrigMuSuperEF_MuonCaloTagTool", "TrigMuSuperEF_MuonCaloTagTool")
-#addTool("TrigMuSuperEF.TrigMuSuperEFConfig.TrigMuSuperEF_TrackDepositInCaloTool", "TrigMuSuperEF_TrackDepositInCaloTool")
-#addTool("TrigMuSuperEF.TrigMuSuperEFConfig.TrigMuSuperEF_CaloTrkSelectorTool", "TrigMuSuperEF_CaloTrkSelectorTool")
