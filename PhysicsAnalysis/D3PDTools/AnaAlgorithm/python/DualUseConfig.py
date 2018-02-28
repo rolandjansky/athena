294ff43331c284269c41364313d8cd202fc9dc56
Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
2018-02-28 23:51:54 +0100

    started on dual-use configuration, not working yet
    So, in principle I factored out the functionality that is different
    between AnalysisBase and Athena, but it seems to have real trouble
    with setting and configuring private tools.
    
    
    Former-commit-id: a9f522b370d5fa383f91cea57e9da94b1040e7b2

---
+++ PhysicsAnalysis/D3PDTools/AnaAlgorithm/python/DualUseConfig.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+import sys
+
+this = sys.modules[__name__]
+this.CfgMgr = None
+
+def setCfgMgr (val_CfgMgr) :
+    this = sys.modules[__name__]
+    this.CfgMgr = val_CfgMgr
+    pass
+
+def createAlgorithm (type, name) :
+    this = sys.modules[__name__]
+    if this.CfgMgr :
+        type = "__".join (type.split ("::"))
+        return this.CfgMgr.__getattr__ (type) (name)
+    else :
+        from AnaAlgorithm.AnaAlgorithmConfig import AnaAlgorithmConfig
+        return AnaAlgorithmConfig( type + "/" + name )
+
+def addPrivateTool (alg, name, type) :
+    this = sys.modules[__name__]
+    if this.CfgMgr :
+        parent = alg
+        path = name.split (".")
+        for x in path[0:-1] :
+            print path, x
+            parent = parent.__getattr__ (x)
+            pass
+        type = "__".join (type.split ("::"))
+        parent.__setattr__ (path[-1], type + "/" + path[-1])
+        pass
+    else :
+        alg.addPrivateTool (name, type)
+        pass
