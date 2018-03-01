e7c51e3ce050f233629b63f822ed89c99b173827
Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>
2018-03-01 16:54:37 +0100

    ART version 0.8.20
    Former-commit-id: cb10c91bf7691171d13d5c980f1bcc88d9931baa

---
+++ Tools/ART/scripts/art-download.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+#
+# NOTE do NOT run with /bin/bash -x as the output is too big for gitlab-ci
+# arguments:  INPUTNAME
+#
+# author : Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>
+#
+# example: art-download NAME DIRECTORY
+
+if [ $# -ne 2 ]; then
+    echo 'Usage: art-get-input.sh NAME DIRECTORY'
+    exit 1
+fi
+
+NAME=$1
+shift
+DIRECTORY=$1
+shift
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
+
+unset ALRB_noGridMW
+
+lsetup -f rucio
+
+echo "Name: ${NAME}"
+echo "Directory: ${DIRECTORY}"
+
+# Do not use: rucio delivers warnings as exit code 127
+#set -e
+
+rucio download --dir ${DIRECTORY} ${NAME}
