2cce68111dc11a2b97582fcd38052b5b3818bdd2
Tomasz Bold <tomasz.bold@gmail.com>
2018-03-02 09:41:36 +0100

    Integrated NewJO test
    Former-commit-id: 1207ecc1c9f230a2ea203f95db245fdaa46fd70c

--- Trigger/TrigValidation/TrigUpgradeTest/test/test_newJO.sh
+++ Trigger/TrigValidation/TrigUpgradeTest/test/test_newJO.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # art-type: build
 # art-ci: master
-
-athena.py --threads=1 --evtMax=10 --filesInput="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1" TrigUpgradeTest/newJOtest.py
+get_files TrigUpgradeTest/newJOtest.py
+python newJOtest.py # generate pickle
+athena --threads=1 newJOtest.pkl
