48146e845f6ecd7da1924f0d3493888c3f2ef7d4
Sebastian Mergelmeyer <sebastian.mergelmeyer@cern.ch>
2018-03-02 20:11:07 +0100

    persist analysis metadata into AnalysisTop output (ANALYSISTO-482)
    Former-commit-id: 07949e6a2e225fcb3888b3cf102fb4c9fafd266c

--- PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/util/top-xaod.cxx
+++ PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/util/top-xaod.cxx
@@ -35,6 +35,7 @@
 #include "TopConfiguration/TopConfig.h"
 #include "TopConfiguration/SelectionConfigurationData.h"
 
+#include "TopAnalysis/AnalysisTrackingHelper.h"
 #include "TopAnalysis/EventSelectionManager.h"
 #include "TopAnalysis/Tools.h"
 #include "TopCPTools/TopToolStore.h"
@@ -111,6 +112,16 @@ int main(int argc, char** argv) {
 
     const std::string libraryNames = settings->value("LibraryNames");
     top::loadLibraries(libraryNames);
+
+    // Analysis tracking
+    std::unique_ptr<top::AnalysisTrackingHelper> tracker;
+    {
+        bool useTracking = true;
+        settings->retrieve("WriteTrackingData", useTracking);
+        if (useTracking) {
+            tracker.reset(new top::AnalysisTrackingHelper());
+        }
+    }
 
     // I/O Performance stats?
     // Summary gives a summary
@@ -282,6 +293,7 @@ int main(int argc, char** argv) {
 
     // OK let's printout the TopConfig
     std::cout << *topConfig << "\n";
+    if (tracker) tracker->setTopConfig(topConfig);
 
     //Top parton history for MC events
     // This is quite ugly and simple, it will be harmonized with in the future
@@ -512,7 +524,8 @@ int main(int argc, char** argv) {
 	  firstEvent = topConfig->numberOfEventsToSkip()-skippedEventsSoFar;
 	  skippedEventsSoFar += entries < firstEvent ? entries : firstEvent;
 	}
-        for (unsigned int entry = firstEvent; entry < entries; ++entry, ++totalYieldSoFar) {
+        unsigned int entry;
+        for (entry = firstEvent; entry < entries; ++entry, ++totalYieldSoFar) {
 
    	    if (topConfig->numberOfEventsToRun() != 0 && totalYieldSoFar >= topConfig->numberOfEventsToRun() ) break;
 
@@ -744,6 +757,7 @@ int main(int argc, char** argv) {
             eventSaver->saveEventToxAOD();
 
         } //loop over events in current file
+        if (tracker) tracker->addInputFile(inputFile->GetName(), entry-firstEvent);
 
         // do it at the end, so we can get the DS id from the first event
         // notice this is different from the normal sum of weights: all entries matter, not only the highest per file
@@ -841,6 +855,8 @@ int main(int argc, char** argv) {
     outputFile->cd();
     eventSelectionManager.finalise();
     eventSaver->finalize();
+    outputFile->cd();
+    if (tracker) tracker->writeTree("AnalysisTracking");
     outputFile->Close();
     bool outputFileGood = !outputFile->TestBit(TFile::kWriteError);
     if (outputFileGood) {
