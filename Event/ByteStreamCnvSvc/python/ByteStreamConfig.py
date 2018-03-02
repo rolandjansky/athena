2076a8644d0bfb30875f48f3428599a92280bef1
Tomasz Bold <tomasz.bold@gmail.com>
2018-03-02 09:33:50 +0100

    BS reading config works
    Former-commit-id: fb15a4246c62f16fcff347957fdd6a92705d4152

---
+++ Event/ByteStreamCnvSvc/python/ByteStreamConfig.py
@@ -0,0 +1,105 @@
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+
+
+def TrigBSReadCfg( inputFlags ):
+    """
+    Creates accumulator for BS reading
+    """
+    filenames=inputFlags.get("AthenaConfiguration.GlobalFlags.InputFiles")
+    
+    acc = ComponentAccumulator()
+    
+    from ByteStreamCnvSvc.ByteStreamCnvSvcConf import ByteStreamCnvSvc, ByteStreamEventStorageInputSvc, EventSelectorByteStream
+
+    eventSelector = EventSelectorByteStream("EventSelector")
+    acc.addService( eventSelector )
+    acc.setAppProperty( "EvtSel", eventSelector.name() )
+
+    bsInputSvc = ByteStreamEventStorageInputSvc( "ByteStreamInputSvc" )
+    bsInputSvc.FullFileName = filenames
+    acc.addService( bsInputSvc )
+
+    from GaudiSvc.GaudiSvcConf import EvtPersistencySvc
+    eventPersistencySvc = EvtPersistencySvc( "EventPersistencySvc" )
+    acc.addService( eventPersistencySvc )
+    
+    bsCnvSvc = ByteStreamCnvSvc()
+    eventSelector.ByteStreamInputSvc = bsInputSvc.name();
+    eventPersistencySvc.CnvServices = [ bsCnvSvc.name() ]
+    acc.addService( bsCnvSvc )
+
+    from ByteStreamCnvSvcBase.ByteStreamCnvSvcBaseConf import ROBDataProviderSvc
+    robDPSvc = ROBDataProviderSvc()
+    acc.addService( robDPSvc ) 
+
+    from ByteStreamCnvSvcBase.ByteStreamCnvSvcBaseConf import ByteStreamAddressProviderSvc
+    bsAddressProviderSvc = ByteStreamAddressProviderSvc()
+    acc.addService( bsAddressProviderSvc )
+
+    from IOVDbMetaDataTools.IOVDbMetaDataToolsConf import IOVDbMetaDataTool
+    iovMetaDataTool = IOVDbMetaDataTool()
+    acc.addAlgTool( iovMetaDataTool )    
+
+    from ByteStreamCnvSvc.ByteStreamCnvSvcConf import ByteStreamMetadataTool
+    bsMetaDataTool = ByteStreamMetadataTool()
+    acc.addAlgTool( bsMetaDataTool )
+
+    from StoreGate.StoreGateConf import ProxyProviderSvc, StoreGateSvc
+    metaDataStore = StoreGateSvc("MetaDataStore")   
+    acc.addService( metaDataStore )
+    inputMetaDataStore = StoreGateSvc("InputMetaDataStore")   
+    acc.addService( inputMetaDataStore )
+
+    from AthenaServices.AthenaServicesConf import MetaDataSvc
+    metaDataSvc = MetaDataSvc()
+    acc.addService( metaDataSvc )
+
+    metaDataSvc.MetaDataContainer = "MetaDataHdr"
+    metaDataSvc.MetaDataTools = [ iovMetaDataTool.name(), bsMetaDataTool.name() ]    
+    
+    proxy = ProxyProviderSvc()
+    proxy.ProviderNames += [ bsAddressProviderSvc.name(), metaDataSvc.name() ]
+    acc.addService( proxy )
+
+    from ByteStreamCnvSvc.ByteStreamCnvSvcConf import ByteStreamAttListMetadataSvc
+    acc.addService( ByteStreamAttListMetadataSvc() )
+    
+           
+    # this is trigger specific and should only be loaded if some doTrigger flags is set
+    # or it shoudl be moved elsewhere, however, since there is no better location now let is stick here
+    bsCnvSvc.InitCnvs += [ "EventInfo",
+                        "ROIB::RoIBResult",
+                        "HLT::HLTResult" ]
+    
+    bsAddressProviderSvc.TypeNames += [
+        "TileCellIDC/TileCellIDC",
+        "MdtDigitContainer/MDT_DIGITS",
+        "RpcDigitContainer/RPC_DIGITS",
+        "TgcDigitContainer/TGC_DIGITS",
+        "CscDigitContainer/CSC_DIGITS",
+        "ROIB::RecRoIBResult/RecRoIBResult",
+        "MuCTPI_RIO/MUCTPI_RIO",
+        "CTP_RIO/CTP_RIO"
+    ]
+
+    bsAddressProviderSvc.TypeNames += [
+        "LArRawChannelContainer/LArRawChannels",
+        "TileRawChannelContainer/TileRawChannelCnt",
+        "ROIB::RoIBResult/RoIBResult",
+        "MuCTPI_RDO/MUCTPI_RDO",
+        "HLT::HLTResult/HLTResult_L2",
+        "HLT::HLTResult/HLTResult_EF",
+        "CTP_RDO/CTP_RDO",
+        "L1TopoRDOCollection/L1TopoRDOCollection"
+    ]
+
+
+    
+    if inputFlags.get( "AthenaConfiguration.GlobalFlags.isMC" ) == False:        
+        bsCnvSvc.GetDetectorMask=True
+        # still need to figure out how conditions are setup in new system
+        #from IOVDbSvc.CondDB import conddb
+        #conddb.addFolder( 'TDAQ', '/TDAQ/RunCtrl/SOR_Params' )
+        #acc.addService( conddb )    
+
+    return acc
--- Trigger/TrigValidation/TrigUpgradeTest/share/newJOtest.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/newJOtest.py
@@ -1,23 +1,50 @@
 
-doL1Unpacking=False
-include("testHLT_MT.py")
-
 
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ConfigFlags import ConfigFlagContainer    
 from AthenaConfiguration.CfgLogMsg import cfgLogMsg
+from AthenaCommon.CFElements import parOR, seqOR, seqAND, stepSeq
+from AthenaCommon.AlgSequence import dumpMasterSequence
+from AthenaCommon.AppMgr import theApp
+#theApp.setup()
+
 cfgLogMsg.setLevel("debug")
 flags = ConfigFlagContainer()
+
+flags.set("AthenaConfiguration.GlobalFlags.isMC",False)
+flags.set("AthenaConfiguration.GlobalFlags.InputFiles",
+          ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"])
+
 acc = ComponentAccumulator()
-from L1Decoder.L1DecoderMod import L1DecoderMod
+from ByteStreamCnvSvc.ByteStreamConfig import TrigBSReadCfg
+acc.executeModule( TrigBSReadCfg, flags )
 
-acc.addConfig( L1DecoderMod, flags )
-l1 = acc.getEventAlgo( "L1Decoder" )
+from AtlasGeoModel.GeoModelConfig import GeoModelCfg
+acc.executeModule( GeoModelCfg, flags )
 
-from TrigUpgradeTest.TestUtils import applyMenu
-applyMenu( l1 )
-l1.OutputLevel = DEBUG
-for u in l1.roiUnpackers:
-    u.OutputLevel = DEBUG
+# HLT top CF, will be able to add it once MR 8533 is there
+# This commented could should stay 
+# acc.addSequence( seqOR( "hltTop") )
+# acc.addSequence( seqAND("hltSteps" ), sequence="hltTop" )
+# for step in range(1, 6):
+#     acc.addSequence( parOR("hltStep%d" %s ), sequence="hltSteps" )
 
+# for now we run trivial tester, 
+from AthenaCommon.Constants import DEBUG
+from ByteStreamCnvSvcBase.ByteStreamCnvSvcBaseConf import ROBDataProviderMTTest
+acc.addEventAlgo( ROBDataProviderMTTest("Tester", OutputLevel=DEBUG) )
+
+
+# that is how the L1 decoder can be added but it needs more work to bring all needed services (i.e. TrigConfiguration)
+#from L1Decoder.L1DecoderMod import L1DecoderMod
+#acc.executeModule( L1DecoderMod, flags )
+#l1 = acc.getEventAlgo( "L1Decoder" )
+#from TrigUpgradeTest.TestUtils import applyMenu
+#applyMenu( l1 )
+
+#acc.printConfig() waiting for MR 8533 is there
+
+with file("newJOtest.pkl", "w") as p:
+    acc.store( p )
+    p.close()
 
