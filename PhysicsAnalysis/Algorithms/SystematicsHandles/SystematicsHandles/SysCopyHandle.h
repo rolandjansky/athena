3ad4b2b1c473be197ec8b8a2d15593051bd379ec
Nils Krumnack <krumnack@iastate.edu>
2018-03-05 17:16:18 +0100

    update copyright statements
    Former-commit-id: 22260b658da962344d543658600a2750304686ae

--- PhysicsAnalysis/Algorithms/SystematicsHandles/SystematicsHandles/SysCopyHandle.h
+++ PhysicsAnalysis/Algorithms/SystematicsHandles/SystematicsHandles/SysCopyHandle.h
@@ -1,15 +1,12 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+/// @author Nils Krumnack
+
+
 #ifndef SYSTEMATICS_HANDLES__SYS_COPY_HANDLE_H
 #define SYSTEMATICS_HANDLES__SYS_COPY_HANDLE_H
-
-//        Copyright Iowa State University 2017.
-//                  Author: Nils Krumnack
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
-// Please feel free to contact me (nils.erik.krumnack@cern.ch) for bug
-// reports, feature suggestions, praise and complaints.
-
 
 #include <AsgTools/MsgStream.h>
 #include <PATInterfaces/SystematicSet.h>
