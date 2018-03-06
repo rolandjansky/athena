387529e6af4e381151318db56f7da0e4883848f0
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2018-03-06 17:42:46 +0000

    Merge branch '21.0-sleptonsLLP' into '21.0'
    Updating slepton configuration to allow more general application
    
    See merge request atlas/athena!9279
    
    (cherry picked from commit 3dbc7046ce72f7bb314eb1c21f6571e267b7ced0 [formerly ecf03ffee16b605c0f8f8334b4fb2f3cca103d00])
    
    349ba86f Updating slepton configuration to allow more general application
    2852c731 Updating slepton configuration to allow more general application
    a98ff12a Merge branch '21.0-sleptonsLLP' of https://gitlab.cern.ch:8443/ekuwertz/athena…
    5d5ed5df Removing/improving comments, removing unused function, addressing comments from MR.
    
    Former-commit-id: 8b9166847deb1875b95ef697f3b69b6f07003a94

--- Simulation/G4Extensions/Sleptons/src/G4SMuonLMinus.cxx
+++ Simulation/G4Extensions/Sleptons/src/G4SMuonLMinus.cxx
@@ -2,21 +2,22 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "G4SMuonPlus.hh"
+#include "G4SMuonLMinus.hh"
 // ######################################################################
-// ###                         SMuonPlus                              ###
+// ###                         SMuonLMinus                              ###
 // ######################################################################
 
-G4SMuonPlus* G4SMuonPlus::theInstance = NULL;
 
-G4SMuonPlus* G4SMuonPlus::Definition(G4double mass, G4double width, G4double charge, G4double PDG, G4bool stable, G4double lifetime, G4bool shortlived)
+G4SMuonLMinus* G4SMuonLMinus::theInstance = NULL;
+
+G4SMuonLMinus* G4SMuonLMinus::Definition(G4double mass, G4double width, G4double charge, G4double PDG, G4bool stable, G4double lifetime, G4bool shortlived)
 {
 
   if (theInstance !=0 && (mass>=0. || width>=0. || lifetime>=0.) )
     {
       G4ExceptionDescription description;
-      description << "Trying to redefine the G4SMuon Plus properties after it has been constructed is not allowed";
-      G4Exception("G4SMuonPlus", "FailedRedefinition", FatalException, description);
+      description << "Trying to redefine the G4SMuonL Minus properties after it has been constructed is not allowed";
+      G4Exception("G4SMuonLMinus", "FailedRedefinition", FatalException, description);
       abort();
     }
 
@@ -34,24 +35,22 @@ G4SMuonPlus* G4SMuonPlus::Definition(G4double mass, G4double width, G4double cha
   //             stable         lifetime    decay table
   //             shortlived      subType    anti_encoding
   if (mass >= 0) {
-
     G4ParticleDefinition* anInstance =
-      new G4ParticleDefinition("s_mu_plus_R",     mass,    width,    charge,
+      new G4ParticleDefinition("s_mu_minus_L",     mass,    width,    charge,
                                0,                  0,               0,
                                0,                  0,               0,
                                "slepton",   1,               0,          PDG,
                                stable,               lifetime,            NULL,
-                               shortlived,              "SMuonPlus");
+                               shortlived,              "SMuonLMinus");
 
-    theInstance = reinterpret_cast<G4SMuonPlus*>(anInstance);
+    theInstance = reinterpret_cast<G4SMuonLMinus*>(anInstance);
     return theInstance;
-
   }
   else
     {
       G4ExceptionDescription description;
       description << "Trying to create a particle with default constructor is not allowed";
-      G4Exception("G4SMuonPlus", "DefaultConstructorCalled", FatalException, description);
+      G4Exception("G4SMuonLMinus", "DefaultConstructorCalled", FatalException, description);
       abort();
     }
 }
--- Simulation/G4Extensions/Sleptons/src/G4SMuonRMinus.cxx
+++ Simulation/G4Extensions/Sleptons/src/G4SMuonRMinus.cxx
@@ -2,21 +2,22 @@
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "G4STauMinus.hh"
+#include "G4SMuonRMinus.hh"
 // ######################################################################
-// ###                         STauMinus                              ###
+// ###                         SMuonRMinus                              ###
 // ######################################################################
 
-G4STauMinus* G4STauMinus::theInstance = NULL;
 
-G4STauMinus* G4STauMinus::Definition(G4double mass, G4double width, G4double charge, G4double PDG, G4bool stable, G4double lifetime, G4bool shortlived)
+G4SMuonRMinus* G4SMuonRMinus::theInstance = NULL;
+
+G4SMuonRMinus* G4SMuonRMinus::Definition(G4double mass, G4double width, G4double charge, G4double PDG, G4bool stable, G4double lifetime, G4bool shortlived)
 {
 
   if (theInstance !=0 && (mass>=0. || width>=0. || lifetime>=0.) )
     {
       G4ExceptionDescription description;
-      description << "Trying to redefine the G4Stau Minus properties after it has been constructed is not allowed";
-      G4Exception("G4StauMinus", "FailedRedefinition", FatalException, description);
+      description << "Trying to redefine the G4SMuonL Minus properties after it has been constructed is not allowed";
+      G4Exception("G4SMuonRMinus", "FailedRedefinition", FatalException, description);
       abort();
     }
 
@@ -33,25 +34,23 @@ G4STauMinus* G4STauMinus::Definition(G4double mass, G4double width, G4double cha
   //               type    lepton number  baryon number   PDG encoding
   //             stable         lifetime    decay table
   //             shortlived      subType    anti_encoding
-
   if (mass >= 0) {
-
     G4ParticleDefinition* anInstance =
-      new G4ParticleDefinition("s_tau_minus_1",     mass,    width,    charge,
+      new G4ParticleDefinition("s_mu_minus_R",     mass,    width,    charge,
                                0,                  0,               0,
                                0,                  0,               0,
                                "slepton",   1,               0,          PDG,
                                stable,               lifetime,            NULL,
-                               shortlived,              "STauMinus");
+                               shortlived,              "SMuonRMinus");
 
-    theInstance = reinterpret_cast<G4STauMinus*>(anInstance);
+    theInstance = reinterpret_cast<G4SMuonRMinus*>(anInstance);
     return theInstance;
   }
   else
     {
       G4ExceptionDescription description;
       description << "Trying to create a particle with default constructor is not allowed";
-      G4Exception("G4StauMinus", "DefaultConstructorCalled", FatalException, description);
+      G4Exception("G4SMuonRMinus", "DefaultConstructorCalled", FatalException, description);
       abort();
     }
 }
