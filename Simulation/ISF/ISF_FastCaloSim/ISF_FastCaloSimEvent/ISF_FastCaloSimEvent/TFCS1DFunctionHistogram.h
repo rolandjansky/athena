02dd5b3a9e4f63247c8a74d0778da6e55e36ee43
Jana Schaarschmidt <jana.schaarschmidt@cern.ch>
2018-03-06 01:53:00 +0100

    update FCS epara get_inverse and smart_rebinning
    Former-commit-id: 21c89eb728cd64a1fd53fdee5823966a9eced97e

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/ISF_FastCaloSimEvent/TFCS1DFunctionHistogram.h
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/ISF_FastCaloSimEvent/TFCS1DFunctionHistogram.h
@@ -20,14 +20,16 @@ class TFCS1DFunctionHistogram:public TFCS1DFunction
 
     virtual double rnd_to_fct(double rnd);
     TH1* vector_to_histo();
-          double get_inverse(double rnd);
-          double linear(double x1,double x2,double y1,double y2,double x);
-
-    void smart_rebin_loop(TH1* hist, int,double);
-    TH1D* smart_rebin(TH1D* , double );
+    double get_inverse(double rnd);
+    double linear(double x1,double x2,double y1,double y2,double x);
+    
+    double  get_maxdev(TH1*, TH1D*);
+    void    smart_rebin_loop(TH1* hist, double);
+    double  get_change(TH1*);
+    TH1D*   smart_rebin(TH1D*);
     double* histo_to_array(TH1*);
-    double sample_from_histo(TH1* hist, double);
-    double sample_from_histovalues(double);
+    double  sample_from_histo(TH1* hist, double);
+    double  sample_from_histovalues(double);
 
     vector<float> get_HistoBorders() {return m_HistoBorders;};
     vector<float> get_HistoContents()  {return m_HistoContents;};
@@ -41,5 +43,9 @@ class TFCS1DFunctionHistogram:public TFCS1DFunction
   ClassDef(TFCS1DFunctionHistogram,1)  //TFCS1DFunctionHistogram
 
 };
+
+#if defined(__MAKECINT__) && defined(__FastCaloSimStandAlone__)
+#pragma link C++ class TFCS1DFunctionHistogram+;
+#endif
 
 #endif
