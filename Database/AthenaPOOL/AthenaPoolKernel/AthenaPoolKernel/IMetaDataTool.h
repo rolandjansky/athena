6ba09cf70b702385cd0be89da925a83da8cb2e17
cranshaw <Jack.Cranshaw@cern.ch>
2018-03-07 04:50:24 -0600

    Move IMetaDataTool to AthenaKernel in preparation to deprecate AthenaPoolKernel.
    Former-commit-id: bb1f4e8729e6521c38880105c13f675810e15838

--- Database/AthenaPOOL/AthenaPoolKernel/AthenaPoolKernel/IMetaDataTool.h
+++ Database/AthenaPOOL/AthenaPoolKernel/AthenaPoolKernel/IMetaDataTool.h
@@ -11,33 +11,6 @@
  *  $Id: IMetaDataTool.h,v 1.2 2007-07-30 19:06:50 gemmeren Exp $
  **/
 
-#include "GaudiKernel/IAlgTool.h"
-
-/** @class IMetaDataTool
- *  @brief This class provides the interface for MetaDataTools.
- **/
-class IMetaDataTool : virtual public IAlgTool {
-
-public: // Non-static members
-
-  /// Function called when a new input file is opened
-  virtual StatusCode beginInputFile() = 0;
-
-  /// Function called when the currently open input file got completely
-  /// processed
-  virtual StatusCode endInputFile() = 0;
-
-  /// Function called when the tool should write out its metadata
-  virtual StatusCode metaDataStop() = 0;
-
-  /// Gaudi boilerplate
-   static const InterfaceID& interfaceID();
-private: // Data
-};
-
-inline const InterfaceID& IMetaDataTool::interfaceID() {
-   static const InterfaceID IID("IMetaDataTool", 1, 0);
-   return(IID);
-}
+#include "AthenaKernel/IMetaDataTool.h"
 
 #endif
