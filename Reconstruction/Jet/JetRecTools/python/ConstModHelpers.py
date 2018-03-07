50631b08c2a39c687559d7a0a4bef00d38c5068b
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2018-03-07 22:11:17 +0100

    Provide helper functions for adding constituent-modified jets to derivations   * ExtendedJetCommon.addConstModJets will schedule the constituent modifiers     and subsequently run the jet algorithm -- defined for PF and TC jets   * Some changes made to ConstModHelpers to support this
    Former-commit-id: b544d1f7257c26a825625496305e8dea774c40e9

--- Reconstruction/Jet/JetRecTools/python/ConstModHelpers.py
+++ Reconstruction/Jet/JetRecTools/python/ConstModHelpers.py
@@ -14,11 +14,16 @@ constmodlog = Logging.logging.getLogger('ConstModHelpers')
 def getConstModSeq(sequence,inputtype,suffix=""):
 
     tooltypes = {
-        "Origin":"origin",
         "SK06":"softkiller06",
         "CS":"constsub",
         "Vor":"voronoi",
     }
+
+    from JetRec.JetRecStandard import jtm
+    sequencestr = "".join(sequence)
+    seqname = "ConstitMod{0}_{1}{2}".format(sequencestr,inputtype,suffix)
+    if hasattr(jtm,seqname):
+        return getattr(jtm,seqname)
 
     from JetRecTools.JetRecToolsConfig import ctm
 
@@ -28,6 +33,8 @@ def getConstModSeq(sequence,inputtype,suffix=""):
         modlist.append("correctPFO")
     elif inputtype=="EMTopo":
         modlist.append("clus_emscale")
+    if "Topo" in inputtype:
+        modlist.append("clus_origin")
     for step in sequence:
         tool = None
         toolname = "ConstitMod{0}_{1}{2}".format(inputtype,step,suffix)
@@ -35,9 +42,7 @@ def getConstModSeq(sequence,inputtype,suffix=""):
         if alias in ctm.modifiersMap.keys():
             tool = ctm.modifiersMap[alias]
         else:
-            if step=="Origin":
-                tool = CfgMgr.CaloClusterConstituentsOrigin(toolname)
-            elif step=="Vor":
+            if step=="Vor":
                 tool = CfgMgr.VoronoiWeightTool(toolname, doSpread=False, nSigma=0)
             elif step=="CS":
                 tool = CfgMgr.ConstituentSubtractorTool(toolname)
@@ -50,8 +55,6 @@ def getConstModSeq(sequence,inputtype,suffix=""):
             ctm.add(tool,alias)
         modlist.append(alias)
 
-    sequencestr = "".join(sequence)
-    seqname = "ConstitMod{0}_{1}{2}".format(sequencestr,inputtype,suffix)
     inputcontainer = ""
     outputcontainer = ""
     if inputtype=="EMPFlow":
@@ -69,15 +72,16 @@ def getConstModSeq(sequence,inputtype,suffix=""):
                                             OutputContainer = outputcontainer,
                                             InputContainer= inputcontainer,
                                             modList = modlist)
+    jtm += modseq
     return modseq
 
-def getJetFinder(label,pjname,modkey):
+def getPseudoJetGetter(label,pjname):
     from JetRec.JetRecStandard import jtm
     pjg = None
-    getterbase = ""
-    jetsname = "AntiKt4"+label+"Jets"
-    edtool = None
-    from EventShapeTools.EventDensityConfig import configEventDensityTool
+
+    if hasattr(jtm,pjname+"get"):
+        return getattr(jtm,pjname+"get")
+
     if "PFlow" in label:
         pjg = CfgMgr.PFlowPseudoJetGetter(pjname+"get",
                                           InputContainer=label[7:]+"ParticleFlowObjects",
@@ -86,8 +90,6 @@ def getJetFinder(label,pjname,modkey):
                                           SkipNegativeEnergy = True,
                                           GhostScale = 0.0)
         jtm += pjg
-        edtool = configEventDensityTool("Kt4"+label+"EventShape", pjg, 0.4)
-        getterbase = "empflow_reduced"
     else:
         tclabel = label.replace('Topo','TopoOrigin')
         pjg = CfgMgr.PseudoJetGetter(pjname+"get",
@@ -97,18 +99,33 @@ def getJetFinder(label,pjname,modkey):
                                      SkipNegativeEnergy = True,
                                      GhostScale = 0.0)
         jtm += pjg
-        edtool = configEventDensityTool("Kt4"+tclabel+"EventShape", pjg, 0.4)
-        getterbase = "emtopo_reduced"
+
+    return pjg
+
+def getEventDensityTool(pjg):
+    label = pjg.Label.replace('Topo','TopoOrigin')
+    toolname = "Kt4"+label+"EventShape"
+    if hasattr(ToolSvc,toolname):
+        return getattr(ToolSvc,toolname)
+
+    from EventShapeTools.EventDensityConfig import configEventDensityTool
+    edtool = configEventDensityTool("Kt4"+label+"EventShape", pjg, 0.4)
+
     from AthenaCommon.AppMgr import ToolSvc
     ToolSvc += edtool
+    return edtool
 
-    getterkey = pjname
+def getJetFinder(inputtype,label,pjname,modkey):
+    from JetRec.JetRecStandard import jtm
+    pjg = getPseudoJetGetter(label,pjname)
+    jetsname = "AntiKt4"+label+"Jets"
+    getterbase = inputtype.lower()+"_ungroomed"
+
     jtm.gettersMap[pjname] = [pjg]+list(jtm.gettersMap[getterbase])[1:]
     finder = jtm.addJetFinder(jetsname, "AntiKt", 0.4, pjname, modkey,
                               ghostArea=0.01, ptmin=2000, ptminFilter=2000, calibOpt="none")
 
-    jtm += edtool
-    return finder, edtool
+    return finder
 
 def getConstModMETAlg(suffix, doPFlow):
   from METReconstruction.METRecoFlags import metFlags
@@ -116,10 +133,10 @@ def getConstModMETAlg(suffix, doPFlow):
   from METReconstruction.METAssocConfig import getMETAssocAlg
   inputtype = ""
   if doPFlow:
-    cfg = getEMPFlowConfig(suffix)
+    cfg = getEMPFlowMETConfig(suffix)
     inputtype = "EMPFlow"
   else:
-    cfg = getEMTopoConfig(suffix)
+    cfg = getEMTopoMETConfig(suffix)
     inputtype = "EMTopo"
 
   metsuffix = "AntiKt4"+inputtype+suffix
@@ -135,7 +152,7 @@ def getConstModMETAlg(suffix, doPFlow):
   return alg, outputdict
 
 
-def getEMTopoConfig(suffix):
+def getEMTopoMETConfig(suffix):
   from METReconstruction.METAssocConfig import AssocConfig, METAssocConfig
   metsuffix = 'AntiKt4EMTopo'+suffix
   jetname = metsuffix+"Jets"
@@ -155,7 +172,7 @@ def getEMTopoConfig(suffix):
   return cfg
 
 
-def getEMPFlowConfig(suffix):
+def getEMPFlowMETConfig(suffix):
   from METReconstruction.METAssocConfig import AssocConfig, METAssocConfig
   metsuffix = 'AntiKt4EMPFlow'+suffix
   jetname = metsuffix+"Jets"
