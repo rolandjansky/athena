d166692cf297f6b74e9ef91ae78b18844e0c9320
scott snyder <scott.snyder@cern.ch>
2018-03-08 09:49:56 +0100

    SCT_ConditionsServices: Fix circular dependency.
    A circular #include dependency was introduced between SCT_ConditionsData
    and SCT_ConditionsServices.  Resolve by moving SCT_ConditionsParameters.h
    from SCT_ConditionsServices to SCT_ConditionsData.
    
    
    Former-commit-id: e3781aa89d1ca99581d577ca3b602a5d4b772187

--- InnerDetector/InDetConditions/SCT_ConditionsServices/SCT_ConditionsServices/SCT_ConditionsParameters.h
+++ InnerDetector/InDetConditions/SCT_ConditionsServices/SCT_ConditionsServices/SCT_ConditionsParameters.h
@@ -12,20 +12,8 @@
 #ifndef SCT_ConditionsParameters_h
 #define SCT_ConditionsParameters_h
 
-#include <string>
 
-namespace SCT_ConditionsServices{
-  enum NumberOfElements{SIDES_PER_MODULE=2, CHIPS_PER_SIDE=6, CHIPS_PER_MODULE=12, NUMBER_OF_MODULES=4088, STRIPS_PER_WAFER=768, NUMBER_OF_WAFERS=8176};
-  enum NumberOfCalibrationData{N_NPTGAIN=6, N_NOISEOCC=4};
-  enum MajorityParameters{HighAndLowVoltageOK=17, /* 17 = 0x11 -> majority state for both LV and HV. */
-                          OVERALL=110,
-                          BARREL=111,
-                          ECA=114,
-                          ECC=115,
-                          N_REGIONS=4,
-                          INDEX_HVfraction=1,
-                          INDEX_MajorityState=3};
-  enum Bec{bec_ECC=-2, bec_BARREL=0, bec_ECA=+2};
-}
+#include "SCT_ConditionsData/SCT_ConditionsParameters.h"
+
 
 #endif // SCT_ConditionsParameters_h
