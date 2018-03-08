2f35260df19c5c27117f96d5f88113c64b647473
scott snyder <scott.snyder@cern.ch>
2018-03-08 09:49:41 +0100

    SCT_ConditionsData: Fix circular dependency.
    A circular #include dependency was introduced between SCT_ConditionsData
    and SCT_ConditionsServices.  Resolve by moving SCT_ConditionsParameters.h
    from SCT_ConditionsServices to SCT_ConditionsData.
    
    
    Former-commit-id: a260a0bbffcf2f77658350400764308dfee16a62

--- InnerDetector/InDetConditions/SCT_ConditionsData/SCT_ConditionsData/SCT_AllGoodStripInfo.h
+++ InnerDetector/InDetConditions/SCT_ConditionsData/SCT_ConditionsData/SCT_AllGoodStripInfo.h
@@ -14,7 +14,7 @@
 #include "SCT_ConditionsData/SCT_WaferGoodStripInfo.h"
 
 // Definition of the number of elements
-#include "SCT_ConditionsServices/SCT_ConditionsParameters.h"
+#include "SCT_ConditionsData/SCT_ConditionsParameters.h"
 
 typedef boost::array<SCT_WaferGoodStripInfo, SCT_ConditionsServices::NUMBER_OF_WAFERS> SCT_AllGoodStripInfo;
 
--- InnerDetector/InDetConditions/SCT_ConditionsData/SCT_ConditionsData/SCT_ModuleGainCalibData.h
+++ InnerDetector/InDetConditions/SCT_ConditionsData/SCT_ConditionsData/SCT_ModuleGainCalibData.h
@@ -14,7 +14,7 @@
 #include "SCT_ConditionsData/SCT_ModuleCalibParameter.h"
 
 // Definition of the number of elements
-#include "SCT_ConditionsServices/SCT_ConditionsParameters.h"
+#include "SCT_ConditionsData/SCT_ConditionsParameters.h"
 
 typedef boost::array<SCT_ModuleCalibParameter, SCT_ConditionsServices::N_NPTGAIN> SCT_ModuleGainCalibData;
 
