9a545a18807abb9e37c2b62897ce8ca075ce0e3c
James Robinson <james.em.robinson@gmail.com>
2018-03-08 17:19:34 +0100

    Generate 10% more events than maxEvents when maxEvents is specified. 2015-03-09 11:33:46. (PowhegControl-00-00-18)
    Former-commit-id: bb3e4a8727f1b521dc0c4bcd3a8b96a84375d523

--- Generators/PowhegControl/python/PowhegUtils.py
+++ Generators/PowhegControl/python/PowhegUtils.py
@@ -35,7 +35,7 @@ class PowhegConfig_base() :
   # These values can be changed in the jobOptions - defaults are set here.
   # Process-specific options are in the relevant class
   runArgs        = None
-  nEvents        = 5200
+  nEvents        = 5500
   beam_energy    = 4000
   randomSeed     = 1
   mu_F, mu_R     = 1.0, 1.0
@@ -147,7 +147,7 @@ class PowhegConfig_base() :
         self.beam_energy = 0.5 * self.runArgs.ecmEnergy
       if hasattr(self.runArgs,'maxEvents') :
         if self.runArgs.maxEvents > 0 :
-          self.nEvents = self.runArgs.maxEvents
+          self.nEvents = int( 1.1 * self.runArgs.maxEvents + 0.5 )
       if hasattr(self.runArgs,'randomSeed') :
         self.randomSeed = self.runArgs.randomSeed
       # Set inputGeneratorFile to match output events file. Otherwise Generate_trf check will fail.
