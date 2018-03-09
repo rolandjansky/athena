b5deda51c4932c2038a078fc552cb328ae70d18c
Denis Oliveira Damazio <damazio@mail.cern.ch>
2018-03-09 10:34:03 +0100

    cout removal
    Former-commit-id: f158303b6f327c2065586f7583b3b3da90dcbaf2

--- Trigger/TrigAlgorithms/TrigCaloRec/src/HLTCaloCellMaker.cxx
+++ Trigger/TrigAlgorithms/TrigCaloRec/src/HLTCaloCellMaker.cxx
@@ -39,7 +39,6 @@ StatusCode HLTCaloCellMaker::initialize() {
 //StatusCode HLTCaloCellMaker::execute() {
 StatusCode HLTCaloCellMaker::execute_r( const EventContext& context ) const {
 
-  std::cout << "Will try to execute" << std::endl;
 
   SG::WriteHandle<ConstDataVector<CaloCellContainerVector> > cellContainerV( m_cellContainerVKey, context );
   auto cdv = CxxUtils::make_unique<ConstDataVector<CaloCellContainerVector> >();
@@ -48,30 +47,24 @@ StatusCode HLTCaloCellMaker::execute_r( const EventContext& context ) const {
   auto roisHandle = SG::makeHandle( m_roiCollectionKey );
   const TrigRoiDescriptorCollection* roiCollection = roisHandle.cptr();
 
-  std::cout << "still here" << roiCollection->size() << std::endl;
 
   // datahandle 
   for( const TrigRoiDescriptor* roiDescriptor : *roiCollection) {
-  	std::cout << "still here roi" << std::endl;
 	if ( roiDescriptor->isFullscan() ) {
-  		std::cout << "still here roi FS" << std::endl;
 		auto c = std::make_unique<ConstDataVector<CaloCellContainer> >(SG::VIEW_ELEMENTS);
 		m_dataAccessSvc->loadFullCollections( context, *c );
 		cellContainerV->push_back( c.release()->asDataVector() );
 		
 	} else {
-  		std::cout << "still here roi RoI" << std::endl;
 		LArTT_Selector<LArCellCont> sel;
 		m_dataAccessSvc->loadCollections( context, *roiDescriptor, TTEM, 2, sel );
 		auto c = std::make_unique<ConstDataVector<CaloCellContainer> >(SG::VIEW_ELEMENTS);
 		int cc(0);
 		for( const auto cell : sel ) {c->push_back( cell ); cc++;}
-		std::cout << "RoI size in cells : " << cc << std::endl;
 		cellContainerV->push_back( c.release()->asDataVector() );
 	}
   }
 
-  std::cout << "Finishing" << std::endl;
 
    
   return StatusCode::SUCCESS;
