860c3e651594a17c06bdc5b2c436039acf54695c
Jennifer Kathryn Roloff <jroloff2@gmail.com>
2018-03-09 13:09:11 +0100

    Fixing issue with LArDigitGetter configuration
    Former-commit-id: 604626d23f0722c41a03ae3cad67957516581701

--- LArCalorimeter/LArDigitization/python/LArDigitGetter.py
+++ LArCalorimeter/LArDigitization/python/LArDigitGetter.py
@@ -8,7 +8,9 @@ from RecExConfig.Configured import Configured
 
 class LArDigitGetter (Configured) :
     _outputType = "LArDigitContainer"
+    _outputType_DigiHSTruth = "LArDigitContainer_DigiHSTruth"
     _output = { _outputType : "LArDigitContainer_MC" }
+    _output_DigiHSTruth = { _outputType_DigiHSTruth : "LArDigitContainer_DigiHSTruth" }
 
     def configure(self):
         mlog = logging.getLogger( 'LArDigitGetter.py::configure :' )
@@ -52,8 +54,15 @@ class LArDigitGetter (Configured) :
     def outputKey(cls):
         return cls._output[cls._outputType]
 
+    def outputKey_DigiHSTruth(cls):
+        return cls._output[cls._outputType_DigiHSTruth]
+
     def outputType(cls):
         return cls._outputType
 
     def outputTypeKey(self):
         return str(self.outputType()+"#"+self.outputKey())
+
+    def outputTypeKey_DigiHSTruth(self):
+        return str(self.outputType()+"#"+self.outputKey_DigiHSTruth())
+
