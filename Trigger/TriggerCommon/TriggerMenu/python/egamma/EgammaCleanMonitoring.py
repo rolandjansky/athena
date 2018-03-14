5880c6161192572f174788dd4569504f137bc4a0
fernando <Fernando.Monticelli@cern.ch>
2018-03-14 18:20:34 +0100

    Added Jpsiee chain in EgammaCleanMonitoring
    Former-commit-id: bf75ed36bb0c82e5261812a3f5ee42cb2a116bc7

--- Trigger/TriggerCommon/TriggerMenu/python/egamma/EgammaCleanMonitoring.py
+++ Trigger/TriggerCommon/TriggerMenu/python/egamma/EgammaCleanMonitoring.py
@@ -2,7 +2,10 @@
 
 
 EgammaChainsToKeepMonitoring={
-# ES triggers
-"e26_lhtight_nod0_ivarloose",
-"g35_medium_g25_medium_L12EM20VH",
+"e26_lhtight",
+"g22_tight",
+"g35_medium",
+"e60_lhmedium_nod0",
+"e17_lhvloose_nod0",
+"e5_lhtight_nod0_e4_etcut_Jpsiee_L1JPSI-1M5",
 }
