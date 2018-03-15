9d2fd9396ea13268332ee707901e2ef05ce06806
Ishan Pokharel <ispokhar@lxplus001.cern.ch>
2018-03-15 10:15:48 +0100

    Added TriggerAPI. Further file size tests needs to be done in master
    Former-commit-id: b82f7bc8595c029cb4d9bb03abddd72f5e253077

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/TriggerLists.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/TriggerLists.py
@@ -2,29 +2,46 @@
 from TriggerMenu.api.TriggerAPI import TriggerAPI
 from TriggerMenu.api.TriggerEnums import TriggerPeriod, TriggerType
 
-# jet triggers (single Jets)
-jetTriggers = TriggerAPI.getAllHLT(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.j_single)
+# jet triggers (single Jets, prescaled and unprescaled)
+def single_jetTrig():
+	jetTrig = TriggerAPI.getAllHLT(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.j_single)
+	API_jetTrig = list(jetTrig.keys())
+	return API_jetTrig
+# electron triggers (unprescaled)
+def single_el_Trig():
+	singleElTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.el_single)
+	API_singleElTriggers = list(singleElTriggers.keys())
+	return API_singleElTriggers
 
-# single electron triggers (unprescaled)
+def multi_el_Trig():
+	multiElTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.el_multi)
+	API_multiElTriggers = list(multiElTriggers.keys())
+	return API_multiElTriggers
 
-singleElTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.el_single)
+# single muon triggers (unprescaled)
+def single_mu_Trig():
+	singleMuTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.mu_single)
+	API_singleMuTriggers = list(singleMuTriggers.keys())
+	return API_singleMuTriggers
 
-# single muon triggers
+def multi_mu_Trig():
+	multiMuTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.mu_multi)
+	API_multiMuTriggers = list(multiMuTriggers.keys())
+	return API_multiMuTriggers
 
-singleMuTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.mu_single)
+# xe triggers (prescaled and unprescaled)
+def MET_Trig():
+	MET_Triggers = TriggerAPI.getAllHLT(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.xe)
+	API_MET_Triggers = list(MET_Triggers,keys())
+	return API_MET_Triggers
 
-# xe triggers
-metTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.xe)
+# photon triggers (prescaled and unprescaled)
+def single_photon_Trig():
+	singlePhotonTriggers = TriggerAPI.getAllHLT(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.g_single)
+	API_singlePhotonTriggers = list(singlePhotonTriggers.keys())
+	return API_singlePhotonTriggers
 
-#multi-muon triggers
-
-multiMuTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.mu_multi)
-
-# multi-el triggers
-
-multiElTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.el_multi)
-
-# photon triggers
-singlePhotonTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.g_single)
-
-multiPhotonTriggers = TriggerAPI.getUnprescaled(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.g_multi)
+def multi_photon_Trig():
+	multiPhotonTriggers = TriggerAPI.getAllHLT(TriggerPeriod.y2015 | TriggerPeriod.y2016 | TriggerPeriod.y2017, TriggerType.g_multi)
+	API_multiPhotonTriggers = list(multiPhotonTriggers.keys())
+	return API_multiPhotonTriggers
