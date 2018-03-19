55579971914d1e12ccacbd37d7b9162724e96c94
sutt <sutt@cern.ch>
2018-03-19 09:53:05 +0100

    Use the phi roi containment by default
    When disabling the rigorous containment by default, the phi Roi containment
    was also disabled by default. These changes allow separate setting of the full
    containment and the phi containment - the full containment is disabled by default,
    the phi containment, is enabled by default.
    
    Also two additional histograms were added to the Tier0 monitoring.
    
    
    Former-commit-id: 31e95b3c9cdcfdb036a4993166468f945ac91816

--- Trigger/TrigAnalysis/TrigInDetAnalysisUtils/TrigInDetAnalysisUtils/Filters.h
+++ Trigger/TrigAnalysis/TrigInDetAnalysisUtils/TrigInDetAnalysisUtils/Filters.h
@@ -230,6 +230,7 @@ public:
   Filter_Combined( TrackFilter* f1, TrackFilter* f2) : 
     mf1(f1), mf2(f2), m_roi(0), 
     m_debugPrintout(false), 
+    m_containPhi(true),
     m_contain(false)
   { } 
 
@@ -240,6 +241,11 @@ public:
   /// should be fully contained in the RoI or not 
 
   void containtracks( bool b=true ) { m_contain=b; }
+
+  /// set / unset to allow the strict phi containment to be
+  /// used even if the full rigorous containement is not 
+
+  void containtracksPhi( bool b=true ) { m_containPhi=b; }
 
 
   bool contains( const TIDA::Track* t, const TIDARoiDescriptor* r ) const { 
@@ -269,7 +275,7 @@ public:
       ///     within an Roi but failthis condition
       bool contained_eta = ( t->eta()<r->etaPlus() && t->eta()>r->etaMinus() );
       
-      if ( m_contain ) { 
+      if ( m_contain || m_containPhi ) { 
 
 	///  includes calculation of approximate z position of the 
 	///  track at radius r and test if track within that z position at radius r 
@@ -287,9 +293,11 @@ public:
  
 	double cross0 = zexit*r->rMinusZed() - rexit*r->zedMinusR();
 	double cross1 = zexit*r->rPlusZed()  - rexit*r->zedPlusR(); 
-
-	if ( cross0>0 && cross1<0 ) contained_eta=true;
-	else                        contained_eta=false;
+	
+	if ( m_contain ) {  
+	  if ( cross0>0 && cross1<0 ) contained_eta=true;
+	  else                        contained_eta=false;
+	}
 
 	/// now check phi taking account of the track transverse curvature
 
@@ -380,6 +388,7 @@ private:
 
   bool  m_debugPrintout;
 
+  bool   m_containPhi;
   bool   m_contain;
 
 };
