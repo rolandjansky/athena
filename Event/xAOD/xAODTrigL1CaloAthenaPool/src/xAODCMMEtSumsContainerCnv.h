0b44971968ed6b31f88150506e15a0af3651d99e
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2018-03-19 19:04:26 +0000

    Merge branch 'l1calo-derivation-fix' into '21.0'
    Changes to allow for the correct production of L1CALO1 derived xAODs with thinned TriggerTower containers
    
    See merge request atlas/athena!9750
    
    (cherry picked from commit 148cf937a60a45146432d4b09f718c002b173d67 [formerly c07cad3272dbf4bf00658a7e6ecce76acf954d00])
    
    6de795a2 Fixed bug in production of L1CALO1 derivation files by switching to recommended template classes
    
    Former-commit-id: 108c510ec2667735a823f8d8d710ef9ae0ebd116

--- Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODCMMEtSumsContainerCnv.h
+++ Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODCMMEtSumsContainerCnv.h
@@ -5,52 +5,11 @@
 #ifndef XAODTRIGL1CALOATHENAPOOL_XAODCMMETSUMSCONTAINERCNV_H
 #define XAODTRIGL1CALOATHENAPOOL_XAODCMMETSUMSCONTAINERCNV_H
 
-// Gaudi/Athena include(s):
-#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
 
-// EDM include(s):
+#include "AthenaPoolCnvSvc/T_AthenaPoolxAODCnv.h"
 #include "xAODTrigL1Calo/CMMEtSumsContainer.h"
 
-/// Type definition for the converter's base
-typedef T_AthenaPoolCustomCnv< xAOD::CMMEtSumsContainer,
-                               xAOD::CMMEtSumsContainer >
-   xAODCMMEtSumsContainerCnvBase;
-
-/**
- *  @short POOL converter for the xAOD::CMMEtSumsContainer class
- *
- *         Simple converter class making the xAOD::CMMEtSumsContainer
- *         class known to POOL.
- *
- * @author Edward Moyse <Edward.Moyse@cern.ch>
- * @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
- * @author John Morris <john.morris@cern.ch>
- *
- */
-class xAODCMMEtSumsContainerCnv : public xAODCMMEtSumsContainerCnvBase {
-
-   // Declare the factory as our friend:
-   friend class CnvFactory< xAODCMMEtSumsContainerCnv >;
-
-public:
-   /// Converter constructor
-   xAODCMMEtSumsContainerCnv( ISvcLocator* svcLoc );
-
-   /// Re-implemented function in order to get access to the SG key
-   virtual StatusCode createObj( IOpaqueAddress* pAddr, DataObject*& pObj );    
-   /// Function preparing the container to be written out
-   virtual xAOD::CMMEtSumsContainer*
-   createPersistent( xAOD::CMMEtSumsContainer* trans );
-   /// Function reading in the persistent object
-   virtual xAOD::CMMEtSumsContainer* createTransient();
-
-private:
-   /// Function preparing a vertex object for persistence
-   void toPersistent( xAOD::CMMEtSums* cluster ) const;
-   
-   /// StoreGate key of the container just being created
-   std::string m_key;
-
-}; // class xAODCMMEtSumsContainerCnv
+typedef T_AthenaPoolxAODCnv<xAOD::CMMEtSumsContainer>
+  xAODCMMEtSumsContainerCnv;
 
 #endif 
--- Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODRODHeaderAuxContainerCnv.h
+++ Event/xAOD/xAODTrigL1CaloAthenaPool/src/xAODRODHeaderAuxContainerCnv.h
@@ -5,48 +5,10 @@
 #ifndef XAODTRIGL1CALOATHENAPOOL_XAODRODHEADERAUXCONTAINERCNV_H
 #define XAODTRIGL1CALOATHENAPOOL_XAODRODHEADERAUXCONTAINERCNV_H
 
-// Gaudi/Athena include(s):
-#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
-
-// EDM include(s):
+#include "AthenaPoolCnvSvc/T_AthenaPoolAuxContainerCnv.h"
 #include "xAODTrigL1Calo/RODHeaderAuxContainer.h"
 
-/// Base class for the converter
-typedef T_AthenaPoolCustomCnv< xAOD::RODHeaderAuxContainer,
-                               xAOD::RODHeaderAuxContainer >
-   xAODRODHeaderAuxContainerCnvBase;
-
-/**
- *  @short POOL converter for the xAOD::RODHeaderAuxContainer class
- *
- *         This is the converter doing the actual schema evolution
- *         of the package... The converter for xAOD::RODHeaderContainer
- *         doesn't do much, as the "interface classes" don't contain
- *         too much/any payload. Most of the payload is in the auxiliary
- *         containers like this one.
- *
- * @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
- * @author John Morris <john.morris@cern.ch>
- *
- * $Revision: 576323 $
- * $Date: 2013-12-19 17:07:14 +0100 (Thu, 19 Dec 2013) $
- */
-class xAODRODHeaderAuxContainerCnv :
-   public xAODRODHeaderAuxContainerCnvBase {
-
-   // Declare the factory as our friend:
-   friend class CnvFactory< xAODRODHeaderAuxContainerCnv >;
-
-protected:
-   /// Converter constructor
-   xAODRODHeaderAuxContainerCnv( ISvcLocator* svcLoc );
-
-   /// Function preparing the container to be written out
-   virtual xAOD::RODHeaderAuxContainer*
-   createPersistent( xAOD::RODHeaderAuxContainer* trans );
-   /// Function reading in the object from the input file
-   virtual xAOD::RODHeaderAuxContainer* createTransient();
-
-}; // class xAODRODHeaderAuxContainerCnv
-
-#endif // XAODTRIGL1CALOATHENAPOOL_XAODRODHEADERAUXCONTAINERCNV_H
+typedef T_AthenaPoolAuxContainerCnv<xAOD::RODHeaderAuxContainer>
+  xAODRODHeaderAuxContainerCnv;
+  
+#endif
