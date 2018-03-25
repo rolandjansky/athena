945bb62b561945c691b742b7efdee24185036249
James Richard Catmore <james.catmore@cern.ch>
2018-03-25 15:16:32 +0200

    Removing obsolete tests from DerivationFrameworkART
    See above - no changes to C++ or python, just configuration of ART tests
    
    
    Former-commit-id: ea8416ee0ec259129c927bc015f5f5fd91304a89

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16HION9.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# art-description: DAOD building HION9 mc16
+# art-type: grid
+# art-output: *.pool.root
+# art-output: checkFile.txt
+# art-output: checkxAOD.txt
+
+set -e
+
+Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.12169019._004055.pool.root.1 --outputDAODFile art.pool.root --reductionConf HION9 --maxEvents -1 --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-40" ' 
+
+DAODMerge_tf.py --inputDAOD_HION9File DAOD_HION9.art.pool.root --outputDAOD_HION9_MRGFile art_merged.pool.root
+checkFile.py DAOD_HION9.art.pool.root > checkFile.txt
+checkxAOD.py DAOD_HION9.art.pool.root > checkxAOD.txt
