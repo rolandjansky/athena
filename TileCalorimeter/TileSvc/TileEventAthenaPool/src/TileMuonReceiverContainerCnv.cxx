b3155f7bfeef2b47089db5b450faf54e245f8832
Alexander Solodkov <Sanya.Solodkov@cern.ch>
2018-03-28 14:01:13 +0200

    Storing TileDigits in POOL file as floats
    Former-commit-id: 1fbb5e505d2dd35ab88fc04d9d37b0fee16e6102

--- TileCalorimeter/TileSvc/TileEventAthenaPool/src/TileMuonReceiverContainerCnv.cxx
+++ TileCalorimeter/TileSvc/TileEventAthenaPool/src/TileMuonReceiverContainerCnv.cxx
@@ -28,7 +28,7 @@ TileMuonReceiverContainerCnv::createPersistent( TileMuonReceiverContainer* trans
   TileMuonReceiverContainer_PERS *persObj = cnv.createPersistent( transCont, msg );
 
   if (msg.level()<=MSG::DEBUG)
-    msg << MSG::DEBUG << "::createPersistent [Success]" << endreq;
+    msg << MSG::DEBUG << "::createPersistent [Success]" << endmsg;
   return persObj; 
 }
 
