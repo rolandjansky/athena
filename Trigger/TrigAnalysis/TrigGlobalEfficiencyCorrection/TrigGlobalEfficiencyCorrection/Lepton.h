9b26a7ea1167697fbf7d77faf7f286afad54cef6
Julien Maurer <julien.maurer@cern.ch>
2018-03-28 15:06:40 +0300

    including xAOD::Electron/Photon headers instead of relying on forward declarations
    this allows changing the reinterpret_cast's into static_cast's (or nothing)
    
    
    Former-commit-id: 446a63dc61c1cc8aa421b5c801ee92db04f649dd

--- Trigger/TrigAnalysis/TrigGlobalEfficiencyCorrection/TrigGlobalEfficiencyCorrection/Lepton.h
+++ Trigger/TrigAnalysis/TrigGlobalEfficiencyCorrection/TrigGlobalEfficiencyCorrection/Lepton.h
@@ -7,6 +7,8 @@
 #ifndef TRIGGLOBALEFFICIENCYCORRECTION_LEPTON_H
 #define TRIGGLOBALEFFICIENCYCORRECTION_LEPTON_H 1
 
+#include "xAODEgamma/Electron.h"
+#include "xAODEgamma/Photon.h"
 
 namespace TrigGlobEffCorr
 {
@@ -15,8 +17,8 @@ class Lepton
 {
 public:
 	Lepton(const xAOD::IParticle* ptr, std::size_t tag=0) : m_obj(ptr), m_tag(tag) {}
-	Lepton(const xAOD::Electron* ptr, std::size_t tag=0) : m_obj(reinterpret_cast<const xAOD::IParticle*>(ptr)), m_tag(tag) {}
-	Lepton(const xAOD::Photon* ptr, std::size_t tag=0) : m_obj(reinterpret_cast<const xAOD::IParticle*>(ptr)), m_tag(tag) {}
+	Lepton(const xAOD::Electron* ptr, std::size_t tag=0) : m_obj(ptr), m_tag(tag) {}
+	Lepton(const xAOD::Photon* ptr, std::size_t tag=0) : m_obj(ptr), m_tag(tag) {}
 	Lepton(const Lepton&) = default;
 	Lepton& operator=(const Lepton&) = default;
 	Lepton(Lepton&&) = default;
@@ -24,9 +26,9 @@ public:
 	float pt() const { return m_obj->pt(); }
 	xAOD::Type::ObjectType type() const { return m_obj->type(); }
 	std::size_t tag() const { return m_tag; }
-	const xAOD::Electron* electron() const { return reinterpret_cast<const xAOD::Electron*>(m_obj); }
+	const xAOD::Electron* electron() const { return static_cast<const xAOD::Electron*>(m_obj); }
 	const xAOD::Muon* muon() const { return static_cast<const xAOD::Muon*>(m_obj); }
-	const xAOD::Photon* photon() const { return reinterpret_cast<const xAOD::Photon*>(m_obj); }
+	const xAOD::Photon* photon() const { return static_cast<const xAOD::Photon*>(m_obj); }
 	const xAOD::IParticle* particle() const { return m_obj; }
 	bool operator<(const Lepton& rhs) const { return m_obj<rhs.m_obj; }
 protected:
