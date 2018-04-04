5b7d4fb3ad48b777679d8a29416f033a4e9834fd
Margherita Spalla <margherita.spalla@cern.ch>
2018-04-04 13:56:32 +0200

    Dummy algorithm monitoring correlations. Added file LArCalorimeter/LArMonTools/share/LArNoiseCorrelationMon_jobOptions.py
    Former-commit-id: 18509ff66d9777f123f3e82477b44c4c3d37bd97

---
+++ LArCalorimeter/LArMonTools/share/LArNoiseCorrelationMon_jobOptions.py
@@ -0,0 +1,61 @@
+from LArCellRec.LArNoisyROFlags import larNoisyROFlags
+
+# --- for athena online running ---
+if 'EventBlockSize' not in dir():
+    EventBlockSize=0
+
+#Run is Online or Offline:
+OnlineMode=athenaCommonFlags.isOnline()
+
+#retrieve the condition for the run in cool offline, in IS online
+if OnlineMode:
+    peakSample_expected = FirstSample
+    LArSampleNumber = NSamples   
+else:
+    from LArConditionsCommon.LArCool import larcool
+    if (larcool is not None):
+        peakSample_expected = larcool.firstSample()
+        LArSampleNumber = larcool.nSamples()
+    else:
+        peakSample_expected = 0
+        LArSampleNumber = 0
+
+#Run is Cosmic or Beam:
+CosmicMode=( jobproperties.Beam.beamType()== 'cosmics')
+
+#Declare stream if Cosmic or Beam:
+if CosmicMode:
+	StreamsToMonitor = ["CosmicCalo","Standby","LArCellsEmpty","Background"]
+else:
+	StreamsToMonitor = ["CosmicCalo","ZeroBias","Standby","express","Main","L1Calo","L1Topo","LArCells","LArCellsEmpty","Background","LArCellsEmpty"]
+
+
+###### LArNoiseCorrelationMon Configuration ###############
+from LArMonTools.LArMonToolsConf import LArNoiseCorrelationMon
+theLArNoiseCorrelationMon = LArNoiseCorrelationMon(name="LArNoiseCorrelationMon",
+#                             OutputLevel=VERBOSE,
+                             LArDigitContainerKey  = LArMonFlags.LArDigitKey(),
+                             IsOnline              = OnlineMode,
+#                             SampleRangeLow        = 5, # If no value set, range defined 
+#                             SampleRangeUp         = 7, # dynamically in the code
+#                             ExpectedSampleMax     = peakSample_expected,
+#                             SampleNumberFromDB    = LArSampleNumber,
+#                             ComputeHistError      = False,
+                             IgnoreBadChannels     = True,
+                             LArBadChannelMask     = theLArBadChannelsMasker,
+                             ProcessNEvents        = EventBlockSize,
+#                             NumberBadFebs         = larNoisyROFlags.BadFEBCut(),
+#                             Streams = StreamsToMonitor
+                             )
+
+from AthenaCommon.BeamFlags import jobproperties
+if jobproperties.Global.DataSource.get_Value() == 'data':
+    theLArNoiseCorrelationMon.LArPedestalKey='Pedestal'
+else:
+    theLArNoiseCorrelationMon.LArPedestalKey='LArPedestal'
+                
+
+ToolSvc += theLArNoiseCorrelationMon
+LArMon.AthenaMonTools+=[ theLArNoiseCorrelationMon ] 
+
+
