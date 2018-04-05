5d4e094c24d68763a6445f2ec3c015b451cff7d1
Paul Gessinger <hello@paulgessinger.com>
2018-04-05 17:17:33 +0200

    job options cleanup
    Former-commit-id: b9de7132895db8cd512920a9836456441564ade9

--- GeomACTS/share/ACTSTrackingGeometry.py
+++ GeomACTS/share/ACTSTrackingGeometry.py
@@ -36,17 +36,10 @@ from IOVDbSvc.CondDB import conddb
 conddb.setGlobalTag('OFLCOND-SIM-00-00-00')
 conddb.addOverride("/Indet/Align", "InDetAlign_R2_Nominal")
 
-# Use MagneticFieldSvc
-# include( "BFieldAth/BFieldAth_jobOptions.py" )
-
-# from AthenaCommon.CfgGetter import getService
-# getService('TrackingGeometrySvc')
 
 from AthenaCommon.AppMgr import ServiceMgr
 from GeomACTS.GeomACTSConfig import TrackingGeometrySvc
 ServiceMgr += TrackingGeometrySvc()
-# trkGeomSvc = CfgMgr.Acts__TrackingGeometrySvc("TrackingGeometrySvc")
-# ServiceMgr += trkGeomSvc
 
 import MagFieldServices.SetupField
 
@@ -54,18 +47,6 @@ from AthenaCommon.AlgSequence import AlgSequence
 job = AlgSequence()
 
 # Load algorithms
-
-# from InDetDetDescrExample.InDetDetDescrExampleConf import ACTSTrackingGeometry
-
-# job += ACTSTrackingGeometry()
-#job.ACTSTrackingGeometry.ModulesOnly = True
-#job.ACTSTrackingGeometry.ExpandId = True
-#job.ACTSTrackingGeometry.NominalPosition = True
-#job.ACTSTrackingGeometry.AlignedPosition = False
-#job.ACTSTrackingGeometry.FullRotationMatrix = False
-#job.ACTSTrackingGeometry.OutputFile = "geometry.dat"
-# print job.ACTSTrackingGeometry
-
 # include("GeneratorUtils/StdEvgenSetup.py")
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
 athenaCommonFlags.PoolEvgenInput.set_Off()
@@ -82,23 +63,7 @@ alg.ExtrapolationTool = exTool
 # alg.OutputLevel = DEBUG
 job += alg
 
-from G4AtlasApps.SimFlags import simFlags
-simFlags.OptionalUserActionList.addAction('G4UA::VolumeDebuggerTool',['BeginOfRun'])
 
-# from AthenaCommon.CfgGetter import getPublicTool
-# getPublicTool('G4UA::VolumeDebuggerTool', tryDefaultConfigurable=True).OutputPath="./test.gdml"
-# getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='IDET::IDET'
-# getPublicTool('G4UA::VolumeDebuggerTool').Resolution=1000000 # Default = 1000$
-# getPublicTool('G4UA::VolumeDebuggerTool').Tolerance=0.000001
-# getPublicTool('G4UA::VolumeDebuggerTool').Verbose=True
-# getPublicTool('G4UA::VolumeDebuggerTool').RunGeoTest=False
-
-# from G4AtlasApps.PyG4Atlas import PyG4AtlasAlg
-# job += PyG4AtlasAlg()
-
-# #--------------------------------------------------------------
-# Set output level threshold (2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL )
-#--------------------------------------------------------------
 # job.ACTSTrackingGeometry.OutputLevel = INFO
 
 
