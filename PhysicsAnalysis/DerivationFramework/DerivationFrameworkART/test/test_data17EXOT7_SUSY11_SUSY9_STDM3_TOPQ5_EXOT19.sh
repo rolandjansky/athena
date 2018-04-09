b754ee6a9037527c46c48fdcd6c079bae156cb59
James Richard Catmore <james.catmore@cern.ch>
2018-04-09 14:01:21 +0200

    Updating train configurations in DerivationFrameworkART
    The trains running in the ART tests are updated to reflect more recent sizes. These trains will be used for a large-scale test before we start 2018 data taking.
    
    
    Former-commit-id: 3aa9e6910e522e8b462a0b6a40d70e724c16e62a

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data17EXOT7_SUSY11_SUSY9_STDM3_TOPQ5_EXOT19.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# art-description: DAOD building EXOT7 SUSY11 SUSY9 STDM3 TOPQ5 EXOT19 data17
+# art-type: grid
+# art-output: *.pool.root
+
+set -e
+
+Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.12818484._004694.pool.root.1 --outputDAODFile art.pool.root --reductionConf EXOT7 SUSY11 SUSY9 STDM3 TOPQ5 EXOT19 --maxEvents 500  --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12Onl-08-40" '  --passThrough True 
