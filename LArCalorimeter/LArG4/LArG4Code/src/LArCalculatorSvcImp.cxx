8d99d647b081b9b09ebf73f24b73eda5b6a23877
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2018-04-10 16:06:37 +0000

    Merge branch 'LArCalculatorTidying_21.0' into '21.0'
    Syntax update for interfaces and base classes of LAr Calculator Services
    
    See merge request atlas/athena!10316
    
    (cherry picked from commit 82eff9f9a672693ee1dfdec17d266358ca3af645 [formerly 59955505669e9c3253befd8733b2a480e7257518])
    
    0c8965fc Clean-up whitespace in LArG4Code interface headers
    d488ebab Migrate LArG4Code Calculator Interface classes to use DeclareInterfaceID pattern
    2fe1abf0 Migrate LArCalculatorSvcImp and LArCalibCalculatorSvcImp to use extends pattern
    
    Former-commit-id: 7adfee7bec0702f9f5f03e0656ce5a1bb0107817

--- LArCalorimeter/LArG4/LArG4Code/src/LArCalculatorSvcImp.cxx
+++ LArCalorimeter/LArG4/LArG4Code/src/LArCalculatorSvcImp.cxx
@@ -7,7 +7,7 @@
 #include "CLHEP/Units/SystemOfUnits.h"
 
 LArCalculatorSvcImp::LArCalculatorSvcImp(const std::string& name, ISvcLocator *pSvcLocator)
-  : AthService(name, pSvcLocator)
+  : base_class(name, pSvcLocator)
   , m_BirksLaw(true)
   , m_Birksk(0.0486)
   , m_OOTcut(300*CLHEP::ns)
@@ -15,16 +15,4 @@ LArCalculatorSvcImp::LArCalculatorSvcImp(const std::string& name, ISvcLocator *p
    declareProperty("BirksLaw",m_BirksLaw);
    declareProperty("Birksk",m_Birksk);
    declareProperty("OOTcut",m_OOTcut);
-}
-
-StatusCode LArCalculatorSvcImp::queryInterface( const InterfaceID & riid,  void** ppvInterface )
-{
-  if ( ILArCalculatorSvc::interfaceID().versionMatch(riid) ) {
-    *ppvInterface = dynamic_cast<ILArCalculatorSvc*>(this);
-  } else {
-    // Interface is not directly available : try out a base class
-    return AthService::queryInterface(riid, ppvInterface);
-  }
-  addRef();
-  return StatusCode::SUCCESS;
 }
