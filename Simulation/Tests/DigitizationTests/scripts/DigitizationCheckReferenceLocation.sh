190ab2186c3833fd6b6b6fac9f830b0c2f90481d
Iain Bertram <iain.bertram@cern.ch>
2018-04-11 10:08:52 +0100

    Updated Digitizations tests to latest version to allow running with Ant (ATLASSIM-3607)
      Just copied files from master branch. No change to any executable or
      settings. Should not chnage output. Allows the RTT tests to be turned off
      freeing up resources
    
    
    Former-commit-id: a2ef3b2423474682f22d7601bc3ab5695256bbea

--- Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
+++ Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
@@ -5,6 +5,8 @@ if [[ $ATLAS_RELEASE_BASE == *"20.7.X.Y-VAL"* ]]; then
     DigitizationTestsVersion="20.7.X.Y-VAL"
 elif [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then
     DigitizationTestsVersion="20.7.X"
+elif [[ $ATLAS_RELEASE_BASE == *"20.9"* ]]; then
+    DigitizationTestsVersion="20.9"
 elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then
     DigitizationTestsVersion="20.20.X"
 elif [[ $ATLAS_RELEASE_BASE == *"mig7"* ]]; then
@@ -15,6 +17,4 @@ elif [[ $ATLAS_RELEASE_BASE == *"20.1"* ]]; then
     DigitizationTestsVersion="20.1.X"
 elif [[ $ATLAS_RELEASE_BASE == *"21.0"* ]]; then
     DigitizationTestsVersion="21.0.X"
-elif [[ $ATLAS_RELEASE_BASE == *"21.3"* ]]; then
-    DigitizationTestsVersion="21.3.X"
 fi
