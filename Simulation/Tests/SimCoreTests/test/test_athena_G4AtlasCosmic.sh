e5314a0c6f7a7931561b83a7dfdb66d1bd38fff3
John Chapman <jchapman@cern.ch>
2018-04-11 12:31:36 +0200

    Update ART tests for SimCoreTests and ISF_Validation packages
    ART Tests have been updated to only run for certain branch/project
    combinations and have also been updated to save output root files
    where useful.
    
    
    Former-commit-id: 5899b8d6743cd4545a78fcb8adbba48665f282ea

--- Simulation/Tests/SimCoreTests/test/test_athena_G4AtlasCosmic.sh
+++ Simulation/Tests/SimCoreTests/test/test_athena_G4AtlasCosmic.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
 # art-description: Run cosmics simulation outside ISF from the athena command-line, generating events on-the-fly
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
+# art-include: master/Athena
 # art-type: grid
 
 athena --preloadlib=${ATLASMKLLIBDIR_PRELOAD}/libintlc.so.5:${ATLASMKLLIBDIR_PRELOAD}/libimf.so G4AtlasApps/jobOptions.G4Cosmic.py
