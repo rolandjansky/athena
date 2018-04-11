37a386d9a7f6f70c8ce8e3fc8c5826a05374b0f1
Iain Bertram <iain.bertram@cern.ch>
2018-04-11 14:42:40 +0100

    Merge digitizations Ant tests from master into 21.9 (ATLASSIM-3607)
    Update digitiation tests in 21.9 to RTT can be shut down and complete
    migration.
    
    No change to executables or settings in release.
    
    
    Former-commit-id: 193dcf67e8bd1b0869f4f1b01d5f8b6c3ac0c4ae

--- Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
+++ Simulation/Tests/DigitizationTests/scripts/DigitizationCheckReferenceLocation.sh
@@ -5,6 +5,8 @@ if [[ $ATLAS_RELEASE_BASE == *"20.7.X.Y-VAL"* ]]; then
     DigitizationTestsVersion="20.7.X.Y-VAL"
 elif [[ $ATLAS_RELEASE_BASE == *"20.7"* ]]; then
     DigitizationTestsVersion="20.7.X"
+elif [[ $ATLAS_RELEASE_BASE == *"20.9"* ]]; then
+    DigitizationTestsVersion="20.9"
 elif [[ $ATLAS_RELEASE_BASE == *"20.20"* ]]; then
     DigitizationTestsVersion="20.20.X"
 elif [[ $ATLAS_RELEASE_BASE == *"mig7"* ]]; then
