96ee3c4c6ff4206c3fd8b0427ccd08a6e1129e4a
Tomasz Bold <tomasz.bold@gmail.com>
2018-04-13 13:42:49 +0200

    Trying adding muons
    Former-commit-id: 38bd103d9c6715b5e79bf7b4a344c25014aceb29

--- Trigger/TrigValidation/TrigUpgradeTest/share/newJOtest.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/newJOtest.py
@@ -18,7 +18,7 @@ flags.set( "global.InputFiles",
            ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"] )
 
 flags.set( "Trigger.inputLVL1ConfigFile", "LVL1config_Physics_pp_v7.xml" )
-flags.set( "Trigger.L1Decoder.doMuon", False )
+flags.set( "Trigger.L1Decoder.doMuon", True )
 
 flags.lock()
 
@@ -35,6 +35,9 @@ acc.addConfig( GeoModelCfg, flags )
 
 # that is how the L1 decoder can be added but it needs more work to bring all needed services (i.e. muon rois decoding)
 acc.addSequence( seqOR( "hltTop") )
+
+
+
 from L1Decoder.L1DecoderConfig import L1DecoderCfg
 acc.addConfig( L1DecoderCfg, flags, sequence="hltTop" )
 l1 = acc.getEventAlgo( "L1Decoder" )
