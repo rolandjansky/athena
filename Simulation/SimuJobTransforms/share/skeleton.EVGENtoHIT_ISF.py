43333dff00523538bad7b240971ee4a7c50637ae
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2018-04-14 02:09:47 +0200

    Transform support for Event Service (ATLASJT-379). Manual cherry-pick of !9923
    Former-commit-id: 3ec09ad3ccfe13db98d3b492fce97bfd6834403e

--- Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_ISF.py
+++ Simulation/SimuJobTransforms/share/skeleton.EVGENtoHIT_ISF.py
@@ -114,10 +114,6 @@ if jobproperties.Beam.beamType.get_Value() != 'cosmics':
     if hasattr(runArgs, "outputEVNT_TRFile"):
         include('SimulationJobOptions/preInclude.G4WriteCavern.py')
 
-# Avoid command line preInclude for event service
-if hasattr(runArgs, "eventService") and runArgs.eventService:
-    include('AthenaMP/AthenaMP_EventService.py')
-
 from ISF_Config.ISF_jobProperties import ISF_Flags
 if jobproperties.Beam.beamType.get_Value() == 'cosmics':
     ISF_Flags.Simulator.set_Value_and_Lock('CosmicsG4')
