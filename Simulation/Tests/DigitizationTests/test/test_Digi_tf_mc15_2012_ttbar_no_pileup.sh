3b9247a349b308acf5c01fa93f94dce2cc4995c8
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-04-16 10:38:33 +0000

    Merge branch '21.3-digitizationtests-ant' into '21.3'
    Incorporate latest ART tests for Digitization into 21.3 (ATLASSIM-3607)
    
    See merge request atlas/athena!10354
    
    (cherry picked from commit c28991a3745293abbcebb60bfb6ae3d91c35cf30 [formerly baced2440e1ef3ded8a2c1284138673f71208667])
    
    a2ef3b24 Updated Digitizations tests to latest version to allow running with Ant (ATLASSIM-3607)
    487d6445 Fix discrepancy between 21.3 and master in reference locations (ATLASSIM-3607).
    
    Former-commit-id: 4c547581f29c3f00781eea2eb7d87250162005dc

--- Simulation/Tests/DigitizationTests/test/test_Digi_tf_mc15_2012_ttbar_no_pileup.sh
+++ Simulation/Tests/DigitizationTests/test/test_Digi_tf_mc15_2012_ttbar_no_pileup.sh
@@ -1,12 +1,41 @@
 #!/bin/sh
 #
 # art-description: Run digitization of an MC15 ttbar sample with 2012 geometry and conditions, without pile-up
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
+# art-include: master/Athena
 # art-type: grid
+# art-output: mc15_2012_ttbar_no_pileup.RDO.pool.root
+
+DigiOutFileName="mc15_2012_ttbar_no_pileup.RDO.pool.root"
 
 
-Digi_tf.py --inputHITSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DigitizationTests/ttbar.ATLAS-R1-2012-03-00-00.HITS.pool.root --outputRDOFile mc15_2012_ttbar.RDO.pool.root --maxEvents 25 --skipEvents 0  --digiSeedOffset1=11 --digiSeedOffset2=22 --geometryVersion ATLAS-R1-2012-03-00-00 --conditionsTag OFLCOND-RUN12-SDR-22 --DataRunNumber 212272 --postInclude 'default:PyJobTransforms/UseFrontier.py'
+
+Digi_tf.py --inputHITSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DigitizationTests/ttbar.ATLAS-R1-2012-03-00-00.HITS.pool.root --outputRDOFile ${DigiOutFileName}  --maxEvents 25 --skipEvents 0  --digiSeedOffset1=11 --digiSeedOffset2=22 --geometryVersion ATLAS-R1-2012-03-00-00 --conditionsTag OFLCOND-RUN12-SDR-22 --DataRunNumber 212272 --postInclude 'default:PyJobTransforms/UseFrontier.py'
+
+echo  "art-result: $? Digi_tf.py"
+
+# get reference directory
+source DigitizationCheckReferenceLocation.sh
+echo "Reference set being used: " ${DigitizationTestsVersion}
+
+# Do reference comparisons
+art-diff.py ./$DigiOutFileName   /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DigitizationTests/ReferenceFiles/$DigitizationTestsVersion/$CMTCONFIG/$DigiOutFileName
+echo  "art-result: $? diff-pool"
+
+
+
+art-diff.py ./$DigiOutFileName /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DigitizationTests/ReferenceFiles/$DigitizationTestsVersion/$CMTCONFIG/$DigiOutFileName --diff-type=diff-root
+echo  "art-result: $? diff-root"
+
+checkFile ./$DigiOutFileName
+echo "art-result: $? checkFile"
+
 
 ArtPackage=$1
 ArtJobName=$2
-# TODO This is a regression test I think. We would also need to compare these files to fixed references and add DCube tests
+
+
 art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
+echo  "art-result: $? art-compare"
