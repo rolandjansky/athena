73e68fd289b45ab6e2604b13d7b07d72d2e3d950
Catrin Bernius <catrin.bernius@cern.ch>
2018-04-17 18:29:48 +0000

    Merge branch 'revert-272d292f' into '21.1'
    Revert "Merge branch 'revert-e93cea3e' into '21.1'"
    
    See merge request atlas/athena!9802
    
    (cherry picked from commit 2bdb87a4b398c220be87a049bee6fca9be923500 [formerly d090b34f825a91f2091113c16c8089a2bee3ea94])
    
    2568a469 Revert "Merge branch 'revert-e93cea3e' into '21.1'"
    
    Former-commit-id: 785dcbd6e46044cd958f0647bda97f3ce427e84f

--- Trigger/TrigEvent/TrigInDetEvent/TrigInDetEvent/TrigSiSpacePoint.h
+++ Trigger/TrigEvent/TrigInDetEvent/TrigInDetEvent/TrigSiSpacePoint.h
@@ -19,8 +19,14 @@ public:
 		     const Identifier& elementId, long layer,
 		     double r,  double phi,  double z,
 		     double dr=0.0, double dphi=0.0, double dz=0.0 ) : 
-    TrigSiSpacePointBase(layer, r, phi, z, dr, dphi, dz), 
+    TrigSiSpacePointBase(layer, r, phi, z, dr, dz), 
     m_clusters(cluster1, 0),
+    m_dphi(dphi),
+    m_locT(0.), m_locL(0.),
+    m_original_r(0.),
+    m_original_phi(0.),
+    m_original_x(0.),
+    m_original_y(0.),
     m_elementId(elementId) {};
 
   // Constructor from two clusters using cylindrical co-ordinates, no. errors  
@@ -29,15 +35,32 @@ public:
 		     const Identifier& elementId, long layer,
 		     double r,  double phi,  double z,
 		     double dr=0.0, double dphi=0.0, double dz=0.0) : 
-    TrigSiSpacePointBase(layer, r, phi, z, dr, dphi, dz), 
+    TrigSiSpacePointBase(layer, r, phi, z, dr, dz), 
     m_clusters(cluster1, cluster2),
+    m_dphi(dphi),
+    m_original_r(0.),
+    m_original_phi(0.),
+    m_original_x(0.),
+    m_original_y(0.),
     m_elementId(elementId) {};
     
  // Destructor
  virtual ~TrigSiSpacePoint() {};
+
+ //Setter methods
+ void locT(const double locT) {m_locT = locT;}
+ void locL(  const double locL  ) {m_locL   = locL;  }
       
  // Methods to return values of data members
  const Identifier& identify() const {return m_elementId;}
+ double dphi() const {return m_dphi;}
+ double locT() const {return m_locT;}
+ double locL() const {return m_locL;}
+ double original_r() const {return  m_original_r;}
+ double original_phi() const {return  m_original_phi;}
+ double original_x() const {return  m_original_x;}
+ double original_y() const {return  m_original_y;}
+
 
  // Methods to retrieve data members 
  std::pair < const InDet::SiCluster*, const InDet::SiCluster* > clusters() const {
@@ -47,6 +70,14 @@ public:
 private:
 
   std::pair<const InDet::SiCluster*, const InDet::SiCluster*> m_clusters;
+  double	m_dphi;
+  double	m_locT;
+  double	m_locL;
+  double  m_original_r; 
+  double  m_original_phi; 
+  double  m_original_x;
+  double  m_original_y;
+
   const Identifier m_elementId;
 };
 
