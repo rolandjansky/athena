b3548eb7b4ecdc3d99a2db19abe441638990598e
James Beacham <j.beacham@cern.ch>
2018-04-30 17:33:51 +0000

    Update test_recentrun_main_2018.sh
    Former-commit-id: 8fcb3f7c5dbe5a4a8903dcf3d916037b20b76b34

--- Tools/Tier0ChainTests/test/test_recentrun_main_2018.sh
+++ Tools/Tier0ChainTests/test/test_recentrun_main_2018.sh
@@ -3,7 +3,7 @@
 # art-description: RecoTrf
 # art-type: grid
 
-Reco_tf.py --AMI=f921 --maxEvents=250 --outputESDFile=myESD_Main.pool.root --outputAODFile=myAOD_Main.AOD.pool.root --outputTAGFile=myTAG_Main.root --outputHISTFile=myMergedMonitoring_Main.root --ignoreErrors=False --inputBSFile=/eos/atlas/atlascerngroupdisk/data-art/grid-input/Tier0ChainTests/data18_13TeV.00348618.physics_Main.daq.RAW._lb0220._SFO-1._0001.data
+Reco_tf.py --AMI=f921 --maxEvents=250 --outputESDFile=myESD_Main.pool.root --outputAODFile=myAOD_Main.AOD.pool.root --outputTAGFile=myTAG_Main.root --outputHISTFile=myMergedMonitoring_Main.root --ignoreErrors=False --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00348618.physics_Main.daq.RAW._lb0220._SFO-1._0001.data
 echo "art-result: $? Reco"
 
 ArtPackage=$1
