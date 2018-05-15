b4272d872a3704004fffa97094516ced244f4275
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2018-05-15 11:13:29 +0200

    Re-wrote CP::detail::ShallowCopy to work on view containers as well.
    This required some serious rewriting unfortunately. The previous nice design, where single objects and containers could be handled by almost the same exact code, had to be broken. As the code for the two cases is very different in the updated implementation.
    
    
    Former-commit-id: 5596cc3cc665bbbbf31bbc937170c226cf09bf72

--- PhysicsAnalysis/Algorithms/SystematicsHandles/SystematicsHandles/CopyHelpers.h
+++ PhysicsAnalysis/Algorithms/SystematicsHandles/SystematicsHandles/CopyHelpers.h
@@ -15,6 +15,8 @@
 #include <xAODBase/IParticleHelpers.h>
 #include <xAODCore/ShallowCopy.h>
 
+#include <memory>
+
 namespace CP
 {
   namespace detail
@@ -35,45 +37,6 @@ namespace CP
       static const bool value = std::is_same<std::true_type, decltype(test<T>(nullptr))>::value;
     };
 
-
-
-    /// \brief a helper class to perform the core operations of
-    /// shallow copies (copying and setting original object links)
-    template<typename T, bool IP = IsIParticleContainer<T>::value>
-    struct ShallowCopyCore;
-
-    template<typename T>
-    struct ShallowCopyCore<T,true>
-    {
-      static auto copy (const T& input)
-	-> decltype (xAOD::shallowCopyContainer (input))
-      {
-	return xAOD::shallowCopyContainer (input);
-      }
-
-      static ::StatusCode setLink (const T& input, T& output)
-      {
-	return xAOD::setOriginalObjectLink(input, output);
-      }
-    };
-
-    template<typename T>
-    struct ShallowCopyCore<T,false>
-    {
-      static auto copy (const T& input)
-	-> decltype (xAOD::shallowCopyObject (input))
-      {
-	return xAOD::shallowCopyObject (input);
-      }
-
-      static ::StatusCode setLink (const T& /*input*/, T& /*output*/)
-      {
-	return ::StatusCode::SUCCESS;
-      }
-    };
-
-
-
     /// \brief a helper class to create shallow copies and register
     /// them in the event store
     ///
@@ -81,39 +44,155 @@ namespace CP
     /// straightforward to do partial specializations for base classes
     /// which need special handling to register their objects with the
     /// correct type.
+    template<typename T, bool IP = IsIParticleContainer<T>::value>
+    struct ShallowCopy;
+
     template<typename T>
-    struct ShallowCopy
+    struct ShallowCopy<T,true>
     {
       /// \brief the type of the event store we use
     public:
-      typedef std::decay<decltype(*((EL::AnaAlgorithm*)0)->evtStore())>::type StoreType;
+      typedef std::decay<decltype(*((EL::AnaAlgorithm*)0)->evtStore())>::type
+        StoreType;
 
       static StatusCode
       getCopy (MsgStream& msgStream, StoreType& store,
                T*& object, const T *inputObject,
                const std::string& outputName, const std::string& auxName)
       {
-        const auto msg = [&] (MSG::Level lvl) -> MsgStream& {msgStream << lvl; return msgStream;};
+        // Define the msg(...) function as a lambda.
+        const auto msg = [&] (MSG::Level lvl) -> MsgStream& {msgStream << lvl;
+                                                             return msgStream;};
 
+        // Make sure that we get a valid pointer.
         assert (inputObject != nullptr);
-        auto copy = ShallowCopyCore<T>::copy (*inputObject);
-        if (!copy.first || !copy.second)
-        {
-          ANA_MSG_ERROR ("failed to shallow copy object: " << outputName);
-          ANA_MSG_ERROR ("likely shallow copying a view container");
-          return StatusCode::FAILURE;
+
+        // Handle the case when the input object is a view container.
+        if( ! inputObject->getConstStore() ) {
+
+           // Decide how to handle the container.
+           if( inputObject->size() ) {
+              // Get the pointer to the "owning container" from the first
+              // element.
+              const T* originContainer =
+                 dynamic_cast< const T* >( inputObject->at( 0 )->container() );
+              // Postfix for the shallow-copy container of the origin container.
+              static const char* ORIGIN_POSTFIX = "_ShallowCopyOrigin";
+              // Make a shallow copy of the origin container.
+              auto originCopy = xAOD::shallowCopyContainer( *originContainer );
+              if( ( ! originCopy.first ) || ( ! originCopy.second ) ) {
+                 ANA_MSG_ERROR( "Failed to shallow copy the origin of a view "
+                                << "container, meant for: " << outputName );
+                 return StatusCode::FAILURE;
+              }
+              // ...and record it.
+              if( store.record( originCopy.first,
+                                outputName + ORIGIN_POSTFIX ).isFailure() ) {
+                 return StatusCode::FAILURE;
+              }
+              if( store.record( originCopy.second,
+                                outputName + ORIGIN_POSTFIX +
+                                "Aux." ).isFailure() ) {
+                 return StatusCode::FAILURE;
+              }
+              // Make a view copy on top of it.
+              auto viewCopy = std::make_unique< T >( SG::VIEW_ELEMENTS );
+              auto viewCopyPtr = viewCopy.get();
+              for( const auto* element : *inputObject ) {
+                 viewCopy->push_back( originCopy.first->at( element->index() ) );
+              }
+              // Set the origin links on it. Note that
+              // xAOD::setOriginalObjectLink's "container version" doesn't work
+              // with view containers, we have to call this function one-by-one
+              // on the elements.
+              for( size_t i = 0; i < inputObject->size(); ++i ) {
+                 if( ! xAOD::setOriginalObjectLink( *( ( *inputObject )[ i ] ),
+                                                    *( ( *viewCopy )[ i ] ) ) ) {
+                    return StatusCode::FAILURE;
+                 }
+              }
+              // Finally, record the view container with the requested name.
+              if( store.record( viewCopy.release(), outputName ).isFailure() ) {
+                 return StatusCode::FAILURE;
+              }
+              // The copy is done.
+              object = viewCopyPtr;
+              return StatusCode::SUCCESS;
+           } else {
+              // If the container was empty, then let's just make a new empty
+              // container, and that's that...
+              auto viewCopy = std::make_unique< T >( SG::VIEW_ELEMENTS );
+              auto viewCopyPtr = viewCopy.get();
+              if( store.record( viewCopy.release(), outputName ).isFailure() ) {
+                 return StatusCode::FAILURE;
+              }
+              // The copy is done.
+              object = viewCopyPtr;
+              return StatusCode::SUCCESS;
+           }
+
+        } else {
+
+           // We can just copy the container as is.
+           auto copy = xAOD::shallowCopyContainer( *inputObject );
+           if (!copy.first || !copy.second)
+           {
+              ANA_MSG_ERROR ("failed to shallow copy object: " << outputName);
+              ANA_MSG_ERROR ("likely shallow copying a view container");
+              return StatusCode::FAILURE;
+           }
+
+           if (!xAOD::setOriginalObjectLink (*inputObject, *copy.first)) {
+              return StatusCode::FAILURE;
+           }
+
+           if (store.record (copy.second, auxName).isFailure()) {
+              return StatusCode::FAILURE;
+           }
+           if (store.record (copy.first, outputName).isFailure()) {
+              return StatusCode::FAILURE;
+           }
+           object = copy.first;
+           return StatusCode::SUCCESS;
         }
-
-        if (ShallowCopyCore<T>::setLink (*inputObject, *copy.first).isFailure())
-          return StatusCode::FAILURE;
-
-        if (store.record (copy.second, auxName).isFailure())
-          return StatusCode::FAILURE;
-        if (store.record (copy.first, outputName).isFailure())
-          return StatusCode::FAILURE;
-        object = copy.first;
-        return StatusCode::SUCCESS;
       }
+    };
+
+    template<typename T>
+    struct ShallowCopy<T,false>
+    {
+       /// \brief the type of the event store we use
+    public:
+       typedef std::decay<decltype(*((EL::AnaAlgorithm*)0)->evtStore())>::type
+          StoreType;
+
+       static StatusCode
+       getCopy (MsgStream& msgStream, StoreType& store,
+                T*& object, const T *inputObject,
+                const std::string& outputName, const std::string& auxName)
+       {
+          // Define the msg(...) function as a lambda.
+          const auto msg = [&] (MSG::Level lvl) -> MsgStream& {msgStream << lvl;
+             return msgStream;};
+
+          // We can just copy the object as is.
+          auto copy = xAOD::shallowCopyObject( *inputObject );
+          if (!copy.first || !copy.second)
+          {
+             ANA_MSG_ERROR ("failed to shallow copy object: " << outputName);
+             ANA_MSG_ERROR ("likely shallow copying a view container");
+             return StatusCode::FAILURE;
+          }
+
+          if (store.record (copy.second, auxName).isFailure()) {
+             return StatusCode::FAILURE;
+          }
+          if (store.record (copy.first, outputName).isFailure()) {
+             return StatusCode::FAILURE;
+          }
+          object = copy.first;
+          return StatusCode::SUCCESS;
+       }
     };
 
     template<>
