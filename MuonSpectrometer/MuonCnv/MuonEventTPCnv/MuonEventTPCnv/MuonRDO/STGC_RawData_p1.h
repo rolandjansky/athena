bebd84ada871f3f6d5d3c3c99ea219e771362937
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-05-16 18:24:00 +0000

    Merge branch 'nsw-dev-2' into '21.3'
    Fixes in the persistent to transient converters for MM and STGC
    
    See merge request atlas/athena!10868
    
    (cherry picked from commit 0989332a910e658453ba3e7f975af1ab71043843 [formerly 8014dc60575b25d466c6aa7d058ef5aff6a03752])
    
    940b0bc5 Fixes in the persistent to transient converters for MM and STGC
    
    Former-commit-id: da7f971075d6dcd9af198073039022fe3e8c86fa

--- MuonSpectrometer/MuonCnv/MuonEventTPCnv/MuonEventTPCnv/MuonRDO/STGC_RawData_p1.h
+++ MuonSpectrometer/MuonCnv/MuonEventTPCnv/MuonEventTPCnv/MuonRDO/STGC_RawData_p1.h
@@ -11,9 +11,19 @@ namespace Muon
     class STGC_RawData_p1
     {
     public:
-        STGC_RawData_p1() : m_id(0) {}
+    STGC_RawData_p1() : m_id(0), m_time(0.), m_charge(0.), m_bcTag(0), m_isDead(false) {}
                 
         unsigned int        m_id; //!< FIXME! Remove this eventually
+	
+	/// time
+	float m_time;
+	/// charge
+	float m_charge;
+	/// bctag
+	uint16_t m_bcTag;
+	/// isDead
+	bool m_isDead;
+
     };
 }
 
