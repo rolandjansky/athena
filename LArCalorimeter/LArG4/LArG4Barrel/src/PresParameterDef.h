f07cce804997004114c94bef1606d69133a29db1
scott snyder <scott.snyder@cern.ch>
2018-05-16 16:50:31 +0200

    LArG4Barrel: Fix clang warnings.
    clang warnings: unused private data members.
    (Deliberately commented out rather than deleted, to preserve the knowledge
    of these constants.)
    
    
    Former-commit-id: 0b183742ebe3f177c0762c91cae9b28e9853f13f

--- LArCalorimeter/LArG4/LArG4Barrel/src/PresParameterDef.h
+++ LArCalorimeter/LArG4/LArG4Barrel/src/PresParameterDef.h
@@ -7,11 +7,11 @@
  G4double m_cmm{(1-0.0026)*Athena::Units::mm}; // contraction factor
 
  /** Electrodes */
- G4double m_smallLength{275.6};
- G4double m_bigLength{277.5};
- G4double m_anode_th{0.330};
+ //G4double m_smallLength{275.6};
+ //G4double m_bigLength{277.5};
+ //G4double m_anode_th{0.330};
  G4double m_cathode_th{0.270};
- G4double m_larheight{13.};
+ //G4double m_larheight{13.};
 
  /** modules para [Length,NAnodes,NCathodes,elec. inclination,Pitch,Pos0]*/
  // mod[#module#][ 0:length, 1:Number of Anodes, 2:number of cathodes,
@@ -28,24 +28,24 @@
  };
 
  /** Prep., MB, ..... */
- G4double m_prep1_th{1.};  // bottom prepreg layer
- G4double m_prep2_th{4.5}; // top prepreg layer
- G4double m_shell_th{0.4}; // Protection Shell Thickness
- G4double m_prot_th{0.5};  // Top protection Shell Thickness
+ //G4double m_prep1_th{1.};  // bottom prepreg layer
+ //G4double m_prep2_th{4.5}; // top prepreg layer
+ //G4double m_shell_th{0.4}; // Protection Shell Thickness
+ //G4double m_prot_th{0.5};  // Top protection Shell Thickness
 
- G4double  m_mech_clear{0.5}; // Bottom Mechanical clearence
- G4double m_rail_th{8.6}; // Support bar Thickness
- G4double m_rail_pos{22.}; // Lateral position from the edge
- G4double m_rail_width{24.}; // Support bar width
+ //G4double  m_mech_clear{0.5}; // Bottom Mechanical clearence
+ //G4double m_rail_th{8.6}; // Support bar Thickness
+ //G4double m_rail_pos{22.}; // Lateral position from the edge
+ //G4double m_rail_width{24.}; // Support bar width
 
- G4double m_mb_th{2.2}; // Mother Board Thickness
- G4double m_mb_width{169.}; // Mother Board Width
- G4double m_mb_length{3100.3}; // Mother Board Length
+ //G4double m_mb_th{2.2}; // Mother Board Thickness
+ //G4double m_mb_width{169.}; // Mother Board Width
+ //G4double m_mb_length{3100.3}; // Mother Board Length
 
  /** connectics */
- G4double m_widthFront{2.8}; // width at eta = 0. , width at etamax = MB_width
- G4double m_heightIn{1.5}; // Height at eta = 0.
- G4double m_heightOut{5.}; // Height at etamax
+ //G4double m_widthFront{2.8}; // width at eta = 0. , width at etamax = MB_width
+ //G4double m_heightIn{1.5}; // Height at eta = 0.
+ //G4double m_heightOut{5.}; // Height at etamax
 
  /** Presampler Mother Volume parameters */
  G4double m_rMinPresamplerMother{1385*Athena::Units::mm};
@@ -58,4 +58,4 @@
 
  /** GU add phi min and phi span of overall volume */
  G4int m_nsectors{32};  // number of sectors / half barrel
- G4int m_nbsectors{32}; // number of sectors to put in simulation
+//G4int m_nbsectors{32}; // number of sectors to put in simulation
