67c989ecf0888407ef089c29b0983a982044b43d
Roger Felipe Naranjo Garcia <roger.naranjo@cern.ch>
2018-05-28 11:27:03 +0200

    changing max events
    Former-commit-id: 8c3fb4de25520451db0bea8fccb3d08cc9c6c91e

--- Reconstruction/egamma/egammaValidation/test/test_electron.sh
+++ Reconstruction/egamma/egammaValidation/test/test_electron.sh
@@ -4,7 +4,7 @@
 #
 # art-type: grid
 # art-input: mc16_13TeV.423000.ParticleGun_single_electron_egammaET.recon.RDO.e3566_s3113_r9388
-# art-input-nfiles: 40
+# art-input-nfiles: 20
 # art-cores: 4
 # art-include: 21.0/Athena
 # art-include: master/Athena
@@ -65,7 +65,7 @@ case $ArtProcess in
 	unset  ATHENA_NUM_PROC
 	unset  ATHENA_PROC_NUMBER
 	
-	Reco_tf.py --inputRDOFile=$x --outputAODFile=Nightly_AOD_electron.pool.root --maxEvents=5 --autoConfiguration="everything" --preExec="rec.doTrigger=False; rec.doMuon=False; rec.doTau=False ; from RecExConfig.RecAlgsFlags import recAlgs; recAlgs.doMuonSpShower=False ; rec.doBTagging=False ; recAlgs.doEFlow=False ; recAlgs.doEFlowJet=False ; recAlgs.doMissingET=False ; recAlgs.doMissingETSig=False ; from JetRec.JetRecFlags import jetFlags ; jetFlags.Enabled=False ; "
+	Reco_tf.py --inputRDOFile=$x --outputAODFile=Nightly_AOD_electron.pool.root --maxEvents=2000 --autoConfiguration="everything" --preExec="rec.doTrigger=False; rec.doMuon=False; rec.doTau=False ; from RecExConfig.RecAlgsFlags import recAlgs; recAlgs.doMuonSpShower=False ; rec.doBTagging=False ; recAlgs.doEFlow=False ; recAlgs.doEFlowJet=False ; recAlgs.doMissingET=False ; recAlgs.doMissingETSig=False ; from JetRec.JetRecFlags import jetFlags ; jetFlags.Enabled=False ; "
 	
 	echo  "art-result: $? reconstruction"
 
