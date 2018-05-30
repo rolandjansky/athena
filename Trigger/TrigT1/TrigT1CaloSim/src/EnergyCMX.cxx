4bcae06a60eea5a923c821035a496d15acda99bb
Edward Moyse <edward.moyse@cern.ch>
2018-05-30 15:20:12 +0200

    Removing deprecated licence statements, and copyright declarations, and inserting the atlas copyright where necessary [TrigT1CaloSim]
    Former-commit-id: f24498e1ac4dabadaf38370ce9708492f905e029

--- Trigger/TrigT1/TrigT1CaloSim/src/EnergyCMX.cxx
+++ Trigger/TrigT1/TrigT1CaloSim/src/EnergyCMX.cxx
@@ -1,9 +1,11 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
 
 // /***************************************************************************
 //                           EnergyCMX.cxx  -  description
 //                              -------------------
  //     begin                : 04 04 2014
- //     copyright            : (C) 2014 by Alan Watson
  //     email                : Alan.Watson@cern.ch
 //  ***************************************************************************/
 //
@@ -13,10 +15,6 @@
 //
 /***************************************************************************
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 //
--- Trigger/TrigT1/TrigT1CaloSim/src/Tester.cxx
+++ Trigger/TrigT1/TrigT1CaloSim/src/Tester.cxx
@@ -1,8 +1,10 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
 /***************************************************************************
                           Tester.cxx  -  description
                              -------------------
     begin                : Mon Apr 23 2001
-    copyright            : (C) 2000 by moyse
     email                : e.moyse@qmw.ac.uk
  ***************************************************************************/
 //
@@ -11,10 +13,6 @@
 // 
 /***************************************************************************
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 // ================================================
