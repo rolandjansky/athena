cba029b4e59a0508d51e4f91cd8320dc4dcf4355
Tomasz Bold <tomasz.bold@gmail.com>
2018-06-04 21:46:52 +0200

    First version of the MET alg and tool
    Former-commit-id: c2da79d2dc459a8bb0fe024b620f1f6d8bc1b6d1

---
+++ Trigger/TrigAlgorithms/TrigEFMissingET/share/testMET.py
@@ -0,0 +1,57 @@
+#
+#  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+#
+
+include("TrigUpgradeTest/testHLT_MT.py")
+
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence = AlgSequence()
+
+isData = False
+if globalflags.InputFormat.is_bytestream():
+  isData = True
+
+
+
+import math
+from TrigT2CaloCommon.TrigT2CaloCommonConf import TrigCaloDataAccessSvc#, TestCaloDataAccess
+from AthenaMonitoring.GenericMonitoringTool import GenericMonitoringTool, defineHistogram
+
+mon = GenericMonitoringTool("CaloDataAccessSvcMon")
+mon.Histograms += [defineHistogram( "TIME_locking_LAr_RoI", title="Time spent in unlocking the LAr collection", xbins=100, xmin=0, xmax=100 ),
+                   defineHistogram( "roiROBs_LAr", title="Number of ROBs unpacked in RoI requests", xbins=20, xmin=0, xmax=20 ),
+                   defineHistogram( "TIME_locking_LAr_FullDet", title="Time spent in unlocking the LAr collection", xbins=100, xmin=0, xmax=100 ),
+                   defineHistogram( "roiEta_LAr,roiPhi_LAr", type="TH2F", title="Geometric usage", xbins=50, xmin=-5, xmax=5, ybins=64, ymin=-math.pi, ymax=math.pi )]
+
+svcMgr += TrigCaloDataAccessSvc()
+svcMgr.TrigCaloDataAccessSvc.MonTool = mon
+svcMgr.TrigCaloDataAccessSvc.OutputLevel=INFO
+
+from TrigCaloRec.TrigCaloRecConf import HLTCaloCellMaker
+cellMakerAlgo =  HLTCaloCellMaker("CellMakerMT", roiMode=True)
+cellMakerAlgo.RoIs="METRoIs" 
+cellMakerAlgo.OutputLevel=VERBOSE
+cellMakerAlgo.CellsName="cells"
+topSequence += cellMakerAlgo
+
+from TrigEFMissingET.TrigEFMissingETConf import EFMissingETAlgMT, EFMissingETFromCellsMT
+
+cellTool = EFMissingETFromCellsMT( name="METFromCellsTool" )
+cellTool.CellsCollection = "cells"
+
+metAlg = EFMissingETAlgMT( name="EFMET" )
+metAlg.METTools=[ cellTool ]
+metAlg += cellTool
+topSequence += metAlg
+
+#   from TrigCaloRec.TrigCaloRecConfig import TrigCaloClusterMakerMT_topo
+  
+# #  clusterMakerAlgo = TrigCaloClusterMakerMT_topo(name="ClustermakerMT_topo",cells="cells",doMoments=True,doLC=True)
+# #  Get conditions error using TrigLocalCalib tool, so set doLC=False
+#   clusterMakerAlgo = TrigCaloClusterMakerMT_topo(name="ClustermakerMT_topo",cells="cells",doMoments=True,doLC=False)
+#   clusterMakerAlgo.OutputLevel=VERBOSE
+#   topSequence += clusterMakerAlgo
+  
+# print ToolSvc
+
+
---
+++ Trigger/TrigAlgorithms/TrigEFMissingET/src/EFMissingETAlgMT.cxx
@@ -0,0 +1,95 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+#include <cmath>
+#include "xAODTrigMissingET/TrigMissingETAuxContainer.h"
+#include "TrigEFMissingET/EFMissingETHelper.h"
+#include "AthenaMonitoring/MonitoredScope.h"
+#include "EFMissingETAlgMT.h"
+
+
+
+EFMissingETAlgMT::EFMissingETAlgMT( const std::string& name, 
+				    ISvcLocator* pSvcLocator ) 
+  : AthReentrantAlgorithm( name, pSvcLocator ) {}
+
+EFMissingETAlgMT::~EFMissingETAlgMT() {}
+
+StatusCode EFMissingETAlgMT::initialize() {
+  CHECK( m_metTools.retrieve() );
+  CHECK( m_metContainerKey.initialize() );
+  CHECK( m_monTool.retrieve() );
+  return StatusCode::SUCCESS;
+}
+
+
+//!< Converts from MeV to GeV and them log10, preserving the sign, the minimum dictated by monitoring histograms
+double toLogGeV( double x, double fallback = 0, double epsilon = 1.189 ) {
+  const double absXGeV =  std::fabs( x * 1.e-3 );
+  if ( absXGeV < epsilon ) 
+    return fallback;
+  return std::copysign( std::log10( absXGeV ), x );
+}
+
+//!< converts to from MeV to GeV if above threshold, else falback value
+double toLinGeV( double x, double fallback = 0, double epsilon = 1e-6 ) {
+    const double xGeV = x * 1.e-3;
+  if ( xGeV < epsilon ) 
+    return fallback;
+  return xGeV;
+}
+
+
+StatusCode EFMissingETAlgMT::execute_r( const EventContext& context ) const {  
+  ATH_MSG_DEBUG ( "Executing " << name() << "..." );
+  using namespace Monitored;
+  auto totalTimer = MonitoredTimer::declare( "Total" );
+  auto loopTimer = MonitoredTimer::declare( "Loop" );
+
+  auto metContainer = std::make_unique<xAOD::TrigMissingETContainer>();
+  auto metAuxContainer = std::make_unique<xAOD::TrigMissingETAuxContainer>();
+  metContainer->setStore( metAuxContainer.get() );
+
+  auto met = new xAOD::TrigMissingET();
+  metContainer->push_back( met );
+
+  TrigEFMissingEtHelper metHelper(42);
+  
+  loopTimer.start();
+  for ( auto& t: m_metTools ) {
+    ATH_MSG_DEBUG( "Invoking tool " << t->name() << " to update the MET obejct" );
+    t->update( met, &metHelper );
+  }
+  loopTimer.stop();
+
+  
+  auto EF_MEx_log = MonitoredScalar::declare( "EF_MEx_log", toLogGeV( met->ex() ) );
+  auto EF_MEy_log = MonitoredScalar::declare( "EF_MEy_log", toLogGeV( met->ey() ) );
+  auto EF_MEz_log = MonitoredScalar::declare( "EF_MEz_log", toLogGeV( met->ez() ) );
+  auto EF_MET_log = MonitoredScalar::declare( "EF_MET_log", toLogGeV( std::hypot( met->ex(), met->ey() ) ) );
+  auto EF_ME_log    = MonitoredScalar::declare( "EF_ME_log",  toLogGeV( std::hypot( std::hypot(met->ex(), met->ey()), met->ez() ) ) );
+  auto EF_SumEt_log = MonitoredScalar::declare( "EF_SumEt_log", toLogGeV( met->sumEt() )  );
+  auto EF_SumE_log  = MonitoredScalar::declare( "EF_SumE_log",  toLogGeV( met->sumE(), -9e9 ) );
+  
+  auto EF_MEx_lin = MonitoredScalar::declare( "EF_MEx_lin", toLinGeV( met->ex() ) );
+  auto EF_MEy_lin = MonitoredScalar::declare( "EF_MEy_lin", toLinGeV( met->ey() ) );
+  auto EF_MEz_lin = MonitoredScalar::declare( "EF_MEz_lin", toLinGeV( met->ez() ) );
+  auto EF_MET_lin = MonitoredScalar::declare( "EF_MET_lin", toLinGeV( std::hypot( met->ex(), met->ey() ) ) );			     
+  auto EF_ME_lin  = MonitoredScalar::declare( "EF_ME_lin",  toLinGeV( std::hypot( std::hypot(met->ex(), met->ey()), met->ez() ) ) );
+  auto EF_SumEt_lin =  MonitoredScalar::declare( "EF_SumEt_lin", toLinGeV( met->sumEt() ) );
+  auto EF_SumE_lin  = MonitoredScalar::declare( "EF_SumE_lin",  toLinGeV( met->sumE(), -9e9 ) );
+  auto EF_XS        = MonitoredScalar::declare( "EF_XS", toLinGeV( std::hypot( met->ex(), met->ey() ) ) / toLinGeV( met->sumEt() ) );
+  auto EF_MET_phi   = MonitoredScalar::declare( "EF_MET_phi",   std::atan2( met->ey(), met->ex() ) );
+ 
+  auto monitorIt = MonitoredScope::declare( m_monTool, 
+					    totalTimer, loopTimer,
+					    EF_MEx_log, EF_MEy_log, EF_MEz_log, EF_MET_log, EF_ME_log, EF_ME_log, EF_SumE_log,
+					    EF_MEx_lin, EF_MEy_lin, EF_MEz_lin, EF_MET_lin, EF_ME_lin, EF_SumEt_lin, EF_SumE_lin, 
+					    EF_XS, EF_MET_phi);
+    
+  auto handle = SG::makeHandle( m_metContainerKey, context );
+  CHECK( handle.record( std::move( metContainer ), std::move ( metAuxContainer ) ) );
+  return StatusCode::SUCCESS;
+}
+
+
