df802993c5bbba895c901ff0508aad7649be6a43
Alan Watson <Alan.Watson@cern.ch>
2018-06-05 15:01:05 +0200

    Changes to enable more sophisticated eta masking in MET and TE triggers, bringing offline in line wih online system. Necessary for correct behaviour in MC and to avoid false error reports in monitoring.
    Similar changes are being collected for Tier 0, and probably P1
    
    
    Former-commit-id: 8686d57f2a67dc43db6471f51261459a69db3627

--- Trigger/TrigT1/TrigT1CaloToolInterfaces/TrigT1CaloToolInterfaces/IL1EtTools.h
+++ Trigger/TrigT1/TrigT1CaloToolInterfaces/TrigT1CaloToolInterfaces/IL1EtTools.h
@@ -38,16 +38,16 @@ Interface definition for L1EtTools
     virtual void moduleSums(const xAOD::JetElementMap_t* jemap,
                             DataVector<ModuleEnergy>* modules, int slice = -1) const = 0;
     virtual void crateSums(const DataVector<ModuleEnergy>* modules,
-                            DataVector<CrateEnergy>* crates, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0 ;
+                            DataVector<CrateEnergy>* crates, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0 ;
     virtual void crateSums(const DataVector<EnergyCMXData>* modules,
-                            DataVector<CrateEnergy>* crates, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0 ;
+                            DataVector<CrateEnergy>* crates, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0 ;
     virtual void crateSums(const xAOD::JetElementContainer* jetelements,
-                           DataVector<CrateEnergy>* crates, int slice = -1, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0;
+                           DataVector<CrateEnergy>* crates, int slice = -1, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0;
     virtual void crateSums(const xAOD::JetElementMap_t* jemap,
-                           DataVector<CrateEnergy>* crates, int slice = -1, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0;
+                           DataVector<CrateEnergy>* crates, int slice = -1, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0;
     virtual SystemEnergy systemSums(const DataVector<CrateEnergy>* crates) const = 0;
-    virtual SystemEnergy systemSums(const xAOD::JetElementContainer* jetelements, int slice = -1, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0;
-    virtual SystemEnergy systemSums(const xAOD::JetElementMap_t* jemap, int slice = -1, float etaMaxXE = 5., float etaMaxTE = 5., bool restricted = false) const = 0;
+    virtual SystemEnergy systemSums(const xAOD::JetElementContainer* jetelements, int slice = -1, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0;
+    virtual SystemEnergy systemSums(const xAOD::JetElementMap_t* jemap, int slice = -1, uint32_t maskXE = 0xff, uint32_t maskTE = 0xff, bool restricted = false) const = 0;
   
   };
 
