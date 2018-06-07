d68a2aaf7a82bf3af4e7b716ca6287e804800d15
scott snyder <scott.snyder@cern.ch>
2018-06-07 14:22:20 +0200

    AthenaKernel: Add calls from CondCont to the conditions cleaning service.
    CondCont makes a call to the cleaning service after a conditions
    object is added to the container.
    
    
    Former-commit-id: 0803c3978913a9e7c5735d05d04630a66b88d9b3

--- Control/AthenaKernel/AthenaKernel/CondCont.h
+++ Control/AthenaKernel/AthenaKernel/CondCont.h
@@ -15,12 +15,15 @@
 #include "AthenaKernel/ClassID_traits.h"
 #include "AthenaKernel/BaseInfo.h"
 #include "AthenaKernel/RCUUpdater.h"
+#include "AthenaKernel/IConditionsCleanerSvc.h"
 #include "CxxUtils/ConcurrentRangeMap.h"
+#include "CxxUtils/checker_macros.h"
 
 #include "GaudiKernel/EventIDBase.h"
 #include "GaudiKernel/EventIDRange.h"
 #include "GaudiKernel/DataObjID.h"
 #include "GaudiKernel/StatusCode.h"
+#include "GaudiKernel/ServiceHandle.h"
 #include "boost/preprocessor/facilities/overload.hpp"
 
 #include <iostream>
@@ -333,6 +336,14 @@ public:
   };
 
 
+  /**
+   * @brief Allow overriding the name of the global conditions cleaner
+   *        service (for testing purposes).
+   * @param name The name of the global conditions cleaner service.
+   */
+  static void setCleanerSvcName ATLAS_NOT_THREAD_SAFE (const std::string& name);
+
+  
 protected:
   /**
    * @brief Internal constructor.
@@ -373,10 +384,22 @@ protected:
                                   const EventIDBase& t,
                                   EventIDRange const** r) const = 0;
 
-  
+
+  /**
+   * @brief Tell the cleaner that a new object was added to the container.
+   */
+  StatusCode inserted (const EventContext& ctx);
+
+
 private:
   /// CLID of the most-derived @c CondCont
   CLID m_clid;
+
+  /// Handle to the cleaner service.
+  ServiceHandle<Athena::IConditionsCleanerSvc> m_cleanerSvc;
+
+  /// Name of the global conditions cleaner service.
+  static std::string s_cleanerSvcName ATLAS_THREAD_SAFE;
 };
 
 
