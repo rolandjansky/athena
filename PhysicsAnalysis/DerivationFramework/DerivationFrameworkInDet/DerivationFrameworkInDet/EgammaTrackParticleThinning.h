423a6fb3ccfbfc9bdb4d27f4492c8c0716879738
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2018-06-11 14:34:24 +0200

    Cleaned up the implementation of DerivationFramework::EgammaTrackParticleThinning.
    At the same time cleaned the CMake configuration of DerivationFrameworkInDet as well.
    
    
    Former-commit-id: 7c44dab6982686a5ab5b4c924598ee4a77f67f8e

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/DerivationFrameworkInDet/EgammaTrackParticleThinning.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/DerivationFrameworkInDet/EgammaTrackParticleThinning.h
@@ -1,55 +1,116 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-///////////////////////////////////////////////////////////////////
-// EgammaTrackParticleThinning.h, (c) ATLAS Detector software
-///////////////////////////////////////////////////////////////////
+#ifndef DERIVATIONFRAMEWORKINDET_EGAMMATRACKPARTICLETHINNING_H
+#define DERIVATIONFRAMEWORKINDET_EGAMMATRACKPARTICLETHINNING_H
 
-#ifndef DERIVATIONFRAMEWORK_EGAMMATRACKPARTICLETHINNING_H
-#define DERIVATIONFRAMEWORK_EGAMMATRACKPARTICLETHINNING_H
-
+// System include(s):
 #include <string>
+#include <memory>
+#include <vector>
 
+// Framework include(s):
+#include "GaudiKernel/ServiceHandle.h"
+#include "AthenaKernel/IThinningSvc.h"
 #include "AthenaBaseComps/AthAlgTool.h"
+
+// DF include(s):
 #include "DerivationFrameworkInterfaces/IThinningTool.h"
-#include "GaudiKernel/ToolHandle.h"
+#include "DerivationFrameworkInterfaces/ExpressionParserHelper.h"
 #include "DerivationFrameworkInDet/TracksInCone.h"
-#include "xAODEgamma/EgammaContainer.h"
 
-namespace ExpressionParsing {
-  class ExpressionParser;
-}
-
-class IThinningSvc;
+// EDM include(s):
+#include "xAODEgamma/EgammaFwd.h"
 
 namespace DerivationFramework {
 
-  class EgammaTrackParticleThinning : public AthAlgTool, public IThinningTool {
-    public: 
-      EgammaTrackParticleThinning(const std::string& t, const std::string& n, const IInterface* p);
-      ~EgammaTrackParticleThinning();
-      StatusCode initialize();
-      StatusCode finalize();
-      virtual StatusCode doThinning() const;
+   /// Tool selecting track particles from e/gamma objects to keep
+   ///
+   /// This tool is used in derivation jobs to set up the thinning such that
+   /// track particles associated with e/gamma objects would be kept for a given
+   /// output stream.
+   ///
+   /// @author James Catmore (James.Catmore@cern.ch)
+   ///
+   class EgammaTrackParticleThinning : public AthAlgTool, public IThinningTool {
 
-    private:
-      ServiceHandle<IThinningSvc> m_thinningSvc;
-      mutable unsigned int m_ntot, m_ntotGSF, m_npass, m_nGSFPass;
+   public:
+      /// AlgTool constructor
+      EgammaTrackParticleThinning( const std::string& type,
+                                   const std::string& name,
+                                   const IInterface* parent );
+
+      /// @name Function(s) implementing the @c IAlgTool interface
+      /// @{
+
+      /// Function initialising the tool
+      StatusCode initialize() override;
+      /// Function finalising the tool
+      StatusCode finalize() override;
+
+      /// @}
+
+      /// @name Function(s) implementing the
+      ///       @c DerivationFramework::IThinningTool interface
+      /// @{
+
+      /// Function performing the configured thinning operation
+      StatusCode doThinning() const override;
+
+      /// @}
+
+   private:
+      /// @name Function(s) implementing most of the logic
+      /// @{
+
+      /// Set up the filtering mask for the track particles from photons
+      StatusCode
+      setPhotonMasks( std::vector< bool >& tpMask,
+                      std::vector< bool >& gsfTpMask,
+                      const std::vector< const xAOD::Egamma* >& egammas,
+                      const xAOD::TrackParticleContainer* tps,
+                      const xAOD::TrackParticleContainer* gsfTps ) const;
+      /// Set up the filtering mask for the track particles from electrons
+      StatusCode
+      setElectronMasks( std::vector< bool >& tpMask,
+                        std::vector< bool >& gsfTpMask,
+                        const std::vector< const xAOD::Egamma* >& egammas,
+                        const xAOD::TrackParticleContainer* tps,
+                        const xAOD::TrackParticleContainer* gsfTps ) const;
+
+      /// @}
+
+      /// @name Tool properties
+      /// @{
+
+      /// SG key of the e/gamma container to use
       std::string m_sgKey;
-      std::string m_inDetSGKey;
-      std::string m_gsfSGKey;
+      /// SG key of the track particle container to use
+      std::string m_inDetSGKey = "InDetTrackParticles";
+      /// SG key of the GSF track particle container to use
+      std::string m_gsfSGKey = "GSFTrackParticles";
+      /// Selection string to use with the expression evaluation
       std::string m_selectionString;
-      bool m_bestMatchOnly;
-      float m_coneSize;
-      bool m_and;
-      ExpressionParsing::ExpressionParser *m_parser;
-      void setPhotonMasks(std::vector<bool>&, std::vector<bool>&, const xAOD::EgammaContainer*&, const xAOD::TrackParticleContainer*&, const xAOD::TrackParticleContainer*&, const bool&) const;
-      void setPhotonMasks(std::vector<bool>&, std::vector<bool>&, std::vector<const xAOD::Egamma*>&, const xAOD::TrackParticleContainer*&, const xAOD::TrackParticleContainer*&, const bool&) const;
-      void setElectronMasks(std::vector<bool>&, std::vector<bool>&, const xAOD::EgammaContainer*&, const xAOD::TrackParticleContainer*&, const xAOD::TrackParticleContainer*&, const bool&) const;
-      void setElectronMasks(std::vector<bool>&, std::vector<bool>&, std::vector<const xAOD::Egamma*>&, const xAOD::TrackParticleContainer*&, const xAOD::TrackParticleContainer*&, const bool&) const;
+      /// Flag for keeping just the track particles best matching e/g object
+      bool m_bestMatchOnly = true;
+      /// Cone around e/g objects to keep track particles in
+      float m_coneSize = -1.0;
+      /// Flag for using @c IThinningSvc::Operator::And (instead of "or")
+      bool m_and = false;
 
-  };
-}
+      /// @}
 
-#endif // DERIVATIONFRAMEWORK_EGAMMATRACKPARTICLETHINNING_H
+      /// Handle for accessing the thinning service
+      ServiceHandle< IThinningSvc > m_thinningSvc;
+      /// Variables keeping statistics information about the job
+      mutable unsigned int m_ntot = 0, m_ntotGSF = 0, m_npass = 0,
+                           m_nGSFPass = 0;
+      /// The expression evaluation helper object
+      std::unique_ptr< ExpressionParserHelper > m_parser;
+
+   }; // class EgammaTrackParticleThinning
+
+} // namespace DerivationFramework
+
+#endif // DERIVATIONFRAMEWORKINDET_EGAMMATRACKPARTICLETHINNING_H
