4e339119dc1b8e606b80a4d9e581a56a16240149
Zach Marshall <ZLMarshall@lbl.gov>
2018-06-12 03:15:07 +0200

    Adding options for stopping condition in simulation
    Adding python-level configurable stopping condition option for the R-hadron
    stopping code.  There are a few different options, and the default is
    being set to something slightly different from what the old
    configuration did.  Option settings that are negative ignore gasses with
    low density, so that particles cannot stop in there (previously large
    numbers of particles were stopping in vacuum, which doesn't make any
    physical sense).  abs(Option)==2 stops only negatively charged
    particles, as they won't have to overcome a coulomb barrier near the
    nucleus.  Needs a little bit of R&D before going further in stopping
    conditions, but these seemed to be the obvious starting places.  Added a
    little bit of code showing how the configuration works in job options to
    SimulationJobOptions (can also be done via preExec).
    
    
    Former-commit-id: 69ef44557d864c4b5a6423a77a8945e9fe4e4268

--- Simulation/G4Utilities/G4UserActions/python/G4UserActionsConfig.py
+++ Simulation/G4Utilities/G4UserActions/python/G4UserActionsConfig.py
@@ -116,3 +116,14 @@ def getFluxRecorderTool(name="G4UA::FluxRecorderTool", **kwargs):
 
 def getRadiationMapsMakerTool(name="G4UA::RadiationMapsMakerTool", **kwargs):
     return CfgMgr.G4UA__RadiationMapsMakerTool(name, **kwargs)
+
+def getStoppedParticleActionTool(name="G4UA::StoppedParticleActionTool", **kwargs):
+    # Just have to set the stopping condition
+    from G4AtlasApps.SimFlags import simFlags
+    # example custom configuration
+    print 'ZLM at',simFlags.UserActionConfig.get_Value()
+    if name in simFlags.UserActionConfig.get_Value().keys():
+        for prop,value in simFlags.UserActionConfig.get_Value()[name].iteritems():
+            kwargs.setdefault(prop,value)
+            print 'ZLM setting default',prop,'to',value
+    return CfgMgr.G4UA__StoppedParticleActionTool(name, **kwargs)
