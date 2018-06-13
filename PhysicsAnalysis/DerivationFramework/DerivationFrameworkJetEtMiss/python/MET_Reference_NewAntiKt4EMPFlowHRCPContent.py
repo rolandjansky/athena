b4a0b1b161dca94c7d1c79f0011ce23e5042e490
Artur Trofymov <artur.trofymov@cern.ch>
2018-06-13 22:22:33 +0200

    update with CHSParticleFlowObjects and STDM4
    Former-commit-id: c0d3b3e8d33805961150c9ee3a6c3622320237e6

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/MET_Reference_NewAntiKt4EMPFlowHRCPContent.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+MET_Reference_NewAntiKt4EMPFlowHRCPContent  = [
+"AntiKt4EMPFlowJets",
+"AntiKt4EMPFlowJetsAux.pt.eta.phi.m.JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m.GhostTrack.NumTrkPt500.SumPtTrkPt500.Width.EMFrac.EnergyPerSampling",
+"Electrons",
+"InDetTrackParticles",
+"METAssoc_NewAntiKt4EMPFlowHRCPContent",
+"METAssoc_NewAntiKt4EMPFlowHRCPContentAux.",
+"MET_Core_NewAntiKt4EMPFlowHRCPContent",
+"MET_Core_NewAntiKt4EMPFlowHRCPContentAux.name.mpx.mpy.sumet.source",
+"MET_Reference_NewAntiKt4EMPFlowHRCPContent",
+"MET_Reference_NewAntiKt4EMPFlowHRCPContentAux.name.mpx.mpy.sumet.source",
+#"MET_Truth",
+#"MET_TruthAux.name.mpx.mpy.sumet.source",
+"Muons",
+"MuonsAux.EnergyLoss.energyLossType.muonType",
+"Photons"
+]
---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/MET_Reference_myAntiKt4EMPFlowCPContent.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+MET_Reference_myAntiKt4EMPFlowCPContent  = [
+"AntiKt4EMPFlowJets",
+"AntiKt4EMPFlowJetsAux.pt.eta.phi.m.JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m.GhostTrack.NumTrkPt500.SumPtTrkPt500.Width.EMFrac.EnergyPerSampling",
+"Electrons",
+"InDetTrackParticles",
+"METAssoc_myAntiKt4EMPFlow",
+"METAssoc_myAntiKt4EMPFlowAux.",
+"MET_Core_myAntiKt4EMPFlow",
+"MET_Core_myAntiKt4EMPFlowAux.name.mpx.mpy.sumet.source",
+"MET_Reference_myAntiKt4EMPFlow",
+"MET_Reference_myAntiKt4EMPFlowAux.name.mpx.mpy.sumet.source",
+#"MET_Truth",
+#"MET_TruthAux.name.mpx.mpy.sumet.source",
+"Muons",
+"MuonsAux.EnergyLoss.energyLossType.muonType",
+"Photons"
+]
