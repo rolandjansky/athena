0e9b6edcd5342e6231ef3ab2a668b3fdb29336e0
James Beacham <j.beacham@cern.ch>
2018-06-20 18:06:50 +0000

    Merge branch 'ART_TESTS_v4' into '21.0'
    Removed whitespace on FastChain jobs and did some minor cosmetic cleanups
    
    See merge request atlas/athena!12158
    
    (cherry picked from commit 79323265db6742361b864a5ac903d7a1dfe5022d [formerly 1e700abaa53aaf27fce20dfd5f8bbaff63ae36f5])
    
    39aad760 Removed whitespace on FastChain jobs and did some minor cosmetic cleanups
    
    Former-commit-id: 5c655c670a672e712604fc0346f281afd668357a

--- Simulation/FastSimulation/FastChainPileup/test/test_ttFC_fastSim_fullDigi.sh
+++ Simulation/FastSimulation/FastChainPileup/test/test_ttFC_fastSim_fullDigi.sh
@@ -2,13 +2,30 @@
 # art-description: test for job configuration ttFC_fastSim_fulldigi (Sim/Digi job)
 # art-type: grid
 #
-# specify branches of athena that are being targeted:                                                               
-# art-include: 21.0/Athena                                                                                          
-# art-include: 21.3/Athena                                                                                          
-# Also include temporary branch 21.3-hmpl                                                                           
-# art-include: 21.3-hmpl/Athena       
+# specify branches of athena that are being targeted:
+#
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+#
+# Also include temporary branch 21.3-hmpl
+# art-include: 21.3-hmpl/Athena
 
-FastChain_tf.py --simulator ATLFASTIIF_PileUp --digiSteeringConf "SplitNoMerge" --useISF True --randomSeed 123 --enableLooperKiller True --inputEVNTFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/ttbar_muplusjets-pythia6-7000.evgen.pool.root --outputRDOFile RDO_pileup_fastsim_fulldigi.pool.root --maxEvents 100 --skipEvents 0 --geometryVersion ATLAS-R2-2015-03-01-00 --conditionsTag OFLCOND-RUN12-SDR-31 --preSimExec 'from TrkDetDescrSvc.TrkDetDescrJobProperties import TrkDetFlags;TrkDetFlags.TRT_BuildStrawLayers=True' --preSimInclude FastChainPileup/FastPileup.py --postInclude='PyJobTransforms/UseFrontier.py,G4AtlasTests/postInclude.DCubeTest_FCpileup.py,DigitizationTests/postInclude.RDO_Plots.py' --postExec 'from AthenaCommon.ConfigurationShelve import saveToAscii;saveToAscii("config.txt")' --DataRunNumber '284500' --postSimExec='genSeq.Pythia8.NCollPerEvent=10;' 
+FastChain_tf.py --simulator ATLFASTIIF_PileUp \
+    --digiSteeringConf "SplitNoMerge" \
+    --useISF True \
+    --randomSeed 123 \
+    --enableLooperKiller True \
+    --inputEVNTFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/ttbar_muplusjets-pythia6-7000.evgen.pool.root --outputRDOFile RDO_pileup_fastsim_fulldigi.pool.root \
+    --maxEvents 100 \
+    --skipEvents 0 \
+    --geometryVersion ATLAS-R2-2015-03-01-00 \
+    --conditionsTag OFLCOND-RUN12-SDR-31 \
+    --preSimExec 'from TrkDetDescrSvc.TrkDetDescrJobProperties import TrkDetFlags;TrkDetFlags.TRT_BuildStrawLayers=True' \
+    --preSimInclude FastChainPileup/FastPileup.py \
+    --postInclude='PyJobTransforms/UseFrontier.py,G4AtlasTests/postInclude.DCubeTest_FCpileup.py,DigitizationTests/postInclude.RDO_Plots.py' \
+    --postExec 'from AthenaCommon.ConfigurationShelve import saveToAscii;saveToAscii("config.txt")' \
+    --DataRunNumber '284500' \
+    --postSimExec='genSeq.Pythia8.NCollPerEvent=10;' 
 
 echo "art-result: $? evgen step"
 
@@ -17,7 +34,7 @@ ArtJobName=$2
 art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}  
 echo  "art-result: $? regression"
 #add an additional payload from the job (corollary file).                                                           
-# art-output: InDetStandardPlots.root  
+# art-output: InDetStandardPlots.root
 /cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube TEST_ttFC_fastSim_fullDigi InDetStandardPlots.root /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/dcube_configs/config/dcube_indetplots_no_pseudotracks.xml /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/InDetStandardPlots_TEST.root
 
 
