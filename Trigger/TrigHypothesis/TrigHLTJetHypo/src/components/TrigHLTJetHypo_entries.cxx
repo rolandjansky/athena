e156768a9a32cf26b15ee287f05a10bbf94662bc
sherwood <peter.sherwood@cern.ch>
2018-06-22 20:05:51 +0200

    Remove out of date hypo tools.
    Former-commit-id: 0063480e08a079cd9c521296e20647efa03acee2

--- Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
@@ -4,12 +4,7 @@
 #include "TrigHLTJetHypo/TrigEFDPhiMetJetAllTE.h"
 // #include "TrigHLTJetHypo/TrigHLTJetHypo_HT.h"
 
-#include "../TrigHLTJetHypo_EtaEtTool.h"
-#include "../TrigHLTJetHypo_DijetMassDEtaTool.h"
-#include "../TrigHLTJetHypo_SMCTool.h"
-#include "../TrigHLTJetHypo_TLATool.h"
 #include "../TrigJetHypoAlg.h"
-#include "../TrigJetHypoTool_EtaEt.h"
 
 #include "../TrigJetHypoToolConfig_EtaEt.h"
 #include "../TrigJetHypoToolMT.h"
@@ -19,14 +14,6 @@ DECLARE_COMPONENT( TrigJetHypoAlg )
 
 DECLARE_COMPONENT( TrigEFRazorAllTE )
 DECLARE_COMPONENT( TrigEFDPhiMetJetAllTE )
-
-DECLARE_COMPONENT( TrigHLTJetHypo_EtaEtTool )
-DECLARE_COMPONENT( TrigHLTJetHypo_DijetMassDEtaTool )
-DECLARE_COMPONENT( TrigHLTJetHypo_SMCTool )
-DECLARE_COMPONENT( TrigHLTJetHypo_TLATool )
-
-
-DECLARE_COMPONENT( TrigJetHypoTool_EtaEt )
 
 DECLARE_COMPONENT(TrigJetHypoToolMT)
 DECLARE_COMPONENT(TrigJetHypoToolConfig_EtaEt)
