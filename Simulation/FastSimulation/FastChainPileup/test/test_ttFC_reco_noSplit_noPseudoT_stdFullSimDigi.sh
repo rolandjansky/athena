65abeb0527e8a1128cfd431e5e2840656355d436
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-06-22 21:01:24 +0000

    Merge branch 'cherry-pick-79323265db [formerly 1e700abaa5]-21.3' into '21.3'
    Sweeping !12158 from 21.0 to 21.3.
    Removed whitespace on FastChain jobs and did some minor cosmetic cleanups
    
    See merge request atlas/athena!12260
    
    (cherry picked from commit 05cecd509cbfe9511162dce5287961dfdc5b7211 [formerly 649ef6ca96322d5b83d5f340012e44c6674f5254])
    
    13285dd0 Merge branch 'ART_TESTS_v4' into '21.0'
    
    Former-commit-id: 862efff8d6f33f73e34195184a6d130d69c54a86

--- Simulation/FastSimulation/FastChainPileup/test/test_ttFC_reco_noSplit_noPseudoT_stdFullSimDigi.sh
+++ Simulation/FastSimulation/FastChainPileup/test/test_ttFC_reco_noSplit_noPseudoT_stdFullSimDigi.sh
@@ -3,28 +3,59 @@
 # art-type: grid
 # 
 
-# specify branches of athena that are being targeted:                                                               
-# art-include: 21.0/Athena                                                                                          
-# art-include: 21.3/Athena                                                                                          
-# Also include temporary branch 21.3-hmpl                                                                           
-# art-include: 21.3-hmpl/Athena       
+# specify branches of athena that are being targeted:
 
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+# Also include temporary branch 21.3-hmpl
+# art-include: 21.3-hmpl/Athena
 # job 1: Simulation from evgen
-Sim_tf.py --conditionsTag 'default:OFLCOND-RUN12-SDR-19' --physicsList 'FTFP_BERT' --truthStrategy 'MC15aPlus' --simulator 'FullG4' --postInclude 'default:RecJobTransforms/UseFrontier.py,G4AtlasTests/postInclude.DCubeTest.py' --preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py,SimulationJobOptions/preInclude.FrozenShowersFCalOnly.py' --DataRunNumber '222525' --geometryVersion 'default:ATLAS-R2-2015-03-01-00_VALIDATION' --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/mc12_valid.110401.PowhegPythia_P2012_ttbar_nonallhad.evgen.EVNT.e3099.01517252._000001.pool.root.1" --outputHITSFile "Hits.pool.root" --maxEvents 50
+
+Sim_tf.py --conditionsTag 'default:OFLCOND-RUN12-SDR-19' \
+    --physicsList 'FTFP_BERT' \
+    --truthStrategy 'MC15aPlus' \
+    --simulator 'FullG4' \
+    --postInclude 'default:RecJobTransforms/UseFrontier.py,G4AtlasTests/postInclude.DCubeTest.py' \
+    --preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py,SimulationJobOptions/preInclude.FrozenShowersFCalOnly.py' \
+    --DataRunNumber '222525' \
+    --geometryVersion 'default:ATLAS-R2-2015-03-01-00_VALIDATION' \
+    --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/mc12_valid.110401.PowhegPythia_P2012_ttbar_nonallhad.evgen.EVNT.e3099.01517252._000001.pool.root.1" \
+    --outputHITSFile "Hits.pool.root" \
+    --maxEvents 50
 
 echo "art-result: $? evgen step"
 
 #merging of hits file
 
-HITSMerge_tf.py --inputHITSFile='Hits.pool.root' --outputHITS_MRGFile='Merge.pool.root' --maxEvents=50 --skipEvents='0' --geometryVersion 'ATLAS-R2-2015-03-01-00' --conditionsTag 'OFLCOND-RUN12-SDR-19'
+HITSMerge_tf.py --inputHITSFile='Hits.pool.root' \
+    --outputHITS_MRGFile='Merge.pool.root' \
+    --maxEvents=50 \
+    --skipEvents='0' \
+    --geometryVersion 'ATLAS-R2-2015-03-01-00' \
+    --conditionsTag 'OFLCOND-RUN12-SDR-19'
 
 echo "art-result: $? Merge step"
 #digi
-Digi_tf.py --inputHITSFile 'Merge.pool.root' --outputRDOFile 'RDO.pool.root' --maxEvents '50' --skipEvents '0' --geometryVersion 'ATLAS-R2-2015-03-01-00' --digiSeedOffset1 '123456' --digiSeedOffset2 '2345678' --postInclude 'PyJobTransforms/UseFrontier.py' --AddCaloDigi 'False' --conditionsTag 'OFLCOND-RUN12-SDR-31'
+Digi_tf.py --inputHITSFile 'Merge.pool.root' \
+    --outputRDOFile 'RDO.pool.root' \
+    --maxEvents '50' \
+    --skipEvents '0' \
+    --geometryVersion 'ATLAS-R2-2015-03-01-00' \
+    --digiSeedOffset1 '123456' \
+    --digiSeedOffset2 '2345678' \
+    --postInclude 'PyJobTransforms/UseFrontier.py' \
+    --AddCaloDigi 'False' \
+    --conditionsTag 'OFLCOND-RUN12-SDR-31'
 
 echo "art-result: $? Digi step"
 
-FastChain_tf.py --maxEvents 50 --skipEvents 0 --geometryVersion ATLAS-R2-2015-03-01-00 --conditionsTag OFLCOND-RUN12-SDR-31  --inputRDOFile RDO.pool.root --outputAODFile AOD_Split_stdFullSimDigi.pool.root --preExec "RAWtoESD:rec.doTrigger.set_Value_and_Lock(False);recAlgs.doTrigger.set_Value_and_Lock(False);from InDetRecExample.InDetJobProperties import InDetFlags;InDetFlags.doStandardPlots.set_Value_and_Lock(True);"
+FastChain_tf.py --maxEvents 50 \
+    --skipEvents 0 \
+    --geometryVersion ATLAS-R2-2015-03-01-00 \
+    --conditionsTag OFLCOND-RUN12-SDR-31 \
+    --inputRDOFile RDO.pool.root \
+    --outputAODFile AOD_Split_stdFullSimDigi.pool.root \
+    --preExec "RAWtoESD:rec.doTrigger.set_Value_and_Lock(False);recAlgs.doTrigger.set_Value_and_Lock(False);from InDetRecExample.InDetJobProperties import InDetFlags;InDetFlags.doStandardPlots.set_Value_and_Lock(True);"
 #end of job
 echo "art-result: $? AOD step"
 #add an additional payload from the job (corollary file).
