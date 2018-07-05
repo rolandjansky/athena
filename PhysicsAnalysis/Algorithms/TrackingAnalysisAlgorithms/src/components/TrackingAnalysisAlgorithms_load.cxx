ddf804b4079a6558db855ad4e5f5ff7e29d99d5d
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2018-07-05 12:30:13 +0200

    Created the TrackingAnalysisAlgorithms package.
    I had to add a filter algorithm for selecting events with at least
    one primary vertex, and this seemed the best way to go about it.
    
    
    Former-commit-id: 50a6092ac5ffb79d3b74fe028888ab2592f84491

---
+++ PhysicsAnalysis/Algorithms/TrackingAnalysisAlgorithms/src/components/TrackingAnalysisAlgorithms_load.cxx
@@ -0,0 +1,9 @@
+//
+// Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+//
+
+// Framework include(s):
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+// Declare the library to Gaudi:
+LOAD_FACTORY_ENTRIES( TrackingAnalysisAlgorithms )
