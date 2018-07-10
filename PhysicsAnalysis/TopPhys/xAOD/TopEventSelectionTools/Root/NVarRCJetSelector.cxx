ac4d2537df87d169bd0477e10ab5fd556f7c7675
Petr Jacka <petr.jacka@cern.ch>
2018-07-10 14:57:23 +0200

    Adding NVarRCJetSelector
    Former-commit-id: 3036d718517d971c22097587784b953b6152aad5

---
+++ PhysicsAnalysis/TopPhys/xAOD/TopEventSelectionTools/Root/NVarRCJetSelector.cxx
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "TopEventSelectionTools/NVarRCJetSelector.h"
+#include "TopEvent/EventTools.h"
+#include <iostream>
+
+namespace top{
+
+  NVarRCJetSelector::NVarRCJetSelector(const std::string& name,const std::string& params) : 
+      SignValueSelector("VRCJET_N " + name, params, true),
+      m_name(name){
+    checkMultiplicityIsInteger();
+    std::cout << "NVarRCJetSelector::NVarRCJetSelector: m_name = " << m_name << std::endl;
+  }
+
+  bool NVarRCJetSelector::apply(const top::Event& event) const {
+    auto func = [&](const xAOD::Jet* jetPtr){return jetPtr->pt() > value();};
+    top::check(event.m_VarRCJets.find(m_name)!=event.m_VarRCJets.end(),"Error in NVarRCJetSelector: Variable-R reclustered jets with parameter " + m_name + " not defined!" );
+    std::unordered_map< std::string,std::shared_ptr<xAOD::JetContainer> > VarRCJets=event.m_VarRCJets;
+    auto count = std::count_if(VarRCJets[m_name]->begin(), VarRCJets[m_name]->end(), func);
+    return checkInt(count, multiplicity());
+  }
+
+  bool NVarRCJetSelector::applyParticleLevel(const top::ParticleLevelEvent& /*event*/) const { // To be implemented
+    //auto func = [&](const xAOD::Jet* jetPtr){return jetPtr->pt() > value();};
+    //auto count = std::count_if(event.m_largeRJets->begin(), event.m_largeRJets->end(), func);
+    //return checkInt(count, multiplicity());
+    return true; 
+  }
+
+}
--- PhysicsAnalysis/TopPhys/xAOD/TopEventSelectionTools/Root/TopEventSelectionToolsLoader.cxx
+++ PhysicsAnalysis/TopPhys/xAOD/TopEventSelectionTools/Root/TopEventSelectionToolsLoader.cxx
@@ -52,6 +52,7 @@
 #include "TopEventSelectionTools/RunNumberSelector.h"
 #include "TopEventSelectionTools/NLargeJetSelector.h"
 #include "TopEventSelectionTools/NRCJetSelector.h"
+#include "TopEventSelectionTools/NVarRCJetSelector.h"
 
 #include "TopConfiguration/TopConfig.h"
 
@@ -80,6 +81,12 @@ namespace top {
         return new top::NLargeJetSelector(param);
     else if (toolname == "RCJET_N")
         return new top::NRCJetSelector(param);
+    else if (toolname == "VRCJET_N"){
+	std::istringstream is(param);
+	std::string name;
+	getline(is,name,' ' );
+        return new top::NVarRCJetSelector(name,param.substr(name.size()+1));
+    }
     else if (toolname == "MV1_N")
         return new top::MV1Selector(param);
     else if (toolname == "MV2C20_N")
