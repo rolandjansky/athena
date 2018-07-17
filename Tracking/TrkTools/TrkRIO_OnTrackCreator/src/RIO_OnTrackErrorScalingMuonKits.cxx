bb3c141043e5dd87e5c4f41fec5029c94cf8cb8f
Goetz Gaycken <goetz.gaycken@cern.ch>
2018-07-17 17:50:44 +0200

    Replace tool for TGC, RPC, CSC RIO error scaling by specialised conditions data.
    The TGC, RPC, and CSC RIO on track error scaling is now handled by
    specialised conditions data rather than a tool where the conditions data
    is created by a conditions algorithm.
    
    
    Former-commit-id: 5975f71cf8d1f10cb0dfefd489ad2e4dacf08282

---
+++ Tracking/TrkTools/TrkRIO_OnTrackCreator/src/RIO_OnTrackErrorScalingMuonKits.cxx
@@ -0,0 +1,44 @@
+#include "RIO_OnTrackErrorScalingKit.h"
+#include "MuonRIO_OnTrack/MuonEtaPhiRIO_OnTrackErrorScaling.h"
+
+typedef char * CharPtr_t;
+class MuonEtaPhiRIO_OnTrackErrorScalingKit
+  : public RIO_OnTrackErrorScalingSpecialisedKit<MuonEtaPhiRIO_OnTrackErrorScaling>
+{
+public:
+  MuonEtaPhiRIO_OnTrackErrorScalingKit(const std::string &prefix) {
+    // use C-style array of strings to satisfy the interface
+    m_names=new CharPtr_t[MuonEtaPhiRIO_OnTrackErrorScaling::kNParamTypes];
+    for (unsigned int idx=0; idx< MuonEtaPhiRIO_OnTrackErrorScaling::kNParamTypes; ++idx ) {
+      std::string tmp( prefix + " " + MuonEtaPhiRIO_OnTrackErrorScaling::paramNames()[idx] );
+      tmp.shrink_to_fit();
+      m_names[idx]=new char[ tmp.capacity() ];
+      strncpy( m_names[idx], tmp.c_str(), tmp.capacity() );
+    }
+  }
+  ~MuonEtaPhiRIO_OnTrackErrorScalingKit() {
+    for (unsigned idx=0; idx<MuonEtaPhiRIO_OnTrackErrorScaling::kNParamTypes; ++idx) {
+      delete [] m_names[idx];
+    }
+    delete [] m_names;
+  }
+  virtual unsigned int nParametres() const override {
+    return MuonEtaPhiRIO_OnTrackErrorScaling::kNParamTypes;
+  }
+  virtual const char **paramNames()  const override {
+    return const_cast<const char **>(m_names);
+  }
+private:
+  char **m_names;
+};
+
+
+namespace {
+  // register all kits with the help of a dummy function which sets a global anonymous bool
+  bool registered = ( []() -> bool {
+    return
+         RIO_OnTrackErrorScalingKitManager::instance().registerKit("RPCRIO_OnTrackErrorScaling",    new MuonEtaPhiRIO_OnTrackErrorScalingKit("RPC"))
+      && RIO_OnTrackErrorScalingKitManager::instance().registerKit("TGCRIO_OnTrackErrorScaling",    new MuonEtaPhiRIO_OnTrackErrorScalingKit("TGC"))
+      && RIO_OnTrackErrorScalingKitManager::instance().registerKit("CSCRIO_OnTrackErrorScaling",    new MuonEtaPhiRIO_OnTrackErrorScalingKit("CSC"));
+    } ) ();
+}
