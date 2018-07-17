f3adf709bdc28cf751d4ad0b245d0ed3731184b2
tkharlam <tatyana.kharlamova@cern.ch>
2018-07-17 10:44:15 +0200

    add art-include for Overlay ART
    Former-commit-id: 24278cfa536ba7c2c3568412c68ac91772da3aa9

--- Event/EventOverlay/OverlayMonitoringRTT/test/test_MCOverlayMC16_singlemu.sh
+++ Event/EventOverlay/OverlayMonitoringRTT/test/test_MCOverlayMC16_singlemu.sh
@@ -2,7 +2,9 @@
 
 # art-description: MC+MC Overlay for MC16, single mu, no trigger
 # art-type: grid
-# art-ci: 21.0
+# art-include: 21.0/Athena                                
+# art-include: 21.3/Athena                                        
+# art-include: master/Athena 
 
 # art-output: *.root
 # art-output: log.*
