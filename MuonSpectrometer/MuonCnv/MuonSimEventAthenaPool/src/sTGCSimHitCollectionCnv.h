26441e74fcd2f3e44cef91c22907752a3b1c0a83
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-07-18 08:55:11 +0000

    Merge branch 'NSWsTGCHit1306' into '21.3'
    Add new sTGCSimHit (ATLASSIM-3755)
    
    See merge request atlas/athena!12093
    
    
    Former-commit-id: 0b08f5c3956b53c1dc6aa2f99acc164e94e9af35

---
+++ MuonSpectrometer/MuonCnv/MuonSimEventAthenaPool/src/sTGCSimHitCollectionCnv.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef MUONSIMEVENTATHENAPOOL_STGCSIMHITCOLLECTIONCNV_H
+#define MUONSIMEVENTATHENAPOOL_STGCSIMHITCOLLECTIONCNV_H
+
+#include "AthenaPoolCnvSvc/T_AthenaPoolCustomCnv.h"
+#include "MuonSimEvent/sTGCSimHitCollection.h"
+
+#include "MuonSimEventTPCnv/sTGCSimHitCollectionCnv_p1.h"
+#include "MuonSimEventTPCnv/sTGCSimHitCollectionCnv_p2.h"
+
+// the latest persistent representation type of DataCollection:
+typedef  Muon::sTGCSimHitCollection_p2  sTGCSimHitCollection_PERS;
+typedef  T_AthenaPoolCustomCnv<sTGCSimHitCollection, sTGCSimHitCollection_PERS >  sTGCSimHitCollectionCnvBase;
+
+/**
+ ** Create derived converter to customize the saving of identifiable
+ ** container
+ **/
+class sTGCSimHitCollectionCnv : 
+    public sTGCSimHitCollectionCnvBase 
+{
+    
+    friend class CnvFactory<sTGCSimHitCollectionCnv>;
+    
+public:
+    sTGCSimHitCollectionCnv(ISvcLocator* svcloc);
+    virtual ~sTGCSimHitCollectionCnv();
+    
+    virtual sTGCSimHitCollection_PERS*   createPersistent (sTGCSimHitCollection* transCont);
+    virtual sTGCSimHitCollection*        createTransient ();
+        
+private:
+    sTGCSimHitCollectionCnv_p1    m_TPConverter_p1;
+    sTGCSimHitCollectionCnv_p2    m_TPConverter_p2;
+};
+
+#endif
