b43e1ea453025631bf5b0252856f497df9582ff3
Tomasz Bold <tomasz.bold@gmail.com>
2018-07-19 13:32:29 +0200

    moved to new trigger flags
    Former-commit-id: aba7005db86114d7fdad5aa7eb59764cc33579d7

--- Trigger/TrigSteer/L1Decoder/python/L1MuonConfig.py
+++ Trigger/TrigSteer/L1Decoder/python/L1MuonConfig.py
@@ -96,8 +96,6 @@ if __name__ == '__main__':
     Configurable.configurableRun3Behavior=1
     from AthenaConfiguration.AllConfigFlags import ConfigFlags
 
-    ConfigFlags.set('Trigger.L1Decoder.doCalo',True)
-    ConfigFlags.set('Trigger.L1Decoder.doMuon',True)
     ConfigFlags.set('global.InputFiles',["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1",])
     ConfigFlags.lock()
 
