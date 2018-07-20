75535ad6d757de53dcf50589b499d390e029bd5e
Goetz Gaycken <goetz.gaycken@cern.ch>
2018-07-20 10:38:02 +0200

    Added missing variables for LC jet variants.
    Former-commit-id: 411522e5ba55a60fd300b8e84219a90e868f03be

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5Common.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5Common.py
@@ -17,7 +17,8 @@ def getHIGG5Common() :
         ("AntiKt10LCTopoJets.GhostVR30Rmax4Rmin02TrackJet"
             ".NumTrkPt1000.NumTrkPt500.TrackWidthPt1000.TrackWidthPt500.SumPtTrkPt1000.SumPtTrkPt500"),
         ("AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets"
-            ".PlanarFlow.Angularity.Aplanarity.FoxWolfram0.FoxWolfram2.KtDR.ZCut12"),
+            ".PlanarFlow.Angularity.Aplanarity.FoxWolfram0.FoxWolfram2.KtDR.ZCut12"
+            ".GhostTrack.GhostTrackCount"),
         ("AntiKt10TrackCaloClusterTrimmedPtFrac5SmallR20Jets.pt.eta.phi.m.constituentLinks"
             ".JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m"
             ".Angularity.Aplanarity.DetectorEta.ECF1.ECF2.ECF3.FoxWolfram0.FoxWolfram2.GhostMuonSegmentCount.GhostTrackCount.KtDR.Parent"
@@ -25,7 +26,7 @@ def getHIGG5Common() :
             ".NumTrkPt1000.NumTrkPt500.TrackWidthPt1000.TrackWidthPt500.SumPtTrkPt1000.SumPtTrkPt500"
             ".GhostAntiKt2TrackJet.GhostTrack"),
         ("AntiKt10TrackCaloClusterJets"
-         ".NumTrkPt1000.NumTrkPt500.SumPtTrkPt1000.SumPtTrkPt500.TrackWidthPt1000.TrackWidthP"),
+         ".NumTrkPt1000.NumTrkPt500.SumPtTrkPt1000.SumPtTrkPt500.TrackWidthPt1000.TrackWidthPt500"),
         "BTagging_AntiKtVR30Rmax4Rmin02Track.MV2c10_discriminant",
         "BTagging_AntiKt4EMTopo.MV2cl100_discriminant",
         "BTagging_AntiKt4EMPFlow.MV2cl100_discriminant",
