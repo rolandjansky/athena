d76e7f8ecf3decf6542018657f5bb9aec1bf1b4a
Dmitriy Maximov <Dmitriy.Maximov@cern.ch>
2018-07-23 15:06:16 +0200

    Copyright update
    Former-commit-id: 1485905c414aaaa8a72a4e136f24ecfef9ada63c

--- Trigger/TriggerCommon/TriggerMenu/python/egamma/EgammaDef.py
+++ Trigger/TriggerCommon/TriggerMenu/python/egamma/EgammaDef.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
 __author__  = 'Ryan White'
 __version__=""
