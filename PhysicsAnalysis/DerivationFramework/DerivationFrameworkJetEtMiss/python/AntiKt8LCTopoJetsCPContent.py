15160c5de8c5a5524404b5990d276524dd7aa29e
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2018-07-23 17:17:16 +0200

    Add DetectorY moment to reco jets
    Former-commit-id: 6cc500dbad8511b193098c7100ab8001dbf5f8a8

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt8LCTopoJetsCPContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt8LCTopoJetsCPContent.py
@@ -4,7 +4,7 @@ AntiKt8LCTopoJetsCPContent = [
 "Kt4LCTopoOriginEventShape",
 "Kt4LCTopoOriginEventShapeAux.Density",
 "AntiKt8LCTopoJets",
-"AntiKt8LCTopoJetsAux.pt.eta.phi.m.JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m.NumTrkPt500.SumPtTrkPt500.EnergyPerSampling.ActiveArea4vec_eta.ActiveArea4vec_m.ActiveArea4vec_phi.ActiveArea4vec_pt.DetectorEta.FracSamplingMax.FracSamplingMaxIndex.GhostTrack.Jvt.JVFCorr.JvtRpt.NumTrkPt1000.TrackWidthPt1000.GhostMuonSegmentCount.PartonTruthLabelID.ConeTruthLabelID.HadronConeExclExtendedTruthLabelID.HadronConeExclTruthLabelID.TrueFlavor.Timing",
+"AntiKt8LCTopoJetsAux.pt.eta.phi.m.JetConstitScaleMomentum_pt.JetConstitScaleMomentum_eta.JetConstitScaleMomentum_phi.JetConstitScaleMomentum_m.NumTrkPt500.SumPtTrkPt500.EnergyPerSampling.ActiveArea4vec_eta.ActiveArea4vec_m.ActiveArea4vec_phi.ActiveArea4vec_pt.DetectorEta.DetectorY.FracSamplingMax.FracSamplingMaxIndex.GhostTrack.Jvt.JVFCorr.JvtRpt.NumTrkPt1000.TrackWidthPt1000.GhostMuonSegmentCount.PartonTruthLabelID.ConeTruthLabelID.HadronConeExclExtendedTruthLabelID.HadronConeExclTruthLabelID.TrueFlavor.Timing",
 "MET_Track",
 "MET_TrackAux.name.mpx.mpy",
 "MuonSegments",
