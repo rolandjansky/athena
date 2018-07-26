73acff5e190ab1747cc0ef4b682ccda99f5e7c50
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2018-07-26 10:14:46 +0200

    DataHandle migration for CopyTruth*Particles
    Former-commit-id: 695112cd3a9b84597c88c30a1fb418e4525949ea

--- PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/CopyTruthParticles.cxx
+++ PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/CopyTruthParticles.cxx
@@ -3,10 +3,7 @@
 */
 
 #include "ParticleJetTools/CopyTruthParticles.h"
-#include "xAODTruth/TruthParticleContainer.h"
-#include "xAODTruth/TruthParticleAuxContainer.h"
-#include "xAODTruth/TruthEventContainer.h"
-#include "AthContainers/ConstDataVector.h"
+#include <memory>
 #include "TruthUtils/PIDHelpers.h"
 #include "AsgTools/Check.h"
 
@@ -16,39 +13,53 @@ using namespace std;
 CopyTruthParticles::CopyTruthParticles(const std::string& name)
   : AsgTool(name)
 {
-  declareProperty("OutputName", m_outputname="TagInputs", "Name of the resulting IParticle collection");
   declareProperty("PtMin", m_ptmin=0, "Minimum pT of particles to be accepted for tagging (in MeV)");
 }
 
+StatusCode CopyTruthParticles::initialize() {
+
+  ATH_CHECK(m_truthEventKey.initialize());
+  ATH_CHECK(m_outTruthPartKey.initialize());
+
+  return StatusCode::SUCCESS;
+}
 
 int CopyTruthParticles::execute() const {
   // Retrieve the xAOD truth objects
-  const xAOD::TruthEventContainer* xTruthEventContainer = NULL;
-  ASG_CHECK( evtStore()->retrieve( xTruthEventContainer, "TruthEvents"), 1);
-
-  // Make a new TruthParticleContainer and link it to StoreGate
-  if (evtStore()->contains<xAOD::TruthParticleContainer>(m_outputname))
-    ATH_MSG_ERROR("Tag input TruthParticleContainer " << m_outputname << " already exists");
-
-  ConstDataVector<xAOD::TruthParticleContainer> *ipc = new ConstDataVector<xAOD::TruthParticleContainer>(SG::VIEW_ELEMENTS);
-  if (evtStore()->record(ipc, m_outputname).isFailure())
-    ATH_MSG_ERROR("Failed to record a new TruthParticleContainer " << m_outputname);
-
+  auto truthEvents = SG::makeHandle(m_truthEventKey);
+  if ( !truthEvents.isValid() ) {
+    ATH_MSG_ERROR("Failed to retrieve truth event container " << m_truthEventKey.key());
+    return 1;
+  }
+  
   // Classify particles for tagging and add to the TruthParticleContainer
-  const xAOD::TruthEvent* evt = *xTruthEventContainer->begin();
+  std::unique_ptr<ConstDataVector<xAOD::TruthParticleContainer> > ptruth(new ConstDataVector<xAOD::TruthParticleContainer>(SG::VIEW_ELEMENTS));
   size_t numCopied = 0;
-  for (unsigned int ip = 0; ip < evt->nTruthParticles(); ++ip) {
-    const xAOD::TruthParticle* tp = evt->truthParticle(ip);
+  const xAOD::TruthEvent& hsevt = *truthEvents->front();
+  for (size_t itp(0); itp<hsevt.nTruthParticles(); ++itp) {
+    const xAOD::TruthParticle* tp = hsevt.truthParticle(itp);
     if (!tp || tp->pt() < m_ptmin)
         continue;
 
     if (classify(tp)) {
-      ipc->push_back(tp);
+      ptruth->push_back(tp);
       numCopied += 1;
     }
   }
 
   ATH_MSG_DEBUG("Copied " << numCopied << " truth particles into " << m_outputname << " TruthParticle container");
+
+  // record
+  auto truthParticles_out = SG::makeHandle(m_outTruthPartKey);
+  ATH_MSG_DEBUG("Recorded truth particle collection " << m_outTruthPartKey.key());
+  // notify
+  if (!truthParticles_out.put(std::move(ptruth))) {
+    ATH_MSG_ERROR("Unable to write new TruthParticleContainer to event store: " 
+                  << m_outTruthPartKey.key());
+  } else {
+    ATH_MSG_DEBUG("Created new TruthParticleContainer in event store: " 
+                  << m_outTruthPartKey.key());
+  }
 
   return 0;
 }
