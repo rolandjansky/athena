d15e3276cb0aa1be72493c57f60e9b0e25828651
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-07-30 07:39:42 +0000

    Merge branch 'DefaultNSWParams' into '21.3'
    Remove RUN3 default reading of RUN2 XML
    
    See merge request atlas/athena!13068
    
    (cherry picked from commit 46a70378c5ba5a4c727cb1260ac59d7baa782cb5 [formerly 28a68884a03b813095a310f4e0b47a6452b5f5d2])
    
    e48a6156 Remove RUN3 default reading of RUN2 XML
    
    Former-commit-id: b2d8378c63cbb3646d4c5de0446d87eb93984846

--- MuonSpectrometer/MuonDetDescr/MuonAGDD/python/MuonAGDDConfig.py
+++ MuonSpectrometer/MuonDetDescr/MuonAGDD/python/MuonAGDDConfig.py
@@ -1,6 +1,12 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
+# As of the summer of 2018, there is no way to turn off the NSW for RUN3 configuration
+# By default for RUN3, readAGDD is true so the NSW information is pulled directly from the database
+# If the user sets readAGDD to false in the config files, an XML file must be defined also.
+# Otherwise, with readAGDD overwritten to false, the job will crash 
+
 from AthenaCommon import CfgMgr
+from AtlasGeoModel.CommonGMJobProperties import CommonGeometryFlags
 
 def MuonAGDDTool(name="MuonSpectrometer", **kwargs):
     kwargs.setdefault("BuildNSW", False)
@@ -8,7 +14,8 @@ def MuonAGDDTool(name="MuonSpectrometer", **kwargs):
 
 def NSWAGDDTool(name="NewSmallWheel", **kwargs):
     kwargs.setdefault("Locked", False)
-    kwargs.setdefault("XMLFiles", ["NSW_StationDescription.xml"])
+    if not CommonGeometryFlags.Run() == "RUN3" :
+        kwargs.setdefault("XMLFiles", ["NSW_StationDescription.xml"])
     kwargs.setdefault("Volumes", ["NewSmallWheel"])
     kwargs.setdefault("DefaultDetector", "Muon")
     return CfgMgr.NSWAGDDTool(name, **kwargs)
