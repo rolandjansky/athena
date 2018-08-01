766509f93e8a06937b5036e4474d7a9b5bc91aa5
iconnell <ian.connelly@cern.ch>
2018-08-01 15:57:47 +0100

    Adding new ART tests, for testing
    Former-commit-id: 8af1113cfbe79c37c21d7ddd00a6d02783d7ec0e

---
+++ PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/test/test_410470afmc16a.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python                                                                                         
+
+# art-description: DxAOD MC16a TOPQ1 AtlFast2 410470                                                          
+# art-type: grid                                                                                              
+# art-output: output.root                                                                                     
+# art-input: mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_TOPQ1.e6337_a875_r9364_p3554
+
+import ROOT
+from PathResolver import PathResolver
+import subprocess, sys, shlex, random, shutil, os, glob
+
+# Create empty PoolFileCatalog
+os.system('art.py createpoolfile')
+
+# -- Settings --
+cutfilename    = "artcut_"+sys.argv[0].replace(".py",".txt")
+inputfilenames = glob.glob("*.root*")
+
+# -- Get the validation file path from the most recent location --
+cutfilepath   = ROOT.PathResolver.find_file(cutfilename,
+                                            "DATAPATH", 
+                                            ROOT.PathResolver.RecursiveSearch)
+
+# -- Print the file location for debugging --
+print "ART Test : Using cutfile (%s) from %s"%(cutfilename, cutfilepath)
+print "ART Test : Using inputfiles :",inputfilenames
+print "Running on full statistics"
+
+# -- Copy the cutfile locally to be updated -- 
+shutil.copyfile(cutfilepath, cutfilename)
+
+# -- Write the input file path to a temporary file --
+inputfilepath = open("input.txt","w")
+for filename in inputfilenames:
+    inputfilepath.write(filename+"\n")
+inputfilepath.close()
+
+# -- Run top-xaod --
+cmd  = "top-xaod %s input.txt"%(cutfilename)
+proc = subprocess.Popen(shlex.split(cmd))
+proc.wait()
+
+# -- Print the art-result return code
+print "art-result: " + str(proc.returncode)
+
+# -- Check the return code and exit this script with that --
+sys.exit( proc.returncode )
