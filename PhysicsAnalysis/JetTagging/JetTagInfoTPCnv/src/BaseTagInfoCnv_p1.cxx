016e3e049d47aa9bcf34f1ef11a868412ab2a3a2
Scott Snyder <scott.snyder@cern.ch>
2018-08-06 12:33:09 +0000

    JetTagInfoTPCnv, JetEvent, JetTagInfo: Get rid of uses of #define private.
    Former-commit-id: cf902ca0528c6b792eff646408f7a11606a086db

--- PhysicsAnalysis/JetTagging/JetTagInfoTPCnv/src/BaseTagInfoCnv_p1.cxx
+++ PhysicsAnalysis/JetTagging/JetTagInfoTPCnv/src/BaseTagInfoCnv_p1.cxx
@@ -6,11 +6,7 @@
 /// Translator for the base tag info -- very simple, but common for everyone.
 ///
 
-#define private public
-#define protected public
 #include "JetTagInfo/BaseTagInfo.h"
-#undef private
-#undef protected
 #include "JetTagInfoTPCnv/BaseTagInfoCnv_p1.h"
 
 #include <iostream>
@@ -18,17 +14,18 @@
 namespace Analysis {
 
 void BaseTagInfoCnv_p1::transToPers(const BaseTagInfo* pa, BaseTagInfo_p1* pb, MsgStream &) {
-  pb->m_isValid = pa->m_isValid;
-  pb->m_tagLikelihood.assign(pa->m_tagLikelihood.begin(), pa->m_tagLikelihood.end());
-  pb->m_weight = pa->m_weight;
-  pb->m_tagJetInfoType = pa->m_tagJetInfoType;
+  pb->m_isValid = pa->isValid();
+  pb->m_tagLikelihood.assign(pa->tagLikelihood().begin(), pa->tagLikelihood().end());
+  pb->m_weight = pa->weight();
+  pb->m_tagJetInfoType = pa->infoType();
 }
 
 void BaseTagInfoCnv_p1::persToTrans(const BaseTagInfo_p1* pa, BaseTagInfo* pb, MsgStream &) {
-  pb->m_isValid = pa->m_isValid;
-  pb->m_tagLikelihood.assign (pa->m_tagLikelihood.begin(), pa->m_tagLikelihood.end());
-  pb->m_weight = pa->m_weight;
-  pb->m_tagJetInfoType = pa->m_tagJetInfoType;
+  pb->setValid (pa->m_isValid);
+  std::vector<double> tagLikelihood (pa->m_tagLikelihood.begin(), pa->m_tagLikelihood.end());
+  pb->setTagLikelihood (std::move (tagLikelihood));
+  pb->setWeight (pa->m_weight);
+  pb->setTagJetInfoType (pa->m_tagJetInfoType);
 }
 
 }
