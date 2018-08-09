8ab0514a5c70299f679865fdc9e0a42d250ab74b
Matthew Gignac <matthew.gignac@cern.ch>
2018-08-09 16:21:14 -0700

    adding DisplacedSoftPion into list of AOD creation and trk conversion
    Former-commit-id: 04e94bb6019efd7762d1c13edae32446d9e65acb

--- InnerDetector/InDetExample/InDetRecExample/share/InDetxAODCreator.py
+++ InnerDetector/InDetExample/InDetRecExample/share/InDetxAODCreator.py
@@ -1,10 +1,10 @@
 from InDetRecExample.InDetJobProperties import InDetFlags
 from InDetRecExample.InDetKeys import InDetKeys
 
-doCreation = ( InDetFlags.doNewTracking() or InDetFlags.doPseudoTracking() or InDetFlags.doLargeD0() or InDetFlags.doLowPtLargeD0() ) \
+doCreation = ( InDetFlags.doNewTracking() or InDetFlags.doPseudoTracking() or InDetFlags.doLargeD0() or InDetFlags.doLowPtLargeD0() or InDetFlags.doDisplacedSoftPion() ) \
                     and InDetFlags.doParticleCreation()
 doConversion = not InDetFlags.doNewTracking()  and not InDetFlags.doPseudoTracking() and not InDetFlags.doLargeD0() \
-                    and not InDetFlags.doLowPtLargeD0() and InDetFlags.doParticleConversion()
+                    and not InDetFlags.doLowPtLargeD0() and not InDetFlags.doDisplacedSoftPion() and InDetFlags.doParticleConversion()
 
 if doCreation:
     print "Creating xAOD::TrackParticles from Trk::Tracks"
