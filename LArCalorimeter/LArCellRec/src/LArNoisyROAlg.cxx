c105dffb68b6465d5e977e058be3baa319fc2cfe
Pavol Strizenec <pavol.strizenec@cern.ch>
2018-08-14 06:25:24 +0200

    including MR 13441 fix
    Former-commit-id: 17b6a9dafd66e6da8e6e2c7e2961d7254a387920

--- LArCalorimeter/LArCellRec/src/LArNoisyROAlg.cxx
+++ LArCalorimeter/LArCellRec/src/LArNoisyROAlg.cxx
@@ -27,6 +27,7 @@ StatusCode LArNoisyROAlg::initialize() {
   ATH_CHECK(m_noisyROTool.retrieve());
   ATH_CHECK(m_CaloCellContainerName.initialize());
   ATH_CHECK(m_outputKey.initialize());
+  ATH_CHECK(m_eventInfoKey.initialize());
   ATH_CHECK(m_knownBadFEBsVecKey.initialize() );
   ATH_CHECK(m_knownMNBFEBsVecKey.initialize() );
   return StatusCode::SUCCESS;
