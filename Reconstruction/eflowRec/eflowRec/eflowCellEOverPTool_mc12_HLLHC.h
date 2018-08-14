73e45980f80507d88977955878161b76f98b0509
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2018-08-14 09:49:53 +0100

    Change copyright year range to include 2018. Update comments for doxygen description of Reconstruction/eflowRec/eflowRec/eflowCellEOverPTool_mc12_HLLHC.h
    Former-commit-id: 1b240954cdfedf509f9b730f40c25e919d2801b3

--- Reconstruction/eflowRec/eflowRec/eflowCellEOverPTool_mc12_HLLHC.h
+++ Reconstruction/eflowRec/eflowRec/eflowCellEOverPTool_mc12_HLLHC.h
@@ -1,26 +1,17 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef EFLOWCELLEOVERPTOOL_MC12_HLLHC_H
 #define EFLOWCELLEOVERPTOOL_MC12_HLLHC_H
 
-/********************************************************************
-
-NAME:     eflowCellEOverPTool_mc12_HLLHC.h
-PACKAGE:  offline/Reconstruction/eflowRec
-
-AUTHORS:  M.Hodgkinson
-CREATED:  March 2014
-
-Description: New EM e/p for DC14 - JetMET ONLY - this is because taus already tuned PanTau on eflowRec, and hence prefer no updates for DC14
-
-********************************************************************/
-
 #include "eflowRec/IEFlowCellEOverPTool.h"
 
 class eflowBaseParameters;
 
+/**
+Class to store reference e/p mean and widths, as well as reference energy density radial profile fit parameters. The data is input to an eflowEEtaBinnedParameters object in the execute method. Stores data at the uncalibrated (EM) scale and is for HLLHC setup. This inherits from IEFlowCellEOverPTool. The values are derived using single pions with the AMI tags e3895_s3020_r9063.
+*/
 class eflowCellEOverPTool_mc12_HLLHC : public IEFlowCellEOverPTool {
 
  public:
--- Reconstruction/eflowRec/python/eflowObjectBuilderGetter_HLLHC.py
+++ Reconstruction/eflowRec/python/eflowObjectBuilderGetter_HLLHC.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.SystemOfUnits import *  # loads MeV etc...
 from AthenaCommon.Constants import * # Loads DEBUG INFO etc..
