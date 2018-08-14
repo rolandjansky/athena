bf16ceb01e80ae5401e9d79e656280e5f7c6bc41
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2018-08-14 10:25:03 +0100

    Remove not needed includes from Reconstruction/eflowRec/python/eflowCellLevelSubtractionToolDefault_HLLHC.py
    Former-commit-id: d3e10168c0c2e1f6155afc636b27613fcdb90392

--- Reconstruction/eflowRec/python/eflowCellLevelSubtractionToolDefault_HLLHC.py
+++ Reconstruction/eflowRec/python/eflowCellLevelSubtractionToolDefault_HLLHC.py
@@ -1,17 +1,9 @@
 # Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.Logging import logging
-from AthenaCommon.SystemOfUnits import *
-from AthenaCommon.Constants import *
-from AthenaCommon.Include import include
-#from AthenaCommon.GlobalFlags import *
-from AthenaCommon.JobProperties import jobproperties
 import traceback
-import sys
 # import the base class
 from eflowRec.eflowRecConf import eflowCellLevelSubtractionTool
-
-from eflowRec.eflowRecFlags import jobproperties
 
 class eflowCellLevelSubtractionToolDefault_HLLHC(eflowCellLevelSubtractionTool) :
 
