37c106227759200283af7eb7228e2aff7d598ef4
Nils Erik Krumnack <nils.erik.krumnack@cern.ch>
2018-08-14 19:54:36 +0000

    Sweep to master after conflicts solved
    Merge branch '21.2-hi-btagging' into '21.2'
    
    Added support for HI jet b-tagging (ATLASRECTS-4522)
    
    See merge request atlas/athena!12980
    
    
    Former-commit-id: 71783f592ad00cd6ee747b61a67e6839854fd70c

--- Event/xAOD/xAODJet/Root/JetContainerInfo.cxx
+++ Event/xAOD/xAODJet/Root/JetContainerInfo.cxx
@@ -74,6 +74,7 @@ namespace xAOD {
       static bool inited = false;
       if(inited) return;
       // name in the atlas convention
+<<<<<<< HEAD
       nameToTypemap["LCTopo"] =             LCTopo;            
       nameToTypemap["EMTopo"] =             EMTopo;            
       nameToTypemap["LCTopoOrigin"] =       LCTopoOrigin;
