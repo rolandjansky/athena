c7fc727cffee43f94a8eeeee4574803cb19d49fa
Rachael Creager <rcreager@cern.ch>
2018-08-17 19:17:22 +0200

    added diff-pool art echo, fixed data18 path
    Former-commit-id: 62cd7ae18900dd71a340859cd7acd86c2be11105

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data18FTAG1_compare.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_data18FTAG1_compare.sh
@@ -11,5 +11,6 @@ Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Deri
 
 echo "art-result: $? reco"
 
-art-diff.py ./DAOD_FTAG1.art.pool.root /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/ReferenceFiles/21.2.34.0/mc16_13TeV.361023.e3668_s3126_r10201/DAOD_FTAG1.mc16_dijet.pool.root 
+art-diff.py ./DAOD_FTAG1.art.pool.root /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/ReferenceFiles/21.2.34.0/data18_13TeV.00349533.f929_m1955._lb0352/DAOD_FTAG1.data18.pool.root --diff-type=diff-pool 
 
+echo  "art-result: $? diff-pool"
