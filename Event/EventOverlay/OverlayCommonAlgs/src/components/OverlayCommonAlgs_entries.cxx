389702bc5c7b70e558f08e04050795ed4bf96191
Tadej Novak <tadej.novak@cern.ch>
2018-08-17 10:54:29 +0200

    Remove obsolete G4HitMerger algorithm and G4HitMerge_trf
    Former-commit-id: 71aecb1ff6cd503f52af1014561e0142a18a8e4d

--- Event/EventOverlay/OverlayCommonAlgs/src/components/OverlayCommonAlgs_entries.cxx
+++ Event/EventOverlay/OverlayCommonAlgs/src/components/OverlayCommonAlgs_entries.cxx
@@ -2,13 +2,11 @@
 #include "../CopyTimings.h"
 #include "../CopyTruthInfo.h"
 #include "../BSFilter.h"
-#include "../G4HitMerger.h"
 #include "../ByteStreamMultipleOutputStreamCopyTool.h"
 
 DECLARE_COMPONENT( CopyMcEventCollection )
 DECLARE_COMPONENT( CopyTimings )
 DECLARE_COMPONENT( CopyTruthInfo )
 DECLARE_COMPONENT( BSFilter )
-DECLARE_COMPONENT( G4HitMerger )
 
 DECLARE_COMPONENT( ByteStreamMultipleOutputStreamCopyTool )
