4bfb5e245e82db90b4b0d410b74130808bada30c
Pavol Strizenec <pavol.strizenec@cern.ch>
2018-08-22 09:35:48 +0000

    Creating LAr HVId mapping conditions algo
    Former-commit-id: 4e1942358ff728abb7d2dd65c86a4c7f4be0e855

--- LArCalorimeter/LArExample/LArConditionsCommon/python/LArHVDB.py
+++ LArCalorimeter/LArExample/LArConditionsCommon/python/LArHVDB.py
@@ -6,5 +6,7 @@ from IOVDbSvc.CondDB import conddb
 if not conddb.isMC and not conddb.isOnline:
     conddb.addFolder("DCS_OFL","/LAR/DCS/HV/BARREl/I16")
     conddb.addFolder("DCS_OFL","/LAR/DCS/HV/BARREL/I8")
-    conddb.addFolder("LAR_OFL","/LAR/IdentifierOfl/HVLineToElectrodeMap")
+    conddb.addFolder("LAR_OFL","/LAR/IdentifierOfl/HVLineToElectrodeMap",className="AthenaAttributeList")
     conddb.addFolder("LAR_OFL","/LAR/HVPathologiesOfl/Pathologies")
+if conddb.isMC:
+    conddb.addFolderWithTag("LAR_OFL","/LAR/Identifier/HVLineToElectrodeMap","LARHVLineToElectrodeMap-001",className="AthenaAttributeList")
