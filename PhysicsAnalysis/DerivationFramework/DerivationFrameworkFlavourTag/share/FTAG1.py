593df926c99e01d04446552d79b8818ad594415a
Dan Guest <dguest@cern.ch>
2018-08-23 21:27:17 +0200

    Add GhostTag VR jets to FTAG2, Xbb tagger to FTAG1
    Former-commit-id: 8d8e82a2c1b83b12c07398b944eb269ffc1a2ea0

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkFlavourTag/share/FTAG1.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkFlavourTag/share/FTAG1.py
@@ -144,8 +144,12 @@ addExKtDoubleTaggerRCJets(FTAG1Seq, ToolSvc)
 
 # Create variable-R trackjets and dress AntiKt10LCTopo with ghost VR-trkjet
 addVRJets(FTAG1Seq)
-# Also add Hbb Tagger
+# Also add Hbb Tagger(s)
 addHbbTagger(FTAG1Seq, ToolSvc)
+addHbbTagger(
+    FTAG1Seq, ToolSvc,
+    nn_file_name="BoostedJetTaggers/HbbTagger/Summer2018/MulticlassNetwork.json",
+    nn_config_file="BoostedJetTaggers/HbbTaggerDNN/MulticlassConfigJune2018.json")
 
 # alias for VR
 BTaggingFlags.CalibrationChannelAliases += ["AntiKtVR30Rmax4Rmin02Track->AntiKtVR30Rmax4Rmin02Track,AntiKt4EMTopo"]
