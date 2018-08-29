f85ae811fd4f71ec3b96d130808d71903642d6d0
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2018-08-29 14:06:43 +0200

    Updated egamma smart slimming lists. See https://its.cern.ch/jira/projects/ATLEGAMDPD/issues/ATLEGAMDPD-95
    Former-commit-id: 7e069248f644f0477186e1dc6e27b2808fbd0b98

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/python/ElectronsCPDetailedContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/python/ElectronsCPDetailedContent.py
@@ -1,3 +1,3 @@
 # Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
-ElectronsCPDetailedContent = ["Electrons.Rhad.Rhad1.e277.Reta.Rphi.weta2.f1.Eratio.DeltaE.weta1.fracs1.wtots1.f3.f3core.deltaEta1.deltaPhi1.deltaPhi2.deltaPhiRescaled2.deltaPhiFromLastMeasurement"]
+ElectronsCPDetailedContent = ["Electrons.Rhad.Rhad1.e277.Reta.Rphi.weta2.f1.Eratio.DeltaE.weta1.fracs1.wtots1.f3.f3core.deltaEta1.deltaPhi1.deltaPhi2.deltaPhiRescaled2.deltaPhiFromLastMeasurement.parameterX.parameterPX.parameterPY.parameterPZ.parameterPosition.numberOfTRTHits.numberOfTRTOutliers.numberOfTRTHighThresholdHits.numberOfTRTHighThresholdOutliers.numberOfTRTXenonHits.lastEgMotherTruthType.lastEgMotherTruthOrigin.lastEgMotherTruthParticleLink.lastEgMotherPdgId"]
