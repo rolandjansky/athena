fc090d9608c3cc5993aa67ae99c9ed9ec84db8b0
Alexander Solodkov <Sanya.Solodkov@cern.ch>
2018-08-30 10:11:39 +0200

    fixing bug - wrong number of channels in ext.barrel
    Former-commit-id: d78f8119865ce35cc6a15014ec6f229e210d6b87

--- DataQuality/DCSCalculator2/python/subdetectors/tile.py
+++ DataQuality/DCSCalculator2/python/subdetectors/tile.py
@@ -7,7 +7,7 @@ from .lib import (DCSC_DefectTranslate_Subdetector, DCSC_Variable,
 WHITE, BLACK, GREY, RED, YELLOW, GREEN = None, -1, 0, 1, 2, 3
 
 TILBA, TILBC, TIEBA, TIEBC = 232, 233, 234, 235
-N_CHANNELS_PER_MODULE = [90]*148+[32]*14+[30]+[32]*66+[30]+[32]*46
+N_CHANNELS_PER_MODULE = [90]*148+[64]*14+[60]+[64]*66+[60]+[64]*46
 
 from itertools import product
 
@@ -101,9 +101,9 @@ class Tile_NoHighVoltage(DCSC_Variable_With_Mapping):
         def too_many_nohv(iov):
             """
             Read the TileCalib blob to determine what problems are present.
-            If all channels appear to have (2002, No HV), then count this module
-            as being OUT OF CONFIG. Number of channels is 90 for LBA/LBC partitions
-            32 for most of the modules in EBA/EBC partitions and 30 for EBA15 and EBC18
+            If all ADCs appear to have (2002, No HV), then count this module
+            as being OUT OF CONFIG. Number of ADCs is 45*2 for LBA/LBC partitions
+            32*2 for most of the modules in EBA/EBC partitions and 30*2 for EBA15 and EBC18
             """
             if not iov.TileCalibBlob: return False
             nbad, ngood, naffected, problems = iov.TileCalibBlob
