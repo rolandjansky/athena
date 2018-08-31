e4a6b39442c60debeccd57a88a53fab30b593f58
Jovan Mitrevski <Jovan.Mitrevski@cern.ch>
2018-08-31 19:38:18 +0200

    move egammaTopoClusterCopier earlier in the reconstruction
    Former-commit-id: cdebf0b5294010d469a3104511f590305a9e340a

--- Reconstruction/RecExample/RecExCommon/share/SystemRec_config.py
+++ Reconstruction/RecExample/RecExCommon/share/SystemRec_config.py
@@ -18,6 +18,13 @@ pdr.flag_domain('calo')
 protectedInclude ("CaloRec/CaloRec_jobOptions.py")
 AODFix_postCaloRec()
 
+#make the egammaTopoClusters containers, used for seeding
+from egammaAlgs.egammaTopoClusterCopier import egammaTopoClusterCopier
+try:
+    egammaTopoClusterCopier()
+except Exception:
+    treatExeption("could not get handle to egammaTopoClusterCopier")
+
 #then run ID reco:
 
 pdr.flag_domain('id')
