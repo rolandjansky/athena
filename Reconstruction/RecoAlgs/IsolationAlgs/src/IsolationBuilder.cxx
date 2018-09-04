b133f9763beab18bbe5d4cdbbc50dad935a86068
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2018-09-04 17:19:52 +0000

    Did you know cake is the word?
    Former-commit-id: 449445c5fa40865b4f0d2c6537acf2f43f61f01c

--- Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.cxx
+++ Reconstruction/RecoAlgs/IsolationAlgs/src/IsolationBuilder.cxx
@@ -798,8 +798,9 @@ StatusCode IsolationBuilder::runLeakage() {
       ATH_MSG_ERROR("Couldn't retrieve photon container with key: " << m_PhotonContainerName);
       return StatusCode::FAILURE;
     }
-    for (auto ph : *photons) {           
-        CP::CorrectionCode code = m_leakTool->applyCorrection(*ph);
+    xAOD::PhotonContainer::const_iterator it = photons->begin(), itE = photons->end();
+    for (; it != itE; ++it) {           
+        CP::CorrectionCode code = m_leakTool->applyCorrection(**it);
         if (code == CP::CorrectionCode::Error){
             ATH_MSG_ERROR("Failed to apply the photon leakage correction");
             return StatusCode::FAILURE;
@@ -813,8 +814,9 @@ StatusCode IsolationBuilder::runLeakage() {
       ATH_MSG_ERROR("Couldn't retrieve electron container with key: " << m_ElectronContainerName);
       return StatusCode::FAILURE;
     }
-    for (auto el : *electrons) {    
-        CP::CorrectionCode code = m_leakTool->applyCorrection(*el);
+    xAOD::ElectronContainer::const_iterator it = electrons->begin(), itE = electrons->end();
+    for (; it != itE; ++it) {           
+        CP::CorrectionCode code = m_leakTool->applyCorrection(**it);
         if (code == CP::CorrectionCode::Error){
             ATH_MSG_ERROR("Failed to apply the electron leakage correction");
             return StatusCode::FAILURE;
