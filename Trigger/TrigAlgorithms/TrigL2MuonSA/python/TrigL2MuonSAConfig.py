5872d9c3d471910fdbe66533bb7ad85d3b4fed0a
Shota Hayashida <hayashida@hepl.phys.nagoya-u.ac.jp>
2018-09-06 05:56:45 +0200

    Fix a issue, NO valid proxy for object MDTCSM
    Former-commit-id: d4898532846be815e5aa06e8d69fd6c69187a66e

--- Trigger/TrigAlgorithms/TrigL2MuonSA/python/TrigL2MuonSAConfig.py
+++ Trigger/TrigAlgorithms/TrigL2MuonSA/python/TrigL2MuonSAConfig.py
@@ -22,6 +22,12 @@ ToolSvc += rpcDataPreparator
 
 theDataPreparator.RPCDataPreparator = rpcDataPreparator
 
+mdtDataPreparator = TrigL2MuonSA__MdtDataPreparator()
+mdtDataPreparator.DecodeBS = DetFlags.readRDOBS.MDT_on()
+ToolSvc += mdtDataPreparator
+
+theDataPreparator.MDTDataPreparator = mdtDataPreparator
+print "suesuesuesuesue"
 ToolSvc += theDataPreparator
 ToolSvc += thePatternFinder
 ToolSvc += theStationFitter
