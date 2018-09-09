ca5808e99abc1fadaa63c33689c2d2b164359fa1
Hass AbouZeid <hass.abouzeid@cern.ch>
2018-09-09 20:40:36 +0200

    SUSYTools: Extend functionality of ART tests
    Added PRW autoconfig for AOD ART tests, as well as a SUSY1 MC based
    ART test.
    
    
    Former-commit-id: 4aa6091c280cbd891f86628b2089a5054cb43398

---
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/test/test_SUSYTools_ART_mc-SUSY1-AnaBase-grid.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# art-description: SUSYTools ART test - TestSUSYToolsAlg.py
+# art-type: grid
+# art-include: 21.2/AnalysisBase
+# art-output: hist-MC_DAOD_SUSY1.root
+# art-output: dcube
+
+# Create empty pool file
+art.py createpoolfile
+
+echo "Running SUSYTools test: \'TestSUSYToolsAlg.py -t MC -m -1 -d 1\'"
+TestSUSYToolsAlg.py -t MC -m -1 -d 1
+echo  "art-result: $? TEST"
+
+mv submitDir/hist-ARTInput.root ./hist-MC_DAOD_SUSY1.root
+
+echo "Running DCube post-processing"
+
+tName="MC-SUSY1"
+dcubeRef=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/References/hist-MC_DAOD_SUSY1.root
+dcubeXml=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/xml/dcube_config_master.xml
+
+/cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube ${tName} hist-MC_DAOD_SUSY1.root ${dcubeXml} ${dcubeRef}
+
+echo "art-result: $? DCUBE"
+
+echo "Done."
