281eb6df07ad39690389512410b6a66297393c28
christos <christos@cern.ch>
2018-09-10 17:53:45 +0100

    update from release 21.2
    Former-commit-id: 28d722a40dd47746f88e8bf3970a41a3fd0b550e

--- PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/util/EgEfficiencyCorr_mem_check.cxx
+++ PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/util/EgEfficiencyCorr_mem_check.cxx
@@ -16,26 +16,19 @@ http://valgrind.org/docs/manual/faq.html#faq.deflost
 #include <string>
 #include <vector>
 #include "EgammaAnalysisInterfaces/IAsgElectronEfficiencyCorrectionTool.h"
-#include "AsgAnalysisInterfaces/IEfficiencyScaleFactorTool.h"
 #include "AsgTools/AsgMessaging.h"
 #include "AsgTools/AnaToolHandle.h"
 #ifdef ASGTOOL_STANDALONE
 // xAOD include(s):
 #   include "xAODRootAccess/TEvent.h"
 #endif // ASGTOOL_STANDALONE
-#include "CxxUtils/ubsan_suppress.h"
-#include "TInterpreter.h"
-
 
 int main( ) {
 
-    // Suppress known ubsan warning we get from cling.
-    CxxUtils::ubsan_suppress ([]() { TInterpreter::Instance(); });
-    
 #ifdef ASGTOOL_STANDALONE
     xAOD::TEvent event;
 #endif
-
+    
     using namespace asg::msgUserCode;
     ANA_CHECK_SET_TYPE (int);
 
@@ -45,7 +38,7 @@ int main( ) {
             tool.setProperty("IdKey", "Medium") &&
             tool.retrieve());
 
-    asg::AnaToolHandle<CP::IEfficiencyScaleFactorTool> eccTool;
+    asg::AnaToolHandle<IAsgElectronEfficiencyCorrectionTool> eccTool;
     eccTool.setTypeAndName("CP::ElectronChargeEfficiencyCorrectionTool/ElectronChargeCorrection");
     ANA_CHECK(eccTool.setProperty( "CorrectionFileName", 
                 "ElectronEfficiencyCorrection/2015_2016/rel20.7/Moriond_February2017_v1/charge_misID/ChargeCorrectionSF.Medium_FixedCutTight.root" )&&
