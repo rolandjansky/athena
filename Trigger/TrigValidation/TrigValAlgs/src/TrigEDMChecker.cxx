47cde6ea6af1c8185b51479e2e8e8d39eae575bf
Tim Martin <Tim.Martin@cern.ch>
2018-09-11 16:11:49 +0200

    add decision to dot graph output
    Former-commit-id: fd6477b1ddb9751b8d71230333d253823547930d

--- Trigger/TrigValidation/TrigValAlgs/src/TrigEDMChecker.cxx
+++ Trigger/TrigValidation/TrigValAlgs/src/TrigEDMChecker.cxx
@@ -4079,9 +4079,17 @@ StatusCode TrigEDMChecker::TrigCompositeNavigationToDot(std::string& returnValue
     // ss << "    rank=same" << std::endl; // dot cannot handle this is seems
     for (const xAOD::TrigComposite* tc : *container ) {
       // Output my name
-      ss << "    \"" << tc << "\" [label=\"" << typeNameTC << "\\n" << key << ":" << std::to_string(index);
-      if (tc->name() != "") ss << "\\n" << tc->name();
-      ss << "\"]" << std::endl;    
+      ss << "    \"" << tc << "\" [label=\"" << typeNameTC; 
+      if (tc->name() != "") ss << "\\nname=" << tc->name();
+      ss << "\\nkey=" << key << ":index=" << std::to_string(index);
+      std::vector<int> decisions;
+      if (tc->getDetail("decisions", decisions) && decisions.size() > 0) {
+        ss << "\\npass=";
+        for (int decisionID : decisions) {
+          ss << std::to_string(decisionID) << "," ;
+        }
+      }
+      ss << "\"]" << std::endl;
       // Output all the things I link to
       for (size_t i = 0; i < tc->linkColNames().size(); ++i) {
         const std::string link = tc->linkColNames().at(i);
