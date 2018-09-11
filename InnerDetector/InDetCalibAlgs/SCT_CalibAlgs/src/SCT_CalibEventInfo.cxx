9275ebb96547e3f2a9b606d3d9f34677c333468a
Susumu Oda <Susumu.Oda@cern.ch>
2018-09-11 10:09:04 +0200

    Remove incident from SCT_CalibEventInfo
    Former-commit-id: 9772af0f274e98bdb7a9f2772b385f380fc0beb6

--- InnerDetector/InDetCalibAlgs/SCT_CalibAlgs/src/SCT_CalibEventInfo.cxx
+++ InnerDetector/InDetCalibAlgs/SCT_CalibAlgs/src/SCT_CalibEventInfo.cxx
@@ -24,8 +24,6 @@ const long long oneBillion(1000000000LL);
 }
 
 SCT_CalibEventInfo::SCT_CalibEventInfo(const std::string &name, ISvcLocator * svc):AthService(name,svc),
-   m_eventInfoKey(std::string("ByteStreamEventInfo")),
-   m_incidentSvc ( "IncidentSvc", name ),
    m_timeStampBegin(INTMAX),
    m_tsBeginString(""),
    m_tsEndString(""),
@@ -48,9 +46,6 @@ SCT_CalibEventInfo::SCT_CalibEventInfo(const std::string &name, ISvcLocator * sv
 StatusCode
 SCT_CalibEventInfo::initialize() {
    msg( MSG::INFO)<<"Initialize of evtInfo in "<<PACKAGE_VERSION<<endmsg;
-   ATH_CHECK(m_eventInfoKey.initialize());
-   const int pri(500);
-   m_incidentSvc->addListener( this, "BeginEvent",       pri, true );
    return StatusCode::SUCCESS;
 }
 
@@ -73,35 +68,6 @@ SCT_CalibEventInfo::queryInterface(const InterfaceID & riid, void** ppvInterface
 int SCT_CalibEventInfo::lumiBlock() const {
    return m_lumiBlock;
 }
-
-void
-SCT_CalibEventInfo::handle(const Incident&) {
-
-   if ( m_source == "BS" ) {
-      SG::ReadHandle<EventInfo> evt(m_eventInfoKey);
-      if (not evt.isValid()) {
-         msg(MSG:: ERROR) << "Unable to get the EventInfo" << endmsg;
-         return;
-      }
-
-      const EventInfo* evt_ptr = &(*evt);
-      msg( MSG::VERBOSE ) << SCT_CalibAlgs::eventInfoAsString(evt_ptr) << endmsg;
-      //--- TimeStamp/LB range analyzed
-      const int timeStamp = evt->event_ID()->time_stamp();
-      const int lumiBlock = evt->event_ID()->lumi_block();
-      setTimeStamp(std::min(timeStamp, m_timeStampBegin), std::max(timeStamp, m_timeStampEnd));
-      setLumiBlock(std::min(lumiBlock, m_LBBegin), std::max(lumiBlock, m_LBEnd));
-      m_lumiBlock=lumiBlock;
-      m_timeStamp=timeStamp;
-
-   } else if ( m_source == "HIST" ) {
-
-      msg( MSG::INFO ) << "RunNumber, TimeStamp (Begin, End): " << m_runNumber << ", " << m_timeStampBegin << ", " << m_timeStampEnd<< endmsg;
-
-   } else msg( MSG::FATAL ) << "SCT_CalibEventInfo: Unknown source!" << endmsg;
-
-}
-
 
 void
 SCT_CalibEventInfo::setTimeStamp(const int begin, const int end) {
@@ -127,6 +93,13 @@ void
 SCT_CalibEventInfo::setTimeStamp(const int ts) {
    m_timeStamp=ts;
 }
+
+void
+SCT_CalibEventInfo::getLumiBlock(int & begin, int & end) const {
+   begin=m_LBBegin;
+   end=m_LBEnd;
+}
+
 void
 SCT_CalibEventInfo::setLumiBlock(const int begin, const int end) {
    m_LBBegin=begin;
@@ -143,7 +116,7 @@ SCT_CalibEventInfo::setSource(const std::string source) {
 }
 void
 SCT_CalibEventInfo::setRunNumber(const int rn) {
-   m_runNumber=rn;
+  m_runNumber=rn;
 }
 void
 SCT_CalibEventInfo::setBunchCrossing(const int bc) {
