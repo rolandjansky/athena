1ff2c2c548d40c2bc361a8d93041de47a0c0ed2e
Vadim Kostyukhin <vadim.kostyukhin@cern.ch>
2018-09-14 17:54:21 +0200

    Change 2017 to 2018 in copyright statements
    Former-commit-id: bca71ac630bfc10c7003fcd32d78967af86a96d3

--- InnerDetector/InDetRecTools/InDetVKalVxInJetTool/src/BTagVrtSec.cxx
+++ InnerDetector/InDetRecTools/InDetVKalVxInJetTool/src/BTagVrtSec.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 // Header include
@@ -15,7 +15,7 @@
 #include "TH2D.h"
 #include  "TMath.h"
 //
-#include<iostream>
+//#include<iostream>
 
 //----------------------------------------------------------------------------------------
 //  GetVrtSec resurns the vector Results with the following
--- InnerDetector/InDetRecTools/InDetVKalVxInJetTool/src/InDetVKalVxInJetTool.cxx
+++ InnerDetector/InDetRecTools/InDetVKalVxInJetTool/src/InDetVKalVxInJetTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 // Header include
@@ -18,7 +18,7 @@
 //
 //-------------------------------------------------
 // Other stuff
-#include<iostream>
+//#include<iostream>
 
 //extern "C" {
 //  float chisin_(const float & prob, const long int& ndf);             
