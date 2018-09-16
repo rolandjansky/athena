7df308e249d9e9ae54e04f28c1f279fac62b7f45
David Shope <david.richard.shope@cern.ch>
2018-09-16 16:49:12 +0200

    Add extra variables to AntiKt4TruthJets in HIGG3D1 derivation format
    Former-commit-id: 8ca6ee8a70d59f79a473376c332ed27420d7c281

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG3D1ExtraContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG3D1ExtraContent.py
@@ -20,6 +20,7 @@ HIGG3D1ExtraTruthVariables = [ "Electrons.truthOrigin.truthType.truthParticleLin
                                "InDetTrackParticles.truthOrigin.truthType",
                                "AntiKt4PV0TrackJets.TruthLabelDeltaR_B.TruthLabelDeltaR_C.TruthLabelDeltaR_T.GhostTruth.GhostTruthCount.GhostTruthPt.HadronConeExclTruthLabelID.PartonTruthLabelID.ConeTruthLabelID",
                                "AntiKt4TruthWZJets.pt.eta.phi.m.constituentLinks.constituentWeights.HadronConeExclTruthLabelID.ConeTruthLabelID.PartonTruthLabelID.TruthLabelDeltaR_B.TruthLabelDeltaR_C.TruthLabelDeltaR_T.btaggingLink",
+                               "AntiKt4TruthJets.GhostBHadronsFinalCount.GhostBHadronsFinalPt.GhostCHadronsFinalCount.GhostCHadronsFinalPt.GhostTausFinalCount.GhostTausFinalPt.ActiveArea.ActiveArea4vec_m.ActiveArea4vec_pt.ActiveArea4vec_eta.ActiveArea4vec_phi",
                                "AntiKt4EMPFlowJets.TruthLabelID.ConeTruthLabelID.PartonTruthLabelID.HadronConeExclTruthLabelID"]
 
 HIGG3D1ExtraContainers = [ ]
