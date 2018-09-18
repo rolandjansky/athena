373b64a8ec13809bf9abe5b3c901289f2d61df40
Paul Gessinger <paul.gessinger@cern.ch>
2018-09-18 20:34:12 +0200

    Remove remnants of separate ActsAlignment package
    Former-commit-id: 2e67445c9653a68afe581d5a73bf44539d3e001d

--- Tracking/Acts/ActsAlignment/src/components/ActsAlignment_entries.cxx
+++
@@ -1,7 +0,0 @@
-#include "ActsAlignment/ActsAlignedExtrapAlg.h"
-#include "ActsAlignment/GeomShiftCondAlg.h"
-#include "ActsAlignment/NominalAlignmentCondAlg.h"
-
-DECLARE_COMPONENT( ActsAlignedExtrapAlg )
-DECLARE_COMPONENT( GeomShiftCondAlg )
-DECLARE_COMPONENT( NominalAlignmentCondAlg )
