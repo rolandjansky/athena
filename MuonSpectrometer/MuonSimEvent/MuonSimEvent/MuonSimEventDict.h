21adbe4a60cd6f6804f3e321c4cca74a5d453a8b
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-09-18 07:26:24 +0000

    Merge branch 'MuonSimEventDictionaryFix_21.3' into '21.3'
    Fix warnings in root dictionary classes for sTGCSimHitCollection classes
    
    See merge request atlas/athena!14214
    
    (cherry picked from commit 8e1e8dd5d19e0c343a5116b1c28013d4f517e17a [formerly 9b77a5e690cfe82b5ff4f2b68ff8360bc1d035d6])
    
    ddc7ba18 Fix Unused class warnings in root dictionary creation for MuonSimEvent package
    18cabce6 Fixed Unused class warnings in the root dictionary generation for the MuonSimEventTPCnv package
    
    Former-commit-id: 10b64674ce8fba11d1695760c9a8a451ee254cb8

--- MuonSpectrometer/MuonSimEvent/MuonSimEvent/MuonSimEventDict.h
+++ MuonSpectrometer/MuonSimEvent/MuonSimEvent/MuonSimEventDict.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef MUONSIMEVENT_MUONSIMEVENTDICT_H
@@ -10,5 +10,6 @@
 #include "MuonSimEvent/RPCSimHitCollection.h"
 #include "MuonSimEvent/TGCSimHitCollection.h"
 #include "MuonSimEvent/GenericMuonSimHitCollection.h"
+#include "MuonSimEvent/sTGCSimHitCollection.h"
 
 #endif
