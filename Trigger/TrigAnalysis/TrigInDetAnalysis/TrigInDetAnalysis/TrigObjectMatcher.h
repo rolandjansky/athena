00ac1e24f3ca6681593099bf28fbebc1e90a157a
sutt <sutt@cern.ch>
2018-09-19 15:51:42 +0200

    Fix year in copyright
    Former-commit-id: f495361f693e7ea895a76fbe412a1392c2405d48

--- Trigger/TrigAnalysis/TrigInDetAnalysis/TrigInDetAnalysis/TrigObjectMatcher.h
+++ Trigger/TrigAnalysis/TrigInDetAnalysis/TrigInDetAnalysis/TrigObjectMatcher.h
@@ -5,7 +5,7 @@
 //
 //   $Id: TrigObjectMatcher.h, v0.0   Fri 14 Sep 2018 15:55:41 CEST sutt $
 //
-//   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+//   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 
 #ifndef  TRIGOBJECTMATCHER_H
 #define  TRIGOBJECTMATCHER_H
