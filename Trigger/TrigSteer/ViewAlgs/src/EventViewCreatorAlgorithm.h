d7aed7aa9444ed15b7cb14038f6a08006b27e24e
Tomasz Bold <tomasz.bold@gmail.com>
2018-09-21 16:44:57 +0200

    Copyright in 2018. Mabye CI can be spared for that
    Former-commit-id: 1c630ca2d0a7794827c36ae135ddda75ed7de51f

--- Trigger/TrigSteer/ViewAlgs/src/EventViewCreatorAlgorithm.h
+++ Trigger/TrigSteer/ViewAlgs/src/EventViewCreatorAlgorithm.h
@@ -1,7 +1,7 @@
 /*
   General-purpose view creation algorithm <bwynne@cern.ch>
 
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef ViewAlgs_EventViewCreatorAlgorithm_h
