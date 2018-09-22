a6aa6e0d63a9de39dfe6e285fdbae565f1169237
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2018-09-22 13:15:26 +0200

    Users can now decide whether they'd like to assign the tools on the random run number
    Former-commit-id: 9f94a02005be1e62cd8830b0c16398768fe409bb

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonMomentumCorrections/Root/MuonCalibrationPeriodTool.cxx
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonMomentumCorrections/Root/MuonCalibrationPeriodTool.cxx
@@ -2,17 +2,27 @@
   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-// EDM include(s):
-#include "xAODEventInfo/EventInfo.h"
-
 // Local include(s):
-#include "MuonMomentumCorrections/MuonCalibrationPeriodTool.h"
-#include <cmath>
+#include <MuonMomentumCorrections/MuonCalibrationPeriodTool.h>
 
 namespace CP {
     
     
     StatusCode MuonCalibrationPeriodTool::beginEvent() {
+        ATH_CHECK(evtStore()->retrieve(m_evInfo, "EventInfo"));
+        bool isData = !m_evInfo->eventType(xAOD::EventInfo::IS_SIMULATION);        
+        // reset the active tool
+        m_activeTool = nullptr;
+        if (m_useRndRun && !isData) {
+            ATH_MSG_DEBUG("Random run number is used on MC. Skip the assignment");
+            return StatusCode::SUCCESS;
+        }
+        m_activeTool = getTool();
+        if (m_activeTool == nullptr) return StatusCode::FAILURE;
+        return StatusCode::SUCCESS;
+    }
+   unsigned int MuonCalibrationPeriodTool::getPeriod() const {
+        static SG::AuxElement::ConstAccessor<unsigned int> acc_rnd("RandomRunNumber");
         // I've copied the run number ranges from SUSYTools
         // https://gitlab.cern.ch/atlas/athena/blob/21.2/PhysicsAnalysis/SUSYPhys/SUSYTools/Root/SUSYObjDef_xAOD.cxx#L2438
         static const unsigned int last_run_16  = 320000;
@@ -20,28 +30,51 @@ namespace CP {
         // Not the appropiate value but okay we can fix it afterwards
         static const unsigned int last_run_18  = 500000;
       
-        const xAOD::EventInfo* info = nullptr;
-        ATH_CHECK(evtStore()->retrieve(info, "EventInfo"));
-     
-        unsigned int run = info->runNumber();
-        bool isData = !info->eventType(xAOD::EventInfo::IS_SIMULATION);        
-        
-        if ( (isData  && run <= last_run_16) || (!isData && std::find(m_MCperiods1516.begin(), m_MCperiods1516.end(),run) != m_MCperiods1516.end()) ){
-             ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data 15-16 / mc16a");
-             m_activeTool = m_calibTool_1516.get();
-        } else if ( (isData  && run <= last_run_17) || (!isData && std::find(m_MCperiods17.begin(), m_MCperiods17.end(),run) != m_MCperiods17.end()) ) {
-            ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data 17 / mc16d");
-            m_activeTool = m_calibTool_17.get();
-        } else if ( (isData  && run <= last_run_18) || (!isData && std::find(m_MCperiods18.begin(), m_MCperiods18.end(),run) != m_MCperiods18.end()) ){
-            ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data 18 / mc16e");
-            m_activeTool = m_calibTool_18.get();
-        } else {
-            ATH_MSG_FATAL("Could not determine on which year we are running");
-            return StatusCode::FAILURE;
+        bool isData = !m_evInfo->eventType(xAOD::EventInfo::IS_SIMULATION);        
+        unsigned int run = m_evInfo->runNumber();
+        // retrieve the random run number
+        if (!isData && m_useRndRun){
+             if (acc_rnd.isAvailable(*m_evInfo)) run = acc_rnd(*m_evInfo);
+             else ATH_MSG_WARNING("No random runnumber could be found allthough the tool is configured to assign the years based on random run number. User MC periods themselves.");
         }
-         
-        return StatusCode::SUCCESS;
+        // Check the Monte carlo
+        if (!isData &&  (!m_useRndRun || !acc_rnd.isAvailable(*m_evInfo)) ){
+            if (std::find(m_MCperiods1516.begin(), m_MCperiods1516.end(),run) != m_MCperiods1516.end()) {
+                ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data mc16a / data15-16");                
+                return 1516;
+            } else if (std::find(m_MCperiods17.begin(), m_MCperiods17.end(),run) != m_MCperiods17.end()) {
+                ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data mc16d / data17");                
+                return 17;
+            } else if (std::find(m_MCperiods18.begin(), m_MCperiods17.end(),run) != m_MCperiods18.end()){ 
+                ATH_MSG_DEBUG("The current run "<<run<<" corresponds to data mc16e / data18");                
+                return 18;
+            }
+        } 
+        // Check data itself
+        else if (isData) {
+            if (run < last_run_16) {
+                ATH_MSG_DEBUG("The current run "<<run<<" is taken in data 15-16");
+                return 1516;
+            } else if (run < last_run_17) {
+                 ATH_MSG_DEBUG("The current run "<<run<<" is taken in data 17");
+                return 17;
+            } else if (run < last_run_18) {
+                ATH_MSG_DEBUG("The current run "<<run<<" is taken in data 18");
+                return 18;
+            }
+        }
+        ATH_MSG_FATAL("Could not assign run-number "<<run<<" to a specific year of data-taking");
+        return -1;
     }
+   const CP::IMuonCalibrationAndSmearingTool* MuonCalibrationPeriodTool::getTool() const {
+        if (m_activeTool != nullptr) return m_activeTool;
+        unsigned int period = getPeriod();
+        if (period ==  1516) return m_calibTool_1516.get();
+        else if (period ==  17) return m_calibTool_17.get();
+        else if (period ==  18) return m_calibTool_18.get();
+        return nullptr;
+    }
+ 
 
     StatusCode MuonCalibrationPeriodTool::initialize() {
         if(m_MCperiods1516.empty()) {
@@ -54,6 +87,7 @@ namespace CP {
             ATH_MSG_FATAL("No period numbers have been provided for mc16e");
             return StatusCode::FAILURE;
         }
+        if (m_useRndRun) ATH_MSG_INFO("The assignment of the calibration tools will be based on the random run number. Please make sure to call the pileup-tool before applying this tool");
         if (!m_calibTool_1516.isUserConfigured()){
             ATH_MSG_INFO("Setup the MuonMomentum calibration tool for 2015-2016 & mc16a");
             m_calibTool_1516.setTypeAndName("CP::MuonCalibrationAndSmearingTool/"+name()+"_1516");
@@ -87,6 +121,7 @@ namespace CP {
             ATH_CHECK(m_calibTool_18.setProperty("doSagittaMCDistortion", true));
             ATH_CHECK(m_calibTool_18.setProperty("SagittaCorrPhaseSpace", true));
         }
+        
         ATH_CHECK(m_calibTool_1516.retrieve());
         ATH_CHECK(m_calibTool_17.retrieve());
         ATH_CHECK(m_calibTool_18.retrieve());
@@ -99,13 +134,15 @@ namespace CP {
         m_calibTool_17(),
         m_calibTool_18(),
         m_activeTool(nullptr),
+        m_evInfo(nullptr),
         m_sagittaRelease1516("sagittaBiasDataAll_25_07_17"),
         m_sagittaRelease17("sagittaBiasDataAll_30_07_18"),
         m_sagittaRelease18("sagittaBiasDataAll_30_07_18"),
         m_release("Recs2017_08_02"),
         m_MCperiods1516(),
         m_MCperiods17(),
-        m_MCperiods18() {
+        m_MCperiods18(),
+        m_useRndRun(false) {
 
         m_calibTool_1516.declarePropertyFor(this, "CalibrationTool1516", "Parse the configured instance of the calibration tool for 15&16 data or mc16a");
         m_calibTool_17.declarePropertyFor(this, "CalibrationTool17", "Parse the configured instance of the calibration tool for 17 data or mc16d");
@@ -118,45 +155,51 @@ namespace CP {
         declareProperty("MCperiods1516", m_MCperiods1516 = {284500});
         declareProperty("MCperiods17", m_MCperiods17 = {300000, 304000, 305000});
         declareProperty("MCperiods18", m_MCperiods18 = {310000});
+        declareProperty("useRandomRunNumber", m_useRndRun);
         // run 304000/305000 correspond to 13/7 TeV low-mu data in 2017
     }
     MuonCalibrationPeriodTool::~MuonCalibrationPeriodTool(){}
 
     CorrectionCode MuonCalibrationPeriodTool::applyCorrection( xAOD::Muon& mu ) const {
-        if (m_activeTool == nullptr){
+        const CP::IMuonCalibrationAndSmearingTool* tool = getTool();
+        if (tool == nullptr){
             ATH_MSG_FATAL("No valid tool has been found to apply the correction of muon with pt: "<<mu.pt()/1.e3<<"  eta: "<<mu.eta()<<" phi: "<<mu.phi());
             return CorrectionCode::Error;
         }
-        return m_activeTool->applyCorrection(mu);
+        return tool->applyCorrection(mu);
     }
     CorrectionCode MuonCalibrationPeriodTool::correctedCopy( const xAOD::Muon& input, xAOD::Muon*& output ) const {
-        if (m_activeTool == nullptr){
-            ATH_MSG_FATAL("No valid tool has been found to apply the correction of muon with pt: "<<input.pt()/1.e3<<"  eta: "<<input.eta()<<" phi: "<<input.phi());
+        const CP::IMuonCalibrationAndSmearingTool* tool = getTool();
+        if (tool == nullptr){
+            ATH_MSG_FATAL("No valid tool has been found to create the corrected copy of muon with pt: "<<input.pt()/1.e3<<"  eta: "<<input.eta()<<" phi: "<<input.phi());
             return CorrectionCode::Error;
         }
-        return m_activeTool->correctedCopy(input, output);
+        return tool->correctedCopy(input, output);
     
     }
     CorrectionCode MuonCalibrationPeriodTool::applyCorrectionTrkOnly( xAOD::TrackParticle& inTrk, const int DetType) const {
-        if (m_activeTool == nullptr){
+        const CP::IMuonCalibrationAndSmearingTool* tool = getTool();
+        if (tool == nullptr){
             ATH_MSG_FATAL("No valid tool has been found to apply the correction of the track with pt: "<<inTrk.pt()/1.e3<<"  eta: "<<inTrk.eta()<<" phi: "<<inTrk.phi());
             return CorrectionCode::Error;
         }
-        return m_activeTool->applyCorrectionTrkOnly(inTrk, DetType);        
+        return tool->applyCorrectionTrkOnly(inTrk, DetType);        
     }    
     double MuonCalibrationPeriodTool::expectedResolution( const std::string& DetType, const xAOD::Muon& mu, const bool mc ) const {
-        if (m_activeTool == nullptr){
+        const CP::IMuonCalibrationAndSmearingTool* tool = getTool();
+        if (tool == nullptr){
             ATH_MSG_FATAL("No valid tool has been found to predict the resolution of muon with pt: "<<mu.pt()/1.e3<<"  eta: "<<mu.eta()<<" phi: "<<mu.phi());
             return std::nan("float");
         }
-        return m_activeTool->expectedResolution(DetType,mu,mc);
+        return tool->expectedResolution(DetType,mu,mc);
     }
     double MuonCalibrationPeriodTool::expectedResolution( const int& DetType, const xAOD::Muon& mu, const bool mc ) const {
-        if (m_activeTool == nullptr){
+        const CP::IMuonCalibrationAndSmearingTool* tool = getTool();
+        if (tool == nullptr){
             ATH_MSG_FATAL("No valid tool has been found to predict the resolution of muon with pt: "<<mu.pt()/1.e3<<"  eta: "<<mu.eta()<<" phi: "<<mu.phi());
             return std::nan("float");
         }
-        return m_activeTool->expectedResolution(DetType,mu,mc);
+        return tool->expectedResolution(DetType,mu,mc);
     }
     // Systematics do not change throughout the year. So let's take the ones given by 1516 tool
     bool MuonCalibrationPeriodTool::isAffectedBySystematic( const SystematicVariation& systematic ) const {
