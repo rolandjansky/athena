b8331f0796874645a9fc87acaa5cc9bda71ab8af
Roger Naranjo <roger.naranjo@cern.ch>
2018-09-22 04:35:55 +0000

    ATLASRECTS-4569: Fix smallChrono RAII wrapper and new flag in egammaBuilders to start the ChronoStatSvc
    Former-commit-id: 11674fe8f30a73e5d19c4d33da872f391f197a0d

--- Reconstruction/egamma/egammaAlgs/src/topoEgammaBuilder.cxx
+++ Reconstruction/egamma/egammaAlgs/src/topoEgammaBuilder.cxx
@@ -4,12 +4,13 @@
 
 
 #include "topoEgammaBuilder.h"
+#include "smallChrono.h"
 
 #include "AthenaKernel/errorcheck.h"
 #include "GaudiKernel/IToolSvc.h"
+#include "GaudiKernel/ServiceHandle.h"
 #include "StoreGate/ReadHandle.h"
 #include "StoreGate/WriteHandle.h"
-
 #include "xAODCaloEvent/CaloClusterContainer.h"
 #include "xAODTracking/TrackParticleContainer.h"
 #include "xAODTracking/VertexContainer.h"
@@ -26,32 +27,10 @@
 #include <algorithm> 
 #include <cmath>
 
-namespace{
-    class smallChrono{
-    public:
-        smallChrono(IChronoStatSvc* timingProfile, const std::string& name): 
-            m_time(timingProfile),
-            m_name(name){
-                if(m_time){
-                    m_time->chronoStart(m_name);
-                }
-            }
-        ~smallChrono(){
-            if(m_time){
-                m_time->chronoStop(m_name);
-            }
-        }
-    private:
-        IChronoStatSvc* m_time;
-        const std::string m_name;
-    };
-}
-
-
 topoEgammaBuilder::topoEgammaBuilder(const std::string& name, 
         ISvcLocator* pSvcLocator): 
     AthAlgorithm(name, pSvcLocator),
-    m_timingProfile(0)
+    m_timingProfile("ChronoStatSvc", name)
 {
 }
 
@@ -81,7 +60,7 @@ StatusCode topoEgammaBuilder::initialize()
     CHECK( RetrieveTools(m_photonTools) );
 
     // retrieve timing profile
-    CHECK( service("ChronoStatSvc",m_timingProfile) );
+    if (m_doChrono) CHECK( m_timingProfile.retrieve() );
 
     ATH_MSG_DEBUG("Initialization completed successfully");
     return StatusCode::SUCCESS;
@@ -340,7 +319,7 @@ StatusCode topoEgammaBuilder::CallTool(ToolHandle<IegammaBaseTool>& tool,
         xAOD::PhotonContainer *photonContainer /* = 0*/){
 
 
-    smallChrono timer(m_timingProfile,this->name()+"_"+tool->name());  
+    smallChrono timer(*m_timingProfile,this->name()+"_"+tool->name(), m_doChrono);  
 
     if (electronContainer){    
         ATH_MSG_DEBUG("Executing tool on electrons: " << tool );
