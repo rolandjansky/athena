576862ebd86cf757aab4dee9bbac5bdf5296d2c9
Chiao-Ying Lin <cylin@cern.ch>
2018-09-27 17:48:33 +0200

    code cleaning up and noise file for new sample
    Former-commit-id: e3641c654ee5e7c3ff669383464dc8af519fe943

--- Trigger/TrigT1/TrigT1CaloFexSim/src/JGTowerReader.cxx
+++ Trigger/TrigT1/TrigT1CaloFexSim/src/JGTowerReader.cxx
@@ -8,8 +8,6 @@
 #include "TrigT1CaloFexSim/JetAlg.h"
 #include "TSystem.h"
 #include "xAODEventInfo/EventInfo.h"
-//uncomment the line below to use the HistSvc for outputting trees and histograms
-#include "GaudiKernel/ITHistSvc.h"
 #include "TTree.h"
 #include "TH1D.h"
 #include "iostream"
@@ -33,7 +31,9 @@
 
 
 
-JGTowerReader::JGTowerReader( const std::string& name, ISvcLocator* pSvcLocator ) : AthAlgorithm( name, pSvcLocator ){
+JGTowerReader::JGTowerReader( const std::string& name, ISvcLocator* pSvcLocator ) : 
+AthAlgorithm( name, pSvcLocator ),
+histSvc("THistSvc",name){
 
   declareProperty("outputNoise",m_outputNoise=false);
   declareProperty("noise_file",m_noise_file="");
@@ -61,109 +61,26 @@ JGTowerReader::~JGTowerReader() {
 
 StatusCode JGTowerReader::initialize() {
   ATH_MSG_INFO ("Initializing " << name() << "...");
-  //
-  //This is called once, before the start of the event loop
-  //Retrieves of tools you have configured in the joboptions go here
-  //
-
-  //HERE IS AN EXAMPLE
-  //We will create a histogram and a ttree and register them to the histsvc
-  //Remember to uncomment the configuration of the histsvc stream in the joboptions
-  //
-  ServiceHandle<ITHistSvc> histSvc("THistSvc",name());
 
   CHECK( histSvc.retrieve() );
   CHECK(detStore()->retrieve(m_jTowerId));
   CHECK(detStore()->retrieve(m_gTowerId));
 
-  m_jTowerHashMax=m_jTowerId->tower_hash_max();
-  m_gTowerHashMax=m_gTowerId->tower_hash_max();
- 
   TFile*noise = new TFile(m_noise_file.c_str());
   TH1F*jh_noise = (TH1F*)noise->Get("jT_noise");
   TH1F*gh_noise = (TH1F*)noise->Get("gT_noise");
 
-  for(int i=0;i<m_jTowerHashMax;i++){
+  for(int i=0;i<jh_noise->GetSize();i++){
      float noise_base = jh_noise->GetBinContent(i+1);
      jT_noise.push_back(noise_base);
      jJet_thr.push_back(noise_base*m_jJet_thr);
   }
 
-  for(int i=0;i<m_gTowerHashMax;i++){
+  for(int i=0;i<gh_noise->GetSize();i++){
      float noise_base = gh_noise->GetBinContent(i+1);
-     gT_noise.push_back(noise_base*2);
+     gT_noise.push_back(noise_base);
      gJet_thr.push_back(noise_base*m_gJet_thr);
   } 
-
-  
-
-  if(m_outputNoise){
-
-    for(int i=0; i<m_jTowerHashMax; i++){
-
-       char hist_name[50];
-       sprintf(hist_name,"Hist_Et_jT%d",i);
-       hName[hist_name]=new TH1F(hist_name,hist_name,100,-5000,15000);       
-       CHECK( histSvc->regHist(Form("/OUTPUT/JTower_Et/%s",hist_name),hName[hist_name]));
-    }
-
-    
-    int m_gTowerHashMax=m_gTowerId->tower_hash_max();  
-
-    for(int i=0; i<m_gTowerHashMax; i++){
-    
-       char hist_name[50];
-       sprintf(hist_name,"Hist_Et_gT%d",i);
-       hName[hist_name]=new TH1F(hist_name,hist_name,100,-7000,18000);
-       CHECK( histSvc->regHist(Form("/OUTPUT/GTower_Et/%s",hist_name),hName[hist_name]));
-    }
-
-  }
-
-  char hist_name[50];
-  sprintf(hist_name,"jJet_et");
-  hName[hist_name]=new TH1F(hist_name,hist_name,100,0,500000);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"gJet_et");
-  hName[hist_name]=new TH1F(hist_name,hist_name,100,0,1000000);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-
-  sprintf(hist_name,"jJet_eta");
-  hName[hist_name]=new TH1F(hist_name,hist_name,49,-4.9,4.9);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"gJet_eta");
-  hName[hist_name]=new TH1F(hist_name,hist_name,49,-4.9,4.9);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-
-  sprintf(hist_name,"jJet_phi");
-  hName[hist_name]=new TH1F(hist_name,hist_name,62,-3.14,3.14);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"gJet_phi");
-  hName[hist_name]=new TH1F(hist_name,hist_name,62,-3.14,3.14);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-                                                                            
-
-  sprintf(hist_name,"jMet_et");
-  hName[hist_name]=new TH1F(hist_name,hist_name,100,0,500000);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"gMet_et");
-  hName[hist_name]=new TH1F(hist_name,hist_name,100,0,500000);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"jMet_phi");
-  hName[hist_name]=new TH1F(hist_name,hist_name,62,-3.14,3.14);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
-  sprintf(hist_name,"gMet_phi");
-  hName[hist_name]=new TH1F(hist_name,hist_name,62,-3.14,3.14);
-  CHECK( histSvc->regHist(Form("/OUTPUT/%s",hist_name),hName[hist_name]));
-
 
   return StatusCode::SUCCESS;
 }
@@ -195,42 +112,9 @@ StatusCode JGTowerReader::execute() {
   const xAOD::JGTowerContainer* gTowers =0;
   CHECK( evtStore()->retrieve( gTowers,"GTower"));
 
-
-
-  if(m_outputNoise){
-
-    int m_jTowerHashMax=m_jTowerId->tower_hash_max();
-    for(int hs=0; hs<m_jTowerHashMax;hs++){
-       const xAOD::JGTower*jT = jTowers->at(hs);
-       float Et = jT->et();
-       if(!Et) continue;
-       char hist_name[50];
-       sprintf(hist_name,"Hist_Et_jT%d",hs);
-       hName[hist_name]->Fill(Et);  
-    }
-
-    int m_gTowerHashMax=m_gTowerId->tower_hash_max();
-    for(int hs=0; hs<m_gTowerHashMax;hs++){
-       const xAOD::JGTower*gT = gTowers->at(hs);
-       float Et = gT->et();
-       if(!Et) continue;
-       char hist_name[50];
-       sprintf(hist_name,"Hist_Et_gT%d",hs);
-       hName[hist_name]->Fill(Et);
-    }
-  }
-
-  //
-  //Your main analysis code goes here
-  //If you will use this algorithm to perform event skimming, you
-  //should ensure the setFilterPassed method is called
-  //If never called, the algorithm is assumed to have 'passed' by default
-  //
-
-  
-  CHECK(JFexAlg(jTowers));
-  CHECK(GFexAlg(gTowers));
-  CHECK(ProcessObject());
+  CHECK(JFexAlg(jTowers)); // all the functions for JFex shall be executed here
+  CHECK(GFexAlg(gTowers)); // all the functions for GFex shall be executed here
+  CHECK(ProcessObject());  // this is the function to make output as well as memory cleaning
 
 
   setFilterPassed(true); //if got here, assume that means algorithm passed
@@ -263,7 +147,6 @@ StatusCode JGTowerReader::JFexAlg(const xAOD::JGTowerContainer* jTs){
   if(jSeeds->eta.empty()) CHECK(JetAlg::SeedGrid(jTs,jSeeds));
   
   CHECK(JetAlg::SeedFinding(jTs,jSeeds,m_jSeed_size,m_jMax_r,jJet_thr)); //the diameter of seed, and its range to be local maximum
-                                          //Careful to ensure the range set to be no tower double counted
   CHECK(JetAlg::BuildJet(jTs, jJet_thr,jSeeds, jL1Jets,m_jJet_r)); 
   CHECK(METAlg::BuildMET(jTs,jT_noise,jMET));
 
@@ -275,7 +158,6 @@ StatusCode JGTowerReader::GFexAlg(const xAOD::JGTowerContainer* gTs){
 // jet algorithms
   if(gSeeds->eta.empty()) CHECK(JetAlg::SeedGrid(gTs,gSeeds));
   CHECK(JetAlg::SeedFinding(gTs,gSeeds,m_gSeed_size,m_gMax_r,gJet_thr)); // the diameter of seed, and its range to be local maximum
-                                          // Careful to ensure the range set to be no tower double counted
   CHECK(JetAlg::BuildJet(gTs,gJet_thr,gSeeds,gL1Jets,m_gJet_r)); //default gFex jets are cone jets wih radius of 1.0
   CHECK(METAlg::BuildMET(gTs,gT_noise,gMET));
 
@@ -287,21 +169,20 @@ StatusCode JGTowerReader::ProcessObject(){
   
   for(unsigned j=0;j<jL1Jets.size();j++  ){
      JetAlg::L1Jet jet = jL1Jets.at(j);
-     hName["jJet_et"]->Fill(jet.et);
-     hName["jJet_eta"]->Fill(jet.eta);
-     hName["jJet_phi"]->Fill(jet.phi);
+     CHECK(HistBookFill("jJet_et",50,0,500,jet.et/1000.,1.));
+     CHECK(HistBookFill("jJet_eta",49,-4.9,4.9,jet.eta,1.));
+     CHECK(HistBookFill("jJet_phi",31,-3.1416,3.1416,jet.phi,1.));
   }
   for(unsigned j=0;j<gL1Jets.size();j++  ){
      JetAlg::L1Jet jet = gL1Jets.at(j);
-     hName["gJet_et"]->Fill(jet.et);
-     hName["gJet_eta"]->Fill(jet.eta);
-     hName["gJet_phi"]->Fill(jet.phi);
-
+     CHECK(HistBookFill("jJet_et",50,0,500,jet.et/1000.,1.));
+     CHECK(HistBookFill("jJet_eta",49,-4.9,4.9,jet.eta,1.));
+     CHECK(HistBookFill("jJet_phi",31,-3.1416,3.1416,jet.phi,1.));
   }
-  hName["jMet_et"]->Fill(jMET->et);
-  hName["gMet_et"]->Fill(gMET->et);
-  hName["jMet_phi"]->Fill(jMET->phi);
-  hName["gMet_phi"]->Fill(gMET->phi);
+  CHECK(HistBookFill("jMet_et",50,0,500,jMET->et,1.));
+  CHECK(HistBookFill("jMet_phi",31,-3.1416,-3.1416,jMET->et,1.));
+  CHECK(HistBookFill("gMet_et",50,0,500,gMET->et,1.));
+  CHECK(HistBookFill("gMet_phi",31,-3.1416,-3.1416,gMET->et,1.));
   
   jL1Jets.clear();
   gL1Jets.clear();
@@ -311,3 +192,35 @@ StatusCode JGTowerReader::ProcessObject(){
   gSeeds->local_max.clear();
   return StatusCode::SUCCESS;
 }
+
+StatusCode JGTowerReader::HistBookFill(const TString name, Int_t nbinsx, const Double_t* xbins, float xvalue,float wei){
+
+  if(std::find(hists.begin(),hists.end(),name)==hists.end()) {
+    TH1F*h = new TH1F( name, name, nbinsx, xbins);
+    h->Sumw2();
+    CHECK( histSvc->regHist(Form("/OUTPUT/%s",name.Data()),h));
+    hName[name]=h;
+    hName[name]->Fill(xvalue,wei);
+    hists.push_back(name);
+
+  }
+  else hName[name]->Fill(xvalue,wei);
+  return StatusCode::SUCCESS;
+}
+
+StatusCode JGTowerReader::HistBookFill(const TString name, Int_t nbinsx, Double_t xbin_down, Double_t xbin_up, float xvalue,float wei){
+
+
+  if(std::find(hists.begin(),hists.end(),name)==hists.end()) {
+    TH1F*h=new TH1F( name, name, nbinsx, xbin_down,xbin_up);
+    h->Sumw2();
+    CHECK( histSvc->regHist(Form("/OUTPUT/%s",name.Data()),h));
+    hName[name]=h;
+    hName[name]->Fill(xvalue,wei);
+    hists.push_back(name);
+  }
+
+  else  hName[name]->Fill(xvalue,wei);
+  return StatusCode::SUCCESS;
+}
+
