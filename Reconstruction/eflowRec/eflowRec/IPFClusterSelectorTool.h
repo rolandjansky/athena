3df6a856b44fd5f5947111f77651592ddc7bd690
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2018-09-27 10:42:28 +0100

    Add new PFClusterSelectorTool (which is an AthAlgTool), which is identical algorithmically as PFClusterSelector (which is an AthAlgorithm). This new tool will be called from PFAlgorithm.
    Former-commit-id: d8a55d560ef733b132bc021d5c4855ce8dda2a86

---
+++ Reconstruction/eflowRec/eflowRec/IPFClusterSelectorTool.h
@@ -0,0 +1,21 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef IPFCLUSTERSELECTIONTOOL_H
+#define IPFCLUSTERSELECTORTOOL_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include "xAODCaloEvent/CaloClusterContainer.h"
+
+class IPFClusterSelectorTool : virtual public IAlgTool {
+
+ public:
+
+  /** Execute method to modify xAOD::CaloClusterContainer */
+  virtual StatusCode execute(xAOD::CaloClusterContainer&) = 0;
+
+  DeclareInterfaceID(IPFClusterSelectorTool,1,0);
+
+};
+#endif
