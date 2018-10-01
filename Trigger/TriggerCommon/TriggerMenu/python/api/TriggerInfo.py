06278f0c49b870899af5b3e2993ad2e5094ff862
Javier Montejo Berlingen <javier.montejo.berlingen@cern.ch>
2018-10-01 17:56:56 +0000

    Updates to TriggerAPI
    Former-commit-id: 21eab41d696487603570d3abac424e0998467a33

--- Trigger/TriggerCommon/TriggerMenu/python/api/TriggerInfo.py
+++ Trigger/TriggerCommon/TriggerMenu/python/api/TriggerInfo.py
@@ -10,14 +10,14 @@ class TriggerInfo:
     ''' Object containing all the HLT information related to a given period.
         Stores a list of TriggerChain objects and the functions to skim them
     '''
-    def __init__(self,period=0, customGRL=None):
+    def __init__(self,period=0, customGRL=None, release=None):
         self.triggerChains = []
         self.period = period
         self.totalLB = 0
 
         if not period: return
         from TriggerDataAccess import getHLTlist
-        HLTlist, totalLB = getHLTlist(period, customGRL)
+        HLTlist, totalLB = getHLTlist(period, customGRL, release)
         self.totalLB = totalLB
         for hlt, l1, livefraction, activeLB, hasRerun in HLTlist:
             self.triggerChains.append( TriggerChain(hlt, l1, livefraction, activeLB, hasRerun))
@@ -95,8 +95,8 @@ class TriggerInfo:
 class TriggerLeg:
     types          = ('e','j','mu','tau','xe','g','ht')
     legpattern     = re.compile('([0-9]*)(%s)([0-9]+)' % '|'.join(types))
-    detailpattern  = re.compile('(?:-?\d+)|(?:[^0-9|-]+)')
-    bjetpattern    = re.compile('bmv|btight|bmedium|bloose')
+    detailpattern  = re.compile('(?:-?\d+)|(?:[^0-9 -]+)') #split into text-only vs number-only
+    bjetpattern    = re.compile('bmv|bhmv|btight|bmedium|bloose')
     bphyspattern   = re.compile('b[A-Z]')
     exoticspattern = re.compile('llp|LLP|muvtx|hiptrt|LATE|NOMATCH')
     afppattern     = re.compile('afp|AFP')
@@ -224,7 +224,7 @@ class TriggerLeg:
                     return 1
             return -9
         compl1seed  = self.compareTags(self.l1seed, other.l1seed, stringSubset=True, debug=debug)
-        compdetails = self.compareTags("".join(self.details), "".join(other.details), debug=debug )
+        compdetails = self.compareTags(" ".join(self.details), " ".join(other.details), debug=debug )
         if self.l1seed == other.l1seed:
             if self.details == other.details: return -1
             if debug: print "compareTags 1:",compdetails
