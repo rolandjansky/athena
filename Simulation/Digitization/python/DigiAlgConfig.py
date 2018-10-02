b554516739fe2f09fccc49ea5a9d969f7ad49ad8
Vincent R. Pascuzzi <vpascuzz@cern.ch>
2018-10-02 15:27:13 -0400

    Update DigiAlgConfig.py: add configurations based on pileup type
    Former-commit-id: bde434e6f113528ee788292e4de991df5cf29a4a

--- Simulation/Digitization/python/DigiAlgConfig.py
+++ Simulation/Digitization/python/DigiAlgConfig.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon import CfgMgr
+from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
 
 #####################################
 ##     Test PileUpTool methods     ##
@@ -27,7 +28,20 @@ def getStandardTruthPileUpTools():
     from AthenaCommon.DetFlags import DetFlags
     PileUpToolsList = []
     if DetFlags.pileup.Truth_on():
-        PileUpToolsList += [ "MergeMcEventCollTool" ]
+        from Digitization.DigitizationFlags import digitizationFlags
+        if 'NewMerge' in digitizationFlags.experimentalDigi():
+            if not athenaCommonFlags.DoFullChain():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_Signal" ]
+            if digitizationFlags.doLowPtMinBias.get_Value():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_MinBias" ]
+            if digitizationFlags.doHighPtMinBias.get_Value():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_HighPtMinBias" ]
+            if digitizationFlags.doCavern.get_Value():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_Cavern" ]
+            if digitizationFlags.doBeamGas.get_Value() or digitizationFlags.doBeamHalo.get_Value():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_HaloGas" ]
+        else:
+            PileUpToolsList += [ "MergeMcEventCollTool" ]
         PileUpToolsList += [ "MergeTruthJetsTool" ]
         if DetFlags.writeRDOPool.Muon_on(): #possibly this should be digitize.Muon_on()
             PileUpToolsList += [ "MergeTrackRecordCollTool" ]
@@ -39,9 +53,14 @@ def getStandardSignalOnlyTruthPileUpTools():
     from AthenaCommon.DetFlags import DetFlags
     PileUpToolsList = []
     if DetFlags.pileup.Truth_on():
-        PileUpToolsList += [ "SignalOnlyMcEventCollTool" ]
+        from Digitization.DigitizationFlags import digitizationFlags
+        if 'NewMerge' in digitizationFlags.experimentalDigi():
+            if not athenaCommonFlags.DoFullChain():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_Signal" ]
+        else:
+            PileUpToolsList += [ "SignalOnlyMcEventCollTool" ]
         PileUpToolsList += [ "MergeTruthJetsTool" ]
-        if DetFlags.writeRDOPool.Muon_on(): #possibly this should be digitize.Muon_on()
+        if not athenaCommonFlags.DoFullChain() and DetFlags.writeRDOPool.Muon_on(): #possibly this should be digitize.Muon_on()
             PileUpToolsList += [ "MergeTrackRecordCollTool" ]
         if DetFlags.writeRDOPool.Calo_on(): #possibly this should be digitize.Calo_on()
             PileUpToolsList += [ "MergeCalibHitsTool" ]
@@ -51,9 +70,22 @@ def getStandardInTimeOnlyTruthPileUpTools():
     from AthenaCommon.DetFlags import DetFlags
     PileUpToolsList = []
     if DetFlags.pileup.Truth_on():
-        PileUpToolsList += [ "InTimeOnlyMcEventCollTool" ]
+        from Digitization.DigitizationFlags import digitizationFlags
+        if 'NewMerge' in digitizationFlags.experimentalDigi():
+            if not athenaCommonFlags.DoFullChain():
+                PileUpToolsList += [ "NewMergeMcEventCollTool_Signal" ]
+            if digitizationFlags.doLowPtMinBias.get_Value():
+                PileUpToolsList += [ "InTimeOnlyNewMergeMcEventCollTool_MinBias" ]
+            if digitizationFlags.doHighPtMinBias.get_Value():
+                PileUpToolsList += [ "InTimeOnlyNewMergeMcEventCollTool_HighPtMinBias" ]
+            if digitizationFlags.doCavern.get_Value():
+                PileUpToolsList += [ "InTimeOnlyNewMergeMcEventCollTool_Cavern" ]
+            if digitizationFlags.doBeamGas.get_Value() or digitizationFlags.doBeamHalo.get_Value():
+                PileUpToolsList += [ "InTimeOnlyNewMergeMcEventCollTool_HaloGas" ]
+        else:
+            PileUpToolsList += [ "InTimeOnlyMcEventCollTool" ]
         PileUpToolsList += [ "MergeTruthJetsTool" ]
-        if DetFlags.writeRDOPool.Muon_on(): #possibly this should be digitize.Muon_on()
+        if not athenaCommonFlags.DoFullChain() and DetFlags.writeRDOPool.Muon_on(): #possibly this should be digitize.Muon_on()
             PileUpToolsList += [ "MergeTrackRecordCollTool" ]
         if DetFlags.writeRDOPool.Calo_on(): #possibly this should be digitize.Calo_on()
             PileUpToolsList += [ "MergeCalibHitsTool" ]
