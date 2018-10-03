77214cfd1d96f6c9ee504c95167915b3c3cdfb34
Francesca Pastore <francesca.pastore@cern.ch>
2018-10-03 12:10:35 +0200

    added missing files
    Former-commit-id: 424cbf01993583b84b88e4d751d762afbc248c80

---
+++ Trigger/TrigValidation/TrigUpgradeTest/python/jetMenuDefs.py
@@ -0,0 +1,31 @@
+from AthenaCommon.Constants import VERBOSE,DEBUG,INFO
+
+
+def jetSequence():
+    # menu components   
+    from TriggerMenuMT.HLTMenuConfig.Menu.MenuComponents import MenuSequence
+    from AthenaCommon.CFElements import parOR, seqAND, seqOR, stepSeq
+
+    #input maker
+    from TrigUpgradeTest.TrigUpgradeTestConf import HLTTest__TestInputMaker
+    InputMakerAlg = HLTTest__TestInputMaker("JetInputMaker", OutputLevel = DEBUG, LinkName="initialRoI")
+    InputMakerAlg.Output='FSJETRoIs'
+
+    #reco sequence
+    from TrigUpgradeTest.jetDefs import jetRecoSequence
+    (recoSequence, sequenceOut) = jetRecoSequence(InputMakerAlg.Output)
+
+    #hypo
+    from TrigHLTJetHypo.TrigHLTJetHypoConf import TrigJetHypoAlgMT
+    from TrigHLTJetHypo.TrigJetHypoToolConfig import trigJetHypoToolFromName
+    hypo = TrigJetHypoAlgMT("TrigJetHypoAlgMT")
+    hypo.OutputLevel = DEBUG
+    hypo.Jets = sequenceOut
+
+    # sequence
+    JetAthSequence =  seqAND("jetAthSequence",[InputMakerAlg, recoSequence ])
+
+    return  MenuSequence( Sequence    = JetAthSequence,
+                          Maker       = InputMakerAlg,
+                          Hypo        = hypo,
+                          HypoToolGen = trigJetHypoToolFromName )
