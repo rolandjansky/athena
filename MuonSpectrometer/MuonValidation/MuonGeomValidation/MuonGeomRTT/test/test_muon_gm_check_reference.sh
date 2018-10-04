19eb4ef3001716512333450896c3b4d5307fab11
Olga Igonkina <olga.igonkina@cern.ch>
2018-10-04 13:44:12 +0000

    Merge branch 'ArtMuon211' into '21.1'
    Delete Muon ART Test Scripts in Undesired Branches
    
    See merge request atlas/athena!14692
    
    (cherry picked from commit bacca598bd574087f67623922ea6f0605e796f3a [formerly 892f280aefd95f31cf79926612adefcef1e8fa3f])
    
    27c67c23 Delete Muon ART Test Scripts in Undesired Branches
    
    Former-commit-id: 12000878d4fd603cc3f151f8977ee9270657f86b

--- MuonSpectrometer/MuonValidation/MuonGeomValidation/MuonGeomRTT/test/test_muon_gm_check_reference.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# art-description: Art test job for MuonGeomRTT package, migrated from RTT job: MuonGMCheckReference
-# # Purpose: Compare MuonGeoModel raw and readout geometry relative to a reference
-#
-# art-type: grid
-
-athena.py -c 'DetDescrVersion="ATLAS-GEO-16-00-01"' -b MuonGeomRTT/MuonGMCheck_RTT_topOptions.py
