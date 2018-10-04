bec4868d5206d1d994579a36c083133f75d5b2f4
Manfredi Ronzani <manfredi.ronzani@cern.ch>
2018-10-04 16:52:31 +0200

    first HelperTool add to metAlg, it does run
    Former-commit-id: 866339134840f4afce84b910867c68ebd600f8a1

--- Trigger/TrigAlgorithms/TrigEFMissingET/share/testMET.py
+++ Trigger/TrigAlgorithms/TrigEFMissingET/share/testMET.py
@@ -43,10 +43,10 @@ metAlg = EFMissingETAlgMT( name="EFMET" )
 
 
 metAlg.METTools=[ cellTool ]
-
-
+metAlg.HelperTool= helperTool 
 
 #metAlg.OutputLevel=DEBUG
+
 metMon = GenericMonitoringTool("METMonTool")
 metMon.Histograms = [ defineHistogram( "TIME_Total", title="Time spent Alg", xbins=100, xmin=0, xmax=100 ),
                       defineHistogram( "TIME_Loop", title="Time spent in Tools loop", xbins=100, xmin=0, xmax=100 )]
@@ -58,9 +58,7 @@ metMon.Histograms += [ hCompEx, hCompEy, hCompEz, hCompEt, hCompSumEt, hCompSumE
 metMon.Histograms += [ hCompEt_lin, hCompSumEt_lin ]
 
 metAlg.MonTool = metMon
-#metAlg += cellTool
 topSequence += metAlg
-topSequence += helperTool
 
 #   from TrigCaloRec.TrigCaloRecConfig import TrigCaloClusterMakerMT_topo
   
