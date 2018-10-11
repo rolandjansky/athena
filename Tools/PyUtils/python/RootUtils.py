2b960bc745ab2cfa41ed894e71bd78cd9e444799
Will Buttinger <will@cern.ch>
2018-10-11 14:14:40 +0100

    ensure fix from 5f553898 is only applied in analysis releases
    Former-commit-id: 00543cc80d1fd3b7bf6e3a247fef2e5902044788

--- Tools/PyUtils/python/RootUtils.py
+++ Tools/PyUtils/python/RootUtils.py
@@ -22,6 +22,7 @@ import sys
 import re
 from pprint import pprint
 from array import array
+import ROOT
 
 from .Decorators import memoize
 
@@ -29,8 +30,13 @@ from .Decorators import memoize
 # Set buffer size, in bytes.
 # The argument to SetSize is in elements, not bytes.
 def _set_byte_size (buf, sz):
-    eltsz = array(buf.typecode).itemsize
-    buf.SetSize (sz / eltsz)
+    if ROOT.gROOT.GetVersionInt() >= 61400:
+        eltsz = array(buf.typecode).itemsize
+        buf.SetSize (sz / eltsz)
+    else:
+        #in older versions of ROOT (6.08) the above causes issues 
+        #so do old thing instead ..
+        buf.SetSize( sz )
     return
 
 def import_root(batch=True):
