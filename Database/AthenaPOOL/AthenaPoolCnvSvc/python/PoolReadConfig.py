88f3d0ff1e81000fecc6b1e0e04766fd43cbc41c
Walter Lampl <Walter.Lampl@cern.ch>
2018-10-16 11:10:46 +0200

    AthenaPoolAddressProvider service needs now MetaDataStore. Add to config.
    Former-commit-id: a2c280d88476d4b4986f029b06d01e577ef3e841

--- Database/AthenaPOOL/AthenaPoolCnvSvc/python/PoolReadConfig.py
+++ Database/AthenaPOOL/AthenaPoolCnvSvc/python/PoolReadConfig.py
@@ -16,12 +16,15 @@ def PoolReadCfg(configFlags):
     from EventSelectorAthenaPool.EventSelectorAthenaPoolConf import AthenaPoolAddressProviderSvc, EventSelectorAthenaPool
     from GaudiSvc.GaudiSvcConf import EvtPersistencySvc
     
-
+    from StoreGate.StoreGateConf import StoreGateSvc
 
     result.addService(PoolSvc(MaxFilesOpen=0))
     apcs=AthenaPoolCnvSvc()
     result.addService(apcs)
     result.addService(EvtPersistencySvc("EventPersistencySvc",CnvServices=[apcs.getFullJobOptName(),])) #No service handle yet???
+
+
+    result.addService(StoreGateSvc("MetaDataStore"))
 
     apaps=AthenaPoolAddressProviderSvc()
     result.addService(apaps)
