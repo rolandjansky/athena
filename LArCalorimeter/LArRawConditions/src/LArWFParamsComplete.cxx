9cd5b54fd43ddbbc150ee60f6e02540e3bd9b810
Walter Lampl <Walter.Lampl@cern.ch>
2018-10-16 17:36:35 +0200

    remove dead code
    Former-commit-id: af4a8788a9ee61135fb91046cffaf72b51b143b4

--- LArCalorimeter/LArRawConditions/src/LArWFParamsComplete.cxx
+++ LArCalorimeter/LArRawConditions/src/LArWFParamsComplete.cxx
@@ -1,20 +1,8 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "LArRawConditions/LArWFParamsComplete.h" 
-#include "GaudiKernel/IMessageSvc.h"
-
-#include "LArIdentifier/LArOnlineID.h"
-#include "LArCabling/LArCablingService.h"
-
-#include "GaudiKernel/Bootstrap.h"
-#include "GaudiKernel/ISvcLocator.h"
-#include "GaudiKernel/IToolSvc.h"
-
-#include "AthenaKernel/getMessageSvc.h"
-#include <iostream> 
-//using namespace std ;
 
 LArWFParams LArWFParamsComplete::s_dummyParams = LArWFParams() ;
 
