719689708240c330c2c7ebac24eb45977649823f
tlari <tommaso.lari@cern.ch>
2018-10-17 16:10:45 +0200

    update copyright statement
    Former-commit-id: df2daef029575a9e2c2d8b5b998aa83c2b073db0

--- InnerDetector/InDetDigitization/FastTRT_Digitization/FastTRT_Digitization/TRTFastDigitizationTool.h
+++ InnerDetector/InDetDigitization/FastTRT_Digitization/FastTRT_Digitization/TRTFastDigitizationTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef FASTTRT_DIGITIZATION_FASTTRT_DIGITIZATIONTOOL_H
