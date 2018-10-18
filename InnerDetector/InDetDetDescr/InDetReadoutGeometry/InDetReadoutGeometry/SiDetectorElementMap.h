e21e22b0f9f8344447d23f0ba78ebf68bf35e6c5
scott snyder <snyder@bnl.gov>
2018-10-18 17:59:47 +0200

    InDetReadoutGeometry: CLIDSvc -> AthenaKernel
    Update references to headers in CLIDSvc moved to AthenaKernel.
    Also remove unneeded dependency on SGTools.
    
    
    
    Former-commit-id: 59ca70d3074ac5336a1a8a63b1d810d0469464ca

--- InnerDetector/InDetDetDescr/InDetReadoutGeometry/InDetReadoutGeometry/SiDetectorElementMap.h
+++ InnerDetector/InDetDetDescr/InDetReadoutGeometry/InDetReadoutGeometry/SiDetectorElementMap.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 
@@ -26,7 +26,7 @@ class SiDetectorElementMap {
 
 // Class PixelDetectorFactoryFastGeo 
 
-#include "SGTools/CLASS_DEF.h"
+#include "AthenaKernel/CLASS_DEF.h"
 CLASS_DEF( SiDetectorElementMap , 73688 , 1 )
 
 #endif
