8fcd24a7f2bd95959ef492c3a7522da67b69d6b5
scott snyder <snyder@bnl.gov>
2018-10-18 18:04:17 +0200

    InDetRawData: CLIDSvc -> AthenaKernel
    Update references to headers in CLIDSvc moved to AthenaKernel.
    Also remove unneeded dependency on SGTools.
    
    
    
    Former-commit-id: 3c81bde853ed3d605f1d4fa1153c6ce70b9455b6

--- InnerDetector/InDetRawEvent/InDetRawData/InDetRawData/InDetRawDataContainer.h
+++ InnerDetector/InDetRawEvent/InDetRawData/InDetRawData/InDetRawDataContainer.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -20,7 +20,7 @@
 #include "EventContainers/IdentifiableContainer.h"
 //Needed Classes
 #include "InDetRawData/InDetRawDataCollection.h"
-#include "CLIDSvc/CLASS_DEF.h"
+#include "AthenaKernel/CLASS_DEF.h"
 
 template<class CollectionT>
 class InDetRawDataContainer 
