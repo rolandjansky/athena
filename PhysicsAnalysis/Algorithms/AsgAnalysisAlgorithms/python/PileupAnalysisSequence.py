5d2e0b225701f9d6270d24d78344cccd5952ca86
Martin Errenst <martin.errenst@cern.ch>
2018-10-18 10:31:05 +0200

    Copy input lists for further usage
    Former-commit-id: 85a62a0fe07f18de08924fcb6fc4d51a4918bebd

--- PhysicsAnalysis/Algorithms/AsgAnalysisAlgorithms/python/PileupAnalysisSequence.py
+++ PhysicsAnalysis/Algorithms/AsgAnalysisAlgorithms/python/PileupAnalysisSequence.py
@@ -17,14 +17,14 @@ def makePileupAnalysisSequence( dataType, userPileupConfigs=[], userLumicalcFile
     # Create the analysis algorithm sequence object:
     seq = AnaAlgSequence( "PileupAnalysisSequence" )
 
-    muMcFiles = userPileupConfigs
+    muMcFiles = userPileupConfigs[:]
 
     if userLumicalcFiles==[]:
         muDataFiles = ["GoodRunsLists/data15_13TeV/20170619/PHYS_StandardGRL_All_Good_25ns_276262-284484_OflLumi-13TeV-008.root",
                    "GoodRunsLists/data16_13TeV/20180129/PHYS_StandardGRL_All_Good_25ns_297730-311481_OflLumi-13TeV-009.root",
                    "GoodRunsLists/data17_13TeV/20180619/physics_25ns_Triggerno17e33prim.lumicalc.OflLumi-13TeV-010.root" ]
     else:
-        muDataFiles = userLumicalcFiles
+        muDataFiles = userLumicalcFiles[:]
 
     # Set up the only algorithm of the sequence:
     alg = createAlgorithm( 'CP::PileupReweightingAlg', 'PileupReweightingAlg' )
