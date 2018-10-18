c93ef89b93c7f26dddca8858c896894163d5ae11
scott snyder <snyder@bnl.gov>
2018-10-18 18:01:47 +0200

    BCM_G4_SD: CLIDSvc -> AthenaKernel
    Update references to headers in CLIDSvc moved to AthenaKernel.
    Also remove unneeded dependency on SGTools.
    
    
    
    Former-commit-id: a4606fde77193a0bfe6cf66c75eba6df2f4b66d3

--- InnerDetector/InDetG4/BCM_G4_SD/src/BCMExtra.h
+++ InnerDetector/InDetG4/BCM_G4_SD/src/BCMExtra.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef BCMEXTRA_H
@@ -21,7 +21,7 @@ private:
   double m_vertexZ;
 };
 
-#include "CLIDSvc/CLASS_DEF.h"
+#include "AthenaKernel/CLASS_DEF.h"
 CLASS_DEF(BCMExtra, 207661756, 1)
 
 #endif
