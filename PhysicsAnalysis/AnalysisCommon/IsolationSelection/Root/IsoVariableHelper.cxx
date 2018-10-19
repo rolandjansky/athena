b95fcb8dc4b790761bdc5eef8a1811cc2915c5ff
Johannes Josef Junggeburth <johannes.josef.junggeburth@CERN.CH>
2018-10-19 08:20:48 +0200

    There is also a new copyright
    Former-commit-id: 8c5686d80a1344dc01bd80a605ba74c4aefb291c

--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsoVariableHelper.cxx
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsoVariableHelper.cxx
@@ -1,9 +1,6 @@
 /*
- Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+ Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
  */
-
-
-
 
 #include <xAODPrimitives/IsolationHelpers.h>
 #include <IsolationSelection/IsoVariableHelper.h>
