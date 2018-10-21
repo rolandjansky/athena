a0a9afc64de86ec604c64d2f599c82ba83db978c
Dan Guest <dguest@cern.ch>
2018-10-21 23:34:40 +0200

    Add more matching logic
    Former-commit-id: d31d66e1c517362710788bad24fe50d4fd24b896

--- PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/DL2.h
+++ PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/DL2.h
@@ -23,12 +23,20 @@ namespace FlavorTagDiscriminants {
     EDMType type;
   };
 
+  // Most b-tagging variables have "default" flags defined.
+  struct DefaultRegex
+  {
+    std::regex regex;
+    std::string default_flag;
+  };
+
   // All the code here should just be a thin wrapper on lwtnn plus
   // some stuff to pull variables out of the EDM
   struct DL2Config
   {
-    std::string nnFilePath;
-    std::vector<InputRegex> inputRegexes;
+    std::string nn_file_path;
+    std::vector<InputRegex> input_regexes;
+    std::vector<DefaultRegex> defatul_regexes;
   };
 
   // internally we want a bunch of std::functions that return pairs to
@@ -49,11 +57,57 @@ namespace FlavorTagDiscriminants {
       m_name(name)
       {
       }
-    std::pair<std::string, double> operator()(const xAOD::Jet& jet) {
+    std::pair<std::string, double> operator()(const xAOD::Jet& jet) const {
       const xAOD::BTagging* btag = jet.btagging();
       if (!btag) throw std::logic_error("can't find btagging object");
       return {m_name, m_default_flag(*btag) ? NAN : m_getter(*btag)};
     }
   };
+
+  // now we define some additional helper functions
+  //
+  // Matcher
+  template <typename T>
+  class Matcher
+  {
+  private:
+    std::vector<std::pair<std::regex, T> > m_regexes;
+  public:
+    Matcher(const std::vector<std::pair<std::regex, T> >& regexes):
+      m_regexes(regexes)
+      {}
+    T operator()(const std::string& var_name) const {
+      for (const auto& pair: m_regexes) {
+        if (std::regex_match(var_name, pair.first)) {
+          return pair.second;
+        }
+      }
+      throw std::logic_error("no match found for " + var_name);
+    }
+  };
+  //
+  // Type finder
+  class TypeFinder
+  {
+  public:
+    TypeFinder(std::vector<InputRegex>);
+    EDMType operator()(const std::string&) const;
+  private:
+    std::vector<InputRegex> m_regexes;
+  };
+  // Default finder
+  class DefaultFinder
+  {
+  public:
+    DefaultFinder(std::vector<DefaultRegex>);
+    std::string operator()(const std::string&) const;
+  private:
+    std::vector<DefaultRegex> m_regexes;
+  };
+  //
+  // Filler function
+  std::function<std::pair<std::string, double>(const xAOD::Jet&)>
+  get_filler(std::string name, EDMType, std::string default_flag);
+
 }
 #endif
