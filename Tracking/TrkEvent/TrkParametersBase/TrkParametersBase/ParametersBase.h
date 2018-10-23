4d580a8dc634b58ab6583b5030a321bf2c3b526e
christos <christos@cern.ch>
2018-10-23 22:24:28 +0100

    113531 Missing move assignment operator
    Former-commit-id: 97484f3bf0a0776decd0a5a939ad22a2351264f4

--- Tracking/TrkEvent/TrkParametersBase/TrkParametersBase/ParametersBase.h
+++ Tracking/TrkEvent/TrkParametersBase/TrkParametersBase/ParametersBase.h
@@ -54,9 +54,18 @@ namespace Trk
   {
   public:
     static constexpr int dim = DIM;
-    
+   
+    /*
+     * Rule of 5 
+     */
+
     /** virtual Destructor */
-    virtual ~ParametersBase() {}
+    virtual ~ParametersBase()=default;
+    /** default copy ctors and assignment operators*/
+     ParametersBase(const ParametersBase&)=default;
+     ParametersBase& operator=(const ParametersBase&)=default;
+     ParametersBase(ParametersBase&&)=default;
+     ParametersBase& operator=(ParametersBase&&)=default;  
 
     //** equality operator */
     virtual bool operator==(const ParametersBase<DIM,T>&) const = 0;
