fa4efbc3b7dc2820c6c1810651ea77ae854f65de
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2018-10-24 16:18:33 +0000

    Merge branch 'nsw_edm_fix' into '21.3'
    Adding RDO's for the NSW L1 trigger
    
    See merge request atlas/athena!14995
    
    (cherry picked from commit f33064c749784de8ea079685732c7d72bbff1115 [formerly 347e3f1b3d8c1e46e6b968aae65d68dded7311db])
    
    2a57cc0b Adding RDO's for the NSW L1 trigger
    d4b383aa Adding the RDO's for the L1 of the NSW muon detectors
    
    Former-commit-id: 61824c3f45a99fc62df7417732d4f62b95b50efb

---
+++ MuonSpectrometer/MuonRDO/MuonRDO/NSW_TrigRawDataContainer.h
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/  
+
+#ifndef NSW_TRIGRAWDATACONTAINER_H
+#define NSW_TRIGRAWDATACONTAINER_H
+
+#include "DataModel/DataVector.h"
+#include "SGTools/CLASS_DEF.h"
+
+#include "MuonRDO/NSW_TrigRawData.h"
+
+namespace Muon {
+  class NSW_TrigRawDataContainer : public DataVector<Muon::NSW_TrigRawData> {
+
+ public:
+  NSW_TrigRawDataContainer();
+
+  ~NSW_TrigRawDataContainer() { };
+
+ private:
+
+
+};
+}
+
+CLASS_DEF(Muon::NSW_TrigRawDataContainer,1172141273,1)
+
+#endif   /// NSW_TRIGRAWDATACONTAINER_H
+
+
+
+
+
