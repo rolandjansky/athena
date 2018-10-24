856c2ee03d2b28e677edbe09514faa7284e18e6f
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2018-10-24 11:11:11 +0200

    add file Projects/AthGeneration/externals/Superchic.cmake update herwig to v. 713 (in Projects/AthGeneration/externals/Herwig3.cmake) and ThePEG to 213 (in Projects/AthGeneration/externals/ThePEG.cmake)
    Former-commit-id: f9908e286b8b61c75cb2d0c41cc779e790b0727a

---
+++ Projects/AthGeneration/externals/Superchic.cmake
@@ -0,0 +1,7 @@
+#
+# File specifying the location of Superchic 3.0 to use.
+#
+
+set( SUPERCHIC_VERSION 3.0 )
+set( SUPERCHIC_ROOT
+   ${LCG_RELEASE_DIR}/MCGenerators/superchic/${SUPERCHIC_VERSION}/${LCG_PLATFORM} )
