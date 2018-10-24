7c4c40c91c1cf79176535553bdbc9fceb7e5293d
Paul Gessinger <paul.gessinger@cern.ch>
2018-10-24 13:44:44 +0200

    Make ATLAS field wrapper compatible with new upcoming interface
    Former-commit-id: 01da71f3f868a40e3159e423c6b63fed39d01f57

--- Tracking/Acts/ActsGeometry/ActsGeometry/ATLASMagneticFieldWrapper.h
+++ Tracking/Acts/ActsGeometry/ActsGeometry/ATLASMagneticFieldWrapper.h
@@ -15,6 +15,11 @@ class ATLASMagneticFieldWrapper
 
 public:
 
+  struct Cache {
+    // empty, no cache for now
+  };
+
+  // FieldCell is not needed anymore, keep it for backwards compatibility right now.
   struct FieldCell {
   public:
     FieldCell(MagField::IMagFieldSvc* fieldService)
@@ -56,23 +61,49 @@ public:
   };
 
   ATLASMagneticFieldWrapper(MagField::IMagFieldSvc *fieldService)
-    : m_fieldCell(fieldService)
+    : m_fieldCell(fieldService),
+      m_fieldService(fieldService)
   {
   }
 
   Acts::Vector3D
   getField(const Acts::Vector3D& pos) const
   {
-    return m_fieldCell.getField(pos);
+    Acts::Vector3D bfield;
+    m_fieldService->getField(&pos, &bfield);
+
+    bfield *= m_bFieldUnit; // kT -> T;
+
+    return bfield;
   }
   
   Acts::Vector3D
-  getFieldGradient(const Acts::Vector3D& position, Acts::ActsMatrixD<3, 3>& gradient) const
+  getField(const Acts::Vector3D& pos, Cache& /*cache*/) const
   {
-    return m_fieldCell.getFieldGradient(position, gradient);
+    return getField(pos);
   }
 
+  Acts::Vector3D
+  getFieldGradient(const Acts::Vector3D& position, Acts::ActsMatrixD<3, 3>& gradient) const
+  {
+    Acts::Vector3D bfield;
+    m_fieldService->getField(&position, &bfield, &gradient);
 
+    bfield *= m_bFieldUnit; // kT -> T;
+    gradient *= m_bFieldUnit;
+
+    return bfield;
+  }
+  
+  Acts::Vector3D
+  getFieldGradient(const Acts::Vector3D& position,
+                   Acts::ActsMatrixD<3, 3>& gradient,
+                   Cache& /*cache*/) const
+  {
+    return getFieldGradient(position, gradient);
+  }
+
+  // only kept for backwards compatibility
   Acts::concept::AnyFieldCell<>
   getFieldCell(const Acts::Vector3D& /*position*/) const
   {
@@ -80,7 +111,11 @@ public:
   }
         
 private:
+  // only kept for backwards compatibility
   FieldCell m_fieldCell;
+  
+  MagField::IMagFieldSvc *m_fieldService;
+  const double m_bFieldUnit = 1000.*Acts::units::_T;
 };
 
 
