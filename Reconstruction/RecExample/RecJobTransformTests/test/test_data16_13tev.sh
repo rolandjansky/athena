2be34287c482d9a2a180f06735ff01ac3ed3783d
Xiaohu Sun <xiaohu.sun@cern.ch>
2018-10-25 11:05:52 +0200

    add art include in head RecJobTransformTests
    Former-commit-id: 9ff8d0e29e1cce66a9287d1eb88394d25f56bb0d

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev.sh
@@ -2,8 +2,14 @@
 #
 # art-description: Reco_tf runs on 13TeV collision data with all streams 2016
 # art-type: grid
+# art-include: 21.0/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
+
 
 Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data16_13TeV.00310809.physics_Main.daq.RAW._lb1219._SFO-2._0001.data --maxEvents 300 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2016-19" --preExec 'rec.doTrigger=False;' --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputTAGFile myTAG.pool.root --outputHISTFile myHist.root
 
-echo "art-result: $?"
+echo "art-result: $? Reco"
 
