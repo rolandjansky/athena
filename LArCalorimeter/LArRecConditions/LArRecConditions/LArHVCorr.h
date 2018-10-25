96f00f0ce330487716b3a2a9362732169d36336e
charles leggett <leggett@cern.ch>
2018-10-25 10:45:35 -0700

    fix location of CLASS_DEF macro
    Former-commit-id: 15b9dd2e3ca0b94f7b25f70826ef827b42d81387

--- LArCalorimeter/LArRecConditions/LArRecConditions/LArHVCorr.h
+++ LArCalorimeter/LArRecConditions/LArRecConditions/LArHVCorr.h
@@ -41,9 +41,8 @@ class LArHVCorr : public ILArHVScaleCorr {
   const float              m_badCorr;
 };
 
-#include "CLIDSvc/CLASS_DEF.h"
-CLASS_DEF( LArHVCorr, 52206080, 1)
 #include "AthenaKernel/CondCont.h"
-CLASS_DEF( CondCont<LArHVCorr>, 24667986, 1) 
+CLASS_DEF( LArHVCorr, 52206080, 1)
+CONDCONT_DEF( LArHVCorr, 24667986 );
 
 #endif
