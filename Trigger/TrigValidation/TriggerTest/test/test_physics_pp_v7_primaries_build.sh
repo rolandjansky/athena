edc192c3e26443aa10d5fb8a6fce2f0b181da294
Julie Kirk <Julie.Kirk@cern.ch>
2018-10-25 08:35:35 +0200

    correct the menu in "primaries" ART tests
    	modified:   Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_build.sh
    	modified:   Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_grid.sh
    
    
    Former-commit-id: e0078a38af8d7247dd76309da427b97100641e1f

--- Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_build.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_build.sh
@@ -3,11 +3,7 @@
 # art-description: Physics v7 TriggerTest on MC
 # art-type: build
 # art-include: 21.1/AthenaP1
-# art-include: 21.1-dev/AthenaP1
-# art-include: 21.0/Athena
-# art-include: 21.0-TrigMC/Athena
 # art-include: master/Athena
-# art-include: master/AthenaP1
 # art-output: HLTChain.txt
 # art-output: HLTTE.txt
 # art-output: L1AV.txt
@@ -22,7 +18,7 @@
 # art-output: TotalEventsProcessed.txt
 
 export NAME="physics_pp_v7_build"
-export MENU="Physics_pp_v7"
+export MENU="Physics_pp_v7_primaries"
 export EVENTS="5"
 export COST_MONITORING="False"
 
