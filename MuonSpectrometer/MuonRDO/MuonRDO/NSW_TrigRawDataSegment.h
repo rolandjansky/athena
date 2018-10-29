ff1aba8564dfdba7d523f7834dd638abb100d258
Stefano Rosati <stefano.rosati@cern.ch>
2018-10-29 19:46:49 +0100

    Adding NSW L1 trigger RDO's 	new file:   MuonSpectrometer/MuonRDO/MuonRDO/NSW_TrigRawData.h 	new file:   MuonSpectrometer/MuonRDO/MuonRDO/NSW_TrigRawDataContainer.h 	new file:   MuonSpectrometer/MuonRDO/MuonRDO/NSW_TrigRawDataSegment.h 	new file:   MuonSpectrometer/MuonRDO/src/NSW_TrigRawData.cxx 	new file:   MuonSpectrometer/MuonRDO/src/NSW_TrigRawDataContainer.cxx 	new file:   MuonSpectrometer/MuonRDO/src/NSW_TrigRawDataSegment.cxx
    Former-commit-id: 1336c9f43e4d6029bed2331fcc6f0b071f63ce97

---
+++ MuonSpectrometer/MuonRDO/MuonRDO/NSW_TrigRawDataSegment.h
@@ -0,0 +1,50 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/  
+
+#ifndef NSW_TRIGRAWDATASEGMENT_H
+#define NSW_TRIGRAWDATASEGMENT_H
+
+#include "AthContainers/DataVector.h"
+#include "SGTools/CLASS_DEF.h"
+
+
+namespace Muon {
+class NSW_TrigRawDataSegment
+{
+
+ public:
+  NSW_TrigRawDataSegment();
+
+  NSW_TrigRawDataSegment(uint8_t deltaTheta, uint8_t phiIndex, uint8_t rIndex,
+			 bool lowRes, bool phiRes);
+
+  ~NSW_TrigRawDataSegment() { };
+
+  uint8_t deltaTheta() {return m_deltaTheta;}
+  uint8_t phiIndex() {return m_phiIndex;}
+  uint8_t rIndex() {return m_rIndex;}
+
+  bool lowRes()   {return m_lowRes;}
+  bool phiRes()   {return m_phiRes;}
+
+
+ private:
+
+  uint8_t m_deltaTheta;
+  uint8_t m_phiIndex;
+  uint8_t m_rIndex;
+
+  bool m_lowRes;
+  bool m_phiRes;
+
+};
+}
+
+CLASS_DEF(Muon::NSW_TrigRawDataSegment,218364457,1)
+
+#endif   ///  NSW_TRIGRAWDATASEGMENT_H
+
+
+
+
