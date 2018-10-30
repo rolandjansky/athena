cb796849a8264b0fdcab956ef6debeeec24e5425
Julie Hart Kirk <julie.kirk@stfc.ac.uk>
2018-10-30 11:40:33 +0000

    Add new ART tests with physics_pp_v7_primaries menu
    Former-commit-id: bce2245eb2b070374227544d21fca09f83aaa7a1

--- Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_grid.sh
@@ -3,12 +3,7 @@
 # art-description: Physics v7 TriggerTest on MC
 # art-type: grid
 # art-include: 21.1/AthenaP1
-# art-include: 21.1-dev/AthenaP1
-# art-include: 21.0/Athena
-# art-include: 21.3/Athena
-# art-include: 21.0-TrigMC/Athena
 # art-include: master/Athena
-# art-include: master/AthenaP1
 # art-output: HLTChain.txt
 # art-output: HLTTE.txt
 # art-output: L1AV.txt
@@ -22,8 +17,8 @@
 # art-output: *perfmon*
 # art-output: TotalEventsProcessed.txt
 
-export NAME="physics_pp_v7_grid"
-export MENU="Physics_pp_v7"
+export NAME="physics_pp_v7_grid_primaries"
+export MENU="Physics_pp_v7_primaries"
 export EVENTS="500"
 
 source exec_athena_art_trigger_validation.sh
