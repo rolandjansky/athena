08137ad6deeaafd073ec8367dce349f93123acb0
James Beacham <j.beacham@cern.ch>
2018-10-30 19:12:48 +0000

    Merge branch 'add-arthead' into '21.0'
    Add art include in head of test files RecJobTransformTests and RecExRecoTest
    
    See merge request atlas/athena!15339
    
    (cherry picked from commit f76694fcba87edcb5173c57fb923264f69e5e66f [formerly eb805fde445ee2d227616623b69d85a55db94a8e])
    
    9ff8d0e2 add art include in head RecJobTransformTests
    d250335d add art head and new tests from master in RecExRecoTest
    31ad6d75 minor updates in recjobtrasnformtests
    
    Former-commit-id: 77d311a358a26c0973ac33cc429800a492f9fde5

--- Reconstruction/RecExample/RecJobTransformTests/test/test_recoshift_recotf_q221.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_recoshift_recotf_q221.sh
@@ -2,5 +2,12 @@
 #
 # art-description: RIG convenors - q221 reconstruction
 # art-type: build
+# art-include: 21.0/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
 
 export TRF_ECHO=True; Reco_tf.py --AMIConfig=q221 --preExec 'rec.doDetailedAuditor=True' 'rec.doNameAuditor=True'
+
+echo "art-result: $? Reco"
