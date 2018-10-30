12c965db964e585afd1aef280a7cf6379826f878
James Beacham <j.beacham@cern.ch>
2018-10-30 19:12:48 +0000

    Merge branch 'add-arthead' into '21.0'
    Add art include in head of test files RecJobTransformTests and RecExRecoTest
    
    See merge request atlas/athena!15339
    
    (cherry picked from commit f76694fcba87edcb5173c57fb923264f69e5e66f [formerly eb805fde445ee2d227616623b69d85a55db94a8e])
    
    9ff8d0e2 add art include in head RecJobTransformTests
    d250335d add art head and new tests from master in RecExRecoTest
    31ad6d75 minor updates in recjobtrasnformtests
    
    Former-commit-id: 850db5160f3c68d86b4d3a6fcfbabe2eb22905d3

--- Reconstruction/RecExample/RecJobTransformTests/test/test_recotf_8teV_data_no.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_recotf_8teV_data_no.sh
@@ -2,6 +2,13 @@
 #
 # art-description: David Rousseau, data12_8TeV reconstruction, all reco switched off
 # art-type: build
+# art-include: 21.0/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
 
 
 export TRF_ECHO=True; Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data12_8TeV.00209109.physics_JetTauEtmiss.merge.RAW._lb0186._SFO-1._0001.1 --maxEvents 5 --autoConfiguration everything --preExec 'rec.doCalo=False' 'rec.doInDet=False' 'rec.doMuon=False' 'rec.doJetMissingETTag=False' 'rec.doEgamma=False' 'rec.doMuonCombined=False' 'rec.doTau=False' 'rec.doTrigger=False' 'rec.doDetailedAuditor=True' 'rec.doNameAuditor=True' --outputESDFile my.ESD.pool.root
+
+echo "art-result: $? Reco"
