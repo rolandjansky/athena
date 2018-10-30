67b58770b4c69d6a2de7b0824570c05d53686297
Tomasz Bold <tomasz.bold@gmail.com>
2018-10-30 16:30:28 +0100

    Placeholder for egamma config
    Former-commit-id: 9b393a91d48cb8b28bfbd3c46e010e55de0ecd33

---
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Egamma/generateEgamma.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+
+def generateChains( flags,  chainDict ):
+    print "Generating electron chain"
+    import pprint
+    pprint.pprint( chainDict )
+    
+    
+    return []
