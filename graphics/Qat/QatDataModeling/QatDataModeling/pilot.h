6397be3c52f80f200899c9a2f967b00be6cfc41c
Edward Moyse <edward.moyse@cern.ch>
2018-11-01 16:53:33 +0100

    Qat has been disabled for a while now. Removing it completely.
    Former-commit-id: 9dc8a2c8628564d199dc0d9b3b7c0e74254fd8c7

--- graphics/Qat/QatDataModeling/QatDataModeling/pilot.h
+++
@@ -1,7 +0,0 @@
-#if 0
-#endif
-#if !defined(CERNLIB_SINGLE)
-#ifndef CERNLIB_DOUBLE
-#define CERNLIB_DOUBLE
-#endif
-#endif
--- graphics/Qat/QatPlotting/QatPlotting/PlotWave1DProperties.h
+++
@@ -1,5 +0,0 @@
-#ifndef _PLOTWAVE1DPROPERTIES_H_
-#define _PLOTWAVE1DPROPERTIES_H_
-#include "QatPlotting/PlotFunction1DProperties.h"
-typedef PlotFunction1DProperties PlotWave1DProperties;
-#endif
