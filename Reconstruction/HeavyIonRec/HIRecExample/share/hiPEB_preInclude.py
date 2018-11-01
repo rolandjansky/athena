b295bd5ff88efbbdb98dfe2c5ebe345df64578fb
Dominik Karol Derendarz <dominik.derendarz@cern.ch>
2018-11-01 18:56:46 +0100

    Config files for HI reconstruciton in HIRecExample
    Added two python files that move part of the configs from Reco_trf
    to make ami reco tags less clutered.
    hiPEB_preInclude.py will be preInluded for reco partial events
    topoculsters_moments_AOD.py have the postExec the is needed to
    add mising topocluster moments as discused in ATLHI-227
    
    
    Former-commit-id: 891edbe1c7eab4c1291c19e1dd0c417aac422fdb

--- Reconstruction/HeavyIonRec/HIRecExample/share/hiPEB_preInclude.py
+++ Reconstruction/HeavyIonRec/HIRecExample/share/hiPEB_preInclude.py
@@ -1,12 +1,10 @@
+jobproperties.Beam.bunchSpacing.set_Value_and_Lock(100)
 from HIRecExample.HIRecExampleFlags import jobproperties
 jobproperties.HIRecExampleFlags.doHIJetRec.set_Value_and_Lock(False)
 jobproperties.HIRecExampleFlags.doHIegamma.set_Value_and_Lock(False)
 jobproperties.HIRecExampleFlags.doHIJetAnalysis.set_Value_and_Lock(False)
-jobproperties.Beam.bunchSpacing.set_Value_and_Lock(100)
 from RecExConfig.RecFlags import rec
-rec.doMuon.set_Value_and_Lock(False)
 rec.doEgamma.set_Value_and_Lock(False)
-rec.doMuonCombined.set_Value_and_Lock(False)
 rec.doTau.set_Value_and_Lock(False)
 rec.doJetMissingETTag.set_Value_and_Lock(False)
 rec.doZdc.set_Value_and_Lock(True)
@@ -15,13 +13,10 @@ rec.doAlfa.set_Value_and_Lock(False)
 rec.doAFP.set_Value_and_Lock(False)
 rec.doFTK.set_Value_and_Lock(False)
 rec.doBTagging.set_Value_and_Lock(False)
+from CaloRec.CaloRecFlags import jobproperties 
+jobproperties.CaloRecFlags.doCaloTopoCluster.set_Value_and_Lock(True)
 from TriggerJobOpts.TriggerFlags import TriggerFlags
 TriggerFlags.doTriggerConfigOnly.set_Value_and_Lock(True)
 TriggerFlags.doHLT=False
 TriggerFlags.doFEX=False
 TriggerFlags.doHypo=False
-from InDetRecExample.InDetJobProperties import InDetFlags
-InDetFlags.useDynamicAlignFolders.set_Value_and_Lock(True)
-DQMonFlags.doStreamAwareMon.set_Value_and_Lock(False)
-from InDetRecExample.InDetJobProperties import InDetFlags
-InDetFlags.cutLevel.set_Value_and_Lock(2)
