175b40372980d21f5941ab1b11d7cfcbeb3c6f49
Eirik Gramstad <egramsta@lxplus036.cern.ch>
2018-11-02 08:26:26 +0100

    Updates to DF ART
    Updated CalibrationTag in the preExec and changed input file for EXOT23
    
    
    Former-commit-id: 83ed60046cfb2c8d46f2bb88fb9c816c21fe4432

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16TOPQ1_TOPQ2_TOPQ4_TOPQ5.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/test/test_mc16TOPQ1_TOPQ2_TOPQ4_TOPQ5.sh
@@ -7,4 +7,4 @@
 
 set -e
 
-Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.14795494._005958.pool.root.1 --outputDAODFile art.pool.root --reductionConf TOPQ1 TOPQ2 TOPQ4 TOPQ5 --maxEvents 500  --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-40" '  --passThrough True 
+Reco_tf.py --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.14795494._005958.pool.root.1 --outputDAODFile art.pool.root --reductionConf TOPQ1 TOPQ2 TOPQ4 TOPQ5 --maxEvents 500  --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-46" '  --passThrough True 
