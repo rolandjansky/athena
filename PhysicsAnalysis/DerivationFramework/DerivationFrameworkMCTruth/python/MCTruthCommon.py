5755607004b1ce8cd06e3a9bd60541dce8658386
Zach Marshall <ZLMarshall@lbl.gov>
2018-11-05 20:34:22 +0100

    First attempt at TruthLinkRepointTool
    There are a number of cases where reco objects have a decoration that is
    a link back to a truth particle. In some cases, these truth particles
    live in large collections that are dropped during derivation making.
    This code provides a tool and a default python configuration that will
    re-point these links from the small collections over to the mini truth
    collections that are included in the derivations.
    
    
    Former-commit-id: b0e25c8a3401fbc6fd5881d678c74bcdd8ed67a4

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/python/MCTruthCommon.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/python/MCTruthCommon.py
@@ -500,3 +500,28 @@ def addLargeRJetD2(kernel=None):
     ToolSvc += TruthD2Decorator
     kernel +=CfgMgr.DerivationFramework__DerivationKernel("TRUTHD2Kernel",
                                                           AugmentationTools = [TruthD2Decorator] )
+
+def addMiniTruthCollectionLinks(kernel=None):
+    # Sets up modifiers to move pointers to old truth collections to new mini truth collections
+    # Ensure that we are adding it to something
+    if kernel is None:
+        from DerivationFrameworkCore.DerivationFrameworkMaster import DerivationFrameworkJob
+        kernel = DerivationFrameworkJob
+    if hasattr(kernel,'MiniCollectionTruthLinkKernel'):
+        # Already there!  Carry on...
+        return
+    # Truth link setup for electrons, photons, and muons
+    from DerivationFrameworkMCTruth.DerivationFrameworkMCTruthConf import DerivationFramework__TruthLinkRepointTool
+    electron_relink = DerivationFramework__TruthLinkRepointTool("ElMiniCollectionTruthLinkTool",
+                                                                RecoCollection="Electrons", TargetCollection="TruthElectrons")
+    photon_relink = DerivationFramework__TruthLinkRepointTool("PhMiniCollectionTruthLinkTool",
+                                                                RecoCollection="Photons", TargetCollection="TruthPhotons")
+    muon_relink = DerivationFramework__TruthLinkRepointTool("MuMiniCollectionTruthLinkTool",
+                                                                RecoCollection="Muons", TargetCollection="TruthMuons")
+    from AthenaCommon.AppMgr import ToolSvc
+    ToolSvc += electron_relink
+    ToolSvc += photon_relink
+    ToolSvc += muon_relink
+    kernel +=CfgMgr.DerivationFramework__DerivationKernel("MiniCollectionTruthLinkKernel",
+                                                          AugmentationTools = [electron_relink,photon_relink,muon_relink] )
+
---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/src/TruthLinkRepointTool.h
@@ -0,0 +1,38 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef DERIVATIONFRAMEWORK_TruthLinkRepointTool_H
+#define DERIVATIONFRAMEWORK_TruthLinkRepointTool_H
+
+// Interface classes
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "DerivationFrameworkInterfaces/IAugmentationTool.h"
+
+// Types for functions. Note these are typedefs, so can't forward reference.
+#include "xAODTruth/TruthParticleContainer.h"
+
+// STL includes
+#include <string>
+
+namespace DerivationFramework {
+
+  class TruthLinkRepointTool : public AthAlgTool, public IAugmentationTool {
+    public: 
+      TruthLinkRepointTool(const std::string& t, const std::string& n, const IInterface* p);
+      ~TruthLinkRepointTool();
+      virtual StatusCode addBranches() const;
+
+    private:
+      /// Parameter: input collection key
+      std::string m_recoKey;
+      /// Parameter: output decoration
+      std::string m_decOutput;
+      /// Parameter: target collection
+      std::string m_targetKey;
+      // Helper function for finding matching truth particle and warning consistently
+      const int find_match(const xAOD::TruthParticle* p, const DataHandle<xAOD::TruthParticleContainer> & c) const;
+  }; 
+}
+
+#endif // DERIVATIONFRAMEWORK_TruthLinkRepointTool_H
