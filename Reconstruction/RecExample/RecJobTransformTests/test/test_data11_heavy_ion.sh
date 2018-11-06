ce13156e63e6c4dbd01e53d3d952686859b3ecbe
Xiaohu Sun <xiaohu.sun@cern.ch>
2018-11-06 10:50:41 +0100

    Add art include in head of test files RecJobTransformTests and RecExRecoTest
    Former-commit-id: 764dcf9ba207a64f396c63c4f503b3aa03a36d35

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data11_heavy_ion.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data11_heavy_ion.sh
@@ -2,8 +2,14 @@
 #
 # art-description: heavy ion reconstruction test from Andrzej Olszewski and Iwona Grabowska-Bold
 # art-type: grid
+# art-include: 21.0/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
+
 
 export TRF_ECHO=True; Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data11_hi.00193321.physics_HardProbes.merge.RAW._lb0050._SFO-9._0002.1 --outputESDFile ESD.pool.root --outputAODFile AOD.pool.root --conditionsTag COMCOND-BLKPA-RUN1-06 --geometryVersion ATLAS-R1-2011-02-00-00 --autoConfiguration=everything --maxEvents='25'
 
-echo "art-result: $?"
+echo "art-result: $? Reco"
 
