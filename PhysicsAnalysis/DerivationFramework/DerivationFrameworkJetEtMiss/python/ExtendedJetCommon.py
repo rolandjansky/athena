c0711df23993fd210de663d3f12255998c82220b
Maximilian Swiatlowski <swiatlow@uchicago.edu>
2018-11-07 18:11:47 +0100

    Fixing up updateJVT to work for PFlow
    Former-commit-id: 6d66efabb3b314c69db333bf6c441e8f0aa37199

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/ExtendedJetCommon.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/ExtendedJetCommon.py
@@ -317,21 +317,36 @@ def updateJVT(jetalg,algname,sequence):
         extjetlog.warning('*** You must apply jet calibration before scheduling JVT! ***')
 
     jvttoolname = 'DFJetJvt_'+jetalg
+    jvtefftoolname = 'DFJetJvtEff_'+jetalg
     from AthenaCommon.AppMgr import ToolSvc
+
+    #setup the jvt updating tools if not already done
     if hasattr(ToolSvc,jvttoolname):
         jetaugtool.JetJvtTool = getattr(ToolSvc,jvttoolname)
     else:
-        jvttool = CfgMgr.JetVertexTaggerTool()
+        jvttool = CfgMgr.JetVertexTaggerTool() # this should pass in the right name
         ToolSvc += jvttool
         jetaugtool.JetJvtTool = jvttool
+
+    #now do the same for the efftool
+    if hasattr(ToolSvc,jvtefftoolname):
+        jetaugtool.JetJvtEffTool = getattr(ToolSvc,jvtefftoolname)
+        extjetlog.info('Setup the jvt eff tool {}'.format(jvtefftoolname))
+    else:
+        extjetlog.info('Setting up the jvt eff tool {}'.format(jvtefftoolname))
+        jvtefftool = CfgMgr.CP__JetJvtEfficiency(jvtefftoolname)
+        if 'PFlow' in jetalg:
+          jvtefftool.SFFile="JetJvtEfficiency/Moriond2018/JvtSFFile_EMPFlow.root"
+        ToolSvc += jvtefftool
+        jetaugtool.JetJvtEffTool = jvtefftool
 
     extjetlog.info('ExtendedJetCommon: Updating JVT for jet collection: '+jetalg+'Jets')
     applyJetAugmentation(jetalg,algname,sequence,jetaugtool)
 
 def updateJVT_xAODColl(jetalg='AntiKt4EMTopo',sequence=DerivationFrameworkJob):
-    supportedJets = ['AntiKt4EMTopo']
+    supportedJets = ['AntiKt4EMTopo','AntiKt4EMPFlow']
     if not jetalg in supportedJets:
-        extjetlog.warning('*** JVT update requested for unsupported jet collection! ***')
+        extjetlog.warning('*** JVT update requested for unsupported jet collection {}! ***'.format(jetalg))
         return
     else:
         updateJVT(jetalg,'JetCommonKernel_xAODJets',sequence)
