817e4170a94192ed969b2e3070a7a9c467f0ef9e
TJ Khoo <khoo@cern.ch>
2018-11-07 18:36:50 +0100

    Adding some alternative jet selection working points (tighter JVT) intended for PFlow jets
    Former-commit-id: 55bb9bfde8b1b654fced871b42fbf5a7bfcea604

--- Reconstruction/MET/METUtilities/Root/METMaker.cxx
+++ Reconstruction/MET/METUtilities/Root/METMaker.cxx
@@ -164,6 +164,15 @@ namespace met {
       m_JvtCutTight  = 0.91; m_JvtTightPtMax  = 40.0e3;
       m_JvtCutMedium = 0.59; m_JvtMediumPtMax = 60.0e3;
       m_JvtCut       = 0.11; m_JvtPtMax = 120e3; }
+    else if (m_jetSelection == "TightJVT04")  { m_CenJetPtCut = 20e3; m_FwdJetPtCut = 30e3; m_JvtCut = 0.4; m_JvtPtMax = 60e3; if(!m_doPFlow) {ATH_MSG_WARNING("Caution: For testing with PFlow collections only!");} }
+    else if (m_jetSelection == "TighterJVT04"){ m_CenJetPtCut = 20e3; m_FwdJetPtCut = 35e3;  m_JvtCut = 0.4; m_JvtPtMax = 60e3; if(!m_doPFlow) {ATH_MSG_WARNING("Caution: For testing with PFlow collections only!");} }
+    else if (m_jetSelection == "TenaciousJVT421")  {
+        m_CenJetPtCut  = 20e3; m_FwdJetPtCut = 35e3;
+        m_JvtCutTight  = 0.4; m_JvtTightPtMax  = 40.0e3;
+        m_JvtCutMedium = 0.2; m_JvtMediumPtMax = 60.0e3;
+        m_JvtCut       = 0.1; m_JvtPtMax = 120e3;
+        if(!m_doPFlow) {ATH_MSG_WARNING("Caution: For testing with PFlow collections only!");}
+    }
     else if (m_jetSelection == "Tier0")  { m_CenJetPtCut = 0;    m_FwdJetPtCut = 0;    m_JvtCut = -1;   m_JvtPtMax = 0; }
     else if (m_jetSelection == "Expert")  { 
       ATH_MSG_INFO("Custom jet selection configured. *** FOR EXPERT USE ONLY ***");
