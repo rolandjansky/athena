bec31f00560e76ae1a5326cb34859d419e457e1c
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2018-11-08 12:58:03 +0000

    Remove association of tracks to vertex from PFOChargedCreatorAlgorithm, because the vertex subsequently attached to the PFO is never used. JetETMiss has agreed there is no likelihood that it will be used.
    Former-commit-id: 6eaee9c56e753d378892fcf06a2a0bd07a7096d8

--- Reconstruction/eflowRec/eflowRec/PFOChargedCreatorAlgorithm.h
+++ Reconstruction/eflowRec/eflowRec/PFOChargedCreatorAlgorithm.h
@@ -7,10 +7,7 @@
 #include "GaudiKernel/ToolHandle.h"
 #include "StoreGate/DataHandle.h"
 
-#include "TrackVertexAssociationTool/ITrackVertexAssociationTool.h"
-
 #include "xAODPFlow/PFOContainer.h"
-#include "xAODTracking/VertexContainer.h"
 
 class PFOChargedCreatorAlgorithm : public AthAlgorithm {
   
@@ -21,7 +18,6 @@ public:
   ~PFOChargedCreatorAlgorithm() {}
 
   StatusCode initialize();
-  void execute(const eflowCaloObject& energyFlowCaloObject, SG::WriteHandle<xAOD::PFOContainer>& chargedPFOContainerWriteHandle);
   StatusCode execute();
   StatusCode finalize();
 
@@ -34,18 +30,11 @@ private:
   /** Toggle EOverP algorithm mode, whereby no charged shower subtraction is performed */
   Gaudi::Property<bool> m_eOverPMode{this,"EOverPMode",false,"Toggle EOverP algorithm mode, whereby no charged shower subtraction is performed"};
 
-  /** ToolHandle to tracking CP loose vertex selection tool */
-  ToolHandle<CP::ITrackVertexAssociationTool> m_trackVertexAssociationTool{this,"TrackVertexAssociationTool","","ToolHandle to tracking CP loose vertex selection tool"};
-
-  /** ReadHandleKey for vertex container */
-  SG::ReadHandleKey<xAOD::VertexContainer> m_vertexContainerReadHandleKey{this,"VertexContainerName","PrimaryVertices","ReadHandleKey for vertex container"};
-
   /** ReadHandleKey for eflowCaloObjectContainer */
   SG::ReadHandleKey<eflowCaloObjectContainer> m_eflowCaloObjectContainerReadHandleKey{this,"eflowCaloObjectContainerName","eflowCaloObjects","ReadHandleKey for eflowCaloObjectContainer"};
   
   /** WriteHandleKey for charged PFO */
   SG::WriteHandleKey<xAOD::PFOContainer> m_chargedPFOContainerWriteHandleKey{this,"PFOOutputName","JetETMissChargedParticleFlowObjects","WriteHandleKey for charged PFO"};
-  
-  
+    
 };
 #endif
