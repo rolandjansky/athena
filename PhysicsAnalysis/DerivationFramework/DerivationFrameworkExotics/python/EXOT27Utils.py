7f7b91567942e4313cca13538450bef9f36bf09b
Jon Burr <jon.burr@cern.ch>
2018-11-12 09:27:57 +0100

    Interim commit to fetch upstream
    Former-commit-id: 5dbd23e67e1c9febbd14a7198718454b53f05322

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/python/EXOT27Utils.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/python/EXOT27Utils.py
@@ -1,30 +1,116 @@
 from JetRec.JetRecStandardToolManager import jtm
+import DerivationFrameworkJetEtMiss.JetCommon as JetCommon
+import DerivationFrameworkJetEtMiss.ExtendedJetCommon as ExtendedJetCommon
+from JetRec.JetRecConf import JetAlgorithm
 # Create a logger for this stream
 import AthenaCommon
 logger = AthenaCommon.Logging.logging.getLogger("EXOT27Utils")
 
-def getJetRecTool(collection, tryGetParent=True):
-  """Get the JetRecTool for a given collection from the jtm. If tryGetParent is
+def getJetRecTool(collection, getParent=True):
+  """Get the JetRecTool for a given collection from the jtm. If getParent is
      set then if that tool has an InputContainer property set then try and 
      retrieve the JetRecTool for that parent, continue going until there is no
-     InputContainer property or if the corresponding JetRecTool is not defined.
-     In this case it is assumed that this means the InputContainer is already 
-     present and not built. Will raise a KeyError if no JetRecTool can be
-     found.
+     InputContainer property. Will raise a KeyError if no JetRecTool can be
+     found at any stage.
   """
   try:
     jetRecTool = jtm[collection]
   except KeyError as e:
      raise KeyError("JetRecTool {0} not present in jtm".format(collection) )
-  if tryGetParent and hasattr(jetRecTool, "InputContainer") and jetRecTool.InputContainer:
-    try:
-      parent = getJetRecTool(jetRecTool.InputContainer, True)
-      jetRecTool = parent
-    except KeyError as e:
-      pass
+  if getParent and hasattr(jetRecTool, "InputContainer") and jetRecTool.InputContainer:
+    jetRecTool = getJetRecTool(jetRecTool.InputContainer, True)
   return jetRecTool
 
-def addPseudojetgettersToJetCollection(
+def linkPseudoJetGettersToExistingJetCollection(
+    sequence, collection, getters):
+  """Re-run jet finding for a jet collection using a new list of 
+    PseudoJetGetters. These PseudoJetGetters should already have been loaded
+    into jtm.
+    collection should be the name of the jet collection, which should already
+    have been sequenced, so it's jet rec tool will exist in jtm.
+    getters should be a map of PseudoJetGetters, each key being the name of the
+    (non ghost) collection, with the value being the name of the 
+    PseudoJetGetter in jtm.
+    Returns the name of the ungroomed collection that is the parent of
+    'collection' (this will be the same as 'collection' if this isn't groomed)
+    *and* the list of ghost labels (these are the element link names).
+    This code is largely based on the linkVRJetsToLargeRJets from HbbCommon
+  """
+  # First, retrieve the original JetRecTool - this is the one that made the
+  # *ungroomed* jets, not the groomed ones. Ghost association is done to
+  # ungroomed objects
+  originalJetRecTool = getJetRecTool(collection, getParent = True)
+  originalUngroomedName = originalJetRecTool.name()
+  ungroomedJetAlg = originalUngroomedName
+  if ungroomedJetAlg.endswith("Jets"):
+    ungroomedJetAlg = ungroomedJetAlg[:-4]
+  originalFinder = jtm[originalJetRecTool.JetFinder.getName()]
+  originalGetters = [jtm[g.getName()] for g in originalJetRecTool.PseudoJetGetters]
+  newGetters = [jtm[g] for g in getters.values()]
+
+  # Next, prepare the names of the new objects we'll need from jtm
+
+  comb_name = "_".join(getters.keys() )
+  LargeRJetFindingAlg = "jfind_{0}_{1}".format(collection, comb_name).lower()
+  LargeRJetPrefix     = "{0}_{1}".format(collection, comb_name)
+  LargeRJets          = LargeRJetPrefix+"Jets"
+  LinkTransferAlg     = "LinkTransfer_{0}_{1}".format(collection, comb_name)
+
+  # Check to see if this large R jet collection is already known to JetCommon
+  if LargeRJetFindingAlg in JetCommon.DFJetAlgs:
+    logger.info("Found {0} in DFJetAlgs".format(LargeRJetFindingAlg) )
+    # Is it in our sequence?
+    if hasattr(sequence, LargeRJetFindingAlg):
+      logger.info("Algorithm already exists in the input sequence. Will not "
+          "add again")
+    else:
+      logger.info("Adding algorithm into the sequence {0}".format(sequence) )
+      sequence += JetCommon.DFJetAlgs[LargeRJetFindingAlg]
+  else:
+    # Check to see if the corresponding JetRecTool already exists
+    if hasattr(jtm, LargeRJets):
+      logger.info("JetRecTool {0} already exists in jtm".format(LargeRJets) )
+    else:
+      logger.info("Create a new JetRecTool {0}".format(LargeRJets) )
+      JetCommon.OutputJets.setdefault("CustomJets", []).append(LargeRJets)
+      originalModifiers = [jtm[m.getName()] for m in originalJetRecTool.JetModifiers]
+      jtm.addJetFinder(
+          output = LargeRJets,
+          alg = originalFinder.JetAlgorithm,
+          radius = originalFinder.JetRadius,
+          gettersin = originalGetters + newGetters,
+          modifiersin = originalModifiers,
+          ghostArea = 0,
+          ptmin = originalFinder.PtMin,
+          variableRMinRadius = originalFinder.VariableRMinRadius,
+          variableRMassScale = originalFinder.VariableRMassScale,
+          calibOpt = "none")
+      # Note that we don't need ptminFilter as this was included in the original
+      # list of JetModifiers
+    logger.info(
+        "Creating new jet algorithm {0} and adding it to sequence {1}".format(
+          LargeRJetFindingAlg, sequence) )
+    theJetAlg = JetAlgorithm(LargeRJetFindingAlg, Tools = [jtm[LargeRJets] ])
+    sequence += theJetAlg
+    JetCommon.DFJetAlgs[LargeRJetFindingAlg] = theJetAlg
+
+  # Everything so far has been to create the links on a copy of the ungroomed
+  # collection. Now we need to copy those links over to the original ungroomed
+  # collection.
+
+  assocTool = ExtendedJetCommon.getJetExternalAssocTool(
+      ungroomedJetAlg,
+      LargeRJetPrefix,
+      MomentPrefix = '',
+      ListOfOldLinkNames=[g.Label for g in newGetters]
+      )
+  ExtendedJetCommon.applyJetAugmentation(
+      ungroomedJetAlg, LinkTransferAlg, sequence, assocTool)
+
+  return originalUngroomedName, [g.Label for g in newGetters]
+
+""" Not to be used!!
+def addPseudoJetGettersToNewJetCollection(
     collection, getters, addToParent=True):
   # First, get the jet rec tool from jtm. The JetRec functions appear to always
   # add this tool using the collection name so retrieve it this way
@@ -47,3 +133,4 @@ def addPseudojetgettersToJetCollection(
   if locked:
     logger.debug("Re-locking tool {0}".format(jetRecTool.getFullName() ) )
     jetRecTool.lock()
+"""
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/share/EXOT27.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/share/EXOT27.py
@@ -1,10 +1,10 @@
 ################################################################################
-# EXOT27 - MonoHbb tests (private only!)
+# EXOT27 - MonoHbb tests - candidate for official monoHbb+monoScalar derviation
 ################################################################################
 
 from DerivationFrameworkCore.DerivationFrameworkMaster import *
 from DerivationFrameworkCore.DerivationFrameworkCoreConf import DerivationFramework__DerivationKernel
-from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool, DerivationFramework__TriggerSkimmingTool
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool, DerivationFramework__TriggerSkimmingTool, DerivationFramework__GenericObjectThinning
 from TriggerMenu.api.TriggerAPI import TriggerAPI
 from TriggerMenu.api.TriggerEnums import TriggerPeriod, TriggerType
 from DerivationFrameworkCore.ThinningHelper import ThinningHelper
@@ -14,18 +14,23 @@ from DerivationFrameworkInDet.DerivationFrameworkInDetConf import DerivationFram
 # CP group common variables
 import DerivationFrameworkJetEtMiss.JetCommon as JetCommon
 import DerivationFrameworkJetEtMiss.ExtendedJetCommon as ExtendedJetCommon
-# import DerivationFrameworkJetEtMiss.PFlowJetCommon as PFlowJetCommon
 import DerivationFrameworkJetEtMiss.METCommon as METCommon
 import DerivationFrameworkEGamma.EGammaCommon as EGammaCommon
 import DerivationFrameworkMuons.MuonsCommon as MuonsCommon
 import DerivationFrameworkFlavourTag.HbbCommon as HbbCommon
 from JetRec.JetRecStandardToolManager import jtm
+from DerivationFrameworkMCTruth.DerivationFrameworkMCTruthConf import DerivationFramework__GenericTruthThinning
+import DerivationFrameworkMCTruth.MCTruthCommon as MCTruthCommon
+from BTagging.BTaggingFlags import BTaggingFlags
+from DerivationFrameworkCore.FullListOfSmartContainers import FullListOfSmartContainers
 
 import DerivationFrameworkExotics.EXOT27Utils as EXOT27Utils
 
 # Create a logger for this stream
 import AthenaCommon
 logger = AthenaCommon.Logging.logging.getLogger("EXOT27")
+
+from collections import defaultdict
 
 ################################################################################
 # Setup the stream and our private sequence
@@ -40,6 +45,37 @@ EXOT27Seq = CfgMgr.AthSequencer("EXOT27Sequence")
 DerivationFrameworkJob += EXOT27Seq
 # As we create skimming algs, etc add them to this list
 EXOT27AcceptAlgs = []
+# As we add extra variables add them to this dictionary (key is container name,
+# aux data in the value)
+EXOT27ExtraVariables = defaultdict(set)
+# As we add extra containers add them to these lists (note that the jets have
+# their own way of doing this so anything EXOT27Jets is treated differently)
+EXOT27SmartContainers = [
+  "Electrons", "Photons", "AntiKt4EMTopoJets", "TauJets", "Muons",
+  "InDetTrackParticles", "PrimaryVertices", "BTagging_AntiKt4EMTopo",
+  ]
+EXOT27AllVariables = [
+  "MET_Core_AntiKt4EMTopo",
+  "METAssoc_AntiKt4EMTopo",
+  ]
+if DerivationFrameworkIsMonteCarlo:
+  EXOT27AllVariables += [
+    "TruthParticles",
+    "MET_Truth",
+    ]
+def outputContainer(container, warnIfNotSmart=True):
+  if container in EXOT27SmartContainers + EXOT27AllVariables:
+    logger.debug("Container '{0}' already requested for output!")
+    return
+  if container in FullListOfSmartContainers:
+    EXOT27SmartContainers.append(container)
+  else:
+    if warnIfNotSmart:
+      logger.warning(
+          "Container '{0}' added but not in smart lists!".format(container) )
+      logger.warning("Will be added to AllVariables - could inflate the size!")
+    EXOT27AllVariables.append(container)
+
 
 ################################################################################
 # Setup preliminary skimming (remove whole events)
@@ -82,16 +118,15 @@ EXOT27AcceptAlgs.append("EXOT27PreliminaryKernel")
 # Create the new jet sequence in JetCommon (if it doesn't already exist)
 JetCommon.OutputJets.setdefault("EXOT27Jets", [])
 # Create the VR track jets
-# The 'addVRJets' function creates the track jets and then ghost associates them
-# to the AntiKt10LCTopo jets. Two issues with this - one, it doesn't give us the
-# ghosts for the VR jets back and two, we don't actually want to write out the
-# AntiKt10LCTopo jets so this is a waste of time to run...
-# Therefore instead use the buildVRJets function directly. Note that this has a
-# do_ghost parameter and I'm not sure what it does - it doesn't control the
-# creation of the ghosts for the VR jets...
-vr_track_jets, vr_track_jets_ghosts = HbbCommon.buildVRJets(
+# Use buildVRJets instead of addVRJets so we can manually control the ghost
+# association to the large-R jets
+# do_ghost is ghost *tagging* - future improvement, not yet calibrated
+vrTrackJets, vrTrackJetGhosts = HbbCommon.buildVRJets(
     sequence = EXOT27Seq, do_ghost = False, logger = logger)
-JetCommon.OutputJets["EXOT27Jets"].append(vr_track_jets)
+JetCommon.OutputJets["EXOT27Jets"].append(vrTrackJets)
+vrGhostTagTrackJets, vrGhostTagTrackJetsGhosts = HbbCommon.buildVRJets(
+    sequence = EXOT27Seq, do_ghost = True, logger = logger)
+JetCommon.OutputJets["EXOT27Jets"].append(vrGhostTagTrackJets)
 
 # I don't know if we actually care about the track jets any more but I'm adding
 # them in for now. Same goes for the truth jets
@@ -114,19 +149,51 @@ ExtendedJetCommon.addDefaultTrimmedJets(EXOT27Seq, "EXOT27Jets")
 HbbCommon.addVRCaloJets(EXOT27Seq, "EXOT27Jets")
 
 # Add the default soft drop collection
+# Note that this function is a little eager - builds the jets with the VR ghost
+# tag jets already in place!
 ExtendedJetCommon.addCSSKSoftDropJets(EXOT27Seq, "EXOT27Jets")
 
-to_be_associated_to = [
+# Add the R-scan jets
+JetCommon.addStandardJets(
+    "AntiKt", 0.2, "LCTopo", mods="lctopo_ungroomed", calibOpt="none",
+    ghostArea=0.1, ptmin=2000, ptminFilter=7000,
+    algseq=EXOT27Seq, outputGroup="EXOT27Jets")
+
+# Ghost-associated the track jets to these large-R jets
+toBeAssociatedTo = [
   "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets",
   "AntiKtVR600Rmax10Rmin2LCTopoTrimmedPtFrac5SmallR20Jets",
-  "AntiKt10LCTopoCSSKSoftDropBeta100Zcut10Jets"
+  "AntiKt2LCTopoJets"
   ]
-to_associate = [
-  vr_track_jets_ghosts
-  ]
-    
-for collection in to_be_associated_to:
-  EXOT27Utils.addPseudojetgettersToJetCollection(collection, to_associate)
+toAssociate = {
+  vrTrackJetGhosts : vrTrackJetGhosts.lower(),
+  vrGhostTagTrackJetsGhosts : vrGhostTagTrackJetsGhosts.lower()
+}
+for collection in toBeAssociatedTo:
+  ungroomed, labels = EXOT27Utils.linkPseudoJetGettersToExistingJetCollection(
+      EXOT27Seq, collection, toAssociate)
+  EXOT27ExtraVariables[ungroomed].update(labels)
+# Link the SoftDrop jets *only* to the non-ghost tag jets. The other links
+# already exist and will cause a crash if scheduled again
+
+ungroomed, labels = EXOT27Utils.linkPseudoJetGettersToExistingJetCollection(
+    EXOT27Seq, "AntiKt10LCTopoCSSKSoftDropBeta100Zcut10Jets",
+    {vrTrackJetGhosts : vrTrackJetGhosts.lower()})
+# However, we still want to write out both so add both to the extra variables
+EXOT27ExtraVariables[ungroomed].update(labels)
+EXOT27ExtraVariables[ungroomed].update(vrGhostTagTrackJetsGhosts)
+
+# Alias b-tagging container for VR track jets
+BTaggingFlags.CalibrationChannelAliases += ["AntiKtVR30Rmax4Rmin02Track->AntiKtVR30Rmax4Rmin02Track,AntiKt4EMTopo"]
+# Schedule for output
+outputContainer("BTagging_AntiKtVR30Rmax4Rmin02Track")
+
+# Add in Xbb tagging variables
+HbbCommon.addRecommendedXbbTaggers(EXOT27Seq, ToolSvc, logger=logger)
+# Add the new variables too
+for extra in HbbCommon.xbbTaggerExtraVariables:
+  partition = extra.partition('.')
+  EXOT27ExtraVariables[partition[0]].update(partition[2].split('.') )
 
 ################################################################################
 # Setup thinning (remove objects from collections)
@@ -137,44 +204,69 @@ EXOT27ThinningHelper.TriggerChains = "|".join(trigger_list)
 EXOT27ThinningHelper.AppendToStream(EXOT27Stream)
 EXOT27ThinningTools = []
 
+# Apply a pt cut on output large r jet collections
+# TODO - revisit if this is harmful/necessary
+for large_r in [
+  "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets",
+  "AntiKtVR600Rmax10Rmin2LCTopoTrimmedPtFrac5SmallR20Jets",
+  "AntiKt10LCTopoCSSKSoftDropBeta100Zcut10Jets"
+  ]:
+  EXOT27ThinningTools.append(DerivationFramework__GenericObjectThinning(
+        "{0}ThinningTool".format(large_r),
+        SelectionString="{0}.pt > 100*GeV".format(large_r),
+        ContainerName = large_r) )
+
+
 # Set up the standard set of track thinning tools
 EXOT27ThinningTools += [
   DerivationFramework__EgammaTrackParticleThinning(
       "EXOT27ElectronTrackParticleThinningTool",
       ThinningService = EXOT27ThinningHelper.ThinningSvc(),
       SGKey           = "Electrons",
-      SelectionString = "Electrons.pt > 10*GeV"
-      # SelectionString = "Electrons.pt > 10*GeV && Electrons.DFCommonElectronsLooseBL"
+      # SelectionString = "Electrons.pt > 10*GeV && Electrons.DFCommonElectronsLHLooseBL"
       ),
   DerivationFramework__EgammaTrackParticleThinning(
       "EXOT27PhotonTrackParticleThinningTool",
       ThinningService = EXOT27ThinningHelper.ThinningSvc(),
       SGKey           = "Photons",
-      SelectionString = "Photons.pt > 10*GeV && Photons.DFCommonPhotonsIsEMTight"
+      # SelectionString = "Photons.pt > 10*GeV && Photons.DFCommonPhotonsIsEMTight"
       ),
   DerivationFramework__MuonTrackParticleThinning(
       "EXOT27MuonTrackParticleThinningTool",
       ThinningService = EXOT27ThinningHelper.ThinningSvc(),
       MuonKey         = "Muons",
-      SelectionString = "Muons.pt > 10*GeV && Muons.DFCommonGoodMuon && Muons.DFCommonMuonsPreselection"
+      # SelectionString = "Muons.pt > 10*GeV && Muons.DFCommonGoodMuon && Muons.DFCommonMuonsPreselection"
       ),
   DerivationFramework__TauTrackParticleThinning(
       "EXOT27TauTrackParticleThinningTool",
       ThinningService = EXOT27ThinningHelper.ThinningSvc(),
       TauKey          = "TauJets",
-      SelectionString = "TauJets.pt > 10*GeV"
+      # SelectionString = "TauJets.pt > 10*GeV"
       ),
   DerivationFramework__JetTrackParticleThinning(
       "EXOT27JetTrackParticleThinningTool",
       ThinningService = EXOT27ThinningHelper.ThinningSvc(),
       JetKey          = "AntiKt4EMTopoJets",
-      SelectionString = "AntiKt4EMTopoJets.DFCommonJets_Calib_pt > 10*GeV"
-      )
+      SelectionString = "AntiKt4EMTopoJets.DFCommonJets_Calib_pt > 15*GeV"
+      ),
   ]
 
-ToolSvc += EXOT27ThinningTools
-
 # TODO (perhaps): truth thinning
+# What I have here is extremely simplistic - designed to at least have what I
+# need for my immediate studies
+if DerivationFrameworkIsMonteCarlo:
+  truth_sel_list = ["abs(TruthParticles.pdgId) == {0}".format(ii) for ii in [5,6, 23, 24, 25]]
+  truth_sel = "||".join(map("({0})".format, truth_sel_list) )
+  EXOT27ThinningTools += [
+    DerivationFramework__GenericTruthThinning(
+        "EXOT27TruthThinningTool",
+        ThinningService = EXOT27ThinningHelper.ThinningSvc(),
+        ParticleSelectionString = truth_sel,
+        PreserveDescendants     = True),
+    ]
+  
+for tool in EXOT27ThinningTools:
+  ToolSvc += tool
 
 
 ################################################################################
@@ -200,34 +292,45 @@ EXOT27Seq += CfgMgr.DerivationFramework__DerivationKernel(
     SkimmingTools = EXOT27SkimmingTools,
     ThinningTools = EXOT27ThinningTools
     )
+
 EXOT27AcceptAlgs.append("EXOT27SecondaryKernel")
+
+
 
 ################################################################################
 # Setup slimming (remove variables/collections)
 ################################################################################
 EXOT27SlimmingHelper = SlimmingHelper("EXOT27SlimmingHelper")
-EXOT27SlimmingHelper.SmartCollections = [
-  "Electrons",
-  "Photons",
-  "AntiKt4EMTopoJets",
-  "TauJets",
-  "Muons",
-  "InDetTrackParticles",
-  "PrimaryVertices",
-  "BTagging_AntiKt4EMTopo"
-  ]
-EXOT27SlimmingHelper.AllVariables = [
-  "MET_Core_AntiKt4EMTopo",
-  "METAssoc_AntiKt4EMTopo",
-  "TruthParticles",
-  "TruthLabelBQuarksFinal",
-  vr_track_jets
-  ]
+EXOT27SlimmingHelper.SmartCollections += EXOT27SmartContainers
+EXOT27SlimmingHelper.AllVariables += EXOT27AllVariables
 JetCommon.addJetOutputs(
     slimhelper = EXOT27SlimmingHelper,
     contentlist=["EXOT27Jets"],
-    smartlist = ["AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets"]
+    smartlist = [
+      "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets",
+      "AntiKt2LCTopoJets",
+      "AntiKt10LCTopoCSSKSoftDropBeta100Zcut10Jets"
+    ],
+    vetolist = [
+    "AntiKt10LCTopoJets",
+    "AntiKt10TruthJets",
+    "AntiKtVR600Rmax10Rmin2LCTopoJets",
+    "AntiKtVR600Rmax10Rmin2Pv0TrackTrimmedPtFrac5SmallR20Jets",
+    "AntiKtVR600Rmax10Rmin2TruthTrimmedPtFrac5SmallR20Jets",
+    "AntiKt10LCTopoCSSKJets"]
     )
+
+EXOT27SlimmingHelper.ExtraVariables += [
+  "{0}.{1}".format(k, '.'.join(v) ) for k, v in EXOT27ExtraVariables.iteritems()
+]
+EXOT27SlimmingHelper.AppendToDictionary.update( {
+    "BTagging_AntiKtVR30Rmax4Rmin02Track"    : "xAOD::BTaggingContainer",
+    "BTagging_AntiKtVR30Rmax4Rmin02TrackAux" : "xAOD::BTaggingAuxContainer",
+    } )
+
+EXOT27SlimmingHelper.IncludeMuonTriggerContent = True
+EXOT27SlimmingHelper.IncludeEGammaTriggerContent = True
+EXOT27SlimmingHelper.InlcudeEtMissTriggerContent = True
 EXOT27SlimmingHelper.AppendContentToStream(EXOT27Stream)
 
 ################################################################################
