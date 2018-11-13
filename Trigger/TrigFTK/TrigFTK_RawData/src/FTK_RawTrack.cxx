ed800e3df62fa9e225a78f6ec780c58e35f4dc4b
John Baines <john.baines@cern.ch>
2018-11-13 18:06:37 +0100

    fix to setInvPt method to store  1/(2pt) in dataword
    Former-commit-id: 0c24793522a9608536c38f7ab079c09d7ab853e6

--- Trigger/TrigFTK/TrigFTK_RawData/src/FTK_RawTrack.cxx
+++ Trigger/TrigFTK/TrigFTK_RawData/src/FTK_RawTrack.cxx
@@ -305,7 +305,7 @@ void FTK_RawTrack::setPhi(float track_phi){
 
 void FTK_RawTrack::setInvPt(float track_invpt){
 
-  uint16_t invpt = Eigen::half(track_invpt).x;
+  uint16_t invpt = Eigen::half(0.5*track_invpt).x;// 1/(2pt) is stored in dataword from version 2 onwards
 
   m_word_th6 = invpt | m_word_th6;
   return;
