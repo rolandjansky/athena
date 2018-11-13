5571bb5004babacf0bf2f4dad34944cce6549e95
Petr Jacka <petr.jacka@cern.ch>
2018-11-13 13:21:28 +0100

    Added Track jet cleaning selection for variable-R track jets
    Former-commit-id: c8a805c85e8be141158f524384f2ae8c90d1dc99

---
+++ PhysicsAnalysis/TopPhys/xAOD/TopEventSelectionTools/Root/TrackJetCleaningSelector.cxx
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "TopEventSelectionTools/TrackJetCleaningSelector.h"
+#include "TopEvent/EventTools.h"
+#include "xAODJet/JetContainer.h"
+#include "TopConfiguration/TopConfig.h"
+
+namespace top {
+
+  TrackJetCleaningSelector::TrackJetCleaningSelector(std::shared_ptr<top::TopConfig> config) :
+    m_config(config)
+  {
+    m_pt_signal=10e3;
+    m_pt_baseline=5e3;
+    
+  }
+
+  bool TrackJetCleaningSelector::apply(const top::Event& event) const {
+    
+    //std::cout << "Applying TrackJetCleaningSelector" << std::endl;
+
+    // Selection is applied only if Variable-R track jets are used
+    if (m_config->sgKeyTrackJets() == "AntiKtVR30Rmax4Rmin02TrackJets") {
+      static SG::AuxElement::Accessor<char> acc_passDRcut("passDRcut");
+      for (const auto& jet : event.m_trackJets) {
+        top::check(acc_passDRcut.isAvailable(*jet),"passDRcut info not found in variable-R track jet. Needed for cleaning algorithm.");
+	if(!acc_passDRcut(*jet) ) return false;
+      }
+    }
+    return true;
+  }
+
+  std::string TrackJetCleaningSelector::name() const 
+  {
+    return "TRACKJETCLEAN";
+  }
+
+}
