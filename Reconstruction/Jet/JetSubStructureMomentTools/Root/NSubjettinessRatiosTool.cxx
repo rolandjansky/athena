431225717c5a979aa046e3c98d517ece5c716d10
pbutti <pierfrancesco.butti@cern.ch>
2018-11-14 18:49:54 +0100

    JetSubStructureMomentTools update. NSubjettinessRatioTools can now pick up the correct decorations prefixes.
    Former-commit-id: 829591530e873b7e794d67a02b1f5ae8252f5e1a

--- Reconstruction/Jet/JetSubStructureMomentTools/Root/NSubjettinessRatiosTool.cxx
+++ Reconstruction/Jet/JetSubStructureMomentTools/Root/NSubjettinessRatiosTool.cxx
@@ -13,33 +13,45 @@ NSubjettinessRatiosTool::NSubjettinessRatiosTool(std::string name) :
 }
 
 int NSubjettinessRatiosTool::modifyJet(xAOD::Jet &jet) const {
+  
+  if (!jet.isAvailable<float>(m_prefix+"Tau1") || 
+      !jet.isAvailable<float>(m_prefix+"Tau2") ||
+      !jet.isAvailable<float>(m_prefix+"Tau3") ||
+      !jet.isAvailable<float>(m_prefix+"Tau1_wta") || 
+      !jet.isAvailable<float>(m_prefix+"Tau2_wta") ||
+      !jet.isAvailable<float>(m_prefix+"Tau3_wta")) {
+    
+    ATH_MSG_WARNING("Tau decorations for "<<m_prefix<<"are not all available. Exiting..");
+    return 1;
+  }
+  
   // Regular
-  float tau1 = jet.getAttribute<float>("Tau1");
-  float tau2 = jet.getAttribute<float>("Tau2");
-  float tau3 = jet.getAttribute<float>("Tau3");
+  float tau1 = jet.getAttribute<float>(m_prefix+"Tau1");
+  float tau2 = jet.getAttribute<float>(m_prefix+"Tau2");
+  float tau3 = jet.getAttribute<float>(m_prefix+"Tau3");
 
   if(fabs(tau1) > 1e-8) // Prevent div-0
-    jet.setAttribute("Tau21", tau2/tau1);
+    jet.setAttribute(m_prefix+"Tau21", tau2/tau1);
   else
-    jet.setAttribute("Tau21", -999.0);
+    jet.setAttribute(m_prefix+"Tau21", -999.0);
   if(fabs(tau2) > 1e-8) // Prevent div-0
-    jet.setAttribute("Tau32", tau3/tau2);
+    jet.setAttribute(m_prefix+"Tau32", tau3/tau2);
   else
-    jet.setAttribute("Tau32", -999.0);
-
-  float tau1_wta = jet.getAttribute<float>("Tau1_wta");
-  float tau2_wta = jet.getAttribute<float>("Tau2_wta");
-  float tau3_wta = jet.getAttribute<float>("Tau3_wta");
-
+    jet.setAttribute(m_prefix+"Tau32", -999.0);
+  
+  float tau1_wta = jet.getAttribute<float>(m_prefix+"Tau1_wta");
+  float tau2_wta = jet.getAttribute<float>(m_prefix+"Tau2_wta");
+  float tau3_wta = jet.getAttribute<float>(m_prefix+"Tau3_wta");
+  
   // WTA
   if(fabs(tau1_wta) > 1e-8) // Prevent div-0
-    jet.setAttribute("Tau21_wta", tau2_wta/tau1_wta);
+    jet.setAttribute(m_prefix+"Tau21_wta", tau2_wta/tau1_wta);
   else
-    jet.setAttribute("Tau21_wta", -999.0);
+    jet.setAttribute(m_prefix+"Tau21_wta", -999.0);
   if(fabs(tau2_wta) > 1e-8) // Prevent div-0
-    jet.setAttribute("Tau32_wta", tau3_wta/tau2_wta);
+    jet.setAttribute(m_prefix+"Tau32_wta", tau3_wta/tau2_wta);
   else
-    jet.setAttribute("Tau32_wta", -999.0);
-
+    jet.setAttribute(m_prefix+"Tau32_wta", -999.0);
+  
   return 0;
 }
