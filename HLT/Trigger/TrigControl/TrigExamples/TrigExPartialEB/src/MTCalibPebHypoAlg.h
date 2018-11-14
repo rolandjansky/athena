e9a9cbdc4d12b610a3d53ee59dfe39092d7b2177
Rafal Bielski <rafal.bielski@cern.ch>
2018-11-14 15:01:39 +0000

    Implement MTCalibPeb functionality in the new framework
    Former-commit-id: 92b5986dd031487cca6929df22b671e7f83a17f7

---
+++ HLT/Trigger/TrigControl/TrigExamples/TrigExPartialEB/src/MTCalibPebHypoAlg.h
@@ -0,0 +1,30 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef TRIGEXPARTIALEB_MTCALIBPEBHYPOALG_H
+#define TRIGEXPARTIALEB_MTCALIBPEBHYPOALG_H
+
+// Trigger includes
+#include "MTCalibPebHypoTool.h"
+#include "DecisionHandling/HypoBase.h"
+
+/** @class MTCalibPebHypoAlg
+ *  @brief Hypo algorithm for framework testing
+ **/
+class MTCalibPebHypoAlg : public HypoBase {
+public:
+  /// Standard constructor
+  MTCalibPebHypoAlg(const std::string& name, ISvcLocator* svcLoc);
+  /// Standard destructor
+  virtual ~MTCalibPebHypoAlg();
+
+  // ------------------------- AthReentrantAlgorithm methods -------------------
+  virtual StatusCode initialize() override;
+  virtual StatusCode finalize() override;
+  virtual StatusCode execute_r(const EventContext& eventContext) const override;
+private:
+  ToolHandleArray<MTCalibPebHypoTool> m_hypoTools {this, "HypoTools", {}, "Tools to perform selection"};
+};
+
+#endif // TRIGEXPARTIALEB_MTCALIBPEBHYPOALG_H
