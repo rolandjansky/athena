c75fcc040a0da3afd16d69501ce59db8de968d7f
scott snyder <scott.snyder@cern.ch>
2018-11-15 21:04:27 +0100

    AtlasSTLAddReflex: Tweak selection rules for gcc 8.2.1.
    In gcc 8.2.1, std::pair derives from std::__pair_base.
    Tweak selection rules so that we generate dictionaries for both, if present.
    Avoids root streamer warnings about missing dictionaries.
    
    
    Former-commit-id: c90342448198e27890dc0684159714e9c90d9b99

--- Database/AtlasSTLAddReflex/AtlasSTLAddReflex/AtlasSTLAddReflexDict.h
+++ Database/AtlasSTLAddReflex/AtlasSTLAddReflex/AtlasSTLAddReflexDict.h
@@ -1,7 +1,7 @@
 // dear emacs, this is -*- C++ -*-
 
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 // Some common classes for general use in ATLAS
@@ -17,4 +17,10 @@
 #include <boost/array.hpp>
 
 
-
+template struct std::pair<unsigned int, unsigned int>;
+template struct std::pair<std::string, unsigned int>;
+template struct std::pair<std::string, std::vector<int> >;
+template struct std::pair<std::string, std::vector<double> >;
+template struct std::pair<std::pair<std::string, unsigned int>, std::pair<std::string, unsigned int> >;
+template struct std::pair<unsigned int, std::vector<float> >;
+template struct std::pair<unsigned int, std::vector<unsigned int> >;
