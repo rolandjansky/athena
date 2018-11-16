69348a66cebcd928d213511ebf75e7b38b050e3b
Scott Snyder <scott.snyder@cern.ch>
2018-11-16 15:34:53 +0000

    Removing QcdD3PDMaker.
    Former-commit-id: 2680672fa833ef90938dae80d831a14647b3e005

--- PhysicsAnalysis/D3PDMaker/QcdD3PDMaker/share/QcdD3PDMakerPreSetupJETMET.py
+++
@@ -1,7 +0,0 @@
-#==================================
-# Common Settings
-#==================================
-
-from RecExConfig.RecFlags import rec
-rec.UserAlgs+=['QcdD3PDMaker/QcdD3PD_preSetup.py']
-include('QcdD3PDMaker/QcdD3PDMakerPreSetup.common.py')
