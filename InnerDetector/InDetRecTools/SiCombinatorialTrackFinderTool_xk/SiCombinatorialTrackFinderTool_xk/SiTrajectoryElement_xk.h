1729a71fd5695102f385fc425f6c4aa3b8e0a2d4
Matthew Gignac <matthew.gignac@cern.ch>
2018-11-18 07:53:26 -0800

    proper init and copy constructor for m_pixIdHelper and m_sctIdHelper
    Former-commit-id: fd6d68b29f0890e623c40d724a6b0f27ee3e5649

--- InnerDetector/InDetRecTools/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinderTool_xk/SiTrajectoryElement_xk.h
+++ InnerDetector/InDetRecTools/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinderTool_xk/SiTrajectoryElement_xk.h
@@ -24,9 +24,6 @@
 #include "SiCombinatorialTrackFinderTool_xk/SiClusterLink_xk.h"
 #include "SiCombinatorialTrackFinderTool_xk/SiDetElementBoundaryLink_xk.h"
 
-class SCT_ID;
-class PixelID;
-
 namespace InDet{
 
   class SiTrajectoryElement_xk
@@ -335,8 +332,8 @@ namespace InDet{
       const InDetDD::SiDetectorElement*           m_detelement  ;
       const InDet::SiDetElementBoundaryLink_xk*   m_detlink     ;
       const Trk::Surface*                         m_surface     ;
-      const SCT_ID*                               m_sctID       ;
-      const PixelID*                              m_pixID       ;
+      const PixelID*                              m_pixIdHelper ;
+      const SCT_ID*                               m_sctIdHelper ;
       InDet::SiClusterCollection ::const_iterator m_sibegin     ;
       InDet::SiClusterCollection ::const_iterator m_siend       ;
       const InDet::SiCluster*                     m_cluster     ;
@@ -404,6 +401,8 @@ namespace InDet{
       m_detelement  = 0 ; 
       m_detlink     = 0 ;
       m_surface     = 0 ;
+      m_pixIdHelper = 0 ;
+      m_sctIdHelper = 0 ;
       m_cluster     = 0 ;
       m_clusterOld  = 0 ;
       m_clusterNoAdd= 0 ;
@@ -446,6 +445,8 @@ namespace InDet{
       m_detelement   = E.m_detelement  ;
       m_detlink      = E.m_detlink     ;
       m_surface      = E.m_surface     ;
+      m_pixIdHelper  = E.m_pixIdHelper ;
+      m_sctIdHelper  = E.m_sctIdHelper ;
       m_sibegin      = E.m_sibegin     ;
       m_siend        = E.m_siend       ; 
       m_cluster      = E.m_cluster     ;
