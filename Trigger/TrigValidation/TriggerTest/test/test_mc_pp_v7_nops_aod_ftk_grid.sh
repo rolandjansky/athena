eeceb2706cda5e792dd02d0d2695dcdfa86e83a7
Julie Kirk <Julie.Kirk@cern.ch>
2018-11-19 16:49:43 +0100

    modified:   Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v7_nops_aod_ftk_grid.sh
    Former-commit-id: 5c315a4882e3b070642db55067e3043a14f2a8ef

--- Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v7_nops_aod_ftk_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v7_nops_aod_ftk_grid.sh
@@ -22,7 +22,7 @@
 # art-output: TotalEventsProcessed.txt
 
 export NAME="mc_pp_v7_nops_aod_ftk_grid"
-export MENU="MC_pp_v7_no_prescale"
+export MENU="Physics_pp_v7_primaries"
 export INPUT="ftk"
 export JOBOPTIONS="TriggerTest/testCommonSliceAthenaTrigRDOtoAOD.py"
 export EXTRA="from TriggerJobOpts.TriggerFlags import TriggerFlags;TriggerFlags.doFTK=True;from RecExConfig.RecFlags import rec;rec.doFTK.set_Value_and_Lock(True);"
