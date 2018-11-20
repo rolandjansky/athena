17b7d0ffe74c80a93ef53a644405e7dca428549e
Soshi Tsuno <soshi.tsuno@cern.ch>
2018-11-20 13:34:39 +0000

    Pixel Dead Map conditions migration (ATLASRECTS-4048)
    Former-commit-id: 27eaffd56cb4250420387a61bb0866e31c2d95b1

--- InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/PixelSiliconConditionsTestAlg.cxx
+++ InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/PixelSiliconConditionsTestAlg.cxx
@@ -17,6 +17,7 @@ StatusCode PixelSiliconConditionsTestAlg::initialize()
   ATH_MSG_INFO("Calling initialize");
 
   ATH_CHECK(m_siliconTool.retrieve());
+  ATH_CHECK(m_condKey.initialize());
   return StatusCode::SUCCESS;
 }
 
@@ -24,9 +25,18 @@ StatusCode PixelSiliconConditionsTestAlg::execute(){
   //This method is only used to test the summary service, and only used within this package,
   // so the INFO level messages have no impact on performance of these services when used by clients
 
+//  for (int i=0; i<2048; i++) {
+//    std::cout << "Hash ID=" << IdentifierHash(i) << " Bias Voltage=" << m_siliconTool->biasVoltage(IdentifierHash(i)) << " Temperature=" << m_siliconTool->temperature(IdentifierHash(i)) << " Depletion V=" << m_siliconTool->depletionVoltage(IdentifierHash(i)) << " FSM state=" << m_siliconTool->PixelFSMState(IdentifierHash(i)) << " FSM status=" << m_siliconTool->PixelFSMStatus(IdentifierHash(i)) << " ModuleStatus=" << m_deadmapTool->ModuleStatus(IdentifierHash(i)) << " ChipStatus=" << m_deadmapTool->ChipStatus(IdentifierHash(i)) << " TDAQModuleStatus=" << m_tdaqTool->ModuleStatus(IdentifierHash(i)) << std::endl;
+//  }
+
   for (int i=0; i<2048; i++) {
-    std::cout << "Hash ID=" << IdentifierHash(i) << " Bias Voltage=" << m_siliconTool->biasVoltage(IdentifierHash(i)) << " Temperature=" << m_siliconTool->temperature(IdentifierHash(i)) << " Depletion V=" << m_siliconTool->depletionVoltage(IdentifierHash(i)) << " FSM state=" << m_siliconTool->PixelFSMState(IdentifierHash(i)) << " FSM status=" << m_siliconTool->PixelFSMStatus(IdentifierHash(i)) <<  std::endl;
+    std::cout << "Hash ID=" << IdentifierHash(i) 
+              << " ModuleStatus=" << SG::ReadCondHandle<PixelModuleData>(m_condKey)->getModuleStatus(IdentifierHash(i))
+              << " ChipStatus=" << SG::ReadCondHandle<PixelModuleData>(m_condKey)->getChipStatus(IdentifierHash(i))
+              << " TDAQModuleStatus=" << SG::ReadCondHandle<PixelModuleData>(m_condKey)->getTDAQModuleStatus(IdentifierHash(i))
+              << std::endl;
   }
+
 
   return StatusCode::SUCCESS;
 }
--- InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/components/PixelConditionsAlgorithms_entries.cxx
+++ InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/components/PixelConditionsAlgorithms_entries.cxx
@@ -1,11 +1,15 @@
 #include "../PixelDCSCondHVAlg.h"
 #include "../PixelDCSCondTempAlg.h"
 #include "../PixelDCSCondStateAlg.h"
+#include "../PixelDeadMapCondAlg.h"
+#include "../PixelTDAQCondAlg.h"
 #include "../PixelSiliconConditionsTestAlg.h"
 #include "../SpecialPixelMapCondAlg.h"
 
 DECLARE_COMPONENT( PixelDCSCondHVAlg )
 DECLARE_COMPONENT( PixelDCSCondTempAlg )
 DECLARE_COMPONENT( PixelDCSCondStateAlg )
+DECLARE_COMPONENT( PixelDeadMapCondAlg )
+DECLARE_COMPONENT( PixelTDAQCondAlg )
 DECLARE_COMPONENT( PixelSiliconConditionsTestAlg )
 DECLARE_COMPONENT( SpecialPixelMapCondAlg )
