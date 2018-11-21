008c8caa53c8c3ce398215541fab00b12458f517
Julie Kirk <Julie.Kirk@cern.ch>
2018-11-21 10:10:03 +0100

    Updates to ART tests - add regtest files to output
    Update to trigger ART tests so that the file athena.regtest is copied to the output directory. This is needed for updating references and debugging problems Effectively a CP of MR!16070 to master (automatic CP failed)
    
    
    Former-commit-id: c59e64b471e827eedfb8e519fc88fa91d5dd96a2

--- Trigger/TrigValidation/TriggerTest/test/test_slice_l1_build.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_slice_l1_build.sh
@@ -20,6 +20,7 @@
 # art-output: ntuple.pmon.gz
 # art-output: *perfmon*
 # art-output: TotalEventsProcessed.txt
+# art-output: *.regtest
 
 export NAME="slice_l1_build"
 export EVENTS="10"
--- Trigger/TrigValidation/TriggerTest/test/test_slice_l1_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_slice_l1_grid.sh
@@ -20,6 +20,7 @@
 # art-output: ntuple.pmon.gz
 # art-output: *perfmon*
 # art-output: TotalEventsProcessed.txt
+# art-output: *.regtest
 
 export NAME="slice_l1_grid"
 export JOBOPTIONS="TriggerTest/testLVL1CTPAthenaTrigRDO.py"
