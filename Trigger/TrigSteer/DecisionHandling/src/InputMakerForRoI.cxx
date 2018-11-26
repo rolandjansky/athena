488f6ac69a5686ee3c46258adbf7198194289b27
Francesca Pastore <francesca.pastore@cern.ch>
2018-11-26 11:13:57 +0100

    fix comments by L1 shifter
    Former-commit-id: 6f33ab02d8d73c42c0115dd36d733e21b259d2f6

--- Trigger/TrigSteer/DecisionHandling/src/InputMakerForRoI.cxx
+++ Trigger/TrigSteer/DecisionHandling/src/InputMakerForRoI.cxx
@@ -1,8 +1,7 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-// TrigUpgradeTest includes
 #include "InputMakerForRoI.h"
 #include <xAODTrigger/TrigCompositeAuxContainer.h>
 #include <TrigSteeringEvent/TrigRoiDescriptorCollection.h>
--- Trigger/TrigSteer/L1Decoder/share/testL1Decoder.py
+++ Trigger/TrigSteer/L1Decoder/share/testL1Decoder.py
@@ -10,15 +10,7 @@ for unpack in topSequence.L1DecoderTest.roiUnpackers:
     unpack.OutputLevel=DEBUG
     print unpack
     
-    
-#topSequence.L1DecoderTest.roiUnpackers[3].OutputLevel=DEBUG
-#topSequence.L1DecoderTest.OutputFSTrigRoI="FSRoI"
-#for unpack in topSequence.L1DecoderTest.roiUnpackers:
-#    if unpack.name() is "JRoIsUnpackingTool":
-#        unpack.Decisions="L1JET"
-#        unpack.FSRoIKey = "FSRoI"
-# this is empty        unpack.OutputTrigRoIs= "JETRoIs"
-
+  
 
 CTPToChainMapping = {"HLT_j85":       "L1_J20"   }
 testChains =[x for x, y in CTPToChainMapping.items()]
@@ -27,7 +19,6 @@ print testChains
 print topSequence.L1DecoderTest
 
 #from TrigUpgradeTest.jetDefs import jetRecoSequence
-#(recoSequence, sequenceOut) = jetRecoSequence("FSRoI")
- 
+#(recoSequence, sequenceOut) = jetRecoSequence("FSRoI") 
 #topSequence += recoSequence
     
