e74bc1bea753e3487c040846e58420f318b9c81a
Petr Jacka <petr.jacka@cern.ch>
2018-11-28 18:36:13 +0100

    Changing to new coordinates used for shower shape parameterization in FCal
    Former-commit-id: 3686c1bc650b0814ebe2ec11a39f8c70e0b16212

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSHistoLateralShapeParametrizationFCal.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSHistoLateralShapeParametrizationFCal.cxx
@@ -11,6 +11,9 @@
 
 #include "TMath.h"
 
+#include "HepPDT/ParticleData.hh"
+#include "HepPDT/ParticleDataTable.hh"
+
 
 //=============================================
 //======= TFCSHistoLateralShapeParametrizationFCal =========
@@ -25,19 +28,19 @@ TFCSHistoLateralShapeParametrizationFCal::~TFCSHistoLateralShapeParametrizationF
 {
 }
 
-FCSReturnCode TFCSHistoLateralShapeParametrizationFCal::simulate_hit(Hit &hit, TFCSSimulationState &simulstate, const TFCSTruthState* /*truth*/, const TFCSExtrapolationState* extrapol)
+FCSReturnCode TFCSHistoLateralShapeParametrizationFCal::simulate_hit(Hit &hit, TFCSSimulationState &simulstate, const TFCSTruthState* truth, const TFCSExtrapolationState* extrapol)
 {
   if (!simulstate.randomEngine()) {
     return FCSFatal;
   }
 
+  const int     pdgId    = truth->pdgid();
+  const double  charge   = HepPDT::ParticleID(pdgId).charge();
+
   const int cs=calosample();
   const double center_phi=0.5*( extrapol->phi(cs, CaloSubPos::SUBPOS_ENT) + extrapol->phi(cs, CaloSubPos::SUBPOS_EXT) );
   const double center_r=0.5*( extrapol->r(cs, CaloSubPos::SUBPOS_ENT) + extrapol->r(cs, CaloSubPos::SUBPOS_EXT) );
   const double center_z=0.5*( extrapol->z(cs, CaloSubPos::SUBPOS_ENT) + extrapol->z(cs, CaloSubPos::SUBPOS_EXT) );
-  
-  const double center_x=center_r*cos(center_phi);
-  const double center_y=center_r*sin(center_phi);
 
   float alpha, r, rnd1, rnd2;
   rnd1 = CLHEP::RandFlat::shoot(simulstate.randomEngine());
@@ -64,10 +67,12 @@ FCSReturnCode TFCSHistoLateralShapeParametrizationFCal::simulate_hit(Hit &hit, T
     return FCSFatal;
   }
   
-  const float delta_x = r * cos(alpha);
-  const float delta_y = r * sin(alpha);
+  const float hit_r = r*cos(alpha) + center_r;
+  float delta_phi = r*sin(alpha)/center_r;
+  if(charge < 0.) delta_phi = -delta_phi;
+  const float hit_phi= delta_phi + center_phi;
 
-  hit.setXYZE(center_x+delta_x,center_y+delta_y,center_z,hit.E());
+  hit.setXYZE(hit_r*cos(hit_phi),hit_r*sin(hit_phi),center_z,hit.E());
 
   ATH_MSG_DEBUG("HIT: E="<<hit.E()<<" cs="<<cs<<" x="<<hit.x()<<" y="<<hit.y()<<" z="<<hit.z()<<" r=" << r <<" alpha="<<alpha);
 
