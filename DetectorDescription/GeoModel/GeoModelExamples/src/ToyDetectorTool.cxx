cbb1fdef59bebf3df2d914acfd4654d042057928
scott snyder <scott.snyder@cern.ch>
2018-11-29 15:44:13 +0100

    GeoModelExamples: Avoid use of non-bound DataHandle.
    Deprecating use of non-bound DataHandle, especially non-const.
    Also adding const for materials.
    
    
    Former-commit-id: f03b4f25f8ea6e3fbd63b13271ca6fc1d19ae4fe

--- DetectorDescription/GeoModel/GeoModelExamples/src/ToyDetectorTool.cxx
+++ DetectorDescription/GeoModel/GeoModelExamples/src/ToyDetectorTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "ToyDetectorTool.h"
@@ -42,7 +42,7 @@ ToyDetectorTool::create()
   // 
   // Locate the top level experiment node 
   // 
-  DataHandle<GeoModelExperiment> theExpt; 
+  GeoModelExperiment* theExpt = nullptr;
   if (StatusCode::SUCCESS != detStore()->retrieve( theExpt, "ATLAS" )) { 
     log << MSG::ERROR 
 	<< "Could not find GeoModelExperiment ATLAS" 
