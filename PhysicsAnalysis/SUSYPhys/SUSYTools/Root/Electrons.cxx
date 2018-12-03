32379bd07bcd88dbdc0d6dbcfe9b8527b74af824
Hass AbouZeid <hass.abouzeid@cern.ch>
2018-12-03 14:46:23 +0100

    SUSYTools: Don't decorate isolation parameters if not needed
    Former-commit-id: 0d79410ae487751443bd96b1a77d900654e6adcc

--- PhysicsAnalysis/SUSYPhys/SUSYTools/Root/Electrons.cxx
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/Root/Electrons.cxx
@@ -200,11 +200,13 @@ StatusCode SUSYObjDef_xAOD::FillElectron(xAOD::Electron& input, float etcut, flo
 
   dec_baseline(input) = true;
   dec_selected(input) = 2;
-  dec_isol(input) = m_isoTool->accept(input);
-  if (m_eleIsoHighPt_WP == "FixedCutHighPtCaloOnly" && acc_topoetcone20.isAvailable(input)) {
-    dec_isolHighPt(input) = acc_topoetcone20(input)/input.pt() < 0.015 || m_isoHighPtTool->accept(input);
-  } else {
-    dec_isolHighPt(input) = m_isoHighPtTool->accept(input);
+  if (m_doElIsoSignal){
+    dec_isol(input) = m_isoTool->accept(input);
+    if (m_eleIsoHighPt_WP == "FixedCutHighPtCaloOnly" && acc_topoetcone20.isAvailable(input)) {
+      dec_isolHighPt(input) = acc_topoetcone20(input)/input.pt() < 0.015 || m_isoHighPtTool->accept(input);
+    } else {
+      dec_isolHighPt(input) = m_isoHighPtTool->accept(input);
+    }
   }
 
   //ChargeIDSelector
