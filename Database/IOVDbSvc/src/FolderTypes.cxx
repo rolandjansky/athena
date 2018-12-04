ca02e99f83b76da95f2392edefd0362d68633ba4
Shaun Roe <shaun.roe@cern.ch>
2018-12-04 08:52:48 +0000

    22.0-refactor-IOVDbSvc
    Former-commit-id: aef792dee6d2946bc499aedb2550a713ba629f90

---
+++ Database/IOVDbSvc/src/FolderTypes.cxx
@@ -0,0 +1,79 @@
+#include "FolderTypes.h"
+#include "AthenaKernel/IClassIDSvc.h"
+#include "IOVDbStringFunctions.h"
+#include "CoolKernel/IRecordSpecification.h"
+#include "AthenaPoolUtilities/CondAttrListCollection.h"
+#include <vector>
+
+namespace IOVDbNamespace{
+  //determine folder type from DB folder ptr
+  FolderType
+  determineFolderType(const cool::IFolderPtr & pFolder, IClassIDSvc* /*clidsvc*/){
+    const auto & folderDescription = pFolder->description();
+    //int clid = parseClid(folderDescription);
+    //If you find a coracool tag, it is unambiguously a coracool folder
+    std::cout<<folderDescription<<std::endl;
+    if (folderDescription.find("<coracool>") != std::string::npos) return CoraCool;
+    const std::string typeName = parseTypename(folderDescription);
+    std::cout<<typeName<<std::endl;
+    //if the type is CondAttrListVec, and yet it is not a CoraCool, it must be a CoolVector
+    if (typeName=="CondAttrListVec") return CoolVector;
+    //check if the payload spec is compatible with a pool ref/pool ref collection
+    if (poolCompatible(pFolder->payloadSpecification())){
+      const auto & chans=pFolder->listChannels();
+      const auto nchans = chans.size();
+      const bool onlyOneSpecialChannel = ((nchans == 1) and (chans[0] == 0));
+      return onlyOneSpecialChannel ? PoolRef : PoolRefColl;
+    }
+    if (typeName == "CondAttrListCollection") return AttrListColl;
+    //defaults to AttrList
+    return AttrList;
+   }
+   
+   //determine folder type from CondAttrListCollection ptr
+   FolderType
+   determineFolderType(const CondAttrListCollection * pAttrListColl){
+     FolderType ftype(AttrList);
+     //has a single magic channel?
+     if (pAttrListColl->size()==1 && pAttrListColl->begin()->first==0xFFFF) {
+       if (poolCompatible (pAttrListColl)) return PoolRef;
+       return AttrList;
+     } else {
+       ftype=AttrListColl;
+       if ( pAttrListColl->size()>0) {
+         if (poolCompatible (pAttrListColl)) return PoolRefColl;
+       }
+     }
+     return ftype;
+   }
+   
+   FolderType
+   determineFolderType(const coral::AttributeSpecification& spec){
+     FolderType result(UNKNOWN);
+     if (spec.name()=="PoolRef" && spec.typeName()=="string") result = PoolRef;
+     return result;
+   }
+   
+   bool
+   poolCompatible(const cool::IRecordSpecification& rspec){
+     return (rspec.exists("PoolRef") && rspec[0].name()=="PoolRef" &&
+      rspec[0].storageType()==cool::StorageType::String4k);
+   }
+   
+   bool
+   poolCompatible(const CondAttrListCollection * pAttrListColl){
+     const coral::AttributeList& payload1=pAttrListColl->begin()->second;
+     const coral::AttributeSpecification& spec=payload1[0].specification();
+     return (spec.name()=="PoolRef" && spec.typeName()=="string");
+   }
+   
+   std::string
+   folderTypeName(const FolderType f){
+      static const std::vector<std::string> names{"AttrList", "AttrListColl", "PoolRef",
+      "PoolRefColl", "CoraCool", "CoolVector", "JSON"};
+      if (f==UNKNOWN) return "Unknown";
+      return names[f];
+   }
+
+}
+
---
+++ Database/IOVDbSvc/src/FolderTypes.h
@@ -0,0 +1,58 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+// @file FolderTypes.h
+// Header for FolderTypes utilities
+// @author Shaun Roe
+// @date 05 October 2017
+#ifndef FolderTypes_H
+#define FolderTypes_H
+
+#include "CoolKernel/IFolder.h"
+#include "CoolKernel/pointers.h" //IFolderPtr etc, typdefs
+#include <string>
+
+class IClassIDSvc;
+class CondAttrListCollection;
+
+namespace cool{
+  class IRecordSpecification;
+}
+
+
+namespace IOVDbNamespace{
+  //basic folder types
+  enum FolderType {
+    UNKNOWN=-1,
+    AttrList=0,
+    AttrListColl,
+    PoolRef,
+    PoolRefColl,
+    CoraCool,
+    CoolVector,
+    JSON,
+    NUM_FOLDER_TYPES
+   };
+   
+   //determine folder type with optional check using clid service to check clid matches typename
+   FolderType
+   determineFolderType(const cool::IFolderPtr & pFolder, IClassIDSvc* clidsvc=nullptr);
+   
+   std::string
+   folderTypeName(const FolderType f);
+   
+   FolderType
+   determineFolderType(const CondAttrListCollection * pAttrListColl);
+   
+   FolderType
+   determineFolderType(const coral::AttributeSpecification& spec);
+   
+   bool
+   poolCompatible(const cool::IRecordSpecification& rspec);
+   
+   bool
+   poolCompatible(const CondAttrListCollection * pAttrListColl);
+}
+
+#endif
+
