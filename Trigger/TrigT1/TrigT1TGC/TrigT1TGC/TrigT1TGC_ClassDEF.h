d16af76a6b18280ac7f285255381e4c2ce220816
scott snyder <snyder@bnl.gov>
2018-12-04 15:56:51 +0100

    TrigT1TGC: CLIDSvc -> AthenaKernel
    Adjust for header moved from CLIDSvc to AthenaKernel.
    Also remove unneeded dependency on SGTools.
    
    
    
    Former-commit-id: 88bed65d1d7d0688d65a586beeede4ac81bfd5e1

--- Trigger/TrigT1/TrigT1TGC/TrigT1TGC/TrigT1TGC_ClassDEF.h
+++ Trigger/TrigT1/TrigT1TGC/TrigT1TGC/TrigT1TGC_ClassDEF.h
@@ -2,15 +2,14 @@
   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef __TrigT1TGC_ClassDEF_H__
-#define __TrigT1TGC_ClassDEF_H__
+#ifndef TRIGT1TGC_TRIGT1TGC_CLASSDEF_H
+#define TRIGT1TGC_TRIGT1TGC_CLASSDEF_H
 
 #include <vector>
 #include "TrigT1TGC/TGCASDOut.h"
 
 // 2003.Feb.17 M.I //
-//#include "StoreGate/tools/ClassID_traits.h"
-#include "CLIDSvc/CLASS_DEF.h"
+#include "AthenaKernel/CLASS_DEF.h"
 
 CLASS_DEF(std::vector<LVL1TGCTrigger::TGCASDOut>, 6150, 1)
 
