c28d66400f6ff51033dd4f62efa3eee52001bc3c
Stewart Martin-Haugh <smh@cern.ch>
2018-12-04 16:32:51 +0100

    New unit test for TileGeoModel configuration
    Former-commit-id: 986318ce74a5bd981e5e3a8f2acecd8e3f6dc898

--- TileCalorimeter/TileGeoModel/python/TileGMConfig.py
+++ TileCalorimeter/TileGeoModel/python/TileGMConfig.py
@@ -1,5 +1,7 @@
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AtlasGeoModel.GeoModelConfig import GeoModelCfg
+from AthenaCommon.Configurable import Configurable
+Configurable.configurableRun3Behavior=1
 
 def TileGMCfg(configFlags):
     result,gms=GeoModelCfg(configFlags)
@@ -10,3 +12,14 @@ def TileGMCfg(configFlags):
     result.addService(gms)
 
     return result
+
+
+if __name__ == "__main__":
+    from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+    acc = ComponentAccumulator()
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    ConfigFlags.Input.Files = ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"]
+
+    acc = TileGMCfg( ConfigFlags )
+    acc.store( file( "test.pkl", "w" ) )
+    print "All OK"
