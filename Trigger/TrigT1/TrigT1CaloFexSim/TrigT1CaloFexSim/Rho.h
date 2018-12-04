a94a9c76a842d4d48a4bccc3bad0736136fbc975
amyers <amyers@cern.ch>
2018-12-04 18:16:58 +0100

    Used units in upper threshold
    Former-commit-id: 386c863f803e5f70bbcde5f3814756dd5efd440b

--- Trigger/TrigT1/TrigT1CaloFexSim/TrigT1CaloFexSim/Rho.h
+++ Trigger/TrigT1/TrigT1CaloFexSim/TrigT1CaloFexSim/Rho.h
@@ -31,6 +31,7 @@ Rho_med(): Calculates rho as the median tower energy density in the barrel, agai
 #include "JetCalibTools/IJetCalibrationTool.h"
 #include "JetInterface/IJetUpdateJvt.h"
 #include "TrigAnalysisInterfaces/IBunchCrossingTool.h"
+#include "GaudiKernel/SystemOfUnits.h"
 #include "Identifier/IdentifierHash.h"
 #include <algorithm>
 #include <vector>
@@ -40,57 +41,59 @@ Rho_med(): Calculates rho as the median tower energy density in the barrel, agai
  *@brief Calculates rho as the average tower energy density in the barrel region of the calorimeter, to be scaled with are in more forward regions. An upper threshold of 10 GeV is applied to eliminate bias from hard scatter events
  *@return @c float
  */
-float Rho_bar(const xAOD::JGTowerContainer* towers, bool useNegTowers){
+float Rho_bar(const xAOD::JGTowerContainer* towers, const bool useNegTowers){
+
   float rho = 0;
-  float et_max = 3000;  //an upper threshold such that the average rho is not biased by hard scatter events
+  float et_max = 10*Gaudi::Units::GeV; //an upper threshold such that the average rho is not biased by hard scatter eve\
+nts                                                                                                                     
   const unsigned int size = towers->size();
-  int length = 0;
+ int length = 0;
 
-  for(unsigned int i = 0; i < size; i++){
-    const xAOD::JGTower* tower = towers->at(i);
-    float eta = TMath::Abs(tower->eta());
-    float Et = tower->et();
+ for(unsigned int i = 0; i < size; i++){
+   const xAOD::JGTower* tower = towers->at(i);
+   float eta = TMath::Abs(tower->eta());
+   float Et = tower->et();
 
-    if(!useNegTowers) Et = TMath::Abs(Et);
-    
-    if(eta < 2.4){
-      if(Et < et_max){
-	rho += Et;
-	length++;
-      }
-    }
-  }
-  float rho_bar = rho/length;
-  
-  return rho_bar;
+   if(!useNegTowers) Et = TMath::Abs(Et);
+
+   if(eta < 2.4){
+     if(Et < et_max){
+       rho += Et;
+       length++;
+     }
+   }
+ }
+ float rho_bar = rho/length;
+
+ return rho_bar;
 }
-
 
 /**
  *@brief Calculates rho as the median tower energy density in the barrel region of the calorimeter, to be scaled with area in more forward regions
  *@return @c float
  */
-float Rho_med(const xAOD::JGTowerContainer* towers, bool useNegTowers){
+float Rho_med(const xAOD::JGTowerContainer* towers, const bool useNegTowers){
   float rho = 0;
-  const unsigned int size = towers->size();
+  const unsigned int size = 0;
   std::vector<float> Et_in;
 
   for(unsigned int i = 0; i < size; i++){
     const xAOD::JGTower* tower = towers->at(i);
     float Et = tower->et();
     float eta = TMath::Abs(tower->eta());
-   
+
     if(!useNegTowers) Et = TMath::Abs(Et);
 
     if(eta < 2.4) Et_in.push_back(Et);
   }
   std::sort(Et_in.begin(), Et_in.end());
 
-  if(Et_in.size() == 1) rho = Et_in.at(0);
-  if(Et_in.size() % 2 == 0) rho = (Et_in.at(size/2) + Et_in.at(size/2 - 1))/2;
-  else rho = Et_in.at(Et_in.size()/2);
+  if(Et_in.size() == 1) rho = Et_in[0];
+  else if(Et_in.size() % 2 == 0) rho = (Et_in[size/2] + Et_in[size/2 -1])/2;
+  else rho = Et_in[size/2];
 
   return rho;
 }
+
 
 #endif
