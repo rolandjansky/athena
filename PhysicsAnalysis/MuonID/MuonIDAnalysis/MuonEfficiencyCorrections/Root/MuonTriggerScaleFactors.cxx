0007e6d7cf4e79eb100f79295ed12352da1e5077
morgens <marcus.matthias.morgenstern@cern.ch>
2018-12-04 16:13:10 +0100

    update calibration release
    Former-commit-id: 8da41bb92a77b6de765bc88f0e20aa3897e324a4

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/Root/MuonTriggerScaleFactors.cxx
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/Root/MuonTriggerScaleFactors.cxx
@@ -47,7 +47,7 @@ namespace CP {
       m_efficiencyMap(),
       m_efficiencyMapReplicaArray(),
       m_muonquality("Medium"),
-      m_calibration_version("180905_TriggerUpdate"),
+      m_calibration_version("181204_TriggerUpdate"),
       m_custom_dir(),
       m_binning("fine"),
       m_eventInfoContName("EventInfo"),
