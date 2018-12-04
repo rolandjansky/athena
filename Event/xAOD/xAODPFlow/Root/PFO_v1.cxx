fbe39034def73b3c68a03e76c9af2ab7132db2bb
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2018-12-04 11:48:33 +0000

    Do not ask to get static accessor from accessor() function, which already returns a static accessor.
    Former-commit-id: fb4207f90b7df7499872cfd548051e83d07ab799

--- Event/xAOD/xAODPFlow/Root/PFO_v1.cxx
+++ Event/xAOD/xAODPFlow/Root/PFO_v1.cxx
@@ -294,7 +294,7 @@ namespace xAOD {
       return isValid;
     }
     else{
-      const static ConstAccessor<float>* p_acc = PFOAttributesAccessor_v1<float>::accessor(AttributeType);
+      const ConstAccessor<float>* p_acc = PFOAttributesAccessor_v1<float>::accessor(AttributeType);
       //check if accessor pointer is NULL
       if( ! p_acc ) {  return false ;}
       //check if variable is avaialable
