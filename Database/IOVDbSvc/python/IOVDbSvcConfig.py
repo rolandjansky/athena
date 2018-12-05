0600abcdf4d9a315cd00fafd2c3d06e00cb90e6d
Stewart Martin-Haugh <smh@cern.ch>
2018-12-05 12:19:19 +0100

    New unit test for IOVDbSvc configuration
    Former-commit-id: dd0b1df8c416257448171f60f25ed12b3c86a9f7

--- Database/IOVDbSvc/python/IOVDbSvcConfig.py
+++ Database/IOVDbSvc/python/IOVDbSvcConfig.py
@@ -218,3 +218,17 @@ def _extractFolder(folderstr):
 
     
         
+if __name__ == "__main__":
+    from AthenaCommon.Configurable import Configurable
+    Configurable.configurableRun3Behavior=1
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+
+    ConfigFlags.Input.Files =["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"]
+    ConfigFlags.lock()
+
+    from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+    acc, iovdbSvc = IOVDbSvcCfg(ConfigFlags)
+
+    f=open('test.pkl','w')
+    acc.store(f)
+    f.close()
