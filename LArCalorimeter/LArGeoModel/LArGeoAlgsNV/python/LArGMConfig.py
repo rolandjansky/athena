55a57293b724191a10d09bf4f92db0f270f32513
Stewart Martin-Haugh <smh@cern.ch>
2018-12-05 12:04:49 +0100

    New unit test for LArGeoAlgsNV configuration
    Former-commit-id: 0c83be5a9c59205dcb2f5e8b7b7e4e88e8d6a7a0

--- LArCalorimeter/LArGeoModel/LArGeoAlgsNV/python/LArGMConfig.py
+++ LArCalorimeter/LArGeoModel/LArGeoAlgsNV/python/LArGMConfig.py
@@ -23,3 +23,17 @@ def LArGMCfg(configFlags):
 
             
     return result
+
+if __name__ == "__main__":
+    from AthenaCommon.Configurable import Configurable
+    Configurable.configurableRun3Behavior=1
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+
+    ConfigFlags.Input.Files =["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1"]
+    ConfigFlags.lock()
+
+    from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+    acc = LArGMCfg(ConfigFlags)
+    f=open('LArGMCfg.pkl','w')
+    acc.store(f)
+    f.close()
