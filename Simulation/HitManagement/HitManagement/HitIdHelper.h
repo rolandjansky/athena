e181febea6326a8d74e5ede054095aff97cfb091
John Chapman <jchapman@cern.ch>
2018-12-06 15:31:58 +0100

    Add new methods to avoid changing internal state of HitIdHelper after initialization
    The HitIdHelpers are singletons, so there is the potential for multiple threads
    to try to use them at the same time. Using a member variable to manipulate the hit
    IDs in this situation could lead to data-races.
    
    
    Former-commit-id: cb81c8af6f795241a442d05da739f730cf56557d

--- Simulation/HitManagement/HitManagement/HitIdHelper.h
+++ Simulation/HitManagement/HitManagement/HitIdHelper.h
@@ -1,9 +1,9 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef HitIdHelper_H
-#define HitIdHelper_H
+#ifndef HITMANAGEMENT_HitIdHelper_H
+#define HITMANAGEMENT_HitIdHelper_H
 
 #include <string>
 #include <vector>
@@ -11,36 +11,38 @@
 typedef int HitID;
 
 struct IdField {
-	std::string fieldName;
-	int vMin;
-	int vMax;
-	int nBits;
-	int firstBit;
+  std::string fieldName;
+  int vMin;
+  int vMax;
+  int nBits;
+  int firstBit;
 };
 
 typedef std::vector<IdField> IdFields;
 
 class HitIdHelper {
 public:
-	HitIdHelper():m_id(0), m_currentStorage(0)  {}
-	virtual ~HitIdHelper() {}
-	void InitializeField(std::string n, int vmn, int vmx);
-	void InitializeField(std::string n, int nb);
-	void SetFieldValue(std::string name, int n);
-	int  GetFieldValue(std::string name);
-	void PrintFields();
-	void SetID(int i) 	{m_id=i;}
-	HitID GetID() 		{return m_id;}
-	void ResetID() 		{m_id=0;}
-	void Print(int id);
+  HitIdHelper():m_id(0), m_currentStorage(0)  {}
+  virtual ~HitIdHelper() {}
+  void InitializeField(std::string n, int vmn, int vmx);
+  void InitializeField(std::string n, int nb);
+  void SetFieldValue(std::string name, int n);
+  void SetFieldValue(std::string name, int n, HitID& targetID) const;
+  int  GetFieldValue(std::string name);
+  int  GetFieldValue(std::string name, HitID targetID) const;
+  void PrintFields();
+  void SetID(int i)       {m_id=i;}      // FIXME drop this method?
+  HitID GetID()           {return m_id;} // FIXME drop this method?
+  void ResetID()          {m_id=0;} // FIXME drop this method?
+  void Print(int id);
 protected:
-	HitID m_id;
-	IdFields m_IDs;
-	int m_currentStorage;
-	void SetBit(int& i, int bitpos);
-	void UnsetBit(int& i, int bitpos);
-	void Store(int& id, int value, int first, int field);
-	int  Retrieve(int id, int first, int field);
+  HitID m_id; // FIXME drop this member variable
+  IdFields m_IDs;
+  int m_currentStorage;
+  void SetBit(int& i, int bitpos) const;
+  void UnsetBit(int& i, int bitpos) const;
+  void Store(int& id, int value, int first, int field) const;
+  int  Retrieve(int id, int first, int field) const;
 };
 
 #endif
--- Simulation/HitManagement/src/HitIdHelper.cxx
+++ Simulation/HitManagement/src/HitIdHelper.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "HitManagement/HitIdHelper.h"
@@ -9,106 +9,138 @@
 
 void HitIdHelper::InitializeField(std::string n, int vmn, int vmx)
 {
-	int field = vmx -vmn+1;
-	int nb=1;
-	while (field>pow(2,nb)) nb++;
-	IdField id={n,vmn,vmx,nb,m_currentStorage+1};
-	m_IDs.push_back(id);
-	m_currentStorage+=nb;
-	assert (m_currentStorage<33);
+  int field = vmx -vmn+1;
+  int nb=1;
+  while (field>pow(2,nb)) nb++;
+  IdField id={n,vmn,vmx,nb,m_currentStorage+1};
+  m_IDs.push_back(id);
+  m_currentStorage+=nb;
+  assert (m_currentStorage<33);
 }
 
 void HitIdHelper::InitializeField(std::string n,int nb)
 {
-	int vmn=0;
-	int vmx=int(pow(2,nb))-1;
-	IdField id={n,vmn,vmx,nb,m_currentStorage+1};
-	m_IDs.push_back(id);
-	m_currentStorage+=nb;
-	assert (m_currentStorage<33);
+  int vmn=0;
+  int vmx=int(pow(2,nb))-1;
+  IdField id={n,vmn,vmx,nb,m_currentStorage+1};
+  m_IDs.push_back(id);
+  m_currentStorage+=nb;
+  assert (m_currentStorage<33);
 }
+
 void HitIdHelper::SetFieldValue(std::string name, int n)
 {
-	for (unsigned int i=0;i<m_IDs.size();i++)
-	{
-		if (m_IDs[i].fieldName==name) 
-		{
-			if (n<m_IDs[i].vMin || n>m_IDs[i].vMax)
-				std::cout<<"HitIdHelper: field "<<name<<": supplied value "<<n<<
-				" is not within the allowed range ["<<m_IDs[i].vMin<<","<<m_IDs[i].vMax
-				<<"] : the result of this action is unpredictable "<<std::endl;
-			n-=m_IDs[i].vMin;
-			Store(m_id,n,m_IDs[i].firstBit,m_IDs[i].nBits);
-			break;
-		}
-	}
+  for (unsigned int i=0;i<m_IDs.size();i++)
+    {
+      if (m_IDs[i].fieldName==name)
+        {
+          if (n<m_IDs[i].vMin || n>m_IDs[i].vMax)
+            std::cout<<"HitIdHelper: field "<<name<<": supplied value "<<n<<
+              " is not within the allowed range ["<<m_IDs[i].vMin<<","<<m_IDs[i].vMax
+                     <<"] : the result of this action is unpredictable "<<std::endl;
+          n-=m_IDs[i].vMin;
+          Store(m_id,n,m_IDs[i].firstBit,m_IDs[i].nBits);
+          break;
+        }
+    }
+}
+
+void HitIdHelper::SetFieldValue(std::string name, int n, HitID& targetID) const
+{
+  for (unsigned int i=0;i<m_IDs.size();i++)
+    {
+      if (m_IDs[i].fieldName==name)
+        {
+          if (n<m_IDs[i].vMin || n>m_IDs[i].vMax)
+            std::cout<<"HitIdHelper: field "<<name<<": supplied value "<<n<<
+              " is not within the allowed range ["<<m_IDs[i].vMin<<","<<m_IDs[i].vMax
+                     <<"] : the result of this action is unpredictable "<<std::endl;
+          n-=m_IDs[i].vMin;
+          Store(targetID,n,m_IDs[i].firstBit,m_IDs[i].nBits);
+          break;
+        }
+    }
 }
 
 int  HitIdHelper::GetFieldValue(std::string name)
 {
-	for (unsigned int i=0;i<m_IDs.size();i++)
-	{
-		if (m_IDs[i].fieldName==name) 
-		{
-			int n=Retrieve(m_id,m_IDs[i].firstBit,m_IDs[i].nBits);
-			return n+m_IDs[i].vMin;
-		}
-	}
-	std::cout<<"HitIdHelper: field "<<name<<" not found: returning 0"<<
-	std::endl;
-	return 0;
+  for (unsigned int i=0;i<m_IDs.size();i++)
+    {
+      if (m_IDs[i].fieldName==name)
+        {
+          int n=Retrieve(m_id,m_IDs[i].firstBit,m_IDs[i].nBits);
+          return n+m_IDs[i].vMin;
+        }
+    }
+  std::cout<<"HitIdHelper: field "<<name<<" not found: returning 0"<<
+    std::endl;
+  return 0;
+}
+
+int HitIdHelper::GetFieldValue(std::string name, HitID targetID) const
+{
+  for (unsigned int i=0;i<m_IDs.size();i++)
+    {
+      if (m_IDs[i].fieldName==name)
+        {
+          int n=Retrieve(targetID,m_IDs[i].firstBit,m_IDs[i].nBits);
+          return n+m_IDs[i].vMin;
+        }
+    }
+  std::cout<<"HitIdHelper: field "<<name<<" not found: returning 0"<<
+    std::endl;
+  return 0;
 }
 
 void HitIdHelper::PrintFields()
 {
-	for (unsigned int i=0;i<m_IDs.size();i++)
-	{
-		std::cout<<"-- Field "<<m_IDs[i].fieldName<<"\t["<<m_IDs[i].vMin
-		         <<","<<m_IDs[i].vMax<<"]\tnBits "<<m_IDs[i].nBits
-			 <<"\tfirst Bit "<<m_IDs[i].firstBit<<std::endl;
-	}
+  for (unsigned int i=0;i<m_IDs.size();i++)
+    {
+      std::cout<<"-- Field "<<m_IDs[i].fieldName<<"\t["<<m_IDs[i].vMin
+               <<","<<m_IDs[i].vMax<<"]\tnBits "<<m_IDs[i].nBits
+               <<"\tfirst Bit "<<m_IDs[i].firstBit<<std::endl;
+    }
 }
 
 void HitIdHelper::Print(int id)
 {
-	int l=1;
-	for (unsigned int i=0;i<8*sizeof(int);i++)
-	{
-		if ((id & (l<<i))) std::cout<<"1";
-		else std::cout<<"0";
-	}
-	std::cout<<std::endl;
+  int l=1;
+  for (unsigned int i=0;i<8*sizeof(int);i++)
+    {
+      if ((id & (l<<i))) std::cout<<"1";
+      else std::cout<<"0";
+    }
+  std::cout<<std::endl;
 }
 
-void HitIdHelper::SetBit(int& i, int bitpos)
+void HitIdHelper::SetBit(int& i, int bitpos) const
 {
-	int l=1;
-	l<<=(bitpos-1);
-	i|=l;
+  int l=1;
+  l<<=(bitpos-1);
+  i|=l;
 }
-void HitIdHelper::UnsetBit(int& i, int bitpos)
+void HitIdHelper::UnsetBit(int& i, int bitpos) const
 {
-	int l=0;
-	SetBit(l,bitpos);
-	int k=~l;
-	i&=k;
-}
-
-void HitIdHelper::Store(int& id, int value, int firstbit, int field)
-{
-	int l=0;
-	for (int i=0;i<field;i++)
-		UnsetBit(id,i+firstbit);
-	l=value<<(firstbit-1);
-	id |= l;
+  int l=0;
+  SetBit(l,bitpos);
+  int k=~l;
+  i&=k;
 }
 
-int  HitIdHelper::Retrieve(int id, int first, int field)
+void HitIdHelper::Store(int& id, int value, int firstbit, int field) const
 {
-	int j=0;
-	for (int i=0;i<field;i++)
-		SetBit(j,(i+first));
-	int l=id&j;
-	return l>>(first-1);
+  int l=0;
+  for (int i=0;i<field;i++)
+    UnsetBit(id,i+firstbit);
+  l=value<<(firstbit-1);
+  id |= l;
 }
 
+int  HitIdHelper::Retrieve(int id, int first, int field) const
+{
+  int j=0;
+  for (int i=0;i<field;i++)
+    SetBit(j,(i+first));
+  int l=id&j;
+  return l>>(first-1);
+}
