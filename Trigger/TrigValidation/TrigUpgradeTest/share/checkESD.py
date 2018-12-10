88ed8c690537785ee779e9419bf08039e8763ae6
Tim Martin <Tim.Martin@cern.ch>
2018-12-10 18:15:36 +0100

    Update Run3 checkESD to use temporary hard-coded trigger configuration

--- Trigger/TrigValidation/TrigUpgradeTest/share/checkESD.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/checkESD.py
@@ -30,3 +30,11 @@ topSequence += checker
 from TrigDecisionMaker.TrigDecisionMakerConfig import TrigDecisionMakerMT
 topSequence+=TrigDecisionMakerMT()
 topSequence.TrigDecMakerMT.OutputLevel = DEBUG
+# Note that for now we don't properly configure the trigger configuration service
+from TrigConfigSvc.TrigConfigSvcConfig import HLTConfigSvc
+theApp.Dlls += [ "TrigConfigSvc" ]
+theApp.CreateSvc += [ "TrigConf::HLTConfigSvc/HLTConfigSvc" ]
+ServiceMgr += HLTConfigSvc()
+ServiceMgr.HLTConfigSvc.ConfigSource   = "RUN3_DUMMY"
+ServiceMgr.HLTConfigSvc.OutputLevel    = VERBOSE
+print ServiceMgr.HLTConfigSvc
