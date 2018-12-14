e8bb14af14b7c316f132d2cd7c38fe20ce77a5c5
John Kenneth Anders <janders@lxplus034.cern.ch>
2018-12-14 18:19:55 +0100

    added ART include to 21-mc16d
    Former-commit-id: 0fd5ca9cb96679862895c2f2367ffb9d939ca8d9

--- Tools/Tier0ChainTests/test/test_run00184169_egamma_collisions.sh
+++ Tools/Tier0ChainTests/test/test_run00184169_egamma_collisions.sh
@@ -2,14 +2,19 @@
 #
 # art-description: RecoTrf with beamType=collisions
 # art-type: grid
+# art-include: 21.0/Athena
+# art-include: 21.0-mc16a/Athena
+# art-include: 21.0-mc16d/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
 
-Reco_tf.py  --outputHISTFile=myMergedMonitoring_Egamma_0.root --maxEvents=500 --outputESDFile=myESD_Egamma_0.pool.root --outputAODFile=myAOD_Egamma_0.AOD.pool.root --outputTAGFile=myTAG_Egamma_0.root --ignoreErrors=False --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-10._0001.1,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-11._0001.1,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-12._0001.1 --preExec='rec.doTrigger=False;' --imf False
-echo "art-result: $?"
+Reco_tf.py  --outputHISTFile=myMergedMonitoring_Egamma_0.root --maxEvents=500 --outputESDFile=myESD_Egamma_0.pool.root --outputAODFile=myAOD_Egamma_0.AOD.pool.root --outputTAGFile=myTAG_Egamma_0.root --ignoreErrors=False --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-10._0001.1,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-11._0001.1,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data11_7TeV.00184169.physics_Egamma.merge.RAW._lb0900._SFO-12._0001.1 --preExec='rec.doTrigger=True;DQMonFlags.doCTPMon=False;' --imf False
+echo "art-result: $? Reco"
 
 ArtPackage=$1
 ArtJobName=$2
 art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
-echo "art-result: $?"
+echo "art-result: $? Diff"
 
-art.py compare grid --days=3 --entries 10 ${ArtPackage} ${ArtJobName}
-echo "art-result: $?"
