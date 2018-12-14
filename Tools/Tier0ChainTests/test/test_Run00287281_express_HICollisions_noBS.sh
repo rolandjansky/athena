f790b931527f83520313722d490293673a5fffe0
John Kenneth Anders <janders@lxplus084.cern.ch>
2018-12-14 17:40:06 +0100

    Adding art-include for 21-mc16a branch
    Former-commit-id: 43b7c9f715f7a289a62abb202587e7240c335322

--- Tools/Tier0ChainTests/test/test_Run00287281_express_HICollisions_noBS.sh
+++ Tools/Tier0ChainTests/test/test_Run00287281_express_HICollisions_noBS.sh
@@ -2,12 +2,19 @@
 #
 # art-description: RecoTrf no BS constraint on 2015 PbPb express stream (Run287281 LB124)
 # art-type: grid
+# art-include: 21.0/Athena
+# art-include: 21.0-mc16a/Athena
+# art-include: 21.0-mc16d/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
 
 Reco_tf.py --outputESDFile=myESD_0.pool.root --outputAODFile=myAOD_0.AOD.pool.root --outputTAGFile=myTAG_0.root --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data15_hi.00287281.express_express.merge.RAW._lb0124._SFO-ALL._0001.1 --AMI=q314 --maxEvents=25 --preExec "'r2e:from InDetRecExample.InDetJobProperties import InDetFlags;InDetFlags.useBeamConstraint.set_Value_and_Lock(False)'" --imf False
 echo "art-result: $? Reco"
 
 ArtPackage=$1
 ArtJobName=$2
-art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
+art.py compare grid --entries 10 --file=*AOD*.pool.root ${ArtPackage} ${ArtJobName}
 echo "art-result: $? Diff"
 
