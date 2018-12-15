c40408feff0719f1c2632c55b700e3c5aae8b64c
Edward Moyse <edward.moyse@cern.ch>
2018-12-15 07:55:20 +0000

    Merge branch '21.3-add-MdtCalibSvc' into '21.3'
    Add functionality for r-t relation smearing (ATLASRECTS-3882)
    
    See merge request atlas/athena!12101
    
    (cherry picked from commit 4af1692b6693de1e453835a6e3fee028eb874ebe [formerly a8f19a5b2c5e5989673f8f7b2603c48ef1f1b1d3])
    
    4d6ad18a Add functionality for r-t relation smearing (ATLASRECTS-3882)
    632d955f Adding copyright statement.
    27cf5b04 Add missing depency. Change MuonMDT_Cabling -> MuonMDT_CablingLib in linked…
    c0aa2e99 First test with common interface
    73d2a9a7 Implemented all public methods in new interface class. Committing non-compiling…
    f3ea2a81 Removed initialize and finalize methods from dervied classes
    3ef440ca Fixed compile errors
    d8cd13e6 Fixup prev. commit
    ab4d260e Untabify
    b38f8b1e Write/read to/from text file instead of binary
    
    Former-commit-id: 572e8ed5e119c5f54bb2c149962f6e7e2fe74d48

--- MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibSvc/src/components/MdtCalibSvc_entries.cxx
+++ MuonSpectrometer/MuonCalib/MdtCalib/MdtCalibSvc/src/components/MdtCalibSvc_entries.cxx
@@ -3,13 +3,19 @@
 #include "MdtCalibSvc/MdtCalibrationSvc.h"
 #include "MdtCalibSvc/MdtCalibrationRegionSvc.h"
 #include "MdtCalibSvc/MdtCalibrationDbSvc.h"
+#include "MdtCalibSvc/MdtCalibrationT0ShiftSvc.h"
+#include "MdtCalibSvc/MdtCalibrationTMaxShiftSvc.h"
 
 DECLARE_SERVICE_FACTORY ( MdtCalibrationSvc )
 DECLARE_SERVICE_FACTORY ( MdtCalibrationRegionSvc )
 DECLARE_SERVICE_FACTORY ( MdtCalibrationDbSvc )
+DECLARE_SERVICE_FACTORY ( MdtCalibrationT0ShiftSvc )
+DECLARE_SERVICE_FACTORY ( MdtCalibrationTMaxShiftSvc )
 
 DECLARE_FACTORY_ENTRIES(MdtCalibSvc) {
   DECLARE_SERVICE(MdtCalibrationSvc)
   DECLARE_SERVICE(MdtCalibrationRegionSvc)
   DECLARE_SERVICE(MdtCalibrationDbSvc)
+  DECLARE_SERVICE(MdtCalibrationT0ShiftSvc)
+  DECLARE_SERVICE(MdtCalibrationTMaxShiftSvc)
 }
