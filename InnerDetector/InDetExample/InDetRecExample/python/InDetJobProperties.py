c0572f25fb12b4443d5747359eae9812ec03e7b1
Sofia Adorni Braccesi Chiassi <sofia.adorni@cern.ch>
2018-12-16 13:48:09 +0000

    Changes in the ambiguity solver to improve tracking for high Pt Bs. Changes can be turned on by using two different flags.
    Former-commit-id: 363b497df1d78b92a67c4b497c878d36eb4c7acc

--- InnerDetector/InDetExample/InDetRecExample/python/InDetJobProperties.py
+++ InnerDetector/InDetExample/InDetRecExample/python/InDetJobProperties.py
@@ -322,6 +322,18 @@ class doHadCaloSeededSSS(InDetFlagsJobProperty):
     allowedTypes = ['bool']
     StoredValue  = False
 
+class doCaloSeededAmbi(InDetFlagsJobProperty):
+    """Use Calo ROIs to seed specific cuts for the ambi"""
+    statusOn     = True
+    allowedTypes = ['bool']
+    StoredValue  = False
+
+class doCaloSeededRefit(InDetFlagsJobProperty):
+    """Use Calo ROIs to seed refif for the ambi processor"""
+    statusOn     = True
+    allowedTypes = ['bool']
+    StoredValue  = False
+
 class doBeamGas(InDetFlagsJobProperty):
     """Turn running of BeamGas second pass on and off"""
     statusOn     = True
@@ -2646,6 +2658,8 @@ _list_InDetJobProperties = [Enabled,
                             doBremRecovery,
                             doCaloSeededBrem,
                             doHadCaloSeededSSS,
+                            doCaloSeededAmbi,
+                            doCaloSeededRefit,
                             doBeamGas,
                             doBeamHalo,
                             doVtxLumi,
