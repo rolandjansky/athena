cbb1d2f3023d5ff8c54bc8058ac5961cf934d56e
Alexander Solodkov <Sanya.Solodkov@cern.ch>
2018-12-18 22:10:24 +0100

    additional plotting option added

--- TileCalorimeter/TileCalib/TileCalibBlobPython/share/PlotCalibFromCool.py
+++ TileCalorimeter/TileCalib/TileCalibBlobPython/share/PlotCalibFromCool.py
@@ -9,7 +9,7 @@
 
 
 import getopt,sys,os,string,math,re
-os.environ['TERM'] = 'linux' 
+os.environ['TERM'] = 'linux'
 
 def usage():
     print "How to use: ",sys.argv[0]," [OPTION] ... "
@@ -37,11 +37,12 @@ def usage():
     print "-c, --chan=     specify channel to use for 1D plots, default is 0"
     print "-g, --gain=, -a, --adc=  specify adc(gain) to use, default is 0 (low gain)"
     print "-v, --val=,  -i, --ind=  specify index of value to use, default is 0 "
+    print "-C, --cut=      specify additional cut on displayed values"
     print "-s, --schema=   specify schema to use, like 'COOLONL_TILE/CONDBR2' or 'sqlite://;schema=tileSqlite.db;dbname=CONDBR2' or tileSqlite.db"
 
 
-letters = "hr:l:s:t:f:a:g:b:e:o:Pp:Tv:i:m:c:I:A:N:X:Y:Z:n"
-words = ["help","run=","lumi=","schema=","tag=","folder=","adc=","gain=","print","module=","opt=","chan=","begin=","end=","tree","val=","ind=","part=","modmin=","modmax=","chmin=","chmax=","gmin=","gmax=","norm"]
+letters = "hr:l:s:t:f:a:g:b:e:o:Pp:Tv:i:m:c:I:A:N:X:Y:Z:C:n"
+words = ["help","run=","lumi=","schema=","tag=","folder=","adc=","gain=","print","module=","opt=","chan=","begin=","end=","tree","val=","ind=","part=","modmin=","modmax=","chmin=","chmax=","gmin=","gmax=","cut=","norm"]
 try:
     options,args = getopt.getopt(sys.argv[1:],letters,words)
 except getopt.GetoptError, err:
@@ -53,7 +54,7 @@ except getopt.GetoptError, err:
 
 
 
-# defaults 
+# defaults
 schema = 'COOLOFL_TILE/CONDBR2'
 folderPath =  "/TILE/OFL02/CALIB/CIS/LIN"
 tag = "UPD4"
@@ -63,7 +64,7 @@ vsbin = False
 plotopt= "dist"
 save_tree = False
 print_msg = False
-opt2d = False 
+opt2d = False
 partname = "LBA"
 modulename = "LBA01"
 modmin=0
@@ -71,7 +72,7 @@ modmax=9999
 chanmin=0
 chanmax=9999
 gainmin=0
-gainmax=1
+gainmax=0
 mod_n = 0
 chan_n = 0
 gain_n = 0
@@ -83,6 +84,7 @@ lumiNum = 0
 one_run = False
 multi = False
 norm = False
+cut = None
 
 for o, a in options:
     if o in ("-f","--folder"):
@@ -114,30 +116,34 @@ for o, a in options:
         val_n = int(a)
     elif o in ("-p","--part"):
         partname = a
-        opt2d = True
+        modulename = partname+"00"
     elif o in ("-I","--modmin"):
         modmin = int(a)
     elif o in ("-A","--modmax"):
         modmax = int(a)
     elif o in ("-N","--chmin"):
         chanmin = int(a)
+        chan_n = chanmin
     elif o in ("-X","--chmax"):
         chanmax = int(a)
     elif o in ("-Y","--gmin"):
         gainmin = int(a)
+        gain_n = gainmin
     elif o in ("-Z","--gmax"):
         gainmax = int(a)
     elif o in ("-o","--opt"):
         plotopt = a
+    elif o in ("-C","--cut"):
+        cut = a
     elif o in ("-T","--tree"):
-        save_tree = True 
+        save_tree = True
     elif o in ("-P","--print"):
-        print_msg = True 
+        print_msg = True
     elif o in ("-n","--norm"):
-        norm = True 
+        norm = True
     elif o in ("-h","--help"):
         usage()
-        sys.exit(2) 
+        sys.exit(2)
     else:
         assert False, "unhandeled option"
 
@@ -152,15 +158,18 @@ elif plotopt == "2d": opt2d = True; one_run = True
 chan_n=max(0,chan_n)
 gain_n=max(0,gain_n)
 val_n=max(0,val_n)
+modmin=max(1,modmin)
+modmax=min(64,modmax)
+chanmin=max(0,chanmin)
+chanmax=min(47,chanmax)
+gainmin=max(0,gainmin)
+gainmax=min(5,gainmax) # up to 6 gains for integrators
+nval=1
 
-if opt2d: 
+if opt2d:
     partname=partname[:3]
     modulename=partname+"00"
-    modmin=max(1,modmin)
-    modmax=min(64,modmax)
-    chanmin=max(0,chanmin)
-    chanmax=min(47,chanmax)
-else: 
+else:
     if len(modulename) < 5 or len(modulename) > 5:
         print "Wrong module name:",modulename
         sys.exit(2)
@@ -168,20 +177,15 @@ else:
     modnum=modulename[3:]
     if modnum.isdigit():
         mod_n=int(modnum)
-        if mod_n<1 or mod_n>64:
+        if mod_n<0 or mod_n>64:
             print "Wrong module name:",modulename
             sys.exit(2)
-        modmin=mod_n
-        modmax=mod_n
+        elif mod_n>0:
+            modmin=mod_n
+            modmax=mod_n
     else:
         print "Wrong module name:",modulename
         sys.exit(2)
-    if plotopt != "print":
-        chanmin=chan_n
-        chanmax=chan_n
-if plotopt != "print":
-    gainmin=gain_n
-    gainmax=gain_n
 
 part_dict = {'AUX':0,'LBA':1,'LBC':2,'EBA':3,'EBC':4,'ALL':5}
 if partname in part_dict:
@@ -193,14 +197,49 @@ else:
     else: print "Wrong module name:",modulename
     sys.exit(2)
 
+many=False
+if modmin!=modmax:
+    many=True
+    modulename="%s%2.2d-%s%2.2d" % (partname,modmin,partname,modmax)
+else:
+    mod_n=modmin
+    modulename="%s%2.2d" % (partname,mod_n)
+
+if chanmin!=chanmax:
+    many=True
+    channame="%2.2d-%2.2d" % (chanmin,chanmax)
+else:
+    chan_n=chanmin
+    channame="%2.2d" % (chan_n)
+
+if gainmin!=gainmax:
+    many=True
+    gain_n = -1
+
 if one_run and plotopt == "dist":
     print "Switching to noline mode"
     noline = True
     plotopt = "noline"
 
+if many:
+    many=(line or noline)
+
+if not many and gain_n==-1:
+    gain_n=gainmin
+
+if gain_n == -1:
+    gainname = "AllG"
+elif gain_n == 0:
+    gainname = "LG"
+elif gain_n == 1:
+    gainname = "HG"
+else:
+    gainname = "G"+str(gain_n)
+if val_n>0: gainname += " val[%d]" % (val_n)
+
 
 import ROOT
-from ROOT import TCanvas, TH1D, TH2D, TGraph, TTree
+from ROOT import TCanvas, TH1D, TH2D, TGraph, TTree, TLegend
 from ROOT import gROOT
 from ROOT import kTRUE
 from array import array
@@ -225,6 +264,10 @@ for fp in folderPath:
     log.info("Initializing folder %s with tag %s" % (fp, ft))
     folderTag += [ft]
 multi=(len(folderPath)>1)
+if multi and many:
+    print "Can not calculate product of "+" ".join(folderPath)+" in multi-channel mode"
+    sys.exit(2)
+
 
 #=== Get list of IOVs for given drawer or for comments record
 if ros!=-1:
@@ -331,7 +374,7 @@ if begin != be or end != en:
         end=en
     iovList=iovList[ib:ie]
 
-if one_run: 
+if one_run:
     if opt2d and not (line or noline):
         iovList=iovList[:1]
         print "Plotting values for (Run,Lumi) = (%i,%i) " % (runNum,lumiNum)
@@ -348,8 +391,8 @@ if one_run:
         print iovList
         if not line and runNum!=iovList[-1][0][0]:
             iovList=iovList[:-1]
-            
-if not one_run: 
+
+if not one_run:
     print "Plotting Runs: %i to %i" % (begin, end)
     print len(iovList),"different IOVs in total"
     if iovList[0][0][0]!=begin or iovList[-1][0][0]!=end:
@@ -370,13 +413,14 @@ titsuff=""
 if one_run or opt2d:
     if lumiNum<=0:
         titsuff=" Run " + str(runNum)
-    else: 
+    else:
         titsuff=" Run " + str(runNum) + " LB " + str(lumiNum)
 titsuff+=" Tag " + " ".join(folderTag)
 rlt=titsuff.replace(" ","_").replace("_Tag","")
+gtitle = modulename +" " + " Channel" +("s " if (chanmin!=chanmax) else " ") + channame +" " + gainname + titsuff
 
 if opt2d: fname="%s_g%d_v%d%s_%s" % ( partname,gain_n,val_n,rlt,plotopt)
-else: fname="%s_ch%2.2d_g%d_v%d%s_%s" % ( modulename,chan_n,gain_n,val_n,rlt,plotopt) 
+else: fname="%s_ch%s_g%d_v%d%s_%s" % ( modulename,channame,gain_n,val_n,rlt,plotopt)
 
 if save_tree: tree_file = ROOT.TFile(fname+".root","RECREATE")
 
@@ -385,6 +429,7 @@ run_n = np.zeros(1, dtype = float)
 lumi_n = np.zeros(1, dtype = float)
 channel_n = np.zeros(1, dtype = float)
 module_n = np.zeros(1, dtype = int)
+ros_n = np.zeros(1, dtype = int)
 value = np.zeros(1, dtype = float)
 scale = np.zeros(1, dtype = float)
 
@@ -392,6 +437,7 @@ tree.Branch("runnumber",run_n,'runnumber/D')
 tree.Branch("luminum",lumi_n,'luminum/D')
 tree.Branch("channel",channel_n,'channel/D')
 tree.Branch("module",module_n,'module/I')
+tree.Branch("ros",ros_n,'ros/I')
 tree.Branch("labels", labels)
 tree.Branch("vals", vals)
 if multi: tree.Branch("value", value,'value/D')
@@ -407,7 +453,7 @@ labels.clear()
 one_iov = (len(iovList)<2)
 
 for iovs in iovList:
-        
+
     if one_iov:
         run = runNum
         lumi = lumiNum
@@ -431,8 +477,9 @@ for iovs in iovList:
         labels.clear()
         vals.clear()
         value[0]=1.
-    
+
     for ros in xrange(rosmin,rosmax):
+        ros_n[0] = ros
         for mod in xrange(modmin-1, min(modmax,TileCalibUtils.getMaxDrawer(ros))):
             module_n[0] = mod
             modName = TileCalibUtils.getDrawerString(ros,mod)
@@ -453,7 +500,7 @@ for iovs in iovList:
                                   vals.push_back(flt.getData(chn, adc, val))
                                   if val==0:
                                       value[0] *= flt.getData(chn, adc, val)
-                              if print_msg: 
+                              if print_msg:
                                   if multi:
                                       print fp,msg
                                   else:
@@ -467,7 +514,7 @@ for iovs in iovList:
                                       scale[0]=vals[val_n]
                                   first=False
                               tree.Fill()
-                              if not line:
+                              if not line or many:
                                   labels.clear()
                                   vals.clear()
                                   value[0] = 1.
@@ -513,23 +560,22 @@ ROOT.gStyle.SetOptStat(0)
 if one_run:
     cut_cond = "runnumber == " + str(runNum)
 else:
-    cut_cond = "runnumber >= " + str(begin) + " && " + "runnumber <= " + str(veryEnd) 
-
-##### Which gain to choose
-if gain_n == 0:
-    gainvalue = "LG"
-elif gain_n == 1:
-    gainvalue = "HG"
-else:
-    gainvalue = "G"+str(gain_n)
+    cut_cond = "runnumber >= " + str(begin) + " && " + "runnumber <= " + str(veryEnd)
 
 ##### Which value to choose
 if multi:
-  data = "value"
+    data = "value"
+elif many:
+    data = "vals[%d]"
 else:
-  data = "vals[" + str(val_n) + "]"
+    data = "vals[" + str(val_n) + "]"
+
+if cut:
+    if not "val" in cut: cut = data + cut
+    cut_cond = "(" + cut_cond + ") && (" + cut + ")"
+
 if norm:
-  data += "/scale"
+    data += "/scale"
 
 if (line or noline) and not one_run:
     data += ":runnumber"
@@ -543,43 +589,112 @@ elif one_run:
 
 canv = TCanvas("PlotCalib","plotCalib",0,0,1600,800)
 
-if opt2d: 
+if opt2d:
     hhh = TH2D("hhh","hhh",modmax-modmin+1,modmin-0.5,modmax+0.5,chanmax-chanmin+1,chanmin-0.5,chanmax+0.5)
 
-if vsbin: tree.Draw(data,cut_cond,"goff",15)
-else: tree.Draw(data,cut_cond,"goff")
+if not many and not opt2d:
+    print "Plotting",data
+    print "With cut",cut_cond
+    if vsbin: tree.Draw(data,cut_cond,"goff",15)
+    else: tree.Draw(data,cut_cond,"goff")
 
-if (not opt2d and tree.GetSelectedRows() <= 1) or tree.GetSelectedRows() <= 0:
-    print "Not enough points to plot"
-    sys.exit(2)
+    if tree.GetSelectedRows() <= 0:
+        print "Not enough points to plot"
+        sys.exit(2)
 
-if vsbin:
-    if tree.GetSelectedRows() >= 15:
-        print "Maximum number of bins is 15"
-
+    if vsbin:
+        if tree.GetSelectedRows() >= 15:
+            print "Maximum number of bins is 15"
 
 if line or noline:
-    gr = TGraph(tree.GetSelectedRows(),tree.GetV2(),tree.GetV1())
-    gr.SetTitle(modulename +" " + " Channel "+ str(chan_n) +" " + gainvalue + titsuff)
-    gr.SetMarkerStyle(20)
-    gr.SetMarkerSize(1.3)
-    gr.SetMarkerColor(2)
-    gr.SetLineColor(2)
-    gr.GetXaxis().SetNoExponent(kTRUE)
-    if one_run: gr.GetXaxis().SetTitle("Lumi")
-    else: gr.GetXaxis().SetTitle("Runs")
-    if noline: gr.Draw("ap")
-    else: gr.Draw("apl")
+    if many:
+        first=True
+        color=1
+        grall=[]
+        vmin=1.e+38
+        vmax=-1.e+38
+        cut0 = cut_cond
+        data1 = data
+        leg = TLegend(0.7,0.7,0.9,0.9)
+        for ros in xrange(rosmin,rosmax):
+            for mod in xrange(modmin-1, modmax):
+                for chn in xrange(chanmin,chanmax+1):
+                    for adc in xrange(gainmin,gainmax+1):
+                        if "%d" in data:
+                            data1 = data.replace("%d",str((adc-gainmin)*nval+val_n))
+                        if "%d" in cut_cond:
+                            cut0 = cut_cond.replace("%d",str((adc-gainmin)*nval+val_n))
+                        cut1 = "(ros == %d && module == %d && channel == %d) && %s" % (ros,mod,chn,cut0)
+                        tree.Project("htemp",data1,cut1)
+                        if tree.GetSelectedRows() > 0:
+                            color = color + 1
+                            gr = TGraph(tree.GetSelectedRows(),tree.GetV2(),tree.GetV1())
+                            yy=gr.GetY()
+                            for ii in xrange(len(yy)):
+                                vv=yy[ii]
+                                if vv<vmin: vmin=vv
+                                if vv>vmax: vmax=vv
+                            grall += [gr]
+                            leg.AddEntry(gr,"%s%2.2d ch %2.2d" % ( part_dict.keys()[part_dict.values().index(ros)],mod+1,chn),"lep")
+                            gr.SetMarkerStyle(20)
+                            gr.SetMarkerSize(1.3)
+                            gr.SetMarkerColor(color)
+                            gr.SetLineColor(color)
+                            if first:
+                                print "First plot:"
+                                print "Plotting",data1
+                                print "With cut",cut1
+                                print "Note that in TTree module number starts from 0"
+                                gr.SetTitle(gtitle)
+                                gr.GetXaxis().SetNoExponent(kTRUE)
+                                if one_run: gr.GetXaxis().SetTitle("Lumi")
+                                else: gr.GetXaxis().SetTitle("Runs")
+                                if noline: gr.Draw("ap")
+                                else: gr.Draw("apl")
+                                first=False
+                            else:
+                                if noline: gr.Draw("p")
+                                else: gr.Draw("pl")
+        lg=len(grall)
+        if lg==0:
+            print "Plotting",data
+            print "With cut",cut_cond
+            print "Not enough points to plot"
+            sys.exit(2)
+        if lg>400: nc=20
+        elif lg>144: nc=int(math.sqrt(lg))
+        else: nc=int((lg-1)/12+1)
+        leg.SetNColumns(nc)
+        extra = (2+0.4*int(lg/nc+1))
+        dv=(vmax-vmin)/10.
+        grall[0].SetMinimum(vmin-dv)
+        grall[0].SetMaximum(vmax+dv*extra)
+        if nc>8: nc=8
+        leg.SetX1(0.9-0.1*nc)
+        leg.SetY1(0.9-0.8*(extra-1)/(11+extra))
+        leg.Draw()
+    else:
+        gr = TGraph(tree.GetSelectedRows(),tree.GetV2(),tree.GetV1())
+        gr.SetTitle(gtitle)
+        gr.SetMarkerStyle(20)
+        gr.SetMarkerSize(1.3)
+        gr.SetMarkerColor(2)
+        gr.SetLineColor(2)
+        gr.GetXaxis().SetNoExponent(kTRUE)
+        if one_run: gr.GetXaxis().SetTitle("Lumi")
+        else: gr.GetXaxis().SetTitle("Runs")
+        if noline: gr.Draw("ap")
+        else: gr.Draw("apl")
 
 elif opt2d:
     nentries = tree.GetEntries()
     hhh.SetName(modulename)
-    hhh.SetTitle(partname +" " + gainvalue + titsuff)
+    hhh.SetTitle(gtitle)
     hhh.GetYaxis().SetTitle("Channels")
     hhh.GetXaxis().SetTitle("Modules")
 
     if multi:
-        Matrix = [[1 for y in range(48)] for x in range(64)] 
+        Matrix = [[1 for y in range(48)] for x in range(64)]
         for i in xrange(nentries):
             tree.GetEntry(i)
             if vals.size()>0:
@@ -604,7 +719,7 @@ elif opt2d:
 else:
     h = ROOT.gDirectory.Get("htemp")
     h.SetName(modulename)
-    h.SetTitle(modulename +" " + " Channel "+str(chan_n)  + " " + gainvalue + titsuff)
+    h.SetTitle(gtitle)
     h.SetLineColor(2)
     h.SetLineWidth(2)
     h.GetXaxis().SetTitle("Value")
@@ -613,7 +728,7 @@ else:
         h.SetMarkerSize(1.4)
         h.SetMarkerColor(2)
         h.GetXaxis().SetTitle("Run")
-        if one_run: h.GetXaxis().SetTitle("Lumi");  h.SetTitle(modulename +" " + " Channel "+str(chan_n) +" " + gainvalue + titsuff)
+        if one_run: h.GetXaxis().SetTitle("Lumi");  h.SetTitle(gtitle)
         h.GetYaxis().SetTitle("")
         h.GetYaxis().SetTitleOffset(1.35)
     try:
@@ -631,13 +746,13 @@ canv.Update()
 canv.Print(fname+".eps")
 canv.Print(fname+".png")
 
-if save_tree: 
+if save_tree:
     tree_file.Write()
     tree_file.Close()
 
 
-        #=== display plot
-os.system("display "+fname+".png") 
+#=== display plot
+os.system("display "+fname+".png")
 
 
 #
