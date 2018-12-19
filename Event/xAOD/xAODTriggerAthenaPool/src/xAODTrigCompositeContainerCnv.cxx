ea3cd5d4b9a3df9cd7cecec5b157fc6d7e2e4585
scott snyder <snyder@bnl.gov>
2018-12-19 15:06:56 +0100

    xAODTriggerAthenaPool: Add proper pool coverters fro xAODTrigCompositeContainer.
    Needed to fix test failures in TrigUpgradeTest.

---
+++ Event/xAOD/xAODTriggerAthenaPool/src/xAODTrigCompositeContainerCnv.cxx
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration.
+ */
+/**
+ * @file xAODTriggerAthenaPool/xAODTrigCompositeContainer.cxx
+ * @author scott snyder <snyder@bnl.gov>
+ * @date Dec, 2018
+ * @brief Dummy source file so that cmake will know this is a custom converter.
+ */
