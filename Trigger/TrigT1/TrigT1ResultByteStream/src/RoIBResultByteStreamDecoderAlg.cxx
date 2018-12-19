b37da8271e39c5a28336c003b0f977064387fde7
Rafal Bielski <rafal.bielski@cern.ch>
2018-12-19 23:57:08 +0100

    Add RoIBResult decoder alg and restructure the tool
    Added RoIBResultByteStreamDecoderAlg which should replace the Converter
    call to decode RoIBResult from BS.
    
    Restructured RoIBResultByteStreamTool:
    - new, non-template convert method for decoding to be used by the alg
    - old template convert method only converts the args and calls the new method
    - conversion from RoIBResult to ByteStream (BS encoding) left untouched
    - removed RoIBResultByteStreamTool.icc as the new non-template method is implemented in .cxx
    - the new method does identical job as the old one, but is rearranged for better readability and maintainability

---
+++ Trigger/TrigT1/TrigT1ResultByteStream/src/RoIBResultByteStreamDecoderAlg.cxx
@@ -0,0 +1,51 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "RoIBResultByteStreamDecoderAlg.h"
+
+// =============================================================================
+// Standard constructor
+// =============================================================================
+RoIBResultByteStreamDecoderAlg::RoIBResultByteStreamDecoderAlg(const std::string& name, ISvcLocator* svcLoc)
+: AthReentrantAlgorithm(name, svcLoc),
+  m_robDataProviderSvc("ROBDataProviderSvc", name) {}
+
+// =============================================================================
+// Implementation of AthReentrantAlgorithm::initialize
+// =============================================================================
+StatusCode RoIBResultByteStreamDecoderAlg::initialize() {
+  ATH_MSG_DEBUG("Initialising " << name());
+  ATH_CHECK(m_roibResultWHKey.initialize());
+  ATH_CHECK(m_robDataProviderSvc.retrieve());
+
+  return StatusCode::SUCCESS;
+}
+
+// =============================================================================
+// Implementation of AthReentrantAlgorithm::finalize
+// =============================================================================
+StatusCode RoIBResultByteStreamDecoderAlg::finalize() {
+  ATH_MSG_DEBUG("Finalising " << name());
+  ATH_CHECK(m_robDataProviderSvc.release());
+  return StatusCode::SUCCESS;
+}
+
+// =============================================================================
+// Implementation of AthReentrantAlgorithm::execute
+// =============================================================================
+StatusCode RoIBResultByteStreamDecoderAlg::execute(const EventContext& eventContext) const {
+  ATH_MSG_DEBUG("Executing " << name());
+
+  // Create and record the RoIBResult object
+  auto roibResult = SG::makeHandle(m_roibResultWHKey,eventContext);
+  ATH_CHECK( roibResult.record(std::make_unique<ROIB::RoIBResult>()) );
+  ATH_MSG_DEBUG("Recorded RoIBResult with key " << m_roibResultWHKey.key());
+
+  // Retrieve the BS data and fill the RoIBResult
+  IROBDataProviderSvc::VROBFRAG vrobf; // vector of ROBFragments to be filled
+  m_robDataProviderSvc->getROBData(eventContext, m_robIdsToDecode, vrobf, name());
+  ATH_CHECK(m_decoderTool->convert(vrobf, *roibResult));
+
+  return StatusCode::SUCCESS;
+}
