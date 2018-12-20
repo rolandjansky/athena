1502a25770dfe7a932861d4d200c850ae31a7c17
Siarhei Harkusha <Siarhei.Harkusha@cern.ch>
2018-12-20 14:01:54 +0100

    TileRecUtils: Decrease dependency on TileInfo
    TileInfo is used to return any detector-dependent constants
    or information regarding the Tile Calorimeter,
    including conditions data via different conditions tools. I
    t is not good for AthenaMT.
    
    So functionality of TileInfo which is provided via tools
    will be removed and clients will be migrated to use tools directly.
    It is supposed that TileInfo will keep and provide only constants
    which will be initialized during initialization of job.
    
    TileRecUtils package has been migrated to use tools directly.

--- TileCalorimeter/TileRecUtils/src/TileRawChannelBuilderOpt2Filter.cxx
+++ TileCalorimeter/TileRecUtils/src/TileRawChannelBuilderOpt2Filter.cxx
@@ -157,11 +157,13 @@ StatusCode TileRawChannelBuilderOpt2Filter::initialize() {
 
   if (m_bestPhase) {
     //=== get TileToolTiming
+    // TileToolTiming can be disabled in the TileRawChannelBuilder
+    if (!m_tileToolTiming.isEnabled()) {
+      m_tileToolTiming.enable();
+    }
     ATH_CHECK( m_tileToolTiming.retrieve() );
-  } else {
-    m_tileToolTiming.disable();
   }
-  
+
   ATH_MSG_INFO( "initialization completed" );
 
   return StatusCode::SUCCESS;
@@ -208,10 +210,13 @@ TileRawChannel * TileRawChannelBuilderOpt2Filter::rawChannel(const TileDigits* d
   int ros = m_tileHWID->ros(adcId);
   int drawer = m_tileHWID->drawer(adcId);
   int channel = m_tileHWID->channel(adcId);
+
   chi2 = filter(ros, drawer, channel, gain, pedestal, energy, time);
+
+  unsigned int drawerIdx = TileCalibUtils::getDrawerIdx(ros, drawer);
   
   if (m_calibrateEnergy) {
-    energy = m_tileInfo->CisCalib(adcId, energy);
+    energy = m_tileToolEmscale->doCalibCis(drawerIdx, channel, gain, energy);
   }
   
   if (msgLvl(MSG::VERBOSE)) {
@@ -244,7 +249,9 @@ TileRawChannel * TileRawChannelBuilderOpt2Filter::rawChannel(const TileDigits* d
           && time < m_maxTime
           && time > m_minTime)) {
 
-    rawCh->insertTime(m_tileInfo->TimeCalib(adcId, time));
+    time -= m_tileToolTiming->getSignalPhase(drawerIdx, channel, gain);
+    rawCh->insertTime(time);
+
     ATH_MSG_VERBOSE( "Correcting time, new time=" << rawCh->time() );
 
   }
