4d79f4750a67974604b8b72a466b302821c94a5c
Tadej Novak <tadej.novak@cern.ch>
2018-12-20 14:58:20 +0100

    Minor PRW algorithm improvements

--- PhysicsAnalysis/Algorithms/AsgAnalysisAlgorithms/Root/PileupReweightingAlg.cxx
+++ PhysicsAnalysis/Algorithms/AsgAnalysisAlgorithms/Root/PileupReweightingAlg.cxx
@@ -24,6 +24,9 @@ namespace CP
     , m_pileupReweightingTool ("CP::PileupReweightingTool", this)
   {
     declareProperty ("pileupReweightingTool", m_pileupReweightingTool, "the pileup reweighting tool we apply");
+    declareProperty ("correctedScaledAverageMuDecoration", m_correctedScaledAverageMuDecoration, "the decoration for the corrected and scaled average interactions per crossing");
+    declareProperty ("correctedActualMuDecoration", m_correctedActualMuDecoration, "the decoration for the corrected actual interactions per crossing");
+    declareProperty ("correctedScaledActualMuDecoration", m_correctedScaledActualMuDecoration, "the decoration for the corrected and scaled actual interactions per crossing");
   }
 
 
@@ -31,6 +34,19 @@ namespace CP
   StatusCode PileupReweightingAlg ::
   initialize ()
   {
+    if (!m_correctedScaledAverageMuDecoration.empty())
+    {
+      m_correctedScaledAverageMuAccessor = std::make_unique<SG::AuxElement::Accessor<float> > (m_correctedScaledAverageMuDecoration);
+    }
+    if (!m_correctedActualMuDecoration.empty())
+    {
+      m_correctedActualMuAccessor = std::make_unique<SG::AuxElement::Accessor<float> > (m_correctedActualMuDecoration);
+    }
+    if (!m_correctedScaledActualMuDecoration.empty())
+    {
+      m_correctedScaledActualMuAccessor = std::make_unique<SG::AuxElement::Accessor<float> > (m_correctedScaledActualMuDecoration);
+    }
+
     ANA_CHECK (m_pileupReweightingTool.retrieve());
     m_systematicsList.addHandle (m_eventInfoHandle);
     ANA_CHECK (m_systematicsList.addAffectingSystematics (m_pileupReweightingTool->affectingSystematics()));
@@ -50,6 +66,25 @@ namespace CP
         ANA_CHECK (m_eventInfoHandle.getCopy (eventInfo, sys));
 
         ATH_CHECK (m_pileupReweightingTool->apply (*eventInfo));
+
+        // Add additional decorations
+        if (m_correctedScaledAverageMuAccessor)
+        {
+          (*m_correctedScaledAverageMuAccessor) (*eventInfo)
+            = m_pileupReweightingTool->getCorrectedAverageInteractionsPerCrossing (*eventInfo, true);
+        }
+
+        if (m_correctedActualMuAccessor)
+        {
+          (*m_correctedActualMuAccessor) (*eventInfo)
+            = m_pileupReweightingTool->getCorrectedActualInteractionsPerCrossing (*eventInfo);
+        }
+
+        if (m_correctedScaledActualMuAccessor)
+        {
+          (*m_correctedScaledActualMuAccessor) (*eventInfo)
+            = m_pileupReweightingTool->getCorrectedActualInteractionsPerCrossing (*eventInfo, true);
+        }
 
         //--- PRWHash to recalculate PU weights using analysis ntuples
         //--- https://twiki.cern.ch/twiki/bin/view/AtlasProtected/ExtendedPileupReweighting#Using_PRWHash_to_change_pileup_w
