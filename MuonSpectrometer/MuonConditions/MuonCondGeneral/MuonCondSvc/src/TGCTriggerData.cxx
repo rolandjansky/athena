3e5a7106ea334568db47c2f2d1c15f1ccd059be2
Tomoe Kishimoto <tomoe.kishimoto@cern.ch>
2019-01-08 08:16:36 +0900

    Modified to use the correct copyright.

--- MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondSvc/src/TGCTriggerData.cxx
+++ MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondSvc/src/TGCTriggerData.cxx
@@ -1,6 +1,6 @@
 /*
- *   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
- */
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+*/
 
 #include "MuonCondSvc/TGCTriggerData.h"
 #include "AthenaBaseComps/AthMessaging.h"
--- MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondSvc/src/TGCTriggerDbAlg.cxx
+++ MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondSvc/src/TGCTriggerDbAlg.cxx
@@ -1,6 +1,6 @@
 /*
- *   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
- */
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
 
 #include "MuonCondSvc/TGCTriggerDbAlg.h"
 #include "StoreGate/ReadCondHandle.h"
