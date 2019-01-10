44a641dcbd9c6595557a7579a3d86b8e03b08f19
scott snyder <sss@karma>
2019-01-10 16:42:05 +0100

    TrigMultiVarHypo: Fix warnings.
    Warnings:
      Catching exceptions by value.
      Obsolete Gaudi header file.

--- Trigger/TrigHypothesis/TrigMultiVarHypo/src/components/TrigMultiVarHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigMultiVarHypo/src/components/TrigMultiVarHypo_entries.cxx
@@ -1,7 +1,6 @@
 /*
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
-#include "GaudiKernel/DeclareFactoryEntries.h"
 #include "TrigMultiVarHypo/TrigL2CaloRingerFex.h"
 #include "TrigMultiVarHypo/TrigL2CaloRingerHypo.h"
 #include "../TrigL2CaloRingerFexMT.h"
