f22608784b695febe3f7347b5602b18b117540cc
Matthew Henry Klein <matthew.henry.klein@cern.ch>
2019-01-11 13:51:24 +0100

    Adds qg tagger to HIGG5D3

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5D3ExtraContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG5D3ExtraContent.py
@@ -8,7 +8,8 @@ ExtraContent=HIGG5Common.filterContentList('egammaClusters|Muons|TauJets|AntiKt4
 ExtraContent+=[
     "Photons.f3core",
 #    "AntiKt4EMPFlowJets.btaggingLink",
-    "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets.HbbScore.KtDR.Dip12.ZCut12.ThrustMin.ThrustMaj.Angularity.Aplanarity.Sphericity.PlanarFlow.FoxWolfram2.FoxWolfram0"
+    "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets.HbbScore.KtDR.Dip12.ZCut12.ThrustMin.ThrustMaj.Angularity.Aplanarity.Sphericity.PlanarFlow.FoxWolfram2.FoxWolfram0",
+    "AntiKt4EMTopoJets.DFCommonJets_QGTagger_NTracks.DFCommonJets_QGTagger_NTracks.DFCommonJets_QGTagger_TracksWidth.DFCommonJets_QGTagger_TracksC1"
     ]
 
 ExtraContentTruth=HIGG5Common.getHIGG5CommonTruth()
