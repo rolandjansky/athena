c0a181e3f765c93255bebde27635642ed7d2f3bc
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2019-01-11 22:56:32 +0000

    Adjust histogram ranges, to reflect the fact fractions can be slightly larger than 1.0. Remove debugging code left in by mistake.

--- Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/PFOCalibHitClusterMomentPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFOHistUtils/src/PFOCalibHitClusterMomentPlots.cxx
@@ -30,25 +30,25 @@ namespace PFO {
 
   void PFOCalibHitClusterMomentPlots::initializePlots(){
 
-    m_PFO_ENG_CALIB_FRAC_EM = Book1D("_ENG_CALIB_FRAC_EM",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_HAD = Book1D("_ENG_CALIB_FRAC_HAD",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_REST = Book1D("_ENG_CALIB_FRAC_REST",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST",20,0,1);
+    m_PFO_ENG_CALIB_FRAC_EM = Book1D("_ENG_CALIB_FRAC_EM",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_HAD = Book1D("_ENG_CALIB_FRAC_HAD",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_REST = Book1D("_ENG_CALIB_FRAC_REST",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST",22,0,1.1);
     
-    m_PFO_ENG_CALIB_FRAC_EM_etaBinA = Book1D("_ENG_CALIB_FRAC_EM_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (|eta| < 1.5)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_HAD_etaBinA = Book1D("_ENG_CALIB_FRAC_HAD_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (|eta| < 1.5)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_REST_etaBinA = Book1D("_ENG_CALIB_FRAC_REST_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (|eta| < 1.5)",20,0,1);
+    m_PFO_ENG_CALIB_FRAC_EM_etaBinA = Book1D("_ENG_CALIB_FRAC_EM_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (|eta| < 1.5)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_HAD_etaBinA = Book1D("_ENG_CALIB_FRAC_HAD_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (|eta| < 1.5)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_REST_etaBinA = Book1D("_ENG_CALIB_FRAC_REST_A",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (|eta| < 1.5)",22,0,1.1);
     
-    m_PFO_ENG_CALIB_FRAC_EM_etaBinB = Book1D("_ENG_CALIB_FRAC_EM_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (1.5 <= |eta| < 2.5)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_HAD_etaBinB = Book1D("_ENG_CALIB_FRAC_HAD_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (1.5 <= |eta| < 2.5)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_REST_etaBinB = Book1D("_ENG_CALIB_FRAC_REST_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (1.5 <= |eta| < 2.5)",20,0,1);
+    m_PFO_ENG_CALIB_FRAC_EM_etaBinB = Book1D("_ENG_CALIB_FRAC_EM_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (1.5 <= |eta| < 2.5)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_HAD_etaBinB = Book1D("_ENG_CALIB_FRAC_HAD_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (1.5 <= |eta| < 2.5)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_REST_etaBinB = Book1D("_ENG_CALIB_FRAC_REST_B",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (1.5 <= |eta| < 2.5)",22,0,1.1);
     
-    m_PFO_ENG_CALIB_FRAC_EM_etaBinC = Book1D("_ENG_CALIB_FRAC_EM_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (2.5 <= |eta| < 3.2)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_HAD_etaBinC = Book1D("_ENG_CALIB_FRAC_HAD_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (2.5 <= |eta| < 3.2)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_REST_etaBinC = Book1D("_ENG_CALIB_FRAC_REST_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (2.5 <= |eta| < 3.2)",20,0,1);
+    m_PFO_ENG_CALIB_FRAC_EM_etaBinC = Book1D("_ENG_CALIB_FRAC_EM_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (2.5 <= |eta| < 3.2)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_HAD_etaBinC = Book1D("_ENG_CALIB_FRAC_HAD_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (2.5 <= |eta| < 3.2)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_REST_etaBinC = Book1D("_ENG_CALIB_FRAC_REST_C",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (2.5 <= |eta| < 3.2)",22,0,1.1);
 
-    m_PFO_ENG_CALIB_FRAC_EM_etaBinD = Book1D("_ENG_CALIB_FRAC_EM_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (|eta| >= 3.2)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_HAD_etaBinD = Book1D("_ENG_CALIB_FRAC_HAD_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (|eta| >= 3.2)",20,0,1);
-    m_PFO_ENG_CALIB_FRAC_REST_etaBinD = Book1D("_ENG_CALIB_FRAC_REST_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (|eta| >= 3.2)",20,0,1);
+    m_PFO_ENG_CALIB_FRAC_EM_etaBinD = Book1D("_ENG_CALIB_FRAC_EM_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_EM (|eta| >= 3.2)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_HAD_etaBinD = Book1D("_ENG_CALIB_FRAC_HAD_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_HAD (|eta| >= 3.2)",22,0,1.1);
+    m_PFO_ENG_CALIB_FRAC_REST_etaBinD = Book1D("_ENG_CALIB_FRAC_REST_D",m_sPFOContainerName + "_ENG_CALIB_FRAC_REST (|eta| >= 3.2)",22,0,1.1);
     
   }
 
@@ -57,11 +57,6 @@ namespace PFO {
     float moment_ENG_CALIB_FRAC_EM = -1.0;
     PFO.getClusterMoment(moment_ENG_CALIB_FRAC_EM,xAOD::CaloCluster::ENG_CALIB_FRAC_EM);
     m_PFO_ENG_CALIB_FRAC_EM->Fill(moment_ENG_CALIB_FRAC_EM);
-    
-    std::cout << "MARK: frac is " << moment_ENG_CALIB_FRAC_EM << std::endl;
-    float test = -1.0;
-    bool testBool = PFO.getClusterMoment(test,xAOD::CaloCluster::ENG_CALIB_TOT);
-    std::cout << "MARK: tot is " << test << " and return value was " << testBool << std::endl;
     
     float moment_ENG_CALIB_FRAC_HAD = -1.0;
     PFO.getClusterMoment(moment_ENG_CALIB_FRAC_HAD,xAOD::CaloCluster::ENG_CALIB_FRAC_HAD);
