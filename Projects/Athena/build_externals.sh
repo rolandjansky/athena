19c2a828becf4babc0a53a94cabb20a9472701fe
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2019-01-15 15:49:49 +0100

    Forcing Athena to use LCG_94.
    The latest version of atlasexternals now defaults to using LCG_94a. But until
    the tdaq releases are built against 94a, Athena needs to stay with 94.

--- Projects/Athena/build_externals.sh
+++ Projects/Athena/build_externals.sh
@@ -22,7 +22,7 @@ BUILDDIR=""
 BUILDTYPE="RelWithDebInfo"
 FORCE=""
 CI=""
-EXTRACMAKE=()
+EXTRACMAKE=(-DLCG_VERSION_NUMBER=94 -DLCG_VERSION_POSTFIX="")
 while getopts ":t:b:x:fch" opt; do
     case $opt in
         t)
