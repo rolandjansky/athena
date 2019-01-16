74bcdf2d0a3635dc11362e7cab5fadbaddcf89ed
Michiel Jan Veen <michiel.jan.veen@cern.ch>
2019-01-16 13:17:27 +0100

    Integrating NSW matching algorithm in finalise of NSWPRDTest

---
+++ MuonSpectrometer/MuonValidation/MuonPRDTest/src/EDM_object.h
@@ -0,0 +1,67 @@
+/*
+  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef EDM_OBJECT_H
+#define EDM_OBJECT_H
+
+#include "TString.h"
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class EDM_object
+{
+public:
+	// Matching
+	void init_matching();
+	bool update_match(int index, int ch_candidate);
+	bool identifierMatch(EDM_object &data0, EDM_object &data1, uint i, uint j);
+
+	// Efficiency	
+	void update_efficiency ( int maximum_difference );
+	void printEfficiency (ofstream& file);
+
+	// setters
+	void setDataAdress (EDM_object &oData, TString branch_name);
+	void setName (TString name) { m_name = name; }
+	void setName (TString name, TString detector) { m_name = name; m_detector = detector; }
+	void setDetector (TString detector) { m_detector = detector; }
+	void setMatchedwith (TString matchedwith) { m_matchedwith = matchedwith; }
+
+	// getters 
+	TString getName () { return m_name; }
+	TString getDetector () { return m_detector; }
+	TString getMatchedwith () { return m_matchedwith; }
+
+	// Helper functions
+	size_t size ();
+	void checksize ();
+	void printInfo();
+	void clearVars();
+	bool empty ();
+
+	EDM_object ();
+	~EDM_object () { clearVars(); }
+
+	vector<string>  *m_stationName;
+	vector<int>     *m_stationEta;
+	vector<int>     *m_stationPhi;
+	vector<int>     *m_multiplet;
+	vector<int>     *m_gas_gap;
+	vector<int>     *m_channel_type;
+	vector<int>     *m_channel;
+
+	vector<int>     *m_matchedchannel;
+
+private:
+	TString m_name;
+	TString m_matchedwith;
+	TString m_detector;
+	int m_mismatches;
+	int m_total;
+};
+
+#endif
--- MuonSpectrometer/MuonValidation/MuonPRDTest/src/NSWPRDValAlg.cxx
+++ MuonSpectrometer/MuonValidation/MuonPRDTest/src/NSWPRDValAlg.cxx
@@ -2,6 +2,9 @@
   Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
+#include <fstream>
+
+// NSWValAlg inlcudes
 #include "NSWPRDValAlg.h"
 
 #include "MMDigitVariables.h"
@@ -21,17 +24,22 @@
 #include "MuEntryVariables.h"
 #include "TruthVariables.h"
 
-#include "GaudiKernel/ITHistSvc.h"
-#include "TTree.h"
+#include "EDM_object.h"
 
+// Other NSW includes
 #include "MuonReadoutGeometry/MuonDetectorManager.h"
-
 #include "MuonIdHelpers/MmIdHelper.h"
 #include "MuonIdHelpers/sTgcIdHelper.h"
 #include "MuonIdHelpers/CscIdHelper.h"
 
+// ROOT includes
+#include "TTree.h"
+
+// Misc includes
+#include "GaudiKernel/ITHistSvc.h"
 #include "EventInfo/EventInfo.h"
 #include "EventInfo/EventID.h"
+
 
 NSWPRDValAlg::NSWPRDValAlg(const std::string& name, ISvcLocator* pSvcLocator)
   : AthAlgorithm(name, pSvcLocator),
@@ -45,7 +53,7 @@ NSWPRDValAlg::NSWPRDValAlg(const std::string& name, ISvcLocator* pSvcLocator)
     m_sTgcPrdVar(nullptr),
     m_MmSimHitVar(nullptr),
     m_MmSdoVar(nullptr),
-	 m_MmFastSdoVar(nullptr),
+    m_MmFastSdoVar(nullptr),
     m_MmDigitVar(nullptr),
     m_MmRdoVar(nullptr),
     m_MmPrdVar(nullptr),
@@ -59,6 +67,7 @@ NSWPRDValAlg::NSWPRDValAlg(const std::string& name, ISvcLocator* pSvcLocator)
     m_runNumber(0),
     m_eventNumber(0)
 {
+  // Input properties: Container names
   declareProperty("Truth_ContainerName",            m_Truth_ContainerName="TruthEvent");
   declareProperty("MuonEntryLayer_ContainerName",   m_MuEntry_ContainerName="MuonEntryLayer");
   declareProperty("NSWsTGC_ContainerName",          m_NSWsTGC_ContainerName="sTGCSensitiveDetector");
@@ -73,6 +82,7 @@ NSWPRDValAlg::NSWPRDValAlg(const std::string& name, ISvcLocator* pSvcLocator)
   declareProperty("NSWMM_PRDContainerName",         m_NSWMM_PRDContainerName="MM_Measurements");
   declareProperty("CSC_DigitContainerName",         m_CSC_DigitContainerName="CSC_DIGITS");
 
+  // Input properties: do EDM objects
   declareProperty("doTruth",         m_doTruth=false);
   declareProperty("doMuEntry",       m_doMuEntry=false);
   declareProperty("doSTGCHit",       m_doSTGCHit=false);
@@ -86,6 +96,11 @@ NSWPRDValAlg::NSWPRDValAlg(const std::string& name, ISvcLocator* pSvcLocator)
   declareProperty("doMMRDO",         m_doMMRDO=false);
   declareProperty("doMMPRD",         m_doMMPRD=false);
   declareProperty("doCSCDigit",      m_doCSCDigit=false);
+
+  // Input properties: NSW Maching algorithm
+  declareProperty("doNSWMatchingAlg",   m_doNSWMatching=true);
+  declareProperty("doNSWMatchingMuonOnly",  m_doNSWMatchingMuon=false);
+  declareProperty("setMaxStripDistance",  m_maxStripDiff=3);
 }
 
 StatusCode NSWPRDValAlg::initialize() {
@@ -210,7 +225,8 @@ StatusCode NSWPRDValAlg::initialize() {
 
 StatusCode NSWPRDValAlg::finalize()
 {
-  ATH_MSG_INFO("finalize()");
+  ATH_MSG_DEBUG("NSWPRDValAlg:: Finalize + Matching");
+  if (m_doNSWMatching) { ATH_CHECK ( NSWMatchingAlg() );} 
 
   if (m_TruthVar) { delete m_TruthVar; m_TruthVar=0;}
   if (m_doMuEntry) { delete m_MuEntryVar; m_MuEntryVar=0;}
@@ -233,7 +249,7 @@ StatusCode NSWPRDValAlg::finalize()
 
 StatusCode NSWPRDValAlg::execute()
 {
-  ATH_MSG_INFO("execute()");
+  ATH_MSG_DEBUG("execute()");
 
   // Event information
   const EventInfo* pevt(0);
@@ -278,3 +294,195 @@ StatusCode NSWPRDValAlg::execute()
   return StatusCode::SUCCESS;
 }
 
+/*****************************************************************************************************************************************
+The rest of this file is the NSW matching algorithm. This can be (de)activated with the input variable doNSWMatchingAlg. 
+The matching algorithm will check each conversion in the Hit->PRD digitization chain.
+For each conversion, the algorithm will try to match the two objects. 
+The input variable setMaxStripDistance (default value = 3) give the maximum strip distance two objects can have, while still be considered a match
+The input variable doNSWMatchingMuonOnly can be set to true to only use events which contain exclusively muons 
+******************************************************************************************************************************************/
+
+// First set up which object should be matched, given the input used to fill the NSW Ntuple
+StatusCode NSWPRDValAlg::NSWMatchingAlg () {
+
+  ATH_MSG_DEBUG("NSWMatchingAlg: building Data objects");
+  
+  // Use the EDM object, as defined in EDM_object.h, to build the hits, digits, SDO, RDO and PRD for both sTGC and MM
+  EDM_object Hits_sTGC, Digits_sTGC, SDO_sTGC, RDO_sTGC, PRD_sTGC;
+  EDM_object Hits_MM, Digits_MM, SDO_MM, RDO_MM, PRD_MM;
+
+  Hits_sTGC.setName("Hits", "sTGC");
+  Digits_sTGC.setName("Digits", "sTGC");
+  SDO_sTGC.setName("SDO", "sTGC");
+  RDO_sTGC.setName("RDO", "sTGC");
+  PRD_sTGC.setName("PRD", "sTGC");
+
+  Hits_MM.setName("Hits", "MM");
+  Digits_MM.setName("Digits", "MM");
+  SDO_MM.setName("SDO", "MM");
+  RDO_MM.setName("RDO", "MM");
+  PRD_MM.setName("PRD", "MM");
+
+  // Match the EDM objects with the variables in the NSW Validation Ntuple
+  TString branch_name;
+  TObjArray* brlst = m_tree->GetListOfBranches();
+  for (TBranch *branch = (TBranch*) brlst->First(); branch; branch = (TBranch*) brlst->After(branch)) {
+    branch_name = branch->GetName(); 
+    ATH_MSG_VERBOSE("About to check branch: " << branch_name);
+
+    ATH_CHECK( setDataAdress(Hits_sTGC, branch_name) ); 
+    ATH_CHECK( setDataAdress(Digits_sTGC, branch_name) ); 
+    ATH_CHECK( setDataAdress(SDO_sTGC, branch_name) ); 
+    ATH_CHECK( setDataAdress(RDO_sTGC, branch_name) ); 
+    ATH_CHECK( setDataAdress(PRD_sTGC, branch_name) ); 
+
+    ATH_CHECK( setDataAdress(Hits_MM, branch_name) ); 
+    ATH_CHECK( setDataAdress(Digits_MM, branch_name) ); 
+    ATH_CHECK( setDataAdress(SDO_MM, branch_name) ); 
+    ATH_CHECK( setDataAdress(RDO_MM, branch_name) ); 
+    ATH_CHECK( setDataAdress(PRD_MM, branch_name) ); 
+  }
+
+  // Prepare the output file
+  ofstream efficiencies;
+  efficiencies.open("NSWMatchingAlg_efficiencies.txt");
+  efficiencies << "NSW Matching algorithm, efficiencies of conversion from and to various EDM objects" << endl;
+  efficiencies << "Settings:\n doNSWMatchingMuonOnly = " << doNSWMatchingMuonOnly << endl;
+  efficiencies << "Maximum Strip Distance = " << setMaxStripDistance << endl;
+  efficiencies.close();
+
+  // sTGC matching
+  if (m_doSTGCHit && m_doSTGCDigit) { 
+    ATH_CHECK( NSWMatchingAlg(Hits_sTGC, Digits_sTGC) ); 
+    ATH_CHECK( NSWMatchingAlg(Hits_sTGC, SDO_sTGC) );
+  }
+  if (m_doSTGCDigit && m_doSTGCRDO) { 
+    ATH_CHECK( NSWMatchingAlg(Digits_sTGC, RDO_sTGC) );
+    ATH_CHECK( NSWMatchingAlg(SDO_sTGC, RDO_sTGC) );
+  }
+  if (m_doSTGCRDO && m_doSTGCPRD) { NSWMatchingAlg(RDO_sTGC, PRD_sTGC); }
+
+  // sTGC fast digitization
+  if (m_doSTGCHit && m_doSTGCFastDigit) { 
+    ATH_CHECK( NSWMatchingAlg(Hits_sTGC, PRD_sTGC) );
+    ATH_CHECK( NSWMatchingAlg(Hits_sTGC, SDO_sTGC) );
+    ATH_CHECK( NSWMatchingAlg(SDO_sTGC, PRD_sTGC) );
+  }
+
+  // MM matching
+  if (m_doMMHit && m_doMMDigit) { 
+    ATH_CHECK( NSWMatchingAlg(Hits_MM, Digits_MM) );
+    ATH_CHECK( NSWMatchingAlg(Hits_MM, SDO_MM) );
+  }
+  if (m_doMMDigit && m_doMMRDO) { 
+    ATH_CHECK( NSWMatchingAlg(Digits_MM, RDO_MM) );
+    ATH_CHECK( NSWMatchingAlg(SDO_MM, RDO_MM) );
+  }
+  if (m_doMMRDO && m_doMMPRD) { NSWMatchingAlg(RDO_MM, PRD_MM); }
+
+  //  MM fast digitization
+  if (m_doMMHit && m_doMMFastDigit) { 
+    ATH_CHECK( NSWMatchingAlg(Hits_MM, PRD_MM) );
+    ATH_CHECK( NSWMatchingAlg(Hits_MM, SDO_MM) );
+    ATH_CHECK( NSWMatchingAlg(SDO_MM, PRD_MM) );
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+// This part of the matching algortihm does the actual comparison given two EDM obects
+StatusCode NSWPRDValAlg::NSWMatchingAlg (EDM_object data0, EDM_object data1) {
+  if (data0.getDetector() != data1.getDetector()) 
+    { ATH_MSG_ERROR ("Matching " << data0.getDetector() << " data with " << data1.getDetector() << " data. This is not implemented in this algorithm"); }
+  ATH_MSG_INFO("NSWMatchingAlg: Start matching " << data0.getName() << " and " << data1.getName() << " for " << data0.getDetector());
+
+  // Prepare Muon ony check
+  vector<int>* TruthParticle_Pdg;
+  if ( doNSWMatchingMuonOnly ) { m_tree->SetBranchAddress("TruthParticle_Pdg", &TruthParticle_Pdg); }
+
+  Long64_t nEntries = m_tree->GetEntriesFast();
+  for (Long64_t i_entry = 0; i_entry < nEntries; ++i_entry) {
+    // event numbering starts at 1
+    ATH_MSG_DEBUG("Now checking event number " << i_entry + 1);
+    m_tree->GetEntry(i_entry);
+    
+    if (data0.empty()) { ATH_MSG_WARNING("No " << data0.getDetector() << data0.getName() << " found in event " << i_entry + 1); continue; }
+    if (data1.empty()) { ATH_MSG_WARNING("No " << data1.getDetector() << data1.getName() << " found in event " << i_entry + 1); continue; }
+
+    ATH_MSG_DEBUG("Number of " << data0.getDetector() << data0.getName() << ": " << data0.size() << 
+                  ", number of " << data1.getDetector() << data1.getName() << ": " << data1.size());
+
+  //only muon events:
+  if (doNSWMatchingMuonOnly) {
+    bool allMu = true;
+    int mu_pdg = 13;
+    for ( int pdg : *TruthParticle_Pdg ) { allMu &= (abs(pdg) == mu_pdg); }
+    if ( !allMu ) { ATH_MSG_VERBOSE("Skipping event, because doNSWMatchingMuonOnly is true and there non muon particles"); continue; }
+  }
+
+  // Reset variables to have a clean sheet
+    data0.init_matching();
+    data1.init_matching();
+
+    // Actual Matching
+    for (uint i = 0; i < data0.size(); ++i)
+     {
+      int nMatch = 0;
+        for (uint j = 0; j < data1.size(); ++j)
+        {
+           if ( data0.identifierMatch(data0, data1, i, j) )
+           {
+              nMatch++;
+              data0.update_match(i, data1.m_channel->at(j));
+              data1.update_match(j, data0.m_channel->at(i));
+           }
+        }
+        ATH_MSG_VERBOSE("Total Number of matches found: " << nMatch << data1.getName() << " for a single " << data0.getName() );
+        if (nMatch == 0) {
+          ATH_MSG_WARNING("No match found!");
+        }
+     }
+    if (msgLevel() <= MSG::DEBUG) { 
+      ATH_MSG_DEBUG("Full info for " << data0.getName() << data0.getDetector());
+      data0.printInfo(); 
+      ATH_MSG_DEBUG("Full info for " << data1.getName() << data1.getDetector());
+      data1.printInfo(); 
+    }
+    
+    data0.update_efficiency(m_maxStripDiff);
+    data1.update_efficiency(m_maxStripDiff);
+
+  // The data0 vs data1 matching will now be overwritten
+  data0.clearVars();
+  data1.clearVars();
+  }
+
+  // Write result to file
+  ofstream efficiencies; 
+  efficiencies.open("NSWMatchingAlg_efficiencies.txt", ofstream::app);
+  data0.printEfficiency(efficiencies);
+  data1.printEfficiency(efficiencies);
+  efficiencies.close();
+
+  return StatusCode::SUCCESS;
+
+}
+
+// This function couples the branch of the NSW validation Ntuple with the EDM object. 
+// In case the brach does not exist, or is completely empty, the object will remain empty and the code will skip the matching entirly, giving a ATH_WARNING in the process
+StatusCode NSWPRDValAlg::setDataAdress (EDM_object &oData, TString branch_name) {
+  bool setBranch = false;
+  if (!(branch_name.Contains(oData.getName()) && branch_name.Contains(oData.getDetector()))) { return StatusCode::SUCCESS; }
+  // For sim hits select the offline identifiers, rather than the sim identifiers
+  if (branch_name.Contains("_sim_")) { return StatusCode::SUCCESS; }
+  if (branch_name.EndsWith("stationName")) { m_tree->SetBranchAddress(branch_name, &oData.m_stationName); setBranch = true; }
+  if (branch_name.EndsWith("stationEta")) { m_tree->SetBranchAddress(branch_name, &oData.m_stationEta); setBranch = true; }
+  if (branch_name.EndsWith("stationPhi")) { m_tree->SetBranchAddress(branch_name, &oData.m_stationPhi); setBranch = true; }
+  if (branch_name.EndsWith("multiplet")) { m_tree->SetBranchAddress(branch_name, &oData.m_multiplet); setBranch = true; }
+  if (branch_name.EndsWith("gas_gap")) { m_tree->SetBranchAddress(branch_name, &oData.m_gas_gap); setBranch = true; }
+  if (branch_name.EndsWith("channel")) { m_tree->SetBranchAddress(branch_name, &oData.m_channel); setBranch = true; }
+  if (branch_name.EndsWith("channel_type")) { m_tree->SetBranchAddress(branch_name, &oData.m_channel_type); setBranch = true; }
+  if (setBranch) { ATH_MSG_DEBUG("Set data adress of branch " << branch_name); }
+  
+  return StatusCode::SUCCESS;
+}
