add662c4bddb9fc745700ed0566a29ad06aeee3f
David Rousseau <rousseau@lal.in2p3.fr>
2019-01-21 15:11:50 +0100

    read json file with PathResolver

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimServices/src/DNNCaloSimSvc.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimServices/src/DNNCaloSimSvc.cxx
@@ -88,8 +88,7 @@ StatusCode ISF::DNNCaloSimSvc::initialize()
   m_caloGeo->LoadGeometryFromCaloDDM(m_caloDetDescrManager);
   if(!m_caloGeo->LoadFCalChannelMapFromFCalDDM(fcalManager) )ATH_MSG_FATAL("Found inconsistency between FCal_Channel map and GEO file. Please, check if they are configured properly.");
   
-  const std::string fileName = m_paramsFilename;
-  std::string inputFile=PathResolverFindCalibFile(fileName);
+
 
   // FIXME should open the jason file here
 
@@ -223,23 +222,7 @@ StatusCode ISF::DNNCaloSimSvc::simulate(const ISF::ISFParticle& isfp)
   m_FastCaloSimCaloExtrapolation->extrapolate(extrapol,&truth);
   ATH_MSG_DEBUG(" calo surface eta " << extrapol.CaloSurface_eta() <<
 		" phi " << extrapol.CaloSurface_phi() );
-  extrapol.Print();
- // namespace CaloCell_ID_FCS {
- //   enum CaloSample {
- //     FirstSample=CaloCell_ID::PreSamplerB,
- //     PreSamplerB=CaloCell_ID::PreSamplerB, EMB1=CaloCell_ID::EMB1, EMB2=CaloCell_ID::EMB2, EMB3=CaloCell_ID::EMB3,       // LAr barrel
- //     PreSamplerE=CaloCell_ID::PreSamplerE, EME1=CaloCell_ID::EME1, EME2=CaloCell_ID::EME2, EME3=CaloCell_ID::EME3,       // LAr EM endcap
- //     HEC0=CaloCell_ID::HEC0, HEC1=CaloCell_ID::HEC1, HEC2=CaloCell_ID::HEC2, HEC3=CaloCell_ID::HEC3,      // Hadronic end cap cal.
- //     TileBar0=CaloCell_ID::TileBar0, TileBar1=CaloCell_ID::TileBar1, TileBar2=CaloCell_ID::TileBar2,      // Tile barrel
- //     TileGap1=CaloCell_ID::TileGap1, TileGap2=CaloCell_ID::TileGap2, TileGap3=CaloCell_ID::TileGap3,      // Tile gap (ITC & scint)
- //     TileExt0=CaloCell_ID::TileExt0, TileExt1=CaloCell_ID::TileExt1, TileExt2=CaloCell_ID::TileExt2,      // Tile extended barrel
- //     FCAL0=CaloCell_ID::FCAL0, FCAL1=CaloCell_ID::FCAL1, FCAL2=CaloCell_ID::FCAL2,                        // Forward EM endcap
- 
- //     // Beware of MiniFCAL! We don't have it, so different numbers after FCAL2
-     
- //     LastSample = CaloCell_ID::FCAL2,
- //     MaxSample = LastSample+1,
- //     noSample = -1
+  //  extrapol.Print();
  
 
   if (false)
@@ -301,19 +284,8 @@ StatusCode ISF::DNNCaloSimSvc::simulate(const ISF::ISFParticle& isfp)
 
 
 
-
+  //FIXME remove ?
   TFCSSimulationState simulstate(m_randomEngine);
-
-
-  
-  ATH_MSG_DEBUG ("DNNCaloSim only debugging!");
-  //  m_param->setLevel(MSG::DEBUG);
-  // compute simulstate, simulated cells and a few more goodies
-  // FIXME run the DNN HERE
-  //  FCSReturnCode status = m_param->simulate(simulstate, &truth, &extrapol);
-  // if (status != FCSSuccess) {
-  //  return StatusCode::FAILURE;
-  // }
 
 
   // note that m_theCellContainer has all the calorimeter cells
@@ -377,11 +349,22 @@ StatusCode ISF::DNNCaloSimSvc::simulate(const ISF::ISFParticle& isfp)
 
 
   ATH_MSG_DEBUG("NNN total cells passing cuts     " << n_sqCuts << "        eta phi raw" << vall << "and" << valll<< "sampling" << sampling);
-  //ATH_MSG_DEBUG("NNN total cells passing cuts" << n_sqCuts );
+
 
   ATH_MSG_INFO("start neural network part");
+
+  // FIXME do once, what can be done once
   // get neural net JSON file as an std::istream object
-  std::ifstream input(m_paramsFilename);
+  std::string inputFile=PathResolverFindDataFile(m_paramsFilename);
+  if (inputFile==""){
+    ATH_MSG_ERROR("Could not find json file " << m_paramsFilename );
+    return StatusCode::FAILURE;
+  } 
+
+  ATH_MSG_DEBUG("Using json file " << m_paramsFilename );
+  
+
+  std::ifstream input(inputFile);
   // build the graph
   lwt::LightweightGraph graph(lwt::parse_json_graph(input));
 
@@ -409,7 +392,7 @@ StatusCode ISF::DNNCaloSimSvc::simulate(const ISF::ISFParticle& isfp)
     }
   // compute the output values
   std::map<std::string, double> outputs = graph.compute(inputs);
-  ATH_MSG_INFO("neural network output = "<<outputs);
+  ATH_MSG_DEBUG("neural network output = "<<outputs);
 
 
   std::vector<CaloCell*>::iterator windowCell;
