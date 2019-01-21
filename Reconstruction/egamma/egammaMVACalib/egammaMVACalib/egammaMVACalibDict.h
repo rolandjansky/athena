84e03bb17e9f44ac8d2d54d4fa6578eaf8e950a3
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2019-01-21 12:28:28 +0000

    Add AnalysisBase CI test for egammaMVACalib

--- Reconstruction/egamma/egammaMVACalib/egammaMVACalib/egammaMVACalibDict.h
+++ Reconstruction/egamma/egammaMVACalib/egammaMVACalib/egammaMVACalibDict.h
@@ -7,18 +7,5 @@
 #include "egammaMVACalib/egammaMVACalib.h"
 #include "egammaMVACalib/IegammaMVATool.h"
 #include "egammaMVACalib/egammaMVATool.h"
-//#include "egammaMVACalib/egammaMVATree.h"
-#include <vector>
-
-/*
-namespace egammaMVACalibDict
-{
-  struct tmp
-  {
-    std::vector<egammaMVACalibNmsp::Node*> tmp1;
-  };
-
-} // namespace egammaMVACalibDict
-*/
 
 #endif // EGAMMAMVACALIB_EGAMMAMVACALIBDICT_H
