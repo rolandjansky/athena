dc50d0dd29de2c27bf8b6e0e08c926473e6bfefc
Amber Roepe <amber.rebecca.roepe@cern.ch>
2019-01-23 15:19:16 +0100

    VHLowTrackJetFilter addition to DRAWRPVLL stream

---
+++ PhysicsAnalysis/SUSYPhys/LongLivedParticleDPDMaker/share/PhysDESDM_VH.py
@@ -0,0 +1,46 @@
+import AthenaCommon.SystemOfUnits as Units
+
+
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__xAODStringSkimmingTool
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__TriggerSkimmingTool
+from DerivationFrameworkCore.DerivationFrameworkCoreConf import DerivationFramework__DerivationKernel
+from DerivationFrameworkTools.DerivationFrameworkToolsConf import DerivationFramework__FilterCombinationAND
+
+def VHTrkTriggerFilterString(flags):
+    selectionString = ''
+    if flags.triggerNames:
+        triggers = list(set(flags.triggerNames))
+        selectionString += '(' + ' || '.join(triggers) + ')'
+
+    return selectionString
+
+
+
+VHJetTriggerFilterTool = DerivationFramework__TriggerSkimmingTool( name = "VHJetTriggerFilterTool",
+                                                                   TriggerListOR = primRPVLLDESDM.VHLowTrackJet_FilterFlags.triggerNames)
+
+ToolSvc += VHJetTriggerFilterTool
+
+
+from LongLivedParticleDPDMaker.LongLivedParticleDPDMakerConf import DerivationFramework__VHLowTrackJetFilterTool
+
+VHLowTrackJetFilterTool=DerivationFramework__VHLowTrackJetFilterTool(name="VHLowTrackJetFilterTool",
+                                                                     Debug         =primRPVLLDESDM.VHLowTrackJet_FilterFlags.debug,
+                                                                     JetPtCut      =primRPVLLDESDM.VHLowTrackJet_FilterFlags.cutJet ,
+                                                                     JetEtaCut     =primRPVLLDESDM.VHLowTrackJet_FilterFlags.cutJetEta,
+                                                                     NJetsRequired =primRPVLLDESDM.VHLowTrackJet_FilterFlags.nJets ,
+                                                                     ElectronPtCut =primRPVLLDESDM.VHLowTrackJet_FilterFlags.cutEl ,
+                                                                     MuonPtCut     =primRPVLLDESDM.VHLowTrackJet_FilterFlags.cutMu )
+
+ToolSvc += VHLowTrackJetFilterTool
+
+VHLTrkJetFilterTool=DerivationFramework__FilterCombinationAND(name="VHLTrkJetFilterTool",
+                                                              FilterList=[VHJetTriggerFilterTool,VHLowTrackJetFilterTool],
+                                                              OutputLevel=INFO)
+
+ToolSvc += VHLTrkJetFilterTool
+
+topSequence += DerivationFramework__DerivationKernel( "RPVLL_VHLowTrackJetFilterKernel",
+                       SkimmingTools = [VHLTrkJetFilterTool] )
+
+RPVLLfilterNames.extend(["RPVLL_VHLowTrackJetFilterKernel"])
