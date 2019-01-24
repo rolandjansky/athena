2da38ac7e03ee4a506d8daf6080d7d8cbf2fc717
Oleg Zenin <ozenin@lxplus137.cern.ch>
2019-01-24 07:48:24 +0100

    printing timestamp with the full list of arguments in a safe way (ATLINFR-2731)

--- Projects/AthSimulation/build.sh
+++ Projects/AthSimulation/build.sh
@@ -3,7 +3,7 @@
 # Script for building the release on top of externals built using one of the
 # scripts in this directory.
 #
-_time_="/usr/bin/time -f time::\t%C::\treal:\t%E\tuser:\t%U\tsys:\t%S\n "
+_time_() { local c="time -p " ; while test "X$1" != "X" ; do c+=" \"$1\"" ; shift; done; ( eval "$c" ) 2>&1 | sed "s,^real[[:space:]],time::${c}:: real ," ; }
 
 # Function printing the usage information for the script
 usage() {
@@ -99,7 +99,7 @@ if [ -n "$EXE_CMAKE" ]; then
     # from scratch in an incremental build.
     rm -f CMakeCache.txt
     # Now run the actual CMake configuration:
-    $_time_ cmake -DCMAKE_BUILD_TYPE:STRING=${BUILDTYPE} \
+    _time_ cmake -DCMAKE_BUILD_TYPE:STRING=${BUILDTYPE} \
         -DCTEST_USE_LAUNCHERS:BOOL=TRUE \
         ${AthSimulationSrcDir} 2>&1 | tee cmake_config.log
 fi
@@ -112,17 +112,17 @@ fi
 
 # make:
 if [ -n "$EXE_MAKE" ]; then
-    $_time_ make -k 2>&1 | tee cmake_build.log
+    _time_ make -k 2>&1 | tee cmake_build.log
 fi
 
 # Install the results:
 if [ -n "$EXE_INSTALL" ]; then
-    $_time_ make install/fast \
+    _time_ make install/fast \
 	DESTDIR=${BUILDDIR}/install/AthSimulation/${NICOS_PROJECT_VERSION} 2>&1 | tee cmake_install.log
 fi
 
 # Build an RPM for the release:
 if [ -n "$EXE_CPACK" ]; then
-    $_time_ cpack 2>&1 | tee cmake_cpack.log
+    _time_ cpack 2>&1 | tee cmake_cpack.log
     cp AthSimulation*.rpm ${BUILDDIR}/
 fi
