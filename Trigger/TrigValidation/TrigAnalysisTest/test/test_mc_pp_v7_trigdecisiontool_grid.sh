30be36a9ac827449d98d3ff3be551fc375f69a00
Joerg Stelzer <joerg.stelzer@cern.ch>
2019-01-25 17:18:19 +0000

    21.0 fix nightly test2

--- Trigger/TrigValidation/TrigAnalysisTest/test/test_mc_pp_v7_trigdecisiontool_grid.sh
+++ Trigger/TrigValidation/TrigAnalysisTest/test/test_mc_pp_v7_trigdecisiontool_grid.sh
@@ -22,7 +22,7 @@
 # art-output: ntuple.pmon.gz
 # art-output: *perfmon*
 # art-output: TotalEventsProcessed.txt
-# art-output: *.regtest
+# art-output: *.regtest.new
 
 export NAME="mc_pp_v7_trigdecisiontool_grid"
 export JOB_LOG="athena.log"
