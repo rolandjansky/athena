051e89dbf5cd995bb54a8eb8149c1593cf378fdf
cranshaw <Jack.Cranshaw@cern.ch>
2019-01-30 11:48:43 -0600

    Update copyright statements to 2019

--- Event/xAOD/xAODEventFormatCnv/src/EventFormatSvc.cxx
+++ Event/xAOD/xAODEventFormatCnv/src/EventFormatSvc.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 // $Id: EventFormatSvc.cxx 777715 2016-10-11 16:35:49Z ssnyder $
