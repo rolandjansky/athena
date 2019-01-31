f578e4d8b0c550a63afa2eb33328728e444c650c
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2019-01-31 14:43:55 +0100

    update versions for Pythia8 to 8.240p1                     EvtGen to 1.6.0                     Rivet to 2.6.2                     YODA to 1.7.4

--- Projects/AthGeneration/externals/YODA.cmake
+++ Projects/AthGeneration/externals/YODA.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of YODA to use.
 #
 
-set( YODA_VERSION 1.7.1 )
+set( YODA_VERSION 1.7.4 )
 set( YODA_ROOT
    ${LCG_RELEASE_DIR}/MCGenerators/yoda/${YODA_VERSION}/${LCG_PLATFORM} )
