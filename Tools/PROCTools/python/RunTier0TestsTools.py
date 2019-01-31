fc2ac5fc1ceba71cff8e397fbbc8a1f31d8b79a8
John Chapman <jchapman@cern.ch>
2019-01-31 12:44:51 +0100

    Increment the reference file version for s3126-22.0

--- Tools/PROCTools/python/RunTier0TestsTools.py
+++ Tools/PROCTools/python/RunTier0TestsTools.py
@@ -21,7 +21,7 @@ ciRefFileMap = {
                 's3126-21.0'           : 'v1',
                 's3126-21.3'           : 'v1',
                 's3126-21.9'           : 'v1',
-                's3126-22.0'           : 'v1',
+                's3126-22.0'           : 'v2',
                 # OverlayTier0Test_required-test
                 'overlay-d1498-21.0'   : 'v1',
                 'overlay-d1498-22.0'   : 'v1',
