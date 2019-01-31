3d4cfbceef1d19acaf94ace87ba702d7f5020a49
TJ Khoo <khoo@cern.ch>
2019-02-01 00:41:27 +0100

    correct typos in requested FTK jet chains

--- Trigger/TriggerCommon/TriggerMenu/python/menu/MC_pp_v8.py
+++ Trigger/TriggerCommon/TriggerMenu/python/menu/MC_pp_v8.py
@@ -398,8 +398,10 @@ def setupMenu():
 
     if TriggerFlags.doFTK():
         TriggerFlags.JetSlice.signatures = TriggerFlags.JetSlice.signatures() + [
+        ['j0_perf_L1RD0_FILLED',   'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleJet', 'BW:Jet'], -1],
         ['j0_perf_ftk_L1RD0_FILLED',   'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleJet', 'BW:Jet'], -1],
         ['j0_perf_ftkrefit_L1RD0_FILLED',   'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleJet', 'BW:Jet'], -1],
+        ['j0_gsc0_boffperf_split_L1RD0_FILLED', 'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleBJet', 'BW:BJet'], -1],
         ['j0_gsc0_boffperf_split_ftk_L1RD0_FILLED', 'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleBJet', 'BW:BJet'], -1],
         ['j0_gsc0_boffperf_split_ftkrefit_L1RD0_FILLED', 'L1_RD0_FILLED', [], [PhysicsStream], ['RATE:SingleBJet', 'BW:BJet'], -1],
         ] 
