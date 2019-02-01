f453905cda20d66df89a2cc59eb922aa4a337e2f
Daniel Noel <daniel.louis.noel@cern.ch>
2019-02-01 10:14:23 +0000

    BeamEffectsCongfig rewritten to new style
    Output is the same as running the old style config. New flags added in SimulationFlags.py.
    
    Issue with "VERBOSE" output - not printing currently.

--- Simulation/BeamEffects/python/BeamEffectsAlgConfig.py
+++ Simulation/BeamEffects/python/BeamEffectsAlgConfig.py
@@ -15,6 +15,7 @@ from BeamEffects.BeamEffectsConf import Simulation__LongBeamspotVertexPositioner
 # For the Algorithm
 from BeamEffects.BeamEffectsConf import Simulation__BeamEffectsAlg
 
+
 # possible components from BeamEffectsConf
 # todo names required to copy function name? what are names used for?
 # todo add default construction options to make these potentiall useful
@@ -34,17 +35,55 @@ def makeGenEventBeamEffectBooster(name="GenEventBeamEffectBooster", **kwargs):
     # todo needs random seed, more?
     return Simulation__GenEventVertexPositioner(name, **kwargs)
 
-def makeGenEventVertexPositioner(name="GenEventVertexPositioner", **kwargs):
+def makeGenEventVertexPositioner(ConfigFlags,name="GenEventVertexPositioner", **kwargs):
     """Return a vertex positioner tool"""
     # todo needs input file(s?)
-    return Simulation__GenEventVertexPositioner(name, **kwargs)
+    #from BeamEffectsConfigFlagsTest import ConfigFlags #or have this as an argument?
+
+    result=ComponentAccumulator()
+
+    readVtxPosFromFile = ConfigFlags.Vertex.Source == "VertexOverrideFile.txt" or ConfigFlags.Vertex.Source == "VertexOverrideEventFile.txt"
+    if readVtxPosFromFile:
+        kwargs.setdefault("VertexShifters"          , [ Simulation__VertexPositionFromFile("VertexPositionFromFile") ])
+    elif ConfigFlags.Vertex.Source == "CondDB" :
+        kwargs.setdefault("VertexShifters"          , [ Simulation__VertexBeamCondPositioner('VertexBeamCondPositioner') ])
+    elif ConfigFlags.Vertex.Source == "LongBeamspotVertexPositioner":
+        kwargs.setdefault("VertexShifters"          , [ Simulation__LongBeamspotVertexPositioner('LongBeamspotVertexPositioner') ])
+
+    #for debugging
+    print(ConfigFlags.Vertex.Source)
+    print(ConfigFlags.Vertex.Source == "VertexOverrideFile.txt")
+    print(readVtxPosFromFile)
+    print(kwargs)
+    
+
+    acc, alg = makeVertexBeamCondPositioner(ConfigFlags) #pass in config flags here?
+
+    result.merge(acc)
+    return result, Simulation__GenEventVertexPositioner(name, **kwargs) #return acc, alg
 
 #--------------------------------------------------------------------------------------------------
 ## LorentzVectorGenerators
-def makeVertexBeamCondPositioner(name="VertexBeamCondPositioner", **kwargs):
+def makeVertexBeamCondPositioner(ConfigFlags,name="VertexBeamCondPositioner", **kwargs):
     """Return a conditional (? todo) vertex positioner tool"""
     # todo needs RandomSvc
-    return Simulation__VertexBeamCondPositioner(name, **kwargs)
+    #from BeamEffectsConfigFlagsTest import ConfigFlags
+    from IOVDbSvc.IOVDbSvcConfig import addFoldersSplitOnline
+    from BeamSpotConditions.BeamSpotConditionsConf import BeamSpotCondAlg
+    from RngComps.RandomServices import RNG, AthEngines
+
+    from AthenaCommon.Constants import VERBOSE
+    result = ComponentAccumulator()
+    
+    Engine = ConfigFlags.Random.Engine
+    kwargs.setdefault('RandomSvc', AthEngines[Engine])
+    
+    
+    kwargs.setdefault('OutputLevel', VERBOSE) #Add verbose output to the tool
+    
+    result.merge(addFoldersSplitOnline(ConfigFlags,"INDET","/Indet/Onl/Beampos","/Indet/Beampos", className='AthenaAttributeList'))
+    result.addCondAlgo(BeamSpotCondAlg( "BeamSpotCondAlg"))
+    return result, Simulation__VertexBeamCondPositioner(name, **kwargs)
 
 def makeVertexPositionFromFile(name="VertexPositionFromFile", **kwargs):
     """Return a vertex positioner tool"""
@@ -62,62 +101,83 @@ def makeLongBeamspotVertexPositioner(name="LongBeamspotVertexPositioner", **kwar
     return Simulation__LongBeamspotVertexPositioner(name, **kwargs)
 
 #----------------------------------------------------------------------------------------------------
-def BeamEffectsAlgCfg(configFlags, **kwargs):
+def BeamEffectsAlgCfg(ConfigFlags, **kwargs):
     """Return an accumulator and algorithm for beam effects
     
     Arguments:
-    configFlags --- todo not used
+    ConfigFlags ---
     """
     acc = ComponentAccumulator()
     alg = Simulation__BeamEffectsAlg(name="BeamEffectsAlg", **kwargs)
 
     # Set default properties (todo - check if we can remove ISFRun and InputMCEventCollection?)
-    alg.ISFRun = False
+    alg.ISFRun = False # or True?! seems to have no difference
     alg.InputMcEventCollection = "GEN_EVENT"
     alg.OutputMcEventCollection = "BeamTruthEvent"
+
+    accVertexPositioner, algVertexPositioner = makeGenEventVertexPositioner(ConfigFlags)
 
      # Set (todo) the appropriate manipulator tools
     manipulators = []
     manipulators.append(makeValidityChecker())
     manipulators.append(makeGenEventRotator())
-    manipulators.append(makeGenEventVertexPositioner())
+    manipulators.append(algVertexPositioner) 
     # manipulators.append(makeGenEventBeamEffectBooster()) # todo segmentation violation
-    # manipulators.append(makeVertexBeamCondPositioner()) # todo Callback registration failed
     # manipulators.append(makeVertexPositionFromFile()) # todo
     # manipulators.append(makeCrabKissingVertexPositioner()) # todo Callback registration failed
     # manipulators.append(makeLongBeamspotVertexPositioner()) # todo Callback registration failed
     alg.GenEventManipulators += manipulators
+
+    #merge the accumulators
+    acc.merge(accVertexPositioner)
+
     return acc, alg
 
 if __name__ == "__main__":
     from AthenaCommon.Logging import log
     from AthenaCommon.Constants import DEBUG
     from AthenaCommon.Configurable import Configurable
-    from AthenaConfiguration.AllConfigFlags import ConfigFlags
     from AthenaConfiguration.MainServicesConfig import MainServicesSerialCfg
     from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
     from OutputStreamAthenaPool.OutputStreamConfig import OutputStreamCfg
 
+
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from SimulationFlags import ConfigFlagsSimulation
+
+    ConfigFlags.join(ConfigFlagsSimulation)
     # Set up logging and config behaviour
     log.setLevel(DEBUG)
     Configurable.configurableRun3Behavior = 1
 
     import os
     inputDir = os.environ.get ('ATLAS_REFERENCE_DATA',
-                               '/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art')
-    # Provide input
+                                '/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art')
+     # Provide input
     ConfigFlags.Input.Files = [
-        inputDir + 
-        "/SimCoreTests/e_E50_eta34_49.EVNT.pool.root"
-        ]
+         inputDir + 
+         "/SimCoreTests/e_E50_eta34_49.EVNT.pool.root"
+         ]
+ 
 
     # Specify output
-    ConfigFlags.Output.HITFileName = "myHITS.pool.root"
+    ConfigFlags.Output.HITFileName = "myHITS.pool.root" #todo change the flag name in AllConfigFlags
+
+    #set the source of vertex positioning
+    #ConfigFlags.Vertex.Source = "VertexOverrideFile.txt"# Vertex.OverrideFile/Vertex.OverrideEventFile
+    ConfigFlags.Vertex.Source = "CondDB" # Vertex.FromCondD
+    #ConfigFlags.Vertex.Source = "LongBeamspotVertexPositioner"
+
+    #included to stop segmentation error - TODO see why it's failing
+    ConfigFlags.Input.isMC = True
+    ConfigFlags.IOVDb.GlobalTag = "OFLCOND-MC16-SDR-14" #conditions tag for conddb (which one to use - old one for simulation)
+    ConfigFlags.Input.RunNumber = 284500 # run test job with and without run number and 222510
+
     # Finalize 
     ConfigFlags.lock()
 
     ## Initialize a new component accumulator
-    cfg = MainServicesSerialCfg() #This syntax for storegate
+    cfg = MainServicesSerialCfg() #use this syntax for storegate
     # Add configuration to read EVNT pool file
     cfg.merge(PoolReadCfg(ConfigFlags))
 
@@ -129,8 +189,10 @@ if __name__ == "__main__":
     cfg.addEventAlgo(alg, sequenceName="AthAlgSeq")
 
     # Add configuration to write HITS pool file
-    outConfig = OutputStreamCfg(ConfigFlags, "HITS", ItemList=["McEventCollection#" + alg.OutputMcEventCollection])
-    cfg.merge (outConfig)
+    cfg.merge( OutputStreamCfg(ConfigFlags,
+     "HITS", 
+     ItemList=["McEventCollection#" + alg.OutputMcEventCollection])) #which collection in storegate gets written to output file !!!!!!!!
+
 
     cfg.getService("StoreGateSvc").Dump=True
     cfg.printConfig(withDetails=True)
