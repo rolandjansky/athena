dc0e99789a945f936e3fa96f67797677f4909bac
Johannes Josef Junggeburth <johannes.josef.junggeburth@CERN.CH>
2019-02-03 16:14:14 +0100

    Make usage of unique_ptr

--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationCondition.cxx
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationCondition.cxx
@@ -1,19 +1,14 @@
 /*
- * IsolationCondition.cxx
- *
- *  Created on: 15.06.2017
- *      Author: johannes
+ Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
  */
 
 #include "IsolationSelection/IsolationCondition.h"
 #include "AthContainers/AuxElement.h"
-
 namespace CP{
 
     IsolationCondition::IsolationCondition(const std::string& name, const std::vector<xAOD::Iso::IsolationType> &isoTypes) :
                 m_name(name),
-                m_isolationType(isoTypes),
-                m_customAcc(false),
+                m_isolationType(isoTypes),             
                 m_cutValue(-999.) {
         for (auto iso_type : m_isolationType){
             m_acc.push_back(xAOD::getIsolationAccessor(iso_type));
@@ -21,12 +16,11 @@ namespace CP{
     }
     IsolationCondition::IsolationCondition(const std::string& name, const std::vector<std::string> &isoTypes) :
                 m_name(name),
-                m_isolationType(std::vector<xAOD::Iso::IsolationType>()),
-                m_customAcc(true),
+                m_isolationType(std::vector<xAOD::Iso::IsolationType>()),              
                 m_cutValue(-999.) {
         for (auto iso_type : isoTypes){
             m_isolationType.push_back(xAOD::Iso::IsolationType(0));
-            m_acc.push_back(new SG::AuxElement::Accessor< float >( iso_type ));
+            m_acc.push_back(std::make_unique< SG::AuxElement::Accessor< float > >( iso_type ));
         }        
     }
     IsolationCondition::IsolationCondition(const std::string &name, xAOD::Iso::IsolationType isoType) :
@@ -41,7 +35,6 @@ namespace CP{
     }
             
     IsolationCondition::~IsolationCondition() {
-      if (m_customAcc) for (size_t i = 0; i < m_acc.size(); i++) delete m_acc[i];
     }
     std::string IsolationCondition::name() const{
         return m_name;
@@ -50,7 +43,7 @@ namespace CP{
         return  m_isolationType[n];
     }
     SG::AuxElement::Accessor<float>* IsolationCondition::accessor(unsigned int n) const{
-        return m_acc[n];
+        return m_acc[n].get();
     }
 }
 
