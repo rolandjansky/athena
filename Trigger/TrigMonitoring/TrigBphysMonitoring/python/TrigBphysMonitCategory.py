29c140df76004bff240f84707ec911c91970b7de
Oleg Meshkov <oleg.meshkov@cern.ch>
2019-02-04 09:44:56 +0000

    Change year in copyright

--- Trigger/TrigMonitoring/TrigBphysMonitoring/python/TrigBphysMonitCategory.py
+++ Trigger/TrigMonitoring/TrigBphysMonitoring/python/TrigBphysMonitCategory.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 monitoring_bphys = [
                     'HLT_mu11_mu6_bDimu', 
