f2f6b21d60b0431c2cdc7de68749139ab0b1dbf1
Susumu Oda <Susumu.Oda@cern.ch>
2019-02-04 03:33:47 +0100

    Use ReadHandle(Key). Remove const_cast. Inherit AthReentrantAlgorithm

--- InnerDetector/InDetPerformance/SCTExtension/SCTExtension/SCTExtensionAlg.h
+++ InnerDetector/InDetPerformance/SCTExtension/SCTExtension/SCTExtensionAlg.h
@@ -1,18 +1,23 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef SCTExtensionAlg_H
 #define SCTExtensionAlg_H
 
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
+#include "StoreGate/ReadHandleKey.h"
+#include "xAODTracking/TrackParticleContainer.h"
 
-class SCTExtensionAlg:public AthAlgorithm {
-    public:
-    SCTExtensionAlg (const std::string& name, ISvcLocator* pSvcLocator);
-    StatusCode initialize();
-    StatusCode execute();
-    StatusCode finalize();
+class SCTExtensionAlg:public AthReentrantAlgorithm {
+ public:
+  SCTExtensionAlg (const std::string& name, ISvcLocator* pSvcLocator);
+  virtual StatusCode initialize() override;
+  virtual StatusCode execute(const EventContext& ctx) const override;
+  virtual StatusCode finalize() override;
+ private:
+  SG::ReadHandleKey<xAOD::TrackParticleContainer> m_pixelTracklets{this, "pixelTracklets", "InDetPixelTrackParticles"};
+  SG::ReadHandleKey<xAOD::TrackParticleContainer> m_tracks{this, "tracks", "InDetTrackParticles"};
 };
 
 #endif
