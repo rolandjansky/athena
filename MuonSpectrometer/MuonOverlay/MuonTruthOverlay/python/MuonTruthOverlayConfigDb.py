1eb53e97cc6fbf9259063a04c5334ca7a00247c4
Tadej Novak <tadej.novak@cern.ch>
2019-02-05 15:36:54 +0100

    Add CscSimDataOverlay

--- MuonSpectrometer/MuonOverlay/MuonTruthOverlay/python/MuonTruthOverlayConfigDb.py
+++ MuonSpectrometer/MuonOverlay/MuonTruthOverlay/python/MuonTruthOverlayConfigDb.py
@@ -2,6 +2,7 @@
 
 from AthenaCommon.CfgGetter import addAlgorithm
 
+addAlgorithm("MuonTruthOverlay.MuonTruthOverlayConfig.getCscTruthOverlay", "CscTruthOverlay")
 addAlgorithm("MuonTruthOverlay.MuonTruthOverlayConfig.getMdtTruthOverlay", "MdtTruthOverlay")
 addAlgorithm("MuonTruthOverlay.MuonTruthOverlayConfig.getRpcTruthOverlay", "RpcTruthOverlay")
 addAlgorithm("MuonTruthOverlay.MuonTruthOverlayConfig.getTgcTruthOverlay", "TgcTruthOverlay")
--- MuonSpectrometer/MuonOverlay/MuonTruthOverlay/src/components/MuonTruthOverlay_entries.cxx
+++ MuonSpectrometer/MuonOverlay/MuonTruthOverlay/src/components/MuonTruthOverlay_entries.cxx
@@ -1,8 +1,11 @@
 #include <GaudiKernel/DeclareFactoryEntries.h>
+#include <MuonTruthOverlay/CscSimDataOverlay.h>
 #include <MuonTruthOverlay/MuonSimDataOverlay.h>
 
+DECLARE_ALGORITHM_FACTORY( CscSimDataOverlay )
 DECLARE_ALGORITHM_FACTORY( MuonSimDataOverlay )
 
 DECLARE_FACTORY_ENTRIES( MuonTruthOverlay ) {
+  DECLARE_ALGORITHM( CscSimDataOverlay )
   DECLARE_ALGORITHM( MuonSimDataOverlay )
 }
