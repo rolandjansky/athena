803dd7e841b86354118bacf49cc910eef5f745ff
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2019-02-05 02:07:28 +0100

    Added EventInfoCnvAlg to AtlasUnixGeneratorJob to allow for adiabatic migration of client algorithms to xAOD::EventInfo
    Migrated AthExHive to xAOD::EventInfo

--- Control/AthenaExamples/AthExHive/src/CondAlgY.cxx
+++ Control/AthenaExamples/AthExHive/src/CondAlgY.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "CondAlgY.h"
@@ -7,9 +7,6 @@
 #include "StoreGate/WriteCondHandle.h"
 
 #include "GaudiKernel/ServiceHandle.h"
-
-#include "EventInfo/EventInfo.h"
-#include "EventInfo/EventID.h"
 
 #include <thread>
 #include <chrono>
