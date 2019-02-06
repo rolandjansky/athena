bd0ec714311e93b03708ae765cd24b49f40b94fc
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-02-06 12:17:48 +0100

    Enable thread safety checker and small fixes
    Enable the thread safety checker for some of the MT-related trigger
    packages and fix a few small warnings found by it.

--- Trigger/TrigSteer/DecisionHandling/src/TrigCompositeUtils.cxx
+++ Trigger/TrigSteer/DecisionHandling/src/TrigCompositeUtils.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 #include "StoreGate/WriteHandle.h"
 #include "StoreGate/ReadHandle.h"
@@ -12,8 +12,8 @@
 
 
 
-static SG::AuxElement::Accessor< std::vector< int > >   readWriteAccessor( "decisions" );
-static SG::AuxElement::ConstAccessor< std::vector<int> > readOnlyAccessor( "decisions" );
+static const SG::AuxElement::Accessor< std::vector< int > >   readWriteAccessor( "decisions" );
+static const SG::AuxElement::ConstAccessor< std::vector<int> > readOnlyAccessor( "decisions" );
 
 namespace TrigCompositeUtils {  
 
