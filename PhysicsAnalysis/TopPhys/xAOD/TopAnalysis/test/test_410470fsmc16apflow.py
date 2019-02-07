9fad383db4745492577b0889303774a91d6e2544
Marco Vanadia <marco.vanadia@cern.ch>
2019-02-07 13:10:25 +0100

    update of input datasets for AnalysisTop ART tests

--- PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/test/test_410470fsmc16apflow.py
+++ PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/test/test_410470fsmc16apflow.py
@@ -3,7 +3,7 @@
 # art-description: DxAOD MC16a TOPQ1 FullSim 410470 PFlow Jets
 # art-type: grid
 # art-output: output.root
-# art-input: user.iconnell.410470.DAOD_TOPQ1.e6337_s3126_r9364_p3554.ART.v2
+# art-input: user.artprod.user.iconnell.410470.DAOD_TOPQ1.e6337_s3126_r9364_p3554.ART.v2
 # art-input-nfiles: 2
 # art-include: 21.2/AnalysisTop
 
