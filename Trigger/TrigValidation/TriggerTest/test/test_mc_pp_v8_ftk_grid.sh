63720d2d3e487d8140d0da013abb48b21a882c41
Julie Kirk <Julie.Kirk@cern.ch>
2019-02-11 14:35:36 +0100

    Add TriggerTest ART tests for FTK with v8 menu
    	new file:   Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v8_ftk_build.sh
    	new file:   Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v8_ftk_grid.sh

---
+++ Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v8_ftk_grid.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# art-description: MC v7 no prescales TriggerTest on FTK MC to AOD
+# art-type: grid
+# art-include: 21.1/AthenaP1
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+# art-include: 21.0-TrigMC/Athena
+# art-include: master/Athena
+# art-output: HLTChain.txt
+# art-output: HLTTE.txt
+# art-output: L1AV.txt
+# art-output: HLTconfig*.xml
+# art-output: L1Topoconfig*.xml
+# art-output: LVL1config*.xml
+# art-output: *.log
+# art-output: costMonitoring_*
+# art-output: *.root
+# art-output: ntuple.pmon.gz
+# art-output: *perfmon*
+# art-output: TotalEventsProcessed.txt
+# art-output: *.regtest.new
+
+export NAME="mc_pp_v8_ftk_grid"
+export MENU="MC_pp_v8"
+export INPUT="ftk"
+export JOBOPTIONS="TriggerTest/testCommonSliceAthenaTrigRDOtoAOD.py"
+export EXTRA="from TriggerJobOpts.TriggerFlags import TriggerFlags;TriggerFlags.doFTK=True;from RecExConfig.RecFlags import rec;rec.doFTK.set_Value_and_Lock(True);"
+
+source exec_athena_art_trigger_validation.sh
+source exec_art_triggertest_post.sh
