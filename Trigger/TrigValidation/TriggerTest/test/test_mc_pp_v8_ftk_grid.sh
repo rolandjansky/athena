bc320077ebd3f299edd260cfe8020ca3e1778120
Julie Kirk <Julie.Kirk@cern.ch>
2019-02-11 14:47:32 +0100

    Remove other branches (non 21.3) from art-include

--- Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v8_ftk_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_mc_pp_v8_ftk_grid.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# art-description: MC v7 no prescales TriggerTest on FTK MC to AOD
+# art-description: MC v8 TriggerTest on FTK MC to AOD
 # art-type: grid
-# art-include: 21.1/AthenaP1
-# art-include: 21.0/Athena
 # art-include: 21.3/Athena
-# art-include: 21.0-TrigMC/Athena
-# art-include: master/Athena
 # art-output: HLTChain.txt
 # art-output: HLTTE.txt
 # art-output: L1AV.txt
