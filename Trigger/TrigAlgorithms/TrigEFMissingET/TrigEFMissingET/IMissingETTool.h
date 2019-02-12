69eeea5f662fd9a5aca08fabdee002f41f62a1a6
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2019-02-12 15:05:21 +0100

    Migrate component flags from EFMissingETBaseTool to IMissingETTool

--- Trigger/TrigAlgorithms/TrigEFMissingET/TrigEFMissingET/IMissingETTool.h
+++ Trigger/TrigAlgorithms/TrigEFMissingET/TrigEFMissingET/IMissingETTool.h
@@ -28,6 +28,61 @@ public:
   virtual StatusCode update( xAOD::TrigMissingET *met,
 			     TrigEFMissingEtHelper *metHelper ) const = 0;
 
+ /** definition of the meaning for the component flag bits
+  **/
+ const unsigned short m_maskProcessing         = 0x0001 ; // bit  0 Component is being processed
+ const unsigned short m_maskErrBSconv          = 0x0002 ; // bit  1 Error detected in ByteStream conversion
+ const unsigned short m_maskErrMuon            = 0x0004 ; // bit  2 Unexpected no. of input muons
+ const unsigned short m_maskErrFEB             = 0x0008 ; // bit  3 Error detected in a FEB header
+ const unsigned short m_maskSkipped            = 0x0010 ; // bit  4 Component has been skipped
+ const unsigned short m_maskCompBigMEtSEtRatio = 0x0020 ; // bit  5 large value for |MET/SumET|
+ const unsigned short m_maskBadCompEnergy      = 0x0040 ; // bit  6 Component energy too large (negative or positive)
+ const unsigned short m_maskBadEnergyRatio     = 0x0080 ; // bit  7 Bad component energy ratio
+
+ const unsigned short m_maskBadCellQuality     = 0x0100 ; // bit  8 Bad cell quality detected
+ const unsigned short m_maskBadCellEnergy      = 0x0200 ; // bit  9 Bad cell energy
+ const unsigned short m_maskBadCellTime        = 0x0400 ; // bit 10 Bad cell time
+ const unsigned short m_maskNoisyEnergyRatio   = 0x0800 ; // bit 11 Bad value for BadSET/SET ratio
+ const unsigned short m_maskNoMuonTrack        = 0x1000 ; // bit 12 Got muon without associated track
+ // const unsigned short m_spare               = 0x2000 ; // bit 13 Spare
+ const unsigned short m_maskProcessed          = 0x4000 ; // bit 14 Component has been processed
+ const unsigned short m_maskCompErrors         = 0x8000 ; // bit 15 Error detected
+
+ /** definition of the meaning for the global flag highest 16 bits
+  ** (see the constructor)
+  **/
+ //    const unsigned m_maskEMB_A_Missing;          //!< EMB_A absent in DetMask
+ //    const unsigned m_maskEMB_C_Missing;          //!< EMB_C absent in DetMask
+ //    const unsigned m_maskEME_A_Missing;          //!< EME_A absent in DetMask
+ //    const unsigned m_maskEME_C_Missing;          //!< EME_C absent in DetMask
+ //    const unsigned m_maskHEC_A_Missing;          //!< HEC_A absent in DetMask
+ //    const unsigned m_maskHEC_C_Missing;          //!< HEC_C absent in DetMask
+ //    const unsigned m_maskFCAL_A_Missing;         //!< FCAL_A absent in DetMask
+ //    const unsigned m_maskFCAL_C_Missing;         //!< FCAL_C absent in DetMask
+ //    const unsigned m_maskTileB_A_Missing;        //!< TileB_A absent in DetMask
+ //    const unsigned m_maskTileB_C_Missing;        //!< TileB_C absent in DetMask
+ //    const unsigned m_maskTileE_A_Missing;        //!< TileE_A absent in DetMask
+ //    const unsigned m_maskTileE_C_Missing;        //!< TileE_C absent in DetMask
+
+ /** definition of the meaning for the global flag bits (highest ones only) **/
+ // spare                    = 0x00010000; // bit 16
+ // spare                    = 0x00020000; // bit 17
+ // spare                    = 0x00040000; // bit 18
+ // spare                    = 0x00080000; // bit 19
+ // spare                    = 0x00100000; // bit 20
+ // spare                    = 0x00200000; // bit 21
+ // spare                    = 0x00400000; // bit 22
+ // spare                    = 0x00800000; // bit 23
+ // spare                    = 0x01000000; // bit 24
+ // spare                    = 0x02000000; // bit 25
+ // spare                    = 0x04000000; // bit 26
+
+ const unsigned m_maskGlobNoisyEnergyRatio = 0x08000000; // bit 27 Bad value for BadSET/SET ratio
+ const unsigned m_maskBadEMfraction        = 0x10000000; // bit 28 Bad value for EM/tot SumE ratio
+ const unsigned m_maskGlobBigMEtSEtRatio   = 0x20000000; // bit 29 large value for |MET/SumET|
+ const unsigned m_maskObjInCrack           = 0x40000000; // bit 30 High energy in crack scintillators
+ const unsigned m_maskGlobErrors           = 0x80000000; // bit 31 Error detected 
+
 }; 
 
 
