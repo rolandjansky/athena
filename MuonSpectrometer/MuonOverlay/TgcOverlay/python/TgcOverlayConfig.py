5104a2b108e3b6c75289fedb906bd8909aa76bca
Tadej Novak <tadej.novak@cern.ch>
2019-02-12 16:37:40 +0100

    Reorganise RPC and TGC overlays

--- MuonSpectrometer/MuonOverlay/TgcOverlay/python/TgcOverlayConfig.py
+++ MuonSpectrometer/MuonOverlay/TgcOverlay/python/TgcOverlayConfig.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon import CfgMgr
 
 def getTgcOverlay(name="TgcOverlay", **kwargs):
     from OverlayCommonAlgs.OverlayFlags import overlayFlags
-    kwargs.setdefault("MainInputDigitKey",overlayFlags.dataStore()+"+TGC_DIGITS")
-    kwargs.setdefault("OverlayInputDigitKey",overlayFlags.evtStore()+"+TGC_DIGITS")
-    kwargs.setdefault("OutputDigitKey",overlayFlags.outputStore()+"+TGC_DIGITS")
-    kwargs.setdefault("MCStore",overlayFlags.evtStore())
-    kwargs.setdefault("DataStore", overlayFlags.dataStore())
+
+    kwargs.setdefault("BkgInputKey", overlayFlags.dataStore() + "+TGC_DIGITS")
+    kwargs.setdefault("SignalInputKey", overlayFlags.evtStore() + "+TGC_DIGITS")
+    kwargs.setdefault("OutputKey", overlayFlags.outputStore() + "+TGC_DIGITS")
+
     return CfgMgr.TgcOverlay(name, **kwargs)
