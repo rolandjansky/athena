bec929136400fa5457b1aa5a898b20c8d1cfa33a
Nils Krumnack <krumnack@iastate.edu>
2019-02-13 07:20:48 -0600

    update copyrights

--- PhysicsAnalysis/D3PDTools/EventLoopTest/EventLoopTest/UnitTestAlgXAOD.h
+++ PhysicsAnalysis/D3PDTools/EventLoopTest/EventLoopTest/UnitTestAlgXAOD.h
@@ -1,24 +1,12 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
+
+/// @author Nils Krumnack
+
 
 #ifndef EVENT_LOOP_UNIT_TEST_ALG_XAOD_H
 #define EVENT_LOOP_UNIT_TEST_ALG_XAOD_H
-
-//          Copyright Nils Krumnack 2011.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
-// Please feel free to contact me (krumnack@iastate.edu) for bug
-// reports, feature suggestions, praise and complaints.
-
-
-/// This module defines an algorithm used for unit tests.  The
-/// interface provided in this module is intended for experts only.
-/// The module is considered to be in the pre-alpha stage.
-
-
 
 #include <EventLoopTest/Global.h>
 
