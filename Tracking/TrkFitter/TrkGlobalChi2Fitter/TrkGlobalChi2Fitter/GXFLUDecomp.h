f4c7d23e2f7a4bcc2e7f7f15df90e32967b843be
Stephen Swatman <s.swatman@nikhef.nl>
2019-02-14 15:51:35 +0100

    Improve the style of GX2F and remove dead code
    This commit is an attempt to improve the coding style (purely in terms
    of syntax) of the GlobalChi2Fitter to make it more readable and
    maintainable. In addition, we remove a lot of dead code. Some of that is
    truth code that no longer works (or even compiles) and some is code that
    was commented out. We also modify the way the GlobalChi2Fitter
    initializer looks to make it more inline with idiomatic C++.

--- Tracking/TrkFitter/TrkGlobalChi2Fitter/TrkGlobalChi2Fitter/GXFLUDecomp.h
+++ Tracking/TrkFitter/TrkGlobalChi2Fitter/TrkGlobalChi2Fitter/GXFLUDecomp.h
@@ -2,13 +2,10 @@
    Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #ifndef GXFLUDECOMP_H
 #define GXFLUDECOMP_H
 
 #include "EventPrimitives/EventPrimitives.h"
-
-
 
 /**
    * @brief A simple class to hold the LU decomposition of a matrix/
@@ -16,20 +13,18 @@
    @author amorley@cern.ch
   */
 
-namespace Trk{
-  // Simple class that hold
-  class GXFLUDecomp{
-   public:
-    GXFLUDecomp(){}
-    void SetMatrix( Amg::SymMatrixX& );
-    Amg::VectorX Solve( Amg::VectorX&, bool& ok );
-    Amg::SymMatrixX Invert(bool& ok);
-   private:
+namespace Trk {
+  class GXFLUDecomp {
+  public:
+    GXFLUDecomp() {} 
+    void SetMatrix(Amg::SymMatrixX &);
+    Amg::VectorX Solve(Amg::VectorX &, bool &ok);
+    Amg::SymMatrixX Invert(bool &ok);
+  private:
     Amg::SymMatrixX m_matrix;
     bool m_luSet = false;
-    Eigen::LLT<Eigen::MatrixXd> m_lu;
+    Eigen::LLT < Eigen::MatrixXd > m_lu;
   };
 }
-
 
 #endif
