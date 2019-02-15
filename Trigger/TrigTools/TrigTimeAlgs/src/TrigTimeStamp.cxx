282ae270beb78581a85ebaa41e7a833c190f9631
Tim Martin <tim.martin@cern.ch>
2019-02-15 16:11:50 +0000

    Trigger Run 3 Cost Monitoring Output

--- Trigger/TrigTools/TrigTimeAlgs/src/TrigTimeStamp.cxx
+++ Trigger/TrigTools/TrigTimeAlgs/src/TrigTimeStamp.cxx
@@ -12,10 +12,8 @@ double TrigTimeStamp::millisecondsDifference(const TrigTimeStamp& other) const {
   return std::chrono::duration_cast<std::chrono::microseconds>( other.get() - m_stamp ).count();
 }
 
-std::pair<uint32_t,uint32_t> TrigTimeStamp::secondsAndMicrosecondsSinceEpoch() const {
-  // Note: A uint64_t at microsecond precision has capacity of ~292,000 years
+uint64_t TrigTimeStamp::microsecondsSinceEpoch() const {
+  // Note: A uint64_t at microsecond precision has capacity of ~585,000 years
   uint64_t microsecondsSinceEpoch = std::chrono::duration_cast<std::chrono::microseconds>( m_stamp.time_since_epoch() ).count();
-  uint64_t secondsSinceEpoch = microsecondsSinceEpoch / 1000000ull;
-  microsecondsSinceEpoch -= (secondsSinceEpoch * 1000000ull);
-  return std::make_pair(static_cast<uint32_t>(secondsSinceEpoch), static_cast<uint32_t>(microsecondsSinceEpoch));
+  return microsecondsSinceEpoch;
 }
