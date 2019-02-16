d485b468750074430b2b680ba3ebc774f8ca1eef
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2019-02-16 20:12:33 +0000

    Merge branch 'TrigP1Test_ATR-19204_fix5' into '21.3'
    TrigP1Test: fix log file names (ATR-19204)
    
    See merge request atlas/athena!21174
    
    (cherry picked from commit dc39ebe3ad52b52265c707b4bf3bb00890a8ee0a)
    
    1a212524 TrigP1Test: fix log file names (ATR-19204)

--- Trigger/TrigValidation/TrigP1Test/test/test_physics_pp_v7_PSed_nightlyDayBefore_grid.sh
+++ Trigger/TrigValidation/TrigP1Test/test/test_physics_pp_v7_PSed_nightlyDayBefore_grid.sh
@@ -18,7 +18,7 @@
 # art-output: *stdout*
 
 export NAME="athenaHLT_prescaled_PhysicsV7"
-export JOB_LOG="${NAME}.log"
+export JOB_LOG="atn_test.log"
 
 #source /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/prescaleKeys_17000.txt | tee ${JOB_LOG} 
 #for testing:
