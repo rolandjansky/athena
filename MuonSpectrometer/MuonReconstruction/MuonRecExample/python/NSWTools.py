2d080e5ae3fe4770a285a525099e6bbe0e2902e3
Stefano Rosati <stefano.rosati@cern.ch>
2019-02-18 18:28:47 +0100

    Adding configuration of the STGC clusterization tool 	modified:   MuonSpectrometer/MuonReconstruction/MuonRecExample/python/MuonPrdProviderToolsConfig.py 	modified:   MuonSpectrometer/MuonReconstruction/MuonRecExample/python/MuonRecExampleConfigDb.py 	renamed:    MuonSpectrometer/MuonReconstruction/MuonRecExample/python/MMTools.py -> MuonSpectrometer/MuonReconstruction/MuonRecExample/python/NSWTools.py 	modified:   MuonSpectrometer/MuonReconstruction/MuonRecExample/share/MuonRDO_to_PRD_jobOptions.py

--- MuonSpectrometer/MuonReconstruction/MuonRecExample/python/NSWTools.py
+++ MuonSpectrometer/MuonReconstruction/MuonRecExample/python/NSWTools.py
@@ -1,12 +1,20 @@
 # Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
-__doc__ = """Configuration of tools for MicroMegas reconstruction"""
+__doc__ = """Configuration of tools for MicroMegas and STgc reconstruction"""
 
 from AthenaCommon.Logging import logging
 logging.getLogger().info("Importing %s", __name__)
 
 from AthenaCommon import CfgMgr
 
+##
+## Micromegas Tools
+##
 def SimpleMMClusterBuilderTool(name="SimpleMMClusterBuilderTool",extraFlags=None,**kwargs):
     return CfgMgr.Muon__SimpleMMClusterBuilderTool(name,**kwargs)
-
+##
+## STgc Tools
+##
+def SimpleSTgcClusterBuilderTool(name="SimpleSTgcClusterBuilderTool",extraFlags=None,**kwargs):
+    return CfgMgr.Muon__SimpleSTgcClusterBuilderTool(name,**kwargs)
+    
