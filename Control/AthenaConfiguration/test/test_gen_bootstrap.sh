7e5861d645c0c96de226ea8b583f5bb2f25e7b1e
Stewart Martin-Haugh <smh@cern.ch>
2019-02-18 18:22:09 +0100

    rm -f to avoid warning

--- Control/AthenaConfiguration/test/test_gen_bootstrap.sh
+++ Control/AthenaConfiguration/test/test_gen_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Generate threaded and serial bootstrap pickles and diff them
 #If there's a major change in AthenaCommon or Gaudi it will get picked up here
-rm bootstrap*pkl
+rm -f bootstrap*pkl
 athena --config-only=bootstrap_test.pkl AthenaConfiguration/zeroJO.py &> /dev/null
 get_files -remove -data bootstrap.pkl
 confTool.py --diff bootstrap.pkl bootstrap_test.pkl | grep -v identical
