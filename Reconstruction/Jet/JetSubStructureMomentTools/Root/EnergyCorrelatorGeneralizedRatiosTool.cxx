0aabfaad71b4699d6b327ed50d8ae7968ae85aef
Jason Robert Veatch <jason.veatch@cern.ch>
2019-02-20 18:02:43 +0000

    JetSubStructureMomentTools: add flags to generalized ecf tools

--- Reconstruction/Jet/JetSubStructureMomentTools/Root/EnergyCorrelatorGeneralizedRatiosTool.cxx
+++ Reconstruction/Jet/JetSubStructureMomentTools/Root/EnergyCorrelatorGeneralizedRatiosTool.cxx
@@ -1,10 +1,9 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "JetSubStructureMomentTools/EnergyCorrelatorGeneralizedRatiosTool.h"
 #include "JetSubStructureUtils/EnergyCorrelatorGeneralized.h" 
-#include <math.h>
 
 using namespace std;
 using fastjet::PseudoJet;
@@ -16,12 +15,10 @@ EnergyCorrelatorGeneralizedRatiosTool::EnergyCorrelatorGeneralizedRatiosTool(std
 
 int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
 
-  
   if (!jet.isAvailable<float>(m_prefix+"ECFG_2_1") ||
       !jet.isAvailable<float>(m_prefix+"ECFG_3_2") ||
       !jet.isAvailable<float>(m_prefix+"ECFG_4_2") ||
       !jet.isAvailable<float>(m_prefix+"ECFG_3_1") ||
-      
 
       !jet.isAvailable<float>(m_prefix+"ECFG_2_1_2") ||
       !jet.isAvailable<float>(m_prefix+"ECFG_3_1_1") ||
@@ -33,7 +30,6 @@ int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
     ATH_MSG_WARNING("Energy correlation fractions with prefix '"<<m_prefix<<"' are not all available. Exiting..");
     return 1;
   }
-  
   
   float ecfg_2_1 = jet.getAttribute<float>(m_prefix+"ECFG_2_1");
   float ecfg_3_2 = jet.getAttribute<float>(m_prefix+"ECFG_3_2");
@@ -52,19 +48,19 @@ int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
 
   // N2
     
-  if(fabs(ecfg_2_1) > 1e-8) // Prevent div-0
+  if(ecfg_2_1 > 1e-8) // Prevent div-0
     jet.setAttribute(m_prefix+"N2", ecfg_3_2  / (pow(ecfg_2_1, 2.0)));
   else
     jet.setAttribute(m_prefix+"N2", -999.0);
 
   // N3
-  if(fabs(ecfg_3_1) > 1e-8) // Prevent div-0
+  if(ecfg_3_1 > 1e-8) // Prevent div-0
     jet.setAttribute(m_prefix+"N3", ecfg_4_2  / (pow(ecfg_3_1, 2.0)));
   else
     jet.setAttribute(m_prefix+"N3", -999.0);
 
   // M2
-  if(fabs(ecfg_2_1) > 1e-8) // Prevent div-0
+  if(ecfg_2_1 > 1e-8) // Prevent div-0
     jet.setAttribute(m_prefix+"M2", ecfg_3_2  / ecfg_2_1);
   else
     jet.setAttribute(m_prefix+"M2", -999.0);
@@ -85,7 +81,7 @@ int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
     E = (3*1) / (1*2) = 3./2.
   */
 
-  if(fabs(ecfg_2_1_2) > 1e-8)
+  if(ecfg_2_1_2 > 1e-8) // Prevent div-0
     {
       jet.setAttribute(m_prefix+"L1", ecfg_3_2_1 / (pow(ecfg_2_1_2, (1.) )));
       jet.setAttribute(m_prefix+"L2", ecfg_3_3_1 / (pow(ecfg_2_1_2, (3./2.) )));
@@ -96,7 +92,7 @@ int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
       jet.setAttribute(m_prefix+"L2",-999.0);
     }
 
-  if(fabs(ecfg_3_3_1) > 1e-8)
+  if(ecfg_3_3_1 > 1e-8) // Prevent div-0
     {  
       jet.setAttribute(m_prefix+"L3", ecfg_3_1_1 / (pow(ecfg_3_3_1, (1./3.) )) );
       jet.setAttribute(m_prefix+"L4", ecfg_3_2_2 / (pow(ecfg_3_3_1, (4./3.) )) );
@@ -107,7 +103,7 @@ int EnergyCorrelatorGeneralizedRatiosTool::modifyJet(xAOD::Jet &jet) const {
       jet.setAttribute(m_prefix+"L4",-999.0);
     }
 
-  if(fabs(ecfg_4_4_1) > 1e-8)
+  if(ecfg_4_4_1 > 1e-8) // Prevent div-0
     jet.setAttribute(m_prefix+"L5", ecfg_4_2_2 / (pow(ecfg_4_4_1, (1.) )) );
   else
     jet.setAttribute(m_prefix+"L5",-999.0);
--- Reconstruction/Jet/JetSubStructureMomentTools/Root/EnergyCorrelatorGeneralizedTool.cxx
+++ Reconstruction/Jet/JetSubStructureMomentTools/Root/EnergyCorrelatorGeneralizedTool.cxx
@@ -1,20 +1,20 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "JetSubStructureMomentTools/EnergyCorrelatorGeneralizedTool.h"
 #include "JetSubStructureUtils/EnergyCorrelatorGeneralized.h" 
-#include "fastjet/contrib/EnergyCorrelator.hh" 
 #include "JetSubStructureUtils/EnergyCorrelator.h" 
 
-using namespace std;
 using fastjet::PseudoJet;
 
 EnergyCorrelatorGeneralizedTool::EnergyCorrelatorGeneralizedTool(std::string name) : 
   JetSubStructureMomentToolsBase(name)
 {
-  ATH_MSG_DEBUG("Initializing EnergyCorrelator tool.");
+  ATH_MSG_DEBUG("Initializing EnergyCorrelatorGeneralized tool");
   declareProperty("Beta", m_Beta = 1.0);  
+  declareProperty("DoN3", m_doN3 = false);  
+  declareProperty("DoLSeries", m_doLSeries = false);  
 }
 
 int EnergyCorrelatorGeneralizedTool::modifyJet(xAOD::Jet &injet) const {
@@ -22,73 +22,79 @@ int EnergyCorrelatorGeneralizedTool::modifyJet(xAOD::Jet &injet) const {
   fastjet::PseudoJet jet;
   bool decorate = SetupDecoration(jet,injet);
 
-  float ECFG_2_1_value = -999, ECFG_3_2_value = -999, ECFG_3_1_value = -999, ECFG_4_2_value = -999;
-  //These ones for t/H discrimination
-  float ECFG_3_3_2_value = -999, ECFG_4_6_1_value = -999, ECFG_3_2_2_value = -999, ECFG_3_3_1_value = -999,
+  // These are necessary for N2 and M2
+  float ECFG_2_1_value = -999, ECFG_3_2_value = -999;
+  
+  // These are necessary for N3
+  float ECFG_3_1_value = -999, ECFG_4_2_value = -999;
+
+  // These are for t/H discrimination
+  float ECFG_3_3_2_value = -999, ECFG_3_2_2_value = -999, ECFG_3_3_1_value = -999,
     ECFG_4_2_2_value = -999, ECFG_4_4_1_value = -999, ECFG_2_1_2_value = -999, ECFG_3_2_1_value =  -999,
     ECFG_3_1_1_value = -999;
-  
-
-  // N.B. here _angles, _n, _beta !!
-
-  // These ones are necessary for N2, M2, etc.
+  // N.B. ECFG_angles_n_beta !!
 
   if (decorate) {
 
+    // These are necessary for N2 and M2
+
     JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_2(2, 3, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
     JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_2_1(1, 2, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_2(2, 4, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_1(1, 3, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
     ECFG_2_1_value = ECFG_2_1.result(jet);
     ECFG_3_2_value = ECFG_3_2.result(jet);
-    ECFG_3_1_value = ECFG_3_1.result(jet);
-    ECFG_4_2_value = ECFG_4_2.result(jet);
 
-    // these ones for t/H discrimination
-    // 332
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_3_2(3, 3, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_3_3_2_value = ECFG_3_3_2.result(jet);
-    // 461
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_6_1(6, 4, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_4_6_1_value = ECFG_4_6_1.result(jet);
+    // These are necessary for N3
+
+    if(m_doN3) {
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_2(2, 4, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_1(1, 3, m_Beta, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_1_value = ECFG_3_1.result(jet);
+      ECFG_4_2_value = ECFG_4_2.result(jet);
+    }
+
+    // These are for t/H discrimination
     
-    // 322
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_2_2(2, 3, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_3_2_2_value =  ECFG_3_2_2.result(jet);
-    
-    // 331
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_3_1(3, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_3_3_1_value = ECFG_3_3_1.result(jet);
-    
-    // 422
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_2_2(2, 4, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_4_2_2_value = ECFG_4_2_2.result(jet);
-    
-    // 441
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_4_1(4, 4, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_4_4_1_value = ECFG_4_4_1.result(jet);
-    
-    // 212
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_2_1_2(1, 2, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_2_1_2_value = ECFG_2_1_2.result(jet);
-    
-    // 321
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_2_1(2, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_3_2_1_value = ECFG_3_2_1.result(jet);
-    
-    // 311
-    JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_1_1(1, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
-    ECFG_3_1_1_value = ECFG_3_1_1.result(jet);
-    
+    if(m_doLSeries) {
+      // 332
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_3_2(3, 3, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_3_2_value = ECFG_3_3_2.result(jet);
+
+      // 322
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_2_2(2, 3, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_2_2_value =  ECFG_3_2_2.result(jet);
+
+      // 331
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_3_1(3, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_3_1_value = ECFG_3_3_1.result(jet);
+
+      // 422
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_2_2(2, 4, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_4_2_2_value = ECFG_4_2_2.result(jet);
+
+      // 441
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_4_4_1(4, 4, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_4_4_1_value = ECFG_4_4_1.result(jet);
+
+      // 212
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_2_1_2(1, 2, 2, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_2_1_2_value = ECFG_2_1_2.result(jet);
+
+      // 321
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_2_1(2, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_2_1_value = ECFG_3_2_1.result(jet);
+
+      // 311
+      JetSubStructureUtils::EnergyCorrelatorGeneralized ECFG_3_1_1(1, 3, 1, JetSubStructureUtils::EnergyCorrelator::pt_R);
+      ECFG_3_1_1_value = ECFG_3_1_1.result(jet);
+    }
+
   }
-  
-  
+
   injet.setAttribute(m_prefix+"ECFG_2_1", ECFG_2_1_value);
   injet.setAttribute(m_prefix+"ECFG_3_2", ECFG_3_2_value);
   injet.setAttribute(m_prefix+"ECFG_3_1", ECFG_3_1_value);
   injet.setAttribute(m_prefix+"ECFG_4_2", ECFG_4_2_value);
   injet.setAttribute(m_prefix+"ECFG_3_3_2", ECFG_3_3_2_value);
-  injet.setAttribute(m_prefix+"ECFG_4_6_1", ECFG_4_6_1_value);
   injet.setAttribute(m_prefix+"ECFG_3_2_2", ECFG_3_2_2_value);
   injet.setAttribute(m_prefix+"ECFG_3_3_1", ECFG_3_3_1_value);
   injet.setAttribute(m_prefix+"ECFG_4_2_2", ECFG_4_2_2_value);
