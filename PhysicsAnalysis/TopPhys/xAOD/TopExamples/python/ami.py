b0962432f4c456e835c8dc6742cb6e9a425f71a1
Knut Zoch <knut.zoch@cern.ch>
2019-02-22 11:55:45 +0100

    Update copyright notices in touched files

--- PhysicsAnalysis/TopPhys/xAOD/TopExamples/python/ami.py
+++ PhysicsAnalysis/TopPhys/xAOD/TopExamples/python/ami.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 import re
 import os
 import sys
--- PhysicsAnalysis/TopPhys/xAOD/TopExamples/scripts/01SubmitToGrid.py
+++ PhysicsAnalysis/TopPhys/xAOD/TopExamples/scripts/01SubmitToGrid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 import TopExamples.grid
 import DerivationTags
 import Data_rel21
