3df7a38c3c9c9d8f026927e3ee799c6c41b20468
Tadej Novak <tadej.novak@cern.ch>
2019-02-24 10:08:50 +0100

    Use the same BCM configuration for MC and data overlay

--- Event/EventOverlay/EventOverlayJobTransforms/share/BeamOverlay_jobOptions.py
+++ Event/EventOverlay/EventOverlayJobTransforms/share/BeamOverlay_jobOptions.py
@@ -3,13 +3,9 @@ include.block ( "EventOverlayJobTransforms/BeamOverlay_jobOptions.py" )
 
 from AthenaCommon.DetFlags import DetFlags
 
-from AthenaCommon.Resilience import treatException,protectedInclude
-
 if DetFlags.overlay.BCM_on():
-    protectedInclude( "InDetBCM_EventAthenaPool/InDetBCM_EventAthenaPool_joboptions.py" )
-    from AthenaCommon.AlgSequence import AlgSequence
-    job = AlgSequence()
+    if overlayFlags.isDataOverlay():
+        job.InDetBCMRawDataProvider.RDOKey = overlayFlags.dataStore() + "+BCM_RDOs"
+
     from AthenaCommon import CfgGetter
     job += CfgGetter.getAlgorithm("BCM_OverlayDigitization")
-
-
