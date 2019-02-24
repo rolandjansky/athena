eb2c39a869d46de28fa0707e39a3f48d4d36e6ed
Tadej Novak <tadej.novak@cern.ch>
2019-02-24 22:12:26 +0100

    Tier0Tests: Add versioning for overlay background RDO samples

--- Tools/PROCTools/python/RunTier0TestsTools.py
+++ Tools/PROCTools/python/RunTier0TestsTools.py
@@ -23,6 +23,19 @@ ciRefFileMap = {
                 's3126-21.9'           : 'v1',
                 's3126-22.0'           : 'v1',
                 # OverlayTier0Test_required-test
-                'overlay-d1498-21.0'   : 'v1',
+                'overlay-d1498-21.0'   : 'v2',
                 'overlay-d1498-22.0'   : 'v1',
+                'overlay-bkg-21.0'     : 'v1',
+                'overlay-bkg-22.0'     : 'v1',
                }
+
+
+#####
+# CI special input files
+#####
+# Simulation tests
+SimInput = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/SimCoreTests/ttbar_muplusjets-pythia6-7000.evgen.pool.root"
+
+# Overlay tests
+OverlayInputHits = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayMonitoringRTT/mc16_13TeV.424000.ParticleGun_single_mu_Pt100.simul.HITS.e3580_s3126/HITS.11330296._000376.pool.root.1"
+OverlayInputBkg = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayMonitoringRTT/PileupPremixing/{}/{}/RDO.merged-pileup.100events.pool.root"
