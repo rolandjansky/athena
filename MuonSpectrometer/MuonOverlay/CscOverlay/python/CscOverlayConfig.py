65e2a9290a42f80cae04550212e7c23f41ff1b53
John Chapman <jchapman@cern.ch>
2019-02-27 16:55:52 +0100

    Remove obsolete CscOverlayDigitToRDO Algorithm

--- MuonSpectrometer/MuonOverlay/CscOverlay/python/CscOverlayConfig.py
+++ MuonSpectrometer/MuonOverlay/CscOverlay/python/CscOverlayConfig.py
@@ -7,13 +7,3 @@ def getCscOverlay(name="CscOverlay", **kwargs):
 
     from AthenaCommon import CfgMgr
     return CfgMgr.CscOverlay(name,**kwargs)
-
-
-def getCscOverlayDigitToRDO(name="CscOverlayDigitToRDO", **kwargs):
-    from OverlayCommonAlgs.OverlayFlags import overlayFlags
-    kwargs.setdefault("InputBkgRDOKey", overlayFlags.dataStore()+"+CSCRDO")
-    kwargs.setdefault("MakeRDOTool2", "CscDigitToCscRDOTool2")
-    kwargs.setdefault("MakeRDOTool4", "CscDigitToCscRDOTool4")
-
-    from AthenaCommon import CfgMgr
-    return CfgMgr.CscOverlayDigitToRDO(name,**kwargs)
