237dda68a42f37eeae6c83ca73189f6d7b3b6189
Johannes Josef Junggeburth <johannes.josef.junggeburth@CERN.CH>
2019-02-27 10:55:57 +0100

    Hupps didn't delete the debug message

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/Root/KinematicSystHandler.cxx
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/Root/KinematicSystHandler.cxx
@@ -143,7 +143,6 @@ namespace CP {
     }
     CorrectionCode BadMuonVetoSystHandler::GetKineDependent(const xAOD::Muon &mu, float& Eff) const {
         TF1* Poly = nullptr;
-        std::cout<<"Eine Insel mit 2 bergen "<<m_SystWeight<<std::endl;
         if (m_SystWeight == 0) {
             return CorrectionCode::Ok;
         }
