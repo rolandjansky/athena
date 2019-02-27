301a6bdf990be156994e21e660f889c34c4ce7c8
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-02-27 10:21:58 +0100

    athenaHLT: enable unit tests and fix log file names
    - Enable unit tests for basic athenaHLT command line functionality
    - Update references
    - Make sure the application_name is unique to allow running multiple
      athenaHLT processes in paralle on the same machine
    - Adapt the log_name to the new convention in HLTMPPU-00-01-16

--- HLT/Trigger/TrigControl/TrigCommon/share/test_athenaHLT.sh
+++ HLT/Trigger/TrigControl/TrigCommon/share/test_athenaHLT.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 #
 # Configuration testing script for athenaHLT.py
 #
@@ -22,4 +23,6 @@ detmask="00000000000000000001fffffffffff7"
 
 # Run only config stage (exit immediately via interactive mode) and filter final ptree
 # If there was a failure, the exit code will be non-zero
-echo "e" | athenaHLT.py --file ${file} --detector-mask ${detmask} --run-number ${run} --sor-time ${sortime} --interactive ${test_options} | sed -n '/<Configuration>/,/<\/Magnets>/p;/<\/Magnets>/q' | grep '<'
+log=test_athenaHLT-${BASHPID}
+echo "e" | athenaHLT.py --file ${file} --detector-mask ${detmask} --run-number ${run} --sor-time ${sortime} --interactive ${test_options} &> $log
+cat $log | sed -n '/<Configuration>/,/<\/Magnets>/p;/<\/Magnets>/q' | grep '<' | grep -v 'LogRoot'
