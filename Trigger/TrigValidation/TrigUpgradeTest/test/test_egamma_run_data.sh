74e3528125ee79a4819326fc7d0cbdac744e518a
Tim Martin <Tim.Martin@cern.ch>
2019-03-01 13:11:03 +0100

    Remove defunct call to LumiBlockMuWriter from trigger modifiers. see \!21458

--- Trigger/TrigValidation/TrigUpgradeTest/test/test_egamma_run_data.sh
+++ Trigger/TrigValidation/TrigUpgradeTest/test/test_egamma_run_data.sh
@@ -7,3 +7,4 @@ rm -rf  data_test.*.data
 athena  --threads=1 --skipEvents=10 --evtMax=20 --filesInput="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1" TrigUpgradeTest/egamma.withViews.py &&
 checkxAOD.py myESD.pool.root &&
 athena TrigUpgradeTest/checkESD.py 
+
