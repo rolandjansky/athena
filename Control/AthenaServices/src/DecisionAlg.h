9d2f094a1bc03f3f244c2b2cb424191161115c45
cranshaw <Jack.Cranshaw@cern.ch>
2019-03-01 10:04:19 -0600

    Code cleanup.

--- Control/AthenaServices/src/DecisionAlg.h
+++ Control/AthenaServices/src/DecisionAlg.h
@@ -1,5 +1,3 @@
-// Dear emacs, this is -*- C++ -*-
-
 /*
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
@@ -7,41 +5,12 @@
 #ifndef ATHENASERVICES_DECISIONALG_H
 #define ATHENASERVICES_DECISIONALG_H
 
-// STL include files
-#include <memory>
-#include <map>
-#include <set>
-#include <vector>
-#include <string>
-
-// Required for inheritance
-#include "GaudiKernel/IDataSelector.h"
-#include "GaudiKernel/Property.h"
-#include "GaudiKernel/ServiceHandle.h"
-#include "GaudiKernel/ToolHandle.h"
-
-#include "GaudiKernel/IIncidentListener.h"
 #include "AthenaBaseComps/FilteredAlgorithm.h"
-#include "GaudiKernel/IIoComponent.h"
-
-// forward declarations
-class IClassIDSvc;
-class OutputStreamSequencerSvc;
-class StoreGateSvc;
-class IDecisionAlgTool;
-class IAthenaOutputTool;
-class IItemListSvc;
-
-namespace SG {
-   class DataProxy;
-   class IFolder;
-   class FolderItem;
-}
 
 /** @class DecisionAlg
    * @brief Call through interface to FilteredAlgorithm
    * 
-   * @author srinir@bnl.gov
+   * @author cranshaw@anl.gov
    * $Id: DecisionAlg.h,v 1.12 2008-12-15 19:14:58 binet Exp $
    */
 class DecisionAlg : public FilteredAlgorithm
@@ -55,4 +24,4 @@ public:
 
 };
 
-#endif // ATHENASERVICES_OUTPUTSTREAM_H
+#endif // ATHENASERVICES_DECISIONALG_H
