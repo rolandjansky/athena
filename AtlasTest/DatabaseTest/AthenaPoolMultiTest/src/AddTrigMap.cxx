7c132e446ae00d9361f6704612545b4f1a973a47
scott snyder <snyder@bnl.gov>
2019-03-03 17:12:17 +0100

    AthenaPoolMultiTest: Finish handle migration.
    HandleKey members need to be declared as properties in order
    for the scheduler to see them.

--- AtlasTest/DatabaseTest/AthenaPoolMultiTest/src/AddTrigMap.cxx
+++ AtlasTest/DatabaseTest/AthenaPoolMultiTest/src/AddTrigMap.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /**
@@ -17,10 +17,7 @@
     
 AddTrigMap::AddTrigMap(const std::string& name, 
 			ISvcLocator* pSvcLocator) :
-              AthAlgorithm(name, pSvcLocator),
-              m_evt("EventInfo"),
-              m_wftm("MultiTestTrigMap"),
-              m_wftm2("ExcludeTestTrigMap")
+              AthAlgorithm(name, pSvcLocator)
 {}
 
 AddTrigMap::~AddTrigMap() 
