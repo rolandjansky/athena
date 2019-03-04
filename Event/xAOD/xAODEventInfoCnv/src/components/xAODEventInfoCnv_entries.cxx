60436a1d03dc444020c70949b960cd9ecdd00398
Adam Edward Barton <adam.edward.barton@cern.ch>
2019-03-04 13:27:24 +0000

    Revert "Merge branch 'master-EventInfoMigration' into 'master'"
    This reverts merge request !21559

--- Event/xAOD/xAODEventInfoCnv/src/components/xAODEventInfoCnv_entries.cxx
+++ Event/xAOD/xAODEventInfoCnv/src/components/xAODEventInfoCnv_entries.cxx
@@ -1,7 +1,6 @@
 #include "../EventInfoCnvTool.h"
 #include "../EventInfoSelectorTool.h"
 #include "../EventInfoCnvAlg.h"
-#include "../EventInfoNonConstCnvAlg.h"
 #include "../EventInfoReaderAlg.h"
 #include "../EventDuplicateFinderAlg.h"
 
@@ -10,7 +9,6 @@
 DECLARE_COMPONENT( xAODMaker::EventInfoCnvTool )
 DECLARE_COMPONENT( xAODMaker::EventInfoSelectorTool )
 DECLARE_COMPONENT( xAODMaker::EventInfoCnvAlg )
-DECLARE_COMPONENT( xAODMaker::EventInfoNonConstCnvAlg )
 DECLARE_COMPONENT( xAODReader::EventInfoReaderAlg )
 DECLARE_COMPONENT( xAODReader::EventDuplicateFinderAlg )
 
