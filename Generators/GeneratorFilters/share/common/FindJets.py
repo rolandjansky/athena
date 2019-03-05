e3b1126d51e605ed886c97cb5d733cbd380ef298
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2019-03-05 10:34:17 +0100

    add jet filter common fragments

--- Generators/GeneratorFilters/share/common/FindJets.py
+++ Generators/GeneratorFilters/share/common/FindJets.py
@@ -6,24 +6,19 @@ prefiltSeq.xAODCnv.AODContainerName = 'GEN_EVENT'
 #CreateTruthJets(prefiltSeq,filtSeq,runArgs.ecmEnergy,0.6)
 
 # Turn off ghost association algorithms
-include("GeneratorFilters/JetFilter_JZX.py")
+#include("GeneratorFilters/JetFilter_JZX.py")
 #from JetFilter_JZX import JZSlice
 
 #JZSlice(0,prefiltSeq,filtSeq,runArgs.ecmEnergy,0.6)
 
-def CreateJets(prefiltSeq,filtSeq,ecmEnergy, jetR):
+def CreateJets(prefiltSeq,filtSeq,ecmEnergy, jetR, mods=""):
        include("GeneratorFilters/GenerateTruthJets.py")
 
        PrepareTruthJetInputs(prefiltSeq)
-       ScheduleAntiKtTruthJets(jetradius=jetR,algseq=prefiltSeq)
+       ScheduleAntiKtTruthJets(jetradius=jetR,algseq=prefiltSeq,mods=mods)
 
        include("GeneratorFilters/QCDJetFilter.py")
 #    from QCDJetFilter import AddJetFilter
        AddJetFilter(filtSeq,ecmEnergy)
-
-       if (jetR==0.4):
-          filtSeq.QCDTruthJetFilter.TruthJetContainer = "AntiKt4TruthJets"
-       elif (jetR==1.0):
-          filtSeq.QCDTruthJetFilter.TruthJetContainer = "AntiKt10TruthJets"
-       else:
-          filtSeq.QCDTruthJetFilter.TruthJetContainer = "AntiKt6TruthJets" 
+       jetcollname = 'AntiKt{0}{1}TruthJets'.format(int(jetR*10),mods)
+       filtSeq.QCDTruthJetFilter.TruthJetContainer = jetcollname
