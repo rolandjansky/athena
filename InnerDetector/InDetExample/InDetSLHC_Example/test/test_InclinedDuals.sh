028f47a4a4ab97d7c266678efb23273b2febd928
Nicholas Styles <nicholas.styles@desy.de>
2019-03-06 10:26:56 +0100

    Change geo tags while memory issues in ATLITKSW-75 are understood

--- InnerDetector/InDetExample/InDetSLHC_Example/test/test_InclinedDuals.sh
+++ InnerDetector/InDetExample/InDetSLHC_Example/test/test_InclinedDuals.sh
@@ -92,7 +92,7 @@ if [ $dosim -ne 0 ]; then
     --skipEvents      0 \
     --maxEvents       10 \
     --randomSeed      873254 \
-    --geometryVersion ATLAS-P2-ITK-20-00-00_VALIDATION \
+    --geometryVersion ATLAS-P2-ITK-20-03-00_VALIDATION \
     --conditionsTag   OFLCOND-MC15c-SDR-14-03 \
     --truthStrategy   MC15aPlus \
     --DataRunNumber   242000 \
@@ -127,7 +127,7 @@ if [ $dorec -ne 0 ]; then
     --outputDAOD_IDTRKVALIDFile "$daod" \
     --maxEvents        10 \
     --digiSteeringConf StandardInTimeOnlyTruth \
-    --geometryVersion  ATLAS-P2-ITK-20-00-00 \
+    --geometryVersion  ATLAS-P2-ITK-20-03-00 \
     --conditionsTag    OFLCOND-MC15c-SDR-14-03 \
     --DataRunNumber    242000 \
       --steering doRAWtoALL \
