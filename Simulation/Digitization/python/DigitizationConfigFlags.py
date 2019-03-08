7c71fe229a12c00b5d0a570f95c51688168c5098
Rupert Tombs <rt500@cam.ac.uk>
2019-03-08 17:10:32 +0000

    First changes to new style
    Initial test structure. Update EventStore flags to Legacy.
    
    CSC name updates and test tweaks towards working.
    
    Update tests to parity attempting to run on the default hits test file. Switch to returning ComponentAccumulators containing private tools rather than returning tuple.
    
    Add comments for future progress
    
    Convert to PrivateTools features for ComponentAccumulator. Introduce algorithm methods for non-overlay mode. Convert tests to non-overlay. Fix permissions for tests.
    
    Tidy up whitespace and comments in _test.py files.
    
    Configure special configuration in the MDT test.
    
    Add special configuration file to TestDefaults. Move getSpecialConfiguration to DigitizationConfigFlags, update test.
    
    Clean __init__ comments and remove a .merge in tests

--- Simulation/Digitization/python/DigitizationConfigFlags.py
+++ Simulation/Digitization/python/DigitizationConfigFlags.py
@@ -3,6 +3,39 @@
 Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 """
 from AthenaConfiguration.AthConfigFlags import AthConfigFlags
+from AthenaCommon.Logging import log
+from PyUtils import AthFile
+
+def getSpecialConfiguration(flags):
+    """Return a dict of Special configuration as parsed from flags.Input.Files"""
+    if len(flags.Input.Files) > 1:
+        log.info("Multiple input files. Using the first for Digitization special configuration.")
+    log.info("Obtaining Digitization special configuration from %s." % flags.Input.Files[0])
+    File = AthFile.fopen(flags.Input.Files[0])
+    # extract the special config list
+    tag_info = File.infos.get("tag_info", {})
+    SpecialCfg = tag_info.get("specialConfiguration", "").split(";")
+    # fill containers
+    preIncludes = []
+    out = {}
+    for KeyEqValue in SpecialCfg:
+        # Ignore empty or "NONE" substrings, e.g. from consecutive or trailing semicolons
+        if not KeyEqValue or KeyEqValue.upper() == "NONE":
+            continue
+        # If not in key=value format, treat as v, with k="preInclude"
+        if "=" not in KeyEqValue:
+            KeyEqValue = "preInclude=" + KeyEqValue
+        # Handle k=v directives
+        key, value = KeyEqValue.split("=")
+        if key == "preInclude":
+            preIncludes += value.split(",")
+        else:
+            out[key] = value
+    # FIXME includes not migrated
+    # from AthenaCommon.Include import include
+    # for inc in preIncludes:
+    #     include(inc)
+    return out
 
 def createDigitizationCfgFlags():
     """Return an AthConfigFlags object with required flags"""
@@ -15,5 +48,7 @@ def createDigitizationCfgFlags():
     flags.addFlag("Digitization.PileUpPremixing", False)
     # Run pile-up premixing for Multi-Threading
     flags.addFlag("Digitization.PileUpPremixingForMT", False)
+    # Special configuration read from flags.Input.Files
+    flags.addFlag("Digitization.SpecialConfiguration", getSpecialConfiguration)
     return flags
 
