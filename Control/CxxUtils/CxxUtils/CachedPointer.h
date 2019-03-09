042f49c26785fd336031d4e97cd77a384da5b6aa
scott snyder <sss@karma>
2019-03-09 16:14:45 +0100

    CxxUtils: CachedPointer::store() should not be const.
    CachedPointer::store() is the non-atomic, non-const version.

--- Control/CxxUtils/CxxUtils/CachedPointer.h
+++ Control/CxxUtils/CxxUtils/CachedPointer.h
@@ -1,6 +1,6 @@
 // This file's extension implies that it's C, but it's really -*- C++ -*-.
 /*
- * Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration.
+ * Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration.
  */
 // $Id$
 /**
@@ -81,7 +81,7 @@ public:
 
 
   /// Store a new value to the element.
-  void store (pointer_t elt) const;
+  void store (pointer_t elt);
 
   
   /// Return the current value of the element.
