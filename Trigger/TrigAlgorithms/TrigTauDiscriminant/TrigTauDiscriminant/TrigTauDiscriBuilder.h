57f704a1e24735b81eb491296443d4cdcb91a40b
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-03-13 14:24:18 +0100

    TrigTauDiscriminant: Enable thread checker

--- Trigger/TrigAlgorithms/TrigTauDiscriminant/TrigTauDiscriminant/TrigTauDiscriBuilder.h
+++ Trigger/TrigAlgorithms/TrigTauDiscriminant/TrigTauDiscriminant/TrigTauDiscriBuilder.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 //
@@ -22,6 +22,9 @@
 //#include "TauDiscriminant/TauDiscriToolBase.h"
 #include "tauRecTools/ITauToolBase.h"
 #include <vector>
+
+#include "CxxUtils/checker_macros.h"
+ATLAS_NO_CHECK_FILE_THREAD_SAFETY;  // legacy trigger code
 
 namespace HLT {
   class TriggerElement;
