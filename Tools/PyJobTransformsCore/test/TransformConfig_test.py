792a2d6525cf1261c8bb3c9fccb1e94dd2636bb7
Umesh Worlikar <umesh.dharmaji.worlikar@cern.ch>
2019-03-13 19:14:18 +0100

    ATLASJT-392: Add unit tests 'AllowedValuesTestCase'
    Validates "allowed values" feature of ListOfStrings class

--- Tools/PyJobTransformsCore/test/TransformConfig_test.py
+++ Tools/PyJobTransformsCore/test/TransformConfig_test.py
@@ -3,17 +3,17 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 import sys
 import myunittest
-from PyJobTransformsCore.TransformConfig import TransformConfig, ListOfStrings
+from PyJobTransformsCore.TransformConfig import TransformConfig, TransformConfigError, ListOfStrings
 
 sys.tracebacklimit = 0
 
 
-class DerivedTransformConfig(TransformConfig):
+class TransformConfigKeywords(TransformConfig):
     __slots__ = ()
     keywords = ListOfStrings("Search keywords")
 
     def __init__(self, name=None):
-        super(DerivedTransformConfig, self).__init__(name)
+        super(TransformConfigKeywords, self).__init__(name)
 
     def __setattr__(self, name, value):
         object.__setattr__(self, name, value)
@@ -23,7 +23,7 @@ class DerivedTransformConfig(TransformConfig):
 # Test methods are named in the format testPrecondition_ExpectedResults
 class UniqueListTestCase(myunittest.TestCase):
     def setUp(self):
-        self.dummyConfig = DerivedTransformConfig('DummyTransformConfig')
+        self.dummyConfig = TransformConfigKeywords('DummyTransformConfig')
 
     def testSetEmptyList_ReturnsEmptyList(self):
         self.dummyConfig.keywords = []
@@ -46,12 +46,59 @@ class UniqueListTestCase(myunittest.TestCase):
         self.assertListEqual(self.dummyConfig.keywords, ["QCD", "minBias", "SM"])
 
 
+# This class mimics a transform configuration with restricted keywords.
+class TransformConfigAllowedKeywords(TransformConfig):
+    __slots__ = ()
+    keywords = ListOfStrings("Search keywords", allowedValues=["QCD", "minBias", "SM"])
+
+    def __init__(self, name=None):
+        super(TransformConfigAllowedKeywords, self).__init__(name)
+
+    def __setattr__(self, name, value):
+        object.__setattr__(self, name, value)
+
+
 # This test case mimics a set of use cases to validate the "Allowed Values" feature of ListOfStrings class.
 # Test methods are named in the format testPrecondition_ExpectedResults
 class AllowedValuesTestCase(myunittest.TestCase):
     def setUp(self):
-        self.dummyTrfConfig = DerivedTransformConfig('DummyTransformConfig')
+        self.dummyConfig = TransformConfigAllowedKeywords('DummyTransformConfig')
 
+    def testSetEmptyList_ReturnsEmptyList(self):
+        self.dummyConfig.keywords = []
+        self.assertListEqual(self.dummyConfig.keywords, [])
+
+    def testSetOneAllowedValue_ReturnsListWithSameValue(self):
+        self.dummyConfig.keywords = ["QCD"]
+        self.assertListEqual(self.dummyConfig.keywords, ["QCD"])
+
+    def testSetOneAllowedRepeatedValue_ReturnsListWithSingleValue(self):
+        self.dummyConfig.keywords = ["QCD", "QCD", "QCD"]
+        self.assertListEqual(self.dummyConfig.keywords, ["QCD"])
+
+    def testSetMultipleAllowedAndUniqueValues_ReturnsSameValues_KeepsSequence(self):
+        self.dummyConfig.keywords = ["QCD", "minBias", "SM"]
+        self.assertListEqual(self.dummyConfig.keywords, ["QCD", "minBias", "SM"])
+
+    def testSetMultipleAllowedAndRepeatedValues_ReturnsUniqueValues_KeepsSequence(self):
+        self.dummyConfig.keywords = ["QCD", "minBias", "minBias", "SM", "SM"]
+        self.assertListEqual(self.dummyConfig.keywords, ["QCD", "minBias", "SM"])
+
+    def testSetOneValueNotAllowed_ThrowsTransformConfigError(self):
+        with self.assertRaises(TransformConfigError):
+            self.dummyConfig.keywords = ["InvalidKeyword"]
+
+    def testSetOneValueNotAllowedAndRepeated_ThrowsTransformConfigError(self):
+        with self.assertRaises(TransformConfigError):
+            self.dummyConfig.keywords = ["InvalidKeyword", "InvalidKeyword"]
+
+    def testSetMultipleValuesNotAllowedAndUnique_ThrowsTransformConfigError(self):
+        with self.assertRaises(TransformConfigError):
+            self.dummyConfig.keywords = ["InvalidKeyword1", "InvalidKeyword2"]
+
+    def testSetMultipleValuesNotAllowedAndRepeated_ThrowsTransformConfigError(self):
+        with self.assertRaises(TransformConfigError):
+            self.dummyConfig.keywords = ["InvalidKeyword1", "InvalidKeyword1", "InvalidKeyword2", "InvalidKeyword2"]
 
 if __name__ == "__main__":
     myunittest.main()
