469955863d8039f104c54ba26728594d9f34307b
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-03-20 09:30:25 +0100

    Modernize and update TrigMessageSvc
    - Rewrite TrigMessageSvc based on the latest MessageSvc in Gaudi
    - Remove unused features (e.g. colors)
    - Add property for separate ERS message format
    
    TODO:
    - Message dispatch in separate thread (if needed)
    - Add monitoring histograms
    - Separate message statistics for each run

--- HLT/Trigger/TrigControl/TrigServices/src/TrigMessageSvc.h
+++ HLT/Trigger/TrigControl/TrigServices/src/TrigMessageSvc.h
@@ -1,31 +1,33 @@
 /*
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
-
 #ifndef TRIGSERVICES_TRIGMESSAGESVC_H
 #define TRIGSERVICES_TRIGMESSAGESVC_H
 
 // Include files
+#include <iosfwd>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <set>
 #include <string>
 #include <vector>
-#include <map>
 
-#include "GaudiKernel/StatusCode.h"
-#include "GaudiKernel/Service.h"
-#include "GaudiKernel/ServiceHandle.h"
+#include "CxxUtils/checker_macros.h"
 #include "GaudiKernel/IMessageSvc.h"
-#include "GaudiKernel/IIncidentListener.h"
 #include "GaudiKernel/Message.h"
 #include "GaudiKernel/Property.h"
-
-#include "StoreGate/StoreGateSvc.h"
+#include "GaudiKernel/Service.h"
+#include "GaudiKernel/StatusCode.h"
 
 #include "TrigKernel/ITrigMessageSvc.h"
 
+// Helper to mark some virtual methods as not supported
+#define NOTSUPPORTED                                                                               \
+  throw std::logic_error(std::string(__func__) + " is not supported by TrigMessageSvc")
+
 // Forward declarations
 class ISvcLocator;
-class TH1I;
-class TH2I;
 
 /**@class TrigMessageSvc
  * @brief MessageSvc used by the HLT applications
@@ -44,172 +46,147 @@ class TH2I;
  *
  * @author    Iain Last, Werner Wiedenmann, Frank Winklmeier
  */
+class TrigMessageSvc : public extends<Service, IMessageSvc, ITrigMessageSvc> {
+public:
+  typedef std::map<std::string, int> ThresholdMap;
 
-class TrigMessageSvc : public extends<Service,IMessageSvc,ITrigMessageSvc,IIncidentListener> {
-public:  
-  typedef std::pair< std::string, std::ostream* > NamedStream;
-  typedef std::multimap< int, NamedStream > StreamMap;
-  typedef std::multimap< StatusCode, Message > MessageMap;
-  typedef std::map< std::string, int > ThresholdMap;
-  
-  // Default constructor.
-  TrigMessageSvc( const std::string& name, ISvcLocator* svcloc );
-  // Destructor.
-  virtual ~TrigMessageSvc();
+  TrigMessageSvc(const std::string& name, ISvcLocator* svcloc);
 
-  // Implementation of IService::reinitialize()
   virtual StatusCode reinitialize() override;
-  // Implementation of IService::initialize()
   virtual StatusCode initialize() override;
-  // Implementation of IService::finalize()
   virtual StatusCode finalize() override;
 
-  // Implementation of IMessageSvc::reportMessage()
-  virtual void reportMessage( const Message& message ) override;
-
-  // Implementation of IMessageSvc::reportMessage()
-  virtual void reportMessage( const Message& message, int outputLevel ) override;
-
-  // Implementation of IMessageSvc::reportMessage()
-  virtual void reportMessage( const StatusCode& code, const std::string& source = "") override;
-
-  // Implementation of IMessageSvc::reportMessage()
-  virtual void reportMessage( const char* source, int type, const char* message) override;
-
-  // Implementation of IMessageSvc::reportMessage()
-  virtual void reportMessage( const std::string& source, int type, const std::string& message) override;
-  
-  // Implementation of IMessageSvc::insertMessage()
-  virtual void insertMessage( const StatusCode& code, const Message& message ) override;
-
-  // Implementation of IMessageSvc::eraseMessage()
-  virtual void eraseMessage() override;
-
-  // Implementation of IMessageSvc::eraseMessage()
-  virtual void eraseMessage( const StatusCode& code ) override;
-
-  // Implementation of IMessageSvc::eraseMessage()
-  virtual void eraseMessage( const StatusCode& code, const Message& message ) override;
-
-  // Implementation of IMessageSvc::insertStream()
-  virtual void insertStream( int message_type, const std::string& name, std::ostream* stream ) override;
-  
-  // Implementation of IMessageSvc::eraseStream()
-  virtual void eraseStream() override;
-  
-  // Implementation of IMessageSvc::eraseStream()
-  virtual void eraseStream( int message_type ) override;
-  
-  // Implementation of IMessageSvc::eraseStream()
-  virtual void eraseStream( int message_type, std::ostream* stream ) override;
-
-  // Implementation of IMessageSvc::eraseStream()
-  virtual void eraseStream( std::ostream* stream ) override;
-
-  // Implementation of IMessageSvc::desaultStream()
-  virtual std::ostream* defaultStream() const override {
-    return m_defaultStream; 
+  virtual void reportMessage(const Message& message) override;
+  virtual void reportMessage(const Message& msg, int outputLevel) override;
+  virtual void reportMessage(const char* source, int type, const char* message) override;
+  virtual void reportMessage(const std::string& source, int type,
+                             const std::string& message) override;
+  virtual std::ostream* defaultStream ATLAS_NOT_THREAD_SAFE() const override
+  {
+    return m_defaultStream;
   }
 
-  // Implementation of IMessageSvc::setDefaultStream()
-  virtual void setDefaultStream( std::ostream* stream ) override {
+  virtual void setDefaultStream(std::ostream* stream) override
+  {
+    std::unique_lock<std::recursive_mutex> lock(m_reportMutex);
     m_defaultStream = stream;
   }
 
-  // Implementation of IMessageSvc::ouputLevel()
-  virtual int outputLevel()   const override;
-
-  // Implementation of IMessageSvc::ouputLevel()
-  virtual int outputLevel(const std::string& source)   const override;
-
-  // Implementation of IMessageSvc::setOuputLevel()
+  virtual int outputLevel() const override;
+  virtual int outputLevel(const std::string& source) const override;
   virtual void setOutputLevel(int new_level) override;
-
-  // Implementation of IMessageSvc::setOuputLevel()
   virtual void setOutputLevel(const std::string& source, int new_level) override;
-
-  // Implementation of IMessageSvc::useColor()
-  virtual bool useColor() const override { return m_color; }
-
-  // Implementation of IMessageSvc::getLogColor()
-  virtual std::string getLogColor(int logLevel) const override;
-
-  // Implementation of IMessageSvc::messageCount()
-  virtual int messageCount( MSG::Level logLevel ) const override;
- 
-  /// \name ITrigMessageSvc implementation
-  //@{
-  /// Reset all individual output levels
+  virtual int messageCount(MSG::Level logLevel) const override;
   virtual void resetOutputLevels() override;
-  //@}
 
-  // Implemenation of IIncidentListener::handle()
-  virtual void handle(const Incident& inc) override;
+  virtual bool useColor() const override { return m_color; }
+  virtual std::string getLogColor(int) const override { return ""; }
+
+  ///@{ Not supported by this implementation
+  virtual void reportMessage(const StatusCode&, const std::string&) override { NOTSUPPORTED; }
+  virtual void insertMessage(const StatusCode&, const Message&) override { NOTSUPPORTED; }
+  virtual void eraseMessage() override { NOTSUPPORTED; }
+  virtual void eraseMessage(const StatusCode&) override { NOTSUPPORTED; }
+  virtual void eraseMessage(const StatusCode&, const Message&) override { NOTSUPPORTED; }
+  virtual void insertStream(int, const std::string&, std::ostream*) override { NOTSUPPORTED; }
+  virtual void eraseStream() override { NOTSUPPORTED; }
+  virtual void eraseStream(int) override { NOTSUPPORTED; }
+  virtual void eraseStream(int, std::ostream*) override { NOTSUPPORTED; }
+  virtual void eraseStream(std::ostream*) override { NOTSUPPORTED; }
+  ///@}
 
 private:
-  typedef std::map<std::string, MSG::Color> ColorMap;
-  typedef std::map< unsigned int, unsigned int > MsgCountMap;
-  
-  std::ostream* m_defaultStream;      ///< Pointer to the output stream.
-  Message m_defaultMessage;           ///< Default Message
-  StreamMap m_streamMap;              ///< Stream map
-  MessageMap m_messageMap;            ///< Message map
-  ThresholdMap m_thresholdMap;        ///< Output level threshold map
-  std::string m_defaultFormat;        ///< Default format for the messages
-  std::string m_defaultTimeFormat;    ///< Default time format for the messages
-  int m_defaultLimit;                 ///< Original default message limit
-  bool m_doSuppress;                  ///< Message suppression currently active?
-  bool m_running;                     ///< In RUNNING state
-  bool m_canEnter;                    ///< used to avoid infinite recursions
-  
-  ServiceHandle<StoreGateSvc> m_evtStore;   ///< Event store
-  
-  /* Properties */
-  StringArrayProperty m_thresholdProp[MSG::NUM_LEVELS];
-  StringArrayProperty m_logColors[MSG::NUM_LEVELS];
-  UnsignedIntegerProperty m_statLevel;
-  UnsignedIntegerProperty m_statLevelRun;
-  UnsignedIntegerProperty m_publishLevel;
-  UnsignedIntegerProperty m_eventIDLevel;
-  IntegerProperty m_msgLimit[MSG::NUM_LEVELS];
-  BooleanProperty m_color;
-  BooleanProperty m_stats;
-  BooleanProperty m_publishStats;
-  BooleanProperty m_resetStats;
-  StringArrayProperty m_useERS[MSG::NUM_LEVELS];
-  BooleanProperty m_alwaysUseMsgStream;
-  BooleanProperty m_useErsRunningOnly;  
-  BooleanProperty m_suppress;
-  BooleanProperty m_suppressRunningOnly;
-  BooleanProperty m_forceOutputLevel;
-  
-  /* Message colors */
-  std::string m_logColorCodes[MSG::NUM_LEVELS];
-  std::string colTrans(std::string, int);
-  ColorMap m_colMap;
+  //////////////////////////////////////////////////////////////////////
+  // Properties
+  //////////////////////////////////////////////////////////////////////
+  Gaudi::Property<std::string> m_defaultFormat{this, "Format", Message::getDefaultFormat(),
+                                               "Default message format"};
+  Gaudi::Property<std::string> m_ersFormat{this, "ErsFormat", Message::getDefaultFormat(),
+                                           "ERS message format"};
+  Gaudi::Property<std::string> m_defaultTimeFormat{
+      this, "timeFormat", Message::getDefaultTimeFormat(), "Message time format"};
+  Gaudi::Property<bool> m_stats{this, "showStats", false, "Show message statistics"};
+  Gaudi::Property<unsigned int> m_statLevel{this, "statLevel", 0,
+                                            "Show total message statistics for >= level"};
+  Gaudi::Property<unsigned int> m_statLevelRun{this, "statLevelRun", 0,
+                                               "Show per-run statistics for messages >= level"};
+  Gaudi::Property<bool> m_resetStats{this, "resetStatsAtBeginRun", false,
+                                     "Reset message statistics at BeginRun"};
+  Gaudi::Property<unsigned int> m_eventIDLevel{this, "printEventIDLevel", MSG::NIL,
+                                               "Print event ID for this and higher message levels"};
+  Gaudi::Property<bool> m_color{this, "useColors", false,
+                                "Colors are not supported by TrigMessageSvc"};
+  Gaudi::Property<bool> m_suppress{this, "enableSuppression", false, "Enable message suppression"};
+  Gaudi::Property<bool> m_suppressRunningOnly{this, "suppressRunningOnly", true,
+                                              "Use message suppression only during RUNNING state"};
 
-  /* Message counting */
-  struct msgAry { int msg[MSG::NUM_LEVELS]; };
-  std::map<std::string, msgAry> m_sourceMap;      ///< Current message counting per source
-  std::map<std::string, msgAry> m_sourceMapTotal; ///< Total message counting per source
-  int m_msgCount[MSG::NUM_LEVELS];                ///< Message counting per level
-  MsgCountMap m_msgCountMap;                      ///< Message counting per message
-  TH1I* m_msgCountHist;                           ///< Message counting per level histogram
-  TH2I* m_msgCountSrcHist;                        ///< Message counting per message source
+  std::array<Gaudi::Property<std::vector<std::string>>, MSG::NUM_LEVELS> m_thresholdProp{
+      {{/*ignored*/},
+       {this, "setVerbose"},
+       {this, "setDebug"},
+       {this, "setInfo"},
+       {this, "setWarning"},
+       {this, "setError"},
+       {this, "setFatal"},
+       {this, "setAlways"}}};
 
-  /* Update handlers */
-  void initColors(Property& prop);
-  void setupColors(Property& prop);
-  void setupLimits(Property& prop);
-  void setupThreshold(Property& prop);
-  void setupSuppression(Property& prop);
-  
-  unsigned int msgHash(const Message& msg);   ///< Hash of message (for log. suppression)
-  void resetMsgStats();                       ///< Reset message statistics
-  void reportMsgStats(uint statLevel, bool total=false);        ///< Report message statistics
-  void setOutputLevelViaJobOptSvc();          ///< Change OutputLevel properties in JobOptionsSvc
-  void bookHistograms();                      ///< Book histograms for message statistics
-  bool passErsFilter(const std::string& source, const std::vector<std::string>& filter);  ///< Source passes ERS filter?
+  std::array<Gaudi::Property<int>, MSG::NUM_LEVELS> m_msgLimit{{{this, "defaultLimit", 500},
+                                                                {this, "verboseLimit", 500},
+                                                                {this, "debugLimit", 500},
+                                                                {this, "infoLimit", 500},
+                                                                {this, "warningLimit", 500},
+                                                                {this, "errorLimit", 500},
+                                                                {this, "fatalLimit", 500},
+                                                                {this, "alwaysLimit", 0}}};
+
+  /**
+   * Special properties to control output to ERS of individual sources.
+   * The syntax is as follows (these are NOT regular expressions):
+   *
+   * useErsFatal = []                       # forward none (default)
+   * useErsFatal = ['*']                    # forward all
+   * useErsFatal = ['CoreDumpSvc','MyAlg']  # forward these sources
+   * useErsFatal = ['*','!MyAlg']           # forward all except MyAlg
+   */
+  std::array<Gaudi::Property<std::vector<std::string>>, MSG::NUM_LEVELS> m_useERS{
+      {{/*ignored*/},
+       {this, "useErsVerbose", {}},
+       {this, "useErsDebug", {}},
+       {this, "useErsInfo", {}},
+       {this, "useErsWarning", {}},
+       {this, "useErsError", {}},
+       {this, "useErsFatal", {}},
+       {this, "useErsAlways", {}}}};
+
+  //////////////////////////////////////////////////////////////////////
+  // Private members
+  //////////////////////////////////////////////////////////////////////
+  std::ostream* m_defaultStream = &std::cout; ///< Pointer to the output stream.
+  ThresholdMap m_thresholdMap;                ///< Output level threshold map
+
+  /// Private helper class to keep the count of messages of a type (MSG::LEVEL).
+  struct MsgAry final {
+    /// Internal array of counters.
+    std::array<int, MSG::NUM_LEVELS> msg = {{0}};
+    /// Default constructor.
+    MsgAry() = default;
+  };
+
+  std::map<std::string, MsgAry> m_sourceMap;     ///< counts per source
+  std::array<int, MSG::NUM_LEVELS> m_msgCount;   ///< counts per level
+  std::map<size_t, unsigned int> m_msgHashCount; ///< counts per message hash
+
+  bool m_running{false};    ///< are we in running state?
+  bool m_doSuppress{false}; ///< is suppression currently enabled?
+
+  mutable std::recursive_mutex m_reportMutex;       ///< mutex for printing
+  mutable std::recursive_mutex m_thresholdMapMutex; /// (@see MsgStream::doOutput).
+
+  void setupLimits(Gaudi::Details::PropertyBase& prop);
+  void setupThreshold(Gaudi::Details::PropertyBase& prop);
+  bool passErsFilter(const std::string& source, const std::vector<std::string>& filter) const;
+  void i_reportMessage(const Message& msg, int outputLevel);
+  void i_reportERS(const Message& msg) const;
 };
 
 #endif
