0342516e8ad7ba3dadeeed607c978eef423aa752
John Chapman <jchapman@cern.ch>
2019-03-21 14:56:02 +0100

    Add ART tests based on MC16 configuration

---
+++ Simulation/ISF/ISF_Validation/test/test_MC16_FullG4_QS_ttbar.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# art-description: MC16-style simulation using FullG4_LongLived
+# art-include: 21.0/Athena
+# art-include: 21.3/Athena
+# art-include: 21.9/Athena
+# art-include: master/Athena
+# art-type: grid
+# art-output: test.HITS.pool.root
+# art-output: truth.root
+
+# MC16 setup
+# ATLAS-R2-2016-01-00-01 and OFLCOND-MC16-SDR-14
+Sim_tf.py \
+--conditionsTag 'default:OFLCOND-MC16-SDR-14' \
+--physicsList 'FTFP_BERT_ATL' \
+--truthStrategy 'MC15aPlus' \
+--simulator 'FullG4_LongLived' \
+--postInclude 'default:PyJobTransforms/UseFrontier.py' \
+--preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py,SimulationJobOptions/preInclude.FrozenShowersFCalOnly.py' \
+--preExec 'EVNTtoHITS:simFlags.TightMuonStepping=True' \
+--DataRunNumber '284500' \
+--geometryVersion 'default:ATLAS-R2-2016-01-00-01' \
+--inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/SimCoreTests/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.evgen.EVNT.e4993.EVNT.08166201._000012.pool.root.1" \
+--outputHITSFile "Hits.pool.root" \
+--maxEvents 4 \
+--imf False
+
+echo  "art-result: $? simulation"
+
+ArtPackage=$1
+ArtJobName=$2
+# TODO This is a regression test I think. We would also need to compare these files to fixed references
+art.py compare grid  --entries 4 ${ArtPackage} ${ArtJobName}
+
+echo  "art-result: $? regression"
