d7ffeaa669c08d8903c0acd2362c3b6b15340586
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2019-03-21 15:52:58 +0000

    Update job options file to be an ESD from cvmfs

--- Reconstruction/tauRec/share/run_tau_standalone.py
+++ Reconstruction/tauRec/share/run_tau_standalone.py
@@ -83,7 +83,7 @@ svcMgr += CondSvc()
 include( "PerfMonGPerfTools/DisablePerfMon_jobOFragment.py" )
 
 # Input file
-dataFile="/afs/cern.ch/work/a/adbailey/public/ESD/mc16_13TeV.301046.PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYtautau_1000M1250.recon.ESD.e3649_s3170_r9466/ESD.11318157._000005.pool.root.1"
+dataFile="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/mc16_13TeV.361022.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ2W.recon.ESD.e3668_s3170_r10572_homeMade.pool.root"
 
 from AthenaCommon.AthenaCommonFlags  import athenaCommonFlags
 athenaCommonFlags.FilesInput=[dataFile,dataFile]
