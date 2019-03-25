aa25c3ae68768aaefe9e0c16e17f3900f30a0b9e
amorley <anthony.morley@cern.ch>
2019-03-25 08:36:27 +0100

    Update copyright date

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/DerivationFrameworkHiggs/MergedElectronDetailsDecorator.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/DerivationFrameworkHiggs/MergedElectronDetailsDecorator.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 // Tool to decorate the Electrons object with additional information for merged electron ID
