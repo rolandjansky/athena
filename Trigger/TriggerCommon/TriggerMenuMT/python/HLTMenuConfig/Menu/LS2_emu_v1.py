2efee1d0d7e56f709f63ee5e021cc74c1d19fc54
Catrin Bernius <Catrin.Bernius@cern.ch>
2019-03-26 14:27:21 +0100

    fix of copyright year

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/LS2_emu_v1.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/LS2_emu_v1.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 #------------------------------------------------------------------------#
 #------------------------------------------------------------------------#
