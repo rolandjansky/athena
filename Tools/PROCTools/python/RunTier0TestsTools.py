f54c7a7f6d3217afdbce85159663f1c14cfb3cde
John Chapman <jchapman@cern.ch>
2019-03-27 16:25:57 +0100

    Update simulation reference file for 21.3
    The reference file needed to be updated after the sweep of !21465 from 21.0 into 21.3.

--- Tools/PROCTools/python/RunTier0TestsTools.py
+++ Tools/PROCTools/python/RunTier0TestsTools.py
@@ -19,7 +19,7 @@ ciRefFileMap = {
                 'q431-21.3'            : 'v3',
                 # SimulationTier0Test_required-test
                 's3126-21.0'           : 'v2',
-                's3126-21.3'           : 'v1',
+                's3126-21.3'           : 'v2',
                 's3126-21.9'           : 'v1',
                 's3126-22.0'           : 'v1',
                 # OverlayTier0Test_required-test
