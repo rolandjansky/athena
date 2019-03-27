57b67ffccb152b01449c66442679966f6078df0c
scott snyder <scott.snyder@cern.ch>
2019-03-27 16:45:57 +0100

    MuonSimData: Fix gcc9 warnings.
    In C++11, implicit declarations of copy and assignment are deprecated if the
    class has a user defined destructor or copy or assignment.  gcc9 now warns
    about this by default.
    Adjust to avoid the warning.

--- MuonSpectrometer/MuonSimData/src/CscSimData.cxx
+++ MuonSpectrometer/MuonSimData/src/CscSimData.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 // CscSimData.cxx
@@ -21,9 +21,4 @@ CscSimData(std::vector<Deposit>&& deposits, int simDataWord)
 : m_word((deposits.size()<<29 & 0xe0000000) | (simDataWord & 0x1fffffff)) {
   m_deposits = std::move(deposits);
 }
-
-CscSimData::CscSimData(const CscSimData& rhs)
-: m_word(rhs.m_word), m_deposits(rhs.m_deposits) { }
-
-CscSimData::~CscSimData() { }
 
--- MuonSpectrometer/MuonSimData/src/MuonSimData.cxx
+++ MuonSpectrometer/MuonSimData/src/MuonSimData.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /***************************************************************************
@@ -33,15 +33,5 @@ MuonSimData::MuonSimData (std::vector<Deposit>&& deposits,
     : m_word((deposits.size()<<29 & 0xe0000000) | (simDataWord & 0x1fffffff)), m_x(0.0),m_y(0.0),m_z(0.0),m_t(0.0)
 {
   m_deposits = std::move(deposits);
-}
-
-MuonSimData::MuonSimData (const MuonSimData& other)
-    :m_word(other.m_word),
-     m_deposits(other.m_deposits), m_x(other.m_x),m_y(other.m_y),m_z(other.m_z),m_t(other.m_t)
-{}
-
-
-MuonSimData::~MuonSimData()
-{
 }
 
