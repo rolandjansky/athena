0209c4bbeea91bbc04817c39afccbd99351a4c99
Scott Snyder <scott.snyder@cern.ch>
2019-03-27 12:10:51 +0000

    Navigation: Fix gcc9 warnings.

--- Control/Navigation/Navigation/AthenaBarCodeImpl.h
+++ Control/Navigation/Navigation/AthenaBarCodeImpl.h
@@ -1,7 +1,7 @@
 ///////////////////////// -*- C++ -*- /////////////////////////////
 
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef ATHENABARCODEIMPL_H
@@ -53,8 +53,7 @@ public:
 public:
   //Constructors
   
-  virtual ~AthenaBarCodeImpl() {
-  }
+  virtual ~AthenaBarCodeImpl() = default;
   
   AthenaBarCodeImpl();
   
--- Control/Navigation/Navigation/Navigable.h
+++ Control/Navigation/Navigation/Navigable.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef NAVIGATION_NAVIGABLE_H
@@ -165,7 +165,7 @@ class Navigable : virtual public INavigable
 
   Navigable();
 
-  virtual ~Navigable();
+  virtual ~Navigable() = default;
 
   ////////////////////////////////////////////////////////////////////
   // NavigationToken handling (INavigable interface implementation) //
