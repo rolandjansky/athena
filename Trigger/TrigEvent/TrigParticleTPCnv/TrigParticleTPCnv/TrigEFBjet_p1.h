a984a4cbf26d1d6c80cb3b72ea54b202538b3546
scott snyder <scott.snyder@cern.ch>
2019-03-29 15:25:46 +0100

    TrigParticleTPCnv: Fix gcc9 warnings.
    In C++11, implicit declarations of copy and assignment are deprecated if the
    class has a user defined destructor or copy or assignment.  gcc9 now warns
    about this by default.
    Adjust to avoid the warning.

--- Trigger/TrigEvent/TrigParticleTPCnv/TrigParticleTPCnv/TrigEFBjet_p1.h
+++ Trigger/TrigEvent/TrigParticleTPCnv/TrigParticleTPCnv/TrigEFBjet_p1.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /**********************************************************************************
@@ -25,7 +25,7 @@ class TrigEFBjet_p1
  public:
   
   TrigEFBjet_p1() {}
-  virtual ~TrigEFBjet_p1(){}
+  virtual ~TrigEFBjet_p1() = default;
  
   //private:
 
