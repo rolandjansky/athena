70d5e28d390d405c0adb932974f69e20c4c3a570
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2019-03-29 09:48:30 +0100

    fix syntax error in test_pythia8_jets.sh

--- Generators/EvgenJobTransforms/test/test_pythia8_jets.sh
+++ Generators/EvgenJobTransforms/test/test_pythia8_jets.sh
@@ -8,8 +8,7 @@
 ## Any arguments are considered overrides, and will be added at the end
 export TRF_ECHO=True;
 Generate_tf.py --ecmEnergy=13000 --runNumber=369000 --maxEvents=5 \
-    --jobConfig=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC15JobOptions/latest/share/DSID369xxx/MC15.369000.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0.py 
-\
+    --jobConfig=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC15JobOptions/latest/share/DSID369xxx/MC15.369000.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0.py \
     --outputEVNTFile=test_jets.EVNT.pool.root \
 
 echo "art-result: $? generate"
