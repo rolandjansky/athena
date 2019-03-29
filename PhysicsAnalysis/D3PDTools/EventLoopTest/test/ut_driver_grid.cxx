4a96fc3cd31f6581fa9bb8c23f908a70a0c58a8b
Nils Krumnack <Nils.Erik.Krumnack@cern.ch>
2019-03-29 13:58:56 +0100

    clean up comments
    Requested in merge review.

--- PhysicsAnalysis/D3PDTools/EventLoopTest/test/ut_driver_grid.cxx
+++ PhysicsAnalysis/D3PDTools/EventLoopTest/test/ut_driver_grid.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 
@@ -30,6 +30,5 @@ int main ()
   UnitTest ut ("grid");
   ut.gridInput = true;
   // ut.cleanup = false;
-  // ut.location = "$HOME/unit-test.$$";
   return ut.run (driver);
 }
