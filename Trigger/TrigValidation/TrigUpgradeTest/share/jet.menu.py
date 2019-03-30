b49d15f810e43da34f8fd7558064c3e27c407d63
sherwood <peter.sherwood@cern.ch>
2019-03-30 09:34:45 +0100

    adding chains to the jet menu

--- Trigger/TrigValidation/TrigUpgradeTest/share/jet.menu.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/jet.menu.py
@@ -16,7 +16,13 @@ step1=ChainStep("Step1_jet", [jetSeq1])
 
 testChains  = [
     Chain(name='HLT_j85',  Seed="L1_J20",  ChainSteps=[step1]  ),
-    Chain(name='HLT_j45', Seed="L1_J20",  ChainSteps=[step1]  )
+    Chain(name='HLT_j45', Seed="L1_J20",  ChainSteps=[step1]  ),
+    Chain(name='HLT_j420', Seed='L1_J100', ChainSteps=[step1] ),
+    Chain(name='HLT_j225_gsc420_boffperf_split', Seed='L1_J100', ChainSteps=[step1] ),
+    # Chain(name='HLT_j260_320eta490', Seed='L1_100', ChainSteps=[step1] ),
+
+    # Chain(name='HLT_3j200', Seed='L1_100', ChainSteps=[step1] ),
+    # Chain(name='HLT_5j70_0eta240_L14J15', Seed='L1_100', ChainSteps=[step1] ),
     ]
     
 
