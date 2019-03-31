2f80ee99af8b7ec3d32dd9f5342d96e0d43c2ef2
Piotr Sarna <piotr.sarna@cern.ch>
2019-03-31 21:51:38 +0200

    Add missing unit tests for HistogramFactory

--- Control/AthenaMonitoring/test/HistogramFactoryTestSuite.cxx
+++ Control/AthenaMonitoring/test/HistogramFactoryTestSuite.cxx
@@ -46,6 +46,12 @@ class HistogramFactoryTestSuite {
         REGISTER_TEST_CASE(test_shouldProperlyFormatPathForOnlineHistograms),
         REGISTER_TEST_CASE(test_shouldProperlyFormatPathForDefaultHistograms),
         REGISTER_TEST_CASE(test_shouldProperlyFormatPathForCustomHistograms),
+        REGISTER_TEST_CASE(test_shouldSetXAxisLabelsFor1DHistogram),
+        REGISTER_TEST_CASE(test_shouldSetXAndYAxisLabelsFor2DHistogram),
+        REGISTER_TEST_CASE(test_shouldSetExtendAxesWhenkCanRebinIsSet),
+        REGISTER_TEST_CASE(test_shouldNotSetExtendAxesWhenkCanRebinIsNotSet),
+        REGISTER_TEST_CASE(test_shouldSetSumw2WhenSumw2IsSet),
+        REGISTER_TEST_CASE(test_shouldNotSetSumw2WhenSumw2IsNotSet),
       };
     }
 
@@ -62,55 +68,55 @@ class HistogramFactoryTestSuite {
     void test_shouldRegisterAndReturnTH1FHistogram() {
       TH1F* const histogram = createHistogram<TH1F>("TH1F");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH1F")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH1F*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTH1DHistogram() {
       TH1D* const histogram = createHistogram<TH1D>("TH1D");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH1D")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH1D*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTH1IHistogram() {
       TH1I* const histogram = createHistogram<TH1I>("TH1I");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH1I")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH1I*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTH2FHistogram() {
       TH2F* const histogram = createHistogram<TH2F>("TH2F");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH2F")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH2F*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTH2DHistogram() {
       TH2D* const histogram = createHistogram<TH2D>("TH2D");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH2D")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH2D*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTH2IHistogram() {
       TH2I* const histogram = createHistogram<TH2I>("TH2I");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TH2I")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TH2I*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTProfileHistogram() {
       TProfile* const histogram = createHistogram<TProfile>("TProfile");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TProfile")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TProfile*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTProfile2DHistogram() {
       TProfile2D* const histogram = createHistogram<TProfile2D>("TProfile2D");
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TProfile2D")) EXPECTED(true);
-      VALUE(histogram) NOT_EXPECTED((TProfile2D*)nullptr);
+      VALUE(histogram) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldRegisterAndReturnTEfficiencyHistogram() {
       TEfficiency* const graph = createHistogram<TEfficiency>("TEfficiency");
       // VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/TEfficiency")) EXPECTED(true);
-      VALUE(graph) NOT_EXPECTED((TEfficiency*)nullptr);
+      VALUE(graph) NOT_EXPECTED(nullptr);
     }
 
     void test_shouldThrowExceptionForUnknownHistogramType() {
@@ -159,6 +165,76 @@ class HistogramFactoryTestSuite {
       histogramDef.alias = "customAlias";
       m_testObj->create(histogramDef);
       VALUE(m_histSvc->exists("/HistogramFactoryTestSuite/custom/path/for/histogram/customAlias")) EXPECTED(true);
+    }
+
+    void test_shouldSetXAxisLabelsFor1DHistogram() {
+      HistogramDef histogramDef = defaultHistogramDef("TH1F");
+      histogramDef.alias = "labels1DTestAlias";
+      histogramDef.xbins = 3;
+      histogramDef.ybins = 0;
+      histogramDef.labels = { "label1", "label2", "label3", "label4" };
+      TH1F* const histogram = dynamic_cast<TH1F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->GetXaxis()->GetNbins()) EXPECTED(3);
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(1))) EXPECTED("label1");
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(2))) EXPECTED("label2");
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(3))) EXPECTED("label3");
+      VALUE(histogram->GetYaxis()->GetNbins()) EXPECTED(1);
+      VALUE(string(histogram->GetYaxis()->GetBinLabel(1))) EXPECTED("label4");
+    }
+
+    void test_shouldSetXAndYAxisLabelsFor2DHistogram() {
+      HistogramDef histogramDef = defaultHistogramDef("TH2F");
+      histogramDef.alias = "labels2DTestAlias";
+      histogramDef.xbins = 3;
+      histogramDef.ybins = 3;
+      histogramDef.labels = { "label1", "label2", "label3", "label4", "label5", "label6" };
+      TH2F* const histogram = dynamic_cast<TH2F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->GetXaxis()->GetNbins()) EXPECTED(3);
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(1))) EXPECTED("label1");
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(2))) EXPECTED("label2");
+      VALUE(string(histogram->GetXaxis()->GetBinLabel(3))) EXPECTED("label3");
+      VALUE(histogram->GetYaxis()->GetNbins()) EXPECTED(3);
+      VALUE(string(histogram->GetYaxis()->GetBinLabel(1))) EXPECTED("label4");
+      VALUE(string(histogram->GetYaxis()->GetBinLabel(2))) EXPECTED("label5");
+      VALUE(string(histogram->GetYaxis()->GetBinLabel(3))) EXPECTED("label6");
+    }
+
+    void test_shouldSetExtendAxesWhenkCanRebinIsSet() {
+      HistogramDef histogramDef = defaultHistogramDef("TH1F");
+      histogramDef.alias = "allAxesRebinAlias";
+      histogramDef.opt = "kCanRebin";
+      TH1F* const histogram = dynamic_cast<TH1F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->CanExtendAllAxes()) EXPECTED(true);
+    }
+
+    void test_shouldNotSetExtendAxesWhenkCanRebinIsNotSet() {
+      HistogramDef histogramDef = defaultHistogramDef("TH1F");
+      histogramDef.alias = "noAxesRebinAlias";
+      histogramDef.opt = "";
+      TH1F* const histogram = dynamic_cast<TH1F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->CanExtendAllAxes()) EXPECTED(false);
+    }
+
+    void test_shouldSetSumw2WhenSumw2IsSet() {
+      HistogramDef histogramDef = defaultHistogramDef("TH1F");
+      histogramDef.alias = "Sumw2ActiveAlias";
+      histogramDef.opt = "Sumw2";
+      TH1F* const histogram = dynamic_cast<TH1F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->GetSumw2N()) EXPECTED(3);
+    }
+
+    void test_shouldNotSetSumw2WhenSumw2IsNotSet() {
+      HistogramDef histogramDef = defaultHistogramDef("TH1F");
+      histogramDef.alias = "Sumw2InactiveAlias";
+      histogramDef.opt = "";
+      TH1F* const histogram = dynamic_cast<TH1F*>(m_testObj->create(histogramDef));
+ 
+      VALUE(histogram->GetSumw2N()) EXPECTED(0);
     }
 
   // ==================== Helper methods ====================
