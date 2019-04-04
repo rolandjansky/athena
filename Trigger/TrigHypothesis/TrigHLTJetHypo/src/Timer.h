84ee9600c1407b8f993b5c0122258947873741a1
scott snyder <sss@karma>
2019-04-04 19:18:25 +0200

    TrigHLTJetHypo: Fix placement of using directive.
    Do not put using directives in headers in the global namespace.

--- Trigger/TrigHypothesis/TrigHLTJetHypo/src/Timer.h
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/Timer.h
@@ -9,7 +9,6 @@
 #include <vector>
 #include <string>
 
-using namespace std::chrono;
 class JetTrigTimer{
  public:
   void start();
@@ -17,8 +16,8 @@ class JetTrigTimer{
   std::string readAndReset();
   void reset() noexcept;
  private:
-  system_clock::time_point m_start;
-  system_clock::time_point m_stop;
+  std::chrono::system_clock::time_point m_start;
+  std::chrono::system_clock::time_point m_stop;
   bool m_isRunning{false};
   double m_delta{0.};
   std::size_t m_nCalls{0};
