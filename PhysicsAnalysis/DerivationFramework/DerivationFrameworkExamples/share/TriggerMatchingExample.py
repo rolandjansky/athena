8a3e065e88915314550b91d8ae27532e1bde993e
Jon Burr <jonathan.thomas.burr@cern.ch>
2019-04-04 12:07:53 +0000

    Add copyright

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExamples/share/TriggerMatchingExample.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExamples/share/TriggerMatchingExample.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+
 #====================================================================
 # TriggerMatchingExample.py
 # This an example job options script showing how to set up trigger
