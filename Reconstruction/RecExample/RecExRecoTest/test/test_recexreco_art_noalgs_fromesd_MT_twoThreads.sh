8ec196312097454313340645a129e15b04722c2b
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2019-04-05 10:44:38 +0100

    Add tests with two threads for master.

---
+++ Reconstruction/RecExample/RecExRecoTest/test/test_recexreco_art_noalgs_fromesd_MT_twoThreads.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# art-description: Athena runs topoclustering from an ESD file
+# art-type: grid
+# art-include: master/Athena
+
+athena --threads=2 RecExRecoTest/RecExRecoTest_ART_noAlgs_fromESD.py
+echo "art-result: $?"
