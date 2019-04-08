735844457ccf0cd51b02f2dede8c0caa1917765c
Tadej Novak <tadej.novak@cern.ch>
2019-04-08 11:23:35 +0200

    Add PFlow jets to EXOT12, EXOT19 and EXOT22

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/python/EXOT22ContentList.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkExotics/python/EXOT22ContentList.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 EXOT22Content = []
 
@@ -21,8 +21,11 @@ EXOT22SmartCollections = [
     "Electrons",
     "Muons",
     "MET_Reference_AntiKt4EMTopo",
+    "MET_Reference_AntiKt4EMPFlow",
     "BTagging_AntiKt4EMTopo",
+    "BTagging_AntiKt4EMPFlow",
     "AntiKt4EMTopoJets",
+    "AntiKt4EMPFlowJets",
     "PrimaryVertices",
     "InDetTrackParticles"
 ]
