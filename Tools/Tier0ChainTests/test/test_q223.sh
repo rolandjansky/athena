0548ef21bdf4e8bde365e194e22ed1094f11ae6a
John Kenneth Anders <janders@lxplus716.cern.ch>
2019-04-09 14:49:57 +0200

    Updated some Master ART tests to be in line with current 21.0 tests

--- Tools/Tier0ChainTests/test/test_q223.sh
+++ Tools/Tier0ChainTests/test/test_q223.sh
@@ -8,14 +8,10 @@
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
 
-Reco_tf.py --AMI=q223 --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data15_comm.00264034.physics_MinBias.daq.RAW._lb0644._SFO-6._0001.data --imf False
-echo "art-result: $?"
+Reco_tf.py --AMI=q223 --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
+echo "art-result: $? Reco"
 
 ArtPackage=$1
 ArtJobName=$2
 art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
-echo "art-result: $?"
-
-art.py compare grid --days=3 --entries 10 ${ArtPackage} ${ArtJobName}
-echo "art-result: $?"
-
+echo "art-result: $? Diff"
--- Tools/Tier0ChainTests/test/test_q223_mt.sh
+++ Tools/Tier0ChainTests/test/test_q223_mt.sh
@@ -8,10 +8,12 @@
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
 
-Reco_tf.py --AMI=q431 --outputTAGFile=myTAG.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
+Reco_tf.py --AMI=q223 --athenaopts='--threads=1' --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
 echo "art-result: $? Reco"
 
 ArtPackage=$1
 ArtJobName=$2
 art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
 echo "art-result: $? Diff"
+
+
