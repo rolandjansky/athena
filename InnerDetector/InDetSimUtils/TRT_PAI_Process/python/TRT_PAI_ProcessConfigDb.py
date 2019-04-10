48f7e532d457a10a54730f1971842de7ecbd5708
John Chapman <jchapman@cern.ch>
2019-04-10 17:54:20 +0200

    Add CfgGetter methods for TRT_PAI_Process

---
+++ InnerDetector/InDetSimUtils/TRT_PAI_Process/python/TRT_PAI_ProcessConfigDb.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.CfgGetter import addTool
+addTool("TRT_PAI_Process.TRT_PAI_ProcessConfig.getTRTPAI_Process_Xe", "TRTPAI_Process_Xe")
+addTool("TRT_PAI_Process.TRT_PAI_ProcessConfig.getTRTPAI_Process_Ar", "TRTPAI_Process_Ar")
+addTool("TRT_PAI_Process.TRT_PAI_ProcessConfig.getTRTPAI_Process_Kr", "TRTPAI_Process_Kr")
