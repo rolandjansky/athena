2fb53027e1f7e527d23be904e05e5fe412220bb3
Rafal Bielski <rafal.bielski@cern.ch>
2019-04-11 18:58:12 +0200

    fix missing argument handling

--- Trigger/TrigValidation/TrigValTools/bin/chainDumpWorkaround.sh
+++ Trigger/TrigValidation/TrigValTools/bin/chainDumpWorkaround.sh
@@ -4,14 +4,14 @@
 # Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 #
 
-# 
+#
 # This script is a temporary workaround to generate counts file HLTChain.txt from TrigUpgradeTest expert-monitoring.root
 # It will be needed only as long as we don't implement the ChainAcceptance histogram in the HLT monitoring
 #
 
 histFile=$1
-if [ ! -f ${histFile} ]; then
-  echo "Usage: $0 file.root"
+if [ -z "${histFile}" ] || [ ! -f ${histFile} ]; then
+  echo "Usage: $(basename -- $0) file.root"
   exit 1
 fi
 
@@ -22,4 +22,3 @@ TH1* h1d=(TH1*)h2d->ProjectionX(\"\",biny,biny); \
 for (int i=0; i<=h1d->GetNbinsX(); ++i) {int num=h1d->GetBinContent(i); printf(\"%s %d\\n\",h1d->GetXaxis()->GetBinLabel(i),num);}"
 
 root -b -l -q -e "${rootScript}" | grep 'HLT_' | sort > HLTChain.txt
-
