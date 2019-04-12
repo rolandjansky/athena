f6d58e79d99f0ff0450be0ad7193130f8387d8cf
Rafal Bielski <rafal.bielski@cern.ch>
2019-04-12 19:59:56 +0200

    clean up TriggerTest

--- Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_aod_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_aod_grid.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# art-description: Physics v7 TriggerTest on data
+# art-description: Physics v7 TriggerTest on MC to AOD
 # art-type: grid
+# art-include: 21.1/AthenaP1
 # art-include: 21.0/Athena
-# art-include: 21.3/Athena
+# art-include: 21.0-TrigMC/Athena
 # art-include: master/Athena
 # art-output: HLTChain.txt
 # art-output: HLTTE.txt
 # art-output: L1AV.txt
-# art-output: HLTconfig_*.xml
+# art-output: HLTconfig*.xml
 # art-output: L1Topoconfig*.xml
 # art-output: LVL1config*.xml
 # art-output: *.log
@@ -19,14 +20,10 @@
 # art-output: TotalEventsProcessed.txt
 # art-output: *.regtest.new
 
-export NAME="data_physics_pp_v7_grid"
-export MENU="Physics_pp_v7"
-export EVENTS="1000"
-export INPUT="data"
-
-# The flag below skips HLTTE and L1 counts checks, but keeps the HLTChain counts check.
-# This is needed because we run on data without rerunLvl1, so we only produce HLTChain counts.
-export SKIP_CHAIN_DUMP=2
+export NAME="physics_pp_v7_primaries_aod_grid"
+export MENU="Physics_pp_v7_primaries"
+export EVENTS="500"
+export JOBOPTION="TriggerTest/testCommonSliceAthenaTrigRDOtoAOD.py"
 
 source exec_athena_art_trigger_validation.sh
 source exec_art_triggertest_post.sh
