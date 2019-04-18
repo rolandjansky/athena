412bef09dcd3163edfe18d489c55a8b72c31d1e6
Nicola Orlando <nicola.orlando@cern.ch>
2019-04-18 13:44:34 +0000

    Update EnableTopoAlgorithms.py

--- Trigger/TrigT1/TrigT1CaloFexSim/share/EnableTopoAlgorithms.py
+++ Trigger/TrigT1/TrigT1CaloFexSim/share/EnableTopoAlgorithms.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 from L1TopoSimulation.L1TopoSimulationConfig import L1TopoSimulation
 include ("L1TopoSimulation/L1Simulation_topOptions.py")
