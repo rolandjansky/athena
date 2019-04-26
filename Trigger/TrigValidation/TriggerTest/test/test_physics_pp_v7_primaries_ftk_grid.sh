45f7c5c03b27c06370cf75627d33a699891ec1a8
Julie Kirk <Julie.Kirk@cern.ch>
2019-04-26 14:52:49 +0200

    Fix naming of primaries, FTK tests

--- Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_ftk_grid.sh
+++ Trigger/TrigValidation/TriggerTest/test/test_physics_pp_v7_primaries_ftk_grid.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# art-description: MC v7 no prescales TriggerTest on FTK MC to AOD
-# art-type: build
+# art-description: Physics_pp_v7_primaries TriggerTest on FTK MC
+# art-type: grid
 # art-include: 21.1/AthenaP1
 # art-include: 21.0/Athena
+# art-include: 21.3/Athena
 # art-include: 21.0-TrigMC/Athena
 # art-include: master/Athena
 # art-output: HLTChain.txt
@@ -20,12 +21,9 @@
 # art-output: TotalEventsProcessed.txt
 # art-output: *.regtest.new
 
-export NAME="mc_pp_v7_nops_aod_ftk_build"
+export NAME="mc_pp_v7_primaries_ftk_grid"
 export MENU="Physics_pp_v7_primaries"
 export INPUT="ftk"
-export JOBOPTIONS="TriggerTest/testCommonSliceAthenaTrigRDOtoAOD.py"
-export EVENTS="5"
-export COST_MONITORING="False"
 export EXTRA="from TriggerJobOpts.TriggerFlags import TriggerFlags;TriggerFlags.doFTK=True;from RecExConfig.RecFlags import rec;rec.doFTK.set_Value_and_Lock(True);"
 
 source exec_athena_art_trigger_validation.sh
