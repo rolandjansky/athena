83cb2f98f0f24c6fb4448a697ee25c66ee1b2d08
Dan Guest <dguest@cern.ch>
2019-04-27 23:20:59 +0200

    Add paths to calibration dev area for smt and dl1rmu

--- PhysicsAnalysis/JetTagging/JetTagAlgs/BTagging/python/JetCollectionToTrainingMaps.py
+++ PhysicsAnalysis/JetTagging/JetTagAlgs/BTagging/python/JetCollectionToTrainingMaps.py
@@ -1,6 +1,11 @@
 preTagDL2JetToTrainingMap={
-    'AntiKt4EMPFlow': ['dev/flavtag/march2018/iprnn/antikt4emtopo/lwtnn-model.json']
+    'AntiKt4EMPFlow': [
+        'dev/flavtag/march2018/iprnn/antikt4emtopo/lwtnn-model.json',
+        'dev/flavtag/april2019/smt/antikt4empflow/extended-hybrid.json',
+    ]
 }
 postTagDL2JetToTrainingMap={
-    'AntiKt4EMPFlow': []
+    'AntiKt4EMPFlow': [
+        'dev/flavtag/april2019/dl1rmu/antikt4empflow/preliminary-network.json',
+    ]
 }
