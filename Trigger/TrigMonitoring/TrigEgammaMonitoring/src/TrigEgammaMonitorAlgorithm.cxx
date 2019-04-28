c0633a5006c356703b47a1411065938fa85ddf9e
Elin Bergeaas Kuutmann <elin.bergeaas.kuutmann@physics.uu.se>
2019-04-28 22:58:16 +0200

    Setting up example for trigger DQ ws 29 April 2019

---
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/src/TrigEgammaMonitorAlgorithm.cxx
@@ -0,0 +1,47 @@
+/*
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "TrigEgammaMonitorAlgorithm.h"
+
+TrigEgammaMonitorAlgorithm::TrigEgammaMonitorAlgorithm( const std::string& name, ISvcLocator* pSvcLocator )
+  : AthMonitorAlgorithm(name,pSvcLocator)
+{}
+
+
+TrigEgammaMonitorAlgorithm::~TrigEgammaMonitorAlgorithm() {}
+
+
+StatusCode TrigEgammaMonitorAlgorithm::initialize() {
+    return AthMonitorAlgorithm::initialize();
+}
+
+
+StatusCode TrigEgammaMonitorAlgorithm::fillHistograms( const EventContext& ctx ) const {
+    using namespace Monitored;
+
+    // Declare the quantities which should be monitored
+    //NB! The variables and histograms defined here must match the ones in the py file exactly!
+    auto lumiPerBCID = Monitored::Scalar<float>("lumiPerBCID",0.0);
+    auto lb = Monitored::Scalar<int>("lb",0);
+    auto run = Monitored::Scalar<int>("run",0);
+
+    // Two variables (value and passed) needed for TEfficiency
+    auto pT = Monitored::Scalar<float>("pT",0.0);
+    auto pT_passed = Monitored::Scalar<float>("pT_passed",false);
+
+    //// Set the values of the monitored variables for the event		
+    lumiPerBCID = lbAverageInteractionsPerCrossing();
+    lb = GetEventInfo(ctx)->lumiBlock();
+    run = GetEventInfo(ctx)->runNumber();
+    
+    // Fill. First argument is the tool (GMT) name as defined in the py file, 
+    // all others are the variables to be saved.
+    fill("TrigEgammaMonitor",lumiPerBCID,lb);
+
+    // Alternative fill method. Get the group yourself, and pass it to the fill function.
+    auto tool = getGroup("TrigEgammaMonitor");
+    fill(tool,run);
+
+    return StatusCode::SUCCESS;
+}
---
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/src/components/TrigEgammaMonitoring_entries.cxx
@@ -0,0 +1,9 @@
+/*
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "../TrigEgammaMonitorAlgorithm.h"
+
+
+DECLARE_COMPONENT( TrigEgammaMonitorAlgorithm )
+
