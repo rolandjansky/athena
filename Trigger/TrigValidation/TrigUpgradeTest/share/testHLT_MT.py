eec27761e6f8045184f82b5c3f100a8ea6b0ed4c
scott snyder <snyder@bnl.gov>
2019-04-29 18:21:15 +0200

    TrigUpgradeTest: Disable configuration of offset correction for tests.

--- Trigger/TrigValidation/TrigUpgradeTest/share/testHLT_MT.py
+++ Trigger/TrigValidation/TrigUpgradeTest/share/testHLT_MT.py
@@ -119,6 +119,7 @@ jobproperties.Beam.beamType = 'collisions'
 jobproperties.Beam.bunchSpacing = 25
 globalflags.DatabaseInstance='CONDBR2' if opt.useCONDBR2 else 'COMP200'
 athenaCommonFlags.isOnline.set_Value_and_Lock(opt.isOnline)
+TriggerFlags.doCaloOffsetCorrection = False
 
 log.info('Configured the following global flags:')
 globalflags.print_JobProperties()
