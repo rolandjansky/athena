da367cf1a49d573c2693f53cf24ff89b09ad0be1
pyin <pengqi.yin@cern.ch>
2019-04-29 16:44:37 +0200

    add new header file to do our own Msg. it will return what we want to print to a string and then will be called in tool by AthenaMSG there

---
+++ ForwardDetectors/ZDC/ZdcAnalysis/ZdcAnalysis/ZDCMsg.h
@@ -0,0 +1,11 @@
+#ifndef _ZDCMsg_h
+#define _ZDCMsg_h
+
+namespace ZDCMsg {
+
+  typedef bool (*MessageFunctionPtr)(unsigned int, std::string);
+
+  enum MSGLevels {Error=0, Warn, Info, Verbose, Debug, Fatal=9};
+};
+
+#endif
