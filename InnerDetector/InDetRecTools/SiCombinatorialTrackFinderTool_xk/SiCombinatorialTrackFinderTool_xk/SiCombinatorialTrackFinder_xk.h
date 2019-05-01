9671d096be9ee2f995cc3f9f96cc2018394fdcdc
Susumu Oda <susumu.oda@cern.ch>
2019-05-01 13:52:36 +0000

    Cleanup of SiCombinatorialTrackFinder_xk class

--- InnerDetector/InDetRecTools/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinder_xk.h
+++ InnerDetector/InDetRecTools/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinderTool_xk/SiCombinatorialTrackFinder_xk.h
@@ -1,5 +1,7 @@
+// -*- C++ -*-
+
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -15,26 +17,28 @@
 #ifndef SiCombinatorialTrackFinder_xk_H
 #define SiCombinatorialTrackFinder_xk_H
 
-#include <list>
-#include <vector>
-#include <map>
-#include "GaudiKernel/ToolHandle.h"
-#include "GaudiKernel/ServiceHandle.h"
-#include "StoreGate/ReadCondHandleKey.h"
-#include "StoreGate/ReadHandleKey.h"
-#include "MagFieldInterfaces/IMagFieldSvc.h"
-#include "AthenaBaseComps/AthAlgTool.h"
-#include "InDetReadoutGeometry/SiDetectorElementCollection.h"
 #include "InDetRecToolInterfaces/ISiCombinatorialTrackFinder.h"
+#include "AthenaBaseComps/AthAlgTool.h"
 
+#include "InDetReadoutGeometry/SiDetectorElementCollection.h"
+#include "MagFieldInterfaces/IMagFieldSvc.h"
 #include "SiCombinatorialTrackFinderTool_xk/SiTrajectory_xk.h"
 #include "SiCombinatorialTrackFinderTool_xk/SiTools_xk.h"
 #include "SiCombinatorialTrackFinderTool_xk/SiDetElementBoundaryLink_xk.h"
 #include "SiCombinatorialTrackFinderTool_xk/SiDetElementBoundaryLinks_xk.h"
+#include "StoreGate/ReadCondHandleKey.h"
+#include "StoreGate/ReadHandleKey.h"
+
+#include "GaudiKernel/ServiceHandle.h"
+#include "GaudiKernel/ToolHandle.h"
+
+#include <list>
+#include <map>
+#include <vector>
 
 class MsgStream          ;
 
-namespace InDet{
+namespace InDet {
 
   //class SiClusterContainer;
 
@@ -64,7 +68,7 @@ namespace InDet{
 
       SiCombinatorialTrackFinder_xk
 	(const std::string&,const std::string&,const IInterface*);
-      virtual ~SiCombinatorialTrackFinder_xk();
+      virtual ~SiCombinatorialTrackFinder_xk() = default;
       virtual StatusCode initialize();
       virtual StatusCode finalize  ();
 
@@ -113,62 +117,70 @@ namespace InDet{
       // Protected Data
       ///////////////////////////////////////////////////////////////////
 
-      ToolHandle<IInDetConditionsTool>        m_pixelCondSummaryTool;
-      ToolHandle<IInDetConditionsTool>    m_sctCondSummaryTool{this, "SctSummaryTool",
+      ServiceHandle<MagField::IMagFieldSvc>  m_fieldServiceHandle{this, "MagFieldSvc",
+          "AtlasFieldSvc"};
+      ToolHandle<IInDetConditionsTool> m_pixelCondSummaryTool{this, "PixelSummaryTool",
+          "PixelConditionsSummaryTool"};
+      ToolHandle<IInDetConditionsTool> m_sctCondSummaryTool{this, "SctSummaryTool",
           "InDetSCT_ConditionsSummaryTool/SCT_ConditionsSummaryTool", "Tool to retrieve SCT Conditions summary"};
-      ServiceHandle<MagField::IMagFieldSvc>  m_fieldServiceHandle;
-      ToolHandle<Trk::IPatternParametersPropagator> m_proptool   ;
-      ToolHandle<Trk::IPatternParametersUpdator>    m_updatortool;
-      ToolHandle<Trk::IRIO_OnTrackCreator>          m_riocreator ;
-      ToolHandle<Trk::IPRD_AssociationTool>         m_assoTool   ;
+      PublicToolHandle<Trk::IPatternParametersPropagator> m_proptool{this, "PropagatorTool",
+          "Trk::RungeKuttaPropagator/InDetPropagator"};
+      PublicToolHandle<Trk::IPatternParametersUpdator> m_updatortool{this, "UpdatorTool",
+          "Trk::KalmanUpdator_xk/InDetPatternUpdator"};
+      PublicToolHandle<Trk::IRIO_OnTrackCreator> m_riocreator{this, "RIOonTrackTool",
+          "Trk::RIO_OnTrackCreator/RIO_OnTrackCreator"};
+      PublicToolHandle<Trk::IPRD_AssociationTool> m_assoTool{this, "AssosiationTool",
+          "InDet::InDetPRD_AssociationToolGangedPixels"};
 
-      bool                           m_usePIX        ;
-      bool                           m_useSCT        ;
-      int                            m_outputlevel   ;
-      int                            m_nprint        ;  // Kind output information
-      int                            m_inputseeds    ;  // Number input seeds
-      int                            m_goodseeds     ;  // Number accepted seeds
-      int                            m_findtracks    ;  // Number found tracks
-      int                            m_inittracks    ;  // Number initial tracks
-      int                            m_roadbug       ;  // Number wrong DE roads
-      int                            m_cosmicTrack   ;  // Is it cosmic track (0 or 1)
-      SiTrajectory_xk                m_trajectory    ;  // Track trajector
-      std::string                    m_fieldmode     ;  // Mode of magnetic field
-//      std::string                    m_pixelname     ;  // Name container with pixels 
-//      std::string                    m_sctname       ;  // Name container with scts
-      std::string                    m_pixm          ;  // PIX manager   location
-      std::string                    m_sctm          ;  // SCT manager   location
-      std::string                    m_callbackString;
-      Trk::TrackInfo                 m_trackinfo     ;
-
-      InDet::SiTools_xk              m_tools         ;
-      bool                           m_pix           ;
-      bool                           m_sct           ;
-      bool                           m_simpleTrack   ;
-      bool                           m_heavyion      ;
-
-      SG::ReadHandleKey<InDet::SiClusterContainer>  m_pixcontainerkey  ;
-      SG::ReadHandleKey<InDet::SiClusterContainer>  m_sctcontainerkey  ;
-      MagField::IMagFieldSvc*        m_fieldService  ;
-      Trk::MagneticFieldProperties   m_fieldprop     ; // Magnetic field properties
-      double                         m_xi2max        ; // max Xi2 for updators
-      double                         m_xi2maxNoAdd   ; // max Xi2 for clusters
-      double                         m_xi2maxlink    ; // max Xi2 for clusters
-      double                         m_pTmin         ; // min pT
-      double                         m_pTminBrem     ; // min pT for brem noise model
-      double                         m_qualityCut    ; // Simple track quality cut
-      int                            m_nholesmax     ; // Max number holes
-      int                            m_dholesmax     ; // Max holes gap
-      int                            m_nclusmin      ; // Min number clusters
-      int                            m_nclusminb     ; // Min number clusters
-      int                            m_nwclusmin     ; // Min number weighted clusters
-      std::list<Trk::Track*>         m_tracks        ; // List found tracks
-      InDet::SiDetElementBoundaryLinks_xk m_boundaryPIX;
+      SG::ReadHandleKey<InDet::SiClusterContainer> m_pixcontainerkey{this, "PixelClusterContainer",
+          "PixelClusters"};
+      SG::ReadHandleKey<InDet::SiClusterContainer> m_sctcontainerkey{this, "SCT_ClusterContainer",
+          "SCT_Clusters"};
       SG::ReadCondHandleKey<InDet::SiDetElementBoundaryLinks_xk> m_boundarySCTKey{this, "SCT_DetElementBoundaryLinks_xk",
           "SCT_DetElementBoundaryLinks_xk", "Key of InDet::SiDetElementBoundaryLinks_xk for SCT"};
       // For P->T converter of SCT_Clusters
       SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_SCTDetEleCollKey{this, "SCTDetEleCollKey",
           "SCT_DetectorElementCollection", "Key of SiDetectorElementCollection for SCT"};
+
+      BooleanProperty m_usePIX{this, "usePixel", true};
+      BooleanProperty m_useSCT{this, "useSCT", true};
+      StringProperty m_fieldmode{this, "MagneticFieldMode", "MapSolenoid", "Mode of magnetic field"};
+      StringProperty m_pixm{this, "PixManagerLocation", "Pixel", "PIX manager location"};
+      StringProperty m_sctm{this, "SCTManagerLocation", "SCT", "SCT manager location"};
+      DoubleProperty m_qualityCut{this, "TrackQualityCut", 9.3, "Simple track quality cut"};
+
+      SiTrajectory_xk                m_trajectory    ;  // Track trajector1
+      Trk::TrackInfo                 m_trackinfo     ;
+      InDet::SiTools_xk              m_tools         ;
+      Trk::MagneticFieldProperties   m_fieldprop     ; // Magnetic field properties
+      std::list<Trk::Track*>         m_tracks        ; // List found tracks
+      InDet::SiDetElementBoundaryLinks_xk m_boundaryPIX;
+
+      int                            m_outputlevel   ;
+      int                            m_nprint        ;  // Kind output information
+      int                            m_inputseeds{0};  // Number input seeds
+      int                            m_goodseeds     ;  // Number accepted seeds
+      int                            m_findtracks{0};  // Number found tracks
+      int                            m_inittracks    ;  // Number initial tracks
+      int                            m_roadbug       ;  // Number wrong DE roads
+      std::string                    m_callbackString;
+
+      bool                           m_pix           ;
+      bool                           m_sct           ;
+      bool                           m_heavyion{false};
+
+      int                            m_cosmicTrack   ;  // Is it cosmic track (0 or 1)
+      int                            m_nclusmin      ; // Min number clusters
+      int                            m_nclusminb     ; // Min number clusters
+      int                            m_nwclusmin     ; // Min number weighted clusters
+      int                            m_nholesmax     ; // Max number holes
+      int                            m_dholesmax     ; // Max holes gap
+      bool                           m_simpleTrack{false};
+      double                         m_pTmin         ; // min pT
+      double                         m_pTminBrem     ; // min pT for brem noise model
+      double                         m_xi2max        ; // max Xi2 for updators
+      double                         m_xi2maxNoAdd   ; // max Xi2 for clusters
+      double                         m_xi2maxlink    ; // max Xi2 for clusters
 
       ///////////////////////////////////////////////////////////////////
       // Methods 
@@ -188,18 +200,18 @@ namespace InDet{
  
       void magneticFieldInit();
 
-      StatusCode mapDetectorElementsProduction(IOVSVC_CALLBACK_ARGS);
+      StatusCode mapDetectorElementsProduction(IOVSVC_CALLBACK_ARGS) ;
 
       bool spacePointsToClusters
 	(const std::list<const Trk::SpacePoint*>&,
-	 std::list<const InDet::SiCluster*>     &); 
+	 std::list<const InDet::SiCluster*>     &) const; 
 
       void detectorElementLinks
 	(std::list<const InDetDD::SiDetectorElement*>        &,
-	 std::list<const InDet::SiDetElementBoundaryLink_xk*>&);
+	 std::list<const InDet::SiDetElementBoundaryLink_xk*>&) const;
 
-      MsgStream&    dumpconditions(MsgStream&    out) const;
-      MsgStream&    dumpevent     (MsgStream&    out) const;
+      MsgStream& dumpconditions(MsgStream& out) const;
+      MsgStream& dumpevent     (MsgStream& out) const;
 
     };
 
