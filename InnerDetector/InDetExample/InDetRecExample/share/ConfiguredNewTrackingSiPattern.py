8e34c9bf8754678d9da25d7cfe77bb7e02d0d688
Susumu Oda <susumu.oda@cern.ch>
2019-05-06 07:59:59 +0000

    Make SeedToTrackConversionTool thread-safe.
    Make SeedToTrackConversionTool const

--- InnerDetector/InDetExample/InDetRecExample/share/ConfiguredNewTrackingSiPattern.py
+++ InnerDetector/InDetExample/InDetRecExample/share/ConfiguredNewTrackingSiPattern.py
@@ -258,9 +258,13 @@ class  ConfiguredNewTrackingSiPattern:
          else:
            InDetSiTrackMaker.TrackPatternRecoInfo = 'SiSPSeededFinder'
 					  
-         if InDetFlags. doStoreTrackSeeds():
-              InDetSiTrackMaker.SeedSegmentsWrite=True
-              InDetSiTrackMaker.SeedToTrackConversion=InDet_SeedToTrackConversion
+         if InDetFlags.doStoreTrackSeeds():
+              from SeedToTrackConversionTool.SeedToTrackConversionToolConf import InDet__SeedToTrackConversionTool
+              InDet_SeedToTrackConversion = InDet__SeedToTrackConversionTool(name = "InDet_SeedToTrackConversion",
+                                                                             OutputName = InDetKeys.SiSPSeedSegments()+NewTrackingCuts.extension())
+              InDetSiTrackMaker.SeedToTrackConversion = InDet_SeedToTrackConversion
+              InDetSiTrackMaker.SeedSegmentsWrite = True
+
          #InDetSiTrackMaker.OutputLevel = VERBOSE				  
          ToolSvc += InDetSiTrackMaker
          if (InDetFlags.doPrintConfigurables()):
