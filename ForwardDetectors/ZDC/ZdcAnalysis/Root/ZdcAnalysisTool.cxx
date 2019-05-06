6476e2bfd8596d98f93f697ee3dc0219d1622272
pyin <pengqi.yin@cern.ch>
2019-05-06 20:37:23 +0200

    update default values

--- ForwardDetectors/ZDC/ZdcAnalysis/Root/ZdcAnalysisTool.cxx
+++ ForwardDetectors/ZDC/ZdcAnalysis/Root/ZdcAnalysisTool.cxx
@@ -347,19 +347,19 @@ std::unique_ptr<ZDCDataAnalyzer> ZdcAnalysisTool::initializePbPb2018()
     const float peak2ndDerivThreshHG = -12;
     const float peak2ndDerivThreshLG = -10;
 
-    ZDCDataAnalyzer::ZDCModuleFloatArray tau1Arr = {4.000, 3.380, 3.661, 3.679,
-                                                    4.472, 4.656, 3.871, 4.061
+    ZDCDataAnalyzer::ZDCModuleFloatArray tau1Arr = {3.877, 3.998, 3.821, 3.858,
+                                                    4.296, 4.064, 3.497, 3.642
                                                    };
 
-    ZDCDataAnalyzer::ZDCModuleFloatArray tau2Arr = {22,    24.81, 24.48, 24.45,
-                                                    24.17, 24.22, 25.46, 24.45
+    ZDCDataAnalyzer::ZDCModuleFloatArray tau2Arr = {24.40, 25.28, 25.66, 24.12,
+                                                    24.42, 24.99, 25.72, 25.29
                                                    };
 
-    ZDCDataAnalyzer::ZDCModuleFloatArray t0HG = {70.00, 72.74, 73.09, 72.25,
-                                                 75.11, 74.94, 73.93, 74.45
+    ZDCDataAnalyzer::ZDCModuleFloatArray t0HG = {70.51, 70.57, 70.13, 69.98,
+                                                 74.18, 72.79, 71.77, 72.62
                                                 };
-    ZDCDataAnalyzer::ZDCModuleFloatArray t0LG = {70.00, 73.41, 74.27, 73.30,
-                                                 76.28, 76.07, 74.98, 76.54
+    ZDCDataAnalyzer::ZDCModuleFloatArray t0LG = {70.70, 70.78, 70.76, 70.91,
+                                                 75.16, 73.71, 72.25, 73.61
                                                 };
 
     // Delta T0 cut
