4863bfaf97d46ed78954b85f0a61b4987207aae6
David Shope <david.richard.shope@cern.ch>
2019-05-06 16:58:24 +0200

    Add logging to HIGG3DxAugmentation

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG3DxAugmentation.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHiggs/python/HIGG3DxAugmentation.py
@@ -8,11 +8,19 @@
 # May 2019                                  #
 #############################################
 
+from AthenaCommon.Logging import logging
+logger = logging.getLogger( 'HIGG3DxAugmentation' )
+
+# for debugging output
+from AthenaCommon.Constants import INFO,WARNING,DEBUG,VERBOSE
+
 def getHIGG3TruthDecoratorTool(toolNamePrefix = "HIGG3DX"):
+    toolName = toolNamePrefix+"TruthDecoratorTool"
     from DerivationFrameworkHiggs.DerivationFrameworkHiggsConf import DerivationFramework__HIGG3TruthDecorator
-    HIGG3DTruthDecoratorTool = DerivationFramework__HIGG3TruthDecorator(name = toolNamePrefix+"TruthDecoratorTool",
-                                                                         OutputLevel       = VERBOSE,
-                                                                         )
+    HIGG3DTruthDecoratorTool = DerivationFramework__HIGG3TruthDecorator(name        = toolName,
+                                                                        OutputLevel = VERBOSE,
+                                                                        )
+    logger.info("Created HIGG3TruthDecorator with name {}".format(toolName))
     return HIGG3DTruthDecoratorTool
 
 
