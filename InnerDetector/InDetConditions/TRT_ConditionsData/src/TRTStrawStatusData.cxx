76598eedec3ce1b8067174371fdfc4a9e1536eb6
Peter Hansen <hansenph@lxplus778.cern.ch>
2019-05-10 20:57:02 +0200

    Replaced std::cout with MsgStream. Removed unused classes.

--- InnerDetector/InDetConditions/TRT_ConditionsData/src/TRTStrawStatusData.cxx
+++ InnerDetector/InDetConditions/TRT_ConditionsData/src/TRTStrawStatusData.cxx
@@ -36,15 +36,10 @@ int TRTStrawStatusData::findStatus(const Identifier offID) const
   if (it == m_statMap.end()){
         stat = 0;
  return stat;   
- //return (*it).second;
   }
   else{
     stat = (*it).second;
-
- //std::cout << "In TRTStrawStatusData Object : Straw with ID"
-  //            << offID << " is found with status : "<<std::endl;
     return stat;
-    //return 0;
  }
 
 
@@ -54,20 +49,18 @@ int TRTStrawStatusData::findStatus(const Identifier offID) const
 std::vector<Identifier>  TRTStrawStatusData::scan_entries(int stat) const
 {
   //this scans the map for all entries with status = stat(1/2/3)
-   //these channels are returned in a vector
+  //these channels are returned in a vector
 
 
   std::vector<Identifier> badChan;
   Identifier offID;
   int status;
   statusMap::const_iterator it;
-  //std::cout << "Printing map" << channel_status.size() << std::endl;
 
   for(it=m_statMap.begin(); it!=m_statMap.end(); ++it){
-    //std::cout << it->first << it->second << std::endl;
    
-     offID = it->first;
-    status    = it->second;
+    offID   = it->first;
+    status  = it->second;
    
  if(status==stat){
       badChan.push_back(offID);
