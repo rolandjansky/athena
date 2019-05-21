7ee7f5ade1f2a8afed97d34eeaa242568fc6c967
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-05-21 14:20:19 +0200

    AthenaMonitoring: Do not hard-code THistSvc type
    Do not hard-code the histogramming service type but only the name. This
    is important to allow for a different online implementation of the
    `THistSvc`. This fix was already done in commit 3f8d79e8 but we somehow
    lost it subsequently.
    
    Fixes crash seen in ADHI-4683.

--- Control/AthenaMonitoring/AthenaMonitoring/GenericMonitoringTool.h
+++ Control/AthenaMonitoring/AthenaMonitoring/GenericMonitoringTool.h
@@ -89,7 +89,8 @@ public:
   virtual const ServiceHandle<ITHistSvc>& histogramService() { return m_histSvc; }
   virtual uint32_t lumiBlock();
 private:
-  ServiceHandle<ITHistSvc> m_histSvc { this, "THistSvc", "THistSvc/THistSvc", "Histogramming svc" };  
+  /// THistSvc (do NOT fix the service type (only the name) to allow for a different implementation online
+  ServiceHandle<ITHistSvc> m_histSvc { this, "THistSvc", "THistSvc", "Histogramming svc" };
   Gaudi::Property<std::string> m_histoPath { this, "HistPath", {}, "Directory for histograms [name of parent if not set]" };
   Gaudi::Property<std::vector<std::string> > m_histograms { this, "Histograms", {},  "Definitions of histograms"};
   Gaudi::Property<bool> m_explicitBooking { this, "ExplicitBooking", false, "Do not create histograms automatically in initialize but wait until the method book is called." };
