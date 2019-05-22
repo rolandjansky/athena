6ee73c95f4f6b3843d34dae0b45f31c9359fd372
Stewart Martin-Haugh <smh@cern.ch>
2019-05-22 20:39:47 +0200

    Update overlay reference file after changes for ATR-19427

--- Tools/PROCTools/python/RunTier0TestsTools.py
+++ Tools/PROCTools/python/RunTier0TestsTools.py
@@ -24,7 +24,7 @@ ciRefFileMap = {
                 's3126-22.0'           : 'v5',
                 # OverlayTier0Test_required-test
                 'overlay-d1498-21.0'   : 'v2',
-                'overlay-d1498-22.0'   : 'v15',
+                'overlay-d1498-22.0'   : 'v16',
                 'overlay-bkg-21.0'     : 'v1',
                 'overlay-bkg-22.0'     : 'v1',
                }
