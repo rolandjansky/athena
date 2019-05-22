eef5a7c995b8a99cd6d0e81481fc322ffde4546f
John Chapman <jchapman@cern.ch>
2019-05-22 13:09:12 +0200

    Use Summary mode for art compare checks in OverlayTests ART jobs

--- Simulation/Tests/OverlayTests/test/test_MCOverlay_MT_ttbar_1thread.sh
+++ Simulation/Tests/OverlayTests/test/test_MCOverlay_MT_ttbar_1thread.sh
@@ -27,5 +27,5 @@ echo "art-result: $? overlaypool"
 
 ArtPackage=$1
 ArtJobName=$2
-art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
+art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
 echo "art-result: $? regression"
