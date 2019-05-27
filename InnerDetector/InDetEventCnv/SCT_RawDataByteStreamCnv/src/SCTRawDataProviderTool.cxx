704882be1e8cd7ef6143ecdc9768e724d419c0b1
Susumu Oda <susumu.oda@cern.ch>
2019-05-27 14:48:43 +0200

    Merge getRobIDSet method into convert method to use std::mutex instead of std::recursive_mutex.

--- InnerDetector/InDetEventCnv/SCT_RawDataByteStreamCnv/src/SCTRawDataProviderTool.cxx
+++ InnerDetector/InDetEventCnv/SCT_RawDataByteStreamCnv/src/SCTRawDataProviderTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "SCTRawDataProviderTool.h"
@@ -10,7 +10,6 @@
 using OFFLINE_FRAGMENTS_NAMESPACE::ROBFragment;
 
 // Constructor
-
 SCTRawDataProviderTool::SCTRawDataProviderTool(const std::string& type, const std::string& name, 
                                                const IInterface* parent) : 
   base_class(type, name, parent)
@@ -18,7 +17,6 @@ SCTRawDataProviderTool::SCTRawDataProviderTool(const std::string& type, const st
 }
 
 // Initialize
-
 StatusCode SCTRawDataProviderTool::initialize()
 {
   ATH_CHECK(m_decoder.retrieve());
@@ -27,7 +25,6 @@ StatusCode SCTRawDataProviderTool::initialize()
 }
 
 // Convert method
-
 StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecROBFrags,
                                            ISCT_RDO_Container& rdoIDCont,
                                            InDetBSErrContainer* errs,
@@ -38,7 +35,6 @@ StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecR
 }
 
 // Convert method
-
 StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecROBFrags,
                                            ISCT_RDO_Container& rdoIDCont,
                                            InDetBSErrContainer* errs,
@@ -50,19 +46,33 @@ StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecR
   
   StatusCode sc{StatusCode::SUCCESS};
 
-  std::lock_guard<std::recursive_mutex> lock{m_mutex};
+  // Retrieve or prepare the already decoded ROBIDs in this thread.
+  EventContext::ContextID_t slot{ctx.slot()};
+  EventContext::ContextEvt_t evt{ctx.evt()};
+  std::lock_guard<std::mutex> lock{m_mutex};
+  if (slot<m_cache.size() and m_cache[slot]==evt) {
+    // Cache is valid. Do nothing
+  } else {
+    // Expand cache if necessary
+    static const EventContext::ContextEvt_t invalidValue{EventContext::INVALID_CONTEXT_EVT};
+    if (slot>=m_cache.size()) {
+      m_cache.resize(slot+1, invalidValue);
+      m_robIDSet.resize(slot+1);
+    }
 
-  std::set<uint32_t>& robIDSet{getRobIDSet(ctx)};
+    // Set event number and clear cache for the new event
+    m_cache[slot] = evt;
+    m_robIDSet[slot].clear();
+  }
+  //  m_robIDSet[slot] is the already decoded ROBIDs in this thread.
 
   // loop over the ROB fragments
-
   std::set<uint32_t> tmpROBIDSet;
-
   for (const ROBFragment* robFrag : vecROBFrags) {
     // get the ID of this ROB/ROD
     uint32_t robid{(robFrag)->rod_source_id()};
     // check if this ROBFragment was already decoded (EF case in ROIs)
-    if (robIDSet.count(robid) or tmpROBIDSet.count(robid)) {
+    if (m_robIDSet[slot].count(robid) or tmpROBIDSet.count(robid)) {
       ATH_MSG_DEBUG(" ROB Fragment with ID  "
                     << std::hex<<robid << std::dec
                     << " already decoded, skip");
@@ -86,7 +96,8 @@ StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecR
     }
   }
 
-  robIDSet.insert(tmpROBIDSet.begin(), tmpROBIDSet.end());
+  // Insert the ROBIDs decoded in this process to the already decoded ROBIDs.
+  m_robIDSet[slot].insert(tmpROBIDSet.begin(), tmpROBIDSet.end());
 
   if (sc == StatusCode::FAILURE) {
     ATH_MSG_ERROR("There was a problem with SCT ByteStream conversion");
@@ -94,28 +105,4 @@ StatusCode SCTRawDataProviderTool::convert(std::vector<const ROBFragment*>& vecR
   }
 
   return sc;
-}
-
-std::set<uint32_t>& SCTRawDataProviderTool::getRobIDSet(const EventContext& ctx) const {
-  std::lock_guard<std::recursive_mutex> lock{m_mutex};
-  EventContext::ContextID_t slot{ctx.slot()};
-  EventContext::ContextEvt_t evt{ctx.evt()};
-
-  if (slot<m_cache.size() and m_cache[slot]==evt) {
-    // Cache is valid
-    return m_robIDSet[slot];
-  }
-
-  // Expand cache if necessary
-  static const EventContext::ContextEvt_t invalidValue{EventContext::INVALID_CONTEXT_EVT};  
-  if (slot>=m_cache.size()) {
-    m_cache.resize(slot+1, invalidValue);
-    m_robIDSet.resize(slot+1);
-  }
-
-  // Set event number and clear cache for the new event
-  m_cache[slot] = evt;
-  m_robIDSet[slot].clear();
-
-  return m_robIDSet[slot];
 }
