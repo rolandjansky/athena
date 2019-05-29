47cd86ae286cf98b96421a86371fd24a07cf1a57
John Kenneth Anders <john.kenneth.anders@cern.ch>
2019-05-29 14:38:18 +0000

    Merge branch 'upstream/21.0' into '21.0'
    CaloTopoTower implementation
    
    See merge request atlas/athena!22602
    
    (cherry picked from commit 4ae55218eb006a136fce962cad3817d75f05213b)
    
    a4744a11 initial commit
    a875a0a3 development step
    927b3350 development
    c0aebd43 development
    694800cf development
    bef5ae45 development
    9d01cc8f development
    c4c11312 development
    84645d5b bug fixes
    8e0e8863 milestone topo-towers
    24284741 prepare for merger
    64c2766c fixed moment list
    e2445f9c output list fixes
    e2d35803 python syntax fix

--- Calorimeter/CaloRec/CaloRec/CaloTopoClusterFromTowerCalibrator.h
+++ Calorimeter/CaloRec/CaloRec/CaloTopoClusterFromTowerCalibrator.h
@@ -3,6 +3,8 @@
 #ifndef CALOREC_CALOTOPOCLUSTERFROMTOWERCALIBRATOR_H
 #define CALOREC_CALOTOPOCLUSTERFROMTOWERCALIBRATOR_H
 
+#include "StoreGate/ReadHandleKey.h"
+
 #include "AthenaBaseComps/AthAlgTool.h" 
 
 #include "CaloRec/CaloClusterCollectionProcessor.h"
@@ -18,6 +20,9 @@ public:
   /// @brief Tool constructor
   CaloTopoClusterFromTowerCalibrator(const std::string& type,const std::string& name,const IInterface* pParent);
 
+  /// @brief Tool initialization
+  StatusCode initialize();
+ 
   /// @brief Tool execution
   StatusCode execute(xAOD::CaloClusterContainer* pClusCont);
 
@@ -25,16 +30,20 @@ private:
 
   /// @name Tool properties
   /// @{
-  std::string m_cellClusterWeightsKey; ///> @brief Key for cell-cluster weight look up object
-  bool        m_orderByPt;             ///> @brief Turn on pT ordering
+  SG::ReadHandleKey<CaloCellClusterWeights> m_cellClusterWeightKey; ///< Key for handle to cell-cluster weight look up object 
+  bool                                      m_orderByPt;            ///< Turn on pT ordering if @c true
   /// @}
-
-  /// @brief Default container key
-  static std::string m_defaultKey;
 };
 /// @class CaloTopoClusterFromTowerCalibrator
 ///
-/// @brief A cluster builder tool calibrated topo-clusters formed from calorimeter towers to the LCW scale. 
+/// @brief A cluster builder tool to calibrate topo-clusters formed from (EM) calorimeter towers to the LCW scale. 
 ///
+/// This module applies LCW weights to cells contributing to towers represented by @c xAOD::CaloCluster objects. 
+/// The overall energy contribution of a given cell contributing to a given tower is then @f$ w_{\rm geo} \times w_{\rm LCW} \times E_{\rm cell} @f$,
+/// where @f$ w_{\rm geo} @f$ is the geometrical weight, @f$ w_{\rm LCW} @f$ is
+/// the calibration weight the cell received from the LCW calibration in the context of the @c xAOD::CaloCluster objects it contributes to (at most two),
+/// and @f$ E_{\rm cell} @f$ is the cell energy on EM scale. More details on the weights are given on 
+/// <a href="https://twiki.cern.ch/twiki/bin/view/AtlasSandboxProtected/CaloTowerPerformance" title="https://twiki.cern.ch/twiki/bin/view/AtlasSandboxProtected/CaloTowerPerformance">this page</a>.
+///  
 /// @author Peter Loch <loch@physics.arizona.edu>      
 #endif
