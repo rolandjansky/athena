73c551f7f5edf6e7d47ffe8fe04726a8171ff993
Julien Maurer <julien.maurer@cern.ch>
2019-06-03 20:12:28 +0300

    proper condition check for message verbosity

--- PhysicsAnalysis/AnalysisCommon/FakeBkgTools/Root/LhoodMM_tools.cxx
+++ PhysicsAnalysis/AnalysisCommon/FakeBkgTools/Root/LhoodMM_tools.cxx
@@ -478,7 +478,7 @@ double LhoodMM_tools::logPoisson(double obs, double pred) {
 }
 
 #ifdef FAKEBKGTOOLS_ATLAS_ENVIRONMENT
-    #define ASG_MSG_VERBOSE(x) do { if ( m_current_lhoodMM_tool->msg(MSG::VERBOSE)) m_current_lhoodMM_tool->msg(MSG::VERBOSE) <<  x << endmsg; } while(0)
+    #define ASG_MSG_VERBOSE(x) do { if ( m_current_lhoodMM_tool->msgLvl(MSG::VERBOSE)) m_current_lhoodMM_tool->msg(MSG::VERBOSE) <<  x << endmsg; } while(0)
 #else
     #define ASG_MSG_VERBOSE(x) do (if(m_current_lhoodMM_tool->msgLvl(MSG::VERBOSE)) std::cout << x << std::endl; } while (0)      
 #endif
