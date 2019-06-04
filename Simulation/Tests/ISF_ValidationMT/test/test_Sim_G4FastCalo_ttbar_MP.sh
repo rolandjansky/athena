ebff8794247821f39cef50a930e925ebe696e8a8
John Chapman <jchapman@cern.ch>
2019-06-04 14:12:21 +0200

    Add new package for running ISF jobs in AthenaMP and AthenaMT
    The first example is for running `G4FastCalo` in AthenaMT. This should
    resolve the issue in ATLASSIM-4127.

--- Simulation/Tests/ISF_ValidationMT/test/test_Sim_G4FastCalo_ttbar_MP.sh
+++ Simulation/Tests/ISF_ValidationMT/test/test_Sim_G4FastCalo_ttbar_MP.sh
@@ -6,32 +6,39 @@
 # art-include: 21.9/Athena
 # art-include: master/Athena
 # art-type: grid
+# art-athena-mt: 4
 # art-output: test.HITS.pool.root
-# art-output: truth.root
 
 Sim_tf.py \
 --conditionsTag 'default:OFLCOND-RUN12-SDR-19' \
 --physicsList 'FTFP_BERT' \
 --truthStrategy 'MC15aPlus' \
 --simulator 'G4FastCalo' \
---postInclude 'default:PyJobTransforms/UseFrontier.py' 'EVNTtoHITS:G4AtlasTests/postInclude.DCubeTest.py' \
+--postInclude 'default:PyJobTransforms/UseFrontier.py' \
 --preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py' \
 --DataRunNumber '222525' \
 --geometryVersion 'default:ATLAS-R2-2015-03-01-00' \
 --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ISF_Validation/mc12_valid.110401.PowhegPythia_P2012_ttbar_nonallhad.evgen.EVNT.e3099.01517252._000001.pool.root.1" \
---outputHITSFile "test.HITS.pool.root" \
+--outputHITSFile "unordered.HITS.pool.root" \
 --maxEvents 250 \
 --imf False
 
 rc=$?
-rc2=-9999
 echo  "art-result: $rc simulation"
+rc2=-9999
 if [ $rc -eq 0 ]
+then
+    tree-orderer.py -i unordered.HITS.pool.root -o test.HITS.pool.root
+    rc2=$?
+    rm unordered.HITS.pool.root
+fi
+echo  "art-result: $rc2 reordering"
+rc3=-9999
+if [ $rc2 -eq 0 ]
 then
     ArtPackage=$1
     ArtJobName=$2
     art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
-    rc2=$?
+    rc3=$?
 fi
-
-echo  "art-result: $rc2 regression"
+echo  "art-result: $rc3 regression"
