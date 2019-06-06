0067d1b040477cda38e3c90f33e8ecf0c1afa7e5
Stewart Martin-Haugh <smh@cern.ch>
2019-06-06 12:24:30 +0200

    Set EvtMax and SkipEvents to athenaCommonFlags value, even if not set at the command line

--- Control/AthenaCommon/share/runbatch.py
+++ Control/AthenaCommon/share/runbatch.py
@@ -8,12 +8,9 @@
 #   athena.py <myJobOptions.py> runbatch.py
 
 try:
-   #first check if command-line evtMax or skipEvents options were provided
-   if opts.evtMax != None:
-      theApp.EvtMax = jps.AthenaCommonFlags.EvtMax()
-   if opts.skipEvents != None:
-      if hasattr(svcMgr,"EventSelector"):
-         svcMgr.EventSelector.SkipEvents = jps.AthenaCommonFlags.SkipEvents()
+   theApp.EvtMax = jps.AthenaCommonFlags.EvtMax()
+   if hasattr(svcMgr,"EventSelector"):
+      svcMgr.EventSelector.SkipEvents = jps.AthenaCommonFlags.SkipEvents()
 
    theApp.run()     # runs until theApp.EvtMax events reached
    from AthenaCommon.Debugging import hookDebugger,DbgStage
