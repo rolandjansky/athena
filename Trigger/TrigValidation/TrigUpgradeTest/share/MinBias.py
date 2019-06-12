80242ff66e6a0834e17f35dfd65a9f47e0274308
Tomasz Bold <tomasz.bold@gmail.com>
2019-06-12 18:33:32 +0200

    added min bias bootstrap test

---
+++ Trigger/TrigValidation/TrigUpgradeTest/share/MinBias.py
@@ -0,0 +1,31 @@
+#
+#  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+#
+
+include("TrigUpgradeTest/testHLT_MT.py")
+
+from TrigUpgradeTest.InDetSetup import inDetSetup
+inDetSetup()
+
+from TriggerMenuMT.HLTMenuConfig.CommonSequences.InDetSetup import makeInDetAlgs
+eventAlgs,viewAlgs = makeInDetAlgs(whichSignature='FS', separateTrackParticleCreator='MinBias') 
+
+
+from AthenaCommon.AlgSequence import AlgSequence
+topSequence  = AlgSequence()
+topSequence += eventAlgs
+
+topSequence.InDetSCTRawDataProvider_FS.RoIs   = "FSRoI"
+topSequence.InDetPixelRawDataProvider_FS.RoIs = "FSRoI"
+topSequence.InDetTRTRawDataProvider_FS.RoIs   = "FSRoI"
+topSequence.InDetSCT_Clusterization_FS.RoIs   = "FSRoI"
+topSequence.InDetPixelClusterization_FS.RoIs  = "FSRoI"
+
+print topSequence.InDetTrigTrackParticleCreatorAlgMinBias
+topSequence.InDetTrigTrackParticleCreatorAlgMinBias.roiCollectionName = "FSRoI"
+
+from AthenaCommon.Constants import *
+#topSequence.InDetSiTrackerSpacePointFinder.OutputLevel=VERBOSE
+
+
+
