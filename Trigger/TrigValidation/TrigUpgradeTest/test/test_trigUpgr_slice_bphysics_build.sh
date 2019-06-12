995f42ecbf0ac1060da29ecccee633964ea97c09
Heather Russell <heather.russell@cern.ch>
2019-06-12 13:23:58 +0200

    Update full menu reference and change bphys slice to run from menu not cf menu

--- Trigger/TrigValidation/TrigUpgradeTest/test/test_trigUpgr_slice_bphysics_build.sh
+++ Trigger/TrigValidation/TrigUpgradeTest/test/test_trigUpgr_slice_bphysics_build.sh
@@ -11,7 +11,7 @@ export SKIPEVENTS=50
 export EVENTS=10
 export THREADS=1
 export SLOTS=1
-export JOBOPTION="TrigUpgradeTest/full_menu_cf.py"
+export JOBOPTION="TrigUpgradeTest/full_menu.py"
 export REGTESTEXP="TriggerSummaryStep.*HLT_.*|TriggerMonitorFinal.*HLT_.*|TrigSignatureMoniMT.*HLT_.*"
 export EXTRA="doEmptyMenu=True;doBphysicsSlice=True"
 
