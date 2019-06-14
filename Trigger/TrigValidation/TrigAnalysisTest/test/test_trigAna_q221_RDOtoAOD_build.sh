6ff9a36a3f2fd59e789cf08e3aede6759e81b577
Rafal Bielski <rafal.bielski@cern.ch>
2019-06-14 07:19:33 +0000

    Add q221-based ART tests for RDO->RDO_TRIG->ESD->AOD transform
    Also merge logs in RDO->RDO_TRIG test for post-processing

--- Trigger/TrigValidation/TrigAnalysisTest/test/test_trigAna_q221_RDOtoAOD_build.sh
+++ Trigger/TrigValidation/TrigAnalysisTest/test/test_trigAna_q221_RDOtoAOD_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# art-description: Test of the RDOtoRDOTrigger transform with threads=1
+# art-description: Test of transform RDO->RDO_TRIG->ESD->AOD with serial athena (legacy trigger)
 # art-type: build
 # art-include: master/Athena
 # art-output: log.*
@@ -10,23 +10,22 @@
 # art-output: runargs.*
 # art-output: runwrapper.*
 
-export NAME="trigAna_q221_RDOtoRDOTrig_mt1_build"
+export NAME="trigAna_q221_RDOtoAOD_build"
 export TEST="TrigAnalysisTest"
 export INPUT="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TriggerTest/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.RDO.e4993_s3214_r11315/RDO.17533168._000001.pool.root.1"
 export EVENTS=20
-export THREADS=1
 export JOB_LOG="athena.log"
-export AODTOCHECK="RDO_TRIG.pool.root" # to run checkFile and checkxAOD on this
+export AODTOCHECK="AOD.pool.root" # to run checkFile and checkxAOD on this
 
-echo "Running RDO->RDO_TRIG with Reco_tf command:"
+echo "Running RDO->RDO_TRIG->ESD->AOD with Reco_tf command:"
 (set -x
 Reco_tf.py \
 --AMI=q221 \
 --imf=True \
---athenaopts="--threads=${THREADS}" \
 --maxEvents=${EVENTS} \
 --inputRDOFile=${INPUT} \
---outputRDO_TRIGFile=RDO_TRIG.pool.root \
+--outputAODFile=AOD.pool.root \
+--steering="doRDO_TRIG" \
 >${JOB_LOG} 2>&1
 ) 2>&1
 
@@ -36,10 +35,10 @@ echo "art-result: ${ATH_RETURN} ${JOB_LOG%.*}"
 # merge transform logs for post-processing
 echo "### ${JOB_LOG} ###" > athena.merged.log
 cat ${JOB_LOG} >> athena.merged.log
-trfNames="RDOtoRDOTrigger"
+trfNames="RDOtoRDOTrigger RAWtoESD ESDtoAOD"
 for trf in ${trfNames}; do
   if [ -f log.${trf} ]; then
-    echo "### log.${trf} ###"
+    echo "### log.${trf} ###" >> athena.merged.log
     cat log.${trf} >> athena.merged.log
   else
     echo "### WARNING: log.${trf} MISSING ###" >> athena.merged.log
