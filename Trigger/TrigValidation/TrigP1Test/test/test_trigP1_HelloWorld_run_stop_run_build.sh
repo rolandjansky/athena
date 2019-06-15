c618edc9b91dd3351c4115c7af1caf2a5767192e
Rafal Bielski <rafal.bielski@cern.ch>
2019-06-15 18:56:47 +0000

    TrigP1Test: rename all tests and add EmptyMenu tests

--- Trigger/TrigValidation/TrigP1Test/test/test_trigP1_HelloWorld_run_stop_run_build.sh
+++ Trigger/TrigValidation/TrigP1Test/test/test_trigP1_HelloWorld_run_stop_run_build.sh
@@ -4,7 +4,7 @@
 # art-type: build
 # art-include: master/Athena
 
-export NAME=HelloWorldHLT_run_stop_run
+export NAME=HelloWorld_run_stop_run
 # use export ART_FILE_NAME=... to set the INPUT_FILE value
 export TRANSITIONS="run-stop-run-saveHist.trans"
 export ART_CMD="get_files -data ${TRANSITIONS}; athenaHLT.py -i -M -f INPUT_FILE AthExHelloWorld/HelloWorldOptions.py < ${TRANSITIONS}"
