b2a50061164a4d0edc7b3e70f103233f03b3ebd5
Peter van Gemmeren <gemmeren@anl.gov>
2019-06-17 14:47:58 -0500

    Enable file peeking to obtain ROOT storage settings and use them for AthenaMP dispatching. This is work done by ANL SULI Jason Saunders (saundersj@anl.gov).

--- Control/AthenaCommon/share/Preparation.py
+++ Control/AthenaCommon/share/Preparation.py
@@ -221,5 +221,6 @@ if not opts.minimal:
 
       import AthenaCommon.AtlasThreadedJob
 
-
-
+   if opts.chunk_size:
+      from AthenaMP.AthenaMPFlags import jobproperties as mpjps
+      mpjps.AthenaMPFlags.ChunkSize = opts.chunk_size
