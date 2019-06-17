20a1597d377e6fee1c1fd809d67c3fffdb152164
Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>
2019-06-17 14:19:56 +0200

    resolve discussion

--- Reconstruction/egamma/egammaAlgs/python/EMGSFCaloExtensionBuilderConfig.py
+++ Reconstruction/egamma/egammaAlgs/python/EMGSFCaloExtensionBuilderConfig.py
@@ -4,7 +4,6 @@ __doc__ = "Instantiate the EMGSFCaloExtensionBuilder with default configuration"
 
 from AthenaCommon.Logging import logging
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
-from TrkExTools.AtlasExtrapolatorConfig import AtlasExtrapolatorCfg
 from TrackToCalo.TrackToCaloConfig import ParticleCaloExtensionToolCfg
 from egammaAlgs.egammaAlgsConf import EMGSFCaloExtensionBuilder
 
@@ -15,6 +14,21 @@ def EMGSFCaloExtensionBuilderCfg(flags, name='EMGSFCaloExtensionBuilder', **kwar
     mlog.info('Starting configuration')
 
     acc = ComponentAccumulator()
+
+    if "PerigeeCaloExtensionTool" not in kwargs:
+        perigeeCaloExtrapAcc = ParticleCaloExtensionToolCfg(flags,
+                                                            name="PerigeeCaloExtensionTool",
+                                                            ParticleType="electron",
+                                                            StartFromPerigee=True)
+        kwargs["PerigeeCaloExtensionTool"] = perigeeCaloExtrapAcc.popPrivateTools()
+        acc.merge(perigeeCaloExtrapAcc)
+
+    if "LastCaloExtensionTool" not in kwargs:
+        lastCaloExtrapAcc = ParticleCaloExtensionToolCfg(flags,
+                                                         name="LastCaloExtensionTool",
+                                                         ParticleType="electron")
+        kwargs["LastCaloExtensionTool"] = lastCaloExtrapAcc.popPrivateTools()
+        acc.merge(lastCaloExtrapAcc)
 
     kwargs.setdefault("GFFTrkPartContainerName", flags.Egamma.Keys.Output.GSFTrackParticles)
 
