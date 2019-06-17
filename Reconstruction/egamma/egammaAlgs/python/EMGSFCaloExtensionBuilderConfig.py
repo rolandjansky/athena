ae3bcf326f08886f58e3ac181f373597f7a2239b
Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>
2019-06-17 13:12:26 +0200

    Update EMGSFCaloExtensionBuilderConfig to resolve discussions

--- Reconstruction/egamma/egammaAlgs/python/EMGSFCaloExtensionBuilderConfig.py
+++ Reconstruction/egamma/egammaAlgs/python/EMGSFCaloExtensionBuilderConfig.py
@@ -16,30 +16,6 @@ def EMGSFCaloExtensionBuilderCfg(flags, name='EMGSFCaloExtensionBuilder', **kwar
 
     acc = ComponentAccumulator()
 
-    if "Extrapolator" not in kwargs:
-        extrapAcc = AtlasExtrapolatorCfg(flags)
-        kwargs["Extrapolator"] = extrapAcc.popPrivateTools()
-        acc.merge(extrapAcc)
-
-    if "PerigeeCaloExtensionTool" not in kwargs:
-        perigeeCaloExtrapAcc = ParticleCaloExtensionToolCfg(flags,
-                                                            name="PerigeeCaloExtensionTool",
-                                                            Extrapolator=kwargs["Extrapolator"],
-                                                            ParticleType="electron",
-                                                            StartFromPerigee=True)
-        kwargs["PerigeeCaloExtensionTool"] = perigeeCaloExtrapAcc.popPrivateTools()
-        acc.merge(perigeeCaloExtrapAcc)
-
-    if "LastCaloExtensionTool" not in kwargs:
-        lastCaloExtrapAcc = ParticleCaloExtensionToolCfg(flags,
-                                                         name="LastCaloExtensionTool",
-                                                         ParticleType="electron",
-                                                         Extrapolator=kwargs["Extrapolator"])
-        kwargs["LastCaloExtensionTool"] = lastCaloExtrapAcc.popPrivateTools()
-        acc.merge(lastCaloExtrapAcc)
-
-    kwargs.setdefault("GSFPerigeeCache", 'GSFPerigeeCaloExtension')
-    kwargs.setdefault("GSFLastCache",    'GSFLastCaloExtension')
     kwargs.setdefault("GFFTrkPartContainerName", flags.Egamma.Keys.Output.GSFTrackParticles)
 
     emgscaloextfAlg = EMGSFCaloExtensionBuilder(name, **kwargs)
