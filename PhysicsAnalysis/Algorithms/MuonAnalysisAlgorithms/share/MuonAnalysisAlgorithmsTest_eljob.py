11f364e13262df9c72e55f3e79a45d0e585d6b61
Nils Krumnack <krumnack@iastate.edu>
2019-06-22 11:56:13 -0400

    merge algorithm test code between EventLoop and Athena
    So far the algorithm test sequence was duplicated between EventLoop
    and Athena.  Keeping them in sync was needlessly error-prone, so I now
    merged them.

--- PhysicsAnalysis/Algorithms/MuonAnalysisAlgorithms/share/MuonAnalysisAlgorithmsTest_eljob.py
+++ PhysicsAnalysis/Algorithms/MuonAnalysisAlgorithms/share/MuonAnalysisAlgorithmsTest_eljob.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 #
 # @author Nils Krumnack
 
@@ -56,81 +56,13 @@ job = ROOT.EL.Job()
 job.sampleHandler( sh )
 job.options().setDouble( ROOT.EL.Job.optMaxEvents, 500 )
 
-# Set up the systematics loader/handler algorithm:
-from AnaAlgorithm.AnaAlgorithmConfig import AnaAlgorithmConfig
-sysLoader = AnaAlgorithmConfig( 'CP::SysListLoaderAlg/SysLoaderAlg' )
-sysLoader.sigmaRecommended = 1
-job.algsAdd( sysLoader )
 
-# Include, and then set up the pileup analysis sequence:
-from AsgAnalysisAlgorithms.PileupAnalysisSequence import \
-    makePileupAnalysisSequence
-pileupSequence = makePileupAnalysisSequence( dataType )
-pileupSequence.configure( inputName = 'EventInfo', outputName = 'EventInfo' )
-print( pileupSequence ) # For debugging
-
-# Add the pileup algorithm(s) to the job:
-for alg in pileupSequence:
+from MuonAnalysisAlgorithms.MuonAnalysisAlgorithmsTest import makeSequence
+algSeq = makeSequence (dataType)
+print algSeq # For debugging
+for alg in algSeq:
     job.algsAdd( alg )
     pass
-
-# Include, and then set up the muon analysis algorithm sequence:
-from MuonAnalysisAlgorithms.MuonAnalysisSequence import makeMuonAnalysisSequence
-muonSequenceMedium = makeMuonAnalysisSequence( dataType, deepCopyOutput = True, shallowViewOutput = False,
-                                               workingPoint = 'Medium.Iso', postfix = 'medium' )
-muonSequenceMedium.configure( inputName = 'Muons',
-                              outputName = 'AnalysisMuonsMedium_%SYS%' )
-print( muonSequenceMedium ) # For debugging
-
-# Add all algorithms to the job:
-for alg in muonSequenceMedium:
-    job.algsAdd( alg )
-    pass
-
-muonSequenceTight = makeMuonAnalysisSequence( dataType, deepCopyOutput = True, shallowViewOutput = False,
-                                               workingPoint = 'Tight.Iso', postfix = 'tight' )
-muonSequenceTight.removeStage ("calibration")
-muonSequenceTight.configure( inputName = 'AnalysisMuonsMedium_%SYS%',
-                             outputName = 'AnalysisMuons_%SYS%',
-                             affectingSystematics = muonSequenceMedium.affectingSystematics())
-print( muonSequenceTight ) # For debugging
-
-# Add all algorithms to the job:
-for alg in muonSequenceTight:
-    job.algsAdd( alg )
-    pass
-
-# Add ntuple dumper algorithms:
-from AnaAlgorithm.DualUseConfig import createAlgorithm
-treeMaker = createAlgorithm( 'CP::TreeMakerAlg', 'TreeMaker' )
-treeMaker.TreeName = 'muons'
-job.algsAdd( treeMaker )
-ntupleMaker = createAlgorithm( 'CP::AsgxAODNTupleMakerAlg', 'NTupleMakerEventInfo' )
-ntupleMaker.TreeName = 'muons'
-ntupleMaker.Branches = [ 'EventInfo.runNumber     -> runNumber',
-                         'EventInfo.eventNumber   -> eventNumber', ]
-ntupleMaker.systematicsRegex = '(^$)'
-job.algsAdd( ntupleMaker )
-ntupleMaker = createAlgorithm( 'CP::AsgxAODNTupleMakerAlg', 'NTupleMakerMuons' )
-ntupleMaker.TreeName = 'muons'
-ntupleMaker.Branches = [ 'AnalysisMuons_NOSYS.eta -> mu_eta',
-                         'AnalysisMuons_NOSYS.phi -> mu_phi',
-                         'AnalysisMuons_%SYS%.pt  -> mu_%SYS%_pt', ]
-ntupleMaker.systematicsRegex = '(^MUON_.*)'
-job.algsAdd( ntupleMaker )
-treeFiller = createAlgorithm( 'CP::TreeFillerAlg', 'TreeFiller' )
-treeFiller.TreeName = 'muons'
-job.algsAdd( treeFiller )
-
-# Set up a mini-xAOD writer algorithm:
-xaodWriter = AnaAlgorithmConfig( 'CP::xAODWriterAlg/xAODWriter' )
-xaodWriter.ItemList = \
-   [ 'xAOD::EventInfo#EventInfo',
-     'xAOD::EventAuxInfo#EventInfoAux.-',
-     'xAOD::MuonContainer#AnalysisMuons_NOSYS',
-     'xAOD::AuxContainerBase#AnalysisMuons_NOSYSAux.eta.phi.pt' ]
-xaodWriter.systematicsRegex = '.*'
-job.algsAdd( xaodWriter )
 
 # Make sure that both the ntuple and the xAOD dumper have a stream to write to.
 job.outputAdd( ROOT.EL.OutputStream( 'ANALYSIS' ) )
