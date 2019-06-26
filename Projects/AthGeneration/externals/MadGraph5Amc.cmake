9d0f64c2bdc0b53162ea3f48901a1d7dc0ed2600
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2019-06-26 15:46:00 +0200

    update to atlasexternals-01-00-51 update to Lhapdf6.2.3 and versions of generators recompiled with this version of Lhapdf

--- Projects/AthGeneration/externals/MadGraph5Amc.cmake
+++ Projects/AthGeneration/externals/MadGraph5Amc.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of MadGraph to use.
 #
 
-set( MADGRAPH5AMC_VERSION 2.6.5p2.atlas3)
+set( MADGRAPH5AMC_VERSION 2.6.5p3.atlas3)
 set( MADGRAPH5AMC_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/madgraph5amc/${MADGRAPH5AMC_VERSION}/${LCG_PLATFORM} )
--- Projects/AthGeneration/externals/Sherpa.cmake
+++ Projects/AthGeneration/externals/Sherpa.cmake
@@ -1,6 +1,6 @@
 #
 # File specifying the location of Sherpa to use.
 #
-set( SHERPA_VERSION 2.2.7p1 )
+set( SHERPA_VERSION 2.2.7p3 )
 set( SHERPA_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/sherpa/${SHERPA_VERSION}/${LCG_PLATFORM} )
