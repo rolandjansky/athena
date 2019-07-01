2dd0ce35d8464b5419bb6d634ea6fcfe6b4c0f53
MihaMuskinja <miha.muskinja@gmail.com>
2019-07-01 20:55:34 +0200

    Implementation of basic vector D mesons

--- Simulation/G4Atlas/G4AtlasApps/python/SimFlags.py
+++ Simulation/G4Atlas/G4AtlasApps/python/SimFlags.py
@@ -778,6 +778,12 @@ class TruthService(JobProperty):
     allowedTypes = ['str']
     StoredValue  = 'ISF_TruthService'
 
+class ParticleSimWhiteList(JobProperty):
+    """Steering of ISF: set the ParticleSimWhiteList tool"""
+    statusOn     = False
+    allowedTypes = ['str']
+    StoredValue  = 'ISF_ParticleSimWhiteList'
+
 
 ## Definition and registration of the simulation flag container
 class SimFlags(JobPropertyContainer):
