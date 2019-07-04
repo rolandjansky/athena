1e5e46d6d59a863d0e299f81458d47015f700cbc
Takuya Nobe <tnobe@lxplus714.cern.ch>
2019-07-04 15:36:05 +0200

    update comments

--- Reconstruction/Jet/BoostedJetTaggers/BoostedJetTaggers/IJetSelectorLabelTool.h
+++ Reconstruction/Jet/BoostedJetTaggers/BoostedJetTaggers/IJetSelectorLabelTool.h
@@ -2,12 +2,22 @@
 
 /*
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
-  
-  This is an interface class for JSS taggers, for those who want to decorate
-  a truth label on their jets for the JES uncertainties depending on the label,
-  but do not actually tag their jets.
 */
 
+/// Interfaces for JSS taggers tool (e.g. SmoothedWZTagger, JSSWTopTaggerDNN)
+/// 
+/// If you call:
+///    tag(const xAOD::Jet& jet) 
+/// function, both a truth label and tagging result are decorated to the 
+/// given jet. If it is not a desired  use case, e.g. a truth label is only 
+/// needed for the JES uncertainties depending on the label, the separate 
+/// interface:
+///    decorateTruthLabel(const xAOD::Jet& jet)
+/// can be used just to attach the truth label to the jet, which doesn't 
+/// allow tagging it.
+/// 
+/// @author Takuya Nobe <Takuya.Nobe@cern.ch>
+/// 
 #ifndef IJETSELECTORLABELTOOL_H
 #define IJETSELECTORLABELTOOL_H
 
@@ -23,8 +33,16 @@ class IJetSelectorLabelTool : public virtual IJetSelectorTool {
 
   virtual ~IJetSelectorLabelTool() {}
   
+  /**
+   * @fn
+   * attach a truth label and the tagging result to the given jet
+   */
   virtual Root::TAccept tag(const xAOD::Jet& jet) const = 0;
   
+  /**
+   * @fn
+   * attach a truth label only
+   */
   virtual StatusCode decorateTruthLabel(const xAOD::Jet& jet) const = 0;
 
 };
