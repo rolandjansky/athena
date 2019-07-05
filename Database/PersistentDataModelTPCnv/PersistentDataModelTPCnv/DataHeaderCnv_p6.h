007a4f38c5727eb413f252b1ad7c242af88bf32c
Marcin Nowak <Marcin.Nowak@cern.ch>
2019-07-05 13:43:31 +0200

    Added ability to read DataHeader_p6 for compability with master

--- Database/PersistentDataModelTPCnv/PersistentDataModelTPCnv/DataHeaderCnv_p6.h
+++ Database/PersistentDataModelTPCnv/PersistentDataModelTPCnv/DataHeaderCnv_p6.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef PERSISTENTDATAMODELTPCNV_DATAHEADERCNV_P6_H
@@ -7,58 +7,33 @@
 
 /**
  *  @file DataHeaderCnv_p6.h
- *  @brief This file contains the class definition for the DataHeaderCnv_p6 and DataHeaderElementCnv_p6 classes.
+ *  @brief This file contains the class definition for DataHeaderCnv_p6 
  *  @author Peter van Gemmeren <gemmeren@anl.gov>
- *  $Id: DataHeaderCnv_p6.h,v 1.1 2009-04-21 21:48:34 gemmeren Exp $
  **/
 
-#include "PersistentDataModel/DataHeader.h"
-#include "PersistentDataModelTPCnv/DataHeader_p6.h"
-
-#include <map>
-
-/** @class DataHeaderElementCnv_p6
- *  @brief This class provides the converter to customize the saving of DataHeaderElement_p6.
- **/
-class DataHeaderElementCnv_p6 {
-public:
-   DataHeaderElementCnv_p6();
-   virtual ~DataHeaderElementCnv_p6();
-
-   void persToTrans(const DataHeaderElement_p6* pers, DataHeaderElement* trans, const DataHeaderForm_p6& form);
-   void transToPers(const DataHeaderElement* trans, DataHeaderElement_p6* pers, DataHeaderForm_p6& form);
-
-friend class DataHeaderCnv_p6;
-};
+class DataHeader;
+class DataHeader_p6;
+class DataHeaderForm_p6;
 
 /** @class DataHeaderCnv_p6
  *  @brief This class provides the converter to customize the saving of DataHeader_p6.
  **/
 class DataHeaderCnv_p6 {
 public:
-   DataHeaderCnv_p6();
-   virtual ~DataHeaderCnv_p6();
+   DataHeaderCnv_p6() {}
 
-   DataHeader* createTransient(const DataHeader_p6* persObj);
-   void persToTrans(const DataHeader_p6* pers, DataHeader* trans);
-   DataHeader_p6* createPersistent(const DataHeader* transObj);
-   void transToPers(const DataHeader* trans, DataHeader_p6* pers);
+   DataHeader* createTransient(const DataHeader_p6* persObj, const DataHeaderForm_p6& form);
+   DataHeader_p6* createPersistent(const DataHeader* transObj, DataHeaderForm_p6& form);
 
-   void insertDHRef(DataHeader_p6* pers, const std::string& key, const std::string& strToken);
-
-private:
-   DataHeaderElementCnv_p6 m_elemCnv;
+   /// convert single DH element to persistent represenation
+   void elemToPers(const DataHeaderElement* trans, DataHeader_p6* pers, DataHeaderForm_p6& form);
+   /// restore single DH element from persistent represenation
+   void persToElem(const DataHeader_p6* pers, unsigned p_idx, DataHeaderElement* trans,
+                   const DataHeaderForm_p6& form );
+  
+   void insertDHRef( DataHeader_p6* pers,
+                     const std::string& key, const std::string& strToken,
+                     DataHeaderForm_p6& form );
 };
-
-inline DataHeader* DataHeaderCnv_p6::createTransient(const DataHeader_p6* persObj) {
-   DataHeader* trans = new DataHeader();
-   persToTrans(persObj, trans);
-   return(trans);
-}
-inline DataHeader_p6* DataHeaderCnv_p6::createPersistent(const DataHeader* transObj){
-   DataHeader_p6* pers = new DataHeader_p6();
-   transToPers(transObj, pers);
-   return(pers);
-}
 
 #endif
--- Database/PersistentDataModelTPCnv/PersistentDataModelTPCnv/PersistentDataModelTPCnvDict.h
+++ Database/PersistentDataModelTPCnv/PersistentDataModelTPCnv/PersistentDataModelTPCnvDict.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef PERSISTENTDATAMODEL_PERSISTENTDATAMODELTPCNVDICT_H
@@ -11,15 +11,5 @@
 #include "PersistentDataModelTPCnv/DataHeader_p6.h"
 #include "PersistentDataModelTPCnv/DataHeaderCnv_p5.h"
 #include "PersistentDataModelTPCnv/DataHeaderCnv_p6.h"
-
-// Need to instantiate iterators
-namespace PersistentDataModelTPCnvDict {
-   // template instantiations
-   std::pair<Guid, unsigned int> dummy_dbRecord;
-   std::vector<std::pair<Guid, unsigned int> > dummy_dbRecords;
-   std::pair<std::string, unsigned int> dummy_sgRecord;
-   std::pair<Guid, std::pair<std::string, unsigned int> > dummy_objRecord;
-   std::vector<std::pair<Guid, std::pair<std::string, unsigned int> > >dummy_objRecords;
-}
 
 #endif
