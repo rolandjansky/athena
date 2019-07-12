2b3029a2431a3f6af0c1b0e140012daf259561bd
Pascal Boeschoten <pascal.boeschoten@cern.ch>
2019-07-12 12:17:36 +0200

    Clean up in destructor

--- MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonRecToolInterfaces/MuonRecToolInterfaces/HoughDataPerSec.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonRecToolInterfaces/MuonRecToolInterfaces/HoughDataPerSec.h
@@ -37,7 +37,29 @@ namespace Muon {
       nmaxHitsInRegion.resize(MuonStationIndex::DetectorRegionIndexMax);
       nphimaxHitsInRegion.resize(MuonStationIndex::DetectorRegionIndexMax);
     }
-    void cleanUp();
+
+    ~HoughDataPerSec() {
+      cleanUp();
+    }
+
+    void cleanUp() {
+      for(RegionHitVec::iterator it=hitVec.begin();it!=hitVec.end();++it)
+        for( HitVec::iterator it2=it->begin();it2!=it->end();++it2 ) delete *it2;
+      hitVec.clear();
+
+      for(RegionPhiHitVec::iterator it=phiHitVec.begin();it!=phiHitVec.end();++it)
+        for( PhiHitVec::iterator it2=it->begin();it2!=it->end();++it2 ) delete *it2;
+      phiHitVec.clear();
+
+      for(RegionMaximumVec::iterator it=maxVec.begin();it!=maxVec.end();++it)
+        for( MaximumVec::iterator it2=it->begin();it2!=it->end();++it2 ) delete *it2;
+      maxVec.clear();
+
+      for(RegionPhiMaximumVec::iterator it=phiMaxVec.begin();it!=phiMaxVec.end();++it)
+        for( PhiMaximumVec::iterator it2=it->begin();it2!=it->end();++it2 ) delete *it2;
+      phiMaxVec.clear();
+    }
+
     int                   sector;
     RegionHitVec          hitVec;
     RegionPhiHitVec       phiHitVec;
