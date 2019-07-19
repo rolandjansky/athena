687750a8f1c3821853b9e745190a56f558863955
Takuya Nobe <tnobe@lxplus718.cern.ch>
2019-07-19 18:07:33 +0200

    updated comments

--- Reconstruction/Jet/BoostedJetTaggers/BoostedJetTaggers/IJetSelectorLabelTool.h
+++ Reconstruction/Jet/BoostedJetTaggers/BoostedJetTaggers/IJetSelectorLabelTool.h
@@ -4,12 +4,19 @@
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
+#ifndef IJETSELECTORLABELTOOL_H
+#define IJETSELECTORLABELTOOL_H
+
+#include "PATInterfaces/CorrectionCode.h"
+#include "JetAnalysisInterfaces/IJetSelectorTool.h"
+#include "xAODJet/JetContainer.h"
+
 /// Interfaces for JSS taggers tool (e.g. SmoothedWZTagger, JSSWTopTaggerDNN)
 /// 
 /// If you call:
 ///    tag(const xAOD::Jet& jet) 
-/// function, both a truth label and tagging result are decorated to the 
-/// given jet. If it is not a desired  use case, e.g. a truth label is only 
+/// function, a truth label is decorated to the given jet and tagging result
+/// is calculated. If it is not a desired  use case, e.g. a truth label is only 
 /// needed for the JES uncertainties depending on the label, the separate 
 /// interface:
 ///    decorateTruthLabel(const xAOD::Jet& jet)
@@ -18,13 +25,6 @@
 /// 
 /// @author Takuya Nobe <Takuya.Nobe@cern.ch>
 /// 
-#ifndef IJETSELECTORLABELTOOL_H
-#define IJETSELECTORLABELTOOL_H
-
-#include "PATInterfaces/CorrectionCode.h"
-#include "JetAnalysisInterfaces/IJetSelectorTool.h"
-#include "xAODJet/JetContainer.h"
-
 class IJetSelectorLabelTool : public virtual IJetSelectorTool {
 
   ASG_TOOL_INTERFACE( CP::IJetSelectorLabelTool )
@@ -35,7 +35,7 @@ class IJetSelectorLabelTool : public virtual IJetSelectorTool {
   
   /**
    * @fn
-   * attach a truth label and the tagging result to the given jet
+   * attach a truth label to the given jet and return the tagging result
    */
   virtual Root::TAccept tag(const xAOD::Jet& jet) const = 0;
   
