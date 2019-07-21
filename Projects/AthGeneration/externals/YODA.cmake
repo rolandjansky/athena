c495bd7987b3ff19f94b667bc56f3f696ec0e06f
Christian Gutschow <chris.g@cern.ch>
2019-07-21 09:53:49 +0100

    updating Rivet version to 2.7.2b-y177

--- Projects/AthGeneration/externals/YODA.cmake
+++ Projects/AthGeneration/externals/YODA.cmake
@@ -1,6 +1,6 @@
 #
 # File specifying the location of YODA to use.
 #
-set( YODA_LCGVERSION 1.7.5 )
+set( YODA_LCGVERSION 1.7.7 )
 set( YODA_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/yoda/${YODA_LCGVERSION}/${LCG_PLATFORM} )
