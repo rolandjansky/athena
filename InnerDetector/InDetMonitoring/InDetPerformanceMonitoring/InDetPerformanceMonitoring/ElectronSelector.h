4707b21609c3c799002337dbeebe9c76fee70c6c
Salvador Marti <salvador.marti@ific.uv.es>
2019-07-22 17:53:43 +0200

    skeleton for ElectronSelector

---
+++ InnerDetector/InDetMonitoring/InDetPerformanceMonitoring/InDetPerformanceMonitoring/ElectronSelector.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+ */
+
+#ifndef IDPERFMON_ELECTRONSELECTOR_H
+#define IDPERFMON_ELECTRONSELECTOR_H
+
+//==============================================================================
+// Include files...
+//==============================================================================
+#include "InDetPerformanceMonitoring/EventAnalysis.h"
+
+#include <map>
+#include "TH1.h"
+
+#include "xAODMuon/Muon.h"
+#include "xAODTracking/Vertex.h"
+#include "xAODTracking/VertexContainer.h"
+
+#include "GaudiKernel/ServiceHandle.h"
+#include "AsgTools/ToolHandle.h" 
+//==============================================================================
+// Forward class declarations...
+//==============================================================================
+class ElectronSelector : public EventAnalysis
+{
+ public:
+  ElectronSelector();
+  ~ElectronSelector();
+
+
+  //  void setDebug(bool debug){m_doDebug = debug;}
+  //  ToolHandle<CP::IMuonSelectionTool> m_muonSelectionTool;
+ 
+  // Override functions from EventAnalysis
+  virtual void Init();
+  // virtual bool Reco();
+
+ protected:
+  // virtual void BookHistograms();
+
+ private:
+  typedef EventAnalysis PARENT;
+
+  static unsigned int s_uNumInstances;
+
+  // message stream
+  MsgStream * m_msgStream;
+
+  // Class variables
+  const xAOD::Muon*   m_pxElectron;
+
+  // 
+  bool m_debug;
+};
+
+#endif
---
+++ InnerDetector/InDetMonitoring/InDetPerformanceMonitoring/src/ElectronSelector.cxx
@@ -0,0 +1,72 @@
+//==================================================================================
+//
+//  ElectronSelector.cxx :       Class designed to reconstruct di-electrons events
+//                        in particular Z0 -> e+ e- events.
+//==================================================================================
+
+//==================================================================================
+// Include files...
+//==================================================================================
+
+// This files header
+#include "InDetPerformanceMonitoring/ElectronSelector.h"
+// Package Headers
+#include "InDetPerformanceMonitoring/PerfMonServices.h"
+#include <sstream>
+// ATLAS headers
+#include "StoreGate/StoreGateSvc.h"
+#include "CLHEP/Random/RandFlat.h"
+
+#include "GaudiKernel/IToolSvc.h"
+
+//#include "xAODMuon/Muon.h"
+//#include "xAODMuon/MuonContainer.h"
+
+
+
+// Local debug variables. Global scope, but only accessible by this file.
+static const float CGeV              =  1.0e-3;  // Conversion factor to remove evil MeV
+                                                 // nonsense.
+
+// Static declarations
+unsigned int ElectronSelector::s_uNumInstances;
+
+//==================================================================================
+// Public Methods
+//==================================================================================
+ElectronSelector::ElectronSelector():
+  m_debug ( false )  
+{
+  ++s_uNumInstances;
+  
+  std::stringstream xTmp;  xTmp << s_uNumInstances;
+  m_xSampleName     = "ElectronSelector_" + xTmp.str();
+  
+  m_pxElectron = nullptr;
+  
+  m_msgStream =  new MsgStream(PerfMonServices::getMessagingService(), "InDetPerformanceMonitoring" );
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ElectronSelector::~ElectronSelector()
+{
+  --s_uNumInstances;
+  delete m_msgStream;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void ElectronSelector::Init()
+{
+  ISvcLocator* serviceLocator = Gaudi::svcLocator();
+  IToolSvc* toolSvc;
+  StatusCode sc = serviceLocator->service("ToolSvc", toolSvc, true);
+  
+  if ( sc.isFailure() || toolSvc == 0 ) {
+    (*m_msgStream) << MSG::ERROR << "  * ElectronSelector::Init * Unable to retrieve ToolSvc " << endreq;
+    return;
+  }
+  
+  PARENT::Init();
+}
+
+
