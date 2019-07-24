a7506cbc623e31bf05daa13fa69f5e0efbde880e
scott snyder <snyder@bnl.gov>
2019-07-24 17:21:45 +0200

    StoreGate: Remove use of obsolete CxxUtils headers.
    Remove use of headers from CxxUtils that are no longer needed
    now that we're building with c++17.

--- Control/StoreGate/test/CondHandleKeyArray_test.cxx
+++ Control/StoreGate/test/CondHandleKeyArray_test.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /**
@@ -22,7 +22,6 @@
 #include "AthContainers/ConstDataVector.h"
 #include "AthenaKernel/errorcheck.h"
 #include "AthenaKernel/ExtendedEventContext.h"
-#include "CxxUtils/unused.h"
 #include "CxxUtils/checker_macros.h"
 #include <cassert>
 #include <iostream>
