c448043a739a7279cf169d14ffe4caf6bc0b8968
haozturk <h.ozturk@cern.ch>
2019-07-26 10:38:40 +0200

    made the event loop monitoring optional

--- Control/PerformanceMonitoring/PerfMonComps/share/PerfMonMTSvc_jobOptions.py
+++ Control/PerformanceMonitoring/PerfMonComps/share/PerfMonMTSvc_jobOptions.py
@@ -10,6 +10,8 @@ from AthenaCommon.AppMgr import ServiceMgr as svcMgr
 if not hasattr(svcMgr, 'PerfMonMTSvc'):
     from PerfMonComps.MTJobOptCfg import PerfMonMTSvc
     svcMgr += PerfMonMTSvc( "PerfMonMTSvc"  )
+    # To disable event loop monitoring, make false the following property 
+    svcMgr.PerfMonMTSvc.doEventLoopMonitoring = True
     pass
 
 
