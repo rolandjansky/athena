7bb6a8de8a7a3a88f4803da8028a1c2b11c03b9b
Carlo Varni <cvarni@pc-tbed-pub-30.cern.ch>
2019-07-26 15:33:00 +0200

    Correcting type of GSC/Split Jet Aux Container in EDM

--- Trigger/TriggerCommon/TrigEDMConfig/python/TriggerEDMRun3.py
+++ Trigger/TriggerCommon/TrigEDMConfig/python/TriggerEDMRun3.py
@@ -225,10 +225,10 @@ TriggerHLTList = [
     #bjet jets
 
     ('xAOD::JetContainer#HLT_SplitJet',                           'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet'),
-    ('xAOD::JetContainer#HLT_SplitJetAux.',                       'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet'),
+    ('xAOD::JetAuxContainer#HLT_SplitJetAux.',                       'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet'),
 
     ('xAOD::JetContainer#HLT_GSCJet',                             'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet', 'inViews:BJetViews'),
-    ('xAOD::JetContainer#HLT_GSCJetAux.',                         'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet'),
+    ('xAOD::JetAuxContainer#HLT_GSCJetAux.',                         'BS ESD AODFULL AODSLIM AODVERYSLIM', 'Bjet'),
 
     # MinBias
 
