c0118d4012a40a7516c98cde48099d3ddeaebf4a
Dan Guest <dguest@cern.ch>
2019-07-29 19:49:09 +0200

    Add preliminary smart container list, throws a warning not an exception

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/PreliminarySmartContainers.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+
+PreliminarySmartContainers = [
+    "AntiKtVR30Rmax4Rmin02TrackJets",
+    "AntiKtVR30Rmax4Rmin02TrackGhostTagJets",
+]
