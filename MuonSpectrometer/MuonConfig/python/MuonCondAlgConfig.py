3a01856cb8f731daf6465e34bc0531abe2bd1e97
Constantin Heidegger <constantin.heidegger@cern.ch>
2019-07-30 20:58:24 +0200

    removing unused imports

--- MuonSpectrometer/MuonConfig/python/MuonCondAlgConfig.py
+++ MuonSpectrometer/MuonConfig/python/MuonCondAlgConfig.py
@@ -3,8 +3,8 @@
 ## Configuration Access to OFFLINE DB (COMP200)
 
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
-from MuonCondAlg.MuonCondAlgConf import MdtCondDbAlg,RpcCondDbAlg,CscCondDbAlg,TgcCondDbAlg
-from IOVDbSvc.IOVDbSvcConfig import IOVDbSvcCfg, addFolders
+from MuonCondAlg.MuonCondAlgConf import MdtCondDbAlg,RpcCondDbAlg,CscCondDbAlg ##,TgcCondDbAlg
+from IOVDbSvc.IOVDbSvcConfig import addFolders
 
 def MdtCondDbAlgCfg(flags, **kwargs):
     result  = ComponentAccumulator()
