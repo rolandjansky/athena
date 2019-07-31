0a7c26cf9a0e37e8b8f43f89d1fe202ce7ee1392
haozturk <h.ozturk@cern.ch>
2019-07-31 17:10:22 +0200

    Removed compilation warnings

--- Control/PerformanceMonitoring/PerfMonComps/src/PerfMonMTAuditor.cxx
+++ Control/PerformanceMonitoring/PerfMonComps/src/PerfMonMTAuditor.cxx
@@ -4,13 +4,14 @@
 
 // Framework includes
 #include "GaudiKernel/INamedInterface.h"
+#include "GaudiKernel/MsgStream.h"
 
 // PerfMonKernel includes
 #include "PerfMonKernel/IPerfMonMTSvc.h"
 
 // PerfMonComps includes
 #include "PerfMonMTAuditor.h"
-#include "GaudiKernel/MsgStream.h"
+
 
 /*
  * Constructor
@@ -40,48 +41,33 @@ StatusCode PerfMonMTAuditor::initialize()
  * Implementation of base class methods
  */
 void PerfMonMTAuditor::before( StandardEventType etype, INamedInterface* component ) {
-  MsgStream msg( msgSvc(), name() );
-  //msg << MSG::INFO << "before Standard: " << etype << " - " << component->name() << endmsg;
   return m_perfMonMTSvc->startAud( toStr(etype) , component->name() );
 }
 
-void PerfMonMTAuditor::before( StandardEventType, const std::string& ) {
-  return;
+void PerfMonMTAuditor::before( StandardEventType etype, const std::string& compName ) {
+  return m_perfMonMTSvc->startAud( toStr(etype) , compName );
 }
 
-void PerfMonMTAuditor::before( CustomEventTypeRef, INamedInterface* ) {
-  // don't poll for custom event type for now...
-  return;
+void PerfMonMTAuditor::before( CustomEventTypeRef etype, INamedInterface* component ) {
+  return m_perfMonMTSvc->startAud( etype , component->name() );
 }
 
-void PerfMonMTAuditor::before( CustomEventTypeRef etype, const std::string& component ) {
-  MsgStream msg( msgSvc(), name() );
-  //msg << MSG::INFO << "before Custom: " << etype << " - " << component << endmsg;
-  return m_perfMonMTSvc->startAud( etype , component );
-  // don't poll for custom event type for now...
-  //return;
+void PerfMonMTAuditor::before( CustomEventTypeRef etype, const std::string& compName ) {
+  return m_perfMonMTSvc->startAud( etype , compName );
 }
 
 void PerfMonMTAuditor::after( StandardEventType etype, INamedInterface* component, const StatusCode& ) {
-  MsgStream msg( msgSvc(), name() );
-  //msg << MSG::INFO << "after Standard : " << etype << " - " << component->name() << endmsg;
   return m_perfMonMTSvc->stopAud( toStr(etype), component->name() );
 }
 
-void PerfMonMTAuditor::after( StandardEventType, const std::string&, const StatusCode& ) {
-  return;
+void PerfMonMTAuditor::after( StandardEventType etype, const std::string& compName, const StatusCode& ) {
+  return m_perfMonMTSvc->stopAud( toStr(etype), compName );
 }
 
-void PerfMonMTAuditor::after( CustomEventTypeRef, INamedInterface*, const StatusCode& ) {
-  // don't poll for custom event type for now...
-  return;
+void PerfMonMTAuditor::after( CustomEventTypeRef etype, INamedInterface* component, const StatusCode& ) {
+  return m_perfMonMTSvc->stopAud( etype, component->name() );
 }
 
-void PerfMonMTAuditor::after( CustomEventTypeRef etype, const std::string& component , const StatusCode& sc ) {
-  MsgStream msg( msgSvc(), name() );
-  //msg << MSG::INFO << "after Custom: " << etype << " - " << component << endmsg;
-
-  return m_perfMonMTSvc->stopAud( etype, component );
-  // don't poll for custom event type for now...
-  //return;
+void PerfMonMTAuditor::after( CustomEventTypeRef etype, const std::string& compName , const StatusCode& ) {
+  return m_perfMonMTSvc->stopAud( etype, compName );
 }
