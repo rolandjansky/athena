9b39836b2fdd6b88ab1573fc84ca832ce6966954
Nils Krumnack <krumnack@iastate.edu>
2019-08-05 11:35:44 -0500

    add extra includes for backwards compatibility
    Some headers were previously pulling in more header files, I now added
    those includes separately to the files for backwards compatibility.

--- Control/AthToolSupport/AsgTools/AsgTools/MessageCheck.h
+++ Control/AthToolSupport/AsgTools/AsgTools/MessageCheck.h
@@ -12,5 +12,6 @@
 // #warning "use of this file is deprecated, please include the header from AsgMessaging instead"
 
 #include <AsgMessaging/MessageCheck.h>
+#include <xAODRootAccess/tools/TReturnCode.h>
 
 #endif
