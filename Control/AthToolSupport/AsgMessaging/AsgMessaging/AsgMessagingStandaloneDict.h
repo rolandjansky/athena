181234bbd58917013cdb3a507c91434b50c060c3
Nils Krumnack <krumnack@iastate.edu>
2019-08-06 10:57:08 -0500

    merge dictionaries, clean up CMakeLists.txt files
    There was no real reason for having three dictionaries in
    AsgMessaging, two is enough.  Also removed some unneeded dependencies
    from the CMakeLists.txt files at the same time.

--- Control/AthToolSupport/AsgMessaging/AsgMessaging/AsgMessagingStandaloneDict.h
+++ Control/AthToolSupport/AsgMessaging/AsgMessaging/AsgMessagingStandaloneDict.h
@@ -5,11 +5,18 @@
 #ifndef ASGMESSAGING_ASGMESSAGINGSTANDALONEDICT_H
 #define ASGMESSAGING_ASGMESSAGINGSTANDALONEDICT_H
 
+/// This file is used exclusively in standalone compilation to generate
+/// dictionaries for types that in Athena are defined in other packages, and
+/// hence get their dictionaries from elsewhere. As a result, the Athena
+/// compilation must not use this file to generate a dictionary.
+
 #ifdef __GNUC__
 # pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
-// In athena, we get this from GAUDI
+#include "AsgMessaging/INamedInterface.h"
+#include "AsgMessaging/MsgLevel.h"
+#include "AsgMessaging/MsgStream.h"
 #include "AsgMessaging/StatusCode.h"
 
 #endif // not ASGMESSAGING_ASGMESSAGINGSTANDALONEDICT_H
