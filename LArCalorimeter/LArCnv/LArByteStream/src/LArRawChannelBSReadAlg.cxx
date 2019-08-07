60c3fc99c4a65be7a460fdfc058d2e97293f7cb7
Walter Lampl <Walter.Lampl@cern.ch>
2019-08-07 10:45:58 +0200

    LArRawChannelBSReadAlg: add checksum verification, refine handling of data corruption

--- LArCalorimeter/LArCnv/LArByteStream/src/LArRawChannelBSReadAlg.cxx
+++ LArCalorimeter/LArCnv/LArByteStream/src/LArRawChannelBSReadAlg.cxx
@@ -2,9 +2,10 @@
 #include "LArIdentifier/LArOnlineID.h"
 #include "ByteStreamCnvSvcBase/IROBDataProviderSvc.h" 
 #include "LArRawEvent/LArRawChannelContainer.h"
+#include "LArRawEvent/LArDigitContainer.h"
+#include "LArRawEvent/LArFebHeaderContainer.h"
 #include "eformat/Version.h"
 #include "eformat/index.h"
-//#include "eformat/Issue.h"
 
 
 #include "LArByteStream/LArRodBlockStructure.h"
@@ -18,7 +19,28 @@ LArRawChannelBSReadAlg::LArRawChannelBSReadAlg(const std::string& name, ISvcLoca
   
 
 StatusCode LArRawChannelBSReadAlg::initialize() {
-  ATH_CHECK(m_rawChannelKey.initialize());
+  if (m_rawChannelKey.key().size()>0) {
+    ATH_CHECK(m_rawChannelKey.initialize());
+  }
+  else {
+    m_doRawChannels=false;
+  }
+
+  if (m_digitKey.key().size()>0) {
+    ATH_CHECK(m_digitKey.initialize());
+  }
+  else {
+    m_doDigits=false;
+  }
+
+  if (m_febHeaderKey.key().size()>0) {
+    ATH_CHECK(m_febHeaderKey.initialize());
+  }
+  else {
+    m_doFebHeaders=false;
+  }
+  
+
   ATH_CHECK(m_robDataProviderSvc.retrieve());
   ATH_CHECK(detStore()->retrieve(m_onlineId,"LArOnlineID"));  
   return StatusCode::SUCCESS;
@@ -43,69 +65,87 @@ StatusCode LArRawChannelBSReadAlg::execute(const EventContext& ctx) const {
      return StatusCode::SUCCESS;
   } 
   
+  
+  std::unique_ptr<LArRodBlockStructure> rodBlock;
+  uint16_t rodMinorVersion=0x0;
+  uint32_t rodBlockType=0x0;
+
+
   for (const uint32_t* robPtr : larRobs->second) {
     OFFLINE_FRAGMENTS_NAMESPACE::ROBFragment rob(robPtr);
     ATH_MSG_VERBOSE("Decoding ROB fragment 0x" << std::hex << rob.rob_source_id () << " with " << std::dec << rob.rod_fragment_size_word() << "ROB words");
 
     if (rob.rod_fragment_size_word() <3) {
       ATH_MSG_ERROR("Encountered corrupt ROB fragment, less than 3 words!");
-      return StatusCode::FAILURE;
+      if (m_failOnCorruption) {
+	return StatusCode::FAILURE;
+      }else 
+	continue;
     }
     
+     eformat::helper::Version ver(rob.rod_version());
+    //(re-)init rodBlock only once per event or if (very unlikly or even impossible) some FEBs have a differnt firmware
+    if (rodBlock==nullptr || rodMinorVersion !=ver.minor_version() || rodBlockType!=(rob.rod_detev_type()&0xff)) {
+      rodMinorVersion=ver.minor_version();
+      rodBlockType=rob.rod_detev_type()&0xff;
+      ATH_MSG_VERBOSE("Found version " << rodMinorVersion <<  " of Rod Block Type  " <<  rodBlockType);
+      if (rodBlockType==4) { //Physics mode
+	switch(rodMinorVersion) {
+	case 12: //Physics mode v6 09.03.2011 for LHC
+	  rodBlock.reset(new LArRodBlockPhysicsV6);
+	  break;
+	case 11: //Physics mode v5 16.06.2008 for LHC 
+	case 10: //Physics mode v5 16.06.2008 for LHC 
+	  rodBlock.reset(new LArRodBlockPhysicsV5);
+	  break;
+	default:
+	  ATH_MSG_ERROR("Found unsupported ROD Block version " << rodMinorVersion 
+			<< " of ROD block type " << rodBlockType);
+	  return m_failOnCorruption ? StatusCode::FAILURE : StatusCode::SUCCESS;
+	}// end switch(rodMinorVersion)
+      }//end of rodBlockType==4
+      else {
+	ATH_MSG_ERROR("Found unsupported Rod block type " << rodBlockType);
+	return m_failOnCorruption ? StatusCode::FAILURE : StatusCode::SUCCESS;
+      }
+    }//End if need to re-init RodBlock
+
     const uint32_t* pData=rob.rod_data();
     const uint32_t  nData=rob.rod_ndata();
- 
-    eformat::helper::Version ver(rob.rod_version());
-    const uint16_t rodMinorVersion=ver.minor_version();
-    const uint32_t rodBlockType=rob.rod_detev_type()&0xff;
-    ATH_MSG_VERBOSE("Found version " << rodMinorVersion <<  " of Rod Block Type  " <<  rodBlockType);
-
-    std::unique_ptr<LArRodBlockStructure> rodBlock;
-
-
-    if (rodBlockType==4) { //Physics mode
-      switch(rodMinorVersion) {
-      case 12: //Physics mode v6 09.03.2011 for LHC
-	rodBlock.reset(new LArRodBlockPhysicsV6);
-	break;
-      case 11: //Physics mode v5 16.06.2008 for LHC 
-      case 10: //Physics mode v5 16.06.2008 for LHC 
-	rodBlock.reset(new LArRodBlockPhysicsV5);
-	break;
-      default:
-	ATH_MSG_ERROR("Found unsupported ROD Block version " << rodMinorVersion 
-		      << " of ROD block type " << rodBlockType);
-	return StatusCode::FAILURE;
-      }// end switch(rodMinorVersion)
-    }//end of rodBlockType==4
-    else {
-      ATH_MSG_ERROR("Found unsupported Rod block type " << rodBlockType);
-      return StatusCode::FAILURE;
-    }
-
     if (!rodBlock->setFragment(pData,nData)) {
       ATH_MSG_ERROR("Failed to assign fragment pointer to LArRodBlockStructure");
       return StatusCode::FAILURE;
     }
 
+    if(m_verifyChecksum) {
+      const uint32_t onsum  = rodBlock->onlineCheckSum();
+      const uint32_t offsum = rodBlock->offlineCheckSum();
+      if(onsum!=offsum) {
+	ATH_MSG_ERROR("Checksum error:");
+	ATH_MSG_ERROR("online checksum  = 0x" << MSG::hex << onsum);
+	ATH_MSG_ERROR("offline checksum = 0x" << MSG::hex << offsum << MSG::dec);
+	if (m_failOnCorruption) 
+	  return StatusCode::FAILURE;
+	else
+	  continue; //Jump to the next ROD-block
+      }
+    }
+
     //Loop over FEBs in ROD:
     do {
-      HWIdentifier fId( Identifier32(rodBlock->getFEBID()) );
+      HWIdentifier fId(Identifier32(rodBlock->getFEBID()));
       if (!m_onlineId->isValidId(fId)) {
 	ATH_MSG_WARNING("Invalid FEB identifer 0x" << std::hex << fId.get_identifier32().get_compact() 
 			<< ". Skipping");
 	continue;
       }
       
-      //FIXME: Add status-word and checksum verification to catch corrupt events
-
-
+      const int NthisFebChannel=m_onlineId->channelInSlotMax(fId);
       int32_t energy;
       int32_t time;
       int32_t quality;
       uint32_t gain;
       int fcNb;
-      const int NthisFebChannel=m_onlineId->channelInSlotMax(fId);
       while (rodBlock->getNextEnergy(fcNb,energy,time,quality,gain)) {
 	if (fcNb>=NthisFebChannel)
 	  continue;
--- LArCalorimeter/LArCnv/LArByteStream/src/LArRawChannelBSReadAlg.h
+++ LArCalorimeter/LArCnv/LArByteStream/src/LArRawChannelBSReadAlg.h
@@ -17,6 +17,8 @@
 
 //Event classes
 class LArRawChannelContainer;
+class LArDigitContainer;
+class LArFebHeaderContainer;
 class LArOnlineID;
 class IROBDataProviderSvc;
 
@@ -33,12 +35,26 @@ class LArRawChannelBSReadAlg : public  AthReentrantAlgorithm {
   //Event output:
   SG::WriteHandleKey<LArRawChannelContainer> m_rawChannelKey{this,"LArRawChannelKey","LArRawChannels",
       "SG key of the LArRawChannelContainer"};
-  
-  
+
+  SG::WriteHandleKey<LArDigitContainer> m_digitKey{this,"LArDigitKey","FREE"};
+
+  SG::WriteHandleKey<LArFebHeaderContainer> m_febHeaderKey{this,"LArFebHeaderKey","LArFebHeaders"};
+    
   ServiceHandle<IROBDataProviderSvc> m_robDataProviderSvc{this,"ROBDataProviderSvc","ROBDataProviderSvc"};
   
+  //Other properties:
+  BooleanProperty m_verifyChecksum{this,"VerifyChecksum",true,"Calculate and compare checksums to detect data transmission errors"};
+  
+  BooleanProperty m_failOnCorruption{this,"FailOnCorruption",true,"Return FAILURE if data corruption is found"};
+
   //Identifier helper
   const LArOnlineID* m_onlineId;
+
+  //Switches set in initialize based of SG keys 
+  bool m_doRawChannels=true;
+  bool m_doDigits=true;
+  bool m_doFebHeaders=true;
+ 
 };
 
 #endif
