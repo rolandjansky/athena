bfe1193052f943d5982f9fa7ebf7058fbf51b5da
haozturk <h.ozturk@cern.ch>
2019-08-09 13:01:13 +0200

    Updated plotting script according to memory monitoring

--- Control/PerformanceMonitoring/PerfMonComps/scripts/PerfMonMTSvc_plotter.py
+++ Control/PerformanceMonitoring/PerfMonComps/scripts/PerfMonMTSvc_plotter.py
@@ -67,7 +67,7 @@ def snapshot_plotter(snapshot_data, plotname):
   fig.set_tight_layout( True )
   fig.savefig(plotname) 
 
-def comp_plotter(complevel_data, plotname):
+def time_plotter(complevel_data, plotname):
   # Plot Component Level Monitoring
  
   #fig = plt.figure(figsize=(31,150))
@@ -151,6 +151,111 @@ def comp_plotter(complevel_data, plotname):
    
   fig.savefig(plotname)
 
+def mem_plotter(complevel_data, plotname):
+ 
+  fig = plt.figure(figsize=(50,150))
+  stepNum = len(complevel_data)
+ 
+  measurement_threshold = 5 # 5 kB
+
+  for i, step in enumerate(complevel_data):
+
+    components = []
+    vmem_measurements = []
+    rss_measurements = []
+    pss_measurements = []
+    swap_measurements = []
+
+    
+    for component in complevel_data[step]:
+
+      vmem = complevel_data[step][component]['vmem']
+      rss = complevel_data[step][component]['rss']
+      pss = complevel_data[step][component]['pss']
+      swap = complevel_data[step][component]['swap']
+      
+      # Only take components whose measurements are higher a certain threshold
+      if vmem + rss + pss + swap > measurement_threshold:
+        components.append(component)  
+        vmem_measurements.append(vmem) 
+        rss_measurements.append(rss) 
+        pss_measurements.append(pss) 
+        swap_measurements.append(swap) 
+
+    # Sort the components 
+
+    # Prepare the necessary data structures for sorting ( could be more efficient!  )
+    mem_stats_tuple_list = zip(vmem_measurements, rss_measurements,pss_measurements,swap_measurements)
+    comp_dict = dict( zip(components, mem_stats_tuple_list) )
+    sortby_list = [ vmem + rss + pss + swap for vmem, rss, pss, swap in zip(vmem_measurements, rss_measurements,pss_measurements,swap_measurements)]
+    sort_dict = dict(zip(components, sortby_list))
+    
+    # Sort the components according to vmem + rss + pss + swap
+    sorted_comp_tuple_list = sorted(sort_dict.items() , key = operator.itemgetter(1))
+
+    sorted_components = []
+    sorted_vmem_measurements = []
+    sorted_rss_measurements = []
+    sorted_pss_measurements = []
+    sorted_swap_measurements = []
+ 
+    # Fill the necessary lists for plotting
+    for idx in sorted_comp_tuple_list:
+      curr_comp = idx[0]
+      curr_vmem = comp_dict[curr_comp][0]
+      curr_rss = comp_dict[curr_comp][1]
+      curr_pss = comp_dict[curr_comp][2]
+      curr_swap = comp_dict[curr_comp][3]
+
+      sorted_components.append(curr_comp)
+
+      sorted_vmem_measurements.append(curr_vmem)
+      sorted_rss_measurements.append(curr_rss)
+      sorted_pss_measurements.append(curr_pss)
+      sorted_swap_measurements.append(curr_swap)
+    
+    # if there is no nonzero measurement in the step, then skip it
+    if len(sorted_components) == 0:
+      continue
+
+    # Horizontal Bar Chart
+    ax = fig.add_subplot(stepNum,1,i+1)
+
+    index = np.arange(len(sorted_components))
+    bar_width = 0.20
+    opacity = 0.8
+
+    rects1 = plt.barh(index + (2.5)*bar_width, sorted_vmem_measurements,bar_width,
+    alpha=opacity,
+    label='Virtual Memory')
+
+    rects2 = plt.barh(index + (1.5)*bar_width, sorted_rss_measurements, bar_width,
+    alpha=opacity,
+    label='Resident Set Size')
+
+    rects3 = plt.barh(index + (0.5)*bar_width, sorted_pss_measurements,bar_width,
+    alpha=opacity,
+    label='Proportional Set Size')
+
+    rects4 = plt.barh(index - (0.5)*bar_width, sorted_swap_measurements, bar_width,
+    alpha=opacity,
+    label='Swap')
+
+    plt.ylabel('Components',fontsize = 35)
+    plt.xlabel('Time(ms)', fontsize = 35)
+    plt.title(step, fontsize = 40, fontweight = "bold")
+    plt.yticks(index + bar_width, sorted_components)
+    plt.legend(prop={'size': 30})
+    
+    ax.tick_params(axis='both', which='major', labelsize=30)
+    ax.tick_params(axis='both', which='minor', labelsize=30)
+
+
+    fig.set_tight_layout( True )
+    
+   
+  fig.savefig(plotname)
+
 
 with open( result_file ) as json_file:
   data = json.load(json_file)
@@ -158,9 +263,11 @@ with open( result_file ) as json_file:
   snapshot_data = data['Snapshot_level']
   snapshot_plotter(snapshot_data, 'snapshot_level.pdf')
   
-  serial_complevel_data = data['Serial_Component_level']
-  comp_plotter(serial_complevel_data, 'serial_complevel.pdf')
+  timeMon_serial_data = data['TimeMon_Serial']
+  time_plotter(timeMon_serial_data, 'TimeMon_Serial.pdf')
 
-  parallel_complevel_data = data['Parallel_Component_level']
-  comp_plotter(parallel_complevel_data, 'parallel_complevel.pdf')
+  timeMon_parallel_data = data['TimeMon_Parallel']
+  time_plotter(timeMon_parallel_data, 'TimeMon_Parallel.pdf')
 
+  memMon_serial_data = data['MemMon_Serial']
+  mem_plotter(memMon_serial_data, 'MemMon_Serial.pdf')
