d167bad68caf0b22bff10318d4ab21c8d3ee00c4
MihaMuskinja <miha.muskinja@gmail.com>
2019-08-12 18:15:39 +0200

    remove the Mesons package and all dependencies

--- Simulation/G4Extensions/Mesons/python/MesonsConfigDb.py
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
-
-from AthenaCommon.CfgGetter import addTool
-addTool("Mesons.MesonsConfig.getMesonsPhysicsTool"         , "MesonsPhysicsTool" )
--- Simulation/G4Extensions/Mesons/src/components/Mesons_entries.cxx
+++
@@ -1,3 +0,0 @@
-#include "../MesonsPhysicsTool.h"
-
-DECLARE_COMPONENT( MesonsPhysicsTool )
