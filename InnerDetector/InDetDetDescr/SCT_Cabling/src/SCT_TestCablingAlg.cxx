22cd62e3d7ad6b91c74f390d5aceffcaff2b6ac6
Susumu Oda <susumu.oda@cern.ch>
2019-08-13 11:12:35 +0000

    Some updates/fixes of SCT_CablingWriteToFile.py

--- InnerDetector/InDetDetDescr/SCT_Cabling/src/SCT_TestCablingAlg.cxx
+++ InnerDetector/InDetDetDescr/SCT_Cabling/src/SCT_TestCablingAlg.cxx
@@ -63,7 +63,8 @@ SCT_TestCablingAlg::coordString(const Identifier& offlineId) const {
 
 StatusCode
 SCT_TestCablingAlg::execute(const EventContext& ctx) const {
-  const string testAreaPath{CoveritySafe::getenv("TestArea")};
+  // const string testAreaPath{CoveritySafe::getenv("TestArea")};
+  const string testAreaPath = ".";
   string filename{testAreaPath+"/cabling.txt"};
   ATH_MSG_INFO("Filename: " << filename << " will be written to your $TestArea.");
   ofstream opFile1{filename.c_str(), ios::out};
@@ -109,7 +110,7 @@ SCT_TestCablingAlg::execute(const EventContext& ctx) const {
     rodHashMap[rods[i]]=i;
   }
   //open a file for writing
-  const std::string fullFileName{testAreaPath + "/" + makeCablingFileName()};
+  const std::string fullFileName{testAreaPath + "/SCT_Cabling_svc" + (m_POSIXtime ? ("_" +std::to_string(m_POSIXtime)) : "") + ".dat"};
   ATH_MSG_INFO("Open file for write " << fullFileName);
   ofstream opFile{fullFileName.c_str(), ios::out};
   opFile << XmlHeader << endl;
