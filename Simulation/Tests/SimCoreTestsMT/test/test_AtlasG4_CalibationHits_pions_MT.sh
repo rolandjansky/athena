0ccf2bee48c1451617685805bc97b493bb0ce88d
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2019-08-14 11:38:15 +0000

    New MT Simulation ART jobs

---
+++ Simulation/Tests/SimCoreTestsMT/test/test_AtlasG4_CalibationHits_pions_MT.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# art-description: Run MT simulation outside ISF, reading single pion events, writing HITS including CaloCalibrationHits, using MC16 geometry and conditions
+# art-include: master/Athena
+
+# art-type: grid
+# art-athena-mt: 8
+# art-output: log.*
+# art-output: test.HITS.pool.root
+
+export ATHENA_CORE_NUMBER=8
+
+AtlasG4_tf.py \
+--multithreaded \
+--inputEVNTFile '/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ISF_Validation/pi_E50_eta0-60.evgen.pool.root' \
+--outputHITSFile 'test.HITS.pool.root' \
+--maxEvents '10' \
+--skipEvents '0' \
+--randomSeed '10' \
+--geometryVersion 'ATLAS-R2-2016-01-00-01' \
+--conditionsTag 'OFLCOND-MC16-SDR-14' \
+--DataRunNumber '284500' \
+--physicsList 'FTFP_BERT_ATL' \
+--postInclude 'PyJobTransforms/UseFrontier.py' \
+--preExec 'AtlasG4Tf:simFlags.ReleaseGeoModel=False;' \
+--preInclude 'SimulationJobOptions/preInclude.CalHits.py,SimulationJobOptions/preInclude.ParticleID.py' \
+--imf False
+
+rc=$?
+echo  "art-result: $rc simulation"
+rc2=-9999
+if [ $rc -eq 0 ]
+then
+    ArtPackage=$1
+    ArtJobName=$2
+    art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary --order-trees
+    rc2=$?
+fi
+echo  "art-result: $rc2 regression"
