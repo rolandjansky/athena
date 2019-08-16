89be07e3e6f4ca50be7afa282040b1c3da2f292d
Siarhei Harkusha <siarhei.harkusha@cern.ch>
2019-08-16 11:04:38 +0000

    TileSimAlgs+TileL2Algs: Update Tile digitization configuration for Run3
    Added possibility to put Tile L2 container
    produced by Tile raw channels to L2 algorithm into output file.
    
    Tile digitization configuration has been udpated to include
    all Tile components participating in digitization.
    
    Test for Tile digitization configuration for run 3 has been added.

--- TileCalorimeter/TileSimAlgs/python/TileDigitizationConfig.py
+++ TileCalorimeter/TileSimAlgs/python/TileDigitizationConfig.py
@@ -2,16 +2,76 @@
 
 # Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 """
-from TileSimAlgs.TileHitToTTL1Config import TileHitToTTL1Cfg
-from TileSimAlgs.TileDigitsMakerConfig import TileDigitsMakerCfg
-from TileSimAlgs.TileHitVecToCntConfig import TileHitOutputCfg
 
-# TODO implement all configuration. Status is progress for DigitizationConfigNew_test.py
+def TileTriggerDigitizationCfg(flags):
+    """Return ComponentAccumulator with standard Tile Digitization configuration"""
+
+    from TileSimAlgs.TileHitToTTL1Config import TileHitToTTL1OutputCfg
+    acc = TileHitToTTL1OutputCfg(flags)
+
+    from TileSimAlgs.TileMuonReceiverConfig import TilePulseForTileMuonReceiverOutputCfg
+    acc.merge( TilePulseForTileMuonReceiverOutputCfg(flags) )
+
+    if not flags.Digitization.PileUpPremixing:
+        from TileSimAlgs.TileMuonReceiverDecisionConfig import TileMuonReceiverDecisionOutputCfg
+        acc.merge( TileMuonReceiverDecisionOutputCfg(flags) )
+
+        from TileL2Algs.TileL2Config import TileRawChannelToL2OutputCfg
+        acc.merge( TileRawChannelToL2OutputCfg(flags, streamName = 'RDO') )
+
+    return acc
 
 def TileDigitizationCfg(flags):
     """Return ComponentAccumulator with standard Tile Digitization configuration"""
-    acc = TileHitToTTL1Cfg(flags)
-    acc.merge(TileDigitsMakerCfg(flags))
-    acc.merge(TileHitOutputCfg(flags))
+
+    from TileSimAlgs.TileDigitsMakerConfig import TileDigitsMakerOutputCfg
+    acc = TileDigitsMakerOutputCfg(flags)
+
+    if not flags.Digitization.PileUpPremixing:
+        from TileRecUtils.TileRawChannelMakerConfig import TileRawChannelMakerOutputCfg
+        acc.merge( TileRawChannelMakerOutputCfg(flags, streamName = 'RDO') )
+
     return acc
 
+
+
+if __name__ == "__main__":
+
+    from AthenaCommon.Configurable import Configurable
+    Configurable.configurableRun3Behavior = 1
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from AthenaConfiguration.TestDefaults import defaultTestFiles
+    from AthenaCommon.Logging import log
+    from AthenaCommon.Constants import DEBUG
+
+    # Test setup
+    log.setLevel(DEBUG)
+
+    ConfigFlags.Input.Files = defaultTestFiles.HITS
+    ConfigFlags.Tile.RunType = 'PHY'
+    ConfigFlags.Output.RDOFileName = 'myRDO.pool.root'
+    ConfigFlags.IOVDb.GlobalTag = 'OFLCOND-MC16-SDR-16'
+    ConfigFlags.Digitization.Pileup = False
+
+    ConfigFlags.fillFromArgs()
+
+    ConfigFlags.lock()
+    ConfigFlags.dump()
+
+    # Construct our accumulator to run
+    from AthenaConfiguration.MainServicesConfig import MainServicesThreadedCfg
+    acc = MainServicesThreadedCfg(ConfigFlags)
+
+    from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
+    acc.merge(PoolReadCfg(ConfigFlags))
+
+    acc.merge( TileDigitizationCfg(ConfigFlags) )
+    acc.merge( TileTriggerDigitizationCfg(ConfigFlags) )
+
+    acc.printConfig(withDetails = True, summariseProps = True)
+    acc.store( open('TileDigitization.pkl','w') )
+
+    sc = acc.run(maxEvents=3)
+    # Success should be 0
+    import sys
+    sys.exit(not sc.isSuccess())
