41de74029cae167ab84982714138f33773a90180
James Catmore <james.catmore@cern.ch>
2019-08-19 08:04:20 +0000

    Adding code to handle MC event weight string from metadata
    ATLASG-1510
    
    This MR addresses an issue where the MC weight name metadata is written into the AOD as a string rather than a dictionary, causing the job to crash. The code re-builds the dictionary so that it can be processed downstream. It does not use the full weight name (which for these samples is a long series of MadGraph input cards) but instead uses the weight index also as the key. This was suggested by the analysts.
    
    The code tests whether the metadata is a string or not, and only executes if it is a string

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/LHE3WeightMetadata.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/LHE3WeightMetadata.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
 import operator
-
+import json 
 def addLHE3Weights(seq, pref = '', var_dict = {}):
   
   from AthenaCommon.AppMgr import ToolSvc
@@ -28,11 +28,28 @@ from DerivationFrameworkCore.DerivationFrameworkMaster import *
 from AthenaCommon.GlobalFlags import globalflags
 if globalflags.DataSource() == 'geant4':
 
+  mcweight_dict_orig = dict()
   mcweight_dict = dict()
   try:
-    mcweight_dict = inputFileSummary['metadata']['/Generation/Parameters']['HepMCWeightNames']
+    mcweight_dict_orig = inputFileSummary['metadata']['/Generation/Parameters']['HepMCWeightNames']
   except:
     print 'Could not retrieve HepMCWeightNames /Generation/Parameters metadata.'
-    
+  
+  # Recent versions of MadGraph are writing the dictionary as a flat string
+  # The next few lines ensure the string is reformatted as a dictionary
+  # before it gets to addLHE3Weights
+  if type(mcweight_dict_orig) is str:
+    # json.loads requires double quotes around the key
+    mcweight_dict_orig = mcweight_dict_orig.replace('\n', '').replace('\'','\"')
+    mcweight_dict_orig = json.loads(mcweight_dict_orig)
+    # The "keys" in MG are long input cards and not suitable for storage.
+    # So we just store the index in str form...
+    for key in mcweight_dict_orig.keys():
+      val = mcweight_dict_orig[key]
+      newkey = str(val)
+      mcweight_dict[newkey] = val
+  else:
+    mcweight_dict = mcweight_dict_orig
+        
   addLHE3Weights(DerivationFrameworkJob, "", mcweight_dict)
 
