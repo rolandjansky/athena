4be9166ad811685058b634eb8e68a537f157a90e
James Richard Catmore <james.catmore@cern.ch>
2019-08-23 16:16:46 +0200

    Adding DAOD_PHYS2 to derivation framework
    ATLASG-1502
    
    This MR introduces a new format DAOD_PHYS2 to the derivation framework. It is a clone of the DAOD_PHYS format, the prototype general format for Run 3, with the addition of lossy compression applied to most float containers. Alongside the python job options two lists of the containers to be compressed are provided (one for data, one for MC). The generation of these lists will be automated in the future according to the file contents, similarly to other applications in the derivation framework. This PHYS2 format will be merged with DAOD_PHYS once the lossy compression is established properly, so it is a temporary format that will not be produced in bulk.
    
    The obsolete DAOD_MINI format (same as PHYS but with a now-abandoned name) is dropped in the MR as well.
    
    This MR should have no impact on any current production as the prototype formats are not used for analysis yet.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/DerivationFrameworkProdFlags.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/DerivationFrameworkProdFlags.py
@@ -32,19 +32,6 @@ listAODtoDPD.append(WriteDAOD_PHYSVALStream.StreamName)
 # Run-3 prototypes
 ####################################
 
-class WriteDAOD_MINIStream(JobProperty):
-    """ DAOD_MINI - prototype format for Run 3 """
-    statusOn     = True
-    allowedTypes = ['bool']
-    StoredValue  = False
-    StreamName   = "StreamDAOD_MINI"
-    FileName     = ""
-    isVirtual      = False
-    DPDMakerScript = "DerivationFrameworkExamples/MINI.py"
-    pass
-jobproperties.DerivationFrameworkProdFlags.add_JobProperty(WriteDAOD_MINIStream)
-listAODtoDPD.append(WriteDAOD_MINIStream.StreamName)
-
 class WriteDAOD_PHYSStream(JobProperty):
     """ DAOD_PHYS - prototype format for Run 3 """
     statusOn     = True
@@ -57,6 +44,19 @@ class WriteDAOD_PHYSStream(JobProperty):
     pass
 jobproperties.DerivationFrameworkProdFlags.add_JobProperty(WriteDAOD_PHYSStream)
 listAODtoDPD.append(WriteDAOD_PHYSStream.StreamName)
+
+class WriteDAOD_PHYS2Stream(JobProperty):
+    """ DAOD_PHYS2 - prototype format for Run 3 """
+    statusOn     = True
+    allowedTypes = ['bool']
+    StoredValue  = False
+    StreamName   = "StreamDAOD_PHYS2"
+    FileName     = ""
+    isVirtual      = False
+    DPDMakerScript = "DerivationFrameworkExamples/PHYS2.py"
+    pass
+jobproperties.DerivationFrameworkProdFlags.add_JobProperty(WriteDAOD_PHYS2Stream)
+listAODtoDPD.append(WriteDAOD_PHYS2Stream.StreamName)
 
 class WriteDAOD_NANOStream(JobProperty):
     """ DAOD_NANO - prototype format for Run 3 """
