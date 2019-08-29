89498f911a8fd6b6943e2a423ba304e0bb6e4044
Alexandre Laurier <alexandre.laurier@cern.ch>
2019-08-30 01:00:57 +0200

    Create NSW-MM Overlay package

---
+++ MuonSpectrometer/MuonOverlay/MM_Overlay/src/components/MM_Overlay_entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "MM_Overlay/MM_Overlay.h"
+
+DECLARE_ALGORITHM_FACTORY( MM_Overlay )
+  
+DECLARE_FACTORY_ENTRIES( MM_Overlay ) {
+  DECLARE_ALGORITHM( MM_Overlay )
+}
