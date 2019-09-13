2999bd938fb9c16c3c52fb3347d14f0fc2076def
Peter Onyisi <ponyisi@utexas.edu>
2019-09-13 16:32:48 +0000

    Migrate some DataQualityTools to new framework

--- Control/AthenaMonitoring/test/HistogramFillerRebinable1DTestSuiteExtendAxes.cxx
+++ Control/AthenaMonitoring/test/HistogramFillerRebinable1DTestSuiteExtendAxes.cxx
@@ -35,27 +35,21 @@ class HistogramFillerRebinable1DTestSuite {
   private:
     list<function<void(void)>> registeredTestCases() {
       return {
-        REGISTER_TEST_CASE(test_shouldKeepNumberOfBeansForValueInHistogramsRange),
-        REGISTER_TEST_CASE(test_shouldDoubleNumberOfBeansForBoundaryValueOf3),
-
-        REGISTER_TEST_CASE(test_shouldDoubleNumberOfBeansForValueSlightlySmallerThan5),
-        REGISTER_TEST_CASE(test_shouldQuadrupleNumberOfBeansForBoundaryValueOf5),
-        REGISTER_TEST_CASE(test_shouldQuadrupleNumberOfBeansForValueSlightlyBiggerThan5),
-
-        REGISTER_TEST_CASE(test_shouldQuadrupleNumberOfBeansForValueSlightlySmallerThan9),
-        REGISTER_TEST_CASE(test_shouldOctupleNumberOfBeansForBoundaryValueOf9),
-        REGISTER_TEST_CASE(test_shouldOctupleNumberOfBeansForValueSlightlyBiggerThan9),
+        REGISTER_TEST_CASE(test_shouldNotChangeForValueInHistogramsRange),
+        REGISTER_TEST_CASE(test_shouldChangeXmaxForBoundaryValueOf3),
+        REGISTER_TEST_CASE(test_shouldChangeXmaxForValueSlightlySmallerThan5),
+        REGISTER_TEST_CASE(test_shouldChangeXminForValueOf0),
       };
     }
 
   // ==================== Test code ====================
   private:
     void beforeEach() {
-        m_histogramDef.opt = "kAddBinsDynamically";
-        m_histogramProvider.reset(new MockHistogramProvider());
-        m_monitoredVariable.reset(new MockMonitoredVariable(""));
-        m_histogram.reset(new TH1D("MockHistogram", "Mock Histogram", 8, 1.0, 3.0));
-        m_testObj.reset(new HistogramFillerRebinable1D(m_histogramDef, m_histogramProvider));
+        m_histogramDef.opt = "kExtendAxes";
+        m_histogramProvider = make_shared<MockHistogramProvider>();
+        m_monitoredVariable = make_shared<MockMonitoredVariable>("");
+        m_histogram = make_shared<TH1D>("MockHistogram", "Mock Histogram", 8, 1.0, 3.0);
+        m_testObj = make_shared<HistogramFillerRebinable1D>(m_histogramDef, m_histogramProvider);
 
         m_testObj->setMonitoredVariables({ *m_monitoredVariable });
         m_histogramProvider->mock_histogram = [this]() { return m_histogram.get(); };
@@ -64,7 +58,7 @@ class HistogramFillerRebinable1DTestSuite {
     void afterEach() {
     }
 
-    void test_shouldKeepNumberOfBeansForValueInHistogramsRange() {
+    void test_shouldNotChangeForValueInHistogramsRange() {
       m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
         return { 2.9 };
       };
@@ -81,7 +75,7 @@ class HistogramFillerRebinable1DTestSuite {
       VALUE(m_histogram->GetBinContent(8)) EXPECTED(1.0);
     }
 
-    void test_shouldDoubleNumberOfBeansForBoundaryValueOf3() {
+    void test_shouldChangeXmaxForBoundaryValueOf3() {
       m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
         return { 3.0 };
       };
@@ -92,13 +86,13 @@ class HistogramFillerRebinable1DTestSuite {
 
       m_testObj->fill();
 
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(16);
+      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
       VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
       VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(5.0);
-      VALUE(m_histogram->GetBinContent(9)) EXPECTED(1.0);
+      VALUE(m_histogram->GetBinContent(5)) EXPECTED(1.0);
     }
 
-    void test_shouldDoubleNumberOfBeansForValueSlightlySmallerThan5() {
+    void test_shouldChangeXmaxForValueSlightlySmallerThan5() {
       m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
         return { 4.9 };
       };
@@ -109,15 +103,15 @@ class HistogramFillerRebinable1DTestSuite {
 
       m_testObj->fill();
 
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(16);
+      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
       VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
       VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(5.0);
-      VALUE(m_histogram->GetBinContent(16)) EXPECTED(1.0);
+      VALUE(m_histogram->GetBinContent(8)) EXPECTED(1.0);
     }
 
-    void test_shouldQuadrupleNumberOfBeansForBoundaryValueOf5() {
+    void test_shouldChangeXminForValueOf0() {
       m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
-        return { 5.0 };
+        return { 0.0 };
       };
 
       VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
@@ -126,78 +120,10 @@ class HistogramFillerRebinable1DTestSuite {
 
       m_testObj->fill();
 
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(32);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(9.0);
-      VALUE(m_histogram->GetBinContent(17)) EXPECTED(1.0);
-    }
-
-    void test_shouldQuadrupleNumberOfBeansForValueSlightlyBiggerThan5() {
-      m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
-        return { 5.1 };
-      };
-
       VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
+      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(-1.0);
       VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(3.0);
-
-      m_testObj->fill();
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(32);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(9.0);
-      VALUE(m_histogram->GetBinContent(17)) EXPECTED(1.0);
-    }
-
-    void test_shouldQuadrupleNumberOfBeansForValueSlightlySmallerThan9() {
-      m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
-        return { 8.9 };
-      };
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(3.0);
-
-      m_testObj->fill();
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(32);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(9.0);
-      VALUE(m_histogram->GetBinContent(32)) EXPECTED(1.0);
-    }
-
-    void test_shouldOctupleNumberOfBeansForBoundaryValueOf9() {
-      m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
-        return { 9.0 };
-      };
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(3.0);
-
-      m_testObj->fill();
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(64);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(17.0);
-      VALUE(m_histogram->GetBinContent(33)) EXPECTED(1.0);
-    }
-
-    void test_shouldOctupleNumberOfBeansForValueSlightlyBiggerThan9() {
-      m_monitoredVariable->mock_getVectorRepresentation = []() -> vector<double> {
-        return { 9.1 };
-      };
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(8);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(3.0);
-
-      m_testObj->fill();
-
-      VALUE(m_histogram->GetXaxis()->GetNbins()) EXPECTED(64);
-      VALUE(m_histogram->GetXaxis()->GetXmin()) EXPECTED(1.0);
-      VALUE(m_histogram->GetXaxis()->GetXmax()) EXPECTED(17.0);
-      VALUE(m_histogram->GetBinContent(33)) EXPECTED(1.0);
+      VALUE(m_histogram->GetBinContent(3)) EXPECTED(1.0);
     }
 
   // ==================== Helper methods ====================
@@ -206,7 +132,7 @@ class HistogramFillerRebinable1DTestSuite {
   // ==================== Initialization & run ====================
   public:
     HistogramFillerRebinable1DTestSuite() 
-      : m_log(Athena::getMessageSvc(), "HistogramFillerRebinable1DTestSuite") {
+      : m_log(Athena::getMessageSvc(), "HistogramFillerRebinable1DTestSuiteExtendAxes") {
     }
 
     void run() {
