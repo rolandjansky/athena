75e837717c40ed1949ba99e07f553dc79053c943
sherwood <peter.sherwood@cern.ch>
2019-09-13 09:47:34 +0200

    remove debug output from SpecifiedJetsCondition.cxx

--- Trigger/TrigHypothesis/TrigHLTJetHypoUnitTests/src/SpecifiedJetsCondition.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypoUnitTests/src/SpecifiedJetsCondition.cxx
@@ -20,32 +20,24 @@ bool
 SpecifiedJetsCondition::groupIsSatisfied(const HypoJetVector& ips,
 					 const HypoJetVector& group,
 					 const std::unique_ptr<ITrigJetHypoInfoCollector>&) const {
-  std::cerr<<"SpecifiedJetsCondition::isSatisfied seeking ";
+
   for(const auto& j : ips){
-    auto j_addr = static_cast<const void*>(j);
-    std::cerr<< j_addr << " ";
     if(std::find(group.begin(), group.end(), j) == group.end()){
       return false;
     }
   }
+
   return true;
 }
 
 bool 
 SpecifiedJetsCondition::isSatisfied(const HypoJetVector& ips,
 				    const std::unique_ptr<ITrigJetHypoInfoCollector>& collector) const {
-  std::cerr<<toString();
-  std::cerr<<"SpecifiedJetsCondition::isSatisfied seeking ";
-  std::size_t count{0};
   for(const auto& jg : m_acceptedJetGroups){
     if(groupIsSatisfied(ips, jg, collector)){
-      std::cerr <<"SpecifiedJetsCondition::isSatisfied job group "
-		<< count << " is satisfied ";
-      ++count;
       return true;
     }
   }
-  std::cerr<<"SpecifiedJetsCondition::isSatisfied no job group satisfied ";
   return false;
 }
 
