a41ad43abf75780d439a8504ed58561133b22d91
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2019-09-23 16:57:40 +0200

    Updating AthGeneration to Pythia8 243p4.
    This is to fix the issue discussed in
    https://sft.its.cern.ch/jira/browse/SPI-1459.

--- Projects/AthGeneration/externals/Pythia8.cmake
+++ Projects/AthGeneration/externals/Pythia8.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of Pythia 8 to use.
 #
 
-set( PYTHIA8_LCGVERSION 243p3 )
+set( PYTHIA8_LCGVERSION 243p4 )
 set( PYTHIA8_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/pythia8/${PYTHIA8_LCGVERSION}/${LCG_PLATFORM} )
