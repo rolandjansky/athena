51d97dca3a4cd254b0451bd5f18f3a5792f9e7b0
Adam Edward Barton <adam.edward.barton@cern.ch>
2019-09-25 16:16:47 +0000

    Remove rob set caching from DataProviderTools

--- InnerDetector/InDetEventCnv/SCT_RawDataByteStreamCnv/src/SCTRawDataProviderTool.h
+++ InnerDetector/InDetEventCnv/SCT_RawDataByteStreamCnv/src/SCTRawDataProviderTool.h
@@ -17,8 +17,7 @@
 #include "GaudiKernel/ToolHandle.h"
 
 #include <atomic>
-#include <mutex>
-#include <unordered_set>
+
 
 class ISCT_RodDecoder;
 
@@ -45,20 +44,6 @@ class SCTRawDataProviderTool : public extends<AthAlgTool, ISCTRawDataProviderToo
   virtual StatusCode initialize() override;
 
   /**
-   * @brief Old-style decoding method.
-   *
-   * Retrieve EventContext and call the convert method with EventContext
-   *
-   * @param vecROBFrags Vector containing ROB framgents.
-   * @param rdoIDCont RDO ID Container to be filled.
-   * @param errs Byte stream error container.
-   * @param bsFracCont Byte stream fraction container.
-   *  */
-  virtual StatusCode convert(std::vector<const OFFLINE_FRAGMENTS_NAMESPACE::ROBFragment*>& vecROBFrags,
-                             ISCT_RDO_Container& rdoIDCont,
-                             InDetBSErrContainer* errs,
-                             SCT_ByteStreamFractionContainer* bsFracCont) const override;
-  /**
    * @brief Main decoding method.
    *
    * Loops over ROB fragments, get ROB/ROD ID, then decode if not allready decoded.
@@ -72,26 +57,17 @@ class SCTRawDataProviderTool : public extends<AthAlgTool, ISCTRawDataProviderToo
   virtual StatusCode convert(std::vector<const OFFLINE_FRAGMENTS_NAMESPACE::ROBFragment*>& vecROBFrags,
                              ISCT_RDO_Container& rdoIDCont,
                              InDetBSErrContainer* errs,
-                             SCT_ByteStreamFractionContainer* bsFracCont,
-                             const EventContext& ctx) const override;
+                             SCT_ByteStreamFractionContainer* bsFracCont) const override;
 
  private: 
 
   /** Algorithm Tool to decode ROD byte stream into RDO. */
   ToolHandle<ISCT_RodDecoder> m_decoder{this, "Decoder", "SCT_RodDecoder", "Decoder"};
-  
-  /** For bookkeeping of decoded ROBs */
-  struct CacheEntry {
-    EventContext::ContextEvt_t m_evt{EventContext::INVALID_CONTEXT_EVT};
-    std::unordered_set<uint32_t> m_robIDSet;
-  };
-  mutable SG::SlotSpecificObj<CacheEntry> m_cache ATLAS_THREAD_SAFE; // Guarded by m_mutex
 
   /** Number of decode errors encountered in decoding. 
       Turning off error message after 100 errors are counted */
   mutable std::atomic_int m_decodeErrCount{0};
 
-  mutable std::mutex m_mutex{};
 };
 
 #endif // SCT_RAWDATABYTESTREAMCNV_SCTRAWDATAPROVIDERTOOL_H
