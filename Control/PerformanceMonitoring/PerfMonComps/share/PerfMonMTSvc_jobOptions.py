ecb11ee4da458d0969e3faf5045382ef5dd0a962
haozturk <h.ozturk@cern.ch>
2019-09-27 20:53:41 +0200

    Configure the service such that it starts first in the sequence

--- Control/PerformanceMonitoring/PerfMonComps/share/PerfMonMTSvc_jobOptions.py
+++ Control/PerformanceMonitoring/PerfMonComps/share/PerfMonMTSvc_jobOptions.py
@@ -5,13 +5,15 @@ from AthenaCommon.Logging import logging
 log = logging.getLogger("PerfMonMTSvc_jobOptions.py")
 log.info("Setting up PerfMonMT...")
 
-
 from AthenaCommon.AppMgr import ServiceMgr as svcMgr
 if not hasattr(svcMgr, 'PerfMonMTSvc'):
     from PerfMonComps.MTJobOptCfg import PerfMonMTSvc
     svcMgr += PerfMonMTSvc( "PerfMonMTSvc"  )
-    # To disable event loop monitoring, make false the following property 
-    svcMgr.PerfMonMTSvc.doEventLoopMonitoring = True
+    # Set up the service as the first service to be created:
+    theApp.CreateSvc.insert( 0, "PerfMonMTSvc/PerfMonMTSvc" )
+    # Disable event loop monitoring by default
+    svcMgr.PerfMonMTSvc.doEventLoopMonitoring = False
     pass
+
 
 
