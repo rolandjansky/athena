0a76ded5c51a2d54cfcf417d8e9fb29a91c6c056
Edward Moyse <edward.moyse@cern.ch>
2019-10-01 14:58:00 +0200

    Fixed some typos (property names) and added some missing overrides, and tool definition.

--- MuonSpectrometer/MuonCnv/MuonEventCnvTools/src/MuonEventCnvTool.h
+++ MuonSpectrometer/MuonCnv/MuonEventCnvTools/src/MuonEventCnvTool.h
@@ -44,22 +44,22 @@ namespace Muon {
         virtual StatusCode initialize();
 
         /** check that the RoT is correctly filled*/
-        virtual void checkRoT( const Trk::RIO_OnTrack& rioOnTrack ) const;
+        virtual void checkRoT( const Trk::RIO_OnTrack& rioOnTrack ) const override;
 
         virtual std::pair<const Trk::TrkDetElementBase*, const Trk::PrepRawData*> 
-            getLinks( const Trk::RIO_OnTrack& rioOnTrack ) const;
+            getLinks( const Trk::RIO_OnTrack& rioOnTrack ) const override;
         
         /** @copydoc Trk::ITrkEventCnvTool::prepareRIO_OnTrack( Trk::RIO_OnTrack* rot)*/    
-        virtual void prepareRIO_OnTrack( Trk::RIO_OnTrack* rot) const;
+        virtual void prepareRIO_OnTrack( Trk::RIO_OnTrack* rot) const override;
         
         /** Take the passed RoT and recreate it (i.e. fill missing pointers etc)*/
-        virtual void recreateRIO_OnTrack( Trk::RIO_OnTrack *RoT ) const;
+        virtual void recreateRIO_OnTrack( Trk::RIO_OnTrack *RoT ) const override;
         
         /** Return the detectorElement associated with this Identifier*/
-        virtual const Trk::TrkDetElementBase* getDetectorElement(const Identifier& id, const IdentifierHash& idHash) const;
+        virtual const Trk::TrkDetElementBase* getDetectorElement(const Identifier& id, const IdentifierHash& idHash) const override;
         
         /** Return the detectorElement associated with this Identifier*/
-        virtual const Trk::TrkDetElementBase* getDetectorElement(const Identifier& id) const;
+        virtual const Trk::TrkDetElementBase* getDetectorElement(const Identifier& id) const override;
         
         template<class CONT>
         const Trk::PrepRawData* getLink( const Identifier& id,  const IdentifierHash& idHash, const SG::ReadHandleKey<CONT>& handle ) const;
@@ -74,20 +74,20 @@ namespace Muon {
 
         const MuonGM::MuonDetectorManager* m_muonMgr;                 //!<Muon detector manager
 
-        ToolHandle<Muon::MuonIdHelperTool> m_idHelperTool;
+        ToolHandle<Muon::MuonIdHelperTool> m_idHelperTool {this, "IdHelperTool", "Muon::MuonIdHelperTool/MuonIdHelperTool"};
 
         SG::ReadHandleKey<RpcPrepDataContainer>   m_rpcPrdKey
              { this, "RpcClusterContainer", "RPC_Measurements", "Location for RPC PRDs" };
         SG::ReadHandleKey<CscPrepDataContainer>   m_cscPrdKey
-             { this, "RpcClusterContainer", "CSC_Clusters", "Location for CSC PRDs" };
+             { this, "CscClusterContainer", "CSC_Clusters", "Location for CSC PRDs" };
         SG::ReadHandleKey<TgcPrepDataContainer>   m_tgcPrdKey
-             { this, "RpcClusterContainer", "TGC_MeasurementsAllBCs", "Location for TGC PRDs" };
+             { this, "TgcClusterContainer", "TGC_MeasurementsAllBCs", "Location for TGC PRDs" };
         SG::ReadHandleKey<MdtPrepDataContainer>   m_mdtPrdKey
-             { this, "RpcClusterContainer", "MDT_DriftCircles", "Location for MDT PRDs" };
+             { this, "MdtClusterContainer", "MDT_DriftCircles", "Location for MDT PRDs" };
         SG::ReadHandleKey<MMPrepDataContainer>   m_mmPrdKey
-             { this, "RpcClusterContainer", "MM_Measurements", "Location for MM PRDs" };
+             { this, "MM_ClusterContainer", "MM_Measurements", "Location for MM PRDs" };
         SG::ReadHandleKey<sTgcPrepDataContainer>   m_stgcPrdKey
-             { this, "RpcClusterContainer", "STGC_Measurements", "Location for sTGC PRDs" };
+             { this, "STGC_ClusterContainer", "STGC_Measurements", "Location for sTGC PRDs" };
         
         /** If true, search for PRDs manually - i.e. do not rely on ElementLink working
         @warning This functionality is left in primarily for debugging - will probably be removed in near future.*/
