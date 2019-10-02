ac3450d516da2c4a51580f0f8bd22f3990ca2ee3
Martin Rybar <martin.rybar@cern.ch>
2019-10-02 23:44:32 +0200

    Developement of the pileup tool for HI collisions from rel20 to rel 21

---
+++ PhysicsAnalysis/HeavyIonPhys/HIEventUtils/HIEventUtils/HIEventUtilsDict.h
@@ -0,0 +1,9 @@
+#ifndef HIEVENTUTILS_HIEVENTUTILS_DICT_H
+#define HIEVENTUTILS_HIEVENTUTILS_DICT_H
+
+// This file includes all the header files that you need to create
+// dictionaries for.
+
+#include <HIEventUtils/HIPileupTool.h>
+
+#endif
