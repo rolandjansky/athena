e804497af7d3b27822c9829b1b3fdcc90d4b63b8
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2019-10-02 14:03:48 +0200

    add FJContrib.cmake which sets up LCG version and location for FJContrib package

---
+++ Projects/AthGeneration/externals/FJContrib.cmake
@@ -0,0 +1,7 @@
+#
+# File specifying the location of FJContrib to use.
+#
+
+set( FJCONTRIB_LCGVERSION 1.041 )
+set( FJCONTRIB_LCGROOT
+   ${LCG_RELEASE_DIR}/fjcontrib/${FJCONTRIB_LCGVERSION}/${LCG_PLATFORM} )
