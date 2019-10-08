12436be874b925f4e86e8910b22edfa7d299c34f
Savanna Marie Shaw <savanna.marie.shaw@cern.ch>
2019-10-08 13:00:18 +0000

    Update configuration for full scan muon trigger hypo tools
    For muon triggers running full scan muon finding, we want to make sure that the full scan muon hypo considers the number/thresholds for all muons in the full chain (to avoid double counting muons that were all ready found in RoIs).
    The hypo tool configuration was updated to take the thresholds from the full chain name (instead of just the full scan part). Also added in a FS RoI building to the muon event view creator. This can be revisted/removed once the official version of the serial merging is available (the very simple version currently in use for these chains has some limitations that make this necessary in the intermediate term to avoid running the full scan algorithms multiple times based on the number of initial L1 RoIs). This is part of the mgration in ATR-17553.

--- Trigger/TrigSteer/ViewAlgs/src/EventViewCreatorAlgorithmWithMuons.cxx
+++ Trigger/TrigSteer/ViewAlgs/src/EventViewCreatorAlgorithmWithMuons.cxx
@@ -67,44 +67,67 @@ StatusCode EventViewCreatorAlgorithmWithMuons::execute( const EventContext& cont
 
     ATH_MSG_DEBUG( "Got output "<< outputHandle.key()<<" with " << outputHandle->size() << " elements" );
 
+    if(m_doFSRoI){	
+      //create one RoI over full detector
+      auto roi = new TrigRoiDescriptor(true);
+      roisWriteHandle->push_back( roi );
+      const auto roiEL = ElementLink<TrigRoiDescriptorCollection>(*roisWriteHandle, roisWriteHandle->size() - 1, context);
+
+      // make the view
+      ATH_MSG_DEBUG( "Making the View "<<name()<<"_view" );
+      auto newView = ViewHelper::makeView( name()+"_view", viewCounter++, m_viewFallThrough );
+      viewVector->push_back( newView );
+      contexts.emplace_back( context );
+      Atlas::setExtendedEventContext (contexts.back(),
+				      Atlas::ExtendedEventContext( viewVector->back(), conditionsRun, roi ) );
+      ATH_CHECK( placeRoIInView( roiEL, viewVector->back(), contexts.back() ) );
+    }
+
     // loop over output decisions in container of outputHandle, follow link to inputDecision
     for ( auto outputDecision : *outputHandle){ 
       ElementLinkVector<DecisionContainer> inputLinks = getLinkToPrevious(outputDecision);
 
-      //create one RoI per muon
+      if(m_doFSRoI){	
+	// link decision to this view
+	outputDecision->setObjectLink( "view", ElementLink< ViewContainer >(m_viewsKey.key(), viewVector->size()-1 ));//adding view to TC
+      }
+      else{
 
-      // loop over input links as predecessors
-      for (auto input: inputLinks){
-        const Decision* inputDecision = *input;
+	//create one RoI per muon
 
-        // Retrieve muons ...
-        ATH_MSG_DEBUG( "Checking there are muons linked to decision object" );
-        TrigCompositeUtils::LinkInfo< xAOD::MuonContainer > muonELInfo = TrigCompositeUtils::findLink< xAOD::MuonContainer >( inputDecision,m_muonsLink );
-        ATH_CHECK( muonELInfo.isValid() );
-        const xAOD::Muon *muon = *muonELInfo.link;
-        ATH_MSG_DEBUG( "Placing xAOD::MuonContainer " );
-        ATH_MSG_DEBUG( "   -- pt="<< muon->p4().Et() <<" eta="<< muon->eta() << " muon="<< muon->phi() );
+	// loop over input links as predecessors
+	for (auto input: inputLinks){
+	  const Decision* inputDecision = *input;
 
-        // create the RoI around muon
-        auto roi = new TrigRoiDescriptor(muon->eta(), muon->eta()-m_roiEtaWidth, muon->eta()+m_roiEtaWidth, muon->phi(), muon->phi()-m_roiPhiWidth, muon->phi()+m_roiPhiWidth);
-        roisWriteHandle->push_back( roi );
-        const auto roiEL = ElementLink<TrigRoiDescriptorCollection>(*roisWriteHandle, roisWriteHandle->size() - 1, context);
-        ATH_CHECK( roiEL.isValid() );
-        ATH_MSG_DEBUG("Created roi around muon: "<<*roi);
-        // make the view
-        ATH_MSG_DEBUG( "Making the View "<<name()<<"_view" );
-        auto newView = ViewHelper::makeView( name()+"_view", viewCounter++, m_viewFallThrough ); //pointer to the view
-        viewVector->push_back( newView );
-        contexts.emplace_back( context );
-        Atlas::setExtendedEventContext (contexts.back(),
-                                        Atlas::ExtendedEventContext( viewVector->back(), conditionsRun, roi ) );
-        // link decision to this view
-        outputDecision->setObjectLink( TrigCompositeUtils::viewString(), ElementLink< ViewContainer >(m_viewsKey.key(), viewVector->size()-1 ));//adding view to TC
-        ATH_MSG_DEBUG( "Adding new view to new decision; storing view in viewVector component " << viewVector->size()-1 );
-        ATH_CHECK( linkViewToParent( inputDecision, viewVector->back() ) );
-        ATH_CHECK( placeRoIInView( roiEL, viewVector->back(), contexts.back() ) );
-        ATH_CHECK( placeMuonInView( muon, viewVector->back(), contexts.back() ) );
-      }// loop over previous inputs
+	  // Retrieve muons ...
+	  ATH_MSG_DEBUG( "Checking there are muons linked to decision object" );
+	  TrigCompositeUtils::LinkInfo< xAOD::MuonContainer > muonELInfo = TrigCompositeUtils::findLink< xAOD::MuonContainer >( inputDecision,m_muonsLink );
+	  ATH_CHECK( muonELInfo.isValid() );
+	  const xAOD::Muon *muon = *muonELInfo.link;
+	  ATH_MSG_DEBUG( "Placing xAOD::MuonContainer " );
+	  ATH_MSG_DEBUG( "   -- pt="<< muon->p4().Et() <<" eta="<< muon->eta() << " muon="<< muon->phi() );
+
+	  // create the RoI around muon
+	  auto roi = new TrigRoiDescriptor(muon->eta(), muon->eta()-m_roiEtaWidth, muon->eta()+m_roiEtaWidth, muon->phi(), muon->phi()-m_roiPhiWidth, muon->phi()+m_roiPhiWidth);
+	  roisWriteHandle->push_back( roi );
+	  const auto roiEL = ElementLink<TrigRoiDescriptorCollection>(*roisWriteHandle, roisWriteHandle->size() - 1, context);
+	  ATH_CHECK( roiEL.isValid() );
+	  ATH_MSG_DEBUG("Created roi around muon: "<<*roi);
+	  // make the view
+	  ATH_MSG_DEBUG( "Making the View "<<name()<<"_view" );
+	  auto newView = ViewHelper::makeView( name()+"_view", viewCounter++, m_viewFallThrough ); //pointer to the view
+	  viewVector->push_back( newView );
+	  contexts.emplace_back( context );
+	  Atlas::setExtendedEventContext (contexts.back(),
+					  Atlas::ExtendedEventContext( viewVector->back(), conditionsRun, roi ) );
+	  // link decision to this view
+	  outputDecision->setObjectLink( TrigCompositeUtils::viewString(), ElementLink< ViewContainer >(m_viewsKey.key(), viewVector->size()-1 ));//adding view to TC
+	  ATH_MSG_DEBUG( "Adding new view to new decision; storing view in viewVector component " << viewVector->size()-1 );
+	  ATH_CHECK( linkViewToParent( inputDecision, viewVector->back() ) );
+	  ATH_CHECK( placeRoIInView( roiEL, viewVector->back(), contexts.back() ) );
+	  ATH_CHECK( placeMuonInView( muon, viewVector->back(), contexts.back() ) );
+	}// loop over previous inputs
+      }//Not FS view
     } // loop over decisions   
   }// loop over output keys
 
