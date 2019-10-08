6541fd2da40b246a48964bf52623c38e0e880b9d
Dan Guest <dguest@cern.ch>
2019-10-08 13:50:51 +0200

    Consistent name convention with CutBookkeeper

--- PhysicsAnalysis/AnalysisCommon/CutBookKeeperUtils/Root/OriginalAodCounts.cxx
+++ PhysicsAnalysis/AnalysisCommon/CutBookKeeperUtils/Root/OriginalAodCounts.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "CutBookKeeperUtils/OriginalAodCounts.h"
+#include "CutBookkeeperUtils/OriginalAodCounts.h"
 
 #include "xAODCutflow/CutBookkeeper.h"
 #include "xAODCutflow/CutBookkeeperContainer.h"
