a980b357b1934ca6e71bb8d228cafcedee12352a
lihan <hanliangliang_1_2@163.com>
2019-10-09 04:12:14 +0200

    temporary modification

--- ForwardDetectors/ZDC/ZDC_SD/test/ZDC_StripSD_gtest.cxx
+++ ForwardDetectors/ZDC/ZDC_SD/test/ZDC_StripSD_gtest.cxx
@@ -32,17 +32,15 @@ class GaudiEnvironment : public ::testing::Environment {
     virtual void SetUp() override {
    Athena_test::initGaudi("ZDC_SD/optionForTest.txt", g_svcLoc);
   }
+//  ISvcLocator* g_svcLoc = nullptr;
 };
 class ZDC_StripSDtest : public ::testing::Test {	
   protected:
     virtual void SetUp() override {
-//add somthing
     }
 
     virtual void TearDown() override {
-//add somthing
     }
-//add somthing
 };
 //end of environment setting
 
@@ -85,100 +83,6 @@ TEST_F( ZDC_StripSDtest, ProcessHits )
   G4String nop3 = "photon";
   DerivedG4SensitiveDetectorTestSetting(sp, totalenergydeposit, physicalname, logicalname, copyno, preStepPos, postStepPos, globaltime0, kineticenergy0, velocity0, globaltime, kineticenergy, globaltime1, kineticenergy1, velocity1, steplength, charge, encoding, antiencoding, astring, atype, nop1, nop2, nop3);
 
-/*
-//decorate sp with the variable called TotalEnergyDeposit
-  sp.SetTotalEnergyDeposit( 0.8 );
-//end
-
-//decorate sp with a G4StepPoint object
-  G4StepPoint* stepPoint = new G4StepPoint();
-  G4ThreeVector preStepPos(0, 0, 1);
-  stepPoint->SetPosition(preStepPos);
-  G4NavigationHistory* navigationHistory = new G4NavigationHistory();
-  G4String boxName = "name";
-  G4Box* box = new G4Box(boxName, 1.0, 1.0, 1.0);
-  G4NistManager* man = G4NistManager::Instance();
-  G4Material* material = man->FindOrBuildMaterial("G4_AIR");
-  G4String name = "logicalName";
-  G4LogicalVolume fLogical(box, material, name);
-  G4String name1 = "physicsTDQuarticBar[9]";
-  G4VPhysicalVolume* pPhysical = NULL;
-  G4MyPhysicalVolume physicalVolume(0, G4ThreeVector(0,0,0), name1, &fLogical, pPhysical);
-  physicalVolume.SetCopyNo(11000);
-  G4int nReplica = 2;
-  navigationHistory->SetFirstEntry(&physicalVolume);
-  navigationHistory->NewLevel(&physicalVolume, kNormal, nReplica);
-  navigationHistory->NewLevel(&physicalVolume, kNormal, nReplica);
-  navigationHistory->NewLevel(&physicalVolume, kNormal, nReplica);
-  navigationHistory->NewLevel(&physicalVolume, kNormal, nReplica);
-  navigationHistory->NewLevel(&physicalVolume, kNormal, nReplica);
-  G4TouchableHistory* touchableHistory = new G4TouchableHistory(*navigationHistory);
-  G4TouchableHandle touchableHandle(touchableHistory);
-  stepPoint->SetTouchableHandle(touchableHandle);
-  stepPoint->SetGlobalTime(0.5);
-  stepPoint->SetKineticEnergy(1.5);
-  stepPoint->SetVelocity(2500);
-
-  sp.SetPreStepPoint(stepPoint);
-//end
-
-//decorate sp with another G4StepPoint object
-  G4StepPoint* stepPoint1 = new G4StepPoint();
-  G4ThreeVector postStepPos(0, 0, 2);
-  stepPoint1->SetPosition(postStepPos);
-  stepPoint1->SetVelocity(2500);
-
-  sp.SetPostStepPoint(stepPoint1);
-//end
-
-//set step length for the step
-  sp.SetStepLength(1.0);
-//end
-
-//decorate sp with a G4Track object
-G4ParticleDefinition particle("anyon",         0.0*MeV,       0.0*MeV,         1.0, 
-		    2,              -1,            -1,          
-		    0,               0,             0,             
-	      "anyon",               0,             0,          22,
-	      true,               -1.0,          NULL,
-             false,           "anyon",          22
-	      );
-  G4ThreeVector aMomentumDirection(0,0,0);
-  G4double aKineticEnergy = 0.5;
-  G4DynamicParticle* dynamicPar = new G4DynamicParticle(&particle, aMomentumDirection, aKineticEnergy);
-  G4double aValueTime = 1;
-  G4ThreeVector ValuePosition(1.0, 1.0, 1.0);
-  G4Track* track = new G4Track(dynamicPar, aValueTime, ValuePosition);
-  G4double globalTime = 5.0;
-  track->SetGlobalTime(globalTime);
-  int trackID = 3;
-  track->SetTrackID(trackID);
-  G4String boxName1 = "name";
-  G4Box* box1 = new G4Box(boxName1, 1.0, 1.0, 1.0);
-  G4NistManager* man1 = G4NistManager::Instance();
-  G4Material* material1 = man1->FindOrBuildMaterial("G4_AIR");
-  G4String name2 = "name2";
-  G4LogicalVolume* fLogical1 = new G4LogicalVolume(box1, material1, name2);
-  track->SetLogicalVolumeAtVertex(fLogical1);
-  G4Step* stepForTrack = new G4Step();
-  G4StepPoint* stepPoint2 = new G4StepPoint();
-  G4Material* mat1 = man1->FindOrBuildMaterial("G4_AIR");
-  G4MaterialPropertiesTable* propertiesTable = new G4MaterialPropertiesTable();
-  G4double Energies[5] = {1,2,3,4,5};
-  G4double Values[5] = {1,2,3,4,5};
-  size_t VectorLength = 5;
-  G4MaterialPropertyVector* mpv = new G4MaterialPropertyVector(Energies, Values, VectorLength);
-  G4MaterialPropertyVector* mpv1 = new G4MaterialPropertyVector(Energies, Values, VectorLength);
-  propertiesTable->AddProperty("RINDEX", mpv);
-  propertiesTable->AddProperty("ABSLENGTH", mpv1);
-  mat1->SetMaterialPropertiesTable(propertiesTable);
-  stepPoint2->SetMaterial(mat1);
-  stepForTrack->SetPreStepPoint(stepPoint2);
-  track->SetStep(stepForTrack);
-
-  sp.SetTrack(track);
-//end
-*/
   ZDC_StripSD sd2("name2", "name2");
   sd2.Initialize(&hce);
   sd2.StartOfAthenaEvent();
