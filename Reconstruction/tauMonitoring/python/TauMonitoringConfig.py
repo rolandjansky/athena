f7bf6a5ee75f822a43a107b00c1e9a43a2c39e38
Harald Fox <harald.fox@cern.ch>
2019-10-10 15:29:26 +0200

    - added TauMonitoringConfig.py

---
+++ Reconstruction/tauMonitoring/python/TauMonitoringConfig.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+#
+
+def TauMonitoringConfig(flags):
+    from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+    from .DQTDataFlowMonAlg import DQTDataFlowMonAlgConfig
+    from .DQTDetSynchMonAlg import DQTDetSynchMonAlgConfig
+    
+    result = ComponentAccumulator()
+    result.merge(DQTDataFlowMonAlgConfig(flags))
+
+    # really should only configure when input is RAW
+    result.merge(DQTDetSynchMonAlgConfig(flags))
+
+    return result
