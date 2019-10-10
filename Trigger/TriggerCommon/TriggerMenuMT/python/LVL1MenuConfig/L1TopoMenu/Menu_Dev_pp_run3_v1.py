58c3bd5cb1c9ecc33a124420248bed24d4946c72
lixia <ligang.xia@cern.ch>
2019-10-10 11:20:20 +0200

    rename menus to MC/Dev_pp_run3_v1 and add L1 menus

---
+++ Trigger/TriggerCommon/TriggerMenuMT/python/LVL1MenuConfig/L1TopoMenu/Menu_Dev_pp_run3_v1.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+
+def defineMenu():
+    """
+    Defines the following LlTopoFlags:
+    
+    algos   .... list of all algo names in the menu 
+
+    """
+
+    import Menu_LS2_v1
+    Menu_LS2_v1.defineMenu()
+
