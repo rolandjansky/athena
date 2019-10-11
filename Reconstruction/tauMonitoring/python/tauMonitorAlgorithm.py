7d172554c4a7630515775d9588ece9899084cf95
Harald Fox <harald.fox@cern.ch>
2019-10-11 16:32:40 +0200

    - cleanup of .py and .cxx file

--- Reconstruction/tauMonitoring/python/tauMonitorAlgorithm.py
+++ Reconstruction/tauMonitoring/python/tauMonitorAlgorithm.py
@@ -13,11 +13,6 @@ def tauMonitoringConfig(inputFlags):
     '''Function to configures some algorithms in the monitoring system.'''
 
     ### STEP 1 ###
-    # If you need to set up special tools, etc., you will need your own ComponentAccumulator;
-    # uncomment the following 2 lines and use the last three lines of this function instead of the ones
-    # just before
-    # from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
-    # result = ComponentAccumulator()
 
     # The following class will make a sequence, configure algorithms, and link
     # them to GenericMonitoringTools
@@ -36,14 +31,6 @@ def tauMonitoringConfig(inputFlags):
     tauMonAlgCR = cfgHelper.addAlgorithm( tauMonitorAlgorithm, name='tauMonAlgCR')
     tauMonAlgEC = cfgHelper.addAlgorithm( tauMonitorAlgorithm, name='tauMonAlgEC')
 
-    # You can actually make multiple instances of the same algorithm and give 
-    # them different configurations
-    #anotherTauMonAlg = cfgHelper.addAlgorithm(tauMonitorAlgorithm,'AnotherTauMonAlg')
-
-    # # If for some really obscure reason you need to instantiate an algorithm
-    # # yourself, the AddAlgorithm method will still configure the base 
-    # # properties and add the algorithm to the monitoring sequence.
-    # cfgHelper.AddAlgorithm(myExistingAlg)
 
 
     ### STEP 3 ###
@@ -68,10 +55,6 @@ def tauMonitoringConfig(inputFlags):
     # Add some tools. N.B. Do not use your own trigger decion tool. Use the
     # standard one that is included with AthMonitorAlgorithm.
 
-    # # Then, add a tool that doesn't have its own configuration function. In
-    # # this example, no accumulator is returned, so no merge is necessary.
-    # from MyDomainPackage.MyDomainPackageConf import MyDomainTool
-    # exampleMonAlg.MyDomainTool = MyDomainTool()
 
     # Add a generic monitoring tool (a "group" in old language). The returned 
     # object here is the standard GenericMonitoringTool.
@@ -85,16 +68,6 @@ def tauMonitoringConfig(inputFlags):
 
     ### STEP 5 ###
     # Configure histograms
-    # myKinGroupBA.defineHistogram('lumiPerBCID',title='Luminosity,WithCommaInTitle;L/BCID;Events',
-    #                         path='ToRuleThemAll',xbins=40,xmin=0.0,xmax=80.0)
-    # myKinGroupBA.defineHistogram('lb', title='Luminosity Block;lb;Events',
-    #                         path='ToFindThem',xbins=1000,xmin=-0.5,xmax=999.5,weight='testweight')
-    # myKinGroupBA.defineHistogram('random', title='LB;x;Events',
-    #                         path='ToBringThemAll',xbins=30,xmin=0,xmax=1,opt='kLBNHistoryDepth=10')
-    # myKinGroupBA.defineHistogram('random', title='title;x;y',path='ToBringThemAll',
-    #                         xbins=[0,.1,.2,.4,.8,1.6])
-    #myKinGroupBA.defineHistogram('random,pT', type='TH2F', title='title;x;y',path='ToBringThemAll',
-    #                             xbins=[0,.1,.2,.4,.8,1.6],ybins=[0,10,30,40,60,70,90])
     
     for itup in [(myKinGroupBA,'BA'),(myKinGroupCR,'CR'),(myKinGroupEC,'EC')]:
         (igroup, postfix) = itup
@@ -106,44 +79,13 @@ def tauMonitoringConfig(inputFlags):
                                xbins=30,xmin=-3.0,xmax=3.0,ybins=32,ymin=-3.15,ymax=3.15)
 
 
-    # myKinGroupBA.defineHistogram('pT_passed,pT',type='TEfficiency',title='Test TEfficiency;x;Eff',
-    #                         path='AndInTheDarkness',xbins=100,xmin=0.0,xmax=50.0)
-
-    # anotherGroup.defineHistogram('lbWithFilter',title='Lumi;lb;Events',
-    #                             path='top',xbins=1000,xmin=-0.5,xmax=999.5)
-    # anotherGroup.defineHistogram('run',title='Run Number;run;Events',
-    #                             path='top',xbins=1000000,xmin=-0.5,xmax=999999.5)
-
-    # Example defining an array of histograms. This is useful if one seeks to create a
-    # number of histograms in an organized manner. (For instance, one plot for each ASIC
-    # in the subdetector, and these components are mapped in eta, phi, and layer.) Thus,
-    # one might have an array of TH1's such as quantity[etaIndex][phiIndex][layerIndex].
-    #for alg in [exampleMonAlg,anotherTauMonAlg]:
-    #    # Using an array of groups
-    #    array = cfgHelper.addArray([2],alg,'tauMonitor')
-    #    array.defineHistogram('a,b',title='AB',type='TH2F',path='Eta',
-    #                          xbins=10,xmin=0.0,xmax=10.0,
-    #                          ybins=10,ymin=0.0,ymax=10.0)
-    #    array.defineHistogram('c',title='C',path='Eta',
-    #                          xbins=10,xmin=0.0,xmax=10.0)
-    #    array = cfgHelper.addArray([4,2],alg,'tauMonitor')
-    #    array.defineHistogram('a',title='A',path='EtaPhi',
-    #                          xbins=10,xmin=0.0,xmax=10.0)
-    #    # Using a map of groups
-    #    layerList = ['layer1','layer2']
-    #    clusterList = ['clusterX','clusterB']
-    #    array = cfgHelper.addArray([layerList],alg,'tauMonitor')
-    #    array.defineHistogram('c',title='C',path='Layer',
-    #                          xbins=10,xmin=0,xmax=10.0)
-    #    array = cfgHelper.addArray([layerList,clusterList],alg,'tauMonitor')
-    #    array.defineHistogram('c',title='C',path='LayerCluster',
-    #                          xbins=10,xmin=0,xmax=10.0)
 
     ### STEP 6 ###
     # Finalize. The return value should be a tuple of the ComponentAccumulator
     # and the sequence containing the created algorithms. If we haven't called
     # any configuration other than the AthMonitorCfgHelper here, then we can 
     # just return directly (and not create "result" above)
+
     return cfgHelper.result()
     
     # # Otherwise, merge with result object and return
--- Reconstruction/tauMonitoring/src/tauMonitorAlgorithm.cxx
+++ Reconstruction/tauMonitoring/src/tauMonitorAlgorithm.cxx
@@ -23,13 +23,6 @@ tauMonitorAlgorithm::~tauMonitorAlgorithm() {}
 
 StatusCode tauMonitorAlgorithm::initialize() {
     using namespace Monitored;
-    // m_abGroups1 = buildToolMap<int>(m_tools,"tauMonitor",2);
-    // m_abGroups2 = buildToolMap<std::vector<int>>(m_tools,"tauMonitor",4,2);
-    // 
-    // std::vector<std::string> layers = {"layer1","layer2"};
-    // std::vector<std::string> clusters = {"clusterX","clusterB"};
-    // m_cGroups1 = buildToolMap<int>(m_tools,"tauMonitor",layers);
-    // m_cGroups2 = buildToolMap<std::map<std::string,int>>(m_tools,"tauMonitor",layers,clusters);
 
     ATH_CHECK( m_TauContainerKey.initialize() );
 
@@ -40,72 +33,11 @@ StatusCode tauMonitorAlgorithm::initialize() {
 StatusCode tauMonitorAlgorithm::fillHistograms( const EventContext& ctx ) const {
     using namespace Monitored;
 
-    // // Declare the quantities which should be monitored
-    // auto lumiPerBCID = Monitored::Scalar<float>("lumiPerBCID",0.0);
-    // auto lb = Monitored::Scalar<int>("lb",0);
-    // auto run = Monitored::Scalar<int>("run",0);
-    // auto random = Monitored::Scalar<float>("random",0.0);
-    // auto testweight = Monitored::Scalar<float>("testweight",1.0);
-    // 
-    // // Two variables (value and passed) needed for TEfficiency
-    // auto pT = Monitored::Scalar<float>("pT",0.0);
-    // auto pT_passed = Monitored::Scalar<bool>("pT_passed",false);
 
     auto Eflow = Monitored::Scalar<float>("Eflow",-1.);
     auto eta = Monitored::Scalar<float>("eta",-4.0);
     auto phi = Monitored::Scalar<float>("phi",-1.0);
 
-    // // Set the values of the monitored variables for the event
-    // lumiPerBCID = lbAverageInteractionsPerCrossing(ctx);
-    // lb = GetEventInfo(ctx)->lumiBlock();
-    // run = GetEventInfo(ctx)->runNumber();
-    // testweight = 2.0;
-    
-    // TRandom3 r(ctx.eventID().event_number());
-    // // Example of using flags
-    // if (m_doRandom) {
-    //     random = r.Rndm();
-    // }
-    // 
-    // // Fake efficiency calculator
-    // pT = r.Landau(15);
-    // pT_passed = pT>r.Poisson(15);
-
-    // Fill. First argument is the tool name, all others are the variables to be saved.
-    // fill("myTauKinGroupBA",lumiPerBCID,lb,random,pT,pT_passed,testweight);
-
-    // Alternative fill method. Get the group yourself, and pass it to the fill function.
-    //auto tool = getGroup("myTauKinGroupBA");
-    //fill(tool,run);
-
-    // Fill with a vector; useful in some circumstances.
-    //std::vector<std::reference_wrapper<Monitored::IMonitoredVariable>> varVec = {lumiPerBCID,pT};
-    //fill("myTauKinGroupBA",varVec);
-    //fill(tool,varVec);
-
-    // Filling using a pre-defined array of groups.
-    // auto a = Scalar<float>("a",0.0);
-    // auto b = Scalar<float>("b",1.0);
-    // auto c = Scalar<float>("c",2.0);
-    // for ( auto iEta : {0,1} ) {
-    //     // 1) Valid but inefficient fill
-    //     fill("myTauKinGroupBA_"+std::to_string(iEta),a,b,c);
-    //     // 2) Faster way to fill a vector of histograms
-    //     fill(m_tools[m_abGroups1[iEta]],a,b,c);
-    //     for ( auto iPhi : {0,1} ) {
-    //         // Same efficient method for 2D array
-    //         fill(m_tools[m_abGroups2[iEta][iPhi]],a,b);
-    //     }
-    // }
-    // 
-    // // Filling using a pre-defined map of groups.
-    // for ( auto& layer : std::vector<std::string>({"layer1","layer2"}) ) {
-    //     fill(m_tools[m_cGroups1.at(layer)],c);
-    //     for ( auto& cluster : std::vector<std::string>({"clusterX","clusterB"}) ) {
-    //         // Same efficient method for 2D map
-    //         fill(m_tools[m_cGroups2.at(layer).at(cluster)],c);
-    //     }
-    // }
 
     SG::ReadHandle<xAOD::TauJetContainer> taus(m_TauContainerKey, ctx);
     if (! taus.isValid() ) {
