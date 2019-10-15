ebf1d9060307edd3054f5e08822987948089ab87
sakatsuk <shunichi.akatsuka@cern.ch>
2019-10-16 00:40:27 +0900

    adding missing line to src/components/IsolationSelection_entries.cxx

--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/src/components/IsolationSelection_entries.cxx
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/src/components/IsolationSelection_entries.cxx
@@ -8,6 +8,7 @@
 #include "IsolationSelection/IsolationCloseByCorrectionTool.h"
 #include "../TestIsolationCloseByCorrAthenaAlg.h"
 
+#include "IsolationSelection/IsolationCloseByCorrectionTool.h"
 using namespace xAOD;
 
 DECLARE_NAMESPACE_TOOL_FACTORY(CP, IsolationSelectionTool)
