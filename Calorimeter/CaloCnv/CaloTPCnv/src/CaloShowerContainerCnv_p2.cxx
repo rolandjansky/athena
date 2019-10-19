99de08201411292668a05591a50d0bc98ff002e2
scott snyder <sss@karma>
2019-10-19 16:35:18 +0200

    CaloTPCnv: Clean up converters.
    Make converter interfaces const.
    Prepare cell container converter for MT thinning.
    Remove uses of #define private.
    Other cleanups.

--- Calorimeter/CaloCnv/CaloTPCnv/src/CaloShowerContainerCnv_p2.cxx
+++ Calorimeter/CaloCnv/CaloTPCnv/src/CaloShowerContainerCnv_p2.cxx
@@ -1,26 +1,29 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "CaloTPCnv/CaloShowerContainerCnv_p2.h"
 
 #include "CaloTPCnv/CaloShowerContainer_p2.h"
-#define private public
 #include "CaloEvent/CaloShowerContainer.h"
-#undef private
 #include "AthenaKernel/errorcheck.h"
 
 CaloShowerContainerCnv_p2::CaloShowerContainerCnv_p2()
 {
 }
 
-void CaloShowerContainerCnv_p2::persToTrans(const CaloShowerContainer_p2* pers, CaloShowerContainer* trans,
-					    MsgStream&)
+void CaloShowerContainerCnv_p2::persToTrans(const CaloShowerContainer_p2* pers,
+                                            CaloShowerContainer* trans,
+					    MsgStream&) const
 {
+  CaloClusterMomentContainer_p1::const_iterator momentStoreIterator;
+  CaloSamplingDataContainerCnv_p1::State samplingState;
   if (! m_momentContainerCnv.setIterator(&pers->m_momentContainer,
-                                         pers->m_nClusters) ||
-      ! m_samplingContainerCnv.setIterator(&pers->m_samplingDataContainer,
-                                           pers->m_nClusters))
+                                         pers->m_nClusters,
+                                         momentStoreIterator) ||
+      ! m_samplingContainerCnv.setState(&pers->m_samplingDataContainer,
+                                        pers->m_nClusters,
+                                        samplingState))
   {
     REPORT_MESSAGE_WITH_CONTEXT(MSG::WARNING, "CaloShowerContainerCnv_p2")
       << "Not converting CaloShowerContainer.";
@@ -32,14 +35,18 @@ void CaloShowerContainerCnv_p2::persToTrans(const CaloShowerContainer_p2* pers, 
   for (unsigned i=0;i<pers->m_nClusters;++i) {
     CaloShower* shower=new CaloShower();
     //trans->at(i)=shower;
-    m_momentContainerCnv.persToTrans(&(pers->m_momentContainer),&(shower->m_momentStore));
-    m_samplingContainerCnv.persToTrans(&(pers->m_samplingDataContainer),&(shower->m_samplingStore));
+    m_momentContainerCnv.persToTrans(&(pers->m_momentContainer),&(shower->getMomentStore()),
+                                     momentStoreIterator);
+    m_samplingContainerCnv.persToTrans(&(pers->m_samplingDataContainer),&(shower->getSamplingStore()),
+                                       samplingState);
     trans->push_back(shower);
   }
 }
 
-void CaloShowerContainerCnv_p2::transToPers(const CaloShowerContainer* trans, CaloShowerContainer_p2* pers, MsgStream& log) { 
-					    
+void CaloShowerContainerCnv_p2::transToPers(const CaloShowerContainer* trans,
+                                            CaloShowerContainer_p2* pers,
+                                            MsgStream& log) const
+{ 
   pers->m_nClusters=trans->size();
   CaloShowerContainer::const_iterator it=trans->begin();
   CaloShowerContainer::const_iterator it_e=trans->end();
@@ -48,12 +55,12 @@ void CaloShowerContainerCnv_p2::transToPers(const CaloShowerContainer* trans, Ca
     //(once for the entire container)
     //pers->m_samplingDataContainer.m_varTypePattern=(*it)->m_samplingStore.m_varTypePattern;
     //pers->m_samplingDataContainer.m_samplingPattern=(*it)->m_samplingStore.m_samplingPattern; *** TO BE SOLVED !!!!!
-    pers->m_momentContainer.m_nMoments=(*it)->m_momentStore.size();
+    pers->m_momentContainer.m_nMoments=(*it)->getMomentStore().size();
     
   }
   for(;it!=it_e;++it) {
-    m_momentContainerCnv.transToPers(&((*it)->m_momentStore),&(pers->m_momentContainer));
-    m_samplingContainerCnv.transToPers(&((*it)->m_samplingStore),&(pers->m_samplingDataContainer),log);
+    m_momentContainerCnv.transToPers(&((*it)->getMomentStore()),&(pers->m_momentContainer));
+    m_samplingContainerCnv.transToPers(&((*it)->getSamplingStore()),&(pers->m_samplingDataContainer),log);
   }
 }
 
