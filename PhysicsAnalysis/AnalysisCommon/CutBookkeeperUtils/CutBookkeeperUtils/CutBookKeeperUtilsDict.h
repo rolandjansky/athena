7cbcb66e3adeac9de3dd4a790b7b4f37fa051119
Dan Guest <dguest@cern.ch>
2019-10-21 20:17:18 +0200

    add a bunch of ASG wrapper crap

---
+++ PhysicsAnalysis/AnalysisCommon/CutBookkeeperUtils/CutBookkeeperUtils/CutBookKeeperUtilsDict.h
@@ -0,0 +1,11 @@
+/*
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+*/
+
+
+#ifndef CBCU_DICT_H
+#define CBCU_DICT_H
+
+#include <CutBookkeeperUtils/OriginalAODCountsTool.h>
+
+#endif
