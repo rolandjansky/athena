2dc9306e9755c37cec80823197983e4d92a96bb3
Tomas Dado <tomas.dado@cern.ch>
2019-10-22 09:49:42 +0200

    Fixing TopConfiguration

--- PhysicsAnalysis/TopPhys/xAOD/TopConfiguration/TopConfiguration/AodMetaDataAccess.h
+++ PhysicsAnalysis/TopPhys/xAOD/TopConfiguration/TopConfiguration/AodMetaDataAccess.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
+   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+ */
 
 #ifndef TOPCONFIGURATION_AODMETADATAACCESS_H
 #define TOPCONFIGURATION_AODMETADATAACCESS_H
@@ -9,63 +9,55 @@
 #include <string>
 
 namespace top {
+  class AodMetaDataAccess {
+  public:
+    AodMetaDataAccess();
+    virtual ~AodMetaDataAccess();
+    virtual void loadWithFilesFrom(std::string const& fileListPath);
 
-class AodMetaDataAccess {
+    /**
+     * @brief retrieve an AOD meta-data value
+     *
+     * @param folderName the folder name, e.g. "/TagInfo"
+     * @param keyName the key name, e.g. "project_name"
+     *
+     * @return the meta-data value as a string
+     */
+    std::string get(std::string const& folderName, std::string const& keyName) const;
 
-public:
+    /**
+     * @brief retrieve an AOD meta-data value
+     *
+     * @param folderName the folder name, e.g. "/TagInfo"
+     * @param keyName the key name, e.g. "project_name"
+     *
+     * @return the meta-data value as a string, or @p defaultValue if the meta-data entry does not exist
+     */
+    std::string get(std::string const& folderName, std::string const& keyName, std::string const& defaultValue) const;
 
-   AodMetaDataAccess();
-   virtual ~AodMetaDataAccess();
-   virtual void loadWithFilesFrom(std::string const & fileListPath);
+    /**
+     * @brief check whether AOD meta-data has been loaded
+     *
+     * @return whether the AOD meta-data has been loaded
+     */
+    bool valid() const;
 
-   /**
-    * @brief retrieve an AOD meta-data value
-    * 
-    * @param folderName the folder name, e.g. "/TagInfo"
-    * @param keyName the key name, e.g. "project_name"
-    *
-    * @return the meta-data value as a string
-    */
-   std::string get(std::string const & folderName, std::string const & keyName) const;
+    /**
+     * @brief check whether the data are simulated or real
+     *
+     * @return true for MC, false for real data
+     */
+    bool isSimulation() const;
 
-   /**
-    * @brief retrieve an AOD meta-data value
-    * 
-    * @param folderName the folder name, e.g. "/TagInfo"
-    * @param keyName the key name, e.g. "project_name"
-    *
-    * @return the meta-data value as a string, or @p defaultValue if the meta-data entry does not exist
-    */
-   std::string get(std::string const & folderName, std::string const & keyName, std::string const & defaultValue) const;
-
-   /**
-    * @brief check whether AOD meta-data has been loaded
-    *
-    * @return whether the AOD meta-data has been loaded
-    */
-   bool valid() const;
-
-   /**
-    * @brief check whether the data are simulated or real
-    *
-    * @return true for MC, false for real data
-    */
-   bool isSimulation() const;
-
-   /**
-    * @brief check whether this sample is simulation, and then check if it was simulated with ATLFASTII
-    *
-    * @return true for AFII, false otherwise (FS or Data)
-    */
-   bool isAFII() const;
-
-
-private:
-
-   std::map<std::string, std::map< std::string, std::string > > * m_data;
-
-};
-
+    /**
+     * @brief check whether this sample is simulation, and then check if it was simulated with ATLFASTII
+     *
+     * @return true for AFII, false otherwise (FS or Data)
+     */
+    bool isAFII() const;
+  private:
+    std::map<std::string, std::map< std::string, std::string > >* m_data;
+  };
 }
 
 #endif
