4e2ed73c45e5b9516788abd44c9b988db4ed8233
Tomas Dado <tomas.dado@cern.ch>
2019-10-22 09:52:25 +0200

    Fixing TopEventReconstructionTools

--- PhysicsAnalysis/TopPhys/xAOD/TopEventReconstructionTools/util/topreco_test_mt2.cxx
+++ PhysicsAnalysis/TopPhys/xAOD/TopEventReconstructionTools/util/topreco_test_mt2.cxx
@@ -1,6 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
+   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+ */
 
 #include <iostream>
 
@@ -14,22 +14,21 @@
 #endif
 
 int main() {
-
 #ifndef ROOTCORE_PACKAGE_Oxbridgekinetics
-    std::cout << "Oxbridgekinetics library not found when compiling" << std::endl;
+  std::cout << "Oxbridgekinetics library not found when compiling" << std::endl;
 #else
-    std::cout << "Starting top mt2 test\n";
-    Mt2::LorentzTransverseVector a;
-    Mt2::LorentzTransverseVector b;
-    Mt2::TwoVector c;
-    double d = 0.;
+  std::cout << "Starting top mt2 test\n";
+  Mt2::LorentzTransverseVector a;
+  Mt2::LorentzTransverseVector b;
+  Mt2::TwoVector c;
+  double d = 0.;
 
-    Mt2::Basic_Mt2_332_Calculator mt2Calculator;
-    const double mt2  = mt2Calculator.mt2_332( a, b, c, d);
+  Mt2::Basic_Mt2_332_Calculator mt2Calculator;
+  const double mt2 = mt2Calculator.mt2_332(a, b, c, d);
 
-    std::cout << "mt2 " << mt2 << std::endl;
-    std::cout << "Ending\n";
+  std::cout << "mt2 " << mt2 << std::endl;
+  std::cout << "Ending\n";
 #endif
 
-    return 0;
+  return 0;
 }
