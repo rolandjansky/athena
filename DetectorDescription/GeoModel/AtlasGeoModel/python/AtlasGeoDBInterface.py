47976bfc41d0dbb4b2f5413c5bd20751ed3ee550
Nicolas Köhler <nicolas.koehler@cern.ch>
2019-10-23 16:28:27 +0200

    fix python print issue

--- DetectorDescription/GeoModel/AtlasGeoModel/python/AtlasGeoDBInterface.py
+++ DetectorDescription/GeoModel/AtlasGeoModel/python/AtlasGeoDBInterface.py
@@ -126,7 +126,7 @@ class AtlasGeoDBInterface:
 
         # Check if the geometry tag was found in the DB
         if self.dbGeoTagId=="":
-            Logging.log.error("The geometry tag ",self.dbGeoTag," could not be found in the database.")
+            Logging.log.error("The geometry tag "+self.dbGeoTag+" could not be found in the database.")
             Logging.log.error("Its name might be misspelled and/or the script might access a local DB that is not up to date.")
             import sys
             sys.exit();
