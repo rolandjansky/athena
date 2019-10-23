10f44055c6d5510f5e200c3b4dcd3bebd075b1b2
Tadej Novak <tadej.novak@cern.ch>
2019-10-23 11:55:45 +0000

    Add a new PMGDecayProductsSelectionTool to PMGTools

--- PhysicsAnalysis/AnalysisCommon/PMGTools/src/components/PMGTools_entries.cxx
+++ PhysicsAnalysis/AnalysisCommon/PMGTools/src/components/PMGTools_entries.cxx
@@ -2,6 +2,7 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "PMGTools/PMGCrossSectionTool.h"
+#include "PMGTools/PMGDecayProductsSelectionTool.h"
 #include "PMGTools/PMGSherpa22VJetsWeightTool.h"
 #include "PMGTools/PMGSherpaVjetsSysTool.h"
 #include "PMGTools/PMGTruthWeightTool.h"
@@ -9,14 +10,16 @@
 using namespace PMGTools;
 
 DECLARE_TOOL_FACTORY( PMGCrossSectionTool )
+DECLARE_TOOL_FACTORY( PMGDecayProductsSelectionTool )
 DECLARE_TOOL_FACTORY( PMGSherpa22VJetsWeightTool )
 DECLARE_TOOL_FACTORY( PMGSherpaVjetsSysTool )
 DECLARE_TOOL_FACTORY( PMGTruthWeightTool )
 
 DECLARE_FACTORY_ENTRIES( PMGTools )
 {
+  DECLARE_TOOL( PMGCrossSectionTool );
+  DECLARE_TOOL( PMGDecayProductsSelectionTool )
   DECLARE_TOOL( PMGSherpa22VJetsWeightTool );
   DECLARE_TOOL( PMGSherpaVjetsSysTool );
-  DECLARE_TOOL( PMGCrossSectionTool );
   DECLARE_TOOL( PMGTruthWeightTool );
 }
