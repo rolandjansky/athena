1bf3f142fab14d84918e1a3b93a60fd6ecd2bd70
Peter Onyisi <ponyisi@utexas.edu>
2019-10-24 16:18:12 +0200

    Fix flake8 warnings, import config from SCT (should refactor at some point ... but in any case this should be taken care of by ID configuration)

--- InnerDetector/InDetMonitoring/PixelMonitoring/python/PixelMonitoringConfig.py
+++ InnerDetector/InDetMonitoring/PixelMonitoring/python/PixelMonitoringConfig.py
@@ -43,32 +43,126 @@ def PixelMonitoringConfig(flags):
         }
         
         from AthenaMonitoring import AthMonitorCfgHelper
-        helper = AthMonitorCfgHelper(DQMonFlags, "NewPixelMonitoring")
+        helper = AthMonitorCfgHelper(flags, "NewPixelMonitoring")
 
-        pixelAthMonAlgHitMonTool = helper.addAlgorithm(PixelAthHitMonTool, 'PixelAthHitMonTool', 'PixelAthHitMonTool')
+        from PixelMonitoring.PixelMonitoringConf import PixelAthHitMonTool
+        from PixelMonitoring.PixelAthHitMonToolCfg import PixelAthHitMonToolCfg        
+        pixelAthHitMonAlg = helper.addAlgorithm(PixelAthHitMonTool, 'PixelAthHitMonAlg')
         for k, v in kwargsHitMonTool.items():
-            setattr(pixelAthMonAlgHitMonTool, k, v)
-            PixelAthHitMonToolCfg(helper, pixelAthMonAlgHitMonTool, **kwargsHitMonTool)
+            setattr(pixelAthHitMonAlg, k, v)
+        PixelAthHitMonToolCfg(helper, pixelAthHitMonAlg, **kwargsHitMonTool)
 
- 
-        pixelAthClusterMonTool = helper.addAlgorithm(PixelAthClusterMonTool, 'PixelAthClusterMonTool', 'PixelAthClusterMonTool')
+        from PixelMonitoring.PixelMonitoringConf import PixelAthClusterMonTool
+        from PixelMonitoring.PixelAthClusterMonToolCfg import PixelAthClusterMonToolCfg 
+        pixelAthClusterMonAlg = helper.addAlgorithm(PixelAthClusterMonTool, 'PixelAthClusterMonAlg')
         for k, v in kwargsClusMonTool.items():
-            setattr(pixelAthClusterMonTool, k, v)
-        pixelAthClusterMonTool.TrackSelectionTool.UseTrkTrackTools = True
-        pixelAthClusterMonTool.TrackSelectionTool.CutLevel         = "TightPrimary"
-        pixelAthClusterMonTool.TrackSelectionTool.maxNPixelHoles   = 1
-        pixelAthClusterMonTool.TrackSelectionTool.maxD0            = 2
-        pixelAthClusterMonTool.TrackSelectionTool.maxZ0            = 150
-        pixelAthClusterMonTool.TrackSelectionTool.TrackSummaryTool = InDetTrackSummaryTool
-        pixelAthClusterMonTool.TrackSelectionTool.Extrapolator     = InDetExtrapolator
+            setattr(pixelAthClusterMonAlg, k, v)
+        pixelAthClusterMonAlg.TrackSelectionTool.UseTrkTrackTools = True
+        pixelAthClusterMonAlg.TrackSelectionTool.CutLevel         = "TightPrimary"
+        pixelAthClusterMonAlg.TrackSelectionTool.maxNPixelHoles   = 1
+        pixelAthClusterMonAlg.TrackSelectionTool.maxD0            = 2
+        pixelAthClusterMonAlg.TrackSelectionTool.maxZ0            = 150
+
+        # Run 3 configs - stolen from SCT
+        # Taken from InnerDetector/InDetDigitization/PixelDigitization/python/PixelDigitizationConfigNew.py
+        from PixelConditionsTools.PixelConditionsSummaryConfig import PixelConditionsSummaryCfg
+        InDetPixelConditionsSummaryTool = acc.popToolsAndMerge(PixelConditionsSummaryCfg(flags))
+        # Taken from Tracking/TrkExtrapolation/TrkExTools/python/AtlasExtrapolatorConfig.py
+        # AtlasExtrapolatorConfig can give only private extrapolator. We need public extrapolator.
+        from TrkDetDescrSvc.AtlasTrackingGeometrySvcConfig import TrackingGeometrySvcCfg
+        trackGeomCfg = TrackingGeometrySvcCfg(flags)
+        geom_svc = trackGeomCfg.getPrimary() 
+        geom_svc.GeometryBuilder.Compactify = False ######## To avoid crash ########
+        acc.merge(trackGeomCfg)
+        from MagFieldServices.MagFieldServicesConfig import MagneticFieldSvcCfg
+        acc.merge(MagneticFieldSvcCfg(flags))
+        from TrkExTools.TrkExToolsConf import Trk__Navigator
+        AtlasNavigator = Trk__Navigator(name = 'AtlasNavigator')
+        AtlasNavigator.TrackingGeometrySvc = geom_svc
+        acc.addPublicTool(AtlasNavigator) 
+        # Taken from InnerDetector/InDetExample/InDetRecExample/share/InDetRecLoadTools.py
+        from TrkExRungeKuttaPropagator.TrkExRungeKuttaPropagatorConf import Trk__RungeKuttaPropagator as Propagator
+        InDetPropagator = Propagator(name = 'InDetPropagator')
+        InDetPropagator.AccuracyParameter = 0.0001
+        InDetPropagator.MaxStraightLineStep = .004
+        acc.addPublicTool(InDetPropagator)
+        from TrkExTools.TrkExToolsConf import Trk__MaterialEffectsUpdator
+        InDetMaterialUpdator = Trk__MaterialEffectsUpdator(name = "InDetMaterialEffectsUpdator")
+        acc.addPublicTool(InDetMaterialUpdator)
+        InDetSubPropagators = []
+        InDetSubUpdators    = []
+        # -------------------- set it depending on the geometry ----------------------------------------------------
+        # default for ID is (Rk,Mat)
+        InDetSubPropagators += [ InDetPropagator.name() ]
+        InDetSubUpdators    += [ InDetMaterialUpdator.name() ]
+        # default for Calo is (Rk,MatLandau)
+        InDetSubPropagators += [ InDetPropagator.name() ]
+        InDetSubUpdators    += [ InDetMaterialUpdator.name() ]
+        # default for MS is (STEP,Mat)
+        #InDetSubPropagators += [ InDetStepPropagator.name() ]
+        InDetSubUpdators    += [ InDetMaterialUpdator.name() ]
+        from TrkExTools.TrkExToolsConf import Trk__Extrapolator
+        InDetExtrapolator = Trk__Extrapolator(name                    = 'InDetExtrapolator',
+                                              Propagators             = [ InDetPropagator ],
+                                              MaterialEffectsUpdators = [ InDetMaterialUpdator ],
+                                              Navigator               = AtlasNavigator,
+                                              SubPropagators          = InDetSubPropagators,
+                                              SubMEUpdators           = InDetSubUpdators)
+        acc.addPublicTool(InDetExtrapolator)
+        from InDetTestPixelLayer.InDetTestPixelLayerConf import InDet__InDetTestPixelLayerTool
+        InDetTestPixelLayerTool = InDet__InDetTestPixelLayerTool(name = "InDetTestPixelLayerTool",
+                                                                 PixelSummaryTool = InDetPixelConditionsSummaryTool,
+                                                                 CheckActiveAreas=True,
+                                                                 CheckDeadRegions=True,
+                                                                 CheckDisabledFEs=True)
+        acc.addPublicTool(InDetTestPixelLayerTool)
+        from InDetTrackHoleSearch.InDetTrackHoleSearchConf import InDet__InDetTrackHoleSearchTool
+        InDetHoleSearchTool = InDet__InDetTrackHoleSearchTool(name = "InDetHoleSearchTool",
+                                                              Extrapolator = InDetExtrapolator,
+                                                              PixelSummaryTool = InDetPixelConditionsSummaryTool,
+                                                              usePixel      = flags.Detector.GeometryPixel,
+                                                              useSCT        = flags.Detector.GeometrySCT,
+                                                              CountDeadModulesAfterLastHit = True,
+                                                              PixelLayerTool = InDetTestPixelLayerTool)
+        acc.addPublicTool(InDetHoleSearchTool)
+        from InDetAssociationTools.InDetAssociationToolsConf import InDet__InDetPRD_AssociationToolGangedPixels
+        InDetPrdAssociationTool = InDet__InDetPRD_AssociationToolGangedPixels(name                           = "InDetPrdAssociationTool",
+                                                                              PixelClusterAmbiguitiesMapName = "PixelClusterAmbiguitiesMap",
+                                                                              SetupCorrect                   = True,
+                                                                              addTRToutliers                 = True)
+        acc.addPublicTool(InDetPrdAssociationTool)
+        from InDetTrackSummaryHelperTool.InDetTrackSummaryHelperToolConf import InDet__InDetTrackSummaryHelperTool
+        InDetTrackSummaryHelperTool = InDet__InDetTrackSummaryHelperTool(name            = "InDetSummaryHelper",
+                                                                         AssoTool        = InDetPrdAssociationTool,
+                                                                         PixelToTPIDTool = None,
+                                                                         TestBLayerTool  = None,
+                                                                         RunningTIDE_Ambi = True,
+                                                                         DoSharedHits    = False,
+                                                                         HoleSearch      = InDetHoleSearchTool,
+                                                                         usePixel        = flags.Detector.GeometryPixel,
+                                                                         useSCT          = flags.Detector.GeometrySCT,
+                                                                         useTRT          = flags.Detector.GeometryTRT)
+        from TrkTrackSummaryTool.TrkTrackSummaryToolConf import Trk__TrackSummaryTool
+        InDetTrackSummaryTool = Trk__TrackSummaryTool(name = "InDetTrackSummaryTool",
+                                                      InDetSummaryHelperTool = InDetTrackSummaryHelperTool,
+                                                      doSharedHits           = False,
+                                                      doHolesInDet           = True,
+                                                      TRT_ElectronPidTool    = None,
+                                                      TRT_ToT_dEdxTool       = None,
+                                                      PixelToTPIDTool        = None)
+        ############################## WORKAROUND (END) ############################
+        pixelAthClusterMonAlg.TrackSelectionTool.TrackSummaryTool = InDetTrackSummaryTool
+        pixelAthClusterMonAlg.TrackSelectionTool.Extrapolator     = InDetExtrapolator
         
-        #print getattr(pixelAthClusterMonTool, 'onTrack') 
-        PixelAthClusterMonToolCfg(helper, pixelAthClusterMonTool, **kwargsClusMonTool)
- 
-        pixelAthMonAlgErrorMonTool = helper.addAlgorithm(PixelAthErrorMonTool, 'PixelAthErrorMonTool', 'PixelAthErrorMonTool')
+        
+        PixelAthClusterMonToolCfg(helper, pixelAthClusterMonAlg, **kwargsClusMonTool)
+
+        from PixelMonitoring.PixelMonitoringConf import PixelAthErrorMonTool
+        from PixelMonitoring.PixelAthErrorMonToolCfg import PixelAthErrorMonToolCfg
+        pixelAthMonAlgErrorMonAlg = helper.addAlgorithm(PixelAthErrorMonTool, 'PixelAthErrorMonAlg')
         for k, v in kwargsErrMonTool.items():
-            setattr(pixelAthMonAlgErrorMonTool, k, v)
-        PixelAthErrorMonToolCfg(helper, pixelAthMonAlgErrorMonTool, **kwargsErrMonTool)
+            setattr(pixelAthMonAlgErrorMonAlg, k, v)
+        PixelAthErrorMonToolCfg(helper, pixelAthMonAlgErrorMonAlg, **kwargsErrMonTool)
         acc.merge(helper.result())
 
     return acc
