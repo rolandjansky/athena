1b23c6ea98937b99593fcc0d8ab6f23a2fd4153b
Laurynas Mince <laurynas.mince@cern.ch>
2019-10-24 13:44:08 +0100

    Removed MS track and segment variables from MuonCreatorAlg

--- Reconstruction/MuonIdentification/MuonCombinedAlgs/python/MuonCombinedAlgsMonitoring.py
+++ Reconstruction/MuonIdentification/MuonCombinedAlgs/python/MuonCombinedAlgsMonitoring.py
@@ -16,15 +16,7 @@ class MuonCreatorAlgMonitoring(GenericMonitoringTool):
         self.defineHistogram( "satrks_eta", type="TH1F", path="EXPERT", title="Extrapolated Trk eta", xbins=50, xmin=-5, xmax=5)
         self.defineHistogram( "satrks_phi", type="TH1F", path="EXPERT", title="Extrapolated Trk phi", xbins=50, xmin=-5, xmax=5)
 
-        self.defineHistogram( "mstrks_n", type="TH1F", path="EXPERT", title="MS-only Extrapolated Trk n", xbins=50, xmin=0, xmax=50)
-        # self.defineHistogram( "mstrks_pt", type="TH1F", path="EXPERT", title="MS-only Extrapolated Trk pT", xbins=100, xmin=0, xmax=300)
-        # self.defineHistogram( "mstrks_eta", type="TH1F", path="EXPERT", title="MS-only Extrapolated Trk eta", xbins=50, xmin=-5, xmax=5)
-        # self.defineHistogram( "mstrks_phi", type="TH1F", path="EXPERT", title="MS-only Extrapolated Trk phi", xbins=50, xmin=-5, xmax=5)
-
         if "CB" in name:
             self.defineHistogram( "cbtrks_pt", type="TH1F", path="EXPERT", title="Combined Trk pT", xbins=100, xmin=0, xmax=300)
             self.defineHistogram( "cbtrks_eta", type="TH1F", path="EXPERT", title="Combined Trk eta", xbins=50, xmin=-5, xmax=5)
             self.defineHistogram( "cbtrks_phi", type="TH1F", path="EXPERT", title="Combined Trk phi", xbins=50, xmin=-5, xmax=5)
-
-        self.defineHistogram( "segs_n", type="TH1F", path="EXPERT", title="Segments n", xbins=50, xmin=0, xmax=50)
-
