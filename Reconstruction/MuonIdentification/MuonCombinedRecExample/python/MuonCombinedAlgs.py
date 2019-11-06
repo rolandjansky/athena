54fd7a89c05f16dfca0f7c73f9020474aec98d40
Edward Moyse <edward.moyse@cern.ch>
2019-11-06 14:43:57 +0100

    Protection for case where MuGirl is off, but Stau is on (which shouldn't happen).

--- Reconstruction/MuonIdentification/MuonCombinedRecExample/python/MuonCombinedAlgs.py
+++ Reconstruction/MuonIdentification/MuonCombinedRecExample/python/MuonCombinedAlgs.py
@@ -188,5 +188,5 @@ class MuonCombinedReconstruction(ConfiguredMuonRec):
         # runs over outputs and create xAODMuon collection
         topSequence += getAlgorithm("MuonCreatorAlg")
         
-        if muonCombinedRecFlags.doMuGirlLowBeta():
+        if muonCombinedRecFlags.doMuGirl() and muonCombinedRecFlags.doMuGirlLowBeta():
             topSequence += getAlgorithm("StauCreatorAlg")
