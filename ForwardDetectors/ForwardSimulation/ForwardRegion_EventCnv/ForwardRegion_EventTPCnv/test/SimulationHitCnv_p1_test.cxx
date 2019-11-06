25e10fc7ea37104faa2df68a028948a70209b73c
scott snyder <snyder@bnl.gov>
2019-11-06 23:47:06 +0100

    Remove leftover commented-out code.

--- ForwardDetectors/ForwardSimulation/ForwardRegion_EventCnv/ForwardRegion_EventTPCnv/test/SimulationHitCnv_p1_test.cxx
+++ ForwardDetectors/ForwardSimulation/ForwardRegion_EventCnv/ForwardRegion_EventTPCnv/test/SimulationHitCnv_p1_test.cxx
@@ -16,52 +16,6 @@
 #include <iostream>
 
 
-#if 0
-void compare (const Trk::EnergyLoss& p1,
-              const Trk::EnergyLoss& p2)
-{
-  assert (p1.deltaE() == p2.deltaE());
-  assert (p1.sigmaDeltaE() == p2.sigmaDeltaE());
-  assert (p1.sigmaMinusDeltaE() == p2.sigmaMinusDeltaE());
-  assert (p1.sigmaPlusDeltaE() == p2.sigmaPlusDeltaE());
-}
-
-
-void compare (const DepositInCalo& p1,
-              const DepositInCalo& p2)
-{
-  assert (p1.subCaloId() == p2.subCaloId());
-  assert (p1.energyDeposited() == p2.energyDeposited());
-  assert (p1.muonEnergyLoss() == p2.muonEnergyLoss());
-  assert (p1.etDeposited() == p2.etDeposited());
-}
-
-
-void compare (const CaloEnergy& p1,
-              const CaloEnergy& p2)
-{
-  compare (static_cast<const Trk::EnergyLoss&> (p1),
-           static_cast<const Trk::EnergyLoss&> (p2));
-  assert (p1.energyLossType() == p2.energyLossType());
-  assert (p1.caloLRLikelihood() == p2.caloLRLikelihood());
-  assert (p1.caloMuonIdTag() == p2.caloMuonIdTag());
-  assert (p1.fsrCandidateEnergy() == p2.fsrCandidateEnergy());
-  assert (p1.etCore() == p2.etCore());
-  assert (p1.deltaEParam() == p2.deltaEParam());
-  assert (p1.sigmaMinusDeltaEParam() == p2.sigmaMinusDeltaEParam());
-  assert (p1.sigmaPlusDeltaEParam() == p2.sigmaPlusDeltaEParam());
-  assert (p1.deltaEMeas() == p2.deltaEMeas());
-  assert (p1.sigmaDeltaEMeas() == p2.sigmaDeltaEMeas());
-
-  const std::vector<DepositInCalo>& d1 = p1.depositInCalo();
-  const std::vector<DepositInCalo>& d2 = p2.depositInCalo();
-  assert (d1.size() == d2.size());
-  for (size_t i = 0; i < d1.size(); i++)
-    compare (d1[i], d2[i]);
-}
-#endif
-
-
 void compare (const CLHEP::Hep3Vector& p1,
               const CLHEP::Hep3Vector& p2)
 {
