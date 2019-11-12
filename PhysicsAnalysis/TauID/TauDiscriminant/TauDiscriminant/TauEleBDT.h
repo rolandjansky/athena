d37b36e490bec0b98c447dec0ac27e16ff9b386b
adbailey <adam.bailey@cern.ch>
2019-11-12 11:47:26 +0100

    Fixed missing updates to TauDiscriminant headers for tau tool base change

--- PhysicsAnalysis/TauID/TauDiscriminant/TauDiscriminant/TauEleBDT.h
+++ PhysicsAnalysis/TauID/TauDiscriminant/TauDiscriminant/TauEleBDT.h
@@ -75,7 +75,7 @@ class TauEleBDT: virtual public TauRecToolBase
 	virtual StatusCode eventInitialize() { return StatusCode::SUCCESS; }
 	virtual	StatusCode executeShotFinder(xAOD::TauJet&, xAOD::CaloClusterContainer&, xAOD::PFOContainer&) { return StatusCode::SUCCESS; }
 	virtual	StatusCode executePi0CreateROI(xAOD::TauJet&, CaloCellContainer&) { return StatusCode::SUCCESS; }
-	virtual	StatusCode executePi0ClusterCreator(xAOD::TauJet&, xAOD::PFOContainer&, xAOD::PFOContainer&, xAOD::CaloClusterContainer&) { return StatusCode::SUCCESS;}
+	virtual	StatusCode executePi0ClusterCreator(xAOD::TauJet&, xAOD::PFOContainer&, xAOD::PFOContainer&, xAOD::CaloClusterContainer&, const xAOD::CaloClusterContainer&) { return StatusCode::SUCCESS;}
 	virtual	StatusCode executeVertexVariables(xAOD::TauJet&, xAOD::VertexContainer&) { return StatusCode::SUCCESS; }
 	virtual StatusCode executePi0ClusterScaler(xAOD::TauJet&, xAOD::PFOContainer&, xAOD::PFOContainer&) { return StatusCode::SUCCESS; }
 	virtual StatusCode executePi0nPFO(xAOD::TauJet&, xAOD::PFOContainer&) { return StatusCode::SUCCESS; }
