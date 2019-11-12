e6c55f2bb2a8617290246406f7b351e70572c795
Peter Onyisi <ponyisi@utexas.edu>
2019-11-12 03:27:06 +0100

    Fix buggy script; use better hash function

--- Control/AthenaMonitoring/share/hist_diff.sh
+++ Control/AthenaMonitoring/share/hist_diff.sh
@@ -4,7 +4,7 @@ FILE1=$1
 FILE2=$2
 shift 2
 
-GREPOPTS='-E ""'
+GREPOPTS="--regexp=.*"
 while getopts "x:" opt; do
     case ${opt} in
 	x )
@@ -18,16 +18,16 @@ while getopts "x:" opt; do
     esac
 done
 
-if [[ ! -f $FILE1 ]] ; then { echo "$1 does not exist"; exit 1; } ; fi
-if [[ ! -f $FILE2 ]] ; then { echo "$2 does not exist"; exit 1; } ; fi
+if [[ ! -f $FILE1 ]] ; then { echo "$FILE1 does not exist"; exit 1; } ; fi
+if [[ ! -f $FILE2 ]] ; then { echo "$FILE2 does not exist"; exit 1; } ; fi
 
-LOG1=$(mktemp FILE1.XXXXXXX)
-LOG2=$(mktemp FILE2.XXXXXXX)
+LOG1=$(mktemp F1.XXXXXXX)
+LOG2=$(mktemp F2.XXXXXXX)
 
-hist_file_dump.py $FILE1 --hash | grep $GREPOPTS > $LOG1
+hist_file_dump.py $FILE1 --hash | grep ${GREPOPTS} > $LOG1
 RV=$?
-if [ $RV != 0 ]; then { echo "Failure dumping $FILE1"; rm -f $LOG1 $LOG2; exit $RV ; }; fi
-hist_file_dump.py $FILE2 --hash | grep $GREPOPTS > $LOG2
+if [ $RV != 0 ]; then { echo "Failure dumping $FILE1"; echo $RV ; cat $LOG1; rm -f $LOG1 $LOG2; exit $RV ; }; fi
+hist_file_dump.py $FILE2 --hash | grep ${GREPOPTS} > $LOG2
 RV=$?
 if [ $RV != 0 ]; then { echo "Failure dumping $FILE2"; rm -f $LOG1 $LOG2; exit $RV ; }; fi
 
