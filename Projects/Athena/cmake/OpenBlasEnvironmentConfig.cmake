ea7a732d158229b38dc96dc7e457ef82c8ed304d
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2019-11-12 19:36:44 +1030

    Added an environment setting for OpenBLAS.
    Using the OPENBLAS_NUM_THREADS environment variable should restrict how
    many threads the library would be allowed to use.

---
+++ Projects/Athena/cmake/OpenBlasEnvironmentConfig.cmake
@@ -0,0 +1,9 @@
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+#
+# Module for setting up the runtime environment for OpenBLAS.
+#
+
+# Set the necessary variable(s).
+set( OPENBLASENVIRONMENT_ENVIRONMENT
+   SET OPENBLAS_NUM_THREADS 1 )
+set( OPENBLASENVIRONMENT_FOUND TRUE )
