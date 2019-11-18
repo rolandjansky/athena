b1b1e52ca74448a85325cb45f7153f94b6c9354d
Noemi Calace <noemi.calace@cern.ch>
2019-11-18 10:47:09 +0100

    Committing the code for updates to SiSPSeededTrackFinder, SiSpacePointsSeedTool_xk, SiTrackMakerTool_xk, SiCombinatorialTrackFinderTool_xk

--- InnerDetector/InDetRecAlgs/SiSPSeededTrackFinder/src/SiSPSeededTrackFinder.cxx
+++ InnerDetector/InDetRecAlgs/SiSPSeededTrackFinder/src/SiSPSeededTrackFinder.cxx
@@ -110,6 +110,12 @@ StatusCode InDet::SiSPSeededTrackFinder::initialize()
   //
   ATH_CHECK(m_seedsmaker.retrieve());
 
+  if(m_useZvertexTool) {
+    // Get tool for z-coordinates ptimary vertices search
+    //
+    ATH_CHECK(m_zvertexmaker.retrieve());
+  }
+
   // Get tool for z-coordinates ptimary vertices search
   //
   if(m_useZvertexTool)
@@ -399,12 +405,6 @@ StatusCode InDet::SiSPSeededTrackFinder::itkStrategy()
   if (m_doFastTracking) return itkFastTrackingStrategy();
   
   m_outputTracks = CxxUtils::make_unique<TrackCollection>();
-  
-  // For HI events we can use MBTS information from calorimeter
-  //
-  if(!isGoodEvent()) {
-    return StatusCode::SUCCESS;
-  }
 
   std::multimap<double,Trk::Track*>    qualityTrack;
   const InDet::SiSpacePointsSeed* seed = 0;
