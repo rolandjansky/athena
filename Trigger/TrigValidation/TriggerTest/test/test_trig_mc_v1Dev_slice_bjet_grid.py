aa7462201d11f374b18b557aa413267733fa008c
Rafal Bielski <rafal.bielski@cern.ch>
2019-11-20 18:40:32 +0000

    TriggerTest: Replace legacy slice tests with Run-3 ones + update naming

---
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1Dev_slice_bjet_grid.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# art-description: Trigger RDO->RDO_TRIG athena test of the b-jet slice in Dev_pp_run3_v1 menu
+# art-type: grid
+# art-include: master/Athena
+# art-output: *.txt
+# art-output: *.log
+# art-output: log.*
+# art-output: *.out
+# art-output: *.err
+# art-output: *.log.tar.gz
+# art-output: *.new
+# art-output: *.json
+# art-output: *.root
+# art-output: *.pmon.gz
+# art-output: *perfmon*
+# art-output: *.check*
+
+from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
+
+ex = ExecStep.ExecStep()
+ex.type = 'athena'
+ex.job_options = 'TrigUpgradeTest/full_menu.py'
+ex.input = 'ttbar'
+ex.threads = 1
+# LS2_v1 soon to be renamed to Dev_pp_run3_v1
+ex.args = '-c "setMenu=\'LS2_v1\';doEmptyMenu=True;doBjetSlice=True;doWriteBS=False;doWriteRDOTrigger=True;"'
+
+test = Test.Test()
+test.art_type = 'grid'
+test.exec_steps = [ex]
+test.check_steps = CheckSteps.default_check_steps(test)
+
+import sys
+sys.exit(test.run())
