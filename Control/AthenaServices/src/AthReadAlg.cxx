223dde1e3a54de5e9ddf6690b7ad63340ca2352d
scott snyder <snyder@bnl.gov>
2019-11-22 16:36:17 +0100

    AthenaServices: Extend AthReadAlg to handle aliases.
    Have AthReadAlg create the same aliases that the original object had.
    Have AthReadAlg take a configurable list of aliases to declare to the
    scheduler as write dependencies.

--- Control/AthenaServices/src/AthReadAlg.cxx
+++ Control/AthenaServices/src/AthReadAlg.cxx
@@ -25,6 +25,17 @@ StatusCode AthReadAlg::initialize()
   // so that it ends with _DELETED.  Cache the sgkey for this proxy.
   m_sgkey = evtStore()->stringToKey (m_key.key() + "_DELETED",
                                      m_key.clid());
+
+  std::string storename;
+  const std::string& objkey = m_key.objKey();
+  std::string::size_type ppos = objkey.find ('+');
+  if (ppos != std::string::npos) {
+    storename = objkey.substr (0, ppos+1);
+  }
+  for (const std::string& a : m_aliases) {
+    this->addDependency (DataObjID (m_key.clid(), storename + a),
+                         Gaudi::DataHandle::Writer);
+  }
   return StatusCode::SUCCESS;
 }
 
@@ -35,7 +46,7 @@ StatusCode AthReadAlg::initialize()
  */
 StatusCode AthReadAlg::execute (const EventContext& ctx) const
 {
-  // Look up the proxy for the orignal object.
+  // Look up the proxy for the original object.
   const SG::DataProxy* proxy = evtStore()->proxy_exact (m_sgkey);
   if (!proxy) {
     ATH_MSG_ERROR( "Cannot find proxy for original object: " <<
@@ -56,6 +67,49 @@ StatusCode AthReadAlg::execute (const EventContext& ctx) const
   // Record the result in the event store.
   SG::TypelessWriteHandle h (m_key, ctx);
   ATH_CHECK( h.typelessPut (std::move (ptr) ) );
+
+  // Any extra aliases?
+  std::vector<std::string> extraAliases;
+  for (const std::string& a : m_aliases) {
+    if (!proxy->hasAlias (a + "_DELETED")) {
+      extraAliases.push_back (a);
+    }
+  }
+
+  // Restore original aliases.
+  if (!proxy->alias().empty() || !extraAliases.empty()) {
+    SG::DataProxy* newprox = evtStore()->proxy (m_key.clid(), m_key.key());
+    if (!newprox) {
+      ATH_MSG_ERROR( "Can't find proxy for just-recorded object " << 
+                     m_key.clid() << "/" << m_key.key());
+      return StatusCode::FAILURE;
+    }
+    SG::DataObjectSharedPtr<DataObject> dobj (newprox->accessData());
+
+    for (std::string a : proxy->alias()) {
+#if __cplusplus >= 201709
+      if (a.ends_with ("_DELETED"))
+#else
+      if (a.substr (a.size() - 8, 8) == "_DELETED")
+#endif
+      {
+        a.erase (a.size() - 8, 8);
+        if (!evtStore()->recordObject (dobj, a, false, true)) {
+          ATH_MSG_ERROR( "Can't make alias " << a << " for " <<
+                         m_key.clid() << "/" << m_key.key());
+          return StatusCode::FAILURE;
+        }
+      }
+    }
+
+    for (const std::string& a : extraAliases) {
+      if (!evtStore()->recordObject (dobj, a, false, true)) {
+        ATH_MSG_ERROR( "Can't make alias " << a << " for " <<
+                       m_key.clid() << "/" << m_key.key());
+        return StatusCode::FAILURE;
+      }
+    }
+  }
 
   return StatusCode::SUCCESS;
 }
