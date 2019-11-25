66ec04ffd909fcd5fc6ff0c807ac97de9b9f9965
scott snyder <sss@karma>
2019-11-25 15:02:48 +0100

    AFP_DBTools: Fix cppcheck warnings.
    cppcheck warnings: strings should be passed by const reference, not by value.

--- ForwardDetectors/AFP/AFP_DBTools/src/DBObjectBuilderBase.cxx
+++ ForwardDetectors/AFP/AFP_DBTools/src/DBObjectBuilderBase.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 /**
@@ -18,7 +18,7 @@
 namespace AFP
 {
 
-  std::string DBObjectBuilderBase::setSpecification (const std::string specification) 
+  std::string DBObjectBuilderBase::setSpecification (const std::string& specification) 
   {
     parsePayloadSpec(specification, m_specification);
     return generateBuilderCommands();
