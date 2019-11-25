be52017d2f9d868499c0a115200bd7c05f24acba
John Chapman <jchapman@cern.ch>
2019-11-25 17:59:02 +0100

    Python3 syntax fixes for old-style configuration

--- Database/AthenaPOOL/AthenaPoolUtilities/share/TPCnvTest.py
+++ Database/AthenaPOOL/AthenaPoolUtilities/share/TPCnvTest.py
@@ -34,8 +34,8 @@ import os
 if os.path.exists ('PoolFileCatalog.xml'):
     os.remove ('PoolFileCatalog.xml')
 
-if (not globals().has_key ('ATLAS_REFERENCE_TAG') and
-    os.environ.has_key ('ATLAS_REFERENCE_TAG')):
+if (not 'ATLAS_REFERENCE_TAG' in globals() and
+    'ATLAS_REFERENCE_TAG' in os.environ):
     ATLAS_REFERENCE_TAG = os.environ['ATLAS_REFERENCE_TAG']
 
 refpaths = [os.environ.get ('ATLAS_REFERENCE_DATA', None),
@@ -101,7 +101,7 @@ def checknewvars (output):
     return l
             
 
-if not globals().has_key ('get_dumper_fct'):
+if not 'get_dumper_fct' in globals():
     from PyDumper.Dumpers import get_dumper_fct
 from AthenaPython import PyAthena
 class Dumper (PyAthena.Alg):
@@ -117,14 +117,14 @@ class Dumper (PyAthena.Alg):
         if not os.path.exists (self.reffile_name):
             self.reffile_name = '../' + self.reffile_name
 
-        if not os.path.exists (self.reffile_name) and globals().has_key ('ATLAS_REFERENCE_TAG'):
+        if not os.path.exists (self.reffile_name) and 'ATLAS_REFERENCE_TAG' in globals():
             from AthenaCommon.Utils.unixtools import find_datafile
             r = find_datafile (ATLAS_REFERENCE_TAG)
             if r:
                 self.reffile_name = os.path.join (r, ATLAS_REFERENCE_TAG,
                                                   refbase)
 
-        if not os.path.exists (self.reffile_name) and globals().has_key ('ATLAS_REFERENCE_TAG'):
+        if not os.path.exists (self.reffile_name) and 'ATLAS_REFERENCE_TAG' in globals():
             self.reffile_name = find_file (os.path.join (ATLAS_REFERENCE_TAG,
                                                          refbase))
 
