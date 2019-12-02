9175a59f43b8e3ac5dea62924bb04dc0c692170f
scott snyder <snyder@bnl.gov>
2019-12-02 22:03:02 +0100

    egammaTrackTools:  Basic python3 fixes.
    - Relative import fixes.

--- Reconstruction/egamma/egammaTrackTools/python/egammaTrackToolsFactories.py
+++ Reconstruction/egamma/egammaTrackTools/python/egammaTrackToolsFactories.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 __doc__ = "ToolFactories to instantiate all egammaCaloTools with default configuration"
 __author__ = "Bruno Lenzi , Christos Anastopoulos"
 from egammaRec.Factories import FcnWrapper, ToolFactory
-import egammaTrackToolsConf
+from egammaTrackTools import egammaTrackToolsConf
 from egammaRec import egammaKeys
 from egammaTools.InDetTools import egammaCaloExtrapolator,egammaExtrapolator
 import AthenaCommon.CfgMgr as CfgMgr
