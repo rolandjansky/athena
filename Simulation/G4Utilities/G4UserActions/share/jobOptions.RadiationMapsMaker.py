a2bb884e32fbcc9998c1ea3a13cd4e03e6a4275a
Sven Menke <sven.menke@cern.ch>
2019-12-05 17:08:24 +0100

    RadiationMapsMaker: update/add copyright lines

--- Simulation/G4Utilities/G4UserActions/share/jobOptions.RadiationMapsMaker.py
+++ Simulation/G4Utilities/G4UserActions/share/jobOptions.RadiationMapsMaker.py
@@ -1,3 +1,6 @@
+#
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+#
 from G4AtlasApps.SimFlags import simFlags
 from G4UserActions.G4UserActionsConf import G4UA__RadiationMapsMakerTool
 #
--- Simulation/G4Utilities/G4UserActions/src/RadiationMapsMakerTool.cxx
+++ Simulation/G4Utilities/G4UserActions/src/RadiationMapsMakerTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 #include "TH2D.h"
 #include "TH3D.h"
