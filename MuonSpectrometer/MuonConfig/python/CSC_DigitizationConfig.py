43bdfceccf1d1975f2ac677009ed461f2bff5875
Constantin Heidegger <constantin.heidegger@cern.ch>
2019-12-06 09:00:03 +0100

    replace CscCoolStrSvc call by new CscDbAlg

--- MuonSpectrometer/MuonConfig/python/CSC_DigitizationConfig.py
+++ MuonSpectrometer/MuonConfig/python/CSC_DigitizationConfig.py
@@ -93,7 +93,7 @@ def CSC_DigitizationBasicCfg(flags, **kwargs):
 def CSC_OverlayDigitizationBasicCfg(flags, **kwargs):
     """Return ComponentAccumulator with CSC Overlay digitization"""
     acc = MuonGeoModelCfg(flags)
-    acc.merge(CscCoolStrSvcCfg(flags))
+    acc.merge(CscCondDbAlgCfg(flags))
     if "DigitizationTool" not in kwargs:
         tool = acc.popToolsAndMerge(CSC_OverlayDigitizationToolCfg(flags))
         kwargs["DigitizationTool"] = tool
