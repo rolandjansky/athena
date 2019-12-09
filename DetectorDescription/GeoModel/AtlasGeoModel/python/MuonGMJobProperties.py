d2003dc1e00ca9b2464e548bfef5e375883aacba
scott snyder <snyder@bnl.gov>
2019-12-09 17:14:05 +0100

    AtlasGeoModel:  Basic python3 fixes.
    - py2 print function compatibility.

--- DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGMJobProperties.py
+++ DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGMJobProperties.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
+from __future__ import print_function
+
 from AthenaCommon.JobProperties import JobProperty, JobPropertyContainer, jobproperties
 from AtlasGeoModel.CommonGMJobProperties import CommonGMFlags, CommonGeometryFlags
 
