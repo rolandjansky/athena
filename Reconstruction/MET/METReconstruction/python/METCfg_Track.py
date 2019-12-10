b254da3013cb53914d66215c5a20f32eb3add613
williams <sarah.louise.williams@cern.ch>
2019-12-10 08:42:14 +0000

    ConfigUpdatesNov2019

--- Reconstruction/MET/METReconstruction/python/METCfg_Track.py
+++ Reconstruction/MET/METReconstruction/python/METCfg_Track.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 
-from METReconstruction.METRecoCfg import BuildConfig, RefConfig, METConfig,clusterSigStates,getMETRecoAlg
+from METReconstruction.METRecoCfg import BuildConfig, RefConfig, METConfig,clusterSigStates,getMETRecoAlg,getMETRecoTool
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+from AthenaCommon import CfgMgr
 
 
 def METTrack_Cfg(configFlags):
@@ -18,9 +19,13 @@ def METTrack_Cfg(configFlags):
     cfg_trk.refiners['TrackFilter'].DoLepRecovery=True
     cfg_trk.refiners['TrackFilter'].DoVxSep=configFlags.MET.UseTracks
     cfg_trk.refiners['TrackFilter'].DoEoverPSel=True
+    recotool = getMETRecoTool(cfg_trk)
+    #recotool = CfgMgr.met__METRecoTool('MET_RecoTool_empty')
+    print "Printing recotool"
+    print recotool
     print "MET track helper object defined!!!"
     print cfg_trk
-    recoAlg=getMETRecoAlg(sequencename,{'Track':cfg_trk})
+    recoAlg=getMETRecoAlg(algName='METRecoAlg_Track',tools=[recotool])
     print "ALG defined"
     print recoAlg
     components.addEventAlgo(recoAlg, sequencename)
