06883dc65fba5931ef682cbe45f3e3c949a4ea3e
Nicolas Köhler <nicolas.koehler@cern.ch>
2019-12-10 19:41:29 +0100

    manual sweep of 28591 and 28134

--- MuonSpectrometer/MuonDetDescr/MuonAGDDBase/src/mm_readoutHandler.cxx
+++ MuonSpectrometer/MuonDetDescr/MuonAGDDBase/src/mm_readoutHandler.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "MuonAGDDBase/mm_readoutHandler.h"
@@ -44,20 +44,50 @@ void mm_readoutHandler::ElementHandle()
     MM->roParameters.roLength              = getAttributeAsDouble    ("roLength", ret);
     if (! ret) std::cout << "WARNING: roLength is missing" << std::endl;
 
-    MM->roParameters.sStripWidth           = getAttributeAsDouble    ("sStripWidth", ret);
-    if (! ret) std::cout << "WARNING: sStripWidth is missing" << std::endl;
+    MM->roParameters.activeBottomLength           = getAttributeAsDouble    ("activeBottomLength", ret);
+    if (! ret) std::cout << "WARNING: activeBottomLength is missing" << std::endl;
 
-    MM->roParameters.lStripWidth           = getAttributeAsDouble    ("lStripWidth", ret);
-    if (! ret) std::cout << "WARNING: lStripWidth is missing" << std::endl;
+    MM->roParameters.activeTopLength           = getAttributeAsDouble    ("activeTopLength", ret);
+    if (! ret) std::cout << "WARNING: activeTopLength is missing" << std::endl;
 
     MM->roParameters.activeH               = getAttributeAsDouble    ("activeH", ret);
     if (! ret) std::cout << "WARNING: activeH is missing" << std::endl;
 
-    MM->roParameters.minYPhi               = getAttributeAsDouble    ("minYPhi", ret);
-    if (! ret) std::cout << "WARNING: minYPhi is missing" << std::endl;
+    MM->roParameters.minYPhiL               = getAttributeAsDouble    ("minYPhiL", ret);
+    if (! ret) std::cout << "WARNING: minYPhiL is missing" << std::endl;
+    
+    MM->roParameters.minYPhiR               = getAttributeAsDouble    ("minYPhiR", ret);
+    if (! ret) std::cout << "WARNING: minYPhiR is missing" << std::endl;
 
     MM->roParameters.maxYPhi               = getAttributeAsDouble    ("maxYPhi", ret);
     if (! ret) std::cout << "WARNING: maxYPhi is missing" << std::endl;
+    
+    MM->roParameters.dlStereoBottom               = getAttributeAsDouble    ("dlStereoBottom", ret);
+    if (! ret) std::cout << "WARNING: dlStereoBottom is missing" << std::endl;
+    
+    MM->roParameters.dlStereoTop               = getAttributeAsDouble    ("dlStereoTop", ret);
+    if (! ret) std::cout << "WARNING: dlStereoTop is missing" << std::endl;
+    
+    MM->roParameters.nMissedTopEta               = getAttributeAsInt    ("nMissedTopEta", ret);
+    if (! ret) std::cout << "WARNING: nMissedTopEta is missing" << std::endl;
+    
+    MM->roParameters.nMissedBottomEta               = getAttributeAsInt    ("nMissedBottomEta", ret);
+    if (! ret) std::cout << "WARNING: nMissedBottomEta is missing" << std::endl;
+    
+    MM->roParameters.nMissedTopStereo               = getAttributeAsInt    ("nMissedTopStereo", ret);
+    if (! ret) std::cout << "WARNING: nMissedTopStereo is missing" << std::endl;
+    
+    MM->roParameters.nMissedBottomStereo              = getAttributeAsInt    ("nMissedBottomStereo", ret);
+    if (! ret) std::cout << "WARNING: nMissedBottomStereo is missing" << std::endl;
+    
+    MM->roParameters.nRoutedTop               = getAttributeAsInt    ("nRoutedTop", ret);
+    if (! ret) std::cout << "WARNING: nRoutedTop is missing" << std::endl;
+    
+    MM->roParameters.nRoutedBottom               = getAttributeAsInt    ("nRoutedBottom", ret);
+    if (! ret) std::cout << "WARNING: nRoutedBottom is missing" << std::endl;
+    
+    MM->roParameters.tStrips             = getAttributeAsInt    ("totalStrips", ret);
+    if (! ret) std::cout << "WARNING: totalStrips is missing" << std::endl;
 
 
 }
--- MuonSpectrometer/MuonDetDescr/MuonAGDDDescription/MuonAGDDDescription/MMDetectorDescription.h
+++ MuonSpectrometer/MuonDetDescr/MuonAGDDDescription/MuonAGDDDescription/MMDetectorDescription.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef MMDetectorDescription_H
@@ -19,13 +19,23 @@ struct MMReadoutParameters {
     std::vector<double> stereoAngle;
     std::vector<int> readoutSide;
     double zpos;
-    double distanceFromZAxis;
-    double roLength;
-    double sStripWidth;
-    double lStripWidth;
-    double activeH;
-    double minYPhi;
+    double distanceFromZAxis;  //inner radius: distance from the IP till the bottom part of the module
+    double roLength;   //module's radial size
+    double activeBottomLength;  // active area: bottom length
+    double activeTopLength;    //active area: top length
+    double activeH;            //active area: radial size
+     double minYPhiR;          //active area: the distance between the first eta and stereo strips (active) [R:right, L:left, min:bottom part, max:top part]. LM1 is the special case as there is no space to route any strips at the bottom, we use two distances (left and right).
+    double minYPhiL;
     double maxYPhi;
+    int nMissedTopEta;      //number of strips that are not connected at any FE board (eta layer)
+    int nMissedBottomEta;
+    int nMissedTopStereo;   //number of strips that are not connected at any FE board (stereo layer)
+    int nMissedBottomStereo;
+    int nRoutedTop;     // number of strips needed to cover the low efficient parts of the module (these strips are shorter in lenght than the normal ones)
+    int nRoutedBottom;
+    double dlStereoTop;
+    double dlStereoBottom;
+    int tStrips; //total strips per module (disconnected strips are included)
 };
 
 class MMDetectorDescription: public AGDDDetector {
