35269fe5cf37f74b20e33dcac88e5301132f4957
scott snyder <snyder@bnl.gov>
2019-12-12 16:18:15 +0100

    InDetRecExample:  Basic python3 fixes.
    print function.

--- InnerDetector/InDetExample/InDetRecExample/python/ConfiguredSecVertexFinding.py
+++ InnerDetector/InDetExample/InDetRecExample/python/ConfiguredSecVertexFinding.py
@@ -1,4 +1,7 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+
+from __future__ import print_function
+
 
 # ------------------------------------------------------------------------------------
 #
