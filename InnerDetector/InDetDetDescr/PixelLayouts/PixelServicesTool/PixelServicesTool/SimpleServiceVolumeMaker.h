17f39200c3621930fc1d1a87bfeb813b1dc5021e
nreadiof <nreadiof@cern.ch>
2019-12-13 16:20:05 +0100

    Merging ServiceVolumeMakerMgr with ServiceVolumeMaker

--- InnerDetector/InDetDetDescr/PixelLayouts/PixelServicesTool/PixelServicesTool/SimpleServiceVolumeMaker.h
+++ InnerDetector/InDetDetDescr/PixelLayouts/PixelServicesTool/PixelServicesTool/SimpleServiceVolumeMaker.h
@@ -20,83 +20,7 @@ namespace InDetDD {
   class ServiceVolume;
   class AthenaComps;
 
-  class SimpleServiceVolumeSchema {
-  public:
-    SimpleServiceVolumeSchema();
-
-    std::string rmin() const {return m_rmin;}
-    std::string rmax() const {return m_rmax;}
-    std::string rmin2() const {return m_rmin2;}
-    std::string rmax2() const {return m_rmax2;}
-    std::string zmin() const {return m_zmin;}
-    std::string zmax() const {return m_zmax;}
-    std::string zsymm() const {return m_zsymm;}
-    std::string shapeType() const {return m_shapeType;}
-    std::string materialName() const {return m_materialName;}
-    std::string volName() const {return m_volName;}
-    std::string phiWidth() const {return m_phiWidth;}
-    std::string width() const {return m_width;}
-    std::string repeat() const {return m_repeat;}
-    std::string phiStart() const {return m_phiStart;}
-    std::string phiDelta() const {return m_phiDelta;}
-    std::string phiStep() const {return m_phiStep;}
-    std::string radialDiv() const {return m_radialDiv;}
-    
-  private:
-    std::string m_rmin;
-    std::string m_rmax;
-    std::string m_rmin2;
-    std::string m_rmax2;
-    std::string m_zmin;
-    std::string m_zmax;
-    std::string m_zsymm;
-    std::string m_shapeType;
-    std::string m_materialName;
-    std::string m_volName;
-    std::string m_phiWidth;
-    std::string m_width;
-    std::string m_repeat;
-    std::string m_phiStart;
-    std::string m_phiDelta;
-    std::string m_phiStep;
-    std::string m_radialDiv;
-  };
-
-  class SimpleServiceVolumeMakerMgr : public GeoXMLUtils, public PixelGeoBuilder {
-
-  public:
-    SimpleServiceVolumeMakerMgr(const std::string & table, bool readDataFromDB, const PixelGeoBuilderBasics* basics);
-    
-    double rmin(int index) const;
-    double rmax(int index) const;
-    double rmin2(int index) const;
-    double rmax2(int index) const;
-    double zmin(int index) const;
-    double zmax(int index) const;
-    double width(int index) const;
-    double phiStart(int index) const;
-    double phiDelta(int index) const;
-    double phiStep(int index) const;
-    bool   zsymm(int index) const;
-    int    repeat(int index) const;
-    int    radialDiv(int index) const;
-    std::string shapeType(int index) const;
-    std::string volName(int index) const;
-    std::string materialName(int index) const;
-    
-    unsigned int numElements() const;
-
-    const IGeometryDBSvc *db() const;
-    
-  private:
-    std::string m_table;
-    PixelSimpleServiceXMLHelper* m_simpleSrvXMLHelper;
-    bool m_readFromDB;
-    bool m_XMLdefined; 
-  };
-
-
-  class SimpleServiceVolumeMaker {
+  class SimpleServiceVolumeMaker : public GeoXMLUtils, public PixelGeoBuilder {
   public:
     SimpleServiceVolumeMaker(const std::string &table, const std::string &label, const PixelGeoBuilderBasics* basics, bool readDataFromDB) ;
     ~SimpleServiceVolumeMaker();
@@ -107,9 +31,11 @@ namespace InDetDD {
   private:
     std::string m_table;
     std::string m_label;
-    SimpleServiceVolumeMakerMgr * m_mgr;    
+    PixelSimpleServiceXMLHelper* m_simpleSrvXMLHelper;
     std::vector<const ServiceVolume *> m_services;
     const OraclePixGeoAccessor* m_geoAccessor;
+    bool m_readFromDB;
+    bool m_XMLdefined;
   };
 } // end namespace
 
--- InnerDetector/InDetDetDescr/PixelLayouts/PixelServicesTool/src/SimpleServiceVolumeMaker.cxx
+++ InnerDetector/InDetDetDescr/PixelLayouts/PixelServicesTool/src/SimpleServiceVolumeMaker.cxx
@@ -17,30 +17,11 @@ Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 namespace InDetDD {
 
-SimpleServiceVolumeSchema::SimpleServiceVolumeSchema() 
-{
-  m_rmin = "RIN";
-  m_rmax = "ROUT";
-  m_rmin2 = "RIN2";
-  m_rmax2 = "ROUT2";
-  m_zmin = "ZIN";
-  m_zmax = "ZOUT";
-  m_zsymm = "ZSYMM";
-  m_materialName = "MATERIALNAME";
-  m_repeat = "REPEAT";
-  m_phiStart = "PHI";
-  m_phiDelta = "WIDTH";
-  m_width = "WIDTH";
-  m_shapeType = "SHAPE";
-  m_volName = "VOLNAME";
-  m_radialDiv = "";
-  m_phiStep = "";
-}
-
-SimpleServiceVolumeMakerMgr::SimpleServiceVolumeMakerMgr(const std::string & table, bool readDataFromDB, const PixelGeoBuilderBasics* basics)
+SimpleServiceVolumeMaker::SimpleServiceVolumeMaker(const std::string & table, const std::string & label, const PixelGeoBuilderBasics* basics, bool readDataFromDB) 
   : GeoXMLUtils(),
     PixelGeoBuilder(basics),
     m_table(table),
+    m_label(label),
     m_simpleSrvXMLHelper(0),
     m_readFromDB(readDataFromDB),
     m_XMLdefined(false)
@@ -51,127 +32,24 @@ SimpleServiceVolumeMakerMgr::SimpleServiceVolumeMakerMgr(const std::string & tab
   }
 }
 
-// Question - why does the db() getter never return the database?
-const IGeometryDBSvc *SimpleServiceVolumeMakerMgr::db() const {
-  //  return m_athenaComps->geomDB();
-  return 0;
-}
-
-double SimpleServiceVolumeMakerMgr::rmin(int index) const
-{  
-  return m_simpleSrvXMLHelper->rmin(index);
-}
-
-
-double SimpleServiceVolumeMakerMgr::rmax(int index) const
-{ 
-  return m_simpleSrvXMLHelper->rmax(index);
-}
-
-
-double SimpleServiceVolumeMakerMgr::rmin2(int index) const
-{
-  return m_simpleSrvXMLHelper->rmin2(index);
-}
-
-double SimpleServiceVolumeMakerMgr::rmax2(int index) const
-{
-  return m_simpleSrvXMLHelper->rmax2(index);
-}
-
-double SimpleServiceVolumeMakerMgr::zmin(int index) const
-{ 
-  return m_simpleSrvXMLHelper->zmin(index);
-}
-
-double SimpleServiceVolumeMakerMgr::zmax(int index) const
-{
-  
-  return m_simpleSrvXMLHelper->zmax(index);
-}
-
-double SimpleServiceVolumeMakerMgr::phiDelta(int index) const
-{
-  return m_simpleSrvXMLHelper->phiDelta(index);
-}
-
-double SimpleServiceVolumeMakerMgr::width(int index) const
-{
-  return m_simpleSrvXMLHelper->width(index);
-}
-
-double SimpleServiceVolumeMakerMgr::phiStart(int index) const
-{
-  return m_simpleSrvXMLHelper->phiStart(index);
-}
-
-double SimpleServiceVolumeMakerMgr::phiStep(int index) const
-{
-  return 0;
-}
-
-bool SimpleServiceVolumeMakerMgr::zsymm(int index) const
-{
-  return m_simpleSrvXMLHelper->zsymm(index);
-}
-
-
-int SimpleServiceVolumeMakerMgr::repeat(int index) const
-{
-  return m_simpleSrvXMLHelper->repeat(index);
-}
-
-int SimpleServiceVolumeMakerMgr::radialDiv(int index) const
-{
-  return 0;
-}
-
-std::string SimpleServiceVolumeMakerMgr::shapeType(int index) const
-{
-  return m_simpleSrvXMLHelper->shapeType(index);
-}
-
-std::string SimpleServiceVolumeMakerMgr::volName(int index) const
-{
-  return m_simpleSrvXMLHelper->volName(index);
-}
-
-std::string SimpleServiceVolumeMakerMgr::materialName(int index) const
-{
-  return m_simpleSrvXMLHelper->materialName(index);
-}
-
-unsigned int SimpleServiceVolumeMakerMgr::numElements() const
-{
-  return m_simpleSrvXMLHelper->numElements();
-}
-
-
-SimpleServiceVolumeMaker::SimpleServiceVolumeMaker(const std::string & table, const std::string & label, const PixelGeoBuilderBasics* basics, bool readDataFromDB) 
-  : m_table(table),
-    m_label(label)
-{
-  m_mgr = new SimpleServiceVolumeMakerMgr(table, readDataFromDB, basics);
-}
 
 SimpleServiceVolumeMaker::~SimpleServiceVolumeMaker()
 {
   for (unsigned int i = 0; i < m_services.size(); i++) {
     delete m_services[i];
   }
-  delete m_mgr;
 }
 
 const std::vector<const ServiceVolume *>& SimpleServiceVolumeMaker::makeAll()
 {
-  for (unsigned int ii = 0; ii < numElements(); ++ii) {
+  for (unsigned int ii = 0; ii < m_simpleSrvXMLHelper->numElements(); ++ii) {
     m_services.push_back(make(ii));
   }
   return m_services;
 }
 
 unsigned int SimpleServiceVolumeMaker::numElements() const {
-  return m_mgr->numElements();
+  return m_simpleSrvXMLHelper->numElements();
 }
 
 InDetDD::ServiceVolume *SimpleServiceVolumeMaker::make(int ii)
@@ -180,60 +58,55 @@ InDetDD::ServiceVolume *SimpleServiceVolumeMaker::make(int ii)
   // Retrieve/calculate the parameters for the volume.
   //
   ServiceVolume * param = new ServiceVolume ;
-  param->setMaterial(m_mgr->materialName(ii));
-  param->setRmin(m_mgr->rmin(ii));
-  param->setRmax(m_mgr->rmax(ii));
-  param->setZmin(m_mgr->zmin(ii));
-  param->setZmax(m_mgr->zmax(ii));
-  param->setZsymm(m_mgr->zsymm(ii));
-  param->setVolName(m_mgr->volName(ii));
-  
-  
+  param->setMaterial(m_simpleSrvXMLHelper->materialName(ii));
+  param->setRmin    (m_simpleSrvXMLHelper->rmin(ii));
+  param->setRmax    (m_simpleSrvXMLHelper->rmax(ii));
+  param->setZmin    (m_simpleSrvXMLHelper->zmin(ii));
+  param->setZmax    (m_simpleSrvXMLHelper->zmax(ii));
+  param->setZsymm   (m_simpleSrvXMLHelper->zsymm(ii));
+  param->setVolName (m_simpleSrvXMLHelper->volName(ii));
+
   bool needsRotation = false;
   
   // For TUBE there is no need to read the rest 
-  std::string shapeType = m_mgr->shapeType(ii);
+  std::string shapeType =  m_simpleSrvXMLHelper->shapeType(ii);
   if (!shapeType.empty() && shapeType != "TUBE") {
       
-    double rmin2 = m_mgr->rmin2(ii);
-    double rmax2 = m_mgr->rmax2(ii);
+    double rmin2 = m_simpleSrvXMLHelper->rmin2(ii);
+    double rmax2 = m_simpleSrvXMLHelper->rmax2(ii);
     
     if (rmin2 <= 0) rmin2 = param->rmin(); 
     if (rmax2 <= 0) rmax2 = param->rmax(); 
     
-    int radialDiv = m_mgr->radialDiv(ii);
+    // FIXME radialDiv feature not fully implemented, or saved to param
+    //int radialDiv = 0; 
     
-    double phiDelta =  m_mgr->phiDelta(ii);
+    double phiDelta =  m_simpleSrvXMLHelper->phiDelta(ii);
     
     bool fullPhiSector = false;
     if (phiDelta == 0 || phiDelta >=359.9*CLHEP::degree) {
       phiDelta = 360*CLHEP::degree;
       fullPhiSector = true;
     } 
-    //else {
-    //phiDelta -= 2*phiepsilon;
-    //phiStart += phiepsilon;
-    // }
     
     if (shapeType == "UNKNOWN") {
-      if (radialDiv > 0) {
-	shapeType = "RADIAL";
-      } else if (param->rmin() == rmin2  &&  param->rmax() == rmax2 ) {
-	if (fullPhiSector) {
+      //if (radialDiv > 0) {
+      //  shapeType = "RADIAL";
+      //} else 
+      if (param->rmin() == rmin2  &&  param->rmax() == rmax2 ) {
+	if (fullPhiSector)
 	  shapeType = "TUBE";
-	} else {
+	else
 	  shapeType = "TUBS";
-	}
-      } else {
+      } else
 	shapeType = "CONS";
-      } 
     }
     
     
-    int repeat = m_mgr->repeat(ii);
+    int repeat = m_simpleSrvXMLHelper->repeat(ii);
     if (repeat == 0) repeat = 1;
     
-    double phiStart =  m_mgr->phiStart(ii);
+    double phiStart =  m_simpleSrvXMLHelper->phiStart(ii);
     double phiWidth =  phiDelta;
     
     if (shapeType == "CONS"  || shapeType == "TUBS") { 
@@ -245,7 +118,7 @@ InDetDD::ServiceVolume *SimpleServiceVolumeMaker::make(int ii)
     // Can be in degree or CLHEP::mm. Usually it is CLHEP::deg expect for BOX, TRAP and ROD shape
     // Geometry manager makes no assumptions about units. So we must interpret here.
     if (shapeType == "BOX" || shapeType == "ROD" || shapeType=="ROD2" || shapeType == "TRAP") {
-      phiWidth = m_mgr->width(ii); // in mm
+      phiWidth = m_simpleSrvXMLHelper->width(ii); // in mm
     } 
     
     if (shapeType == "PGON"  || shapeType == "PGON2" || 
