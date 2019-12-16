767d06afccec3bef8fae34dffcb797e650e19425
Tim Martin <Tim.Martin@cern.ch>
2019-12-16 19:05:49 +0100

    Remove RatesAnalysisExamplesXAOD package, its ExampleRatesFullMenu is now FullMenu in the RatesAnalysis class and RatesGetCrossSectionMC.py moves there too

--- Trigger/TrigCost/RatesAnalysisExamplesXAOD/src/components/RatesAnalysis_entries.cxx
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "../ExampleRatesEmulation.h"
-#include "../ExampleRatesFullMenu.h"
-
-DECLARE_COMPONENT( ExampleRatesEmulation )
-DECLARE_COMPONENT( ExampleRatesFullMenu )
