1fb7100298b223b05e561499ecdda067d88a8211
scott snyder <snyder@bnl.gov>
2019-12-20 16:00:58 +0100

    D3PDMakerTest: python 3 fixes
    Get tests working with python 3.

--- PhysicsAnalysis/D3PDMaker/D3PDMakerTest/python/changerun.py
+++ PhysicsAnalysis/D3PDMaker/D3PDMakerTest/python/changerun.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+
+from __future__ import print_function
 
 import sys
 
 if len(sys.argv) < 4:
-    print """Change the run number in a pool file.
+    print ("""Change the run number in a pool file.
 Probably only works correctly for simple, single-run MC files.
 
 Usage: changerun.py NEWRUN INFILE OUTFILE
-"""
+""")
 
 newrun = int (sys.argv[1])
 infile = sys.argv[2]
