4c0487c8ad5107131628bdb5bbd80eced5a26906
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-01-03 13:02:54 +0100

    Moved the custom output operators into the global namespace.
    This should be a little better than having them in the std
    namespace. Even if this code is only used as part of the unit tests.

--- Control/xAODDataSource/test/helpers.h
+++ Control/xAODDataSource/test/helpers.h
@@ -1,6 +1,6 @@
 // Dear emacs, this is -*- c++ -*-
 //
-// Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+// Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 //
 #ifndef XAODDATASOURCE_TEST_HELPERS_H
 #define XAODDATASOURCE_TEST_HELPERS_H
@@ -23,32 +23,28 @@
       }                                                                 \
    } while( false )
 
-namespace std {
+/// Helper print operator
+template< typename FIRST, typename SECOND >
+std::ostream& operator<< ( std::ostream& out,
+                           const std::pair< FIRST, SECOND >& pair ) {
 
-   /// Helper print operator
-   template< typename FIRST, typename SECOND >
-   std::ostream& operator<< ( std::ostream& out,
-                              const std::pair< FIRST, SECOND >& pair ) {
+   out << "[" << pair.first << ", " << pair.second << "]";
+   return out;
+}
 
-      out << "[" << pair.first << ", " << pair.second << "]";
-      return out;
-   }
+/// Helper print operator
+template< typename T >
+std::ostream& operator<< ( std::ostream& out, const std::vector< T >& vec ) {
 
-   /// Helper print operator
-   template< typename T >
-   std::ostream& operator<< ( std::ostream& out, const std::vector< T >& vec ) {
-
-      out << "[";
-      for( size_t i = 0; i < vec.size(); ++i ) {
-         out << vec[ i ];
-         if( i + 1 < vec.size() ) {
-            out << ", ";
-         }
+   out << "[";
+   for( size_t i = 0; i < vec.size(); ++i ) {
+      out << vec[ i ];
+      if( i + 1 < vec.size() ) {
+         out << ", ";
       }
-      out << "]";
-      return out;
    }
-
-} // namespace std
+   out << "]";
+   return out;
+}
 
 #endif // XAODDATASOURCE_TEST_HELPERS_H
