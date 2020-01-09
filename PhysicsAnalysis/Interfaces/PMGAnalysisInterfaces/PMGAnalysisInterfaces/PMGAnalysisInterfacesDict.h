e05e51a9088cd5f7ffb03d2e72d087d1ac226456
Nils Erik Krumnack <nils.erik.krumnack@cern.ch>
2020-01-09 17:00:14 +0000

    Revert "Merge branch 'PMGToolFixes-21.2-20200109' into '21.2'"
    This reverts merge request !29355

--- PhysicsAnalysis/Interfaces/PMGAnalysisInterfaces/PMGAnalysisInterfaces/PMGAnalysisInterfacesDict.h
+++ PhysicsAnalysis/Interfaces/PMGAnalysisInterfaces/PMGAnalysisInterfaces/PMGAnalysisInterfacesDict.h
@@ -1,14 +1,9 @@
-// Dear emacs, this is -*- c++ -*
-//
-// Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
-//
-#ifndef PMGANALYSISINTERFACES_PMGANALYSISINTERFACESDICT_H
-#define PMGANALYSISINTERFACES_PMGANALYSISINTERFACESDICT_H
+#ifndef PMGANALYSISINTERFACES_PMGANALYSISINTERFACESDICT
+#define PMGANALYSISINTERFACES_PMGANALYSISINTERFACESDICT
 
-// Local include(s).
-#include "PMGAnalysisInterfaces/IHFORSelectionTool.h"
-#include "PMGAnalysisInterfaces/IPMGCrossSectionTool.h"
-#include "PMGAnalysisInterfaces/IPMGSherpaVjetsSysTool.h"
-#include "PMGAnalysisInterfaces/IPMGTruthWeightTool.h"
+#include "IHFORSelectionTool.h"
+#include "IPMGCrossSectionTool.h"
+#include "IPMGSherpaVjetsSysTool.h"
+#include "IPMGTruthWeightTool.h"
 
-#endif // PMGANALYSISINTERFACES_PMGANALYSISINTERFACESDICT_H
+#endif
