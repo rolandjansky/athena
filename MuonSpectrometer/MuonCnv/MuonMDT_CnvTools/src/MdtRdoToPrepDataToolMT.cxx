593904f108558e51a99082c4242d05e62c44196b
Ian Connelly <ian.connelly@cern.ch>
2020-01-10 18:37:22 +0000

    WIP for MDT PRD cache

--- MuonSpectrometer/MuonCnv/MuonMDT_CnvTools/src/MdtRdoToPrepDataToolMT.cxx
+++ MuonSpectrometer/MuonCnv/MuonMDT_CnvTools/src/MdtRdoToPrepDataToolMT.cxx
@@ -35,16 +35,38 @@ StatusCode Muon::MdtRdoToPrepDataToolMT::finalize()
 
 Muon::MdtRdoToPrepDataToolMT::SetupMdtPrepDataContainerStatus Muon::MdtRdoToPrepDataToolMT::setupMdtPrepDataContainer()
 {
-  // MT version of this method always adds container. Caching will be added later.
   m_fullEventDone=false;
 
   SG::WriteHandle< Muon::MdtPrepDataContainer >handle(m_mdtPrepDataContainerKey);
-  StatusCode status = handle.record(std::make_unique<Muon::MdtPrepDataContainer>(m_muonIdHelperTool->mdtIdHelper().module_hash_max()));
 
-  if (status.isFailure() || !handle.isValid() ) 	{
-    ATH_MSG_FATAL("Could not record container of MDT PrepData Container at " << m_mdtPrepDataContainerKey.key());	
-    return FAILED;
+  // Caching of PRD container
+  const bool externalCachePRD = !m_prdContainerCacheKey.key().empty();
+  if (!externalCachePRD) {
+    // without the cache we just record the container
+    StatusCode status = handle.record(std::make_unique<Muon::MdtPrepDataContainer>(m_muonIdHelperTool->mdtIdHelper().module_hash_max()));
+    if (status.isFailure() || !handle.isValid() )   {
+      ATH_MSG_FATAL("Could not record container of MDT PrepData Container at " << m_mdtPrepDataContainerKey.key()); 
+      return FAILED;
+    }
+    ATH_MSG_DEBUG("Created container " << m_mdtPrepDataContainerKey.key());
+  } 
+  else {
+    // use the cache to get the container
+
+    SG::UpdateHandle<MdtPrepDataCollection_Cache> update(m_prdContainerCacheKey);
+    if (!update.isValid()){
+      ATH_MSG_FATAL("Invalid UpdateHandle " << m_prdContainerCacheKey.key());
+      return FAILED;
+    }
+    StatusCode status = handle.record(std::make_unique<Muon::MdtPrepDataContainer>(update.ptr()));
+    if (status.isFailure() || !handle.isValid() )   {
+      ATH_MSG_FATAL("Could not record container of MDT PrepData Container using cache " 
+        << m_prdContainerCacheKey.key() << " - " <<m_mdtPrepDataContainerKey.key()); 
+      return FAILED;
+    }
+    ATH_MSG_DEBUG("Created container using cache for " << m_prdContainerCacheKey.key());
   }
+  // Pass the container from the handle
   m_mdtPrepDataContainer = handle.ptr();
   return ADDED;
 }
