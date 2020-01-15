522fc4f92ce8d4dd3872d1c259858f31777fcae3
Ruth Pottgen <ruth.poettgen@cern.ch>
2020-01-15 16:20:37 +0000

    Merge branch '21.0_Cosmics' into '21.0'
    Fixing digitization error message irony
    
    See merge request atlas/athena!29334
    
    (cherry picked from commit 21735050c5bc64a8efef05e5795a31d7876dd279)
    
    0c12ac8f Fixing error message irony

--- Simulation/Digitization/python/DigiConfigCheckers.py
+++ Simulation/Digitization/python/DigiConfigCheckers.py
@@ -22,7 +22,7 @@ def checkTRT_DigitizationConfiguration():
 
 
 def checkDetFlagConfiguration():
-    from AthenaCommon.Logging import logging
+    from AthenaCommon.Logging import logging,AthError
     logDigiConfigChecker = logging.getLogger( 'DigiConfigChecker' )
 
     from AthenaCommon.DetFlags import DetFlags
