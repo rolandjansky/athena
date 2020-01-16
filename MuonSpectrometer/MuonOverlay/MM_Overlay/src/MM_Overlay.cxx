9b61406aa58fc63453425da321603390fe787365
Tadej Novak <tadej.novak@cern.ch>
2020-01-16 12:34:18 +0100

    Remove redundant includeBkg flag from InDet and Muon overlay algorithms

--- MuonSpectrometer/MuonOverlay/MM_Overlay/src/MM_Overlay.cxx
+++ MuonSpectrometer/MuonOverlay/MM_Overlay/src/MM_Overlay.cxx
@@ -76,11 +76,6 @@ StatusCode MM_Overlay::initialize()
 {
   ATH_MSG_DEBUG("Initializing...");
 
-  if (!m_includeBkg) {
-    ATH_MSG_DEBUG("Disabling use of background RDOs...");
-    ATH_CHECK( m_bkgInputKey.assign("") );
-  }
-
   // Check and initialize keys
   ATH_CHECK( m_bkgInputKey.initialize(!m_bkgInputKey.key().empty()) );
   ATH_MSG_VERBOSE("Initialized ReadHandleKey: " << m_bkgInputKey );
@@ -98,7 +93,7 @@ StatusCode MM_Overlay::execute(const EventContext& ctx) const {
 
 
   const MmDigitContainer *bkgContainerPtr = nullptr;
-  if (m_includeBkg) {
+  if (!m_bkgInputKey.empty()) {
     SG::ReadHandle<MmDigitContainer> bkgContainer (m_bkgInputKey, ctx);
     if (!bkgContainer.isValid()) {
       ATH_MSG_ERROR("Could not get background MM container " << bkgContainer.name() << " from store " << bkgContainer.store());
