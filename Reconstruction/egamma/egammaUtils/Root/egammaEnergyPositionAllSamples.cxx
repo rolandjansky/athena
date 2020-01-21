af83c854f5365dc78b18732035024e3324366b15
christos <christos@cern.ch>
2020-01-21 20:57:00 +0000

    try to re-organise the if

--- Reconstruction/egamma/egammaUtils/Root/egammaEnergyPositionAllSamples.cxx
+++ Reconstruction/egamma/egammaUtils/Root/egammaEnergyPositionAllSamples.cxx
@@ -4,7 +4,6 @@
 
 #include "egammaUtils/egammaEnergyPositionAllSamples.h"
 #include "xAODCaloEvent/CaloCluster.h"
-#include <cmath>
 
 double
 egammaEnergyPositionAllSamples::e(const xAOD::CaloCluster& cluster)
@@ -33,61 +32,90 @@ egammaEnergyPositionAllSamples::e2(const xAOD::CaloCluster& cluster)
 double
 egammaEnergyPositionAllSamples::e3(const xAOD::CaloCluster& cluster)
 {
-
   return (cluster.eSample(CaloSampling::EMB3) + cluster.eSample(CaloSampling::EME3));
 }
 
 bool
-egammaEnergyPositionAllSamples::inBarrel(const xAOD::CaloCluster& cluster, const int is)
+egammaEnergyPositionAllSamples::inBarrel(const xAOD::CaloCluster& cluster, const int sampling)
 {
 
   // from cluster position and energy define
   // if we are in barrel or end-cap
   // In particular it uses energy deposit in 2nd sampling
   // to check close to the crack region
-  // is = sampling (0=presampler,1=strips,2=middle,3=back)
+  // sampling (0=presampler,1=strips,2=middle,3=back)
   //
-  bool in_barrel = true;
-
   if (cluster.inBarrel() && !cluster.inEndcap()) {
-    in_barrel = true;
+    return true;//barrel
   } else if (!cluster.inBarrel() && cluster.inEndcap()) {
-    in_barrel = false;
+    return  false;//endcap
   }
-  // if both in barrel and end-cap then have to
-  // rely on energy deposition
-  // be careful to test 0 precisely either
-  // 0 (no deposit) > noise (which is negative) !!!!
+  /*if both in barrel and end-cap then have to
+   * rely on energy deposition
+   * be careful to test 0 precisely
+   * As 0 (no deposit) > noise (which is negative)*/
   else if (cluster.inBarrel() && cluster.inEndcap()) {
-    if (
-      // case for PreSampler
-      ((is == 0 && ((cluster.eSample(CaloSampling::PreSamplerB) != 0. &&
-                     cluster.eSample(CaloSampling::PreSamplerE) != 0.) &&
-                    (cluster.eSample(CaloSampling::PreSamplerB) >=
-                     cluster.eSample(CaloSampling::PreSamplerE)))) ||
-       (cluster.eSample(CaloSampling::PreSamplerE) == 0.)) ||
-      // case for 1st sampling
-      ((is == 1 &&
-        ((cluster.eSample(CaloSampling::EMB1) != 0. && cluster.eSample(CaloSampling::EME1) != 0.) &&
-         (cluster.eSample(CaloSampling::EMB1) >= cluster.eSample(CaloSampling::EME1)))) ||
-       (cluster.eSample(CaloSampling::EME1) == 0.)) ||
-      // case for middle sampling
-      ((is == 2 &&
-        ((cluster.eSample(CaloSampling::EMB2) != 0. && cluster.eSample(CaloSampling::EME2) != 0.) &&
-         (cluster.eSample(CaloSampling::EMB2) >= cluster.eSample(CaloSampling::EME2)))) ||
-       (cluster.eSample(CaloSampling::EME2) == 0.)) ||
-      // case for back sampling
-      ((is == 3 &&
-        ((cluster.eSample(CaloSampling::EMB3) != 0. && cluster.eSample(CaloSampling::EME3) != 0.) &&
-         (cluster.eSample(CaloSampling::EMB3) >= cluster.eSample(CaloSampling::EME3)))) ||
-       (cluster.eSample(CaloSampling::EME3) == 0.)))
+    switch (sampling) {
+    case 0: {
+      if (cluster.eSample(CaloSampling::PreSamplerB) == 0. && cluster.eSample(CaloSampling::PreSamplerE) != 0.) {
+        return false; // endcap
+      }
+      if (cluster.eSample(CaloSampling::PreSamplerB) != 0. && cluster.eSample(CaloSampling::PreSamplerE) == 0.) {
+        return true; // endcap
+      }
+      if (cluster.eSample(CaloSampling::PreSamplerB) >= cluster.eSample(CaloSampling::PreSamplerE)) {
+        return true; // barrel;
+      } else {
+        return false; // endcap;
+      }
+    }
+    case 1: {
+      if (cluster.eSample(CaloSampling::EMB1) == 0. && cluster.eSample(CaloSampling::EME1) != 0.) {
+        return false; // endcap
+      }
+      if (cluster.eSample(CaloSampling::EMB1) != 0. && cluster.eSample(CaloSampling::EME1) == 0.) {
+        return true; // endcap
+      }
+      if (cluster.eSample(CaloSampling::EMB1) >= cluster.eSample(CaloSampling::EME1)) {
+        return true; // barrel
+      } else {
+        return false; // endcap
+      }
+    }
+    case 2: {
+      if (cluster.eSample(CaloSampling::EMB2) == 0. && cluster.eSample(CaloSampling::EME2) != 0.) {
+        return false; // endcap
+      }
+      if (cluster.eSample(CaloSampling::EMB2) != 0. && cluster.eSample(CaloSampling::EME2) == 0.) {
+        return true; // endca
+      }
 
-    {
-      in_barrel = true;
-    } else {
-      in_barrel = false;
+      if (cluster.eSample(CaloSampling::EMB2) >= cluster.eSample(CaloSampling::EME2)) {
+        return true; // barrel
+      } else {
+        return false; // endcap
+      }
+    }
+    case 3: {
+      if (cluster.eSample(CaloSampling::EMB3) == 0. && cluster.eSample(CaloSampling::EME3) != 0.) {
+        return false; // endcap
+      }
+      if (cluster.eSample(CaloSampling::EMB3) != 0. && cluster.eSample(CaloSampling::EME3) == 0.) {
+        return true; // endcap
+      }
+
+      if (cluster.eSample(CaloSampling::EMB3) >= cluster.eSample(CaloSampling::EME3)) {
+        return true; // barrel
+      } else {
+        return false; // endcap
+      }
+    }
+    default: {
+      return true; // barrel default
+    }
     }
   }
-  return in_barrel;
+
+  return true; // barrel defautl
 }
 
