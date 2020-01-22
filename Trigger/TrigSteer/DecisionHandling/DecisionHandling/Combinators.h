0e632d11c9e89ff684083d3672ef2e15ce0e8a20
Tim Martin <Tim.Martin@cern.ch>
2020-01-22 13:49:36 +0100

    minor typos in MR 29500

--- Trigger/TrigSteer/DecisionHandling/DecisionHandling/Combinators.h
+++ Trigger/TrigSteer/DecisionHandling/DecisionHandling/Combinators.h
@@ -2,10 +2,5 @@
   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef DECISIONHANDLING_COMBINATORS_H
-#define DECISIONHANDLING_COMBINATORS_H
-
 /// This has moved to a different package
 #include "TrigCompositeUtils/Combinators.h"
-
-#endif // DECISIONHANDLING_COMBINATORS_H
