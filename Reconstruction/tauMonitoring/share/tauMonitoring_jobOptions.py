c434309cc9c0b2ffe1a417287be3fd705253c611
scott snyder <snyder@bnl.gov>
2020-01-24 16:19:46 +0100

    tauMonitoring: python 3 fixes
     - print function

--- Reconstruction/tauMonitoring/share/tauMonitoring_jobOptions.py
+++ Reconstruction/tauMonitoring/share/tauMonitoring_jobOptions.py
@@ -15,7 +15,7 @@ from AthenaMonitoring.AtlasReadyFilterTool import GetAtlasReadyFilterTool
 LowStat = True
 if DQMonFlags.monManEnvironment() == 'online' :
     LowStat = False 
-print "doLowStat " , LowStat
+printfunc ("doLowStat " , LowStat)
 
 myTDT = None
 if DQMonFlags.useTrigger() and LowStat :
@@ -35,7 +35,7 @@ else :
     tauMonTool.doTrigger = DQMonFlags.useTrigger()
     tauMonTool.TrigDecisionTool = myTDT
 
-print "doTrigger " , tauMonTool.doTrigger
+printfunc ("doTrigger " , tauMonTool.doTrigger)
 
 if tauMonTool.doTrigger :
     tauMonTool.TauEleJetMbtsTriggerItems = [ [ "L1_TAU1.*" , "L1_TAU2.*" , "L1_TAU60" , "L1_EM15VHI_2TAU.*" , "L1_EM15VHI_TAU40.*" , "L1_MU10_TAU.*" , "L1_J25_3J12_EM15-TAU.*" ] , [ "HLT_e2.*" , "L1_EM2.*" , "L1_2EM15" ] , [ "HLT_j55" , "HLT_j60" , "HLT_j85" , "HLT_j110" , "HLT_j175" , "HLT_4j85" , "HLT_4j10.*" ] , [ "HLT_mb_sp2000_trk70_hmt" ] ]
