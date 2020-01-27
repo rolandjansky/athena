d5b1bc5ac716323de576ddbb408f410f101e1d97
Carlo Varni <cvarni@pc-tbed-pub-31.cern.ch>
2020-01-27 10:25:42 +0100

    Remove DEBUG Output Level

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Bjet/BjetFlavourTaggingConfiguration.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Bjet/BjetFlavourTaggingConfiguration.py
@@ -1,13 +1,12 @@
 
 from TrigEDMConfig.TriggerEDMRun3 import recordable
-from AthenaCommon.Constants import DEBUG
+#from AthenaCommon.Constants import DEBUG
 
 def getFlavourTagging( inputJets, inputVertex, inputTracks ):
     algSequence = []
     
     from TrigBjetHypo.TrigBjetHypoConf import TrigBtagFexMT
     bTagFex = TrigBtagFexMT( "TrigBtagFexMT" )
-    bTagFex.OutputLevel = DEBUG
     bTagFex.JetKey = inputJets
     bTagFex.PriVtxKey = inputVertex
     bTagFex.TracksKey = inputTracks
