b2eb4b8eed88f12779f7d8c9ea06dcc87e2ac6bb
Chara Kitsaki <chara.kitsaki@cern.ch>
2020-01-27 20:25:41 +0000

    Inserting ALines for the NSW

--- MuonSpectrometer/MuonDetDescr/MuonReadoutGeometry/MuonReadoutGeometry/MMReadoutElement.h
+++ MuonSpectrometer/MuonDetDescr/MuonReadoutGeometry/MuonReadoutGeometry/MMReadoutElement.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 /***************************************************************************
@@ -118,7 +118,14 @@ namespace MuonGM {
     
     /** set methods only to be used by MuonGeoModel */
     void setChamberLayer(int ml) {m_ml=ml;}
+
+    inline double getALine_rots() const;
+    inline double getALine_rotz() const;
+    inline double getALine_rott() const;
+    inline bool has_ALines() const;
+    void setDelta(double, double, double, double, double, double); //input: translations, rotations
   private:
+
 
     //MuonChannelDesign m_phiDesign;
     std::vector<MuonChannelDesign> m_etaDesign;
@@ -134,9 +141,29 @@ namespace MuonGM {
     double m_minHalfY; // 0.5*bottom length (active area)
     double m_maxHalfY; // 0.5*top length (active area)
 
+    double m_rots;
+    double m_rotz;
+    double m_rott;
+
+    bool m_hasALines;
+
+    HepGeom::Transform3D* m_delta;
+    
     // transforms (RE->layer)
     Amg::Transform3D m_Xlg[4];
   };
+
+  double MMReadoutElement::getALine_rots() const
+  { return m_rots;}
+
+  double MMReadoutElement::getALine_rotz() const
+  { return m_rotz;}
+
+  double MMReadoutElement::getALine_rott() const
+  { return m_rott;}
+
+  bool MMReadoutElement::has_ALines() const
+  { return m_hasALines;}
 
   inline int MMReadoutElement::surfaceHash( const Identifier& id ) const {
     return surfaceHash(manager()->mmIdHelper()->gasGap(id),0);
