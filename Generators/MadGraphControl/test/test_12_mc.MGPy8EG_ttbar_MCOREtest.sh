ffecb75af5f234514a72a03090063f998633fa36
Zach Marshall <ZLMarshall@lbl.gov>
2020-01-30 14:51:29 +0100

    Bugfix and test update for MGC
    With the removal of a function, I missed that it was being used in the
    helpers (this was a little evil: utils calling helpers calling utils).
    Fixed now.
    
    Adding to the tests some attempts to use the log parser for tests that
    create an EVNT file. This should let us catch more errors simply than
    those caught by panda.

--- Generators/MadGraphControl/test/test_12_mc.MGPy8EG_ttbar_MCOREtest.sh
+++ Generators/MadGraphControl/test/test_12_mc.MGPy8EG_ttbar_MCOREtest.sh
@@ -13,4 +13,9 @@ unset ATHENA_CORE_NUMBER # This is not an MT job!
 
 Gen_tf.py --ecmEnergy=13000. --maxEvents=-1 --firstEvent=1 --randomSeed=123456 --outputEVNTFile=EVNT.root --jobConfig=950112
 
-echo "art-result: $?"
+echo "art-result: $? generation"
+
+# Run tests on the log file
+env -u PYTHONPATH -u PYTHONHOME python3 /cvmfs/atlas.cern.ch/repo/sw/Generators/MCJobOptions/scripts/logParser.py -s -i log.generate
+
+echo "art-result: $? log-check"
