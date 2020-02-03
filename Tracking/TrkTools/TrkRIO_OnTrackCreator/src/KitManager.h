1c57c3d609a73e55d8ba34a9fcf393402c1bd44d
christos <christos@cern.ch>
2020-02-03 23:15:29 +0000

    update copyrights

--- Tracking/TrkTools/TrkRIO_OnTrackCreator/src/KitManager.h
+++ Tracking/TrkTools/TrkRIO_OnTrackCreator/src/KitManager.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 #ifndef _KitManager_H_
 #define _KitManager_H_
