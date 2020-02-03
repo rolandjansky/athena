0f3786437fae8c6bd25f989bc8178438f40173ac
christos <christos@cern.ch>
2020-02-03 20:01:48 +0000

    ATLASRECTS-5012. Remove instances of CaloDetDescrManager::instance()

--- Calorimeter/CaloUtils/src/CaloCellDetPos.cxx
+++ Calorimeter/CaloUtils/src/CaloCellDetPos.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 // for a given calorimeter layer, this class will convert detector eta,phi to Atlas eta,phi
