417d563a2052ef47d37fa61f65d247eaa4df676b
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2020-02-09 16:57:22 +0100

    add cvmfs path, where the .dat MG fragments are kept , to DATAPATH

--- Generators/EvgenJobTransforms/scripts/Gen_tf.py
+++ Generators/EvgenJobTransforms/scripts/Gen_tf.py
@@ -55,6 +55,8 @@ class EvgenExecutor(athenaExecutor):
         os.environ['LOCAL_INSTALL_DIR'] = (os.environ['JOBOPTSEARCHPATH']).split(":")[0]
         CommonCvmfsDir = '/cvmfs/atlas.cern.ch/repo/sw/Generators/MCJobOptions/common'
         os.environ["JOBOPTSEARCHPATH"] = CommonCvmfsDir+":"+os.environ["JOBOPTSEARCHPATH"]
+        datCvmfsDir = '/cvmfs/atlas.cern.ch/repo/sw/Generators/MCJobOptions/common/MadGraphControl/dat'
+        os.environ["DATAPATH"] = datCvmfsDir+":"+os.environ["DATAPATH"]
 #        msg.info("Using JOBOPTSEARCHPATH = '%s'" % os.environ["LOCAL_INSTALL_DIR"])
         dsidparam = (self._trf.argdict["jobConfig"].value).values()[0][0]
         # Adding cvmfs path to JOBOPTSEARCHPATH
