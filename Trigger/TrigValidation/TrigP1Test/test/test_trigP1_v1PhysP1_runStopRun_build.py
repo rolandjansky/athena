38443d8804106d725c7cc8ee653cb65eeba70537
Sara Alderweireldt <sara.alderweireldt@cern.ch>
2020-02-10 16:23:37 +0000

    Adding TrigP1Test v1PhysP1_runStopRun

---
+++ Trigger/TrigValidation/TrigP1Test/test/test_trigP1_v1PhysP1_runStopRun_build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# art-description: start/stop/start transition test with PhysicsP1_pp_run3_v1 menu
+# art-type: build
+# art-include: master/Athena
+# Skipping art-output which has no effect for build tests.
+# If you create a grid version, check art-output in existing grid tests.
+
+from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
+
+ex = ExecStep.ExecStep()
+ex.type = 'athenaHLT'
+ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
+ex.input = 'data'
+ex.args = '-i -M -ul -c "setMenu=\'PhysicsP1_pp_run3_v1\';"'
+ex.perfmon = False # perfmon currently not fully supported with athenaHLT -M
+
+# Trick to pass the transitions file into athenaHLT -i
+ex.job_options += ' < `find_data.py run-stop-run-saveHist.trans`'
+
+test = Test.Test()
+test.art_type = 'build'
+test.exec_steps = [ex]
+test.check_steps = CheckSteps.default_check_steps(test)
+
+# Extra merging pattern for logs produced with -ul option
+logmerge = test.get_step("LogMerge")
+logmerge.extra_log_regex = 'athenaHLT-.*-.*(.out|.err)'
+
+import sys
+sys.exit(test.run())
