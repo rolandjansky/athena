14f24e150831e09e9b8df0d1c0893a30d6c8c578
scott snyder <sss@karma>
2020-02-10 12:26:11 +0100

    DPDUtils: Remove obsolete thinning code.
    Remove some obsolete code that uses old (non-MT) thinning and which is also
    not used anywhere.

--- PhysicsAnalysis/DPDUtils/src/components/DPDUtils_entries.cxx
+++ PhysicsAnalysis/DPDUtils/src/components/DPDUtils_entries.cxx
@@ -1,11 +1,3 @@
-#include "DPDUtils/ZeeFilter.h"
-#include "DPDUtils/DPDPhotonFilter.h"
 #include "DPDUtils/DPDTagTool.h"
-#include "DPDUtils/ObjectThinner.h"
-
-DECLARE_COMPONENT( DPDPhotonFilter )
-DECLARE_COMPONENT( ZeeFilter )
-DECLARE_COMPONENT( ObjectThinner )
-
 DECLARE_COMPONENT( DPDTagTool )
 
