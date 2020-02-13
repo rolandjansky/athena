d6a03e2d3d534d3ae12435be384427005761284c
Michael Duehrssen <michael.duehrssen@cern.ch>
2020-02-13 09:47:11 +0100

    Add new package CaloSamplingFractionAnalysis for calorimeter sampling fraction extraction

---
+++ Simulation/Tools/CaloSamplingFractionAnalysis/share/LarEMSamplingFraction_analysis.C
@@ -0,0 +1,39 @@
+{
+  TFile* file = TFile::Open("LArEM_SF.root");
+  new TCanvas("Etot","Etot");
+  mytree->Draw("Sum$(energy_active_total+energy_inactive_total)");
+
+  new TCanvas("ELAr_hit","ELAr_hit");
+  mytree->Draw("(energy_hit[0]+energy_hit[1]+energy_hit[2]+energy_hit[3])");
+
+  //new TCanvas("SF","SF");
+  //mytree->Draw("Sum$(energy_active_total)/Sum$(energy_active_total+energy_inactive_total):mc_eta>>SF(14,0,1.4,20,0.15,0.25)","","colz");
+
+  //new TCanvas("LAr_barrel","LAr_barrel");
+  //mytree->Draw("(energy_active_total[0]+energy_active_total[1]+energy_active_total[2]+energy_active_total[3]):mc_eta>>LAr_barrel(56,0,1.4,50,7000,12000)","","colz");
+
+  new TCanvas("SF_LAr_barrel_calibhit","SF_LAr_barrel_calibhit");
+  mytree->Draw("(energy_active_total[0]+energy_active_total[1]+energy_active_total[2]+energy_active_total[3])/(energy_active_total[0]+energy_active_total[1]+energy_active_total[2]+energy_active_total[3]+energy_inactive_total[0]+energy_inactive_total[1]+energy_inactive_total[2]+energy_inactive_total[3]):mc_eta>>SF_LAr_barrel_calibhit(56,0,1.4,70,0.18,0.25)","","colz");
+  TH2* SF_LAr_barrel_calibhit=(TH2*)gDirectory->Get("SF_LAr_barrel_calibhit");
+  //ProfileX(const char* name = "_pfx", Int_t firstybin = 1, Int_t lastybin = -1, Option_t* option = "")
+  TProfile* SF_prof_calibhit=SF_LAr_barrel_calibhit->ProfileX();
+  SF_prof_calibhit->SetLineColor(2);
+  SF_prof_calibhit->Draw("same");
+  //TFitResultPtr* fitres=
+  SF_prof_calibhit->Fit("pol0","","",0.1,0.75);
+  SF_prof_calibhit->Fit("pol0","+","",0.85,1.35);
+  
+  new TCanvas("SF_LAr_barrel","SF_LAr_barrel");
+  mytree->Draw("(energy_hit[0]+energy_hit[1]+energy_hit[2]+energy_hit[3])/(energy_active_total[0]+energy_active_total[1]+energy_active_total[2]+energy_active_total[3]+energy_inactive_total[0]+energy_inactive_total[1]+energy_inactive_total[2]+energy_inactive_total[3]):mc_eta>>SF_LAr_barrel(56,0,1.4,70,0.16,0.23)","","colz");
+  TH2* SF_LAr_barrel=(TH2*)gDirectory->Get("SF_LAr_barrel");
+  //ProfileX(const char* name = "_pfx", Int_t firstybin = 1, Int_t lastybin = -1, Option_t* option = "")
+  TProfile* SF_prof=SF_LAr_barrel->ProfileX();
+  SF_prof->SetLineColor(2);
+  SF_prof->Draw("same");
+  //TFitResultPtr* fitres=
+  SF_prof->Fit("pol0","","",0.1,0.75);
+  SF_prof->Fit("pol0","+","",0.85,1.35);
+  
+  //mytree->Print();
+  
+}
---
+++ Simulation/Tools/CaloSamplingFractionAnalysis/src/LarEMSamplingFraction.h
@@ -0,0 +1,84 @@
+
+#ifndef LarEMSamplingFraction_H
+#define LarEMSamplingFraction_H 1
+
+//#include "GaudiKernel/HistoDef.h"
+#include "AthenaBaseComps/AthAlgorithm.h"
+#include "LArElecCalib/ILArfSampl.h"
+#include "TileConditions/TileInfo.h"
+#include "CaloIdentifier/TileID.h"
+#include "CaloIdentifier/CaloIdManager.h"
+
+#include "StoreGate/StoreGateSvc.h"
+#include "CaloIdentifier/CaloCell_ID.h"
+#include "CaloDetDescr/CaloDetDescrManager.h"
+
+#include <vector>
+#include <string>
+#include "TTree.h"
+
+using namespace std;
+
+class LarEMSamplingFraction  : public ::AthAlgorithm
+{
+
+ public:
+
+  LarEMSamplingFraction(const std::string& name, ISvcLocator* pSvcLocator);
+  virtual ~LarEMSamplingFraction();
+  virtual StatusCode initialize();
+  virtual StatusCode execute();
+  virtual StatusCode finalize();
+  
+ private:
+  
+  /// Default constructor: 
+  LarEMSamplingFraction();
+  
+  bool m_docells;
+  
+  int mc_pdg;
+	double mc_eta;
+	double mc_phi;
+	double mc_e;
+	double mc_pt;
+  
+  std::vector<Long64_t>* m_cell_identifier;
+  std::vector<float>*    m_cell_energy_reco;
+  std::vector<float>*    m_cell_energy_active_total_corrected;
+  std::vector<float>*    m_cell_energy_active_total;
+  std::vector<float>*    m_cell_energy_inactive_total;
+  std::vector<int>*      m_cell_sampling;
+  std::vector<float>*    m_cell_eta;
+  std::vector<float>*    m_cell_phi;
+
+  vector<float> *energy_reco;
+  vector<float> *energy_hit;
+
+  vector<float> *energy_inactive_total;
+  vector<float> *energy_inactive_em;
+  vector<float> *energy_inactive_nonem;
+  vector<float> *energy_inactive_inv;
+  vector<float> *energy_inactive_esc;
+  
+  vector<float> *energy_active_total_corrected;
+  vector<float> *energy_active_total;
+  vector<float> *energy_active_em;
+  vector<float> *energy_active_nonem;
+  vector<float> *energy_active_inv;
+  vector<float> *energy_active_esc;
+  
+  TTree* mytree =0;
+
+  vector<string> m_CalibrationHitContainerNames;
+
+  const CaloDetDescrManager* m_calo_dd_man; 
+  
+  const CaloCell_ID* m_calo_id;
+  
+  const TileInfo *m_tileInfo;
+  const TileID * m_tileID;
+  const DataHandle<ILArfSampl>   m_dd_fSampl;
+};
+
+#endif //> !LarEMSamplingFraction_H
