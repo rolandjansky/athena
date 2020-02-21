d4b4032c3668c52662b4ae2cc6f4638f848b5cfa
Rafal Bielski <rafal.bielski@cern.ch>
2020-02-21 20:00:32 +0100

    Move more legacy trigger tests to TrigValSteering framework

--- Trigger/TrigValidation/TriggerTest/test/test_trig_data_v7Primaries_build.py
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_data_v7Primaries_build.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
-# art-description: Legacy trigger test on MC with v7 primaries menu
+# art-description: Legacy trigger test on data with v7 primaries menu
 # art-type: build
 # art-include: master/Athena
 # Skipping art-output which has no effect for build tests.
 # If you create a grid version, check art-output in existing grid tests.
 
 from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
+from TrigValTools.TrigValSteering.Input import get_input
 
 ex = ExecStep.ExecStep()
 ex.type = 'athena'
-ex.job_options = 'TriggerTest/testCommonSliceAthenaTrigRDO.py'
-ex.input = 'ttbar_rel21'
-ex.args = '-c \'menu="Physics_pp_v7_primaries";enableCostMonitoring=False;RunningRTT=True;\''
+ex.job_options = 'TriggerJobOpts/runHLT_standalone_run2.py'
+ex.input = ''  # These JO expect BSRDOInput variable instead of normal athena input handling
+ex.explicit_input = True
+ex.args = '-c \'setMenu="Physics_pp_v7_primaries"; BSRDOInput=["{:s}"]\''.format(get_input('data').paths[0])
 
 test = Test.Test()
 test.art_type = 'build'
