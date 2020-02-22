78b60dc546f68b00bbfde1dc398754048dddf1b6
Zach Marshall <ZLMarshall@lbl.gov>
2020-02-22 03:47:15 +0100

    Patching up test 10

--- Generators/MadGraphControl/test/test_10_mc.aMC_ttbar_gridpackTest.sh
+++ Generators/MadGraphControl/test/test_10_mc.aMC_ttbar_gridpackTest.sh
@@ -17,7 +17,12 @@ cd ../
 mkdir run_generateFromGridpack
 cd run_generateFromGridpack
 
-Gen_tf.py --ecmEnergy=13000. --maxEvents=-1 --firstEvent=1 --randomSeed=123456 --outputTXTFile=test_lhe_events --jobConfig=950110 --inputGenConfFile=../run_makeGridpack/run_01_gridpack.tar.gz
+# In order to test out the new gridpack, we must run off of local files
+cp -r /cvmfs/atlas.cern.ch/repo/sw/Generators/MCJobOptions/950xxx/950110/ .
+# Wildcard match in order to check for errors in naming
+cp ../run_makeGridpack/mc*.tar.gz 950110/
+
+Gen_tf.py --ecmEnergy=13000. --maxEvents=-1 --firstEvent=1 --randomSeed=123456 --outputTXTFile=test_lhe_events --jobConfig=./950110
 
 echo "art-result: $? Gen_tf"
 
