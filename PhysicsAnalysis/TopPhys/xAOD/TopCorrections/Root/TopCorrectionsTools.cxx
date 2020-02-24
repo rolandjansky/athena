ea8c06a4b133aa48a05a9b9042c59040af53f113
Tomas Dado <tomas.dado@cern.ch>
2020-02-24 19:46:19 +0100

    Fixing problems with calo/trk jets

--- PhysicsAnalysis/TopPhys/xAOD/TopCorrections/Root/TopCorrectionsTools.cxx
+++ PhysicsAnalysis/TopPhys/xAOD/TopCorrections/Root/TopCorrectionsTools.cxx
@@ -10,24 +10,39 @@ using namespace TopCorrections;
 
 namespace top {
   // B-tagging naming tool
-  std::string bTagNamedSystCheck(std::shared_ptr<top::TopConfig> config, const std::string systCollection,
-                                 const std::string tagWP, bool verbose = false) {
+  std::string bTagNamedSystCheck(std::shared_ptr<top::TopConfig> config,
+                                 const std::string& systCollection,
+                                 const std::string& tagWP,
+                                 const bool isTrackJet,
+                                 const bool verbose = false) {
     //  TODO -- to be reworked to use new message level settings to be implemented
     if (verbose) ATH_MSG_INFO("Searching for " << systCollection << " for WP " << tagWP);
-    for (auto name : config->btagging_namedSysts(tagWP)) {
-      if (verbose) ATH_MSG_INFO("...Possible matches : ");
-      if (systCollection.find(betterBtagNamedSyst(name)) != std::string::npos) {
-        if (verbose) ATH_MSG_INFO("......Matched!");
-        if (systCollection.find("up") != std::string::npos) return name + "__1up";
-        else if (systCollection.find("down") != std::string::npos) return name + "__1down";
-        else return name;
+    if (isTrackJet) {
+      for (const std::string& name : config->trkjet_btagging_namedSysts(tagWP)) {
+        if (verbose) ATH_MSG_INFO("...Possible matches : ");
+        if (systCollection.find(betterBtagNamedSyst(name)) != std::string::npos) {
+          if (verbose) ATH_MSG_INFO("......Matched!");
+          if (systCollection.find("up") != std::string::npos) return name + "__1up";
+          else if (systCollection.find("down") != std::string::npos) return name + "__1down";
+          else return name;
+        }
+      }
+    } else {
+      for (const std::string& name : config->btagging_namedSysts(tagWP)) {
+        if (verbose) ATH_MSG_INFO("...Possible matches : ");
+        if (systCollection.find(betterBtagNamedSyst(name)) != std::string::npos) {
+          if (verbose) ATH_MSG_INFO("......Matched!");
+          if (systCollection.find("up") != std::string::npos) return name + "__1up";
+          else if (systCollection.find("down") != std::string::npos) return name + "__1down";
+          else return name;
+        }
       }
     }
     return "";
   }
 
   // B-tagging naming tool
-  std::string betterBtagNamedSyst(const std::string name) {
+  std::string betterBtagNamedSyst(const std::string& name) {
     // remove "FT_EFF_", spaces, and "-" in named systematics
     std::string str = "FT_EFF_";
     std::string out = name;
