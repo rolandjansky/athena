08cbb846351c7c476242dc8135fad610b748bea3
scott snyder <snyder@bnl.gov>
2020-02-24 16:10:27 +0100

    LArCafJobs: Python 3 / flake8 fixes
    Update to work with python 3.
        
    Also add flake8 checking and fix warnings.

--- LArCalorimeter/LArCafJobs/share/samplesNtuple_all.py
+++ LArCalorimeter/LArCafJobs/share/samplesNtuple_all.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 from LArCafJobs.LArCafJobsConfig import DefaultShapeDumper
 DefaultShapeDumper('LArShapeDumper', 'FREE', noiseSignifCut = -1, doShape = True, caloType = 'EMHECFCAL', dumpChannelInfos = True)
