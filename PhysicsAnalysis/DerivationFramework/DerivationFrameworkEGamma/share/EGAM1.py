318899b3f78be04d16407977cd6fecdb179d1a0f
scott snyder <sss@karma>
2020-02-25 16:24:10 +0100

    DerivationFrameworkEGamma: Update GenericTruthThinning for MT thinning.
    Convert GenericTruthThinning to use MT-compatible thinning.
    Update uses of it (pass stream name rather than thinning service).

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/share/EGAM1.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/share/EGAM1.py
@@ -29,10 +29,8 @@ EGAM1Stream.AcceptAlgs(["EGAM1Kernel"])
 
 #Special lines for thinning
 # Thinning service name must match the one passed to the thinning tools
-from AthenaServices.Configurables import ThinningSvc, createThinningSvc
 augStream = MSMgr.GetStream( streamName )
 evtStream = augStream.GetEventStream()
-svcMgr += createThinningSvc( svcName="EGAM1ThinningSvc", outStreams=[evtStream] )
 
 
 #====================================================================
@@ -276,7 +274,7 @@ truth_expression = '(' + truth_cond_WZH + ' ||  ' + truth_cond_lep +' || '+truth
 
 from DerivationFrameworkMCTruth.DerivationFrameworkMCTruthConf import DerivationFramework__GenericTruthThinning
 EGAM1TruthThinningTool = DerivationFramework__GenericTruthThinning(name                    = "EGAM1TruthThinningTool",
-                                                                   ThinningService         = "EGAM1ThinningSvc",
+                                                                   StreamName              = streamName,
                                                                    ParticleSelectionString = truth_expression,
                                                                    PreserveDescendants     = False,
                                                                    PreserveGeneratorDescendants     = True,
