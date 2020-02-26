2bbe40363ce9f3d7f8332f1fca3a618819d83adb
Nils Gillwald <n.gillwald@cern.ch>
2020-02-26 17:57:05 +0100

    Properly implement AsgConfigHelper::HelperString
    AsgConfigHelper::HelperString function threw logical runtime errors,
    as the former use via the template tried to instantiate a string with
    a nullpointer in the constructor, which is forbidden. Therefore,
    the function was implemented without use of the template.

--- PhysicsAnalysis/ElectronPhotonID/ElectronPhotonShowerShapeFudgeTool/Root/AsgElectronPhotonCorrectionConfigHelper.cxx
+++ PhysicsAnalysis/ElectronPhotonID/ElectronPhotonShowerShapeFudgeTool/Root/AsgElectronPhotonCorrectionConfigHelper.cxx
@@ -9,7 +9,7 @@
 #include <sstream>   
 
 namespace AsgConfigHelper{
-  
+
   template <typename T>
   bool strtof(const std::string& input, T& f){  
     int diff = 0 ;
@@ -30,8 +30,8 @@ namespace AsgConfigHelper{
       //if nor error
       if (last == std::string::npos) {
         static const asg::AsgMessaging msg("Egamma::AsgConfigHelper");
-	msg.msg(MSG::WARNING)<<" Improper comment format , inline comment should be enclosed between two #  "<<endmsg;
-	return false;
+	      msg.msg(MSG::WARNING)<<" Improper comment format , inline comment should be enclosed between two #  "<<endmsg;
+	      return false;
       }
       //else if between two "#" remove this part 
       diff = last - first ;
@@ -49,14 +49,14 @@ namespace AsgConfigHelper{
     if (env_input.size() > 0) {
       std::string::size_type end;
       do {
-	end = env_input.find(";");
-	T  myValue(0);
-	if(AsgConfigHelper::strtof(env_input.substr(0,end),myValue)){
-	  CutVector.push_back(myValue);
-	}
-	if (end != std::string::npos) {
-	  env_input= env_input.substr(end+1);
-	}
+	      end = env_input.find(";");
+	      T  myValue(0);
+	      if(AsgConfigHelper::strtof(env_input.substr(0,end),myValue)){
+	        CutVector.push_back(myValue);
+	      }
+	      if (end != std::string::npos) {
+	        env_input= env_input.substr(end+1);
+	      }
       } while (end != std::string::npos);     
     }
     return CutVector;
@@ -73,8 +73,24 @@ std::vector<float> AsgConfigHelper::HelperFloat(const std::string& input,  TEnv&
 std::vector<int> AsgConfigHelper::HelperInt(const std::string& input, TEnv& env){
   return AsgConfigHelper::Helper<int> (input, env);  
 }
+// template does not work for std::string because of the T myValue(0); declaration, so implement it again for std::string
 std::vector<std::string> AsgConfigHelper::HelperString(const std::string& input, TEnv& env){
-  return AsgConfigHelper::Helper<std::string> (input, env);  
+  std::vector<std::string> CutVector;
+  std::string env_input(env.GetValue(input.c_str(), ""));
+  if (env_input.size() > 0) {
+    std::string::size_type end;
+    do {
+	    end = env_input.find(";");
+	    std::string myValue("");
+	    if(AsgConfigHelper::strtof(env_input.substr(0,end),myValue)){
+	      CutVector.push_back(myValue);
+	    }
+	    if (end != std::string::npos) {
+	      env_input= env_input.substr(end+1);
+	    }
+    } while (end != std::string::npos);
+  }
+  return CutVector;
 }
 
 
