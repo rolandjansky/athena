786aa82c84d485abd64d697e24be8a1697f8b3c7
scott snyder <snyder@bnl.gov>
2020-02-28 18:03:37 +0100

    DataModelRoot: Get TClassEdit.h from RootUtils
    To work around a clang 9 bug.

--- Control/DataModelRoot/src/RootType.cxx
+++ Control/DataModelRoot/src/RootType.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 
@@ -8,7 +8,7 @@
 // ROOT  
 #include "TBaseClass.h"
 #include "TClass.h"
-#include "TClassEdit.h"
+#include "RootUtils/TClassEditRootUtils.h"
 #include "TClassTable.h"
 #include "TDataType.h"
 #include "TDataMember.h"
