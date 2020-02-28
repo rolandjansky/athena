f0099db72fb4cb107c668fa39ca94b527f33d876
scott snyder <snyder@bnl.gov>
2020-02-28 17:20:41 +0100

    RecExAlgs: Python 3 / flake8 fixes
    Update to work with python 3.
        
    Also add flake8 checking and fix warnings.

--- Reconstruction/RecExample/RecExAlgs/share/SimpleRawObjMaker_jobOptions.py
+++ Reconstruction/RecExample/RecExAlgs/share/SimpleRawObjMaker_jobOptions.py
@@ -3,5 +3,6 @@ from AthenaCommon.AlgSequence import AlgSequence
 job = AlgSequence()
 
 ## add our algorithm
+from AthenaCommon                       import CfgMgr
 job += CfgMgr.SimpleRawObjMaker()
 job.SimpleRawObjMaker.RawObjOutputName = "reallyRaw"
