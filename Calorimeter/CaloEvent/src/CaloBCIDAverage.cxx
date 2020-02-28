0c11bc73452043930fdbdfb7edde5839a5bcab0f
Scott Snyder <scott.snyder@cern.ch>
2020-02-28 11:25:54 +0000

    CaloEvent: Adjust CaloBCIDAverage for better efficiency.
    Adjust CaloBCIDAverage so that it holds its information in a flat vector,
    rather than in a map.  Add methods to retrieve data by HWIdentifier
    and index, in addition to offline Identifier.

--- Calorimeter/CaloEvent/src/CaloBCIDAverage.cxx
+++ Calorimeter/CaloEvent/src/CaloBCIDAverage.cxx
@@ -1,10 +1,11 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 #include "CaloEvent/CaloBCIDAverage.h"
 
 
-CaloBCIDAverage::CaloBCIDAverage(const LArMCSym* mcSym, std::unordered_map<unsigned, float>&& data):
+CaloBCIDAverage::CaloBCIDAverage(const LArMCSym* mcSym, std::vector<float>&& data):
   m_mcSym(mcSym),
-  m_avg(data) {}
-
+  m_avg(std::move(data))
+{
+}
