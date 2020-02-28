ea83d636cf0b846c7d8bdcb671067e74f9772baa
William Henry Taylor <william.henry.taylor@cern.ch>
2020-02-28 15:02:44 +0000

    Combine repeated menu setup functionality

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/LS2_v1.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/LS2_v1.py
@@ -9,6 +9,7 @@
 #['name', 'L1chainParts'=[], 'stream', 'groups', 'merging'=[], 'topoStartFrom'=False],
 
 from TriggerMenuMT.HLTMenuConfig.Menu.ChainDefInMenu import ChainProp
+from TriggerMenuMT.HLTMenuConfig.Menu.MenuPrescaleConfig import addSliceChainsToPrescales
 
 import TriggerMenuMT.HLTMenuConfig.Menu.MC_pp_run3_v1 as mc_menu
 import TriggerMenuMT.HLTMenuConfig.Menu.PhysicsP1_pp_run3_v1 as p1_menu
@@ -197,24 +198,7 @@ def setupMenu():
     # Random Seeded EB chains which select at the HLT based on L1 TBP bits
     TriggerFlags.EnhancedBiasSlice.signatures = TriggerFlags.EnhancedBiasSlice.signatures() + [ ]
 
-    signatureList=[]
-    for prop in dir(TriggerFlags):
-        if prop[-5:]=='Slice':
-            sliceName=prop
-            slice=getattr(TriggerFlags,sliceName)
-            if slice.signatures():
-                signatureList.extend(slice.signatures())
-            else:
-                log.debug('SKIPPING '+str(sliceName))
-    mySigList=[]
-    for allInfo in signatureList:
-        mySigList.append(allInfo[0])
-    mydict={}
-    for chain in mySigList:
-        mydict[chain]=[-1,0,0]
-    mydict.update(Prescales.HLTPrescales_cosmics)
-    from copy import deepcopy
-    Prescales.HLTPrescales_cosmics = deepcopy(mydict)
+    addSliceChainsToPrescales(TriggerFlags, Prescales.HLTPrescales_cosmics)
 
 
 Prescales = mc_menu.Prescales
--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/Physics_pp_run3_v1.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/Physics_pp_run3_v1.py
@@ -8,6 +8,7 @@
 # always required are: name, stream and groups
 #['name', 'L1chainParts'=[], 'stream', 'groups', 'merging'=[], 'topoStartFrom'=False],
 from TriggerMenuMT.HLTMenuConfig.Menu.ChainDefInMenu import ChainProp
+from TriggerMenuMT.HLTMenuConfig.Menu.MenuPrescaleConfig import addSliceChainsToPrescales
 
 PhysicsStream="Main"
 SingleMuonGroup = ['RATE:SingleMuon', 'BW:Muon']
@@ -117,24 +118,7 @@ def setupMenu():
     # Random Seeded EB chains which select at the HLT based on L1 TBP bits
     TriggerFlags.EnhancedBiasSlice.signatures = [ ]
 
-    signatureList=[]
-    for prop in dir(TriggerFlags):
-        if prop[-5:]=='Slice':
-            sliceName=prop
-            slice=getattr(TriggerFlags,sliceName)
-            if slice.signatures():
-                signatureList.extend(slice.signatures())
-            else:
-                log.debug('SKIPPING '+str(sliceName))
-    mySigList=[]
-    for allInfo in signatureList:
-        mySigList.append(allInfo[0])
-    mydict={}
-    for chain in mySigList:
-        mydict[chain]=[-1,0,0]
-    mydict.update(Prescales.HLTPrescales_cosmics)
-    from copy import deepcopy
-    Prescales.HLTPrescales_cosmics = deepcopy(mydict)
+    addSliceChainsToPrescales(TriggerFlags, Prescales.HLTPrescales_cosmics)
 
 
 class Prescales(object):
