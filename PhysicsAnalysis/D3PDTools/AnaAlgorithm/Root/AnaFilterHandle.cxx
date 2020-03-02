522b5a29ae831263156a1bc486189d0e0b9f7bee
Nils Krumnack <krumnack@iastate.edu>
2020-03-02 10:50:07 -0600

    add first version of a filter handle
    Just doing the bare minimum we need to do.  The next step would be to
    add instrumentation and a systematics-aware version.

---
+++ PhysicsAnalysis/D3PDTools/AnaAlgorithm/Root/AnaFilterHandle.cxx
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+*/
+
+/// @author Nils Krumnack
+
+
+//
+// includes
+//
+
+#include <AnaAlgorithm/AnaFilterHandle.h>
+
+#include <AsgMessaging/MessageCheck.h>
+#include <AsgMessaging/StatusCode.h>
+
+//
+// method implementations
+//
+
+namespace EL
+{
+  StatusCode AnaFilterHandle ::
+  initialize ()
+  {
+    assert (!m_isInitialized);
+
+    // currently not doing anything here, but presumably if we want to
+    // add instrumentation, we may/will need to do some initialization
+    // work here.
+
+    m_isInitialized = true;
+    return StatusCode::SUCCESS;
+  }
+
+
+
+  void AnaFilterHandle ::
+  beginExecute () noexcept
+  {
+    assert (m_isInitialized);
+    ANA_MSG_DEBUG ("resetting algorithm-filter-passed flag at beginning of execute()");
+    m_setFilterPassed (false);
+  }
+
+
+
+  void AnaFilterHandle ::
+  setPassed (bool val_passed) noexcept
+  {
+    assert (m_isInitialized);
+    ANA_MSG_DEBUG ("setting algorithm-filter-passed flag to " << val_passed);
+    m_setFilterPassed (val_passed);
+  }
+}
