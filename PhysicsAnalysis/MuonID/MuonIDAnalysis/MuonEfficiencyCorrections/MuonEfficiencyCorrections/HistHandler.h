f4280333ca2d624c4c667793259a9323b9a12658
Stefano Zambito <stefano.zambito@cern.ch>
2020-03-02 19:55:15 +0100

    Limiting number of warnings when dRJet decoration is missing

--- PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/MuonEfficiencyCorrections/HistHandler.h
+++ PhysicsAnalysis/MuonID/MuonIDAnalysis/MuonEfficiencyCorrections/MuonEfficiencyCorrections/HistHandler.h
@@ -246,13 +246,16 @@ namespace CP {
             CorrectionCode GetBinningParameter(const xAOD::Muon & mu, float & value) const override {
                 static const SG::AuxElement::ConstAccessor<float> dRJet("dRJet");
                 value = dRJet.isAvailable(mu) ? dRJet(mu) : -2;
-
-                if (!dRJet.isAvailable(mu)){
+		// We want these warnings to be printed few times per job, so that they're visible, then stop before log file's size blows up 
+                static unsigned int warned = 0;
+                if (warned<5 && !dRJet.isAvailable(mu)){
                     using namespace msgMuonEfficiency;
-                    ANA_MSG_WARNING("The dRJet decoration has not been found for the Muon. Isolation scale-factors are now binned in #Delta R(jet,#mu)");
+                    ANA_MSG_WARNING("The dRJet decoration has not been found for the Muon. Isolation scale-factors are now also binned in #Delta R(jet,#mu)");
                     ANA_MSG_WARNING("using the closest calibrated AntiKt4EMTopo jet with p_{T}>20~GeV and surving the standard OR criteria.");
-                    ANA_MSG_WARNING("Please decorate your muon appropiately before passing to the tool with dRJet = -1 in cases there is no jet in the event.");
+                    ANA_MSG_WARNING("You should decorate your muon appropiately before passing to the tool, and use dRJet = -1 in case there is no jet in an event.");
                     ANA_MSG_WARNING("For the time being the inclusive scale-factor is going to be returned.");
+		    ANA_MSG_WARNING("In future derivations, muons will also be decorated centrally with dRJet, for your benefit.");
+		    warned++;
                 }
                 return CorrectionCode::Ok;
             }
