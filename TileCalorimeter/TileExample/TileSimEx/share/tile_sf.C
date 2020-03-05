b8a0135fc1080545cd0f0b826c6c1515c5e9b0f6
Alexander Solodkov <Sanya.Solodkov@cern.ch>
2020-03-05 15:38:32 +0100

    delete old histograms before creating new ones

--- TileCalorimeter/TileExample/TileSimEx/share/tile_sf.C
+++ TileCalorimeter/TileExample/TileSimEx/share/tile_sf.C
@@ -5,30 +5,26 @@
 //
 //-----------------------------------------------------------------------
 
-#include <stdlib.h>
-#include <vector>
+#include <iostream>
+using namespace std;
 
-#include "TSystem.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TChain.h"
-#include "TH1.h"
-#include "TH2.h"
-#include "TProfile.h"
-#include "TF1.h"
 #include "TROOT.h"
 #include "TStyle.h"
+#include "TDirectory.h"
 #include "TString.h"
+#include "TFile.h"
+#include "TF1.h"
+#include "TH1F.h"
+#include "TTree.h"
 #include "TCanvas.h"
-#include "TVector3.h"
-#include "TPaveStats.h"
-
+#include "TProfile.h"
+#include "TProfile2D.h"
 
 void Make_plots(TString infile_hit, TString infile_truth, TString outfile, bool dofit) {
 
-  TTree *t = new TTree();
-  t->AddFriend("h32",infile_hit);
-  t->AddFriend("truth",infile_truth);
+  TTree t;
+  t.AddFriend("h32",infile_hit);
+  t.AddFriend("truth",infile_truth);
 
   //function - dependence of the total energy in scintillators on the impact point
   Float_t thefun_p0   = 3000.;
@@ -36,13 +32,16 @@ void Make_plots(TString infile_hit, TString infile_truth, TString outfile, bool 
   Float_t thefun_p2   = 18.;
   Float_t thefun_p3   = 0.;
 
-  TF1 * thefun = new TF1("thefun","[0]+[1]*cos((x-[3])*(2*TMath::Pi()/[2]))",-20.,20.);
+  TF1 *thefun = (TF1 *)gROOT->FindObject("thefun");
+  if (thefun) thefun->Delete();
+  thefun = new TF1("thefun","[0]+[1]*cos((x-[3])*(2*TMath::Pi()/[2]))",-20.,20.);
   thefun->SetLineColor(2);
 
   //************************************************************************
   // Histo definitions
   //************************************************************************
 
+  gDirectory->GetList()->Delete(); // delete old histograms first
   TH1F* h_totalE = new TH1F("h_totalE","Total hit energy in scintillators",600,0.,6000.);
   TH1F* h_ptgen = new TH1F("h_ptgen","Pt of the generated electron",121,-500.,120500.);
   TProfile2D* hp_EvsYvsZ = new TProfile2D("hp_EvsYvsZ","Total hit energy vs Y & Z",50,-25.,25.,50,-25.,25.,0.,100000.);
@@ -50,23 +49,25 @@ void Make_plots(TString infile_hit, TString infile_truth, TString outfile, bool 
 
   TObjArray HistArray(0);
 
-  TCanvas *c_g4=new TCanvas("c_g4","Tile TB simulation plots");
+  TCanvas *c_g4 = (TCanvas *)gROOT->FindObject("c_g4");
+  if (c_g4) delete c_g4;
+  c_g4 = new TCanvas("c_g4","Tile TB simulation plots");
   c_g4->Divide(2,2);
 
   c_g4->cd(1);
-  t->Draw("truth.mcpart_pt[0]>>h_ptgen","","histo");
+  t.Draw("truth.mcpart_pt[0]>>h_ptgen","","histo");
   h_ptgen->SetXTitle("Energy (MeV)");
   HistArray.Add(h_ptgen);
 
   c_g4->cd(2);
-  t->Draw("h32.totalE>>h_totalE","","histo");
+  t.Draw("h32.totalE>>h_totalE","","histo");
 
   //h_totalE->Fit("gaus","","");
   h_totalE->SetXTitle("Energy (MeV)");
   HistArray.Add(h_totalE);
 
   c_g4->cd(3);
-  t->Draw("(h32.totalE):(truth.mcvtx_y):(truth.mcvtx_z)>>hp_EvsYvsZ","","prof");
+  t.Draw("(h32.totalE):(truth.mcvtx_y):(truth.mcvtx_z)>>hp_EvsYvsZ","","prof");
   hp_EvsYvsZ->SetXTitle("ZVGen (mm)");
   hp_EvsYvsZ->SetYTitle("YVGen (mm)");
   //hp_EvsYvsZ->GetZaxis()->SetRangeUser(h_ptgen->GetMean()*0.01,h_ptgen->GetMean()*0.05);
@@ -74,7 +75,7 @@ void Make_plots(TString infile_hit, TString infile_truth, TString outfile, bool 
   HistArray.Add(hp_EvsYvsZ);
 
   c_g4->cd(4);
-  t->Draw("(h32.totalE):(truth.mcvtx_z)>>hp_totalEvsZ","","prof");
+  t.Draw("(h32.totalE):(truth.mcvtx_z)>>hp_totalEvsZ","","prof");
   hp_totalEvsZ->SetXTitle("ZVGen (mm)");
   hp_totalEvsZ->SetYTitle("Energy (MeV)");
   hp_totalEvsZ->GetYaxis()->SetRangeUser(h_ptgen->GetMean()*0.01,h_ptgen->GetMean()*0.05);
@@ -116,8 +117,8 @@ void Make_plots(TString infile_hit, TString infile_truth, TString outfile, bool 
   cout << "file name " << infile_hit << endl;
   cout << "beamE/total_E(hit) - no z correction " <<   (h_ptgen->GetMean())/(h_totalE->GetMean()) << endl;
   if (dofit) {
-    cout << "total_E(hits) with z correction " << thefun->GetParameter(0) << "+/-" << thefun->GetParError(0) << " dErr % " << thefun->GetParError(0)/thefun->GetParameter(0)*100 << endl;
-    cout << "1/SF value: " << (h_ptgen->GetMean())/(thefun->GetParameter(0)) << "+/-" << (h_ptgen->GetMean()/thefun->GetParameter(0))*(thefun->GetParError(0)/thefun->GetParameter(0)) << " dErr % " <<  (thefun->GetParError(0)/thefun->GetParameter(0))*100 << endl;
+    cout << "total_E(hits) with z correction " << thefun->GetParameter(0) << " +/- " << thefun->GetParError(0) << " dErr % " << thefun->GetParError(0)/thefun->GetParameter(0)*100 << endl;
+    cout << "1/SF value: " << (h_ptgen->GetMean())/(thefun->GetParameter(0)) << " +/- " << (h_ptgen->GetMean()/thefun->GetParameter(0))*(thefun->GetParError(0)/thefun->GetParameter(0)) << " dErr % " <<  (thefun->GetParError(0)/thefun->GetParameter(0))*100 << endl;
   }
 
   HistArray.Add(hp_totalEvsZ);
