f0ee11e33dff7191360be51c7020c379061110f2
sutt <sutt@cern.ch>
2020-03-13 00:36:17 +0100

    Slight code tidy up, removing unused variables and commented code
    Removed some of the class variables that are not needed for the algorithm
    to execute, mostly the histohram class member variables that were only
    included for diagnostic purposes to access the histograms from outside
    the findZ() method itself.

--- Trigger/TrigTools/IDScanZFinder/src/TrigZFinderInternal.h
+++ Trigger/TrigTools/IDScanZFinder/src/TrigZFinderInternal.h
@@ -31,9 +31,9 @@
 
 
 
-class TrigZFinderInternal
-{
-  public: 
+class TrigZFinderInternal {
+
+public: 
 
     struct vertex { 
       vertex( double z, double weight ) : _z(z), _weight(weight) { } 
@@ -41,7 +41,7 @@ class TrigZFinderInternal
       double _weight;
     };
 
-  public:
+public:
 
     TrigZFinderInternal( const std::string&, const std::string& );
     virtual ~TrigZFinderInternal(){};
@@ -51,18 +51,12 @@ class TrigZFinderInternal
     std::vector<vertex>* findZInternal( const std::vector< TrigSiSpacePointBase >& spVec, 
 					const IRoiDescriptor& roi ) const;
 
-  //    const std::vector< std::vector<long> >*   GetnHisto() { return m_nHisto; }
-  //    const std::vector< std::vector<double> >* GetzHisto() { return m_zHisto; }
-
-    //    long  GetNMax() { return m_NMax; } 
-
-    //  void setLayers(long maxLayers=20, long lastBarrelLayer=7) {
     void setLayers(long maxLayers, long lastBarrelLayer) {
       m_IdScan_MaxNumLayers = maxLayers;        // dphiEC depends on this value
       m_IdScan_LastBrlLayer = lastBarrelLayer;  // dphiBrl depends on this value
     } 
 
-  protected:  // member functions
+protected:  // member functions
 
     std::string getType() const { return m_type; }
     std::string getName() const { return m_name; }
@@ -80,17 +74,11 @@ class TrigZFinderInternal
 		      long& numPhiSlices ) const;
 
 
-  //    int GetInternalStatus() const { return m_Status; }
-  //    int SetInternalStatus(int s)  { m_Status = s; return m_Status; }
-
     double computeZV(double r1, double z1, double r2, double z2) const;
     double computeZV(double r1, double p1, double z1, double r2, double p2, double z2) const;
 
-  //    void   SetReturnValue(double d) { m_returnval=d; }
-  //    double GetReturnValue() const   { return m_returnval; }
 
-
-  protected:  // data members
+protected:  // data members
 
     /// maximum number of layers and last barrel layer 
     long   m_IdScan_MaxNumLayers; 
@@ -116,22 +104,15 @@ class TrigZFinderInternal
     std::string m_type;  // type information for internal book keeping
     std::string m_name;  // name information for the same
 
-  //    int    m_status;   // return status of the algorithm: 0=ok, -1=error
 
     bool m_chargeAware ;  // maintain separate sets of z histos for + & - tracks
     bool m_zHistoPerPhi;  // maintain one set of z histos per each phi slice
 
     double m_dphideta; // how, as a function of eta, the number of phi neighbours decreases
     double m_neighborMultiplier; // extra factor to manually increase the number of phi neighbors
-    //  long extraPhi[IdScan_MaxNumLayers][IdScan_MaxNumLayers]; // number of phi neighbours to look at
+
     std::vector< std::vector<long> > m_extraPhi; // ( IdScan_MaxNumLayers, std::vector<long>(IdScan_MaxNumLayers) ); // number of phi neighbours to look at
 
-    // access the ZFinder histogram from outside the findZInternal method
-
-    std::vector < std::vector<long>   >  m_nHisto[2]; // the actual z histogram count of pairs
-    std::vector < std::vector<double> >  m_zHisto[2]; // summed z position histograms
-
-    long m_NMax; // maximum number of z histogram entries
 
     int m_nFirstLayers;    // When the pairs of SPs are made, the inner SP comes from up to this "filled layer"
 
@@ -157,7 +138,7 @@ class TrigZFinderInternal
     double m_percentile;
 
 
-    /// to apply a hreshold to the found vertex candidates
+    /// to apply a threshold to the found vertex candidates
 
     double  m_weightThreshold;
 
