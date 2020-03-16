60b7d3b11ae976829d2045d1cde1a77053d2ebb1
Nils Krumnack <krumnack@iastate.edu>
2020-03-16 10:21:21 -0500

    rename filter handle/reporter classes
    Based on discussion in the merge request, reporter seems a better name
    than handle.

--- DataQuality/GoodRunsLists/GoodRunsLists/GRLSelectorAlg.h
+++ DataQuality/GoodRunsLists/GoodRunsLists/GRLSelectorAlg.h
@@ -6,7 +6,7 @@
 #define GOODRUNSLISTS_GRLSELECTORALG_H 1
 
 #include <AnaAlgorithm/AnaAlgorithm.h>
-#include <AnaAlgorithm/AnaFilterHandle.h>
+#include <AnaAlgorithm/FilterReporterParams.h>
 #include <AsgTools/ToolHandle.h> //included under assumption you'll want to use some tools! Remove if you don't!
 
 #include "AsgAnalysisInterfaces/IGoodRunsListSelectionTool.h"
@@ -29,7 +29,7 @@ class GRLSelectorAlg: public EL::AnaAlgorithm {
 
  private: 
   ToolHandle<IGoodRunsListSelectionTool> m_grlTool;
-  EL::AnaFilterHandle m_filterHandle {this};
+  EL::FilterReporterParams m_filterParams {this};
   int m_passed = 0;
   int m_total = 0;
 
