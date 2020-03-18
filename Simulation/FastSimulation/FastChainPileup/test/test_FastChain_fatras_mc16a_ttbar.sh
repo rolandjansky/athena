8da804cbf5f8a52cae7546c843d7faaf55b06c0a
Vincent Pascuzzi <Vincent.Pascuzzi@cern.ch>
2020-03-18 17:03:49 +0100

    Add back ART syntax for test

--- Simulation/FastSimulation/FastChainPileup/test/test_FastChain_fatras_mc16a_ttbar.sh
+++ Simulation/FastSimulation/FastChainPileup/test/test_FastChain_fatras_mc16a_ttbar.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 #
+# art-description: Run AFII simulation and full digitization of an MC16a ttbar sample with 2016a geometry and conditions, 25ns pile-up
+# art-type: grid
+# art-include: 21.3/Athena
+# art-output: *.root
+# art-output: config.txt
 
 HighPtMinbiasHitsFiles="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361239.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_high.merge.HITS.e4981_s3087_s3089/*"
 LowPtMinbiasHitsFiles="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361238.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_low.merge.HITS.e4981_s3087_s3089/*"
@@ -13,7 +18,7 @@ FastChain_tf.py \
     --enableLooperKiller True \
     --inputEVNTFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ISF_Validation/mc12_valid.110401.PowhegPythia_P2012_ttbar_nonallhad.evgen.EVNT.e3099.01517252._000001.pool.root.1 \
     --outputRDOFile RDO.pool.root \
-    --maxEvents 5 \
+    --maxEvents 10 \
     --skipEvents 0 \
     --geometryVersion default:ATLAS-R2-2016-01-00-01 \
     --conditionsTag default:OFLCOND-MC16-SDR-16 \
@@ -29,17 +34,17 @@ FastChain_tf.py \
     --numberOfLowPtMinBias '44.3839246425' \
     --numberOfCavernBkg 0 \
     --imf False
-# rc=$?
-# echo  "art-result: $rc EVNTtoRDO"
+rc=$?
+echo  "art-result: $rc EVNTtoRDO"
 
-# rc2=-9999
-# if [ ${rc} -eq 0 ]
-# then
-#     # Regression test
-#     ArtPackage=$1
-#     ArtJobName=$2
-#     art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
-#     rc2=$?
+rc2=-9999
+if [ ${rc} -eq 0 ]
+then
+    # Regression test
+    ArtPackage=$1
+    ArtJobName=$2
+    art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
+    rc2=$?
 
-# fi
-# echo  "art-result: ${rc2} regression"
+fi
+echo  "art-result: ${rc2} regression"
