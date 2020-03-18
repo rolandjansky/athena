bdbb02c401c04baafe5581f38a74c61f86eebaa1
fernando <Fernando.Monticelli@cern.ch>
2020-03-18 16:45:23 +0100

    Added noringer chains and fixed photon FastCalo seq

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/CommonSequences/CaloSequenceSetup.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/CommonSequences/CaloSequenceSetup.py
@@ -13,7 +13,7 @@ class CaloMenuDefs(object):
 
 
 
-def fastCaloSequence(ConfigFlags):
+def fastCaloSequence(doRinger):
     """ Creates L2 Fast Calo sequence"""
     # EV creator
     from TrigT2CaloCommon.CaloDef import fastCaloEVCreator
@@ -21,18 +21,18 @@ def fastCaloSequence(ConfigFlags):
 
     # reco sequence
     from TrigT2CaloCommon.CaloDef import fastCaloRecoSequence
-    (fastCaloInViewSequence, sequenceOut) = fastCaloRecoSequence(InViewRoIs, doRinger=True)
+    (fastCaloInViewSequence, sequenceOut) = fastCaloRecoSequence(InViewRoIs, doRinger=doRinger)
 
      # connect EVC and reco
     fastCaloSequence = seqAND("fastCaloSequence", [fastCaloViewsMaker, fastCaloInViewSequence ])
     return (fastCaloSequence, fastCaloViewsMaker, sequenceOut)
 
 
-def fastCaloMenuSequence(name):
+def fastCaloMenuSequence(name, doRinger=True):
     """ Creates L2 Fast Calo  MENU sequence
     The Hypo name changes depending on name, so for different implementations (Electron, Gamma,....)
     """
-    (sequence, fastCaloViewsMaker, sequenceOut) = RecoFragmentsPool.retrieve(fastCaloSequence, ConfigFlags)
+    (sequence, fastCaloViewsMaker, sequenceOut) = RecoFragmentsPool.retrieve(fastCaloSequence, doRinger)
 
     # hypo    
     from TrigEgammaHypo.TrigEgammaHypoConf import TrigL2CaloHypoAlgMT
