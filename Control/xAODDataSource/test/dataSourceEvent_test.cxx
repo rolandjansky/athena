961d5af9905565b1d1219aef037e4f455bfb4c05
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-03-20 16:28:11 +0100

    Added the xAODDataSource package from branch 21.2.
    With minimal modifications necessary for the newer CMake setup
    of this branch.

---
+++ Control/xAODDataSource/test/dataSourceEvent_test.cxx
@@ -0,0 +1,50 @@
+//
+// Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+//
+
+// Local include(s).
+#include "helpers.h"
+#include "../Root/RDataSourceEvent.h"
+
+// xAOD include(s).
+#include "xAODRootAccess/Init.h"
+#include "xAODRootAccess/tools/Message.h"
+
+// ROOT include(s).
+#include <TFile.h>
+#include <TError.h>
+
+// System include(s).
+#include <iostream>
+#include <memory>
+
+int main() {
+
+   // Set up the runtime environment.
+   CHECK( xAOD::Init() );
+
+   // Open the input file.
+   std::unique_ptr< TFile > ifile( TFile::Open( "${ASG_TEST_FILE_DATA}",
+                                                "READ" ) );
+   if( ( ! ifile ) || ifile->IsZombie() ) {
+      Error( "dataSourceEvent_test",
+             XAOD_MESSAGE( "Could not open the input file" ) );
+      return 1;
+   }
+
+   // Set up the event object.
+   xAOD::RDataSourceEvent event;
+   CHECK( event.readFrom( ifile.get() ) );
+   if( event.getEntry( 0 ) < 0 ) {
+      Error( "dataSourceEvent_test",
+             XAOD_MESSAGE( "Couldn't load the first event of the input "
+                           "file" ) );
+      return 1;
+   }
+
+   // Print the column names and their C++ types.
+   std::cout << event.columnAndTypeNames() << std::endl;
+
+   // Return gracefully.
+   return 0;
+}
