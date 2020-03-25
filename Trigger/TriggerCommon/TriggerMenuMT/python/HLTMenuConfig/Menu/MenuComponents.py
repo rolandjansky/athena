edd3095dcaa21ecb7b55af0c984877cb6c93fa88
Mark Owen <mark.andrew.owen@cern.ch>
2020-03-25 18:39:33 +0000

    Fix typo

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/MenuComponents.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/MenuComponents.py
@@ -1,4 +1,4 @@
-1;95;0c# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.Logging import logging
 log = logging.getLogger( __name__ )
