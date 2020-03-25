b483e23f8b4a958f86e19e0992c6b8039b8e5d9a
Tim Martin <Tim.Martin@cern.ch>
2020-03-25 20:06:04 +0100

    Add additional ViewCreator ROI tools

--- Trigger/TrigSteer/ViewAlgs/src/components/ViewAlgs_entries.cxx
+++ Trigger/TrigSteer/ViewAlgs/src/components/ViewAlgs_entries.cxx
@@ -6,6 +6,9 @@
 #include "../MergeViews.h"
 #include "../ViewCreatorInitialROITool.h"
 #include "../ViewCreatorPreviousROITool.h"
+#include "../ViewCreatorFSROITool.h"
+#include "../ViewCreatorFetchFromViewROITool.h"
+#include "../ViewCreatorCentredOnIParticleROITool.h"
 
 DECLARE_COMPONENT( EventViewCreatorAlgorithm )
 DECLARE_COMPONENT( EventViewCreatorAlgorithmWithJets )
@@ -13,3 +16,6 @@ DECLARE_COMPONENT( EventViewCreatorAlgorithmWithMuons )
 DECLARE_COMPONENT( MergeViews )
 DECLARE_COMPONENT( ViewCreatorInitialROITool )
 DECLARE_COMPONENT( ViewCreatorPreviousROITool )
+DECLARE_COMPONENT( ViewCreatorFSROITool )
+DECLARE_COMPONENT( ViewCreatorFetchFromViewROITool )
+DECLARE_COMPONENT( ViewCreatorCentredOnIParticleROITool )
