8661290d7388efcc5d997a5c387889519329804f
Nils Krumnack <krumnack@iastate.edu>
2020-03-26 09:08:32 -0500

    allow AsgToolConfig to retrieve tools with arbitrary interfaces
    That's sort of needed, not sure why I didn't do it before.

--- Control/AthToolSupport/AsgTools/Root/AsgToolConfig.cxx
+++ Control/AthToolSupport/AsgTools/Root/AsgToolConfig.cxx
@@ -25,18 +25,4 @@ namespace asg
   AsgToolConfig (const std::string& val_typeAndName)
     : AsgComponentConfig (val_typeAndName)
   {}
-
-
-
-  ::StatusCode AsgToolConfig ::
-  makeTool (std::unique_ptr<AsgTool>& tool) const
-  {
-    using namespace msgComponentConfig;
-
-    ANA_CHECK (makeComponentExpert (tool, "new %1% (\"%2%\")", true));
-    ANA_CHECK (tool->initialize());
-
-    ANA_MSG_DEBUG ("Created component of type " << type());
-    return StatusCode::SUCCESS;
-  }
 }
