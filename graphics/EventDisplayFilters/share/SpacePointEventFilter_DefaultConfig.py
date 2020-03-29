0bb099261bed9072e34dceb9d1bd582313d60db3
scott snyder <snyder@bnl.gov>
2020-03-29 18:20:48 +0200

    EventDisplayFilters: Python 3 / flake8 fixes
    Updates for python 3 compatibility.
    Enable flake8 checking and fix warnings.

--- graphics/EventDisplayFilters/share/SpacePointEventFilter_DefaultConfig.py
+++ graphics/EventDisplayFilters/share/SpacePointEventFilter_DefaultConfig.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+
 #####################################################################################
 # Add a filter algorithm that cuts on the number of SpacePoints
 #####################################################################################
