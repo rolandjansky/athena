e6fef6d57fc0e61ba5180234a18e743fca8697c2
Nils Krumnack <krumnack@iastate.edu>
2020-03-31 07:59:03 -0500

    make dedicated testing algorithm for use in gt_AnaAlgorithm
    Before I had been piggy-backing on the test algorithm developed for
    another test.  Unfortunately this doesn't quite work anymore for this
    test, so I just made a dedicated testing algorithm.

---
+++ PhysicsAnalysis/D3PDTools/EventLoopTest/EventLoopTest/EventLoopTestDict.h
@@ -0,0 +1,10 @@
+#ifndef EVENT_LOOP_TEST_EVENT_LOOP_TEST_DICT_H
+#define EVENT_LOOP_TEST_EVENT_LOOP_TEST_DICT_H
+
+#ifdef __GNUC__
+# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
+#include "EventLoopTest/UnitTestAlg5.h"
+
+#endif
