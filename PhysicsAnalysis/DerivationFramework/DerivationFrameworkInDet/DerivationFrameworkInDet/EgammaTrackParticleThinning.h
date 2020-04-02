afd6e128867cfa1f070b14d834d4ab5106d419e1
Goetz Gaycken <goetz.gaycken@cern.ch>
2020-04-02 14:58:30 +0200

    Migrate EgammaTrackParticleThinning to data handles.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/DerivationFrameworkInDet/EgammaTrackParticleThinning.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/DerivationFrameworkInDet/EgammaTrackParticleThinning.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -36,18 +36,26 @@ namespace DerivationFramework {
       virtual StatusCode doThinning() const override;
 
     private:
-      mutable std::atomic<unsigned int> m_ntot, m_ntotGSF, m_npass, m_nGSFPass;
+      mutable std::atomic<unsigned int> m_ntot {};
+      mutable std::atomic<unsigned int> m_ntotGSF {};
+      mutable std::atomic<unsigned int> m_npass {};
+      mutable std::atomic<unsigned int> m_nGSFPass {};
       StringProperty m_streamName
         { this, "StreamName", "", "Name of the stream being thinned" };
+      SG::ReadHandleKey<xAOD::EgammaContainer> m_egammaKey
+         { this, "SGKey", "" ""};
       SG::ThinningHandleKey<xAOD::TrackParticleContainer> m_inDetSGKey
         { this, "InDetTrackParticlesKey", "InDetTrackParticles", "" };
       SG::ThinningHandleKey<xAOD::TrackParticleContainer> m_gsfSGKey
         { this, "GSFTrackParticlesKey", "GSFTrackParticles", "" };
-      std::string m_sgKey;
-      std::string m_selectionString;
-      bool m_bestMatchOnly;
-      float m_coneSize;
-      ExpressionParsing::ExpressionParser *m_parser;
+      Gaudi::Property<std::string> m_selectionString
+        { this, "SelectionString", "", ""};
+      Gaudi::Property<bool> m_bestMatchOnly
+        { this, "BestMatchOnly", true, ""};
+      Gaudi::Property<float> m_coneSize
+        { this, "ConeSize", -1.0, ""};
+
+      std::unique_ptr<ExpressionParsing::ExpressionParser> m_parser;
       void setPhotonMasks(std::vector<bool>&, std::vector<bool>&, const xAOD::EgammaContainer*, const xAOD::TrackParticleContainer*, const xAOD::TrackParticleContainer*, const bool) const;
       void setPhotonMasks(std::vector<bool>&, std::vector<bool>&, std::vector<const xAOD::Egamma*>&, const xAOD::TrackParticleContainer*, const xAOD::TrackParticleContainer*, const bool) const;
       void setElectronMasks(std::vector<bool>&, std::vector<bool>&, const xAOD::EgammaContainer*, const xAOD::TrackParticleContainer*, const xAOD::TrackParticleContainer*, const bool) const;
