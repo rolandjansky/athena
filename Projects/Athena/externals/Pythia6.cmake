db73e5de88baddefeb91b7d862ef6f378aa008b2
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-04-03 11:27:45 +0200

    Removed the explicit setting of external (generator) versions.
    With LCG_97+ it is no longer necessary, as a given LCG release
    will only have a single version of everything.
    
    At the same time cleaned up the configuration of AthGeneration
    a little bit.

--- Projects/Athena/externals/Pythia6.cmake
+++
@@ -1,7 +0,0 @@
-#
-# File specifying the location of Pythia 6 to use.
-#
-
-set( PYTHIA6_LCGVERSION 429.2 )
-set( PYTHIA6_LCGROOT
-   ${LCG_RELEASE_DIR}/MCGenerators/pythia6/${PYTHIA6_LCGVERSION}/${LCG_PLATFORM} )
--- Projects/Athena/externals/YODA.cmake
+++
@@ -1,7 +0,0 @@
-#
-# File specifying the location of YODA to use.
-#
-
-set( YODA_LCGVERSION 1.8.0 )
-set( YODA_LCGROOT
-   ${LCG_RELEASE_DIR}/MCGenerators/yoda/${YODA_LCGVERSION}/${LCG_PLATFORM} )
