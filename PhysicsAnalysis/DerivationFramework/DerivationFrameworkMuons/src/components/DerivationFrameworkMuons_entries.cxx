8993d623402e7ba5ba52a62a35a1cc1304dc2a6e
Matthew Henry Klein <matthew.henry.klein@cern.ch>
2020-04-03 06:16:08 +0000

    Added muon jet dr tool for 2D muon SFs

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkMuons/src/components/DerivationFrameworkMuons_entries.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkMuons/src/components/DerivationFrameworkMuons_entries.cxx
@@ -7,6 +7,7 @@
 #include "DerivationFrameworkMuons/MuonIDCovMatrixDecorator.h"
 #include "DerivationFrameworkMuons/MuonTruthClassifierFallback.h"
 #include "DerivationFrameworkMuons/MuonTruthIsolationTool.h"
+#include "DerivationFrameworkMuons/MuonJetDrTool.h"
 #include "DerivationFrameworkMuons/MuonTPJpsiVertexFittingAlg.h"
 
 
@@ -22,6 +23,7 @@ DECLARE_TOOL_FACTORY (IDTrackCaloDepositsDecoratorTool)
 DECLARE_TOOL_FACTORY (MuonIDCovMatrixDecorator)
 DECLARE_TOOL_FACTORY (MuonTruthClassifierFallback)
 DECLARE_TOOL_FACTORY (MuonTruthIsolationTool)
+DECLARE_TOOL_FACTORY (MuonJetDrTool)
 DECLARE_ALGORITHM_FACTORY(VertexDecoratorAlg)
 DECLARE_ALGORITHM_FACTORY(MuonTPJpsiVertexFittingAlg)
 
@@ -35,6 +37,7 @@ DECLARE_FACTORY_ENTRIES( DerivationFrameworkMuons ) {
    DECLARE_TOOL(MuonIDCovMatrixDecorator)
    DECLARE_TOOL(MuonTruthClassifierFallback)
    DECLARE_TOOL(MuonTruthIsolationTool)
+   DECLARE_TOOL(MuonJetDrTool)
    
    DECLARE_ALGORITHM(MuonTPJpsiVertexFittingAlg)
    DECLARE_ALGORITHM(VertexDecoratorAlg)
