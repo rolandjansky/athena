a9bd67cc025eba0969b3dd19ac96c0df9435011c
Christian Gutschow <chris.g@cern.ch>
2020-04-08 16:58:24 +0100

    add OL_IGNORE_MODEL to EW_scheme_sinthetaW_mZ fragment

--- Generators/Sherpa_i/share/common/EW_scheme_sinthetaW_mZ.py
+++ Generators/Sherpa_i/share/common/EW_scheme_sinthetaW_mZ.py
@@ -12,9 +12,11 @@ genSeq.Sherpa_i.Parameters += [ "MODEL=SM_sinThetaW_mZ",
 ]
 
 ## Overwrite OL_PREFIX to not use it from cvmfs (which doesn't have this scheme implemented yet)
+## Also adding OL_IGNORE_MODEL (default to SM) since the scheme names aren't synchronised yet
 genSeq.Sherpa_i.Parameters += [
     "OL_PREFIX=./Process/OpenLoops",
     "OL_PARAMETERS=ew_renorm_scheme 22 write_parameters 1",
+    "OL_IGNORE_MODEL=1",
 ]
 
 
