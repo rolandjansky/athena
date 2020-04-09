b1e4dc124ed41d7fadefeed9844f104459fb2ba4
Andrii Verbytskyi <averbyts@cern.ch>
2020-04-09 22:49:09 +0200

    Helpers

--- Generators/TruthUtils/TruthUtils/HepMCHelpers.h
+++ Generators/TruthUtils/TruthUtils/HepMCHelpers.h
@@ -11,14 +11,10 @@
 
 #include "TruthUtils/PIDHelpers.h"
 #include "TruthUtils/TruthParticleHelpers.h"
-#include "MCUtils/HepMCUtils.h"
-#include "AtlasHepMC/GenParticle.h"
+#include "MCUtils/PIDUtils.h"
+#include "AtlasHepMC/GenEvent.h"
 
 namespace MC {
-
-  // Use the MCUtils and HEPUtils functions as if they were defined in the ATLAS MC and MC::PID namespaces
-  using namespace MCUtils;
-  using namespace HEPUtils;
 
 
   /// @name Extra ATLAS-specific particle classifier functions
@@ -29,7 +25,7 @@ namespace MC {
   /// The receipe for this is barcode < 200k and status = 1. Gen-stable particles decayed by
   /// G4 are not set to have status = 2 in ATLAS, but simply have more status = 1 children,
   /// with barcodes > 200k.
-  inline bool isGenStable(const HepMC::GenParticle* p) {
+  inline bool isGenStable(const HepMC::ConstGenParticlePtr p) {
     return isGenStable(p->status(), HepMC::barcode(p));
   }
 
@@ -38,7 +34,7 @@ namespace MC {
 
 
   /// @brief Identify if the particle is considered stable at the post-detector-sim stage
-  inline bool isSimStable(const HepMC::GenParticle* p) {
+  inline bool isSimStable(const HepMC::ConstGenParticlePtr p) {
     if (p->status() != 1) return false;
     if (isGenStable(p)) return p->end_vertex() == NULL;
     return true;
@@ -47,24 +43,26 @@ namespace MC {
   /// @brief Identify if the particle is considered stable at the post-detector-sim stage
   /// @todo I'm sure this shouldn't be exactly the same as isGenStable, but it is...
   /// @deprecated Use isSimulStable: this function _will_ be removed!
-  inline bool isGenSimulStable(const HepMC::GenParticle* p) {
+  inline bool isGenSimulStable(const HepMC::ConstGenParticlePtr p) {
     return isSimStable(p);
   }
 
 
   /// @brief Identify if the particle would not interact with the detector, i.e. not a neutrino or WIMP
-  inline bool isNonInteracting(const HepMC::GenParticle* p) {
+  inline bool isNonInteracting(const HepMC::ConstGenParticlePtr p) {
     return MC::isNonInteracting(p->pdg_id()); //< From TruthUtils/PIDHelpers.h
   }
 
-  inline bool isSimInteracting(const HepMC::GenParticle* p) {
+
+  /// @brief Identify if the particle could interact with the detector during the simulation, e.g. not a neutrino or WIMP
+  inline bool isSimInteracting(const HepMC::ConstGenParticlePtr p) {
     if (! MC::isGenStable(p)) return false; //skip particles which the simulation would not see
     return !MC::isNonInteracting(p);
   }
 
   /// @brief Oddly-named alias for isSimInteracting
   /// @deprecated Use isSimInteracting: this function _will_ be removed!
-  inline bool isGenInteracting(const HepMC::GenParticle* p) {
+  inline bool isGenInteracting(const HepMC::ConstGenParticlePtr p) {
     return isSimInteracting(p);
   }
 
