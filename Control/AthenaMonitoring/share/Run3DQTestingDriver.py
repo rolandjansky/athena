e3b8187c7047c4686069523d0e6d396221b63393
Walter Lampl <Walter.Lampl@cern.ch>
2020-04-10 16:34:47 +0200

    Implement run3-config of BadLBFilterTool, switch LArCellMon to use it, adjust Run3DQTestingDriver.py to run3 syntax. Fixes DQ CI-test

--- Control/AthenaMonitoring/share/Run3DQTestingDriver.py
+++ Control/AthenaMonitoring/share/Run3DQTestingDriver.py
@@ -90,12 +90,12 @@ if __name__=='__main__':
     # Force loading of conditions in MT mode
     if ConfigFlags.Concurrency.NumThreads > 0:
         from AthenaConfiguration.ComponentFactory import CompFactory
-        if len([_ for _ in cfg._conditionsAlgs if _.getName()=="PixelDetectorElementCondAlg"]) > 0:
+        if len([_ for _ in cfg._conditionsAlgs if _.name=="PixelDetectorElementCondAlg"]) > 0:
             beginseq = cfg.getSequence("AthBeginSeq")
-            beginseq += CompFactory.ForceIDConditionsAlg("ForceIDConditionsAlg")
-        if len([_ for _ in cfg._conditionsAlgs if _.getName()=="MuonAlignmentCondAlg"]) > 0:
+            beginseq.Members.append(CompFactory.ForceIDConditionsAlg("ForceIDConditionsAlg"))
+        if len([_ for _ in cfg._conditionsAlgs if _.name=="MuonAlignmentCondAlg"]) > 0:
             beginseq = cfg.getSequence("AthBeginSeq")
-            beginseq += CompFactory.ForceMSConditionsAlg("ForceMSConditionsAlg")
+            beginseq.Members.append(CompFactory.ForceMSConditionsAlg("ForceMSConditionsAlg"))
     
     # any last things to do?
     if args.postExec:
