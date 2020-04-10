a62e05f654a061c3e0c10e785a3bc0eaa982574a
Marcelo Vogel <mavogel@cern.ch>
2020-04-10 16:55:54 +0200

    This commit removes test for deprecated functionality, and fixes test by using AMI tag, which is resolved with pyAMI as opposed to PANDAClient

--- Tools/PyJobTransforms/test/test_trfAMI.py
+++ Tools/PyJobTransforms/test/test_trfAMI.py
@@ -33,26 +33,6 @@ class trfAMIUnitTests(unittest.TestCase):
         self.assertEqual(tag.trfs[0].release, 'AtlasProduction,17.2.0.3')
         self.assertEqual(tag.trfs[0].physics, physics)
 
-#    # test multiple transforms tag from ProdSys
-#    def test_info_a180(self):
-#        physics = {'conditionsTag':'OFLCOND-MC12-SIM-00',
-#                  'postInclude':'FastCaloSimHit/postInclude.AF2FilterHitItems.py FastSimulationJobTransforms/jobConfig.FastCaloSim_ID_cuts.py FastSimulationJobTransforms/jobConfig.egamma_lateral_shape_tuning.config20.py',
-#                  'DBRelease':'20.2.2',
-#                  'preInclude':'FastSimulationJobTransforms/jobConfig.v14_Parametrisation.py FastCaloSimHit/preInclude.AF2Hit.py',
-#                  'geometryVersion':'ATLAS-GEO-20-00-01'}
-
-
-#        tag = TagInfo('a180')
-#        self.assertEqual(len(tag.trfs), 2)
-#        self.assertTrue(isinstance(tag.trfs[0], TrfConfig))
-#        self.assertTrue(isinstance(tag.trfs[1], TrfConfig))
-#        self.assertEqual(tag.isProdSys, True)
-#        self.assertEqual(tag.trfs[0].name, 'Merging_trf.py')
-#        self.assertEqual(tag.trfs[0].release, '17.2.1.4.2,TrigMC')
-#        self.assertEqual(tag.trfs[0].physics, physics)
-#        self.assertEqual(tag.trfs[1].name, 'DigiMReco_trf.py')
-#        self.assertEqual(tag.trfs[1].release, '17.2.1.4.2,TrigMC')
-
     # test a new transform tag from AMI
     def test_info_q220(self):
         self.maxDiff = None
@@ -250,25 +230,26 @@ class trfAMIUnitTests(unittest.TestCase):
         self.assertEqual(tag.trfs[0].inDS, None)
         self.assertEqual(tag.trfs[0].outfmts, [])
 
-#    def test_transform(self):
-#        from PyJobTransforms.transform import transform
-#        from PyJobTransforms.trfArgClasses import argFactory, argString
-#        from PyJobTransforms.trfDefaultFiles import getInputFileName
+    def test_transform(self):
+        from PyJobTransforms.transform import transform
+        from PyJobTransforms.trfArgClasses import argFactory, argString, argBool
+        from PyJobTransforms.trfDefaultFiles import getInputFileName
 
-#        tf = transform()
-#        addAthenaArguments(tf.parser)
-#        tf.parser.add_argument('--inputBSFile', type = argFactory(argString))
-#        tf.parser.add_argument('--outputBSFile', type = argFactory(argString))
-#        tf.parser.add_argument('--extraArg', type = argFactory(argString))
+        tf = transform()
+        addAthenaArguments(tf.parser)
+        tf.parser.add_argument('--inputBSFile', type = argFactory(argString))
+        tf.parser.add_argument('--outputBS_MRGFile', type = argFactory(argString))
+        tf.parser.add_argument('--extraArg', type = argFactory(argString))
+        tf.parser.add_argument('--maskEmptyInputs', type=argFactory(argBool))
 
-#        tf.parseCmdLineArgs('--AMIConfig=p1346 --extraArg=special --outputBSFile=myOutput'.split())
+        tf.parseCmdLineArgs('--AMIConfig=m1831 --extraArg=special --outputBS_MRGFile=myOutput'.split())
 
-#        # not part of tag
-#        self.assertEqual(tf.argdict['extraArg'].value, 'special')
-#        #  part of tag
-#        self.assertEqual(tf.argdict['inputBSFile'].value, getInputFileName('inputBSFile'))
-#        #  part of tag but overwritten at command line
-#        self.assertEqual(tf.argdict['outputBSFile'].value, 'myOutput')
+        # not part of tag
+        self.assertEqual(tf.argdict['extraArg'].value, 'special')
+        #  part of tag
+        self.assertEqual(tf.argdict['inputBSFile'].value, getInputFileName('inputBSFile'))
+        #  part of tag but overwritten at command line
+        self.assertEqual(tf.argdict['outputBS_MRGFile'].value, 'myOutput')
 
 
 if __name__ == '__main__':
