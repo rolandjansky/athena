3e4a45a64661c1d93a1d16036e63598afd409619
Timothée Theveneaux-Pelzer <timothee.theveneaux-pelzer@cern.ch>
2020-04-14 18:27:50 +0200

    Implement suggestion for filepaths + remove modification in gridpack tarball making

--- Generators/MadGraphControl/python/MadGraphUtils.py
+++ Generators/MadGraphControl/python/MadGraphUtils.py
@@ -422,7 +422,7 @@ def generate(process_dir='PROC_mssm_0',grid_pack=False,gridpack_compile=False,ex
             ### NLO RUN ###
             mglog.info('Package up process_dir')
             os.rename(process_dir,MADGRAPH_GRIDPACK_LOCATION)
-            tar = subprocess.Popen(['tar','czf',gridpack_name,MADGRAPH_GRIDPACK_LOCATION,'--exclude={lib/PDFsets,Events/*/*events*.gz}'])
+            tar = subprocess.Popen(['tar','czf',gridpack_name,MADGRAPH_GRIDPACK_LOCATION,'--exclude=lib/PDFsets'])
             tar.wait()
             os.rename(MADGRAPH_GRIDPACK_LOCATION,process_dir)
 
@@ -951,9 +951,9 @@ def arrange_output(process_dir=MADGRAPH_GRIDPACK_LOCATION,lhe_version=None,saveP
     #isNLO=is_NLO_run(process_dir=process_dir)
 
 
-    if len(glob.glob(process_dir+'/Events/*'))<1:
+    if len(glob.glob(os.path.join(process_dir, 'Events','*')))<1:
         mglog.error('Process dir '+process_dir+' does not contain events?')
-    proc_dir_list = glob.glob(process_dir+'/Events/*')
+    proc_dir_list = glob.glob(os.path.join(process_dir, 'Events', '*'))
     this_run_name=None
     # looping over possible directories to find the right one
     for adir in proc_dir_list:
@@ -963,7 +963,7 @@ def arrange_output(process_dir=MADGRAPH_GRIDPACK_LOCATION,lhe_version=None,saveP
             if 'GridRun_' in adir:
                 this_run_name=adir
                 break # GridRun_* directories have priority
-            elif process_dir+'/Events/'+MADGRAPH_RUN_NAME in adir:
+            elif os.path.join(process_dir, 'Events',MADGRAPH_RUN_NAME) in adir:
                 this_run_name=adir
     if not os.access(this_run_name,os.R_OK):
         raise RuntimeError('Unable to locate run directory')
