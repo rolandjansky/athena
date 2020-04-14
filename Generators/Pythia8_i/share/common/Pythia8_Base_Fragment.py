92ab32339340abed8683d193f07f5ad1fd77aab1
Silke Mobius <silke.mobius@cern.ch>
2020-04-14 16:24:51 +0000

    Deleted commented out code in Pythia8_Base_Fragment.py

--- Generators/Pythia8_i/share/common/Pythia8_Base_Fragment.py
+++ Generators/Pythia8_i/share/common/Pythia8_Base_Fragment.py
@@ -3,31 +3,24 @@ from Pythia8_i.Pythia8_iConf import Pythia8_i
 genSeq += Pythia8_i("Pythia8")
 evgenConfig.generators += ["Pythia8"]
 
-## Old version of loading parameters
+## Load basic parameters
 genSeq.Pythia8.Commands += [
     "Main:timesAllowErrors = 500",
-#    "6:m0 = 172.5",
-#    "23:m0 = 91.1876",
-#    "23:mWidth = 2.4952",
-#    "24:m0 = 80.399",
-#    "24:mWidth = 2.085",
-#    "StandardModel:sin2thetaW = 0.23113",
-#    "StandardModel:sin2thetaWbar = 0.23146",
     "ParticleDecays:limitTau0 = on",
     "ParticleDecays:tau0Max = 10.0"]
 
-## New version to load parameters by including parameter dictionary 'parameters' in 'offline_dict'
+## Load parameters by including parameter dictionary 'parameters' in 'offline_dict'
 from EvgenProdTools.offline_dict import parameters
 for k,v in parameters.items():
     if k == 'particles':
         for key,value in v.items():
-# Only the top quark, the leptons and the bosons are included
+## Only the top quark, the leptons and the bosons are included
             if (int(key) in range(6,26)):
                 genSeq.Pythia8.Commands += [
                     str(key)+':m0 = '+str(value['mass']),
                     str(key)+':mWidth = '+str(value['width']),
                 ]
-# Only the parameters sin2thetaW and sin2thetaWbar are included
+## Only the parameters sin2thetaW and sin2thetaWbar are included
     if k == 'EW_parameters':
         for key,value in v.items():
             if key[1] == 'sin2thetaW' or key[1] == 'sin2thetaWbar':
