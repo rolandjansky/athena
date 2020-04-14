45e6a7e22dee4f5057a45141cda1730e7f8c41b2
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2020-04-14 11:47:07 +0000

    Merge branch 'usingNSWCalibInMMClusterBuilderTool' into '21.3'
    Using nsw calib in mm cluster builder tool
    
    See merge request atlas/athena!31853
    
    (cherry picked from commit a78a2812e6400a3a4f90db253c2812b5f455a983)
    
    595b4a29 debug crosstalk
    505b486b using MMprds driftDist and cov; removing toRad and vector printing; start code formating cleanup
    b65a92cd removing whitespace changes to make git diff less crowded
    2d7c1359 removing more white spaces
    53bf915a more less white spaces
    3debf60a less whitespaces
    9cc7d2cc fix compilation
    2fa8ef58 projection cluter builder tool: use drift dist and remove print vec
    27b6a66a updating the constraint angle cluster builder tool
    7406d917 Merge branch '21.3' of https://gitlab.cern.ch:8443/atlas/athena into...
    61fbf306 adding back uTPC job property which was deleted by accident

--- MuonSpectrometer/MuonReconstruction/MuonDataPrep/MMClusterization/src/UTPCMMClusterBuilderTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/MMClusterization/src/UTPCMMClusterBuilderTool.h
@@ -4,12 +4,16 @@
 #ifndef UTPCMMClusterBuilderTool_h
 #define UTPCMMClusterBuilderTool_h
 
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <string>
+
+
 #include "GaudiKernel/ToolHandle.h"
 #include "MMClusterization/IMMClusterBuilderTool.h"
 #include "MuonPrepRawData/MMPrepData.h"
 #include "AthenaBaseComps/AthAlgTool.h"
-
-#include <numeric>
 
 #include "TH2D.h"
 #include "TF1.h"
@@ -36,18 +40,18 @@ namespace Muon
   public:
     /** Default constructor */
     UTPCMMClusterBuilderTool(const std::string&, const std::string&, const IInterface*);
-     
+
     /** Default destructor */
     virtual ~UTPCMMClusterBuilderTool() = default;
 
     /** standard initialize method */
     virtual StatusCode initialize() override;
-    
+
     /** standard finalize method */
     //virtual StatusCode finalize();
 
     StatusCode getClusters(std::vector<Muon::MMPrepData>& MMprds, 
-			   std::vector<Muon::MMPrepData*>& clustersVec)const ;
+	 		   std::vector<Muon::MMPrepData*>& clustersVec)const ;
 
   private: 
 
@@ -61,25 +65,24 @@ namespace Muon
     double m_dMin,m_dMax,m_dResolution,m_driftRange;
     int m_houghMinCounts;
 
-    double m_timeOffset,m_dHalf,m_vDrift,m_transDiff,m_longDiff, m_ionUncertainty, m_scaleXerror, m_scaleYerror;
     double m_outerChargeRatioCut;
     int m_maxStripsCut;
 
     bool m_digiHasNegativeAngles;
     float m_scaleClusterError;
 
-    StatusCode runHoughTrafo(std::vector<int>& flag,std::vector<double>& xpos, std::vector<double>& time,std::vector<int>& idx_selected)const;
-    StatusCode fillHoughTrafo(std::unique_ptr<TH2D>& cummulator,std::vector<int>& flag, std::vector<double>& xpos, std::vector<double>& time)const;
-    StatusCode houghInitCummulator(std::unique_ptr<TH2D>& cummulator,double xmax,double xmin)const;
+    StatusCode runHoughTrafo(const std::vector<Muon::MMPrepData> &mmPrd, std::vector<double>& xpos, std::vector<int>& flag, std::vector<int>& idx_selected)const;
+    StatusCode fillHoughTrafo(const std::vector<Muon::MMPrepData> &mmPrd, std::vector<double>& xpos, std::vector<int>& flag,  std::unique_ptr<TH2D>& h_hough)const;
+    StatusCode houghInitCummulator(std::unique_ptr<TH2D>& cummulator, double xmax, double xmin)const;
 
     StatusCode findAlphaMax(std::unique_ptr<TH2D>& h_hough, std::vector<std::tuple<double,double>> &maxPos)const;
-    StatusCode selectTrack(std::vector<std::tuple<double,double>> &tracks,std::vector<double>& xpos, std::vector<double>& time,std::vector<int>& flag,std::vector<int>& idx_selected)const;
+    StatusCode selectTrack(const std::vector<Muon::MMPrepData> &mmPrd, std::vector<double>& xpos, std::vector<int>& flag, std::vector<std::tuple<double,double>> &tracks, std::vector<int> &idxGoodStrips)const;
 
-    StatusCode transformParameters(double alpha, double d, double dRMS, double& slope,double& intercept, double& interceptRMS)const;
+    StatusCode transformParameters(double alpha, double d, double dRMS, double& slope, double& intercept, double& interceptRMS)const;
     StatusCode applyCrossTalkCut(std::vector<int> &idxSelected,const std::vector<MMPrepData> &MMPrdsOfLayer,std::vector<int> &flag,int &nStripsCut)const;
-    StatusCode finalFit(const std::vector<Muon::MMPrepData> &mmPrd, std::vector<double>& time, std::vector<int>& idxSelected,double& x0, double &sigmaX0, double &fitAngle, double &chiSqProb)const;
-};
+    StatusCode finalFit(const std::vector<Muon::MMPrepData> &mmPrd, std::vector<int>& idxSelected,double& x0, double &sigmaX0, double &fitAngle, double &chiSqProb)const;
+  };
 
 
-}
+} //  namespace Muon
 #endif
