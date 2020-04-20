1b0e17cacb577990696ac1bd2ddb030b05a9fbf9
Marcin Wolter <marcin.wolter@cern.ch>
2020-04-20 17:39:04 +0200

    updtates for art tests ATLAS-P2-ITK-22-02-00

---
+++ InnerDetector/InDetExample/InDetSLHC_Example/test/test_ATLAS-P2-ITK-22-02-00_single_muons_1GeV.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# art-description: InclinedDuals sim+reco
+# art-type: grid
+# art-include: 21.9/Athena
+# art-output: *.root
+# art-output: *.xml
+# art-output: dcube*
+#
+#
+# Running art test for single muon, geometry ATLAS-P2-ITK-22-02-00
+#
+# resolve the script directory:
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+# and https://acode-browser2.usatlas.bnl.gov/lxr/source/AthAna/atlas/PhysicsAnalysis/TauID/TauCorrUncert/setup.sh
+#
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../share"  >/dev/null 2>&1 && pwd )"
+
+
+
+file=${SCRIPT_DIR}/test_ITk_single_particle.sh
+if [ -f "$file" ]
+then
+	echo "Executing script $file"
+        echo " "
+        "$file" muons 1GeV  ATLAS-P2-ITK-22-02-00
+else
+	echo "ERROR: script $file not found."
+fi
+
+
---
+++ InnerDetector/InDetExample/InDetSLHC_Example/test/test_InclinedAlternative_17-06-00_single.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+# art-description: InclinedDuals sim+reco
+# art-type: grid
+# art-include: 21.9/Athena
+# art-output: *.root
+# art-output: *.xml
+# art-output: dcube*
+
+echo "Input Parameters"
+
+particle=$1
+energy=$2
+geometry=$3
+echo 'particle type '$particle' energy '$energy' geometry '${geometry} 
+
+exit
+
+
+# Fix ordering of output in logfile
+exec 2>&1
+run() { (set -x; exec "$@") }
+
+# Following specify which steps to run.
+dosim=1
+dorec=1    # Reco_tf.py not yet working
+dophy=1    # If dorec=0, set dophy=1 to run InDetPhysValMonitoring over old ESD
+
+# Following specify DCube output directories. Set empty to disable.
+dcube_sim_fixref="dcube_sim_${particle}_${energy}"
+dcube_sim_lastref="dcube_sim_${particle}_${energy}_last"
+dcube_digi_pixel_fixref="dcube_digi_pixel_${particle}_${energy}"
+dcube_digi_pixel_lastref="dcube_digi_pixel_${particle}_${energy}_last"
+dcube_digi_strip_fixref="dcube_digi_strip_${particle}_${energy}"
+dcube_digi_strip_lastref="dcube_digi_strip_${particle}_${energy}_last"
+dcube_rec_fixref="dcube_${particle}_${energy}"
+dcube_rec_lastref="dcube_${particle}_${energy}_last"
+
+artdata=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art
+#artdata=/eos/atlas/atlascerngroupdisk/data-art/grid-input
+
+name="InclinedDuals"
+script="`basename \"$0\"`"
+
+# Set the input file
+if [ $particle == "muons" -a $energy == "1GeV" ]; then
+  evnt_particle_energy=$artdata/InDetSLHC_Example/inputs/EVNT.09244569.*.pool.root.1
+elif [ $particle == "muons" -a $energy == "10GeV" ]; then
+  evnt_particle_energy=$artdata/InDetSLHC_Example/inputs/EVNT.09244573.*.pool.root.1
+elif [ $particle == "muons" -a $energy == "100GeV" ]; then
+  evnt_particle_energy=$artdata/InDetSLHC_Example/inputs/EVNT.09244578.*.pool.root.1
+elif [ $particle == "electrons" -a $energy == "100GeV" ]; then
+  evnt_particle_energy=$artdata/InDetSLHC_Example/inputs/electrons_100GeV_EVNT.09244567._000004.pool.root.1
+fi
+
+
+
+if [ $dosim -ne 0 ]; then
+  hits_particle_energy=physval_${particle}_${energy}.HITS.root
+else
+  echo "Sim job not configured to run... no HITS input available for reco step, exiting test!"
+  exit
+###  hits_muons_1GeV="$hits_ref_muons_1GeV"
+fi
+if [ $dorec -ne 0 ]; then
+  esd_particle_energy=physval_${particle}_${energy}.ESD.root
+  daod_particle_energy=physval_${particle}_${energy}.DAOD_IDTRKVALID.root
+else
+  echo "Sim job not configured to run... no HITS input available for reco step, exiting test!"
+  exit
+###   esd_muons_1GeV=$artdata/InDetSLHC_Example/inputs/InclinedDuals_ESD_mu_1GeV.root
+###   daod_muons_1GeV=$artdata/InDetSLHC_Example/inputs/physval_muons_1GeV.DAOD_IDTRKVALID.root
+fi
+#jo=$artdata/InDetSLHC_Example/jobOptions/PhysValITk_jobOptions.py moved to share/
+dcubemon_sim=SiHitValid_${particle}_${energy}.root
+dcubemon_digi_pixel=PixelRDOAnalysis_${particle}_${energy}.root
+dcubemon_digi_strip=SCT_RDOAnalysis_${particle}_${energy}.root
+dcubemon_rec=physval_${particle}_${energy}.root
+dcubecfg_sim=$artdata/InDetSLHC_Example/dcube/config/InclinedDuals_SiHitValid.xml 
+dcubecfg_digi_pixel=$artdata/InDetSLHC_Example/dcube/config/InclinedDuals_PixelRDOAnalysis.xml 
+dcubecfg_digi_strip=$artdata/InDetSLHC_Example/dcube/config/InclinedDuals_SCT_RDOAnalysis.xml 
+dcubecfg_rec=$artdata/InDetSLHC_Example/dcube/config/InclinedDuals_physval.xml
+dcuberef_sim=$artdata/InDetSLHC_Example/ReferenceHistograms/InclinedDuals_${particle}_${energy}_SiHitValid.root
+dcuberef_rec=$artdata/InDetSLHC_Example/ReferenceHistograms/InclinedDuals_${particle}_${energy}_physval.root
+dcuberef_digi_pixel=$artdata/InDetSLHC_Example/ReferenceHistograms/PixelRDOAnalysis_${particle}_${energy}.root
+dcuberef_digi_strip=$artdata/InDetSLHC_Example/ReferenceHistograms/SCT_RDOAnalysis_${particle}_${energy}.root
+art_dcube=/cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube
+lastref_dir=last_results_${particle}_${energy}
+
+
+
+dcube() {
+  # Run DCube and print art-result (if $2 is not empty)
+  step="$1" statname="$2" dcubemon="$3" dcubecfg="$4" dcuberef="$5" dcubedir="$6"
+  test -n "$dcubedir" || return
+  run ls -lLU "$art_dcube" "$dcubemon" "$dcubecfg" "$dcuberef"
+  if [ ! -s "$dcubemon" ]; then
+    echo "$script: $step output '$dcubemon' not created. Don't create $dcubedir output." 2>&1
+    test -n "$statname" && echo "art-result: 20 $statname"
+    return
+  fi
+  if [ ! -s "$dcuberef" ]; then
+    echo "$script: $step DCube reference '$dcuberef' not available. Don't create $dcubedir output." 2>&1
+    test -n "$statname" && echo "art-result: 21 $statname"
+    return
+  fi
+  echo "$script: DCube comparison of $step output '$dcubemon' with '$dcuberef' reference"
+  keep=""
+  test "$dcubedir" != "dcube" -a -d "dcube" && keep="dcube_keep_`uuidgen`"
+  test -n "$keep" && run mv -f dcube "$keep"
+  run "$art_dcube" "$name" "$dcubemon" "$dcubecfg" "$dcuberef"
+  dcube_stat=$?
+  test -n "$statname" && echo "art-result: $dcube_stat $statname"
+  test "$dcubedir" != "dcube" && run mv -f dcube "$dcubedir"
+  test -n "$keep" && run mv -f "$keep" dcube
+}
+
+if [ $dosim -ne 0 ]; then
+
+  ## this runs single particle simulation
+  run ls -lL "$evnt_${particle}_${energy}"
+
+  # Sim step: based on RTT SimInclinedDuals_GMX job:
+  run Sim_tf.py \
+    --inputEVNTFile   $evnt_particle_energy \
+    --outputHITSFile  $hits_particle_energy \
+    --skipEvents      0 \
+    --maxEvents       10000 \
+    --randomSeed      873254 \
+    --geometryVersion ${geometry}_VALIDATION \
+    --conditionsTag   OFLCOND-MC15c-SDR-14-04 \
+    --truthStrategy   MC15aPlus \
+    --DataRunNumber   242000 \
+    --preInclude  all:'InDetSLHC_Example/preInclude.SLHC.py,InDetSLHC_Example/preInclude.SiliconOnly.py,InDetSLHC_Example/preInclude.SLHC_Setup_InclBrl_4.py,InDetSLHC_Example/preInclude.SLHC_Setup_Strip_GMX.py' \
+    --preExec     all:'from InDetSLHC_Example.SLHC_JobProperties import SLHC_Flags; SLHC_Flags.doGMX.set_Value_and_Lock(True); SLHC_Flags.LayoutOption="InclinedAlternative"' \
+    --postInclude all:'PyJobTransforms/UseFrontier.py,InDetSLHC_Example/postInclude.SLHC_Setup_InclBrl_4.py,InDetSLHC_Example/postInclude.SLHC_Setup.py,InDetSLHC_Example/postInclude.SiHitAnalysis.py' \
+    --postExec    EVNTtoHITS:'ServiceMgr.DetDescrCnvSvc.DoInitNeighbours = False; from AthenaCommon import CfgGetter;CfgGetter.getService("ISF_MC15aPlusTruthService").BeamPipeTruthStrategies+=["ISF_MCTruthStrategyGroupIDHadInt_MC15"];'
+
+
+  echo "art-result: $? sim"
+ 
+
+  
+ 
+  mv ./SiHitValid.root ./$dcubemon_sim
+
+
+  if [ \( $dosim -ne 0 -a -n "$dcube_sim_lastref" \) -o \( $dophy -ne 0 -a -n "$dcube_rec_lastref" \) ]; then
+    run art.py download --user=artprod --dst="$lastref_dir" InDetSLHC_Example "$script"
+    run ls -la "$lastref_dir"
+  fi
+
+
+  # DCube Sim hit plots
+  dcube Sim_tf sim-plot "$dcubemon_sim" "$dcubecfg_sim" "$lastref_dir/$dcubemon_sim" "$dcube_sim_lastref"
+  dcube Sim_tf ""       "$dcubemon_sim" "$dcubecfg_sim"                         "$dcuberef_sim" "$dcube_sim_fixref"
+  
+fi
+
+if [ $dorec -ne 0 ]; then
+
+  ## Starting reconstruction for single particles
+  if [ $dosim -ne 0 ] && [ ! -s "$hits_particle_energy" ] ; then
+    echo "$script: Sim_tf output '$hits_particle_energy' not created. Not running Reco_tf and stopping" 2>&1
+    exit
+  fi
+
+  run ls -lL "$hits_particle_energy"
+
+  # Reco step: based on RecoInclinedDuals_GMX RTT job
+  # some preExecs for E2D step are workarounds - should be revisited. 
+  run Reco_tf.py \
+      --inputHITSFile    "$hits_particle_energy" \
+      --outputRDOFile    "physval_${particle}_${energy}.RDO.root" \
+      --outputESDFile    "$esd_particle_energy" \
+      --outputAODFile    "physval_${particle}_${energy}.AOD.root" \
+      --outputDAOD_IDTRKVALIDFile "$daod_particle_energy" \
+      --maxEvents        -1 \
+      --digiSteeringConf StandardInTimeOnlyTruth \
+      --geometryVersion  ${geometry} \
+      --conditionsTag    OFLCOND-MC15c-SDR-14-04 \
+      --DataRunNumber    242000 \
+      --steering doRAWtoALL \
+      --postInclude all:'PyJobTransforms/UseFrontier.py,InDetSLHC_Example/postInclude.SLHC_Setup_InclBrl_4.py,InDetSLHC_Example/postInclude.SLHC_Setup.py' \
+      HITtoRDO:'InDetSLHC_Example/postInclude.SLHC_Digitization_lowthresh.py,InDetSLHC_Example/postInclude.RDOAnalysis.py' \
+      RAWtoALL:'InDetSLHC_Example/postInclude.DigitalClustering.py' \
+      --preInclude  all:'InDetSLHC_Example/preInclude.SLHC_Setup_InclBrl_4.py,InDetSLHC_Example/preInclude.SLHC_Setup_Strip_GMX.py,InDetSLHC_Example/preInclude.SLHC_Calorimeter_mu0.py' \
+      HITtoRDO:'InDetSLHC_Example/preInclude.SLHC.py,InDetSLHC_Example/preInclude.SiliconOnly.py' \
+      default:'InDetSLHC_Example/preInclude.SLHC.SiliconOnly.Reco.py,InDetSLHC_Example/SLHC_Setup_Reco_TrackingGeometry_GMX.py' \
+      RDOMergeAthenaMP:'InDetSLHC_Example/preInclude.SLHC.py,InDetSLHC_Example/preInclude.SiliconOnly.pyInDetSLHC_Example/SLHC_Setup_Reco_TrackingGeometry_GMX.py' \
+      POOLMergeAthenaMPAOD0:'InDetSLHC_Example/preInclude.SLHC.SiliconOnly.Ana.py,InDetSLHC_Example/SLHC_Setup_Reco_Alpine.py' \
+      POOLMergeAthenaMPDAODIDTRKVALID0:'InDetSLHC_Example/preInclude.SLHC.SiliconOnly.Ana.py,InDetSLHC_Example/SLHC_Setup_Reco_Alpine.py' \
+      --preExec     all:'from AthenaCommon.GlobalFlags import globalflags; globalflags.DataSource.set_Value_and_Lock("geant4"); from InDetSLHC_Example.SLHC_JobProperties import SLHC_Flags; SLHC_Flags.doGMX.set_Value_and_Lock(True); SLHC_Flags.LayoutOption="InclinedAlternative";' \
+      HITtoRDO:'from Digitization.DigitizationFlags import digitizationFlags; digitizationFlags.doInDetNoise.set_Value_and_Lock(False); digitizationFlags.doBichselSimulation.set_Value_and_Lock(False); digitizationFlags.overrideMetadata+=["SimLayout","PhysicsList"];' \
+      RAWtoALL:'from InDetRecExample.InDetJobProperties import InDetFlags; InDetFlags.doStandardPlots.set_Value_and_Lock(True);from PixelConditionsServices.PixelConditionsServicesConf import PixelCalibSvc;ServiceMgr +=PixelCalibSvc();InDetFlags.useDCS.set_Value_and_Lock(True);ServiceMgr.PixelCalibSvc.DisableDB=True;from InDetPrepRawDataToxAOD.InDetDxAODJobProperties import InDetDxAODFlags;InDetDxAODFlags.DumpLArCollisionTime.set_Value_and_Lock(False);InDetDxAODFlags.DumpSctInfo.set_Value_and_Lock(True);InDetDxAODFlags.ThinHitsOnTrack.set_Value_and_Lock(False)' \
+      ESDtoDPD:'rec.DPDMakerScripts.set_Value_and_Lock(["PrimaryDPDMaker/PrimaryDPDMaker.py"]);from InDetRecExample.InDetJobProperties import InDetFlags;InDetFlags.useDCS.set_Value_and_Lock(True);from PixelConditionsServices.PixelConditionsServicesConf import PixelCalibSvc;ServiceMgr +=PixelCalibSvc();ServiceMgr.PixelCalibSvc.DisableDB=True' \
+    --postExec    HITtoRDO:'pixeldigi.EnableSpecialPixels=False; CfgMgr.MessageSvc().setError+=["HepMcParticleLink"];' \
+      RAWtoALL:'ToolSvc.InDetSCT_ClusteringTool.useRowInformation=True; from AthenaCommon.AppMgr import ToolSvc; ToolSvc.InDetTrackSummaryTool.OutputLevel=INFO;from InDetPhysValMonitoring.InDetPhysValMonitoringConf import InDetPhysValDecoratorAlg;decorators = InDetPhysValDecoratorAlg();topSequence += decorators'
+
+
+  reco_stat=$?
+  echo "art-result: $reco_stat reco"
+  if [ "$reco_stat" -ne 0 ]; then
+      echo "$script: Reco_tf.py isn't working yet. Remove jobReport.json to prevent pilot declaring a failed job."
+      run rm -f jobReport.json
+  fi
+  
+  mv ./PixelRDOAnalysis.root ./$dcubemon_digi_pixel
+  mv ./SCT_RDOAnalysis.root ./$dcubemon_digi_strip
+  
+  # DCube digi plots
+  dcube Reco_tf digi-plot "$dcubemon_digi_pixel" "$dcubecfg_digi_pixel" "$lastref_dir/$dcubemon_digi_pixel" "$dcube_digi_pixel_lastref"
+  dcube Reco_tf  ""       "$dcubemon_digi_pixel" "$dcubecfg_digi_pixel"                           "$dcuberef_digi_pixel" "$dcube_digi_pixel_fixref"
+  
+  dcube Reco_tf digi-plot "$dcubemon_digi_strip" "$dcubecfg_digi_strip" "$lastref_dir/$dcubemon_digi_strip" "$dcube_digi_strip_lastref"
+  dcube Reco_tf  ""       "$dcubemon_digi_strip" "$dcubecfg_digi_strip"                           "$dcuberef_digi_strip" "$dcube_digi_strip_fixref"
+    
+fi
+
+if [ $dophy -ne 0 ]; then
+
+  ## phys validation and dcube for single particles
+  run ls -lLU "$esd_particle_energy"
+
+  if [ ! -s "$esd_particle_energy" ]; then
+    echo "$script: Reco_tf output '$esd_particle_energy' not created - exit" 2>&1
+    echo "art-result: 21 physval"
+#   test -n "$dcube_rec_fixref"  && echo "art-result: 22 plot-fixref"
+    test -n "$dcube_rec_lastref" && echo "art-result: 22 plot"
+    exit
+  fi
+  
+  # Run InDetPhysValMonitoring on DAOD.
+  # It should eventually be possible to include this in the reco step, but this needs Reco_tf to support the ITk IDPVM setup.
+  ( set -x
+    inputDAOD_IDTRKVALIDFile="$daod_particle_energy" exec athena.py InDetSLHC_Example/PhysValITk_jobOptions.py
+  )
+  echo "art-result: $? physval"
+  
+  mv ./physval.root ./$dcubemon_rec
+  
+  # DCube InDetPhysValMonitoring performance plots
+  dcube InDetPhysValMonitoring plot "$dcubemon_rec" "$dcubecfg_rec" "$lastref_dir/$dcubemon_rec" "$dcube_rec_lastref"
+  dcube InDetPhysValMonitoring ""   "$dcubemon_rec" "$dcubecfg_rec"                         "$dcuberef_rec" "$dcube_rec_fixref"
+  
+fi
