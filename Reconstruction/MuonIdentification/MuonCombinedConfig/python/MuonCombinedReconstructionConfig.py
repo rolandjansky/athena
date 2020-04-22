6ab41fed3235772a580b641b0a368b04d9cbb211
Edward Moyse <edward.moyse@cern.ch>
2020-04-22 10:59:03 +0200

    Whoops. Do need other detectors.

--- Reconstruction/MuonIdentification/MuonCombinedConfig/python/MuonCombinedReconstructionConfig.py
+++ Reconstruction/MuonIdentification/MuonCombinedConfig/python/MuonCombinedReconstructionConfig.py
@@ -267,7 +267,30 @@ if __name__=="__main__":
     from MuonConfig.MuonConfigUtils import SetupMuonStandaloneArguments, SetupMuonStandaloneConfigFlags, SetupMuonStandaloneOutput, SetupMuonStandaloneCA
 
     args = SetupMuonStandaloneArguments()
-    ConfigFlags = SetupMuonStandaloneConfigFlags(args)
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+
+    ConfigFlags.Input.Files = ['/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/ESD.16747874._000011_100events.pool.root']
+    
+    ConfigFlags.Concurrency.NumThreads=args.threads
+    ConfigFlags.Concurrency.NumConcurrentEvents=args.threads # Might change this later, but good enough for the moment.
+
+    ConfigFlags.Detector.GeometryMDT   = True 
+    ConfigFlags.Detector.GeometryTGC   = True
+    ConfigFlags.Detector.GeometryCSC   = True     
+    ConfigFlags.Detector.GeometryTile   = True 
+    ConfigFlags.Detector.GeometryLAr   = True 
+    ConfigFlags.Detector.GeometryPixel   = True 
+    ConfigFlags.Detector.GeometrySCT   = True 
+    ConfigFlags.Detector.GeometryTRT   = True 
+
+    
+    ConfigFlags.Output.ESDFileName=args.output
+    
+    ConfigFlags.Input.isMC = True
+    ConfigFlags.lock()
+
+    ConfigFlags.dump()
+
     cfg = SetupMuonStandaloneCA(args,ConfigFlags)
 
 >>>>>>> Use the standard configurations for running via Python from MuonConfig.
@@ -300,10 +323,14 @@ if __name__=="__main__":
     # cfg.merge( OutputStreamCfg( ConfigFlags, 'ESD', ItemList=itemsToRecord) )
   
 <<<<<<< HEAD
+<<<<<<< HEAD
     cfg.printConfig(withDetails=True)
 =======
     cfg.printConfig()
 >>>>>>> Use the standard configurations for running via Python from MuonConfig.
+=======
+    cfg.printConfig(withDetails=True)
+>>>>>>> Whoops. Do need other detectors.
     f=open("MuonCombinedReconstruction.pkl","wb")
     cfg.store(f)
     f.close()
