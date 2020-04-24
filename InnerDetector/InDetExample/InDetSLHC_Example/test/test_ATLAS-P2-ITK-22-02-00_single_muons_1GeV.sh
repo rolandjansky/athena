e22a3b0389763ba7ff42e91a1b6829609199510b
Marcin Wolter <marcin.wolter@cern.ch>
2020-04-24 21:13:36 +0200

    art tests for ATLAS-P2-ITK-22-02-00

--- InnerDetector/InDetExample/InDetSLHC_Example/test/test_ATLAS-P2-ITK-22-02-00_single_muons_1GeV.sh
+++ InnerDetector/InDetExample/InDetSLHC_Example/test/test_ATLAS-P2-ITK-22-02-00_single_muons_1GeV.sh
@@ -9,17 +9,8 @@
 #
 # Running art test for single muon, geometry ATLAS-P2-ITK-22-02-00
 #
-# resolve the script directory:
-# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-# and https://acode-browser2.usatlas.bnl.gov/lxr/source/AthAna/atlas/PhysicsAnalysis/TauID/TauCorrUncert/setup.sh
-#
 
 
-### SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../share"  >/dev/null 2>&1 && pwd )"
-
-
-
-### file=${SCRIPT_DIR}/test_ITk_single_particle.sh
 file=test_ITk_single_particle.sh
 
 echo "Executing script ${file}"
