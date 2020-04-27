3d23cd03690160a73c4a581f597fc6f4ad53714b
Dan Guest <daniel.hay.guest@cern.ch>
2020-04-27 07:48:10 +0000

    copyright commit noise

--- PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/customGetter.h
+++ PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/customGetter.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2012 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 // The customGetter file is a catch-all for various getter functinos
