9cef046ecec4a8f622c6d4734d28ebf255723f90
Marcin Wolter <marcin.wolter@cern.ch>
2020-04-28 16:17:34 +0200

    adding two more art test pT points

---
+++ InnerDetector/InDetExample/InDetSLHC_Example/test/test_ATLAS-P2-ITK-22-02-00_single_muons_10GeV.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# art-description: InclinedDuals sim+reco
+# art-type: grid
+# art-include: 21.9/Athena
+# art-output: *.root
+# art-output: *.xml
+# art-output: dcube*
+#
+#
+# Running art test for single muon, 1GeV, geometry ATLAS-P2-ITK-22-02-00
+#
+
+
+file=test_ITk_single_particle.sh
+
+echo "Executing script ${file}"
+echo " "
+"$file" muons 10GeV  ATLAS-P2-ITK-22-02-00
+
+
+
