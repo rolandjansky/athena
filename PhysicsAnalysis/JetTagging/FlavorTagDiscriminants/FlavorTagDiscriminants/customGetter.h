8deab175a840c2c205f807831ba50907e5014dae
Dan Guest <dguest@cern.ch>
2020-04-28 18:08:01 +0200

    add another column of / for doxygen

--- PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/customGetter.h
+++ PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/FlavorTagDiscriminants/customGetter.h
@@ -19,25 +19,25 @@
 
 namespace FlavorTagDiscriminants {
 
-  // Factory function to produce TrackParticle -> vector<double> functions
-  //
-  // DL2 configures the its inputs when the algorithm is initalized,
-  // meaning that the list of track and jet properties that are used
-  // as inputs won't be known at compile time. Instead we build an
-  // array of "getter" functions, each of which returns one input for
-  // the tagger. The function here returns those getter functions.
-  //
-  // Many of the getter functions are trivial: they will, for example,
-  // read one double of auxdata off of the BTagging object. The
-  // sequence input getters tend to be more complicated. Since we'd
-  // like to avoid reimplementing the logic in these functions in
-  // multiple places, they are exposed here.
-  //
-  // This function will return a getter based on a string key. See the
-  // implementation for the definitions.
-  //
-  // NOTE: This function is for experts only, don't expect support.
-  //
+  /// Factory function to produce TrackParticle -> vector<double> functions
+  ///
+  /// DL2 configures the its inputs when the algorithm is initalized,
+  /// meaning that the list of track and jet properties that are used
+  /// as inputs won't be known at compile time. Instead we build an
+  /// array of "getter" functions, each of which returns one input for
+  /// the tagger. The function here returns those getter functions.
+  ///
+  /// Many of the getter functions are trivial: they will, for example,
+  /// read one double of auxdata off of the BTagging object. The
+  /// sequence input getters tend to be more complicated. Since we'd
+  /// like to avoid reimplementing the logic in these functions in
+  /// multiple places, they are exposed here.
+  ///
+  /// This function will return a getter based on a string key. See the
+  /// implementation for the definitions.
+  ///
+  /// NOTE: This function is for experts only, don't expect support.
+  ///
   std::function<std::vector<double>(
     const xAOD::Jet&,
     const std::vector<const xAOD::TrackParticle*>&)>
