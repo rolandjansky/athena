6487fb570c2ab02d91a77df6c193e98789fc7a79
Tulay Cuhadar Donszelmann <tcuhadar@cern.ch>
2020-04-30 16:24:04 +0200

    update tests - dcube, ref and config file, move to physval output

--- Simulation/FastSimulation/FastChainPileup/test/test_fastchain_mc16a_ttbar.sh
+++ Simulation/FastSimulation/FastChainPileup/test/test_fastchain_mc16a_ttbar.sh
@@ -5,13 +5,18 @@
 # art-include: master/Athena
 # art-output: *.root
 # art-output: config.txt
+# art-output: dcube-rdo-truth
+# art-html: dcube-rdo-truth
 
 HighPtMinbiasHitsFiles="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361239.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_high.merge.HITS.e4981_s3087_s3089/*"
 LowPtMinbiasHitsFiles="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361238.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_low.merge.HITS.e4981_s3087_s3089/*"
-
+inputRefDir="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/DCube-refs/${AtlasBuildBranch}/test_fastchain_mc16a_ttbar"
+inputXmlDir="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/DCube-configs/${AtlasBuildBranch}"
+dcubeXmlRDO="${inputXmlDir}/dcube_RDO_truth_ATLFASTII.xml"
+dcubeRefRDO="${inputRefDir}/RDO_truth.root"
 
 FastChain_tf.py \
-    --simulator ATLFASTIIF \
+    --simulator ATLFASTII \
     --digiSteeringConf "Split" \
     --useISF True \
     --randomSeed 123 \
@@ -24,8 +29,8 @@ FastChain_tf.py \
     --conditionsTag default:OFLCOND-MC16-SDR-16 \
     --preSimExec 'from TrkDetDescrSvc.TrkDetDescrJobProperties import TrkDetFlags;TrkDetFlags.TRT_BuildStrawLayers=True;from Digitization.DigitizationFlags import digitizationFlags;digitizationFlags.experimentalDigi=["NewMerge"]' \
     --preExec 'EVNTtoRDO:ToolSvc.NewMergeMcEventCollTool.OutputLevel=VERBOSE;' \
-    --postInclude='PyJobTransforms/UseFrontier.py' \
-    --postExec 'from AthenaCommon.ConfigurationShelve import saveToAscii;saveToAscii("config.txt");ServiceMgr.MessageSvc.Format = "% F%32W%S%7W%R%T %0W%M"' \
+    --postInclude='PyJobTransforms/UseFrontier.py,DigitizationTests/postInclude.RDO_Plots.py' \
+    --postExec 'from AthenaCommon.ConfigurationShelve import saveToAscii;saveToAscii("config.txt")' \
     --DataRunNumber '284500' \
     --inputHighPtMinbiasHitsFile ${HighPtMinbiasHitsFiles} \
     --inputLowPtMinbiasHitsFile ${LowPtMinbiasHitsFiles} \
@@ -34,17 +39,30 @@ FastChain_tf.py \
     --numberOfLowPtMinBias '44.3839246425' \
     --numberOfCavernBkg 0 \
     --imf False
+
+# Add Reco step?
+
 rc=$?
 echo  "art-result: $rc EVNTtoRDO"
-
 rc2=-9999
+rc3=-9999
+
 if [ ${rc} -eq 0 ]
 then
     # Regression test
     ArtPackage=$1
     ArtJobName=$2
+    echo ArtJobName
+    echo $ArtJobName
     art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
     rc2=$?
 
+    # Histogram comparison with DCube
+    $ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py \
+    -p -x dcube-rdo-truth \
+    -c ${dcubeXmlRDO} -r ${dcubeRefRDO} RDO_truth.root
+    rc3=$?
+
 fi
 echo  "art-result: ${rc2} regression"
+echo  "art-result: ${rc3} dcubeRDO"
--- Simulation/FastSimulation/FastChainPileup/test/test_ttFC_recoinp_noSplit_noPseudoT_fastSim_fullDigi.sh
+++ Simulation/FastSimulation/FastChainPileup/test/test_ttFC_recoinp_noSplit_noPseudoT_fastSim_fullDigi.sh
@@ -7,44 +7,48 @@
 # art-output: RAWtoESD_config.txt
 # art-output: *.root
 # art-output: dcube-id
+# art-html: dcube-id
 
 inputRefDir="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/DCube-refs/${AtlasBuildBranch}/test_ttFC_recoinp_noSplit_noPseudoT_fastSim_fullDigi"
 inputXmlDir="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/DCube-configs/${AtlasBuildBranch}"
-art_dcube="/cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube"
-dcubeName="ttFC_recoinp_noSplit_noPseudoT_fastSim_fullDigi"
-dcubeXmlID="${inputXmlDir}/dcube_ID_recoinp.xml"
-dcubeRefID="${inputRefDir}/InDetStandardPlots.root"
+dcubeXmlID="${inputXmlDir}/physval-noSplit_noPseudoT_fastSim_fullDigi.xml"
+dcubeRefID="${inputRefDir}/physval-noSplit_noPseudoT_fastSim_fullDigi.root"
 
-# RDO input from 21.3/Nov 13 Nightly test_ttFC_reco_noSplit_noPseudoT_fastSim_fullDigi.sh
+# RDO input from 21.3/Apr 27 Nightly test_ttFC_reco_noSplit_noPseudoT_fastSim_fullDigi.sh
 FastChain_tf.py --maxEvents 500 \
     --skipEvents 0 \
     --geometryVersion ATLAS-R2-2015-03-01-00 \
     --conditionsTag OFLCOND-RUN12-SDR-31 \
     --inputRDOFile '/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/FastChainPileup/RecoInputRefs/RDO_pileup_fastsim_fulldigi.pool.root' \
     --outputAODFile AOD_noSplit_noPseudoT_fastSim_fullDigi.pool.root \
-    --preExec "all:rec.doTrigger.set_Value_and_Lock(False);recAlgs.doTrigger.set_Value_and_Lock(False);" "InDetFlags.doStandardPlots.set_Value_and_Lock(True)" \
+    --preExec "all:rec.doTrigger.set_Value_and_Lock(False);recAlgs.doTrigger.set_Value_and_Lock(False);" \
     --postExec 'RAWtoESD:from AthenaCommon.ConfigurationShelve import saveToAscii;saveToAscii("RAWtoESD_config.txt")' \
+    --outputNTUP_PHYSVALFile 'physval-noSplit_noPseudoT_fastSim_fullDigi.root' \
+    --validationFlags 'doInDet' \
+    --valid 'True' \
     --imf False
-
 rc3=$?
+
 rc4=-9999
 rc5=-9999
-if [ ${rc3} -eq 0 ]
+# return to this check after physval returns proper exit_code
+# if [ ${rc3} -eq 0 ]
+if [ -f "AOD_noSplit_noPseudoT_fastSim_fullDigi.pool.root" ]
 then
     # Regression test
     ArtPackage=$1
     ArtJobName=$2
     art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName} --mode=summary
     rc4=$?
-
-    # Histogram comparison with DCube
-    bash ${art_dcube} ${dcubeName} InDetStandardPlots.root ${dcubeXmlID} ${dcubeRefID}
-    rc5=$?
-    if [ -d "dcube" ]
-    then
-       mv "dcube" "dcube-id"
-    fi
 fi
+
+# Histogram comparison with DCube
+$ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py \
+-p -x dcube-id \
+-c ${dcubeXmlID} -r ${dcubeRefID} physval-noSplit_noPseudoT_fastSim_fullDigi.root
+rc5=$?
+
+
 echo  "art-result: ${rc3} RDOtoAOD"
 echo  "art-result: ${rc4} regression"
 echo  "art-result: ${rc5} dcubeID"
