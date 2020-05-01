62bdcc5e40f7352a38da95b9a3aa5a847b641200
Dan Guest <dguest@cern.ch>
2020-05-01 20:56:58 +0200

    Support ancient version of git
    Apparently 'git remote get-url' is a relatively new addition to the library,
    in that it was added in the last 5 years. This moves to something that was
    supported in git 1.8 as well.

--- Build/AtlasBuildScripts/checkout_atlasexternals.sh
+++ Build/AtlasBuildScripts/checkout_atlasexternals.sh
@@ -111,9 +111,9 @@ fi
 # check whether the source directory points to the right repository
 RECLONE=false
 pushd ${SOURCEDIR}
-SOURCEURL=$(git remote get-url origin)
+SOURCEURL=$(git ls-remote --get-url origin)
 if [ "${SOURCEURL}" != "${EXTERNALSURL}" ] ; then
-    echo "${SOURCEURL} wasn't cloned from ${EXTERNALSURL}, deleting"
+    echo "checkout_externals: Warning! ${SOURCEURL} wasn't cloned from ${EXTERNALSURL}, deleting"
     RECLONE=true
 fi
 popd
