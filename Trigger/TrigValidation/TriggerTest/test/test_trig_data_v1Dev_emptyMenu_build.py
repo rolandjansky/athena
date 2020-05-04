b884517a2a3a6e12cd201038675a29fac51d9fed
Rafal Bielski <rafal.bielski@cern.ch>
2020-05-04 09:05:48 +0000

    Further clean-up of TrigUpgradeTest

--- Trigger/TrigValidation/TriggerTest/test/test_trig_data_v1Dev_emptyMenu_build.py
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_data_v1Dev_emptyMenu_build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# art-description: Trigger BS->RDO_TRIG athena test of the Dev_pp_run3_v1 menu
+# art-description: Trigger BS->RDO_TRIG athena test without any HLT chains
 # art-type: build
 # art-include: master/Athena
 # Skipping art-output which has no effect for build tests.
@@ -12,14 +12,17 @@ ex = ExecStep.ExecStep()
 ex.type = 'athena'
 ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
 ex.input = 'data'
-ex.threads = 2
-ex.concurrent_events = 2
+ex.max_events = 80
+ex.threads = 4
+ex.concurrent_events = 4
 precommand = ''.join([
   "setMenu='LS2_v1';",  # LS2_v1 soon to be renamed to Dev_pp_run3_v1
+  "doEmptyMenu=True;",
   "doWriteBS=False;",
   "doWriteRDOTrigger=True;",
   "forceEnableAllChains=True;",
   "fpeAuditor=True;",
+  "failIfNoProxy=True;"
 ])
 ex.args = '-c "{:s}"'.format(precommand)
 
@@ -28,12 +31,14 @@ test.art_type = 'build'
 test.exec_steps = [ex]
 test.check_steps = CheckSteps.default_check_steps(test)
 
-# Add a step comparing counts in the log against reference from test_trig_data_v1Dev_build
-refcomp = CheckSteps.RegTestStep("CountRefComp")
-refcomp.regex = 'TrigSignatureMoniMT.*HLT_.*|TrigSignatureMoniMT.*-- #[0-9]+ (Events|Features).*'
-refcomp.reference = 'TriggerTest/ref_data_v1Dev_build.ref'
-refcomp.required = True # Final exit code depends on this step
-CheckSteps.add_step_after_type(test.check_steps, CheckSteps.LogMergeStep, refcomp)
+# Skip ZeroCounts check because empty menu has no counts
+test.check_steps.remove(test.get_step("ZeroCounts"))
+
+# Overwrite default MessageCount settings
+msgcount = test.get_step("MessageCount")
+msgcount.info_threshold = 600
+msgcount.other_threshold = 50
+msgcount.required = True # make the test exit code depend on this step
 
 import sys
 sys.exit(test.run())
