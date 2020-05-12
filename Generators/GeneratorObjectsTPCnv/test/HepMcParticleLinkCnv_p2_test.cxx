9348c67b97541f9a84917cbc839e960ba2d2473e
John Chapman <jchapman@cern.ch>
2020-05-12 11:34:47 +0200

    Update HepMcParticleLink unit tests
    This change updates the HepMcParticleLink unit tests, so that they cover all use-cases found
    in current production jobs.

--- Generators/GeneratorObjectsTPCnv/test/HepMcParticleLinkCnv_p2_test.cxx
+++ Generators/GeneratorObjectsTPCnv/test/HepMcParticleLinkCnv_p2_test.cxx
@@ -59,6 +59,54 @@ void populateGenEvent(HepMC::GenEvent & ge, int pdgid1, int pdgid2, std::vector<
   ge.set_beam_particles(inParticle1,inParticle2);
 }
 
+void populateFilteredGenEvent(HepMC::GenEvent & ge, std::vector<HepMC::GenParticle*>& genPartList)
+{
+  //.......Create new particle (geantino) to link  hits from pileup
+  HepMC::GenParticle* genPart=new HepMC::GenParticle();
+  genPart->set_pdg_id(999); //Geantino
+  genPart->set_status(1); //!< set decay status
+  genPart->suggest_barcode( std::numeric_limits<int32_t>::max() );
+
+  HepMC::GenVertex* genVertex=new HepMC::GenVertex();
+  genVertex->add_particle_out(genPart);
+  genPartList.push_back(genPart);
+
+  //to set geantino vertex as a truth primary vertex
+  HepMC::GenVertex* hScatVx = ge.barcode_to_vertex(-3);
+  if(hScatVx!=nullptr) {
+    HepMC::FourVector pmvxpos=hScatVx->position();
+    genVertex->set_position(pmvxpos);
+    //to set geantino kinematic phi=eta=0, E=p=E_hard_scat
+    HepMC::GenVertex::particles_in_const_iterator itrp =hScatVx->particles_in_const_begin();
+    if (hScatVx->particles_in_size()==2){
+      HepMC::FourVector mom1=(*itrp)->momentum();
+      HepMC::FourVector mom2=(*(++itrp))->momentum();
+      HepMC::FourVector vxmom;
+      vxmom.setPx(mom1.e()+mom2.e());
+      vxmom.setPy(0.);
+      vxmom.setPz(0.);
+      vxmom.setE(mom1.e()+mom2.e());
+
+      genPart->set_momentum(vxmom);
+    }
+  }
+
+  if(!ge.vertices_empty()){
+    std::vector<HepMC::GenVertex *> vtxvec;
+    HepMC::GenEvent::vertex_iterator itvtx = ge.vertices_begin();
+    for (;itvtx != ge.vertices_end(); ++itvtx ) {
+      ge.remove_vertex(*itvtx);
+      vtxvec.push_back((*itvtx));
+      //fix me: delete vertex pointer causes crash
+      //delete (*itvtx);
+    }
+    for(unsigned int i=0;i<vtxvec.size();i++)  delete vtxvec[i];
+  }
+
+  //.....add new vertex with geantino
+  ge.add_vertex(genVertex);
+}
+
 void createMcEventCollectionInStoreGate(std::vector<HepMC::GenParticle*>& genPartList)
 {
   // create dummy input McEventCollection with a name that
@@ -78,6 +126,7 @@ void createMcEventCollectionInStoreGate(std::vector<HepMC::GenParticle*>& genPar
   // HepMcParticleLink_p2. A workaround is used to suppport larger
   // values for the first event in the McEventCollection.
   const int event_number3(64);
+  const int event_number4(89);
   inputTestDataHandle->push_back(new HepMC::GenEvent(process_id1, event_number1));
   HepMC::GenEvent& ge1 = *(inputTestDataHandle->at(0));
   populateGenEvent(ge1,-11,11,genPartList);
@@ -90,6 +139,10 @@ void createMcEventCollectionInStoreGate(std::vector<HepMC::GenParticle*>& genPar
   HepMC::GenEvent& ge3 = *(inputTestDataHandle->at(2));
   populateGenEvent(ge3,-11,11,genPartList);
   populateGenEvent(ge3,22,22,genPartList);
+  inputTestDataHandle->push_back(new HepMC::GenEvent(ge1));
+  HepMC::GenEvent& ge4 = *(inputTestDataHandle->at(3));
+  ge4.set_event_number(event_number4);
+  populateFilteredGenEvent(ge4,genPartList);
   return;
 }
 
@@ -110,18 +163,45 @@ void test1()
   std::cout << "test1\n";
   std::vector<HepMC::GenParticle*> genPartList;
   createMcEventCollectionInStoreGate(genPartList);
-  assert ( genPartList.size() == 12 );
+  assert ( genPartList.size() == 13 );
+  // HepMcParticleLinks pointing at GenParticles in the first GenEvent in the McEventCollection
+  // By event_number
   const HepMC::GenParticle * particle1 = genPartList.at(0);
-  HepMcParticleLink trans1(particle1->barcode(),particle1->parent_event()->event_number());
-  testit (trans1);
-  HepMcParticleLink trans2(particle1->barcode(),0,EBC_MAINEVCOLL,HepMcParticleLink::IS_POSITION);
-  testit (trans2);
+  HepMcParticleLink trans1a(particle1->barcode(),particle1->parent_event()->event_number());
+  testit (trans1a);
+  // By position
+  HepMcParticleLink trans1b(particle1->barcode(),0,EBC_MAINEVCOLL,HepMcParticleLink::IS_POSITION);
+  testit (trans1b);
+  // HepMcParticleLinks pointing at GenParticles in other GenEvents in the McEventCollection
   const HepMC::GenParticle * particle2 = genPartList.at(7);
-  HepMcParticleLink trans3(particle2->barcode(),particle2->parent_event()->event_number());
-  testit (trans3);
+  HepMcParticleLink trans2(particle2->barcode(),particle2->parent_event()->event_number());
+  testit (trans2);
   const HepMC::GenParticle * particle3 = genPartList.at(8);
-  HepMcParticleLink trans4(particle3->barcode(),particle3->parent_event()->event_number());
+  HepMcParticleLink trans3(particle3->barcode(),particle3->parent_event()->event_number());
+  testit (trans3);
+  // HepMcParticleLinks pointing at filtered pileup truth
+  const HepMC::GenParticle * particle4 = genPartList.at(12);
+  HepMcParticleLink trans4(particle4->barcode(),particle4->parent_event()->event_number());
   testit (trans4);
+  // HepMcParticleLinks pointing at delta-ray (barcode=0 - not recorded in McEventCollection) using event number
+  int deltaRayBarcode(0);
+  HepMcParticleLink trans5a(deltaRayBarcode,particle1->parent_event()->event_number());
+  testit (trans5a);
+  // HepMcParticleLinks pointing at delta-ray (barcode=0 - not recorded in McEventCollection) using hard-scatter event position
+  HepMcParticleLink trans5b(deltaRayBarcode,0,EBC_MAINEVCOLL,HepMcParticleLink::IS_POSITION);
+  testit (trans5b);
+  // Link to a GenParticle which was not recorded to the
+  // McEventCollection, even though other parts of the same GenEvent
+  // were recorded.
+  const HepMcParticleLink::index_type refEvtNum1 = static_cast<HepMcParticleLink::index_type>(particle1->parent_event()->event_number());
+  const int cutBarcode(210001);
+  HepMcParticleLink trans6a(cutBarcode, refEvtNum1);
+  testit (trans6a);
+  // Link to a GenEvent which was not recorded to the McEventCollection
+  const int event_number5(460);
+  const HepMcParticleLink::index_type refEvtNum5 = static_cast<HepMcParticleLink::index_type>(event_number5);
+  HepMcParticleLink trans6b(cutBarcode, refEvtNum5);
+  testit (trans6b);
 }
 
 
