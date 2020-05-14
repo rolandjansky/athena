97b13ca40a0d1560e6dcbbda014ae606a0a3ae40
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2020-05-14 14:26:13 +0200

    add new ART test and correct other

--- Generators/EvgenJobTransforms/test/test_gen_mgpy8eg_tWgamma_madspin.sh
+++ Generators/EvgenJobTransforms/test/test_gen_mgpy8eg_tWgamma_madspin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# art-description: Generation test Powheg LHE only Z->tautau
+# art-description: Generation test MG+Py8+EG pp->ttbar+W+a with MadSpin
 # art-type: build
 # art-include: 21.6/AthGeneration
 # art-output: *.root
@@ -8,8 +8,8 @@
 ## Any arguments are considered overrides, and will be added at the end
 export TRF_ECHO=True;
 rm *;
-Gen_tf.py --ecmEnergy=13000 --jobConfig=421111 \
-    --outputTXTFile=test_powheg_Ztautau.TXT.tar.gz \
+Gen_tf.py --ecmEnergy=13000 --jobConfig=500334 --maxEvents=10 \
+    --outputEVNTFile=test_mgpy8eg_tWgamma_madspin.EVNT.pool.root \
 
 echo "art-result: $? generate"
 
