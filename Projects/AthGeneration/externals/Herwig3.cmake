a3966803cafb9a857affc3a86339e50043f50158
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2020-05-14 11:19:23 +0200

    update H7 version to 721

--- Projects/AthGeneration/externals/Herwig3.cmake
+++ Projects/AthGeneration/externals/Herwig3.cmake
@@ -2,7 +2,7 @@
 # File specifying the location of Herwig3 to use.
 #
 
-set( HERWIG3_LCGVERSION 7.2.0p3 )
+set( HERWIG3_LCGVERSION 7.2.1p3 )
 # Comment the following line for H7 versions <7.2
 set( HW3_VER_IS_72 1 )
 set( HERWIG3_LCGROOT
