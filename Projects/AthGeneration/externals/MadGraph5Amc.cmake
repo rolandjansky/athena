ac4303faf3b36a1559192fcdc01d28bad1c149ac
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2020-05-18 14:46:01 +0000

    Update MadGraph5Amc.cmake

--- Projects/AthGeneration/externals/MadGraph5Amc.cmake
+++ Projects/AthGeneration/externals/MadGraph5Amc.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of MadGraph to use.
 #
 
-set( MADGRAPH5AMC_LCGVERSION 2.7.2p3.atlas5)
+set( MADGRAPH5AMC_LCGVERSION 2.6.7p3.atlas5)
 set( MADGRAPH5AMC_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/madgraph5amc/${MADGRAPH5AMC_LCGVERSION}/${LCG_PLATFORM} )
