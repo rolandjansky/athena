86280ab73ba8d516dc58d42b093e87a74af27309
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-05-19 13:02:30 +0200

    Updated the xAODEventShape package's dictionary generation.
    Made sure that all potentially useful/necessary types are generated
    in an organised fashion, using the helper code from xAODCore.
    
    Declared the typedef names to the generated ROOT dictionary.

--- Event/xAOD/xAODEventShape/xAODEventShape/xAODEventShapeDict.h
+++ Event/xAOD/xAODEventShape/xAODEventShape/xAODEventShapeDict.h
@@ -1,15 +1,17 @@
 // Dear emacs, this is -*- c++ -*-
-
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-
-// $Id: xAODEventShapeDict.h 629489 2014-11-19 14:37:55Z krasznaa $
 #ifndef XAODEVENTSHAPE_XAODEVENTSHAPEDICT_H
 #define XAODEVENTSHAPE_XAODEVENTSHAPEDICT_H
 
-// Includes for the dictionary generation:
+// Local include(s).
+#include "xAODEventShape/EventShape.h"
+#include "xAODEventShape/EventShapeAuxInfo.h"
 #include "xAODEventShape/versions/EventShape_v1.h"
 #include "xAODEventShape/versions/EventShapeAuxInfo_v1.h"
+
+// EDM include(s).
+#include "xAODCore/tools/DictHelpers.h"
 
 #endif // XAODEVENTSHAPE_XAODEVENTSHAPEDICT_H
