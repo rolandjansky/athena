381692138046af9ea429736abd32d5b0edaf894d
Nils Erik Krumnack <nils.erik.krumnack@cern.ch>
2020-05-19 21:00:53 +0000

    Fix conflicts in master
    Add HbbTagTool, also a VR jet overlap decoration tool
    
    See merge request atlas/athena!32978
    
    (cherry picked from commit cef8bb696007aa0dc63872f3e414f256fd4725ea)
    
    6967d18d Add a 'Tool' interface for HbbTag
    2f82a1fd Add ASG interface for VR Jet overlap decorator tool
    8a2c0c5f Fixes for nills
    92441191 Remove unneeded destructors
    6e631d4d Add override to decorate calls

--- PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/src/components/FlavorTagDiscriminants_entries.cxx
+++ PhysicsAnalysis/JetTagging/FlavorTagDiscriminants/src/components/FlavorTagDiscriminants_entries.cxx
@@ -2,12 +2,16 @@
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
+#include "FlavorTagDiscriminants/VRJetOverlapDecoratorTool.h"
+#include "FlavorTagDiscriminants/HbbTagTool.h"
 #include "FlavorTagDiscriminants/DL2Tool.h"
 #include "FlavorTagDiscriminants/BTagAugmenterTool.h"
 #include "FlavorTagDiscriminants/BTagMuonAugmenterTool.h"
 
 using namespace FlavorTagDiscriminants;
 
+DECLARE_COMPONENT(VRJetOverlapDecoratorTool)
+DECLARE_COMPONENT(HbbTagTool)
 DECLARE_COMPONENT(DL2Tool)
 DECLARE_COMPONENT(BTagAugmenterTool)
 DECLARE_COMPONENT(BTagMuonAugmenterTool)
