aea7f4af5c8065f53c1559653abc8b0dc64d20eb
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-05-22 08:51:08 +0200

    Updated the xAODMetaData package's dictionary generation.
    Made sure that all potentially useful/necessary types are generated
    in an organised fashion, using the helper code from xAODCore.

--- Event/xAOD/xAODMetaData/xAODMetaData/xAODMetaDataDict.h
+++ Event/xAOD/xAODMetaData/xAODMetaData/xAODMetaDataDict.h
@@ -1,15 +1,17 @@
 // Dear emacs, this is -*- c++ -*-
-
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-
-// $Id: xAODMetaDataDict.h 670157 2015-05-27 11:52:51Z krasznaa $
 #ifndef XAODMETADATA_XAODMETADATADICT_H
 #define XAODMETADATA_XAODMETADATADICT_H
 
-// Local include(s):
+// Local include(s).
+#include "xAODMetaData/FileMetaData.h"
+#include "xAODMetaData/FileMetaDataAuxInfo.h"
 #include "xAODMetaData/versions/FileMetaData_v1.h"
 #include "xAODMetaData/versions/FileMetaDataAuxInfo_v1.h"
+
+// EDM include(s).
+#include "xAODCore/tools/DictHelpers.h"
 
 #endif // XAODMETADATA_XAODMETADATADICT_H
