a542afdb0b49f3bfab52b243b7d378527528c3cd
Michele Renda <michele.renda@cern.ch>
2020-05-29 23:31:47 +0300

    Added flag --show-same

--- Tools/PyUtils/bin/meta-diff.py
+++ Tools/PyUtils/bin/meta-diff.py
@@ -80,8 +80,15 @@ def print_diff_dict_keys(parent_key, obj1, obj2, diff_format):
         print('> ' + value1)
         print('----------')
         print('< ' + value2)
+        
+def print_same(parent_key, obj1, obj2, diff_format):
+    if diff_format == 'simple':
+        print('{}  is the same'.format(parent_key))
+    else:
+        print('')
+        print('{}: same'.format(parent_key))
 
-def compare(obj1, obj2, parent_key=None, ordered=False, diff_format='simple'):
+def compare(obj1, obj2, parent_key=None, ordered=False, show_same=False, diff_format='simple'):
     
  
     same = True;
@@ -98,25 +105,37 @@ def compare(obj1, obj2, parent_key=None, ordered=False, diff_format='simple'):
                     child_key += parent_key + '/'
                 child_key += key
                 
-                same &= compare(obj1[key] if key in obj1 else None, obj2[key] if key in obj2 else None, child_key, ordered, diff_format)
+                same &= compare(obj1[key] if key in obj1 else None, obj2[key] if key in obj2 else None, child_key, ordered, show_same, diff_format)
                 
     elif  isinstance(obj1, list) and isinstance(obj2, list):
         if ordered:
             if sorted(obj1) != sorted(obj2):
                 print_diff(parent_key, obj1, obj2, diff_format)
                 same = False
+            else:
+                if show_same:
+                    print_same(parent_key, obj1, obj2, diff_format)
         else:
             if obj1 != obj2:
                 print_diff(parent_key, obj1, obj2, diff_format) 
-                same = False     
+                same = False
+            else:
+                if show_same:
+                    print_same(parent_key, obj1, obj2, diff_format)
     elif  isinstance(obj1, set) and isinstance(obj2, set):
         if obj1 != obj2:
             print_diff(parent_key, obj1, obj2, diff_format)
             same = False
+        else:
+            if show_same:
+                print_same(parent_key, obj1, obj2, diff_format)
     elif type(obj1) == type(obj2):
         if obj1 != obj2:
             print_diff(parent_key, obj1, obj2, diff_format)
             same = False
+        else:
+            if show_same:
+                print_same(parent_key, obj1, obj2, diff_format)
     else:
         print_diff_type(parent_key, obj1, obj2, diff_format)
         same = False
@@ -134,8 +153,7 @@ def _main():
                         '--verbose',
                         action='store_true',
                         help='print detailed output on screen')
-    parser.add_argument('-s',
-                        '--ordered',
+    parser.add_argument('--ordered',
                         action='store_true',
                         help='When comparing lists, check the element order too.')
     parser.add_argument('-o',
@@ -158,6 +176,11 @@ def _main():
                     '--check',
                     action='store_true',
                     help='If difference are found, produce a non-zero return code')
+                             
+    parser.add_argument('-s',
+                    '--show-same',
+                    action='store_true',
+                    help='Show also values that are the same')
                     
     parser.add_argument('-t',
                         '--type',
@@ -193,6 +216,7 @@ def _main():
     args = parser.parse_args()
 
     check = args.check
+    show_same = args.show_same
     verbose = args.verbose
     ordered = args.ordered
     filename1 = args.filename1
@@ -218,7 +242,14 @@ def _main():
     metadatas = read_metadata([filename1, filename2], file_type, mode=mode, meta_key_filter=meta_key_filter, promote=args.promote)
     metadata1 = metadatas[filename1]
     metadata2 = metadatas[filename2]
-    same = compare(metadata1, metadata2, ordered=ordered, diff_format=diff_format)
+    
+    del metadata1['file_guid']
+    del metadata2['file_guid']
+    
+    del metadata1['file_size']
+    del metadata2['file_size']
+    
+    same = compare(metadata1, metadata2, ordered=ordered, diff_format=diff_format, show_same=show_same)
     
     
     if check and not same:
