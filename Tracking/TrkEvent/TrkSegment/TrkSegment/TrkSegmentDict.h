4e29a126e9346a4cd6915deba019f813fdd512e9
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-05-29 12:17:40 +0200

    Added a dictionary for SG::AuxTypeVectorFactory<ElementLink<Trk::SegmentCollection> >.
    In order to avoid https://sft.its.cern.ch/jira/browse/ROOT-10777, for instance
    in the xAODDataSource unit tests.

--- Tracking/TrkEvent/TrkSegment/TrkSegment/TrkSegmentDict.h
+++ Tracking/TrkEvent/TrkSegment/TrkSegment/TrkSegmentDict.h
@@ -1,15 +1,18 @@
+// Dear emacs, this is -*- c++ -*-
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-
 #ifndef TRKSEGMENT_TRKSEGMENTDICT_H
 #define TRKSEGMENT_TRKSEGMENTDICT_H
 
+// Local include(s).
 #include "TrkSegment/SegmentCollection.h"
 #include "TrkSegment/TrackSegment.h"
 #include "TrkSegment/Segment.h"
+
+// EDM include(s).
+#include "AthContainers/tools/AuxTypeVectorFactory.h"
 #include "AthLinks/ElementLink.h"
-#include "TrkSegment/SegmentCollection.h"
 
 class TrkSegmentDummyDict {
   class RIO_onTrack;
@@ -18,6 +21,7 @@ class TrkSegmentDummyDict {
   std::vector<const Trk::RIO_OnTrack*> m_myRioVec;
   ElementLink< Trk::SegmentCollection >                               m_i3;
   std::vector<ElementLink< Trk::SegmentCollection > >                 m_i4;
+  SG::AuxTypeVectorFactory< ElementLink< Trk::SegmentCollection > >   m_i5;
 };
 
-#endif 
+#endif // TRKSEGMENT_TRKSEGMENTDICT_H
