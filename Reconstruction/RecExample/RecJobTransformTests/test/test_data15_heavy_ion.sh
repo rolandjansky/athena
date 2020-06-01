f3840da8f361136c06599fe95dc0976d8dda89c9
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-06-01 13:14:09 +0200

    Update all RecJobTransformTests job requirements.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data15_heavy_ion.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data15_heavy_ion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # art-description: heavy ion reconstruction test from Andrzej Olszewski and Iwona Grabowska-Bold
-# art-memory: 6000
+# art-athena-mt: 4
 # art-type: grid
 # art-include: 21.0/Athena
 # art-include: 21.0-TrigMC/Athena
