0ba469cebb16195ffda25c59e5634ea92bb8805a
Michele Renda <michele.renda@cern.ch>
2020-06-04 17:56:06 +0300

    Restored the modifications of commit
    92cf019e67f4f8dfb5cb3a20a5cb40c6f40a51ab

--- Tools/PyUtils/python/MetaDiff.py
+++ Tools/PyUtils/python/MetaDiff.py
@@ -9,55 +9,83 @@ import logging
 from PyUtils.MetaReader import read_metadata
 
 
-def print_diff(parent_key, obj1, obj2):
+def print_diff(parent_key, obj1, obj2, diff_format):
     """build comparison string for two non-dictionary objects"""
     result = '\n'
-
-    if parent_key is not None:
-        result += '{}:\n'.format(parent_key)
-    result += '''\
-    > {}
-    ----------
-    < {}
-    '''.format(obj1, obj2)
+    
+    if diff_format == 'simple':
+        if obj1 is None:
+            result += '{} has been inserted'.format(parent_key)
+        elif obj2 is None:
+            result += '{} has been deleted'.format(parent_key)
+        else:
+            result += '{} has changed from \'{}\' to \'{}\''.format(parent_key, obj1, obj2)
+        result += '\n'
+    else:
+        if parent_key is not None:
+            result += '{}:\n'.format(parent_key)
+        result += '''\
+        > {}
+        ----------
+        < {}
+        '''.format(obj1, obj2)
 
     return result
 
 
-def print_diff_type(parent_key, obj1, obj2):
+def print_diff_type(parent_key, obj1, obj2, diff_format):
     """Build diff string for objet of different type"""
     result = '\n'
 
-    if parent_key is not None:
-        result += '{}:\n'.format(parent_key)
-    result += '''\
-    > {} (type: {})
-    ----------
-    < {} (type: {})
-    '''.format(obj1, type(obj1), obj2, type(obj2))
+    if diff_format == 'simple':
+        if obj1 is None:
+            result += '{} has been inserted'.format(parent_key)
+        elif obj2 is None:
+            result += '{} has been deleted'.format(parent_key)
+        else:
+            result += '{} has changed changed type from {} (value: \'{}\') to {} (value: \'{}\')'.format(parent_key, type(obj1), obj1,  type(obj2), obj2)
+        result += '\n'
+    else:
+        if parent_key is not None:
+            result += '{}:\n'.format(parent_key)
+        result += '''\
+        > {} (type: {})
+        ----------
+        < {} (type: {})
+        '''.format(obj1, type(obj1), obj2, type(obj2))
 
     return result
 
 
-def print_diff_dict_keys(parent_key, obj1, obj2):
+def print_diff_dict_keys(parent_key, obj1, obj2, diff_format):
     """build diff style string for dictionary objects"""
     result = '\n'
 
-    if parent_key is not None:
-        result += '{}:\n'.format(parent_key)
-    result += '> ' + ', '.join([
-        '{}: {}'.format(k, '{...}' if isinstance(v, dict) else v)
-        for k, v in sorted(obj1.items())])
-    result += '\n----------\n'
-    result += '< ' + ', '.join([
-        '{}: {}'.format(k, '{...}' if isinstance(v, dict) else v)
-        for k, v in sorted(obj2.items())])
+    value1 = ', '.join(['{}: {}'.format(k, '{...}' if isinstance(v, dict) else v)
+            for k, v in sorted(obj1.items())]);
+    value2 = ', '.join(['{}: {}'.format(k, '{...}' if isinstance(v, dict) else v)
+            for k, v in sorted(obj2.items())])
+    
+    if diff_format == 'simple':
+        if obj1 is None:
+            result += '{} has been inserted'.format(parent_key)
+        elif obj2 is None:
+            result += '{} has been deleted'.format(parent_key)
+        else:
+            result += '{} has changed from \'{}\' to \'{}\''.format(parent_key, value1, value2)
+    else:
+        if parent_key is not None:
+            result += '{}:\n'.format(parent_key)
+        result += '> ' + value1
+        result += '\n----------\n'
+        result += '< ' + value2
+    
     result += '\n'
 
     return result
 
 
-def compare(obj1, obj2, parent_key=None, ordered=False):
+def compare(obj1, obj2, parent_key=None, ordered=False, diff_format='simple'):
     """Caclulate difference between two objects
 
     Keyword arguments:
@@ -82,26 +110,26 @@ def compare(obj1, obj2, parent_key=None, ordered=False):
         if isinstance(obj1, dict):
 
             if sorted(obj1.keys()) != sorted(obj2.keys()):
-                result += [print_diff_dict_keys(parent_key, obj1, obj2)]
+                result += [print_diff_dict_keys(parent_key, obj1, obj2, diff_format)]
             else:
                 for key in sorted(set(obj1.keys() + obj2.keys())):
                     if parent_key:
                         child_key = '{}/{}'.format(parent_key, key)
                     else:
                         child_key = key
-                    result += compare(obj1[key], obj2[key], child_key, ordered)
+                    result += compare(obj1[key], obj2[key], child_key, ordered, diff_format)
 
         else:
-            result += [print_diff(parent_key, obj1, obj2)]
+            result += [print_diff(parent_key, obj1, obj2, diff_format)]
 
     else:
-        result += [print_diff_type(parent_key, obj1, obj2)]
+        result += [print_diff_type(parent_key, obj1, obj2, diff_format)]
 
     return result
 
 
-def meta_diff(files, verbose=False, ordered=False, drop=None,
-              mode='lite', meta_key_filter=None, file_type=None, promote=False):
+def meta_diff(files, verbose=False, ordered=False, drop=None, mode='lite',
+              meta_key_filter=None, file_type=None, promote=False, diff_format='simple'):
     """
     Compare the in-file metadata in two given files. Uses PyUtils.MetaReader
     to obtain file content. Generates list of string that show difference.
@@ -116,8 +144,9 @@ def meta_diff(files, verbose=False, ordered=False, drop=None,
                Allowed values are: tiny, lite, peeker, and full
     meta_key_filter -- MetaReader argument selecting keys to retrieve (default
                        get all)
-    file_type -- Type of files, POOL or BS (default: auto-configure)
-    promote   -- MetaReader argument (default: False)
+    file_type    -- Type of files, POOL or BS (default: auto-configure)
+    promote      -- MetaReader argument (default: False)
+    diff_format  -- Show 'simple' or 'diff' style differences (default: 'simple')
     """
     if len(files) != 2:
         raise ValueError("Wrong number of files passes, need two")
@@ -136,4 +165,4 @@ def meta_diff(files, verbose=False, ordered=False, drop=None,
     except TypeError:
         pass
 
-    return compare(metadata[files[0]], metadata[files[1]], ordered=ordered)
+    return compare(metadata[files[0]], metadata[files[1]], ordered=ordered, diff_format=diff_format)
