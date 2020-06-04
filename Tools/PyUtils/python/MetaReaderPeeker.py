8775b4b4a039fad9fd7b07445fd36c71a1fc1773
Michele Renda <michele.renda@cern.ch>
2020-06-04 16:42:40 +0000

    Resolve ATLASDQ-749 "Fix  2"

--- Tools/PyUtils/python/MetaReaderPeeker.py
+++ Tools/PyUtils/python/MetaReaderPeeker.py
@@ -9,7 +9,7 @@ metadata_all_files = {}
 
 
 def _setup():
-	
+    
     from PyUtils.MetaReader import read_metadata
 
     from AthenaCommon.Logging import logging
@@ -20,10 +20,9 @@ def _setup():
 
     # get input file name
     from RecExConfig.RecoFunctions import InputFileNames
-    inFiles = InputFileNames()
         
     from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
-    if athenaCommonFlags.isOnline() and not inFiles:
+    if athenaCommonFlags.isOnline() and (not InputFileNames() or all([f.strip() == '' for f in InputFileNames()])):
         # set minimal items of inputFileSummary
         metadata = {
           'file_type':'BS',
@@ -31,6 +30,7 @@ def _setup():
           'TagStreamsRef':''
         }
     else:
+        inFiles = InputFileNames()
         if len(inFiles) < 1:
             msg.warning("No input files specified yet! Cannot do anything.")
             return
