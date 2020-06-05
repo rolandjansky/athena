ad3e5f44a97dea546cf01bff9350c7827f01161f
Bastian Schlag <bastian.schlag@cern.ch>
2020-06-05 17:16:51 +0200

    remove ActsPrimaryVtxFinderAlg for now

--- Tracking/Acts/ActsPriVtxFinder/src/components/ActsPriVtxFinder_entries.cxx
+++ Tracking/Acts/ActsPriVtxFinder/src/components/ActsPriVtxFinder_entries.cxx
@@ -2,9 +2,9 @@
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "ActsPriVtxFinder/ActsPrimaryVtxFinderAlg.h"
+//#include "ActsPriVtxFinder/ActsPrimaryVtxFinderAlg.h"
 #include "ActsPriVtxFinder/ActsAdaptiveMultiPriVtxFinderTool.h"
 
-DECLARE_COMPONENT( ActsPrimaryVtxFinderAlg )
+// DECLARE_COMPONENT( ActsPrimaryVtxFinderAlg )
 DECLARE_COMPONENT( ActsAdaptiveMultiPriVtxFinderTool )
 
