135d68562dee79b04e9eca2d50e7813ee5f7130a
Frank Winklmeier <frank.winklmeier@cern.ch>
2020-06-13 14:00:56 +0200

    TrigFake: Package cleanup
    - move headers to src/
    - remove atlas_depends_on_subdirs
    - delete obsolete files
    - remove library and update link dependencies

--- Trigger/TrigFake/share/jobOfragment_TrigFake.py
+++
@@ -1,9 +0,0 @@
-theApp.Dlls += [ "TrigFake" ]
-if doTruth:
-    theApp.TopAlg += [ "ZVertexFromKine"  ]
-
-    if DetDescrVersion[0:2]=="DC2":
-        Algorithm("ZVertexFromKine").McEventCollectionName = "G4Truth"
-    else:
-        Algorithm("ZVertexFromKine").McEventCollectionName = "TruthEvent"
-
