73f8431a7aaccb903824b8978aed764900b7a79a
scott snyder <sss@karma>
2020-06-17 16:43:08 +0200

    TrigNavTools: Merge TrigNavigationSlimmingTool into TrigNavigationThinningSvc.
    Remove TrigNavigationSlimmingTool by merging it into TrigNavigationThinningSvc.

--- Trigger/TrigEvent/TrigNavTools/src/components/TrigNavTools_entries.cxx
+++ Trigger/TrigEvent/TrigNavTools/src/components/TrigNavTools_entries.cxx
@@ -1,8 +1,6 @@
 
-#include "TrigNavTools/TrigNavigationSlimmingTool.h"
 #include "../TrigNavigationThinningSvc.h"
 
 
-DECLARE_COMPONENT( HLT::TrigNavigationSlimmingTool )
 DECLARE_COMPONENT( TrigNavigationThinningSvc )
 
