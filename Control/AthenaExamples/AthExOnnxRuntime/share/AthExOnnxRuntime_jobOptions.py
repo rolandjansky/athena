d3f6c5a90a39d6cc945e3d74e784668468127709
Debo <debottam.bakshi.gupta@cern.ch>
2020-06-19 12:28:49 +0200

    jobOption sanitization

--- Control/AthenaExamples/AthExOnnxRuntime/share/AthExOnnxRuntime_jobOptions.py
+++ Control/AthenaExamples/AthExOnnxRuntime/share/AthExOnnxRuntime_jobOptions.py
@@ -3,12 +3,21 @@
 # Get a	random no. between 0 to	10k for	test sample
 from random import randint
 
+import optparse
+parser = optparse.OptionParser()
+parser.add_option( '-s', '--submission-dir', dest = 'submission_dir',
+                   action = 'store', type = 'string', default = 'submitDir',
+                   help = 'Submission directory for EventLoop' )
+( options, args ) = parser.parse_args()
+
+
 # Set up (Py)ROOT.
 import ROOT
 
 # Create an EventLoop job.
 job = ROOT.EL.Job()
 job.options().setDouble( ROOT.EL.Job.optMaxEvents, 10 )
+job.options().setString( ROOT.EL.Job.optSubmitDirMode, 'unique-link')
 
 # Create the algorithm's configuration.
 from AnaAlgorithm.DualUseConfig import createAlgorithm
@@ -21,3 +30,4 @@ job.algsAdd( alg )
 alg.TestSample = randint(0, 9999)
 # Run the job using the direct driver.
 driver = ROOT.EL.DirectDriver()
+driver.submit( job, options.submission_dir )
