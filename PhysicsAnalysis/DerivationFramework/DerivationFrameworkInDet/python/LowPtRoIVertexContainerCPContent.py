2c55d6a7cd7b349e37219e223330e0bddb4aeeb3
William Patrick Mccormack <william.patrick.mc.cormack.iii@cern.ch>
2020-06-20 17:50:21 +0000

    Adding Low-Pt RoI tracks to DAOD_STDM7 derivation

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkInDet/python/LowPtRoIVertexContainerCPContent.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration 
+
+LowPtRoIVertexContainerCPContent = [
+"LowPtRoIVertexContainer",
+"LowPtRoIVertexContainerAux.z.IsHS.boundary_high.boundary_low.perigee_z0_lead.perigee_z0_sublead"
+]
