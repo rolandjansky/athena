62da5b5d5118b29cf1defd660cb56e73b1e767b4
scott snyder <snyder@bnl.gov>
2020-06-26 21:43:58 +0200

    fix typo

--- Control/AthToolSupport/AsgTools/Root/ToolStore.cxx
+++ Control/AthToolSupport/AsgTools/Root/ToolStore.cxx
@@ -99,7 +99,7 @@ namespace asg {
       if( itool != tools().end() )
         return itool->second;
 
-      if (name.substr(0, 8) == "ToolSvc.") {
+      if (name.substr(0, 8) != "ToolSvc.") {
         itool = s_tools.find( "ToolSvc." + name );
         if( itool != s_tools.end() )
           return itool->second;
