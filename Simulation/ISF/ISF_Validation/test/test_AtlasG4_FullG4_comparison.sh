9fd92da80b31af6295bca9b840d14b4207f05a2d
Michael Duehrssen <michael.duehrssen@cern.ch>
2020-06-26 17:04:49 +0200

    Address comments in MR34043

--- Simulation/ISF/ISF_Validation/test/test_AtlasG4_FullG4_comparison.sh
+++ Simulation/ISF/ISF_Validation/test/test_AtlasG4_FullG4_comparison.sh
@@ -49,12 +49,15 @@ AtlasG4_tf.py \
 rc2=$?
 echo  "art-result: $rc2 simulation AtlasG4"
 
+rc3=-999
 if [ $rc1 -eq 0 ]
 then
   if [ $rc2 -eq 0 ]
   then
     # Compare the merged outputs
     acmd.py diff-root HITS.FullG4.pool.root HITS.AtlasG4.pool.root --error-mode resilient --ignore-leaves RecoTimingObj_p1_EVNTtoHITS_timings TrackRecordCollection_p2_CaloEntryLayer TrackRecordCollection_p2_MuonEntryLayer TrackRecordCollection_p2_MuonExitLayer RecoTimingObj_p1_RAWtoESD_mems RecoTimingObj_p1_RAWtoESD_timings RAWtoESD_mems RAWtoESD_timings ESDtoAOD_mems ESDtoAOD_timings HITStoRDO_timings RAWtoALL_mems RAWtoALL_timings RecoTimingObj_p1_RAWtoALL_mems RecoTimingObj_p1_RAWtoALL_timings RecoTimingObj_p1_EVNTtoHITS_timings EVNTtoHITS_timings RecoTimingObj_p1_Bkg_HITStoRDO_timings index_ref
-    echo "art-result: $? comparison"
+    rc3=$?
   fi
 fi
+echo "art-result: $rc3 comparison"
+
