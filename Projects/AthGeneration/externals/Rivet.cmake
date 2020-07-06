1384f5f22264a7a968d02ded6c703036238980f0
Christian Gutschow <chris.g@cern.ch>
2020-07-06 12:30:21 +0100

    update of Rivet/YODA to 3.1.2/1.8.2

--- Projects/AthGeneration/externals/Rivet.cmake
+++ Projects/AthGeneration/externals/Rivet.cmake
@@ -1,6 +1,6 @@
 #
 # File specifying the location of Rivet to use.
 #
-set( RIVET_LCGVERSION 3.1.1 )
+set( RIVET_LCGVERSION 3.1.2 )
 set( RIVET_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/rivet/${RIVET_LCGVERSION}/${LCG_PLATFORM} )
