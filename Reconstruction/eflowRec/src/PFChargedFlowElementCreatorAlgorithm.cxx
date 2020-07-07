c70ac557be4a073757419b60403c53ae895dfe93
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-07-07 18:18:51 +0200

    Add creation of FE and set its track links + charge.

--- Reconstruction/eflowRec/src/PFChargedFlowElementCreatorAlgorithm.cxx
+++ Reconstruction/eflowRec/src/PFChargedFlowElementCreatorAlgorithm.cxx
@@ -1,5 +1,9 @@
+//eflowRec includes
 #include "eflowRec/PFChargedFlowElementCreatorAlgorithm.h"
+#include "eflowRec/eflowRecTrack.h"
 
+//EDM includes
+#include "xAODBase/IParticleContainer.h"
 #include "xAODPFlow/FlowElementAuxContainer.h"
 
 PFChargedFlowElementCreatorAlgorithm::PFChargedFlowElementCreatorAlgorithm(const std::string& name, ISvcLocator* pSvcLocator) :
@@ -36,5 +40,34 @@ StatusCode PFChargedFlowElementCreatorAlgorithm::execute(const EventContext& ctx
 StatusCode PFChargedFlowElementCreatorAlgorithm::finalize(){ return StatusCode::SUCCESS; }
 
 void PFChargedFlowElementCreatorAlgorithm::createChargedFlowElements(const eflowCaloObject& energyFlowCaloObject, bool addClusters, SG::WriteHandle<xAOD::FlowElementContainer>& chargedFlowElementContainerWriteHandle) const {
+
+  /* Loop over all tracks in the eflowCaloObject */
+  int nTracks = energyFlowCaloObject.nTracks();
+  for (int iTrack = 0; iTrack < nTracks; ++iTrack) {
+
+    eflowRecTrack* efRecTrack = energyFlowCaloObject.efRecTrack(iTrack);
+
+    /* Skip tracks that haven't been subtracted */
+    if (false == m_eOverPMode){
+      if (!efRecTrack->isSubtracted()){ continue; }
+    }
+
+    /* Create new xAOD::FlowElement */
+    xAOD::FlowElement* thisFE = new xAOD::FlowElement();
+    chargedFlowElementContainerWriteHandle->push_back(thisFE);
+
+    /* Get the track elementLink and add it to the xAOD:FE. 
+    Note we first have to convert it to an IParticle ElementLink. */
+    ElementLink<xAOD::TrackParticleContainer> theTrackLink = efRecTrack->getTrackElemLink();
+    ElementLink< xAOD::IParticleContainer > theIParticleTrackLink; 
+    theIParticleTrackLink.resetWithKeyAndIndex(theTrackLink.persKey(),theTrackLink.persIndex() ); 
+    std::vector<ElementLink<xAOD::IParticleContainer> > vecIParticleTrackLinkContainer;
+    vecIParticleTrackLinkContainer.push_back(theIParticleTrackLink);
+    thisFE->setChargedObjectLinks(vecIParticleTrackLinkContainer);
+
+    //Now set the charge
+    thisFE->setCharge(efRecTrack->getTrack()->charge());
+
+  }//loop over eflowRecTracks
 
 }
