9542b2455d993efe02c99bb43973ed654b1c0de5
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-07-07 18:05:05 +0200

    Fill in execute of PFChargedFlowElementCreatorAlgorithm.

--- Reconstruction/eflowRec/src/PFChargedFlowElementCreatorAlgorithm.cxx
+++ Reconstruction/eflowRec/src/PFChargedFlowElementCreatorAlgorithm.cxx
@@ -1,5 +1,7 @@
 #include "eflowRec/PFChargedFlowElementCreatorAlgorithm.h"
 
+#include "xAODPFlow/FlowElementAuxContainer.h"
+
 PFChargedFlowElementCreatorAlgorithm::PFChargedFlowElementCreatorAlgorithm(const std::string& name, ISvcLocator* pSvcLocator) :
   AthReentrantAlgorithm(name,pSvcLocator)
 {
@@ -17,6 +19,15 @@ StatusCode PFChargedFlowElementCreatorAlgorithm::initialize(){
 StatusCode PFChargedFlowElementCreatorAlgorithm::execute(const EventContext& ctx) const {
 
   ATH_MSG_DEBUG("Starting PFOChargedCreatorAlgorithm::execute");
+
+  SG::WriteHandle<xAOD::FlowElementContainer> chargedFlowElementContainerWriteHandle(m_chargedFlowElementContainerWriteHandleKey,ctx);
+  ATH_CHECK(chargedFlowElementContainerWriteHandle.record(std::make_unique<xAOD::FlowElementContainer>(),std::make_unique<xAOD::FlowElementAuxContainer>()));
+  
+  /* Create Charged FlowElements from all eflowCaloObjects */
+  SG::ReadHandle<eflowCaloObjectContainer> eflowCaloObjectContainerReadHandle(m_eflowCaloObjectContainerReadHandleKey,ctx);
+  for (auto thisEflowCaloObject : *eflowCaloObjectContainerReadHandle) createChargedFlowElements(*thisEflowCaloObject,true,chargedFlowElementContainerWriteHandle);
+
+  std::sort(chargedFlowElementContainerWriteHandle->begin(), chargedFlowElementContainerWriteHandle->end(), [] (const xAOD::FlowElement* flowElement1, const xAOD::FlowElement* flowElement2) {return flowElement1->pt()>flowElement2->pt();});
 
   return StatusCode::SUCCESS;  
 
