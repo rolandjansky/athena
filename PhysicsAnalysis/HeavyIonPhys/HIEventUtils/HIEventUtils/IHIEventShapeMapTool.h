7679086c6c1893252d4cae7c961a6308b72b0236
rlongo <riccardo.longo@cern.ch>
2020-07-07 19:36:04 -0500

    Improvements to the new HIEventShapeMapTool and ts interface

--- PhysicsAnalysis/HeavyIonPhys/HIEventUtils/HIEventUtils/IHIEventShapeMapTool.h
+++ PhysicsAnalysis/HeavyIonPhys/HIEventUtils/HIEventUtils/IHIEventShapeMapTool.h
@@ -2,28 +2,34 @@
   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef HIEVENTUTILS_HIEVENTSHAPEMAPTOOL_H
-#define HIEVENTUTILS_HIEVENTSHAPEMAPTOOL_H
+#ifndef HIEVENTUTILS_IHIEVENTSHAPEMAPTOOL_H
+#define HIEVENTUTILS_IHIEVENTSHAPEMAPTOOL_H
 
 #include "xAODHIEvent/HIEventShape.h"
 #include "HIEventUtils/HIEventShapeIndex.h"
 #include "AsgTools/IAsgTool.h"
 #include "AsgTools/AsgTool.h"
+
 #include <string>
+#include <map>
 
 class IHIEventShapeMapTool : virtual public asg::AsgTool
 {
-    ASG_TOOL_INTERFACE(IHITowerWeightTool)
+    ASG_TOOL_INTERFACE(IHIEventShapeMapTool)
 
     public:
 
     virtual ~IHIEventShapeMapTool() {};
+
     virtual StatusCode initialize() = 0;
-    virtual const HIEventShapeIndex* getIndex(std::string key)  = 0;
+    virtual StatusCode insert(std::string key, const HIEventShapeIndex& index, bool clobber=false) = 0;
+    virtual const HIEventShapeIndex* getIndex(std::string key) const  = 0;
     virtual bool hasKey(std::string key)  = 0;
-    virtual std::map<std::string,HIEventShapeIndex> getMap() { return m_map; }
+
+    virtual std::map<std::string,HIEventShapeIndex> getMap() const { return m_map; }
 
     private:
     std::map<std::string,HIEventShapeIndex> m_map;
+
 };
 #endif
--- PhysicsAnalysis/HeavyIonPhys/HIEventUtils/Root/HIEventShapeMapTool.cxx
+++ PhysicsAnalysis/HeavyIonPhys/HIEventUtils/Root/HIEventShapeMapTool.cxx
@@ -6,18 +6,58 @@
 #include "HIEventUtils/HIEventShapeIndex.h"
 #include "HIEventUtils/HIEventShapeMapTool.h"
 
-HIEventShapeMapTool::HIEventShapeMapTool() : asg::AsgTool(){
+HIEventShapeMapTool::HIEventShapeMapTool(const std::string& n) : asg::AsgTool(n){
 
 }
 
-const HIEventShapeIndex* HIEventShapeMapTool::getIndex(std::string key)
+StatusCode HIEventShapeMapTool::initialize(){
+  //The idea is that the tool is being configured w/ all the necessary info and intialiazed accordingly
+  //This is to prevent access and changes afterwards, that have to be mutexed
+  HIEventShapeIndex index_1;
+  index_1.setBinning(HIEventShapeIndex::COMPACT);
+  //What was done before at level of HIEventShapeFillerTool
+  ATH_CHECK(insert(m_containerName,index_1));
+  //Now the HIEventShapeJetIteration - here we depend from the initialization flags
+  std::vector < HIEventShapeIndex > index_i;
+  if( m_useCaloCell == true || (m_useCaloCell == false && m_useClusters == false) ){
+    for(int i = 0; i < (int)m_JetIterNames.size(); i++)
+    {
+      index_i.push_back(HIEventShapeIndex());
+      index_i.back().setBinning(HIEventShapeIndex::COMPACT);
+      ATH_CHECK(insert(m_JetIterNames[i],index_i.back()));
+    }
+  }
+  else if ( m_useCaloCell == false && m_useClusters == true ){
+    for(int i = 0; i < (int)m_JetIterNames.size(); i++)
+    {
+      index_i.push_back(HIEventShapeIndex());
+      index_i.back().setBinning(HIEventShapeIndex::TOWER);
+      ATH_CHECK(insert(m_JetIterNames[i],index_i.back()));
+    }
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode HIEventShapeMapTool::insert(std::string key, const HIEventShapeIndex& index, bool clobber)
 {
-  auto itr=m_map.find(key);
-  if(itr==m_map.end()) return nullptr;
+  std::map<std::string,HIEventShapeIndex>::iterator mItr=getMap().find(key);
+  if(mItr!=getMap().end())
+  {
+    if(clobber) mItr->second=index;
+  }
+  else mItr=getMap().insert(std::pair<std::string,HIEventShapeIndex>(key,index)).first;
+  //return &(mItr->second);
+  return StatusCode::SUCCESS;
+}
+
+const HIEventShapeIndex* HIEventShapeMapTool::getIndex(std::string key) const
+{
+  auto itr=getMap().find(key);
+  if(itr==getMap().end()) return nullptr;
   return &(itr->second);
 }
 
 bool HIEventShapeMapTool::hasKey(std::string key)
 {
-  return (m_map.find(key)!=m_map.end());
+  return (getMap().find(key)!=getMap().end());
 }
