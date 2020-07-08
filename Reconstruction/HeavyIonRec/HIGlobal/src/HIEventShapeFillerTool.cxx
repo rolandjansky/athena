065030be8738baa70f1eb723a2849991da6dbe51
rlongo <riccardo.longo@cern.ch>
2020-07-08 18:05:04 -0500

    Added the HIEventShapeMapTool and replaced the static old instance with it in the code

--- Reconstruction/HeavyIonRec/HIGlobal/src/HIEventShapeFillerTool.cxx
+++ Reconstruction/HeavyIonRec/HIGlobal/src/HIEventShapeFillerTool.cxx
@@ -8,6 +8,7 @@
 #include <CaloEvent/CaloCellContainer.h>
 #include <xAODHIEvent/HIEventShape.h>
 #include "HIEventUtils/HIEventShapeMap.h"
+#include "HIEventUtils/HIEventShapeMapTool.h"
 
 #include <iostream>
 #include <iomanip>
@@ -32,9 +33,7 @@ StatusCode HIEventShapeFillerTool::initializeCollection(xAOD::HIEventShapeContai
    //tool is initialized only once
    if(!m_index)
    {
-     HIEventShapeIndex index;
-     index.setBinning(HIEventShapeIndex::COMPACT);
-     m_index=HIEventShapeMap::getMap()->insert(getContainerName(),index);
+     m_index=m_eventShapeMapTool->getIndex(getContainerName());
    }
    //fix this to have proper name passing
    //use tool to initialize event shape object
