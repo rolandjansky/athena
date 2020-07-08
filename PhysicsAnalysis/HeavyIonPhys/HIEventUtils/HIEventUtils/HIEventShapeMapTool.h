4bcd6ce2f36a06761e8ea63b31325aad9788cf9f
rlongo <riccardo.longo@cern.ch>
2020-07-07 19:36:04 -0500

    Improvements to the new HIEventShapeMapTool and ts interface

--- PhysicsAnalysis/HeavyIonPhys/HIEventUtils/HIEventUtils/HIEventShapeMapTool.h
+++ PhysicsAnalysis/HeavyIonPhys/HIEventUtils/HIEventUtils/HIEventShapeMapTool.h
@@ -10,22 +10,34 @@
 #include "HIEventUtils/IHIEventShapeMapTool.h"
 #include "AsgTools/IAsgTool.h"
 #include "AsgTools/AsgTool.h"
+
 #include <string>
 #include <map>
 
-class HIEventShapeMapTool : virtual public asg::AsgTool
+class HIEventShapeMapTool : virtual public asg::AsgTool, virtual public IHIEventShapeMapTool
 {
   ASG_TOOL_CLASS(HIEventShapeMapTool,IHIEventShapeMapTool)
+
   public:
 
-  HIEventShapeMapTool();
+  HIEventShapeMapTool(const std::string& n);
   virtual ~HIEventShapeMapTool() {};
+
   virtual StatusCode initialize() override;
-  virtual const HIEventShapeIndex* getIndex(std::string key) override;
+  virtual StatusCode insert(std::string key, const HIEventShapeIndex& index, bool clobber=false) override;
+  virtual const HIEventShapeIndex* getIndex(std::string key) const override;
+
   virtual bool hasKey(std::string key) override;
 
   private:
-  Gaudi::Property< string > m_containerName { this, "ContainerName", "", "use Clusters boolean switch" }  ;
+  //This type of tool should be able to retrieve all the configuration that set the event shape binning
+  Gaudi::Property< std::string > m_containerName { this, "ContainerName", "", "Container Name" };
+
+  Gaudi::Property< std::vector < std::string > > m_JetIterNames { this, "JetIterNames", {}, "Key for the shape passed in input to HI Event Shape Jet Iteration" };
+
+  Gaudi::Property< bool > m_useCaloCell { this, "UseCaloCell", false , "Flag for Calo Cells" };
+
+  Gaudi::Property< bool > m_useClusters { this, "UseClusters", true  , "Flag for Clusters" };
 
 };
 #endif
