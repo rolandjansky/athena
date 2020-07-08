978c2a901a787769c96ab0327550e8200a87c619
Frank Winklmeier <fwinkl@cern>
2020-07-08 10:06:15 +0200

    TrigInDetTrackFitter: Delete TrigInDetBremDetectionTool
    Delete obsolete `[I]TrigInDetBremDetectionTool` and enable
    thread-checker.

--- Trigger/TrigTools/TrigInDetTrackFitter/src/components/TrigInDetTrackFitter_entries.cxx
+++ Trigger/TrigTools/TrigInDetTrackFitter/src/components/TrigInDetTrackFitter_entries.cxx
@@ -1,9 +1,7 @@
 #include "../TrigInDetTrackFitter.h"
-#include "../TrigInDetBremDetectionTool.h"
 #include "../TrigDkfTrackMakerTool.h"
 #include "../TrigL2ResidualCalculator.h"
 
 DECLARE_COMPONENT( TrigInDetTrackFitter )
-DECLARE_COMPONENT( TrigInDetBremDetectionTool )
 DECLARE_COMPONENT( TrigDkfTrackMakerTool )
 DECLARE_COMPONENT( TrigL2ResidualCalculator )
