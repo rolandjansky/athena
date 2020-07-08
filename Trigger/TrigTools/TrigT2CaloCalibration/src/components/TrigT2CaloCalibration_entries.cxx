f68a60243fc769f51c60b509f257f3c93abe0b83
Frank Winklmeier <fwinkl@cern>
2020-07-08 10:40:29 +0200

    Delete IT2HadCalibTool, T2GSCalibTool, T2JESCalibTool
    Obsolete calibration tools for unused L2 jets.

--- Trigger/TrigTools/TrigT2CaloCalibration/src/components/TrigT2CaloCalibration_entries.cxx
+++ Trigger/TrigTools/TrigT2CaloCalibration/src/components/TrigT2CaloCalibration_entries.cxx
@@ -1,13 +1,9 @@
-#include "TrigT2CaloCalibration/T2JESCalibTool.h"
-#include "TrigT2CaloCalibration/T2GSCalibTool.h"
 #include "TrigT2CaloCalibration/EgammaHitsCalibration.h"
 #include "TrigT2CaloCalibration/EgammaLWCalibration.h"
 #include "TrigT2CaloCalibration/EgammaGapCalibration.h"
 #include "TrigT2CaloCalibration/EgammaSshapeCalibration.h"
 #include "TrigT2CaloCalibration/EgammaTransitionRegions.h"
 
-DECLARE_COMPONENT( T2JESCalibTool )
-DECLARE_COMPONENT( T2GSCalibTool )
 DECLARE_COMPONENT( EgammaHitsCalibration )
 DECLARE_COMPONENT( EgammaLWCalibration )
 DECLARE_COMPONENT( EgammaGapCalibration )
