89507e9314b6c3e85f335d07e170712109267e5c
Patrick Scholer <patick.scholer@cern.ch>
2020-07-08 10:01:13 +0200

    Adding override to interface functions in MMClusterOnTrackCreator.h; adding joboption to doc string

--- MuonSpectrometer/MuonReconstruction/MuonRIO_OnTrackCreators/MuonClusterOnTrackCreator/src/MMClusterOnTrackCreator.h
+++ MuonSpectrometer/MuonReconstruction/MuonRIO_OnTrackCreators/MuonClusterOnTrackCreator/src/MMClusterOnTrackCreator.h
@@ -35,13 +35,14 @@ namespace Muon {
          @code createRIO_OnTrack(const Trk::PrepRawData& RIO, const Trk::GlobalPosition& GP) const @endcode
        
        JobOptions Flags:
+       - MuonIdHelperTool : choose muon id helper tool
    */
  class MMClusterOnTrackCreator : public AthAlgTool, virtual public IMuonClusterOnTrackCreator {
   public:
     
     MMClusterOnTrackCreator(const std::string&,const std::string&,const IInterface*);
     virtual ~MMClusterOnTrackCreator()=default;
-    virtual StatusCode initialize();
+    virtual StatusCode initialize() override;
  
     /** @brief Create new Muon::MuonClusterOnTrack from a Trk::PrepRawData and a predicted Trk::TrackParameter. 
 	@param RIO Trk::PrepRawData object to be calibrated
@@ -50,7 +51,7 @@ namespace Muon {
 	The ownership of the new Muon::MuonClusterOnTrack is passed to the client calling the tool
     */       
     virtual const MuonClusterOnTrack* createRIO_OnTrack(const Trk::PrepRawData& RIO,
-                                                        const Amg::Vector3D& GP) const;
+                                                        const Amg::Vector3D& GP) const override;
 
     /** @brief Create new Muon::MuonClusterOnTrack from a Trk::PrepRawData and a prediction of the global position and direction.
 	It is only implemented for the CSCs, for RPC and TGC Trk::PrepRawData the result is the same as for the routine without the direction. 
@@ -62,7 +63,7 @@ namespace Muon {
     */
     virtual const MuonClusterOnTrack* createRIO_OnTrack(const Trk::PrepRawData& RIO,
                                                         const Amg::Vector3D& GP,
-                                                        const Amg::Vector3D& GD) const;
+                                                        const Amg::Vector3D& GD) const override;
  
     /** @brief Create new Muon::MuonClusterOnTrack from a Trk::PrepRawData and the predicted Trk::TrackParameter at the measurement surface. 
 	@param RIO Trk::PrepRawData object to be calibrated
@@ -70,7 +71,7 @@ namespace Muon {
 	@return a pointer to a new Muon::MuonClusterOnTrack object, zero if calibration failed.
 	The ownership of the new Muon::MuonClusterOnTrack is passed to the client calling the tool
     */
-    virtual const MuonClusterOnTrack* correct(const Trk::PrepRawData& RIO,const Trk::TrackParameters& TP) const; 
+    virtual const MuonClusterOnTrack* correct(const Trk::PrepRawData& RIO,const Trk::TrackParameters& TP) const override; 
   
   private:
 
