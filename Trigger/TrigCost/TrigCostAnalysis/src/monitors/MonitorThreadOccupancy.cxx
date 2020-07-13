353da416154b8f619da59dffeeb70f2aa77b1c2d
Tim Martin <Tim.Martin@cern.ch>
2020-07-13 18:54:46 +0200

    (C) and typo

--- Trigger/TrigCost/TrigCostAnalysis/src/monitors/MonitorThreadOccupancy.cxx
+++ Trigger/TrigCost/TrigCostAnalysis/src/monitors/MonitorThreadOccupancy.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "MonitorThreadOccupancy.h"
@@ -14,7 +14,7 @@ MonitorThreadOccupancy::MonitorThreadOccupancy(const std::string& name, const Mo
 StatusCode MonitorThreadOccupancy::newEvent(const CostData& data, const float weight) {
 
   // Only look at events in the master slot
-  if (not data.isMaterSlot()) {
+  if (not data.isMasterSlot()) {
     return StatusCode::SUCCESS;
   }
 
