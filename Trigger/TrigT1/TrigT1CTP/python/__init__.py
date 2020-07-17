0b2187f0904c1851a67dde6e9d23ed52f70fee5c
Frank Winklmeier <fwinkl@cern>
2020-07-17 13:33:52 +0200

    TrigT1CTP: Cleanup cmake config, enable flake8
    Also delete an obsolete python module.

--- Trigger/TrigT1/TrigT1CTP/python/__init__.py
+++ Trigger/TrigT1/TrigT1CTP/python/__init__.py
@@ -1,8 +1,1 @@
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
-
-#
-# $Id: __init__.py,v 1.4 2008-06-10 13:48:37 efeld Exp $
-#
-__version__ = '1.0.0'
-__author__  = 'Wolfgang.Ehrenfeld@desy.de,berge@cern.ch'
-__all__ = [ 'TrigT1CTPConfig', 'TrigT1CTPMonitoring', 'TrigT1CTPAlgorithmConfig' ]
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
