e4dac4c6cef4ea920cbe22604c20fab03c16f9c7
Joerg Stelzer <joerg.stelzer@cern.ch>
2020-07-24 11:46:16 +0200

    Update test references

--- Trigger/TriggerCommon/TriggerMenuMT/scripts/trigCompareOldandNewL1Menus.py
+++ Trigger/TriggerCommon/TriggerMenuMT/scripts/trigCompareOldandNewL1Menus.py
@@ -8,13 +8,44 @@
 # this helps with the consistent transition from old to new L1 menu. It should be 
 # deleted once we have concluded this transition
 
-import os
 import sys
-import json
 
+from AthenaCommon.Logging import log
 from TriggerMenuMT.LVL1MenuConfig.LVL1.XMLReader import L1MenuXMLReader
 from TrigConfIO.L1TriggerConfigAccess import L1MenuAccess
 from TrigConfIO.HLTTriggerConfigAccess import HLTMenuAccess
+
+def compareBothL1Menus(l1items_xml, l1items_json):
+    # legacyCaloItems = [ k for k,v in l1items_json.items() if 'legacy' in v  ]
+
+    itemNames_xml = [x['name'] for x in l1items_xml]
+    itemNames_json = l1items_json.keys()
+    ids_xml = dict([ (x['name'],int(x['ctpid'])) for x in l1items_xml])
+    ids_json = dict([ (x['name'],x['ctpid']) for x in l1items_json.values()])
+
+    if l1items_xml and l1items_json:
+        itemsOnlyInJson =  list( set(itemNames_json) - set(itemNames_xml)  )
+        itemsOnlyInXML =  list( set(itemNames_xml) - set(itemNames_json)  )
+        log.info("These %i items are new in the json version" , len(itemsOnlyInJson))
+        log.info(itemsOnlyInJson)
+        log.info("\nThese %i items have disappeared in the json version" , len(itemsOnlyInXML))
+        log.info(itemsOnlyInXML)
+
+    inboth = set(itemNames_json).intersection(set(itemNames_xml))
+    noMatchId = []
+    for name in sorted(inboth):
+        if ids_xml[name] != ids_json[name]:
+            noMatchId += [(name, ids_xml[name], ids_json[name])]
+    
+    log.info("\nFrom %i items that are in both, these %i have non-matching CTP id's" , len(inboth),len(noMatchId))
+    log.info("Name, CTPID in xml, CTPID in json")
+    for x in noMatchId:
+        log.info(x)
+
+
+
+
+
 
 def main():
     
@@ -23,7 +54,7 @@ def main():
     hltinput = None
 
     if len(sys.argv) <3:
-        print("Please specify at least two menu files (L1Menu.xml, L1Menu.json, HLTMenu.json)")
+        log.info("Please specify at least two menu files (L1Menu.xml, L1Menu.json, HLTMenu.json)")
 
     for fileName in sys.argv[1:]:
         if fileName.endswith(".json"):
@@ -37,31 +68,13 @@ def main():
             l1menu = L1MenuXMLReader(fileName)
             l1items_xml = l1menu.getL1Items()
 
-    legacyCaloItems = [ k for k,v in l1items_json.items() if 'legacy' in v  ]
-
-    itemNames_xml = [x['name'] for x in l1items_xml]
-    itemNames_json = l1items_json.keys()
-    ids_xml = dict([ (x['name'],int(x['ctpid'])) for x in l1items_xml])
-    ids_json = dict([ (x['name'],x['ctpid']) for x in l1items_json.values()])
 
     if l1items_xml and l1items_json:
-        itemsOnlyInJson =  list( set(itemNames_json) - set(itemNames_xml)  )
-        itemsOnlyInXML =  list( set(itemNames_xml) - set(itemNames_json)  )
-        print("These %i items are new in the json version" % len(itemsOnlyInJson))
-        print(itemsOnlyInJson)
-        print("\nThese %i items have disappeared in the json version" % len(itemsOnlyInXML))
-        print(itemsOnlyInXML)
+        compareBothL1Menus(l1items_xml, l1items_json)
 
-    inboth = set(itemNames_json).intersection(set(itemNames_xml))
-    noMatchId = []
-    for name in sorted(inboth):
-        if ids_xml[name] != ids_json[name]:
-            noMatchId += [(name, ids_xml[name], ids_json[name])]
-    
-    print("\nFrom %i items that are in both, these %i have non-matching CTP id's" % (len(inboth),len(noMatchId)))
-    print("Name, CTPID in xml, CTPID in json")
-    for x in noMatchId:
-        print(x)
+    if l1items_json and hltinput:
+        pass # check not yet implemented
+
 
     return 0
     
