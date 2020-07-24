2218817a8b6b74d905c3e0ef22a7664da033b4ed
Nils Krumnack <krumnack@iastate.edu>
2020-07-24 10:16:48 -0500

    add a unit test for AsgDataHandles
    I should have added that right when I added the read handles, but
    better late than never.

---
+++ Control/AthToolSupport/AsgExampleTools/Root/DataHandleTestTool.cxx
@@ -0,0 +1,63 @@
+/*
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+*/
+
+/// @author Nils Krumnack
+
+
+
+//
+// includes
+//
+
+#include <AsgExampleTools/DataHandleTestTool.h>
+
+#include <AsgDataHandles/ReadHandle.h>
+#include <AsgTesting/UnitTest.h>
+#include <gtest/gtest.h>
+#include <map>
+
+//
+// method implementations
+//
+
+namespace asg
+{
+  DataHandleTestTool ::
+  DataHandleTestTool (const std::string& val_name)
+    : AsgTool (val_name)
+  {
+    declareProperty ("readFailure", m_readFailure, "whether to expect a read failure");
+  }
+
+
+
+  DataHandleTestTool ::
+  ~DataHandleTestTool ()
+  {
+  }
+
+
+
+  StatusCode DataHandleTestTool ::
+  initialize ()
+  {
+    ANA_CHECK (m_readKey.initialize ());
+    return StatusCode::SUCCESS;
+  }
+
+
+
+  void DataHandleTestTool ::
+  runTest ()
+  {
+    const xAOD::MuonContainer *muonsStore {nullptr};
+    ASSERT_SUCCESS (evtStore()->retrieve (muonsStore, "Muons"));
+
+    auto readHandle = makeHandle (m_readKey);
+    if (m_readFailure == false)
+      EXPECT_EQ (muonsStore, readHandle.get());
+    else
+      EXPECT_EQ (nullptr, readHandle.get());
+  }
+}
--- Control/AthToolSupport/AsgExampleTools/src/components/AsgExampleTools_entries.cxx
+++ Control/AthToolSupport/AsgExampleTools/src/components/AsgExampleTools_entries.cxx
@@ -3,6 +3,7 @@
 #include "../AsgExampleAlgorithm.h"
 #include "AsgExampleTools/AsgHelloTool.h"
 
+#include <AsgExampleTools/DataHandleTestTool.h>
 #include <AsgExampleTools/UnitTestTool1.h>
 #include <AsgExampleTools/UnitTestTool1A.h>
 #include <AsgExampleTools/UnitTestTool2.h>
@@ -12,6 +13,7 @@ DECLARE_COMPONENT( AsgHelloTool )
 
 DECLARE_COMPONENT( AsgExampleAlgorithm )
 
+DECLARE_COMPONENT( asg::DataHandleTestTool )
 DECLARE_COMPONENT( asg::UnitTestTool1 )
 DECLARE_COMPONENT( asg::UnitTestTool1A )
 DECLARE_COMPONENT( asg::UnitTestTool2 )
