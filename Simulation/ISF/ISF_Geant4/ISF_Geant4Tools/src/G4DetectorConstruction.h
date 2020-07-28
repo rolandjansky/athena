6968f5372fe0147e3aef20d62b21d37555d8cf85
Michael Duehrssen <michael.duehrssen@cern.ch>
2020-07-28 18:22:48 +0200

    Update special Fatras G4DetectorConstruction to either use the ATLAS Vacuum as material or a special one with a different name

--- Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/G4DetectorConstruction.h
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Tools/src/G4DetectorConstruction.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -36,17 +36,10 @@ private:
   // Logical volumes
   //
   G4LogicalVolume*          m_worldLog;
-  //G4LogicalVolume*          m_detectorLog;
 
   // Physical volumes
   //
   G4VPhysicalVolume*        m_worldPhys;
-  //G4VPhysicalVolume*        m_detectorPhys;
-
-  // Vaccum material
-  //
-  static G4Material*        s_g4vacuum;
-
 };
 
 #endif
