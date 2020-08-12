cd60a8ffef2646693b6773ce6ff066ef9f6c6b92
John Chapman <jchapman@cern.ch>
2020-08-12 14:43:08 +0200

    Migrate Event/xAOD/xAODParticleEventAthenaPool TPCnv unit tests to ComponentAccumulator xAODParticleAthenaPool_21.0.79_test - passes (reference file updated)

---
+++ Event/xAOD/xAODParticleEventAthenaPool/test/xAODParticleAthenaPool_21.0.79_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+"""
+
+from AthenaPoolUtilities.TPCnvTestConfig import TPCnvTest
+
+if __name__ == "__main__":
+
+    infile = 'aod/AOD-21.0.79/AOD-21.0.79-full.pool.root'
+    keys = [
+        #xAOD::ParticleAuxContainer_v1
+        'finalTauPi0s',
+             ]
+
+    TPCnvTest(infile, keys)
