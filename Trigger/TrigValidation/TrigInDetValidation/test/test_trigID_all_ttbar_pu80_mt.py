68cb1cef57f2023f3fb5f06e0d350f266fdb4f49
Julie Kirk <Julie.Kirk@cern.ch>
2020-08-12 15:27:17 +0200

    fixes to bring "mt" tests up to date.

--- Trigger/TrigValidation/TrigInDetValidation/test/test_trigID_all_ttbar_pu80_mt.py
+++ Trigger/TrigValidation/TrigInDetValidation/test/test_trigID_all_ttbar_pu80_mt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# art-description: art job for all_ttbar_pu80_mt
+# art-description: art job for all_ttbar_pu80
 # art-type: grid
 # art-include: master/Athena
 # art-input-nfiles: 3
@@ -16,10 +16,8 @@
 # art-output: *.json
 # art-output: *.root
 # art-output: *.check*
-# art-output: HLTEF-plots
-# art-output: HLTL2-plots
-# art-output: times
-# art-output: times-FTF
+# art-output: HLT*
+# art-output: times*
 # art-output: cost-perCall
 # art-output: cost-perEvent
 # art-output: cost-perCall-chain
@@ -34,24 +32,31 @@ from TrigInDetValidation.TrigInDetArtSteps import TrigInDetReco, TrigInDetAna, T
 import sys,getopt
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"l",["local"])
+    opts, args = getopt.getopt(sys.argv[1:],"lxp",["local"])
 except getopt.GetoptError:
-    print("Usage: -l(--local)")
+    print("Usage:  ")
+    print("-l(--local)    run locally with input file from art eos grid-input")
+    print("-x             don't run athena or post post-processing, only plotting")
+    print("-p             run post-processing, even if -x is set")
 
 
 local=False
+exclude=False
+postproc=False
 for opt,arg in opts:
     if opt in ("-l", "--local"):
         local=True
-
-
+    if opt=="-x":
+        exclude=True
+    if opt=="-p":
+        postproc=True
 
 
 rdo2aod = TrigInDetReco()
 rdo2aod.slices = ['muon','electron','tau','bjet']
 rdo2aod.max_events = 2000 
 rdo2aod.threads = 1 # TODO: change to 4
-rdo2aod.concurrent_events = 4 
+rdo2aod.concurrent_events = 4 # TODO: change to 4
 rdo2aod.perfmon = False
 rdo2aod.timeout = 18*3600
 rdo2aod.input = 'ttbar_pu80'   # defined in TrigValTools/share/TrigValInputs.json  
@@ -59,14 +64,17 @@ rdo2aod.input = 'ttbar_pu80'   # defined in TrigValTools/share/TrigValInputs.jso
 
 test = Test.Test()
 test.art_type = 'grid'
-test.exec_steps = [rdo2aod]
-test.check_steps = CheckSteps.default_check_steps(test)
+if (not exclude):
+    test.exec_steps = [rdo2aod]
+    test.exec_steps.append(TrigInDetAna()) # Run analysis to produce TrkNtuple
+    test.check_steps = CheckSteps.default_check_steps(test)
 
-# Run analysis to produce TrkNtuple
-test.exec_steps.append(TrigInDetAna())
- 
 # Run Tidardict
-test.check_steps.append(TrigInDetdictStep())
+if ((not exclude) or postproc ):
+    rdict = TrigInDetdictStep()
+    rdict.args='TIDAdata-run3.dat -r Offline -f data-hists.root -b Test_bin.dat '
+    test.check_steps.append(rdict)
+
  
 # Now the comparitor steps
 comp=TrigInDetCompStep('Comp_L2muon')
@@ -110,7 +118,6 @@ comp8=TrigInDetCompStep('Comp_EFele')
 comp8.flag='EFele'
 comp8.test='ttbar'
 test.check_steps.append(comp8)
-
 
 # CPU cost steps
 cpucost=TrigInDetCpuCostStep('CpuCostStep1')
