8a7fc6fdc927d3ec3e4e270a757d55ba123ff30a
Nils Krumnack <krumnack@iastate.edu>
2020-08-13 12:24:03 -0500

    add more functionality to data handles

--- Control/AthToolSupport/AsgDataHandles/AsgDataHandles/ReadHandleKeyArray.h
+++ Control/AthToolSupport/AsgDataHandles/AsgDataHandles/ReadHandleKeyArray.h
@@ -21,6 +21,12 @@ namespace SG {
    * @brief class to hold an array of ReadHandleKeys
    *
    * See StoreGate/ReadHandleKeyArray for details.
+   *
+   * This currently (13 Aug 20) contains a commented out template
+   * argument that was part of the original handle implementation, but
+   * has not yet been implemented in the standalone version.  The plan
+   * is to either implement or remove them, depending on what is
+   * needed in AnalysisBase as we add more packages.
    */
   template <class T>
   using ReadHandleKeyArray = HandleKeyArray<ReadHandle<T>, ReadHandleKey<T>/*, Gaudi::DataHandle::Reader*/ >;
