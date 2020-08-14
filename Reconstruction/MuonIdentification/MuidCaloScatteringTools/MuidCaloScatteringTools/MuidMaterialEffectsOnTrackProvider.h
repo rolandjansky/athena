1cad51c944d9cb13a959c1777815f6673e421d22
Maksim Penzin <penzin.maksim@gmail.com>
2020-08-14 21:45:22 +0200

    Style fix (auto)

--- Reconstruction/MuonIdentification/MuidCaloScatteringTools/MuidCaloScatteringTools/MuidMaterialEffectsOnTrackProvider.h
+++ Reconstruction/MuonIdentification/MuidCaloScatteringTools/MuidCaloScatteringTools/MuidMaterialEffectsOnTrackProvider.h
@@ -11,57 +11,52 @@
 
 // Gaudi
 #include "AthenaBaseComps/AthAlgTool.h"
+#include "GaudiKernel/ToolHandle.h"
+#include "MuidInterfaces/IMuidCaloTrackStateOnSurface.h"
 #include "TrkDetDescrInterfaces/IMaterialEffectsOnTrackProvider.h"
+#include "TrkExInterfaces/IMultipleScatteringUpdator.h"
+#include "TrkParameters/TrackParameters.h"
 #include "TrkSurfaces/PlaneSurface.h"
 
-#include "TrkParameters/TrackParameters.h"
-#include "MuidInterfaces/IMuidCaloTrackStateOnSurface.h"
-#include "TrkExInterfaces/IMultipleScatteringUpdator.h"
-
-#include "GaudiKernel/ToolHandle.h"
-
 namespace Trk {
-    class Surface;
+class Surface;
 }
 
-  /** @class MuidMaterialEffectsOnTrackProvider
-      
-  @author thijs.cornelissen@cern.ch
-  */
-namespace Rec{  
-  class MuidMaterialEffectsOnTrackProvider : public AthAlgTool,
-    virtual public Trk::IMaterialEffectsOnTrackProvider {
-      
-  public:
-      
-      /** AlgTool like constructor */
-      MuidMaterialEffectsOnTrackProvider(const std::string&,const std::string&,const IInterface*);
-      
-      /**Virtual destructor*/
-      virtual ~MuidMaterialEffectsOnTrackProvider();
-      
-      /** AlgTool initailize method.*/
-      StatusCode initialize();
-      /** AlgTool finalize method */
-      StatusCode finalize();
-      
-      /** return all MaterialLayers associated to this track **/
-      std::vector< Trk::MaterialEffectsOnTrack > extrapolationSurfacesAndEffects(const Trk::TrackingVolume& ,
-														  const Trk::IPropagator& ,
-														  const Trk::TrackParameters& ,
-														  const Trk::Surface& ,
-														  Trk::PropDirection ,
-									
-				  Trk::ParticleHypothesis ) const;
-  private:      
-      
-     ToolHandle< Rec::IMuidCaloTrackStateOnSurface > m_calotsos;
-     ToolHandle< Rec::IMuidCaloTrackStateOnSurface > m_calotsosparam;
-     ToolHandle< Trk::IMultipleScatteringUpdator > m_scattool;
-     
-     
-     bool m_cosmics;
-    };
-  } // end namespace
+/** @class MuidMaterialEffectsOnTrackProvider
 
-#endif // MuidMaterialEffectsOnTrackProvider_H
+@author thijs.cornelissen@cern.ch
+*/
+namespace Rec {
+class MuidMaterialEffectsOnTrackProvider : public AthAlgTool, virtual public Trk::IMaterialEffectsOnTrackProvider {
+
+  public:
+    /** AlgTool like constructor */
+    MuidMaterialEffectsOnTrackProvider(const std::string&, const std::string&, const IInterface*);
+
+    /**Virtual destructor*/
+    virtual ~MuidMaterialEffectsOnTrackProvider();
+
+    /** AlgTool initailize method.*/
+    StatusCode initialize();
+    /** AlgTool finalize method */
+    StatusCode finalize();
+
+    /** return all MaterialLayers associated to this track **/
+    std::vector<Trk::MaterialEffectsOnTrack> extrapolationSurfacesAndEffects(const Trk::TrackingVolume&,
+                                                                             const Trk::IPropagator&,
+                                                                             const Trk::TrackParameters&,
+                                                                             const Trk::Surface&, Trk::PropDirection,
+
+                                                                             Trk::ParticleHypothesis) const;
+
+  private:
+    ToolHandle<Rec::IMuidCaloTrackStateOnSurface> m_calotsos;
+    ToolHandle<Rec::IMuidCaloTrackStateOnSurface> m_calotsosparam;
+    ToolHandle<Trk::IMultipleScatteringUpdator>   m_scattool;
+
+
+    bool m_cosmics;
+};
+}  // namespace Rec
+
+#endif  // MuidMaterialEffectsOnTrackProvider_H
