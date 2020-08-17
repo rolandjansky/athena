50dead6dc1cbf457d01b8de80cf581d7981c0d4b
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-08-17 15:16:10 +0200

    Revert "A first version of validation code for pflow FlowElements (charged)."
    This reverts commit d1bc5d6d5d48541b3ada980b8c04acf8df0a44af.

--- Reconstruction/PFlow/PFlowValidation/PFODQA/src/components/PFODQA_entries.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFODQA/src/components/PFODQA_entries.cxx
@@ -1,8 +1,6 @@
-#include "../PhysValFlowElement.h"
 #include "../PhysValPFO.h"
 #include "../PhysValCluster.h"
 	
-DECLARE_COMPONENT( PhysValFlowElement )
 DECLARE_COMPONENT( PhysValPFO )
 DECLARE_COMPONENT( PhysValCluster )
 
