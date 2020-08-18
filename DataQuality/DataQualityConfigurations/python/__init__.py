bba270ea9ef1195705bc7ad97ca4ab9c55f7a0a4
Peter Onyisi <ponyisi@utexas.edu>
2020-08-18 17:59:51 +0200

    Python 3 fixes for web display generation

--- DataQuality/DataQualityConfigurations/python/__init__.py
+++ DataQuality/DataQualityConfigurations/python/__init__.py
@@ -5,4 +5,4 @@ __all__ = ['data09', 'data09_1beam','data09_calocomm', 'data09_calophys',
            'data09_cos', 'data09_idcomm', 'data09_larcomm', 'data09_muoncomm',
            'data09_tilecomm', 'data08', 'data08_1beam', 'data08_1beammag',
            'data08_cos', 'data08_cosmag', 'data08_idcomm', 'TestDisplay']
-from DQCDispatch import getmodule
+from .DQCDispatch import getmodule
