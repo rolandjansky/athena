512cc7e722f7dabdeb868322767286e17f8ba1f1
gipezzul <g.pezzullo@yale.edu>
2020-08-19 17:18:57 +0200

    removed useless lines

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/CalibCosmicMon/CalibChainConfiguration.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/CalibCosmicMon/CalibChainConfiguration.py
@@ -19,19 +19,6 @@ class CalibChainConfiguration(ChainConfigurationBase):
                          
         chainSteps = []
         log.debug("Assembling chain for " + self.chainName)
-        # --------------------
-        # define here the names of the steps and obtain the chainStep configuration 
-        # --------------------
-        stepDictionary = {
-            "sct_noise":[],
-            "tilecalib_laser" : []
-        }
-
-        key = self.chainPart['chainPartName']
-        steps=stepDictionary[key]
-        for step in steps:
-            chainSteps+=[step]
-            
         myChain = self.buildChain(chainSteps)
         return myChain
 
