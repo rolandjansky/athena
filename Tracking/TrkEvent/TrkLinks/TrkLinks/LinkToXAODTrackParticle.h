50e410e78d39f5efbadd5c67c5c713c1fec723ca
christos <christos@cern.ch>
2020-08-19 20:33:07 +0200

    ITrackLink , LinkToXAODTrackParticle,LinkToXAODNeutralParticle, default copy,move, assignment

--- Tracking/TrkEvent/TrkLinks/TrkLinks/LinkToXAODTrackParticle.h
+++ Tracking/TrkEvent/TrkLinks/TrkLinks/LinkToXAODTrackParticle.h
@@ -5,29 +5,27 @@
 #ifndef TRKTRACK_LINKTOXAODTRACKPARTICLE_H
 #define TRKTRACK_LINKTOXAODTRACKPARTICLE_H
 
-#include "TrkTrackLink/ITrackLink.h"
 #include "AthLinks/ElementLink.h"
+#include "TrkTrackLink/ITrackLink.h"
 
-#include "xAODTracking/TrackParticleContainer.h"
-#include "TrkParameters/TrackParameters.h"
 #include "TrkNeutralParameters/NeutralParameters.h"
+#include "TrkParameters/TrackParameters.h"
+#include "xAODTracking/TrackParticleContainer.h"
 
-
-namespace Trk
-{
- /** @brief Element link to XAOD TrackParticle. 
-  *  An EDM object to reduce dependencies among 
-  * Tracking/InDet/XAOD domains. This is NOT an
-  * analysis class. This link keeps the EDM of 
-  * different domains together and makes it interchangeable.
-  * WARNING: Do not use this class unless you understand 
-  * how the ITrackLink functions.  
-  *
-  * A hack to the EDM which will be possibly removed at later stages
-  * Kirill.Prokofiev@cern.ch
-  *
-  * February 2014
-  */
+namespace Trk {
+/** @brief Element link to XAOD TrackParticle.
+ *  An EDM object to reduce dependencies among
+ * Tracking/InDet/XAOD domains. This is NOT an
+ * analysis class. This link keeps the EDM of
+ * different domains together and makes it interchangeable.
+ * WARNING: Do not use this class unless you understand
+ * how the ITrackLink functions.
+ *
+ * A hack to the EDM which will be possibly removed at later stages
+ * Kirill.Prokofiev@cern.ch
+ *
+ * February 2014
+ */
 
 class LinkToXAODTrackParticle
   : public ITrackLink
@@ -41,12 +39,15 @@ public:
   /** constructor with ElementLink */
   LinkToXAODTrackParticle(ElementLink<xAOD::TrackParticleContainer>& link);
 
-  /** default destructor */
+  LinkToXAODTrackParticle(const LinkToXAODTrackParticle&) = default;
+  LinkToXAODTrackParticle(LinkToXAODTrackParticle&&) = default;
+  LinkToXAODTrackParticle& operator=(const LinkToXAODTrackParticle&) = default;
+  LinkToXAODTrackParticle& operator=(LinkToXAODTrackParticle&&) = default;
   virtual ~LinkToXAODTrackParticle() = default;
 
   /** return the track parameters of the track (to which the EL< TrackCollection
    * > points) */
-  virtual  const TrackParameters* parameters() const override final;
+  virtual const TrackParameters* parameters() const override final;
 
   /** return the neutral parameters of the NeutralParticle) */
   virtual const NeutralParameters* neutralParameters() const override final
@@ -56,12 +57,13 @@ public:
 
   /** method to clone the LinkToXAODTrackParticle object */
   virtual LinkToXAODTrackParticle* clone() const override final;
-    
-  virtual ITrackLinkType type() const override final {
+
+  virtual ITrackLinkType type() const override final
+  {
     return ToxAODTrackParticle;
   }
 
- };//end of class definitions
-}//end of namespace definitions
+}; // end of class definitions
+} // end of namespace definitions
 
-#endif //TRKTRACK_LINKTOXAODTRACKPARTICLE_H
+#endif // TRKTRACK_LINKTOXAODTRACKPARTICLE_H
