20336abfe8ff325054c54252e45bdc89ea6fc131
Rupert Tombs <rt500@cam.ac.uk>
2020-08-20 09:51:15 +0100

    Initial ISF python testing and fixes to make it run
    Correct SvcV2 to V2Svc
    
    Config fixes; missing flags args, wrong imports, missing private svc
    
    Add basic G4Atlas config first
    
    Attempt to link existing ISF_G4RunManagerHelper
    
    Merge accumulators (since when did this error not exit?)
    
    Replace test from dnoel/ISF_newstyle
    
    Add temp printout to understand error
    
    Return acc with privatetool
    
    Add new style tool handle. Add TODOs for others
    
    Convert to new style tool linkage
    
    Add CaloG4SimConfigNew
    
    Work around circular imports. Enable attempts to run.
    
    Attach DetGeoSvc
    
    Comment test
    
    Undo AtlasSemantics message
    
    Return accumulator with private tools from getDefaultActions
    
    Flake8 fixes

--- Simulation/ISF/ISF_Core/ISF_Tools/python/ISF_ToolsConfigNew.py
+++ Simulation/ISF/ISF_Core/ISF_Tools/python/ISF_ToolsConfigNew.py
@@ -43,11 +43,13 @@ def EntryLayerFilterCfg(ConfigFlags, **kwargs):
 
 
 def MC12EntryLayerFilterCfg(flags, name="ISF_MC12EntryLayerFilter", **kwargs):
+    acc = ComponentAccumulator()
     kwargs.setdefault("AllowOnlyDefinedBarcodes", True)
     kwargs.setdefault("AllowOnlyLegacyPrimaries", False)
     kwargs.setdefault("LegacyParticleGenerationIncrement", 1000000)
     kwargs.setdefault("LegacyFirstSecondaryBarcode", 200001)
-    return CompFactory.ISF.GenericBarcodeFilter(name, **kwargs)
+    acc.setPrivateTools(CompFactory.ISF.GenericBarcodeFilter(name, **kwargs))
+    return acc
 
 
 def MC12LLPEntryLayerFilterCfg(flags, name="ISF_MC12LLPEntryLayerFilter", **kwargs):
@@ -59,9 +61,11 @@ def MC12PlusEntryLayerFilterCfg(flags, name="ISF_MC12PlusEntryLayerFilter", **kw
 
 
 def MC15EntryLayerFilterCfg(flags, name="ISF_MC15EntryLayerFilter", **kwargs):
+    acc = ComponentAccumulator()
     kwargs.setdefault("MinEkinCharged", 100.*MeV)
     kwargs.setdefault("MinEkinNeutral", -1.)
-    return CompFactory.ISF.EntryLayerFilter(name, **kwargs  )
+    acc.setPrivateTools(CompFactory.ISF.EntryLayerFilter(name, **kwargs))
+    return acc
 
 
 def MC15aEntryLayerFilterCfg(flags, name="ISF_MC15aEntryLayerFilter", **kwargs):
