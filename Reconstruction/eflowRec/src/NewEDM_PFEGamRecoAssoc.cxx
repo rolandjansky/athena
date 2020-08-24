3907fe51afe2eb192aef80e315184bb1ae027e28
manthony <matthew.thomas.anthony@cern.ch>
2020-08-24 13:44:48 +0200

    Update EGamma Association links to PFOs to use new EDM (FlowElements). Current naming convention though uses "Dummy" nomenclature. ToFix

--- Reconstruction/eflowRec/src/NewEDM_PFEGamRecoAssoc.cxx
+++ Reconstruction/eflowRec/src/NewEDM_PFEGamRecoAssoc.cxx
@@ -71,7 +71,7 @@ StatusCode NewEDM_PFEGamRecoAssoc::execute()
 {
   // This algorithm does the following:
   // 1) Read the Input containers for Flow Elements, Electrons and Photons
-  // 2) Match the Clusters in the flow elements to the relevant E/Photon clusters
+  // 2) Match the Clusters in the flow elements to the relevant E/Photon clusters (Neutral) or match tracks to relevant Electron/Photon tracks (Charged)
   // 3) Link them
   // 4) output the Electron/Photon containers with the linkers to the Flow element containers
 
@@ -112,7 +112,6 @@ StatusCode NewEDM_PFEGamRecoAssoc::execute()
   ///////////////////////////
   // Loop over neutral flow elements (PFOs)
   ///////////////////////////
-  std::cout<<"MATT'S DEBUG: PFO elements size "<<neutralpfoElectronWriteDecorHandle->size()<<std::endl;
   for (const xAOD::FlowElement* pfo: *neutralpfoElectronWriteDecorHandle){
     //Obtain the index of the PFO calo-cluster
     size_t pfoClusterIndex=pfo->otherObjects().at(0)->index();
@@ -121,29 +120,133 @@ StatusCode NewEDM_PFEGamRecoAssoc::execute()
     std::vector<ElectronLink_t> pfoElectronLinks;
     std::vector<PhotonLink_t> pfoPhotonLinks;
     
-    std::cout<<"DEBUG 1:  pfoclusterindex: "<<pfoClusterIndex<<std::endl; 
     //Loop over electrons:
     for (const xAOD::Electron* electron: *electronNeutralPFOWriteDecorHandle){
       // get the calo clusters from the electron
-      std::cout<<"DEBUG 2: Electron pt : "<<electron->pt()<<std::endl;
+
       const std::vector<const xAOD::CaloCluster*> electronTopoClusters = xAOD::EgammaHelpers::getAssociatedTopoClusters(electron->caloCluster());
 	
+      
       for(const xAOD::CaloCluster* cluster : electronTopoClusters){
-
+	// obtain the index of the electron seed topocluster
 	size_t electronClusterIndex=cluster->index();	
-	std::cout<<"DEBUG 3: Cluster index : "<<electronClusterIndex<<std::endl;
+	//match the indices: Cluster match between Flow Element (PFO) and electron
+	if(electronClusterIndex==pfoClusterIndex){
+	  pfoElectronLinks.push_back( ElectronLink_t(*electronReadHandle,electron->index()) );
+	  //Add Flow Element (PFO) link to a vector
+	  //index() is the unique index of the Flow Element in the container
+	  electronNeutralPFOVec.at(electron->index()).push_back(FlowElementLink_t(*neutralpfoReadHandle, pfo->index()) );
+	}// end of matching block
 
       } // end loop over cluster
       
     } // end Electron loop
 
+    // now loop over photons
+    for (const xAOD::Photon* photon: *photonNeutralPFOWriteDecorHandle){
+      // retrieve clusters from the photon container
+      const std::vector<const xAOD::CaloCluster*> photonTopoClusters = xAOD::EgammaHelpers::getAssociatedTopoClusters(photon->caloCluster());
+      //loop over clusters, and do the matching
+      for (const xAOD::CaloCluster* cluster: photonTopoClusters){
+	//retrieve index of the cluster
+	size_t photonClusterIndex=cluster->index();
+	//do the matching
+	if(photonClusterIndex==pfoClusterIndex){
+	  // Add flow element (PFO) links to photon
+	  pfoPhotonLinks.push_back( PhotonLink_t(*photonReadHandle,photon->index()) );
+	  //Add Flow Element (PFO) link to a vector
+	  //index() is the unique index of the Flow Element in the container
+	  photonNeutralPFOVec.at(photon->index()).push_back(FlowElementLink_t(*neutralpfoReadHandle, pfo->index()) );	  
+	}// end of matching block
+      } // end of neutral loop
+      
+
+    }// end of photon loop
+    
     //Add vector of electron element links as decoration to FlowElement container
-    //    neutralpfoElectronWriteDecorHandle (*pfo)=pfoElectronLinks;
-
-
+    neutralpfoElectronWriteDecorHandle (*pfo)=pfoElectronLinks;
+    neutralpfoPhotonWriteDecorHandle (*pfo)=pfoPhotonLinks;
 
   } // end neutral PFO loop
 
+  ////////////////////////////////////////////////////////
+  //             Loop over charged Flow Elements (PFOs)
+  ////////////////////////////////////////////////////////
+  for (const xAOD::FlowElement* pfo: *chargedpfoElectronWriteDecorHandle){
+    // retrieve the track from the Flow element
+    size_t pfoTrackIndex=pfo->chargedObjects().at(0)->index();
+    // Initialise a vector of element links to electrons/Photons
+    std::vector<ElectronLink_t> pfoElectronLinks;
+    std::vector<PhotonLink_t> pfoPhotonLinks;
+
+    //loop over electrons
+    for (const xAOD::Electron* electron: *electronChargedPFOWriteDecorHandle){
+      //obtain the clusters
+       const std::vector<const xAOD::TrackParticle*> electronTrackParticles = xAOD::EgammaHelpers::getTrackParticlesVec(electron, true, true); // useBremAssoc = true (get original non-GSF track), allParticles = true (include all track particles)
+       // loop over tracks
+       for (const xAOD::TrackParticle* electronTrack: electronTrackParticles){
+	 size_t electronTrackIndex = electronTrack->index();
+
+	 //link to PFO if track indices match
+	 if(electronTrackIndex==pfoTrackIndex){
+	   // Add electron element link to a vector 
+	   // index() is the unique index of the electron in the electron container 
+	   pfoElectronLinks.push_back( ElectronLink_t(*electronReadHandle, electron->index()) );
+	   // Add pfo element link to a vector 
+	   // index() is the unique index of the cPFO in the cPFO container 
+	   electronChargedPFOVec.at(electron->index()).push_back( FlowElementLink_t(*chargedpfoReadHandle, pfo->index()) );  
+
+	 }//end of matching block
+	 
+       }//end of loop on clusters
+    } // end of loop on electrons
+    
+    for(const xAOD::Photon* photon: *photonChargedPFOWriteDecorHandle){
+      //obtain indices of the converted photon's original tracks
+      const std::set<const xAOD::TrackParticle*> photonTrackParticles = xAOD::EgammaHelpers::getTrackParticles(photon, true);
+      // loop over the tracks
+      for (const xAOD::TrackParticle* photonTrack: photonTrackParticles){
+	size_t photonTrackIndex=photonTrack->index();
+	
+	// Link the photon to the Flow Element (PFO) if the track indices match
+	if (photonTrackIndex==pfoTrackIndex){
+	            // Add photon element link to a vector
+          // index() is the unique index of the photon in the photon container
+          pfoPhotonLinks.push_back( PhotonLink_t(*photonReadHandle, photon->index()) );
+          // Add pfo element link to a vector
+          // index() is the unique index of the cPFO in the cPFO container
+          photonChargedPFOVec.at(photon->index()).push_back( FlowElementLink_t(*chargedpfoReadHandle, pfo->index()) );
+	}// end of matching block
+
+      }// end of loop on tracks
+      
+    }//end of loop on photons
+
+
+    // Add vector of electron element links as decoration to PFO container     
+    chargedpfoElectronWriteDecorHandle (*pfo) = pfoElectronLinks;  
+    // Add vector of photon element links as decoration to PFO container
+    chargedpfoPhotonWriteDecorHandle (*pfo) = pfoPhotonLinks;
+
+  } // end of charged PFO loop
+  
+
+
+  ////////////////////////////////////////////////////
+  //   WRITE OUTPUT: ADD HANDLES TO EL/PHOT CONTAINERS
+  ////////////////////////////////////////////////////
+  // Add the vectors of the Flow Element (PFO) Links as decorations to the electron container
+  for (const xAOD::Electron* electron : *electronNeutralPFOWriteDecorHandle){
+    electronNeutralPFOWriteDecorHandle (*electron) = electronNeutralPFOVec.at(electron->index());
+    electronChargedPFOWriteDecorHandle (*electron) = electronChargedPFOVec.at(electron->index());
+  } //end of photon loop
+  // Add the vectors of the Flow Element (PFO) Links as decorations to the photon container
+  for (const xAOD::Photon* photon: *photonNeutralPFOWriteDecorHandle){
+    photonNeutralPFOWriteDecorHandle (*photon) = photonNeutralPFOVec.at(photon->index());
+    photonChargedPFOWriteDecorHandle (*photon) = photonChargedPFOVec.at(photon->index());
+  } // end of loop on photons
+
+  ATH_MSG_DEBUG("Execute completed successfully");
 
   return StatusCode::SUCCESS;
 }
