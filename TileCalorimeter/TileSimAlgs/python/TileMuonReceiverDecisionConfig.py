6796d9ef36a8826be0cf474f9727544247fba8ff
Tadej Novak <tadej.novak@cern.ch>
2020-08-26 14:20:49 +0200

    TileSimAlgs: ensure we have xAOD::EventInfo for output writing

--- TileCalorimeter/TileSimAlgs/python/TileMuonReceiverDecisionConfig.py
+++ TileCalorimeter/TileSimAlgs/python/TileMuonReceiverDecisionConfig.py
@@ -97,6 +97,12 @@ if __name__ == "__main__":
     from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
     acc.merge(PoolReadCfg(ConfigFlags))
 
+    if 'EventInfo' not in ConfigFlags.Input.Collections:
+        from xAODEventInfoCnv.xAODEventInfoCnvConfig import EventInfoCnvAlgCfg
+        acc.merge(EventInfoCnvAlgCfg(ConfigFlags,
+                                     inputKey='McEventInfo',
+                                     outputKey='EventInfo'))
+
     acc.merge( TileMuonReceiverDecisionOutputCfg(ConfigFlags, TileMuonReceiverContainer = 'TileMuRcvCntNew') )
 
     acc.printConfig(withDetails = True, summariseProps = True)
