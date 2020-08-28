5f944da1e636b345dc37c6db88bd8803d09d1bed
Stanislava <stanislavasevova2013@u.northwestern.edu>
2020-08-28 11:20:09 +0200

    add bare-bones package structure

---
+++ InnerDetector/InDetDetDescr/PixelGeoModelXml/python/ITkPixelGeoModelConfig.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+
+from AthenaConfiguration.ComponentFactory import CompFactory
+#Comment to avoid python compilation warnings until block below is re-added
+#from IOVDbSvc.IOVDbSvcConfig import addFoldersSplitOnline
+
+def ITkPixelGeometryCfg( flags ):
+    from AtlasGeoModel.GeoModelConfig import GeoModelCfg
+    acc = GeoModelCfg( flags )
+    geoModelSvc=acc.getPrimary()
+    GeometryDBSvc=CompFactory.GeometryDBSvc
+    acc.addService(GeometryDBSvc("ITkGeometryDBSvc"))
+    PixelDetectorTool=CompFactory.PixelDetectorTool
+    ITkPixelDetectorTool = PixelDetectorTool()
+    #ITkStripDetectorTool.useDynamicAlignFolders = flags.GeoModel.Align.Dynamic
+    ITkPixelDetectorTool.Alignable = False # make this a flag? Set true as soon as decided on folder structure
+    ITkPixelDetectorTool.DetectorName = "ITkPixel"
+    geoModelSvc.DetectorTools += [ ITkPixelDetectorTool ]
