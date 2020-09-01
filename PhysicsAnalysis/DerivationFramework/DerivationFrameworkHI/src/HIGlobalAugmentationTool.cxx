70f15001c44ecd2e4835e43b45931291a08f0884
Virginia Bailey <virginia.ruth.bailey@cern.ch>
2020-09-01 07:01:02 +0000

    DerivationFrameworkHI: Add q-vector and FCal energy information for only FCal with |eta| > 4.0

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHI/src/HIGlobalAugmentationTool.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHI/src/HIGlobalAugmentationTool.cxx
@@ -32,14 +32,20 @@ namespace DerivationFramework
     unsigned int nTrackSels  = m_trkSelTools.size();
     for (unsigned int its = 0; its < nTrackSels; ++its) {      
         ATH_CHECK(m_trkSelTools[its].retrieve());
-        m_decTrack_count.push_back(new SG::AuxElement::Decorator<int>("TrackParticleMultiplicity_" + m_cutLevels[its] ));
+        m_decTrack_count.emplace_back(SG::AuxElement::Decorator<int>("TrackParticleMultiplicity_" + m_cutLevels[its] ));
     }
     //initilize decorators for flow vectors
     for (int vn = 1; vn <= m_nHarmonic; ++vn){    
-        m_decFCalEtA_Qnx.push_back(new SG::AuxElement::Decorator<float>("FCalEtA_Q" + std::to_string(vn+1) + "x"));
-        m_decFCalEtA_Qny.push_back(new SG::AuxElement::Decorator<float>("FCalEtA_Q" + std::to_string(vn+1) + "y"));
-        m_decFCalEtC_Qnx.push_back(new SG::AuxElement::Decorator<float>("FCalEtC_Q" + std::to_string(vn+1) + "x"));
-        m_decFCalEtC_Qny.push_back(new SG::AuxElement::Decorator<float>("FCalEtC_Q" + std::to_string(vn+1) + "y"));
+      m_decFCalEtA_Qnx.emplace_back( SG::AuxElement::Decorator<float>("FCalEtA_Q" + std::to_string(vn+1) + "x") );
+      m_decFCalEtA_Qny.emplace_back( SG::AuxElement::Decorator<float>("FCalEtA_Q" + std::to_string(vn+1) + "y") );
+      m_decFCalEtC_Qnx.emplace_back( SG::AuxElement::Decorator<float>("FCalEtC_Q" + std::to_string(vn+1) + "x") );
+      m_decFCalEtC_Qny.emplace_back( SG::AuxElement::Decorator<float>("FCalEtC_Q" + std::to_string(vn+1) + "y") );
+      
+      //half FCal is for FCal with eta > 4.0 only
+      m_decHalfFCalEtA_Qnx.emplace_back( SG::AuxElement::Decorator<float>("HalfFCalEtA_Q" + std::to_string(vn+1) + "x") );
+      m_decHalfFCalEtA_Qny.emplace_back( SG::AuxElement::Decorator<float>("HalfFCalEtA_Q" + std::to_string(vn+1) + "y") );
+      m_decHalfFCalEtC_Qnx.emplace_back( SG::AuxElement::Decorator<float>("HalfFCalEtC_Q" + std::to_string(vn+1) + "x") );
+      m_decHalfFCalEtC_Qny.emplace_back( SG::AuxElement::Decorator<float>("HalfFCalEtC_Q" + std::to_string(vn+1) + "y") );
     }     
     
     return StatusCode::SUCCESS;
@@ -49,12 +55,7 @@ namespace DerivationFramework
     for (auto trkSelTool : m_trkSelTools ) { 
         ATH_CHECK(trkSelTool->finalize());
     }
-    for(const auto& idec : m_decTrack_count) delete idec;
-    
-    for(const auto& fdec : m_decFCalEtA_Qnx) delete fdec;
-    for(const auto& fdec : m_decFCalEtA_Qny) delete fdec;
-    for(const auto& fdec : m_decFCalEtC_Qnx) delete fdec;
-    for(const auto& fdec : m_decFCalEtC_Qny) delete fdec;
+
     return StatusCode::SUCCESS;
   }
 
@@ -93,8 +94,8 @@ namespace DerivationFramework
     }
     //decorate eventInfo with track multiplicity
     for (unsigned int its = 0; its < nTrackSels; ++its) {  
-        (*m_decTrack_count[its])(*eventInfo) = TrackParticleMultiplicity[its];    
-	}
+      (m_decTrack_count[its])(*eventInfo) = TrackParticleMultiplicity[its];    
+    }
     
     float FCalEtA = 0;
     float FCalEtC = 0;
@@ -104,11 +105,21 @@ namespace DerivationFramework
     std::vector<float> FCalEtC_Qnx(m_nHarmonic,0);
     std::vector<float> FCalEtC_Qny(m_nHarmonic,0);
 
+    float HalfFCalEtA = 0;
+    float HalfFCalEtC = 0;
+
+    std::vector<float> HalfFCalEtA_Qnx(m_nHarmonic,0);
+    std::vector<float> HalfFCalEtA_Qny(m_nHarmonic,0);
+    std::vector<float> HalfFCalEtC_Qnx(m_nHarmonic,0);
+    std::vector<float> HalfFCalEtC_Qny(m_nHarmonic,0);
         
     // Set up the decorators for FCal Et
     SG::AuxElement::Decorator< float > decFCalEtA("FCalEtA");
     SG::AuxElement::Decorator< float > decFCalEtC("FCalEtC");    
     
+    SG::AuxElement::Decorator< float > decHalfFCalEtA("HalfFCalEtA");
+    SG::AuxElement::Decorator< float > decHalfFCalEtC("HalfFCalEtC");
+
     //Retrieve HIEventShape
     const xAOD::HIEventShapeContainer *eventShape = nullptr;      
     if(evtStore()->retrieve( eventShape, "HIEventShape" ).isFailure()){
@@ -131,22 +142,43 @@ namespace DerivationFramework
               FCalEtA_Qnx.at(vn-1) += c1.at(vn);
               FCalEtA_Qny.at(vn-1) += s1.at(vn);
           } 
+	  if(eta > 4.0) {
+	    HalfFCalEtA += ptrEvtShp->et();
+	    for (int vn = 1; vn <= m_nHarmonic; ++vn){
+              HalfFCalEtA_Qnx.at(vn-1) += c1.at(vn);
+              HalfFCalEtA_Qny.at(vn-1) += s1.at(vn);
+	    }
+	  }
        } else if (eta < 0) {
           FCalEtC += ptrEvtShp->et();
           for (int vn = 1; vn <= m_nHarmonic; ++vn){    
               FCalEtC_Qnx.at(vn-1) += c1.at(vn);
               FCalEtC_Qny.at(vn-1) += s1.at(vn);
-          }   
+          }
+	  if(eta < -4.0){
+	    HalfFCalEtC += ptrEvtShp->et();
+	    for (int vn = 1; vn <= m_nHarmonic; ++vn){
+              HalfFCalEtC_Qnx.at(vn-1) += c1.at(vn);
+              HalfFCalEtC_Qny.at(vn-1) += s1.at(vn);
+	    }
+	  }
        }
     } 
     //decorate
     decFCalEtA(*eventInfo) = FCalEtA;
     decFCalEtC(*eventInfo) = FCalEtC;
+    decHalfFCalEtA(*eventInfo) = HalfFCalEtA;
+    decHalfFCalEtC(*eventInfo) = HalfFCalEtC;
     for (int vn = 0; vn < m_nHarmonic; ++vn){ 
-        (*m_decFCalEtA_Qnx[vn])(*eventInfo) = FCalEtA_Qnx.at(vn);
-        (*m_decFCalEtA_Qny[vn])(*eventInfo) = FCalEtA_Qny.at(vn);
-        (*m_decFCalEtC_Qnx[vn])(*eventInfo) = FCalEtC_Qnx.at(vn);
-        (*m_decFCalEtC_Qny[vn])(*eventInfo) = FCalEtC_Qny.at(vn);
+        (m_decFCalEtA_Qnx[vn])(*eventInfo) = FCalEtA_Qnx.at(vn);
+        (m_decFCalEtA_Qny[vn])(*eventInfo) = FCalEtA_Qny.at(vn);
+        (m_decFCalEtC_Qnx[vn])(*eventInfo) = FCalEtC_Qnx.at(vn);
+        (m_decFCalEtC_Qny[vn])(*eventInfo) = FCalEtC_Qny.at(vn);
+    
+	(m_decHalfFCalEtA_Qnx[vn])(*eventInfo) = HalfFCalEtA_Qnx.at(vn);
+        (m_decHalfFCalEtA_Qny[vn])(*eventInfo) = HalfFCalEtA_Qny.at(vn);
+        (m_decHalfFCalEtC_Qnx[vn])(*eventInfo) = HalfFCalEtC_Qnx.at(vn);
+        (m_decHalfFCalEtC_Qny[vn])(*eventInfo) = HalfFCalEtC_Qny.at(vn);
     }
         
     return StatusCode::SUCCESS;
