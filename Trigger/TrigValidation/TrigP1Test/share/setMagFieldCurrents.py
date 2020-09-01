6ea38a25599f22cb0bc97bac6896015c19fc74ea
Frank Winklmeier <fwinkl@cern>
2020-09-01 15:17:40 +0200

    TrigP1Test: Fix and redesign magFieldToggle test
    Redesign the magFieldToggle test. It now uses a dummy algorithm to check
    the field status instead of the full physics menu. This is currently the
    only way this can be tested because we need to use 3 different run
    numbers. Using a full physics menu would require valid information for
    all these three runs in COOL (e.g. Beamspot). Might look into fixing
    this at a later point.

--- Trigger/TrigValidation/TrigP1Test/share/setMagFieldCurrents.py
+++ Trigger/TrigValidation/TrigP1Test/share/setMagFieldCurrents.py
@@ -6,15 +6,16 @@ from GaudiPython.Bindings import iService, iAlgorithm
 from AthenaCommon.Logging import logging
 
 log = logging.getLogger("setMagFieldCurrents.py")
-propSvc = iService('AtlasFieldSvc')   # deprecated soon: ATLASRECTS-4687
-propAlg = iAlgorithm('AtlasFieldMapCondAlg')
+mapCondAlg = iAlgorithm('AtlasFieldMapCondAlg')
 
 if 'solCur' in dir():
    log.info("Setting solenoid current to %d", solCur)
-   setattr(propSvc, 'UseSoleCurrent', solCur)
-   setattr(propAlg, 'MapSoleCurrent', solCur)
-   
+   setattr(mapCondAlg, 'MapSoleCurrent', solCur)
+
 if 'torCur' in dir():
    log.info("Setting toroid current to %d", torCur)
-   setattr(propSvc, 'UseToroCurrent', torCur)
-   setattr(propAlg, 'MapToroCurrent', torCur)
+   setattr(mapCondAlg, 'MapToroCurrent', torCur)
+
+if 'run' in dir():
+   log.info("Setting run number to %d", run)
+   setattr(iService('HltEventLoopMgr'), 'forceRunNumber', run)
