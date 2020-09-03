750601a72b9a7ee8542b91db3507e49f10418cc8
John Chapman <jchapman@cern.ch>
2020-09-03 17:58:01 +0200

    Use a fixed (configurable) random stream name for LArPileUpTool and LArTTL1Maker
    Using a fixed random stream name rather than simply the component name will
    make the LAr Digitization output more closely match 21.0 and will mean that
    it will no longer vary for different named components (unless explicitly
    configured to do so).

--- LArCalorimeter/LArL1Sim/src/LArTTL1Maker.cxx
+++ LArCalorimeter/LArL1Sim/src/LArTTL1Maker.cxx
@@ -344,8 +344,8 @@ StatusCode LArTTL1Maker::execute()
   ATH_MSG_DEBUG ( "Begining of execution"  );
 
   // Prepare RNG Service
-  ATHRNG::RNGWrapper* rngWrapper = m_RandomSvc->getEngine(this);
-  rngWrapper->setSeed( name(), Gaudi::Hive::currentContext() );
+  ATHRNG::RNGWrapper* rngWrapper = m_RandomSvc->getEngine(this, m_randomStreamName);
+  rngWrapper->setSeed( m_randomStreamName, Gaudi::Hive::currentContext() );
 
 
   //
@@ -1245,7 +1245,7 @@ std::vector<float> LArTTL1Maker::computeNoise(const Identifier towerId, const in
   const float c76 = (c21*c11-c61*c71-c62*c72-c63*c73-c64*c74-c65*c75)/c66;
   const float c77 = sqrt(c11*c11-c71*c71-c72*c72-c73*c73-c74*c74-c75*c75-c76*c76);
 
-  ATHRNG::RNGWrapper* rngWrapper = m_RandomSvc->getEngine(this);
+  ATHRNG::RNGWrapper* rngWrapper = m_RandomSvc->getEngine(this, m_randomStreamName);
   double rndm[s_NBSAMPLES];
   RandGaussZiggurat::shootArray(*rngWrapper,static_cast<int>(s_NBSAMPLES),rndm,0.,1.);
   outputV[0] = inputV[0] + c11*rndm[0];
