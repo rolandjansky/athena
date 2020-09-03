66288198d329dc36671ae29cb92acc8787c9b307
scott snyder <snyder@bnl.gov>
2020-09-04 01:26:20 +0200

    Fix configuration for case where we do not use DigVers.

--- InnerDetector/InDetDigitization/TRT_Digitization/python/TRT_DigitizationConfigNew.py
+++ InnerDetector/InDetDigitization/TRT_Digitization/python/TRT_DigitizationConfigNew.py
@@ -44,6 +44,8 @@ def TRT_DigitizationBasicToolCfg(flags, name="TRT_DigitizationBasicTool", **kwar
     if flags.Detector.Overlay and not flags.Input.isMC:
         acc.merge(addFolders(flags, "/TRT/Cond/DigVers", "TRT_OFL", tag="TRTCondDigVers-Collisions-01", db="OFLP200",
                              className = 'AthenaAttributeList'))
+    else:
+        kwargs.setdefault ('DigVersContainerKey', '')
     # default arguments
     kwargs.setdefault("PAI_Tool_Ar", TRT_PAI_Process_ArToolCfg(flags))
     kwargs.setdefault("PAI_Tool_Kr", TRT_PAI_Process_KrToolCfg(flags))
