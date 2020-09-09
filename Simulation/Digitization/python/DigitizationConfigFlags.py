607d4ec0e83143ec5c71b23547d8609c5984ce62
scott snyder <snyder@bnl.gov>
2020-09-09 22:56:47 +0200

    Digitization: Add missing flag.
    Add missing flag Digitization.PileUpBunchSpacing.
    
    Fixes failure in AthenaConfiguration.

--- Simulation/Digitization/python/DigitizationConfigFlags.py
+++ Simulation/Digitization/python/DigitizationConfigFlags.py
@@ -97,6 +97,7 @@ def createDigitizationCfgFlags():
     # Beam Halo input collections
     flags.addFlag("Digitization.PU.BeamHaloInputCols", [])
     # LHC Bunch Structure (list of non-negative floats)
+    flags.addFlag("Digitization.PileUpBunchSpacing", 25)
     flags.addFlag("Digitization.PU.BeamIntensityPattern", lambda prevFlags: constBunchSpacingPattern(prevFlags.Digitization.PileUpBunchSpacing))
     # Beam Gas input collections
     flags.addFlag("Digitization.PU.BeamGasInputCols", [])
