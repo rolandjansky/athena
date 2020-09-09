011ca714bb117f5ba8d1e4396af86f2c15b6aadb
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2020-09-09 05:19:53 +0000

    cherry-pick deletion of PyJobOptionSvc

--- Control/AthenaPython/src/components/AthenaPython_entries.cxx
+++ Control/AthenaPython/src/components/AthenaPython_entries.cxx
@@ -3,7 +3,6 @@
 #include "AthenaPython/PyAthenaTool.h"
 #include "AthenaPython/PyAthenaAud.h"
 #include "../PyComponentMgr.h"
-#include "../PyJobOptionsSvc.h"
 
 
 DECLARE_COMPONENT( PyAthena::Tool )
@@ -11,5 +10,4 @@ DECLARE_COMPONENT( PyAthena::Alg )
 DECLARE_COMPONENT( PyAthena::Aud )
 DECLARE_COMPONENT( PyAthena::Svc )
 DECLARE_COMPONENT( PyAthena::PyComponentMgr )
-DECLARE_COMPONENT( PyJobOptionsSvc )
 
