c1ef4b3df0367d5f5711d379ce3ec30076db4a2e
Matthew Joseph Basso <matthew.joseph.basso@cern.ch>
2020-09-10 12:21:40 -0700

    Update test alg in anticipation of MVA tool testing

--- InnerDetector/InDetRecTools/TrackVertexAssociationTool/src/TrackVertexAssoTestAlg.h
+++ InnerDetector/InDetRecTools/TrackVertexAssociationTool/src/TrackVertexAssoTestAlg.h
@@ -1,13 +1,12 @@
 ///////////////////////// -*- C++ -*- /////////////////////////////
-
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-
-// TrackVertexAssoTestAlg.h 
+// TrackVertexAssoTestAlg.h
 // Header file for class TrackVertexAssoTestAlg
 // Author: Rachid Mazini, Bo Liu
-/////////////////////////////////////////////////////////////////// 
+///////////////////////////////////////////////////////////////////
+
 #ifndef TRACKVERTEXASSOCIATIONTOOL_TRACKVERTEXASSOTESTALG_H
 #define TRACKVERTEXASSOCIATIONTOOL_TRACKVERTEXASSOTESTALG_H 1
 
@@ -15,73 +14,58 @@
 #include <string>
 
 // FrameWork includes
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "AnaAlgorithm/AnaAlgorithm.h"
 #include "GaudiKernel/ToolHandle.h"
 
 // Tools
 #include "TrackVertexAssociationTool/ITrackVertexAssociationTool.h"
 
-//xAOD 
+// xAOD
 #include "xAODTracking/TrackParticleContainer.h"
 #include "xAODTracking/VertexContainer.h"
 
-class TrackVertexAssoTestAlg
-  : public ::AthAlgorithm
+// ROOT
+#include "TEfficiency.h"
+
+class TrackVertexAssoTestAlg : public EL::AnaAlgorithm
 { 
 
-  /////////////////////////////////////////////////////////////////// 
-  // Public methods: 
-  /////////////////////////////////////////////////////////////////// 
- public: 
+///////////////////////////////////////////////////////////////////
+// Public methods:
+///////////////////////////////////////////////////////////////////
+public: 
 
-  // Copy constructor: 
-
-  /// Constructor with parameters: 
+  /// Constructor with parameters:
   TrackVertexAssoTestAlg( const std::string& name, ISvcLocator* pSvcLocator );
 
   /// Destructor: 
-  virtual ~TrackVertexAssoTestAlg(); 
-
-  // Assignment operator: 
-  //TrackVertexAssoTestAlg &operator=(const TrackVertexAssoTestAlg &alg); 
+  virtual ~TrackVertexAssoTestAlg();
 
   // Athena algorithm's Hooks
-  virtual StatusCode  initialize();
-  virtual StatusCode  execute();
-  virtual StatusCode  finalize();
+  virtual StatusCode initialize();
+  virtual StatusCode execute();
+  virtual StatusCode finalize();
 
-  /////////////////////////////////////////////////////////////////// 
-  // Const methods: 
-  ///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Private data:
+///////////////////////////////////////////////////////////////////
+private:
 
-  /////////////////////////////////////////////////////////////////// 
-  // Non-const methods: 
-  /////////////////////////////////////////////////////////////////// 
-
-  /////////////////////////////////////////////////////////////////// 
-  // Private data: 
-  /////////////////////////////////////////////////////////////////// 
- private: 
-
-  /// Default constructor: 
+  /// Default constructor:
   TrackVertexAssoTestAlg();
 
   ToolHandle<CP::ITrackVertexAssociationTool> m_TVATool;
 
   /// Containers
-  
-  SG::ReadHandleKey<xAOD::TrackParticleContainer> m_trkContname 
-                                                  { this, "TrackContainer", "InDetTrackParticles", "Input track particles" };
+  SG::ReadHandleKey<xAOD::TrackParticleContainer> m_trkContname { this, "TrackContainer", "InDetTrackParticles", "Input track particles" };
   SG::ReadHandleKey<xAOD::VertexContainer> m_vertexContname { this, "VertexContainer", "PrimaryVertices", "Input vertices" };
 
-}; 
+  int   m_nbins = 26;
+  float m_xmin  = -2.5;
+  float m_xmax  = +2.5;
+  TEfficiency* m_h_eff = nullptr; //!
+  TEfficiency* m_h_imp = nullptr; //!
 
-// I/O operators
-//////////////////////
-
-/////////////////////////////////////////////////////////////////// 
-// Inline methods: 
-/////////////////////////////////////////////////////////////////// 
-
+};
 
 #endif //> !TRACKVERTEXASSOCIATIONTOOL_TRACKVERTEXASSOTESTALG_H
