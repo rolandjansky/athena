52f6a433b2619ced49ed99192974a599e114c18c
Noemi Calace <noemi.calace@cern.ch>
2020-09-16 17:54:17 +0200

    Bumbing the map version

--- InnerDetector/InDetExample/InDetSLHC_Example/share/SLHC_Setup_Reco_TrackingGeometry_GMX.py
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/SLHC_Setup_Reco_TrackingGeometry_GMX.py
@@ -10,7 +10,10 @@ DetDescrVersion = jobproperties.Global.DetDescrVersion()
 splitGeo        = DetDescrVersion.split('-')
 TrkDetFlags.SLHC_Geometry           = True
 TrkDetFlags.MaterialStoreGateKey    = "/GLOBAL/TrackingGeo/LayerMaterialITK"
-TrkDetFlags.MaterialVersion         = 20
+if globalflags.DetDescrVersion().startswith('ATLAS-P2-ITK-17') or  globalflags.DetDescrVersion().startswith('ATLAS-P2-ITK-22') :
+  TrkDetFlags.MaterialVersion         = 20
+else:
+  TrkDetFlags.MaterialVersion         = 21
 TrkDetFlags.MaterialSubVersion      = ""
 # change for GMX: also need splitGeo[4] and splitGeo[5]
 TrkDetFlags.MaterialMagicTag = splitGeo[0] + '-' + splitGeo[1] + '-' + splitGeo[2] + '-' + splitGeo[3] + '-' + splitGeo[4] + '-' + splitGeo[5]
