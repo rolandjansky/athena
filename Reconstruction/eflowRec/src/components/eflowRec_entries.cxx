b45c0eb12e88686c2cc6c8c47f34743c1149b9bf
Laura Stephanie Miller <lmiller@lxplus710.cern.ch>
2020-09-29 16:09:13 +0200

    Added copyright statement

--- Reconstruction/eflowRec/src/components/eflowRec_entries.cxx
+++ Reconstruction/eflowRec/src/components/eflowRec_entries.cxx
@@ -1,3 +1,7 @@
+/*
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+*/
+
 #include "eflowRec/eflowTrackCaloExtensionTool.h"
 #include "eflowRec/eflowTrackCaloDummyExtensionTool.h"
 #include "eflowRec/PFTrackClusterMatchingTool.h"
