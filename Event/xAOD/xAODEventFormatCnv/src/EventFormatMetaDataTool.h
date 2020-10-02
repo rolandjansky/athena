33f21ee76ed94d43a69f0c8172497ff70c165bf4
Frank Berghaus <frank.berghaus@cern.ch>
2020-10-02 13:28:17 +0000

    Enabled the unit test in xAODEventFormat.
    This has just laid dormant ever since the CMT->CMake migration. :-(
    
    At the same time introduced some tests for the implicit copy-constructor
    and copy-operator of xAOD::EventFormat.

--- Event/xAOD/xAODEventFormatCnv/src/EventFormatMetaDataTool.h
+++ Event/xAOD/xAODEventFormatCnv/src/EventFormatMetaDataTool.h
@@ -1,19 +1,19 @@
 // Dear emacs, this is -*- c++ -*-
-
-/*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
-*/
+/* Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration */
 
 // $Id: EventFormatMetaDataTool.h 651874 2015-03-05 14:16:19Z krasznaa $
-#ifndef XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
-#define XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
+#ifndef XAODEVENTFORMATCNV_EVENTFORMATMETADATATOOL
+#define XAODEVENTFORMATCNV_EVENTFORMATMETADATATOOL
 
 // System include(s):
 #include <string>
 #include <memory>
+#include <mutex>
 
 // Gaudi/Athena include(s):
+#include "Gaudi/Property.h"
 #include "GaudiKernel/ServiceHandle.h"
+#include "AthenaKernel/IMetaDataSvc.h"
 #include "AthenaKernel/IMetaDataTool.h"
 #include "AthenaBaseComps/AthAlgTool.h"
 #include "StoreGate/StoreGateSvc.h"
@@ -23,69 +23,75 @@
 
 namespace xAODMaker {
 
-   /// Tool taking care of copying the event format object from file to file
-   ///
-   /// This tool does the heavy lifting when fast-merging DxAOD files to
-   /// make sure that the xAOD::EventFormat metadata object is propagated
-   /// correctly from the input files to the output.
-   ///
-   /// @author Jack Cranshaw <cranshaw@anl.gov>
-   /// @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
-   ///
-   /// $Revision: 651874 $
-   /// $Date: 2015-03-05 15:16:19 +0100 (Thu, 05 Mar 2015) $
-   ///
-   class EventFormatMetaDataTool : public virtual ::IMetaDataTool,
-                                   public ::AthAlgTool {
+/// Tool taking care of copying the event format object from file to file
+///
+/// This tool does the heavy lifting when fast-merging DxAOD files to
+/// make sure that the xAOD::EventFormat metadata object is propagated
+/// correctly from the input files to the output.
+///
+/// @author Jack Cranshaw <cranshaw@anl.gov>
+/// @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
+/// @author Frank Berghaus <fberghaus@anl.gov>
+///
+/// $Revision: 651874 $
+/// $Date: 2015-03-05 15:16:19 +0100 (Thu, 05 Mar 2015) $
+///
+class EventFormatMetaDataTool : public virtual ::IMetaDataTool, public ::AthAlgTool {
+ public:
+  /// Regular AlgTool constructor
+  EventFormatMetaDataTool(const std::string& type,
+                          const std::string& name,
+                          const IInterface* parent);
 
-   public:
-      /// Regular AlgTool constructor
-      EventFormatMetaDataTool( const std::string& type, const std::string& name,
-                               const IInterface* parent );
+  /// Function initialising the tool
+  virtual StatusCode initialize();
 
-      /// Function initialising the tool
-      virtual StatusCode initialize();
+  /// Function collecting the metadata from a new input file
+  virtual StatusCode beginInputFile(const SG::SourceID&) {return beginInputFile();}
 
-   /// Function collecting the metadata from a new input file
-   virtual StatusCode beginInputFile(const SG::SourceID&) {return beginInputFile();}
+  /// Function collecting the metadata from a new input file
+  virtual StatusCode endInputFile(const SG::SourceID&) {return endInputFile();}
 
-   /// Function collecting the metadata from a new input file
-   virtual StatusCode endInputFile(const SG::SourceID&) {return endInputFile();}
+  /// Wait for metadata write operations to finish, then returns SUCCESS
+  virtual StatusCode metaDataStop(const SG::SourceID&) {return metaDataStop();}
 
-   /// Function writing the collected metadata to the output
-   virtual StatusCode metaDataStop(const SG::SourceID&) {return metaDataStop();}
+  /// Function called when a new input file is opened
+  virtual StatusCode beginInputFile();
 
-      /// Function called when a new input file is opened
-      virtual StatusCode beginInputFile();
-  
-      /// Function called when the currently open input file got completely
-      /// processed
-      virtual StatusCode endInputFile();
-  
-      /// Function called when the tool should write out its metadata
-      virtual StatusCode metaDataStop();
-  
-   private:
-      /// Function collecting the event format metadata from the input file
-      StatusCode collectMetaData();
-      /// Function writing out the collected metadata
-      StatusCode writeMetaData();
+  /// Function called when the currently open input file got completely
+  /// processed
+  virtual StatusCode endInputFile() {return StatusCode::SUCCESS;}
 
-      /// Connection to the input metadata store
-      ServiceHandle< ::StoreGateSvc > m_inputMetaStore;
-      /// Connection to the output metadata store
-      ServiceHandle< ::StoreGateSvc > m_outputMetaStore;
+  /// Wait for metadata write operations to finish, then return SUCCESS
+  virtual StatusCode metaDataStop();
 
-      /// The key of the trigger menu in the input file
-      std::string m_inputKey;
-      /// The key of the trigger menu for the output file
-      std::string m_outputKey;
+ private:
+  /// Function collecting the event format metadata from the input file
+  StatusCode collectMetaData(std::unique_ptr< xAOD::EventFormat >&);
 
-      /// The merged event format object
-      std::unique_ptr< xAOD::EventFormat > m_format;
+  /// Function writing out the collected metadata
+  StatusCode writeMetaData(std::unique_ptr< xAOD::EventFormat >&);
 
-   }; // class EventFormatMetaDataTool
+  /// Connection to the input metadata store
+  ServiceHandle< ::StoreGateSvc > m_inputMetaStore{this, "InputMetaStore",
+    "StoreGateSvc/InputMetaDataStore", name()};
 
-} // namespace xAODMaker
+  /// Connection to the output metadata store
+  ServiceHandle< IMetaDataSvc > m_outputMetaStore{this, "MetaDataSvc",
+    "MetaDataSvc", name()};
 
-#endif // XAODTRIGGERCNV_TRIGGERMENUMETADATATOOL_H
+  /// The key of the trigger menu in the input file
+  Gaudi::Property< std::string > m_inputKey{this, "InputKey", "",
+    "optionally specify the key of the xAOD::EventFormat in the input."};
+
+  /// The key of the trigger menu for the output file
+  Gaudi::Property< std::string > m_outputKey{this, "OutputKey",
+    "EventFormat", "Specifies the key of the xAOD::EventFormat object in "
+        "the MetaDataStore, default: EventFormat"};
+
+  /// MetaDataStop need to wait for ongoing writes
+  std::mutex m_outputMutex;
+};  // class EventFormatMetaDataTool
+}  // namespace xAODMaker
+
+#endif  // XAODEVENTFORMATCNV_EVENTFORMATMETADATATOOL
