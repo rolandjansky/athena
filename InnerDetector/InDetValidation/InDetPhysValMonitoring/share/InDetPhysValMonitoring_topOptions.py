a30a0ecfb8d52ae0b7560633f698d5b408bbbcaf
Maximilian Goblirsch-Kolb <goblirsc@cern.ch>
2020-10-02 23:41:21 +0200

    revise standalone IDPVM job options to re-use existing code

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/share/InDetPhysValMonitoring_topOptions.py
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/share/InDetPhysValMonitoring_topOptions.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
-
 # Define a few configuration arguments that can be passed by the user via the command line.
 # Avoid the need of hacking the job option file 
 def GetCustomAthArgs():
@@ -14,28 +13,31 @@ def GetCustomAthArgs():
     IDPVMparser.add_argument("--HSFlag", help='Hard-scatter flag - decides what is used for truth matching', choices=['HardScatter', 'All', 'PileUp'],default="HardScatter")
     return IDPVMparser.parse_args()
 
-
+# Parse the arguments 
 MyArgs = GetCustomAthArgs()
 
+# Forward the args into the InDetPhysValFlags used to steer our job 
+from InDetPhysValMonitoring.InDetPhysValJobProperties import InDetPhysValFlags
+InDetPhysValFlags.setTruthStrategy.set_Value_and_Lock(MyArgs.HSFlag)
+InDetPhysValFlags.doExpertOutput.set_Value_and_Lock(MyArgs.doExpertPlots)
+InDetPhysValFlags.doPhysValOutput.set_Value_and_Lock(True)
+InDetPhysValFlags.doValidateTracksInBJets.set_Value_and_Lock(MyArgs.doTracksInBJets)
+InDetPhysValFlags.doValidateTracksInJets.set_Value_and_Lock(MyArgs.doTracksInJets)
+InDetPhysValFlags.doValidateTightPrimaryTracks.set_Value_and_Lock(MyArgs.doTightPrimary)
+
+# Print the configuration
+print(" ==== Final Phys Val flags to run === ")
+print(InDetPhysValFlags)
+
+# Set up the I/O and the RecExCommon infrastructure 
 from InDetRecExample.InDetJobProperties import InDetFlags
 InDetFlags.doSlimming.set_Value_and_Lock(False)
 rec.doTrigger.set_Value_and_Lock(False)
-
 include( "AthenaPython/iread_file.py" )
 
+# Add our monitoring manager to the topSequence
 from AthenaCommon.AlgSequence import AlgSequence
 topSequence = AlgSequence()
-
-from InDetPhysValMonitoring.InDetPhysValMonitoringConf import HistogramDefinitionSvc
-ToolSvc = ServiceMgr.ToolSvc
-ServiceMgr+=HistogramDefinitionSvc()
-ServiceMgr.HistogramDefinitionSvc.DefinitionSource="../share/InDetPVMPlotDefRun2.xml"
-ServiceMgr.HistogramDefinitionSvc.DefinitionFormat="text/xml"
-
-import InDetPhysValMonitoring.InDetPhysValDecoration
-for decorator in InDetPhysValMonitoring.InDetPhysValDecoration.getDecorators() :
-  topSequence += decorator
-
 from AthenaMonitoring.AthenaMonitoringConf import AthenaMonManager
 monMan = AthenaMonManager( "PhysValMonManager",
   FileKey = "M_output",
@@ -45,57 +47,13 @@ monMan = AthenaMonManager( "PhysValMonManager",
   ManualRunLBSetup = True,
   Run = 1,
   LumiBlock = 1  )
-  
-print (monMan)
-
 topSequence += monMan
 
-
-
-from InDetTrackSelectionTool.InDetTrackSelectionToolConf import InDet__InDetTrackSelectionTool
-InDetTrackSelectorTool=InDet__InDetTrackSelectionTool()
-ToolSvc += InDetTrackSelectorTool
-InDetTrackSelectorTool.CutLevel = "TightPrimary"
-                                                 
-from InDetPhysValMonitoring.InDetPhysValMonitoringConf import AthTruthSelectionTool
-AthTruthSelectionTool = AthTruthSelectionTool()
-
-print (AthTruthSelectionTool)
-
-from InDetPhysValMonitoring.InDetPhysValMonitoringConf import InDetPhysValMonitoringTool
-
-tool1 = InDetPhysValMonitoringTool()
-tool1.TruthSelectionTool = AthTruthSelectionTool
-tool1.useTrackSelection = False
-tool1.TrackSelectionTool=InDetTrackSelectorTool
-tool1.PileupSwitch=MyArgs.HSFlag
-tool1.FillTrackInJetPlots = MyArgs.doTracksInJets
-tool1.FillTrackInBJetPlots = MyArgs.doTracksInBJets
-tool1.SkillLevel = 201 if MyArgs.doExpertPlots else 1
-print (tool1)
-tool1.ManagerName = 'PhysValMonManager'
-monMan.AthenaMonTools += [tool1]
-
-if MyArgs.doTightPrimary:
-  from InDetPhysValMonitoring.InDetPhysValMonitoringConf import InDetPhysValMonitoringTool
-  toolTP = InDetPhysValMonitoringTool("IDPVMTool_TightPrimary")
-  toolTP.TruthSelectionTool = AthTruthSelectionTool
-  toolTP.useTrackSelection = True
-  toolTP.SubFolder = "TightPrimary/"
-  toolTP.PileupSwitch=MyArgs.HSFlag
-  toolTP.TrackSelectionTool=InDetTrackSelectorTool
-  toolTP.FillTrackInJetPlots = MyArgs.doTracksInJets
-  toolTP.FillTrackInBJetPlots = MyArgs.doTracksInBJets
-  tool1.SkillLevel = 201 if MyArgs.doExpertPlots else 1
-  print (toolTP)
-  toolTP.ManagerName = 'PhysValMonManager'
-  monMan.AthenaMonTools += [toolTP  ]
-
+# Now we can attach our IDPVM tool instances 
+# To do this, we leverage the existing job options from PhysValMonitoring
+include ("PhysValMonitoring/PhysValInDet_jobOptions.py") 
+  
+# finally, set up the infrastructure for writing our output
 from GaudiSvc.GaudiSvcConf import THistSvc
 ServiceMgr += THistSvc()
 svcMgr.THistSvc.Output += ["M_output DATAFILE='"+MyArgs.outputFile+"' OPT='RECREATE'"]
-
-# Do some additional tweaking:
-from AthenaCommon.AppMgr import theApp
-ServiceMgr.MessageSvc.OutputLevel = INFO
-ServiceMgr.MessageSvc.defaultLimit = 10000
