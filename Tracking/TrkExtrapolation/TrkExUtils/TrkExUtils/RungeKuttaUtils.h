f8f3d4412dc13a368afdcd83c30e62f1248e3292
christos <christos@cern.ch>
2020-10-05 02:07:21 +0200

    tidy a bit the header file

--- Tracking/TrkExtrapolation/TrkExUtils/TrkExUtils/RungeKuttaUtils.h
+++ Tracking/TrkExtrapolation/TrkExUtils/TrkExUtils/RungeKuttaUtils.h
@@ -122,8 +122,17 @@ namespace RungeKuttaUtils
   /////////////////////////////////////////////////////////////////////////////////
 
   bool transformLocalToGlobal(bool, const Trk::TrackParameters&, double*);
-
   bool transformLocalToGlobal(bool, const Trk::NeutralParameters&, double*);
+
+  /////////////////////////////////////////////////////////////////////////////////
+  // Transformations from local to local system coordinates
+  /////////////////////////////////////////////////////////////////////////////////
+
+  bool
+  transformLocalToGlobal(bool,
+                         const Trk::Surface*,
+                         const double* ATH_RESTRICT,
+                         double* ATH_RESTRICT);
 
   /////////////////////////////////////////////////////////////////////////////////
   // Transformations from local to global system coordinates
@@ -131,7 +140,7 @@ namespace RungeKuttaUtils
   /////////////////////////////////////////////////////////////////////////////////
 
   bool transformLocalToGlobal(bool, const Trk::PatternTrackParameters&, double*);
-
+  
   /////////////////////////////////////////////////////////////////////////////////
   // Transformations from global to local system coordinates
   /////////////////////////////////////////////////////////////////////////////////
@@ -142,9 +151,7 @@ namespace RungeKuttaUtils
                               double* ATH_RESTRICT,
                               double* ATH_RESTRICT,
                               double* ATH_RESTRICT);
-  /////////////////////////////////////////////////////////////////////////////////
-
-  /////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
   // Covariance matrix production for Trk::TrackParameters
   /////////////////////////////////////////////////////////////////////////////////
 
@@ -169,17 +176,13 @@ namespace RungeKuttaUtils
   /////////////////////////////////////////////////////////////////////////////////
 
   void jacobianTransformCurvilinearToLocal(const Trk::TrackParameters&, double*);
-
   void jacobianTransformCurvilinearToLocal(const Trk::PatternTrackParameters&, double*);
-
   void jacobianTransformCurvilinearToLocal(double* ATH_RESTRICT, const Trk::Surface*, double* ATH_RESTRICT);
-
   void jacobianTransformCurvilinearToDisc(double* ATH_RESTRICT, double* ATH_RESTRICT);
   void jacobianTransformCurvilinearToPlane(double* ATH_RESTRICT, double* ATH_RESTRICT);
   void jacobianTransformCurvilinearToCylinder(double* ATH_RESTRICT, double* ATH_RESTRICT);
   void jacobianTransformCurvilinearToStraightLine(const double* ATH_RESTRICT, double* ATH_RESTRICT);
 
-  bool transformLocalToGlobal(bool, const Trk::Surface*, const double* ATH_RESTRICT, double* ATH_RESTRICT);
 }
 }
 
