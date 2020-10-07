b5b0f7949ca25cb072f9e8297d51243b3fed3950
Margherita Spalla <margherita.spalla@cern.ch>
2020-10-07 11:04:10 +0200

    actual helper class code

---
+++ LArCalorimeter/LArMonitoring/LArMonitoring/LArStrHelper.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
+
+/**
+ * @class LArStrHelper
+ * @author Margherita Spalla <margherita.spalla@cern.ch>
+ * @date 30-September-2020
+ * @brief Helper class to manipulate strings for LArOnlineID names
+ *
+ * Helper class to manipulate strings for LArOnlineID names:
+ * Since most of the naming is done at python level in AthenaMT, we only add strictly needed functions.
+ * For now, it just contains the FEB naming scheme used by LArNoiseCorrelations. */
+
+
+
+#ifndef LARMONITORING_LARSTRHELPER_H
+#define LARMONITORING_LARSTRHELPER_H
+
+
+#include "TString.h"
+
+class LArStrHelper
+{
+ public:
+
+  /** @brief constructructor */
+  LArStrHelper() {};
+
+  /** @brief default destructor */
+  ~LArStrHelper() {};
+
+  /** FEB names */
+  std::string febNameString(bool,int,int,int) const;
+  std::string fixFEBname(TString) const;
+};
+
+
+
+#endif
---
+++ LArCalorimeter/LArMonitoring/src/LArStrHelper.cxx
@@ -0,0 +1,37 @@
+
+
+#include "LArMonitoring/LArStrHelper.h"
+
+
+
+
+std::string LArStrHelper::febNameString(bool isBarrel,int pos_neg ,int feedthrough,int slot) const {
+  std::string eb=isBarrel ? "Barrel" : "Endcap";
+  std::string ac=(pos_neg==1) ? "A" : "C";
+  return eb+ac+Form("ft%02d",feedthrough)+Form("slot%02d",slot);
+}
+
+
+/** fix custom strings to avoid errors due to uppr-lower cases */
+std::string LArStrHelper::fixFEBname(TString nm) const {
+  TString res=nm;
+  res.ToLower();
+  res.ReplaceAll("barrela","BarrelA");
+  res.ReplaceAll("barrelc","BarrelC");
+  res.ReplaceAll("endcapa","EndcapA");
+  res.ReplaceAll("endcapc","EndcapC");
+  res.ReplaceAll("endcapa","EndcapA");
+  //check for single digit slots of feedthrough and try to correct them
+  int pos = res.Index("ft")+2;
+  TString num=res(pos,2);
+  if(!num.IsDec()) {
+    num=num(0,1);
+    if(num.IsDec()) res.Replace(pos,1,"0"+num,2);
+  }
+  pos = res.Index("slot")+4;
+  num = res(pos,2);
+  if(num.Length()<2) {
+    if(num.IsDec()) res.Replace(pos,1,"0"+num,2);
+  }
+  return (std::string)res;
+}
