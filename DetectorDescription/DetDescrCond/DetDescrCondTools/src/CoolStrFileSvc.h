8ae88c9a9ccce6d9373afa7076dca6c77e1f6a40
scott snyder <snyder@bnl.gov>
2020-10-08 15:42:06 +0200

    DetDescrCondTools: Remove reference to StoreGate.h.
    Remove reference to obsolete header StoreGate.h.

--- DetectorDescription/DetDescrCond/DetDescrCondTools/src/CoolStrFileSvc.h
+++ DetectorDescription/DetDescrCond/DetDescrCondTools/src/CoolStrFileSvc.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef DETDESCRCONDTOOLS_COOLSTRFILESVC_H
@@ -10,7 +10,7 @@
 #include "DetDescrCondTools/ICoolStrFileSvc.h"
 #include "AthenaBaseComps/AthService.h"
 
-#include "StoreGate/StoreGate.h"
+#include "StoreGate/StoreGateSvc.h"
 class CoolStrFileSvc : public virtual ICoolStrFileSvc, public virtual AthService
 {
   template <class TYPE> class SvcFactory;
