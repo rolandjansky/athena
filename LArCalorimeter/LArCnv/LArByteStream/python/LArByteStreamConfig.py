1d7b455b90f1da7faf1ae5364f30d5cd88bc8137
scott snyder <sss@karma>
2020-10-09 10:33:09 -0400

    LArByteStream: Change from ICaloNoiseTool to CaloNoise conditions data.
    Change from ICaloNoiseTool to CaloNoise conditions data.

--- LArCalorimeter/LArCnv/LArByteStream/python/LArByteStreamConfig.py
+++ LArCalorimeter/LArCnv/LArByteStream/python/LArByteStreamConfig.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 
 from LArByteStream.LArByteStreamConf import LArRawDataContByteStreamTool
@@ -7,12 +7,20 @@ from LArByteStream.LArByteStreamConf import LArRawDataContByteStreamTool
 
 class LArRawDataContByteStreamToolConfig(LArRawDataContByteStreamTool):
    __slots__ = []
-   def __init__(self, name="LArRawDataContByteStreamTool", **kwargs):
+   def __init__(self, name="LArRawDataContByteStreamTool",
+                InitializeForWriting = False,
+                stream=None,
+                **kwargs):
       super( LArRawDataContByteStreamToolConfig, self).__init__(name)
-      from CaloTools.CaloNoiseToolDefault import CaloNoiseToolDefault
-      theCaloNoiseTool=CaloNoiseToolDefault()
-      from AthenaCommon.AppMgr import ToolSvc
-      ToolSvc+=theCaloNoiseTool
-      self.NoiseTool = theCaloNoiseTool
+      if InitializeForWriting:
+         from CaloTools.CaloNoiseCondAlg import CaloNoiseCondAlg
+         noisealg = CaloNoiseCondAlg ('totalNoise')
+         if stream:
+            key = noisealg.OutputKey
+            if key.find ('+') < 0:
+               key = 'ConditionsStore+' + key
+            stream.ExtraInputs += [('CaloNoise', key)]
       for n,v in kwargs.items():
-       setattr(self, n, v)
+         setattr(self, n, v)
+      self.InitializeForWriting = InitializeForWriting
+      return
