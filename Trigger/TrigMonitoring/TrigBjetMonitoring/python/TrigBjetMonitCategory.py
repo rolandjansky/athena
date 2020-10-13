730933cd7c1a3c4991ceb2459ed5910a650636f6
enagy <elemer.nagy@cern.ch>
2020-10-13 13:06:25 +0200

    Removing HLT_j225_subjesgscIS_ftf_bdl1r40_split_L1J100 and  HLT_j225_pf_subjesgscIS_ftf_bdl1r40_split_L1J100  per request of Dan Guest in ATR-22165

--- Trigger/TrigMonitoring/TrigBjetMonitoring/python/TrigBjetMonitCategory.py
+++ Trigger/TrigMonitoring/TrigBjetMonitoring/python/TrigBjetMonitCategory.py
@@ -10,9 +10,9 @@ from __future__ import print_function
 
 monitoring_bjet      = ["E_HLT_j35_boffperf_split","E_HLT_j55_boffperf_split","S_HLT_j85_gsc110_boffperf_split","S_HLT_j45_gsc55_boffperf_split","E_HLT_j225_gsc300_boffperf_split",
                         "E_HLT_j45_subjesgscIS_ftf_boffperf_split_L1J20","E_HLT_j45_subjesgscIS_ftf_bmv2c1070_split_L1J20","E_HLT_j275_subjesgscIS_ftf_boffperf_split_L1J100",
-                        "E_HLT_j225_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
+                        "E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
                         "E_HLT_j360_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_subjesgscIS_ftf_bdl1r70_split_L1J20",
-                        "E_HLT_j225_pf_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
+                        "E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
                         "E_HLT_j300_pf_subjesgscIS_ftf_bdl1r70_split_L1J100","E_HLT_j360_pf_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_pf_subjesgscIS_ftf_bdl1r70_split_L1J20",
                         "E_HLT_j45_pf_subjesgscIS_ftf_boffperf_split_L1J20"]
 
@@ -23,9 +23,9 @@ monitoring_mujet     = ["S_HLT_mu4_j45_gsc55_boffperf_split_dr05_dz02","E_HLT_mu
 
 monitoring_bjet_pp      = ["E_HLT_j35_boffperf_split","E_HLT_j55_boffperf_split","S_HLT_j85_gsc110_boffperf_split","S_HLT_j45_gsc55_boffperf_split","E_HLT_j225_gsc300_boffperf_split",
                            "E_HLT_j45_subjesgscIS_ftf_boffperf_split_L1J20","E_HLT_j45_subjesgscIS_ftf_bmv2c1070_split_L1J20","E_HLT_j275_subjesgscIS_ftf_boffperf_split_L1J100",
-                           "E_HLT_j225_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
+                           "E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
                            "E_HLT_j360_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_subjesgscIS_ftf_bdl1r70_split_L1J20",
-                           "E_HLT_j225_pf_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
+                           "E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
                            "E_HLT_j300_pf_subjesgscIS_ftf_bdl1r70_split_L1J100","E_HLT_j360_pf_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_pf_subjesgscIS_ftf_bdl1r70_split_L1J20",
                            "E_HLT_j45_pf_subjesgscIS_ftf_boffperf_split_L1J20"]
 
@@ -51,9 +51,9 @@ monitoring_mujet_hi    = ["E_HLT_mu4_j20_ion_dr05", "S_HLT_mu4_j60_ion_dr05", "S
 
 monitoring_bjet_validation = ["E_HLT_j35_boffperf_split","E_HLT_j55_boffperf_split","S_HLT_j85_gsc110_boffperf_split","S_HLT_j45_gsc55_boffperf_split","E_HLT_j225_gsc300_boffperf_split",
                               "E_HLT_j45_subjesgscIS_ftf_boffperf_split_L1J20","E_HLT_j45_subjesgscIS_ftf_bmv2c1070_split_L1J20","E_HLT_j275_subjesgscIS_ftf_boffperf_split_L1J100",
-                              "E_HLT_j225_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
+                              "E_HLT_j275_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j300_subjesgscIS_ftf_bdl1r70_split_L1J100",
                               "E_HLT_j360_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_subjesgscIS_ftf_bdl1r70_split_L1J20",
-                              "E_HLT_j225_pf_subjesgscIS_ftf_bdl1r40_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
+                              "E_HLT_j275_pf_subjesgscIS_ftf_bdl1r60_split_L1J100","E_HLT_j275_pf_subjesgscIS_ftf_boffperf_split_L1J100",
                               "E_HLT_j300_pf_subjesgscIS_ftf_bdl1r70_split_L1J100","E_HLT_j360_pf_subjesgscIS_ftf_bdl1r77_split_L1J100","E_HLT_j45_pf_subjesgscIS_ftf_bdl1r70_split_L1J20",
                               "E_HLT_j45_pf_subjesgscIS_ftf_boffperf_split_L1J20"]
 
