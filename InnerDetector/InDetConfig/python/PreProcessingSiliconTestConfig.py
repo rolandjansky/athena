509a4059f412807b37af142693af510deb27ef02
Vasyukov Artem <artem.vasyukov@cern.ch>
2020-10-16 01:06:08 +0200

    SpacePointFinder.py and PreProcessingSiliconTestConfid.py have been modified and merged into SiliconPreProcessing.py; SpacePointFinder.py and PreProcessingSiliconTestConfig.py should be removed.

--- InnerDetector/InDetConfig/python/PreProcessingSiliconTestConfig.py
+++ InnerDetector/InDetConfig/python/PreProcessingSiliconTestConfig.py
@@ -1,57 +1,212 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
-## this is test for clusterization configuration
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+from AthenaConfiguration.ComponentFactory import CompFactory
 
-#import TrackReco 	   as TrackReco
-#import PixelGeoModelConfig      as PixelGeoModelConfig
-#import SCT_GeoModelConfig	as SCT_GeoModelConfig
-import SpacePointFinder as SPF
+def NnPixelClusterSplitProbToolCfg(flags, name="NnPixelClusterSplitProbTool", **kwargs):
+    acc = ComponentAccumulator()
 
-#arg_TrackingCuts             = 'TrackingCuts'
-#arg_TrackCollectionKeys      = 'tracks'
-#arg_TrackCollectionTruthKeys = 'truth'
+    # --- new NN prob tool
+    MultiplicityContent = [1 , 1 , 1]
 
-# the requires a single additional layer where the algorithm returned by getInDetPixelClusterization is collected:
-def main(flags) :
-    from AthenaCommon.Configurable import Configurable
-    Configurable.configurableRun3Behavior = 1
-    from ClusterizationConfig import InDetClusterizationAlgorithmsCfg
-    result = InDetClusterizationAlgorithmsCfg(flags)
+    from InDetConfig.TrackingCommonConfig import NnClusterizationFactoryCfg
+    NnClusterizationFactory = NnClusterizationFactoryCfg(flags)
+    acc.addPublicTool(NnClusterizationFactory)
 
-    from BeamSpotConditions.BeamSpotConditionsConfig import BeamSpotCondAlgCfg
-    acc.merge(BeamSpotCondAlgCfg(flags))
+    useBeamConstraint = flags.InDet.useBeamConstraint
 
-    if flags.InDetFlags.doSpacePointFormation:
-        result.merge(SPF.InDetSiElementPropertiesTableCondAlgCfg(flags))
-        result.merge(SPF.InDetSiTrackerSpacePointFinderCfg(flags))
-    ### configure silicon pre processing 
+    kwargs.setdefault("PriorMultiplicityContent", MultiplicityContent)
+    kwargs.setdefault("NnClusterizationFactory", NnClusterizationFactory)
+    kwargs.setdefault("useBeamSpotInfo", useBeamConstraint)
 
-    return result
+    if flags.InDet.doSLHC:
+        NnPixelClusterSplitProbTool = CompFactory.InDet.TruthPixelClusterSplitProbTool(name=name,**kwargs)
+    else:
+        NnPixelClusterSplitProbTool = CompFactory.InDet.NnPixelClusterSplitProbTool(name=name,**kwargs)
+
+    acc.setPrivateTools(NnPixelClusterSplitProbTool)
+    return acc
+
+def NnPixelClusterSplitterCfg(flags, name="NnPixelClusterSplitter", **kwargs):
+    acc = ComponentAccumulator()
+
+    from InDetConfig.TrackingCommonConfig import NnClusterizationFactoryCfg
+    NnClusterizationFactory = NnClusterizationFactoryCfg(flags)
+    acc.addPublicTool(NnClusterizationFactory)
+
+    useBeamConstraint = flags.InDet.useBeamConstraint
+
+    kwargs.setdefault("NnClusterizationFactory", NnClusterizationFactory)
+    kwargs.setdefault("ThresholdSplittingIntoTwoClusters", 0.5)
+    kwargs.setdefault("ThresholdSplittingIntoThreeClusters", 0.25)
+    kwargs.setdefault("SplitOnlyOnBLayer", False)
+    kwargs.setdefault("useBeamSpotInfo", useBeamConstraint)
+    # --- new NN splitter
+    if flags.InDet.doSLHC:
+        NnPixelClusterSplitter = CompFactory.InDet.TruthPixelClusterSplitter(name=name,**kwargs)
+    else:
+        NnPixelClusterSplitter = CompFactory.InDet.NnPixelClusterSplitter(name=name,**kwargs)
+
+    acc.setPrivateTools(NnPixelClusterSplitter)
+    return acc
+
+def InDetRecPreProcessingSiliconCfg(flags, **kwargs):
+    acc = ComponentAccumulator()
+    # ------------------------------------------------------------
+    #
+    # ----------- Data-Preparation stage
+    #
+    # ------------------------------------------------------------
+    #
+    # ----------- PrepRawData creation from Raw Data Objects
+    #
+    redoPatternRecoAndTracking = False
+
+    if flags.InDet.doPRDFormation:
+        #
+        # --- Slim BCM RDOs by zero-suppressing
+        #   
+        if flags.Detector.RecoBCM:
+            from InDetConfig.TrackRecoConfig import BCM_ZeroSuppressionCfg
+            acc.merge(BCM_ZeroSuppressionCfg(flags))
+        
+        if flags.Detector.RecoPixel or flags.Detector.RecoSCT:
+            #
+            # --- SiLorentzAngleTool
+            #
+            from SiLorentzAngleTool.PixelLorentzAngleConfig import PixelLorentzAngleTool, PixelLorentzAngleCfg
+            PixelLorentzAngleTool = PixelLorentzAngleTool(flags)
+            acc.addPublicTool(PixelLorentzAngleTool)
+            acc.addPublicTool(acc.popToolsAndMerge(PixelLorentzAngleCfg(flags)))
+
+            from SiLorentzAngleTool.SCT_LorentzAngleConfig import SCT_LorentzAngleCfg
+            SCTLorentzAngleTool = acc.popToolsAndMerge( SCT_LorentzAngleCfg(flags) )    
+            acc.addPublicTool(SCTLorentzAngleTool)
+            #
+            # --- ClusterMakerTool (public), needed by Pixel and SCT Clusterization
+            #
+            from InDetConfig.TrackRecoConfig import ClusterMakerToolCfg
+            acc.merge(ClusterMakerToolCfg(flags, PixelLorentzAngleTool=PixelLorentzAngleTool, SCTLorentzAngleTool=SCTLorentzAngleTool))
+        #
+        # -- Pixel Clusterization
+        #
+        if (flags.Detector.RecoPixel and flags.InDet.doPixelPRDFormation) or redoPatternRecoAndTracking:
+            #
+            # --- do we use new splittig or not ?
+            #
+            if flags.InDet.doPixelClusterSplitting:
+                #
+                # --- Neutral Network version ?
+                #
+                if flags.InDet.pixelClusterSplittingType == 'NeuralNet':
+                    NnPixelClusterSplitProbTool = acc.popToolsAndMerge(NnPixelClusterSplitProbToolCfg(flags))
+                    acc.addPublicTool(NnPixelClusterSplitProbTool)
+                
+                    NnPixelClusterSplitter = acc.popToolsAndMerge(NnPixelClusterSplitterCfg(flags))
+                    acc.addPublicTool(NnPixelClusterSplitter)
+                #
+                # --- Neutral Network version ?
+                #
+                elif flags.InDet.pixelClusterSplittingType == 'AnalogClus':      
+                    # new splitter tool
+                    TotPixelClusterSplitter=CompFactory.InDet.TotPixelClusterSplitter (name="TotPixelClusterSplitter")
+                    acc.addPublicTool(TotPixelClusterSplitter)
+            #
+            # --- PixelClusterization algorithm
+            #
+            from InDetConfig.TrackRecoConfig import PixelClusterizationCfg
+            acc.merge(PixelClusterizationCfg(flags))
+            if flags.InDet.doSplitReco :
+                from InDetConfig.TrackRecoConfig import PixelClusterizationPUCfg
+                acc.merge(PixelClusterizationPUCfg(flags))
+        #
+        # --- SCT Clusterization
+        #
+        if flags.Detector.RecoSCT and flags.InDet.doSCT_PRDFormation:
+            #
+            # --- SCT_Clusterization algorithm
+            #
+            from InDetConfig.TrackRecoConfig import SCTClusterizationCfg
+            acc.merge(SCTClusterizationCfg(flags))
+            if flags.InDet.doSplitReco :
+                from InDetConfig.TrackRecoConfig import SCTClusterizationPUCfg
+                acc.merge(SCTClusterizationPUCfg(flags))
+    #
+    # ----------- form SpacePoints from clusters in SCT and Pixels
+    #
+    #
+    if flags.InDet.doSpacePointFormation:
+        from InDetConfig.SpacePointFinder import InDetSiTrackerSpacePointFinderCfg, InDetSiElementPropertiesTableCondAlgCfg
+        acc.merge(InDetSiElementPropertiesTableCondAlgCfg(flags))
+        acc.merge(InDetSiTrackerSpacePointFinderCfg(flags))
+
+    # this truth must only be done if you do PRD and SpacePointformation
+    # If you only do the latter (== running on ESD) then the needed input (simdata)
+    # is not in ESD but the resulting truth (clustertruth) is already there ...
+    if flags.InDet.doPRDFormation and flags.InDet.doSpacePointFormation:
+        if flags.InDet.doTruth:
+            from InDetConfig.SpacePointFinder import InDetPRD_MultiTruthMakerSiCfg
+            acc.merge(InDetPRD_MultiTruthMakerSiCfg(flags))
+            if flags.InDet.doSplitReco:
+                from InDetConfig.SpacePointFinder import InDetPRD_MultiTruthMakerSiPUCfg
+                acc.merge(InDetPRD_MultiTruthMakerSiPUCfg(flags))
+
+    return acc
 
 
 if __name__ == "__main__":
-    import IDTestFlags_orig
-    flags = IDTestFlags_orig.IDTestFlags_q221()
-    ##import InDetConfigFlags
-    ##flags = InDetConfigFlags.createInDetConfigFlags()
-    ##from AthenaConfiguration.AllConfigFlags import ConfigFlags
-    ##flags = ConfigFlags
-    flags.IOVDb.GlobalTag = "OFLCOND-MC16-SDR-16"
-    flags.Input.isMC = True
-    flags.Detector.SimulatePixel = False
-    flags.Detector.SimulateSCT   = False
-    flags.Detector.SimulateTRT   = False
-    flags.GeoModel.Align.Dynamic    = False
-    flags.GeoModel.AtlasVersion = 'ATLAS-R2-2016-01-00-01'
-    #######flags.InDetFlags.doPixelClusterSplitting = True
+    from AthenaCommon.Configurable import Configurable
+    Configurable.configurableRun3Behavior=1
 
-    flags.Input.Files=["myRDO.pool.root"]
-    flags.lock()
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from AthenaConfiguration.TestDefaults import defaultTestFiles
+    ConfigFlags.Input.Files=defaultTestFiles.RDO
 
-    acc = main(flags)
-    ##acc.setAppProperty("EvtMax",25)
-    ##acc.store(open("test_SiClusterization.pkl", "w"))
-    acc.run(25)
-    #with open('test4.pkl', mode="wb") as f:
-    #   dill.dump(acc, f)
-    acc.store(open("test01.pkl", "w"))
+    ConfigFlags.Detector.GeometryPixel   = True 
+    ConfigFlags.Detector.GeometrySCT   = True
+    ConfigFlags.InDet.doPixelClusterSplitting = True
+
+    ConfigFlags.Detector.RecoIBL = True
+    ConfigFlags.Detector.RecoPixel = True
+    ConfigFlags.Detector.RecoSCT = True
+    ConfigFlags.Detector.RecoTRT = True
+    
+    numThreads=1
+    ConfigFlags.Concurrency.NumThreads=numThreads
+    ConfigFlags.Concurrency.NumConcurrentEvents=numThreads
+
+    ConfigFlags.loadAllDynamicFlags()
+
+    ConfigFlags.lock()
+    ConfigFlags.dump()
+    
+    from AthenaConfiguration.MainServicesConfig import MainServicesCfg
+    top_acc = MainServicesCfg(ConfigFlags)
+
+    from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
+    top_acc.merge(PoolReadCfg(ConfigFlags))
+
+    from MagFieldServices.MagFieldServicesConfig import MagneticFieldSvcCfg
+    top_acc.merge(MagneticFieldSvcCfg(ConfigFlags))
+
+    from BeamSpotConditions.BeamSpotConditionsConfig import BeamSpotCondAlgCfg
+    top_acc.merge(BeamSpotCondAlgCfg(ConfigFlags))
+
+    from PixelGeoModel.PixelGeoModelConfig import PixelGeometryCfg
+    top_acc.merge( PixelGeometryCfg(ConfigFlags) )
+
+    from SCT_GeoModel.SCT_GeoModelConfig import SCT_GeometryCfg
+    top_acc.merge(SCT_GeometryCfg(ConfigFlags))
+
+    from PixelConditionsAlgorithms.PixelConditionsConfig import PixelDetectorElementCondAlgCfg, PixelAlignCondAlgCfg
+    top_acc.merge(PixelAlignCondAlgCfg(ConfigFlags))
+    top_acc.merge(PixelDetectorElementCondAlgCfg(ConfigFlags))
+
+    top_acc.merge(InDetRecPreProcessingSiliconCfg(ConfigFlags))
+
+    iovsvc = top_acc.getService('IOVDbSvc')
+    iovsvc.OutputLevel=5
+
+    top_acc.getService('StoreGateSvc').Dump = True
+    top_acc.printConfig(withDetails = True, summariseProps = True)
+    top_acc.run(25)
--- InnerDetector/InDetConfig/python/SpacePointFinder.py
+++ InnerDetector/InDetConfig/python/SpacePointFinder.py
@@ -1,136 +1,176 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
-"""Define fuctions to configure Space Point Finder
-
-Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
-"""
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ComponentFactory import CompFactory
-# from IOVDbSvc.IOVDbSvcConfig import addFolders,addFoldersSplitOnline
 
-def InDetSiElementPropertiesTableCondAlgCfg(flags, **kwargs):
+def InDetSiElementPropertiesTableCondAlgCfg(flags, name="InDetSiElementPropertiesTableCondAlg", **kwargs):
     acc = ComponentAccumulator()
-    acc.addCondAlgo(CompFactory.InDet.SiElementPropertiesTableCondAlg(name = "InDetSiElementPropertiesTableCondAlg", **kwargs))
+    acc.addCondAlgo(CompFactory.InDet.SiElementPropertiesTableCondAlg(name = name, **kwargs))
     return acc
 
-def InDetSiTrackerSpacePointFinderCfg(flags, **kwargs):
-    if flags.InDetFlags.doSLHC:
+def SiSpacePointMakerToolCfg(flags, name="InDetSiSpacePointMakerTool", **kwargs):
+    acc = ComponentAccumulator()
+    #
+    # --- SiSpacePointMakerTool (public)
+    #
+
+    if flags.InDet.doSLHC:
         kwargs.setdefault("SCTGapParameter", 0.0015)
-    
-    if flags.InDetFlags.doCosmics or flags.InDetFlags.doBeamHalo:
+
+    if flags.Beam.Type == "cosmics" or flags.InDet.doBeamHalo:
         kwargs.setdefault("StripLengthTolerance", 0.05)
         kwargs.setdefault("UsePerpendicularProjection", True)
-
+    
     InDetSiSpacePointMakerTool = CompFactory.InDet.SiSpacePointMakerTool(name = "InDetSiSpacePointMakerTool", **kwargs)
-        
+    acc.setPrivateTools(InDetSiSpacePointMakerTool)
+    return acc
+
+def InDetSiTrackerSpacePointFinderCfg(flags, name = "InDetSiTrackerSpacePointFinder", **kwargs):
     acc = ComponentAccumulator()
+    #
+    # SiTrackerSpacePointFinder algorithm
+    #
+
+    InDetSiSpacePointMakerTool = acc.popToolsAndMerge(SiSpacePointMakerToolCfg(flags))
     acc.addPublicTool(InDetSiSpacePointMakerTool) ## I decided to merge it
-    kwargs.update(
-        SiSpacePointMakerTool  = InDetSiSpacePointMakerTool,
-        PixelsClustersName     = flags.InDetKeys.PixelClusters,
-        SCT_ClustersName       = flags.InDetKeys.SCT_Clusters,
-        SpacePointsPixelName   = flags.InDetKeys.PixelSpacePoints,
-        SpacePointsSCTName     = flags.InDetKeys.SCT_SpacePoints,
-        SpacePointsOverlapName = flags.InDetKeys.OverlapSpacePoints,
-        ProcessPixels          = flags.DetFlags.haveRIO.pixel_on,
-        ProcessSCTs            = flags.DetFlags.haveRIO.SCT_on,
-        ProcessOverlaps        = flags.DetFlags.haveRIO.SCT_on)
 
-    if flags.InDetFlags.doDBMstandalone:
-        kwargs.update(
-            OverlapLimitEtaMax = 5.0,
-            OverlapLimitEtaMin = 0)
+    kwargs.setdefault("SiSpacePointMakerTool", InDetSiSpacePointMakerTool)
+    kwargs.setdefault("PixelsClustersName", 'PixelClusters') # InDetKeys.PixelClusters
+    kwargs.setdefault("SCT_ClustersName", 'SCT_Clusters') # InDetKeys.SCT_Clusters
+    kwargs.setdefault("SpacePointsPixelName", 'PixelSpacePoints') # InDetKeys.PixelSpacePoints
+    kwargs.setdefault("SpacePointsSCTName", 'SCT_SpacePoints') # InDetKeys.SCT_SpacePoints
+    kwargs.setdefault("SpacePointsOverlapName", 'OverlapSpacePoints') # InDetKeys.OverlapSpacePoints)
+    kwargs.setdefault("ProcessPixels", flags.Detector.RecoPixel)
+    kwargs.setdefault("ProcessSCTs", flags.Detector.RecoSCT)
+    kwargs.setdefault("ProcessOverlaps", flags.Detector.RecoSCT)
 
-    if flags.InDetFlags.doCosmics:
-        kwargs.update(
-            ProcessOverlaps      = False,
-            OverrideBeamSpot     = True,
-            VertexZ              = 0,
-            VertexX              = 0,
-            VertexY              = 99999999,
-            OverlapLimitOpposite = 5)  
-    acc.addEventAlgo(CompFactory.InDet.SiTrackerSpacePointFinder(name = "InDetSiTrackerSpacePointFinder", **kwargs))
+    if flags.InDet.doDBMstandalone:
+        kwargs.setdefault("OverlapLimitEtaMax", 5.0)
+        kwargs.setdefault("OverlapLimitEtaMin", 0)
+
+    if flags.Beam.Type == "cosmics":
+        kwargs.setdefault("ProcessOverlaps", False)
+        kwargs.setdefault("OverrideBeamSpot", True)
+        kwargs.setdefault("VertexZ", 0)
+        kwargs.setdefault("VertexX", 0)
+        kwargs.setdefault("VertexY", 99999999)
+        kwargs.setdefault("OverlapLimitOpposite", 5)
+
+    acc.addEventAlgo(CompFactory.InDet.SiTrackerSpacePointFinder(name = name, **kwargs))
     return acc
 
-''' ### Py:ConfigurableDb WARNING : Class InDet.PRD_MultiTruthMakerSi not in database 
-
-def InDetPRD_MultiTruthMakerSiCfg(flags, **kwargs):
-    InDet.PRD_MultiTruthMakerSi = CompFactory.InDet.PRD_MultiTruthMakerSi
-    InDetPRD_MultiTruthMakerSi = InDet.PRD_MultiTruthMakerSi(name = "InDetPRD_MultiTruthMakerSi")
-    kwargs.update(
-        PixelClusterContainerName   = flags.InDetKeys.PixelClusters,
-        SCTClusterContainerName     = flags.InDetKeys.SCT_Clusters,
-        TRTDriftCircleContainerName = "",
-        SimDataMapNamePixel         = flags.InDetKeys.PixelSDOs,
-        SimDataMapNameSCT           = flags.InDetKeys.SCT_SDOs,
-        SimDataMapNameTRT           = "",
-        TruthNamePixel              = flags.InDetKeys.PixelClustersTruth,
-        TruthNameSCT                = flags.InDetKeys.SCT_ClustersTruth,
-        TruthNameTRT                = "")
-    
-    if (not flags.DetFlags.haveRIO.pixel_on or not flags.InDetFlags.doPixelPRDFormation):
-        kwargs.update(
-            PixelClusterContainerName = "",
-            SimDataMapNamePixel       = "",
-            TruthNamePixel            = "")
-    if (not flags.DetFlags.haveRIO.SCT_on or not InDetFlags.doSCT_PRDFormation):
-        kwargs.update(
-            SCTClusterContainerName   = "",
-            SimDataMapNameSCT         = "",
-            TruthNameSCT              = "")
-    
+def InDetPRD_MultiTruthMakerSiCfg(flags, name="InDetPRD_MultiTruthMakerSi", **kwargs):
     acc = ComponentAccumulator()
-    acc.addEventAlgo(CompFactory.InDet.PRD_MultiTruthMakerSi(name = "InDetPRD_MultiTruthMakerSi", **kwargs))
+    if flags.InDet.doTruth:
+        kwargs.setdefault("PixelClusterContainerName", 'PixelClusters') # InDetKeys.PixelClusters()
+        kwargs.setdefault("SCTClusterContainerName", 'SCT_Clusters') # InDetKeys.SCT_Clusters()
+        kwargs.setdefault("TRTDriftCircleContainerName", "")
+        kwargs.setdefault("SimDataMapNamePixel", 'PixelSDO_Map') # InDetKeys.PixelSDOs()
+        kwargs.setdefault("SimDataMapNameSCT", 'SCT_SDO_Map') # InDetKeys.SCT_SDOs()
+        kwargs.setdefault("SimDataMapNameTRT", "")
+        kwargs.setdefault("TruthNamePixel", 'PRD_MultiTruthPixel') # InDetKeys.PixelClustersTruth()
+        kwargs.setdefault("TruthNameSCT", 'PRD_MultiTruthSCT') # InDetKeys.SCT_ClustersTruth()
+        kwargs.setdefault("TruthNameTRT", "")
+         # a bit complicated, but this is how the truth maker gets to know which detector is on
+        if (not flags.Detector.RecoPixel or not flags.InDet.doPixelPRDFormation):
+            kwargs.setdefault("PixelClusterContainerName", "")
+            kwargs.setdefault("SimDataMapNamePixel", "")
+            kwargs.setdefault("TruthNamePixel", "")
+        if (not flags.Detector.RecoSCT or not flags.InDet.doSCT_PRDFormation):
+            kwargs.setdefault("SCTClusterContainerName", "")
+            kwargs.setdefault("SimDataMapNameSCT", "")
+            kwargs.setdefault("TruthNameSCT", "")
+
+    InDetPRD_MultiTruthMakerSi = CompFactory.InDet.PRD_MultiTruthMaker(name = name, **kwargs)
+    acc.addEventAlgo(InDetPRD_MultiTruthMakerSi)
     return acc
 
-def InDetPRD_MultiTruthMakerSiPUCfg(flags, **kwargs):
-    kwargs.update(
-        PixelClusterContainerName   = flags.InDetKeys.PixelPUClusters,
-        SCTClusterContainerName     = flags.InDetKeys.SCT_PU_Clusters,
-        TRTDriftCircleContainerName = "",
-        SimDataMapNamePixel         = flags.InDetKeys.PixelPUSDOs,
-        SimDataMapNameSCT           = flags.InDetKeys.SCT_PU_SDOs,
-        SimDataMapNameTRT           = "",
-        TruthNamePixel              = flags.InDetKeys.PixelPUClustersTruth,
-        TruthNameSCT                = flags.InDetKeys.SCT_PU_ClustersTruth,
-        TruthNameTRT                = "",
-        OutputLevel                 = VERBOSE)
-    
-    if (not flags.DetFlags.haveRIO.pixel_on or not flags.InDetFlags.doPixelPRDFormation):
-        kwargs.update(
-            PixelClusterContainerName = "",
-            SimDataMapNamePixel       = "",
-            TruthNamePixel            = "")
-    if (not flags.DetFlags.haveRIO.SCT_on or not InDetFlags.doSCT_PRDFormation):
-        kwargs.update(
-            SCTClusterContainerName   = "",
-            SimDataMapNameSCT         = "",
-            TruthNameSCT              = "")
-    
+def InDetPRD_MultiTruthMakerSiPUCfg(flags, name="InDetPRD_MultiTruthMakerSiPU", **kwargs):
     acc = ComponentAccumulator()
-    acc.addEventAlgo(CompFactory.InDet.PRD_MultiTruthMakerSiPU(name = "InDetPRD_MultiTruthMakerSi", **kwargs))
-    return acc '''
+    if flags.InDet.doTruth:
+        kwargs.setdefault("PixelClusterContainerName", 'PixelPUClusters') # InDetKeys.PixelPUClusters()
+        kwargs.setdefault("SCTClusterContainerName", 'SCT_PU_Clusters') # InDetKeys.SCT_PU_Clusters()
+        kwargs.setdefault("TRTDriftCircleContainerName", "")
+        kwargs.setdefault("SimDataMapNamePixel", 'Pixel_PU_SDO_Map') # InDetKeys.PixelPUSDOs()
+        kwargs.setdefault("SimDataMapNameSCT", 'SCT_PU_SDO_Map') # InDetKeys.SCT_PU_SDOs()
+        kwargs.setdefault("SimDataMapNameTRT", "")
+        kwargs.setdefault("TruthNamePixel", 'PRD_PU_MultiTruthPixel') # InDetKeys.PixelPUClustersTruth()
+        kwargs.setdefault("TruthNameSCT", 'PRD_PU_MultiTruthSCT') # InDetKeys.SCT_PU_ClustersTruth()
+        kwargs.setdefault("TruthNameTRT", "")
+         # a bit complicated, but this is how the truth maker gets to know which detector is on
+        if (not flags.Detector.RecoPixel or not flags.InDet.doPixelPRDFormation):
+            kwargs.setdefault("PixelClusterContainerName", "")
+            kwargs.setdefault("SimDataMapNamePixel", "")
+            kwargs.setdefault("TruthNamePixel", "")
+        if (not flags.Detector.RecoSCT or not flags.InDet.doSCT_PRDFormation):
+            kwargs.setdefault("SCTClusterContainerName", "")
+            kwargs.setdefault("SimDataMapNameSCT", "")
+            kwargs.setdefault("TruthNameSCT", "")
+
+    InDetPRD_MultiTruthMakerSi = CompFactory.InDet.PRD_MultiTruthMaker(name = name, **kwargs)
+    acc.addEventAlgo(InDetPRD_MultiTruthMakerSi)
+    return acc
+
 
 if __name__ == "__main__":
-    import IDTestFlags
-    flags = IDTestFlags.IDTestFlags_q221()
     from AthenaCommon.Configurable import Configurable
-    Configurable.configurableRun3Behavior = 1
+    Configurable.configurableRun3Behavior=1
+
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from AthenaConfiguration.TestDefaults import defaultTestFiles
+    ConfigFlags.Input.Files=defaultTestFiles.RDO
+
+    ConfigFlags.Detector.RecoTRT = True
+    ConfigFlags.Detector.RecoIBL = True
+
+    numThreads=1
+    ConfigFlags.Concurrency.NumThreads=numThreads
+    ConfigFlags.Concurrency.NumConcurrentEvents=numThreads
+
+    ConfigFlags.loadAllDynamicFlags()
+
+    ConfigFlags.lock()
+    ConfigFlags.dump()
     
     from AthenaConfiguration.MainServicesConfig import MainServicesCfg
-    acc = MainServicesCfg(flags)
-    if flags.InDetFlags.doSpacePointFormation:
-        acc.merge(InDetSiElementPropertiesTableCondAlgCfg(flags))
-        acc.merge(InDetSiTrackerSpacePointFinderCfg(flags))
-    '''
-    if flags.InDetFlags.doPRDFormation and flags.InDetFlags.doSpacePointFormation:
-        if flags.InDetFlags.doTruth:
-            acc.merge(InDetPRD_MultiTruthMakerSiCfg(flags))
-            acc.merge(InDetPRD_MultiTruthMakerSiPUCfg(flags))
-    '''
+    top_acc = MainServicesCfg(ConfigFlags)
 
-    acc.printConfig()
-    #print(acc.getPublicTool("InDetSiSpacePointMakerTool"))
+    from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
+    top_acc.merge(PoolReadCfg(ConfigFlags))
+
+    from MagFieldServices.MagFieldServicesConfig import MagneticFieldSvcCfg
+    top_acc.merge(MagneticFieldSvcCfg(ConfigFlags))
+
+    from BeamSpotConditions.BeamSpotConditionsConfig import BeamSpotCondAlgCfg
+    top_acc.merge(BeamSpotCondAlgCfg(ConfigFlags))
+
+    from PixelGeoModel.PixelGeoModelConfig import PixelGeometryCfg
+    top_acc.merge( PixelGeometryCfg(ConfigFlags) )
+
+    from SCT_GeoModel.SCT_GeoModelConfig import SCT_GeometryCfg
+    top_acc.merge(SCT_GeometryCfg(ConfigFlags))
+
+    from PixelConditionsAlgorithms.PixelConditionsConfig import PixelDetectorElementCondAlgCfg, PixelAlignCondAlgCfg
+    top_acc.merge(PixelAlignCondAlgCfg(ConfigFlags))
+    top_acc.merge(PixelDetectorElementCondAlgCfg(ConfigFlags))
+
+    from InDetConfig.TrackRecoConfig import PixelClusterizationCfg, SCTClusterizationCfg
+    top_acc.merge(PixelClusterizationCfg(ConfigFlags))
+    top_acc.merge(SCTClusterizationCfg(ConfigFlags))
     
+    if ConfigFlags.InDet.doSpacePointFormation:
+        top_acc.merge(InDetSiElementPropertiesTableCondAlgCfg(ConfigFlags))
+        top_acc.merge(InDetSiTrackerSpacePointFinderCfg(ConfigFlags))
 
+    if ConfigFlags.InDet.doPRDFormation and ConfigFlags.InDet.doSpacePointFormation:
+        if ConfigFlags.InDet.doTruth:
+            top_acc.merge(InDetPRD_MultiTruthMakerSiCfg(ConfigFlags))
+            if ConfigFlags.InDet.doSplitReco:
+                top_acc.merge(InDetPRD_MultiTruthMakerSiPUCfg(ConfigFlags))
 
+    iovsvc = top_acc.getService('IOVDbSvc')
+    iovsvc.OutputLevel=5
+
+    top_acc.getService('StoreGateSvc').Dump = True
+    top_acc.printConfig(withDetails = True, summariseProps = True)
+    top_acc.run(25)
