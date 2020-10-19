71a24a258b6aacccc320ba66064e0afd7cb45b1e
scott snyder <snyder@bnl.gov>
2020-10-19 17:02:13 +0200

    DPDUtils: Remove reference to StoreGate.h.
    Remove reference to obsolete header StoreGate.h.

--- PhysicsAnalysis/DPDUtils/DPDUtils/DPDTagTool.h
+++ PhysicsAnalysis/DPDUtils/DPDUtils/DPDTagTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef DPDTAGTOOL_H
@@ -15,7 +15,6 @@
 
 #include "GaudiKernel/Algorithm.h"
 #include "AthenaBaseComps/AthAlgTool.h"
-#include "StoreGate/StoreGate.h"
 #include "StoreGate/StoreGateSvc.h"
 #include "TagEvent/TagFragmentCollection.h"
 #include "AthenaPoolUtilities/AthenaAttributeSpecification.h"
