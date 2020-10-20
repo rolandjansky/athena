cdfa36662f0010f792d3884e5b7f415446c3389d
scott snyder <snyder@bnl.gov>
2020-10-20 15:24:13 +0200

    Fix addition of totalNoise depednency to steam algorithm.

--- LArCalorimeter/LArCnv/LArByteStream/python/LArByteStreamConfig.py
+++ LArCalorimeter/LArCnv/LArByteStream/python/LArByteStreamConfig.py
@@ -5,22 +5,18 @@ from LArByteStream.LArByteStreamConf import LArRawDataContByteStreamTool
 
 
 
-class LArRawDataContByteStreamToolConfig(LArRawDataContByteStreamTool):
-   __slots__ = []
-   def __init__(self, name="LArRawDataContByteStreamTool",
-                InitializeForWriting = False,
-                stream=None,
-                **kwargs):
-      super( LArRawDataContByteStreamToolConfig, self).__init__(name)
+def LArRawDataContByteStreamToolConfig (name="LArRawDataContByteStreamTool",
+                                        InitializeForWriting = False,
+                                        stream=None,
+                                        **kwargs):
+      tool = LArRawDataContByteStreamTool (name, **kwargs)
       if InitializeForWriting:
          from CaloTools.CaloNoiseCondAlg import CaloNoiseCondAlg
          noisealg = CaloNoiseCondAlg ('totalNoise')
          if stream:
             key = noisealg.OutputKey
             if key.find ('+') < 0:
-               key = 'ConditionsStore+' + key
+               key = 'ConditionStore+' + key
             stream.ExtraInputs += [('CaloNoise', key)]
-      for n,v in kwargs.items():
-         setattr(self, n, v)
-      self.InitializeForWriting = InitializeForWriting
-      return
+      tool.InitializeForWriting = InitializeForWriting
+      return tool
