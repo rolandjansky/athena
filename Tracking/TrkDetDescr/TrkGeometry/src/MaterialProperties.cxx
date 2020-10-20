53e876c80605257d66eeb400d8771cdf1446bc1c
christos <christos@cern.ch>
2020-10-20 02:40:48 +0200

    Material properties use default for default ctor, copy , move , dtor . Move inline methods to separate .icc file

--- Tracking/TrkDetDescr/TrkGeometry/src/MaterialProperties.cxx
+++ Tracking/TrkDetDescr/TrkGeometry/src/MaterialProperties.cxx
@@ -9,12 +9,6 @@
 #include "TrkGeometry/MaterialProperties.h"
 #include <iostream>
 
-Trk::MaterialProperties::MaterialProperties():
-  m_material(),
-  m_dInX0(0.),
-  m_dInL0(0.),  
-  m_zOaTrTd(0.)   
-{}
 
 Trk::MaterialProperties::MaterialProperties(
 			             float path,
@@ -27,11 +21,10 @@ Trk::MaterialProperties::MaterialProperties(
   m_material(Xo, Lo, averageA, averageZ, averageRho, dEdX),
   m_dInX0( Xo*Xo > 10e-10 ? path/Xo : 0.),
   m_dInL0( Lo*Lo > 10e-10 ? path/Lo : 0.),
-  m_zOaTrTd(averageA * averageA > 10e-10 ? averageZ/averageA*averageRho*path : 0.)   
+  m_zOaTrTd(averageA * averageA > 10e-10 ? averageZ/averageA*averageRho*path : 0.)
 {}
 
 // TODO add constructor with element composition
-
 Trk::MaterialProperties::MaterialProperties(const Trk::Material& material, float path) :
   m_material(material),
   m_dInX0( material.X0*material.X0 > 10e-10 ? path/material.X0 : 0.),
@@ -39,38 +32,22 @@ Trk::MaterialProperties::MaterialProperties(const Trk::Material& material, float
   m_zOaTrTd( material.A*material.A > 10e-10  ? path*material.Z/material.A*material.rho : 0.)
 {}
 
-Trk::MaterialProperties::MaterialProperties(const Trk::MaterialProperties& mprop)   
-   
-= default;
-
-
-Trk::MaterialProperties* Trk::MaterialProperties::clone() const 
+Trk::MaterialProperties* Trk::MaterialProperties::clone() const
 { return new Trk::MaterialProperties(*this); }
 
 
-Trk::MaterialProperties& Trk::MaterialProperties::operator =( const Trk::MaterialProperties& mprop)
-{
-  if (this!= &mprop){
-      m_material = mprop.m_material;
-      m_dInX0    = mprop.m_dInX0;
-      m_dInL0    = mprop.m_dInL0;  
-      m_zOaTrTd  = mprop.m_zOaTrTd;  
-  }
-  return(*this);
-}
-
 Trk::MaterialProperties& Trk::MaterialProperties::operator *=( float scale )
 {
-  // assuming rescaling of the material thickness 
+  // assuming rescaling of the material thickness
   m_dInX0       *= scale;
   m_dInL0       *= scale;
   m_zOaTrTd     *= scale;
-  
+
   return(*this);
 }
 
 void Trk::MaterialProperties::addMaterial(const Trk::Material& mat, float dInX0){
-  
+
       //!< @todo update ?
       // averaging factors based on thickness
       float fnew = dInX0*mat.X0/(m_dInX0*m_material.X0+dInX0*mat.X0);
@@ -78,7 +55,7 @@ void Trk::MaterialProperties::addMaterial(const Trk::Material& mat, float dInX0)
 
       // updated material thickness
       m_dInX0 += dInX0;
-      
+
       // updated material
       m_material = Trk::Material(1./(fnew/mat.X0+fold/m_material.X0),
 				 1./(fnew/mat.L0+fold/m_material.L0),
@@ -88,7 +65,7 @@ void Trk::MaterialProperties::addMaterial(const Trk::Material& mat, float dInX0)
 
       // updated derived members
       m_dInL0 = m_dInX0*m_material.X0/m_material.L0;
-      m_zOaTrTd = m_material.A > 0 ? m_dInX0*m_material.X0*m_material.Z/m_material.A*m_material.rho : 0;      
+      m_zOaTrTd = m_material.A > 0 ? m_dInX0*m_material.X0*m_material.Z/m_material.A*m_material.rho : 0;
 }
 
 void Trk::MaterialProperties::setMaterial(const Trk::Material& mat, float thickness) {
@@ -108,37 +85,21 @@ void Trk::MaterialProperties::setDeDx(float dEdX)
 MsgStream& Trk::operator << ( MsgStream& sl, const Trk::MaterialProperties& mprop)
 {
     sl << "Trk::MaterialProperties: "                                                 << endmsg;
-    sl << "   - thickness/X0                          = " << mprop.thicknessInX0()    << endmsg; 
+    sl << "   - thickness/X0                          = " << mprop.thicknessInX0()    << endmsg;
     sl << "   - thickness                       [mm]  = " << mprop.thickness()        << endmsg;
     sl << "   - radiation length X0             [mm]  = " << mprop.x0()               << endmsg;
     sl << "   - nuclear interaction length L0   [mm]  = " << mprop.l0()               << endmsg;
     sl << "   - average material Z/A*rho [gram/mm^3]  = " << mprop.zOverAtimesRho()   << endmsg;
-    /*  interface not finalized
-    if (mprop.material().composition){
-        sl << "   - material composition from " << mprop.material().composition->size() << " elements " << std::endl;
-        sl << "       listing them (prob. ordereded ) : " << std::endl;
-        for ( auto& eIter : (*mprop.material().composition) )
-            sl << "         -> Z : " << eIter.element() << "( fraction : "  <<  eIter.fraction() << " )" << std::endl;
-    }
-    */
     return sl;
 }
 
 std::ostream& Trk::operator << ( std::ostream& sl, const MaterialProperties& mprop)
-{ 
+{
     sl << "Trk::MaterialProperties: "                                                 << std::endl;
-    sl << "   - thickness/X0                          = " << mprop.thicknessInX0()    << std::endl; 
+    sl << "   - thickness/X0                          = " << mprop.thicknessInX0()    << std::endl;
     sl << "   - thickness                       [mm]  = " << mprop.thickness()        << std::endl;
     sl << "   - radiation length X0             [mm]  = " << mprop.x0()               << std::endl;
     sl << "   - nuclear interaction length L0   [mm]  = " << mprop.l0()               << std::endl;
     sl << "   - average material Z/A*rho [gram/mm^3]  = " << mprop.zOverAtimesRho()   << std::endl;
-    /*  interface not finalized
-    if (mprop.material().composition){
-        sl << "   - material composition from " << mprop.material().composition->size() << " elements " << std::endl;
-        sl << "       listing them (prob. ordereded ) : " << std::endl;
-        for ( auto& eIter : (*mprop.material().composition) )
-            sl << "         -> Z : " << eIter.element() << "( fraction : "  <<  eIter.fraction() << " )" << std::endl;
-    }    
-    */
     return sl;
 }
