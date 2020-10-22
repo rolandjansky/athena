7f9334177e1509d997c8f210d4e19733e6d18506
Lukas Heinrich <lukas.heinrich@cern.ch>
2020-10-22 05:17:01 +0200

    enable athena components

--- PhysicsAnalysis/Algorithms/EgammaAnalysisAlgorithms/src/components/EgammaAnalysisAlgorithms_entries.cxx
+++ PhysicsAnalysis/Algorithms/EgammaAnalysisAlgorithms/src/components/EgammaAnalysisAlgorithms_entries.cxx
@@ -10,20 +10,11 @@
 #include <EgammaAnalysisAlgorithms/PhotonEfficiencyCorrectionAlg.h>
 #include <EgammaAnalysisAlgorithms/PhotonShowerShapeFudgeAlg.h>
 
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, EgammaCalibrationAndSmearingAlg)
-DECLARE_NAMESPACE_TOOL_FACTORY (CP, EgammaIsGoodOQSelectionTool)
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, EgammaIsolationCorrectionAlg)
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, EgammaIsolationSelectionAlg)
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, ElectronEfficiencyCorrectionAlg)
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, PhotonEfficiencyCorrectionAlg)
-DECLARE_NAMESPACE_ALGORITHM_FACTORY (CP, PhotonShowerShapeFudgeAlg)
+DECLARE_COMPONENT (CP::EgammaCalibrationAndSmearingAlg)
+DECLARE_COMPONENT (CP::EgammaIsGoodOQSelectionTool)
+DECLARE_COMPONENT (CP::EgammaIsolationCorrectionAlg)
+DECLARE_COMPONENT (CP::EgammaIsolationSelectionAlg)
+DECLARE_COMPONENT (CP::ElectronEfficiencyCorrectionAlg)
+DECLARE_COMPONENT (CP::PhotonEfficiencyCorrectionAlg)
+DECLARE_COMPONENT (CP::PhotonShowerShapeFudgeAlg)
 
-DECLARE_FACTORY_ENTRIES(EgammaAnalysisAlgorithms) {
-  DECLARE_NAMESPACE_ALGORITHM (CP, EgammaCalibrationAndSmearingAlg)
-  DECLARE_NAMESPACE_TOOL (CP, EgammaIsGoodOQSelectionTool)
-  DECLARE_NAMESPACE_ALGORITHM (CP, EgammaIsolationCorrectionAlg)
-  DECLARE_NAMESPACE_ALGORITHM (CP, EgammaIsolationSelectionAlg)
-  DECLARE_NAMESPACE_ALGORITHM (CP, ElectronEfficiencyCorrectionAlg)
-  DECLARE_NAMESPACE_ALGORITHM (CP, PhotonEfficiencyCorrectionAlg)
-  DECLARE_NAMESPACE_ALGORITHM (CP, PhotonShowerShapeFudgeAlg)
-}
--- PhysicsAnalysis/Algorithms/MetAnalysisAlgorithms/src/components/MetAnalysisAlgorithms_entries.cxx
+++ PhysicsAnalysis/Algorithms/MetAnalysisAlgorithms/src/components/MetAnalysisAlgorithms_entries.cxx
@@ -1,20 +1,12 @@
 // AsgExampleTools_entries.cxx
 
-#include <GaudiKernel/DeclareFactoryEntries.h>
-
 #include <MetAnalysisAlgorithms/MetBuilderAlg.h>
 #include <MetAnalysisAlgorithms/MetMakerAlg.h>
 #include <MetAnalysisAlgorithms/MetSignificanceAlg.h>
 #include <MetAnalysisAlgorithms/MetSystematicsAlg.h>
 
-DECLARE_ALGORITHM_FACTORY (CP::MetBuilderAlg)
-DECLARE_ALGORITHM_FACTORY (CP::MetMakerAlg)
-DECLARE_ALGORITHM_FACTORY (CP::MetSignificanceAlg)
-DECLARE_ALGORITHM_FACTORY (CP::MetSystematicsAlg)
+DECLARE_COMPONENT (CP::MetBuilderAlg)
+DECLARE_COMPONENT (CP::MetMakerAlg)
+DECLARE_COMPONENT (CP::MetSignificanceAlg)
+DECLARE_COMPONENT (CP::MetSystematicsAlg)
 
-DECLARE_FACTORY_ENTRIES(MetAnalysisAlgorithms) {
-  DECLARE_ALGORITHM (CP::MetBuilderAlg)
-  DECLARE_ALGORITHM (CP::MetMakerAlg)
-  DECLARE_ALGORITHM (CP::MetSignificanceAlg)
-  DECLARE_ALGORITHM (CP::MetSystematicsAlg)
-}
