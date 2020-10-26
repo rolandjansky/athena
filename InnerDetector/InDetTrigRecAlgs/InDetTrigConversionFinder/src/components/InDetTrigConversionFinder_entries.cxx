0a8e22ec9958a37fe29c2ab6ced357c8c3f1cbcb
Jiří Mašík <Jiri.Masik@manchester.ac.uk>
2020-10-26 12:28:56 +0100

    Remove EFID algs not used in the main reconstruction sequences of the legacy trigger

--- InnerDetector/InDetTrigRecAlgs/InDetTrigConversionFinder/src/components/InDetTrigConversionFinder_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "InDetTrigConversionFinder/TrigConversionFinder.h"
-
-using namespace InDet;
-
-DECLARE_COMPONENT( TrigConversionFinder )
-
--- InnerDetector/InDetTrigRecAlgs/TRT_TrigStandaloneTrackFinder/src/components/TRT_TrigStandaloneTrackFinder_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "TRT_TrigStandaloneTrackFinder/TRT_TrigStandaloneTrackFinder.h"
-
-using namespace InDet;
-
-DECLARE_COMPONENT( TRT_TrigStandaloneTrackFinder )
-
