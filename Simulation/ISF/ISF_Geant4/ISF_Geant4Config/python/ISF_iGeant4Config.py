cc0c9dff099a25d94a87ac42d0a1adbe7f31ed31
Frank Winklmeier <fwinkl@cern>
2020-10-26 14:51:38 +0100

    Simulation/ISF: enable flake8 and fixes

--- Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/ISF_iGeant4Config.py
+++ Simulation/ISF/ISF_Geant4/ISF_Geant4Config/python/ISF_iGeant4Config.py
@@ -1,19 +1,11 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 """
 Tools configurations for ISF
 KG Tan, 17/06/2012
 """
 
-from __future__ import print_function
-
-from AthenaCommon.CfgGetter import getPrivateTool,getPrivateToolClone,getPublicTool,getPublicToolClone,\
-        getService,getServiceClone,getAlgorithm,getAlgorithmClone
-
-from AthenaCommon.Constants import *  # FATAL,ERROR etc.
-from AthenaCommon.SystemOfUnits import *
-from AthenaCommon.DetFlags import DetFlags
-
-from ISF_Config.ISF_jobProperties import ISF_Flags # IMPORTANT: Flags must be set before tools are retrieved
-
+from ISF_Config.ISF_jobProperties import ISF_Flags
+import sys
 
 cache = {}
 
@@ -28,18 +20,16 @@ def getIGeant4(**kwargs):
         i = cache['iGeant4']
         # if FullGeant4 specified, make sure returning cached version with FullGeant4 already in use
         if FullGeant4:
-            if (i.FullGeant4 == False) :
+            if (i.FullGeant4 is False) :
                 print ("asking for FullGeant4 but already configured Geant4 without specifying Full Geant4! Must specify FullGeant4 everywhere (or nowhere)!!!")
                 sys.exit()
     else:
-        from ISF_Config.ISF_jobProperties import ISF_Flags
         from iGeant4 import iGeant4
         i = iGeant4(**kwargs)
         cache['iGeant4'] = i
     return i
 
 def getGeant4SimSvc(name="ISF_Geant4SimSvc", **kwargs):
-    from ISF_Config.ISF_jobProperties import ISF_Flags
     kwargs.setdefault('FullGeant4'            , False                        )
     kwargs.setdefault('SimulationTool'        , 'ISFG4TransportTool'         )
     kwargs.setdefault('Identifier'            , "Geant4"                     )
