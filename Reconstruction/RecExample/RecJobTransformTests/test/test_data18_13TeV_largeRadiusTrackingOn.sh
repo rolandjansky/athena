58d59859e83b91e0ba240463d8c1abdc33e30439
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-10-28 11:20:13 +0100

    Update data18 LRT reco test to also set InDetFlags.storeSeparateLargeD0Container to true.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data18_13TeV_largeRadiusTrackingOn.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data18_13TeV_largeRadiusTrackingOn.sh
@@ -10,7 +10,7 @@
 # art-include: 21.9/Athena
 
 
-Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data18_13TeV/data18_13TeV.00348885.physics_Main.daq.RAW._lb0827._SFO-8._0002.data --preExec r2a:'from InDetRecExample.InDetJobProperties import InDetFlags; InDetFlags.doR3LargeD0.set_Value_and_Lock(True);' --maxEvents 300 --AMI=f926 --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
+Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data18_13TeV/data18_13TeV.00348885.physics_Main.daq.RAW._lb0827._SFO-8._0002.data --preExec r2a:'from InDetRecExample.InDetJobProperties import InDetFlags; InDetFlags.doR3LargeD0.set_Value_and_Lock(True);InDetFlags.storeSeparateLargeD0Container.set_Value_and_Lock(True);' --maxEvents 300 --AMI=f926 --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
 
 #Remember retval of transform as art result
 RES=$?
