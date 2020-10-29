a8f033fd277794b3775e09024d63aa4ba2047a23
Nicholas Styles <nicholas.styles@desy.de>
2020-10-29 10:01:06 +0000

    Merge branch 'improveVMMSpeed' into '21.9'
    Improve VMM CPU performance
    
    See merge request atlas/athena!37699
    
    (cherry picked from commit d9bf15156060e7825b7908dfb786443fd42e5f71)
    
    a41e6316 faster vmm shaper
    a2932329 make methods const
    0f10a543 remove left over variable
    a77b62b2 calculate pow() just once instead of always
    b5b3b096 adressing comments
    33741bf0 hasChargeAboveThreshold only start close to a first possible peak; introduce a...

--- MuonSpectrometer/MuonDigitization/MM_Digitization/MM_Digitization/VMM_Shaper.h
+++ MuonSpectrometer/MuonDigitization/MM_Digitization/MM_Digitization/VMM_Shaper.h
@@ -5,38 +5,38 @@
 #ifndef MM_DIGITIZATION_VMM_SHAPER_H
 #define MM_DIGITIZATION_VMM_SHAPER_H
 
-#include <TH1F.h>
 #include <vector>
 
 
 class VMM_Shaper{
  public:
-    VMM_Shaper(float peakTime);
+    VMM_Shaper(const float peakTime, const float lowerTimeWindow, const float upperTimeWindow);
     virtual ~VMM_Shaper() {}
 
     void initialize();
 
-    void vmmPeakResponse(const std::vector<float> effectiveCharge, const std::vector<float> electronsTime, const double electronicsThreshold, double &amplitudeFirstPeak, double &timeFirstPeak);
-    void vmmThresholdResponse(const std::vector<float> effectiveCharge, const std::vector<float> electronsTime, const double electronicsThreshold, double &amplitudeAtFirstPeak, double &timeAtThreshold);
+    void vmmPeakResponse(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double electronicsThreshold, double &amplitudeFirstPeak, double &timeFirstPeak) const;
+    void vmmThresholdResponse(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double electronicsThreshold, double &amplitudeAtFirstPeak, double &timeAtThreshold) const;
 
-    bool hasChargeAboveThreshold(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double &electronicsThreshold);
+    bool hasChargeAboveThreshold(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double electronicsThreshold) const;
 
 
  private:
      double m_peakTime;
-     double m_timeStep;
-     double m_maxTime;
+     double m_lowerTimeWindow, m_upperTimeWindow;
+     
+     double m_timeStep, m_inverseTimeStep;
 
-
+     double m_preCalculationVMMShaper; 
      // shaper parameters
      double m_a, m_pole0, m_re_pole1, m_im_pole1, m_pole1_square, m_k1_abs, m_argK1;
      int m_nBins;
 
      double m_peakTimeChargeScaling;
 
-     void vmmResponse(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, TH1F &response);
-     int findPeak(const TH1F &response, const double &electronicsThreshold);
-
+     double vmmResponse(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, double time) const;
+     double findPeak(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double electronicsThreshold) const;
+     bool aboveThresholdSimple(const std::vector<float> &effectiveCharge, const std::vector<float> &electronsTime, const double electronicsThreshold) const;
 
 
 };
