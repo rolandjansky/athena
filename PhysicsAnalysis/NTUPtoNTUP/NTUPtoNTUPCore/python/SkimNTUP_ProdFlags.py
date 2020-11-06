d72132eca8331f1ce1e79ca81ad7d46614459d65
Frank Winklmeier <fwinkl@cern>
2020-11-06 15:01:47 +0100

    JetN2N: delete obsolete NTUP reduction package

--- PhysicsAnalysis/NTUPtoNTUP/NTUPtoNTUPCore/python/SkimNTUP_ProdFlags.py
+++ PhysicsAnalysis/NTUPtoNTUP/NTUPtoNTUPCore/python/SkimNTUP_ProdFlags.py
@@ -106,7 +106,4 @@ listAllKnownSkimNTUP.append (prodFlags.WriteMySkimNTUP2)
 # Egamma D3PDs
 _importFlagsFromModule ('EgammaN2N.EgammaN2NProdFlags')
 
-# Jet D3PDs
-_importFlagsFromModule ('JetN2N.JetN2NProdFlags')
-
 print ('Known NTUPs are', listAllKnownSkimNTUP)
