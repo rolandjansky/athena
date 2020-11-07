7be5b2d89297fb0a4f4dfdc330dc13a75f14072f
scott snyder <sss@karma>
2020-11-07 12:23:49 -0500

    TrigEgammaMonitoring: Fix clang warnings.
    Missing override keywords.

--- Trigger/TrigMonitoring/TrigEgammaMonitoring/src/egammaMonitorElectronAlgorithm.h
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/src/egammaMonitorElectronAlgorithm.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef egammaMonitorElectronAlgorithm_H
@@ -33,7 +33,7 @@ class egammaMonitorElectronAlgorithm: public AthAlgorithm
   public:
 
     egammaMonitorElectronAlgorithm( const std::string& name, ISvcLocator* pSvcLocator );
-    StatusCode initialize();
+    virtual StatusCode initialize() override;
     virtual StatusCode execute() override final {
 
         return execute_r(Algorithm::getContext());
