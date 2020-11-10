3a94ca36e342b951021c0b1e37ced71c04e369e5
Frank Winklmeier <fwinkl@cern>
2020-11-10 12:14:13 +0100

    HeavyIonPhysTagTools: delete obsolete physics tag package

--- PhysicsAnalysis/HeavyIonPhys/HeavyIonPhysTagTools/share/HeavyIonPhysTagTool_jobOptions.py
+++
@@ -1,7 +0,0 @@
-include.block ("HeavyIonPhysTagTools/HeavyIonPhysTagTool_jobOptions.py")
-
-from HeavyIonPhysTagTools.HeavyIonPhysTagToolsConf import \
-     HeavyIonPhysTagTool as ConfiguredHeavyIonPhysTagTool
-HeavyIonPhysTagTool = ConfiguredHeavyIonPhysTagTool(
-    EtCut       = 6.0*GeV)
-ToolSvc += HeavyIonPhysTagTool
