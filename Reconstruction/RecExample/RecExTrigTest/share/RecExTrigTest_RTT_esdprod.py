868b979f28b6eed3620878ea41e525c92a92d1b3
Frank Winklmeier <fwinkl@cern>
2020-11-11 14:22:52 +0100

    RecExTrigTest: delete obsolete RTT testing package

--- Reconstruction/RecExample/RecExTrigTest/share/RecExTrigTest_RTT_esdprod.py
+++
@@ -1,9 +0,0 @@
-include ("RecExRecoTest/RecExRecoTest_RTT_common.py")
-
-rec.doTrigger=True
-
-# main jobOption
-include ("RecExCommon/rdotoesdprod.py")
-
-include ("RecExRecoTest/RecExRecoTest_RTT_common_postOptions.py")
-
