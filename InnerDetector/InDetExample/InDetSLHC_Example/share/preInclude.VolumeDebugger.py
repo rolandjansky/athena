70caaae96534cade0f6ef6d0eaa2c86be1e95c1f
Nicholas Styles <nicholas.styles@desy.de>
2020-11-11 16:24:50 +0100

    updates to VolumeDebugger options to make it more flexible and usable eventually in CI pipelines

--- InnerDetector/InDetExample/InDetSLHC_Example/share/preInclude.VolumeDebugger.py
+++ InnerDetector/InDetExample/InDetSLHC_Example/share/preInclude.VolumeDebugger.py
@@ -1,23 +1,34 @@
+target_volume = "IDET::IDET"
+
+import os
+if 'TARGETVOLUME' in os.environ:
+    target_volume = os.environ['TARGETVOLUME']
+    print("Running overlap check in "+target_volume)
+else:
+    print("Running overlap check in default volume ("+target_volume+")")
+
 
 from AthenaCommon.CfgGetter import getPublicTool
 from G4AtlasApps.SimFlags import simFlags
 simFlags.OptionalUserActionList.addAction('G4UA::VolumeDebuggerTool',['Run'])
 
-# Choose a region to check from the following: (dumpAll will give all possibilities)
-#------------------------
-#getPublicTool('G4UA::VolumeDebuggerTool', tryDefaultConfigurable=True).OutputPath=gdmlFile
-#getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='IDET::IDET'
-#getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='SCT::SCT'
-getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='Pixel::Pixel'
-#getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='Layer3_Sector1_Module_2_3_3_0'
-#getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='av_27654_impr_1_SCT::ABC_Chip_pv_3_ABC_Chip'
-#getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='dumpAll'
-#------------------------
-# Resolution is an integer (so don't try 1.e7)
-getPublicTool('G4UA::VolumeDebuggerTool').Resolution=10 # Default = 1000.   smaller is faster
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume=target_volume
+getPublicTool('G4UA::VolumeDebuggerTool').Resolution=1000 # Default = 1000.   smaller is faster, this is an int
 getPublicTool('G4UA::VolumeDebuggerTool').Tolerance=0.
-#getPublicTool('G4UA::VolumeDebuggerTool').Verbose=True
+getPublicTool('G4UA::VolumeDebuggerTool').Verbose=False
 getPublicTool('G4UA::VolumeDebuggerTool').RunGeoTest=True
 getPublicTool('G4UA::VolumeDebuggerTool').MaxCopiesToCheck=2.
 getPublicTool('G4UA::VolumeDebuggerTool').DumpGDML=True
 
+
+"""
+...some examples of different configuration possibilities
+getPublicTool('G4UA::VolumeDebuggerTool', tryDefaultConfigurable=True).OutputPath=gdmlFile
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='IDET::IDET'
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='SCT::SCT'
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='Pixel::Pixel'
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='Layer3_Sector1_Module_2_3_3_0'
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='av_27654_impr_1_SCT::ABC_Chip_pv_3_ABC_Chip'
+getPublicTool('G4UA::VolumeDebuggerTool').TargetVolume='dumpAll'
+
+"""
