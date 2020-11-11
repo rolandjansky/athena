626be71d1f24142ff1b528f60df840aa4f282fe4
Frank Winklmeier <fwinkl@cern>
2020-11-11 19:49:11 +0100

    ElectronPhotonTagTools: delete obsolete TAG maker
    TAG files are no longer being produced. Delete this obsolete e/gamma TAG
    maker and code referencing it.

--- PhysicsAnalysis/EventTag/EventTagAlgs/src/components/EventTagBuilder_entries.cxx
+++ PhysicsAnalysis/EventTag/EventTagAlgs/src/components/EventTagBuilder_entries.cxx
@@ -1,10 +1,8 @@
 #include "../AnalysisTagBuilder.h" 
 #include "../RawInfoSummaryTagBuilder.h"
-#include "../ElectronTagBuilder.h"
 #include "../GlobalEventTagBuilder.h"
 #include "../GlobalTriggerTagBuilder.h"
 #include "../MuonTagBuilder.h"
-#include "../PhotonTagBuilder.h"
 #include "../MissingETTagBuilder.h"
 #include "../ParticleJetTagBuilder.h"
 
@@ -13,11 +11,9 @@
 
 DECLARE_COMPONENT( AnalysisTagBuilder )
 DECLARE_COMPONENT( RawInfoSummaryTagBuilder )
-DECLARE_COMPONENT( ElectronTagBuilder )
 DECLARE_COMPONENT( GlobalEventTagBuilder )
 DECLARE_COMPONENT( GlobalTriggerTagBuilder )
 DECLARE_COMPONENT( MuonTagBuilder )
-DECLARE_COMPONENT( PhotonTagBuilder )
 DECLARE_COMPONENT( ParticleJetTagBuilder )
 DECLARE_COMPONENT( MissingETTagBuilder )
 
