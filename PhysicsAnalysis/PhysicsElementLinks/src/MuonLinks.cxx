308f574a2902e09e83f502bb4e77f9188b7e795c
Frank Winklmeier <fwinkl@cern>
2020-11-12 16:02:18 +0100

    PhysicsElementLinks: delete obsolete packages
    Delete obsolete "physics" element link packages:
    - PhysicsElementLinks
    - PhysicsElementLinksAthenaPool
    - PhysicsElementLinksTPCnv

--- PhysicsAnalysis/PhysicsElementLinks/src/MuonLinks.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "PhysicsElementLinks/MuonLinks.h"
-
-template class ParticleLinks<Analysis::MuonContainer>;
