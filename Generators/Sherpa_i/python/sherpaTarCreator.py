9edb707fd9771deb17fe85be96d7941203260d51
Frank Siegert <frank.siegert@cern.ch>
2020-11-16 14:38:06 +0100

    Bugfix for sherpaTarCreator.py

--- Generators/Sherpa_i/python/sherpaTarCreator.py
+++ Generators/Sherpa_i/python/sherpaTarCreator.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
-
 #!/usr/bin/env python
+
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 from sherpaTarCreator import main
 
