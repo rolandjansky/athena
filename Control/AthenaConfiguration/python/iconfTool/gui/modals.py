b5f2ce51c3c831f78a42b9723568c90e0ea1edf8
Michal Leszczynski <michal.leszczynski@cern.ch>
2020-11-17 15:59:17 +0000

    Update iconfTool

--- Control/AthenaConfiguration/python/iconfTool/gui/modals.py
+++ Control/AthenaConfiguration/python/iconfTool/gui/modals.py
@@ -1,34 +1,94 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 import curses.textpad
 
 
-class Modal(object):
-    def __init__(self, start_x, start_y, width, height):
-        self.width = width
-        self.height = height
-        self.start_x = start_x
-        self.start_y = start_y
+class Modal:
+    def __init__(
+        self, start_x: int, start_y: int, width: int, height: int
+    ) -> None:
+        self.width: int = width
+        self.height: int = height
+        self.start_x: int = start_x
+        self.start_y: int = start_y
         self.window = curses.newwin(height, width, start_y, start_x)
         self.window.box()
         self.refresh()
 
-    def refresh(self):
+    def refresh(self) -> None:
         self.window.refresh()
 
-    def destroy(self):
+    def destroy(self) -> None:
         del self.window
 
 
 class SearchModal(Modal):
-    def __init__(self, start_x, start_y, width, height):
-        super(SearchModal, self).__init__(start_x, start_y, width, height)
-        self.search_window = self.window.derwin(self.height - 2,
-                                                self.width - 2, 1, 1)
+    def __init__(
+        self, start_x: int, start_y: int, width: int, height: int
+    ) -> None:
+        super().__init__(start_x, start_y, width, height)
+        self.search_window = self.window.derwin(
+            self.height - 2, self.width - 2, 1, 1
+        )
         self.search = curses.textpad.Textbox(self.search_window)
 
-    def edit(self):
+    def edit(self) -> str:
         return self.search.edit().strip()
+
+
+class HelpModal(Modal):
+    def __init__(
+        self, start_x: int, start_y: int, width: int, height: int
+    ) -> None:
+        super().__init__(start_x, start_y, width, height)
+        self.help_window = self.window.derwin(
+            self.height - 2, self.width - 2, 1, 1
+        )
+
+        self.begin_y, self.begin_x = self.window.getparyx()
+        self.y, self.x = self.begin_y, self.begin_x
+        self.y += 2
+        self.x += 2
+
+    def add_line(self, text: str, blank_lines: int = 1) -> None:
+        self.window.addstr(self.y, self.x, text)
+        self.y += blank_lines
+
+    def show_help(self) -> None:
+        pass
+
+
+class SingleHelpModal(HelpModal):
+    def show_help(self) -> None:
+
+        self.add_line("Help", 2)
+
+        self.add_line("Arrows for navigation", 2)
+
+        self.add_line("h - display this help")
+        self.add_line("m - search")
+        self.add_line("space - mark/unmark")
+        self.add_line("r - refresh the screen", 2)
+
+        self.add_line("q - quit the program")
+
+        self.refresh()
+
+
+class DoubleHelpModal(HelpModal):
+    def show_help(self) -> None:
+
+        self.add_line("Help", 2)
+
+        self.add_line("Arrows for navigation", 2)
+
+        self.add_line("tab - change column")
+
+        self.add_line("h - display this help")
+        self.add_line("m - search")
+        self.add_line("space - mark/unmark")
+        self.add_line("r - refresh the screen", 2)
+
+        self.add_line("q - quit the program")
+
+        self.refresh()
--- Control/AthenaConfiguration/python/iconfTool/gui/pad.py
+++ Control/AthenaConfiguration/python/iconfTool/gui/pad.py
@@ -1,63 +1,81 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 import curses
+import logging
+import sys
+from typing import Dict, List, Set, Tuple
+
+from models.element import Element, GroupingElement
+from models.structure import ComponentsStructure
+
+logger = logging.getLogger(__name__)
+
 
 def report():
-    print("Can not init textGUI, probably wrong terminal. Try setting export TERM=xterm-256color")
-    import sys
+    logger.error(
+        "Can not init textGUI, probably wrong terminal. Try setting export TERM=xterm-256color"
+    )
     sys.exit(-1)
 
 
-class Pad(object):
-    def __init__(self, data_structure, width, height, start_x=0):
-        self.structure = data_structure
-        self.structure_list = self.structure.get_list()
-        self.root_items_names = {element.name for element in
-                                 self.structure_list}
-        self.width = width
-        self.start_x = start_x
-        self.MAX_PAD_HEIGHT = 32000
+class Pad:
+    def __init__(
+        self,
+        data_structure: ComponentsStructure,
+        width: int,
+        height: int,
+        start_x: int = 0,
+    ) -> None:
+        self.structure: ComponentsStructure = data_structure
+        self.structure_list: List[Element] = self.structure.get_list()
+        self.root_items_names: Set = {
+            element.name for element in self.structure_list
+        }
+        self.width: int = width
+        self.start_x: int = start_x
+        self.MAX_PAD_HEIGHT: int = 32000
         self.pad = curses.newpad(self.MAX_PAD_HEIGHT, width)
-        self.actual_offset = 0
-        self.pad_height = height - 1
+        self.actual_offset: int = 0
+        self.pad_height: int = height - 1
 
-        self.mark_character = ' '
-        self.styles = None
-        self.lines = []
-        self.search_text = ''
+        self.mark_character: str = " "
+        self.styles: Dict
+        self.lines: List[Element] = []
+        self.search_text: str = ""
         try:
             self.initialize_styles()
         except Exception:
             report()
         self.draw_all_structures()
-        self.actual_y = 0
+        self.actual_y: int = 0
         try:
             self.initialize_cursor()
         except Exception:
             report()
         self.refresh()
 
-    def initialize_styles(self):
-        curses.init_pair(1, curses.COLOR_BLACK,
-                         curses.COLOR_WHITE)  # Sets up color pair
+    def initialize_styles(self) -> None:
+        curses.init_pair(
+            1, curses.COLOR_BLACK, curses.COLOR_WHITE
+        )  # Sets up color pair
         highlighted = curses.color_pair(1)  # highlighted menu option
         normal = curses.A_NORMAL  # non-highlighted menu option
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
         marked = curses.color_pair(3)
-        self.styles = {'normal': normal, 'highlighted': highlighted,
-                       'marked': marked}
+        self.styles = {
+            "normal": normal,
+            "highlighted": highlighted,
+            "marked": marked,
+        }
 
-    def reload_data(self, data_structure):
+    def reload_data(self, data_structure: ComponentsStructure) -> None:
         self.structure = data_structure
         self.structure_list = self.structure.get_list()
-        self.root_items_names = {element.name for element in
-                                 self.structure_list}
-        self.filter(self.search_text)  # Filter also draws structures
+        self.root_items_names = {
+            element.name for element in self.structure_list
+        }
 
-    def initialize_cursor(self):
+    def initialize_cursor(self) -> None:
         # Highlight cursor on initial position
         if self.lines_empty():
             return
@@ -66,30 +84,47 @@ class Pad(object):
                 self.actual_y = 0
             element = self.lines[self.actual_y]
             curses.setsyx(self.actual_y, element.x_pos + 1)
-            self.pad.addch(curses.getsyx()[0], curses.getsyx()[1],
-                           element.get_mark_character(),
-                           self.styles['highlighted'])
+            self.pad.addch(
+                curses.getsyx()[0],
+                curses.getsyx()[1],
+                element.get_mark_character(),
+                self.styles["highlighted"],
+            )
             curses.curs_set(0)
         except IndexError:
             # Handle situation when self.lines list is empty
             pass
 
-    def draw_structure(self, structure, start_y):
+    def draw_structure(self, structure: Element, start_y: int) -> int:
         """start_y - starting vertical position where elements will be drawn"""
-        if start_y >= self.MAX_PAD_HEIGHT:
-            return
-        style = self.styles['marked' if structure.is_marked() else 'normal']
-        self.pad.addstr(start_y, structure.x_pos,
-                        '[{}] {}'.format(structure.get_mark_character(),
-                                         structure.get_name()), style)
+        style = self.styles["marked" if structure.is_marked() else "normal"]
+        logger.debug(
+            f"Adding str: x:{structure.x_pos}, y:{start_y}/{self.MAX_PAD_HEIGHT}"
+        )
+        try:
+            self.pad.addstr(
+                start_y,
+                structure.x_pos,
+                structure.get_line(),
+                style,
+            )
+        except Exception as e:
+            logger.error(
+                f"Error {e} loading str y:{start_y}, x: {structure.x_pos}"
+            )
         self.lines.insert(start_y, structure)
         start_y += 1
-        if structure.type == 'GROUP' and structure.show_children:
+        if isinstance(structure, GroupingElement) and structure.show_children:
             for child in structure.children:
-                start_y = self.draw_structure(child, start_y)
+                if start_y < self.MAX_PAD_HEIGHT:
+                    start_y = self.draw_structure(child, start_y)
+                else:
+                    logger.error(
+                        f"Too many structures: {start_y}. Max count: {self.MAX_PAD_HEIGHT}"
+                    )
         return start_y
 
-    def draw_all_structures(self):
+    def draw_all_structures(self, start_y=0) -> None:
         self.lines = []
         start_y = 0
         for i in self.structure_list:
@@ -98,20 +133,26 @@ class Pad(object):
             start_y = self.draw_structure(i, start_y)
         self.refresh()
 
-    def refresh(self):
-        self.pad.refresh(self.actual_offset, 0, 0, self.start_x,
-                         self.pad_height, self.width + self.start_x - 2)
+    def refresh(self) -> None:
+        self.pad.refresh(
+            self.actual_offset,
+            0,
+            0,
+            self.start_x,
+            self.pad_height,
+            self.width + self.start_x - 2,
+        )
         cursor_pos = curses.getsyx()
-        curses.setsyx(cursor_pos[0], cursor_pos[1] - self.start_x)
+        curses.setsyx(cursor_pos[0], int(cursor_pos[1] - self.start_x))
 
-    def lines_empty(self):
+    def lines_empty(self) -> bool:
         return len(self.lines) == 0
 
-    def clear(self):
+    def clear(self) -> None:
         self.pad.clear()
         self.refresh()
 
-    def redraw(self, reinit_cursor=False):
+    def redraw(self, reinit_cursor: bool = False) -> None:
         cursor_pos = curses.getsyx()
         self.pad.clear()
         self.draw_all_structures()
@@ -122,81 +163,95 @@ class Pad(object):
             self.actual_y = 0
             self.initialize_cursor()
         else:
+            logger.debug(
+                f"y: {self.actual_y}, lines length: {len(self.lines)}"
+            )
             element = self.lines[self.actual_y]
             self.pad.move(self.actual_y, cursor_pos[1])
-            self.pad.addch(self.actual_y, cursor_pos[1] - 1,
-                           element.get_mark_character(),
-                           self.styles['highlighted'])
+            self.pad.addch(
+                self.actual_y,
+                cursor_pos[1] - 1,
+                element.get_mark_character(),
+                self.styles["highlighted"],
+            )
         self.refresh()
 
-    def filter(self, text, strict=False):
-        """ If is strict then it leaves only elements which fits to text
-        If not then it leaves whole structures in which text appear somewhere
-        """
-        self.search_text = text if text else ''
+    def filter(self, text: str) -> None:
+        self.search_text = text if text else ""
+        self.collapse_all()
+
         if len(self.search_text) > 0:
-            if strict:
-                self.structure_list = self.structure.get_filtered_by_text(text)
-            else:
-                self.structure_list = filter(
-                    lambda el: el.check_filter(self.search_text),
-                    self.structure.get_list())
-        else:
-            self.structure_list = self.structure.get_list()
-        self.redraw(True)
+            self.structure.filter_by_text(text)
+            if self.structure.filter.comps_to_save:
+                self.expand_all(filter_comps=True)
+                for element in self.lines:
+                    if text in element.get_name() and not element.is_checked():
+                        element.set_as_checked()
+        self.redraw()
 
-    def show_diff_only(self):
-        self.structure_list = self.structure.get_filtered_by_mark()
-        self.redraw(True)
-
-    def show_all(self):
+    def show_all(self) -> None:
         self.structure_list = self.structure.get_list()
         self.redraw(True)
 
-    def get_character(self, y, x):
-        current_character = chr(self.pad.inch(y, x) & 0xFF)
+    def get_character(self, y: int, x: int) -> str:
+        current_character = chr(
+            int(self.pad.inch(y, x)) & 0xFF
+        )  # get only the first 8 bits of a number
         return current_character
 
-    def move_cursor(self, number):
+    def move_cursor(self, number: int) -> None:
         cursor_pos = self.hide_cursor()
         self.actual_y += number
         if self.actual_y < len(self.lines):
-            self.set_cursor(cursor_pos[0] + number,
-                            self.lines[self.actual_y].x_pos + 1)
+            self.set_cursor(
+                cursor_pos[0] + number, self.lines[self.actual_y].x_pos + 1
+            )
 
-    def hide_cursor(self):
+    def hide_cursor(self) -> Tuple[int, int]:
         cursor_pos = curses.getsyx()
         current_character = self.get_character(
-            cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1)
-        self.pad.addch(cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1,
-                       current_character,
-                       self.styles['normal'])
+            cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1
+        )
+        self.pad.addch(
+            cursor_pos[0] + self.actual_offset,
+            cursor_pos[1] - 1,
+            current_character,
+            self.styles["normal"],
+        )
         return cursor_pos
 
-    def show_cursor(self):
+    def show_cursor(self) -> None:
         cursor_pos = curses.getsyx()
         current_character = self.get_character(
-            cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1)
-        self.pad.addch(cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1,
-                       current_character,
-                       self.styles['highlighted'])
+            cursor_pos[0] + self.actual_offset, cursor_pos[1] - 1
+        )
+        self.pad.addch(
+            cursor_pos[0] + self.actual_offset,
+            cursor_pos[1] - 1,
+            current_character,
+            self.styles["highlighted"],
+        )
         self.refresh()
 
-    def set_cursor(self, y, x):
+    def set_cursor(self, y: int, x: int) -> None:
         curses.setsyx(y, x)
         cursor_pos = curses.getsyx()
         current_character = self.get_character(
-            cursor_pos[0] + self.actual_offset, cursor_pos[1])
-        self.pad.addch(cursor_pos[0] + self.actual_offset, cursor_pos[1],
-                       current_character,
-                       self.styles['highlighted'])
+            cursor_pos[0] + self.actual_offset, cursor_pos[1]
+        )
+        self.pad.addch(
+            cursor_pos[0] + self.actual_offset,
+            cursor_pos[1],
+            current_character,
+            self.styles["highlighted"],
+        )
         self.refresh()
 
-    def scroll(self, number):
+    def scroll(self, number: int) -> None:
         self.actual_offset += number
         self.refresh()
 
-    def scroll_to_element(self, new_y):
+    def scroll_to_element(self, new_y: int) -> None:
         self.actual_y = new_y
         self.hide_cursor()
         # Handle the situation when element is above current view
@@ -208,23 +263,24 @@ class Pad(object):
             self.actual_offset = new_y - 1
             if self.actual_offset + self.pad_height > len(self.lines):
                 self.actual_offset = len(self.lines) - self.pad_height
-        self.set_cursor(new_y - self.actual_offset,
-                        self.lines[new_y].x_pos + 1)
+        self.set_cursor(
+            new_y - self.actual_offset, self.lines[new_y].x_pos + 1
+        )
 
-    def get_index_by_name(self, name):
+    def get_index_by_name(self, name: str) -> int:
         for index, element in enumerate(self.lines):
             if element.parent is None and element.name == name:
                 return index
         return -1
 
-    def move_down(self):
+    def move_down(self) -> None:
         if self.actual_y < len(self.lines) - 1:
             self.move_cursor(1)
             cursor_pos = curses.getsyx()
             if cursor_pos[0] > self.pad_height - 4:
                 self.scroll(1)
 
-    def move_up(self):
+    def move_up(self) -> None:
         if len(self.lines) == 0:
             return
         if self.actual_y >= 1:
@@ -233,23 +289,59 @@ class Pad(object):
             if cursor_pos[0] < 4 and self.actual_offset > 0:
                 self.scroll(-1)
 
-    def expand(self):
+    def expand(self) -> None:
         if self.actual_y >= len(self.lines):
             return
         element = self.lines[self.actual_y]
-        if element.type == 'GROUP':
+        if isinstance(element, GroupingElement):
+            self.structure.replace_references(element)
             element.show_children = True
             self.redraw()
 
-    def collapse(self):
+    def expand_all(self, filter_comps=False) -> None:
+        for element in self.structure_list:
+            self.expand_children(element, filter_comps)
+        self.structure.filter.comps_to_save = []
+        self.redraw()
+
+    def expand_children(self, element: Element, filter_comps) -> None:
+        if (
+            filter_comps
+            and element.get_reference_name()
+            not in self.structure.filter.comps_to_save
+            and element.get_reference_name() in self.structure.roots_dict
+        ):
+            logger.debug(
+                f"Skipped expanding for: {element.get_reference_name()}"
+            )
+            return
+        if isinstance(element, GroupingElement):
+            self.structure.replace_references(element)
+            element.show_children = True
+            for child in element.children:
+                self.expand_children(child, filter_comps)
+
+    def collapse(self) -> None:
         if self.actual_y >= len(self.lines):
             return
         element = self.lines[self.actual_y]
-        if element.type == 'GROUP':
+        if isinstance(element, GroupingElement):
             element.show_children = False
             self.redraw()
 
-    def set_as_checked(self):
+    def collapse_all(self):
+        for element in self.structure_list:
+            self.collapse_children(element)
+        self.actual_y = 0
+        self.redraw()
+
+    def collapse_children(self, element: Element) -> None:
+        if isinstance(element, GroupingElement):
+            element.show_children = False
+            for child in element.children:
+                self.collapse_children(child)
+
+    def set_as_checked(self) -> None:
         element = self.lines[self.actual_y]
         if element.checked:
             element.set_as_unchecked()
@@ -259,27 +351,7 @@ class Pad(object):
             self.structure.add_to_checked(element)
         self.redraw()
 
-    def go_to_reference(self):
-        if self.actual_y >= len(self.lines):
-            return
-        element = self.lines[self.actual_y]
-        if not element.type == 'SINGLE':
-            return
-        reference_name = element.get_reference_name()
-        if reference_name not in self.root_items_names:
-            return
-        # Cancel eventual search result
-        self.filter(None)
-        index = self.get_index_by_name(reference_name)
-        if index != -1:
-            self.scroll_to_element(index)
-        else:
-            # Scroll to first element if name was not found
-            # (should never happen as the name is checked
-            # in root_items_names first)
-            self.scroll_to_element(0)
-
-    def handle_event(self, event):
+    def handle_event(self, event: int) -> None:
         if event == curses.KEY_DOWN:
             self.move_down()
         elif event == curses.KEY_UP:
@@ -288,7 +360,5 @@ class Pad(object):
             self.expand()
         elif event == curses.KEY_LEFT:
             self.collapse()
-        elif event == ord(' '):
+        elif event == ord(" "):
             self.set_as_checked()
-        elif event == ord('g'):
-            self.go_to_reference()
