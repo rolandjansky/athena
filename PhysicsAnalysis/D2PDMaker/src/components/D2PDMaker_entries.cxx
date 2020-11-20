c811b52f40376a329fd6e699731566aef0fb0101
Frank Winklmeier <fwinkl@cern>
2020-11-20 14:50:23 +0100

    D2PDMaker: Package cleanup
    - delete unused `CheckD2PD` and `CreateControlPlots`
    - delete all obsolete job options in `share/`
    - remove obsolete flags in `D2PDFlags.py`
    - delete unused `SUSYFilters.py`
    - delete obsolete GRL from 2010
    - cleanup cmake configuaration and enable flake8
    
    There are some other unused "Selector" classes but kept those for
    consistency with those that are still used in `AODSelect`.

--- PhysicsAnalysis/D2PDMaker/src/components/D2PDMaker_entries.cxx
+++ PhysicsAnalysis/D2PDMaker/src/components/D2PDMaker_entries.cxx
@@ -8,8 +8,6 @@
 #include "D2PDMaker/D2PDTrackParticleSelector.h"
 #include "D2PDMaker/D2PDVertexSelector.h"
 #include "D2PDMaker/D2PDTruthParticleSelector.h"
-#include "D2PDMaker/CheckD2PD.h"
-#include "D2PDMaker/CreateControlPlots.h"
 #include "D2PDMaker/FilterTool.h"
 
 DECLARE_COMPONENT( D2PDINavigable4MomentumSelector )
@@ -22,7 +20,4 @@ DECLARE_COMPONENT( D2PDJetSelector )
 DECLARE_COMPONENT( D2PDTrackParticleSelector )
 DECLARE_COMPONENT( D2PDVertexSelector )
 DECLARE_COMPONENT( D2PDTruthParticleSelector )
-DECLARE_COMPONENT( CheckD2PD )
-DECLARE_COMPONENT( CreateControlPlots )
 DECLARE_COMPONENT( FilterTool )
-
