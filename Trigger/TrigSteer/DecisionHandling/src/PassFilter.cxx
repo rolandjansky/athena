03fc475f3e190c4e351a215ae8f566e9732fea90
Rafal Bielski <rafal.bielski@cern.ch>
2020-11-23 18:57:29 +0100

    DecisionHandling: Catch soft timeout in Filter algorithms

--- Trigger/TrigSteer/DecisionHandling/src/PassFilter.cxx
+++ Trigger/TrigSteer/DecisionHandling/src/PassFilter.cxx
@@ -3,6 +3,8 @@
 */
 
 #include "PassFilter.h"
+#include "AthenaKernel/Timeout.h"
+#include "AthenaKernel/AthStatusCode.h"
 
 PassFilter::PassFilter(const std::string& name, ISvcLocator* pSvcLocator) :
   AthReentrantAlgorithm(name, pSvcLocator)
@@ -23,8 +25,12 @@ StatusCode PassFilter::finalize()
   return StatusCode::SUCCESS;
 }
 
-StatusCode PassFilter::execute(const EventContext&) const
+StatusCode PassFilter::execute(const EventContext& ctx) const
 {
+  if (Athena::Timeout::instance(ctx).reached()) {
+    ATH_MSG_ERROR("Timeout reached before " << name());
+    return Athena::Status::TIMEOUT;
+  }
   return StatusCode::SUCCESS;
 }
 
