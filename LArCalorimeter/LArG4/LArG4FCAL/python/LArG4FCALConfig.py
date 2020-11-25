127d35df0bbbb586a2dbe5d7e4cc1380a629547b
Frank Winklmeier <fwinkl@cern>
2020-11-25 12:32:14 +0100

    LArG4FCAL: cmake cleanup, enable flake8
    Remove unused external dependencies, update link dependencies and enable
    flake8.

--- LArCalorimeter/LArG4/LArG4FCAL/python/LArG4FCALConfig.py
+++ LArCalorimeter/LArG4/LArG4FCAL/python/LArG4FCALConfig.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon import CfgMgr
 from AthenaCommon.SystemOfUnits import mm,deg,ns
@@ -10,37 +10,37 @@ def getFCALCalculatorBase(name="FCALCalculatorBase", **kwargs):
 
 
 def getFCAL1Calculator(name="FCAL1Calculator", **kwargs):
-    kwargs.setdefault("FCALSampling",1);
+    kwargs.setdefault("FCALSampling",1)
     return getFCALCalculatorBase(name, **kwargs)
 
 
 def getFCAL2Calculator(name="FCAL2Calculator", **kwargs):
-    kwargs.setdefault("FCALSampling",2);
+    kwargs.setdefault("FCALSampling",2)
     return getFCALCalculatorBase(name, **kwargs)
 
 
 def getFCAL3Calculator(name="FCAL3Calculator", **kwargs):
-    kwargs.setdefault("FCALSampling",3);
+    kwargs.setdefault("FCALSampling",3)
     return getFCALCalculatorBase(name, **kwargs)
 
 
 def getFCAL1CalibCalculator(name="FCAL1CalibCalculator", **kwargs):
-    kwargs.setdefault("FCALdeltaX",7.5*mm);
-    kwargs.setdefault("FCALdeltaY",7.5*mm*math.sin(60*deg));
-    kwargs.setdefault("FCALSampling",1);
+    kwargs.setdefault("FCALdeltaX",7.5*mm)
+    kwargs.setdefault("FCALdeltaY",7.5*mm*math.sin(60*deg))
+    kwargs.setdefault("FCALSampling",1)
     return CfgMgr.LArG4__FCAL__LArFCALCalibCalculatorBase(name, **kwargs)
 
 
 def getFCAL2CalibCalculator(name="FCAL2CalibCalculator", **kwargs):
-    kwargs.setdefault("FCALdeltaX",8.179*mm);
-    kwargs.setdefault("FCALdeltaY",8.179*mm*math.sin(60*deg));
-    kwargs.setdefault("FCALSampling",2);
+    kwargs.setdefault("FCALdeltaX",8.179*mm)
+    kwargs.setdefault("FCALdeltaY",8.179*mm*math.sin(60*deg))
+    kwargs.setdefault("FCALSampling",2)
     return CfgMgr.LArG4__FCAL__LArFCALCalibCalculatorBase(name, **kwargs)
 
 
 def getFCAL3CalibCalculator(name="FCAL3CalibCalculator", **kwargs):
-    kwargs.setdefault("FCALdeltaX",9.0*mm);
-    kwargs.setdefault("FCALdeltaY",9.0*mm*math.sin(60*deg));
-    kwargs.setdefault("FCALSampling",3);
+    kwargs.setdefault("FCALdeltaX",9.0*mm)
+    kwargs.setdefault("FCALdeltaY",9.0*mm*math.sin(60*deg))
+    kwargs.setdefault("FCALSampling",3)
     return CfgMgr.LArG4__FCAL__LArFCALCalibCalculatorBase(name, **kwargs)
 
