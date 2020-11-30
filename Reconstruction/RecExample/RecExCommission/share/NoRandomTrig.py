38298fc7b79c330784cce0770f89cc796ea8e098
Frank Winklmeier <fwinkl@cern>
2020-11-30 15:20:24 +0100

    RecExCommission: delete package
    Delete the obsolete `Reconstruction/RecExample/RecExCommission` package.
    Also delete files in other packages that are related and obsolete. The
    `rec.Commissioning` flag remains as that seems to be set to `True` in
    various places. Even for those cases, there should be no change in job
    configuration.
    
    Move only remaining `MinimalCommissioningSetup.py` to RecExCond package.

--- Reconstruction/RecExample/RecExCommission/share/NoRandomTrig.py
+++
@@ -1,5 +0,0 @@
-#Random trigger off for reproducibility
-from TrigSteering.TrigSteeringConfig import ScalerSvc
-ScalerSvc = ScalerSvc()
-ScalerSvc.DefaultType = "HLT::PeriodicScaler"
-
--- Reconstruction/RecExample/RecExCommission/share/RecExCommission_SingleBeamtopOptions.py
+++
@@ -1,5 +0,0 @@
-from AthenaCommon.BeamFlags import jobproperties
-jobproperties.Beam.beamType.set_Value_and_Lock("singlebeam")
-
-include("RecExCommon/RecExCommon_topOptions.py")
-
