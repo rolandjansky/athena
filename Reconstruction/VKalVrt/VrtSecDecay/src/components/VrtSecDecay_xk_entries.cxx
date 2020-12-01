4227bff2f88e5d95c38abb3726c2c391e7d0763b
Matthew Gignac <matthew.gignac@cern.ch>
2020-12-01 19:14:42 +0000

    adding VrtSecDecay alg; modifying SUSY6 to run vertexing algoirthm during AOD->DAOD; decorating tracks to know whether vertex contraint came from PV or not

---
+++ Reconstruction/VKalVrt/VrtSecDecay/src/components/VrtSecDecay_xk_entries.cxx
@@ -0,0 +1,17 @@
+//
+// Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+//
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "VrtSecDecay/VrtSecDecay.h"
+
+DECLARE_NAMESPACE_ALGORITHM_FACTORY( VKalVrtAthena, VrtSecDecay )
+DECLARE_FACTORY_ENTRIES( VrtSecDecay )
+{
+    DECLARE_NAMESPACE_ALGORITHM( VKalVrtAthena, VrtSecDecay )
+}
+
+
+
+
+
