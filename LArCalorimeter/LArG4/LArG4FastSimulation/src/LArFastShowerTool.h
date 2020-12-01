e954a1a55966ab43c29f67947937223ed1763813
John Chapman <jchapman@cern.ch>
2020-12-01 18:08:24 +0100

    Remove unused forward declaration of G4FastSimulationModel class
    The correct class name is `G4VFastSimulationModel` and is included
    via `FastSimulationBase.h`.

--- LArCalorimeter/LArG4/LArG4FastSimulation/src/LArFastShowerTool.h
+++ LArCalorimeter/LArG4/LArG4FastSimulation/src/LArFastShowerTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef LARG4FASTSIMULATION_LARFASTSHOWERTOOL_H
@@ -15,8 +15,6 @@
 
 // STL headers
 #include <string>
-
-class G4FastSimulationModel;
 
 /// NEEDS DOCUMENTATION
 class LArFastShowerTool: public FastSimulationBase
