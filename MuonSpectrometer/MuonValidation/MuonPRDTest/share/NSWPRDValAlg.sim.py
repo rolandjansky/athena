775ba124ce13d262d1ae44e35e4629aee5db6be8
Nicolas Köhler <nicolas.koehler@cern.ch>
2020-12-02 11:21:26 +0100

    add jobOptions for sim MuonPRDTest

--- MuonSpectrometer/MuonValidation/MuonPRDTest/share/NSWPRDValAlg.sim.py
+++ MuonSpectrometer/MuonValidation/MuonPRDTest/share/NSWPRDValAlg.sim.py
@@ -1,9 +1,8 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
 # jobOptions to activate the dump of the NSWPRDValAlg nTuple
-# This file can be used with Reco_tf by specifying --postInclude MuonPRDTest/NSWPRDValAlg.reco.py
-# It dumps Truth, MuEntry and Hits, Digits, SDOs, RDOs and PRDs for MM and sTGC
-# It should only be used with Reco since the alg crashes in the absence of PRDs
+# This file can be used with Sim_tf by specifying --postInclude MuonPRDTest/NSWPRDValAlg.sim.py
+# It dumps Truth, MuEntry and Hits, for MDT, RPC, MM and sTGC
 
 #-----------------------------------------------------------------------------
 # Algorithms
@@ -12,27 +11,20 @@ from AthenaCommon.AlgSequence import AlgSequence
 job = AlgSequence()
 from MuonPRDTest.MuonPRDTestConf import *
 
-job+=NSWPRDValAlg('NSWPRDValAlg', OutputLevel = DEBUG)
+job+=NSWPRDValAlg('NSWPRDValAlg', OutputLevel = WARNING)
 NSWPRDValAlg.OutputLevel = INFO
 NSWPRDValAlg.doTruth = True
 
 NSWPRDValAlg.doMuEntry = True
 
+NSWPRDValAlg.doMDTHit= True
+NSWPRDValAlg.doRPCHit= True
 NSWPRDValAlg.doMMHit = True
-NSWPRDValAlg.doMMDigit = True
-NSWPRDValAlg.doMMRDO = True
-NSWPRDValAlg.doMMPRD = True
-NSWPRDValAlg.doMMFastDigit = False
-
 NSWPRDValAlg.doSTGCHit = True
-NSWPRDValAlg.doSTGCDigit = True
-NSWPRDValAlg.doSTGCRDO = True
-NSWPRDValAlg.doSTGCPRD = True
-NSWPRDValAlg.doSTGCFastDigit = False
 
 #-----------------------------------------------------------------------------
 # save ROOT histograms and Tuple
 #-----------------------------------------------------------------------------
 from GaudiSvc.GaudiSvcConf import THistSvc
 ServiceMgr += THistSvc()
-ServiceMgr.THistSvc.Output = [ "NSWPRDValAlg DATAFILE='NSWPRDValAlg.reco.ntuple.root' OPT='RECREATE'" ]
+ServiceMgr.THistSvc.Output = [ "NSWPRDValAlg DATAFILE='NSWPRDValAlg.sim.ntuple.root' OPT='RECREATE'" ]
