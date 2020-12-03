804d282563df6477f5bbda674a0edb6a87608bc1
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2020-12-03 12:48:24 +0100

    Update usage of xAODDigest such that the resulting log file has headers showing what the printed data refer to.

--- Reconstruction/RecExample/RecExRecoTest/test/test_recexreco_art_q431_compareFiveThreads.sh
+++ Reconstruction/RecExample/RecExRecoTest/test/test_recexreco_art_q431_compareFiveThreads.sh
@@ -28,7 +28,9 @@ then
  echo "Compare two directories"
  art.py compare ref --entries 10 --mode=semi-detailed --order-trees --diff-root . ../runOne/ | tee diffFiveThreads.log
  rcDiff=${PIPESTATUS[0]}
- diff digestTwo.log ../runOne/digestOne.log | tee digestDiffOneTwo.log
+ diff digestTwo.log ../runOne/digestOne.log | tee temp.log
+ grep nTopo digestTwo.log | sed 's/^/   /g' >& grep.log
+ cat grep.log temp.log >& digestDiffOneTwo.log 
  echo "art-result: $rcDiff Diff-Serial-OneThread"
 fi
 
