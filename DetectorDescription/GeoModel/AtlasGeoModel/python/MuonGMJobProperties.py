e40ce6ea19cf7d5a01ed685da8238b00bef82063
Frank Winklmeier <fwinkl@cern>
2020-12-07 13:03:27 +0100

    Refactor code to read GeoDB
    Move detector-specific code to read the geometry database into
    standalone "[Det]GeoDB.py" modules. Use this code both in the old
    jobproperties as well as in the new-style `AutoConfigFlags` to avoid the
    copy&paste of the same code.
    
    Also fix the caching of `AutoConfigFlags.GetDetDescrInfo` to make sure the
    geometry DB is only queried once per geometry tag.

--- DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGMJobProperties.py
+++ DetectorDescription/GeoModel/AtlasGeoModel/python/MuonGMJobProperties.py
@@ -1,44 +1,13 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
+from AthenaCommon.GlobalFlags import globalflags
 from AthenaCommon.JobProperties import JobProperty, JobPropertyContainer, jobproperties
-from AtlasGeoModel.CommonGMJobProperties import CommonGMFlags
+from AtlasGeoModel.AtlasGeoDBInterface import AtlasGeoDBInterface
+from MuonGeoModel import MuonGeoDB
 
 # -------------------------------------------------------------------------------------
-#  Muon geometry flags initialization
+#  Muon geometry flags
 # -------------------------------------------------------------------------------------
-class MuonGMFlags(CommonGMFlags, object):
-
-    def __init__(self, geoTag="none"):
-        super(MuonGMFlags, self).__init__()
-
-    def InitializeGeometryParameters(self):
-        dbId,dbSwitches,dbParam = self.dbGeomCursor.GetCurrentLeafContent("MuonSwitches")
-        _layoutName=None
-        _hasCsc=True
-        _hasStgc=True
-        _hasMM=True
-        if len(dbId)>0:
-            key=dbId[0] 
-            if "LAYOUTNAME" in dbParam: _layoutName = dbSwitches[key][dbParam.index("LAYOUTNAME")]
-            if "HASCSC" in dbParam: _hasCsc = dbSwitches[key][dbParam.index("HASCSC")]
-            if "HASSTGC" in dbParam: _hasStgc = dbSwitches[key][dbParam.index("HASSTGC")]
-            if "HASMM" in dbParam: _hasMM = dbSwitches[key][dbParam.index("HASMM")]
-            
-        self.__dict__["Layout"] = (_layoutName if _layoutName else "UNDEFINED")
-        if _hasCsc == 0: self.__dict__["HasCSC"] = False
-        else: self.__dict__["HasCSC"] = True
-        if _hasStgc == 0: self.__dict__["HasSTGC"] = False
-        else: self.__dict__["HasSTGC"] = True
-        if _hasMM == 0: self.__dict__["HasMM"] = False
-        else: self.__dict__["HasMM"] = True
-
-    def dump(self):
-        print ("MuonGMFlags:")
-        print ("Layout      = ",self.__dict__["Layout"])
-        print ("HasCSC      = ",self.__dict__["HasCSC"])
-        print ("HasSTGC     = ",self.__dict__["HasSTGC"])
-        print ("HasMM       = ",self.__dict__["HasMM"])
-
 
 class GeoLayout(JobProperty):
      """ geometry layout """
@@ -70,14 +39,18 @@ class MuonGeometryFlags_JobProperties(JobPropertyContainer):
     def __init__(self, context=""):
         JobPropertyContainer.__init__(self,context)
         return
-    def setupValuesFromDB(self,geoTagName="none"):
-        MuonGeoFlags = MuonGMFlags(geoTagName)
-        self.GeoLayout.set_Value_and_Lock(MuonGeoFlags.getValue("Layout"))
-        self.hasCSC.set_Value_and_Lock(MuonGeoFlags.getValue("HasCSC"))
-        self.hasSTGC.set_Value_and_Lock(MuonGeoFlags.getValue("HasSTGC"))
-        self.hasMM.set_Value_and_Lock(MuonGeoFlags.getValue("HasMM"))
+    def setupValuesFromDB(self,geoTagName=None):
 
-    def reset(self,geoTagName="none"):
+        dbGeomCursor = AtlasGeoDBInterface(geoTagName or globalflags.DetDescrVersion())
+        dbGeomCursor.ConnectAndBrowseGeoDB()
+        params = MuonGeoDB.InitializeGeometryParameters(dbGeomCursor)
+
+        self.GeoLayout.set_Value_and_Lock(params["Layout"])
+        self.hasCSC.set_Value_and_Lock(params["HasCSC"])
+        self.hasSTGC.set_Value_and_Lock(params["HasSTGC"])
+        self.hasMM.set_Value_and_Lock(params["HasMM"])
+
+    def reset(self):
         self.GeoLayout.unlock()
         self.hasCSC.unlock()
         self.hasSTGC.unlock()
