3747e1c9e93f9f4d59a61b45487c4f8c2720ee44
Tadej Novak <tadej.novak@cern.ch>
2020-12-07 19:19:30 +0100

    PyJobTransforms: Fix input/output file parsing from AMI

--- Tools/PyJobTransforms/python/trfAMI.py
+++ Tools/PyJobTransforms/python/trfAMI.py
@@ -356,13 +356,11 @@ def getTrfConfigFromPANDA(tag):
             if arg.lstrip('-').startswith('input') and arg.endswith('File'):
                 value=physics.pop(arg)
                 msg.debug("Found input file argument %s=%s.", arg, value ) 
-                fmt=arg.lstrip('-').replace('input','').replace('File','')
-                trf.inFiles[arg]=getInputFileName(arg)
+                trf.inFiles[arg]=value
             elif arg.lstrip('-').startswith('output') and arg.endswith('File'):
                 value=physics.pop(arg)
                 msg.debug("Found output file argument %s=%s.", arg, value )
-                fmt=arg.lstrip('-').replace('output','').replace('File','')
-                trf.outFiles[arg]=getOutputFileName(fmt)
+                trf.outFiles[arg]=value
 
         msg.debug("Checking for not set arguments...")
         for arg,value in listitems(physics):
@@ -490,13 +488,11 @@ def getTrfConfigFromAMI(tag, suppressNonJobOptions = True):
                 if arg.lstrip('-').startswith('input') and arg.endswith('File'):
                     value = physics.pop(arg)
                     msg.debug("Found input file argument %s=%s.", arg, value)
-                    fmt = arg.lstrip('-').replace('input', '').replace('File', '')
-                    trf.inFiles[arg] = getInputFileName(arg)
+                    trf.inFiles[arg] = value
                 elif arg.lstrip('-').startswith('output') and arg.endswith('File'):
                     value = physics.pop(arg)
                     msg.debug("Found output file argument %s=%s.", arg, value)
-                    fmt = arg.lstrip('-').replace('output', '').replace('File', '')
-                    trf.outFiles[arg] = getOutputFileName(fmt)
+                    trf.outFiles[arg] = value
 
             msg.debug("Checking for not set arguments...")
             for arg, value in listitems(physics):
