c30b5b0427a577a0b0679e7776105f2c140e316c
Soshi Tsuno <soshi.tsuno@cern.ch>
2020-12-11 14:41:31 +0000

    Radiation damage parameter in conditions data (ATLASSIM-4977)

--- InnerDetector/InDetDigitization/PixelDigitization/src/SensorSimTool.h
+++ InnerDetector/InDetDigitization/PixelDigitization/src/SensorSimTool.h
@@ -23,6 +23,9 @@
 #include "PixelReadoutGeometry/PixelModuleDesign.h"
 #include "SiPropertiesTool/ISiPropertiesTool.h"
 
+#include "PixelConditionsData/PixelModuleData.h"
+#include "StoreGate/ReadCondHandleKey.h"
+
 static const InterfaceID IID_ISensorSimTool("SensorSimTool", 1, 0);
 
 class SensorSimTool:public AthAlgTool,virtual public IAlgTool {
@@ -39,6 +42,7 @@ class SensorSimTool:public AthAlgTool,virtual public IAlgTool {
     virtual StatusCode initialize() {
       ATH_CHECK(AthAlgTool::initialize()); 
       ATH_CHECK(m_siPropertiesTool.retrieve());
+      ATH_CHECK(m_moduleDataKey.initialize());
       return StatusCode::SUCCESS;
     }
 
@@ -52,7 +56,12 @@ class SensorSimTool:public AthAlgTool,virtual public IAlgTool {
     SensorSimTool();
 
   protected:
-    ToolHandle<ISiPropertiesTool>   m_siPropertiesTool{this, "SiPropertiesTool", "SiPropertiesTool", "Tool to retrieve SiProperties"};
+    ToolHandle<ISiPropertiesTool> m_siPropertiesTool
+    {this, "SiPropertiesTool", "SiPropertiesTool", "Tool to retrieve SiProperties"};
+
+    SG::ReadCondHandleKey<PixelModuleData> m_moduleDataKey
+    {this, "PixelModuleData", "PixelModuleData", "Pixel module data"};
+
 };
 
 #endif // PIXELDIGITIZATION_SensorSimTool_H
