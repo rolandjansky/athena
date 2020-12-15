bcbc8d40134df8dca2ecceff01a503d9b79b04d1
Hannes Mildner <hannes.mildner@cern.ch>
2020-12-15 00:20:12 +0000

    MGutils: fix for systematics name check

--- Generators/MadGraphControl/python/MadGraphUtils.py
+++ Generators/MadGraphControl/python/MadGraphUtils.py
@@ -1477,8 +1477,8 @@ def get_expected_systematic_names(syst_setting):
     if syst_setting is None or not 'central_pdf' in syst_setting:
         mglog.warning("Systematics have not been defined via base fragment or 'MADGRAPH_PDFSETTING', cannot check for expected weights")
         return []
-    names+=[MadGraphSystematicsUtils.SYSTEMATICS_WEIGHT_INFO%{'mur':1.0,'muf':1.0,'pdf':syst_setting['central_pdf']}]
     if 'pdf_variations' in syst_setting and isinstance(syst_setting['pdf_variations'],list):
+        names+=[MadGraphSystematicsUtils.SYSTEMATICS_WEIGHT_INFO%{'mur':1.0,'muf':1.0,'pdf':syst_setting['central_pdf']}]
         for pdf in syst_setting['pdf_variations']:
             names+=[MadGraphSystematicsUtils.SYSTEMATICS_WEIGHT_INFO%{'mur':1.0,'muf':1.0,'pdf':pdf+1}]
     if 'alternative_pdfs' in syst_setting and isinstance(syst_setting['alternative_pdfs'],list):
