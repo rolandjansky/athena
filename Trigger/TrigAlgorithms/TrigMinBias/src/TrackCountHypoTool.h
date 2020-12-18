ca2760e6264419179a458f66e94b37df83296958
Tomasz Bold <tomasz.bold@gmail.com>
2020-12-18 12:32:02 +0000

    Small fixes and renaming in min bias algs & config

--- Trigger/TrigAlgorithms/TrigMinBias/src/TrackCountHypoTool.h
+++ Trigger/TrigAlgorithms/TrigMinBias/src/TrackCountHypoTool.h
@@ -31,13 +31,11 @@ public:
   private:
 
     HLT::Identifier m_decisionId;
-    Gaudi::Property<float> m_min_pt{this, "min_pt",0.1, "Accept events with momentum higher than this limit in MeV"};
-    Gaudi::Property<float> m_max_z0{this, "max_z0",100, "Accept events with absolute value of vertex lower than this limit in mm"};
-    Gaudi::Property<float> m_required_ntrks{this, "required_ntrks", 1, "Accept events with minimum of this number of tracks"};
-    Gaudi::Property<float> m_max_required_ntrks{this, "max_required_ntrks", -1, "Accept events with maximum of this number of tracks"};
+    Gaudi::Property<float> m_minPt{this, "minPt",0.1, "Accept events with momentum higher than this limit in MeV"};
+    Gaudi::Property<float> m_maxZ0{this, "maxZ0",100, "Accept events with absolute value of vertex lower than this limit in mm"};
+    Gaudi::Property<float> m_minNtrks{this, "minNtrks", 1, "Accept events with minimum of this number of tracks, -1 means this cut is disabled"};
+    Gaudi::Property<float> m_maxNtrks{this, "maxNtrks", -1, "Accept events with maximum of this number of tracks, -1 means this cut is disabled"};
     Gaudi::Property<bool> m_acceptAll{this, "acceptAll", false, "Accept all events"};
-    Gaudi::Property<bool> m_cutMinAndMax{this, "cutMinAndMax", false, "take events within the given higher and lower limt/cut"};
-    Gaudi::Property<bool> m_logicAnd{this, "TriggerTypeAnd", true, "And/Or Logic"};
   };
 
   #endif //> !TRIGT2MINBIAS_TRACKCOUNTHYPOTOOL_H
