cb7db94c1523ab6886093c4d6f9d094203103124
Edward Moyse <edward.moyse@cern.ch>
2020-12-21 12:57:25 +0000

    Revert "Remove extraneous lines from MuonRec_jobOptions.py"
    This reverts commit 4f37164b43037984920210a057fe7333f79f3677

--- MuonSpectrometer/MuonReconstruction/MuonRecExample/share/MuonRec_jobOptions.py
+++ MuonSpectrometer/MuonReconstruction/MuonRecExample/share/MuonRec_jobOptions.py
@@ -193,6 +193,9 @@ elif muonRecFlags.doCalib():
     from MuonRecExample import MuonAlignConfig
     from MuonCnvExample import setupMuonCalib
     setupMuonCalib()
+else:
+    logMuon.warning("Loading %s but not setting up any MuonCalibration or Ntuple" % __name__ )
+
 
 #--------------------------------------------------------------------------
 # Evaluate tracking performance
