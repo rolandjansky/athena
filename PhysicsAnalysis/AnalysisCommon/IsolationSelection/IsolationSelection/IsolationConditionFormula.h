0cdd97b01dcf52321bb2b15aa19e443dbb0eab77
Fabio Cardillo <fabio.cardillo@cern.ch>
2020-12-29 15:40:29 +0000

    Added new PLIV WPs to IsolationSelectionTool

--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/IsolationSelection/IsolationConditionFormula.h
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/IsolationSelection/IsolationConditionFormula.h
@@ -19,6 +19,7 @@ namespace CP {
         public:
             IsolationConditionFormula(std::string name, xAOD::Iso::IsolationType isoType, const std::string& cutFunction, bool invertCut = false);
             IsolationConditionFormula(std::string name, std::string isoType, const std::string& cutFunction, bool invertCut = false);
+      IsolationConditionFormula(std::string name, std::string isoType, std::string isoBin, std::pair<int,int> isoBinRange, std::pair<float,float> etaRange, const std::string& cutFunction, bool invertCut = false);
             virtual ~IsolationConditionFormula() {
             }
 
@@ -28,10 +29,15 @@ namespace CP {
             virtual bool accept(const xAOD::IParticle& x, std::map<xAOD::Iso::IsolationType, float>* cutValues = 0) override;
             virtual bool accept(const strObj& x, std::map<xAOD::Iso::IsolationType, float>* cutValues = 0) override;
             void getCutValue(const float pt);
+            void getAbsEta(const xAOD::IParticle& x, float &eta);
 
         private:
             std::shared_ptr<TF1> m_cutFunction;
             bool m_invertCut;
+            int m_cutBinUp  = -99;
+            int m_cutBinLow = -99;
+            float m_cutEtaLow = -99.;
+            float m_cutEtaUp  = -99.;
     };
 }
 #endif
--- PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationCondition.cxx
+++ PhysicsAnalysis/AnalysisCommon/IsolationSelection/Root/IsolationCondition.cxx
@@ -23,17 +23,31 @@ namespace CP{
             m_acc.push_back(std::make_unique< SG::AuxElement::Accessor< float > >( iso_type ));
         }        
     }
+    IsolationCondition::IsolationCondition(const std::string& name, const std::vector<std::string> &isoTypes, const std::vector<std::string> &isoBins) :
+                m_name(name),
+                m_isolationType(std::vector<xAOD::Iso::IsolationType>()),
+                m_cutValue(-999.) {
+        for (auto iso_type : isoTypes){
+            m_isolationType.push_back(xAOD::Iso::IsolationType(0));
+            m_acc.push_back(std::make_unique< SG::AuxElement::Accessor< float > >( iso_type ));
+        }
+        for (auto iso_bin : isoBins){
+            m_acc_bin.push_back(std::make_unique< SG::AuxElement::Accessor< short > >( iso_bin ));
+        }
+    }
     IsolationCondition::IsolationCondition(const std::string &name, xAOD::Iso::IsolationType isoType) :
         IsolationCondition(name, std::vector<xAOD::Iso::IsolationType> {isoType}){
         }
     IsolationCondition::IsolationCondition(const std::string &name, std::string &isoType) :
         IsolationCondition(name, std::vector<std::string> {isoType}){
         }
+    IsolationCondition::IsolationCondition(const std::string &name, std::string &isoType, std::string &isoBin) :
+        IsolationCondition(name, std::vector<std::string> {isoType}, std::vector<std::string> {isoBin}){
+        }
 
     unsigned int IsolationCondition::num_types() const {
         return m_isolationType.size();
     }
-            
     IsolationCondition::~IsolationCondition() {
     }
     std::string IsolationCondition::name() const{
@@ -45,6 +59,10 @@ namespace CP{
     SG::AuxElement::Accessor<float>* IsolationCondition::accessor(unsigned int n) const{
         return m_acc[n].get();
     }
+    SG::AuxElement::Accessor<short>* IsolationCondition::accessor_bin(unsigned int n) const{
+        return m_acc_bin[n].get();
+  }
+
 }
 
 
