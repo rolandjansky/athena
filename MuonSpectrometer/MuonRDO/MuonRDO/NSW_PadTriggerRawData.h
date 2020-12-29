98848d3c4013d8a23e2a6bc06913ff37e78ddaa5
yzach <yuval.zach@cern.ch>
2020-12-29 14:35:04 +0100

    Add getters/setters and constructor to pad trigger data class

--- MuonSpectrometer/MuonRDO/MuonRDO/NSW_PadTriggerRawData.h
+++ MuonSpectrometer/MuonRDO/MuonRDO/NSW_PadTriggerRawData.h
@@ -6,16 +6,17 @@
 
 namespace Muon {
 class NSW_PadTriggerRawData {
-// TODO: Perhaps move this out of the class?
 using hitlist_t = std::vector<uint16_t>;
 friend class NSW_PadTriggerRawDataCnv_p1;
 
 private:
+    // TODO: Perhaps move this out of the class / make public?
     static constexpr std::size_t BC_COUNT{ 3 };
 
     // Metadata
     uint8_t m_sectorID;
     uint8_t m_endcap;
+    // BCID of the "L1A" bunch crossing (not of the additional two recorded)
     uint16_t m_BCID;
     uint32_t m_L1ID;
 
@@ -27,6 +28,23 @@ private:
     std::array<uint8_t, 4> m_phiIDs;
     // Coincidence counts for each of the two wedges, per band ID.
     std::array<std::array<uint8_t, 2>, 4> m_coincidences;
+public:
+    NSW_PadTriggerRawData() = default;
+    // Maybe too many parameters - can refactor class to hold e.g. struct TriggerMetadata, PadTriggerDecision, etc.
+    NSW_PadTriggerRawData(uint8_t sectorID, uint8_t endcap, uint16_t BCID, uint32_t L1ID,
+        std::array<hitlist_t, BC_COUNT> hitlists, std::array<uint8_t, 4> bandIDs, std::array<uint8_t, 4> phiIDs,
+        std::array<std::array<uint8_t, 2>, 4> m_coincidences);
+
+    uint8_t sectorID() const;
+    uint8_t endcap() const;
+    uint16_t BCID() const;
+    uint32_t L1ID() const;
+
+    std::array<hitlist_t, BC_COUNT> hitlists() const;
+    
+    std::array<uint8_t, 4> bandIDs() const;
+    std::array<uint8_t, 4> phiIDs() const;
+    std::array<std::array<uint8_t, 2>, 4> coincidences() const;
 };
 } // namespace Muon
 
--- MuonSpectrometer/MuonRDO/src/NSW_PadTriggerRawData.cxx
+++ MuonSpectrometer/MuonRDO/src/NSW_PadTriggerRawData.cxx
@@ -1,5 +1,42 @@
 #include "MuonRDO/NSW_PadTriggerRawData.h"
 
 namespace Muon {
-    // TODO: impl here
+NSW_PadTriggerRawData::NSW_PadTriggerRawData(uint8_t sectorID, uint8_t endcap, uint16_t BCID, uint32_t L1ID,
+    std::array<hitlist_t, BC_COUNT> hitlists, std::array<uint8_t, 4> bandIDs, std::array<uint8_t, 4> phiIDs,
+    std::array<std::array<uint8_t, 2>, 4> coincidences)
+    : m_sectorID(sectorID), m_endcap(endcap), m_BCID(BCID), m_L1ID(L1ID), m_hitlists(hitlists), m_bandIDs(bandIDs),
+    m_phiIDs(phiIDs), m_coincidences(coincidences) { }
+
+uint8_t NSW_PadTriggerRawData::sectorID() const {
+    return m_sectorID;
+}
+
+uint8_t NSW_PadTriggerRawData::endcap() const {
+    return m_endcap;
+}
+
+uint16_t NSW_PadTriggerRawData::BCID() const {
+    return m_BCID;
+}
+
+uint32_t NSW_PadTriggerRawData::L1ID() const {
+    return m_L1ID;
+}
+
+std::array<NSW_PadTriggerRawData::hitlist_t, NSW_PadTriggerRawData::BC_COUNT> NSW_PadTriggerRawData::hitlists() const {
+    return m_hitlists;
+}
+
+std::array<uint8_t, 4> NSW_PadTriggerRawData::bandIDs() const {
+    return m_bandIDs;
+}
+
+std::array<uint8_t, 4> NSW_PadTriggerRawData::phiIDs() const {
+    return m_phiIDs;
+}
+
+std::array<std::array<uint8_t, 2>, 4> NSW_PadTriggerRawData::coincidences() const {
+    return m_coincidences;
+}
+
 } // namespace Muon
