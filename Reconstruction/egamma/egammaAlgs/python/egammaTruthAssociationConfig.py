d3d902fc899bc98d8cc3d9684a53fb7a7362728a
christos <christos@cern.ch>
2021-01-05 03:15:25 +0100

    Disable TRTNN for PID for now , reprocessing. autopep8/flake8 aesthetic fixes

--- Reconstruction/egamma/egammaAlgs/python/egammaTruthAssociationConfig.py
+++ Reconstruction/egamma/egammaAlgs/python/egammaTruthAssociationConfig.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 
+from MCTruthClassifier.MCTruthClassifierConfig import MCTruthClassifierCaloTruthMatchCfg
 __doc__ = "Configure egammaTruthAssociation"
 
 from AthenaCommon.Logging import logging
 from AthenaConfiguration.ComponentFactory import CompFactory
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
-egammaTruthAssociationAlg=CompFactory.egammaTruthAssociationAlg
-from MCTruthClassifier.MCTruthClassifierConfig import MCTruthClassifierCaloTruthMatchCfg
+egammaTruthAssociationAlg = CompFactory.egammaTruthAssociationAlg
 
 
 def egammaTruthAssociationCfg(flags, name='egammaTruthAssociation', **kwargs):
@@ -21,15 +21,32 @@ def egammaTruthAssociationCfg(flags, name='egammaTruthAssociation', **kwargs):
         kwargs["MCTruthClassifier"] = mctruth.popPrivateTools()
         acc.merge(mctruth)
 
-    kwargs.setdefault("ClusterContainerName",       flags.Egamma.Keys.Output.CaloClusters)
-    kwargs.setdefault("ElectronContainerName",      flags.Egamma.Keys.Output.Electrons)
-    kwargs.setdefault("PhotonContainerName",        flags.Egamma.Keys.Output.Photons)
-    kwargs.setdefault("FwdElectronContainerName",   flags.Egamma.Keys.Output.ForwardElectrons)
-    kwargs.setdefault("TruthEventContainerName",    flags.Egamma.Keys.Input.TruthEvents)
-    kwargs.setdefault("TruthParticleContainerName", flags.Egamma.Keys.Input.TruthParticles)
-    kwargs.setdefault("EgammaTruthContainerName",   flags.Egamma.Keys.Output.TruthParticles)
-    kwargs.setdefault("MatchForwardElectrons",      flags.Egamma.doForwardSeeded)
-    kwargs.setdefault("SimBarcodeOffset",           flags.Sim.SimBarcodeOffset)
+    kwargs.setdefault(
+        "ClusterContainerName",
+        flags.Egamma.Keys.Output.CaloClusters)
+    kwargs.setdefault(
+        "ElectronContainerName",
+        flags.Egamma.Keys.Output.Electrons)
+    kwargs.setdefault(
+        "PhotonContainerName",
+        flags.Egamma.Keys.Output.Photons)
+    kwargs.setdefault(
+        "FwdElectronContainerName",
+        flags.Egamma.Keys.Output.ForwardElectrons)
+    kwargs.setdefault(
+        "TruthEventContainerName",
+        flags.Egamma.Keys.Input.TruthEvents)
+    kwargs.setdefault(
+        "TruthParticleContainerName",
+        flags.Egamma.Keys.Input.TruthParticles)
+    kwargs.setdefault(
+        "EgammaTruthContainerName",
+        flags.Egamma.Keys.Output.TruthParticles)
+    kwargs.setdefault(
+        "MatchForwardElectrons",
+        flags.Egamma.doForwardSeeded)
+    kwargs.setdefault("SimBarcodeOffset",
+                      flags.Sim.SimBarcodeOffset)
 
     egtruthAlg = egammaTruthAssociationAlg(name, **kwargs)
 
