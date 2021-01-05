3a6140cb2cd737a4cc54e5addaf5812b356ef8e5
Tomasz Bold <tomasz.bold@gmail.com>
2021-01-05 15:13:56 +0100

    Updated documentation

--- Trigger/TrigAlgorithms/TrigT2MinBias/src/SPCountHypoTool.h
+++ Trigger/TrigAlgorithms/TrigT2MinBias/src/SPCountHypoTool.h
@@ -31,17 +31,17 @@ private:
   bool applyCut(const Gaudi::Property<int> &threshold, const xAOD::TrigComposite *composit) const;
   HLT::Identifier m_decisionId;
 
-  Gaudi::Property<int> m_totNumPixSP{this, "totNumPixSP", -1, "Accept events with SP Count higher than this threshold in pixel"};
-  Gaudi::Property<int> m_totNumPixCL_1{this, "totNumPixCL_1", -1, "Accept events with 1 SP Count higher than this threshold in pixel"};
-  Gaudi::Property<int> m_totNumPixCL_2{this, "totNumPixCL_2", -1, "Accept events with 2 SP Count higher than this threshold in pixel"};
-  Gaudi::Property<int> m_totNumPixCLmin3{this, "totNumPixCLmin3", -1, "Accept eventss with minimum 3 SP Count higher than this threshold in pixel"};
-  Gaudi::Property<int> m_pixClBarrel{this, "pixClBarrel", -1, "Accept events with SP Count higher than this threshold in pixel Barrel"};
-  Gaudi::Property<int> m_pixClEndcapA{this, "pixClEndcapA", -1, "Accept events with SP Count higher than this threshold in pixel EndcapA"};
-  Gaudi::Property<int> m_pixClEndcapC{this, "pixClEndcapC", -1, "Accept events with SP Count higher than this threshold in pixel EndcapC"};
-  Gaudi::Property<int> m_totNumSctSP{this, "totNumSctSP", -1, "Accept events with SP Count higher than this threshold in SCT"};
-  Gaudi::Property<int> m_SctSpBarrel{this, "SctSpBarrel", -1, "Accept events with SP Count higher than this threshold in SCT Barrel"};
-  Gaudi::Property<int> m_SctSpEndcapA{this, "SctSpEndcapA", -1, "Accept events with SP Count higher than this threshold in SCT EndcapA"};
-  Gaudi::Property<int> m_SctSpEndcapC{this, "SctSpEndcapC", -1, "Accept events with SP Count higher than this threshold in SCT EndcapC"};
+  Gaudi::Property<int> m_totNumPixSP{this, "totNumPixSP", -1, "Require Clusters  (of any size) Count > threshold (-1 disabled) in whole pixel"};
+  Gaudi::Property<int> m_totNumPixCL_1{this, "totNumPixCL_1", -1, "Require Clusters Count made of clustes of size 1 > threshold (-1 disabled) in whole pixel"};
+  Gaudi::Property<int> m_totNumPixCL_2{this, "totNumPixCL_2", -1, "Require Clusters Count made of clustes of size 2 > threshold (-1 disabled) in whole pixel"};
+  Gaudi::Property<int> m_totNumPixCLmin3{this, "totNumPixCLmin3", -1, "Accept eventss with minimum 3 SP Count > threshold (-1 disabled) in pixel"};
+  Gaudi::Property<int> m_pixClBarrel{this, "pixClBarrel", -1, "Require Clusters Count > threshold (-1 disabled) in pixel Barrel"};
+  Gaudi::Property<int> m_pixClEndcapA{this, "pixClEndcapA", -1, "Require Clusters Count > threshold (-1 disabled) in pixel Endcap A side"};
+  Gaudi::Property<int> m_pixClEndcapC{this, "pixClEndcapC", -1, "Require Clusters > threshold (-1 disabled) in pixel Endcap C side"};
+  Gaudi::Property<int> m_totNumSctSP{this, "totNumSctSP", -1, "Require SP Count > threshold (-1 disabled) in whole SCT"};
+  Gaudi::Property<int> m_SctSpBarrel{this, "SctSpBarrel", -1, "Require SP Count > threshold (-1 disabled) in SCT Barrel"};
+  Gaudi::Property<int> m_SctSpEndcapA{this, "SctSpEndcapA", -1, "Require SP Count > threshold (-1 disabled) in SCT Endcap A side"};
+  Gaudi::Property<int> m_SctSpEndcapC{this, "SctSpEndcapC", -1, "Require SP Count > threshold (-1 disabled) in SCT Endcap C side"};
   Gaudi::Property<bool> m_logicAnd{this, "TriggerTypeAnd", true, "And/Or Logic"};
 };
 
