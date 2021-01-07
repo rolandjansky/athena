fb31b80535e9d3bcf85666f89036dec47f3ad15a
Mark Hodgkinson <mhodgkin@aiatlas025.cern.ch>
2021-01-07 18:06:26 +0100

    Migrate all non-HI RecJobTransform ART tests to use MT with 1 thread.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data12_8tev.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data12_8tev.sh
@@ -10,7 +10,7 @@
 # art-include: 21.9/Athena
 
 
-Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data12_8TeV.00209109.physics_JetTauEtmiss.merge.RAW._lb0186._SFO-1._0001.1 --maxEvents 300 --autoConfiguration everything --conditionsTag="COMCOND-BLKPA-RUN1-07" --preExec 'rec.doTrigger=False;' --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root 
+Reco_tf.py --athenaopts="--threads=1" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data12_8TeV.00209109.physics_JetTauEtmiss.merge.RAW._lb0186._SFO-1._0001.1 --maxEvents 300 --autoConfiguration everything --conditionsTag="COMCOND-BLKPA-RUN1-07" --preExec 'rec.doTrigger=False;' --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root 
 
 RES=$?
 echo "art-result: $RES Reco"
--- Reconstruction/RecExample/RecJobTransformTests/test/test_splash_events.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_splash_events.sh
@@ -9,7 +9,7 @@
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
 
-export TRF_ECHO=True; Reco_tf.py --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data15_comm.00265545.physics_MinBias.merge.RAW._lb0048._SFO-ALL._0001.1 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2015-04" --geometryVersion="ATLAS-R2-2015-03-01-00" --outputESDFile myESD.pool.root --outputHISTFile myMergedMonitoring.root --maxEvents 300 --preExec 'rec.doTrigger=False;'
+export TRF_ECHO=True; Reco_tf.py --athenaopts="--threads=1" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data15_comm.00265545.physics_MinBias.merge.RAW._lb0048._SFO-ALL._0001.1 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2015-04" --geometryVersion="ATLAS-R2-2015-03-01-00" --outputESDFile myESD.pool.root --outputHISTFile myMergedMonitoring.root --maxEvents 300 --preExec 'rec.doTrigger=False;'
 
 RES=$?
 echo "art-result: $RES Reco"
