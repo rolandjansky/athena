8993969dd5c6e136fc13514900c9cc6cfc8d6577
Jannik Geisen <jannik.geisen@cern.ch>
2021-01-07 11:22:02 +0100

    simplifying loops over variables to select on

--- Reconstruction/Jet/JetMonitoring/Root/JetEventSelector.cxx
+++ Reconstruction/Jet/JetMonitoring/Root/JetEventSelector.cxx
@@ -20,21 +20,18 @@ JetEventSelector:: ~JetEventSelector(){
 
 StatusCode JetEventSelector::initialize() {
 
-  unsigned int it=0;
-  for (auto var : m_var) {
-    ATH_CHECK(var.retrieve() );
-    ATH_MSG_INFO( "Selecting on var ("<< var->varName() << ") in ["<< m_min.at(it) << " , "<< m_max.at(it)<< "]");
-    it++;
+  for (unsigned int it = 0; it < m_var.size(); it++) {
+    ATH_CHECK(m_var[it].retrieve());
+    ATH_MSG_INFO( "Selecting on var ("<< m_var[it]->varName() << ") in ["<< m_min.at(it) << " , "<< m_max.at(it)<< "]");
   }
   return StatusCode::SUCCESS;
 }
 
 int JetEventSelector::keep(const xAOD::EventInfo& e, const xAOD::JetContainer & jets) const {
-  unsigned int it=0;
-  for (auto var : m_var) {
-    float v = var->value(e, jets);
+
+  for (unsigned int it = 0; it < m_var.size(); it++) {
+    float v = m_var[it]->value(e,jets);
     if ((m_min.at(it) > v) || (m_max.at(it) < v)) return false;
-    it++;
   }
   return true;
 }
