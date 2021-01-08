d36feba769e769ba8df452fc3c6c31c69002054c
scott snyder <sss@karma>
2021-01-08 10:41:11 -0500

    JetMomentTools: Fix clang warnings.
    Don't use references in a range-for if the object must be copied.

--- Reconstruction/Jet/JetMomentTools/Root/JetWidthTool.cxx
+++ Reconstruction/Jet/JetMomentTools/Root/JetWidthTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 // JetWidthTool.cxx
@@ -68,7 +68,7 @@ float JetWidthTool::width(const xAOD::Jet& jet, float& widthEta, float& widthPhi
   float ptSum = 0;
 
   const xAOD::JetConstituentVector constituents = jet.getConstituents();
-  for (const auto *constituent : constituents) {
+  for (const auto constituent : constituents) {
     const float dR   = jet::JetDistances::deltaR(jetEta, jetPhi, constituent->eta(),  constituent->phi() );
     const float dEta = fabs(jet::JetDistances::deltaEta(jetEta, constituent->eta() ));
     const float dPhi = fabs(jet::JetDistances::deltaPhi(jetPhi, constituent->phi() ));
