ee86ea82c85fdc079481aab28ae752e6b6eaccf9
Frank Winklmeier <fwinkl@cern>
2021-01-12 16:28:10 +0100

    MissingETD3PDMaker: Delete unused MissingETEtaRingsFillerTool
    Delete unused `MissingETEtaRingsFillerTool` to remove dependency on
    `MissingETPerformance` package.

--- PhysicsAnalysis/D3PDMaker/MissingETD3PDMaker/src/components/MissingETD3PDMaker_entries.cxx
+++ PhysicsAnalysis/D3PDMaker/MissingETD3PDMaker/src/components/MissingETD3PDMaker_entries.cxx
@@ -10,7 +10,6 @@
 #include "MissingETD3PDMaker/SumETTruthIntFillerTool.h"
 #include "MissingETD3PDMaker/MissingETTruthIntPhiFillerTool.h"
 #include "MissingETD3PDMaker/MissingETTruthIntFillerTool.h"
-#include "MissingETD3PDMaker/MissingETEtaRingsFillerTool.h"
 #include "MissingETD3PDMaker/MissingETJetsInfoFillerTool.h"
 #include "MissingETD3PDMaker/MissingETGoodnessFillerTool.h"
 #include "MissingETD3PDMaker/MissingETRegionFillerTool.h"
@@ -32,7 +31,6 @@ DECLARE_COMPONENT( D3PD::ScalarMissingETTruthIntFillerTool )
 DECLARE_COMPONENT( D3PD::SumETTruthIntFillerTool )
 DECLARE_COMPONENT( D3PD::MissingETTruthIntPhiFillerTool )
 DECLARE_COMPONENT( D3PD::MissingETTruthIntFillerTool )
-DECLARE_COMPONENT( D3PD::MissingETEtaRingsFillerTool )
 DECLARE_COMPONENT( D3PD::MissingETJetsInfoFillerTool )
 DECLARE_COMPONENT( D3PD::MissingETGoodnessFillerTool )
 DECLARE_COMPONENT( D3PD::MissingETRegionFillerTool )
