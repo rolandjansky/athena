5a38f4be1dabbe3c85b69c40c27fe7b81dac7706
scott snyder <sss@karma>
2021-01-13 11:59:39 -0500

    InDetGlobalMonitoring: Fix clang warnings.
    Don't use references in a range-for if the object must be copised.

--- InnerDetector/InDetMonitoring/InDetGlobalMonitoring/src/InDetGlobalTrackMonTool.cxx
+++ InnerDetector/InDetMonitoring/InDetGlobalMonitoring/src/InDetGlobalTrackMonTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 /** @file InDetGlobalTrackMonTool.cxx
@@ -946,7 +946,7 @@ void InDetGlobalTrackMonTool::FillTIDE()
 		    const xAOD::Vertex* foundVertex { nullptr };
 		    if ( vertices.isValid() )
 		    {
-		      for ( const auto& vx : *vertices )
+		      for ( const auto vx : *vertices )
 		      {
 			for ( const auto& tpLink : vx->trackParticleLinks() )
 			{
