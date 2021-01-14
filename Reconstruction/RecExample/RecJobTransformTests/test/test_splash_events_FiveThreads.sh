a1445f2826fd2d82e01bad26f9ec5efcebd09adf
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-01-14 13:02:35 +0100

    Updste splash event tests to use splash data.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_splash_events_FiveThreads.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_splash_events_FiveThreads.sh
@@ -10,7 +10,7 @@
 # art-include: 21.9/Athena
 
 #Monitoring is disabled because it tries to use the trigger information, which is disabled.   
-export TRF_ECHO=True; Reco_tf.py --athenaopts="--threads=1" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data15_comm.00265545.physics_MinBias.merge.RAW._lb0048._SFO-ALL._0001.1 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2015-04" --geometryVersion="ATLAS-R2-2015-03-01-00" --outputESDFile myESD.pool.root  --maxEvents 300 --preExec 'rec.doTrigger=False;from AthenaMonitoring.DQMonFlags import jobproperties;jobproperties.DQMonFlagsCont.doMonitoring.set_Value_and_Lock(False)'
+export TRF_ECHO=True; Reco_tf.py --athenaopts="--threads=1" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data15_1beam/data15_1beam.00260466.physics_L1Calo.merge.RAW._lb1380._SFO-ALL._0001.1 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2015-04" --geometryVersion="ATLAS-R2-2015-03-01-00" --outputESDFile myESD.pool.root  --maxEvents 300 --preExec 'rec.doTrigger=False;from AthenaMonitoring.DQMonFlags import jobproperties;jobproperties.DQMonFlagsCont.doMonitoring.set_Value_and_Lock(False)'
 
 RES=$?
 echo "art-result: $RES Reco"
