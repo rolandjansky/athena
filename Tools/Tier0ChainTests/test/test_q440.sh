c6a2b3d156ef1bc4493972fadf431124df67d18b
Peter Berta <peter.berta@cern.ch>
2021-01-19 10:42:09 +0000

    enabling MT configuration also for tests using only 1 thread; increased number of events for q220 and q440

--- Tools/Tier0ChainTests/test/test_q440.sh
+++ Tools/Tier0ChainTests/test/test_q440.sh
@@ -8,11 +8,11 @@
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
 
-Reco_tf.py --AMI=q440 --outputRDOFile=myRDO.pool.root --outputTAGFile=myTAG.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
+Reco_tf.py --AMI=q440 --athenaopts "RDOtoRDOTrigger:--imf --threads=1 --concurrent-events=1" --outputRDOFile=myRDO.pool.root --outputTAGFile=myTAG.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False --maxEvents=100
 echo "art-result: $? Reco"
 
 ArtPackage=$1
 ArtJobName=$2
-art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
+art.py compare grid --entries 30 ${ArtPackage} ${ArtJobName}
 echo "art-result: $? Diff"
 
