7a2128e8946637cb38892cb0ac9538f6b724bac9
Tadej Novak <tadej.novak@cern.ch>
2021-01-27 10:35:24 +0100

    Initial digitisation transform support

---
+++ Simulation/Tests/DigitizationTests/test/test_Digi_tf_mc16e_CA_vs_CG_no_pileup.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+#
+# art-description: Run a digitization example to compare configuration between ConfGetter and the new ComponentAccumulator approach.
+# art-type: grid
+# art-include: master/Athena
+# art-output: mc16e_ttbar.CG.RDO.pool.root
+# art-output: mc16e_ttbar.CA.RDO.pool.root
+# art-output: log.*
+# art-output: DigiPUConfig*
+
+Events=3
+DigiOutFileNameCG="mc16e_ttbar.CG.RDO.pool.root"
+DigiOutFileNameCA="mc16e_ttbar.CA.RDO.pool.root"
+HSHitsFile="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3091/HITS.10504490._000425.pool.root.1"
+
+
+# config only
+Digi_tf.py \
+--conditionsTag default:OFLCOND-MC16-SDR-25 \
+--digiSeedOffset1 170 --digiSeedOffset2 170 \
+--geometryVersion default:ATLAS-R2-2016-01-00-01 \
+--inputHITSFile ${HSHitsFile} \
+--jobNumber 568 \
+--maxEvents ${Events} \
+--outputRDOFile ${DigiOutFileNameCG} \
+--postExec 'all:CfgMgr.MessageSvc().setError+=["HepMcParticleLink"]' 'HITtoRDO:job+=CfgMgr.JobOptsDumperAlg(FileName="DigiPUConfigCG.txt")' \
+--postInclude 'default:PyJobTransforms/UseFrontier.py' \
+--preExec 'all:from AthenaCommon.BeamFlags import jobproperties;jobproperties.Beam.numberOfCollisions.set_Value_and_Lock(20.0);from LArROD.LArRODFlags import larRODFlags;larRODFlags.NumberOfCollisions.set_Value_and_Lock(20);larRODFlags.nSamples.set_Value_and_Lock(4);larRODFlags.doOFCPileupOptimization.set_Value_and_Lock(True);larRODFlags.firstSample.set_Value_and_Lock(0);larRODFlags.useHighestGainAutoCorr.set_Value_and_Lock(True); from LArDigitization.LArDigitizationFlags import jobproperties;jobproperties.LArDigitizationFlags.useEmecIwHighGain.set_Value_and_Lock(False)' \
+--skipEvents 0 \
+--athenaopts '"--config-only=DigiPUConfigCG.pkl"'
+
+# full run
+Digi_tf.py \
+--conditionsTag default:OFLCOND-MC16-SDR-25 \
+--digiSeedOffset1 170 --digiSeedOffset2 170 \
+--geometryVersion default:ATLAS-R2-2016-01-00-01 \
+--inputHITSFile ${HSHitsFile} \
+--jobNumber 568 \
+--maxEvents ${Events} \
+--outputRDOFile ${DigiOutFileNameCG} \
+--postExec 'all:CfgMgr.MessageSvc().setError+=["HepMcParticleLink"]' 'HITtoRDO:job+=CfgMgr.JobOptsDumperAlg(FileName="DigiPUConfigCG.txt")' \
+--postInclude 'default:PyJobTransforms/UseFrontier.py' \
+--preExec 'all:from AthenaCommon.BeamFlags import jobproperties;jobproperties.Beam.numberOfCollisions.set_Value_and_Lock(20.0);from LArROD.LArRODFlags import larRODFlags;larRODFlags.NumberOfCollisions.set_Value_and_Lock(20);larRODFlags.nSamples.set_Value_and_Lock(4);larRODFlags.doOFCPileupOptimization.set_Value_and_Lock(True);larRODFlags.firstSample.set_Value_and_Lock(0);larRODFlags.useHighestGainAutoCorr.set_Value_and_Lock(True); from LArDigitization.LArDigitizationFlags import jobproperties;jobproperties.LArDigitizationFlags.useEmecIwHighGain.set_Value_and_Lock(False)' \
+--skipEvents 0
+
+rc=$?
+echo "art-result: $rc CGdigi"
+mv runargs.HITtoRDO.py runargs.legacy.HITtoRDO.py 
+mv log.HITtoRDO legacy.HITtoRDO
+
+rc2=-9999
+if [ $rc -eq 0 ]
+then
+    Digi_tf.py \
+    --CA \
+    --conditionsTag default:OFLCOND-MC16-SDR-25 \
+    --digiSeedOffset1 170 --digiSeedOffset2 170 \
+    --geometryVersion default:ATLAS-R2-2016-01-00-01 \
+    --inputHITSFile ${HSHitsFile} \
+    --jobNumber 568 \
+    --maxEvents ${Events} \
+    --outputRDOFile ${DigiOutFileNameCA} \
+    --postInclude 'PyJobTransforms.UseFrontier' 'HITtoRDO:Digitization.DigitizationSteering.DigitizationTestingPostInclude' \
+    --preInclude 'HITtoRDO:Campaigns.MC20e' \
+    --skipEvents 0
+
+    rc2=$?
+fi
+
+echo  "art-result: $rc2 CAdigi"
+
+rc3=-9999
+if [ $rc2 -eq 0 ]
+then
+    acmd.py diff-root ${DigiOutFileNameCG} ${DigiOutFileNameCA} \
+        --mode=semi-detailed --error-mode resilient --order-trees \
+        --ignore-leaves RecoTimingObj_p1_HITStoRDO_timings index_ref
+    rc3=$?
+fi
+
+echo  "art-result: $rc3 comparison"
