99404f4c4153e20f538325df9142b35348703944
Yasuyuki Okumura <yasuyuki.okumura@cern.ch>
2021-01-30 13:33:52 +0100

    to add ART tests with Dev_HI_run3_v1 (AthenaMT)

---
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1DevHI_build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+
+# art-description: Trigger RDO->RDO_TRIG athena test of the Dev_HI_run3_v1 menu
+# art-type: build
+# art-include: master/Athena
+# Skipping art-output which has no effect for build tests.
+# If you create a grid version, check art-output in existing grid tests.
+
+from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
+
+ex = ExecStep.ExecStep()
+ex.type = 'athena'
+ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
+ex.input = 'ttbar'
+ex.threads = 1
+precommand = ''.join([
+  "setMenu='Dev_HI_run3_v1';",
+  "doWriteBS=False;",
+  "doWriteRDOTrigger=True;",
+  "fpeAuditor=True;"
+])
+ex.args = '-c "{:s}"'.format(precommand)
+
+test = Test.Test()
+test.art_type = 'build'
+test.exec_steps = [ex]
+test.check_steps = CheckSteps.default_check_steps(test)
+
+import sys
+sys.exit(test.run())
