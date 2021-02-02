4d4e16a05532b159ad0bb7d81f05e060b639cfc2
Cecilia Tosciri <cecilia.tosciri@cern.ch>
2021-02-02 16:27:46 +0000

    Including gFex EDM

---
+++ Event/xAOD/xAODTrigger/Root/gFexGlobalRoIAuxContainer_v1.cxx
@@ -0,0 +1,23 @@
+/*
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Local include(s):
+#include "xAODTrigger/versions/gFexGlobalRoIAuxContainer_v1.h"
+
+namespace xAOD {
+
+   gFexGlobalRoIAuxContainer_v1::gFexGlobalRoIAuxContainer_v1()
+      : AuxContainerBase() {
+
+      AUX_VARIABLE( word);
+      AUX_VARIABLE( saturated);
+      AUX_VARIABLE( globalType);
+      AUX_VARIABLE( statusOne);
+      AUX_VARIABLE( statusTwo);
+      AUX_VARIABLE( quantityOne);
+      AUX_VARIABLE( quantityTwo);
+
+   }
+
+} // namespace xAOD
