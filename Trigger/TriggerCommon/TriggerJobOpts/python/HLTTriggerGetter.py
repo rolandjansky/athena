83f317779deceb58d8676fab87d744ec95b4ebab
Stewart Martin-Haugh <smh@cern.ch>
2021-02-05 18:31:23 +0100

    Move THistSvc config out of HLTTriggerGetter

--- Trigger/TriggerCommon/TriggerJobOpts/python/HLTTriggerGetter.py
+++ Trigger/TriggerCommon/TriggerJobOpts/python/HLTTriggerGetter.py
@@ -27,23 +27,6 @@ def makeKeysList(inputDict):
     return tmpList
 
 
-def setTHistSvcOutput(outputList):
-    """Build the Output list of the THistSvc. This is used below and to
-    configure athenaMT/PT when running from the online DB but with the
-    offline THistSvc"""
-    
-    if 1 not in [ o.count('SHIFT') for o in outputList ]:
-        outputList += [ "SHIFT DATAFILE='shift-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('EXPERT') for o in outputList ]:
-        outputList += [ "EXPERT DATAFILE='expert-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('run_1') for o in outputList ]:
-        outputList += [ "run_1 DATAFILE='lbn-monitoring.root' OPT='RECREATE'"]        
-    if 1 not in [ o.count('RUNSTAT') for o in outputList ]:
-        outputList += [ "RUNSTAT DATAFILE='runstat-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('DEBUG') for o in outputList ]:        
-        outputList += [ "DEBUG DATAFILE='debug-monitoring.root' OPT='RECREATE'"]
-
-    return
         
     
 def monitoringTools(steering):
