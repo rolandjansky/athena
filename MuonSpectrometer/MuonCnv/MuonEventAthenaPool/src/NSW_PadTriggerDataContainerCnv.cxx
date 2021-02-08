f2af0407486ab308dd00fad89d676a6fa902ccb5
yzach <yuval.zach@cern.ch>
2021-02-08 16:24:11 +0200

    Update NSW_PadTriggerDataContainer_p1 UUID

--- MuonSpectrometer/MuonCnv/MuonEventAthenaPool/src/NSW_PadTriggerDataContainerCnv.cxx
+++ MuonSpectrometer/MuonCnv/MuonEventAthenaPool/src/NSW_PadTriggerDataContainerCnv.cxx
@@ -11,7 +11,7 @@ NSW_PadTriggerDataContainer_PERS* NSW_PadTriggerDataContainerCnv::createPersiste
 Muon::NSW_PadTriggerDataContainer* NSW_PadTriggerDataContainerCnv::createTransient() {
     MsgStream log(msgSvc(), "NSW_PadTriggerDataContainerCnv");
     // UUID of of NSW_PadTriggerDataContainer_p1
-    static pool::Guid p1_guid("2930850B-526A-4A6B-BDC6-C86D43B06C7C");
+    static pool::Guid p1_guid("5E3C969A-84BF-4DA1-94B9-F1A6A94C3123");
     if(compareClassGuid(p1_guid)) {
         std::unique_ptr<Muon::NSW_PadTriggerDataContainer_p1> pContainer
             { poolReadObject<Muon::NSW_PadTriggerDataContainer_p1>() };
