b3dc9527676333e40dc5b27a82144123e528d1a3
Peter Sherwood <peter.sherwood@cern.ch>
2021-02-09 09:10:25 +0100

    jet hypo: remove unused files
    remove header file which are not included, and heder/implmention files where the heade is
    included only by the implementation file.

--- Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypo/src/components/TrigHLTJetHypo_entries.cxx
@@ -2,19 +2,6 @@
   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-/*
-#include "TrigHLTJetHypo/TrigHLTJetHypo2.h"
-#include "TrigHLTJetHypo/TrigEFRazorAllTE.h"
-#include "TrigHLTJetHypo/TrigEFDPhiMetJetAllTE.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_Dijet.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_DijetMassDEta.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_DijetMassDEtaDPhi.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_EtaEt.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_HT.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_TLA.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypo_SMC.h"
-*/
-
 #include "../TrigJetHypoAlgMT.h"
 #include "../TrigJetHypoToolConfig_fastreduction.h"
 //
@@ -40,31 +27,6 @@
 #include "../TrigJetTLAHypoAlgMT.h"
 #include "../TrigJetTLAHypoToolMT.h"
 
-/*
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/BasicCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/AntiCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/EtaEtCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/LlpCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/LooseCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/NullCleanerTool.h"
-#include "TrigHLTJetHypo/TrigHLTJetHypoUtils/TightCleanerTool.h"
-
-
-DECLARE_COMPONENT(TrigHLTJetHypo2)
-*/
-
-/*
-DECLARE_COMPONENT(TrigEFRazorAllTE)
-DECLARE_COMPONENT(TrigEFDPhiMetJetAllTE)
-DECLARE_COMPONENT(TrigHLTJetHypo_Dijet)
-DECLARE_COMPONENT(TrigHLTJetHypo_DijetMassDEta)
-DECLARE_COMPONENT(TrigHLTJetHypo_DijetMassDEtaDPhi)
-DECLARE_COMPONENT(TrigHLTJetHypo_SMC)
-DECLARE_COMPONENT(TrigHLTJetHypo_HT)
-DECLARE_COMPONENT(TrigHLTJetHypo_TLA)
-DECLARE_COMPONENT(TrigHLTJetHypo_EtaEt)
-*/
-
 DECLARE_COMPONENT(TrigJetHypoToolConfig_fastreduction)
 DECLARE_COMPONENT(TrigJetConditionConfig_phi)
 DECLARE_COMPONENT(TrigJetConditionConfig_signed_eta)
@@ -88,12 +50,3 @@ DECLARE_COMPONENT(TrigJetTLAHypoToolMT)
 DECLARE_COMPONENT(TrigJetHypoToolHelperMT)
 DECLARE_COMPONENT(TrigJetHypoToolHelperNoGrouper)
 
-/*
-DECLARE_COMPONENT(BasicCleanerTool)
-DECLARE_COMPONENT(AntiCleanerTool)
-DECLARE_COMPONENT(EtaEtCleanerTool)
-DECLARE_COMPONENT(LlpCleanerTool)
-DECLARE_COMPONENT(LooseCleanerTool)
-DECLARE_COMPONENT(NullCleanerTool)
-DECLARE_COMPONENT(TightCleanerTool)
-*/
