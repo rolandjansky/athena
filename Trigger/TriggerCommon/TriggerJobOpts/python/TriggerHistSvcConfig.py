729262a7291f6649a46103ea992efb8df62e1d3e
Frank Winklmeier <fwinkl@cern>
2021-02-11 14:11:26 +0100

    TriggerJobOpts: remove unnecessary THistSvc output files
    Remove the `debug-monitoring.root`, `lbn-monitoring.root`, ... THistSvc
    output files. These histogram streams were not being filled and only
    produced empty ROOT files.

--- Trigger/TriggerCommon/TriggerJobOpts/python/TriggerHistSvcConfig.py
+++ Trigger/TriggerCommon/TriggerJobOpts/python/TriggerHistSvcConfig.py
@@ -1,22 +1,12 @@
 #
-#  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+#  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 #
 
 def setTHistSvcOutput(outputList):
-    """Build the Output list of the THistSvc. This is used to
-    configure athenaHLT when running from the online DB but with the
-    offline THistSvc"""
+    """Build the Output list of the THistSvc ensuring minimum set of output files"""
 
-    if 1 not in [ o.count('SHIFT') for o in outputList ]:
-        outputList += [ "SHIFT DATAFILE='shift-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('EXPERT') for o in outputList ]:
-        outputList += [ "EXPERT DATAFILE='expert-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('run_1') for o in outputList ]:
-        outputList += [ "run_1 DATAFILE='lbn-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('RUNSTAT') for o in outputList ]:
-        outputList += [ "RUNSTAT DATAFILE='runstat-monitoring.root' OPT='RECREATE'"]
-    if 1 not in [ o.count('DEBUG') for o in outputList ]:
-        outputList += [ "DEBUG DATAFILE='debug-monitoring.root' OPT='RECREATE'"]
+    if not any('EXPERT' in o for o in outputList):
+        outputList += ["EXPERT DATAFILE='expert-monitoring.root' OPT='RECREATE'"]
 
     return
 
