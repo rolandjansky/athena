8196a165031361e1795e42a6110688f70317c2af
scott snyder <sss@karma>
2021-02-12 22:58:11 -0500

    CoralDB: Fix cppcheck warnings.
    Fix cppcheck warnings.

--- Database/CoralDB/CoralDB/Timer.h
+++ Database/CoralDB/CoralDB/Timer.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 // $Id: Timer.h,v 1.1 2007-02-25 07:23:13 jb Exp $
@@ -17,7 +17,10 @@ using namespace std;
 class Timer {
 
  public:
-  Timer(string description = "Timer", double n = 1.0) { m_description = description; m_n = n; stop(); start(); }
+  Timer(const std::string& description = "Timer", double n = 1.0)
+    : m_description (description),
+      m_n (n)
+  { stop(); start(); }
   void start() { gettimeofday(&m_t0, NULL); }
   void stop() { gettimeofday(&m_t1, NULL); }
   double elapsed() { return m_t1.tv_sec - m_t0.tv_sec + 1.0e-6 * (m_t1.tv_usec - m_t0.tv_usec); }
