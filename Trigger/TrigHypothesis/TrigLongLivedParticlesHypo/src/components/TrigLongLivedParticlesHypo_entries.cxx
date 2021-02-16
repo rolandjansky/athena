96c976c6af94f963ed4d560a6816bb45ae636440
Frank Winklmeier <fwinkl@cern>
2021-02-16 14:40:05 +0100

    TrigLongLivedParticlesHypo: delete Run-2 trigger hypos
    Delete obsolete Run-2 trigger hypothesis algorithms (ATR-22712).

--- Trigger/TrigHypothesis/TrigLongLivedParticlesHypo/src/components/TrigLongLivedParticlesHypo_entries.cxx
+++ Trigger/TrigHypothesis/TrigLongLivedParticlesHypo/src/components/TrigLongLivedParticlesHypo_entries.cxx
@@ -1,30 +1,9 @@
-#include "TrigLongLivedParticlesHypo/TrigL2HVJetHypoAllCuts.h"
-#include "TrigLongLivedParticlesHypo/TrigL2HVJetHypo.h"
-#include "TrigLongLivedParticlesHypo/TrigL2HVJetHypoTrk.h"
-#include "TrigLongLivedParticlesHypo/MuonClusterHypo.h"
-#include "TrigLongLivedParticlesHypo/MuonClusterIsolationHypo.h"
-#include "TrigLongLivedParticlesHypo/TrigLoFRemovalHypo.h"
-#include "TrigLongLivedParticlesHypo/TrigNewLoFHypo.h"
-#include "TrigLongLivedParticlesHypo/TrigCaloRatioHypo.h"
-#include "TrigLongLivedParticlesHypo/TrigLLPInnerDetectorHypo.h"
+#include "TrigLongLivedParticlesHypo/FastTrackFinderLRTHypoTool.h"
+#include "TrigLongLivedParticlesHypo/FastTrackFinderLRTHypoAlg.h"
 #include "../TrigIsoHPtTrackTriggerHypoAlgMT.h"
 #include "../TrigIsoHPtTrackTriggerHypoTool.h"
 
-
 DECLARE_COMPONENT (TrigIsoHPtTrackTriggerHypoAlgMT )
 DECLARE_COMPONENT (TrigIsoHPtTrackTriggerHypoTool )
-
-#include "TrigLongLivedParticlesHypo/FastTrackFinderLRTHypoTool.h"
-#include "TrigLongLivedParticlesHypo/FastTrackFinderLRTHypoAlg.h"
-
-DECLARE_COMPONENT( TrigL2HVJetHypoAllCuts )
-DECLARE_COMPONENT( TrigL2HVJetHypo )
-DECLARE_COMPONENT( TrigL2HVJetHypoTrk )
-DECLARE_COMPONENT( MuonClusterHypo )
-DECLARE_COMPONENT( MuonClusterIsolationHypo )
-DECLARE_COMPONENT( TrigLoFRemovalHypo )
-DECLARE_COMPONENT( TrigNewLoFHypo )
-DECLARE_COMPONENT( TrigCaloRatioHypo )
-
 DECLARE_COMPONENT( FastTrackFinderLRTHypoTool )
 DECLARE_COMPONENT( FastTrackFinderLRTHypoAlg )
