25b3becfcc04fc37c370c6acc52d96114bc42a9c
Tomasz Bold <tomasz.bold@gmail.com>
2021-02-18 07:05:02 +0000

    Add tracks selection to MinBias DQ

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/src/HLTMinBiasTrkMonAlg.h
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/src/HLTMinBiasTrkMonAlg.h
@@ -1,8 +1,8 @@
 /*
 Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-#ifndef TRIGMINBIASMONITORING_HLTMINBIASMONALGMT_H
-#define TRIGMINBIASMONITORING_HLTMINBIASMONALGMT_H
+#ifndef TRIGMINBIASMONITORING_HLTMinBiasTrkMonAlg_H
+#define TRIGMINBIASMONITORING_HLTMinBiasTrkMonAlg_H
 
 // Framework includes
 
@@ -12,19 +12,19 @@ Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 #include "xAODTracking/TrackParticleContainer.h"
 #include "TrigDecisionTool/TrigDecisionTool.h"
 #include "GaudiKernel/ToolHandle.h"
-//#include "TriggerD3PDMaker/TrigDecisionFillerTool.h"
+#include "InDetTrackSelectionTool/IInDetTrackSelectionTool.h"
 
 // STL includes
 #include <string>
 
 /**
-* @class HLTMinBiasMonAlgMT
+* @class HLTMinBiasTrkMonAlg
 * @brief
 **/
-class HLTMinBiasMonAlgMT : public AthMonitorAlgorithm {
+class HLTMinBiasTrkMonAlg : public AthMonitorAlgorithm {
 public:
-  HLTMinBiasMonAlgMT(const std::string& name, ISvcLocator* pSvcLocator);
-  virtual ~HLTMinBiasMonAlgMT() override;
+  HLTMinBiasTrkMonAlg(const std::string& name, ISvcLocator* pSvcLocator);
+  virtual ~HLTMinBiasTrkMonAlg() override;
 
   virtual StatusCode initialize() override;
   virtual StatusCode fillHistograms(const EventContext& context) const override;
@@ -37,8 +37,12 @@ private:
   SG::ReadHandleKey<xAOD::TrackParticleContainer> m_onlineTrkKey { this, "OnlineTrkKey", "HLT_IDTrack_MinBias_IDTrig", "Name of track counts info object produced by the HLT track counting FEX algorithm" };
 
   Gaudi::Property<std::vector<std::string>> m_triggerList{this, "triggerList",{}, "Add triggers to this to be monitored"};
+  ToolHandle<InDet::IInDetTrackSelectionTool> m_trackSelectionTool {this, "TrackSelectionTool", "InDetTrackSelectionTool", "Tool for selecting tracks"};
+
   StatusCode monitorPurities(const EventContext& context) const;
   StatusCode monitorSPCounts(const EventContext& context) const;
   StatusCode monitorTrkCounts(const EventContext& context) const;
+
+
 };
-#endif // TRIGMINBIASMONITORING_HLTMINBIASMONALGMT_H
+#endif // TRIGMINBIASMONITORING_HLTMinBiasTrkMonAlg_H
