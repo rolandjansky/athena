e48c904aba16e9afd2e0e70566eb898535621087
Emily Anne Thompson <emily.anne.thompson@cern.ch>
2021-02-18 11:22:12 +0100

    Remove commented-out tau triggers

--- PhysicsAnalysis/SUSYPhys/LongLivedParticleDPDMaker/python/TauFlags.py
+++ PhysicsAnalysis/SUSYPhys/LongLivedParticleDPDMaker/python/TauFlags.py
@@ -29,15 +29,8 @@ class Taus_SingleTauFilterFlags(JobProperty):
     allowedTypes=["bool"]
     StoredValue=True
     triggers=[
-        #'HLT_tau160_medium1_tracktwoMVA_L1TAU100',
         'HLT_tau160_mediumRNN_tracktwoMVA_L1TAU100',
-        #'HLT_tau160_medium1_tracktwo_L1TAU100',
-        #'HLT_tau160_medium1_tracktwoEF_L1TAU100',
-        'HLT_tau200_mediumRNN_tracktwoMVA_L1TAU100',
-        #'HLT_tau200_medium1_tracktwo_L1TAU100',
-        #'HLT_tau200_medium1_tracktwoEF_L1TAU100',
-        #'HLT_tau200_medium1_tracktwoMVA_L1TAU100'
-
+        'HLT_tau200_mediumRNN_tracktwoMVA_L1TAU100'
         ] # Primary 2018 RNN trigger
     # I preffer to not rely on the trigger API at the moment, while R&D in progress
     #if rpvllTrig.doRPVLLTriggerAPI:
@@ -50,12 +43,7 @@ class Taus_DiTauFilterFlags(JobProperty):
     allowedTypes=["bool"]
     StoredValue=True
     triggers=[
-    #'HLT_tau35_mediumRNN_tracktwoMVA_tau25_mediumRNN_tracktwoMVA_L1DR-TAU20ITAU12I-J25',
-    #'HLT_tau35_mediumRNN_tracktwoMVA_tau25_mediumRNN_tracktwoMVA_L1TAU20IM_2TAU12IM_4J12.0ETA23',
-    #'HLT_tau80_mediumRNN_tracktwoMVA_L1TAU60_tau35_mediumRNN_tracktwoMVA_L1TAU12IM_L1TAU60_DR-TAU20ITAU12I', # This Topo trigger causes crash in code and needs special attention (https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/DerivationFramework)
-    'HLT_tau80_mediumRNN_tracktwoMVA_L1TAU60_tau60_mediumRNN_tracktwoMVA_L1TAU40',
-    #'HLT_tau40_mediumRNN_tracktwoMVA_tau35_mediumRNN_tracktwoMVA',
-    #'HLT_tau35_mediumRNN_tracktwoMVA_tau25_mediumRNN_tracktwoMVA_03dR30_L1DR-TAU20ITAU12I-J25',
+    'HLT_tau80_mediumRNN_tracktwoMVA_L1TAU60_tau60_mediumRNN_tracktwoMVA_L1TAU40'
     ]
     # I preffer to not rely on the trigger API at the moment, while R&D in progress
     #if rpvllTrig.doRPVLLTriggerAPI:
@@ -68,14 +56,8 @@ class Taus_TauMETFilterFlags(JobProperty):
     allowedTypes=["bool"]
     StoredValue=True
     triggers=[
-        #'HLT_tau35_medium1_tracktwoEF_xe70_L1XE45',
-        #'HLT_tau35_medium1_tracktwoMVA_xe70_L1XE45',
         'HLT_tau35_mediumRNN_tracktwoMVA_xe70_L1XE45',
-        #'HLT_tau35_medium1_tracktwo_xe70_L1XE45',
-        'HLT_tau60_mediumRNN_tracktwoMVA_tau25_mediumRNN_tracktwoMVA_xe50',
-        #'HLT_tau60_medium1_tracktwoMVA_tau25_medium1_tracktwoMVA_xe50',
-        #'HLT_tau60_medium1_tracktwo_tau25_medium1_tracktwo_xe50',
-        #'HLT_tau60_medium1_tracktwoEF_tau25_medium1_tracktwoEF_xe50'
+        'HLT_tau60_mediumRNN_tracktwoMVA_tau25_mediumRNN_tracktwoMVA_xe50'
     ] # Primary 2018 RNN trigger
     # I preffer to not rely on the trigger API at the moment, while R&D in progress
     #if rpvllTrig.doRPVLLTriggerAPI:
@@ -83,7 +65,6 @@ class Taus_TauMETFilterFlags(JobProperty):
     pass
 primRPVLLDESDM.add_JobProperty(Taus_TauMETFilterFlags)
 
-# Do we have to define a prescale?
 class Taus_PrescalerFlags(JobProperty):
     statusOn=True
     allowedTypes=['bool']
