2bf4c1edba07da702b4e3f3c493e9b2201a061e9
Jiri Masik <jiri.masik@cern.ch>
2021-02-22 15:12:23 +0000

    remove code and packages unneeded in Run 3. ATR-22797

--- InnerDetector/InDetTrigRecAlgs/InDetTrigTrackSlimmer/src/components/InDetTrigTrackSlimmer_entries.cxx
+++
@@ -1,5 +0,0 @@
-#include "InDetTrigTrackSlimmer/TrigTrackSlimmer.h"
-#include "TrkTrack/Track.h"
-
-DECLARE_COMPONENT( InDet::TrigTrackSlimmer )
-
--- InnerDetector/InDetTrigRecAlgs/SiTrigSPSeededTrackFinder/src/components/SiTrigSPSeededTrackFinder_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "SiTrigSPSeededTrackFinder/SiTrigSPSeededTrackFinder.h"
-
-using namespace InDet;
-
-DECLARE_COMPONENT( SiTrigSPSeededTrackFinder )
-
