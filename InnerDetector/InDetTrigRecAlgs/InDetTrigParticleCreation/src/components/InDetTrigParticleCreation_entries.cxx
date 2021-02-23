01250a455f8ac6f9f194afc1a93079efcafe58ee
Frank Winklmeier <fwinkl@cern>
2021-02-23 16:36:05 +0100

    InDetTrigParticleCreation: delete Run-2 algorithms
    Delete obsolete Run-2 algorithms and cleanup `TrigTrackingxAODCnvMT`.
    
    Relates to ATR-22797.

--- InnerDetector/InDetTrigRecAlgs/InDetTrigParticleCreation/src/components/InDetTrigParticleCreation_entries.cxx
+++ InnerDetector/InDetTrigRecAlgs/InDetTrigParticleCreation/src/components/InDetTrigParticleCreation_entries.cxx
@@ -1,11 +1,3 @@
-#include "InDetTrigParticleCreation/TrigParticleCreator.h"
-#include "InDetTrigParticleCreation/TrigTrackingxAODCnv.h"
 #include "../TrigTrackingxAODCnvMT.h"
-#include "InDetTrigParticleCreation/TrigVertexxAODCnv.h"
-#include "TrkTrack/Track.h"
 
-DECLARE_COMPONENT( InDet::TrigParticleCreator )
-DECLARE_COMPONENT( InDet::TrigTrackingxAODCnv )
 DECLARE_COMPONENT( InDet::TrigTrackingxAODCnvMT )
-DECLARE_COMPONENT( InDet::TrigVertexxAODCnv )
-
