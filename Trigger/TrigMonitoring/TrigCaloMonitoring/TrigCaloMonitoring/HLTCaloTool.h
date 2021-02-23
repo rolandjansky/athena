a6339c36709103deb72dcc676155a19a00d91739
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2021-02-23 18:13:21 +0000

    Added ReadHandle for CaloCellContainer to HLTCaloTool
    Fixes crash in the RAWtoESD step of multi-threaded q431

--- Trigger/TrigMonitoring/TrigCaloMonitoring/TrigCaloMonitoring/HLTCaloTool.h
+++ Trigger/TrigMonitoring/TrigCaloMonitoring/TrigCaloMonitoring/HLTCaloTool.h
@@ -1,10 +1,10 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 
-#ifndef HLTCALOTOOL_H
-#define HLTCALOTOOL_H
+#ifndef TRIGCALOMONITORING_HLTCALOTOOL_H
+#define TRIGCALOMONITORING_HLTCALOTOOL_H
 
 // Simple description to come
 
@@ -17,35 +17,41 @@ class TileID;
 #include "TrigHLTMonitoring/IHLTMonTool.h"
 #include "GaudiKernel/ToolHandle.h"
 
+#include "StoreGate/ReadHandleKey.h"
+class CaloCellContainer;
+
 class HLTCaloTool : public IHLTMonTool {
 
-public:
-	// Constructor
-	HLTCaloTool (const std::string & type, const std::string & name, const IInterface* parent);
-	virtual ~HLTCaloTool();
-	StatusCode init(); // Empty for the moment.
+ public:
+  // Constructor
+  HLTCaloTool (const std::string & type, const std::string & name, const IInterface* parent);
+  virtual ~HLTCaloTool();
+  StatusCode init(); // Empty for the moment.
 #ifdef ManagedMonitorToolBase_Uses_API_201401
-	StatusCode book(); // called by bookHistograms
+  StatusCode book(); // called by bookHistograms
 #else
-	StatusCode book(bool newEventsBlock, bool newLumiBlock, bool newRun); // called by bookHistograms
+  StatusCode book(bool newEventsBlock, bool newLumiBlock, bool newRun); // called by bookHistograms
 #endif
-	StatusCode fill(); // called by fillHistograms
+  StatusCode fill(); // called by fillHistograms
 #ifdef ManagedMonitorToolBase_Uses_API_201401
-        StatusCode proc() ; // called by procHistograms
+  StatusCode proc() ; // called by procHistograms
 #else
-	StatusCode proc(bool /*endOfEventsBlock*/, bool /*endOfLumiBlock*/, bool /*endOfRun*/) ; // called by procHistograms
+  StatusCode proc(bool /*endOfEventsBlock*/, bool /*endOfLumiBlock*/, bool /*endOfRun*/) ; // called by procHistograms
 #endif
 
-private:
-	/** EF Calorimeter tools for calorimeter data unpacking. */
-	ToolHandleArray<IAlgToolEFCalo>     m_tcrAlgTools;
-	/** use or not ntuples */
-	bool m_ntuple;
-	const LArOnlineID * m_onlineHelper;
-        ToolHandle<LArCablingLegacyService>     m_cablingSvc;
-        const TileID* m_tileID;
-
-
+ private:
+  /** EF Calorimeter tools for calorimeter data unpacking. */
+  ToolHandleArray<IAlgToolEFCalo>     m_tcrAlgTools;
+  /** use or not ntuples */
+  bool m_ntuple;
+  const LArOnlineID * m_onlineHelper;
+  ToolHandle<LArCablingLegacyService>     m_cablingSvc;
+  const TileID* m_tileID;
+  
+  SG::ReadHandleKey<CaloCellContainer> m_CaloCellContainerName {this
+      , "CaloCellContainer"
+      , "AllCalo"
+      , "input Calo cell container key"};
 };
 
 #endif
