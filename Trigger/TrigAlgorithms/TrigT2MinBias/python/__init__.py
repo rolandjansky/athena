a325b755519d07db1cdf7d97b4d927bf9ffc7b3f
Frank Winklmeier <fwinkl@cern>
2021-02-24 11:07:29 +0100

    TrigT2MinBias: delete unused python files and cleanup cmake
    Delete unused python files used to configure the Run-2 trigger and
    cleanup cmake link dependencies.
    
    Relates to ATR-22841.

--- Trigger/TrigAlgorithms/TrigT2MinBias/python/__init__.py
+++ Trigger/TrigAlgorithms/TrigT2MinBias/python/__init__.py
@@ -1,7 +1,1 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-
-__version__ = "1.0.0"
-__author__  = "William.Bell@cern.ch"
-__all__     = [ "TrigT2MinBiasConfig" ]
-
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
