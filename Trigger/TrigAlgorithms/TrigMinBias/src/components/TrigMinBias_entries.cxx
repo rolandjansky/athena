6bdaf13939e777477da0d1b8ea31dd0474b752cb
Tomasz Bold <tomasz.bold@gmail.com>
2021-02-25 16:06:30 +0000

    Drop Run 2 legacy code from TrigMinBias

--- Trigger/TrigAlgorithms/TrigMinBias/src/components/TrigMinBias_entries.cxx
+++ Trigger/TrigAlgorithms/TrigMinBias/src/components/TrigMinBias_entries.cxx
@@ -1,14 +1,6 @@
-#include "../TrigTrackCounter.h"
-#include "../TrigTrackCounterHypo.h"
-#include "../TrigVertexCounter.h"
-#include "../TrigVertexCounterHypo.h"
+
 #include "../TrackCountHypoAlgMT.h"
 #include "../TrackCountHypoTool.h"
 
-
-DECLARE_COMPONENT( TrigTrackCounter )
-DECLARE_COMPONENT( TrigTrackCounterHypo )
-DECLARE_COMPONENT( TrigVertexCounter )
-DECLARE_COMPONENT( TrigVertexCounterHypo )
 DECLARE_COMPONENT( TrackCountHypoAlgMT )
 DECLARE_COMPONENT( TrackCountHypoTool )
