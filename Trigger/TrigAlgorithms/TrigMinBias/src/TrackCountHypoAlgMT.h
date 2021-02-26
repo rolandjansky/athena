e7d0dff89a82578e52abed8251fa6f5d92dbd0bd
Frank Winklmeier <fwinkl@cern>
2021-02-26 14:44:14 +0100

    TrigMinBias: cmake/header cleanup
    Remove some unused link dependencies and cleanup includes.

--- Trigger/TrigAlgorithms/TrigMinBias/src/TrackCountHypoAlgMT.h
+++ Trigger/TrigAlgorithms/TrigMinBias/src/TrackCountHypoAlgMT.h
@@ -1,32 +1,28 @@
 /*
-Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 #ifndef TRIGMINBIAS_TRACKCOUNTHYPOALGMT_H
 #define TRIGMINBIAS_TRACKCOUNTHYPOALGMT_H
 
 // Framework includes
+#include "TrackCountHypoTool.h"
 #include "DecisionHandling/HypoBase.h"
 #include "xAODTrigger/TrigCompositeContainer.h"
-#include "TrackCountHypoTool.h"
-#include "TrigCompositeUtils/TrigCompositeUtils.h"
 #include "xAODTracking/TrackParticleContainer.h"
 #include "AthenaMonitoringKernel/Monitored.h"
-#include <map>
 #include "Gaudi/Property.h"
-#include "AthViews/View.h"
 
 // STL includes
 #include <string>
+
 class TrackCountHypoAlgMT : public ::HypoBase  {
 public:
   TrackCountHypoAlgMT(const std::string& name, ISvcLocator* pSvcLocator);
 
-  virtual ~TrackCountHypoAlgMT() = default;
   virtual StatusCode initialize() override;
   virtual StatusCode execute(const EventContext& context) const override;
 
 private:
-  TrackCountHypoAlgMT();
   ToolHandleArray< TrackCountHypoTool > m_hypoTools {this, "HypoTools", {}, "Tools that perform actual selection"};
   SG::ReadHandleKey<xAOD::TrackParticleContainer> m_tracksKey{this, "tracksKey", "Undefined", ""};
   SG::WriteHandleKey<xAOD::TrigCompositeContainer> m_trackCountKey{this, "trackCountKey", "Undefined", ""};
@@ -35,4 +31,5 @@ private:
       than this limit in mm; this vector has to be of the same size as minPt vector, repetition of cut values are allowed if that makes the number of elements the same"};
   ToolHandle<GenericMonitoringTool> m_monTool{this,"MonTool", "", "Monitoring tool"};
 };
-#endif // TRIGT2MINBIAS_TRACKCOUNTHYPOALGMT_H
+
+#endif
