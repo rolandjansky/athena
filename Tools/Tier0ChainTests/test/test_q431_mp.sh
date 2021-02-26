e281b17cad12fa4f3a23194df16c20d959d2783f
Peter Berta <peter.berta@cern.ch>
2021-02-26 15:46:29 +0100

    TCT: updated tests q431 - enabling doRAWtoALL and updated conditionsTag

--- Tools/Tier0ChainTests/test/test_q431_mp.sh
+++ Tools/Tier0ChainTests/test/test_q431_mp.sh
@@ -8,12 +8,23 @@
 Reco_tf.py \
 --AMI=q431 \
 --athenaopts='--nprocs=2' \
---maxEvents=100 \
+--maxEvents=200 \
+--steering doRAWtoALL \
+--conditionsTag 'all:CONDBR2-BLKPA-RUN2-03' \
 --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
 
-echo "art-result: $? Reco"
+rc1=$?
+echo "art-result: $rc1 Reco"
 
-ArtPackage=$1
-ArtJobName=$2
-art.py compare grid --entries 30 ${ArtPackage} ${ArtJobName}
-echo "art-result: $? Diff"
+rc2=-9999
+if [ ${rc1} -eq 0 ]
+then
+  ArtPackage=$1
+  ArtJobName=$2
+  art.py compare grid --entries 30 ${ArtPackage} ${ArtJobName} --mode=semi-detailed
+  rc2=$?
+fi
+echo  "art-result: ${rc2} Diff"
+
+
+
