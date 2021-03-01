047e0cbf849e20ef891fa54e1797ddec89a1db87
Tim Martin <tim.martin@cern.ch>
2021-03-01 12:28:35 +0000

    Update xAODConfigTool.cxx to set the key values into the objects

--- Trigger/TrigConfiguration/TrigConfxAOD/Root/xAODConfigTool.cxx
+++ Trigger/TrigConfiguration/TrigConfxAOD/Root/xAODConfigTool.cxx
@@ -348,6 +348,13 @@ namespace TrigConf {
 
          ATH_CHECK( loadPtrees() );
 
+         // Loading the payload doesn't additionally let the object know about its own key. We can load this in now too.
+         m_impl->m_currentHlt.setSMK( m_currentHltJson->key() );
+         m_impl->m_currentL1.setSMK( m_currentL1Json->key() );
+         m_impl->m_currentHltps.setPSK( m_currentHltpsJson->key() );
+         m_impl->m_currentL1ps.setPSK( m_currentL1psJson->key() );
+         //m_impl->m_currentBg 
+
          ATH_CHECK( prepareTriggerMenu( m_impl->m_currentHlt,
                                         m_impl->m_currentL1,
                                         m_impl->m_currentHltps,
@@ -473,7 +480,16 @@ namespace TrigConf {
       // ATH_CHECK( loadJsonByKey("Bunchgroups", m_bgJson, TODO, m_currentBgJson) );
 
       // ... and from these serialised JSON strings, populate the ptree data structures...
-      ATH_CHECK( loadPtrees() ); // R3 interfaces now active
+      ATH_CHECK( loadPtrees() ); 
+
+      // ... and set the keys of the objects in the objects (not part of the JSON pyaload) ...
+      m_impl->m_currentHlt.setSMK( m_currentHltJson->key() );
+      m_impl->m_currentL1.setSMK( m_currentL1Json->key() );
+      m_impl->m_currentHltps.setPSK( m_currentHltpsJson->key() );
+      m_impl->m_currentL1ps.setPSK( m_currentL1psJson->key() );
+
+      // R3 interfaces now active
+
       // ... and finally populate the legacy interface from the ptree data
       ATH_CHECK( prepareTriggerMenu( m_impl->m_currentHlt,
                                      m_impl->m_currentL1,
