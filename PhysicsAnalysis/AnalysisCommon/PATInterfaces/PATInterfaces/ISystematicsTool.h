93b1386802371d6c2e2a07a30b011de6134808ad
Nils Krumnack <krumnack@iastate.edu>
2021-03-03 10:09:59 -0600

    introduce IReentrantSystematicsTool
    This splits up the current ISystematicsTool into two interfaces, one
    part from which tools can inherit if they want to implement
    systematics in a thread-friendly manner, and one which contains the
    current interface and inherits from the former.  See interface
    documentation for more details.

--- PhysicsAnalysis/AnalysisCommon/PATInterfaces/PATInterfaces/ISystematicsTool.h
+++ PhysicsAnalysis/AnalysisCommon/PATInterfaces/PATInterfaces/ISystematicsTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 /// @author Nils Krumnack
@@ -8,7 +8,7 @@
 #ifndef PATINTERFACES_ISYSTEMATICSTOOL_H
 #define PATINTERFACES_ISYSTEMATICSTOOL_H
 
-#include "AsgTools/IAsgTool.h"
+#include "PATInterfaces/IReentrantSystematicsTool.h"
 
 // Local include(s):
 #include "AsgMessaging/StatusCode.h"
@@ -29,27 +29,16 @@ namespace CP {
    /// $Revision: 676499 $
    /// $Date: 2015-06-18 22:13:50 +0200 (Thu, 18 Jun 2015) $
    ///
-   class ISystematicsTool : public virtual asg::IAsgTool {
+   class ISystematicsTool : public virtual IReentrantSystematicsTool {
 
       /// Declare the interface that this class provides
       ASG_TOOL_INTERFACE( CP::ISystematicsTool )
 
    public:
 
-      /// Virtual destructor
-      virtual ~ISystematicsTool(){};
-
       /// returns: whether this tool is affected by the given systematics
       virtual bool
       isAffectedBySystematic( const SystematicVariation& systematic ) const = 0;
-
-      /// returns: the list of all systematics this tool can be affected by
-      virtual SystematicSet
-      affectingSystematics() const = 0;
-
-      /// returns: the list of all systematics this tool recommends to use
-      virtual SystematicSet
-      recommendedSystematics() const = 0;
 
       /// effects: configure this tool for the given list of systematic
       ///   variations.  any requested systematics that are not
