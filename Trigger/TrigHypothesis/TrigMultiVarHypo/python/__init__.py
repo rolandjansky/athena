0bb9cc97fb979b8328fad11ccf9c1cb9bc8c682c
Debottam Bakshi Gupta <debottam.bakshi.gupta@cern.ch>
2021-03-07 10:29:55 +0000

    Removing obsolete run2 codes from Egamma side

--- Trigger/TrigHypothesis/TrigMultiVarHypo/python/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-
-
-# File: TrigMultiVarHypo/__init__.py
-# Author: Joao Victor da Fonseca Pinto
-
-__version__ = '1.0.0'
-__author__  = 'jodafons@cern.ch'
-#__all__ = [ 'TrigL2CaloRingerHypoConfig', 'TrigL2CaloRingerHypoMonitoring' ]
