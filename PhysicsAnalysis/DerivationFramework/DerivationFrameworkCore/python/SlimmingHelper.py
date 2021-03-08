0034e9939f7312732d0e3670dae59339a43e90d5
James Catmore <james.catmore@cern.ch>
2021-03-08 16:26:12 +0000

    Fix DAOD slimming bug which can cause dynamic EventInfo variables to be lost
    ATLASDPD-1570
    
    The EventInfo in the AOD is expressed both as static and dynamic aux variables. The static part is handled in CompulsoryContent.py but this does not address the dynamic part. Left alone all variables in EventInfo are still included via EventInfoAux. , but if a user erroneously tries to add an extra EventInfo item via ExtraVariables, this is the only dynamic variable that will make it into the output file (EventInfoAux.X) which is obviously not intended. This MR addresses this issue by adding a new list to CompulsoryContent into which containers that must be kept but which are expressed as dynamic variables in the AOD be added. The slimming machinery then adds these container names to AllVariables, meaning that (a) all variables are written and (b) if a user accidentally adds one of these variables as ExtraVariables, no harm is done because the overlap removal handles it (as with other AllVariables content). At the same time the extra EventInfo variable in DAOD_PHYS that revealed this bug is removed, as it is superfluous since all variables will be kept.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/SlimmingHelper.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/SlimmingHelper.py
@@ -34,7 +34,7 @@
 # may be unreadable.
 ####################################################################
 
-from DerivationFrameworkCore.CompulsoryContent import CompulsoryContent, CompulsoryTriggerNavigation
+from DerivationFrameworkCore.CompulsoryContent import CompulsoryContent, CompulsoryTriggerNavigation, CompulsoryDynamicContent
 from DerivationFrameworkCore.ContentHandler import ContentHandler
 from DerivationFrameworkCore.ContainersForExpansion import ContainersForExpansion
 from DerivationFrameworkCore.ContainersOnTheFly import ContainersOnTheFly
@@ -118,11 +118,11 @@ class SlimmingHelper:
                         if item.endswith("#*"):
                                 compare_str=item[:-2].replace("xAOD::","")
                                 for m_item in masterList:
-                                        if m_item.startswith(compare_str):
+                                        if m_item.startswith(compare_str) and m_item.replace("Aux.","") not in CompulsoryDynamicContent:
                                                 conflicted_items.append(m_item)
                 if len(conflicted_items)!=0:
-                        msg.error("Smart Slimming lists attempting to add " +str(conflicted_items)+" which are already included in Compulsory content please remove these items from Smart Slimming List")
-                        raise RuntimeError("Conflict in Smart Slimming List and Compulsory Content")
+                        msg.error("Slimming list contains " +str(conflicted_items)+" which are already included in compulsory content: please remove these items from slimming list")
+                        raise RuntimeError("Conflict in Slimming List and Compulsory Content")
 
 
         # The main routine: called by all job options once.
@@ -133,6 +133,7 @@ class SlimmingHelper:
                 # All variables list: where all variables are requested, no variable lists are needed
                 # This list ensures that variables are not added individually in such cases
                 allVariablesList = []
+                self.AllVariables += CompulsoryDynamicContent
                 # Add all-variable collections
                 if len(self.AllVariables)>0:
                         formatName = Stream.Name.strip("Stream_DAOD")
@@ -224,7 +225,6 @@ class SlimmingHelper:
                 # Add processed items to the stream
                 excludedAuxData = "-caloExtension.-cellAssociation.-clusterAssociation" #  From https://svnweb.cern.ch/trac/atlasoff/browser/InnerDetector/InDetExample/InDetRecExample/trunk/share/WriteInDetAOD.py#L41
                 excludedAuxEntries= [entry.strip("-") for entry in excludedAuxData.split(".")]
-
                 for item in mainEntries:
                         Stream.AddItem(item)
                 for item in auxEntries.keys():
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkPhys/share/PHYS.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkPhys/share/PHYS.py
@@ -370,8 +370,7 @@ PHYSSlimmingHelper.ExtraVariables += ["AntiKt10TruthTrimmedPtFrac5SmallR20Jets.T
                                       "AntiKt4EMTopoJets.DFCommonJets_QGTagger_truthjet_nCharged.DFCommonJets_QGTagger_truthjet_pt.DFCommonJets_QGTagger_truthjet_eta.DFCommonJets_QGTagger_NTracks.DFCommonJets_QGTagger_TracksWidth.DFCommonJets_QGTagger_TracksC1.PartonTruthLabelID",
                                       "AntiKt4EMPFlowJets.DFCommonJets_QGTagger_truthjet_nCharged.DFCommonJets_QGTagger_truthjet_pt.DFCommonJets_QGTagger_truthjet_eta.DFCommonJets_QGTagger_NTracks.DFCommonJets_QGTagger_TracksWidth.DFCommonJets_QGTagger_TracksC1.PartonTruthLabelID.DFCommonJets_fJvt",
                                       "TruthPrimaryVertices.t.x.y.z",
-                                      "InDetTrackParticles.TTVA_AMVFVertices.TTVA_AMVFWeights",
-                                      "EventInfo.hardScatterVertexLink"]
+                                      "InDetTrackParticles.TTVA_AMVFVertices.TTVA_AMVFWeights"]
 
 # Add trigger matching
 trigmatching_helper_notau.add_to_slimming(PHYSSlimmingHelper)
