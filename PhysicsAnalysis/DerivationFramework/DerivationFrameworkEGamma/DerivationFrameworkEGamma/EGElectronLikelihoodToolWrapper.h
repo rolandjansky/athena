4df006d19a3fdc2409ebb95ae1f59f33e9698f3f
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2021-03-15 16:26:00 +0000

    EGElectronLikelihoodToolWrapper move to handles and cleanup

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/DerivationFrameworkEGamma/EGElectronLikelihoodToolWrapper.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/DerivationFrameworkEGamma/EGElectronLikelihoodToolWrapper.h
@@ -6,43 +6,62 @@
 // EGLikelihoodToolWrapper.h, (c) ATLAS Detector software
 ///////////////////////////////////////////////////////////////////
 // Author: Giovanni Marchiori (giovanni.marchiori@cern.ch)
-// Note: while EGSelectionToolWrapper permits to only store the boolean 
-// accept and the isEM-like mask (works for both isEM and likelihood selectors), 
+// Note: while EGSelectionToolWrapper permits to only store the boolean
+// accept and the isEM-like mask (works for both isEM and likelihood selectors),
 // this tool (EGElectronLikelihoodToolWrapper) allows also to store
 // the double TResult output (i.e. the value of the likelihood or the ECIDS BDT)
 // if StoreTResult is set to true. Otherwise one can simply use the other tool.
 
-
 #ifndef DERIVATIONFRAMEWORK_EGELECTRONLIKELIHOODTOOLWRAPPER_H
 #define DERIVATIONFRAMEWORK_EGELECTRONLIKELIHOODTOOLWRAPPER_H
-
-#include <string>
-
-#include "AthenaBaseComps/AthAlgTool.h"
 #include "DerivationFrameworkInterfaces/IAugmentationTool.h"
-#include "EgammaAnalysisInterfaces/IElectronPhotonShowerShapeFudgeTool.h"
-#include "EgammaAnalysisInterfaces/IAsgElectronLikelihoodTool.h"
+//
+#include "StoreGate/ReadHandle.h"
+#include "StoreGate/ReadHandleKey.h"
+#include "StoreGate/WriteDecorHandle.h"
+#include "StoreGate/WriteHandleKey.h"
+//
 #include "AsgTools/IAsgTool.h"
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "EgammaAnalysisInterfaces/IAsgElectronLikelihoodTool.h"
+#include "EgammaAnalysisInterfaces/IElectronPhotonShowerShapeFudgeTool.h"
 #include "GaudiKernel/ToolHandle.h"
+#include "xAODEgamma/EgammaContainer.h"
+//
+#include <string>
 
 namespace DerivationFramework {
 
-  class EGElectronLikelihoodToolWrapper : public AthAlgTool, public IAugmentationTool {
-    public: 
-      EGElectronLikelihoodToolWrapper(const std::string& t, const std::string& n, const IInterface* p);
+class EGElectronLikelihoodToolWrapper
+  : public AthAlgTool
+  , public IAugmentationTool
+{
+public:
+  EGElectronLikelihoodToolWrapper(const std::string& t,
+                                  const std::string& n,
+                                  const IInterface* p);
 
-      StatusCode initialize();
-      StatusCode finalize();
-      virtual StatusCode addBranches() const;
+  virtual StatusCode initialize() override final;
+  virtual StatusCode addBranches() const override final;
 
-    private:
-      ToolHandle<IAsgElectronLikelihoodTool> m_tool;
-      ToolHandle<IElectronPhotonShowerShapeFudgeTool> m_fudgeMCTool;
-      std::string m_cut;
-      std::string m_sgName;
-      std::string m_containerName;
-      bool m_storeTResult;
-  }; 
+private:
+  ToolHandle<IAsgElectronLikelihoodTool> m_tool;
+  ToolHandle<IElectronPhotonShowerShapeFudgeTool> m_fudgeMCTool;
+
+  SG::ReadHandleKey<xAOD::EgammaContainer> m_ContainerName{ this,
+                                                            "ContainerName",
+                                                            "",
+                                                            "Input" };
+
+  // Write decoration handle keys
+  // these are not really configuarable
+  SG::WriteDecorHandleKey<xAOD::EgammaContainer> m_decoratorPass{};
+  SG::WriteDecorHandleKey<xAOD::EgammaContainer> m_decoratorIsEM{};
+  SG::WriteDecorHandleKey<xAOD::EgammaContainer> m_decoratorResult{};
+  std::string m_cut;
+  std::string m_sgName;
+  bool m_storeTResult;
+};
 }
 
 #endif // DERIVATIONFRAMEWORK_EGELECTRONLIKELIHOODTOOLWRAPPER_H
