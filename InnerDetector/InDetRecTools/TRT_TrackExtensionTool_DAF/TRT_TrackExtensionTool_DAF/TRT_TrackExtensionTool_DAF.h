bf7db9b903da67cee8eb0eb26d2137df22118bee
Shaun Roe <shaun.roe@cern.ch>
2021-03-22 15:15:21 +0000

    22.0-cleanup-TRT_TrackExtensionTool_DAF

--- InnerDetector/InDetRecTools/TRT_TrackExtensionTool_DAF/TRT_TrackExtensionTool_DAF/TRT_TrackExtensionTool_DAF.h
+++ InnerDetector/InDetRecTools/TRT_TrackExtensionTool_DAF/TRT_TrackExtensionTool_DAF/TRT_TrackExtensionTool_DAF.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 // (c) ATLAS Detector software
 /////////////////////////////////////////////////////////////////////////////////
-//  Concret implementation for base class ITRT_TrackExtensionTool
+//  Concrete implementation for base class ITRT_TrackExtensionTool
 /////////////////////////////////////////////////////////////////////////////////
 // Sebastian Fleischmann
 /////////////////////////////////////////////////////////////////////////////////
@@ -82,7 +82,7 @@ public:
 
     virtual std::vector<const Trk::MeasurementBase*>&
        extendTrack(const EventContext& ctx,
-                   const Trk::TrackParameters&,
+                   const Trk::TrackParameters * pTrackParams,
                    InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
 
     virtual Trk::Track*
@@ -96,7 +96,7 @@ public:
     ///////////////////////////////////////////////////////////////////
 
     virtual Trk::TrackSegment* findSegment(const EventContext& ctx,
-                                           const Trk::TrackParameters&,
+                                           const Trk::TrackParameters *,
                                            InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
 
     ///////////////////////////////////////////////////////////////////
@@ -133,9 +133,8 @@ protected:
        const TRT_DriftCircleContainer               *m_trtcontainer = nullptr;
        std::vector<const Trk::MeasurementBase*>      m_measurement;               //!< vector of MeasurementBase for the output
        std::vector <const InDetDD::TRT_BaseElement*> m_detectorElements;          //!< vector to store the detElements
-       std::vector <const Trk::TrackParameters*>     m_propagatedTrackParameters; //!< vector to store the propagated track parameters (propagated to the related entry of m_detectorElements)
-
-       const Trk::TrackParameters                  *m_siliconTrkParams = nullptr; //!< track parameters at the outermost Silicon layer
+       std::vector <std::shared_ptr< const Trk::TrackParameters>>     m_propagatedTrackParameters; //!< vector to store the propagated track parameters (propagated to the related entry of m_detectorElements)
+       const Trk::TrackParameters                   *m_siliconTrkParams = nullptr; //!< track parameters at the outermost Silicon layer
     };
 
 
@@ -162,9 +161,9 @@ protected:
     ///////////////////////////////////////////////////////////////////
 
     /** find an element-wise extension (ie. the RIOs in a CompROT belong to one detElement) */
-    StatusCode elementWiseExtension(int, int, InDet::TRT_TrackExtensionTool_DAF::EventData &event_data) const;
+    bool elementWiseExtension(int, int, InDet::TRT_TrackExtensionTool_DAF::EventData &event_data) const;
     /** find a barrel extension with RIOs grouped along the globalPositions of the track */
-    StatusCode groupedBarrelExtension(int, int, InDet::TRT_TrackExtensionTool_DAF::EventData &event_data) const;
+    bool groupedBarrelExtension(int, int, InDet::TRT_TrackExtensionTool_DAF::EventData &event_data) const;
 
 };
 
--- InnerDetector/InDetRecTools/TRT_TrackExtensionTool_xk/TRT_TrackExtensionTool_xk/TRT_TrackExtensionTool_xk.h
+++ InnerDetector/InDetRecTools/TRT_TrackExtensionTool_xk/TRT_TrackExtensionTool_xk/TRT_TrackExtensionTool_xk.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -75,9 +75,9 @@ namespace InDet{
       virtual std::vector<const Trk::MeasurementBase*>& extendTrack
         (const EventContext& ctx, const Trk::Track&,InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
       virtual std::vector<const Trk::MeasurementBase*>& extendTrack
-        (const EventContext& ctx, const Trk::TrackParameters&,InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
+        (const EventContext& ctx, const Trk::TrackParameters *,InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
       virtual Trk::TrackSegment* findSegment
-        (const EventContext& ctx, const Trk::TrackParameters&, InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
+        (const EventContext& ctx, const Trk::TrackParameters *, InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
       virtual Trk::Track* newTrack
         (const EventContext& ctx, const Trk::Track&, InDet::ITRT_TrackExtensionTool::IEventData &virt_event_data) const override;
       virtual std::unique_ptr<InDet::ITRT_TrackExtensionTool::IEventData> newEvent(const EventContext& ctx) const override;
@@ -111,7 +111,7 @@ namespace InDet{
 
       std::vector<const Trk::MeasurementBase*>& extendTrackFromParameters
       (const EventContext& ctx,
-       const Trk::TrackParameters&,
+       const Trk::TrackParameters *,
        InDet::TRT_TrackExtensionTool_xk::EventData &event_data) const;
 
       ///////////////////////////////////////////////////////////////////
@@ -156,7 +156,7 @@ namespace InDet{
       void       magneticFieldInit();
       StatusCode magneticFieldInit(IOVSVC_CALLBACK_ARGS);
       bool isGoodExtension(const EventContext& ctx,
-                           const Trk::TrackParameters&,
+                           const Trk::TrackParameters *,
                            InDet::TRT_TrackExtensionTool_xk::EventData &event_data) const;
       bool numberPIXandSCTclustersCut(const Trk::Track&) const;
 
