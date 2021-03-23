c4bad197886c19af4f16ab1dc53164f2a5ba4990
Tadej Novak <tadej.novak@cern.ch>
2021-03-23 12:52:05 +0100

    Make MBTS geometry building configurable

--- LArCalorimeter/LArGeoModel/LArGeoAlgsNV/src/LArDetectorToolNV.h
+++ LArCalorimeter/LArGeoModel/LArGeoAlgsNV/src/LArDetectorToolNV.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 /**
@@ -61,6 +61,7 @@ class LArDetectorToolNV final : public GeoModelTool {
     std::string m_EMECVariantOuter;
 
     bool m_activateFT;
+    bool m_enableMBTS;
 };
 
 #endif
