3a92795f6074197b0e2e3fba45f707853f55e651
Luzhan Yue <luzhan.yue@cern.ch>
2021-03-24 10:03:03 +0000

    Added complete Pythia_8 ART tests

--- Generators/Pythia8_i/test/test_01_Z_tautau.sh
+++ Generators/Pythia8_i/test/test_01_Z_tautau.sh
@@ -5,9 +5,28 @@
 # art-description: Pythia event generation -- Z to tautau
 # art-type: grid
 # art-output: EVNT.root
+# art-output: MyOutput.root
+# art-output: MyOutput.yoda.gz
+# art-output: dcube
+# art-html: dcube
 
-Gen_tf.py --ecmEnergy=13000. --maxEvents=-1 --firstEvent=1 --randomSeed=123456 --outputEVNTFile=EVNT.root --jobConfig=950073
+
+Gen_tf.py --ecmEnergy=13000. --maxEvents=10000 --firstEvent=-1 --randomSeed=123456 --outputEVNTFile=EVNT.root --outputYODAFile=MyOutput.yoda.gz --postInclude=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Pythia8i/JO_MC/JO_MC_Ztt.py --jobConfig=950073
+
 
 echo "art-result:$? Gen_tf"
 
+python /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Pythia8i/rootconvert.py MyOutput.yoda.gz
 
+dcubeName = "Pythia8i"
+dcubeXml="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Pythia8i/test_01_Z_tautau/config_Ztt.xml"
+dcubeRef="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Pythia8i/test_01_Z_tautau/MyOutput.root"
+
+#dcubeName="Pythia8i"
+#dcubeXml="/afs/cern.ch/user/l/lyue//Qualification_task_ART/Pythia_8/DcubeReference/test_01_Z_tautau/config_Ztt.xml"
+#dcubeRef="/afs/cern.ch/user/l/lyue//Qualification_task_ART/Pythia_8/DcubeReference/test_01_Z_tautau/MyOutput.root"
+
+bash /cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube $dcubeName MyOutput.root $dcubeXml $dcubeRef
+
+#yodadiff -o yodadiff MyOutput.yoda.gz ~/Qualification_task_ART/Pythia_8/result/Pythia_8/test_01_Z_tautau/MyOutput.yoda.gz     # use this for histogram comparison if one only care if they are exactly the same
+echo  "art-result: $? Dcube" 
