b0fb9608066cc883dc1f9551ff38ea6ba71b66a7
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2021-03-30 08:51:29 +0200

    Add DRAW_ZMUMU and DESDM_MCP outputs and introduce test_trf_data18_mp.sh as AthenaMP test

--- Tools/TrfTestsART/test/test_trf_data18_11k_mt.sh
+++ Tools/TrfTestsART/test/test_trf_data18_11k_mt.sh
@@ -7,7 +7,10 @@
 
 timeout 64800 Reco_tf.py \
   --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0001.data,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0002.data,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0003.data,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0004.data,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0005.data,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0083._SFO-1._0006.data \
-  --outputAODFile=aod.pool.root --outputHISTFile=hist.root \
+  --outputAODFile=myAOD.pool.root \
+  --outputHISTFile=myHIST.root \
+  --outputDESDM_MCPFile=myDESDM_MCP.pool.root \
+  --outputDRAW_ZMUMUFile=myDRAW_ZMUMU.data \
   --multithreaded='True' \
   --preExec 'all:from AthenaMonitoring.DQMonFlags import DQMonFlags; DQMonFlags.doMonitoring=True; DQMonFlags.doNewMonitoring=True' \
   --postExec 'FPEAuditor.NStacktracesOnFPE=10' \
