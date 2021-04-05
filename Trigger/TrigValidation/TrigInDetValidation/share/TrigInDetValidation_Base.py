8ff37c6311d111f8ac69381dac3e709d9b8903b8
Julie Kirk <julie.kirk@stfc.ac.uk>
2021-04-05 06:22:45 +0000

    TrigInDetValidation: add options to change MALLOC setting

--- Trigger/TrigValidation/TrigInDetValidation/share/TrigInDetValidation_Base.py
+++ Trigger/TrigValidation/TrigInDetValidation/share/TrigInDetValidation_Base.py
@@ -38,6 +38,7 @@ dry_run       = False
 
 if "Art_type"  not in locals(): Art_type = 'grid'
 if "GridFiles" not in locals(): GridFiles=False
+if "Malloc" not in locals(): Malloc=False
 
 for opt,arg in opts:
     if opt in ("-l", "--local"):
@@ -100,7 +101,10 @@ if GridFiles:
        rdo2aod.input = ''
        rdo2aod.args += ' --inputRDOFile=$ArtInFile '
 
-
+if (Malloc):
+    import os
+    os.environ["MALLOC_CHECK_"] = "3"
+    rdo2aod.malloc = True
 
 # Run athena analysis to produce TrkNtuple
 
