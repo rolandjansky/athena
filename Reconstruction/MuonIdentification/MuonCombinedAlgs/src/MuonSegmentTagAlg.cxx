edc2be5f43d95602d5c1b02aca81ca4aa275d14f
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-04-07 14:38:48 +0000

    Reindent Muon combined algs
    (cherry picked from commit c0877d099d348b0d5e3409bc67ee0c13369f7ea0)

--- Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonSegmentTagAlg.cxx
+++ Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonSegmentTagAlg.cxx
@@ -3,50 +3,45 @@
 */
 
 #include "MuonSegmentTagAlg.h"
+
 #include "xAODMuon/MuonSegmentContainer.h"
 
-MuonSegmentTagAlg::MuonSegmentTagAlg(const std::string& name, ISvcLocator* pSvcLocator):
-  AthAlgorithm(name,pSvcLocator) {  
+MuonSegmentTagAlg::MuonSegmentTagAlg(const std::string& name, ISvcLocator* pSvcLocator) : AthAlgorithm(name, pSvcLocator) {}
+
+StatusCode MuonSegmentTagAlg::initialize() {
+    ATH_CHECK(m_muonSegmentTagTool.retrieve());
+    ATH_CHECK(m_indetCandidateCollectionName.initialize());
+    ATH_CHECK(m_muonSegmentCollectionName.initialize());
+    ATH_CHECK(m_tagMap.initialize());
+
+    return StatusCode::SUCCESS;
 }
 
-StatusCode MuonSegmentTagAlg::initialize()
-{
-  
-  ATH_CHECK(m_muonSegmentTagTool.retrieve());
-  ATH_CHECK(m_indetCandidateCollectionName.initialize());
-  ATH_CHECK(m_muonSegmentCollectionName.initialize());
-  ATH_CHECK(m_tagMap.initialize());
-  
-  return StatusCode::SUCCESS; 
-}
+StatusCode MuonSegmentTagAlg::execute() {
+    SG::ReadHandle<InDetCandidateCollection> indetCandidateCollection(m_indetCandidateCollectionName);
+    if (!indetCandidateCollection.isValid()) {
+        ATH_MSG_ERROR("Could not read " << m_indetCandidateCollectionName);
+        return StatusCode::FAILURE;
+    }
+    if (!indetCandidateCollection.isPresent()) {
+        ATH_MSG_WARNING(m_indetCandidateCollectionName << " not found in StoreGate");
+        return StatusCode::SUCCESS;
+    }
 
-StatusCode MuonSegmentTagAlg::execute()
-{
+    SG::ReadHandle<xAOD::MuonSegmentContainer> segments(m_muonSegmentCollectionName);
+    if (!segments.isPresent()) {
+        ATH_MSG_WARNING(m_muonSegmentCollectionName << " not found in StoreGate");
+        return StatusCode::SUCCESS;
+    }
+    if (!segments.isValid()) {
+        ATH_MSG_FATAL("Unable to retrieve " << m_muonSegmentCollectionName);
+        return StatusCode::FAILURE;
+    }
 
-  SG::ReadHandle<InDetCandidateCollection> indetCandidateCollection(m_indetCandidateCollectionName);
-  if(!indetCandidateCollection.isValid()){
-    ATH_MSG_ERROR("Could not read "<< m_indetCandidateCollectionName);
-    return StatusCode::FAILURE;
-  }
-  if(!indetCandidateCollection.isPresent()){
-    ATH_MSG_WARNING(m_indetCandidateCollectionName<<" not found in StoreGate");
+    SG::WriteHandle<MuonCombined::InDetCandidateToTagMap> tagMap(m_tagMap);
+    ATH_CHECK(tagMap.record(std::make_unique<MuonCombined::InDetCandidateToTagMap>()));
+
+    m_muonSegmentTagTool->tag(*indetCandidateCollection, *segments, tagMap.ptr());
+
     return StatusCode::SUCCESS;
-  }
-
-  SG::ReadHandle<xAOD::MuonSegmentContainer>segments(m_muonSegmentCollectionName);
-  if(!segments.isPresent()){
-    ATH_MSG_WARNING(m_muonSegmentCollectionName<<" not found in StoreGate");
-    return StatusCode::SUCCESS;
-  }
-  if(!segments.isValid()){
-    ATH_MSG_FATAL( "Unable to retrieve " << m_muonSegmentCollectionName );
-    return StatusCode::FAILURE;
-  }
-
-  SG::WriteHandle<MuonCombined::InDetCandidateToTagMap> tagMap(m_tagMap);
-  ATH_CHECK( tagMap.record (std::make_unique<MuonCombined::InDetCandidateToTagMap>()) );
-
-  m_muonSegmentTagTool->tag(*indetCandidateCollection,*segments,tagMap.ptr());
-  
-  return StatusCode::SUCCESS;
 }
