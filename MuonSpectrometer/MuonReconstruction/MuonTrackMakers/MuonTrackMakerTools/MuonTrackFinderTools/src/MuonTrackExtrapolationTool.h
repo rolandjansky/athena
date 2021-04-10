674e7d03ebe47b52eb3997160e48ad3363390634
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-04-10 15:43:50 +0200

    Reindentation of MuonTrackFinderTools

--- MuonSpectrometer/MuonReconstruction/MuonTrackMakers/MuonTrackMakerTools/MuonTrackFinderTools/src/MuonTrackExtrapolationTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonTrackMakers/MuonTrackMakerTools/MuonTrackFinderTools/src/MuonTrackExtrapolationTool.h
@@ -5,22 +5,19 @@
 #ifndef MUON_MUONTRACKEXTRAPOLATIONTOOL_H
 #define MUON_MUONTRACKEXTRAPOLATIONTOOL_H
 
+#include "AthenaBaseComps/AthAlgTool.h"
 #include "GaudiKernel/ServiceHandle.h"
 #include "GaudiKernel/ToolHandle.h"
-#include "AthenaBaseComps/AthAlgTool.h"
-
+#include "MuonIdHelpers/IMuonIdHelperSvc.h"
 #include "MuonRecHelperTools/IMuonEDMHelperSvc.h"
 #include "MuonRecHelperTools/MuonEDMPrinterTool.h"
-#include "MuonIdHelpers/IMuonIdHelperSvc.h"
-
 #include "MuonRecToolInterfaces/IMuonTrackExtrapolationTool.h"
 #include "TrkDetDescrInterfaces/ITrackingGeometrySvc.h"
-#include "TrkGeometry/TrackingGeometry.h"
 #include "TrkExInterfaces/IExtrapolator.h"
+#include "TrkGeometry/TrackingGeometry.h"
 
 // For magneticfield
 #include "MagFieldConditions/AtlasFieldCacheCondObj.h"
-
 #include "TrkParameters/TrackParameters.h"
 
 namespace Muon {
@@ -29,79 +26,84 @@ namespace Muon {
      @brief tool to select tracks
 
      */
-    class MuonTrackExtrapolationTool: virtual public IMuonTrackExtrapolationTool, public AthAlgTool {
-        public:
-            /** @brief constructor */
-            MuonTrackExtrapolationTool(const std::string&, const std::string&, const IInterface*);
+    class MuonTrackExtrapolationTool : virtual public IMuonTrackExtrapolationTool, public AthAlgTool {
+    public:
+        /** @brief constructor */
+        MuonTrackExtrapolationTool(const std::string &, const std::string &, const IInterface *);
 
-            /** @brief destructor */
-            virtual ~MuonTrackExtrapolationTool() = default;
+        /** @brief destructor */
+        virtual ~MuonTrackExtrapolationTool() = default;
 
-            /** @brief AlgTool initilize */
-            virtual StatusCode initialize() override;
+        /** @brief AlgTool initilize */
+        virtual StatusCode initialize() override;
 
-            /** extrapolates track parameters to muon entry record, will return a zero pointer if the extrapolation fails. The caller gets ownership of the new parameters */
-            const Trk::TrackParameters* extrapolateToMuonEntryRecord(const Trk::TrackParameters &pars, Trk::ParticleHypothesis particleHypo = Trk::muon) const;
+        /** extrapolates track parameters to muon entry record, will return a zero pointer if the extrapolation fails. The caller gets
+         * ownership of the new parameters */
+        const Trk::TrackParameters *extrapolateToMuonEntryRecord(const Trk::TrackParameters &pars,
+                                                                 Trk::ParticleHypothesis particleHypo = Trk::muon) const;
 
-            /** extrapolates track parameters to muon entry record, will return a zero pointer if the extrapolation fails. The caller gets ownership of the new parameters */
-            virtual const Trk::TrackParameters* extrapolateToIP(const Trk::TrackParameters &pars, Trk::ParticleHypothesis particleHypo = Trk::muon) const;
+        /** extrapolates track parameters to muon entry record, will return a zero pointer if the extrapolation fails. The caller gets
+         * ownership of the new parameters */
+        virtual const Trk::TrackParameters *extrapolateToIP(const Trk::TrackParameters &pars,
+                                                            Trk::ParticleHypothesis particleHypo = Trk::muon) const;
 
-            /** @brief extrapolates a muon track to the muon entry record and returns a new track expressed at the destination.
-             @param track input track
-             @return a pointer to the extrapolated track, zero if extrapolation failed.
-             The ownership of the track is passed to the client calling the tool.
-             */
-            virtual Trk::Track* extrapolate(const Trk::Track &track) const override;
+        /** @brief extrapolates a muon track to the muon entry record and returns a new track expressed at the destination.
+         @param track input track
+         @return a pointer to the extrapolated track, zero if extrapolation failed.
+         The ownership of the track is passed to the client calling the tool.
+         */
+        virtual Trk::Track *extrapolate(const Trk::Track &track) const override;
 
-            /** @brief extrapolates a muon track collection to the muon entry record and returns a new track expressed at the destination.
-             @param tracks input track collection
-             @return a pointer to the extrapolated track collection, zero if extrapolation failed.
-             The ownership of the collection is passed to the client calling the tool.
-             */
-            virtual TrackCollection* extrapolate(const TrackCollection &tracks) const override;
+        /** @brief extrapolates a muon track collection to the muon entry record and returns a new track expressed at the destination.
+         @param tracks input track collection
+         @return a pointer to the extrapolated track collection, zero if extrapolation failed.
+         The ownership of the collection is passed to the client calling the tool.
+         */
+        virtual TrackCollection *extrapolate(const TrackCollection &tracks) const override;
 
-        private:
+    private:
+        bool getMuonEntrance() const;
+        bool retrieveTrackingGeometry() const;
 
-            bool getMuonEntrance() const;
-            bool retrieveTrackingGeometry() const;
+        double estimateDistanceToEntryRecord(const Trk::TrackParameters &pars) const;
+        const Trk::TrackParameters *checkForSecondCrossing(const Trk::TrackParameters &firstCrossing, const Trk::Track &track) const;
+        const Trk::TrackParameters *findClosestParametersToMuonEntry(const Trk::Track &track) const;
 
-            double estimateDistanceToEntryRecord(const Trk::TrackParameters &pars) const;
-            const Trk::TrackParameters* checkForSecondCrossing(const Trk::TrackParameters &firstCrossing, const Trk::Track &track) const;
-            const Trk::TrackParameters* findClosestParametersToMuonEntry(const Trk::Track &track) const;
+        const Trk::Perigee *createPerigee(const Trk::TrackParameters &pars) const;
 
-            const Trk::Perigee* createPerigee(const Trk::TrackParameters &pars) const;
+        // Read handle for conditions object to get the field cache
+        SG::ReadCondHandleKey<AtlasFieldCacheCondObj> m_fieldCacheCondObjInputKey{this, "AtlasFieldCacheCondObj", "fieldCondObj",
+                                                                                  "Name of the Magnetic Field conditions object key"};
 
-            // Read handle for conditions object to get the field cache
-            SG::ReadCondHandleKey<AtlasFieldCacheCondObj> m_fieldCacheCondObjInputKey { this, "AtlasFieldCacheCondObj", "fieldCondObj", "Name of the Magnetic Field conditions object key" };
+        ServiceHandle<Trk::ITrackingGeometrySvc> m_trackingGeometrySvc{this, "TrackingGeometrySvc",
+                                                                       "TrackingGeometrySvc/AtlasTrackingGeometrySvc"};
+        SG::ReadCondHandleKey<Trk::TrackingGeometry> m_trackingGeometryReadKey{this, "TrackingGeometryReadKey", "",
+                                                                               "Key of input TrackingGeometry"};
+        ServiceHandle<Muon::IMuonEDMHelperSvc> m_edmHelperSvc{this, "edmHelper", "Muon::MuonEDMHelperSvc/MuonEDMHelperSvc",
+                                                              "Handle to the service providing the IMuonEDMHelperSvc interface"};
+        ServiceHandle<Muon::IMuonIdHelperSvc> m_idHelperSvc{this, "MuonIdHelperSvc", "Muon::MuonIdHelperSvc/MuonIdHelperSvc"};
 
-            ServiceHandle<Trk::ITrackingGeometrySvc> m_trackingGeometrySvc { this, "TrackingGeometrySvc", "TrackingGeometrySvc/AtlasTrackingGeometrySvc" };
-            SG::ReadCondHandleKey<Trk::TrackingGeometry> m_trackingGeometryReadKey { this, "TrackingGeometryReadKey", "", "Key of input TrackingGeometry" };
-            ServiceHandle<Muon::IMuonEDMHelperSvc> m_edmHelperSvc { this, "edmHelper", "Muon::MuonEDMHelperSvc/MuonEDMHelperSvc", "Handle to the service providing the IMuonEDMHelperSvc interface" };
-            ServiceHandle<Muon::IMuonIdHelperSvc> m_idHelperSvc { this, "MuonIdHelperSvc", "Muon::MuonIdHelperSvc/MuonIdHelperSvc" };
+        ToolHandle<Muon::MuonEDMPrinterTool> m_printer{this, "EDMPrinter", "Muon::MuonEDMPrinterTool/MuonEDMPrinterTool",
+                                                       "helper to nicely print out tracks"};
+        ToolHandle<Trk::IExtrapolator> m_atlasExtrapolator{this, "AtlasExtrapolator", "Trk::Extrapolator/AtlasExtrapolator"};
+        ToolHandle<Trk::IExtrapolator> m_muonExtrapolator{this, "MuonExtrapolator", "Trk::Extrapolator/MuonExtrapolator"};
+        ToolHandle<Trk::IExtrapolator> m_muonExtrapolator2{this, "MuonExtrapolator2", "Trk::Extrapolator/MuonExtrapolator"};
 
-            ToolHandle<Muon::MuonEDMPrinterTool> m_printer { this, "EDMPrinter", "Muon::MuonEDMPrinterTool/MuonEDMPrinterTool", "helper to nicely print out tracks" };
-            ToolHandle<Trk::IExtrapolator> m_atlasExtrapolator { this, "AtlasExtrapolator", "Trk::Extrapolator/AtlasExtrapolator" };
-            ToolHandle<Trk::IExtrapolator> m_muonExtrapolator { this, "MuonExtrapolator", "Trk::Extrapolator/MuonExtrapolator" };
-            ToolHandle<Trk::IExtrapolator> m_muonExtrapolator2 { this, "MuonExtrapolator2", "Trk::Extrapolator/MuonExtrapolator" };
+        Gaudi::Property<bool> m_cosmics{this, "Cosmics", false};
+        Gaudi::Property<bool> m_keepOldPerigee{this, "KeepInitialPerigee", true};
+        Gaudi::Property<std::string> m_msEntranceName{this, "MuonSystemEntranceName", "MuonSpectrometerEntrance"};
 
-            Gaudi::Property<bool> m_cosmics { this, "Cosmics", false };
-            Gaudi::Property<bool> m_keepOldPerigee { this, "KeepInitialPerigee", true };
-            Gaudi::Property<std::string> m_msEntranceName { this, "MuonSystemEntranceName", "MuonSpectrometerEntrance" };
-
-            inline const Trk::TrackingVolume* getVolume(const std::string &vol_name) const {
-                /// Good old way of retrieving the volume via the geometry service
-                if (m_trackingGeometryReadKey.empty()) {
-                    return m_trackingGeometrySvc->trackingGeometry()->trackingVolume(vol_name);
-                }
-                SG::ReadCondHandle < Trk::TrackingGeometry > handle(m_trackingGeometryReadKey, Gaudi::Hive::currentContext());
-                if (!handle.isValid()) {
-                    ATH_MSG_WARNING("Could not retrieve a valid tracking geometry");
-                    return nullptr;
-                }
-                return handle.cptr()->trackingVolume(vol_name);
+        inline const Trk::TrackingVolume *getVolume(const std::string &vol_name) const {
+            /// Good old way of retrieving the volume via the geometry service
+            if (m_trackingGeometryReadKey.empty()) { return m_trackingGeometrySvc->trackingGeometry()->trackingVolume(vol_name); }
+            SG::ReadCondHandle<Trk::TrackingGeometry> handle(m_trackingGeometryReadKey, Gaudi::Hive::currentContext());
+            if (!handle.isValid()) {
+                ATH_MSG_WARNING("Could not retrieve a valid tracking geometry");
+                return nullptr;
             }
-
+            return handle.cptr()->trackingVolume(vol_name);
+        }
     };
-}
+}  // namespace Muon
 
 #endif
--- MuonSpectrometer/MuonReconstruction/MuonTrackMakers/MuonTrackMakerTools/MuonTrackFinderTools/src/MuonTrackToSegmentTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonTrackMakers/MuonTrackMakerTools/MuonTrackFinderTools/src/MuonTrackToSegmentTool.h
@@ -2,89 +2,83 @@
   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #ifndef MUON_MUONTRACKTOSEGMENTTOOL_H
 #define MUON_MUONTRACKTOSEGMENTTOOL_H
+
+#include <vector>
 
 #include "AthenaBaseComps/AthAlgTool.h"
 #include "GaudiKernel/ServiceHandle.h"
 #include "GaudiKernel/ToolHandle.h"
-
-#include "MuonRecToolInterfaces/IMuonTrackToSegmentTool.h"
-
-#include "MuonRecHelperTools/IMuonEDMHelperSvc.h"
 #include "MuonCondData/MdtCondDbData.h"
-#include "MuonReadoutGeometry/MuonDetectorManager.h"
-#include "MuonRecHelperTools/MuonEDMPrinterTool.h"
 #include "MuonIdHelpers/IMuonIdHelperSvc.h"
-#include "TrkExInterfaces/IPropagator.h"
+#include "MuonReadoutGeometry/MuonDetectorManager.h"
+#include "MuonRecHelperTools/IMuonEDMHelperSvc.h"
+#include "MuonRecHelperTools/MuonEDMPrinterTool.h"
+#include "MuonRecToolInterfaces/IMuonTrackToSegmentTool.h"
 #include "MuonStationIntersectSvc/MuonStationIntersectSvc.h"
-
+#include "TrkExInterfaces/IPropagator.h"
 #include "TrkParameters/TrackParameters.h"
 
-#include <vector>
-
 namespace Trk {
-  class Track;
-  class MeasurementBase;
+    class Track;
+    class MeasurementBase;
+}  // namespace Trk
+
+namespace Muon {
+    class MuonSegment;
 }
 
 namespace Muon {
-  class MuonSegment;
-}
 
-namespace Muon {
+    /**
+        @class MuonTrackToSegmentTool
 
-  /** 
-      @class MuonTrackToSegmentTool
+        Tool to combine two segments. It appoximates the combined segment by a straight line.
+        The tool assumes that this is a valid assumption.
 
-      Tool to combine two segments. It appoximates the combined segment by a straight line. 
-      The tool assumes that this is a valid assumption.
-      
-      For more details look at the mainpage of this package.
-  */
-  class MuonTrackToSegmentTool :  virtual public IMuonTrackToSegmentTool, public AthAlgTool   {
-  public:
-    typedef std::vector<const Trk::MeasurementBase*> MeasVec;
-    typedef MeasVec::iterator                        MeasIt;
-    typedef MeasVec::const_iterator                  MeasCit;
+        For more details look at the mainpage of this package.
+    */
+    class MuonTrackToSegmentTool : virtual public IMuonTrackToSegmentTool, public AthAlgTool {
+    public:
+        typedef std::vector<const Trk::MeasurementBase*> MeasVec;
+        typedef MeasVec::iterator MeasIt;
+        typedef MeasVec::const_iterator MeasCit;
 
-  public:
-    /** default AlgTool constructor */
-    MuonTrackToSegmentTool(const std::string&, const std::string&, const IInterface*);
-    
-    /** destructor */
-    ~MuonTrackToSegmentTool()=default;
-    
-    /** initialize method, method taken from bass-class AlgTool */
-    StatusCode initialize();
-    
-    /** @brief convert track to segment */
-    MuonSegment* convert( const Trk::Track& track ) const;
+    public:
+        /** default AlgTool constructor */
+        MuonTrackToSegmentTool(const std::string&, const std::string&, const IInterface*);
 
-  private:
-    /** @brief calculate holes */
-    std::vector<Identifier> calculateHoles( const Identifier& chid, const Trk::TrackParameters& pars, const MeasVec& measurements ) const;
+        /** destructor */
+        ~MuonTrackToSegmentTool() = default;
 
-    SG::ReadCondHandleKey<MuonGM::MuonDetectorManager> m_DetectorManagerKey {this, "DetectorManagerKey", 
-	"MuonDetectorManager", 
-	"Key of input MuonDetectorManager condition data"};    
+        /** initialize method, method taken from bass-class AlgTool */
+        StatusCode initialize();
 
-    ServiceHandle<MuonStationIntersectSvc> m_intersectSvc {this, "MuonStationIntersectSvc", "MuonStationIntersectSvc", "pointer to hole search service"};
-    ServiceHandle<Muon::IMuonIdHelperSvc> m_idHelperSvc {this, "MuonIdHelperSvc", "Muon::MuonIdHelperSvc/MuonIdHelperSvc"};
-    ServiceHandle<IMuonEDMHelperSvc>    m_edmHelperSvc {this, "edmHelper", 
-      "Muon::MuonEDMHelperSvc/MuonEDMHelperSvc", 
-      "Handle to the service providing the IMuonEDMHelperSvc interface" };       //<! multipurpose helper tool
-    
-    ToolHandle<MuonEDMPrinterTool> m_printer {this, "EDMPrinter", "Muon::MuonEDMPrinterTool/MuonEDMPrinterTool", "helper to nicely print out tracks"};
-    ToolHandle<Trk::IPropagator> m_propagator {this, "Propagator", "Trk::RungeKuttaPropagator/AtlasRungeKuttaPropagator"};
+        /** @brief convert track to segment */
+        MuonSegment* convert(const Trk::Track& track) const;
 
-    SG::ReadCondHandleKey<MdtCondDbData> m_condKey{this, "MdtCondKey", "MdtCondDbData", "Key of MdtCondDbData"};
+    private:
+        /** @brief calculate holes */
+        std::vector<Identifier> calculateHoles(const Identifier& chid, const Trk::TrackParameters& pars, const MeasVec& measurements) const;
 
-  };
+        SG::ReadCondHandleKey<MuonGM::MuonDetectorManager> m_DetectorManagerKey{this, "DetectorManagerKey", "MuonDetectorManager",
+                                                                                "Key of input MuonDetectorManager condition data"};
 
+        ServiceHandle<MuonStationIntersectSvc> m_intersectSvc{this, "MuonStationIntersectSvc", "MuonStationIntersectSvc",
+                                                              "pointer to hole search service"};
+        ServiceHandle<Muon::IMuonIdHelperSvc> m_idHelperSvc{this, "MuonIdHelperSvc", "Muon::MuonIdHelperSvc/MuonIdHelperSvc"};
+        ServiceHandle<IMuonEDMHelperSvc> m_edmHelperSvc{
+            this, "edmHelper", "Muon::MuonEDMHelperSvc/MuonEDMHelperSvc",
+            "Handle to the service providing the IMuonEDMHelperSvc interface"};  //<! multipurpose helper tool
 
-}
+        ToolHandle<MuonEDMPrinterTool> m_printer{this, "EDMPrinter", "Muon::MuonEDMPrinterTool/MuonEDMPrinterTool",
+                                                 "helper to nicely print out tracks"};
+        ToolHandle<Trk::IPropagator> m_propagator{this, "Propagator", "Trk::RungeKuttaPropagator/AtlasRungeKuttaPropagator"};
 
+        SG::ReadCondHandleKey<MdtCondDbData> m_condKey{this, "MdtCondKey", "MdtCondDbData", "Key of MdtCondDbData"};
+    };
+
+}  // namespace Muon
 
 #endif
