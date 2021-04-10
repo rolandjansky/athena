4b9ec49b34cdaa60dc24a7850316681fe594b549
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-04-10 15:45:34 +0200

    Reindentation of MuonRecToolInterfaces

--- MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonRecToolInterfaces/MuonRecToolInterfaces/IMuonHoleRecoveryTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecTools/MuonRecToolInterfaces/MuonRecToolInterfaces/IMuonHoleRecoveryTool.h
@@ -4,39 +4,35 @@
 
 #ifndef MUON_IMUONHOLERECOVERYTOOL_H
 #define MUON_IMUONHOLERECOVERYTOOL_H
- 
-#include "GaudiKernel/IAlgTool.h"
 
 #include <vector>
 
+#include "GaudiKernel/IAlgTool.h"
 
 namespace Trk {
-  class Track;
+    class Track;
 }
 
 namespace Muon {
 
-  /** @brief The IMuonHoleRecoveryTool is a pure virtual interface for tools that recover hole on a track */
-  class IMuonHoleRecoveryTool : virtual public IAlgTool 
-  {      
+    /** @brief The IMuonHoleRecoveryTool is a pure virtual interface for tools that recover hole on a track */
+    class IMuonHoleRecoveryTool : virtual public IAlgTool {
     public:
-    /** access to tool interface */
-    static const InterfaceID& interfaceID(){
-        static const InterfaceID IID_IMuonHoleRecoveryTool("Muon::IMuonHoleRecoveryTool",1,0);
-        return IID_IMuonHoleRecoveryTool;
-    }
+        /** access to tool interface */
+        static const InterfaceID& interfaceID() {
+            static const InterfaceID IID_IMuonHoleRecoveryTool("Muon::IMuonHoleRecoveryTool", 1, 0);
+            return IID_IMuonHoleRecoveryTool;
+        }
 
+        /** @brief recover missing hits (holes) on tracks
+            @return a pointer to a track, the ownership of the output track is passed to the client calling the tool.
+        */
+        virtual Trk::Track* recover(const Trk::Track& track) const = 0;
 
-    /** @brief recover missing hits (holes) on tracks
-	@return a pointer to a track, the ownership of the output track is passed to the client calling the tool.
-    */
-    virtual Trk::Track* recover( const Trk::Track& track ) const = 0;
+        /** virtual destructor **/
+        virtual ~IMuonHoleRecoveryTool() = default;
+    };
 
-    /** virtual destructor **/
-    virtual ~IMuonHoleRecoveryTool() = default;
-  };
- 
-} // end of name space
+}  // namespace Muon
 
-#endif 
-
+#endif
