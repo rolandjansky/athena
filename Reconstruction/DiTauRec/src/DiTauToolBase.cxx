c3990f15fce14244e596b8c4bfa2cfb9d16c09af
Bertrand Martin Dit Latour <bertrand.martindl@cern.ch>
2021-04-14 13:59:22 +0000

    DiTauRec: minor code cleanup
    Hello,
    
    This MR is doing some DiTauRec code cleanup and indentation.
    It does not change the reconstruction output.
    
    Cheers,
    Bertrand

--- Reconstruction/DiTauRec/src/DiTauToolBase.cxx
+++ Reconstruction/DiTauRec/src/DiTauToolBase.cxx
@@ -1,103 +1,87 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 //-----------------------------------------------------------------------------
- // original file: TauBuilderHelper.cxx
- // package:     Reconstruction/tauEvent
- // authors:     Lukasz Janyst
- // date:        2007-02-13
- //
- //  MODIFICATIONS
- // 2008-04-22: (nicom) moved setObject()/getObject() to TauCandidateData
- //-----------------------------------------------------------------------------
+// original file: TauBuilderHelper.cxx
+// package:     Reconstruction/tauEvent
+// authors:     Lukasz Janyst
+// date:        2007-02-13
+//
+//  MODIFICATIONS
+// 2008-04-22: (nicom) moved setObject()/getObject() to TauCandidateData
+//-----------------------------------------------------------------------------
  
- #include "DiTauRec/DiTauToolBase.h"
+#include "DiTauRec/DiTauToolBase.h"
  
- static const InterfaceID DiTauToolBaseID( "DiTauToolBase", 1, 0 );
+static const InterfaceID DiTauToolBaseID( "DiTauToolBase", 1, 0 );
  
- const InterfaceID& DiTauToolBase::interfaceID() {
-     return DiTauToolBaseID;
- }
- 
- 
- //-------------------------------------------------------------------------
- // Constructor
- //-------------------------------------------------------------------------
- DiTauToolBase :: DiTauToolBase( const std::string &type,
-         const std::string &name,
-         const IInterface  *parent ):
-     AthAlgTool(type, name, parent)
- {
- }
- 
- //-------------------------------------------------------------------------
- // Destructor
- //-------------------------------------------------------------------------
- DiTauToolBase :: ~DiTauToolBase()
- {
- }
- 
- //-------------------------------------------------------------------------
- // Initializer
- //-------------------------------------------------------------------------
- StatusCode DiTauToolBase :: initialize()
- {
-     return StatusCode :: SUCCESS;
- }
- 
- //-------------------------------------------------------------------------
- // Initializer
- //-------------------------------------------------------------------------
- StatusCode DiTauToolBase :: eventInitialize( DiTauCandidateData *)
- {
-     return StatusCode :: SUCCESS;
- }
- 
- //-------------------------------------------------------------------------
- // Execute
- //-------------------------------------------------------------------------
- StatusCode DiTauToolBase :: execute( DiTauCandidateData *,
-                                      const EventContext& /*ctx*/) const
- {
-     return StatusCode :: SUCCESS;
- }
- 
- //-------------------------------------------------------------------------
- // Cleanup
- //-------------------------------------------------------------------------
- void DiTauToolBase :: cleanup( DiTauCandidateData * )
- {
- }
- 
- //-------------------------------------------------------------------------
- // Finalizer
- //-------------------------------------------------------------------------
- StatusCode DiTauToolBase :: finalize()
- {
-     return StatusCode :: SUCCESS;
- }
+const InterfaceID& DiTauToolBase::interfaceID() {
+  return DiTauToolBaseID;
+}
  
  
- // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- // Helpers
- template <class T>
- bool DiTauToolBase::openContainer(T* &container, std::string containerName, bool printFATAL) {
-     StatusCode sc = evtStore()->retrieve(container, containerName);
-     if (sc.isFailure() || !container) {
-       if (printFATAL) ATH_MSG_FATAL("Container (" << containerName << ") not found in StoreGate");
-       return 0;
-     }
-     return container;
- }
+DiTauToolBase::DiTauToolBase( const std::string &type,
+			      const std::string &name,
+			      const IInterface *parent ):
+  AthAlgTool(type, name, parent)
+{
+}
  
- template <class T>
- bool DiTauToolBase::retrieveTool(T & tool) {
-     if (tool.retrieve().isFailure()) {
-         ATH_MSG_FATAL("Failed to retrieve tool " << tool);
-         return false;
-     } else {
-         ATH_MSG_VERBOSE("Retrieved tool " << tool);
-     }
-     return true;
- }
+
+DiTauToolBase::~DiTauToolBase()
+{
+}
+ 
+
+StatusCode DiTauToolBase::initialize()
+{
+  return StatusCode::SUCCESS;
+}
+ 
+
+StatusCode DiTauToolBase::eventInitialize( DiTauCandidateData *)
+{
+  return StatusCode::SUCCESS;
+}
+ 
+
+StatusCode DiTauToolBase::execute( DiTauCandidateData *,
+				   const EventContext& /*ctx*/) const
+{
+  return StatusCode::SUCCESS;
+}
+
+ 
+void DiTauToolBase::cleanup( DiTauCandidateData * )
+{
+}
+ 
+
+StatusCode DiTauToolBase::finalize()
+{
+  return StatusCode::SUCCESS;
+}
+ 
+ 
+// Helpers
+template <class T>
+bool DiTauToolBase::openContainer(T* &container, std::string containerName, bool printFATAL) {
+  StatusCode sc = evtStore()->retrieve(container, containerName);
+  if (sc.isFailure() || !container) {
+    if (printFATAL) ATH_MSG_FATAL("Container (" << containerName << ") not found in StoreGate");
+    return 0;
+  }
+  return container;
+}
+ 
+template <class T>
+bool DiTauToolBase::retrieveTool(T & tool) {
+  if (tool.retrieve().isFailure()) {
+    ATH_MSG_FATAL("Failed to retrieve tool " << tool);
+    return false;
+  } else {
+    ATH_MSG_VERBOSE("Retrieved tool " << tool);
+  }
+  return true;
+}
