d59baab7434586cc31b9c9b724c62d375b0874d0
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-04-15 09:31:53 +0200

    Reindentation of MuonLayerEvent

--- MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonLayerEvent/MuonLayerEvent/MuonLayerSurface.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecEvent/MuonLayerEvent/MuonLayerEvent/MuonLayerSurface.h
@@ -5,28 +5,30 @@
 #ifndef MUON_MUONLAYERSURFACE_H
 #define MUON_MUONLAYERSURFACE_H
 
-
 #include "MuonStationIndex/MuonStationIndex.h"
 
 namespace Muon {
-  
-  /** types */
-  struct MuonLayerSurface {
-    typedef std::shared_ptr<const Trk::Surface> SurfacePtr;
 
-    MuonLayerSurface() :
-      surfacePtr(nullptr), sector(-1), regionIndex(MuonStationIndex::DetectorRegionUnknown), layerIndex(MuonStationIndex::LayerUnknown) {}
-    
-    MuonLayerSurface( SurfacePtr surfacePtr_, int sector_, MuonStationIndex::DetectorRegionIndex regionIndex_, MuonStationIndex::LayerIndex layerIndex_ ) : 
-      surfacePtr(surfacePtr_), sector(sector_), regionIndex(regionIndex_), layerIndex(layerIndex_) {}
-    
-    SurfacePtr surfacePtr;
-    int sector;
-    MuonStationIndex::DetectorRegionIndex regionIndex;
-    MuonStationIndex::LayerIndex layerIndex;
-  };
-  
-}
+    /** types */
+    struct MuonLayerSurface {
+        typedef std::shared_ptr<const Trk::Surface> SurfacePtr;
+
+        MuonLayerSurface() :
+            surfacePtr(nullptr),
+            sector(-1),
+            regionIndex(MuonStationIndex::DetectorRegionUnknown),
+            layerIndex(MuonStationIndex::LayerUnknown) {}
+
+        MuonLayerSurface(SurfacePtr surfacePtr_, int sector_, MuonStationIndex::DetectorRegionIndex regionIndex_,
+                         MuonStationIndex::LayerIndex layerIndex_) :
+            surfacePtr(surfacePtr_), sector(sector_), regionIndex(regionIndex_), layerIndex(layerIndex_) {}
+
+        SurfacePtr surfacePtr;
+        int sector;
+        MuonStationIndex::DetectorRegionIndex regionIndex;
+        MuonStationIndex::LayerIndex layerIndex;
+    };
+
+}  // namespace Muon
 
 #endif
-
