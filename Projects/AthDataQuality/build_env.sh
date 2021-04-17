8a164a70e31901b80ed121aeb7ad450544027e3c
Tadej Novak <tadej.novak@cern.ch>
2021-04-17 11:18:48 +0200

    Improve ZSH support in build scripts

--- Projects/AthDataQuality/build_env.sh
+++ Projects/AthDataQuality/build_env.sh
@@ -5,7 +5,13 @@
 
 # Set up the variables necessary for the script doing the heavy lifting.
 ATLAS_PROJECT_NAME="AthDataQuality"
-ATLAS_PROJECT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+if [ "x${BASH_SOURCE[0]}" = "x" ]; then
+  # Zsh fallback
+  ATLAS_PROJECT_DIR=$(cd "$(dirname "$(print -P %x)")" && pwd)
+else
+  # Bash
+  ATLAS_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+fi
 
 # Let "the common script" do all the heavy lifting.
 source "${ATLAS_PROJECT_DIR}/../../Build/AtlasBuildScripts/setup_project_build_env.sh"
