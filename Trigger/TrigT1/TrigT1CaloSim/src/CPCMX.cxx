5b772a2324728f6a6ce247723a0f506442a0bf97
Stewart Martin-Haugh <smh@cern.ch>
2021-04-20 18:26:59 +0200

    Migrate CPCMX to DataHandles, ATR-17916

--- Trigger/TrigT1/TrigT1CaloSim/src/CPCMX.cxx
+++ Trigger/TrigT1/TrigT1CaloSim/src/CPCMX.cxx
@@ -54,25 +54,7 @@ const int CPCMX::s_SourceTotal;
 
 CPCMX::CPCMX
   ( const std::string& name, ISvcLocator* pSvcLocator )
-    : AthAlgorithm( name, pSvcLocator ), 
-      m_configSvc("TrigConf::LVL1ConfigSvc/LVL1ConfigSvc", name),
-      m_emTauCTP(0)
-{
-    m_CMXCPHitLocation      = TrigT1CaloDefs::CMXCPHitsLocation;
-    m_CMXCPTobLocation      = TrigT1CaloDefs::CMXCPTobLocation;
-    m_CPMCMXDataLocation    = TrigT1CaloDefs::CPMCMXDataLocation;
-    m_TopoOutputLocation    = TrigT1CaloDefs::EmTauTopoTobLocation;
-
-    // This is how you declare the paramembers to Gaudi so that
-    // they can be over-written via the job options file
-    
-    declareProperty( "CMXCPHitLocation",        m_CMXCPHitLocation );
-    declareProperty( "CMXCPTobLocation",        m_CMXCPTobLocation );
-    declareProperty( "CPMCMXDataLocation",      m_CPMCMXDataLocation );
-    declareProperty( "TopoOutputLocation",      m_TopoOutputLocation );
-    declareProperty( "LVL1ConfigSvc", m_configSvc, "LVL1 Config Service");
-
-}
+    : AthAlgorithm( name, pSvcLocator ) {}
 
 
 //---------------------------------
@@ -81,10 +63,14 @@ CPCMX::CPCMX
 
 StatusCode CPCMX::initialize()
 {
-  ATH_CHECK( m_CTPOutputKey.initialize() );
+  ATH_CHECK(m_CMXCPHitsLocation.initialize());
+  ATH_CHECK(m_CMXCPTobLocation.initialize());
+  ATH_CHECK(m_TopoOutputLocation.initialize());
+  ATH_CHECK(m_CTPOutputKey.initialize());
+  ATH_CHECK(m_CPMCMXDataLocation.initialize());
 
   ATH_CHECK( m_configSvc.retrieve() );
-  return StatusCode::SUCCESS ;
+  return StatusCode::SUCCESS;
 }
 
 
@@ -109,12 +95,11 @@ StatusCode CPCMX::execute( )
 
   ATH_MSG_DEBUG ( "starting CPCMX" );
   
-  /** Initialise pointer */
-  m_emTauCTP = 0;
-
   /** Create containers for BS simulation */
   DataVector<CMXCPTob>*  CMXTobs = new DataVector<CMXCPTob>;
+  ATH_CHECK(SG::makeHandle(m_CMXCPTobLocation).record( std::unique_ptr<DataVector<CMXCPTob>>(CMXTobs ) ));
   DataVector<CMXCPHits>* CMXHits = new DataVector<CMXCPHits>;
+  ATH_CHECK(SG::makeHandle(m_CMXCPHitsLocation).record( std::unique_ptr<DataVector<CMXCPHits>>(CMXHits ) ));
 
 
   /** Create and initialise arrays for storing hit results */
@@ -139,7 +124,8 @@ StatusCode CPCMX::execute( )
   
   
   // Create objects to store TOBs for L1Topo
-  DataVector<CPCMXTopoData>* topoData = new DataVector<CPCMXTopoData>;
+  SG::WriteHandle<DataVector<CPCMXTopoData>> topoData = SG::makeHandle(m_TopoOutputLocation);
+  ATH_CHECK(topoData.record(std::make_unique<DataVector<CPCMXTopoData>>()));
   for (int crate = 0; crate < 4; ++crate) {
     for (int cmx = 0; cmx < 2; ++cmx) {
        CPCMXTopoData* link = new CPCMXTopoData(crate, cmx);
@@ -163,161 +149,132 @@ StatusCode CPCMX::execute( )
 
 
   /** Retrieve the CPCMXData (backplane data packages) */
-  const t_cpmDataContainer* bpData;
-  if (evtStore()->contains<t_cpmDataContainer>(m_CPMCMXDataLocation)) {
-    StatusCode sc = evtStore()->retrieve(bpData, m_CPMCMXDataLocation);  
-    if ( sc==StatusCode::SUCCESS ) {
-	
-      // Analyse module results
-      t_cpmDataContainer::const_iterator it = bpData->begin();
-      for ( ; it != bpData->end(); ++it) {
-        int crate = (*it)->crate();
-        int cmx   = (*it)->type();
-        std::vector<unsigned int> tobWords = (*it)->TopoTOBs();
-    
-        // Store data for L1Topo
-        int index = 2*crate + cmx;	
-        bool overflow = (*it)->overflow();
-        if (overflow) {
-          (*topoData)[index]->setOverflow(true);
-          cpmOverflow[cmx] = true;
-        }
-	
-        for (std::vector<unsigned int>::const_iterator word = tobWords.begin();
-             word != tobWords.end(); ++word) {
-	    
-          // Push back to Topo link
-          (*topoData)[index]->addTOB( (*word) );
-	   
-          // Decode TOB word 
-          CPTopoTOB tob( crate, cmx, (*word) );
-          int ieta = tob.ieta() - 1;
-          int iphi = tob.iphi();
-          if (iphi < 0) iphi += 64;
-          int et = tob.et();
-          unsigned int isol = tob.isolation();
-	   
-          // Now check against trigger thresholds
-          for ( std::vector< TriggerThreshold* >::const_iterator itTh = thresholds.begin();
-                itTh != thresholds.end(); ++itTh ) {
-            // Right type?
-            if ( (*itTh)->type() != triggerTypes[cmx] ) continue;
-            // Does TOB satisfy this threshold?
-            TriggerThresholdValue* ttv = (*itTh)->triggerThresholdValue( ieta, iphi );
-            ClusterThresholdValue* ctv = dynamic_cast< ClusterThresholdValue* >( ttv );
-            if (ctv) {
-              int etCut             = ctv->ptcut()*cpScale;
-              unsigned int isolMask = ctv->isolationMask();
-               
-              bool isolationPassed = true;
-              for (unsigned int bit = 0; bit < TrigT1CaloDefs::numOfIsolationBits; ++bit) 
-                if ( (isolMask & (1<<bit)) && !(isol & (1<<bit)) ) isolationPassed = false;
-               
-              if ( et > etCut && isolationPassed ) {		
-                int num = ( *itTh )->thresholdNumber();
-                if (num < 16) {
-                  if (crateHits[crate][cmx][num] < 7) crateHits[crate][cmx][num]++;
-                  if (Hits[cmx][num] < 7)             Hits[cmx][num]++;
-                }
-                else ATH_MSG_WARNING("Invalid threshold number " << num );
-              } // passes cuts
-		    
-            } // ClusterThresholdValue pointer valid
-          } // Loop over thresholds
-		 
-        } // Loop over TOBs
-	    
-      } // Loop over module results
+  SG::ReadHandle<t_cpmDataContainer> bpData = SG::makeHandle(m_CPMCMXDataLocation);
+  // Analyse module results
+  t_cpmDataContainer::const_iterator it = bpData->begin();
+  for ( ; it != bpData->end(); ++it) {
+    int crate = (*it)->crate();
+    int cmx   = (*it)->type();
+    std::vector<unsigned int> tobWords = (*it)->TopoTOBs();
 
-      // Overflow sets all trigger bits
-      for (int cmx = 0; cmx < 2; ++cmx) {
-        if (cpmOverflow[cmx]) {
-          for (int i = 0; i < 16; ++i) Hits[cmx][i] = 7;
-        }
-      }
-      
-      // Form CTP data objects
-      unsigned int cableWord0 = 0;
-      unsigned int cableWord1 = 0;
-      unsigned int cableWord2 = 0;
-      unsigned int cableWord3 = 0;
+    // Store data for L1Topo
+    int index = 2*crate + cmx;	
+    bool overflow = (*it)->overflow();
+    if (overflow) {
+      (*topoData)[index]->setOverflow(true);
+      cpmOverflow[cmx] = true;
+    }
+
+    for (std::vector<unsigned int>::const_iterator word = tobWords.begin();
+        word != tobWords.end(); ++word) {
+
+      // Push back to Topo link
+      (*topoData)[index]->addTOB( (*word) );
+
+      // Decode TOB word 
+      CPTopoTOB tob( crate, cmx, (*word) );
+      int ieta = tob.ieta() - 1;
+      int iphi = tob.iphi();
+      if (iphi < 0) iphi += 64;
+      int et = tob.et();
+      unsigned int isol = tob.isolation();
+
+      // Now check against trigger thresholds
+      for ( std::vector< TriggerThreshold* >::const_iterator itTh = thresholds.begin();
+          itTh != thresholds.end(); ++itTh ) {
+        // Right type?
+        if ( (*itTh)->type() != triggerTypes[cmx] ) continue;
+        // Does TOB satisfy this threshold?
+        TriggerThresholdValue* ttv = (*itTh)->triggerThresholdValue( ieta, iphi );
+        ClusterThresholdValue* ctv = dynamic_cast< ClusterThresholdValue* >( ttv );
+        if (ctv) {
+          int etCut             = ctv->ptcut()*cpScale;
+          unsigned int isolMask = ctv->isolationMask();
+
+          bool isolationPassed = true;
+          for (unsigned int bit = 0; bit < TrigT1CaloDefs::numOfIsolationBits; ++bit) 
+            if ( (isolMask & (1<<bit)) && !(isol & (1<<bit)) ) isolationPassed = false;
+
+          if ( et > etCut && isolationPassed ) {		
+            int num = ( *itTh )->thresholdNumber();
+            if (num < 16) {
+              if (crateHits[crate][cmx][num] < 7) crateHits[crate][cmx][num]++;
+              if (Hits[cmx][num] < 7)             Hits[cmx][num]++;
+            }
+            else ATH_MSG_WARNING("Invalid threshold number " << num );
+          } // passes cuts
+
+        } // ClusterThresholdValue pointer valid
+      } // Loop over thresholds
+
+    } // Loop over TOBs
+
+  } // Loop over module results
+
+  // Overflow sets all trigger bits
+  for (int cmx = 0; cmx < 2; ++cmx) {
+    if (cpmOverflow[cmx]) {
+      for (int i = 0; i < 16; ++i) Hits[cmx][i] = 7;
+    }
+  }
+
+  // Form CTP data objects
+  unsigned int cableWord0 = 0;
+  unsigned int cableWord1 = 0;
+  unsigned int cableWord2 = 0;
+  unsigned int cableWord3 = 0;
+  for (int i = 0; i < 8; ++i) {
+    cableWord0 |= ( Hits[0][i]<<(3*i) );
+    cableWord1 |= ( Hits[0][i+8]<<(3*i) );
+    cableWord2 |= ( Hits[1][i]<<(3*i) );
+    cableWord3 |= ( Hits[1][i+8]<<(3*i) );
+  }
+
+  SG::WriteHandle<EmTauCTP> emTauCTP = SG::makeHandle(m_CTPOutputKey);
+  ATH_CHECK(emTauCTP.record(std::make_unique<EmTauCTP>(cableWord0, cableWord1, cableWord2, cableWord3)));
+
+  // Form and store CPCMXHits
+  std::vector<int> error0;  // Dummies - there will be no actual errors simulated
+  std::vector<int> error1;
+
+  // Now form hits words from module results and insert into objects
+  std::vector<unsigned int> cratehits0;
+  std::vector<unsigned int> cratehits1;
+  const int peak = 0;
+  const int system_crate = 3;
+
+  // Crate sums (local and remote)  
+  for (int crate = 0; crate < 4; ++crate) {
+    for (int cmx = 0; cmx < 2; ++cmx) {
+      cratehits0.assign(1,0);
+      cratehits1.assign(1,0);
       for (int i = 0; i < 8; ++i) {
-        cableWord0 |= ( Hits[0][i]<<(3*i) );
-        cableWord1 |= ( Hits[0][i+8]<<(3*i) );
-        cableWord2 |= ( Hits[1][i]<<(3*i) );
-        cableWord3 |= ( Hits[1][i+8]<<(3*i) );
+        cratehits0[0] |= ( crateHits[crate][cmx][i]<<(3*i) );
+        cratehits1[0] |= ( crateHits[crate][cmx][i+8]<<(3*i) );
       }
+      CMXCPHits* crateCMXHits = new CMXCPHits(crate, cmx, LVL1::CMXCPHits::LOCAL,
+          cratehits0, cratehits1, error0, error1, peak);
+      CMXHits->push_back(crateCMXHits);
+      if (crate != system_crate) {
+        CMXCPHits* remoteCMXHits = new CMXCPHits(system_crate, cmx, crate,
+            cratehits0, cratehits1, error0, error1, peak);
+        CMXHits->push_back(remoteCMXHits);
+      }
+    } // loop over CMXes
+  } // loop over crates
 
-      m_emTauCTP = new EmTauCTP( cableWord0, cableWord1, cableWord2, cableWord3 );
+  // global sums
+  cratehits0.assign(1,cableWord0);
+  cratehits1.assign(1,cableWord1);
+  CMXCPHits* totalCMXHits0 = new CMXCPHits(system_crate, 0, LVL1::CMXCPHits::TOTAL,
+      cratehits0, cratehits1, error0, error1, peak);
+  cratehits0.assign(1,cableWord2);
+  cratehits1.assign(1,cableWord3);
+  CMXCPHits* totalCMXHits1 = new CMXCPHits(system_crate, 1, LVL1::CMXCPHits::TOTAL,
+      cratehits0, cratehits1, error0, error1, peak);
+  CMXHits->push_back(totalCMXHits0);
+  CMXHits->push_back(totalCMXHits1);
       
-      // Form and store CPCMXHits
-      std::vector<int> error0;  // Dummies - there will be no actual errors simulated
-      std::vector<int> error1;
-      
-      // Now form hits words from module results and insert into objects
-      std::vector<unsigned int> cratehits0;
-      std::vector<unsigned int> cratehits1;
-      const int peak = 0;
-      const int system_crate = 3;
-
-      // Crate sums (local and remote)  
-      for (int crate = 0; crate < 4; ++crate) {
-        for (int cmx = 0; cmx < 2; ++cmx) {
-          cratehits0.assign(1,0);
-          cratehits1.assign(1,0);
-          for (int i = 0; i < 8; ++i) {
-            cratehits0[0] |= ( crateHits[crate][cmx][i]<<(3*i) );
-            cratehits1[0] |= ( crateHits[crate][cmx][i+8]<<(3*i) );
-          }
-          CMXCPHits* crateCMXHits = new CMXCPHits(crate, cmx, LVL1::CMXCPHits::LOCAL,
-      		                                  cratehits0, cratehits1, error0, error1, peak);
-          CMXHits->push_back(crateCMXHits);
-          if (crate != system_crate) {
-            CMXCPHits* remoteCMXHits = new CMXCPHits(system_crate, cmx, crate,
-	  	                                     cratehits0, cratehits1, error0, error1, peak);
-            CMXHits->push_back(remoteCMXHits);
-          }
-        } // loop over CMXes
-      } // loop over crates
-      
-      // global sums
-      cratehits0.assign(1,cableWord0);
-      cratehits1.assign(1,cableWord1);
-      CMXCPHits* totalCMXHits0 = new CMXCPHits(system_crate, 0, LVL1::CMXCPHits::TOTAL,
-					       cratehits0, cratehits1, error0, error1, peak);
-      cratehits0.assign(1,cableWord2);
-      cratehits1.assign(1,cableWord3);
-      CMXCPHits* totalCMXHits1 = new CMXCPHits(system_crate, 1, LVL1::CMXCPHits::TOTAL,
-					       cratehits0, cratehits1, error0, error1, peak);
-      CMXHits->push_back(totalCMXHits0);
-      CMXHits->push_back(totalCMXHits1);
-      
-    } // Successfully read input data
-
-  } // Input collection exists in StoreGate
-
-
-  // Store output for BS simulation
-  StatusCode sc = evtStore()->overwrite(CMXTobs, m_CMXCPTobLocation, true);
-  if (sc != StatusCode::SUCCESS) ATH_MSG_WARNING ( "Problem writeing CMXTobs to StoreGate" );
-
-  sc = evtStore()->overwrite(CMXHits, m_CMXCPHitLocation, true);
-  if (sc != StatusCode::SUCCESS) ATH_MSG_WARNING ( "Problem writeing CMXHits to StoreGate" );
-
-  // Store Topo results
-  sc = evtStore()->overwrite(topoData, m_TopoOutputLocation, true);
-  if (sc != StatusCode::SUCCESS) ATH_MSG_WARNING ( "Problem writeing CPCMXTopoData object to StoreGate" );
-
-  // Store CTP results
-  if (m_emTauCTP == 0) {
-    m_emTauCTP = new EmTauCTP(0,0,0,0);
-    ATH_MSG_WARNING("No EmTauCTP found. Creating empty object" );
-  }
-
-  if(!m_CTPOutputKey.empty()) {
-    sc = SG::makeHandle(m_CTPOutputKey).record( std::unique_ptr<EmTauCTP>(m_emTauCTP) );
-    m_emTauCTP = nullptr;    
-  }
 
   return StatusCode::SUCCESS ;
 }
