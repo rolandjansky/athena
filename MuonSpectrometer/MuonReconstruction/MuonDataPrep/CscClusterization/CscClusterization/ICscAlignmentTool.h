ddf49dfcd88818053ecc1d37b92efed822ea9c6d
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-04-28 13:01:06 +0000

    CscClusterization - Reindentation & small code clean-ups

--- MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/CscClusterization/ICscAlignmentTool.h
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/CscClusterization/ICscAlignmentTool.h
@@ -14,33 +14,31 @@
 // It needs an input as CscClusterOnTrack, RIO_OnTrack, on top of CscPrepData
 
 #include <vector>
-#include "GaudiKernel/IAlgTool.h"
+
 #include "CscClusterization/ICscClusterFitter.h"
+#include "GaudiKernel/IAlgTool.h"
 
 class Identifier;
 
 namespace Muon {
-  class CscClusterOnTrack;
-  class CscPrepData;
-  class CscStripPrepData;
-}
+    class CscClusterOnTrack;
+    class CscPrepData;
+    class CscStripPrepData;
+}  // namespace Muon
 namespace Trk {
-  class RIO_OnTrack;
+    class RIO_OnTrack;
 }
 
 /** Must declare this, with name of interface*/
 static const InterfaceID IID_ICscAlignmentTool("ICscAlignmentTool", 1, 0);
 
 class ICscAlignmentTool : virtual public IAlgTool {
-
 public:  // Static methods.
-
-  // Return the interface ID.
-  static const InterfaceID& interfaceID() {  return IID_ICscAlignmentTool; }
+    // Return the interface ID.
+    static const InterfaceID& interfaceID() { return IID_ICscAlignmentTool; }
 
 public:  // Interface methods
-  virtual double getAlignmentOffset(Identifier pstripId) const=0;
-  
+    virtual double getAlignmentOffset(Identifier pstripId) const = 0;
 };
 
 #endif
--- MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/src/SimpleCscClusterFitter.cxx
+++ MuonSpectrometer/MuonReconstruction/MuonDataPrep/CscClusterization/src/SimpleCscClusterFitter.cxx
@@ -18,14 +18,13 @@ using Muon::CscStripPrepData;
 using MuonGM::CscReadoutElement;
 
 typedef ICscClusterFitter::Result Result;
-typedef std::vector<Result>       Results;
+typedef std::vector<Result> Results;
 
 enum CscStation { UNKNOWN_STATION, CSS, CSL };
 enum CscPlane { CSS_R, CSL_R, CSS_PHI, CSL_PHI, UNKNOWN_PLANE };
 
-SimpleCscClusterFitter::SimpleCscClusterFitter(std::string type, std::string aname, const IInterface* parent)
-    : AthAlgTool(type, aname, parent)
-{
+SimpleCscClusterFitter::SimpleCscClusterFitter(std::string type, std::string aname, const IInterface* parent) :
+    AthAlgTool(type, aname, parent) {
     declareInterface<ICscClusterFitter>(this);
     declareProperty("position_option", m_option = "MEAN", "Cluster fitting option: MEAN, PEAK, CENTROID");
     declareProperty("intrinsic_cluster_width", m_intrinsic_cluster_width = 20.0, "Intrinsic cluster width (mm)");
@@ -36,10 +35,7 @@ SimpleCscClusterFitter::SimpleCscClusterFitter(std::string type, std::string ana
 
 //**********************************************************************
 
-StatusCode
-SimpleCscClusterFitter::initialize()
-{
-
+StatusCode SimpleCscClusterFitter::initialize() {
     ATH_MSG_VERBOSE("Initializing " << name());
 
     ATH_MSG_DEBUG("Properties for " << name() << ":");
@@ -58,15 +54,11 @@ SimpleCscClusterFitter::initialize()
     return StatusCode::SUCCESS;
 }
 
-
 //**********************************************************************
 
-Results
-SimpleCscClusterFitter::fit(const StripFitList& sfits) const
-{
-
+Results SimpleCscClusterFitter::fit(const StripFitList& sfits) const {
     Results results;
-    Result  res;
+    Result res;
 
     // Check the input lists.
     unsigned int nstrip = sfits.size();
@@ -94,43 +86,43 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
 
     // retrieve MuonDetectorManager from the conditions store
     SG::ReadCondHandle<MuonGM::MuonDetectorManager> DetectorManagerHandle{m_DetectorManagerKey};
-    const MuonGM::MuonDetectorManager*              MuonDetMgr = DetectorManagerHandle.cptr();
+    const MuonGM::MuonDetectorManager* MuonDetMgr = DetectorManagerHandle.cptr();
     if (MuonDetMgr == nullptr) {
         ATH_MSG_ERROR("Null pointer to the MuonDetectorManager conditions object");
         return results;
     }
     const CscReadoutElement* pro = MuonDetMgr->getCscReadoutElement(idStrip0);
 
-    bool   measphi  = m_idHelperSvc->cscIdHelper().CscIdHelper::measuresPhi(idStrip0);
-    double pitch    = pro->cathodeReadoutPitch(0, measphi);
-    int    maxstrip = pro->maxNumberOfStrips(measphi);
-    int    strip0   = m_idHelperSvc->cscIdHelper().strip(idStrip0) - 1;
+    bool measphi = m_idHelperSvc->cscIdHelper().CscIdHelper::measuresPhi(idStrip0);
+    double pitch = pro->cathodeReadoutPitch(0, measphi);
+    int maxstrip = pro->maxNumberOfStrips(measphi);
+    int strip0 = m_idHelperSvc->cscIdHelper().strip(idStrip0) - 1;
 
-    int zsec    = m_idHelperSvc->cscIdHelper().stationEta(idStrip0);
+    int zsec = m_idHelperSvc->cscIdHelper().stationEta(idStrip0);
     int station = m_idHelperSvc->cscIdHelper().stationName(idStrip0) - 49;  // 1=CSS, 2=CSL
-    int phisec  = m_idHelperSvc->cscIdHelper().stationPhi(idStrip0);
+    int phisec = m_idHelperSvc->cscIdHelper().stationPhi(idStrip0);
 
     int sector = zsec * (2 * phisec - station + 1);
-    int wlay   = m_idHelperSvc->cscIdHelper().wireLayer(idStrip0);
+    int wlay = m_idHelperSvc->cscIdHelper().wireLayer(idStrip0);
 
     // In SimpleCscClusterFitter  istrip_peak = strip0;
-    int    peak_count = 0;            // # peaks in the cluster
-    bool   edge       = strip0 == 0;  // is cluster on the edge of the chamber?
-    int    stripidx   = 0;            // actual strip position [0-191] or [0-47]
-    int    countstrip = 0;            // counting strip in for loop
-    double qsum       = 0;            // charge sum of strips in cluster
-    double xsum       = 0;            // stripidx sum in cluster
-    double qxsum      = 0;            // position weighted (stripidx) charge sum
-    double qerravg    = 0;
+    int peak_count = 0;       // # peaks in the cluster
+    bool edge = strip0 == 0;  // is cluster on the edge of the chamber?
+    int stripidx = 0;         // actual strip position [0-191] or [0-47]
+    int countstrip = 0;       // counting strip in for loop
+    double qsum = 0;          // charge sum of strips in cluster
+    double xsum = 0;          // stripidx sum in cluster
+    double qxsum = 0;         // position weighted (stripidx) charge sum
+    double qerravg = 0;
     // istrip starts from 0.
     // stripidx is for actual strip position [0-191] or [0-47].
     // Out of for loop, stripidx will be the last strip of cluster.
     unsigned int istrip_peak = 0;
-    double       lastqpeak   = 0;
+    double lastqpeak = 0;
     for (unsigned int istrip = 0; istrip < nstrip; ++istrip) {
-        StripFit sfit  = sfits[istrip];
-        float    qthis = sfit.charge;
-        float    qlast = 0.0;
+        StripFit sfit = sfits[istrip];
+        float qthis = sfit.charge;
+        float qlast = 0.0;
         if (istrip > 0) qlast = sfits[istrip - 1].charge;
         float qnext = 0.0;
         if (istrip < nstrip - 1) qnext = sfits[istrip + 1].charge;
@@ -148,9 +140,7 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
         // Require the previous strip has less charge and the next following
         // strip be absent or have less charge.
         if (!ispeak) {
-            if (qthis == qnext) {
-                ispeak = (qthis > qlast) && (istrip + 2 == nstrip || sfits[istrip + 2].charge < qthis);
-            }
+            if (qthis == qnext) { ispeak = (qthis > qlast) && (istrip + 2 == nstrip || sfits[istrip + 2].charge < qthis); }
         }
         // Special case: first and second strips have the same charge.
         // Require the third strip has less charge.
@@ -164,7 +154,7 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
         // Record if peak.
         if (ispeak && qthis > lastqpeak) {
             istrip_peak = istrip;
-            lastqpeak   = qthis;
+            lastqpeak = qthis;
         }
     }
     if (stripidx == maxstrip - 1) edge = true;
@@ -172,29 +162,28 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
     if (countstrip == 1) ++peak_count;
     if (countstrip > 1 && sfits[nstrip - 1].charge >= sfits[nstrip - 2].charge) ++peak_count;
 
-
     // Fix to avoid division-by-zero (W.L. 29 Jun 2012)
     if (qsum <= 0) {
         // ATH_MSG_WARNING("Charge sum is not positive.");
         // ATH_MSG_WARNING("Charge sum : "<<qsum);
-        res.fitStatus      = 0;
-        double savg        = strip0 + istrip_peak;  // peak position: this strip has Q>0
-        res.position       = pitch * (savg + 0.5 - 0.5 * maxstrip);
-        res.strip          = istrip_peak;  // relative to cluster start
+        res.fitStatus = 0;
+        double savg = strip0 + istrip_peak;  // peak position: this strip has Q>0
+        res.position = pitch * (savg + 0.5 - 0.5 * maxstrip);
+        res.strip = istrip_peak;  // relative to cluster start
         double errorScaler = measphi ? m_defaultErrorScaler_phi : m_defaultErrorScaler_eta;
-        res.dposition      = errorScaler * pitch / sqrt(12.0);
+        res.dposition = errorScaler * pitch / sqrt(12.0);
         //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++added 13/06/2014 to avoid index out of range in
-        //CscThreshholdClusterBuilder.cxx
-        res.fstrip     = 0;
-        res.lstrip     = nstrip - 1;
-        res.time       = sfits[istrip_peak].time;
+        // CscThreshholdClusterBuilder.cxx
+        res.fstrip = 0;
+        res.lstrip = nstrip - 1;
+        res.time = sfits[istrip_peak].time;
         res.timeStatus = sfits[istrip_peak].timeStatus;
-        res.qpeak      = sfits[istrip_peak].charge;
+        res.qpeak = sfits[istrip_peak].charge;
 
-        res.charge              = res.qpeak;
+        res.charge = res.qpeak;
         res.charge_beforeBPCorr = sfits[res.strip].charge_beforeBPCorr;
 
-        res.qleft  = 0.;
+        res.qleft = 0.;
         res.qright = 0.;
 
         if (istrip_peak >= 1) {
@@ -214,14 +203,13 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
         return results;
     }
 
-
     // Calculate strip averages.
-    double strip_mean  = xsum / nstrip;     // Avg strip position
-    double strip_qmean = qxsum / qsum;      // Avg strip position weighted by charge
-    qerravg            = qerravg / nstrip;  // for centroid error calculation....
+    double strip_mean = xsum / nstrip;  // Avg strip position
+    double strip_qmean = qxsum / qsum;  // Avg strip position weighted by charge
+    qerravg = qerravg / nstrip;         // for centroid error calculation....
     // Assign cluster status.
     // Cluster is spoiled if it is on the edge or has multiple peaks.
-    res.fitStatus     = 0;
+    res.fitStatus = 0;
     res.clusterStatus = Muon::CscStatusSimple;
     if (edge)
         res.clusterStatus = Muon::CscStatusEdge;
@@ -254,17 +242,17 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
     res.position = pitch * (savg + 0.5 - 0.5 * maxstrip);
 
     // internal alignment ...
-    Identifier id     = sfits[res.strip].strip->identify();
-    double     offset = m_alignmentTool->getAlignmentOffset(id);
+    Identifier id = sfits[res.strip].strip->identify();
+    double offset = m_alignmentTool->getAlignmentOffset(id);
     res.position -= offset;
 
     // Assign position error.
-    double wmeas   = pitch * nstrip;
-    double weff    = wmeas - m_intrinsic_cluster_width;
+    double wmeas = pitch * nstrip;
+    double weff = wmeas - m_intrinsic_cluster_width;
     double weffmin = 0.5 * wmeas;
     if (weff < weffmin) weff = weffmin;
     double errorScaler = measphi ? m_defaultErrorScaler_phi : m_defaultErrorScaler_eta;
-    res.dposition      = errorScaler * weff / sqrt(12.0);  // CENTROID doesn't make any effect on the case nstrip=1
+    res.dposition = errorScaler * weff / sqrt(12.0);  // CENTROID doesn't make any effect on the case nstrip=1
     /*
     if (nstrip>1) {
       if ( m_option == "CENTROID" || ( m_use_peakthreshold && nstrip>2 && peak_count==1) ) {
@@ -283,23 +271,22 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
     }
     */
     ATH_MSG_VERBOSE("    Simple Fit Result "
-                    << " nstr=" << nstrip << "[sector:wlay:measphi]= " << sector << ":" << wlay << ":" << measphi
-                    << " strip0=" << strip0 << " istrip_peak=" << istrip_peak << " peaktime=" << sfits[istrip_peak].time
-                    << " peakstatus=" << sfits[istrip_peak].status
-                    << " peaktimeStatus=" << sfits[istrip_peak].timeStatus << " pos=" << res.position
-                    << " dpos=" << res.dposition << " chg=" << qsum);
+                    << " nstr=" << nstrip << "[sector:wlay:measphi]= " << sector << ":" << wlay << ":" << measphi << " strip0=" << strip0
+                    << " istrip_peak=" << istrip_peak << " peaktime=" << sfits[istrip_peak].time
+                    << " peakstatus=" << sfits[istrip_peak].status << " peaktimeStatus=" << sfits[istrip_peak].timeStatus
+                    << " pos=" << res.position << " dpos=" << res.dposition << " chg=" << qsum);
 
     // cluster charge should be qsum over three strip... 3/21/2011
-    res.fstrip     = 0;
-    res.lstrip     = nstrip - 1;
-    res.time       = sfits[istrip_peak].time;
+    res.fstrip = 0;
+    res.lstrip = nstrip - 1;
+    res.time = sfits[istrip_peak].time;
     res.timeStatus = sfits[istrip_peak].timeStatus;
-    res.qpeak      = sfits[istrip_peak].charge;
+    res.qpeak = sfits[istrip_peak].charge;
 
-    res.charge              = res.qpeak;
+    res.charge = res.qpeak;
     res.charge_beforeBPCorr = sfits[res.strip].charge_beforeBPCorr;
 
-    res.qleft  = 0.;
+    res.qleft = 0.;
     res.qright = 0.;
 
     if (istrip_peak >= 1) {
@@ -323,15 +310,7 @@ SimpleCscClusterFitter::fit(const StripFitList& sfits) const
 
 //**********************************************************************
 
-Results
-SimpleCscClusterFitter::fit(const StripFitList& sfits, double) const
-{
-    return fit(sfits);
-}
+Results SimpleCscClusterFitter::fit(const StripFitList& sfits, double) const { return fit(sfits); }
 
 //**********************************************************************
-double
-SimpleCscClusterFitter::getCorrectedError(const CscPrepData* /*pclu*/, double /*slope*/) const
-{
-    return 0;
-}
+double SimpleCscClusterFitter::getCorrectedError(const CscPrepData* /*pclu*/, double /*slope*/) const { return 0; }
