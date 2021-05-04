415558d91c9a216a8f244fef04606958e92f3f92
christos <christos@cern.ch>
2021-05-04 02:35:40 +0200

    DerivationFrameworkEGamma , use private Tools via new Gaudi style. format the code , std::abs and tidy

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/DerivationFrameworkEGamma/EGTransverseMassTool.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/DerivationFrameworkEGamma/EGTransverseMassTool.h
@@ -24,68 +24,73 @@
 
 namespace DerivationFramework {
 
-  class EGTransverseMassTool : public ExpressionParserUser<AthAlgTool>, public IAugmentationTool {
-    public:
-      EGTransverseMassTool(const std::string& t, const std::string& n, const IInterface* p);
+class EGTransverseMassTool
+  : public ExpressionParserUser<AthAlgTool>
+  , public IAugmentationTool
+{
+public:
+  EGTransverseMassTool(
+    const std::string& t,
+    const std::string& n,
+    const IInterface* p);
 
-      virtual StatusCode initialize() override final;
-      virtual StatusCode addBranches() const override final;
+  virtual StatusCode initialize() override final;
+  virtual StatusCode addBranches() const override final;
 
-    private:
-      StatusCode getTransverseMasses(const EventContext& ctx,
-                                     std::vector<float>&) const;
+private:
+  StatusCode getTransverseMasses(const EventContext& ctx, std::vector<float>&)
+    const;
 
-      std::string m_expression1;
-      float m_METmin;
-      float m_mass1Hypothesis;
+  std::string m_expression1;
+  float m_METmin;
+  float m_mass1Hypothesis;
 
-      SG::WriteHandleKey<std::vector<float>> m_sgName{ this,
-                                                       "StoreGateEntryName",
-                                                       "",
-                                                       "SG key of output object" };
-      
-      SG::ReadHandleKey<xAOD::IParticleContainer> m_container1Name{
-        this,
-        "ObjectContainerName",
-        "",
-        "SG key of first container"
-      };
-      SG::ReadHandleKey<xAOD::MissingETContainer> m_container2Name{
-        this,
-        "METContainerName",
-        "MET_LocHadTopo",
-        "SG key of second container"
-      };
+  SG::WriteHandleKey<std::vector<float>> m_sgName{ this,
+                                                   "StoreGateEntryName",
+                                                   "",
+                                                   "SG key of output object" };
 
-      SG::ReadHandleKey<std::vector<float>> m_pt1BranchName{
-        this,
-        "ObjectPtBranchName",
-        "",
-        "Pt1 if different than default"
-      };
+  SG::ReadHandleKey<xAOD::IParticleContainer> m_container1Name{
+    this,
+    "ObjectContainerName",
+    "",
+    "SG key of first container"
+  };
+  SG::ReadHandleKey<xAOD::MissingETContainer> m_container2Name{
+    this,
+    "METContainerName",
+    "MET_LocHadTopo",
+    "SG key of second container"
+  };
 
-      SG::ReadHandleKey<std::vector<float>> m_phi1BranchName{
-        this,
-        "ObjectPhiBranchName",
-        "",
-        "Phi1 if different than default"
-      };
+  SG::ReadHandleKey<std::vector<float>> m_pt1BranchName{
+    this,
+    "ObjectPtBranchName",
+    "",
+    "Pt1 if different than default"
+  };
 
-      SG::ReadHandleKey<std::vector<float>> m_pt2BranchName{
-        this,
-        "METPtBranchName",
-        "",
-        "Pt2 if different than default"
-      };
+  SG::ReadHandleKey<std::vector<float>> m_phi1BranchName{
+    this,
+    "ObjectPhiBranchName",
+    "",
+    "Phi1 if different than default"
+  };
 
-      SG::ReadHandleKey<std::vector<float>> m_phi2BranchName{
-        this,
-        "METPhiBranchName",
-        "",
-        "Phi2 if different than default"
-      };
+  SG::ReadHandleKey<std::vector<float>> m_pt2BranchName{
+    this,
+    "METPtBranchName",
+    "",
+    "Pt2 if different than default"
+  };
 
-  }; 
+  SG::ReadHandleKey<std::vector<float>> m_phi2BranchName{
+    this,
+    "METPhiBranchName",
+    "",
+    "Phi2 if different than default"
+  };
+};
 }
 
 #endif // DERIVATIONFRAMEWORK_EGTRANSVERSEMASSTOOL_H
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/src/EGTransverseMassTool.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkEGamma/src/EGTransverseMassTool.cxx
@@ -8,8 +8,8 @@
 //
 // The tool computes the transverse mass between the MET and the
 // particles belonging to some collection.
-// The MET met and phi values can either be stored in SG under 
-// keys specified with METPtBranchName and METPhiBranchName, 
+// The MET met and phi values can either be stored in SG under
+// keys specified with METPtBranchName and METPhiBranchName,
 // otherwise the met() and phi() of the MET object stored in
 // SG under the METContainerName (default = MET_LocHadTopo)
 // will be used.
@@ -32,150 +32,158 @@ using std::sqrt;
 
 namespace DerivationFramework {
 
-  EGTransverseMassTool::EGTransverseMassTool(const std::string& t,
-                                             const std::string& n,
-                                             const IInterface* p) : 
-    ExpressionParserUser<AthAlgTool>(t, n, p),
-    m_expression1("true"),
-    m_METmin(-999.),
-    m_mass1Hypothesis(0.0)
-  {
-    declareInterface<DerivationFramework::IAugmentationTool>(this);
-    declareProperty("ObjectRequirements", m_expression1);
-    declareProperty("METmin", m_METmin);
-    declareProperty("ObjectMassHypothesis", m_mass1Hypothesis);
+EGTransverseMassTool::EGTransverseMassTool(const std::string& t,
+                                           const std::string& n,
+                                           const IInterface* p)
+  : ExpressionParserUser<AthAlgTool>(t, n, p)
+  , m_expression1("true")
+  , m_METmin(-999.)
+  , m_mass1Hypothesis(0.0)
+{
+  declareInterface<DerivationFramework::IAugmentationTool>(this);
+  declareProperty("ObjectRequirements", m_expression1);
+  declareProperty("METmin", m_METmin);
+  declareProperty("ObjectMassHypothesis", m_mass1Hypothesis);
+}
+
+StatusCode
+EGTransverseMassTool::initialize()
+{
+  if (m_sgName.key().empty()) {
+    ATH_MSG_ERROR(
+      "No SG name provided for the output of the transverse mass tool!");
+    return StatusCode::FAILURE;
+  }
+  ATH_CHECK(m_sgName.initialize());
+
+  if (!m_container1Name.key().empty()) {
+    ATH_CHECK(m_container1Name.initialize());
+  }
+  if (!m_container2Name.key().empty()) {
+    ATH_CHECK(m_container2Name.initialize());
+  }
+  if (!m_pt1BranchName.key().empty()) {
+    ATH_CHECK(m_pt1BranchName.initialize());
+  }
+  if (!m_phi1BranchName.key().empty()) {
+    ATH_CHECK(m_phi1BranchName.initialize());
+  }
+  if (!m_pt2BranchName.key().empty()) {
+    ATH_CHECK(m_pt2BranchName.initialize());
+  }
+  if (!m_phi2BranchName.key().empty()) {
+    ATH_CHECK(m_phi2BranchName.initialize());
   }
 
-  StatusCode EGTransverseMassTool::initialize()
-  {
-    if (m_sgName.key().empty()) {
-      ATH_MSG_ERROR("No SG name provided for the output of the transverse mass tool!");
-      return StatusCode::FAILURE;
-    }
-    ATH_CHECK(m_sgName.initialize());
+  ATH_CHECK(initializeParser(m_expression1));
 
-    if (!m_container1Name.key().empty()) {
-      ATH_CHECK(m_container1Name.initialize());
-    }
-    if (!m_container2Name.key().empty()) {
-      ATH_CHECK(m_container2Name.initialize());
-    }
-    if (!m_pt1BranchName.key().empty()) {
-      ATH_CHECK(m_pt1BranchName.initialize());
-    }
-    if (!m_phi1BranchName.key().empty()) {
-      ATH_CHECK(m_phi1BranchName.initialize());
-    }
-    if (!m_pt2BranchName.key().empty()) {
-      ATH_CHECK(m_pt2BranchName.initialize());
-    }
-    if (!m_phi2BranchName.key().empty()) {
-      ATH_CHECK(m_phi2BranchName.initialize());
-    }
+  return StatusCode::SUCCESS;
+}
 
-    ATH_CHECK( initializeParser(m_expression1) );
+StatusCode
+EGTransverseMassTool::addBranches() const
+{
+  const EventContext& ctx = Gaudi::Hive::currentContext();
+  SG::WriteHandle<std::vector<float>> writeHandle{ m_sgName, ctx };
 
+  // create the vector which will hold the values invariant masses
+  auto masses = std::make_unique<std::vector<float>>();
+  // compute the invariant mass values
+  ATH_CHECK(getTransverseMasses(ctx, *masses));
+
+  ATH_CHECK(writeHandle.record(std::move(masses)));
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode
+EGTransverseMassTool::getTransverseMasses(const EventContext& ctx,
+                                          std::vector<float>& masses) const
+{
+  // Get optional payload
+  const std::vector<float>* pt1 = nullptr;
+  if (!m_pt1BranchName.key().empty()) {
+    SG::ReadHandle<std::vector<float>> readHandle{ m_pt1BranchName, ctx };
+    pt1 = readHandle.ptr();
+  }
+  const std::vector<float>* pt2 = nullptr;
+  if (!m_pt2BranchName.key().empty()) {
+    SG::ReadHandle<std::vector<float>> readHandle{ m_pt2BranchName, ctx };
+    pt2 = readHandle.ptr();
+  }
+
+  const std::vector<float>* phi1 = nullptr;
+  if (!m_phi1BranchName.key().empty()) {
+    SG::ReadHandle<std::vector<float>> readHandle{ m_phi1BranchName, ctx };
+    phi1 = readHandle.ptr();
+  }
+  const std::vector<float>* phi2 = nullptr;
+  if (!m_phi2BranchName.key().empty()) {
+    SG::ReadHandle<std::vector<float>> readHandle{ m_phi2BranchName, ctx };
+    phi2 = readHandle.ptr();
+  }
+
+  // Get the input particle and MET
+  SG::ReadHandle<xAOD::IParticleContainer> inputParticles1{ m_container1Name,
+                                                            ctx };
+  SG::ReadHandle<xAOD::MissingETContainer> inputParticles2{ m_container2Name,
+                                                            ctx };
+  const xAOD::IParticleContainer* particles1 = inputParticles1.ptr();
+  const xAOD::MissingETContainer* particles2 = inputParticles2.ptr();
+
+  // compute MET
+  if (particles2->empty()) {
+    if (!pt2) {
+      ATH_MSG_WARNING("No MET info found");
+      return StatusCode::SUCCESS;
+    }
+    if (pt2->empty()) {
+      ATH_MSG_WARNING("No MET info found");
+      return StatusCode::SUCCESS;
+    }
+  }
+
+  float MET = pt2 ? (*pt2)[0] : particles2->at(0)->met();
+  float MET_phi = phi2 ? (*phi2)[0] : particles2->at(0)->phi();
+
+  // apply MET requirement
+  if (MET < m_METmin)
     return StatusCode::SUCCESS;
-  }
 
-  StatusCode EGTransverseMassTool::addBranches() const
-  {
-    const EventContext& ctx = Gaudi::Hive::currentContext();
-    SG::WriteHandle<std::vector<float>> writeHandle{ m_sgName, ctx };
+  // get the positions of the particles which pass the requirement
+  std::vector<int> entries1 = m_parser->evaluateAsVector();
+  unsigned int nEntries1 = entries1.size();
 
-    // create the vector which will hold the values invariant masses
-    auto masses = std::make_unique<std::vector<float>>();
-    // compute the invariant mass values
-    ATH_CHECK(getTransverseMasses(ctx,*masses));
-
-    ATH_CHECK(writeHandle.record(std::move(masses)));   
-
+  // if there are no particles in one of the two lists to combine, just leave
+  // function
+  if (nEntries1 == 0)
     return StatusCode::SUCCESS;
+
+  // check that the sizes are compatible
+  if (particles1->size() != nEntries1) {
+    ATH_MSG_ERROR("Branch sizes incompatible - returning zero");
+    return StatusCode::FAILURE;
+  }
+  if ((pt1 && pt1->size() != nEntries1) ||
+      (phi1 && phi1->size() != nEntries1)) {
+    ATH_MSG_ERROR("Branch sizes incompatible - returning zero");
+    return StatusCode::FAILURE;
   }
 
-  StatusCode EGTransverseMassTool::getTransverseMasses(const EventContext& ctx,
-                                                       std::vector<float>& masses) const
-  {
-    // Get optional payload
-    const std::vector<float>* pt1 = nullptr;
-    if (!m_pt1BranchName.key().empty()) {
-      SG::ReadHandle<std::vector<float>> readHandle{ m_pt1BranchName, ctx };
-      pt1 = readHandle.ptr();
-    }
-    const std::vector<float>* pt2 = nullptr;
-    if (!m_pt2BranchName.key().empty()) {
-      SG::ReadHandle<std::vector<float>> readHandle{ m_pt2BranchName, ctx };
-      pt2 = readHandle.ptr();
-    }
+  // Loop over the objects and calculate the transverse mass
+  unsigned int iter;
+  for (iter = 0; iter < nEntries1; ++iter) {
+    if (entries1[iter] != 1)
+      continue;
+    float apt1 = pt1 ? (*pt1)[iter] : ((*particles1)[iter])->p4().Pt();
+    float aphi1 = phi1 ? (*phi1)[iter] : ((*particles1)[iter])->p4().Phi();
 
-    const std::vector<float>* phi1 = nullptr;
-    if (!m_phi1BranchName.key().empty()) {
-      SG::ReadHandle<std::vector<float>> readHandle{ m_phi1BranchName, ctx };
-      phi1 = readHandle.ptr();
-    }
-    const std::vector<float>* phi2 = nullptr;
-    if (!m_phi2BranchName.key().empty()) {
-      SG::ReadHandle<std::vector<float>> readHandle{ m_phi2BranchName, ctx };
-      phi2 = readHandle.ptr();
-    }
-
-    // Get the input particle and MET
-    SG::ReadHandle<xAOD::IParticleContainer> inputParticles1{ m_container1Name, ctx };
-    SG::ReadHandle<xAOD::MissingETContainer> inputParticles2{ m_container2Name, ctx };
-    const xAOD::IParticleContainer* particles1 = inputParticles1.ptr();
-    const xAOD::MissingETContainer* particles2 = inputParticles2.ptr();
-
-    // compute MET
-    if (particles2->empty()) {
-      if (!pt2){
-	      ATH_MSG_WARNING("No MET info found");
-	      return StatusCode::SUCCESS; 
-      }
-      if (pt2->empty()) {
-	      ATH_MSG_WARNING("No MET info found");
-	      return StatusCode::SUCCESS; 
-      }
-    }
-
-    float MET = pt2 ? (*pt2)[0] : particles2->at(0)->met();
-    float MET_phi = phi2 ? (*phi2)[0] : particles2->at(0)->phi();
-
-    // apply MET requirement
-    if (MET<m_METmin)
-      return StatusCode::SUCCESS; 
-
-    // get the positions of the particles which pass the requirement
-    std::vector<int> entries1 =  m_parser->evaluateAsVector();
-    unsigned int nEntries1 = entries1.size();
-
-    // if there are no particles in one of the two lists to combine, just leave function
-    if (nEntries1==0)
-      return StatusCode::SUCCESS; 
-
-    // check that the sizes are compatible
-    if (particles1->size() != nEntries1 ) { 
-      ATH_MSG_ERROR("Branch sizes incompatible - returning zero");
-      return StatusCode::FAILURE;
-    } 
-    if ((pt1 && pt1->size() != nEntries1) || 
-        (phi1 && phi1->size() != nEntries1)) {
-      ATH_MSG_ERROR("Branch sizes incompatible - returning zero");
-      return StatusCode::FAILURE;
-    } 
-
-    // Loop over the objects and calculate the transverse mass
-    unsigned int iter;
-    for (iter=0; iter<nEntries1; ++iter) {
-      if (entries1[iter]!=1) continue;
-      float apt1 = pt1 ? (*pt1)[iter] : ((*particles1)[iter])->p4().Pt(); 
-      float aphi1 = phi1 ? (*phi1)[iter] : ((*particles1)[iter])->p4().Phi(); 
-
-      TLorentzVector v1, v2;
-      v1.SetPtEtaPhiM(apt1,0.,aphi1,m_mass1Hypothesis);
-      v2.SetPtEtaPhiM(MET,0.,MET_phi,0.);
-      float mass = (v1+v2).M();
-      masses.push_back(mass);
-    }
-    return StatusCode::SUCCESS; 
+    TLorentzVector v1, v2;
+    v1.SetPtEtaPhiM(apt1, 0., aphi1, m_mass1Hypothesis);
+    v2.SetPtEtaPhiM(MET, 0., MET_phi, 0.);
+    float mass = (v1 + v2).M();
+    masses.push_back(mass);
   }
+  return StatusCode::SUCCESS;
+}
 }
