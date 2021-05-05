472a224d6988d3d655b7108c62e33480a25e58e9
Tadej Novak <tadej.novak@cern.ch>
2021-05-05 17:20:07 +0200

    Setup separate MC16 and MC20 campaign configurations

--- Tools/Campaigns/python/__init__.py
+++ Tools/Campaigns/python/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 
+from .MC16 import MC16a, MC16d, MC16e
 from .MC20 import MC20a, MC20d, MC20e
 
-__all__ = ['MC20a', 'MC20d', 'MC20e']
+__all__ = [
+  'MC16a', 'MC16d', 'MC16e',
+  'MC20a', 'MC20d', 'MC20e'
+]
