bba587d52035b9f5829b064a094be7fcdb1d3895
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-05-09 17:38:00 +0000

    MuonTrackPerformance - reindendation & MuonTruthAlgs: Reject creation of TruthSegements from LF muons only

--- MuonSpectrometer/MuonValidation/MuonRecValidation/MuonTrackPerformance/doc/packagedoc.h
+++ MuonSpectrometer/MuonValidation/MuonRecValidation/MuonTrackPerformance/doc/packagedoc.h
@@ -5,14 +5,14 @@
 /**
 
 @page MuonTrackPerformance_page MuonTrackPerformance package
-	 @author Niels.Van.Eldik@cern.ch
-	
+         @author Niels.Van.Eldik@cern.ch
+
 @section MuonTrackPerformance_MuonTrackPerformanceIntro Introduction
 
 The MuonTrackPerformance is a package to monitor the performance of the muon track finding.
-It can be used to produce track and segment based histograms. 
+It can be used to produce track and segment based histograms.
 When running on simulated data it can be configured to perform truth matching as well.
- 	
+
 @section MuonTrackPerformance_ExtrasMuonTrackPerformance Extra Pages
 
 */
