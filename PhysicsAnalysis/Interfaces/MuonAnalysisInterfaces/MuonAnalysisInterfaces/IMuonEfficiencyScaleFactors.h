99a569cf25b62657f075e7d81f6e5324540d53c7
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2021-05-10 15:59:42 +0000

    Make the MuonSelectorTool more thread safe

--- PhysicsAnalysis/Interfaces/MuonAnalysisInterfaces/MuonAnalysisInterfaces/IMuonEfficiencyScaleFactors.h
+++ PhysicsAnalysis/Interfaces/MuonAnalysisInterfaces/MuonAnalysisInterfaces/IMuonEfficiencyScaleFactors.h
@@ -9,17 +9,18 @@
 #include "AsgTools/IAsgTool.h"
 
 // EDM include(s):
-#include "xAODMuon/Muon.h"
 #include <xAODEventInfo/EventInfo.h>
+
+#include "xAODMuon/Muon.h"
 
 // Local include(s):
 #include "PATInterfaces/CorrectionCode.h"
 #include "PATInterfaces/ISystematicsTool.h"
 
 // STL include(s):
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
 /**
  * @mainpage MuonEfficiencyCorrections
@@ -36,7 +37,7 @@
  * </ul>
  * <li><b>"DataPeriod"</b> -> Takes a string </li><br>
  *  supported data period:/li>
- * <li> "2015": 13 TeV pp data - default, so no need to configure this atm! 
+ * <li> "2015": 13 TeV pp data - default, so no need to configure this atm!
  * </ul>
  * <li><b>Optional: "doAudit"</b> -> Activates audit mode  (for debugging) </li><br>
  *  supported choices:
@@ -98,55 +99,54 @@
 namespace CP {
     class SystematicSet;
 
-    class IMuonEfficiencyScaleFactors: public virtual CP::ISystematicsTool {
-            ///
-            /// @class IMuonEfficiencyScaleFactors
-            /// @brief Interface class.
-            /// Abstract interface class. Provides the user interface for the
-            /// MuonEfficiencyScaleFactors class.
+    class IMuonEfficiencyScaleFactors : public virtual CP::ISystematicsTool {
+        ///
+        /// @class IMuonEfficiencyScaleFactors
+        /// @brief Interface class.
+        /// Abstract interface class. Provides the user interface for the
+        /// MuonEfficiencyScaleFactors class.
 
-            ASG_TOOL_INTERFACE (CP::IMuonEfficiencyScaleFactors)
+        ASG_TOOL_INTERFACE(CP::IMuonEfficiencyScaleFactors)
 
-        public:
+    public:
+        /// Retrieve the Scale factor
+        virtual CorrectionCode getEfficiencyScaleFactor(const xAOD::Muon& mu, float& sf, const xAOD::EventInfo* info = 0) const = 0;
+        /// decorate the muon with scale factor information
+        virtual CorrectionCode applyEfficiencyScaleFactor(const xAOD::Muon& mu, const xAOD::EventInfo* info = 0) const = 0;
 
-            /// Retrieve the Scale factor
-            virtual CorrectionCode getEfficiencyScaleFactor(const xAOD::Muon& mu, float& sf, const xAOD::EventInfo* info = 0) const = 0;
-            /// decorate the muon with scale factor information
-            virtual CorrectionCode applyEfficiencyScaleFactor(const xAOD::Muon& mu, const xAOD::EventInfo* info = 0) const = 0;
+        /// BONUS functionality
 
-            /// BONUS functionality
+        /// An optional, less conservative way of treating the SF stat error.
+        /// Here, we generate a set of SF replicas,
+        /// each by smearing the SF bin by bin with a gaussian with the width of the SF error in that bin
+        /// the SF stat systematic is then the variation of the final observable within the replicas
 
-            /// An optional, less conservative way of treating the SF stat error.
-            /// Here, we generate a set of SF replicas,
-            /// each by smearing the SF bin by bin with a gaussian with the width of the SF error in that bin
-            /// the SF stat systematic is then the variation of the final observable within the replicas
+        virtual CorrectionCode getEfficiencyScaleFactorReplicas(const xAOD::Muon& mu, std::vector<float>& sf_err,
+                                                                const xAOD::EventInfo* info = 0) const = 0;
+        /// decorate the muon with a set of SF replica weights.
+        virtual CorrectionCode applyEfficiencyScaleFactorReplicas(const xAOD::Muon& mu, int nreplicas = 50,
+                                                                  const xAOD::EventInfo* info = 0) const = 0;
 
-            virtual CorrectionCode getEfficiencyScaleFactorReplicas(const xAOD::Muon& mu, std::vector<float> & sf_err, const xAOD::EventInfo* info = 0) const = 0;
-            /// decorate the muon with a set of SF replica weights.
-            virtual CorrectionCode applyEfficiencyScaleFactorReplicas(const xAOD::Muon& mu, int nreplicas = 50, const xAOD::EventInfo* info = 0) const = 0;
+        /// Obtain the muon efficiency measured using the data
+        //  Can for example be used to scale truth-level MC
+        virtual CorrectionCode getDataEfficiency(const xAOD::Muon& mu, float& eff, const xAOD::EventInfo* info = 0) const = 0;
+        /// decorate a muon with the efficiency information
+        virtual CorrectionCode applyDataEfficiency(const xAOD::Muon& mu, const xAOD::EventInfo* info = 0) const = 0;
 
-            /// Obtain the muon efficiency measured using the data
-            //  Can for example be used to scale truth-level MC
-            virtual CorrectionCode getDataEfficiency(const xAOD::Muon& mu, float& eff, const xAOD::EventInfo* info = 0) const = 0;
-            /// decorate a muon with the efficiency information
-            virtual CorrectionCode applyDataEfficiency(const xAOD::Muon& mu, const xAOD::EventInfo* info = 0) const= 0;
+        /// Obtain the muon efficiency measured using the MonteCarlo
+        virtual CorrectionCode getMCEfficiency(const xAOD::Muon& mu, float& eff, const xAOD::EventInfo* info = 0) const = 0;
+        // decorate a muon with the efficiency information
+        virtual CorrectionCode applyMCEfficiency(const xAOD::Muon& mu, const xAOD::EventInfo* info = 0) const = 0;
 
-            ///Obtain the muon efficiency measured using the MonteCarlo
-            virtual CorrectionCode getMCEfficiency(const xAOD::Muon &mu, float& eff, const xAOD::EventInfo* info = 0) const=0;
-            // decorate a muon with the efficiency information
-            virtual CorrectionCode applyMCEfficiency(const xAOD::Muon &mu, const xAOD::EventInfo* info = 0) const =0;
+        // Functions to get the names of the unfolded systematic bins
+        virtual int getUnCorrelatedSystBin(const xAOD::Muon& mu) const = 0;
+        virtual std::string getUncorrelatedSysBinName(unsigned int Bin) const = 0;
+        virtual std::string getUncorrelatedSysBinName(const SystematicSet& systConfig) const = 0;
 
-            //Functions to get the names of the unfolded systematic bins
-            virtual int getUnCorrelatedSystBin(const xAOD::Muon& mu) const=0;
-            virtual std::string getUncorrelatedSysBinName(unsigned int Bin) const = 0;
-            virtual std::string getUncorrelatedSysBinName(const SystematicSet& systConfig) const = 0;
-
-            virtual ~IMuonEfficiencyScaleFactors() {
-            }
-
+        virtual ~IMuonEfficiencyScaleFactors() {}
     };
-// class IMuonEfficiencyTool
+    // class IMuonEfficiencyTool
 
-}// namespace CP
+}  // namespace CP
 
 #endif /* IMUONEFFICIENCYSCALEFACTORS_H_ */
--- PhysicsAnalysis/MuonID/MuonSelectorTools/src/components/MuonSelectorTools_entries.cxx
+++ PhysicsAnalysis/MuonID/MuonSelectorTools/src/components/MuonSelectorTools_entries.cxx
@@ -1,10 +1,10 @@
 /*
   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
-#include "MuonSelectorTools/MuonSelectionTool.h"
-#include "../MuonSelectionAlg.h"
 #include "../MuonQualityUpdaterAlg.h"
+#include "../MuonSelectionAlg.h"
+#include "MuonSelectorTools/MuonSelectionTool.h"
 
-DECLARE_COMPONENT( CP::MuonSelectionTool )
-DECLARE_COMPONENT( CP::MuonSelectionAlg )
-DECLARE_COMPONENT( CP::MuonQualityUpdaterAlg )
+DECLARE_COMPONENT(CP::MuonSelectionTool)
+DECLARE_COMPONENT(CP::MuonSelectionAlg)
+DECLARE_COMPONENT(CP::MuonQualityUpdaterAlg)
