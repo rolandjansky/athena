6c2af809f7647609d76b8061319e1720efede60c
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-05-11 17:46:13 +0200

    Made the setting/retrieval of the "default" btagging object more efficient.
    Most of the clients of these functions do not specify an explicit string
    name for the btag link variable. For those use cases I now introduced
    separate implementations of the functions. Ones that do not need to
    create Accessor and Decorator objects on every call.

--- Event/xAOD/xAODBTagging/xAODBTagging/BTaggingUtilities.h
+++ Event/xAOD/xAODBTagging/xAODBTagging/BTaggingUtilities.h
@@ -1,17 +1,45 @@
-#ifndef XAODBTAGGING_UTILITIES_H
-#define XAODBTAGGING_UTILITIES_H
+// Dear emacs, this is -*- c++ -*-
+//
+// Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+//
+#ifndef XAODBTAGGING_BTAGGINGUTILITIES_H
+#define XAODBTAGGING_BTAGGINGUTILITIES_H
 
-#include "AthContainers/AuxElement.h"
+// Local include(s).
 #include "xAODBTagging/BTaggingContainer.h"
 
+// EDM include(s).
+#include "AthContainers/AuxElement.h"
+
+// System include(s).
+#include <string>
+
 namespace xAOD::BTaggingUtilities {
-    const xAOD::BTagging* getBTagging( const SG::AuxElement&,
-				       const std::string& linkName="btaggingLink" );
-    const ElementLink< xAOD::BTaggingContainer >& getBTaggingLink( const SG::AuxElement&,
-								   const std::string& linkName="btaggingLink" );
-    void setBTaggingLink( const SG::AuxElement&,
-			  const ElementLink< xAOD::BTaggingContainer >&,
-			  const std::string& linkName="btaggingLink" );
+
+   /// The default key for the @c xAOD::BTagging link
+   static const std::string DEFAULT_BTAG_LINK_NAME = "btaggingLink";
+
+   /// Access the default @c xAOD::BTagging object associated to an object
+   const BTagging* getBTagging( const SG::AuxElement& part );
+   /// Access a non-standard @c xAOD::BTagging object associated to an object
+   const BTagging* getBTagging( const SG::AuxElement& part,
+                                const std::string& linkName );
+
+   /// Access the default @c xAOD::BTagging link for an object.
+   const ElementLink< BTaggingContainer >&
+   getBTaggingLink( const SG::AuxElement& part );
+   /// Access a non-standard @c xAOD::BTagging link for an object.
+   const ElementLink< BTaggingContainer >&
+   getBTaggingLink( const SG::AuxElement& part, const std::string& linkName );
+
+   /// Set the default @c xAOD::BTagging link for an object
+   void setBTaggingLink( const SG::AuxElement& part,
+                         const ElementLink< BTaggingContainer >& link );
+   /// Set a non-standard @c xAOD::BTagging link for an object
+   void setBTaggingLink( const SG::AuxElement& part,
+                         const ElementLink< BTaggingContainer >& link,
+                         const std::string& linkName );
+
 } // namespace xAOD::BTaggingUtilities
 
-#endif
+#endif // XAODBTAGGING_BTAGGINGUTILITIES_H
