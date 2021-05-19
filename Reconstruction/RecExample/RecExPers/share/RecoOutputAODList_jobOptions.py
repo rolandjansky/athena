fffee6b1cce03bf880b947ec70e9e56f634f67e9
Giuliano Gustavino <giuliano.gustavino@cern.ch>
2021-05-19 13:55:59 +0000

    LRT electrons - Ensure that they are written to the AODs in any case

--- Reconstruction/RecExample/RecExPers/share/RecoOutputAODList_jobOptions.py
+++ Reconstruction/RecExample/RecExPers/share/RecoOutputAODList_jobOptions.py
@@ -220,12 +220,11 @@ if rec.doEgamma():
     except Exception:
         treatException("Could not load egammaRec/egammaOutputItemList_jobOptions.py" )
 
-    if InDetFlags.doR3LargeD0() and InDetFlags.storeSeparateLargeD0Container():
-        try:
-            include ( "egammaRec/egammaLRTOutputItemList_jobOptions.py" )
-            fullAODList += CfgItemList( "egammaLRTAod", items = egammaLRTAODList )
-        except Exception:
-            treatException("Could not load egammaRec/egammaLRTOutputItemList_jobOptions.py" )
+    try:
+        include ( "egammaRec/egammaLRTOutputItemList_jobOptions.py" )
+        fullAODList += CfgItemList( "egammaLRTAod", items = egammaLRTAODList )
+    except Exception:
+        treatException("Could not load egammaRec/egammaLRTOutputItemList_jobOptions.py" )
 
 
 #eflow
