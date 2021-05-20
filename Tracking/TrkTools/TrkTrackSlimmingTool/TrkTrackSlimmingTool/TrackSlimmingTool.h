209995a6acfbc35173ca72635de31cf1e2605021
christos <christos@cern.ch>
2021-05-21 00:41:35 +0200

    TrackSlimming Tool . Format the code and add some more comments

--- Tracking/TrkTools/TrkTrackSlimmingTool/TrkTrackSlimmingTool/TrackSlimmingTool.h
+++ Tracking/TrkTools/TrkTrackSlimmingTool/TrkTrackSlimmingTool/TrackSlimmingTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -10,95 +10,116 @@
 #define TRKTRACKSLIMMINGTOOL_H
 
 #include "AthenaBaseComps/AthAlgTool.h"
+#include "CxxUtils/checker_macros.h"
 #include "TrkToolInterfaces/ITrackSlimmingTool.h"
 #include "TrkTrack/Track.h"
 #include <memory>
-#include "CxxUtils/checker_macros.h"
 class AtlasDetectorID;
 class Identifier;
 
-namespace Trk 
+namespace Trk {
+class TrackStateOnSurface;
+
+/** @class TrackSlimmingTool
+
+  A tool to produce 'slimmed' Tracks from a reference track.
+
+  By default, this slimmed track will include all the measurements, and the
+  Perigee parameters (currently these are assumed to exist, but eventually an
+  extrapolator should be used to provide it if the perigee is missing)
+
+  @author  Edward Moyse <Edward.Moysecern.ch>
+  @author  Christos Anastopoulos Athena MT modifications
+
+*/
+class TrackSlimmingTool
+  : virtual public ITrackSlimmingTool
+  , public AthAlgTool
 {
-  class TrackStateOnSurface;
+public:
+  TrackSlimmingTool(const std::string&, const std::string&, const IInterface*);
 
-  /** @class TrackSlimmingTool 
+  /** default destructor */
+  virtual ~TrackSlimmingTool();
 
-    A tool to produce 'slimmed' Tracks from a reference track.
+  /** standard Athena-Algorithm method */
+  virtual StatusCode initialize() override;
 
-    By default, this slimmed track will include all the measurements, and the Perigee parameters (currently these
-    are assumed to exist, but eventually an extrapolator should be used to provide it if the perigee is missing)
+  /** standard Athena-Algorithm method */
+  virtual StatusCode finalize() override;
 
-    @author  Edward Moyse <Edward.Moysecern.ch>
-  */
-  class TrackSlimmingTool : virtual public ITrackSlimmingTool, public AthAlgTool
-    {
-    public:
-      TrackSlimmingTool(const std::string&,const std::string&,const IInterface*);
-      
-      /** default destructor */
-      virtual ~TrackSlimmingTool ();
-      
-      /** standard Athena-Algorithm method */
-      virtual StatusCode initialize() override;
-      
-      /** standard Athena-Algorithm method */
-      virtual StatusCode finalize  () override;
-      
-      /**This method 'skims' interesting information from the passed track, and creates a 
-       * new one with cloned copies of this information
-       * When m_setPersistificationHints = False
-	     @param track A const reference to the track to be skimmed. It will not be modified in any way.
-	     @return A 'slimmed' version of 'track', where exactly what information is copied depends on how the tool is configured
-       * When m_setPersistificationHints = True
-       @param track A reference to the track to be skimmed.It gets modified by setting persistification hints
-       @return nullptr
-       The later behaviour can be not thread-safe , look method slimCopy below 
-      */
-      Trk::Track* slim  ATLAS_NOT_THREAD_SAFE (const Trk::Track& track) const override final;
-      /**This method always creates a std::unique_ptr<Trk::Track*> with information removed
-       * based on the tool configuration (m_setPersistificationHints is not used)
-       @param track A const reference to the track to be skimmed. It will not be modified in any way.
-       @return A 'slimmed' version of 'track', where exactly what information is copied depends on how the tool is configured
-       */
-      std::unique_ptr<Trk::Track> slimCopy(const Trk::Track& track) const override final;
-      
-       /**
-        * Slim/skim a non const Track. (m_setPersistificationHints is not used)
-        * @param track A reference to the track to be skimmed. It will be modified.
-        */     
-      void slimTrack(Trk::Track& track) const override final;
-    private:
-      /** any CaloDeposit with its adjacent MEOT's will be kept on the slimmed track (combined muon property) */
-      bool m_keepCaloDeposit;
-	
-      /** If true, Outliers will be kept on the slimmed track*/
-      bool m_keepOutliers;
-      
-      /** If true, the first and last parameters of ID & MS subsystems will be kept on the slimmed track*/
-      bool m_keepParameters;
+  /**This method 'skims' interesting information from the passed track.
+   * @param track A const reference to the track to be skimmed
+   * For compatibility reasons in can do two different things
+   * depending on the value of m_setPersistificationHints.
+   *
+   * When m_setPersistificationHints = False
+   * @return A 'slimmed' version of 'track'.
+   * This is equivalent to calling the slimCopy below
+   *
+   * When m_setPersistificationHints = True
+   * it sets persistification hints
+   * @return nullptr
+   * The later behaviour can be not thread-safe as it
+   * modifies the const TrackStateOnSurfaces attached
+   * to the Trk::Track.
+   */
+  Trk::Track* slim
+  ATLAS_NOT_THREAD_SAFE(const Trk::Track& track) const override final;
 
-      /** Do not create slimmed tracks but only set hints for the persistifier to drop information while writing.
-       */
-      bool m_setPersistificationHints;
-      
-      /**atlas id helper*/
-      const AtlasDetectorID* m_detID;
-      
-      void checkForValidMeas(const Trk::TrackStateOnSurface* tsos, bool& isIDmeas, bool& isMSmeas) const;
-      
+  /**
+   * This method always creates a std::unique_ptr<Trk::Track*> with information
+   * removed
+   * @param track A const reference to the track to be skimmed. It will not be
+   * modified in any way.
+   * @return A 'slimmed' version of 'track', where exactly what information is
+   * copied depends on how the tool is configured
+   */
+  std::unique_ptr<Trk::Track> slimCopy(
+    const Trk::Track& track) const override final;
 
-      void findLastValidTSoS(const DataVector<const Trk::TrackStateOnSurface>* oldTrackStates,
-                             const Trk::TrackStateOnSurface*& lastValidIDTSOS,
-                             const TrackStateOnSurface*& lastValidMSTSOS) const;
-    
-      bool keepParameters(const Trk::TrackStateOnSurface* TSoS,
-                          const TrackStateOnSurface*& firstValidIDTSOS, 
-                          const TrackStateOnSurface*& lastValidIDTSOS,
-                          const TrackStateOnSurface*& firstValidMSTSOS,
-                          const TrackStateOnSurface*& lastValidMSTSOS) const;
+  /**
+   * Slim/skim a non const Track. (m_setPersistificationHints is not used)
+   * @param track A reference to the track to be skimmed.
+   * It will be modified.
+   */
+  void slimTrack(Trk::Track& track) const override final;
 
+private:
+  /** any CaloDeposit with its adjacent MEOT's will be kept on the slimmed track
+   * (combined muon property) */
+  bool m_keepCaloDeposit;
 
-    }; 
+  /** If true, Outliers will be kept on the slimmed track*/
+  bool m_keepOutliers;
+
+  /** If true, the first and last parameters of ID & MS subsystems will be kept
+   * on the slimmed track*/
+  bool m_keepParameters;
+
+  /** Do not create slimmed tracks but only set hints for the persistifier to
+   * drop information while writing.
+   */
+  bool m_setPersistificationHints;
+
+  /**atlas id helper*/
+  const AtlasDetectorID* m_detID;
+
+  void checkForValidMeas(const Trk::TrackStateOnSurface* tsos,
+                         bool& isIDmeas,
+                         bool& isMSmeas) const;
+
+  void findLastValidTSoS(
+    const DataVector<const Trk::TrackStateOnSurface>* oldTrackStates,
+    const Trk::TrackStateOnSurface*& lastValidIDTSOS,
+    const TrackStateOnSurface*& lastValidMSTSOS) const;
+
+  bool keepParameters(const Trk::TrackStateOnSurface* TSoS,
+                      const TrackStateOnSurface*& firstValidIDTSOS,
+                      const TrackStateOnSurface*& lastValidIDTSOS,
+                      const TrackStateOnSurface*& firstValidMSTSOS,
+                      const TrackStateOnSurface*& lastValidMSTSOS) const;
+};
 } // end of namespace
 
-#endif 
+#endif
