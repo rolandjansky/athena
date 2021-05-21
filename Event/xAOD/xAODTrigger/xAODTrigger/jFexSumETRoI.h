8e1fbe0aec3147f5ed8456316084b3ef018a489d
Varsiha Sothilingam <varsiha.sothilingam@kip.uni-heidelberg.de>
2021-05-21 07:30:43 +0000

    L1Calo Bitwise Offline Software Simulation: Sum ET and MET EDMs

---
+++ Event/xAOD/xAODTrigger/xAODTrigger/jFexSumETRoI.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef XAODJFEXSUMETROI_H
+#define XAODJFEXSUMETROI_H
+
+// Local include(s):
+#include "xAODTrigger/versions/jFexSumETRoI_v1.h"
+// Namespace holding all the xAOD EDM classes
+namespace xAOD{
+  /// Define the latest version of the jFexSumETJetRoI class  
+  typedef jFexSumETRoI_v1 jFexSumETRoI;
+}
+
+#include "xAODCore/CLASS_DEF.h"
+CLASS_DEF(xAOD::jFexSumETRoI , 225041099 , 1 )
+#endif //XAODJFEXTAUROI_H
