f7a36e1762f0d1b86b81714d7be8884b3deb8735
Frank Winklmeier <frank.winklmeier@cern.ch>
2021-05-28 12:24:25 +0200

    TrigMETMonitoring: enable thread-checker and file cleanup
    Enable thread-checker and move private headers to src directory.

--- Trigger/TrigMonitoring/TrigMETMonitoring/src/HLTMETMonTool.cxx
+++ Trigger/TrigMonitoring/TrigMETMonitoring/src/HLTMETMonTool.cxx
@@ -1,8 +1,8 @@
 /*
-   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "TrigMETMonitoring/HLTMETMonTool.h"
+#include "HLTMETMonTool.h"
 
 #include "GaudiKernel/MsgStream.h"
 #include "StoreGate/StoreGateSvc.h"
