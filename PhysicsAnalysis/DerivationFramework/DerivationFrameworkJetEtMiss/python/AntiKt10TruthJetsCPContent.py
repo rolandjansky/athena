b8b4882ea19e5a59b96e940a3ca9cd6761b6da98
Takuya Nobe <takuya.nobe@cern.ch>
2021-05-29 13:52:03 +0000

    21.2 tnobe deriv jet

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt10TruthJetsCPContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt10TruthJetsCPContent.py
@@ -6,4 +6,5 @@ AntiKt10TruthJetsCPContent = [
 "AntiKt10TruthJetsAux.GhostBHadronsFinal.GhostCHadronsFinalCount.GhostBHadronsFinalCount.GhostTQuarksFinal.GhostTQuarksFinalCount",
 "AntiKt10TruthJetsAux.GhostHBosons.GhostHBosonsCount.GhostWBosons.GhostWBosonsCount.GhostZBosons.GhostZBosonsCount",
 "AntiKt10TruthJetsAux.R10TruthLabel_R21Precision",
+"AntiKt10TruthJetsAux.Split12.Split23",
 ]
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt10UFOCSSKSoftDropBeta100Zcut10JetsCPContent.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/AntiKt10UFOCSSKSoftDropBeta100Zcut10JetsCPContent.py
@@ -11,4 +11,6 @@ AntiKt10UFOCSSKSoftDropBeta100Zcut10JetsCPContent = [
 "AntiKt10UFOCSSKJets",
 "AntiKt10UFOCSSKJetsAux.NumTrkPt500.GhostVR30Rmax4Rmin02TrackJet_BTagging201903",
 "AntiKt10TruthSoftDropBeta100Zcut10Jets.pt.eta.phi.m.ECF1.ECF2.ECF3.Tau2_wta.Tau3_wta.Qw.GhostCHadronsFinalCount.GhostBHadronsFinalCount",
+"AntiKt10TruthJets.pt.eta.phi.m.Split12.Split23.GhostCHadronsFinalCount.GhostBHadronsFinalCount",
+"AntiKt10TruthWZJetsAux.pt.eta.phi.m.Split12.Split23.GhostCHadronsFinalCount.GhostBHadronsFinalCount",
 ]
