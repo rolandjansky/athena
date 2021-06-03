efe82467f1cc6a7cace3ec6380f4f926d18e2c12
Tomasz Bold <tomasz.bold@gmail.com>
2021-06-03 15:57:45 +0000

    Configure min-bias eff monitoring in depending on the chains present in the menu

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigEffMonitoring.py
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigEffMonitoring.py
@@ -5,6 +5,7 @@
 '''
 @brief configuration for the trigger efficiency monitoring
 '''
+from AthenaConfiguration.AutoConfigFlags import GetFileMD
 
 
 def TrigEff(configFlags):
@@ -22,30 +23,69 @@ def TrigEff(configFlags):
 
     alg.TrackSelectionTool = trkSel
 
-    alg.triggerList = ["HLT_mb_sptrk_L1RD0_FILLED", "HLT_mb_sp_L1RD0_FILLED", "HLT_mb_mbts_L1MBTS_1",
-                       "HLT_mb_mbts_L1MBTS_1_1", "HLT_mb_mbts_L1MBTS_2", "HLT_mb_sp400_trk40_hmt_L1RD0_FILLED",
-                       "HLT_mb_sp600_trk45_hmt_L1RD0_FILLED", "HLT_mb_sp1200_trk60_hmt_L1RD0_FILLED", "HLT_mb_sp1400_trk80_hmt_L1RD0_FILLED",
-                       "HLT_mb_sp2000_trk100_hmt_L1RD0_FILLED", "HLT_mb_sp3000_trk100_hmt_L1RD0_FILLED", "HLT_mb_sp5000_trk80_hmt_L1RD0_FILLED",
-                       "HLT_mb_sp5000_trk100_hmt_L1RD0_FILLED", "HLT_mb_sp5000_trk200_hmt_L1RD0_FILLED", "HLT_mb_sp400_pusup300_trk40_hmt_L1RD0_FILLED"]
-    alg.refTriggerList = ["HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1MBTS_1_1",
-                          "HLT_noalg_L1MBTS_1_1", "HLT_noalg_L1MBTS_2", "HLT_noalg_L1RD0_FILLED",
-                          "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED",
-                          "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED",
-                          "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED", "HLT_noalg_L1RD0_FILLED"]
+
+    # configure the comonitoring dynamically according to the chains present in the menu
+    mbChains = [ c for c in GetFileMD(configFlags.Input.Files)['TriggerMenu']['HLTChains'] if '_mb_' in c]
+
+    # here we keep chain with detailed settings
+    def _c(chain, refchain, **kwargs):
+        conf = {"chain":chain, "refchain": refchain, "xmin":0, "xmax":20 }
+        conf.update(kwargs)
+        return conf
+
+    # check all mb_sptrk chains w.r.t. random noalg
+
+    # define first the basic chains
+    triggerAndRef = [ _c(chain, "HLT_noalg_L1RD0_FILLED")  for chain in mbChains
+                    if ("HLT_mb_sptrk_" in chain or "HLT_mb_sp_" in chain or "HLT_mb_mbts_" in chain)]
+    triggerAndRef += [ _c("HLT_mb_sptrk_L1RD0_FILLED", "HLT_mb_sp_L1RD0_FILLED") ]
+
+    hmt = [c for c in mbChains if ('_hmt_' in c and '_pusup' not in c)]
+    # sort by trk threshold
+    def _trk(chain):
+        part = chain.split("_")
+        for el in part:
+            if el.startswith("trk"):
+                return int(el.strip("trk"))
+        raise RuntimeError(f"Chain {chain} is not the hmt chain")
+
+    hmt.sort(key=lambda c: int(_trk(c)))
+    # monitor first hmt w.r.t sptrk
+    triggerAndRef += [ _c(hmt[0], "HLT_mb_sptrk_L1RD0_FILLED", xmax=_trk(hmt[0])+30)]
+
+    # group set the ref for each trigger to be one of lower threshold
+    triggerAndRef += [  _c(chain, ref, xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain,ref in zip(hmt[1:], hmt) ]
+
+    # pu suppressing trigger should be monitored using trigger of the same threshold w/o pu suppression
+    pusup = [c for c in mbChains if '_hmt_' in c and '_pusup' in c]
+    def _dropsup(chain):
+        s = chain.split("_")
+        return "_".join(s[:3]+s[4:])
+    triggerAndRef += [  _c(chain, _dropsup(chain),  xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain in pusup ]
+
+
+
+
+    alg.triggerList = [ el["chain"] for el in  triggerAndRef ]
+    alg.refTriggerList = [ el["refchain"] for el in  triggerAndRef ]
 
     length = len(alg.triggerList)
 
     mainGroup = monConfig.addGroup(
         alg, 'TrigAll', topPath='HLT/')
     mainGroup.defineHistogram('TrigCounts', title='Trigger counts;;Event rate',
-                              xbins=length, xmin=0, xmax=length, xlabels=list(alg.triggerList))
+                              xbins=length, xmin=0, xmax=len(mbChains), xlabels=list(mbChains))
 
-    for refchain in alg.refTriggerList:
-        for chain in alg.triggerList:
-            effGroup = monConfig.addGroup(
-                alg, chain+refchain, topPath='HLT/EffAll/')
-            effGroup.defineHistogram('EffPassed,nTrkOffline;' + chain + '_ref_' + refchain, type='TEfficiency',
-                                     title=chain+';Offline Good nTrk;Efficiency', xbins=200, xmin=0, xmax=400)
+    for cdef in triggerAndRef:
+        chain = cdef["chain"]
+        refchain   = cdef["refchain"]
+        xmin  = cdef["xmin"]
+        xmax  = cdef["xmax"]
+        xbins = xmax-xmin
+        effGroup = monConfig.addGroup(
+            alg, chain+refchain, topPath='HLT/EffAll/')
+        effGroup.defineHistogram('EffPassed,nTrkOffline;' + chain + '_ref_' + refchain, type='TEfficiency',
+                                  title=chain+';Offline Good nTrk;Efficiency', xbins=xbins, xmin=xmin, xmax=xmax)
 
     return monConfig.result()
 
@@ -62,7 +102,9 @@ if __name__ == '__main__':
 
     #ConfigFlags.Input.Files = ['myAOD.pool.root']
     ConfigFlags.Input.Files = [
-        '/afs/cern.ch/user/k/kburka/workspace/mbts/AOD.25577237._000120.pool.root.1']
+        'AOD.25577237._000002.pool.root.1'
+#        '/afs/cern.ch/user/k/kburka/workspace/mbts/AOD.25577237._000120.pool.root.1'
+    ]
     ConfigFlags.Output.HISTFileName = 'TestEffMonitorOutput.root'
     ConfigFlags.lock()
 
