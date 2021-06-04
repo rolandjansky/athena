f66051b91517f55d83a8804605ecce4dc2de3635
scott snyder <sss@karma>
2021-06-03 23:33:13 -0400

    TileTPCnv: Fix tests.
    Fix use-after-free errors in some unit tests.

--- TileCalorimeter/TileSvc/TileTPCnv/test/TileDigitsContainerCnv_p3_test.cxx
+++ TileCalorimeter/TileSvc/TileTPCnv/test/TileDigitsContainerCnv_p3_test.cxx
@@ -24,13 +24,12 @@
 TileHWID hwid;
 TileTBID tbid;
 TileID   tileid;
-IdDictParser parser;
 
 class TileCablingSvc
 {
 public:
   static
-  void init_idhelpers()
+  void init_idhelpers (IdDictParser& parser)
   {
     tileid.set_do_neighbours (false);
     IdDictMgr& idd = parser.parse ("IdDictParser/ATLAS_IDS.xml");
@@ -172,7 +171,8 @@ void test1()
 int main()
 {
   std::cout << "TileTPCnv/TileDigitsContainerCnv_p3_test\n";
-  TileCablingSvc::init_idhelpers();
+  IdDictParser parser;
+  TileCablingSvc::init_idhelpers(parser);
   test1();
   return 0;
 }
