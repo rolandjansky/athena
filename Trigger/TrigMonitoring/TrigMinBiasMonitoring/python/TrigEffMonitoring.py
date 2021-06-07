7317ba40f13623fd22e1cab29c4be81c304a8532
Tomasz Bold <tomasz.bold@gmail.com>
2021-06-07 15:25:32 +0200

    Check if chains are in place before configuring their monitring

--- Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigEffMonitoring.py
+++ Trigger/TrigMonitoring/TrigMinBiasMonitoring/python/TrigEffMonitoring.py
@@ -13,6 +13,11 @@ def TrigEff(configFlags):
     monConfig = AthMonitorCfgHelper(
         configFlags, 'HLTEfficiencyMonitoringAlgflags')
 
+    # configure the monitoring dynamically according to the chains present in the menu
+    mbChains = [ c for c in GetFileMD(configFlags.Input.Files)['TriggerMenu']['HLTChains'] if '_mb_' in c]
+    if len(mbChains) == 0:
+        return monConfig.result()
+
     from AthenaConfiguration.ComponentFactory import CompFactory
     alg = monConfig.addAlgorithm(
         CompFactory.HLTEfficiencyMonitoringAlg, 'HLTEfficiencyMonitoringAlg')
@@ -20,12 +25,7 @@ def TrigEff(configFlags):
     trkSel = CompFactory.InDet.InDetTrackSelectionTool(
         "InDetTrackSelectionTool_TightPrimary", CutLevel="TightPrimary"
     )
-
     alg.TrackSelectionTool = trkSel
-
-
-    # configure the comonitoring dynamically according to the chains present in the menu
-    mbChains = [ c for c in GetFileMD(configFlags.Input.Files)['TriggerMenu']['HLTChains'] if '_mb_' in c]
 
     # here we keep chain with detailed settings
     def _c(chain, refchain, **kwargs):
@@ -41,27 +41,28 @@ def TrigEff(configFlags):
     triggerAndRef += [ _c("HLT_mb_sptrk_L1RD0_FILLED", "HLT_mb_sp_L1RD0_FILLED") ]
 
     hmt = [c for c in mbChains if ('_hmt_' in c and '_pusup' not in c)]
-    # sort by trk threshold
-    def _trk(chain):
-        part = chain.split("_")
-        for el in part:
-            if el.startswith("trk"):
-                return int(el.strip("trk"))
-        raise RuntimeError(f"Chain {chain} is not the hmt chain")
+    if len(hmt) != 0:
+        # sort by trk threshold
+        def _trk(chain):
+            part = chain.split("_")
+            for el in part:
+                if el.startswith("trk"):
+                    return int(el.strip("trk"))
+            raise RuntimeError(f"Chain {chain} is not the hmt chain")
 
-    hmt.sort(key=lambda c: int(_trk(c)))
-    # monitor first hmt w.r.t sptrk
-    triggerAndRef += [ _c(hmt[0], "HLT_mb_sptrk_L1RD0_FILLED", xmax=_trk(hmt[0])+30)]
+        hmt.sort(key=lambda c: int(_trk(c)))
+        # monitor first hmt w.r.t sptrk
+        triggerAndRef += [ _c(hmt[0], "HLT_mb_sptrk_L1RD0_FILLED", xmax=_trk(hmt[0])+30)]
 
-    # group set the ref for each trigger to be one of lower threshold
-    triggerAndRef += [  _c(chain, ref, xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain,ref in zip(hmt[1:], hmt) ]
+        # group set the ref for each trigger to be one of lower threshold
+        triggerAndRef += [  _c(chain, ref, xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain,ref in zip(hmt[1:], hmt) ]
 
-    # pu suppressing trigger should be monitored using trigger of the same threshold w/o pu suppression
-    pusup = [c for c in mbChains if '_hmt_' in c and '_pusup' in c]
-    def _dropsup(chain):
-        s = chain.split("_")
-        return "_".join(s[:3]+s[4:])
-    triggerAndRef += [  _c(chain, _dropsup(chain),  xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain in pusup ]
+        # pu suppressing trigger should be monitored using trigger of the same threshold w/o pu suppression
+        pusup = [c for c in mbChains if '_hmt_' in c and '_pusup' in c]
+        def _dropsup(chain):
+            s = chain.split("_")
+            return "_".join(s[:3]+s[4:])
+        triggerAndRef += [  _c(chain, _dropsup(chain),  xmin=_trk(chain)-20, xmax=_trk(chain)+30) for chain in pusup ]
 
 
 
@@ -78,14 +79,15 @@ def TrigEff(configFlags):
 
     for cdef in triggerAndRef:
         chain = cdef["chain"]
-        refchain   = cdef["refchain"]
+        refchain = cdef["refchain"]
         xmin  = cdef["xmin"]
         xmax  = cdef["xmax"]
         xbins = xmax-xmin
         effGroup = monConfig.addGroup(
             alg, chain+refchain, topPath='HLT/EffAll/')
-        effGroup.defineHistogram('EffPassed,nTrkOffline;' + chain + '_ref_' + refchain, type='TEfficiency',
+        effGroup.defineHistogram('EffPassed,nTrkOffline;' + chain + '_ref_' + refchain, cutmask="RefPassed", type='TEfficiency',
                                   title=chain+';Offline Good nTrk;Efficiency', xbins=xbins, xmin=xmin, xmax=xmax)
+
 
     return monConfig.result()
 
