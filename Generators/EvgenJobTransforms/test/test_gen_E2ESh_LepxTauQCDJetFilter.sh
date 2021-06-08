30f6bbb8d068a2a7ec93884b29e12fd72c7d2fef
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2021-06-08 17:44:19 +0200

    fix issue in a local ART test and in one of the Powheg processes

--- Generators/EvgenJobTransforms/test/test_gen_E2ESh_LepxTauQCDJetFilter.sh
+++ Generators/EvgenJobTransforms/test/test_gen_E2ESh_LepxTauQCDJetFilter.sh
@@ -8,7 +8,7 @@
 ## Any arguments are considered overrides, and will be added at the end
 export TRF_ECHO=True;
 Gen_tf.py --ecmEnergy=13000 --jobConfig=421408 --maxEvents=10 \
-    --inputEVNT_PreFile=/eos/atlas/atlascerngroupdisk/data-art/grid-input/Evgen_E2E/mc15_13TeV/EVNT.25508216._000003.pool.root.1,/eos/atlas/atlascerngroupdisk/data-art/grid-input/Evgen_E2E/mc15_13TeV/EVNT.25508216._000001.pool.root.1 \
+    --inputEVNT_PreFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Evgen_E2E/mc15_13TeV/EVNT.25508216._000003.pool.root.1,/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Evgen_E2E/mc15_13TeV/EVNT.25508216._000001.pool.root.1 \
     --outputEVNTFile=test_E2E_SH_LeptonxTaunotQCDTruthJetFilters.EVNT.pool.root \
 
 echo "art-result: $? generate"
