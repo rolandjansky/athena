62661b2791beff19e6fc83a36755d91ad9f19727
christos <christos@cern.ch>
2021-06-09 22:07:20 +0200

    CaloCluster On Track simplify

--- Tracking/TrkEvent/TrkCaloCluster_OnTrack/TrkCaloCluster_OnTrack/CaloCluster_OnTrack.h
+++ Tracking/TrkEvent/TrkCaloCluster_OnTrack/TrkCaloCluster_OnTrack/CaloCluster_OnTrack.h
@@ -4,98 +4,108 @@
 
 #ifndef TRKCaloCluster_OnTrack_H
 #define TRKCaloCluster_OnTrack_H
- // Trk
-#include "TrkMeasurementBase/MeasurementBase.h"
+//
 #include "EventPrimitives/EventPrimitives.h"
 #include "GeoPrimitives/GeoPrimitives.h"
+#include "TrkMaterialOnTrack/EnergyLoss.h"
+#include "TrkMeasurementBase/MeasurementBase.h"
+#include "TrkSurfaces/SurfaceHolders.h"
 //
 #include <iosfwd>
 #include <memory>
 class MsgStream;
 
 namespace Trk {
-  class Surface;
-  class EnergyLoss;
+class EnergyLoss;
 
-  /** @class CaloCluster_OnTrack
-    Class to handle Cluster On Tracks (ROT) for CaloClusters,
-    it inherits from the common MeasurementBase.
+/** @class CaloCluster_OnTrack
+  Class to handle Cluster On Tracks (ROT) for CaloClusters,
+  it inherits from the common MeasurementBase.
 
-    The Track holds a vector of TrackStateOnSurface
-    that carry the MeasurmentBase class pointers.
-  */
+  The Track holds a vector of TrackStateOnSurface
+  that carry the MeasurmentBase class pointers.
+*/
 
-  class CaloCluster_OnTrack final: public MeasurementBase {
-    public:
-      /** Default Constructor for POOL */
-      CaloCluster_OnTrack();
-      /** Copy Constructor */
-      CaloCluster_OnTrack(const CaloCluster_OnTrack& cot);
+class CaloCluster_OnTrack final
+  : public MeasurementBase
+  , public SurfacePtrHolder
+{
+public:
+  /** Default Constructor for POOL */
+  CaloCluster_OnTrack();
+  /** Copy Constructor */
+  CaloCluster_OnTrack(const CaloCluster_OnTrack& cot);
+  /** Assignment operator */
+  CaloCluster_OnTrack& operator=(const CaloCluster_OnTrack& cot);
+  /** Move Constructor */
+  CaloCluster_OnTrack(CaloCluster_OnTrack&& cot) noexcept = default;
+  /** Move Assignment  operator */
+  CaloCluster_OnTrack& operator=(CaloCluster_OnTrack&& cot) noexcept = default;
 
-      /** Assignment operator */
-      CaloCluster_OnTrack& operator=(const CaloCluster_OnTrack& cot);
+  /** Constructor with parameters  LocalParameters*,  LocalErrorMatrix*,
+  Surface& This class owns the LocalParameters, ErrorMatrix & EnergyLoss.  A
+  copy of the surface will be made if it is not owned by a detector element. */
+  CaloCluster_OnTrack(const LocalParameters& locpars,
+                      const Amg::MatrixX& locerr,
+                      const Surface& surf,
+                      const EnergyLoss* eloss = nullptr);
 
-      /** Constructor with parameters  LocalParameters*,  LocalErrorMatrix*, Surface&
-      This class owns the LocalParameters, ErrorMatrix & EnergyLoss.  A copy of the
-			surface will be made if it is not owned by a detector element.    */
-      CaloCluster_OnTrack( const LocalParameters& locpars,
-                           const Amg::MatrixX& locerr,
-                           const Surface& surf,
-                           const EnergyLoss* eloss = nullptr);
+  /** Destructor */
+  virtual ~CaloCluster_OnTrack() = default;
 
-      /** Destructor */
-      virtual ~CaloCluster_OnTrack();
+  /** Pseudo-constructor, needed to avoid excessive RTTI*/
+  virtual CaloCluster_OnTrack* clone() const override final;
 
-      /** Pseudo-constructor, needed to avoid excessive RTTI*/
-      virtual CaloCluster_OnTrack* clone() const override final;
-      
-      /**NVI Clone giving up unique pointer */
-       std::unique_ptr<CaloCluster_OnTrack> uniqueClone() const{
-         return std::unique_ptr<CaloCluster_OnTrack>(clone());
-       }
+  /**NVI Clone giving up unique pointer */
+  std::unique_ptr<CaloCluster_OnTrack> uniqueClone() const
+  {
+    return std::unique_ptr<CaloCluster_OnTrack>(clone());
+  }
 
-      /** returns the surface for the local to global transformation
-      - interface from MeasurementBase */
-      virtual const Surface& associatedSurface() const override final;
+  /** returns the surface for the local to global transformation
+  - interface from MeasurementBase */
+  virtual const Surface& associatedSurface() const override final;
 
-      /** Interface method to get the global Position
-      - interface from MeasurementBase */
-      virtual const Amg::Vector3D& globalPosition() const override final;
+  /** Interface method to get the global Position
+  - interface from MeasurementBase */
+  virtual const Amg::Vector3D& globalPosition() const override final;
 
-      /** Extended method to get the EnergyLoss */
-      const Trk::EnergyLoss* energyLoss() const;
+  /** Extended method to get the EnergyLoss */
+  const Trk::EnergyLoss* energyLoss() const;
 
-      /** Extended method checking the type*/
-       virtual bool type(MeasurementBaseType::Type type) const override final{
-         return (type==MeasurementBaseType::CaloCluster_OnTrack);
-       }
+  /** Extended method checking the type*/
+  virtual bool type(MeasurementBaseType::Type type) const override final
+  {
+    return (type == MeasurementBaseType::CaloCluster_OnTrack);
+  }
 
-      /**returns the some information about this CaloCluster_OnTrack. */
-      virtual MsgStream&    dump( MsgStream& out ) const override final;
+  /**returns the some information about this CaloCluster_OnTrack. */
+  virtual MsgStream& dump(MsgStream& out) const override final;
 
-      /**returns the some information about this CaloCluster_OnTrack. */
-      virtual std::ostream& dump( std::ostream& out ) const override final;
+  /**returns the some information about this CaloCluster_OnTrack. */
+  virtual std::ostream& dump(std::ostream& out) const override final;
 
+protected:
+  /** global position of the cluster hit*/
+  Amg::Vector3D m_globalpos;
 
-    protected:
-      /** Surface associated to the measurement*/
-      const Surface* m_surface;
+  /** Energy Loss */
+  std::unique_ptr<const Trk::EnergyLoss> m_eloss;
 
-      /** global position of the cluster hit*/
-      std::unique_ptr<const Amg::Vector3D> m_globalpos;
+}; // End of Class
 
-      /** Energy Loss */
-      const Trk::EnergyLoss* m_eloss;
+} // Namespace
 
+inline const Trk::Surface&
+Trk::CaloCluster_OnTrack::associatedSurface() const
+{
+  return *m_associatedSurface;
+}
 
-  };//End of Class
+inline const Trk::EnergyLoss*
+Trk::CaloCluster_OnTrack::energyLoss() const
+{
+  return m_eloss.get();
+}
 
-}//Namespace
-
-inline const Trk::Surface& Trk::CaloCluster_OnTrack::associatedSurface() const
-{ return *m_surface; }
-
-inline const Trk::EnergyLoss* Trk::CaloCluster_OnTrack::energyLoss() const
-{ return m_eloss; }
-
-#endif //TRKCaloCluster_OnTrack_H
+#endif // TRKCaloCluster_OnTrack_H
