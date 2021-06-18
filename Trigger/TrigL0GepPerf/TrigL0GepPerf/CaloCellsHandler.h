3f9e9dc81084a9c38ebd1aafdde022df638074d0
Maria Mazza <maria.mazza@cern.ch>
2021-06-18 11:15:31 +0000

    TrigL0GepPerf: Add custom jet algorithm functionality

--- Trigger/TrigL0GepPerf/TrigL0GepPerf/CaloCellsHandler.h
+++ Trigger/TrigL0GepPerf/TrigL0GepPerf/CaloCellsHandler.h
@@ -1,9 +1,9 @@
 /*
- *   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+ *   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
  */
 
-#ifndef CaloCellsHandler_H
-#define CaloCellsHandler_H
+#ifndef TRIGL0GEPPERF_CaloCellsHandler_H
+#define TRIGL0GEPPERF_CaloCellsHandler_H
 
 #include "CaloEvent/CaloCellContainer.h"
 #include "CaloEvent/CaloCell.h"
@@ -27,14 +27,14 @@ class CaloCellsHandler{
   void passNoiseTool(ToolHandle<ICalorimeterNoiseTool> m_noiseTool) { noiseTool_cells = m_noiseTool; }
   void passCaloCellIDTool( const CaloCell_ID* ccIdHelper) { m_ccIdHelper = ccIdHelper; }  
 
-  CustomCaloCell getCaloCell(unsigned int id) { return m_caloCells[id]; }
-  std::map<unsigned int,CustomCaloCell> getCaloCellsMap(){ return m_caloCells; }
+  Gep::CustomCaloCell getCaloCell(unsigned int id) { return m_caloCells[id]; }
+  std::map<unsigned int,Gep::CustomCaloCell> getCaloCellsMap(){ return m_caloCells; }
 
 
 
  private:
 
-  std::map<unsigned int,CustomCaloCell> m_caloCells; 
+  std::map<unsigned int,Gep::CustomCaloCell> m_caloCells; 
   ToolHandle<ICalorimeterNoiseTool> noiseTool_cells;
   const mutable CaloCell_ID* m_ccIdHelper;
   std::vector<unsigned int> getNeighbours(const CaloCellContainer* allcells, const CaloCell* acell);
