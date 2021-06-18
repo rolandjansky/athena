411a778a300dae01cedf5ac86d1110cdcb37d8c4
Alexander Kevin Gilbert <alkegit.21@cern.ch>
2021-06-18 09:23:09 +0000

    Adding monitoring histogram to z finder algorithm.

--- Trigger/TrigTools/IDScanZFinder/python/ZFinderAlgConfig.py
+++ Trigger/TrigTools/IDScanZFinder/python/ZFinderAlgConfig.py
@@ -3,4 +3,14 @@
 from TrigEDMConfig.TriggerEDMRun3 import recordable
 from IDScanZFinder.IDScanZFinderConf import TrigZFinderAlg
 MinBiasZFinderAlg = TrigZFinderAlg("TrigZFinderAlg", zFinderKey=recordable("HLT_vtx_z"))
-# TODO add monitoring tool and two histograms for weights and the z
+
+from AthenaMonitoringKernel.GenericMonitoringTool import GenericMonitoringTool
+monTool = GenericMonitoringTool('MonTool')
+monTool.OutputLevel=1
+
+monTool.defineHistogram( 'ZVertex', path='EXPERT', type='TH1F', title='Vertex Z distribution;z [mm];Entries',
+                         xbins=400, xmin=-200, xmax=200 )
+monTool.defineHistogram( 'ZVertexWeight', path='EXPERT', type='TH1F', title='Vertex Weight;Weight;Entries',
+                         xbins=100, xmin=0.0, xmax=100 )
+                         
+MinBiasZFinderAlg.MonTool = monTool
