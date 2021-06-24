521706ab7ed164ff8ca97a63d24673342f2b27e2
scott snyder <sss@karma>
2021-06-24 13:12:54 -0400

    AmdcCore: Enable thread-safety checking.
    Enable thread-safety checking.
    Replace AmdcsimrecAccess with a dummy --- the singleton is never
    actually set anymore.

--- MuonSpectrometer/Amdcsimrec/AmdcCore/AmdcCore/AmdcsimrecAccess.h
+++ MuonSpectrometer/Amdcsimrec/AmdcCore/AmdcCore/AmdcsimrecAccess.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef AMDCCORE_AMDCSIMRECACCESS_H
@@ -18,27 +18,9 @@
 
 class AmdcsimrecAccess{
 public:
-   AmdcsimrecAccess();
-  ~AmdcsimrecAccess();
-
-public:
-///////////////////////////////////
 
    /**Get Amdcsimrec interface*/
    static Amdcsimrec* GetAmdcsimrec();
-
-   /**Set Amdcsimrec interface*/
-   static void SetAmdcsimrec(Amdcsimrec* pAmdcsimrec);
-
-   /**Reset Amdcsimrec interface*/
-   static void ResetAmdcsimrec();
-
-private:
-///////////////////////////////////
-// Data
-
-   static Amdcsimrec* s_Amdcsimrec ; //!< Static Pointer on a Amdcsimrec interface
-   
 };
 #endif
 
