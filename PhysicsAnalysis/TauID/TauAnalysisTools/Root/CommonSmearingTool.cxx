7cc59432b0a8e72b19191ed534dba6bfc4d354fa
Bertrand Martin <martindl@cern.ch>
2021-06-26 10:14:18 +0200

    Update combined TES calibration file in client code
    Hello,
    
    This MR is a follow-up of !44709 where the structure of the calibration file for the combined tau energy scale calibration changed.
    The changed was made to the tau reconstruction but not propagated to client codes.
    This MR should fix the problem, until a better solution is found (client codes should try to pick the tauRec file using tauRecFlags, rather than hardcode a file name).
    
    Cheers,
    Bertrand

--- PhysicsAnalysis/TauID/TauAnalysisTools/Root/CommonSmearingTool.cxx
+++ PhysicsAnalysis/TauID/TauAnalysisTools/Root/CommonSmearingTool.cxx
@@ -143,7 +143,7 @@ StatusCode CommonSmearingTool::initialize()
   if (m_bApplyCombinedTES || m_bApplyMVATES) // CombinedTES has to be available for MVA fix
   {
     ATH_CHECK(ASG_MAKE_ANA_TOOL(m_tTauCombinedTES, TauCombinedTES));
-    ATH_CHECK(m_tTauCombinedTES.setProperty("WeightFileName", "CalibLoopResult_v04-04.root"));
+    ATH_CHECK(m_tTauCombinedTES.setProperty("WeightFileName", "CombinedTES_R22_v0.root"));
     ATH_CHECK(m_tTauCombinedTES.initialize());
   }
 
