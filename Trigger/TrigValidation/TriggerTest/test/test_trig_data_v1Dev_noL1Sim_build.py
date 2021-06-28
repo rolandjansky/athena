78b83010587473ca1ac9f943ac816de0ba0201d2
Yasuyuki Okumura <yasuyuki.okumura@cern.ch>
2021-06-28 08:14:54 +0000

    Removal of CoutRefComp step in test_trig_data_v1Dev_L1SimOnly_phase1_build (ATR-23617)

--- Trigger/TrigValidation/TriggerTest/test/test_trig_data_v1Dev_noL1Sim_build.py
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_data_v1Dev_noL1Sim_build.py
@@ -40,15 +40,5 @@ msgcount.thresholds = {
 }
 msgcount.required = True # make the test exit code depend on this step
 
-# Add a step comparing counts against a reference
-chaindump = test.get_step("ChainDump")
-chaindump.args = '--json --yaml ref_data_v1Dev_build.new'
-refcomp = CheckSteps.ChainCompStep("CountRefComp")
-refcomp.input_file = 'ref_data_v1Dev_build.new'
-refcomp.args += ' --patch'
-refcomp.reference_from_release = True # installed from TriggerTest/share
-refcomp.required = True # Final exit code depends on this step
-CheckSteps.add_step_after_type(test.check_steps, CheckSteps.ChainDumpStep, refcomp)
-
 import sys
 sys.exit(test.run())
