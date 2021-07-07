beeca0596c61f3b12049078f0b1596ec7f0ba102
Tomasz Bold <tomasz.bold@gmail.com>
2021-07-07 18:43:21 +0000

    Nested combinations generator

--- Trigger/TrigSteer/TrigCompositeUtils/Root/Combinators.cxx
+++ Trigger/TrigSteer/TrigCompositeUtils/Root/Combinators.cxx
@@ -2,6 +2,7 @@
   Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 */
 #include <algorithm>
+#include <numeric>
 #include <iostream>
 #include <iterator>
 #include "TrigCompositeUtils/Combinators.h"
@@ -12,6 +13,7 @@ CombinationGenerator::CombinationGenerator( const std::initializer_list<size_t>&
   : m_maxes( collectionSizes ),
     m_current( m_maxes.size() ) {
   reset();
+
 }
 
 void CombinationGenerator::reset() {
@@ -19,13 +21,14 @@ void CombinationGenerator::reset() {
   if ( std::any_of( m_maxes.begin(), m_maxes.end(),
 		    []( size_t m ){ return m == 0; } ) )
     m_current.clear(); 
-  else
+  else {
     fill( m_current.begin(), m_current.end(), 0 );
+  }
 }
 
 void CombinationGenerator::operator++() {
-  if ( m_current.empty() )  return;
-  for ( size_t i = 0, imax = m_current.size(); i < imax; ++i ) { // iterate over current indices and find the index which can be increase
+  if ( m_current.empty() )  return;  
+  for ( size_t i = 0, imax = m_current.size(); i < imax; ++i ) { // iterate over current indices and find the index which can be increased
     if ( m_maxes[i] - m_current[i] == 1 ) { // reached end of the range of i-th collection, return to the beginning
       m_current[i] = 0;
     } else {
@@ -33,18 +36,93 @@ void CombinationGenerator::operator++() {
       return;
     }
   }
-  // unsuccesfull index increase == we need to make op bool returning false and next ++ should not happen
+  
+  // unsuccessful index increase == we need to make op bool returning false and next ++ should not happen
   m_current.clear();
-
 }
+
+UniqueCombinationGenerator::UniqueCombinationGenerator( size_t nelems, size_t comblen)
+  : m_nElements(nelems),
+  m_combLen(comblen),
+  m_current(m_combLen) { 
+    reset(); 
+  }
+
 
 CombinationGenerator::operator bool() const {
-  if ( m_current.empty() )
-    return false;
-  return true;
+  return not m_current.empty();
+}
+
+void UniqueCombinationGenerator::reset() {
+  m_bitmask.resize(m_combLen, true);
+  m_bitmask.resize(m_nElements, false);
+  m_current.resize(m_combLen);
+  std::iota(m_current.begin(), m_current.end(), 0);
+}
+
+UniqueCombinationGenerator::operator bool() const {
+  return not m_current.empty();
+}
+
+void UniqueCombinationGenerator::operator++() {
+  if ( m_current.empty() )  return;  
+
+  const bool exists = std::prev_permutation( m_bitmask.begin(), m_bitmask.end());
+  if ( exists ) {
+    m_current.clear();
+    for (size_t i = 0; i < m_nElements; ++i) {
+      if ( m_bitmask[i] )
+        m_current.push_back(i);
+    }
+    return;
+  }
+  m_current.clear();
 }
 
 
+void NestedUniqueCombinationGenerator::add( const UniqueCombinationGenerator& gen ) { 
+  m_generators.push_back(gen);
+  reset();
+}
+void NestedUniqueCombinationGenerator::reset() {
+  m_current.clear();
+  for ( auto& gen: m_generators) {
+    gen.reset();
+  }
+  cache();
+}
+
+size_t NestedUniqueCombinationGenerator::size() const { 
+  size_t sz = 0;
+  for ( const auto& gen: m_generators) { sz += gen.size(); }
+  return sz;
+}
+
+
+void NestedUniqueCombinationGenerator::operator++() {
+  m_current.clear();
+  for ( auto& gen: m_generators ) { 
+    ++gen;
+    if ( gen ) {
+      cache();
+      return;
+    } else if ( &gen != &m_generators.back()) {
+      gen.reset();
+    }
+  }
+  m_current.clear();
+}
+
+NestedUniqueCombinationGenerator::operator bool() const { 
+  return not m_current.empty();
+}
+
+void NestedUniqueCombinationGenerator::cache() {
+  m_current.clear();
+  for ( auto& gen: m_generators) {
+    m_current.insert(m_current.end(), gen.current().begin(), gen.current().end());
+  }
+}
 
 namespace {
   
