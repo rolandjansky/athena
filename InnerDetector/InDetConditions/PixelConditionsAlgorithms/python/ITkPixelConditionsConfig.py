5a26dba14f8492b26fd78f756ee69b9afff0e817
Tadej Novak <tadej.novak@cern.ch>
2021-07-09 12:29:43 +0200

    Disable charge calibration from DB for ITkPixel

--- InnerDetector/InDetConditions/PixelConditionsAlgorithms/python/ITkPixelConditionsConfig.py
+++ InnerDetector/InDetConditions/PixelConditionsAlgorithms/python/ITkPixelConditionsConfig.py
@@ -45,7 +45,7 @@ def ITkPixelChargeCalibCondAlgCfg(flags, name="ITkPixelChargeCalibCondAlg", **kw
     acc.merge(addFoldersSplitOnline(flags, "PIXEL", "/PIXEL/Onl/PixCalib", "/PIXEL/PixCalib", className="CondAttrListCollection"))
     kwargs.setdefault("PixelDetEleCollKey", "ITkPixelDetectorElementCollection")
     kwargs.setdefault("PixelModuleData", "ITkPixelModuleData")
-    kwargs.setdefault("ReadKey", "/PIXEL/PixCalib")
+    kwargs.setdefault("ReadKey", "")  # TODO: enable when ready
     kwargs.setdefault("WriteKey", "ITkPixelChargeCalibCondData")
     acc.addCondAlgo(CompFactory.PixelChargeCalibCondAlg(name, **kwargs))
     return acc
