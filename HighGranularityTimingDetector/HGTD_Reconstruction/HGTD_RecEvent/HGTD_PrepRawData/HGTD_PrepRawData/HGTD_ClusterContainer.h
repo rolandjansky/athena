ab363aec4acf11ab53207ed395f808112c79d069
David Richard Shope <david.richard.shope@cern.ch>
2021-07-13 06:29:17 +0000

    Port HGTD_Cluster and its TP converter from 21.9 to master

---
+++ HighGranularityTimingDetector/HGTD_Reconstruction/HGTD_RecEvent/HGTD_PrepRawData/HGTD_PrepRawData/HGTD_ClusterContainer.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration.
+ *
+ * @file HGTD_PrepRawData/HGTD_ClusterContainer.h
+ * @author Alexander Leopold <alexander.leopold@cern.ch>
+ * @date Apr, 2021
+ *
+ * @brief The container holds all clusters found in HGTD, the individual
+ *        collections hold the clusters on specific sensors
+ */
+
+#ifndef HGTD_PREPRAWDATA_HGTD_CLUSTERCONTAINER_H
+#define HGTD_PREPRAWDATA_HGTD_CLUSTERCONTAINER_H
+
+#include "AthLinks/DeclareIndexingPolicy.h"
+#include "HGTD_PrepRawData/HGTD_ClusterCollection.h"
+#include "AthenaKernel/CLASS_DEF.h"
+#include "TrkPrepRawData/PrepRawDataContainer.h"
+
+namespace HGTD {
+typedef Trk::PrepRawDataContainer<HGTD_ClusterCollection> HGTD_ClusterContainer;
+}
+
+CLASS_DEF(HGTD::HGTD_ClusterContainer, 1313575059, 1)
+CONTAINER_IS_IDENTCONT(HGTD::HGTD_ClusterContainer)
+
+#endif // HGTD_PREPRAWDATA_HGTD_CLUSTERCONTAINER_H
