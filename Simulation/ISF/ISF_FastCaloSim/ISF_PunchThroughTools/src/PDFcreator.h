51a50b8c0160a8a45e61ba2f579b7ec4e8ea8676
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2021-07-15 11:33:06 +0000

    Manual sweep of !41282 and !40534 from 21.0 to master

--- Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/PDFcreator.h
+++ Simulation/ISF/ISF_FastCaloSim/ISF_PunchThroughTools/src/PDFcreator.h
@@ -2,10 +2,6 @@
   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-///////////////////////////////////////////////////////////////////
-// PDFcreator.h, (c) ATLAS Detector software
-///////////////////////////////////////////////////////////////////
-
 #ifndef ISF_PUNCHTHROUGHTOOLS_SRC_PDFCREATOR_H
 #define ISF_PUNCHTHROUGHTOOLS_SRC_PDFCREATOR_H
 
@@ -15,6 +11,11 @@
 // ROOT includes
 #include "TH1F.h"
 #include "TH2F.h"
+
+//ISF includes
+#include "ISF_FastCaloSimEvent/TFCS1DFunction.h"
+
+
 
 
 namespace ISF
@@ -39,22 +40,22 @@ namespace ISF
 
     /** all following is used to set up the class */
     void setName( std::string PDFname ){ m_name = PDFname; }; //get the pdf's name
-    void addToEnergyEtaRangeHist1DMap(double energy, std::vector<double> etaMinEtaMax, TH1 *hist); //add entry to map linking energy, eta window and histogram
-    void addToEnergyEtaRangeHist2DMap(double energy, std::vector<double> etaMinEtaMax, TH2 *hist);//add entry to map linking energy, eta window and histogram
+    void addToEnergyEtaRangeHist1DMap(double energy, std::vector<double> etaMinEtaMax, TFCS1DFunction *hist); //add entry to map linking energy, eta window and histogram
+    void addToEnergyEtaRangeHist2DMap(double energy, std::vector<double> etaMinEtaMax, std::map< double , TFCS1DFunction* > *hist); //add entry to map linking energy, eta window and histogram
 
     /** get the random value with this method, by providing the input parameters */
     double getRand( const std::vector<double>& inputPar, const double& outEnergy = 0., const double& randMin = 0., const double& randMax = 0.) const;
     std::string getName() const {return m_name;};
-    static bool compareEnergy1D(const std::pair< double , std::map< std::vector<double>, TH1*> > &map, const double &value){ return map.first < value; };
-    static bool compareEnergy2D(const std::pair< double , std::map< std::vector<double>, TH2*> > &map, const double &value){ return map.first < value; };
-    static bool compareEtaMax1D(const std::pair< std::vector<double>, TH1*> &map, const double &value){ return map.first.at(1) < value; };
-    static bool compareEtaMax2D(const std::pair< std::vector<double>, TH2*> &map, const double &value){ return map.first.at(1) < value; };
+    static bool compareEnergy1D(const std::pair< double , std::map< std::vector<double>, TFCS1DFunction*> > map, const double value){ return map.first < value; };
+    static bool compareEnergy2D(const std::pair< double , std::map< std::vector<double>, std::map< double , TFCS1DFunction* >* > > map, const double value){ return map.first < value; };
+    static bool compareEtaMax1D(const std::pair< std::vector<double>, TFCS1DFunction*> map, const double value){ return map.first.at(1) < value; };
+    static bool compareEtaMax2D(const std::pair< std::vector<double>, std::map< double , TFCS1DFunction* >* > map, const double value){ return map.first.at(1) < value; };
 
   private:
     CLHEP::HepRandomEngine             *m_randomEngine;       //!< Random Engine
     std::string                         m_name;               //!< Give pdf a name for debug purposes
-    std::map< double , std::map< std::vector<double>, TH1*> > m_energy_etaRange_hists1D; //!< map of energies to map of eta ranges to 1D histograms
-    std::map< double , std::map< std::vector<double>, TH2*> > m_energy_etaRange_hists2D; //!< map of energies to map of eta ranges to 2D histograms
+    std::map< double , std::map< std::vector<double>, TFCS1DFunction*> > m_energy_etaRange_hists1D; //!< map of energies to map of eta ranges to 1D histograms
+    std::map< double , std::map< std::vector<double>, std::map< double , TFCS1DFunction* >* > > m_energy_etaRange_hists2D; //!< map of energies to map of eta ranges to 2D histograms
     constexpr static double s_sqrtOf2 = M_SQRT2;
 
   };
