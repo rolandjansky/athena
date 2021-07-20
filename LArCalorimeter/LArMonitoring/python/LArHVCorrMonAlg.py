616914f32283d313b7d41be6fe010e640a9665c2
Margherita Spalla <margherita.spalla@cern.ch>
2021-07-20 17:58:26 +0200

    merge opt modified

--- LArCalorimeter/LArMonitoring/python/LArHVCorrMonAlg.py
+++ LArCalorimeter/LArMonitoring/python/LArHVCorrMonAlg.py
@@ -62,135 +62,135 @@ def LArHVCorrMonConfigCore(helper, algoinstance,inputFlags):
 
     #EMB
     hvCorrGroup.defineHistogram('etaEMBA,phiEMBA;LArHVCorrectionEMBA',
-                                          title='HV deviation (nominal - corrected) - EMBA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrEMBA',
-                                          xbins=int(lArDQGlobals.HVeta_EMB["EMBAPS"][2]/delta_eta),xmin=lArDQGlobals.HVeta_EMB["EMBAPS"][1],xmax=lArDQGlobals.HVeta_EMB["EMBAPS"][2],
-                                          ybins=lArDQGlobals.HVphi_EMB["EMBAPS"][0],ymin=lArDQGlobals.HVphi_EMB["EMBAPS"][1],ymax=lArDQGlobals.HVphi_EMB["EMBAPS"][2]
-    )
+                                title='HV deviation (nominal - corrected) - EMBA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrEMBA',
+                                xbins=int(lArDQGlobals.HVeta_EMB["EMBAPS"][2]/delta_eta),xmin=lArDQGlobals.HVeta_EMB["EMBAPS"][1],xmax=lArDQGlobals.HVeta_EMB["EMBAPS"][2],
+                                ybins=lArDQGlobals.HVphi_EMB["EMBAPS"][0],ymin=lArDQGlobals.HVphi_EMB["EMBAPS"][1],ymax=lArDQGlobals.HVphi_EMB["EMBAPS"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('etaEMBC,phiEMBC;LArHVCorrectionEMBC',
-                                          title='HV deviation (nominal - corrected) - EMBA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrEMBC',
-                                          xbins=int(lArDQGlobals.HVeta_EMB["EMBAPS"][2]/delta_eta),xmax=lArDQGlobals.HVeta_EMB["EMBAPS"][1]-0.01,xmin=-lArDQGlobals.HVeta_EMB["EMBAPS"][2]-0.01,
-                                          ybins=lArDQGlobals.HVphi_EMB["EMBAPS"][0],ymin=lArDQGlobals.HVphi_EMB["EMBAPS"][1],ymax=lArDQGlobals.HVphi_EMB["EMBAPS"][2]
-    )
+                                title='HV deviation (nominal - corrected) - EMBA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrEMBC',
+                                xbins=int(lArDQGlobals.HVeta_EMB["EMBAPS"][2]/delta_eta),xmax=lArDQGlobals.HVeta_EMB["EMBAPS"][1]-0.01,xmin=-lArDQGlobals.HVeta_EMB["EMBAPS"][2]-0.01,
+                                ybins=lArDQGlobals.HVphi_EMB["EMBAPS"][0],ymin=lArDQGlobals.HVphi_EMB["EMBAPS"][1],ymax=lArDQGlobals.HVphi_EMB["EMBAPS"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalEMBA;NDeviatingChannelsEMBA',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMBA;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
-
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMBA;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
+    
     hvCorrGroup.defineHistogram('LB,nonnominalEMBC;NDeviatingChannelsEMBC',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMBC;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMBC;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
 
     #EMEC
     hvCorrGroup.defineHistogram('etaEMECA,phiEMECA;LArHVCorrectionEMECA',
-                                          title='HV deviation (nominal - corrected) - EMECA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrEMECA',
-                                          xbins=lArDQGlobals.HVeta_EMEC["EMECA"],
-                                          ybins=lArDQGlobals.HVphi_EMEC["EMECA"]
-    )
+                                title='HV deviation (nominal - corrected) - EMECA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrEMECA',
+                                xbins=lArDQGlobals.HVeta_EMEC["EMECA"],
+                                ybins=lArDQGlobals.HVphi_EMEC["EMECA"],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('etaEMECC,phiEMECC;LArHVCorrectionEMECC',
-                                          title='HV deviation (nominal - corrected) - EMECA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrEMECC',
-                                          xbins=lArDQGlobals.HVeta_EMEC["EMECC"],
-                                          ybins=lArDQGlobals.HVphi_EMEC["EMECC"]
-    )
+                                title='HV deviation (nominal - corrected) - EMECA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrEMECC',
+                                xbins=lArDQGlobals.HVeta_EMEC["EMECC"],
+                                ybins=lArDQGlobals.HVphi_EMEC["EMECC"],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalEMECA;NDeviatingChannelsEMECA',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMECA;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMECA;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalEMECC;NDeviatingChannelsEMECC',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMECC;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - EMECC;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
     
     #HEC
     hvCorrGroup.defineHistogram('etaHECA,phiHECA;LArHVCorrectionHECA',
-                                          title='HV deviation (nominal - corrected) - HECA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrHECA',
-                                          xbins=lArDQGlobals.HVeta_HECFcal["HECA"][0],xmin=lArDQGlobals.HVeta_HECFcal["HECA"][1], xmax=lArDQGlobals.HVeta_HECFcal["HECA"][2],
-                                          ybins=lArDQGlobals.HVphi_HECFcal["HECA"][0], ymin=lArDQGlobals.HVphi_HECFcal["HECA"][1], ymax=lArDQGlobals.HVphi_HECFcal["HECA"][2]
-    )
+                                title='HV deviation (nominal - corrected) - HECA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrHECA',
+                                xbins=lArDQGlobals.HVeta_HECFcal["HECA"][0],xmin=lArDQGlobals.HVeta_HECFcal["HECA"][1], xmax=lArDQGlobals.HVeta_HECFcal["HECA"][2],
+                                ybins=lArDQGlobals.HVphi_HECFcal["HECA"][0], ymin=lArDQGlobals.HVphi_HECFcal["HECA"][1], ymax=lArDQGlobals.HVphi_HECFcal["HECA"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('etaHECC,phiHECC;LArHVCorrectionHECC',
-                                          title='HV deviation (nominal - corrected) - HECA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrHECC',
-                                          xbins=lArDQGlobals.HVeta_HECFcal["HECC"][0],xmin=lArDQGlobals.HVeta_HECFcal["HECC"][1], xmax=lArDQGlobals.HVeta_HECFcal["HECC"][2],
-                                          ybins=lArDQGlobals.HVphi_HECFcal["HECC"][0], ymin=lArDQGlobals.HVphi_HECFcal["HECC"][1], ymax=lArDQGlobals.HVphi_HECFcal["HECC"][2]
-    )
+                                title='HV deviation (nominal - corrected) - HECA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrHECC',
+                                xbins=lArDQGlobals.HVeta_HECFcal["HECC"][0],xmin=lArDQGlobals.HVeta_HECFcal["HECC"][1], xmax=lArDQGlobals.HVeta_HECFcal["HECC"][2],
+                                ybins=lArDQGlobals.HVphi_HECFcal["HECC"][0], ymin=lArDQGlobals.HVphi_HECFcal["HECC"][1], ymax=lArDQGlobals.HVphi_HECFcal["HECC"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalHECA;NDeviatingChannelsHECA',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - HECA;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - HECA;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalHECC;NDeviatingChannelsHECC',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - HECC;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - HECC;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
     
     #FCAL
     hvCorrGroup.defineHistogram('etaFCALA,phiFCALA;LArHVCorrectionFCALA',
-                                          title='HV deviation (nominal - corrected) - FCALA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrFCALA',
-                                          xbins=lArDQGlobals.HVeta_HECFcal["FCalA"][0],xmin=lArDQGlobals.HVeta_HECFcal["FCalA"][1], xmax=lArDQGlobals.HVeta_HECFcal["FCalA"][2],
-                                          ybins=lArDQGlobals.HVphi_HECFcal["FCalA"][0], ymin=lArDQGlobals.HVphi_HECFcal["FCalA"][1], ymax=lArDQGlobals.HVphi_HECFcal["FCalA"][2]
-    )
+                                title='HV deviation (nominal - corrected) - FCALA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrFCALA',
+                                xbins=lArDQGlobals.HVeta_HECFcal["FCalA"][0],xmin=lArDQGlobals.HVeta_HECFcal["FCalA"][1], xmax=lArDQGlobals.HVeta_HECFcal["FCalA"][2],
+                                ybins=lArDQGlobals.HVphi_HECFcal["FCalA"][0], ymin=lArDQGlobals.HVphi_HECFcal["FCalA"][1], ymax=lArDQGlobals.HVphi_HECFcal["FCalA"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('etaFCALC,phiFCALC;LArHVCorrectionFCALC',
-                                          title='HV deviation (nominal - corrected) - FCALA;#eta;#phi',
-                                          type='TH2F',
-                                          path=larHVCorr_hist_path,
-                                          weight='hvcorrFCALC',
-                                          xbins=lArDQGlobals.HVeta_HECFcal["FCalC"][0],xmin=lArDQGlobals.HVeta_HECFcal["FCalC"][1], xmax=lArDQGlobals.HVeta_HECFcal["FCalC"][2],
-                                          ybins=lArDQGlobals.HVphi_HECFcal["FCalC"][0], ymin=lArDQGlobals.HVphi_HECFcal["FCalC"][1], ymax=lArDQGlobals.HVphi_HECFcal["FCalC"][2]
-    )
+                                title='HV deviation (nominal - corrected) - FCALA;#eta;#phi',
+                                type='TH2F',
+                                path=larHVCorr_hist_path,
+                                weight='hvcorrFCALC',
+                                xbins=lArDQGlobals.HVeta_HECFcal["FCalC"][0],xmin=lArDQGlobals.HVeta_HECFcal["FCalC"][1], xmax=lArDQGlobals.HVeta_HECFcal["FCalC"][2],
+                                ybins=lArDQGlobals.HVphi_HECFcal["FCalC"][0], ymin=lArDQGlobals.HVphi_HECFcal["FCalC"][1], ymax=lArDQGlobals.HVphi_HECFcal["FCalC"][2],
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalFCALA;NDeviatingChannelsFCALA',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - FCALA;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - FCALA;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
 
     hvCorrGroup.defineHistogram('LB,nonnominalFCALC;NDeviatingChannelsFCALC',
-                                          title='Number of channels per LB with HV corr>'+str(100*threshold)+' - FCALC;LumiBlock',
-                                          type='TProfile',
-                                          path=larHVCorr_hist_path,
-                                          xbins=num_LB,xmin=0.5,xmax=num_LB+0.5
-    )
+                                title='Number of channels per LB with HV corr>'+str(100*threshold)+' - FCALC;LumiBlock',
+                                type='TProfile',
+                                path=larHVCorr_hist_path,
+                                xbins=num_LB,xmin=0.5,xmax=num_LB+0.5,
+                                merge='weightedAverage')
     
 
 if __name__=='__main__':
