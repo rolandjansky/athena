bc3c8315423b35cb188a738a075ff1b16efb4d7a
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-08-01 18:18:40 +0000

    Update mc20e reco ART tests

--- Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_trigsplit_13TeV.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_trigsplit_13TeV.sh
@@ -6,11 +6,6 @@
 # art-type: grid
 # art-include: master/Athena
 
-#Unset ATHENA_CORE_NUMBER to bypass ATLASJT-418                                                                                                                                                                                                                              
-unset ATHENA_CORE_NUMBER
-unset ATHENA_PROC_NUMBER
-
-export TRF_ECHO=True;
-Reco_tf.py --AMI=r12689 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1 --inputHighPtMinbiasHitsFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/mc16_13TeV.429884.Pythia8EvtGen_A3NNPDF23LO_minbias_inelastic_highjetphoton.simul.HITS.e8314_e7400_s3508/HITS.24109349._010870.pool.root.1 --inputLowPtMinbiasHitsFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/mc16_13TeV.429885.Epos_minbias_inelastic_lowjetphoton.simul.HITS.e8314_e7400_s3508/HITS.24109354._011911.pool.root.1 --maxEvents=500 --jobNumber=1 --imf False --steering RAWtoESD:in-RDO RAWtoESD:in-BS RAWtoESD:in+RDO_TRIG --athenaopts "RAWtoESD:--threads=1" "ESDtoAOD:--threads=1" 
+Reco_tf.py --AMI=r12806 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1 --inputRDO_BKGFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayTests/PresampledPileUp/22.0/Run2/large/mc20e_presampling.2k.latest.RDO.pool.root --maxEvents=500  --imf False --steering RAWtoESD:in-RDO RAWtoESD:in-BS RAWtoESD:in+RDO_TRIG --athenaopts "RAWtoESD:--threads=1" "ESDtoAOD:--threads=1"
 RES=$?
 echo "art-result: $RES Reco"
