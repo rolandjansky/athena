bad82dec8ac1dac835c54601d529c5a27da08076
Alaettin Serhan Mete <alaettin.serhan.mete@cern.ch>
2021-08-04 15:43:34 +0200

    Update DerivationFrameworkTrainsART tests to match TCT ones

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_data18PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_data18PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
@@ -11,14 +11,14 @@
 set -e
 
 ATHENA_CORE_NUMBER=8 Reco_tf.py \
-  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.merge.AOD.f1058_m2064/data18_13TeV.00357750.physics_Main.merge.AOD.f1058_m2064._lb0100._0010.1 \
+  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.merge.AOD.r12879_p4685/AOD.26343283._000019.pool.root.1 \
   --outputDAODFile art.pool.root \
   --reductionConf PHYS PHYSLITE \
   --maxEvents -1 \
   --sharedWriter True \
   --parallelCompression False \
   --multiprocess True \
-  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff(); from AthenaCommon.AlgSequence import AlgSequence; topSequence = AlgSequence(); topSequence += CfgMgr.xAODMaker__DynVarFixerAlg("BTaggingELFixer", Containers = ["BTagging_AntiKt4EMTopoAux."] ); topSequence += CfgMgr.xAODMaker__DynVarFixerAlg("JetELFixer", Containers = ["AntiKt4EMTopoJetsAux."] );'
+  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff();'
 
 echo "art-result: $? reco"
 
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_mc16PHYS_PHYSLITE_mp_sharedwriter.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_mc16PHYS_PHYSLITE_mp_sharedwriter.sh
@@ -11,13 +11,13 @@
 set -e
 
 ATHENA_CORE_NUMBER=8 Reco_tf.py \
-  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.recon.AOD.e3601_s3126_r12305/AOD.23662571._000001.pool.root.1 \
+  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.recon.AOD.e3601_s3126_r12885/* \
   --outputDAODFile art.pool.root \
   --reductionConf PHYS PHYSLITE \
   --maxEvents -1 \
   --sharedWriter True \
   --multiprocess True \
-  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff(); from AthenaCommon.AlgSequence import AlgSequence; topSequence = AlgSequence(); topSequence += CfgMgr.xAODMaker__DynVarFixerAlg("BTaggingELFixer", Containers = ["BTagging_AntiKt4EMTopoAux."] ); topSequence += CfgMgr.xAODMaker__DynVarFixerAlg("JetELFixer", Containers = ["AntiKt4EMTopoJetsAux."] );'
+  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff();'
 
 echo "art-result: $? reco"
 
