ac10474f3e5ee8190b069213adde97c6e9171652
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-08-04 10:37:38 +0200

    Unset grid environment variables in mc20e tests to enable mc20e job transforms to work in ART.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_13TeV.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_13TeV.sh
@@ -6,6 +6,10 @@
 # art-type: grid
 # art-include: master/Athena
 
+#Unset these to get around ATLASJT-420
+unset ATHENA_CORE_NUMBER
+unset ATHENA_PROC_NUMBER
+
 Reco_tf.py --AMI=r12806 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1 --inputRDO_BKGFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayTests/PresampledPileUp/22.0/Run2/large/mc20e_presampling.2k.latest.RDO.pool.root --maxEvents=500 --imf False --athenaopts "RAWtoESD:--threads=1" "ESDtoAOD:--threads=1"
 
 RES=$?
