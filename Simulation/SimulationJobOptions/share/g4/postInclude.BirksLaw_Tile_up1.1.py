c6ac65d0b7040269a6ae4a30313ae0d1d0fedb48
Michael Duehrssen-Debling <michael.duehrssen@cern.ch>
2021-08-05 08:49:17 +0000

    Add postInclude to change Birks law in simulation

---
+++ Simulation/SimulationJobOptions/share/g4/postInclude.BirksLaw_Tile_up1.1.py
@@ -0,0 +1,9 @@
+## postInclude.TileBirks.py
+## Switch off Birks Law corrections entirely
+# ServiceMgr.TileGeoG4SDCalc.DoBirk=False
+import AthenaCommon.SystemOfUnits as Units
+gramsPerMeVcmSq = Units.g/(Units.MeV*Units.cm2)
+## exp. values from NIM 80 (1970) 239-244
+ServiceMgr.TileGeoG4SDCalc.birk1 = 0.0130 * gramsPerMeVcmSq * 1.1 # 20 % up w.r.t. default value
+ServiceMgr.TileGeoG4SDCalc.birk2 = 9.6e-6 * gramsPerMeVcmSq * gramsPerMeVcmSq * 0.0 # set to zero 
+#EOF
