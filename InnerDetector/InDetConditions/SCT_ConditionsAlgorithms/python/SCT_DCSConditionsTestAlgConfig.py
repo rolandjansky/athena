1e93f16ea38cec004e8e6db9566bbf1c366f383f
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2021-08-06 14:27:56 +0000

    Replace usage of ConfigFlags.Input.InitialTimeStamp with ConfigFlags.Input.TimeStamp

--- InnerDetector/InDetConditions/SCT_ConditionsAlgorithms/python/SCT_DCSConditionsTestAlgConfig.py
+++ InnerDetector/InDetConditions/SCT_ConditionsAlgorithms/python/SCT_DCSConditionsTestAlgConfig.py
@@ -25,7 +25,7 @@ if __name__=="__main__":
     ConfigFlags.Input.isMC = False
     ConfigFlags.Input.ProjectName = "data16_13TeV"
     ConfigFlags.Input.RunNumber = 310809
-    ConfigFlags.addFlag("Input.InitialTimeStamp", 1476741326) # LB 18 of run 310809, 10/17/2016 @ 9:55pm (UTC)
+    ConfigFlags.Input.TimeStamp = 1476741326 # LB 18 of run 310809, 10/17/2016 @ 9:55pm (UTC)
     ConfigFlags.IOVDb.GlobalTag = "CONDBR2-BLKPA-2017-06"
     ConfigFlags.GeoModel.AtlasVersion = "ATLAS-R2-2015-03-01-00"
     ConfigFlags.Detector.GeometrySCT = True
