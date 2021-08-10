923a7f2e935223c37a259d5e98377733444987f8
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-08-10 17:18:40 +0000

    Update conditions tag in reco ART tests

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev_MultipleThreads.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev_MultipleThreads.sh
@@ -9,7 +9,7 @@
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
 
-Reco_tf.py --athenaopts="--threads=8" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data16_13TeV.00310809.physics_Main.daq.RAW._lb1219._SFO-2._0001.data --maxEvents 300 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-2016-19"  --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
+Reco_tf.py --athenaopts="--threads=8" --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data16_13TeV.00310809.physics_Main.daq.RAW._lb1219._SFO-2._0001.data --maxEvents 300 --autoConfiguration everything --conditionsTag="CONDBR2-BLKPA-RUN2-06"  --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
 
 RES=$?
 echo "art-result: $RES Reco"
