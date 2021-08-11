6e36c85b99769a240367833ad5b55e1bba1148c6
Masato Aoki <masato.aoki@cern.ch>
2021-08-11 18:37:25 +0000

    bug fix for L1Muon overlap removal

--- MuonSpectrometer/MuonConfig/python/MuonCablingConfig.py
+++ MuonSpectrometer/MuonConfig/python/MuonCablingConfig.py
@@ -18,7 +18,8 @@ def RPCCablingConfigCfg(flags):
         # Relevant folder tags are set for now, until new global tag (RUN3-02) becomes avaialble
         rpcTrigEta="/RPC/TRIGGER/CM_THR_ETA <tag>RPCTriggerCMThrEta_RUN12_MC16_04</tag> <forceRunNumber>330000</forceRunNumber>"
         rpcTrigPhi="/RPC/TRIGGER/CM_THR_PHI <tag>RPCTriggerCMThrPhi_RUN12_MC16_04</tag> <forceRunNumber>330000</forceRunNumber>"
-        if flags.Input.isMC:
+        from AtlasGeoModel.MuonGMJobProperties import MuonGeometryFlags
+        if flags.Input.isMC and MuonGeometryFlags.hasSTGC(): # Run3-geometry
             rpcCabMap="/RPC/CABLING/MAP_SCHEMA <tag>RPCCablingMapSchema_2015-2018Run3-4</tag> <forceRunNumber>330000</forceRunNumber>"
             rpcCabMapCorr="/RPC/CABLING/MAP_SCHEMA_CORR <tag>RPCCablingMapSchemaCorr_2015-2018Run3-4</tag> <forceRunNumber>330000</forceRunNumber>"
 
