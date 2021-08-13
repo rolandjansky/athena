77204fd8894f8ba0832f2012c99cae0bb075882d
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2021-08-13 18:00:04 +0000

    Merge branch 'master-dq-fix-r21-aod' into 'master'
    Fix DQ running on R21 AOD
    
    See merge request atlas/athena!45766
    
    (cherry picked from commit 017a7c47a4d183d657bfdded5aeee4179221b7c1)
    
    80e69ef3 Fix DQ running on R21 AOD

--- Control/AthenaMonitoring/python/AthenaMonitoringAODRecoCfg.py
+++ Control/AthenaMonitoring/python/AthenaMonitoringAODRecoCfg.py
@@ -12,32 +12,38 @@ def AthenaMonitoringAODRecoCfg(flags):
         info('Running on AOD: Scheduling rebuild of standard jet collections if necessary')
         from JetRecConfig.StandardSmallRJets import AntiKt4EMTopo, AntiKt4EMPFlow, AntiKt4LCTopo
         from JetRecConfig.StandardLargeRJets import AntiKt10LCTopo
-        jets_to_schedule = (AntiKt4EMTopo, AntiKt4EMPFlow, AntiKt4LCTopo, AntiKt10LCTopo)
+        jets_to_schedule = [_ for _ in (AntiKt4EMTopo, AntiKt4EMPFlow, AntiKt4LCTopo, AntiKt10LCTopo)
+                            if _.fullname() not in flags.Input.Collections]
 
-        info(f'Ensuring presence of jet collections for monitoring: {jets_to_schedule}')
-        from JetRecConfig.JetRecConfig import JetRecCfg
-        for container in jets_to_schedule:
-            result.merge(JetRecCfg(flags, container))
+        if jets_to_schedule:
+            info(f'Ensuring presence of jet collections for monitoring: {jets_to_schedule}')
+            from JetRecConfig.JetRecConfig import JetRecCfg
+            for container in jets_to_schedule:
+                result.merge(JetRecCfg(flags, container))
 
-        info('Scheduling b-tagging of rebuilt jets')
-        from AtlasGeoModel.AtlasGeoModelConfig import AtlasGeometryCfg
-        result.merge(AtlasGeometryCfg(flags))
-        from BeamSpotConditions.BeamSpotConditionsConfig import BeamSpotCondAlgCfg
-        result.merge(BeamSpotCondAlgCfg(flags))
-        from BTagging.BTagRun3Config import BTagRecoSplitCfg
-        # would rather use removesuffix below but need to wait for Python 3.9
-        for container in [_.fullname().replace('Jets','') for _ in jets_to_schedule]:
-            if container in ('AntiKt4EMTopo', 'AntiKt4EMPFlow'):
+        jet_collections = set([_.fullname().replace('Jets','') for _ in jets_to_schedule])
+        btag_jet_collections = set(['AntiKt4EMTopo', 'AntiKt4EMPFlow'])
+        met_jet_collections = set(['AntiKt4EMTopo', 'AntiKt4EMPFlow', 'AntiKt4LCTopo'])
+
+        if jet_collections & btag_jet_collections:
+            info('Scheduling b-tagging of rebuilt jets')
+            from AtlasGeoModel.AtlasGeoModelConfig import AtlasGeometryCfg
+            result.merge(AtlasGeometryCfg(flags))
+            from BeamSpotConditions.BeamSpotConditionsConfig import BeamSpotCondAlgCfg
+            result.merge(BeamSpotCondAlgCfg(flags))
+            from BTagging.BTagRun3Config import BTagRecoSplitCfg
+            # would rather use removesuffix below but need to wait for Python 3.9
+            for container in jet_collections & btag_jet_collections:
                 result.merge(BTagRecoSplitCfg(flags, [container]))
 
-        info('Scheduling rebuild of standard MET')
-        from METReconstruction.METAssociatorCfg import METAssociatorCfg
-        for container in [_.fullname().replace('Jets','') for _ in jets_to_schedule]:
-            if container in ('AntiKt4EMTopo', 'AntiKt4EMPFlow', 'AntiKt4LCTopo'):
+        if jet_collections & met_jet_collections:
+            info('Scheduling rebuild of standard MET')
+            from METReconstruction.METAssociatorCfg import METAssociatorCfg
+            for container in jet_collections & met_jet_collections:
                 result.merge(METAssociatorCfg(flags, container))          
-        from CaloTools.CaloNoiseCondAlgConfig import CaloNoiseCondAlgCfg
-        result.merge(CaloNoiseCondAlgCfg(flags)) # Prereq for Calo MET
-        from METReconstruction.METCalo_Cfg import METCalo_Cfg
-        result.merge(METCalo_Cfg(flags))
+            from CaloTools.CaloNoiseCondAlgConfig import CaloNoiseCondAlgCfg
+            result.merge(CaloNoiseCondAlgCfg(flags)) # Prereq for Calo MET
+            from METReconstruction.METCalo_Cfg import METCalo_Cfg
+            result.merge(METCalo_Cfg(flags))
 
     return result
