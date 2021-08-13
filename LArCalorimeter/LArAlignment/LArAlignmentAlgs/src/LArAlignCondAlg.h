211874bb252796c646ccdf43a8fb3ecd343e6d6f
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2021-08-13 02:43:13 +0200

    First version of the condition algorithm for applying LAr alignments to GeoModel
    Introduced LArAlignCondAlg nad LArAlignHelper. The latter contains the code for
    applying alignment corrections to LArGeoModel. It is used by both LArAlignCondAlg
    and LArDetectorTool.
    
    Added CONDCONT_DEF for two classes
    * DetCondKeyTrans
    * CaloCellPositionShift
    
    Fixed LArConditionsCommon/python/LArAlignable.py so that it declares "/LAR/Align" and
    "/LAR/LArCellPositionShift" folders to the CondInputLoader, and removed the obsolete
    LArConditionsCommon/share/LArAlignable.py
    
    Switched CaloMgrDetDescrCnv from `fabs` to `std::abs`

---
+++ LArCalorimeter/LArAlignment/LArAlignmentAlgs/src/LArAlignCondAlg.h
@@ -0,0 +1,52 @@
+/*
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef LARALIGNMENTALGS_LARALIGNCONDALG_H
+#define LARALIGNMENTALGS_LARALIGNCONDALG_H
+
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
+#include "StoreGate/ReadCondHandleKey.h"
+#include "StoreGate/WriteCondHandleKey.h"
+#include "GaudiKernel/ICondSvc.h"
+
+#include "DetDescrConditions/DetCondKeyTrans.h"
+#include "GeoModelUtilities/GeoAlignmentStore.h"
+#include "GeoModelUtilities/StoredAlignX.h"
+
+#include "LArGeoCode/LArAlignHelper.h"
+
+/**
+ * @class LArAlignCondAlg
+ *
+ * @brief Condition Algorithm for applying LAr Alignments
+ *
+ **/
+
+class LArAlignCondAlg final : public AthReentrantAlgorithm
+{
+ public:
+  using AthReentrantAlgorithm::AthReentrantAlgorithm;
+  virtual ~LArAlignCondAlg() = default;
+
+  virtual StatusCode initialize() override;
+  virtual StatusCode execute(const EventContext& ctx) const override;
+  virtual StatusCode finalize() override {return StatusCode::SUCCESS;};
+
+ private:
+  SG::ReadCondHandleKey<DetCondKeyTrans> m_readKey {this
+      , "LArAlignFolder"
+      , "/LAR/Align"
+      , "SG key of DetCondKeyTrans object with LAr alignments" };
+
+  SG::WriteCondHandleKey<GeoAlignmentStore>  m_writeKey {this
+      , "LArAlignmentStore"
+      , "LArAlignmentStore"
+      , "SG key of the resulting GeoAlignmentStore for LAr" };
+
+  ServiceHandle<ICondSvc> m_condSvc{this, "CondSvc", "CondSvc"};
+
+  LArAlignHelper m_alignHelper;
+};
+
+#endif
