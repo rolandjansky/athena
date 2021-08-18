365155d5bc43980fcc3a9a02ac666a3b50e582ac
Frank Winklmeier <frank.winklmeier@cern.ch>
2021-08-18 10:50:58 +0200

    TrigT1Result: delete RecRoIBResult
    Delete unused `RecRoIBResult` class and its converter (tool). Even in
    Run-2 this class was not being used. Instead the individual RoI classes
    were used directly in the HLT.

--- Trigger/TrigT1/TrigT1ResultByteStream/src/components/TrigT1ResultByteStream_entries.cxx
+++ Trigger/TrigT1/TrigT1ResultByteStream/src/components/TrigT1ResultByteStream_entries.cxx
@@ -1,6 +1,5 @@
 #include "../L1TriggerResultByteStreamCnv.h"
 #include "../RoIBResultByteStreamCnv.h"
-#include "../RecRoIBResultByteStreamCnv.h"
 
 #include "../MuCTPIByteStreamCnv.h"
 #include "../CTPByteStreamCnv.h"
@@ -11,7 +10,6 @@
 #include "../MuCTPIByteStreamTool.h"
 #include "../RecCTPByteStreamTool.h"
 #include "../RecMuCTPIByteStreamTool.h"
-#include "../RecRoIBResultByteStreamTool.h"
 #include "../RoIBResultByteStreamTool.h"
 
 #include "../L1TriggerByteStreamDecoderAlg.h"
@@ -23,12 +21,10 @@ typedef OFFLINE_FRAGMENTS_NAMESPACE::ROBFragment ROBF;
 
 // converter for offline 
 typedef RoIBResultByteStreamCnv<ROBF> RoIBResultByteStreamCnvT ;
-typedef RecRoIBResultByteStreamCnv<ROBF> RecRoIBResultByteStreamCnvT ;
 
 // declare 
 DECLARE_CONVERTER( L1TriggerResultByteStreamCnv )
 DECLARE_CONVERTER( RoIBResultByteStreamCnvT )
-DECLARE_CONVERTER( RecRoIBResultByteStreamCnvT )
 DECLARE_CONVERTER( MuCTPIByteStreamCnv )
 DECLARE_CONVERTER( CTPByteStreamCnv )
 DECLARE_CONVERTER( RecMuCTPIByteStreamCnv )
@@ -38,7 +34,6 @@ DECLARE_COMPONENT( CTPByteStreamTool )
 DECLARE_COMPONENT( MuCTPIByteStreamTool )
 DECLARE_COMPONENT( RecCTPByteStreamTool )
 DECLARE_COMPONENT( RecMuCTPIByteStreamTool )
-DECLARE_COMPONENT( RecRoIBResultByteStreamTool )
 DECLARE_COMPONENT( RoIBResultByteStreamTool )
 
 DECLARE_COMPONENT( L1TriggerByteStreamDecoderAlg )
