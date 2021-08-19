7f9a9fb139fe493f773bf0416226fec4178907a9
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2021-08-19 12:27:52 +0200

    MadGraph: switch to version 2.9.3.atlas2

--- Projects/AthGeneration/externals/MadGraph5Amc.cmake
+++ Projects/AthGeneration/externals/MadGraph5Amc.cmake
@@ -1,6 +1,6 @@
 #    
 # File specifying the location of MadGraph to use.
 #
-set( MADGRAPH5AMC_LCGVERSION 3.1.1.atlas )
+set( MADGRAPH5AMC_LCGVERSION 2.9.3.atlas2 )
 set( MADGRAPH5AMC_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/madgraph5amc/${MADGRAPH5AMC_LCGVERSION}/${LCG_PLATFORM} )
