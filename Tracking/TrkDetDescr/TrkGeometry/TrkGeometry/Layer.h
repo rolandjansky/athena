d1cd767438f42f2f41bd51b38d3d99cd57727181
christos <christos@cern.ch>
2021-08-19 20:13:15 +0200

    Trk::Layer . Remove 4 const overloads marked as ATLAS_NOT_THREAD_SAFE due to const_cast. Do the const_cast in the single client method that needs them

--- Tracking/TrkDetDescr/TrkGeometry/TrkGeometry/Layer.h
+++ Tracking/TrkDetDescr/TrkGeometry/TrkGeometry/Layer.h
@@ -157,8 +157,6 @@ class Layer {
   const Layer* previousLayer(bool skipNavLayer = false) const;
   /** set the previous Layer*/
   void setPreviousLayer(const Layer*);
-  /** set the previous Layer const not const thread safe*/
-  void setPreviousLayer ATLAS_NOT_THREAD_SAFE(const Layer*) const;
 
   /** getting the next/previous Layer if registered - unit for direction vector
    * required */
@@ -169,8 +167,6 @@ class Layer {
   const Layer* nextLayer(bool skipNavLayer = false) const;
   /** set the next Layer*/
   void setNextLayer(const Layer*);
-  /** set the next Layer const not const thread safe*/
-  void setNextLayer ATLAS_NOT_THREAD_SAFE(const Layer*) const;
 
   /** access the BinUtility*/
   const BinUtility* binUtility() const;
@@ -264,8 +260,6 @@ class Layer {
   double getRef() const;
 
   void encloseTrackingVolume(const TrackingVolume& tvol);
-  void encloseTrackingVolume
-  ATLAS_NOT_THREAD_SAFE(const TrackingVolume& tvol) const;
   //!< private method to set the enclosed detached TV
   void encloseDetachedTrackingVolume(const DetachedTrackingVolume& tvol);
   void encloseDetachedTrackingVolume
