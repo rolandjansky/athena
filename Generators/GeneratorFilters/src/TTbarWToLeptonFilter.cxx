e853ac1dd864405ba2f6d62aa48d369e3c6ebb37
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2021-08-19 18:44:58 +0000

    GeneratorFilters: add changes to TTbarWToLeptonFilter as in 21.6

--- Generators/GeneratorFilters/src/TTbarWToLeptonFilter.cxx
+++ Generators/GeneratorFilters/src/TTbarWToLeptonFilter.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "GeneratorFilters/TTbarWToLeptonFilter.h"
@@ -9,6 +9,9 @@ TTbarWToLeptonFilter::TTbarWToLeptonFilter(const std::string& name, ISvcLocator*
 {
   declareProperty("Ptcut", m_Ptmin=200000.);
   declareProperty("NumLeptons", m_numLeptons=-1); // Negative for >0, positive integers for the specific number
+  declareProperty("fourTopsFilter", m_fourTopsFilter=false);//four top filter or not
+  declareProperty("SSMLFilter", m_SSMLFilter=false);//Same sign multilepton filter or not
+
 }
 
 
@@ -18,6 +21,12 @@ StatusCode TTbarWToLeptonFilter::filterEvent() {
   int N_quark_t_all    = 0;
   int N_quark_tbar_all = 0;
   int N_pt_above_cut = 0;
+  int N_pt_above_cut_plus = 0;
+  int N_pt_above_cut_minus = 0;
+
+  int foundlepton[6] = {0, 0, 0, 0, 0, 0}; // e+, e-, mu+, mu-, tau+, tau-
+  int  count_found_leptons = 1; // In ttbar each charged lepton flavour is counted at most once
+  if(m_fourTopsFilter) count_found_leptons = 2; // In four tops, one can have the same charged lepton flavour twice
 
   for (McEventCollection::const_iterator itr = events()->begin(); itr!=events()->end(); ++itr) {
     const HepMC::GenEvent* genEvt = (*itr);
@@ -105,33 +114,72 @@ StatusCode TTbarWToLeptonFilter::filterEvent() {
                 HepMC::GenVertex::particles_out_const_iterator grandchild_mcpartItrE = w_decayVtx->particles_out_const_end();
 
                 for (; grandchild_mcpartItr != grandchild_mcpartItrE; ++grandchild_mcpartItr) {
+      HepMC::GenParticle * grandchild_mcpart = (*grandchild_mcpartItr);
+		  int grandchild_pid = grandchild_mcpart->pdg_id();
 
-			      HepMC::GenParticle * grandchild_mcpart = (*grandchild_mcpartItr);
-			      int grandchild_pid = grandchild_mcpart->pdg_id();
+		  ATH_MSG_DEBUG("W (t/tbar) has " << mcpart_n_particles_out << " children and the pdg_id of the next is " << grandchild_pid);
 
-			      ATH_MSG_DEBUG("W (t/tbar) has " << mcpart_n_particles_out << " children and the pdg_id of the next is " << grandchild_pid);
+		  // Check if the W's child is W again. If yes, then move to its next decay vertex in a decay tree
+		  if (std::abs(grandchild_pid) == 24) {
+		    w_decayVtx = grandchild_mcpart->end_vertex();
 
-			      // Check if the W's child is W again. If yes, then move to its next decay vertex in a decay tree
-			      if (std::abs(grandchild_pid) == 24) {
-					w_decayVtx = grandchild_mcpart->end_vertex();
+		    // If something wrong comes from truth...
+		    if (!w_decayVtx) {
+		      ATH_MSG_ERROR("A stable W is found... ");
+		      break;
+		    }
 
-					// If something wrong comes from truth...
-					if (!w_decayVtx) {
-                      ATH_MSG_ERROR("A stable W is found... ");
-                      break;
-					}
+		    useNextVertex = true;
+		    break;
+		  }
 
-					useNextVertex = true;
-					break;
-			      }
+		  // use brute force to use only leptons that have not been found already 
+		  if (grandchild_pid == -11 && foundlepton[0] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[0]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_minus++;
+		    }
+		  }
+		  if (grandchild_pid == 11 && foundlepton[1] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[1]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_plus++;
 
-			      if (std::abs(grandchild_pid) == 11 ||  abs(grandchild_pid) == 13 || abs(grandchild_pid) == 15) {
-					if (grandchild_mcpart->momentum().perp() >= m_Ptmin) N_pt_above_cut++;
-					// W decay lepton is found. Break loop over the decay product particles
-					break;
-			      }
-                }
-
+		    }
+		  }
+		  if (grandchild_pid == -13 && foundlepton[2] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[2]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_minus++;
+		    }
+		  }
+		  if (grandchild_pid == 13 && foundlepton[3] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[3]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_plus++;
+		    }
+		  }
+		  if (grandchild_pid == -15 && foundlepton[4] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[4]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_minus++;
+		    }
+		  }
+		  if (grandchild_pid == 15 && foundlepton[5] < count_found_leptons) {
+		    if (grandchild_mcpart->momentum().perp() >= m_Ptmin) { 
+		      foundlepton[5]++;
+		      N_pt_above_cut++;
+                      N_pt_above_cut_plus++;
+		    }
+		  }
+			      
+		}
+		
                 // If investigation of W's next decay vertex is not required then finish looking for leptons
                 if (!useNextVertex) break;
               }
@@ -149,13 +197,17 @@ StatusCode TTbarWToLeptonFilter::filterEvent() {
   ATH_MSG_INFO("Found " << N_quark_tbar << " tbar -> W X decays");
   ATH_MSG_INFO("Num leptons from W decays from tops with lepton pt above " << m_Ptmin/1000.0 << " CLHEP::GeV/c = " << N_pt_above_cut);
 
-  if (N_quark_t_all < 1 || N_quark_tbar_all < 1) {
+  int count_tops = 1;
+  if(m_fourTopsFilter) count_tops = 2;
+  if (N_quark_t_all < count_tops || N_quark_tbar_all < count_tops) {
+
     ATH_MSG_ERROR("No t or tbar quarks were found in a (presumably) ttbar event! Event is rejected.");
     setFilterPassed(false);
     return StatusCode::SUCCESS;
   }
 
-  if (N_quark_t < 1 || N_quark_tbar < 1) {
+    if (N_quark_t < count_tops || N_quark_tbar < count_tops) {
+
     ATH_MSG_ERROR("No t or tbar quarks were found decaying to W in a (presumably) ttbar event! Event is rejected. Event dump follows.");
     int event = 0;
     for (McEventCollection::const_iterator itr = events()->begin(); itr!=events()->end(); ++itr) {
@@ -184,12 +236,18 @@ StatusCode TTbarWToLeptonFilter::filterEvent() {
 
   // If NumLeptons is negative (default), accept if Nlep > 0, otherwise only accept an exact match
   if (m_numLeptons < 0) {
-    if (N_quark_t >= 2 || N_quark_tbar >= 2) {
+    if ( (N_quark_t >= 2 || N_quark_tbar >= 2) && !m_fourTopsFilter) {
+      ATH_MSG_WARNING("More than one t -> W X or tbar -> W X decays found. Event is accepted anyway.");
+    }
+    if ( (N_quark_t > 2 || N_quark_tbar > 2) && m_fourTopsFilter) {
       ATH_MSG_WARNING("More than one t -> W X or tbar -> W X decays found. Event is accepted anyway.");
     }
     setFilterPassed(N_pt_above_cut > 0);
   } else {
-    setFilterPassed(N_pt_above_cut == m_numLeptons);
+    if(m_fourTopsFilter){
+      if(m_SSMLFilter) setFilterPassed( (N_pt_above_cut >= m_numLeptons) && (N_pt_above_cut_plus >= 2 || N_pt_above_cut_minus >= 2));
+      else setFilterPassed(N_pt_above_cut >= m_numLeptons);}
+    else setFilterPassed(N_pt_above_cut == m_numLeptons);
   }
 
   return StatusCode::SUCCESS;
