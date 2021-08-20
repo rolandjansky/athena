7b3a8ecaf9d3f952445cd1836deb4871ad469178
Stewart Martin-Haugh <smh@cern.ch>
2021-08-20 10:16:03 +0200

    Remove MergeSlices and ReprocessTriggerTowers: not referenced anywhere since 2016

--- Trigger/TrigT1/TrigT1CaloSim/src/components/TrigT1CaloSim_entries.cxx
+++ Trigger/TrigT1/TrigT1CaloSim/src/components/TrigT1CaloSim_entries.cxx
@@ -1,9 +1,7 @@
 #include "../Run2TriggerTowerMaker.h"
 #include "../Run2CPMTowerMaker.h"
 #include "../Run2JetElementMaker.h"
-#include "../MergeSlices.h"
 #include "../Tester.h"
-#include "../ReprocessTriggerTowers.h"
 #include "../CPMSim.h"
 #include "../JEMJetSim.h"
 #include "../JEMEnergySim.h"
@@ -22,9 +20,7 @@ using namespace LVL1;
 DECLARE_COMPONENT( Run2TriggerTowerMaker )
 DECLARE_COMPONENT( Run2CPMTowerMaker )
 DECLARE_COMPONENT( Run2JetElementMaker )
-DECLARE_COMPONENT( MergeSlices )
 DECLARE_COMPONENT( Tester )
-DECLARE_COMPONENT( ReprocessTriggerTowers )
 DECLARE_COMPONENT( CPMSim )
 DECLARE_COMPONENT( JEMJetSim )
 DECLARE_COMPONENT( JEMEnergySim )
