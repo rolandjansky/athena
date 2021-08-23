06aede6a8b7a3828b9cc9a0436150bfcaa7996ff
Frank Winklmeier <frank.winklmeier@cern.ch>
2021-08-23 15:53:22 +0200

    TrigConfigSvc: remove legacy configuration support and deprecate service
    Remove the legacy configuration support `UseNewConfig==False` and
    deprecate all interface methods. No client should be using this service
    instance anymore. In a second step we can remove the `ITrigConfigSvc`
    inheritance and possibly completely delete this service in favor of
    instantiating `LVL1/HLTConfigSvc` directly where needed.
    
    Relates to ATR-23102.

--- Trigger/TrigConfiguration/TrigConfigSvc/src/TrigConfigSvc.h
+++ Trigger/TrigConfiguration/TrigConfigSvc/src/TrigConfigSvc.h
@@ -2,26 +2,15 @@
   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-/////////////////////////////////////////////////////////////////////
-//
-// NAME:     TrigConfigSvc.h
-// PACKAGE:  TrigConfig
-//
-// AUTHORs:  Till Eifert, University of Geneva
-//
-// CREATED:  27-Dec-2007
-//
-// PURPOSE:
-//
-//////////////////////////////////////////////////////////////////////
-
 #ifndef TrigConfig_TrigConfigSvc
 #define TrigConfig_TrigConfigSvc
 
 #include "AthenaBaseComps/AthService.h"
+#include "GaudiKernel/ServiceHandle.h"
 #include "TrigConfInterfaces/ITrigConfigSvc.h"
+#include "TrigConfInterfaces/ILVL1ConfigSvc.h"
+#include "TrigConfInterfaces/IHLTConfigSvc.h"
 #include "TrigConfL1Data/CTPConfig.h"
-#include "TrigConfHLTData/HLTFrame.h"
 
 #include "TrigConfData/L1Menu.h"
 #include "TrigConfData/L1PrescalesSet.h"
@@ -29,14 +18,20 @@
 #include "TrigConfData/HLTMenu.h"
 #include "TrigConfData/HLTPrescalesSet.h"
 
-#include <stdint.h>
+#include "TrigConfHLTData/HLTFrame.h"
 
 class EventContext;
 
+#define DONOTUSE \
+   ATH_MSG_ERROR("TrigConfigSvc::" <<__func__ << " is deprecated. Use xAODConfigSvc or L1/HLT menu from DetectorStore.")
+
 namespace TrigConf {
 
-   enum TrigConfSrc {DS = 0, XML, XMLL1, XMLHLT };
-
+   /**
+    * Obsolete Run-2 wrapper service. Its only function currently is to initialize the
+    * LVL1/HLTConfigSvc. All its interface methods are deprecated and the service should be
+    * removed eventually.
+    */
    class TrigConfigSvc : public extends<AthService, ITrigConfigSvc>
    {
    public:
@@ -45,40 +40,39 @@ namespace TrigConf {
 
       virtual StatusCode initialize() override;
 
-      static const InterfaceID& interfaceID() { return ITrigConfigSvc::interfaceID(); }
-
       // Lvl1 Accessors
-      virtual const Muctpi*          muctpiConfig() const override;
-      virtual const CTPConfig*       ctpConfig() const override;
-      virtual const ThresholdConfig* thresholdConfig() const override;
-      virtual const BunchGroupSet*   bunchGroupSet() const override;
+      virtual const Muctpi*          muctpiConfig() const override { DONOTUSE; return nullptr; }
+      virtual const CTPConfig*       ctpConfig() const override { DONOTUSE; return nullptr; }
+      virtual const ThresholdConfig* thresholdConfig() const override { DONOTUSE; return nullptr; }
+      virtual const BunchGroupSet*   bunchGroupSet() const override { DONOTUSE; return nullptr; }
 
       // HLT accessors
-      virtual const HLTChainList&    chains() const override;
-      virtual const HLTSequenceList& sequences() const override;
+      virtual const HLTChainList&    chains() const override { DONOTUSE; return m_NullFrame.getHLTChainList(); }
+      virtual const HLTSequenceList& sequences() const override{ DONOTUSE; return m_NullFrame.getHLTSequenceList(); }
 
       // Super Master Key - 0 if from XML, other cases TBI.
-      virtual uint32_t masterKey() const override;
-      virtual uint32_t lvl1PrescaleKey() const override;
-      virtual uint32_t hltPrescaleKey() const override;
-      virtual std::string configurationSource() const override {
-         return "";
-      }
+      virtual uint32_t masterKey() const override { DONOTUSE; return 0; }
+      virtual uint32_t lvl1PrescaleKey() const override { DONOTUSE; return 0; }
+      virtual uint32_t hltPrescaleKey() const override { DONOTUSE; return 0; }
+      virtual std::string configurationSource() const override { DONOTUSE; return {}; }
 
       /// @name Dummy implementations of the Run 3 L1 JSON trigger configuration interface in IILVL1ConfigSvc.
       /// @brief Use the xAODConfigSvc or xAODConfigTool to access these data.
       /// @{
       virtual const ::TrigConf::L1Menu& l1Menu(const ::EventContext&) const override {
+        DONOTUSE;
         const static ::TrigConf::L1Menu dummy = ::TrigConf::L1Menu();
         return dummy;
       }
 
       virtual const ::TrigConf::L1PrescalesSet& l1PrescalesSet(const ::EventContext&) const override {
+        DONOTUSE;
         const static ::TrigConf::L1PrescalesSet dummy = ::TrigConf::L1PrescalesSet();
         return dummy;
       }
 
       virtual const ::TrigConf::L1BunchGroupSet& l1BunchGroupSet(const ::EventContext&) const override {
+        DONOTUSE;
         const static ::TrigConf::L1BunchGroupSet dummy = ::TrigConf::L1BunchGroupSet();
         return dummy;
       }
@@ -88,27 +82,24 @@ namespace TrigConf {
       /// @brief Use the xAODConfigSvc or xAODConfigTool to access these data.
       /// @{
       virtual const ::TrigConf::HLTMenu& hltMenu(const ::EventContext&) const override {
+         DONOTUSE;
          const static ::TrigConf::HLTMenu dummy = ::TrigConf::HLTMenu();
          return dummy;
       }
 
       virtual const ::TrigConf::HLTPrescalesSet& hltPrescalesSet(const ::EventContext&) const override {
+         DONOTUSE;
          const static ::TrigConf::HLTPrescalesSet dummy = ::TrigConf::HLTPrescalesSet();
          return dummy;
       }
       /// @}
 
    private:
+      ServiceHandle<TrigConf::ILVL1ConfigSvc> m_lvl1Svc{this, "LVL1ConfigSvc",
+           "TrigConf::LVL1ConfigSvc/LVL1ConfigSvc", "Provider for L1 menu in DetectorStore"};
 
-      // jobProperties
-      ServiceHandle<TrigConf::ILVL1ConfigSvc>   m_lvl1Svc; //!< read from xml/db
-      ServiceHandle<TrigConf::IHLTConfigSvc>    m_hltSvc;  //!< read from xml/db
-      ServiceHandle<TrigConf::ITrigConfigSvc >  m_dsSvc;  //!< read from detectorStore (header)
-      std::vector<std::string> m_priorityList;
-      Gaudi::Property<bool> m_useNewConfig { this, "UseNewConfig", false, "When true, only initialize LVL1ConfigSvc and HLTConfigSvc" };
-
-      TrigConf::ILVL1ConfigSvc*   m_l1service  {nullptr};    //!< the available l1 service that will be used
-      TrigConf::IHLTConfigSvc*    m_hltservice {nullptr};   //!< the available hlt service that will be used
+      ServiceHandle<TrigConf::IHLTConfigSvc> m_hltSvc{this, "HLTConfigSvc",
+           "TrigConf::HLTConfigSvc/HLTConfigSvc", "Provider for HLT menu in DetectorStore"};
 
       HLTFrame m_NullFrame;
    };
