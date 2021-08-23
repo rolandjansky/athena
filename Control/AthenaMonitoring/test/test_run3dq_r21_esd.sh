71a3aa1cc13b4f206b4dd0eb3701881b6e7b635a
Peter Onyisi <ponyisi@utexas.edu>
2021-08-23 14:55:54 +0000

    Improve DQ handling of R21 AOD files (and ART updates)

--- Control/AthenaMonitoring/test/test_run3dq_r21_esd.sh
+++ Control/AthenaMonitoring/test/test_run3dq_r21_esd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# art-description: ESD->HIST, R21 data ESD
+# art-description: ESD->HIST, R21 data ESD, MT
 # art-type: grid
 # art-memory: 4096
 # art-include: master/Athena
@@ -7,7 +7,7 @@
 # art-output: ExampleMonitorOutput.root
 # art-output: log*
 
-Run3DQTestingDriver.py --inputFiles=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/AthenaMonitoring/q431/21.0/f946/myESD.pool.root DQ.Steering.doHLTMon=False IOVDb.GlobalTag=\"CONDBR2-BLKPA-RUN2-06\" > log.HIST_Creation 2>&1
+Run3DQTestingDriver.py --inputFiles=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/AthenaMonitoring/q431/21.0/f946/myESD.pool.root DQ.Steering.doHLTMon=False IOVDb.GlobalTag=\"CONDBR2-BLKPA-RUN2-06\" --threads=1  > log.HIST_Creation 2>&1
 
 echo "art-result: $? HIST_Creation"
 
@@ -17,3 +17,9 @@ art.py download ${ArtPackage} ${ArtJobName}
 REFFILE=(./ref-*/ExampleMonitorOutput.root)
 hist_diff.sh ExampleMonitorOutput.root $REFFILE -x TIME_execute -i > log.HIST_Diff 2>&1
 echo "art-result: $? HIST_Diff"
+rm -rf ref-*
+
+art.py download AthenaMonitoring test_run3dq_r21_esd.sh
+hist_diff.sh ExampleMonitorOutput.root $REFFILE -x TIME_execute -i > log.HIST_Diff_Serial 2>&1
+echo "art-result: $? HIST_Diff_Serial"
+
