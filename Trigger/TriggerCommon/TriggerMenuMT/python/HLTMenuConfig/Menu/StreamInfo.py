b208b730b2f3bcf7d4b30ffb8908170844e672c0
Rafal Bielski <rafal.bielski@cern.ch>
2021-08-25 20:08:27 +0200

    Ensure that stream tag type is one of the allowed values

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/StreamInfo.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLTMenuConfig/Menu/StreamInfo.py
@@ -1,13 +1,17 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
+import eformat
 from AthenaCommon.Logging import logging
 log = logging.getLogger( __name__ )
 
+_allowed_tag_types = [eformat.helper.tagtype_to_string(t) for t in eformat.helper.TagType.values.values()]
 
 class StreamInfo(object):
     def __init__(self, name, streamType, obeysLumiBlock, forceFullEventBuilding):
         assert type(name) == str, "name has to be str"
         assert type(streamType) == str, "streamType has to be str"
+        assert streamType in _allowed_tag_types, "streamType '"+streamType+"' is not one of "+\
+                                                 "the allowed types: "+str(_allowed_tag_types)
         assert type(obeysLumiBlock) == bool, "obeysLumiBlock has to be bool"
         assert type(forceFullEventBuilding) == bool, "forceFullEventBuilding has to be bool"
         self.__data = [name, streamType, obeysLumiBlock, forceFullEventBuilding]
