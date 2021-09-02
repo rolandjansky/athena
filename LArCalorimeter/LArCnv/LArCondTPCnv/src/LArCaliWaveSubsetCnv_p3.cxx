e0c9b036ffff15ede9e2a2d28fc1c1e0869ac5be
Walter Lampl <Walter.Lampl@cern.ch>
2021-09-02 14:10:08 +0200

    LArCondTPCnv: Allow for FEBs with nChannels not being a mutiple of 32 (like SuperCells "FEBs")

--- LArCalorimeter/LArCnv/LArCondTPCnv/src/LArCaliWaveSubsetCnv_p3.cxx
+++ LArCalorimeter/LArCnv/LArCondTPCnv/src/LArCaliWaveSubsetCnv_p3.cxx
@@ -259,7 +259,7 @@ LArCaliWaveSubsetCnv_p3::transToPers(const LArCWTransType* transObj,  LArCaliWav
         }
 				
         // Save chansSet
-        if  (j == (chansOffset + 31)) {
+        if  (j == (chansOffset + 31) || j == nfebChans-1 ) {
           persObj->m_subset.m_febsWithSparseData.push_back(chansSet);
           chansSet    =   0;
           chansOffset += 32;
