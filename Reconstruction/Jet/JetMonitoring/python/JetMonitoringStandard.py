15cc751782179a1b46361c9988eee62eff03acd2
Lee Sawyer <lee.sawyer@cern.ch>
2021-09-02 10:11:42 +0200

    Updates to JetMonitoring

--- Reconstruction/Jet/JetMonitoring/python/JetMonitoringStandard.py
+++ Reconstruction/Jet/JetMonitoring/python/JetMonitoringStandard.py
@@ -8,8 +8,7 @@
 @brief Main python configuration for the Run III Jet Monitoring
 '''
 
-#from JetMonitoring.JetMonitoringConfig import JetMonAlgSpec, HistoSpec,  EventHistoSpec, SelectSpec
-from JetMonitoring.JetMonitoringConfig import JetMonAlgSpec,  EventHistoSpec, SelectSpec
+from JetMonitoring.JetMonitoringConfig import JetMonAlgSpec, HistoSpec,  EventHistoSpec, SelectSpec, VarSpec
 
 # *********************************************
 # Define a list of histograms we want to draw for all jet containers.
@@ -30,14 +29,21 @@ commonHistoSpecs = [
     "HECFrac",
     "FracSamplingMax",
     "FracSamplingMaxIndex",
+    "EM3Frac",
+    "Tile0Frac",
     "CentroidR",
     "OotFracClusters5",
     "OotFracClusters10",
+    "JVF",
+    "JVF[0]",
+    "JVF[1]",
     "Jvt",
     "nconstit",
-    "N90Constituents",     
-    "JVF",
+    "N90Constituents",
     "leadingJetsRel",
+    "JetConstitScaleMomentum_pt","JetEMScaleMomentum_pt","JetPileupScaleMomentum_pt","JetEtaJESScaleMomentum_pt",
+    "JetConstitScaleMomentum_eta","JetEMScaleMomentum_eta","JetPileupScaleMomentum_eta","JetEtaJESScaleMomentum_eta",
+    "JetConstitScaleMomentum_phi","JetEMScaleMomentum_phi","JetPileupScaleMomentum_phi","JetEtaJESScaleMomentum_phi",
 
 
     # or we can directly add our custom histo specification in the form of a HistoSpec
@@ -63,45 +69,58 @@ commonHistoSpecs = [
     #  Use a SelectSpec dictionary to define the selection, and the histo to be drawn from it.
     SelectSpec( 'central',   # the name of the selection
                 '|eta|<0.5', # selection expression. The form 'min<var<max' is automatically interpreted.
-                FillerTools = ["pt","m","eta","phi","EMFrac","HECFrac","LArQuality","nconstit","N90Constituents",
+                path='CentralJets', # force the path where the histos are saved in the final ROOT file
+                FillerTools = ["pt","m","eta","phi","EMFrac","Tile0Frac","LArQuality","nconstit","N90Constituents",
+                "JetConstitScaleMomentum_pt","JetEMScaleMomentum_pt","JetPileupScaleMomentum_pt","JetEtaJESScaleMomentum_pt",
+                "JetConstitScaleMomentum_eta","JetEMScaleMomentum_eta","JetPileupScaleMomentum_eta","JetEtaJESScaleMomentum_eta",
+                "JetConstitScaleMomentum_phi","JetEMScaleMomentum_phi","JetPileupScaleMomentum_phi","JetEtaJESScaleMomentum_phi",
                 ] ),
 
      SelectSpec( 'tilegap3',   # the name of the selection
                 '1.0<|eta|<1.4', # selection expression. The form 'min<var<max' is automatically interpreted.
-                FillerTools = ["pt","m","eta","phi","EMFrac","HECFrac","LArQuality","nconstit","N90Constituents",
+                path='TileGap3', # force the path where the histos are saved in the final ROOT file
+                FillerTools = ["pt","m","eta","phi","EMFrac","Tile0Frac","HECFrac","LArQuality","nconstit","N90Constituents",
                 ] ),
 
     # another selection : only leading jets
     SelectSpec( 'leading',
                 '', # no selection on variables
                 SelectedIndex=0, # force 1st (leading) jet (we would set 1 for sub-leading jets)
-                path='standardHistos', # force the path where the histos are saved in the final ROOT file
-                FillerTools = ["pt","m","eta","phi",
+                path='LeadingJets', # force the path where the histos are saved in the final ROOT file
+                FillerTools = ["pt","m","eta","phi","EMFrac","Tile0Frac","HECFrac","LArQuality","nconstit","N90Constituents",
+                "JetConstitScaleMomentum_pt","JetEMScaleMomentum_pt","JetPileupScaleMomentum_pt","JetEtaJESScaleMomentum_pt",
+                "JetConstitScaleMomentum_eta","JetEMScaleMomentum_eta","JetPileupScaleMomentum_eta","JetEtaJESScaleMomentum_eta",
+                "JetConstitScaleMomentum_phi","JetEMScaleMomentum_phi","JetPileupScaleMomentum_phi","JetEtaJESScaleMomentum_phi",
                 ] ),
 
     # another selection : only subleading jets
     SelectSpec( 'subleading',
                 '', # no selection on variables
                 SelectedIndex=1, # force 2nd (sub-leading) jet
-                path='standardHistos', # force the path where the histos are saved in the final ROOT file
-                FillerTools = ["pt","m","eta","phi",
+                path='SubleadingJets', # force the path where the histos are saved in the final ROOT file
+                FillerTools = ["pt","m","eta","phi","EMFrac","Tile0Frac","HECFrac","LArQuality","nconstit","N90Constituents",
+                "JetConstitScaleMomentum_pt","JetEMScaleMomentum_pt","JetPileupScaleMomentum_pt","JetEtaJESScaleMomentum_pt",
+                "JetConstitScaleMomentum_eta","JetEMScaleMomentum_eta","JetPileupScaleMomentum_eta","JetEtaJESScaleMomentum_eta",
+                "JetConstitScaleMomentum_phi","JetEMScaleMomentum_phi","JetPileupScaleMomentum_phi","JetEtaJESScaleMomentum_phi",
                 ] ),
 
     # another selection : only very high pT jets
-    SelectSpec( 'highptrange2TeVto8TeV','2000<pt<8000',path='highptrange2TeVto8TeV',FillerTools = ["pt","m","eta","phi","eta;phi",] ),
-    SelectSpec( 'highptrange1TeVto2TeV','1000<pt<2000',path='highptrange1TeVto2TeV',FillerTools = ["pt","m","eta","phi","eta;phi",] ),
-    SelectSpec( 'highptrange500GeVto1TeV','500<pt<1000',path='highptrange500GeVto1TeV',FillerTools = ["pt","m","eta","phi","eta;phi",] ),
-    SelectSpec( 'highptrange200GeVto500GeV','200<pt<500',path='highptrange200GeVto500GeV',FillerTools = ["pt","m","eta","phi","eta;phi",] ),
+    SelectSpec( 'highptrange2TeVto8TeV','2000<pt<8000',path='highptrange2TeVto8TeV',FillerTools = ["highpt","m","eta","phi","eta;phi",] ),
+    SelectSpec( 'highptrange1TeVto2TeV','1000<pt<2000',path='highptrange1TeVto2TeV',FillerTools = ["highpt","m","eta","phi","eta;phi",] ),
+    SelectSpec( 'highptrange500GeVto1TeV','500<pt<1000',path='highptrange500GeVto1TeV',FillerTools = ["highpt","m","eta","phi","eta;phi",] ),
+    SelectSpec( 'highptrange200GeVto500GeV','200<pt<500',path='highptrange200GeVto500GeV',FillerTools = ["highpt","m","eta","phi","eta;phi",] ),
 
     # Selecting jets failing the LooseBad selection from the JetCleaningTool.
     SelectSpec( 'LooseBadFailedJets', 'LooseBad', InverseJetSel=True, 
-                FillerTools = ["pt","phi","eta","m","EMFrac","LArQuality","HECFrac","nconstit","N90Constituents","phi;eta","phi;eta;e","phi;eta;pt",
+                FillerTools = ["pt","phi","eta","m","EMFrac","LArQuality","Tile0Frac","HECFrac","nconstit","N90Constituents",
+                "phi;eta","phi;eta;e","phi;eta;pt",
                 ]),   
 
     # Selecting jets passing the LooseBad selection from the JetCleaningTool.
     SelectSpec( 'LooseBadJets',
                 'LooseBad', # this is not in the form 'min<x<max', so it will be assumed 'LooseBad' is an entry existing in JetStandardHistoSpecs.knownSelector
-                FillerTools = [ "pt","eta","phi","m","EMFrac","LArQuality","HECFrac","nconstit","N90Constituents","phi;eta","phi;eta;e","phi;eta;pt",
+                FillerTools = [ "pt","eta","phi","m","EMFrac","LArQuality","Tile0Frac","HECFrac","nconstit","N90Constituents",
+                "phi;eta","phi;eta;e","phi;eta;pt",
                 ] ),
     ] # end commonHistoSpecs
 
@@ -113,15 +132,41 @@ jvfHistosSpec = [
                 ] ),
 ]
 
-
 topoHistosSpec = [
-    # histos common to all topo jets
-    # to be filled ...
+    # histos common to all topo jets. Energy By Sampling layer plots.
+    HistoSpec("PreSamplerB",(100,0.,200.),title="Energy in PreSamplerB;E;Entries",xvar=VarSpec('EnergyPerSampling[0]')),
+    HistoSpec("EMB1",(100,0.,200.),title="Energy in EMB1;E;Entries",xvar=VarSpec('EnergyPerSampling[1]')),
+    HistoSpec("EMB2",(100,0.,200.),title="Energy in EMB2;E;Entries",xvar=VarSpec('EnergyPerSampling[2]')),
+    HistoSpec("EMB3",(100,0.,200.),title="Energy in EMB3;E;Entries",xvar=VarSpec('EnergyPerSampling[3]')),
+    HistoSpec("PreSamplerE",(100,0.,200.),title="Energy in PreSamplerE;E;Entries",xvar=VarSpec('EnergyPerSampling[4]')),
+    HistoSpec("EME1",(100,0.,200.),title="Energy in EME2;E;Entries",xvar=VarSpec('EnergyPerSampling[5]')),
+    HistoSpec("EME2",(100,0.,200.),title="Energy in EME2;E;Entries",xvar=VarSpec('EnergyPerSampling[6]')),
+    HistoSpec("EME3",(100,0.,200.),title="Energy in EME3;E;Entries",xvar=VarSpec('EnergyPerSampling[7]')),
+    HistoSpec("HEC0",(100,0.,200.),title="Energy in HEC0;E;Entries",xvar=VarSpec('EnergyPerSampling[8]')),
+    HistoSpec("HEC1",(100,0.,200.),title="Energy in HEC1;E;Entries",xvar=VarSpec('EnergyPerSampling[9]')),
+    HistoSpec("HEC2",(100,0.,200.),title="Energy in HEC2;E;Entries",xvar=VarSpec('EnergyPerSampling[10]')),
+    HistoSpec("HEC3",(100,0.,200.),title="Energy in HEC3;E;Entries",xvar=VarSpec('EnergyPerSampling[11]')),
+    HistoSpec("TileBar0",(100,0.,200.),title="Energy in TileBar0;E;Entries",xvar=VarSpec('EnergyPerSampling[12]')),
+    HistoSpec("TileBar1",(100,0.,200.),title="Energy in TileBar1;E;Entries",xvar=VarSpec('EnergyPerSampling[13]')),
+    HistoSpec("TileBar2",(100,0.,200.),title="Energy in TileBar1;E;Entries",xvar=VarSpec('EnergyPerSampling[14]')),
+    HistoSpec("TileGap1",(100,0.,200.),title="Energy in TileGap1;E;Entries",xvar=VarSpec('EnergyPerSampling[15]')),
+    HistoSpec("TileGap2",(100,0.,200.),title="Energy in TileGap2;E;Entries",xvar=VarSpec('EnergyPerSampling[16]')),
+    HistoSpec("TileGap3",(100,0.,200.),title="Energy in TileGap3;E;Entries",xvar=VarSpec('EnergyPerSampling[17]')),
+    HistoSpec("TileExt0",(100,0.,200.),title="Energy in TileExt0;E;Entries",xvar=VarSpec('EnergyPerSampling[18]')),
+    HistoSpec("TileExt1",(100,0.,200.),title="Energy in TileExt1;E;Entries",xvar=VarSpec('EnergyPerSampling[19]')),
+    HistoSpec("TileExt2",(100,0.,200.),title="Energy in TileExt2;E;Entries",xvar=VarSpec('EnergyPerSampling[20]')),
+    HistoSpec("FCAL0",(100,0.,200.),title="Energy in FCAL0;E;Entries",xvar=VarSpec('EnergyPerSampling[21]')),
+    HistoSpec("FCAL1",(100,0.,200.),title="Energy in FCAL1;E;Entries",xvar=VarSpec('EnergyPerSampling[22]')),
+    HistoSpec("FCAL2",(100,0.,200.),title="Energy in FCAL2;E;Entries",xvar=VarSpec('EnergyPerSampling[23]')),
 ]
 
 pflowHistosSpec = [
-    # histos common to all PFlow jets
-    # to be filled ...
+    # histos common to all PFlow jets. These are defined in JetStandardHistoSpecs.py
+    "SumPtChargedPFOPt500[0]",
+    "SumPtTrkPt500[0]",
+    "NumTrkPt500[0]",
+    "NumTrkPt1000[0]",
+    "fCharged",
 ]
 
 
