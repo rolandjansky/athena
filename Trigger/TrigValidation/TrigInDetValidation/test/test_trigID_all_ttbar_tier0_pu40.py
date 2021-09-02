f747cbfd7722a069d787a8cd789ec9b1cf44a642
Mark Sutton <mark.sutton@cern.ch>
2021-09-02 10:04:13 +0200

    Add a TrigInDetAnalysis nightly test which runs the tier 0 monitoring

---
+++ Trigger/TrigValidation/TrigInDetValidation/test/test_trigID_all_ttbar_tier0_pu40.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# art-description: art job for all_ttbar_tier0_pu40
+# art-type: grid
+# art-include: master/Athena
+# art-athena-mt: 8
+# art-memory: 4096
+# art-html: https://idtrigger-val.web.cern.ch/idtrigger-val/TIDAWeb/TIDAart/?jobdir=
+# art-output: *.txt
+# art-output: *.log
+# art-output: log.*
+# art-output: *.out
+# art-output: *.err
+# art-output: *.log.tar.gz
+# art-output: *.new
+# art-output: *.json
+# art-output: *.root
+# art-output: *.check*
+# art-output: HLT*
+# art-output: times*
+# art-output: cost-perCall
+# art-output: cost-perEvent
+# art-output: cost-perCall-chain
+# art-output: cost-perEvent-chain
+# art-output: *.dat 
+
+import os
+os.system("echo 'doTIDATier0=True ' > tier0.py")
+
+preinclude_file = 'tier0.py'
+
+
+Slices  = ['muon','electron','tau','bjet','fsjet']
+Events  = 4000
+Threads = 8 
+Slots   = 8
+Release = "current"
+
+Input   = 'ttbar'    # defined in TrigValTools/share/TrigValInputs.json  
+
+Jobs = [] 
+
+Comp = [ ( "L2muon",       "L2muon",      "TIDA_T0.root",   " -b HLT/TRIDT/Muon/Expert     -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTL2-plots-muon " ),
+         ( "L2electron",   "L2electron",  "TIDA_T0.root",   " -b HLT/TRIDT/Electron/Expert -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTL2-plots-electron " ),
+         ( "L2tau",        "L2tau",       "TIDA_T0.root",   " -b HLT/TRIDT/Tau/Expert      -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTL2-plots-tau " ),
+         ( "L2bjet",       "L2bjet",      "TIDA_T0.root",   " -b HLT/TRIDT/Bjet/Expert     -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTL2-plots-bjet " ),   
+         ( "FSjetoffline", "L2fsjet",     "TIDA_T0.root",   " -b HLT/TRIDT/Bjet/Expert     -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTL2-plots-FS " ),
+
+         ( "EFmuon",       "EFmuon",      "TIDA_T0.root",   " -b HLT/TRIDT/Muon/Expert     -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTEF-plots-muon " ),
+         ( "EFelectron",   "EFelectron",  "TIDA_T0.root",   " -b HLT/TRIDT/Electron/Expert -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTEF-plots-electron " ),
+         ( "EFtau",        "EFtau",       "TIDA_T0.root",   " -b HLT/TRIDT/Tau/Expert      -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTEF-plots-tau " ),
+         ( "EFbjet",       "EFbjet",      "TIDA_T0.root",   " -b HLT/TRIDT/Bjet/Expert     -s '_HLT_IDTrack' '/HLT_IDTrack' -c TIDAhisto-tier0.dat --ncols 3 -d HLTEF-plots-bjet " ) ]
+   
+from AthenaCommon.Include import include 
+include("TrigInDetValidation/TrigInDetValidation_Base.py")
+
