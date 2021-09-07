bb048017bb005054e85cdbfeb9e28986bf7b877b
John Chapman <jchapman@cern.ch>
2021-09-07 14:58:17 +0200

    Add RenameHitCollectionAlg
    This Algorithm is used to rename Hit collections back to their original names in ReSimulation jobs.
    It is used in the case that the current event does not need to be resimulated and so the original
    HitCollections are still valid.

--- Simulation/ISF/ISF_Core/ISF_Algorithms/src/components/ISF_Algorithms_entries.cxx
+++ Simulation/ISF/ISF_Core/ISF_Algorithms/src/components/ISF_Algorithms_entries.cxx
@@ -3,9 +3,11 @@
 #include "../CollectionMerger.h"
 #include "../SimHitTreeCreator.h"
 #include "../SimEventFilter.h"
+#include "../RenameHitCollectionsAlg.h"
 
 DECLARE_COMPONENT( ISF::SimKernel )
 DECLARE_COMPONENT( ISF::SimKernelMT )
 DECLARE_COMPONENT( ISF::CollectionMerger )
 DECLARE_COMPONENT( ISF::SimHitTreeCreator )
 DECLARE_COMPONENT( ISF::SimEventFilter )
+DECLARE_COMPONENT( ISF::RenameHitCollectionsAlg )
