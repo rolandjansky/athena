7100e2fd97d9cfd84bea876b950e231c382a9f06
scott snyder <sss@karma>
2021-09-08 22:30:53 -0400

    AthAllocators: const fix
    Fix for instantiating STL allocators with a const type.
    Otherwise, we end up with a duplicate member because reference
    and const_reference are the same.

--- Control/AthAllocators/test/ArenaPoolSTLAllocator_test.cxx
+++ Control/AthAllocators/test/ArenaPoolSTLAllocator_test.cxx
@@ -156,6 +156,12 @@ void test1()
 
   assert (Payload::n == 2);
   assert (Payload::v.size() == 0);
+
+  {
+    SG::ArenaPoolSTLAllocator<const Payload, int> a5;
+    const Payload* p = a5.allocate (1);
+    a5.deallocate (p, 1);
+  }
 }
 
 
