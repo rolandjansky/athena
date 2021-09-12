b2f725a7434d60cc6b8006be9af75122f5536ccf
Moe Wakida <wakida@hepl.phys.nagoya-u.ac.jp>
2021-09-12 19:36:07 +0200

    Support L1Muon Rename

--- Trigger/TrigMonitoring/TrigMuonMonitoringMT/python/MuonTriggerCountMTConfig.py
+++ Trigger/TrigMonitoring/TrigMuonMonitoringMT/python/MuonTriggerCountMTConfig.py
@@ -7,7 +7,7 @@ def MuonTriggerCountMTConfig(helper):
     from AthenaConfiguration.ComponentFactory import CompFactory
 
     GroupName = 'TriggerCount'
-    Chains =  ['HLT_mu6_L1MU6', 'HLT_mu26_ivarmedium_L1MU20', 'HLT_mu50_L1MU20', 'HLT_mu60_0eta105_msonly_L1MU20', 'HLT_2mu14_L12MU10', 'HLT_mu22_mu8noL1_L1MU20']
+    Chains =  ['HLT_mu6_L1MU5VF', 'HLT_mu26_ivarmedium_L1MU14FCH', 'HLT_mu50_L1MU14FCH', 'HLT_mu60_0eta105_msonly_L1MU14FCH', 'HLT_2mu14_L12MU8F', 'HLT_mu22_mu8noL1_L1MU14FCH']
 
     monAlg = helper.addAlgorithm(CompFactory.MuonTriggerCountMT,'MuonTriggerCountMT')
     # HLT_mu6_L1MU6 is test chain for small statistics, so it will be removed.
