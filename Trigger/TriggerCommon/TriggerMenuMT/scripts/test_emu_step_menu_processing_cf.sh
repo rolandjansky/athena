ac778c3dfc59bed8c9490d9356fbb359ea1c3fc4
Francesca Pastore <francesca.pastore@cern.ch>
2021-09-13 20:31:12 +0200

    Cleanup of HLT-CF emulation tests (ATR-24087)

---
+++ Trigger/TriggerCommon/TriggerMenuMT/scripts/test_emu_step_menu_processing_cf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is a unit test of HLT Control Flow 
+athena.py --imf --threads=1  --evtMax 4 --filesInput /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1 -c "menuType='emuMenuTest';" TriggerMenuMT/test_menu_cf.py
+
+
---
+++ Trigger/TriggerCommon/TriggerMenuMT/scripts/test_manual_menu_cf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This is not an ART test, but a unit test
+
+# This is a unit test of HLT Control Flow 
+
+athena.py --imf --threads=1 --evtMax 5  --filesInput /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrigP1Test/data17_13TeV.00327265.physics_EnhancedBias.merge.RAW._lb0100._SFO-1._0001.1 --config-only=config.pkl -c "menuType='menuManual';" TriggerMenuMT/test_menu_cf.py
