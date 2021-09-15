662bc4ae5815cbfa666a4cbb578ec719811304f9
David Richard Shope <david.richard.shope@cern.ch>
2021-09-15 20:23:18 +0200

    Port HGTD fast digi from 21.9 to master

---
+++ HighGranularityTimingDetector/HGTD_Digitization/HGTD_FastDigitization/src/components/HGTD_FastDigitization_entries.cxx
@@ -0,0 +1,7 @@
+/**
+ *  * Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration.
+ *   */
+
+#include "HGTD_FastDigitization/HGTD_SmearedDigitizationTool.h"
+
+DECLARE_COMPONENT( HGTD_SmearedDigitizationTool )
