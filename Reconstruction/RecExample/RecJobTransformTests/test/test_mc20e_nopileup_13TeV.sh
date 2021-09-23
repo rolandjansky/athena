c837254434e603f802f55dd0d4451e96e4b70891
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-09-23 12:38:43 +0200

    Update conditions tag to fix ATLASSIM-5413.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_nopileup_13TeV.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_mc20e_nopileup_13TeV.sh
@@ -10,6 +10,6 @@
 unset ATHENA_CORE_NUMBER
 unset ATHENA_PROC_NUMBER
 
-Reco_tf.py --AMI=r12898 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1  --maxEvents=500 --imf False  --athenaopts "RAWtoESD:--threads=1" "ESDtoAOD:--threads=1" --conditionsTag="OFLCOND-MC16-SDR-RUN2-08"
+Reco_tf.py --AMI=r12898 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1  --maxEvents=500 --imf False  --athenaopts "RAWtoESD:--threads=1" "ESDtoAOD:--threads=1" --conditionsTag="OFLCOND-MC16-SDR-RUN2-09"
 RES=$?
 echo "art-result: $RES Reco"
