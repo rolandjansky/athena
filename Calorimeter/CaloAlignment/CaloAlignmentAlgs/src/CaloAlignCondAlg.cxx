1f837c4be5fa62c1cf7fe985b371e123e7408c3e
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2021-10-08 00:01:10 +0200

    Allow empty keys for read condition handles in CaloAlignCondAlg
    When both of keys are empty, then the algorithm will build an unaligned
    Calo DD Manager and write it into Condition Store with infinite validity.
    This is needed for some unit test

--- Calorimeter/CaloAlignment/CaloAlignmentAlgs/src/CaloAlignCondAlg.cxx
+++ Calorimeter/CaloAlignment/CaloAlignmentAlgs/src/CaloAlignCondAlg.cxx
@@ -6,6 +6,7 @@
 
 #include "CaloDetDescrUtils/CaloDetDescrBuilder.h"
 #include "AthenaKernel/getMessageSvc.h"
+#include "AthenaKernel/IOVInfiniteRange.h"
 
 #include <memory>
 
@@ -14,8 +15,8 @@ StatusCode CaloAlignCondAlg::initialize()
   ATH_MSG_DEBUG("initialize " << name());
 
   ATH_CHECK(m_condSvc.retrieve());
-  ATH_CHECK(m_readKeyGeoAlign.initialize());
-  ATH_CHECK(m_readKeyCellPosShift.initialize());
+  ATH_CHECK(m_readKeyGeoAlign.initialize(SG::AllowEmpty));
+  ATH_CHECK(m_readKeyCellPosShift.initialize(SG::AllowEmpty));
   ATH_CHECK(m_writeCaloMgrKey.initialize());
 
   // Register Write Cond Handle
@@ -38,22 +39,34 @@ StatusCode CaloAlignCondAlg::execute(const EventContext& ctx) const
   
   // ____________ Get Read Cond Objects ____________
   // 1. GeoAlignmentStore
-  SG::ReadCondHandle<GeoAlignmentStore> readHandleGeoAlign{m_readKeyGeoAlign,ctx};
-  ATH_CHECK(readHandleGeoAlign.isValid());
-  ATH_MSG_DEBUG("Retrieved GeoAlignmentStore object form the Condition Store");
-  writeCaloMgrHandle.addDependency(readHandleGeoAlign);
+  const GeoAlignmentStore* geoAlign{nullptr};
+  if(!m_readKeyGeoAlign.empty()) {
+    SG::ReadCondHandle<GeoAlignmentStore> readHandleGeoAlign{m_readKeyGeoAlign,ctx};
+    ATH_CHECK(readHandleGeoAlign.isValid());
+    ATH_MSG_DEBUG("Retrieved GeoAlignmentStore object form the Condition Store");
+    writeCaloMgrHandle.addDependency(readHandleGeoAlign);
+    geoAlign = *readHandleGeoAlign;
+  }
 
   // 2. CaloCellPositionShift
-  SG::ReadCondHandle<CaloRec::CaloCellPositionShift> readHandleCellPosShift{m_readKeyCellPosShift,ctx};
-  ATH_CHECK(readHandleCellPosShift.isValid());
-  ATH_MSG_DEBUG("Retrieved CaloRec::CaloCellPositionShift object form the Condition Store");
-  writeCaloMgrHandle.addDependency(readHandleCellPosShift);
+  const CaloRec::CaloCellPositionShift* cellPosShift{nullptr};
+  if(!m_readKeyCellPosShift.empty()) {
+    SG::ReadCondHandle<CaloRec::CaloCellPositionShift> readHandleCellPosShift{m_readKeyCellPosShift,ctx};
+    ATH_CHECK(readHandleCellPosShift.isValid());
+    ATH_MSG_DEBUG("Retrieved CaloRec::CaloCellPositionShift object form the Condition Store");
+    writeCaloMgrHandle.addDependency(readHandleCellPosShift);
+    cellPosShift = *readHandleCellPosShift;
+  }
+
+  if(m_readKeyGeoAlign.empty() && m_readKeyCellPosShift.empty()) {
+    writeCaloMgrHandle.addDependency(EventIDRange(IOVInfiniteRange::infiniteRunLB()));
+  }
 
   // ____________ Build new CaloDetDescrManager _________________
   std::unique_ptr<CaloDetDescrManager> caloMgr = buildCaloDetDescr(serviceLocator()
 								   , Athena::getMessageSvc()
-								   , *readHandleGeoAlign
-								   , *readHandleCellPosShift);
+								   , geoAlign
+								   , cellPosShift);
 
   ATH_CHECK(writeCaloMgrHandle.record(std::move(caloMgr)));
   ATH_MSG_INFO("recorded new CaloDetDescr Manager condition object with key " << writeCaloMgrHandle.key() 
