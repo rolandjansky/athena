b754777242e49c49576d305ec8b00b2d92671268
Frank Winklmeier <frank.winklmeier@cern.ch>
2021-10-07 16:54:53 +0200

    JetSimTools: delete obsolete job options
    Delete all job options in `share/` as none of them are used, have not
    been updated in many years and are referencing code (e.g. CBNT) that
    doesn't even exist anymore.

--- Reconstruction/Jet/JetSimTools/share/ConeAtlfastJets_CBNT_jobOptions.py
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------------------
-#      jobOptions for CBNT for JetRec package
-#
-
-CBNT_Athena.Members += [ "CBNT_Jet/CBNT_ConeAtlJets" ]
-CBNT_ConeAtlJets = Algorithm( "CBNT_ConeAtlJets" )
-CBNT_ConeAtlJets.InputJetContainer = "ConeAtlfastJets"
-CBNT_ConeAtlJets.BlockTag = "CAtlJets"
-CBNT_ConeAtlJets.VarTag = "CAtlJets"
