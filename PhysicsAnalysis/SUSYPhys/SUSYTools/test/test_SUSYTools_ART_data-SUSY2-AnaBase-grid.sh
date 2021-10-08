34971c63f4f525609bd2de44d65d8a1762b4da68
Margherita Spalla <margherita.spalla@cern.ch>
2021-10-08 18:14:48 +0200

    put back susy files

---
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/test/test_SUSYTools_ART_data-SUSY2-AnaBase-grid.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# art-description: SUSYTools ART test - TestSUSYToolsAlg.py
+# art-type: grid
+# art-include: 21.2/AnalysisBase
+# art-output: hist-DATA_DAOD_SUSY2.root
+# art-output: dcube
+
+# Create empty pool file
+art.py createpoolfile
+
+echo "Running SUSYTools test: \'TestSUSYToolsAlg.py -t data18 -m 5000 -d 2\'"
+TestSUSYToolsAlg.py -t data18 -m 5000 -d 2
+echo  "art-result: $? TEST"
+
+mv submitDir/hist-ARTInput.root ./hist-DATA_DAOD_SUSY2.root
+
+echo "Running DCube post-processing"
+
+tName="DATA-SUSY2"
+dcubeRef=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/References/hist-DATA_DAOD_SUSY2.root
+dcubeXml=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/xml/dcube_config_master.xml
+
+/cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube ${tName} hist-DATA_DAOD_SUSY2.root ${dcubeXml} ${dcubeRef}
+
+echo "art-result: $? DCUBE"
+
+echo "Done."
