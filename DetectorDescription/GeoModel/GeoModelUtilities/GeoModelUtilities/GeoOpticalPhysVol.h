df5c880165db50ba7d396a88af7b5ad2de596f77
Johannes Josef Junggeburth <johannes.josef.junggeburth@CERN.CH>
2021-10-12 12:36:46 +0200

    Declare GeoModelPrimitives dependency to ensure that Atlas eigen extensions are loaded first

--- DetectorDescription/GeoModel/GeoModelUtilities/GeoModelUtilities/GeoOpticalPhysVol.h
+++ DetectorDescription/GeoModel/GeoModelUtilities/GeoModelUtilities/GeoOpticalPhysVol.h
@@ -1,9 +1,12 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef GeoOpticalPhysVol_h
 #define GeoOpticalPhysVol_h 1
+
+/// Ensure that the extensions for the Vector3D are properly loaded
+#include "GeoPrimitives/GeoPrimitives.h"
 
 #include "GeoModelKernel/GeoPhysVol.h"
 
