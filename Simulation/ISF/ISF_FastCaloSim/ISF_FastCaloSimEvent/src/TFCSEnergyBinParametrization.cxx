5813332a3679e36a2b90eadba8a4e280b8002c8e
Jonathan Bossio <jonathan.bossio@cern.ch>
2021-10-13 15:41:46 +0200

    Provide more information for debugging

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSEnergyBinParametrization.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSEnergyBinParametrization.cxx
@@ -168,9 +168,15 @@ FCSReturnCode TFCSEnergyBinParametrization::simulate(TFCSSimulationState& simuls
   int chosenBin=TMath::BinarySearch(n_bins()+1, Ebin_probability.data(), searchRand)+1;
   if(chosenBin<0){
     ATH_MSG_WARNING("TFCSEnergyBinParametrization::simulate(): chosenBin<0 (will use chosenBin=0)");
+    std::string array = "";
+    for(const auto& prob : Ebin_probability){array+=prob;array+=" ";}
+    ATH_MSG_WARNING(" E="<<simulstate.E()<<" Ebin="<<chosenBin<<" rnd="<<searchRand<<" array="<<array);
     chosenBin = 0;
   } else if (chosenBin>n_bins()){
     ATH_MSG_WARNING("TFCSEnergyBinParametrization::simulate(): chosenBin>n_bins() (will use chosenBin=n_bins())");
+    std::string array = "";
+    for(const auto& prob : Ebin_probability){array+=prob;array+=" ";}
+    ATH_MSG_WARNING(" E="<<simulstate.E()<<" Ebin="<<chosenBin<<" rnd="<<searchRand<<" array="<<array);
     chosenBin = n_bins();
   }
   simulstate.set_Ebin(chosenBin);
