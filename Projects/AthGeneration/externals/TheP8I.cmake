288017ad01c364bf6033c6a55ffe9ae77584b395
Yoran Yeh <yoran.yeh@cern.ch>
2021-10-18 16:29:57 +0200

    Update TheP8I package name in Externals CMakefile

--- Projects/AthGeneration/externals/TheP8I.cmake
+++ Projects/AthGeneration/externals/TheP8I.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of TheP8I to use.
 #
 
-set( THEP8I_LCGVERSION "2.0.0" )
+set( THEP8I_LCGVERSION "2.0.0.thepeg220" )
 set( THEP8I_LCGROOT
   "${LCG_RELEASE_DIR}/MCGenerators/thep8i/${THEP8I_LCGVERSION}/${LCG_PLATFORM}" )
