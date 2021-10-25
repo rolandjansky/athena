80dfd69875949c423f69da309870f7431f112ca0
Yoran Yeh <yoran.yeh@cern.ch>
2021-10-25 15:33:51 +0200

    set TheP8I paths and set PYTHIA8PATH during Pythia8_i compilation

--- Generators/Herwig7_i/share/common/Herwig7_TheP8I.py
+++ Generators/Herwig7_i/share/common/Herwig7_TheP8I.py
@@ -1,21 +1,15 @@
 
 assert hasattr(genSeq, "Herwig7")
 
-#The path is hardcoded, exactly in a way that is done for OpenLoops
-try:
-# This should be a prefix      
-      TheP8I_path=os.environ['THEP8I_PATH']
-except:
-      TheP8I_path='/cvmfs/sft.cern.ch/lcg/releases/LCG_88/MCGenerators/thep8i/2.0.0/x86_64-slc6-gcc62-opt'
-os.environ["LD_LIBRARY_PATH"]=TheP8I_path+"/lib64/ThePEG"+":"+TheP8I_path+"/lib/ThePEG"+":"+os.environ["LD_LIBRARY_PATH"]
+# THEP8I_PATH is set during compilation, now add to LD_LIBRARY_PATH when running event generation with the TheP8I plugin
+TheP8Ipath=os.environ['THEP8IPATH']
+os.environ["LD_LIBRARY_PATH"]=TheP8Ipath+"/lib64/ThePEG"+":"+TheP8Ipath+"/lib/ThePEG"+":"+os.environ["LD_LIBRARY_PATH"]
 
 try:
       TheP8I_PYTHIA8DATA=os.environ['PYTHIA8DATA']
 except:
-      TheP8I_PYTHIA8DATA='/cvmfs/sft.cern.ch/lcg/releases/LCG_88/MCGenerators/pythia8/244/x86_64-slc6-gcc62-opt/share/Pythia8/xmldoc/'
+      TheP8I_PYTHIA8DATA=os.environ['PY8PATH'] + '/share/Pythia8/xmldoc/'
 os.environ['PYTHIA8DATA']=TheP8I_PYTHIA8DATA
-
-
 
 Herwig7Config.add_commands("""
 mkdir /TheP8I
