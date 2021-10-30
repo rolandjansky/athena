756dbcb631bc86ce46dc5b17ec31861e4ab8b5b5
Peter Berta <peter.berta@cern.ch>
2021-10-30 18:14:55 +0200

    updates based on MR 47694

--- Tools/Tier0ChainTests/test/test_mc20e.sh
+++ Tools/Tier0ChainTests/test/test_mc20e.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 #
-# art-description: Run mc20e reco with pileup and MT = 8
+# art-description: Run reco using mc20e (overlay+trigger+rco)
+# art-output: log.*
+# art-athena-mt: 8
 # art-type: grid
 # art-include: master/Athena
 # art-include: 22.0-mc20/Athena
-# art-athena-mt: 8
 
-#Unset these to get around ATLASJT-420
-unset ATHENA_CORE_NUMBER
-unset ATHENA_PROC_NUMBER
-
-Reco_tf.py --AMI=r12806 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/mc16_13TeV/valid1/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.simul.HITS.e4993_s3227/HITS.12560240._000287.pool.root.1 --inputRDO_BKGFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayTests/PresampledPileUp/22.0/Run2/large/mc20e_presampling.2k.latest.RDO.pool.root  --maxEvents=500 --imf False --conditionsTag="OFLCOND-MC16-SDR-RUN2-09"
+Reco_tf.py --AMI=r13145 --outputRDOFile=myRDO.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --inputHITSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.simul.HITS.e6337_s3681/HITS.25836812._004813.pool.root.1 --inputRDO_BKGFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/OverlayTests/PresampledPileUp/22.0/Run2/large/mc20_13TeV.900149.PG_single_nu_Pt50.digit.RDO.e8307_s3482_s3136_d1715/RDO.26811908._031801.pool.root.1 --maxEvents=500 --imf False
 
 
 rc1=$?
