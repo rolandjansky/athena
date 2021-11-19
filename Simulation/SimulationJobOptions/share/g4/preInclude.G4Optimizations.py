5108eee34533140234528a22f8d5a8776bbac318
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2021-11-19 14:18:19 +0100

    Add the G4GammaGeneralProcess to the G4Optimizations job option for Run3

--- Simulation/SimulationJobOptions/share/g4/preInclude.G4Optimizations.py
+++ Simulation/SimulationJobOptions/share/g4/preInclude.G4Optimizations.py
@@ -84,3 +84,16 @@ else:
 from G4AtlasApps import callbacks
 callbacks.turn_on_gamma_range_cuts()
 
+
+##########################################################
+#
+# Activate the G4GammaGeneralProcess and the corresponding
+# job options
+#  
+##########################################################
+
+from G4AtlasApps.SimFlags import simFlags
+simFlags.G4Commands+=["/process/em/UseGeneralProcess true"];
+simFlags.OptionalUserActionList.addAction("G4UA::FixG4CreatorProcessTool")
+
+
