b50ebc90cf482142dc041c3a5ff7d1bf6726e6b0
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2021-11-23 11:56:00 +0100

    Add PFlowTests package to 21.9. Include one test to run the e/p ITK style workflow in 21.9 from ESD.

---
+++ Reconstruction/PFlow/PFlowTests/test/test_pflow_EOverP_fromESD_noLinking.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# art-description: Athena runs EOverP ESD pflow reconstruction, PFO linking off
+# art-type: grid
+# art-include: 21.9/Athena
+# art-output: AOD.pool.root
+
+athena.py --threads=8  eflowRec/run_ESDPFlowRecoR21_9_EOverP.py
+rc1=$?
+
+echo "art-result: ${rc1}"
+
