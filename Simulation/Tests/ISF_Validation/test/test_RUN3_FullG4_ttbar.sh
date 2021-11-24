899999a068eedfbe7d374226ad4f7e9b0e03af8a
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2021-11-24 11:14:09 +0100

    Update Run3 Simulation ART tests to match initial production configuration

--- Simulation/Tests/ISF_Validation/test/test_RUN3_FullG4_ttbar.sh
+++ Simulation/Tests/ISF_Validation/test/test_RUN3_FullG4_ttbar.sh
@@ -9,13 +9,13 @@
 # art-output: test.HITS.pool.root
 # art-output: truth.root
 
-# RUN3 setup
-# ATLAS-R3S-2021-02-00-00 and OFLCOND-MC16-SDR-RUN3-02
+# RUN3 setup - Frozen Showers currently off by default
+# ATLAS-R3S-2021-02-00-00 and OFLCOND-MC21-SDR-RUN3-03
 Sim_tf.py \
---conditionsTag 'default:OFLCOND-MC16-SDR-RUN3-02' \
+--conditionsTag 'default:OFLCOND-MC21-SDR-RUN3-03' \
 --simulator 'FullG4' \
 --postInclude 'default:PyJobTransforms/UseFrontier.py' \
---preInclude 'EVNTtoHITS:Campaigns/MC21Simulation.py' \
+--preInclude 'EVNTtoHITS:Campaigns/MC21Simulation.py,SimulationJobOptions/preInclude.FrozenShowersFCalOnly.py' \
 --geometryVersion 'default:ATLAS-R3S-2021-02-00-00_VALIDATION' \
 --inputEVNTFile "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/SimCoreTests/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.evgen.EVNT.e4993.EVNT.08166201._000012.pool.root.1" \
 --outputHITSFile "test.HITS.pool.root" \
