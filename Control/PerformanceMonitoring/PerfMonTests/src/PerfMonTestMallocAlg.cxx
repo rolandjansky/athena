5302d3e63cf9a06106fc6e58662e3c400e406445
scott snyder <sss@karma>
2021-11-24 11:06:13 -0500

    PerfMonTests: Remove references to IAtRndmGenSvc.
    Remove (unused) references to deprecated IAtRndmGenSvc.

--- Control/PerformanceMonitoring/PerfMonTests/src/PerfMonTestMallocAlg.cxx
+++ Control/PerformanceMonitoring/PerfMonTests/src/PerfMonTestMallocAlg.cxx
@@ -1,7 +1,7 @@
 ///////////////////////// -*- C++ -*- /////////////////////////////
 
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 // PerfMonTestMallocAlg.cxx 
@@ -15,7 +15,6 @@
 
 // FrameWork includes
 #include "Gaudi/Property.h"
-#include "AthenaKernel/IAtRndmGenSvc.h"
 
 // CLHEP includes
 #include "CLHEP/Units/SystemOfUnits.h"
