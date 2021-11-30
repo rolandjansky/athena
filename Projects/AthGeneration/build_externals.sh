533ba3be1550243d0520111dda89db57db2ed699
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2021-11-30 02:43:18 +0100

    Updated GeoModel version hash
    In the new GeoModel version several thread-safety patches have been implemented
    to protect against memory corruption when legacy alignment handling mechanism is
    used by AthenaMT

--- Projects/AthGeneration/build_externals.sh
+++ Projects/AthGeneration/build_externals.sh
@@ -13,7 +13,7 @@ ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=101
                         -DLCG_VERSION_POSTFIX="_ATLAS_6"
                         -DATLAS_GAUDI_TAG="v36r2.000"
                         -DATLAS_COOL_TAG="COOL_3_3_9"
-                        -DATLAS_GEOMODEL_TAG="c04bd9d5")
+                        -DATLAS_GEOMODEL_TAG="3ce7f324")
 ATLAS_EXTRA_MAKE_ARGS=()
 
 # Let "the common script" do all the heavy lifting.
