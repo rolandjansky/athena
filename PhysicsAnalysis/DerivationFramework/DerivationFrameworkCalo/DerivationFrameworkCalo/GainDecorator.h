b26fa88aaddd2e8711f18ff16b97cafc6d838da3
christos <christos@cern.ch>
2021-12-01 18:12:57 +0100

    DerivationFrameworkCalo format code, after migration to MT done

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkCalo/DerivationFrameworkCalo/GainDecorator.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCalo/DerivationFrameworkCalo/GainDecorator.h
@@ -5,85 +5,82 @@
 ///////////////////////////////////////////////////////////////////
 // GainDecorator.h, (c) ATLAS Detector software
 ///////////////////////////////////////////////////////////////////
-// Decorate egamma objects with the energy and number of cells per layer per gain 
+// Decorate egamma objects with the energy and number of cells per layer per
+// gain
 
 #ifndef DERIVATIONFRAMEWORK_GainDecorator_H
 #define DERIVATIONFRAMEWORK_GainDecorator_H
 
-#include <string>
 #include <map>
+#include <string>
 
 #include "AthenaBaseComps/AthAlgTool.h"
 #include "DerivationFrameworkInterfaces/IAugmentationTool.h"
 
+#include "CaloEvent/CaloCell.h"
 #include "StoreGate/ReadHandleKey.h"
 #include "StoreGate/WriteDecorHandleKeyArray.h"
-#include "CaloEvent/CaloCell.h"
 #include "xAODEgamma/EgammaContainer.h"
 
 namespace DerivationFramework {
 
-  class GainDecorator
+class GainDecorator
   : public AthAlgTool
   , public IAugmentationTool
+{
+public:
+  GainDecorator(const std::string& t,
+                const std::string& n,
+                const IInterface* p);
+  ~GainDecorator();
+  StatusCode initialize();
+  StatusCode finalize();
+  virtual StatusCode addBranches() const;
+  static int getLayer(const CaloCell* cell);
+
+  struct calculation
   {
-    public:
-      GainDecorator(const std::string& t,
-                    const std::string& n,
-                    const IInterface* p);
-      ~GainDecorator();
-      StatusCode initialize();
-      StatusCode finalize();
-      virtual StatusCode addBranches() const;      
-      static int getLayer(const CaloCell *cell);
-
-      struct calculation
-      {
-        std::map< std::pair<int, int>, float > E;
-        std::map< std::pair<int, int>, uint8_t > nCells;
-      };
-      
-    private:
-      SG::ReadHandleKey<xAOD::EgammaContainer> m_SGKey_photons{
-        this,
-        "SGKey_photons",
-        "",
-        "SG key of photon container"
-      };
-
-      SG::ReadHandleKey<xAOD::EgammaContainer> m_SGKey_electrons{
-        this,
-        "SGKey_electrons",
-        "",
-        "SG key of electron container"
-      };
-
-      std::string m_decorationPattern;
-      std::map<int, std::string> m_gainNames;
-      std::vector<unsigned int> m_layers;
-      
-      // Name of the decorations
-      std::map< std::pair<int,int>, std::string> m_names_E;
-      std::map< std::pair<int,int>, std::string> m_names_nCells;
-
-      SG::WriteDecorHandleKeyArray<xAOD::EgammaContainer>
-        m_SGKey_photons_decorations{
-          this,
-          "SGKey_photons_decorations_noConf",
-          {},
-          "SG keys for photon decorations not really configurable"
-        };
-
-      SG::WriteDecorHandleKeyArray<xAOD::EgammaContainer>
-        m_SGKey_electrons_decorations{
-          this,
-          "SGKey_electrons_decorations_noConf",
-          {},
-          "SG keys for electrons decorations not really configurable"
-        };
-
-      calculation decorateObject(const xAOD::Egamma*& egamma) const;
+    std::map<std::pair<int, int>, float> E;
+    std::map<std::pair<int, int>, uint8_t> nCells;
   };
+
+private:
+  SG::ReadHandleKey<xAOD::EgammaContainer>
+    m_SGKey_photons{ this, "SGKey_photons", "", "SG key of photon container" };
+
+  SG::ReadHandleKey<xAOD::EgammaContainer> m_SGKey_electrons{
+    this,
+    "SGKey_electrons",
+    "",
+    "SG key of electron container"
+  };
+
+  std::string m_decorationPattern;
+  std::map<int, std::string> m_gainNames;
+  std::vector<unsigned int> m_layers;
+
+  // Name of the decorations
+  std::map<std::pair<int, int>, std::string> m_names_E;
+  std::map<std::pair<int, int>, std::string> m_names_nCells;
+
+  SG::WriteDecorHandleKeyArray<xAOD::EgammaContainer>
+    m_SGKey_photons_decorations{
+      this,
+      "SGKey_photons_decorations_noConf",
+      {},
+      "SG keys for photon decorations not really configurable"
+    };
+
+  SG::WriteDecorHandleKeyArray<xAOD::EgammaContainer>
+    m_SGKey_electrons_decorations{
+      this,
+      "SGKey_electrons_decorations_noConf",
+      {},
+      "SG keys for electrons decorations not really configurable"
+    };
+
+  calculation decorateObject(const xAOD::Egamma*& egamma) const;
+};
 }
 
 #endif // DERIVATIONFRAMEWORK_GainDecorator_H
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkCalo/src/JetCaloClusterThinning.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkCalo/src/JetCaloClusterThinning.cxx
@@ -9,182 +9,220 @@
 
 #include "DerivationFrameworkCalo/JetCaloClusterThinning.h"
 
-#include "xAODJet/Jet.h"
 #include "xAODCaloEvent/CaloCluster.h"
+#include "xAODJet/Jet.h"
 
-#include "xAODJet/JetConstituentVector.h"
-#include "StoreGate/ThinningHandle.h"
 #include "GaudiKernel/ThreadLocalContext.h"
+#include "StoreGate/ThinningHandle.h"
+#include "xAODJet/JetConstituentVector.h"
 
-#include <vector>
 #include <string>
+#include <vector>
 
 // Constructor
-DerivationFramework::JetCaloClusterThinning::JetCaloClusterThinning(const std::string& t,
-                                                                    const std::string& n,
-                                                                    const IInterface* p ) :
-base_class(t,n,p),
-m_ntotTopo(0),
-m_npassTopo(0),
-m_selectionString(""),
-m_coneSize(-1.0)
+DerivationFramework::JetCaloClusterThinning::JetCaloClusterThinning(
+  const std::string& t,
+  const std::string& n,
+  const IInterface* p)
+  : base_class(t, n, p)
+  , m_ntotTopo(0)
+  , m_npassTopo(0)
+  , m_selectionString("")
+  , m_coneSize(-1.0)
 {
-    declareInterface<DerivationFramework::IThinningTool>(this);
-    declareProperty("SelectionString", m_selectionString);
-    declareProperty("ConeSize", m_coneSize);
+  declareInterface<DerivationFramework::IThinningTool>(this);
+  declareProperty("SelectionString", m_selectionString);
+  declareProperty("ConeSize", m_coneSize);
 }
 
 // Destructor
-DerivationFramework::JetCaloClusterThinning::~JetCaloClusterThinning() {
-}
+DerivationFramework::JetCaloClusterThinning::~JetCaloClusterThinning() {}
 
 // Athena initialize and finalize
-StatusCode DerivationFramework::JetCaloClusterThinning::initialize()
+StatusCode
+DerivationFramework::JetCaloClusterThinning::initialize()
 {
-    // Decide which collections need to be checked for ID TrackParticles
-    ATH_MSG_VERBOSE("initialize() ...");
-    ATH_CHECK( m_TopoClSGKey.initialize (m_streamName) );
-    ATH_MSG_INFO("Using " << m_TopoClSGKey.key() << "as the source collection for topo calo clusters");
-    if (m_sgKey.empty()) {
-        ATH_MSG_FATAL("No jet collection provided for thinning.");
-        return StatusCode::FAILURE;
-    }
-    else {
-      ATH_MSG_INFO("Calo clusters associated with objects in " << m_sgKey.key() << " will be retained in this format with the rest being thinned away");
-      ATH_CHECK( m_sgKey.initialize() );
-    }
+  // Decide which collections need to be checked for ID TrackParticles
+  ATH_MSG_VERBOSE("initialize() ...");
+  ATH_CHECK(m_TopoClSGKey.initialize(m_streamName));
+  ATH_MSG_INFO("Using " << m_TopoClSGKey.key()
+                        << "as the source collection for topo calo clusters");
+  if (m_sgKey.empty()) {
+    ATH_MSG_FATAL("No jet collection provided for thinning.");
+    return StatusCode::FAILURE;
+  } else {
+    ATH_MSG_INFO(
+      "Calo clusters associated with objects in "
+      << m_sgKey.key()
+      << " will be retained in this format with the rest being thinned away");
+    ATH_CHECK(m_sgKey.initialize());
+  }
 
-    // Set up the text-parsing machinery for selectiong the photon directly according to user cuts
-    if (!m_selectionString.empty()) {
-       ATH_CHECK( initializeParser( m_selectionString ));
-    }
+  // Set up the text-parsing machinery for selectiong the photon directly
+  // according to user cuts
+  if (!m_selectionString.empty()) {
+    ATH_CHECK(initializeParser(m_selectionString));
+  }
 
-    return StatusCode::SUCCESS;
+  return StatusCode::SUCCESS;
 }
 
-StatusCode DerivationFramework::JetCaloClusterThinning::finalize()
+StatusCode
+DerivationFramework::JetCaloClusterThinning::finalize()
 {
-    ATH_MSG_VERBOSE("finalize() ...");
-    ATH_MSG_INFO("Processed "<< m_ntotTopo <<" topo clusters, of which "<< m_npassTopo << " were retained ");
-    ATH_CHECK( finalizeParser());
-    return StatusCode::SUCCESS;
+  ATH_MSG_VERBOSE("finalize() ...");
+  ATH_MSG_INFO("Processed " << m_ntotTopo << " topo clusters, of which "
+                            << m_npassTopo << " were retained ");
+  ATH_CHECK(finalizeParser());
+  return StatusCode::SUCCESS;
 }
 
 // The thinning itself
-StatusCode DerivationFramework::JetCaloClusterThinning::doThinning() const
+StatusCode
+DerivationFramework::JetCaloClusterThinning::doThinning() const
 {
-    const EventContext& ctx = Gaudi::Hive::currentContext();
+  const EventContext& ctx = Gaudi::Hive::currentContext();
 
-    // Retrieve CalCaloTopo collection if required
-    SG::ThinningHandle<xAOD::CaloClusterContainer> importedTopoCaloCluster
-      (m_TopoClSGKey, ctx);
-    
-    // Check the event contains tracks
-    unsigned int nTopoClusters = importedTopoCaloCluster->size();
-    if (nTopoClusters==0) return StatusCode::SUCCESS;
+  // Retrieve CalCaloTopo collection if required
+  SG::ThinningHandle<xAOD::CaloClusterContainer> importedTopoCaloCluster(
+    m_TopoClSGKey, ctx);
 
-    // Set up a mask with the same entries as the full TrackParticle collection(s)
-    std::vector<bool> topomask;
-    topomask.assign(nTopoClusters,false);
-    m_ntotTopo += nTopoClusters;
-    
-    // Retrieve jet container
-    const xAOD::JetContainer* importedJets(nullptr);
-    SG::ReadHandle<xAOD::JetContainer> importedJetsHandle{ m_sgKey, ctx };
-    importedJets = importedJetsHandle.ptr();
-    if (importedJets==nullptr) {
-      ATH_MSG_ERROR("No jet collection with name " << m_sgKey.key() << " found in StoreGate!");
-      return StatusCode::FAILURE;
-    }
-    unsigned int nJets(importedJets->size());
-    if (nJets==0) return StatusCode::SUCCESS;
-    std::vector<const xAOD::Jet*> jetToCheck; jetToCheck.clear();
-    
-    // Execute the text parsers if requested
-    if (!m_selectionString.empty()) {
-        std::vector<int> entries =  m_parser->evaluateAsVector();
-        unsigned int nEntries = entries.size();
-        // check the sizes are compatible
-        if (nJets!= nEntries ) {
-        	ATH_MSG_ERROR("Sizes incompatible! Are you sure your selection string used jets objects??");
-            return StatusCode::FAILURE;
-        } else {
-        	// identify which e-gammas to keep for the thinning check
-        	for (unsigned int i=0; i<nJets; ++i) if (entries[i]==1) jetToCheck.push_back((*importedJets)[i]);
-        }
-    }
-    
-    // Set elements in the mask to true if there is a corresponding ElementLink from a reconstructed object
-    if (m_selectionString.empty()) { // check all objects as user didn't provide a selection string
-        setJetClustersMask(topomask, importedJets, importedTopoCaloCluster.cptr());
-    } else {
-        setJetClustersMask(topomask, jetToCheck, importedTopoCaloCluster.cptr());
-    }
-    
-    // Count up the mask contents
-    for (unsigned int i=0; i<nTopoClusters; ++i) {
-        if (topomask[i]) ++m_npassTopo;
-    }
-    
-    // Execute the thinning service based on the mask. Finish.
-    importedTopoCaloCluster.keep (topomask);
-    
+  // Check the event contains tracks
+  unsigned int nTopoClusters = importedTopoCaloCluster->size();
+  if (nTopoClusters == 0)
     return StatusCode::SUCCESS;
+
+  // Set up a mask with the same entries as the full TrackParticle collection(s)
+  std::vector<bool> topomask;
+  topomask.assign(nTopoClusters, false);
+  m_ntotTopo += nTopoClusters;
+
+  // Retrieve jet container
+  const xAOD::JetContainer* importedJets(nullptr);
+  SG::ReadHandle<xAOD::JetContainer> importedJetsHandle{ m_sgKey, ctx };
+  importedJets = importedJetsHandle.ptr();
+  if (importedJets == nullptr) {
+    ATH_MSG_ERROR("No jet collection with name " << m_sgKey.key()
+                                                 << " found in StoreGate!");
+    return StatusCode::FAILURE;
+  }
+  unsigned int nJets(importedJets->size());
+  if (nJets == 0)
+    return StatusCode::SUCCESS;
+  std::vector<const xAOD::Jet*> jetToCheck;
+  jetToCheck.clear();
+
+  // Execute the text parsers if requested
+  if (!m_selectionString.empty()) {
+    std::vector<int> entries = m_parser->evaluateAsVector();
+    unsigned int nEntries = entries.size();
+    // check the sizes are compatible
+    if (nJets != nEntries) {
+      ATH_MSG_ERROR("Sizes incompatible! Are you sure your selection string "
+                    "used jets objects??");
+      return StatusCode::FAILURE;
+    } else {
+      // identify which e-gammas to keep for the thinning check
+      for (unsigned int i = 0; i < nJets; ++i)
+        if (entries[i] == 1)
+          jetToCheck.push_back((*importedJets)[i]);
+    }
+  }
+
+  // Set elements in the mask to true if there is a corresponding ElementLink
+  // from a reconstructed object
+  if (m_selectionString.empty()) { // check all objects as user didn't provide a
+                                   // selection string
+    setJetClustersMask(topomask, importedJets, importedTopoCaloCluster.cptr());
+  } else {
+    setJetClustersMask(topomask, jetToCheck, importedTopoCaloCluster.cptr());
+  }
+
+  // Count up the mask contents
+  for (unsigned int i = 0; i < nTopoClusters; ++i) {
+    if (topomask[i])
+      ++m_npassTopo;
+  }
+
+  // Execute the thinning service based on the mask. Finish.
+  importedTopoCaloCluster.keep(topomask);
+
+  return StatusCode::SUCCESS;
 }
 
-void DerivationFramework::JetCaloClusterThinning::setJetClustersMask(std::vector<bool>& mask,
-                                                                      const xAOD::JetContainer* jets,
-                                                                      const xAOD::CaloClusterContainer* cps
-                                                                      ) const {
-    for (xAOD::JetContainer::const_iterator jetIt=jets->begin(); jetIt!=jets->end(); ++jetIt) {
-        const xAOD::Jet* jet = dynamic_cast<const xAOD::Jet*>(*jetIt);
-        if (jet) select(jet, m_coneSize, cps, mask); // check clusters amongst constituents
-    }
+void
+DerivationFramework::JetCaloClusterThinning::setJetClustersMask(
+  std::vector<bool>& mask,
+  const xAOD::JetContainer* jets,
+  const xAOD::CaloClusterContainer* cps) const
+{
+  for (xAOD::JetContainer::const_iterator jetIt = jets->begin();
+       jetIt != jets->end();
+       ++jetIt) {
+    const xAOD::Jet* jet = dynamic_cast<const xAOD::Jet*>(*jetIt);
+    if (jet)
+      select(jet, m_coneSize, cps, mask); // check clusters amongst constituents
+  }
 
-    return;
+  return;
 }
 
-void DerivationFramework::JetCaloClusterThinning::setJetClustersMask(std::vector<bool>& mask,
-                                                                      std::vector<const xAOD::Jet*>& jets,
-                                                                      const xAOD::CaloClusterContainer* cps
-                                                                      ) const {
-    for (std::vector<const xAOD::Jet*>::iterator jetIt=jets.begin(); jetIt!=jets.end(); ++jetIt) {
-        const xAOD::Jet* jet = dynamic_cast<const xAOD::Jet*>(*jetIt);
-        if (jet) select(jet, m_coneSize, cps, mask); // check clusters amongst constituents
-    }
-    return;
+void
+DerivationFramework::JetCaloClusterThinning::setJetClustersMask(
+  std::vector<bool>& mask,
+  std::vector<const xAOD::Jet*>& jets,
+  const xAOD::CaloClusterContainer* cps) const
+{
+  for (std::vector<const xAOD::Jet*>::iterator jetIt = jets.begin();
+       jetIt != jets.end();
+       ++jetIt) {
+    const xAOD::Jet* jet = dynamic_cast<const xAOD::Jet*>(*jetIt);
+    if (jet)
+      select(jet, m_coneSize, cps, mask); // check clusters amongst constituents
+  }
+  return;
 }
 
-void DerivationFramework::JetCaloClusterThinning::select(const xAOD::Jet* particle,
-                                                         float coneSize,
-                                                         const xAOD::CaloClusterContainer* clusters,
-                                                         std::vector<bool> &mask) {
-    xAOD::JetConstituentVector vec = particle->getConstituents();
-    xAOD::JetConstituentVector::iterator it = vec.begin();
-    xAOD::JetConstituentVector::iterator itE = vec.end();
-    for (; it != itE; ++it) {
-        unsigned int i(0);
-        for (xAOD::CaloClusterContainer::const_iterator clIt = clusters->begin(); clIt!=clusters->end(); ++clIt, ++i) {
-            float deltaEta = (*clIt)->eta() - (*it)->eta();
-            float deltaPhi = fabs((*clIt)->phi() - (*it)->phi());
-            if (deltaPhi > TMath::Pi()) deltaPhi = 2.0*TMath::Pi() - deltaPhi;
-            float deltaR = sqrt(deltaEta*deltaEta + deltaPhi*deltaPhi);
-            if (deltaR < 0.05) mask[i] = true;
-        }
+void
+DerivationFramework::JetCaloClusterThinning::select(
+  const xAOD::Jet* particle,
+  float coneSize,
+  const xAOD::CaloClusterContainer* clusters,
+  std::vector<bool>& mask)
+{
+  xAOD::JetConstituentVector vec = particle->getConstituents();
+  xAOD::JetConstituentVector::iterator it = vec.begin();
+  xAOD::JetConstituentVector::iterator itE = vec.end();
+  for (; it != itE; ++it) {
+    unsigned int i(0);
+    for (xAOD::CaloClusterContainer::const_iterator clIt = clusters->begin();
+         clIt != clusters->end();
+         ++clIt, ++i) {
+      float deltaEta = (*clIt)->eta() - (*it)->eta();
+      float deltaPhi = fabs((*clIt)->phi() - (*it)->phi());
+      if (deltaPhi > TMath::Pi())
+        deltaPhi = 2.0 * TMath::Pi() - deltaPhi;
+      float deltaR = sqrt(deltaEta * deltaEta + deltaPhi * deltaPhi);
+      if (deltaR < 0.05)
+        mask[i] = true;
     }
-    if (coneSize > 0) {
-        unsigned int i(0);
-        float particleEta = particle->eta();
-        float particlePhi = particle->phi();
-        for (xAOD::CaloClusterContainer::const_iterator clIt = clusters->begin(); clIt!=clusters->end(); ++clIt, ++i) {
-            float deltaEta = (*clIt)->eta() - particleEta;
-            float deltaPhi = fabs((*clIt)->phi() - particlePhi);
-            if (deltaPhi > TMath::Pi()) deltaPhi = 2.0*TMath::Pi() - deltaPhi;
-            float deltaR = sqrt(deltaEta*deltaEta + deltaPhi*deltaPhi);
-            if (deltaR < coneSize) mask[i] = true;
-        }
+  }
+  if (coneSize > 0) {
+    unsigned int i(0);
+    float particleEta = particle->eta();
+    float particlePhi = particle->phi();
+    for (xAOD::CaloClusterContainer::const_iterator clIt = clusters->begin();
+         clIt != clusters->end();
+         ++clIt, ++i) {
+      float deltaEta = (*clIt)->eta() - particleEta;
+      float deltaPhi = fabs((*clIt)->phi() - particlePhi);
+      if (deltaPhi > TMath::Pi())
+        deltaPhi = 2.0 * TMath::Pi() - deltaPhi;
+      float deltaR = sqrt(deltaEta * deltaEta + deltaPhi * deltaPhi);
+      if (deltaR < coneSize)
+        mask[i] = true;
     }
-    return;
+  }
+  return;
 }
-
 
