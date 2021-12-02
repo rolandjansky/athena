e68a6d27a51a4978372f5c71f157e28acb136175
Sergi Rodriguez Bosca <sergi.rodriguez@cern.ch>
2021-12-02 19:08:12 +0100

    jFEX TOB bit shifted to agree with firmware

--- Trigger/TrigT1/L1CaloFEX/L1CaloFEXSim/src/jFEXFPGA.cxx
+++ Trigger/TrigT1/L1CaloFEX/L1CaloFEXSim/src/jFEXFPGA.cxx
@@ -669,7 +669,7 @@ uint32_t jFEXFPGA::formSmallRJetTOB(int &iphi, int &ieta) {
         Sat = 1;
     }
     //create basic tobword with 32 bits
-    tobWord = tobWord + (eta<<27) + (phi << 23) + (jFEXSmallRJetTOBEt << 12) + (Res<<1) + (Sat);
+    tobWord = tobWord + (Res << 21) + (eta << 16) + (phi << 12) + (jFEXSmallRJetTOBEt << 1)  + (Sat);
     ATH_MSG_DEBUG("tobword smallRJet with et, phi, eta, res and sat : " << std::bitset<32>(tobWord) );
      //Retrieve the L1 menu configuration
     SG::ReadHandle<TrigConf::L1Menu> l1Menu (m_l1MenuKey/*, ctx*/);
@@ -734,7 +734,7 @@ uint32_t jFEXFPGA::formLargeRJetTOB(int &iphi, int &ieta) {
         Sat = 1;
     }
     //create basic tobword with 32 bits
-    tobWord = tobWord + (eta<<27) + (phi << 23) + (jFEXLargeRJetTOBEt << 10) + (Res<<1) + (Sat);
+    tobWord = tobWord + (Res << 23) + (eta << 18) + (phi << 14) + (jFEXLargeRJetTOBEt << 1) + (Sat);
     ATH_MSG_DEBUG("tobword largeRJet with et, phi, eta, sub and sat : " << std::bitset<32>(tobWord) );
 
     // Retrieve the L1 menu configuration
@@ -872,7 +872,7 @@ uint32_t jFEXFPGA::formMetTOB(int METX, int METY ) {
     }
 
     //create basic tobword with 32 bits
-    tobWord = tobWord + ((metX & 0x7fff) << 17) + (sat << 16) + ((metY & 0x7fff) << 1) + res ;
+    tobWord = tobWord + (res << 31) +((metX & 0x7fff) << 16) + (sat << 15) + (metY & 0x7fff)  ;
     ATH_MSG_DEBUG("tobword MET with MET_X, Sat, MET_Y and Res : " << std::bitset<32>(tobWord) );
 
     return tobWord;
