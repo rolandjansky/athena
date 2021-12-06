fc4739cb40a71e738e1f8cfc553db6f920508e7e
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2021-12-06 10:13:07 +0100

    xAOD::EventAuxInfo_v3, master branch (2021.11.30.)

--- Event/xAOD/xAODEventInfoAthenaPool/share/xAODEventInfoAthenaPool_20.0.0.3_test.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
-
-infile = 'aod/AOD-20.0.0.3/AOD-20.0.0.3-full.pool.root'
-keys = [
-   #xAOD::EventInfo
-   'EventInfo'
-   ]
-include ('AthenaPoolUtilities/TPCnvTest.py')
--- Event/xAOD/xAODEventInfoAthenaPool/src/xAODEventAuxInfoCnv_v2.cxx
+++ Event/xAOD/xAODEventInfoAthenaPool/src/xAODEventAuxInfoCnv_v2.cxx
@@ -1,7 +1,7 @@
 // Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 
 // Local include(s).
-#include "xAODEventAuxInfoCnv_v1.h"
+#include "xAODEventAuxInfoCnv_v2.h"
 #include "MessageMacros.h"
 
 // Core EDM include(s):
@@ -10,31 +10,31 @@
 // System include(s).
 #include <stdexcept>
 
-void xAODEventAuxInfoCnv_v1::persToTrans( const xAOD::EventAuxInfo_v1* oldObj,
+void xAODEventAuxInfoCnv_v2::persToTrans( const xAOD::EventAuxInfo_v2* oldObj,
                                           xAOD::EventAuxInfo* newObj,
                                           MsgStream& log ) const {
 
    // Greet the user.
-   ATH_MSG( "Converting xAOD::EventAuxInfo_v1 to the current version..." );
+   ATH_MSG( "Converting xAOD::EventAuxInfo_v2 to the current version..." );
 
-   // Copy the payload of the v1 object into the latest one by misusing
+   // Copy the payload of the v2 object into the latest one by misusing
    // the thinning code a bit...
    SG::copyAuxStoreThinned( *oldObj, *newObj, nullptr );
 
    // Print what happened:
-   ATH_MSG( "Converting xAOD::EventAuxInfo_v1 to the current version... "
+   ATH_MSG( "Converting xAOD::EventAuxInfo_v2 to the current version... "
             "[OK]" );
    return;
 }
 
-void xAODEventAuxInfoCnv_v1::transToPers( const xAOD::EventAuxInfo*,
-                                          xAOD::EventAuxInfo_v1*,
+void xAODEventAuxInfoCnv_v2::transToPers( const xAOD::EventAuxInfo*,
+                                          xAOD::EventAuxInfo_v2*,
                                           MsgStream& log ) const {
 
    log << MSG::ERROR
-       << "Somebody called xAODEventAuxInfoCnv_v1::transToPers"
+       << "Somebody called xAODEventAuxInfoCnv_v2::transToPers"
        << endmsg;
-   throw std::runtime_error( "Somebody called xAODEventAuxInfoCnv_v1::"
+   throw std::runtime_error( "Somebody called xAODEventAuxInfoCnv_v2::"
                              "transToPers" );
    return;
 }
