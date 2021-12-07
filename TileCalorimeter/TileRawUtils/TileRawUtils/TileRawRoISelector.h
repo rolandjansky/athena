cd0d85c087edfaa7426140db80e137b091661309
scott snyder <snyder@bnl.gov>
2021-12-07 20:57:16 +0100

    TileRawUtils: Fix clang13 warning.
    Deprecated implicit declaration.

--- TileCalorimeter/TileRawUtils/TileRawUtils/TileRawRoISelector.h
+++ TileCalorimeter/TileRawUtils/TileRawUtils/TileRawRoISelector.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef TILERAWUTILS_TILERAWROISELECTOR_H
@@ -56,8 +56,6 @@ class TileRawRoISelector
         friend class TileRawRoISelector ; 
 	public:
 	const_iterator() ; 
-
-        const_iterator& operator = ( const const_iterator & it ) = default;
 
 	const_iterator& operator ++ () ; 
 
