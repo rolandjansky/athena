f2483c73fbfbec380728e986e265ec7a82ae6b9f
Petr Balek <petr.balek@cern.ch>
2021-12-08 01:54:03 +0100

    adding new AFP clustering tool

--- ForwardDetectors/AFP/AFP_Reconstruction/AFP_SiClusterTools/src/components/AFP_SiClusterTools_entries.cxx
+++ ForwardDetectors/AFP/AFP_Reconstruction/AFP_SiClusterTools/src/components/AFP_SiClusterTools_entries.cxx
@@ -3,10 +3,12 @@
 #include "AFP_SiClusterTools/AFPSiClusterSingleHitTool.h"
 #include "AFP_SiClusterTools/AFPSiRowColToLocalCSTool.h"
 #include "AFP_SiClusterTools/AFPSiClusterBasicNearestNeighbour.h"
+#include "AFP_SiClusterTools/AFPSiClusterAllNeighbours.h"
 
 DECLARE_COMPONENT( AFPSiCluster )
 DECLARE_COMPONENT( AFPSiClusterTool )
 DECLARE_COMPONENT( AFPSiClusterSingleHitTool )
 DECLARE_COMPONENT( AFPSiRowColToLocalCSTool )
 DECLARE_COMPONENT( AFPSiClusterBasicNearestNeighbour )
+DECLARE_COMPONENT( AFPSiClusterAllNeighbours )
 
