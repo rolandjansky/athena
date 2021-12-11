c7724fbe61909aba2eac1040b2bad10fa04752f8
christos <christos@cern.ch>
2021-12-11 22:32:21 +0100

    CaloClusterROI_Builder pass CaloDetDescr to the CaloSurfaceBuilder and cleanup

--- InnerDetector/InDetRecTools/InDetCaloClusterROIBuilder/src/CaloClusterROI_Builder.cxx
+++ InnerDetector/InDetRecTools/InDetCaloClusterROIBuilder/src/CaloClusterROI_Builder.cxx
@@ -9,80 +9,66 @@
 #include "xAODCaloEvent/CaloCluster.h"
 #include "xAODEgamma/EgammaxAODHelpers.h"
 
-
-  
 //#include "TrkEventPrimitives/GlobalPosition.h"
 #include "TrkEventPrimitives/LocalParameters.h"
 #include "TrkSurfaces/Surface.h"
 
 #include "TrkCaloClusterROI/CaloClusterROI.h"
 
-
 InDet::CaloClusterROI_Builder::CaloClusterROI_Builder(const std::string& t,
-                                               const std::string& n,
-                                               const IInterface*  p )
-: AthAlgTool(t,n,p)
+                                                      const std::string& n,
+                                                      const IInterface* p)
+  : AthAlgTool(t, n, p)
 {
   declareInterface<ICaloClusterROI_Builder>(this);
 }
 
-//--------------------------------------------------------------------------------------------
 InDet::CaloClusterROI_Builder::~CaloClusterROI_Builder() {}
-//--------------------------------------------------------------------------------------------
 
+StatusCode
+InDet::CaloClusterROI_Builder::initialize()
+{
 
-//--------------------------------------------------------------------------------------------
-StatusCode InDet::CaloClusterROI_Builder::initialize() {
-//--------------------------------------------------------------------------------------------
-  
-  ATH_MSG_INFO("Initializing CaloClusterROI_Builder");
- 
-  // Retrieve the updator CaloSurfaceBuilder
-  if ( m_calosurf.retrieve().isFailure() ){
-    ATH_MSG_FATAL ( "Unable to retrieve the instance " << m_calosurf.name() << "... Exiting!" );
-    return StatusCode::FAILURE;
-  }
+  ATH_CHECK(m_calosurf.retrieve());
+  ATH_CHECK(m_caloMgrKey.initialize());
 
   return StatusCode::SUCCESS;
 }
 
-//--------------------------------------------------------------------------------------------
-StatusCode InDet::CaloClusterROI_Builder::finalize(){ return StatusCode::SUCCESS; }
-//--------------------------------------------------------------------------------------------
-
-
-
-//--------------------------------------------------------------------------------------------
-Trk::CaloClusterROI* InDet::CaloClusterROI_Builder::buildClusterROI( const xAOD::CaloCluster* cluster ) const
-//--------------------------------------------------------------------------------------------
+Trk::CaloClusterROI*
+InDet::CaloClusterROI_Builder::buildClusterROI(
+  const xAOD::CaloCluster* cluster) const
 {
 
   ATH_MSG_DEBUG("Building Trk::CaloCluster_OnTrack");
-   
-  if(!cluster) return nullptr;
 
-  const Trk::Surface* surface = getCaloSurface( cluster );
-  
-  if(!surface) return nullptr;
-  
-  const Trk::LocalParameters* lp = getClusterLocalParameters( cluster, surface );
+  if (!cluster)
+    return nullptr;
 
-  if (!lp){
+  const Trk::Surface* surface = getCaloSurface(cluster);
+
+  if (!surface)
+    return nullptr;
+
+  const Trk::LocalParameters* lp = getClusterLocalParameters(cluster, surface);
+
+  if (!lp) {
     delete surface;
     return nullptr;
   }
-  
-  double energy    = cluster->e();
-  double  widthPhi = 0.1;
-  double  widthEta = 0.1;
-  
+
+  double energy = cluster->e();
+  double widthPhi = 0.1;
+  double widthEta = 0.1;
+
   // do we want to make energy be EM energy only?
   if (m_EMEnergyOnly) {
-    static const  SG::AuxElement::ConstAccessor<float> acc("EMFraction");
+    static const SG::AuxElement::ConstAccessor<float> acc("EMFraction");
     double emFrac(0.);
     if (acc.isAvailable(*cluster)) {
       emFrac = acc(*cluster);
-    } else if (!cluster->retrieveMoment(xAOD::CaloCluster::ENG_FRAC_EM,emFrac)){
+    } else if (!cluster->retrieveMoment(xAOD::CaloCluster::ENG_FRAC_EM,
+                                        emFrac)) {
       ATH_MSG_ERROR("EM energy requested, but No EM fraction momement stored");
       delete surface;
       delete lp;
@@ -90,84 +76,82 @@ Trk::CaloClusterROI* InDet::CaloClusterROI_Builder::buildClusterROI( const xAOD:
     }
     energy *= emFrac;
   }
-  
-  Trk::CaloClusterROI* ccROI = new  Trk::CaloClusterROI( lp, *surface, energy, widthPhi, widthEta );
+
+  Trk::CaloClusterROI* ccROI =
+    new Trk::CaloClusterROI(lp, *surface, energy, widthPhi, widthEta);
   delete surface;
-  
-  if(ccROI) {
+
+  if (ccROI) {
     ATH_MSG_DEBUG("Successful build of Trk::CaloClusterROI");
   }
-  
+
   return ccROI;
-
-
 }
 
-
-
-//--------------------------------------------------------------------------------------------
-const Trk::Surface*  InDet::CaloClusterROI_Builder::getCaloSurface( const xAOD::CaloCluster* cluster ) const
-//--------------------------------------------------------------------------------------------
+const Trk::Surface*
+InDet::CaloClusterROI_Builder::getCaloSurface(
+  const xAOD::CaloCluster* cluster) const
 {
- 
+
   const Trk::Surface* destinationSurface = nullptr;
-  
-  // Determine if we want to extrapolate to the barrel or endcap.  If in the crack choose the 
-  // detector with largest amount of energy in the second sampling layer 
-  if ( xAOD::EgammaHelpers::isBarrel( cluster ) )
-  {
-    destinationSurface = m_calosurf->CreateUserSurface (CaloCell_ID::EMB2, 0. , cluster->eta() );
-  } else{ 
-    destinationSurface = m_calosurf->CreateUserSurface (CaloCell_ID::EME2, 0. , cluster->eta() );
+
+  SG::ReadCondHandle<CaloDetDescrManager> caloMgrHandle{ m_caloMgrKey };
+  const CaloDetDescrManager* caloDDMgr = *caloMgrHandle;
+
+  // Determine if we want to extrapolate to the barrel or endcap.  If in the
+  // crack choose the detector with largest amount of energy in the second
+  // sampling layer
+  if (xAOD::EgammaHelpers::isBarrel(cluster)) {
+    destinationSurface = m_calosurf->CreateUserSurface(
+      CaloCell_ID::EMB2, 0., cluster->eta(), caloDDMgr);
+  } else {
+    destinationSurface = m_calosurf->CreateUserSurface(
+      CaloCell_ID::EME2, 0., cluster->eta(), caloDDMgr);
   }
   return destinationSurface;
 }
 
-
-//--------------------------------------------------------------------------------------------
-const Trk::LocalParameters*  InDet::CaloClusterROI_Builder::getClusterLocalParameters( const xAOD::CaloCluster* cluster, 
-                                                                                       const Trk::Surface* surf) const
-//--------------------------------------------------------------------------------------------
+const Trk::LocalParameters*
+InDet::CaloClusterROI_Builder::getClusterLocalParameters(
+  const xAOD::CaloCluster* cluster,
+  const Trk::Surface* surf) const
 {
- 
+
   Amg::Vector3D surfRefPoint = surf->globalReferencePoint();
-  //std::cout << "REFPOINT " << "[r,phi,z] = [ " << surfRefPoint.perp() << ", " << surfRefPoint.phi() << ", " << surfRefPoint.z() << " ]" <<std::endl; 
-  
+
   double eta = cluster->eta();
-  double theta = 2*atan(exp(-eta)); //  -log(tan(theta/2));
+  double theta = 2 * atan(exp(-eta)); //  -log(tan(theta/2));
   double tantheta = tan(theta);
   double phi = cluster->phi();
-  
+
   Trk::LocalParameters* newLocalParameters(nullptr);
- 
-  if (  xAOD::EgammaHelpers::isBarrel( cluster ) ){
-    //Two corindate in a cyclinder are 
-    //Trk::locRPhi = 0 (ie phi)
-    //Trk::locZ    = 1(ie z)
-    double r = surfRefPoint.perp() ;
-    double z = tantheta == 0 ? 0. : r/tantheta;      
-    Trk::DefinedParameter locRPhi( r * phi  ,  Trk::locRPhi  ) ;
-    Trk::DefinedParameter locZ   ( z        ,  Trk::locZ     ) ;
-    std::vector<Trk::DefinedParameter> defPar ;
-    defPar.push_back( locRPhi ) ;
-    defPar.push_back( locZ ) ;
-    newLocalParameters = new Trk::LocalParameters( defPar ) ;
-  } else{ 
-    //Local paramters of a disk are
-    //Trk::locR   = 0
-    //Trk::locPhi = 1
+
+  if (xAOD::EgammaHelpers::isBarrel(cluster)) {
+    // Two corindate in a cyclinder are
+    // Trk::locRPhi = 0 (ie phi)
+    // Trk::locZ    = 1(ie z)
+    double r = surfRefPoint.perp();
+    double z = tantheta == 0 ? 0. : r / tantheta;
+    Trk::DefinedParameter locRPhi(r * phi, Trk::locRPhi);
+    Trk::DefinedParameter locZ(z, Trk::locZ);
+    std::vector<Trk::DefinedParameter> defPar;
+    defPar.push_back(locRPhi);
+    defPar.push_back(locZ);
+    newLocalParameters = new Trk::LocalParameters(defPar);
+  } else {
+    // Local paramters of a disk are
+    // Trk::locR   = 0
+    // Trk::locPhi = 1
     double z = surfRefPoint.z();
-    double r = z*tantheta; 
-    Trk::DefinedParameter locR  ( r   ,  Trk::locR    ) ;
-    Trk::DefinedParameter locPhi( phi ,  Trk::locPhi  ) ;
-    std::vector<Trk::DefinedParameter> defPar ;
-    defPar.push_back( locR ) ;
-    defPar.push_back( locPhi ) ;
-    newLocalParameters = new Trk::LocalParameters( defPar ) ;
+    double r = z * tantheta;
+    Trk::DefinedParameter locR(r, Trk::locR);
+    Trk::DefinedParameter locPhi(phi, Trk::locPhi);
+    std::vector<Trk::DefinedParameter> defPar;
+    defPar.push_back(locR);
+    defPar.push_back(locPhi);
+    newLocalParameters = new Trk::LocalParameters(defPar);
   }
-  
+
   return newLocalParameters;
-
 }
-
 
