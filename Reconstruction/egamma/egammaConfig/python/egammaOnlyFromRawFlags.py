49831428acd1fa5ad4ad5db065e6e1610dedd8e7
christos <christos@cern.ch>
2021-12-14 21:20:56 +0100

    explicit disable CombinedMuon in egamma alone C.A preinclude

--- Reconstruction/egamma/egammaConfig/python/egammaOnlyFromRawFlags.py
+++ Reconstruction/egamma/egammaConfig/python/egammaOnlyFromRawFlags.py
@@ -4,6 +4,7 @@
 def egammaOnlyFromRaw(flags):
     """egammaOnlyFromRaw flags for Reco_tf with CA"""
     flags.Detector.GeometryMuon = False
+    flags.Reco.EnableCombinedMuon = False
     flags.Reco.EnablePFlow = False
     flags.Reco.EnableTau = False
     flags.Reco.EnableJet = False
