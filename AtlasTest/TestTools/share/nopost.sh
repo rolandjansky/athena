efb1d5cfbb9f73c9d42c0de75905e8ab6f873ea0
Frank Winklmeier <frank.winklmeier@cern.ch>
2021-12-14 14:40:16 +0100

    TestTools: remove use of command line args in post.sh
    Instead of using command line args to drive the execution of `post.sh`
    use the new environment variables that are set by the test executor (see
    atlasexternals!901, atlasexternals!908).

--- AtlasTest/TestTools/share/nopost.sh
+++ AtlasTest/TestTools/share/nopost.sh
@@ -3,9 +3,9 @@
 # Post-processing script to be used with atlas_add_test if no
 # log file post-processing is required (as done in post.sh)
 
-if [ -z "$testStatus" ]; then
-   echo "nopost.sh> Error: athena exit status is not available (\$testStatus is not set)"
+if [ -z "$ATLAS_CTEST_TESTSTATUS" ]; then
+   echo "nopost.sh> Error: athena exit status is not available (\$ATLAS_CTEST_TESTSTATUS is not set)"
    exit 1
 fi
 
-exit $testStatus
+exit $ATLAS_CTEST_TESTSTATUS
