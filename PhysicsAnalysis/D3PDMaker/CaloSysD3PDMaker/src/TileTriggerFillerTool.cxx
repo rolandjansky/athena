4c8d75a0e86ef573f940eced436f958ea8cefe84
scott snyder <sss@karma>
2021-12-16 11:06:36 -0500

    CaloSysD3PDMaker: Fix cppcheck 2.6 warnings.
    Explicitly qualify virtual methods called from ctors.

--- PhysicsAnalysis/D3PDMaker/CaloSysD3PDMaker/src/TileTriggerFillerTool.cxx
+++ PhysicsAnalysis/D3PDMaker/CaloSysD3PDMaker/src/TileTriggerFillerTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 /* 
@@ -26,7 +26,7 @@ TileTriggerFillerTool::TileTriggerFillerTool(const string& type,
   BlockFillerTool<TileTrigger>(type,name,parent),
   m_TT_ID(0)
 {
-  book().ignore(); // Avoid coverity warnings
+  TileTriggerFillerTool::book().ignore(); // Avoid coverity warnings
 }
 
 
