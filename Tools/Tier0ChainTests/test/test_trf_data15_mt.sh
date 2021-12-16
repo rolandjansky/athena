69174521076e30567781ed09ae2f3fc550aecf51
Peter Berta <peter.berta@cern.ch>
2021-12-16 15:01:38 +0100

    moved the data tests with output comparison for Run2 Reprocessing to TCT

--- Tools/Tier0ChainTests/test/test_trf_data15_mt.sh
+++ Tools/Tier0ChainTests/test/test_trf_data15_mt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# art-description: Reco_tf.py data17 RAWtoALL in MT mode
+# art-description: Reco_tf.py data15 RAWtoALL in MT mode
 # art-type: grid
 # art-include: master/Athena
 # art-include: 22.0-mc20/Athena
@@ -9,7 +9,7 @@
 # Updated on 7.12.2021 based on the tag r13100 which is currently used for Run2 Reprocessing
 
 timeout 64800 Reco_tf.py \
-  --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data17_13TeV.00330470.physics_Main.daq.RAW._lb0310._SFO-1._0001.data \
+  --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data15_13TeV.00276689.physics_Main.daq.RAW._lb0220._SFO-1._0001.data \
   --outputAODFile=myAOD.pool.root \
   --outputHISTFile=myHIST.root \
   --outputDESDM_MCPFile=myDESDM_MCP.pool.root \
@@ -19,7 +19,27 @@ timeout 64800 Reco_tf.py \
   --preExec 'all:from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; from InDetRecExample.InDetJobProperties import InDetFlags;  from RecExConfig.RecFlags import rec; InDetFlags.useMuForTRTErrorScaling.set_Value_and_Lock(True if not rec.doRDOTrigger else False); from AthenaMonitoring.DQMonFlags import DQMonFlags; DQMonFlags.doHLTMon=False;' \
   --autoConfiguration='everything' \
   --conditionsTag 'all:CONDBR2-BLKPA-RUN2-09' --geometryVersion='default:ATLAS-R2-2016-01-00-01' \
-  --runNumber='357750' --steering='doRAWtoALL' --maxEvents='-1'
+  --runNumber='357750' --steering='doRAWtoALL' --maxEvents=200 --imf False
 
 rc1=$?
-echo "art-result: $rc1 Reco_tf_data17_mt"
+echo "art-result: $rc1 Reco_tf_data15_mt"
+
+rc2=-9999
+if [ ${rc1} -eq 0 ]
+then
+  ArtPackage=$1
+  ArtJobName=$2
+  art.py compare grid --entries 30 ${ArtPackage} ${ArtJobName} --mode=semi-detailed --order-trees --ignore-exit-code diff-pool
+  rc2=$?
+fi
+echo  "art-result: ${rc2} Comparison with the latest result"
+
+
+rc3=-9999
+if [ ${rc1} -eq 0 ]
+then
+  art.py compare ref . /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/TrfTestsART_22.0-mc20/test_trf_data15_mt_2021-12-04T2101 --entries 30 --mode=semi-detailed --order-trees --ignore-exit-code diff-pool
+  rc3=$?
+fi
+echo  "art-result: ${rc3} Comparison with fixed reference"
+
--- Tools/Tier0ChainTests/test/test_trf_data18_mt.sh
+++ Tools/Tier0ChainTests/test/test_trf_data18_mt.sh
@@ -19,7 +19,27 @@ timeout 64800 Reco_tf.py \
   --preExec 'all:from AthenaCommon.AthenaCommonFlags import athenaCommonFlags; from InDetRecExample.InDetJobProperties import InDetFlags;  from RecExConfig.RecFlags import rec; InDetFlags.useMuForTRTErrorScaling.set_Value_and_Lock(True if not rec.doRDOTrigger else False); from AthenaMonitoring.DQMonFlags import DQMonFlags; DQMonFlags.doHLTMon=False;' \
   --autoConfiguration='everything' \
   --conditionsTag 'all:CONDBR2-BLKPA-RUN2-09' --geometryVersion='default:ATLAS-R2-2016-01-00-01' \
-  --runNumber='357750' --steering='doRAWtoALL' --maxEvents='-1'
+  --runNumber='357750' --steering='doRAWtoALL' --maxEvents=200 --imf False
 
 rc1=$?
 echo "art-result: $rc1 Reco_tf_data18_mt"
+
+rc2=-9999
+if [ ${rc1} -eq 0 ]
+then
+  ArtPackage=$1
+  ArtJobName=$2
+  art.py compare grid --entries 30 ${ArtPackage} ${ArtJobName} --mode=semi-detailed --order-trees --ignore-exit-code diff-pool
+  rc2=$?
+fi
+echo  "art-result: ${rc2} Comparison with the latest result"
+
+
+rc3=-9999
+if [ ${rc1} -eq 0 ]
+then
+  art.py compare ref . /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/TrfTestsART_22.0-mc20/test_trf_data18_mt_2021-12-04T2101 --entries 30 --mode=semi-detailed --order-trees --ignore-exit-code diff-pool
+  rc3=$?
+fi
+echo  "art-result: ${rc3} Comparison with fixed reference"
+
