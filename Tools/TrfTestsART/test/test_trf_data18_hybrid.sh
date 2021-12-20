a6e05ddf1731444ffa57c1c16a9467975041e780
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2021-12-20 14:44:32 +0100

    Add FPE check to hybrid and MC20e tests and DESDM_MCP+IDTIDE from the hybrid test due to broken merging steps

--- Tools/TrfTestsART/test/test_trf_data18_hybrid.sh
+++ Tools/TrfTestsART/test/test_trf_data18_hybrid.sh
@@ -10,9 +10,7 @@ timeout 64800 Reco_tf.py \
   --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357750.physics_Main.daq.RAW/data18_13TeV.00357750.physics_Main.daq.RAW._lb0114._SFO-5._0003.data \
   --outputAODFile=myAOD.pool.root \
   --outputHISTFile=myHIST.root \
-  --outputDESDM_MCPFile=myDESDM_MCP.pool.root \
   --outputDRAW_ZMUMUFile=myDRAW_ZMUMU.data \
-  --outputDAOD_IDTIDEFile=myIDTIDE.pool.root \
   --athenaopts="--nprocs=2 --threads=4" \
   --preExec 'all:from AthenaMonitoring.DQMonFlags import DQMonFlags; DQMonFlags.doHLTMon=False' \
   --postExec 'FPEAuditor.NStacktracesOnFPE=10' \
@@ -20,4 +18,26 @@ timeout 64800 Reco_tf.py \
   --conditionsTag 'all:CONDBR2-BLKPA-RUN2-09' --geometryVersion='default:ATLAS-R2-2016-01-00-01' \
   --runNumber='357750' --steering='doRAWtoALL' --maxEvents='-1'
 
-echo "art-result: $? Reco_tf_data18_hybrid"
+rc1=$?
+echo "art-result: ${rc1} Reco_tf_data18_hybrid"
+
+echo "==================Checking for FPEs"
+grep --quiet "WARNING FPE" log.*
+fpeStat=$?
+# Let's flip this - finding a FPE is a failure and it'd be confusing to check for 0
+if [[ "$fpeStat" == "0" ]]; then
+  fpeStat="1"
+else
+  fpeStat="0"
+fi
+
+if [[ "$fpeStat" != "0" ]]; then
+  echo "Found FPEs! FAILING the test. FPEs reported below." `date`
+  for file in `ls log.*`;
+    do
+      echo "=====" $file;
+      grep "WARNING FPE" $file | awk '{print $11}' | sed 's/\[//' | sed 's/\]//' | sed -r '/^\s*$/d' | sort  | uniq -c
+    done
+fi
+
+echo "art-result: ${fpeStat} FPEs in logfiles"
