5326eda2eec4c47c75a6ec30927ae1009f2d3697
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2021-12-20 16:53:38 +0100

    TheP8I: switch to version needed for H7.16, Tauolapp add changes from MR48979

--- Projects/AthGeneration/externals/TheP8I.cmake
+++ Projects/AthGeneration/externals/TheP8I.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of TheP8I to use.
 #
 
-set( THEP8I_LCGVERSION "2.0.1.thepeg221p3" )
+set( THEP8I_LCGVERSION "2.0.1.thepeg216p3" )
 set( THEP8I_LCGROOT
   "${LCG_RELEASE_DIR}/MCGenerators/thep8i/${THEP8I_LCGVERSION}/${LCG_PLATFORM}" )
