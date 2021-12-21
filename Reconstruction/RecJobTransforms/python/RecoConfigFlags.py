aaf367205a26469706d4a94d954be9a212a655c6
Tadej Novak <tadej.novak@cern.ch>
2021-12-21 18:00:14 +0100

    Add boilerplate reco postprocessing and try to setup dependencies

--- Reconstruction/RecJobTransforms/python/RecoConfigFlags.py
+++ Reconstruction/RecJobTransforms/python/RecoConfigFlags.py
@@ -6,13 +6,19 @@ def createRecoConfigFlags():
     """Return an AthConfigFlags object with required flags"""
     flags = AthConfigFlags()
     # Different components
-    flags.addFlag("Reco.EnableBTagging", True)
-    flags.addFlag("Reco.EnableCombinedMuon", lambda prevFlags: prevFlags.Detector.EnableMuon)
-    flags.addFlag("Reco.EnableEgamma", True)
-    flags.addFlag("Reco.EnableJet", True)
-    flags.addFlag("Reco.EnablePFlow", True)
-    flags.addFlag("Reco.EnableTau", True)
-    flags.addFlag("Reco.EnableTracking", True)
+    flags.addFlag("Reco.EnableBTagging", lambda prevFlags: prevFlags.Reco.EnableJet)
+    flags.addFlag("Reco.EnableCombinedMuon", lambda prevFlags: prevFlags.Detector.EnableMuon and prevFlags.Reco.EnableTracking)
+    flags.addFlag("Reco.EnableEgamma",
+                  lambda prevFlags: prevFlags.Detector.EnableCalo)
+    flags.addFlag("Reco.EnableJet", lambda prevFlags: (
+        prevFlags.Detector.EnableCalo
+        and prevFlags.Reco.EnableTracking
+        and prevFlags.Reco.EnableEgamma
+        and prevFlags.Reco.EnableCombinedMuon
+        and prevFlags.Reco.EnablePFlow))
+    flags.addFlag("Reco.EnablePFlow", lambda prevFlags: prevFlags.Reco.EnableTracking and prevFlags.Detector.EnableCalo)
+    flags.addFlag("Reco.EnableTau", lambda prevFlags: prevFlags.Reco.EnableJet)
+    flags.addFlag("Reco.EnableTracking", lambda prevFlags: prevFlags.Detector.EnableID or prevFlags.Detector.EnableITk)
     flags.addFlag(
         "Reco.EnableCaloExtension", lambda prevFlags: (
             (
@@ -26,6 +32,8 @@ def createRecoConfigFlags():
     flags.addFlag("Reco.EnableTrigger", lambda prevFlags: prevFlags.Input.Format == "BS")
     # enable automatically for HI data
     flags.addFlag("Reco.EnableHI", lambda prevFlags: "_hi" in prevFlags.Input.ProjectName)
+    # common thinning and other post-processing
+    flags.addFlag("Reco.EnablePostProcessing", True)
 
     return flags
 
@@ -35,7 +43,7 @@ def recoRunArgsToFlags(runArgs, flags):
         flags.Input.RunNumber = runArgs.RunNumber
         flags.Input.OverrideRunNumber = True
 
-    if hasattr(runArgs, "projectName"): 
+    if hasattr(runArgs, "projectName"):
         flags.Input.projectName = runArgs.projectName
 
     # TODO: not handled yet
