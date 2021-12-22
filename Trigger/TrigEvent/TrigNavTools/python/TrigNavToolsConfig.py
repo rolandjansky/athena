5a95f8ae84cb7a5fbba7a58e0599a5365f7dc58f
Tomasz Bold <tomasz.bold@gmail.com>
2021-12-22 11:13:57 +0100

    Restructure to be able to configure in new JO way

--- Trigger/TrigEvent/TrigNavTools/python/TrigNavToolsConfig.py
+++ Trigger/TrigEvent/TrigNavTools/python/TrigNavToolsConfig.py
@@ -1,4 +1,15 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+
+from AthenaConfiguration.ComponentFactory import CompFactory
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+def _actions(mode):
+  options =   {'drop':    [ 'Drop' ],
+    'slimming': [  'DropFeatures', 'Squeeze', 'Reload', 'SyncThinning', 'DropChains', 'Save', 'Restore'],
+    'trigger':  [ 'DropFeatures', 'Reload', 'SyncThinning', 'DropChains', 'Save', 'Restore'],
+    'cleanup':  [ 'DropFeatures', 'Reload', 'SyncThinning', 'Save'],
+    'cleanup_noreload':    [ 'DropFeatures', 'SyncThinning', 'Save']
+    }
+  return options[mode]
 
 
 def navigationThinningSvc (config):
@@ -12,18 +23,7 @@ def navigationThinningSvc (config):
     svc.ChainsRegex = config['chains']
   if 'features' in config:
     svc.FeatureInclusionList=config['features']  
-
-  if config['mode'] == 'drop':    
-    svc.Actions = [ 'Drop' ]
-  if config['mode'] == 'slimming':    
-    svc.Actions = [  'DropFeatures', 'Squeeze', 'Reload', 'SyncThinning', 'DropChains', 'Save', 'Restore']
-  if config['mode'] == 'trigger':    
-    svc.Actions = [ 'DropFeatures', 'Reload', 'SyncThinning', 'DropChains', 'Save', 'Restore']
-  if config['mode'] == 'cleanup':    
-    svc.Actions = [ 'DropFeatures', 'Reload', 'SyncThinning', 'Save']
-  if config['mode'] == 'cleanup_noreload':    
-    svc.Actions = [ 'DropFeatures', 'SyncThinning', 'Save']
-
+  svc.Actions = _actions(config['mode'])
   if 'Print' in svc.Actions:
     from AthenaCommon.Constants import DEBUG
     svc.OutputLevel=DEBUG
@@ -31,3 +31,21 @@ def navigationThinningSvc (config):
   from AthenaCommon.AppMgr import ServiceMgr
   ServiceMgr += svc
   return svc
+
+
+def TrigNavigationThinningSvcCfg(flags, thinningConfig):
+  assert 'name' in thinningConfig, 'name of the configuration is missing'
+  assert 'mode' in thinningConfig, 'mode of slimming has to be configured'
+
+  acc = ComponentAccumulator()
+  svc = CompFactory.TrigNavigationThinningSvc(thinningConfig['name']+'ThinSvc', 
+          Actions = _actions(thinningConfig['mode']) )
+  if 'chains' in thinningConfig:
+    svc.ChainsRegex = thinningConfig['chains']
+  if 'features' in thinningConfig:
+    svc.FeatureInclusionList = thinningConfig['features']
+  if 'Print' in svc.Actions:
+    from AthenaCommon.Constants import DEBUG
+    svc.OutputLevel=DEBUG
+  acc.addService(svc, primary=True)
+  return acc
