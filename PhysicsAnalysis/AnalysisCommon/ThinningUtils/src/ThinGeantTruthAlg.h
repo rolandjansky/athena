611896fa62d9bc9d77d713a5705751ea47411405
christos <christos@cern.ch>
2021-12-24 02:48:02 +0100

    ThinGeantTruth make re-entrant and tidy up

--- PhysicsAnalysis/AnalysisCommon/ThinningUtils/src/ThinGeantTruthAlg.h
+++ PhysicsAnalysis/AnalysisCommon/ThinningUtils/src/ThinGeantTruthAlg.h
@@ -4,7 +4,6 @@
   Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 */
 
-
 #ifndef THINNINGUTILS_ThinGeantTruthAlg_H
 #define THINNINGUTILS_ThinGeantTruthAlg_H 1
 
@@ -12,107 +11,103 @@
  @class ThinGeantTruthAlg
 */
 
-
 // STL includes
+#include <atomic>
 #include <string>
 
 // FrameWork includes
-#include "GaudiKernel/ToolHandle.h"
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
 #include "GaudiKernel/ServiceHandle.h"
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "GaudiKernel/ToolHandle.h"
 #include "StoreGate/ThinningHandleKey.h"
-#include "xAODTruth/TruthParticleContainer.h"
-#include "xAODTruth/TruthVertexContainer.h"
-#include "xAODMuon/MuonContainer.h"
 #include "xAODEgamma/ElectronContainer.h"
 #include "xAODEgamma/PhotonContainer.h"
-#include "xAODEgamma/PhotonContainer.h"
-#
+#include "xAODMuon/MuonContainer.h"
+#include "xAODTruth/TruthParticleContainer.h"
+#include "xAODTruth/TruthVertexContainer.h"
 
-class ThinGeantTruthAlg
-  : public ::AthAlgorithm
+class ThinGeantTruthAlg : public AthReentrantAlgorithm
 {
 public:
-  
-    /// Constructor with parameters:
-    ThinGeantTruthAlg( const std::string& name, ISvcLocator* pSvcLocator );
-    
-    /// Destructor:
-    virtual ~ThinGeantTruthAlg();
-    
-    /// Athena algorithm's initalize hook
-    virtual StatusCode  initialize();
-    
-    /// Athena algorithm's execute hook
-    virtual StatusCode  execute();
-    
-    /// Athena algorithm's finalize hook
-    virtual StatusCode  finalize();
-    
-    /// Inline method
-    void ancestors(const xAOD::TruthParticle*,
-                   std::vector<bool> &,
-                   std::unordered_set<int> & );
-    void descendants(const xAOD::TruthParticle*,
-                     std::vector<bool> &,
-                     std::unordered_set<int> & );   
-    bool isStatus1BSMParticle(const xAOD::TruthParticle*) const;
+  using AthReentrantAlgorithm::AthReentrantAlgorithm;
+  virtual StatusCode initialize() override;
+  virtual StatusCode finalize() override;
+  virtual StatusCode execute(const EventContext& ctx) const override final;
 
- 
+  /// Inline method
+  void ancestors(const xAOD::TruthParticle*,
+                 std::vector<bool>&,
+                 std::unordered_set<int>&) const;
+  void descendants(const xAOD::TruthParticle*,
+                   std::vector<bool>&,
+                   std::unordered_set<int>&) const;
+  static bool isStatus1BSMParticle(const xAOD::TruthParticle*) ;
+
 private:
+  StringProperty m_streamName{ this,
+                               "StreamName",
+                               "",
+                               "Stream for which thinning is to be done." };
 
-    /// Should the thinning run?
-    bool m_doThinning;
-   
-    /// Geant barcode
-    int m_geantOffset; 
- 
-    /// Geant-decayed longer lived particles
-    std::vector<int> m_longlived;
+  Gaudi::Property<int> m_geantOffset{ this,
+                                      "GeantBarcodeOffset",
+                                      200000,
+                                      "Barcode offset for Geant particles" };
+  Gaudi::Property<bool> m_keepMuons{ this, "keepMuons", true };
+  Gaudi::Property<bool> m_keepEGamma{ this, "keepEGamma", true };
 
-    StringProperty m_streamName
-    { this, "StreamName", "", "Stream for which thinning is to be done." };
+  Gaudi::Property<std::vector<int>> m_longlived{
+    this,
+    "LongLivedParticleList",
+    { 310, 3122, 3222, 3112, 3322, 3312 },
+    "List of long lifetime particles which are likely to be decayed by "
+    "Geant but whose children must be kept"
+  };
 
-    SG::ThinningHandleKey<xAOD::TruthParticleContainer>  m_truthParticlesKey {this, 
-        "TruthParticlesKey", 
-        "TruthParticles", 
-        "Name of the input Truth Particle container"};
+  SG::ThinningHandleKey<xAOD::TruthParticleContainer> m_truthParticlesKey{
+    this,
+    "TruthParticlesKey",
+    "TruthParticles",
+    "Name of the input Truth Particle container"
+  };
 
+  SG::ThinningHandleKey<xAOD::TruthVertexContainer> m_truthVerticesKey{
+    this,
+    "TruthVerticesKey",
+    "TruthVertices",
+    "Name of the input Truth Vertices container"
+  };
 
-    SG::ThinningHandleKey<xAOD::TruthVertexContainer> m_truthVerticesKey{this, 
-        "TruthVerticesKey", 
-        "TruthVertices", 
-        "Name of the input Truth Vertices container"};
+  SG::ReadHandleKey<xAOD::ElectronContainer> m_electronsKey{
+    this,
+    "ElectronsKey",
+    "Electrons",
+    "Name of the input electron container"
+  };
 
+  SG::ReadHandleKey<xAOD::PhotonContainer> m_photonsKey{
+    this,
+    "PhotonsKey",
+    "Photons",
+    "Name of the input photon container"
+  };
 
-    SG::ReadHandleKey<xAOD::ElectronContainer> m_electronsKey {this, 
-        "ElectronsKey", 
-        "Electrons", 
-        "Name of the input electron container"};
+  SG::ReadHandleKey<xAOD::MuonContainer>
+    m_muonsKey{ this, "MuonsKey", "Muons", "Name of the input muon container" };
 
-    SG::ReadHandleKey<xAOD::PhotonContainer> m_photonsKey {this, 
-        "PhotonsKey", 
-        "Photons", 
-        "Name of the input photon container"};
+  SG::ReadHandleKey<xAOD::TruthParticleContainer> m_egammaTruthKey{
+    this,
+    "EGammaTruthKey",
+    "egammaTruthParticles",
+    "Name of the input egammaTruth container"
+  };
 
-    SG::ReadHandleKey<xAOD::MuonContainer> m_muonsKey {this, 
-        "MuonsKey", 
-        "Muons", 
-        "Name of the input muon container"};
-
-    SG::ReadHandleKey<xAOD::TruthParticleContainer> m_egammaTruthKey {this, 
-        "EGammaTruthKey", 
-        "egammaTruthParticles", 
-        "Name of the input egammaTruth container"};
-    
-    /// Counters
-    unsigned long m_nEventsProcessed;
-    unsigned long m_nParticlesProcessed;
-    unsigned long m_nVerticesProcessed;
-    unsigned long m_nParticlesThinned;
-    unsigned long m_nVerticesThinned;
-
+  /// Counters
+  mutable std::atomic<unsigned long> m_nEventsProcessed{};
+  mutable std::atomic<unsigned long> m_nParticlesProcessed{};
+  mutable std::atomic<unsigned long> m_nVerticesProcessed{};
+  mutable std::atomic<unsigned long> m_nParticlesThinned{};
+  mutable std::atomic<unsigned long> m_nVerticesThinned{};
 };
-
 
 #endif //> !THINNINGUTILS_ThinGeantTruthAlg_H
