4b1c4b35d5b20d1e493b757b4a22a72e808c772b
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2022-01-06 14:03:54 +0100

    Add 2 news tests: q431 and q221 reconstruction in MT with subsequent DAOD_PHYS/LITE in MP and checkxAOD checks

---
+++ Tools/TrfTestsART/test/test_trf_q221_phys_physlite_mt_mp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# art-description: Reco_tf.py q221, HITtoRDO/RDOtoRDOTrigger/RAWtoALL in MT and AODtoDAOD in MP
+# art-type: grid
+# art-include: master/Athena
+# art-athena-mt: 8
+
+export ATHENA_CORE_NUMBER=8
+Reco_tf.py \
+  --AMI q221 \
+  --conditionsTag 'all:OFLCOND-MC16-SDR-RUN2-09' \
+  --sharedWriter True \
+  --steering 'doRDO_TRIG' 'doTRIGtoALL' \
+  --outputDAODFile art.pool.root \
+  --reductionConf PHYS PHYSLITE \
+  --athenaopts "HITtoRDO:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "RDOtoRDOTrigger:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "RAWtoALL:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "AODtoDAOD:--threads=0 --nprocs=${ATHENA_CORE_NUMBER}" \
+  --postExec "from AthenaAuditors.AthenaAuditorsConf import FPEAuditor;FPEAuditor.NStacktracesOnFPE=10" \
+  --maxEvents 1000
+
+rc1=$?
+echo "art-result: ${rc1} Reco_tf_q221_phys_physlite_mt_mp" 
+
+# Check for FPEs in the logiles
+test_trf_check_fpe.sh
+fpeStat=$?
+
+echo "art-result: ${fpeStat} FPEs in logfiles"
+
+echo "============ checkxAOD DAOD_PHYS.art.pool.root"
+checkxAOD DAOD_PHYS.art.pool.root
+echo "============ checkxAOD DAOD_PHYSLITE.art.pool.root"
+checkxAOD DAOD_PHYSLITE.art.pool.root
+rc2=$?
+echo "art-result: ${rc2} checkxAOD" 
