5bc884ce6d445189523c4809d26ba532b0548066
Nils Krumnack <krumnack@iastate.edu>
2022-01-13 09:46:54 -0600

    split CP algorithm sequence configuration into three parts
    Essentially the idea here is that this makes it easier to build
    additional configuration layers on top, since it makes this more
    composable.  In particular it means that multiple working points can
    be added to a single sequence without having to do some complicated
    special handling to deduplicate the calibration part of the sequence.
    I also purposely pass in the actual sequence I operate on instead of
    creating them internally, which (hopefully) gives the caller some more
    freedom as to what that sequence may look like.
    
    I didn't try to do anything particularly clever in splitting the
    sequences up, and some cleanup could be sensible at some point.
    Basically I split the sequences into three parts:
    * calibration: any algorithms that can be shared between multiple
      working points
    * working point: any algorithms that do things specific to a
      a working point that can't be shared between object types
    * post processing: any algorithms that can be shared between multiple
      object types (essentially container copies and debugging histograms)
    
    There is one algorithm that could go into either the working point or
    the post-processing, and I chose working point: And that's the
    algorithm that collects all the decorations for that working point and
    makes an overall selection decoration for that working point.  My
    reasoning here being that while that is a completely generic algorithm
    it is still something we will always want to do and that the working
    point sequence will feel incomplete if we leave it out (unlike the
    post processing which the user could easily do his own way).  If we
    find that is not the case we could move this around later on (or make
    it optional).
    
    Also, I found that at least in some places we have two types of
    selections: One for the analysis object selection and one for the
    output object selection.  For simplicity/consistency I just forced
    that on all sequences, but it may be worth revisiting at some point.
    
    I also had to do some amount of reordering to fit into this overall
    scheme.  Most notably the e-gamma sequences did one of their WP specific
    selections before running calibrations.  I flipped the order (at least
    for now), as this is essentially a performance optimization (skipping
    calibration for some objects) and we can bring it back later if we
    decide that such an optimization is worth the headache it brings with it.
    
    I also updated the photon sequence no longer to use view-copies
    internally, but instead rely on preselection decorations.  Since that
    is the way we said all sequences ought to have been working already
    that seemed like a sensible change.
    
    I also put the preselection configuration into the tau sequences.
    Technically that is not used, but it allows pre-loading preselection
    algorithms.
    
    I'm not sure how sensible my updates to the di-tau sequence are, but
    the original sequence was neither functional nor complete.  The unit
    tests for that sequence were already commented out, and the sequence
    itself doesn't even contain a selection algorithm.  Still I tried my
    best to update it, to reduce future work.
    
    I did leave the original top-level function around, calling the new
    functions as they ought to be called.  That is mostly ment for
    backward compatibility and it ought to be fine to call the new
    functions directly as a user (though they may see more updates to
    calling conventions than the top-level function).

--- PhysicsAnalysis/Algorithms/TauAnalysisAlgorithms/python/DiTauAnalysisSequence.py
+++ PhysicsAnalysis/Algorithms/TauAnalysisAlgorithms/python/DiTauAnalysisSequence.py
@@ -1,11 +1,16 @@
 # Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
 
 # AnaAlgorithm import(s):
+from AsgAnalysisAlgorithms.AnalysisObjectSharedSequence import makeSharedObjectSequence
 from AnaAlgorithm.AnaAlgSequence import AnaAlgSequence
 from AnaAlgorithm.DualUseConfig import createAlgorithm, addPrivateTool
 
-def makeDiTauAnalysisSequence( dataType, workingPoint,
-                             deepCopyOutput = False, postfix = '' ):
+def makeDiTauAnalysisSequence( dataType, workingPoint, postfix = '',
+                             deepCopyOutput = False,
+                             shallowViewOutput = True,
+                             rerunTruthMatching = True,
+                             enableCutflow = False,
+                             enableKinematicHistograms = False ):
     """Create a tau analysis algorithm sequence
 
     Keyword arguments:
@@ -13,10 +18,14 @@ def makeDiTauAnalysisSequence( dataType, workingPoint,
       deepCopyOutput -- If set to 'True', the output containers will be
                         standalone, deep copies (slower, but needed for xAOD
                         output writing)
+      shallowViewOutput -- Create a view container if required
       postfix -- a postfix to apply to decorations and algorithm
                  names.  this is mostly used/needed when using this
                  sequence with multiple working points to ensure all
                  names are unique.
+      rerunTruthMatching -- Whether or not to rerun truth matching
+      enableCutflow -- Whether or not to dump the cutflow
+      enableKinematicHistograms -- Whether or not to dump the kinematic histograms
     """
 
     if dataType not in ["data", "mc", "afii"] :
@@ -25,6 +34,94 @@ def makeDiTauAnalysisSequence( dataType, workingPoint,
     if postfix != '' :
         postfix = '_' + postfix
         pass
+
+    # Make sure selection options make sense
+    if deepCopyOutput and shallowViewOutput:
+        raise ValueError ("deepCopyOutput and shallowViewOutput can't both be true!")
+
+    # Create the analysis algorithm sequence object:
+    seq = AnaAlgSequence( "DiTauAnalysisSequence" + postfix )
+
+    seq.addMetaConfigDefault ("selectionDecorNames", [])
+    seq.addMetaConfigDefault ("selectionDecorNamesOutput", [])
+    seq.addMetaConfigDefault ("selectionDecorCount", [])
+
+    makeDiTauCalibrationSequence (seq, dataType, postfix=postfix,
+                                rerunTruthMatching = rerunTruthMatching)
+    makeDiTauWorkingPointSequence (seq, dataType, workingPoint, postfix=postfix)
+    makeSharedObjectSequence (seq, deepCopyOutput = deepCopyOutput,
+                              shallowViewOutput = shallowViewOutput,
+                              postfix = '_DiTau' + postfix,
+                              enableCutflow = enableCutflow,
+                              enableKinematicHistograms = enableKinematicHistograms )
+
+    # Return the sequence:
+    return seq
+
+
+
+
+def makeDiTauCalibrationSequence( seq, dataType,
+                                 postfix = '',
+                             rerunTruthMatching = True):
+    """Create tau calibration analysis algorithms
+
+    This makes all the algorithms that need to be run first befor
+    all working point specific algorithms and that can be shared
+    between the working points.
+
+    Keyword arguments:
+      dataType -- The data type to run on ("data", "mc" or "afii")
+      postfix -- a postfix to apply to decorations and algorithm
+                 names.  this is mostly used/needed when using this
+                 sequence with multiple working points to ensure all
+                 names are unique.
+      rerunTruthMatching -- Whether or not to rerun truth matching
+    """
+
+    if dataType not in ["data", "mc", "afii"] :
+        raise ValueError ("invalid data type: " + dataType)
+
+    # Set up the tau 4-momentum smearing algorithm:
+    alg = createAlgorithm( 'CP::DiTauSmearingAlg', 'DiTauSmearingAlg' + postfix )
+    addPrivateTool( alg, 'smearingTool', 'TauAnalysisTools::DiTauSmearingTool' )
+    seq.append( alg, inputPropName = 'taus', outputPropName = 'tausOut',
+                stageName = 'calibration',
+                dynConfig = {'preselection' : lambda meta : "&&".join (meta["selectionDecorNames"])} )
+
+    # Set up the tau truth matching algorithm:
+    if dataType != 'data':
+        alg = createAlgorithm( 'CP::DiTauTruthMatchingAlg',
+                               'DiTauTruthMatchingAlg' + postfix )
+        addPrivateTool( alg, 'matchingTool',
+                        'TauAnalysisTools::DiTauTruthMatchingTool' )
+        alg.matchingTool.WriteTruthTaus = 1
+        seq.append( alg, inputPropName = 'taus',
+                    stageName = 'selection',
+                    dynConfig = {'preselection' : lambda meta : "&&".join (meta["selectionDecorNames"])} )
+        pass
+
+    pass
+
+
+
+
+
+def makeDiTauWorkingPointSequence( seq, dataType, workingPoint, postfix = '',
+                             legacyRecommendations = False):
+    """Create tau analysis algorithms for a single working point
+
+    Keyword arguments:
+      dataType -- The data type to run on ("data", "mc" or "afii")
+      legacyRecommendations -- use legacy tau BDT and electron veto recommendations
+      postfix -- a postfix to apply to decorations and algorithm
+                 names.  this is mostly used/needed when using this
+                 sequence with multiple working points to ensure all
+                 names are unique.
+    """
+
+    if dataType not in ["data", "mc", "afii"] :
+        raise ValueError ("invalid data type: " + dataType)
 
     splitWP = workingPoint.split ('.')
     if len (splitWP) != 1 :
@@ -46,21 +143,6 @@ def makeDiTauAnalysisSequence( dataType, workingPoint,
                           "\", allowed values are Tight, Medium, Loose, " +
                           "VeryLoose")
 
-    # Create the analysis algorithm sequence object:
-    seq = AnaAlgSequence( "DiTauAnalysisSequence" + postfix )
-
-    # Set up the tau 4-momentum smearing algorithm:
-    alg = createAlgorithm( 'CP::DiTauSmearingAlg', 'DiTauSmearingAlg' + postfix )
-    addPrivateTool( alg, 'smearingTool', 'TauAnalysisTools::DiTauSmearingTool' )
-    seq.append( alg, inputPropName = 'taus', outputPropName = 'tausOut',
-                stageName = 'calibration' )
-
-    # Set up an algorithm dumping the properties of the taus, for debugging:
-    alg = createAlgorithm( 'CP::KinematicHistAlg', 'DiTauKinematicDumperAlg' + postfix )
-    alg.histPattern = "tau_%VAR%_%SYS%"
-    seq.append( alg, inputPropName = 'input',
-                stageName = 'selection' )
-
     # Set up the algorithm calculating the efficiency scale factors for the
     # taus:
     alg = createAlgorithm( 'CP::DiTauEfficiencyCorrectionsAlg',
@@ -72,27 +154,7 @@ def makeDiTauAnalysisSequence( dataType, workingPoint,
     # alg.outOfValidity = 2 #silent
     # alg.outOfValidityDeco = "bad_eff"
     seq.append( alg, inputPropName = 'taus',
-                stageName = 'efficiency' )
+                stageName = 'efficiency',
+                dynConfig = {'preselection' : lambda meta : "&&".join (meta["selectionDecorNames"])} )
 
-    # Set up the tau truth matching algorithm:
-    if dataType != 'data':
-        alg = createAlgorithm( 'CP::DiTauTruthMatchingAlg',
-                               'DiTauTruthMatchingAlg' + postfix )
-        addPrivateTool( alg, 'matchingTool',
-                        'TauAnalysisTools::DiTauTruthMatchingTool' )
-        alg.matchingTool.WriteTruthTaus = 1
-        seq.append( alg, inputPropName = 'taus',
-                    stageName = 'selection' )
-        pass
-
-    # Set up a final deep copy making algorithm if requested:
-    if deepCopyOutput:
-        alg = createAlgorithm( 'CP::AsgViewFromSelectionAlg',
-                               'DiTauDeepCopyMaker' + postfix )
-        alg.deepCopy = True
-        seq.append( alg, inputPropName = 'input', outputPropName = 'output',
-                    stageName = 'selection' )
-        pass
-
-    # Return the sequence:
-    return seq
+    pass
