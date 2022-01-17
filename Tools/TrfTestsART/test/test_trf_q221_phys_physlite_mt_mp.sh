fdc10c185a10bb04b1bfc5b894bb5bfaf3fcd77c
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2022-01-17 08:31:20 +0100

    Add ESD merging test in MT and increase DAOD_PHYS/LITE tests to all events

--- Tools/TrfTestsART/test/test_trf_q221_phys_physlite_mt_mp.sh
+++ Tools/TrfTestsART/test/test_trf_q221_phys_physlite_mt_mp.sh
@@ -15,7 +15,7 @@ Reco_tf.py \
   --reductionConf PHYS PHYSLITE \
   --athenaopts "HITtoRDO:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "RDOtoRDOTrigger:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "RAWtoALL:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "AODtoDAOD:--threads=0 --nprocs=${ATHENA_CORE_NUMBER}" \
   --postExec "from AthenaAuditors.AthenaAuditorsConf import FPEAuditor;FPEAuditor.NStacktracesOnFPE=10" \
-  --maxEvents 1000
+  --maxEvents -1
 
 rc1=$?
 echo "art-result: ${rc1} Reco_tf_q221_phys_physlite_mt_mp" 
