cba02fcc4c7236f1f926d6c41d7d6a418f2a20a3
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-01-18 13:55:11 +0100

    Revert "DecisionObjectHandle: remove DataHandle inheritance"
    This reverts commit 9c3ced8e71b2434607427f663bb227cfa016643c.
    
    Fixes performance degradation reported in ATR-24792.

--- Trigger/TrigAnalysis/TrigDecisionTool/src/DecisionObjectHandleAthena.cxx
+++ Trigger/TrigAnalysis/TrigDecisionTool/src/DecisionObjectHandleAthena.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef XAOD_ANALYSIS // Full Athena only
@@ -10,30 +10,35 @@
 using namespace Trig;
 
 DecisionObjectHandleAthena::DecisionObjectHandleAthena( SG::ReadHandleKey<TrigDec::TrigDecision>* olddeckey )
-  : m_oldDecKey(olddeckey)
+  : m_oldDecKey(olddeckey),
+    m_object(nullptr)
 {
 }
 
 TrigDec::TrigDecision const * DecisionObjectHandleAthena::getDecision() const {
   
-  if ( m_oldDecKey->empty() ) return nullptr;
-
-  const EventContext& ctx = Gaudi::Hive::currentContext();
-  SG::ReadHandle<TrigDec::TrigDecision> oldDecisionReadHandle = SG::makeHandle(*m_oldDecKey, ctx);
-  if( ! oldDecisionReadHandle.isValid() ) {
-    [[maybe_unused]] static std::atomic<bool> warningPrinted =
-      [&]() { ATH_MSG_WARNING( "TrigDec::TrigDecision is not available on the input" );
-      return true; }();
-    return nullptr;
+  if ( !m_object && !m_oldDecKey->empty() ) {
+    const EventContext& ctx = Gaudi::Hive::currentContext();
+    SG::ReadHandle<TrigDec::TrigDecision> oldDecisionReadHandle = SG::makeHandle(*m_oldDecKey, ctx);
+    if( ! oldDecisionReadHandle.isValid() ) {
+      [[maybe_unused]] static std::atomic<bool> warningPrinted =
+        [&]() { ATH_MSG_WARNING( "TrigDec::TrigDecision is not available on the input" );
+                return true; }();
+      return nullptr;
+    }
+    m_object = oldDecisionReadHandle.ptr(); 
   }
-  return oldDecisionReadHandle.ptr();
+  
+  return m_object;
 }
-
 TrigDec::TrigDecision const * DecisionObjectHandleAthena::getNavigation() const {
   return getDecision();
 }
 
-void DecisionObjectHandleAthena::reset (bool) {
+
+void DecisionObjectHandleAthena::reset (bool hard) {
+  DataHandle<TrigDec::TrigDecision>::reset (hard);
+  m_object = nullptr;
   invalidate();
   ATH_MSG_DEBUG("invalidated decision object");
 }
