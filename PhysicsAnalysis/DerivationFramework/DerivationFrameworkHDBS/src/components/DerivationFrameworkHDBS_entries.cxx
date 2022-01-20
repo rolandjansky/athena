deca46155d6ce30e9c9f8334bc943f0115d2e2ff
Mihaela Marinescu <mihaela.marinescu@cern.ch>
2022-01-20 16:06:59 +0000

    compilation error fixes

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkHDBS/src/components/DerivationFrameworkHDBS_entries.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkHDBS/src/components/DerivationFrameworkHDBS_entries.cxx
@@ -1,5 +1,4 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
-<<<<<<< PhysicsAnalysis/DerivationFramework/DerivationFrameworkHDBS/src/components/DerivationFrameworkHDBS_entries.cxx
 #include "DerivationFrameworkHDBS/SkimmingToolHDBS2.h"
 #include "DerivationFrameworkHDBS/SkimmingToolHDBS3.h"
 #include "DerivationFrameworkHDBS/HDBSa0Finder.h"
@@ -14,17 +13,4 @@ DECLARE_FACTORY_ENTRIES( DerivationFrameworkHDBS ) {
    DECLARE_TOOL( SkimmingToolHDBS2 )
    DECLARE_TOOL( SkimmingToolHDBS3 )
    DECLARE_TOOL( HDBSa0Finder )
-=======
-#include "DerivationFrameworkHDBS/SkimmingToolHDBS2.h"
-#include "DerivationFrameworkHDBS/SkimmingToolHDBS3.h"
-
-using namespace DerivationFramework;
-
-DECLARE_TOOL_FACTORY( SkimmingToolHDBS2 )
-DECLARE_TOOL_FACTORY( SkimmingToolHDBS3 )
-
-DECLARE_FACTORY_ENTRIES( DerivationFrameworkHDBS ) {
-   DECLARE_TOOL( SkimmingToolHDBS2 )
-   DECLARE_TOOL( SkimmingToolHDBS3 )
->>>>>>> PhysicsAnalysis/DerivationFramework/DerivationFrameworkHDBS/src/components/DerivationFrameworkHDBS_entries.cxx
 }
