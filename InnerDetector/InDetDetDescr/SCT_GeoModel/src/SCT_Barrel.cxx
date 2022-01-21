2b085c1f8813a5db0c53fd8f2fd40b1abd62783e
scott snyder <snyder@bnl.gov>
2022-01-21 04:35:54 +0100

    copyright

--- InnerDetector/InDetDetDescr/SCT_GeoModel/src/SCT_Barrel.cxx
+++ InnerDetector/InDetDetDescr/SCT_GeoModel/src/SCT_Barrel.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "SCT_GeoModel/SCT_Barrel.h"
