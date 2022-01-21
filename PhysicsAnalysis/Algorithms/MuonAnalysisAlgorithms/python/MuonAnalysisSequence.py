6ba6c01d05f40e19e3151c40b18582bdca6f82af
Haider Abidi <syed.haider.abidi@cern.ch>
2022-01-21 14:37:52 +0100

    Explicit calibration setting for Muon Calibration Algo

--- PhysicsAnalysis/Algorithms/MuonAnalysisAlgorithms/python/MuonAnalysisSequence.py
+++ PhysicsAnalysis/Algorithms/MuonAnalysisAlgorithms/python/MuonAnalysisSequence.py
@@ -114,6 +114,7 @@ def makeMuonAnalysisSequence( dataType, workingPoint,
                            'MuonCalibrationAndSmearingAlg' + postfix )
     addPrivateTool( alg, 'calibrationAndSmearingTool',
                     'CP::MuonCalibrationPeriodTool' )
+    alg.calibrationAndSmearingTool.calibrationMode = 2 # choose ID+MS with no sagitta bias
     seq.append( alg, inputPropName = 'muons', outputPropName = 'muonsOut',
                 stageName = 'calibration',
                 dynConfig = {'preselection' : lambda meta : "&&".join (meta["selectionDecorNames"])})
