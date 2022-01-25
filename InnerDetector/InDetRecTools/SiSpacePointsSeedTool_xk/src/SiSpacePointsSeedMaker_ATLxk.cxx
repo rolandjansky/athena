05d9046b163ed34655ed31ae4ca13590349832e8
Scott Snyder <scott.snyder@cern.ch>
2022-01-25 10:52:11 +0100

    SiSpacePointsSeedTool_xk: Update for thread-safety checker const changes.

--- InnerDetector/InDetRecTools/SiSpacePointsSeedTool_xk/src/SiSpacePointsSeedMaker_ATLxk.cxx
+++ InnerDetector/InDetRecTools/SiSpacePointsSeedTool_xk/src/SiSpacePointsSeedMaker_ATLxk.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
      
@@ -20,6 +20,7 @@
 #include "TrkParameters/TrackParameters.h"
 
 #include "StoreGate/ReadCondHandle.h"
+#include "CxxUtils/checker_macros.h"
 #include <iomanip>
 #include <ostream>
 
@@ -2561,7 +2562,9 @@ void InDet::SiSpacePointsSeedMaker_ATLxk::writeNtuple(const SiSpacePointsSeed* s
     m_givesTrack   =   !(track == nullptr);
     m_eventNumber  =   eventNumber;
 
-    m_outputTree->Fill();
+    // Ok: protected by mutex.
+    TTree* outputTree ATLAS_THREAD_SAFE = m_outputTree;
+    outputTree->Fill();
 
   }
 
