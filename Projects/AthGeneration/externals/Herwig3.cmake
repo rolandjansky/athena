c49cae5e6fbdbad363616bfa5b6d54fb29661c46
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-01-25 12:52:31 +0100

    Herwig7.2.2, ThePEG 2.2.2, VBFNLO 3.0.0beta5, TheP8I 2.0.2thepeg2.2.1p3

--- Projects/AthGeneration/externals/Herwig3.cmake
+++ Projects/AthGeneration/externals/Herwig3.cmake
@@ -2,6 +2,6 @@
 # File specifying the location of Herwig3 to use.
 #
 
-set( HERWIG3_LCGVERSION 7.1.6p3 )
+set( HERWIG3_LCGVERSION 7.2.2 )
 set( HERWIG3_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/herwig++/${HERWIG3_LCGVERSION}/${LCG_PLATFORM} )
--- Projects/AthGeneration/externals/ThePEG.cmake
+++ Projects/AthGeneration/externals/ThePEG.cmake
@@ -3,6 +3,6 @@
 # File specifying the location of ThePEG to use.
 #
 
-set( THEPEG_LCGVERSION 2.1.6p3 )
+set( THEPEG_LCGVERSION 2.2.2 )
 set( THEPEG_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/thepeg/${THEPEG_LCGVERSION}/${LCG_PLATFORM} )
