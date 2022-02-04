2e60dc15c67486341b4b2a22912bc39457eb967c
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2022-02-04 18:08:45 +0100

    MuonCondTest - Use identifier hash for RE access & clean-up

--- MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondTest/MuonCondTest/MuonConditionsHistoSummary.h
+++ MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondTest/MuonCondTest/MuonConditionsHistoSummary.h
@@ -1,73 +1,64 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef MuonConditionsHistoSummary_H
-#define MuonConditionsHistoSummary_H 
-//STL
+#define MuonConditionsHistoSummary_H
+// STL
 #include <string>
 
-//Gaudi
+// Gaudi
 #include "AthenaBaseComps/AthAlgorithm.h"
-#include "GaudiKernel/ServiceHandle.h"
 #include "GaudiKernel/ITHistSvc.h"
-//Athena
+#include "GaudiKernel/ServiceHandle.h"
+// Athena
 #include "TH1.h"
 #include "TTree.h"
- 
-//Athena
+
+// Athena
 
 #include "MuonCondInterface/IMDTConditionsSvc.h"
 #include "MuonCondInterface/IRPCConditionsSvc.h"
 #include "MuonCondInterface/ITGCConditionsSvc.h"
 
-//Forward declarations
+// Forward declarations
 class ISvcLocator;
 class StatusCode;
 
 class MuonConditionsHistoSummary : public AthAlgorithm {
- public:
-  MuonConditionsHistoSummary(const std::string &name,ISvcLocator *pSvcLocator) ;
- ~MuonConditionsHistoSummary();
+public:
+    MuonConditionsHistoSummary(const std::string &name, ISvcLocator *pSvcLocator);
+    ~MuonConditionsHistoSummary();
 
-  StatusCode initialize();
-  StatusCode execute();
-  StatusCode finalize();
-   
- private:
-  ServiceHandle<IMDTConditionsSvc> m_pSummarySvc;
-  ServiceHandle<IRPCConditionsSvc> m_rSummarySvc;
-  ServiceHandle<ITGCConditionsSvc> m_tSummarySvc; 
-  
-  ITHistSvc* m_thistSvc;
-//MDT stuff 
-std::string m_deadmultilayer;
-std::string m_deadtube;
-std::string m_deadstat;
+    StatusCode initialize() override;
+    StatusCode execute() override;
 
+private:
+    ServiceHandle<IMDTConditionsSvc> m_pSummarySvc;
+    ServiceHandle<IRPCConditionsSvc> m_rSummarySvc;
+    ServiceHandle<ITGCConditionsSvc> m_tSummarySvc;
 
- std::string m_deadstat_id;
- std::string m_deadmultilayer_id;
- std::string m_deadtube_id;
- 
-  
- TTree * m_t_deadstations;
-  TTree * m_t_deadmultilayer;
-  TTree * m_t_deadtube;
+    ITHistSvc *m_thistSvc;
+    // MDT stuff
+    std::string m_deadmultilayer;
+    std::string m_deadtube;
+    std::string m_deadstat;
 
- //RPC stuff 
- std::string m_panelid ;
- std::string m_stripid ;
- 
-  TTree * m_t_panelid;
-  TTree * m_t_stripid;
-  
-  
-  
-  
- 
- 
+    std::string m_deadstat_id;
+    std::string m_deadmultilayer_id;
+    std::string m_deadtube_id;
 
-}; //end of class
+    TTree *m_t_deadstations;
+    TTree *m_t_deadmultilayer;
+    TTree *m_t_deadtube;
+
+    // RPC stuff
+    std::string m_panelid;
+    std::string m_stripid;
+
+    TTree *m_t_panelid;
+    TTree *m_t_stripid;
+
+};  // end of class
 
 #endif
