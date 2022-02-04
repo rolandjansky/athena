8ef00d99fbd3f9569ad3d48cd8b1098773c99bbd
Cristiano Alpigiani <cristiano.alpigiani@cern.ch>
2022-02-04 11:04:16 +0100

    Updating derivation ART and TCT

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_mc20PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_mc20PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # art-include: master/Athena
-# art-description: DAOD building PHYS and PHYSLITE mc16 MP w/ SharedWriter w/o parallel compression
+# art-description: DAOD building PHYS and PHYSLITE mc20 MP w/ SharedWriter w/o parallel compression
 # art-type: grid
 # art-output: *.pool.root
 # art-output: checkFile*.txt
@@ -10,14 +10,14 @@
 # art-athena-mt: 8
 
 ATHENA_CORE_NUMBER=8 Reco_tf.py \
-  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.recon.AOD.e3601_s3126_r12885/* \
+  --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/mc20_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.recon.AOD.e3601_s3681_r13167/AOD.27312826._000061.pool.root.1 \
   --outputDAODFile art.pool.root \
   --reductionConf PHYS PHYSLITE \
   --maxEvents -1 \
   --sharedWriter True \
   --parallelCompression False \
   --multiprocess True \
-  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff(); DetFlags.digitize.all_setOff();'
+  --preExec 'from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.digitize.all_setOff(); DetFlags.detdescr.Calo_setOn(); DetFlags.simulate.all_setOff(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff();'
 
 echo "art-result: $? reco"
 
