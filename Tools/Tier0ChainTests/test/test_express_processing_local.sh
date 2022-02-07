281a15b95e3766f61de52f2e412db5089a209961
Peter Berta <peter.berta@cern.ch>
2022-02-07 09:05:02 +0100

    TCT: Adding two new TCT tests to run locally as discussed in ATR-24782

---
+++ Tools/Tier0ChainTests/test/test_express_processing_local.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# art-description: RecoTrf
+# art-type: local
+# art-include: master/Athena
+
+Reco_tf.py  \
+--AMI x608  \
+--inputBSFile="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/TCT_Run3/data21_comm.00404400.express_express.merge.RAW._lb2497._SFO-ALL._0001.1" \
+--maxEvents=100 \
+--outputHISTFile="HIST.root" \
+--outputAODFile="AOD.root" \
+--imf False
+
+rc1=$?
+echo "art-result: $rc1 Reco"
