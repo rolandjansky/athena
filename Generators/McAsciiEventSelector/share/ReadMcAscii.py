d8b68c46588b401d8915651c0a1357143b6c0961
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2022-02-07 11:11:34 +0100

    Dropped McAsciiEventSelector

--- Generators/McAsciiEventSelector/share/ReadMcAscii.py
+++
@@ -1,8 +0,0 @@
-## @file McAsciiEventSelector/ReadMcAscii.py
-##
-import McAsciiEventSelector.ReadMcAscii
-
-## backward compatibility hacks
-from AthenaCommon.AppMgr  import ServiceMgr as svcMgr
-EventSelector = svcMgr.EventSelector
-
