e55725b034bb8ad189cb9beb6c2b3fa359976e18
Peter Berta <peter.berta@cern.ch>
2022-02-09 16:50:59 +0100

    TCT: Implementing the fix from ATR-24782 also for local tests

--- Tools/Tier0ChainTests/test/test_bulk_processing_local.sh
+++ Tools/Tier0ChainTests/test/test_bulk_processing_local.sh
@@ -4,6 +4,10 @@
 # art-type: local
 # art-include: master/Athena
 
+# There was a database connection problem reported in ATR-24782. Rodney Walker's solution is to use the following export to fix the problem:
+export TNS_ADMIN=/cvmfs/atlas.cern.ch/repo/sw/database/DBRelease/current/oracle-admin
+
+
 Reco_tf.py  \
 --AMI f1199  \
 --inputBSFile="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/TCT_Run3/data21_comm.00404400.express_express.merge.RAW._lb2497._SFO-ALL._0001.1" \
