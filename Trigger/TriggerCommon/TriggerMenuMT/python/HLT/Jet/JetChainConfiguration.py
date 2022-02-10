a45243bef2cf90d5c600a0eadf00e92c1557cc83
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2022-02-10 16:14:56 +0100

    Refactor jet preselection configuration into new module/helper

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Jet/JetChainConfiguration.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Jet/JetChainConfiguration.py
@@ -4,20 +4,16 @@ from AthenaCommon.Logging import logging
 logging.getLogger().info("Importing %s",__name__)
 log = logging.getLogger(__name__)
 
+import re
+
 from AthenaConfiguration.AllConfigFlags import ConfigFlags
 
 from TriggerMenuMT.HLT.Menu.ChainConfigurationBase import ChainConfigurationBase
 from TriggerMenuMT.HLT.Menu.MenuComponents import ChainStep, RecoFragmentsPool
 from . import JetRecoCommon
+from . import JetPresel
 
 import copy
-
-def jetChainParts(chainParts):
-    jChainParts = []
-    for p in chainParts:
-        if p['trigType'] == 'j':
-            jChainParts.append(p)
-    return jChainParts
 
 #----------------------------------------------------------------
 # Class to configure chain
@@ -43,7 +39,7 @@ class JetChainConfiguration(ChainConfigurationBase):
         #self.chainPartNameNoMultwL1 = ''
 
         # expect that the L1 seed is the same for all jet parts, otherwise we have a problem
-        jChainParts = jetChainParts(self.chainPart)
+        jChainParts = JetRecoCommon.jetChainParts(self.chainPart)
         # Register if this is a performance chain, in which case the HLT should be exactly j0_perf
         self.isPerf = False
         # Exotic hypo (emerging-jets, trackless jets)
@@ -68,16 +64,14 @@ class JetChainConfiguration(ChainConfigurationBase):
             if self.L1Threshold != '' and self.L1Threshold != l1th:
                 raise RuntimeError('Cannot configure a jet chain with different L1 thresholds')
             self.L1Threshold = l1th
-            # We require that if there is any preselection it is only written
-            # in the last chainPart to avoid inconsistencies -- chain should
-            # look like HLT_jA_jB_jC_preselNjX_L1BLAH
-            if p["trkpresel"]!="nopresel":
-                if ip+1==len(jChainParts): # Last jet chainPart, presel should go here
-                    self.trkpresel=p["trkpresel"]
-                    self.trkpresel_parsed_reco = {key:p[key] for key in ['recoAlg']} #Storing here the reco options from last chain part that we want to propagate to preselection (e.g. jet radius)
-                else:
-                    log.error("Likely inconsistency encountered in preselection specification for %s",self.chainName)
-                    raise RuntimeError("Preselection %s specified earlier than in the last chainPart!",p["trkpresel"])
+
+            # Verify that the preselection is defined only once
+            if p["trkpresel"]!="nopresel" and ip+1!=len(jChainParts): # Last jet chainPart, presel should go here
+                log.error("Likely inconsistency encountered in preselection specification for %s",self.chainName)
+                raise RuntimeError("Preselection %s specified earlier than in the last chainPart!",p["trkpresel"])
+
+        self.trkpresel = JetPresel.extractPreselection(self.dict)
+        self.trkpresel_parsed_reco = {key:p[key] for key in ['recoAlg']} #Storing here the reco options from last chain part that we want to propagate to preselection (e.g. jet radius)
 
         self.recoDict = JetRecoCommon.extractRecoDict(jChainParts)
 
@@ -221,90 +215,14 @@ class JetChainConfiguration(ChainConfigurationBase):
 
     def getJetCaloPreselChainStep(self):
         #Find if a a4 or a10 calo jet needs to be used in the pre-selection from the last chain dict
-        import re
         assert 'recoAlg' in self.trkpresel_parsed_reco.keys(), "Impossible to find \'recoAlg\' key in last chain dictionary for preselection"
         #Want to match now only a4 and a10 in the original reco algorithm. We don't want to use a10sd or a10t in the preselection
         matched_reco = re.match(r'^a\d?\d?',self.trkpresel_parsed_reco['recoAlg'])
         assert matched_reco is not None, "Impossible to get matched reco algorithm for jet trigger preselection The reco expression {0} seems to be impossible to be parsed.".format(self.trkpresel_parsed_reco['recoAlg'])
 
-        # Define a fixed preselection dictionary for prototyping -- we may expand the options
-        preselRecoDict = {
-            'recoAlg':matched_reco.group(), #Getting the outcome of the regex reco option (it should correspond to a4 or a10 depending by which chain you are configuring)
-            'constitType':'tc',
-            'clusterCalib':'em',
-            'constitMod':'',
-            'trkopt':'notrk',
-            'ionopt':'noion',
-        }
-        ''' #Here you can set custom calibrations for large-R preselections. If you set to LCW you'll get an issue though, as the trigger expects the *same* topocluster collection to be used in the preselection and in the PFlow stage with tracking. Therefore this would need to be adapted, but it might not be so easy...
-         
-        if preselRecoDict['recoAlg']=='a10': #Setting LC calibrations for large-R jets
-            preselRecoDict['clusterCalib']='lcw'
-        '''
-        from .JetRecoCommon import getJetCalibDefaultString
-        preselRecoDict.update({'jetCalib':getJetCalibDefaultString(preselRecoDict) if preselRecoDict['recoAlg']=='a4' else 'nojcalib'}) #Adding default calibration for corresponding chain
-        from ..Menu.SignatureDicts import JetChainParts_Default
-        preselCommonJetParts = dict(JetChainParts_Default)
-        preselCommonJetParts.update(preselRecoDict)
+        #Getting the outcome of the regex reco option (it should correspond to a4 or a10 depending by which chain you are configuring)
+        preselRecoDict = JetPresel.getPreselRecoDict(matched_reco.group())
 
-        preselChainDict = dict(self.dict)
-        preselChainDict['chainParts']=[]
-
-        # Get from the last chainPart in order to avoid to specify preselection for every leg
-        #TODO: add protection for cases where the preselection is not specified in the last chainPart
-        presel_matched = re.match(r'presel(?P<cut>\d?\d?[jacf][\d\D]+)', self.trkpresel)
-        assert presel_matched is not None, "Impossible to match preselection pattern for self.trkpresel=\'{0}\'.".format(self.trkpresel)
-        presel_cut_str = presel_matched.groupdict()['cut'] #This is the cut string you want to parse. For example 'presel2j50XXj40'
-
-        for p in presel_cut_str.split('XX'):
-            matched = re.match(r'(?P<mult>\d?\d?)(?P<region>[jacf])(?P<cut>\d+)', p)
-            assert matched is not None, "Impossible to extract preselection cut for \'{0}\' substring. Please investigate.".format(p)
-            cut_dict = matched.groupdict()
-            mult,region,cut=cut_dict['mult'],cut_dict['region'],cut_dict['cut']
-            chainPartName=f'{mult}j{cut}'
-            if mult=='': mult='1'
-            etarange = {
-                "j":"0eta320", # default
-                "a":"0eta490",
-                "c":"0eta240",
-                "f":"320eta490"}[region]
-
-            tmpChainDict = dict(preselCommonJetParts) 
-            tmpChainDict.update(
-                {'L1threshold': 'FSNOSEED',
-                 'chainPartName': chainPartName,
-                 'multiplicity': mult,
-                 'threshold': cut,
-                 'etaRange':etarange,
-                 'jvt':'',
-                 }
-            )
-            preselChainDict['chainParts'] += [tmpChainDict]
-
-        # We need to pad by the legs not in the preselection expression
-        # otherwise the ComboHypo does not find the corresponding
-        # legs in the DecisionObject and kills the event
-        jetlegs = sum([p['signature'] in ["Jet","Bjet"] for p in self.chainPart])
-        padding = jetlegs-len(preselChainDict['chainParts'])
-        if padding>0:
-            preselChainDict['chainParts'][-1]['chainPartName']+='_SHARED'
-            preselChainDict['chainParts'][-1]['tboundary']='SHARED'
-            dummyLegPart = dict(preselCommonJetParts)
-            dummyLegPart.update(
-                {'L1threshold': 'FSNOSEED',
-                 'chainPartName': 'j0_SHARED',
-                 'multiplicity': '1',
-                 'threshold': '0',
-                 'jvt':'',
-                 'tboundary': 'SHARED'
-                 }
-            )
-            preselChainDict['chainParts'] += [dict(dummyLegPart) for i in range(padding)]
-            # Last one is not permitted to be shared as there is nothing following
-            preselChainDict['chainParts'][-1]['chainPartName']='j0'
-            preselChainDict['chainParts'][-1]['tboundary']=''
-
-        assert(len(preselChainDict['chainParts'])==len(self.chainPart))
         jetDefStr = JetRecoCommon.jetRecoDictToString(preselRecoDict)
 
         stepName = "PreselStep_jet_"+jetDefStr
@@ -312,7 +230,7 @@ class JetChainConfiguration(ChainConfigurationBase):
         jetSeq, jetDef, clustersKey = RecoFragmentsPool.retrieve( jetCaloPreselMenuSequence,
                                                                   ConfigFlags, **preselRecoDict )
 
-        return str(clustersKey), jetDef, ChainStep(stepName, [jetSeq], multiplicity=[1], chainDicts=[preselChainDict])
+        return str(clustersKey), jetDef, ChainStep(stepName, [jetSeq], multiplicity=[1], chainDicts=[self.dict])
 
 
     def getJetEJsChainStep(self, jetCollectionName, thresh, exotdictstring):
--- Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Jet/docs/ModuleOverview.md
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Jet/docs/ModuleOverview.md
@@ -118,13 +118,22 @@ The main `jetRecoSequence()` function forwards to one of:
 * `groomedJetRecoSequence()` -- grooms an input jet collection (internally calls `standardJetBuildSequence()` to create the ungroomed jets).
 * `reclusteredJetRecoSequence()` -- reclusters an input jet collection (internally calls `standardJetBuildSequence()` to create the basic jets)
 
-[JetRecoConfiguration](../JetRecoConfiguration.py)
+[JetRecoCommon](../JetRecoCommon.py)
 -----
 
 Helper functions to facilitate the operations in `JetRecoSequences.py`:
 * Extraction & compression of the `JetRecoDict`
 * Translation of the `JetRecoDict` contents into the configuration objects from `JetRecConfig`, e.g. `JetConstituent` and `JetDefinition`.
 * Definition of jet modifier lists
+
+[JetPresel](../JetPresel.py)
+-----
+
+Helper functions to facilitate the configuration of preselection steps:
+* Extraction of the preselection reco configuration
+* Hypo tool configurators for the preselection
+
+For preselection steps, the default `trigJetHypoToolFromDict` function is wrapped in a function that translates the preselection expression from the full `chainDict` into temporary `chainParts`, which are should be seen only by the jet code.
 
 [JetTrackingConfig](../JetTrackingConfig.py)
 -----
