cf5b160ba531f26aa283378c15271addd701b2a1
Francesco Giuseppe Gravili <francesco.giuseppe.gravili@cern.ch>
2022-02-14 10:38:25 +0100

    NSW Trigger Simulation - Cleanup package

--- Trigger/TrigT1/TrigT1NSW/src/NSWL1Simulation.cxx
+++ Trigger/TrigT1/TrigT1NSW/src/NSWL1Simulation.cxx
@@ -1,57 +1,18 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
-// Athena/Gaudi includes
-#include "GaudiKernel/ITHistSvc.h"
-
-// root includes
-#include "TTree.h"
-// Local includes
 #include "NSWL1Simulation.h"
-#include <vector>
 
 namespace NSWL1 {
-
   NSWL1Simulation::NSWL1Simulation( const std::string& name, ISvcLocator* pSvcLocator )
     : AthAlgorithm( name, pSvcLocator ),
-      m_monitors(this),
-      m_pad_tds("NSWL1::PadTdsOfflineTool",this),
-      m_pad_trigger("NSWL1::PadTriggerLogicOfflineTool",this),
-      m_pad_trigger_lookup("NSWL1::PadTriggerLookupTool",this),
-      m_strip_tds("NSWL1::StripTdsOfflineTool",this),
-      m_strip_cluster("NSWL1::StripClusterTool",this),
-      //m_strip_segment("NSWL1::StripSegmentTool",this), TODO: this line makes the code crash in initialization... please, sTGC friends, fix it!!!
-      m_mmstrip_tds("NSWL1::MMStripTdsOfflineTool",this),
-      m_mmtrigger("NSWL1::MMTriggerTool",this),
-      m_trigProcessor("NSWL1::TriggerProcessorTool",this),
+      //m_strip_segment("NSWL1::StripSegmentTool", this),
       m_tree(nullptr),
       m_current_run(-1),
       m_current_evt(-1)
   {
-
-    // Property setting general behaviour:
-    declareProperty( "DoOffline",    m_doOffline    = false, "Steers the offline emulation of the LVL1 logic" );
-    declareProperty( "UseLookup",    m_useLookup    = false, "Toggle Lookup mode on and off default is the otf(old) mode" );
-    declareProperty( "DoNtuple",     m_doNtuple     = false,  "Create an ntuple for data analysis" );
-    declareProperty( "DoMM",         m_doMM         = false,  "Run data analysis for MM" );
-    declareProperty( "DoMMDiamonds", m_doMMDiamonds = false, "Run data analysis for MM using Diamond Roads algorithm" );
-    declareProperty( "DosTGC",       m_dosTGC       = false, "Run data analysis for sTGCs" );
-    declareProperty( "DoStrip",      m_doStrip      = false, "Run data analysis for sTGC strip trigger" );
-
-    // declare monitoring tools
-    declareProperty( "AthenaMonTools",          m_monitors,           "List of monitoring tools to be run with this instance, if incorrect then tool is silently skipped.");
-    declareProperty( "PadTdsTool",              m_pad_tds,            "Tool that simulates the functionalities of the PAD TDS");
-    //PadTriggerTool : in principle can be totally wuiped out. necesary for ntuples currently. Once you isolate ntuple making code and the trigger code you can abandon this method. Things ae still tangled a bit somewhow so keep it just in case
-    declareProperty( "PadTriggerTool",          m_pad_trigger,        "Tool that simulates the pad trigger logic");
-    declareProperty( "PadTriggerLookupTool",    m_pad_trigger_lookup, "Tool that is used to lookup pad trigger patterns per execute against the same LUT as in trigger FPGA");
-    declareProperty( "StripTdsTool",            m_strip_tds,          "Tool that simulates the functionalities of the Strip TDS");
-    declareProperty( "StripClusterTool",        m_strip_cluster,      "Tool that simulates the Strip Clustering");
-    declareProperty( "StripSegmentTool",        m_strip_segment,      "Tool that simulates the Segment finding");
-    declareProperty( "MMStripTdsTool",          m_mmstrip_tds,        "Tool that simulates the functionalities of the MM STRIP TDS");
-    declareProperty( "MMTriggerTool",           m_mmtrigger,          "Tool that simulates the MM Trigger");
-    declareProperty( "MMTriggerProcessorTool",  m_trigProcessor,      "Tool that simulates the TP");
-    declareProperty( "NSWTrigRDOContainerName", m_trigRdoContainer = "NSWTRGRDO"," Give a name to NSW trigger rdo container");
+    declareProperty( "StripSegmentTool", m_strip_segment, "Tool simulating the Segment finding");
   }
 
 
@@ -88,19 +49,14 @@ namespace NSWL1 {
         ATH_CHECK(m_pad_trigger.retrieve());
       }
       if(m_doStrip){
-	ATH_CHECK(m_strip_tds.retrieve());
-	ATH_CHECK(m_strip_cluster.retrieve());
-	//ATH_CHECK(m_strip_segment.retrieve());
+        ATH_CHECK(m_strip_tds.retrieve());
+        ATH_CHECK(m_strip_cluster.retrieve());
+        //ATH_CHECK(m_strip_segment.retrieve());
       }
     }
 
     if(m_doMM ){
       ATH_CHECK(m_mmtrigger.retrieve());
-    }
-
-    // Connect to Monitoring Service
-    if(m_doNtuple){
-      ATH_CHECK(m_monitors.retrieve());
     }
 
     ATH_CHECK(m_trigProcessor.retrieve());
@@ -110,11 +66,6 @@ namespace NSWL1 {
 
   StatusCode NSWL1Simulation::start() {
     ATH_MSG_DEBUG("start " << name() );
-    if(m_doNtuple){
-      for ( auto& mon : m_monitors ) {
-        ATH_CHECK(mon->bookHists());
-      }
-    }
     return StatusCode::SUCCESS;
   }
 
@@ -140,9 +91,9 @@ namespace NSWL1 {
         ATH_CHECK( m_pad_trigger->compute_pad_triggers(pads, padTriggers) );
       }
       if(m_doStrip){
-	ATH_CHECK( m_strip_tds->gather_strip_data(strips,padTriggers) );
-	ATH_CHECK( m_strip_cluster->cluster_strip_data(strips,clusters) );
-	//ATH_CHECK( m_strip_segment->find_segments(clusters,trgContainer) );
+        ATH_CHECK( m_strip_tds->gather_strip_data(strips,padTriggers) );
+        ATH_CHECK( m_strip_cluster->cluster_strip_data(strips,clusters) );
+        //ATH_CHECK( m_strip_segment->find_segments(clusters,trgContainer) );
       }
 
       ATH_CHECK(PadTriggerAdapter::fillContainer(padTriggerContainer, padTriggers, m_current_evt));
@@ -154,9 +105,6 @@ namespace NSWL1 {
       ATH_CHECK( m_mmtrigger->fillRDO(MMTriggerContainer.get(), m_doMMDiamonds) );
     }
     if(m_doNtuple){
-      for ( auto& mon : m_monitors) {
-        ATH_CHECK(mon->fillHists());
-      }
       if (m_tree) m_tree->Fill();
     }
 
@@ -170,16 +118,6 @@ namespace NSWL1 {
 
   StatusCode NSWL1Simulation::finalize() {
     ATH_MSG_DEBUG( "finalize" << name() );
-    if(m_doNtuple){
-      for ( auto& mon :  m_monitors ) {
-        ATH_CHECK(mon->finalHists());
-      }
-    }
     return StatusCode::SUCCESS;
   }
-
-  int NSWL1Simulation::resultBuilder() const {
-    return 0;
-  }
-
 }
