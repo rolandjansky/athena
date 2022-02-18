f44b4295db58b0b104ccbc118989e398616a9e4d
Dan Guest <daniel.hay.guest@cern.ch>
2022-02-18 21:37:24 +0100

    ATLASRECTS-6833: Update xAODBTaggingAuxContainer to v2

---
+++ Event/xAOD/xAODBTagging/Root/BTaggingAuxContainer_v2.cxx
@@ -0,0 +1,14 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Local include(s):
+#include "xAODBTagging/versions/BTaggingAuxContainer_v2.h"
+
+namespace xAOD {
+
+   BTaggingAuxContainer_v2::BTaggingAuxContainer_v2()
+      : AuxContainerBase() {
+   }
+
+} // namespace xAOD
