4b77e96c5b11db3fc26af97f4e8e3d18c7b84db5
Alessandro Lapertosa <alessandro.lapertosa@cern.ch>
2022-02-25 19:28:16 +0100

    r22 JetTagging: JetTracks

--- PhysicsAnalysis/JetTagging/JetTagMonitoring/src/JetTagMonitorAlgorithm.h
+++ PhysicsAnalysis/JetTagging/JetTagMonitoring/src/JetTagMonitorAlgorithm.h
@@ -10,20 +10,45 @@
 #include "StoreGate/ReadHandleKey.h"
 #include "StoreGate/ReadHandle.h"
 
+#include "xAODEventInfo/EventInfo.h"
+#include "LArRecEvent/LArEventBitInfo.h"
+
+#include "xAODTracking/Vertex.h"
+#include "xAODTracking/VertexContainer.h"
+#include "VxVertex/VxContainer.h"
+#include "VxSecVertex/VxSecVKalVertexInfo.h"
+#include "VxSecVertex/VxSecVertexInfo.h"
+
+#include "xAODTracking/TrackParticle.h"
+#include "xAODTracking/TrackParticleContainer.h"
+#include "TrkTrack/Track.h"
+#include "TrkParticleBase/LinkToTrackParticleBase.h"
+#include "TrkVertexFitterInterfaces/ITrackToVertexIPEstimator.h"
+#include "TrkTrack/TrackCollection.h"
+#include "InDetTrackSelectionTool/IInDetTrackSelectionTool.h"
+
+#include "xAODEgamma/ElectronContainer.h"
+#include "xAODMuon/MuonContainer.h"
+
 #include "xAODJet/Jet.h"
 #include "xAODJet/JetContainer.h"
-#include "xAODMuon/MuonContainer.h"
-#include "xAODEgamma/ElectronContainer.h"
-#include "xAODEventInfo/EventInfo.h"
-#include "VxVertex/VxContainer.h"
-#include "xAODTracking/VertexContainer.h"
-#include "TrkTrack/TrackCollection.h"
+#include "xAODJet/JetAttributes.h"
+#include "xAODBTagging/BTagging.h"
+#include "xAODBTagging/BTaggingUtilities.h"
 
-#include <string>
-#include <vector>
 #include "AthenaBaseComps/AthAlgorithm.h"
 #include "GaudiKernel/ToolHandle.h"
 #include "GaudiKernel/ServiceHandle.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TMath.h"
+#include "TEfficiency.h"
+
+#include <vector>
+#include <string>
+#include <sstream>
 #include <bitset>
 #include <map>
 
@@ -35,6 +60,7 @@ class JetTagMonitorAlgorithm : public AthMonitorAlgorithm {
   virtual StatusCode fillHistograms( const EventContext& ctx ) const override;
 
  private:
+
   SG::ReadHandleKey<xAOD::VertexContainer> m_VertContainerKey{this,"VerticesKey","PrimaryVertices","RHK for primary vertices"};
   SG::ReadHandleKey<xAOD::TrackParticleContainer> m_TrackContainerKey{this,"TracksKey","InDetTrackParticles","RHK for ID tracks"};
 
@@ -49,22 +75,16 @@ class JetTagMonitorAlgorithm : public AthMonitorAlgorithm {
   SG::ReadDecorHandleKey<xAOD::ElectronContainer> m_EleEtIsoDecorKey {this,"EleEtIsoDecorKey","Electrons.topoetcone20"};
   SG::ReadDecorHandleKey<xAOD::ElectronContainer> m_ElePtIsoDecorKey {this,"ElePtIsoDecorKey","Electrons.ptvarcone20"};
 
+  ToolHandle<InDet::IInDetTrackSelectionTool> m_TrackSelectionTool{this, "TrackSelectionTool", "InDetTrackSelectionTool", "Tool for selecting tracks"};
+
   bool m_SkipJetFilter; //true for HI/HI-p collisions, false for pp collisions
-  bool m_DoExtraTaggerHistos; //true if interested in IP2D, IP3D, SV1, JetFitter, RRNIP and MV2c10 (r21) histograms
+  bool m_DoExtraTaggerHistos; //true if interested in extra tagger histograms
 
   int m_MinGoodTrackCut;
   float m_TrackPtCut;
-  float m_TrackEtaCut;
   float m_Trackd0Cut;
   float m_Trackz0sinCut;
-  float m_TrackChi2ndfCut;
   int m_TrackHitIBLCut;
-  int m_TrackHitPixCut;
-  int m_TrackHolePixCut;
-  int m_TrackHitSCTCut;
-  int m_TrackHoleSCTCut;
-  int m_TrackHitSiCut;
-  int m_TrackHoleSiCut;
 
   float m_JetPtCut;
   float m_JetEtaCut;
@@ -94,7 +114,6 @@ class JetTagMonitorAlgorithm : public AthMonitorAlgorithm {
   enum Jet_t {goodJet, suspectJet, badJet};
   void fillGoodJetHistos(const xAOD::Jet *jet) const;
   void fillSuspectJetHistos(const xAOD::Jet *jet) const;
-  void fillJetTracksHistos(const xAOD::Jet *jet, float PV_Z)  const;
   void fillExtraTaggerHistos(const xAOD::Jet *jet) const;
   void fillTTbarEventJetHistos(const xAOD::Jet *jet) const;
   bool passJetFilterCut(const xAOD::Jet *jet) const;
