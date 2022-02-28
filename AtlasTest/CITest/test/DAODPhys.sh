9dc1aa3d88f5d7a65375250aa4ae40f8f9415268
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-02-28 13:56:40 +0100

    CITest: migrate Run2Data and DAODPHYS tests
    Migrate the following tests to CITest:
      - RecoRun2Data
      - RecoRun2Data_CAConfig
      - PHYS and PHYSLITE tests running on the output of RecoRun2Data
    
    Also delete some obsolete scripts.
    
    Relates to ATLINFR-4360.

--- AtlasTest/CITest/test/DAODPhys.sh
+++ AtlasTest/CITest/test/DAODPhys.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 
 format=$1
 inputAOD=$2
 
-Reco_tf.py --AMI q431 \
+Reco_tf.py --AMI q442 \
            --steering doRAWtoALL \
            --inputAODFile ${inputAOD} \
            --outputDAODFile pool.root \
            --reductionConf ${format} \
-           --preExec 'AODtoDAOD:from AthenaCommon.DetFlags import DetFlags;DetFlags.detdescr.all_setOff();DetFlags.BField_setOn()'
+           --preExec 'AODtoDAOD:from AthenaCommon.DetFlags import DetFlags; DetFlags.detdescr.all_setOff(); DetFlags.BField_setOn(); DetFlags.digitize.all_setOff(); DetFlags.detdescr.Calo_setOn(); DetFlags.simulate.all_setOff(); DetFlags.pileup.all_setOff(); DetFlags.overlay.all_setOff();'
