a8376b8f2129af02fae0d004954a5ae473e52647
Nicholas Styles <nicholas.styles@desy.de>
2022-03-01 14:26:12 +0100

    fix for ATLASRECTS-6827

--- Reconstruction/RecExample/RecExCommon/share/RecExCommon_topOptions.py
+++ Reconstruction/RecExample/RecExCommon/share/RecExCommon_topOptions.py
@@ -1346,9 +1346,11 @@ if rec.doWriteAOD():
         if rec.doCalo and AODFlags.ThinNegativeEnergyNeutralPFOs:
             from ThinningUtils.ThinNegativeEnergyNeutralPFOs import ThinNegativeEnergyNeutralPFOs
             ThinNegativeEnergyNeutralPFOs()
+        from InDetRecExample.InDetJobProperties import InDetFlags   
         if (AODFlags.ThinInDetForwardTrackParticles() and
             not (rec.readESD() and not objKeyStore.isInInput('xAOD::TrackParticleContainer',
-                                                             'InDetForwardTrackParticles'))):
+                                                             'InDetForwardTrackParticles'))
+            and InDetFlags.doForwardTracks()):                                                 
             from ThinningUtils.ThinInDetForwardTrackParticles import ThinInDetForwardTrackParticles
             ThinInDetForwardTrackParticles()
 
