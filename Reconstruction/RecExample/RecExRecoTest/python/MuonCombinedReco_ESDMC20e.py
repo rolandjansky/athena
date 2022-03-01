e87d7773a7bd2575a11f8a5c5a8e983c353675da
Thomas Strebler <thomas.strebler@cern.ch>
2022-03-01 15:24:10 +0100

    Update of TrackingReco and CombinedMuon ESDMC20e ART tests [fix ATLASRECTS-6852, ATLASRECTS-6845, ATLASRECTS-6846]

--- Reconstruction/RecExample/RecExRecoTest/python/MuonCombinedReco_ESDMC20e.py
+++ Reconstruction/RecExample/RecExRecoTest/python/MuonCombinedReco_ESDMC20e.py
@@ -4,6 +4,7 @@ if __name__=="__main__":
     from AthenaConfiguration.AllConfigFlags import ConfigFlags
     
     ConfigFlags.Input.Files = ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/mc20e_13TeV/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.ESD.e4993_s3227_r12689/myESD.pool.root"]
+    ConfigFlags.IOVDb.GlobalTag = "OFLCOND-MC16-SDR-RUN2-09"
     ConfigFlags.lock()
 
     from AthenaConfiguration.MainServicesConfig import MainServicesCfg
