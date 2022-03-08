01a67d68afe1e0df06ae1a89a84e761e1a9c5f31
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-03-08 18:12:20 +0100

    MuonMDT_Cabling: remove redundant regHandle call

--- MuonSpectrometer/MuonCablings/MuonMDT_Cabling/src/MuonMDT_CablingAlg.cxx
+++ MuonSpectrometer/MuonCablings/MuonMDT_Cabling/src/MuonMDT_CablingAlg.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "MuonMDT_Cabling/MuonMDT_CablingAlg.h"
@@ -23,14 +23,9 @@ MuonMDT_CablingAlg::MuonMDT_CablingAlg(const std::string& name, ISvcLocator* pSv
 StatusCode MuonMDT_CablingAlg::initialize(){
 
   ATH_MSG_DEBUG( "initialize " << name() );  
-  ATH_CHECK(m_condSvc.retrieve());
   ATH_CHECK(m_readKeyMez.initialize());
   ATH_CHECK(m_readKeyMap.initialize());
   ATH_CHECK(m_writeKey.initialize());
-  if(m_condSvc->regHandle(this, m_writeKey).isFailure()) {
-    ATH_MSG_FATAL("unable to register WriteCondHandle " << m_writeKey.fullKey() << " with CondSvc");
-    return StatusCode::FAILURE;
-  }
   ATH_CHECK(m_idHelperSvc.retrieve());
   return StatusCode::SUCCESS;
 }
