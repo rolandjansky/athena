a2e4a1ebbe2f1ec5dccbd9a2a228b7a64e7ffff5
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-03-09 12:51:04 +0100

    MuidTrackBuilder: Move private headers to src directory
    Remove the usage of `atlas_install_headers` and move private header files
    to the src directory. Also clenaup link dependencies.

--- Reconstruction/MuonIdentification/MuidTrackBuilder/src/MuonTrackQuery.cxx
+++ Reconstruction/MuonIdentification/MuidTrackBuilder/src/MuonTrackQuery.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "MuidTrackBuilder/MuonTrackQuery.h"
+#include "MuonTrackQuery.h"
 
 #include <cmath>
 #include <iomanip>
