03e6b791656b93650775cac7c6eb12847edf7595
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-03-10 15:09:59 +0100

    MuonAlignErrorTool: make headers private and cmake fixes

--- MuonSpectrometer/MuonAlignment/MuonAlignError/MuonAlignErrorTool/src/components/MuonAlignErrorTool_entries.cxx
+++ MuonSpectrometer/MuonAlignment/MuonAlignError/MuonAlignErrorTool/src/components/MuonAlignErrorTool_entries.cxx
@@ -1,5 +1,5 @@
-#include "MuonAlignErrorTool/AlignmentErrorTool.h"
-#include "MuonAlignErrorTool/MuonAlignErrorExampleAlg.h"
+#include "../AlignmentErrorTool.h"
+#include "../MuonAlignErrorExampleAlg.h"
 
 DECLARE_COMPONENT(MuonAlign::MuonAlignErrorExampleAlg)
 DECLARE_COMPONENT(MuonAlign::AlignmentErrorTool)
