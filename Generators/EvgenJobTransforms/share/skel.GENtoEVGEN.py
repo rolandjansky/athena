b3681d06fe1c3a4d277a989318bd9ed6c45c4524
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-03-11 14:39:22 +0100

    EvgenJobTransforms: add checkup of list with problems in specific processes

--- Generators/EvgenJobTransforms/share/skel.GENtoEVGEN.py
+++ Generators/EvgenJobTransforms/share/skel.GENtoEVGEN.py
@@ -533,6 +533,29 @@ def checkBlackList(relFlavour,cache,generatorName) :
                 isError=relFlavour+","+cache+" is blacklisted for " + badGens
                 return isError
     return isError
+
+def checkPurpleList(relFlavour,cache,generatorName) :
+    isError = None
+    with open('/cvmfs/atlas.cern.ch/repo/sw/Generators/MC16JobOptions/common/PurpleList_generators.txt') as bfile:
+        for line in bfile.readlines():
+            if not line.strip():
+                continue
+            # Purple-listed release flavours
+            purpleRelFlav=line.split(',')[0].strip()
+            # Purple-listed caches
+            purpleCache=line.split(',')[1].strip()
+            # Purple-listed generators
+            purpleGens=line.split(',')[2].strip()
+            # Purple-listed process
+            purpleProcess=line.split(',')[3].strip()
+
+            used_gens = ','.join(generatorName)
+            #Match Generator and release type e.g. AtlasProduction, MCProd
+            if relFlavour==purpleRelFlav and cache==purpleCache and re.search(purpleGens,used_gens) is not None:
+                isError=relFlavour+","+cache+" is blacklisted for " + purpleGens + " if it uses " + purpleProcess
+                return isError
+    return isError
+
 ## Announce start of JO checkingrelease number checking
 evgenLog.debug("****************** CHECKING RELEASE IS NOT BLACKLISTED *****************")
 rel = os.popen("echo $AtlasVersion").read()
@@ -541,6 +564,11 @@ errorBL = checkBlackList("AthGeneration",rel,gennames)
 if (errorBL): 
    raise RuntimeError("This run is blacklisted for this generator, please use a different one !! "+ errorBL)  
 #    evgenLog.warning("This run is blacklisted for this generator, please use a different one !! "+ errorBL )
+errorPL = checkPurpleList("AthGeneration",rel,gennames)
+if (errorPL):
+   evgenLog.warning("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+   evgenLog.warning("!!! WARNING  !!! "+ errorPL )
+   evgenLog.warning("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 ##==============================================================
 ## Handling of a post-include/exec args at the end of standard configuration
@@ -608,7 +636,7 @@ def find_unique_file(pattern):
     "Return a matching file, provided it is unique"
     import glob
     files = glob.glob(pattern)
-    ## Check that there is exactly 1 match
+## Check that there is exactly 1 match
     if not files:
         raise RuntimeError("No '%s' file found" % pattern)
     elif len(files) > 1:
