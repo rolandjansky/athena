d9843e4d5c0a7a558f6a69a5f181def43e9bd746
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-03-11 16:49:00 +0100

    MuonCondTool: make component headers private and fix link dependencies

--- MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondTool/src/CSC_DCSConditionsTool.cxx
+++ MuonSpectrometer/MuonConditions/MuonCondGeneral/MuonCondTool/src/CSC_DCSConditionsTool.cxx
@@ -2,7 +2,7 @@
   Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "MuonCondTool/CSC_DCSConditionsTool.h"
+#include "CSC_DCSConditionsTool.h"
 
 #include "SGTools/TransientAddress.h"
 #include "CoralBase/Attribute.h"
