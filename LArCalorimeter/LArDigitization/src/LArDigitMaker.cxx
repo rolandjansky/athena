4e4e607300753b76c6b8f897e46a2db00c65f198
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2022-03-11 15:28:26 +0100

    Clean-up LArDigitizationLib

--- LArCalorimeter/LArDigitization/src/LArDigitMaker.cxx
+++ LArCalorimeter/LArDigitization/src/LArDigitMaker.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 // +==========================================================================+
@@ -9,7 +9,7 @@
 // +                                                                          +
 // +==========================================================================+
 //
-#include "LArDigitization/LArDigitMaker.h"
+#include "LArDigitMaker.h"
 
 //----------------------------------------------------------------------------------
 
