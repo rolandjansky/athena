cb6fd04fd556a503ae33c9522d06a33a9dd2304d
Johannes Josef Junggeburth <johannes.josef.junggeburth@CERN.CH>
2022-03-17 08:23:24 +0100

    Downgrade WARNING

--- Reconstruction/RecoTools/IsolationTool/Root/TrackIsolationTool.cxx
+++ Reconstruction/RecoTools/IsolationTool/Root/TrackIsolationTool.cxx
@@ -187,7 +187,7 @@ namespace xAOD {
       if( (!m_trkselTool->accept( *tp , input.vertex)) ||
         (m_useTTVATool && (!input.vertex || !m_ttvaTool->isCompatible(*tp, *input.vertex)))){
           if (m_useTTVATool && !input.vertex){
-            ATH_MSG_WARNING("Encountered a track isolation input with invalid vertex while requiring TTVA. Rejecting, please check your inputs!"); 
+            ATH_MSG_DEBUG("Encountered a track isolation input with invalid vertex while requiring TTVA. Rejecting, please check your inputs!"); 
           } 
           ATH_MSG_DEBUG("reject track pt = " << tp->pt());
           continue;
