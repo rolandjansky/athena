cd476de68fc177902f247102a998e4ae8c1f2223
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-03-24 09:32:47 +0100

    update to Superchic4.11p1 and Apfel3.0.4.lhapdf623

--- Projects/AthGeneration/externals/Superchic.cmake
+++ Projects/AthGeneration/externals/Superchic.cmake
@@ -1,7 +1,12 @@
 #
-# File specifying the location of Superchic 3.0 to use.
+# File specifying the location of Superchic 4.0 to use.
 #
 
-set( SUPERCHIC_LCGVERSION 4.02p2 )
+set( SUPERCHIC_LCGVERSION 4.11p1 )
 set( SUPERCHIC_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/superchic/${SUPERCHIC_LCGVERSION}/${LCG_PLATFORM} )
+
+set( APFEL_LCGVERSION 3.0.4.lhapdf623 )
+set( APFEL_LCGROOT
+   ${LCG_RELEASE_DIR}/MCGenerators/apfel/${APFEL_LCGVERSION}/${LCG_PLATFORM} )
+
