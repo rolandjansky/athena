719c14535dbe2bbe83366d9486c84ddf907c5a12
Michele Faucci Giannelli <michele.faucci.giannelli@cern.ch>
2022-03-25 17:33:58 +0100

    Updated options to allow running FCS following changes in ISF classes

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_DigiTilePostInclude.py
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimParametrization/share/ISF_FastCaloSimParametrization_DigiTilePostInclude.py
@@ -3,11 +3,7 @@ from AthenaCommon.AppMgr import ServiceMgr
 puAlg = job.StandardPileUpToolsAlg
 puAlg.PileUpTools["TileHitVecToCntTool"].HitTimeFlag=1
 
-ServiceMgr.TileInfoLoader.NPhElec=0
-ServiceMgr.TileInfoLoader.NPhElecA=0
-ServiceMgr.TileInfoLoader.NPhElecC=0
-ServiceMgr.TileInfoLoader.NPhElecD=0
-ServiceMgr.TileInfoLoader.NPhElecE=0
+puAlg.PileUpTools["TileHitVecToCntTool"].usePhotoStatistics=False
 
 topSequence.TileDigitsMaker.IntegerDigits=True
 
