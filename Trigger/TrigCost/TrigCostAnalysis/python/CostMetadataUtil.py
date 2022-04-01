7d23c9ed8e2f2c13df7caae74dff5e54b88b428f
Aleksandra Poreba <aleksandra.poreba@cern.ch>
2022-04-01 19:03:41 +0200

    TrigCost: Improve finding oks metadata in postprocessing

--- Trigger/TrigCost/TrigCostAnalysis/python/CostMetadataUtil.py
+++ Trigger/TrigCost/TrigCostAnalysis/python/CostMetadataUtil.py
@@ -8,7 +8,7 @@
 @brief Helper functions to create cost metadata json file based on input ntuple
         and histogram under/overflows
 '''
-
+import xml.etree.ElementTree as ET
 from AthenaCommon.Logging import logging
 log = logging.getLogger('CostAnalysisPostProcessing')
 
@@ -32,8 +32,10 @@ def saveMetadata(inputFile, userDetails, jira="", processingWarnings=[], readOKS
 
     metadata += readHLTConfigKeys(metatree.runNumber)
 
-    if readOKSDetails and metatree.hostname:
-        metadata += addOKSDetails(metatree.hostname, metatree.runNumber, partition)
+    if metatree.hostname and readOKSDetails:
+        metadata.append({'OKS configuration' : addOKSDetails(str(metatree.hostname), metatree.runNumber, partition)})
+    elif metadata.hostname:
+        metadata.append({'Hostnames' : str(metatree.hostname)})
 
     metadata.append({'AtlasCostProcessingProject' : str(metatree.AtlasProject)})
     metadata.append({'AtlasCostProcessingVersion' : str(metatree.AtlasVersion)})
@@ -114,9 +116,7 @@ def addOKSDetails(hostname, runNumber, partition):
     import os
     os.system("git clone https://gitlab.cern.ch/atlas-tdaq-oks/p1/tdaq-09-04-00.git --branch " + oksTag + " --single-branch")
 
-    # Browse OKS
-    import xml.etree.ElementTree as ET
-    
+    # Browse OKS    
     try:
         if partition == "TDAQ":
             partitionRoot = ET.parse('tdaq-09-04-00/daq/partitions/TDAQ.data.xml').getroot()
@@ -128,14 +128,31 @@ def addOKSDetails(hostname, runNumber, partition):
         log.warning("OKS files not available: {0}".format(e))
         return []
 
-    # Read F/T/S
-    for host in hostname.split(","):
-        hltApplication = findHLTApplication(partitionRoot, hltRoot, host, partition)
 
-        oksMetadata.append([{'Hostname' : host},
-                            {'Forks' : hltRoot.findall("./*[@id='{0}']/*[@name='numForks']".format(hltApplication))[0].get("val")},
-                            {'Threads' : hltRoot.findall("./*[@id='{0}']/*[@name='numberOfAthenaMTThreads']".format(hltApplication))[0].get("val")},
-                            {'Slots' : hltRoot.findall("./*[@id='{0}']/*[@name='numberOfEventSlots']".format(hltApplication))[0].get("val")}])
+    # Read F/T/S
+    racksToComp = dict()
+    if 'pc' in hostname:
+        # Convert computer names to set of racks
+        for computerName in hostname.split(","):
+            rackName = findRackForComputer(computerName)
+            if rackName not in racksToComp:
+                racksToComp[rackName] = list()
+            racksToComp[rackName].append(computerName)
+
+        hostname = ",".join(racksToComp.keys())
+
+    for rack in hostname.split(","):
+        hltApplication = findHLTApplication(partitionRoot, hltRoot, rack, partition)
+
+        metadataDict = [{'Hostname' : rack},                     
+                        {'Forks' : hltRoot.findall("./*[@id='{0}']/*[@name='numForks']".format(hltApplication))[0].get("val")},
+                        {'Threads' : hltRoot.findall("./*[@id='{0}']/*[@name='numberOfAthenaMTThreads']".format(hltApplication))[0].get("val")},
+                        {'Slots' : hltRoot.findall("./*[@id='{0}']/*[@name='numberOfEventSlots']".format(hltApplication))[0].get("val")}]
+
+        if rack in racksToComp:
+            metadataDict.append({'Computers' : str(racksToComp[rack])})
+
+        oksMetadata.append(metadataDict)
 
     # Cleanup cloned repository
     os.system("rm -rf tdaq-09-04-00")
@@ -146,6 +163,7 @@ def findHLTApplication(partitionRoot, hltRoot, hostname, partitionName="ATLAS"):
     ''' @brief Find HLT application based on hostname and disabled segments
     '''
     segments = []
+
     if hostname:
         # Find segment based on hostname
         for segment in hltRoot.findall("./*[@class='TemplateSegment']/*[@name='Racks']/*[@id='{0}'].../...".format(hostname)):
@@ -167,6 +185,20 @@ def findHLTApplication(partitionRoot, hltRoot, hostname, partitionName="ATLAS"):
         log.warning("Found more than one enabled segment, will use {0}".format(segments[0]))
 
     return hltRoot.findall("./*[@id='{0}']/*[@name='Applications']/*[@class='HLTRCApplication']".format(segments[0]))[0].get("id")
+
+
+def findRackForComputer(computerName):
+    ''' Find rack for computer name '''
+
+    import re
+    m = re.search(r'pc-tdq-tpu-(.*?)\.cern\.ch', computerName)
+    if m:
+        computerNum = m.group(1)
+        rackNumber = computerNum[0:2]
+        return "tpu-rack-{0}".format(rackNumber)
+
+    log.warning("Cannot retrieve rack number from {0}".format(computerName))
+    return ""
 
 
 def readHLTConfigKeys(runNumber):
@@ -201,8 +233,8 @@ def readHLTConfigKeys(runNumber):
             # Run 3 format
             configMetadata.append({'AtlasProject' : dbInfo[2]})
 
-        configMetadata.append({'HLTPSK' : TriggerCoolUtil.getHLTPrescaleKeys(dbconn, [[runNumber, runNumber]])[runNumber]['HLTPSK2']})
-        configMetadata.append({'LVL1PSK' : TriggerCoolUtil.getL1ConfigKeys(dbconn, [[runNumber, runNumber]])[runNumber]['LVL1PSK']})
+        configMetadata.append({'HLTPSK' : str(TriggerCoolUtil.getHLTPrescaleKeys(dbconn, [[runNumber, runNumber]])[runNumber]['HLTPSK2'])})
+        configMetadata.append({'LVL1PSK' : str(TriggerCoolUtil.getL1ConfigKeys(dbconn, [[runNumber, runNumber]])[runNumber]['LVL1PSK'])})
 
     else:
         log.warning("Config keys not found in COOL")
