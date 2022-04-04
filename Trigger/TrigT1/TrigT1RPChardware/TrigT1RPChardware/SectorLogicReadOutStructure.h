bbc46a1fee13d9a29a55db3a2375fe7889254cb6
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-04-04 09:58:19 +0200

    TrigT1RPChardware: make RPCReadOut class non-virtual

--- Trigger/TrigT1/TrigT1RPChardware/TrigT1RPChardware/SectorLogicReadOutStructure.h
+++ Trigger/TrigT1/TrigT1RPChardware/TrigT1RPChardware/SectorLogicReadOutStructure.h
@@ -16,20 +16,20 @@ public:
 
     void setInit();
     //
-    virtual ubit16 makeHeader(ubit16 *inputData) override final;
-    virtual ubit16 makeSubHeader() override final { return 0; };
-    virtual ubit16 makeBody(ubit16 *inputData) override final { return (*inputData) & 0x0; };
-    virtual ubit16 makeFooter(ubit16 errorCode) override final { return errorCode & 0x0; };
-    virtual ubit16 makeFooter(ubit16 *errorCode) override final;
+    ubit16 makeHeader(ubit16 *inputData);
+    ubit16 makeSubHeader() { return 0; };
+    ubit16 makeBody(ubit16 *inputData) { return (*inputData) & 0x0; };
+    ubit16 makeFooter(ubit16 errorCode) { return errorCode & 0x0; };
+    ubit16 makeFooter(ubit16 *errorCode);
 
     //**************************************************//
     // general user methods to decode ReadOut stream    //
     //**************************************************//
     ubit16 decodeFragment(ubit16 inputWord, char &field);
     //
-    bool isHeader() override final;
-    bool isBody() override final;
-    bool isFooter() override final;
+    bool isHeader();
+    bool isBody();
+    bool isFooter();
     //
 private:
     //
