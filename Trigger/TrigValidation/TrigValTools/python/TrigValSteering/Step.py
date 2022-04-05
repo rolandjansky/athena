253dc346aa0422837ba7729a149bfa436a70d087
Rafal Bielski <rafal.bielski@cern.ch>
2022-04-05 14:45:26 +0200

    Allow to generate HLT configuration without specifying an input file

--- Trigger/TrigValidation/TrigValTools/python/TrigValSteering/Step.py
+++ Trigger/TrigValidation/TrigValTools/python/TrigValSteering/Step.py
@@ -33,6 +33,7 @@ class Step(object):
         self.type = None
         self.executable = None
         self.args = ''
+        self.cmd_prefix = None
         self.cmd_suffix = None
         self.output_stream = self.OutputStream.FILE_ONLY
         self.log_file_name = None
@@ -160,6 +161,8 @@ class Step(object):
 
     def run(self, dry_run=False):
         cmd = '{} {}'.format(self.executable, self.args)
+        if self.cmd_prefix:
+            cmd = self.cmd_prefix + cmd
         if self.cmd_suffix:
             cmd += self.cmd_suffix
         if self.output_stream == self.OutputStream.NO_PRINT:
