8766b365bc2a4fc268f138c6e68499a1df42bc19
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2022-04-06 18:52:36 +0200

    Revert "Merge branch 'master-TMMT-skipCPSCheck' into 'master'"

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Config/GenerateMenuMT.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Config/GenerateMenuMT.py
@@ -496,15 +496,9 @@ class GenerateMenuMT(object, metaclass=Singleton):
         from TriggerMenuMT.HLT.Config.Validation.CheckL1HLTConsistency import checkL1HLTConsistency
         checkL1HLTConsistency(flags)
         
-        doCPSCheck = True
-        # Disable if we have a chain filter that only runs specific chains
-        # as this is likely to result in single-chain CPS groups
-        if self.chainFilter is not None and hasattr(self.chainFilter,'selectChains') and self.chainFilter.selectChains:
-            doCPSCheck = False
-        if doCPSCheck:
-            log.info("Checking the Coherent Prescale assignments...")
-            from TriggerMenuMT.HLT.Config.Validation.CheckCPSGroups import checkCPSGroups
-            checkCPSGroups(HLTMenuConfig.dictsList())
+        log.info("Checking the Coherent Prescale assignments...")
+        from TriggerMenuMT.HLT.Config.Validation.CheckCPSGroups import checkCPSGroups
+        checkCPSGroups(HLTMenuConfig.dictsList())
 
         log.info("Generating HLT menu JSON...")
         
