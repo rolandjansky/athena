dea41b4d87290b31292ac6b5432566869ed0eaea
Ewelina.Maria.Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-04-06 14:58:20 +0200

    PowhegControl: manual sweep

--- Generators/PowhegControl/python/algorithms/postprocessors/madspin.py
+++ Generators/PowhegControl/python/algorithms/postprocessors/madspin.py
@@ -258,7 +258,7 @@ def __run_executable(executable):
         raise OSError("MadSpin executable {} not found!".format(executable))
     logger.info("MadSpin executable: {}".format(executable))
     with open("madspin_runcard.txt", "r") as runcard_input:
-        processes = [SingleProcessThread([executable], stdin=runcard_input, ignore_output=["INFO:","MadSpin>"],
+        processes = [SingleProcessThread(['python', executable], stdin=runcard_input, ignore_output=["INFO:","MadSpin>"],
                                          error_output=["Command \"launch\" interrupted with error:","MadSpinError"],
                                          info_output=["generating the production square matrix element"],
                                          warning_output=["stty: standard input: Invalid argument",
