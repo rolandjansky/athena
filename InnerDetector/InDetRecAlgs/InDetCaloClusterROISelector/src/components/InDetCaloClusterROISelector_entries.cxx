990da8ff96cf7a43d4a0dcd337b0462b3dc240cc
Goetz Gaycken <goetz.gaycken@cern.ch>
2022-04-13 19:36:58 +0200

    Introduce flat container to replace the CaloClusterROI collection.
    The flat container stores the ROIs in a format which can be directly used
    by its client without any further conversion. Also the ROIs can be phi
    ordered to allow for a binary search for matching calo cluster ROIs
    in phi.

--- InnerDetector/InDetRecAlgs/InDetCaloClusterROISelector/src/components/InDetCaloClusterROISelector_entries.cxx
+++ InnerDetector/InDetRecAlgs/InDetCaloClusterROISelector/src/components/InDetCaloClusterROISelector_entries.cxx
@@ -1,7 +1,9 @@
 #include "InDetCaloClusterROISelector/CaloClusterROI_Selector.h"
+#include "InDetCaloClusterROISelector/CaloClusterROIPhiRZContainerMaker.h"
 
 using namespace InDet;
 
 DECLARE_COMPONENT( CaloClusterROI_Selector )
-	
+DECLARE_COMPONENT( CaloClusterROIPhiRZContainerMaker )
+
 
