63b062904e6caa66ead328c3f93a1d9f1b477d4c
William Keaton Balunas <bill.balunas@cern.ch>
2022-04-13 20:11:45 +0200

    Add options to record jets/MET to ESD/AOD in new config

--- Reconstruction/MET/METReconstruction/python/METConfigFlags.py
+++ Reconstruction/MET/METReconstruction/python/METConfigFlags.py
@@ -1,1 +1,10 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaborationfrom AthenaConfiguration.AthConfigFlags import AthConfigFlagsdef createMETConfigFlags():    metConfigFlags=AthConfigFlags()    metConfigFlags.addFlag("MET.UseTracks",True)     metConfigFlags.addFlag("MET.DoPFlow",True)     return metConfigFlags
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+
+from AthenaConfiguration.AthConfigFlags import AthConfigFlags
+
+def createMETConfigFlags():
+    metConfigFlags=AthConfigFlags()
+    metConfigFlags.addFlag("MET.UseTracks",True)
+    metConfigFlags.addFlag("MET.DoPFlow",True)
+    metConfigFlags.addFlag("MET.WritetoAOD",False)
+    return metConfigFlags
