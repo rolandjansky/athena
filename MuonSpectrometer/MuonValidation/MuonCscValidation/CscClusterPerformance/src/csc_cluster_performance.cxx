4d33a51d89b82a5e0d4d1d9ca4fc7c429fb75ab3
scott snyder <snyder@bnl.gov>
2022-04-14 16:16:42 +0200

    CscClusterPerformance: Suppress clang14 warnings.
    Suppress clang14 -Wbitwise-instead-of-logical that we get from the ROOT
    TBits.h header.

--- MuonSpectrometer/MuonValidation/MuonCscValidation/CscClusterPerformance/src/csc_cluster_performance.cxx
+++ MuonSpectrometer/MuonValidation/MuonCscValidation/CscClusterPerformance/src/csc_cluster_performance.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 // csc_cluster_performance.cxx
@@ -21,6 +21,7 @@
 #include <map>
 #include <vector>
 #include <cmath>
+#include "CxxUtils/no_bitwise_op_warning.h"
 #include "TTree.h"
 #include "TTreeIndex.h"
 #include "SimposAccessor.h"
