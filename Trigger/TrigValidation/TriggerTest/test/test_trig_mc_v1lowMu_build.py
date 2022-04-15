5cc165214c6fcce1f147e2e2017879bc2708695e
Tomasz Bold <tomasz.bold@gmail.com>
2022-04-15 19:36:10 +0200

    Enable entire lowMu menu in min bias test

--- Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1lowMu_build.py
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1lowMu_build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
-# art-description: Trigger RDO->RDO_TRIG athena test of the min-bias slice
+# art-description: Trigger RDO->RDO_TRIG athena test of the lowMu menu
 # art-type: build
 # art-include: master/Athena
 # Skipping art-output which has no effect for build tests.
@@ -12,9 +12,9 @@ from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
 ex = ExecStep.ExecStep()
 ex.type = 'athena'
 ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
-ex.input = 'data_lowmu'
+ex.input = 'minbias'
 ex.threads = 1
-ex.args = '-c "setMenu=\'PhysicsP1_pp_lowMu_run3_v1\';doEmptyMenu=True;doMinBiasSlice=True;doJetSlice=True;doJetSlice=True;doL1Sim=True;doWriteBS=False;doWriteRDOTrigger=True;"'
+ex.args = '-c "setMenu=\'PhysicsP1_pp_lowMu_run3_v1\';doWriteBS=False;doWriteRDOTrigger=True;"'
 
 test = Test.Test()
 test.art_type = 'build'
