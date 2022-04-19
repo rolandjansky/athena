f9121a93b65752447f2827d0d0b366fa51e1976a
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2022-04-19 14:19:43 +0200

    MuonCombined - Restrict number of ID candidates for inside -> out chain

--- Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonInDetToMuonSystemExtensionAlg.h
+++ Reconstruction/MuonIdentification/MuonCombinedAlgs/src/MuonInDetToMuonSystemExtensionAlg.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef MUONCOMBINEDALGS_MUONINDETSYSTEMEXTENSIONALG_H
@@ -20,10 +20,10 @@
 #include "StoreGate/ReadHandleKey.h"
 #include "StoreGate/WriteHandleKey.h"
 #include "MuonIdHelpers/IMuonIdHelperSvc.h"
-
-namespace Muon {
-    class MuonSegment;
-}
+#include "MuonSegmentMakerToolInterfaces/IMuonSegmentSelectionTool.h"
+#include "MuonSegment/MuonSegment.h"
+#include "MuonDetDescrUtils/MuonSectorMapping.h"
+#include "MuonRecHelperTools/IMuonEDMHelperSvc.h"
 
 /// The MuonInDetToMuonSystemExtensionAlg processes further the InDetCandidates created by the MuonCombinedInDetCandidateAlg. 
 /// The idea is that the latter is only selecting the Id tracks based on the outcome of the Id selection tool and 
@@ -48,8 +48,15 @@ private:
     SG::WriteHandleKey<InDetCandidateCollection> m_stauInDetCandKey{this, "WriteStauCandidates", ""};
 
     /// Use the hough data to find sectors in the speectrometer traversed by a muon.
-    SG::ReadHandleKey<Muon::HoughDataPerSectorVec> m_houghDataPerSectorVecKey{this, "Key_MuonLayerHoughToolHoughDataPerSectorVec",
+    SG::ReadHandleKey<Muon::HoughDataPerSectorVec> m_houghDataPerSectorVecKey{this, "LayerHoughKey",
                                                                               "HoughDataPerSectorVec", "HoughDataPerSectorVec key"};
+    /// Let's exploit the segments to get rid of as much tracks as possible
+    SG::ReadHandleKey<xAOD::MuonSegmentContainer> m_segmentKey{this, "MuonSegmentLocation", "xaodMuonSegments",
+                                                                              "name of muon segment container"};
+ 
+    PublicToolHandle<Muon::IMuonSegmentSelectionTool> m_segmentSelector{this, "MuonSegmentSelectionTool",
+                                                                      "Muon::MuonSegmentSelectionTool/MuonSegmentSelectionTool"};
+    
     /// The system extension tool extrapolates the candidate through the spectrometer 
     ToolHandle<Muon::IMuonSystemExtensionTool> m_muonSystemExtensionTool{this, "MuonSystemExtensionTool", "", "Muon system extension tool"};
     /// That is the collection of MuidCo candidates
@@ -58,7 +65,8 @@ private:
 
     ServiceHandle<Muon::IMuonIdHelperSvc> m_idHelperSvc{this, "MuonIdHelperSvc", "Muon::MuonIdHelperSvc/MuonIdHelperSvc"};
 
-      
+    ServiceHandle<Muon::IMuonEDMHelperSvc> m_edmHelperSvc{this, "edmHelper", "Muon::MuonEDMHelperSvc/MuonEDMHelperSvc",
+                                                              "Handle to the service providing the IMuonEDMHelperSvc interface"};
     /// Helper struct to cache the MuidCo track and it's associated segments while keeping
     /// the association InDetCandidate <-- > muidCo Tag
     struct MuidCoCache {
@@ -69,8 +77,8 @@ private:
             id_trk{_id}, cmb_trk{_cb} {}
     };
     using MuidCoVector = std::vector<MuidCoCache>;
-
-
+   
+    
     struct InDetCandidateCache {
         /// Collection of the InDet candidates to consider
         const InDetCandidateCollection* input_candidates{nullptr};      
@@ -79,19 +87,32 @@ private:
         std::set<const MuonCombined::InDetCandidate*> excluded_trks;
         /// Combined fit tag map
         MuidCoVector tag_map;
-        
+        /// List of segments that were successfully combined to a muon
+        std::set<const Muon::MuonSegment*> combined_segs{};
+
         /// Output container for the StoreGate
         std::unique_ptr<InDetCandidateCollection> outputContainer = std::make_unique<InDetCandidateCollection>(SG::OWN_ELEMENTS);
 
-     
+        /// Hit sector map
         std::map<Muon::MuonStationIndex::DetectorRegionIndex, std::set<int>> hit_sectors;
+        
+        std::map<int, std::vector<double>> eta_seeds{};
+        
+        /// Map of hits that were successfully combined to a muon segment
+        /// Key is the hash made out of region & layer index
+        std::map<unsigned int, std::set<Identifier>> consumed_hits{};
+        
+        /// Muon segments that could be potentially used for MuGirl / MuTagIMO
+        std::vector<const Muon::MuonSegment*> candidate_segments{};
     };
 
     /// Restrict the extrapolation of ID tracks to sectors in the MS where at least a hit is recorded     
     Gaudi::Property<bool> m_restrictExtension{this, "UseOnlyHitSectors", false};
     /// The hough maxima always contain at least 2 hits. If a muon track passes through a chamber one expects more than that.
     /// Apply a minimum cuts on the hits in the hough maximum to reject background --> needs to be further tuned
-    Gaudi::Property<unsigned int> m_houghMin{this,"minHoughHits", 4, "Minimum number of hits required for a hough maximum to be accepted"};
+    Gaudi::Property<int> m_houghMin{this,"minHoughHits", 4, "Minimum number of hits required for a hough maximum to be accepted"};
+    
+    Gaudi::Property<int> m_segmentQuality{this, "segQuality", 1, "Quality cut on the segment."};
    
     /// Minimum pt threshold of the IdCandidate to be extrapolated through the spectrometer
     Gaudi::Property<float> m_extThreshold{this, "ExtensionPtThreshold", 2500};
@@ -100,17 +121,30 @@ private:
     /// Shall ordinary ID tracks  be equiped with a muon system extension used by MuGirl later
     Gaudi::Property<bool> m_extendBulk{this, "ExtendBulk", true};
 
+    const Muon::MuonSectorMapping m_sector_mapping{};
+
     /// Select the MuidCo candidates and put the associated id tracks on a black list
     StatusCode selectCandidates(const EventContext& ctx, InDetCandidateCache& cache) const;
     /// Find the sectors in the MS with muon signals
     StatusCode findHitSectors(const EventContext& ctx, 
                                  InDetCandidateCache& output_cache) const;
+    /// Select good segments
+    StatusCode findSegments(const EventContext& ctx,
+                            InDetCandidateCache& output_cache) const;
+    
+    /// Searches for segments within theta < 0.2 && sharing the same sector
+    bool hasMatchingSegment(const MuonCombined::InDetCandidate& id_cand, const InDetCandidateCache& cache) const;
+    /// Searches for hough seeds within theta < 0.2 && sharing the same sector
+    bool hasMatchingSeed(const MuonCombined::InDetCandidate& id_cand, const InDetCandidateCache& cache) const;
+
     /// Create the InDetCandidaes with system extensions
     StatusCode create(const EventContext& ctx, InDetCandidateCache& cache) const;
 
     StatusCode createStaus(const EventContext& ctx, 
                            const InDetCandidateCollection* ext_candidates,
                            const MuidCoVector& tag_map) const;  
+    /// Mask the hits of the corresponding muon segment as used.
+    void maskHits(const Muon::MuonSegment* muon_seg, InDetCandidateCache& output_cache) const;
     
 };
 
