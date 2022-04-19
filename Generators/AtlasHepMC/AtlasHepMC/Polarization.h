a3bb655ac6a1498ff8eecaa231684ff603c70271
Scott Snyder <scott.snyder@cern.ch>
2022-04-19 14:13:17 +0200

    AtlasHepMC: HepMC3 polarization fix.

--- Generators/AtlasHepMC/AtlasHepMC/Polarization.h
+++ Generators/AtlasHepMC/AtlasHepMC/Polarization.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 /* Author: Andrii Verbytskyi andrii.verbytskyi@mpp.mpg.de */
 
@@ -11,29 +11,34 @@
 namespace HepMC {
 class Polarization {
 public:
-    Polarization( const double a=0.0, const double b=0.0): m_theta(a), m_phi(b) {};
+    Polarization( const double a=0.0, const double b=0.0,
+                  bool is_defined = false)
+      : m_theta(a), m_phi(b), m_is_defined(is_defined) {};
     ~Polarization() {};
     double theta() const  { return m_theta;}
     double phi() const { return m_phi;}
-    bool is_defined() const { return (std::abs(m_theta) >= 0.00001)||(std::abs(m_phi) >= 0.00001); }
+    bool is_defined() const { return m_is_defined; }
     inline bool operator ==(const Polarization &a) { return (std::abs(a.phi() - phi()) <= 0.00001) && (std::abs(a.theta() - theta()) <= 0.00001);}
 private:
     double m_theta;
     double m_phi;
+    bool m_is_defined;
 };
 inline Polarization polarization(HepMC3::GenParticlePtr a) {
     std::shared_ptr<HepMC3::DoubleAttribute> phi_A =a->attribute<HepMC3::DoubleAttribute>("phi");
     std::shared_ptr<HepMC3::DoubleAttribute> theta_A=a->attribute<HepMC3::DoubleAttribute>("theta");
     double phi=(phi_A?phi_A->value():0.0);
     double theta=(theta_A?theta_A->value():0.0);
-    return Polarization(theta,phi);
+    bool is_defined = phi_A && theta_A;
+    return Polarization(theta,phi,is_defined);
 }
 inline Polarization polarization(HepMC3::ConstGenParticlePtr a) {
     std::shared_ptr<HepMC3::DoubleAttribute> phi_A =a->attribute<HepMC3::DoubleAttribute>("phi");
     std::shared_ptr<HepMC3::DoubleAttribute> theta_A=a->attribute<HepMC3::DoubleAttribute>("theta");
     double phi=(phi_A?phi_A->value():0.0);
     double theta=(theta_A?theta_A->value():0.0);
-    return Polarization(theta,phi);
+    bool is_defined = phi_A && theta_A;
+    return Polarization(theta,phi,is_defined);
 }
 template<class T> void  set_polarization( T a,  Polarization b) {
     a->add_attribute("phi",std::make_shared<HepMC3::DoubleAttribute>(b.phi()));
