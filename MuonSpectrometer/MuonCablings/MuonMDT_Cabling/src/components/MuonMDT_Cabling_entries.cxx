4e02eefa4de395a9a48e2f79aa0719f6bf891383
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2022-04-21 11:39:38 +0200

    Mdt cabling - Add unit test

--- MuonSpectrometer/MuonCablings/MuonMDT_Cabling/src/components/MuonMDT_Cabling_entries.cxx
+++ MuonSpectrometer/MuonCablings/MuonMDT_Cabling/src/components/MuonMDT_Cabling_entries.cxx
@@ -1,2 +1,8 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
 #include "../MuonMDT_CablingAlg.h"
+#include "../MdtCablingTestAlg.h"
+
 DECLARE_COMPONENT(MuonMDT_CablingAlg)
+DECLARE_COMPONENT(MdtCablingTestAlg)
