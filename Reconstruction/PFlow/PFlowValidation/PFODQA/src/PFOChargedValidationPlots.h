0be010db1e7840d3721a0c0d7ad6d8d14bb5324d
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2022-04-25 18:38:58 +0200

    PFODQA pass string by const ref not by  value

--- Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFOChargedValidationPlots.h
+++ Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFOChargedValidationPlots.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef PFOCHARGEDVALIDATIONPLOTS_H
@@ -19,7 +19,7 @@ class PFOChargedValidationPlots : public PlotBase {
  public:
 
   /** Standard Constructor */
-  PFOChargedValidationPlots(PlotBase* pParent, std::string sDir, std::string sFEContainerName);
+  PFOChargedValidationPlots(PlotBase* pParent, const std::string& sDir, const std::string& sFEContainerName);
 
   /** fill the histograms up */  
   void fill(const xAOD::FlowElement& theFE, const xAOD::Vertex* theVertex, const xAOD::EventInfo& eventInfo);
--- Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFONeutralValidationPlots.cxx
+++ Reconstruction/PFlow/PFlowValidation/PFODQA/src/PFONeutralValidationPlots.cxx
@@ -1,15 +1,20 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "PFONeutralValidationPlots.h"
 
-PFONeutralValidationPlots::PFONeutralValidationPlots(PlotBase* pParent, std::string sDir, std::string sFEContainerName) : PlotBase(pParent, sDir),																			 
-																			  m_FEPlots(this,"",sFEContainerName),
-																			  m_FEClusterMomentPlots(this,"",sFEContainerName),
-																			  m_FEAttributePlots(this,"",sFEContainerName),
-                                        m_FELinkerPlots(this,"",sFEContainerName,true)
-{}
+PFONeutralValidationPlots::PFONeutralValidationPlots(
+  PlotBase* pParent,
+  const std::string& sDir,
+  const std::string& sFEContainerName)
+  : PlotBase(pParent, sDir)
+  , m_FEPlots(this, "", sFEContainerName)
+  , m_FEClusterMomentPlots(this, "", sFEContainerName)
+  , m_FEAttributePlots(this, "", sFEContainerName)
+  , m_FELinkerPlots(this, "", sFEContainerName, true)
+{
+}
 
 void PFONeutralValidationPlots::fill(const xAOD::FlowElement& theFE, const xAOD::EventInfo& eventInfo){
   //overload of previous function using FlowElement
