e8a72935231d3dfbfa9162a610217c6272ca8b03
leggett <leggett@cern.ch>
2022-04-27 10:06:48 -0700

    update Gaudi to v36r5.001

--- Projects/AthGeneration/build_externals.sh
+++ Projects/AthGeneration/build_externals.sh
@@ -11,7 +11,7 @@ ATLAS_EXT_PROJECT_NAME="AthGenerationExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=101
                         -DLCG_VERSION_POSTFIX="_ATLAS_18"
-                        -DATLAS_GAUDI_TAG="v36r5.000"
+                        -DATLAS_GAUDI_TAG="v36r5.001"
                         -DATLAS_GEOMODEL_TAG="4.2.8") 
 ATLAS_EXTRA_MAKE_ARGS=()
 
