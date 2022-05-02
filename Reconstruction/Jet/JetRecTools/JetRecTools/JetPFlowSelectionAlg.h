3b4c1be49591ca40f628957f73c1cdb69577f450
Bertrand Martin Dit Latour <bertrand.martindl@cern.ch>
2022-05-02 15:13:15 +0200

    JetPFlowSelectionAlg: add ReadDecorHandleKeys to ensure FEs are finalised before they are copied

--- Reconstruction/Jet/JetRecTools/JetRecTools/JetPFlowSelectionAlg.h
+++ Reconstruction/Jet/JetRecTools/JetRecTools/JetPFlowSelectionAlg.h
@@ -48,14 +48,13 @@ private:
   SG::WriteHandleKey<xAOD::FlowElementContainer> m_outputChargedPFlowHandleKey= {this, "ChargedPFlowOutputContainer", "GlobalPFlowChargedParticleFlowObjects", "The output filtered Charged PFlow Objects"};
   SG::WriteHandleKey<xAOD::FlowElementContainer> m_outputNeutralPFlowHandleKey= {this, "NeutralPFlowOutputContainer", "GlobalPFlowNeutralParticleFlowObjects", "The output filtered Neutral PFlow Objects"};
 
-  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_chargedFEElectronsReadDecorKey {this,
-      "ChargedFEElectronsReadDecorKey",
-      "JetETMissChargedParticleFlowObjects.FE_ElectronLinks",
-      "Key for links from charged FE to electrons"};
-  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_chargedFEMuonsReadDecorKey {this,
-      "ChargedFEMuonsReadDecorKey",
-      "JetETMissChargedParticleFlowObjects.FE_MuonLinks",
-      "Key for links from charged FE to muons"};
+  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_chargedFEElectronsReadDecorKey {this, "ChargedFEElectronsReadDecorKey", "JetETMissChargedParticleFlowObjects.FE_ElectronLinks", "Key for links from charged FE to electrons"};
+  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_chargedFEMuonsReadDecorKey {this, "ChargedFEMuonsReadDecorKey", "JetETMissChargedParticleFlowObjects.FE_MuonLinks", "Key for links from charged FE to muons"};
+
+  // these ReadDecorHandleKeys are introduced to make sure PFTauFlowElementAssoc runs before JetPFlowSelectionAlg
+  // otherwise FEs can be simultaneously decorated in PFTauFlowElementAssoc and deep-copied in JetPFlowSelectionAlg, causing std::bad_array_new_length exceptions
+  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_chargedFETausReadDecorKey {this, "ChargedFETausReadDecorKey", "JetETMissChargedParticleFlowObjects.FE_TauLinks", "Key for links from charged FE to taus"};
+  SG::ReadDecorHandleKey<xAOD::FlowElementContainer> m_neutralFETausReadDecorKey {this, "NeutralFETausReadDecorKey", "JetETMissNeutralParticleFlowObjects.FE_TauLinks", "Key for links from neutral FE to taus"};
 
 };
 
