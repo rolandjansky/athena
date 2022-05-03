c04f2f065d539ffe816c78abf67f3f283ab8818a
Scott Snyder <scott.snyder@cern.ch>
2022-05-03 09:47:56 +0200

    TrigMissingEtEvent: Fix cppcheck warnings.

--- Trigger/TrigEvent/TrigMissingEtEvent/src/TrigMissingEtComponent.cxx
+++ Trigger/TrigEvent/TrigMissingEtEvent/src/TrigMissingEtComponent.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 // ********************************************************************
@@ -18,8 +18,9 @@
 #include <math.h>
 #include <stdio.h>
 
-TrigMissingEtComponent::TrigMissingEtComponent(){
-  m_name = "empty";
+TrigMissingEtComponent::TrigMissingEtComponent()
+  : m_name ("empty")
+{
   reset();
 }
 
