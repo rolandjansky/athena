215e3196fd5f24dfe5c0e6c63187b93195781848
Yi Yu <yi.yu@cern.ch>
2022-05-04 12:51:39 +0200

    sTGC Strip Trigger: implement the physical cluster formation logics with adjacent strips in the same layer

--- Trigger/TrigT1/TrigT1NSWSimTools/src/StripClusterTool.cxx
+++ Trigger/TrigT1/TrigT1NSWSimTools/src/StripClusterTool.cxx
@@ -6,13 +6,13 @@
 
 namespace NSWL1 {
 
-    StripClusterTool::StripClusterTool( const std::string& type, const std::string& name, const IInterface* parent) :
-      AthAlgTool(type,name,parent),
-      m_detManager(nullptr),
-      m_tree(nullptr)
-    {
-      declareInterface<NSWL1::IStripClusterTool>(this);
-    }
+  StripClusterTool::StripClusterTool( const std::string& type, const std::string& name, const IInterface* parent) :
+    AthAlgTool(type,name,parent),
+    m_detManager(nullptr),
+    m_tree(nullptr)
+  {
+    declareInterface<NSWL1::IStripClusterTool>(this);
+  }
 
   StatusCode StripClusterTool::initialize() {
     ATH_MSG_DEBUG( "initializing " << name() );
@@ -87,106 +87,106 @@ namespace NSWL1 {
     return StatusCode::SUCCESS;
   }
 
-    StatusCode StripClusterTool::book_branches() {
-      m_cl_n= 0;
-      m_cl_charge = new std::vector< int >();
-      m_cl_size = new std::vector< int >();
-      m_cl_x= new std::vector< float >();
-      m_cl_y= new std::vector< float >();
-      m_cl_z= new std::vector< float >();
-      m_cl_lx= new std::vector< float >();
-      m_cl_ly= new std::vector< float >();
-      m_cl_lz= new std::vector< float >();
-      m_cl_ltgx= new std::vector< float >();
-      m_cl_ltgy= new std::vector< float >();
-      m_cl_ltgz= new std::vector< float >();
-      m_cl_truth_x= new std::vector<float >();
-      m_cl_truth_y= new std::vector<float >();
-      m_cl_truth_z= new std::vector<float>();
-      m_cl_truth_lx= new std::vector<float >();
-      m_cl_truth_ly= new std::vector<float >();
-      m_cl_truth_lz= new std::vector<float>();
-      m_cl_truth_E= new std::vector<float >();
-      m_cl_truth_n= new std::vector<int >();
-      m_cl_side= new std::vector<int>();
-      m_cl_isSmall= new std::vector<int>();
-      m_cl_wedge= new std::vector<int>();
-      m_cl_sector= new std::vector<int>();
-      m_cl_module= new std::vector<int>();
-      m_cl_layer= new std::vector<int>();
-      m_cl_bandId= new std::vector<int>();
-      m_cl_phiId= new std::vector<int>();
+  StatusCode StripClusterTool::book_branches() {
+    m_cl_n= 0;
+    m_cl_charge = new std::vector< int >();
+    m_cl_size = new std::vector< int >();
+    m_cl_x= new std::vector< float >();
+    m_cl_y= new std::vector< float >();
+    m_cl_z= new std::vector< float >();
+    m_cl_lx= new std::vector< float >();
+    m_cl_ly= new std::vector< float >();
+    m_cl_lz= new std::vector< float >();
+    m_cl_ltgx= new std::vector< float >();
+    m_cl_ltgy= new std::vector< float >();
+    m_cl_ltgz= new std::vector< float >();
+    m_cl_truth_x= new std::vector<float >();
+    m_cl_truth_y= new std::vector<float >();
+    m_cl_truth_z= new std::vector<float>();
+    m_cl_truth_lx= new std::vector<float >();
+    m_cl_truth_ly= new std::vector<float >();
+    m_cl_truth_lz= new std::vector<float>();
+    m_cl_truth_E= new std::vector<float >();
+    m_cl_truth_n= new std::vector<int >();
+    m_cl_side= new std::vector<int>();
+    m_cl_isSmall= new std::vector<int>();
+    m_cl_wedge= new std::vector<int>();
+    m_cl_sector= new std::vector<int>();
+    m_cl_module= new std::vector<int>();
+    m_cl_layer= new std::vector<int>();
+    m_cl_bandId= new std::vector<int>();
+    m_cl_phiId= new std::vector<int>();
 
-      if (m_tree) {
-        std::string ToolName = name().substr(  name().find("::")+2,std::string::npos );
-        const char* n = ToolName.c_str();
-        m_tree->Branch(TString::Format("%s_cl_n",n).Data(),&m_cl_n,TString::Format("%s_cl_n/i",n).Data());
-        m_tree->Branch(TString::Format("%s_cl_charge",n).Data(),&m_cl_charge);
-        m_tree->Branch(TString::Format("%s_cl_x",n).Data(),&m_cl_x);
-        m_tree->Branch(TString::Format("%s_cl_y",n).Data(),&m_cl_y);
-        m_tree->Branch(TString::Format("%s_cl_z",n).Data(),&m_cl_z);
-        m_tree->Branch(TString::Format("%s_cl_lx",n).Data(),&m_cl_lx);
-        m_tree->Branch(TString::Format("%s_cl_ly",n).Data(),&m_cl_ly);
-        m_tree->Branch(TString::Format("%s_cl_lz",n).Data(),&m_cl_lz);
-        m_tree->Branch(TString::Format("%s_cl_ltgx",n).Data(),&m_cl_ltgx);
-        m_tree->Branch(TString::Format("%s_cl_ltgy",n).Data(),&m_cl_ltgy);
-        m_tree->Branch(TString::Format("%s_cl_ltgz",n).Data(),&m_cl_ltgz);
-        m_tree->Branch(TString::Format("%s_cl_size",n).Data(),&m_cl_size);
-        m_tree->Branch(TString::Format("%s_cl_isSmall",n).Data(),&m_cl_isSmall);
-        m_tree->Branch(TString::Format("%s_cl_side",n).Data(),&m_cl_side);
-        m_tree->Branch(TString::Format("%s_cl_wedge",n).Data(),&m_cl_wedge);
-        m_tree->Branch(TString::Format("%s_cl_sector",n).Data(),&m_cl_sector);
-        m_tree->Branch(TString::Format("%s_cl_module",n).Data(),&m_cl_module);
-        m_tree->Branch(TString::Format("%s_cl_layer",n).Data(),&m_cl_layer);
-        m_tree->Branch(TString::Format("%s_cl_bandId",n).Data(),&m_cl_bandId);
-        m_tree->Branch(TString::Format("%s_cl_phiId",n).Data(),&m_cl_phiId);
-        m_tree->Branch(TString::Format("%s_cl_truth_x",n).Data(),&m_cl_truth_x);
-        m_tree->Branch(TString::Format("%s_cl_truth_y",n).Data(),&m_cl_truth_y);
-        m_tree->Branch(TString::Format("%s_cl_truth_z",n).Data(),&m_cl_truth_z);
-        m_tree->Branch(TString::Format("%s_cl_truth_lx",n).Data(),&m_cl_truth_lx);
-        m_tree->Branch(TString::Format("%s_cl_truth_ly",n).Data(),&m_cl_truth_ly);
-        m_tree->Branch(TString::Format("%s_cl_truth_lz",n).Data(),&m_cl_truth_lz);
-        m_tree->Branch(TString::Format("%s_cl_truth_E",n).Data(),&m_cl_truth_E);
-        m_tree->Branch(TString::Format("%s_cl_truth_n",n).Data(),&m_cl_truth_n);
-      }
-      return StatusCode::SUCCESS;
+    if (m_tree) {
+      std::string ToolName = name().substr(  name().find("::")+2,std::string::npos );
+      const char* n = ToolName.c_str();
+      m_tree->Branch(TString::Format("%s_cl_n",n).Data(),&m_cl_n,TString::Format("%s_cl_n/i",n).Data());
+      m_tree->Branch(TString::Format("%s_cl_charge",n).Data(),&m_cl_charge);
+      m_tree->Branch(TString::Format("%s_cl_x",n).Data(),&m_cl_x);
+      m_tree->Branch(TString::Format("%s_cl_y",n).Data(),&m_cl_y);
+      m_tree->Branch(TString::Format("%s_cl_z",n).Data(),&m_cl_z);
+      m_tree->Branch(TString::Format("%s_cl_lx",n).Data(),&m_cl_lx);
+      m_tree->Branch(TString::Format("%s_cl_ly",n).Data(),&m_cl_ly);
+      m_tree->Branch(TString::Format("%s_cl_lz",n).Data(),&m_cl_lz);
+      m_tree->Branch(TString::Format("%s_cl_ltgx",n).Data(),&m_cl_ltgx);
+      m_tree->Branch(TString::Format("%s_cl_ltgy",n).Data(),&m_cl_ltgy);
+      m_tree->Branch(TString::Format("%s_cl_ltgz",n).Data(),&m_cl_ltgz);
+      m_tree->Branch(TString::Format("%s_cl_size",n).Data(),&m_cl_size);
+      m_tree->Branch(TString::Format("%s_cl_isSmall",n).Data(),&m_cl_isSmall);
+      m_tree->Branch(TString::Format("%s_cl_side",n).Data(),&m_cl_side);
+      m_tree->Branch(TString::Format("%s_cl_wedge",n).Data(),&m_cl_wedge);
+      m_tree->Branch(TString::Format("%s_cl_sector",n).Data(),&m_cl_sector);
+      m_tree->Branch(TString::Format("%s_cl_module",n).Data(),&m_cl_module);
+      m_tree->Branch(TString::Format("%s_cl_layer",n).Data(),&m_cl_layer);
+      m_tree->Branch(TString::Format("%s_cl_bandId",n).Data(),&m_cl_bandId);
+      m_tree->Branch(TString::Format("%s_cl_phiId",n).Data(),&m_cl_phiId);
+      m_tree->Branch(TString::Format("%s_cl_truth_x",n).Data(),&m_cl_truth_x);
+      m_tree->Branch(TString::Format("%s_cl_truth_y",n).Data(),&m_cl_truth_y);
+      m_tree->Branch(TString::Format("%s_cl_truth_z",n).Data(),&m_cl_truth_z);
+      m_tree->Branch(TString::Format("%s_cl_truth_lx",n).Data(),&m_cl_truth_lx);
+      m_tree->Branch(TString::Format("%s_cl_truth_ly",n).Data(),&m_cl_truth_ly);
+      m_tree->Branch(TString::Format("%s_cl_truth_lz",n).Data(),&m_cl_truth_lz);
+      m_tree->Branch(TString::Format("%s_cl_truth_E",n).Data(),&m_cl_truth_E);
+      m_tree->Branch(TString::Format("%s_cl_truth_n",n).Data(),&m_cl_truth_n);
     }
+    return StatusCode::SUCCESS;
+  }
 
-    void StripClusterTool::clear_ntuple_variables() {
-      if ( m_tree==0 ) return;
+  void StripClusterTool::clear_ntuple_variables() {
+    if ( m_tree==0 ) return;
 
-      //clear the ntuple variables
-      m_cl_n = 0;
-      m_cl_charge->clear();
-      m_cl_x->clear();
-      m_cl_y->clear();
-      m_cl_z->clear();
-      m_cl_lx->clear();
-      m_cl_ly->clear();
-      m_cl_lz->clear();
-      m_cl_ltgx->clear();
-      m_cl_ltgy->clear();
-      m_cl_ltgz->clear();
-      m_cl_size->clear();
-      m_cl_truth_x->clear();
-      m_cl_truth_y->clear();
-      m_cl_truth_z->clear();
-      m_cl_truth_E->clear();
-      m_cl_truth_n->clear();
-      m_cl_truth_lx->clear();
-      m_cl_truth_ly->clear();
-      m_cl_truth_lz->clear();
-      m_cl_side->clear();
-      m_cl_isSmall->clear();
-      m_cl_wedge->clear();
-      m_cl_sector->clear();
-      m_cl_module->clear();
-      m_cl_layer->clear();
-      m_cl_bandId->clear();
-      m_cl_phiId->clear();
-    }
+    //clear the ntuple variables
+    m_cl_n = 0;
+    m_cl_charge->clear();
+    m_cl_x->clear();
+    m_cl_y->clear();
+    m_cl_z->clear();
+    m_cl_lx->clear();
+    m_cl_ly->clear();
+    m_cl_lz->clear();
+    m_cl_ltgx->clear();
+    m_cl_ltgy->clear();
+    m_cl_ltgz->clear();
+    m_cl_size->clear();
+    m_cl_truth_x->clear();
+    m_cl_truth_y->clear();
+    m_cl_truth_z->clear();
+    m_cl_truth_E->clear();
+    m_cl_truth_n->clear();
+    m_cl_truth_lx->clear();
+    m_cl_truth_ly->clear();
+    m_cl_truth_lz->clear();
+    m_cl_side->clear();
+    m_cl_isSmall->clear();
+    m_cl_wedge->clear();
+    m_cl_sector->clear();
+    m_cl_module->clear();
+    m_cl_layer->clear();
+    m_cl_bandId->clear();
+    m_cl_phiId->clear();
+  }
 
-  StatusCode StripClusterTool::fill_strip_validation_id(std::vector<std::unique_ptr<StripClusterData>>& clusters,
+  StatusCode StripClusterTool::fill_strip_validation_id(std::vector<std::unique_ptr<StripClusterData>>& clusters, 
                                                         std::vector<std::shared_ptr<std::vector<std::unique_ptr<StripData> >>  > &cluster_cache) {
 
     ATH_MSG_DEBUG("Cluster cache received " << cluster_cache.size());
@@ -302,6 +302,15 @@ namespace NSWL1 {
         ATH_MSG_DEBUG("Channel " << strip_cl->channelId());
 
       }//end of this_cl loop
+
+      if ( abs(x_pos/charge)<200 && abs(y_pos/charge)<200){
+        ATH_MSG_WARNING("Cluster ------------------------------------------" );
+        ATH_MSG_WARNING("Cluster strip charge: " << charge );
+        ATH_MSG_WARNING("Cluster strip glob X: " << x_pos << x_pos/charge);
+        ATH_MSG_WARNING("Cluster strip glob Y: " << y_pos << y_pos/charge);
+        ATH_MSG_WARNING("Cluster strip glob Z: " << z_pos << z_pos/charge);
+      }
+    
       if (charge != 0){
         x_pos=x_pos/charge;
         y_pos=y_pos/charge;
@@ -362,84 +371,90 @@ namespace NSWL1 {
         ATH_MSG_WARNING("Received 0 strip hits... Skip event");
         return StatusCode::SUCCESS;
       }
+      
+      std::map<uint32_t, std::vector<std::unique_ptr<StripData>> > stripMap;
+      // Filter by layer:
+      for (auto& st : strips) {
+        // sideId [0, 1]
+        auto sideId = st->sideId();
+        // sectorType: small==0, large==1
+        auto sectorType = st->sectorType();
+        // sectorId [1,8]
+        auto sectorId = st->sectorId();
+        // etaId [1,3]
+        auto etaId = st->moduleId();
+        // wedgeId [1,2]
+        auto wedgeId = st->wedge();
+        // layer [1,4]
+        auto layerId = st->layer();
+        
+        // add 1 as the first placeholder, hence cache_hash always has 7 digits
+        std::string id_str = std::to_string(1)+std::to_string(sideId)+std::to_string(sectorType)+std::to_string(sectorId)+std::to_string(etaId)+std::to_string(wedgeId)+std::to_string(layerId);
+        const uint32_t cache_hash = atoi(id_str.c_str()); 
 
-      //S.I sort strip w.r.t channelId in ascending order
-      std::sort(strips.begin(), strips.end(), [](const auto& s1,const auto& s2) { return s1->channelId()<s2->channelId(); });
-
-      /*S.I remove duplicate strip channels ...
-       The rightmost way is to define an == operator for stripdata// not sure if we need to include charge+time and such though
-       Im sure somehow we get duplicated strip channels. Need to check if all the other aspects like charge, time, etc are exactly the same for those ...
-      */
-      // TODO
-      // auto pos = std::unique(strips.begin(), strips.end(),
-      //                        [](const auto& s1, const auto& s2){
-      //                            return s1->channelId()==s2->channelId() && s1->channelId()==s2->channelId();
-      //                        });
-
-      // strips.resize(std::distance(strips.begin(), pos));
-      auto hit=strips.begin();
-
-      auto cr_cluster=std::make_shared< std::vector<std::unique_ptr<StripData>> >();
-
-      StripData* prev_hit=nullptr;
-      int first_ch=(*hit)->channelId();//channel id of the first strip
-      ATH_MSG_DEBUG("Cluster Hits :" << (*hit)->channelId() << " " << m_idHelperSvc->stgcIdHelper().gasGap( (*hit)->Identity())
-                    << "   " << (*hit)->moduleId() << "   " << (*hit)->sectorId() << "   " << (*hit)->wedge() << "  " << (*hit)->sideId() );
-      // hit++;//S.I is this ncessary ?
-
-      std::vector< std::shared_ptr<std::vector<std::unique_ptr<StripData> >>  > cluster_cache;
-      for(auto & this_hit : strips){
-        if(!(this_hit)->readStrip() ) continue;
-        if( ((this_hit)->bandId()==-1 || this_hit->phiId()==-1) ){
-          ATH_MSG_WARNING("Read Strip without BandId :" << (this_hit)->channelId() << " " << m_idHelperSvc->stgcIdHelper().gasGap( (this_hit)->Identity())
-                          << "   " << (this_hit)->moduleId() << "   " << (this_hit)->sectorId() << "   " << (this_hit)->wedge() << "  " << (this_hit)->sideId() );
-          continue;
+        auto it = stripMap.find(cache_hash);
+        if (it != stripMap.end()){
+          it->second.push_back(std::move(st));
+        }
+        else{
+          stripMap[cache_hash].push_back(std::move(st));
         }
 
-        bool sameMod=false;
-        if (!prev_hit){//for the first time...
-          prev_hit=this_hit.get();
-          cr_cluster->push_back(std::move(this_hit));
-          continue;
-        }
-        else{ //executed when we are in the second strip and so on...
-          static auto MatchHits=[](const auto& a1,const auto& a2){
-            if( a1->sideId() != a2->sideId() ||
-                a1->sectorId() !=a2->sectorId() ||
-                a1->isSmall() !=a2->isSmall() ||
-                a1->moduleId() !=a2->moduleId() ||
-                a1->wedge() !=a2->wedge() ||
-                a1->layer() !=a2->layer()
-            ) return false;
-            return true;
-          };
-          sameMod=MatchHits(this_hit,prev_hit);
-        }
-        prev_hit=this_hit.get();
-        int this_chid=(this_hit)->channelId();
-        if ( (this_hit->channelId()<first_ch) && sameMod ) {
-          ATH_MSG_ERROR("Hits Ordered incorrectly!!!" );
-          // return StatusCode::FAILURE;
-        }
-        if ((this_hit)->channelId()==first_ch && sameMod){
-          ATH_MSG_ERROR("Hits entered twice !!! ChannelId: " << (this_hit)->channelId() );
-          // return StatusCode::FAILURE;
+      }
+          
+      for (auto &item : stripMap) {
+        std::vector<std::unique_ptr<StripData>>& stripList = item.second;
+      
+        //S.I sort strip w.r.t channelId in ascending order
+        std::sort(stripList.begin(), stripList.end(), [](const auto& s1,const auto& s2) { return s1->channelId()<s2->channelId(); });
+
+        auto hit=stripList.begin();
+        ATH_MSG_DEBUG("Cluster Hits :" << (*hit)->channelId() << " " << m_idHelperSvc->stgcIdHelper().gasGap( (*hit)->Identity())
+                      << "   " << (*hit)->moduleId() << "   " << (*hit)->sectorId() << "   " << (*hit)->wedge() << "  " << (*hit)->sideId() );
+        int first_ch=(*hit)->channelId();//channel id of the first strip
+        int prev_ch=-1;
+
+        auto cr_cluster=std::make_shared< std::vector<std::unique_ptr<StripData>> >();
+        std::vector< std::shared_ptr<std::vector<std::unique_ptr<StripData> >>  > cluster_cache;
+
+        for(auto& this_hit : stripList){
+          if(!(this_hit)->readStrip() ) continue;
+          if( ((this_hit)->bandId()==-1 || this_hit->phiId()==-1) ){
+            ATH_MSG_WARNING("Read Strip without BandId :" << (this_hit)->channelId() << " " << m_idHelperSvc->stgcIdHelper().gasGap( (this_hit)->Identity())
+                            << "   " << (this_hit)->moduleId() << "   " << (this_hit)->sectorId() << "   " << (this_hit)->wedge() << "  " << (this_hit)->sideId() );
+            continue;
+          }
+
+          if (first_ch==(this_hit)->channelId()){//for the first time...
+            prev_ch = first_ch;
+            cr_cluster->push_back(std::move(this_hit));
+            continue;
+          }
+                 
+          int this_ch=(this_hit)->channelId();
+          if ( (this_ch < prev_ch)) {
+            ATH_MSG_ERROR("Hits Ordered incorrectly!!!" );
+            return StatusCode::FAILURE;
+          }
+
+          if (this_ch == prev_ch || this_ch == prev_ch+1) cr_cluster->push_back(std::move(this_hit)); // form cluster with adjacent +-1 strips 
+          else {
+            cluster_cache.push_back(std::move(cr_cluster));                         //put the current cluster into the clusters buffer
+            cr_cluster=std::make_shared<std::vector<std::unique_ptr<StripData>>>(); //create a new empty cluster and assign this hit as the first hit
+            cr_cluster->push_back(std::move(this_hit));
+          }
+
+          prev_ch=this_ch;
         }
 
-        // if ((this_hit)->channelId()==first_ch+1 && sameMod) cr_cluster->push_back(std::move(this_hit)); // form cluster with adjacent +-1 strips 
-        else {
-          cluster_cache.push_back(std::move(cr_cluster));                         //put the current cluster into the clusters buffer
-          cr_cluster=std::make_shared<std::vector<std::unique_ptr<StripData>>>(); //create a new empty cluster and assign this hit as the first hit
-          cr_cluster->push_back(std::move(this_hit));
-       }
-       first_ch=this_chid;
+        if(!cr_cluster->empty()) cluster_cache.push_back(std::move(cr_cluster));    //don't forget the last cluster in the loop
+
+        ATH_MSG_DEBUG("Found :" << cluster_cache.size() << " clusters");
+        ATH_CHECK(fill_strip_validation_id(clusters, cluster_cache));
+        cluster_cache.clear();
       }
 
-      if(!cr_cluster->empty()) cluster_cache.push_back(std::move(cr_cluster));    //don't forget the last cluster in the loop
-
-      ATH_MSG_DEBUG("Found :" << cluster_cache.size() << " clusters");
-      ATH_CHECK(fill_strip_validation_id(clusters, cluster_cache));
-      cluster_cache.clear();
       return StatusCode::SUCCESS;
   }
 }
+
