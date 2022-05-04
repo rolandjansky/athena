5121238af5fbf7cc80c054cb451bb7ccbbe21c3f
Nils Krumnack <krumnack@iastate.edu>
2022-05-04 07:54:13 -0500

    return a reference to an actual object in currentContext()
    So far it had returned a reference to a `nullptr` to avoid anybody
    somehow doing anything with it, but aparently by now gcc checks that
    at run-time during debug builds, so this will now provide an actual
    object and also provides a dummy class definition.  I added the later
    as in the past I had seen people define their own version of
    EventContext which would create problems when used with a concrete
    object, and this at least gives the compiler a chance to catch that.

--- Control/AthToolSupport/AsgTools/AsgTools/CurrentContext.h
+++ Control/AthToolSupport/AsgTools/AsgTools/CurrentContext.h
@@ -24,11 +24,31 @@ namespace Gaudi
     /// This is mostly meant so that you can call a function requiring
     /// an `EventContext` even if you yourself got called without one.
     /// This matches the signature from GaudiHive, but in AnalysisBase
-    /// this just returns a dummy reference (for now).
+    /// this just returns a reference to a dummy object (for now).
 
     const EventContext& currentContext ();
   }
 }
+
+/// \warn this is a dummy class, do not try to use it for anything,
+/// except passing arround a reference to it where needed.
+///
+/// This may be turned into a more meaningful class at some point, but
+/// for now, just get a reference to an `EventContext` object via
+/// `Gaudi::Hive::currentContext()`.
+
+class EventContext final
+{
+  // deleting these for now, as users should be passing these objects
+  // by reference.
+  EventContext (const EventContext&) noexcept = delete;
+  EventContext& operator = (const EventContext&) noexcept = delete;
+
+  // making this private, so that users can't instantiate their own
+  // EventContext.
+  EventContext () noexcept = default;
+  friend const EventContext& Gaudi::Hive::currentContext ();
+};
 
 #endif
 
