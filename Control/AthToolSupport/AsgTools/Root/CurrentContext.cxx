13ef5924a16d58bc54a8280d6927bd61cfb398d5
Walter Lampl <walter.lampl@cern.ch>
2022-05-05 11:32:35 +0200

    Merge branch 'current_context_object' into 'master'
    return a reference to an actual object in currentContext()
    
    See merge request atlas/athena!52887
    
    (cherry picked from commit beefe237243e86fd9c09c370d5490f3bf8051a58)
    
    5121238a return a reference to an actual object in currentContext()

--- Control/AthToolSupport/AsgTools/Root/CurrentContext.cxx
+++ Control/AthToolSupport/AsgTools/Root/CurrentContext.cxx
@@ -24,13 +24,8 @@ namespace Gaudi
   {
     const EventContext& currentContext ()
     {
-      // For now there is no actual EventContext class in AnalysisBase,
-      // so for now we return a null-reference.  Since there is
-      // currently no (legal) way to dereference an EventContext in
-      // AnalysisBase, so this is fine, and indeed should catch any case
-      // in which the user might somehow access this anyways.
-      static EventContext *result {nullptr};
-      return *result;
+      static const EventContext result;
+      return result;
     }
   }
 }
