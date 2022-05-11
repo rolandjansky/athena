9b2fb7984eaa966c306ba76a8ae63c92c4c6a6e9
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-05-11 09:29:47 +0200

    TrkGeometry: remove unused MsgStreamMember include

--- Tracking/TrkDetDescr/TrkGeometry/TrkGeometry/TrackingGeometry.h
+++ Tracking/TrkDetDescr/TrkGeometry/TrkGeometry/TrackingGeometry.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -20,7 +20,6 @@
 #include <map>
 // ATH_MSG macros
 #include "AthenaBaseComps/AthMsgStreamMacros.h"
-#include "AthenaKernel/MsgStreamMember.h"
 
 #include "CxxUtils/checker_macros.h"
 
