2d39896883e08b7ec7db05d6e75f4bd3256dba1f
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-05-11 09:57:35 +0200

    G4Utilities: remove unused MsgStreamMember include

--- Simulation/G4Utilities/G4UserActions/src/LooperKiller.h
+++ Simulation/G4Utilities/G4UserActions/src/LooperKiller.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef G4USERACTIONS_G4UA_LOOPERKILLER_H
@@ -8,7 +8,6 @@
 #include "GaudiKernel/ServiceHandle.h"
 
 // Infrastructure includes
-#include "AthenaKernel/MsgStreamMember.h"
 #include "G4UserSteppingAction.hh"
 #include "AthenaBaseComps/AthMessaging.h"
 
