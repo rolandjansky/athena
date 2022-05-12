a2fcfc6e16efb4bfcffa09e075ca1894c83378fe
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-05-12 16:44:31 +0200

    Update ThePEG.cmake

--- Projects/AthGeneration/externals/ThePEG.cmake
+++ Projects/AthGeneration/externals/ThePEG.cmake
@@ -1,8 +1,7 @@
-
 #
 # File specifying the location of ThePEG to use.
 #
 
-set( THEPEG_LCGVERSION 2.2.2 )
+set( THEPEG_LCGVERSION 2.2.3 )
 set( THEPEG_LCGROOT
    ${LCG_RELEASE_DIR}/MCGenerators/thepeg/${THEPEG_LCGVERSION}/${LCG_PLATFORM} )
