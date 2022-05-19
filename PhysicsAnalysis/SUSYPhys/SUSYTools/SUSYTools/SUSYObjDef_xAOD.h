1582fd7de3f5de1aacd4ad8f03dc2416d147b574
Marco Rimoldi <marco.rimoldi@cern.ch>
2022-05-19 19:30:35 +0200

    SUSYTools - Update SF electron
    SUSYTools - Update SF electron

--- PhysicsAnalysis/SUSYPhys/SUSYTools/SUSYTools/SUSYObjDef_xAOD.h
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/SUSYTools/SUSYObjDef_xAOD.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 // This file's extension implies that it's C, but it's really -*- C++ -*-.
