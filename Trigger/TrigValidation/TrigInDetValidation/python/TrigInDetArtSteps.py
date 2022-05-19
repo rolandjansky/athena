80a49c7c7c1e97aadfb9da78a5f0fee6fc430b39
Jonathan Long <longjon@umich.edu>
2022-05-19 07:25:12 +0200

    Raise thresholds to reduce LRT electron FTF  size
    Raise thresholds on electron lrt idperf chain to reduce size of FTF collection.  HLT_e5_idperf_loose_lrtloose_L1EM3 to HLT_e20_idperf_loose_lrtloose_L1EM15VH

--- Trigger/TrigValidation/TrigInDetValidation/python/TrigInDetArtSteps.py
+++ Trigger/TrigValidation/TrigInDetValidation/python/TrigInDetArtSteps.py
@@ -92,7 +92,7 @@ class TrigInDetReco(ExecStep):
                 chains += "'HLT_mu26_ivarperf_L1MU14FCH',"
                 flags += 'doMuonSlice=True;'
             if (i=='L2electronLRT') :
-                chains += "'HLT_e5_idperf_loose_lrtloose_L1EM3',"
+                chains += "'HLT_e20_idperf_loose_lrtloose_L1EM15VH',"
                 chains += "'HLT_e30_idperf_loose_lrtloose_L1EM22VHI',"
                 chains += "'HLT_e26_lhtight_ivarloose_e5_lhvloose_nopix_lrtloose_idperf_probe_L1EM22VHI',"
                 chains += "'HLT_e5_lhvloose_nopix_lrtloose_idperf_probe_g25_medium_L1EM20VH',"
