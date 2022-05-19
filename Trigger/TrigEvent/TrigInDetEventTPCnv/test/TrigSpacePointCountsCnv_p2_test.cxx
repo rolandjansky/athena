b3f38b84d76ddcc8afda8061c879dbeb7479d909
scott snyder <sss@karma>
2022-05-19 12:51:02 -0400

    TrigInDetEventTPCnv: Enable thread-safety checking
    Enable thread-safety checking and fix warnings for tests.

--- Trigger/TrigEvent/TrigInDetEventTPCnv/test/TrigSpacePointCountsCnv_p2_test.cxx
+++ Trigger/TrigEvent/TrigInDetEventTPCnv/test/TrigSpacePointCountsCnv_p2_test.cxx
@@ -1,8 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
-
-// $Id$
 /**
  * @file TrigInDetEventTPCnv/test/TrigSpacePointCountsCnv_p2_test.cxx
  * @author scott snyder <snyder@bnl.gov>
@@ -15,6 +13,7 @@
 #include "TrigInDetEventTPCnv/TrigSpacePointCountsCnv_p2.h"
 #include "TestTools/random.h"
 #include "TestTools/leakcheck.h"
+#include "CxxUtils/checker_macros.h"
 #include "GaudiKernel/MsgStream.h"
 #include <cassert>
 #include <iostream>
@@ -83,7 +82,7 @@ void testit (const TrigSpacePointCounts& trans1)
 }
 
 
-void test1()
+void test1 ATLAS_NOT_THREAD_SAFE ()
 {
   std::cout << "test1\n";
   Athena_test::Leakcheck check;
@@ -114,7 +113,7 @@ void test1()
 }
 
 
-int main()
+int main ATLAS_NOT_THREAD_SAFE ()
 {
   test1();
   return 0;
--- Trigger/TrigEvent/TrigInDetEventTPCnv/test/TrigTrtHitCountsCnv_p1_test.cxx
+++ Trigger/TrigEvent/TrigInDetEventTPCnv/test/TrigTrtHitCountsCnv_p1_test.cxx
@@ -1,8 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
-
-// $Id$
 /**
  * @file TrigInDetEventTPCnv/test/TrigTrtHitCountsCnv_p1_test.cxx
  * @author scott snyder <snyder@bnl.gov>
@@ -15,6 +13,7 @@
 #include "TrigInDetEventTPCnv/TrigTrtHitCountsCnv_p1.h"
 #include "TestTools/random.h"
 #include "TestTools/leakcheck.h"
+#include "CxxUtils/checker_macros.h"
 #include "GaudiKernel/MsgStream.h"
 #include <cassert>
 #include <iostream>
@@ -68,7 +67,7 @@ void testit (const TrigTrtHitCounts& trans1)
 }
 
 
-void test1()
+void test1 ATLAS_NOT_THREAD_SAFE ()
 {
   std::cout << "test1\n";
   Athena_test::Leakcheck check;
@@ -91,7 +90,7 @@ void test1()
 }
 
 
-int main()
+int main ATLAS_NOT_THREAD_SAFE ()
 {
   test1();
   return 0;
