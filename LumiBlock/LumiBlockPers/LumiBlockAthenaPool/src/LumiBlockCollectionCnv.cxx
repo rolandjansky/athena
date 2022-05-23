f6776e7ccdd8e22378160db1593437abcd61a2ef
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-05-23 16:15:25 +0200

    LumiBlockAthenaPool: enable thread-checker and fix warnings

--- LumiBlock/LumiBlockPers/LumiBlockAthenaPool/src/LumiBlockCollectionCnv.cxx
+++ LumiBlock/LumiBlockPers/LumiBlockAthenaPool/src/LumiBlockCollectionCnv.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -8,13 +8,11 @@
 /////////////////////////////////////////////////////////////////
 
 #include "LumiBlockCollectionCnv.h"
-#include "LumiBlockTPCnv/LumiBlockCollectionCnv_p1.h"
-#include "LumiBlockTPCnv/LumiBlockCollectionCnv_p2.h"
 
 
 LumiBlockCollection_PERS* LumiBlockCollectionCnv::createPersistent(LumiBlockCollection* transCont) {
   MsgStream log(msgSvc(), "LumiBlockCollectionConverter" );
-  LumiBlockCollection_PERS * persObj = m_TPConverter.createPersistent( transCont,   log );
+  LumiBlockCollection_PERS * persObj = m_converter.createPersistent( transCont,   log );
 
   log << MSG::DEBUG << "Success" << endmsg;
   return persObj; 
@@ -22,20 +20,16 @@ LumiBlockCollection_PERS* LumiBlockCollectionCnv::createPersistent(LumiBlockColl
     
 LumiBlockCollection* LumiBlockCollectionCnv::createTransient() {
   MsgStream log(msgSvc(), "LumiBlockCollectionConverter" );
-  static pool::Guid   p1_guid("CF1F40C9-6125-4C35-87FF-DDA2C319000C");
-  static pool::Guid   p2_guid("DEF9282A-F174-4382-8248-B94567CD869F");
-
-  static  LumiBlockCollectionCnv_p1   TPConverter_p1;
-  static  LumiBlockCollectionCnv_p2   TPConverter_p2;
-
+  static const pool::Guid   p1_guid("CF1F40C9-6125-4C35-87FF-DDA2C319000C");
+  static const pool::Guid   p2_guid("DEF9282A-F174-4382-8248-B94567CD869F");
 
   if( compareClassGuid(p2_guid) ) {
     std::unique_ptr< LumiBlockCollection_p2 > col_vect( poolReadObject< LumiBlockCollection_p2 >() );
-    return TPConverter_p2.createTransient( col_vect.get(), log );
+    return m_converter.createTransient( col_vect.get(), log );
   }
   else if( compareClassGuid(p1_guid) ) {
     std::unique_ptr< LumiBlockCollection_p1 > col_vect( poolReadObject< LumiBlockCollection_p1 >() );
-    return TPConverter_p1.createTransient( col_vect.get(), log );
+    return m_converter_p1.createTransient( col_vect.get(), log );
   }
   else {
     throw std::runtime_error("Unsupported persistent version of LumiBlockCollection");
