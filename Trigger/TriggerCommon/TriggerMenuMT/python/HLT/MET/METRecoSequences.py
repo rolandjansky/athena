5b95ba57c0139ea0725e865b3721df735904b9b6
Claire Antel <claire.antel@cern.ch>
2022-05-23 11:49:56 +0200

    Trigger Jet/Met: Removing Trigger.usexAODFlowElements flag and migrating fully to xAODtype FlowElements (ATR-24619)
    Trigger Jet/Met: Removing Trigger.usexAODFlowElements flag and migrating fully to xAODtype FlowElements (ATR-24619)

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLT/MET/METRecoSequences.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLT/MET/METRecoSequences.py
@@ -318,11 +318,7 @@ class MergedPFOInputConfig(AlgInputConfig):
     @staticmethod
     def getPFOPrepAlg(flags, **inputs):
         '''Alg generator for RecoFragmentsPool. Need to unpack dict as not hashable'''
-        from AthenaConfiguration.AllConfigFlags import ConfigFlags
-        if ConfigFlags.Trigger.usexAODFlowElements:
-            from TrigEFMissingET.TrigEFMissingETConf import HLT__MET__FlowElementPrepAlg as PrepAlg
-        else:
-          from TrigEFMissingET.TrigEFMissingETConf import HLT__MET__PFOPrepAlg as PrepAlg
+        from TrigEFMissingET.TrigEFMissingETConf import HLT__MET__FlowElementPrepAlg as PrepAlg
         return PrepAlg(
             f"{inputs['PFOPrefix']}METTrigPFOPrepAlg",
             InputNeutralKey=inputs["nPFOs"],
@@ -348,10 +344,7 @@ class MergedPFOInputConfig(AlgInputConfig):
         from AthenaConfiguration.ComponentFactory import CompFactory
 
         acc = ComponentAccumulator()
-        if flags.Trigger.usexAODFlowElements:
-            prepAlgType = CompFactory.getComp("HLT::MET::FlowElementPrepAlg")
-        else:
-            prepAlgType = CompFactory.getComp("HLT::MET::PFOPrepAlg")
+        prepAlgType = CompFactory.getComp("HLT::MET::FlowElementPrepAlg")
         alg = prepAlgType(
             f"{inputs['PFOPrefix']}METTrigPFOPrepAlg",
             InputNeutralKey=inputs["nPFOs"],
