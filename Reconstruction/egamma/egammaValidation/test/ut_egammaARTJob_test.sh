94fe8f27d8583ad8a581407de2dec7f803d79ead
Adam Edward Barton <adam.edward.barton@cern.ch>
2022-05-25 10:46:31 +0200

    Merge branch 'egValid' into 'master'
    Fix ART for photons
    
    See merge request atlas/athena!53620
    
    (cherry picked from commit 78bf54b44f16cc07942a30dd43df4d78b4695a35)
    
    78cb9cea small fixes

--- Reconstruction/egamma/egammaValidation/test/ut_egammaARTJob_test.sh
+++ Reconstruction/egamma/egammaValidation/test/ut_egammaARTJob_test.sh
@@ -2,7 +2,7 @@
 
 
 # Reco stage
-Reco_tf.py --steering doRAWtoALL --inputRDOFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/WorkflowReferences/master/q443/v1/myRDO.pool.root  --outputAODFile=Nightly_AOD_electron.pool.root --maxEvents=1 --autoConfiguration="everything" --preExec="from egammaValidation.egammaOnlyPreExec import setRunEgammaOnlyRecoFlags; setRunEgammaOnlyRecoFlags()" --postInclude "RAWtoALL:egammaValidation/egammaArtCaloCalPostInclude.py" "POOLMergeAthenaMPAOD0:egammaValidation/egammaArtCaloCalPostInclude.py" >>/dev/null 2>&1 
+Reco_tf.py --steering doRAWtoALL --inputRDOFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/WorkflowReferences/master/q443/v1/myRDO.pool.root  --outputAODFile=Nightly_AOD.pool.root --maxEvents=1 --autoConfiguration="everything" --preExec="from egammaValidation.egammaOnlyPreExec import setRunEgammaOnlyRecoFlags; setRunEgammaOnlyRecoFlags()" --postInclude "RAWtoALL:egammaValidation/egammaArtCaloCalPostInclude.py" "POOLMergeAthenaMPAOD0:egammaValidation/egammaArtCaloCalPostInclude.py" >>/dev/null 2>&1
 
 stat=$?
 if [ $stat -eq 0 ] 
@@ -15,13 +15,10 @@ else
 	exit $stat
 fi
 # rm files not needed anymore
-rm tmp.ESD >> /dev/null 2>&1
-rm log.RAWtoESD >> /dev/null 2>&1
-rm log.ESDtoAOD >> /dev/null 2>&1
-
+rm log.RAWtoALL >> /dev/null 2>&1
 
 # Merge stage
-AODMerge_tf.py --inputAODFile=Nightly_AOD_electron.pool.root --outputAOD_MRGFile=Nightly_AOD_electron.pool.root --preExec "from egammaValidation.egammaOnlyPreExec import setRunEgammaOnlyMergeFlags; setRunEgammaOnlyMergeFlags()" --postInclude "all:egammaValidation/egammaArtCaloCalPostInclude.py">>/dev/null 2>&1 
+AODMerge_tf.py --inputAODFile=Nightly_AOD.pool.root --outputAOD_MRGFile=Nightly_AOD.pool.root --preExec "from egammaValidation.egammaOnlyPreExec import setRunEgammaOnlyMergeFlags; setRunEgammaOnlyMergeFlags()" --postInclude "all:egammaValidation/egammaArtCaloCalPostInclude.py">>/dev/null 2>&1
 
 stat=$?
 if [ $stat -eq 0 ] 
@@ -36,45 +33,52 @@ fi
 # rm files not needed anymore
 rm log.AODMerge >> /dev/null 2>&1
 
-
-# Histo stage 
+# Histo stage
+rm -f PoolFileCatalog.xml
+ln -fs Nightly_AOD.pool.root Nightly_AOD_electron.pool.root
 get_files -jo egamma_art_checker_joboptions.py >> /dev/null 2>&1
 athena -c "particleType='electron'" egamma_art_checker_joboptions.py >> histo.log 2>&1
+state=$?
+rm -f PoolFileCatalog.xml
+ln -fs Nightly_AOD.pool.root Nightly_AOD_gamma.pool.root
+athena -c "particleType='gamma'" egamma_art_checker_joboptions.py >> histo.log 2>&1
+statg=$?
 
-stat=$?
-if [ $stat -eq 0 ] 
+if [ $state -eq 0 -a $statg -eq 0 ]
 then
 	echo "=== HISTO MAKER SUCCESS === "
 else
-	echo "==== HISTO MAKER FAILURE"
+	echo "==== HISTO MAKER FAILURE" $state $statg
 	echo " HISTO MAKER  log ===> "
 	cat  histo.log
-	exit $stat
+	exit 1
 fi
 
 
 # rm files not needed anymore
-rm Nightly_AOD_electron.pool.root >> /dev/null 2>&1
+rm -f Nightly_AOD*.pool.root >> /dev/null 2>&1
 rm histo.log  >> /dev/null 2>&1
 
 
 # Final plot stage 
 EgammaARTmonitoring_plotsMaker.py Nightly-monitoring_electron.hist.root Nightly-monitoring_electron.hist.root electron >> plot.log 2>&1
+state=$?
+EgammaARTmonitoring_plotsMaker.py Nightly-monitoring_gamma.hist.root Nightly-monitoring_gamma.hist.root gamma >> plot.log 2>&1
+statg=$?
 
-stat=$?
-if [ $stat -eq 0 ] 
+if [ $state -eq 0 -a $statg -eq 0 ]
 then
 	echo "=== PLOT MAKER SUCCESS === "
 else
-	echo "=== PLOT MAKER SUCCESS === "
+	echo "=== PLOT MAKER FAILURE === " $state $statg
 	echo " PLOT MAKER  log ===> "
 	cat plot.log
-	exit $stat
+	exit 1
 fi
 
 # rm files not needed anymore
 rm *.png >> /dev/null 2>&1
-rm Nightly-monitoring_electron.hist.root >> /dev/null 2>&1
-rm BN_ComparisonPlots_electron.hist.root >>  /dev/null 2>&1
+rm Nightly-monitoring_*.hist.root >> /dev/null 2>&1
+rm BN_ComparisonPlots_*.hist.root >>  /dev/null 2>&1
 rm plot.log >>  /dev/null 2>&1
 
