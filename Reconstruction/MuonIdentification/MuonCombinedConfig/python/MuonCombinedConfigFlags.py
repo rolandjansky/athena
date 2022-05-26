adc0ab1350be42ceb6353cdcb2460c0899f54af3
Edward Moyse <edward.moyse@cern.ch>
2022-05-26 19:20:21 +0200

    Missing Muon collections from ESD/AOD, and other CA configuration fixes
    Missing configuration and fix for doSegmentT0
    Add missing MuonSegmentCnvAlg_NCB to MuonSegmentFindingCfg
    AndDCMathSegmentMakerCfg: handle doSegmentT0 properly

--- Reconstruction/MuonIdentification/MuonCombinedConfig/python/MuonCombinedConfigFlags.py
+++ Reconstruction/MuonIdentification/MuonCombinedConfig/python/MuonCombinedConfigFlags.py
@@ -17,7 +17,7 @@ def createMuonCombinedConfigFlags():
     # Switch on/off algorithms that make Muons for the CaloMuonCollection
     mcf.addFlag("MuonCombined.doCaloTrkMuId",True) 
     # Switch on/off algorithms that make Muons for the MuGirlLowBetaMuonCollection         
-    mcf.addFlag("MuonCombined.doMuGirlLowBeta",lambda prevFlags : prevFlags.doMuGirl is True )
+    mcf.addFlag("MuonCombined.doMuGirlLowBeta",lambda prevFlags : prevFlags.MuonCombined.doMuGirl)
     mcf.addFlag("MuonCombined.writeUnAssocSegments", True)
 
     return mcf
