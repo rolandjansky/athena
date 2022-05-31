98fc13b2b030593275096cc0ce0e856386ed8c57
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2022-05-31 17:49:21 +0200

    AtlasExtrapolatorToolsConfig add comments and tidy
    AtlasExtrapolatorToolsConfig add comments and tidy

--- Tracking/TrkConfig/python/AtlasExtrapolatorToolsConfig.py
+++ Tracking/TrkConfig/python/AtlasExtrapolatorToolsConfig.py
@@ -1,47 +1,61 @@
 # Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 
-# Configuration of tools for ATLAS extrapolator
+# Configuration of tools needed by the Extrapolator
 
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ComponentFactory import CompFactory
 
 
+# Navigator used in order to  handle the
+# navigation inside the Tracking Geometry
+# i.e search for the next TrackingVolume
 def AtlasNavigatorCfg(flags,
                       name='AtlasNavigator',
                       **kwargs):
     # get the correct TrackingGeometry setup
     result = ComponentAccumulator()
     if 'TrackingGeometryKey' not in kwargs:
-        from TrackingGeometryCondAlg.AtlasTrackingGeometryCondAlgConfig import TrackingGeometryCondAlgCfg
+        from TrackingGeometryCondAlg.AtlasTrackingGeometryCondAlgConfig import (
+            TrackingGeometryCondAlgCfg)
         acc = TrackingGeometryCondAlgCfg(flags)
         geom_cond_key = acc.getPrimary().TrackingGeometryWriteKey
         result.merge(acc)
         kwargs.setdefault("TrackingGeometryKey", geom_cond_key)
-
-    # the UNIQUE NAVIGATOR ( === UNIQUE GEOMETRY) ------------------------
-    result.setPrivateTools(CompFactory.Trk.Navigator(name,**kwargs))
+    result.setPrivateTools(CompFactory.Trk.Navigator(name, **kwargs))
     return result
 
+
+# Configured Energy Loss  uses the TrkUtils parametrization for
+# energy loss due to ionization and radiation
 def AtlasEnergyLossUpdatorCfg(flags,
                               name='AtlasEnergyLossUpdator',
                               **kwargs):
     result = ComponentAccumulator()
+    kwargs.setdefault("UseTrkUtils", True)
     kwargs.setdefault("UseBetheBlochForElectrons", False)
     result.setPrivateTools(CompFactory.Trk.EnergyLossUpdator(name, **kwargs))
     return result
 
+
+# Configured Updators for Layer based material effects
+# by default applies Multiple Scattering and
+# Energy Loss.
 def AtlasMaterialEffectsUpdatorCfg(flags,
                                    name='AtlasMaterialEffectsUpdator',
                                    **kwargs):
     result = ComponentAccumulator()
-    kwargs.setdefault("EnergyLossUpdator", result.popToolsAndMerge(AtlasEnergyLossUpdatorCfg(flags)))
-    result.setPrivateTools(CompFactory.Trk.MaterialEffectsUpdator(name, **kwargs))
+    kwargs.setdefault("EnergyLossUpdator", result.popToolsAndMerge(
+        AtlasEnergyLossUpdatorCfg(flags)))
+    result.setPrivateTools(
+        CompFactory.Trk.MaterialEffectsUpdator(name, **kwargs))
     return result
 
 
 def AtlasMaterialEffectsUpdatorLandauCfg(flags,
                                          name='AtlasMaterialEffectsUpdatorLandau',
                                          **kwargs):
+    # Landau most probable energy loss is not additive
+    # special treatment on how we accumulate
     kwargs.setdefault("LandauMode", True)
     return AtlasMaterialEffectsUpdatorCfg(flags, name, **kwargs)
 
