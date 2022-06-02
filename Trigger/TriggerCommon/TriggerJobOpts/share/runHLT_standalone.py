b00577019caee60849de6f0aad4a78f9e4aea988
Masato Aoki <masato.aoki@cern.ch>
2022-06-02 16:21:26 +0200

    bug fix of TGC-BIS78 coindicence, and activation

--- Trigger/TriggerCommon/TriggerJobOpts/share/runHLT_standalone.py
+++ Trigger/TriggerCommon/TriggerJobOpts/share/runHLT_standalone.py
@@ -50,7 +50,7 @@ class opt:
     enableL1NSWMMTrigger = True       # Enable MM trigger for TGC-NSW coincidence : ConfigFlags.Trigger.L1MuonSim.doMMTrigger
     enableL1NSWPadTrigger = True      # Enable sTGC Pad trigger for TGC-NSW coincidence : ConfigFlags.Trigger.L1MuonSim.doPadTrigger
     enableL1NSWStripTrigger = False   # Enable sTGC Strip trigger for TGC-NSW coincidence : ConfigFlags.Trigger.L1MuonSim.doStripTrigger
-    enableL1RPCBIS78    = False       # Enable TGC-RPC BIS78 coincidence : ConfigFlags.Trigger.L1MuonSim.doBIS78
+    enableL1RPCBIS78    = True        # Enable TGC-RPC BIS78 coincidence : ConfigFlags.Trigger.L1MuonSim.doBIS78
 #Individual slice flags
     doCalibSlice        = True
     doTestSlice         = True
