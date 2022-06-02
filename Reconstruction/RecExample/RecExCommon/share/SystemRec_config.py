d5466cf0a24ba527edd96efa17d7b17164c18786
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-06-02 14:02:49 +0200

    RecExCommon: cleanup usage of CaloRecoCfg
    RecExCommon: cleanup usage of CaloRecoCfg
    
    Remove the "TopAlg" sequence setting when using `CaloRecCfg`. This is
    not needed as the default sequence (AthAlgSeq) is equivalent.

--- Reconstruction/RecExample/RecExCommon/share/SystemRec_config.py
+++ Reconstruction/RecExample/RecExCommon/share/SystemRec_config.py
@@ -1,9 +1,12 @@
-from AthenaCommon.Logging import logging
-mlog = logging.getLogger( 'SystemRec_config' )
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 
-from AthenaCommon.GlobalFlags  import globalflags
+# make the logger known to flake8:
+logRecExCommon_topOptions = logRecExCommon_topOptions  # noqa: F821 (from RecExCommon_topOptions.py)
+
+from AthenaCommon.DetFlags import DetFlags
+from AthenaCommon.GlobalFlags import globalflags
+from AthenaCommon.JobProperties import jobproperties
 from RecExConfig.RecFlags import rec
-from RecExConfig.RecAlgsFlags import recAlgs
 
 from AthenaCommon.Resilience import treatException,protectedInclude
 from AthenaConfiguration.ComponentAccumulator import CAtoGlobalWrapper
@@ -11,70 +14,47 @@ from AthenaConfiguration.ComponentAccumulator import CAtoGlobalWrapper
 # use to flag domain
 import PerfMonComps.DomainsRegistry as pdr
 
-from AODFix.AODFix import *
-AODFix_Init()
-
-wrap_indet = True
+from AODFix import AODFix
+AODFix.AODFix_Init()
 
 #First do Calo-Reco
 pdr.flag_domain('calo')
 if DetFlags.detdescr.Calo_on() and rec.doESD():
-    try:
-        from AthenaCommon.Configurable import Configurable
-        Configurable.configurableRun3Behavior=1
-        from AthenaConfiguration.ComponentAccumulator import appendCAtoAthena
-        from AthenaConfiguration.AllConfigFlags import ConfigFlags
-        from CaloRec.CaloRecoConfig import CaloRecoCfg
-        ca=CaloRecoCfg(ConfigFlags)
-    
-        for el in ca._allSequences:
-            el.name = "TopAlg"
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from CaloRec.CaloRecoConfig import CaloRecoCfg
+    CAtoGlobalWrapper(CaloRecoCfg, ConfigFlags)
 
-            appendCAtoAthena(ca)
-    except Exception:
-        treatException("Could translate CaloRecoCfg into athena")
-    finally:
-        Configurable.configurableRun3Behavior=0
-    pass
-
-    
     #Move L1Calo Trigger tower decoration here for simplicity:
     if globalflags.DataSource()=='data' and rec.doESD() and rec.doCalo() and rec.doTrigger():
         include("TrigT1CaloCalibTools/DecorateL1CaloTriggerTowers_prodJobOFragment.py")
 
 
-AODFix_postCaloRec()
+AODFix.AODFix_postCaloRec()
 
 #make the egammaTopoClusters containers, used for seeding
 if jobproperties.CaloRecFlags.doCaloTopoCluster():
     from AthenaConfiguration.AllConfigFlags import ConfigFlags
     from egammaAlgs.egammaTopoClusterCopierConfig import egammaTopoClusterCopierCfg
-    CAtoGlobalWrapper(egammaTopoClusterCopierCfg,ConfigFlags,**HIDict)
+    CAtoGlobalWrapper(egammaTopoClusterCopierCfg,ConfigFlags,**HIDict)  # noqa: F821 (HIDict from RecExCommon_topOptions.py)
 
 #then run ID reco:
 
 pdr.flag_domain('id')
 if DetFlags.detdescr.ID_on():
     if jobproperties.InDetJobProperties.useNewConfig():
-        print('Wrapping InDet new configuration')
+        logRecExCommon_topOptions.info('Wrapping InDet new configuration')
         from AthenaConfiguration.AllConfigFlags import ConfigFlags
         from InDetConfig.TrackRecoConfig import InDetTrackRecoCfg
         CAtoGlobalWrapper(InDetTrackRecoCfg,ConfigFlags)
     else:
         protectedInclude( "InDetRecExample/InDetRec_jobOptions.py" )
-        AODFix_postInDetRec()
+        AODFix.AODFix_postInDetRec()
 
-# functionality : FTK reconstruction
-if DetFlags.detdescr.FTK_on() :
-    protectedInclude( "FTK_RecExample/FTKRec_jobOptions.py")
-         
-        
 # functionality : Muon reconstruction
 pdr.flag_domain('muon')
 if DetFlags.detdescr.Muon_on() :
     try:
-        from RecExConfig.RecConfFlags import recConfFlags
-        from AthenaCommon.AthenaCommonFlags  import athenaCommonFlags
+        from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
         from MuonRecExample.MuonRecFlags import muonRecFlags
         muonRecFlags.applyResilience = athenaCommonFlags.AllowIgnoreConfigError()
     except Exception:
@@ -85,7 +65,7 @@ if DetFlags.detdescr.Muon_on() :
 #    4 the essential collision reco 3 high priority 2 medium priodity 1 nice to have
 
     if  muonRecFlags.useNewConfig():
-        print('Wrapping Muon new configuration')
+        logRecExCommon_topOptions.info('Wrapping Muon new configuration')
         from AthenaConfiguration.AllConfigFlags import ConfigFlags
         from MuonConfig.MuonReconstructionConfig import MuonReconstructionCfg
         # TODO Keep here for the moment, since we still have debugging to do.
@@ -104,7 +84,7 @@ if DetFlags.detdescr.Muon_on() :
 else:
     from MuonRecExample.MuonRecFlags import muonRecFlags
     muonRecFlags.doStandalone=False
-AODFix_postMuonRec()
+AODFix.AODFix_postMuonRec()
 
     
 pdr.flag_domain('forward')
@@ -116,5 +96,4 @@ if DetFlags.detdescr.Forward_on():
     except Exception:
         treatException("Problem including ForwardRec/ForwardRec_jobOptions.py !!")
 
-AODFix_postForwardRec()
-
+AODFix.AODFix_postForwardRec()
