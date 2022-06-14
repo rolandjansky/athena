9f1283f99bfcd1c7fa7b310239b0fe6e0f586786
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2022-06-14 11:26:24 +0200

    SourceCompAlg: removed hardwired setting for the Supported Geometry flag
    This change is required to fix the geometry database consistency checker
    which is run as part of the Athena installation procedure at Point 1.
    Now instead of having the supported geometry flag hardwired in the code
    the checker algorithm gets it from python configuration, consistently with
    how this is implemented for the GeoModelSvc

--- Database/RDBAccessSvc/share/jobOptions.CompareGeomDBSources.py
+++ Database/RDBAccessSvc/share/jobOptions.CompareGeomDBSources.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+
 # This job options compares geometry data from two sources
 #
 # In order run this script you need to define two sessions named Session0 and Session1
@@ -12,6 +14,8 @@
 # If no differences are found, then the log file will be empty
 #
 
+from AthenaCommon import Logging
+
 # Create an event selector:
 import AthenaCommon.AtlasUnixGeneratorJob
 
@@ -19,10 +23,18 @@ import AthenaCommon.AtlasUnixGeneratorJob
 from AthenaCommon.AppMgr import AppMgr as appMgr
 appMgr.EvtMax = 1
 
+# Set the SupportedGeometry flag
+from AthenaCommon.AppMgr import release_metadata
+rel_metadata = release_metadata()
+relversion = rel_metadata['release'].split('.')
+if len(relversion) < 3:
+    relversion = rel_metadata['base release'].split('.')
+
 # Configure the comparator algorithm
 from RDBAccessSvc.RDBAccessSvcConf import SourceCompAlg
 SourceCompAlg = SourceCompAlg()
-#SourceCompAlg.GlobalTag = "ATLAS-R2-2016-01-00-01"
+Logging.log.info("CompareGeomDBSource.py obtained major release version %s", relversion[0])
+SourceCompAlg.SupportedGeometry = relversion[0]
 
 # Add the comparator to the AlgSequence
 from AthenaCommon.AlgSequence import AlgSequence
