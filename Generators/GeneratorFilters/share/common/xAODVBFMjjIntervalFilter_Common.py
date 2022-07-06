cfe5a29b07cf86318c3467f8e151e4a42a848bd4
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2022-07-06 13:19:36 +0200

    correct xAODVBFMjjIntervalFilter_Common.py -add correct filter name
    correct xAODVBFMjjIntervalFilter_Common.py -add correct filter name

--- Generators/GeneratorFilters/share/common/xAODVBFMjjIntervalFilter_Common.py
+++ Generators/GeneratorFilters/share/common/xAODVBFMjjIntervalFilter_Common.py
@@ -15,21 +15,21 @@ xAODVBFMjjIntervalFilter = xAODVBFMjjIntervalFilter("xAODVBFMjjIntervalFilter")
 filtSeq += xAODVBFMjjIntervalFilter
 
 # to modify cuts put into JOs e.g.:
-filtSeq.VBFMjjIntervalFilter.RapidityAcceptance = 5.0
-filtSeq.VBFMjjIntervalFilter.MinSecondJetPT = 15.*GeV
-filtSeq.VBFMjjIntervalFilter.MinOverlapPT = 15.*GeV
-filtSeq.VBFMjjIntervalFilter.TruthJetContainerName = "AntiKt4TruthJets"
-filtSeq.VBFMjjIntervalFilter.NoJetProbability  = 1000.0*0.0000252
-filtSeq.VBFMjjIntervalFilter.OneJetProbability = 1000.0*0.0000252
-filtSeq.VBFMjjIntervalFilter.LowMjjProbability = 1000.0*0.0000252
-filtSeq.VBFMjjIntervalFilter.HighMjjProbability = 1.0
-filtSeq.VBFMjjIntervalFilter.LowMjj = 100.*GeV
-filtSeq.VBFMjjIntervalFilter.TruncateAtLowMjj = False
-filtSeq.VBFMjjIntervalFilter.HighMjj = 2500.*GeV
-filtSeq.VBFMjjIntervalFilter.TruncateAtHighMjj = False
-filtSeq.VBFMjjIntervalFilter.PhotonJetOverlapRemoval = True
-filtSeq.VBFMjjIntervalFilter.ElectronJetOverlapRemoval = True
-filtSeq.VBFMjjIntervalFilter.TauJetOverlapRemoval = True
+filtSeq.xAODVBFMjjIntervalFilter.RapidityAcceptance = 5.0
+filtSeq.xAODVBFMjjIntervalFilter.MinSecondJetPT = 15.*GeV
+filtSeq.xAODVBFMjjIntervalFilter.MinOverlapPT = 15.*GeV
+filtSeq.xAODVBFMjjIntervalFilter.TruthJetContainerName = "AntiKt4TruthJets"
+filtSeq.xAODVBFMjjIntervalFilter.NoJetProbability  = 1000.0*0.0000252
+filtSeq.xAODVBFMjjIntervalFilter.OneJetProbability = 1000.0*0.0000252
+filtSeq.xAODVBFMjjIntervalFilter.LowMjjProbability = 1000.0*0.0000252
+filtSeq.xAODVBFMjjIntervalFilter.HighMjjProbability = 1.0
+filtSeq.xAODVBFMjjIntervalFilter.LowMjj = 100.*GeV
+filtSeq.xAODVBFMjjIntervalFilter.TruncateAtLowMjj = False
+filtSeq.xAODVBFMjjIntervalFilter.HighMjj = 2500.*GeV
+filtSeq.xAODVBFMjjIntervalFilter.TruncateAtHighMjj = False
+filtSeq.xAODVBFMjjIntervalFilter.PhotonJetOverlapRemoval = True
+filtSeq.xAODVBFMjjIntervalFilter.ElectronJetOverlapRemoval = True
+filtSeq.xAODVBFMjjIntervalFilter.TauJetOverlapRemoval = True
 
 
 
