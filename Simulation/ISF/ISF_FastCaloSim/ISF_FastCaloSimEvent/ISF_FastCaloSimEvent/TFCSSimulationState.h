bcf3307e60e08a8f327104740daf192b10cf4088
Ke Li <ke.li@cern.ch>
2022-07-06 11:46:17 +0200

    add an option (default is false) for cuda part for fast calorimeter simulation
    add an option (default is false) for cuda part for fast calorimeter simulation

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/ISF_FastCaloSimEvent/TFCSSimulationState.h
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/ISF_FastCaloSimEvent/TFCSSimulationState.h
@@ -60,6 +60,7 @@ class TFCSSimulationState:public TObject
 
 #ifdef USE_GPU
     //for FastCaloSim-GPU
+    //will not compile by default
     void * get_gpu_rand(){ return m_gpu_rand; };
     void set_gpu_rand(void * rand ){ m_gpu_rand=rand ; } ;
     void * get_geold() { return m_geold; };
