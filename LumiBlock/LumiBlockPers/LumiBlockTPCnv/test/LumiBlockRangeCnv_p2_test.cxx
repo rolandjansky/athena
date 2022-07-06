b50612b9634b614b080cebdc655f08765a71bf96
Scott Snyder <scott.snyder@cern.ch>
2022-07-06 14:47:30 +0200

    LumiBlockTPCnv: Fix thread-safety warnings.
    LumiBlockTPCnv: Fix thread-safety warnings.
    
    Fix thread-safety checker warnings when warnings on calls to unchecked functions
    are enabled.

--- LumiBlock/LumiBlockPers/LumiBlockTPCnv/test/LumiBlockRangeCnv_p2_test.cxx
+++ LumiBlock/LumiBlockPers/LumiBlockTPCnv/test/LumiBlockRangeCnv_p2_test.cxx
@@ -1,8 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
-
-// $Id$
 /**
  * @file LumiBlockTPCnv/test/LumuBlockRangeCnv_p2_test.cxx
  * @author scott snyder <snyder@bnl.gov>
@@ -14,12 +12,13 @@
 #include "LumiBlockTPCnv/LumiBlockRangeCnv_p2.h"
 #include "TestTools/leakcheck.h"
 #include "LumiBlockData/LB_IOVRange.h"
+#include "CxxUtils/checker_macros.h"
 #include "GaudiKernel/MsgStream.h"
 #include <cassert>
 #include <iostream>
 
 
-void test1()
+void test1 ATLAS_NOT_THREAD_SAFE ()
 {
   std::cout << "test1\n";
   Athena_test::Leakcheck check;
@@ -39,7 +38,7 @@ void test1()
 }
 
 
-int main()
+int main ATLAS_NOT_THREAD_SAFE ()
 {
   test1();
   return 0;
