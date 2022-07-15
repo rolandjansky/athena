94881059264d8e8ee98ec2a9f0ae7115e29b85a1
Atlas Nightlybuild <atlas.nightlybuild@cern.ch>
2022-07-15 14:16:23 +0200

    Sweeping !55048 from master to 22.0. Enable RoI-based decoding for MM when running trigger ATR-25917
    Merge branch 'MO_NSW_RoIaccess_0722' into 'master'
    
    Enable RoI-based decoding for MM when running trigger ATR-25917
    
    See merge request atlas/athena!55048
    
    (cherry picked from commit 268642aad48a6ee0d291ef13dcf3d302fffef637)
    
    c2762013 Enable RoI-based decoding for MM when running trigger.

--- MuonSpectrometer/MuonCnv/MuonRdoToPrepData/MuonRdoToPrepData/MM_RdoToMM_PrepData.h
+++ MuonSpectrometer/MuonCnv/MuonRdoToPrepData/MuonRdoToPrepData/MM_RdoToMM_PrepData.h
@@ -8,6 +8,8 @@
 #include "GaudiKernel/ToolHandle.h"
 #include "AthenaBaseComps/AthAlgorithm.h"
 #include "MuonCnvToolInterfaces/IMuonRdoToPrepDataTool.h"
+#include "TrigSteeringEvent/TrigRoiDescriptor.h"
+#include "IRegionSelector/IRegSelTool.h"
 
 
 class MM_RdoToMM_PrepData : public AthAlgorithm {
@@ -23,6 +25,15 @@ private:
 
   ToolHandle<Muon::IMuonRdoToPrepDataTool> m_tool{this,"DecodingTool","Muon::MmRdoToPrepDataTool/MM_PrepDataProviderTool"};
   
+  /// Handle for region selector service
+  ToolHandle<IRegSelTool> m_regsel_mm{this, "RegionSelectorTool", "RegSelTool/RegSelTool_MM", "MM Region Selector Tool"};
+
+  /// Property to decide whether or not to do RoI based decoding
+  Gaudi::Property< bool > m_seededDecoding { this, "DoSeededDecoding", false, "If true do decoding in RoIs"};
+
+  /// ReadHandle for the input RoIs
+  SG::ReadHandleKey<TrigRoiDescriptorCollection> m_roiCollectionKey{ this, "RoIs", "OutputRoIs",  "Name of RoI collection to read in" };
+
   bool                                    m_print_inputRdo; //!<< If true, will dump information about the input RDOs.
   bool                                    m_print_prepData; //!<< If true, will dump information about the resulting PRDs.
 
