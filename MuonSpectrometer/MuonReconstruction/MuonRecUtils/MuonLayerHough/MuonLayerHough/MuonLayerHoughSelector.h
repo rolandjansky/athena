aa7a57c59c338ec62af12d6f1fa29c82024802e6
Shaun Roe <shaun.roe@cern.ch>
2022-07-18 18:54:56 +0200

    23.0-coverity-MuonLayerHough
    23.0-coverity-MuonLayerHough

--- MuonSpectrometer/MuonReconstruction/MuonRecUtils/MuonLayerHough/MuonLayerHough/MuonLayerHoughSelector.h
+++ MuonSpectrometer/MuonReconstruction/MuonRecUtils/MuonLayerHough/MuonLayerHough/MuonLayerHoughSelector.h
@@ -1,17 +1,22 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef MUONLAYERHOUGH_MUONLAYERHOUGHSELECTOR_H
 #define MUONLAYERHOUGH_MUONLAYERHOUGHSELECTOR_H
 
 #include <vector>
+#include <utility> //for std::pair
 
 namespace MuonHough {
     class MuonLayerHoughSelector {
     public:
         /** Default constructor */
         MuonLayerHoughSelector() = default;
+        MuonLayerHoughSelector(const MuonLayerHoughSelector &) = default;
+        MuonLayerHoughSelector(MuonLayerHoughSelector &&) = default;
+        MuonLayerHoughSelector & operator= (MuonLayerHoughSelector &&) = default;
+        MuonLayerHoughSelector & operator= (const MuonLayerHoughSelector &) = default;
         MuonLayerHoughSelector(std::vector<std::pair<int, float>> cutValues);
 
         /** Destructor */
