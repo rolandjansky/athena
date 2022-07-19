687f218cdb33319599d46f0547d54c3332ee33df
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-07-19 13:02:15 +0200

    athenaHLT: read HLT prescale key from COOL if not specified
    When no HLT PSK is specified on the command line, configure the
    HLTPrescaleCondAlg to read them from COOL. This ensures that the correct
    keys are being used if multiple LBs (with different keys) are being
    processed.
    
    Fixes ATR-25974.

--- HLT/Trigger/TrigControl/TrigPSC/python/PscConfig.py
+++ HLT/Trigger/TrigControl/TrigPSC/python/PscConfig.py
@@ -1,17 +1,26 @@
-# Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 
 ## @file   PscConfig.py
 ## @brief  Python flags that are used to configure C++ Psc
 ## @author Frank Winklmeier
 ##
-## This module is loaded in Psc.cxx and flags are set in TrigPSCPython*Setup.py
-## Add flags here that are used to configure the C++ instance
+## This module is loaded in Psc.cxx and some of the flags here are
+## are used to configure the C++ instance.
 
 ## In interactive mode, call AppMgr::initialize() in connect instead of configure
 interactive = False
 
 ## Dump job properties (used in TrigPSCPythonSetup.py)
 dumpJobProperties = False
+
+## Exit after dumping job properties
+exitAfterDump = False
+
+## Load configuration from file and restart process
+reloadAfterDump = False
+
+## Read PSK from database and ignore COOL
+forcePSK = False
 
 ## string:string map equivalent to TrigPsc/Config.h (filled in TrigPsc.cxx)
 optmap = {}
