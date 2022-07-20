ef46a28a3dec49fa4785594af3d52b2bc1ec6aea
Wolfgang Walkowiak <wolfgang.walkowiak@cern.ch>
2022-07-20 15:26:00 +0200

    BPHY23: Adding ART tests
    BPHY23: Adding ART tests

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/makeARTScripts.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/makeARTScripts.py
@@ -23,7 +23,7 @@ formatList = ['PHYSVAL','PHYS',
               'IDTR1',
               'EGAM1', 'EGAM2', 'EGAM3', 'EGAM4', 'EGAM5', 'EGAM6', 'EGAM7', 'EGAM8', 'EGAM9',
               'FTAG1', 'FTAG2', 'FTAG3', 'FTAG4', 'FTAG5',
-              'BPHY1', 'BPHY2', 'BPHY3', 'BPHY4', 'BPHY5', 'BPHY6', 'BPHY7', 'BPHY8', 'BPHY9', 'BPHY10', 'BPHY11', 'BPHY12', 'BPHY13', 'BPHY14','BPHY15','BPHY16', 'BPHY17', 'BPHY18','BPHY19','BPHY20','BPHY21','BPHY22',
+              'BPHY1', 'BPHY2', 'BPHY3', 'BPHY4', 'BPHY5', 'BPHY6', 'BPHY7', 'BPHY8', 'BPHY9', 'BPHY10', 'BPHY11', 'BPHY12', 'BPHY13', 'BPHY14','BPHY15','BPHY16', 'BPHY17', 'BPHY18','BPHY19','BPHY20','BPHY21','BPHY22','BPHY23',
               'MUON0', 'MUON1', 'MUON2', 'MUON3', 'MUON4',
               'TCAL1',
               'HION3','HION4','HION5','HION7','HION8'
@@ -54,6 +54,7 @@ dataLabel = "data18"
 truthLabel = "mc15"
 delayedStreamLabel = "data16DELAYED"
 blsStreamLabel = "data17BPHYSLS"
+mcFileBPHY23 = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.27332925._000069.pool.root.1"
 mcFileBPHY22 = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.12399377._000199.pool.root.1"
 mcFileBPHY21 = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.16767830._000007.pool.root.1"
 mcFileBPHY18 = "/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/DerivationFrameworkART/AOD.16278878._000048.pool.root.1"
@@ -169,7 +170,7 @@ if (makeDataDAODs or makeMCDAODs):
             generateText(formatName,dataLabel,dataFile,False,False,"-1")
             generateText(formatName,delayedStreamLabel,dataFileDelayed,False,False,"-1")
             generateText(formatName,blsStreamLabel,dataFileBLS,False,False,"1000") 
-         elif formatName in ['BPHY13', 'BPHY18','BPHY21','BPHY22']:
+         elif formatName in ['BPHY13', 'BPHY18','BPHY21','BPHY22','BPHY23']:
             generateText(formatName,dataLabel,dataFile,False,False,"-1")
             generateText(formatName,blsStreamLabel,dataFileBLS,False,False,"1000") 
          elif formatName in ['BPHY10','BPHY19']:
@@ -216,6 +217,8 @@ if (makeDataDAODs or makeMCDAODs):
             generateText(formatName,mcLabel,mcFileBPHY21,False,True,"5000")
          elif formatName=="BPHY22":
             generateText(formatName,mcLabel,mcFileBPHY22,False,True,"5000")
+         elif formatName=="BPHY23":
+            generateText(formatName,mcLabel,mcFileBPHY23,False,True,"-1")
          else: generateText(formatName,mcLabel,mcFile,False,True,"-1")
 
 if (makeTruthDAODs):
