b4692a30200920208a8f3d760e8a10ef22d1d2be
Shaun Roe <shaun.roe@cern.ch>
2022-07-20 12:48:40 +0200

    23.0-cppcheck-CscOverlay
    23.0-cppcheck-CscOverlay

--- MuonSpectrometer/MuonOverlay/CscOverlay/src/CscOverlay.cxx
+++ MuonSpectrometer/MuonOverlay/CscOverlay/src/CscOverlay.cxx
@@ -327,7 +327,7 @@ void CscOverlay::mergeCollections(const CscRawDataCollection *bkgCollection,
        uint32_t hash      = std::min( sigHash, ovlHash );
        uint32_t address   = std::min( sigAddress, ovlAddress );
        if (sigSamples.size()!=0 && ovlSamples.size()!=0 && needtoflip(address)){
-         ATH_MSG_DEBUG("Looking for overlap of hashes and addresses within witdths because needtoflip");
+         ATH_MSG_DEBUG("Looking for overlap of hashes and addresses within widths because needtoflip");
          msg() << MSG::VERBOSE ;
          std::set<int> sig;  int lastindex=-1;
          for (std::map< int,std::vector<uint16_t> >::const_iterator si=sigSamples.begin(); si!=sigSamples.end(); ++si) {
@@ -408,7 +408,7 @@ void CscOverlay::mergeCollections(const CscRawDataCollection *bkgCollection,
           else{     continue;     }
 
           //keep count
-          clusterCounts[spuID] += 1;
+          if (spuID <10) clusterCounts[spuID] += 1;
           if ( spuID <= 4 ) rpuCount[0] = 5;
           else if ( spuID > 4 && spuID <= 9 ) rpuCount[1] = 11;
        }//loop over datum
