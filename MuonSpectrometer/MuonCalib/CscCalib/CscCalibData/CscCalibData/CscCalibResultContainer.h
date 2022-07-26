f61b1180beaf44bab998bcd7a86787606d0b87c0
scott snyder <sss@karma>
2022-07-26 15:56:07 -0400

    CscCalibData: Fix cppcheck warnings.
    Pass strings by const reference, not by value.

--- MuonSpectrometer/MuonCalib/CscCalib/CscCalibData/CscCalibData/CscCalibResultContainer.h
+++ MuonSpectrometer/MuonCalib/CscCalib/CscCalibData/CscCalibData/CscCalibResultContainer.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef CSCCALIBDATA_CSCCALIBRESULTCONTAINER_H
@@ -31,7 +31,7 @@ class CscCalibResultContainer : public DataVector<CscCalibResultCollection>
             { };
 
         /** Creates an empty container ready for filling. */
-        CscCalibResultContainer(std::string calibType) 
+        CscCalibResultContainer(const std::string& calibType) 
             : DataVector<CscCalibResultCollection>(),m_calibType(calibType)
             { };
 
