85b6a84b75fd76bd943f263f07f8f5e43b5da0a9
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2022-07-29 15:18:35 +0200

    Legacy Simulation Configuration Module Renaming (Preparation for ComponentAccumulator Migration) (part 1)
    Legacy Simulation Configuration Module Renaming (Preparation for ComponentAccumulator Migration) (part 1)

--- Simulation/G4Extensions/G4CosmicFilter/python/G4CosmicFilterConfigDb.py
+++ Simulation/G4Extensions/G4CosmicFilter/python/G4CosmicFilterConfigDb.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 
 from AthenaCommon.CfgGetter import addTool
 
 # this returns three different tools, depending on the runtime config
-addTool("G4CosmicFilter.G4CosmicFilterConfig.getCosmicFilterTool", "G4UA::G4CosmicFilterTool")
-addTool("G4CosmicFilter.G4CosmicFilterConfig.getStoppedParticleFilterTool", "G4UA::StoppedParticleFilterTool")
+addTool("G4CosmicFilter.G4CosmicFilterConfigLegacy.getCosmicFilterTool", "G4UA::G4CosmicFilterTool")
+addTool("G4CosmicFilter.G4CosmicFilterConfigLegacy.getStoppedParticleFilterTool", "G4UA::StoppedParticleFilterTool")
