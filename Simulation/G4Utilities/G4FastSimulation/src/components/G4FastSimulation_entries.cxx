28c8f1d484d3de4bc210f9e140c7f90bb7342346
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2022-08-01 20:09:45 +0200

    Add protection against building FastCaloSim G4VFastSimulationModel in AthSimulation builds
    Add protection against building FastCaloSim G4VFastSimulationModel in AthSimulation builds

--- Simulation/G4Utilities/G4FastSimulation/src/components/G4FastSimulation_entries.cxx
+++ Simulation/G4Utilities/G4FastSimulation/src/components/G4FastSimulation_entries.cxx
@@ -1,8 +1,11 @@
 #include "../DeadMaterialShowerTool.h"
 #include "../SimpleFastKillerTool.h"
+#ifndef SIMULATIONBASE
 #include "../FastCaloSimTool.h"
+#endif
 
 DECLARE_COMPONENT( DeadMaterialShowerTool )
 DECLARE_COMPONENT( SimpleFastKillerTool )
+#ifndef SIMULATIONBASE
 DECLARE_COMPONENT( FastCaloSimTool )
-
+#endif
