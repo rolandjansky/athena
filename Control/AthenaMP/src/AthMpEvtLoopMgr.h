1589e2c0138c920385b486748bff731c40aeccf4
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2022-08-05 19:05:03 +0200

    Merge branch 'master-eventprintoutlvl' into 'master'
    DerivationFrameworkConfiguration+RecJobTransforms: Set Loop Manager EventPrintoutInterval to 100 events in CA-based AODMerge and Derivations
    
    See merge request atlas/athena!55613
    
    (cherry picked from commit d8d47324d5e9c2b4f8339a14f17928669cc3a7f0)
    
    b4c2df1e Set Loop Manager EventPrintoutInterval to 100 events in CA-based AODMerge and Derivations
    5c8bef7e Introduce EventPrintoutInterval to AthMpEvtLoopMgr and pass this along to AthenaEventLoopMgr

--- Control/AthenaMP/src/AthMpEvtLoopMgr.h
+++ Control/AthenaMP/src/AthMpEvtLoopMgr.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2019 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef ATHENAMP_ATHMPEVTLOOPMGR_H
@@ -49,6 +49,7 @@ class AthMpEvtLoopMgr
   int                            m_nPollingInterval;      // in milliseconds
   int                            m_nMemSamplingInterval;  // in seconds
   int                            m_nEventsBeforeFork;
+  unsigned int                   m_eventPrintoutInterval;
   pid_t                          m_masterPid;
 
   // vectors for collecting memory samples
