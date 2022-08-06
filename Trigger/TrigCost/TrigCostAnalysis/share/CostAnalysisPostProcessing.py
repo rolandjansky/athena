ed61d33fccf7da38be77885fb0c6613d8c280db5
Aleksandra Poreba <aleksandra.poreba@cern.ch>
2022-08-06 18:22:57 +0200

    TrigCost: Add maximum number of ranges to postprocess
    TrigCost: Add maximum number of ranges to postprocess

--- Trigger/TrigCost/TrigCostAnalysis/share/CostAnalysisPostProcessing.py
+++ Trigger/TrigCost/TrigCostAnalysis/share/CostAnalysisPostProcessing.py
@@ -26,7 +26,8 @@ def main():
     parser.add_argument('--partition', default='ATLAS', help='Used partition (needed to read OKS details)')
     parser.add_argument('--underflowThreshold', default=0.5, help='Threshold of underflow percent value to save warning in metadata tree.')
     parser.add_argument('--overflowThreshold', default=0.1, help='Threshold of underflow percent value to save warning in metadata tree.')
-    parser.add_argument('--dumpAlgorithmSummary', action='store_true', help='Print algorithm\'s mean time of execution to the log file')     
+    parser.add_argument('--dumpAlgorithmSummary', action='store_true', help='Print algorithm\'s mean time of execution to the log file') 
+    parser.add_argument('--maxRanges', type=int, default=5, help="Maximum number of ranges to process. Use -1 to process all.")   
     args = parser.parse_args()
     
     inputFile = ROOT.TFile(args.file, 'READ')
@@ -40,7 +41,7 @@ def main():
     }
 
     if inputFile.IsOpen():
-        warningMsg = exploreTree(inputFile, args.dumpAlgorithmSummary, args.underflowThreshold, args.overflowThreshold)
+        warningMsg = exploreTree(inputFile, args.dumpAlgorithmSummary, args.underflowThreshold, args.overflowThreshold, args.maxRanges)
         saveMetadata(inputFile, metadata, warningMsg)
     else:
         log.error("File %s not found", args.file)
